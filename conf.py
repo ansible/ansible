8bd523fe3689502e13d2ec75e655c997be791631
Tim Bielawa <tbielawa@redhat.com>
2012-03-09 11:50:07 -0500

    More bootstrap tweaks

--- conf.py
+++ conf.py
@@ -154,7 +154,7 @@ html_last_updated_fmt = '%b %d, %Y'
 #html_split_index = False
 
 # If true, the reST sources are included in the HTML build as _sources/<name>.
-#html_copy_source = True
+html_copy_source = False
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
