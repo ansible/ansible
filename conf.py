59d2f57d642ea7487d1b72623157a995137577ee
Michael DeHaan <michael.dehaan@gmail.com>
2012-03-31 22:44:02 -0400

    Add favicon to pages

--- conf.py
+++ conf.py
@@ -123,7 +123,7 @@ html_title = 'Ansible - SSH-Based Configuration Management & Deployment'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
