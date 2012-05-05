bad2cf2aaebe2b917ef8d29ec9fd51faa5e44c33
Michael DeHaan <michael.dehaan@gmail.com>
2012-05-05 16:37:28 -0400

    missing files

---
+++ lib/ansible/host.py
@@ -0,0 +1,62 @@
+# (c) 2012, Michael DeHaan <michael.dehaan@gmail.com>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+#############################################
+
+from ansible import errors
+import ansible.constants as C
+
+class Host(object):
+    """ 
+    Group of ansible hosts
+    """
+
+    def __init__(self, name=None, port=None):
+        self.name = name
+        self.vars = {}
+        self.groups = []
+        if port and port != C.DEFAULT_REMOTE_PORT:
+            self.set_variable('ansible_ssh_port', int(port))
+
+        if self.name is None:
+            raise Exception("host name is required")
+
+    def add_group(self, group):
+        self.groups.append(group)
+
+    def set_variable(self, key, value):
+        self.vars[key]=value;
+
+    def get_groups(self):
+        groups = {}
+        for g in self.groups:
+            groups[g.name] = g
+            ancestors = g.get_ancestors()
+            for a in ancestors:
+                groups[a.name] = a                
+        return groups.values()
+
+    def get_variables(self):
+        results = {}
+        for group in self.groups:
+            results.update(group.get_variables())
+        results.update(self.vars)
+        results['inventory_hostname'] = self.name
+        groups = self.get_groups()
+        results['group_names'] = [ g.name for g in groups if g.name != 'all']
+        return results
+
