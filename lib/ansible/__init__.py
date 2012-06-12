a454bc6738e9d5eb055beb199a02e9d37d19c456
Tim Bielawa <tbielawa@redhat.com>
2012-06-12 11:02:09 -0400

    Versioning updates for 0.4.1

--- lib/ansible/__init__.py
+++ lib/ansible/__init__.py
@@ -14,4 +14,4 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-__version__ = '0.4'
+__version__ = '0.4.1'
