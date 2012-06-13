1bf2494c8063fd1ef22a156bc2e8f1b1e5af9778
Tim Bielawa <tbielawa@redhat.com>
2012-06-13 09:43:44 -0400

    Fix missing __author__ in __init__. Correct 'AUTHORS' credits in manpages.

--- lib/ansible/__init__.py
+++ lib/ansible/__init__.py
@@ -15,3 +15,4 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 __version__ = '0.4.1'
+__author__ = 'Michael DeHaan'
