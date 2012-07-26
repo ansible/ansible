f371092845866e900f473a63965509532dc772bb
Michael DeHaan <michael.dehaan@gmail.com>
2012-07-25 20:31:57 -0400

    rm stray comment

--- lib/ansible/color.py
+++ lib/ansible/color.py
@@ -40,7 +40,6 @@ codeCodes = {
     'blue':      '0;34',        'white':          '1;37',
     'green':     '0;32',        'bright blue':    '1;34',
     'cyan':      '0;36',        'bright green':   '1;32',
-#    'cyan':      '0;32',        'bright green':   '1;32',
     'red':       '0;31',        'bright cyan':    '1;36',
     'purple':    '0;35',        'bright red':     '1;31',
     'yellow':    '0;33',        'bright purple':  '1;35',
