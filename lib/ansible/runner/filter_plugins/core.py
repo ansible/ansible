5a98c1f95a4396d8e314431576050e09c6fb1e15
Jeroen Hoekx <jeroen@hoekx.be>
2012-11-05 15:09:34 +0100

    Support custom jinja2 filters.
    This uses the plugin framework to add filter plugins.
    The previously hardcoded core filters are defined using the plugin
    framework now.

---
+++ lib/ansible/runner/filter_plugins/core.py
@@ -0,0 +1,31 @@
+# (c) 2012, Jeroen Hoekx <jeroen@hoekx.be>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+import yaml
+
+class FilterModule(object):
+    ''' Ansible core jinja2 filters '''
+
+    def filters(self):
+        return {
+            'to_json': json.dumps,
+            'from_json': json.loads,
+            'to_yaml': yaml.dump,
+            'from_yaml': yaml.load,
+        }
+    
