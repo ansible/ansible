12bae7649ae6df0e9c0c647dea228a6e72afc8b2
smoothify <dev@smoothify.com>
2012-12-30 23:43:04 +0100

    Fix recursive copy functionality, to make it work more reliably in more use cases.

--- lib/ansible/runner/action_plugins/rcopy.py
+++ lib/ansible/runner/action_plugins/rcopy.py
@@ -20,6 +20,7 @@ import pwd
 import random
 import traceback
 import tempfile
+from collections import defaultdict
 
 import ansible.constants as C
 from ansible import utils
@@ -39,10 +40,6 @@ class ActionModule(object):
         options = utils.parse_kv(module_args)
         source  = options.get('src', None)
         dest    = options.get('dest', None)
-
-        if dest.endswith("/"):
-            base = os.path.basename(source)
-            dest = os.path.join(dest, base)
 
         if (source is None and not 'first_available_file' in inject) or dest is None:
             result=dict(failed=True, msg="src and dest are required")
@@ -66,55 +63,73 @@ class ActionModule(object):
         source = utils.template(self.runner.basedir, source, inject)
         source = utils.path_dwim(self.runner.basedir, source)
 
-        source_files = []
-        results = []
+        # Try to emulate rsync behaviour:
+        # A trailing / on a source name means "copy the contents of this directory".
+        # Without a trailing slash it means "copy the directory".
+        if source.endswith("/"):
+            source_base = source
+        else:
+            source_base = utils.rreplace(os.path.basename(source), '', source)
+
+        file_queue = defaultdict(list)
 
         if os.path.isdir(source):
-            for base_path, sub_folders, files in os.walk(source):
-                for file in files:
-                    source_files.append(os.path.join(base_path, file))
+            for directory, sub_folders, filenames in os.walk(source):
+                directory = utils.lreplace(source_base, '', directory)
+                file_queue[directory] = []
+                for filename in filenames:
+                    file_queue[directory].append(filename)
         else:
-            source_files.append(source)
+            directory = source_base
+            filename = os.path.basename(source)
+            file_queue[directory] = []
+            file_queue[directory].append(filename)
 
         changed = False
 
-        for source_file in source_files:
-            # We need to get a new tmp path for each file, otherwise the copy module deletes the folder.
-            tmp = self.runner._make_tmp_path(conn)
-            local_md5 = utils.md5(source_file)
+        for source_directory, source_files in file_queue.items():
+            source_path = os.path.join(source_base, source_directory.lstrip('/'))
+            dest_path = os.path.join(dest, source_directory.lstrip('/'))
 
-            if local_md5 is None:
-                result = dict(failed=True, msg="could not find src=%s" % source_file)
-                return ReturnData(conn=conn, result=result)
+            for filename in source_files:
+                module_options = options.copy()
 
-            remote_md5 = self.runner._remote_md5(conn, tmp, dest)
+                dest_file = os.path.join(dest_path, filename)
+                source_file = os.path.join(source_path, filename)
 
-            exec_rc = None
-            if local_md5 != remote_md5:
-                # transfer the file to a remote tmp location
+                # We need to get a new tmp path for each file, otherwise the copy module deletes the folder.
+                tmp = self.runner._make_tmp_path(conn)
+                local_md5 = utils.md5(source_file)
+
+                if local_md5 is None:
+                    result = dict(failed=True, msg="could not find src=%s" % source_file)
+                    return ReturnData(conn=conn, result=result)
+
+                remote_md5 = self.runner._remote_md5(conn, tmp, dest)
                 tmp_src = tmp + os.path.basename(source_file)
-                conn.put_file(source_file, tmp_src)
-                # fix file permissions when the copy is done as a different user
-                if self.runner.sudo and self.runner.sudo_user != 'root':
-                    self.runner._low_level_exec_command(conn, "chmod a+r %s" % tmp_src, tmp)
+                module_options.update({'src': tmp_src, 'dest': dest_file})
 
-                # run the copy module
-                module_args = "%s src=%s" % (module_args, tmp_src)
-                module_return = self.runner._execute_module(conn, tmp, 'copy', module_args, inject=inject)
+                module_args = ' '.join(['%s=%s' % (key, value) for (key, value) in module_options.items()])
 
-            else:
-                # no need to transfer the file, already correct md5, but still need to call
-                # the file module in case we want to change attributes
+                exec_rc = None
+                if local_md5 != remote_md5:
+                    # transfer the file to a remote tmp location
+                    conn.put_file(source_file, tmp_src)
+                    # fix file permissions when the copy is done as a different user
+                    if self.runner.sudo and self.runner.sudo_user != 'root':
+                        self.runner._low_level_exec_command(conn, "chmod a+r %s" % tmp_src, tmp)
+                    # run the copy module
+                    module_return = self.runner._execute_module(conn, tmp, 'rcopy', module_args, inject=inject)
+                else:
+                    # no need to transfer the file, already correct md5, but still need to call
+                    # the file module in case we want to change attributes
+                    module_return = self.runner._execute_module(conn, tmp, 'file', module_args, inject=inject)
 
-                tmp_src = tmp + os.path.basename(source_file)
-                module_args = "%s src=%s" % (module_args, tmp_src)
-                module_return = self.runner._execute_module(conn, tmp, 'file', module_args, inject=inject)
-
-            module_result = module_return.result
-            if 'failed' in module_result and module_result['failed'] == True:
-                return module_return
-            if 'changed' in module_result and module_result['changed'] == True:
-                changed = True
+                module_result = module_return.result
+                if 'failed' in module_result and module_result['failed'] == True:
+                    return module_return
+                if 'changed' in module_result and module_result['changed'] == True:
+                    changed = True
 
         res_args = dict(
             dest=dest, src=source, changed=changed
