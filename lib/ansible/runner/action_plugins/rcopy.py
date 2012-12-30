2fbbe95c693465456e2ab8c06fdedbff478778c9
smoothify <dev@smoothify.com>
2012-12-30 16:42:17 +0100

    Initial changes to allow rcopy to handle recursive files/folders.

--- lib/ansible/runner/action_plugins/rcopy.py
+++ lib/ansible/runner/action_plugins/rcopy.py
@@ -66,31 +66,58 @@ class ActionModule(object):
         source = utils.template(self.runner.basedir, source, inject)
         source = utils.path_dwim(self.runner.basedir, source)
 
-        local_md5 = utils.md5(source)
-        if local_md5 is None:
-            result=dict(failed=True, msg="could not find src=%s" % source)
-            return ReturnData(conn=conn, result=result)
+        source_files = []
+        results = []
 
-        remote_md5 = self.runner._remote_md5(conn, tmp, dest)
-
-        exec_rc = None
-        if local_md5 != remote_md5:
-            # transfer the file to a remote tmp location
-            tmp_src = tmp + os.path.basename(source)
-            conn.put_file(source, tmp_src)
-            # fix file permissions when the copy is done as a different user
-            if self.runner.sudo and self.runner.sudo_user != 'root':
-                self.runner._low_level_exec_command(conn, "chmod a+r %s" % tmp_src, tmp)
-
-            # run the copy module
-            module_args = "%s src=%s" % (module_args, tmp_src)
-            return self.runner._execute_module(conn, tmp, 'copy', module_args, inject=inject)
-
+        if os.path.isdir(source):
+            for base_path, sub_folders, files in os.walk(source):
+                for file in files:
+                    source_files.append(os.path.join(base_path, file))
         else:
-            # no need to transfer the file, already correct md5, but still need to call
-            # the file module in case we want to change attributes
+            source_files.append(source)
 
-            tmp_src = tmp + os.path.basename(source)
-            module_args = "%s src=%s" % (module_args, tmp_src)
-            return self.runner._execute_module(conn, tmp, 'file', module_args, inject=inject)
+        changed = False
 
+        for source_file in source_files:
+            # We need to get a new tmp path for each file, otherwise the copy module deletes the folder.
+            tmp = self.runner._make_tmp_path(conn)
+            local_md5 = utils.md5(source_file)
+
+            if local_md5 is None:
+                result = dict(failed=True, msg="could not find src=%s" % source_file)
+                return ReturnData(conn=conn, result=result)
+
+            remote_md5 = self.runner._remote_md5(conn, tmp, dest)
+
+            exec_rc = None
+            if local_md5 != remote_md5:
+                # transfer the file to a remote tmp location
+                tmp_src = tmp + os.path.basename(source_file)
+                conn.put_file(source_file, tmp_src)
+                # fix file permissions when the copy is done as a different user
+                if self.runner.sudo and self.runner.sudo_user != 'root':
+                    self.runner._low_level_exec_command(conn, "chmod a+r %s" % tmp_src, tmp)
+
+                # run the copy module
+                module_args = "%s src=%s" % (module_args, tmp_src)
+                module_return = self.runner._execute_module(conn, tmp, 'copy', module_args, inject=inject)
+
+            else:
+                # no need to transfer the file, already correct md5, but still need to call
+                # the file module in case we want to change attributes
+
+                tmp_src = tmp + os.path.basename(source_file)
+                module_args = "%s src=%s" % (module_args, tmp_src)
+                module_return = self.runner._execute_module(conn, tmp, 'file', module_args, inject=inject)
+
+            module_result = module_return.result
+            if 'failed' in module_result and module_result['failed'] == True:
+                return module_return
+            if 'changed' in module_result and module_result['changed'] == True:
+                changed = True
+
+        res_args = dict(
+            dest=dest, src=source, changed=changed
+        )
+
+        return ReturnData(conn=conn, result=res_args)
