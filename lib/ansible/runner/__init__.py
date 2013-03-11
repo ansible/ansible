bbfbd403a63ceef2d59a9cf22c5e40313b563eda
Michael DeHaan <michael.dehaan@gmail.com>
2013-03-11 17:19:43 -0400

    Quote remote md5 operations.  Fixes #2368

--- lib/ansible/runner/__init__.py
+++ lib/ansible/runner/__init__.py
@@ -546,6 +546,7 @@ class Runner(object):
     def _remote_md5(self, conn, tmp, path):
         ''' takes a remote md5sum without requiring python, and returns 0 if no file '''
 
+        path = pipes.quote(path)
         test = "rc=0; [ -r \"%s\" ] || rc=2; [ -f \"%s\" ] || rc=1; [ -d \"%s\" ] && rc=3" % (path, path, path)
         md5s = [
             "(/usr/bin/md5sum %s 2>/dev/null)" % path,  # Linux
