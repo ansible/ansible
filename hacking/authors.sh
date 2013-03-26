d59ac291c6a0f173c373b7f13b27b3f4e3ab5fec
Michael DeHaan <michael.dehaan@gmail.com>
2013-03-25 23:36:53 -0400

    Remove authors file from version control (can still be built from 'make authors')

--- hacking/authors.sh
+++ hacking/authors.sh
@@ -4,11 +4,11 @@ set -e
 
 # Get a list of authors ordered by number of commits
 # and remove the commit count column
-AUTHORS=$(git --no-pager shortlog -nse | cut -f 2-)
+AUTHORS=$(git --no-pager shortlog -nse | cut -f 2- | sort -f)
 if [ -z "$AUTHORS" ] ; then
     echo "Authors list was empty"
     exit 1
 fi
 
 # Display the authors list and write it to the file
-echo "$AUTHORS" | tee "$(git rev-parse --show-toplevel)/AUTHORS.TXT" | sort
+echo "$AUTHORS" | tee "$(git rev-parse --show-toplevel)/AUTHORS.TXT"
