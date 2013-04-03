60e773df8ed69cb38bb1df4831bd3f907ea814cf
Yang Zhang <yaaang@gmail.com>
2013-04-03 14:30:52 -0700

    Require paramiko 1.10+

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(name='ansible',
       author_email='michael.dehaan@gmail.com',
       url='http://ansible.github.com/',
       license='GPLv3',
-      install_requires=['paramiko', 'jinja2', "PyYAML"],
+      install_requires=['paramiko>=1.10', 'jinja2', "PyYAML"],
       package_dir={ 'ansible': 'lib/ansible' },
       packages=[
          'ansible',
