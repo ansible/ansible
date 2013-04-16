f74a1fa4f0b1a928a85c1316dfceb42637624a2f
Seth Vidal <skvidal@fedoraproject.org>
2013-04-16 15:15:58 -0400

    make atomic_replace use shutil.copy2 instead of os.rename() so it will work across filesystems

--- lib/ansible/module_common.py
+++ lib/ansible/module_common.py
@@ -797,7 +797,11 @@ class AnsibleModule(object):
             if self.selinux_enabled():
                 context = self.selinux_default_context(dest)
                 self.set_context_if_different(src, context, False)
-        os.rename(src, dest)
+        try:
+            shutil.copy2(src, dest)
+        except shutil.Error, e:
+            self.fail_json(msg='Could not atomic_replace file: %s to %s: %s' % (src, dest, e))
+            
 
     def run_command(self, args, check_rc=False, close_fds=False, executable=None, data=None):
         '''
