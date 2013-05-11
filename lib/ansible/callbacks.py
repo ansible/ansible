ea8302e68d8f2ee239be32792e212fd6754fa5c8
Michael DeHaan <michael@ansibleworks.com>
2013-05-11 17:24:40 -0400

    Indentation.

--- lib/ansible/callbacks.py
+++ lib/ansible/callbacks.py
@@ -66,13 +66,13 @@ def get_cowsay_info():
 cowsay, noncow = get_cowsay_info()
 
 def log_lockfile():
-   tempdir = tempfile.gettempdir() 
-   uid = os.getuid()
-   path = os.path.join(tempdir, ".ansible-lock.%s" % uid)
-   if not os.path.exists(path):
-       fh = open(path, 'w')
-       fh.close()   
-   return path
+    tempdir = tempfile.gettempdir() 
+    uid = os.getuid()
+    path = os.path.join(tempdir, ".ansible-lock.%s" % uid)
+    if not os.path.exists(path):
+        fh = open(path, 'w')
+        fh.close()   
+    return path
 
 LOG_LOCK = open(log_lockfile(), 'r')
 
