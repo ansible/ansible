517fc0461f12e8a08a49cca4ac4e8c994866210a
Les Aker <me@lesaker.org>
2013-05-19 11:57:08 -0400

    ignore unicode errors in stdout

--- lib/ansible/utils/__init__.py
+++ lib/ansible/utils/__init__.py
@@ -127,6 +127,9 @@ def jsonify(result, format=False):
     if result is None:
         return "{}"
     result2 = result.copy()
+    for key, value in result2.items():
+      if type(value) is str:
+        result2[key] = value.decode('utf-8', 'ignore')
     if format:
         return json.dumps(result2, sort_keys=True, indent=4)
     else:
