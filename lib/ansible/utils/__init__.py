1179d1bbd8b1545fac2dad9e75c9789194a8ce09
Les Aker <me@lesaker.org>
2013-05-19 12:25:19 -0400

    corrected indentation

--- lib/ansible/utils/__init__.py
+++ lib/ansible/utils/__init__.py
@@ -128,8 +128,8 @@ def jsonify(result, format=False):
         return "{}"
     result2 = result.copy()
     for key, value in result2.items():
-      if type(value) is str:
-        result2[key] = value.decode('utf-8', 'ignore')
+        if type(value) is str:
+            result2[key] = value.decode('utf-8', 'ignore')
     if format:
         return json.dumps(result2, sort_keys=True, indent=4)
     else:
