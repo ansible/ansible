7f5dedba0a7b4369a10120a6090aababaff2fe00
Michael DeHaan <michael.dehaan@gmail.com>
2013-07-05 21:42:41 -0400

    Make it possible to tell paramiko to not record new host keys, which can be slow with a large number of hosts. -c ssh is preferred in most cases if you have ControlPersist available, otherwise if you are comfortable you can turn off recording while leaving host key checking on, etc.

--- lib/ansible/runner/connection_plugins/paramiko_ssh.py
+++ lib/ansible/runner/connection_plugins/paramiko_ssh.py
@@ -303,9 +303,14 @@ class Connection(object):
         if self.sftp is not None:
             self.sftp.close()
 
-        if self._any_keys_added():
+        if C.PARAMIKO_RECORD_HOST_KEYS and self._any_keys_added():
+
             # add any new SSH host keys -- warning -- this could be slow
             lockfile = self.keyfile.replace("known_hosts",".known_hosts.lock") 
+            dirname = os.path.dirname(self.keyfile)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+
             KEY_LOCK = open(lockfile, 'w')
             fcntl.lockf(KEY_LOCK, fcntl.LOCK_EX)
             try:
