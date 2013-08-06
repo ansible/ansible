c91ae21f4dd54205cca66bf93f38bd71d1e87dcf
Chris Church <chris@ninemoreminutes.com>
2013-08-06 12:42:30 -0400

    Correctly check for failed status from an async task.  Use runner_on_async_failed callback (instead of runner_on_failed) when an async task times out.  Add runner_on_async_ok callback when a task is started in fire and forget mode.

--- lib/ansible/playbook/__init__.py
+++ lib/ansible/playbook/__init__.py
@@ -277,7 +277,7 @@ class PlayBook(object):
         # since these likely got killed by async_wrapper
         for host in poller.hosts_to_poll:
             reason = { 'failed' : 1, 'rc' : None, 'msg' : 'timed out' }
-            self.runner_callbacks.on_failed(host, reason)
+            self.runner_callbacks.on_async_failed(host, reason, poller.jid)
             results['contacted'][host] = reason
 
         return results
@@ -319,6 +319,9 @@ class PlayBook(object):
             if task.async_poll_interval > 0:
                 # if not polling, playbook requested fire and forget, so don't poll
                 results = self._async_poll(poller, task.async_seconds, task.async_poll_interval)
+            else:
+                for (host, res) in results.get('contacted', {}).iteritems():
+                    self.runner_callbacks.on_async_ok(host, res, poller.jid)
 
         contacted = results.get('contacted',{})
         dark      = results.get('dark', {})
