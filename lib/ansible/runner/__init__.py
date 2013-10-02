070d183bc1b5a893f09e06b0b11087ad290b69d6
bennojoy <benno@ansibleworks.com>
2013-10-02 19:05:10 +0530

    fixed the extra colon

--- lib/ansible/runner/__init__.py
+++ lib/ansible/runner/__init__.py
@@ -660,7 +660,7 @@ class Runner(object):
         result = handler.run(conn, tmp, module_name, module_args, inject, complex_args)
         # Code for do until feature
         until = self.module_vars.get('until', None)
-        if until is not None and result.comm_ok and "failed" not in result.result::
+        if until is not None and result.comm_ok and "failed" not in result.result:
             inject[self.module_vars.get('register')] = result.result
             cond = template.template(self.basedir, until, inject, expand_lists=False)
             if not utils.check_conditional(cond,  self.basedir, inject, fail_on_undefined=self.error_on_undefined_vars):
