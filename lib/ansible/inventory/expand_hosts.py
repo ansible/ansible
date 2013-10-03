ae813f118bb305d403af53f4418e526be34492c7
Martynas Mickevicius <mmartynas@gmail.com>
2013-10-03 11:41:34 +0300

    Allow leading ranges in the inventory host entries.

--- lib/ansible/inventory/expand_hosts.py
+++ lib/ansible/inventory/expand_hosts.py
@@ -41,8 +41,7 @@ def detect_range(line = None):
 
     Returnes True if the given line contains a pattern, else False.
     '''
-    if (not line.startswith("[") and
-        line.find("[") != -1 and
+    if (line.find("[") != -1 and
         line.find(":") != -1 and
         line.find("]") != -1 and
         line.index("[") < line.index(":") < line.index("]")):
