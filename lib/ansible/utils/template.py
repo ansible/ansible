cac3cdf20c2d61387c1dedf68f54055160a74346
Michael DeHaan <michael.dehaan@gmail.com>
2013-10-19 13:52:48 -0400

    Fix a typo.

--- lib/ansible/utils/template.py
+++ lib/ansible/utils/template.py
@@ -305,7 +305,7 @@ def legacy_varReplace(basedir, raw, vars, lookup_fatal=True, depth=0, expand_lis
 
     if result != orig:
         from ansible import utils
-        utils.deprecated("Legacy variable subsitution, such as using ${foo} or $foo instead of {{ foo }} is currently valid but will be phased out and has been out of favor since version 1.2. This is the last of legacy features on our deprecation list. You may continue to use this if you have specific needs for now","1.5")
+        utils.deprecated("Legacy variable substitution, such as using ${foo} or $foo instead of {{ foo }} is currently valid but will be phased out and has been out of favor since version 1.2. This is the last of legacy features on our deprecation list. You may continue to use this if you have specific needs for now","1.5")
     return result
 
 
