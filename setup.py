729dae0e028935105772656ecb472083047b9d71
Christian Berendt <berendt@b1-systems.de>
2013-10-29 09:15:35 +0100

    cleaning up several pep8 issues
    setup.py:13:5: E225
    setup.py:16:80: E501
    setup.py:26:20: E201
    setup.py:26:45: E202
    setup.py:28:10: E121
    setup.py:41:10: E121
    setup.py:47:1: E124

--- setup.py
+++ setup.py
@@ -10,38 +10,40 @@ from distutils.core import setup
 
 # find library modules
 from ansible.constants import DEFAULT_MODULE_PATH
-dirs=os.listdir("./library/")
+dirs = os.listdir("./library/")
 data_files = []
 for i in dirs:
-    data_files.append((os.path.join(DEFAULT_MODULE_PATH, i), glob('./library/' + i + '/*')))
+    data_files.append((os.path.join(DEFAULT_MODULE_PATH, i),
+                      glob('./library/' + i + '/*')))
 
-setup(name='ansible',
-      version=__version__,
-      description='Radically simple IT automation',
-      author=__author__,
-      author_email='michael@ansibleworks.com',
-      url='http://ansibleworks.com/',
-      license='GPLv3',
-      install_requires=['paramiko', 'jinja2', "PyYAML"],
-      package_dir={ 'ansible': 'lib/ansible' },
-      packages=[
-         'ansible',
-         'ansible.utils',
-         'ansible.inventory',
-         'ansible.inventory.vars_plugins',
-         'ansible.playbook',
-         'ansible.runner',
-         'ansible.runner.action_plugins',
-         'ansible.runner.lookup_plugins',
-         'ansible.runner.connection_plugins',
-         'ansible.runner.filter_plugins',
-         'ansible.callback_plugins',
-      ],
-      scripts=[
-         'bin/ansible',
-         'bin/ansible-playbook',
-         'bin/ansible-pull',
-         'bin/ansible-doc'
-      ],
-      data_files=data_files
+setup(
+    name='ansible',
+    version=__version__,
+    description='Radically simple IT automation',
+    author=__author__,
+    author_email='michael@ansibleworks.com',
+    url='http://ansibleworks.com/',
+    license='GPLv3',
+    install_requires=['paramiko', 'jinja2', "PyYAML"],
+    package_dir={'ansible': 'lib/ansible'},
+    packages=[
+        'ansible',
+        'ansible.utils',
+        'ansible.inventory',
+        'ansible.inventory.vars_plugins',
+        'ansible.playbook',
+        'ansible.runner',
+        'ansible.runner.action_plugins',
+        'ansible.runner.lookup_plugins',
+        'ansible.runner.connection_plugins',
+        'ansible.runner.filter_plugins',
+        'ansible.callback_plugins',
+    ],
+    scripts=[
+        'bin/ansible',
+        'bin/ansible-playbook',
+        'bin/ansible-pull',
+        'bin/ansible-doc'
+    ],
+    data_files=data_files
 )
