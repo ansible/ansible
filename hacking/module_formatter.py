8b1a36df989a8ef3b631750920cd5bc7a233b257
Michael DeHaan <michael.dehaan@gmail.com>
2013-11-18 18:55:49 -0500

    Add version_added to all modules missing version_added information, the docs formatter will now raise errors when omitted, updated changelog with new modules.

--- hacking/module_formatter.py
+++ hacking/module_formatter.py
@@ -362,6 +362,13 @@ def main():
             if not doc is None:
  
                 all_keys = []
+
+                if not 'version_added' in doc:
+                    sys.stderr.write("*** ERROR: missing version_added in: %s ***\n" % module)
+                    sys.exit(1)
+                    if doc['version_added'] == 'historical':
+                       del doc['version_added']
+
                 for (k,v) in doc['options'].iteritems():
                     all_keys.append(k)
                 all_keys = sorted(all_keys)
