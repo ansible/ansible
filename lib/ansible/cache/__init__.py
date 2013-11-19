6c9b72c1d404c1e0f2e30c1828e2fa45c9d3d3ec
Brian Coca <briancoca+dev@gmail.com>
2013-11-19 18:03:13 -0500

    switched to using dictmixin for cache object, mulling about collections.mutablemappings as alternative Signed-off-by: Brian Coca <briancoca+dev@gmail.com>

--- lib/ansible/cache/__init__.py
+++ lib/ansible/cache/__init__.py
@@ -1,55 +1,53 @@
 from ansible import utils
+from UserDict import DictMixin
 
-class Cache(dict):
+class Cache(DictMixin, dict):
 
     def __init__(self, default=dict, *a, **kw):
         if (default is not None and not hasattr(default, '__call__')):
             raise TypeError('first argument must be callable')
-        dict.__init__(self, *a, **kw)
-        self.__dict__ = self
-        self.default = default
-        self.caches = utils.plugins.cache_loader.all()
+        self.data = dict(*a, **kw)
+        self._default = default
+        self._caches = utils.plugins.cache_loader.all()
 
     def __getitem__(self, name):
         value = None
         try:
-            value = dict.__getitem__(self, name)
+            value = super(Cache, self).__getitem__(name)
         except KeyError:
-            value = self.default()
-            for cache in self.caches:
+            for cache in self._caches:
                 value = cache.get(name)
                 if value is not None:
                     break
+
         if value is None:
-            value = self.default()
-        self[name] = value
+            value = self._default()
+            super(Cache, self).__setitem__(name, value)
         return value
 
-    def __setitem__(self, name, val):
-        for cache in self.caches:
-            cache.save(name, val)
-        super(Cache, self).__setitem__(name, val)
+    def __setitem__(self, name, value):
+        for cache in self._caches:
+            cache.save(name, value)
+        super(Cache, self).__setitem__(name, value)
 
     def update(self, *a, **kw):
         if a and len(a) > 1:
             raise TypeError("update expected at most 1 arguments, got %d" % len(a))
             other = dict(*a, **kw)
-            self[key] = other[key]
+            super(Cache, self).__setitem__(name, other[key])
             for key in other:
-                for cache in self.caches:
+                for cache in self._caches:
                     cache.save(key, other[key])
         for key in kw:
-            self[key] = kw[key]
-            for cache in self.caches:
+            super(Cache, self).__setitem__(name, kw[key])
+            for cache in self._caches:
                 cache.save(key, kw[key])
 
     def setdefault(self, key, value=None):
         if value is None:
-            value = self.default()
-        if key not in self:
-            self[key] = value
-        return self[key]
+            value = self._default()
+            super(Cache, self).__setitem__(name, value)
+        return value
 
-    def clear(self):
-        super(Cache, self).clear()
-        self.__init__()
+#    def clear(self):
+#        self.data.clear()
