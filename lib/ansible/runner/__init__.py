9af44b1b429d5ea53cc7425f16af7cd7eed6376b
Aleksey Ovcharenko <aleksey.ovcharenko@gmail.com>
2013-11-22 04:20:14 +0200

    Update to 4 space indent

--- lib/ansible/runner/__init__.py
+++ lib/ansible/runner/__init__.py
@@ -588,7 +588,7 @@ class Runner(object):
         self.sudo_pass = inject.get('ansible_sudo_pass', self.sudo_pass)
 
         if actual_private_key_file is not None:
-          actual_private_key_file = os.path.expanduser(actual_private_key_file)
+            actual_private_key_file = os.path.expanduser(actual_private_key_file)
 
         if self.accelerate and actual_transport != 'local':
             #Fix to get the inventory name of the host to accelerate plugin
@@ -631,7 +631,7 @@ class Runner(object):
                 self.sudo_pass = delegate_info.get('ansible_sudo_pass', self.sudo_pass)
 
                 if actual_private_key_file is not None:
-                  actual_private_key_file = os.path.expanduser(actual_private_key_file)
+                    actual_private_key_file = os.path.expanduser(actual_private_key_file)
 
                 for i in delegate_info:
                     if i.startswith("ansible_") and i.endswith("_interpreter"):
