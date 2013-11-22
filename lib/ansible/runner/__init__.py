512ebdb9713744d1690ea7095ff8ee80ef1c713e
James Tanner <tanner.jc@gmail.com>
2013-11-22 00:09:37 -0500

    Revert "Merge pull request #5001 from ovcharenko/devel"
    This reverts commit d6c3103a0049a3da3b2f531ff075cc4485b2f4bc, reversing
    changes made to b11f96eaa47515ef0ab18e73e13e8ce5c8b79b82.

--- lib/ansible/runner/__init__.py
+++ lib/ansible/runner/__init__.py
@@ -584,7 +584,7 @@ class Runner(object):
         actual_user = inject.get('ansible_ssh_user', self.remote_user)
         actual_pass = inject.get('ansible_ssh_pass', self.remote_pass)
         actual_transport = inject.get('ansible_connection', self.transport)
-        actual_private_key_file = os.path.expanduser(inject.get('ansible_ssh_private_key_file', self.private_key_file))
+        actual_private_key_file = inject.get('ansible_ssh_private_key_file', self.private_key_file)
         self.sudo_pass = inject.get('ansible_sudo_pass', self.sudo_pass)
 
         if self.accelerate and actual_transport != 'local':
@@ -623,7 +623,7 @@ class Runner(object):
                 actual_port = delegate_info.get('ansible_ssh_port', port)
                 actual_user = delegate_info.get('ansible_ssh_user', actual_user)
                 actual_pass = delegate_info.get('ansible_ssh_pass', actual_pass)
-                actual_private_key_file = os.path.expanduser(delegate_info.get('ansible_ssh_private_key_file', self.private_key_file))
+                actual_private_key_file = delegate_info.get('ansible_ssh_private_key_file', self.private_key_file)
                 actual_transport = delegate_info.get('ansible_connection', self.transport)
                 self.sudo_pass = delegate_info.get('ansible_sudo_pass', self.sudo_pass)
                 for i in delegate_info:
