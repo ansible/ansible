53bb4a3c8352638bed4394b0388638d57f1fe678
Brian Coca <briancoca+dev@gmail.com>
2013-11-23 08:30:41 -0500

    now value is correct for in memory cache, if it comes from cache plugin, new facts or default (dict).
    corrected setdefault param mismatch
    
    Signed-off-by: Brian Coca <briancoca+dev@gmail.com>

--- lib/ansible/cache/__init__.py
+++ lib/ansible/cache/__init__.py
@@ -3,17 +3,23 @@ from UserDict import DictMixin
 
 class Cache(DictMixin, dict):
 
+
     def __init__(self, default=dict, *a, **kw):
         if (default is not None and not hasattr(default, '__call__')):
             raise TypeError('first argument must be callable')
         self._default = default
+        #TODO: pull single/list from config, maybe ALL as special keyword
         self._caches = utils.plugins.cache_loader.all()
+
 
     def __getitem__(self, name):
         value = None
         try:
             value = super(Cache, self).__getitem__(name)
         except KeyError:
+            pass
+
+        if value is None or not value:
             for cache in self._caches:
                 value = cache.get(name)
                 if value is not None:
@@ -21,15 +27,20 @@ class Cache(DictMixin, dict):
 
         if value is None:
             value = self._default()
-            super(Cache, self).__setitem__(name, value)
+
+        super(Cache, self).__setitem__(name, value)
         return value
 
+
     def __setitem__(self, name, value):
+
         for cache in self._caches:
             cache.save(name, value)
         super(Cache, self).__setitem__(name, value)
 
+
     def update(self, *a, **kw):
+
         if a and len(a) > 1:
             raise TypeError("update expected at most 1 arguments, got %d" % len(a))
             other = dict(*a, **kw)
@@ -37,13 +48,17 @@ class Cache(DictMixin, dict):
                 super(Cache, self).__setitem__(name, other[key])
                 for cache in self._caches:
                     cache.save(key, other[key])
+
         for key in kw:
             super(Cache, self).__setitem__(name, kw[key])
             for cache in self._caches:
                 cache.save(key, kw[key])
 
-    def setdefault(self, key, value=None):
+
+    def setdefault(self, name, value=None):
+
         if value is None:
             value = self._default()
             super(Cache, self).__setitem__(name, value)
+
         return value
