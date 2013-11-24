2dcd0179e41870b7f2b9f923b3c93a0be65f2223
Brian Coca <briancoca+dev@gmail.com>
2013-11-23 20:28:11 -0500

    added new functions and error checking
    - now checks if cachd_dir is actualy a dir
    - cached function returns list of cached keys/hosts
    - now stats file to se if it has 'expired'
    
    Signed-off-by: Brian Coca <briancoca+dev@gmail.com>

--- lib/ansible/cache/file.py
+++ lib/ansible/cache/file.py
@@ -18,10 +18,15 @@ class CacheModule(object):
         if not os.path.exists(self._cache_dir):
             os.makedirs(self._cache_dir)
 
+        if not os.path.isdir(self._cache_dir):
+            raise "Not a useable direcory: %s" % self._cache_dir
 
     def save(self, name, val):
 
-        f = open('/'.join([self._cache_dir, str(name)]) ,'w')
+        try:
+            f = open('/'.join([self._cache_dir, str(name)]) ,'w')
+        except OSError as e:
+           raise "Unable to write cache: %s" % str(e)
         f.write(json.dumps(val))
         f.close()
         print 'FILE save: %s = %s\n' % (name, val)
@@ -34,7 +39,28 @@ class CacheModule(object):
 
         if os.path.exists(path):
             if int(time.time() - os.stat(path).st_mtime) < self._expires:
-                f = open(path,'r')
+                try:
+                    f = open(path,'r')
+                except OSError as e:
+                    raise "Unable to read cache: %s" % str(e)
                 ret = json.loads(f.read())
                 f.close()
         return ret
+
+
+    def cached(self):
+
+        ret = []
+
+        for cfile in os.listdir(self._cache_dir):
+            ret.append(cfile)
+        return ret
+
+
+    def __getattribute__(self, name):
+        import inspect
+        returned = object.__getattribute__(self, name)
+        if inspect.isfunction(returned) or inspect.ismethod(returned):
+            print 'called File.', returned.__name__
+        return returned
+
