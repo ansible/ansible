0373e10993ebb418ee1cb11a9ed9d23db1e26a68
anatoly techtonik <techtonik@gmail.com>
2013-12-05 17:27:23 +0300

    constants: More defaults are reloadable

--- lib/ansible/constants.py
+++ lib/ansible/constants.py
@@ -135,6 +135,7 @@ DEFAULT_ASK_SUDO_PASS     ask_sudo_pass     ANSIBLE_ASK_SUDO_PASS     False     
 DEFAULT_SUDO              sudo              ANSIBLE_SUDO              False               B
 DEFAULT_SUDO_EXE          sudo_exe          ANSIBLE_SUDO_EXE          sudo
 DEFAULT_SUDO_FLAGS        sudo_flags        ANSIBLE_SUDO_FLAGS        -H
+DEFAULT_EXECUTABLE        executable        ANSIBLE_EXECUTABLE        /bin/sh
 
 DEFAULT_TRANSPORT         transport         ANSIBLE_TRANSPORT         smart
 DEFAULT_REMOTE_PORT       remote_port       ANSIBLE_REMOTE_PORT       22                  I
@@ -149,6 +150,16 @@ DEFAULT_FILTER_PLUGIN_PATH      filter_plugins      ANSIBLE_FILTER_PLUGINS      
 
 # lookup plugin related
 ANSIBLE_ETCD_URL          etcd_url          ANSIBLE_ETCD_URL          http://127.0.0.1:4001
+
+# software tuning
+DEFAULT_JINJA2_EXTENSIONS  jinja2_extensions  ANSIBLE_JINJA2_EXTENSIONS  None
+ANSIBLE_NOCOLOR            nocolor            ANSIBLE_NOCOLOR            False  B
+ANSIBLE_NOCOWS             nocows             ANSIBLE_NOCOWS             False  B
+
+# not recommended / deprecated
+DEFAULT_HASH_BEHAVIOUR             hash_behaviour             ANSIBLE_HASH_BEHAVIOUR             replace
+DEFAULT_LEGACY_PLAYBOOK_VARIABLES  legacy_playbook_variables  ANSIBLE_LEGACY_PLAYBOOK_VARIABLES  True      B
+DEPRECATION_WARNINGS               deprecation_warnings       ANSIBLE_DEPRECATION_WARNINGS       True      B
 '''
 
 def load_constants(config_str):
@@ -196,17 +207,10 @@ DEFAULT_SCP_IF_SSH        = get_config(p, 'ssh_connection', 'scp_if_ssh',       
 DEFAULT_MANAGED_STR       = get_config(p, DEFAULTS, 'ansible_managed',  None,           'Ansible managed: {file} modified on %Y-%m-%d %H:%M:%S by {uid} on {host}')
 DEFAULT_SYSLOG_FACILITY   = get_config(p, DEFAULTS, 'syslog_facility',  'ANSIBLE_SYSLOG_FACILITY', 'LOG_USER')
 DEFAULT_KEEP_REMOTE_FILES = get_config(p, DEFAULTS, 'keep_remote_files', 'ANSIBLE_KEEP_REMOTE_FILES', False, boolean=True)
-DEFAULT_HASH_BEHAVIOUR    = get_config(p, DEFAULTS, 'hash_behaviour', 'ANSIBLE_HASH_BEHAVIOUR', 'replace')
-DEFAULT_LEGACY_PLAYBOOK_VARIABLES = get_config(p, DEFAULTS, 'legacy_playbook_variables', 'ANSIBLE_LEGACY_PLAYBOOK_VARIABLES', True, boolean=True)
-DEFAULT_JINJA2_EXTENSIONS = get_config(p, DEFAULTS, 'jinja2_extensions', 'ANSIBLE_JINJA2_EXTENSIONS', None)
-DEFAULT_EXECUTABLE        = get_config(p, DEFAULTS, 'executable', 'ANSIBLE_EXECUTABLE', '/bin/sh')
 
-ANSIBLE_NOCOLOR                = get_config(p, DEFAULTS, 'nocolor', 'ANSIBLE_NOCOLOR', None, boolean=True)
-ANSIBLE_NOCOWS                 = get_config(p, DEFAULTS, 'nocows', 'ANSIBLE_NOCOWS', None, boolean=True)
 DISPLAY_SKIPPED_HOSTS          = get_config(p, DEFAULTS, 'display_skipped_hosts', 'DISPLAY_SKIPPED_HOSTS', True, boolean=True)
 DEFAULT_UNDEFINED_VAR_BEHAVIOR = get_config(p, DEFAULTS, 'error_on_undefined_vars', 'ANSIBLE_ERROR_ON_UNDEFINED_VARS', True, boolean=True)
 HOST_KEY_CHECKING              = get_config(p, DEFAULTS, 'host_key_checking',  'ANSIBLE_HOST_KEY_CHECKING',    True, boolean=True)
-DEPRECATION_WARNINGS           = get_config(p, DEFAULTS, 'deprecation_warnings', 'ANSIBLE_DEPRECATION_WARNINGS', True, boolean=True)
 
 # CONNECTION RELATED
 ANSIBLE_SSH_ARGS               = get_config(p, 'ssh_connection', 'ssh_args', 'ANSIBLE_SSH_ARGS', None)
