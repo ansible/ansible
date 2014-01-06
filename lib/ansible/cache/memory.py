629a4be806a2a1d2e3aa69072efbd8ea4fa31fed
Michael DeHaan <michael@ansibleworks.com>
2014-01-06 17:59:00 -0500

    Work in progress on fact caching implementation.

---
+++ lib/ansible/cache/memory.py
@@ -0,0 +1,25 @@
+import os
+import time
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+_cache = {}
+
+class CacheModule(object):
+
+    def __init__(self, *args, **kwargs):
+        global _cache
+        _cache = {}
+
+    def get(self, key, default):
+        global _cache
+        return _cache.get(key, default)
+
+    def set(self, key, value):
+        global cache
+        _cache[key] = value
+
+
