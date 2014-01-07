5f30dbb139bba81fa576aa1f0e863468d9b56534
Michael DeHaan <michael@ansibleworks.com>
2014-01-07 08:47:40 -0500

    Remove unneccessary optimization.

--- lib/ansible/cache/memory.py
+++ lib/ansible/cache/memory.py
@@ -1,29 +1,12 @@
-import os
-import time
-
-try:
-    import json
-except ImportError:
-    import simplejson as json
-
-_cache = {}
 
 class CacheModule(object):
 
     def __init__(self, *args, **kwargs):
-        global _cache
-        _cache = {}
+        self._cache = {}
 
     def get(self, key, default):
-        global _cache
-        return _cache.get(key, default)
+        return self._cache.get(key, default)
 
     def set(self, key, value):
-        global _cache
-        _cache[key] = value
-
-    #def update(self, *args, **kwargs):
-    #   global _cache
-    #   _cache.update(args, kwargs)
-
+        self._cache[key] = value
 
