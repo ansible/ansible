ansible/ansible

commit f493cccbba781df391f955b2dbf6f048f9b615ab

Check real user's known_hosts when sudo ansible...
closes #4834

lib/ansible/runner/connection_plugins/ssh.py
@@ -116,7 +116,7 @@ class Connection(object):
             os.close(self.wfd)
 
     def not_in_host_file(self, host):
-        host_file = os.path.expanduser("~/.ssh/known_hosts")
+        host_file = os.path.expanduser(os.path.expandvars("~${USER}/.ssh/known_hosts"))
         if not os.path.exists(host_file):
             print "previous known host file not found"
             return True

commit 7891afc4a707905aa6536c44b0e34ebdc035aa87

Fix .ssh/known_hosts path expansion.
In particular, do not rely on the $USER environment variable always existing.
tmux for example seems to clear it, causing lots of invalid messages:
"previous known host file not found"

This broke in commit f493cccb, but instead of reverting that commit, we now
fall back to expanding just ~ when $USER is not set.

lib/ansible/runner/connection_plugins/ssh.py
@@ -117,7 +117,11 @@ class Connection(object):
             os.close(self.wfd)
 
     def not_in_host_file(self, host):
-        host_file = os.path.expanduser(os.path.expandvars("~${USER}/.ssh/known_hosts"))
+        if 'USER' in os.environ:
+            host_file = os.path.expandvars("~${USER}/.ssh/known_hosts")
+        else:
+            host_file = "~/.ssh/known_hosts"
+        host_file = os.path.expanduser(host_file)
         if not os.path.exists(host_file):
             print "previous known host file not found"
             return True