fd5d45c3f0bbfe678ca435c84ae2f7343aec040c
g-k-r <gkr@gkr.i24.cc>
2014-01-31 12:34:21 +0100

    modifed test to use get_hosts instead of get_groups
    closes #5749
    
    Conflicts:
    	test/units/TestInventory.py

--- test/units/TestInventory.py
+++ test/units/TestInventory.py
@@ -434,8 +434,7 @@ class TestInventory(unittest.TestCase):
 
     def test_dir_inventory_multiple_groups(self):
          inventory = self.dir_inventory()
-         group_greek = inventory.get_group('greek')
-         group_major_god = inventory.get_group('major-god')
-         actual_host_names = [host.name for host in group_greek.get_hosts()];
-         print "%s : %s " % (group_greek.name, actual_host_names)
-         assert actual_host_names == ['zeus','morpheus']
+         group_greek = inventory.get_hosts('greek')
+         actual_host_names = [host.name for host in group_greek];
+         print "greek : %s " % (actual_host_names)
+         assert actual_host_names == ['zeus','morpheus'] 
