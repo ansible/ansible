320dfd82715f834aa4df7aabeb079af2d8625715
James Tanner <tanner.jc@gmail.com>
2014-02-17 15:05:46 -0500

    Give entropy warning on encrypt

--- lib/ansible/utils/vault.py
+++ lib/ansible/utils/vault.py
@@ -442,6 +442,7 @@ class AES(object):
 
         # Get a block of random data. EL does not have Crypto.Random.new() 
         # so os.urandom is used for cross platform purposes
+        print "WARNING: if encryption hangs, add more entropy"
         salt = os.urandom(bs - len('Salted__'))
 
         key, iv = self.aes_derive_key_and_iv(password, salt, key_length, bs)
