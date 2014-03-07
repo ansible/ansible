f80d02d84114c7565d3c906421bbcfe7c0222f05
James Cammarata <jimi@sngx.net>
2014-03-07 00:07:10 -0600

    Properly wrap logical elements together for su/sudo detection

--- lib/ansible/runner/__init__.py
+++ lib/ansible/runner/__init__.py
@@ -415,7 +415,7 @@ class Runner(object):
 
         environment_string = self._compute_environment_string(inject)
 
-        if tmp.find("tmp") != -1 and (self.sudo and self.sudo_user != 'root') or (self.su and self.su_user != 'root'):
+        if tmp.find("tmp") != -1 and ((self.sudo and self.sudo_user != 'root') or (self.su and self.su_user != 'root')):
             # deal with possible umask issues once sudo'ed to other user
             cmd_chmod = "chmod a+r %s" % remote_module_path
             self._low_level_exec_command(conn, cmd_chmod, tmp, sudoable=False)
