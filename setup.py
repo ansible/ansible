02519a2a77e7d88c32fb3f3b4db7c6cb20da7006
Toshio Kuratomi <toshio@fedoraproject.org>
2014-03-14 13:16:04 -0700

    Use setuptools to get a recent enough version of python-crypto2.6

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(name='ansible',
       author_email='michael@ansible.com',
       url='http://ansible.com/',
       license='GPLv3',
-      install_requires=['paramiko', 'jinja2', "PyYAML"],
+      install_requires=['paramiko', 'jinja2', "PyYAML", 'setuptools', 'pycrypto >= 2.6'],
       package_dir={ 'ansible': 'lib/ansible' },
       packages=[
          'ansible',
