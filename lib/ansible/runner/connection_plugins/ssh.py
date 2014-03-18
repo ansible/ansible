9124de6103778e4bd128f2a3f8827bd6fed9e136
James Cammarata <jimi@sngx.net>
2014-03-18 11:14:56 -0500

    Change print message to vvv for missing known hosts in ssh.py

--- lib/ansible/runner/connection_plugins/ssh.py
+++ lib/ansible/runner/connection_plugins/ssh.py
@@ -157,7 +157,7 @@ class Connection(object):
                         return False
 
         if (hfiles_not_found == len(host_file_list)):
-            print "previous known host file not found"
+            vvv("EXEC previous known host file not found for %s" % host)
         return True
 
     def exec_command(self, cmd, tmp_path, sudo_user=None, sudoable=False, executable='/bin/sh', in_data=None, su_user=None, su=False):
