a0e027fe362fbc209dbeff2f72d6e95f39885c69
James Cammarata <jimi@sngx.net>
2014-04-18 11:40:20 -0500

    Make sure umask is set restrictively before creating any vault files

--- lib/ansible/utils/vault.py
+++ lib/ansible/utils/vault.py
@@ -189,6 +189,7 @@ class VaultEditor(object):
             raise errors.AnsibleError("%s exists, please use 'edit' instead" % self.filename)
 
         # drop the user into vim on file
+        old_umask = os.umask(0077)
         EDITOR = os.environ.get('EDITOR','vim')
         call([EDITOR, self.filename])
         tmpdata = self.read_data(self.filename)
@@ -196,6 +197,7 @@ class VaultEditor(object):
         this_vault.cipher_name = self.cipher_name
         enc_data = this_vault.encrypt(tmpdata)
         self.write_data(enc_data, self.filename)
+        os.umask(old_umask)
 
     def decrypt_file(self):
 
@@ -217,6 +219,9 @@ class VaultEditor(object):
 
         if not HAS_AES or not HAS_COUNTER or not HAS_PBKDF2 or not HAS_HASH:
             raise errors.AnsibleError(CRYPTO_UPGRADE)
+
+        # make sure the umask is set to a sane value
+        old_mask = os.umask(0077)
 
         # decrypt to tmpfile
         tmpdata = self.read_data(self.filename)
@@ -242,6 +247,9 @@ class VaultEditor(object):
 
         # shuffle tmp file into place
         self.shuffle_files(tmp_path, self.filename)
+
+        # and restore the old umask
+        os.umask(old_mask)
 
     def encrypt_file(self):
 
