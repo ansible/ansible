db32e0399cc910510351dfc7451fa51e088cc1bf
Andrew Hamilton <ahamilton55@gmail.com>
2014-05-21 16:42:13 -0700

    Have ec2.py expand tilde and vars when looking up the EC2_INI_PATH env variable

--- plugins/inventory/ec2.py
+++ plugins/inventory/ec2.py
@@ -186,7 +186,7 @@ class Ec2Inventory(object):
 
         config = ConfigParser.SafeConfigParser()
         ec2_default_ini_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ec2.ini')
-        ec2_ini_path = os.environ.get('EC2_INI_PATH', ec2_default_ini_path)
+        ec2_ini_path = os.path.expanduser(os.path.expandvars(os.environ.get('EC2_INI_PATH', ec2_default_ini_path)))
         config.read(ec2_ini_path)
 
         # is eucalyptus?
