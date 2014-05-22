066b7079effbb97f34510a2b4d84b4b6dda56794
Nick Irvine <nirvine@hotheadgames.com>
2014-05-21 19:24:28 -0700

    Clean non-printable chars from stdout instead of dropping the whole thing

--- lib/ansible/runner/__init__.py
+++ lib/ansible/runner/__init__.py
@@ -1075,7 +1075,7 @@ class Runner(object):
             if hasattr(sys.stdout, "isatty"):
                 if "stdout" in data and sys.stdout.isatty():
                     if not string_functions.isprintable(data['stdout']):
-                        data['stdout'] = ''
+                        data['stdout'] = ''.join(c for c in data['stdout'] if string_functions.isprintable(c))
 
             if 'item' in inject:
                 result.result['item'] = inject['item']
