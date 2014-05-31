0c640e23529dbaba24161e0fdbab09f37c8696a2
Martin Joehren <m.joehren@gmail.com>
2014-05-31 12:08:54 +0200

    azure - inventory plugin

--- lib/ansible/module_utils/azure.py
+++ lib/ansible/module_utils/azure.py
@@ -1,13 +1,32 @@
 import json
+import sys
 
-from azure.servicemanagement import *
-import os
+try:
+    import azure
+    from azure.servicemanagement import ServiceManagementService
+    from azure import WindowsAzureError
+    from azure import _USER_AGENT_STRING, _update_request_uri_query
+    from azure.http import HTTPResponse
+    from azure.http import HTTPError
+except ImportError:
+    print("failed=True msg='azure required for this module. Please install with [pip install azure]'")
+    sys.exit(1)
+
+
+import os, sys
+
+
+if sys.version_info < (3,):
+    from urlparse import urlparse
+else:
+    from urllib.parse import urlparse
 
 def azure_common_argument_spec():
     return dict(
         certificate_path=dict(aliases=['CERTIFICATE_PATH'], type='str'),
         subscription_id=dict(aliases=['SUBSCRIPTION_ID'], no_log=True, type='str'),
-        management_endpoint=dict(aliases=['MANAGEMENT_ENDPOINT'], type='str'))
+        cert_thumbprint=dict(aliases=['CERT_THUMBPRINT'], no_log=True, type='str'),
+        cloud_service_cert=dict(aliases=['CLOUD_SERVICE_CERT'], type='str'))
 
 
 def azure_props(x):
@@ -25,47 +44,98 @@ def create_azure_conform_name(name):
         return name
 
 
-class Azure():
+class AzureConfig(object):
     '''
     To use azure from ansible you need at least:
     * an azure account
     * working pem/cer pair (https://github.com/Azure/azure-sdk-for-python)
     * azure python sdk (pip install azure)
     '''
-    AZURE_MANAGEMENT_ENDPOINT = 'https://management.core.windows.net'
 
-    @staticmethod
-    def get_sms(module):
-        subscription_id, certificate_path = Azure.get_creds(module)
-        return ServiceManagementService(subscription_id, certificate_path)
+    def __init__(self, module):
+        config_filename = "azureProfile.json"
+        tmp_filename = os.path.join(os.getcwd(), config_filename)
+        if not os.path.exists(tmp_filename):
+            if "USERPROFILE" in os.environ:
+                tmp_filename = os.path.join(os.environ["USERPROFILE"], ".azure",
+                                            config_filename)
+            elif "HOME" in os.environ:
+                tmp_filename = os.path.join(os.environ["HOME"], ".azure",
+                                            config_filename)
+        if os.path.exists(tmp_filename):
+            with open(tmp_filename, "r") as f:
+                self.ns = json.load(f)
 
-    @staticmethod
-    def connection_info(module):
-        certificate_path = module.params.get('certificate_path')
-        subscription_id = module.params.get('subscription_id')
-        management_endpoint = module.params.get('management_endpoint')
+        self.subscription_id = self._set_field(module, 'subscription_id', u'subscription_id', 'AZURE_SUBSCRIPTION_ID', True)
+        self.certificate_path = self._set_field(module, 'certificate_path', u'certificate_path', 'AZURE_MANAGEMENT_CERTIFICATE', True)
+        self.cert_thumbprint = self._set_field(module, 'cert_thumbprint', u'cert_thumbprint', 'AZURE_CERT_THUMBPRINT')
+        self.cloud_service_cert = self._set_field(module, 'cloud_service_cert', u'cloud_service_cert', 'AZURE_CLOUD_SERVICE_CERT')
 
-        if not subscription_id:
-            if 'AZURE_SUBSCRIPTION_ID' in os.environ:
-                subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
+    def _set_field(self, module, arg_name, file_prop_name, env_name, required=False):
+        arg_value = module.params[arg_name]
+        if not arg_value:
+            if self.ns and file_prop_name in self.ns:
+                arg_value = str(self.ns[file_prop_name], 'utf-8')
 
-        if not certificate_path:
-            if 'AZURE_MANAGEMENT_CERTIFICATE' in os.environ:
-                certificate_path = os.environ['AZURE_MANAGEMENT_CERTIFICATE']
-
-        if not management_endpoint:
-            if 'AZURE_MANAGEMENT_ENDPOINT' in os.environ:
-                management_endpoint = os.environ['AZURE_MANAGEMENT_ENDPOINT']
+        if not arg_value:
+            if env_name in os.environ:
+                arg_value = os.environ[env_name]
             else:
-                management_endpoint = Azure.AZURE_MANAGEMENT_ENDPOINT
+                arg_value = None
 
-        facts = dict(certificate_path=certificate_path,
-                     subscription_id=subscription_id,
-                     management_endpoint=management_endpoint)
+        if required and not arg_value:
+            module.fail_json(msg=arg_name + " must be provided by config file ("+file_prop_name+"), param ("+arg_name+") or env var ("+env_name+").")
 
-        return facts
+        return arg_value
 
-    @staticmethod
-    def get_creds(module):
-        azure_env = Azure.connection_info(module)
-        return azure_env['subscription_id'], azure_env['certificate_path']
+
+# monkey patch for temporary redirects until https://github.com/Azure/azure-sdk-for-python/issues/129 is fixed
+def perform_request_new(self, request):
+    connection = self.get_connection(request)
+    try:
+        connection.putrequest(request.method, request.path)
+
+        if not self.use_httplib:
+            if self.proxy_host and self.proxy_user:
+                connection.set_proxy_credentials(
+                    self.proxy_user, self.proxy_password)
+
+        self.send_request_headers(connection, request.headers)
+        self.send_request_body(connection, request.body)
+
+        resp = connection.getresponse()
+        self.status = int(resp.status)
+        self.message = resp.reason
+        self.respheader = headers = resp.getheaders()
+
+        # for consistency across platforms, make header names lowercase
+        for i, value in enumerate(headers):
+            headers[i] = (value[0].lower(), value[1])
+
+        respbody = None
+        if resp.length is None:
+            respbody = resp.read()
+        elif resp.length > 0:
+            respbody = resp.read(resp.length)
+        response = HTTPResponse(
+            int(resp.status), resp.reason, headers, respbody)
+        if self.status == 307:
+            print("Temporary redirect detected...")
+            new_url = urlparse(dict(headers)['location'])
+            request.host = new_url.hostname
+            request.path = new_url.path
+            if new_url.query:
+                request.path += '?' + new_url.query
+            request.path, request.query = _update_request_uri_query(request)
+            return self.perform_request(request)
+        if self.status >= 300:
+            raise HTTPError(self.status, self.message,
+                            self.respheader, respbody)
+
+        return response
+    finally:
+        connection.close()
+
+
+azure.http.httpclient._HTTPClient.perform_request = perform_request_new
+
