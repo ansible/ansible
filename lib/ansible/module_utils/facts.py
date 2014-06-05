fd6f0cb4b4a71ef5c142410d351e47c455289bf7
Dag Wieers <dag@wieers.com>
2014-06-05 15:09:43 +0200

    Update facts.py

--- lib/ansible/module_utils/facts.py
+++ lib/ansible/module_utils/facts.py
@@ -140,7 +140,7 @@ class Facts(object):
                 days = int(raw.split()[2])
                 if 'min' in raw:
                     hours = 0
-                    minutes = int(raw[4])
+                    minutes = int(raw.split()[4])
                 else:
                     hours, minutes = map(int, raw.split()[4].split(':'))
                 self.facts['uptime'] = days*24*60*60 + hours*60*60 + minutes*60
