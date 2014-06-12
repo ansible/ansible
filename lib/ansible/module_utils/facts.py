cb3b0bed88ec846036b2a3ded1a318e309cd9c97
Jakub Kramarz <lenwe@lenwe.net>
2014-06-12 12:14:40 +0200

    changed sector size fact source from hw_sector_size to physical_block_size, fixes issue 5761

--- lib/ansible/module_utils/facts.py
+++ lib/ansible/module_utils/facts.py
@@ -739,7 +739,7 @@ class LinuxHardware(Hardware):
 
                     part['start'] = get_file_content(part_sysdir + "/start",0)
                     part['sectors'] = get_file_content(part_sysdir + "/size",0)
-                    part['sectorsize'] = get_file_content(part_sysdir + "/queue/hw_sector_size",512)
+                    part['sectorsize'] = get_file_content(part_sysdir + "/queue/physical_block_size",512)
                     part['size'] = module.pretty_bytes((float(part['sectors']) * float(part['sectorsize'])))
                     d['partitions'][partname] = part
 
@@ -754,7 +754,7 @@ class LinuxHardware(Hardware):
             d['sectors'] = get_file_content(sysdir + "/size")
             if not d['sectors']:
                 d['sectors'] = 0
-            d['sectorsize'] = get_file_content(sysdir + "/queue/hw_sector_size")
+            d['sectorsize'] = get_file_content(sysdir + "/queue/physical_block_size")
             if not d['sectorsize']:
                 d['sectorsize'] = 512
             d['size'] = module.pretty_bytes(float(d['sectors']) * float(d['sectorsize']))
