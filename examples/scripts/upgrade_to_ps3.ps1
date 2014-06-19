bdd0bac306412878fc4a36850b8f7aa38031ef52
Don Schenck <don.schenck@gmail.com>
2014-06-19 13:21:02 -0500

    Start-Process
    Debugging

--- examples/scripts/upgrade_to_ps3.ps1
+++ examples/scripts/upgrade_to_ps3.ps1
@@ -79,4 +79,4 @@ else
 $FileName = $DownLoadUrl.Split('/')[-1]
 download-file $downloadurl "$powershellpath\$filename"
 
-Start-Process -FilePath "$powershellpath\$filename" -ArgumentList /quiet
+Start-Process -FilePath "$powershellpath\$filename /quiet" -WindowStyle Hidden
