2316b7785cbdb7b88cf623bd094a046ee0aee4c6
Matt Martz <matt@sivel.net>
2014-06-19 13:48:45 -0500

    Make sure the doc stubs for windows modules have proper license headers

--- examples/scripts/upgrade_to_ps3.ps1
+++ examples/scripts/upgrade_to_ps3.ps1
@@ -79,4 +79,4 @@ else
 $FileName = $DownLoadUrl.Split('/')[-1]
 download-file $downloadurl "$powershellpath\$filename"
 
-Start-Process -FilePath "$powershell\$filename" -ArgumentList /quiet
+Start-Process -FilePath ".$powershellpath\$filename" -ArgumentList /quiet
