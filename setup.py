03efbfca2992b9e91b607f766dfb3e2f7a89e8fe
James Laska <jlaska@ansible.com>
2014-07-17 08:57:26 -0400

    Correct string concatenation error

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from ansible import __version__, __author__
 try:
     from setuptools import setup
 except ImportError:
-    print "Ansible now needs setuptools in order to build. " + \
+    print "Ansible now needs setuptools in order to build. " \
           "Install it using your package manager (usually python-setuptools) or via pip (pip install setuptools)."
     sys.exit(1)
 
