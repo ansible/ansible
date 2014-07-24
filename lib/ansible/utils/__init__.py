d66b245b962bd2596eca76e988986c8af036ed93
James Cammarata <jimi@sngx.net>
2014-07-24 13:57:35 -0500

    Using custom splitting function for module param counting

--- lib/ansible/utils/__init__.py
+++ lib/ansible/utils/__init__.py
@@ -30,6 +30,7 @@ from ansible.utils import template
 from ansible.utils.display_functions import *
 from ansible.utils.plugins import *
 from ansible.callbacks import display
+from ansible.module_utils.basic import split_args
 import ansible.constants as C
 import ast
 import time
