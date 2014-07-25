1d73e8c0133b707c015633cc87b821b128aa8656
CaptTofu <patg@patg.net>
2014-07-24 20:16:14 -0700

    Work on nova inventory plugin- pep8 plus connects to recent client

--- plugins/inventory/nova.py
+++ plugins/inventory/nova.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # (c) 2012, Marco Vito Moscaritolo <marco@agavee.com>
+# (c) 2014 Patrick "CaptTofu" Galbraith <patg@patg.net> 
 #
 # This file is part of Ansible,
 #
@@ -21,7 +22,8 @@ import sys
 import re
 import os
 import ConfigParser
-from novaclient import client as nova_client
+from novaclient.v1_1 import client as nova_client
+from novaclient import exceptions
 
 try:
     import json
@@ -32,14 +34,29 @@ except:
 # executed with no parameters, return the list of
 # all groups and hosts
 
-NOVA_CONFIG_FILES = [os.getcwd() + "/nova.ini",
-                     os.path.expanduser(os.environ.get('ANSIBLE_CONFIG', "~/nova.ini")),
-                     "/etc/ansible/nova.ini"]
+USERNAME = os.getenv('OS_USERNAME')
+PASSWORD = os.getenv('OS_PASSWORD')
+AUTH_URL = os.getenv('OS_AUTH_URL')
+REGION_NAME = os.getenv('OS_REGION_NAME')
+TENANT_NAME = os.getenv('OS_TENANT_NAME')
+
+NOVA_CONFIG_FILES = [
+    os.getcwd() + "/nova.ini",
+    os.path.expanduser(
+        os.environ.get(
+            'ANSIBLE_CONFIG',
+            "~/nova.ini")
+    ),
+    "/etc/ansible/nova.ini"
+]
 
 NOVA_DEFAULTS = {
-    'auth_system': None,
-    'region_name': None,
+    'auth_system': 'keystone',
+    'region_name': 'region1',
+    'service_type': 'compute'
 }
+SERVICE_TYPE = NOVA_DEFAULTS['service_type']
+AUTH_SYSTEM = NOVA_DEFAULTS['auth_system']
 
 
 def nova_load_config_file():
@@ -52,45 +69,80 @@ def nova_load_config_file():
 
     return None
 
+# use a config file if it exists where expected
 config = nova_load_config_file()
 if not config:
-    sys.exit('Unable to find configfile in %s' % ', '.join(NOVA_CONFIG_FILES))
+    if not (USERNAME and PASSWORD and AUTH_URL
+            and REGION_NAME and TENANT_NAME):
+        sys.exit(
+            'Unable to find configfile in %s' % ','
+            .join(NOVA_CONFIG_FILES))
+else:
+    USERNAME = config.get('openstack', 'username')
+    PASSWORD = config.get('openstack', 'password')
+    TENANT_NAME = config.get('openstack', 'project_id')
+    if not TENANT_NAME:
+        TENANT_NAME = config.get('openstack', 'tenant_name')
+    AUTH_URL = config.get('openstack', 'auth_url')
+    REGION_NAME = config.get('openstack', 'region_name')
+    SERVICE_TYPE = config.get('openstack', 'service_type')
+    AUTH_SYSTEM = config.get('openstack', 'auth_system')
 
+# Make the connection
 client = nova_client.Client(
-    version     = config.get('openstack', 'version'),
-    username    = config.get('openstack', 'username'),
-    api_key     = config.get('openstack', 'api_key'),
-    auth_url    = config.get('openstack', 'auth_url'),
-    region_name = config.get('openstack', 'region_name'),
-    project_id  = config.get('openstack', 'project_id'),
-    auth_system = config.get('openstack', 'auth_system')
+    USERNAME,
+    PASSWORD,
+    TENANT_NAME,
+    AUTH_URL,
+    region_name=REGION_NAME,
+    service_type=SERVICE_TYPE,
+    auth_system=AUTH_SYSTEM
 )
+
+try:
+    client.authenticate()
+except exceptions.Unauthorized, e:
+    print("Invalid OpenStack Nova credentials.: %s" %
+          e.message)
+    sys.exit(1)
+except exceptions.AuthorizationFailure, e:
+    print("Unable to authorize user: %s" % e.message)
+    sys.exit(1)
+
+if client is None:
+    print("Failed to instantiate nova client. This "
+          "could mean that your credentials are wrong.")
+    sys.exit(1)
 
 if len(sys.argv) == 2 and (sys.argv[1] == '--list'):
     groups = {}
 
     # Cycle on servers
     for f in client.servers.list():
-	private = [ x['addr'] for x in getattr(f, 'addresses').itervalues().next() if x['OS-EXT-IPS:type'] == 'fixed']
-	public  = [ x['addr'] for x in getattr(f, 'addresses').itervalues().next() if x['OS-EXT-IPS:type'] == 'floating']
-	    
-	# Define group (or set to empty string)
-        group = f.metadata['group'] if f.metadata.has_key('group') else 'undefined'
+        private = [x['addr'] for x in
+                   getattr(f, 'addresses').itervalues().next()
+                   if x['OS-EXT-IPS:type'] == 'fixed']
+        public = [x['addr'] for x in
+                  getattr(f, 'addresses').itervalues().next()
+                  if x['OS-EXT-IPS:type'] == 'floating']
+
+        # Define group (or set to empty string)
+        group = f.metadata['group'] if 'group' in f.metadata else 'undefined'
 
         # Create group if not exist
         if group not in groups:
             groups[group] = []
 
         # Append group to list
-	if f.accessIPv4:
-        	groups[group].append(f.accessIPv4)
-		continue
-	if public:
-        	groups[group].append(''.join(public))
-		continue
-	if private:
-        	groups[group].append(''.join(private))
-		continue
+        if f.accessIPv4:
+            groups[group].append(f.accessIPv4)
+            continue
+        if public:
+            groups[group].append(''.join(public))
+            continue
+        if private:
+            groups[group].append(''.join(private))
+            continue
 
     # Return server list
     print(json.dumps(groups, sort_keys=True, indent=2))
@@ -104,13 +156,17 @@ elif len(sys.argv) == 3 and (sys.argv[1] == '--host'):
     results = {}
     ips = []
     for instance in client.servers.list():
-	private = [ x['addr'] for x in getattr(instance, 'addresses').itervalues().next() if x['OS-EXT-IPS:type'] == 'fixed']
-	public =  [ x['addr'] for x in getattr(instance, 'addresses').itervalues().next() if x['OS-EXT-IPS:type'] == 'floating']
-        ips.append( instance.accessIPv4)
-	ips.append(''.join(private))
-	ips.append(''.join(public))
-	if sys.argv[2] in ips:
-            for key in vars(instance):
+        private = [x['addr'] for x in
+                   getattr(instance, 'addresses').itervalues().next()
+                   if x['OS-EXT-IPS:type'] == 'fixed']
+        public = [x['addr'] for x in
+                  getattr(instance, 'addresses').itervalues().next()
+                  if x['OS-EXT-IPS:type'] == 'floating']
+        ips.append(instance.accessIPv4)
+    ips.append(''.join(private))
+    ips.append(''.join(public))
+    if sys.argv[2] in ips:
+        for key in vars(instance):
                 # Extract value
                 value = getattr(instance, key)
 
