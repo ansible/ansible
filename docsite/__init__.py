560f35246e09e580cdb86ba60c11a4bc63825189
BadDev <me_buss777@yahoo.fr>
2014-07-31 20:43:46 +0200

    add working dir for stat module

---
+++ docsite/__init__.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+SYNOPSIS
+
+    TODO helloworld [-h,--help] [-v,--verbose] [--version]
+
+DESCRIPTION
+
+    TODO This describes how to use this script. This docstring
+    will be printed by the script if there is an error or
+    if the user requests help (-h or --help).
+
+EXAMPLES
+
+    TODO: Show some examples of how to use this script.
+
+EXIT STATUS
+
+    TODO: List exit codes
+
+AUTHOR
+
+    TODO: BadDev <baddev@la-machine.org>
+    
+COPYRIGHT
+
+    Copyright © 2014 BadDev
+
+LICENSE
+
+    This file is part of Foobar.
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+VERSION
+
+    $
+"""
+
+
+# import defaults
