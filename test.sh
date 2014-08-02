86bf4aebbd1aca659446c881b6951123872511ee
Victor Lin <bornstub@gmail.com>
2014-08-02 00:17:45 -0700

    Try to fix env not setup issue

--- test.sh
+++ test.sh
@@ -9,6 +9,7 @@ if [ "$TEST_SUITE" == "units" ]; then
     flake8 lib/ansible --ignore=E501,E221,W293
     flake8 test/units --ignore=E501,E221,W293
 else
+	./hacking/env-setup
     cd test/integration
     make non_destructive
     # TODO: run as many tests as possible
