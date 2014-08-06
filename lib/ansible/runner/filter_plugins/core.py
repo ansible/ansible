0bb1c8522b6a4985c1f09b339d5baf9c9465f46d
Sébastien Gross <sebchezwamorg>
2014-08-06 10:56:16 +0200

    Add regular expression escaping filter.

--- lib/ansible/runner/filter_plugins/core.py
+++ lib/ansible/runner/filter_plugins/core.py
@@ -210,6 +210,10 @@ def version_compare(value, version, operator='eq', strict=False):
     except Exception, e:
         raise errors.AnsibleFilterError('Version comparison: %s' % e)
 
+def re_escape(string):
+    '''Escape all regular expressions special characters from STRING.'''
+    return re.escape(string)
+
 @environmentfilter
 def rand(environment, end, start=None, step=None):
     r = SystemRandom()
@@ -282,6 +286,7 @@ class FilterModule(object):
             'search': search,
             'regex': regex,
             'regex_replace': regex_replace,
+            're_escape': re_escape,
 
             # list
             'unique' : unique,
