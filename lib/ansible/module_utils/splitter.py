bc85f6e54c245bd7b988f2d8db65e8237c7034d7
James Cammarata <jimi@sngx.net>
2014-08-11 09:34:14 -0500

    Correct parser to ignore escaped quotes when not in quotes already
    Related to #8481

--- lib/ansible/module_utils/splitter.py
+++ lib/ansible/module_utils/splitter.py
@@ -26,9 +26,9 @@ def _get_quote_state(token, quote_char):
     for idx, cur_char in enumerate(token):
         if idx > 0:
             prev_char = token[idx-1]
-        if cur_char in '"\'':
+        if cur_char in '"\'' and prev_char != '\\':
             if quote_char:
-                if cur_char == quote_char and prev_char != '\\':
+                if cur_char == quote_char:
                     quote_char = None
             else:
                 quote_char = cur_char
