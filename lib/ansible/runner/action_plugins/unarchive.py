cdf938f2c2fb25032d09f135ecaebfc4c1bb5c5b
James Cammarata <jimi@sngx.net>
2014-09-15 13:15:46 -0500

    Ensure remote files are chmod'd properly for su/su_user too
    Fixes #8594

--- lib/ansible/runner/action_plugins/unarchive.py
+++ lib/ansible/runner/action_plugins/unarchive.py
@@ -76,7 +76,7 @@ class ActionModule(object):
         # handle check mode client side
         # fix file permissions when the copy is done as a different user
         if copy:
-            if self.runner.sudo and self.runner.sudo_user != 'root':
+            if self.runner.sudo and self.runner.sudo_user != 'root' or self.runner.su and self.runner.su_user != 'root':
                 self.runner._remote_chmod(conn, 'a+r', tmp_src, tmp)
             # Build temporary module_args.
             new_module_args = dict(
