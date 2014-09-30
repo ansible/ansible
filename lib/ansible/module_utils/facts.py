4614a574eadb0a245edae3bcf7acee1f84cd37b3
Jonathan Süssemilch Poulain <jonathan@sofiero.net>
2014-09-30 20:55:50 +0200

    Allows network network interface facts collection as an unprivileged user and adds more facts

--- lib/ansible/module_utils/facts.py
+++ lib/ansible/module_utils/facts.py
@@ -2278,6 +2278,26 @@ class AIXNetwork(GenericBsdIfconfigNetwork, Network):
     """
     platform = 'AIX'
 
+    def get_default_interfaces(self, route_path):
+        netstat_path = module.get_bin_path('netstat')
+
+        rc, out, err = module.run_command([netstat_path, '-nr'])
+
+        interface = dict(v4 = {}, v6 = {})
+
+        lines = out.split('\n')
+        for line in lines:
+            words = line.split()
+            if len(words) > 1 and words[0] == 'default':
+                if '.' in words[1]:
+                    interface['v4']['gateway'] = words[1]
+                    interface['v4']['interface'] = words[5]
+                elif ':' in words[1]:
+                    interface['v6']['gateway'] = words[1]
+                    interface['v6']['interface'] = words[5]
+
+        return interface['v4'], interface['v6']
+
     # AIX 'ifconfig -a' does not have three words in the interface line
     def get_interfaces_info(self, ifconfig_path, ifconfig_options):
         interfaces = {}
