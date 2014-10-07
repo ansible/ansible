ad9ab8e033ca327cadea3d046a196a2759bc4300
Michael DeHaan <michael@ansible.com>
2014-10-07 14:59:55 -0400

    Stub class for mod arg parsing tests.

---
+++ test/v2/parsing/test_mod_args.py
@@ -0,0 +1,16 @@
+# TODO: header
+
+#from ansible.playbook.task import Task
+import unittest
+
+class TestModArgs(unittest.TestCase):
+
+    def setUp(self):
+        pass
+   
+    def tearDown(self):
+        pass
+
+    def test_sample(self):
+        pass
+   
