d8c615148e927e85dd6097e7081817fcbca27120
Boris Manojlovic <boris@steki.net>
2014-10-10 19:51:44 +0200

    fixed zypper.py and zypper_repository.py for SLE10 platform

--- lib/ansible/modules/extras/packaging/zypper_repository.py
+++ lib/ansible/modules/extras/packaging/zypper_repository.py
@@ -72,16 +72,24 @@ EXAMPLES = '''
 # Add python development repository
 - zypper_repository: repo=http://download.opensuse.org/repositories/devel:/languages:/python/SLE_11_SP3/devel:languages:python.repo
 '''
-from xml.dom.minidom import parseString as parseXML
 
 REPO_OPTS = ['alias', 'name', 'priority', 'enabled', 'autorefresh', 'gpgcheck']
 
+def zypper_version(module):
+    """Return (rc, message) tuple"""
+    cmd = ['/usr/bin/zypper', '-V']
+    rc, stdout, stderr = module.run_command(cmd, check_rc=False)
+    if rc == 0:
+        return rc, stdout
+    else:
+        return rc, stderr
 
 def _parse_repos(module):
     """parses the output of zypper -x lr and returns a parse repo dictionary"""
     cmd = ['/usr/bin/zypper', '-x', 'lr']
     repos = []
 
+    from xml.dom.minidom import parseString as parseXML
     rc, stdout, stderr = module.run_command(cmd, check_rc=True)
     dom = parseXML(stdout)
     repo_list = dom.getElementsByTagName('repo')
@@ -95,8 +103,25 @@ def _parse_repos(module):
 
     return repos
 
+def _parse_repos_old(module):
+    """parses the output of zypper sl and returns a parse repo dictionary"""
+    cmd = ['/usr/bin/zypper', 'sl']
+    repos = []
+    rc, stdout, stderr = module.run_command(cmd, check_rc=True)
+    for line in stdout.split('\n'):
+        matched = re.search(r'\d+\s+\|\s+(?P<enabled>\w+)\s+\|\s+(?P<autorefresh>\w+)\s+\|\s+(?P<type>\w+)\s+\|\s+(?P<name>\w+)\s+\|\s+(?P<url>.*)', line)
+        if matched == None:
+            continue
 
-def repo_exists(module, **kwargs):
+        m = matched.groupdict()
+        m['alias']= m['name']
+        m['priority'] = 100
+        m['gpgcheck'] = 1
+        repos.append(m)
+
+    return repos
+
+def repo_exists(module, old_zypper, **kwargs):
 
     def repo_subset(realrepo, repocmp):
         for k in repocmp:
@@ -109,7 +134,10 @@ def repo_exists(module, **kwargs):
                     return False
         return True
 
-    repos = _parse_repos(module)
+    if old_zypper:
+        repos = _parse_repos_old(module)
+    else:
+        repos = _parse_repos(module)
 
     for repo in repos:
         if repo_subset(repo, kwargs):
@@ -117,13 +145,21 @@ def repo_exists(module, **kwargs):
     return False
 
 
-def add_repo(module, repo, alias, description, disable_gpg_check):
-    cmd = ['/usr/bin/zypper', 'ar', '--check', '--refresh']
+def add_repo(module, repo, alias, description, disable_gpg_check, old_zypper):
+    if old_zypper:
+        cmd = ['/usr/bin/zypper', 'sa']
+    else:
+        cmd = ['/usr/bin/zypper', 'ar', '--check', '--refresh']
+
+    if repo.startswith("file:/") and old_zypper:
+        cmd.extend(['-t', 'Plaindir'])
+    else:
+        cmd.extend(['-t', 'plaindir'])
 
     if description:
         cmd.extend(['--name', description])
 
-    if disable_gpg_check:
+    if disable_gpg_check and not old_zypper:
         cmd.append('--no-gpgcheck')
 
     cmd.append(repo)
@@ -138,14 +174,21 @@ def add_repo(module, repo, alias, description, disable_gpg_check):
     elif 'already exists. Please use another alias' in stderr:
         changed = False
     else:
-        module.fail_json(msg=stderr if stderr else stdout)
+        #module.fail_json(msg=stderr if stderr else stdout)
+        if stderr:
+            module.fail_json(msg=stderr)
+        else:
+            module.fail_json(msg=stdout)
 
     return changed
 
 
-def remove_repo(module, repo, alias):
+def remove_repo(module, repo, alias, old_zypper):
 
-    cmd = ['/usr/bin/zypper', 'rr']
+    if old_zypper:
+        cmd = ['/usr/bin/zypper', 'sd']
+    else:
+        cmd = ['/usr/bin/zypper', 'rr']
     if alias:
         cmd.append(alias)
     else:
@@ -158,7 +201,11 @@ def remove_repo(module, repo, alias):
 
 def fail_if_rc_is_null(module, rc, stdout, stderr):
     if rc != 0:
-        module.fail_json(msg=stderr if stderr else stdout)
+        #module.fail_json(msg=stderr if stderr else stdout)
+        if stderr:
+            module.fail_json(msg=stderr)
+        else:
+            module.fail_json(msg=stdout)
 
 
 def main():
@@ -182,6 +229,13 @@ def main():
     def exit_unchanged():
         module.exit_json(changed=False, repo=repo, state=state, name=name)
 
+    rc, out = zypper_version(module)
+    match = re.match(r'zypper\s+(\d+)\.(\d+)\.(\d+)', out)
+    if not match or  int(match.group(1)) > 0:
+        old_zypper = False
+    else:
+        old_zypper = True
+
     # Check run-time module parameters
     if state == 'present' and not repo:
         module.fail_json(msg='Module option state=present requires repo')
@@ -196,22 +250,22 @@ def main():
             module.fail_json(msg='Name required when adding non-repo files:')
 
     if repo and repo.endswith('.repo'):
-        exists = repo_exists(module, url=repo, alias=name)
+        exists = repo_exists(module, old_zypper, url=repo, alias=name)
     elif repo:
-        exists = repo_exists(module, url=repo)
+        exists = repo_exists(module, old_zypper, url=repo)
     else:
-        exists = repo_exists(module, alias=name)
+        exists = repo_exists(module, old_zypper, alias=name)
 
     if state == 'present':
         if exists:
             exit_unchanged()
 
-        changed = add_repo(module, repo, name, description, disable_gpg_check)
+        changed = add_repo(module, repo, name, description, disable_gpg_check, old_zypper)
     elif state == 'absent':
         if not exists:
             exit_unchanged()
 
-        changed = remove_repo(module, repo, name)
+        changed = remove_repo(module, repo, name, old_zypper)
 
     module.exit_json(changed=changed, repo=repo, state=state)
 
