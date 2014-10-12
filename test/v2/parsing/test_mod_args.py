a1cf7f201eb27d75596de92461e16768ce1db8ba
Toshio Kuratomi <a.badger@gmail.com>
2014-10-12 03:09:37 -0400

    Update other asserts that have a py2.6 unittest method

--- test/v2/parsing/test_mod_args.py
+++ test/v2/parsing/test_mod_args.py
@@ -39,65 +39,65 @@ class TestModArgsDwim(unittest.TestCase):
     def test_basic_shell(self):
         mod, args, to = self.m.parse(dict(shell='echo hi'))
         self._debug(mod, args, to)
-        assert mod == 'command'
-        assert args == dict(
-           _raw_params = 'echo hi',
-           _uses_shell = True,
-        )
+        self.assertEqual(mod, 'command')
+        self.assertEqual(args, dict(
+                                _raw_params = 'echo hi',
+                                _uses_shell = True,
+                        ))
         assert to is None
 
     def test_basic_command(self):
         mod, args, to = self.m.parse(dict(command='echo hi'))
         self._debug(mod, args, to)
-        assert mod == 'command'
-        assert args == dict(
-           _raw_params = 'echo hi',
-        )
+        self.assertEqual(mod, 'command')
+        self.assertEqual(args, dict(
+                                _raw_params = 'echo hi',
+                        ))
         assert to is None
 
     def test_shell_with_modifiers(self):
         mod, args, to = self.m.parse(dict(shell='/bin/foo creates=/tmp/baz removes=/tmp/bleep'))
         self._debug(mod, args, to)
-        assert mod == 'command'
-        assert args == dict(
-           creates     = '/tmp/baz',
-           removes     = '/tmp/bleep',
-           _raw_params = '/bin/foo',
-           _uses_shell = True,
-        )
+        self.assertEqual(mod, 'command')
+        self.assertEqual(args, dict(
+                                creates     = '/tmp/baz',
+                                removes     = '/tmp/bleep',
+                                _raw_params = '/bin/foo',
+                                _uses_shell = True,
+                        ))
         assert to is None
 
     def test_normal_usage(self):
         mod, args, to = self.m.parse(dict(copy='src=a dest=b'))
         self._debug(mod, args, to)
-        assert mod == 'copy'
-        assert args == dict(src='a', dest='b')
+        self.assertEqual(mod, 'copy')
+        self.assertEqual(args, dict(src='a', dest='b'))
         assert to is None
 
     def test_complex_args(self):
         mod, args, to = self.m.parse(dict(copy=dict(src='a', dest='b')))
         self._debug(mod, args, to)
-        assert mod == 'copy'
-        assert args == dict(src='a', dest='b')
+        self.assertEqual(mod, 'copy')
+        self.assertEqual(args, dict(src='a', dest='b'))
         assert to is None
 
     def test_action_with_complex(self):
         mod, args, to = self.m.parse(dict(action=dict(module='copy', src='a', dest='b')))
         self._debug(mod, args, to)
-        assert mod == 'copy'
-        assert args == dict(src='a', dest='b')
+        self.assertEqual(mod, 'copy')
+        self.assertEqual(args, dict(src='a', dest='b'))
         assert to is None
 
     def test_action_with_complex_and_complex_args(self):
         mod, args, to = self.m.parse(dict(action=dict(module='copy', args=dict(src='a', dest='b'))))
         self._debug(mod, args, to)
-        assert mod == 'copy'
-        assert args == dict(src='a', dest='b')
+        self.assertEqual(mod, 'copy')
+        self.assertEqual(args, dict(src='a', dest='b'))
         assert to is None
 
     def test_local_action_string(self):
         mod, args, to = self.m.parse(dict(local_action='copy src=a dest=b'))
         self._debug(mod, args, to)
-        assert mod == 'copy'
-        assert args == dict(src='a', dest='b')
+        self.assertEqual(mod, 'copy')
+        self.assertEqual(args, dict(src='a', dest='b'))
         assert to is 'localhost'
