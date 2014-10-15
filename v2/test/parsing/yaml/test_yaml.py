c451a8051ce7aacf8811582b7d7174d2f1a4b0c5
Toshio Kuratomi <a.badger@gmail.com>
2014-10-15 18:40:03 -0400

    Fix relative imports

--- v2/test/parsing/yaml/test_yaml.py
+++ v2/test/parsing/yaml/test_yaml.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
-from ..compat import unittest
+from ... compat import unittest
 
 from yaml.scanner import ScannerError
 
