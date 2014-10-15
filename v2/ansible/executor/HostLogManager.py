2ce9c7a576027458a80257704be6d35dacd01fbf
Michael DeHaan <michael.dehaan@gmail.com>
2014-10-15 15:53:43 -0700

    Starting to stub out some classes.

--- v2/ansible/executor/HostLogManager.py
+++ v2/ansible/executor/HostLogManager.py
@@ -14,3 +14,12 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+class HostLogManager(object):
+
+   def __init__(self):
+       pass
+
+   def get_log_for_host(self, host):
+       assert False
+
