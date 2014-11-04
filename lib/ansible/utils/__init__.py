a0d09871149d3073890ca5835dbbc64cadd6f1e2
Ilja Bauer <iljabauer@gmail.com>
2014-11-04 22:36:16 +0100

    Improved solution to first search the normal way and then search in symlinked path. This allow simple template inheritance

--- lib/ansible/utils/__init__.py
+++ lib/ansible/utils/__init__.py
@@ -347,15 +347,21 @@ def path_dwim_relative(original, dirname, source, playbook_base, check=True):
     if os.path.islink(basedir):
         basedir = unfrackpath(basedir)
         template2 = os.path.join(basedir, dirname, source)
-    elif os.path.islink(original):
-        linked_file = unfrackpath(original)
-        basedir = os.path.dirname(linked_file)
-        template2 = os.path.join(basedir, "..", dirname, source)
     else:
         template2 = os.path.join(basedir, '..', dirname, source)
     source2 = path_dwim(basedir, template2)
     if os.path.exists(source2):
         return source2
+
+    #if the original file is a symlink try to search in real path
+    if os.path.islink(original):
+        linked_file = unfrackpath(original)
+        basedir = os.path.dirname(linked_file)
+        template2 = os.path.join(basedir, "..", dirname, source)
+        source2 = path_dwim(basedir, template2)
+        if os.path.exists(source2):
+            return source2
+
     obvious_local_path = path_dwim(playbook_base, source)
     if os.path.exists(obvious_local_path):
         return obvious_local_path
