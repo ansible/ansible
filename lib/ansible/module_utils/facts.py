8822bea5d761ed144cb3780542cd24a060c8228d
John Stanton <john@stantonnet.org>
2014-11-04 16:52:03 -0500

    Update facts.py

--- lib/ansible/module_utils/facts.py
+++ lib/ansible/module_utils/facts.py
@@ -767,15 +767,14 @@ class LinuxHardware(Hardware):
             diskname = os.path.basename(sysdir)
 
             sg_inq = module.get_bin_path('sg_inq') 
-            for key in ['serial']:
-                device = "/dev/%s" % (block)
-                try:
-                    rc, drivedata, err = module.run_command([sg_inq, device])
-                except:
-                    return
-                serial = re.search("Unit serial number:\s+(\w+)", drivedata)
-                if serial:
-                     d[key] = serial.group(1)
+            device = "/dev/%s" % (block)
+            try:
+                rc, drivedata, err = module.run_command([sg_inq, device])
+            except:
+                return
+            serial = re.search("Unit serial number:\s+(\w+)", drivedata)
+            if serial:
+                d[key] = serial.group(1)
 
             for key in ['vendor', 'model']:
                 d[key] = get_file_content(sysdir + "/device/" + key)
