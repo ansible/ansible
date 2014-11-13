726df5cc9587d0a6566a56053a3acd53619f20cf
John Batty <john.batty@metaswitch.com>
2014-11-13 13:26:20 +0000

    Fix get_flavor_id() when flavor_ram is specified
    Without this fix, _get_flavor_id() fails to find a matching flavor if
    both:
    * the flavor_ram parameter is specified
    * the first flavor in the list does not match.
    
    The bug is simply that the module.fail_json() call lies within the loop
    iterating through the flavors.  This call should only be made if the
    loop completes and no matching flavors have been found.

--- lib/ansible/modules/cloud/openstack/nova_compute.py
+++ lib/ansible/modules/cloud/openstack/nova_compute.py
@@ -405,7 +405,7 @@ def _get_flavor_id(module, nova):
             if (flavor.ram >= module.params['flavor_ram'] and
                     (not module.params['flavor_include'] or module.params['flavor_include'] in flavor.name)):
                 return flavor.id
-            module.fail_json(msg = "Error finding flavor with %sMB of RAM" % module.params['flavor_ram'])
+        module.fail_json(msg = "Error finding flavor with %sMB of RAM" % module.params['flavor_ram'])
     return module.params['flavor_id']
 
 
