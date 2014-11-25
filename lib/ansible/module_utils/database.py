9a77aefc338d15c5fe5c1407200cff7eeb8dfd16
Toshio Kuratomi <toshio@fedoraproject.org>
2014-11-25 08:06:15 -0800

    Special case the lone asterisk fragment in mysql

--- lib/ansible/module_utils/database.py
+++ lib/ansible/module_utils/database.py
@@ -117,4 +117,12 @@ def mysql_quote_identifier(identifier, id_type):
     identifier_fragments = _identifier_parse(identifier, quote_char='`')
     if len(identifier_fragments) > _MYSQL_IDENTIFIER_TO_DOT_LEVEL[id_type]:
         raise SQLParseError('MySQL does not support %s with more than %i dots' % (id_type, _MYSQL_IDENTIFIER_TO_DOT_LEVEL[id_type]))
-    return '.'.join(identifier_fragments)
+
+    special_cased_fragments = []
+    for fragment in identifier_fragments:
+        if fragment == '`*`':
+            special_cased_fragments.append('*')
+        else:
+            special_cased_fragments.append(fragment)
+
+    return '.'.join(special_cased_fragments)
