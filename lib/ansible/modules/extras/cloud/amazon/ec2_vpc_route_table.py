9812e196b80b3b01022281353d554fb19d22275b
Robert Estelle <robert@getpreo.com>
2014-12-04 22:10:02 -0500

    ec2_vpc_route_tables - Remove more dict comprehensions.

--- lib/ansible/modules/extras/cloud/amazon/ec2_vpc_route_table.py
+++ lib/ansible/modules/extras/cloud/amazon/ec2_vpc_route_table.py
@@ -172,8 +172,8 @@ class AnsibleTagCreationException(AnsibleRouteTableException):
 
 
 def get_resource_tags(vpc_conn, resource_id):
-    return {t.name: t.value for t in
-            vpc_conn.get_all_tags(filters={'resource-id': resource_id})}
+    return dict((t.name, t.value) for t in
+                vpc_conn.get_all_tags(filters={'resource-id': resource_id}))
 
 
 def tags_match(match_tags, candidate_tags):
@@ -187,11 +187,11 @@ def ensure_tags(vpc_conn, resource_id, tags, add_only, check_mode):
         if tags == cur_tags:
             return {'changed': False, 'tags': cur_tags}
 
-        to_delete = {k: cur_tags[k] for k in cur_tags if k not in tags}
+        to_delete = dict((k, cur_tags[k]) for k in cur_tags if k not in tags)
         if to_delete and not add_only:
             vpc_conn.delete_tags(resource_id, to_delete, dry_run=check_mode)
 
-        to_add = {k: tags[k] for k in tags if k not in cur_tags}
+        to_add = dict((k, tags[k]) for k in tags if k not in cur_tags)
         if to_add:
             vpc_conn.create_tags(resource_id, to_add, dry_run=check_mode)
 
