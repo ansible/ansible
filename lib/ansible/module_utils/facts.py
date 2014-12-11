36d6b8cd8b9174d7d20147723195387d7f744894
Jonathan Davila <jdavila@ansible.com>
2014-12-11 09:02:18 -0500

    patch

--- lib/ansible/module_utils/facts.py
+++ lib/ansible/module_utils/facts.py
@@ -582,7 +582,6 @@ class LinuxHardware(Hardware):
         self.get_memory_facts()
         self.get_dmi_facts()
         self.get_device_facts()
-        self.get_memory_facts()
         try:
             self.get_mount_facts()
         except TimeoutError:
