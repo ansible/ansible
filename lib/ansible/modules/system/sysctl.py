d335ab3685234dca4b4317c1ff5c815243cb6467
Brian Coca <bcoca@ansible.com>
2015-01-19 19:37:57 -0500

    now handles non string values for sysctl

--- lib/ansible/modules/system/sysctl.py
+++ lib/ansible/modules/system/sysctl.py
@@ -185,12 +185,20 @@ class SysctlModule(object):
     def _parse_value(self, value):
         if value is None:
             return ''
-        elif value.lower() in BOOLEANS_TRUE:
-            return '1'
-        elif value.lower() in BOOLEANS_FALSE:
-            return '0'
+        elif isinstance(value, bool):
+            if value:
+                return '1'
+            else:
+                return '0'
+        elif isinstance(value, basestring):
+            if value.lower() in BOOLEANS_TRUE:
+                return '1'
+            elif value.lower() in BOOLEANS_FALSE:
+                return '0'
+            else:
+                return value.strip()
         else:
-            return value.strip()
+            return value
 
     # ==============================================================
     #   SYSCTL COMMAND MANAGEMENT
