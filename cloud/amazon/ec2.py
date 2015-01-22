9e1847ed033a8b283940fbb16660ffe06b14316d
Brian Coca <brian.coca+git@gmail.com>
2015-01-21 20:09:46 -0500

    ec2 fix docs format

--- cloud/amazon/ec2.py
+++ cloud/amazon/ec2.py
@@ -64,7 +64,7 @@ options:
   tenancy:
     version_added: "1.9"
     description:
-      - An instance with a tenancy of "dedicated" runs on single-tenant hardware and can only be launched into a VPC. Valid values are:"default" or "dedicated". NOTE: To use dedicated tenancy you MUST specify a vpc_subnet_id as well. Dedicated tenancy is not available for EC2 "micro" instances. 
+      - An instance with a tenancy of "dedicated" runs on single-tenant hardware and can only be launched into a VPC. Valid values are "default" or "dedicated". Note that to use dedicated tenancy you MUST specify a vpc_subnet_id as well. Dedicated tenancy is not available for EC2 "micro" instances.
     required: false
     default: default
     aliases: []
