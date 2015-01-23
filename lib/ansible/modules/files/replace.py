59c7d68cf98c24d59a118d1ff2dd7cc75f9a1304
sysadmin75 <krisnewsome@gmail.com>
2015-01-23 18:10:00 -0500

    Fix #10059 - replace module does not obey follow=yes

--- lib/ansible/modules/files/replace.py
+++ lib/ansible/modules/files/replace.py
@@ -152,6 +152,8 @@ def main():
     if changed and not module.check_mode:
         if params['backup'] and os.path.exists(dest):
             module.backup_local(dest)
+        if params['follow'] and os.path.islink(dest):
+            dest = os.path.realpath(dest)
         write_changes(module, result[0], dest)
 
     msg, changed = check_file_attrs(module, changed, msg)
