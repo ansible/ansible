a14248ffe1ec7a7cf8623732ebc24e680883fa20
Alvaro Lopez Ortega <alvaro@gnu.org>
2015-02-21 16:40:50 +0100

    Fixes bug #10281 - Trailing zeros were truncated from strings

--- lib/ansible/inventory/ini.py
+++ lib/ansible/inventory/ini.py
@@ -54,7 +54,11 @@ class InventoryParser(object):
     def _parse_value(v):
         if "#" not in v:
             try:
-                return ast.literal_eval(v)
+                re = ast.literal_eval(v)
+                if type(re) == float:
+                    # Do not trim floats. Eg: "1.20" to 1.2
+                    return v
+                return re
             # Using explicit exceptions.
             # Likely a string that literal_eval does not like. We wil then just set it.
             except ValueError:
