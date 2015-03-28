4c35f75597196cc30127e1812cbd9eb012f3447a
Rene Moser <mail@renemoser.net>
2015-03-28 22:09:21 +0100

    cloudstack_ssh: update description

--- lib/ansible/modules/extras/cloud/cloudstack/cloudstack_sshkey.py
+++ lib/ansible/modules/extras/cloud/cloudstack/cloudstack_sshkey.py
@@ -23,8 +23,8 @@ DOCUMENTATION = '''
 module: cloudstack_sshkey
 short_description: Manages SSH keys on Apache CloudStack based clouds.
 description:
-  - If no public key is provided, a new ssh private/public key pair will be
-    created and the private key will be returned.
+  - If no key was found and no public key was provided and a new SSH
+    private/public key pair will be created and the private key will be returned.
 version_added: '2.0'
 author: René Moser
 options:
