164cfdfda4780973272b9cc63dcf376a36317b0e
Amandine Lee <amandine.m.lee@gmail.com>
2015-04-13 14:28:00 -0700

    Fix typo

--- v2/ansible/plugins/strategies/__init__.py
+++ v2/ansible/plugins/strategies/__init__.py
@@ -19,7 +19,7 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import six.moves import queue as Queue
+from six.moves import queue as Queue
 import time
 
 from ansible.errors import *
