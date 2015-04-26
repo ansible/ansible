4bcbcc886936ac7c672a230dec80ac14b6e12211
Martin Froehlich <martin.j.froehlich@googlemail.com>
2015-04-26 14:50:07 +0100

    Use get_aws_connection_info to get AWS creds.
    Previously used get_ec2_creds did not consider security_token,
    making MFA requests fail.

--- cloud/amazon/ec2.py
+++ cloud/amazon/ec2.py
@@ -1207,15 +1207,11 @@ def main():
 
     ec2 = ec2_connect(module)
 
-    ec2_url, aws_access_key, aws_secret_key, region = get_ec2_creds(module)
+    region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module)
 
     if region:
         try:
-            vpc = boto.vpc.connect_to_region(
-                region,
-                aws_access_key_id=aws_access_key,
-                aws_secret_access_key=aws_secret_key
-            )
+            vpc = boto.vpc.connect_to_region(region, **aws_connect_kwargs)
         except boto.exception.NoAuthHandlerFound, e:
             module.fail_json(msg = str(e))
     else:
