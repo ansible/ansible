d5e09b75c124c048b7e83eaa5089567bbaed7355
Brian Coca <bcoca@ansible.com>
2015-05-01 09:57:48 -0400

    minor doc fixes, set version added for new tags: parameter should Fix #10894

--- lib/ansible/modules/cloud/amazon/ec2_ami.py
+++ lib/ansible/modules/cloud/amazon/ec2_ami.py
@@ -20,38 +20,33 @@ module: ec2_ami
 version_added: "1.3"
 short_description: create or destroy an image in ec2
 description:
-     - Creates or deletes ec2 images. 
+     - Creates or deletes ec2 images.
 options:
   instance_id:
     description:
       - instance id of the image to create
     required: false
     default: null
-    aliases: []
   name:
     description:
       - The name of the new image to create
     required: false
     default: null
-    aliases: []
   wait:
     description:
       - wait for the AMI to be in state 'available' before returning.
     required: false
     default: "no"
     choices: [ "yes", "no" ]
-    aliases: []
   wait_timeout:
     description:
       - how long before wait gives up, in seconds
     default: 300
-    aliases: []
   state:
     description:
       - create or deregister/delete image
     required: false
     default: 'present'
-    aliases: []
   region:
     description:
       - The AWS region to use.  Must be specified if ec2_url is not used. If not specified then the value of the EC2_REGION environment variable, if any, is used.
@@ -63,32 +58,28 @@ options:
       - An optional human-readable string describing the contents and purpose of the AMI.
     required: false
     default: null
-    aliases: []
   no_reboot:
     description:
       - An optional flag indicating that the bundling process should not attempt to shutdown the instance before bundling. If this flag is True, the responsibility of maintaining file system integrity is left to the owner of the instance. The default choice is "no".
     required: false
     default: no
     choices: [ "yes", "no" ]
-    aliases: []
   image_id:
     description:
       - Image ID to be deregistered.
     required: false
     default: null
-    aliases: []
   delete_snapshot:
     description:
       - Whether or not to delete an AMI while deregistering it.
     required: false
     default: null
-    aliases: []
   tags:
     description:
       - a hash/dictionary of tags to add to the new image; '{"key":"value"}' and '{"key":"value","key":"value"}'
     required: false
     default: null
-    aliases: []
+    version_added: "2.0"
 
 author: Evan Duffield <eduffield@iacquire.com>
 extends_documentation_fragment: aws
