00e146649f6b628eb2195070a390b51c128f1180
Quentin Stafford-Fraser <quentin@pobox.com>
2015-05-03 20:58:21 +0100

    Add webfaction modules

---
+++ lib/ansible/modules/extras/cloud/webfaction/webfaction_db.py
@@ -0,0 +1,147 @@
+#! /usr/bin/python
+# Create webfaction database using Ansible and the Webfaction API
+#
+# Quentin Stafford-Fraser 2015
+
+DOCUMENTATION = '''
+---
+module: webfaction_db
+short_description: Add or remove a database on Webfaction
+description:
+    - Add or remove a database on a Webfaction host. Further documentation at http://github.com/quentinsf/ansible-webfaction.
+author: Quentin Stafford-Fraser
+version_added: 1.99
+notes:
+    - "You can run playbooks that use this on a local machine, or on a Webfaction host, or elsewhere, since the scripts use the remote webfaction API - the location is not important. However, running them on multiple hosts I(simultaneously) is best avoided. If you don't specify I(localhost) as your host, you may want to add C(serial: 1) to the plays."
+    - See `the webfaction API <http://docs.webfaction.com/xmlrpc-api/>`_ for more info.
+options:
+
+    name:
+        description:
+            - The name of the database
+        required: true
+        default: null
+
+    state:
+        description:
+            - Whether the database should exist
+        required: false
+        choices: ['present', 'absent']
+        default: "present"
+
+    type:
+        description:
+            - The type of database to create.
+        required: true
+        choices: ['mysql', 'postgresql']
+
+    login_name:
+        description:
+            - The webfaction account to use
+        required: true
+
+    login_password:
+        description:
+            - The webfaction password to use
+        required: true
+'''
+
+EXAMPLES = '''
+  # This will also create a default DB user with the same
+  # name as the database, and the specified password.
+  
+  - name: Create a database
+    webfaction_db:
+      name: "{{webfaction_user}}_db1"
+      password: mytestsql
+      type: mysql
+      login_name: "{{webfaction_user}}"
+      login_password: "{{webfaction_passwd}}"
+'''
+
+import socket
+import xmlrpclib
+from ansible.module_utils.basic import *
+
+webfaction = xmlrpclib.ServerProxy('https://api.webfaction.com/')
+
+def main():
+
+    module = AnsibleModule(
+        argument_spec = dict(
+            name = dict(required=True, default=None),
+            state = dict(required=False, default='present'),
+            # You can specify an IP address or hostname.
+            type = dict(required=True, default=None),
+            password = dict(required=False, default=None),
+            login_name = dict(required=True),
+            login_password = dict(required=True),
+        ),
+        supports_check_mode=True
+    )
+    db_name  = module.params['name']
+    db_state = module.params['state']
+    db_type  = module.params['type']
+    db_passwd = module.params['password']
+
+    session_id, account = webfaction.login(
+        module.params['login_name'],
+        module.params['login_password']
+    )
+
+    db_list = webfaction.list_dbs(session_id)
+    db_map = dict([(i['name'], i) for i in db_list])
+    existing_db = db_map.get(db_name)
+
+    result = {}
+    
+    # Here's where the real stuff happens
+
+    if db_state == 'present':
+
+        # Does an app with this name already exist?
+        if existing_db:
+            # Yes, but of a different type - fail
+            if existing_db['db_type'] != db_type:
+                module.fail_json(msg="Database already exists but is a different type. Please fix by hand.")
+
+            # If it exists with the right type, we don't change anything.
+            module.exit_json(
+                changed = False,
+            )
+
+
+        if not module.check_mode:
+            # If this isn't a dry run, create the app
+            # print positional_args
+            result.update(
+                webfaction.create_db(
+                    session_id, db_name, db_type, db_passwd
+                )
+            )
+
+    elif db_state == 'absent':
+
+        # If the app's already not there, nothing changed.
+        if not existing_db:
+            module.exit_json(
+                changed = False,
+            )
+
+        if not module.check_mode:
+            # If this isn't a dry run, delete the app
+            result.update(
+                webfaction.delete_db(session_id, db_name, db_type)
+            )
+
+    else:
+        module.fail_json(msg="Unknown state specified: {}".format(db_state))
+
+    module.exit_json(
+        changed = True,
+        result = result
+    )
+
+# The conventional ending
+main()
+
---
+++ lib/ansible/modules/extras/cloud/webfaction/webfaction_mailbox.py
@@ -0,0 +1,112 @@
+#! /usr/bin/python
+# Create webfaction mailbox using Ansible and the Webfaction API
+#
+# Quentin Stafford-Fraser and Andy Baker 2015
+
+DOCUMENTATION = '''
+---
+module: webfaction_mailbox
+short_description: Add or remove mailboxes on Webfaction
+description:
+    - Add or remove mailboxes on a Webfaction account. Further documentation at http://github.com/quentinsf/ansible-webfaction.
+author: Quentin Stafford-Fraser
+version_added: 1.99
+notes:
+    - "You can run playbooks that use this on a local machine, or on a Webfaction host, or elsewhere, since the scripts use the remote webfaction API - the location is not important. However, running them on multiple hosts I(simultaneously) is best avoided. If you don't specify I(localhost) as your host, you may want to add C(serial: 1) to the plays."
+    - See `the webfaction API <http://docs.webfaction.com/xmlrpc-api/>`_ for more info.
+options:
+
+    mailbox_name:
+        description:
+            - The name of the mailbox
+        required: true
+        default: null
+
+    mailbox_password:
+        description:
+            - The password for the mailbox
+        required: true
+        default: null
+
+    state:
+        description:
+            - Whether the mailbox should exist
+        required: false
+        choices: ['present', 'absent']
+        default: "present"
+
+    login_name:
+        description:
+            - The webfaction account to use
+        required: true
+
+    login_password:
+        description:
+            - The webfaction password to use
+        required: true
+'''
+
+import socket
+import xmlrpclib
+from ansible.module_utils.basic import *
+
+webfaction = xmlrpclib.ServerProxy('https://api.webfaction.com/')
+
+def main():
+
+    module = AnsibleModule(
+        argument_spec=dict(
+            mailbox_name=dict(required=True, default=None),
+            mailbox_password=dict(required=True),
+            state=dict(required=False, default='present'),
+            login_name=dict(required=True),
+            login_password=dict(required=True),
+        ),
+        supports_check_mode=True
+    )
+
+    mailbox_name = module.params['mailbox_name']
+    site_state = module.params['state']
+
+    session_id, account = webfaction.login(
+        module.params['login_name'],
+        module.params['login_password']
+    )
+
+    mailbox_list = webfaction.list_mailboxes(session_id)
+    existing_mailbox = mailbox_name in mailbox_list
+
+    result = {}
+    
+    # Here's where the real stuff happens
+
+    if site_state == 'present':
+
+        # Does a mailbox with this name already exist?
+        if existing_mailbox:
+            module.exit_json(changed=False,)
+
+        positional_args = [session_id, mailbox_name]
+
+        if not module.check_mode:
+            # If this isn't a dry run, create the mailbox
+            result.update(webfaction.create_mailbox(*positional_args))
+
+    elif site_state == 'absent':
+
+        # If the mailbox is already not there, nothing changed.
+        if not existing_mailbox:
+            module.exit_json(changed=False)
+
+        if not module.check_mode:
+            # If this isn't a dry run, delete the mailbox
+            result.update(webfaction.delete_mailbox(session_id, mailbox_name))
+
+    else:
+        module.fail_json(msg="Unknown state specified: {}".format(site_state))
+
+    module.exit_json(changed=True, result=result)
+
+# The conventional ending
+main()
+
