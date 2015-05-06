52a87e16379edf21c46bf45725694cad4c5a91f5
Peter Mounce <peter.mounce@just-eat.com>
2015-05-06 21:51:05 +0100

    version -> 2, quote doc strings with colons

--- windows/win_dotnet_ngen.py
+++ windows/win_dotnet_ngen.py
@@ -24,15 +24,15 @@
 DOCUMENTATION = '''
 ---
 module: win_dotnet_ngen
-version_added: "1.9"
+version_added: "2.0"
 short_description: Runs ngen to recompile DLLs after .NET  updates
 description:
     - After .NET framework is installed/updated, Windows will probably want to recompile things to optimise for the host.
     - This happens via scheduled task, usually at some inopportune time.
     - This module allows you to run this task on your own schedule, so you incur the CPU hit at some more convenient and controlled time.
-    - http://blogs.msdn.com/b/dotnet/archive/2013/08/06/wondering-why-mscorsvw-exe-has-high-cpu-usage-you-can-speed-it-up.aspx
-    - Note: there are in fact two scheduled tasks for ngen but they have no triggers so aren't a problem
-    - Note: there's no way to test if they've been completed (?)
+    - "http://blogs.msdn.com/b/dotnet/archive/2013/08/06/wondering-why-mscorsvw-exe-has-high-cpu-usage-you-can-speed-it-up.aspx"
+    - "Note: there are in fact two scheduled tasks for ngen but they have no triggers so aren't a problem"
+    - "Note: there's no way to test if they've been completed (?)"
     - Note: the stdout is quite likely to be several megabytes
 options:
 author: Peter Mounce
