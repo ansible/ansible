3d1090fc661bc69465f1b08000200fc72b38cf55
Quentin Stafford-Fraser <quentin@pobox.com>
2015-05-10 20:40:50 +0100

    Documentation version_added numbers are strings.

--- lib/ansible/modules/extras/cloud/webfaction/webfaction_mailbox.py
+++ lib/ansible/modules/extras/cloud/webfaction/webfaction_mailbox.py
@@ -10,7 +10,7 @@ short_description: Add or remove mailboxes on Webfaction
 description:
     - Add or remove mailboxes on a Webfaction account. Further documentation at http://github.com/quentinsf/ansible-webfaction.
 author: Quentin Stafford-Fraser
-version_added: 2.0
+version_added: "2.0"
 notes:
     - "You can run playbooks that use this on a local machine, or on a Webfaction host, or elsewhere, since the scripts use the remote webfaction API - the location is not important. However, running them on multiple hosts I(simultaneously) is best avoided. If you don't specify I(localhost) as your host, you may want to add C(serial: 1) to the plays."
     - See `the webfaction API <http://docs.webfaction.com/xmlrpc-api/>`_ for more info.
