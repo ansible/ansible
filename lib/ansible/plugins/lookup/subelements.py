830225d9c14b002babb9b8d10a3e1d7be31a97bd
James Cammarata <jimi@sngx.net>
2015-05-12 15:09:03 -0500

    Fix errors in subelements lookup plugin and associated tests (v2)

--- lib/ansible/plugins/lookup/subelements.py
+++ lib/ansible/plugins/lookup/subelements.py
@@ -33,8 +33,8 @@ class LookupModule(LookupBase):
             raise errors.AnsibleError(
                 "subelements lookup expects a list of two or three items, "
                 + msg)
-        terms = listify_lookup_plugin_terms(terms, self.basedir, inject)
-        terms[0] = listify_lookup_plugin_terms(terms[0], self.basedir, inject)
+        terms = listify_lookup_plugin_terms(terms, variables, loader=self._loader)
+        terms[0] = listify_lookup_plugin_terms(terms[0], variables, loader=self._loader)
 
         # check lookup terms - check number of terms
         if not isinstance(terms, list) or not 2 <= len(terms) <= 3:
