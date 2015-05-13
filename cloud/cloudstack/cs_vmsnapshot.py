d6781a5c8910e4bcddd74711d9320ce34e309faa
Greg DeKoenigsberg <greg.dekoenigsberg@gmail.com>
2015-05-13 14:13:09 -0400

    Adding author's github id

--- cloud/cloudstack/cs_vmsnapshot.py
+++ cloud/cloudstack/cs_vmsnapshot.py
@@ -25,7 +25,7 @@ short_description: Manages VM snapshots on Apache CloudStack based clouds.
 description:
     - Create, remove and revert VM from snapshots.
 version_added: '2.0'
-author: René Moser
+author: "René Moser (@resmo)" <mail@renemoser.net>
 options:
   name:
     description:
