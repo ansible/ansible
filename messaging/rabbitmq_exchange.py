709817bf19bd1a108ed96f69b90ba6a375cc92ea
Manuel Sousa <manuel.sousa@gmail.com>
2015-05-15 16:08:47 +0100

    Missed variable exchangeExists -> exchange_exists

--- messaging/rabbitmq_exchange.py
+++ messaging/rabbitmq_exchange.py
@@ -139,10 +139,10 @@ def main():
     r = requests.get( url, auth=(module.params['login_user'],module.params['login_password']))
 
     if r.status_code==200:
-        exchangeExists = True
+        exchange_exists = True
         response = r.json()
     elif r.status_code==404:
-        exchangeExists = False
+        exchange_exists = False
         response = r.text
     else:
         module.fail_json(
@@ -151,9 +151,9 @@ def main():
         )
 
     if module.params['state']=='present':
-        change_required = not exchangeExists
+        change_required = not exchange_exists
     else:
-        change_required = exchangeExists
+        change_required = exchange_exists
 
     # Check if attributes change on existing exchange
     if not change_required and r.status_code==200 and module.params['state'] == 'present':
