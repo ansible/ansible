9b6a7416155e26e939c7681cb2d61de900792ff3
Schlueter <bs@schlueter.blue>
2015-05-21 12:04:13 -0400

    Correct pass by object reference issue

--- packaging/language/easy_install.py
+++ packaging/language/easy_install.py
@@ -90,7 +90,7 @@ EXAMPLES = '''
 '''
 
 def _is_package_installed(module, name, easy_install, executable_arguments):
-    executable_arguments.append('--dry-run')
+    executable_arguments = executable_arguments + ['--dry-run']
     cmd = '%s %s %s' % (easy_install, ' '.join(executable_arguments), name)
     rc, status_stdout, status_stderr = module.run_command(cmd)
     return not ('Reading' in status_stdout or 'Downloading' in status_stdout)
