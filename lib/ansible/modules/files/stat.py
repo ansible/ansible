322518d06cd47a02eb9afc3863117daacd9786b0
Brian Coca <brian.coca+git@gmail.com>
2015-05-26 15:22:39 -0400

    stat doc fix fixes #1371

--- lib/ansible/modules/files/stat.py
+++ lib/ansible/modules/files/stat.py
@@ -233,13 +233,13 @@ stat:
         md5:
             description: md5 hash of the path
             returned: success, path exists and user can read stats and path supports hashing and md5 is supported
-            type: boolean
-            sample: True
+            type: string
+            sample: f88fa92d8cf2eeecf4c0a50ccc96d0c0
         checksum:
             description: hash of the path
             returned: success, path exists and user can read stats and path supports hashing
-            type: boolean
-            sample: True
+            type: string
+            sample: 50ba294cdf28c0d5bcde25708df53346825a429f
         pw_name:
             description: User name of owner
             returned: success, path exists and user can read stats and installed python supports it
