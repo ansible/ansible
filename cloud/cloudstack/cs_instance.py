eb66f683f538c1fe73902e246de7e8d3bec29c5d
Rene Moser <mail@renemoser.net>
2015-05-30 11:03:32 +0200

    cloudstack: add new param api_timeout

--- cloud/cloudstack/cs_instance.py
+++ cloud/cloudstack/cs_instance.py
@@ -801,6 +801,7 @@ def main():
             api_secret = dict(default=None, no_log=True),
             api_url = dict(default=None),
             api_http_method = dict(default='get'),
+            api_timeout = dict(type='int', default=10),
         ),
         required_together = (
             ['api_key', 'api_secret', 'api_url'],
