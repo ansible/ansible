9417b58011747e7ce99208e298ed462a71e46c4e
Quentin Stafford-Fraser <quentin@pobox.com>
2015-06-02 09:25:55 +0100

    Add GPL notices

--- lib/ansible/modules/extras/cloud/webfaction/webfaction_app.py
+++ lib/ansible/modules/extras/cloud/webfaction/webfaction_app.py
@@ -1,10 +1,29 @@
 #! /usr/bin/python
+#
 # Create a Webfaction application using Ansible and the Webfaction API
 #
 # Valid application types can be found by looking here:
 # http://docs.webfaction.com/xmlrpc-api/apps.html#application-types
 #
-# Quentin Stafford-Fraser 2015
+# ------------------------------------------
+#
+# (c) Quentin Stafford-Fraser 2015
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 DOCUMENTATION = '''
 ---
--- lib/ansible/modules/extras/cloud/webfaction/webfaction_site.py
+++ lib/ansible/modules/extras/cloud/webfaction/webfaction_site.py
@@ -1,7 +1,26 @@
 #! /usr/bin/python
+#
 # Create Webfaction website using Ansible and the Webfaction API
 #
-# Quentin Stafford-Fraser 2015
+# ------------------------------------------
+#
+# (c) Quentin Stafford-Fraser 2015
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 DOCUMENTATION = '''
 ---
