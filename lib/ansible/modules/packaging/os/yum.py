a61095b0b1e793f351a5d191c1f3e1ca96ba1247
Albert Mikaelyan <tahvok@gmail.com>
2015-06-03 16:48:19 +0300

    Update yum.py

--- lib/ansible/modules/packaging/os/yum.py
+++ lib/ansible/modules/packaging/os/yum.py
@@ -46,7 +46,7 @@ description:
 options:
   name:
     description:
-      - "Package name, or package specifier with version, like C(name-1.0). When using state=latest, this can be '*' which means run: yum -y update. You can also pass a url or a local path to a rpm file.  To operate on several packages this can accept a comma separated list of packages or (as of 2.0) a list of packages."
+      - "Package name, or package specifier with version, like C(name-1.0). When using state=latest, this can be '*' which means run: yum -y update. You can also pass a url or a local path to a rpm file (using state=present).  To operate on several packages this can accept a comma separated list of packages or (as of 2.0) a list of packages."
     required: true
     default: null
     aliases: [ 'pkg' ]
