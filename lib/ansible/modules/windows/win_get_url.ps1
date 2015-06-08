69960b3c2e82bac1e72f27f60ffc4f6b4a8281ab
Tal Auslander <tal@cloudshare.com>
2015-06-08 14:45:20 +0300

    win_get_url re-download file only if modified

--- lib/ansible/modules/windows/win_get_url.ps1
+++ lib/ansible/modules/windows/win_get_url.ps1
@@ -1,7 +1,7 @@
 #!powershell
 # This file is part of Ansible.
 #
-# Copyright 2014, Paul Durivage <paul.durivage@rackspace.com>
+# (c)) 2015, Paul Durivage <paul.durivage@rackspace.com>, Tal Auslander <tal@cloudshare.com>
 #
 # Ansible is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -40,14 +40,38 @@ Else {
     Fail-Json $result "missing required argument: dest"
 }
 
-$client = New-Object System.Net.WebClient
+If (-not (Test-Path $dest)) {
+    $client = New-Object System.Net.WebClient
 
-Try {
-    $client.DownloadFile($url, $dest)
-    $result.changed = $true
+    Try {
+        $client.DownloadFile($url, $dest)
+        $result.changed = $true
+    }
+    Catch {
+        Fail-Json $result "Error downloading $url to $dest"
+    }
 }
-Catch {
-    Fail-Json $result "Error downloading $url to $dest"
+Else {
+    Try {
+        $webRequest = [System.Net.HttpWebRequest]::Create($url)
+        $webRequest.IfModifiedSince = ([System.IO.FileInfo]$dest).LastWriteTime
+        $webRequest.Method = "GET"
+        [System.Net.HttpWebResponse]$webResponse = $webRequest.GetResponse()
+        
+        $stream = New-Object System.IO.StreamReader($response.GetResponseStream())
+        
+        $stream.ReadToEnd() | Set-Content -Path $dest -Force
+        
+        $result.changed = $true
+    }
+    Catch [System.Net.WebException] {
+        If ($_.Exception.Response.StatusCode -ne [System.Net.HttpStatusCode]::NotModified) {
+            Fail-Json $result "Error downloading $url to $dest"
+        }
+    }
+    Catch {
+        Fail-Json $result "Error downloading $url to $dest"
+    }
 }
 
 Set-Attr $result.win_get_url "url" $url
