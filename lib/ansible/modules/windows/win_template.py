6dcc275d8d906f81d4fdd9a85a05b2f23e83c3f9
Brian Coca <bcoca@ansible.com>
2015-06-10 11:08:12 -0400

    updated version added

--- lib/ansible/modules/windows/win_template.py
+++ lib/ansible/modules/windows/win_template.py
@@ -3,7 +3,7 @@
 DOCUMENTATION = '''
 ---
 module: win_template
-version_added: 1.8
+version_added: "2.0"
 short_description: Templates a file out to a remote server.
 description:
      - Templates are processed by the Jinja2 templating language
