ef0ddb1890189977a78fddd61aeae2f488354d89
Benno Joy <benno@ansible.com>
2015-06-10 12:45:25 +0530

    fixes issue where hostvars are not loaded in the Host object

--- lib/ansible/inventory/__init__.py
+++ lib/ansible/inventory/__init__.py
@@ -661,10 +661,16 @@ class Inventory(object):
             if group and host is None:
                 # load vars in dir/group_vars/name_of_group
                 base_path = os.path.join(basedir, "group_vars/%s" % group.name)
+                #The variables also needs to go into the Host objects vars key
+                if self._loader.path_exists(base_path):
+                    results = self._loader.load_from_file(base_path)
                 self._variable_manager.add_group_vars_file(base_path, self._loader)
             elif host and group is None:
                 # same for hostvars in dir/host_vars/name_of_host
                 base_path = os.path.join(basedir, "host_vars/%s" % host.name)
+                #The variables also needs to go into the Host objects vars key
+                if self._loader.path_exists(base_path):
+                    results = self._loader.load_from_file(base_path)
                 self._variable_manager.add_host_vars_file(base_path, self._loader)
 
         # all done, results is a dictionary of variables for this particular host.
