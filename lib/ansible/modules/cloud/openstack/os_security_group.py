072110c757178a68980863efbc329dfbb3d6f43b
dagnello <dagnello@hp.com>
2015-06-19 10:45:12 -0700

    Resolving secgroup.id issue in this module
    secgroup['id'] was not being returned in all cases where the
    specified security group exists.

--- lib/ansible/modules/cloud/openstack/os_security_group.py
+++ lib/ansible/modules/cloud/openstack/os_security_group.py
@@ -115,24 +115,24 @@ def main():
         if module.check_mode:
             module.exit_json(changed=_system_state_change(module, secgroup))
 
-        changed = False
         if state == 'present':
             if not secgroup:
                 secgroup = cloud.create_security_group(name, description)
-                changed = True
+                module.exit_json(changed=True, id=secgroup['id'])
             else:
                 if _needs_update(module, secgroup):
                     secgroup = cloud.update_security_group(
                         secgroup['id'], description=description)
-                    changed = True
-            module.exit_json(
-                changed=changed, id=secgroup.id, secgroup=secgroup)
+                    module.exit_json(changed=True, id=secgroup['id'])
+                else:
+                    module.exit_json(changed=False, id=secgroup['id'])
 
         if state == 'absent':
-            if secgroup:
+            if not secgroup:
+                module.exit_json(changed=False)
+            else:
                 cloud.delete_security_group(secgroup['id'])
-                changed=True
-            module.exit_json(changed=changed)
+                module.exit_json(changed=True)
 
     except shade.OpenStackCloudException as e:
         module.fail_json(msg=e.message)
