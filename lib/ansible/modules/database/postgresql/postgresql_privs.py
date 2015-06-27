3f4ffda583bf983177d0125eecca3c8b00dec9ce
ToBeReplaced <ToBeReplaced@gmail.com>
2015-06-27 11:41:01 -0600

    Make ALL_IN_SCHEMA for tables affect views
    ALL TABLES is considered to include views, so we must check for reltypes
    'r' and 'v', not just 'r'. This bug was introduced due to using a
    custom, backwards-compatible version of "ALL TABLES IN SCHEMA".

--- lib/ansible/modules/database/postgresql/postgresql_privs.py
+++ lib/ansible/modules/database/postgresql/postgresql_privs.py
@@ -315,7 +315,7 @@ class Connection(object):
         query = """SELECT relname
                    FROM pg_catalog.pg_class c
                    JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-                   WHERE nspname = %s AND relkind = 'r'"""
+                   WHERE nspname = %s AND relkind in ('r', 'v')"""
         self.cursor.execute(query, (schema,))
         return [t[0] for t in self.cursor.fetchall()]
 
