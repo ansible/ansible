8b3875f2866c73525e448dcea97ebd329edf4cf9
Toshio Kuratomi <toshio@fedoraproject.org>
2015-07-06 13:15:11 -0700

    Test unquote works as expected and fix two bugs: * escaped end quote * a single quote character

--- v2/ansible/parsing/splitter.py
+++ v2/ansible/parsing/splitter.py
@@ -249,7 +249,7 @@ def split_args(args):
     return params
 
 def is_quoted(data):
-    return len(data) > 0 and (data[0] == '"' and data[-1] == '"' or data[0] == "'" and data[-1] == "'")
+    return len(data) > 1 and data[0] == data[-1] and data[0] in ('"', "'") and data[-2] != '\\'
 
 def unquote(data):
     ''' removes first and last quotes from a string, if the string starts and ends with the same quotes '''
