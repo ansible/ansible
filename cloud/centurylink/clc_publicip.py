8a41108b2b0f25f709ee7939f918550a44b81fe4
Siva Popuri <popuri.siva@gmail.com>
2015-07-14 16:36:50 -0500

    corrected the license string to make it compatible with GPLV3

--- cloud/centurylink/clc_publicip.py
+++ cloud/centurylink/clc_publicip.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python
 
-# CenturyLink Cloud Ansible Modules.
 #
-# These Ansible modules enable the CenturyLink Cloud v2 API to be called
-# from an within Ansible Playbook.
+# Copyright (c) 2015 CenturyLink
 #
-# This file is part of CenturyLink Cloud, and is maintained
-# by the Workflow as a Service Team
+# This file is part of Ansible.
 #
-# Copyright 2015 CenturyLink Cloud
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# CenturyLink Cloud: http://www.CenturyLinkCloud.com
-# API Documentation: https://www.centurylinkcloud.com/api-docs/v2/
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>
 #
 
 DOCUMENTATION = '''
