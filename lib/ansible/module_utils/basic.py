6e9af6456f228a9ea45ab10e73c16c18505b327f
evanccnyc <evanccnyc@users.noreply.github.com>
2015-07-14 16:11:53 -0400

    Fixing bug #1655
    This seems to fix bug #1655

--- lib/ansible/module_utils/basic.py
+++ lib/ansible/module_utils/basic.py
@@ -1260,7 +1260,7 @@ class AnsibleModule(object):
             self.fail_json(msg='Boolean %s not in either boolean list' % arg)
 
     def jsonify(self, data):
-        for encoding in ("utf-8", "latin-1"):
+        for encoding in ("utf-8", "latin-1", "unicode_escape"):
             try:
                 return json.dumps(data, encoding=encoding)
             # Old systems using old simplejson module does not support encoding keyword.
