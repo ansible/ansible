46f01535ec343e4e136694d6f3ad04b039beb5a4
Brian Coca <brian.coca+git@gmail.com>
2015-07-20 11:34:57 -0400

    minor docfixes

--- cloud/centurylink/clc_publicip.py
+++ cloud/centurylink/clc_publicip.py
@@ -60,12 +60,12 @@ requirements:
 notes:
     - To use this module, it is required to set the below environment variables which enables access to the
       Centurylink Cloud
-          - CLC_V2_API_USERNAME: the account login id for the centurylink cloud
-          - CLC_V2_API_PASSWORD: the account passwod for the centurylink cloud
+          - CLC_V2_API_USERNAME, the account login id for the centurylink cloud
+          - CLC_V2_API_PASSWORD, the account passwod for the centurylink cloud
     - Alternatively, the module accepts the API token and account alias. The API token can be generated using the
       CLC account login and password via the HTTP api call @ https://api.ctl.io/v2/authentication/login
-          - CLC_V2_API_TOKEN: the API token generated from https://api.ctl.io/v2/authentication/login
-          - CLC_ACCT_ALIAS: the account alias associated with the centurylink cloud
+          - CLC_V2_API_TOKEN, the API token generated from https://api.ctl.io/v2/authentication/login
+          - CLC_ACCT_ALIAS, the account alias associated with the centurylink cloud
     - Users can set CLC_V2_API_URL to specify an endpoint for pointing to a different CLC environment.
 '''
 
