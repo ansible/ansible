31912b4c0cd5f68a9da5ba8515f5bb1b72f3c742
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2015-07-21 17:13:21 +0530

    Don't try to _get_file_contents(None)
    When run from a playbook without roles, 'role_path' was unset, so
    relative_path was never set, so the lookup broke while trying to coerce
    None to a string, and the expected 'could not locate file' message was
    never reached.

--- lib/ansible/plugins/lookup/file.py
+++ lib/ansible/plugins/lookup/file.py
@@ -53,9 +53,10 @@ class LookupModule(LookupBase):
 
             for path in (basedir_path, relative_path, playbook_path):
                 try:
-                    contents, show_data = self._loader._get_file_contents(path)
-                    ret.append(contents.rstrip())
-                    break
+                    if path:
+                        contents, show_data = self._loader._get_file_contents(path)
+                        ret.append(contents.rstrip())
+                        break
                 except AnsibleParserError:
                     continue
             else:
