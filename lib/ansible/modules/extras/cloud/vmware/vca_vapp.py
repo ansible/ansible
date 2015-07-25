d2977d4c95289f619c9fc80bdaf791549d7b3c9d
Brian Coca <bcoca@ansible.com>
2015-07-24 23:16:35 -0400

    fixed license and copyright

--- lib/ansible/modules/extras/cloud/vmware/vca_vapp.py
+++ lib/ansible/modules/extras/cloud/vmware/vca_vapp.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python
 
-# Copyright (c) 2015 VMware, Inc. All Rights Reserved.
+# Copyright (c) 2015 Ansible, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# This file is part of Ansible
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
 
 DOCUMENTATION = '''
