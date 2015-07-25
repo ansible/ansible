dc92f0af4ca6c569a3048c25bdb9efef59c1a006
Maciej Delmanowski <drybjed@gmail.com>
2015-07-25 18:15:23 +0200

    Rewrite method to not use try/except
    Additional checks are not needed, because 'self.conn.get_entry(-1)'
    returns all existing entries, each one should have state defined.

--- cloud/misc/virt_net.py
+++ cloud/misc/virt_net.py
@@ -317,18 +317,13 @@ class VirtNetwork(object):
         return self.conn.find_entry(entryid)
 
     def list_nets(self, state=None):
-        entries = self.conn.find_entry(-1)
         results = []
-        for x in entries:
-            try:
-                if state:
-                    entrystate = self.conn.get_status2(x)
-                    if entrystate == state:
-                        results.append(x.name())
-                else:
-                    results.append(x.name())
-            except:
-                pass
+        for entry in self.conn.find_entry(-1):
+            if state:
+                if state == self.conn.get_status2(entry):
+                    results.append(entry.name())
+            else:
+                results.append(entry.name())
         return results
 
     def state(self):
