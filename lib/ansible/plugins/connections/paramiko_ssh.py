75d3d5d4f9dacbb9de6c2f5450ed889ced8f898e
Brian Coca <bcoca@ansible.com>
2015-07-29 20:13:17 -0400

    fixed non ignore errors path to actually interrupt fixed cases where missing/inaccessible file gave exception, now you get nice error

--- lib/ansible/plugins/connections/paramiko_ssh.py
+++ lib/ansible/plugins/connections/paramiko_ssh.py
@@ -179,6 +179,7 @@ class Connection(ConnectionBase):
                 key_filename=key_filename,
                 password=self._play_context.password,
                 timeout=self._play_context.timeout,
+                compress=True,
                 port=port,
             )
         except Exception as e:
