deb36b00d06c55cb815594667e76ded878bccf91
Brian Coca <brian.coca+git@gmail.com>
2015-07-30 14:48:59 -0400

    added missing license headers fixes #508

--- lib/ansible/modules/extras/monitoring/bigpanda.py
+++ lib/ansible/modules/extras/monitoring/bigpanda.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
 DOCUMENTATION = '''
 ---
@@ -6,7 +22,7 @@ module: bigpanda
 author: BigPanda
 short_description: Notify BigPanda about deployments
 version_added: "1.8"
-description: 
+description:
    - Notify BigPanda when deployments start and end (successfully or not). Returns a deployment object containing all the parameters for future module calls.
 options:
   component:
