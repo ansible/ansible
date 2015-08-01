5887e96b270b2f0d19d3e34a09e16e21824c4887
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2015-08-01 09:26:07 +0530

    Introduce a connection locking infrastructure
    The lock file is (a temporary file) opened in the parent process, whose
    open fd is inherited by the workers after fork, and passed down through
    the PlayContext. Connection grows lock/unlock methods which can be used
    by individual connection plugins.

--- lib/ansible/playbook/play_context.py
+++ lib/ansible/playbook/play_context.py
@@ -161,6 +161,7 @@ class PlayContext(Base):
     _private_key_file = FieldAttribute(isa='string', default=C.DEFAULT_PRIVATE_KEY_FILE)
     _timeout          = FieldAttribute(isa='int', default=C.DEFAULT_TIMEOUT)
     _shell            = FieldAttribute(isa='string')
+    _connection_lockfd= FieldAttribute(isa='int', default=None)
 
     # privilege escalation fields
     _become           = FieldAttribute(isa='bool')
@@ -243,6 +244,11 @@ class PlayContext(Base):
 
         if options.connection:
             self.connection = options.connection
+
+        # The lock file is opened in the parent process, and the workers will
+        # inherit the open file, so we just need to help them find it.
+        if options.connection_lockfile:
+            self.connection_lockfd = options.connection_lockfile.fileno()
 
         self.remote_user = options.remote_user
         self.private_key_file = options.private_key_file
