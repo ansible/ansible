a76ea8b96a20655f7ebd18ec800523f16f40bf21
Sam Yaple <sam@yaple.net>
2015-08-03 10:49:37 +0000

    Revert "escapeds changes"
    While this change doesn't break the creation, it does break
    idempotency. This change will convert '*.*' to '`*`.*' which is
    functionally the same, however when the user_mod() function looks up
    the current privileges with privileges_get() it will read '*.*'
    
    Since '*.*' != '`*`.*' it will go through the process of updating the
    privleges always resulting in a 'changed' result.
    
    This reverts commit db9ab9b2629f00350a743a4eca72fb5ee8dc8c77.

--- lib/ansible/modules/database/mysql/mysql_user.py
+++ lib/ansible/modules/database/mysql/mysql_user.py
@@ -317,8 +317,13 @@ def privileges_unpack(priv):
     privs = []
     for item in priv.strip().split('/'):
         pieces = item.strip().split(':')
-        dbpriv = pieces[0].rsplit(".", 1)
-        pieces[0] = "`%s`.%s" % (dbpriv[0].strip('`'), dbpriv[1])
+        if '.' in pieces[0]:
+            pieces[0] = pieces[0].split('.')
+            for idx, piece in enumerate(pieces):
+                if pieces[0][idx] != "*":
+                    pieces[0][idx] = "`" + pieces[0][idx] + "`"
+            pieces[0] = '.'.join(pieces[0])
+
         if '(' in pieces[1]:
             output[pieces[0]] = re.split(r',\s*(?=[^)]*(?:\(|$))', pieces[1].upper())
             for i in output[pieces[0]]:
