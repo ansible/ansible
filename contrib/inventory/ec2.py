920801359eb5274128916b557626ae330af01873
John Barbuto <john.barbuto@venmo.com>
2015-08-07 16:15:27 -0700

    Linting fixes for ec2 dynamic inventory script (E251)

--- contrib/inventory/ec2.py
+++ contrib/inventory/ec2.py
@@ -414,7 +414,7 @@ class Ec2Inventory(object):
             reservations = []
             if self.ec2_instance_filters:
                 for filter_key, filter_values in self.ec2_instance_filters.items():
-                    reservations.extend(conn.get_all_instances(filters = {filter_key: filter_values}))
+                    reservations.extend(conn.get_all_instances(filters={filter_key: filter_values}))
             else:
                 reservations = conn.get_all_instances()
 
