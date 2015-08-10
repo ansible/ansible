ansible/ansible

commit d35b9569006be8b6fb2090b24969b678166386e2

listify lookup plugin terms when they're specified as "{{ lookup(terms) }}"
Before this, they were not listified there but they were listified when
specified like this:

with_lookup: terms

lib/ansible/template/__init__.py
@@ -251,6 +251,7 @@ class Templar:
         instance = self._lookup_loader.get(name.lower(), loader=self._loader, templar=self)
 
         if instance is not None:
+            loop_terms = listify_lookup_plugin_terms(terms=8args, templar=self, loader=self._loader, fail_on_undefined=True)
             # safely catch run failures per #5059
             try:
                 ran = instance.run(*args, variables=self._available_variables, **kwargs)

lib/ansible/executor/task_executor.py
@@ -153,7 +153,6 @@ class TaskExecutor:
         items = None
         if self._task.loop:
             if self._task.loop in self._shared_loader_obj.lookup_loader:
-                loop_terms = listify_lookup_plugin_terms(terms=self._task.loop_args, templar=templar, loader=self._loader, fail_on_undefined=True)
                 items = self._shared_loader_obj.lookup_loader.get(self._task.loop, loader=self._loader, templar=templar).run(terms=loop_terms, variables=vars_copy)
             else:
                 raise AnsibleError("Unexpected failure in finding the lookup named '%s' in the available lookup plugins" % self._task.loop)

lib/ansible/plugins/lookup/cartesian.py
@@ -29,7 +29,13 @@ class LookupModule(LookupBase):
     [1, 2, 3], [a, b] -> [1, a], [1, b], [2, a], [2, b], [3, a], [3, b]
     """
 
-    def __lookup_variables(self, terms):
+    def _lookup_variables(self, terms):
+        """
+        Turn this:
+            terms == ["1,2,3", "a,b"]
+        into this:
+            terms == [[1,2,3], [a, b]]
+        """
         results = []
         for x in terms:
             intermediate = listify_lookup_plugin_terms(x, templar=self._templar, loader=self._loader)
@@ -38,7 +44,7 @@ class LookupModule(LookupBase):
 
     def run(self, terms, variables=None, **kwargs):
 
-        terms = self.__lookup_variables(terms)
+        terms = self._lookup_variables(terms)
 
         my_list = terms[:]
         if len(my_list) == 0:

lib/ansible/plugins/lookup/credstash.py
@@ -35,9 +35,6 @@ class LookupModule(LookupBase):
         if not CREDSTASH_INSTALLED:
             raise AnsibleError('The credstash lookup plugin requires credstash to be installed.')
 
-        if isinstance(terms, basestring):
-            terms = [terms]
-
         ret = []
         for term in terms:
             try:

lib/ansible/plugins/lookup/csvfile.py
@@ -42,9 +42,6 @@ class LookupModule(LookupBase):
 
     def run(self, terms, variables=None, **kwargs):
 
-        if isinstance(terms, basestring):
-            terms = [ terms ]
-
         basedir = self.get_basedir(variables)
 
         ret = []

lib/ansible/plugins/lookup/dnstxt.py
@@ -44,9 +44,6 @@ class LookupModule(LookupBase):
         if HAVE_DNS == False:
             raise AnsibleError("Can't LOOKUP(dnstxt): module dns.resolver is not installed")
 
-        if isinstance(terms, basestring):
-            terms = [ terms ]
-
         ret = []
         for term in terms:
             domain = term.split()[0]

lib/ansible/plugins/lookup/env.py
@@ -25,9 +25,6 @@ class LookupModule(LookupBase):
 
     def run(self, terms, variables, **kwargs):
 
-        if isinstance(terms, basestring):
-            terms = [ terms ]
-
         ret = []
         for term in terms:
             var = term.split()[0]

lib/ansible/plugins/lookup/etcd.py
@@ -66,9 +66,6 @@ class LookupModule(LookupBase):
 
     def run(self, terms, variables, **kwargs):
 
-        if isinstance(terms, basestring):
-            terms = [ terms ]
-
         validate_certs = kwargs.get('validate_certs', True)
 
         etcd = Etcd(validate_certs=validate_certs)

commit 31b71421a6e7901e2cc46067d076167471b03a72

Fix syntax error introduced in d35b9569006be8b6

lib/ansible/template/__init__.py
@@ -251,7 +251,7 @@ class Templar:
         instance = self._lookup_loader.get(name.lower(), loader=self._loader, templar=self)
 
         if instance is not None:
-            loop_terms = listify_lookup_plugin_terms(terms=8args, templar=self, loader=self._loader, fail_on_undefined=True)
+            loop_terms = listify_lookup_plugin_terms(terms=args, templar=self, loader=self._loader, fail_on_undefined=True)
             # safely catch run failures per #5059
             try:
                 ran = instance.run(*args, variables=self._available_variables, **kwargs)