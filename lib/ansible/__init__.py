bf5353767ea80d31d076a603e333097324fa05af
James Cammarata <jimi@sngx.net>
2015-08-11 13:28:41 -0400

    Version bump for 1.9.3-rc1

--- lib/ansible/__init__.py
+++ lib/ansible/__init__.py
@@ -14,5 +14,5 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-__version__ = '1.9.2'
+__version__ = '1.9.3'
 __author__ = 'Ansible, Inc.'
