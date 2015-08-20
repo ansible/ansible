e4a5f3b4099765a103ab949741312e520e8d35d6
Brian Richards <Brian.Richards@emerson.com>
2015-08-20 11:07:58 -0500

    Preventing servers that are deleted and left in the deleted state from being included in the server list

--- cloud/rackspace/rax_facts.py
+++ cloud/rackspace/rax_facts.py
@@ -97,7 +97,9 @@ def rax_facts(module, address, name, server_id):
             servers.append(cs.servers.get(server_id))
         except Exception, e:
             pass
-
+    
+    servers[:] = [server for server in servers if server.status != "DELETED"]
+    
     if len(servers) > 1:
         module.fail_json(msg='Multiple servers found matching provided '
                              'search parameters')
