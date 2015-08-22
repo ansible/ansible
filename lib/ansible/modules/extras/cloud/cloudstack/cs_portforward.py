eee7e8da99249e05fda9c71185933115f17a3780
Rene Moser <mail@renemoser.net>
2015-08-23 00:04:51 +0200

    cloudstack: cs_portforward: fix returns for int casting
    * missing self.
    * variable must be named portforwarding_rule, not resource

--- lib/ansible/modules/extras/cloud/cloudstack/cs_portforward.py
+++ lib/ansible/modules/extras/cloud/cloudstack/cs_portforward.py
@@ -361,9 +361,9 @@ class AnsibleCloudStackPortforwarding(AnsibleCloudStack):
         super(AnsibleCloudStackPortforwarding, self).get_result(portforwarding_rule)
         if portforwarding_rule:
             # Bad bad API does not always return int when it should.
-            for search_key, return_key in returns_to_int.iteritems():
-                if search_key in resource:
-                    self.result[return_key] = int(resource[search_key])
+            for search_key, return_key in self.returns_to_int.iteritems():
+                if search_key in portforwarding_rule:
+                    self.result[return_key] = int(portforwarding_rule[search_key])
         return self.result
 
 
