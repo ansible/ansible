1faf8dd5c2619fa8474e30fa2fd8a0ee5d4f0a05
Yann Hamon <yann.hamon@9cookies.com>
2015-08-26 00:01:35 +0200

    Docker module: restarted should update the container when necessary

--- lib/ansible/modules/cloud/docker/docker.py
+++ lib/ansible/modules/cloud/docker/docker.py
@@ -1643,6 +1643,10 @@ def restarted(manager, containers, count, name):
 
     containers.refresh()
 
+    for container in manager.get_differing_containers():
+        manager.stop_containers([container])
+        manager.remove_containers([container])
+
     manager.restart_containers(containers.running)
     started(manager, containers, count, name)
 
