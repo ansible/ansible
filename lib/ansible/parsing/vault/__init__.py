ansible/ansible

commit 7ce9e1d13dad4065f9f186f2e1786df4bda010ab

Encrypt the vault file after editing only if the contents changed

lib/ansible/parsing/vault/__init__.py
@@ -227,6 +227,10 @@ class VaultEditor(object):
         call(self._editor_shell_command(tmp_path))
         tmpdata = self.read_data(tmp_path)
 
+        # Do nothing if the content has not changed
+        if existing_data == tmpdata:
+            return
+
         # create new vault
         this_vault = VaultLib(self.password)
         if cipher:

commit a45414f98063e38f3c7e761da560f78c1d83601d

Remove decrypted vault temp_file mistakenly left from patch making vault edit idempotent
This bug was introduced in commit 7ce9e1d on July 27.  Hasn't gone out
in a release yet.

lib/ansible/parsing/vault/__init__.py
@@ -257,6 +257,7 @@ class VaultEditor:
 
         # Do nothing if the content has not changed
         if existing_data == tmpdata and not force_save:
+            os.remove(tmp_path)
             return
 
         # create new vault