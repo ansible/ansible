7d90f68aeee4fc6ccf59bd2e9ef623100eba2fe4
Brian Coca <bcoca@ansible.com>
2015-08-28 12:14:23 -0400

    implemented not showing skipped hosts config

--- lib/ansible/plugins/callback/default.py
+++ lib/ansible/plugins/callback/default.py
@@ -77,10 +77,11 @@ class CallbackModule(CallbackBase):
         self._handle_warnings(result._result)
 
     def v2_runner_on_skipped(self, result):
-        msg = "skipping: [%s]" % result._host.get_name()
-        if (self._display.verbosity > 0 or '_ansible_verbose_always' in result._result) and not '_ansible_verbose_override' in result._result:
-            msg += " => %s" % self._dump_results(result._result)
-        self._display.display(msg, color='cyan')
+        if not C.DISPLAY_SKIPPED_HOSTS:
+            msg = "skipping: [%s]" % result._host.get_name()
+            if (self._display.verbosity > 0 or '_ansible_verbose_always' in result._result) and not '_ansible_verbose_override' in result._result:
+                msg += " => %s" % self._dump_results(result._result)
+            self._display.display(msg, color='cyan')
 
     def v2_runner_on_unreachable(self, result):
         self._display.display("fatal: [%s]: UNREACHABLE! => %s" % (result._host.get_name(), self._dump_results(result._result)), color='red')
