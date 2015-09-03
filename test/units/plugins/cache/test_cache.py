ansible/ansible

commit 787a82f653c5d4581984ebc76d726b32100377ab

Replace .iteritems() with six.iteritems()
Replace .iteritems() with six.iteritems() everywhere except in
module_utils (because there's no 'six' on the remote host).  And except
in lib/ansible/galaxy/data/metadata_template.j2, because I'm not sure
six is available there.

lib/ansible/plugins/cache/__init__.py
@@ -60,7 +60,7 @@ class FactCache(MutableMapping):
 
     def copy(self):
         """ Return a primitive copy of the keys and values from the cache. """
-        return dict([(k, v) for (k, v) in self.iteritems()])
+        return dict([(k, v) for (k, v) in iteritems(self)])
 
     def keys(self):
         return self._plugin.keys()

lib/ansible/playbook/role/__init__.py
@@ -51,7 +51,7 @@ def hash_params(params):
         return params
     else:
         s = set()
-        for k,v in params.iteritems():
+        for k,v in iteritems(params):
             if isinstance(v, dict):
                 s.update((k, hash_params(v)))
             elif isinstance(v, list):
@@ -105,7 +105,7 @@ class Role(Base, Become, Conditional, Taggable):
                 params['tags'] = role_include.tags
             hashed_params = hash_params(params)
             if role_include.role in play.ROLE_CACHE:
-                for (entry, role_obj) in play.ROLE_CACHE[role_include.role].iteritems():
+                for (entry, role_obj) in iteritems(play.ROLE_CACHE[role_include.role]):
                     if hashed_params == entry:
                         if parent_role:
                             role_obj.add_parent(parent_role)

lib/ansible/plugins/strategies/__init__.py
@@ -20,6 +20,8 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 from six.moves import queue as Queue
+from six import iteritems
+
 import time
 
 from ansible import constants as C
@@ -207,7 +209,7 @@ class StrategyBase:
                     if task_result._task._role is not None and result[0] in ('host_task_ok', 'host_task_failed'):
                         # lookup the role in the ROLE_CACHE to make sure we're dealing
                         # with the correct object and mark it as executed
-                        for (entry, role_obj) in iterator._play.ROLE_CACHE[task_result._task._role._role_name].iteritems():
+                        for (entry, role_obj) in iteritems(iterator._play.ROLE_CACHE[task_result._task._role._role_name]):
                             if role_obj._uuid == task_result._task._role._uuid:
                                 role_obj._had_task_run[host.name] = True
 
@@ -358,7 +360,7 @@ class StrategyBase:
                     groups[group_name] = []
                 groups[group_name].append(host)
 
-        for group_name, hosts in groups.iteritems():
+        for group_name, hosts in iteritems(groups):
             new_group = self._inventory.get_group(group_name)
             if not new_group:
                 # create the new group and add it to inventory

contrib/inventory/apache-libcloud.py
@@ -37,6 +37,7 @@ import re
 from time import time
 import ConfigParser
 
+from six import iteritems
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 import libcloud.security as sec
@@ -268,7 +269,7 @@ class LibcloudInventory(object):
             elif key == 'ec2_region':
                 instance_vars[key] = value.name
             elif key == 'ec2_tags':
-                for k, v in value.iteritems():
+                for k, v in iteritems(value):
                     key = self.to_safe('ec2_tag_' + k)
                     instance_vars[key] = v
             elif key == 'ec2_groups':

contrib/inventory/cobbler.py
@@ -72,6 +72,8 @@ try:
 except ImportError:
     import simplejson as json
 
+from six import iteritems
+
 # NOTE -- this file assumes Ansible is being accessed FROM the cobbler
 # server, so it does not attempt to login with a username and password.
 # this will be addressed in a future version of this script.
@@ -171,7 +173,7 @@ class CobblerInventory(object):
             interfaces = host['interfaces']
             # hostname is often empty for non-static IP hosts
             if dns_name == '':
-                for (iname, ivalue) in interfaces.iteritems():
+                for (iname, ivalue) in iteritems(interfaces):
                     if ivalue['management'] or not ivalue['static']:
                         this_dns_name = ivalue.get('dns_name', None)
                         if this_dns_name is not None and this_dns_name is not "":
@@ -203,7 +205,7 @@ class CobblerInventory(object):
 
             self.cache[dns_name] = host
             if "ks_meta" in host:
-                for key, value in host["ks_meta"].iteritems():
+                for key, value in iteritems(host["ks_meta"]):
                     self.cache[dns_name][key] = value
 
         self.write_to_cache(self.cache, self.cache_path_cache)

contrib/inventory/collins.py
@@ -83,6 +83,8 @@ try:
 except ImportError:
     import simplejson as json
 
+from six import iteritems
+
 
 class CollinsDefaults(object):
     ASSETS_API_ENDPOINT = '%s/api/assets'
@@ -174,7 +176,7 @@ class CollinsInventory(object):
         # the CQL search feature as described here:
         # http://tumblr.github.io/collins/recipes.html
         attributes_query = [ '='.join(attr_pair)
-            for attr_pair in attributes.iteritems() ]
+            for attr_pair in iteritems(attributes) ]
         query_parameters = {
             'details': ['True'],
             'operation': [operation],

contrib/inventory/consul_io.py
@@ -141,6 +141,7 @@ except ImportError as e:
   http://python-consul.readthedocs.org/en/latest/#installation'""")
   sys.exit(1)
 
+from six import iteritems
 
 
 class ConsulInventory(object):
@@ -187,7 +188,7 @@ class ConsulInventory(object):
     an 'available' or 'unavailable' grouping. The suffix for each group can be
     controlled from the config'''
     if self.config.has_config('availability'):
-      for service_name, service in node['Services'].iteritems():
+      for service_name, service in iteritems(node['Services']):
         for node in self.consul_api.health.service(service_name)[1]:
             for check in node['Checks']:
                 if check['ServiceName'] == service_name:

contrib/inventory/nova.py
@@ -26,6 +26,7 @@ import re
 import os
 import ConfigParser
 from novaclient import client as nova_client
+from six import iteritems
 
 try:
     import json
@@ -194,7 +195,7 @@ if (len(sys.argv) == 2 and sys.argv[1] == '--list') or len(sys.argv) == 1:
         push(groups, server.name, access_ip)
 
         # Run through each metadata item and add instance to it
-        for key, value in server.metadata.iteritems():
+        for key, value in iteritems(server.metadata):
             composed_key = to_safe('tag_{0}_{1}'.format(key, value))
             push(groups, composed_key, access_ip)
 

commit 656de7038dbb0e6035b75f9640b094b80ff307f1

Simplify FactCache.copy()
Also fix the bug (missing from six import iteritems) I introduced in
787a82f653c5d4581984ebc76d726b32100377ab.

lib/ansible/plugins/cache/__init__.py
@@ -60,7 +60,7 @@ class FactCache(MutableMapping):
 
     def copy(self):
         """ Return a primitive copy of the keys and values from the cache. """
-        return dict([(k, v) for (k, v) in iteritems(self)])
+        return dict(self)
 
     def keys(self):
         return self._plugin.keys()

test/units/plugins/cache/test_cache.py
@@ -19,7 +19,8 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-from ansible.compat.tests import unittest
+from ansible.compat.tests import unittest, mock
+from ansible.plugins.cache import FactCache
 from ansible.plugins.cache.base import BaseCacheModule
 from ansible.plugins.cache.memory import CacheModule as MemoryCache
 
@@ -40,6 +41,20 @@ except ImportError:
     HAVE_REDIS = False
 else:
     from ansible.plugins.cache.redis import CacheModule as RedisCache
+
+
+class TestFactCache(unittest.TestCase):
+
+    def setUp(self):
+        with mock.patch('ansible.constants.CACHE_PLUGIN', 'memory'):
+            self.cache = FactCache()
+
+    def test_copy(self):
+        self.cache['avocado'] = 'fruit'
+        self.cache['daisy'] = 'flower'
+        a_copy = self.cache.copy()
+        self.assertEqual(type(a_copy), dict)
+        self.assertEqual(a_copy, dict(avocado='fruit', daisy='flower'))
 
 
 class TestAbstractClass(unittest.TestCase):