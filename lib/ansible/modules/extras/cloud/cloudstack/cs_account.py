cd39bdd11d2ca061ade4f410b40d7d89e95abbe4
Rene Moser <mail@renemoser.net>
2015-09-11 19:04:41 +0200

    cloudstack: cs_account re-factor error handling state=present

--- lib/ansible/modules/extras/cloud/cloudstack/cs_account.py
+++ lib/ansible/modules/extras/cloud/cloudstack/cs_account.py
@@ -277,21 +277,16 @@ class AnsibleCloudStackAccount(AnsibleCloudStack):
     def present_account(self):
         missing_params = []
 
-        if not self.module.params.get('email'):
-            missing_params.append('email')
-
-        if not self.module.params.get('username'):
-            missing_params.append('username')
-
-        if not self.module.params.get('password'):
-            missing_params.append('password')
-
-        if not self.module.params.get('first_name'):
-            missing_params.append('first_name')
-
-        if not self.module.params.get('last_name'):
-            missing_params.append('last_name')
-
+        missing_params = []
+        for required_params in [
+            'email',
+            'username',
+            'password',
+            'first_name',
+            'last_name',
+        ]:
+            if not self.module.params.get(required_params):
+                missing_params.append(required_params)
         if missing_params:
             self.module.fail_json(msg="missing required arguments: %s" % ','.join(missing_params))
 
