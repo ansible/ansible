1209be238bf7f0fd7ae11bbcfc6cf2726bd9cacf
James Cammarata <jimi@sngx.net>
2015-09-15 17:30:36 -0400

    Fix galaxy install dep failure
    Also fixes issue where force does not force reinstall of deps
    
    Fixes #10425

--- lib/ansible/galaxy/role.py
+++ lib/ansible/galaxy/role.py
@@ -39,7 +39,7 @@ class GalaxyRole(object):
     ROLE_DIRS = ('defaults','files','handlers','meta','tasks','templates','vars')
 
 
-    def __init__(self, galaxy, name, src=None, version=None, scm=None):
+    def __init__(self, galaxy, name, src=None, version=None, scm=None, role_path=None):
 
         self._metadata = None
         self._install_info = None
@@ -52,7 +52,20 @@ class GalaxyRole(object):
         self.src = src or name
         self.scm = scm
 
-        self.path = (os.path.join(galaxy.roles_path, self.name))
+        if role_path is not None:
+            self.path = role_path
+        else:
+            for path in galaxy.roles_paths:
+                role_path = os.path.join(path, self.name)
+                if os.path.exists(role_path):
+                    self.path = role_path
+                    break
+            else:
+                # use the first path by default
+                self.path = os.path.join(galaxy.roles_paths[0], self.name)
+
+    def __eq__(self, other):
+        return self.name == other.name
 
     def fetch_from_scm_archive(self):
 
