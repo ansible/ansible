57389d55b1722ec4358172fb4c58a61c513148f8
Daniel Donckers <ddonckers@gmail.com>
2015-09-16 10:31:54 -0600

    Removing unnecessary import

--- lib/ansible/utils/template.py
+++ lib/ansible/utils/template.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import print_function
 import sys
 import os
 import re
