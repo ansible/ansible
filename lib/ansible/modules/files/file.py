9942f456cfbeb4f9807c347c4e48ab16d93ec896
varnav <varnavruz@gmail.com>
2015-09-23 08:24:16 +0300

    Fixed confusion in the docs
    As 'path' is actually alias for 'dest', this could be confusing.

--- lib/ansible/modules/files/file.py
+++ lib/ansible/modules/files/file.py
@@ -93,10 +93,10 @@ EXAMPLES = '''
 # change file ownership, group and mode. When specifying mode using octal numbers, first digit should always be 0.
 - file: path=/etc/foo.conf owner=foo group=foo mode=0644
 - file: src=/file/to/link/to dest=/path/to/symlink owner=foo group=foo state=link
-- file: src=/tmp/{{ item.path }} dest={{ item.dest }} state=link
+- file: src=/tmp/{{ item.src }} dest={{ item.dest }} state=link
   with_items:
-    - { path: 'x', dest: 'y' }
-    - { path: 'z', dest: 'k' }
+    - { src: 'x', dest: 'y' }
+    - { src: 'z', dest: 'k' }
 
 # touch a file, using symbolic modes to set the permissions (equivalent to 0644)
 - file: path=/etc/foo.conf state=touch mode="u=rw,g=r,o=r"
