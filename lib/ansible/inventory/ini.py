5d29a2eabdb4d85419e11d4bf20f316d6222d142
Marius Gedminas <marius@gedmin.as>
2015-09-24 12:26:10 +0300

    Python 3: shlex.split() wants unicode
    On Python 2, shlex.split() raises if you pass it a unicode object with
    non-ASCII characters in it.  The Ansible codebase copes by explicitly
    converting the string using to_bytes() before passing it to
    shlex.split().
    
    On Python 3, shlex.split() raises ('bytes' object has no attribute 'read')
    if you pass a bytes object.  Oops.
    
    This commit introduces a new wrapper function, shlex_split, that
    transparently performs the to_bytes/to_unicode conversions only on
    Python 2.
    
    Currently I've only converted one call site (the one that was causing a
    unit test to fail on Python 3).  If this approach is deemed suitable,
    I'll convert them all.

--- lib/ansible/inventory/ini.py
+++ lib/ansible/inventory/ini.py
@@ -20,7 +20,6 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 import ast
-import shlex
 import re
 
 from ansible import constants as C
@@ -30,7 +29,8 @@ from ansible.inventory.group import Group
 from ansible.inventory.expand_hosts import detect_range
 from ansible.inventory.expand_hosts import expand_hostname_range
 from ansible.parsing.utils.addresses import parse_address
-from ansible.utils.unicode import to_unicode, to_bytes
+from ansible.utils.shlex import shlex_split
+from ansible.utils.unicode import to_unicode
 
 class InventoryParser(object):
     """
@@ -231,13 +231,11 @@ class InventoryParser(object):
         # beta:2345 user=admin      # we'll tell shlex
         # gamma sudo=True user=root # to ignore comments
 
-        line = to_bytes(line)
         try:
-            tokens = shlex.split(line, comments=True)
+            tokens = shlex_split(line, comments=True)
         except ValueError as e:
             self._raise_error("Error parsing host definition '%s': %s" % (varstring, e))
 
-        tokens = [ to_unicode(t) for t in tokens]
         (hostnames, port) = self._expand_hostpattern(tokens[0])
         hosts = self._Hosts(hostnames, port)
 
