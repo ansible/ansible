513af578abe58073a059318d95cb2a85884777e1
James Cammarata <jimi@sngx.net>
2015-09-25 03:01:42 -0400

    Make sure formatted strings don't bomb on tuples
    Fixes #12501

--- lib/ansible/plugins/callback/default.py
+++ lib/ansible/plugins/callback/default.py
@@ -81,7 +81,7 @@ class CallbackModule(CallbackBase):
         else:
 
             if (self._display.verbosity > 0 or '_ansible_verbose_always' in result._result) and not '_ansible_verbose_override' in result._result and result._task.action != 'include':
-                msg += " => %s" % self._dump_results(result._result)
+                msg += " => %s" % (self._dump_results(result._result),)
             self._display.display(msg, color=color)
 
         self._handle_warnings(result._result)
@@ -139,7 +139,7 @@ class CallbackModule(CallbackBase):
             msg = "ok: [%s]" % result._host.get_name()
             color = 'green'
 
-        msg += " => (item=%s)" % result._result['item']
+        msg += " => (item=%s)" % (result._result['item'],)
 
         if (self._display.verbosity > 0 or '_ansible_verbose_always' in result._result) and not '_ansible_verbose_override' in result._result and result._task.action != 'include':
             msg += " => %s" % self._dump_results(result._result)
