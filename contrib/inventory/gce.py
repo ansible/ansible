0a58f469e8ba97df54e5869c1c0e47e86db6331f
dohoangkhiem <dohoangkhiem@gmail.com>
2015-09-28 13:24:21 +0700

    Add node private & public ips to gce groups

--- contrib/inventory/gce.py
+++ contrib/inventory/gce.py
@@ -282,6 +282,13 @@ class GceInventory(object):
             if groups.has_key(stat): groups[stat].append(name)
             else: groups[stat] = [name]
 
+            for private_ip in node.private_ips:
+                groups[private_ip] = [name]
+
+            if len(node.public_ips) >= 1:
+                for public_ip in node.public_ips:
+                    groups[public_ip] = [name]
+
         groups["_meta"] = meta
 
         return groups
