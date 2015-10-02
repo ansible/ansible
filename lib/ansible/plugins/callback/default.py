4c9f58a1524454e0b8a8ee4e7a4ab6c31cacbd59
Brian Coca <brian.coca+git@gmail.com>
2015-10-02 09:09:40 -0400

    added no_log censoring to item output should be last commit that fixes #12214

--- lib/ansible/plugins/callback/default.py
+++ lib/ansible/plugins/callback/default.py
@@ -89,8 +89,11 @@ class CallbackModule(CallbackBase):
     def v2_runner_on_skipped(self, result):
         if C.DISPLAY_SKIPPED_HOSTS:
             msg = "skipping: [%s]" % result._host.get_name()
-            if (self._display.verbosity > 0 or '_ansible_verbose_always' in result._result) and not '_ansible_verbose_override' in result._result:
-                msg += " => %s" % self._dump_results(result._result)
+            if result._task.loop and 'results' in result._result:
+                self._process_items(result)
+            else:
+                if (self._display.verbosity > 0 or '_ansible_verbose_always' in result._result) and not '_ansible_verbose_override' in result._result:
+                    msg += " => %s" % self._dump_results(result._result)
             self._display.display(msg, color='cyan')
 
     def v2_runner_on_unreachable(self, result):
