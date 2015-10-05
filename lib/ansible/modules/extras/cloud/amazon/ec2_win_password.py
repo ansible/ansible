7bb6a5aa90193386a6d243849fba48c8612f3edb
Rick Mendes <rmendes@illumina.com>
2015-10-05 10:29:25 -0700

    added missing license

--- lib/ansible/modules/extras/cloud/amazon/ec2_win_password.py
+++ lib/ansible/modules/extras/cloud/amazon/ec2_win_password.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python
+#
+# This is a free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This Ansible library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 DOCUMENTATION = '''
 ---
