82877c9ff45c8c27bc333d5707a97fa5e4e37214
Dreamcat4 <dreamcat4@gmail.com>
2015-10-06 14:03:27 +0100

    fix: Add 'enable:' flag for enabling existing rules which are disabled by default.
    This is a very much needed flag. To turn on/off existing firewall rules. And like the recent fix of the 'Profile' key, the netsh cmd prints 'Enabled' in the textual output. (at least on win10 it does). So again a similar small code added for the necessary exception handling when the difference check happens.
    
    Please merge / push upstream like the other fixes. Many thanks. This is the last fix I have put together for this patch set. So I will raise my PR now.
    
    But if you want to fix more bugs, it seems there may be others. In terms of the control code. Sometimes it will delete a rule under 'force' condition (when found difference) - but instead it is supposed to just modify the existing rule. Some weird behaviour regarding that. The other problem is that ansible does not return the error text printed by 'netsh' cmd verbatim... but it should as that makes debugging these errors a *lot* easier.

--- lib/ansible/modules/extras/windows/win_firewall_rule.py
+++ lib/ansible/modules/extras/windows/win_firewall_rule.py
@@ -25,7 +25,13 @@ author: Timothy Vandenbrande
 short_description: Windows firewall automation
 description:
     - allows you to create/remove/update firewall rules
-options: 
+options:
+    enable:
+        description:
+            - is this firewall rule enabled or disabled
+        default: null
+        required: false
+        choices: ['yes', 'no']
     state:
         description:
             - create/remove/update or powermanage your VM
@@ -108,6 +114,7 @@ EXAMPLES = '''
   action: win_firewall_rule
   args:
       name: smtp
+      enabled: yes
       state: present
       localport: 25
       action: allow
