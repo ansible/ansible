8cf943ed2d6d96d8aca85392498112057fb6aa80
Brian Coca <bcoca@ansible.com>
2015-10-22 08:27:32 -0400

    make callbacks declare needing whitelisting this will allow for custom or v1 callbacks locally loaded to automatically be triggered as they were in 1.x

--- lib/ansible/plugins/callback/tree.py
+++ lib/ansible/plugins/callback/tree.py
@@ -34,6 +34,7 @@ class CallbackModule(CallbackBase):
     CALLBACK_VERSION = 2.0
     CALLBACK_TYPE = 'aggregate'
     CALLBACK_NAME = 'tree'
+    CALLBACK_NEEDS_WHITELIST = True
 
     def __init__(self, display):
         super(CallbackModule, self).__init__(display)
