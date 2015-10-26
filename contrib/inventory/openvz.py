3b1b2cf3b0ce47d2be726226b19d56223c5d096f
Jason O'Donnell <odonnelljp01@gmail.com>
2015-10-26 17:13:00 -0400

    Fixing typo

--- contrib/inventory/openvz.py
+++ contrib/inventory/openvz.py
@@ -24,7 +24,7 @@
 # https://github.com/ansible/ansible/blob/e5ef0eca03cbb6c8950c06dc50d0ca22aa8902f4/plugins/inventory/libvirt_lxc.py
 #
 # Groups are determined by the description field of openvz guests
-# multiple groups can be seperated by commas: webserver,dbserver
+# multiple groups can be separated by commas: webserver,dbserver
 
 from subprocess import Popen,PIPE
 import sys
