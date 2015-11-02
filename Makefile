ansible/ansible

commit ef89a2f917dd6a66a5ee9eb0268941181dc54669

[Makefile] Ensure english dates are generated in debian/changelog placeholder when building deb package.
It

Makefile
@@ -58,7 +58,7 @@ DEBUILD_BIN ?= debuild
 DEBUILD_OPTS = --source-option="-I"
 DPUT_BIN ?= dput
 DPUT_OPTS ?=
-DEB_DATE := $(shell date +"%a, %d %b %Y %T %z")
+DEB_DATE := $(LC_TIME=C shell date +"%a, %d %b %Y %T %z")
 ifeq ($(OFFICIAL),yes)
     DEB_RELEASE = $(RELEASE)ppa
     # Sign OFFICIAL builds using 'DEBSIGN_KEYID'

commit 61513c864ad048982ba82e5d028eb585cd934a7a

Correct broken DEB builds by fixing LC_TIME assignment
Fixes a `make deb` build failure introduced by
ef89a2f917dd6a66a5ee9eb0268941181dc54669

Makefile
@@ -58,7 +58,7 @@ DEBUILD_BIN ?= debuild
 DEBUILD_OPTS = --source-option="-I"
 DPUT_BIN ?= dput
 DPUT_OPTS ?=
-DEB_DATE := $(LC_TIME=C shell date +"%a, %d %b %Y %T %z")
+DEB_DATE := $(shell LC_TIME=C date +"%a, %d %b %Y %T %z")
 ifeq ($(OFFICIAL),yes)
     DEB_RELEASE = $(RELEASE)ppa
     # Sign OFFICIAL builds using 'DEBSIGN_KEYID'