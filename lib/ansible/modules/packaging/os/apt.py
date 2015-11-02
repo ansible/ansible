7ae4bdadfc6e1dc6d153123c9d5980a8722e8c7a
Toshio Kuratomi <toshio@fedoraproject.org>
2015-11-02 13:03:18 -0800

    Fixes for bcoca's review of #1916

--- lib/ansible/modules/packaging/os/apt.py
+++ lib/ansible/modules/packaging/os/apt.py
@@ -64,7 +64,7 @@ options:
     description:
       - Corresponds to the C(--no-install-recommends) option for I(apt). C(yes) installs recommended packages.  C(no) does not install recommended packages. By default, Ansible will use the same defaults as the operating system. Suggested packages are never installed.
     required: false
-    default: yes
+    default: null
     choices: [ "yes", "no" ]
   force:
     description:
@@ -339,7 +339,7 @@ def expand_pkgspec_from_fnmatches(m, pkgspec, cache):
     return new_pkgspec
 
 def install(m, pkgspec, cache, upgrade=False, default_release=None,
-            install_recommends='default', force=False,
+            install_recommends=None, force=False,
             dpkg_options=expand_dpkg_options(DPKG_OPTIONS),
             build_dep=False):
     pkg_list = []
@@ -385,10 +385,12 @@ def install(m, pkgspec, cache, upgrade=False, default_release=None,
 
         if default_release:
             cmd += " -t '%s'" % (default_release,)
-        if install_recommends == 'no':
+
+        if install_recommends is False:
             cmd += " -o APT::Install-Recommends=no"
-        elif install_recommends == 'yes':
+        elif install_recommends is True:
             cmd += " -o APT::Install-Recommends=yes"
+        # install_recommends is None uses the OS default
 
         rc, out, err = m.run_command(cmd)
         if rc:
@@ -549,7 +551,7 @@ def main():
             package = dict(default=None, aliases=['pkg', 'name'], type='list'),
             deb = dict(default=None),
             default_release = dict(default=None, aliases=['default-release']),
-            install_recommends = dict(default='default', aliases=['install-recommends'], choices=['default', 'yes', 'no']),
+            install_recommends = dict(default=None, aliases=['install-recommends'], type='bool'),
             force = dict(default='no', type='bool'),
             upgrade = dict(choices=['no', 'yes', 'safe', 'full', 'dist']),
             dpkg_options = dict(default=DPKG_OPTIONS)
