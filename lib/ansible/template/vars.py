1027abd5963182a73cec3b87f035572c0f59475a
James Cammarata <jimi@sngx.net>
2015-11-05 16:42:12 -0500

    Cleaning up bugs related to perf improvement changes

--- lib/ansible/template/vars.py
+++ lib/ansible/template/vars.py
@@ -83,10 +83,7 @@ class AnsibleJ2Vars:
         if isinstance(variable, dict) and varname == "vars" or isinstance(variable, HostVars):
             return variable
         else:
-            if self._templar._contains_vars(variable):
-                return self._templar.template(variable)
-            else:
-                return variable
+            return self._templar.template(variable)
 
     def add_locals(self, locals):
         '''
