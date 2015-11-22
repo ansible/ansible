367b88a2ab19d28e42ffffeb4702badebc09dbcb
Brian Coca <brian.coca+git@gmail.com>
2015-11-21 16:07:12 -0800

    removed json import

--- system/puppet.py
+++ system/puppet.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>.
 
-import json
 import os
 import pipes
 import stat
