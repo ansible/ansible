bef2b7585743fd1106d01fc594a1e0c7e43e15e3
Jay Rogers <jay@521dimensions.com>
2015-11-25 18:46:17 -0600

    Update in Amazon IAM Policy Documentation
    There were typos in the documentation that made the examples seem misleading of what was being demonstrated. This update fixes that.

--- lib/ansible/modules/cloud/amazon/iam_policy.py
+++ lib/ansible/modules/cloud/amazon/iam_policy.py
@@ -62,9 +62,9 @@ extends_documentation_fragment: aws
 '''
 
 EXAMPLES = '''
-# Create and policy with the name of 'Admin' to the group 'administrators'
+# Create a policy with the name of 'Admin' to the group 'administrators'
 tasks:
-- name: Create two new IAM users with API keys
+- name: Assign a policy called Admin to the administrators group
   iam_policy:
     iam_type: group
     iam_name: administrators
@@ -85,7 +85,7 @@ task:
      - Luigi
   register: new_groups
 
-- name:
+- name: Apply READ-ONLY policy to new groups that have been recently created
   iam_policy:
     iam_type: group
     iam_name: "{{ item.created_group.group_name }}"
