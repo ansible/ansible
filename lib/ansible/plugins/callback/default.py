2bc69c5286d047c488d232fdc2a42619e61175f1
Brian Coca <bcoca@ansible.com>
2015-12-02 20:36:21 -0800

    added playbook and options info to callbacks
    will display on certain verbosity levels, both playbook/file info
    and non empty options with which it's running.
    avoid errors when not using CLI classes

--- lib/ansible/plugins/callback/default.py
+++ lib/ansible/plugins/callback/default.py
@@ -230,3 +230,16 @@ class CallbackModule(CallbackBase):
 
         self._display.display("", screen_only=True)
 
+    def v2_playbook_on_start(self, playbook):
+        if self._display.verbosity > 1:
+            from os.path import basename
+            self._display.banner("PLAYBOOK: %s" % basename(playbook._file_name))
+
+        if self._display.verbosity > 3:
+            if self._options is not None:
+                for option in dir(self._options):
+                    if option.startswith('_') or option in ['read_file', 'ensure_value', 'read_module']:
+                        continue
+                    val =  getattr(self._options,option)
+                    if val:
+                        self._display.vvvv('%s: %s' % (option,val))
