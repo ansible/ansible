3974b13a5a2fee827f36e9981faf6fdb048c8e13
Luca Berruti <nadirio@gmail.com>
2015-12-05 19:43:02 +0100

    Make no_target_syslog consistent.
    no_target_syslog = False --> do log on target

--- lib/ansible/plugins/action/async.py
+++ lib/ansible/plugins/action/async.py
@@ -48,7 +48,7 @@ class ActionModule(ActionBase):
         env_string = self._compute_environment_string()
 
         module_args = self._task.args.copy()
-        if self._play_context.no_log or not C.DEFAULT_NO_TARGET_SYSLOG:
+        if self._play_context.no_log or C.DEFAULT_NO_TARGET_SYSLOG:
             module_args['_ansible_no_log'] = True
 
         # configure, upload, and chmod the target module
