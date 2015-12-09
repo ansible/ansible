e571bf94d4fb0c05b07cc2096d0b58b7d75b8f26
Brian Coca <brian.coca+git@gmail.com>
2015-12-09 08:22:58 -0800

    Revert "avoid persistent containers in attribute defaults"
    This reverts commit 87969868d42cd8aba1c65c8207d059d73407373b.
    found better way to do it

--- lib/ansible/playbook/role/metadata.py
+++ lib/ansible/playbook/role/metadata.py
@@ -40,7 +40,7 @@ class RoleMetadata(Base):
     '''
 
     _allow_duplicates = FieldAttribute(isa='bool', default=False)
-    _dependencies     = FieldAttribute(isa='list', default='_ansible_container')
+    _dependencies     = FieldAttribute(isa='list', default=[])
     _galaxy_info      = FieldAttribute(isa='GalaxyInfo')
 
     def __init__(self, owner=None):
