57f86cb750930fcdcdab48c5f543bdd657e12cc5
Brian Coca <bcoca@ansible.com>
2015-12-09 08:23:45 -0800

    removed default from hosts to make it requried
    prevents writing a play w/o a hosts entry which would default to
    all/empty

--- lib/ansible/playbook/play.py
+++ lib/ansible/playbook/play.py
@@ -64,7 +64,7 @@ class Play(Base, Taggable, Become):
 
     # Connection
     _gather_facts        = FieldAttribute(isa='bool', default=None, always_post_validate=True)
-    _hosts               = FieldAttribute(isa='list', default=[], required=True, listof=string_types, always_post_validate=True)
+    _hosts               = FieldAttribute(isa='list', required=True, listof=string_types, always_post_validate=True)
     _name                = FieldAttribute(isa='string', default='', always_post_validate=True)
 
     # Variable Attributes
