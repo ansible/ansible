876fc4bc6dbc67d3ef4ae8bab65186209415c9b0
pascalheraud <pascal.heraud@laroueverte.com>
2015-12-10 09:11:03 +0100

    Changed licence and main()

--- lib/ansible/modules/extras/cloud/ovh/ovh_ip_loadbalancing_backend.py
+++ lib/ansible/modules/extras/cloud/ovh/ovh_ip_loadbalancing_backend.py
@@ -1,5 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/python
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 import sys
 try:
     import ovh
@@ -281,4 +294,5 @@ def main():
     # We should never reach here
     module.fail_json(msg='Internal ovh_ip_loadbalancing_backend module error')
 
-main()
+if __name__ == '__main__':
+    main()
