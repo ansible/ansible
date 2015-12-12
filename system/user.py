f3ed8192412e8c9b9528a04bfef3cad1bad9f62f
Brian Coca <brian.coca+git@gmail.com>
2015-12-12 17:29:27 -0500

    added missing version_added

--- system/user.py
+++ system/user.py
@@ -53,6 +53,7 @@ options:
         required: false
         description:
             - Optionally sets the seuser type (user_u) on selinux enabled systems.
+        version_added: "2.1"
     group:
         required: false
         description:
