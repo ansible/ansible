457f86f61a3bef95b562dbf91b523c563bff2f63
Toshio Kuratomi <toshio@fedoraproject.org>
2015-12-14 08:50:37 -0800

    Minor: Correct type pyhton => python

--- test/units/plugins/cache/test_cache.py
+++ test/units/plugins/cache/test_cache.py
@@ -110,6 +110,6 @@ class TestAbstractClass(unittest.TestCase):
     def test_memory_cachemodule(self):
         self.assertIsInstance(MemoryCache(), MemoryCache)
 
-    @unittest.skipUnless(HAVE_REDIS, 'Redis pyhton module not installed')
+    @unittest.skipUnless(HAVE_REDIS, 'Redis python module not installed')
     def test_redis_cachemodule(self):
         self.assertIsInstance(RedisCache(), RedisCache)
