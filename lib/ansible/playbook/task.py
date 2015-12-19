0ab9d3cbe2a1d9be469ffc56744e6e2f27ca84f7
James Cammarata <jimi@sngx.net>
2015-12-19 12:49:06 -0500

    Fixing bugs in conditional testing with until and some integration runner tweaks

--- lib/ansible/playbook/task.py
+++ lib/ansible/playbook/task.py
@@ -82,7 +82,7 @@ class Task(Base, Conditional, Taggable, Become):
     _poll                 = FieldAttribute(isa='int')
     _register             = FieldAttribute(isa='string')
     _retries              = FieldAttribute(isa='int', default=3)
-    _until                = FieldAttribute(isa='list')
+    _until                = FieldAttribute(isa='string')
 
     def __init__(self, block=None, role=None, task_include=None):
         ''' constructors a task, without the Task.load classmethod, it will be pretty blank '''
