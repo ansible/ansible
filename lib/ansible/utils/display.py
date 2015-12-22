a7f44adc8a32f222ab1783db185a4a71ef2afdae
Kari Palenius <kari.palenius@gmail.com>
2015-12-22 11:22:18 +0200

    display: implement windows terminal width handling

--- lib/ansible/utils/display.py
+++ lib/ansible/utils/display.py
@@ -18,7 +18,6 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import fcntl
 import textwrap
 import os
 import random
@@ -29,7 +28,6 @@ import locale
 import logging
 import getpass
 from struct import unpack, pack
-from termios import TIOCGWINSZ
 from multiprocessing import Lock
 
 from ansible import constants as C
@@ -279,10 +277,41 @@ class Display:
         if encoding in ('mac-roman',):
             encoding = 'utf-8'
         return encoding
-
-    def _set_column_width(self):
-        if os.isatty(0):
-            tty_size = unpack('HHHH', fcntl.ioctl(0, TIOCGWINSZ, pack('HHHH', 0, 0, 0, 0)))[1]
+    
+    def _get_column_width_posix(self):
+        import fcntl
+        from termios import TIOCGWINSZ
+        return unpack('HHHH', fcntl.ioctl(0, TIOCGWINSZ, pack('HHHH', 0, 0, 0, 0)))[1]
+    
+    def _get_column_width_windows(self):
+        res=None
+        try:
+            from ctypes import windll, create_string_buffer
+    
+            # stdin handle is -10
+            # stdout handle is -11
+            # stderr handle is -12
+    
+            h = windll.kernel32.GetStdHandle(-12)
+            csbi = create_string_buffer(22)
+            res = windll.kernel32.GetConsoleScreenBufferInfo(h, csbi)
+        except:
+            return 0
+        if res:
+            import struct
+            (bufx, bufy, curx, cury, wattr,
+            left, top, right, bottom, maxx, maxy) = struct.unpack("hhhhHhhhhhh", csbi.raw)
+            return right - left + 1
         else:
-            tty_size = 0
+            return 0
+    
+    def _set_column_width(self):
+        tty_size = 0
+        import platform
+        
+        if platform.system() == 'Windows':
+             tty_size = self._get_column_width_windows()
+        else:
+            if os.isatty(0):
+                tty_size = self._get_column_width_posix()
         self.columns = max(79, tty_size)
