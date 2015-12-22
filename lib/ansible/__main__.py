dfc4c3cb725d62838798b829bd8b6c43eed094c0
Kari Palenius <kari.palenius@gmail.com>
2015-12-22 13:07:29 +0200

    Add missing __futures__ and __metaclasses__.

--- lib/ansible/__main__.py
+++ lib/ansible/__main__.py
@@ -1,2 +1,5 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 from .cli import adhoc
 adhoc.entry_point()
--- lib/ansible/cli/common.py
+++ lib/ansible/cli/common.py
@@ -1,7 +1,5 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 
 import os
 import sys
