b90f2ce7366396554814ec3d2aba81dbfb954880
Kari Palenius <kari.palenius@gmail.com>
2015-12-22 14:42:39 +0200

    Use python -m ansible in testing.

--- lib/ansible/__main__.py
+++ lib/ansible/__main__.py
@@ -1,5 +1,35 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-from .cli import adhoc
-adhoc.entry_point()
+import sys
+
+##
+## Handle module invocation (i.e. python -m)
+##
+
+if len(sys.argv) > 1:
+	# Read the subcommand
+	sub = sys.argv[1]
+	
+	# Shift the args so that command line parsing
+	# should work identically.
+	sys.argv = sys.argv[1:]
+	
+	if sub == 'playbook':
+		from .cli import playbook
+		playbook.entry_point()
+	elif sub == 'pull':
+		from .cli import pull
+		pull.entry_point()
+	elif sub == 'doc':
+		from .cli import doc
+		doc.entry_point()
+	elif sub == 'galaxy':
+		from .cli import galaxy
+		galaxy.entry_point()
+	elif sub == 'vault':
+		from .cli import vault
+		vault.entry_point()
+else:
+	from .cli import adhoc
+	adhoc.entry_point()
