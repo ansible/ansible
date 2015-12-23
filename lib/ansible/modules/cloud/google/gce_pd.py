353db36515a8dc25e641568116dccf66b4962cfe
Vic Iglesias <viglesias@google.com>
2015-12-23 11:37:12 -0800

    Update GCE PEM deprecation text and examples

--- lib/ansible/modules/cloud/google/gce_pd.py
+++ lib/ansible/modules/cloud/google/gce_pd.py
@@ -97,7 +97,8 @@ options:
   pem_file:
     version_added: "1.6"
     description:
-      - (deprecated) path to the pem file associated with the service account email
+      - path to the pem file associated with the service account email
+        This option is deprecated. Use 'credentials_file'.
     required: false
     default: null
     aliases: []
@@ -126,7 +127,7 @@ options:
 
 requirements:
     - "python >= 2.6"
-    - "apache-libcloud >= 0.17.0"
+    - "apache-libcloud >= 0.13.3, >= 0.17.0 if using JSON credentials"
 author: "Eric Johnson (@erjohnso) <erjohnso@google.com>"
 '''
 
