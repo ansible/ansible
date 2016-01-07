9bba1b66c569ebb3fe26a3e219d5aca29fdf9cd1
Stepan Stipl <stepan@stipl.net>
2016-01-07 18:06:35 +0000

    Fix: route53_facts hosted_zone_id boto error
    Boto is expecting parameter called "Id", not "HostedZoneId".
    See
    http://boto3.readthedocs.org/en/latest/reference/services/route53.html#Route53.Client.get_hosted_zone
    
    Fixes ansible/ansible-modules-extras/#1465

--- lib/ansible/modules/extras/cloud/amazon/route53_facts.py
+++ lib/ansible/modules/extras/cloud/amazon/route53_facts.py
@@ -177,7 +177,7 @@ def get_hosted_zone(client, module):
     params = dict()
 
     if module.params.get('hosted_zone_id'):
-        params['HostedZoneId'] = module.params.get('hosted_zone_id')
+        params['Id'] = module.params.get('hosted_zone_id')
     else:
         module.fail_json(msg="Hosted Zone Id is required")
 
