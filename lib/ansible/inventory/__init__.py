f49ea24ad374401fb944c0ed64f242f0f8a9bfc0
Will Thames <will@thames.id.au>
2016-01-13 23:25:41 +1000

    Include all inventory group_vars and host_vars
    Ensure results from all basedirs are included, and
    not just overwritten by the results from the last
    one

--- lib/ansible/inventory/__init__.py
+++ lib/ansible/inventory/__init__.py
@@ -736,11 +736,11 @@ class Inventory(object):
             if group and host is None:
                 # load vars in dir/group_vars/name_of_group
                 base_path = os.path.realpath(os.path.join(basedir, "group_vars/%s" % group.name))
-                results = self._variable_manager.add_group_vars_file(base_path, self._loader)
+                results.update(self._variable_manager.add_group_vars_file(base_path, self._loader))
             elif host and group is None:
                 # same for hostvars in dir/host_vars/name_of_host
                 base_path = os.path.realpath(os.path.join(basedir, "host_vars/%s" % host.name))
-                results = self._variable_manager.add_host_vars_file(base_path, self._loader)
+                results.update(self._variable_manager.add_host_vars_file(base_path, self._loader))
 
         # all done, results is a dictionary of variables for this particular host.
         return results
