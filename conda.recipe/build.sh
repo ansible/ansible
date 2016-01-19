c24cca736927511d3f3377daa27bb3ca987b70ac
Ian Stokes-Rees <ijstokes@continuum.io>
2016-01-19 15:34:06 -0500

    added conda recipe for v2 latest

---
+++ conda.recipe/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$PYTHON setup.py install
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
