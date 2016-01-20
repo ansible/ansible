391420d88f7c8dd691833cd0edb1fe4c31b9c191
James Cammarata <jimi@sngx.net>
2016-01-20 18:23:30 -0500

    Re-add cache clearing call to Inventory init
    This prevents a bug where the existing cache outside of the class
    is not cleared when creating a new Inventory object. This only really
    affects people using the API directly right now, but wanted to fix it
    to prevent weird errors from popping up.

--- lib/ansible/inventory/__init__.py
+++ lib/ansible/inventory/__init__.py
@@ -78,6 +78,10 @@ class Inventory(object):
         self._restriction = None
         self._subset = None
 
+        # clear the cache here, which is only useful if more than
+        # one Inventory objects are created when using the API directly
+        self.clear_pattern_cache()
+
         self.parse_inventory(host_list)
 
     def serialize(self):
