8ac8f4c2abf556472c603769e13c12091a22bf0d
Brian Coca <brian.coca+git@gmail.com>
2016-02-02 13:13:02 -0500

    make executable setting connection dependant
    winrm shoudl not use executable, rest should?
    fixes #14233

--- lib/ansible/plugins/connection/__init__.py
+++ lib/ansible/plugins/connection/__init__.py
@@ -60,6 +60,7 @@ class ConnectionBase(with_metaclass(ABCMeta, object)):
     # as discovered by the specified file extension.  An empty string as the
     # language means any language.
     module_implementation_preferences = ('',)
+    allow_executable = True
 
     def __init__(self, play_context, new_stdin, *args, **kwargs):
         # All these hasattrs allow subclasses to override these parameters
