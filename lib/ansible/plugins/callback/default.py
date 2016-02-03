c769846ba9e497c82073a957bb4702d3c2278766
Tobias Wolf <towolf@gmail.com>
2016-02-03 16:18:02 +0100

    Do not print empty diffs with _display.display()

--- lib/ansible/plugins/callback/default.py
+++ lib/ansible/plugins/callback/default.py
@@ -137,10 +137,14 @@ class CallbackModule(CallbackBase):
     def v2_on_file_diff(self, result):
         if result._task.loop and 'results' in result._result:
             for res in result._result['results']:
-                if 'diff' in res:
-                    self._display.display(self._get_diff(res['diff']))
+                if 'diff' in res and res['diff']:
+                    diff = self._get_diff(res['diff'])
+                    if diff:
+                        self._display.display(diff)
         elif 'diff' in result._result and result._result['diff']:
-            self._display.display(self._get_diff(result._result['diff']))
+            diff = self._get_diff(result._result['diff'])
+            if diff:
+                self._display.display(diff)
 
     def v2_playbook_item_on_ok(self, result):
 
