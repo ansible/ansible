0bcbcb20b01b40c7e525890e9127088e43d2e842
Toshio Kuratomi <toshio@fedoraproject.org>
2016-02-05 08:59:50 -0800

    Fix --diff to respect no_log task parameter.

--- lib/ansible/runner/action_plugins/copy.py
+++ lib/ansible/runner/action_plugins/copy.py
@@ -366,6 +366,8 @@ class ActionModule(object):
             diff['after_header'] = source
             diff['after'] = src.read()
 
+        if self.runner.no_log and 'after' in diff:
+            diff["after"] = " [[ Diff output has been hidden because 'no_log: true' was specified for this result ]]"
         return diff
 
     def _remove_tempfile_if_content_defined(self, content, content_tempfile):
