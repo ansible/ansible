e8c3dea84e1ebec01eaf42a75f131bfa970d756d
Peter Sprygada <psprygada@ansible.com>
2016-02-09 14:34:17 -0500

    update nxos_config doc strings and return values
    This commit unifys the return values for the nxos_config module to always
    return updates and responses

--- lib/ansible/modules/network/nxos/nxos_config.py
+++ lib/ansible/modules/network/nxos/nxos_config.py
@@ -142,19 +142,17 @@ EXAMPLES = """
 """
 
 RETURN = """
-
-lines:
+updates:
   description: The set of commands that will be pushed to the remote device
   returned: always
   type: list
   sample: ['...', '...']
 
-response:
+responses:
   description: The set of responses from issuing the commands on the device
-  returned: always
+  retured: when not check_mode
   type: list
   sample: ['...', '...']
-
 """
 import re
 import itertools
@@ -258,10 +256,10 @@ def main():
 
         if not module.check_mode:
             response = module.configure(candidate)
-            result['response'] = response
+            result['responses'] = response
         result['changed'] = True
 
-    result['lines'] = candidate
+    result['updates'] = candidate
     return module.exit_json(**result)
 
 from ansible.module_utils.basic import *
