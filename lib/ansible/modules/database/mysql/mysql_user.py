59fca32b86ddc490690851eedc6b2b4c42531b48
Toshio Kuratomi <a.badger@gmail.com>
2016-02-11 13:04:00 -0800

    password needs to be type str.
    In 1.9 this was the default.  In 2.0.0 and 2.0.1 this doesn't get set.
    2.1.0 will fix the default but we should fix this now.

--- lib/ansible/modules/database/mysql/mysql_user.py
+++ lib/ansible/modules/database/mysql/mysql_user.py
@@ -476,7 +476,7 @@ def main():
             login_port=dict(default=3306, type='int'),
             login_unix_socket=dict(default=None),
             user=dict(required=True, aliases=['name']),
-            password=dict(default=None, no_log=True),
+            password=dict(default=None, no_log=True, type='str'),
             encrypted=dict(default=False, type='bool'),
             host=dict(default="localhost"),
             host_all=dict(type="bool", default="no"),
