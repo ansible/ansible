3b624e9dab571adbd50e7c1a8b25ab224de2f0ad
Javier Palacios <javiplx@gmail.com>
2016-02-16 00:00:31 +0100

    Use the same env var for galaxy than ansible 2.x

--- lib/ansible/constants.py
+++ lib/ansible/constants.py
@@ -135,7 +135,7 @@ DEFAULT_SU_USER           = get_config(p, DEFAULTS, 'su_user', 'ANSIBLE_SU_USER'
 DEFAULT_ASK_SU_PASS       = get_config(p, DEFAULTS, 'ask_su_pass', 'ANSIBLE_ASK_SU_PASS', False, boolean=True)
 DEFAULT_GATHERING         = get_config(p, DEFAULTS, 'gathering', 'ANSIBLE_GATHERING', 'implicit').lower()
 DEFAULT_LOG_PATH          = shell_expand_path(get_config(p, DEFAULTS, 'log_path',           'ANSIBLE_LOG_PATH', ''))
-DEFAULT_API_SERVER        = get_config(p, DEFAULTS, 'api_server', 'ANSIBLE_API_SERVER', 'https://galaxy.ansible.com')
+DEFAULT_API_SERVER        = get_config(p, DEFAULTS, 'api_server', 'ANSIBLE_GALAXY_SERVER't , 'https://galaxy.ansible.com')
 
 # selinux
 DEFAULT_SELINUX_SPECIAL_FS = get_config(p, 'selinux', 'special_context_filesystems', None, 'fuse, nfs, vboxsf', islist=True)
