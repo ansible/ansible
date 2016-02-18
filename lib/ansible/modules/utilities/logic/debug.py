d0f22ba97b5e9d5cba21b2cf0a9c396276f68b6a
Brian Coca <bcoca@ansible.com>
2016-02-18 07:01:39 -0800

    documented new verbosity option

--- lib/ansible/modules/utilities/logic/debug.py
+++ lib/ansible/modules/utilities/logic/debug.py
@@ -38,6 +38,12 @@ options:
   var:
     description:
       - A variable name to debug.  Mutually exclusive with the 'msg' option.
+  verbosity:
+    description:
+      - A number that controls when the debug is run, if you set to 3 it will only run debug when -vvv or above
+    required: False
+    default: 0
+    version_added: "2.1"
 author: 
     - "Dag Wieers (@dagwieers)"
     - "Michael DeHaan"
@@ -53,8 +59,8 @@ EXAMPLES = '''
 - shell: /usr/bin/uptime
   register: result
 
-- debug: var=result
+- debug: var=result verbosity=2
 
 - name: Display all variables/facts known for a host
-  debug: var=hostvars[inventory_hostname]
+  debug: var=hostvars[inventory_hostname] verbosity=4
 '''
