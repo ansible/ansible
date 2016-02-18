fb442206ca3cc1e9734dd231af005e2af4fee478
Dag Wieers <dag@wieers.com>
2016-02-18 18:04:28 +0100

    Template role_params to avoid merging dict and unicode
    This fixes #12915
    
    Since combine_vars() is being run directly on role_params, we have to avoid merge_hash() to complain about merging a dict with a string (jinja template).

--- lib/ansible/runner/__init__.py
+++ lib/ansible/runner/__init__.py
@@ -647,7 +647,8 @@ class Runner(object):
         # vars_files which had host-specific templating done)
         inject = utils.combine_vars(inject, self.vars_cache.get(host, {}))
         # role parameters next
-        inject = utils.combine_vars(inject, self.role_params)
+        role_params = template.template(self.basedir, self.role_params, inject)
+        inject = utils.combine_vars(inject, role_params)
         # and finally -e vars are the highest priority
         inject = utils.combine_vars(inject, self.extra_vars)
         # and then special vars
