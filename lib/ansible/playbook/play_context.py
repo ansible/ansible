ansible/ansible

commit 078ebb0dec17675dbb0aaf9d656d2c0042988d46

Don't re-inject become* variables as it causes problems when templating is involved
Prior to 75b6f61, we strictly limited variables we re-injected. After that
patch however, we re-injected everything which causes problems under certain
circumstances. For now, we'll continue to filter out some properties of
PlayContext for re-injection.

Fixes #14352

lib/ansible/playbook/play_context.py
@@ -513,8 +513,14 @@ class PlayContext(Base):
         '''
 
         for prop, var_list in MAGIC_VARIABLE_MAPPING.items():
-            var_val = getattr(self, prop, None)
-            if var_val is not None:
+            try:
+                if 'become' in prop:
+                    continue
+
+                var_val = getattr(self, prop)
                 for var_opt in var_list:
                     if var_opt not in variables:
                         variables[var_opt] = var_val
+            except AttributeError:
+                continue
+

commit da02aba173d49eabc890a1e3fbd2765ad69e9de3

Don't inject PlayContext properties as variables if they're None
Fixes bug introduced in 078ebb0

lib/ansible/playbook/play_context.py
@@ -519,7 +519,7 @@ class PlayContext(Base):
 
                 var_val = getattr(self, prop)
                 for var_opt in var_list:
-                    if var_opt not in variables:
+                    if var_opt not in variables and var_val is not None:
                         variables[var_opt] = var_val
             except AttributeError:
                 continue