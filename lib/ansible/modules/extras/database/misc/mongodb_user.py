467d9a80906b09ea0dcf36be76fcfa7998d23d3d
Guillaume Dufour <guillaume.duff@gmail.com>
2016-02-26 15:08:04 +0100

    avoid problem with old mongo version without roles

--- lib/ansible/modules/extras/database/misc/mongodb_user.py
+++ lib/ansible/modules/extras/database/misc/mongodb_user.py
@@ -278,7 +278,7 @@ def main():
         uinfo = user_find(client, user, db_name)
         if update_password != 'always' and uinfo:
             password = None
-            if list(map((lambda x: x['role']), uinfo['roles'])) == roles:
+            if 'roles' in uinfo and list(map((lambda x: x['role']), uinfo['roles'])) == roles:
                 module.exit_json(changed=False, user=user)
 
         if module.check_mode:
