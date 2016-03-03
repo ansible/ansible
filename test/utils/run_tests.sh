57e4ecd429887546d72f77866813bce8aab94ec9
James Cammarata <jimi@sngx.net>
2016-03-03 15:23:44 -0500

    Adding docker container capabilities to Travis

---
+++ test/utils/run_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+
+if [ "${TARGET}" = "sanity" ]; then
+    ./test/code-smell/replace-urlopen.sh .
+    ./test/code-smell/use-compat-six.sh lib
+    ./test/code-smell/boilerplate.sh
+    ./test/code-smell/required-and-default-attributes.sh
+    if test x"$TOXENV" != x'py24' ; then tox ; fi
+    if test x"$TOXENV" = x'py24' ; then python2.4 -V && python2.4 -m compileall -fq -x 'module_utils/(a10|rax|openstack|ec2|gce).py' lib/ansible/module_utils ; fi
+else
+    docker build -t ansible_test/${TARGET} test/utils/docker/${TARGET}
+    docker run -d --volume="${PWD}:/root/ansible" ${TARGET_OPTIONS} ansible_test/${TARGET} > /tmp/cid_${TARGET}
+    docker exec -ti $(cat /tmp/cid_${TARGET}) /bin/sh -c 'cd /root/ansible; . hacking/env-setup; make tests && (cd test/integration; make)'
+    docker kill $(cat /tmp/cid_${TARGET})
+fi
