d6546a75134c0498e6c46372d20a297f64356a20
Brian Coca <brian.coca+git@gmail.com>
2016-03-04 13:12:35 -0500

    make sure group_names is always sorted
    this makes it consistent with previous ansilbe versions and other
    paths that create the group_names variable

--- lib/ansible/vars/__init__.py
+++ lib/ansible/vars/__init__.py
@@ -362,7 +362,7 @@ class VariableManager:
         variables['playbook_dir'] = loader.get_basedir()
 
         if host:
-            variables['group_names'] = [group.name for group in host.get_groups() if group.name != 'all']
+            variables['group_names'] = sorted([group.name for group in host.get_groups() if group.name != 'all'])
 
             if self._inventory is not None:
                 variables['groups']  = dict()
