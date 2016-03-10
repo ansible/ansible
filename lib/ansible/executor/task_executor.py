1ef981e5b543fbd4e26da6b9904d094481a650a2
James Cammarata <jimi@sngx.net>
2016-03-10 11:02:26 -0500

    Don't always insert failed in the results (again)

--- lib/ansible/executor/task_executor.py
+++ lib/ansible/executor/task_executor.py
@@ -465,7 +465,6 @@ class TaskExecutor:
                     result['failed_when_result'] = result['failed'] = failed_when_result
                 else:
                     failed_when_result = False
-                    result['failed'] = result.get('failed', False)
                 return failed_when_result
 
             if 'ansible_facts' in result:
