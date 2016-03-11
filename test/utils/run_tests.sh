b3b37bca5916e5b921eb1ed194680a1652f90e7b
James Cammarata <jimi@sngx.net>
2016-03-10 21:52:11 -0500

    Moving docker tests to pre-compiled images

--- test/utils/run_tests.sh
+++ test/utils/run_tests.sh
@@ -9,12 +9,11 @@ if [ "${TARGET}" = "sanity" ]; then
     if test x"$TOXENV" = x'py24' ; then python2.4 -V && python2.4 -m compileall -fq -x 'module_utils/(a10|rax|openstack|ec2|gce).py' lib/ansible/module_utils ; fi
 else
     export C_NAME="testAbull_$$_$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)"
-    docker build -t ansible_test/${TARGET} test/utils/docker/${TARGET}
-    docker run -d --volume="${PWD}:/root/ansible"  --name "${C_NAME}" ${TARGET_OPTIONS} ansible_test/${TARGET} > /tmp/cid_${TARGET}
+    docker run -d --volume="${PWD}:/root/ansible"  --name "${C_NAME}" ${TARGET_OPTIONS} ansible/ansible:${TARGET} > /tmp/cid_${TARGET}
     docker exec -ti $(cat /tmp/cid_${TARGET}) /bin/sh -c "export TEST_FLAGS='${TEST_FLAGS}'; cd /root/ansible; . hacking/env-setup; (cd test/integration; LC_ALL=en_US.utf-8 make)"
     docker kill $(cat /tmp/cid_${TARGET})
 
-    if [ "X${TESTS_KEEP_CONTAINER}" == "X" ]; then
+    if [ "X${TESTS_KEEP_CONTAINER}" = "X" ]; then
         docker rm "${C_NAME}"
     fi
 fi
