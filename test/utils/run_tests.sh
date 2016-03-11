d51a2ef45c927af2c551188bdf514b61d243fe4d
James Cammarata <jimi@sngx.net>
2016-03-11 11:25:28 -0500

    Reorganizing tox stuff and making py3-specific requirements

--- test/utils/run_tests.sh
+++ test/utils/run_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -xe
+#!/bin/sh -x
 
 if [ "${TARGET}" = "sanity" ]; then
     ./test/code-smell/replace-urlopen.sh .
@@ -8,6 +8,7 @@ if [ "${TARGET}" = "sanity" ]; then
     if test x"$TOXENV" != x'py24' ; then tox ; fi
     if test x"$TOXENV" = x'py24' ; then python2.4 -V && python2.4 -m compileall -fq -x 'module_utils/(a10|rax|openstack|ec2|gce).py' lib/ansible/module_utils ; fi
 else
+    set -e
     export C_NAME="testAbull_$$_$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)"
     docker run -d --volume="${PWD}:/root/ansible"  --name "${C_NAME}" ${TARGET_OPTIONS} ansible/ansible:${TARGET} > /tmp/cid_${TARGET}
     docker exec -ti $(cat /tmp/cid_${TARGET}) /bin/sh -c "export TEST_FLAGS='${TEST_FLAGS}'; cd /root/ansible; . hacking/env-setup; (cd test/integration; LC_ALL=en_US.utf-8 make)"
