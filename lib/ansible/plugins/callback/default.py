25bca5e59fb27d3770803f8b9a5042676cea0d49
James Cammarata <jimi@sngx.net>
2016-03-17 14:19:28 -0400

    Make sure tuples returned by _get_item() are handled correctly in formatting
    Fixes #14800

--- lib/ansible/plugins/callback/default.py
+++ lib/ansible/plugins/callback/default.py
@@ -163,7 +163,7 @@ class CallbackModule(CallbackBase):
         else:
             msg += ": [%s]" % result._host.get_name()
 
-        msg += " => (item=%s)" % (self._get_item(result._result))
+        msg += " => (item=%s)" % (self._get_item(result._result),)
 
         if (self._display.verbosity > 0 or '_ansible_verbose_always' in result._result) and not '_ansible_verbose_override' in result._result:
             msg += " => %s" % self._dump_results(result._result)
