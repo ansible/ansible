723139900e6fb4c92e52ad7737403feb8b39369d
Aaron Brady <aaron@insom.me.uk>
2016-03-21 22:17:58 +0000

    Make the template lookup match the template action
    The `template` action sets variables (such as `ansible_managed`) which are not
    set for the equivalent `template` lookup. This change remedies this and is
    covered by matching integration tests.

--- lib/ansible/plugins/lookup/template.py
+++ lib/ansible/plugins/lookup/template.py
@@ -17,12 +17,15 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import datetime
 import os
+import pwd
+import time
 
 from ansible import constants as C
 from ansible.errors import AnsibleError
 from ansible.plugins.lookup import LookupBase
-from ansible.utils.unicode import to_unicode
+from ansible.utils.unicode import to_bytes, to_unicode
 
 try:
     from __main__ import display
@@ -49,13 +52,42 @@ class LookupModule(LookupBase):
                 with open(lookupfile, 'r') as f:
                     template_data = to_unicode(f.read())
 
+                    try:
+                        template_uid = pwd.getpwuid(os.stat(lookupfile).st_uid).pw_name
+                    except:
+                        template_uid = os.stat(lookupfile).st_uid
+
+                    temp_vars = self._templar._available_variables.copy()
+
+                    temp_vars['template_host']     = os.uname()[1]
+                    temp_vars['template_path']     = lookupfile
+                    temp_vars['template_mtime']    = datetime.datetime.fromtimestamp(os.path.getmtime(lookupfile))
+                    temp_vars['template_uid']      = template_uid
+                    temp_vars['template_fullpath'] = os.path.abspath(lookupfile)
+                    temp_vars['template_run_date'] = datetime.datetime.now()
+
+                    managed_default = C.DEFAULT_MANAGED_STR
+                    managed_str = managed_default.format(
+                        host = temp_vars['template_host'],
+                        uid  = temp_vars['template_uid'],
+                        file = to_bytes(temp_vars['template_path'])
+                    )
+                    temp_vars['ansible_managed'] = time.strftime(
+                        managed_str,
+                        time.localtime(os.path.getmtime(lookupfile))
+                    )
+
                     searchpath = [self._loader._basedir, os.path.dirname(lookupfile)]
                     if 'role_path' in variables:
                         searchpath.insert(1, C.DEFAULT_ROLES_PATH)
                         searchpath.insert(1, variables['role_path'])
 
                     self._templar.environment.loader.searchpath = searchpath
+
+                    old_vars = self._templar._available_variables
+                    self._templar.set_available_variables(temp_vars)
                     res = self._templar.template(template_data, preserve_trailing_newlines=True,convert_data=convert_data_p)
+                    self._templar.set_available_variables(old_vars)
                     ret.append(res)
             else:
                 raise AnsibleError("the template file %s could not be found for the lookup" % term)
