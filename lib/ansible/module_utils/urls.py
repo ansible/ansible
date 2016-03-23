9313b1867e2ec38f7e1d9d94874fd914bbd39348
job-so <github@grosjeanne.name>
2016-03-23 10:30:25 +0100

    HTTPError - adding response to info dictionnary

--- lib/ansible/module_utils/urls.py
+++ lib/ansible/module_utils/urls.py
@@ -849,7 +849,7 @@ def fetch_url(module, url, data=None, headers=None, method=None,
     except (ConnectionError, ValueError), e:
         module.fail_json(msg=str(e))
     except urllib2.HTTPError, e:
-        info.update(dict(msg=str(e), status=e.code, response=e, **e.info()))
+        info.update(dict(msg=str(e), status=e.code, response=e.read(), **e.info()))
     except urllib2.URLError, e:
         code = int(getattr(e, 'code', -1))
         info.update(dict(msg="Request failed: %s" % str(e), status=code))
