a6d52ce098314567351c27a992c18c78804e0e05
Matt Martz <matt@sivel.net>
2016-03-22 20:41:37 -0500

    Ensure that any command in run_tests.sh that fails results in immediate failure

--- test/utils/run_tests.sh
+++ test/utils/run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -x
 
+set -e
+
 if [ "${TARGET}" = "sanity" ]; then
     ./test/code-smell/replace-urlopen.sh .
     ./test/code-smell/use-compat-six.sh lib
@@ -8,7 +10,6 @@ if [ "${TARGET}" = "sanity" ]; then
     if test x"$TOXENV" != x'py24' ; then tox ; fi
     if test x"$TOXENV" = x'py24' ; then python2.4 -V && python2.4 -m compileall -fq -x 'module_utils/(a10|rax|openstack|ec2|gce).py' lib/ansible/module_utils ; fi
 else
-    set -e
     export C_NAME="testAbull_$$_$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)"
     docker pull ansible/ansible:${TARGET}
     docker run -d --volume="${PWD}:/root/ansible:Z"  --name "${C_NAME}" ${TARGET_OPTIONS} ansible/ansible:${TARGET} > /tmp/cid_${TARGET}
