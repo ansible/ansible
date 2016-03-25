32ede3cffd1f8a7d8fca8e6cd0a3e4e8affb1dfc
Toshio Kuratomi <a.badger@gmail.com>
2016-03-24 20:07:52 -0700

    Reverse boilerplate order because from __future__ must be the first code in the file

--- lib/ansible/plugins/test/mathstuff.py
+++ lib/ansible/plugins/test/mathstuff.py
@@ -15,8 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
-__metaclass__ = type
 from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 
 def issubset(a, b):
     return set(a) <= set(b)
