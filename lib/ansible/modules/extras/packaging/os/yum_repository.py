7552c54c0dc0ef3c9f1eab9fe9b85608f41deb6e
Michael Scherer <misc@zarb.org>
2016-03-30 21:49:58 +0200

    Use type='path' for reposdir, since that's a path

--- lib/ansible/modules/extras/packaging/os/yum_repository.py
+++ lib/ansible/modules/extras/packaging/os/yum_repository.py
@@ -662,7 +662,7 @@ def main():
             proxy_password=dict(no_log=True),
             proxy_username=dict(),
             repo_gpgcheck=dict(type='bool'),
-            reposdir=dict(default='/etc/yum.repos.d'),
+            reposdir=dict(default='/etc/yum.repos.d', type='path'),
             retries=dict(),
             s3_enabled=dict(type='bool'),
             skip_if_unavailable=dict(type='bool'),
