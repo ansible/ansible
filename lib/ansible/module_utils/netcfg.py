e6cf0afa8a2d66a156b5fcc557aa6481fdc8d563
Peter Sprygada <psprygada@ansible.com>
2016-04-04 08:10:22 -0400

    bugfix in netcfg to handle multilevel commands
    this fixes a bug where netcfg would not properly find a statement that was
    more than one level deep

--- lib/ansible/module_utils/netcfg.py
+++ lib/ansible/module_utils/netcfg.py
@@ -174,7 +174,8 @@ class NetworkConfig(object):
     def get_object(self, path):
         for item in self.items:
             if item.text == path[-1]:
-                if item.parents == path[:-1]:
+                parents = [p.text for p in item.parents]
+                if parents == path[:-1]:
                     return item
 
     def get_children(self, path):
