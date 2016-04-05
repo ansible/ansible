ansible/ansible

commit c84cd7a9c5a25a7c2702240ca8b86747e8248172

fix unconfigured plugin path case
We tried to expand paths for None- ensure we're not passing [None] in that case, but [] instead.

lib/ansible/plugins/__init__.py
@@ -64,8 +64,10 @@ class PluginLoader:
         self.subdir             = subdir
         self.aliases            = aliases
 
-        if not isinstance(config, list):
+        if config and not isinstance(config, list):
             config = [config]
+        else:
+            config = []
 
         self.config = config
 

commit 6c0d10ca3f41e8644f977c3121f94e7d9c26372c

Bugfix: proper plugin configuration was reset
Before this patch, if config was ['/some/path'] then it would enter the
else block and config would be set to [].

The regression this patch fixes was introduced by c84cd7a9.

lib/ansible/plugins/__init__.py
@@ -66,7 +66,7 @@ class PluginLoader:
 
         if config and not isinstance(config, list):
             config = [config]
-        else:
+        elif not config:
             config = []
 
         self.config = config

test/units/plugins/test_plugins.py
@@ -75,3 +75,7 @@ class TestErrors(unittest.TestCase):
         #with patch('glob.glob', mock_glob):
         #    pass
 
+    def test_plugin__config(self):
+        config = ['/one', '/two']
+        pl = PluginLoader('test', '', config, 'test_plugin')
+        self.assertEqual(pl.config, config)