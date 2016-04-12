da4d4239260d130da08e29d4af6ea9696f24b5c6
Bjørnar Ness <bjornar.ness@gmail.com>
2016-04-12 14:16:18 +0200

    enable mysql connection via unix socket

--- lib/ansible/module_utils/mysql.py
+++ lib/ansible/module_utils/mysql.py
@@ -29,10 +29,8 @@
 
 
 
-def mysql_connect(module, login_user=None, login_password=None, config_file='', ssl_cert=None, ssl_key=None, ssl_ca=None, db=None, cursor_class=None):
-    config = {
-        'host': module.params['login_host']
-    }
+def mysql_connect(module, login_user=None, login_password=None, config_file='', ssl_cert=None, ssl_key=None, ssl_ca=None, db=None, cursor_class=None, connect_timeout=30):
+    config = {}
 
     if ssl_ca is not None or ssl_key is not None or ssl_cert is not None:
         config['ssl'] = {}
@@ -40,6 +38,7 @@ def mysql_connect(module, login_user=None, login_password=None, config_file='', 
     if module.params['login_unix_socket']:
         config['unix_socket'] = module.params['login_unix_socket']
     else:
+        config['host'] = module.params['login_host']
         config['port'] = module.params['login_port']
 
     if os.path.exists(config_file):
