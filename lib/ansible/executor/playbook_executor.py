3e21408dc2da22a462b2db84073ccad133b82611
Vincent Roy <vincentroy8@gmail.com>
2016-04-12 19:04:44 -0300

    Backward compatibility execution failures with 1.9

--- lib/ansible/executor/playbook_executor.py
+++ lib/ansible/executor/playbook_executor.py
@@ -151,9 +151,8 @@ class PlaybookExecutor:
                             elif len(batch) == failed_hosts_count:
                                 break
 
-                            # clear the failed hosts dictionaires in the TQM for the next batch
+                            # save the unreachable hosts from this batch
                             self._unreachable_hosts.update(self._tqm._unreachable_hosts)
-                            self._tqm.clear_failed_hosts()
 
                         # if the last result wasn't zero or 3 (some hosts were unreachable),
                         # break out of the serial batch loop
