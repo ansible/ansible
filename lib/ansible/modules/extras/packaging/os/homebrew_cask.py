5e8946b5b3dad8e8288a4cc437501121dbbe4851
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2016-04-13 16:03:26 +0200

    Sprinkle some LANG/LC_* where command output is parsed (#2019)

--- lib/ansible/modules/extras/packaging/os/homebrew_cask.py
+++ lib/ansible/modules/extras/packaging/os/homebrew_cask.py
@@ -481,6 +481,9 @@ def main():
         ),
         supports_check_mode=True,
     )
+
+    module.run_command_environ_update = dict(LANG='C', LC_ALL='C', LC_MESSAGES='C', LC_CTYPE='C')
+
     p = module.params
 
     if p['name']:
