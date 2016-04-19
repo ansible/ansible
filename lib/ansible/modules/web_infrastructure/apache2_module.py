ac1d80892cdf01ee8adaf56af8093821acf95191
Pål Nilsen <paal.nilsen@gmail.com>
2016-04-19 23:44:08 +0200

    Add optional force parameter for apache2_module (#3251)
    * Add optional force parameter for apache2_module
    
    Fixes #2499
    
    * Add documentation for the force parameter
    
    * Clarify in description that -f(orce) is a Debian parameter

--- lib/ansible/modules/web_infrastructure/apache2_module.py
+++ lib/ansible/modules/web_infrastructure/apache2_module.py
@@ -29,6 +29,12 @@ options:
      description:
         - name of the module to enable/disable
      required: true
+   force:
+     description:
+        - force disabling of default modules and override Debian warnings
+     required: false
+     choices: ['yes', 'no']
+     default: no
    state:
      description:
         - indicate the desired state of the resource
@@ -50,9 +56,13 @@ import re
 
 def _disable_module(module):
     name = module.params['name']
+    force = module.params['force']
     a2dismod_binary = module.get_bin_path("a2dismod")
     if a2dismod_binary is None:
         module.fail_json(msg="a2dismod not found.  Perhaps this system does not use a2dismod to manage apache")
+
+    if force:
+        a2dismod_binary += ' -f'
 
     result, stdout, stderr = module.run_command("%s %s" % (a2dismod_binary, name))
 
@@ -82,6 +92,7 @@ def main():
     module = AnsibleModule(
         argument_spec = dict(
             name  = dict(required=True),
+            force = dict(required=False, type='bool', default=False),
             state = dict(default='present', choices=['absent', 'present'])
         ),
     )
