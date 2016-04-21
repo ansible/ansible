79714b0f6c4d76027e669323cc2e9f89daa9d9e5
Nathan Davison <ndavison85@gmail.com>
2016-04-22 09:59:21 +1000

    Added process stime and name to the output, and added a check on the process stime before killing.

--- lib/ansible/modules/system/portenforce.py
+++ lib/ansible/modules/system/portenforce.py
@@ -66,6 +66,10 @@ killed:
       description: The protocol, TCP or UDP, of the listening connection.
       type: string
       sample: 'tcp'
+    stime:
+      description: The start time of the killed process.
+      type: string
+      sample: Thu Apr 21 17:30:16 2016
 '''
 
 import re
@@ -85,11 +89,12 @@ def netStatParse(raw):
             elif 'udp' in splitted[0]:
                 proto = 'udp'
                 pidstr = splitted[5]
-            pids = re.search('([0-9]+)/', pidstr)
+            pids = re.search('([0-9]+)/(.*)', pidstr)
             if conns and pids:
                 port = conns.group(1)
                 pid = pids.group(1)
-                result = dict(pid=int(pid), port=int(port), proto=proto)
+                name = pids.group(2)
+                result = dict(pid=int(pid), port=int(port), proto=proto, name=name)
                 if result not in results:
                     results.append(result)
             elif not pids:
@@ -100,9 +105,18 @@ def applyWhitelist(portspids, whitelist=list()):
     kill_pids = list()
     for p in portspids:
         if int(p['port']) not in whitelist and str(p['port']) not in whitelist:
-            if dict(pid=p['pid'], port=p['port'], proto=p['proto']) not in kill_pids:
-                kill_pids.append(dict(pid=p['pid'], port=p['port'], proto=p['proto']))
+            if dict(pid=p['pid'], port=p['port'], proto=p['proto'], name=p['name'], stime=p['stime']) not in kill_pids:
+                kill_pids.append(dict(pid=p['pid'], port=p['port'], proto=p['proto'], name=p['name'], stime=p['stime']))
     return kill_pids
+
+def getPidSTime(pid):
+    p1 = Popen(['ps', '-o', 'lstart', '-p', str(pid)], stdout=PIPE, stderr=PIPE)
+    p2 = Popen(['grep', '^[^ ]'], stdin=p1.stdout, stdout=PIPE, stderr=PIPE)
+    p1.stdout.close()
+    p3 = Popen(['tr', '-d', '"\n"'], stdin=p2.stdout, stdout=PIPE, stderr=PIPE)
+    p2.stdout.close()
+    stime = p3.communicate()[0]
+    return stime
 
 def main():
 
@@ -123,34 +137,42 @@ def main():
         p1 = Popen(['netstat', '-plnt'], stdout=PIPE, stderr=PIPE)
         output_tcp = p1.communicate()[0]
         kill_tcp = netStatParse(output_tcp)
+        for i, p in enumerate(kill_tcp):
+            p['stime'] = getPidSTime(p['pid']) 
+            kill_tcp[i] = p
 
         # which UDP ports are listening for connections?
         p1 = Popen(['netstat', '-plnu'], stdout=PIPE, stderr=PIPE)
         output_udp = p1.communicate()[0]
         kill_udp = netStatParse(output_udp)
+        for i, p in enumerate(kill_udp):
+            p['stime'] = getPidSTime(p['pid']) 
+            kill_udp[i] = p
+
     except EnvironmentError, err:
         module.fail_json(msg=str(err))
 
     # gather details of the pids to kill
     kill_pids_tcp = applyWhitelist(kill_tcp, module.params['whitelist_tcp'])
     kill_pids_udp = applyWhitelist(kill_udp, module.params['whitelist_udp'])
-    killed = list(kill_pids_tcp)
-    killed.extend(x for x in kill_pids_udp if x not in kill_pids_tcp)
+    kill_all = list(kill_pids_tcp)
+    kill_all.extend(x for x in kill_pids_udp if x not in kill_pids_tcp)
 
     # get just the pids to avoid duplicates across protocols
-    pids = list()
-    for p in killed:
-        if p['pid'] not in pids:
-            pids.append(p['pid'])
+    kill_unique = list()
+    for p in kill_all:
+        if p['pid'] not in kill_unique:
+            kill_unique.append(p)
 
     # kill! kill!
     if not module.check_mode:
-        for pid in pids:
-            p1 = Popen(['kill', str(pid)], stdout=PIPE)
+        for p in kill_unique:
+            if p['stime'] == getPidSTime(p['pid']):
+                p1 = Popen(['kill', str(p['pid'])], stdout=PIPE)
 
-    if killed:
+    if kill_all:
         result['changed'] = True
-        result['killed'] = killed
+        result['killed'] = kill_all
 
     module.exit_json(**result)
 
