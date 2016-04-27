e66c6ad3a1561311c0e730d9a424f50b67afbc35
Brian Coca <bcoca@ansible.com>
2016-04-27 14:36:56 -0400

    updated version added

--- lib/ansible/modules/extras/system/git_config.py
+++ lib/ansible/modules/extras/system/git_config.py
@@ -25,7 +25,7 @@ module: git_config
 author:
   - "Matthew Gamble"
   - "Marius Gedminas"
-version_added: 2.1
+version_added: 2.2
 requirements: ['git']
 short_description: Read and write git configuration
 description:
