fda13deb0839451c541a38a3f0e5f4fc75cfcc80
Toshio Kuratomi <toshio@fedoraproject.org>
2016-04-29 20:33:06 -0700

    Switch blockinfile to using the latest best way to get ansible version

--- lib/ansible/modules/extras/files/blockinfile.py
+++ lib/ansible/modules/extras/files/blockinfile.py
@@ -150,8 +150,6 @@ import re
 import os
 import tempfile
 
-from ansible import __version__
-
 
 def write_changes(module, contents, dest):
 
@@ -246,7 +244,7 @@ def main():
     marker1 = re.sub(r'{mark}', 'END', marker)
     if present and block:
         # Escape seqeuences like '\n' need to be handled in Ansible 1.x
-        if __version__.startswith('1.'):
+        if module.constants['ANSIBLE_VERSION'].startswith('1.'):
             block = re.sub('', block, '')
         blocklines = [marker0] + block.splitlines() + [marker1]
     else:
