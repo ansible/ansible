11621f9ede97e6979c1e89a8b8957f0048664671
Jordan Cohen <hypergig@gmail.com>
2016-05-02 07:30:15 -0400

    doc update to demostrate message template vars

--- lib/ansible/modules/extras/monitoring/datadog_monitor.py
+++ lib/ansible/modules/extras/monitoring/datadog_monitor.py
@@ -107,7 +107,7 @@ datadog_monitor:
   name: "Test monitor"
   state: "present"
   query: "datadog.agent.up".over("host:host1").last(2).count_by_status()"
-  message: "Some message."
+  message: "Host [[host.name]] with IP [[host.ip]] is failing to report to datadog."
   api_key: "9775a026f1ca7d1c6c5af9d94d9595a4"
   app_key: "87ce4a24b5553d2e482ea8a8500e71b8ad4554ff"
 
