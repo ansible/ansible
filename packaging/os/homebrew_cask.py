806ebef383ae418c47da5349cda15aa1447213bc
Rene Moser <mail@renemoser.net>
2016-05-04 08:29:46 +0200

    homebrew_cask: fix doc version_added for install_options
    See #2086

--- packaging/os/homebrew_cask.py
+++ packaging/os/homebrew_cask.py
@@ -44,7 +44,7 @@ options:
             - options flags to install a package
         required: false
         default: null
-        version_added: "2.1"
+        version_added: "2.2"
 '''
 EXAMPLES = '''
 - homebrew_cask: name=alfred state=present
