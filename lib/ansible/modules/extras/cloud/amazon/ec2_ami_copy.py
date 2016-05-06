d4301f2ac1a71cb5cb8462090390220377ba5e30
Matt Martz <matt@sivel.net>
2016-05-06 14:00:32 -0500

    Fix HAS_BOTO fail_json call for ec2_ami_copy

--- lib/ansible/modules/extras/cloud/amazon/ec2_ami_copy.py
+++ lib/ansible/modules/extras/cloud/amazon/ec2_ami_copy.py
@@ -91,9 +91,6 @@ try:
     HAS_BOTO = True
 except ImportError:
     HAS_BOTO = False
-    
-if not HAS_BOTO:
-    module.fail_json(msg='boto required for this module')
 
 def copy_image(module, ec2):
     """
@@ -182,6 +179,9 @@ def main():
         tags=dict(type='dict')))
 
     module = AnsibleModule(argument_spec=argument_spec)
+
+    if not HAS_BOTO:
+        module.fail_json(msg='boto required for this module')
 
     try:
         ec2 = ec2_connect(module)
