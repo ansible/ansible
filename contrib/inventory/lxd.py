37c3e7ad2d8b33f2e59327558a81758e3a6eecbe
Andrew Clarke <andrew@glydetechnology.com>
2016-05-13 15:22:24 +0100

    Updated formatting, indenting and python3 support
    Updated to work on python3

--- contrib/inventory/lxd.py
+++ contrib/inventory/lxd.py
@@ -22,12 +22,15 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
-import ConfigParser
 import os
 from subprocess import Popen,PIPE
 import distutils.spawn
 import sys
 import json
+try:
+    import configparser
+except:
+    from six.moves import configparser
 
 # Set up defaults
 resource = 'local:'
@@ -35,55 +38,55 @@ result = {}
 hosts = []
 
 # Read the settings from the lxd.ini file
-config = ConfigParser.SafeConfigParser()
+config = configparser.SafeConfigParser()
 config.read(os.path.dirname(os.path.realpath(__file__)) + '/lxd.ini')
 
 # Resource
 if config.has_option('lxd', 'resource'):
-	resource = config.get('lxd', 'resource')
+    resource = config.get('lxd', 'resource')
 
 # Ensure executable exists
 if distutils.spawn.find_executable('lxc'):
-	
-	# Set up containers result array
+
+    # Set up containers result array
     result['containers'] = {}
-    
+
     # Run the command and load json result
     pipe = Popen(['lxc', 'list', resource, '--format', 'json'], stdout=PIPE, universal_newlines=True)
     lxdjson = json.load(pipe.stdout)
 
-	# Iterate the json lxd output
+    # Iterate the json lxd output
     for item in lxdjson:
 
-		# Check state and network
-		if 'state' in item and item['state'] is not None and 'network' in item['state']:
-			network = item['state']['network']
+        # Check state and network
+        if 'state' in item and item['state'] is not None and 'network' in item['state']:
+            network = item['state']['network']
 
-			# Check for eth0 and addresses
-			if 'eth0' in network and 'addresses' in network['eth0']:
-				addresses = network['eth0']['addresses']
+            # Check for eth0 and addresses
+            if 'eth0' in network and 'addresses' in network['eth0']:
+                addresses = network['eth0']['addresses']
 
-				# Iterate addresses
-				for address in addresses:
+                # Iterate addresses
+                for address in addresses:
 
-					# Only return inet family addresses
-					if 'family' in address and address['family'] == 'inet':
-						if 'address' in address:
-							ip = address['address']
-							hosts.append(ip)
+                    # Only return inet family addresses
+                    if 'family' in address and address['family'] == 'inet':
+                        if 'address' in address:
+                            ip = address['address']
+                            hosts.append(ip)
 
-	# Set the other containers result values
+    # Set the other containers result values
     result['containers']['hosts'] = hosts
     result['containers']['vars'] = {}
     result['containers']['vars']['ansible_connection'] = 'local'
 
 # Process arguments
 if len(sys.argv) == 2 and sys.argv[1] == '--list':
-    print json.dumps(result)
+    print(json.dumps(result))
 elif len(sys.argv) == 3 and sys.argv[1] == '--host':
     if sys.argv[2] == 'localhost':
-        print json.dumps({'ansible_connection': 'local'})
+        print(json.dumps({'ansible_connection': 'local'}))
     else:
-        print json.dumps({'ansible_connection': 'local'})
+        print(json.dumps({'ansible_connection': 'local'}))
 else:
-    print "Need an argument, either --list or --host <host>"
+    print("Need an argument, either --list or --host <host>")
