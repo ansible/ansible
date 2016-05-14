0527adedbf413746f61bda2743c775934d421a50
Michael Scherer <misc@redhat.com>
2016-05-14 12:08:39 +0200

    Use the proper type for 'dest' argument

--- lib/ansible/modules/source_control/hg.py
+++ lib/ansible/modules/source_control/hg.py
@@ -211,7 +211,7 @@ def main():
     module = AnsibleModule(
         argument_spec = dict(
             repo = dict(required=True, aliases=['name']),
-            dest = dict(required=True),
+            dest = dict(required=True, type='path'),
             revision = dict(default=None, aliases=['version']),
             force = dict(default='no', type='bool'),
             purge = dict(default='no', type='bool'),
@@ -220,7 +220,7 @@ def main():
         ),
     )
     repo = module.params['repo']
-    dest = os.path.expanduser(module.params['dest'])
+    dest = module.params['dest']
     revision = module.params['revision']
     force = module.params['force']
     purge = module.params['purge']
