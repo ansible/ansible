c4a500bc28135fc8321a83410e098f4a396b8e7e
Michael Scherer <misc@zarb.org>
2016-05-16 14:12:08 +0200

    Port module_utils/ec2.py to python 3 syntax
    Since boto is considered as python 2.6 only (cf
    https://github.com/ansible/ansible/blob/devel/test/utils/run_tests.sh#L15 ),
    no need to use the 2.4 compatible syntax.

--- lib/ansible/module_utils/ec2.py
+++ lib/ansible/module_utils/ec2.py
@@ -226,13 +226,13 @@ def ec2_connect(module):
     if region:
         try:
             ec2 = connect_to_aws(boto.ec2, region, **boto_params)
-        except (boto.exception.NoAuthHandlerFound, AnsibleAWSError), e:
+        except (boto.exception.NoAuthHandlerFound, AnsibleAWSError) as e:
             module.fail_json(msg=str(e))
     # Otherwise, no region so we fallback to the old connection method
     elif ec2_url:
         try:
             ec2 = boto.connect_ec2_endpoint(ec2_url, **boto_params)
-        except (boto.exception.NoAuthHandlerFound, AnsibleAWSError), e:
+        except (boto.exception.NoAuthHandlerFound, AnsibleAWSError) as e:
             module.fail_json(msg=str(e))
     else:
         module.fail_json(msg="Either region or ec2_url must be specified")
