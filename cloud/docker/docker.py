10ee35ff268889009848e8fe19dc20e28e3e2f52
chouseknecht <chouseknecht@ansible.com>
2016-05-18 14:35:20 -0400

    Make the new bits a NOTE

--- cloud/docker/docker.py
+++ cloud/docker/docker.py
@@ -28,8 +28,8 @@ version_added: "1.4"
 short_description: manage docker containers
 description:
   - This is the original Ansible module for managing the docker container life cycle.
-  - Additional and newer modules are available. For the latest on orchestrating containers with Ansible
-    visit our Getting Started with Docker guide at U(https://github.com/ansible/ansible/blob/devel/docsite/rst/guide_docker.rst).
+  - NOTE: Additional and newer modules are available. For the latest on orchestrating containers with Ansible
+    visit our Getting Started with Docker Guide at U(https://github.com/ansible/ansible/blob/devel/docsite/rst/guide_docker.rst).
 options:
   count:
     description:
