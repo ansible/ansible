a1cd75ac905652a06a40d54c9a62e655f5627208
John Barker <john@johnrbarker.com>
2016-05-20 09:46:48 +0100

    Resolve merge conflicts

--- test/utils/run_tests.sh
+++ test/utils/run_tests.sh
@@ -15,13 +15,19 @@ if [ "${TARGET}" = "sanity" ]; then
     if test x"$TOXENV" = x'py24' ; then python2.4 -V && python2.4 -m compileall -fq -x 'module_utils/(a10|rax|openstack|ec2|gce|docker_common|azure_rm_common|vca|vmware).py' lib/ansible/module_utils ; fi
 else
     if [ ! -e /tmp/cid_httptester ]; then
-        docker pull ansible/ansible:httptester
-        docker run -d --name=httptester ansible/ansible:httptester > /tmp/cid_httptester
+        docker pull sivel/httptester
+        docker run -d --name=httptester sivel/httptester > /tmp/cid_httptester
+
+    # Should we test "make deb" or "make rpm"?
+    if [ "X${MAKETARGETS:-""}" -ne "X" ]; then
+        RELEASE_BUILD_CMD="make ${MAKETARGETS}"
+    else
+        RELEASE_BUILD_CMD="true"
     fi
     export C_NAME="testAbull_$$_$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)"
     docker pull ansible/ansible:${TARGET}
-    docker run -d --volume="${PWD}:/root/ansible:Z" $LINKS --name "${C_NAME}" --env HTTPTESTER=1 ${TARGET_OPTIONS:=''} ansible/ansible:${TARGET} > /tmp/cid_${TARGET}
-    docker exec -ti $(cat /tmp/cid_${TARGET}) /bin/sh -c "export TEST_FLAGS='${TEST_FLAGS:-''}'; cd /root/ansible; . hacking/env-setup; (cd test/integration; LC_ALL=en_US.utf-8 make ${MAKE_TARGET:-})"
+    docker run -d --volume="${PWD}:/root/ansible:Z" $LINKS --name "${C_NAME}" ${TARGET_OPTIONS:=''} ansible/ansible:${TARGET} > /tmp/cid_${TARGET}
+    docker exec -ti $(cat /tmp/cid_${TARGET}) /bin/sh -c "export TEST_FLAGS='${TEST_FLAGS:-''}'; cd /root/ansible; . hacking/env-setup; ${RELEASE_BUILD_CMD} && (cd test/integration; LC_ALL=en_US.utf-8 make ${MAKE_TARGET:-})"
     docker kill $(cat /tmp/cid_${TARGET})
 
     if [ "X${TESTS_KEEP_CONTAINER:-""}" = "X" ]; then
