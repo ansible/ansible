6cc4aa49d03ff64cb32b3488aa0e987d812a42cd
Hagai Kariti <hkariti@gmail.com>
2016-05-23 10:29:26 +0300

    bigpanda: Remove docs using complex args (#2275)
    As they're going to be deprecated, don't recommend using them.

--- lib/ansible/modules/extras/monitoring/bigpanda.py
+++ lib/ansible/modules/extras/monitoring/bigpanda.py
@@ -83,19 +83,11 @@ EXAMPLES = '''
 ...
 - bigpanda: component=myapp version=1.3 token={{ bigpanda_token }} state=finished
 
-or using a deployment object:
-- bigpanda: component=myapp version=1.3 token={{ bigpanda_token }} state=started
-  register: deployment
-
-- bigpanda: state=finished
-  args: deployment
-
-If outside servers aren't reachable from your machine, use local_action and pass the hostname:
-- local_action: bigpanda component=myapp version=1.3 hosts={{ansible_hostname}} token={{ bigpanda_token }} state=started
+If outside servers aren't reachable from your machine, use local_action and override hosts:
+- local_action: bigpanda component=myapp version=1.3 token={{ bigpanda_token }} hosts={{ansible_hostname}} state=started
   register: deployment
 ...
-- local_action: bigpanda state=finished
-  args: deployment
+- local_action: bigpanda component=deployment.component version=deployment.version token=deployment.token state=finished
 '''
 
 # ===========================================
