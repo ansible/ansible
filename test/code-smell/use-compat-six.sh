650bfdce8f5ea0d83450e628db622d7ff34cb0a7
Toshio Kuratomi <toshio@fedoraproject.org>
2016-05-24 13:48:41 -0700

    Update grep for six to not falsely trigger when six is only a substring of a different library

--- test/code-smell/use-compat-six.sh
+++ test/code-smell/use-compat-six.sh
@@ -3,7 +3,7 @@
 # Do we want to check dynamic inventory, bin, etc?
 BASEDIR=${1-"lib"}
 
-SIX_USERS=$(find "$BASEDIR" -name '*.py' -exec grep -H six \{\} \;|grep import |grep -v ansible.compat)
+SIX_USERS=$(find "$BASEDIR" -name '*.py' -exec grep -wH six \{\} \;|grep import |grep -v ansible.compat)
 if test -n "$SIX_USERS" ; then
   printf "$SIX_USERS"
   exit 1
