28a48ac93b4a14d8c88b3f6a22701275675043f1
Toshio Kuratomi <a.badger@gmail.com>
2016-05-25 11:48:16 -0700

    Fix python3 syntax compilation

--- lib/ansible/modules/extras/cloud/amazon/ec2_vpc_peer.py
+++ lib/ansible/modules/extras/cloud/amazon/ec2_vpc_peer.py
@@ -343,8 +343,8 @@ def main():
     state = module.params.get('state').lower()
     try:
         region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
-        client = boto3_conn(module, conn_type='client', resource='ec2', region=region, endpoint=ec2_url, **aws_connect_kwargs)       
-    except botocore.exceptions.NoCredentialsError, e:
+        client = boto3_conn(module, conn_type='client', resource='ec2', region=region, endpoint=ec2_url, **aws_connect_kwargs)
+    except botocore.exceptions.NoCredentialsError as e:
         module.fail_json(msg="Can't authorize connection - "+str(e))
 
     if state == 'present':
