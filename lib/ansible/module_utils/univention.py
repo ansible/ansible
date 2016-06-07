342d09af9929fae51d0001d85d23712f65bc5051
David Vogt <david.vogt@adfinis-sygroup.ch>
2016-06-07 17:20:46 +0200

    Code Review with @2-B, slight API changes and refactoring.
    * Added module documentation overview, describing the provided functions
    * Moved module-global objects into getter functions, so that we don't
      need to import possibly-unavailable univention modules at the module level.
    * Renamed some exports for improved consistency:
    
      - module_name()    -> module_by_name()
      - orig_ldap        -> ldap_module()
      - ldap             -> uldap()
    
    Note that this introduces slight API changes from the outside. Instead of
    directly accessing module properties, you now have module functions with the
    same name. Examples:
    
       - ansible.module_utils.univention.position_base_dn()
       - ansible.module_utils.univention.config_registry()
       - ansible.module_utils.univention.base_dn()
       - ansible.module_utils.univention.config()

--- lib/ansible/module_utils/univention.py
+++ lib/ansible/module_utils/univention.py
@@ -20,58 +20,123 @@
 #
 
 
-"""univention corporate server access module"""
+"""Univention Corporate Server (UCS) access module.
+
+Provides the following functions for working with an UCS server.
+
+  - ldap_search(filter, base=None, attr=None)
+    Search the LDAP via Univention's LDAP wrapper (ULDAP)
+
+  - config_registry()
+    Return the UCR registry object
+
+  - base_dn()
+    Return the configured Base DN according to the UCR
+
+  - uldap()
+    Return a handle to the ULDAP LDAP wrapper
+
+  - umc_module_for_add(module, container_dn, superordinate=None)
+    Return a UMC module for creating a new object of the given type
+
+  - umc_module_for_edit(module, object_dn, superordinate=None)
+    Return a UMC module for editing an existing object of the given type
+
+
+Any other module is not part of the "official" API and may change at any time.
+"""
 
 import re
-
-try:
-    import univention
-    import univention.uldap
-    import univention.config_registry
-    import univention.admin.uldap
-    import univention.admin.objects
-    import univention.admin.config
-    import ldap as orig_ldap
-    has_lib_univention = True
-except ImportError:
-    has_lib_univention = False
-
 
 
 __all__ = [
     'ldap_search',
     'config_registry',
     'base_dn',
-    'ldap',
-    'config',
-    'position_base_dn',
-    'get_umc_admin_objects',
+    'uldap',
+    'umc_module_for_add',
+    'umc_module_for_edit',
 ]
 
-config_registry = univention.config_registry.ConfigRegistry()
-config_registry.load()
-base_dn         = config_registry["ldap/base"]
+
+_singletons = {}
+
+
+def ldap_module():
+    import ldap as orig_ldap
+    return orig_ldap
+
+
+def _singleton(name, constructor):
+    if name in _singletons:
+        return _singletons[name]
+    _singletons[name] = constructor()
+    return _singletons[name]
+
+
+def config_registry():
+
+    def construct():
+        import univention.config_registry
+        ucr = univention.config_registry.ConfigRegistry()
+        ucr.load()
+        return ucr
+
+    return _singleton('config_registry', construct)
+
+
+def base_dn():
+    config_registry()['ldap/base']
+
+
+def uldap():
+    "Return a configured univention uldap object"
+
+    def construct():
+        try:
+            secret_file = open('/etc/ldap.secret', 'r')
+            bind_dn     = 'cn=admin,{}'.format(base_dn())
+        except IOError:  # pragma: no cover
+            secret_file = open('/etc/machine.secret', 'r')
+            bind_dn     = config_registry()["ldap/hostdn"]
+        pwd_line        = secret_file.readline()
+        pwd             = re.sub('\n', '', pwd_line)
+
+        import univention.admin.uldap
+        return univention.admin.uldap.access(
+            host      = config_registry()['ldap/master'],
+            base      = base_dn(),
+            binddn    = bind_dn(),
+            bindpw    = pwd,
+            start_tls = 1
+        )
+
+    return _singleton('uldap', construct)
+
+
+def config():
+    def construct():
+        import univention.admin.config
+        return univention.admin.config.config()
+    return _singleton('config', construct)
+
+
+def init_modules():
+    import univention.admin.modules
+    univention.admin.modules.update()
+
 
 try:
-    secret_file = open('/etc/ldap.secret', 'r')
-    bind_dn     = 'cn=admin,{}'.format(base_dn)
-except IOError:  # pragma: no cover
-    secret_file = open('/etc/machine.secret', 'r')
-    bind_dn     = config_registry["ldap/hostdn"]
-pwd_line        = secret_file.readline()
-pwd             = re.sub('\n', '', pwd_line)
+    init_modules()
+except:
+    pass
 
-ldap = univention.admin.uldap.access(
-    host      = config_registry['ldap/master'],
-    base      = base_dn,
-    binddn    = bind_dn,
-    bindpw    = pwd,
-    start_tls = 1
-)
-config = univention.admin.config.config()
-univention.admin.modules.update()
-position_base_dn = univention.admin.uldap.position(base_dn)
-modules_by_name = {}
+
+def position_base_dn():
+    def construct():
+        import univention.admin.uldap
+        univention.admin.uldap.position(base_dn())
+    return _singleton('position_base_dn', construct)
 
 
 def ldap_dn_tree_parent(dn, count=1):
@@ -80,12 +145,15 @@ def ldap_dn_tree_parent(dn, count=1):
     return ','.join(dn_array)
 
 
-def ldap_search(filter, base=base_dn, attr=None):
+def ldap_search(filter, base=None, attr=None):
     """Replaces uldaps search and uses a generator.
    !! Arguments are not the same."""
-    msgid = ldap.lo.lo.search(
+
+    if base is None:
+        base = base_dn()
+    msgid = uldap().lo.lo.search(
         base,
-        orig_ldap.SCOPE_SUBTREE,
+        ldap_module().SCOPE_SUBTREE,
         filterstr=filter,
         attrlist=attr
     )
@@ -93,48 +161,49 @@ def ldap_search(filter, base=base_dn, attr=None):
     # which swallows the KeyboardInterrupt
     # The abandon now doesn't make too much sense
     while True:
-        result_type, result_data = ldap.lo.lo.result(msgid, all=0)
+        result_type, result_data = uldap().lo.lo.result(msgid, all=0)
         if not result_data:
             break
-        if result_type is orig_ldap.RES_SEARCH_RESULT:  # pragma: no cover
+        if result_type is ldap_module().RES_SEARCH_RESULT:  # pragma: no cover
             break
         else:
-            if result_type is orig_ldap.RES_SEARCH_ENTRY:
+            if result_type is ldap_module().RES_SEARCH_ENTRY:
                 for res in result_data:
                     yield res
-    ldap.lo.lo.abandon(msgid)
+    uldap().lo.lo.abandon(msgid)
 
 
-def module_name(module_name_):
+def module_by_name(module_name_):
     """Returns an initialized UMC module, identified by the given name.
 
-   The module is a module specification according to the udm commandline.
-   Example values are:
-       * users/user
-       * shares/share
-       * groups/group
+    The module is a module specification according to the udm commandline.
+    Example values are:
+    * users/user
+    * shares/share
+    * groups/group
 
-   If the module does not exist, a KeyError is raised.
+    If the module does not exist, a KeyError is raised.
 
-   The modules are cached, so they won't be re-initialized
-   in subsequent calls.
-   """
+    The modules are cached, so they won't be re-initialized
+    in subsequent calls.
+    """
 
-    if module_name_ not in modules_by_name:
+    def construct():
+        import univention.admin.modules
         module = univention.admin.modules.get(module_name_)
-        univention.admin.modules.init(ldap, position_base_dn, module)
+        univention.admin.modules.init(uldap(), position_base_dn(), module)
+        return module
 
-        modules_by_name[module_name_] = module
-
-    return modules_by_name[module_name_]
+    return _singleton('module/%s' % module_name_, construct)
 
 
 def get_umc_admin_objects():
     """Convenience accessor for getting univention.admin.objects.
 
-   This implements delayed importing, so the univention.* modules
-   are not loaded until this function is called.
-   """
+    This implements delayed importing, so the univention.* modules
+    are not loaded until this function is called.
+    """
+    import univention.admin
     return univention.admin.objects
 
 
@@ -150,13 +219,13 @@ def umc_module_for_add(module, container_dn, superordinate=None):
    The container_dn MUST be the dn of the container (not of the object to
    be created itself!).
    """
-    mod = module_name(module)
+    mod = module_by_name(module)
 
-    position = position_base_dn
+    position = position_base_dn()
     position.setDn(container_dn)
 
     # config, ldap objects from common module
-    obj = mod.object(config, ldap, position, superordinate=superordinate)
+    obj = mod.object(config(), uldap(), position, superordinate=superordinate)
     obj.open()
 
     return obj
@@ -173,17 +242,17 @@ def umc_module_for_edit(module, object_dn, superordinate=None):
 
    The object_dn MUST be the dn of the object itself, not the container!
    """
-    mod = module_name(module)
+    mod = module_by_name(module)
 
     objects = get_umc_admin_objects()
 
-    position = position_base_dn
+    position = position_base_dn()
     position.setDn(ldap_dn_tree_parent(object_dn))
 
     obj = objects.get(
         mod,
-        config,
-        ldap,
+        config(),
+        uldap(),
         position=position,
         superordinate=superordinate,
         dn=object_dn
@@ -213,4 +282,3 @@ def create_containers_and_parents(container_dn):
         )
         obj['name'] = container_dn.split(',')[0].split('=')[1]
         obj['description'] = "container created by import"
-
