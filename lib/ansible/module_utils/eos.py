176e6b9a3c23b4b336807278269432c34367cc1b
Nathaniel Case <ncase@redhat.com>
2016-06-09 13:56:14 -0400

    Move eos to NetCli

--- lib/ansible/module_utils/eos.py
+++ lib/ansible/module_utils/eos.py
@@ -21,31 +21,14 @@ import collections
 import re
 
 from ansible.module_utils.basic import json
-from ansible.module_utils.network import NetworkError, get_module, get_exception
+from ansible.module_utils.network import NetCli, NetworkError, get_module
 from ansible.module_utils.network import add_argument, register_transport, to_list
-from ansible.module_utils.shell import Shell, ShellError, Command, HAS_PARAMIKO
+from ansible.module_utils.shell import Command
 from ansible.module_utils.urls import fetch_url, url_argument_spec
 
 NET_PASSWD_RE = re.compile(r"[\r\n]?password: $", re.I)
 
 EAPI_FORMATS = ['json', 'text']
-
-CLI_PROMPTS_RE = [
-    re.compile(r"[\r\n]?[\w+\-\.:\/\[\]]+(?:\([^\)]+\)){,3}(?:>|#) ?$"),
-    re.compile(r"\[\w+\@[\w\-\.]+(?: [^\]])\] ?[>#\$] ?$")
-]
-
-CLI_ERRORS_RE = [
-    re.compile(r"% ?Error"),
-    re.compile(r"^% \w+", re.M),
-    re.compile(r"% ?Bad secret"),
-    re.compile(r"invalid input", re.I),
-    re.compile(r"(?:incomplete|ambiguous) command", re.I),
-    re.compile(r"connection timed out", re.I),
-    re.compile(r"[^\r\n]+ not found", re.I),
-    re.compile(r"'[^']' +returned error code: ?\d+"),
-    re.compile(r"[^\r\n]\/bin\/(?:ba)?sh")
-]
 
 add_argument('use_ssl', dict(default=True, type='bool'))
 add_argument('validate_certs', dict(default=True, type='bool'))
@@ -96,6 +79,9 @@ class Eapi(object):
 
         self.url = '%s://%s:%s/command-api' % (proto, host, port)
 
+        if params.get('authorize'):
+            self.authorize(params)
+
     def disconnect(self, **kwargs):
         self.url = None
 
@@ -137,88 +123,37 @@ class Eapi(object):
     def get_config(self, **kwargs):
         return self.run_commands(['show running-config'], format='text')[0]
 
-    def load_config(self, commands, session_name='ansible_temp_session', commit=False, **kwargs):
-        commands = to_list(commands)
-        commands.insert(0, 'configure session %s' % session_name)
-        commands.append('show session-config diffs')
-        commands.append('end')
-
-        responses = self.run_commands(commands, format='text')
-        if commit:
-            self.commit_config(session_name)
-        return responses[-2]['output']
-
-    def replace_config(self, contents, params, **kwargs):
-        remote_user = params['username']
-        remote_path = '/home/%s/ansible-config' % remote_user
-
-        commands = [
-            'bash timeout 10 echo "%s" > %s' % (contents, remote_path),
-            'diff running-config file:/%s' % remote_path,
-            'config replace file:/%s' % remote_path,
-        ]
-
-        responses = self.run_commands(commands)
-        return responses[-2]
-
-    def commit_config(self, session_name, **kwargs):
-        session = 'configure session %s' % session_name
-        commands = [session, 'commit', 'no '+session]
-        self.run_commands(commands)
-
-    def abort_config(self, session_name, **kwargs):
-        command = 'no configure session %s' % session_name
-        self.run_commands([command])
-
 
 @register_transport('cli', default=True)
-class Cli(object):
+class Cli(NetCli, EosConfigMixin):
+    CLI_PROMPTS_RE = [
+        re.compile(r"[\r\n]?[\w+\-\.:\/\[\]]+(?:\([^\)]+\)){,3}(?:>|#) ?$"),
+        re.compile(r"\[\w+\@[\w\-\.]+(?: [^\]])\] ?[>#\$] ?$")
+    ]
+
+    CLI_ERRORS_RE = [
+        re.compile(r"% ?Error"),
+        re.compile(r"^% \w+", re.M),
+        re.compile(r"% ?Bad secret"),
+        re.compile(r"invalid input", re.I),
+        re.compile(r"(?:incomplete|ambiguous) command", re.I),
+        re.compile(r"connection timed out", re.I),
+        re.compile(r"[^\r\n]+ not found", re.I),
+        re.compile(r"'[^']' +returned error code: ?\d+"),
+        re.compile(r"[^\r\n]\/bin\/(?:ba)?sh")
+    ]
 
     def __init__(self):
-        if not HAS_PARAMIKO:
-            raise NetworkError(
-                msg='paramiko is required but does not appear to be installed.  '
-                'It can be installed using  `pip install paramiko`'
-            )
-
-        self.shell = None
+        super(Cli, self).__init__()
+        self.default_output = 'text'
 
     def connect(self, params, **kwargs):
-        host = params['host']
-        port = params.get('port') or 22
-
-        username = params['username']
-        password = params.get('password')
-        key_file = params.get('ssh_keyfile')
-        timeout = params['timeout']
-
-        try:
-            self.shell = Shell(prompts_re=CLI_PROMPTS_RE, errors_re=CLI_ERRORS_RE)
-            self.shell.open(
-                host, port=port, username=username, password=password,
-                key_filename=key_file, timeout=timeout,
-            )
-        except ShellError:
-            exc = get_exception()
-            raise NetworkError(
-                msg='failed to connect to %s:%s' % (host, port), exc=str(exc)
-            )
-
+        super(Cli, self).connect(params, kickstart=True, **kwargs)
         self.shell.send('terminal length 0')
-
-    def disconnect(self, **kwargs):
-        self.shell.close()
 
     def authorize(self, params, **kwargs):
         passwd = params['auth_pass']
         self.run_commands(Command('enable', prompt=NET_PASSWD_RE, response=passwd))
-
-    def run_commands(self, commands, **kwargs):
-        try:
-            return self.shell.send(commands)
-        except ShellError:
-            exc = get_exception()
-            raise NetworkError(exc.message, commands=commands)
 
     def get_config(self, **kwargs):
         return self.run_commands('show running-config')[0]
