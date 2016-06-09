cc6a4651f57b1e1c3ef26154fe1e1f0aed635a40
Matt Clay <matt@mystile.com>
2016-06-09 16:11:10 -0700

    Proof of concept for parallel integration tests.

--- test/utils/shippable/integration.sh
+++ test/utils/shippable/integration.sh
@@ -5,13 +5,17 @@ source_root=$(python -c "from os import path; print(path.abspath(path.join(path.
 test_image="${IMAGE}"
 test_privileged="${PRIVILEGED:-false}"
 test_flags="${TEST_FLAGS:-}"
-test_target="${TARGET:-}"
+test_targets="${TARGET:-all}"
 test_ansible_dir="${TEST_ANSIBLE_DIR:-/root/ansible}"
 
 http_image="${HTTP_IMAGE:-ansible/ansible:httptester}"
 
 keep_containers="${KEEP_CONTAINERS:-}"
 copy_source="${COPY_SOURCE:-}"
+
+if [ "${test_targets}" = "destructive" ] || [ "${test_targets}" = "non_destructive" ]; then
+    test_targets="${test_targets}_1 ${test_targets}_2"
+fi
 
 if [ "${SHIPPABLE_BUILD_DIR:-}" ]; then
     host_shared_dir="/home/shippable/cache/build-${BUILD_NUMBER}"
@@ -27,7 +31,7 @@ else
     test_shared_dir="${test_ansible_dir}"
 fi
 
-container_id=
+container_ids=
 httptester_id=
 
 function show_environment
@@ -47,9 +51,9 @@ function cleanup
     fi
 
     if [ "${keep_containers}" == "" ]; then
-        if [ "${container_id}" ]; then
+        for container_id in ${container_ids}; do
             docker rm -f "${container_id}"
-        fi
+        done
 
         if [ "${httptester_id}" ]; then
             docker rm -f "${httptester_id}"
@@ -67,25 +71,41 @@ if [ "${controller_shared_dir}" ]; then
 fi
 
 httptester_id=$(docker run -d "${http_image}")
-container_id=$(docker run -d \
-    -v "/sys/fs/cgroup:/sys/fs/cgroup:ro" \
-    -v "${host_shared_dir}:${test_shared_dir}" \
-    --link="${httptester_id}:ansible.http.tests" \
-    --link="${httptester_id}:sni1.ansible.http.tests" \
-    --link="${httptester_id}:sni2.ansible.http.tests" \
-    --link="${httptester_id}:fail.ansible.http.tests" \
-    --privileged="${test_privileged}" \
-    "${test_image}")
+pids=""
 
-show_environment
+for test_target in ${test_targets}; do
+    container_id=$(docker run -d \
+        -v "/sys/fs/cgroup:/sys/fs/cgroup:ro" \
+        -v "${host_shared_dir}:${test_shared_dir}" \
+        --link="${httptester_id}:ansible.http.tests" \
+        --link="${httptester_id}:sni1.ansible.http.tests" \
+        --link="${httptester_id}:sni2.ansible.http.tests" \
+        --link="${httptester_id}:fail.ansible.http.tests" \
+        --privileged="${test_privileged}" \
+        "${test_image}")
 
-docker exec "${container_id}" pip install junit-xml
+    container_ids+=" ${container_id}"
 
-if [ "${copy_source}" ]; then
-    docker exec "${container_id}" cp -a "${test_shared_dir}" "${test_ansible_dir}"
-fi
+    show_environment
 
-docker exec "${container_id}" mkdir -p "${test_shared_dir}/shippable/testresults"
-docker exec "${container_id}" /bin/sh -c "cd '${test_ansible_dir}' && . hacking/env-setup && cd test/integration && \
-    JUNIT_OUTPUT_DIR='${test_shared_dir}/shippable/testresults' ANSIBLE_CALLBACK_WHITELIST=junit \
-    HTTPTESTER=1 TEST_FLAGS='${test_flags}' LC_ALL=en_US.utf-8 make ${test_target}"
+    docker exec "${container_id}" pip install junit-xml
+
+    if [ "${copy_source}" ]; then
+        docker exec "${container_id}" cp -a "${test_shared_dir}" "${test_ansible_dir}"
+    fi
+
+    docker exec "${container_id}" mkdir -p "${test_shared_dir}/shippable/testresults"
+    docker exec "${container_id}" /bin/sh -c "cd '${test_ansible_dir}' && . hacking/env-setup && cd test/integration && \
+        JUNIT_OUTPUT_DIR='${test_shared_dir}/shippable/testresults' ANSIBLE_CALLBACK_WHITELIST=junit \
+        HTTPTESTER=1 TEST_FLAGS='${test_flags}' LC_ALL=en_US.utf-8 make ${test_target}" &
+
+    pids+=" $!"
+done
+
+fail=0
+
+for pid in ${pids}; do
+    wait ${pid} || fail=$((fail+1))
+done
+
+exit ${fail}
