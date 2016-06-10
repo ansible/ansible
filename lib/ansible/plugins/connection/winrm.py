a69e80b196c2f665cf24fb92001f1666946482f5
nitzmahone <mdavis@ansible.com>
2016-06-10 14:40:29 -0700

    call base _connect() from winrm._connect()
    without it, we don't get the base's free become method error check

--- lib/ansible/plugins/connection/winrm.py
+++ lib/ansible/plugins/connection/winrm.py
@@ -232,6 +232,7 @@ class Connection(ConnectionBase):
                 self.protocol.cleanup_command(self.shell_id, command_id)
 
     def _connect(self):
+        super(Connection, self)._connect()
         if not self.protocol:
             self.protocol = self._winrm_connect()
             self._connected = True
