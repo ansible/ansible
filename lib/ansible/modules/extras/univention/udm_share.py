ae8355f77eaa5f2128afce3bd283c13dbe0e4a57
Tobias Rueetschi <tr@brief.li>
2016-06-10 12:44:26 +0200

    univention udm_share: add license information

--- lib/ansible/modules/extras/univention/udm_share.py
+++ lib/ansible/modules/extras/univention/udm_share.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+# Copyright (c) 2016, Adfinis SyGroup AG
+# Tobias Rueetschi <tobias.ruetschi@adfinis-sygroup.ch>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 
 from ansible.module_utils.basic import *
 from ansible.module_utils.univention_umc import (
