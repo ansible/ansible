3843614de1692befb622db0707ecc2abc7f00745
Aleksey Ovcharenko <aleksey.ovcharenko@gmail.com>
2016-06-17 21:02:05 +0300

    Fix for issue @synchronize doesn't substitute variables properly #16347

--- lib/ansible/plugins/action/synchronize.py
+++ lib/ansible/plugins/action/synchronize.py
@@ -272,13 +272,10 @@ class ActionModule(ActionBase):
 
         if not dest_is_local:
             # Private key handling
-            if use_delegate:
-                private_key = task_vars.get('ansible_ssh_private_key_file') or self._play_context.private_key_file
-            else:
-                private_key = task_vars.get('ansible_ssh_private_key_file') or self._play_context.private_key_file
+            private_key = self._play_context.private_key_file
 
             if private_key is not None:
-                private_key = os.path.expanduser(private_key)
+                private_key = format(os.path.expanduser(private_key))
                 self._task.args['private_key'] = private_key
 
             # Src and dest rsync "path" handling
