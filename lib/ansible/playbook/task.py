04fca42b621f75ed3253ece4d73926afa979e55b
James Cammarata <jimi@sngx.net>
2016-06-24 13:52:24 -0500

    Fixing issues with getattr caused by 5a3493b

--- lib/ansible/playbook/task.py
+++ lib/ansible/playbook/task.py
@@ -415,13 +415,13 @@ class Task(Base, Conditional, Taggable, Become):
             value = self._attributes[attr]
 
             if self._block and (value is None or extend):
-                parent_value = getattr(self._block, attr)
+                parent_value = getattr(self._block, attr, None)
                 if extend:
                     value = self._extend_value(value, parent_value)
                 else:
                     value = parent_value
             if self._task_include and (value is None or extend):
-                parent_value = getattr(self._task_include, attr)
+                parent_value = getattr(self._task_include, attr, None)
                 if extend:
                     value = self._extend_value(value, parent_value)
                 else:
@@ -447,3 +447,5 @@ class Task(Base, Conditional, Taggable, Become):
         '''
         return self._get_parent_attribute('any_errors_fatal')
 
+    def _get_attr_loop(self):
+        return self._attributes['loop']
