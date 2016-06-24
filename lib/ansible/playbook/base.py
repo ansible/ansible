da80f9a68f8b40aea0530ac9ca662b4bd32842f0
nitzmahone <mdavis@ansible.com>
2016-06-24 11:39:04 -0700

    minimalist py3 fix to generic_g delegating impl

--- lib/ansible/playbook/base.py
+++ lib/ansible/playbook/base.py
@@ -112,31 +112,18 @@ class Base:
         if hasattr(self, method):
             return getattr(self, method)()
 
-        # value_found is here because we think that value needs to be changed
-        # in the future.  self._attributes[prop_name] will return None
-        # sometimes, apparently if it's not explicitly set in the playbook.
-        # This would seem to make None a sentinel value.  However, the user
-        # could set the attribute to None explicitly (via !!nil) which will
-        # not be recognized because it's being used as a sentinel.  And
-        # sometimes _attributes[prop_name] throws a KeyError so None doesn't
-        # always mean that prop_name was not set.  To work around these
-        # issues, value_found is here so that if value's behaviour is changed
-        # in the future, things can still be made to work.
+        value = None
+
         try:
             value = self._attributes[prop_name]
-            value_found = True
         except KeyError:
-            value = None
-            value_found = False
+            pass
 
-        if not value_found and hasattr(self, '_get_parent_attribute'):
+        if value is None and hasattr(self, '_get_parent_attribute'):
             value = self._get_parent_attribute(prop_name)
-            value_found = True
 
-        if value_found:
-            return value
-        raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, prop_name))
-
+        return value
+        
     @staticmethod
     def _generic_s(prop_name, self, value):
         self._attributes[prop_name] = value
