1b29c879362cc528e82c151a8ff61da2ae955b79
Matt Clay <matt@mystile.com>
2016-06-24 19:32:54 -0700

    Test ansible version and ping during sanity tests. (#16441)

--- test/utils/shippable/sanity.sh
+++ test/utils/shippable/sanity.sh
@@ -15,7 +15,14 @@ if [ "${TOXENV}" = 'py24' ]; then
     python2.4 -m compileall -fq -x 'module_utils/(a10|rax|openstack|ec2|gce|docker_common|azure_rm_common|vca|vmware).py' lib/ansible/module_utils
 else
     if [ "${install_deps}" != "" ]; then
-        pip install tox
+        pip install \
+            tox \
+            pyyaml \
+            jinja2 \
+            setuptools \
+            --upgrade
+
+        pip list
     fi
 
     xunit_dir="${source_root}/shippable/testresults"
@@ -28,4 +35,9 @@ else
     coverage_file="${coverage_dir}/nosetests-coverage.xml"
 
     TOX_TESTENV_PASSENV=NOSETESTS NOSETESTS="nosetests --with-xunit --xunit-file='${xunit_file}' --cover-xml --cover-xml-file='${coverage_file}'" tox
+
+    source hacking/env-setup
+    python --version
+    ansible --version
+    ansible -m ping localhost
 fi
