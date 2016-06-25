0e8d24116a6f220e1b7b93abf99b5ca673670fe8
Hiroaki Nakamura <hnakamur@gmail.com>
2016-06-25 18:04:29 +0900

    Fix bug in comparing config

--- lib/ansible/modules/extras/cloud/lxd/lxd_container.py
+++ lib/ansible/modules/extras/cloud/lxd/lxd_container.py
@@ -444,7 +444,7 @@ class LxdContainerManagement(object):
         if key not in self.config:
             return False
         if key == 'config':
-            old_configs = dict((k, v) for k, v in self.old_container_json['metadata'][key].items() if k.startswith('volatile.'))
+            old_configs = dict((k, v) for k, v in self.old_container_json['metadata'][key].items() if not k.startswith('volatile.'))
         else:
             old_configs = self.old_container_json['metadata'][key]
         return self.config[key] != old_configs
