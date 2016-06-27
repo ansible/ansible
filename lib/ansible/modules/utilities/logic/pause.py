c59510aaf40f58a765bc948950e1d1587728d63b
Brian Coca <brian.coca+git@gmail.com>
2016-06-27 09:35:26 -0400

    updated docs in pause to reflect 0 value behaviour

--- lib/ansible/modules/utilities/logic/pause.py
+++ lib/ansible/modules/utilities/logic/pause.py
@@ -27,12 +27,12 @@ version_added: "0.8"
 options:
   minutes:
     description:
-      - Number of minutes to pause for.
+      - A positive number of minutes to pause for.
     required: false
     default: null
   seconds:
     description:
-      - Number of seconds to pause for.
+      - A positive number of seconds to pause for.
     required: false
     default: null
   prompt:
@@ -41,6 +41,8 @@ options:
     required: false
     default: null
 author: "Tim Bielawa (@tbielawa)"
+notes:
+      - Starting in 2.2,  if you specify 0 or negative for minutes or seconds, it will wait for 1 second, previously it would wait indefinitely.
 '''
 
 EXAMPLES = '''
