a000acbcd6c401bd7ac0354b8af75e521f39a60b
Rene Moser <mail@renemoser.net>
2016-06-29 11:19:50 +0200

    basic: compare args check with default values
    Currently, for required_one_of, mutually_exclusive, required_together, etc. the checks complain if the param was passed.
    
    In roles, the best practice is to default role parameters which are passed to the task.
    
    Defaulting role in defaults/main.yml e.g. mutually exclusive args, even with the same value as the arg spec default one, the check will complain.
    
    With this commit, the checks required_one_of, mutually_exclusive, required_together will only complain if the value given is different then the default value.
    
    As the param has the default value set anyway, passing the default value again, should not be handled as an error. This change looks safe to me.

--- lib/ansible/module_utils/basic.py
+++ lib/ansible/module_utils/basic.py
@@ -1276,7 +1276,9 @@ class AnsibleModule(object):
         count = 0
         for term in check:
             if term in self.params:
-                count += 1
+                v = self.argument_spec[term]
+                if self.params[term] != v.get('default'):
+                    count += 1
         return count
 
     def _check_mutually_exclusive(self, spec):
