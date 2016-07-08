50a64c65bc42fdbc1bcd8e3130c07287bad5cca1
s-hertel <hertels@berea.edu>
2016-07-08 09:31:45 -0400

    Changing the test for the GalaxyCLI method parse() to be thorough and systematic.

--- test/units/cli/test_galaxy.py
+++ test/units/cli/test_galaxy.py
@@ -21,7 +21,7 @@ __metaclass__ = type
 
 from ansible.compat.six import PY3
 from ansible.compat.tests import unittest
-from mock import patch
+from mock import patch, call
 
 from ansible.errors import AnsibleOptionsError
 
@@ -57,30 +57,167 @@ class TestGalaxy(unittest.TestCase):
             self.fail('Expected galaxy_tags to be indented twice')
 
     def test_parse(self):
-        ''' tests that an options parser is created for bin/ansible; entails creating SortedOptParser instance and Galaxy instance '''
-        for arguments in [[], ["list", "info"], ["bad_arg", "list"], ["bad_arg", "invalid"]]:
-            # setup
-            gc = GalaxyCLI(args=arguments)
-            first_arg = False
-            for arg in arguments:
-                if arg in gc.VALID_ACTIONS:
-                    # getting data to use for testing
-                    first_arg = arg
-                    # stop looking after a valid action is identified
-                    break
+        ''' systematically testing that the expected options parser is created '''
+        # testing no action given
+        gc = GalaxyCLI(args=[])
+        with patch('sys.argv', ["-c"]):
+            self.assertRaises(AnsibleOptionsError, gc.parse)
+                                                
+        # testing action that doesn't exist
+        gc = GalaxyCLI(args=["NOT_ACTION"])
+        with patch('sys.argv', ["-c"]):
+            self.assertRaises(AnsibleOptionsError, gc.parse)
 
-            # testing case when no valid action is found
-            if first_arg == False:
-                with patch('sys.argv', ["-c"]):
-                    self.assertRaises(AnsibleOptionsError, gc.parse)
-            # testing case when valid action is found
-            else:
-                with patch('sys.argv', ["-c"]):
-                    created_parser = gc.parse()
-                self.assertTrue(created_parser)
+        # testing action 'delete'
+        gc = GalaxyCLI(args=["delete"])    
+        with patch.object(ansible.cli.SortedOptParser, "set_usage") as mocked_usage:
+            with patch('sys.argv', ["-c"]):
+                galaxy_parser = gc.parse()
+
+                # tests
+                self.assertTrue(galaxy_parser)
                 self.assertTrue(isinstance(gc.parser, ansible.cli.SortedOptParser))
                 self.assertTrue(isinstance(gc.galaxy, ansible.galaxy.Galaxy))
-                self.assertTrue(gc.action)
-                self.assertTrue(gc.options.roles_path == ['/etc/ansible/roles'])
-                self.assertTrue(gc.action == first_arg)
-                self.assertNotIn(first_arg, arguments)
+                calls = [call('usage: %prog [delete|import|info|init|install|list|login|remove|search|setup] [--help] [options] ...'), call('usage: %prog delete [options] github_user github_repo')]
+                mocked_usage.assert_has_calls(calls)
+                self.assertTrue(gc.options.verbosity==0)
+        
+        # testing action 'import'
+        gc = GalaxyCLI(args=["import"])
+        with patch.object(ansible.cli.SortedOptParser, "set_usage") as mocked_usage:
+            with patch('sys.argv', ["-c"]):
+                galaxy_parser = gc.parse()
+
+                # tests
+                self.assertTrue(galaxy_parser)
+                self.assertTrue(isinstance(gc.parser, ansible.cli.SortedOptParser))
+                self.assertTrue(isinstance(gc.galaxy, ansible.galaxy.Galaxy))                
+                calls = [call('usage: %prog [delete|import|info|init|install|list|login|remove|search|setup] [--help] [options] ...'), call('usage: %prog import [options] github_user github_repo')]
+                mocked_usage.assert_has_calls(calls)
+                self.assertTrue(gc.options.wait==True)
+                self.assertTrue(gc.options.reference==None)
+                self.assertTrue(gc.options.check_status==False)
+                self.assertTrue(gc.options.verbosity==0)
+
+
+        # testing action 'info'
+        gc = GalaxyCLI(args=["info"])
+        with patch.object(ansible.cli.SortedOptParser, "set_usage") as mocked_usage:
+            with patch('sys.argv', ["-c"]):
+                galaxy_parser = gc.parse()
+
+                # tests
+                self.assertTrue(galaxy_parser)
+                self.assertTrue(isinstance(gc.parser, ansible.cli.SortedOptParser))
+                self.assertTrue(isinstance(gc.galaxy, ansible.galaxy.Galaxy))                
+                calls = [call('usage: %prog [delete|import|info|init|install|list|login|remove|search|setup] [--help] [options] ...'), call('usage: %prog info [options] role_name[,version]')]
+                mocked_usage.assert_has_calls(calls)
+                self.assertTrue(gc.options.offline==False)
+
+        # testing action 'init'
+        gc = GalaxyCLI(args=["init"])
+        with patch.object(ansible.cli.SortedOptParser, "set_usage") as mocked_usage:
+            with patch('sys.argv', ["-c"]):
+                galaxy_parser = gc.parse()
+
+                # tests
+                self.assertTrue(galaxy_parser)
+                self.assertTrue(isinstance(gc.parser, ansible.cli.SortedOptParser))
+                self.assertTrue(isinstance(gc.galaxy, ansible.galaxy.Galaxy))                
+                calls = [call('usage: %prog [delete|import|info|init|install|list|login|remove|search|setup] [--help] [options] ...'), call('usage: %prog init [options] role_name')]
+                mocked_usage.assert_has_calls(calls)
+                self.assertTrue(gc.options.offline==False)
+                self.assertTrue(gc.options.force==False)
+
+        # testing action 'install'
+        gc = GalaxyCLI(args=["install"])
+        with patch.object(ansible.cli.SortedOptParser, "set_usage") as mocked_usage:
+            with patch('sys.argv', ["-c"]):
+                galaxy_parser = gc.parse()
+
+                # tests
+                self.assertTrue(galaxy_parser)
+                self.assertTrue(isinstance(gc.parser, ansible.cli.SortedOptParser))
+                self.assertTrue(isinstance(gc.galaxy, ansible.galaxy.Galaxy))                
+                calls = [call('usage: %prog [delete|import|info|init|install|list|login|remove|search|setup] [--help] [options] ...'), call('usage: %prog install [options] [-r FILE | role_name(s)[,version] | scm+role_repo_url[,version] | tar_file(s)]')]
+                mocked_usage.assert_has_calls(calls)
+                self.assertTrue(gc.options.ignore_errors==False)
+                self.assertTrue(gc.options.no_deps==False)
+                self.assertTrue(gc.options.role_file==None)
+                self.assertTrue(gc.options.force==False)
+
+        # testing action 'list'
+        gc = GalaxyCLI(args=["list"])
+        with patch.object(ansible.cli.SortedOptParser, "set_usage") as mocked_usage:
+            with patch('sys.argv', ["-c"]):
+                galaxy_parser = gc.parse()
+
+                # tests
+                self.assertTrue(galaxy_parser)
+                self.assertTrue(isinstance(gc.parser, ansible.cli.SortedOptParser))
+                self.assertTrue(isinstance(gc.galaxy, ansible.galaxy.Galaxy))                
+                calls = [call('usage: %prog [delete|import|info|init|install|list|login|remove|search|setup] [--help] [options] ...'), call('usage: %prog list [role_name]')]
+                mocked_usage.assert_has_calls(calls)
+                self.assertTrue(gc.options.verbosity==0)
+
+        # testing action 'login'
+        gc = GalaxyCLI(args=["login"])
+        with patch.object(ansible.cli.SortedOptParser, "set_usage") as mocked_usage:
+            with patch('sys.argv', ["-c"]):
+                galaxy_parser = gc.parse()
+
+                # tests
+                self.assertTrue(galaxy_parser)
+                self.assertTrue(isinstance(gc.parser, ansible.cli.SortedOptParser))
+                self.assertTrue(isinstance(gc.galaxy, ansible.galaxy.Galaxy))                
+                calls = [call('usage: %prog [delete|import|info|init|install|list|login|remove|search|setup] [--help] [options] ...'), call('usage: %prog login [options]')]
+                mocked_usage.assert_has_calls(calls)
+                self.assertTrue(gc.options.verbosity==0)
+                self.assertTrue(gc.options.token==None)
+
+        # testing action 'remove'
+        gc = GalaxyCLI(args=["remove"])
+        with patch.object(ansible.cli.SortedOptParser, "set_usage") as mocked_usage:
+            with patch('sys.argv', ["-c"]):
+                galaxy_parser = gc.parse()
+
+                # tests
+                self.assertTrue(galaxy_parser)
+                self.assertTrue(isinstance(gc.parser, ansible.cli.SortedOptParser))
+                self.assertTrue(isinstance(gc.galaxy, ansible.galaxy.Galaxy))                
+                calls = [call('usage: %prog [delete|import|info|init|install|list|login|remove|search|setup] [--help] [options] ...'), call('usage: %prog remove role1 role2 ...')]
+                mocked_usage.assert_has_calls(calls)
+                self.assertTrue(gc.options.verbosity==0)
+
+        # testing action 'search'
+        gc = GalaxyCLI(args=["search"])
+        with patch.object(ansible.cli.SortedOptParser, "set_usage") as mocked_usage:
+            with patch('sys.argv', ["-c"]):
+                galaxy_parser = gc.parse()
+
+                # tests
+                self.assertTrue(galaxy_parser)
+                self.assertTrue(isinstance(gc.parser, ansible.cli.SortedOptParser))
+                self.assertTrue(isinstance(gc.galaxy, ansible.galaxy.Galaxy))                
+                calls = [call('usage: %prog [delete|import|info|init|install|list|login|remove|search|setup] [--help] [options] ...'), call('usage: %prog search [searchterm1 searchterm2] [--galaxy-tags galaxy_tag1,galaxy_tag2] [--platforms platform1,platform2] [--author username]')]
+                mocked_usage.assert_has_calls(calls)
+                self.assertTrue(gc.options.platforms==None)
+                self.assertTrue(gc.options.tags==None)
+                self.assertTrue(gc.options.author==None)
+
+        # testing action 'setup'
+        gc = GalaxyCLI(args=["setup"])
+        with patch.object(ansible.cli.SortedOptParser, "set_usage") as mocked_usage:
+            with patch('sys.argv', ["-c"]):
+                galaxy_parser = gc.parse()
+
+                # tests
+                self.assertTrue(galaxy_parser)
+                self.assertTrue(isinstance(gc.parser, ansible.cli.SortedOptParser))
+                self.assertTrue(isinstance(gc.galaxy, ansible.galaxy.Galaxy))                
+                calls = [call('usage: %prog [delete|import|info|init|install|list|login|remove|search|setup] [--help] [options] ...'), call('usage: %prog setup [options] source github_user github_repo secret')]
+                mocked_usage.assert_has_calls(calls)
+                self.assertTrue(gc.options.verbosity==0)
+                self.assertTrue(gc.options.remove_id==None)
+                self.assertTrue(gc.options.setup_list==False)
+
