cb6f26344725e230266e87f0e45a15724ca68f34
Peter Sprygada <psprygada@ansible.com>
2016-07-13 14:45:26 -0700

    bug fix in ios_config module for handling config contents
    Config contents when passed via argument were returning a string but
    the module expects an instance of NetworkConfig.  This fixes that
    problem.

--- lib/ansible/modules/network/ios/ios_config.py
+++ lib/ansible/modules/network/ios/ios_config.py
@@ -202,7 +202,7 @@ def check_args(module, warnings):
 def get_candidate(module):
     candidate = NetworkConfig(indent=1)
     if module.params['src']:
-        candidate = module.params['src']
+        candidate.load(module.params['src'])
     elif module.params['lines']:
         parents = module.params['parents'] or list()
         candidate.add(module.params['lines'], parents=parents)
