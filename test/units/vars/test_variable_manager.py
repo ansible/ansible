ansible/ansible

commit 3ced6d3e90ef70813f994f5fe76daa43946d1875

Further tweaks to variable precedence to make it match our docs
Also removes looking at role variables from the Block, as those are
merged in separately via VariableManager

lib/ansible/vars/__init__.py
@@ -323,25 +323,37 @@ class VariableManager:
                         display.vvv("skipping vars_file '%s' due to an undefined variable" % vars_file_item)
                         continue
 
+            # By default, we now merge in all vars from all roles in the play,
+            # unless the user has disabled this via a config option
             if not C.DEFAULT_PRIVATE_ROLE_VARS:
                 for role in play.get_roles():
                     all_vars = combine_vars(all_vars, role.get_vars(include_params=False))
 
+        # next, we merge in the vars from the role, which will specifically
+        # follow the role dependency chain, and then we merge in the tasks
+        # vars (which will look at parent blocks/task includes)
+        if task:
+            if task._role:
+                all_vars = combine_vars(all_vars, task._role.get_vars(include_params=False))
+            all_vars = combine_vars(all_vars, task.get_vars())
+
+        # next, we merge in the vars cache (include vars) and nonpersistent
+        # facts cache (set_fact/register), in that order
         if host:
             all_vars = combine_vars(all_vars, self._vars_cache.get(host.get_name(), dict()))
             all_vars = combine_vars(all_vars, self._nonpersistent_fact_cache.get(host.name, dict()))
 
+        # next, we merge in role params and task include params
         if task:
             if task._role:
-                all_vars = combine_vars(all_vars, task._role.get_vars(include_params=False))
                 all_vars = combine_vars(all_vars, task._role.get_role_params(task._block.get_dep_chain()))
-            all_vars = combine_vars(all_vars, task.get_vars())
 
             # special case for include tasks, where the include params
             # may be specified in the vars field for the task, which should
             # have higher precedence than the vars/np facts above
             all_vars = combine_vars(all_vars, task.get_include_params())
 
+        # finally, we merge in extra vars and the magic variables
         all_vars = combine_vars(all_vars, self._extra_vars)
         all_vars = combine_vars(all_vars, magic_variables)
 

lib/ansible/playbook/block.py
@@ -65,8 +65,6 @@ class Block(Base, Become, Conditional, Taggable):
 
         all_vars = self.vars.copy()
 
-        if self._role:
-            all_vars.update(self._role.get_vars(self._dep_chain, include_params=False))
         if self._parent_block:
             all_vars.update(self._parent_block.get_vars())
         if self._task_include:

commit f63f5a50474bc1fe15dfac590541d382dd8177b5

Get role vars with the dependency chain
This fixes issue #16729 introduced by commit 3ced6d3e.

Also add a test for role vars with dependencies.

lib/ansible/vars/__init__.py
@@ -334,7 +334,7 @@ class VariableManager:
         # vars (which will look at parent blocks/task includes)
         if task:
             if task._role:
-                all_vars = combine_vars(all_vars, task._role.get_vars(include_params=False))
+                all_vars = combine_vars(all_vars, task._role.get_vars(task._block._dep_chain, include_params=False))
             all_vars = combine_vars(all_vars, task.get_vars())
 
         # next, we merge in the vars cache (include vars) and nonpersistent

test/units/vars/test_variable_manager.py
@@ -300,3 +300,57 @@ class TestVariableManager(unittest.TestCase):
 
         res = v.get_vars(loader=fake_loader, play=play1, host=h1)
         self.assertEqual(res['fact_cache_var'], 'fact_cache_var_from_fact_cache')
+
+    @patch('ansible.playbook.role.definition.unfrackpath', mock_unfrackpath_noop)
+    def test_variable_manager_role_vars_dependencies(self):
+        '''
+        Tests vars from role dependencies with duplicate dependencies.
+        '''
+
+        v = VariableManager()
+        v._fact_cache = defaultdict(dict)
+
+        fake_loader = DictDataLoader({
+            # role common-role
+            '/etc/ansible/roles/common-role/tasks/main.yml': """
+            - debug: msg="{{role_var}}"
+            """,
+            # We do not need allow_duplicates: yes for this role
+            # because eliminating duplicates is done by the execution
+            # strategy, which we do not test here.
+
+            # role role1
+            '/etc/ansible/roles/role1/vars/main.yml': """
+            role_var: "role_var_from_role1"
+            """,
+            '/etc/ansible/roles/role1/meta/main.yml': """
+            dependencies:
+              - { role: common-role }
+            """,
+
+            # role role2
+            '/etc/ansible/roles/role2/vars/main.yml': """
+            role_var: "role_var_from_role2"
+            """,
+            '/etc/ansible/roles/role2/meta/main.yml': """
+            dependencies:
+              - { role: common-role }
+            """,
+        })
+
+        play1 = Play.load(dict(
+           hosts=['all'],
+           roles=['role1', 'role2'],
+        ), loader=fake_loader, variable_manager=v)
+
+        # The task defined by common-role exists twice because role1
+        # and role2 depend on common-role.  Check that the tasks see
+        # different values of role_var.
+        blocks = play1.compile()
+        task = blocks[1].block[0]
+        res = v.get_vars(loader=fake_loader, play=play1, task=task)
+        self.assertEqual(res['role_var'], 'role_var_from_role1')
+
+        task = blocks[2].block[0]
+        res = v.get_vars(loader=fake_loader, play=play1, task=task)
+        self.assertEqual(res['role_var'], 'role_var_from_role2')