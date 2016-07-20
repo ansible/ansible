d269fe59279a4d336c01ed58c92974aabc3f0858
Adam Hamsik <adh@rsd.com>
2016-07-20 17:03:57 +0200

    Add initial version of hpilo modules, based on work done by dag@wieers.com in ansible-provisioning

---
+++ lib/ansible/modules/bmc/hpilo/hpilo_boot.py
@@ -0,0 +1,216 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright 2012 Dag Wieers <dag@wieers.com>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+DOCUMENTATION = '''
+---
+author: Dag Wieers
+module: hpilo_boot
+requirements: [ python hpilo ]
+short_description: Boot system using specific media through HP iLO interface
+description:
+  - "This module boots a system through its HP iLO interface. The boot media
+    can be one of: cdrom, floppy, hdd, network or usb."
+  - This module requires the hpilo python module.
+version_added: "2.2"
+options:
+  host:
+    description:
+      - The HP iLO hostname/address that is linked to the physical system.
+    default: null
+    required: true
+    aliases: ['ilo_host']
+  login:
+    description:
+      - The login name to authenticate to the HP iLO interface.
+    required: true
+    default: Administrator
+    aliases: ['user']
+  password:
+    description:
+      - The password to authenticate to the HP iLO interface.
+    required: true
+    default: admin
+    aliases: ['pass']
+  media:
+    description:
+      - The boot media to boot the system from
+    required: true
+    default: network
+    choices: [ "cdrom", "floppy", "hdd", "network", "normal", "usb" ]
+  image:
+    description:
+      - "The URL of a cdrom, floppy or usb boot media image.
+        'protocol://username:password@hostname:port/filename'"
+      - protocol is either 'http' or 'https'
+      - "username:password is optional"
+      - port is optional
+  state:
+    description:
+      - The state of the boot media.
+      - "no_boot: Do not boot from the device"
+      - "boot_once: Boot from the device once and then notthereafter"
+      - "boot_always: Boot from the device each time the serveris rebooted"
+      - "connect: Connect the virtual media device and set to boot_always"
+      - "disconnect: Disconnects the virtual media device and set to no_boot"
+      - "poweroff: Power off the server"
+    required: true
+    default: boot_once
+    choices: [ "boot_always", "boot_once", "connect", "disconnect", "no_boot", "poweroff" ]
+  force:
+    description:
+      - Whether to force a reboot (even when the system is already booted).
+      - As a safeguard, without force, hpilo_boot will refuse to reboot a server that is already running.
+    default: no
+    choices: [ "yes", "no" ]
+  timeout:
+    description:
+      - Set HPilo connection timeout.
+    required: False
+    default: 60
+notes:
+  - To use a USB key image you need to specify floppy as boot media.
+  - This module ought to be run from a system that can access the HP iLO
+    interface directly, either by using local_action or
+    using delegate_to.
+'''
+
+EXAMPLES = '''
+# Task to boot a system using an ISO from an HP iLO interface only if the system is an HP server
+- hpilo_boot:
+    host: YOUR_ILO_ADDRESS
+    login: YOUR_ILO_LOGIN
+    password: YOUR_ILO_PASSWORD
+    media: cdrom
+    image: http://some-web-server/iso/boot.iso
+  when: cmdb_hwmodel.startswith('HP ')
+  delegate_to: localhost
+
+# Power off a server
+- hpilo_boot:
+    host: YOUR_ILO_HOST
+    login: YOUR_ILO_LOGIN
+    password: YOUR_ILO_PASSWORD
+    state: poweroff
+  delegate_to: localhost
+'''
+
+RETURN = '''
+#
+'''
+
+import warnings
+try:
+    import hpilo
+    HAS_HPILO = True
+except ImportError:
+    HAS_HPILO = False
+
+# Surpress warnings from hpilo
+warnings.simplefilter('ignore')
+
+def main():
+
+    module = AnsibleModule(
+        argument_spec=dict(
+            host=dict(required=True, type='str', aliases=['ilo_host']),
+            login=dict(default='Administrator', type='str', aliases=['user']),
+            password=dict(default='admin', type='str', aliases=['pass']),
+            media=dict(default='network', choices=['cdrom', 'floppy', 'rbsu', 'hdd', 'network', 'normal', 'usb']),
+            image=dict(default=None, type='str'),
+            state=dict(default='boot_once', choices=['boot_always', 'boot_once', 'connect', 'disconnect', 'no_boot', 'poweroff']),
+            force=dict(default='no', type='bool'),
+            timeout=dict(default=60, type='int')
+        )
+    )
+
+    host = module.params.get('host')
+    login = module.params.get('login')
+    password = module.params.get('password')
+    media = module.params.get('media')
+    image = module.params.get('image')
+    state = module.params.get('state')
+    force = module.params.get('force')
+    timeout = module.params.get('timeout')
+
+    if not HAS_HPILO:
+        module.fail_json(msg="hpilo python module is required.")
+
+    ilo = hpilo.Ilo(host, login=login, password=password, timeout=timeout)
+    changed = False
+    status = {}
+    power_status = 'UNKNOWN'
+
+    if media and state in ('boot_always', 'boot_once', 'connect', 'disconnect', 'no_boot'):
+
+        # Workaround for: Error communicating with iLO: Problem manipulating EV
+        try:
+            ilo.set_one_time_boot(media)
+        except hpilo.IloError:
+            #time.sleep(60)
+            ilo.set_one_time_boot(media)
+
+        # TODO: Verify if image URL exists/works
+        if image:
+            ilo.insert_virtual_media(media, image)
+            changed = True
+
+        if media == 'cdrom':
+            ilo.set_vm_status('cdrom', state, True)
+            status = ilo.get_vm_status()
+            changed = True
+        elif media in ('floppy', 'usb'):
+            ilo.set_vf_status(state, True)
+            status = ilo.get_vm_status(device='floppy')
+            changed = True
+
+    # Only perform a boot when state is boot_once or boot_always, or in case we want to force a reboot
+    if state in ('boot_once', 'boot_always') or force:
+
+        power_status = ilo.get_host_power_status()
+
+        if not force and power_status == 'ON':
+            module.fail_json(msg='HP iLO (%s) reports that the server is already powered on !' % host)
+
+        if power_status == 'ON':
+#            ilo.cold_boot_server()
+            ilo.warm_boot_server()
+            changed = True
+        else:
+            ilo.press_pwr_btn()
+#            ilo.reset_server()
+#            ilo.set_host_power(host_power=True)
+            changed = True
+
+    elif state in 'poweroff':
+        power_status = ilo.get_host_power_status()
+
+        if not power_status == 'OFF':
+            ilo.hold_pwr_btn()
+#            ilo.set_host_power(host_power=False)
+            changed = True
+
+    module.exit_json(changed=changed, power=power_status, **status)
+
+# import module snippets
+from ansible.module_utils.basic import *
+from ansible.module_utils.urls import *
+
+if __name__ == "__main__":
+    main()
---
+++ lib/ansible/modules/bmc/hpilo/hpilo_facts.py
@@ -0,0 +1,209 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright 2012 Dag Wieers <dag@wieers.com>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+DOCUMENTATION = '''
+---
+author: Dag Wieers
+module: hpilo_facts
+requirements: [ python hpilo ]
+short_description: Gather facts through an HP iLO interface
+description:
+    - This module gathers facts for a specific system using its HP iLO interface.
+      These facts include hardware and network related information useful
+      for provisioning (e.g. macaddress, uuid).
+    - This module requires the hpilo python module.
+version_added: "2.2"
+options:
+    host:
+        description:
+            - The HP iLO hostname/address that is linked to the physical system.
+        required: true
+    login:
+        description:
+            - The login name to authenticate to the HP iLO interface.
+        default: Administrator
+    password:
+        description:
+            - The password to authenticate to the HP iLO interface.
+        default: admin
+notes:
+    - This module ought to be run from a system that can access the HP iLO
+    interface directly, either by using local_action or
+    using delegate_to.
+'''
+
+EXAMPLES = '''
+# Task to gather facts from a HP iLO interface only if the system is an HP server
+- hpilo_facts:
+    host: YOUR_ILO_ADDRESS
+    login: YOUR_ILO_LOGIN
+    password: YOUR_ILO_PASSWORD
+  when: cmdb_hwmodel.startswith('HP ')
+  delegate_to: localhost
+
+- fail:
+    msg: 'CMDB serial ({{ cmdb_serialno }}) does not match hardware serial ({{ hw_system_serial }}) !'
+    when: cmdb_serialno != hw_system_serial
+'''
+
+RETURN = '''
+
+- hw_bios_date: "05/05/2011"
+  hw_bios_version: "P68"
+  hw_eth0:
+- macaddress: "00:11:22:33:44:55"
+  macaddress_dash: "00-11-22-33-44-55"
+  hw_eth1:
+- macaddress: "00:11:22:33:44:57"
+  macaddress_dash: "00-11-22-33-44-57"
+  hw_eth2:
+- macaddress: "00:11:22:33:44:5A"
+  macaddress_dash: "00-11-22-33-44-5A"
+  hw_eth3:
+- macaddress: "00:11:22:33:44:5C"
+  macaddress_dash: "00-11-22-33-44-5C"
+  hw_eth_ilo:
+- macaddress: "00:11:22:33:44:BA"
+  macaddress_dash: "00-11-22-33-44-BA"
+  hw_product_name: "ProLiant DL360 G7"
+  hw_product_uuid: "ef50bac8-2845-40ff-81d9-675315501dac"
+  hw_system_serial: "ABC12345D6"
+  hw_uuid: "123456ABC78901D2"
+'''
+
+import re
+import warnings
+try:
+    import hpilo
+    HAS_HPILO = True
+except ImportError:
+    HAS_HPILO = False
+
+# Surpress warnings from hpilo
+warnings.simplefilter('ignore')
+
+def parse_flat_interface(entry, non_numeric='hw_eth_ilo'):
+    try:
+        factname = 'hw_eth' + str(int(entry['Port']) - 1)
+    except:
+        factname = non_numeric
+
+        facts = {
+            'macaddress': entry['MAC'].replace('-', ':'),
+            'macaddress_dash': entry['MAC']
+        }
+        return (factname, facts)
+
+def main():
+    argument_spec = dict(
+        host     = dict(default=None, required=True, type='str', aliases=['ilo_host']),
+        login    = dict(default='Administrator', type='str', aliases=['user']),
+        password = dict(default='admin', type='str', aliases=['pass'])
+    )
+
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+    )
+
+    host = module.params.get('host')
+    login = module.params.get('login')
+    password = module.params.get('password')
+
+    if not HAS_HPILO:
+        module.fail_json(msg="hpilo python module is required.")
+
+    ilo = hpilo.Ilo(host, login=login, password=password)
+
+    # TODO: Count number of CPUs, DIMMs and total memory
+    data = ilo.get_host_data()
+
+    facts = {
+        'module_hw': True,
+    }
+
+    for entry in data:
+        if not entry.has_key('type'):
+            continue
+        if entry['type'] == 0: # BIOS Information
+            facts['hw_bios_version'] = entry['Family']
+            facts['hw_bios_date'] = entry['Date']
+        elif entry['type'] == 1: # System Information
+            facts['hw_uuid'] = entry['UUID']
+            facts['hw_system_serial'] = entry['Serial Number'].rstrip()
+            facts['hw_product_name'] = entry['Product Name']
+            facts['hw_product_uuid'] = entry['cUUID']
+        elif entry['type'] == 209: # Embedded NIC MAC Assignment
+            if entry.has_key('fields'):
+                for (name, value) in [ (e['name'], e['value']) for e in entry['fields'] ]:
+                    if name.startswith('Port'):
+                        try:
+                            factname = 'hw_eth' + str(int(value) - 1)
+                        except:
+                            factname = 'hw_eth_ilo'
+                    elif name.startswith('MAC'):
+                        facts[factname] = {
+                            'macaddress': value.replace('-', ':'),
+                            'macaddress_dash': value
+                        }
+                    else:
+                        (factname, entry_facts) = parse_flat_interface(entry, 'hw_eth_ilo')
+                        facts[factname] = entry_facts
+        elif entry['type'] == 209:  # HPQ NIC iSCSI MAC Info
+            for (name, value) in [(e['name'], e['value']) for e in entry['fields']]:
+                if name.startswith('Port'):
+                    try:
+                        factname = 'hw_iscsi' + str(int(value) - 1)
+                    except:
+                        factname = 'hw_iscsi_ilo'
+                elif name.startswith('MAC'):
+                    facts[factname] = {
+                    'macaddress': value.replace('-', ':'),
+                    'macaddress_dash': value
+                    }
+                elif entry['type'] == 233:  # Embedded NIC MAC Assignment (Alternate data format)
+                    (factname, entry_facts) = parse_flat_interface(entry, 'hw_eth_ilo')
+                    facts[factname] = entry_facts
+
+    # collect health (RAM/CPU data)
+    health = ilo.get_embedded_health()
+    facts['hw_health'] = health
+    memory_details_summary = health.get('memory', {}).get('memory_details_summary')
+
+    # RAM as reported by iLO 2.10 on ProLiant BL460c Gen8
+    if memory_details_summary:
+        facts['hw_memory_details_summary'] = memory_details_summary
+        facts['hw_memory_total'] = 0
+        for cpu, details in memory_details_summary.iteritems():
+            cpu_total_memory_size = details.get('total_memory_size')
+            if cpu_total_memory_size:
+                ram = re.search('(\d+)\s+(\w+)', cpu_total_memory_size)
+                if ram:
+                    if ram.group(2) == 'GB':
+                        facts['hw_memory_total'] = int(facts['hw_memory_total']) + int(ram.group(1))
+
+    facts['hw_memory_total'] = "{0} GB".format(facts['hw_memory_total'])
+
+    module.exit_json(ansible_facts=facts)
+
+# import module snippets
+from ansible.module_utils.basic import *
+
+if __name__ == "__main__":
+    main()
