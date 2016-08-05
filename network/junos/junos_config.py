5f02f54ff99003e4d3860ec1907b2ced2669f6a1
John R Barker <john@johnrbarker.com>
2016-08-05 16:38:58 +0100

    Update junos_config.py

--- network/junos/junos_config.py
+++ network/junos/junos_config.py
@@ -5,7 +5,7 @@
 # Ansible is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# (at your option) any later version.choices
 #
 # Ansible is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -56,9 +56,6 @@ options:
         argument will effectively remove all current configuration
         statements on the remote device.
     required: false
-    choices:
-        - yes
-        - no
     default: null
   confirm:
     description:
