4148b744331bd2d7c5a3904be5d81a55a51ee109
John R Barker <john@johnrbarker.com>
2016-08-05 17:01:52 +0100

    Update junos_config.py

--- network/junos/junos_config.py
+++ network/junos/junos_config.py
@@ -5,7 +5,7 @@
 # Ansible is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.choices
+# (at your option) any later version.
 #
 # Ansible is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
