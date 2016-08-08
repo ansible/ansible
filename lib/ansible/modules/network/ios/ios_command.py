d9c28454ae0d83807ef53ed2cf9c49402d9ea08b
Alvaro Aleman <alv2412@googlemail.com>
2016-08-08 14:38:07 +0200

    Make ios_command example working

--- lib/ansible/modules/network/ios/ios_command.py
+++ lib/ansible/modules/network/ios/ios_command.py
@@ -78,33 +78,34 @@ vars:
     password: cisco
     transport: cli
 
-- name: run show version on remote devices
-  ios_command:
-    commands: show version
-    provider "{{ cli }}"
+tasks:
+  - name: run show version on remote devices
+    ios_command:
+      commands: show version
+      provider "{{ cli }}"
 
-- name: run show version and check to see if output contains IOS
-  ios_command:
-    commands: show version
-    wait_for: result[0] contains IOS
-    provider "{{ cli }}"
+  - name: run show version and check to see if output contains IOS
+    ios_command:
+      commands: show version
+      wait_for: result[0] contains IOS
+      provider "{{ cli }}"
 
-- name: run multiple commands on remote nodes
-   ios_command:
-    commands:
-      - show version
-      - show interfaces
-    provider "{{ cli }}"
+  - name: run multiple commands on remote nodes
+     ios_command:
+      commands:
+        - show version
+        - show interfaces
+      provider "{{ cli }}"
 
-- name: run multiple commands and evaluate the output
-  ios_command:
-    commands:
-      - show version
-      - show interfaces
-    wait_for:
-      - result[0] contains IOS
-      - result[1] contains Loopback0
-    provider "{{ cli }}"
+  - name: run multiple commands and evaluate the output
+    ios_command:
+      commands:
+        - show version
+        - show interfaces
+      wait_for:
+        - result[0] contains IOS
+        - result[1] contains Loopback0
+      provider: "{{ cli }}"
 """
 
 RETURN = """
