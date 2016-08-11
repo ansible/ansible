a0c0e9f3f404485c1e885de7b20a694337e99f52
Hugh Ma <hugh.ma@flextronics.com>
2016-08-11 21:20:45 +0000

    adding contrib info; retriggering

--- contrib/inventory/openstack.py
+++ contrib/inventory/openstack.py
@@ -4,6 +4,7 @@
 # Copyright (c) 2013, Jesse Keating <jesse.keating@rackspace.com>
 # Copyright (c) 2015, Hewlett-Packard Development Company, L.P.
 # Copyright (c) 2016, Rackspace Australia
+# Copyright (c) 2016, Hugh Ma <hugh.ma@flextronics.com>
 #
 # This module is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
