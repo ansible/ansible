94886ff6fb3bcae011965be059a838e750cac31a
James Tanner <tanner.jc@gmail.com>
2016-08-11 19:24:16 -0400

    Restore previous behavior of ignoring missing files via with_fileglob
    Fixes #16801

--- lib/ansible/plugins/lookup/fileglob.py
+++ lib/ansible/plugins/lookup/fileglob.py
@@ -21,6 +21,7 @@ import os
 import glob
 
 from ansible.plugins.lookup import LookupBase
+from ansible.errors import AnsibleFileNotFound
 
 class LookupModule(LookupBase):
 
@@ -29,7 +30,11 @@ class LookupModule(LookupBase):
         ret = []
         for term in terms:
             term_file = os.path.basename(term)
-            dwimmed_path = self.find_file_in_search_path(variables, 'files', os.path.dirname(term))
-            globbed = glob.glob(os.path.join(dwimmed_path, term_file))
-            ret.extend(g for g in globbed if os.path.isfile(g))
+            try:
+                dwimmed_path = self.find_file_in_search_path(variables, 'files', os.path.dirname(term))
+            except AnsibleFileNotFound:
+                dwimmed_path = None
+            if dwimmed_path:
+                globbed = glob.glob(os.path.join(dwimmed_path, term_file))
+                ret.extend(g for g in globbed if os.path.isfile(g))
         return ret
