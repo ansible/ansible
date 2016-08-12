722dca9a3b86205eee6ea8d73e2a7fba86b976b3
Josh Preston <jdp.github@prestoncentral.com>
2016-08-12 10:18:53 -0400

    suggested rework
    * removed second cache / dict with duplicate info
    * added purge_actions configuration option to remove the actions from a host (defaults to False)
    * added prefer_ip_address configuration option so give the option of using ip address instead of name (defaults to True)
    * removed self variables — just use the arguments directly
    * added --pretty command line option to pretty print results
    * renamed _resolve_params to _resolve_host

--- contrib/inventory/cloudforms.py
+++ contrib/inventory/cloudforms.py
@@ -41,13 +41,9 @@ class CloudFormsInventory(object):
         """
         self.inventory = dict()  # A list of groups and the hosts in that group
         self.cache = dict()      # Details about hosts in the inventory
-        self.params = dict()     # Params of each host
 
         # Parse CLI arguments
         self.parse_cli_args()
-
-        # Debug?
-        self.debug = self.args.debug
 
         # Read settings
         self.read_settings()
@@ -57,14 +53,13 @@ class CloudFormsInventory(object):
             self.update_cache()
         else:
             self.load_inventory_from_cache()
-            self.load_params_from_cache()
             self.load_cache_from_cache()
 
         data_to_print = ""
 
         # Data to print
         if self.args.host:
-            if self.debug:
+            if self.args.debug:
                 print "Fetching host [%s]" % self.args.host
             data_to_print += self.get_host_info(self.args.host)
         else:
@@ -72,9 +67,8 @@ class CloudFormsInventory(object):
             for hostname in self.cache:
                 self.inventory['_meta']['hostvars'][hostname] = {
                     'cloudforms': self.cache[hostname],
-                    'cloudforms_params': self.params[hostname],
                 }
-            data_to_print += self.json_format_dict(self.inventory, True)
+            data_to_print += self.json_format_dict(self.inventory, self.args.pretty)
 
         print(data_to_print)
 
@@ -82,20 +76,19 @@ class CloudFormsInventory(object):
         """
         Determines if the cache files have expired, or if it is still valid
         """
-        if self.debug:
+        if self.args.debug:
             print "Determining if cache [%s] is still valid (< %s seconds old)" % (self.cache_path_cache, self.cache_max_age)
 
         if os.path.isfile(self.cache_path_cache):
             mod_time = os.path.getmtime(self.cache_path_cache)
             current_time = time()
             if (mod_time + self.cache_max_age) > current_time:
-                if (os.path.isfile(self.cache_path_inventory) and
-                    os.path.isfile(self.cache_path_params)):
-                    if self.debug:
+                if os.path.isfile(self.cache_path_inventory):
+                    if self.args.debug:
                         print "Cache is still valid!"
                     return True
 
-        if self.debug:
+        if self.args.debug:
             print "Cache is stale or does not exist."
 
         return False
@@ -114,7 +107,7 @@ class CloudFormsInventory(object):
         if env_value is not None:
             config_paths.append(os.path.expanduser(os.path.expandvars(env_value)))
 
-        if self.debug:
+        if self.args.debug:
             for config_path in config_paths:
                 print "Reading from configuration file [%s]" % config_path
 
@@ -127,6 +120,8 @@ class CloudFormsInventory(object):
         self.cloudforms_ssl_verify = config.getboolean('cloudforms', 'ssl_verify')
         self.cloudforms_version = config.get('cloudforms', 'version')
         self.cloudforms_limit = config.getint('cloudforms', 'limit')
+        self.cloudforms_prefer_ip_address = config.getboolean('cloudforms', 'prefer_ip_address')
+        self.cloudforms_purge_actions = config.getboolean('cloudforms', 'purge_actions')
 
         # Ansible related
         try:
@@ -144,22 +139,22 @@ class CloudFormsInventory(object):
         (script, ext) = os.path.splitext(os.path.basename(__file__))
         self.cache_path_cache = cache_path + "/%s.cache" % script
         self.cache_path_inventory = cache_path + "/%s.inventory" % script
-        self.cache_path_params = cache_path + "/%s.params" % script
         self.cache_max_age = config.getint('cache', 'max_age')
 
-        if self.debug:
+        if self.args.debug:
             print "CloudForms settings:"
-            print "cloudforms_url        = %s" % self.cloudforms_url
-            print "cloudforms_user       = %s" % self.cloudforms_user
-            print "cloudforms_pw         = %s" % self.cloudforms_pw
-            print "cloudforms_ssl_verify = %s" % self.cloudforms_ssl_verify
-            print "cloudforms_version    = %s" % self.cloudforms_version
-            print "cloudforms_limit      = %s" % self.cloudforms_limit
+            print "cloudforms_url               = %s" % self.cloudforms_url
+            print "cloudforms_user              = %s" % self.cloudforms_user
+            print "cloudforms_pw                = %s" % self.cloudforms_pw
+            print "cloudforms_ssl_verify        = %s" % self.cloudforms_ssl_verify
+            print "cloudforms_version           = %s" % self.cloudforms_version
+            print "cloudforms_limit             = %s" % self.cloudforms_limit
+            print "cloudforms_prefer_ip_address = %s" % self.cloudforms_prefer_ip_address
+            print "cloudforms_purge_actions     = %s" % self.cloudforms_purge_actions
             print "Cache settings:"
-            print "cache_path_cache      = %s" % self.cache_path_cache
-            print "cache_path_inventory  = %s" % self.cache_path_inventory
-            print "cache_path_params     = %s" % self.cache_path_params
-            print "cache_max_age         = %s" % self.cache_max_age
+            print "cache_max_age        = %s" % self.cache_max_age
+            print "cache_path_cache     = %s" % self.cache_path_cache
+            print "cache_path_inventory = %s" % self.cache_path_inventory
 
     def parse_cli_args(self):
         """
@@ -168,8 +163,9 @@ class CloudFormsInventory(object):
         parser = argparse.ArgumentParser(description='Produce an Ansible Inventory file based on CloudForms managed VMs')
         parser.add_argument('--list', action='store_true', default=True, help='List instances (default: True)')
         parser.add_argument('--host', action='store', help='Get all the variables about a specific instance')
+        parser.add_argument('--pretty', action='store_true', default=False, help='Pretty print JSON output (default: False)')
         parser.add_argument('--refresh-cache', action='store_true', default=False,
-                            help='Force refresh of cache by making API requests to cloudforms (default: False - use cache files)')
+                            help='Force refresh of cache by making API requests to CloudForms (default: False - use cache files)')
         parser.add_argument('--debug', action='store_true', default=False, help='Show debug output while running (default: False)')
         self.args = parser.parse_args()
 
@@ -186,7 +182,7 @@ class CloudFormsInventory(object):
         ret.raise_for_status()
         results = json.loads(ret.text)
 
-        if self.debug:
+        if self.args.debug:
             print "======================================================================="
             print "======================================================================="
             print "======================================================================="
@@ -221,17 +217,17 @@ class CloudFormsInventory(object):
     def _get_host_by_id(self, hid):
         return self._get_json("%s/api/vms/%s" % (self.cloudforms_url, hid))
 
-    def _resolve_params(self, host):
+    def _resolve_host(self, host):
         """
-        Resolve all params of the host.
+        Resolve host details.
         """
         host_id = host['id']
-        params = self._get_host_by_id(host_id)
+        ret = self._get_host_by_id(host_id)
 
-        # get rid of the silly actions...
-        del params['actions']
+        if self.cloudforms_purge_actions:
+            del ret['actions']
 
-        return params
+        return ret
 
     def update_cache(self):
         """
@@ -240,21 +236,21 @@ class CloudFormsInventory(object):
         self.groups = dict()
         self.hosts = dict()
 
-        if self.debug:
+        if self.args.debug:
             print "Updating cache..."
 
         for host in self._get_hosts():
             if host['power_state'] != 'on':
-                if self.debug:
+                if self.args.debug:
                     print "Skipping %s because power_state = %s" % (host['name'], host['power_state'])
                 continue
 
-            if host.has_key('ipaddresses') and host['ipaddresses'][0]:
+            if self.cloudforms_prefer_ip_address and host.has_key('ipaddresses') and host['ipaddresses'][0]:
                 dns_name = host['ipaddresses'][0]
             else:
                 dns_name = host['name']
 
-            if self.debug:
+            if self.args.debug:
                 print "Host named [%s] will use [%s] as dns_name" % (host['name'], dns_name)
 
             # Create ansible groups for hostgroup, location and organization
@@ -263,44 +259,44 @@ class CloudFormsInventory(object):
                     safe_key = self.to_safe(group['name'])
                     self.push(self.inventory, safe_key, dns_name)
 
-                    if self.debug:
+                    if self.args.debug:
                         print "Found tag [%s] for host which will be mapped to [%s]" % (group['name'], safe_key)
 
-            params = self._resolve_params(host)
+            full_host = self._resolve_host(host)
+            if host.has_key('ipaddresses'):
+                full_host['ipaddresses'] = host['ipaddresses']
 
-            self.cache[dns_name] = host
-            self.params[dns_name] = params
+            self.cache[dns_name] = full_host
             self.push(self.inventory, 'all', dns_name)
 
-        if self.debug:
+        if self.args.debug:
             print "Saving cached data"
 
         self.write_to_cache(self.cache, self.cache_path_cache)
         self.write_to_cache(self.inventory, self.cache_path_inventory)
-        self.write_to_cache(self.params, self.cache_path_params)
 
     def get_host_info(self, host):
         """
         Get variables about a specific host
         """
-        if not self.params or len(self.params) == 0:
-            # Need to load params from cache
-            self.load_params_from_cache()
+        if not self.cache or len(self.cache) == 0:
+            # Need to load cache from cache
+            self.load_cache_from_cache()
 
-        if host not in self.params:
-            if self.debug:
+        if host not in self.cache:
+            if self.args.debug:
                 print "[%s] not found in cache." % host
 
             # try updating the cache
             self.update_cache()
 
-            if host not in self.params:
-                if self.debug:
+            if host not in self.cache:
+                if self.args.debug:
                     print "[%s] does not exist after cache update." % host
                 # host might not exist anymore
-                return self.json_format_dict({}, True)
+                return self.json_format_dict({}, self.args.pretty)
 
-        return self.json_format_dict(self.params[host], True)
+        return self.json_format_dict(self.cache[host], self.args.pretty)
 
     def push(self, d, k, v):
         """
@@ -318,14 +314,6 @@ class CloudFormsInventory(object):
         cache = open(self.cache_path_inventory, 'r')
         json_inventory = cache.read()
         self.inventory = json.loads(json_inventory)
-
-    def load_params_from_cache(self):
-        """
-        Reads the params from the cache file sets self.params
-        """
-        cache = open(self.cache_path_params, 'r')
-        json_params = cache.read()
-        self.params = json.loads(json_params)
 
     def load_cache_from_cache(self):
         """
