53ea0a4110faf2eec6b74acaa1c5d60b970b4ebb
Michael Grandjean <grandjean@univention.de>
2016-09-01 22:47:38 +0200

    Fix typo in EXAMPLE section (#2833)

--- lib/ansible/modules/extras/univention/udm_share.py
+++ lib/ansible/modules/extras/univention/udm_share.py
@@ -375,7 +375,7 @@ options:
 
 EXAMPLES = '''
 # Create a share named home on the server ucs.example.com with the path /home.
-- udm_sahre: name=home
+- udm_share: name=home
              path=/home
              host=ucs.example.com
              sambaName=Home
