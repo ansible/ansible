f553f188b98ef703177b353dfa0d5b75632c87fd
Nick Aslanidis <nick.aslanidis@gmail.com>
2016-09-04 19:43:52 +1000

    correct syntax

--- lib/ansible/modules/cloud/amazon/rds_facts.py
+++ lib/ansible/modules/cloud/amazon/rds_facts.py
@@ -284,7 +284,8 @@ def main():
     try:
         region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
         connection = boto3_conn(module, conn_type='client', resource='rds', region=region, endpoint=ec2_url, **aws_connect_kwargs)
-    except botocore.exceptions.NoCredentialsError, e:
+    except botocore.exceptions.NoCredentialsError:
+        e = get_exception()
         module.fail_json(msg="Can't authorize connection - "+str(e))
 
     # call your function here
