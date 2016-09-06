b17c73757595a871b53f7d58bd1c21b42316cc44
James Cammarata <jimi@sngx.net>
2016-09-05 23:41:21 -0500

    Fixing incorrect 'task' variable bug in strategy/__init__.py

--- lib/ansible/plugins/strategy/__init__.py
+++ lib/ansible/plugins/strategy/__init__.py
@@ -341,7 +341,7 @@ class StrategyBase:
                                     # find the host we're actually refering too here, which may
                                     # be a host that is not really in inventory at all
                                     if original_task.delegate_to is not None and original_task.delegate_facts:
-                                        task_vars = self._variable_manager.get_vars(loader=self._loader, play=iterator._play, host=host, task=task)
+                                        task_vars = self._variable_manager.get_vars(loader=self._loader, play=iterator._play, host=host, task=original_task)
                                         self.add_tqm_variables(task_vars, play=iterator._play)
                                         if item is not None:
                                             task_vars[loop_var] = item
