fab5f5adbe69eb0865e831ae31e561b64b382998
Toshio Kuratomi <a.badger@gmail.com>
2016-09-06 16:35:25 -0700

    Fix UnicodeError loading plugins with non-ascii in them on python3

--- lib/ansible/plugins/__init__.py
+++ lib/ansible/plugins/__init__.py
@@ -317,7 +317,7 @@ class PluginLoader:
             return sys.modules[name]
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", RuntimeWarning)
-            with open(path, 'r') as module_file:
+            with open(path, 'rb') as module_file:
                 module = imp.load_source(name, path, module_file)
         return module
 
