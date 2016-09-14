9bef6f6597b58e41cb5e5ea61dd53e25dc10f95f
GGabriele <gabrielegerbino@gmail.com>
2016-09-14 15:52:37 +0200

    Fixing file writing format

--- network/nxos/nxos_snapshot.py
+++ network/nxos/nxos_snapshot.py
@@ -561,8 +561,9 @@ def write_on_file(content, filename, module):
         path += '/'
     filepath = '{0}{1}'.format(path, filename)
     try:
-        with open(filepath, 'w') as report:
-            report.write(content)
+    	report = open(filepath, 'w')
+        report.write(content)
+        report.close()
     except:
         module.fail_json(msg="Error while writing on file.")
 
