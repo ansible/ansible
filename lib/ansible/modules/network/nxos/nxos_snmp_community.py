808992b7ef220eac985210f7c22151fcf2fcd87c
GGabriele <gabrielegerbino@gmail.com>
2016-09-14 15:12:42 +0200

    Adding nxos_snmp_community

---
+++ lib/ansible/modules/network/nxos/nxos_snmp_community.py
@@ -0,0 +1,500 @@
+#!/usr/bin/python
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+DOCUMENTATION = '''
+---
+module: nxos_snmp_community
+version_added: "2.2"
+short_description: Manages SNMP community configs.
+description:
+    - Manages SNMP community configuration.
+extends_documentation_fragment: nxos
+author:
+    - Jason Edelman (@jedelman8)
+    - Gabriele Gerbino (@GGabriele)
+options:
+    community:
+        description:
+            - Case-sensitive community string.
+        required: true
+    access:
+        description:
+            - Access type for community.
+        required: false
+        default: null
+        choices: ['ro','rw']
+    group:
+        description:
+            - Group to which the community belongs.
+        required: false
+        default: null
+    acl:
+        description:
+            - ACL name to filter snmp requests.
+        required: false
+        default: 1
+    state:
+        description:
+            - Manage the state of the resource.
+        required: true
+        default: present
+        choices: ['present','absent']
+'''
+
+EXAMPLES = '''
+# ensure snmp community is configured
+- nxos_snmp_community:
+    community=TESTING7
+    group=network-operator
+    state=present
+    host={{ inventory_hostname }}
+    username={{ un }}
+    password={{ pwd }}
+'''
+
+RETURN = '''
+proposed:
+    description: k/v pairs of parameters passed into module
+    returned: always
+    type: dict
+    sample: {"group": "network-operator"}
+existing:
+    description: k/v pairs of existing snmp community
+    type: dict
+    sample:  {}
+end_state:
+    description: k/v pairs of snmp community after module execution
+    returned: always
+    type: dict or null
+    sample:  {"acl": "None", "group": "network-operator"}
+updates:
+    description: commands sent to the device
+    returned: always
+    type: list
+    sample: ["snmp-server community TESTING7 group network-operator"]
+changed:
+    description: check to see if a change was made on the device
+    returned: always
+    type: boolean
+    sample: true
+'''
+
+import json
+
+# COMMON CODE FOR MIGRATION
+import re
+
+from ansible.module_utils.basic import get_exception
+from ansible.module_utils.netcfg import NetworkConfig, ConfigLine
+from ansible.module_utils.shell import ShellError
+
+try:
+    from ansible.module_utils.nxos import get_module
+except ImportError:
+    from ansible.module_utils.nxos import NetworkModule
+
+
+def to_list(val):
+     if isinstance(val, (list, tuple)):
+         return list(val)
+     elif val is not None:
+         return [val]
+     else:
+         return list()
+
+
+class CustomNetworkConfig(NetworkConfig):
+
+    def expand_section(self, configobj, S=None):
+        if S is None:
+            S = list()
+        S.append(configobj)
+        for child in configobj.children:
+            if child in S:
+                continue
+            self.expand_section(child, S)
+        return S
+
+    def get_object(self, path):
+        for item in self.items:
+            if item.text == path[-1]:
+                parents = [p.text for p in item.parents]
+                if parents == path[:-1]:
+                    return item
+
+    def to_block(self, section):
+        return '\n'.join([item.raw for item in section])
+
+    def get_section(self, path):
+        try:
+            section = self.get_section_objects(path)
+            return self.to_block(section)
+        except ValueError:
+            return list()
+
+    def get_section_objects(self, path):
+        if not isinstance(path, list):
+            path = [path]
+        obj = self.get_object(path)
+        if not obj:
+            raise ValueError('path does not exist in config')
+        return self.expand_section(obj)
+
+
+    def add(self, lines, parents=None):
+        """Adds one or lines of configuration
+        """
+
+        ancestors = list()
+        offset = 0
+        obj = None
+
+        ## global config command
+        if not parents:
+            for line in to_list(lines):
+                item = ConfigLine(line)
+                item.raw = line
+                if item not in self.items:
+                    self.items.append(item)
+
+        else:
+            for index, p in enumerate(parents):
+                try:
+                    i = index + 1
+                    obj = self.get_section_objects(parents[:i])[0]
+                    ancestors.append(obj)
+
+                except ValueError:
+                    # add parent to config
+                    offset = index * self.indent
+                    obj = ConfigLine(p)
+                    obj.raw = p.rjust(len(p) + offset)
+                    if ancestors:
+                        obj.parents = list(ancestors)
+                        ancestors[-1].children.append(obj)
+                    self.items.append(obj)
+                    ancestors.append(obj)
+
+            # add child objects
+            for line in to_list(lines):
+                # check if child already exists
+                for child in ancestors[-1].children:
+                    if child.text == line:
+                        break
+                else:
+                    offset = len(parents) * self.indent
+                    item = ConfigLine(line)
+                    item.raw = line.rjust(len(line) + offset)
+                    item.parents = ancestors
+                    ancestors[-1].children.append(item)
+                    self.items.append(item)
+
+
+def get_network_module(**kwargs):
+    try:
+        return get_module(**kwargs)
+    except NameError:
+        return NetworkModule(**kwargs)
+
+def get_config(module, include_defaults=False):
+    config = module.params['config']
+    if not config:
+        try:
+            config = module.get_config()
+        except AttributeError:
+            defaults = module.params['include_defaults']
+            config = module.config.get_config(include_defaults=defaults)
+    return CustomNetworkConfig(indent=2, contents=config)
+
+def load_config(module, candidate):
+    config = get_config(module)
+
+    commands = candidate.difference(config)
+    commands = [str(c).strip() for c in commands]
+
+    save_config = module.params['save']
+
+    result = dict(changed=False)
+
+    if commands:
+        if not module.check_mode:
+            try:
+                module.configure(commands)
+            except AttributeError:
+                module.config(commands)
+
+            if save_config:
+                try:
+                    module.config.save_config()
+                except AttributeError:
+                    module.execute(['copy running-config startup-config'])
+
+        result['changed'] = True
+        result['updates'] = commands
+
+    return result
+# END OF COMMON CODE
+
+
+def execute_config_command(commands, module):
+    try:
+        module.configure(commands)
+    except ShellError:
+        clie = get_exception()
+        module.fail_json(msg='Error sending CLI commands',
+                         error=str(clie), commands=commands)
+    except AttributeError:
+        try:
+            commands.insert(0, 'configure')
+            module.cli.add_commands(commands, output='config')
+            module.cli.run_commands()
+        except ShellError:
+            clie = get_exception()
+            module.fail_json(msg='Error sending CLI commands',
+                             error=str(clie), commands=commands)
+
+
+def get_cli_body_ssh(command, response, module):
+    """Get response for when transport=cli.  This is kind of a hack and mainly
+    needed because these modules were originally written for NX-API.  And
+    not every command supports "| json" when using cli/ssh.  As such, we assume
+    if | json returns an XML string, it is a valid command, but that the
+    resource doesn't exist yet. Instead, the output will be a raw string
+    when issuing commands containing 'show run'.
+    """
+    if 'xml' in response[0]:
+        body = []
+    elif 'show run' in command:
+        body = response
+    else:
+        try:
+            if isinstance(response[0], str):
+                body = [json.loads(response[0])]
+            else:
+                body = response
+        except ValueError:
+            module.fail_json(msg='Command does not support JSON output',
+                             command=command)
+    return body
+
+
+def execute_show(cmds, module, command_type=None):
+    command_type_map = {
+        'cli_show': 'json',
+        'cli_show_ascii': 'text'
+    }
+
+    try:
+        if command_type:
+            response = module.execute(cmds, command_type=command_type)
+        else:
+            response = module.execute(cmds)
+    except ShellError:
+        clie = get_exception()
+        module.fail_json(msg='Error sending {0}'.format(cmds),
+                         error=str(clie))
+    except AttributeError:
+        try:
+            if command_type:
+                command_type = command_type_map.get(command_type)
+                module.cli.add_commands(cmds, output=command_type)
+                response = module.cli.run_commands()
+            else:
+                module.cli.add_commands(cmds)
+                response = module.cli.run_commands()
+        except ShellError:
+            clie = get_exception()
+            module.fail_json(msg='Error sending {0}'.format(cmds),
+                             error=str(clie))
+    return response
+
+
+def execute_show_command(command, module, command_type='cli_show'):
+    if module.params['transport'] == 'cli':
+        if 'show run' not in command:
+            command += ' | json'
+        cmds = [command]
+        response = execute_show(cmds, module)
+        body = get_cli_body_ssh(command, response, module)
+    elif module.params['transport'] == 'nxapi':
+        cmds = [command]
+        body = execute_show(cmds, module, command_type=command_type)
+
+    return body
+
+
+def apply_key_map(key_map, table):
+    new_dict = {}
+    for key, value in table.items():
+        new_key = key_map.get(key)
+        if new_key:
+            value = table.get(key)
+            if value:
+                new_dict[new_key] = str(value)
+            else:
+                new_dict[new_key] = value
+    return new_dict
+
+
+def flatten_list(command_lists):
+    flat_command_list = []
+    for command in command_lists:
+        if isinstance(command, list):
+            flat_command_list.extend(command)
+        else:
+            flat_command_list.append(command)
+    return flat_command_list
+
+
+def get_snmp_groups(module):
+    command = 'show snmp group'
+    data = execute_show_command(command, module)[0]
+
+    group_list = []
+
+    try:
+        group_table = data['TABLE_role']['ROW_role']
+        for group in group_table:
+            group_list.append(group['role_name'])
+    except (KeyError, AttributeError):
+        return group_list
+
+    return group_list
+
+
+def get_snmp_community(module, find_filter=None):
+    command = 'show snmp community'
+    data = execute_show_command(command, module)[0]
+
+    community_dict = {}
+
+    community_map = {
+        'grouporaccess': 'group',
+        'aclfilter': 'acl'
+    }
+
+    try:
+        community_table = data['TABLE_snmp_community']['ROW_snmp_community']
+        for each in community_table:
+            community = apply_key_map(community_map, each)
+            key = each['community_name']
+            community_dict[key] = community
+    except (KeyError, AttributeError):
+        return community_dict
+
+    if find_filter:
+        find = community_dict.get(find_filter, None)
+
+    if find_filter is None or find is None:
+        return {}
+    else:
+        fix_find = {}
+        for (key, value) in find.iteritems():
+            if isinstance(value, str):
+                fix_find[key] = value.strip()
+            else:
+                fix_find[key] = value
+        return fix_find
+
+
+def config_snmp_community(delta, community):
+    CMDS = {
+        'group': 'snmp-server community {0} group {group}',
+        'acl': 'snmp-server community {0} use-acl {acl}'
+    }
+    commands = []
+    for k, v in delta.iteritems():
+        cmd = CMDS.get(k).format(community, **delta)
+        if cmd:
+            commands.append(cmd)
+            cmd = None
+    return commands
+
+
+def main():
+    argument_spec = dict(
+            community=dict(required=True, type='str'),
+            access=dict(choices=['ro', 'rw']),
+            group=dict(type='str'),
+            acl=dict(type='str'),
+            state=dict(choices=['absent', 'present'], default='present'),
+    )
+    module = get_network_module(argument_spec=argument_spec,
+                                required_one_of=[['access', 'group']],
+                                mutually_exclusive=[['access', 'group']],
+                                supports_check_mode=True)
+
+    access = module.params['access']
+    group = module.params['group']
+    community = module.params['community']
+    acl = module.params['acl']
+    state = module.params['state']
+
+    if access:
+        if access == 'ro':
+            group = 'network-operator'
+        elif access == 'rw':
+            group = 'network-admin'
+
+    # group check - ensure group being configured exists on the device
+    configured_groups = get_snmp_groups(module)
+
+    if group not in configured_groups:
+        module.fail_json(msg="group not on switch."
+                             "please add before moving forward")
+
+    existing = get_snmp_community(module, community)
+    args = dict(group=group, acl=acl)
+    proposed = dict((k, v) for k, v in args.iteritems() if v is not None)
+    delta = dict(set(proposed.iteritems()).difference(existing.iteritems()))
+
+    changed = False
+    end_state = existing
+    commands = []
+
+    if state == 'absent':
+        if existing:
+            command = "no snmp-server community {0}".format(community)
+            commands.append(command)
+        cmds = flatten_list(commands)
+    elif state == 'present':
+        if delta:
+            command = config_snmp_community(dict(delta), community)
+            commands.append(command)
+        cmds = flatten_list(commands)
+
+    if cmds:
+        if module.check_mode:
+            module.exit_json(changed=True, commands=cmds)
+        else:
+            changed = True
+            execute_config_command(cmds, module)
+            end_state = get_snmp_community(module, community)
+
+    results = {}
+    results['proposed'] = proposed
+    results['existing'] = existing
+    results['end_state'] = end_state
+    results['updates'] = cmds
+    results['changed'] = changed
+
+    module.exit_json(**results)
+
+
+if __name__ == '__main__':
+    main()
