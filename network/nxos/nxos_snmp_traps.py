79e0890cd0b19c6bc0d641548d4bf9dec8e6f8b9
GGabriele <gabrielegerbino@gmail.com>
2016-09-14 17:49:02 +0200

    Fixing argument_spec

--- network/nxos/nxos_snmp_traps.py
+++ network/nxos/nxos_snmp_traps.py
@@ -452,7 +452,7 @@ def get_trap_commands(group, state, existing, module):
 
 def main():
     argument_spec = dict(
-            state=dict(choices=['enabled', 'disabled'], required=True),
+            state=dict(choices=['enabled', 'disabled'], default='enabled'),
             group=dict(choices=['aaa', 'bridge', 'callhome', 'cfs', 'config',
                                 'entity', 'feature-control', 'hsrp',
                                 'license', 'link', 'lldp', 'ospf', 'pim', 'rf',
