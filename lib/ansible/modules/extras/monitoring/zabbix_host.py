024e75ce3012fbcd810297e3f13668396c7ec6d7
Rene Moser <mail@renemoser.net>
2016-09-14 23:42:29 +0200

    zabbix_host: fix typos in arg spec of login_user
    Credits to @JasonCormie

--- lib/ansible/modules/extras/monitoring/zabbix_host.py
+++ lib/ansible/modules/extras/monitoring/zabbix_host.py
@@ -415,7 +415,7 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             server_url=dict(type='str', required=True, aliases=['url']),
-            login_user=dict(rtype='str', equired=True),
+            login_user=dict(type='str', required=True),
             login_password=dict(type='str', required=True, no_log=True),
             host_name=dict(type='str', required=True),
             http_login_user=dict(type='str', required=False, default=None),
