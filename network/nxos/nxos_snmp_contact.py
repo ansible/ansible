6a5ab25264261dde60da91d624a5434bf0225e09
GGabriele <gabrielegerbino@gmail.com>
2016-09-14 08:36:09 +0200

    Fixing docstring

--- network/nxos/nxos_snmp_contact.py
+++ network/nxos/nxos_snmp_contact.py
@@ -1,25 +1,29 @@
-#!/usr/bin/env python
-
-# Copyright 2015 Jason Edelman <jedelman8@gmail.com>
+#!/usr/bin/python
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# This file is part of Ansible
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 DOCUMENTATION = '''
 ---
 module: nxos_snmp_contact
+version_added: "2.2"
 short_description: Manages SNMP contact info.
 description:
     - Manages SNMP contact information.
+extends_documentation_fragment: nxos
 author:
     - Jason Edelman (@jedelman8)
     - Gabriele Gerbino (@GGabriele)
@@ -63,11 +67,6 @@ end_state:
     returned: always
     type: dict
     sample: {"contact": "New_Test"}
-state:
-    description: state as sent in from the playbook
-    returned: always
-    type: string
-    sample: "present"
 commands:
     description: command string sent to the device
     returned: always
@@ -359,7 +358,6 @@ def main():
     results['proposed'] = proposed
     results['existing'] = existing
     results['end_state'] = end_state
-    results['state'] = state
     results['commands'] = cmds
     results['changed'] = changed
 
