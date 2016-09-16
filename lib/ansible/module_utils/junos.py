eeadae90bcdba8ae1a9aed584040d1d3b9cbb5d0
Nathaniel Case <ncase@redhat.com>
2016-09-16 11:28:45 -0400

    Add simple checks for libraries to junos

--- lib/ansible/module_utils/junos.py
+++ lib/ansible/module_utils/junos.py
@@ -72,6 +72,16 @@ def xml_to_string(val):
 class Netconf(object):
 
     def __init__(self):
+        if not HAS_PYEZ:
+            raise NetworkError(
+                msg='junos-eznc >= 1.2.2 is required but does not appear to be installed.  '
+                'It can be installed using `pip install junos-eznc`'
+            )
+        if not HAS_JXMLEASE:
+            raise NetworkError(
+                msg='jxmlease is required but does not appear to be installed.  '
+                'It can be installed using `pip install jxmlease`'
+            )
         self.device = None
         self.config = None
         self._locked = False
