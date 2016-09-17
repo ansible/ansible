f1be653737f7a1bf59bc6255b2072b6480a0a1f2
Matt Clay <matt@mystile.com>
2016-09-16 22:27:17 -0700

    Run same tests on Shippable as on Travis. (#4889)
    Run the same tests as used on Travis.

---
+++ lib/ansible/modules/test/utils/shippable/sanity.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -eux
+
+source_root=$(python -c "from os import path; print(path.abspath(path.join(path.dirname('$0'), '../../..')))")
+
+install_deps="${INSTALL_DEPS:-}"
+
+cd "${source_root}"
+
+if [ "${install_deps}" != "" ]; then
+    add-apt-repository ppa:fkrull/deadsnakes && apt-get update -qq && apt-get install python2.4 -qq
+
+    pip install git+https://github.com/ansible/ansible.git@devel#egg=ansible
+    pip install git+https://github.com/sivel/ansible-testing.git#egg=ansible_testing
+fi
+
+python2.4 -m compileall -fq   -i                   "test/utils/shippable/sanity-test-python24.txt"
+python2.4 -m compileall -fq   -x "($(printf %s "$(< "test/utils/shippable/sanity-skip-python24.txt"))" | tr '\n' '|')" .
+python2.6 -m compileall -fq .
+python2.7 -m compileall -fq .
+python3.5 -m compileall -fq . -x "($(printf %s "$(< "test/utils/shippable/sanity-skip-python3.txt"))"  | tr '\n' '|')"
+
+ansible-validate-modules --exclude '/utilities/|/shippable(/|$)' .
