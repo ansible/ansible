f2c2dddc0197ee4a6c9ca7789d10b337a098035e
Matt Clay <matt@mystile.com>
2016-09-19 15:01:48 -0700

    Remove duplicate keys from module docs. (#4920)

--- network/nxos/nxos_acl.py
+++ network/nxos/nxos_acl.py
@@ -95,7 +95,6 @@ options:
               keyword 'any'.
         required: false
         default: null
-        default: null
     dest_port_op:
         description:
             - Destination port operands such as eq, neq, gt, lt, range.
