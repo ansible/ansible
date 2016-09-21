aa1d8ed066a67a4e41a2c3ab54d0a003e1e92512
Ryan S. Brown <sb@ryansb.com>
2016-09-21 15:04:09 -0400

    Accept JSON type as the content of policy_json parameter on `iam_policy` module

--- lib/ansible/modules/cloud/amazon/iam_policy.py
+++ lib/ansible/modules/cloud/amazon/iam_policy.py
@@ -280,7 +280,7 @@ def main():
       iam_name=dict(default=None, required=False),
       policy_name=dict(default=None, required=True),
       policy_document=dict(default=None, required=False),
-      policy_json=dict(default=None, required=False),
+      policy_json=dict(type='json', default=None, required=False),
       skip_duplicates=dict(type='bool', default=True, required=False)
   ))
 
