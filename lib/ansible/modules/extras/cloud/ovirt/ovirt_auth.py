999a6488cffa61fc7ff06197ed6c6470b5e13de4
Ondra Machacek <machacek.ondra@gmail.com>
2016-09-21 18:48:05 +0200

    Ovirt auth/vms/disks modules (#2836)
    * Add oVirt module to manage VMs
    
    This patch add oVirt module to manage Virtual Machines
    
    * Add oVirt module to manage authentication
    
    This patch add oVirt module to manage authentication
    
    * Add oVirt module to manage disks
    
    * Added VM state management and fixups

---
+++ lib/ansible/modules/extras/cloud/ovirt/ovirt_auth.py
@@ -0,0 +1,233 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 Red Hat, Inc.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+try:
+    import ovirtsdk4 as sdk
+except ImportError:
+    pass
+
+
+DOCUMENTATION = '''
+---
+module: ovirt_auth
+short_description: "Module to manage authentication to oVirt."
+author: "Ondra Machacek (@machacekondra)"
+version_added: "2.2"
+description:
+    - "This module authenticates to oVirt engine and creates SSO token, which should be later used in
+       all other oVirt modules, so all modules don't need to perform login and logout.
+       This module returns an Ansible fact called I(ovirt_auth). Every module can use this
+       fact as C(auth) parameter, to perform authentication."
+options:
+    state:
+        default: present
+        choices: ['present', 'absent']
+        description:
+            - "Specifies if a token should be created or revoked."
+    username:
+        required: True
+        description:
+            - "The name of the user. For example: I(admin@internal)."
+    password:
+        required: True
+        description:
+            - "The password of the user."
+    url:
+        required: True
+        description:
+            - "A string containing the base URL of the server.
+               For example: I(https://server.example.com/ovirt-engine/api)."
+    insecure:
+        required: False
+        description:
+            - "A boolean flag that indicates if the server TLS certificate and host name should be checked."
+    ca_file:
+        required: False
+        description:
+            - "A PEM file containing the trusted CA certificates. The
+               certificate presented by the server will be verified using these CA
+               certificates. If C(ca_file) parameter is not set, system wide
+               CA certificate store is used."
+    timeout:
+        required: False
+        description:
+            - "The maximum total time to wait for the response, in
+               seconds. A value of zero (the default) means wait forever. If
+               the timeout expires before the response is received an exception
+               will be raised."
+    compress:
+        required: False
+        description:
+            - "A boolean flag indicating if the SDK should ask
+               the server to send compressed responses. The default is I(True).
+               Note that this is a hint for the server, and that it may return
+               uncompressed data even when this parameter is set to I(True)."
+    kerberos:
+        required: False
+        description:
+            - "A boolean flag indicating if Kerberos authentication
+               should be used instead of the default basic authentication."
+notes:
+  - "Everytime you use ovirt_auth module to obtain ticket, you need to also revoke the ticket,
+     when you no longer need it, otherwise the ticket would be revoked by engine when it expires.
+     For an example of how to achieve that, please take a look at I(examples) section."
+'''
+
+EXAMPLES = '''
+tasks:
+  - block:
+       # Create a vault with `ovirt_password` variable which store your
+       # oVirt user's password, and include that yaml file with variable:
+       - include_vars: ovirt_password.yml
+
+       # Always be sure to pass 'no_log: true' to ovirt_auth task,
+       # so the oVirt user's password is not logged:
+       - name: Obtain SSO token with using username/password credentials:
+         no_log: true
+         ovirt_auth:
+           url: https://ovirt.example.com/ovirt-engine/api
+           username: admin@internal
+           ca_file: ca.pem
+           password: "{{ ovirt_password }}"
+
+       # Previous task generated I(ovirt_auth) fact, which you can later use
+       # in different modules as follows:
+       - ovirt_vms:
+           auth: "{{ ovirt_auth }}"
+           state: absent
+           name: myvm
+
+      always:
+        - name: Always revoke the SSO token
+          ovirt_auth:
+            state: absent
+            ovirt_auth: {{ ovirt_auth }}
+'''
+
+RETURN = '''
+ovirt_auth:
+    description: Authentication facts, needed to perform authentication to oVirt.
+    returned: success
+    type: dictionary
+    contains:
+        token:
+            description: SSO token which is used for connection to oVirt engine.
+            returned: success
+            type: string
+            sample: "kdfVWp9ZgeewBXV-iq3Js1-xQJZPSEQ334FLb3eksoEPRaab07DhZ8ED8ghz9lJd-MQ2GqtRIeqhvhCkrUWQPw"
+        url:
+            description: URL of the oVirt engine API endpoint.
+            returned: success
+            type: string
+            sample: "https://ovirt.example.com/ovirt-engine/api"
+        ca_file:
+            description: CA file, which is used to verify SSL/TLS connection.
+            returned: success
+            type: string
+            sample: "ca.pem"
+        insecure:
+            description: Flag indicating if insecure connection is used.
+            returned: success
+            type: bool
+            sample: False
+        timeout:
+            description: Number of seconds to wait for response.
+            returned: success
+            type: int
+            sample: 0
+        compress:
+            description: Flag indicating if compression is used for connection.
+            returned: success
+            type: bool
+            sample: True
+        kerberos:
+            description: Flag indicating if kerberos is used for authentication.
+            returned: success
+            type: bool
+            sample: False
+'''
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            url=dict(default=None),
+            username=dict(default=None),
+            password=dict(default=None),
+            ca_file=dict(default=None),
+            insecure=dict(required=False, type='bool', default=False),
+            timeout=dict(required=False, type='int', default=0),
+            compress=dict(required=False, type='bool', default=True),
+            kerberos=dict(required=False, type='bool', default=False),
+            state=dict(default='present', choices=['present', 'absent']),
+            ovirt_auth=dict(required=None, type='dict'),
+        ),
+        required_if=[
+            ('state', 'absent', ['ovirt_auth']),
+            ('state', 'present', ['username', 'password', 'url']),
+        ],
+    )
+    check_sdk(module)
+
+    state = module.params.get('state')
+    if state == 'present':
+        params = module.params
+    elif state == 'absent':
+        params = module.params['ovirt_auth']
+
+    connection = sdk.Connection(
+        url=params.get('url'),
+        username=params.get('username'),
+        password=params.get('password'),
+        ca_file=params.get('ca_file'),
+        insecure=params.get('insecure'),
+        timeout=params.get('timeout'),
+        compress=params.get('compress'),
+        kerberos=params.get('kerberos'),
+        token=params.get('token'),
+    )
+    try:
+        token = connection.authenticate()
+        module.exit_json(
+            changed=False,
+            ansible_facts=dict(
+                ovirt_auth=dict(
+                    token=token,
+                    url=params.get('url'),
+                    ca_file=params.get('ca_file'),
+                    insecure=params.get('insecure'),
+                    timeout=params.get('timeout'),
+                    compress=params.get('compress'),
+                    kerberos=params.get('kerberos'),
+                ) if state == 'present' else dict()
+            )
+        )
+    except Exception as e:
+        module.fail_json(msg="Error: %s" % e)
+    finally:
+        # Close the connection, but don't revoke token
+        connection.close(logout=state == 'absent')
+
+
+from ansible.module_utils.basic import *
+from ansible.module_utils.ovirt import *
+if __name__ == "__main__":
+    main()
---
+++ lib/ansible/modules/extras/cloud/ovirt/ovirt_disks.py
@@ -0,0 +1,316 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 Red Hat, Inc.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+try:
+    import ovirtsdk4 as sdk
+    import ovirtsdk4.types as otypes
+except ImportError:
+    pass
+
+from ansible.module_utils.ovirt import *
+
+
+DOCUMENTATION = '''
+---
+module: ovirt_disks
+short_description: "Module to manage Virtual Machine and floating disks in oVirt."
+version_added: "2.2"
+author: "Ondra Machacek (@machacekondra)"
+description:
+    - "Module to manage Virtual Machine and floating disks in oVirt."
+options:
+    id:
+        description:
+            - "ID of the disk to manage. Either C(id) or C(name) is required."
+    name:
+        description:
+            - "Name of the disk to manage. Either C(id) or C(name)/C(alias) is required."
+        aliases: ['alias']
+    vm_name:
+        description:
+            - "Name of the Virtual Machine to manage. Either C(vm_id) or C(vm_name) is required if C(state) is I(attached) or I(detached)."
+    vm_id:
+        description:
+            - "ID of the Virtual Machine to manage. Either C(vm_id) or C(vm_name) is required if C(state) is I(attached) or I(detached)."
+    state:
+        description:
+            - "Should the Virtual Machine disk be present/absent/attached/detached."
+        choices: ['present', 'absent', 'attached', 'detached']
+        default: 'present'
+    size:
+        description:
+            - "Size of the disk. Size should be specified using IEC standard units. For example 10GiB, 1024MiB, etc."
+    interface:
+        description:
+            - "Driver of the storage interface."
+        choices: ['virtio', 'ide', 'virtio_scsi']
+        default: 'virtio'
+    format:
+        description:
+            - "Format of the disk. Either copy-on-write or raw."
+        choices: ['raw', 'cow']
+    storage_domain:
+        description:
+            - "Storage domain name where disk should be created. By default storage is chosen by oVirt engine."
+    profile:
+        description:
+            - "Disk profile name to be attached to disk. By default profile is chosen by oVirt engine."
+    bootable:
+        description:
+            - "I(True) if the disk should be bootable. By default when disk is created it isn't bootable."
+    shareable:
+        description:
+            - "I(True) if the disk should be shareable. By default when disk is created it isn't shareable."
+    logical_unit:
+        description:
+            - "Dictionary which describes LUN to be directly attached to VM:"
+            - "C(address) - Address of the storage server. Used by iSCSI."
+            - "C(port) - Port of the storage server. Used by iSCSI."
+            - "C(target) - iSCSI target."
+            - "C(lun_id) - LUN id."
+            - "C(username) - CHAP Username to be used to access storage server. Used by iSCSI."
+            - "C(password) - CHAP Password of the user to be used to access storage server. Used by iSCSI."
+            - "C(storage_type) - Storage type either I(fcp) or I(iscsi)."
+extends_documentation_fragment: ovirt
+'''
+
+
+EXAMPLES = '''
+# Examples don't contain auth parameter for simplicity,
+# look at ovirt_auth module to see how to reuse authentication:
+
+# Create and attach new disk to VM
+- ovirt_disks:
+    name: myvm_disk
+    vm_name: rhel7
+    size: 10GiB
+    format: cow
+    interface: virtio
+
+# Attach logical unit to VM rhel7
+- ovirt_disks:
+    vm_name: rhel7
+    logical_unit:
+      target: iqn.2016-08-09.brq.str-01:omachace
+      id: 1IET_000d0001
+      address: 10.34.63.204
+    interface: virtio
+
+# Detach disk from VM
+- ovirt_disks:
+    state: detached
+    name: myvm_disk
+    vm_name: rhel7
+    size: 10GiB
+    format: cow
+    interface: virtio
+'''
+
+
+RETURN = '''
+id:
+    description: "ID of the managed disk"
+    returned: "On success if disk is found."
+    type: str
+    sample: 7de90f31-222c-436c-a1ca-7e655bd5b60c
+disk:
+    description: "Dictionary of all the disk attributes. Disk attributes can be found on your oVirt instance
+                  at following url: https://ovirt.example.com/ovirt-engine/api/model#types/disk."
+    returned: "On success if disk is found and C(vm_id) or C(vm_name) wasn't passed."
+
+disk_attachment:
+    description: "Dictionary of all the disk attachment attributes. Disk attachment attributes can be found
+                  on your oVirt instance at following url:
+                  https://ovirt.example.com/ovirt-engine/api/model#types/disk_attachment."
+    returned: "On success if disk is found and C(vm_id) or C(vm_name) was passed and VM was found."
+'''
+
+
+
+def _search_by_lun(disks_service, lun_id):
+    """
+    Find disk by LUN ID.
+    """
+    res = [
+        disk for disk in disks_service.list(search='disk_type=lun') if (
+            disk.lun_storage.id == lun_id
+        )
+    ]
+    return res[0] if res else None
+
+
+class DisksModule(BaseModule):
+
+    def build_entity(self):
+        logical_unit = self._module.params.get('logical_unit')
+        return otypes.Disk(
+            id=self._module.params.get('id'),
+            name=self._module.params.get('name'),
+            description=self._module.params.get('description'),
+            format=otypes.DiskFormat(
+                self._module.params.get('format')
+            ) if self._module.params.get('format') else None,
+            provisioned_size=convert_to_bytes(
+                self._module.params.get('size')
+            ),
+            storage_domains=[
+                otypes.StorageDomain(
+                    name=self._module.params.get('storage_domain'),
+                ),
+            ],
+            shareable=self._module.params.get('shareable'),
+            lun_storage=otypes.HostStorage(
+                type=otypes.StorageType(
+                    logical_unit.get('storage_type', 'iscsi')
+                ),
+                logical_units=[
+                    otypes.LogicalUnit(
+                        address=logical_unit.get('address'),
+                        port=logical_unit.get('port', 3260),
+                        target=logical_unit.get('target'),
+                        id=logical_unit.get('id'),
+                        username=logical_unit.get('username'),
+                        password=logical_unit.get('password'),
+                    )
+                ],
+            ) if logical_unit else None,
+        )
+
+    def update_check(self, entity):
+        return (
+            equal(self._module.params.get('description'), entity.description) and
+            equal(convert_to_bytes(self._module.params.get('size')), entity.provisioned_size) and
+            equal(self._module.params.get('format'), str(entity.format)) and
+            equal(self._module.params.get('shareable'), entity.shareable)
+        )
+
+
+class DiskAttachmentsModule(DisksModule):
+
+    def build_entity(self):
+        return otypes.DiskAttachment(
+            disk=super(DiskAttachmentsModule, self).build_entity(),
+            interface=otypes.DiskInterface(
+                self._module.params.get('interface')
+            ) if self._module.params.get('interface') else None,
+            bootable=self._module.params.get('bootable'),
+            active=True,
+        )
+
+    def update_check(self, entity):
+        return (
+            equal(self._module.params.get('interface'), str(entity.interface)) and
+            equal(self._module.params.get('bootable'), entity.bootable)
+        )
+
+
+def main():
+    argument_spec = ovirt_full_argument_spec(
+        state=dict(
+            choices=['present', 'absent', 'attached', 'detached'],
+            default='present'
+        ),
+        id=dict(default=None),
+        name=dict(default=None, aliases=['alias']),
+        vm_name=dict(default=None),
+        vm_id=dict(default=None),
+        size=dict(default=None),
+        interface=dict(default=None,),
+        allocation_policy=dict(default=None),
+        storage_domain=dict(default=None),
+        profile=dict(default=None),
+        format=dict(default=None, choices=['raw', 'cow']),
+        bootable=dict(default=None, type='bool'),
+        shareable=dict(default=None, type='bool'),
+        logical_unit=dict(default=None, type='dict'),
+    )
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True,
+    )
+    check_sdk(module)
+    check_params(module)
+
+    try:
+        disk = None
+        state = module.params['state']
+        connection = create_connection(module.params.pop('auth'))
+        disks_service = connection.system_service().disks_service()
+        disks_module = DisksModule(
+            connection=connection,
+            module=module,
+            service=disks_service,
+        )
+
+        lun = module.params.get('logical_unit')
+        if lun:
+            disk = _search_by_lun(disks_service, lun.get('id'))
+
+        ret = None
+        # First take care of creating the VM, if needed:
+        if state == 'present' or state == 'detached' or state == 'attached':
+            ret = disks_module.create(
+                entity=disk,
+                result_state=otypes.DiskStatus.OK if lun is None else None,
+            )
+            # We need to pass ID to the module, so in case we want detach/attach disk
+            # we have this ID specified to attach/detach method:
+            module.params['id'] = ret['id'] if disk is None else disk.id
+        elif state == 'absent':
+            ret = disks_module.remove()
+
+        # If VM was passed attach/detach disks to/from the VM:
+        if 'vm_id' in module.params or 'vm_name' in module.params and state != 'absent':
+            vms_service = connection.system_service().vms_service()
+
+            # If `vm_id` isn't specified, find VM by name:
+            vm_id = module.params['vm_id']
+            if vm_id is None:
+                vm_id = getattr(search_by_name(vms_service, module.params['vm_name']), 'id', None)
+
+            if vm_id is None:
+                module.fail_json(
+                    msg="VM don't exists, please create it first."
+                )
+
+            disk_attachments_service = vms_service.vm_service(vm_id).disk_attachments_service()
+            disk_attachments_module = DiskAttachmentsModule(
+                connection=connection,
+                module=module,
+                service=disk_attachments_service,
+                changed=ret['changed'] if ret else False,
+            )
+
+            if state == 'present' or state == 'attached':
+                ret = disk_attachments_module.create()
+            elif state == 'detached':
+                ret = disk_attachments_module.remove()
+
+        module.exit_json(**ret)
+    except Exception as e:
+        module.fail_json(msg=str(e))
+    finally:
+        connection.close(logout=False)
+
+
+from ansible.module_utils.basic import *
+if __name__ == "__main__":
+    main()
