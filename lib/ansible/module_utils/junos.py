6c4552f2c8dcd0b73e5ea8007693ce62a9448b9b
Peter Sprygada <privateip@users.noreply.github.com>
2016-09-28 14:36:53 -0400

    adds overwrite kwarg to load_config in junos (#17798)
    The junos load_config() method supports operations of overwrite, replace
    and merge.  This adds the missing overwrite keyword arg to load_config()
    so that action in junos_template can be procesed correctly.

--- lib/ansible/module_utils/junos.py
+++ lib/ansible/module_utils/junos.py
@@ -171,11 +171,17 @@ class Netconf(object):
             return ele
 
     def load_config(self, config, commit=False, replace=False, confirm=None,
-                    comment=None, config_format='text'):
+                    comment=None, config_format='text', overwrite=False):
+
+        if all([replace, overwrite]):
+            self.raise_exc('setting both replace and overwrite to True is invalid')
 
         if replace:
             merge = False
-            overwrite = True
+            overwrite = False
+        elif overwrite:
+            merge = True
+            overwrite = False
         else:
             merge = True
             overwrite = False
