770fd68b42f6a8590b386aca22c7a5c53ae6fdc1
Gabriele <gabrielegerbino@gmail.com>
2016-09-30 16:17:54 -0400

    Fixing nxos_vrf (#5033)

--- network/nxos/nxos_vrf.py
+++ network/nxos/nxos_vrf.py
@@ -274,6 +274,15 @@ def execute_config_command(commands, module):
         clie = get_exception()
         module.fail_json(msg='Error sending CLI commands',
                          error=str(clie), commands=commands)
+    except AttributeError:
+        try:
+            commands.insert(0, 'configure')
+            module.cli.add_commands(commands, output='config')
+            module.cli.run_commands()
+        except ShellError:
+            clie = get_exception()
+            module.fail_json(msg='Error sending CLI commands',
+                             error=str(clie), commands=commands)
 
 
 def get_cli_body_ssh_vrf(module, command, response):
@@ -316,7 +325,7 @@ def execute_show(cmds, module, command_type=None):
                 module.cli.add_commands(cmds, output=command_type)
                 response = module.cli.run_commands()
             else:
-                module.cli.add_commands(cmds, output=command_type)
+                module.cli.add_commands(cmds, raw=True)
                 response = module.cli.run_commands()
         except ShellError:
             clie = get_exception()
@@ -503,6 +512,8 @@ def main():
             execute_config_command(commands, module)
             changed = True
             end_state = get_vrf(vrf, module)
+            if 'configure' in commands:
+                commands.pop(0)
 
     results = {}
     results['proposed'] = proposed
