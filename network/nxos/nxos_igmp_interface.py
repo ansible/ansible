d47e38a8f99473be4099c7d2afb26de0c739fdfa
Gabriele <gabrielegerbino@gmail.com>
2016-10-01 20:34:31 -0400

    Fixing nxos_igmp_interface (#5010)

--- network/nxos/nxos_igmp_interface.py
+++ network/nxos/nxos_igmp_interface.py
@@ -430,7 +430,7 @@ def execute_show(cmds, module, command_type=None):
                 module.cli.add_commands(cmds, output=command_type)
                 response = module.cli.run_commands()
             else:
-                module.cli.add_commands(cmds, output=command_type)
+                module.cli.add_commands(cmds, raw=True)
                 response = module.cli.run_commands()
         except ShellError:
             clie = get_exception()
@@ -715,6 +715,15 @@ def execute_config_command(commands, module):
         clie = get_exception()
         module.fail_json(msg='Error sending CLI commands',
                          error=str(clie), commands=commands)
+    except AttributeError:
+        try:
+            commands.insert(0, 'configure')
+            module.cli.add_commands(commands, output='config')
+            module.cli.run_commands()
+        except ShellError:
+            clie = get_exception()
+            module.fail_json(msg='Error sending CLI commands',
+                             error=str(clie), commands=commands)
 
 
 def main():
@@ -881,6 +890,8 @@ def main():
             execute_config_command(cmds, module)
             changed = True
             end_state = get_igmp_interface(module, interface)
+            if 'configure' in cmds:
+                cmds.pop(0)
 
     results['proposed'] = proposed
     results['existing'] = existing_copy
