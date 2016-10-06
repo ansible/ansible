ansible/ansible

commit 6db31bb4c6f8b6338a38ae602d3b4f5ca8cbce5b

Moves 'statically included' messages to -vv verbosity

lib/ansible/playbook/helpers.py
@@ -184,7 +184,7 @@ def load_list_of_tasks(ds, play, block=None, role=None, task_include=None, use_h
                         # the same fashion used by the on_include callback. We also do it here,
                         # because the recursive nature of helper methods means we may be loading
                         # nested includes, and we want the include order printed correctly
-                        display.display("statically included: %s" % include_file, color=C.COLOR_SKIP)
+                        display.vv("statically included: %s" % include_file, color=C.COLOR_SKIP)
                     except AnsibleFileNotFound as e:
                         if t.static or \
                            C.DEFAULT_TASK_INCLUDES_STATIC or \

commit 402935caab6e54af2616e7adb7338ddddd840232

Fix #17920
6db31bb4c6f8b6338a38ae602d3b4f5ca8cbce5b broke the stable-2.1
branch because an incorrect version of
https://github.com/ansible/ansible/pull/17918 got applied.

lib/ansible/playbook/helpers.py
@@ -184,7 +184,7 @@ def load_list_of_tasks(ds, play, block=None, role=None, task_include=None, use_h
                         # the same fashion used by the on_include callback. We also do it here,
                         # because the recursive nature of helper methods means we may be loading
                         # nested includes, and we want the include order printed correctly
-                        display.vv("statically included: %s" % include_file, color=C.COLOR_SKIP)
+                        display.vv("statically included: %s" % include_file)
                     except AnsibleFileNotFound as e:
                         if t.static or \
                            C.DEFAULT_TASK_INCLUDES_STATIC or \