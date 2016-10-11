1591e3504a7db97a7f73cf2043cf43cf93926cf3
Patrick F. Marques <patrickfmarques@gmail.com>
2016-10-12 00:40:37 +0100

    Refactor do_floating_ip to use Req/Res like digital_ocean_tag

--- lib/ansible/modules/cloud/digital_ocean/do_floating_ip.py
+++ lib/ansible/modules/cloud/digital_ocean/do_floating_ip.py
@@ -76,174 +76,201 @@ EXAMPLES = '''
 
 '''
 
-def digitalocean_api_call(module, method, url, data=None, retry=0):
-    """ Get digitalocean floating ips """
 
-    headers = {
-        "Authorization" : "Bearer %s" % (module.params['oauth_token']),
-        "Content-Type"  : "application/json"
+RETURN = '''
+data:
+    description: a DigitalOcean Floating IP resource
+    Digital Ocean API info https://developers.digitalocean.com/documentation/v2/#floating-ips
+    returned: success and no resource constraint
+    type: dict
+    sample: {
+      "action": {
+        "id": 68212728,
+        "status": "in-progress",
+        "type": "assign_ip",
+        "started_at": "2015-10-15T17:45:44Z",
+        "completed_at": null,
+        "resource_id": 758603823,
+        "resource_type": "floating_ip",
+        "region": {
+          "name": "New York 3",
+          "slug": "nyc3",
+          "sizes": [
+            "512mb",
+            "1gb",
+            "2gb",
+            "4gb",
+            "8gb",
+            "16gb",
+            "32gb",
+            "48gb",
+            "64gb"
+          ],
+          "features": [
+            "private_networking",
+            "backups",
+            "ipv6",
+            "metadata"
+          ],
+          "available": true
+        },
+        "region_slug": "nyc3"
+      }
     }
+'''
 
-    if data:
-        data = module.jsonify(data)
+import json
+import os
 
-    response, info = fetch_url(module, url, headers=headers, method=method, data=data)
+from ansible.module_utils.basic import env_fallback
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.urls import fetch_url
 
-    if info['status'] == 204:
-        # 204 has no content
-        response = {}
-    elif info['status'] in [404, 422]:
+
+class Response(object):
+
+    def __init__(self, resp, info):
+        self.body = None
+        if resp:
+            self.body = resp.read()
+        self.info = info
+
+    @property
+    def json(self):
+        if not self.body:
+            if "body" in self.info:
+                return json.loads(self.info["body"])
+            return None
         try:
-            # TODO: Return as a message?
-            info['body'] = json.loads(info['body'])
-            response = {}
-        except Exception:
-            module.fail_json(changed=False, msg="Error parsing digitalocean response", response=response, info=info)
+            return json.loads(self.body)
+        except ValueError:
+            return None
+
+    @property
+    def status_code(self):
+        return self.info["status"]
+
+
+class Rest(object):
+
+    def __init__(self, module, headers):
+        self.module = module
+        self.headers = headers
+        self.baseurl = 'https://api.digitalocean.com/v2'
+
+    def _url_builder(self, path):
+        if path[0] == '/':
+            path = path[1:]
+        return '%s/%s' % (self.baseurl, path)
+
+    def send(self, method, path, data=None, headers=None):
+        url = self._url_builder(path)
+        data = self.module.jsonify(data)
+
+        resp, info = fetch_url(self.module, url, data=data, headers=self.headers, method=method)
+
+        return Response(resp, info)
+
+    def get(self, path, data=None, headers=None):
+        return self.send('GET', path, data, headers)
+
+    def put(self, path, data=None, headers=None):
+        return self.send('PUT', path, data, headers)
+
+    def post(self, path, data=None, headers=None):
+        return self.send('POST', path, data, headers)
+
+    def delete(self, path, data=None, headers=None):
+        return self.send('DELETE', path, data, headers)
+
+
+def core(module):
+    api_token = module.params['oauth_token']
+    state = module.params['state']
+    ip = module.params['ip']
+    droplet_id = module.params['droplet_id']
+    region = module.params['region']
+
+    rest = Rest(module, {'Authorization': 'Bearer {}'.format(api_token),
+                         'Content-type': 'application/json'})
+
+    if state in ('present'):
+        if module.params['droplet_id'] is not None and module.params['ip'] is not None:
+            # Lets try to associate the ip to the specified droplet
+            result = associate_floating_ips(module, rest)
+        else:
+            result = create_floating_ips(module, rest)
+
+    elif state in ('absent'):
+        response = rest.delete("floating_ips/{}".format(ip))
+        status_code = response.status_code
+        json = response.json
+        if status_code == 204:
+            module.exit_json(changed=True)
+        elif status_code == 404:
+            module.exit_json(changed=False)
+        else:
+            module.exit_json(changed=False, data=json)
+
+
+def get_floating_ip_details(module, rest):
+    ip = module.params['ip']
+
+    response = rest.get("floating_ips/{}".format(ip))
+    status_code = response.status_code
+    json = response.json
+    if status_code == 200:
+        return response.json['floating_ip']
     else:
-        try:
-            response = response.read()
-            response = json.loads(response)
-        except Exception:
-            module.fail_json(changed=False, msg="Error parsing digitalocean response", response=response, info=info)
+        module.fail_json(msg="Error assigning floating ip [{}: {}]".format(
+            status_code, response.json["message"]), region=module.params['region'])
 
-    return info, response
 
-def get_floating_ip_details(module):
+def assign_floating_id_to_droplet(module, rest):
     ip = module.params['ip']
 
-    info, response = digitalocean_api_call(module, "GET", "https://api.digitalocean.com/v2/floating_ips/%s" % (ip))
-
-    status_code = info['status']
-
-    if status_code == 404:
-        body_id = info['body']['id']
-        if body_id == "not_found":
-            module.fail_json(changed=False,
-                             msg="Floating IP does not exist!",
-                             response=response,
-                             info=info,
-                            )
-
-    if status_code != 200:
-        status_code = status_code
-        module.fail_json(changed=False, msg="unknow response: HTTP status_code=%s" % (status_code), response=response, info=info)
-
-    return response['floating_ip']
-
-def assign_floating_id_to_droplet(module):
-    ip = module.params['ip']
-
-    data = {
+    payload = {
         "type": "assign",
         "droplet_id": module.params['droplet_id'],
     }
 
-    # Floating IP is pointing to the wrong Droplet, update it!
-    info, response = digitalocean_api_call(module, "POST", "https://api.digitalocean.com/v2/floating_ips/%s/actions" % (ip), data)
-
-    status_code = info['status']
-
+    response = rest.post("floating_ips/{}/actions".format(ip), data=payload)
+    status_code = response.status_code
+    json = response.json
     if status_code == 201:
-        changed = True
-    elif status_code == 422:
-        unprocessable_entity = info['body']['id']
-        if unprocessable_entity == "unprocessable_entity":
-            message = info['body']['message']
-            if message == "Droplet can't be blank":
-                module.fail_json(changed=False,
-                                 msg="This returned '%s', provided Droplet ID doesn't exists!" % (message),
-                                 response=response,
-                                 info=info,
-                                )
-            else:
-                module.fail_json(changed=False,
-                                 msg="Droplet have already a Floating IP assigned!",
-                                 response=response,
-                                 info=info,
-                                )
-        else:
-            module.fail_json(changed=False,
-                             msg="Unknown id on body response: id=%s" % (unprocessable_entity),
-                             response=response,
-                             info=info,
-                            )
+        module.exit_json(changed=True, data=response.json)
     else:
-        module.fail_json(changed=False, msg="Error parsing digitalocean response", response=response, info=info)
+        module.fail_json(msg="Error creating floating ip [{}: {}]".format(
+            status_code, response.json["message"]), region=module.params['region'])
 
-    return info, response, changed
-
-def associate_floating_ips(module):
-    floating_ip = get_floating_ip_details(module)
+def associate_floating_ips(module, rest):
+    floating_ip = get_floating_ip_details(module, rest)
     droplet = floating_ip['droplet']
 
-    # If already assigned to a droplet verify if is one of the specified as valid
+    # TODO: If already assigned to a droplet verify if is one of the specified as valid
     if droplet is not None and str(droplet['id']) in [module.params['droplet_id']]:
-        changed = False
+        module.exit_json(changed=False)
     else:
-        info, response, changed = assign_floating_id_to_droplet(module)
+        assign_floating_id_to_droplet(module, rest)
 
-    response = {
-        "changed": changed,
-        "http_response": response,
-    }
-
-    return response
-
-def create_floating_ips(module):
-    changed = False
-    data = {
+def create_floating_ips(module, rest):
+    payload = {
     }
 
     if module.params['region'] is not None:
-        data["region"] = module.params['region']
+        payload["region"] = module.params['region']
     if module.params['droplet_id'] is not None:
-        data["droplet_id"] = module.params['droplet_id']
+        payload["droplet_id"] = module.params['droplet_id']
 
-    # How to detect if "this" Floating IP is already created? tags are only supported for droplets -.-'
-    info, response = digitalocean_api_call(module, "POST", "https://api.digitalocean.com/v2/floating_ips", data)
-
-    status_code = info['status']
-
+    response = rest.post("floating_ips", data=payload)
+    status_code = response.status_code
+    json = response.json
     if status_code == 202:
-        changed = True
-
-    response = {
-        "changed": changed,
-        "http_response": response,
-        "public_ip": response['floating_ip']['ip'],
-    }
-
-    return response
-
-
-def delete_floating_ips(module):
-    url = "https://api.digitalocean.com/v2/floating_ips/%s" % (module.params['ip'])
-    info, response = digitalocean_api_call(module, "DELETE", url)
-
-    status_code = info['status']
-    changed = False
-
-    # sucesseful DELETE will return HTTP 204 status code
-    if status_code == 204:
-        changed = True
-    # Floating IP not found!
-    elif status_code == 404:
-        # If id is not_found, awesome! if not... dammit!
-        body_id = info['body']['id']
-        if body_id != "not_found":
-            module.fail_json(changed=False,
-                             msg="Unknown id on body response: id=%s" % (body_id),
-                             response=response,
-                             info=info,
-                            )
-        # Is not present so, is not changed but is as expected!
+        module.exit_json(changed=True, data=response.json)
     else:
-        module.fail_json(changed=False, msg="unknow response: HTTP status_code=%s" % (status_code), response=response, info=info)
+        module.fail_json(msg="Error creating floating ip [{}: {}]".format(
+            status_code, response.json["message"]), region=module.params['region'])
 
-    return {
-        "changed": changed,
-        "http_response": response,
-    }
 
 def main():
     module = AnsibleModule(
@@ -270,21 +297,10 @@ def main():
         ),
     )
 
-    if module.params['state'] == 'absent':
-        result = delete_floating_ips(module)
-    elif module.params['state'] == 'present':
-        if module.params['droplet_id'] is not None and module.params['ip'] is not None:
-            # Lets try to associate the ip to the specified droplet
-            result = associate_floating_ips(module)
-        else:
-            result = create_floating_ips(module)
-    else:
-        module.fail_json(changed=False, msg="Unknown action!")
+    try:
+        core(module)
+    except Exception as e:
+        module.fail_json(msg=str(e))
 
-    module.exit_json(**result)
-
-# import module snippets
-from ansible.module_utils.urls import *
-from ansible.module_utils.basic import *
 if __name__ == '__main__':
     main()
