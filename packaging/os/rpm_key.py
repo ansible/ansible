68c78cf40dfe19bccf89fdc6c3896ef1d84edb93
Sam Doran <sdoran@ansible.com>
2016-10-12 18:06:26 -0400

    Change example syntax on rpm_key module

--- packaging/os/rpm_key.py
+++ packaging/os/rpm_key.py
@@ -52,13 +52,19 @@ options:
 
 EXAMPLES = '''
 # Example action to import a key from a url
-- rpm_key: state=present key=http://apt.sw.be/RPM-GPG-KEY.dag.txt
+- rpm_key:
+    state: present
+    key: 'http://apt.sw.be/RPM-GPG-KEY.dag.txt'
 
 # Example action to import a key from a file
-- rpm_key: state=present key=/path/to/key.gpg
+- rpm_key:
+    state: present
+    key: /path/to/key.gpg
 
 # Example action to ensure a key is not present in the db
-- rpm_key: state=absent key=DEADB33F
+- rpm_key:
+    state: absent
+    key: DEADB33F
 '''
 import re
 import os.path
