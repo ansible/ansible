2bb5ab9776998f7753b9b0623ed7912f734a5da4
Sam Doran <sdoran@ansible.com>
2016-10-13 09:36:56 -0400

    Change example syntax on pip module

--- lib/ansible/modules/packaging/language/pip.py
+++ lib/ansible/modules/packaging/language/pip.py
@@ -142,46 +142,73 @@ author: "Matt Wright (@mattupstate)"
 
 EXAMPLES = '''
 # Install (Bottle) python package.
-- pip: name=bottle
+- pip:
+    name: bottle
 
 # Install (Bottle) python package on version 0.11.
-- pip: name=bottle version=0.11
+- pip:
+    name: bottle
+    version: 0.11
 
 # Install (MyApp) using one of the remote protocols (bzr+,hg+,git+,svn+). You do not have to supply '-e' option in extra_args.
-- pip: name='svn+http://myrepo/svn/MyApp#egg=MyApp'
+- pip:
+    name: 'svn+http://myrepo/svn/MyApp#'
+    egg: MyApp'
 
 # Install MyApp using one of the remote protocols (bzr+,hg+,git+) in a non editable way.
-- pip: name='git+http://myrepo/app/MyApp' editable=false
+- pip:
+    name: 'git+http://myrepo/app/MyApp'
+    editable: false
 
 # Install (MyApp) from local tarball
-- pip: name='file:///path/to/MyApp.tar.gz'
+- pip:
+    name: 'file:///path/to/MyApp.tar.gz'
 
 # Install (Bottle) into the specified (virtualenv), inheriting none of the globally installed modules
-- pip: name=bottle virtualenv=/my_app/venv
+- pip:
+    name: bottle
+    virtualenv: /my_app/venv
 
 # Install (Bottle) into the specified (virtualenv), inheriting globally installed modules
-- pip: name=bottle virtualenv=/my_app/venv virtualenv_site_packages=yes
+- pip:
+    name: bottle
+    virtualenv: /my_app/venv
+    virtualenv_site_packages: yes
 
 # Install (Bottle) into the specified (virtualenv), using Python 2.7
-- pip: name=bottle virtualenv=/my_app/venv virtualenv_command=virtualenv-2.7
+- pip:
+    name: bottle
+    virtualenv: /my_app/venv
+    virtualenv_command: virtualenv-2.7
 
 # Install specified python requirements.
-- pip: requirements=/my_app/requirements.txt
+- pip:
+    requirements: /my_app/requirements.txt
 
 # Install specified python requirements in indicated (virtualenv).
-- pip: requirements=/my_app/requirements.txt virtualenv=/my_app/venv
+- pip:
+    requirements: /my_app/requirements.txt
+    virtualenv: /my_app/venv
 
 # Install specified python requirements and custom Index URL.
-- pip: requirements=/my_app/requirements.txt extra_args='-i https://example.com/pypi/simple'
+- pip:
+    requirements: /my_app/requirements.txt
+    extra_args: '-i https://example.com/pypi/simple'
 
 # Install (Bottle) for Python 3.3 specifically,using the 'pip-3.3' executable.
-- pip: name=bottle executable=pip-3.3
+- pip:
+    name: bottle
+    executable: pip-3.3
 
 # Install (Bottle), forcing reinstallation if it's already installed
-- pip: name=bottle state=forcereinstall
+- pip:
+    name: bottle
+    state: forcereinstall
 
 # Install (Bottle) while ensuring the umask is 0022 (to ensure other users can use it)
-- pip: name=bottle umask=0022
+- pip:
+    name: bottle
+    umask: 0022
   become: True
 '''
 
