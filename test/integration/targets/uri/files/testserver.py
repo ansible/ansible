6a75726eff31961a4ce1255c0b03ed96bb76daf3
Toshio Kuratomi <a.badger@gmail.com>
2016-10-23 22:14:46 -0700

    Fix the uri testserver to run on python3

--- test/integration/targets/uri/files/testserver.py
+++ test/integration/targets/uri/files/testserver.py
@@ -1,7 +1,17 @@
-import mimetypes
+import sys
 
 if __name__ == '__main__':
-    mimetypes.init()
-    mimetypes.add_type('application/json', '.json')
-    import SimpleHTTPServer
-    SimpleHTTPServer.test()
+    if sys.version_info[0] >= 3:
+        import http.server
+        import socketserver
+        PORT = int(sys.argv[1])
+        Handler = http.server.SimpleHTTPRequestHandler
+        httpd = socketserver.TCPServer(("", PORT), Handler)
+        httpd.serve_forever()
+    else:
+        import mimetypes
+        mimetypes.init()
+        mimetypes.add_type('application/json', '.json')
+        import SimpleHTTPServer
+        SimpleHTTPServer.test()
+
