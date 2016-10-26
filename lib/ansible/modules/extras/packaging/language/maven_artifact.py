39e3c530c009c5ea658be617b016b71745557c79
Michal Klempa <michal.klempa@gmail.com>
2016-10-26 21:39:14 +0200

    maven_artifact: fix download of SBT published snapshot artifact (#3085)
    Fixes #1717

--- lib/ansible/modules/extras/packaging/language/maven_artifact.py
+++ lib/ansible/modules/extras/packaging/language/maven_artifact.py
@@ -222,6 +222,9 @@ class MavenDownloader:
             xml = self._request(self.base + path, "Failed to download maven-metadata.xml", lambda r: etree.parse(r))
             timestamp = xml.xpath("/metadata/versioning/snapshot/timestamp/text()")[0]
             buildNumber = xml.xpath("/metadata/versioning/snapshot/buildNumber/text()")[0]
+            for snapshotArtifact in xml.xpath("/metadata/versioning/snapshotVersions/snapshotVersion"):
+                if len(snapshotArtifact.xpath("classifier/text()")) > 0 and snapshotArtifact.xpath("classifier/text()")[0] == artifact.classifier and len(snapshotArtifact.xpath("extension/text()")) > 0 and snapshotArtifact.xpath("extension/text()")[0] == artifact.extension:
+                    return self._uri_for_artifact(artifact, snapshotArtifact.xpath("value/text()")[0])
             return self._uri_for_artifact(artifact, artifact.version.replace("SNAPSHOT", timestamp + "-" + buildNumber))
 
         return self._uri_for_artifact(artifact, artifact.version)
