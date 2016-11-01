64d539549436501f375590dc615e09385f4bdb57
Ievgen Khmelenko <evgeny@pragmaticplay.com>
2016-11-01 17:46:20 +0200

    GPL v3 license preamble, ImportError

--- lib/ansible/plugins/callback/logstash.py
+++ lib/ansible/plugins/callback/logstash.py
@@ -1,4 +1,19 @@
 # (C) 2016, Ievgen Khmelenko <ujenmr@gmail.com>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
@@ -9,7 +24,12 @@ import socket
 import uuid
 
 import logging
-import logstash
+
+try:
+    import logstash
+    HAS_LOGSTASH = True
+except ImportError:
+    HAS_LOGSTASH = False
 
 from ansible.plugins.callback import CallbackBase
 
@@ -22,12 +42,15 @@ class CallbackModule(CallbackBase):
     and put the plugin in <path_to_callback_plugins_folder>
 
     logstash config:
-    input {
-        tcp {
-            port => 5000
-            codec => json
+        input {
+            tcp {
+                port => 5000
+                codec => json
+            }
         }
-    }
+
+    Requires:
+        python-logstash
 
     This plugin makes use of the following environment variables:
         LOGSTASH_SERVER   (optional): defaults to localhost
@@ -42,20 +65,25 @@ class CallbackModule(CallbackBase):
     def __init__(self):
         super(CallbackModule, self).__init__()
 
-        self.logger =  logging.getLogger('python-logstash-logger')
-        self.logger.setLevel(logging.DEBUG)
+        if not HAS_LOGSTASH:
+            self.disabled = True
+            self._display.warning("The required python-logstash is not installed. "
+                "pip install python-logstash")
+        else:
+            self.logger =  logging.getLogger('python-logstash-logger')
+            self.logger.setLevel(logging.DEBUG)
 
-        self.handler = logstash.TCPLogstashHandler(
-            os.getenv('LOGSTASH_SERVER', 'localhost'),
-            int(os.getenv('LOGSTASH_PORT', 5000)),
-            version=1,
-            message_type=os.getenv('LOGSTASH_TYPE', 'ansible')
-        )
+            self.handler = logstash.TCPLogstashHandler(
+                os.getenv('LOGSTASH_SERVER', 'localhost'),
+                int(os.getenv('LOGSTASH_PORT', 5000)),
+                version=1,
+                message_type=os.getenv('LOGSTASH_TYPE', 'ansible')
+            )
 
-        self.logger.addHandler(self.handler)
-        self.hostname = socket.gethostname()
-        self.session = str(uuid.uuid1())
-        self.errors = 0
+            self.logger.addHandler(self.handler)
+            self.hostname = socket.gethostname()
+            self.session = str(uuid.uuid1())
+            self.errors = 0
 
     def v2_playbook_on_start(self, playbook):
         self.playbook = playbook._file_name
