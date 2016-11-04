bb3801bafd2886bf04f7903245b8e82e151f105e
Matt Clay <matt@mystile.com>
2016-11-03 23:30:27 -0700

    Cleanup and fixes for code smell scripts.
    - Fix shellcheck issues.
    - Add .tox exclusions.

--- test/sanity/code-smell/no-basestring.sh
+++ test/sanity/code-smell/no-basestring.sh
@@ -6,10 +6,18 @@ BASEDIR=${1-"."}
 # * basestring is still present and harmless in comments
 # * basestring is also currently present in modules.  Porting of modules is more
 #   of an Ansible 2.3 or greater goal.
-BASESTRING_USERS=$(grep -r basestring $BASEDIR |grep isinstance| grep -v lib/ansible/compat/six/_six.py|grep -v lib/ansible/module_utils/six.py|grep -v lib/ansible/modules/core|grep -v lib/ansible/modules/extras)
+BASESTRING_USERS=$(grep -r basestring "${BASEDIR}" \
+    | grep isinstance \
+    | grep -v \
+    -e lib/ansible/compat/six/_six.py \
+    -e lib/ansible/module_utils/six.py \
+    -e lib/ansible/modules/core \
+    -e lib/ansible/modules/extras \
+    -e '/.tox/' \
+    )
 
-if test -n "$BASESTRING_USERS" ; then
-  printf "$BASESTRING_USERS"
+if test -n "${BASESTRING_USERS}"; then
+  printf "%s" "${BASESTRING_USERS}"
   exit 1
 else
   exit 0
