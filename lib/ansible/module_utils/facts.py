76308c55eeaf2f4bf367425985e77cf67aea7cb0
Michael Scherer <misc@zarb.org>
2016-11-06 15:41:32 +0100

    Add support for OpenBSD dmi fact gathering

--- lib/ansible/module_utils/facts.py
+++ lib/ansible/module_utils/facts.py
@@ -1601,6 +1601,7 @@ class OpenBSDHardware(Hardware):
         self.get_processor_facts()
         self.get_device_facts()
         self.get_mount_facts()
+        self.get_dmi_facts()
         return self.facts
 
     def get_sysctl(self):
