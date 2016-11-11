b89cfe42ef5cf2c70b0f92c5cc7b9c310979a8b1
Matthieu Dolci <matthieu.dolci@gmail.com>
2016-11-11 14:33:18 -0800

    ec2_customer_gateway: state present expect bgp_arn instead of bgp_asn (#3366)

--- cloud/amazon/ec2_customer_gateway.py
+++ cloud/amazon/ec2_customer_gateway.py
@@ -204,7 +204,7 @@ def main():
     module = AnsibleModule(argument_spec=argument_spec,
                            supports_check_mode=True,
                            required_if=[
-                               ('state', 'present', ['bgp_arn'])
+                               ('state', 'present', ['bgp_asn'])
                            ]
                            )
 
