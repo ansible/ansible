f97e9e06a0d4b7e1cdb5deaee51f1528800705ea
Rene Moser <mail@renemoser.net>
2016-11-11 11:41:05 +0100

    cloudstack: add additional CLOUDSTACK_VPC env var

--- lib/ansible/module_utils/cloudstack.py
+++ lib/ansible/module_utils/cloudstack.py
@@ -217,6 +217,8 @@ class AnsibleCloudStack(object):
 
         vpc = self.module.params.get('vpc')
         if not vpc:
+            vpc = os.environ.get('CLOUDSTACK_VPC')
+        if not vpc:
             return None
 
         args = {
