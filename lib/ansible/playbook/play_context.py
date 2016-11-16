2291163a7a37876fde2b63746ca52aeba0bb7e2a
Andrea Tartaglia <me@andreatartaglia.com>
2016-11-16 10:38:43 +0000

    Added DIFF_ALWAYS constant
    When set to True, will always print the diff. Defaults to False.
    
    Fixes #18416 #16073

--- lib/ansible/playbook/play_context.py
+++ lib/ansible/playbook/play_context.py
@@ -209,7 +209,7 @@ class PlayContext(Base):
     _force_handlers   = FieldAttribute(isa='bool', default=False)
     _start_at_task    = FieldAttribute(isa='string')
     _step             = FieldAttribute(isa='bool', default=False)
-    _diff             = FieldAttribute(isa='bool', default=False)
+    _diff             = FieldAttribute(isa='bool', default=C.DIFF_ALWAYS)
 
     # Fact gathering settings
     _gather_subset    = FieldAttribute(isa='string', default=C.DEFAULT_GATHER_SUBSET)
