eef8ac42a7ba4b9b3769d6a67e25b26cf42c4f43
Saravanan K R <krsacme@users.noreply.github.com>
2016-11-18 22:45:28 +0530

    Update the code fragment contents to have correct format (#3342)

--- lib/ansible/modules/extras/cloud/atomic/atomic_image.py
+++ lib/ansible/modules/extras/cloud/atomic/atomic_image.py
@@ -26,7 +26,7 @@ description:
 version_added: "2.2"
 author: "Saravanan KR @krsacme"
 notes:
-    - Host should be support ```atomic``` command
+    - Host should be support C(atomic) command
 requirements:
   - atomic
   - "python >= 2.6"
@@ -39,7 +39,7 @@ options:
     state:
         description:
           - The state of the container image.
-          - The state ```latest``` will ensure container image is upgraded to the latest version and forcefully restart container, if running.
+          - The state C(latest) will ensure container image is upgraded to the latest version and forcefully restart container, if running.
         required: False
         choices: ["present", "absent", "latest"]
         default: latest
