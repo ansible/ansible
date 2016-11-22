04856cac1866435e837bca1496a0c298a680b6c5
Matt Clay <matt@mystile.com>
2016-11-22 14:59:05 -0800

    Add comment about re-implementing async_wrapper.

--- lib/ansible/plugins/action/async.py
+++ lib/ansible/plugins/action/async.py
@@ -91,6 +91,7 @@ class ActionModule(ActionBase):
 
         # call the interpreter for async_wrapper directly
         # this permits use of a script for an interpreter on non-Linux platforms
+        # TODO: re-implement async_wrapper as a regular module to avoid this special case
         interpreter = shebang.replace('#!', '')
         async_cmd = [interpreter, remote_async_module_path, async_jid, async_limit, remote_module_path]
 
