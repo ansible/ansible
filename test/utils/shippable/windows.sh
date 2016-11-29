d7c5c0a2bbbe305ace57468cb95b4f01833fa85e
Matt Clay <matt@mystile.com>
2016-11-28 23:56:36 -0800

    Combine tests for Windows specific changes.

--- test/utils/shippable/windows.sh
+++ test/utils/shippable/windows.sh
@@ -5,14 +5,22 @@ set -o pipefail
 declare -a args
 IFS='/:' read -ra args <<< "${TEST}"
 
-target="windows/ci/group${args[1]}/"
+job="${args[1]}"
 
 ansible-test windows-integration --explain 2>&1 | grep ' windows-integration: .* (targeted)$' > /tmp/windows.txt
 
 if [ -s /tmp/windows.txt ]; then
     echo "Detected changes requiring integration tests specific to Windows:"
     cat /tmp/windows.txt
+
+    if [ "${job}" != "1" ]; then
+        echo "Nothing to do, all Windows tests will run under TEST=windows/1 instead."
+        exit 0
+    fi
+
     echo "Running Windows integration tests for multiple versions concurrently."
+
+    target="windows/ci/"
 
     ansible-test windows-integration --color -v --retry-on-error "${target}" --requirements \
         --windows 2008-SP2 \
@@ -23,6 +31,8 @@ if [ -s /tmp/windows.txt ]; then
 else
     echo "No changes requiring integration tests specific to Windows were detected."
     echo "Running Windows integration tests for a single version only."
+
+    target="windows/ci/group${job}/"
 
     ansible-test windows-integration --color -v --retry-on-error "${target}" --requirements \
         --windows 2012-R2_RTM
