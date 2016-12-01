ea23276bd0556a7827dd382b716b404aa36da7b6
Laurent Mazuel <lmazuel@microsoft.com>
2016-11-30 16:44:40 -0800

    Fix bug 17934

--- contrib/inventory/azure_rm.py
+++ contrib/inventory/azure_rm.py
@@ -362,7 +362,10 @@ class AzureRM(object):
             resource_client = self.rm_client
             resource_client.providers.register(key)
         except Exception as exc:
-            self.fail("One-time registration of {0} failed - {1}".format(key, str(exc)))
+            self.log("One-time registration of {0} failed - {1}".format(key, str(exc)))
+            self.log("You might need to register {0} using an admin account".format(key))
+            self.log(("To register a provider using the Python CLI: "
+                      "https://docs.microsoft.com/cli/azure/provider#register"))
 
     @property
     def network_client(self):
