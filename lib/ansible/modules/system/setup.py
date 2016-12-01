e3a43c0d4bdb34be26746b20b50a9b3e8ca768d2
Brian Coca <bcoca@users.noreply.github.com>
2016-12-01 18:48:47 -0500

    made fact_path actual path (#5789)

--- lib/ansible/modules/system/setup.py
+++ lib/ansible/modules/system/setup.py
@@ -119,7 +119,7 @@ def main():
             gather_subset=dict(default=["all"], required=False, type='list'),
             gather_timeout=dict(default=10, required=False, type='int'),
             filter=dict(default="*", required=False),
-            fact_path=dict(default='/etc/ansible/facts.d', required=False),
+            fact_path=dict(default='/etc/ansible/facts.d', required=False, type='path'),
         ),
         supports_check_mode = True,
     )
