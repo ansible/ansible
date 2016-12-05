b53e8906ec7feecde30cac6d83eb4b15b85acacb
Fabio Alessandro Locati <me@fale.io>
2016-12-05 18:00:18 +0000

    Call main in conditional way - packaging (#5826)

--- lib/ansible/modules/packaging/os/apt_repository.py
+++ lib/ansible/modules/packaging/os/apt_repository.py
@@ -556,4 +556,5 @@ def main():
 from ansible.module_utils.basic import *
 from ansible.module_utils.urls import *
 
-main()
+if __name__ == '__main__':
+    main()
--- lib/ansible/modules/packaging/os/rhn_channel.py
+++ lib/ansible/modules/packaging/os/rhn_channel.py
@@ -171,5 +171,6 @@ def main():
 
 # import module snippets
 from ansible.module_utils.basic import *
-main()
 
+if __name__ == '__main__':
+    main()
