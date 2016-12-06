5daab586824201712ff389a302a47f6c5f3fd69e
Pete Travis <immanetize@fedoraproject.org>
2016-12-06 02:41:08 -0600

    device path is optional for cbs
    per
    https://developer.rackspace.com/docs/cloud-servers/v2/api-reference/svr-basic-operations/#post-attach-volume-to-server-servers-server-id-os-volume-attachments
    , device is "The name of the device, such as /dev/xvdb. Specify null for
    auto- assignment."

--- lib/ansible/modules/cloud/rackspace/rax_cbs_attachments.py
+++ lib/ansible/modules/cloud/rackspace/rax_cbs_attachments.py
@@ -33,7 +33,7 @@ options:
     description:
       - The device path to attach the volume to, e.g. /dev/xvde
     default: null
-    required: true
+    required: false
   volume:
     description:
       - Name or id of the volume to attach/detach
@@ -189,7 +189,7 @@ def main():
     argument_spec = rax_argument_spec()
     argument_spec.update(
         dict(
-            device=dict(required=True),
+            device=dict(required=False),
             volume=dict(required=True),
             server=dict(required=True),
             state=dict(default='present', choices=['present', 'absent']),
