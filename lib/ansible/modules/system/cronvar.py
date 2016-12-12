8293cdb563fff003a717ca6c8815ac86af84c632
Michael Kwardakov <kwardakov+github@gmail.com>
2016-12-12 16:00:01 +0300

    Update cronvar.py

--- lib/ansible/modules/system/cronvar.py
+++ lib/ansible/modules/system/cronvar.py
@@ -135,8 +135,6 @@ class CronVar(object):
     def __init__(self, module, user=None, cron_file=None):
         self.module = module
         self.user = user
-        if self.user is None:
-            self.user = 'root'
         self.lines = None
         self.wordchars = ''.join(chr(x) for x in range(128) if chr(x) not in ('=', "'", '"', ))
 
