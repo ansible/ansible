66bff1a141a781c4dcd4c9dcc0bb938320311ce6
Toshio Kuratomi <a.badger@gmail.com>
2016-12-13 16:39:03 -0800

    Add a code-smell test for iterkeys (#18589)
    * Add a code-smell test for iterkeys
    * Add a message on how to port iterkeys problems and make the grep more robust

---
+++ test/sanity/code-smell/no-iterkeys.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ITERKEYS_USERS=$(grep -r -I iterkeys . \
+    --exclude-dir .git \
+    --exclude-dir .tox \
+    --exclude-dir docsite \
+    | grep -v \
+    -e lib/ansible/compat/six/_six.py \
+    -e lib/ansible/module_utils/six.py \
+    -e test/sanity/code-smell/no-iterkeys.sh \
+    -e '^[^:]*:#'
+    )
+
+if [ "${ITERKEYS_USERS}" ]; then
+    echo 'iterkeys has been removed in python3.  Use "for KEY in DICT:" instead'
+    echo "${ITERKEYS_USERS}"
+    exit 1
+fi
