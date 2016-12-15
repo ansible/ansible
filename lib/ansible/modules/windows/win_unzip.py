05107cbe0bec684839973db49fb2d1638b67926f
Fabio Alessandro Locati <me@fale.io>
2016-12-15 19:37:02 +0100

    Remove unneeded quotes - windows/win_unzip.py (#18827)

--- lib/ansible/modules/windows/win_unzip.py
+++ lib/ansible/modules/windows/win_unzip.py
@@ -98,13 +98,13 @@ $ ansible -i hosts -m win_unzip -a "src=C:\LibraryToUnzip.zip dest=C:\Lib rm=tru
 # Install PSCX to use for extracting a gz file
   - name: Grab PSCX msi
     win_get_url:
-      url: 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pscx&DownloadId=923562&FileTime=130585918034470000&Build=20959'
-      dest: 'C:\pscx.msi'
+      url: http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pscx&DownloadId=923562&FileTime=130585918034470000&Build=20959
+      dest: C:\pscx.msi
   - name: Install PSCX
     win_msi:
-      path: 'C:\pscx.msi'
+      path: C:\pscx.msi
   - name: Unzip gz log
     win_unzip:
-      src: "C:\Logs\application-error-logs.gz"
-      dest: "C:\ExtractedLogs\application-error-logs"
+      src: C:\Logs\application-error-logs.gz
+      dest: C:\ExtractedLogs\application-error-logs
 '''
