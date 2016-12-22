3e72e0c173144df5c4986b00c60e0c307da2fb3b
Gordon Gao <gordon@expressvpn.com>
2016-12-22 16:19:50 +0800

    let chdir support relative path in more modules (#16736)
    (cherry picked from commit d9e1e374b249b568b7b7376b5cc5586327a9a206)

--- lib/ansible/module_utils/basic.py
+++ lib/ansible/module_utils/basic.py
@@ -2198,14 +2198,13 @@ class AnsibleModule(object):
             stderr=subprocess.PIPE,
         )
 
-        if cwd and os.path.isdir(cwd):
-            kwargs['cwd'] = cwd
-
         # store the pwd
         prev_dir = os.getcwd()
 
         # make sure we're in the right working directory
         if cwd and os.path.isdir(cwd):
+            cwd = os.path.abspath(os.path.expanduser(cwd))
+            kwargs['cwd'] = cwd
             try:
                 os.chdir(cwd)
             except (OSError, IOError):
@@ -2217,7 +2216,6 @@ class AnsibleModule(object):
             old_umask = os.umask(umask)
 
         try:
-
             if self._debug:
                 self.log('Executing: ' + clean_args)
             cmd = subprocess.Popen(args, **kwargs)
