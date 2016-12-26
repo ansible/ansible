14c235cc0db2d19585eea1ca76f2eb69754b9189
Joern Heissler <github@joern.heissler.de>
2016-12-26 18:44:44 +0100

    Add 3 new Amazon AWS regions

--- lib/ansible/modules/cloud/amazon/ec2_facts.py
+++ lib/ansible/modules/cloud/amazon/ec2_facts.py
@@ -70,10 +70,13 @@ class Ec2Metadata(object):
                    'ap-southeast-1',
                    'ap-southeast-2',
                    'ap-south-1',
+                   'ca-central-1',
                    'eu-central-1',
                    'eu-west-1',
+                   'eu-west-2',
                    'sa-east-1',
                    'us-east-1',
+                   'us-east-2',
                    'us-west-1',
                    'us-west-2',
                    'us-gov-west-1'
