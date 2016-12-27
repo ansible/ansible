8ce65f8fc1a7ef904ec65608637bede8c0301718
THEBAULT Julien <julien@thebault.co>
2016-12-28 00:57:33 +0100

    Check the command result status in easy_install plugin (#16519)

--- lib/ansible/modules/packaging/language/easy_install.py
+++ lib/ansible/modules/packaging/language/easy_install.py
@@ -108,6 +108,8 @@ def _is_package_installed(module, name, easy_install, executable_arguments):
     executable_arguments = executable_arguments + ['--dry-run']
     cmd = '%s %s %s' % (easy_install, ' '.join(executable_arguments), name)
     rc, status_stdout, status_stderr = module.run_command(cmd)
+    if rc:
+        module.fail_json(msg=status_stderr)
     return not ('Reading' in status_stdout or 'Downloading' in status_stdout)
 
 
