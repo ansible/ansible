ae631f384819d4ef3cecbcd1da4f6f43840e0034
Ivan Bojer <ijb@networkrift.com>
2017-01-04 14:38:00 -0800

    new module for mgmt interface management

--- lib/ansible/modules/network/panos/panos_mgtconfig.py
+++ lib/ansible/modules/network/panos/panos_mgtconfig.py
@@ -202,7 +202,9 @@ def main():
 
         if changed and commit:
             xapi.commit(cmd="<commit></commit>", sync=True, interval=1)
-    except PanXapiError as x:
+    except PanXapiError:
+        import sys
+        x = sys.exc_info()[1]
         module.fail_json(msg=x.message)
 
     module.exit_json(changed=changed, msg="okey dokey")
