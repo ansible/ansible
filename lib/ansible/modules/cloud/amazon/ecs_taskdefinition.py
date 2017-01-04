dceace6bb2718ac5e99da58b9989633a0cb71338
Kash <kash.karimi@gmail.com>
2017-01-04 13:50:13 +0000

    ecs_taskdefinition: Fix missing import camel_dict_to_snake_dict (#19560)
    ecs_taskdefinition: Fix missing import camel_dict_to_snake_dict

--- lib/ansible/modules/cloud/amazon/ecs_taskdefinition.py
+++ lib/ansible/modules/cloud/amazon/ecs_taskdefinition.py
@@ -125,8 +125,7 @@ except ImportError:
     HAS_BOTO3 = False
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.ec2 import boto3_conn, ec2_argument_spec, get_aws_connection_info
-
+from ansible.module_utils.ec2 import boto3_conn, camel_dict_to_snake_dict, ec2_argument_spec, get_aws_connection_info
 
 class EcsTaskManager:
     """Handles ECS Tasks"""
