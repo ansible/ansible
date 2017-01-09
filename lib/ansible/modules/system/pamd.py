93798523dea9abfba358c49febee9b4d23c6fe72
Valentín Gutiérrez <valentin.gutierrez@schibsted.com>
2017-01-09 18:24:04 +0100

    Proper handling of parameter=value arguments

--- lib/ansible/modules/system/pamd.py
+++ lib/ansible/modules/system/pamd.py
@@ -408,26 +408,33 @@ def add_module_arguments(service, old_rule, module_args):
                 old_rule.rule_control == rule.rule_control and
                 old_rule.rule_module_path == rule.rule_module_path):
             for arg_to_add in module_args:
-                if arg_to_add not in rule.rule_module_args:
+                if "=" in arg_to_add:
+                    pre_string = arg_to_add[:arg_to_add.index('=')+1]
+                    indicies = [i for i, arg
+                                in enumerate(rule.rule_module_args)
+                                if arg.startswith(pre_string)]
+                    if len(indicies) == 0:
+                        rule.rule_module_args.append(arg_to_add)
+                        changed = True
+                        result['added_arg_'+str(change_count)] = arg_to_add
+                        result['to_rule_'+str(change_count)] = str(rule)
+                        change_count += 1
+                    else:
+                        for i in indicies:
+                            if rule.rule_module_args[i] != arg_to_add:
+                                rule.rule_module_args[i] = arg_to_add
+                                changed = True
+                                result['updated_arg_' +
+                                       str(change_count)] = arg_to_add
+                                result['in_rule_' +
+                                       str(change_count)] = str(rule)
+                                change_count += 1
+                elif arg_to_add not in rule.rule_module_args:
                     rule.rule_module_args.append(arg_to_add)
                     changed = True
                     result['added_arg_'+str(change_count)] = arg_to_add
                     result['to_rule_'+str(change_count)] = str(rule)
                     change_count += 1
-                elif "=" in arg_to_add:
-                    pre_string = arg_to_add[:arg_to_add.index('=')+1]
-                    indicies = [i for i, arg
-                                in enumerate(rule.rule_module_args)
-                                if arg.startswith(pre_string)]
-                    for i in indicies:
-                        if rule.rule_module_args[i] != arg_to_add:
-                            rule.rule_module_args[i] = arg_to_add
-                            changed = True
-                            result['updated_arg_' +
-                                   str(change_count)] = arg_to_add
-                            result['in_rule_' +
-                                   str(change_count)] = str(rule)
-                            change_count += 1
     result['change_count'] = change_count
     return changed, result
 
