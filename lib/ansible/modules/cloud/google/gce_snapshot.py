e68b5981303a3300005a7abb738b94d02c9840e0
Rob Wagner <Rob.Wagner@oncloudone.com>
2017-01-09 13:48:42 -0500

    Fixed using sys.exit in favor of fail_json. Followed the pattern in gce_pd

--- lib/ansible/modules/cloud/google/gce_snapshot.py
+++ lib/ansible/modules/cloud/google/gce_snapshot.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.gce import gce_connect
 
