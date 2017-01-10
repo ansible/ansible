ec99c90b6ac2088c91c67e1cd233e1f6776a4fd6
Computest <anon@@computest.nl>
2017-01-10 16:51:40 -0600

    Fixing another corner case for security related to CVE-2016-9587

--- lib/ansible/vars/unsafe_proxy.py
+++ lib/ansible/vars/unsafe_proxy.py
@@ -98,10 +98,14 @@ class AnsibleJSONUnsafeDecoder(json.JSONDecoder):
 
 
 def _wrap_dict(v):
+    # Create new dict to get rid of the keys that are not wrapped.
+    new = {}
     for k in v.keys():
         if v[k] is not None:
-            v[wrap_var(k)] = wrap_var(v[k])
-    return v
+            new[wrap_var(k)] = wrap_var(v[k])
+        else:
+            new[wrap_var(k)] = None
+    return new
 
 
 def _wrap_list(v):
