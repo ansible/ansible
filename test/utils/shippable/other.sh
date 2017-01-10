9113375bb625d56a1c8c1c3c27e3a820af43af3d
Matt Clay <matt@mystile.com>
2017-01-10 10:30:41 -0800

    Add test support for python 3.6.

--- test/utils/shippable/other.sh
+++ test/utils/shippable/other.sh
@@ -4,9 +4,13 @@ set -o pipefail
 
 add-apt-repository 'deb http://archive.ubuntu.com/ubuntu trusty-backports universe'
 add-apt-repository 'ppa:fkrull/deadsnakes'
+add-apt-repository 'ppa:jonathonf/python-3.6'
 
 apt-get update -qq
-apt-get install python2.4 shellcheck -qq
+apt-get install -qq \
+    shellcheck \
+    python2.4 \
+    python3.6 \
 
 pip install tox --disable-pip-version-check
 
