819d1db4681cc23145d0e87deb53644d8c586953
Rajeshkumar <rajeshkumar27390@gmail.com>
2017-01-11 16:15:25 +0530

    Updated code for new attributes in win_shorcut
    Updated code for new attributes in win_shorcut

--- lib/ansible/modules/windows/win_shortcut.ps1
+++ lib/ansible/modules/windows/win_shortcut.ps1
@@ -19,35 +19,64 @@
 
 $ErrorActionPreference = "Stop"
 $params = Parse-Args $args
+$State = Get-AnsibleParam -obj $params -name "state" -failifempty $true
 $TargetFile = Get-AnsibleParam -obj $params -name "src" -failifempty $true
 $ShortcutFile = Get-AnsibleParam -obj $params -name "dest" -failifempty $true
+$Arguments = Get-AnsibleParam -obj $params -name "arguments" -failempty $true
+$Directory =  Get-AnsibleParam -obj $params -name "directory" -failempty $true
 $Hotkey = Get-AnsibleParam -obj $params -name "hotkey" -failifempty $true
+$Description = Get-AnsibleParam -obj $params -name "desc" -failifempty $true
 $IconLocation = Get-AnsibleParam -obj $params -name "icon" -failifempty $true
 $result = New-Object psobject @{
     changed = $FALSE
 }
 try
 {
- if(!(Test-Path $TargetFile))
+ if ($State -eq "absent")
  {
-  Fail-Json (New-Object psobject) "missing required argument: Provide valid exe path"
- }
+   if(Test-Path $ShortcutFile)
+   {
+     Remove-Item -Path "$ShortcutFile";
+     $result.changed = $TRUE
+   }
+   else
+    {
+     Fail-Json (New-Object psobject) "missing required argument: Provide valid shortcut path to remove"
+    }
+  }
+ elseif($State -eq "present")
+ {
+  if(!(Test-Path $TargetFile))
+  {
+   Fail-Json (New-Object psobject) "missing required argument: Provide valid exe path to create Shortcut"
+  }
  $WScriptShell = New-Object -ComObject WScript.Shell
  $targetPath = $WScriptShell.CreateShortcut($ShortcutFile).TargetPath
  $ShortcutKey = $WScriptShell.CreateShortcut($ShortcutFile).HotKey
  $ShortcutIconloc = $WScriptShell.CreateShortcut($ShortcutFile).IconLocation
- if(($targetPath -eq $TargetFile) -and ($ShortcutKey -eq $Hotkey) -and ($ShortcutIconloc -eq $IconLocation))
- {
- $result.changed = $FALSE
+ $ShortcutDescription =  $WScriptShell.CreateShortcut($ShortcutFile).Description
+ $ShortcutDirectory =  $WScriptShell.CreateShortcut($ShortcutFile).WorkingDirectory
+ $ShortcutArguments = $WscriptShell.CreateShortcut($ShortcutFile).Arguments
+ if(($targetPath -eq $TargetFile) -and ($ShortcutKey -eq $Hotkey) -and ($ShortcutIconloc -eq $IconLocation) -and ($ShortcutDescription -eq $Description) -and ($ShortcutDirectory -eq $Directory) -and ($ShortcutArguments -eq $Arguments))
+  {
+   $result.changed = $FALSE
+  }
+  else
+  {
+   $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+   $Shortcut.TargetPath = $TargetFile
+   $ShortCut.Arguments = $Arguments
+   $Shortcut.WorkingDirectory = $Directory
+   $Shortcut.HotKey = $Hotkey
+   $Shortcut.IconLocation = $IconLocation
+   $Shortcut.Description = $Description
+   $Shortcut.Save()
+   $result.changed = $TRUE
+  }
  }
- else
+else
  {
-  $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-  $Shortcut.TargetPath = $TargetFile
-  $Shortcut.HotKey = $Hotkey
-  $Shortcut.IconLocation = $IconLocation
-  $Shortcut.Save()
-  $result.changed = $TRUE
+  Fail-Json (New-Object psobject) "missing required argument: Provide state either :present or absent"
  }
 }
 catch
