af0b67591160e6b5093f262f8660b0b64df6a8ec
Sergey Mamontov <parfeon@me.com>
2017-01-13 01:44:33 +0200

    . fixed multi-line documentation

--- lib/ansible/modules/cloud/pubnub/pubnub_blocks.py
+++ lib/ansible/modules/cloud/pubnub/pubnub_blocks.py
@@ -80,7 +80,7 @@ options:
   event_handlers:
     description:
       - List of event handlers which should be updated for specified C(block).
-      - Each entry for new event handler should contain: C(name), C(src), C(channels), C(event). C(name) used as event
+      - Each entry for new event handler should contain: C(name), C(src), C(channels), C(event). C(name) used as event \
         handler name which can be used later to make changes to it. C(src) is full path to file with event handler \
         code. C(channels) is name of channel from which event handler is waiting for events. C(event) is type of event \
         which is able to trigger event handler: I(js-before-publish), I(js-after-publish), I(js-after-presence).
@@ -99,7 +99,7 @@ options:
     default: {}
   validate_certs:
     description:
-      - This key allow to try skip certificates check when performing REST API calls. Sometimes host may have issues
+      - This key allow to try skip certificates check when performing REST API calls. Sometimes host may have issues \
         with certificates on it and this will cause problems to call PubNub REST API.
       - If check should be ignored C(False) should be passed to this parameter.
     required: false
