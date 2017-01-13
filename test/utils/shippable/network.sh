7b3c89a5594c90bf56f197b085f19847199f513a
Matt Clay <matt@mystile.com>
2017-01-12 18:06:16 -0800

    Use --requirements instead of --tox.

--- test/utils/shippable/network.sh
+++ test/utils/shippable/network.sh
@@ -2,8 +2,6 @@
 
 set -o pipefail
 
-pip install tox --disable-pip-version-check
-
 ansible-test network-integration --explain 2>&1 | { grep ' network-integration: .* (targeted)$' || true; } > /tmp/network.txt
 
 target="network/ci/"
@@ -14,13 +12,13 @@ if [ -s /tmp/network.txt ]; then
 
     echo "Running network integration tests for multiple platforms concurrently."
 
-    ansible-test network-integration --color -v --retry-on-error "${target}" --tox --python 2.7 \
+    ansible-test network-integration --color -v --retry-on-error "${target}" --requirements \
         --platform vyos/1.1.0 \
 
 else
     echo "No changes requiring integration tests specific to networking were detected."
     echo "Running network integration tests for a single platform only."
 
-    ansible-test network-integration --color -v --retry-on-error "${target}" --tox --python 2.7 \
+    ansible-test network-integration --color -v --retry-on-error "${target}" --requirements \
         --platform vyos/1.1.0
 fi
