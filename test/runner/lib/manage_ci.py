b5d5888b6e57fb08a784652e1a1e0daa6484d16b
Matt Clay <matt@mystile.com>
2017-01-16 14:33:52 -0800

    Add support for ios to ansible-test.

--- test/runner/lib/manage_ci.py
+++ test/runner/lib/manage_ci.py
@@ -75,6 +75,7 @@ class ManageNetworkCI(object):
             'ansible_port=%s' % self.core_ci.connection.port,
             'ansible_connection=network_cli',
             'ansible_ssh_private_key_file=%s' % self.core_ci.ssh_key.key,
+            'ansible_network_os=%s' % self.core_ci.platform,
         ]
 
         name = '%s-%s' % (self.core_ci.platform, self.core_ci.version.replace('.', '_'))
