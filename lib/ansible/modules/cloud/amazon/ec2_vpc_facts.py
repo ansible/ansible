2555d29a79f6cc00c424a141a88be837af02d2e5
Will Thames <will.thames@xvt.com.au>
2017-01-17 17:10:52 +1000

    Update ec2_vpc_facts to meet latest standards
    Improve documentation
    Meet flake8 standards
    Make return data structure more pythonic
    Reuse shared ec2 code rather than reimplement
    Update for python3

--- lib/ansible/modules/cloud/amazon/ec2_vpc_facts.py
+++ lib/ansible/modules/cloud/amazon/ec2_vpc_facts.py
@@ -13,10 +13,38 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# import module snippets
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.ec2 import get_aws_connection_info, boto3_conn
+from ansible.module_utils.ec2 import ec2_argument_spec, HAS_BOTO3
+from ansible.module_utils.ec2 import boto3_tag_list_to_ansible_dict
+from ansible.module_utils.ec2 import ansible_dict_to_boto3_filter_list
+
 DOCUMENTATION = '''
 module: ec2_vpc_facts
-short_description: Describe vpcs given a set of filters 
+short_description: Gathers facts about AWS VPCs
+description:
+  - Gather facts about AWS VPCs
+version_added: "2.3"
+author: "Mike Mochan (@mmochan)"
+requires: [ boto3 ]
+options:
+  filters:
+    description:
+      - >-
+           A dict of filters to apply. Each dict item consists of a filter key
+           and a filter value. See
+           U(http://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DescribeVpcs.html)
+           for possible filters. Filter names and values are case sensitive.
+    required: false
+    default: {}
+notes:
+  - By default, the module will return all VPCs
 
+extends_documentation_fragment:
+  - aws
+  - ec2
 '''
 
 EXAMPLES = '''
@@ -30,44 +58,28 @@ register: ec2_vpc_facts
 
 
 try:
-    import json
     import botocore
-    import boto3    
-    HAS_BOTO3 = True
 except ImportError:
-    HAS_BOTO3 = False
+    pass  # will be picked up by imported HAS_BOTO3
+
 
 def get_vpc_info(vpc):
-    
-   vpc_info = {'VpcId': vpc['VpcId'],
-               'State': vpc['State'],
-               'CidrBlock': vpc['CidrBlock'],
-               'DhcpOptionsId': vpc['DhcpOptionsId'],
-               'Tags': vpc['Tags'],
-               'InstanceTenancy': vpc['InstanceTenancy'],
-               'IsDefault': vpc['IsDefault']
-               }
-    
-    
-   return vpc_info
+
+    return dict(vpc_id=vpc['VpcId'],
+                state=vpc['State'],
+                cidr_block=vpc['CidrBlock'],
+                dhcp_options_id=vpc['DhcpOptionsId'],
+                tags=boto3_tag_list_to_ansible_dict(vpc['Tags']),
+                instance_tenancy=vpc['InstanceTenancy'],
+                is_default=vpc['IsDefault'])
+
 
 def list_vpcs(client, module):
-    input_filters = module.params.get("filters")
-    input_dryrun = module.params.get("DryRun")
+    filters = ansible_dict_to_boto3_filter_list(module.params.get("filters"))
 
-
-    filters = []
-
-    for key, value in input_filters.iteritems():
-        filters.append({'Name': key, 'Values':[value]}) 
-
-    all_vpcs = client.describe_vpcs(DryRun=input_dryrun, Filters=filters)
-    all_vpcs_array = []
- 
-    for vpc in all_vpcs['Vpcs']:
-        all_vpcs_array.append(get_vpc_info(vpc))
-    
-    module.exit_json(vpcs=all_vpcs_array)
+    return [get_vpc_info(vpc)
+            for vpc in client.describe_vpcs(DryRun=module.check_mode,
+                                            Filters=filters)['Vpcs']]
 
 
 def main():
@@ -75,35 +87,26 @@ def main():
     argument_spec = ec2_argument_spec()
     argument_spec.update(
         dict(
-            filters = dict(default=None, type='dict'),
-            DryRun = dict(type='bool', default=False)
+            filters = dict(default=dict(), type='dict'),
         )
     )
-    
-    module = AnsibleModule(argument_spec=argument_spec)
+    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
 
-     # Validate Requirements
+    # Validate Requirements
     if not HAS_BOTO3:
-        module.fail_json(msg='json and botocore/boto3 is required.')
+        module.fail_json(msg='botocore and boto3 are required.')
 
     try:
         region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
         connection = boto3_conn(module, conn_type='client', resource='ec2', region=region, endpoint=ec2_url, **aws_connect_kwargs)
-    except botocore.exceptions.NoCredentialsError, e:
-        module.fail_json(msg="Can't authorize connection - "+str(e))
+    except botocore.exceptions.NoCredentialsError as e:
+        module.fail_json(msg="Can't authorize connection - " + str(e))
 
     # call your function here
+    vpcs = list_vpcs(connection, module)
 
-    list_vpcs(connection, module)
+    module.exit_json(changed=False, vpc_facts=vpcs)
 
-    module.exit_json(changed=changed, vpc_facts_result=results)
-
-# import module snippets
-from ansible.module_utils.basic import *
-from ansible.module_utils.ec2 import *
 
 if __name__ == '__main__':
     main()
-
-
-
