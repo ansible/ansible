32acc0ebbecfd24845779d30072c2c34eaa73fcb
Rene Moser <mail@renemoser.net>
2017-01-17 08:20:35 +0100

    doc: changelog: new module dimensiondata_network

--- lib/ansible/modules/cloud/dimensiondata/dimensiondata_network.py
+++ lib/ansible/modules/cloud/dimensiondata/dimensiondata_network.py
@@ -28,7 +28,7 @@ module: dimensiondata_network
 short_description: Create, update, and delete MCP 1.0 & 2.0 networks
 description:
   - Create, update, and delete MCP 1.0 & 2.0 networks
-version_added: "2.2"
+version_added: "2.3"
 author: 'Aimon Bustardo (@aimonb)'
 options:
   region:
