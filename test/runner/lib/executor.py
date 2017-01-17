aeb5bcbc32d024cc302fc2e936780eb2ea364e3b
John Barker <john@johnrbarker.com>
2017-01-17 10:51:14 +0000

    ansible-test changes made in another PR

--- test/runner/lib/executor.py
+++ test/runner/lib/executor.py
@@ -239,8 +239,7 @@ def network_inventory(remotes):
 
     for remote in remotes:
         groups[remote.platform].append(
-            '%s ansible_host=%s ansible_user=%s ' \
-            'ansible_connection=network_cli ansible_ssh_private_key_file=%s ansible_network_os=%s' % (
+            '%s ansible_host=%s ansible_user=%s ansible_connection=network_cli ansible_ssh_private_key_file=%s ansible_network_os=%s' % (
                 remote.name.replace('.', '_'),
                 remote.connection.hostname,
                 remote.connection.username,
