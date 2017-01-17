8b39b1e11012ef81bf1dfd7b3f333a23250026fe
Matt Martz <matt@sivel.net>
2017-01-17 17:20:39 -0600

    Address some of the requested changes

--- test/integration/targets/groupby_filter/runme.sh
+++ test/integration/targets/groupby_filter/runme.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 
-set -e
+# We don't set -u here, due to pypa/virtualenv#150
+set -ex
 
 MYTMPDIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 
+# This is needed for the ubuntu1604py3 tests
+# Ubuntu patches virtualenv to make the default python2
+# but for the python3 tests we need virtualenv to use python3
 if [ -f /usr/bin/python3 ]
 then
-    PYTHON="--python /usr/bin/python3"
+    PYTHON="/usr/bin/python3"
 else
-    PYTHON=""
+    PYTHON="$(which python)"
 fi
 
-virtualenv --system-site-packages $PYTHON "${MYTMPDIR}/jinja2"
+virtualenv --system-site-packages --python $PYTHON "${MYTMPDIR}/jinja2"
 
 source "${MYTMPDIR}/jinja2/bin/activate"
 
 pip install -U jinja2==2.9.4
 
-ansible-playbook -i ../../inventory test_jinja2_groupby.yml -v
+ansible-playbook -i ../../inventory test_jinja2_groupby.yml -v "$@"
 
 pip install -U "jinja2<2.9.0"
 
-ansible-playbook -i ../../inventory test_jinja2_groupby.yml -v
+ansible-playbook -i ../../inventory test_jinja2_groupby.yml -v "$@"
 
 deactivate
 
