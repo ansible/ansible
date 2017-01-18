ab70ca463bb5a7fb1c880aa148f937efdb664a79
Lucas Alvares Gomes <lucasagomes@gmail.com>
2017-01-18 10:34:25 +0000

    rpm_key: Decode bytes to string to work with Python 3 (#20326)
    The read() method will return bytes we need to then decode() those bytes
    to a string before trying to match() it using the re module.

--- lib/ansible/modules/packaging/os/rpm_key.py
+++ lib/ansible/modules/packaging/os/rpm_key.py
@@ -78,7 +78,7 @@ import tempfile
 def is_pubkey(string):
     """Verifies if string is a pubkey"""
     pgp_regex = ".*?(-----BEGIN PGP PUBLIC KEY BLOCK-----.*?-----END PGP PUBLIC KEY BLOCK-----).*"
-    return re.match(pgp_regex, string, re.DOTALL)
+    return re.match(pgp_regex, string.decode('utf-8'), re.DOTALL)
 
 class RpmKey:
 
