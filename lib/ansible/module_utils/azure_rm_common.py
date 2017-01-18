39405d64171d32be0bc404fddc82c15192e0048b
Baptiste DB <bathizte@gmail.com>
2017-01-18 23:32:17 +0100

    UPD Azure_rm_common with non-blocking pull request from azure_rm.py (#20338)

--- lib/ansible/module_utils/azure_rm_common.py
+++ lib/ansible/module_utils/azure_rm_common.py
@@ -567,7 +567,11 @@ class AzureRMModuleBase(object):
             resource_client = self.rm_client
             resource_client.providers.register(key)
         except Exception as exc:
-            self.fail("One-time registration of {0} failed - {1}".format(key, str(exc)))
+            self.log("One-time registration of {0} failed - {1}".format(key, str(exc)))
+            self.log("You might need to register {0} using an admin account".format(key))
+            self.log(("To register a provider using the Python CLI: "
+                      "https://docs.microsoft.com/azure/azure-resource-manager/"
+                      "resource-manager-common-deployment-errors#noregisteredproviderfound"))
 
     @property
     def storage_client(self):
