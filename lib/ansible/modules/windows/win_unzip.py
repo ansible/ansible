9fb06a12a1ae8d35bb18f9c76a04b78267fe0543
Fabio Alessandro Locati <me@fale.io>
2017-01-19 15:13:56 +0000

    Remove all examples about other modules and streamline examples

--- lib/ansible/modules/windows/win_unzip.py
+++ lib/ansible/modules/windows/win_unzip.py
@@ -70,41 +70,16 @@ author: Phil Schwartz
 '''
 
 EXAMPLES = r'''
-# This unzips a library that was downloaded with win_get_url, and removes the file after extraction
-# $ ansible -i hosts -m win_unzip -a "src=C:\\LibraryToUnzip.zip dest=C:\\Lib rm=true" all
-# Playbook example
-
-# Simple unzip
----
 - name: Unzip a bz2 (BZip) file
   win_unzip:
     src: C:\Users\Phil\Logs.bz2
     dest: C:\Users\Phil\OldLogs
     creates: C:\Users\Phil\OldLogs
 
-# This playbook example unzips a .zip file and recursively decompresses the contained .gz files and removes all unneeded compressed files after completion.
----
-- name: Unzip ApplicationLogs.zip and decompress all GZipped log files
-  hosts: all
-  gather_facts: false
-  tasks:
-    - name: Recursively decompress GZ files in ApplicationLogs.zip
-      win_unzip:
-        src: C:\Downloads\ApplicationLogs.zip
-        dest: C:\Application\Logs
-        recurse: yes
-        rm: true
-
-# Install PSCX to use for extracting a gz file
-  - name: Grab PSCX msi
-    win_get_url:
-      url: http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pscx&DownloadId=923562&FileTime=130585918034470000&Build=20959
-      dest: C:\pscx.msi
-  - name: Install PSCX
-    win_msi:
-      path: C:\pscx.msi
-  - name: Unzip gz log
-    win_unzip:
-      src: C:\Logs\application-error-logs.gz
-      dest: C:\ExtractedLogs\application-error-logs
+- name: Recursively decompress GZ files in ApplicationLogs.zip
+  win_unzip:
+    src: C:\Downloads\ApplicationLogs.zip
+    dest: C:\Application\Logs
+    recurse: True
+    rm: True
 '''
