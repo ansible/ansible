537b3b75a6681c643d39bee3975b0c8310a7d600
Matt Martz <matt@sivel.net>
2017-01-19 13:39:59 -0600

    Add jinja2 groupby filter override to cast namedtuple to tuple. Fixes #20098 (#20362)
    * Add jinja2 groupby filter override to cast namedtuple to tuple. Fixes #20098
    
    * Address some of the requested changes
    
    * Quoting
    
    * Print the python path and version
    
    * Be less explicitly verbose, rely on implicit verbosity

---
+++ test/integration/targets/groupby_filter/runme.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# We don't set -u here, due to pypa/virtualenv#150
+set -ex
+
+MYTMPDIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
+
+# This is needed for the ubuntu1604py3 tests
+# Ubuntu patches virtualenv to make the default python2
+# but for the python3 tests we need virtualenv to use python3
+if [ -f /usr/bin/python3 ]
+then
+    PYTHON="--python /usr/bin/python3"
+else
+    PYTHON=""
+fi
+
+virtualenv --system-site-packages $PYTHON "${MYTMPDIR}/jinja2"
+
+source "${MYTMPDIR}/jinja2/bin/activate"
+
+which python
+python -V
+
+pip install -U jinja2==2.9.4
+
+ansible-playbook -i ../../inventory test_jinja2_groupby.yml -v "$@"
+
+pip install -U "jinja2<2.9.0"
+
+ansible-playbook -i ../../inventory test_jinja2_groupby.yml -v "$@"
+
+deactivate
+
+rm -r "${MYTMPDIR}"
