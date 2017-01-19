3eead10cce64ecdead3ac263e77977e66dae7046
Robert Sax <robert.sax@stansonhealth.com>
2017-01-19 15:14:05 -0500

    fixing mime types in s3_sync module (#20059)

--- lib/ansible/modules/cloud/amazon/s3_sync.py
+++ lib/ansible/modules/cloud/amazon/s3_sync.py
@@ -428,12 +428,11 @@ def upload_files(s3, bucket, filelist, params):
     ret = []
     for entry in filelist:
         args = {
-          'ContentLength': entry['bytes'],
           'ContentType': entry['mime_type']
         }
         if params.get('permission'):
             args['ACL'] = params['permission']
-        s3.upload_file(entry['fullpath'], bucket, entry['s3_path'], ExtraArgs=None, Callback=None, Config=None)
+        s3.upload_file(entry['fullpath'], bucket, entry['s3_path'], ExtraArgs=args, Callback=None, Config=None)
         ret.append(entry)
     return ret
 
