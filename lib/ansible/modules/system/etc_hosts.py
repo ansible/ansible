7c542e9e7767bb034da6310ec6dd21bfcd1ec004
Jiri tyr <jiri.tyr@gmail.com>
2017-01-20 00:06:13 +0000

    Adding METADATA block

--- lib/ansible/modules/system/etc_hosts.py
+++ lib/ansible/modules/system/etc_hosts.py
@@ -19,6 +19,14 @@
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
 
+ANSIBLE_METADATA = {
+    'status': [
+        'preview'
+    ],
+    'supported_by': 'community',
+    'version': '1.0'
+}
+
 DOCUMENTATION = '''
 ---
 module: etc_hosts
