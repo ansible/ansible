9f3d3322212376822bd9e06a1b66895032e84fa7
GGabriele <gabrielegerbino@gmail.com>
2017-01-25 17:12:24 +0100

    Remove capitalize

--- lib/ansible/modules/network/nxos/nxos_vrf_interface.py
+++ lib/ansible/modules/network/nxos/nxos_vrf_interface.py
@@ -387,7 +387,7 @@ def get_vrf_list(module):
 def get_interface_info(interface, module):
     if not interface.startswith('loopback'):
         interface = interface.capitalize()
-    command = 'show run | section interface.{0}'.format(interface.capitalize())
+    command = 'show run | section interface.{0}'.format(interface)
     vrf_regex = ".*vrf\s+member\s+(?P<vrf>\S+).*"
 
     try:
