4c9cfc9ef0399a87a557e5b74d8b29ba8d9b86f7
Elliott Sims <elliott@elliotts-mbp.local>
2017-01-24 17:09:20 -0800

    Add trailing comma

--- lib/ansible/modules/net_tools/basics/get_url.py
+++ lib/ansible/modules/net_tools/basics/get_url.py
@@ -325,7 +325,7 @@ def main():
         # not checking because of daisy chain to file module
         argument_spec = argument_spec,
         add_file_common_args=True,
-        supports_check_mode=True
+        supports_check_mode=True,
     )
 
     url  = module.params['url']
