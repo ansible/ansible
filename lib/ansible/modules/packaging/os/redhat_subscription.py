7f60a9f31dc6ca7625d3ff095f65034d2de3229e
Adrian Likins <alikins@redhat.com>
2017-01-26 16:55:13 -0500

    remove unused code

--- lib/ansible/modules/packaging/os/redhat_subscription.py
+++ lib/ansible/modules/packaging/os/redhat_subscription.py
@@ -277,10 +277,6 @@ class Rhsm(RegistrationBase):
               * Boolean - whether the current system is currently registered to
                           RHSM.
         '''
-        # Quick version...
-        if False:
-            return os.path.isfile('/etc/pki/consumer/cert.pem') and \
-                os.path.isfile('/etc/pki/consumer/key.pem')
 
         args = [SUBMAN_CMD, 'identity']
         rc, stdout, stderr = self.module.run_command(args, check_rc=False)
