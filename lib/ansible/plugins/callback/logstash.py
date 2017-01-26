f7d41e168c4fc2f15cacf1553cd4e8175107727d
Yevhen Khmelenko <ujenmr@gmail.com>
2017-01-26 23:46:46 +0200

    Update version of ansible-logstash-callback plugin

--- lib/ansible/plugins/callback/logstash.py
+++ lib/ansible/plugins/callback/logstash.py
@@ -1,4 +1,4 @@
-# (C) 2016, Ievgen Khmelenko <ujenmr@gmail.com>
+# (C) 2018, Yevhen Khmelenko <ujenmr@gmail.com>
 # (C) 2017 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
@@ -8,6 +8,7 @@ __metaclass__ = type
 DOCUMENTATION = '''
     callback: logstash
     type: notification
+    author: Yevhen Khmelenko <ujenmr@gmail.com>
     short_description: Sends events to Logstash
     description:
       - This callback will report facts and task events to Logstash https://www.elastic.co/products/logstash
@@ -21,25 +22,75 @@ DOCUMENTATION = '''
         env:
           - name: LOGSTASH_SERVER
         default: localhost
+        ini:
+          - section: callback_logstash
+            key: server
       port:
         description: Port on which logstash is listening
         env:
-            - name: LOGSTASH_PORT
+          - name: LOGSTASH_PORT
         default: 5000
+        ini:
+          - section: callback_logstash
+            key: port
+      pre_command:
+        description: Execute command before playbook start and set result to ansible_pre_command_output field
+        env:
+          - name: LOGSTASH_PRE_COMMAND
+        version_added: "2.8"
+        ini:
+          - section: callback_logstash
+            key: pre_command
       type:
         description: Message type
         env:
           - name: LOGSTASH_TYPE
         default: ansible
+        ini:
+          - section: callback_logstash
+            key: type
+'''
+
+EXAMPLES = '''
+examples: >
+    1. Install python module python-logstash
+        pip install python-logstash
+
+    2. Enable callback plugin
+    ansible.cfg:
+        [defaults]
+            callback_whitelist = logstash
+
+    3. Setup logstash connection via environment variables or ansible.cfg
+    environment variables:
+        export LOGSTASH_SERVER=logstash.example.com
+        export LOGSTASH_PORT=5000
+        export LOGSTASH_PRE_COMMAND="git rev-parse HEAD"
+        export LOGSTASH_TYPE=ansible
+
+    or same in ansible.cfg:
+        [callback_logstash]
+        server = logstash.example.com
+        port = 5000
+        pre_command = git rev-parse HEAD
+        type = ansible
+
+    4. Add to logstash tcp-input
+        logstash config:
+            input {
+                tcp {
+                    port => 5000
+                    codec => json
+                }
+            }
 '''
 
 import os
 import json
 import socket
 import uuid
-from datetime import datetime
-
 import logging
+from datetime import datetime
 
 try:
     import logstash
@@ -51,29 +102,6 @@ from ansible.plugins.callback import CallbackBase
 
 
 class CallbackModule(CallbackBase):
-    """
-    ansible logstash callback plugin
-    ansible.cfg:
-        callback_plugins   = <path_to_callback_plugins_folder>
-        callback_whitelist = logstash
-    and put the plugin in <path_to_callback_plugins_folder>
-
-    logstash config:
-        input {
-            tcp {
-                port => 5000
-                codec => json
-            }
-        }
-
-    Requires:
-        python-logstash
-
-    This plugin makes use of the following environment variables:
-        LOGSTASH_SERVER   (optional): defaults to localhost
-        LOGSTASH_PORT     (optional): defaults to 5000
-        LOGSTASH_TYPE     (optional): defaults to ansible
-    """
 
     CALLBACK_VERSION = 2.0
     CALLBACK_TYPE = 'aggregate'
@@ -87,33 +115,58 @@ class CallbackModule(CallbackBase):
             self.disabled = True
             self._display.warning("The required python-logstash is not installed. "
                                   "pip install python-logstash")
-        else:
-            self.logger = logging.getLogger('python-logstash-logger')
-            self.logger.setLevel(logging.DEBUG)
 
-            self.handler = logstash.TCPLogstashHandler(
-                os.getenv('LOGSTASH_SERVER', 'localhost'),
-                int(os.getenv('LOGSTASH_PORT', 5000)),
-                version=1,
-                message_type=os.getenv('LOGSTASH_TYPE', 'ansible')
-            )
-
-            self.logger.addHandler(self.handler)
-            self.hostname = socket.gethostname()
-            self.session = str(uuid.uuid1())
-            self.errors = 0
         self.start_time = datetime.utcnow()
 
-    def v2_playbook_on_start(self, playbook):
-        self.playbook = playbook._file_name
-        data = {
-            'status': "OK",
-            'host': self.hostname,
+    def _init_plugin(self):
+        self.logger = logging.getLogger('python-logstash-logger')
+        self.logger.setLevel(logging.DEBUG)
+
+        self.handler = logstash.TCPLogstashHandler(
+            self.ls_server,
+            self.ls_port,
+            version=1,
+            message_type=self.ls_type
+        )
+
+        self.logger.addHandler(self.handler)
+        self.hostname = socket.gethostname()
+        self.session = str(uuid.uuid4())
+        self.errors = 0
+
+        self.base_data = {
             'session': self.session,
-            'ansible_type': "start",
-            'ansible_playbook': self.playbook,
+            'host': self.hostname
         }
-        self.logger.info("ansible start", extra=data)
+
+        if self.ls_pre_command is not None:
+            self.base_data['ansible_pre_command_output'] = os.popen(
+                self.ls_pre_command).read()
+
+        if self._options is not None:
+            self.base_data['ansible_checkmode'] = self._options.check
+            self.base_data['ansible_tags'] = self._options.tags
+            self.base_data['ansible_skip_tags'] = self._options.skip_tags
+            self.base_data['inventory'] = self._options.inventory
+
+    def set_options(self, task_keys=None, var_options=None, direct=None):
+        super(CallbackModule, self).set_options(task_keys=task_keys,
+                                                var_options=var_options,
+                                                direct=direct)
+
+        self.ls_server = self.get_option('server')
+        self.ls_port = int(self.get_option('port'))
+        self.ls_type = self.get_option('type')
+        self.ls_pre_command = self.get_option('pre_command')
+
+        self._init_plugin()
+
+    def v2_playbook_on_start(self, playbook):
+        data = self.base_data.copy()
+        data['ansible_type'] = "start"
+        data['status'] = "OK"
+        data['ansible_playbook'] = playbook._file_name
+        self.logger.info("START PLAYBOOK | %s", data['ansible_playbook'], extra=data)
 
     def v2_playbook_on_stats(self, stats):
         end_time = datetime.utcnow()
@@ -127,103 +180,155 @@ class CallbackModule(CallbackBase):
         else:
             status = "FAILED"
 
-        data = {
-            'status': status,
-            'host': self.hostname,
-            'session': self.session,
-            'ansible_type': "finish",
-            'ansible_playbook': self.playbook,
-            'ansible_playbook_duration': runtime.total_seconds(),
-            'ansible_result': json.dumps(summarize_stat),
-        }
-        self.logger.info("ansible stats", extra=data)
+        data = self.base_data.copy()
+        data['ansible_type'] = "finish"
+        data['status'] = status
+        data['ansible_playbook_duration'] = runtime.total_seconds()
+        data['ansible_result'] = json.dumps(summarize_stat)
+
+        self.logger.info("FINISH PLAYBOOK | %s", json.dumps(summarize_stat), extra=data)
+
+    def v2_playbook_on_play_start(self, play):
+        self.play_id = str(play._uuid)
+
+        if play.name:
+            self.play_name = play.name
+
+        data = self.base_data.copy()
+        data['ansible_type'] = "start"
+        data['status'] = "OK"
+        data['ansible_play_id'] = self.play_id
+        data['ansible_play_name'] = self.play_name
+
+        self.logger.info("START PLAY | %s", self.play_name, extra=data)
+
+    def v2_playbook_on_task_start(self, task, is_conditional):
+        self.task_id = str(task._uuid)
+
+    '''
+    Tasks and handler tasks are dealt with here
+    '''
 
     def v2_runner_on_ok(self, result, **kwargs):
-        data = {
-            'status': "OK",
-            'host': self.hostname,
-            'session': self.session,
-            'ansible_type': "task",
-            'ansible_playbook': self.playbook,
-            'ansible_host': result._host.name,
-            'ansible_task': result._task,
-            'ansible_result': self._dump_results(result._result)
-        }
-        self.logger.info("ansible ok", extra=data)
+        task_name = str(result._task).replace('TASK: ', '').replace('HANDLER: ', '')
+
+        data = self.base_data.copy()
+        data['status'] = "OK"
+        data['ansible_host'] = result._host.name
+        data['ansible_play_id'] = self.play_id
+        data['ansible_play_name'] = self.play_name
+        data['ansible_task'] = task_name
+
+        if task_name == 'setup':
+            data['ansible_type'] = "setup"
+            data['ansible_facts'] = self._dump_results(result._result)
+
+            self.logger.info("SETUP FACTS | %s", self._dump_results(result._result), extra=data)
+        else:
+            if 'changed' in result._result.keys():
+                data['ansible_changed'] = result._result['changed']
+            else:
+                data['ansible_changed'] = False
+            data['ansible_type'] = "task"
+            data['ansible_task_id'] = self.task_id
+            data['ansible_result'] = self._dump_results(result._result)
+
+            self.logger.info("TASK OK | %s | RESULT | %s",
+                             task_name, self._dump_results(result._result), extra=data)
 
     def v2_runner_on_skipped(self, result, **kwargs):
-        data = {
-            'status': "SKIPPED",
-            'host': self.hostname,
-            'session': self.session,
-            'ansible_type': "task",
-            'ansible_playbook': self.playbook,
-            'ansible_task': result._task,
-            'ansible_host': result._host.name
-        }
-        self.logger.info("ansible skipped", extra=data)
+        task_name = str(result._task).replace('TASK: ', '').replace('HANDLER: ', '')
+
+        data = self.base_data.copy()
+        if 'changed' in result._result.keys():
+            data['ansible_changed'] = result._result['changed']
+        else:
+            data['ansible_changed'] = False
+        data['ansible_type'] = "task"
+        data['status'] = "SKIPPED"
+        data['ansible_host'] = result._host.name
+        data['ansible_play_id'] = self.play_id
+        data['ansible_play_name'] = self.play_name
+        data['ansible_task'] = task_name
+        data['ansible_task_id'] = self.task_id
+        data['ansible_result'] = self._dump_results(result._result)
+
+        self.logger.info("TASK SKIPPED | %s", task_name, extra=data)
 
     def v2_playbook_on_import_for_host(self, result, imported_file):
-        data = {
-            'status': "IMPORTED",
-            'host': self.hostname,
-            'session': self.session,
-            'ansible_type': "import",
-            'ansible_playbook': self.playbook,
-            'ansible_host': result._host.name,
-            'imported_file': imported_file
-        }
-        self.logger.info("ansible import", extra=data)
+        data = self.base_data.copy()
+        data['ansible_type'] = "import"
+        data['status'] = "IMPORTED"
+        data['ansible_host'] = result._host.name
+        data['ansible_play_id'] = self.play_id
+        data['ansible_play_name'] = self.play_name
+        data['imported_file'] = imported_file
+
+        self.logger.info("IMPORT | %s", imported_file, extra=data)
 
     def v2_playbook_on_not_import_for_host(self, result, missing_file):
-        data = {
-            'status': "NOT IMPORTED",
-            'host': self.hostname,
-            'session': self.session,
-            'ansible_type': "import",
-            'ansible_playbook': self.playbook,
-            'ansible_host': result._host.name,
-            'missing_file': missing_file
-        }
-        self.logger.info("ansible import", extra=data)
+        data = self.base_data.copy()
+        data['ansible_type'] = "import"
+        data['status'] = "NOT IMPORTED"
+        data['ansible_host'] = result._host.name
+        data['ansible_play_id'] = self.play_id
+        data['ansible_play_name'] = self.play_name
+        data['imported_file'] = missing_file
+
+        self.logger.info("NOT IMPORTED | %s", missing_file, extra=data)
 
     def v2_runner_on_failed(self, result, **kwargs):
-        data = {
-            'status': "FAILED",
-            'host': self.hostname,
-            'session': self.session,
-            'ansible_type': "task",
-            'ansible_playbook': self.playbook,
-            'ansible_host': result._host.name,
-            'ansible_task': result._task,
-            'ansible_result': self._dump_results(result._result)
-        }
+        task_name = str(result._task).replace('TASK: ', '').replace('HANDLER: ', '')
+
+        data = self.base_data.copy()
+        if 'changed' in result._result.keys():
+            data['ansible_changed'] = result._result['changed']
+        else:
+            data['ansible_changed'] = False
+
+        data['ansible_type'] = "task"
+        data['status'] = "FAILED"
+        data['ansible_host'] = result._host.name
+        data['ansible_play_id'] = self.play_id
+        data['ansible_play_name'] = self.play_name
+        data['ansible_task'] = task_name
+        data['ansible_task_id'] = self.task_id
+        data['ansible_result'] = self._dump_results(result._result)
+
         self.errors += 1
-        self.logger.error("ansible failed", extra=data)
+        self.logger.error("TASK FAILED | %s | HOST | %s | RESULT | %s", task_name,
+                          self.hostname, self._dump_results(result._result), extra=data)
 
     def v2_runner_on_unreachable(self, result, **kwargs):
-        data = {
-            'status': "UNREACHABLE",
-            'host': self.hostname,
-            'session': self.session,
-            'ansible_type': "task",
-            'ansible_playbook': self.playbook,
-            'ansible_host': result._host.name,
-            'ansible_task': result._task,
-            'ansible_result': self._dump_results(result._result)
-        }
-        self.logger.error("ansible unreachable", extra=data)
+        task_name = str(result._task).replace('TASK: ', '').replace('HANDLER: ', '')
+
+        data = self.base_data.copy()
+        data['ansible_type'] = "task"
+        data['status'] = "UNREACHABLE"
+        data['ansible_host'] = result._host.name
+        data['ansible_play_id'] = self.play_id
+        data['ansible_play_name'] = self.play_name
+        data['ansible_task'] = task_name
+        data['ansible_task_id'] = self.task_id
+        data['ansible_result'] = self._dump_results(result._result)
+
+        self.errors += 1
+        self.logger.error("UNREACHABLE | %s | HOST | %s | RESULT | %s", task_name,
+                          self.hostname, self._dump_results(result._result), extra=data)
 
     def v2_runner_on_async_failed(self, result, **kwargs):
-        data = {
-            'status': "FAILED",
-            'host': self.hostname,
-            'session': self.session,
-            'ansible_type': "task",
-            'ansible_playbook': self.playbook,
-            'ansible_host': result._host.name,
-            'ansible_task': result._task,
-            'ansible_result': self._dump_results(result._result)
-        }
+        task_name = str(result._task).replace('TASK: ', '').replace('HANDLER: ', '')
+
+        data = self.base_data.copy()
+        data['ansible_type'] = "task"
+        data['status'] = "FAILED"
+        data['ansible_host'] = result._host.name
+        data['ansible_play_id'] = self.play_id
+        data['ansible_play_name'] = self.play_name
+        data['ansible_task'] = task_name
+        data['ansible_task_id'] = self.task_id
+        data['ansible_result'] = self._dump_results(result._result)
+
         self.errors += 1
-        self.logger.error("ansible async", extra=data)
+        self.logger.error("ASYNC FAILED | %s | HOST | %s | RESULT | %s", task_name,
+                          self.hostname, self._dump_results(result._result), extra=data)
