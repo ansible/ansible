d3989b0b386aeca88a7137d2e52288b43e2822a7
Ted Elhourani <theodore.elhourani@gmail.com>
2017-01-29 20:24:08 -0800

    Remove comma

--- lib/ansible/modules/network/bigswitch/bigmon_chain.py
+++ lib/ansible/modules/network/bigswitch/bigmon_chain.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Ansible module to manage Big Monitoring Fabric service chains
-# (c) 2016, Ted Elhourani <ted@bigswitch.com>,
+# (c) 2016, Ted Elhourani <ted@bigswitch.com>
 #
 # This file is part of Ansible
 #
