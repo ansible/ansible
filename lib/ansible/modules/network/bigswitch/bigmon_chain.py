c5f0276402dac8f4c3534a9d8318d209ef18273d
Ted Elhourani <theodore.elhourani@gmail.com>
2017-01-29 20:08:48 -0800

    Remove extra white space and add end of file newline

--- lib/ansible/modules/network/bigswitch/bigmon_chain.py
+++ lib/ansible/modules/network/bigswitch/bigmon_chain.py
@@ -120,7 +120,7 @@ def chain(module):
 
     if state in ('absent') and not config_present:
         module.exit_json(changed=False)
-        
+ 
     if state in ('present'):
         response = rest.put('chain[name="%s"]' % name, data={'name': name})
         if response.status_code == 204:
@@ -154,3 +154,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
