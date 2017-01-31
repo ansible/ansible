a2a8253eea0a010231d3f722c8431152de29cf55
Pieter Voet <pietervoet@nl.ibm.com>
2017-01-31 12:08:29 +0100

    Corrected indent

--- lib/ansible/plugins/action/set_extra_var.py
+++ lib/ansible/plugins/action/set_extra_var.py
@@ -8,7 +8,7 @@ from ansible.plugins.action import ActionBase
 class ActionModule(ActionBase):
     def run(self, tmp=None, task_vars=None):
         if task_vars is None:
-           task_vars = dict()
+            task_vars = dict()
 
         result = super(ActionModule, self).run(tmp, task_vars)
 
