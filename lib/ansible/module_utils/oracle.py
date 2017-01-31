3e69fe0e4c0d85bd662626951e29dc836c8c3da8
tkrah <thomas.krahn@esailors.de>
2017-01-31 14:28:52 +0100

    oracle: Fix: ERROR: Interpreter linuxe is not "#!/usr/bin/python"

--- lib/ansible/module_utils/oracle.py
+++ lib/ansible/module_utils/oracle.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+#/usr/bin/python
+#  -*- coding: utf-8 -*-
 
 # Copyright (c) 2017 Thomas Krahn (@Nosmoht)
 # All rights reserved.
--- lib/ansible/modules/database/oracle/oracle_system_parameter.py
+++ lib/ansible/modules/database/oracle/oracle_system_parameter.py
@@ -1,3 +1,4 @@
+#/usr/bin/python
 # -*- coding: utf-8 -*-
 
 # Copyright (c) 2017 Thomas Krahn (@Nosmoht)
