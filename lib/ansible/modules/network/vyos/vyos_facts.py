fbf0396fd28bf6556480c969a2648a2b8c910d17
Nathaniel Case <ncase@redhat.com>
2017-02-01 15:21:49 -0500

    Update documentation

--- lib/ansible/modules/network/vyos/vyos_facts.py
+++ lib/ansible/modules/network/vyos/vyos_facts.py
@@ -33,13 +33,12 @@ description:
     base network fact keys with U(ansible_net_<fact>).  The facts
     module will always collect a base set of facts from the device
     and can enable or disable collection of additional facts.
-extends_documentation_fragment: vyos
 options:
   gather_subset:
     description:
       - When supplied, this argument will restrict the facts collected
         to a given subset.  Possible values for this argument include
-        all, hardware, config, and interfaces.  Can specify a list of
+        all, default, config, and neighbors.  Can specify a list of
         values to include a larger subset.  Values can also be used
         with an initial C(M(!)) to specify that a specific subset should
         not be collected.
