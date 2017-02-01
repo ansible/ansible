1e19f6e4c0901bdea9732a579b97965d15a7bb23
Pierre-Louis Bonicoli <pierre-louis@libregerbil.fr>
2017-02-01 11:41:04 +0100

    default/minimal callback: don't display warnings twice

--- lib/ansible/plugins/callback/minimal.py
+++ lib/ansible/plugins/callback/minimal.py
@@ -58,6 +58,7 @@ class CallbackModule(CallbackBase):
         if result._task.action in C.MODULE_NO_JSON and 'module_stderr' not in result._result:
             self._display.display(self._command_generic_msg(result._host.get_name(), result._result, "FAILED"), color=C.COLOR_ERROR)
         else:
+            self._handle_warnings(result._result)
             self._display.display("%s | FAILED! => %s" % (result._host.get_name(), self._dump_results(result._result, indent=4)), color=C.COLOR_ERROR)
 
     def v2_runner_on_ok(self, result):
@@ -65,11 +66,11 @@ class CallbackModule(CallbackBase):
         if result._task.action in C.MODULE_NO_JSON:
             self._display.display(self._command_generic_msg(result._host.get_name(), result._result, "SUCCESS"), color=C.COLOR_OK)
         else:
+            self._handle_warnings(result._result)
             if 'changed' in result._result and result._result['changed']:
                 self._display.display("%s | SUCCESS => %s" % (result._host.get_name(), self._dump_results(result._result, indent=4)), color=C.COLOR_CHANGED)
             else:
                 self._display.display("%s | SUCCESS => %s" % (result._host.get_name(), self._dump_results(result._result, indent=4)), color=C.COLOR_OK)
-            self._handle_warnings(result._result)
 
     def v2_runner_on_skipped(self, result):
         self._display.display("%s | SKIPPED" % (result._host.get_name()), color=C.COLOR_SKIP)
