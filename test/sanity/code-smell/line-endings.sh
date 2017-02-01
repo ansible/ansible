60874077c748505c9b79f81ca6a358787fada556
Adrian Likins <alikins@redhat.com>
2017-02-01 15:42:06 -0500

    use git-ls instead of 'grep -r' to avoid unstaged files

--- test/sanity/code-smell/line-endings.sh
+++ test/sanity/code-smell/line-endings.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-grep -rIPl '\r' . \
-    --exclude-dir .git \
-    --exclude-dir .tox \
-    | grep -v -F \
-    -e './test/integration/targets/win_regmerge/templates/win_line_ending.j2'
+git grep -IPl '\r' . \
+    | grep -v -F 'test/integration/targets/win_regmerge/templates/win_line_ending.j2'
 
 if [ $? -ne 1 ]; then
     printf 'One or more file(s) listed above have invalid line endings.\n'
