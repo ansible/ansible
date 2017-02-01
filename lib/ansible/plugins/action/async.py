0e09ac6dfc635fbf7d4e50827ce853f01dfca644
Nehal J Wani <nehaljw.kkd1@gmail.com>
2017-02-02 02:19:57 +0530

    Async module: Update module args for the target module
    When any ansible task is run with async module on a machine where
    SELinux is enabled, the module arg _ansible_selinux_special_fs is
    not set; resulting into an error like:
    
    (ab) ~/h/ansible ❯❯❯ getenforce
    Enforcing
    (ab) ~/h/ansible ❯❯❯ ansible localhost -m file -B 4 -a 'path=/tmp/junk state=directory'
     [WARNING]: provided hosts list is empty, only localhost is available
    
    localhost | FAILED! => {
        "ansible_job_id": "739794416549.30537",
        "changed": false,
        "failed": true,
        "finished": 1,
        "gid": 1000,
        "group": "wani",
        "mode": "0755",
        "msg": "There was an issue creating /tmp/junk as requested: 'AnsibleModule' object has no attribute '_selinux_special_fs'",
        "owner": "wani",
        "path": "/tmp/junk",
        "secontext": "unconfined_u:object_r:user_tmp_t:s0",
        "size": 40,
        "state": "directory",
        "uid": 1000
    }
    
    This patch ensures that the common module args are updated for the
    target module, before it is configured and transferred.

--- lib/ansible/plugins/action/async.py
+++ lib/ansible/plugins/action/async.py
@@ -51,8 +51,9 @@ class ActionModule(ActionBase):
         env_string = self._compute_environment_string()
 
         module_args = self._task.args.copy()
-        if self._play_context.no_log or C.DEFAULT_NO_TARGET_SYSLOG:
-            module_args['_ansible_no_log'] = True
+
+        # update common module args
+        self._update_module_args(module_name, module_args, task_vars)
 
         # configure, upload, and chmod the target module
         (module_style, shebang, module_data, module_path) = self._configure_module(module_name=module_name, module_args=module_args, task_vars=task_vars)
