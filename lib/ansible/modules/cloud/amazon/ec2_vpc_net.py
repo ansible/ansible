cc240e3f2d34c5ace489f150b115bb67e58514bb
Pau Rodriguez <prodriguez@ackcent.com>
2017-02-02 00:28:38 +0100

    fixed multi_ok(multi) usage in vpc_exists function

--- lib/ansible/modules/cloud/amazon/ec2_vpc_net.py
+++ lib/ansible/modules/cloud/amazon/ec2_vpc_net.py
@@ -118,9 +118,9 @@ def boto_exception(err):
     return error
 
 def vpc_exists(module, vpc, name, cidr_block, multi):
-    """Returns True or False in regards to the existence of a VPC. When supplied
+    """Returns None or a vpc object depending on the existence of a VPC. When supplied
     with a CIDR, it will check for matching tags to determine if it is a match
-    otherwise it will assume the VPC does not exist and thus return false.
+    otherwise it will assume the VPC does not exist and thus return None.
     """
     matched_vpc = None
 
@@ -130,11 +130,12 @@ def vpc_exists(module, vpc, name, cidr_block, multi):
         e_msg=boto_exception(e)
         module.fail_json(msg=e_msg)
 
-    if len(matching_vpcs) == 1:
+    if multi:
+        return None
+    elif len(matching_vpcs) == 1:
         matched_vpc = matching_vpcs[0]
     elif len(matching_vpcs) > 1:
-        if multi:
-            module.fail_json(msg='Currently there are %d VPCs that have the same name and '
+        module.fail_json(msg='Currently there are %d VPCs that have the same name and '
                              'CIDR block you specified. If you would like to create '
                              'the VPC anyway please pass True to the multi_ok param.' % len(matching_vpcs))
 
