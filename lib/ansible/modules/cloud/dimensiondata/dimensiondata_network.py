3976efaaab78d23e0c9b62624aa0dccc56bd26b6
tintoy <tintoy@tintoy.io>
2017-02-05 11:05:31 +1100

    Refactor dimensiondata_network to use shared base class for common functionality.

--- lib/ansible/modules/cloud/dimensiondata/dimensiondata_network.py
+++ lib/ansible/modules/cloud/dimensiondata/dimensiondata_network.py
@@ -22,9 +22,11 @@
 #   - Adam Friedman  <tintoy@tintoy.io>
 #
 
-ANSIBLE_METADATA = {'status': ['preview'],
-                    'supported_by': 'community',
-                    'version': '1.0'}
+ANSIBLE_METADATA = {
+    'status': ['preview'],
+    'supported_by': 'community',
+    'version': '1.0'
+}
 
 DOCUMENTATION = '''
 ---
@@ -38,21 +40,22 @@ options:
   region:
     description:
       - The target region.
-      - Valid regions are defined in Apache libcloud project [libcloud/common/dimensiondata.py]
-      - Regions are also listed in https://libcloud.readthedocs.io/en/latest/compute/drivers/dimensiondata.html
+    choices:
+      - Regions are defined in Apache libcloud project [libcloud/common/dimensiondata.py]
+      - They are also listed in U(https://libcloud.readthedocs.io/en/latest/compute/drivers/dimensiondata.html)
       - Note that the default value "na" stands for "North America".
-      - The module prepends 'dd-' to the region.
+      - The module prepends 'dd-' to the region choice.
     default: na
   mcp_user:
     description:
       - The username used to authenticate to the CloudControl API.
-      - If not specified, will fall back to MCP_USER from environment variable or  ~/.dimensiondata.
+      - If not specified, will fall back to C(MCP_USER) from environment variable or C(~/.dimensiondata).
     required: false
   mcp_password:
     description:
       - The password used to authenticate to the CloudControl API.
-      - If not specified, will fall back to MCP_PASSWORD from environment variable or  ~/.dimensiondata.
-      - Required if mcp_user is specified.
+      - If not specified, will fall back to C(MCP_PASSWORD) from environment variable or C(~/.dimensiondata).
+      - Required if I(mcp_user) is specified.
     required: false
   location:
     description:
@@ -89,7 +92,7 @@ options:
     default: 600
   wait_poll_interval:
     description:
-      - The amount to time inbetween polling for task completion
+      - The length of time between successive polls for completion
     required: false
     default: 2
   state:
@@ -159,181 +162,241 @@ network:
 
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.dimensiondata import get_credentials, DimensionDataAPIException, LibcloudNotFound
+from ansible.module_utils.dimensiondata import DimensionDataModule, DimensionDataAPIException
 from ansible.module_utils.pycompat24 import get_exception
 try:
     from libcloud.compute.types import Provider
     from libcloud.compute.providers import get_driver
     from libcloud.compute.base import NodeLocation
+
     import libcloud.security
+
     HAS_LIBCLOUD = True
 except ImportError:
+    libcloud, Provider, get_driver, NodeLocation = None
+
     HAS_LIBCLOUD = False
 
 
-def network_obj_to_dict(network, version):
-    network_dict = dict(id=network.id, name=network.name,
-                        description=network.description)
-    if isinstance(network.location, NodeLocation):
-        network_dict['location'] = network.location.id
-    else:
-        network_dict['location'] = network.location
+class DimensionDataNetworkModule(DimensionDataModule):
+    """
+    The dimensiondata_network module for Ansible.
+    """
 
-    if version == '1.0':
-        network_dict['private_net'] = network.private_net
-        network_dict['multicast'] = network.multicast
-        network_dict['status'] = None
-    else:
-        network_dict['private_net'] = None
-        network_dict['multicast'] = None
-        network_dict['status'] = network.status
-    return network_dict
+    def __init__(self):
+        """
+        Create a new Dimension Data network module.
+        """
 
-
-def get_mcp_version(driver, location):
-    # Get location to determine if MCP 1.0 or 2.0
-    location = driver.ex_get_location_by_id(location)
-    if 'MCP 2.0' in location.name:
-        return '2.0'
-    return '1.0'
-
-
-def create_network(module, driver, mcp_version, location,
-                   name, description):
-
-    # Make sure service_plan argument is defined
-    if mcp_version == '2.0' and 'service_plan' not in module.params:
-        module.fail_json('service_plan required when creating network and ' +
-                         'location is MCP 2.0')
-    service_plan = module.params['service_plan']
-
-    # Create network
-    try:
-        if mcp_version == '1.0':
-            res = driver.ex_create_network(location, name,
-                                           description=description)
-        else:
-            res = driver.ex_create_network_domain(location, name,
-                                                  service_plan,
-                                                  description=description)
-    except DimensionDataAPIException:
-        e = get_exception()
-
-        module.fail_json(msg="Failed to create new network: %s" % str(e))
-
-    if module.params['wait'] is True:
-        wait_for_network_state(module, driver, res.id, 'NORMAL')
-    msg = "Created network %s in %s" % (name, location)
-    network = network_obj_to_dict(res, mcp_version)
-
-    module.exit_json(changed=True, msg=msg, network=network)
-
-
-def delete_network(module, driver, matched_network, mcp_version):
-    try:
-        if mcp_version == '1.0':
-            res = driver.ex_delete_network(matched_network[0])
-        else:
-            res = driver.ex_delete_network_domain(matched_network[0])
-        if res is True:
-            module.exit_json(changed=True,
-                             msg="Deleted network with id %s" %
-                             matched_network[0].id)
-
-        module.fail_json("Unexpected failure deleting network with " +
-                         "id %s", matched_network[0].id)
-    except DimensionDataAPIException:
-        e = get_exception()
-
-        module.fail_json(msg="Failed to delete network: %s" % str(e))
-
-
-def wait_for_network_state(module, driver, net_id, state_to_wait_for):
-    try:
-        return driver.connection.wait_for_state(
-            state_to_wait_for, driver.ex_get_network_domain,
-            module.params['wait_poll_interval'],
-            module.params['wait_time'], net_id
+        super(DimensionDataNetworkModule, self).__init__(
+            module=AnsibleModule(
+                argument_spec=dict(
+                    region=dict(
+                        type='str', default='na'
+                    ),
+                    mcp_user=dict(
+                        type='str', required=False
+                    ),
+                    mcp_password=dict(
+                        type='str', required=False
+                    ),
+                    location=dict(
+                        type='str', required=True
+                    ),
+                    name=dict(
+                        type='str', required=True
+                    ),
+                    description=dict(
+                        type='str', required=False
+                    ),
+                    service_plan=dict(
+                        default='ADVANCED', choices=['ADVANCED', 'ESSENTIALS']
+                    ),
+                    state=dict(
+                        default='present', choices=['present', 'absent']
+                    ),
+                    wait=dict(
+                        type='bool', required=False, default=False
+                    ),
+                    wait_time=dict(
+                        type='int', required=False, default=600
+                    ),
+                    wait_poll_interval=dict(
+                        type='int', required=False, default=2
+                    ),
+                    verify_ssl_cert=dict(
+                        type='bool', required=False, default=True
+                    )
+                ),
+                required_together=[
+                    ['mcp_user', 'mcp_password']
+                ]
+            )
         )
-    except DimensionDataAPIException:
-        e = get_exception()
 
-        module.fail_json(msg='Network did not reach % state in time: %s'
-                         % (state_to_wait_for, e.msg))
+        self.name = self.module.params['name']
+        self.description = self.module.params['description']
+        self.location = self.module.params['location']
+        self.state = self.module.params['state']
+
+        # Get MCP API Version
+        self.mcp_version = self.get_mcp_version(self.location)
+
+    def state_present(self):
+        network = self._get_network()
+
+        if network:
+            self.module.exit_json(
+                changed=False,
+                msg='Network already exists',
+                network=self._network_to_dict(network)
+            )
+
+            return
+
+        network = self._create_network()
+
+        self.module.exit_json(
+            changed=True,
+            msg='Created network "%s" in datacenter "%s".' % (self.name, self.location),
+            network=self._network_to_dict(network)
+        )
+
+    def state_absent(self):
+        network = self._get_network()
+
+        if not network:
+            self.module.exit_json(
+                changed=False,
+                msg='Network "%s" does not exist' % self.name,
+                network=self._network_to_dict(network)
+            )
+
+            return
+
+        self._delete_network(network)
+
+    def _get_network(self):
+        if self.mcp_version == '1.0':
+            networks = self.driver.list_networks(location=self.location)
+        else:
+            networks = self.driver.ex_list_network_domains(location=self.location)
+
+        matched_network = [network for network in networks if network.name == self.name]
+        if matched_network:
+            return matched_network[0]
+
+        return None
+
+    def _network_to_dict(self, network):
+        network_dict = dict(
+            id=network.id,
+            name=network.name,
+            description=network.description
+        )
+
+        if isinstance(network.location, NodeLocation):
+            network_dict['location'] = network.location.id
+        else:
+            network_dict['location'] = network.location
+
+        if self.mcp_version == '1.0':
+            network_dict['private_net'] = network.private_net
+            network_dict['multicast'] = network.multicast
+            network_dict['status'] = None
+        else:
+            network_dict['private_net'] = None
+            network_dict['multicast'] = None
+            network_dict['status'] = network.status
+
+        return network_dict
+
+    def _create_network(self):
+
+        # Make sure service_plan argument is defined
+        if self.mcp_version == '2.0' and 'service_plan' not in self.module.params:
+            self.module.fail_json(
+                msg='service_plan required when creating network and location is MCP 2.0'
+            )
+
+            return None
+
+        # Create network
+        try:
+            if self.mcp_version == '1.0':
+                network = self.driver.ex_create_network(
+                    self.location,
+                    self.name,
+                    description=self.description
+                )
+            else:
+                network = self.driver.ex_create_network_domain(
+                    self.location,
+                    self.name,
+                    self.module.params['service_plan'],
+                    description=self.description
+                )
+        except DimensionDataAPIException:
+            api_exception = get_exception()
+
+            self.module.fail_json(
+                msg="Failed to create new network: %s" % str(api_exception)
+            )
+
+            return None
+
+        if self.module.params['wait'] is True:
+            network = self._wait_for_network_state(network.id, 'NORMAL')
+
+        return network
+
+    def _delete_network(self, network):
+        try:
+            if self.mcp_version == '1.0':
+                deleted = self.driver.ex_delete_network(network)
+            else:
+                deleted = self.driver.ex_delete_network_domain(network)
+
+            if deleted:
+                self.module.exit_json(
+                    changed=True,
+                    msg="Deleted network with id %s" % network.id
+                )
+
+            self.module.fail_json(
+                "Unexpected failure deleting network with id %s", network.id
+            )
+
+        except DimensionDataAPIException:
+            api_exception = get_exception()
+
+            self.module.fail_json(
+                msg="Failed to delete network: %s" % str(api_exception)
+            )
+
+    def _wait_for_network_state(self, net_id, state_to_wait_for):
+        try:
+            return self.driver.connection.wait_for_state(
+                state_to_wait_for,
+                self.driver.ex_get_network_domain,
+                self.module.params['wait_poll_interval'],
+                self.module.params['wait_time'],
+                net_id
+            )
+        except DimensionDataAPIException:
+            api_exception = get_exception()
+
+            self.module.fail_json(
+                msg='Network did not reach % state in time: %s' % (state_to_wait_for, api_exception.msg)
+            )
 
 
 def main():
-    module = AnsibleModule(
-        argument_spec=dict(
-            region=dict(default='na'),
-            mcp_user=dict(required=False, type='str'),
-            mcp_password=dict(required=False, type='str'),
-            location=dict(required=True, type='str'),
-            name=dict(required=True, type='str'),
-            description=dict(required=False, type='str'),
-            service_plan=dict(default='ADVANCED', choices=['ADVANCED',
-                              'ESSENTIALS']),
-            state=dict(default='present', choices=['present', 'absent']),
-            wait=dict(required=False, default=False, type='bool'),
-            wait_time=dict(required=False, default=600, type='int'),
-            wait_poll_interval=dict(required=False, default=2, type='int'),
-            verify_ssl_cert=dict(required=False, default=True, type='bool')
-        )
-    )
-
-    try:
-        credentials = get_credentials(module)
-    except LibcloudNotFound:
-        module.fail_json(msg='libcloud is required for this module.')
-
-    if not credentials:
-        module.fail_json(msg="User credentials not found")
-
-    # set short vars for readability
-    user_id = credentials['user_id']
-    key = credentials['key']
-    region = 'dd-%s' % module.params['region']
-    location = module.params['location']
-    name = module.params['name']
-    description = module.params['description']
-    verify_ssl_cert = module.params['verify_ssl_cert']
-    state = module.params['state']
-
-    # Instantiate driver
-    libcloud.security.VERIFY_SSL_CERT = verify_ssl_cert
-    DimensionData = get_driver(Provider.DIMENSIONDATA)
-    driver = DimensionData(user_id, key, region=region)
-
-    # Get MCP API Version
-    mcp_version = get_mcp_version(driver, location)
-
-    # Get network list
-    if mcp_version == '1.0':
-        networks = driver.list_networks(location=location)
-    else:
-        networks = driver.ex_list_network_domains(location=location)
-    matched_network = [network for network in networks if network.name == name]
-
-    # Ensure network state
-    if state == 'present':
-        # Network already exists
-        if matched_network:
-            module.exit_json(
-                changed=False,
-                msg="Network already exists",
-                network=network_obj_to_dict(matched_network[0], mcp_version)
-            )
-        create_network(module, driver, mcp_version, location, name,
-                       description)
-    elif state == 'absent':
-        # Destroy network
-        if matched_network:
-            delete_network(module, driver, matched_network, mcp_version)
-        else:
-            module.exit_json(changed=False, msg="Network does not exist")
-    else:
-        module.fail_json(msg="Requested state was " +
-                             "'%s'. State must be 'absent' or 'present'" % state)
+    module = DimensionDataNetworkModule()
+    if module.state == 'present':
+        module.state_present()
+    elif module.state == 'absent':
+        module.state_absent()
 
 if __name__ == '__main__':
     main()
