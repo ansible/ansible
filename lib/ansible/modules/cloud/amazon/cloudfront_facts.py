783022f2f175de79f7479c5664d7a65de89374e3
Willem van Ketwich <willvk@gmail.com>
2017-02-07 21:54:35 +1100

    reverted line lengthening as advised by @willthames

--- lib/ansible/modules/cloud/amazon/cloudfront_facts.py
+++ lib/ansible/modules/cloud/amazon/cloudfront_facts.py
@@ -245,7 +245,9 @@ class CloudFrontFactsServiceManager:
 
         try:
             region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
-            self.client = boto3_conn(module, conn_type='client', resource='cloudfront', region=region, endpoint=ec2_url, **aws_connect_kwargs)
+            self.client = boto3_conn(module, conn_type='client',
+                                     resource='cloudfront', region=region,
+                                     endpoint=ec2_url, **aws_connect_kwargs)
         except botocore.exceptions.NoRegionError:
             self.module.fail_json(msg="Region must be specified as a parameter, in AWS_DEFAULT_REGION "
                                   "environment variable or in boto configuration file")
