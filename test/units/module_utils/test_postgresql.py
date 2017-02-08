dee0919a350efba3ecf2a93c65cfd2c0a8cbbf09
tedder <ted@timmons.me>
2017-02-08 11:23:14 -0800

    use test_basic as a template for the magic swapfoo

--- test/units/module_utils/test_postgresql.py
+++ test/units/module_utils/test_postgresql.py
@@ -13,14 +13,11 @@ import pprint
 class TestPostgres(unittest.TestCase):
     def test(self):
         params = {'foo': 'blippy'}
-        # module weirdness and testing shenagigans
         args = json.dumps(dict(ANSIBLE_MODULE_ARGS=params))
-        self.stdin_swap = swap_stdin_and_argv(stdin_data=args)
-        self.stdin_swap.__enter__()
 
-        a_module = AnsibleModule(argument_spec={'foo': {'required': True, 'aliases': ['name']}})
-        res = postgres.params_to_kwmap(a_module)
-        pprint.pprint(res)
+        with swap_stdin_and_argv(stdin_data=args):
+            basic._ANSIBLE_ARGS = None
+            a_module = AnsibleModule(argument_spec={'foo': {'required': True, 'aliases': ['name']}})
+            res = postgres.params_to_kwmap(a_module)
+            pprint.pprint(res)
 
-        # unittest doesn't have a clean place to use a context manager, so we have to enter/exit manually
-        self.stdin_swap.__exit__(None, None, None)
