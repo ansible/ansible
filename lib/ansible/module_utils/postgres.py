4fb1e7dbd719c35fb58c97da1fcd7897b17bf329
tedder <ted@timmons.me>
2017-02-07 16:31:21 -0800

    rm commented-out bits from stealing code

--- lib/ansible/module_utils/postgres.py
+++ lib/ansible/module_utils/postgres.py
@@ -70,7 +70,7 @@ def params_to_kwmap(module):
 
     return kw
 
-def postgres_conn(module, database=None, kw=None, enable_autocommit=False, **params): # conn_type=None, resource=None, region=None, endpoint=None, **params):
+def postgres_conn(module, database=None, kw=None, enable_autocommit=False, **params):
 
     try:
         if psycopg2.__version__ < '2.4.3' and module.params['sslrootcert'] is not None:
