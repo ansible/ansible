22134cbe9c27522d1aa083e88f66987f9cb78af3
Adrian Likins <alikins@redhat.com>
2017-02-08 09:48:34 -0500

    unit tests improvements

--- test/units/modules/packaging/os/test_yum.py
+++ test/units/modules/packaging/os/test_yum.py
@@ -1,6 +1,5 @@
 
 # -*- coding: utf-8 -*-
-from ansible.compat.tests import mock
 from ansible.compat.tests import unittest
 
 from ansible.modules.packaging.os import yum
@@ -40,42 +39,6 @@ empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-em
 libtiff.x86_64                      4.0.3-27.el7_3                       updates
 """
 
-yum_check_update_output_rhel7 = """
-
-Loaded plugins: etckeeper, product-id, search-disabled-repos, subscription-
-              : manager
-This system is not registered to Red Hat Subscription Management. You can use subscription-manager to register.
-
-NetworkManager-openvpn.x86_64         1:1.2.6-1.el7           epel
-NetworkManager-openvpn-gnome.x86_64   1:1.2.6-1.el7           epel
-cabal-install.x86_64                  1.16.1.0-2.el7          epel
-cgit.x86_64                           1.1-1.el7               epel
-python34-libs.x86_64                  3.4.5-3.el7             epel
-python34-test.x86_64                  3.4.5-3.el7             epel
-python34-tkinter.x86_64               3.4.5-3.el7             epel
-python34-tools.x86_64                 3.4.5-3.el7             epel
-qgit.x86_64                           2.6-4.el7               epel
-rdiff-backup.x86_64                   1.2.8-12.el7            epel
-stoken-libs.x86_64                    0.91-1.el7              epel
-xlockmore.x86_64                      5.49-2.el7              epel
-Obsoleting Packages
-ddashboard.x86_64                     0.2.0.1-1.el7_3         mhlavink-developerdashboard
-    developerdashboard.x86_64         0.1.12.2-1.el7_2        @mhlavink-developerdashboard
-python-bugzilla.noarch                1.2.2-3.el7_2.1         mhlavink-developerdashboard
-    python-bugzilla-develdashboardfixes.noarch
-                                      1.2.2-3.el7             @mhlavink-developerdashboard
-python2-futures.noarch                3.0.5-1.el7             epel
-    python-futures.noarch             3.0.3-1.el7             @epel
-python2-pip.noarch                    8.1.2-5.el7             epel
-    python-pip.noarch                 7.1.0-1.el7             @epel
-python2-pyxdg.noarch                  0.25-6.el7              epel
-    pyxdg.noarch                      0.25-5.el7              @epel
-python2-simplejson.x86_64             3.10.0-1.el7            epel
-    python-simplejson.x86_64          3.3.3-1.el7             @epel
-Security: kernel-3.10.0-327.28.2.el7.x86_64 is an installed security update
-Security: kernel-3.10.0-327.22.2.el7.x86_64 is the currently running version
-"""
-
 # From https://github.com/ansible/ansible/issues/20608#issuecomment-276698431
 wrapped_output_3 = """
 Loaded plugins: fastestmirror, langpacks
@@ -112,15 +75,70 @@ sssd-client.x86_64                    1.13.0-40.el7_2.12    rhelosp-rhel-7.2-z
 """
 
 
+# A 'normal-ish' yum check-update output, without any wrapped lines
+unwrapped_output_rhel7 = """
+
+Loaded plugins: etckeeper, product-id, search-disabled-repos, subscription-
+              : manager
+This system is not registered to Red Hat Subscription Management. You can use subscription-manager to register.
+
+NetworkManager-openvpn.x86_64         1:1.2.6-1.el7           epel
+NetworkManager-openvpn-gnome.x86_64   1:1.2.6-1.el7           epel
+cabal-install.x86_64                  1.16.1.0-2.el7          epel
+cgit.x86_64                           1.1-1.el7               epel
+python34-libs.x86_64                  3.4.5-3.el7             epel
+python34-test.x86_64                  3.4.5-3.el7             epel
+python34-tkinter.x86_64               3.4.5-3.el7             epel
+python34-tools.x86_64                 3.4.5-3.el7             epel
+qgit.x86_64                           2.6-4.el7               epel
+rdiff-backup.x86_64                   1.2.8-12.el7            epel
+stoken-libs.x86_64                    0.91-1.el7              epel
+xlockmore.x86_64                      5.49-2.el7              epel
+"""
+
+# Some wrapped obsoletes for prepending to output for testing both
+wrapped_output_rhel7_obsoletes_postfix = """
+Obsoleting Packages
+ddashboard.x86_64                     0.2.0.1-1.el7_3         mhlavink-developerdashboard
+    developerdashboard.x86_64         0.1.12.2-1.el7_2        @mhlavink-developerdashboard
+python-bugzilla.noarch                1.2.2-3.el7_2.1         mhlavink-developerdashboard
+    python-bugzilla-develdashboardfixes.noarch
+                                      1.2.2-3.el7             @mhlavink-developerdashboard
+python2-futures.noarch                3.0.5-1.el7             epel
+    python-futures.noarch             3.0.3-1.el7             @epel
+python2-pip.noarch                    8.1.2-5.el7             epel
+    python-pip.noarch                 7.1.0-1.el7             @epel
+python2-pyxdg.noarch                  0.25-6.el7              epel
+    pyxdg.noarch                      0.25-5.el7              @epel
+python2-simplejson.x86_64             3.10.0-1.el7            epel
+    python-simplejson.x86_64          3.3.3-1.el7             @epel
+Security: kernel-3.10.0-327.28.2.el7.x86_64 is an installed security update
+Security: kernel-3.10.0-327.22.2.el7.x86_64 is the currently running version
+"""
+
+unwrapped_output_rhel7_obsoletes = unwrapped_output_rhel7 + wrapped_output_rhel7_obsoletes_postfix
+unwrapped_output_rhel7_expected_pkgs = ["NetworkManager-openvpn", "NetworkManager-openvpn-gnome", "cabal-install",
+                                        "cgit", "python34-libs", "python34-test", "python34-tkinter",
+                                        "python34-tools", "qgit", "rdiff-backup", "stoken-libs", "xlockmore"]
+
+
 class TestYumUpdateCheckParse(unittest.TestCase):
     def _assert_expected(self, expected_pkgs, result):
+
         for expected_pkg in expected_pkgs:
             self.assertIn(expected_pkg, result)
+        self.assertEqual(len(result), len(expected_pkgs))
+        self.assertIsInstance(result, dict)
+
+    def test_empty_output(self):
+        res = yum.parse_check_update("")
+        expected_pkgs = []
+        self._assert_expected(expected_pkgs, res)
 
     def test_plugin_load_error(self):
         res = yum.parse_check_update(yum_plugin_load_error)
-        self.assertEqual(res, {})
-        self.assertEqual(len(res), 0)
+        expected_pkgs = []
+        self._assert_expected(expected_pkgs, res)
 
     def test_wrapped_output_1(self):
         res = yum.parse_check_update(wrapped_output_1)
@@ -129,25 +147,30 @@ class TestYumUpdateCheckParse(unittest.TestCase):
 
     def test_wrapped_output_2(self):
         res = yum.parse_check_update(wrapped_output_2)
-        expected_pkgs = ["empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty", "libtiff"]
+        expected_pkgs = ["empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty-empty",
+                         "libtiff"]
 
         self._assert_expected(expected_pkgs, res)
 
     def test_wrapped_output_3(self):
         res = yum.parse_check_update(wrapped_output_3)
         expected_pkgs = ["ceph", "ceph-base", "ceph-common", "ceph-mds",
-                        "ceph-mon", "ceph-osd", "ceph-selinux", "libcephfs1",
-                        "librados2", "libradosstriper1", "librbd1", "librgw2",
+                         "ceph-mon", "ceph-osd", "ceph-selinux", "libcephfs1",
+                         "librados2", "libradosstriper1", "librbd1", "librgw2",
                          "python-cephfs", "python-rados", "python-rbd"]
         self._assert_expected(expected_pkgs, res)
 
     def test_wrapped_output_4(self):
         res = yum.parse_check_update(wrapped_output_4)
 
-        expected_pkgs = ["ipxe-roms-qemu"]
+        expected_pkgs = ["ipxe-roms-qemu", "quota", "quota-nls", "rdma", "screen",
+                         "sos", "sssd-client"]
         self._assert_expected(expected_pkgs, res)
 
-    def test_yum_check_update_output_rhel7(self):
-        res = yum.parse_check_update(yum_check_update_output_rhel7)
-        expected_pkgs = ["NetworkManager-openvpn", "xlockmore"]
-        self._assert_expected(expected_pkgs, res)
+    def test_wrapped_output_rhel7(self):
+        res = yum.parse_check_update(unwrapped_output_rhel7)
+        self._assert_expected(unwrapped_output_rhel7_expected_pkgs, res)
+
+    def test_wrapped_output_rhel7_obsoletes(self):
+        res = yum.parse_check_update(unwrapped_output_rhel7_obsoletes)
+        self._assert_expected(unwrapped_output_rhel7_expected_pkgs, res)
