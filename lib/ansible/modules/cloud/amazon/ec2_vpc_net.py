81f8d11be4276ee4a14b6a00e8bc0ec935f6d65d
s-hertel <shertel@redhat.com>
2017-02-09 08:58:02 -0500

    Removing cmp to be compatible with Python3

--- lib/ansible/modules/cloud/amazon/ec2_vpc_net.py
+++ lib/ansible/modules/cloud/amazon/ec2_vpc_net.py
@@ -149,7 +149,12 @@ def update_vpc_tags(vpc, module, vpc_obj, tags, name):
     tags.update({'Name': name})
     try:
         current_tags = dict((t.name, t.value) for t in vpc.get_all_tags(filters={'resource-id': vpc_obj.id}))
-        if cmp(tags, current_tags):
+        if sorted(tags) == sorted(current_tags):
+            for tag_key in sorted(tags):
+                if tags[tag_key] != current_tags[tag_key] and not module.check_mode:
+                    vpc.create_tags(vpc_obj.id, tags)
+                return True
+        else:
             if not module.check_mode:
                 vpc.create_tags(vpc_obj.id, tags)
             return True
