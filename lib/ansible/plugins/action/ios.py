e68f8c061deec29e610927acde224b4b8f1ba53e
Peter Sprygada <psprygada@ansible.com>
2017-02-09 19:35:14 -0500

    updates ios modules to support persistent socket
    * updates all ios modules to support persistent socket
    * adds ios action plugin to connect to device
    * adds exec_command() to ios shared module
    * fixes ios_config and ios_template local action
    * update all unit test cases
    * adds base test module for ios module testing

---
+++ lib/ansible/plugins/action/ios.py
@@ -0,0 +1,96 @@
+#
+# (c) 2016 Red Hat Inc.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import sys
+import copy
+
+from ansible.plugins.action.normal import ActionModule as _ActionModule
+from ansible.utils.path import unfrackpath
+from ansible.plugins import connection_loader
+from ansible.compat.six import iteritems
+from ansible.module_utils.ios import ios_argument_spec
+from ansible.module_utils.basic import AnsibleFallbackNotFound
+from ansible.module_utils._text import to_bytes
+
+class ActionModule(_ActionModule):
+
+    def run(self, tmp=None, task_vars=None):
+
+        self.load_provider()
+
+        provider = self._task.args['provider']
+
+        pc = copy.deepcopy(self._play_context)
+        pc.connection = 'network_cli'
+        pc.port = provider['port'] or self._play_context.port
+        pc.remote_user = provider['username'] or self._play_context.connection_user
+        pc.password = provider['password'] or self._play_context.password
+        pc.become = provider['authorize'] or False
+        pc.become_pass = provider['auth_pass']
+
+        socket_path = self._get_socket_path(pc)
+        if not os.path.exists(socket_path):
+            # start the connection if it isn't started
+            connection = self._shared_loader_obj.connection_loader.get('persistent', pc, sys.stdin)
+            connection.exec_command('EXEC: show version')
+
+        task_vars['ansible_socket'] = socket_path
+
+        if self._play_context.become_method == 'enable':
+            self._play_context.become = False
+            self._play_context.become_method = None
+
+        return super(ActionModule, self).run(tmp, task_vars)
+
+    def _get_socket_path(self, play_context):
+        ssh = connection_loader.get('ssh', class_only=True)
+        cp = ssh._create_control_path(play_context.remote_addr, play_context.port, play_context.remote_user)
+        path = unfrackpath("$HOME/.ansible/pc")
+        return cp % dict(directory=path)
+
+    def load_provider(self):
+        provider = self._task.args.get('provider', {})
+        for key, value in iteritems(ios_argument_spec):
+            if key in self._task.args:
+                provider[key] = self._task.args[key]
+            elif 'fallback' in value:
+                provider[key] = self._fallback(value['fallback'])
+            elif key not in provider:
+                provider[key] = None
+        self._task.args['provider'] = provider
+
+    def _fallback(self, fallback):
+        strategy = fallback[0]
+        args = []
+        kwargs = {}
+
+        for item in fallback[1:]:
+            if isinstance(item, dict):
+                kwargs = item
+            else:
+                args = item
+        try:
+            return strategy(*args, **kwargs)
+        except AnsibleFallbackNotFound:
+            pass
+
+
--- test/units/modules/network/ios/test_ios_command.py
+++ test/units/modules/network/ios/test_ios_command.py
@@ -19,43 +19,15 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import os
 import json
 
-from ansible.compat.tests import unittest
-from ansible.compat.tests.mock import patch, MagicMock
-from ansible.errors import AnsibleModuleExit
+from ansible.compat.tests.mock import patch
 from ansible.modules.network.ios import ios_command
-from ansible.module_utils import basic
-from ansible.module_utils._text import to_bytes
+from .ios_module import TestIosModule, load_fixture, set_module_args
 
+class TestIosCommandModule(TestIosModule):
 
-def set_module_args(args):
-    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
-    basic._ANSIBLE_ARGS = to_bytes(args)
-
-fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
-fixture_data = {}
-
-def load_fixture(name):
-    path = os.path.join(fixture_path, name)
-
-    if path in fixture_data:
-        return fixture_data[path]
-
-    with open(path) as f:
-        data = f.read()
-
-    try:
-        data = json.loads(data)
-    except:
-        pass
-
-    fixture_data[path] = data
-    return data
-
-
-class test_iosCommandModule(unittest.TestCase):
+    module = ios_command
 
     def setUp(self):
         self.mock_run_commands = patch('ansible.modules.network.ios.ios_command.run_commands')
@@ -64,7 +36,7 @@ class test_iosCommandModule(unittest.TestCase):
     def tearDown(self):
         self.mock_run_commands.stop()
 
-    def execute_module(self, failed=False, changed=False):
+    def load_fixtures(self, commands=None):
 
         def load_from_file(*args, **kwargs):
             module, commands = args
@@ -81,18 +53,6 @@ class test_iosCommandModule(unittest.TestCase):
             return output
 
         self.run_commands.side_effect = load_from_file
-
-        with self.assertRaises(AnsibleModuleExit) as exc:
-            ios_command.main()
-
-        result = exc.exception.result
-
-        if failed:
-            self.assertTrue(result.get('failed'))
-        else:
-            self.assertEqual(result.get('changed'), changed, result)
-
-        return result
 
     def test_ios_command_simple(self):
         set_module_args(dict(commands=['show version']))
