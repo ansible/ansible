6077e044439d2b6e133843a135fe66e6244f28e6
Sumit Kumar <sumit.kumar2@netapp.com>
2017-02-12 00:39:26 -0500

    Make changes to adhere to community guidelines

--- lib/ansible/modules/storage/netapp/na_cdot_aggregate.py
+++ lib/ansible/modules/storage/netapp/na_cdot_aggregate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# (c) 2016, NetApp, Inc
+# (c) 2017, NetApp, Inc
 #
 # This file is part of Ansible
 #
@@ -17,19 +17,22 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 #
+ANSIBLE_METADATA = {'status': ['preview'],
+                    'supported_by': 'community',
+                    'version': '1.0'}
 
 DOCUMENTATION = '''
 
 module: na_cdot_aggregate
 
-short_description: Manage NetApp cDOT aggregates
+short_description: Manage NetApp cDOT aggregates.
 extends_documentation_fragment:
     - netapp.ontap
 version_added: '2.3'
 author: Sumit Kumar (sumit4@netapp.com)
 
 description:
-- Create or destroy aggregates on NetApp cDOT
+- Create or destroy aggregates on NetApp cDOT.
 
 options:
 
@@ -42,18 +45,15 @@ options:
   name:
     required: true
     description:
-    - The name of the aggregate to manage
-
-  new_name:
-    required: false
-    description:
-    - Rename the aggregate
+    - The name of the aggregate to manage.
 
   disk_count:
-    required: false
-    notes: required when state == 'present'
     description:
-    - Number of disks to place into the aggregate, including parity disks. The disks in this newly-created aggregate come from the spare disk pool. The smallest disks in this pool join the aggregate first, unless the "disk-size" argument is provided. Either "disk-count" or "disks" must be supplied. Range [0..2^31-1].
+    - Number of disks to place into the aggregate, including parity disks.
+    - The disks in this newly-created aggregate come from the spare disk pool.
+    - The smallest disks in this pool join the aggregate first, unless the C(disk-size) argument is provided.
+    - Either C(disk-count) or C(disks) must be supplied. Range [0..2^31-1].
+    - Required when C(state=present).
 
 '''
 
@@ -71,7 +71,6 @@ EXAMPLES = """
           na_cdot_aggregate:
             state: present
             name: ansibleAggr
-            new_name: ansibleAggrRenamed
             hostname: "{{ netapp_hostname }}"
             username: "{{ netapp_username }}"
             password: "{{ netapp_password }}"
@@ -79,49 +78,26 @@ EXAMPLES = """
 """
 
 RETURN = """
-msg:
-    description: Successfully created new aggregate (if not already available)
-    returned: success
-    type: string
-    sample: '{"changed": true}'
-
-msg:
-    description: Successfully renamed aggregate
-    returned: success
-    type: string
-    sample: '{"changed": true}'
 
 """
-
-import logging
-from traceback import format_exc
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.pycompat24 import get_exception
 import ansible.module_utils.netapp as netapp_utils
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 HAS_NETAPP_LIB = netapp_utils.has_netapp_lib()
-
-logging.basicConfig(level=logging.ERROR)
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 
 class NetAppCDOTAggregate(object):
 
     def __init__(self):
-        logger.debug('Init %s', self.__class__.__name__)
-
         self.argument_spec = netapp_utils.ontap_sf_host_argument_spec()
         self.argument_spec.update(dict(
-                state=dict(required=True, choices=['present', 'absent']),
-                name=dict(required=True, type='str'),
-                new_name=dict(required=False, type='str', default=None),
-                disk_count=dict(required=False, type='int'),
-            ))
+            state=dict(required=True, choices=['present', 'absent']),
+            name=dict(required=True, type='str'),
+            disk_count=dict(required=False, type='int'),
+        ))
 
         self.module = AnsibleModule(
             argument_spec=self.argument_spec,
@@ -136,7 +112,6 @@ class NetAppCDOTAggregate(object):
         # set up state variables
         self.state = p['state']
         self.name = p['name']
-        self.new_name = p['new_name']
         self.disk_count = p['disk_count']
 
         if HAS_NETAPP_LIB is False:
@@ -171,7 +146,7 @@ class NetAppCDOTAggregate(object):
             if str(e.code) == "13040":
                 return False
             else:
-                raise
+                self.module.fail_json(exception=str(e))
 
         if (result.get_child_by_name('num-records') and
                 int(result.get_child_content('num-records')) >= 1):
@@ -180,8 +155,6 @@ class NetAppCDOTAggregate(object):
             return False
 
     def create_aggr(self):
-        logger.debug('Creating aggregate %s', self.name)
-
         aggr_create = netapp_utils.zapi.NaElement.create_node_with_children(
             'aggr-create', **{'aggregate': self.name,
                               'disk-count': str(self.disk_count)})
@@ -190,14 +163,11 @@ class NetAppCDOTAggregate(object):
             self.server.invoke_successfully(aggr_create,
                                             enable_tunneling=False)
         except netapp_utils.zapi.NaApiError:
-            e = get_exception()
-            logger.exception('Error provisioning aggregate %s. Error code: '
-                             '%s', self.name, str(e.code))
-            raise
+            err = get_exception()
+            self.module.fail_json(msg="Error provisioning aggregate %s." % self.name,
+                                  exception=str(err))
 
     def delete_aggr(self):
-        logger.debug('Removing aggregate %s', self.name)
-
         aggr_destroy = netapp_utils.zapi.NaElement.create_node_with_children(
             'aggr-destroy', **{'aggregate': self.name})
 
@@ -205,25 +175,23 @@ class NetAppCDOTAggregate(object):
             self.server.invoke_successfully(aggr_destroy,
                                             enable_tunneling=False)
         except netapp_utils.zapi.NaApiError:
-            e = get_exception()
-            logger.exception('Error removing aggregate %s', self.name)
-            raise
+            err = get_exception()
+            self.module.fail_json(msg="Error removing aggregate %s." % self.name,
+                                  exception=str(err))
 
     def rename_aggregate(self):
         aggr_rename = netapp_utils.zapi.NaElement.create_node_with_children(
             'aggr-rename', **{'aggregate': self.name,
                               'new-aggregate-name':
-                                  self.new_name})
+                                  self.name})
 
         try:
             self.server.invoke_successfully(aggr_rename,
                                             enable_tunneling=False)
         except netapp_utils.zapi.NaApiError:
-            e = get_exception()
-            logger.exception(
-                'Error renaming aggregate %s. Error code: '
-                '%s', self.name, str(e.code))
-            raise
+            err = get_exception()
+            self.module.fail_json(msg="Error renaming aggregate %s." % self.name,
+                                  exception=str(err))
 
     def apply(self):
         changed = False
@@ -234,30 +202,22 @@ class NetAppCDOTAggregate(object):
 
         if aggregate_exists:
             if self.state == 'absent':
-                logger.debug(
-                    "CHANGED: aggregate exists, but requested state is "
-                    "'absent'")
                 changed = True
 
             elif self.state == 'present':
-                if self.new_name is not None and not self.new_name ==\
+                if self.name is not None and not self.name == \
                         self.name:
-                    logger.debug(
-                        "CHANGED: renaming aggregate")
                     rename_aggregate = True
                     changed = True
 
         else:
             if self.state == 'present':
-                logger.debug(
-                    "CHANGED: aggregate does not exist, but requested state is"
-                    "'present'")
-
+                # Aggregate does not exist, but requested state is present.
                 changed = True
 
         if changed:
             if self.module.check_mode:
-                logger.debug('skipping changes due to check mode')
+                pass
             else:
                 if self.state == 'present':
                     if not aggregate_exists:
@@ -269,21 +229,13 @@ class NetAppCDOTAggregate(object):
 
                 elif self.state == 'absent':
                     self.delete_aggr()
-        else:
-            logger.debug("exiting with no changes")
 
         self.module.exit_json(changed=changed)
 
 
 def main():
     v = NetAppCDOTAggregate()
-
-    try:
-        v.apply()
-    except Exception:
-        e = get_exception()
-        logger.debug("Exception in apply(): \n%s" % format_exc(e))
-        raise
+    v.apply()
 
 if __name__ == '__main__':
     main()
