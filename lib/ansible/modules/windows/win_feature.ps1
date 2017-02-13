4822653a7df81d80604e9712ede76fcec33aee55
Dag Wieers <dag@wieers.com>
2017-02-13 19:40:32 +0100

    Clean up parameter handling and added check-mode support
    Changes include:
    - Remove trailing semi-colons
    - Replaced PSObjects into normal hashes
    - Make use of Get-AnsibleParam and types
    - Added check-mode support

--- lib/ansible/modules/windows/win_feature.ps1
+++ lib/ansible/modules/windows/win_feature.ps1
@@ -19,99 +19,98 @@
 # WANT_JSON
 # POWERSHELL_COMMON
 
-Import-Module Servermanager;
+Import-Module Servermanager
 
-$params = Parse-Args $args;
-
-$result = New-Object PSObject -Property @{
+$result = @{
     changed = $false
 }
 
-$name = Get-Attr $params "name" -failifempty $true
+$params = Parse-Args $args -supports_check_mode $true
+$check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
+
+$name = Get-AnsibleParam -obj $params -name "name" -type "str" -failifempty $true
+$state = Get-AnsibleParam -obj $params -name "state" -type "str" -default "present" -validateset "present","absent"
+$restart = Get-AnsibleParam -obj $params -name "restart" -type "bool" -default $false
+$includesubfeatures = Get-AnsibleParam -obj $params -name "include_sub_features" -type "bool" -default $false
+$includemanagementtools = Get-AnsibleParam -obj $params -name "include_management_tools" -type "bool" -default $false
+$source = Get-AnsibleParam -obj $params -name "source" -type "str"
 
 $name = $name -split ',' | % { $_.Trim() }
 
-$state = Get-Attr $params "state" "present"
-$state = $state.ToString().ToLower()
-If (($state -ne 'present') -and ($state -ne 'absent')) {
-    Fail-Json $result "state is '$state'; must be 'present' or 'absent'"
-}
-
-$restart = Get-Attr $params "restart" $false | ConvertTo-Bool
-$includesubfeatures = Get-Attr $params "include_sub_features" $false | ConvertTo-Bool
-$includemanagementtools = Get-Attr $params "include_management_tools" $false | ConvertTo-Bool
-$source = Get-Attr $params "source" $false
 
 # Determine which cmdlets we need to work with. Then we can set options appropriate for the cmdlet
 $installWF= $false
 $addWF = $false
 
 try {
-	# We can infer uninstall/remove if install/add cmdlets exist
-    if (Get-Command "Install-WindowsFeature" -ErrorAction SilentlyContinue) {		
-        $addCmdlet = "Install-WindowsFeature"        
-        $removeCmdlet = "Uninstall-WindowsFeature"        
-        $installWF = $true        
-    }
-    elseif (Get-Command "Add-WindowsFeature" -ErrorAction SilentlyContinue) {
+    # We can infer uninstall/remove if install/add cmdlets exist
+    if (Get-Command "Install-WindowsFeature" -ErrorAction SilentlyContinue) {
+        $addCmdlet = "Install-WindowsFeature"
+        $removeCmdlet = "Uninstall-WindowsFeature"
+        $installWF = $true
+    } elseif (Get-Command "Add-WindowsFeature" -ErrorAction SilentlyContinue) {
         $addCmdlet = "Add-WindowsFeature"
         $removeCmdlet = "Remove-WindowsFeature"
-        $addWF = $true        
+        $addWF = $true
+    } else {
+        throw [System.Exception] "Not supported on this version of Windows"
     }
-    else {
-        throw [System.Exception] "Not supported on this version of Windows"        
-    }
-}
-catch {
+} catch {
     Fail-Json $result $_.Exception.Message
 }
 
 
-If ($state -eq "present") {    
-    
-	# Base params to cover both Add/Install-WindowsFeature
+If ($state -eq "present") {
+
+    # Base params to cover both Add/Install-WindowsFeature
     $InstallParams = @{
-        "Name"=$name;
-        "Restart"=$Restart;
-        "IncludeAllSubFeature"=$includesubfeatures;
-        "ErrorAction"="SilentlyContinue"
+        Name = $name
+        Restart = $restart
+        IncludeAllSubFeature = $includesubfeatures
+        ErrorAction = "SilentlyContinue"
     }
-    
+
     # IncludeManagementTools and source are options only for Install-WindowsFeature
     if ($installWF) {
-        
+
         if ($source) {
-            if (!(test-path $source)) {            
+            if (-not (Test-Path -Path $source)) {
                 Fail-Json $result "Failed to find source path $source"
             }
-            
-            $InstallParams.add("Source",$source)
+
+            $InstallParams.add("Source", $source)
         }
 
         if ($IncludeManagementTools) {
-            $InstallParams.add("IncludeManagementTools",$includemanagementtools)
+            $InstallParams.add("IncludeManagementTools", $includemanagementtools)
         }
     }
-    
+
     try {
-        $featureresult = Invoke-Expression "$addCmdlet @InstallParams"        
-    }
-    catch {
+        if ($check_mode) {
+            $featureresult = Invoke-Expression "$addCmdlet @InstallParams -WhatIf"
+        } else {
+            $featureresult = Invoke-Expression "$addCmdlet @InstallParams"
+        }
+    } catch {
         Fail-Json $result $_.Exception.Message
     }
-}
-ElseIf ($state -eq "absent") {
 
-	$UninstallParams = @{
-        "Name"=$name;
-        "Restart"=$Restart;        
-        "ErrorAction"="SilentlyContinue"
+} ElseIf ($state -eq "absent") {
+
+    $UninstallParams = @{
+        Name = $name
+        Restart = $restart
+        ErrorAction = "SilentlyContinue"
     }
-	
-    try {        
+
+    try {
+        if ($check_mode) {
+            $featureresult = Invoke-Expression "$removeCmdlet @UninstallParams -WhatIf"
+        } else {
             $featureresult = Invoke-Expression "$removeCmdlet @UninstallParams"
-    }
-    catch {
+        }
+    } catch {
         Fail-Json $result $_.Exception.Message
     }
 }
@@ -120,18 +119,18 @@ ElseIf ($state -eq "absent") {
 # each role/feature that is installed/removed
 $installed_features = @()
 #$featureresult.featureresult is filled if anything was changed
-If ($featureresult.FeatureResult)
-{
+If ($featureresult.FeatureResult) {
+
     ForEach ($item in $featureresult.FeatureResult) {
         $message = @()
         ForEach ($msg in $item.Message) {
-            $message += New-Object PSObject -Property @{
+            $message += @{
                 message_type = $msg.MessageType.ToString()
                 error_code = $msg.ErrorCode
                 text = $msg.Text
             }
         }
-        $installed_features += New-Object PSObject -Property @{
+        $installed_features += @{
             id = $item.Id
             display_name = $item.DisplayName
             message = $message
@@ -143,17 +142,15 @@ If ($featureresult.FeatureResult)
     $result.changed = $true
 }
 
-Set-Attr $result "feature_result" $installed_features
-Set-Attr $result "success" ($featureresult.Success.ToString() | ConvertTo-Bool)
-Set-Attr $result "exitcode" $featureresult.ExitCode.ToString()
-Set-Attr $result "restart_needed" ($featureresult.RestartNeeded.ToString() | ConvertTo-Bool)
+$result.feature_result = $installed_features
+$result.success = ($featureresult.Success.ToString() | ConvertTo-Bool)
+$result.exitcode = ($featureresult.ExitCode.ToString())
+$result.restart_needed = ($featureresult.RestartNeeded.ToString() | ConvertTo-Bool)
 
 If ($result.success) {
     Exit-Json $result
-}
-ElseIf ($state -eq "present") {
+} ElseIf ($state -eq "present") {
     Fail-Json $result "Failed to add feature"
-}
-Else {
+} Else {
     Fail-Json $result "Failed to remove feature"
 }
