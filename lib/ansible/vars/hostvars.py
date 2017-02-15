ansible/ansible

commit 20bff7781c6a2aeb4ba901b86443b7423719c1b2

When iterating over hostvars yield the hostname not the host object
Also fixes HostVars to raise the correct jinja2 error type.

Fixes #16836

lib/ansible/vars/hostvars.py
@@ -21,7 +21,7 @@ __metaclass__ = type
 
 import collections
 
-from jinja2 import Undefined as j2undefined
+from jinja2.exceptions import UndefinedError
 
 from ansible import constants as C
 from ansible.template import Templar
@@ -73,7 +73,7 @@ class HostVars(collections.Mapping):
         '''
         host = self._find_host(host_name)
         if host is None:
-            raise j2undefined
+            raise UndefinedError("%s not found in hostvars" % host_name)
 
         return self._variable_manager.get_vars(loader=self._loader, host=host, include_hostvars=False)
 
@@ -102,7 +102,7 @@ class HostVars(collections.Mapping):
 
     def __iter__(self):
         for host in self._inventory.get_hosts(ignore_limits=True, ignore_restrictions=True):
-            yield host
+            yield host.name
 
     def __len__(self):
         return len(self._inventory.get_hosts(ignore_limits=True, ignore_restrictions=True))

commit 4d59a6a77600cd4d3ebc1c889075b27830879d5e

Fix bug introduced in 20bff77 regarding undefined entries in HostVars
Fixes #21084

lib/ansible/vars/hostvars.py
@@ -21,7 +21,7 @@ __metaclass__ = type
 
 import collections
 
-from jinja2.exceptions import UndefinedError
+from jinja2.runtime import Undefined
 
 from ansible import constants as C
 from ansible.template import Templar
@@ -73,7 +73,7 @@ class HostVars(collections.Mapping):
         '''
         host = self._find_host(host_name)
         if host is None:
-            raise UndefinedError("'hostvars[\"%s\"]' is undefined" % host_name)
+            raise Undefined(name="hostvars['%s']" % host_name)
 
         return self._variable_manager.get_vars(loader=self._loader, host=host, include_hostvars=False)
 