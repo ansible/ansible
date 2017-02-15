1cd60c8c3ddd65ce6cf0206226e4b35fdd76ee3c
Dan Hirsch <dan.hirsch@verint.com>
2017-02-15 11:40:30 +0200

    added GPLv3 license block and metadata...

--- lib/ansible/modules/system/reset_ssh_session.py
+++ lib/ansible/modules/system/reset_ssh_session.py
@@ -1,12 +1,35 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-# Empty module to serve as frontend for running the action plugin
+"""
+Ansible module to launch the reset_ssh_session action plugin
+
+This file is part of Ansible
+
+Ansible is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Ansible is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+ANSIBLE_METADATA = {'status': ['preview'],
+                    'supported_by': 'community',
+                    'version': '1.0'}
 
 DOCUMENTATION = '''
 ---
 module: reset_ssh_session
+version_added: "2.2"
 short_description: Remove the CP handle file forcing Ansible to reconnect on next task
-author: Dan Hirsch (@hackndoes)
+author: "Dan Hirsch (@hackndoes)"
 options:
   control_persist_path:
     description:
@@ -16,7 +39,7 @@ options:
       - None/Undefined
       - String of Path to location of the file as configuration in ansible.cfg
     default:
-      - {{ ansible_user_dir }}/.ansible/cp/ansible-ssh-{{ inventory_hostname }}-22-{{ ansible_user_id }}
+      - <ansible_user_dir>/.ansible/cp/ansible-ssh-<inventory_hostname>-22-<ansible_user_id>
 '''
 
 EXAMPLES = '''
@@ -29,7 +52,7 @@ control_path = "/tmp/my-file-%%r-%%h-22"
 
 - name: Reset the ssh connection with this configured ansible.cfg
   reset_ssh_connection:
-    control_persist_path: "{{ ansible_user_dir }}/.ansible/cp/ansible-ssh-{{ inventory_hostname }}-22-{{ ansible_user_id }}"
+    control_persist_path: "<ansible_user_dir>/.ansible/cp/ansible-ssh-<inventory_hostname>-22-<ansible_user_id>"
 '''
 
 RETURN = '''
