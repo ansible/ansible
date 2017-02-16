1b083dace5a26c074a8b4c6bac57a33da5fe5295
Ondra Machacek <omachace@redhat.com>
2017-02-16 13:52:56 +0100

    cloud: ovirt: logout if token is not used

--- lib/ansible/modules/cloud/ovirt/ovirt_quotas.py
+++ lib/ansible/modules/cloud/ovirt/ovirt_quotas.py
@@ -238,7 +238,8 @@ def main():
     check_sdk(module)
 
     try:
-        connection = create_connection(module.params.pop('auth'))
+        auth = module.params.pop('auth')
+        connection = create_connection(auth)
         datacenters_service = connection.system_service().data_centers_service()
         dc_name = module.params['datacenter']
         dc_id = getattr(search_by_name(datacenters_service, dc_name), 'id', None)
@@ -290,7 +291,7 @@ def main():
     except Exception as e:
         module.fail_json(msg=str(e), exception=traceback.format_exc())
     finally:
-        connection.close(logout=False)
+        connection.close(logout=auth.get('token') is None)
 
 
 if __name__ == "__main__":
