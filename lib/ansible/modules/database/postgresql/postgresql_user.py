66bd9620fcf9ceb8c4aa9643ec2799e900d64d0e
Ted Timmons <ted@perljam.net>
2017-02-16 11:29:43 -0800

    Postgres ssl mode prefer (#21498)
    * refactor postgres,
    * adds a basic unit test module
    * first step towards a common utils module
    * set postgresql_db doc argument defaults to what the code actually uses
    
    * unit tests that actually test a missing/found psycopg2, no dependency needed
    
    * add doc fragments, use common args, ansible2ify the imports
    
    * update dict
    
    * add AnsibleModule import
    
    * mv AnsibleModule import to correct file
    
    * restore some database utils we need
    
    * rm some more duplicated pg doc fragments
    
    * change ssl_mode from disable to prefer, add update docs
    
    * use LibraryError pattern for import verification
    
    per comments on #21435. basically LibraryError and touching up its usage in pg_db and the tests.

--- lib/ansible/modules/database/postgresql/postgresql_user.py
+++ lib/ansible/modules/database/postgresql/postgresql_user.py
@@ -128,8 +128,9 @@ options:
     description:
       - Determines whether or with what priority a secure SSL TCP/IP connection will be negotiated with the server.
       - See https://www.postgresql.org/docs/current/static/libpq-ssl.html for more information on the modes.
+      - Default of C(prefer) matches libpq default.
     required: false
-    default: disable
+    default: prefer
     choices: [disable, allow, prefer, require, verify-ca, verify-full]
     version_added: '2.3'
   ssl_rootcert:
@@ -646,7 +647,7 @@ def main():
             encrypted=dict(type='bool', default='no'),
             no_password_changes=dict(type='bool', default='no'),
             expires=dict(default=None),
-            ssl_mode=dict(default='disable', choices=['disable', 'allow', 'prefer', 'require', 'verify-ca', 'verify-full']),
+            ssl_mode=dict(default='prefer', choices=['disable', 'allow', 'prefer', 'require', 'verify-ca', 'verify-full']),
             ssl_rootcert=dict(default=None)
         ),
         supports_check_mode = True
