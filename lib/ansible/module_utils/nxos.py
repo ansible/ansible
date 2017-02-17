1127afcbdc078c7ae0bdeb560b3051e940b5068c
Peter Sprygada <privateip@users.noreply.github.com>
2017-02-16 22:11:32 -0500

    fixes issue with cli shell left open (#21548)
    The nxos action plugin will now close the shell connection once the
    module has completely run

--- lib/ansible/module_utils/nxos.py
+++ lib/ansible/module_utils/nxos.py
@@ -68,11 +68,12 @@ def get_connection(module):
     global _DEVICE_CONNECTION
     if not _DEVICE_CONNECTION:
         load_params(module)
-        transport = module.params['transport']
-        if transport == 'cli':
+        if 'transport' not in module.params:
             conn = Cli(module)
-        elif transport == 'nxapi':
+        elif module.params['transport'] == 'nxapi':
             conn = Nxapi(module)
+        else:
+            conn = Cli(module)
         _DEVICE_CONNECTION = conn
     return _DEVICE_CONNECTION
 
