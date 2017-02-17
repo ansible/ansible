2a01995945dafd605be8c5328b45e78eb58341fb
Peter Sprygada <privateip@users.noreply.github.com>
2017-02-17 13:56:00 -0500

    maps ios provider ssh_keyfile to play_context (#21597)
    This allows the key file to be specified in the playbook for the ios
    module provider argument

--- lib/ansible/plugins/action/ios.py
+++ lib/ansible/plugins/action/ios.py
@@ -56,6 +56,7 @@ class ActionModule(_ActionModule):
         pc.port = provider['port'] or self._play_context.port or 22
         pc.remote_user = provider['username'] or self._play_context.connection_user
         pc.password = provider['password'] or self._play_context.password
+        pc.private_key_file = provider['ssh_keyfile'] or self._play_context.private_key_file
         pc.timeout = provider['timeout'] or self._play_context.timeout
         pc.become = provider['authorize'] or False
         pc.become_pass = provider['auth_pass']
