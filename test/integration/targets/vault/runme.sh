30929b65da1a2b3a3581e7f2a89d0c21023c9cfd
Adrian Likins <alikins@redhat.com>
2017-02-20 17:30:00 -0500

    Add a '$@' arg to vault cli intg test command lines
    So test runner can bump verbosity, etc.

--- test/integration/targets/vault/runme.sh
+++ test/integration/targets/vault/runme.sh
@@ -21,27 +21,29 @@ ansible-vault decrypt --vault-password-file vault-password "${TEST_FILE}"
 NEW_VAULT_PASSWORD="${MYTMPDIR}/new-vault-password"
 echo "newpassword" > "${NEW_VAULT_PASSWORD}"
 
-ansible-vault encrypt --vault-password-file vault-password "${TEST_FILE}"
+ansible-vault encrypt "$@" --vault-password-file vault-password "${TEST_FILE}"
 
-ansible-vault rekey --vault-password-file vault-password --new-vault-password-file "${NEW_VAULT_PASSWORD}" "${TEST_FILE}"
+ansible-vault rekey "$@" --vault-password-file vault-password --new-vault-password-file "${NEW_VAULT_PASSWORD}" "${TEST_FILE}"
 
-ansible-vault view --vault-password-file "${NEW_VAULT_PASSWORD}" "${TEST_FILE}"
+ansible-vault view "$@" --vault-password-file "${NEW_VAULT_PASSWORD}" "${TEST_FILE}"
 
-ansible-vault decrypt --vault-password-file "${NEW_VAULT_PASSWORD}" "${TEST_FILE}"
+ansible-vault decrypt "$@" --vault-password-file "${NEW_VAULT_PASSWORD}" "${TEST_FILE}"
 
-ansible-vault encrypt_string --vault-password-file "${NEW_VAULT_PASSWORD}" "a test string"
+ansible-vault encrypt_string "$@" --vault-password-file "${NEW_VAULT_PASSWORD}" "a test string"
 
-ansible-vault encrypt_string --vault-password-file "${NEW_VAULT_PASSWORD}" --name "blippy" "a test string names blippy"
+ansible-vault encrypt_string "$@" --vault-password-file "${NEW_VAULT_PASSWORD}" --name "blippy" "a test string names blippy"
 
 
 # from stdin
-ansible-vault encrypt_string --vault-password-file "${NEW_VAULT_PASSWORD}" < "${TEST_FILE}"
+ansible-vault encrypt_string "$@" --vault-password-file "${NEW_VAULT_PASSWORD}" < "${TEST_FILE}"
 
-ansible-vault encrypt_string --vault-password-file "${NEW_VAULT_PASSWORD}" --stdin-name "the_var_from_stdin" < "${TEST_FILE}"
+ansible-vault encrypt_string "$@" --vault-password-file "${NEW_VAULT_PASSWORD}" --stdin-name "the_var_from_stdin" < "${TEST_FILE}"
 
 # write to file
-ansible-vault encrypt_string --vault-password-file "${NEW_VAULT_PASSWORD}" --name "blippy" "a test string names blippy" --output "${MYTMPDIR}/enc_string_test_file"
+ansible-vault encrypt_string "$@" --vault-password-file "${NEW_VAULT_PASSWORD}" --name "blippy" "a test string names blippy" --output "${MYTMPDIR}/enc_string_test_file"
 
+
+# test playbooks using vaulted files
 ansible-playbook test_vault.yml          -i ../../inventory -v "$@" --vault-password-file vault-password --list-tasks
 ansible-playbook test_vault.yml          -i ../../inventory -v "$@" --vault-password-file vault-password --list-hosts
 ansible-playbook test_vault.yml          -i ../../inventory -v "$@" --vault-password-file vault-password --syntax-check
