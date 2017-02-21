38511acc5852cc120a95620d9a478771724b67c5
Michael De La Rue <jababubic.margoteen@spamgourmet.com>
2017-02-21 11:12:11 +0000

    aws_api_gateway - version means version of metadata not module - fix to 1.0

--- lib/ansible/modules/cloud/amazon/aws_api_gateway.py
+++ lib/ansible/modules/cloud/amazon/aws_api_gateway.py
@@ -17,7 +17,7 @@
 
 ANSIBLE_METADATA = {'status': ['preview'],
                     'supported_by': 'community',
-                    'version': '0.1'}
+                    'version': '1.0'}
 
 DOCUMENTATION = '''
 ---
