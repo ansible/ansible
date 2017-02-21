c52b0fb95f8ad4c88976ef25a31f4aed2aa0b01e
Michael De La Rue <browfresh.mddlr@9ox.net>
2017-02-21 09:09:25 +0000

    lambda module - some tests; demonstrates no update when only one parameter changed fixed in 22701806.

---
+++ test/units/modules/cloud/amazon/test_lambda.py
@@ -0,0 +1,205 @@
+#
+# (c) 2016 Michael De La Rue
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# Make coding more python3-ish
+
+from __future__ import (absolute_import, division, print_function)
+
+from nose.plugins.skip import SkipTest
+import unittest
+import pytest
+import os
+import json
+import copy
+#from ansible.errors import AnsibleModuleExit
+from ansible.module_utils._text import to_bytes
+from ansible.module_utils import basic
+
+from ansible.compat.tests.mock import Mock, patch
+
+
+try:
+    import boto3
+    import botocore
+    from moto import mock_lambda
+except ImportError:
+    raise SkipTest("test_ec2_asg.py requires the `boto3`, `botocore`, and `moto` modules")
+
+#lambda is a keyword so we have to hack this.
+_temp = __import__("ansible.modules.cloud.amazon.lambda")
+
+lda=getattr(_temp.modules.cloud.amazon,"lambda")
+
+
+exit_return_dict={}
+
+def fake_exit_json(self,**kwargs):
+    """ store the kwargs given to exit_json rather than putting them out to stdout"""
+    global exit_return_dict
+    exit_return_dict=kwargs
+    sys.exit(0)
+
+def set_module_args(args):
+    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
+    basic._ANSIBLE_ARGS = to_bytes(args)
+
+base_function_config={
+    'FunctionName' : 'lambda_name',
+    'Role' : 'arn:aws:iam::987654321012:role/lambda_basic_execution',
+    'Handler' : 'lambda_python.my_handler',
+    'Description' : 'this that the other',
+    'Timeout' : 3,
+    'MemorySize' : 128,
+    'Runtime' : 'python2.7',
+    'CodeSha256' : 'AqMZ+xptM7aC9VXu+5jyp1sqO+Nj4WFMNzQxtPMP2n8=',
+}
+
+one_change_function_config=copy.copy(base_function_config)
+one_change_function_config['Timeout']=4
+two_change_function_config=copy.copy(one_change_function_config)
+two_change_function_config['Role']='arn:aws:iam::987654321012:role/lambda_advanced_execution'
+
+base_module_args={
+        "region": "us-west-1",
+        "name": "lambda_name",
+        "state": "present",
+        "zip_file": "test/units/modules/cloud/amazon/fixtures/thezip.zip",
+        "runtime": 'python2.7',
+        "role": 'arn:aws:iam::987654321012:role/lambda_basic_execution',
+        "memory_size": 128,
+        "timeout" : 3,
+        "handler": 'lambda_python.my_handler'
+}
+
+fake_lambda_connection = Mock()
+
+
+#TODO: def test_handle_different_types_in_config_params(monkeypatch):
+
+def test_update_lambda_if_config_changed(monkeypatch):
+
+    fake_lambda_connection.get_function.configure_mock(
+        return_value={
+            'Configuration' : two_change_function_config
+        }
+    )
+    fake_lambda_connection.update_function_configuration.configure_mock(
+        return_value={
+            'Version' : 1
+        }
+    )
+    fake_boto3_conn=Mock(return_value=fake_lambda_connection)
+
+    set_module_args(base_module_args)
+    @patch("ansible.modules.cloud.amazon.lambda.boto3_conn", fake_boto3_conn)
+    def call_module():
+        with pytest.raises(SystemExit) as exc:
+            lda.main()
+
+    call_module()
+
+    #guard against calling other than for a lambda connection (e.g. IAM)
+    fake_boto3_conn.assert_called_once()
+    fake_lambda_connection.update_function_configuration.assert_called_once()
+
+def test_update_lambda_if_only_one_config_item_changed(monkeypatch):
+
+    fake_lambda_connection = Mock()
+    fake_lambda_connection.get_function.configure_mock(
+        return_value={
+            'Configuration' : one_change_function_config
+        }
+    )
+    fake_lambda_connection.update_function_configuration.configure_mock(
+        return_value={
+            'Version' : 1
+        }
+    )
+    fake_boto3_conn=Mock(return_value=fake_lambda_connection)
+
+    @patch("ansible.modules.cloud.amazon.lambda.boto3_conn", fake_boto3_conn)
+    def call_module():
+        with pytest.raises(SystemExit) as exc:
+            lda.main()
+
+    call_module()
+
+    #guard against calling other than for a lambda connection (e.g. IAM)
+    fake_boto3_conn.assert_called_once()
+    fake_lambda_connection.update_function_configuration.assert_called_once()
+
+def test_dont_update_lambda_if_nothing_changed(monkeypatch):
+
+    fake_lambda_connection = Mock()
+    fake_lambda_connection.get_function.configure_mock(
+        return_value={
+            'Configuration' : base_function_config
+        }
+    )
+    fake_lambda_connection.update_function_configuration.configure_mock(
+        return_value={
+            'Version' : 1
+        }
+    )
+    fake_boto3_conn=Mock(return_value=fake_lambda_connection)
+
+    @patch("ansible.modules.cloud.amazon.lambda.boto3_conn", fake_boto3_conn)
+    def call_module():
+        with pytest.raises(SystemExit) as exc:
+            lda.main()
+
+    call_module()
+
+    #guard against calling other than for a lambda connection (e.g. IAM)
+    fake_boto3_conn.assert_called_once()
+    fake_lambda_connection.update_function_configuration.assert_not_called()
+
+
+@mock_lambda
+def test_warn_region_not_specified():
+
+    set_module_args({
+        "name": "lambda_name",
+        "state": "present",
+# Module is called without a region causing error 
+#        "region": "us-east-1",
+        "zip_file": "test/units/modules/cloud/amazon/fixtures/thezip.zip",
+        "runtime": 'python2.7',
+        "role": 'arn:aws:iam::987654321012:role/lambda_basic_execution',
+        "handler": 'lambda_python.my_handler'})
+
+    def side_effect(*args,**kwargs):
+        if "region must be specified" in kwargs['msg']:
+            raise(SystemExit("BOOM NO REGION"))
+        else:
+            try:
+                raise(SystemExit("Module failed with message:" + kwargs['msg']))
+            except KeyError:
+                raise(SystemExit("Module failed without message"))
+
+    fake_fail_json=Mock(side_effect=side_effect)
+    @patch("ansible.module_utils.basic.AnsibleModule.fail_json", fake_fail_json)
+    def call_module():
+        with pytest.raises(SystemExit) as e:
+            lda.main()
+        assert ("BOOM NO REGION" in str(e))
+
+    call_module()
+
+    fake_fail_json.assert_called_once()
+
