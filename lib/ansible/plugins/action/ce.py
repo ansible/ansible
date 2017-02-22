8c00d97e492f067c104c8888efba77d6ddaf2255
justbiaoliu <liubiao1@huawei.com>
2017-02-22 10:21:19 +0800

    update plugins/action/ce.py
    update plugins/action/ce.py

--- lib/ansible/plugins/action/ce.py
+++ lib/ansible/plugins/action/ce.py
@@ -31,34 +31,60 @@ from ansible.module_utils.ce import ce_argument_spec
 from ansible.module_utils.basic import AnsibleFallbackNotFound
 from ansible.module_utils._text import to_bytes
 
+try:
+    from __main__ import display
+except ImportError:
+    from ansible.utils.display import Display
+    display = Display()
 
 class ActionModule(_ActionModule):
 
     def run(self, tmp=None, task_vars=None):
+        if self._play_context.connection != 'local':
+            return dict(
+                failed=True,
+                msg='invalid connection specified, expected connection=local, '
+                    'got %s' % self._play_context.connection
+            )
 
         provider = self.load_provider()
-        transport = provider['transport']
+        transport = provider['transport'] or 'cli'
 
-        if not transport or 'cli' in transport:
+        display.vvvv('connection transport is %s' % transport, self._play_context.remote_addr)
+
+        if transport == 'cli':
             pc = copy.deepcopy(self._play_context)
             pc.connection = 'network_cli'
             pc.network_os = 'ce'
             pc.port = provider['port'] or self._play_context.port or 22
             pc.remote_user = provider['username'] or self._play_context.connection_user
             pc.password = provider['password'] or self._play_context.password
+            pc.timeout = provider['timeout'] or self._play_context.timeout
+            pc.remote_addr = provider['host'] or self._play_context.remote_addr
 
+            connection = self._shared_loader_obj.connection_loader.get('persistent', pc, sys.stdin)
             socket_path = self._get_socket_path(pc)
+            display.vvvv('socket_path: %s' % socket_path, pc.remote_addr)
+
             if not os.path.exists(socket_path):
+                display.vvvv('socket_path not exists: %s' % socket_path, pc.remote_addr)
                 # start the connection if it isn't started
-                connection = self._shared_loader_obj.connection_loader.get('persistent', pc, sys.stdin)
-                connection.exec_command('EXEC: display version')
+                rc, out, err = connection.exec_command('open_shell()')
+                display.vvv('open_shell() returned %s %s %s' % (rc, out, err))
+                if rc != 0:
+                    return {'failed': True, 'msg': 'unable to open shell', 'rc': rc}
+            else:
+                display.vvvv('socket_path exists: %s' % socket_path, pc.remote_addr)
+                # make sure we are in the right cli context which should be
+                # enable mode and not config module
+                rc, out, err = connection.exec_command('prompt()')
+                display.vvvv('prompt is: %s, error: %s' % (out, err),  pc.remote_addr)
+                while str(out).strip().endswith(']'):
+                    display.vvvv('wrong context, sending exit to device', self._play_context.remote_addr)
+                    connection.exec_command('return')
+                    rc, out, err = connection.exec_command('prompt()')
 
             task_vars['ansible_socket'] = socket_path
-
-        transport = self._task.args.get('transport')
-        if not transport:
-            transport = self._task.args.get('provider', {}).get('transport')
-        self._task.args['transport'] = transport or 'cli'
 
         return super(ActionModule, self).run(tmp, task_vars)
 
