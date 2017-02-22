f6af85e95d7f5fd30394ad69a74ee8a505dd47c7
justbiaoliu <liubiao1@huawei.com>
2017-02-22 10:11:46 +0800

    update module_utils/ce.py to support netconf
    update module_utils/ce.py to support netconf

--- lib/ansible/module_utils/ce.py
+++ lib/ansible/module_utils/ce.py
@@ -38,6 +38,15 @@ from ansible.module_utils.six import iteritems
 from ansible.module_utils.urls import fetch_url
 
 
+try:
+    from ncclient import manager
+    from ncclient import xml_
+    HAS_NCCLIENT = True
+except ImportError:
+    HAS_NCCLIENT = False
+    pass
+
+
 _DEVICE_CONNECTION = None
 
 ce_argument_spec = {
@@ -48,7 +57,7 @@ ce_argument_spec = {
     'use_ssl': dict(type='bool'),
     'validate_certs': dict(type='bool'),
     'timeout': dict(type='int'),
-    'provider': dict(type='dict'),
+    'provider': dict(type='dict', no_log=True),
     'transport': dict(choices=['cli'])
 }
 
@@ -73,6 +82,20 @@ def get_connection(module):
         conn = Cli(module)
         _DEVICE_CONNECTION = conn
     return _DEVICE_CONNECTION
+
+def rm_config_prefix(cfg):
+    if not cfg:
+        return cfg
+
+    cmds = cfg.split("\n")
+    for i in range(len(cmds)):
+        if not cmds[i]:
+            continue
+        if '~' in cmds[i]:
+            index = cmds[i].index('~')
+            if cmds[i][:index] == ' ' * index:
+                cmds[i] = cmds[i].replace("~", "", 1)
+    return '\n'.join(cmds)
 
 class Cli:
 
@@ -100,6 +123,12 @@ class Cli:
             if rc != 0:
                 self._module.fail_json(msg=err)
             cfg = str(out).strip()
+            # remove default configuration prefix '~'
+            for flag in flags:
+                if "include-default" in flag:
+                    cfg = rm_config_prefix(cfg)
+                    break
+
             self._device_configs[cmd] = cfg
             return cfg
 
@@ -114,7 +143,7 @@ class Cli:
             rc, out, err = self.exec_command(cmd)
 
             if check_rc and rc != 0:
-                self._module.fail_json(msg=err)
+                self._module.fail_json(msg=cli_err_msg(cmd.strip(), err))
 
             try:
                 out = self._module.from_json(out)
@@ -137,9 +166,40 @@ class Cli:
         for cmd in config:
             rc, out, err = self.exec_command(cmd)
             if rc != 0:
-                self._module.fail_json(msg=err)
+                self._module.fail_json(msg=cli_err_msg(cmd.strip(), err))
 
         self.exec_command('return')
+
+
+def cli_err_msg(cmd, err):
+    """ get cli exception message"""
+
+    if not err:
+        return "Error: Fail to get cli exception message."
+
+    msg = list()
+    err_list = str(err).split("\r\n")
+    for err in err_list:
+        err = err.strip('.,\r\n\t ')
+        if not err:
+            continue
+        if cmd and cmd == err:
+            continue
+        if " at '^' position" in err:
+            err = err.replace(" at '^' position", "").strip()
+        err = err.strip('.,\r\n\t ')
+        if err == "^":
+            continue
+        if len(err) > 2 and err[0] in ["<", "["] and err[-1] in [">", "]"]:
+            continue
+        err.strip('.,\r\n\t ')
+        if err:
+            msg.append(err)
+
+    if cmd:
+        msg.insert(0, "Command: %s" % cmd)
+
+    return ", ".join(msg).capitalize() + "."
 
 
 def to_command(module, commands):
@@ -161,8 +221,155 @@ def get_config(module, flags=[]):
 
 def run_commands(module, commands, check_rc=True):
     conn = get_connection(module)
-    return conn.run_commands(to_command(module, commands))
+    return conn.run_commands(to_command(module, commands), check_rc)
 
 def load_config(module, config):
     conn = get_connection(module)
     return conn.load_config(config)
+
+
+def ce_unknown_host_cb(host, fingerprint):
+    """ ce_unknown_host_cb """
+
+    return True
+
+def get_nc_set_id(xml_str):
+    """get netconf set-id value"""
+
+    result = re.findall(r'<rpc-reply.+?set-id=\"(\d+)\"', xml_str)
+    if not result:
+        return None
+    return result[0]
+
+
+def get_xml_line(xml_list, index):
+    """get xml specified line valid string data"""
+
+    ele = None
+    while xml_list and not ele:
+        if index >= 0 and index >= len(xml_list):
+            return None
+        if index < 0 and abs(index) > len(xml_list):
+            return None
+
+        ele = xml_list[index]
+        if not ele.replace(" ", ""):
+            xml_list.pop(index)
+            ele = None
+    return ele
+
+
+def merge_xml(xml1, xml2):
+    """merge xml1 and xml2"""
+
+    xml1_list = xml1.split("</data>")[0].split("\n")
+    xml2_list = xml2.split("<data>")[1].split("\n")
+
+    while True:
+        xml1_ele1 = get_xml_line(xml1_list, -1)
+        xml1_ele2 = get_xml_line(xml1_list, -2)
+        xml2_ele1 = get_xml_line(xml2_list, 0)
+        xml2_ele2 = get_xml_line(xml2_list, 1)
+        if not xml1_ele1 or not xml1_ele2 or not xml2_ele1 or not xml2_ele2:
+            return xml1
+
+        if "xmlns" in xml2_ele1:
+            xml2_ele1 = xml2_ele1.lstrip().split(" ")[0] + ">"
+        if "xmlns" in xml2_ele2:
+            xml2_ele2 = xml2_ele2.lstrip().split(" ")[0] + ">"
+        if xml1_ele1.replace(" ", "").replace("/", "") == xml2_ele1.replace(" ", "").replace("/", ""):
+            if xml1_ele2.replace(" ", "").replace("/", "") == xml2_ele2.replace(" ", "").replace("/", ""):
+                xml1_list.pop()
+                xml2_list.pop(0)
+            else:
+                break
+        else:
+            break
+
+    return "\n".join(xml1_list + xml2_list)
+
+
+class Netconf(object):
+    """ Netconf """
+
+    def __init__(self, **kwargs):
+
+        if not HAS_NCCLIENT:
+            raise Exception("the ncclient library is required")
+
+        self.mc = None
+
+        host = kwargs["host"]
+        port = kwargs["port"]
+        username = kwargs["username"]
+        password = kwargs["password"]
+
+        self.mc = manager.connect(host=host, port=port,
+                                  username=username,
+                                  password=password,
+                                  unknown_host_cb=ce_unknown_host_cb,
+                                  allow_agent=False,
+                                  look_for_keys=False,
+                                  hostkey_verify=False,
+                                  device_params={'name': 'huawei'},
+                                  timeout=30)
+
+    def __del__(self):
+
+        self.mc.close_session()
+
+    def set_config(self, **kwargs):
+        """ set_config """
+
+        confstr = kwargs["config"]
+        con_obj = self.mc.edit_config(target='running', config=confstr)
+
+        return con_obj
+
+    def get_config(self, **kwargs):
+        """ get_config """
+
+        filterstr = kwargs["filter"]
+        con_obj = self.mc.get(filter=filterstr)
+        set_id = get_nc_set_id(con_obj.xml)
+        if not set_id:
+            return con_obj
+
+        # continue to get next
+        xml_str = con_obj.xml
+        while set_id:
+            set_attr = dict()
+            set_attr["set-id"] = str(set_id)
+            xsd_fetch = xml_.new_ele_ns('get-next', "http://www.huawei.com/netconf/capability/base/1.0", set_attr)
+            # get next data
+            con_obj_next = self.mc.dispatch(xsd_fetch)
+            if "<data/>" in con_obj_next.xml:
+                break
+            # merge two xml data
+            xml_str = merge_xml(xml_str, con_obj_next.xml)
+            set_id = get_nc_set_id(con_obj_next.xml)
+
+        con_obj._raw = xml_str
+        return con_obj
+
+    def execute_action(self, **kwargs):
+        """huawei execute-action"""
+
+        confstr = kwargs["action"]
+        con_obj = self.mc.action(action=confstr)
+
+        return con_obj
+
+    def execute_cli(self, **kwargs):
+        """huawei execute-cli"""
+
+        confstr = kwargs["command"]
+        con_obj = self.mc.cli(command=confstr)
+
+        return con_obj
+
+
+def get_netconf(**kwargs):
+    """ get_netconf """
+
+    return Netconf(**kwargs)
