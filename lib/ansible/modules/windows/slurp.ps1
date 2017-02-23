55e76ad0e4fea49c4a6a9483e5ce5db7220fbc00
Dag Wieers <dag@wieers.com>
2017-02-24 00:11:29 +0100

    Revert to original formatting

--- lib/ansible/modules/windows/slurp.ps1
+++ lib/ansible/modules/windows/slurp.ps1
@@ -17,20 +17,25 @@
 # WANT_JSON
 # POWERSHELL_COMMON
 
-$params = Parse-Args $args -supports_check_mode $true
-$src = Get-AnsibleParam -obj $params -name "src" -type "path" -aliases "path" -failifempty $true
+$params = Parse-Args $args -supports_check_mode $true;
+$src = Get-AnsibleParam -obj $params -name "src" -type "path" -aliases "path" -failifempty $true;
 
 $result = @{
-    changed = $false
+    changed = $false;
 }
 
-if (Test-Path -Path $src -PathType Leaf) {
-    $bytes = [System.IO.File]::ReadAllBytes($src)
-    $result.content = [System.Convert]::ToBase64String($bytes)
-    $result.encoding = "base64"
-    Exit-Json $result
-} elseif (Test-Path -Path $src -PathType Container) {
-    Fail-Json $result "Path $src is a directory"
-} else {
-    Fail-Json $result "Path $src is not found"
+If (Test-Path -Path $src -PathType Leaf)
+{
+    $bytes = [System.IO.File]::ReadAllBytes($src);
+    $result.content = [System.Convert]::ToBase64String($bytes);
+    $result.encoding = "base64";
+    Exit-Json $result;
+}
+ElseIf (Test-Path -Path $src -PathType Container)
+{
+    Fail-Json $result "Path $src is a directory";
+}
+Else
+{
+    Fail-Json $result "Path $src is not found";
 }
