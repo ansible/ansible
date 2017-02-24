1f136d78d38de12e9b7db179da43a663b388672c
Adrian Likins <alikins@redhat.com>
2017-02-24 12:18:22 -0500

    Add pool id/name matches to 'to keep' list
    use subscribe() to use the pools-first logic on update

--- lib/ansible/modules/packaging/os/redhat_subscription.py
+++ lib/ansible/modules/packaging/os/redhat_subscription.py
@@ -432,11 +432,12 @@ class Rhsm(RegistrationBase):
         changed = False
         consumed_pools = RhsmPools(self.module, consumed=True)
         pool_ids_to_keep = [p.get_pool_id() for p in consumed_pools.filter_pools(regexp)]
+        pool_ids_to_keep.extend([p.get_pool_id() for p in consumed_pools.filter_products(regexp)])
 
         serials_to_remove = [p.Serial for p in consumed_pools if p.get_pool_id() not in pool_ids_to_keep]
         serials = self.unsubscribe(serials=serials_to_remove)
 
-        subscribed_pool_ids = self.subscribe_pool(regexp)
+        subscribed_pool_ids = self.subscribe(regexp)
 
         if subscribed_pool_ids or serials:
             changed = True
