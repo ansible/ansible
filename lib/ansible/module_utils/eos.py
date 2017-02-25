cabd2da39bf98806b46709fbf5e2e528852026f6
Peter Sprygada <privateip@users.noreply.github.com>
2017-02-25 15:20:32 -0500

    fixes issue that caused eapi transport setting to get lost (#21919)
    The eos action didn't properly set provider transport argument for the
    module.  This patch fixes that problem

--- lib/ansible/module_utils/eos.py
+++ lib/ansible/module_utils/eos.py
@@ -79,9 +79,9 @@ def get_connection(module):
     global _DEVICE_CONNECTION
     if not _DEVICE_CONNECTION:
         load_params(module)
-        if 'transport' not in module.params:
-            conn = Cli(module)
-        elif module.params['transport'] == 'eapi':
+        transport = module.params['transport']
+        provider_transport = (module.params['provider'] or {}).get('transport')
+        if 'eapi' in (transport, provider_transport):
             conn = Eapi(module)
         else:
             conn = Cli(module)
