ac36f6501748a459d2ebfc6d636596013da570aa
Nathaniel Case <this.is@nathanielca.se>
2017-02-25 21:12:49 -0500

    Fix a few errors (#21854)

--- lib/ansible/plugins/action/eos.py
+++ lib/ansible/plugins/action/eos.py
@@ -57,8 +57,8 @@ class ActionModule(_ActionModule):
             pc.connection = 'network_cli'
             pc.network_os = 'eos'
             pc.remote_user = provider['username'] or self._play_context.connection_user
-            pc.password = provider['password'] or self._play_context.password or 22
-            pc.privateip_key_file = provider['ssh_keyfile'] or self._play_context.private_key_file
+            pc.password = provider['password'] or self._play_context.password
+            pc.private_key_file = provider['ssh_keyfile'] or self._play_context.private_key_file
             pc.timeout = provider['timeout'] or self._play_context.timeout
             pc.become = provider['authorize'] or False
             pc.become_pass = provider['auth_pass']
@@ -138,4 +138,3 @@ class ActionModule(_ActionModule):
             return strategy(*args, **kwargs)
         except AnsibleFallbackNotFound:
             pass
-
