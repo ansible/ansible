876b9dbd8c662f0efd342a0c78a0a3776f55d8b5
Wittmer, Christian <chris@computersalat.de>
2017-02-27 21:01:02 +0100

    fix for #21729, use VERSION_ID instead of PRETTY_NAME to get release (#21730)
    * fix for #21729, use VERSION_ID instead of PRETTY_NAME to get release

--- test/units/module_utils/test_distribution_version.py
+++ test/units/module_utils/test_distribution_version.py
@@ -153,7 +153,7 @@ CODENAME = Malachite
         "result":{
             "distribution": "openSUSE Leap",
             "distribution_major_version": "42",
-            "distribution_release": "x86_64",
+            "distribution_release": "1",
             "os_family": "Suse",
             "distribution_version": "42.1",
         }
@@ -179,7 +179,7 @@ ID_LIKE="suse"
         'platform.dist': ('SuSE', '13.2', 'x86_64'),
         'result': {'distribution': u'openSUSE',
                    'distribution_major_version': u'13',
-                   'distribution_release': u'Harlequin',
+                   'distribution_release': u'2',
                    'os_family': u'Suse',
                    'distribution_version': u'13.2'}
     },
@@ -196,7 +196,7 @@ ID_LIKE="suse"
         },
         "name": "openSUSE Tumbleweed 20160917",
         "result": {
-            "distribution_release": "NA",
+            "distribution_release": "",
             "distribution": "openSUSE Tumbleweed",
             "distribution_major_version": "NA",
             "os_family": "Suse",
