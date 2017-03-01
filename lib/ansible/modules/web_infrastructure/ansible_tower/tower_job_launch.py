7bde7016161d85038897e5c7a89a664e0acf32d1
Wayne Witzel III <wayne@riotousliving.com>
2017-03-01 12:36:15 -0500

    remove superfluos required attributes, make tags a list, and fix some typos

--- lib/ansible/modules/web_infrastructure/ansible_tower/tower_job_launch.py
+++ lib/ansible/modules/web_infrastructure/ansible_tower/tower_job_launch.py
@@ -36,43 +36,35 @@ options:
     job_explanation:
       description:
         - Job explanation field.
-      required: False
       default: null
     job_type:
       description:
         - Job_type to use for the job, only used if prompt for job_type is set.
-      required: False
       choices: ["run", "check", "scan"]
       default: null
     inventory:
       description:
         - Inventory to use for the job, only used if prompt for inventory is set.
-      required: False
       default: null
     credential:
       description:
         - Credential to use for job, only used if prompt for credential is set.
-      required: False
       default: null
     extra_vars:
       description:
         - Extra_vars to use for the job_template. Use '@' for a file.
-      required: False
       default: null
     limit:
       description:
         - Limit to use for the job_template.
-      required: False
       default: null
     tags:
       description:
         - Specific tags to use for from playbook.
-      required: False
       default: null
     use_job_endpoint:
       description:
         - Disable launching jobs from job template.
-      required: False
       default: False
 extends_documentation_fragment: tower
 '''
@@ -125,8 +117,8 @@ def main():
         inventory = dict(),
         credential = dict(),
         limit = dict(),
-        tags = dict(),
-        extra_vars = dict(type='list', required=False),
+        tags = dict(type='list'),
+        extra_vars = dict(type='list'),
     ))
 
     module = AnsibleModule(
@@ -138,12 +130,14 @@ def main():
         module.fail_json(msg='ansible-tower-cli required for this module')
 
     json_output = {}
+    tags = module.params.get('tags', [])
 
     tower_auth = tower_auth_config(module)
     with settings.runtime_values(**tower_auth):
         tower_check_mode(module)
         try:
             params = module.params.copy()
+            params['tags'] = ','.join(tags)
             job = tower_cli.get_resource('job')
 
             lookup_fields = ('job_template', 'inventory', 'credential')
@@ -153,7 +147,7 @@ def main():
                     result = tower_cli.get_resource(field).get(name=name)
                     params[field] = result['id']
                 except exc.NotFound as excinfo:
-                    module.fail_json(msg='Unalbe to launch job, {0}({1}) was not found: {2}'.format(field, name, excinfo), changed=False)
+                    module.fail_json(msg='Unable to launch job, {0}/{1} was not found: {2}'.format(field, name, excinfo), changed=False)
 
             result = job.launch(no_input=True, **params)
             json_output['id'] = result['id']
