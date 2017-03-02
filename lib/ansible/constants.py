9bd15c10b8009176f2275a931838c29e92a80105
azenk <andrew@andrewzenk.com>
2017-03-01 19:23:18 -0600

    Add role skeleton support (#17079)
    * Add role skeleton support
    
    The default role skeleton used by ansible-galaxy is good, but it doesn't
    allow organizations to customize it to suit their needs.  This patch
    addresses that by adding the option to point ansible-galaxy at a
    role skeleton directory.  The contents of this directory are then
    copied (or rendered) into the output role directory.  Appropriate
    command line options and configuration entries are added to allow for
    further customization from the role skeleton.
    
    * Remove dependency on platforms list from test
    
    Platforms are no longer provided to the template by galaxy init.
    Removing the code in our test meta/main.yml template that relied on it.
    
    * Fix whitespace issue

--- lib/ansible/constants.py
+++ lib/ansible/constants.py
@@ -364,6 +364,8 @@ GALAXY_SERVER                  = get_config(p, 'galaxy', 'server', 'ANSIBLE_GALA
 GALAXY_IGNORE_CERTS            = get_config(p, 'galaxy', 'ignore_certs', 'ANSIBLE_GALAXY_IGNORE', False, value_type='boolean')
 # this can be configured to blacklist SCMS but cannot add new ones unless the code is also updated
 GALAXY_SCMS                    = get_config(p, 'galaxy', 'scms', 'ANSIBLE_GALAXY_SCMS', 'git, hg', value_type='list')
+GALAXY_ROLE_SKELETON = get_config(p, 'galaxy', 'role_skeleton', 'ANSIBLE_GALAXY_ROLE_SKELETON', None, value_type='path')
+GALAXY_ROLE_SKELETON_IGNORE = get_config(p, 'galaxy', 'role_skeleton_ignore', 'ANSIBLE_GALAXY_ROLE_SKELETON_IGNORE', ['^.git$', '^.*/.git_keep$'], value_type='list')
 
 STRING_TYPE_FILTERS = get_config(p, 'jinja2', 'dont_type_filters', 'ANSIBLE_STRING_TYPE_FILTERS', ['string', 'to_json', 'to_nice_json', 'to_yaml', 'ppretty', 'json'], value_type='list' )
 
