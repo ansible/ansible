5f58cea905ee0c7c64a04b10d957467480358331
Nathaniel Case <ncase@redhat.com>
2017-03-02 14:57:24 -0500

    I don't know what's wrong with this

--- lib/ansible/module_utils/junos.py
+++ lib/ansible/module_utils/junos.py
@@ -175,7 +175,6 @@ def get_config(module, flags=[]):
 def run_commands(module, commands, check_rc=True):
     responses = list()
     for cmd in to_list(commands):
-        cmd = module.jsonify(cmd)
         rc, out, err = exec_command(module, cmd)
         if check_rc and rc != 0:
             module.fail_json(msg=err, rc=rc)
