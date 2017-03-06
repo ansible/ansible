15d33303c13440de192967b9c166b22b13bc75c9
bonuscheese <steakncheese@gmail.com>
2017-03-06 15:43:59 +1000

    ec2_elb_facts - Fix for empty health_check path
    This change was made after troubleshooting errors for ELBs that have a Health Check path that is just a slash - i.e. HTTP:2000/
    
    In these cases the port_path.split('/', 1) will leave 'path' as an empty var and an error is thrown when it then tries to format this empty string. In dealing with this error, the whole port_path var is then asserted as an INT and set as the 'port' value. This obviously throws a module failure as the port value still has a slash in it - i.e. '2000/'
    
    To get around this I've added a check that will set 'path' as a slash if the path var appears empty, therefore the format will still work and the error isn't thrown which would result in an incorrect INT assertion.

--- lib/ansible/modules/cloud/amazon/ec2_elb_facts.py
+++ lib/ansible/modules/cloud/amazon/ec2_elb_facts.py
@@ -143,7 +143,10 @@ class ElbInformation(object):
         protocol, port_path = health_check.target.split(':')
         try:
             port, path = port_path.split('/', 1)
-            path = '/{}'.format(path)
+            if path == '':
+                path = '/'  
+            else:
+                path = '/{}'.format(path)
         except ValueError:
             port = port_path
             path = None
