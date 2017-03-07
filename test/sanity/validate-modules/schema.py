239d032da57a4ee87a748081d348ac1e9261729e
John Barker <john@johnrbarker.com>
2017-03-07 10:09:33 +0000

    Stricter module documentation validation

--- test/sanity/validate-modules/schema.py
+++ test/sanity/validate-modules/schema.py
@@ -16,32 +16,45 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from voluptuous import ALLOW_EXTRA, Any, Required, Schema
+from voluptuous import PREVENT_EXTRA, Any, Required, Schema
 
 option_schema = Schema(
     {
         Required('description'): Any(basestring, [basestring]),
         'required': bool,
         'choices': list,
-        'aliases': list,
-        'version_added': Any(basestring, float)
+        'aliases': Any(basestring, list),
+        'version_added': Any(basestring, float),
+        'default': Any(None, basestring, float, int, bool, list, dict),
+        # FIXME: Recursive Schema: https://github.com/alecthomas/voluptuous/issues/128
+        'subspec': Any(None, basestring),
     },
-    extra=ALLOW_EXTRA
+    extra=PREVENT_EXTRA
 )
 
 doc_schema = Schema(
     {
         Required('module'): basestring,
-        'short_description': basestring,
-        'description': Any(basestring, [basestring]),
-        'version_added': Any(basestring, float),
-        'author': Any(None, basestring, [basestring]),
+        'deprecated': basestring,
+        Required('short_description'): basestring,
+        Required('description'): Any(basestring, [basestring]),
+        # FIXME version_added should be a quoted float, legacy marker, NOT an int/float
+        Required('version_added'): Any(basestring, float),
+        Required('author'): Any(None, basestring, [basestring]),
         'notes': Any(None, [basestring]),
         'requirements': [basestring],
-        'options': Any(None, dict),
+        'todo': Any(None, basestring, [basestring]),
+        # Note: Types are strings, not literal bools, such as True or False
+        'type': Any(None, "bool"),
+        'options': Any(
+            None,
+            {
+                basestring: option_schema
+                }
+        ),
         'extends_documentation_fragment': Any(basestring, [basestring])
     },
-    extra=ALLOW_EXTRA
+    extra=PREVENT_EXTRA
 )
 
 metadata_schema = Schema(
