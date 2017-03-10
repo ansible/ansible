12635054bfb96874f8c9d0a1ca4b16ad9c9c7670
Johannes Kohnen <kohnen@traum-ferienwohnungen.de>
2017-03-10 11:31:18 +0100

    Enable path expansion in GCE modules
    This is basically a replay of #16064 from `gce.py` to other pertinent
    `gce_*.py` files. It enables expansions of paths in `gce.ini` like
    `~/.project.json`

--- lib/ansible/modules/cloud/google/gce_mig.py
+++ lib/ansible/modules/cloud/google/gce_mig.py
@@ -721,8 +721,8 @@ def main():
         named_ports=dict(type='list', default=None),
         service_account_email=dict(),
         service_account_permissions=dict(type='list'),
-        pem_file=dict(),
-        credentials_file=dict(),
+        pem_file=dict(type='path'),
+        credentials_file=dict(type='path'),
         project_id=dict(), ), )
 
     if not HAS_PYTHON26:
