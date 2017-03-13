2553a37da52c3c54f4b654f348189f49e1043c74
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2017-03-13 20:21:46 +0100

    Add no_log to all network module_utils provider argument (#22566)
    The provider argument on the network modules argspec can contain
    credentials.
    Make sure we don't log it.
    
    Fixes #21892

--- lib/ansible/module_utils/eos.py
+++ lib/ansible/module_utils/eos.py
@@ -54,7 +54,7 @@ eos_argument_spec = {
     'validate_certs': dict(type='bool'),
     'timeout': dict(type='int'),
 
-    'provider': dict(type='dict'),
+    'provider': dict(type='dict', no_log=True),
     'transport': dict(choices=['cli', 'eapi'])
 }
 
