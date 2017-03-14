7966320472e0928cb68dfe0a3fdf19123d6996be
Igor T <tsiganenok@gmail.com>
2017-03-14 22:55:08 +0200

    metadata_version fix

--- lib/ansible/modules/cloud/amazon/ec2_asg_lifecycle_hook.py
+++ lib/ansible/modules/cloud/amazon/ec2_asg_lifecycle_hook.py
@@ -15,7 +15,7 @@
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 ANSIBLE_METADATA = {'status': ['preview'],
                     'supported_by': 'community',
-                    'version': '1.0'}
+                    'metadata_version': '1.0'}
 
 DOCUMENTATION = """
 ---
