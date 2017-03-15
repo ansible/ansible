869449e288cc02986984daec430086f6b6fe656f
Matt Clay <matt@mystile.com>
2017-03-15 12:17:42 -0700

    Add test verification to ansible-test. (#22636)
    * Add unified git diff parser.
    * Add metadata and diff handling.
    * Add test confidence/verification to bot output.

--- test/utils/shippable/other.sh
+++ test/utils/shippable/other.sh
@@ -17,9 +17,13 @@ ln -sf x86_64-linux-gnu-gcc-4.9 /usr/bin/x86_64-linux-gnu-gcc
 
 retry.py pip install tox --disable-pip-version-check
 
+echo '{"verified": false, "results": []}' > test/results/bot/ansible-test-failure.json
+
 ansible-test compile --failure-ok --color -v --junit --requirements
 ansible-test sanity  --failure-ok --color -v --junit --tox --skip-test ansible-doc --python 2.7
 ansible-test sanity  --failure-ok --color -v --junit --tox --test ansible-doc --coverage
+
+rm test/results/bot/ansible-test-failure.json
 
 if find test/results/bot/ -mindepth 1 -name '.*' -prune -o -print -quit | grep -q .; then
     echo "One or more of the above ansible-test commands recorded at least one test failure."
