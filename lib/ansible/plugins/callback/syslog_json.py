fa9db33bdde16999d57c47faf23291bd78ed6f81
jralbert <jralbert@uvic.ca>
2017-03-16 10:38:55 -0700

    Cast syslogport value from getenv as int (#22664)
    SysLogHandler requires an int for port, but getenv produces a string.
    Cast return from getenv as int to overcome this mismatch.

--- lib/ansible/plugins/callback/syslog_json.py
+++ lib/ansible/plugins/callback/syslog_json.py
@@ -38,7 +38,7 @@ class CallbackModule(CallbackBase):
 
         self.handler = logging.handlers.SysLogHandler(
             address = (os.getenv('SYSLOG_SERVER','localhost'),
-                       os.getenv('SYSLOG_PORT',514)),
+                       int(os.getenv('SYSLOG_PORT',514))),
             facility= os.getenv('SYSLOG_FACILITY',logging.handlers.SysLogHandler.LOG_USER)
         )
         self.logger.addHandler(self.handler)
