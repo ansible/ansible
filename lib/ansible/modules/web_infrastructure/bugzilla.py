9b785dd3dc2157379a651028cbe63d57ade8cb96
bbhavsar <bbhavsar@redhat.com>
2017-03-17 19:43:09 +0530

    fixed changes suggested in code review
    Signed-off-by: bbhavsar <bbhavsar@redhat.com>

--- lib/ansible/modules/web_infrastructure/bugzilla.py
+++ lib/ansible/modules/web_infrastructure/bugzilla.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
-#
-# Created on Mar 10, 2017
-# @author: Bhavik Bhavsar (9.bhavik@gmail.com)
-#
-#
+# -*- coding: utf-8 -*-
+
+# (c) 2017, Bhavik Bhavsar <9.bhavik@gmail.com>
 #
 # This file is part of Ansible
 #
@@ -19,85 +17,92 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-#
 
-ANSIBLE_METADATA = {'status': ['preview'],
-                    'supported_by': 'community',
-                    'version': '1.0'}
 
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.pycompat24 import get_exception
+
+try:
+    import bugzilla
+
+    HAS_BUGZILLA = True
+except ImportError:
+    HAS_BUGZILLA = False
+
+
+ANSIBLE_METADATA = {
+    'status': ['preview'],
+    'supported_by': 'community',
+    'metadata_version': '1.0'
+}
 
 DOCUMENTATION = '''
 ---
 module: bugzilla
 short_description: Perform various actions with bugzilla
 description:
-    - Fetch status of particular bugzilla id from Bugzilla url provided.
-version_added: "2.3"
-author: Bhavik Bhavsar
+  - Fetch status of particular bugzilla id from Bugzilla URL provided.
+version_added: '2.3'
+author:
+  - Bhavik Bhavsar (@bhavik)
 requirements:
-    - "python >= 2.6"
-    - "python-bugzilla >= 2.0.0"
+  - python >= 2.6
+  - python-bugzilla >= 2.0.0
 options:
-    url:
-        description:
-            - url of bugzilla server
-        required: true
-    bug_id:
-        description:
-            - Bugzilla ID from Bugzilla
-        required: true
-    user:
-        description:
-            - username to login into Bugzilla
-        required: false
-    password:
-        description:
-            - password to login into Bugzilla
-        required: false
-    get_status:
-        description:
-            - Enable to fetch status of particular bugzilla id
-        required: false
+  url:
+    description:
+      - URL of bugzilla server
+    required: true
+  bug_id:
+    description:
+      - I(bug_id) from Bugzilla
+    required: true
+  user:
+    description:
+      - I(user) to login into Bugzilla
+    required: false
+  password:
+    description:
+      - I(password) along with I(user) to login to Bugzilla
+    required: false
+  get_status:
+    required: false
+    choices: [ yes, no ]
+    default: yes
+    description:
+      - Enable to fetch status of particular I(bug_id)
 '''
 
 
 EXAMPLES = '''
-
-#for private bug
-
+# For private bug
 - name: Get status for BZ 14295
   bugzilla:
-    url: "bugzilla.redhat.com"
+    url: bugzilla.redhat.com
     bug_id: 14295
-    user: <username>
-    password: <password>
-    get_status: True
+    user: userfoo
+    password: password123
+    get_status: yes
   register: status
 
-#for non-private bug
-
+# For non-private bug
 - name: Get status for BZ 1429535
   bugzilla:
-    url: "bugzilla.redhat.com"
+    url: bugzilla.redhat.com
     bug_id: 1429535
-    get_status: True
+    get_status: yes
   register: status
-
 '''
 
 
 RETURN = '''
 status:
   description: Fetches status of bugzilla
-  returned: Only when commands get_status is set to True and Bug_id is provided
+  returned: Only when commands I(get_status) is set to C(yes)
+            and I(bug_id) is provided
   type: string
-  sample: "NEW"
+  sample: NEW
 '''
-
-import bugzilla
-import sys
-
-from ansible.module_utils.basic import AnsibleModule
 
 
 def bugzilla_status(bug):
@@ -106,12 +111,16 @@ def bugzilla_status(bug):
 
 def main():
     module = AnsibleModule(
-        argument_spec = dict(
-            url = dict(required=True, type='str'),
-            bug_id = dict(required=True, type='int'),
-            user = dict(required=False, type='str'),
-            password = dict(required=False, type='str', no_log=True),
-            get_status = dict(required=False, type='bool')
+        argument_spec=dict(url=dict(
+            required=True, type='str'),
+            bug_id=dict(required=True, type='int'),
+            user=dict(required=False, type='str'),
+            password=dict(required=False,
+                          type='str',
+                          no_log=True),
+            get_status=dict(required=False,
+                            choices=['yes', 'no'],
+                            default='yes')
         ),
         required_together=[['user', 'password']]
     )
@@ -122,24 +131,24 @@ def main():
     password = module.params['password']
     get_status = module.params['get_status']
 
-    if not bug_id:
-        module.fail_json(msg="bug_id missing")
-
-    if not get_status:
-        module.fail_json(msg="get_status param not set")
+    if not HAS_BUGZILLA:
+        module.fail_json(
+            msg="Missing required 'python-bugzilla' module "
+                "(pip install python-bugzilla)")
 
     try:
         bzapi = bugzilla.Bugzilla(url, user, password)
         bug = bzapi.getbug(bug_id)
-
     except:
-        t, e = sys.exc_info()[:2]
-        response = {e.faultCode:e.faultString}
-        module.fail_json(msg="FaultError", FaultError=response)
-
+        e = get_exception()
+        module.fail_json(msg="Error", Error=str(e))
     else:
-        status = bugzilla_status(bug)
-        module.exit_json(changed=False, status=status)
+        if get_status == 'yes':
+            status = bugzilla_status(bug)
+            module.exit_json(changed=False, status=status)
+        else:
+            module.fail_json(msg='Please set get_status as "yes"')
+
 
 if __name__ == '__main__':
     main()
