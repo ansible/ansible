ebd8a5a67669e15bc37302c0cff5a9ed305427fd
Glen Maetzig <glen.maetzig@integrationqa.com>
2017-03-21 14:58:34 +1300

    Import camel_dict_to_snake_dict method to fix error when executing get_nat_gateways() method
    (cherry picked from commit 223fc34ab91b3980eebc641cb2649ad58e8bf385)

--- lib/ansible/modules/cloud/amazon/ec2_vpc_nat_gateway_facts.py
+++ lib/ansible/modules/cloud/amazon/ec2_vpc_nat_gateway_facts.py
@@ -91,7 +91,7 @@ result:
 import json
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.ec2 import ec2_argument_spec, get_aws_connection_info, boto3_conn
+from ansible.module_utils.ec2 import ec2_argument_spec, get_aws_connection_info, boto3_conn, camel_dict_to_snake_dict
 from ansible.module_utils.ec2 import ansible_dict_to_boto3_filter_list, HAS_BOTO3
 
 try:
