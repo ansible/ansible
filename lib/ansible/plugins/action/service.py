3327aa4985fb336f55374708b681eea694a462c1
Brian Coca <bcoca@ansible.com>
2017-03-21 15:01:24 -0400

    use namespaced facts

--- lib/ansible/plugins/action/service.py
+++ lib/ansible/plugins/action/service.py
@@ -45,9 +45,9 @@ class ActionModule(ActionBase):
         if module == 'auto':
             try:
                 if self._task.delegate_to: # if we delegate, we should use delegated host's facts
-                    module = self._templar.template("{{hostvars['%s']['ansible_service_mgr']}}" % self._task.delegate_to)
+                    module = self._templar.template("{{hostvars['%s']['ansible_facts']['ansible_service_mgr']}}" % self._task.delegate_to)
                 else:
-                    module = self._templar.template('{{ansible_service_mgr}}')
+                    module = self._templar.template('{{ansible_facts["ansible_service_mgr"]}}')
             except:
                 pass # could not get it from template!
 
