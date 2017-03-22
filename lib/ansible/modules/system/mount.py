fcb9460103c162a879848e4ea26910df4e3cf1de
AlexZolotarenko <alpiroz@gmail.com>
2017-03-22 12:05:15 +0100

    Fix incorrect command-line parameters for Solaris

--- lib/ansible/modules/system/mount.py
+++ lib/ansible/modules/system/mount.py
@@ -327,7 +327,7 @@ def unset_mount(module, args):
 def _set_fstab_args(fstab_file):
     result = []
 
-    if fstab_file and fstab_file != '/etc/fstab':
+    if fstab_file and fstab_file != '/etc/fstab' and get_platform().lower() != 'sunos':
         if get_platform().lower().endswith('bsd'):
             result.append('-F')
         else:
