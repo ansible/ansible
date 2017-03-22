a7e92a7f78f21ea7f100bf95f3b93e0dba8ec35e
mjfroehlich <martin.j.froehlich@gmail.com>
2017-03-22 17:49:39 +0100

    [cloud][route53_health_check] Consider port in identity test (#22762)
    Allows to health-check two services on one host. Before, asking for 2 health checks being present for `http://myhost:8888` and `http://myhost:9999`  resulted in just a single health check, as this module considered the two specs as equal.
    
    Bonus: route53_health_check now passes style guidelines

--- lib/ansible/modules/cloud/amazon/route53_health_check.py
+++ lib/ansible/modules/cloud/amazon/route53_health_check.py
@@ -156,7 +156,13 @@ def find_health_check(conn, wanted):
     """Searches for health checks that have the exact same set of immutable values"""
     for check in conn.get_list_health_checks().HealthChecks:
         config = check.HealthCheckConfig
-        if config.get('IPAddress') == wanted.ip_addr and config.get('FullyQualifiedDomainName') == wanted.fqdn and config.get('Type') == wanted.hc_type and config.get('RequestInterval') == str(wanted.request_interval):
+        if (
+            config.get('IPAddress') == wanted.ip_addr and
+            config.get('FullyQualifiedDomainName') == wanted.fqdn and
+            config.get('Type') == wanted.hc_type and
+            config.get('RequestInterval') == str(wanted.request_interval) and
+            config.get('Port') == str(wanted.port)
+        ):
             return check
     return None
 
