6429da460225dcc03022100b6a86d5d809131585
Brian Coca <brian.coca+git@gmail.com>
2017-03-24 12:38:16 -0400

    .--. . .--. ---.. / .. ... / .- -. -. --- -.-- .. -. --.

--- lib/ansible/cli/pull.py
+++ lib/ansible/cli/pull.py
@@ -62,12 +62,10 @@ class PullCLI(CLI):
         2: 'File is not readable'
     }
     SUPPORTED_REPO_MODULES = ['git']
-    ARGUMENTS = {
-                'playbook.yml':  'The name of one the YAML format files to run as an Ansible playbook.'
+    ARGUMENTS = { 'playbook.yml':  'The name of one the YAML format files to run as an Ansible playbook.'
                                  'This can be a relative path within the checkout. By default, Ansible will'
                                  "look for a playbook based on the host's fully-qualified domain name,"
-                                 'on the host hostname and finally a playbook named *local.yml*.',
-                }
+                                 'on the host hostname and finally a playbook named *local.yml*.', }
 
     def parse(self):
         ''' create an options parser for bin/ansible '''
