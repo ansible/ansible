9ea6c290dc5c533eaf1abfda68869d74b9637dc1
Senthil Kumar Ganesan <skg.net.dev@gmail.com>
2017-03-27 11:32:57 -0700

    Ansible 2.3 feature support for dellos9 and dellos10 (#22856)
    * Ansible 2.3 feature support for dellos9 and dellos10
    * Use Persistent Connection Manager
    
    * Fix CI issue, revert the doc and metadata changes
    
    * Reverted the meta_info (supported_by) to community from core
    
    * Fixed the CI issues, use module_utisl.six and updated legacy-files

--- lib/ansible/constants.py
+++ lib/ansible/constants.py
@@ -330,9 +330,8 @@ DEFAULT_STRATEGY_PLUGIN_PATH   = get_config(p, DEFAULTS, 'strategy_plugins', 'AN
                                             '~/.ansible/plugins/strategy:/usr/share/ansible/plugins/strategy', value_type='pathlist')
 
 NETWORK_GROUP_MODULES          = get_config(p, DEFAULTS, 'network_group_modules','NETWORK_GROUP_MODULES', ['eos', 'nxos', 'ios', 'iosxr', 'junos',
-                                                                                                           'vyos', 'sros'],
+                                                                                                           'vyos', 'sros', 'dellos9', 'dellos10'],
                                             value_type='list')
-
 DEFAULT_STRATEGY               = get_config(p, DEFAULTS, 'strategy',           'ANSIBLE_STRATEGY', 'linear')
 DEFAULT_STDOUT_CALLBACK        = get_config(p, DEFAULTS, 'stdout_callback',    'ANSIBLE_STDOUT_CALLBACK', 'default')
 # cache
