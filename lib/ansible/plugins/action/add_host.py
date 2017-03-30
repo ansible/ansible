636ab2f6611d497754c6dc97c49f495abc12b85c
Will Thames <will.thames@xvt.com.au>
2017-03-30 13:08:10 +1000

    Support check_mode for add_host
    `add_host` doesn't really actually change anything - there's no
    reason why it shouldn't work in `check_mode`.

--- lib/ansible/plugins/action/add_host.py
+++ lib/ansible/plugins/action/add_host.py
@@ -41,7 +41,7 @@ class ActionModule(ActionBase):
 
     def run(self, tmp=None, task_vars=None):
 
-        self._supports_check_mode = False
+        self._supports_check_mode = True
 
         result = super(ActionModule, self).run(tmp, task_vars)
 
