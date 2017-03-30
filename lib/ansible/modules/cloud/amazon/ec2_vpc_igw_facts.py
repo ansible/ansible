f9bd652d18ad3d6f3c1f3ad094ff51ce9666ca16
Will Thames <will.thames@xvt.com.au>
2017-03-30 16:01:13 +1000

    ec2_vpc_igw_facts pep8 tidy up

--- lib/ansible/modules/cloud/amazon/ec2_vpc_igw_facts.py
+++ lib/ansible/modules/cloud/amazon/ec2_vpc_igw_facts.py
@@ -113,7 +113,7 @@ except ImportError:
 def get_internet_gateway_info(internet_gateway):
     internet_gateway_info = {'InternetGatewayId': internet_gateway['InternetGatewayId'],
                              'Attachments': internet_gateway['Attachments'],
-                             'Tags': internet_gateway['Tags'] }
+                             'Tags': internet_gateway['Tags']}
     return internet_gateway_info
 
 
@@ -140,8 +140,8 @@ def main():
     argument_spec = ec2_argument_spec()
     argument_spec.update(
         dict(
-            filters = dict(type='dict', default=dict()),
-            internet_gateway_ids = dict(type='list', default=None)
+            filters=dict(type='dict', default=dict()),
+            internet_gateway_ids=dict(type='list', default=None)
         )
     )
 
