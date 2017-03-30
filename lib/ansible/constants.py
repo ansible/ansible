1ba7e6b6f6f3a7da8e44bffda381b130e3b9cf5e
John R Barker <john@johnrbarker.com>
2017-03-30 17:42:11 +0100

    Ansible 2.3 feature support for dellos6. (#23084) (#23147)
    * Ansible 2.3 feature support for dellos6.
    
    - With the new Ansible 2.3 infra changes, the dellos modules doesn't work
      (the new infra changes are not backward compatible), so added the below
      changes support it.
    - Added the new terminal plugin for DellOS6
    - Added the new action plugin for DellOS6
    - Modified the modules to work with the new infra.
    - with that it adds support for DellOS6 Persistent Connection support.
    
    * Remove pep8 confirming files from dellos6.py and dellos6_config legacy-files
    
    (cherry picked from commit a0344acd78c422457b5a018b9de59e213f39dc87)

--- lib/ansible/constants.py
+++ lib/ansible/constants.py
@@ -300,7 +300,7 @@ DEFAULT_TEST_PLUGIN_PATH       = get_config(p, DEFAULTS, 'test_plugins',       '
 DEFAULT_STRATEGY_PLUGIN_PATH   = get_config(p, DEFAULTS, 'strategy_plugins',   'ANSIBLE_STRATEGY_PLUGINS', '~/.ansible/plugins/strategy:/usr/share/ansible/plugins/strategy', value_type='pathlist')
 
 NETWORK_GROUP_MODULES          = get_config(p, DEFAULTS, 'network_group_modules','NETWORK_GROUP_MODULES', ['eos', 'nxos', 'ios', 'iosxr', 'junos',
-                                                                                                           'vyos', 'sros', 'dellos9', 'dellos10'],
+                                                                                                           'vyos', 'sros', 'dellos9', 'dellos10', 'dellos6'],
                                             value_type='list')
 
 DEFAULT_STRATEGY               = get_config(p, DEFAULTS, 'strategy',           'ANSIBLE_STRATEGY', 'linear')
