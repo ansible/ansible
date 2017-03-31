0082b0e754365e9600df6bd205e1f67612eef5ff
Pradeep <pradeepreddy.manu.iitkgp@gmail.com>
2017-03-31 12:13:36 +0530

    Fixed bugs in deleting autoscaling groups

--- lib/ansible/modules/cloud/amazon/ec2_asg.py
+++ lib/ansible/modules/cloud/amazon/ec2_asg.py
@@ -760,6 +760,7 @@ def delete_autoscaling_group(connection, module):
     group_name = module.params.get('name')
     notification_topic = module.params.get('notification_topic')
     wait_for_instances = module.params.get('wait_for_instances')
+    wait_timeout = module.params.get('wait_timeout')
 
     if notification_topic:
         connection.delete_notification_configuration(
@@ -770,13 +771,16 @@ def delete_autoscaling_group(connection, module):
     groups = describe_response.get('AutoScalingGroups')
     if groups:
         if not wait_for_instances:
-            connection.update_auto_scaling_group(
+            connection.delete_auto_scaling_group(AutoScalingGroupName=group_name, ForceDelete=True)
+            return True
+
+        wait_timeout = time.time() + wait_timeout            
+        connection.update_auto_scaling_group(
                 AutoScalingGroupName=group_name,
                 MinSize=0, MaxSize=0,
                 DesiredCapacity=0)
-
         instances = True
-        while instances and wait_for_instances:
+        while instances and wait_for_instances and wait_timeout >= time.time():
             tmp_groups = connection.describe_auto_scaling_groups(AutoScalingGroupNames=[group_name]).get(
                 'AutoScalingGroups')
             if tmp_groups:
@@ -784,6 +788,10 @@ def delete_autoscaling_group(connection, module):
                 if not tmp_group.get('Instances'):
                     instances = False
             time.sleep(10)
+
+        if wait_timeout <= time.time():
+            # waiting took too long
+            module.fail_json(msg = "Waited too long for old instances to terminate. %s" % time.asctime())        
 
         connection.delete_auto_scaling_group(AutoScalingGroupName=group_name)
         while len(connection.describe_auto_scaling_groups(AutoScalingGroupNames=[group_name]).get('AutoScalingGroups')):
