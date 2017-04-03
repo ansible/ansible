ae24b3b08fc2bc4956b8b1ba1d42d0419774eeb1
Nathaniel Case <ncase@redhat.com>
2017-04-03 12:22:31 -0400

    Remove reliance on ability to output configuration in `set` format

--- lib/ansible/modules/network/junos/junos_config.py
+++ lib/ansible/modules/network/junos/junos_config.py
@@ -219,11 +219,9 @@ def filter_delete_statements(module, candidate):
     reply = get_configuration(module, format='set')
     match = reply.find('.//configuration-set')
     if match is None:
-        module.fail_json(msg='unable to retrieve device configuration')
+        # Could not find configuration-set in reply, perhaps device does not support it?
+        return candidate
     config = str(match.text)
-
-    #if 'delete interfaces lo0' in candidate:
-    #    raise ValueError(config)
 
     modified_candidate = candidate[:]
     for index, line in enumerate(candidate):
@@ -307,10 +305,14 @@ def main():
     result = {'changed': False, 'warnings': warnings}
 
     if module.params['backup']:
-        reply = get_configuration(module, format='set')
-        match = reply.find('.//configuration-set')
-        if match is None:
+        for conf_format in ['set', 'text']:
+            reply = get_configuration(module, format=conf_format)
+            match = reply.find('.//configuration-%s' % conf_format)
+            if match is not None:
+                break
+        else:
             module.fail_json(msg='unable to retrieve device configuration')
+
         result['__backup__'] = str(match.text).strip()
 
     if module.params['rollback']:
