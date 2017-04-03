7580cd02db603ef048e38cdfc386d9c5665f5be7
Jordan Borean <jborean93@gmail.com>
2017-04-04 04:38:34 +1000

    win_stat change return islink to islnk to match stat (#23196)

--- test/integration/targets/win_stat/files/setup_share.ps1
+++
@@ -1,6 +0,0 @@
-$share_stat = Get-WmiObject -Class Win32_Share -Filter "name='folder-share'"
-If ($share_stat) {
-    $share_stat.Delete()
-}
-$wmi = [wmiClass] 'Win32_Share'
-$wmi.Create($args[0], 'folder-share', 0)
