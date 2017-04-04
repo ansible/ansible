bf426ee1414cda1e72865dcb4a2e193b4ed2e428
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2017-04-04 16:04:00 +0200

    Pull filesystems from dict in case is not string on eos_facts (#23254)
    Apparently in some devices the filesystems gathering command can return
    a dict containing a 'messages' key with the filesystems, instead of a
    plain string.
    
    Fixes #23217

--- lib/ansible/modules/network/eos/eos_facts.py
+++ lib/ansible/modules/network/eos/eos_facts.py
@@ -201,6 +201,10 @@ class Hardware(FactsBase):
 
     def populate_filesystems(self):
         data = self.responses[0]
+
+        if isinstance(data, dict):
+            data = data['messages'][0]
+
         fs = re.findall(r'^Directory of (.+)/', data, re.M)
         return dict(filesystems=fs)
 
