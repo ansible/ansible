d6e5b3fd2e613989b97efbcc56e71e69a6a6b29e
Will Thames <will.thames@xvt.com.au>
2017-04-06 10:33:27 +1000

    rds_snapshot_facts - fix pep8 issues

--- lib/ansible/modules/cloud/amazon/rds_snapshot_facts.py
+++ lib/ansible/modules/cloud/amazon/rds_snapshot_facts.py
@@ -85,7 +85,7 @@ snapshots:
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.ec2 import ec2_argument_spec, get_aws_connection_info, boto3_conn, HAS_BOTO3
-from ansible.module_utils.ec2 import ansible_dict_to_boto3_filter_list, camel_dict_to_snake_dict
+from ansible.module_utils.ec2 import camel_dict_to_snake_dict
 from ansible.module_utils.rds import RDSSnapshot
 
 import traceback
@@ -135,9 +135,9 @@ def main():
     argument_spec = ec2_argument_spec()
     argument_spec.update(
         dict(
-            name = dict(aliases=['snapshot_name']),
-            instance_name = dict(),
-            snapshot_type = dict(choices=['automated', 'manual', 'shared', 'public'])
+            name=dict(aliases=['snapshot_name']),
+            instance_name=dict(),
+            snapshot_type=dict(choices=['automated', 'manual', 'shared', 'public'])
         )
     )
 
