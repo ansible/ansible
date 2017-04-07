d392c493b9d0e30491df23ecb63c04871fb7e1e0
Brian Coca <brian.coca+git@gmail.com>
2017-04-07 13:37:01 -0400

    removed unused and erroring cmp import

--- lib/ansible/executor/play_iterator.py
+++ lib/ansible/executor/play_iterator.py
@@ -24,7 +24,6 @@ import fnmatch
 from ansible.compat.six import iteritems
 from ansible import constants as C
 from ansible.errors import AnsibleError
-from ansible.module_utils.six import cmp
 from ansible.playbook.block import Block
 from ansible.playbook.task import Task
 from ansible.playbook.role_include import IncludeRole
