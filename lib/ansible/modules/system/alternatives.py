9dbf8c1ab28200a262e25bb5eef8782cd019de3d
Leynos <leynos@troubledskies.net>
2017-04-09 18:15:12 +0100

    Refactor alternatives module into functions

--- lib/ansible/modules/system/alternatives.py
+++ lib/ansible/modules/system/alternatives.py
@@ -84,6 +84,72 @@ import re
 from ansible.module_utils.basic import *
 from ansible.module_utils.pycompat24 import get_exception
 
+def get_current(module, cmd, name, link):
+
+    current_path = None
+    all_alternatives = []
+
+    # Run `update-alternatives --display <name>` to find existing alternatives
+    (rc, display_output, _) = module.run_command(
+        ['env', 'LC_ALL=C', cmd, '--display', name]
+    )
+
+    if rc == 0:
+        # Alternatives already exist for this link group
+        # Parse the output to determine the current path of the symlink and
+        # available alternatives
+        current_path_regex = re.compile(r'^\s*link currently points to (.*)$',
+                                        re.MULTILINE)
+        alternative_regex = re.compile(r'^(\/.*)\s-\spriority', re.MULTILINE)
+
+        current_path = current_path_regex.search(display_output).group(1)
+        all_alternatives = alternative_regex.findall(display_output)
+
+        if not link:
+            # Read the current symlink target from `update-alternatives --query`
+            # in case we need to install the new alternative before setting it.
+            #
+            # This is only compatible on Debian-based systems, as the other
+            # alternatives don't have --query available
+            rc, query_output, _ = module.run_command(
+                ['env', 'LC_ALL=C', cmd, '--query', name]
+            )
+            if rc == 0:
+                for line in query_output.splitlines():
+                    if line.startswith('Link:'):
+                        link = line.split()[1]
+                        break
+
+    return current_path, all_alternatives, link
+
+def set_alternative(module, cmd, name, path, link, priority,
+        current_path, all_alternatives):
+    if current_path != path:
+        if module.check_mode:
+            module.exit_json(changed=True, current_path=current_path)
+        try:
+            # install the requested path if necessary
+            if path not in all_alternatives:
+                if not link:
+                    module.fail_json(msg="Needed to install the alternative, but unable to do so as we are missing the link")
+
+                module.run_command(
+                    [cmd, '--install', link, name, path, str(priority)],
+                    check_rc=True
+                )
+
+            # select the requested path
+            module.run_command(
+                [cmd, '--set', name, path],
+                check_rc=True
+            )
+
+            module.exit_json(changed=True)
+        except subprocess.CalledProcessError:
+            e = get_exception()
+            module.fail_json(msg=str(dir(cpe)))
+    else:
+        module.exit_json(changed=False)
 
 def main():
 
@@ -105,67 +171,11 @@ def main():
     priority = params['priority']
 
     UPDATE_ALTERNATIVES = module.get_bin_path('update-alternatives',True)
+    (current_path, all_alternatives, link) = get_current(
+        module, UPDATE_ALTERNATIVES, name, link)
 
-    current_path = None
-    all_alternatives = []
-
-    # Run `update-alternatives --display <name>` to find existing alternatives
-    (rc, display_output, _) = module.run_command(
-        ['env', 'LC_ALL=C', UPDATE_ALTERNATIVES, '--display', name]
-    )
-
-    if rc == 0:
-        # Alternatives already exist for this link group
-        # Parse the output to determine the current path of the symlink and
-        # available alternatives
-        current_path_regex = re.compile(r'^\s*link currently points to (.*)$',
-                                        re.MULTILINE)
-        alternative_regex = re.compile(r'^(\/.*)\s-\spriority', re.MULTILINE)
-
-        current_path = current_path_regex.search(display_output).group(1)
-        all_alternatives = alternative_regex.findall(display_output)
-
-        if not link:
-            # Read the current symlink target from `update-alternatives --query`
-            # in case we need to install the new alternative before setting it.
-            #
-            # This is only compatible on Debian-based systems, as the other
-            # alternatives don't have --query available
-            rc, query_output, _ = module.run_command(
-                ['env', 'LC_ALL=C', UPDATE_ALTERNATIVES, '--query', name]
-            )
-            if rc == 0:
-                for line in query_output.splitlines():
-                    if line.startswith('Link:'):
-                        link = line.split()[1]
-                        break
-
-    if current_path != path:
-        if module.check_mode:
-            module.exit_json(changed=True, current_path=current_path)
-        try:
-            # install the requested path if necessary
-            if path not in all_alternatives:
-                if not link:
-                    module.fail_json(msg="Needed to install the alternative, but unable to do so as we are missing the link")
-
-                module.run_command(
-                    [UPDATE_ALTERNATIVES, '--install', link, name, path, str(priority)],
-                    check_rc=True
-                )
-
-            # select the requested path
-            module.run_command(
-                [UPDATE_ALTERNATIVES, '--set', name, path],
-                check_rc=True
-            )
-
-            module.exit_json(changed=True)
-        except subprocess.CalledProcessError:
-            e = get_exception()
-            module.fail_json(msg=str(dir(cpe)))
-    else:
-        module.exit_json(changed=False)
+    set_alternative(module, UPDATE_ALTERNATIVES, name, path, link, priority,
+                    current_path, all_alternatives)
 
 if __name__ == '__main__':
     main()
