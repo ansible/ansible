eb6b5c891c5614f281600d5ead739f751cf13037
Toshio Kuratomi <a.badger@gmail.com>
2017-04-19 09:49:44 -0700

    Really minor optimization and style change for 5e2a206b51a90cb6902227c05130644ab2b3e5d6
    * Use a generator expression instead of a list comprehension
    * Use copy() to create a new set instead of the constructor.

--- lib/ansible/modules/system/user.py
+++ lib/ansible/modules/system/user.py
@@ -552,8 +552,8 @@ class User(object):
         if self.groups is None:
             return None
         info = self.user_info()
-        groups = set([x.strip() for x in self.groups.split(',') if x])
-        for g in set(groups):
+        groups = set(x.strip() for x in self.groups.split(',') if x)
+        for g in groups.copy():
             if not self.group_exists(g):
                 self.module.fail_json(msg="Group %s does not exist" % (g))
             if info and remove_existing and self.group_info(g)[2] == info[3]:
