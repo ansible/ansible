a93c9bf6e5357c0aa7f5efa5d961429326a553c5
s-hertel <shertel@redhat.com>
2017-04-19 14:07:29 -0400

    remove unnecessary logic

--- lib/ansible/modules/cloud/amazon/rds.py
+++ lib/ansible/modules/cloud/amazon/rds.py
@@ -1123,9 +1123,6 @@ def main():
         'restore': restore_db_instance,
     }
 
-    if module.params['command'] != 'modify' and module.params['multi_zone'] is None:
-        module.params['multi_zone'] = False
-
     region, ec2_url, aws_connect_params = get_aws_connection_info(module)
     if not region:
         module.fail_json(msg="Region not specified. Unable to determine region from EC2_REGION.")
