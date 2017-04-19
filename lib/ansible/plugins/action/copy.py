d9747dbf4cfa81e6f69337786bd7d3edf6165279
Brian Coca <brian.coca+git@gmail.com>
2017-04-19 11:35:25 -0400

    finer grained failed/skipped on async for actions
    fixes #23729

--- lib/ansible/plugins/action/copy.py
+++ lib/ansible/plugins/action/copy.py
@@ -40,7 +40,7 @@ class ActionModule(ActionBase):
 
         result = super(ActionModule, self).run(tmp, task_vars)
 
-        if result.get('skipped'):
+        if result.get('skipped', False) or result.get('failed', False):
             return result
 
         source  = self._task.args.get('src', None)
