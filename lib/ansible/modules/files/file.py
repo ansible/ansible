abb11f739bccbba7b07f4d5170e0f97e50ee756c
Andrew Erickson <andy@bolt.me>
2017-04-20 12:48:56 -0700

    fix

--- lib/ansible/modules/files/file.py
+++ lib/ansible/modules/files/file.py
@@ -396,7 +396,7 @@ def main():
                 diff['before']['src'] = to_native(b_old_src, errors='strict')
                 diff['after']['src'] = src
                 changed = True
-        elif prev_state in ('file', 'directory'):
+        elif prev_state in ('file', 'hard', 'directory'):
             # continue if the file is a hard link and already correct
             if os.stat(b_path).st_ino == os.stat(b_src).st_ino:
                 pass
