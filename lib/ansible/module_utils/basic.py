7aab695887c705b97c31031bc023a0a964e060d5
Yannig Perré <yannig.perre@cgi.com>
2017-04-21 16:19:46 +0200

    Fix file crash when using recurse=yes (#23861). Do not convert path in set_*_if_different.

--- lib/ansible/module_utils/basic.py
+++ lib/ansible/module_utils/basic.py
@@ -995,7 +995,6 @@ class AnsibleModule(object):
         b_path = to_bytes(path, errors='surrogate_then_strict')
         if expand:
             b_path = os.path.expanduser(os.path.expandvars(b_path))
-        path = to_text(b_path, errors='surrogate_then_strict')
         if owner is None:
             return changed
         orig_uid, orig_gid = self.user_and_group(path, expand)
@@ -1029,7 +1028,6 @@ class AnsibleModule(object):
         b_path = to_bytes(path, errors='surrogate_then_strict')
         if expand:
             b_path = os.path.expanduser(os.path.expandvars(b_path))
-        path = to_text(b_path, errors='surrogate_then_strict')
         if group is None:
             return changed
         orig_uid, orig_gid = self.user_and_group(b_path, expand)
@@ -1063,7 +1061,6 @@ class AnsibleModule(object):
         b_path = to_bytes(path, errors='surrogate_then_strict')
         if expand:
             b_path = os.path.expanduser(os.path.expandvars(b_path))
-        path = to_text(b_path, errors='surrogate_then_strict')
         path_stat = os.lstat(b_path)
 
         if mode is None:
@@ -1143,7 +1140,6 @@ class AnsibleModule(object):
         b_path = to_bytes(path, errors='surrogate_then_strict')
         if expand:
             b_path = os.path.expanduser(os.path.expandvars(b_path))
-        path = to_text(b_path, errors='surrogate_then_strict')
 
         existing = self.get_file_attributes(b_path)
 
