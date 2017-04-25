f97e4c5a142ffee270b4b20e3358aee7d4a9927c
s-hertel <shertel@redhat.com>
2017-04-25 09:15:00 -0400

    dhcp_option will always have a name tag

--- lib/ansible/modules/cloud/amazon/ec2_vpc_dhcp_options_facts.py
+++ lib/ansible/modules/cloud/amazon/ec2_vpc_dhcp_options_facts.py
@@ -99,7 +99,7 @@ except ImportError:
 
 def get_dhcp_options_info(dhcp_option):
     if 'Tags' not in dhcp_option:
-        dhcp_option['Tags'] = []
+        dhcp_option['Tags'] = [{'Value': '', 'Key': 'Name'}]
     dhcp_option_info = {'DhcpOptionsId': dhcp_option['DhcpOptionsId'],
                         'DhcpConfigurations': dhcp_option['DhcpConfigurations'],
                         'Tags': boto3_tag_list_to_ansible_dict(dhcp_option['Tags'])}
