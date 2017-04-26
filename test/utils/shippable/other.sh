19c4e1a992e6f9d781291bce4e174d6f407e98b4
Matt Clay <matt@mystile.com>
2017-04-26 15:04:28 +0800

    Use Shippable image: drydock/u16pytall:master
    * Use Shippable image: drydock/u16pytall:master
    * Do not install python 3.6 on Shippable.

--- test/utils/shippable/other.sh
+++ test/utils/shippable/other.sh
@@ -2,18 +2,12 @@
 
 set -o pipefail
 
-retry.py add-apt-repository 'deb http://archive.ubuntu.com/ubuntu trusty-backports universe'
-retry.py add-apt-repository 'ppa:ubuntu-toolchain-r/test'
 retry.py add-apt-repository 'ppa:fkrull/deadsnakes'
 
 retry.py apt-get update -qq
 retry.py apt-get install -qq \
     shellcheck \
     python2.4 \
-    g++-4.9 \
-    python3.6-dev \
-
-ln -sf x86_64-linux-gnu-gcc-4.9 /usr/bin/x86_64-linux-gnu-gcc
 
 retry.py pip install tox --disable-pip-version-check
 
