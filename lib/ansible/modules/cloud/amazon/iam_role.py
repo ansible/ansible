5da934f4114bf7f82a1c2e85ffaf526206ce7f23
Bill Wanjohi <kaguru@gmail.com>
2017-04-27 15:53:21 +0000

    module_iam_role: add_remove_policies_option

--- lib/ansible/modules/cloud/amazon/iam_role.py
+++ lib/ansible/modules/cloud/amazon/iam_role.py
@@ -36,6 +36,12 @@ options:
       - A list of managed policy ARNs or, since Ansible 2.4, a list of either managed policy ARNs or friendly names.
         To embed an inline policy, use M(iam_policy). To remove existing policies, use an empty list item.
     aliases: [ managed_policies ]
+  remove_unlisted_policies:
+    description:
+      - Detaches any managed policies not listed in the "managed_policy" option. Set to false if you want to attach policies elsewhere.
+    type: bool
+    default: true
+    version_added: "2.4"
   state:
     description:
       - Create or remove the IAM role
@@ -238,6 +244,15 @@ def create_or_update_role(connection, module):
             for policy in current_attached_policies:
                 try:
                     connection.detach_role_policy(RoleName=params['RoleName'], PolicyArn=policy['PolicyArn'])
+                except (ClientError, ParamValidationError) as e:
+                    module.fail_json(msg=e.message, **camel_dict_to_snake_dict(e.response))
+
+            # Detach policies not present
+            current_attached_policies_arn_list = [policy['PolicyArn'] for policy in current_attached_policies]
+
+            for policy_arn in list(set(current_attached_policies_arn_list) - set(managed_policies)):
+                try:
+                    connection.detach_role_policy(RoleName=params['RoleName'], PolicyArn=policy['PolicyArn'])
                 except ClientError as e:
                     module.fail_json(msg=e.message, exception=traceback.format_exc(), **camel_dict_to_snake_dict(e.response))
                 changed = True
@@ -344,7 +359,7 @@ def get_attached_policy_list(connection, module, name):
         return connection.list_attached_role_policies(RoleName=name)['AttachedPolicies']
     except ClientError as e:
         if e.response['Error']['Code'] == 'NoSuchEntity':
-            return None
+            return []
         else:
             module.fail_json(msg=e.message, exception=traceback.format_exc(), **camel_dict_to_snake_dict(e.response))
 
@@ -361,6 +376,7 @@ def main():
             state=dict(type='str', choices=['present', 'absent'], default='present'),
             description=dict(type='str'),
             create_instance_profile=dict(type='bool', default=True)
+            remove_unlisted_policies=dict(type='bool', default=True),
         )
     )
 
