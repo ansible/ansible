a7a03ff57034cdbd7051793fc3d1a6aeba7514cb
Nathaniel Case <ncase@redhat.com>
2017-04-27 11:08:50 -0400

    update docs

--- lib/ansible/modules/network/nxos/nxos_bgp_af.py
+++ lib/ansible/modules/network/nxos/nxos_bgp_af.py
@@ -16,9 +16,11 @@
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-ANSIBLE_METADATA = {'metadata_version': '1.0',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
+ANSIBLE_METADATA = {
+    'metadata_version': '1.0',
+    'status': ['preview'],
+    'supported_by': 'community',
+}
 
 
 DOCUMENTATION = '''
@@ -28,221 +30,221 @@ extends_documentation_fragment: nxos
 version_added: "2.2"
 short_description: Manages BGP Address-family configuration.
 description:
-    - Manages BGP Address-family configurations on NX-OS switches.
+  - Manages BGP Address-family configurations on NX-OS switches.
 author: Gabriele Gerbino (@GGabriele)
 notes:
-    - C(state=absent) removes the whole BGP ASN configuration
-    - Default, where supported, restores params default value.
+  - C(state=absent) removes the whole BGP ASN configuration
+  - Default, where supported, restores params default value.
 options:
-    asn:
-        description:
-            - BGP autonomous system number. Valid values are String,
-              Integer in ASPLAIN or ASDOT notation.
-        required: true
-    vrf:
-        description:
-            - Name of the VRF. The name 'default' is a valid VRF representing
-              the global bgp.
-        required: true
-    afi:
-        description:
-            - Address Family Identifier.
-        required: true
-        choices: ['ipv4','ipv6', 'vpnv4', 'vpnv6', 'l2vpn']
-    safi:
-        description:
-            - Sub Address Family Identifier.
-        required: true
-        choices: ['unicast','multicast', 'evpn']
-    additional_paths_install:
-        description:
-            - Install a backup path into the forwarding table and provide
-              prefix independent convergence (PIC) in case of a PE-CE link
-              failure.
-        required: false
-        choices: ['true','false']
-        default: null
-    additional_paths_receive:
-        description:
-            - Enables the receive capability of additional paths for all of
-              the neighbors under this address family for which the capability
-              has not been disabled.
-        required: false
-        choices: ['true','false']
-        default: null
-    additional_paths_selection:
-        description:
-            - Configures the capability of selecting additional paths for
-              a prefix. Valid values are a string defining the name of
-              the route-map.
-        required: false
-        default: null
-    additional_paths_send:
-        description:
-            - Enables the send capability of additional paths for all of
-              the neighbors under this address family for which the capability
-              has not been disabled.
-        required: false
-        choices: ['true','false']
-        default: null
-    advertise_l2vpn_evpn:
-        description:
-            - Advertise evpn routes.
-        required: false
-        choices: ['true','false']
-        default: null
-    client_to_client:
-        description:
-            - Configure client-to-client route reflection.
-        required: false
-        choices: ['true','false']
-        default: null
-    dampen_igp_metric:
-        description:
-            - Specify dampen value for IGP metric-related changes, in seconds.
-              Valid values are integer and keyword 'default'.
-        required: false
-        default: null
-    dampening_state:
-        description:
-            - Enable/disable route-flap dampening.
-        required: false
-        choices: ['true','false']
-        default: null
-    dampening_half_time:
-        description:
-            - Specify decay half-life in minutes for route-flap dampening.
-              Valid values are integer and keyword 'default'.
-        required: false
-        default: null
-    dampening_max_suppress_time:
-        description:
-            - Specify max suppress time for route-flap dampening stable route.
-              Valid values are integer and keyword 'default'.
-        required: false
-        default: null
-    dampening_reuse_time:
-        description:
-            - Specify route reuse time for route-flap dampening.
-              Valid values are integer and keyword 'default'.
-        required: false
-    dampening_routemap:
-        description:
-            - Specify route-map for route-flap dampening. Valid values are a
-              string defining the name of the route-map.
-        required: false
-        default: null
-    dampening_suppress_time:
-        description:
-            - Specify route suppress time for route-flap dampening.
-              Valid values are integer and keyword 'default'.
-        required: false
-        default: null
-    default_information_originate:
-        description:
-            - Default information originate.
-        required: false
-        choices: ['true','false']
-        default: null
-    default_metric:
-        description:
-            - Sets default metrics for routes redistributed into BGP.
-              Valid values are Integer or keyword 'default'
-        required: false
-        default: null
-    distance_ebgp:
-        description:
-            - Sets the administrative distance for eBGP routes.
-              Valid values are Integer or keyword 'default'.
-        required: false
-        default: null
-    distance_ibgp:
-        description:
-            - Sets the administrative distance for iBGP routes.
-              Valid values are Integer or keyword 'default'.
-        required: false
-        default: null
-    distance_local:
-        description:
-            - Sets the administrative distance for local BGP routes.
-              Valid values are Integer or keyword 'default'.
-        required: false
-        default: null
-    inject_map:
-        description:
-            - An array of route-map names which will specify prefixes to
-              inject. Each array entry must first specify the inject-map name,
-              secondly an exist-map name, and optionally the copy-attributes
-              keyword which indicates that attributes should be copied from
-              the aggregate. For example [['lax_inject_map', 'lax_exist_map'],
-              ['nyc_inject_map', 'nyc_exist_map', 'copy-attributes'],
-              ['fsd_inject_map', 'fsd_exist_map']].
-        required: false
-        default: null
-    maximum_paths:
-        description:
-            - Configures the maximum number of equal-cost paths for
-              load sharing. Valid value is an integer in the range 1-64.
-        default: null
-    maximum_paths_ibgp:
-        description:
-            - Configures the maximum number of ibgp equal-cost paths for
-              load sharing. Valid value is an integer in the range 1-64.
-        required: false
-        default: null
-    networks:
-        description:
-            - Networks to configure. Valid value is a list of network
-              prefixes to advertise. The list must be in the form of an array.
-              Each entry in the array must include a prefix address and an
-              optional route-map. For example [['10.0.0.0/16', 'routemap_LA'],
-              ['192.168.1.1', 'Chicago'], ['192.168.2.0/24],
-              ['192.168.3.0/24', 'routemap_NYC']].
-        required: false
-        default: null
-    next_hop_route_map:
-        description:
-            - Configure a route-map for valid nexthops. Valid values are a
-              string defining the name of the route-map.
-        required: false
-        default: null
-    redistribute:
-        description:
-            - A list of redistribute directives. Multiple redistribute entries
-              are allowed. The list must be in the form of a nested array.
-              the first entry of each array defines the source-protocol to
-              redistribute from; the second entry defines a route-map name.
-              A route-map is highly advised but may be optional on some
-              platforms, in which case it may be omitted from the array list.
-              For example [['direct', 'rm_direct'], ['lisp', 'rm_lisp']].
-        required: false
-        default: null
-    suppress_inactive:
-        description:
-            - Advertises only active routes to peers.
-        required: false
-        choices: ['true','false']
-        default: null
-    table_map:
-        description:
-            - Apply table-map to filter routes downloaded into URIB.
-              Valid values are a string.
-        required: false
-        default: null
-    table_map_filter:
-        description:
-            - Filters routes rejected by the route-map and does not download
-              them to the RIB.
-        required: false
-        choices: ['true','false']
-        default: null
-    state:
-        description:
-            - Determines whether the config should be present or not
-              on the device.
-        required: false
-        default: present
-        choices: ['present','absent']
+  asn:
+    description:
+      - BGP autonomous system number. Valid values are String,
+        Integer in ASPLAIN or ASDOT notation.
+    required: true
+  vrf:
+    description:
+      - Name of the VRF. The name 'default' is a valid VRF representing
+        the global bgp.
+    required: true
+  afi:
+    description:
+      - Address Family Identifier.
+    required: true
+    choices: ['ipv4','ipv6', 'vpnv4', 'vpnv6', 'l2vpn']
+  safi:
+    description:
+      - Sub Address Family Identifier.
+    required: true
+    choices: ['unicast','multicast', 'evpn']
+  additional_paths_install:
+    description:
+      - Install a backup path into the forwarding table and provide
+        prefix independent convergence (PIC) in case of a PE-CE link
+        failure.
+    required: false
+    choices: ['true','false']
+    default: null
+  additional_paths_receive:
+    description:
+      - Enables the receive capability of additional paths for all of
+        the neighbors under this address family for which the capability
+        has not been disabled.
+    required: false
+    choices: ['true','false']
+    default: null
+  additional_paths_selection:
+    description:
+      - Configures the capability of selecting additional paths for
+        a prefix. Valid values are a string defining the name of
+        the route-map.
+    required: false
+    default: null
+  additional_paths_send:
+    description:
+      - Enables the send capability of additional paths for all of
+        the neighbors under this address family for which the capability
+        has not been disabled.
+    required: false
+    choices: ['true','false']
+    default: null
+  advertise_l2vpn_evpn:
+    description:
+      - Advertise evpn routes.
+    required: false
+    choices: ['true','false']
+    default: null
+  client_to_client:
+    description:
+      - Configure client-to-client route reflection.
+    required: false
+    choices: ['true','false']
+    default: null
+  dampen_igp_metric:
+    description:
+      - Specify dampen value for IGP metric-related changes, in seconds.
+        Valid values are integer and keyword 'default'.
+    required: false
+    default: null
+  dampening_state:
+    description:
+      - Enable/disable route-flap dampening.
+    required: false
+    choices: ['true','false']
+    default: null
+  dampening_half_time:
+    description:
+      - Specify decay half-life in minutes for route-flap dampening.
+        Valid values are integer and keyword 'default'.
+    required: false
+    default: null
+  dampening_max_suppress_time:
+    description:
+      - Specify max suppress time for route-flap dampening stable route.
+        Valid values are integer and keyword 'default'.
+    required: false
+    default: null
+  dampening_reuse_time:
+    description:
+      - Specify route reuse time for route-flap dampening.
+        Valid values are integer and keyword 'default'.
+    required: false
+  dampening_routemap:
+    description:
+      - Specify route-map for route-flap dampening. Valid values are a
+        string defining the name of the route-map.
+    required: false
+    default: null
+  dampening_suppress_time:
+    description:
+      - Specify route suppress time for route-flap dampening.
+        Valid values are integer and keyword 'default'.
+    required: false
+    default: null
+  default_information_originate:
+    description:
+      - Default information originate.
+    required: false
+    choices: ['true','false']
+    default: null
+  default_metric:
+    description:
+      - Sets default metrics for routes redistributed into BGP.
+        Valid values are Integer or keyword 'default'
+    required: false
+    default: null
+  distance_ebgp:
+    description:
+      - Sets the administrative distance for eBGP routes.
+        Valid values are Integer or keyword 'default'.
+    required: false
+    default: null
+  distance_ibgp:
+    description:
+      - Sets the administrative distance for iBGP routes.
+        Valid values are Integer or keyword 'default'.
+    required: false
+    default: null
+  distance_local:
+    description:
+      - Sets the administrative distance for local BGP routes.
+        Valid values are Integer or keyword 'default'.
+    required: false
+    default: null
+  inject_map:
+    description:
+      - An array of route-map names which will specify prefixes to
+        inject. Each array entry must first specify the inject-map name,
+        secondly an exist-map name, and optionally the copy-attributes
+        keyword which indicates that attributes should be copied from
+        the aggregate. For example [['lax_inject_map', 'lax_exist_map'],
+        ['nyc_inject_map', 'nyc_exist_map', 'copy-attributes'],
+        ['fsd_inject_map', 'fsd_exist_map']].
+    required: false
+    default: null
+  maximum_paths:
+    description:
+      - Configures the maximum number of equal-cost paths for
+        load sharing. Valid value is an integer in the range 1-64.
+    default: null
+  maximum_paths_ibgp:
+    description:
+      - Configures the maximum number of ibgp equal-cost paths for
+        load sharing. Valid value is an integer in the range 1-64.
+    required: false
+    default: null
+  networks:
+    description:
+      - Networks to configure. Valid value is a list of network
+        prefixes to advertise. The list must be in the form of an array.
+        Each entry in the array must include a prefix address and an
+        optional route-map. For example [['10.0.0.0/16', 'routemap_LA'],
+        ['192.168.1.1', 'Chicago'], ['192.168.2.0/24],
+        ['192.168.3.0/24', 'routemap_NYC']].
+    required: false
+    default: null
+  next_hop_route_map:
+    description:
+      - Configure a route-map for valid nexthops. Valid values are a
+        string defining the name of the route-map.
+    required: false
+    default: null
+  redistribute:
+    description:
+      - A list of redistribute directives. Multiple redistribute entries
+        are allowed. The list must be in the form of a nested array.
+        the first entry of each array defines the source-protocol to
+        redistribute from; the second entry defines a route-map name.
+        A route-map is highly advised but may be optional on some
+        platforms, in which case it may be omitted from the array list.
+        For example [['direct', 'rm_direct'], ['lisp', 'rm_lisp']].
+    required: false
+    default: null
+  suppress_inactive:
+    description:
+      - Advertises only active routes to peers.
+    required: false
+    choices: ['true','false']
+    default: null
+  table_map:
+    description:
+      - Apply table-map to filter routes downloaded into URIB.
+        Valid values are a string.
+    required: false
+    default: null
+  table_map_filter:
+    description:
+      - Filters routes rejected by the route-map and does not download
+        them to the RIB.
+    required: false
+    choices: ['true','false']
+    default: null
+  state:
+    description:
+      - Determines whether the config should be present or not
+        on the device.
+    required: false
+    default: present
+    choices: ['present','absent']
 '''
 EXAMPLES = '''
 # configure a simple address-family
@@ -256,49 +258,12 @@ EXAMPLES = '''
 '''
 
 RETURN = '''
-proposed:
-    description: k/v pairs of parameters passed into module
-    returned: verbose mode
-    type: dict
-    sample: {"advertise_l2vpn_evpn": true, "afi": "ipv4",
-             "asn": "65535", "safi": "unicast", "vrf": "TESTING"}
-existing:
-    description: k/v pairs of existing BGP AF configuration
-    returned: verbose mode
-    type: dict
-    sample: {}
-end_state:
-    description: k/v pairs of BGP AF configuration after module execution
-    returned: verbose mode
-    type: dict
-    sample: {"additional_paths_install": false,
-            "additional_paths_receive": false,
-            "additional_paths_selection": "",
-            "additional_paths_send": false,
-            "advertise_l2vpn_evpn": true, "afi": "ipv4",
-            "asn": "65535", "client_to_client": true,
-            "dampen_igp_metric": "600", "dampening_half_time": "",
-            "dampening_max_suppress_time": "", "dampening_reuse_time": "",
-            "dampening_routemap": "", "dampening_state": false,
-            "dampening_suppress_time": "",
-            "default_information_originate": false, "default_metric": "",
-            "distance_ebgp": "20", "distance_ibgp": "200",
-            "distance_local": "220", "inject_map": [], "maximum_paths": "1",
-            "maximum_paths_ibgp": "1", "networks": [],
-            "next_hop_route_map": "", "redistribute": [], "safi": "unicast",
-            "suppress_inactive": false, "table_map": "",
-            "table_map_filter": false, "vrf": "TESTING"}
-updates:
+commands:
     description: commands sent to the device
     returned: always
     type: list
     sample: ["router bgp 65535", "vrf TESTING",
             "address-family ipv4 unicast", "advertise l2vpn evpn"]
-changed:
-    description: check to see if a change was made on the device
-    returned: always
-    type: boolean
-    sample: true
 '''
 
 import re
