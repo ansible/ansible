ecb7fd4cc8bbd44574e519324d68dc8408f08b99
John R Barker <john@johnrbarker.com>
2017-04-28 12:15:18 +0100

    Correct RETURN data types for Network modules (#24104)
    `path` isn't a datatype

--- lib/ansible/modules/network/junos/junos_config.py
+++ lib/ansible/modules/network/junos/junos_config.py
@@ -180,7 +180,7 @@ RETURN = """
 backup_path:
   description: The full path to the backup file
   returned: when backup is yes
-  type: path
+  type: string
   sample: /playbooks/ansible/backup/config.2016-07-16@22:28:34
 """
 import re
