bc7632cf26d2b6b70a58a5ca4448a475049251b4
Raul Cuza <raul.cuza@intersection.com>
2017-04-29 15:20:51 -0400

    add ansible-vault stdin integration tests

--- test/integration/targets/vault/runme.sh
+++ test/integration/targets/vault/runme.sh
@@ -42,8 +42,16 @@ ansible-vault encrypt_string "$@" --vault-password-file "${NEW_VAULT_PASSWORD}" 
 
 # from stdin
 ansible-vault encrypt_string "$@" --vault-password-file "${NEW_VAULT_PASSWORD}" < "${TEST_FILE}"
-
 ansible-vault encrypt_string "$@" --vault-password-file "${NEW_VAULT_PASSWORD}" --stdin-name "the_var_from_stdin" < "${TEST_FILE}"
+
+ansible-vault encrypt --vault-password-file "${NEW_VAULT_PASSWORD}" < "${TEST_FILE}"
+ansible-vault encrypt --vault-password-file "${NEW_VAULT_PASSWORD}" --stdin-name "the_var_from_stdin" < "${TEST_FILE}"
+
+ansible-vault decrypt --vault-password-file "${NEW_VAULT_PASSWORD}" < "${TEST_FILE}"
+ansible-vault decrypt --vault-password-file "${NEW_VAULT_PASSWORD}" --stdin-name "the_var_from_stdin" < "${TEST_FILE}"
+
+ansible-vault view --vault-password-file "${NEW_VAULT_PASSWORD}" < "${TEST_FILE}"
+ansible-vault view --vault-password-file "${NEW_VAULT_PASSWORD}" --stdin-name "the_var_from_stdin" < "${TEST_FILE}"
 
 # write to file
 ansible-vault encrypt_string "$@" --vault-password-file "${NEW_VAULT_PASSWORD}" --name "blippy" "a test string names blippy" --output "${MYTMPDIR}/enc_string_test_file"
