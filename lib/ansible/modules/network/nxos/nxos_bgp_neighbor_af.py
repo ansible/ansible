be1fd5632df5cbffd9fc1f1e02d8faf09b787162
Nathaniel Case <ncase@redhat.com>
2017-05-01 13:20:50 -0400

    Fix pep8 in bgp_neighbor_af

--- lib/ansible/modules/network/nxos/nxos_bgp_neighbor_af.py
+++ lib/ansible/modules/network/nxos/nxos_bgp_neighbor_af.py
@@ -819,10 +819,8 @@ def main():
         neighbor=dict(required=True, type='str'),
         afi=dict(required=True, type='str'),
         safi=dict(required=True, type='str'),
-        additional_paths_receive=dict(required=False, type='str',
-                                choices=['enable', 'disable', 'inherit']),
-        additional_paths_send=dict(required=False, type='str',
-                                choices=['enable', 'disable', 'inherit']),
+        additional_paths_receive=dict(required=False, type='str', choices=['enable', 'disable', 'inherit']),
+        additional_paths_send=dict(required=False, type='str', choices=['enable', 'disable', 'inherit']),
         advertise_map_exist=dict(required=False, type='list'),
         advertise_map_non_exist=dict(required=False, type='list'),
         allowas_in=dict(required=False, type='bool'),
@@ -843,19 +841,13 @@ def main():
         route_map_in=dict(required=False, type='str'),
         route_map_out=dict(required=False, type='str'),
         route_reflector_client=dict(required=False, type='bool'),
-        send_community=dict(required=False, choices=['none',
-                                                         'both',
-                                                         'extended',
-                                                         'standard',
-                                                         'default']),
-        soft_reconfiguration_in=dict(required=False, type='str',
-                                choices=['enable', 'always', 'inherit']),
+        send_community=dict(required=False, choices=['none', 'both', 'extended', 'standard', 'default']),
+        soft_reconfiguration_in=dict(required=False, type='str', choices=['enable', 'always', 'inherit']),
         soo=dict(required=False, type='str'),
         suppress_inactive=dict(required=False, type='bool'),
         unsuppress_map=dict(required=False, type='str'),
         weight=dict(required=False, type='str'),
-        state=dict(choices=['present', 'absent'], default='present',
-                       required=False),
+        state=dict(choices=['present', 'absent'], default='present', required=False),
         include_defaults=dict(default=True),
         config=dict(),
         save=dict(type='bool', default=False)
@@ -863,27 +855,25 @@ def main():
 
     argument_spec.update(nxos_argument_spec)
 
-    module = AnsibleModule(argument_spec=argument_spec,
-                                mutually_exclusive=[['advertise_map_exist',
-                                             'advertise_map_non_exist']],
-                                supports_check_mode=True)
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        mutually_exclusive=[['advertise_map_exist', 'advertise_map_non_exist']],
+        supports_check_mode=True,
+    )
 
     warnings = list()
     check_args(module, warnings)
 
-
     state = module.params['state']
-    if ((module.params['max_prefix_interval'] or
-            module.params['max_prefix_warning'] or
-            module.params['max_prefix_threshold']) and
-            not module.params['max_prefix_limit']):
+    if ((module.params['max_prefix_interval'] or module.params['max_prefix_warning'] or
+         module.params['max_prefix_threshold']) and not module.params['max_prefix_limit']):
         module.fail_json(msg='max_prefix_limit is required when using '
                              'max_prefix_warning, max_prefix_limit or '
                              'max_prefix_threshold.')
     if module.params['vrf'] == 'default' and module.params['soo']:
         module.fail_json(msg='SOO is only allowed in non-default VRF')
 
-    args =  [
+    args = [
         'afi',
         'asn',
         'neighbor',
@@ -933,8 +923,7 @@ def main():
         module.params['advertise_map_non_exist'] = 'default'
 
     end_state = existing
-    proposed_args = dict((k, v) for k, v in module.params.items()
-                    if v is not None and k in args)
+    proposed_args = dict((k, v) for k, v in module.params.items() if v is not None and k in args)
 
     proposed = {}
     for key, value in proposed_args.items():
@@ -978,4 +967,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
