5d9c19fd703d07a6f0f01d92e5c0888a14621f0c
David Beck <techiscool@gmail.com>
2017-05-03 11:14:49 -0700

    [Fix] Commented out Section due to no Quotes
    Variables are not being passed into ansible unless they are quoted since yaml will automatically interpret them as comments

--- lib/ansible/modules/notification/slack.py
+++ lib/ansible/modules/notification/slack.py
@@ -139,7 +139,7 @@ EXAMPLES = """
   slack:
     token: thetoken/generatedby/slack
     msg: '{{ inventory_hostname }} completed'
-    channel: #ansible
+    channel: '#ansible'
     username: 'Ansible on {{ inventory_hostname }}'
     icon_url: http://www.example.com/some-image-file.png
     link_names: 0
@@ -159,7 +159,7 @@ EXAMPLES = """
     token: thetoken/generatedby/slack
     attachments:
       - text: Display my system load on host A and B
-        color: #ff00dd
+        color: '#ff00dd'
         title: System load
         fields:
           - title: System A
