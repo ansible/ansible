276109cd3275b4ce031c75b2f4c2099edfeecdf0
Toshio Kuratomi <a.badger@gmail.com>
2017-05-04 07:57:17 -0700

    ansible-doc snippet format changes
    * ansible-doc -s is supposed to output a sample snippet of how you could
      add the module into a playbook.  These changes update the style:
      * Use yaml mappings instead of key=value
      * Use the module name directly instead of action: modulename
    * Fixes a bug when displaying option descritpions which are yaml strings
      instead of lists.
    
    This fixes in code the bad formatting reported in #24201

--- lib/ansible/cli/doc.py
+++ lib/ansible/cli/doc.py
@@ -262,23 +262,25 @@ class DocCLI(CLI):
         text = []
         desc = CLI.tty_ify(doc['short_description'])
         text.append("- name: %s" % (desc))
-        text.append("  action: %s" % (doc['module']))
+        text.append("  %s:" % (doc['module']))
         pad = 31
         subdent = " " * pad
         limit = display.columns - pad
 
         for o in sorted(doc['options'].keys()):
             opt = doc['options'][o]
-            desc = CLI.tty_ify(" ".join(opt['description']))
+            if isinstance(opt['description'], string_types):
+                desc = CLI.tty_ify(opt['description'])
+            else:
+                desc = CLI.tty_ify(" ".join(opt['description']))
 
             required = opt.get('required', False)
             if not isinstance(required, bool):
                 raise("Incorrect value for 'Required', a boolean is needed.: %s" % required)
             if required:
-                s = o + "="
-            else:
-                s = o
-            text.append("      %-20s   # %s" % (s, textwrap.fill(desc, limit, subsequent_indent=subdent)))
+                desc = "(required) %s" % desc
+            o = '%s:' % o
+            text.append("      %-20s   # %s" % (o, textwrap.fill(desc, limit, subsequent_indent=subdent)))
         text.append('')
 
         return "\n".join(text)
