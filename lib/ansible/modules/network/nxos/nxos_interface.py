642f6c0361b481d2d56950442fc9856bf7a22053
Trishna Guha <trishnaguha17@gmail.com>
2017-05-05 08:35:16 +0530

    fixes nxos_interface (#24199)
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/modules/network/nxos/nxos_interface.py
+++ lib/ansible/modules/network/nxos/nxos_interface.py
@@ -221,21 +221,22 @@ def get_manual_interface_attributes(interface, module):
     if get_interface_type(interface) == 'svi':
         command = 'show interface {0}'.format(interface)
         try:
-            body = execute_show_command(command, module)[0]
+            body = run_commands(module, [command])[0]
         except IndexError:
             return None
 
-        command_list = body.split('\n')
-        desc = None
-        admin_state = 'up'
-        for each in command_list:
-            if 'Description:' in each:
-                line = each.split('Description:')
-                desc = line[1].strip().split('MTU')[0].strip()
-            elif 'Administratively down' in each:
-                admin_state = 'down'
+        if body:
+            command_list = body.split('\n')
+            desc = None
+            admin_state = 'up'
+            for each in command_list:
+                if 'Description:' in each:
+                    line = each.split('Description:')
+                    desc = line[1].strip().split('MTU')[0].strip()
+                elif 'Administratively down' in each:
+                    admin_state = 'down'
 
-        return dict(description=desc, admin_state=admin_state)
+            return dict(description=desc, admin_state=admin_state)
     else:
         return None
 
