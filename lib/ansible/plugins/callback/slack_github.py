f669b2b699d0ae5612d07e4f176f836b4211c316
Nikita Chernyi <rakshazi@users.noreply.github.com>
2017-05-05 13:55:15 +0300

    SlackGithub callback plugin: bugfix, fixed codestyle

--- lib/ansible/plugins/callback/slack_github.py
+++ lib/ansible/plugins/callback/slack_github.py
@@ -2,10 +2,12 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 import json
+import sys
 import os
 import subprocess
 from ansible.module_utils.urls import open_url
 from ansible.plugins.callback import CallbackBase
+
 
 class CallbackModule(CallbackBase):
     """This is an ansible callback plugin that sends full
@@ -17,7 +19,6 @@ class CallbackModule(CallbackBase):
     ```json
     {
         "webhook": "Slack incoming webhook url",
-        "channel": "#slackchannel",
         "username": "Ansible"
     }
     ```
@@ -33,102 +34,101 @@ class CallbackModule(CallbackBase):
     def __init__(self, display=None):
         super(CallbackModule, self).__init__(display=display)
         self.github_api = 'https://api.github.com/'
-        self.log = u'';
+        self._log = []
 
-    def _init_config(self, playbook):
+    def init_config(self, playbook):
         self.playbook_name = os.path.basename(playbook._file_name)
-        config_file = playbook._basedir.encode('ascii','ignore') + '/slack.json'
+        config_file = os.path.join(playbook._basedir.encode('ascii', 'ignore'), 'slack.json')
         if os.path.isfile(config_file) == False:
-            self._display.warning('File ' + config_file + ' not found. SlackGithub callback has been disabled')
+            self._display.warning('File {} not found. SlackGithub callback has been disabled'.format(config_file))
         with open(config_file) as config_file:
             self.config = json.load(config_file)
         try:
             cmd = ['git', 'config', '--get', 'user.name']
             username = subprocess.check_output(cmd, universal_newlines=True).split('\0')
             self.user = username[0].rstrip()
-        except:
+        except BaseException:
             self.user = 'Anonymous'
 
+    def log(self, text):
+        self._log.append(text)
 
-    def _log(self, text):
-        self.log += text
-
-    def _send(self, color='good'):
+    def send(self, color='good'):
         raw_gist = {
-            'description': 'Ansible play recap',
-            'public': False,
-            'files': {
-                'recap.md': {
-                    'content': self.log
+                'description': 'Ansible play recap',
+                'public': False,
+                'files': {
+                    'recap.md': {
+                        'content': '\n'.join(self._log)
+                        }
+                    }
                 }
-            }
-        }
 
         data = json.dumps(raw_gist)
         try:
-            response = open_url(self.github_api + 'gists', data=data, method='POST')
+            response = open_url(
+                    self.github_api + 'gists',
+                    data=data,
+                    method='POST')
         except Exception as e:
-            self._display.warning('Could not submit play recap: %s' % str(e))
+            self._display.warning('Could not submit play recap: {}'.format(e))
 
         gist = json.loads(response.read())
-        url = gist[u'html_url'].encode('ascii','ignore')
-        hosts = '*'
-        for host in self.play.hosts:
-            hosts+= str(host) + ', '
-        hosts = hosts[:-2]
-        hosts+='*'
-        message = 'Playbook *' + self.playbook_name + '* has been played against ' + hosts + ' by *' + self.user + '*. '
-        message+= '*<' + url + '#file-recap-md|Details>*'
+        url = gist[u'html_url'].encode('ascii', 'ignore')
+        hosts = '*{}*'.format(', '.join(self.play.hosts))
+        message = 'Playbook *{}* has been played against {} by *{}* *<{}#file-recap-md|Details>*'.format(self.playbook_name, hosts, self.user, url)
         payload = {
-            'channel': self.config['channel'],
-            'username': self.config['username'],
-            'attachments': [{
-                'fallback': message,
-                'fields': [{
-                    'value': message
-                }],
-                'color': color,
-                'mrkdwn_in': ['text', 'fallback', 'fields'],
-            }],
-            'parse': 'none',
-            'icon_url': ('http://cdn2.hubspot.net/hub/330046/file-449187601-png/ansible_badge.png'),
-        }
+                'username': self.config['username'],
+                'attachments': [{
+                    'fallback': message,
+                    'fields': [{
+                        'value': message
+                        }],
+                    'color': color,
+                    'mrkdwn_in': ['text', 'fallback', 'fields'],
+                    }],
+                'parse': 'none',
+                }
 
         data = json.dumps(payload)
         try:
             response = open_url(self.config['webhook'], data=data)
         except Exception as e:
-            self._display.warning('Could not submit notification to Slack: %s' % str(e))
+            self._display.warning('Could not submit notification to Slack: {}'.format(e))
 
     def v2_playbook_on_start(self, playbook):
-        self._init_config(playbook)
-        self._log('**Playbook**: ' + self.playbook_name + '\n\n')
+        self.init_config(playbook)
+        self.log('**Playbook**: {}\n\n'.format(os.path.basename(playbook._file_name)))
 
     def v2_playbook_on_play_start(self, play):
         self.play = play
-        name = play.name or 'Not specified #' + play._uuid
-        self._log('# Play: ' + name + '\n\n')
+        name = play.name or 'Not specified #{}'.format(play._uuid)
+        self.log('# Play: {}\n\n'.format(name))
 
     def v2_playbook_on_stats(self, stats):
         color = 'good'
-        self._log('# Stats\n\n')
+        self.log('# Stats\n\n')
         hosts = sorted(stats.processed.keys())
         for host in hosts:
             summary = stats.summarize(host)
-            self._log('**Host**: ' + host + '\n')
-            for item, count in summary.iteritems():
-                self._log('> **' + item + '**: ' + str(count) + '\n\n')
+            self.log('**Host**: {}\n'.format(host))
+            if(sys.version_info >= (3,0)):
+                items = summary.items()
+            else:
+                items = summary.iteritems()
+            for item, count in items:
+                self.log('> **{}**: {}\n\n'.format(item, count))
                 if item == 'failures' and count > 0:
                     color = 'danger'
-        self._send(color)
+        self.send(color)
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
-        self._log('**_FAILED_ ' + result._host.get_name() + '** | ' + result._task.get_name().strip() + '\n\n')
-        self._log('```json\n' + json.dumps(result._result, indent=1, sort_keys=True) + '\n```\n\n')
+        self.log('**_FAILED_ {}** | {}\n\n'.format(result._host.get_name(), result._task.get_name().strip()))
+        self.log('```json\n{}\n```\n\n'.format(json.dumps(result._result, indent=1,sort_keys=True)))
 
     def v2_runner_on_ok(self, result):
-        self._log('**' + result._host.get_name() + '** | ' + result._task.get_name().strip() + '\n\n')
+        self.log('**{}** | {}\n\n'.format(result._host.get_name(), result._task.get_name().strip()))
 
     def v2_runner_on_unreachable(self, result):
-        self._log('**_UNREACHABLE_ ' + result._host.get_name() + '** | ' + result._task.get_name().strip() + '\n\n')
-        self._log('```json\n' + json.dumps(result._result, indent=1, sort_keys=True) + '\n```\n\n')
+        self.log('**_UNREACHABLE_ {}** | {}\n\n'.format(result._host.get_name(), result._task.get_name().strip))
+        self.log('```json\n{}\n```\n\n'.format(json.dumps(result._result, indent=1,sort_keys=True)))
