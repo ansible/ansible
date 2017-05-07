454837f6d339b834b51abdf81764df68bdab8a96
Matt Clay <matt@mystile.com>
2017-05-07 16:32:19 +0800

    Add support for on-demand coverage on Shippable.
    Triggered by setting the COVERAGE environment var to a non-empty value.
    Alternatively the string `ci_coverage` can be added to a commit message.

--- test/utils/shippable/osx.sh
+++ test/utils/shippable/osx.sh
@@ -9,4 +9,5 @@ platform="${args[0]}"
 version="${args[1]}"
 target="posix/ci/"
 
-ansible-test integration --color -v --retry-on-error "${target}" --remote "${platform}/${version}" --remote-terminate success --exclude "posix/ci/cloud/"
+ansible-test integration --color -v --retry-on-error "${target}" --remote "${platform}/${version}" --remote-terminate success --exclude "posix/ci/cloud/" \
+    "${COVERAGE}"
