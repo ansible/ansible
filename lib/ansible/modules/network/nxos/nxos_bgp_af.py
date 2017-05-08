75b50e771010c35381138db103fa0ba164b4bec4
Nathaniel Case <ncase@redhat.com>
2017-05-08 10:17:20 -0400

    nxos_bgp_af changes

--- lib/ansible/modules/network/nxos/nxos_bgp_af.py
+++ lib/ansible/modules/network/nxos/nxos_bgp_af.py
@@ -468,27 +468,28 @@ def get_value(arg, config, module):
         'redistribute'
     ]
 
-    if arg in BOOL_PARAMS:
-        REGEX = re.compile(r'\s+{0}\s*$'.format(PARAM_TO_COMMAND_KEYMAP[arg]), re.M)
-        value = False
-        try:
-            if REGEX.search(config):
-                value = True
-        except TypeError:
-            value = False
-
-    elif arg in custom:
+    if arg in custom:
         value = get_custom_list_value(config, arg, module)
 
     elif (arg.startswith('distance') or arg.startswith('dampening') or
           arg.startswith('table_map')):
         value = get_custom_string_value(config, arg, module)
 
+    elif arg in BOOL_PARAMS:
+        command_re = re.compile(r'\s+{0}\s*'.format(command), re.M)
+        value = False
+
+        if command_re.search(config):
+            value = True
+
     else:
-        REGEX = re.compile(r'(?:{0}\s)(?P<value>.*)$'.format(PARAM_TO_COMMAND_KEYMAP[arg]), re.M)
+        command_val_re = re.compile(r'(?:{0}\s)(?P<value>.*)$'.format(PARAM_TO_COMMAND_KEYMAP[arg]), re.M)
         value = ''
-        if PARAM_TO_COMMAND_KEYMAP[arg] in config:
-            value = REGEX.search(config).group('value')
+
+        has_command = command_val_re.search(config)
+        if has_command:
+            value = has_command.group('value')
+
     return value
 
 
