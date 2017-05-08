aa89fc83306ecde39a66e67db9202b4f4ecb6c36
morph027 <morphsen@gmx.com>
2017-05-08 07:21:03 +0200

    gpl + review

--- contrib/inventory/docker_swarm_nodes.py
+++ contrib/inventory/docker_swarm_nodes.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 
+# (c) 2017, Stefan Heitmueller <stefan.heitmueller@gmx.com>
+#
+# This file is part of Ansible,
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+######################################################################
 ## Installation
 ## ------------
 ##
@@ -24,6 +42,7 @@
 import ConfigParser
 import os
 import json
+import docker
 
 class SwarmNodesInventory(object):
 
@@ -34,11 +53,10 @@ class SwarmNodesInventory(object):
         self.read_settings()
 
         # initiate swarm api connection
-        import docker
         self.client = docker.DockerClient(base_url=self.docker_host)
 
         # build json output for ansible containing hostgroup "swarm"
-        self.data = { "swarm": [], "manager": [], "worker": [] }
+        self.data = {"swarm": [], "manager": [], "worker": []}
         # get manager nodes
         for self.node in self.client.nodes.list(filters={'role': 'manager'}):
             self.node_hostname = self.client.nodes.get(self.node.id).attrs['Description']['Hostname']
@@ -56,7 +74,7 @@ class SwarmNodesInventory(object):
             self.data["worker"].append(self.node_hostname)
 
         # finally print json
-        print json.dumps(self.data)
+        print(json.dumps(self.data))
 
     def read_settings(self):
         """ Reads the settings from the docker_swarm_nodes.ini file """
@@ -66,4 +84,5 @@ class SwarmNodesInventory(object):
 
         self.docker_host = config.get('swarm', 'docker_host')
 
-SwarmNodesInventory()
+if __name__ == "__main__":
+    SwarmNodesInventory()
