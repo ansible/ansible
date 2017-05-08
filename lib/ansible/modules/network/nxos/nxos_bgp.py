3263759703184ef5a68f9fae28a01a7d20e81d8a
Nathaniel Case <ncase@redhat.com>
2017-05-08 11:00:46 -0400

    nxapi has problems reading CustomNetworkConfig directly

--- lib/ansible/modules/network/nxos/nxos_bgp.py
+++ lib/ansible/modules/network/nxos/nxos_bgp.py
@@ -683,9 +683,10 @@ def main():
         state_absent(module, existing, candidate)
 
     if candidate:
+        candidate = candidate.items_text()
         load_config(module, candidate)
         result['changed'] = True
-        result['commands'] = candidate.items_text()
+        result['commands'] = candidate
     else:
         result['commands'] = []
 
