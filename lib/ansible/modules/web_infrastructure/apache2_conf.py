0208db83172459c86268ab08e3449ee7e5ce9c2c
div <div@justcommunication.ru>
2017-05-09 10:49:40 +1000

    Rename apache2_fragment to apache2_conf

--- lib/ansible/modules/web_infrastructure/apache2_conf.py
+++ lib/ansible/modules/web_infrastructure/apache2_conf.py
@@ -29,7 +29,6 @@ author:
 short_description: enables/disables a configuration fragment of the Apache2 webserver
 description:
    - Enables or disables a specified site or configuration snippet of the Apache2 webserver.
-     This module support Debian/Ubuntu based systems, and requires a2enconf, a2disconf, a2ensite, a2dissite scripts.
 options:
    type:
      description:
@@ -49,7 +48,7 @@ requirements: ["a2enconf","a2disconf","a2ensite","a2dissite"]
 '''
 
 EXAMPLES = '''
-# enable config fragment charset.conf
+# enable config fragmen charset.conf
 - apache2_conf:
     type: conf
     state: present
@@ -97,32 +96,18 @@ class ApacheFragmentException(Exception):
     pass
 
 
-class ApacheFragmentFail(ApacheFragmentException):
-    def __init__(self, rc=None, stdout=None, stderr=None):
-        self.rc = rc
-        self.stdout = stdout
-        self.stderr = stderr
-
-
-class ApacheFragmentUnsupported(ApacheFragmentException):
-    pass
-
-
 class ApacheFragment(object):
     available_dir = None
     enabled_dir = None
     fragment_suffix = '.conf'
-    command_names = dict(present=None, absent=None)
+    command_names = {'present': None, 'absent': None}
 
     def __init__(self, name):
         self.fragment_file = basename(name)
-        for required_dir in (self.available_dir, self.enabled_dir):
-            if not exists(required_dir):
-                raise ApacheFragmentUnsupported("directory %s not found" % required_dir)
 
     def state(self):
         """
-        :return: "present" if fragment enabled, "absent" otherwise. Raise exception if fragment not found.
+        :return: True if fragment enabled, False otherwise. Raise exception if fragment not found.
         """
         available_fragment = join(self.available_dir, self.fragment_file)
         if not exists(available_fragment):
@@ -134,29 +119,30 @@ class ApacheFragment(object):
         return "absent"
 
     def set(self, state=None):
-        command_name = module.get_bin_path(self.command_names[state])
-        if command_name is None:
-            raise ApacheFragmentUnsupported("command %s not found" % self.command_names[state])
+        command_name = module.get_bin_path(self.command_names[state], required=True)
         # Short name of fragment: without path and suffix
         fragment_name = splitext(self.fragment_file)[0]
-        return module.run_command([command_name, fragment_name], check_rc=True)
+        return module.run_command("%s %s" % (command_name, fragment_name), check_rc=True)
 
 
 class ApacheConfigFragment(ApacheFragment):
     available_dir = '/etc/apache2/conf-available'
     enabled_dir = '/etc/apache2/conf-enabled'
-    command_names = dict(present='a2enconf', absent='a2disconf')
+    command_names = {'present': 'a2enconf', 'absent': 'a2disconf'}
 
 
 class ApacheSiteFragment(ApacheFragment):
     available_dir = '/etc/apache2/sites-available'
     enabled_dir = '/etc/apache2/sites-enabled'
-    command_names = dict(present='a2ensite', absent='a2dissite')
+    command_names = {'present': 'a2ensite', 'absent': 'a2dissite'}
 
 
 def _set_state(state):
     type = module.params['type']
     name = module.params['name']
+
+    state_string = {'present': 'enabled', 'absent': 'disabled'}[state]
+    success_msg = "Fragment %s %s" % (name, state_string)
 
     if type == 'conf':
         fragment = ApacheConfigFragment(name)
@@ -165,16 +151,16 @@ def _set_state(state):
 
     if fragment.state() != state:
         if module.check_mode:
-            return True
+            return True, success_msg
 
-        rc, stdout, stderr = fragment.set(state)
+        result, stdout, stderr = fragment.set(state)
 
         if fragment.state() == state:
-            return True
+            return True, success_msg
         else:
-            raise ApacheFragmentFail(rc, stdout, stderr)
+            raise ApacheFragmentException("Failed to set fragment %s to %s: %s" % (name, state_string, stdout))
     else:
-        return False
+        return False, success_msg
 
 
 def main():
@@ -190,23 +176,11 @@ def main():
 
     module.warnings = []
 
-    state = module.params['state']
-    name = module.params['name']
-
-    state_string = dict(present='enabled', absent='disabled')[state]
-    success_msg = "Fragment %s %s" % (name, state_string)
-    fail_msg = "Failed to set fragment %s to %s" % (name, state_string)
-
     try:
-        changed = _set_state(state)
-    except ApacheFragmentFail as e:
-        module.fail_json(msg=fail_msg, rc=e.rc, stdout=e.stdout, stderr=e.stderr)
-    except ApacheFragmentUnsupported as e:
-        module.fail_json(msg="Unsupported distribution: %s" % e.message)
+        (changed, msg) = _set_state(module.params['state'])
+        module.exit_json(changed=changed, result=msg, warnings=module.warnings)
     except ApacheFragmentException as e:
         module.fail_json(msg=e.message)
-    else:
-        module.exit_json(changed=changed, result=success_msg, warnings=module.warnings)
 
 
 if __name__ == '__main__':
