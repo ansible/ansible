fb7892e1a3a2f6ad9e4e164e5f2ab6005caaef17
Abhijeet Kasurde <akasurde@redhat.com>
2017-05-09 18:22:13 +0530

    Check login_token for user before any operations (#23600)
    Fix adds extra check if user is authorized or not while
    using login_token.
    
    Fixes https://github.com/ansible/ansible/issues/23033
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/source_control/gitlab_project.py
+++ lib/ansible/modules/source_control/gitlab_project.py
@@ -375,6 +375,14 @@ def main():
         e = get_exception()
         module.fail_json(msg="Failed to connect to Gitlab server: %s " % e)
 
+    # Check if user is authorized or not before proceeding to any operations
+    # if not, exit from here
+    auth_msg = git.currentuser().get('message', None)
+    if auth_msg is not None and auth_msg == '401 Unauthorized':
+        module.fail_json(msg='User unauthorized',
+                         details="User is not allowed to access Gitlab server "
+                                 "using login_token. Please check login_token")
+
     # Validate if project exists and take action based on "state"
     project = GitLabProject(module, git)
     project_exists = project.existsProject(group_name, project_name)
