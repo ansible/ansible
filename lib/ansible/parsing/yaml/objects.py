63a3da64002c562d90e34a56f755b6432eb0d650
Adrian Likins <alikins@redhat.com>
2017-05-10 14:05:52 -0400

    Fix vault vars with jinja with more unicode wrappr
    When jinja process a playbook containing something like
    
       "{{ some_var_that_was_embedded_vault | quote }}"
    
    the ciphertext was being used instead of the plaintext. The reason
    is that the 'quote' filter (on py2) calls pipes.quote(). pipes.quote()
    iterates over the string instead of using its str()/unicode().
    
    AnsibleVaultEncryptedUnicode was using the default __iter__ which
    itererates over the ciphertext.
    
    Fix is to add a __iter__ that iterates over the plaintext.
    Also adds a .replace() method since quote and a few other
    filters use it.
    
    This adds the rest of the unicode methods that reference the
    value as well. It is kind of bruteforce and inelegant, but
    I suppose it is explicit.
    
    Why do that instead of just returning a unicode object with
    the decrypted plaintext? This allows vault strings to be
    read before we know the password. It also prevents having
    as many copies of the plaintext in memory. But main reason
    is that some of the upcoming vault extensions will require it.
    
    Add integration tests.
    
    Fixes #24425

--- lib/ansible/parsing/yaml/objects.py
+++ lib/ansible/parsing/yaml/objects.py
@@ -113,6 +113,9 @@ class AnsibleVaultEncryptedUnicode(yaml.YAMLObject, AnsibleUnicode):
     def __repr__(self):
         return repr(self.data)
 
+    def __iter__(self):
+        return iter(self.data)
+
     # Compare a regular str/text_type with the decrypted hypertext
     def __eq__(self, other):
         if self.vault:
@@ -127,11 +130,157 @@ class AnsibleVaultEncryptedUnicode(yaml.YAMLObject, AnsibleUnicode):
             return other != self.data
         return True
 
+    def __add__(self, other):
+        return self.data + other
+
+    def __len__(self, *args, **kwargs):
+        return len(self.data, *args, **kwargs)
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __getslice__(self, i, j):
+        return self.data.__getslice__(i, j)
+
+    def __format__(self, *args, **kwargs):
+        return self.data.__format__(*args, **kwargs)
+
+    def __contains__(self, *args, **kwargs):
+        return self.data.__contains__(*args, **kwargs)
+
     def __str__(self):
         return str(self.data)
 
     def __unicode__(self):
         return unicode(self.data)
 
-    def encode(self, encoding=None, errors=None):
+    def encode(self, encoding=None, errors='strict'):
         return self.data.encode(encoding, errors)
+
+    def capitalize(self):
+        return self.data.capitalize()
+
+    def center(self, *args, **kwargs):
+        return self.data.center(*args, **kwargs)
+
+    def count(self, *args, **kwargs):
+        return self.data.count(*args, **kwargs)
+
+    def decode(self, *args, **kwargs):
+        return self.data.decode(*args, **kwargs)
+
+    def endswith(self, *args, **kwargs):
+        return self.data.endswith(*args, **kwargs)
+
+    def placeh(self, *args, **kwargs):
+        return self.data.placeh(*args, **kwargs)
+
+    def expandtabs(self, *args, **kwargs):
+        return self.data.expandtabs(*args, **kwargs)
+
+    def format(self, *args, **kwargs):
+        return self.data.format(*args, **kwargs)
+
+    def index(self, *args, **kwargs):
+        return self.data.index(*args, **kwargs)
+
+    def isalnum(self, *args, **kwargs):
+        return self.data.isalnum(*args, **kwargs)
+
+    def isalpha(self, *args, **kwargs):
+        return self.data.isalpha(*args, **kwargs)
+
+    def isdigit(self, *args, **kwargs):
+        return self.data.isdigit(*args, **kwargs)
+
+    def islower(self, *args, **kwargs):
+        return self.data.islower(*args, **kwargs)
+
+    def isspace(self, *args, **kwargs):
+        return self.data.isspace(*args, **kwargs)
+
+    def istitle(self, *args, **kwargs):
+        return self.data.istitle(*args, **kwargs)
+
+    def isupper(self, *args, **kwargs):
+        return self.data.isupper(*args, **kwargs)
+
+    def join(self, *args, **kwargs):
+        return self.data.join(*args, **kwargs)
+
+    def ljust(self, *args, **kwargs):
+        return self.data.ljust(*args, **kwargs)
+
+    def lower(self, *args, **kwargs):
+        return self.data.lower(*args, **kwargs)
+
+    def lstrip(self, chars=None):
+        return self.data.lstrip(chars)
+
+    def partition(self, *args, **kwargs):
+        return self.data.partition(*args, **kwargs)
+
+    def replace(self, old, new, count=None):
+        if count is not None:
+            return self.data.replace(old, new, count)
+        return self.data.replace(old, new)
+
+    def rfind(self, *args, **kwargs):
+        return self.data.rfind(*args, **kwargs)
+
+    def rpartition(self, sep):
+        return self.data.rpartition(sep)
+
+    def rsplit(self, sep=None, maxsplit=-1):
+        return self.data.rsplit(sep, maxsplit)
+
+    def rstrip(self, chars=None):
+        return self.data.rstrip(chars)
+
+    def split(self, sep=None, maxsplit=-1):
+        return self.data.split(sep, maxsplit)
+
+    def splitlines(self, *args, **kwargs):
+        return self.data.splitlines(*args, **kwargs)
+
+    def startswith(self, *args, **kwargs):
+        return self.data.startswith(*args, **kwargs)
+
+    def strip(self, *args, **kwargs):
+        return self.data.strip(*args, **kwargs)
+
+    def swapcase(self, *args, **kwargs):
+        return self.data.swapcase(*args, **kwargs)
+
+    def title(self):
+        return self.data.title()
+
+    def translate(self, *args, **kwargs):
+        return self.data.translate(*args, **kwargs)
+
+    def upper(self, *args, **kwargs):
+        return self.data.upper(*args, **kwargs)
+
+    def zfill(self, *args, **kwargs):
+        return self.data.zfill(*args, **kwargs)
+
+    def isnumberic(self, *args, **kwargs):
+        return self.data.isnumberic(*args, **kwargs)
+
+    def isdecimal(self, *args, **kwargs):
+        return self.data.isdecimal(*args, **kwargs)
+
+    # py3 only
+
+    def casefold(self, *args, **kwargs):
+        return self.data.casefold(*args, **kwargs)
+
+    def format_map(self, *args, **kwargs):
+        return self.data.format_map(*args, **kwargs)
+
+
+#    def placeh(self, *args, **kwargs):
+#        return self.data.placeh(*args, **kwargs)
+
+#    def placeh(self, *args, **kwargs):
+#        return self.data.placeh(*args, **kwargs)
