08df3e3ea5813daf350898eb35fb3783b6584f99
Jonathan Meran <jonathan.meran@sonos.com>
2017-05-11 11:09:41 -0400

    Removed nested suboption. Nested suboptions do not seem to be supported by Ansible’s CI for documentation

--- lib/ansible/modules/cloud/amazon/batch_job_definition.py
+++ lib/ansible/modules/cloud/amazon/batch_job_definition.py
@@ -96,15 +96,12 @@ options:
             instance and where it is stored. If the host parameter is empty, then the Docker daemon assigns a host
             path for your data volume, but the data is not guaranteed to persist after the containers associated with
             it stop running.
-        suboptions:
-          sourcePath:
-            description:
-              - The path on the host container instance that is presented to the container. If this parameter is empty,
-                then the Docker daemon has assigned a host path for you. If the host parameter contains a sourcePath
-                file location, then the data volume persists at the specified location on the host container instance
-                until you delete it manually. If the sourcePath value does not exist on the host container instance,
-                the Docker daemon creates it. If the location does exist, the contents of the source path folder are
-                exported.
+            This is a dictionary with one property, sourcePath - The path on the host container
+            instance that is presented to the container. If this parameter is empty,then the Docker daemon has assigned
+            a host path for you. If the host parameter contains a sourcePath file location, then the data volume
+            persists at the specified location on the host container instance until you delete it manually. If the
+            sourcePath value does not exist on the host container instance, the Docker daemon creates it. If the
+            location does exist, the contents of the source path folder are exported.
       name:
         description:
           - The name of the volume. Up to 255 letters (uppercase and lowercase), numbers, hyphens, and underscores are
