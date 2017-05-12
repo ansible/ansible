8bc150d0b0df341dd7b01346efcee436596040ad
s-hertel <shertel@redhat.com>
2017-05-12 12:38:39 -0400

    bring things in an indentation level

--- lib/ansible/modules/cloud/amazon/ec2.py
+++ lib/ansible/modules/cloud/amazon/ec2.py
@@ -1441,20 +1441,20 @@ def startstop_instances(module, ec2, instance_ids, state, instance_tags):
                 changed = True
 
             # Check security groups and if we're using ec2-vpc; ec2-classic security groups may not be modified
-            if inst.vpc_id and (group_id or group_name):
-                if group_name:
-                    grp_details = ec2.get_all_security_groups(filters={'vpc_id': inst.vpc_id})
-                    if isinstance(group_name, basestring):
-                        group_name = [group_name]
-                    unmatched = set(group_name).difference(str(grp.name) for grp in grp_details)
-                    if len(unmatched) > 0:
-                        module.fail_json(msg="The following group names are not valid: %s" % ', '.join(unmatched))
-                    group_ids = [str(grp.id) for grp in grp_details if str(grp.name) in group_name]
-                elif group_id:
-                    if isinstance(group_id, basestring):
-                        group_id = [group_id]
-                    grp_details = ec2.get_all_security_groups(group_ids=group_id)
-                    group_ids = [grp_item.id for grp_item in grp_details]
+            if inst.vpc_id and group_name:
+                grp_details = ec2.get_all_security_groups(filters={'vpc_id': inst.vpc_id})
+                if isinstance(group_name, basestring):
+                    group_name = [group_name]
+                unmatched = set(group_name).difference(str(grp.name) for grp in grp_details)
+                if len(unmatched) > 0:
+                    module.fail_json(msg="The following group names are not valid: %s" % ', '.join(unmatched))
+                group_ids = [str(grp.id) for grp in grp_details if str(grp.name) in group_name]
+            elif inst.vpc_id and group_id:
+                if isinstance(group_id, basestring):
+                    group_id = [group_id]
+                grp_details = ec2.get_all_security_groups(group_ids=group_id)
+                group_ids = [grp_item.id for grp_item in grp_details]
+            if inst.vpc_id and (group_name or group_id):
                 if set([sg.id for sg in inst.groups]) != set(group_ids):
                     changed = inst.modify_attribute('groupSet', group_ids)
 
