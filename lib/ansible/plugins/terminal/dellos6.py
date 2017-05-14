b9ee05fd54beefbff15e1124abb2c76532ddad53
Ganesh B. Nalawade <gnalawad@redhat.com>
2017-05-14 23:13:03 +0530

    Revert "Add dellos6 python3 changes"
    This reverts commit aab3715be1503c9a819805e7098266bd470693cb.

--- lib/ansible/plugins/terminal/dellos6.py
+++ lib/ansible/plugins/terminal/dellos6.py
@@ -29,34 +29,34 @@ from ansible.errors import AnsibleConnectionFailure
 class TerminalModule(TerminalBase):
 
     terminal_stdout_re = [
-        re.compile(br"[\r\n]?[\w+\-\.:\/\[\]]+(?:\([^\)]+\)){,3}(?:>|#) ?$"),
-        re.compile(br"\[\w+\@[\w\-\.]+(?: [^\]])\] ?[>#\$] ?$")
+        re.compile(r"[\r\n]?[\w+\-\.:\/\[\]]+(?:\([^\)]+\)){,3}(?:>|#) ?$"),
+        re.compile(r"\[\w+\@[\w\-\.]+(?: [^\]])\] ?[>#\$] ?$")
     ]
 
     terminal_stderr_re = [
-        re.compile(br"% ?Error: (?:(?!\bdoes not exist\b)(?!\balready exists\b)(?!\bHost not found\b)(?!\bnot active\b).)*$"),
-        re.compile(br"% ?Bad secret"),
-        re.compile(br"invalid input", re.I),
-        re.compile(br"(?:incomplete|ambiguous) command", re.I),
-        re.compile(br"connection timed out", re.I),
-        re.compile(br"'[^']' +returned error code: ?\d+"),
+        re.compile(r"% ?Error: (?:(?!\bdoes not exist\b)(?!\balready exists\b)(?!\bHost not found\b)(?!\bnot active\b).)*$"),
+        re.compile(r"% ?Bad secret"),
+        re.compile(r"invalid input", re.I),
+        re.compile(r"(?:incomplete|ambiguous) command", re.I),
+        re.compile(r"connection timed out", re.I),
+        re.compile(r"'[^']' +returned error code: ?\d+"),
     ]
 
     def on_authorize(self, passwd=None):
-        if self._get_prompt().endswith(b'#'):
+        if self._get_prompt().endswith('#'):
             return
 
-        cmd = {u'command': u'enable'}
+        cmd = {'command': 'enable'}
         if passwd:
-            cmd[u'prompt'] = to_text(r"[\r\n]?password: $", errors='surrogate_or_strict')
-            cmd[u'answer'] = passwd
+            cmd['prompt'] = r"[\r\n]?password:$"
+            cmd['answer'] = passwd
         try:
-            self._exec_cli_command(to_bytes(json.dumps(cmd), errors='surrogate_or_strict'))
+            self._exec_cli_command(json.dumps(cmd))
         except AnsibleConnectionFailure:
             raise AnsibleConnectionFailure('unable to elevate privilege to enable mode')
         # in dellos6 the terminal settings are accepted after the privilege mode
         try:
-            self._exec_cli_command(b'terminal length 0')
+            self._exec_cli_command('terminal length 0')
         except AnsibleConnectionFailure:
             raise AnsibleConnectionFailure('unable to set terminal parameters')
 
@@ -66,9 +66,9 @@ class TerminalModule(TerminalBase):
             # if prompt is None most likely the terminal is hung up at a prompt
             return
 
-        if prompt.strip().endswith(b')#'):
-            self._exec_cli_command(b'end')
-            self._exec_cli_command(b'disable')
+        if prompt.strip().endswith(')#'):
+            self._exec_cli_command('end')
+            self._exec_cli_command('disable')
 
-        elif prompt.endswith(b'#'):
-            self._exec_cli_command(b'disable')
+        elif prompt.endswith('#'):
+            self._exec_cli_command('disable')
