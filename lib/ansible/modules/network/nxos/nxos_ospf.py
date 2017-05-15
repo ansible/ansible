dc5af995444a5a37eb314033fe0d5fbb1c1b1767
Trishna Guha <trishnaguha17@gmail.com>
2017-05-15 12:15:32 +0530

    nxos_ospf fixes refactor
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/modules/network/nxos/nxos_ospf.py
+++ lib/ansible/modules/network/nxos/nxos_ospf.py
@@ -16,10 +16,11 @@
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-ANSIBLE_METADATA = {'metadata_version': '1.0',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
-
+ANSIBLE_METADATA = {
+    'metadata_version': '1.0',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
 
 DOCUMENTATION = '''
 ---
@@ -28,59 +29,35 @@ extends_documentation_fragment: nxos
 version_added: "2.2"
 short_description: Manages configuration of an ospf instance.
 description:
-    - Manages configuration of an ospf instance.
+  - Manages configuration of an ospf instance.
 author: Gabriele Gerbino (@GGabriele)
 options:
-    ospf:
-        description:
-            - Name of the ospf instance.
-        required: true
-    state:
-        description:
-            - Determines whether the config should be present or not
-              on the device.
-        required: false
-        default: present
-        choices: ['present','absent']
+  ospf:
+    description:
+      - Name of the ospf instance.
+    required: true
+  state:
+    description:
+      - Determines whether the config should be present or not
+        on the device.
+    required: false
+    default: present
+    choices: ['present','absent']
 '''
 
 EXAMPLES = '''
 - nxos_ospf:
     ospf: 1
     state: present
-    username: "{{ un }}"
-    password: "{{ pwd }}"
-    host: "{{ inventory_hostname }}"
 '''
 
 RETURN = '''
-proposed:
-    description: k/v pairs of parameters passed into module
-    returned: verbose mode
-    type: dict
-    sample: {"ospf": "1"}
-existing:
-    description: k/v pairs of existing configuration
-    returned: verbose mode
-    type: dict
-    sample: {"ospf": ["2"]}
-end_state:
-    description: k/v pairs of configuration after module execution
-    returned: verbose mode
-    type: dict
-    sample: {"ospf": ["1", "2"]}
-updates:
+commands:
     description: commands sent to the device
     returned: always
     type: list
     sample: ["router ospf 1"]
-changed:
-    description: check to see if a change was made on the device
-    returned: always
-    type: boolean
-    sample: true
 '''
-
 
 import re
 from ansible.module_utils.nxos import get_config, load_config, run_commands
@@ -88,17 +65,10 @@ from ansible.module_utils.nxos import nxos_argument_spec, check_args
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.netcfg import CustomNetworkConfig
 
-import re
 
 PARAM_TO_COMMAND_KEYMAP = {
     'ospf': 'router ospf'
 }
-
-
-def invoke(name, *args, **kwargs):
-    func = globals().get(name)
-    if func:
-        return func(*args, **kwargs)
 
 
 def get_value(config, module):
@@ -132,14 +102,10 @@ def get_existing(module):
 
 def apply_key_map(key_map, table):
     new_dict = {}
-    for key, value in table.items():
+    for key, value in table:
         new_key = key_map.get(key)
         if new_key:
-            value = table.get(key)
-            if value:
-                new_dict[new_key] = value
-            else:
-                new_dict[new_key] = value
+            new_dict[new_key] = table.get(key)
     return new_dict
 
 
@@ -156,8 +122,7 @@ def state_absent(module, proposed, candidate):
 def main():
     argument_spec = dict(
         ospf=dict(required=True, type='str'),
-        state=dict(choices=['present', 'absent'], default='present',
-                       required=False),
+        state=dict(choices=['present', 'absent'], default='present', required=False),
         include_defaults=dict(default=True),
         config=dict(),
         save=dict(type='bool', default=False)
@@ -165,18 +130,16 @@ def main():
 
     argument_spec.update(nxos_argument_spec)
 
-    module = AnsibleModule(argument_spec=argument_spec,
-                        supports_check_mode=True)
+    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
 
     warnings = list()
     check_args(module, warnings)
-
+    result = dict(changed=False, warnings=warnings)
 
     state = module.params['state']
     ospf = str(module.params['ospf'])
 
-    existing = invoke('get_existing', module)
-    end_state = existing
+    existing = get_existing(module)
     proposed = dict(ospf=ospf)
 
     if not existing:
@@ -184,25 +147,22 @@ def main():
     else:
         existing_list = existing['ospf']
 
-    result = {}
-    if (state == 'present' or (state == 'absent' and ospf in existing_list)):
-        candidate = CustomNetworkConfig(indent=3)
-        invoke('state_%s' % state, module, proposed, candidate)
-        response = load_config(module, candidate)
-        result.update(response)
+    candidate = CustomNetworkConfig(indent=3)
+    if state == 'present':
+        state_present(module, proposed, candidate)
+    if state == 'absent' and ospf in existing_list:
+        state_absent(module, proposed, candidate)
+
+    if candidate:
+        candidate = candidate.items_text()
+        load_config(module, candidate)
+        result['changed'] = True
+        result['commands'] = candidate
 
     else:
-        result['updates'] = []
-
-    if module._verbosity > 0:
-        end_state = invoke('get_existing', module)
-        result['end_state'] = end_state
-        result['existing'] = existing
-        result['proposed'] = proposed
-
+        result['commands'] = []
     module.exit_json(**result)
 
 
 if __name__ == '__main__':
     main()
-
