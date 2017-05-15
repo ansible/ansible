faf6cc4eade193ffc98d2c3b95b257a2ca780014
Trishna Guha <trishnaguha17@gmail.com>
2017-05-15 12:23:23 +0530

    unit test nxos_ospf
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/modules/network/nxos/nxos_ospf.py
+++ lib/ansible/modules/network/nxos/nxos_ospf.py
@@ -60,7 +60,7 @@ commands:
 '''
 
 import re
-from ansible.module_utils.nxos import get_config, load_config, run_commands
+from ansible.module_utils.nxos import get_config, load_config
 from ansible.module_utils.nxos import nxos_argument_spec, check_args
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.netcfg import CustomNetworkConfig
