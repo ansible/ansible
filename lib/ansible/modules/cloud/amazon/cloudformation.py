cc57cd6846046377e4505c29a7fb3d2af3abbc48
Jose Armesto <github@armesto.net>
2017-05-16 18:25:29 +0200

    Fixed comment

--- lib/ansible/modules/cloud/amazon/cloudformation.py
+++ lib/ansible/modules/cloud/amazon/cloudformation.py
@@ -343,7 +343,7 @@ def stack_operation(cfn, stack_name, operation):
             else:
                 ret.update({'changed': False, 'failed': True, 'output' : 'Stack not found.'})
                 return ret
-        # it covers ROLLBACK_COMPLETE, UPDATE_ROLLBACK_COMPLETE and UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS
+        # it covers ROLLBACK_COMPLETE and UPDATE_ROLLBACK_COMPLETE
         # Possible states: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-describing-stacks.html#w1ab2c15c17c21c13
         elif stack['StackStatus'].endswith('ROLLBACK_COMPLETE'):
             ret.update({'changed': True, 'failed': True, 'output': 'Problem with %s. Rollback complete' % operation})
