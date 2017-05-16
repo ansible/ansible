f4e1dc503b1a3c5e159bebdd09fddc4623c266d9
Ganesh Nalawade <ganesh634@gmail.com>
2017-05-17 00:32:41 +0530

    Add iosxr changes for Python3 (#24605)
    * Add iosxr changes for Python3
    
    Make `execute_command` arguments and its
    return value complaint to PY3 changes
    made in PR #24431
    
    * Fix CI issue

--- lib/ansible/module_utils/iosxr.py
+++ lib/ansible/module_utils/iosxr.py
@@ -26,6 +26,7 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
+from ansible.module_utils._text import to_text
 from ansible.module_utils.basic import env_fallback, return_values
 from ansible.module_utils.network_common import to_list, ComplexList
 from ansible.module_utils.connection import exec_command
@@ -42,17 +43,18 @@ iosxr_argument_spec = {
     'provider': dict(type='dict')
 }
 
+
 def check_args(module, warnings):
     provider = module.params['provider'] or {}
     for key in iosxr_argument_spec:
         if key != 'provider' and module.params[key]:
-            warnings.append('argument %s has been deprecated and will be '
-                    'removed in a future version' % key)
+            warnings.append('argument %s has been deprecated and will be removed in a future version' % key)
 
     if provider:
         for param in ('password',):
             if provider.get(param):
                 module.no_log_values.update(return_values(provider[param]))
+
 
 def get_config(module, flags=[]):
     cmd = 'show running-config '
@@ -64,8 +66,8 @@ def get_config(module, flags=[]):
     except KeyError:
         rc, out, err = exec_command(module, cmd)
         if rc != 0:
-            module.fail_json(msg='unable to retrieve current config', stderr=err)
-        cfg = str(out).strip()
+            module.fail_json(msg='unable to retrieve current config', stderr=to_text(err, errors='surrogate_or_strict'))
+        cfg = to_text(out, errors='surrogate_or_strict').strip()
         _DEVICE_CONFIGS[cmd] = cfg
         return cfg
 
@@ -87,15 +89,16 @@ def run_commands(module, commands, check_rc=True):
         cmd = module.jsonify(cmd)
         rc, out, err = exec_command(module, cmd)
         if check_rc and rc != 0:
-            module.fail_json(msg=err, rc=rc)
-        responses.append(out)
+            module.fail_json(msg=to_text(err, errors='surrogate_or_strict'), rc=rc)
+        responses.append(to_text(out, errors='surrogate_or_strict'))
     return responses
+
 
 def load_config(module, commands, warnings, commit=False, replace=False, comment=None):
 
     rc, out, err = exec_command(module, 'configure terminal')
     if rc != 0:
-        module.fail_json(msg='unable to enter configuration mode', err=err)
+        module.fail_json(msg='unable to enter configuration mode', err=to_text(err, errors='surrogate_or_strict'))
 
     failed = False
     for command in to_list(commands):
@@ -109,7 +112,7 @@ def load_config(module, commands, warnings, commit=False, replace=False, comment
 
     if failed:
         exec_command(module, 'abort')
-        module.fail_json(msg=err, commands=commands, rc=rc)
+        module.fail_json(msg=to_text(err, errors='surrogate_or_strict'), commands=commands, rc=rc)
 
     rc, diff, err = exec_command(module, 'show commit changes diff')
     if rc != 0:
@@ -128,4 +131,4 @@ def load_config(module, commands, warnings, commit=False, replace=False, comment
         diff = None
     exec_command(module, cmd)
 
-    return diff
+    return to_text(diff, errors='surrogate_or_strict')
