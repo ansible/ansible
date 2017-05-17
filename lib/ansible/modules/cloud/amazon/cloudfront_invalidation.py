1697c7d7b897d5b03f9a3ba06c651f7cc9a5e472
Willem van Ketwich <willvk@gmail.com>
2017-05-17 22:22:13 +1000

    added recommendations, added module_utils module for common cloudfront helpers

--- lib/ansible/modules/cloud/amazon/cloudfront_invalidation.py
+++ lib/ansible/modules/cloud/amazon/cloudfront_invalidation.py
@@ -32,49 +32,61 @@ author: Willem van Ketwich (@wilvk)
 options:
     distribution_id:
       description:
-        - The alias of the cloudfront distribution.
+        - The id of the cloudfront distribution to invalidate paths for.
+        Can be specified insted of the alias.
+      required: false
+    alias:
+      description:
+        - The alias of the cloudfront distribution to invalidate paths for.
           Can be specified instead of distribution_id.
       required: false
     caller_reference:
       description:
-        - The id of the cloudfront distribution.
+        - A unique reference identifier for the invalidation paths.
       required: false
       default: current datetime stamp
-    invalidation_batch:
+    target_paths:
       description:
         - A list of paths on the distribution to invalidate.
-          Each path should begin with '/'
+          Each path should begin with '/'.
+          Windcards are allowed. eg. '/foo/bar/*'
       required: true
 '''
 
 EXAMPLES = '''
 
-# create a batch of invalidations
+# create a batch of invalidations using a distribution_id for a reference
+
 - cloudfront_invalidation:
     distribution_id: E15BU8SDCGSG57
     caller_reference: testing 123
-    invalidation_batch:
-      - /testpathone/test1.txt
-      - /testpathtwo/test2.log
-      - /testpaththree/test3.log
+    target_paths:
+      - /testpathone/test1.css
+      - /testpathtwo/test2.js
+      - /testpaththree/test3.ss
 
-# create a batch of invalidations
+# create a batch of invalidations using an alias as a reference
+# and one path using a wildcard match
+
 - cloudfront_invalidation:
     alias: alias.test.com
     caller_reference: testing 123
-    invalidation_batch:
-      - /testpathone/test4.txt
-      - /testpathtwo/test5.log
-      - /testpaththree/test6.log
+    target_paths:
+      - /testpathone/test4.css
+      - /testpathtwo/test5.js
+      - /testpaththree/*
+
 '''
 
 RETURN = '''
 
 '''
 
-from ansible.module_utils.ec2 import get_aws_connection_info, ec2_argument_spec, boto3_conn, HAS_BOTO3
+from ansible.module_utils.ec2 import get_aws_connection_info
+from ansible.module_utils.ec2 import ec2_argument_spec, boto3_conn, HAS_BOTO3
+from ansible.module_utils.ec2 import camel_dict_to_snake_dict
 from ansible.module_utils.basic import AnsibleModule
-from ansible.modules.cloud.amazon.cloudfront_distribution import CloudFrontHelpers
+from ansible.module_utils.cloudfront import CloudFrontHelpers
 from ansible.modules.cloud.amazon.cloudfront_facts import CloudFrontFactsServiceManager
 import datetime
 from functools import partial
@@ -88,7 +100,7 @@ except ImportError:
 
 class CloudFrontInvalidationServiceManager:
     """
-    Handles CloudFront service calls to AWS
+    Handles CloudFront service calls to AWS for invalidations
     """
 
     def __init__(self, module):
@@ -97,28 +109,34 @@ class CloudFrontInvalidationServiceManager:
 
     def create_client(self, resource):
         try:
-            region, ec2_url, aws_connect_kwargs = get_aws_connection_info(self.module, boto3=True)
-            self.client = boto3_conn(self.module, conn_type='client', resource=resource,
-                                     region=region, endpoint=ec2_url, **aws_connect_kwargs)
+            region, ec2_url, aws_connect_kwargs = get_aws_connection_info(
+                self.module, boto3=True)
+            self.client = boto3_conn(
+                self.module, conn_type='client', resource=resource,
+                region=region, endpoint=ec2_url, **aws_connect_kwargs)
         except botocore.exceptions.NoRegionError:
-            self.module.fail_json(msg=("region must be specified as a parameter in "
-                                       "AWS_DEFAULT_REGION environment variable or in "
-                                       "boto configuration file"))
+            self.module.fail_json(
+                msg=("region must be specified as a parameter in "
+                     "AWS_DEFAULT_REGION environment variable or in "
+                     "boto configuration file"))
         except botocore.exceptions.ClientError as e:
-            self.module.fail_json(msg="unable to establish connection - " + str(e),
-                                  exception=traceback.format_exc(),
-                                  **camel_dict_to_snake_dict(e.response))
+            self.module.fail_json(
+                msg="unable to establish connection - " + str(e),
+                exception=traceback.format_exc(),
+                **camel_dict_to_snake_dict(e.response))
 
     def create_invalidation(self, distribution_id, invalidation_batch):
         try:
-            response = self.client.create_invalidation(DistributionId=distribution_id,
-                                                       InvalidationBatch=invalidation_batch)
+            response = self.client.create_invalidation(
+                DistributionId=distribution_id,
+                InvalidationBatch=invalidation_batch)
             response.pop('ResponseMetadata', None)
             return response
         except botocore.exceptions.ClientError as e:
-            self.module.fail_json(msg="error creating invalidation(s) - " + str(e),
-                                  exception=traceback.format_exc(),
-                                  **camel_dict_to_snake_dict(e.response))
+            self.module.fail_json(
+                msg="error creating invalidation(s) - " + str(e),
+                exception=traceback.format_exc(),
+                **camel_dict_to_snake_dict(e.response))
 
 
 class CloudFrontInvalidationValidationManager:
@@ -134,14 +152,17 @@ class CloudFrontInvalidationValidationManager:
     def validate_distribution_id(self, distribution_id, alias):
         try:
             if distribution_id is None and alias is None:
-                self.module.fail_json(msg="distribution_id or alias must be specified")
+                self.module.fail_json(
+                    msg="distribution_id or alias must be specified")
             if distribution_id is None:
                 distribution_id = self.__cloudfront_facts_mgr.get_distribution_id_from_domain_name(
                     alias)
             return distribution_id
         except Exception as e:
             self.module.fail_json(
-                msg="error validating parameters for distribution update and delete - " + str(e))
+                msg="error validating parameters for distribution update and delete - " +
+                str(e), exception=traceback.format_exc(),
+                **camel_dict_to_snake_dict(e.response))
 
     def validate_invalidation_batch(self, invalidation_batch, caller_reference):
         try:
@@ -155,7 +176,10 @@ class CloudFrontInvalidationValidationManager:
             }
             return valid_invalidation_batch
         except Exception as e:
-            self.module.fail_json(msg="error validating invalidation batch - " + str(e))
+            self.module.fail_json(
+                msg="error validating invalidation batch - " +
+                str(e), exception=traceback.format_exc(),
+                **camel_dict_to_snake_dict(e.response))
 
 
 def main():
@@ -165,10 +189,15 @@ def main():
         caller_reference=dict(required=False, default=None, type='str'),
         distribution_id=dict(required=False, default=None, type='str'),
         alias=dict(required=False, default=None, type='str'),
-        invalidation_batch=dict(required=True, default=None, type='list')
+        target_paths=dict(required=True, default=None, type='list')
     ))
 
-    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=False)
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=False,
+        mutually_exclusive=[
+            ['distribution_id', 'alias']
+        ])
 
     helpers = CloudFrontHelpers()
     validation_mgr = CloudFrontInvalidationValidationManager(module)
@@ -177,19 +206,19 @@ def main():
     caller_reference = module.params.get('caller_reference')
     distribution_id = module.params.get('distribution_id')
     alias = module.params.get('alias')
-    invalidation_batch = module.params.get('invalidation_batch')
+    target_paths = module.params.get('target_paths')
 
     result = {}
 
-    if distribution_id is None and alias is None:
-        module.fail_json(msg="please specify either distribution_id or alias for the invalidation(s)")
+    distribution_id = validation_mgr.validate_distribution_id(
+        distribution_id, alias)
 
-    distribution_id = validation_mgr.validate_distribution_id(distribution_id, alias)
-
-    valid_invalidation_batch = validation_mgr.validate_invalidation_batch(
-        invalidation_batch, caller_reference)
-    valid_pascal_invalidation_batch = helpers.snake_dict_to_pascal_dict(valid_invalidation_batch)
-    result = service_mgr.create_invalidation(distribution_id, valid_pascal_invalidation_batch)
+    valid_target_paths = validation_mgr.validate_invalidation_batch(
+        target_paths, caller_reference)
+    valid_pascal_target_paths = helpers.snake_dict_to_pascal_dict(
+        valid_target_paths)
+    result = service_mgr.create_invalidation(
+        distribution_id, valid_pascal_target_paths)
 
     module.exit_json(changed=True, **helpers.pascal_dict_to_snake_dict(result))
 
