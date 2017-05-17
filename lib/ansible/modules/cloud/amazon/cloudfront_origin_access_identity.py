5a8cbf82a1ccbbddb1fcb64efc9047514ec2e703
Willem van Ketwich <willvk@gmail.com>
2017-05-17 23:22:16 +1000

    refactoring as advised

--- lib/ansible/modules/cloud/amazon/cloudfront_origin_access_identity.py
+++ lib/ansible/modules/cloud/amazon/cloudfront_origin_access_identity.py
@@ -82,8 +82,8 @@ location:
 
 from ansible.module_utils.ec2 import get_aws_connection_info, ec2_argument_spec
 from ansible.module_utils.ec2 import boto3_conn, HAS_BOTO3
-from ansible.modules.cloud.amazon.cloudfront_distribution import CloudFrontHelpers
 from ansible.modules.cloud.amazon.cloudfront_facts import CloudFrontFactsServiceManager
+from ansible.module_utils.cloudfront import CloudFrontHelpers
 from ansible.module_utils.ec2 import camel_dict_to_snake_dict
 from ansible.module_utils.basic import AnsibleModule
 from botocore.signers import CloudFrontSigner
@@ -100,7 +100,7 @@ except ImportError:
 
 class CloudFrontOriginAccessIdentityServiceManager:
     """
-    handles cloudfront origin access identity service calls to aws
+    Handles cloudfront origin access identity service calls to aws
     """
 
     def __init__(self, module):
@@ -109,75 +109,56 @@ class CloudFrontOriginAccessIdentityServiceManager:
 
     def create_client(self, resource):
         try:
-            region, ec2_url, aws_connect_kwargs = get_aws_connection_info(self.module, boto3=True)
-            self.client = boto3_conn(self.module, conn_type='client', resource=resource,
-                                     region=region, endpoint=ec2_url, **aws_connect_kwargs)
+            region, ec2_url, aws_connect_kwargs = get_aws_connection_info(
+                self.module, boto3=True)
+            self.client = boto3_conn(
+                self.module, conn_type='client', resource=resource,
+                region=region, endpoint=ec2_url, **aws_connect_kwargs)
         except botocore.exceptions.NoRegionError:
-            self.module.fail_json(msg=("region must be specified as a parameter in "
-                                       "AWS_DEFAULT_REGION environment variable or in "
-                                       "boto configuration file"))
+            self.module.fail_json(
+                msg=("region must be specified as a parameter in "
+                     "AWS_DEFAULT_REGION environment variable or in "
+                     "boto configuration file"))
         except botocore.exceptions.ClientError as e:
-            self.module.fail_json(msg="unable to establish connection - " + str(e),
-                                  exception=traceback.format_exc(),
-                                  **camel_dict_to_snake_dict(e.response))
+            self.module.fail_json(
+                msg="unable to establish connection - " + str(e),
+                exception=traceback.format_exc(),
+                **camel_dict_to_snake_dict(e.response))
 
     def create_origin_access_identity(self, caller_reference, comment):
         try:
-            func = partial(self.client.create_cloud_front_origin_access_identity,
-                           CloudFrontOriginAccessIdentityConfig={
-                               'CallerReference': caller_reference,
-                               'Comment': comment
-                           })
-            return self.paginated_response(func)
+            return self.client.create_cloud_front_origin_access_identity(
+                CloudFrontOriginAccessIdentityConfig={
+                    'CallerReference': caller_reference,
+                    'Comment': comment
+                })
         except botocore.exceptions.ClientError as e:
-            self.module.fail_json(msg="error creating cloud front origin access identity - " + str(e),
-                                  exception=traceback.format_exc(),
-                                  **camel_dict_to_snake_dict(e.response))
+            self.module.fail_json(
+                msg="error creating cloud front origin access identity - " +
+                str(e) + "\n" + traceback.format_exc())
 
     def delete_origin_access_identity(self, origin_access_identity_id, e_tag):
         try:
-            func = partial(self.client.delete_cloud_front_origin_access_identity,
-                           Id=origin_access_identity_id, IfMatch=e_tag)
-            return self.paginated_response(func)
+            return self.client.delete_cloud_front_origin_access_identity(
+                Id=origin_access_identity_id, IfMatch=e_tag)
         except botocore.exceptions.ClientError as e:
-            self.module.fail_json(msg="error deleting cloud front origin access identity - " + str(e),
-                                  exception=traceback.format_exc(),
-                                  **camel_dict_to_snake_dict(e.response))
+            self.module.fail_json(
+                msg="error deleting cloud front origin access identity - " +
+                str(e) + "\n" + traceback.format_exc())
 
-    def update_origin_access_identity(self, caller_reference, comment, origin_access_identity_id, e_tag):
+    def update_origin_access_identity(self, caller_reference, comment,
+                                      origin_access_identity_id, e_tag):
         try:
-            func = partial(self.client.update_cloud_front_origin_access_identity,
-                           CloudFrontOriginAccessIdentityConfig={
-                               'CallerReference': caller_reference,
-                               'Comment': comment
-                           },
-                           Id=origin_access_identity_id, IfMatch=e_tag)
-            return self.paginated_response(func)
+            return self.client.update_cloud_front_origin_access_identity(
+                CloudFrontOriginAccessIdentityConfig={
+                    'CallerReference': caller_reference,
+                    'Comment': comment
+                },
+                Id=origin_access_identity_id, IfMatch=e_tag)
         except botocore.exceptions.ClientError as e:
-            self.module.fail_json(msg="error updating cloud front origin access identity - " + str(e),
-                                  exception=traceback.format_exc(),
-                                  **camel_dict_to_snake_dict(e.response))
-
-    def paginated_response(self, func, result_key=''):
-        '''
-        Returns expanded response for paginated operations.
-        The 'result_key' is used to define the concatenated results that are combined
-        from each paginated response.
-        '''
-        args = dict()
-        results = dict()
-        loop = True
-        while loop:
-            response = func(**args)
-            if result_key == '':
-                result = response
-                result.pop('ResponseMetadata', None)
-            else:
-                result = response.get(result_key)
-            results.update(result)
-            args['NextToken'] = response.get('NextToken')
-            loop = args['NextToken'] is not None
-        return results
+            self.module.fail_json(
+                msg="error updating cloud front origin access identity - " +
+                str(e) + "\n" + traceback.format_exc())
 
 
 class CloudFrontOriginAccessIdentityValidationManager:
@@ -192,10 +173,13 @@ class CloudFrontOriginAccessIdentityValidationManager:
 
     def get_etag_from_origin_access_identity_id(self, origin_access_identity_id):
         try:
-            oai = self.__cloudfront_facts_mgr.get_origin_access_identity(origin_access_identity_id)
+            oai = self.__cloudfront_facts_mgr.get_origin_access_identity(
+                origin_access_identity_id)
             return oai.get('ETag')
         except Exception as e:
-            self.module.fail_json(msg="error getting etag from origin_access_identity_id - " + str(e))
+            self.module.fail_json(
+                msg="error getting etag from origin_access_identity_id - " +
+                str(e) + "\n" + traceback.format_exc())
 
 
 def main():
@@ -203,7 +187,7 @@ def main():
 
     argument_spec.update(dict(
         state=dict(choices=['present', 'updated', 'absent'], default='present'),
-        origin_access_identity_id=dict(required=True, default=None, type='str'),
+        origin_access_identity_id=dict(required=False, default=None, type='str'),
         caller_reference=dict(required=False, default=None, type='str'),
         comment=dict(required=False, default=None, type='str')
     ))
@@ -230,6 +214,10 @@ def main():
     elif state == 'updated':
         result = service_mgr.update_origin_access_identity(
             caller_reference, comment, origin_access_identity_id, e_tag)
+
+    results = result
+    results.pop('ResponseMetadata', None)
+    result.update(results)
 
     module.exit_json(changed=True, **helpers.pascal_dict_to_snake_dict(result))
 
