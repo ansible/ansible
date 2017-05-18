5babe2daeac9cc1374cb6246aea60d641cc5d49d
Matt Clay <matt@mystile.com>
2017-05-19 01:37:53 +0800

    Increase python version coverage for tests. (#24762)
    * Improve ansible-test inventory handling.
    * Fix python 3 re-raise of exception from thread.
    * Fix python 3 encoding for windows-integration.
    * Run network tests on multiple python versions.
    * Run windows tests on multiple python versions.
    * Support Shippable delegation using --tox.
    * Skip vyos_command on python 3 tests until fixed.
    * Add python 3 filtering to local and tox.
    * Fix tests to support back to back runs.
    * Temporarily test networking with python 2.7 only.
    
    Running the tests back to back causes intermittent test failures
    which need to be addressed before we can test multiple versions
    in a single test run.

--- test/utils/shippable/network.sh
+++ test/utils/shippable/network.sh
@@ -12,22 +12,38 @@ fi
 
 target="network/ci/"
 
+# python versions to test in order
+# all versions run full tests
+python_versions=(
+    2.7
+)
+
 if [ -s /tmp/network.txt ]; then
     echo "Detected changes requiring integration tests specific to networking:"
     cat /tmp/network.txt
 
     echo "Running network integration tests for multiple platforms concurrently."
 
-    # shellcheck disable=SC2086
-    ansible-test network-integration --color -v --retry-on-error "${target}" --requirements ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
-        --platform vyos/1.1.0 \
-        --platform ios/csr1000v \
-
+    platforms=(
+        --platform vyos/1.1.0
+        --platform ios/csr1000v
+    )
 else
     echo "No changes requiring integration tests specific to networking were detected."
     echo "Running network integration tests for a single platform only."
 
-    # shellcheck disable=SC2086
-    ansible-test network-integration --color -v --retry-on-error "${target}" --requirements ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
+    platforms=(
         --platform vyos/1.1.0
+    )
 fi
+
+retry.py pip install tox --disable-pip-version-check
+
+for version in "${python_versions[@]}"; do
+    # clean up between test runs until we switch from --tox to --docker
+    rm -rf ~/.ansible/{cp,pc,tmp}/
+
+    # shellcheck disable=SC2086
+    ansible-test network-integration --color -v --retry-on-error "${target}" --tox --python "${version}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
+        "${platforms[@]}"
+done
