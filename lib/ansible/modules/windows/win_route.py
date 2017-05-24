766274b538347e86e721dcdcdd18e2f248e1fd44
Daniele Lazzari <lazzari@mailup.com>
2017-05-24 11:22:46 +0200

    add conditional statement in test, add os requirementes in documentation

--- lib/ansible/modules/windows/win_route.py
+++ lib/ansible/modules/windows/win_route.py
@@ -49,6 +49,7 @@ options:
       - If present, it adds a network static route.
         If absent, it removes a network static route.
     default: present
+notes: Works only with Windows 2012 R2 or latest.
 author: Daniele Lazzari
 '''
 
