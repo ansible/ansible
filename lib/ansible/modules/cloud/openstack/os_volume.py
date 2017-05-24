625ee36e06bccfe2f1c3413411c9f23b32936e48
Abhijeet Kasurde <akasurde@redhat.com>
2017-05-24 21:46:23 +0530

    Make os_volume idempotent (#24881)
    Fix adds idempotency while deleting volume in os_volume
    module
    
    Fixes #19619
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/cloud/openstack/os_volume.py
+++ lib/ansible/modules/cloud/openstack/os_volume.py
@@ -131,14 +131,16 @@ def _present_volume(module, cloud):
 
 
 def _absent_volume(module, cloud):
-    try:
-        cloud.delete_volume(
-            name_or_id=module.params['display_name'],
-            wait=module.params['wait'],
-            timeout=module.params['timeout'])
-    except shade.OpenStackCloudTimeout:
-        module.exit_json(changed=False)
-    module.exit_json(changed=True)
+    changed = False
+    if cloud.volume_exists(module.params['display_name']):
+        try:
+            changed = cloud.delete_volume(name_or_id=module.params['display_name'],
+                                          wait=module.params['wait'],
+                                          timeout=module.params['timeout'])
+        except shade.OpenStackCloudTimeout:
+            module.exit_json(changed=changed)
+
+    module.exit_json(changed=changed)
 
 
 def main():
