f829d583d627d60cf72cab48da0d5259219b100f
Daniel Wright <dan@iamaman.com.au>
2017-05-27 00:18:16 +1000

    [cloud] Backport handling of CloudFormation int parameter values - fixes #23593 (#25051)

--- lib/ansible/modules/cloud/amazon/cloudformation.py
+++ lib/ansible/modules/cloud/amazon/cloudformation.py
@@ -433,7 +433,7 @@ def main():
         stack_params['StackPolicyBody'] = open(module.params['stack_policy'], 'r').read()
 
     template_parameters = module.params['template_parameters']
-    stack_params['Parameters'] = [{'ParameterKey':k, 'ParameterValue':v} for k, v in template_parameters.items()]
+    stack_params['Parameters'] = [{'ParameterKey':k, 'ParameterValue':str(v)} for k, v in template_parameters.items()]
 
     if isinstance(module.params.get('tags'), dict):
         stack_params['Tags'] = ansible.module_utils.ec2.ansible_dict_to_boto3_tag_list(module.params['tags'])
