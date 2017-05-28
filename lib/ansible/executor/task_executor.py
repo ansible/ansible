9406c77d71767e7b2ae484ffd35570bc74ee3834
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2017-05-28 15:24:22 +0200

    Pull metadata with plugin_docs get_docstring
    Using load_source from PluginLoader is troublesome, it is not guaranteed
    a module may be importable at the controller, e.g. if a module depends
    on module_utils functions it won't work, because module_utils is not
    in the sys path.
    Rather than putting that module dependencies introspection, just
    use plain parsing like plugin_docs get_docstring does as we only care
    about reading ANSIBLE_METADATA.

--- lib/ansible/executor/task_executor.py
+++ lib/ansible/executor/task_executor.py
@@ -34,6 +34,7 @@ from ansible.plugins.connection import ConnectionBase
 from ansible.template import Templar
 from ansible.utils.encrypt import key_for_hostname
 from ansible.utils.listify import listify_lookup_plugin_terms
+from ansible.utils.plugin_docs import get_docstring
 from ansible.utils.ssh_functions import check_for_controlpersist
 from ansible.utils.unsafe_proxy import UnsafeProxy, wrap_var
 
@@ -778,19 +779,18 @@ class TaskExecutor:
         Returns the correct action plugin to handle the requestion task action
         '''
 
-        module = self._task.action
         module_path = self._shared_loader_obj.module_loader.find_plugin(self._task.action)
         module_prefix = self._task.action.split('_')[0]
         is_python_module = module_path.endswith('.py')
 
         if is_python_module:
-            metadata = self._shared_loader_obj.module_loader._load_module_metadata(module, module_path)
+            metadata = get_docstring(module_path)[3]
 
         # let action plugin override module, fallback to 'normal' action plugin otherwise
         if is_python_module and metadata and 'action_handler' in metadata:
             handler_name = metadata['action_handler']
-        elif module in self._shared_loader_obj.action_loader:
-            handler_name = module
+        elif self._task.action in self._shared_loader_obj.action_loader:
+            handler_name = self._task.action
         elif all((module_prefix in C.NETWORK_GROUP_MODULES, module_prefix in self._shared_loader_obj.action_loader)):
             handler_name = module_prefix
         else:
