f5a0806154388216ca9f923721f500f3472e16ba
Dag Wieers <dag@wieers.com>
2017-05-29 00:21:57 +0200

    Fall back to using mount/umount if 7zip not found
    As discussed with others.
    
    Also improved integration tests.

--- lib/ansible/modules/files/iso_extract.py
+++ lib/ansible/modules/files/iso_extract.py
@@ -3,6 +3,7 @@
 
 # Copyright: (c) 2013, Jeroen Hoekx <jeroen.hoekx@dsquare.be>
 # Copyright: (c) 2016, Matt Robinson <git@nerdoftheherd.com>
+# Copyright: (c) 2017, Dag Wieers <dag@wieers.com>
 # Copyright: (c) 2017, Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
@@ -22,44 +23,51 @@ author:
 module: iso_extract
 short_description: Extract files from an ISO image
 description:
-- This module extracts files from an ISO into a temporary directory and copies
-  files from there to a given destination, if needed.
+- This module has two possible ways of operation.
+- If 7zip is installed on the system, this module extracts files from an ISO
+  into a temporary directory and copies files to a given destination,
+  if needed.
+- If the user has mount-capabilities (CAP_SYS_ADMIN on Linux) this module
+  mounts the ISO image to a temporary location, and copies files to a given
+  destination, if needed.
 version_added: '2.3'
 requirements:
-- 7z (from I(7zip) or I(p7zip) package)
+- Either 7z (from I(7zip) or I(p7zip) package)
+- Or mount capabilities (root-access, or CAP_SYS_ADMIN capability on Linux)
 options:
   image:
     description:
     - The ISO image to extract files from.
-    required: true
-    aliases: ['path', 'src']
+    required: yes
+    aliases: [ path, src ]
   dest:
     description:
     - The destination directory to extract files to.
-    required: true
+    required: yes
   files:
     description:
     - A list of files to extract from the image.
     - Extracting directories does not work.
-    required: true
+    required: yes
   force:
     description:
     - If C(yes), which will replace the remote file when contents are different than the source.
     - If C(no), the file will only be extracted and copied if the destination does not already exist.
-    version_added: '2.4'
     type: bool
     default: 'yes'
-    aliases: [ "thirsty" ]
+    aliases: [ thirsty ]
+    version_added: '2.4'
   executable:
     description:
     - The path to the C(7z) executable to use for extracting files from the ISO.
-    version_added: '2.4'
     default: '7z'
+    version_added: '2.4'
 notes:
 - Only the file checksum (content) is taken into account when extracting files
-  from the ISO image. If C(force=no) then it only checks the presence of the file.
-- In Ansible v2.3 this module was using C(mount) and C(umount) commands, requiring
-  root access. This is no longer needed with the introduction of 7zip for extraction.
+  from the ISO image. If C(force=no),  only checks the presence of the file.
+- In Ansible v2.3 this module was using C(mount) and C(umount) commands only,
+  requiring root access. This is no longer needed with the introduction of 7zip
+  for extraction.
 '''
 
 EXAMPLES = r'''
@@ -95,7 +103,7 @@ def main():
             dest=dict(type='path', required=True),
             files=dict(type='list', required=True),
             force=dict(type='bool', default=True, aliases=['thirsty']),
-            executable=dict(type='path', default='7z'),
+            executable=dict(type='path'),  # No default on purpose
         ),
         supports_check_mode=True,
     )
@@ -111,23 +119,28 @@ def main():
         image=image,
     )
 
+    # We want to know if the user provided it or not, so we set default here
+    if executable is None:
+        executable = '7z'
+
     binary = module.get_bin_path(executable, None)
 
-    if not binary:
-        module.fail_json(msg='Executable "%s" is not found on the system' % executable)
+    # When executable was provided and binary not found, warn user !
+    if module.params['executable'] is not None and not binary:
+        module.warn("Executable '%s' is not found on the system, trying to mount ISO instead." % executable)
 
     if not os.path.exists(dest):
-        module.fail_json(msg='Directory "%s" does not exist' % dest)
+        module.fail_json(msg="Directory '%s' does not exist" % dest)
 
     if not os.path.exists(os.path.dirname(image)):
-        module.fail_json(msg='ISO image "%s" does not exist' % image)
+        module.fail_json(msg="ISO image '%s' does not exist" % image)
 
     result['files'] = []
     extract_files = list(files)
 
     if not force:
         # Check if we have to process any files based on existence
-        for i, f in enumerate(files):
+        for f in files:
             dest_file = os.path.join(dest, os.path.basename(f))
             if os.path.exists(dest_file):
                 result['files'].append(dict(
@@ -135,10 +148,19 @@ def main():
                     dest=dest_file,
                     src=f,
                 ))
-                del(extract_files[i])
+                extract_files.remove(f)
+
+    if not extract_files:
+        module.exit_json(**result)
 
     tmp_dir = tempfile.mkdtemp()
-    cmd = '%s x "%s" -o"%s" %s' % (binary, image, tmp_dir, ' '.join([quote(f) for f in files]))
+
+    # Use 7zip when we have a binary, otherwise try to mount
+    if binary:
+        cmd = '%s x "%s" -o"%s" %s' % (binary, image, tmp_dir, ' '.join([quote(f) for f in extract_files]))
+    else:
+        cmd = 'mount -o loop,ro "%s" "%s"' % (image, tmp_dir)
+
     rc, out, err = module.run_command(cmd)
     if rc != 0:
         result.update(dict(
@@ -148,11 +170,18 @@ def main():
             stdout=out,
         ))
         shutil.rmtree(tmp_dir)
-        module.fail_json(msg='Failed to extract from ISO image "%s" to "%s"' % (image, tmp_dir), **result)
+
+        if binary:
+            module.fail_json(msg="Failed to extract from ISO image '%s' to '%s'" % (image, tmp_dir), **result)
+        else:
+            module.fail_json(msg="Failed to mount ISO image '%s' to '%s', and we could not find executable '%s'." % (image, tmp_dir, executable), **result)
 
     try:
         for f in extract_files:
             tmp_src = os.path.join(tmp_dir, f)
+            if not os.path.exists(tmp_src):
+                module.fail_json(msg="Failed to extract '%s' from ISO image" % f, **result)
+
             src_checksum = module.sha1(tmp_src)
 
             dest_file = os.path.join(dest, os.path.basename(f))
@@ -174,6 +203,9 @@ def main():
 
                 result['changed'] = True
     finally:
+        if not binary:
+            module.run_command('umount "%s"' % tmp_dir)
+
         shutil.rmtree(tmp_dir)
 
     module.exit_json(**result)
