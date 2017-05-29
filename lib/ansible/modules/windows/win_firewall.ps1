1c697ceb895a49e8a2baf9f5d7269db34c63fc84
Dag Wieers <dag@wieers.com>
2017-05-29 12:33:55 +0200

    win_firewall: check-mode support, integration tests
    This PR includes:
    - Check-mode implementation
    - Documentation improvements
    - Ensure module output is consistent (no matter what profiles are provided)
    - Fixed indentation
    - Cosmetic changes
    - Integration tests

--- lib/ansible/modules/windows/win_firewall.ps1
+++ lib/ansible/modules/windows/win_firewall.ps1
@@ -25,8 +25,8 @@ $firewall_profiles = @('Domain', 'Private', 'Public')
 $params = Parse-Args $args -supports_check_mode $true
 $check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
 
+$profiles = Get-AnsibleParam -obj $params -name "profiles" -type "list" -default @("Domain", "Private", "Public")
 $state = Get-AnsibleParam -obj $params -name "state" -type "str" -failifempty $true -validateset 'disabled','enabled'
-$profiles = Get-AnsibleParam -obj $params -name "profiles" -type "list" -default "Domain","Private","Public"
 
 $result = @{
     changed = $false
@@ -34,39 +34,43 @@ $result = @{
     state = $state
 }
 
+if ($PSVersionTable.PSVersion -lt [Version]"5.0") {
+    Fail-Json $result "win_firewall requires Windows Management Framework 5 or higher."
+}
+
 Try {
 
-  ForEach ($profile in $firewall_profiles) {
+    ForEach ($profile in $firewall_profiles) {
 
-    $currentstate = (Get-NetFirewallProfile -Name $profile).Enabled
-    $result.$profile = @{
-        enabled = ($currentstate -eq 1)
-        considered = ($profiles -contains $profile)
-        currentstate = $currentstate
+        $currentstate = (Get-NetFirewallProfile -Name $profile).Enabled
+        $result.$profile = @{
+            enabled = ($currentstate -eq 1)
+            considered = ($profiles -contains $profile)
+            currentstate = $currentstate
+        }
+
+        if ($profiles -notcontains $profile) {
+            continue
+        }
+
+        if ($state -eq 'enabled') {
+
+            if ($currentstate -eq $false) {
+                Set-NetFirewallProfile -name $profile -Enabled true -WhatIf:$check_mode
+                $result.changed = $true
+                $result.$profile.enabled = $true
+            }
+
+        } else {
+
+            if ($currentstate -eq $true) {
+                Set-NetFirewallProfile -name $profile -Enabled false -WhatIf:$check_mode
+                $result.changed = $true
+                $result.$profile.enabled = $false
+            }
+
+        }
     }
-
-    if ($profiles -notcontains $profile) {
-        continue
-    }
-
-    if ($state -eq 'enabled') {
-
-      if ($currentstate -eq $false) {
-          Set-NetFirewallProfile -name $profile -Enabled true -WhatIf:$check_mode
-          $result.changed = $true
-          $result.$profile.enabled = $true
-      }
-
-    } else {
-
-      if ($currentstate -eq $true) {
-          Set-NetFirewallProfile -name $profile -Enabled false -WhatIf:$check_mode
-          $result.changed = $true
-          $result.$profile.enabled = $false
-      }
-
-    }
-  }
 } Catch {
     Fail-Json $result "an error occurred when attempting to change firewall status for profile $profile $($_.Exception.Message)"
 }
