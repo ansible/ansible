3a6490f97bef77972b8909bbb94724593a0bfad1
Bob Tanner <tanner@real-time.com>
2017-05-30 02:16:24 -0500

    Fixes https://github.com/ansible/ansible/issues/25018

--- lib/ansible/modules/windows/win_chocolatey.ps1
+++ lib/ansible/modules/windows/win_chocolatey.ps1
@@ -100,6 +100,11 @@ Function Choco-IsInstalled
         [string]$package
     )
 
+   if ($package -like "all")
+   {
+     return $true
+   } 
+
     $cmd = "$executable list --local-only --exact $package"
     $output = invoke-expression $cmd
 
