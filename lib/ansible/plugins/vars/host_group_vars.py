0f969b100446f515f0eb6c37be6b8c6459ba82f0
Adrian Likins <alikins@redhat.com>
2017-05-30 19:11:06 -0400

    ignore combine_vars from host_group_vars
    just a temp dict it is manipulating

--- lib/ansible/plugins/vars/host_group_vars.py
+++ lib/ansible/plugins/vars/host_group_vars.py
@@ -84,7 +84,7 @@ class VarsModule(BaseVarsPlugin):
                 for found in found_files:
                     new_data = loader.load_from_file(found, cache=True, unsafe=True)
                     if new_data:  # ignore empty files
-                        data = combine_vars(data, new_data, name_b='host_group_vars_new_data')
+                        data = combine_vars(data, new_data, name_b='_host_group_vars_new_data')
 
             except Exception as e:
                 raise AnsibleParserError(to_text(e))
