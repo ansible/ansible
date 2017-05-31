91c12055a032d820730019be9d57b687c0b65423
Dag Wieers <dag@wieers.com>
2017-05-31 12:47:57 +0200

    plugins/lookup/: PEP8 compliancy
    - Make PEP8 compliant

--- lib/ansible/plugins/lookup/dict.py
+++ lib/ansible/plugins/lookup/dict.py
@@ -22,6 +22,7 @@ import collections
 from ansible.errors import AnsibleError
 from ansible.plugins.lookup import LookupBase
 
+
 class LookupModule(LookupBase):
 
     def run(self, terms, variables=None, **kwargs):
