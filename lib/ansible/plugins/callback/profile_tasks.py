ansible/ansible

commit 62a434141e7f96299f820af3ca08fe86231300ed

ensure display is terminal sensistive
fixes #18248 by using display width intead of hardcoded length

lib/ansible/plugins/callback/profile_tasks.py
@@ -136,7 +136,7 @@ class CallbackModule(CallbackBase):
 
         # Print the timings
         for uuid, result in results:
-            msg=u"{0:-<70}{1:->9}".format(result['name'] + u' ',u' {0:.02f}s'.format(result['time']))
+            msg=u"{0:-<{2}}{1:->9}".format(result['name'] + u' ',u' {0:.02f}s'.format(result['time']), self.columns - 9)
             if 'path' in result:
-                msg += u"\n{0:-<79}".format(result['path'] + u' ')
+                msg += u"\n{0:-<{1}}".format(result['path'] + u' '.format(self.columns))
             self._display.display(msg)

commit 7cde71881bf7e86544ead06ef4d265a1064d8144

Fix pylint error and .format() err in profile_task
introduced by 62a434141e7f96299f820af3ca08fe86231300ed

lib/ansible/plugins/callback/profile_tasks.py
@@ -138,5 +138,6 @@ class CallbackModule(CallbackBase):
         for uuid, result in results:
             msg=u"{0:-<{2}}{1:->9}".format(result['name'] + u' ',u' {0:.02f}s'.format(result['time']), self.columns - 9)
             if 'path' in result:
-                msg += u"\n{0:-<{1}}".format(result['path'] + u' '.format(self.columns))
+                msg += u"\n{0:-<{1}}".format(result['path'],
+                                             u'{0}'.format(self.columns))
             self._display.display(msg)