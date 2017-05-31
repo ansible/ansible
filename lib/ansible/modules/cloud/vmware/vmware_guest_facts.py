9d09ec37a0d8d86f1d5addfc7d34eead420ddf2e
James Tanner <tanner.jc@gmail.com>
2017-05-30 23:00:55 -0400

    Refactor PyVmomiHelper into a base class within module_utils/vmware.py
    * clean up pep8 formatting
    * fix various bugs in vm search
    * do not munge the folder path given by the user
    * fallback to a broad search and narrow down by paths

--- lib/ansible/modules/cloud/vmware/vmware_guest_facts.py
+++ lib/ansible/modules/cloud/vmware/vmware_guest_facts.py
@@ -83,87 +83,11 @@ instance:
 """
 
 import os
-import time
 
 # import module snippets
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.pycompat24 import get_exception
-from ansible.module_utils.six import iteritems
-from ansible.module_utils.vmware import connect_to_api, find_vm_by_id, gather_vm_facts
-
-try:
-    import json
-except ImportError:
-    import simplejson as json
-
-HAS_PYVMOMI = False
-try:
-    import pyVmomi
-    from pyVmomi import vim
-
-    HAS_PYVMOMI = True
-except ImportError:
-    pass
-
-
-class PyVmomiHelper(object):
-    def __init__(self, module):
-        if not HAS_PYVMOMI:
-            module.fail_json(msg='pyvmomi module required')
-
-        self.module = module
-        self.params = module.params
-        self.content = connect_to_api(self.module)
-
-    def getvm(self, name=None, uuid=None, folder=None):
-        vm = None
-
-        if uuid:
-            vm = find_vm_by_id(self.content, vm_id=uuid, vm_id_type="uuid")
-        elif folder:
-            # Build the absolute folder path to pass into the search method
-            if not self.params['folder'].startswith('/'):
-                self.module.fail_json(msg="Folder %(folder)s needs to be an absolute path, starting with '/'." % self.params)
-            searchpath = '%(datacenter)s%(folder)s' % self.params
-
-            # get all objects for this path ...
-            f_obj = self.content.searchIndex.FindByInventoryPath(searchpath)
-            if f_obj:
-                if isinstance(f_obj, vim.Datacenter):
-                    f_obj = f_obj.vmFolder
-                for c_obj in f_obj.childEntity:
-                    if not isinstance(c_obj, vim.VirtualMachine):
-                        continue
-                    if c_obj.name == name:
-                        vm = c_obj
-                        if self.params['name_match'] == 'first':
-                            break
-
-        return vm
-
-    def gather_facts(self, vm):
-        return gather_vm_facts(self.content, vm)
-
-
-def get_obj(content, vimtype, name):
-    """
-    Return an object by name, if name is None the
-    first found object is returned
-    """
-    obj = None
-    container = content.viewManager.CreateContainerView(
-        content.rootFolder, vimtype, True)
-    for c in container.view:
-        if name:
-            if c.name == name:
-                obj = c
-                break
-        else:
-            obj = c
-            break
-
-    container.Destroy()
-    return obj
+from ansible.module_utils.vmware import PyVmomiHelper
 
 
 def main():
@@ -190,15 +114,17 @@ def main():
         ),
     )
 
-    # Prepend /vm if it was missing from the folder path, also strip trailing slashes
-    if not module.params['folder'].startswith('/vm') and module.params['folder'].startswith('/'):
-        module.params['folder'] = '/vm%(folder)s' % module.params
-    module.params['folder'] = module.params['folder'].rstrip('/')
-
     pyv = PyVmomiHelper(module)
+
+    # Build searchpath for FindByInventoryPath
+    searchpath = PyVmomiHelper.construct_vm_searchpath(
+        module.params['datacenter'],
+        module.params['folder']
+    )
+
     # Check if the VM exists before continuing
     vm = pyv.getvm(name=module.params['name'],
-                   folder=module.params['folder'],
+                   searchpath=searchpath,
                    uuid=module.params['uuid'])
 
     # VM already exists
@@ -209,7 +135,9 @@ def main():
             e = get_exception()
             module.fail_json(msg="Fact gather failed with exception %s" % e)
     else:
-        module.fail_json(msg="Unable to gather facts for non-existing VM %(name)s" % module.params)
+        msg = "Unable to gather facts for non-existing VM"
+        msg += " %s in folder %s" % (module.params['name'], searchpath)
+        module.fail_json(msg=msg)
 
 if __name__ == '__main__':
     main()
