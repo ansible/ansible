33dfb779ca82f6b1acd01bf72ed2126aacfefaa7
Adrian Likins <alikins@redhat.com>
2017-06-01 17:57:22 -0400

    dont show the repr for now

--- lib/ansible/plugins/strategy/linear.py
+++ lib/ansible/plugins/strategy/linear.py
@@ -252,7 +252,7 @@ class StrategyModule(StrategyBase):
                         # NOTE: add callback hook?
                         # display the details of which vars came from where at vvvvv (where we use TrackingDict for all_vars)
                         if display.verbosity > 4:
-                            display.vvvvv(repr(task_vars))
+                            # display.vvvvv(repr(task_vars))
                             display.vvvvv('TASK VARS:\n%s' % json.dumps(task_vars.as_dict(), indent=4, ensure_ascii=False, sort_keys=True))
 
                         self.add_tqm_variables(task_vars, play=iterator._play)
