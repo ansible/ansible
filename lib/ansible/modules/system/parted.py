5512e7a27c29acac58c0c120dbaf2f7311bf4ee3
Fabrizio Colonna <colofabrix@tin.it>
2017-06-01 20:27:27 +0100

    Fixes parted i18n issues (#24991)
    * Fixed issue 24969
    
    * Used C locale and global env update.
    
    (cherry picked from commit f364d7af8e6134d686e54d65972dc0461e1e3a4a)

--- lib/ansible/modules/system/parted.py
+++ lib/ansible/modules/system/parted.py
@@ -585,6 +585,7 @@ def main():
         },
         supports_check_mode=True,
     )
+    module.run_command_environ_update = {'LANG': 'C', 'LC_ALL': 'C', 'LC_MESSAGES': 'C'}
 
     # Data extraction
     device      = module.params['device']
