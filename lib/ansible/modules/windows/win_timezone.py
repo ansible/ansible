744c682c1b9c1e913fe756790ed338c05a30b553
Dag Wieers <dag@wieers.com>
2017-06-02 02:25:15 +0200

    win_timezone: Add diff support, integration tests
    This PR includes:
    
    - Diff support
    - Returns both previous_timezone and timezone
    - Adds integration tests
    - More examples
    - Improved documentation

--- lib/ansible/modules/windows/win_timezone.py
+++ lib/ansible/modules/windows/win_timezone.py
@@ -29,24 +29,46 @@ ANSIBLE_METADATA = {'metadata_version': '1.0',
 DOCUMENTATION = r'''
 ---
 module: win_timezone
-version_added: "2.1"
+version_added: '2.1'
 short_description: Sets Windows machine timezone
 description:
-    - Sets machine time to the specified timezone, the module will check if the provided timezone is supported on the machine.
+- Sets machine time to the specified timezone.
 options:
   timezone:
     description:
-      - Timezone to set to.  Example Central Standard Time
+    - Timezone to set to. Example Central Standard Time
     required: true
-
+notes:
+- The module will check if the provided timezone is supported on the machine.
+- A list of possible timezones is available from
+  U(https://msdn.microsoft.com/en-us/library/ms912391.aspx)
 author: Phil Schwartz
 '''
 
 
 EXAMPLES = r'''
-  # Set machine's timezone to Central Standard Time
+- name: Set timezone to 'Romance Standard Time' (GMT+01:00)
   win_timezone:
-    timezone: "Central Standard Time"
+    timezone: Romance Standard Time
+
+- name: Set timezone to 'GMT Standard Time' (GMT)
+  win_timezone:
+    timezone: GMT Standard Time
+
+- name: Set timezone to 'Central Standard Time' (GMT-06:00)
+  win_timezone:
+    timezone: Central Standard Time
 '''
 
-RETURN = r'''# '''
+RETURN = r'''
+previous_timezone:
+    description: The previous timezone if it was changed, otherwise the existing timezone
+    returned: success
+    type: string
+    sample: Central Standard Time
+timezone:
+    description: The current timezone (possibly changed)
+    returned: success
+    type: string
+    sample: Central Standard Time
+'''
