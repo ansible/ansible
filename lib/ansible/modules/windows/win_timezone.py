3382090d8f570f0b20c3b3569facf5a0bb2b17b9
Dag Wieers <dag@wieers.com>
2017-06-02 11:24:32 +0200

    Merged the changes from jborean93's PR

--- lib/ansible/modules/windows/win_timezone.py
+++ lib/ansible/modules/windows/win_timezone.py
@@ -25,7 +25,6 @@ ANSIBLE_METADATA = {'metadata_version': '1.0',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
-
 DOCUMENTATION = r'''
 ---
 module: win_timezone
@@ -40,11 +39,13 @@ options:
     required: true
 notes:
 - The module will check if the provided timezone is supported on the machine.
-- A list of possible timezones is available from
+- A list of possible timezones is available from C(tzutil.exe /l) and from
   U(https://msdn.microsoft.com/en-us/library/ms912391.aspx)
+- If running on Server 2008 the hotfix
+  U(https://support.microsoft.com/en-us/help/2556308/tzutil-command-line-tool-is-added-to-windows-vista-and-to-windows-server-2008)
+  needs to be installed to be able to run this module.
 author: Phil Schwartz
 '''
-
 
 EXAMPLES = r'''
 - name: Set timezone to 'Romance Standard Time' (GMT+01:00)
