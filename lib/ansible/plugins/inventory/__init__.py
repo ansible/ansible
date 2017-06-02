e8f7753be91c1a3813fd654f4ec6a68d7ff08d8d
Matt Martz <matt@sivel.net>
2017-06-02 12:41:02 -0500

    Python3 fix for hashing in inventory plugins

--- lib/ansible/plugins/inventory/__init__.py
+++ lib/ansible/plugins/inventory/__init__.py
@@ -61,11 +61,11 @@ class BaseInventoryPlugin(object):
         ''' create predictable unique prefix for plugin/inventory '''
 
         m = hashlib.sha1()
-        m.update(self.NAME)
+        m.update(to_bytes(self.NAME))
         d1 = m.hexdigest()
 
         n = hashlib.sha1()
-        n.update(path)
+        n.update(to_bytes(path))
         d2 = n.hexdigest()
 
         return 's_'.join([d1[:5], d2[:5]])
