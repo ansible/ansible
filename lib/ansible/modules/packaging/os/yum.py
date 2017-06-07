143e6376fe9ffc35086decb846eec0b0041d1801
ReSearchITEng <ReSearchITEng@users.noreply.github.com>
2017-06-07 12:42:34 +0300

    documentation to reflect cases like #24059
    #24059

--- lib/ansible/modules/packaging/os/yum.py
+++ lib/ansible/modules/packaging/os/yum.py
@@ -44,7 +44,7 @@ options:
     aliases: [ 'pkg' ]
   exclude:
     description:
-      - "Package name(s) to exclude when state=present, or latest"
+      - "Package name(s) to exclude when state=present, or latest; Note: updates to ansible binary while a playbook is running can cause issues. It is advisable to exclude ansible when performing os updates from ansible."
     required: false
     version_added: "2.0"
     default: null
@@ -193,6 +193,7 @@ EXAMPLES = '''
   yum:
     name: '*'
     state: latest
+    exclude: 'ansible'
 
 - name: install the nginx rpm from a remote repo
   yum:
