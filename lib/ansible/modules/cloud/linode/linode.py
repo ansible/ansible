45d5183347ff86042f37ed4dd9840d5326181932
Adrian Likins <alikins@redhat.com>
2017-06-08 17:31:32 -0400

    linode 'stopped' state doesnt need name either

--- lib/ansible/modules/cloud/linode/linode.py
+++ lib/ansible/modules/cloud/linode/linode.py
@@ -538,9 +538,8 @@ def linodeServers(module, api, state, name, alert_bwin_enabled, alert_bwin_thres
             instances.append(instance)
 
     elif state in ('stopped'):
-        for arg in (name, linode_id):
-            if not arg:
-                module.fail_json(msg='%s is required for active state' % arg)
+        if not linode_id:
+            module.fail_json(msg='linode_id is required for stopped state')
 
         if not servers:
             module.fail_json(msg = 'Server (lid: %s) not found' % (linode_id))
@@ -561,9 +560,6 @@ def linodeServers(module, api, state, name, alert_bwin_enabled, alert_bwin_thres
     elif state in ('restarted'):
         if not linode_id:
             module.fail_json(msg='linode_id is required for restarted state')
-
-        if not name:
-            module.fail_json(msg='name is required for restarted state')
 
         if not servers:
             module.fail_json(msg = 'Server (lid: %s) not found' % (linode_id))
