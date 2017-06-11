2ce69cc00191e654909972a8c662ff62bf3562b7
Jacco de Leeuw <github@jacco2.dds.nl>
2017-06-11 18:05:10 +0200

    For roles installed as .tar.gz file, use the filename as the rolename.

--- lib/ansible/playbook/role/requirement.py
+++ lib/ansible/playbook/role/requirement.py
@@ -61,8 +61,12 @@ class RoleRequirement(RoleDefinition):
     def repo_url_to_role_name(repo_url):
         # gets the role name out of a repo like
         # http://git.example.com/repos/repo.git" => "repo"
+        # "/tmp/hello-test.tar.gz"                => "hello-test"
 
         if '://' not in repo_url and '@' not in repo_url:
+            trailing_path = repo_url.split('/')[-1]
+            if trailing_path.endswith('.tar.gz'):
+              return trailing_path[:-7]
             return repo_url
         trailing_path = repo_url.split('/')[-1]
         if trailing_path.endswith('.git'):
