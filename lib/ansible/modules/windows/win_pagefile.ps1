8e88c9d9994ce88022dd848a80e0d2e0831cbdf5
LiranNis <lirannis@gmail.com>
2017-06-12 17:49:13 +0300

    Adds win_pagefile module

---
+++ lib/ansible/modules/windows/win_pagefile.ps1
@@ -0,0 +1,147 @@
+#!powershell
+# This file is part of Ansible
+#
+# Copyright 2017, Liran Nisanov <lirannis@gmail.com>
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# WANT_JSON
+# POWERSHELL_COMMON
+
+########
+
+Function Remove-Pagefile($path) 
+{
+    Get-WmiObject Win32_PageFileSetting | WHERE { $_.Name -eq $path } | Remove-WmiObject
+}
+
+Function Get-Pagefile($path)
+{
+    Get-WmiObject Win32_PageFileSetting | WHERE { $_.Name -eq $path }
+}
+
+########
+
+$params = Parse-Args $args;
+
+$result = @{ 
+    changed = $false
+}
+
+$automatic = Get-AnsibleParam -obj $params -name "automatic" -type "bool"
+$drive = Get-AnsibleParam -obj $params -name "drive" -type "str"
+$fullPath = $drive + ":\pagefile.sys"
+$initialSize = Get-AnsibleParam -obj $params -name "initial_size" -type "int"
+$maximumSize = Get-AnsibleParam -obj $params -name "maximum_size" -type "int"
+$override =  Get-AnsibleParam -obj $params -name "override" -type "bool" -default $true
+$removeAll = Get-AnsibleParam -obj $params -name "remove_all" -type "bool" -default $false
+$state = Get-AnsibleParam -obj $params -name "state" -type "str" -default "query" -validateset "present","absent","query"
+$systemManaged = Get-AnsibleParam -obj $params -name "system_managed" -type "bool" -default $false
+
+$ErrorActionPreference = "Stop"
+
+if ($removeAll) {
+    Get-WmiObject Win32_PageFileSetting | Remove-WmiObject | Out-Null
+}
+
+if ($automatic -ne $null) {
+    # change autmoatic managed pagefile 
+    try {
+        $computerSystem = Get-WmiObject -Class win32_computersystem -EnableAllPrivileges
+    
+        if ($computerSystem.AutomaticManagedPagefile -ne $automatic) {
+            $computerSystem.AutomaticManagedPagefile = $automatic
+            $computerSystem.Put() | Out-Null
+            $result.changed = $true
+        }
+    } catch {
+        Fail-Json $result "Failed to set AutomaticManagedPagefile $_.Exception.Message"
+    }
+}
+
+if ($state -eq "absent") {
+    # Remove pagefile
+    if ((Get-Pagefile $fullPath) -ne $null)
+    {
+        try {
+            Remove-Pagefile $fullPath
+            $result.changed = $true
+        } catch {
+            Fail-Json $result "Failed to remove pagefile $_.Exception.Message"
+        }
+    }
+} elseif ($state -eq "present") {   
+    # Remove current pagefile
+    if ($override) {
+        if ((Get-Pagefile $fullPath) -ne $null)
+        {
+            try {
+                Remove-Pagefile $fullPath
+                $result.changed = $true
+            } catch {
+                Fail-Json $result "Failed to remove current pagefile $_.Exception.Message"
+            }
+        }
+    }
+
+    # Make sure drive is accessible
+    if (!(Test-Path "${drive}:")) {
+        Fail-Json $result "Unable to access '${drive}:' drive"
+    }
+
+    # Set pagefile
+    try {
+        if ((Get-Pagefile $fullPath) -eq $null) {
+            $pagefile = Set-WmiInstance -Class Win32_PageFileSetting -Arguments @{name = $fullPath; InitialSize = 0; MaximumSize = 0}
+            if (!$systemManaged) {
+                $pagefile.InitialSize = $initialSize
+                $pagefile.MaximumSize = $maximumSize
+                $pagefile.Put() | out-null
+            }
+            $result.changed = $true
+        }
+    } catch {
+        Fail-Json $result "Failed to set pagefile $_.Exception.Message"
+    }
+} elseif ($state -eq "query") {
+    try {
+
+        $result.pagefiles = @()
+
+        if ($drive -eq $null) {
+            $pagefiles = Get-WmiObject Win32_PageFileSetting
+        } else {
+            $pagefiles = Get-Pagefile $fullPath
+        }
+
+        # Get all pagefiles
+        foreach ($currentPagefile in $pagefiles) {
+            $currentPagefileObject = @{
+                name = $currentPagefile.Name
+                initial_size = $currentPagefile.InitialSize
+                maximum_size = $currentPagefile.MaximumSize
+                caption = $currentPagefile.Caption
+                description = $currentPagefile.Description
+            }
+
+            $result.pagefiles += $currentPagefileObject
+        }
+
+        # Get automatic managed pagefile state
+        $result.automatic_managed_pagefiles = (Get-WmiObject -Class win32_computersystem).AutomaticManagedPagefile
+    } catch {
+        Fail-Json $result "Failed to query current pagefiles $_.Exception.Message"
+    }
+}
+Exit-Json $result
---
+++ lib/ansible/modules/windows/win_pagefile.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2017, Liran Nisanov <lirannis@gmail.com>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# this is a windows documentation stub.  actual code lives in the .ps1
+# file of the same name
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+
+DOCUMENTATION = r'''
+---
+module: win_pagefile
+version_added: "2.3.1"
+short_description: Query or change pagefile configuration
+description:
+    - Query current pagefile configuration
+    - Enable/Disable AutomaticManagedPagefile
+    - Create new or override pagefile configuration
+options:
+  drive:
+    description:
+      - The drive of the pagefile.
+    required: false
+  initial_size:
+    description:
+      - The initial size of the pagefile.
+  maximum_size:
+    description:
+      - The maximum size of the pagefile.
+  override:
+    description:
+      - Override the current pagefile on the drive.
+    choices:
+      - true
+      - false
+    default: true
+  system_managed:
+    description:
+      - Configures current pagefile to be managed by the system.
+    choices:
+      - true
+      - false
+    default: false
+  automatic:
+    description:
+      - Configures AutomaticManagedPagefile for the entire system.
+    choices:
+      - true
+      - false
+  remove_all:
+    description:
+      - Remove all pagefiles in the system, not including automatic managed.
+    choices:
+      - true
+      - false
+    default: false
+  state:
+    description:
+      - State of the pagefile.
+    choices:
+      - present
+      - absent
+      - query
+    default: query
+notes:
+- There is difference between automatic managed pagefiles that configured once for the entire system and system managed pagefile that configured per pagefile.
+- InitialSize 0 and MaximumSize 0 means the pagefile is managed by the system.
+- Value out of range exception may be caused by several different issues, two common problems - No such drive, Pagefile size is too small.
+- Setting a pagefile when AutomaticManagedPagefile is on will disable the AutomaticManagedPagefile.
+author: "Liran Nisanov (@LiranNis)"
+'''
+
+EXAMPLES = r'''
+- name: Query pagefiles configuration
+  win_pagefile:
+
+- name: Query C pagefile
+  win_pagefile:
+    drive=c
+
+- name: Set C pagefile, don't override if exists
+  win_pagefile:
+    drive=C
+    initial_size=1024
+    maximum_size=1024
+    override=false
+    state=present
+
+- name: Set C pagefile, override if exists
+  win_pagefile:
+    drive=C
+    initial_size=1024
+    maximum_size=1024
+    state=present
+
+- name: Remove C pagefile
+  win_pagefile:
+    drive=c
+    state=absent
+
+- name: Remove all current pagefiles, enable AutomaticManagedPagefile and query at the end
+  win_pagefile:
+    remove_all=true
+    automatic=true
+
+- name: Remove all pagefiles disable AutomaticManagedPagefile and set C pagefile
+  win_pagefile:
+    drive=c
+    initial_size=2048
+    maximum_size=2048
+    remove_all=true
+    automatic=false
+    state=present
+
+- name: Set D pagefile, override if exists
+  win_pagefile:
+    drive=d
+    initial_size=1024
+    maximum_size=1024
+    state=present
+'''
+
+RETURN = r'''
+automatic_managed_pagefiles:
+    description: Whether the pagefiles is automatically managed
+    returned: When state is query.   
+    type: boolean
+    sample: true
+pagefiles:
+    description: Pagefiles on the system.
+    returned: When state is query.
+    type: JSON that contains caption, description, initial_size, maximum_size and name.
+    sample: [{"caption": "c:\\ 'pagefile.sys'", "description": "'pagefile.sys' @ c:\\", "initial_size": 2048, "maximum_size": 2048, "name": "c:\\pagefile.sys"}, {"caption": "d:\\ 'pagefile.sys'", "description": "'pagefile.sys' @ d:\\", "initial_size": 1024, "maximum_size": 1024, "name": "d:\\pagefile.sys"}]
+
+'''
