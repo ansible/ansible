53e66364e6c5060e34beecdc88929b40b327cae3
s-hertel <shertel@redhat.com>
2017-06-12 08:47:44 -0400

    Allow successful deletion

--- lib/ansible/modules/cloud/amazon/s3.py
+++ lib/ansible/modules/cloud/amazon/s3.py
@@ -359,18 +359,23 @@ def delete_bucket(module, s3, bucket):
             return False
         elif e.status == 403:
             # bucket appears to exist but user doesn't have list bucket permission; may still be able to delete bucket
-            try:
-                bucket.delete()
-            except s3.provider.storage_response_error as e:
-                if e.status == 403:
-                    module.exit_json(msg="Unable to complete DELETE operation. Check you have have s3:DeleteBucket "
-                                     "permission. Error: {0}.".format(e.message),
-                                     exception=traceback.format_exc())
-                elif e.status == 409:
-                    module.exit_json(msg="Unable to complete DELETE operation. It appears there are contents in the "
-                                     "bucket that you don't have permission to delete. Error: {0}.".format(e.message),
-                                     exception=traceback.format_exc())
-        module.fail_json(msg=str(e), exception=traceback.format_exc())
+            pass
+        else:
+            module.fail_json(msg=str(e), exception=traceback.format_exc())
+    try:
+        bucket.delete()
+        return True
+    except s3.provider.storage_response_error as e:
+        if e.status == 403:
+            module.exit_json(msg="Unable to complete DELETE operation. Check you have have s3:DeleteBucket "
+                             "permission. Error: {0}.".format(e.message),
+                             exception=traceback.format_exc())
+        elif e.status == 409:
+            module.exit_json(msg="Unable to complete DELETE operation. It appears there are contents in the "
+                             "bucket that you don't have permission to delete. Error: {0}.".format(e.message),
+                             exception=traceback.format_exc())
+        else:
+            module.fail_json(msg=str(e), exception=traceback.format_exc())
 
 def delete_key(module, s3, bucket, obj, validate=True):
     if module.check_mode:
