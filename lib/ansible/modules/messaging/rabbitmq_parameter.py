4eef450b68b7689f0f27f8d30ce680c0c829a5b0
Wayne Pascoe <wpascoe@redowl.com>
2017-06-12 14:14:02 +0100

    Added version_added for extra_ctl_paths

--- lib/ansible/modules/messaging/rabbitmq_parameter.py
+++ lib/ansible/modules/messaging/rabbitmq_parameter.py
@@ -64,6 +64,7 @@ options:
       - Only needed when running RabbitMQ as user other than root / rabbitmq
     required: false
     default: ()
+    version_added: "2.4"
   state:
     description:
       - Specify if user is to be added or removed
