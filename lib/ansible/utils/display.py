498aea8acc2f9eb64228f505547ceedaae4f2687
Brian Coca <bcoca@users.noreply.github.com>
2017-06-13 09:27:44 -0400

    Revert "Add log_only to debug messages (#25545)"
    This reverts commit 9e8cc26720f5e8a18a6b94aa88f582a9e291714b.

--- lib/ansible/utils/display.py
+++ lib/ansible/utils/display.py
@@ -174,9 +174,9 @@ class Display:
     def vvvvvv(self, msg, host=None):
         return self.verbose(msg, host=host, caplevel=5)
 
-    def debug(self, msg, log_only=False):
+    def debug(self, msg):
         if C.DEFAULT_DEBUG:
-            self.display("%6d %0.5f: %s" % (os.getpid(), time.time(), msg), color=C.COLOR_DEBUG, log_only=log_only)
+            self.display("%6d %0.5f: %s" % (os.getpid(), time.time(), msg), color=C.COLOR_DEBUG)
 
     def verbose(self, msg, host=None, caplevel=2):
         if self.verbosity > caplevel:
