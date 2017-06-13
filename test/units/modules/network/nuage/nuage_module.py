c27dc690ed03b36e88a8bc197fcbc728dd18f3fd
Philippe Dellaert <philippe@dellaert.org>
2017-06-13 11:00:07 -0700

    Cleanup of imports

--- test/units/modules/network/nuage/nuage_module.py
+++ test/units/modules/network/nuage/nuage_module.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
-
 import json
 from ansible.compat.tests import unittest
 from ansible.compat.tests.mock import patch
--- test/units/modules/network/nuage/test_nuage_vspk.py
+++ test/units/modules/network/nuage/test_nuage_vspk.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
-import __builtin__
 from ansible.compat.tests.mock import patch
 from ansible.modules.network.nuage import nuage_vspk
 from vspk import v5_0 as vsdk
