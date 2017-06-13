fb100c0ace673fa3dfe0d787d37fe5158e3f1021
Chris Gadd <gaddman@email.com>
2017-06-14 11:44:06 +1200

    Revert "Updating ufw.py to support comments"
    This reverts commit 54a42de97c77004d4755543bf310f0ec6e1b4d14.

--- lib/ansible/modules/system/ufw.py
+++ lib/ansible/modules/system/ufw.py
@@ -126,10 +126,6 @@ options:
       - Apply the rule to routed/forwarded packets.
     required: false
     choices: ['yes', 'no']
-  comment:
-    description:
-      - Add a comment to the rule. Requires UFW version >=0.35.
-    required: false
 '''
 
 EXAMPLES = '''
@@ -204,7 +200,6 @@ EXAMPLES = '''
     proto: udp
     src: 1.2.3.4
     port: 514
-    comment: "Block syslog"
 
 # Allow incoming access to eth0 from 1.2.3.5 port 5469 to 1.2.3.4 port 5469
 - ufw:
@@ -255,8 +250,7 @@ def main():
             to_ip     = dict(default='any', aliases=['dest', 'to']),
             to_port   = dict(default=None,  aliases=['port']),
             proto     = dict(default=None,  aliases=['protocol'], choices=['any', 'tcp', 'udp', 'ipv6', 'esp', 'ah']),
-            app       = dict(default=None,  aliases=['name']),
-            comment   = dict(default=None)
+            app       = dict(default=None,  aliases=['name'])
         ),
         supports_check_mode = True,
         mutually_exclusive = [['app', 'proto', 'logging']]
@@ -312,7 +306,7 @@ def main():
             #
             # ufw [--dry-run] [delete] [insert NUM] [route] allow|deny|reject|limit [in|out on INTERFACE] [log|log-all] \
             #     [from ADDRESS [port PORT]] [to ADDRESS [port PORT]] \
-            #     [proto protocol] [app application] [comment COMMENT]
+            #     [proto protocol] [app application]
             cmd.append([module.boolean(params['delete']), 'delete'])
             cmd.append([module.boolean(params['route']), 'route'])
             cmd.append([params['insert'], "insert %s" % params['insert']])
@@ -327,7 +321,6 @@ def main():
 
                 value = params[key]
                 cmd.append([value, template % (value)])
-            cmd.append([params['comment'], "comment '%s'" % params['comment']])
 
             execute(cmd)
 
