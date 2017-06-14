3d0c393aa710036c0b8b921865aaca500a84f49c
Rob <wimnat@users.noreply.github.com>
2017-06-14 14:10:27 +1000

    Remove required=False from argument_spec

--- lib/ansible/modules/cloud/amazon/elb_application_lb.py
+++ lib/ansible/modules/cloud/amazon/elb_application_lb.py
@@ -970,22 +970,22 @@ def main():
     argument_spec = ec2_argument_spec()
     argument_spec.update(
         dict(
-            access_logs_enabled=dict(required=False, type='bool'),
-            access_logs_s3_bucket=dict(required=False, type='str'),
-            access_logs_s3_prefix=dict(required=False, type='str'),
-            deletion_protection=dict(required=False, default=False, type='bool'),
-            idle_timeout=dict(required=False, type='int'),
-            listeners=dict(required=False, type='list'),
+            access_logs_enabled=dict(type='bool'),
+            access_logs_s3_bucket=dict(type='str'),
+            access_logs_s3_prefix=dict(type='str'),
+            deletion_protection=dict(default=False, type='bool'),
+            idle_timeout=dict(type='int'),
+            listeners=dict(type='list'),
             name=dict(required=True, type='str'),
-            purge_listeners=dict(required=False, default=True, type='bool'),
-            purge_tags=dict(required=False, default=True, type='bool'),
-            subnets=dict(required=False, type='list'),
-            security_groups=dict(required=False, type='list'),
-            scheme=dict(required=False, default='internet-facing', choices=['internet-facing', 'internal']),
-            state=dict(required=True, choices=['present', 'absent'], type='str'),
-            tags=dict(required=False, default={}, type='dict'),
-            wait_timeout=dict(required=False, type='int'),
-            wait=dict(required=False, type='bool')
+            purge_listeners=dict(default=True, type='bool'),
+            purge_tags=dict(default=True, type='bool'),
+            subnets=dict(type='list'),
+            security_groups=dict(type='list'),
+            scheme=dict(default='internet-facing', choices=['internet-facing', 'internal']),
+            state=dict(choices=['present', 'absent'], type='str'),
+            tags=dict(default={}, type='dict'),
+            wait_timeout=dict(type='int'),
+            wait=dict(type='bool')
         )
     )
 
