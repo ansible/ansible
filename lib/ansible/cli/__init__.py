9e6fc2b182ac148712cbd1776c209513432425b5
Brian Coca <brian.coca+git@gmail.com>
2017-06-15 10:45:01 -0400

    pager should not log
    fixes #25719
    
    (cherry picked from commit df2fcecd6274f3c25bf875393f8d2b3db8a10065)

--- lib/ansible/cli/__init__.py
+++ lib/ansible/cli/__init__.py
@@ -519,10 +519,10 @@ class CLI(object):
         ''' find reasonable way to display text '''
         # this is a much simpler form of what is in pydoc.py
         if not sys.stdout.isatty():
-            display.display(text)
+            display.display(text, screen_only=True)
         elif 'PAGER' in os.environ:
             if sys.platform == 'win32':
-                display.display(text)
+                display.display(text, screen_only=True)
             else:
                 self.pager_pipe(text, os.environ['PAGER'])
         else:
@@ -531,7 +531,7 @@ class CLI(object):
             if p.returncode == 0:
                 self.pager_pipe(text, 'less')
             else:
-                display.display(text)
+                display.display(text, screen_only=True)
 
     @staticmethod
     def pager_pipe(text, cmd):
