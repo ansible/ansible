c51b73d8007da00ec13c7f579d3b4ef975aa1c75
Tim Rupp <caphrim007@gmail.com>
2017-06-16 02:36:53 -0700

    Makes all unit tests look the same (#25762)
    Just a little cleanup from an earlier patch that makes f5 unit
    tests only run if the f5 sdk is installed

--- test/units/modules/network/f5/test_bigip_pool.py
+++ test/units/modules/network/f5/test_bigip_pool.py
@@ -20,15 +20,14 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import sys
-
-if sys.version_info < (2, 7):
-    from nose.plugins.skip import SkipTest
-    raise SkipTest("F5 Ansible modules require Python >= 2.7")
-
 import os
 import json
 import pytest
+import sys
+
+from nose.plugins.skip import SkipTest
+if sys.version_info < (2, 7):
+    raise SkipTest("F5 Ansible modules require Python >= 2.7")
 
 from ansible.compat.tests import unittest
 from ansible.compat.tests.mock import patch, Mock
@@ -47,9 +46,7 @@ except ImportError:
         from ansible.modules.network.f5.bigip_pool import ModuleManager
         from ansible.modules.network.f5.bigip_pool import ArgumentSpec
     except ImportError:
-        from nose.plugins.skip import SkipTest
-        raise SkipTest("%s requires the python modules for f5/bigip" % __file__)
-
+        raise SkipTest("F5 Ansible modules require the f5-sdk Python library")
 
 fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
 fixture_data = {}
--- test/units/modules/network/f5/test_bigip_virtual_address.py
+++ test/units/modules/network/f5/test_bigip_virtual_address.py
@@ -20,14 +20,13 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import os
+import json
 import sys
 
 from nose.plugins.skip import SkipTest
 if sys.version_info < (2, 7):
     raise SkipTest("F5 Ansible modules require Python >= 2.7")
-
-import os
-import json
 
 from ansible.compat.tests import unittest
 from ansible.compat.tests.mock import patch, Mock
@@ -45,7 +44,7 @@ except ImportError:
         from ansible.modules.network.f5.bigip_virtual_address import ModuleManager
         from ansible.modules.network.f5.bigip_virtual_address import ArgumentSpec
     except ImportError:
-        raise SkipTest("This test requries F5 python libraries")
+        raise SkipTest("F5 Ansible modules require the f5-sdk Python library")
 
 fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
 fixture_data = {}
