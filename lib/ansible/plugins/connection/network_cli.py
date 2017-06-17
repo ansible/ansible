c565e100b7d7eb51008b78986dfd59ca49ce9383
Peter Sprygada <psprygada@ansible.com>
2017-06-17 14:43:24 -0400

    fixes issue with SIGALARM call in network_cli
    The alarm_handler method was calling a method close_shell() that doesn't
    exist.  This updates the alarm handler to call the current method
    close()

--- lib/ansible/plugins/connection/network_cli.py
+++ lib/ansible/plugins/connection/network_cli.py
@@ -242,7 +242,7 @@ class Connection(Rpc, _Connection):
     def alarm_handler(self, signum, frame):
         """Alarm handler raised in case of command timeout """
         display.display('closing shell due to sigalarm', log_only=True)
-        self.close_shell()
+        self.close()
 
     def exec_command(self, cmd):
         """Executes the cmd on in the shell and returns the output
