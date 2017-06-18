f2df2a11731d40169b4529d0ffea720910193213
loqutus <rusik@4ege.ru>
2017-06-18 11:51:42 +0300

    fix comment in inventory/digital_ocean.py (#25828)

--- contrib/inventory/digital_ocean.py
+++ contrib/inventory/digital_ocean.py
@@ -22,7 +22,7 @@ found.  You can force this script to use the cache with --force-cache.
 
 ----
 Configuration is read from `digital_ocean.ini`, then from environment variables,
-then and command-line arguments.
+and then from command-line arguments.
 
 Most notably, the DigitalOcean API Token must be specified. It can be specified
 in the INI file or with the following environment variables:
