a2685787e4f7a5e981ed40a8357fbf341115950d
Matt Martz <matt@sivel.net>
2017-06-19 15:12:35 -0500

    Ensure hosts aren't duplicated in groups

--- lib/ansible/inventory/group.py
+++ lib/ansible/inventory/group.py
@@ -112,7 +112,8 @@ class Group:
             raise AnsibleError("The group named '%s' has a recursive dependency loop." % self.name)
 
     def add_host(self, host):
-
+        if host in self.hosts:
+            return
         self.hosts.append(host)
         host.add_group(self)
         self.clear_hosts_cache()
