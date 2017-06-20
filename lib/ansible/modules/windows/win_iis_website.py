950081ddda4da1c158c289b1422a05a9bfcfdf36
Jordan Borean <jborean93@gmail.com>
2017-06-20 21:34:34 +1000

    win_iis_website: Add integration tests, check mode and better param support

--- lib/ansible/modules/windows/win_iis_website.py
+++ lib/ansible/modules/windows/win_iis_website.py
@@ -22,129 +22,228 @@ ANSIBLE_METADATA = {'metadata_version': '1.0',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
-
 DOCUMENTATION = r'''
 ---
 module: win_iis_website
 version_added: "2.0"
-short_description: Configures a IIS Web site.
+short_description: Configures an IIS Web site.
 description:
-     - Creates, Removes and configures a IIS Web site
+  - Creates, removes and configures an IIS Web site.
+  - If you wish to modify the bindings for a site, please use
+    M(win_iis_webbinding) to perform these tasks.
 options:
   name:
-    description:
-      - Names of web site
+    description: The name of the web site.
     required: true
-    default: null
-    aliases: []
   site_id:
     description:
-      - Explicitly set the IIS numeric ID for a site. Note that this value cannot be changed after the website has been created.
-    required: false
+      - Explicitly set the IIS numeric ID for a site. Note that if this ID does
+        not match a site.
+      - If the site ID does not match then the site will be deleted and
+        recreated with the new ID set.
+      - The module will throw an error if the ID is already used by another
+        site that is not called C(name).
     version_added: "2.1"
-    default: null
   state:
-    description:
-      - State of the web site
+    description: The state of the web site.
     choices:
       - started
       - restarted
       - stopped
       - absent
-    required: false
-    default: null
-    aliases: []
+    default: started
   physical_path:
     description:
-      - The physical path on the remote host to use for the new site. The specified folder must already exist.
-    required: false
-    default: null
-    aliases: []
+      - The physical path on the remote host to use for the new site.
+      - The specified folder must already exist.
+      - This must be set when creating a new web site.
   application_pool:
     description:
-      - The application pool in which the new site executes.
-    required: false
-    default: null
-    aliases: []
+      - The application pool in which the site executes.
+      - If a new site is created and this is not set, the app pool used will
+        be the default on the server.
   port:
     description:
-      - The port to bind to / use for the new site.
-    required: false
-    default: null
-    aliases: []
+      - The port to bind to.
+      - This is only set if the site does not exist and will be created by
+        the module execution.
+      - DEPRECATED as of Ansible 2.4, will be removed in Ansible 2.6.
+      - Use M(win_iis_webbinding) instead to modify the bindings of a site.
   ip:
     description:
-      - The IP address to bind to / use for the new site.
-    required: false
-    default: null
-    aliases: []
+      - The IP address to bind to.
+      - This is only set if the site does not exist and will be created by
+        the module execution.
+      - DEPRECATED as of Ansible 2.4, will be removed in Ansible 2.6.
+      - Use M(win_iis_webbinding) instead to modify the bindings of a site.
   hostname:
     description:
-      - The host header to bind to / use for the new site.
-    required: false
-    default: null
-    aliases: []
+      - The host header to bind to.
+      - This is only set if the site does not exist and will be created by
+        the module execution.
+      - DEPRECATED as of Ansible 2.4, will be removed in Ansible 2.6.
+      - Use M(win_iis_webbinding) instead to modify the bindings of a site.
   ssl:
     description:
-      - Enables HTTPS binding on the site..
-    required: false
-    default: null
-    aliases: []
+      - Enables HTTPS binding on the site.
+      - This is only set if the site does not exist and will be created by
+        the module execution.
+      - DEPRECATED as of Ansible 2.4, will be removed in Ansible 2.6.
+      - Use M(win_iis_webbinding) instead to modify the bindings of a site.
+  attributes:
+    description:
+      - Custom site attributes to set in a dict form.
+      - These attributes are based on the naming standard at
+        U(https://www.iis.net/configreference/system.applicationhost/sites/site#005).
+      - You can also set attributes of child elements like application, limits
+        and other by setting the full path like 'limits.maxBandwidth'.
+      - If setting an enum value, this can be either the enum value or the enum
+        name itself.
+    version_added: "2.4"
   parameters:
     description:
-      - Custom site Parameters from string where properties are separated by a pipe and property name/values by colon Ex. "foo:1|bar:2"
-    required: false
-    default: null
-    aliases: []
-author: Henrik Wallström
+      - DEPRECATED as of Ansible 2.4, use C(attributes) instead, this will be
+        removed in Ansible 2.6.
+      - Custom site parameters in a string where each parameter is separated by
+        a pip and property name/values by colon, e.g. "foo:1|bar:2"
+author:
+  - "Henrik Wallström (@henrikwallstrom)"
+  - "Jordan Borean (@jborean93)"
 '''
 
 EXAMPLES = r'''
-
-# Start a website
-
-- name: Acme IIS site
+- name: create and start a website with defaults
   win_iis_website:
-    name: "Acme"
+    name: Acme
     state: started
-    port: 80
-    ip: 127.0.0.1
-    hostname: acme.local
-    application_pool: "acme"
-    physical_path: c:\sites\acme
-    parameters: logfile.directory:c:\sites\logs
-  register: website
 
-# Remove Default Web Site and the standard port 80 binding
-- name: Remove Default Web Site
+- name: create and start a website with custom attributes
   win_iis_website:
-    name: "Default Web Site"
+    name: Acme
+    state: started
+    application_pool: acme
+    physical_path: C:\sites\acme
+    attributes:
+      serverAutoStart: True
+      # timespan format is "days:hours:minutes:seconds.milliseconds" or "hours:minutes:seconds"
+      limits.connectionTimeout: "00:02:00"
+      logFile.directory: C:\sites\logs
+      logFile.logFormat: IIS
+
+# Note the below is the older format for setting attributes
+# this is only set as a reference for older users and should
+# not be followed for future implementations
+- name: create and start a website with custom attributes
+  win_iis_website:
+    name: Acme
+    state: started
+    application_pool: acme
+    physical_path: C:\sites\acme
+    parameters: "logFile.directory:C:\sites\logs"
+
+- name: remove the Default Web Site
+  win_iis_website:
+    name: Default Web Site
     state: absent
+'''
 
-# Some commandline examples:
-
-# This return information about an existing host
-# $ ansible -i vagrant-inventory -m win_iis_website -a "name='Default Web Site'" window
-# host | success >> {
-#     "changed": false,
-#     "site": {
-#         "ApplicationPool": "DefaultAppPool",
-#         "Bindings": [
-#             "*:80:"
-#         ],
-#         "ID": 1,
-#         "Name": "Default Web Site",
-#         "PhysicalPath": "%SystemDrive%\\inetpub\\wwwroot",
-#         "State": "Stopped"
-#     }
-# }
-
-# This stops an existing site.
-# $ ansible -i hosts -m win_iis_website -a "name='Default Web Site' state=stopped" host
-
-# This creates a new site.
-# $ ansible -i hosts -m win_iis_website -a "name=acme physical_path=c:\\sites\\acme" host
-
-# Change logfile.
-# $ ansible -i hosts -m win_iis_website -a "name=acme physical_path=c:\\sites\\acme" host
+RETURN = r'''
+attributes:
+  description: The attributes that are set when calling the module and parsed
+    during execution.
+  returned: success
+  type: dictionary
+  sample:
+    "serverAutoStart": True
+    "limits.connectionTimeout": "00:02:00"
+    "logFile.directory": "C:\\sites\\logs"
+    "logFile.logFormat": IIS
+info:
+  description: Information about the web site, these are all the attributes
+    that are set and retrieved from the Site. See
+    "https://www.iis.net/configreference/system.applicationhost/sites/site#005"
+    for a full list of attributes that are returned, the below is just a
+    sample.
+  returned: success
+  type: complex
+  contains:
+    applicationDefaults:
+      description: Attributes of the default configuration settings for
+        applications in the site.
+      returned: success
+      type: dictionary
+      sample:
+        applicationPool: ""
+        enabledProtocols: http
+        path: ""
+        preloadEnabled: False
+        serviceAutoStartEnabled: False
+        serviceAutoStartProvider: ""
+    attributes:
+      description: Attributes for the basic site settings.
+      returned: success
+      type: dictionary
+      sample:
+        applicationPool: "AppPool"
+        id: 1
+        name: Acme
+        physicalPath: C:\ansible\win_iis_website
+        serverAutoStart: True
+        state: Started
+    bindings:
+      description: A list of bindings to access the site.
+      returned: success
+      type: list
+      sample: [
+        { 
+          "bindingInformation": "*:80:", "certificateHash": "",
+           "certificateStoreName": "", "isDsMapperEnabled": False,
+           "protocol": "http", "sslFlags": 0
+        }
+      ]
+    limits:
+      description: Attributes to limit the amount of bandwidth, the number of
+        connections, or the amount of time for connections to a site.
+      returned: success
+      type: dictionary
+      sample:
+        connectionTimeout: { "TotalSeconds": 120 }
+        maxBandwidth: 2048
+        maxConnections: 1000
+        maxUrlSegments: 32
+    logFile:
+      description: Attributes for handling and storage of log files for the
+        site.
+      returned: success
+      type: dictionary
+      sample:
+        directory: "%SystemDrive%\\inetpub\\logs\\LogFiles"
+        enabled: true
+    traceFailedRequestsLogging:
+      description: Attributes for logging failed-request traces for the site.
+      returned: success
+      type: dictionary
+      sample:
+        directory: "%SystemDrive%\\inetpub\\logs\\FailedReqLogFiles"
+        enabled: False
+    virtualDirectoryDefaults:
+      description: Attributes for all virtual directories in the site.
+      returned: success
+      type: dictionary
+      sample:
+        logonMethod: "Batch"
+        path: ""
+        userName: "User"
+site:
+  description: Basic information about the web site, this is deprecated and
+    kept for backwards compatibility, use the info key as it contains more
+    info of the site.
+  returned: success
+  type: dictionary
+  sample:
+    ApplicationPool: "AppPool"
+    Bindings: "*:80:"
+    ID: 1
+    PhysicalPath: "C:\\ansible\\win_iis_website"
+    State: "Started"
 '''
