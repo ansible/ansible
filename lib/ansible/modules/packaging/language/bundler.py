8f95e827a41545699b11e6d289c030b02371fc88
Anna Wiggins <awiggins@roostify.com>
2017-06-21 11:32:02 -0700

    Correct documentation for deployment_mode flag

--- lib/ansible/modules/packaging/language/bundler.py
+++ lib/ansible/modules/packaging/language/bundler.py
@@ -77,8 +77,8 @@ options:
     default: "no"
   deployment_mode:
     description:
-      - Only applies if state is C(present). If set it will only install gems
-        that are in the default or production groups. Requires a Gemfile.lock
+      - Only applies if state is C(present). If set it will install gems in
+        ./vendor/bundle instead of the default location. Requires a Gemfile.lock
         file to have been created prior
     required: false
     choices: [yes, no]
@@ -126,7 +126,7 @@ EXAMPLES='''
     state: present
     exclude_groups: production
 
-# Only install gems from the default and production groups
+# Install gems into ./vendor/bundle
 - bundler:
     state: present
     deployment_mode: yes
