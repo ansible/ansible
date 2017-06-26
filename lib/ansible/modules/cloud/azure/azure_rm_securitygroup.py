171d903d04d59316b517100fd9d551a9c73e1e3b
Raja <rajasaur@gmail.com>
2017-06-26 11:43:28 +0530

    Fix Python3 Errors in Azure Cloud Module
    When using Python3, the exec_module function errors out with a
    unsupported operand type(s) for +: 'dict_keys' and 'list'
    error when adding the .keys() to a static list. Use the explicit
    list function to make a list of keys and then add to the ['tags'] list.

--- lib/ansible/modules/cloud/azure/azure_rm_securitygroup.py
+++ lib/ansible/modules/cloud/azure/azure_rm_securitygroup.py
@@ -548,7 +548,7 @@ class AzureRMSecurityGroup(AzureRMModuleBase):
 
     def exec_module(self, **kwargs):
 
-        for key in self.module_arg_spec.keys() + ['tags']:
+        for key in list(self.module_arg_spec.keys()) + ['tags']:
             setattr(self, key, kwargs[key])
 
         changed = False
