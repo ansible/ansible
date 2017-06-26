468d792f621a1d217864841bcca3d74c180fad49
Brian Coca <brian.coca+git@gmail.com>
2017-06-26 15:29:24 -0400

    updated as per feedback

--- lib/ansible/module_utils/basic.py
+++ lib/ansible/module_utils/basic.py
@@ -2135,8 +2135,8 @@ class AnsibleModule(object):
             kwargs['changed'] = False
 
         # add traceback if debug or high verbosity and it is missing
-        # note: badly named as exception, it is really always been 'traceback'
-        if 'exception' not in kwargs and (self._debug or self._verbosity >= 3):
+        # Note: badly named as exception, it is really always been 'traceback'
+        if 'exception' not in kwargs and sys.exc_info()[2] and (self._debug or self._verbosity >= 3):
             kwargs['exception'] = ''.join(traceback.format_tb(sys.exc_info()[2]))
 
         self.do_cleanup_files()
