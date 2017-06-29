18fbab16abb369cac145aa90f0374ae7d500ac4e
jgkirschbaum <juergen.kirschbaum@gmail.com>
2017-06-29 19:14:59 +0200

    Update rabbitmq_global_parameter.py

--- lib/ansible/modules/messaging/rabbitmq_global_parameter.py
+++ lib/ansible/modules/messaging/rabbitmq_global_parameter.py
@@ -58,7 +58,7 @@ options:
 EXAMPLES = '''
 # Set the global parameter 'cluster_name' to a value of 'mq-cluster' (in quotes)
 - rabbitmq_global_parameter: name=cluster_name
-                             value='"mq-cluster"'
+                             value="{{ 'mq-cluster' | to_json }}"
                              state=present
 '''
 
