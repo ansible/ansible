4c41f2c4e9c174a9659326b50608df17c5776330
Julien Vey <vey.julien@gmail.com>
2017-06-29 15:55:03 +0200

    parted: Add missing LC_TYPE override for env vars (#26242)

--- lib/ansible/modules/system/parted.py
+++ lib/ansible/modules/system/parted.py
@@ -590,7 +590,7 @@ def main():
         },
         supports_check_mode=True,
     )
-    module.run_command_environ_update = {'LANG': 'C', 'LC_ALL': 'C', 'LC_MESSAGES': 'C'}
+    module.run_command_environ_update = {'LANG': 'C', 'LC_ALL': 'C', 'LC_MESSAGES': 'C', 'LC_CTYPE': 'C'}
 
     # Data extraction
     device = module.params['device']
