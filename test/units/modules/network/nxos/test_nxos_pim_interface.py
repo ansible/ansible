37ccc674e86eb40d9ba60fa8ef39d5026bf548a9
Nathaniel Case <ncase@redhat.com>
2017-07-03 13:42:59 -0400

    Update tests to match module

--- test/units/modules/network/nxos/test_nxos_pim_interface.py
+++ test/units/modules/network/nxos/test_nxos_pim_interface.py
@@ -19,56 +19,67 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import json
-
 from ansible.compat.tests.mock import patch
-from ansible.modules.network.nxos import nxos_ip_interface
+from ansible.modules.network.nxos import nxos_pim_interface
 from .nxos_module import TestNxosModule, load_fixture, set_module_args
 
 
 class TestNxosIPInterfaceModule(TestNxosModule):
 
-    module = nxos_ip_interface
+    module = nxos_pim_interface
 
     def setUp(self):
-        self.mock_get_interface_mode = patch(
-            'ansible.modules.network.nxos.nxos_ip_interface.get_interface_mode')
-        self.get_interface_mode = self.mock_get_interface_mode.start()
-
-        self.mock_send_show_command = patch(
-            'ansible.modules.network.nxos.nxos_ip_interface.send_show_command')
-        self.send_show_command = self.mock_send_show_command.start()
-
-        self.mock_load_config = patch('ansible.modules.network.nxos.nxos_ip_interface.load_config')
+        self.mock_load_config = patch('ansible.modules.network.nxos.nxos_pim_interface.load_config')
         self.load_config = self.mock_load_config.start()
 
+        self.mock_run_commands = patch('ansible.modules.network.nxos.nxos_pim_interface.run_commands')
+        self.run_commands = self.mock_run_commands.start()
+
     def tearDown(self):
-        self.mock_get_interface_mode.stop()
-        self.mock_send_show_command.stop()
         self.mock_load_config.stop()
+        self.mock_run_commands.stop()
 
-    def load_fixtures(self, commands=None):
-        self.get_interface_mode.return_value = 'layer3'
-        self.send_show_command.return_value = [load_fixture('nxos_ip_interface.cfg')]
+    def load_fixtures(self, commands=None, device=''):
+        module_name = self.module.__name__.rsplit('.', 1)[1]
+        def load_from_file(*args, **kwargs):
+            module, commands = args
+            output = list()
+
+            for command in commands:
+                filename = str(command).split(' | ')[0].replace(' ', '_').replace('/', '_')
+                output.append(load_fixture(module_name, filename))
+            return output
+
         self.load_config.return_value = None
+        self.run_commands.side_effect = load_from_file
 
-    def test_nxos_ip_interface_ip_present(self):
-        set_module_args(dict(interface='eth2/1', addr='1.1.1.2', mask=8))
-        result = self.execute_module(changed=True)
-        self.assertEqual(result['commands'],
-                         ['interface eth2/1',
-                          'no ip address 1.1.1.1/8',
-                          'interface eth2/1',
-                          'ip address 1.1.1.2/8'])
+    def test_nxos_pim_interface_present(self):
+        set_module_args(dict(interface='eth2/1', dr_prio=10, hello_interval=40))
+        self.execute_module(
+            changed=True,
+            commands=['interface eth2/1', 'ip pim dr-priority 10',
+                      'ip pim hello-interval 40000', 'ip pim sparse-mode']
+        )
 
-    def test_nxos_ip_interface_ip_idempotent(self):
-        set_module_args(dict(interface='eth2/1', addr='1.1.1.1', mask=8))
-        result = self.execute_module(changed=False)
-        self.assertEqual(result['commands'], [])
+    def test_nxos_pim_interface_jp(self):
+        set_module_args(dict(
+            interface='eth2/1', jp_policy_in='JPIN', jp_policy_out='JPOUT',
+            jp_type_in='routemap', jp_type_out='routemap',
+        ))
+        self.execute_module(
+            changed=True,
+            commands=['interface eth2/1', 'ip pim jp-policy JPOUT out',
+                      'ip pim jp-policy JPIN in', 'ip pim sparse-mode']
+        )
 
-    def test_nxos_ip_interface_ip_absent(self):
-        set_module_args(dict(interface='eth2/1', state='absent',
-                             addr='1.1.1.1', mask=8))
-        result = self.execute_module(changed=True)
-        self.assertEqual(result['commands'],
-                         ['interface eth2/1', 'no ip address 1.1.1.1/8'])
+    def test_nxos_pim_interface_default(self):
+        set_module_args(dict(interface='eth2/1', state='default'))
+        self.execute_module(
+            changed=True,
+            commands=['interface eth2/1', 'ip pim dr-priority 1',
+                      'no ip pim border', 'ip pim hello-interval 30000']
+        )
+
+    def test_nxos_pim_interface_ip_absent(self):
+        set_module_args(dict(interface='eth2/1', state='absent'))
+        self.execute_module(changed=False, commands=[])
