7f5a0715bfb72b85763feb19500133539f9b5abb
Jordan Borean <jborean93@gmail.com>
2017-07-05 08:17:49 +1000

    win_regedit: fix for same dword value (#26415)
    (cherry picked from commit 8e9d04043ae1ac8ed4fe458396748e127ab78bb8)

--- lib/ansible/modules/windows/win_regedit.ps1
+++ lib/ansible/modules/windows/win_regedit.ps1
@@ -82,14 +82,14 @@ Function Compare-Data {
         } else {
             return $false
         }
-    } elseif ($ReferenceData -is [string] -or $ReferenceData -is [int]) {
-        if ($ReferenceData -eq $DifferenceData) {
+    } elseif ($ReferenceData -is [object[]]) {
+        if (@(Compare-Object $ReferenceData $DifferenceData -SyncWindow 0).Length -eq 0) {
             return $true
         } else {
             return $false
         }
-    } elseif ($ReferenceData -is [object[]]) {
-        if (@(Compare-Object $ReferenceData $DifferenceData -SyncWindow 0).Length -eq 0) {
+    } else {
+        if ($ReferenceData -ceq $DifferenceData) {
             return $true
         } else {
             return $false
