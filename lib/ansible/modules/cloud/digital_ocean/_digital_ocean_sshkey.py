c96aa8a1cfea2dba9cda0b7871de9763b51d70b9
Patrick F. Marques <patrickfmarques@gmail.com>
2017-07-05 20:25:36 +0100

    Fix module deprecation

--- lib/ansible/modules/cloud/digital_ocean/_digital_ocean_sshkey.py
+++ lib/ansible/modules/cloud/digital_ocean/_digital_ocean_sshkey.py
@@ -9,7 +9,7 @@ __metaclass__ = type
 
 
 ANSIBLE_METADATA = {'metadata_version': '1.0',
-                    'status': ['preview'],
+                    'status': ['deprecated'],
                     'supported_by': 'community'}
 
 DOCUMENTATION = '''
