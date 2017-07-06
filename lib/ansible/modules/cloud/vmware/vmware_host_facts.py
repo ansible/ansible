3c1042d5388706df6e4f0e773cb79e75a5a8e9b3
Gawain <gaowei3@qq.com>
2017-07-06 23:51:52 +0800

    add serial number
     get_system_facts() add ansible_product_serial

--- lib/ansible/modules/cloud/vmware/vmware_host_facts.py
+++ lib/ansible/modules/cloud/vmware/vmware_host_facts.py
@@ -109,12 +109,17 @@ def get_network_facts(host):
 
 
 def get_system_facts(host):
+    sn = 'NA'
+    for info in host.hardware.systemInfo.otherIdentifyingInfo:
+        if info.identifierType.key == 'ServiceTag':
+            sn = info.identifierValue
     facts = {
-        'ansible_distribution': host.config.product.name, 
-        'ansible_distribution_version': host.config.product.version, 
-        'ansible_os_type': host.config.product.osType, 
+        'ansible_distribution': host.config.product.name,
+        'ansible_distribution_version': host.config.product.version,
+        'ansible_os_type': host.config.product.osType,
         'ansible_system_vendor': host.hardware.systemInfo.vendor,
         'ansible_system_model': host.hardware.systemInfo.model,
+        'ansible_product_serial': sn,
         'ansible_bios_date': host.hardware.biosInfo.releaseDate,
         'ansible_bios_version': host.hardware.biosInfo.biosVersion
     }
