c1db2cd5c674d5c35076b59bf37b9ed49c8fb9b2
Jose Roberto Vidal <vidal.roberto.j@gmail.com>
2017-07-06 21:29:12 -0500

    Fix Connection#reset in Python 3

--- lib/ansible/plugins/connection/ssh.py
+++ lib/ansible/plugins/connection/ssh.py
@@ -893,7 +893,7 @@ class Connection(ConnectionBase):
 
     def reset(self):
         # If we have a persistent ssh connection (ControlPersist), we can ask it to stop listening.
-        cmd = map(to_bytes, self._build_command(self._play_context.ssh_executable, '-O', 'stop', self.host))
+        cmd = list(map(to_bytes, self._build_command(self._play_context.ssh_executable, '-O', 'stop', self.host)))
         controlpersist, controlpath = self._persistence_controls(cmd)
         if controlpersist:
             display.vvv(u'sending stop: %s' % cmd)
