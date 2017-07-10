adab0ef97706d5db61e37450209fb1366e8a0c27
Michael De La Rue <jababubic.margoteen@spamgourmet.com>
2017-07-10 13:35:59 +0100

    rds_instance_facts module
    Create rds_instance_facts module using boto3.
    
    Create common rds.py module_util library to be used by
    rds_instance_facts and future RDS modules.

---
+++ lib/ansible/module_utils/aws/rds.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2017 Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# Author: Michael De La Rue 2017 largely rewritten but based on work
+# by Will Thames taken in turn from the original rds module.
+
+try:
+    import botocore
+    from botocore import xform_name
+except:
+    pass  # it is assumed that calling modules will detect and provide an appropriate nice error.
+
+from ansible.module_utils.ec2 import camel_dict_to_snake_dict, boto3_tag_list_to_ansible_dict
+
+DEFAULT_PORTS = {
+    'aurora': 3306,
+    'mariadb': 3306,
+    'mysql': 3306,
+    'oracle': 1521,
+    'sqlserver': 1433,
+    'postgres': 5432,
+}
+
+DB_ENGINES = [
+    'aurora',
+    'mariadb',
+    'mysql',
+    'oracle-ee',
+    'oracle-se',
+    'oracle-se1',
+    'oracle-se2',
+    'postgres',
+    'sqlserver-ee',
+    'sqlserver-ex',
+    'sqlserver-se',
+    'sqlserver-web',
+]
+
+LICENSE_MODELS = [
+    'bring-your-own-license',
+    'general-public-license',
+    'license-included',
+    'postgresql-license'
+]
+
+
+def get_db_instance(conn, instancename):
+    """return AWS format DB instance
+
+    This function connects to AWS and retrieves the information about
+    the instance in directly in the standard AWS format.
+    """
+    # FIXME: currently this makes two API calls, one for the instance and
+    # one for the tags.  This mostly make sense since the object we get
+    # back is close to what we want to call a create with, but for
+    # efficiency we should maybe put an option to not make the tags call in
+    # the case where we throw that data away.
+    try:
+        response = conn.describe_db_instances(DBInstanceIdentifier=instancename)
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == 'DBInstanceNotFound':
+            return None
+        raise
+
+    instance = response['DBInstances'][0]
+
+    tags = conn.list_tags_for_resource(ResourceName=instance['DBInstanceArn']).get('TagList', [])
+    instance['Tags'] = boto3_tag_list_to_ansible_dict(tags)
+
+    return instance
+
+
+def instance_to_facts(instance):
+    if not instance:
+        return instance
+    if 'DBInstanceIdentifier' not in instance:
+        raise AssertionError("instance argument was not a valid instance")
+    return camel_dict_to_snake_dict(instance, ignore_list=['Tags'])
+
+
+def get_snapshot(conn, snapshotid):
+    try:
+        response = conn.describe_db_snapshots(DBSnapshotIdentifier=snapshotid)
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == 'DBSnapshotNotFound':
+            return None
+        raise
+    return response['DBSnapshots'][0]
+
+
+def snapshot_to_facts(snapshot):
+    if 'DBSnapshotIdentifier' not in snapshot:
+        raise AssertionError("snapshot argument was not a valid snapshot")
+    return camel_dict_to_snake_dict(snapshot)
+
+
+def instance_facts_diff(connection, state_a, state_b):
+    """compare two fact dictionaries for rds instances
+
+    This function takes two dictionaries of facts related to an RDS
+    and compares them intelligently generating only the differences
+    which ansible controls.  If nothing has changed then the
+    difference should be an empty dictionary which can be treated as
+    False
+
+    The function aims to work with both instance states and a set of
+    module parameters.  It will select those parameters that could be
+    used in a create call.
+
+    The second dict is assumed to represent a target state and so if
+    parameters are missing they will not be considered to show a
+    difference.
+    """
+    operations_model = connection._service_model.operation_model("CreateDBInstance")
+    compare_keys = [xform_name(x) for x in operations_model.input_shape.members.keys()]
+
+    if len(compare_keys) < 2:
+        raise AssertionError
+
+    remove_if_null = []
+    before = dict()
+    after = dict()
+
+    try:
+        old_port = state_a.get("endpoint").get("port")
+    except AttributeError:
+        old_port = None
+
+    if old_port is not None:
+        state_a["port"] = old_port
+
+    try:
+        new_port = state_b.get("endpoint").get("port")
+    except AttributeError:
+        new_port = None
+
+    state_a['db_subnet_group_name'] = state_a.get('db_subnet_group', {}).get('db_subnet_group_name')
+    if state_a['db_subnet_group_name']:
+        del state_a['db_subnet_group']
+
+    state_a['db_parameter_group_name'] = state_a.get('db_parameter_group', {}).get('db_parameter_group_name')
+    if state_a['db_parameter_group_name']:
+        del state_a['db_parameter_group']
+
+    before_groups = state_a.get('vpc_security_groups')
+    if before_groups is not None:
+        state_a['vpc_security_group_ids'] = [sg['vpc_security_group_id'] for sg in before_groups]
+        if state_a['vpc_security_group_ids']:
+            del state_a['vpc_security_groups']
+
+    if new_port is not None:
+        state_b["port"] = new_port
+
+    for k in compare_keys:
+        if state_a.get(k) != state_b.get(k):
+            if state_b.get(k) is None and k not in remove_if_null:
+                continue
+            before[k] = state_a.get(k)
+            after[k] = state_b.get(k)
+
+    if before:
+        result = dict(before_header=state_a.get('instance_id'), before=before, after=after)
+        result['after_header'] = state_b.get('instance_id', state_a.get('instance_id'))
+    else:
+        result = {}
+    return result
---
+++ lib/ansible/modules/cloud/amazon/rds_instance_facts.py
@@ -0,0 +1,153 @@
+#!/usr/bin/python
+# Copyright (c) 2017, 2018 Michael De La Rue
+# Copyright (c) 2017, 2018 Will Thames
+# Copyright (c) 2017 Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+ANSIBLE_METADATA = {'status': ['preview'],
+                    'supported_by': 'community',
+                    'metadata_version': '1.1'}
+
+DOCUMENTATION = '''
+---
+module: rds_instance_facts
+version_added: "2.6"
+short_description: obtain facts about one or more RDS instances
+description:
+  - obtain facts about one or more RDS instances
+options:
+  db_instance_identifier:
+    description:
+      - The RDS instance's unique identifier.
+    required: false
+    aliases:
+      - id
+  filters:
+    description:
+      - A filter that specifies one or more DB instances to describe.
+        See U(https://docs.aws.amazon.com/AmazonRDS/latest/APIReference/API_DescribeDBInstances.html)
+requirements:
+    - "python >= 2.6"
+    - "boto3"
+author:
+    - "Will Thames (@willthames)"
+    - "Michael De La Rue (@mikedlr)"
+extends_documentation_fragment:
+    - aws
+    - ec2
+'''
+
+EXAMPLES = '''
+# Get facts about an instance
+- rds_instance_facts:
+    name: new-database
+  register: new_database_facts
+
+# Get all RDS instances
+- rds_instance_facts:
+'''
+
+# FIXME - return needs updated
+
+RETURN = '''
+instances:
+    description: zero or more instances that match the (optional) parameters
+    type: list
+    returned: always
+    sample:
+       "instances": [
+           {
+               "availability_zone": "ap-southeast-2c",
+               "backup_retention": 10,
+               "create_time": "2017-02-20T06:13:05.724000+00:00",
+               "db_engine": "postgres",
+               "endpoint": "helloworld-rds-master.cyaju3p4tyqv.ap-southeast-2.rds.amazonaws.com",
+               "id": "helloworld-rds-master",
+               "instance_type": "db.t2.micro",
+               "maintenance_window": "sun:15:40-sun:16:10",
+               "multi_zone": false,
+               "port": 5432,
+               "replication_source": null,
+               "size": 5,
+               "status": "available",
+               "username": "hello",
+               "vpc_security_groups": "sg-abcd1234"
+           },
+       ]
+'''
+
+from ansible.module_utils.aws.core import AnsibleAWSModule
+from ansible.module_utils.ec2 import get_aws_connection_info, boto3_conn, ansible_dict_to_boto3_filter_list
+# FIXME - from ansible.module_utils.ec2 import boto3_tag_list_to_ansible_dict
+from ansible.module_utils.aws.rds import instance_to_facts
+
+
+try:
+    import botocore
+except ImportError:
+    pass  # caught by imported HAS_BOTO3
+
+
+def instance_facts(module, conn):
+    instance_name = module.params.get('db_instance_identifier')
+    filters = module.params.get('filters')
+
+    params = dict()
+    if instance_name:
+        params['DBInstanceIdentifier'] = instance_name
+    if filters:
+        params['Filters'] = ansible_dict_to_boto3_filter_list(filters)
+
+    marker = ''
+    try:
+        results = conn.describe_db_instances(Marker=marker, **params)['DBInstances']
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == 'DBInstanceNotFound':
+            results = []
+        module.fail_json_aws(e, "trying to get instance information")
+
+    if instance_name:
+        results = [x for x in results if x['DBInstanceIdentifier'] == instance_name]
+        if len(results) != 1:
+            raise AssertionError
+
+    # FIXME: Get tags for each response
+    # for instance in results:
+    #     instance['tags'] = boto3_tag_list_to_ansible_dict(
+    #         conn.list_tags_for_resource(
+    #             ResourceName=instance['DBInstanceArn'])['TagList'])
+
+    # FIXME - this needs to be simplified to do one snakify as we get rid of the
+    # RDSDBInstance class - see https://github.com/ansible/ansible/pull/26598
+
+    return dict(changed=False, db_instances=[instance_to_facts(instance) for instance in results])
+
+
+argument_spec = dict(
+    db_instance_identifier=dict(aliases=['id']),
+    filters=dict(type='list')
+)
+
+
+def main():
+    module = AnsibleAWSModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True,
+    )
+
+    region, ec2_url, aws_connect_params = get_aws_connection_info(module, boto3=True)
+    if not region:
+        module.fail_json(
+            msg="Region not specified. Unable to determine region from configuration.")
+
+    # connect to the rds endpoint
+    try:
+        conn = boto3_conn(module, 'client', 'rds', region, **aws_connect_params)
+    except Exception as e:
+        module.fail_json_aws(e, msg="trying to connect to AWS")
+
+    module.exit_json(**instance_facts(module, conn))
+
+
+if __name__ == '__main__':
+    main()
