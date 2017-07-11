d81ab9931768d8d53d0193266f079d1c9520e0a5
Gawain <gaowei3@qq.com>
2017-07-11 10:34:32 +0800

    add ansible_hostname ansible_distribution_build

--- lib/ansible/modules/cloud/vmware/vmware_host_facts.py
+++ lib/ansible/modules/cloud/vmware/vmware_host_facts.py
@@ -127,9 +127,11 @@ def get_system_facts(host):
     facts = {
         'ansible_distribution': host.config.product.name,
         'ansible_distribution_version': host.config.product.version,
+        'ansible_distribution_build': host.config.product.build,
         'ansible_os_type': host.config.product.osType,
         'ansible_system_vendor': host.hardware.systemInfo.vendor,
-        'ansible_system_model': host.hardware.systemInfo.model,
+        'ansible_hostname': host.summary.config.name,
+        'ansible_product_name': host.hardware.systemInfo.model,
         'ansible_product_serial': sn,
         'ansible_bios_date': host.hardware.biosInfo.releaseDate,
         'ansible_bios_version': host.hardware.biosInfo.biosVersion
