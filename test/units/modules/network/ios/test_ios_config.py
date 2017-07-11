ed77e3c665674960bfc51088350a4775bb222937
Peter Sprygada <psprygada@ansible.com>
2017-07-11 08:11:51 -0500

    update unit test cases

--- test/units/modules/network/ios/test_ios_config.py
+++ test/units/modules/network/ios/test_ios_config.py
@@ -68,9 +68,9 @@ class TestIosConfigModule(TestIosModule):
         result = self.execute_module()
         self.assertIn('__backup__', result)
 
-    def test_ios_config_save(self):
+    def test_ios_config_save_always(self):
         self.run_commands.return_value = "Hostname foo"
-        set_module_args(dict(save=True))
+        set_module_args(dict(save_when='always'))
         self.execute_module(changed=True)
         self.assertEqual(self.run_commands.call_count, 2)
         self.assertEqual(self.get_config.call_count, 0)
@@ -117,9 +117,9 @@ class TestIosConfigModule(TestIosModule):
         commands = parents + lines
         self.execute_module(changed=True, commands=commands)
 
-    def test_ios_config_force(self):
+    def test_ios_config_match_none(self):
         lines = ['hostname router']
-        set_module_args(dict(lines=lines, force=True))
+        set_module_args(dict(lines=lines, match='none'))
         self.execute_module(changed=True, commands=lines)
 
     def test_ios_config_match_none(self):
