6b1865ad31f3083077e9d92a4902941206719b97
Shyam <srangana@redhat.com>
2017-07-13 16:09:37 -0400

    Fixed incorrect parsing of pvs output
    The Linux LVM pvs command can have empty VG names in the output
    if there are no VGs on the said PVs. Parsing of such output
    was incorrect as a result of such a check missing.
    
    Corrected the same using the provided '--separator' option in the
    pvs CLI.
    
    Signed-off-by: Shyam <srangana@redhat.com>

--- lib/ansible/module_utils/facts/hardware/linux.py
+++ lib/ansible/module_utils/facts/hardware/linux.py
@@ -687,38 +687,39 @@ class LinuxHardware(Hardware):
                     items = lv_line.split()
                     lvs[items[0]] = {'size_g': items[3], 'vg': items[1]}
 
-        pvs_path = self.module.get_bin_path('pvs')
-        # pvs fields: PV VG #Fmt #Attr PSize PFree
-        pvs = {}
-        if pvs_path:
-            pvs_rc, pv_lines, err = self.module.run_command('%s %s' % (pvs_path, lvm_util_options))
-        else:
-            pvs_rc = -1
+            pvs_path = self.module.get_bin_path('pvs')
+            # pvs fields: PV VG #Fmt #Attr PSize PFree
+            pvs = {}
+            if pvs_path:
+                pvs_rc, pv_lines, err = self.module.run_command('%s %s' % (pvs_path, lvm_util_options))
+            else:
+                pvs_rc = -1
 
-        if pvs_rc == 0:
-            # Find a delimiter to use, that does not exist in the normal
-            # output of pvs command.
-            delim = '#'
-            delimfound = re.search(delim, pv_lines)
-            while delimfound is not None:
-                delim = delim + '#'
-                delimfound = re.search(delim, pv_lines)
-
-            pvs_util_options = lvm_util_options + ' --separator ' + "'" + delim + "'"
-            pvs_rc, pv_lines, err = self.module.run_command('%s %s' % (pvs_path, pvs_util_options))
             if pvs_rc == 0:
-                for pv_line in pv_lines.splitlines():
-                    pv_line = pv_line.strip()
-                    items = pv_line.split(delim)
-                    pvs[self._find_mapper_device_name(items[0])] = {
-                        'size_g': items[-2],
-                        'free_g': items[-1],
-                        'vg': items[1]}
+                # Find a delimiter to use, that does not exist in the normal
+                # output of pvs command.
+                delim = '#'
+                delimfound = re.search(delim, pv_lines)
+                while delimfound is not None:
+                    delim = delim + '#'
+                    delimfound = re.search(delim, pv_lines)
 
-        if pvs_rc == 0:
-            lvm_facts['lvm'] = {'lvs': lvs, 'vgs': vgs, 'pvs': pvs}
-        else:
-            lvm_facts['lvm'] = {'lvs': lvs, 'vgs': vgs}
+                pvs_util_options = lvm_util_options + ' --separator ' + "'" + delim + "'"
+                pvs_rc, pv_lines, err = self.module.run_command('%s %s' % (pvs_path, pvs_util_options))
+                if pvs_rc == 0:
+                    for pv_line in pv_lines.splitlines():
+                        pv_line = pv_line.strip()
+                        items = pv_line.split(delim)
+                        pvs[self._find_mapper_device_name(items[0])] = {
+                            'size_g': items[-2],
+                            'free_g': items[-1],
+                            'vgpresent': True if items[1] != "" else False,
+                            'vg': items[1]}
+
+            if pvs_rc == 0:
+                lvm_facts['lvm'] = {'lvs': lvs, 'vgs': vgs, 'pvs': pvs}
+            else:
+                lvm_facts['lvm'] = {'lvs': lvs, 'vgs': vgs}
 
         return lvm_facts
 
