8333a8b90882e364216c499aed7c69274c4d984b
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2017-07-14 13:43:17 +0200

    Fix eos_config defaults logic (#26798)
    Current code is bogus, it was passing flags all unconditionally.

--- lib/ansible/modules/network/eos/eos_config.py
+++ lib/ansible/modules/network/eos/eos_config.py
@@ -288,10 +288,12 @@ def get_candidate(module):
 def get_running_config(module, config=None):
     contents = module.params['running_config']
     if not contents:
-        if not module.params['defaults'] and config:
+        if config:
             contents = config
         else:
-            flags = ['all']
+            flags = []
+            if module.params['defaults']:
+                flags.append('all')
             contents = get_config(module, flags=flags)
     return NetworkConfig(indent=3, contents=contents)
 
