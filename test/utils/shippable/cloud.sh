da9faee9b76858d8f0a3059f25983378b71b5e3f
Matt Clay <matt@mystile.com>
2017-07-14 20:43:35 -0700

    Optimize running of CI cloud integration tests.

--- test/utils/shippable/cloud.sh
+++ test/utils/shippable/cloud.sh
@@ -10,3 +10,4 @@ target="posix/ci/cloud/"
 
 # shellcheck disable=SC2086
 ansible-test integration --color -v --retry-on-error "${target}" --docker "${image}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
+    --changed-all-target 'posix/ci/cloud/vcenter/'
