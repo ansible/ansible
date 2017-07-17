ebabd269463b18d4cbd58a043d3db18f15cbea1c
Sam Doran <sdoran@redhat.com>
2017-07-17 13:09:23 -0400

    Cleanup documentation

--- lib/ansible/modules/system/lightbulb.py
+++ lib/ansible/modules/system/lightbulb.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python
+# (c) 2017, Red Hat
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
 DOCUMENTATION = '''
-Here is the documentation
+---
+module: lightbulb
+version_added: "2.4"
+short_description: Demo module
+author: Sam Doran
+description:
+    - Demo module
 '''
 
-EXPAMLES = '''
+EXAMPLES = '''
 - lightbulb:
   name: test
   enabled: yes
@@ -20,7 +42,7 @@ msg:
 
 ANSIBLE_METADATA = {
         'metadata_version': '1.0',
-        'suppoted_by': 'community',
+        'supported_by': 'community',
         'status': ['preview']
 }
 
