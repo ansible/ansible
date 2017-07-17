8e85aa44fecd6c69d43bd98b3a32a0f2e425d173
Martin Krizek <mkrizek@redhat.com>
2017-07-17 20:10:53 +0200

    Make it actually work

--- lib/ansible/modules/system/lightbulb.py
+++ lib/ansible/modules/system/lightbulb.py
@@ -1,1 +1,45 @@
-hello world
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['stableinterface'],
+                    'supported_by': 'core'}
+
+
+DOCUMENTATION = '''
+---
+module: lightbulb
+author:
+    - "Ansible Core Team"
+version_added: "2.4"
+short_description:  Lightbulb.
+description:
+    - Lightbulb.
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+
+def main():
+    module = AnsibleModule(
+            argument_spec=dict()
+    )
+    module.exit_json(changed=True, msg="hello world")
+
+
+if __name__ == '__main__':
+    main()
