e51a669fb6939d8c04624ea36595016ba6d3055d
Sam Doran <sdoran@redhat.com>
2017-07-17 12:34:51 -0400

    Better module according to docs

--- lib/ansible/modules/system/lightbulb.py
+++ lib/ansible/modules/system/lightbulb.py
@@ -1,1 +1,45 @@
-hello world
+#!/usr/bin/python
+
+DOCUMENTATION = '''
+Here is the documentation
+'''
+
+EXPAMLES = '''
+- lightbulb:
+  name: test
+  enabled: yes
+'''
+
+RETURN = '''
+msg:
+    description: Message
+    returned: success
+    type: string
+    sample: This is a sample
+'''
+
+ANSIBLE_METADATA = {
+        'metadata_version': '1.0',
+        'suppoted_by': 'community',
+        'status': ['preview']
+}
+
+from ansible.module_utils.basic import AnsibleModule
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            echo_message = dict(required=True, type='str')
+        ),
+        supports_check_mode = True
+    )
+
+    echo_message = module.argument_spec['echo_message']
+
+    if module.check_mode:
+        module.exit_json(changed=True, msg='This is check mode')
+
+    module.exit_json(change=True, msg=echo_message)
+
+if __name__ == '__main__':
+    main()
