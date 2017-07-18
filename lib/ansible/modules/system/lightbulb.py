0d36ddfa288d49c2b522729b8374b2254539a588
fostimus <derekfoster94@gmail.com>
2017-07-18 10:24:35 -0400

    debugger and docs

--- lib/ansible/modules/system/lightbulb.py
+++ lib/ansible/modules/system/lightbulb.py
@@ -1,12 +1,40 @@
 #!/usr/bin/python
-import httplib
-import urllib
- 
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['stableinterface'],
+                    'supported_by': 'core'}
+
+
+DOCUMENTATION = '''
+---
+module: lightbulb
+author: "Derek Foster (@fostimus)"
+version_added: "2.3.1.0"
+short_description: "Hello World"
+requirements: 
+description:
+    - Prints "hello world to msg"
+notes:
+    - For Windows targets, use the M(win_group) module instead.
+'''
+
+EXAMPLES = '''
+# Example group command from Ansible Playbooks
+- lightbulb:
+'''
+
+import q
+import pdb
+import Cmd
+
 def main():
 	module = AnsibleModule(
-		argument_spec = dict(		)
+		argument_spec = dict()
 	)
-	module.exit_json(changed=True, msg="hello world")
+	foo = "hello world"
+	q(foo)
+	pdb.set_trace()
+	module.exit_json(changed=True, msg=foo)
 
 from ansible.module_utils.basic import AnsibleModule
 if __name__ == '__main__':
