0b3c04db46e0a670fb3267a1e2d3b2a03a0dae38
Nathan Love <nlove@localhost.localdomain>
2017-07-18 15:28:20 -0400

    fix unit tests

--- test/units/modules/system/test_lightbulb.py
+++ test/units/modules/system/test_lightbulb.py
@@ -2,7 +2,7 @@ from ansible.compat.tests import unittest
 from ansible.modules.system.lightbulb import add_things
 
 
-class LightbulbTestCase(unittest.TesCase):
+class LightbulbTestCase(unittest.TestCase):
 
     def test_add_things(self):
         out = add_things(1,2)
