e158b12e69a79fb48ce865ee33a15fa898a1a3bf
quantumstring <quantumstring42@gmail.com>
2017-07-18 09:23:44 -0500

    We like ansible modules

--- lib/ansible/modules/system/lightbulb.py
+++ lib/ansible/modules/system/lightbulb.py
@@ -1,1 +1,15 @@
-print("hello world")
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            name=dict(required=True)
+        )
+    )
+
+    name = module.params["name"]
+
+    print("hello world")
+
+    module.exit_json(changed=False, name=name)
+
+if __name__ == '__main__':
+    main()
