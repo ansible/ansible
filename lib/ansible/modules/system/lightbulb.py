90bb8ee218982137a4f34001faf5bbd8503fc2cb
Josh <jspringe@redhat.com>
2017-07-18 09:44:25 -0400

    Converted to a module

--- lib/ansible/modules/system/lightbulb.py
+++ lib/ansible/modules/system/lightbulb.py
@@ -1,1 +1,13 @@
-hello world
+#!/usr/bin/python
+
+from ansible.module_utils.basic import *
+
+def main():
+
+    module = roflcopter(argument_spec={})
+    response = {"hello": "world"}
+    module.exit_json(changed=False, meta=response)
+
+
+if __name__ == '__main__':  
+    main()
