72fb63f41985e7905049df837c9214b5b9364d51
Nathan Love <nlove@redhat.com>
2017-07-18 11:26:37 -0400

    add documentation

--- lib/ansible/modules/system/lightbulb.py
+++ lib/ansible/modules/system/lightbulb.py
@@ -1,1 +1,36 @@
-hello world
+#!/usr/bin/env python
+from ansible.module_utils.basic import AnsibleModule
+import logging
+logging.basicConfig(filename='example.log',level=logging.DEBUG)
+#!/usr/bin/python
+ANSIBLE_METADATA = {
+            'metadata_version': '1.0',
+                'supported_by': 'community',
+                    'status': ['preview', 'deprecated']
+                    }
+
+DOCUMENTATION = '''
+---
+module: modulename
+short_description: This is a sentence describing the module
+short_description: short descriptio
+description: description
+version_added: 3.4
+author: nlove
+options:
+    foo:
+        description: foo
+        required: false
+        default: bar
+'''
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            foo = dict(required=False, default='bar')
+        )
+    )
+    logging.debug(module.params['foo'])
+    module.exit_json(changed=True, inputs=module.params)
+
+if __name__ == '__main__':
+    main()
