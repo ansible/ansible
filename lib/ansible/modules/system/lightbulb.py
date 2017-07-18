098df9157ca02a19283d525faf822779e14e3f19
quantumstring <quantumstring42@gmail.com>
2017-07-18 10:59:30 -0500

    Mo compliance

--- lib/ansible/modules/system/lightbulb.py
+++ lib/ansible/modules/system/lightbulb.py
@@ -1,3 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2017, quantumstring (devrandom@devnull.com)
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+
 DOCUMENTATION = '''
 ---
 module: python
@@ -13,6 +38,11 @@ options:
         required: true
         description:
             - Name of the bulb type
+'''
+
+EXAMPLES = '''
+- lightbulb:
+    name: led
 '''
 
 def main():
