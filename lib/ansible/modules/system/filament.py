63e0721308759115c686417c6df2b2419950630e
Roy Williams <roywilli@roywilli.redhat.com>
2017-07-18 11:34:34 -0400

    Added header and metadata.

--- lib/ansible/modules/system/filament.py
+++ lib/ansible/modules/system/filament.py
@@ -1,4 +1,28 @@
-#!/usr/bin/python
+#!/usr/bin/python  
+# -*- coding: utf-8 -*-
+
+# (c) 2017, Roy Williams <roywilliams@redhat.com>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['stableinterface'],
+                    'supported_by': 'core'}
+
 
 DOCUMENTATION = '''
 ---
@@ -16,11 +40,13 @@ import epdb
 
 from ansible.module_utils.basic import AnsibleModule
 
+
+
 def main():
     module = AnsibleModule(
-        argument_spec = dict(
-            state     = dict(default='present', choices=['present', 'absent']),
-            foo       = dict(default='bar'),
+        argument_spec=dict(
+            state=dict(default='present', choices=['present', 'absent']),
+            foo=dict(default='bar'),
         ),
         supports_check_mode=True
     )
