a4a6499b234252f75641e439f89abbc05bde9714
mbach04 <mbach04@gmail.com>
2017-07-18 09:55:35 -0400

    hello roflcopter

---
+++ lib/ansible/modules/system/lightbulb.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from ansible.module_utils.basic import *
+
+def main():
+    print "hello roflcopter"
+    module = roflcopter(argument_spec={})
+    response = {"hello": "world"}
+    module.exit_json(changed=False, meta=response)
+
+
+if __name__ == '__main__':
+    main()
