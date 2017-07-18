5016198b824ff92cb002270a75fdddd15b6742ee
David Igou <igou.david@gmail.com>
2017-07-18 12:49:57 -0400

    Module Fixes

--- lib/ansible/modules/system/lightbulb.py
+++ lib/ansible/modules/system/lightbulb.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python
-# Hello World example module by David Igou July 2017
+# -*- coding: utf-8 -*-
+
+# (c) 2017, David Igou (digou@redhat.com)
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+
 
 ANSIBLE_METADATA = {
     'metadata_version': '1.0',
@@ -11,6 +32,10 @@ DOCUMENTATION = '''
 ---
 module: lightbulb
 description: This module does nothing then exits with msg="HELLO WORLD"
+short_description: Says Hello World
+version_added: "1.0"
+author: "David Igou (@digou)"
+
 '''
 
 
@@ -22,9 +47,9 @@ EXAMPLES = '''
 RETURN = '''
 msg:
    description: simple Hello World message
+   type: string
+   returned: success
 '''
-
-
 from ansible.module_utils.basic import *
 
 
@@ -32,7 +57,5 @@ def main():
         module = AnsibleModule(argument_spec=dict())
         module.exit_json(changed=False, msg="HELLO WORLD")
 
-
-from ansible.module_utils.basic import AnsibleModule
 if __name__ == '__main__':
         main()
