0b5b5ec50a51740288ce2d9cf826e262135bd291
David Newswanger <gamma.dave@gmail.com>
2017-07-19 12:37:20 -0400

    fix unicode errors in iosxr

--- lib/ansible/plugins/action/iosxr_config.py
+++ lib/ansible/plugins/action/iosxr_config.py
@@ -25,7 +25,7 @@ import time
 import glob
 
 from ansible.plugins.action.iosxr import ActionModule as _ActionModule
-from ansible.module_utils._text import to_text
+from ansible.module_utils._text import to_text, to_bytes
 from ansible.module_utils.six.moves.urllib.parse import urlsplit
 from ansible.utils.vars import merge_hash
 
@@ -74,7 +74,7 @@ class ActionModule(_ActionModule):
             os.remove(fn)
         tstamp = time.strftime("%Y-%m-%d@%H:%M:%S", time.localtime(time.time()))
         filename = '%s/%s_config.%s' % (backup_path, host, tstamp)
-        open(filename, 'w').write(contents)
+        open(filename, 'w').write(to_bytes(contents))
         return filename
 
     def _handle_template(self):
