adfb3dc96dc77ffc59cb8b4709c5a7cdfd87451d
David Newswanger <gamma.dave@gmail.com>
2017-07-19 18:06:11 -0400

    Fix #26918: IOS TypeError (#26999)
    Fix IOS TypeError
    
    * if flags are None, then ' '.join(flags) fails
    * fixed get_defaults_flag so that it returns a list, and ignores lines with white space
    
    Fixes #26918

--- lib/ansible/modules/network/ios/ios_config.py
+++ lib/ansible/modules/network/ios/ios_config.py
@@ -343,7 +343,7 @@ def get_running_config(module, current_config=None):
         if not module.params['defaults'] and current_config:
             contents, banners = extract_banners(current_config.config_text)
         else:
-            flags = get_defaults_flag(module) if module.params['defaults'] else None
+            flags = get_defaults_flag(module) if module.params['defaults'] else []
             contents = get_config(module, flags=flags)
     contents, banners = extract_banners(contents)
     return NetworkConfig(indent=1, contents=contents), banners
