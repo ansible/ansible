c7e841e0e4b4db9a5c0e0bab685b825196bbf36d
Brian Coca <brian.coca+git@gmail.com>
2017-07-19 20:23:13 -0400

    better reading of 'facts'

--- lib/ansible/plugins/action/package.py
+++ lib/ansible/plugins/action/package.py
@@ -52,8 +52,7 @@ class ActionModule(ActionBase):
         if module == 'auto':
             facts = self._execute_module(module_name='setup', module_args=dict(filter='ansible_pkg_mgr', gather_subset='!all'), task_vars=task_vars)
             display.debug("Facts %s" % facts)
-            if 'ansible_facts' in facts and 'ansible_pkg_mgr' in facts['ansible_facts']:
-                module = getattr(facts['ansible_facts'], 'ansible_pkg_mgr', 'auto')
+            module = facts.get('ansible_facts', {}).get('ansible_pkg_mgr', 'auto')
 
         if module != 'auto':
 
