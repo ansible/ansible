ed9207efe819ad3cff420b3aaa49f47f08198a38
Rahul Gupta <ragupta4@cisco.com>
2017-07-20 20:25:48 +0530

    Adds module to configure dns on cisco ucs server
    Signed-off-by: Rahul Gupta <ragupta4@cisco.com>

---
+++ lib/ansible/module_utils/cisco_ucs.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+
+try:
+    import ucsmsdk
+    HAS_UCSMSDK = True
+except:
+    HAS_UCSMSDK = False
+
+try:
+    import ucsm_apis
+    HAS_UCSMAPI = True
+except:
+    HAS_UCSMAPI = False
+
+class UcsConnection():
+
+    @staticmethod
+    def is_login_param(param):
+        return param in ["ucs_ip", "ucs_username", "ucs_password",
+                         "ucs_port", "ucs_secure", "ucs_proxy", "ucs_server"]
+
+    def __init__(self, module):
+        if HAS_UCSMSDK is False:
+            results = {}
+            results["msg"] = "ucsmsdk is not installed"
+            module.fail_json(**results)
+        if HAS_UCSMAPI is False:
+            results = {}
+            results["msg"] = "ucsm_apis is not installed"
+            module.fail_json(**results)
+        self.module = module
+        self.handle = None
+
+    def login(self):
+        ansible = self.module.params
+        server = ansible.get('ucs_server')
+        if server:
+            return server
+
+        from ucsmsdk.ucshandle import UcsHandle
+        results = {}
+        try:
+            server = UcsHandle(ip=ansible["ucs_ip"],
+                               username=ansible["ucs_username"],
+                               password=ansible["ucs_password"],
+                               port=ansible["ucs_port"],
+                               secure=ansible["ucs_secure"],
+                               proxy=ansible["ucs_proxy"])
+            server.login()
+        except Exception as e:
+            results["msg"] = str(e)
+            self.module.fail_json(**results)
+        self.handle = server
+        return server
+
+    def logout(self):
+        server = self.module.params.get('ucs_server')
+        if server:
+            # we used a pre-existing handle from a task.
+            # do not logout
+            return False
+
+        if self.handle:
+            self.handle.logout()
+            return True
+        return False
---
+++ lib/ansible/modules/remote_management/cisco/cisco_ucs_dns.py
@@ -0,0 +1,171 @@
+#!/usr/bin/python
+
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from ansible.module_utils.basic import *
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+
+DOCUMENTATION = '''
+---
+module: cisco_ucs_dns
+short_description: Configures dns on a cisco ucs server.
+version_added: "0.9.0.0"
+description:
+   -  Configures dns on a cisco ucs server.
+options:
+    state:
+        description:
+         - if C(present), will perform create/add/enable operation
+         - if C(absent), will perform delete/remove/disable operation
+        required: false
+        choices: ['present', 'absent']
+        default: "present"
+    name:
+        version_added: "1.0(1e)"
+        description: ip address of dns server
+        required: true
+    descr:
+        version_added: "1.0(1e)"
+        description: description of server
+        required: false
+
+requirements:
+    - 'ucsmsdk'
+    - 'ucsm_apis'
+    - 'python2 >= 2.7.9 or python3 >= 3.2'
+    - 'openssl version >= 1.0.1'
+
+author: "Cisco Systems Inc(ucs-python@cisco.com)"
+'''
+
+
+EXAMPLES = '''
+- name:
+  cisco_ucs_dns:
+    name: "10.10.10.10"
+    descr: "description"
+    state: "present"
+    ucs_ip: "192.168.1.1"
+    ucs_username: "admin"
+    ucs_password: "password"
+'''
+
+
+def _argument_mo():
+    return dict(
+                name=dict(required=True, type='str'),
+                descr=dict(type='str'),
+    )
+
+
+def _argument_custom():
+    return dict(
+        state=dict(default="present",
+                   choices=['present', 'absent'],
+                   type='str'),
+    )
+
+
+def _argument_connection():
+    return  dict(
+        # UcsHandle
+        ucs_server=dict(type='dict'),
+
+        # Ucs server credentials
+        ucs_ip=dict(type='str'),
+        ucs_username=dict(default="admin", type='str'),
+        ucs_password=dict(type='str', no_log=True),
+        ucs_port=dict(default=None),
+        ucs_secure=dict(default=None),
+        ucs_proxy=dict(default=None)
+    )
+
+
+def _ansible_module_create():
+    argument_spec = dict()
+    argument_spec.update(_argument_mo())
+    argument_spec.update(_argument_custom())
+    argument_spec.update(_argument_connection())
+
+    return AnsibleModule(argument_spec,
+                         supports_check_mode=True)
+
+
+def _get_mo_params(params):
+    from ansible.module_utils.cisco_ucs import UcsConnection
+    args = {}
+    for key in _argument_mo():
+        if params.get(key) is None:
+            continue
+        args[key] = params.get(key)
+    return args
+
+
+def setup_dns(server, module):
+    from ucsm_apis.admin.dns import dns_server_add
+    from ucsm_apis.admin.dns import dns_server_remove
+    from ucsm_apis.admin.dns import dns_server_exists
+
+    ansible = module.params
+    args_mo  =  _get_mo_params(ansible)
+    exists, mo = dns_server_exists(handle=server, **args_mo)
+
+    if ansible["state"] == "present":
+        if module.check_mode or exists:
+            return not exists
+        dns_server_add(handle=server, **args_mo)
+    else:
+        if module.check_mode or not exists:
+            return exists
+        dns_server_remove(server, mo.name)
+    return True
+
+
+def setup(server, module):
+    result = {}
+    err = False
+
+    try:
+        result["changed"] = setup_dns(server, module)
+    except Exception as e:
+        err = True
+        result["msg"] = "setup error: %s " % str(e)
+        result["changed"] = False
+
+    return result, err
+
+
+def main():
+    from ansible.module_utils.cisco_ucs import UcsConnection
+
+    module = _ansible_module_create()
+    conn = UcsConnection(module)
+    server = conn.login()
+    result, err = setup(server, module)
+    conn.logout()
+    if err:
+        module.fail_json(**result)
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
+
