ansible/ansible

commit 5b109506c4a4d1ecfdc9d77184ed1175f4275c95

[cloud] Rename ec2_facts to ec2_metadata_facts (#26293)
- Update CHANGELOG with ec2_facts deprecation notice

lib/ansible/modules/cloud/amazon/_ec2_facts.py
@@ -1,0 +1,1 @@
+ec2_metadata_facts.py

lib/ansible/modules/cloud/amazon/ec2_metadata_facts.py
@@ -23,7 +23,7 @@ ANSIBLE_METADATA = {'metadata_version': '1.0',
 
 DOCUMENTATION = '''
 ---
-module: ec2_facts
+module: ec2_metadata_facts
 short_description: Gathers facts (instance metadata) about remote hosts within ec2
 version_added: "1.0"
 author:
@@ -34,12 +34,12 @@ description:
       http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html.
       The module must be called from within the EC2 instance itself.
 notes:
-    - Parameters to filter on ec2_facts may be added later.
+    - Parameters to filter on ec2_metadata_facts may be added later.
 '''
 
 EXAMPLES = '''
-# Gather EC2 facts
-- ec2_facts:
+# Gather EC2 metadata facts
+- ec2_metadata_facts:
 
 - debug:
     msg: "This instance is a t1.micro"
@@ -539,10 +539,13 @@ def main():
         supports_check_mode=True,
     )
 
-    ec2_facts = Ec2Metadata(module).run()
-    ec2_facts_result = dict(changed=False, ansible_facts=ec2_facts)
+    if module._name == 'ec2_facts':
+        module.deprecate("The 'ec2_facts' module is being renamed 'ec2_metadata_facts'", version=2.7)
 
-    module.exit_json(**ec2_facts_result)
+    ec2_metadata_facts = Ec2Metadata(module).run()
+    ec2_metadata_facts_result = dict(changed=False, ansible_facts=ec2_metadata_facts)
+
+    module.exit_json(**ec2_metadata_facts_result)
 
 
 if __name__ == '__main__':

commit 573d6f14a044c82bc2d4cfba61d74c22b07f5a6d

deprecate ec2_facts instead of making it an alias
Fixes: bug #27105
Introduced by https://github.com/ansible/ansible/commit/5b109506c4a4d1ecfdc9d77184ed1175f4275c95

lib/ansible/modules/cloud/amazon/_ec2_facts.py
@@ -1,1 +1,43 @@
-ec2_metadata_facts.py
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['deprecated'],
+                    'supported_by': 'curated'}
+
+DOCUMENTATION = '''
+---
+module: ec2_facts
+deprecated: Use ec2_metadata_facts instead.
+short_description: Gathers facts (instance metadata) about remote hosts within ec2
+version_added: "1.0"
+author:
+    - Silviu Dicu (@silviud)
+    - Vinay Dandekar (@roadmapper)
+description:
+    - This module fetches data from the instance metadata endpoint in ec2 as per
+      http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html.
+      The module must be called from within the EC2 instance itself.
+notes:
+    - Parameters to filter on ec2_metadata_facts may be added later.
+'''
+
+from ec2_metadata_facts import *
+
+if __name__ == '__main__':
+    main()
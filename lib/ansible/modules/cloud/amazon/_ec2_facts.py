573d6f14a044c82bc2d4cfba61d74c22b07f5a6d
Frederic Lepied <frederic.lepied@redhat.com>
2017-07-20 15:19:22 +0200

    deprecate ec2_facts instead of making it an alias
    Fixes: bug #27105
    Introduced by https://github.com/ansible/ansible/commit/5b109506c4a4d1ecfdc9d77184ed1175f4275c95

--- lib/ansible/modules/cloud/amazon/_ec2_facts.py
+++ lib/ansible/modules/cloud/amazon/_ec2_facts.py
@@ -1,1 +1,43 @@
-ec2_metadata_facts.py
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['deprecated'],
+                    'supported_by': 'curated'}
+
+DOCUMENTATION = '''
+---
+module: ec2_facts
+deprecated: Use ec2_metadata_facts instead.
+short_description: Gathers facts (instance metadata) about remote hosts within ec2
+version_added: "1.0"
+author:
+    - Silviu Dicu (@silviud)
+    - Vinay Dandekar (@roadmapper)
+description:
+    - This module fetches data from the instance metadata endpoint in ec2 as per
+      http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html.
+      The module must be called from within the EC2 instance itself.
+notes:
+    - Parameters to filter on ec2_metadata_facts may be added later.
+'''
+
+from ec2_metadata_facts import *
+
+if __name__ == '__main__':
+    main()
