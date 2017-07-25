1beafd426aa404dd7eae191b12893cbaa1559987
Adrian Likins <alikins@redhat.com>
2017-07-25 13:48:40 -0400

    Add 2.0-2.3 facts api compat (ansible_facts(), get_all_facts())
    These are intended to provide compatibilty for modules that
    use 'ansible.module_utils.facts.ansible_facts' and
    'ansible.module_utils.facts.get_all_facts' from 2.0-2.3 facts
    API.
    
    Fixes #25686

---
+++ lib/ansible/module_utils/facts/__init__.py
@@ -0,0 +1,21 @@
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+# import from the compat api because 2.0-2.3 had a module_utils.facts.ansible_facts
+# and get_all_facts in top level namespace
+from ansible.module_utils.facts.compat import ansible_facts, get_all_facts   # noqa
