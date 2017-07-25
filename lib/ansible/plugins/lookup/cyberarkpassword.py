ff4b51134718c170f692eb431f1e69bf998a9ce0
CyberArk BizDev <edward.nunez@cyberark.com>
2017-07-25 11:17:40 -0700

    Pep8 - Empty line with whitespaces

--- lib/ansible/plugins/lookup/cyberarkpassword.py
+++ lib/ansible/plugins/lookup/cyberarkpassword.py
@@ -122,7 +122,6 @@ class LookupModule(LookupBase):
     For Ansible on windows, please change the -parameters (-p, -d, and -o) to /parameters (/p, /d, and /o) and change the location of CLIPasswordSDK.exe
 
     cyberarkpassword lookup can not be used as a source for loop
-    
     """
 
     def run(self, terms, variables=None, **kwargs):
