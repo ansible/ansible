47169bfd0745ea3e6b0f718661f4957f61a2cf21
Toshio Kuratomi <a.badger@gmail.com>
2017-07-24 22:14:19 -0700

    Remove the error checking from apply_patch()
    The error checking would itself cause a traceback.  The load_config()
    function that we'd need to check for errors from only returns None so
    there's no way to check for errors via the return value.  In the future
    someone could rewrite the load_config function to return useful
    information and restore the error checking but for now this is better as
    it won't traceback on success and it will let us turn on static analysis
    of undefined variables
    
    Fixes #27255
    References #27254

--- lib/ansible/modules/network/nxos/nxos_smu.py
+++ lib/ansible/modules/network/nxos/nxos_smu.py
@@ -114,8 +114,6 @@ def apply_patch(module, commands):
     for command in commands:
         load_config(module, [command])
         time.sleep(5)
-        if 'failed' in response:
-            module.fail_json(msg="Operation failed!", response=response)
 
 
 def get_commands(module, pkg, file_system):
