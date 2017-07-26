bee4cc2ba82cfbe4501fce1ba405451122a64841
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-07-26 12:30:55 +0100

    Corrects property used to set ACL token in python-consul library.

--- lib/ansible/modules/clustering/consul_acl.py
+++ lib/ansible/modules/clustering/consul_acl.py
@@ -280,7 +280,7 @@ def create_acl(consul_client, configuration):
     """
     rules_as_hcl = encode_rules_as_hcl_string(configuration.rules) if len(configuration.rules) > 0 else None
     token = consul_client.acl.create(
-        name=configuration.name, type=configuration.token_type, rules=rules_as_hcl, token=configuration.token)
+        name=configuration.name, type=configuration.token_type, rules=rules_as_hcl, acl_id=configuration.token)
     rules = configuration.rules
     return Output(changed=True, token=token, rules=rules, operation=CREATE_OPERATION)
 
