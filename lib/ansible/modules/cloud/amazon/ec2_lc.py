05ffd05dbd99eb17ea19edfcf8e2b561b7da712e
Willem van Ketwich <willvk@gmail.com>
2017-07-28 05:18:36 +1000

    fixed bdm check

--- lib/ansible/modules/cloud/amazon/ec2_lc.py
+++ lib/ansible/modules/cloud/amazon/ec2_lc.py
@@ -305,7 +305,7 @@ def create_launch_config(connection, module):
     if classic_link_vpc_security_groups is not None:
         launch_config['ClassicLinkVPCSecurityGroups'] = classic_link_vpc_security_groups
 
-    if not block_device_mapping:
+    if block_device_mapping:
         launch_config['BlockDeviceMappings'] = [block_device_mapping]
 
     if instance_profile_name is not None:
