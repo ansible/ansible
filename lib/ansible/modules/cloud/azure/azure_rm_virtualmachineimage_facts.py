4d92fcbb1aad775de8cf09b1fc4cfaa4aea85ec8
Toshio Kuratomi <a.badger@gmail.com>
2017-07-27 02:22:17 -0700

    Update conventions in azure modules
    * Remove wildcard imports
    * Update GPL header to the one-line form
    * Add from __future__ imports

--- lib/ansible/modules/cloud/azure/azure_rm_virtualmachineimage_facts.py
+++ lib/ansible/modules/cloud/azure/azure_rm_virtualmachineimage_facts.py
@@ -3,21 +3,10 @@
 # Copyright (c) 2016 Matt Davis, <mdavis@ansible.com>
 #                    Chris Houseknecht, <house@redhat.com>
 #
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
 
 
 ANSIBLE_METADATA = {'metadata_version': '1.0',
@@ -112,9 +101,6 @@ azure_vmimages:
     example: []
 '''
 
-from ansible.module_utils.basic import *
-from ansible.module_utils.azure_rm_common import *
-
 try:
     from msrestazure.azure_exceptions import CloudError
     from azure.common import AzureMissingResourceHttpError, AzureHttpError
@@ -122,7 +108,11 @@ except:
     # This is handled in azure_rm_common
     pass
 
+from ansible.module_utils.azure_rm_common import AzureRMModuleBase
+
+
 AZURE_ENUM_MODULES = ['azure.mgmt.compute.models.compute_management_client_enums']
+
 
 class AzureRMVirtualMachineImageFacts(AzureRMModuleBase):
 
@@ -241,4 +231,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
