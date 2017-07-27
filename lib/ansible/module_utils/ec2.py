43c8b9e6956a0b7c960c1b2dfef62575b60a6a05
Matt Zizzi <mzizzi@bandwidth.com>
2017-07-27 14:25:44 -0400

    ansible_dict_to_boto3_tag_list cast tag values to string

--- lib/ansible/module_utils/ec2.py
+++ lib/ansible/module_utils/ec2.py
@@ -495,7 +495,7 @@ def ansible_dict_to_boto3_tag_list(tags_dict, tag_name_key_name='Key', tag_value
 
     tags_list = []
     for k, v in tags_dict.items():
-        tags_list.append({tag_name_key_name: k, tag_value_key_name: v})
+        tags_list.append({tag_name_key_name: k, tag_value_key_name: str(v)})
 
     return tags_list
 
