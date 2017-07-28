c594f1e1c9c8aa7014641ce0835bd80b0c2c5f67
David Newswanger <gamma.dave@gmail.com>
2017-07-28 12:20:09 -0400

    fixed nontype error (#27428)

--- lib/ansible/modules/network/ios/ios_logging.py
+++ lib/ansible/modules/network/ios/ios_logging.py
@@ -232,17 +232,17 @@ def map_config_to_obj(module):
 
     for line in data.split('\n'):
         match = re.search(r'logging (\S+)', line, re.M)
+        if match:
+            if match.group(1) in dest_group:
+                dest = match.group(1)
 
-        if match.group(1) in dest_group:
-            dest = match.group(1)
-        else:
-            pass
-
-        obj.append({'dest': dest,
+                obj.append({
+                    'dest': dest,
                     'name': parse_name(line, dest),
                     'size': parse_size(line, dest),
                     'facility': parse_facility(line),
-                    'level': parse_level(line, dest)})
+                    'level': parse_level(line, dest)
+                })
 
     return obj
 
