efb9cc52fc9d23828ab40b0e92e2f404c2356573
Simon Dodsley <simon@purestorage.com>
2017-07-28 06:24:31 -0400

    Add Pure Storage Volume module (#26357)
    Update documentation to fix typos and define the extend feature
    Force size to be a required variable on create as requested.
    Change type to bool

--- lib/ansible/modules/storage/purestorage/purefa_volume.py
+++ lib/ansible/modules/storage/purestorage/purefa_volume.py
@@ -2,12 +2,31 @@
 # -*- coding: utf-8 -*-
 
 # (c) 2017, Simon Dodsley (simon@purestorage.com)
+<<<<<<< HEAD
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-ANSIBLE_METADATA = {'metadata_version': '1.1',
+=======
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
+>>>>>>> Add Pure Storage Volume module (#26357)
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
@@ -96,14 +115,16 @@ EXAMPLES = r'''
 RETURN = r'''
 '''
 
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.pure import get_system, purefa_argument_spec
+
+
+HAS_PURESTORAGE = True
 try:
     from purestorage import purestorage
     HAS_PURESTORAGE = True
 except ImportError:
     HAS_PURESTORAGE = False
-
-from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.pure import get_system, purefa_argument_spec
 
 
 def human_to_bytes(size):
