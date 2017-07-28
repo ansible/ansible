9badda48acfce4146f411bc7972e6cd7e826adc8
Giovanni Sciortino <giovannibattistasciortino@gmail.com>
2017-07-28 15:05:02 +0000

    add_hosts.py: add note in documentation that 'hosts' alias of name parameter is only available on >=2.4

--- lib/ansible/modules/inventory/add_host.py
+++ lib/ansible/modules/inventory/add_host.py
@@ -44,6 +44,7 @@ notes:
     - This module bypasses the play host loop and only runs once for all the hosts in the play, if you need it
       to iterate use a with\_ directive.
     - This module is also supported for Windows targets.
+    - The alias 'host' of the parameter 'name' is only available on >=2.4
 author:
     - "Ansible Core Team"
     - "Seth Vidal"
