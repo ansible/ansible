754ffd65fb8f54ab57693fcd64bfefb341406bac
Nikita Chernyi <developer.nikus@gmail.com>
2017-07-31 20:57:40 +0300

    Added original skippy.py, moved plugin to full_skip.py

--- lib/ansible/plugins/callback/full_skip.py
+++ lib/ansible/plugins/callback/full_skip.py
@@ -21,7 +21,6 @@ __metaclass__ = type
 
 from ansible.plugins.callback.default import CallbackModule as CallbackModule_default
 
-
 class CallbackModule(CallbackModule_default):
 
     '''
@@ -31,10 +30,42 @@ class CallbackModule(CallbackModule_default):
 
     CALLBACK_VERSION = 2.0
     CALLBACK_TYPE = 'stdout'
-    CALLBACK_NAME = 'skippy'
+    CALLBACK_NAME = 'full_skip'
 
     def v2_runner_on_skipped(self, result):
-        pass
+        self.outlines = []
+
+    def v2_playbook_item_on_skipped(self, result):
+        self.outlines = []
 
     def v2_runner_item_on_skipped(self, result):
-        pass
+        self.outlines = []
+
+    def v2_runner_on_failed(self, result, ignore_errors=False):
+        self.display()
+        super(CallbackModule, self).v2_runner_on_failed(result, ignore_errors)
+
+    def v2_playbook_on_task_start(self, task, is_conditional):
+        self.outlines = []
+        self.outlines.append("TASK [%s]" % task.get_name().strip())
+        if self._display.verbosity >= 2:
+            path = task.get_path()
+            if path:
+                self.outlines.append("task path: %s" % path)
+
+    def v2_playbook_item_on_ok(self, result):
+        self.display()
+        super(CallbackModule, self).v2_playbook_item_on_ok(result)
+
+    def v2_runner_on_ok(self, result):
+        self.display()
+        super(CallbackModule, self).v2_runner_on_ok(result)
+
+    def display(self):
+        if len(self.outlines) == 0:
+            return
+        (first, rest) = self.outlines[0], self.outlines[1:]
+        self._display.banner(first)
+        for line in rest:
+                self._display.display(line)
+        self.outlines = []
