5af42713d16ffcd6de4ea5acfe241561c73ed6fc
Toshio Kuratomi <a.badger@gmail.com>
2017-08-02 09:17:54 -0700

    Add missing copyright header in modules cleaned up yesterday

--- lib/ansible/modules/packaging/os/package.py
+++ lib/ansible/modules/packaging/os/package.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# (c) 2015, Ansible, inc
+# (c) 2015, Ansible Project
 #
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
