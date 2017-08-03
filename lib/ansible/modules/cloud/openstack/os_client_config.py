e4867f0baf0b09b322c01ff9a4072736b04d1a06
Pierre-Louis Bonicoli <pierre-louis.bonicoli@libregerbil.fr>
2017-08-03 18:14:20 +0200

    remove wildcards and add boilerplate

--- lib/ansible/modules/cloud/openstack/os_client_config.py
+++ lib/ansible/modules/cloud/openstack/os_client_config.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python
 
 # Copyright (c) 2015 Hewlett-Packard Development Company, L.P.
-#
-# This module is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This software is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
 
 ANSIBLE_METADATA = {'metadata_version': '1.0',
                     'status': ['preview'],
@@ -54,6 +46,7 @@ EXAMPLES = '''
       - mordred
 '''
 
+from ansible.module_utils.basic import AnsibleModule
 
 try:
     import os_client_config
@@ -84,8 +77,6 @@ def main():
     except exceptions.OpenStackConfigException as e:
         module.fail_json(msg=str(e))
 
-# import module snippets
-from ansible.module_utils.basic import *
 
 if __name__ == "__main__":
     main()
