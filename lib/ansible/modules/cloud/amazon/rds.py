2cdc54aa1a4465258d2869e47cf2aa1e3c6e0b50
Will Thames <will@thames.id.au>
2017-08-04 07:15:18 +1000

    Add oracle-se2 database support (#23482)
    Fixes ansible-modules-core#4808

--- lib/ansible/modules/cloud/amazon/rds.py
+++ lib/ansible/modules/cloud/amazon/rds.py
@@ -50,7 +50,8 @@ options:
       - mariadb was added in version 2.2
     required: false
     default: null
-    choices: ['mariadb', 'MySQL', 'oracle-se1', 'oracle-se', 'oracle-ee', 'sqlserver-ee', 'sqlserver-se', 'sqlserver-ex', 'sqlserver-web', 'postgres', 'aurora']
+    choices: ['mariadb', 'MySQL', 'oracle-se1', 'oracle-se2', 'oracle-se', 'oracle-ee',
+              'sqlserver-ee', 'sqlserver-se', 'sqlserver-ex', 'sqlserver-web', 'postgres', 'aurora']
   size:
     description:
       - Size in gigabytes of the initial storage for the DB instance. Used only when command=create or command=modify.
@@ -1316,7 +1317,7 @@ def main():
         command=dict(choices=['create', 'replicate', 'delete', 'facts', 'modify', 'promote', 'snapshot', 'reboot', 'restore'], required=True),
         instance_name=dict(required=False),
         source_instance=dict(required=False),
-        db_engine=dict(choices=['mariadb', 'MySQL', 'oracle-se1', 'oracle-se', 'oracle-ee', 'sqlserver-ee', 'sqlserver-se', 'sqlserver-ex',
+        db_engine=dict(choices=['mariadb', 'MySQL', 'oracle-se1', 'oracle-se2', 'oracle-se', 'oracle-ee', 'sqlserver-ee', 'sqlserver-se', 'sqlserver-ex',
                                 'sqlserver-web', 'postgres', 'aurora'], required=False),
         size=dict(required=False),
         instance_type=dict(aliases=['type'], required=False),
