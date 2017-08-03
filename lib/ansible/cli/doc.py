b874ffe4cbf182cf8a3f1fc6111e9edc83677105
Sam Doran <sdoran@redhat.com>
2017-08-03 10:38:45 -0400

    Properly include aliases in ansible-doc output
    Use correct variable name for storing aliases and only print them out if the list isn't empty.
    
    Fixes #24498

--- lib/ansible/cli/doc.py
+++ lib/ansible/cli/doc.py
@@ -312,7 +312,8 @@ class DocCLI(CLI):
 
             aliases = ''
             if 'aliases' in opt:
-                choices = "(Aliases: " + ", ".join(str(i) for i in opt['aliases']) + ")"
+                if len(opt['aliases']) > 0:
+                    aliases = "(Aliases: " + ", ".join(str(i) for i in opt['aliases']) + ")"
                 del opt['aliases']
             choices = ''
             if 'choices' in opt:
