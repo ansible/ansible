8e8d4df340722efcf70095f1746c8fc5ad700bde
Alex Bush <bushnoh@users.noreply.github.com>
2017-08-04 06:26:20 +0100

    #26903 Invalid JSON-RPC request (#26906)

--- lib/ansible/modules/identity/ipa/ipa_sudorule.py
+++ lib/ansible/modules/identity/ipa/ipa_sudorule.py
@@ -201,7 +201,7 @@ class SudoRuleIPAClient(IPAClient):
         return self.sudorule_remove_host(name=name, item={'hostgroup': item})
 
     def sudorule_add_allow_command(self, name, item):
-        return self._post_json(method='sudorule_add_allow_command', name=name, item=item)
+        return self._post_json(method='sudorule_add_allow_command', name=name, item={'sudocmd': item})
 
     def sudorule_remove_allow_command(self, name, item):
         return self._post_json(method='sudorule_remove_allow_command', name=name, item=item)
