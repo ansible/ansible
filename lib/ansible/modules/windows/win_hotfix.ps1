d2bd0412dcc68e4e0511519c49e639aad5e5c8b4
Jordan Borean <jborean93@gmail.com>
2017-08-07 12:07:08 +1000

    changed the test hotfix so we can run tests in shippable

--- lib/ansible/modules/windows/win_hotfix.ps1
+++ lib/ansible/modules/windows/win_hotfix.ps1
@@ -4,8 +4,7 @@
 # Copyright (c) 2017 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-# WANT_JSON
-# POWERSHELL_COMMON
+#Requires -Module Ansible.ModuleUtils.Legacy.psm1
 
 $ErrorActionPreference = "Stop"
 
