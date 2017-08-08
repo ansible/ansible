1fb1793c705b5b74738d69e84170c2ec4ad6295f
giovannisciortino <30353557+giovannisciortino@users.noreply.github.com>
2017-08-08 15:48:39 +0200

    helpers.py: Fixes #27334 include empty task file within a 'block' dis… (#27811)
    * helpers.py: Fixes #27334 include empty task file within a 'block' disables the complete block
    
    * include module: added warning if file without tasks is included

--- lib/ansible/playbook/helpers.py
+++ lib/ansible/playbook/helpers.py
@@ -207,7 +207,8 @@ def load_list_of_tasks(ds, play, block=None, role=None, task_include=None, use_h
                     try:
                         data = loader.load_from_file(include_file)
                         if data is None:
-                            return []
+                            display.warning('file %s is empty and had no tasks to include' % include_file)
+                            continue
                         elif not isinstance(data, list):
                             raise AnsibleParserError("included task files must contain a list of tasks", obj=data)
 
