0e85fc6e4899cb1235317ed864c764a7c8a3e808
Matt Zizzi <mzizzi@bandwidth.com>
2017-08-08 19:26:33 -0400

    cloudformation_facts jittered backoff / retries

--- lib/ansible/modules/cloud/amazon/cloudformation_facts.py
+++ lib/ansible/modules/cloud/amazon/cloudformation_facts.py
@@ -152,7 +152,8 @@ try:
 except ImportError:
     HAS_BOTO3 = False
 
-from ansible.module_utils.ec2 import get_aws_connection_info, ec2_argument_spec, boto3_conn, camel_dict_to_snake_dict
+from ansible.module_utils.ec2 import get_aws_connection_info, ec2_argument_spec, boto3_conn, camel_dict_to_snake_dict, \
+    AWSRetry
 from ansible.module_utils.basic import AnsibleModule
 from functools import partial
 import json
@@ -170,6 +171,12 @@ class CloudFormationServiceManager:
             self.client = boto3_conn(module, conn_type='client',
                                      resource='cloudformation', region=region,
                                      endpoint=ec2_url, **aws_connect_kwargs)
+            backoff_wrapper = AWSRetry.jittered_backoff(retries=10, delay=3, max_delay=30)
+            self.client.describe_stacks = backoff_wrapper(self.client.describe_stacks)
+            self.client.list_stack_resources = backoff_wrapper(self.client.list_stack_resources)
+            self.client.describe_stack_events = backoff_wrapper(self.client.describe_stack_events)
+            self.client.get_stack_policy = backoff_wrapper(self.client.get_stack_policy)
+            self.client.get_template = backoff_wrapper(self.client.get_template)
         except botocore.exceptions.NoRegionError:
             self.module.fail_json(msg="Region must be specified as a parameter, in AWS_DEFAULT_REGION environment variable or in boto configuration file")
         except Exception as e:
