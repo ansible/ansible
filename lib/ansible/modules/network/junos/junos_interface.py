9827361b2e2270520d2e4d51153cdf8910af921b
Ganesh B. Nalawade <gnalawad@redhat.com>
2017-08-09 15:51:18 +0530

    junos_interface intent arguments
    *  Add check for intent argument in junos_interface
    *  Integration test for intent arguments

--- lib/ansible/modules/network/junos/junos_interface.py
+++ lib/ansible/modules/network/junos/junos_interface.py
@@ -46,17 +46,17 @@ options:
     choices: ['full', 'half', 'auto']
   tx_rate:
     description:
-      - Transmit rate.
+      - Transmit rate in bits per second (bps).
   rx_rate:
     description:
-      - Receiver rate.
+      - Receiver rate in bits per second (bps).
+  delay:
+    description:
+      - Time in seconds to wait before checking for the operational state on remote
+        device. This wait is applicable for operatinal state argument which are
+        I(enabled), I(tx_rate) and I(rx_rate).
   aggregate:
     description: List of Interfaces definitions.
-  purge:
-    description:
-      - Purge Interfaces not defined in the aggregate parameter.
-        This applies only for logical interface.
-    default: no
   state:
     description:
       - State of the Interface configuration, C(up) means present and
@@ -142,15 +142,19 @@ diff.prepared:
 """
 import collections
 
+from time import sleep
+
 from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.netconf import send_request
+from ansible.module_utils.network_common import conditional
 from ansible.module_utils.junos import junos_argument_spec, check_args
 from ansible.module_utils.junos import load_config, map_params_to_obj, map_obj_to_ele
 from ansible.module_utils.junos import commit_configuration, discard_changes, locked_config, to_param_list
 
 try:
-    from lxml.etree import tostring
+    from lxml.etree import Element, SubElement, tostring
 except ImportError:
-    from xml.etree.ElementTree import tostring
+    from xml.etree.ElementTree import Element, SubElement, tostring
 
 USE_PERSISTENT_CONNECTION = True
 
@@ -182,6 +186,7 @@ def main():
         duplex=dict(choices=['full', 'half', 'auto']),
         tx_rate=dict(),
         rx_rate=dict(),
+        delay=dict(default=10, type='int'),
         state=dict(default='present',
                    choices=['present', 'absent', 'up', 'down']),
         active=dict(default=True, type='bool')
@@ -192,7 +197,6 @@ def main():
 
     argument_spec = dict(
         aggregate=dict(type='list', elements='dict', options=aggregate_spec),
-        purge=dict(default=False, type='bool')
     )
 
     argument_spec.update(element_spec)
@@ -252,7 +256,7 @@ def main():
     diff = None
     with locked_config(module):
         for req in requests:
-            diff = load_config(module, tostring(req), warnings, action='replace')
+            diff = load_config(module, tostring(req), warnings, action='merge')
 
         # issue commit after last configuration change is done
         commit = not module.check_mode
@@ -265,6 +269,42 @@ def main():
 
             if module._diff:
                 result['diff'] = {'prepared': diff}
+
+    failed_conditions = []
+    for item in params:
+        enabled = item.get('enabled')
+        tx_rate = item.get('tx_rate')
+        rx_rate = item.get('rx_rate')
+
+        if enabled is None and tx_rate is None and rx_rate is None:
+            continue
+
+        element = Element('get-interface-information')
+        intf_name = SubElement(element, 'interface-name')
+        intf_name.text = item.get('name')
+
+        sleep(item.get('delay'))
+
+        reply = send_request(module, element, ignore_warning=False)
+
+        if enabled:
+            admin_status = reply.xpath('interface-information/physical-interface/admin-status')
+            if not admin_status or not conditional(enabled, admin_status[0].text.strip()):
+                failed_conditions.append('enabled ' + enabled)
+
+        if tx_rate:
+            output_bps = reply.xpath('interface-information/physical-interface/traffic-statistics/output-bps')
+            if not output_bps or not conditional(tx_rate, output_bps[0].text.strip(), cast=int):
+                failed_conditions.append('tx_rate ' + tx_rate)
+
+        if rx_rate:
+            input_bps = reply.xpath('interface-information/physical-interface/traffic-statistics/input-bps')
+            if not input_bps or not conditional(rx_rate, input_bps[0].text.strip(), cast=int):
+                failed_conditions.append('rx_rate ' + rx_rate)
+
+    if failed_conditions:
+        msg = 'One or more conditional statements have not be satisfied'
+        module.fail_json(msg=msg, failed_conditions=failed_conditions)
 
     module.exit_json(**result)
 
