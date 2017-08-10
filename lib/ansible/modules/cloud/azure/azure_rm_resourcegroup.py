3f76ee9278eed04f0732e2e825ae93bfcbdb0890
Thomas Stringer <tr.stringer@outlook.com>
2017-08-10 17:15:33 -0400

    add fallback for older api resource group listing

--- lib/ansible/modules/cloud/azure/azure_rm_resourcegroup.py
+++ lib/ansible/modules/cloud/azure/azure_rm_resourcegroup.py
@@ -234,8 +234,11 @@ class AzureRMResourceGroup(AzureRMModuleBase):
         found = False
         try:
             response = self.rm_client.resources.list_by_resource_group(self.name)
+        except AttributeError:
+            response = self.rm_client.resource_groups.list_resources(self.name)
         except Exception as exc:
             self.fail("Error checking for resource existence in {0} - {1}".format(self.name, str(exc)))
+
         for item in response:
             found = True
             break
