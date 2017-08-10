0c5e67506673dce0173a8de8afdf8e220dfbcf03
swchundu <swchundu@cisco.com>
2017-08-10 12:13:13 -0700

    Various Fixes

--- lib/ansible/modules/network/aci/aci_anp.py
+++ lib/ansible/modules/network/aci/aci_anp.py
@@ -12,31 +12,33 @@ ANSIBLE_METADATA = {'metadata_version': '1.0',
 
 DOCUMENTATION = r'''
 ---
-
 module: aci_anp
 short_description: Manage top level application network profile objects
 description:
-    -  Manage top level application network profile object, i.e. this does
+-  Manage top level application network profile object, i.e. this does
       not manage EPGs.
 author:
 - Swetha Chunduri (@schunduri)
-- Dag Wieers (@dagwieesrs)
+- Dag Wieers (@dagwieers)
 version_added: '2.4'
 requirements:
-    - ACI Fabric 1.0(3f)+
-notes: Tenant must exist prior to using this module
+- ACI Fabric 1.0(3f)+
+notes:
+- The tenant used must exist before using this module in your playbook. The M(aci_tenant) module can be used for this.
 options:
-   tenant_name:
+   tenant:
      description:
-     - The name of the tenant 
+     - The name of the tenant.
      required: yes
-   app_profile_name:
+     aliases: ['tenant_name']
+   app_profile:
      description:
-     - The name of the application network profile
+     - The name of the application network profile.
      required: yes
+     aliases: ['app_profile']
    description:
      description:
-     - Description for the ANP
+     - Description for the ANP.
    state:
      description:
      - present, absent, query
@@ -46,15 +48,14 @@ extends_documentation_fragment: aci
 '''
 
 EXAMPLES = r'''
-
 - name: Add a new ANP
   aci_anp:
     hostname: apic
     username: admin
     password: SomeSecretPassword
-    tenant_name: Name of an existing tenant
-    application_profile_name: Name of the ANP
-    description: Description for ANP
+    tenant: production
+    app_profile: default
+    description: Production Tenant
     state: present
 
 - name: Remove an ANP
@@ -62,8 +63,8 @@ EXAMPLES = r'''
     hostname: apic
     username: admin
     password: SomeSecretPassword
-    tenant_name: Name of an existing tenant
-    application_profile_name: Name of the ANP
+    tenant: production
+    app_profile: default
     state: absent
 
 - name: Query an ANP
@@ -71,8 +72,8 @@ EXAMPLES = r'''
     hostname: apic
     username: admin
     password: SomeSecretPassword
-    tenant_name: Name of an existing tenant
-    application_profile_name: Name of the ANP
+    tenant: production
+    app_profile: default
     state: query
 
 - name: Query all ANPs
@@ -106,8 +107,8 @@ from ansible.module_utils.basic import AnsibleModule
 def main():
     argument_spec = aci_argument_spec
     argument_spec.update(
-        tenant_name=dict(type='str'),
-        application_profile_name=dict(type='str'),
+        tenant=dict(type='str', aliases=['tenant_name']),
+        app_profile=dict(type='str', aliases=['app_profile_name']),
         description=dict(type='str', aliases=['descr']),
         state=dict(type='str', default='present', choices=['absent', 'present', 'query']),
     )
@@ -117,26 +118,26 @@ def main():
         supports_check_mode=True,
     )
 
-    tenant_name = module.params['tenant_name']
-    application_profile_name = module.params['application_profile_name']
+    tenant = module.params['tenant']
+    app_profile = module.params['app_profile']
     description = str(module.params['description'])
     state = module.params['state']
- 
+
     aci = ACIModule(module)
 
-    if (tenant_name, application_profile_name) is not None:
+    if (tenant, app_profile) is not None:
         # Work with specific anp
-        path = 'api/mo/uni/tn-%(tenant_name)s/ap-%(application_profile_name)s.json' % module.params
+        path = 'api/mo/uni/tn-%(tenant)s/ap-%(app_profile)s.json' % module.params
     elif state == 'query':
         # Query all anps
         path = 'api/node/class/fvAp.json'
     else:
-        module.fail_json(msg="Parameters 'tenant_name' and 'applcation_profile_name' are required for state 'absent' or 'present'")
+        module.fail_json(msg="Parameters 'tenant' and 'app_profile' are required for state 'absent' or 'present'")
 
     if state == 'query':
         aci.request(path)
     else:
-        payload = {"fvAp": {"attributes": {"name": application_profile_name, "descr": description}}}
+        payload = {"fvAp": {"attributes": {"name": app_profile, "descr": description}}}
         aci.request_diff(path, payload=json.dumps(payload))
 
     module.exit_json(**aci.result)
