9b3a74c5b5a81bec58d9cfedc71b26e4cf052087
Peter Sprygada <psprygada@ansible.com>
2017-08-10 09:04:06 -0400

    fix up pep8 issues and remove offending function

--- lib/ansible/modules/network/sros/sros_config.py
+++ lib/ansible/modules/network/sros/sros_config.py
@@ -210,19 +210,8 @@ backup_path:
 """
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.netcfg import NetworkConfig, dumps
-from ansible.module_utils.sros import sros_argument_spec, check_args,
+from ansible.module_utils.sros import sros_argument_spec, check_args
 from ansible.module_utils.sros import load_config, run_commands, get_config
-
-
-def sanitize_config(lines):
-    commands = list()
-    for line in lines:
-        for index, entry in enumerate(commands):
-            if line.startswith(entry):
-                del commands[index]
-                break
-        commands.append(line)
-    return commands
 
 
 def get_active_config(module):
@@ -260,7 +249,6 @@ def run(module, result):
     if configobjs:
         commands = dumps(configobjs, 'commands')
         commands = commands.split('\n')
-        #commands = sanitize_config(commands.split('\n'))
 
         result['commands'] = commands
         result['updates'] = commands
