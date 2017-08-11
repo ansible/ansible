d7a3c2b845e36a1e31bf7c8e2f6c160cdc61c4f6
Michele Zuccala <michele@zuccala.com>
2017-08-11 15:31:20 +0200

    [cloud] ovirt: add headers param to auth (#27881)
    * Add filter param to ovirt module auth
    
    * Update docs to include new filter param
    
    * Fix broken YAML
    
    * Replace filter param with headers

--- lib/ansible/module_utils/ovirt.py
+++ lib/ansible/module_utils/ovirt.py
@@ -143,6 +143,7 @@ def create_connection(auth):
         insecure=auth.get('insecure', False),
         token=auth.get('token', None),
         kerberos=auth.get('kerberos', None),
+        headers=auth.get('headers', None),
     )
 
 
