e52063562541680a0be64fae934ace1bc37aa2aa
Jacob McGill <jacob@networktocode.com>
2017-08-11 12:30:57 -0400

    fix issue with trailing white space when logging into asa (#28073)

--- lib/ansible/plugins/terminal/asa.py
+++ lib/ansible/plugins/terminal/asa.py
@@ -40,7 +40,7 @@ class TerminalModule(TerminalBase):
     ]
 
     def on_open_shell(self):
-        if self._get_prompt().endswith(b'#'):
+        if self._get_prompt().strip().endswith(b'#'):
             self.disable_pager()
 
     def disable_pager(self):
