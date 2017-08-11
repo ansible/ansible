b4b3ca9d64a38f11e1f5bfd708117d6f9e3c4a10
DhivyaP <dhivya_p@dell.com>
2017-08-11 05:53:55 -0500

    module_utils_fix_dellos6

--- lib/ansible/plugins/terminal/dellos6.py
+++ lib/ansible/plugins/terminal/dellos6.py
@@ -48,7 +48,7 @@ class TerminalModule(TerminalBase):
 
         cmd = {u'command': u'enable'}
         if passwd:
-            cmd[u'prompt'] = to_text(r"[\r\n]?password: $", errors='surrogate_or_strict')
+            cmd[u'prompt'] = to_text(r"[\r\n]?password:$", errors='surrogate_or_strict')
             cmd[u'answer'] = passwd
         try:
             self._exec_cli_command(to_bytes(json.dumps(cmd), errors='surrogate_or_strict'))
