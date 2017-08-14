989a4c681c39f3778ff2d7aba89857f1d82644f1
Gabe Muniz <gmuniz@redhat.com>
2017-08-14 19:04:50 +0000

    fixed typos

--- lib/ansible/modules/system/filament.py
+++ lib/ansible/modules/system/filament.py
@@ -1,5 +1,5 @@
-__metaclass__ = community
-__future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+from __future__ import absolute_import, division, print_function
 import q, pdb
 foo = 'apple'
-print(foo)
+print 'foo'
