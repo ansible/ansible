795af89f479c4499b81d260ce395f880c00fdbf2
Gabe Muniz <gmuniz@redhat.com>
2017-08-14 20:46:25 +0000

    removed print

--- lib/ansible/modules/system/filament.py
+++ lib/ansible/modules/system/filament.py
@@ -1,5 +1,14 @@
-__metaclass__ = type
 from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+
+
+
+
 import q, pdb
 foo = 'apple'
-print 'foo'
