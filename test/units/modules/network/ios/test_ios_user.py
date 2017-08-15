7b131e0629bc46f3df678be1faa12a424b28eae5
David Newswanger <gamma.dave@gmail.com>
2017-08-14 21:33:26 -0400

    fix python 3 unit tests

--- test/units/modules/network/ios/test_ios_user.py
+++ test/units/modules/network/ios/test_ios_user.py
@@ -53,10 +53,17 @@ class TestIosUserModule(TestIosModule):
     def test_ios_user_delete(self):
         set_module_args(dict(name='ansible', state='absent'))
         result = self.execute_module(changed=True)
-        cmd = '{"answer": "y", ' +\
-              '"prompt": "This operation will remove all username related ' +\
-              'configurations with same name", "command": "no username ansible"}'
-        self.assertEqual(result['commands'], [cmd])
+        cmd = json.loads(
+            '{"answer": "y", ' +
+            '"prompt": "This operation will remove all username related ' +
+            'configurations with same name", "command": "no username ansible"}'
+        )
+
+        result_cmd = []
+        for i in result['commands']:
+            result_cmd.append(json.loads(i))
+
+        self.assertEqual(result_cmd, [cmd])
 
     def test_ios_user_password(self):
         set_module_args(dict(name='ansible', password='test'))
@@ -75,10 +82,17 @@ class TestIosUserModule(TestIosModule):
     def test_ios_user_purge(self):
         set_module_args(dict(purge=True))
         result = self.execute_module(changed=True)
-        cmd = '{"answer": "y", ' +\
-              '"prompt": "This operation will remove all username related ' +\
-              'configurations with same name", "command": "no username ansible"}'
-        self.assertEqual(result['commands'], [cmd])
+        cmd = json.loads(
+            '{"answer": "y", ' +
+            '"prompt": "This operation will remove all username related ' +
+            'configurations with same name", "command": "no username ansible"}'
+        )
+
+        result_cmd = []
+        for i in result['commands']:
+            result_cmd.append(json.loads(i))
+
+        self.assertEqual(result_cmd, [cmd])
 
     def test_ios_user_view(self):
         set_module_args(dict(name='ansible', view='test'))
