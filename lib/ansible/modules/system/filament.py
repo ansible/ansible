6d99c805c8d1e8ab8a1f37e66ba21efd2b2615e8
Gabe Muniz <gmuniz@redhat.com>
2017-08-15 20:10:21 +0000

    added docs/examples/return

--- lib/ansible/modules/system/filament.py
+++ lib/ansible/modules/system/filament.py
@@ -3,6 +3,26 @@ __metaclass__ = type
 ANSIBLE_METADATA = {'metadata_version': '1.0',
                     'status': ['preview'],
                     'supported_by': 'community'}
+DOCUMENTATION = '''
+
+this is a test document to pass module checks
+
+'''
+
+EXAMPLES = '''
+
+this is to get by that shippable test
+
+'''
+
+RETURN = r'''
+
+this is another check to pass shippable
+
+'''
+
+
+
 foo = 'apple'
 
 
