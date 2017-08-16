3259498bc2b84a898e91e330860b3da7e8925e58
Gabe Muniz <gmuniz@redhat.com>
2017-08-16 05:03:26 +0000

    pep8 fixes with keys

--- lib/ansible/modules/system/filament.py
+++ lib/ansible/modules/system/filament.py
@@ -4,20 +4,42 @@ ANSIBLE_METADATA = {'metadata_version': '1.0',
                     'status': ['preview'],
                     'supported_by': 'community'}
 DOCUMENTATION = '''
+---
+module: my_sample_module
 
-this is a test document to pass module checks
+short_description: This is my sample module
 
+version_added: "2.4"
+
+description:
+    - "This is my longer description explaining my sample module"
+
+options:
+    name:
+        description:
+            - This is the message to send to the sample module
+        required: true
+    new:
+        description:
+            - Control to demo if the result of this module is changed or not
+        required: false
 '''
 
 EXAMPLES = '''
 
-this is to get by that shippable test
+# Pass in a message
+- name: Test with a message
+  my_new_test_module:
+    name: hello world
 
 '''
 
-RETURN = r'''
+RETURN = '''
 
-this is another check to pass shippable
-
+  original_message:
+      description: The original name param that was passed in
+      type: str
+  message:
+      description: The output message that the sample module generates
 '''
 foo = 'apple'
