e8c900afe40ff666cdab840a307ada49fe6004a1
Adrian Likins <alikins@redhat.com>
2017-08-17 17:16:35 -0400

    Handle errors in jmespath in json_query better (#22109)
    * Handle errors in jmespath in json_query better
    
    Catch any exceptions raised from jmespath and raise
    an AnsibleFilterError instead.
    
    Avoid a traceback.
    
    Fixes #20379
    
    * pep8

--- lib/ansible/playbook/base.py
+++ lib/ansible/playbook/base.py
@@ -440,7 +440,6 @@ class Base(with_metaclass(BaseMeta, object)):
 
                 # and assign the massaged value back to the attribute field
                 setattr(self, name, value)
-
             except (TypeError, ValueError) as e:
                 raise AnsibleParserError("the field '%s' has an invalid value (%s), and could not be converted to an %s."
                                          "The error was: %s" % (name, value, attribute.isa, e), obj=self.get_ds(), orig_exc=e)
