9fc4560a5f32e1a49754abc6297ee5032dce8ff0
Moritz Grimm <memo_23@gmx.de>
2017-08-17 15:43:59 +0200

    fixed bug where changed was set without changes when no num_nodes or cache_engine_version was set

--- lib/ansible/modules/cloud/amazon/elasticache.py
+++ lib/ansible/modules/cloud/amazon/elasticache.py
@@ -449,7 +449,7 @@ class ElastiCacheManager(object):
             'EngineVersion': self.cache_engine_version
         }
         for key, value in modifiable_data.items():
-            if value is not None and self.data[key] != value:
+            if value and self.data[key] != value:
                 return True
 
         # Check cache security groups
