520bb4098f17a10072d4065d9444f70d17303caa
Sam Doran <sdoran@redhat.com>
2017-08-17 13:58:46 -0400

    Fix tests and add default query
    PEP8 formatting
    Add metadata
    Shortent license
    Add default filter
    Raise error when an unknown filter is passed

--- lib/ansible/plugins/filter/uri.py
+++ lib/ansible/plugins/filter/uri.py
@@ -1,25 +1,16 @@
 # (c) 2017, Andrea Scarpino <me@andreascarpino.it>
-#
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-# Make coding more python3-ish
-from __future__ import (absolute_import, division, print_function)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-from functools import partial
-import types
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
 
 try:
     from urllib.parse import urlparse
@@ -53,7 +44,15 @@ def _scheme_query(value):
     return urlparse(value).scheme
 
 
-def uri(value, query = ''):
+def _default_query(value):
+    ''' Return the URL minus the file by default '''
+
+    default_url = urlparse(value).scheme + '://' + urlparse(value).netloc
+
+    return default_url
+
+
+def uri(value, query='', alias='uri'):
     ''' Check if string is an URI and filter it '''
 
     query_func_map = {
@@ -61,18 +60,19 @@ def uri(value, query = ''):
         'path': _path_query,
         'port': _port_query,
         'scheme': _scheme_query,
+        '': _default_query
     }
 
     try:
         return query_func_map[query](value)
     except KeyError:
-        return False
+        raise errors.AnsibleFilterError(alias + ': unknown filter type: %s' % query)
+    return False
 
 
 # ---- Ansible filters ----
 class FilterModule(object):
     ''' URI filter '''
-
 
     def filters(self):
         return {
