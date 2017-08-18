444de4090b786bbb00b0ea6c40b4c1173dad0a63
s-hertel <shertel@redhat.com>
2017-08-18 10:45:52 -0400

    Remove unused import

--- lib/ansible/modules/cloud/amazon/s3_sync.py
+++ lib/ansible/modules/cloud/amazon/s3_sync.py
@@ -206,7 +206,6 @@ import traceback
 from dateutil import tz
 
 # import module snippets
-import ansible.module_utils.ec2
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.ec2 import camel_dict_to_snake_dict, ec2_argument_spec, boto3_conn, get_aws_connection_info, HAS_BOTO3
 
