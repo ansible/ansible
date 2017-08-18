0d2a9405913d43eedbb10a7ed8eb26f24877d407
Kira Rose <kira.rose@milliman.com>
2017-08-18 11:56:30 +1000

    Check Windows service status before nssm STOP
    Add a condition on calling nssm STOP inside Nssm-Remove, to check whether or not a service has already been stopped. Currently nssm throws an exception:
    
    ```
    failed: [...] (item=...) => {"changed": false, "failed": true, "item": "...", "msg": "an exception occurred when invoking NSSM: serviceName: STOP: The service has not been started."}
    failed: [...] (item=...) => {"changed": false, "failed": true, "item": "...", "msg": "an exception occurred when invoking NSSM: serviceName: The service has not been started."}
    ```
    
    Behaviour after change:
    
    ```
    ok: [...] => (item=...)
    ok: [...] => (item=...)
    ```

--- lib/ansible/modules/windows/win_nssm.ps1
+++ lib/ansible/modules/windows/win_nssm.ps1
@@ -96,9 +96,10 @@ Function Nssm-Remove
 
     if (Service-Exists -name $name)
     {
-        $cmd = "stop ""$name"""
-        $results = Nssm-Invoke $cmd
-
+        if ((get-service $name).Status -ne "Stopped") {
+            $cmd = "stop ""$name"""
+            $results = Nssm-Invoke $cmd
+        }
         $cmd = "remove ""$name"" confirm"
         $results = Nssm-Invoke $cmd
 
