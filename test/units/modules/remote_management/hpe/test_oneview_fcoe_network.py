77ee12a366f5e860e866c670347534bbd6484cc7
Felipe Bulsoni <fgbulsoni@gmail.com>
2017-08-18 09:35:34 -0300

    Fixing shebang and adding correct copyright header

--- test/units/modules/remote_management/hpe/test_oneview_fcoe_network.py
+++ test/units/modules/remote_management/hpe/test_oneview_fcoe_network.py
@@ -1,20 +1,19 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
-###
+#
 # Copyright (2016-2017) Hewlett Packard Enterprise Development LP
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# You may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-###
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from ansible.compat.tests import unittest
 from oneview_module_loader import FcoeNetworkModule
