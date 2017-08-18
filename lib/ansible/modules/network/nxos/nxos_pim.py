399882c127007bad043b9b717e3cd0d6019c3ba1
Nathaniel Case <this.is@nathanielca.se>
2017-08-18 09:47:41 -0400

    Fix nxos_pim idempotence (#28348)

--- lib/ansible/modules/network/nxos/nxos_pim.py
+++ lib/ansible/modules/network/nxos/nxos_pim.py
@@ -70,7 +70,7 @@ def get_existing(module, args):
 
     for arg in args:
         command = PARAM_TO_COMMAND_KEYMAP[arg]
-        has_command = re.match(r'(?:{0}\s)(?P<value>.*)$'.format(command), config, re.M)
+        has_command = re.search(r'^{0}\s(?P<value>.*)$'.format(command), config, re.M)
 
         value = ''
         if has_command:
