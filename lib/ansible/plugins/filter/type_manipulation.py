eafa95063b95dd47df18d8fa6044d48066e958e5
itdependsnetworks <ken@celenza.org>
2017-08-19 03:46:38 -0400

    update for py3

--- lib/ansible/plugins/filter/type_manipulation.py
+++ lib/ansible/plugins/filter/type_manipulation.py
@@ -1,24 +1,28 @@
-#! /usr/bin/env python
+# Ken Celenza <ken@networktocode.com>
 
-from ansible import errors
+# Make coding more python3-ish
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.errors import AnsibleError, AnsibleFilterError
 
 
 def list_to_dict(data, key):
     new_obj = {}
     
     if not isinstance(data, list):
-        raise errors.AnsibleFilterError("Type is not a valid list")
+        raise AnsibleFilterError("Type is not a valid list")
     for item in data:
         if not isinstance(item, dict):
-            raise errors.AnsibleFilterError("List item is not a valid dict")
+            raise AnsibleFilterError("List item is not a valid dict")
         try:
             key_elem = item.get(key)
         except Exception, e:
-            raise errors.AnsibleFilterError(str(e))
+            raise AnsibleFilterError(str(e))
         if new_obj.get(key_elem):
-            raise errors.AnsibleFilterError("Key {} is not unique, cannot correctly turn into dict".format(key_elem))
+            raise AnsibleFilterError("Key {} is not unique, cannot correctly turn into dict".format(key_elem))
         elif not key_elem:
-            raise errors.AnsibleFilterError("Key {} was not found".format(key_elem))
+            raise AnsibleFilterError("Key {} was not found".format(key_elem))
         else:
             new_obj[key_elem] = item
     return new_obj
@@ -28,12 +32,12 @@ def dict_to_list(data, key_name):
     new_obj = []
 
     if not isinstance(data, dict):
-        raise errors.AnsibleFilterError("Type is not a valid dict")
+        raise AnsibleFilterError("Type is not a valid dict")
     for key, value in data.items():
         if not isinstance(value, dict):
-            raise errors.AnsibleFilterError("Type of key {} value {} is not a valid dict".format(key, value))
+            raise AnsibleFilterError("Type of key {} value {} is not a valid dict".format(key, value))
         if value.get(key):
-            raise errors.AnsibleFilterError("Key name {} is already in use, cannot correctly turn into dict".format(key_name))
+            raise AnsibleFilterError("Key name {} is already in use, cannot correctly turn into dict".format(key_name))
         value[key_name] = key
         new_obj.append(value)
     return new_obj
@@ -51,7 +55,7 @@ class FilterModule(object):
 
 
 if __name__ == "__main__":
-    list_data = [{'proto':'eigrp', 'state':'enabled'}, {'proto':'ospf', 'state':'enabled'}]
-    print list_to_dict(list_data, 'proto')
+    list_data = [{"proto":"eigrp", "state":"enabled"}, {"proto":"ospf", "state":"enabled"}]
+    print(list_to_dict(list_data, 'proto'))
     dict_data = {'eigrp': {'state': 'enabled', 'as': '1'}, 'ospf': {'state': 'enabled', 'as': '2'}}
-    print dict_to_list(dict_data, 'proto')
+    print(dict_to_list(dict_data, 'proto'))
