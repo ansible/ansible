4a8daaae1b9b8aee59bbc175a37df4e0b46b08cd
Dag Wieers <dag@wieers.com>
2017-08-20 03:56:31 +0200

    Original author wishes not to be notified by ansibot (#28447)
    And moving maintainers to BOTMETA.yml as well.

--- lib/ansible/modules/files/xml.py
+++ lib/ansible/modules/files/xml.py
@@ -5,23 +5,9 @@
 #     Tim Bielawa <tbielawa@redhat.com>
 #     Magnus Hedemark <mhedemar@redhat.com>
 # Copyright 2017, Dag Wieers <dag@wieers.com>
-#
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
-# Make coding more python3-ish
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
@@ -115,8 +101,6 @@ author:
 - Tim Bielawa (@tbielawa)
 - Magnus Hedemark (@magnus919)
 - Dag Wieers (@dagwieers)
-- Marko Stanković (@sm4rk0)
-- Christopher Prescott (@cmprescott)
 '''
 
 EXAMPLES = r'''
