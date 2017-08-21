99a971979ab2328a3db7e6d508f205ae722e3bb8
itdependsnetworks <ken@celenza.org>
2017-08-21 05:23:25 -0400

    add new line to kick off shippable

--- test/units/plugins/filter/test_type_manipulation.py
+++ test/units/plugins/filter/test_type_manipulation.py
@@ -40,6 +40,7 @@ class TestTypeFilter(unittest.TestCase):
         key_name = 'proto'
         list_return = [{'state': 'enabled', 'proto': 'ospf', 'as': '2'}, {'state': 'enabled', 'proto': 'eigrp', 'as': '1'}]
         actual_return = dict_to_list(dict_original, key_name)
+
         try:
             _assertItemsEqual = self.assertCountEqual
             _assertItemsEqual(actual_return, list_return)
