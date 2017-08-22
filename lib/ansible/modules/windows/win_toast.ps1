f6ac9197ad72e9d7816efb0dc7476560a41ac4f2
Jon Hawkesworth <figs@unity.demon.co.uk>
2017-08-22 08:21:58 +0100

    replace duff commit version of win_toast

---
+++ lib/ansible/modules/windows/win_toast.ps1
@@ -0,0 +1,100 @@
+#!powershell
+# This file is part of Ansible
+#
+# Copyright 2017, Jon Hawkesworth (@jhawkesworth) <figs@unity.demon.co.uk>
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# WANT_JSON
+# POWERSHELL_COMMON
+
+$ErrorActionPreference = "Stop"
+
+# version check
+$osversion = [Environment]::OSVersion
+$lowest_version = 10
+if ($osversion.Version.Major -lt $lowest_version ) {
+   Fail-Json $result "Sorry, this version of windows, $osversion, does not support Toast notifications.  Toast notifications are available from version $lowest_version" 
+}
+
+$stopwatch = [system.diagnostics.stopwatch]::startNew()
+$now = [DateTime]::Now
+$default_title = "Notification: " + $now.ToShortTimeString()
+
+$params = Parse-Args $args -supports_check_mode $true
+$check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
+
+# $expire_at = Get-AnsibleParam -obj $params -name "msg" -type "str"
+$expire_mins = Get-AnsibleParam -obj $params -name "expire_mins" -type "int" -default 5
+$group = Get-AnsibleParam -obj $params -name "group" -type "str" -default "Powershell"
+$msg = Get-AnsibleParam -obj $params -name "msg" -type "str" -default "Hello world!"
+$popup = Get-AnsibleParam -obj $params -name "popup" -type "bool" -default $true
+$tag = Get-AnsibleParam -obj $params -name "tag" -type "str" -default "Ansible"
+$title = Get-AnsibleParam -obj $params -name "title" -type "str" -default $default_title
+
+$timespan = New-TimeSpan -Minutes $expire_mins
+$expire_at = $now + $timespan
+$expire_at_utc = $($expire_at.ToUniversalTime()|Out-String).Trim()
+
+$result = @{
+    changed = $false
+    expire_mins = $expire_mins
+    expire_at = $expire_at.ToString()
+    expire_at_utc = $expire_at_utc
+    group = $group    
+    msg = $msg
+    popup = $popup
+    tag = $tag
+    title = $title
+}
+
+
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+$template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText01)
+
+#Convert to .NET type for XML manipulation
+$toastXml = [xml] $template.GetXml()
+$toastXml.GetElementsByTagName("text").AppendChild($toastXml.CreateTextNode($title)) > $null
+# TODO add subtitle
+
+#Convert back to WinRT type
+$xml = New-Object Windows.Data.Xml.Dom.XmlDocument
+$xml.LoadXml($toastXml.OuterXml)
+
+$toast = [Windows.UI.Notifications.ToastNotification]::new($xml)
+$toast.Tag = $tag
+$toast.Group = $group
+$toast.ExpirationTime = $expire_at
+$toast.SuppressPopup = -not $popup
+
+try {
+   $notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($msg)
+   if (-not $check_mode) {
+      $notifier.Show($toast);
+   }
+} catch {
+  $excep= $_
+  Fail-Json $result "Failed to create toast notifier, exception was $($excep.Exception.Message) stack trace $($excep.ScriptStackStrace)".
+}
+
+
+
+
+$endsend_at = Get-Date| Out-String
+$stopwatch.Stop()
+
+$result.runtime_seconds = $stopwatch.Elapsed.TotalSeconds
+$result.sent_localtime = $endsend_at.Trim()
+  
+Exit-Json $result
---
+++ lib/ansible/modules/windows/win_toast.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2017, Jon Hawkesworth (@jhawkesworth) <figs@unity.demon.co.uk>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# this is a windows documentation stub.  actual code lives in the .ps1
+# file of the same name
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = r'''
+---
+module: win_toast
+version_added: "2.4"
+short_description: Sends Toast windows notification to Windows 10 or later hosts
+description:
+    - Sends alerts which appear in the Action Center area of the windows desktop.
+options:
+  expire_mins:
+    description:
+      - How long in minutes before the notification expires.
+    default: 5
+  group:
+    description:
+      - Which notification group to add the notification to.
+    default: Powershell
+  msg:
+    description:
+      - The message to appear inside the notification.  May include \n to format the message to appear within the Action Center.
+    default: 'Hello, World!'
+  popup:
+    description:
+      - If false, the notification will not pop up and will only appear in the Action Center.
+    type: bool
+    default: 'Yes'
+  tag:
+    description:
+      - The tag to add to the notification.
+    default: Ansible
+  title:
+    description:
+      - The notification title, which appears in the pop up..
+    default: Notification HH:mm
+author:
+- Jon Hawkesworth (@jhawkesworth)
+notes:
+   - This module must run on a windows 10 or Server 2016 host, so ensure your play targets windows hosts, or delegates to a windows host.
+   - Messages are only sent to the local host where the module is run.
+'''
+
+EXAMPLES = r'''
+- name: Warn logged in users of impending upgrade
+  win_toast:
+    expire_mins: 60
+    title: System Upgrade Notification
+    msg: Automated upgrade about to start.  Please save your work and log off before {{ deployment_start_time }}
+'''
+
+RETURN = r'''
+expire_mins:
+    description: Requested time in minutes before notification expires.
+    returned: allways
+    type: int
+    sample: 5
+expire_at_utc:
+    description: Calculated utc date time when the notification expires.
+    returned: allways
+    type: string
+    sample: 07 July 2017 04:50:54
+group:
+    description: Requested group for the notification to belong to.
+    returned: allways
+    type: string
+    sample: Powershell
+msg:
+    description: Text of the message that was sent.
+    returned: allways
+    type: string
+    sample: Automated upgrade about to start.\nPlease save your work and log off before 22 July 2017 18:00:00
+popup:
+    description: Whether notification pop up was requested or not.
+    returned: allways
+    type: boolean
+    sample: true
+runtime_seconds:
+    description: How long the module took to run on the remote windows host.
+    returned: allways
+    type: float
+    sample: 0.3706631999999997
+sent_localtime:
+    description: local date time when the notification was sent.
+    returned: allways
+    type: string
+    sample: 07 July 2017 05:45:54
+tag:
+    description: Requested tag to associated with the notification.
+
+    returned: allways
+    type: string
+    sample: Ansible
+title:
+    description: The requested text for the notification title, which appears in the popup and Action Center if specified.
+    returned: success
+    type: string
+    sample: Notification 05:45
+'''
