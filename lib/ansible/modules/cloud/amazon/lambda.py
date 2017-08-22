d0c197ade14b1cbd857204d7466eebae3b0bcd5e
Michael De La Rue <jababubic.margoteen@spamgourmet.com>
2017-08-22 07:45:53 +0100

    lambda module - improve account finding according to ryansb

--- lib/ansible/modules/cloud/amazon/lambda.py
+++ lib/ansible/modules/cloud/amazon/lambda.py
@@ -225,7 +225,7 @@ def get_account_id(module, region=None, endpoint=None, **aws_connect_kwargs):
         except ClientError as e:
             if (e.response['Error']['Code'] == 'AccessDenied'):
                 except_msg = to_native(e.message)
-                account_id = except_msg.search("arn:aws:iam::([0-9]{12,32}):user/").group(1)
+                account_id = except_msg.search("arn:aws:iam::([0-9]{12,32}):\w+/").group(1)
             if account_id is None:
                 module.fail_json_aws(e, msg="getting account information")
         except Exception as e:
