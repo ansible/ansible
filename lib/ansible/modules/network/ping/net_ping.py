a2445b44d1306ed06dc5834f2e2c949de57e932b
Jacob McGill <jacob@networktocode.com>
2017-08-23 10:57:43 -0400

    Net Ping: Add new action plugin for *_ping network modules

---
+++ lib/ansible/modules/network/ping/net_ping.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2017, Ansible by Red Hat, inc
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'network'}
+
+
+DOCUMENTATION = r'''
+---
+module: net_vlan
+version_added: "2.4"
+author: "Jacob McGill (@jmcgill298)"
+short_description: Tests reachability using ping from a network device
+description:
+  - Tests reachability using ping from network device to a remote destination.
+options:
+  count:
+    description:
+    - Number of packets to send.
+    required: false
+    default: 5
+  dest:
+    description:
+    - The IP Address or hostname (resolvable by switch) of the remote node.
+    required: true
+  source:
+    description:
+    - The source IP Address.
+    required: false
+    default: null
+  state:
+    description:
+    - Determines if the expected result is success or fail.
+    choices: [ absent, present ]
+    default: present
+  vrf:
+    description:
+    - The VRF to use for forwarding.
+    required: false
+    default: default
+'''
+
+
+EXAMPLES = r'''
+provider:
+  host: "{{ ansible_host }}"
+  username: "{{ username }}"
+  password: "{{ password }}"
+  network_os: "{{ network_os }}"
+
+- name: Test reachability to 10.10.10.10 using default vrf
+  net_ping:
+    provider: "{{ provider }}"
+    dest: 10.10.10.10
+
+- name: Test reachability to 10.20.20.20 using prod vrf
+  net_ping:
+    provider: "{{ provider }}"
+    dest: 10.20.20.20
+    vrf: prod
+
+- name: Test unreachability to 10.30.30.30 using default vrf
+  net_ping:
+    provider: "{{ provider }}"
+    dest: 10.30.30.30
+    state: absent
+
+- name: Test reachability to 10.40.40.40 using prod vrf and setting count and source
+  net_ping:
+    provider: "{{ provider }}"
+    dest: 10.40.40.40
+    source: loopback0
+    vrf: prod
+    count: 20
+'''
+
+RETURN = r'''
+action:
+  description: Show what action has been performed.
+  returned: always
+  type: str
+  sample: "Sending 5, 100-byte ICMP Echos to 10.1.100.1, timeout is 2 seconds:"
+updates:
+  description: Show the command sent.
+  returned: always
+  type: list
+  sample: ["ping vrf prod 10.40.40.40 count 20 source loopback0"]
+count:
+  description: Show amount of packets sent.
+  returned: always
+  type: str
+  sample: "2"
+dest:
+  description: Show the ping destination.
+  returned: always
+  type: str
+  sample: "10.40.40.40"
+rtt:
+  description: Show RTT stats.
+  returned: always
+  type: dict
+  sample: {"avg": "2", "max":"8", "min": "1"}
+packets_rx:
+  description: Packets successfully received.
+  returned: always
+  type: str
+  sample: "20"
+packets_tx:
+  description: Packets successfully transmitted.
+  returned: always
+  type: str
+  sample: "20"
+packet_loss:
+  description: Percentage of packets lost.
+  returned: always
+  type: str
+  sample: "0%"
+'''
