0d4de0769ee8aa04be85c48281983e5df4d06805
Trishna Guha <trishnaguha17@gmail.com>
2017-08-23 13:31:55 -0400

    fix duplicate dict key error vyos_command
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/modules/network/vyos/vyos_command.py
+++ lib/ansible/modules/network/vyos/vyos_command.py
@@ -38,8 +38,6 @@ description:
     value of the environment variable C(ANSIBLE_VYOS_TERMINAL_LENGTH)
     is not set, the default number of 10000 is used.
 extends_documentation_fragment: vyos
-notes:
-  - Tested against VYOS 1.1.7
 options:
   commands:
     description:
@@ -88,6 +86,7 @@ options:
     default: 1
 
 notes:
+  - Tested against VYOS 1.1.7
   - Running C(show system boot-messages all) will cause the module to hang since
     VyOS is using a custom pager setting to display the output of that command.
 """
