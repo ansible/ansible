0ac4700374a9516442d88d851aed2f88ef728c24
nre <nre@ableton.com>
2017-08-23 12:21:51 +0200

    vmware: Only add configured network interfaces to facts
    This change effectively filters out any network interfaces which were
    not explicitly configured for the guest. This fixes some unexpected
    behavior where a machine with multiple IP addresses (for example, when
    Docker is installed, an internal IPv4 interface is added to
    communicate with the container) would show one of the internal
    addresses in the 'ipv4' field, but then no other information about the
    corresponding hardware interface.

--- lib/ansible/module_utils/vmware.py
+++ lib/ansible/module_utils/vmware.py
@@ -365,7 +365,8 @@ def gather_vm_facts(content, vm):
     vmnet = _get_vm_prop(vm, ('guest', 'net'))
     if vmnet:
         for device in vmnet:
-            net_dict[device.macAddress] = list(device.ipAddress)
+            if device.deviceConfigId > 0:
+                net_dict[device.macAddress] = list(device.ipAddress)
 
     if vm.guest.ipAddress:
         if ':' in vm.guest.ipAddress:
