18aff2191accd1157235ba959470f43ddead82bd
s-hertel <shertel@redhat.com>
2017-08-23 12:32:05 -0400

    Rename module to use aws_ prefix.
    Fix copyright.
    
    Fix metadata version.

--- lib/ansible/modules/cloud/amazon/aws_waf_facts.py
+++ lib/ansible/modules/cloud/amazon/aws_waf_facts.py
@@ -1,25 +1,13 @@
 #!/usr/bin/python
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (c) 2017 Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-ANSIBLE_METADATA = {'metadata_version': '1.0',
+ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
 DOCUMENTATION = '''
-module: waf_facts
+module: aws_waf_facts
 short_description: Retrieve facts for WAF ACLs, Rule , Conditions and Filters.
 description:
   - Retrieve facts for WAF ACLs, Rule , Conditions and Filters.
@@ -37,10 +25,10 @@ extends_documentation_fragment: aws
 
 EXAMPLES = '''
 - name: obtain all WAF facts
-  waf_facts:
+  aws_waf_facts:
 
 - name: obtain all facts for a single WAF
-  waf_facts:
+  aws_waf_facts:
     name: test_waf
 '''
 
