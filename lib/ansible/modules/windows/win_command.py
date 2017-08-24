8e3f21c8cf8bdb1ea3de84d6faf029adc0e96381
Fabio Alessandro Locati <me@fale.io>
2017-08-24 17:14:05 +0200

    windows/win_command: improve examples (#20465)
    * windows/win_command: improve examples
    
    * Improve description
    
    * Improvements
    
    * minor updates based on PR feedback

--- lib/ansible/modules/windows/win_command.py
+++ lib/ansible/modules/windows/win_command.py
@@ -64,17 +64,12 @@ author:
 '''
 
 EXAMPLES = r'''
-# Example from Ansible Playbooks.
-- win_command: whoami
+- name: Save the result of 'whoami' in 'whoami_out'
+  win_command: whoami
   register: whoami_out
 
-# Run the command only if the specified file does not exist.
-- win_command: wbadmin -backupTarget:C:\backup\ creates=C:\backup\
-
-# You can also use the 'args' form to provide the options. This command
-# will change the working directory to C:\somedir\\ and will only run when
-# C:\backup\ doesn't exist.
-- win_command: wbadmin -backupTarget:C:\backup\ creates=C:\backup\
+- name: Run command that only runs if folder exists and runs from a specific folder
+  win_command: wbadmin -backupTarget:C:\backup\
   args:
     chdir: C:\somedir\
     creates: C:\backup\
