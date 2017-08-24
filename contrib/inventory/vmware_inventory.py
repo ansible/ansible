c53d1d5c04bcf2c57c89cebb23c931806df437ca
James Tanner <tanner.jc@gmail.com>
2017-08-24 14:29:24 -0400

    Remove extra code

--- contrib/inventory/vmware_inventory.py
+++ contrib/inventory/vmware_inventory.py
@@ -713,8 +713,7 @@ class VMWareInventory(object):
             methods = dir(vobj)
             methods = [str(x) for x in methods if not x.startswith('_')]
             methods = [x for x in methods if x not in self.bad_types]
-            if inkey:
-                methods = [x for x in methods if not inkey + '.' + x.lower() in self.skip_keys]
+            methods = [x for x in methods if not inkey + '.' + x.lower() in self.skip_keys]
             methods = sorted(methods)
 
             for method in methods:
