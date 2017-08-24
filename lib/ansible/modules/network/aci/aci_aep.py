825a096681877f0c705a1a485734f85b08d15600
Jacob McGill <jacob@networktocode.com>
2017-08-24 14:22:31 -0400

    ACI AEP: Update module to use new URL Method

--- lib/ansible/modules/network/aci/aci_aep.py
+++ lib/ansible/modules/network/aci/aci_aep.py
@@ -97,6 +97,8 @@ def main():
     module = AnsibleModule(
         argument_spec=argument_spec,
         supports_check_mode=True,
+        required_if=[['state', 'absent', ['aep']],
+                     ['state', 'present', ['aep']]]
     )
 
     aep = module.params['aep']
@@ -104,16 +106,7 @@ def main():
     state = module.params['state']
 
     aci = ACIModule(module)
-
-    if aep is not None:
-        path = 'api/mo/uni/infra/attentp-%(aep)s.json' % module.params
-    elif state == 'query':
-        path = 'api/class/infraAttEntityP.json'
-    else:
-        module.fail_json(msg="Parameter 'aep' is required for state 'absent' or 'present'")
-
-    aci.result['url'] = '%(protocol)s://%(hostname)s/' % aci.params + path
-
+    aci.construct_url(root_class="aep")
     aci.get_existing()
 
     if state == 'present':
