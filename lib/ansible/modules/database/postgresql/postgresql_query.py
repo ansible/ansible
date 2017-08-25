6e7fb0fa96ce78eec74869a760715815cb1088d3
Félix Archambault <fel.archambault@gmail.com>
2017-08-24 21:15:24 -0400

    postgresql_query_module: improve module args and documentation
    * fix db connection closure
    * use postgresql fragments
    * use argument 'name' instead of 'fact' (same as include_vars)
    * removed booleans choices
    * unset the query arg type and make it mandatory

--- lib/ansible/modules/database/postgresql/postgresql_query.py
+++ lib/ansible/modules/database/postgresql/postgresql_query.py
@@ -1,128 +1,65 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+# Copyright (c) 2017 Felix Archambault
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-
-ANSIBLE_METADATA = {'metadata_version': '1.0',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'supported_by': 'community',
+    'status': ['preview']
+}
 
 DOCUMENTATION = '''
 ---
 module: postgresql_query
-short_description: Run arbitrary queries on a postgresql database.
+short_description: Run arbitrary PostgreSQL queries.
 description:
-   - Run arbitrary SQL queries against a PostgreSQL instance from the current host.
-   - Query results are returned as "query_results" as a list of dictionaries.
-     Number of rows affected are returned as "row_count".
+   - Run arbitrary PostgreSQL queries.
    - Can read queries from a .sql script files.
    - SQL scripts may be templated with ansible variables at execution time.
+   - >
+    Query results may be assigned to a variable for later reuse during ansible
+    runtime.
 version_added: "2.4"
 options:
-  db:
-    description:
-      - name of the database to run queries against.
-    required: false
-    default: None
-  port:
-    description:
-      - Database port to connect to.
-    required: false
-    default: 5432
-  login_user:
-    description:
-      - User (role) used to authenticate with PostgreSQL.
-    required: false
-    default: postgres
-  login_password:
-    description:
-      - Password used to authenticate with PostgreSQL.
-    required: false
-    default: null
-  login_host:
-    description:
-      - Host running PostgreSQL.
-    required: false
-    default: localhost
-  login_unix_socket:
-    description:
-      - Path to a Unix domain socket for local connections.
-    required: false
-    default: null
-  query:
-    description:
-      - SQL query to run. Variables can be escaped with psycopg2 syntax.
-      - Can be a SQL scripts file.
-    required: true
-  positional_args:
-    description:
-      - A list of values to be passed as positional arguments to the query.
-      - Cannot be used with named_args.
-    required: false
-    default: null
-  autocommit:
-      description:
-        - Enable transaction autocommit.
-      required: false
-      default: false
-      type: bool
-  named_args:
-    description:
-      - A dictionary of key-value arguments to pass to the query.
-      - Cannot be used with positional_args.
-    required: false
-    default: null
-  fact:
-    description:
-        - Assign the query_results to a key by the name of this arg.
-        - Will make this new variable available as a fact to subsequent tasks.
-    required: false
-    default: null
-  ssl_mode:
-    description:
-      - Determines whether or with what priority a secure SSL TCP/IP connection will be negotiated with the server.
-      - See https://www.postgresql.org/docs/current/static/libpq-ssl.html for more information on the modes.
-      - Default of C(prefer) matches libpq default.
-    required: false
-    default: prefer
-    choices: [disable, allow, prefer, require, verify-ca, verify-full]
-  ssl_rootcert:
-    description:
-      - Specifies the name of a file containing SSL certificate authority (CA) certificate(s).
-      - If the file exists, the server's certificate will be verified to be signed by one of these authorities.
-    required: false
-    default: null
-notes:
-   - The default authentication assumes that you are either logging in as or
-     sudo'ing to the postgres account on the host.
-   - This module uses psycopg2, a Python PostgreSQL database adapter. You must
-     ensure that psycopg2 is installed on the host before using this module. If
-     the remote host is the PostgreSQL server (which is the default case), then
-     PostgreSQL must also be installed on the remote host. For Ubuntu-based
-     systems, install the postgresql, libpq-dev, and python-psycopg2 packages
-     on the remote host before using this module.
-   - If the passlib library is installed, then passwords that are encrypted
-     in the DB but not encrypted when passed as arguments can be checked for
-     changes. If the passlib library is not installed, unencrypted passwords
-     stored in the DB encrypted will be assumed to have changed.
-   - If you specify PUBLIC as the user, then the privilege changes will apply
-     to all users. You may not specify password or role_attr_flags when the
-     PUBLIC user is specified.
-   - The ssl_rootcert parameter requires at least Postgres version 8.4 and I(psycopg2) version 2.4.3.
-requirements: [ psycopg2 ]
+    db:
+        description:
+            - name of the database to run queries against.
+        required: false
+        default: None
+    query:
+        description:
+            - SQL query to run. Variables can be escaped with psycopg2 syntax.
+            - Can be a SQL scripts file.
+        required: true
+    positional_args:
+        description:
+            - List of values to be passed as positional arguments to the query.
+            - Cannot be used with I(named_args).
+        required: false
+        default: null
+    named_args:
+        description:
+            - Dictionary of key-value arguments to pass to the query.
+            - Cannot be used with I(positional_args).
+        required: false
+        default: null
+    name:
+        description:
+            - The name of a variable into which assign the query_results.
+            - Will make this new variable available as a fact to subsequent tasks.
+        required: false
+        default: null
+    autocommit:
+        description:
+            - Enable transaction autocommit.
+        required: false
+        default: false
+        type: bool
+
+extends_documentation_fragment:
+- postgres
+
+requirements: [psycopg2]
 author:
     - "Felix Archambault (@archf)"
     - "Will Rouesnel (@wrouesnel)"
@@ -139,7 +76,7 @@ EXAMPLES = '''
       - "positional string value 1"
       - "positional string value 2"
 
-# Insert or update a record in a table with named arguments
+# Insert or update a record in a table with named arguments.
 - postgresql_query:
     db: acme
     login_user: django
@@ -149,8 +86,8 @@ EXAMPLES = '''
       a_value: "positional string value 1"
       b_value: "positional string value 2"
 
-
-# Run queries from a '.sql' file
+# Run queries from a '.sql' file. This file may contain placeholders as shown
+# here.
 - postgresql_query:
     db: acme
     login_user: django
@@ -172,7 +109,7 @@ EXAMPLES = '''
     named_args:
       a_value: "positional string value 1"
       b_value: "positional string value 2"
-    fact: my_key
+    name: my_key
 '''
 
 RETURN = '''
@@ -181,16 +118,17 @@ query_results:
     returned: changed
     type: list
     sample: [{"Column": "Value1"},{"Column": "Value2"}]
-row_count:
-    description: number of affected rows by query, if applicable
+rowcount:
+    description: Number of affected rows by query, if applicable.
     returned: changed
     type: int
     sample: 5
 '''
 
+__metaclass__ = type
 import traceback
 from ansible.module_utils.six import iteritems
-from ansible.module_utils.basic import AnsibleModule, BOOLEANS
+from ansible.module_utils.basic import AnsibleModule
 import ansible.module_utils.postgres as pgutils
 
 
@@ -199,11 +137,11 @@ def main():
 
     argument_spec.update(dict(
         db=dict(default=None),
-        query=dict(type="str"),
-        autocommit=dict(type='bool', default=False, choices=BOOLEANS),
+        query=dict(required=True, default=None),
+        autocommit=dict(type="bool", default=False),
         positional_args=dict(type="list"),
         named_args=dict(type="dict"),
-        fact=dict(default=None),
+        name=dict(default=None),
     ))
 
     module = AnsibleModule(
@@ -257,25 +195,26 @@ def main():
         cursor = db_connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
     except TypeError as e:
+        db_connection.close()
         if 'sslrootcert' in e.args[0]:
             module.fail_json(msg='''Postgresql server must be at least version
                              8.4 to support sslrootcert.
                              Exception: {0}'''.format(e), exception=traceback.format_exc())
+
         module.fail_json(msg="unable to connect to database: %s" % e, exception=traceback.format_exc())
-        db_connection.close()
 
     except Exception as e:
-        module.fail_json(msg="unable to connect to database: {0}".format(str(e)), exception=traceback.format_exc())
         db_connection.close()
+        module.fail_json(msg="unable to connect to database: {0}".format(str(e)), exception=traceback.format_exc())
 
-    # if query is a file, load the file and run it
+    # if query is a file, try read it
     query = module.params["query"]
     if query.endswith('.sql'):
         try:
             query = open(query, 'r').read().strip('\n')
         except Exception as e:
-            module.fail_json(msg="Unable to find '%s' in given path." % query)
             db_connection.close()
+            module.fail_json(msg="Unable to find '%s' in given path." % query)
 
     # prepare args
     if module.params["positional_args"] is not None:
@@ -285,13 +224,15 @@ def main():
     else:
         arguments = None
 
+    # execute query
     try:
         cursor.execute(query, arguments)
     except Exception as e:
+        db_connection.close()
         module.fail_json(msg="Unable to execute query: %s" % e,
                          query_arguments=arguments)
-        db_connection.close()
 
+    # retreive results
     if cursor.rowcount > 0:
         # There's no good way to return results arbitrarily without inspecting
         # the SQL, so we act consistent and return the empty set when there's
@@ -302,7 +243,7 @@ def main():
             pass
 
         rowcount = len(query_results)
-        fact = module.params["fact"]
+        fact = module.params["name"]
         if fact is not None:
             ansible_facts = {fact: query_results}
 
