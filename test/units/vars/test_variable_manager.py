c4bedd6ab91ee5f35b8b76f700fe84aa4dd0d86c
Brian Coca <brian.coca+git@gmail.com>
2017-08-25 22:07:22 -0400

    pepe hates extra blank line

--- test/units/vars/test_variable_manager.py
+++ test/units/vars/test_variable_manager.py
@@ -52,7 +52,6 @@ class TestVariableManager(unittest.TestCase):
         v = VariableManager(loader=fake_loader, inventory=mock_inventory)
         variables = v.get_vars(use_cache=False)
 
-
         # Check var manager expected values,  never check: ['omit', 'vars']
         # FIXME:  add the following ['ansible_version', 'ansible_playbook_python', 'groups']
         for varname, value in (('playbook_dir', os.path.abspath('.')), ):
