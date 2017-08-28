16a72a508c4c14f0f67c16c230e3b68060514fdc
Sam Doran <sdoran@redhat.com>
2017-08-28 12:26:45 -0400

    Add options, cleanup output, fix tests

--- lib/ansible/plugins/filter/urlsplit.py
+++ lib/ansible/plugins/filter/urlsplit.py
@@ -13,6 +13,7 @@ ANSIBLE_METADATA = {
 }
 
 
+from ansible.errors import AnsibleFilterError
 from ansible.module_utils.six.moves.urllib.parse import urlsplit
 from ansible.utils import helpers
 
@@ -21,7 +22,18 @@ def split_url(value, query='', alias='urlsplit'):
 
     results = helpers.object_to_dict(urlsplit(value))
 
-    return results
+    # Filter out keys we don't want in the results
+    filter_keys = [x for x in results if x.startswith(('count', 'geturl', 'index', '_'))]
+    [results.pop(x, None) for x in filter_keys]
+
+    # If a query is supplied, make sure it's valid then return it.
+    # Otherwise, return the entire dictionary.
+    if query:
+        if query not in results:
+            raise AnsibleFilterError(alias + ': unknown URL component: %s' % query)
+        return results[query]
+    else:
+        return results
 
 
 # ---- Ansible filters ----
