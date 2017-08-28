118304d88462527004badf0009b5971ecd3472b4
Adrian Likins <alikins@redhat.com>
2017-08-28 19:05:30 -0400

    cleanup, add dump_config --template-file cli opt

--- docs/bin/dump_config.py
+++ docs/bin/dump_config.py
@@ -1,44 +1,58 @@
 #!/usr/bin/env python
 
 import optparse
+import os
+import sys
 import yaml
 
 from jinja2 import Environment, FileSystemLoader
 
-template_file = 'config.rst.j2'
-oblist = {}
-clist = []
-
-p = optparse.OptionParser(
-    version='%prog 1.0',
-    usage='usage: %prog [options]',
-    description='Generate module documentation from metadata',
-)
-p.add_option("-T", "--template-dir", action="store", dest="template_dir", default="../templates", help="directory containing Jinja2 templates")
-p.add_option("-o", "--output-dir", action="store", dest="output_dir", default='/tmp/', help="Output directory for rst files")
-p.add_option("-d", "--docs-source", action="store", dest="docs", default=None, help="Source for attribute docs")
-
-(options, args) = p.parse_args()
+DEFAULT_TEMPLATE_FILE = 'config.rst.j2'
 
 
-if options.docs:
-    with open(options.docs) as f:
-        docs = yaml.safe_load(f)
-else:
-    docs = {}
+def generate_parser():
+    p = optparse.OptionParser(
+        version='%prog 1.0',
+        usage='usage: %prog [options]',
+        description='Generate module documentation from metadata',
+    )
+    p.add_option("-t", "--template-file", action="store", dest="template_file", default=DEFAULT_TEMPLATE_FILE, help="directory containing Jinja2 templates")
+    p.add_option("-o", "--output-dir", action="store", dest="output_dir", default='/tmp/', help="Output directory for rst files")
+    p.add_option("-d", "--docs-source", action="store", dest="docs", default=None, help="Source for attribute docs")
 
-config_options = docs
-#print('config_options: %s' % config_options)
+    (options, args) = p.parse_args()
 
-#    # build ordered list to loop over and dict with attributes
-#    clist.append(name)
-#    oblist[name] = dict((x, aobj.__dict__['_attributes'][x]) for x in aobj.__dict__['_attributes'] if 'private' not in x or not x.private)
+    return p
 
-env = Environment(loader=FileSystemLoader(options.template_dir), trim_blocks=True,)
-template = env.get_template(template_file)
-outputname = options.output_dir + template_file.replace('.j2', '')
-tempvars = {'config_options': config_options}
-import pprint
-pprint.pprint(config_options)
-with open(outputname, 'w') as f:
-    f.write(template.render(tempvars).encode('utf-8'))
+
+def main(args):
+
+    parser = generate_parser()
+    (options, args) = parser.parse_args()
+
+    output_dir = os.path.abspath(options.output_dir)
+    template_file_full_path = os.path.abspath(options.template_file)
+    template_file = os.path.basename(template_file_full_path)
+    template_dir = os.path.dirname(os.path.abspath(template_file_full_path))
+
+    if options.docs:
+        with open(options.docs) as f:
+            docs = yaml.safe_load(f)
+    else:
+        docs = {}
+
+    config_options = docs
+
+    env = Environment(loader=FileSystemLoader(template_dir), trim_blocks=True,)
+    template = env.get_template(template_file)
+    output_name = os.path.join(output_dir, template_file.replace('.j2', ''))
+    temp_vars = {'config_options': config_options}
+
+    with open(output_name, 'w') as f:
+        f.write(template.render(temp_vars).encode('utf-8'))
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[:]))
