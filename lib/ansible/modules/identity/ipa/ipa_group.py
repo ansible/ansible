beed2bf7ff651fd1b4477d6fdf4ae05b067b96c0
Thomas Krahn <ntbc@gmx.net>
2017-08-29 08:37:16 +0200

    ipa_group: Fix issue 25660 (#26282)

--- lib/ansible/modules/identity/ipa/ipa_group.py
+++ lib/ansible/modules/identity/ipa/ipa_group.py
@@ -10,7 +10,6 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
-
 DOCUMENTATION = '''
 ---
 module: ipa_group
@@ -194,6 +193,10 @@ def get_group_diff(client, ipa_group, module_group):
         if not module_group['nonposix'] and ipa_group.get('nonposix'):
             module_group['posix'] = True
         del module_group['nonposix']
+
+    if 'external' in module_group:
+        if module_group['external'] and 'ipaexternalgroup' in ipa_group.get('objectclass'):
+            del module_group['external']
 
     return client.get_diff(ipa_data=ipa_group, module_data=module_group)
 
