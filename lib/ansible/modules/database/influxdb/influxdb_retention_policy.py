d28c85f1fa5d392b3c3bbeaf945957633e8c2893
torz <torz@gmx.com>
2017-08-29 20:45:19 +0000

    3rd time lucky

--- lib/ansible/modules/database/influxdb/influxdb_retention_policy.py
+++ lib/ansible/modules/database/influxdb/influxdb_retention_policy.py
@@ -67,12 +67,12 @@ options:
         description:
             - Use https instead of http to connect to InfluxDB, defaults to False
         required: false
-        version_added: 2.5
+        version_added: 2.4
     verify_ssl:
         description:
             - verify SSL certificates for HTTPS requests, defaults to False
         required: false
-        version_added: 2.5
+        version_added: 2.4
 '''
 
 EXAMPLES = '''
