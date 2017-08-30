649e3c1bacdd8950715d9cdcba51f4846d38322b
Nathan Davison <ndavison85@gmail.com>
2017-08-30 21:19:19 +1000

    Change the Ansible version_added back to 2.4 - ooops.

--- lib/ansible/modules/system/listen_ports_facts.py
+++ lib/ansible/modules/system/listen_ports_facts.py
@@ -7,7 +7,7 @@ DOCUMENTATION = '''
 module: listen_ports_facts
 author:
     - Nathan Davison (@ndavison)
-version_added: "2.5"
+version_added: "2.4"
 description:
     - Gather facts on processes listening on TCP and UDP ports. Optionally provide whitelists to gather facts on violations of the whitelist.
 short_description: Gather facts on processes listening on TCP and UDP ports.
