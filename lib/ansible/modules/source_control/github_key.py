cdbd9a3edcd61dd3238e3d71baff44df020ad221
Abhijeet Kasurde <akasurde@redhat.com>
2017-08-31 00:05:42 +0530

    Use correct variable from response in github_key (#25846)
    Before fix, logic tries to access info from 're' library
    which raises AttributeError.
    Fix adds correct variable usage for accessing next/previous
    search results from github api.
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/source_control/github_key.py
+++ lib/ansible/modules/source_control/github_key.py
@@ -103,7 +103,7 @@ class GitHubResponse(object):
     def links(self):
         links = {}
         if 'link' in self.info:
-            link_header = re.info['link']
+            link_header = self.info['link']
             matches = re.findall('<([^>]+)>; rel="([^"]+)"', link_header)
             for url, rel in matches:
                 links[rel] = url
