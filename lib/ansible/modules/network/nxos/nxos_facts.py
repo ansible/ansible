ef7de671b23ff0e2b70ec8f6e3ca45dd4ba78795
Rahul Shenoy <rahushen@cisco.com>
2017-08-31 13:26:26 -0400

    handle power table for n3k

--- lib/ansible/modules/network/nxos/nxos_facts.py
+++ lib/ansible/modules/network/nxos/nxos_facts.py
@@ -431,7 +431,10 @@ class Legacy(FactsBase):
         return objects
 
     def parse_power_supply_info(self, data):
-        data = data['powersup']['TABLE_psinfo']['ROW_psinfo']
+        if data.get('powersup').get('TABLE_psinfo_n3k'):
+            data = data['powersup']['TABLE_psinfo_n3k']['ROW_psinfo_n3k']
+        else:    
+            data = data['powersup']['TABLE_psinfo']['ROW_psinfo']
         objects = list(self.transform_iterable(data, self.POWERSUP_MAP))
         return objects
 
