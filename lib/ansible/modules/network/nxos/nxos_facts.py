a77a2725bf0518a5c01a73172cfc95ab35b484b4
Rahul Shenoy <rahushen@cisco.com>
2017-08-31 14:17:48 -0400

    satisfy shippable

--- lib/ansible/modules/network/nxos/nxos_facts.py
+++ lib/ansible/modules/network/nxos/nxos_facts.py
@@ -433,7 +433,7 @@ class Legacy(FactsBase):
     def parse_power_supply_info(self, data):
         if data.get('powersup').get('TABLE_psinfo_n3k'):
             data = data['powersup']['TABLE_psinfo_n3k']['ROW_psinfo_n3k']
-        else:    
+        else:
             data = data['powersup']['TABLE_psinfo']['ROW_psinfo']
         objects = list(self.transform_iterable(data, self.POWERSUP_MAP))
         return objects
