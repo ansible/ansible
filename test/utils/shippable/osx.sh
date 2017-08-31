f827d126c1eba78ab726c4a1196a60816560bfac
Matt Clay <matt@mystile.com>
2017-08-31 16:24:09 -0700

    Limit remote tests to group 1.

--- test/utils/shippable/osx.sh
+++ test/utils/shippable/osx.sh
@@ -7,7 +7,7 @@ IFS='/:' read -ra args <<< "${TEST}"
 
 platform="${args[0]}"
 version="${args[1]}"
-target="posix/ci/"
+target="posix/ci/group1/"
 
 # shellcheck disable=SC2086
 ansible-test integration --color -v --retry-on-error "${target}" --remote "${platform}/${version}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
