2cb7030ad06b4e99c052c169affc1d93dd7f440e
s-hertel <shertel@redhat.com>
2017-09-01 10:11:22 -0400

    Match on the error code instead of searching for a string in the message.

--- lib/ansible/modules/cloud/amazon/ec2.py
+++ lib/ansible/modules/cloud/amazon/ec2.py
@@ -1197,7 +1197,7 @@ def create_instances(module, ec2, vpc, override_count=None):
                 try:
                     res = ec2.run_instances(**params)
                 except boto.exception.EC2ResponseError as e:
-                    if "The attribute instanceInitiatedShutdownBehavior can only be used for EBS-backed images." in e.message:
+                    if "InvalidParameterCombination" == e.error_code:
                         params['instance_initiated_shutdown_behavior'] = 'terminate'
                         res = ec2.run_instances(**params)
                     else:
