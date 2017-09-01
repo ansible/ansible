7aedd4af469eb34af3aab5ffc991ceb0f912dc11
Matt Clay <matt@mystile.com>
2017-09-01 16:47:01 -0700

    Split cloud tests into two CI groups.

--- test/utils/shippable/cloud.sh
+++ test/utils/shippable/cloud.sh
@@ -6,8 +6,8 @@ declare -a args
 IFS='/:' read -ra args <<< "${TEST}"
 
 image="ansible/ansible:${args[1]}"
-target="posix/ci/cloud/"
+target="posix/ci/cloud/group${args[2]}/"
 
 # shellcheck disable=SC2086
 ansible-test integration --color -v --retry-on-error "${target}" --docker "${image}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
-    --changed-all-target 'posix/ci/cloud/smoketest/'
+    --changed-all-target "${target}smoketest/"
