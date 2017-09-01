d45e3a2597e163676cbe0b243f3d3eb95b98213e
mikedlr <hubby.hilorg@xoxy.net>
2017-09-01 14:25:54 +0100

    lambda - more limited exception trap when doing get_function for better errors (#28918)

--- lib/ansible/modules/cloud/amazon/lambda.py
+++ lib/ansible/modules/cloud/amazon/lambda.py
@@ -238,8 +238,13 @@ def get_current_function(connection, function_name, qualifier=None):
         if qualifier is not None:
             return connection.get_function(FunctionName=function_name, Qualifier=qualifier)
         return connection.get_function(FunctionName=function_name)
-    except ClientError:
-        return None
+    except ClientError as e:
+        try:
+            if e.response['Error']['Code'] == 'ResourceNotFoundException':
+                return None
+        except (KeyError, AttributeError):
+            pass
+        raise e
 
 
 def sha256sum(filename):
