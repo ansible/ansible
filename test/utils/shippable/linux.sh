2b6177a831bd907e1d46cb256d64bd68e52915ac
Matt Clay <matt@mystile.com>
2017-08-31 23:07:51 -0700

    Revert "Run only uri test."
    This reverts commit a45e4fd2c53b7e35813036e50a53ba5e36b6f2d6.

--- test/utils/shippable/linux.sh
+++ test/utils/shippable/linux.sh
@@ -6,7 +6,7 @@ declare -a args
 IFS='/:' read -ra args <<< "${TEST}"
 
 image="ansible/ansible:${args[1]}"
-target="uri"
+target="posix/ci/group${args[2]}/"
 
 # shellcheck disable=SC2086
 ansible-test integration --color -v --retry-on-error "${target}" --docker "${image}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
