3a797fea0c9a8dfea37807d05d79e895e108ed46
Gawain <gaowei3@qq.com>
2017-09-05 16:21:56 +0800

    Update play_context.py
    fix pep8

--- lib/ansible/playbook/play_context.py
+++ lib/ansible/playbook/play_context.py
@@ -255,13 +255,13 @@ class PlayContext(Base):
     def __init__(self, play=None, options=None, passwords=None, connection_lockfd=None):
 
         super(PlayContext, self).__init__()
-        
+
         if self.only_tags:
-           self.only_tags = set()
+            self.only_tags = set()
 
         if self.skip_tags:
-           self.skip_tags = set()
-        
+            self.skip_tags = set()
+
         if passwords is None:
             passwords = {}
 
