55c7fc197edf1190ff1f4775f2947aa90bca7714
Brian Coca <brian.coca+git@gmail.com>
2017-09-05 18:45:03 -0400

    self note

--- lib/ansible/cli/doc.py
+++ lib/ansible/cli/doc.py
@@ -118,6 +118,7 @@ class DocCLI(CLI):
             paths = loader._get_paths()
             for path in paths:
                 self.find_plugins(path, plugin_type)
+                raise AnsibleError("Need to reimplement, its all bcoca's fault!!!")
                 #TODO: call self to display all plugins
         elif len(self.args) == 0:
             raise AnsibleOptionsError("Incorrect options passed")
