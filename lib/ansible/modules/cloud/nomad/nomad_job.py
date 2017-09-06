3db0a219c92f6fd983fb589ef68271981879272d
Sirk Johannsen <sirk.johannsen@simplesurance.de>
2017-09-06 09:35:58 +0200

    Fix boilerplate tests

--- lib/ansible/modules/cloud/nomad/nomad_job.py
+++ lib/ansible/modules/cloud/nomad/nomad_job.py
@@ -14,6 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
 ANSIBLE_METADATA = {'status': ['preview'],
                     'supported_by': 'community',
                     'metadata_version': '1.1'}
