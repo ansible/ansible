5430b8f24e9c4d504f0123a390231f20431fd2ab
Ryan Brown <sb@ryansb.com>
2017-09-06 11:15:41 -0400

    Move selected AWS modules from community/certified to core support (#28617)

--- lib/ansible/modules/cloud/amazon/ec2_vol.py
+++ lib/ansible/modules/cloud/amazon/ec2_vol.py
@@ -16,7 +16,7 @@
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['stableinterface'],
-                    'supported_by': 'certified'}
+                    'supported_by': 'core'}
 
 
 DOCUMENTATION = '''
