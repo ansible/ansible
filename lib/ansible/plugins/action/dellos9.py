bf9ca8278fedea5c258606d3e6a5517fd9cb0681
Mark Goddard <mark@stackhpc.com>
2017-09-14 15:40:50 +0100

    Fix Dell OS network module timeout (#30355)
    The dellos action plugins should add the remote address of the switch
    provider to the play context. This was fixed in issue #23589 in an
    almost identical manner for the eos, ios, iosxr, and vyos action
    plugins.
    
    Fixes: #30350

--- lib/ansible/plugins/action/dellos9.py
+++ lib/ansible/plugins/action/dellos9.py
@@ -52,6 +52,7 @@ class ActionModule(_ActionModule):
         pc = copy.deepcopy(self._play_context)
         pc.connection = 'network_cli'
         pc.network_os = 'dellos9'
+        pc.remote_addr = provider['host'] or self._play_context.remote_addr
         pc.port = int(provider['port'] or self._play_context.port or 22)
         pc.remote_user = provider['username'] or self._play_context.connection_user
         pc.password = provider['password'] or self._play_context.password
