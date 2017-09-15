1e973e489ca7f4c9fe4dfda68ae9b2d497b574ec
Gawain <gaowei3@qq.com>
2017-09-15 19:24:14 +0800

    Update vmware_host_facts.py
    fix something for recommended

--- lib/ansible/modules/cloud/vmware/vmware_host_facts.py
+++ lib/ansible/modules/cloud/vmware/vmware_host_facts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# (c) 2017, Wei Gao <gaowei3@qq.com>
+# Copyright: (c) 2017, Wei Gao <gaowei3@qq.com>
 #
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
@@ -18,37 +18,32 @@ DOCUMENTATION = '''
 module: vmware_host_facts
 short_description: Gathers facts about remote vmware host
 description:
-    - Gathers facts about remote vmware host
+    - Gathers facts about remote vmware host.
 version_added: 2.5
 author:
     - Wei Gao (@woshihaoren)
-notes:
-    - Tested on vSphere 5.5
 requirements:
-    - "python >= 2.6"
+    - python >= 2.6
     - PyVmomi
 extends_documentation_fragment: vmware.documentation
 '''
 
 EXAMPLES = '''
 - name: Gather vmware host facts
-  local_action:
-    module: vmware_host_facts
+  vmware_host_facts:
     hostname: esxi_ip_or_hostname
     username: username
     password: password
+  register: host_facts
+  delegate_to: localhost
 '''
 
 RETURN = '''
 instance:
-    description: system info about the host machine
-    returned: always
-    type: dict
-    sample: None
 '''
 
+from ansible.module_utils.basic import AnsibleModule, bytes_to_human
 from ansible.module_utils.vmware import connect_to_api, vmware_argument_spec, find_obj
-from ansible.module_utils.basic import AnsibleModule
 
 try:
     from pyVmomi import vim, vmodl
@@ -62,7 +57,7 @@ def get_cpu_facts(host):
         'ansible_processor': host.summary.hardware.cpuModel,
         'ansible_processor_cores': host.summary.hardware.numCpuCores,
         'ansible_processor_count': host.summary.hardware.numCpuPkgs,
-        'ansible_processor_vcpus': host.summary.hardware.numCpuThreads
+        'ansible_processor_vcpus': host.summary.hardware.numCpuThreads,
     }
     return facts
 
@@ -81,8 +76,8 @@ def get_datastore_facts(host):
     for store in host.datastore:
         _tmp = {
             'name': store.summary.name,
-            'total': sizeof_fmt(store.summary.capacity),
-            'free': sizeof_fmt(store.summary.freeSpace)
+            'total': bytes_to_human(store.summary.capacity),
+            'free': bytes_to_human(store.summary.freeSpace),
         }
         facts['ansible_datastore'].append(_tmp)
     return facts
@@ -100,10 +95,10 @@ def get_network_facts(host):
             'device': device,
             'ipv4': {
                 'address': nic.spec.ip.ipAddress,
-                'netmask': nic.spec.ip.subnetMask
+                'netmask': nic.spec.ip.subnetMask,
             },
             'macaddress': nic.spec.mac,
-            'mtu': nic.spec.mtu
+            'mtu': nic.spec.mtu,
         }
         facts['ansible_' + device] = _tmp
     return facts
@@ -124,23 +119,9 @@ def get_system_facts(host):
         'ansible_product_name': host.hardware.systemInfo.model,
         'ansible_product_serial': sn,
         'ansible_bios_date': host.hardware.biosInfo.releaseDate,
-        'ansible_bios_version': host.hardware.biosInfo.biosVersion
+        'ansible_bios_version': host.hardware.biosInfo.biosVersion,
     }
     return facts
-
-
-def sizeof_fmt(num):
-    """
-    Returns the human readable version of a file size
-
-    :param num:
-    :return:
-    """
-    for item in ['bytes', 'KB', 'MB', 'GB']:
-        if num < 1024.0:
-            return "%3.1f%s" % (num, item)
-        num /= 1024.0
-    return "%3.1f%s" % (num, 'TB')
 
 
 def all_facts(content):
@@ -162,16 +143,9 @@ def main():
     if not HAS_PYVMOMI:
         module.fail_json(msg='pyvmomi is required for this module')
 
-    try:
-        content = connect_to_api(module)
-        data = all_facts(content)
-        module.exit_json(changed=False, ansible_facts=data)
-    except vmodl.RuntimeFault as runtime_fault:
-        module.fail_json(msg=runtime_fault.msg)
-    except vmodl.MethodFault as method_fault:
-        module.fail_json(msg=method_fault.msg)
-    except Exception as e:
-        module.fail_json(msg=str(e))
+    content = connect_to_api(module)
+    data = all_facts(content)
+    module.exit_json(changed=False, ansible_facts=data)
 
 
 if __name__ == '__main__':
