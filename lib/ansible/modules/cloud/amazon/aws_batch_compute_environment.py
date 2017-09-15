8361f48f6d093bf1ec2d0056b819e4211d6b78de
Supreeth Gowda <supreeth.gowda@sonos.com>
2017-09-15 12:28:46 -0400

    Fixing ansibot error: Adding ANSIBLE_METADATA with a newer version number

--- lib/ansible/modules/cloud/amazon/aws_batch_compute_environment.py
+++ lib/ansible/modules/cloud/amazon/aws_batch_compute_environment.py
@@ -14,6 +14,11 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
 DOCUMENTATION = '''
 ---
 module: aws_batch_compute_environment
