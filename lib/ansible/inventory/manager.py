c16a2e1e4481b570fe5e24f4d7abb2e34f6b059d
Brian Coca <bcoca@ansible.com>
2017-09-15 13:13:41 -0400

    typo fix

--- lib/ansible/inventory/manager.py
+++ lib/ansible/inventory/manager.py
@@ -268,7 +268,7 @@ class InventoryManager(object):
                 if not parsed and failures:
                     # only if no plugin processed files should we show errors.
                     if C.INVENTORY_UNPARSED_IS_FAILED:
-                        msg = "Could not parse inventory source %s with availabel plugins:\n" % source
+                        msg = "Could not parse inventory source %s with available plugins:\n" % source
                         for fail in failures:
                             msg += 'Plugin %s failed: %s\n' % (fail['plugin'], to_native(fail['exc']))
                             if display.verbosity >= 3:
