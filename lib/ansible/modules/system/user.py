3a0d5803012c977599d6b257d7f26b2c88e8765d
Reid Wahl <nrwahl@protonmail.com>
2017-09-17 16:15:40 -0500

    modules/system/user.py: Change createhome to create_home (#30179)

--- lib/ansible/modules/system/user.py
+++ lib/ansible/modules/system/user.py
@@ -83,7 +83,7 @@ options:
     skeleton:
         required: false
         description:
-            - Optionally set a home skeleton directory. Requires createhome option!
+            - Optionally set a home skeleton directory. Requires create_home option!
         version_added: "2.0"
     password:
         required: false
@@ -100,14 +100,15 @@ options:
         choices: [ present, absent ]
         description:
             - Whether the account should exist or not, taking action if the state is different from what is stated.
-    createhome:
-        required: false
-        default: "yes"
-        choices: [ "yes", "no" ]
+    create_home:
         description:
             - Unless set to C(no), a home directory will be made for the user
               when the account is created or if the home directory does not
               exist.
+            - Changed from C(createhome) to C(create_home) in version 2.5.
+        default: yes
+        type: bool
+        aliases: ['createhome']
     move_home:
         required: false
         default: "no"
@@ -298,7 +299,7 @@ class User(object):
         self.password   = module.params['password']
         self.force      = module.params['force']
         self.remove     = module.params['remove']
-        self.createhome = module.params['createhome']
+        self.create_home = module.params['create_home']
         self.move_home  = module.params['move_home']
         self.skeleton   = module.params['skeleton']
         self.system     = module.params['system']
@@ -419,7 +420,7 @@ class User(object):
             cmd.append('-p')
             cmd.append(self.password)
 
-        if self.createhome:
+        if self.create_home:
             cmd.append('-m')
 
             if self.skeleton is not None:
@@ -815,7 +816,7 @@ class FreeBsdUser(User):
             cmd.append('-G')
             cmd.append(','.join(groups))
 
-        if self.createhome:
+        if self.create_home:
             cmd.append('-m')
 
             if self.skeleton is not None:
@@ -1013,7 +1014,7 @@ class OpenBSDUser(User):
             cmd.append('-p')
             cmd.append(self.password)
 
-        if self.createhome:
+        if self.create_home:
             cmd.append('-m')
 
             if self.skeleton is not None:
@@ -1183,7 +1184,7 @@ class NetBSDUser(User):
             cmd.append('-p')
             cmd.append(self.password)
 
-        if self.createhome:
+        if self.create_home:
             cmd.append('-m')
 
             if self.skeleton is not None:
@@ -1360,7 +1361,7 @@ class SunOS(User):
             cmd.append('-s')
             cmd.append(self.shell)
 
-        if self.createhome:
+        if self.create_home:
             cmd.append('-m')
 
             if self.skeleton is not None:
@@ -1755,7 +1756,7 @@ class DarwinUser(User):
             self.uid = str(self._get_next_uid(self.system))
 
         # Homedir is not created by default
-        if self.createhome:
+        if self.create_home:
             if self.home is None:
                 self.home = '/Users/%s' % self.name
             if not self.module.check_mode:
@@ -1888,7 +1889,7 @@ class AIX(User):
             cmd.append('-s')
             cmd.append(self.shell)
 
-        if self.createhome:
+        if self.create_home:
             cmd.append('-m')
 
             if self.skeleton is not None:
@@ -2038,7 +2039,7 @@ class HPUX(User):
             cmd.append('-p')
             cmd.append(self.password)
 
-        if self.createhome:
+        if self.create_home:
             cmd.append('-m')
         else:
             cmd.append('-M')
@@ -2160,7 +2161,7 @@ def main():
             force=dict(default='no', type='bool'),
             remove=dict(default='no', type='bool'),
             # following options are specific to useradd
-            createhome=dict(default='yes', type='bool'),
+            create_home=dict(default='yes', aliases=['createhome'], type='bool'),
             skeleton=dict(default=None, type='str'),
             system=dict(default='no', type='bool'),
             # following options are specific to usermod
@@ -2210,7 +2211,7 @@ def main():
                 result['system'] = user.name
             else:
                 result['system'] = user.system
-                result['createhome'] = user.createhome
+                result['create_home'] = user.create_home
         else:
             # modify user (note: this function is check mode aware)
             (rc, out, err) = user.modify_user()
@@ -2248,7 +2249,7 @@ def main():
         info = user.user_info()
         if user.home is None:
             user.home = info[5]
-        if not os.path.exists(user.home) and user.createhome:
+        if not os.path.exists(user.home) and user.create_home:
             if not module.check_mode:
                 user.create_homedir(user.home)
                 user.chown_homedir(info[2], info[3], user.home)
