8138f6772c0e01dfea8d182354b7a1a06bcda8ea
Ivan Pepelnjak <ip@ipspace.net>
2017-09-18 13:16:37 +0000

    Fixed indentation (tabs ==> spaces)

--- lib/ansible/plugins/filter/network.py
+++ lib/ansible/plugins/filter/network.py
@@ -56,9 +56,9 @@ def re_matchall(regex, value):
         obj = {}
         if regex.groupindex:
             for name, index in iteritems(regex.groupindex):
-		if len(regex.groupindex) == 1:
-		    obj[name] = match
-		else:
+                if len(regex.groupindex) == 1:
+                    obj[name] = match
+                else:
                     obj[name] = match[index - 1]
             objects.append(obj)
     return objects
