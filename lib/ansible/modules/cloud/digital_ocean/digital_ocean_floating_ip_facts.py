28c30f06e4eb4bf1ff6da5fee1c8973dd653ac8b
Patrick F. Marques <patrickfmarques@gmail.com>
2017-09-18 14:15:50 +0100

    Add timeout documentation

--- lib/ansible/modules/cloud/digital_ocean/digital_ocean_floating_ip_facts.py
+++ lib/ansible/modules/cloud/digital_ocean/digital_ocean_floating_ip_facts.py
@@ -25,6 +25,10 @@ options:
     description:
      - DigitalOcean OAuth token.
     required: true
+  timeout:
+    description:
+    - The timeout in seconds used for polling DigitalOcean's API.
+    default: 30
 
 notes:
   - Version 2 of DigitalOcean API is used.
