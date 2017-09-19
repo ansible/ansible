6a4489def8aac7d57f67b7d3b75f3630dfd6c357
Trishna Guha <trishnaguha17@gmail.com>
2017-09-19 14:46:41 +0530

    fix nxos_reboot command format
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/modules/network/nxos/nxos_reboot.py
+++ lib/ansible/modules/network/nxos/nxos_reboot.py
@@ -67,7 +67,7 @@ from ansible.module_utils.basic import AnsibleModule
 
 def reboot(module):
     cmds = [
-        {'command': 'terminal-dont-ask'},
+        {'command': 'terminal dont-ask', 'output': 'text'},
         {'command': 'reload', 'output': 'text'}
     ]
     run_commands(module, cmds)
