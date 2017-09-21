567b236ebf2507bb51b890d00aed4bad7989faf3
Hervé Beraud <hervberaud.pro@gmail.com>
2017-09-21 17:59:14 +0200

    [fix] py3 compatibility

--- docs/bin/generate_man.py
+++ docs/bin/generate_man.py
@@ -31,14 +31,19 @@ def trim_docstring(docstring):
     # and split into a list of lines:
     lines = docstring.expandtabs().splitlines()
     # Determine minimum indentation (first line doesn't count):
-    indent = sys.maxsize
+    try:
+        indent = sys.maxint
+        limit = sys.maxint
+    except AttributeError:
+        indent = sys.maxsize
+        limit = sys.maxsize
     for line in lines[1:]:
         stripped = line.lstrip()
         if stripped:
             indent = min(indent, len(line) - len(stripped))
     # Remove indentation (first line is special):
     trimmed = [lines[0].strip()]
-    if indent < sys.maxsize:
+    if indent < limit:
         for line in lines[1:]:
             trimmed.append(line[indent:].rstrip())
     # Strip off trailing and leading blank lines:
