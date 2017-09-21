84d853b460a1d54ee9f9229d9a3a5a28b0cd3c65
Dag Wieers <dag@wieers.com>
2017-09-21 21:37:15 +0200

    win_shortcut: Add hotkeys examples to documentation (#30694)
    This fixes #29110

--- lib/ansible/modules/windows/win_shortcut.ps1
+++ lib/ansible/modules/windows/win_shortcut.ps1
@@ -1,29 +1,15 @@
 #!powershell
-# (c) 2016, Dag Wieers <dag@wieers.com>
-#
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
-# WANT_JSON
-# POWERSHELL_COMMON
+# Copyright: (c) 2016, Dag Wieers (@dagwieers) <dag@wieers.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 # Based on: http://powershellblogger.com/2016/01/create-shortcuts-lnk-or-url-files-with-powershell/
 
+#Requires -Module Ansible.ModuleUtils.Legacy.psm1
+
 $ErrorActionPreference = "Stop"
 
-$params = Parse-Args $args -supports_check_mode $true
+$params = Parse-Args -arguments $args -supports_check_mode $true
 $check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
 
 $orig_src = Get-AnsibleParam -obj $params -name "src"
@@ -149,4 +135,4 @@ If ($state -eq "absent") {
     }
 }
 
-Exit-Json $result
+Exit-Json -obj $result
