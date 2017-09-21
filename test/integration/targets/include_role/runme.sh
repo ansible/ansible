c5a1842c459ac6a043004333e654db0e3b68d2f9
Adrian Likins <alikins@redhat.com>
2017-09-21 12:53:42 -0400

    Add test cases based on https://github.com/ansible/ansible/issues/30700

--- test/integration/targets/include_role/runme.sh
+++ test/integration/targets/include_role/runme.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+set -ex
 
-ansible-playbook -i ../../inventory $@ test_include_role.yml
-ansible-playbook -i ../../inventory $@ test_include_role_bogus_option.yml && :
+ansible-playbook -i ../../inventory "$@" test_include_role_with_vars.yml
+ansible-playbook -i ../../inventory "$@" test_include_role_no_vars.yml
+
+ansible-playbook -i ../../inventory "$@" test_include_role.yml
+ansible-playbook -i ../../inventory "$@" test_include_role_bogus_option.yml && :
 WRONG_RC=$?
 echo "rc was $WRONG_RC (4 is expected)"
 [ $WRONG_RC -eq 4 ]
