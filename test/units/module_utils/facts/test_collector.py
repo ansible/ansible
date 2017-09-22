c0752744430edd051573f17acfecd79183a0ad25
Adrian Likins <alikins@redhat.com>
2017-09-22 16:04:12 -0400

    more unit tests

--- test/units/module_utils/facts/test_collector.py
+++ test/units/module_utils/facts/test_collector.py
@@ -64,15 +64,67 @@ class TestSelectCollectorNames(unittest.TestCase):
     def test(self):
         collector_names = set(['distribution', 'all_ipv4_addresses',
                                'local', 'pkg_mgr'])
+        all_fact_subsets = self._all_fact_subsets()
+        all_collector_classes = self._all_collector_classes()
+        res = collector.select_collector_classes(collector_names,
+                                                 all_fact_subsets,
+                                                 all_collector_classes)
+        pprint.pprint(res)
+
+        expected = [default_collectors.DistributionFactCollector,
+                    default_collectors.PkgMgrFactCollector]
+
+        self.assertEqual(res, expected)
+
+    def test_reverse(self):
+        collector_names = set(['distribution', 'all_ipv4_addresses',
+                               'local', 'pkg_mgr'])
+        all_fact_subsets = self._all_fact_subsets()
+        all_collector_classes = self._all_collector_classes()
+        all_collector_classes.reverse()
+        res = collector.select_collector_classes(collector_names,
+                                                 all_fact_subsets,
+                                                 all_collector_classes)
+        pprint.pprint(res)
+
+        expected = [default_collectors.PkgMgrFactCollector,
+                    default_collectors.DistributionFactCollector]
+
+        self.assertEqual(res, expected)
+
+    def test_default_collectors(self):
+        platform_info = {'system': 'Generic'}
+        compat_platforms = [platform_info]
+        collectors_for_platform = collector.find_collectors_for_platform(default_collectors.collectors,
+                                                                         compat_platforms)
+
+        all_fact_subsets, aliases_map = collector.build_fact_id_to_collector_map(collectors_for_platform)
+        pprint.pprint((dict(all_fact_subsets), dict(aliases_map)))
+
+        all_valid_subsets = frozenset(all_fact_subsets.keys())
+        collector_names = collector.get_collector_names(valid_subsets=all_valid_subsets,
+                                                        aliases_map=aliases_map,
+                                                        platform_info=platform_info)
+        res = collector.select_collector_classes(collector_names,
+                                                 all_fact_subsets,
+                                                 default_collectors.collectors)
+
+        pprint.pprint(res)
+
+    def _all_collector_classes(self):
+        return [default_collectors.DistributionFactCollector,
+                default_collectors.PkgMgrFactCollector,
+                default_collectors.LinuxNetworkCollector]
+
+    def _all_fact_subsets(self, data=None):
         all_fact_subsets = defaultdict(list)
         _data = {'pkg_mgr': [default_collectors.PkgMgrFactCollector],
                  'distribution': [default_collectors.DistributionFactCollector],
                  'network': [default_collectors.LinuxNetworkCollector]}
-        for key, value in _data.items():
+        data = data or _data
+        for key, value in data.items():
             all_fact_subsets[key] = value
-
-        res = collector.select_collector_classes(collector_names, all_fact_subsets)
-        pprint.pprint(res)
+        return all_fact_subsets
 
 
 class TestGetCollectorNames(unittest.TestCase):
