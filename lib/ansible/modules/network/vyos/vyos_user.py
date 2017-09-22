2ed02a9b53629986212d6352d4d38b5a8c3fb781
Ganesh B. Nalawade <gnalawad@redhat.com>
2017-09-22 15:11:39 +0530

    Remove check_args for vyos modules
    *  With addition of provider sub option validation check_args()
       is no longer required.

--- lib/ansible/modules/network/vyos/vyos_user.py
+++ lib/ansible/modules/network/vyos/vyos_user.py
@@ -136,7 +136,7 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.network_common import remove_default_spec
 from ansible.module_utils.vyos import get_config, load_config
 from ansible.module_utils.six import iteritems
-from ansible.module_utils.vyos import vyos_argument_spec, check_args
+from ansible.module_utils.vyos import vyos_argument_spec
 
 
 def validate_level(value, module):
@@ -308,8 +308,6 @@ def main():
             'The "password" argument is used to authenticate the current connection. ' +
             'To set a user password use "configured_password" instead.'
         )
-
-    check_args(module, warnings)
 
     result = {'changed': False}
     if warnings:
