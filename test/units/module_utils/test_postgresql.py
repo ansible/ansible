a392aceda42646c6b0bcc2551c1e91c27fa165b0
Pierre-Louis Bonicoli <pierre-louis.bonicoli@libregerbil.fr>
2017-09-25 10:50:01 +0200

    PostgreSQL: add unit test for escape_identifier_cursor

--- test/units/module_utils/test_postgresql.py
+++ test/units/module_utils/test_postgresql.py
@@ -4,15 +4,20 @@ import sys
 from ansible.compat.tests import unittest
 from ansible.compat.tests.mock import patch, MagicMock
 
-from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils import basic
 from ansible.module_utils.six.moves import builtins
-from ansible.module_utils._text import to_native
+from ansible.module_utils._text import to_native, to_bytes
 from units.mock.procenv import swap_stdin_and_argv
 
 
 import pprint
 
 realimport = builtins.__import__
+
+
+class MockCursor(object):
+    def execute(self, query, vars=None):
+        return query
 
 
 class TestPostgres(unittest.TestCase):
@@ -33,6 +38,7 @@ class TestPostgres(unittest.TestCase):
         mod = builtins.__import__('ansible.module_utils.postgres')
 
         self.assertFalse(mod.module_utils.postgres.HAS_PSYCOPG2)
+        self.assertFalse(mod.module_utils.postgres.HAS_PSYCOPG2_QUOTE_IDENT)
 
         with self.assertRaises(mod.module_utils.postgres.LibraryError) as context:
             mod.module_utils.postgres.ensure_libs(sslrootcert=None)
@@ -50,6 +56,7 @@ class TestPostgres(unittest.TestCase):
         mod = builtins.__import__('ansible.module_utils.postgres')
 
         self.assertTrue(mod.module_utils.postgres.HAS_PSYCOPG2)
+        self.assertTrue(mod.module_utils.postgres.HAS_PSYCOPG2_QUOTE_IDENT)
 
         ensure_ret = mod.module_utils.postgres.ensure_libs(sslrootcert=None)
         self.assertFalse(ensure_ret)
@@ -73,3 +80,54 @@ class TestPostgres(unittest.TestCase):
         with self.assertRaises(mod.module_utils.postgres.LibraryError) as context:
             mod.module_utils.postgres.ensure_libs(sslrootcert='yes')
         self.assertIn('psycopg2 must be at least 2.4.3 in order to use', to_native(context.exception))
+
+    @patch.object(builtins, '__import__')
+    def test_escape_identifier_cursor(self, mock_import):
+        self._psycopg2_mock = MagicMock()
+        self._psycopg2_mock.__version__ = '2.4.1'
+        self._psycopg2_mock.quote_ident.return_value = 'quote_ident'
+
+        def _mock_import(name, *args, **kwargs):
+            if 'psycopg2' in name:
+                return self._psycopg2_mock
+            return realimport(name, *args, **kwargs)
+
+        # module.warning required needed for escape_identifier_cursor
+        args = json.dumps({'ANSIBLE_MODULE_ARGS': {}})
+        basic._ANSIBLE_ARGS = to_bytes(args)
+        module = basic.AnsibleModule({})
+
+        self.clear_modules(['psycopg2.extras', 'psycopg2.extensions', 'psycopg2', 'ansible.module_utils.postgres'])
+        mock_import.side_effect = _mock_import
+        mod = builtins.__import__('ansible.module_utils.postgres')
+
+        klass = mod.module_utils.postgres.escape_identifier_cursor(module, MockCursor)
+        cursor = klass()
+
+        with patch.object(cursor, 'escape_identifier', return_value='escape_identifier',
+                          autospec=True) as mock_escape_identifier:
+
+            mod.module_utils.postgres.HAS_PSYCOPG2 = True
+            mod.module_utils.postgres.HAS_LIBPQ_QUOTE_IDENT = False
+            mod.module_utils.postgres.HAS_PSYCOPG2_QUOTE_IDENT = False
+            ret = cursor.execute('select * from ${table}', identifiers={'table': 'users'})
+            self.assertEqual(self._psycopg2_mock.quote_ident.call_count, 0)
+            self.assertEqual(mock_escape_identifier.call_count, 0)
+            self.assertEqual(mock_escape_identifier.call_count, 0)
+            self.assertEqual(ret, 'select * from %(table)s')
+
+            mod.module_utils.postgres.HAS_PSYCOPG2 = True
+            mod.module_utils.postgres.HAS_LIBPQ_QUOTE_IDENT = True
+            mod.module_utils.postgres.HAS_PSYCOPG2_QUOTE_IDENT = True
+            ret = cursor.execute('select * from ${table}', identifiers={'table': 'users'})
+            self.assertEqual(self._psycopg2_mock.quote_ident.call_count, 1)
+            self.assertEqual(mock_escape_identifier.call_count, 0)
+            self.assertEqual(ret, 'select * from quote_ident')
+
+            mod.module_utils.postgres.HAS_PSYCOPG2 = True
+            mod.module_utils.postgres.HAS_PSYCOPG2_QUOTE_IDENT = False
+            mod.module_utils.postgres.HAS_LIBPQ_QUOTE_IDENT = True
+            ret = cursor.execute('select * from ${table}', identifiers={'table': 'users'})
+            self.assertEqual(self._psycopg2_mock.quote_ident.call_count, 1)
+            self.assertEqual(mock_escape_identifier.call_count, 1)
+            self.assertEqual(ret, 'select * from escape_identifier')
