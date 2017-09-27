05efce2472a0808ab3abcdd0e9d5fca9ad4a5cb4
Brian Coca <bcoca@ansible.com>
2017-09-27 09:53:56 -0400

    rmeove ext requirement for yaml inventory plugin
    returns to the state before 2.4 made it a requirement
    fixes #30855

--- lib/ansible/plugins/inventory/yaml.py
+++ lib/ansible/plugins/inventory/yaml.py
@@ -74,7 +74,7 @@ class InventoryModule(BaseFileInventoryPlugin):
         valid = False
         if super(InventoryModule, self).verify_file(path):
             file_name, ext = os.path.splitext(path)
-            if ext and ext in C.YAML_FILENAME_EXTENSIONS:
+            if not ext or ext in C.YAML_FILENAME_EXTENSIONS:
                 valid = True
         return valid
 
