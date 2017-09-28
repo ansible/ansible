4f387d449d23153b426f3d1870ca86373444cab7
Matt Clay <matt@mystile.com>
2017-09-28 01:23:35 -0700

    Remove hacks.

--- lib/ansible/inventory/group.py
+++ lib/ansible/inventory/group.py
@@ -67,23 +67,12 @@ class Group:
 
         return result
 
-    def safe_repr(self, value):
-        try:
-            return value.__repr__()
-        except Exception:
-            return type(value)
-
     def deserialize(self, data):
         self.__init__()
         self.name = data.get('name')
         self.vars = data.get('vars', dict())
         self.depth = data.get('depth', 0)
         self.hosts = data.get('hosts', [])
-
-        for host in self.hosts:
-            if not hasattr(host, 'name'):
-                host_debug = ', '.join(self.safe_repr(key) for key in self.hosts)
-                raise AnsibleError('One or more hosts in group %s do not have a name: %s' % (self.name, host_debug))
 
         self._hosts = set(self.hosts)
 
