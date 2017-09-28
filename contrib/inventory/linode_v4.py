95ecf60ca23fd25bd155938a6ea7ea1bcce1d08c
rwaweber <rwaweber@gmail.com>
2017-09-27 22:44:51 -0400

    linting fixes

--- contrib/inventory/linode_v4.py
+++ contrib/inventory/linode_v4.py
@@ -34,7 +34,7 @@ In order to generate a key for the legacy api, you must do so on the old
 manager; https://manager.linode.com. Likewise goes for the v4 api, you must go
 here: https://cloud.linode.com.
 
-By default, will only show running instances. You can export LINODE_ALL to 
+By default, will only show running instances. You can export LINODE_ALL to
 target all linodes associated with your account.
 
 By default, will use the v4 API. You can export LINODE_LEGACY to use the legacy
@@ -93,31 +93,31 @@ import http.client
 import json
 import sys
 
-def _empty_inventory():
-    return {"_meta": {"hostvars": {}}}
 
-def linode_status(status_id):
+def _linode_status(status_id):
     enum = {
-            -1: "Being Created",
-             0: "Brand New",
-             1: "Running",
-             2: "Powered Off",
-           }
+        -1: "Being Created",
+        0: "Brand New",
+        1: "Running",
+        2: "Powered Off",
+    }
     return enum[status_id]
 
-def datacenter_lookup(dc_id):
+
+def _datacenter_lookup(dc_id):
     enum = {
-      	    2: "dallas",
-            3: "fremont",
-      	    4: "atlanta",
-            6: "newark",
-            7: "london",
-	    8: "tokyo",
-	    9: "singapore",
-   	   10: "frankfurt",
-           11: "shinagawa1",
-           }
+        2: "dallas",
+        3: "fremont",
+        4: "atlanta",
+        6: "newark",
+        7: "london",
+        8: "tokyo",
+        9: "singapore",
+        10: "frankfurt",
+        11: "shinagawa1",
+    }
     return enum[dc_id]
+
 
 def call(legacy, all_linodes, token):
     '''
@@ -130,7 +130,7 @@ def call(legacy, all_linodes, token):
         conn.request("GET", "/?api_key={}&api_action=linode.ip.list".format(
             token))
         response = conn.getresponse()
-        
+
         # check if status code not OK, though it seems to always return 200s...
         if response.status != 200:
             conn.close()
@@ -143,7 +143,7 @@ def call(legacy, all_linodes, token):
         ip_dict = json.loads(response.read())
 
         # check if error returned from linode api
-        if len(ip_dict["ERRORARRAY"]) != 0:
+        if ip_dict["ERRORARRAY"]:
             conn.close()
             ansible_vars_dict['_meta']['error'] = {
                 "reason": ip_dict["ERRORARRAY"],
@@ -161,26 +161,24 @@ def call(legacy, all_linodes, token):
                     "ansible_host": ipitem['IPADDRESS'],
                     "ansible_ssh_host": ipitem['IPADDRESS'],
                     "linode_id": ipitem["LINODEID"],
-                    "status": linode_status(elem["STATUS"]),
+                    "status": _linode_status(elem["STATUS"]),
                     "distro": elem["DISTRIBUTIONVENDOR"],
-                    "datacenter": datacenter_lookup(elem["DATACENTERID"]),
+                    "datacenter": _datacenter_lookup(elem["DATACENTERID"]),
                 }
-                # 2 dimensional iteration to gather as many facts as possible
-                # about the linodes listed on the account.
                 for elem in lin_dict["DATA"] for ipitem in ip_dict["DATA"]
                 # filter linodes by those running or when all_linodes is true
                 # return only unique ones.
-                if ipitem["LINODEID"] == elem["LINODEID"] and 
-                (linode_status(elem["STATUS"]) == "Running") or
+                if ipitem["LINODEID"] == elem["LINODEID"] and
+                (_linode_status(elem["STATUS"]) == "Running") or
                 ipitem["LINODEID"] == elem["LINODEID"] and all_linodes
             }
         ansible_vars_dict[""] = list(
-                ansible_vars_dict['_meta']['hostvars'].keys())
+            ansible_vars_dict['_meta']['hostvars'].keys())
         return ansible_vars_dict
 
     else:
         conn.request("GET", "/v4/linode/instances",
-                headers={"Authorization": "Bearer {}".format(token)})
+                     headers={"Authorization": "Bearer {}".format(token)})
         response = conn.getresponse()
         if response.status != 200:
             conn.close()
@@ -191,44 +189,45 @@ def call(legacy, all_linodes, token):
             return ansible_vars_dict
         resp_json = json.loads(response.read())
         ansible_vars_dict['_meta']['hostvars'] = {
-                datum['label']: {
-                    "public_ip": datum['ipv4'][0],
-                    "ansible_host": datum['ipv4'][0],
-                    "ansible_ssh_host": datum['ipv4'][0],
-                    "linode_id": datum['id'],
-                    "status": datum['status'],
-                    "distro": datum['distribution'],
-                    "group": datum['group'],
-                    "ipv6": datum['ipv6'],
-                    "region": datum['region'],
-                }
+            datum['label']: {
+                "public_ip": datum['ipv4'][0],
+                "ansible_host": datum['ipv4'][0],
+                "ansible_ssh_host": datum['ipv4'][0],
+                "linode_id": datum['id'],
+                "status": datum['status'],
+                "distro": datum['distribution'],
+                "group": datum['group'],
+                "ipv6": datum['ipv6'],
+                "region": datum['region'],
+            }
             for datum in resp_json['data']
             if datum['status'] == 'running' or all_linodes
         }
         ansible_vars_dict[""] = list(
-                ansible_vars_dict['_meta']['hostvars'].keys())
+            ansible_vars_dict['_meta']['hostvars'].keys())
         return ansible_vars_dict
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-            description='dynamic inventory based on Linode APIs')
-    parser.add_argument('--list', action='store_true', default=True,
-                        help='Show instances')
-    p = parser.parse_args()
+    PARSER = argparse.ArgumentParser(
+        description='dynamic inventory based on Linode APIs')
+    PARSER.add_argument('--list', action='store_true', default=True,
+                        help='Placate inventory requirement')
+    _ = PARSER.parse_args()
 
     try:
-        legacy = os.environ['LINODE_LEGACY']
-    except KeyError as e:
-        legacy = False
+        LEGACY_ENV = os.environ['LINODE_LEGACY']
+    except KeyError:
+        LEGACY_ENV = False
 
     try:
-        all_linodes = os.environ['LINODE_ALL']
-    except KeyError as e:
-        all_linodes = False
+        ALL_LINODES_ENV = os.environ['LINODE_ALL']
+    except KeyError:
+        ALL_LINODES_ENV = False
 
     try:
-        token = os.environ['LINODE_API_KEY']
-    except KeyError as e:
+        TOKEN_ENV = os.environ['LINODE_API_KEY']
+    except KeyError:
         print('Please export LINODE_API_KEY')
         sys.exit(1)
-    print(json.dumps(call(legacy, all_linodes, token)))
+    print(json.dumps(call(LEGACY_ENV, ALL_LINODES_ENV, TOKEN_ENV)))
