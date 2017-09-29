de2277b15a4460660c4b8d4d9efa2bee7c2239f4
Peter Murray <peter.murray@osirisoft.com>
2017-09-29 16:06:26 +0100

    Fixing import failure test

--- lib/ansible/modules/source_control/github_organization.py
+++ lib/ansible/modules/source_control/github_organization.py
@@ -3,6 +3,9 @@
 # Copyright (c) 2017 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
@@ -125,8 +128,6 @@ EXAMPLES = '''
 import base64
 from ansible.module_utils.urls import fetch_url
 from ansible.module_utils.basic import AnsibleModule
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
 
 
 def _build_api_url(base_url, path):
