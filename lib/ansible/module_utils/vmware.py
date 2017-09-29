3cadf956f50b94810920cbb007dc058a5c377670
James Tanner <tanner.jc@gmail.com>
2017-09-28 20:36:54 -0400

    Use the group-d1 moId instead of i8ln based names to identify the root folder. (#31077)
    Addresses #29043

--- lib/ansible/module_utils/vmware.py
+++ lib/ansible/module_utils/vmware.py
@@ -235,7 +235,7 @@ def compile_folder_path_for_object(vobj):
     thisobj = vobj
     while hasattr(thisobj, 'parent'):
         thisobj = thisobj.parent
-        if thisobj.name == 'Datacenters':
+        if thisobj._moId == 'group-d1':
             break
         if isinstance(thisobj, vim.Folder):
             paths.append(thisobj.name)
