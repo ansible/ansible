3a1ee9fdf53f47506289bfbcdca8b0455e349e97
Kevin <kevin.klopfenstein@bedag.ch>
2017-10-02 18:34:39 +0200

    nxos_fact: cdp support

--- lib/ansible/modules/network/nxos/nxos_facts.py
+++ lib/ansible/modules/network/nxos/nxos_facts.py
@@ -350,19 +350,22 @@ class Interfaces(FactsBase):
                 objects[local_intf].append(nbor)
 
         return objects
-
+        
     def populate_neighbors_cdp(self, data):
         objects = dict()
-        for item in data['TABLE_cdp_neighbor_detail_info']['ROW_cdp_neighbor_detail_info']:
-            try:
-                local_intf = item['intf_id']
-                objects[local_intf] = list()
-                nbor = dict()
-                nbor['port'] = item['port_id']
-                nbor['host'] = item['device_id']
-                objects[local_intf].append(nbor)
-            except TypeError:
-                objects = {}
+        data = data['TABLE_cdp_neighbor_detail_info']['ROW_cdp_neighbor_detail_info']
+
+        if isinstance(data, dict):
+            data = [data]
+
+        for item in data:
+            local_intf = item['intf_id']
+            objects[local_intf] = list()
+            nbor = dict()
+            nbor['port'] = item['port_id']
+            nbor['host'] = item['device_id']
+            objects[local_intf].append(nbor)
+
         return objects
 
     def parse_ipv6_interfaces(self, data):
