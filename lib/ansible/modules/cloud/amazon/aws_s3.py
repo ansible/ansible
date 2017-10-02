fd3189690674ea2b360dda7badac63e8a494b4ab
Dave Grochowski <ThePumpingLemma@users.noreply.github.com>
2017-10-02 13:18:47 -0400

    [cloud] Fix handling of `encrypt` option in aws_s3 module (#30822)
    (cherry picked from commit e67e7785d3ae785e4aa532a17261b9ae113c8960)

--- lib/ansible/modules/cloud/amazon/aws_s3.py
+++ lib/ansible/modules/cloud/amazon/aws_s3.py
@@ -395,11 +395,12 @@ def upload_s3file(module, s3, bucket, obj, src, expiry, metadata, encrypt, heade
     if module.check_mode:
         module.exit_json(msg="PUT operation skipped - running in check mode", changed=True)
     try:
+        extra = {}
+        if encrypt:
+            extra['ServerSideEncryption'] = 'AES256'
         if metadata:
-            extra = {'Metadata': dict(metadata)}
-            s3.upload_file(Filename=src, Bucket=bucket, Key=obj, ExtraArgs=extra)
-        else:
-            s3.upload_file(Filename=src, Bucket=bucket, Key=obj)
+            extra['Metadata'] = dict(metadata)
+        s3.upload_file(Filename=src, Bucket=bucket, Key=obj, ExtraArgs=extra)
         for acl in module.params.get('permission'):
             s3.put_object_acl(ACL=acl, Bucket=bucket, Key=obj)
         url = s3.generate_presigned_url(ClientMethod='put_object',
