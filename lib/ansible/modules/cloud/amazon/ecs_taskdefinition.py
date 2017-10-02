e482af915c9f40126796243025e97a66f5d3c71a
Takuya SATO <takuya0219@gmail.com>
2017-10-02 16:57:01 +0900

    optimize code
    https://github.com/ansible/ansible/pull/31083/files/745d8ef8787463b68c2cc8d443395937dbf4a258#r142025483

--- lib/ansible/modules/cloud/amazon/ecs_taskdefinition.py
+++ lib/ansible/modules/cloud/amazon/ecs_taskdefinition.py
@@ -241,7 +241,7 @@ def main():
     task_mgr = EcsTaskManager(module)
     results = dict(changed=False)
 
-    if 'containers' in module.params and isinstance(module.params['containers'], list):
+    if isinstance(module.params.get('containers'), list):
         for container in module.params['containers']:
             if 'environment' in container:
                 for environment in container['environment']:
