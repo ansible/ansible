2df013cffd14aaaf15eb1ec3c5019bd797531b67
Prasad Katti <prasadmkatti@gmail.com>
2017-10-02 16:20:09 -0700

    Document return values for aws_s3

--- lib/ansible/modules/cloud/amazon/aws_s3.py
+++ lib/ansible/modules/cloud/amazon/aws_s3.py
@@ -243,10 +243,28 @@ msg:
   type: string
   sample: PUT operation complete
 url:
-  description: url of the uploaded object
-  returned: (for PUT operation)
+  description: url of the object
+  returned: (for put and geturl operations)
   type: string
   sample: https://my-bucket.s3.amazonaws.com/my-key.txt?AWSAccessKeyId=<access-key>&Expires=1506888865&Signature=<signature>
+expiry:
+  description: number of seconds the presigned url is valid for
+  returned: (for geturl operation)
+  type: int
+  sample: 600
+contents:
+  description: contents of the object as string
+  returned: (for getstr operation)
+  type: string
+  sample: "Hello, world!"
+s3_keys:
+  description: list of object keys
+  returned: (for list operation)
+  type: list
+  sample:
+  - prefix1/
+  - prefix1/key1
+  - prefix1/key2
 '''
 
 import os
