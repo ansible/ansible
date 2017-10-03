dd911dce379514516b4c87e10fd0b6207c0d0473
Matt Clay <matt@mystile.com>
2017-10-03 14:37:05 -0700

    Switch CI for OS X back to a single job. (#31265)
    * Update Shippable scripts for group/non-group use.
    * Switch CI for OS X back to a single job.
    
    (cherry picked from commit 5a91ed0a3be0ea4910052535f65fd5c64bf029fe)

--- test/utils/shippable/rhel.sh
+++ test/utils/shippable/rhel.sh
@@ -7,8 +7,14 @@ IFS='/:' read -ra args <<< "$1"
 
 platform="${args[0]}"
 version="${args[1]}"
-target="posix/ci/"
+
+if [ "${#args[@]}" -gt 2 ]; then
+    target="posix/ci/group${args[2]}/"
+else
+    target="posix/ci/"
+fi
 
 # shellcheck disable=SC2086
-ansible-test integration --color -v --retry-on-error "${target}" --remote "${platform}/${version}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
-    --exclude "posix/ci/cloud/"
+ansible-test integration --color -v --retry-on-error "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
+    --exclude "posix/ci/cloud/" \
+    --remote "${platform}/${version}" --remote-terminate always
