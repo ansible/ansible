001c506df4cb7e18d097583bd3a15f945b299c65
Matt Clay <matt@mystile.com>
2017-10-03 14:06:19 -0700

    Update Shippable scripts for group/non-group use.

--- test/utils/shippable/linux.sh
+++ test/utils/shippable/linux.sh
@@ -5,9 +5,15 @@ set -o pipefail
 declare -a args
 IFS='/:' read -ra args <<< "$1"
 
-image="ansible/ansible:${args[1]}"
-target="posix/ci/group${args[2]}/"
+image="${args[1]}"
+
+if [ "${#args[@]}" -gt 2 ]; then
+    target="posix/ci/group${args[2]}/"
+else
+    target="posix/ci/"
+fi
 
 # shellcheck disable=SC2086
 ansible-test integration --color -v --retry-on-error "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
-     --docker "${image}"
+    --exclude "posix/ci/cloud/" \
+    --docker "${image}"
