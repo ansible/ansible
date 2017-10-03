70d53cedafe80b5c738c2aa5ed5448b8de80bdf8
Matt Clay <matt@mystile.com>
2017-10-03 10:44:44 -0700

    Run OS X and FreeBSD CI tests using groups.

--- test/utils/shippable/freebsd.sh
+++ test/utils/shippable/freebsd.sh
@@ -7,8 +7,8 @@ IFS='/:' read -ra args <<< "${TEST}"
 
 platform="${args[0]}"
 version="${args[1]}"
-target="posix/ci/"
+target="posix/ci/group${args[2]}/"
 
 # shellcheck disable=SC2086
-ansible-test integration --color -v --retry-on-error "${target}" --remote "${platform}/${version}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
-    --exclude "posix/ci/cloud/"
+ansible-test integration --color -v --retry-on-error "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
+     --remote "${platform}/${version}" --remote-terminate always
--- test/utils/shippable/linux.sh
+++ test/utils/shippable/linux.sh
@@ -9,4 +9,5 @@ image="ansible/ansible:${args[1]}"
 target="posix/ci/group${args[2]}/"
 
 # shellcheck disable=SC2086
-ansible-test integration --color -v --retry-on-error "${target}" --docker "${image}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
+ansible-test integration --color -v --retry-on-error "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
+     --docker "${image}"
