db9cb964b391dbfbed46240a1bbd71a052c5e6c5
rahushen <rahul.shenoy.86@gmail.com>
2017-10-04 03:23:44 -0400

    Fixes nxos_igmp_interface idempotence issue for N1 images (#31220)
    (cherry picked from commit 1378861fe75fcae2d0ad7b880e70999112d20609)

--- lib/ansible/modules/network/nxos/nxos_igmp_interface.py
+++ lib/ansible/modules/network/nxos/nxos_igmp_interface.py
@@ -335,13 +335,13 @@ def get_igmp_interface(module, interface):
     if body:
         resource = body['TABLE_vrf']['ROW_vrf']['TABLE_if']['ROW_if']
         igmp = apply_key_map(key_map, resource)
-        report_llg = str(resource['ReportingForLinkLocal'])
+        report_llg = str(resource['ReportingForLinkLocal']).lower()
         if report_llg == 'true':
             igmp['report_llg'] = True
         elif report_llg == 'false':
             igmp['report_llg'] = False
 
-        immediate_leave = str(resource['ImmediateLeave'])  # returns en or dis
+        immediate_leave = str(resource['ImmediateLeave']).lower()  # returns en or dis
         if immediate_leave == 'en' or immediate_leave == 'true':
             igmp['immediate_leave'] = True
         elif immediate_leave == 'dis' or immediate_leave == 'false':
