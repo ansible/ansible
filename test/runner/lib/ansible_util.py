cea9b8418c3ec4008a84bbdea5c0a23a6c39dc12
Matt Clay <matt@mystile.com>
2017-10-04 11:36:53 -0700

    Fix ansible-test config management.

--- test/runner/lib/ansible_util.py
+++ test/runner/lib/ansible_util.py
@@ -4,7 +4,14 @@ from __future__ import absolute_import, print_function
 
 import os
 
-from lib.util import common_environment
+from lib.util import (
+    common_environment,
+    ApplicationError,
+)
+
+from lib.config import (
+    IntegrationConfig,
+)
 
 
 def ansible_environment(args, color=True):
@@ -21,18 +28,23 @@ def ansible_environment(args, color=True):
     if not path.startswith(ansible_path + os.pathsep):
         path = ansible_path + os.pathsep + path
 
+    if isinstance(args, IntegrationConfig):
+        ansible_config = 'test/integration/%s.cfg' % args.command
+    else:
+        ansible_config = 'test/%s/ansible.cfg' % args.command
+
+    if not os.path.exists(ansible_config):
+        raise ApplicationError('Configuration not found: %s' % ansible_config)
+
     ansible = dict(
         ANSIBLE_FORCE_COLOR='%s' % 'true' if args.color and color else 'false',
         ANSIBLE_DEPRECATION_WARNINGS='false',
         ANSIBLE_HOST_KEY_CHECKING='false',
+        ANSIBLE_CONFIG=os.path.abspath(ansible_config),
         PYTHONPATH=os.path.abspath('lib'),
         PAGER='/bin/cat',
         PATH=path,
     )
-
-    if os.path.isfile('test/integration/%s.cfg' % args.command):
-        ansible_config = os.path.abspath('test/integration/%s.cfg' % args.command)
-        ansible['ANSIBLE_CONFIG'] = ansible_config
 
     env.update(ansible)
 
