891a7c93819939d1cb0387e19ee0a9fabaa7858e
Trishna Guha <trishnaguha17@gmail.com>
2017-10-04 11:54:25 +0530

    Fixes #30529 nxos_facts ipv6 interface
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/modules/network/nxos/nxos_facts.py
+++ lib/ansible/modules/network/nxos/nxos_facts.py
@@ -348,14 +348,20 @@ class Interfaces(FactsBase):
         return objects
 
     def parse_ipv6_interfaces(self, data):
-        data = data['TABLE_intf']
-        if isinstance(data, dict):
-            data = [data]
-        for item in data:
-            name = item['ROW_intf']['intf-name']
-            intf = self.facts['interfaces'][name]
-            intf['ipv6'] = self.transform_dict(item, self.INTERFACE_IPV6_MAP)
-            self.facts['all_ipv6_addresses'].append(item['ROW_intf']['addr'])
+        try:
+            data = data['TABLE_intf']
+            if data:
+                if isinstance(data, dict):
+                    data = [data]
+                for item in data:
+                    name = item['ROW_intf']['intf-name']
+                    intf = self.facts['interfaces'][name]
+                    intf['ipv6'] = self.transform_dict(item, self.INTERFACE_IPV6_MAP)
+                    self.facts['all_ipv6_addresses'].append(item['ROW_intf']['addr'])
+            else:
+                return ""
+        except TypeError:
+            return ""
 
 
 class Legacy(FactsBase):
