41664755e461e2b76f375dd61e3297d41fd43e33
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2017-10-06 17:19:33 +0530

    Moving future and metaclass to top

--- lib/ansible/modules/network/cnos/cnos_facts.py
+++ lib/ansible/modules/network/cnos/cnos_facts.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 #
 # Copyright (C) 2017 Lenovo, Inc.
 #
@@ -62,9 +64,6 @@ msg:
   type: string
   sample: "Device Sys Info is saved to file"
 '''
-
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
 
 import sys
 import paramiko
