42b7f959d3891b93a32c6c0132c826084c2dd0c7
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2017-10-06 17:32:20 +0530

    Moving future and metaclass to top

--- lib/ansible/modules/network/cnos/cnos_vlag.py
+++ lib/ansible/modules/network/cnos/cnos_vlag.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 #
 # Copyright (C) 2017 Lenovo, Inc.
 #
@@ -248,9 +250,6 @@ msg:
   type: string
   sample: "vLAG configurations accomplished"
 '''
-
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
 
 import sys
 import paramiko
