2e004e838c2f15a09c0ed4a41ade27f9a13fd52c
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2017-10-06 17:30:15 +0530

    Moving future and metaclass to top

--- lib/ansible/modules/network/cnos/cnos_showrun.py
+++ lib/ansible/modules/network/cnos/cnos_showrun.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 #
 # Copyright (C) 2017 Lenovo, Inc.
 #
@@ -65,9 +67,6 @@ msg:
   type: string
   sample: "Running Configuration saved in file"
 '''
-
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
 
 import sys
 import paramiko
