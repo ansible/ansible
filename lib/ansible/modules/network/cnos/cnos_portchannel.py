164eba887463fb576317533e8338e416d1357161
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2017-10-06 13:38:58 +0530

    Update cnos_portchannel.py
    Adding
    from __future__ import (absolute_import, division, print_function)
    __metaclass__ = type

--- lib/ansible/modules/network/cnos/cnos_portchannel.py
+++ lib/ansible/modules/network/cnos/cnos_portchannel.py
@@ -414,6 +414,9 @@ msg:
   sample: "Port Channel configurations accomplished"
 '''
 
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 import sys
 import paramiko
 import time
