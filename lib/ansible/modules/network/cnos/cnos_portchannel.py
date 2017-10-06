62b3bd8595eb5ee4bb957c52e13580e604a3f535
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2017-10-06 17:26:46 +0530

    Moving future and metaclass to top

--- lib/ansible/modules/network/cnos/cnos_portchannel.py
+++ lib/ansible/modules/network/cnos/cnos_portchannel.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 #
 # Copyright (C) 2017 Lenovo, Inc.
 #
@@ -413,9 +415,6 @@ msg:
   type: string
   sample: "Port Channel configurations accomplished"
 '''
-
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
 
 import sys
 import paramiko
