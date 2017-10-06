ef8923b3478a76bfb50a99f2afac827a3748c5eb
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2017-10-06 17:25:42 +0530

    Moving future and metaclass to top

--- lib/ansible/modules/network/cnos/cnos_interface.py
+++ lib/ansible/modules/network/cnos/cnos_interface.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 #
 # Copyright (C) 2017 Lenovo, Inc.
 #
@@ -461,9 +463,6 @@ msg:
   type: string
   sample: "Interface configurations accomplished."
 '''
-
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
 
 import sys
 import paramiko
