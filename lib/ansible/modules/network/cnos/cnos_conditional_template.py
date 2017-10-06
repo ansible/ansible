3771af073e52a7bd190f20fa8d1785eef61c8cf6
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2017-10-06 17:16:01 +0530

    Moving future and metaclass to top

--- lib/ansible/modules/network/cnos/cnos_conditional_template.py
+++ lib/ansible/modules/network/cnos/cnos_conditional_template.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 #
 # Copyright (C) 2017 Lenovo, Inc.
 #
@@ -102,9 +104,6 @@ msg:
   type: string
   sample: "Template Applied."
 '''
-
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
 
 import sys
 import paramiko
