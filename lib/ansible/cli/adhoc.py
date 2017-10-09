d4059b3cb35b01af4824844d8778de5b01685551
Brian Coca <bcoca@ansible.com>
2017-10-09 15:52:52 -0400

    better message for unsupported include/imports
    adhoc cannot currently execute these actions

--- lib/ansible/cli/adhoc.py
+++ lib/ansible/cli/adhoc.py
@@ -138,7 +138,7 @@ class AdHocCLI(CLI):
             raise AnsibleOptionsError(err)
 
         # Avoid modules that don't work with ad-hoc
-        if self.options.module_name in ('include', 'include_role'):
+        if self.options.module_name.startswith(('include', 'import_')):
             raise AnsibleOptionsError("'%s' is not a valid action for ad-hoc commands" % self.options.module_name)
 
         play_ds = self._play_ds(pattern, self.options.seconds, self.options.poll_interval)
