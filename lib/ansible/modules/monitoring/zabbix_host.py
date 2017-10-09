9d062c38e19b7408062008b9fcfc7e1f9f293c5b
Stefan Schatz <stefan.schatz@itsv.at>
2017-10-09 08:40:13 +0200

    lib/ansible/modules/monitoring/zabbix_host.py: increased version_added to 2.5 for option description

--- lib/ansible/modules/monitoring/zabbix_host.py
+++ lib/ansible/modules/monitoring/zabbix_host.py
@@ -68,7 +68,7 @@ options:
         description:
             - Description of the host in Zabbix.
         required: false
-        version_added: '2.4'
+        version_added: '2.5'
     host_groups:
         description:
             - List of host groups the host is part of.
