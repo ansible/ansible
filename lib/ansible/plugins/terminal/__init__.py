59d2eccaba5e67e70d2d56b95298da42ec2e1829
ccdtolle <charles_tolley@byu.edu>
2017-10-12 15:44:41 -0600

    Corrected indentation error from previous commit

--- lib/ansible/plugins/terminal/__init__.py
+++ lib/ansible/plugins/terminal/__init__.py
@@ -48,7 +48,7 @@ class TerminalBase(with_metaclass(ABCMeta, object)):
 
     #: compiled bytes regular expressions to remove ANSI codes
     ansi_re = [
-		# see ECMA-48 Section 5.4 (Control Sequences)
+        # see ECMA-48 Section 5.4 (Control Sequences)
         re.compile(br'((?:\x9b|\x1b\x5b)[\x30-\x3f]*[\x20-\x2f]*[\x40-\x7e])'),
         re.compile(br'\x08.')
     ]
