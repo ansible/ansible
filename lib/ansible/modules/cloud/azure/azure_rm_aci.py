91bb5e4d035a04a30734e9bf61b5e503f260a7a7
zikalino <zikalino@microsoft.com>
2017-10-12 14:09:19 +0800

    added my name ;-)

--- lib/ansible/modules/cloud/azure/azure_rm_aci.py
+++ lib/ansible/modules/cloud/azure/azure_rm_aci.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2017 Julien Stroheker, <juliens@microsoft.com>
+# Copyright (c) 2017 Zim Kalinowski, <zikalino@microsoft.com>
 #
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
