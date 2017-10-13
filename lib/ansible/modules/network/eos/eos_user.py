32d6a0d74f2f05889dcd9bba8d67c3143da54779
TAC Tools Dev <dt@arista.com>
2017-10-12 20:46:21 -0700

    fix the order commands are sent allowing for user attributes to be set on creation fixes #31680

--- lib/ansible/modules/network/eos/eos_user.py
+++ lib/ansible/modules/network/eos/eos_user.py
@@ -180,17 +180,17 @@ def map_obj_to_commands(updates, module):
         if want['state'] == 'absent':
             commands.append('no username %s' % want['name'])
             continue
+        
+        if needs_update('configured_password'):
+            if update_password == 'always' or not have:
+                add('secret %s' % want['configured_password'])
 
         if needs_update('role'):
             add('role %s' % want['role'])
 
         if needs_update('privilege'):
             add('privilege %s' % want['privilege'])
-
-        if needs_update('configured_password'):
-            if update_password == 'always' or not have:
-                add('secret %s' % want['configured_password'])
-
+        
         if needs_update('sshkey'):
             add('sshkey %s' % want['sshkey'])
 
