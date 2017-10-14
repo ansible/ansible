8ab719044e83d1de65ef9d861cf265b840d173f7
Tim Rupp <caphrim007@gmail.com>
2017-10-13 21:51:28 -0700

    Fixes various gtm pool issues (#31728)
    Various formatting related fixes. Also fixed an idempotency problem
    with the 'disabled' state

--- test/units/modules/network/f5/test_bigip_gtm_pool.py
+++ test/units/modules/network/f5/test_bigip_gtm_pool.py
@@ -40,6 +40,7 @@ try:
     from library.bigip_gtm_pool import ArgumentSpec
     from library.bigip_gtm_pool import UntypedManager
     from library.bigip_gtm_pool import TypedManager
+    from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
 except ImportError:
     try:
         from ansible.modules.network.f5.bigip_gtm_pool import Parameters
@@ -47,6 +48,7 @@ except ImportError:
         from ansible.modules.network.f5.bigip_gtm_pool import ArgumentSpec
         from ansible.modules.network.f5.bigip_gtm_pool import UntypedManager
         from ansible.modules.network.f5.bigip_gtm_pool import TypedManager
+        from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
     except ImportError:
         raise SkipTest("F5 Ansible modules require the f5-sdk Python library")
 
