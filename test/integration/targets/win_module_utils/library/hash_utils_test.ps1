74f0c7d87302698a38d23d9bb5b39863bfa25e8e
Gerald Spencer <gerald@3scan.com>
2017-10-15 08:49:49 -0700

    Add Assert-EqualsHash test

--- test/integration/targets/win_module_utils/library/hash_utils_test.ps1
+++ test/integration/targets/win_module_utils/library/hash_utils_test.ps1
@@ -8,13 +8,37 @@ Function Assert-Equals($actual, $expected) {
     }
 }
 
+Function Assert-EqualsHash($actual, $expected) {
+    if ($actual.Count -ne $expected.Count) {
+        Write-Output "Number of hash keys do not match`nActual: $actual.Keys.Count`nExpected: $expected.Keys.Count"
+    }
+
+    foreach ($key in $actual.Keys) {
+        if (!$expected[$key]) {
+            Write-Output "Hash keys do not match`nActual: $actual.Keys`nExpected: $expected.Keys"
+        }
+
+        if ($actual[$key] -ne $expected[$key]) {
+            Write-Output "Hash value does not match`nActual: $actual[$key]`nExpected: $expected[$key]"
+        }
+    }
+}
+
 $test_hash1 = @{
-    key1 = "value1",
+    key1 = "value1";
     key2 = "value2"
 }
 
-$test_hash1_string = Convert-HashToString $test_hash1
+$test_array1 = @(
+    1, "key1=value1", "key2=value2"
+)
+
 $expected_string = "key1=value1 key2=value2"
+
+$test_hash1_string = Convert-HashToString $test_hash1
+$test_array1_hash = Convert-ArrayToHash $test_array1
+
 Assert-Equals $test_hash1_string $expected_string
+Assert-EqualsHash $test_array1_hash $test_hash1
 
 Exit-Json @{ data = "success" }
