212d8e27ff1d2396c0006e43e1902662ccb5f8a2
Gerald Spencer <gerald@3scan.com>
2017-10-15 18:44:05 -0700

    Where did the 1 come from??

--- test/integration/targets/win_module_utils/library/hash_utils_test.ps1
+++ test/integration/targets/win_module_utils/library/hash_utils_test.ps1
@@ -10,16 +10,16 @@ Function Assert-Equals($actual, $expected) {
 
 Function Assert-EqualsHash($actual, $expected) {
     if ($actual.Count -ne $expected.Count) {
-        Write-Output "Number of hash keys do not match`nActual: $actual.Keys.Count`nExpected: $expected.Keys.Count"
+        Fail-Json @{} "Number of hash keys do not match`nActual: $actual.Keys.Count`nExpected: $expected.Keys.Count"
     }
 
     foreach ($key in $actual.Keys) {
         if (!$expected[$key]) {
-            Write-Output "Hash keys do not match`nActual: $actual.Keys`nExpected: $expected.Keys"
+            Fail-Json @{} "Hash keys do not match`nActual: $actual.Keys`nExpected: $expected.Keys"
         }
 
         if ($actual[$key] -ne $expected[$key]) {
-            Write-Output "Hash value does not match`nActual: $actual[$key]`nExpected: $expected[$key]"
+            Fail-Json @{} "Hash value does not match`nActual: $actual[$key]`nExpected: $expected[$key]"
         }
     }
 }
@@ -30,7 +30,7 @@ $test_hash1 = @{
 }
 
 $test_array1 = @(
-    1, "key1=value1", "key2=value2"
+    "key1=value1", "key2=value2"
 )
 
 $expected_string = "key1=value1 key2=value2"
