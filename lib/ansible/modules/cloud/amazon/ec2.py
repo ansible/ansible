8e8c1da7587aca62fb87b50f6b866c9e9871cfab
Kim Blomqvist <kim.blomqvist@lekane.com>
2017-10-17 15:06:54 +0300

    fix syntax error
    * failed with: AttributeError: 'dict' object has no attribute 'id'

--- lib/ansible/modules/cloud/amazon/ec2.py
+++ lib/ansible/modules/cloud/amazon/ec2.py
@@ -1626,7 +1626,7 @@ def warn_if_public_ip_assignment_changed(module, instance):
     public_dns_name = getattr(instance, 'public_dns_name', None)
     if (assign_public_ip or public_dns_name) and (not public_dns_name or not assign_public_ip):
         module.warn("Unable to modify public ip assignment to {0} for instance {1}. "
-                    "Whether or not to assign a public IP is determined during instance creation.".format(assign_public_ip, instance.id))
+                    "Whether or not to assign a public IP is determined during instance creation.".format(assign_public_ip, instance['id']))
 
 
 def main():
