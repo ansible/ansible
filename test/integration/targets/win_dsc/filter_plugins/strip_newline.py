133b840dc284c1026bd595d2f06be918683d63be
Jordan Borean <jborean93@gmail.com>
2017-10-18 20:20:39 +1000

    added custom DSC to fully test out casting

---
+++ test/integration/targets/win_dsc/filter_plugins/strip_newline.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'strip_newline': self.strip_newline
+        }
+
+    def strip_newline(self, value):
+        # will convert \r\n and \n to just \n
+        split_lines = value.splitlines()
+
+        return "\n".join(split_lines)
