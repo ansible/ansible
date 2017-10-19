c59bc2c19257fc468c8c84911b6d73d5b02ed6a4
Noah Sparks <nsparks@outlook.com>
2017-10-19 14:36:18 -0400

    removed the backup/restore functionality
    adjusted to use run-command rather than running the command directly
    
    adjusted testing appropriately for the above changes

--- lib/ansible/modules/windows/win_audit_policy_system.ps1
+++ lib/ansible/modules/windows/win_audit_policy_system.ps1
@@ -3,7 +3,8 @@
 # Copyright (c) 2017 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-#Requires -Module Ansible.ModuleUtils.Legacy
+#Requires -Module Ansible.ModuleUtils.Legacy.psm1
+#Requires -Module Ansible.ModuleUtils.CommandUtil.psm1
 
 $ErrorActionPreference = 'Stop'
 
@@ -12,22 +13,38 @@ $check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "b
 
 $results = @{
     changed = $false
-    backup_taken = $false
 }
 
-#populate sets for -validateset
-Try {
-    $categories = (auditpol /list /category 2>&1).trim() | Where-Object {$_ -ne'Category/Subcategory'}
-    $subcategories = ((auditpol /list /subcategory:* 2>&1 | Select-String "^\s+.*$").matches.value).trim()
+######################################
+### populate sets for -validateset ###
+######################################
+$categories_rc = run-command -command 'auditpol /list /category /r'
+$subcategories_rc = run-command -command 'auditpol /list /subcategory:* /r'
+
+If ($categories_rc.item('rc') -eq 0)
+{
+    $categories = ConvertFrom-Csv $categories_rc.item('stdout') | Select-Object -expand Category*
 }
-Catch {
+Else
+{
     Fail-Json -obj $results -message "Failed to retrive audit policy categories. Please make sure the auditpol command is functional on
     the system and that the account ansible is running under is able to retrieve them. $($_.Exception.Message)"
 }
 
-#ansible params
-$restore_path = Get-AnsibleParam -obj $params -name "restore_path" -type "str"
-$backup_path = Get-AnsibleParam -obj $params -name "backup_path" -type "str"
+If ($subcategories_rc.item('rc') -eq 0)
+{
+    $subcategories = ConvertFrom-Csv $subcategories_rc.item('stdout') | Select-Object -expand Category* |
+    Where-Object {$_ -notin $categories}
+}
+Else
+{
+    Fail-Json -obj $results -message "Failed to retrive audit policy subcategories. Please make sure the auditpol command is functional on
+    the system and that the account ansible is running under is able to retrieve them. $($_.Exception.Message)"
+}
+
+######################
+### ansible params ###
+######################
 $category = Get-AnsibleParam -obj $params -name "category" -type "str" -ValidateSet $categories
 $subcategory = Get-AnsibleParam -obj $params -name "subcategory" -type "str" -ValidateSet $subcategories
 $audit_type = Get-AnsibleParam -obj $params -name "audit_type" -type "str" -ValidateSet 'success', 'failure', 'success and failure', 'none'
@@ -35,15 +52,15 @@ $audit_type = Get-AnsibleParam -obj $params -name "audit_type" -type "str" -Vali
 ########################
 ### Start Processing ###
 ########################
-Function Get-AuditPolicy ($GetList) {
-    Try {
-        $auditpolcsv = auditpol $GetList *>&1
-        $Obj = ConvertFrom-CSV $auditpolcsv | Select-Object @{n='subcategory';e={$_.Subcategory.ToLower()}},
-        #@{n='subcategory_guid';e={$_."Subcategory GUID"}},
+Function Get-AuditPolicy ($GetString) {
+    $auditpolcsv = Run-Command -command $GetString
+    If ($auditpolcsv.item('rc') -eq 0)
+    {
+        $Obj = ConvertFrom-CSV $auditpolcsv.item('stdout') | Select-Object @{n='subcategory';e={$_.Subcategory.ToLower()}},
         @{n='audit_type';e={$_."Inclusion Setting".ToLower()}}
     }
-    Catch {
-        Return "$_"
+    Else {
+        return $auditpolcsv.item('stderr')
     }
 
     $HT = @{}
@@ -58,109 +75,39 @@ Function Get-AuditPolicy ($GetList) {
 ### Deal with parameter sets ###
 ################################
 
-$supplied_params = $params.GetEnumerator() | Where-Object {$_.key -notlike "_*"}
-
-#do nothing and continue
-If ($backup_path -and $restore_path)
-{} 
-#if restoreis defined by itself, do nothing and continue
-ElseIf ($supplied_params.Key -contains 'restore_path' -and $supplied_params.Key.Count -eq 1)
-{}
-#warn if restore + other non backup_path params
-ElseIf ($supplied_params.Key -contains 'restore_path' -and $supplied_params.Key.Count -gt 1)
-{
-    Add-Warning -obj $results -message "Restore_path is intended to be used as the only parameter when it is supplied. All other parameters will be ignored"
-}
-#if backup is defined by itself, set flag and continue
-ElseIf ($supplied_params.Key -contains 'backup_path' -and $supplied_params.Key.Count -eq 1)
-{
-    $backup_only = $true
-}
 #make sure category and subcategory are valid
-Else
-{
-    If (-Not $category -and -Not $subcategory) {Fail-Json -obj $results -message "You must provide either a Category or Subcategory parameter"}
-    If ($category -and $subcategory) {Fail-Json -obj $results -message "Must pick either a specific subcategory or category. You cannot define both"}
-}
-
-######################
-### Backup/Restore ###
-######################
-
-#backup and continue (assuming other params are applied)
-If ($backup_path)
-{
-    #If (Test-Path -Path $backup_path)
-    #{
-        Try {
-            If (-not $check_mode) {
-                auditpol /backup /file:$backup_path
-                $results.backup_taken = $true
-            }
-        }
-        Catch {
-            Fail-Json $results "failed to backup audit policy $($_.exception.message)"
-        }
-    #}
-    #Else {Fail-Json $results "invalid backup path, $backup_path"}
-
-    If ($backup_only = $true)
-    {
-        $results.changed = $true
-        Exit-Json -obj $results
-    }
-}
-
-#restore and exit
-If ($restore_path)
-{
-    If (Test-Path -Path $restore_path)
-    {
-        Try {
-            If (-not $check_mode) {
-                auditpol /restore /file:$restore_path
-            }
-        }
-        Catch {
-            Fail-Json $results "failed to restore audit policy $($_.exception.message)"
-        }
-        $results.changed = $true
-
-        Exit-Json -obj $results #exit here if restore works
-    }
-    Else {Fail-Json $results "invalid restore path, $restore_path"}
-}
+If (-Not $category -and -Not $subcategory) {Fail-Json -obj $results -message "You must provide either a Category or Subcategory parameter"}
+If ($category -and $subcategory) {Fail-Json -obj $results -message "Must pick either a specific subcategory or category. You cannot define both"}
 
 #############################################################
 ### build lists for setting, getting, and comparing rules ###
 #############################################################
-$SetList = @()
-$SetList += '/set'
-$GetList = @()
-$GetList += '/get','/r'
+$SetString = 'auditpol /set'
+$GetString = 'auditpol /get /r'
 
-If ($category) {$SetList += "/category:$category"; $GetList += "/category:$category"}
-If ($subcategory) {$SetList += "/subcategory:$subcategory"; $GetList += "/subcategory:$subcategory"}
+If ($category) {$SetString = "$SetString /category:`"$category`""; $GetString = "$GetString /category:`"$category`""}
+If ($subcategory) {$SetString= "$SetString /subcategory:`"$subcategory`""; $GetString = "$GetString /subcategory:`"$subcategory`""}
 
 Switch ($audit_type)
 {
-    'success and failure' {$SetList += "/success:enable","/failure:enable"; $audit_type_check = $audit_type}
-    'failure' {$SetList += "/success:disable","/failure:enable"; $audit_type_check = $audit_type}
-    'success' {$SetList += "/success:enable","/failure:disable"; $audit_type_check = $audit_type}
-    'none' {$SetList += "/success:disable","/failure:disable"; $audit_type_check = 'No Auditing'}
+    'success and failure' {$SetString = "$SetString /success:enable /failure:enable"; $audit_type_check = $audit_type}
+    'failure' {$SetString = "$SetString /success:disable /failure:enable"; $audit_type_check = $audit_type}
+    'success' {$SetString = "$SetString /success:enable /failure:disable"; $audit_type_check = $audit_type}
+    'none' {$SetString = "$SetString /success:disable /failure:disable"; $audit_type_check = 'No Auditing'}
 }
-
 
 #########################
 ### check Idempotence ###
 #########################
 
-$CurrentRule = Get-AuditPolicy $GetList
+$CurrentRule = Get-AuditPolicy $GetString
 
-#do nothing and continue since change is needed
-If ( ($CurrentRule.Values | Where-Object {$_ -ne $audit_type_check}) ) {}
-#otherwise all items match so we can exit here
-Else {Exit-Json -obj $results}
+#exit if the audit_type is already set properly for the categroy
+If (-not ($CurrentRule.Values | Where-Object {$_ -ne $audit_type_check}) )
+{
+    $results.current_audit_policy = Get-AuditPolicy $GetString
+    Exit-Json -obj $results
+}
 
 ####################
 ### Apply Change ###
@@ -168,17 +115,15 @@ Else {Exit-Json -obj $results}
 
 If (-not $check_mode)
 {
-    Try {
-        auditpol $SetList 2>&1
-    }
-    Catch {
-        $results.current_audit_policy = Get-AuditPolicy $GetList
-        #$results.auditpol_command = "auditpol $($SetList -join ' ')"
+    $ApplyPolicy = Run-Command -command $SetString
+
+    If ($ApplyPolicy.Item('rc') -ne 0)
+    {
+        $results.current_audit_policy = Get-AuditPolicy $GetString
         Fail-Json $results "Failed to set audit policy - $($_.Exception.Message)"
     }
 }
 
 $results.changed = $true
-#$results.auditpol_command = "auditpol $($SetList -join ' ')"
-$results.current_audit_policy = Get-AuditPolicy $GetList
+$results.current_audit_policy = Get-AuditPolicy $GetString
 Exit-Json $results
--- lib/ansible/modules/windows/win_audit_policy_system.py
+++ lib/ansible/modules/windows/win_audit_policy_system.py
@@ -16,25 +16,12 @@ module: win_audit_policy_system
 short_description: Used to make changes to the system wide Audit Policy.
 description:
   - Used to make changes to the system wide Audit Policy.
-  - See this page for in depth information U(https://technet.microsoft.com/en-us/library/cc766468(v=ws.10).aspx).
+  - It is highly recommended to take a backup of the policies before adjusting them for the first time.
+  - See this page for in depth information U(https://technet.microsoft.com/en-us/library/cc766468.aspx).
 version_added: "2.5"
 author:
   - Noah Sparks (@nwsparks)
 options:
-  restore_path:
-    description:
-      - Restores system audit policy settings, per-user audit policy settings for all users, and all auditing options from a file
-        that is syntactically consistent with the comma-separated value (CSV) file format used by the backup option or generated from
-        an auditpol /backup command.
-      - If used, this parameter should be specified by itself or with I(backup_path). If other parameters are specified they will be ignored.
-      - If used with I(backup_path) a backup will be taken before the restore.
-      - This task will always return a changed status.
-  backup_path:
-    description:
-      - Backs up system audit policy settings, per-user audit policy settings for all users, and all auditing options to
-        a comma-separated value (CSV) text file.
-      - This should define a valid filesystem path.
-      - Can be defined by itself or with any other set of parameters.
   category:
     description:
       - Single string value for the category you would like to adjust the policy on.
@@ -51,11 +38,13 @@ options:
 '''
 
 EXAMPLES = r'''
-- name: enable failure auditing for the subcategory "File System" and take backup
+- name: backup audit policy
+  win_command: auditpol /backup /file:c:\auditpolicy.csv
+
+- name: enable failure auditing for the subcategory "File System"
   win_audit_policy_system:
     subcategory: File System
     audit_type: failure
-    backup_path: c:\auditpolicy-{{ '%Y-%m-%d' | strftime }}.csv
 
 - name: enable all auditing types for the category "Account logon events"
   win_audit_policy_system:
@@ -66,10 +55,6 @@ EXAMPLES = r'''
   win_audit_policy_system:
     subcategory: File System
     audit_type: none
-
-- name: restore audit policy from backup
-  win_audit_policy_system:
-    restore_path: c:\auditpolicy_backup.csv
 '''
 
 RETURN = '''
