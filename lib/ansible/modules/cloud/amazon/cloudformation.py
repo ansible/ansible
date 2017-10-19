c6aef055ae4b853a2aee03b9e3d73216143de72b
tedder <ted.timmons@stansonhealth.com>
2017-10-19 09:21:50 -0700

    Exclude stack policy when running in check mode.
    Fixes #31931

--- lib/ansible/modules/cloud/amazon/cloudformation.py
+++ lib/ansible/modules/cloud/amazon/cloudformation.py
@@ -502,7 +502,8 @@ def main():
     else:
         stack_params['NotificationARNs'] = []
 
-    if module.params['stack_policy'] is not None:
+    # can't check the policy when verifying.
+    if module.params['stack_policy'] is not None and not module.check_mode:
         stack_params['StackPolicyBody'] = open(module.params['stack_policy'], 'r').read()
 
     if module.params['changeset_name'] is not None:
