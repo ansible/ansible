7eca1cfca7992fc5a9b0a2f89fcbeb6cfbe56267
Christian Giese <cgiese@juniper.net>
2017-10-19 23:26:41 +0200

    add return documentation

--- lib/ansible/modules/network/junos/junos_scp.py
+++ lib/ansible/modules/network/junos/junos_scp.py
@@ -77,6 +77,10 @@ EXAMPLES = """
     src: test.tgz
     download: true
 """
+
+RETURN = """
+"""
+
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.junos import junos_argument_spec, get_param
 from ansible.module_utils.pycompat24 import get_exception
