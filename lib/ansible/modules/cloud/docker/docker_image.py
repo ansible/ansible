8688c8a6b65622ce080168123a694fe9e78c586d
Alberto Murillo <albertomurillosilva@gmail.com>
2017-10-19 17:05:09 -0500

    docker_image: Fix idempotency of pull
    when pulling an image with force=yes the task was marked as changed
    everytime even when the image hasn't changed.
    
    This was due to a bad comparison of the image tag before the pull
    and after the pull.
    
    Fixes #22596
    
    Signed-off-by: Alberto Murillo <albertomurillosilva@gmail.com>

--- lib/ansible/modules/cloud/docker/docker_image.py
+++ lib/ansible/modules/cloud/docker/docker_image.py
@@ -338,8 +338,8 @@ class ImageManager(DockerBaseClass):
                 self.results['actions'].append('Pulled image %s:%s' % (self.name, self.tag))
                 self.results['changed'] = True
                 if not self.check_mode:
-                    self.results['image'] = self.client.pull_image(self.name, tag=self.tag)
-                    if image and image == self.results['image']:
+                    self.results['image'], already_latest = self.client.pull_image(self.name, tag=self.tag)
+                    if already_latest:
                         self.results['changed'] = False
 
         if self.archive_path:
