264d5d91f2aafe9cd3e467b3fecc6af03b23dba9
Flavien Chantelot <flavien@flavien.io>
2017-10-20 10:04:39 +0200

    Updating metadata

--- lib/ansible/modules/database/postgresql/postgresql_tablespace.py
+++ lib/ansible/modules/database/postgresql/postgresql_tablespace.py
@@ -15,8 +15,11 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
 
-ANSIBLE_METADATA = {'metadata_version': '1.0',
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
