ae59a403fcbe0e5350f2f58a0d24303744b47fee
dariko <git@dariozanzico.com>
2017-10-20 10:21:45 +0200

    add boilerplate as per 1ee08c0f069eb5d3817ba7199adef4f3804056b8

--- lib/ansible/modules/cloud/docker/docker_swarm_service.py
+++ lib/ansible/modules/cloud/docker/docker_swarm_service.py
@@ -3,6 +3,9 @@
 # (c) 2017, Dario Zanzico (git@dariozanzico.com)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
 ANSIBLE_METADATA = {'status': ['preview'],
                     'supported_by': 'community',
                     'metadata_version': '1.1'}
