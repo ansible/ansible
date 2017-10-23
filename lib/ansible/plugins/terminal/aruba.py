aee180f07a72630fa3f8ddc6e0d977fa32adbb9d
James Mighion <james.mighion@gmail.com>
2017-10-23 01:15:30 -0700

    Using shorthand for disabling paging to avoid the syntax difference between versions. (#31942)
    Fixes #31855

--- lib/ansible/plugins/terminal/aruba.py
+++ lib/ansible/plugins/terminal/aruba.py
@@ -48,6 +48,6 @@ class TerminalModule(TerminalBase):
 
     def on_open_shell(self):
         try:
-            self._exec_cli_command(b'no paging')
+            self._exec_cli_command(b'no pag')
         except AnsibleConnectionFailure:
             raise AnsibleConnectionFailure('unable to set terminal parameters')
