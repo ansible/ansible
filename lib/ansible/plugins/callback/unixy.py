fd1a26076f1917190abe9c58c263e7fced323fa0
Allyson Bowles <abowles@hireology.com>
2017-10-23 02:42:24 -0500

    Correctly display debug output and failed output in loops

--- lib/ansible/plugins/callback/unixy.py
+++ lib/ansible/plugins/callback/unixy.py
@@ -28,8 +28,16 @@ from os.path import basename
 class CallbackModule(CallbackBase):
 
     '''
-    Print consolidated output that looks like a *NIX startup log
-    Always dump full, pretty-printed result on failures
+    Design goals:
+    - Print consolidated output that looks like a *NIX startup log
+    - Defaults should avoid displaying unnecessary information wherever possible
+
+    TODOs:
+    - Only display task names if the task runs on at least one host
+    - Add option to display all hostnames on a single line in the appropriate result color (failures may have a separate line)
+    - Consolidate stats display
+    - Display whether run is in --check mode
+    - Don't show play name if no hosts found
     '''
 
     CALLBACK_VERSION = 2.0
@@ -38,89 +46,97 @@ class CallbackModule(CallbackBase):
 
     #def _get_task_banner(self, task):
 
+    def _run_is_verbose(self, result):
+        return ((self._display.verbosity > 0
+                or '_ansible_verbose_always' in result._result)
+                and '_ansible_verbose_override' not in result._result)
 
     def _get_task_display_name(self, task):
         self.task_display_name = None
         display_name = task.get_name().strip().split(" : ")
 
-        if len(display_name) > 1:
-            display_name_index = 1
-        else:
-            display_name_index = 0
-        task_display_name = display_name[display_name_index]
-        if task_display_name == "include" or task_display_name == "include_vars":
+        task_display_name = display_name[-1]
+        if task_display_name.startswith("include"):
             return
         else:
             self.task_display_name = task_display_name
 
+    def _preprocess_result(self, result):
+        self.delegated_vars = result._result.get('_ansible_delegated_vars', None)
+        self._handle_exception(result._result)
+        self._handle_warnings(result._result)
+
+    def _process_result_output(self, result, msg):
+        task_host = result._host.get_name()
+        task_result = "%s %s" % (task_host, msg)
+
+        if self._run_is_verbose(result):
+            task_result = "%s %s: %s" % (task_host, msg, self._dump_results(result._result, indent=4))
+
+        if self.delegated_vars:
+            task_delegate_host = self.delegated_vars['ansible_host']
+            task_result = "%s -> %s %s" % (task_host, task_delegate_host, msg)
+
+        if result._result.get('msg') and result._result.get('msg') != "All items completed":
+            task_result += " | msg: " + result._result.get('msg')
+
+        if result._result.get('stdout'):
+            task_result += " | stdout: " + result._result.get('stdout')
+
+        if result._result.get('stderr'):
+            task_result += " | stderr: " + result._result.get('stderr')
+
+        return task_result
+
     def v2_playbook_on_task_start(self, task, is_conditional):
-        # TODO only display task if it will not be skipped for any hosts
-        # Should be able to achieve this by only running display on ok, changed, and failed
         self._get_task_display_name(task)
         if self.task_display_name is not None:
             self._display.display("%s..." % self.task_display_name)
 
     def v2_playbook_on_handler_task_start(self, task):
         self._get_task_display_name(task)
-        self._display.display("%s (via handler)... " % self.task_display_name)
+        if self.task_display_name is not None:
+            self._display.display("%s (via handler)... " % self.task_display_name)
 
     def v2_playbook_on_play_start(self, play):
         # TODO display name of play and list of play hosts
         # TODO don't display play name if no hosts in play
         name = play.get_name().strip()
-        if not name:
-            msg = u"---"
-        else:
+        if name:
             msg = u"\n- %s -" % name
+        else:
+            msg = u"---"
 
         self._display.display(msg)
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
-        self._handle_exception(result._result)
-        self._handle_warnings(result._result)
+        self._preprocess_result(result)
+        display_color = C.COLOR_ERROR
+        msg = "failed"
 
-        self._display.display("%s failed: %s" % (result._host.get_name(), self._dump_results(result._result, indent=4)), color=C.COLOR_ERROR)
+        task_result = self._process_result_output(result, msg)
+        self._display.display("  " + task_result, display_color)
 
-    def v2_runner_on_ok(self, result):
-        self._clean_results(result._result, result._task.action)
-        self._handle_warnings(result._result)
-        # TODO: handle delegated tasks
+    def v2_runner_on_ok(self, result, msg="ok", display_color=C.COLOR_OK):
+        self._preprocess_result(result)
 
-        result_was_changed = ('changed' in result._result
-                                and result._result['changed'])
-
-        run_is_verbose = ((self._display.verbosity > 0
-                            or '_ansible_verbose_always' in result._result)
-                            and '_ansible_verbose_override' not in result._result)
-
+        result_was_changed = ('changed' in result._result and result._result['changed'])
         if result_was_changed:
             msg = "done"
             display_color = C.COLOR_CHANGED
-        else:
-            msg = "ok"
-            display_color = C.COLOR_OK
 
-        if run_is_verbose:
-            self._display.display("  %s %s: %s" % (result._host.get_name(), msg, self._dump_results(result._result, indent=4)), color=display_color)
-        else:
-            self._display.display("  %s %s" % (result._host.get_name(), msg), color=display_color)
+        task_result = self._process_result_output(result, msg)
+        self._display.display("  " + task_result, display_color)
 
-    def v2_runner_on_skipped(self, result):
-        run_is_verbose = ((self._display.verbosity > 0
-                            or '_ansible_verbose_always' in result._result)
-                            and '_ansible_verbose_override' not in result._result)
-
-        if run_is_verbose:
-            if self._display.verbosity > 1:
-                skip_detail = self._dump_results(result._result, indent=4)
-
-            elif self._display.verbosity == 1:
-                skip_detail = result._result.get('skipped_reason')
-
-            self._display.display("  %s skipped: %s" % (result._host.get_name(), skip_detail), color=C.COLOR_SKIP)
+    def v2_runner_item_on_failed(self, result):
+        self.v2_runner_on_failed(result)
 
     def v2_runner_on_unreachable(self, result):
-        self._display.display("  %s unreachable: %s" % (result._host.get_name(), self._dump_results(result._result, indent=4)), color=C.COLOR_UNREACHABLE)
+        msg = "unreachable"
+        display_color = C.COLOR_UNREACHABLE
+        task_result = self._process_result_output(result, msg)
+
+        self._display.display("  " + task_result, display_color)
 
     def v2_on_file_diff(self, result):
         if result._task.loop and 'results' in result._result:
@@ -160,28 +176,11 @@ class CallbackModule(CallbackBase):
                 log_only=True
             )
 
-        # print custom stats
-        if C.SHOW_CUSTOM_STATS and stats.custom:
-            self._display.banner("Custom Stats: ")
-            # per host
-            # TODO: come up with 'pretty format'
-            for k in sorted(stats.custom.keys()):
-                if k == '_run':
-                    continue
-                self._display.display('\t%s: %s' % (k, self._dump_results(stats.custom[k], indent=1).replace('\n', '')))
-
-            # print per run custom stats
-            if '_run' in stats.custom:
-                self._display.display("", screen_only=True)
-                self._display.display('\tRun: %s' % self._dump_results(stats.custom['_run'], indent=1).replace('\n', ''))
-            self._display.display("", screen_only=True)
-
     def v2_playbook_on_no_hosts_matched(self):
         self._display.display("  No hosts found!", color=C.COLOR_DEBUG)
-        return
 
     def v2_playbook_on_no_hosts_remaining(self):
-        self._display.display("Ran out of hosts!", color=C.COLOR_ERROR)
+        self._display.display("  Ran out of hosts!", color=C.COLOR_ERROR)
 
     def v2_playbook_on_start(self, playbook):
         # TODO display whether this run is happening in check mode
