9c9674e750c59873dc6cecbebdabfa9314752344
africanzoe <chafik.bel@gmail.com>
2017-10-24 12:51:32 +0200

    Fix: typos to be PEP8 compliant

--- lib/ansible/modules/clustering/pacemaker_docker.py
+++ lib/ansible/modules/clustering/pacemaker_docker.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#coding: utf-8 -*-
+# coding: utf-8 -*-
 
 # Copyright (c), Chafik Belhaoues  <chafik.bel@gmail.com>, 2017
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
