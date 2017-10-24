8dea5fc223d8bc4f2ac9555b3cc89ceaf3c644c1
Brian Coca <brian.coca+git@gmail.com>
2017-10-23 22:08:38 -0400

    dicts fix

--- lib/ansible/inventory/data.py
+++ lib/ansible/inventory/data.py
@@ -64,10 +64,10 @@ class InventoryData(object):
     def serialize(self):
         self._groups_dict_cache = None
         data = {
-            groups = self.groups,
-            hosts = self.hosts,
-            local = self.locahost,
-            source = self.current_source,
+            'groups': self.groups,
+            'hosts': self.hosts,
+            'local': self.locahost,
+            'source': self.current_source,
         }
         return data
 
