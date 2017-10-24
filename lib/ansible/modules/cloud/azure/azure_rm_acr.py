9dac688863d1cf2aa0fb8253562adfdbad69b3b7
yawei wang <yaweiwang@yaweis-MBP.guest.corp.microsoft.com>
2017-10-24 12:12:40 +0800

    use get_mgmt_svc_client to enable bi

--- lib/ansible/modules/cloud/azure/azure_rm_acr.py
+++ lib/ansible/modules/cloud/azure/azure_rm_acr.py
@@ -320,10 +320,12 @@ class AzureRMContainerRegistry(AzureRMModuleBase):
     def containerregistry_mgmt_client(self):
         self.log('Getting container registry mgmt client')
         if not self._containerregistry_mgmt_client:
-            self._containerregistry_mgmt_client = ContainerRegistryManagementClient(
-                self.azure_credentials,
-                self.subscription_id
+            self._containerregistry_mgmt_client = self.get_mgmt_svc_client(
+                ContainerRegistryManagementClient,
+                base_url = self._cloud_environment.endpoints.resource_manager,
+                api_version='2017-10-01'
             )
+
         return self._containerregistry_mgmt_client
 
 def main():
