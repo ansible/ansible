221c0731c90e24234c572531f843aa432332c0b1
Trishna Guha <trishnaguha17@gmail.com>
2017-10-25 12:39:28 +0530

    nxos_inteface TypeError fix
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/modules/network/nxos/nxos_interface.py
+++ lib/ansible/modules/network/nxos/nxos_interface.py
@@ -277,70 +277,74 @@ def get_interface(intf, module):
     except IndexError:
         body = []
     if body:
-        interface_table = body['TABLE_interface']['ROW_interface']
-        if interface_table.get('eth_mode') == 'fex-fabric':
-            module.fail_json(msg='nxos_interface does not support interfaces with mode "fex-fabric"')
-        intf_type = get_interface_type(intf)
-        if intf_type in ['portchannel', 'ethernet']:
-            if not interface_table.get('eth_mode'):
-                interface_table['eth_mode'] = 'layer3'
+        try:
+            interface_table = body['TABLE_interface']['ROW_interface']
+        except TypeError:
+            interface_table = {}
+        if interface_table:
+            if interface_table.get('eth_mode') == 'fex-fabric':
+                module.fail_json(msg='nxos_interface does not support interfaces with mode "fex-fabric"')
+            intf_type = get_interface_type(intf)
+            if intf_type in ['portchannel', 'ethernet']:
+                if not interface_table.get('eth_mode'):
+                    interface_table['eth_mode'] = 'layer3'
 
-        if intf_type == 'ethernet':
-            key_map.update(base_key_map)
-            key_map.update(mode_map)
-            temp_dict = apply_key_map(key_map, interface_table)
-            temp_dict = apply_value_map(mode_value_map, temp_dict)
-            interface.update(temp_dict)
+            if intf_type == 'ethernet':
+                key_map.update(base_key_map)
+                key_map.update(mode_map)
+                temp_dict = apply_key_map(key_map, interface_table)
+                temp_dict = apply_value_map(mode_value_map, temp_dict)
+                interface.update(temp_dict)
 
-        elif intf_type == 'svi':
-            key_map.update(svi_map)
-            temp_dict = apply_key_map(key_map, interface_table)
-            interface.update(temp_dict)
-            attributes = get_manual_interface_attributes(intf, module)
-            interface['admin_state'] = str(attributes.get('admin_state',
-                                                          'nxapibug'))
-            interface['description'] = str(attributes.get('description',
-                                                          'nxapi_bug'))
-            command = 'show run interface {0}'.format(intf)
-            body = execute_show_command(command, module)[0]
-            if 'ip forward' in body:
-                interface['ip_forward'] = 'enable'
-            else:
-                interface['ip_forward'] = 'disable'
-            if 'fabric forwarding mode anycast-gateway' in body:
-                interface['fabric_forwarding_anycast_gateway'] = True
-            else:
-                interface['fabric_forwarding_anycast_gateway'] = False
+            elif intf_type == 'svi':
+                key_map.update(svi_map)
+                temp_dict = apply_key_map(key_map, interface_table)
+                interface.update(temp_dict)
+                attributes = get_manual_interface_attributes(intf, module)
+                interface['admin_state'] = str(attributes.get('admin_state',
+                                                              'nxapibug'))
+                interface['description'] = str(attributes.get('description',
+                                                              'nxapi_bug'))
+                command = 'show run interface {0}'.format(intf)
+                body = execute_show_command(command, module)[0]
+                if 'ip forward' in body:
+                    interface['ip_forward'] = 'enable'
+                else:
+                    interface['ip_forward'] = 'disable'
+                if 'fabric forwarding mode anycast-gateway' in body:
+                    interface['fabric_forwarding_anycast_gateway'] = True
+                else:
+                    interface['fabric_forwarding_anycast_gateway'] = False
 
-        elif intf_type == 'loopback':
-            key_map.update(base_key_map)
-            key_map.pop('admin_state')
-            key_map.update(loop_map)
-            temp_dict = apply_key_map(key_map, interface_table)
-            if not temp_dict.get('description'):
-                temp_dict['description'] = "None"
-            interface.update(temp_dict)
+            elif intf_type == 'loopback':
+                key_map.update(base_key_map)
+                key_map.pop('admin_state')
+                key_map.update(loop_map)
+                temp_dict = apply_key_map(key_map, interface_table)
+                if not temp_dict.get('description'):
+                    temp_dict['description'] = "None"
+                interface.update(temp_dict)
 
-        elif intf_type == 'management':
-            key_map.update(base_key_map)
-            temp_dict = apply_key_map(key_map, interface_table)
-            interface.update(temp_dict)
+            elif intf_type == 'management':
+                key_map.update(base_key_map)
+                temp_dict = apply_key_map(key_map, interface_table)
+                interface.update(temp_dict)
 
-        elif intf_type == 'portchannel':
-            key_map.update(base_key_map)
-            key_map.update(mode_map)
-            temp_dict = apply_key_map(key_map, interface_table)
-            temp_dict = apply_value_map(mode_value_map, temp_dict)
-            if not temp_dict.get('description'):
-                temp_dict['description'] = "None"
-            interface.update(temp_dict)
+            elif intf_type == 'portchannel':
+                key_map.update(base_key_map)
+                key_map.update(mode_map)
+                temp_dict = apply_key_map(key_map, interface_table)
+                temp_dict = apply_value_map(mode_value_map, temp_dict)
+                if not temp_dict.get('description'):
+                    temp_dict['description'] = "None"
+                interface.update(temp_dict)
 
-        elif intf_type == 'nve':
-            key_map.update(base_key_map)
-            temp_dict = apply_key_map(key_map, interface_table)
-            if not temp_dict.get('description'):
-                temp_dict['description'] = "None"
-            interface.update(temp_dict)
+            elif intf_type == 'nve':
+                key_map.update(base_key_map)
+                temp_dict = apply_key_map(key_map, interface_table)
+                if not temp_dict.get('description'):
+                    temp_dict['description'] = "None"
+                interface.update(temp_dict)
 
     return interface
 
