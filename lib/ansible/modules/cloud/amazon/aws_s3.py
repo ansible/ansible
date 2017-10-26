8404892143508c3e5a98078ca65376e4dd055910
Ryan Brown <sb@ryansb.com>
2017-10-26 08:17:13 -0400

    [cloud] Bugfix for aws_s3 empty directory creation
    Backport of #32169

--- lib/ansible/modules/cloud/amazon/aws_s3.py
+++ lib/ansible/modules/cloud/amazon/aws_s3.py
@@ -375,12 +375,10 @@ def create_dirkey(module, s3, bucket, obj):
     if module.check_mode:
         module.exit_json(msg="PUT operation skipped - running in check mode", changed=True)
     try:
-        bucket = s3.Bucket(bucket)
-        key = bucket.new_key(obj)
-        key.set_contents_from_string('')
+        s3.put_object(Bucket=bucket, Key=obj, Body=b'')
         for acl in module.params.get('permission'):
             s3.put_object_acl(ACL=acl, Bucket=bucket, Key=obj)
-        module.exit_json(msg="Virtual directory %s created in bucket %s" % (obj, bucket.name), changed=True)
+        module.exit_json(msg="Virtual directory %s created in bucket %s" % (obj, bucket), changed=True)
     except botocore.exceptions.ClientError as e:
         module.fail_json(msg="Failed while creating object %s." % obj, exception=traceback.format_exc(), **camel_dict_to_snake_dict(e.response))
 
