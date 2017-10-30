8559025bd137f9b2de83b1039d358d6a8ba608f6
Adrian Likins <alikins@redhat.com>
2017-10-30 15:26:59 -0400

    Run var_prec tests with/without fact caching
    and test set_fact with cacheable:true persists facts
    across plays in a playbook and the var_prec is correct.

--- test/integration/targets/var_precedence/runme.sh
+++ test/integration/targets/var_precedence/runme.sh
@@ -2,6 +2,23 @@
 
 set -eux
 
+MYTMPDIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
+trap 'rm -rf "${MYTMPDIR}"' EXIT
+
+# no fact caching
 ansible-playbook test_var_precedence.yml -i ../../inventory -v "$@" \
     -e 'extra_var=extra_var' \
-    -e 'extra_var_override=extra_var_override'
+    -e 'extra_var_override=extra_var_override' \
+    -e '{ write_cache: False }'
+
+# use fact cache, and invoke set_fact with 'cacheable=true' so we can test it across plays in a playbook
+ANSIBLE_CACHE_PLUGIN=jsonfile ANSIBLE_CACHE_PLUGIN_CONNECTION="${MYTMPDIR}" ansible-playbook test_var_precedence.yml -i ../../inventory -v "$@" \
+    -e 'extra_var=extra_var' \
+    -e 'extra_var_override=extra_var_override' \
+    -e '{ write_cache: True }'
+
+# use fact cache, but do not use set_fact with 'cacheable=true' so no set_fact calls should persist across plays in a playbook
+ANSIBLE_CACHE_PLUGIN=jsonfile ANSIBLE_CACHE_PLUGIN_CONNECTION="${MYTMPDIR}" ansible-playbook test_var_precedence.yml -i ../../inventory -v "$@" \
+    -e 'extra_var=extra_var' \
+    -e 'extra_var_override=extra_var_override' \
+    -e '{ write_cache: False }'
