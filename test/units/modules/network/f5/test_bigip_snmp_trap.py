e2f2fd23eca739a81510adaca3eb03c752f6202d
Tim Rupp <caphrim007@gmail.com>
2017-11-01 15:20:28 -0700

    Removes bigip_snmp_trap from skip file (#32470)

--- test/units/modules/network/f5/test_bigip_snmp_trap.py
+++ test/units/modules/network/f5/test_bigip_snmp_trap.py
@@ -1,21 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2017 F5 Networks Inc.
-#
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (c) 2017 F5 Networks Inc.
+# GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
@@ -41,6 +27,7 @@ try:
     from library.bigip_snmp_trap import NetworkedManager
     from library.bigip_snmp_trap import NonNetworkedManager
     from library.bigip_snmp_trap import ArgumentSpec
+    from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
 except ImportError:
     try:
         from ansible.modules.network.f5.bigip_snmp_trap import NetworkedParameters
@@ -49,6 +36,7 @@ except ImportError:
         from ansible.modules.network.f5.bigip_snmp_trap import NetworkedManager
         from ansible.modules.network.f5.bigip_snmp_trap import NonNetworkedManager
         from ansible.modules.network.f5.bigip_snmp_trap import ArgumentSpec
+        from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
     except ImportError:
         raise SkipTest("F5 Ansible modules require the f5-sdk Python library")
 
