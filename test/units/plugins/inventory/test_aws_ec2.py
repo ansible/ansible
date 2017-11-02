84f37f804e0da68394da9aeb7f16b63aea05567b
s-hertel <shertel@redhat.com>
2017-11-02 11:27:21 -0400

    Fix pep8 and add a few more tests

--- test/units/plugins/inventory/test_aws_ec2.py
+++ test/units/plugins/inventory/test_aws_ec2.py
@@ -21,90 +21,90 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import unittest
 import pytest
 import datetime
 
 from ansible.errors import AnsibleError
 from ansible.plugins.inventory.aws_ec2 import InventoryModule
-from ansible.plugins.inventory.aws_ec2 import instance_data_filter_to_boto_attr, instance_meta_filter_to_boto_attr
+from ansible.plugins.inventory.aws_ec2 import instance_data_filter_to_boto_attr
 
 instances = {
-u'Instances': [
-    {u'Monitoring': {u'State': 'disabled'},
-     u'PublicDnsName': 'ec2-12-345-67-890.compute-1.amazonaws.com',
-     u'State': {u'Code': 16, u'Name': 'running'},
-     u'EbsOptimized': False,
-     u'LaunchTime': datetime.datetime(2017, 10, 31, 12, 59, 25),
-     u'PublicIpAddress': '12.345.67.890',
-     u'PrivateIpAddress': '098.76.54.321',
-     u'ProductCodes': [],
-     u'VpcId': 'vpc-12345678',
-     u'StateTransitionReason': '',
-     u'InstanceId': 'i-00000000000000000',
-     u'EnaSupport': True,
-     u'ImageId': 'ami-12345678',
-     u'PrivateDnsName': 'ip-098-76-54-321.ec2.internal',
-     u'KeyName': 'testkey',
-     u'SecurityGroups': [{u'GroupName': 'default', u'GroupId': 'sg-12345678'}],
-     u'ClientToken': '',
-     u'SubnetId': 'subnet-12345678',
-     u'InstanceType': 't2.micro',
-     u'NetworkInterfaces': [
-        {u'Status': 'in-use',
-         u'MacAddress': '12:a0:50:42:3d:a4',
-         u'SourceDestCheck': True,
+    u'Instances': [
+        {u'Monitoring': {u'State': 'disabled'},
+         u'PublicDnsName': 'ec2-12-345-67-890.compute-1.amazonaws.com',
+         u'State': {u'Code': 16, u'Name': 'running'},
+         u'EbsOptimized': False,
+         u'LaunchTime': datetime.datetime(2017, 10, 31, 12, 59, 25),
+         u'PublicIpAddress': '12.345.67.890',
+         u'PrivateIpAddress': '098.76.54.321',
+         u'ProductCodes': [],
          u'VpcId': 'vpc-12345678',
-         u'Description': '',
-         u'NetworkInterfaceId': 'eni-12345678',
-         u'PrivateIpAddresses': [
-            {u'PrivateDnsName': 'ip-098-76-54-321.ec2.internal',
+         u'StateTransitionReason': '',
+         u'InstanceId': 'i-00000000000000000',
+         u'EnaSupport': True,
+         u'ImageId': 'ami-12345678',
+         u'PrivateDnsName': 'ip-098-76-54-321.ec2.internal',
+         u'KeyName': 'testkey',
+         u'SecurityGroups': [{u'GroupName': 'default', u'GroupId': 'sg-12345678'}],
+         u'ClientToken': '',
+         u'SubnetId': 'subnet-12345678',
+         u'InstanceType': 't2.micro',
+         u'NetworkInterfaces': [
+            {u'Status': 'in-use',
+             u'MacAddress': '12:a0:50:42:3d:a4',
+             u'SourceDestCheck': True,
+             u'VpcId': 'vpc-12345678',
+             u'Description': '',
+             u'NetworkInterfaceId': 'eni-12345678',
+             u'PrivateIpAddresses': [
+                 {u'PrivateDnsName': 'ip-098-76-54-321.ec2.internal',
+                  u'PrivateIpAddress': '098.76.54.321',
+                  u'Primary': True,
+                  u'Association':
+                      {u'PublicIp': '12.345.67.890',
+                       u'PublicDnsName': 'ec2-12-345-67-890.compute-1.amazonaws.com',
+                       u'IpOwnerId': 'amazon'}}],
+             u'PrivateDnsName': 'ip-098-76-54-321.ec2.internal',
+             u'Attachment':
+                 {u'Status': 'attached',
+                  u'DeviceIndex': 0,
+                  u'DeleteOnTermination': True,
+                  u'AttachmentId': 'eni-attach-12345678',
+                  u'AttachTime': datetime.datetime(2017, 10, 31, 12, 59, 25)},
+             u'Groups': [
+                 {u'GroupName': 'default',
+                  u'GroupId': 'sg-12345678'}],
+             u'Ipv6Addresses': [],
+             u'OwnerId': '123456789000',
              u'PrivateIpAddress': '098.76.54.321',
-             u'Primary': True,
+             u'SubnetId': 'subnet-12345678',
              u'Association':
                 {u'PublicIp': '12.345.67.890',
                  u'PublicDnsName': 'ec2-12-345-67-890.compute-1.amazonaws.com',
                  u'IpOwnerId': 'amazon'}}],
-         u'PrivateDnsName': 'ip-098-76-54-321.ec2.internal',
-         u'Attachment':
-            {u'Status': 'attached',
-             u'DeviceIndex': 0,
-             u'DeleteOnTermination': True,
-             u'AttachmentId': 'eni-attach-12345678',
-             u'AttachTime': datetime.datetime(2017, 10, 31, 12, 59, 25)},
-         u'Groups': [
-            {u'GroupName': 'default',
-             u'GroupId': 'sg-12345678'}],
-         u'Ipv6Addresses': [],
-         u'OwnerId': '123456789000',
-         u'PrivateIpAddress': '098.76.54.321',
-         u'SubnetId': 'subnet-12345678',
-         u'Association':
-            {u'PublicIp': '12.345.67.890',
-             u'PublicDnsName': 'ec2-12-345-67-890.compute-1.amazonaws.com',
-             u'IpOwnerId': 'amazon'}}],
-     u'SourceDestCheck': True,
-     u'Placement':
-        {u'Tenancy': 'default',
-         u'GroupName': '',
-         u'AvailabilityZone': 'us-east-1c'},
-     u'Hypervisor': 'xen',
-     u'BlockDeviceMappings': [
-        {u'DeviceName': '/dev/xvda',
-         u'Ebs':
-            {u'Status': 'attached',
-             u'DeleteOnTermination': True,
-             u'VolumeId': 'vol-01234567890000000',
-             u'AttachTime': datetime.datetime(2017, 10, 31, 12, 59, 26)}}],
-     u'Architecture': 'x86_64',
-     u'RootDeviceType': 'ebs',
-     u'RootDeviceName': '/dev/xvda',
-     u'VirtualizationType': 'hvm',
-     u'Tags': [{u'Value': 'test', u'Key': 'ansible'}],
-     u'AmiLaunchIndex': 0}],
-u'ReservationId': 'r-01234567890000000',
-u'Groups': [],
-u'OwnerId': '123456789000'}
+         u'SourceDestCheck': True,
+         u'Placement':
+            {u'Tenancy': 'default',
+             u'GroupName': '',
+             u'AvailabilityZone': 'us-east-1c'},
+         u'Hypervisor': 'xen',
+         u'BlockDeviceMappings': [
+            {u'DeviceName': '/dev/xvda',
+             u'Ebs':
+                {u'Status': 'attached',
+                 u'DeleteOnTermination': True,
+                 u'VolumeId': 'vol-01234567890000000',
+                 u'AttachTime': datetime.datetime(2017, 10, 31, 12, 59, 26)}}],
+         u'Architecture': 'x86_64',
+         u'RootDeviceType': 'ebs',
+         u'RootDeviceName': '/dev/xvda',
+         u'VirtualizationType': 'hvm',
+         u'Tags': [{u'Value': 'test', u'Key': 'ansible'}, {u'Value': 'aws_ec2', u'Key': 'name'}],
+         u'AmiLaunchIndex': 0}],
+    u'ReservationId': 'r-01234567890000000',
+    u'Groups': [],
+    u'OwnerId': '123456789000'
+}
 
 
 def test_get_group_by_value():
@@ -132,21 +132,56 @@ def test_get_boto_attr_chain():
 
 def test_get_group_by_name_and_value():
     inv = InventoryModule()
-    groups_to_make = ["tag:ansible=tests", "instance-type=t2.micro"]
+    groups_to_make = ["tag:ansible=test", "instance-type=t2.micro"]
     group_generator = inv._get_group_by_name_and_value(groups_to_make)
     formatted_groups_to_make = sorted([(group_name, group_value) for group_name, group_value in group_generator])
-    assert formatted_groups_to_make == [('instance-type', 't2.micro'), ('tag:', 'ansible=tests')]
+    assert formatted_groups_to_make == [('instance-type', 't2.micro'), ('tag:', 'ansible=test')]
 
 
-def test_compare_tag_groupings():
-    # _compare_tag_groupings() takes group_value (a list of tag keys or tag values or a dict of key:values and the tags associated with an instance)
-    pass
+def test_compare_tag_groupings_dict():
+    inv = InventoryModule()
+
+    formatted_instance = [instance for instance in inv._format_instance_data('us-east-1', instances)][0]
+
+    for group_name, group_value in inv._get_group_by_name_and_value(["tag:ansible=test"]):
+        found_value = inv._get_group_by_value(group_name, formatted_instance)
+        assert inv._compare_tag_groupings(group_value, found_value)
+
+
+def test_compare_tag_groupings_dict_false():
+    inv = InventoryModule()
+
+    formatted_instance = [instance for instance in inv._format_instance_data('us-east-1', instances)][0]
+
+    for group_name, group_value in inv._get_group_by_name_and_value(["tag:nothere=test"]):
+        found_value = inv._get_group_by_value(group_name, formatted_instance)
+        assert not inv._compare_tag_groupings(group_value, found_value)
+
+
+def test_compare_tag_groupings_list():
+    inv = InventoryModule()
+
+    formatted_instance = [instance for instance in inv._format_instance_data('us-east-1', instances)][0]
+
+    for group_name, group_value in inv._get_group_by_name_and_value(["tag-value=test", "tag-value=aws_ec2"]):
+        found_value = inv._get_group_by_value(group_name, formatted_instance)
+        assert inv._compare_tag_groupings(group_value, found_value)
+
+
+def test_compare_tag_groupings_list_false():
+    inv = InventoryModule()
+
+    formatted_instance = [instance for instance in inv._format_instance_data('us-east-1', instances)][0]
+
+    for group_name, group_value in inv._get_group_by_name_and_value(["tag-key=nothere"]):
+        found_value = inv._get_group_by_value(group_name, formatted_instance)
+        assert not inv._compare_tag_groupings(group_value, found_value)
 
 
 def test_assemble_groups():
     inv = InventoryModule()
     formatted_instances = [instance for instance in inv._format_instance_data('us-east-1', instances)]
-    groups_to_make = ["tag:ansible=tests", "instance-type=t2.micro"]
+    groups_to_make = ["tag:ansible=test", "instance-type=t2.micro"]
     groups = inv._assemble_groups(formatted_instances, groups_to_make)
     assert sorted(list(groups.keys())) == ['instance-type', 'tag:']
     assert groups['instance-type']['t2.micro'] == formatted_instances
@@ -173,7 +208,8 @@ def test_format_instance_data():
                  "SecurityGroups", "SourceDestCheck", "State", "StateTransitionReason",
                  "SubnetId", "Tags", "VirtualizationType", "VpcId"]
 
-    for instance in InventoryModule._format_instance_data(InventoryModule, 'us-east-1', instances):
+    inv = InventoryModule()
+    for instance in inv._format_instance_data('us-east-1', instances):
         assert instance.region == 'us-east-1'
         assert sorted(list(instance.instance_meta.keys())) == meta_keys
         assert sorted(list(instance.instance_data.keys())) == data_keys
@@ -196,7 +232,8 @@ def test_find_cred(monkeypatch):
     monkeypatch.setenv(name="AWS_PROFILE", value="")
     monkeypatch.setenv(name="AWS_DEFAULT_PROFILE", value="test_profile")
     possible_env_vars = ["AWS_PROFILE", "AWS_DEFAULT_PROFILE"]
-    assert InventoryModule._find_cred(InventoryModule, possible_env_vars) == "test_profile"
+    inv = InventoryModule()
+    assert inv._find_cred(possible_env_vars) == "test_profile"
 
 
 def test_set_credentials(monkeypatch):
