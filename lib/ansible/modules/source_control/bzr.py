2a439c02e671d48cd4e8556397fee3219bfb0bf4
Dag Wieers <dag@wieers.com>
2017-11-06 13:56:27 +0100

    Fix copyright statement

--- lib/ansible/modules/source_control/bzr.py
+++ lib/ansible/modules/source_control/bzr.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# (c) 2013, André Paramés <git@andreparames.com>
+# Copyright: (c) 2013, André Paramés <git@andreparames.com>
 # Based on the Git module by Michael DeHaan <michael.dehaan@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
