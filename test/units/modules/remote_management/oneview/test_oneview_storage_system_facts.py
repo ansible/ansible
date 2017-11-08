63d15d058a6df29f897bd951314c3637ab6919b2
Ricardo Galeno <ricardogaleno.rg@gmail.com>
2017-11-08 19:59:03 +0000

    Adds a necessary import inside storage system facts test file

--- test/units/modules/remote_management/oneview/test_oneview_storage_system_facts.py
+++ test/units/modules/remote_management/oneview/test_oneview_storage_system_facts.py
@@ -2,6 +2,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from ansible.compat.tests import unittest
+from oneview_module_loader import OneViewModuleBase
 from ansible.modules.remote_management.oneview.oneview_storage_system_facts import StorageSystemFactsModule
 from hpe_test_utils import FactsParamsTestCase
 
