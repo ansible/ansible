8c0e8362a0accee82fb07b90d07bfbd63be934cd
jgkirschbaum <juergen.kirschbaum@gmail.com>
2017-11-14 14:45:02 +0100

    __future__ and __metaclass__ added

--- lib/ansible/modules/messaging/rabbitmq_global_parameter.py
+++ lib/ansible/modules/messaging/rabbitmq_global_parameter.py
@@ -5,6 +5,9 @@
 # (c) 2017, Juergen Kirschbaum <jk@jk-itc.de>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
 ANSIBLE_METADATA = {'metadata_version': '1.0',
                     'status': ['preview'],
                     'supported_by': 'community'}
