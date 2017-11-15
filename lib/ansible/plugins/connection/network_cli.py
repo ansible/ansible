1a0bc23a06d192ef5d5476c8cf5a43b90bac5400
Nathaniel Case <ncase@redhat.com>
2017-11-15 11:13:17 -0500

    Propagate persistence setting to lower paramiko connection

--- lib/ansible/plugins/connection/network_cli.py
+++ lib/ansible/plugins/connection/network_cli.py
@@ -164,6 +164,7 @@ class Connection(ConnectionBase):
 
         p = connection_loader.get('paramiko', self._play_context, '/dev/null')
         p.set_options(direct={'look_for_keys': bool(self._play_context.password and not self._play_context.private_key_file)})
+        p.force_persistence = self.force_persistence
         ssh = p._connect()
         self.ssh = ssh.ssh
 
