532a1a285218986caecdc98a7edcad5c16e70b8b
scottb <dharmabumstead@users.noreply.github.com>
2017-11-15 02:00:24 -0800

    Updated term

--- lib/ansible/modules/utilities/logic/import_tasks.py
+++ lib/ansible/modules/utilities/logic/import_tasks.py
@@ -29,8 +29,7 @@ options:
       - Most keywords, including loops and conditionals, only applied to the imported tasks, not to this statement
         itself. If you need any of those to apply, use M(include_tasks) instead.
 notes:
-  - This is really not a module, this is a feature of the Ansible Engine, as such it cannot be overridden the same way
-    a module can.
+  - This is a core feature of Ansible, rather than a module, and cannot be overridden like a module.
 '''
 
 EXAMPLES = """
