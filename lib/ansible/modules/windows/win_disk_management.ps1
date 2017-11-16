b337180885c7d016140b5e52e741291694c96d84
Marc Tschapek <marc.tschapek@itelligence.de>
2017-11-16 16:36:53 +0100

    Changed header

--- lib/ansible/modules/windows/win_disk_management.ps1
+++ lib/ansible/modules/windows/win_disk_management.ps1
@@ -1,6 +1,6 @@
 #!powershell
-# This file is part of Ansible
 
+# Copyright: (c) 2017, Marc Tschapek <marc.tschapek@itelligence.de>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 #Requires -Module Ansible.ModuleUtils.Legacy
