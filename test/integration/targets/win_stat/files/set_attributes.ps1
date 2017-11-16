bc2b40f8fd3cf479655d9627cad6d5048c725bff
Jordan Borean <jborean93@gmail.com>
2017-11-17 07:22:57 +1000

    win_stat: simplified tests before next change (#32996)

--- test/integration/targets/win_stat/files/set_attributes.ps1
+++
@@ -1,9 +0,0 @@
-$path = $args[0]
-$attr = $args[1]
-$item = Get-Item "$path"
-
-$attributes = $item.Attributes -split ','
-If ($attributes -notcontains $attr) {
-    $attributes += $attr
-}
-$item.Attributes = $attributes -join ','
