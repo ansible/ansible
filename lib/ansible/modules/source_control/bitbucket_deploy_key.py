f8486f5a9b53f45372f8c905c3533e5f933df11e
Ali <ibrahimaeali@gmail.com>
2017-11-16 07:32:43 -0500

    Modified module to pass CI tests

--- lib/ansible/modules/source_control/bitbucket_deploy_key.py
+++ lib/ansible/modules/source_control/bitbucket_deploy_key.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
 
 ANSIBLE_METADATA = {
-    'metadata_version': '1.0',
+    'metadata_version': '1.1',
     'supported_by': 'community',
     'status': ['preview']
 }
@@ -25,7 +15,7 @@ ANSIBLE_METADATA = {
 DOCUMENTATION = '''
 ---
 module: bitbucket_deploy_key
-version_added: "2.4"
+version_added: "2.5"
 author: "Ali (@bincyber)"
 short_description: Manages deploy keys for BitBucket repositories.
 description:
