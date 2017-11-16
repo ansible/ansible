4bfdc294d9b049aa34e628c9daa723609acbc216
Jean-Bernard Jansen <jean-bernard.jansen@dataiku.com>
2017-11-16 17:32:42 +0100

    azure_rm_virtualmachine: remove spurious import

--- lib/ansible/modules/cloud/azure/azure_rm_virtualmachine.py
+++ lib/ansible/modules/cloud/azure/azure_rm_virtualmachine.py
@@ -6,7 +6,6 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
-import json
 __metaclass__ = type
 
 
