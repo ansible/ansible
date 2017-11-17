5a18375e6020cb79793a8133bb39bf478b977be7
Jordan Borean <jborean93@gmail.com>
2017-11-17 15:59:50 +1000

    win_stat: better support for links

---
+++ test/integration/targets/win_stat/library/test_symlink_file.ps1
@@ -0,0 +1,30 @@
+#!powershell
+
+#Requires -Module Ansible.ModuleUtils.Legacy
+#Requires -Module Ansible.ModuleUtils.LinkUtil
+
+$params = Parse-Args $args
+
+$state = Get-AnsibleParam -obj $params -name "state" -type "str" -default "present" -validateset "absent","present"
+$src = Get-AnsibleParam -obj $params -name "src" -type "path" -failifempty $true
+$target = Get-AnsibleParam -obj $params -name "target" -type "path" -failifempty $($state -eq "present")
+
+$result = @{
+    changed = $false
+}
+
+if ($state -eq "absent") {
+    if (Test-Path -Path $src) {
+        Load-LinkUtils
+        Remove-Link -link_path $src
+        $result.changed = $true
+    }
+} else {
+    if (-not (Test-Path -Path $src)) {
+        Load-LinkUtils
+        New-Link -link_path $src -link_target $target -link_type "link"
+        $result.changed = $true
+    }
+}
+
+Exit-Json -obj $result
