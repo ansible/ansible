bb10d3d1c05108a30e6970f96d795a88c2d32680
Ricardo Galeno <ricardogaleno.rg@gmail.com>
2017-11-22 19:08:19 +0000

    Adds a fixture to work like a setUp method to Oneview StorageSystemModule unit tests

--- test/units/modules/remote_management/oneview/test_oneview_storage_system.py
+++ test/units/modules/remote_management/oneview/test_oneview_storage_system.py
@@ -103,29 +103,31 @@ del DICT_DEFAULT_STORAGE_SYSTEM_500['credentials']['password']
 
 
 class TestStorageSystemModule(OneViewBaseTest):
-    @pytest.fixture
-    def resource(self, mock_ov_client):
-        return mock_ov_client.storage_systems
+    @pytest.fixture(autouse=True)
+    def setUp(self, mock_ansible_module, mock_ov_client):
+        self.resource = mock_ov_client.storage_systems
+        self.mock_ansible_module = mock_ansible_module
+        self.mock_ov_client = mock_ov_client
 
-    def test_should_add_new_storage_system_with_credentials_from_api300(self, resource, mock_ansible_module):
-        resource.get_by_ip_hostname.return_value = None
-        resource.add.return_value = {"name": "name"}
-        resource.update.return_value = {"name": "name"}
+    def test_should_add_new_storage_system_with_credentials_from_api300(self):
+        self.resource.get_by_ip_hostname.return_value = None
+        self.resource.add.return_value = {"name": "name"}
+        self.resource.update.return_value = {"name": "name"}
 
-        mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM)
+        self.mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM)
 
         StorageSystemModule().run()
 
-        mock_ansible_module.exit_json.assert_called_once_with(
+        self.mock_ansible_module.exit_json.assert_called_once_with(
             changed=True,
             msg=StorageSystemModule.MSG_ADDED,
             ansible_facts=dict(storage_system={"name": "name"})
         )
 
-    def test_should_add_new_storage_system_with_credentials_from_api500(self, resource, mock_ansible_module, mock_ov_client):
-        mock_ov_client.api_version = 500
-        resource.get_by_hostname.return_value = None
-        resource.add.return_value = {
+    def test_should_add_new_storage_system_with_credentials_from_api500(self):
+        self.mock_ov_client.api_version = 500
+        self.resource.get_by_hostname.return_value = None
+        self.resource.add.return_value = {
             "deviceSpecificAttributes": {
                 "discoveredPools": [
                     {
@@ -141,13 +143,13 @@ class TestStorageSystemModule(OneViewBaseTest):
                 ]
             }
         }
-        resource.update.return_value = {"name": "name"}
+        self.resource.update.return_value = {"name": "name"}
 
-        mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM_500)
+        self.mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM_500)
 
         StorageSystemModule().run()
 
-        resource.add.assert_called_once_with(
+        self.resource.add.assert_called_once_with(
             {
                 'username': 'user',
                 'password': 'pass',
@@ -155,7 +157,7 @@ class TestStorageSystemModule(OneViewBaseTest):
                 'family': 'StoreServ'
             }
         )
-        resource.update.assert_called_with(
+        self.resource.update.assert_called_with(
             {
                 'credentials': {
                     'username': 'user'
@@ -174,43 +176,43 @@ class TestStorageSystemModule(OneViewBaseTest):
             }
         )
 
-        mock_ansible_module.exit_json.assert_called_once_with(
+        self.mock_ansible_module.exit_json.assert_called_once_with(
             changed=True,
             msg=StorageSystemModule.MSG_ADDED,
             ansible_facts=dict(storage_system={"name": "name"})
         )
 
-    def test_should_not_update_when_data_is_equals(self, resource, mock_ansible_module):
-        resource.get_by_ip_hostname.return_value = DICT_DEFAULT_STORAGE_SYSTEM
-        resource.update.return_value = {"name": "name"}
+    def test_should_not_update_when_data_is_equals(self):
+        self.resource.get_by_ip_hostname.return_value = DICT_DEFAULT_STORAGE_SYSTEM
+        self.resource.update.return_value = {"name": "name"}
 
-        mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM)
+        self.mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM)
 
         StorageSystemModule().run()
 
-        mock_ansible_module.exit_json.assert_called_once_with(
+        self.mock_ansible_module.exit_json.assert_called_once_with(
             changed=False,
             msg=StorageSystemModule.MSG_ALREADY_PRESENT,
             ansible_facts=dict(storage_system=DICT_DEFAULT_STORAGE_SYSTEM)
         )
 
-    def test_should_not_update_when_data_is_equals_using_name(self, resource, mock_ansible_module):
+    def test_should_not_update_when_data_is_equals_using_name(self):
         dict_by_name = yaml.load(YAML_STORAGE_SYSTEM_BY_NAME)["data"]
 
-        resource.get_by_name.return_value = dict_by_name
+        self.resource.get_by_name.return_value = dict_by_name
 
-        mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM_BY_NAME)
+        self.mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM_BY_NAME)
 
         StorageSystemModule().run()
 
-        mock_ansible_module.exit_json.assert_called_once_with(
+        self.mock_ansible_module.exit_json.assert_called_once_with(
             changed=False,
             msg=StorageSystemModule.MSG_ALREADY_PRESENT,
             ansible_facts=dict(storage_system=dict_by_name.copy())
         )
 
-    def test_should_fail_with_missing_required_attributes(self, resource, mock_ansible_module):
-        mock_ansible_module.params = {
+    def test_should_fail_with_missing_required_attributes(self):
+        self.mock_ansible_module.params = {
             'state': 'present',
             'config': 'config',
             'data': {
@@ -220,72 +222,72 @@ class TestStorageSystemModule(OneViewBaseTest):
 
         StorageSystemModule().run()
 
-        mock_ansible_module.fail_json.assert_called_once_with(exception=mock.ANY, msg=StorageSystemModule.MSG_MANDATORY_FIELDS_MISSING)
+        self.mock_ansible_module.fail_json.assert_called_once_with(exception=mock.ANY, msg=StorageSystemModule.MSG_MANDATORY_FIELDS_MISSING)
 
-    def test_should_fail_when_credentials_attribute_is_missing(self, resource, mock_ansible_module):
-        resource.get_by_name.return_value = []
+    def test_should_fail_when_credentials_attribute_is_missing(self):
+        self.resource.get_by_name.return_value = []
 
-        mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM_BY_NAME)
+        self.mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM_BY_NAME)
 
         StorageSystemModule().run()
 
-        mock_ansible_module.fail_json.assert_called_once_with(exception=mock.ANY, msg=StorageSystemModule.MSG_CREDENTIALS_MANDATORY)
+        self.mock_ansible_module.fail_json.assert_called_once_with(exception=mock.ANY, msg=StorageSystemModule.MSG_CREDENTIALS_MANDATORY)
 
-    def test_update_when_data_has_modified_attributes(self, resource, mock_ansible_module):
+    def test_update_when_data_has_modified_attributes(self):
         data_merged = DICT_DEFAULT_STORAGE_SYSTEM.copy()
         data_merged['credentials']['newIp_hostname'] = '10.10.10.10'
 
-        resource.get_by_ip_hostname.return_value = DICT_DEFAULT_STORAGE_SYSTEM
-        resource.update.return_value = data_merged
+        self.resource.get_by_ip_hostname.return_value = DICT_DEFAULT_STORAGE_SYSTEM
+        self.resource.update.return_value = data_merged
 
-        mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM_CHANGES)
+        self.mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM_CHANGES)
 
         StorageSystemModule().run()
 
-        mock_ansible_module.exit_json.assert_called_once_with(
+        self.mock_ansible_module.exit_json.assert_called_once_with(
             changed=True,
             msg=StorageSystemModule.MSG_UPDATED,
             ansible_facts=dict(storage_system=data_merged)
         )
 
-    def test_update_when_data_has_modified_attributes_when_api500(self, resource, mock_ansible_module, mock_ov_client):
-        mock_ov_client.api_version = 500
+    def test_update_when_data_has_modified_attributes_when_api500(self):
+        self.mock_ov_client.api_version = 500
         data_merged = DICT_DEFAULT_STORAGE_SYSTEM_500.copy()
         data_merged['credentials']['newHostname'] = '10.10.10.10'
 
-        resource.get_by_hostname.return_value = DICT_DEFAULT_STORAGE_SYSTEM_500
-        resource.update.return_value = data_merged
+        self.resource.get_by_hostname.return_value = DICT_DEFAULT_STORAGE_SYSTEM_500
+        self.resource.update.return_value = data_merged
 
-        mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM_CHANGES_500)
+        self.mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM_CHANGES_500)
 
         StorageSystemModule().run()
 
-        mock_ansible_module.exit_json.assert_called_once_with(
+        self.mock_ansible_module.exit_json.assert_called_once_with(
             changed=True,
             msg=StorageSystemModule.MSG_UPDATED,
             ansible_facts=dict(storage_system=data_merged)
         )
 
-    def test_should_remove_storage_system(self, resource, mock_ansible_module):
-        resource.get_by_ip_hostname.return_value = DICT_DEFAULT_STORAGE_SYSTEM
+    def test_should_remove_storage_system(self):
+        self.resource.get_by_ip_hostname.return_value = DICT_DEFAULT_STORAGE_SYSTEM
 
-        mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM_ABSENT)
+        self.mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM_ABSENT)
 
         StorageSystemModule().run()
 
-        mock_ansible_module.exit_json.assert_called_once_with(
+        self.mock_ansible_module.exit_json.assert_called_once_with(
             changed=True,
             msg=StorageSystemModule.MSG_DELETED
         )
 
-    def test_should_do_nothing_when_storage_system_not_exist(self, resource, mock_ansible_module):
-        resource.get_by_ip_hostname.return_value = []
+    def test_should_do_nothing_when_storage_system_not_exist(self):
+        self.resource.get_by_ip_hostname.return_value = []
 
-        mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM_ABSENT)
+        self.mock_ansible_module.params = yaml.load(YAML_STORAGE_SYSTEM_ABSENT)
 
         StorageSystemModule().run()
 
-        mock_ansible_module.exit_json.assert_called_once_with(
+        self.mock_ansible_module.exit_json.assert_called_once_with(
             changed=False,
             msg=StorageSystemModule.MSG_ALREADY_ABSENT
         )
