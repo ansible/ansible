31810a8ce79d4d01d8e5b4d4e7d50b0670d0b84f
Peter Sprygada <psprygada@ansible.com>
2017-11-23 07:18:23 -0500

    fix header text

--- lib/ansible/plugins/lookup/ansible_template.py
+++ lib/ansible/plugins/lookup/ansible_template.py
@@ -1,11 +1,8 @@
-# -*- coding: utf-8 -*-
-
 # (c) 2017, Ansible by Red Hat, inc
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
 # You should have received a copy of the GNU General Public License
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-#
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
