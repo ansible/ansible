34db521b148fc5e3e0117eeb3bc6f7d70f372d12
stravassac <stravassac@gmail.com>
2017-11-24 15:14:32 +0100

    Add some Wmware file operations networkless with wmware-tools

--- lib/ansible/modules/cloud/vmware/vmware_guest_file_operation.py
+++ lib/ansible/modules/cloud/vmware/vmware_guest_file_operation.py
@@ -79,7 +79,27 @@ options:
     directory:
         description:
             - Create or delete directory
+            - 'Valid attributes are:'
+                - 'path: directory path to create or remove'
+                - 'operation: Valid values are create, delete'
+                - 'recurse: Valid values are True, False (not required, default False)'
         required: False
+    file_copy:
+        description:
+            - Copy file to vm networkless
+            - 'Valid attributes are:'
+                - 'src: file source absolute or relative'
+                - 'dest: file destination, path must be exist'
+                - 'overwrite: False or True (not required, default False)'
+        required: False
+    fetch_file:
+        description:
+            - Get file from vm networkless
+            - 'Valid attributes are:'
+                - 'src: The file on the remote system to fetch. This I(must) be a file, not a directory
+                - 'dest: file destination on localhost, path must be exist'
+        required: False
+
 '''
 
 EXAMPLES = '''
@@ -89,14 +109,48 @@ EXAMPLES = '''
     username: myUsername
     password: mySecret
     datacenter: myDatacenter
+    validate_certs: True
     folder: /vm
     vm_id: NameOfVM
     vm_username: root
     vm_password: superSecret
     directory:
       path: "/test"
-      state: present
+      operation: create
       recurse: no
+  delegate_to: localhost
+
+- name: copy file to vm
+  vmware_guest_file_operation:
+    hostname: myVSphere
+    username: myUsername
+    password: mySecret
+    datacenter: myDatacenter
+    validate_certs: True
+    folder: /vm
+    vm_id: NameOfVM
+    vm_username: root
+    vm_password: superSecret
+    copy:
+        src: "files/test.zip"
+        dest: "/root/test.zip"
+        overwrite: False
+  delegate_to: localhost
+
+- name: fetch file from vm
+  vmware_guest_file_operation:
+    hostname: myVSphere
+    username: myUsername
+    password: mySecret
+    datacenter: myDatacenter
+    validate_certs: True
+    folder: /vm
+    vm_id: NameOfVM
+    vm_username: root
+    vm_password: superSecret
+    fetch:
+        src: "/root/test.zip"
+        dest: "files/test.zip"
   delegate_to: localhost
 '''
 
@@ -104,57 +158,109 @@ try:
     from pyVmomi import vim, vmodl
 except ImportError:
     pass
+try:
+    import requests
+except ImportError:
+    REQUESTS_FOUND = False
+else:
+    REQUESTS_FOUND = True
 
+import os
 from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils._text import to_bytes, to_native
 from ansible.module_utils.vmware import (connect_to_api, find_cluster_by_name, find_datacenter_by_name,
                                          find_vm_by_id, HAS_PYVMOMI, vmware_argument_spec)
 
 
-# https://github.com/vmware/pyvmomi-community-samples/blob/master/samples/execute_program_in_vm.py
-def execute_command(content, vm, params):
-    vm_username = params['vm_username']
-    vm_password = params['vm_password']
-    program_path = params['vm_shell']
-    args = params['vm_shell_args']
-    env = params['vm_shell_env']
-    cwd = params['vm_shell_cwd']
 
-    creds = vim.vm.guest.NamePasswordAuthentication(username=vm_username, password=vm_password)
-    cmdspec = vim.vm.guest.ProcessManager.ProgramSpec(arguments=args, envVariables=env, programPath=program_path, workingDirectory=cwd)
-    cmdpid = content.guestOperationsManager.processManager.StartProgramInGuest(vm=vm, auth=creds, spec=cmdspec)
-
-    return cmdpid
-
-def directory(content, vm, params):
-    vm_username = params['vm_username']
-    vm_password = params['vm_password']
+def directory(module, content, vm):
+    operations_permit = ['create','delete']
+    vm_username = module.params['vm_username']
+    vm_password = module.params['vm_password']
         
-    if "path" not in params["directory"]:
-        self.module.fail_json(msg="directory.path is mandatory")
-    if "state" not in params["directory"]:
-        self.module.fail_json(msg="directory.state is mandatory")
-    if not "recurse" in params["directory"]:
+    if "path" not in module.params["directory"]:
+        module.fail_json(msg="directory.path is mandatory")
+    if "operation" not in module.params["directory"]:
+        module.fail_json(msg="directory.operation is mandatory")
+    if not "recurse" in module.params["directory"]:
         recurse = False
     else:
-        recurse = params["directory"]['recurse']
-    state = params["directory"]['state']
-    path = params["directory"]['path']
+        recurse = module.params["directory"]['recurse']
+    operation = module.params["directory"]['operation']
+    path = module.params["directory"]['path']
+    if operation not in operations_permit:
+        module.fail_json(msg="directory.operation valid values are create, delete")
 
     creds = vim.vm.guest.NamePasswordAuthentication(username=vm_username, password=vm_password)
     file_manager = content.guestOperationsManager.fileManager
-    if state == "present":
+    if operation == "create":
         file_manager.MakeDirectoryInGuest(vm=vm, auth=creds, directoryPath=path,
                                                     createParentDirectories=recurse)
-    if state == "absent":
+    if operation == "delete":
         file_manager.DeleteDirectoryInGuest(vm=vm, auth=creds, directoryPath=path,
                                                     recursive=recurse)
 
 
-def file(content, vm, params):
-    vm_username = params['vm_username']
-    vm_password = params['vm_password']
-    vm_path = params['vm_path']
-    state = params['vm_state']
+def fetch(module,content, vm):
+    vm_username = module.params['vm_username']
+    vm_password = module.params['vm_password']
+    
+    if "src" not in module.params["fetch"]:
+        module.fail_json(msg="fetch.src is mandatory")
+    if "dest" not in module.params["fetch"]:
+        module.fail_json(msg="fetch.dest is mandatory")
+    
+    dest = module.params["fetch"]['dest']
+    src = module.params['fetch']['src']
+    
+    creds = vim.vm.guest.NamePasswordAuthentication(username=vm_username, password=vm_password)
+    file_manager = content.guestOperationsManager.fileManager
+
+    fileTransferInfo = file_manager.InitiateFileTransferFromGuest(vm=vm, auth=creds,
+                                                                              guestFilePath=src)
+    url = fileTransferInfo.url
+    f = requests.get(url, verify=module.params['validate_certs'])
+    with open(dest, "wb") as local_file:
+        local_file.write(f.content)
+
+def copy(module,content, vm):
+    vm_username = module.params['vm_username']
+    vm_password = module.params['vm_password']
+    
+    if "src" not in module.params["copy"]:
+        module.fail_json(msg="copy.src is mandatory")
+    if "dest" not in module.params["copy"]:
+        module.fail_json(msg="copy.dest is mandatory")
+    if "overwrite" in module.params["copy"]:
+        overwrite = module.params["copy"]["overwrite"]
+    else:
+        overwrite = False
+    
+    dest = module.params["copy"]['dest']
+    src = module.params['copy']['src']
+    b_src = to_bytes(src, errors='surrogate_or_strict')
+    if not os.path.exists(b_src):
+        module.fail_json(msg="Source %s not found" % (src))
+    if not os.access(b_src, os.R_OK):
+        module.fail_json(msg="Source %s not readable" % (src))
+    if os.path.isdir(b_src):
+        module.fail_json(msg="copy does not support copy of directory: %s" % (src))
+    
+    data = None
+    with open(b_src, "r") as local_file:
+          data = local_file.read()
+    file_size = os.path.getsize(b_src)    
+
+    creds = vim.vm.guest.NamePasswordAuthentication(username=vm_username, password=vm_password)
+    file_attributes = vim.vm.guest.FileManager.FileAttributes()
+    file_manager = content.guestOperationsManager.fileManager
+    url = file_manager.InitiateFileTransferToGuest(vm=vm, auth=creds, guestFilePath=dest,
+                                                           fileAttributes=file_attributes, overwrite=overwrite,
+                                                           fileSize=file_size)
+    r = requests.put(url, data=data, verify=module.params['validate_certs'])
+    if r.status_code <> 200:
+       raise Exception('initiateFileTransferToGuest : problem during file transfer') 
+   
 
 
 def main():
@@ -166,7 +272,9 @@ def main():
                               vm_id_type=dict(default='vm_name', type='str', choices=['inventory_path', 'uuid', 'dns_name', 'vm_name']),
                               vm_username=dict(type='str', required=True),
                               vm_password=dict(type='str', no_log=True, required=True),
-                              directory=dict(type='dict', default={})
+                              directory=dict(type='dict', default={}),
+                              copy=dict(type='dict', default={}),
+                              fetch=dict(type='dict', default={})
                               ))
 
     module = AnsibleModule(argument_spec=argument_spec,
@@ -176,6 +284,10 @@ def main():
 
     if not HAS_PYVMOMI:
         module.fail_json(changed=False, msg='pyvmomi is required for this module')
+    
+    if not REQUESTS_FOUND:
+            self.module.fail_json(
+                msg='requests library is required for this module')
 
     datacenter_name = module.params['datacenter']
     cluster_name = module.params['cluster']
@@ -203,8 +315,12 @@ def main():
         module.fail_json(msg='Unable to find virtual machine.')
 
     try:
-        if 'directory' in module.params:
-            msg = directory(content, vm, module.params)
+        if len(module.params['directory']) > 0:
+            msg = directory(module, content, vm)
+        if len(module.params['copy']) > 0:
+            msg = copy(module, content, vm)
+        if len(module.params['fetch']) > 0:
+            msg = fetch(module, content, vm)
         module.exit_json(changed=True, uuid=vm.summary.config.uuid, msg=msg)
     except vmodl.RuntimeFault as runtime_fault:
         module.fail_json(changed=False, msg=runtime_fault.msg)
