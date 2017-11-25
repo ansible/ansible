32fa4a2506626ea552d1500829c119f5374d7a95
Yanis Guenane <yguenane@gmail.com>
2017-11-25 22:50:28 +0100

    Adding extends_documenation_fragment in crypto/* (#33253)
    All crypto modules uses file common arguments to specify generated file
    permissions. This commits aims to add the extends_documentation_fragment
    in the doc so it is automatically stated.

--- lib/ansible/modules/crypto/openssl_publickey.py
+++ lib/ansible/modules/crypto/openssl_publickey.py
@@ -22,8 +22,7 @@ short_description: Generate an OpenSSL public key from its private key.
 description:
     - "This module allows one to (re)generate OpenSSL public keys from their private keys.
        It uses the pyOpenSSL python library to interact with openssl. Keys are generated
-       in PEM format. This module works only if the version of PyOpenSSL is recent enough (> 16.0.0).
-       This module uses file common arguments to specify generated file permissions."
+       in PEM format. This module works only if the version of PyOpenSSL is recent enough (> 16.0.0)."
 requirements:
     - "python-pyOpenSSL"
 options:
@@ -59,6 +58,7 @@ options:
         description:
             - The passphrase for the privatekey.
         version_added: "2.4"
+extends_documentation_fragment: files
 '''
 
 EXAMPLES = '''
