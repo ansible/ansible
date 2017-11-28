0526bd0716a92cf3bf1aa4bd43222578b94edded
Nathaniel Case <this.is@nathanielca.se>
2017-11-28 16:28:58 -0500

    Auth to become (#33319)
    * terminal.on_(de)?authorize -> terminal.on_(un)?become
    
    * Update network_cli
    
    * Readd on_(de)?authorize as wrappers
    
    * Update tests

--- lib/ansible/plugins/terminal/dellos9.py
+++ lib/ansible/plugins/terminal/dellos9.py
@@ -51,7 +51,7 @@ class TerminalModule(TerminalBase):
         except AnsibleConnectionFailure:
             raise AnsibleConnectionFailure('unable to set terminal parameters')
 
-    def on_authorize(self, passwd=None):
+    def on_become(self, passwd=None):
         if self._get_prompt().endswith(b'#'):
             return
 
@@ -65,7 +65,7 @@ class TerminalModule(TerminalBase):
         except AnsibleConnectionFailure:
             raise AnsibleConnectionFailure('unable to elevate privilege to enable mode')
 
-    def on_deauthorize(self):
+    def on_unbecome(self):
         prompt = self._get_prompt()
         if prompt is None:
             # if prompt is None most likely the terminal is hung up at a prompt
