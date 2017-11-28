0ff4f5a3af8b8408e435a82d327402976be6cc6f
zikalino <zikalino@microsoft.com>
2017-11-28 14:38:37 +0800

    fixed line too long

--- lib/ansible/modules/cloud/azure/azure_rm_containerregistry.py
+++ lib/ansible/modules/cloud/azure/azure_rm_containerregistry.py
@@ -93,7 +93,7 @@ state:
                 - Resource ID
             returned: always
             type: str
-            sample: /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/myResourceGroup/providers/Microsoft.ContainerRegistry/registries/myRegistry
+            sample: /subscriptions/00000000-0000-0000-0000-000000000/resourceGroups/myResourceGroup/providers/Microsoft.ContainerRegistry/registries/myRegistry
         name:
             description:
                 - Registry name
