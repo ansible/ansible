438b3ebe13a51e3bcb8ea6ad7169a6a085985b82
zikalino <zikalino@microsoft.com>
2017-11-29 16:38:12 +0800

    updates as requested in the review

--- lib/ansible/modules/cloud/azure/azure_rm_sql_server.py
+++ lib/ansible/modules/cloud/azure/azure_rm_sql_server.py
@@ -17,9 +17,9 @@ DOCUMENTATION = '''
 ---
 module: azure_rm_sql_server
 version_added: "2.5"
-short_description: Manage an Server.
+short_description: Manage SQL Server instance
 description:
-    - Create, update and delete an instance of Server.
+    - Create, update and delete instance of SQL Server
 
 options:
     resource_group:
@@ -33,33 +33,26 @@ options:
     location:
         description:
             - Resource location.
-        required: True
     tags:
         description:
             - Resource tags.
-        required: False
     identity:
         description:
             - The Azure Active Directory identity of the server.
-        required: False
         suboptions:
             type:
                 description:
                     - "The identity type. Set this to 'SystemAssigned' in order to automatically create and assign an Azure Active Directory principal for th
                        e resource. Possible values include: 'SystemAssigned'"
-                required: False
-    administrator_login:
+    admin_username:
         description:
             - Administrator username for the server. Once created it cannot be changed.
-        required: False
-    administrator_login_password:
+    admin_password:
         description:
             - The administrator login password (required for server creation).
-        required: False
     version:
         description:
             - The version of the server.
-        required: False
 
 extends_documentation_fragment:
     - azure
@@ -71,24 +64,45 @@ author:
 '''
 
 EXAMPLES = '''
-      - name: Create (or update) Servers
-        azure_rm_sql_server:
-          resource_group: "{{ resource_group }}"
-          name: zims-server
-          location: westus
-          tags: "{{ tags }}"
-          identity:
-            type: "{{ type }}"
-          administrator_login: mylogin
-          administrator_login_password: Testpasswordxyz12!
-          version: "{{ version }}"
+  - name: Create (or update) SQL Server
+    azure_rm_sql_server:
+      resource_group: resource_group
+      name: server_name
+      location: westus
+      admin_username: mylogin
+      admin_password: Testpasswordxyz12!
 '''
 
 RETURN = '''
 state:
-    description: Current state of Server
+    description: Current state of SQL Server
     returned: always
-    type: dict
+    type: complex
+    contains:
+        id:
+            description:
+                - Resource ID.
+            returned: always
+            type: str
+            sample: /subscriptions/00000000-1111-2222-3333-444444444444/resourceGroups/sqlcrudtest-7398/providers/Microsoft.Sql/servers/sqlcrudtest-4645
+        version:
+            description:
+                - The version of the server.
+            returned: always
+            type: str
+            sample: 12.0
+        state:
+            description:
+                - The state of the server.
+            returned: always
+            type: str
+            sample: state
+        fully_qualified_domain_name:
+            description:
+                - The fully qualified domain name of the server.
+            returned: always
+            type: str
+            sample: sqlcrudtest-4645.database.windows.net
 '''
 
 from ansible.module_utils.azure_rm_common import AzureRMModuleBase
@@ -104,7 +118,7 @@ except ImportError:
 
 
 class AzureRMServers(AzureRMModuleBase):
-    """Configuration class for an Azure RM Server resource"""
+    """Configuration class for an Azure RM SQL Server resource"""
 
     def __init__(self):
         self.module_arg_spec = dict(
@@ -118,7 +132,7 @@ class AzureRMServers(AzureRMModuleBase):
             ),
             location=dict(
                 type='str',
-                required=True
+                required=False
             ),
             tags=dict(
                 type='dict',
@@ -128,11 +142,11 @@ class AzureRMServers(AzureRMModuleBase):
                 type='dict',
                 required=False
             ),
-            administrator_login=dict(
+            admin_username=dict(
                 type='str',
                 required=False
             ),
-            administrator_login_password=dict(
+            admin_password=dict(
                 type='str',
                 required=False
             ),
@@ -172,16 +186,15 @@ class AzureRMServers(AzureRMModuleBase):
                 self.parameters["tags"] = kwargs[key]
             elif key == "identity":
                 self.parameters["identity"] = kwargs[key]
-            elif key == "administrator_login":
+            elif key == "admin_username":
                 self.parameters["administrator_login"] = kwargs[key]
-            elif key == "administrator_login_password":
+            elif key == "admin_password":
                 self.parameters["administrator_login_password"] = kwargs[key]
             elif key == "version":
                 self.parameters["version"] = kwargs[key]
 
-        response = None
+        old_response = None
         results = dict()
-        to_be_updated = False
 
         self.mgmt_client = self.get_mgmt_svc_client(SqlManagementClient,
                                                     base_url=self._cloud_environment.endpoints.resource_manager)
@@ -191,48 +204,54 @@ class AzureRMServers(AzureRMModuleBase):
         except CloudError:
             self.fail('resource group {0} not found'.format(self.resource_group))
 
-        response = self.get_servers()
+        old_response = self.get_sqlserver()
 
-        if not response:
-            self.log("Server instance doesn't exist")
+        if not old_response:
+            self.log("SQL Server instance doesn't exist")
             if self.state == 'absent':
-                self.log("Nothing to delete")
-            else:
-                to_be_updated = True
+                self.log("Old instance didn't exist")
         else:
-            self.log("Server instance already exists")
+            self.log("SQL Server instance already exists")
             if self.state == 'absent':
-                self.delete_servers()
+                self.delete_sqlserver()
                 self.results['changed'] = True
-                self.log("Server instance deleted")
+                self.log("SQL Server instance deleted")
             elif self.state == 'present':
-                self.log("Need to check if Server instance has to be deleted or may be updated")
-                to_be_updated = True
+                self.log("Need to check if SQL Server instance has to be deleted or may be updated")
 
         if self.state == 'present':
 
-            self.log("Need to Create / Update the Server instance")
+            self.log("Need to Create / Update the SQL Server instance")
 
             if self.check_mode:
                 return self.results
 
-            if to_be_updated:
-                self.results['state'] = self.create_update_servers()
+            self.results['state'] = self.create_update_sqlserver()
+            if not old_response:
                 self.results['changed'] = True
             else:
-                self.results['state'] = response
+                self.results['changed'] = (cmp(old_response, self.results['state']) != 0)
 
+            # remove unnecessary fields from return state
+            self.results['state'].pop('name', None)
+            self.results['state'].pop('type', None)
+            self.results['state'].pop('tags', None)
+            self.results['state'].pop('location', None)
+            self.results['state'].pop('identity', None)
+            self.results['state'].pop('kind', None)
+            self.results['state'].pop('administrator_login', None)
+            self.results['state'].pop('administrator_login_password', None)
             self.log("Creation / Update done")
 
         return self.results
 
-    def create_update_servers(self):
+    def create_update_sqlserver(self):
         '''
-        Creates or updates Server with the specified configuration.
+        Creates or updates SQL Server with the specified configuration.
 
-        :return: deserialized Server instance state dictionary
+        :return: deserialized SQL Server instance state dictionary
         '''
-        self.log("Creating / Updating the Server instance {0}".format(self.name))
+        self.log("Creating / Updating the SQL Server instance {0}".format(self.name))
 
         try:
             response = self.mgmt_client.servers.create_or_update(self.resource_group,
@@ -242,42 +261,42 @@ class AzureRMServers(AzureRMModuleBase):
                 response = self.get_poller_result(response)
 
         except CloudError as exc:
-            self.log('Error attempting to create the Server instance.')
-            self.fail("Error creating the Server instance: {0}".format(str(exc)))
+            self.log('Error attempting to create the SQL Server instance.')
+            self.fail("Error creating the SQL Server instance: {0}".format(str(exc)))
         return response.as_dict()
 
-    def delete_servers(self):
+    def delete_sqlserver(self):
         '''
-        Deletes specified Server instance in the specified subscription and resource group.
+        Deletes specified SQL Server instance in the specified subscription and resource group.
 
         :return: True
         '''
-        self.log("Deleting the Server instance {0}".format(self.name))
+        self.log("Deleting the SQL Server instance {0}".format(self.name))
         try:
             response = self.mgmt_client.servers.delete(self.resource_group,
                                                        self.name)
         except CloudError as e:
-            self.log('Error attempting to delete the Server instance.')
-            self.fail("Error deleting the Server instance: {0}".format(str(e)))
+            self.log('Error attempting to delete the SQL Server instance.')
+            self.fail("Error deleting the SQL Server instance: {0}".format(str(e)))
 
         return True
 
-    def get_servers(self):
+    def get_sqlserver(self):
         '''
-        Gets the properties of the specified Server.
+        Gets the properties of the specified SQL Server.
 
-        :return: deserialized Server instance state dictionary
+        :return: deserialized SQL Server instance state dictionary
         '''
-        self.log("Checking if the Server instance {0} is present".format(self.name))
+        self.log("Checking if the SQL Server instance {0} is present".format(self.name))
         found = False
         try:
             response = self.mgmt_client.servers.get(self.resource_group,
                                                     self.name)
             found = True
             self.log("Response : {0}".format(response))
-            self.log("Server instance : {0} found".format(response.name))
+            self.log("SQL Server instance : {0} found".format(response.name))
         except CloudError as e:
-            self.log('Did not find the Server instance.')
+            self.log('Did not find the SQL Server instance.')
         if found is True:
             return response.as_dict()
 
