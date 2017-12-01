46163f2d645fe8b01491e1022ada63a687b649f8
Sam Doran <sdoran@redhat.com>
2017-12-01 14:50:12 -0500

    Build out more tests and test with linear and free strategy for each type

--- test/integration/targets/include_import/runme.sh
+++ test/integration/targets/include_import/runme.sh
@@ -4,13 +4,26 @@ set -eux
 
 export ANSIBLE_ROLES_PATH=./roles
 
-# Role
-ansible-playbook role/test_include_role.yml -i ../../inventory "$@"
-ansible-playbook role/test_import_role.yml -i ../../inventory "$@"
+## Role
+# Include
+ANSIBLE_STRATEGY='linear' ansible-playbook role/test_include_role.yml -i ../../inventory "$@"
+ANSIBLE_STRATEGY='free' ansible-playbook role/test_include_role.yml -i ../../inventory "$@"
 
-# Tasks
-ansible-playbook tasks/test_include_tasks.yml -i ../../inventory "$@"
-ansible-playbook tasks/test_import_tasks.yml -i ../../inventory "$@"
+# Import
+# ANSIBLE_STRATEGY='linear' ansible-playbook role/test_import_role.yml -i ../../inventory "$@"
+# ANSIBLE_STRATEGY='free' ansible-playbook role/test_import_role.yml -i ../../inventory "$@"
 
-# Play
-ansible-playbook play/test_import_play.yml -i ../../inventory "$@"
+
+## Tasks
+# Include
+# ANSIBLE_STRATEGY='linear' ansible-playbook tasks/test_include_tasks.yml -i ../../inventory "$@"
+# ANSIBLE_STRATEGY='free' ansible-playbook tasks/test_include_tasks.yml -i ../../inventory "$@"
+
+# Import
+# ANSIBLE_STRATEGY='linear' ansible-playbook tasks/test_import_tasks.yml -i ../../inventory "$@"
+# ANSIBLE_STRATEGY='free' ansible-playbook tasks/test_import_tasks.yml -i ../../inventory "$@"
+
+## Play
+# Import
+# ANSIBLE_STRATEGY='linear' ansible-playbook play/test_import_play.yml -i ../../inventory "$@"
+# ANSIBLE_STRATEGY='free' ansible-playbook play/test_import_play.yml -i ../../inventory "$@"
