a01b83522845f0bb0dd2dd7c074c7db9c3f77b95
Brian Coca <brian.coca+git@gmail.com>
2017-12-01 17:44:15 -0500

    singular

--- lib/ansible/plugins/lookup/var.py
+++ lib/ansible/plugins/lookup/var.py
@@ -4,15 +4,15 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 DOCUMENTATION = """
-    lookup: vars
+    lookup: var
     author: Ansible Core
     version_added: "2.5"
     short_description: Lookup templated value of variable
     description:
       - Retrieves the value of an Ansible variable.
     options:
-      _terms:
-        description: The variable name(s) to look up.
+      _term:
+        description: The variable name to look up.
         required: True
       default:
         description:
@@ -22,39 +22,37 @@ DOCUMENTATION = """
 
 EXAMPLES = """
 - name: Show value of 'variablename'
-  debug: msg="{{ lookup('vars', 'variabl' + myvar)}}"
+  debug: msg="{{ lookup('var', 'variabl' + myvar)}}"
   vars:
     variablename: hello
     myvar: ename
 
 - name: Show default empty since i dont have 'variablnotename'
-  debug: msg="{{ lookup('vars', 'variabl' + myvar, default='')}}"
+  debug: msg="{{ lookup('var', 'variabl' + myvar, default='')}}"
   vars:
     variablename: hello
     myvar: notename
 
 - name: Produce an error since i dont have 'variablnotename'
-  debug: msg="{{ lookup('vars', 'variabl' + myvar)}}"
+  debug: msg="{{ lookup('var', 'variabl' + myvar)}}"
   ignore_errors: True
   vars:
     variablename: hello
     myvar: notename
 
 - name:  find some 'prefixed vars' in loop
-  debug: msg="{{ lookup('vars', 'ansible_play_' + item) }}"
+  debug: msg="{{ lookup('var', 'ansible_play_' + item) }}"
   loop:
     - hosts
     - batch
     - hosts_all
 
-- name:  pass list of vars
-  debug: msg="{{ lookup('vars', 'ansible_distribution', 'ansible_pkg_mgr', 'ansible_service_mgr')}}"
 """
 
 RETURN = """
-_values:
+_value:
   description:
-    - list value(s) of the variable(s) requested
+    - valueof the variable requested
 """
 
 from ansible.errors import AnsibleError, AnsibleUndefinedVariable
@@ -66,7 +64,7 @@ class LookupModule(LookupBase):
 
     def run(self, terms, variables=None, **kwargs):
 
-        ret = []
+        ret = None
         if variables is not None:
             self._templar.set_available_variables(variables)
         myvars = getattr(self._templar, '_available_variables', {})
@@ -75,22 +73,28 @@ class LookupModule(LookupBase):
         default = self.get_option('default')
 
         # Assumes listify_plugin_terms is called previous to run so each term is already templated and terms is always a list
-        for term in terms:
-            if not isinstance(term, string_types):
-                raise AnsibleError('Invalid setting identifier, "%s" is not a string, its a %s' % (term, type(term)))
+        if isinstance(terms, list):
+            term = terms[0]
+        elif isinstance(terms, string_types):
+            term = terms
+        else:
+            raise AnsibleError('Invalid terms passed to "var" lookup, "%s" is not a string, its a %s' % (terms, type(terms)))
 
-            try:
-                if term in myvars:
-                    value = myvars[term]
-                elif 'hostvars' in myvars and term in myvars['hostvars']:
-                    # maybe it is a host var?
-                    value = myvars['hostvars'][term]
-                else:
-                    raise AnsibleUndefinedVariable('No variable found with this name: %s' % term)
-                ret.append(self._templar.template(value, fail_on_undefined=True))
-            except AnsibleUndefinedVariable:
-                if default is not None:
-                    ret.append(default)
-                else:
-                    raise
+        if not isinstance(term, string_types):
+            raise AnsibleError('Invalid setting identifier, "%s" is not a string, its a %s' % (term, type(term)))
+
+        try:
+            if term in myvars:
+                value = myvars[term]
+            elif 'hostvars' in myvars and term in myvars['hostvars']:
+                # maybe it is a host var?
+                value = myvars['hostvars'][term]
+            else:
+                raise AnsibleUndefinedVariable('No variable found with this name: %s' % term)
+            ret = self._templar.template(value, fail_on_undefined=True)
+        except AnsibleUndefinedVariable:
+            if default is None:
+                ret = default
+            else:
+                raise
         return ret
