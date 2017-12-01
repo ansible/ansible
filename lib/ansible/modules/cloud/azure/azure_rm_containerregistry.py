664b72d1e2f00948518d61da099dc05f59cb5507
zikalino <zikalino@microsoft.com>
2017-12-01 10:40:43 +0800

    one more update

--- lib/ansible/modules/cloud/azure/azure_rm_containerregistry.py
+++ lib/ansible/modules/cloud/azure/azure_rm_containerregistry.py
@@ -305,7 +305,7 @@ class AzureRMContainerRegistry(AzureRMModuleBase):
 
             self.results['state'] = self.create_update_containerregistry(to_do)
             if to_do != Actions.NoAction:
-                self.results['changed'] = (self.results['state'].__ne__(response))
+                self.results['changed'] = (response is None) or (self.results['state'].__ne__(response))
             else:
                 self.results['changed'] = False
 
