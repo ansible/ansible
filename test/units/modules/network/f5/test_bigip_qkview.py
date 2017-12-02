da5638b4817b86417d4f202e4e17ecf530d160c2
Tim Rupp <caphrim007@gmail.com>
2017-12-01 21:16:11 -0800

    Fixes various things in bigip tests (#33477)
    an upstream change was introduced that broke downstream code. This
    path rectifies that error

--- test/units/modules/network/f5/test_bigip_qkview.py
+++ test/units/modules/network/f5/test_bigip_qkview.py
@@ -15,9 +15,9 @@ if sys.version_info < (2, 7):
     raise SkipTest("F5 Ansible modules require Python >= 2.7")
 
 from ansible.compat.tests import unittest
-from ansible.compat.tests.mock import patch, Mock
+from ansible.compat.tests.mock import Mock
+from ansible.compat.tests.mock import patch
 from ansible.module_utils.f5_utils import AnsibleF5Client
-from units.modules.utils import set_module_args
 
 try:
     from library.bigip_qkview import Parameters
@@ -26,6 +26,7 @@ try:
     from library.bigip_qkview import BulkLocationManager
     from library.bigip_qkview import ArgumentSpec
     from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
+    from test.unit.modules.utils import set_module_args
 except ImportError:
     try:
         from ansible.modules.network.f5.bigip_qkview import Parameters
@@ -34,6 +35,7 @@ except ImportError:
         from ansible.modules.network.f5.bigip_qkview import BulkLocationManager
         from ansible.modules.network.f5.bigip_qkview import ArgumentSpec
         from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
+        from units.modules.utils import set_module_args
     except ImportError:
         raise SkipTest("F5 Ansible modules require the f5-sdk Python library")
 
