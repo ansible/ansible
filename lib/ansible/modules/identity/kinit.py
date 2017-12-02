a1516840ad0f73bdb9291e98e035117a10ab7a85
Ali <ibrahimaeali@gmail.com>
2017-12-02 13:13:40 -0500

    Updated kinit module

--- lib/ansible/modules/identity/kinit.py
+++ lib/ansible/modules/identity/kinit.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
 
 ANSIBLE_METADATA = {
-    'metadata_version': '1.0',
+    'metadata_version': '1.1',
     'supported_by': 'community',
     'status': ['preview']
 }
@@ -25,7 +15,7 @@ ANSIBLE_METADATA = {
 DOCUMENTATION = '''
 ---
 module: kinit
-version_added: "2.4"
+version_added: "2.5"
 author: "Ali (@bincyber)"
 short_description: Obtain a Kerberos ticket-granting ticket.
 description:
@@ -84,7 +74,6 @@ rc:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-from distutils.spawn import find_executable
 import shlex
 
 
@@ -106,9 +95,7 @@ def main():
     password = module.params['password']
     lifetime = module.params.get('lifetime', 60)
 
-    kinit = find_executable('kinit')
-    if kinit is None:
-        module.fail_json(msg="kinit is required")
+    kinit = module.get_bin_path("kinit", required=True)
 
     cmd = shlex.split('{0} {1} -l {2}'.format(kinit, principal, lifetime))
 
