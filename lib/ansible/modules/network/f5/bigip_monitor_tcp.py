c9c3f20c051e9dd85959d655c45dcbcdaee44c4e
Tim Rupp <caphrim007@gmail.com>
2017-12-02 20:35:06 -0800

    Fixes tcp monitors (#33494)
    * Fixes tcp monitors
    
    Formatting fixes and a missing default parent. This patch fixes
    that.
    
    * Fixes upstream errors

--- lib/ansible/modules/network/f5/bigip_monitor_tcp.py
+++ lib/ansible/modules/network/f5/bigip_monitor_tcp.py
@@ -4,14 +4,18 @@
 # Copyright (c) 2017 F5 Networks Inc.
 # GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
-DOCUMENTATION = '''
+DOCUMENTATION = r'''
 ---
 module: bigip_monitor_tcp
-short_description: Manages F5 BIG-IP LTM tcp monitors.
+short_description: Manages F5 BIG-IP LTM tcp monitors
 description: Manages F5 BIG-IP LTM tcp monitors via iControl SOAP API.
 version_added: "1.4"
 options:
@@ -26,7 +30,7 @@ options:
       - The parent template of this monitor template. Once this value has
         been set, it cannot be changed. By default, this value is the C(tcp)
         parent on the C(Common) partition.
-    default: "/Common/tcp"
+    default: /Common/tcp
   send:
     description:
       - The send string for the monitor call.
@@ -45,7 +49,7 @@ options:
       - The template type of this monitor template.
       - Deprecated in 2.4. Use one of the C(bigip_monitor_tcp_echo) or
         C(bigip_monitor_tcp_half_open) modules instead.
-    default: 'tcp'
+    default: tcp
     choices:
       - tcp
       - tcp_echo
@@ -82,6 +86,11 @@ options:
         node to be marked up immediately after a valid response is received
         from the node. If this parameter is not provided when creating
         a new monitor, then the default value will be 0.
+  partition:
+    description:
+      - Device partition to manage resources on.
+    default: Common
+    version_added: 2.5
 notes:
   - Requires the f5-sdk Python package on the host. This is as easy as pip
     install f5-sdk.
@@ -93,70 +102,70 @@ author:
   - Tim Rupp (@caphrim007)
 '''
 
-EXAMPLES = '''
+EXAMPLES = r'''
 - name: Create TCP Monitor
   bigip_monitor_tcp:
-      state: "present"
-      server: "lb.mydomain.com"
-      user: "admin"
-      password: "secret"
-      name: "my_tcp_monitor"
-      type: "tcp"
-      send: "tcp string to send"
-      receive: "tcp string to receive"
+    state: present
+    server: lb.mydomain.com
+    user: admin
+    password: secret
+    name: my_tcp_monitor
+    type: tcp
+    send: tcp string to send
+    receive: tcp string to receive
   delegate_to: localhost
 
 - name: Remove TCP Monitor
   bigip_monitor_tcp:
-      state: "absent"
-      server: "lb.mydomain.com"
-      user: "admin"
-      password: "secret"
-      name: "my_tcp_monitor"
+    state: absent
+    server: lb.mydomain.com
+    user: admin
+    password: secret
+    name: my_tcp_monitor
   delegate_to: localhost
 '''
 
-RETURN = '''
+RETURN = r'''
 parent:
-    description: New parent template of the monitor.
-    returned: changed
-    type: string
-    sample: "tcp"
+  description: New parent template of the monitor.
+  returned: changed
+  type: string
+  sample: tcp
 send:
-    description: The new send string for this monitor.
-    returned: changed
-    type: string
-    sample: "tcp string to send"
+  description: The new send string for this monitor.
+  returned: changed
+  type: string
+  sample: tcp string to send
 receive:
-    description: The new receive string for this monitor.
-    returned: changed
-    type: string
-    sample: "tcp string to receive"
+  description: The new receive string for this monitor.
+  returned: changed
+  type: string
+  sample: tcp string to receive
 ip:
-    description: The new IP of IP/port definition.
-    returned: changed
-    type: string
-    sample: "10.12.13.14"
+  description: The new IP of IP/port definition.
+  returned: changed
+  type: string
+  sample: 10.12.13.14
 port:
-    description: The new port of IP/port definition.
-    returned: changed
-    type: string
-    sample: "admin@root.local"
+  description: The new port of IP/port definition.
+  returned: changed
+  type: string
+  sample: admin@root.local
 interval:
-    description: The new interval in which to run the monitor check.
-    returned: changed
-    type: int
-    sample: 2
+  description: The new interval in which to run the monitor check.
+  returned: changed
+  type: int
+  sample: 2
 timeout:
-    description: The new timeout in which the remote system must respond to the monitor.
-    returned: changed
-    type: int
-    sample: 10
+  description: The new timeout in which the remote system must respond to the monitor.
+  returned: changed
+  type: int
+  sample: 10
 time_until_up:
-    description: The new time in which to mark a system as up after first successful response.
-    returned: changed
-    type: int
-    sample: 2
+  description: The new time in which to mark a system as up after first successful response.
+  returned: changed
+  type: int
+  sample: 2
 '''
 
 import os
@@ -171,8 +180,8 @@ from ansible.module_utils.f5_utils import AnsibleF5Client
 from ansible.module_utils.f5_utils import AnsibleF5Parameters
 from ansible.module_utils.f5_utils import HAS_F5SDK
 from ansible.module_utils.f5_utils import F5ModuleError
-from ansible.module_utils.f5_utils import iteritems
-from ansible.module_utils.f5_utils import defaultdict
+from ansible.module_utils.six import iteritems
+from collections import defaultdict
 
 try:
     from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
@@ -909,7 +918,7 @@ class ArgumentSpec(object):
             # Make this assume "tcp" in the partition specified. The user
             # is required to specify the full path if they want to use a different
             # partition.
-            parent=dict(),
+            parent=dict(default='tcp'),
 
             send=dict(),
             receive=dict(),
