1d80820cced954b370547f3269b2526aae1b1824
Toshio Kuratomi <a.badger@gmail.com>
2017-12-03 08:00:02 -0800

    Port atomic_move test to pytest.
    Working on getting rid of need to maintain procenv

--- test/units/module_utils/basic/test_atomic_move.py
+++ test/units/module_utils/basic/test_atomic_move.py
@@ -9,222 +9,209 @@ __metaclass__ = type
 
 import errno
 import json
-import os
-import sys
-from io import BytesIO, StringIO
+from itertools import product
 
-from units.mock.procenv import ModuleTestCase, swap_stdin_and_argv
+import pytest
 
-from ansible.compat.tests import unittest
-from ansible.compat.tests.mock import patch, MagicMock, mock_open, Mock, call
-from ansible.module_utils.six.moves import builtins
+from ansible.module_utils import basic
 
-realimport = builtins.__import__
 
-class TestAtomicMove(ModuleTestCase):
-    @patch('tempfile.mkstemp')
-    @patch('os.umask')
-    @patch('shutil.copyfileobj')
-    @patch('shutil.move')
-    @patch('shutil.copy2')
-    @patch('os.rename')
-    @patch('pwd.getpwuid')
-    @patch('os.getuid')
-    @patch('os.environ')
-    @patch('os.getlogin')
-    @patch('os.chown')
-    @patch('os.chmod')
-    @patch('os.stat')
-    @patch('os.path.exists')
-    @patch('os.close')
-    def test_module_utils_basic_ansible_module_atomic_move(
-            self,
-            _os_close,
-            _os_path_exists,
-            _os_stat,
-            _os_chmod,
-            _os_chown,
-            _os_getlogin,
-            _os_environ,
-            _os_getuid,
-            _pwd_getpwuid,
-            _os_rename,
-            _shutil_copy2,
-            _shutil_move,
-            _shutil_copyfileobj,
-            _os_umask,
-            _tempfile_mkstemp):
+@pytest.fixture
+def atomic_am(am, mocker):
+    am.selinux_enabled = mocker.MagicMock()
+    am.selinux_context = mocker.MagicMock()
+    am.selinux_default_context = mocker.MagicMock()
+    am.set_context_if_different = mocker.MagicMock()
 
-        from ansible.module_utils import basic
-        basic._ANSIBLE_ARGS = None
+    yield am
 
-        am = basic.AnsibleModule(
-            argument_spec=dict(),
-        )
 
-        environ = dict()
-        _os_environ.__getitem__ = environ.__getitem__
-        _os_environ.__setitem__ = environ.__setitem__
+@pytest.fixture
+def atomic_mocks(mocker):
+    environ = dict()
+    mocks = {
+        'chmod': mocker.patch('os.chmod'),
+        'chown': mocker.patch('os.chown'),
+        'close': mocker.patch('os.close'),
+        'environ': mocker.patch('os.environ', environ),
+        'getlogin': mocker.patch('os.getlogin'),
+        'getuid': mocker.patch('os.getuid'),
+        'path_exists': mocker.patch('os.path.exists'),
+        'rename': mocker.patch('os.rename'),
+        'stat': mocker.patch('os.stat'),
+        'umask': mocker.patch('os.umask'),
+        'getpwuid': mocker.patch('pwd.getpwuid'),
+        'copy2': mocker.patch('shutil.copy2'),
+        'copyfileobj': mocker.patch('shutil.copyfileobj'),
+        'move': mocker.patch('shutil.move'),
+        'mkstemp': mocker.patch('tempfile.mkstemp'),
+    }
 
-        am.selinux_enabled = MagicMock()
-        am.selinux_context = MagicMock()
-        am.selinux_default_context = MagicMock()
-        am.set_context_if_different = MagicMock()
+    mocks['getlogin'].return_value = 'root'
+    mocks['getuid'].return_value = 0
+    mocks['getpwuid'].return_value = ('root', '', 0, 0, '', '', '')
+    mocks['umask'].side_effect = [18, 0]
+    mocks['rename'].return_value = None
 
-        # test destination does not exist, no selinux, login name = 'root',
-        # no environment, os.rename() succeeds
-        _os_path_exists.side_effect = [False, False]
-        _os_getlogin.return_value = 'root'
-        _os_getuid.return_value = 0
-        _pwd_getpwuid.return_value = ('root', '', 0, 0, '', '', '')
-        _os_rename.return_value = None
-        _os_umask.side_effect = [18, 0]
-        am.selinux_enabled.return_value = False
-        _os_chmod.reset_mock()
-        _os_chown.reset_mock()
-        am.set_context_if_different.reset_mock()
-        am.atomic_move('/path/to/src', '/path/to/dest')
-        _os_rename.assert_called_with(b'/path/to/src', b'/path/to/dest')
-        self.assertEqual(_os_chmod.call_args_list, [call(b'/path/to/dest', basic.DEFAULT_PERM & ~18)])
+    yield mocks
 
-        # same as above, except selinux_enabled
-        _os_path_exists.side_effect = [False, False]
-        _os_getlogin.return_value = 'root'
-        _os_getuid.return_value = 0
-        _pwd_getpwuid.return_value = ('root', '', 0, 0, '', '', '')
-        _os_rename.return_value = None
-        _os_umask.side_effect = [18, 0]
-        mock_context = MagicMock()
-        am.selinux_default_context.return_value = mock_context
-        am.selinux_enabled.return_value = True
-        _os_chmod.reset_mock()
-        _os_chown.reset_mock()
-        am.set_context_if_different.reset_mock()
-        am.selinux_default_context.reset_mock()
-        am.atomic_move('/path/to/src', '/path/to/dest')
-        _os_rename.assert_called_with(b'/path/to/src', b'/path/to/dest')
-        self.assertEqual(_os_chmod.call_args_list, [call(b'/path/to/dest', basic.DEFAULT_PERM & ~18)])
-        self.assertEqual(am.selinux_default_context.call_args_list, [call('/path/to/dest')])
-        self.assertEqual(am.set_context_if_different.call_args_list, [call('/path/to/dest', mock_context, False)])
 
-        # now with dest present, no selinux, also raise OSError when using
-        # os.getlogin() to test corner case with no tty
-        _os_path_exists.side_effect = [True, True]
-        _os_getlogin.side_effect = OSError()
-        _os_getuid.return_value = 0
-        _pwd_getpwuid.return_value = ('root', '', 0, 0, '', '', '')
-        _os_rename.return_value = None
-        _os_umask.side_effect = [18, 0]
-        environ['LOGNAME'] = 'root'
-        stat1 = MagicMock()
-        stat1.st_mode = 0o0644
-        stat1.st_uid = 0
-        stat1.st_gid = 0
-        _os_stat.side_effect = [stat1, ]
-        am.selinux_enabled.return_value = False
-        _os_chmod.reset_mock()
-        _os_chown.reset_mock()
-        am.set_context_if_different.reset_mock()
-        am.atomic_move('/path/to/src', '/path/to/dest')
-        _os_rename.assert_called_with(b'/path/to/src', b'/path/to/dest')
+@pytest.fixture
+def fake_stat(mocker):
+    stat1 = mocker.MagicMock()
+    stat1.st_mode = 0o0644
+    stat1.st_uid = 0
+    stat1.st_gid = 0
+    yield stat1
 
-        # dest missing, selinux enabled
-        _os_path_exists.side_effect = [True, True]
-        _os_getlogin.return_value = 'root'
-        _os_getuid.return_value = 0
-        _pwd_getpwuid.return_value = ('root', '', 0, 0, '', '', '')
-        _os_rename.return_value = None
-        _os_umask.side_effect = [18, 0]
-        stat1 = MagicMock()
-        stat1.st_mode = 0o0644
-        stat1.st_uid = 0
-        stat1.st_gid = 0
-        _os_stat.side_effect = [stat1, ]
-        mock_context = MagicMock()
-        am.selinux_context.return_value = mock_context
-        am.selinux_enabled.return_value = True
-        _os_chmod.reset_mock()
-        _os_chown.reset_mock()
-        am.set_context_if_different.reset_mock()
-        am.selinux_default_context.reset_mock()
-        am.atomic_move('/path/to/src', '/path/to/dest')
-        _os_rename.assert_called_with(b'/path/to/src', b'/path/to/dest')
-        self.assertEqual(am.selinux_context.call_args_list, [call('/path/to/dest')])
-        self.assertEqual(am.set_context_if_different.call_args_list, [call('/path/to/dest', mock_context, False)])
 
-        # now testing with exceptions raised
-        # have os.stat raise OSError which is not EPERM
-        _os_stat.side_effect = OSError()
-        _os_path_exists.side_effect = [True, True]
-        _os_getlogin.return_value = 'root'
-        _os_getuid.return_value = 0
-        _pwd_getpwuid.return_value = ('root', '', 0, 0, '', '', '')
-        _os_rename.return_value = None
-        _os_umask.side_effect = [18, 0]
-        self.assertRaises(OSError, am.atomic_move, '/path/to/src', '/path/to/dest')
+@pytest.mark.parametrize('stdin, selinux', product([{}], (True, False)), indirect=['stdin'])
+def test_new_file(atomic_am, atomic_mocks, mocker, selinux):
+    # test destination does not exist, login name = 'root', no environment, os.rename() succeeds
+    mock_context = atomic_am.selinux_default_context.return_value
+    atomic_mocks['path_exists'].return_value = False
+    atomic_am.selinux_enabled.return_value = selinux
 
-        # and now have os.stat return EPERM, which should not fail
-        _os_stat.side_effect = OSError(errno.EPERM, 'testing os stat with EPERM')
-        _os_path_exists.side_effect = [True, True]
-        _os_getlogin.return_value = 'root'
-        _os_getuid.return_value = 0
-        _pwd_getpwuid.return_value = ('root', '', 0, 0, '', '', '')
-        _os_rename.return_value = None
-        _os_umask.side_effect = [18, 0]
-        # FIXME: we don't assert anything here yet
-        am.atomic_move('/path/to/src', '/path/to/dest')
+    atomic_am.atomic_move('/path/to/src', '/path/to/dest')
 
-        # now we test os.rename() raising errors...
-        # first we test with a bad errno to verify it bombs out
-        _os_path_exists.side_effect = [False, False]
-        _os_getlogin.return_value = 'root'
-        _os_getuid.return_value = 0
-        _pwd_getpwuid.return_value = ('root', '', 0, 0, '', '', '')
-        _os_umask.side_effect = [18, 0]
-        _os_rename.side_effect = OSError(errno.EIO, 'failing with EIO')
-        self.assertRaises(SystemExit, am.atomic_move, '/path/to/src', '/path/to/dest')
+    atomic_mocks['rename'].assert_called_with(b'/path/to/src', b'/path/to/dest')
+    assert atomic_mocks['chmod'].call_args_list == [mocker.call(b'/path/to/dest', basic.DEFAULT_PERM & ~18)]
 
-        # next we test with EPERM so it continues to the alternate code for moving
-        # test with mkstemp raising an error first
-        _os_path_exists.side_effect = [False, False]
-        _os_getlogin.return_value = 'root'
-        _os_getuid.return_value = 0
-        _os_close.return_value = None
-        _pwd_getpwuid.return_value = ('root', '', 0, 0, '', '', '')
-        _os_umask.side_effect = [18, 0]
-        _os_rename.side_effect = [OSError(errno.EPERM, 'failing with EPERM'), None]
-        _tempfile_mkstemp.return_value = None
-        _tempfile_mkstemp.side_effect = OSError()
-        am.selinux_enabled.return_value = False
-        self.assertRaises(SystemExit, am.atomic_move, '/path/to/src', '/path/to/dest')
+    if selinux:
+        assert atomic_am.selinux_default_context.call_args_list == [mocker.call('/path/to/dest')]
+        assert atomic_am.set_context_if_different.call_args_list == [mocker.call('/path/to/dest', mock_context, False)]
+    else:
+        assert not atomic_am.selinux_default_context.called
+        assert not atomic_am.set_context_if_different.called
 
-        # then test with it creating a temp file
-        _os_path_exists.side_effect = [False, False, False]
-        _os_getlogin.return_value = 'root'
-        _os_getuid.return_value = 0
-        _pwd_getpwuid.return_value = ('root', '', 0, 0, '', '', '')
-        _os_umask.side_effect = [18, 0]
-        _os_rename.side_effect = [OSError(errno.EPERM, 'failing with EPERM'), None]
-        mock_stat1 = MagicMock()
-        mock_stat2 = MagicMock()
-        mock_stat3 = MagicMock()
-        _os_stat.return_value = [mock_stat1, mock_stat2, mock_stat3]
-        _os_stat.side_effect = None
-        _tempfile_mkstemp.return_value = (None, '/path/to/tempfile')
-        _tempfile_mkstemp.side_effect = None
-        am.selinux_enabled.return_value = False
-        # FIXME: we don't assert anything here yet
-        am.atomic_move('/path/to/src', '/path/to/dest')
 
-        # same as above, but with selinux enabled
-        _os_path_exists.side_effect = [False, False, False]
-        _os_getlogin.return_value = 'root'
-        _os_getuid.return_value = 0
-        _pwd_getpwuid.return_value = ('root', '', 0, 0, '', '', '')
-        _os_umask.side_effect = [18, 0]
-        _os_rename.side_effect = [OSError(errno.EPERM, 'failing with EPERM'), None]
-        _tempfile_mkstemp.return_value = (None, None)
-        mock_context = MagicMock()
-        am.selinux_default_context.return_value = mock_context
-        am.selinux_enabled.return_value = True
-        am.atomic_move('/path/to/src', '/path/to/dest')
+@pytest.mark.parametrize('stdin, selinux', product([{}], (True, False)), indirect=['stdin'])
+def test_existing_file(atomic_am, atomic_mocks, fake_stat, mocker, selinux):
+    # Test destination already present
+    mock_context = atomic_am.selinux_context.return_value
+    atomic_mocks['stat'].return_value = fake_stat
+    atomic_mocks['path_exists'].return_value = True
+    atomic_am.selinux_enabled.return_value = selinux
+
+    atomic_am.atomic_move('/path/to/src', '/path/to/dest')
+
+    atomic_mocks['rename'].assert_called_with(b'/path/to/src', b'/path/to/dest')
+    assert atomic_mocks['chmod'].call_args_list == [mocker.call(b'/path/to/src', basic.DEFAULT_PERM & ~18)]
+
+    if selinux:
+        assert atomic_am.set_context_if_different.call_args_list == [mocker.call('/path/to/dest', mock_context, False)]
+        assert atomic_am.selinux_context.call_args_list == [mocker.call('/path/to/dest')]
+    else:
+        assert not atomic_am.selinux_default_context.called
+        assert not atomic_am.set_context_if_different.called
+
+
+@pytest.mark.parametrize('stdin', [{}], indirect=['stdin'])
+def test_no_tty_fallback(atomic_am, atomic_mocks, fake_stat, mocker):
+    """Raise OSError when using getlogin() to simulate no tty cornercase"""
+    mock_context = atomic_am.selinux_context.return_value
+    atomic_mocks['stat'].return_value = fake_stat
+    atomic_mocks['path_exists'].return_value = True
+    atomic_am.selinux_enabled.return_value = True
+    atomic_mocks['getlogin'].side_effect = OSError()
+    atomic_mocks['environ']['LOGNAME'] = 'root'
+
+    atomic_am.atomic_move('/path/to/src', '/path/to/dest')
+
+    atomic_mocks['rename'].assert_called_with(b'/path/to/src', b'/path/to/dest')
+    assert atomic_mocks['chmod'].call_args_list == [mocker.call(b'/path/to/src', basic.DEFAULT_PERM & ~18)]
+
+    assert atomic_am.set_context_if_different.call_args_list == [mocker.call('/path/to/dest', mock_context, False)]
+    assert atomic_am.selinux_context.call_args_list == [mocker.call('/path/to/dest')]
+
+
+@pytest.mark.parametrize('stdin', [{}], indirect=['stdin'])
+def test_existing_file_stat_failure(atomic_am, atomic_mocks, mocker):
+    """Failure to stat an existing file in order to copy permissions propogates the error (unless EPERM)"""
+    atomic_mocks['stat'].side_effect = OSError()
+    atomic_mocks['path_exists'].return_value = True
+
+    with pytest.raises(OSError):
+        atomic_am.atomic_move('/path/to/src', '/path/to/dest')
+
+
+@pytest.mark.parametrize('stdin', [{}], indirect=['stdin'])
+def test_existing_file_stat_perms_failure(atomic_am, atomic_mocks, mocker):
+    """Failure to stat an existing file to copy the permissions due to permissions passes fine"""
+    # and now have os.stat return EPERM, which should not fail
+    mock_context = atomic_am.selinux_context.return_value
+    atomic_mocks['stat'].side_effect = OSError(errno.EPERM, 'testing os stat with EPERM')
+    atomic_mocks['path_exists'].return_value = True
+    atomic_am.selinux_enabled.return_value = True
+
+    atomic_am.atomic_move('/path/to/src', '/path/to/dest')
+
+    atomic_mocks['rename'].assert_called_with(b'/path/to/src', b'/path/to/dest')
+    # FIXME: Should atomic_move() set a default permission value when it cannot retrieve the
+    # existing file's permissions?  (Right now it's up to the calling code.
+    # assert atomic_mocks['chmod'].call_args_list == [mocker.call(b'/path/to/src', basic.DEFAULT_PERM & ~18)]
+    assert atomic_am.set_context_if_different.call_args_list == [mocker.call('/path/to/dest', mock_context, False)]
+    assert atomic_am.selinux_context.call_args_list == [mocker.call('/path/to/dest')]
+
+
+@pytest.mark.parametrize('stdin', [{}], indirect=['stdin'])
+def test_rename_failure(atomic_am, atomic_mocks, mocker, capfd):
+    """Test os.rename fails with EIO, causing it to bail out"""
+    atomic_mocks['path_exists'].side_effect = [False, False]
+    atomic_mocks['rename'].side_effect = OSError(errno.EIO, 'failing with EIO')
+
+    with pytest.raises(SystemExit):
+        atomic_am.atomic_move('/path/to/src', '/path/to/dest')
+
+    out, err = capfd.readouterr()
+    results = json.loads(out)
+
+    assert 'Could not replace file' in results['msg']
+    assert 'failing with EIO' in results['msg']
+    assert results['failed']
+
+
+@pytest.mark.parametrize('stdin', [{}], indirect=['stdin'])
+def test_rename_perms_fail_temp_creation_fails(atomic_am, atomic_mocks, mocker, capfd):
+    """Test os.rename fails with EPERM working but failure in mkstemp"""
+    atomic_mocks['path_exists'].return_value = False
+    atomic_mocks['close'].return_value = None
+    atomic_mocks['rename'].side_effect = [OSError(errno.EPERM, 'failing with EPERM'), None]
+    atomic_mocks['mkstemp'].return_value = None
+    atomic_mocks['mkstemp'].side_effect = OSError()
+    atomic_am.selinux_enabled.return_value = False
+
+    with pytest.raises(SystemExit):
+        atomic_am.atomic_move('/path/to/src', '/path/to/dest')
+
+    out, err = capfd.readouterr()
+    results = json.loads(out)
+
+    assert 'is not writable by the current user' in results['msg']
+    assert results['failed']
+
+
+@pytest.mark.parametrize('stdin, selinux', product([{}], (True, False)), indirect=['stdin'])
+def test_rename_perms_fail_temp_succeeds(atomic_am, atomic_mocks, fake_stat, mocker, selinux):
+    """Test os.rename raising an error but fallback to using mkstemp works"""
+    mock_context = atomic_am.selinux_default_context.return_value
+    atomic_mocks['path_exists'].return_value = False
+    atomic_mocks['rename'].side_effect = [OSError(errno.EPERM, 'failing with EPERM'), None]
+    atomic_mocks['stat'].return_value = [fake_stat, fake_stat, fake_stat]
+    atomic_mocks['stat'].side_effect = None
+    atomic_mocks['mkstemp'].return_value = (None, '/path/to/tempfile')
+    atomic_mocks['mkstemp'].side_effect = None
+    atomic_am.selinux_enabled.return_value = selinux
+
+    atomic_am.atomic_move('/path/to/src', '/path/to/dest')
+    assert atomic_mocks['rename'].call_args_list == [mocker.call(b'/path/to/src', b'/path/to/dest'),
+                                                     mocker.call(b'/path/to/tempfile', b'/path/to/dest')]
+    assert atomic_mocks['chmod'].call_args_list == [mocker.call(b'/path/to/dest', basic.DEFAULT_PERM & ~18)]
+
+    if selinux:
+        assert atomic_am.selinux_default_context.call_args_list == [mocker.call('/path/to/dest')]
+        assert atomic_am.set_context_if_different.call_args_list == [mocker.call(b'/path/to/tempfile', mock_context, False),
+                                                                     mocker.call('/path/to/dest', mock_context, False)]
+    else:
+        assert not atomic_am.selinux_default_context.called
+        assert not atomic_am.set_context_if_different.called
