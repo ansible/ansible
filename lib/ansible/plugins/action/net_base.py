63b94285963da4ccfe6e330b0cd0db6ad7e1594c
Nathaniel Case <ncase@redhat.com>
2017-12-04 08:57:30 -0500

    Fix module_utils path for network

--- lib/ansible/plugins/action/net_base.py
+++ lib/ansible/plugins/action/net_base.py
@@ -48,9 +48,9 @@ class ActionModule(ActionBase):
         # option is that there isn't a need to push provider into the module
         # since the connection is started in the action handler.
         f, p, d = find_module('ansible')
-        f2, p2, d2 = find_module('module_utils', [p])
-        f3, p3, d3 = find_module(play_context.network_os, [p2])
-        module = load_module('ansible.module_utils.{0}.{1}'.format(play_context.network_os, play_context.network_os), f3, p3, d3)
+        for package in ('module_utils', 'network', play_context.network_os):
+            f, p, d = find_module(package, [p])
+        module = load_module('ansible.module_utils.network.{0}.{0}'.format(play_context.network_os), f, p, d)
 
         if play_context.connection == 'local':
 
