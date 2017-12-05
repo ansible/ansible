abae2dceecb5553fd003070b70b258810d1ad195
Nathaniel Case <ncase@redhat.com>
2017-12-05 11:35:11 -0500

    network_cli needs network_os

--- test/runner/lib/manage_ci.py
+++ test/runner/lib/manage_ci.py
@@ -82,6 +82,7 @@ class ManageNetworkCI(object):
             'ansible_port=%s' % self.core_ci.connection.port,
             'ansible_connection=%s' % connection_type,
             'ansible_ssh_private_key_file=%s' % self.core_ci.ssh_key.key,
+            'ansible_network_os=%s' % self.core_ci.platform,
         ]
 
         name = '%s-%s' % (self.core_ci.platform, self.core_ci.version.replace('.', '-'))
