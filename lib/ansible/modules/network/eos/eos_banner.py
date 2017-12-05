6f00fbea8fcbc5326556489405fb9f6cb07378bd
Nathaniel Case <this.is@nathanielca.se>
2017-12-05 09:57:40 -0500

    Fix authorize in net agnostic modules (#33549)
    * This should fix authorize for net_* modules
    
    * transport: none means network_cli in eos
    
    For now anyway

--- lib/ansible/modules/network/eos/eos_banner.py
+++ lib/ansible/modules/network/eos/eos_banner.py
@@ -130,9 +130,7 @@ def map_config_to_obj(module):
     output = run_commands(module, ['show banner %s' % module.params['banner']])
     obj = {'banner': module.params['banner'], 'state': 'absent'}
     if output:
-        if module.params['transport'] == 'cli':
-            obj['text'] = output[0]
-        else:
+        if module.params['transport'] == 'eapi':
             # On EAPI we need to extract the banner text from dict key
             # 'loginBanner'
             if module.params['banner'] == 'login':
@@ -141,6 +139,8 @@ def map_config_to_obj(module):
                 banner_response_key = 'motd'
             if isinstance(output[0], dict) and banner_response_key in output[0].keys():
                 obj['text'] = output[0]
+        else:
+            obj['text'] = output[0]
         obj['state'] = 'present'
     return obj
 
