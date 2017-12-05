80de24be5a468106f55833f8aded972a8cb91101
zikalino <zikalino@microsoft.com>
2017-12-05 09:44:18 +0800

    replaced has_key by in

--- lib/ansible/modules/cloud/azure/azure_rm_sqldatabase.py
+++ lib/ansible/modules/cloud/azure/azure_rm_sqldatabase.py
@@ -882,7 +882,7 @@ class AzureRMDatabases(AzureRMModuleBase):
         except CloudError:
             self.fail('resource group {0} not found'.format(self.resource_group))
 
-        if not self.parameters.has_key("location"):
+        if not ("location" in self.parameters):
             self.parameters["location"] = resource_group.location
 
         old_response = self.get_database()
