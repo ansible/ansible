28a8eb192b70e4eb36b479d6921d16bb1826a960
Samer Deeb <samerd@mellanox.com>
2017-12-06 06:22:15 -0800

    Adding network_cli support in mlnxos (and removing provider) (#33511)
    Signed-off-by: Samer Deeb <samerd@mellanox.com>

--- lib/ansible/plugins/terminal/mlnxos.py
+++ lib/ansible/plugins/terminal/mlnxos.py
@@ -46,7 +46,7 @@ class TerminalModule(TerminalBase):
         except AnsibleConnectionFailure:
             raise AnsibleConnectionFailure('unable to set terminal parameters')
 
-    def on_authorize(self, passwd=None):
+    def on_become(self, passwd=None):
         if self._get_prompt().endswith(b'#'):
             return
 
@@ -66,7 +66,7 @@ class TerminalModule(TerminalBase):
             raise AnsibleConnectionFailure(
                 'unable to elevate privilege to enable mode')
 
-    def on_deauthorize(self):
+    def on_unbecome(self):
         prompt = self._get_prompt()
         if prompt is None:
             # if prompt is None most likely the terminal is hung up at a prompt
