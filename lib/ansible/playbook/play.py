6eb548e74dac237df4f304d5f7e3be106261a6ce
Brian Coca <brian.coca+git@gmail.com>
2017-12-06 18:55:20 -0500

    caller to preprocess must be None aware
    also removed unused copy of method
    
    fixes #33650

--- lib/ansible/playbook/play.py
+++ lib/ansible/playbook/play.py
@@ -196,22 +196,23 @@ class Play(Base, Taggable, Become):
     def _load_vars_prompt(self, attr, ds):
         new_ds = preprocess_vars(ds)
         vars_prompts = []
-        for prompt_data in new_ds:
-            if 'name' not in prompt_data:
-                display.deprecated("Using the 'short form' for vars_prompt has been deprecated", version="2.7")
-                for vname, prompt in prompt_data.items():
-                    vars_prompts.append(dict(
-                        name=vname,
-                        prompt=prompt,
-                        default=None,
-                        private=None,
-                        confirm=None,
-                        encrypt=None,
-                        salt_size=None,
-                        salt=None,
-                    ))
-            else:
-                vars_prompts.append(prompt_data)
+        if new_ds is not None:
+            for prompt_data in new_ds:
+                if 'name' not in prompt_data:
+                    display.deprecated("Using the 'short form' for vars_prompt has been deprecated", version="2.7")
+                    for vname, prompt in prompt_data.items():
+                        vars_prompts.append(dict(
+                            name=vname,
+                            prompt=prompt,
+                            default=None,
+                            private=None,
+                            confirm=None,
+                            encrypt=None,
+                            salt_size=None,
+                            salt=None,
+                        ))
+                else:
+                    vars_prompts.append(prompt_data)
         return vars_prompts
 
     def _compile_roles(self):
