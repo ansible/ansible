180cfb160700ef946dc5c33d0541be5aa6b912ff
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-12-06 15:18:26 +0000

    Updates copyright for Consul ACL module.

--- lib/ansible/modules/clustering/consul_acl.py
+++ lib/ansible/modules/clustering/consul_acl.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #
 # (c) 2015, Steve Gargan <steve.gargan@gmail.com>
+# Copyright (c) 2017 Genome Research Ltd.
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
