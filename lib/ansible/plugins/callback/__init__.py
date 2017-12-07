96590f20b17c157c08db87e8c3d428b2a372f141
Brian Coca <bcoca@ansible.com>
2017-12-07 11:28:42 -0500

    report correct file for callback

--- lib/ansible/plugins/callback/__init__.py
+++ lib/ansible/plugins/callback/__init__.py
@@ -70,7 +70,7 @@ class CallbackBase(AnsiblePlugin):
             name = getattr(self, 'CALLBACK_NAME', 'unnamed')
             ctype = getattr(self, 'CALLBACK_TYPE', 'old')
             version = getattr(self, 'CALLBACK_VERSION', '1.0')
-            self._display.vvvv('Loading callback plugin %s of type %s, v%s from %s' % (name, ctype, version, __file__))
+            self._display.vvvv('Loading callback plugin %s of type %s, v%s from %s' % (name, ctype, version, sys.modules[self.__module__].__file__))
 
         self.disabled = False
 
