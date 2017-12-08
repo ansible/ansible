1ce02cebec87f249912c3ab8b69514197d543a1a
asand3r <asand3r@gmail.com>
2017-12-08 09:33:36 +0300

    Correct commas as separator.

--- lib/ansible/modules/network/ios/ios_logging.py
+++ lib/ansible/modules/network/ios/ios_logging.py
@@ -375,34 +375,34 @@ def main():
     remove_default_spec(aggregate_spec)
 
     argument_spec = dict(
-        aggregate=dict(type='list',elements='dict',options=aggregate_spec),
+        aggregate=dict(type='list', elements='dict', options=aggregate_spec),
     )
 
     argument_spec.update(element_spec)
     argument_spec.update(ios_argument_spec)
 
-    required_if = [('dest','host',['name'])]
+    required_if = [('dest', 'host', ['name'])]
 
     module = AnsibleModule(argument_spec=argument_spec,
                            required_if=required_if,
                            supports_check_mode=True)
 
     warnings = list()
-    check_args(module,warnings)
+    check_args(module, warnings)
 
     result = {'changed': False}
     if warnings:
         result['warnings'] = warnings
 
-    want = map_params_to_obj(module,required_if=required_if)
+    want = map_params_to_obj(module, required_if=required_if)
     have = map_config_to_obj(module)
 
-    commands = map_obj_to_commands((want,have),module)
+    commands = map_obj_to_commands((want, have), module)
     result['commands'] = commands
 
     if commands:
         if not module.check_mode:
-            load_config(module,commands)
+            load_config(module, commands)
         result['changed'] = True
 
     module.exit_json(**result)
