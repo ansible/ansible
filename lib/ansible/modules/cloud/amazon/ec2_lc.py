f7c53d8050b18b1307fe77556b7a88f34fccd4a9
Sloane Hertel <shertel@redhat.com>
2017-12-08 15:41:25 -0500

    [cloud] document return values for ec2_lc (#32592)

--- lib/ansible/modules/cloud/amazon/ec2_lc.py
+++ lib/ansible/modules/cloud/amazon/ec2_lc.py
@@ -174,6 +174,198 @@ EXAMPLES = '''
 
 '''
 
+RETURN = '''
+arn:
+  description: The Amazon Resource Name of the launch configuration.
+  returned: when I(state=present)
+  type: string
+  sample: arn:aws:autoscaling:us-east-1:148830907657:launchConfiguration:888d9b58-d93a-40c4-90cf-759197a2621a:launchConfigurationName/launch_config_name
+changed:
+  description: Whether the state of the launch configuration has changed.
+  returned: always
+  type: bool
+  sample: false
+created_time:
+  description: The creation date and time for the launch configuration.
+  returned: when I(state=present)
+  type: string
+  sample: '2017-11-03 23:46:44.841000'
+image_id:
+  description: The ID of the Amazon Machine Image used by the launch configuration.
+  returned: when I(state=present)
+  type: string
+  sample: ami-9be6f38c
+instance_type:
+  description: The instance type for the instances.
+  returned: when I(state=present)
+  type: string
+  sample: t1.micro
+name:
+  description: The name of the launch configuration.
+  returned: when I(state=present)
+  type: string
+  sample: launch_config_name
+result:
+  description: The specification details for the launch configuration.
+  returned: when I(state=present)
+  type: complex
+  contains:
+    PlacementTenancy:
+      description: The tenancy of the instances, either default or dedicated.
+      returned: when I(state=present)
+      type: string
+      sample: default
+    associate_public_ip_address:
+      description: (EC2-VPC) Indicates whether to assign a public IP address to each instance.
+      returned: when I(state=present)
+      type: NoneType
+      sample: null
+    block_device_mappings:
+      description: A block device mapping, which specifies the block devices.
+      returned: when I(state=present)
+      type: complex
+      contains:
+        device_name:
+          description: The device name exposed to the EC2 instance (for example, /dev/sdh or xvdh).
+          returned: when I(state=present)
+          type: string
+          sample: /dev/sda1
+        ebs:
+          description: The information about the Amazon EBS volume.
+          returned: when I(state=present)
+          type: complex
+          contains:
+            snapshot_id:
+              description: The ID of the snapshot.
+              returned: when I(state=present)
+              type: NoneType
+              sample: null
+            volume_size:
+              description: The volume size, in GiB.
+              returned: when I(state=present)
+              type: string
+              sample: '100'
+        virtual_name:
+          description: The name of the virtual device (for example, ephemeral0).
+          returned: when I(state=present)
+          type: NoneType
+          sample: null
+    classic_link_vpc_id:
+      description: The ID of a ClassicLink-enabled VPC to link your EC2-Classic instances to.
+      returned: when I(state=present)
+      type: NoneType
+      sample: null
+    classic_link_vpc_security_groups:
+      description: The IDs of one or more security groups for the VPC specified in ClassicLinkVPCId.
+      returned: when I(state=present)
+      type: list
+      sample: []
+    created_time:
+      description: The creation date and time for the launch configuration.
+      returned: when I(state=present)
+      type: string
+      sample: '2017-11-03 23:46:44.841000'
+    delete_on_termination:
+      description: Indicates whether the volume is deleted on instance termination.
+      returned: when I(state=present)
+      type: bool
+      sample: true
+    ebs_optimized:
+      description: Indicates whether the instance is optimized for EBS I/O (true) or not (false).
+      returned: when I(state=present)
+      type: bool
+      sample: false
+    image_id:
+      description: The ID of the Amazon Machine Image used by the launch configuration.
+      returned: when I(state=present)
+      type: string
+      sample: ami-9be6f38c
+    instance_monitoring:
+      description: Indicates whether instances in this group are launched with detailed (true) or basic (false) monitoring.
+      returned: when I(state=present)
+      type: bool
+      sample: true
+    instance_profile_name:
+      description: The name or Amazon Resource Name (ARN) of the instance profile associated with the IAM role for the instance.
+      returned: when I(state=present)
+      type: string
+      sample: null
+    instance_type:
+      description: The instance type for the instances.
+      returned: when I(state=present)
+      type: string
+      sample: t1.micro
+    iops:
+      description: The number of I/O operations per second (IOPS) to provision for the volume.
+      returned: when I(state=present)
+      type: NoneType
+      sample: null
+    kernel_id:
+      description: The ID of the kernel associated with the AMI.
+      returned: when I(state=present)
+      type: string
+      sample: ''
+    key_name:
+      description: The name of the key pair.
+      returned: when I(state=present)
+      type: string
+      sample: testkey
+    launch_configuration_arn:
+      description: The Amazon Resource Name (ARN) of the launch configuration.
+      returned: when I(state=present)
+      type: string
+      sample: arn:aws:autoscaling:us-east-1:148830907657:launchConfiguration:888d9b58-d93a-40c4-90cf-759197a2621a:launchConfigurationName/launch_config_name
+    member:
+      description: ""
+      returned: when I(state=present)
+      type: string
+      sample: "\n      "
+    name:
+      description: The name of the launch configuration.
+      returned: when I(state=present)
+      type: string
+      sample: launch_config_name
+    ramdisk_id:
+      description: The ID of the RAM disk associated with the AMI.
+      returned: when I(state=present)
+      type: string
+      sample: ''
+    security_groups:
+      description: The security groups to associate with the instances.
+      returned: when I(state=present)
+      type: list
+      sample:
+      - sg-5e27db2f
+    spot_price:
+      description: The price to bid when launching Spot Instances.
+      returned: when I(state=present)
+      type: NoneType
+      sample: null
+    use_block_device_types:
+      description: Indicates whether to suppress a device mapping.
+      returned: when I(state=present)
+      type: bool
+      sample: false
+    user_data:
+      description: The user data available to the instances.
+      returned: when I(state=present)
+      type: string
+      sample: ''
+    volume_type:
+      description: The volume type (one of standard, io1, gp2).
+      returned: when I(state=present)
+      type: NoneType
+      sample: null
+security_groups:
+  description: The security groups to associate with the instances.
+  returned: when I(state=present)
+  type: list
+  sample:
+  - sg-5e27db2f
+
+'''
+
+
 import traceback
 from ansible.module_utils.ec2 import (get_aws_connection_info, ec2_argument_spec, ec2_connect, camel_dict_to_snake_dict, get_ec2_security_group_ids_from_names,
                                       boto3_conn, snake_dict_to_camel_dict, HAS_BOTO3)
