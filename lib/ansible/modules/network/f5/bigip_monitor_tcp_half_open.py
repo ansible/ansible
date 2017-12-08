1caa19372ec26d9d74304f5194747981973872c1
Tim Rupp <caphrim007@gmail.com>
2017-12-08 15:39:04 -0800

    Adds token cleanup to some f5 modules (#33728)
    The tokens can build up over time and if too many accumulate, it
    prevents you from logging in. This adds cleanup

--- lib/ansible/modules/network/f5/bigip_monitor_tcp_half_open.py
+++ lib/ansible/modules/network/f5/bigip_monitor_tcp_half_open.py
@@ -543,16 +543,26 @@ class ArgumentSpec(object):
         self.f5_product_name = 'bigip'
 
 
-def main():
+def cleanup_tokens(client):
     try:
-        spec = ArgumentSpec()
-
-        client = AnsibleF5Client(
-            argument_spec=spec.argument_spec,
-            supports_check_mode=spec.supports_check_mode,
-            f5_product_name=spec.f5_product_name
+        resource = client.api.shared.authz.tokens_s.token.load(
+            name=client.api.icrs.token
         )
+        resource.delete()
+    except Exception:
+        pass
 
+
+def main():
+    spec = ArgumentSpec()
+
+    client = AnsibleF5Client(
+        argument_spec=spec.argument_spec,
+        supports_check_mode=spec.supports_check_mode,
+        f5_product_name=spec.f5_product_name
+    )
+
+    try:
         if not HAS_F5SDK:
             raise F5ModuleError("The python f5-sdk module is required")
 
@@ -561,8 +571,10 @@ def main():
 
         mm = ModuleManager(client)
         results = mm.exec_module()
+        cleanup_tokens(client)
         client.module.exit_json(**results)
     except F5ModuleError as e:
+        cleanup_tokens(client)
         client.module.fail_json(msg=str(e))
 
 
