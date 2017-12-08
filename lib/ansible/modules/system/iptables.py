55ff37f604b7515e5ac95a2c4ac98fba61a2ccfc
André Nähring <andre.naehring@credativ.de>
2017-12-08 15:24:55 +0100

    Enhancing include of rule_num

--- lib/ansible/modules/system/iptables.py
+++ lib/ansible/modules/system/iptables.py
@@ -453,7 +453,7 @@ def push_arguments(iptables_path, action, params, make_rule=True):
     cmd = [iptables_path]
     cmd.extend(['-t', params['table']])
     cmd.extend([action, params['chain']])
-    if action == '-I':
+    if action == '-I' and params['rule_num']:
         cmd.extend([params['rule_num']])
     if make_rule:
         cmd.extend(construct_rule(params))
