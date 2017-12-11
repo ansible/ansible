6bebbe6ce671ae08b12a9ef438f95389447c1587
Etienne Perot <EtiennePerot@users.noreply.github.com>
2017-12-10 17:32:40 -0800

    docker_image: Fix up 'changed' event in force mode
    This is the same as https://github.com/ansible/ansible/pull/19235 except it applies to all image-building modes (building the image locally, loading the image from an archive, or pulling the image), rather than only when pulling the image.

--- lib/ansible/modules/cloud/docker/docker_image.py
+++ lib/ansible/modules/cloud/docker/docker_image.py
@@ -348,9 +348,9 @@ class ImageManager(DockerBaseClass):
                 self.results['actions'].append('Pulled image %s:%s' % (self.name, self.tag))
                 self.results['changed'] = True
                 if not self.check_mode:
-                    self.results['image'], already_latest = self.client.pull_image(self.name, tag=self.tag)
-                    if already_latest:
-                        self.results['changed'] = False
+                    self.results['image'], _ = self.client.pull_image(self.name, tag=self.tag)
+            if not self.check_mode and image and image['Id'] == self.results['image']['Id']:
+                self.results['changed'] = False
 
         if self.archive_path:
             self.archive_image(self.name, self.tag)
