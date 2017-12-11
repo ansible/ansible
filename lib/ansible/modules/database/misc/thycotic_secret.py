6087e5fc4d9a070d4beede5ccda833dbd27c846a
Patrick Thomison <pthomiso@redhat.com>
2017-12-11 10:07:48 -0800

    Removing todo's

--- lib/ansible/modules/database/misc/thycotic_secret.py
+++ lib/ansible/modules/database/misc/thycotic_secret.py
@@ -4,9 +4,6 @@
 # Copyright: (c) 2017, Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-# TODO: Write quick & dirty test playbook
-# TODO: Review & Submit PR
-
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
