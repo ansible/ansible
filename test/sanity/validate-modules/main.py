64cf4541852ae4f697948cef3d6d8ff8d4f31f1b
Will Thames <will.thames@xvt.com.au>
2017-12-13 12:00:44 +1000

    Add documentation of 108 error
    Create label in developing modules documentation so that
    the validation page can point to it

--- test/sanity/validate-modules/main.py
+++ test/sanity/validate-modules/main.py
@@ -377,20 +377,20 @@ class ModuleValidator(Validator):
 
     def _check_gpl3_header(self):
         header = '\n'.join(self.text.split('\n')[:20])
-        if ('GNU General Public License' not in header and
+        if ('GNU General Public License' not in header or
                 'version 3' not in header):
             self.reporter.error(
                 path=self.object_path,
                 code=105,
                 msg='GPLv3 license header not found in the first 20 lines of the module'
             )
-        if self._is_new_module():
-            if len([line for line in self.text.split('\n')
+        elif self._is_new_module():
+            if len([line for line in header
                     if 'GNU General Public License' in line]) > 1:
                 self.reporter.error(
                     path=self.object_path,
                     code=108,
-                    msg='GPLv3 simple license header not found: '
+                    msg='Found old style GPLv3 license header: '
                         'https://docs.ansible.com/ansible/devel/dev_guide/developing_modules_documenting.html#copyright'
                 )
 
