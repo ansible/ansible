92daccd549d6169704592274b1b32db482f70f9e
Claes Nästén <cnasten@cisco.com>
2017-12-15 12:29:04 +0100

    nso_action for Cisco NSO (#32781)
    nso_action module for execution actions/RPCs in NSO.

--- test/units/modules/network/nso/nso_module.py
+++ test/units/modules/network/nso/nso_module.py
@@ -20,11 +20,19 @@ from __future__ import (absolute_import, division, print_function)
 import os
 import json
 
-from units.modules.utils import AnsibleExitJson, AnsibleFailJson, ModuleTestCase
+from ansible.compat.tests import unittest
+from ansible.compat.tests.mock import patch
+from ansible.module_utils import basic
+from ansible.module_utils._text import to_bytes
 
 
 fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
 fixture_data = {}
+
+
+def set_module_args(args):
+    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
+    basic._ANSIBLE_ARGS = to_bytes(args)
 
 
 def load_fixture(name):
@@ -72,7 +80,15 @@ def mock_call(calls, url, data=None, headers=None, method=None):
     return result
 
 
-class TestNsoModule(ModuleTestCase):
+class AnsibleExitJson(Exception):
+    pass
+
+
+class AnsibleFailJson(Exception):
+    pass
+
+
+class TestNsoModule(unittest.TestCase):
 
     def execute_module(self, failed=False, changed=False, **kwargs):
         if failed:
@@ -88,16 +104,27 @@ class TestNsoModule(ModuleTestCase):
         return result
 
     def failed(self):
-        with self.assertRaises(AnsibleFailJson) as exc:
-            self.module.main()
+        def fail_json(*args, **kwargs):
+            kwargs['failed'] = True
+            raise AnsibleFailJson(kwargs)
+
+        with patch.object(basic.AnsibleModule, 'fail_json', fail_json):
+            with self.assertRaises(AnsibleFailJson) as exc:
+                self.module.main()
 
         result = exc.exception.args[0]
         self.assertTrue(result['failed'], result)
         return result
 
     def changed(self, changed=False):
-        with self.assertRaises(AnsibleExitJson) as exc:
-            self.module.main()
+        def exit_json(*args, **kwargs):
+            if 'changed' not in kwargs:
+                kwargs['changed'] = False
+            raise AnsibleExitJson(kwargs)
+
+        with patch.object(basic.AnsibleModule, 'exit_json', exit_json):
+            with self.assertRaises(AnsibleExitJson) as exc:
+                self.module.main()
 
         result = exc.exception.args[0]
         self.assertEqual(result['changed'], changed, result)
