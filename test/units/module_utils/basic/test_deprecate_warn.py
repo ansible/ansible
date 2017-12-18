99a03567158be5cc8536b5e6ec184b21f4675c37
Toshio Kuratomi <a.badger@gmail.com>
2017-12-18 10:17:13 -0800

    Split basic units (#33510)
    Split the one monolithic test for basic.py into several files
    
    * Split test_basic.py along categories.
      This is preliminary to get a handle on things.  Eventually we may want
      to further split it so each file is only testing a single function.
    * Cleanup unused imports from splitting test_basic.py
    * Port atomic_move test to pytest.
      Working on getting rid of need to maintain procenv
    * Split a test of symbolic_mode_to_octal to follow unittest best practices
      Each test should only invoke the function under test once
    * Port test_argument_spec to pytest.
    * Fix suboptions failure

--- test/units/module_utils/basic/test_deprecate_warn.py
+++ test/units/module_utils/basic/test_deprecate_warn.py
@@ -4,15 +4,8 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 import json
-import sys
-from collections import MutableMapping
-from io import BytesIO
 
 import pytest
-
-import ansible.module_utils.basic
-from ansible.module_utils.six import PY3, string_types
-from ansible.module_utils._text import to_bytes
 
 
 @pytest.mark.parametrize('stdin', [{}], indirect=['stdin'])
