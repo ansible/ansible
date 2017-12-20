02f19b41342a2dba4a79662deea647eb0869a8cb
Brian Coca <brian.coca+git@gmail.com>
2017-12-20 16:10:56 -0500

    now names match

--- lib/ansible/plugins/action/__init__.py
+++ lib/ansible/plugins/action/__init__.py
@@ -613,7 +613,7 @@ class ActionBase(with_metaclass(ABCMeta, object)):
         module_args['_ansible_shell_executable'] = self._play_context.executable
 
         # let command know it should avoid specific warnings
-        module_args['_ansible_command_warn'] = C.COMMAND_WARNINGS
+        module_args['_ansible_command_warnings'] = C.COMMAND_WARNINGS
 
     def _update_connection_options(self, options, variables=None):
         ''' ensures connections have the appropriate information '''
