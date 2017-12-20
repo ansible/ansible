9f98628eab926d3eabab3a4d1c40b1af54db168c
Trishna Guha <trishnaguha17@gmail.com>
2017-12-20 10:41:10 +0530

    Handle nxos platform diffs with cli conf implementation (#34020)
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- test/units/modules/network/nxos/test_nxos_feature.py
+++ test/units/modules/network/nxos/test_nxos_feature.py
@@ -38,10 +38,15 @@ class TestNxosFeatureModule(TestNxosModule):
         self.mock_load_config = patch('ansible.modules.network.nxos.nxos_feature.load_config')
         self.load_config = self.mock_load_config.start()
 
+        self.mock_get_capabilities = patch('ansible.modules.network.nxos.nxos_feature.get_capabilities')
+        self.get_capabilities = self.mock_get_capabilities.start()
+        self.get_capabilities.return_value = {'network_api': 'cliconf'}
+
     def tearDown(self):
         super(TestNxosFeatureModule, self).tearDown()
         self.mock_run_commands.stop()
         self.mock_load_config.stop()
+        self.mock_get_capabilities.stop()
 
     def load_fixtures(self, commands=None, device=''):
         def load_from_file(*args, **kwargs):
