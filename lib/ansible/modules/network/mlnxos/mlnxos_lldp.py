df0adaff66ecdec585eaffca33b3544c002f46eb
Samer Deeb <samerd@mellanox.com>
2017-12-21 20:18:00 +0000

    Fix header and metadata
    Signed-off-by: Samer Deeb <samerd@mellanox.com>

--- lib/ansible/modules/network/mlnxos/mlnxos_lldp.py
+++ lib/ansible/modules/network/mlnxos/mlnxos_lldp.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 
-# (c) 2017, Ansible by Red Hat, inc
+# Copyright: Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
@@ -9,7 +8,7 @@ __metaclass__ = type
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
-                    'supported_by': 'network'}
+                    'supported_by': 'community'}
 
 DOCUMENTATION = """
 ---
