646e232944f6470c2f32e97ba2d0b6ee7cd0c63b
“dsoper2” <dsoper@cisco.com>
2017-12-21 11:53:27 -0600

    option description spelled out (aliased to descr) removed docs for list of dictionaries syntax follow ACI indent style state absent only requires dn name match (not full prop match)

--- lib/ansible/modules/remote_management/ucs/ucs_wwn_pool.py
+++ lib/ansible/modules/remote_management/ucs/ucs_wwn_pool.py
@@ -28,15 +28,17 @@ options:
   name:
     description:
     - Name of the WWNN/WWPN
-    - If specifying a single WWNN/WWPN, name is required
+    required: yes
   purpose:
     description:
     - Specify node (WWNN) or port (WWPN)
-    - If specifying a single WWNN/WWPN, purpose is required
+    - Optional if state absent
     choices: [node, port]
-  descr:
+    required: yes
+  descrption:
     description:
     - Description for the WWNN/WWPN pool
+    aliases: [ descr ]
   order:
     description:
     - Assignment order
@@ -46,18 +48,6 @@ options:
     description: First WWNN/WWPN address in the WWN addresses block
   last_addr:
     description: Last WWNN/WWPN address in the WWN addresses block
-  wwn_list:
-    description:
-    - List of WWNNs/WWPN pools
-    - wwn_list allows multiple resource updates with a single UCSM connection
-    - Each list item contains the following properties
-    - name (Name of the WWNN/WWPN pool (required))
-    - purporse (node or port (required))
-    - descr (Description for the WWNN/WWPN pool)
-    - order (Assignment order which is default or sequential)
-    - first_addr (First WWNN/WWPN address in the WWN addresses block)
-    - last_addr (Last WWNN/WWPN address in the WWN addresses block)
-    - Either wwn_list or name/purpose is required
   org_dn:
     description:
     - Org dn (distinguished name)
@@ -71,36 +61,38 @@ version_added: '2.5'
 '''
 
 EXAMPLES = r'''
-- name: Configure multiple WWNN/WWPN pools
-  ucs_wwn_pool:
-    hostname: 172.16.143.150
-    username: admin
-    password: password
-    wwn_list:
-      - name: WWNN-Pool
-        purpose: node
-        first_addr: 20:00:00:25:B5:48:00:00
-        last_addr: 20:00:00:25:B5:48:00:0F
-      - name: WWPN-Pool-A
-        purpose: port
-        order: sequential
-        first_addr: 20:00:00:25:B5:48:0A:00
-        last_addr: 20:00:00:25:B5:48:0A:0F
-      - name: WWPN-Pool-B
-        purpose: port
-        order: sequential
-        first_addr: 20:00:00:25:B5:48:0B:00
-        last_addr: 20:00:00:25:B5:48:0B:0F
-
-- name: Configure single WWNN pool
+- name: Configure WWNN/WWPN pools
   ucs_wwn_pool:
     hostname: 172.16.143.150
     username: admin
     password: password
     name: WWNN-Pool
     purpose: node
-    first_addr: 20:00:00:25:B5:58:00:00
-    last_addr: 20:00:00:25:B5:58:00:0F
+    first_addr: 20:00:00:25:B5:48:00:00
+    last_addr: 20:00:00:25:B5:48:00:0F
+  ucs_wwn_pool:
+    hostname: 172.16.143.150
+    username: admin
+    password: password
+    name: WWPN-Pool-A
+    purpose: port
+    order: sequential
+    first_addr: 20:00:00:25:B5:48:0A:00
+    last_addr: 20:00:00:25:B5:48:0A:0F
+
+- name: Remove WWNN/WWPN pools
+  ucs_wwn_pool:
+    hostname: 172.16.143.150
+    username: admin
+    password: password
+    name: WWNN-Pool
+    state: absent
+  ucs_wwn_pool:
+    hostname: 172.16.143.150
+    username: admin
+    password: password
+    name: WWPN-Pool-A
+    state: absent
 '''
 
 RETURN = r'''
@@ -113,26 +105,32 @@ from ansible.module_utils.remote_management.ucs import UCSModule, ucs_argument_s
 
 def main():
     argument_spec = ucs_argument_spec
-    argument_spec.update(wwn_list=dict(type='list'),
-                         org_dn=dict(type='str', default='org-root'),
-                         name=dict(type='str'),
-                         purpose=dict(type='str', choices=['node', 'port']),
-                         descr=dict(type='str'),
-                         order=dict(type='str', default='default', choices=['default', 'sequential']),
-                         first_addr=dict(type='str'),
-                         last_addr=dict(type='str'),
-                         state=dict(type='str', default='present', choices=['present', 'absent']))
-    module = AnsibleModule(argument_spec,
-                           supports_check_mode=True,
-                           required_one_of=[
-                               ['wwn_list', 'name']
-                           ],
-                           mutually_exclusive=[
-                               ['wwn_list', 'name']
-                           ],
-                           required_together=[
-                               ['name', 'purpose']
-                           ])
+    argument_spec.update(
+        org_dn=dict(type='str', default='org-root'),
+        name=dict(type='str'),
+        purpose=dict(type='str', choices=['node', 'port']),
+        descr=dict(type='str'),
+        order=dict(type='str', default='default', choices=['default', 'sequential']),
+        first_addr=dict(type='str'),
+        last_addr=dict(type='str'),
+        state=dict(type='str', default='present', choices=['present', 'absent']),
+        wwn_list=dict(type='list'),
+    )
+
+    # Note that use of wwn_list is an experimental feature which allows multiple resource updates with a single UCSM connection.
+    # Support for wwn_list may change or be removed once persistent UCS connections are supported.
+    # Either wwn_list or name is required (user can specify either a list or single resource).
+
+    module = AnsibleModule(
+        argument_spec,
+        supports_check_mode=True,
+        required_one_of=[
+            ['wwn_list', 'name']
+        ],
+        mutually_exclusive=[
+            ['wwn_list', 'name']
+        ],
+    )
     ucs = UCSModule(module)
 
     err = False
@@ -142,6 +140,9 @@ def main():
 
     changed = False
     try:
+        # Only documented use is a single resource, but to also support experimental
+        # feature allowing multiple updates all params are converted to a wwn_list below.
+
         if module.params['wwn_list']:
             # directly use the list (single resource and list are mutually exclusive
             wwn_list = module.params['wwn_list']
@@ -149,53 +150,61 @@ def main():
             # single resource specified, create list from the current params
             wwn_list = [module.params]
         for wwn in wwn_list:
-            exists = False
+            mo_exists = False
+            props_match = False
             # set default params.  Done here to set values for lists which can't be done in the argument_spec
             if not wwn.get('descr'):
                 wwn['descr'] = ''
             if not wwn.get('order'):
                 wwn['order'] = 'default'
-            # append purpose param with suffix used by UCSM
-            purpose_param = wwn['purpose'] + '-wwn-assignment'
             # dn is <org_dn>/wwn-pool-<name> for WWNN or WWPN
             dn = module.params['org_dn'] + '/wwn-pool-' + wwn['name']
 
             mo = ucs.login_handle.query_dn(dn)
             if mo:
-                # check top-level mo props
-                kwargs = {}
-                kwargs['assignment_order'] = wwn['order']
-                kwargs['descr'] = wwn['descr']
-                kwargs['purpose'] = purpose_param
-                if (mo.check_prop_match(**kwargs)):
-                    # top-level props match, check next level mo/props
-                    if 'last_addr' in wwn and 'first_addr' in wwn:
-                        block_dn = dn + '/block-' + wwn['first_addr'].upper() + '-' + wwn['last_addr'].upper()
-                        mo_1 = ucs.login_handle.query_dn(block_dn)
-                        if mo_1:
-                            exists = True
-                    else:
-                        exists = True
+                mo_exists = True
 
             if module.params['state'] == 'absent':
-                if exists:
+                if mo_exists:
                     if not module.check_mode:
                         ucs.login_handle.remove_mo(mo)
                         ucs.login_handle.commit()
                     changed = True
             else:
-                if not exists:
+                # append purpose param with suffix used by UCSM
+                purpose_param = wwn['purpose'] + '-wwn-assignment'
+                if mo_exists:
+                    # check top-level mo props
+                    kwargs = {}
+                    kwargs['assignment_order'] = wwn['order']
+                    kwargs['descr'] = wwn['descr']
+                    kwargs['purpose'] = purpose_param
+                    if (mo.check_prop_match(**kwargs)):
+                        # top-level props match, check next level mo/props
+                        if 'last_addr' in wwn and 'first_addr' in wwn:
+                            block_dn = dn + '/block-' + wwn['first_addr'].upper() + '-' + wwn['last_addr'].upper()
+                            mo_1 = ucs.login_handle.query_dn(block_dn)
+                            if mo_1:
+                                props_match = True
+                        else:
+                            props_match = True
+
+                if not props_match:
                     if not module.check_mode:
                         # create if mo does not already exist
-                        mo = FcpoolInitiators(parent_mo_or_dn=module.params['org_dn'],
-                                              name=wwn['name'],
-                                              descr=wwn['descr'],
-                                              assignment_order=wwn['order'],
-                                              purpose=purpose_param)
+                        mo = FcpoolInitiators(
+                            parent_mo_or_dn=module.params['org_dn'],
+                            name=wwn['name'],
+                            descr=wwn['descr'],
+                            assignment_order=wwn['order'],
+                            purpose=purpose_param,
+                        )
                         if 'last_addr' in wwn and 'first_addr' in wwn:
-                            mo_1 = FcpoolBlock(parent_mo_or_dn=mo,
-                                               to=wwn['last_addr'],
-                                               r_from=wwn['first_addr'])
+                            mo_1 = FcpoolBlock(
+                                parent_mo_or_dn=mo,
+                                to=wwn['last_addr'],
+                                r_from=wwn['first_addr'],
+                            )
 
                         ucs.login_handle.add_mo(mo, True)
                         ucs.login_handle.commit()
