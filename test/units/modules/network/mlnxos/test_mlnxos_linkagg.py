c461690769fad264b511406f30236d3701397bce
Samer Deeb <samerd@mellanox.com>
2017-12-28 11:51:54 -0800

    Remove unnecessary  method in unit-test
    remove _execute_module and use execute_module from base class

--- test/units/modules/network/mlnxos/test_mlnxos_linkagg.py
+++ test/units/modules/network/mlnxos/test_mlnxos_linkagg.py
@@ -32,23 +32,6 @@ class TestMlnxosLinkaggModule(TestMlnxosModule):
         self.mock_get_config.stop()
         self.mock_load_config.stop()
 
-    def _execute_module(self, failed=False, changed=False, commands=None, sort=True):
-        if failed:
-            result = self.failed()
-            self.assertTrue(result['failed'], result)
-        else:
-            result = self.changed(changed)
-            self.assertEqual(result['changed'], changed, result)
-
-        if commands is not None:
-            commands_res = result.get('commands')
-            if sort:
-                self.assertEqual(sorted(commands), sorted(commands_res), commands_res)
-            else:
-                self.assertEqual(commands, commands_res, commands_res)
-
-        return result
-
     def load_fixture(self, config_file):
         self.get_config.return_value = load_fixture(config_file)
         self.load_config.return_value = None
@@ -65,13 +48,13 @@ class TestMlnxosLinkaggModule(TestMlnxosModule):
         set_module_args(dict(name='Po22', state='present',
                              members=['Eth1/7']))
         self.load_port_channel_fixture()
-        self._execute_module(changed=False)
+        self.execute_module(changed=False)
 
     def test_port_channel_remove(self):
         set_module_args(dict(name='Po22', state='absent'))
         self.load_port_channel_fixture()
         commands = ['no interface port-channel 22']
-        self._execute_module(changed=True, commands=commands)
+        self.execute_module(changed=True, commands=commands)
 
     def test_port_channel_add(self):
         set_module_args(dict(name='Po23', state='present',
@@ -79,32 +62,32 @@ class TestMlnxosLinkaggModule(TestMlnxosModule):
         self.load_port_channel_fixture()
         commands = ['interface port-channel 23', 'exit',
                     'interface ethernet 1/8 channel-group 23 mode on']
-        self._execute_module(changed=True, commands=commands)
+        self.execute_module(changed=True, commands=commands)
 
     def test_port_channel_add_member(self):
         set_module_args(dict(name='Po22', state='present',
                              members=['Eth1/7', 'Eth1/8']))
         self.load_port_channel_fixture()
         commands = ['interface ethernet 1/8 channel-group 22 mode on']
-        self._execute_module(changed=True, commands=commands)
+        self.execute_module(changed=True, commands=commands)
 
     def test_port_channel_remove_member(self):
         set_module_args(dict(name='Po22', state='present'))
         self.load_port_channel_fixture()
         commands = ['interface ethernet 1/7 no channel-group']
-        self._execute_module(changed=True, commands=commands)
+        self.execute_module(changed=True, commands=commands)
 
     def test_mlag_port_channel_no_change(self):
         set_module_args(dict(name='Mpo33', state='present',
                              members=['Eth1/8']))
         self.load_mlag_port_channel_fixture()
-        self._execute_module(changed=False)
+        self.execute_module(changed=False)
 
     def test_mlag_port_channel_remove(self):
         set_module_args(dict(name='Mpo33', state='absent'))
         self.load_mlag_port_channel_fixture()
         commands = ['no interface mlag-port-channel 33']
-        self._execute_module(changed=True, commands=commands)
+        self.execute_module(changed=True, commands=commands)
 
     def test_mlag_port_channel_add(self):
         set_module_args(dict(name='Mpo34', state='present',
@@ -112,17 +95,17 @@ class TestMlnxosLinkaggModule(TestMlnxosModule):
         self.load_mlag_port_channel_fixture()
         commands = ['interface mlag-port-channel 34', 'exit',
                     'interface ethernet 1/9 mlag-channel-group 34 mode on']
-        self._execute_module(changed=True, commands=commands)
+        self.execute_module(changed=True, commands=commands)
 
     def test_mlag_port_channel_add_member(self):
         set_module_args(dict(name='Mpo33', state='present',
                              members=['Eth1/8', 'Eth1/9']))
         self.load_mlag_port_channel_fixture()
         commands = ['interface ethernet 1/9 mlag-channel-group 33 mode on']
-        self._execute_module(changed=True, commands=commands)
+        self.execute_module(changed=True, commands=commands)
 
     def test_mlag_port_channel_remove_member(self):
         set_module_args(dict(name='Mpo33', state='present'))
         self.load_mlag_port_channel_fixture()
         commands = ['interface ethernet 1/8 no mlag-channel-group']
-        self._execute_module(changed=True, commands=commands)
+        self.execute_module(changed=True, commands=commands)
