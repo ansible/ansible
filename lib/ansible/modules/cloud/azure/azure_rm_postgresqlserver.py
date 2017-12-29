ef9b4e8c79f7925746f031492eac3e25bd781eaf
Ubuntu <zim@zims-ubuntu-3.hzdriptz0rguzhq25yp3koz2nd.hx.internal.cloudapp.net>
2017-12-29 01:33:34 +0000

    updates to azure_rm_postgresqlserver

--- lib/ansible/modules/cloud/azure/azure_rm_postgresqlserver.py
+++ lib/ansible/modules/cloud/azure/azure_rm_postgresqlserver.py
@@ -200,7 +200,7 @@ class AzureRMServers(AzureRMModuleBase):
     def exec_module(self, **kwargs):
         """Main module execution method"""
 
-        for key in list(self.module_arg_spec.keys()) + ['tags']:
+        for key in list(self.module_arg_spec.keys()):
             if hasattr(self, key):
                 setattr(self, key, kwargs[key])
             elif kwargs[key] is not None:
