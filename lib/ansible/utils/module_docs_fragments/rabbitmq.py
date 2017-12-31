7cc8fd0b21a9c4bf0a3f27d9ef92bebce988f28a
Wayne Pascoe <wayne@penguinpowered.org>
2017-12-31 11:26:50 +0000

    pep8 fix - add blank line

--- lib/ansible/utils/module_docs_fragments/rabbitmq.py
+++ lib/ansible/utils/module_docs_fragments/rabbitmq.py
@@ -1,6 +1,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 # Copyright: (c) 2017, Ansible Project
 
+
 class ModuleDocFragment(object):
 
     # Documentation fragment for EXTRA CTL PATHS
