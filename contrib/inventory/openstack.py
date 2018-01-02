472d0d5e326e5e03e26f5099e735f519e59cc3e5
Fedele Mantuano <mantuano.fedele@gmail.com>
2018-01-02 17:03:44 +0100

    More stable explicit file close. (#34303)

--- contrib/inventory/openstack.py
+++ contrib/inventory/openstack.py
@@ -118,9 +118,11 @@ def get_host_groups(inventory, refresh=False, cloud=None):
     (cache_file, cache_expiration_time) = get_cache_settings(cloud)
     if is_cache_stale(cache_file, cache_expiration_time, refresh=refresh):
         groups = to_json(get_host_groups_from_cloud(inventory))
-        open(cache_file, 'w').write(groups)
+        with open(cache_file, 'w') as f:
+            f.write(groups)
     else:
-        groups = open(cache_file, 'r').read()
+        with open(cache_file, 'r') as f:
+            groups = f.read()
     return groups
 
 
