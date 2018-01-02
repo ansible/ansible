48abfa6266908f0008965137406d09d3a8c6cb7e
Ganesh B. Nalawade <ganesh634@gmail.com>
2018-01-02 18:28:43 +0530

    Minor change

--- lib/ansible/modules/network/ios/ios_lldp.py
+++ lib/ansible/modules/network/ios/ios_lldp.py
@@ -60,7 +60,7 @@ def has_lldp(module):
     output = run_commands(module, ['show lldp'])
 
     is_lldp_enable = False
-    if len(output) > 0 and "LLDP is not enable" not in output[0]:
+    if len(output) > 0 and "LLDP is not enabled" not in output[0]:
         is_lldp_enable = True
 
     return is_lldp_enable
