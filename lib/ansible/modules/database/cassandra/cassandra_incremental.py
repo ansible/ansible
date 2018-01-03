56309f2df32265921786af77d5d51200beafb079
Rhys Campbell <rhys.james.campbell@googlemail.com>
2018-01-03 20:50:22 +0100

    First commit. Not working yet

---
+++ lib/ansible/modules/database/cassandra/cassandra_incremental.py
@@ -0,0 +1,237 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# 2017 Rhys Campbell <rhys.james.campbell@googlemail.com>
+# Based on the group module by  Stephen Fromm <sfromm@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+
+DOCUMENTATION = '''
+---
+module: cassandra_backup
+author: "Rhys Campbell (rhys.james.campbell@googlemail.com)"
+version_added: "2.1.1"
+short_description: Enable and disable the incremental backup feature
+requirements: [ nodetool ]
+description:
+    - Enable and disable incremental backup feature via playbook
+options:
+    host:
+        description:
+          - Hostname / ip address of Cassandra node instance
+        required: false
+        default: localhost
+    port:
+      description:
+        - Remote jmx agent port number
+      required: False
+      default: ???? TODO
+   password:
+     description:
+       - Remote jmx agent password
+     required: false
+     default: null
+   passwordFile:
+     description:
+       - Path to the JMX password file
+     required: false
+     default: null
+   username:
+     description:
+       - Remote jmx agent username
+     required: false
+     default: null
+   incremental:
+     description:
+       - Enables and disables the incremental backup feature
+     required: true
+     choices [ true, false ]
+   nodetool_path:
+     description:
+       - The path to nodetool
+     required: false
+     default: null
+
+########################
+# LOCAL MODULE TESTING #
+########################
+
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_73.jdk/Contents/Home"
+export PATH=$JAVA_HOME/bin:$PATH
+
+1. Basic test
+#############
+cat << EOF > /tmp/args.json
+{
+    "ANSIBLE_MODULE_ARGS": {
+	    "nodetool_path": "/Users/rhys1/dse/bin",
+	    "incremental": false
+    }
+}
+EOF
+
+python ./lib/ansible/modules/database/cassandra/cassandra_incremental.py /tmp/args.json
+
+cat << EOF > /tmp/args.json
+{
+    "ANSIBLE_MODULE_ARGS": {
+	    "nodetool_path": "/Users/rhys1/dse/bin",
+	    "incremental": true
+    }
+}
+EOF
+
+python ./lib/ansible/modules/database/cassandra/cassandra_incremental.py /tmp/args.json
+
+'''
+
+EXAMPLES = '''
+# Enable the incremental backup feature
+- cassandra_snapshot:
+    username: admin
+    password: secret
+    incremental: true
+# Disable the incremental backup feature
+- cassandra_snapshot:
+    username: admin
+    password: secret
+    incremental: false
+
+'''
+
+import grp
+
+from ansible.module_utils.basic import AnsibleModule, load_platform_subclass
+
+
+class NodeToolCmd(object):
+    """
+    This is a generic NodeToolCmd class for building nodetool commands
+    """
+
+    def __init__(self, module, additional_command):
+        self.module                 = module
+        self.host                   = module.params['host']
+        self.port                   = module.params['port']
+        self.password               = module.params['password']
+        self.passwordFile           = module.params['passwordFile']
+        self.username               = module.params['username']
+        self.nodetool_path          = module.params['nodetool_path']
+        self.additional_command     = additional_command
+        self.debug                  = module.params['debug']
+
+    def execute_command(self, cmd):
+        return self.module.run_command(cmd)
+
+    def nodetool_cmd(self, additional_command):
+        if self.nodetool_path is not None and not self.nodetool_path.endswith('/'):
+            self.nodetool_path += '/'
+        cmd = "{0}nodetool --host {1} --port {2}".format(self.nodetool_path,
+                                                         self.host,
+                                                         self.port)
+        if self.username is not None:
+            cmd += " --username {0}".format(self.username)
+            if self.passwordFile is not None:
+                cmd += " --passwordFile {0}".format(self.passwordFile)
+            else:
+                cmd += " --password '{0}'".format(self.password)
+        # The thing we want nodetool to execute
+        cmd += " {0}".format(self.additional_command)
+        if self.debug:
+            print(cmd)
+        return self.execute_command(cmd)
+
+class NodeToolIncrementalBackup(NodeToolCmd):
+
+    """
+    Inherits from the NodeToolCmd class. Adds the following methods;
+
+        - check_incremental_enabled
+        - enable_incremental
+        - disable_incremental
+    """
+
+    def check_incremental_enabled(self):
+        o = self.nodetool_cmd("statusbackup")
+        if "not running" in str(o):
+            return False
+        elif "running" in str(o):
+            return True
+        else:
+            raise Exception(o)
+
+    def enable_incremental(self):
+        return self.nodetool_cmd("enablebackup")
+
+    def disable_incremental(self):
+        return self.nodetool_cmd("disablebackup")
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            host=dict(type='str', default='localhost'),
+            port=dict(type='int', default=7199),
+            password=dict(type='str', no_log=True),
+            passwordFile=dict(type='str', no_log=True),
+            username=dict(type='str'),
+            incremental=dict(choices=[True, False], type='bool', required=True),
+            nodetool_path=dict(default=None, required=False),
+            debug=dict(default=False, required=False),
+        ),
+        supports_check_mode=True
+    )
+    if module.params['incremental']:
+        module.params['additional_command'] = "enablebackup"
+    else:
+        module.params['additional_command'] = "disablebackup"
+
+    n = NodeToolIncrementalBackup(module, module.params['additional_command'])
+
+    rc = None
+    out = ''
+    err = ''
+    result = {}
+    #result['name'] = group.name
+    #result['state'] = group.state
+
+    if module.params['incremental'] == False:
+
+        if n.check_incremental_enabled():
+            if module.check_mode:
+                module.exit_json(changed=True)
+            (rc, out, err) = n.disable_incremental()
+            if rc != 0:
+                module.fail_json(name=module.params['additional_command'], msg=err)
+
+    elif module.params['incremental'] == True:
+
+        if not  n.check_incremental_enabled():
+            if module.check_mode:
+                module.exit_json(changed=True)
+            (rc, out, err) = n.enable_incremental()
+
+        if rc is not None and rc != 0:
+            module.fail_json(name=module.params['additional_command'], msg=err)
+
+    if rc is None:
+        result['changed'] = False
+    else:
+        result['changed'] = True
+    if out:
+        result['stdout'] = out
+    if err:
+        result['stderr'] = err
+
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
---
+++ lib/ansible/modules/database/cassandra/cassandra_snapshot.py
@@ -0,0 +1,518 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# 2017 Rhys Campbell <rhys.james.campbell@googlemail.com>
+# Based on the group module by  Stephen Fromm <sfromm@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+
+DOCUMENTATION = '''
+---
+module: cassandra_backup
+author: "Rhys Campbell (rhys.james.campbell@googlemail.com)"
+version_added: "2.1.1"
+short_description: Take a snapshot of all keyspaces, specified keyspaces or a snapshot of the specified table
+requirements: [ nodetool ]
+description:
+    - Execute snapshotsc on each node in a cassandra cluster
+    - Full support for all nodetool snapshot command options
+options:
+    host:
+        description:
+          - Hostname / ip address of Cassandra node instance
+        required: false
+        default: localhost
+    port:
+      description:
+        - Remote jmx agent port number
+      required: False
+      default: ???? TODO
+   password:
+     description:
+       - Remote jmx agent password
+     required: false
+     default: null
+   passwordFile:
+     description:
+       - Path to the JMX password file
+     required: false
+     default: null
+   username:
+     description:
+       - Remote jmx agent username
+     required: false
+     default: null
+   keyspaces:
+     description:
+       - List of keyspaces (databases) to snapshot
+       - Be aware of restrictions with columnFamily and ktList when setting this.
+     required: false
+     default: null
+   columnFamily:
+     description:
+       - The table name (you must specify one and only one keyspace for using this option)
+     required: false
+     default: null
+   ktList:
+     description:
+       - The list of Keyspace.table to take snapshot.(you must not specify only keyspace)
+     required: false
+     default: null
+   tag:
+     description:
+       - The name of the snapshot
+     required: false
+     default: null // TODO perhaps a datetime stamp? Or range of options? choices?
+   nodetool_path:
+     description:
+       - The path to nodetool
+     required: false
+     default: null
+'''
+
+EXAMPLES = '''
+# Example Cassandra snapshot command from Ansible Playbooks. Backs up all keyspaces
+- cassandra_snapshot:
+    username: admin
+    password: secret
+# snapshot a single keyspace
+- cassandra_snapshot:
+    username: admin
+    password: secret
+    keyspaces: mykeyspace
+# snapshot a single table in a keyspace
+- cassandra_snapshot:
+    username: admin
+    password: secret
+    keyspaces: mykeyspace
+    columnFamily: mytable
+# Enable the incremental backup feature
+- cassandra_snapshot:
+    username: admin
+    password: secret
+    incremental: true
+# Disable the incremental backup feature
+- cassandra_snapshot:
+    username: admin
+    password: secret
+    incremental: false
+
+'''
+
+import grp
+
+from ansible.module_utils.basic import AnsibleModule, load_platform_subclass
+
+
+class Group(object):
+    """
+    This is a generic Group manipulation class that is subclassed
+    based on platform.
+
+    A subclass may wish to override the following action methods:-
+      - group_del()
+      - group_add()
+      - group_mod()
+
+    All subclasses MUST define platform and distribution (which may be None).
+    """
+
+    platform = 'Generic'
+    distribution = None
+    GROUPFILE = '/etc/group'
+
+    def __new__(cls, *args, **kwargs):
+        return load_platform_subclass(Group, args, kwargs)
+
+    def __init__(self, module):
+        self.module     = module
+        self.state      = module.params['state']
+        self.name       = module.params['name']
+        self.gid        = module.params['gid']
+        self.system     = module.params['system']
+
+    def execute_command(self, cmd):
+        return self.module.run_command(cmd)
+
+    def group_del(self):
+        cmd = [self.module.get_bin_path('groupdel', True), self.name]
+        return self.execute_command(cmd)
+
+    def group_add(self, **kwargs):
+        cmd = [self.module.get_bin_path('groupadd', True)]
+        for key in kwargs:
+            if key == 'gid' and kwargs[key] is not None:
+                cmd.append('-g')
+                cmd.append(kwargs[key])
+            elif key == 'system' and kwargs[key] is True:
+                cmd.append('-r')
+        cmd.append(self.name)
+        return self.execute_command(cmd)
+
+    def group_mod(self, **kwargs):
+        cmd = [self.module.get_bin_path('groupmod', True)]
+        info = self.group_info()
+        for key in kwargs:
+            if key == 'gid':
+                if kwargs[key] is not None and info[2] != int(kwargs[key]):
+                    cmd.append('-g')
+                    cmd.append(kwargs[key])
+        if len(cmd) == 1:
+            return (None, '', '')
+        if self.module.check_mode:
+            return (0, '', '')
+        cmd.append(self.name)
+        return self.execute_command(cmd)
+
+    def group_exists(self):
+        try:
+            if grp.getgrnam(self.name):
+                return True
+        except KeyError:
+            return False
+
+    def group_info(self):
+        if not self.group_exists():
+            return False
+        try:
+            info = list(grp.getgrnam(self.name))
+        except KeyError:
+            return False
+        return info
+
+# ===========================================
+
+class SunOS(Group):
+    """
+    This is a SunOS Group manipulation class. Solaris doesn't have
+    the 'system' group concept.
+
+    This overrides the following methods from the generic class:-
+        - group_add()
+    """
+
+    platform = 'SunOS'
+    distribution = None
+    GROUPFILE = '/etc/group'
+
+    def group_add(self, **kwargs):
+        cmd = [self.module.get_bin_path('groupadd', True)]
+        for key in kwargs:
+            if key == 'gid' and kwargs[key] is not None:
+                cmd.append('-g')
+                cmd.append(kwargs[key])
+        cmd.append(self.name)
+        return self.execute_command(cmd)
+
+
+# ===========================================
+
+class AIX(Group):
+    """
+    This is a AIX Group manipulation class.
+
+    This overrides the following methods from the generic class:-
+      - group_del()
+      - group_add()
+      - group_mod()
+    """
+
+    platform = 'AIX'
+    distribution = None
+    GROUPFILE = '/etc/group'
+
+    def group_del(self):
+        cmd = [self.module.get_bin_path('rmgroup', True), self.name]
+        return self.execute_command(cmd)
+
+    def group_add(self, **kwargs):
+        cmd = [self.module.get_bin_path('mkgroup', True)]
+        for key in kwargs:
+            if key == 'gid' and kwargs[key] is not None:
+                cmd.append('id=' + kwargs[key])
+            elif key == 'system' and kwargs[key] is True:
+                cmd.append('-a')
+        cmd.append(self.name)
+        return self.execute_command(cmd)
+
+    def group_mod(self, **kwargs):
+        cmd = [self.module.get_bin_path('chgroup', True)]
+        info = self.group_info()
+        for key in kwargs:
+            if key == 'gid':
+                if kwargs[key] is not None and info[2] != int(kwargs[key]):
+                    cmd.append('id=' + kwargs[key])
+        if len(cmd) == 1:
+            return (None, '', '')
+        if self.module.check_mode:
+            return (0, '', '')
+        cmd.append(self.name)
+        return self.execute_command(cmd)
+
+# ===========================================
+
+class FreeBsdGroup(Group):
+    """
+    This is a FreeBSD Group manipulation class.
+
+    This overrides the following methods from the generic class:-
+      - group_del()
+      - group_add()
+      - group_mod()
+    """
+
+    platform = 'FreeBSD'
+    distribution = None
+    GROUPFILE = '/etc/group'
+
+    def group_del(self):
+        cmd = [self.module.get_bin_path('pw', True), 'groupdel', self.name]
+        return self.execute_command(cmd)
+
+    def group_add(self, **kwargs):
+        cmd = [self.module.get_bin_path('pw', True), 'groupadd', self.name]
+        if self.gid is not None:
+            cmd.append('-g')
+            cmd.append('%d' % int(self.gid))
+        return self.execute_command(cmd)
+
+    def group_mod(self, **kwargs):
+        cmd = [self.module.get_bin_path('pw', True), 'groupmod', self.name]
+        info = self.group_info()
+        cmd_len = len(cmd)
+        if self.gid is not None and int(self.gid) != info[2]:
+            cmd.append('-g')
+            cmd.append('%d' % int(self.gid))
+        # modify the group if cmd will do anything
+        if cmd_len != len(cmd):
+            if self.module.check_mode:
+                return (0, '', '')
+            return self.execute_command(cmd)
+        return (None, '', '')
+
+# ===========================================
+
+
+
+class DarwinGroup(Group):
+    """
+    This is a Mac OS X Darwin Group manipulation class.
+
+    This overrides the following methods from the generic class:-
+      - group_del()
+      - group_add()
+      - group_mod()
+
+    group manipulation are done using dseditgroup(1).
+    """
+
+    platform = 'Darwin'
+    distribution = None
+
+    def group_add(self, **kwargs):
+        cmd = [self.module.get_bin_path('dseditgroup', True)]
+        cmd += [ '-o', 'create' ]
+        if self.gid is not None:
+            cmd += [ '-i', self.gid ]
+        elif 'system' in kwargs and kwargs['system'] is True:
+            gid = self.get_lowest_available_system_gid()
+            if gid is not False:
+                self.gid = str(gid)
+                cmd += [ '-i', self.gid ]
+        cmd += [ '-L', self.name ]
+        (rc, out, err) = self.execute_command(cmd)
+        return (rc, out, err)
+
+    def group_del(self):
+        cmd = [self.module.get_bin_path('dseditgroup', True)]
+        cmd += [ '-o', 'delete' ]
+        cmd += [ '-L', self.name ]
+        (rc, out, err) = self.execute_command(cmd)
+        return (rc, out, err)
+
+    def group_mod(self, gid=None):
+        info = self.group_info()
+        if self.gid is not None and int(self.gid) != info[2]:
+            cmd = [self.module.get_bin_path('dseditgroup', True)]
+            cmd += [ '-o', 'edit' ]
+            if gid is not None:
+                cmd += [ '-i', gid ]
+            cmd += [ '-L', self.name ]
+            (rc, out, err) = self.execute_command(cmd)
+            return (rc, out, err)
+        return (None, '', '')
+
+    def get_lowest_available_system_gid(self):
+        # check for lowest available system gid (< 500)
+        try:
+            cmd = [self.module.get_bin_path('dscl', True)]
+            cmd += [ '/Local/Default', '-list', '/Groups', 'PrimaryGroupID']
+            (rc, out, err) = self.execute_command(cmd)
+            lines = out.splitlines()
+            highest = 0
+            for group_info in lines:
+                parts = group_info.split(' ')
+                if len(parts) > 1:
+                    gid = int(parts[-1])
+                    if gid > highest and gid < 500:
+                        highest = gid
+            if highest == 0 or highest == 499:
+                return False
+            return (highest + 1)
+        except:
+            return False
+
+class OpenBsdGroup(Group):
+    """
+    This is a OpenBSD Group manipulation class.
+
+    This overrides the following methods from the generic class:-
+      - group_del()
+      - group_add()
+      - group_mod()
+    """
+
+    platform = 'OpenBSD'
+    distribution = None
+    GROUPFILE = '/etc/group'
+
+    def group_del(self):
+        cmd = [self.module.get_bin_path('groupdel', True), self.name]
+        return self.execute_command(cmd)
+
+    def group_add(self, **kwargs):
+        cmd = [self.module.get_bin_path('groupadd', True)]
+        if self.gid is not None:
+            cmd.append('-g')
+            cmd.append('%d' % int(self.gid))
+        cmd.append(self.name)
+        return self.execute_command(cmd)
+
+    def group_mod(self, **kwargs):
+        cmd = [self.module.get_bin_path('groupmod', True)]
+        info = self.group_info()
+        if self.gid is not None and int(self.gid) != info[2]:
+            cmd.append('-g')
+            cmd.append('%d' % int(self.gid))
+        if len(cmd) == 1:
+            return (None, '', '')
+        if self.module.check_mode:
+            return (0, '', '')
+        cmd.append(self.name)
+        return self.execute_command(cmd)
+
+# ===========================================
+
+class NetBsdGroup(Group):
+    """
+    This is a NetBSD Group manipulation class.
+
+    This overrides the following methods from the generic class:-
+      - group_del()
+      - group_add()
+      - group_mod()
+    """
+
+    platform = 'NetBSD'
+    distribution = None
+    GROUPFILE = '/etc/group'
+
+    def group_del(self):
+        cmd = [self.module.get_bin_path('groupdel', True), self.name]
+        return self.execute_command(cmd)
+
+    def group_add(self, **kwargs):
+        cmd = [self.module.get_bin_path('groupadd', True)]
+        if self.gid is not None:
+            cmd.append('-g')
+            cmd.append('%d' % int(self.gid))
+        cmd.append(self.name)
+        return self.execute_command(cmd)
+
+    def group_mod(self, **kwargs):
+        cmd = [self.module.get_bin_path('groupmod', True)]
+        info = self.group_info()
+        if self.gid is not None and int(self.gid) != info[2]:
+            cmd.append('-g')
+            cmd.append('%d' % int(self.gid))
+        if len(cmd) == 1:
+            return (None, '', '')
+        if self.module.check_mode:
+            return (0, '', '')
+        cmd.append(self.name)
+        return self.execute_command(cmd)
+
+# ===========================================
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            state=dict(default='present', choices=['present', 'absent'], type='str'),
+            name=dict(required=True, type='str'),
+            gid=dict(default=None, type='str'),
+            system=dict(default=False, type='bool'),
+        ),
+        supports_check_mode=True
+    )
+
+    group = Group(module)
+
+    module.debug('Group instantiated - platform %s' % group.platform)
+    if group.distribution:
+        module.debug('Group instantiated - distribution %s' % group.distribution)
+
+    rc = None
+    out = ''
+    err = ''
+    result = {}
+    result['name'] = group.name
+    result['state'] = group.state
+
+    if group.state == 'absent':
+
+        if group.group_exists():
+            if module.check_mode:
+                module.exit_json(changed=True)
+            (rc, out, err) = group.group_del()
+            if rc != 0:
+                module.fail_json(name=group.name, msg=err)
+
+    elif group.state == 'present':
+
+        if not group.group_exists():
+            if module.check_mode:
+                module.exit_json(changed=True)
+            (rc, out, err) = group.group_add(gid=group.gid, system=group.system)
+        else:
+            (rc, out, err) = group.group_mod(gid=group.gid)
+
+        if rc is not None and rc != 0:
+            module.fail_json(name=group.name, msg=err)
+
+    if rc is None:
+        result['changed'] = False
+    else:
+        result['changed'] = True
+    if out:
+        result['stdout'] = out
+    if err:
+        result['stderr'] = err
+
+    if group.group_exists():
+        info = group.group_info()
+        result['system'] = group.system
+        result['gid'] = info[2]
+
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
