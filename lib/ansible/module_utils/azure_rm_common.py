96ced6b0bfe539e93dfa92f4524fa5c04539d252
Yuwei Zhou <yuwzho@microsoft.com>
2018-01-05 00:23:15 +0800

    upgrade the azure storage version (#34442)

--- lib/ansible/module_utils/azure_rm_common.py
+++ lib/ansible/module_utils/azure_rm_common.py
@@ -750,7 +750,7 @@ class AzureRMModuleBase(object):
         if not self._storage_client:
             self._storage_client = self.get_mgmt_svc_client(StorageManagementClient,
                                                             base_url=self._cloud_environment.endpoints.resource_manager,
-                                                            api_version='2017-06-01')
+                                                            api_version='2017-10-01')
         return self._storage_client
 
     @property
