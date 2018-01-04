9f8c56eb0699de7cbc7a8dc2f147f507c223d65f
Jason Vanderhoof <jvanderhoof@gmail.com>
2018-01-04 11:43:05 -0700

    Standardized output on `format` and insure it works for 2.6, 2.7, and 3.x.

--- lib/ansible/plugins/lookup/conjur_variable.py
+++ lib/ansible/plugins/lookup/conjur_variable.py
@@ -60,7 +60,7 @@ class LookupModule(LookupBase):
         )
         if not conf:
             raise AnsibleError('Conjur configuration should be in one of the following files: \'~/.conjurrc\', \'/etc/conjur.conf\'')
-        display.vvvv("configuration: {}".format(conf))
+        display.vvvv('configuration: {0}'.format(conf))
 
         # Load Conjur identity
         identity = self.merge_dictionaries(
@@ -69,28 +69,28 @@ class LookupModule(LookupBase):
         )
         if not identity:
             raise AnsibleError('Conjur identity should be in environment variables or in one of the following paths: \'~/.netrc\', \'/etc/conjur.identity\'')
-        display.vvvv("configuration: {}".format(identity))
+        display.vvvv('configuration: {0}'.format(identity))
 
         # Use credentials to retrieve temporary authorization token
-        conjur_url = "{}/authn/{}/{}/authenticate".format(conf['appliance_url'], conf['account'], identity['id'])
-        display.vvvv("Authentication request to Conjur at: {}, with user: {}".format(conjur_url, identity['id']))
+        conjur_url = '{0}/authn/{1}/{2}/authenticate'.format(conf['appliance_url'], conf['account'], identity['id'])
+        display.vvvv('Authentication request to Conjur at: {0}, with user: {1}'.format(conjur_url, identity['id']))
 
         if 'cert_file' in conf:
             response = requests.post(conjur_url, data=identity['api_key'], verify=conf['cert_file'])
         else:
             response = requests.post(conjur_url, data=identity['api_key'])
-        display.vvvv("response: {}".format(response.text))
+        display.vvvv('response: {0}'.format(response.text))
 
         if response.status_code != 200:
-            raise AnsibleError('Failed to authenticate as \'{}\''.format(identity['id']))
+            raise AnsibleError('Failed to authenticate as \'{0}\''.format(identity['id']))
 
         # Retrieve Conjur variable using the temporary token
         token = b64encode(response.text)
-        headers = {'Authorization': "Token token=\"{}\"".format(token)}
-        display.vvvv("Header: {}".format(headers))
+        headers = {'Authorization': 'Token token="{0}"'.format(token)}
+        display.vvvv('Header: {0}'.format(headers))
 
-        url = "{}/secrets/{}/variable/{}".format(conf['appliance_url'], conf['account'], quote_plus(terms[0]))
-        display.vvvv("Conjur Variable URL: {}".format(url))
+        url = '{0}/secrets/{1}/variable/{2}'.format(conf['appliance_url'], conf['account'], quote_plus(terms[0]))
+        display.vvvv('Conjur Variable URL: {0}'.format(url))
 
         if 'cert_file' in conf:
             response = requests.get(url, headers=headers, verify=conf['cert_file'])
@@ -98,27 +98,26 @@ class LookupModule(LookupBase):
             response = requests.get(url, headers=headers)
 
         if response.status_code == 200:
-            display.vvvv("Conjur variable {} was successfully retrieved".format(terms[0]))
+            display.vvvv('Conjur variable {0} was successfully retrieved'.format(terms[0]))
             return [response.text]
         if response.status_code == 401:
             raise AnsibleError('Conjur request has invalid authorization credentials')
         if response.status_code == 403:
-            raise AnsibleError('Conjur host does not have authorization to retrieve {}'.format(terms[0]))
+            raise AnsibleError('Conjur host does not have authorization to retrieve {0}'.format(terms[0]))
         if response.status_code == 404:
-            raise AnsibleError('The variable {} does not exist'.format(terms[0]))
+            raise AnsibleError('The variable {0} does not exist'.format(terms[0]))
 
     # Load configuration and return as dictionary if file is present on file system
     def load_conf(self, conf_path):
         conf_path = os.path.expanduser(conf_path)
 
         if conf_path and os.path.exists(conf_path):
-            display.vvvv("Loading configuration from: %s" % conf_path)
+            display.vvvv('Loading configuration from: {0}'.format(conf_path))
             with open(conf_path) as f:
                 try:
                     return yaml.safe_load(f.read())
                 except Exception as exception:
-                    display.info("Error: parsing %s - %s" % (conf_path, str(exception)))
-                    AnsibleError("Error: parsing %s - %s" % (conf_path, str(exception)))
+                    AnsibleError('Error: parsing {0} - {1}'.format(conf_path, str(exception)))
         return {}
 
     # Load identity and return as dictionary if file is present on file system
@@ -126,16 +125,16 @@ class LookupModule(LookupBase):
         identity_path = os.path.expanduser(identity_path)
 
         if identity_path and os.path.exists(identity_path):
-            display.vvvv("Loading identity from: %s" % identity_path)
+            display.vvvv('Loading identity from: {0}'.format(identity_path))
             try:
                 identity = netrc(identity_path)
-                id, account, api_key = identity.authenticators('{}/authn'.format(appliance_url))
+                id, account, api_key = identity.authenticators('{0}/authn'.format(appliance_url))
                 if not id or not api_key:
                     return {}
 
-                return {"id": id, "api_key": api_key}
+                return {'id': id, 'api_key': api_key}
             except Exception as exception:
-                AnsibleError("Error: parsing %s - %s" % (identity_path, str(exception)))
+                AnsibleError('Error: parsing {0} - {1}'.format(identity_path, str(exception)))
 
         return {}
 
