ec0cba59856438d20f6d1498d6c93be6184159a5
Matt Martz <matt@sivel.net>
2018-01-04 14:02:57 -0600

    Update validate-modules arg_spec introspection to be faster, by only mocking the imports we explicitly list

--- test/sanity/validate-modules/module_args.py
+++ test/sanity/validate-modules/module_args.py
@@ -18,21 +18,56 @@
 
 import imp
 import sys
+import traceback
 
-from modulefinder import ModuleFinder
+from contextlib import contextmanager
 
 import mock
 
 
 MODULE_CLASSES = [
     'ansible.module_utils.basic.AnsibleModule',
-    'ansible.module_utils.vca.VcaAnsibleModule',
-    'ansible.module_utils.network.nxos.nxos.NetworkModule',
-    'ansible.module_utils.network.eos.eos.NetworkModule',
-    'ansible.module_utils.network.ios.ios.NetworkModule',
-    'ansible.module_utils.network.iosxr.iosxr.NetworkModule',
-    'ansible.module_utils.network.junos.junos.NetworkModule',
-    'ansible.module_utils.openswitch.NetworkModule',
+]
+
+
+# This can be removed once we replace ``raise Exception`` with ``module.fail_json``
+HAS_HACKS = [
+    'ansible.module_utils.k8s.common.HAS_K8S_MODULE_HELPER',
+    'ansible.module_utils.k8s.common.HAS_STRING_UTILS',
+]
+
+# Some of these can be removed once we replace ``raise Exception`` with ``module.fail_json``
+# Others are related to using info from imports before ``AnsibleModule`` is executed
+IMPORT_MODULE_HACKS = [
+    # f5
+    'netaddr.IPAddress',
+    'netaddr.AddrFormatError',
+    'objectpath.Tree',
+    'f5.utils.iapp_parser.NonextantTemplateNameException',
+    'f5.sdk_exception.LazyAttributesRequired',
+    'f5.utils.responses.handlers.Stats'
+    'f5.bigip.ManagementRoot',
+    'f5.bigip.contexts.TransactionContextManager',
+    'f5.bigiq.ManagementRoot',
+    'f5.iworkflow.ManagementRoot',
+    'icontrol.exceptions.iControlUnexpectedHTTPError',
+    # radware
+    'vdirect_client.rest_client',
+    # webfaction
+    'xmlrpclib',
+    # azure
+    'msrestazure.azure_exceptions.CloudError',
+    'azure.storage.cloudstorageaccount.CloudStorageAccount',
+    'azure.common.AzureMissingResourceHttpError',
+    'azure.mgmt.storage.models.ProvisioningState',
+    'azure.mgmt.storage.models.SkuName',
+    'azure.mgmt.dns.models.ARecord',
+    # google
+    'libcloud',
+    'libcloud.common.google',
+    'libcloud.dns.types.Provider',
+    # dimensiondata
+    'libcloud.common.dimensiondata',
 ]
 
 
@@ -40,6 +75,7 @@ class AnsibleModuleCallError(RuntimeError):
     pass
 
 
+@contextmanager
 def add_mocks(filename):
     gp = mock.patch('ansible.module_utils.basic.get_platform').start()
     gp.return_value = 'linux'
@@ -48,40 +84,36 @@ def add_mocks(filename):
     mocks = []
     for module_class in MODULE_CLASSES:
         mocks.append(
-            mock.patch('ansible.module_utils.basic.AnsibleModule',
-                       new=module_mock)
+            mock.patch('%s.__init__' % module_class, new=module_mock)
         )
     for m in mocks:
         p = m.start()
-        p.side_effect = AnsibleModuleCallError()
+        p.side_effect = AnsibleModuleCallError('AnsibleModuleCallError')
+    mocks.append(
+        mock.patch('ansible.module_utils.basic._load_params').start()
+    )
 
-    finder = ModuleFinder()
-    try:
-        finder.run_script(filename)
-    except:
-        pass
+    for has in HAS_HACKS:
+        mock.patch(has, True).start()
 
     sys_mock = mock.MagicMock()
     sys_mock.__version__ = '0.0.0'
     sys_mocks = []
-    for module, sources in finder.badmodules.items():
+    for module in IMPORT_MODULE_HACKS:
         if module in sys.modules:
             continue
-        if [s for s in sources if s[:7] in ['ansible', '__main_']]:
-            parts = module.split('.')
-            for i in range(len(parts)):
-                dotted = '.'.join(parts[:i + 1])
-                # Never mock out ansible or ansible.module_utils
-                # we may get here if a specific module_utils file needed mocked
-                if dotted in ('ansible', 'ansible.module_utils',):
-                    continue
-                sys.modules[dotted] = sys_mock
-                sys_mocks.append(dotted)
+        parts = module.split('.')
+        for i in range(len(parts)):
+            dotted = '.'.join(parts[:i + 1])
+            # Never mock out ansible or ansible.module_utils
+            # we may get here if a specific module_utils file needed mocked
+            if dotted in ('ansible', 'ansible.module_utils',):
+                continue
+            sys.modules[dotted] = sys_mock
+            sys_mocks.append(dotted)
 
-    return module_mock, mocks, sys_mocks
+    yield module_mock
 
-
-def remove_mocks(mocks, sys_mocks):
     for m in mocks:
         m.stop()
 
@@ -93,19 +125,15 @@ def remove_mocks(mocks, sys_mocks):
 
 
 def get_argument_spec(filename):
-    module_mock, mocks, sys_mocks = add_mocks(filename)
-
-    try:
-        mod = imp.load_source('module', filename)
-        if not module_mock.call_args:
-            mod.main()
-    except AnsibleModuleCallError:
-        pass
-    except Exception:
-        # We can probably remove this branch, it is here for use while testing
-        pass
-
-    remove_mocks(mocks, sys_mocks)
+    with add_mocks(filename) as module_mock:
+        try:
+            mod = imp.load_source('module', filename)
+            if not module_mock.call_args:
+                mod.main()
+        except AnsibleModuleCallError:
+            pass
+        except Exception:
+            sys.stderr.write('%s\n' % traceback.format_exc())
 
     try:
         args, kwargs = module_mock.call_args
