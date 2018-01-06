29e3c05fabd5018d05a14a5d797fc6e1edd24d74
Rene Moser <mail@renemoser.net>
2018-01-06 10:19:23 +0100

    cs_network_offering: streamline docs a bit

--- lib/ansible/modules/cloud/cloudstack/cs_network_offering.py
+++ lib/ansible/modules/cloud/cloudstack/cs_network_offering.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright: (c) 2017, David Passante (@dpassante)
-#
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
+# Copyright (c) 2017, David Passante (@dpassante)
+# Copyright (c) 2017, René Moser <mail@renemoser.net>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
@@ -25,140 +12,96 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = '''
 ---
 module: cs_network_offering
-
 short_description: Manages network offerings on Apache CloudStack based clouds.
-
 description:
     - Create, update, enable, disable and remove network offerings.
-
 version_added: '2.5'
-
 author: "David Passante (@dpassante)"
-
 options:
   state:
     description:
       - State of the network offering.
-    choices: ['enabled', 'present', 'disabled', 'absent']
-    required: false
+    choices: [ enabled, present, disabled, absent]
     default: present
   display_text:
     description:
-      - Display text of the network offerings
-    required: false
-    default: null
+      - Display text of the network offerings.
   guest_ip_type:
     description:
-      - Guest type of the network offering. Shared or Isolated
-    choices: ['Shared', 'Isolated']
-    required: false
-    default: null
+      - Guest type of the network offering.
+    choices: [ Shared, Isolated ]
   name:
     description:
-      - The name of the network offering
+      - The name of the network offering.
     required: true
-    default: null
   supported_services:
     description:
-      - Services supported by the network offering
-    required: false
-    default: null
+      - Services supported by the network offering.
   traffic_type:
     description:
       - The traffic type for the network offering.
-      - Supported type in current release is GUEST only
-    required: false
     default: GUEST
   availability:
     description:
       - The availability of network offering. Default value is Optional
-    required: false
-    default: null
   conserve_mode:
     description:
-      - true if the network offering is IP conserve mode enabled
-    choices: ['true', 'false']
-    required: false
-    default: null
+      - Whether the network offering has IP conserve mode enabled.
+    choices: [ yes, no ]
   details:
     description:
       - Network offering details in key/value pairs.
-      - Supported keys are internallbprovider/publiclbprovider
       - with service provider as a value
-    choices: ['internallbprovider', 'publiclbprovider']
-    required: false
-    default: null
+    choices: [ internallbprovider, publiclbprovider ]
   egress_default_policy:
     description:
-      - True if guest network default egress policy is allow.
-      - false if default egress policy is deny
-    choices: ['true', 'false']
-    required: false
-    default: null
+      - Whether the default egress policy is allow (yes) or to deny (no).
+    choices: [ yes no ]
   persistent:
     description:
       - True if network offering supports persistent networks
       - defaulted to false if not specified
-    required: false
-    default: null
   keepalive_enabled:
     description:
       - If true keepalive will be turned on in the loadbalancer.
-      - At the time of writing this has only an effect on haproxy;
+      - At the time of writing this has only an effect on haproxy.
       - the mode http and httpclose options are unset in the haproxy conf file.
-    choices: ['true', 'false']
-    required: false
-    default: null
+    choices: [ yes, no ]
   max_connections:
     description:
-      - Maximum number of concurrent connections supported by the network offering
-    required: false
-    default: null
+      - Maximum number of concurrent connections supported by the network offering.
   network_rate:
     description:
-      - Data transfer rate in megabits per second allowed
-    required: false
-    default: null
+      - Data transfer rate in megabits per second allowed.
   service_capabilities:
     description:
-      - Desired service capabilities as part of network offering
+      - Desired service capabilities as part of network offering.
     aliases: [ service_capability ]
-    required: false
-    default: null
   service_offering:
     description:
-      - The service offering name or ID used by virtual router provider
-    required: false
-    default: null
+      - The service offering name or ID used by virtual router provider.
   service_provider_list:
     description:
-      - provider to service mapping
-      - If not specified, the provider for the service
-      - will be mapped to the default provider on the physical network
-    required: false
-    default: null
+      - Provider to service mapping.
+      - If not specified, the provider for the service will be mapped to the default provider on the physical network.
   specify_ip_ranges:
     description:
-      - true if network offering supports specifying ip ranges
-      - defaulted to false if not specified
-    choices: ['true', 'false']
-    required: false
-    default: null
+      - Wheter the network offering supports specifying IP ranges.
+      - Defaulted to C(no) by the API if not specified.
+    choices: [ yes, no ]
   specify_vlan:
     description:
-      - true if network offering supports vlans
-    choices: ['true', 'false']
-    required: false
-    default: null
+      - Whether the network offering supports vlans or not.
+    choices: [ yes, no ]
 extends_documentation_fragment: cloudstack
 '''
 
 EXAMPLES = '''
-# Create a network offering and enable it
-- local_action:
+- name: Create a network offering and enable it
+  local_action:
     module: cs_network_offering
-    name: "my_network_offering"
-    display_text: "network offering description"
+    name: my_network_offering
+    display_text: network offering description
     state: enabled
     guest_ip_type: Isolated
     supported_services: Dns,PortForwarding,Dhcp,SourceNat,UserData,Firewall,StaticNat,Vpn,Lb
@@ -167,17 +110,11 @@ EXAMPLES = '''
       - {service: 'dhcp', provider: 'virtualrouter'}
 
 
-# Remove a network offering
-- local_action:
+- name: Remove a network offering
+  local_action:
     module: cs_network_offering
-    name: "my_network_offering"
-    display_text: "network offering description"
+    name: my_network_offering
     state: absent
-    guest_ip_type: Isolated
-    supported_services: Dns,PortForwarding,Dhcp,SourceNat,UserData,Firewall,StaticNat,Vpn,Lb
-    service_provider_list:
-      - {service: 'dns', provider: 'virtualrouter'}
-      - {service: 'dhcp', provider: 'virtualrouter'}
 '''
 
 RETURN = '''
@@ -188,27 +125,27 @@ id:
   type: string
   sample: a6f7a5fc-43f8-11e5-a151-feff819cdc9f
 name:
-  description: The name of the network offering
+  description: The name of the network offering.
   returned: success
   type: string
   sample: MyCustomNetworkOffering
 display_text:
-  description: The display text of the network offering
+  description: The display text of the network offering.
   returned: success
   type: string
   sample: My network offering
 state:
-  description: The state of the network offering
+  description: The state of the network offering.
   returned: success
   type: string
   sample: Enabled
 guest_ip_type:
-  description: Guest type of the network offering
+  description: Guest type of the network offering.
   returned: success
   type: string
   sample: Isolated
 availability:
-  description: The availability of network offering
+  description: The availability of network offering.
   returned: success
   type: string
   sample: Optional
@@ -320,13 +257,10 @@ class AnsibleCloudStackNetworkOffering(AnsibleCloudStack):
     def delete_network_offering(self):
         network_offering = self.get_network_offering()
 
-        if not network_offering:
-            return network_offering
-
-        self.result['changed'] = True
-
-        if not self.module.check_mode:
-            res = self.query_api('deleteNetworkOffering', id=network_offering['id'])
+        if network_offering:
+            self.result['changed'] = True
+            if not self.module.check_mode:
+                self.query_api('deleteNetworkOffering', id=network_offering['id'])
 
         return network_offering
 
@@ -342,7 +276,7 @@ class AnsibleCloudStackNetworkOffering(AnsibleCloudStack):
             'availability': self.module.params.get('availability'),
         }
 
-        if args['state'] == 'enabled' or args['state'] == 'disabled':
+        if args['state'] in ['enabled', 'disabled']:
             args['state'] = args['state'].title()
         else:
             del args['state']
@@ -398,6 +332,7 @@ def main():
     result = acs_network_offering.get_result(network_offering)
 
     module.exit_json(**result)
+
 
 if __name__ == '__main__':
     main()
