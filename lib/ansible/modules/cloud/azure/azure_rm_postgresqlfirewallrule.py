5c4cbe3a87580a042129c8ddb2d27d71f2995420
Ubuntu <zim@zims-ubuntu-3.hzdriptz0rguzhq25yp3koz2nd.hx.internal.cloudapp.net>
2018-01-09 14:30:12 +0000

    updates to azure_rm_postgresqlfirewallrule

--- lib/ansible/modules/cloud/azure/azure_rm_postgresqlfirewallrule.py
+++ lib/ansible/modules/cloud/azure/azure_rm_postgresqlfirewallrule.py
@@ -67,7 +67,8 @@ id:
         - Resource ID
     returned: always
     type: str
-    sample: /subscriptions/ffffffff-ffff-ffff-ffff-ffffffffffff/resourceGroups/TestGroup/providers/Microsoft.DBforPostgreSQL/servers/testserver/firewallRules/rule1
+    sample: "/subscriptions/ffffffff-ffff-ffff-ffff-ffffffffffff/resourceGroups/TestGroup/providers/Microsoft.DBforPostgreSQL/servers/testserver/firewallRules/r
+             ule1"
 '''
 
 import time
