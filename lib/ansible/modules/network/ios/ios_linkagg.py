3490ed7bc20b249b3033a4dd5340a237a2fdc251
Trishna Guha <trishnaguha17@gmail.com>
2018-01-09 10:23:01 +0530

    fix pep8 issues

--- lib/ansible/modules/network/ios/ios_linkagg.py
+++ lib/ansible/modules/network/ios/ios_linkagg.py
@@ -121,10 +121,10 @@ def map_obj_to_commands(updates, module):
 
         if state == 'absent':
             if obj_in_have:
-                commands.append('no interface port-channel {}'.format(group))
+                commands.append('no interface port-channel {0}'.format(group))
 
         elif state == 'present':
-            cmd = ['interface port-channel {}'.format(group),
+            cmd = ['interface port-channel {0}'.format(group),
                    'end']
             if not obj_in_have:
                 if not group:
@@ -133,7 +133,7 @@ def map_obj_to_commands(updates, module):
 
                 if members:
                     for m in members:
-                        commands.append('interface {}'.format(m))
+                        commands.append('interface {0}'.format(m))
                         commands.append('channel-group {0} mode {1}'.format(group, mode))
 
             else:
@@ -141,32 +141,32 @@ def map_obj_to_commands(updates, module):
                     if 'members' not in obj_in_have.keys():
                         for m in members:
                             commands.extend(cmd)
-                            commands.append('interface {}'.format(m))
+                            commands.append('interface {0}'.format(m))
                             commands.append('channel-group {0} mode {1}'.format(group, mode))
 
                     elif set(members) != set(obj_in_have['members']):
                         missing_members = list(set(members) - set(obj_in_have['members']))
                         for m in missing_members:
                             commands.extend(cmd)
-                            commands.append('interface {}'.format(m))
+                            commands.append('interface {0}'.format(m))
                             commands.append('channel-group {0} mode {1}'.format(group, mode))
 
                         superfluous_members = list(set(obj_in_have['members']) - set(members))
                         for m in superfluous_members:
                             commands.extend(cmd)
-                            commands.append('interface {}'.format(m))
+                            commands.append('interface {0}'.format(m))
                             commands.append('no channel-group {0} mode {1}'.format(group, mode))
 
     if purge:
         for h in have:
             obj_in_want = search_obj_in_list(h['group'], want)
             if not obj_in_want:
-                commands.append('no interface port-channel {}'.format(h['group']))
+                commands.append('no interface port-channel {0}'.format(h['group']))
 
     return commands
 
 
-def map_params_to_obj(module, required_together=None):
+def map_params_to_obj(module):
     obj = []
 
     aggregate = module.params.get('aggregate')
@@ -176,7 +176,6 @@ def map_params_to_obj(module, required_together=None):
                 if item.get(key) is None:
                     item[key] = module.params[key]
 
-            module._check_required_together(required_together, item)
             d = item.copy()
             d['group'] = str(d['group'])
 
