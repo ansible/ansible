86b4472122c3aa336825298205993f5a941b2551
Ubuntu <zim@zims-ubuntu-3.hzdriptz0rguzhq25yp3koz2nd.hx.internal.cloudapp.net>
2018-01-09 14:29:20 +0000

    updates to azure_rm_sqldatabase

--- lib/ansible/modules/cloud/azure/azure_rm_sqldatabase.py
+++ lib/ansible/modules/cloud/azure/azure_rm_sqldatabase.py
@@ -139,7 +139,8 @@ id:
         - Resource ID.
     returned: always
     type: str
-    sample: /subscriptions/00000000-1111-2222-3333-444444444444/resourceGroups/sqlcrudtest-4799/providers/Microsoft.Sql/servers/sqlcrudtest-5961/databases/testdb
+    sample: "/subscriptions/00000000-1111-2222-3333-444444444444/resourceGroups/sqlcrudtest-4799/providers/Microsoft.Sql/servers/sqlcrudtest-5961/databases/test
+             db"
 database_id:
     description:
         - The ID of the database.
