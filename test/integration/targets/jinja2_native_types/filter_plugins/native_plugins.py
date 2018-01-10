cde3e85ac4121ad1086b03b23455b611dbad4215
Martin Krizek <martin.krizek@gmail.com>
2018-01-10 17:31:40 +0100

    Fix pep8

--- test/integration/targets/jinja2_native_types/filter_plugins/native_plugins.py
+++ test/integration/targets/jinja2_native_types/filter_plugins/native_plugins.py
@@ -1,12 +1,14 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 
 from ansible.module_utils._text import to_text
+
 
 def make_text(data):
     try:
         return to_text(data)
     except:
         return str(data)
+
 
 class FilterModule(object):
     def filters(self):
