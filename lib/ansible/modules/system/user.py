cf53776711eff22459db1081c009448db41bdec6
abelbabel <kaltheat@gmail.com>
2018-01-10 14:21:44 +0100

    use FreeBSD's own (more sophisticated) home creation function rather than ansible's create_home(), which does not handle skeleton files correctly for FreeBSD

--- lib/ansible/modules/system/user.py
+++ lib/ansible/modules/system/user.py
@@ -855,8 +855,8 @@ class FreeBsdUser(User):
             cmd.append('-c')
             cmd.append(self.comment)
 
-        if self.home is not None and info[5] != self.home:
-            if self.move_home:
+        if self.home is not None:
+            if (info[5] != self.home and self.move_home) or (not os.path.exists(self.home) and self.createhome):
                 cmd.append('-m')
             cmd.append('-d')
             cmd.append(self.home)
