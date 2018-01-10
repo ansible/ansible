564e013534c6ba28c16b6646a14e624a08b27716
Matt Clay <matt@mystile.com>
2018-01-10 02:17:04 -0800

    Fix pylint issue.

--- lib/ansible/module_utils/azure_rm_common.py
+++ lib/ansible/module_utils/azure_rm_common.py
@@ -786,4 +786,3 @@ class AzureRMModuleBase(object):
         if not self._containerservice_client:
             self._containerservice_client = self.get_mgmt_svc_client(ContainerServiceClient)
         return self._containerservice_client
-
