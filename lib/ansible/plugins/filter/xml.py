76121f7cb949bffc95ea16102bcc7f90cb2d0681
gic <cgiese@juniper.net>
2018-01-10 21:42:56 +0100

    add xml_findall to iterate over xml

--- lib/ansible/plugins/filter/xml.py
+++ lib/ansible/plugins/filter/xml.py
@@ -1,4 +1,4 @@
-# Copyright: (c) 2017, Christian Giese (@GIC-de)
+# Copyright: (c) 2018, Christian Giese (@GIC-de)
 #
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
@@ -6,7 +6,7 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-from ansible.errors import AnsibleError
+from ansible.errors import AnsibleError, AnsibleFilterError
 
 try:
     from lxml import etree
@@ -14,6 +14,18 @@ try:
     HAS_LXML = True
 except ImportError:
     HAS_LXML = False
+
+
+def xml_findall(data, expr):
+    if not HAS_LXML:
+        raise AnsibleError('The lxml module is required but does not appear to be installed.')
+    else:
+        try:
+            xml = etree.XML(data, parser=parser)
+            result = [etree.tostring(_xml) for _xml in xml.findall(expr)]
+        except Exception as ex:
+            raise AnsibleFilterError('xml error: ' + str(ex))
+        return result
 
 
 def xml_findtext(data, expr):
@@ -24,13 +36,14 @@ def xml_findtext(data, expr):
             xml = etree.XML(data, parser=parser)
             result = xml.findtext(expr)
         except Exception as ex:
-            raise AnsibleError('xml error: ' + str(ex))
+            raise AnsibleFilterError('xml error: ' + str(ex))
         return result
 
 
 class FilterModule(object):
-    """XPATH Filter"""
+    """XML XPATH Filter"""
     def filters(self):
         return {
+            'xml_findall': xml_findall,
             'xml_findtext': xml_findtext
         }
