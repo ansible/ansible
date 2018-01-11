b13b32820a1d968b8c4fcf5a57dbd0b55973564b
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2018-01-11 13:33:33 +0100

    Refactor EOS code to use cliconf (#34426)
    * WIP Refactor EOS code to use cliconf
    
    * Fix connection.get where sendonly is True
    
    * Fix pylint issue
    
    * Remove return from send_config and various exec_commands
    
    Also, removed a few try/except, which are anyways handled in
    Connection.

--- lib/ansible/plugins/cliconf/eos.py
+++ lib/ansible/plugins/cliconf/eos.py
@@ -47,14 +47,18 @@ class Cliconf(CliconfBase):
         return device_info
 
     @enable_mode
-    def get_config(self, source='running', format='text'):
+    def get_config(self, source='running', format='text', flags=None):
         lookup = {'running': 'running-config', 'startup': 'startup-config'}
         if source not in lookup:
             return self.invalid_params("fetching configuration from %s is not supported" % source)
         if format == 'text':
-            cmd = b'show %s' % lookup[source]
+            cmd = b'show %s ' % lookup[source]
         else:
             cmd = b'show %s | %s' % (lookup[source], format)
+
+        flags = [] if flags is None else flags
+        cmd += ' '.join(flags)
+        cmd = cmd.strip()
         return self.send_command(cmd)
 
     @enable_mode
