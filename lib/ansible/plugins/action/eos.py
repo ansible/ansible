98059787711542b5ec04f8654a8ef59f972b22bc
Nathaniel Case <this.is@nathanielca.se>
2018-01-11 16:47:48 -0500

    Report invalid connection types for network modules (#34755)
    * Emit error on wrong connection type

--- lib/ansible/plugins/action/eos.py
+++ lib/ansible/plugins/action/eos.py
@@ -101,6 +101,8 @@ class ActionModule(_ActionModule):
                     provider['authorize'] = False
 
                 self._task.args['provider'] = provider
+        else:
+            return {'failed': True, 'msg': 'Connection type %s is not valid for this module' % self._play_context.connection}
 
         if (self._play_context.connection == 'local' and transport == 'cli') or self._play_context.connection == 'network_cli':
             # make sure we are in the right cli context which should be
