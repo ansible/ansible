882f7b66eaf23f1a52e28aa8a6f4b2dd14da9800
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-01-12 16:16:40 +0530

    Changing order of load and loadfp

--- lib/ansible/modules/network/enos/enos_config.py
+++ lib/ansible/modules/network/enos/enos_config.py
@@ -184,9 +184,9 @@ def get_candidate(module):
     candidate = NetworkConfig(indent=1)
     if module.params['src']:
         try:
-            candidate.loadfp(module.params['src'])
-        except IOError:
             candidate.load(module.params['src'])
+        except IOError:
+            candidate.loadfp(module.params['src'])
     elif module.params['lines']:
         parents = module.params['parents'] or list()
         candidate.add(module.params['lines'], parents=parents)
