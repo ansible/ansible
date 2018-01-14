6cba1493f63f9a7eeeab47124d26680100cd0cf9
René Moser <mail@renemoser.net>
2018-01-14 14:20:26 +0100

    vultr: fix config ini params ignored (#34848)
    * vultr: fix config ini params ignored
    
    * vutlr: align docs with configs

--- lib/ansible/utils/module_docs_fragments/vultr.py
+++ lib/ansible/utils/module_docs_fragments/vultr.py
@@ -16,12 +16,12 @@ options:
     description:
       - HTTP timeout to Vultr API.
       - The ENV variable C(VULTR_API_TIMEOUT) is used as default, when defined.
-    default: 10
+    default: 60
   api_retries:
     description:
       - Amount of retries in case of the Vultr API retuns an HTTP 503 code.
       - The ENV variable C(VULTR_API_RETRIES) is used as default, when defined.
-    default: 10
+    default: 5
   api_account:
     description:
       - Name of the ini section in the C(vultr.ini) file.
@@ -30,7 +30,7 @@ options:
   validate_certs:
     description:
       - Validate SSL certs of the Vultr API.
-    default: true
+    default: yes
     type: bool
 requirements:
   - "python >= 2.6"
