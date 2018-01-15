832b61d6ce7f882789de9c4bf3659a82a614f362
Berend de Boer <berend@pobox.com>
2018-01-16 10:49:58 +1300

    FreeBSD STABLE distribution not correctly parsed (#34770)

--- lib/ansible/module_utils/facts/system/distribution.py
+++ lib/ansible/module_utils/facts/system/distribution.py
@@ -496,7 +496,7 @@ class Distribution(object):
     def get_distribution_FreeBSD(self):
         freebsd_facts = {}
         freebsd_facts['distribution_release'] = platform.release()
-        data = re.search(r'(\d+)\.(\d+)-RELEASE.*', freebsd_facts['distribution_release'])
+        data = re.search(r'(\d+)\.(\d+)-(RELEASE|STABLE).*', freebsd_facts['distribution_release'])
         if data:
             freebsd_facts['distribution_major_version'] = data.group(1)
             freebsd_facts['distribution_version'] = '%s.%s' % (data.group(1), data.group(2))
