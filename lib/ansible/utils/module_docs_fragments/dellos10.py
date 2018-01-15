013e7e98747f6e380b0af93fbb3b594409b39d20
Senthil Kumar Ganesan <skg.net.dev@gmail.com>
2018-01-15 13:52:16 -0800

    Support for network_cli plugin and tests

--- lib/ansible/utils/module_docs_fragments/dellos10.py
+++ lib/ansible/utils/module_docs_fragments/dellos10.py
@@ -3,8 +3,7 @@
 #
 # Copyright (c) 2016 Dell Inc.
 #
-# This file is part of Ansible
-#
+# This file is part of Ansible 
 # Ansible is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -24,44 +23,50 @@ class ModuleDocFragment(object):
     # Standard files documentation fragment
     DOCUMENTATION = """
 options:
+  host:
+    description:
+      - Specifies the DNS host name or address for connecting to the remote
+        device over the specified transport.  The value of host is used as
+        the destination address for the transport.
+    required: true
+  port:
+    description:
+      - Specifies the port to use when building the connection to the remote
+        device.
+    required: false
+    default: 22
+  username:
+    description:
+      - This value I(username) is used to authenticate the SSH session to the
+        remote device. If the value is not specified in the task, the
+        value of environment variable ANSIBLE_NET_USERNAME will be used instead.
+    required: false
+  password:
+    description:
+      - This value I(password) is used to authenticate the SSH session to 
+        the remote device. If the value is not specified in the task, the
+        value of environment variable ANSIBLE_NET_PASSWORD will be used instead.
+    required: false
+    default: null
+  ssh_keyfile:
+    description:
+      - This value I(ssh_keyfile) is the path to the key used to authenticate 
+        the SSH session to the remote device.  If the value is not specified
+        in the task, the value of environment variable ANSIBLE_NET_SSH_KEYFILE
+        will be used instead.
+    required: false
+  timeout:
+    description:
+      - Specifies idle timeout (in seconds) for the connection. Useful if the
+        console freezes before continuing. For example when saving
+        configurations.
+    required: false
+    default: 10
   provider:
     description:
-      - A dict object containing connection details.
+      - Convenience method that allows all M(dellos10) arguments to be passed as
+        a dict object.  All constraints (required, choices, etc) must be
+        met either by individual arguments or values in this dict.
+    required: false
     default: null
-    suboptions:
-      host:
-        description:
-          - Specifies the DNS host name or address for connecting to the remote
-            device over the specified transport.  The value of host is used as
-            the destination address for the transport.
-        required: true
-      port:
-        description:
-          - Specifies the port to use when building the connection to the remote
-            device.
-        default: 22
-      username:
-        description:
-          - User to authenticate the SSH session to the remote device. If the
-            value is not specified in the task, the value of environment variable
-            C(ANSIBLE_NET_USERNAME) will be used instead.
-      password:
-        description:
-          - Password to authenticate the SSH session to the remote device. If the
-            value is not specified in the task, the value of environment variable
-            C(ANSIBLE_NET_PASSWORD) will be used instead.
-        default: null
-      ssh_keyfile:
-        description:
-          - Path to an ssh key used to authenticate the SSH session to the remote
-            device.  If the value is not specified in the task, the value of
-            environment variable C(ANSIBLE_NET_SSH_KEYFILE) will be used instead.
-      timeout:
-        description:
-          - Specifies idle timeout (in seconds) for the connection. Useful if the
-            console freezes before continuing. For example when saving
-            configurations.
-        default: 10
-notes:
-  - For more information on using Ansible to manage Dell EMC Network devices see U(https://www.ansible.com/ansible-dell-networking).
 """
