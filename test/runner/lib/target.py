485b162bf5b8135dca95778a3fee44453161c31b
James Tanner <tanner.jc@gmail.com>
2018-01-15 12:30:17 -0500

    Add comment

--- test/runner/lib/target.py
+++ test/runner/lib/target.py
@@ -291,6 +291,7 @@ def walk_test_targets(path=None, module_path=None, extensions=None, prefix=None,
     :type extra_dirs: tuple[str] | None
     :rtype: collections.Iterable[TestTarget]
     """
+    # walk and follow symlinks to allow testing external directories
     for root, _, file_names in os.walk(path or '.', topdown=False, followlinks=True):
         if root.endswith('/__pycache__'):
             continue
