a84d5404fd55bae7f6f2be65e899bde62dff5f47
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2018-01-15 14:16:56 +0100

    Plumb newline param on cliconf (#34868)
    Change cb1b7052182d675d24bbf0f815587165f0cf6f0a introduced the newline
    parameter on network_cli plugin, but that was never introduced on cliconf.
    This causes ios_user to break, since the newline value is never plumbed thru
    to network_cli

--- lib/ansible/plugins/cliconf/__init__.py
+++ lib/ansible/plugins/cliconf/__init__.py
@@ -94,13 +94,14 @@ class CliconfBase(with_metaclass(ABCMeta, object)):
         display.display('closing shell due to command timeout (%s seconds).' % self._connection._play_context.timeout, log_only=True)
         self.close()
 
-    def send_command(self, command, prompt=None, answer=None, sendonly=False):
+    def send_command(self, command, prompt=None, answer=None, sendonly=False, newline=True):
         """Executes a cli command and returns the results
         This method will execute the CLI command on the connection and return
         the results to the caller.  The command output will be returned as a
         string
         """
-        kwargs = {'command': to_bytes(command), 'sendonly': sendonly}
+        kwargs = {'command': to_bytes(command), 'sendonly': sendonly,
+                  'newline': newline}
         if prompt is not None:
             kwargs['prompt'] = to_bytes(prompt)
         if answer is not None:
