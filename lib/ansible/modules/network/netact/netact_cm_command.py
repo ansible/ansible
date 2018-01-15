15987df48df5e2791d3069a8839e01478adbd679
hatuomin <harri.k.tuominen@nokia.com>
2018-01-15 15:36:46 +0200

    adding netact_cm_command
    (cherry picked from commit 1aa737e)

--- lib/ansible/modules/network/netact/netact_cm_command.py
+++ lib/ansible/modules/network/netact/netact_cm_command.py
@@ -182,9 +182,17 @@ EXAMPLES = '''
 RETURN = '''
 original_message:
     description: The original name param that was passed in
-    type: str
+    returned: Command line
+    type: string
+    sample: '/opt/oss/bin/racclimx.sh -op Upload -opsName Uploading_testi -DN PLMN-PLMN/MRBTS-746'
 message:
     description: The output message that the netact_cm_command module generates
+    returned: Command output message
+    type: string
+changed:
+    description: data changed
+    returned: true if data is changed
+    type: boolean
 '''
 
 from ansible.module_utils.basic import AnsibleModule
