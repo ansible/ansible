576335e53dc7721811a9dfe02b29a36ec7f27e56
Tristan de Cacqueray <tdecacqu@redhat.com>
2018-01-16 15:37:50 +0000

    Add GALAXY_TOKEN config option (#34621)
    This change lets user store token in configuration file or environment to
    prevent exposing the secret on the command line.

--- lib/ansible/cli/galaxy.py
+++ lib/ansible/cli/galaxy.py
@@ -545,14 +545,17 @@ class GalaxyCLI(CLI):
         """
         # Authenticate with github and retrieve a token
         if self.options.token is None:
-            login = GalaxyLogin(self.galaxy)
-            github_token = login.create_github_token()
+            if C.GALAXY_TOKEN:
+                github_token = C.GALAXY_TOKEN
+            else:
+                login = GalaxyLogin(self.galaxy)
+                github_token = login.create_github_token()
         else:
             github_token = self.options.token
 
         galaxy_response = self.api.authenticate(github_token)
 
-        if self.options.token is None:
+        if self.options.token is None and C.GALAXY_TOKEN is None:
             # Remove the token we created
             login.remove_github_token()
 
