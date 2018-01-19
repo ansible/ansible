ea92f2558b9a7f3f9ebaf94c036f484a786c01ef
Nathaniel Case <this.is@nathanielca.se>
2018-01-19 17:14:32 -0500

    ios_l2_interface (#35054)
    * Fix sending 'None' to device on ios
    
    * Add ios_l2_interface module & tests
    
    * Skip l2 tests on non-l2 images
    
    * Allow test to continue on non-L2 hosts

--- lib/ansible/plugins/cliconf/ios.py
+++ lib/ansible/plugins/cliconf/ios.py
@@ -86,8 +86,7 @@ class Cliconf(CliconfBase):
                               False, newline)
 
     def get(self, command, prompt=None, answer=None, sendonly=False):
-        return self.send_command(to_bytes(command), prompt=to_bytes(prompt),
-                                 answer=to_bytes(answer), sendonly=sendonly)
+        return self.send_command(command, prompt=prompt, answer=answer, sendonly=sendonly)
 
     def get_capabilities(self):
         result = {}
