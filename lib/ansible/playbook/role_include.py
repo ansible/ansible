2c3b895f1d69de3eeea83da429ad7b672e600cae
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-01-19 22:57:37 +0100

    include_role: add role property

--- lib/ansible/playbook/role_include.py
+++ lib/ansible/playbook/role_include.py
@@ -63,6 +63,7 @@ class IncludeRole(TaskInclude):
         self._parent_role = role
         self._role_name = None
         self._role_path = None
+        self._role = None
 
     def get_block_list(self, play=None, variable_manager=None, loader=None):
 
@@ -81,6 +82,7 @@ class IncludeRole(TaskInclude):
 
         # save this for later use
         self._role_path = actual_role._role_path
+        self._role = actual_role
 
         # compile role with parent roles as dependencies to ensure they inherit
         # variables
@@ -136,6 +138,7 @@ class IncludeRole(TaskInclude):
         new_me._parent_role = self._parent_role
         new_me._role_name = self._role_name
         new_me._role_path = self._role_path
+        new_me._role = self._role
 
         return new_me
 
