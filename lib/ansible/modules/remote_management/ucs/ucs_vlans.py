2bf84075e35205b2ad0dac5e897ee154ce2156cc
“dsoper2” <dsoper@cisco.com>
2018-01-18 17:09:16 -0800

    UCS VLANs and integration tests

--- lib/ansible/modules/remote_management/ucs/ucs_vlans.py
+++ lib/ansible/modules/remote_management/ucs/ucs_vlans.py
@@ -28,24 +28,49 @@ options:
   name:
     description:
     - The name assigned to the VLAN.
+    - The VLAN name is case sensitive.
     - This name can be between 1 and 32 alphanumeric characters.
     - "You cannot use spaces or any special characters other than - (hyphen), \"_\" (underscore), : (colon), and . (period)."
     - You cannot change this name after the VLAN is created.
     required: yes
-  id:
+  multicast_policy:
     description:
-    - The unique string identifier assigned to the VLAN.
-    - Note that Cisco UCS Manager uses VLAN '4048'.  See the UCS Manager configuration guide if you want to assign '4048' to a VLAN.
-    - Optional if state is absent.
-    required: yes
+    - The multicast policy associated with this VLAN.
+    - This option is only valid if the Sharing Type field is set to None or Primary.
+    default: ''
   fabric:
     description:
     - "The fabric configuration of the VLAN.  This can be one of the following:"
-    - "common - The VLAN maps to the same VLAN ID in all available fabrics."
-    - "A - The VLAN maps to the a VLAN ID that exists only in fabric A."
-    - "B - The VLAN maps to the a VLAN ID that exists only in fabric B."
+    - "common - The VLAN applies to both fabrics and uses the same configuration parameters in both cases."
+    - "A — The VLAN only applies to fabric A."
+    - "B — The VLAN only applies to fabric B."
+    - For upstream disjoint L2 networks, Cisco recommends that you choose common to create VLANs that apply to both fabrics.
     choices: [common, A, B]
     default: common
+  id:
+    description:
+    - The unique string identifier assigned to the VLAN.
+    - A VLAN ID can be between '1' and '3967', or between '4048' and '4093'.
+    - You cannot create VLANs with IDs from 4030 to 4047. This range of VLAN IDs is reserved.
+    - The VLAN IDs you specify must also be supported on the switch that you are using.
+    - VLANs in the LAN cloud and FCoE VLANs in the SAN cloud must have different IDs.
+    - Optional if state is absent.
+    required: yes
+  sharing:
+    description:
+    - The Sharing Type field.
+    - "Whether this VLAN is subdivided into private or secondary VLANs. This can be one of the following:"
+    - "none - This VLAN does not have any secondary or private VLANs. This is a regular VLAN."
+    - "primary - This VLAN can have one or more secondary VLANs, as shown in the Secondary VLANs area. This VLAN is a primary VLAN in the private VLAN domain."
+    - "isolated - This is a private VLAN associated with a primary VLAN. This VLAN is an Isolated VLAN."
+    - "community - This VLAN can communicate with other ports on the same community VLAN as well as the promiscuous port. This VLAN is a Community VLAN."
+    choices: [none, primary, isolated, community]
+    default: none
+  native:
+    description:
+    - Designates the VLAN as a native VLAN.
+    choices: ['no', 'yes']
+    default: 'no'
 requirements:
 - ucsmsdk
 author:
@@ -85,11 +110,11 @@ def main():
     argument_spec = ucs_argument_spec
     argument_spec.update(
         name=dict(type='str', required=True),
-        id=dict(type='str'),
-        native=dict(type='str', default='no', choices=['yes', 'no']),
+        multicast_policy=dict(type='str', default=''),
         fabric=dict(type='str', default='common', choices=['common', 'A', 'B']),
-        sharing=dict(type='str', default='none', choices=['none']),
-        multicast_policy=dict(type='str', default='', choices=['']),   
+        id=dict(type='str'),
+        sharing=dict(type='str', default='none', choices=['none', 'primary', 'isolated', 'community']),
+        native=dict(type='str', default='no', choices=['yes', 'no']),
         state=dict(type='str', default='present', choices=['present', 'absent']),
     )
 
