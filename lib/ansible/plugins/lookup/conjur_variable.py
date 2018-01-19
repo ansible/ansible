a7982fee91f4a11b6ecee466a7d81855090e17da
Ryan Prior <ryanprior@gmail.com>
2018-01-19 14:20:36 -0600

    Adds ANSIBLE_METADATA for Conjur variable lookup plugin

--- lib/ansible/plugins/lookup/conjur_variable.py
+++ lib/ansible/plugins/lookup/conjur_variable.py
@@ -4,6 +4,10 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
 DOCUMENTATION = """
     lookup: conjur_variable
     version_added: "2.5"
