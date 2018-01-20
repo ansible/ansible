0726c538f3cb354564b52669791d861ec13566a5
Adrian Likins <alikins@redhat.com>
2018-01-20 14:56:18 -0500

    Support using vault password files that are themselves vault encrypted  (#27668)
    Extract vault related bits of DataLoader._get_file_contents to DataLoader._decrypt_if_vault_data
    
    When loading vault password files, detect if they are vault encrypted, and if so, try to decrypt with any already known vault secrets.
    
    This implements the 'Allow vault password files to be vault encrypted'  (#31002) feature card from
    the 2.5.0 project at https://github.com/ansible/ansible/projects/9
    
    Fixes #31002

--- lib/ansible/parsing/vault/__init__.py
+++ lib/ansible/parsing/vault/__init__.py
@@ -430,6 +430,10 @@ class FileVaultSecret(VaultSecret):
         except (OSError, IOError) as e:
             raise AnsibleError("Could not read vault password file %s: %s" % (filename, e))
 
+        b_vault_data, dummy = self.loader._decrypt_if_vault_data(vault_pass, filename)
+
+        vault_pass = b_vault_data.strip(b'\r\n')
+
         verify_secret_is_not_empty(vault_pass,
                                    msg='Invalid vault password was provided from file (%s)' % filename)
 
