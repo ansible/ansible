b451ae79bc6fb6f4f7a01dc1d41a16fee3530773
“dsoper2” <dsoper@cisco.com>
2018-01-20 13:26:16 -0600

    UCS vNIC template and integration test

--- lib/ansible/modules/remote_management/ucs/ucs_vnic_template.py
+++ lib/ansible/modules/remote_management/ucs/ucs_vnic_template.py
@@ -34,62 +34,38 @@ options:
     required: yes
   description:
     description:
-    - A user-defined description of the template.
+    - A user-defined description of the vNIC template.
     - Enter up to 256 characters.
     - "You can use any characters or spaces except the following:"
     - "` (accent mark), \ (backslash), ^ (carat), \" (double quote), = (equal sign), > (greater than), < (less than), or ' (single quote)."
     aliases: [ descr ]
   fabric:
     description:
-    - The Fabric ID field.
-    - The name of the fabric interconnect that vNICs created with this template are associated with.
-    choices: [A, B]
+    - The Fabric ID field specifying the fabric interconnect associated with vNICs created from this template.
+    - If you want fabric failover enabled on vNICs created from this template, use of of the following:"
+    - "A-B to use Fabric A by default with failover enabled."
+    - "B-A to use Fabric B by default with failover enabled."
+    - "Do not enable vNIC fabric failover under the following circumstances:"
+    - "- If the Cisco UCS domain is running in Ethernet switch mode. vNIC fabric failover is not supported in that mode."
+    - "- If you plan to associate one or more vNICs created from this template to a server with an adapter that does not support fabric failover."
+    choices: [A, B, A-B, B-A]
     default: A
-The fabric interconnect associated with the component.
-
-If you want vNICs created from this template to be able to access the second fabric interconnect if the default one is unavailable, check the Enable Failover check box.
-
-Note
-Do not enable vNIC fabric failover under the following circumstances:
-
-If the Cisco UCS domain is running in Ethernet switch mode. vNIC fabric failover is not supported in that mode. If all Ethernet uplinks on one fabric interconnect fail, the vNICs do not fail over to the other.
-
-If you plan to associate one or more vNICs created from this template to a server with an adapter that does not support fabric failover, such as the Cisco UCS 82598KR-CI 10-Gigabit Ethernet Adapter. If so, Cisco UCS Manager generates a configuration fault when you associate the service profile with the server.
   redundancy_type:
     description:
-    - The Redundancy Type used for template pairing from the Primary or Secondary redundancy template.
+    - The Redundancy Type used for vNIC redundancy pairs during fabric failover.
+    - "This can be one of the following:"
     - "primary — Creates configurations that can be shared with the Secondary template."
-    - Any other shared changes on the Primary template are automatically synchronized to the Secondary template.
     - "secondary — All shared configurations are inherited from the Primary template."
     - "none - Legacy vNIC template behavior. Select this option if you do not want to use redundancy."
     choices: [none, primary, secondary]
     default: none
-target:
-A list of the possible targets for vNICs created from this template. The target you choose determines whether or not Cisco UCS Manager automatically creates a VM-FEX port profile with the appropriate settings for the vNIC template. This can be one of the following:
-
-Adapter—The vNICs apply to all adapters. No VM-FEX port profile is created if you choose this option.
-
-VM—The vNICs apply to all virtual machines. A VM-FEX port profile is created if you choose this option.
-  vlan:
+  target:
     description:
-    - The VLAN to associate with vNICs created from this template.
-    default: default
-VLANs Area
-Name	Description
-Select column
-
-Check the check box in this column for each VLAN that you want to use.
-
-Note
-VLANs and PVLANs can not be assigned to the same vNIC.
-
-Name column
-
-The name of the VLAN.
-
-Native VLAN column
-
-To designate one of the VLANs as the native VLAN, click the radio button in this column.
+    - The possible target for vNICs created from this template.
+    - The target determines whether or not Cisco UCS Manager automatically creates a VM-FEX port profile with the appropriate settings for the vNIC template.
+    - "This can be one of the following:"
+    - "adapter — The vNICs apply to all adapters. No VM-FEX port profile is created if you choose this option."
+    - "vm - The vNICs apply to all virtual machines. A VM-FEX port profile is created if you choose this option."
   template_type:
     description:
     - The Template Type field.
@@ -98,83 +74,50 @@ To designate one of the VLANs as the native VLAN, click the radio button in this
     - "updating-template - vNICs created from this template are updated if the template changes."
     choices: [initial-template, updating-template]
     default: initial-template
-  max_data:
+  vlans_list:
     description:
-    - The Max Data Field Size field.
-    - The maximum size of the Fibre Channel frame payload bytes that the vNIC supports.
-    - Enter an string between '256' and '2112'.
-    default: '2048'
-  wwpn_pool:  
+    - List of VLANs used by the vNIC template.
+    - "Each list element has the following suboptions:"
+    - "= name"
+    - "  The name of the VLAN (required)."
+    - "- native"
+    - "  Designates the VLAN as a native VLAN.  Only one VLAN in the list can be a native VLAN."
+    - "  [choices: 'no', 'yes']"
+    - "  [Default: 'no']"
+  cdn_source:
     description:
-    - The WWPN pool that a vNIC created from this template uses to derive its WWPN address.
-    default: default
+    - CDN Source field.
+    - "This can be one of the following options:"
+    - "vnic-name - Uses the vNIC template name of the vNIC instance as the CDN name. This is the default option."
+    - "user-defined - Uses a user-defined CDN name for the vNIC template. If this option is chosen, cdn_name must also be provided."
+    choices: [vnic-name, user-defined]
+    default: vnic-name
+  cdn_name:
+    description:
+    - CDN Name used when cdn_source is set to user-defined.
+  mtu_field:
+    description:
+    - The MTU field.
+    - The maximum transmission unit, or packet size, that vNICs created from this vNIC template should use.
+    - Enter a string between '1500' and '9000'.
+    - If the vNIC template has an associated QoS policy, the MTU specified here must be equal to or less than the MTU specified in the QoS system class.
+    default: '1500'
+  mac_pool:
+    description:
+    - The MAC address pool that vNICs created from this vNIC template should use.
   qos_policy:
     description:
-    - The QoS policy that is associated with vNICs created from this template.
+    - The quality of service (QoS) policy that vNICs created from this vNIC template should use.
+  network_control_policy:
+    description:
+    - The network control policy that vNICs created from this vNIC template should use.
   pin_group:
     description:
-    - The LAN pin group that is associated with vNICs created from this template.
+    - The LAN pin group that vNICs created from this vNIC template should use.
   stats_policy:
     description:
-    - The statistics collection policy that is associated with vNICs created from this template.
+    - The statistics collection policy that vNICs created from this vNIC template should use.
     default: default
-VLANs and VLAN Groups Properties
-Name	Description
-CDN Source field
-
-This can be one of the following options:
-
-vNIC Name
-
-—Uses the vNIC template name of the vNIC instance as the CDN name. This is the default option.
-User Defined
-
-— Displays the CDN Name field for you to enter a user-defined CDN name for the vNIC template.
-MTU field
-
-The maximum transmission unit, or packet size, that vNICs created from this vNIC template should use.
-
-Enter an integer between 1500 and 9000.
-
-Note
-If the vNIC template has an associated QoS policy, the MTU specified here must be equal to or less than the MTU specified in the associated QoS system class. If this MTU value exceeds the MTU value in the QoS system class, packets may be dropped during data transmission.
-
-MAC Pool drop-down list
-
-The MAC address pool that vNICs created from this vNIC template should use.
-
-QoS Policy drop-down list
-
-The quality of service policy that vNICs created from this vNIC template should use.
-
-Network Control Policy drop-down list
-
-The network control policy that vNICs created from this vNIC template should use.
-
-Pin Group drop-down list
-
-The LAN pin group that vNICs created from this vNIC template should use.
-
-Stats Threshold Policy drop-down list
-
-The statistics collection policy that vNICs created from this vNIC template should use.
-Connection Policies Area
-Name	Description
-Connection Policy radio button
-
-Choose the type of connection policy to associate with the vNIC. This can be one of the following:
-
-Dynamic vNIC
-
-usNIC
-
-VMQ
-
-Connection Policy drop-down list
-
-Choose the connection policy that the vNIC should use. The values displayed depend on the type of connection policy chosen.
-
-You can also create a new connection policy in this area.
   org_dn:
     description:
     - Org dn (distinguished name)
@@ -195,8 +138,21 @@ EXAMPLES = r'''
     password: password
     name: vNIC-A
     fabric: A
-    vlan: VLAN-A
-    mac_pool: WWPN-Pool-A
+    vlans_list:
+    - name: default
+      native: 'yes'
+
+- name: Configure vNIC template with failover
+  ucs_vnic_template:
+    hostname: 172.16.143.150
+    username: admin
+    password: password
+    name: vNIC-A-B
+    fabric: A-B
+    vlans_list:
+    - name: default
+      native: 'yes'
+    - name: finance
 
 - name: Remove vNIC template
   ucs_vnic_template:
@@ -204,6 +160,14 @@ EXAMPLES = r'''
     username: admin
     password: password
     name: vNIC-A
+    state: absent
+
+- name: Remove another vNIC template
+  ucs_vnic_template:
+    hostname: 172.16.143.150
+    username: admin
+    password: password
+    name: vNIC-A-B
     state: absent
 '''
 
@@ -221,15 +185,18 @@ def main():
         org_dn=dict(type='str', default='org-root'),
         name=dict(type='str', required=True),
         descr=dict(type='str', default=''),
-        fabric=dict(type='str', default='A', choices=['A', 'B']),
+        fabric=dict(type='str', default='A', choices=['A', 'B', 'A-B', 'B-A']),
         redundancy_type=dict(type='str', default='none', choices=['none', 'primary', 'secondary']),
-        vlan=dict(type='str', default='default'),
         template_type=dict(type='str', default='initial-template', choices=['initial-template', 'updating-template']),
+        vlans_list=dict(type='list'),
+        cdn_source=dict(type='str', default='vnic-name', choices=['vnic-name', 'user-defined']),
+        cdn_name=dict(type='str', default=''),
         mtu=dict(type='str', default='1500'),
-        mac_pool=dict(type='str', default='default'),
+        mac_pool=dict(type='str', default=''),
         qos_policy=dict(type='str', default=''),
+        network_control_policy=dict(type='str', default=''),
+        pin_group=dict(type='str', default=''),
         stats_policy=dict(type='str', default='default'),
-        network_ctrl_policy=dict(type='str', default=''),   
         state=dict(type='str', default='present', choices=['present', 'absent']),
     )
 
@@ -264,25 +231,38 @@ def main():
                     ucs.login_handle.commit()
                 changed = True
         else:
+            # set default params for lists which can't be done in the argument_spec
+            if module.params.get('vlans_list'):
+                for vlan in module.params['vlans_list']:
+                    if not vlan.get('native'):
+                        vlan['native'] = 'no'
             if mo_exists:
                 # check top-level mo props
                 kwargs = dict(descr=module.params['descr'])
                 kwargs['switch_id'] = module.params['fabric']
-                kwargs['redundancy_pair_type'] = module.params['redundancy_type']   
+                kwargs['redundancy_pair_type'] = module.params['redundancy_type']
                 kwargs['templ_type'] = module.params['template_type']
-                kwargs['max_data_field_size'] = module.params['max_data']
-                kwargs['ident_pool_name'] = module.params['wwpn_pool']
+                kwargs['cdn_source'] = module.params['cdn_source']
+                kwargs['admin_cdn_name'] = module.params['cdn_name']
+                kwargs['mtu'] = module.params['mtu']
+                kwargs['ident_pool_name'] = module.params['mac_pool']
                 kwargs['qos_policy_name'] = module.params['qos_policy']
+                kwargs['nw_ctrl_policy_name'] = module.params['network_control_policy']
                 kwargs['pin_to_group_name'] = module.params['pin_group']
                 kwargs['stats_policy_name'] = module.params['stats_policy']
                 if (mo.check_prop_match(**kwargs)):
                     # top-level props match, check next level mo/props
-                    child_dn = dn + '/if-default'
-                    mo_1 = ucs.login_handle.query_dn(child_dn)
-                    if mo_1:
-                        kwargs = dict(name=module.params['vlan'])
-                        if (mo_1.check_prop_match(**kwargs)):
-                            props_match = True
+                    if not module.params.get('vlans_list'):
+                        props_match = True
+                    else:
+                        # check vlan props
+                        for vlan in module.params['vlans_list']:
+                            child_dn = dn + '/if-' + vlan['name']
+                            mo_1 = ucs.login_handle.query_dn(child_dn)
+                            if mo_1:
+                                kwargs = dict(default_net=vlan['native'])
+                                if (mo_1.check_prop_match(**kwargs)):
+                                    props_match = True
 
             if not props_match:
                 if not module.check_mode:
@@ -293,18 +273,23 @@ def main():
                         descr=module.params['descr'],
                         switch_id=module.params['fabric'],
                         templ_type=module.params['template_type'],
-                        ident_pool_name=module.params['mac_pool'],
+                        cdn_source=module.params['cdn_source'],
+                        admin_cdn_name=module.params['cdn_name'],
                         mtu=module.params['mtu'],
+                        ident_pool_name=module.params['mac_pool'],
                         qos_policy_name=module.params['qos_policy'],
+                        nw_ctrl_policy_name=module.params['network_control_policy'],
+                        pin_to_group_name=module.params['pin_group'],
                         stats_policy_name=module.params['stats_policy'],
-                        nw_ctrl_policy_name=module.params['network_ctrl_policy'],
                     )
 
-                    mo_1 = VnicEtherIf(
-                        parent_mo_or_dn=mo,
-                        name=module.params['vlan'],
-                        default=module.params['native'],
-                    )
+                    if module.params.get('vlans_list'):
+                        for vlan in module.params['vlans_list']:
+                            mo_1 = VnicEtherIf(
+                                parent_mo_or_dn=mo,
+                                name=vlan['name'],
+                                default_net=vlan['native'],
+                            )
 
                     ucs.login_handle.add_mo(mo, True)
                     ucs.login_handle.commit()
