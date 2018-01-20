af67146062a612a5215361ca4010ee54482c368f
Dag Wieers <dag@wieers.com>
2018-01-20 01:15:59 +0100

    ACI: Add ACI version information to VMM domain docs (#35114)

--- lib/ansible/modules/network/aci/aci_domain.py
+++ lib/ansible/modules/network/aci/aci_domain.py
@@ -59,6 +59,8 @@ options:
   vm_provider:
     description:
     - The VM platform for VMM Domains.
+    - Support for Kubernetes was added in ACI v3.0.
+    - Support for CloudFoundry, OpenShift and Red Hat was added in ACI v3.1.
     choices: [ cloudfoundry, kubernetes, microsoft, openshift, openstack, redhat, vmware ]
   vswitch:
     description:
