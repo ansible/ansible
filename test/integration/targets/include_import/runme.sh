b87880d277eed4a861adb9240e76b937285f0509
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-01-20 15:02:08 +0100

    Test to exhibit a bug with include_role and tags

--- test/integration/targets/include_import/runme.sh
+++ test/integration/targets/include_import/runme.sh
@@ -81,3 +81,8 @@ ANSIBLE_STRATEGY='free' ansible-playbook tasks/test_include_dupe_loop.yml -i ../
 test "$(grep -c '"item=foo"' test_include_dupe_loop.out)" = 3
 
 ansible-playbook public_exposure/playbook.yml -i ../../inventory "$@"
+
+## Include roles with tags in meta/main.yml make a role run more than neccesary
+# https://github.com/ansible/ansible/pull/35166
+ANSIBLE_STRATEGY='linear' ansible-playbook playbook/cached_role.yml -i ../../inventory -v "$@"
+ANSIBLE_STRATEGY='free' ansible-playbook playbook/cached_role.yml -i ../../inventory -v "$@"
