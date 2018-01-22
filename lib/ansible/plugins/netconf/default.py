365d73f19b02d2b6d70dd6efebb9acf62a89187a
Peter Sprygada <psprygada@ansible.com>
2018-01-22 18:17:22 -0500

    fix up pep8 issues

--- lib/ansible/plugins/netconf/default.py
+++ lib/ansible/plugins/netconf/default.py
@@ -20,21 +20,9 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 import json
-import re
 
-from ansible import constants as C
 from ansible.module_utils._text import to_text, to_bytes
-from ansible.errors import AnsibleConnectionFailure, AnsibleError
 from ansible.plugins.netconf import NetconfBase
-from ansible.plugins.netconf import ensure_connected
-
-try:
-    from ncclient import manager
-    from ncclient.operations import RPCError
-    from ncclient.transport.errors import SSHUnknownHostError
-    from ncclient.xml_ import to_ele, to_xml, new_ele
-except ImportError:
-    raise AnsibleError("ncclient is not installed")
 
 
 class Netconf(NetconfBase):
@@ -50,16 +38,6 @@ class Netconf(NetconfBase):
         device_info['network_os'] = 'default'
         return device_info
 
-    #@ensure_connected
-    #def execute_rpc(self, name):
-    #    """RPC to be execute on remote device
-    #       :name: Name of rpc in string format"""
-    #    try:
-    #        obj = to_ele(to_bytes(name, errors='surrogate_or_strict'))
-    #        return self.m.rpc(obj).data_xml
-    #    except RPCError as exc:
-    #        raise Exception(to_xml(exc.xml))
-
     def get_capabilities(self):
         result = dict()
         result['rpc'] = self.get_base_rpc() + ['commit', 'discard_changes', 'validate', 'lock', 'unlock', 'copy_copy',
@@ -71,18 +49,3 @@ class Netconf(NetconfBase):
         result['client_capabilities'] = [c for c in self.m.client_capabilities]
         result['session_id'] = self.m.session_id
         return json.dumps(result)
-
-    #def __getattr__(self, name):
-    #    try:
-    #        return getattr(self, m, name)(*args, **kwargs).data_xml
-    #        return partial(self.__rpc__, name)
-    #    except RPCError as exc:
-    #        raise Exception(to_xml(exc.xml))
-
-    #        return self.__dict__[name]
-    #    except KeyError:
-    #        if name.startswith('_'):
-    #            raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, name))
-    #        return partial(self.__rpc__, name)
-
-
