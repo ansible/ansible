686fb8d8b43c50026cb43616139e96914ae237e2
Sloane Hertel <shertel@redhat.com>
2018-01-23 18:55:56 -0500

    Fix aws_account_attribute lookup credentials bug for access keys (#35242)

--- lib/ansible/plugins/lookup/aws_account_attribute.py
+++ lib/ansible/plugins/lookup/aws_account_attribute.py
@@ -121,8 +121,8 @@ def _boto3_conn(region, credentials):
 def _get_credentials(options):
     credentials = {}
     credentials['boto_profile'] = options['boto_profile']
-    credentials['aws_secret_access_key'] = options['aws_access_key']
-    credentials['aws_access_key_id'] = options['aws_secret_key']
+    credentials['aws_secret_access_key'] = options['aws_secret_key']
+    credentials['aws_access_key_id'] = options['aws_access_key']
     credentials['aws_session_token'] = options['aws_security_token']
 
     return credentials
