21169b22282fe9b7efcf7d98a5be4323e1d722da
Peter Sprygada <privateip@users.noreply.github.com>
2018-01-24 08:28:21 -0500

    adds two new infoblox lookup plugins (#35189)
    * adds two new infoblox lookup plugins
    
    * nios - lookup plugin to return nios objects to the playbook
    * nios_next_ip - lookup plugin to return the next avaiable ip address
    
    * adds some additional examples to nios lookup
    
    * fix up pep8 failures
    
    * updates in response to review comments

--- lib/ansible/utils/module_docs_fragments/nios.py
+++ lib/ansible/utils/module_docs_fragments/nios.py
@@ -31,35 +31,49 @@ options:
         description:
           - Specifies the DNS host name or address for connecting to the remote
             instance of NIOS WAPI over REST
+          - Value can also be specified using C(INFOBLOX_HOST) environment
+            variable.
         required: true
       username:
         description:
           - Configures the username to use to authenticate the connection to
             the remote instance of NIOS.
+          - Value can also be specified using C(INFOBLOX_USERNAME) environment
+            variable.
       password:
         description:
           - Specifies the password to use to authenticate the connection to
             the remote instance of NIOS.
+          - Value can also be specified using C(INFOBLOX_PASSWORD) environment
+            variable.
         default: null
       ssl_verify:
         description:
           - Boolean value to enable or disable verifying SSL certificates
+          - Value can also be specified using C(INFOBLOX_SSL_VERIFY) environment
+            variable.
         required: false
         default: false
       http_request_timeout:
         description:
           - The amount of time before to wait before receiving a response
+          - Value can also be specified using C(INFOBLOX_HTTP_REQUEST_TIMEOUT) environment
+            variable.
         required: false
         default: 10
       max_retries:
         description:
           - Configures the number of attempted retries before the connection
             is declared usable
+          - Value can also be specified using C(INFOBLOX_MAX_RETRIES) environment
+            variable.
         required: false
         default: 3
       wapi_version:
         description:
           - Specifies the version of WAPI to use
+          - Value can also be specified using C(INFOBLOX_WAP_VERSION) environment
+            variable.
         required: false
         default: 1.4
 """
