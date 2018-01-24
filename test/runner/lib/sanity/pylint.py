e72d2c4633ee3f0f347c4b0c7d5c0262ed57156b
Matt Clay <matt@mystile.com>
2018-01-24 10:22:04 -0800

    Miscellaneous test fixes. (#35301)
    * Add missing pylint test for invalid path.
    * Fix syntax in integration test.
    * Use Write-Output in win_script test script.
    * Fix pylint in explain mode.

--- test/runner/lib/sanity/pylint.py
+++ test/runner/lib/sanity/pylint.py
@@ -91,6 +91,10 @@ class PylintTest(SanitySingleVersion):
 
                 path, code = ignore_entry.split(' ', 1)
 
+                if not os.path.exists(path):
+                    invalid_ignores.append((line, 'Remove "%s" since it does not exist' % path))
+                    continue
+
                 if ' ' in code:
                     code, version = code.split(' ', 1)
 
@@ -160,6 +164,9 @@ class PylintTest(SanitySingleVersion):
             level=m['type'],
             code=m['symbol'],
         ) for m in messages]
+
+        if args.explain:
+            return SanitySuccess(self.name)
 
         line = 0
 
