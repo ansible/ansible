747438d32afb6c796516bd30d297098f77fd6d89
Dag Wieers <dag@wieers.com>
2018-01-25 16:42:58 +0100

    Improve examples

--- lib/ansible/modules/windows/win_disk_facts.py
+++ lib/ansible/modules/windows/win_disk_facts.py
@@ -34,9 +34,20 @@ EXAMPLES = r'''
   debug:
     var: ansible_facts.disks[0].size
 
-- name: Convert first disk size into GiB
+- name: Convert first system disk into various formats
   debug:
-    msg: '{{ (ansible_facts.disks[0].size|filesizeformat }}Byte'
+    msg: '{{ disksize_gib }} vs {{ disksize_gib_human }}'
+  vars:
+    # Get first system disk
+    disk: '{{ ansible_facts.disks|selectattr("system_disk")|first }}'
+
+    # Show disk size in Gibibytes
+    disksize_gib_human: '{{ disk.size|filesizeformat(True) }}'   # returns "223.6 GiB" (human readable)
+    disksize_gib: '{{ (disk.size/1024|pow(3))|round|int }} GiB'  # returns "224 GiB" (value in GiB)
+
+    # Show disk size in Gigabytes
+    disksize_gb_human: '{{ disk.size|filesizeformat }}'        # returns "240.1 GB" (human readable)
+    disksize_gb: '{{ (disk.size/1000|pow(3))|round|int }} GB'  # returns "240 GB" (value in GB)
 
 - name: Output second disk serial number
   debug:
