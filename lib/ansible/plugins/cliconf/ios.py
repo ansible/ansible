77c4ba1782ff9ddca0e34495f8c6b4eff92776e0
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2018-01-26 00:19:09 +0100

    Fix edit_config multiline commands on IOS
    The current code for multiline commands in IOS is broken: If you
    pass a dict containing a command, prompt and answer it is seen
    later as unicode string, but if you do a json.loads it fails
    as the keys/values are enclosed in single quotes but JSON requires
    double quotes.
    
    Fixes #23539

--- lib/ansible/plugins/cliconf/ios.py
+++ lib/ansible/plugins/cliconf/ios.py
@@ -70,13 +70,13 @@ class Cliconf(CliconfBase):
     @enable_mode
     def edit_config(self, command):
         for cmd in chain(['configure terminal'], to_list(command), ['end']):
-            try:
-                cmd = json.loads(cmd)
+            if cmd[0] == '{':
+                cmd = json.loads(cmd.replace('\'','"'))
                 command = cmd['command']
                 prompt = cmd['prompt']
                 answer = cmd['answer']
                 newline = cmd.get('newline', True)
-            except:
+            else:
                 command = cmd
                 prompt = None
                 answer = None
