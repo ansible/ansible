d3b9d3cc6631c216a03f5c7607eeda7c8c6d4e11
Michael Tharp <gxti@partiallystapled.com>
2018-01-26 16:00:53 -0500

    rabbitmq_policy: fix IndexError when no policies exist yet (#33556)
    If the list of existing policies is empty, an IndexError would be raised
    since stdout was blank.

--- lib/ansible/modules/messaging/rabbitmq_policy.py
+++ lib/ansible/modules/messaging/rabbitmq_policy.py
@@ -113,6 +113,8 @@ class RabbitMqPolicy(object):
         policies = self._exec(['list_policies'], True)
 
         for policy in policies:
+            if not policy:
+                continue
             policy_name = policy.split('\t')[1]
             if policy_name == self._name:
                 return True
