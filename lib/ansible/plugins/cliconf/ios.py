813bd185943a9213a5c074860fad949c7028203d
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2018-01-26 15:43:29 +0100

    Fix pep8

--- lib/ansible/plugins/cliconf/ios.py
+++ lib/ansible/plugins/cliconf/ios.py
@@ -71,7 +71,7 @@ class Cliconf(CliconfBase):
     def edit_config(self, command):
         for cmd in chain(['configure terminal'], to_list(command), ['end']):
             if cmd[0] == '{':
-                cmd = json.loads(cmd.replace('\'','"'))
+                cmd = json.loads(cmd.replace('\'', '"'))
                 command = cmd['command']
                 prompt = cmd['prompt']
                 answer = cmd['answer']
