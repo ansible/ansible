fe2c8400ea1bbee27c1e2f6613a39a0e2730d7a5
Ryan Groten <rgroten@gmail.com>
2018-01-26 11:30:32 -0700

    Add -F to usermod -p to force user password update (#26258)
    If changing root password, usermod will fail with error "Login root is currently in use\n".  -F avoids this

--- lib/ansible/modules/system/user.py
+++ lib/ansible/modules/system/user.py
@@ -2297,6 +2297,7 @@ class HPUX(User):
             cmd.append(self.shell)
 
         if self.update_password == 'always' and self.password is not None and info[1] != self.password:
+            cmd.append('-F')
             cmd.append('-p')
             cmd.append(self.password)
 
