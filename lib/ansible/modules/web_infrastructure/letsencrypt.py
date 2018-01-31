5ea040c946589a421a9805f55695b6ce3c146b2d
Felix Fontein <felix@fontein.de>
2018-01-31 07:41:14 +0100

    Fixing key detection if key was generated with openssl_privatekey.

--- lib/ansible/modules/web_infrastructure/letsencrypt.py
+++ lib/ansible/modules/web_infrastructure/letsencrypt.py
@@ -566,6 +566,12 @@ class ACMEAccount(object):
                 if m is not None:
                     account_key_type = m.group(1).lower()
                     break
+        if account_key_type is None:
+            # This happens for example if openssl_privatekey created this key
+            # (as opposed to the OpenSSL binary). For now, we assume this is
+            # an RSA key.
+            # FIXME: add some kind of auto-detection
+            account_key_type = "rsa"
         if account_key_type not in ("rsa", "ec"):
             return 'unknown key type "%s" % account_key_type', {}
 
