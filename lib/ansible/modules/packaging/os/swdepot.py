5b823d1425170709f87ad527f614872e05851622
melankh <melankh@users.noreply.github.com>
2018-01-31 18:32:17 +0100

    Resolve E501 line too long

--- lib/ansible/modules/packaging/os/swdepot.py
+++ lib/ansible/modules/packaging/os/swdepot.py
@@ -97,9 +97,11 @@ def query_package(module, name, depot=None):
 
     cmd_list = '/usr/sbin/swlist -a revision -l product'
     if depot:
-        rc, stdout, stderr = module.run_command(r"%s -s %s %s | grep -v \# | grep %s" % (cmd_list, pipes.quote(depot), pipes.quote(name), pipes.quote(name)), use_unsafe_shell=True)
+        rc, stdout, stderr = module.run_command(r"%s -s %s %s | grep -v \# | grep %s" % (
+            cmd_list, pipes.quote(depot), pipes.quote(name), pipes.quote(name)), use_unsafe_shell=True)
     else:
-        rc, stdout, stderr = module.run_command(r"%s %s | grep -v \# | grep %s" % (cmd_list, pipes.quote(name), pipes.quote(name)), use_unsafe_shell=True)
+        rc, stdout, stderr = module.run_command(r"%s %s | grep -v \# | grep %s" % (
+            cmd_list, pipes.quote(name), pipes.quote(name)), use_unsafe_shell=True)
     if rc == 0:
         version = re.sub(r"\s\s+|\t", " ", stdout).strip().split()[1]
     else:
