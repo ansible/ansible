af7b48ac47ef95a535f85d83f2f53c55debdd1a9
Ganesh Nalawade <ganesh634@gmail.com>
2018-01-31 18:33:23 +0530

    Handle multiple sub prompts for network_cli connection (#35361)
    * Handle multiple sub prompts for network_cli connection
    
    Fixes #35349
    
    *  Check if the same prompt is repeated in consecutive window
       if it is repeated it indicates there is problem with answer
       provided
    *  In that case report error to user
    
    * Fix CI failure
    
    * Fixes #35349
    
    *  Add prompt_retry count to control max number of times
       to expect the same prompt before it error's out
    
    *  Make required changes in ios and eos terminal plugin to handle
       wrong enable password correctly and return proper error
       message to user.
    
    *  Check if the same prompt is repeated in consecutive window
       if it is repeated it indicates there is the problem with an answer
       provided
    
    *  In that case report error to user

--- lib/ansible/plugins/terminal/eos.py
+++ lib/ansible/plugins/terminal/eos.py
@@ -63,11 +63,16 @@ class TerminalModule(TerminalBase):
         if passwd:
             cmd[u'prompt'] = to_text(r"[\r\n]?password: $", errors='surrogate_or_strict')
             cmd[u'answer'] = passwd
+            cmd[u'prompt_retry_check'] = True
 
         try:
             self._exec_cli_command(to_bytes(json.dumps(cmd), errors='surrogate_or_strict'))
-        except AnsibleConnectionFailure:
-            raise AnsibleConnectionFailure('unable to elevate privilege to enable mode')
+            prompt = self._get_prompt()
+            if prompt is None or not prompt.endswith(b'#'):
+                raise AnsibleConnectionFailure('failed to elevate privilege to enable mode still at prompt [%s]' % prompt)
+        except AnsibleConnectionFailure as e:
+            prompt = self._get_prompt()
+            raise AnsibleConnectionFailure('unable to elevate privilege to enable mode, at prompt [%s] with error: %s' % (prompt, e.message))
 
     def on_unbecome(self):
         prompt = self._get_prompt()
