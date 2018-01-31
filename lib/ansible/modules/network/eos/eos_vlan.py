199046cd317f2a22bbdf946937981712cfdd8dd1
Jeremy <jlee@bgea.org>
2018-01-31 10:41:19 -0500

    Modified logic to allow for more than 6 interfaces to be assigned to a vlan

--- lib/ansible/modules/network/eos/eos_vlan.py
+++ lib/ansible/modules/network/eos/eos_vlan.py
@@ -199,24 +199,29 @@ def map_obj_to_commands(updates, module):
 
 def map_config_to_obj(module):
     objs = []
-    output = run_commands(module, ['show vlan'])
+    output = run_commands(module, ['show vlan conf'])
     lines = output[0].strip().splitlines()[2:]
 
     for l in lines:
         splitted_line = re.split(r'\s{2,}', l.strip())
-        obj = {}
-        obj['vlan_id'] = splitted_line[0]
-        obj['name'] = splitted_line[1]
-        obj['state'] = splitted_line[2]
-
-        if obj['state'] == 'suspended':
-            obj['state'] = 'suspend'
-
-        obj['interfaces'] = []
         if len(splitted_line) > 3:
+            obj = {}
+            obj['vlan_id'] = splitted_line[0]
+            obj['name'] = splitted_line[1]
+            obj['state'] = splitted_line[2]
+            obj['interfaces'] = []
 
             for i in splitted_line[3].split(','):
                 obj['interfaces'].append(i.strip().replace('Et', 'ethernet'))
+
+        elif len(splitted_line) < 3:
+            for i in splitted_line[0].split(','):
+                obj['interfaces'].append(i.strip().replace('Et', 'Ethernet'))
+        else:
+            # do nothing yet
+
+        if obj['state'] == 'suspended':
+            obj['state'] = 'suspend'
 
         objs.append(obj)
 
