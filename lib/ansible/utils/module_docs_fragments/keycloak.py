21f3b7b076e77498911320b1619eba669efa77a9
Eike Frost <ei@kefro.st>
2018-01-31 14:12:53 +0100

    New module: keycloak_clienttemplate (#33419)
    * keycloak_clienttemplate
    
    * BOTMETA maintainership for identity/keycloak namespace
    
    * fix superfluous blank line
    
    * catch ValueError when trying to decode JSON
    
    * further documentation for protocol mappers and some checks
    
    * whitespace fixes, YAML fixes
    
    * remove state: dump, update argument_spec and documentation with suboptions
    
    * add documentation for realm option
    
    * document aliases for auth_keycloak_url, auth_username, and auth_password (i.e. url, username, and password)
    
    * remove bearer_only, consent_required, standard_flow_enabled, implicit_flow_enabled, direct_access_grants_enabled, service_accounts_enabled, public_client, and frontchannel_logout from module options.

--- lib/ansible/utils/module_docs_fragments/keycloak.py
+++ lib/ansible/utils/module_docs_fragments/keycloak.py
@@ -25,6 +25,8 @@ options:
         description:
             - URL to the Keycloak instance.
         required: true
+        aliases:
+          - url
 
     auth_client_id:
         description:
@@ -39,21 +41,23 @@ options:
     auth_client_secret:
         description:
             - Client Secret to use in conjunction with I(auth_client_id) (if required).
-        required: false
 
     auth_username:
         description:
             - Username to authenticate for API access with.
         required: true
+        aliases:
+          - username
 
     auth_password:
         description:
             - Password to authenticate for API access with.
         required: true
+        aliases:
+          - password
 
     validate_certs:
         description:
             - Verify TLS certificates (do not disable this in production).
-        required: false
         default: True
 '''
