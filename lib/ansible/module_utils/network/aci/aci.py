e6a627321b99ad2ab84b7874fc6ae86808d37a04
Dag Wieers <dag@wieers.com>
2018-02-01 16:07:12 +0100

    Support enabled/disabled as well, with deprecation messages

--- lib/ansible/module_utils/network/aci/aci.py
+++ lib/ansible/module_utils/network/aci/aci.py
@@ -36,6 +36,7 @@ import json
 import os
 from copy import deepcopy
 
+from ansible.module_utils.parsing.convert_bool import boolean
 from ansible.module_utils.urls import fetch_url
 from ansible.module_utils._text import to_bytes
 
@@ -188,13 +189,23 @@ class ACIModule(object):
 
     def boolean(self, value, true='yes', false='no'):
         ''' Return an acceptable value back '''
-        if value == None:
+        if value is None:
             return None
-        elif value is True or value == true:
+        elif value is True:
             return true
-        elif value is False or value == false:
+        elif value is False:
             return false
-        else:
+        elif boolean(value) == True:  # When type=raw, this supports Ansible booleans
+            return true
+        elif boolean(value) == False:  # When type=raw, this supports Ansible booleans
+            return false
+        elif value == true:  # When type=raw, this supports the original boolean values
+            self.module.deprecate("Boolean value '%s' is no longer valid, please use 'yes' as a boolean value.", '2.9')
+            return true
+        elif value == false:  # When type=raw, this supports the original boolean values
+            self.module.deprecate("Boolean value '%s' is no longer valid, please use 'no' as a boolean value.", '2.9')
+            return false
+        else:  # When type=raw, escalate back to user
             self.module.fail_json(msg="Boolean value '%s' is an invalid ACI boolean value.")
 
     def iso8601_format(self, dt):
