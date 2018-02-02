56586ccd2eddd32283228e912536254e77a7fc4e
Dag Wieers <dag@wieers.com>
2018-02-03 00:41:56 +0100

    ACI: Change RETURN output as discussed (#35617)
    * ACI: Change result output as discussed
    
    * Update all modules to use new aci.exit_json()
    
    * Update output_level spec and docs
    
    * Fix integration tests
    
    * Small PEP8 fix
    
    * Asorted fixes to tests and aci_rest
    
    * More test fixes and support for ANSIBLE_DEBUG
    
    * Fix another PEP8 issues
    
    * Move response handling inside ACI module
    
    * Reform of ACI error handling and error output
    
    * Diff multiline json output
    
    * Fix a few more tests
    
    * Revert aci_bd tests
    
    * Small correction
    
    * UI change: existing->current, original->previous
    
    * UI change: config->sent
    
    * Update all modules with RETURN values
    
    * Fix a few more tests
    
    * Improve docstring and add 'raw' return value
    
    * Fix thinko
    
    * Fix sanity/pep8 issues
    
    * Rewrite unit tests to comply with new design

--- lib/ansible/utils/module_docs_fragments/aci.py
+++ lib/ansible/utils/module_docs_fragments/aci.py
@@ -45,15 +45,23 @@ options:
     required: yes
   private_key:
     description:
-    - PEM formatted file that contains your private key to be used for client certificate authentication.
+    - PEM formatted file that contains your private key to be used for signature-based authentication.
     - The name of the key (without extension) is used as the certificate name in ACI, unless C(certificate_name) is specified.
     aliases: [ cert_key ]
   certificate_name:
     description:
-    - The X.509 certificate name attached to the APIC AAA user.
+    - The X.509 certificate name attached to the APIC AAA user used for signature-based authentication.
     - It defaults to the C(private_key) basename, without extension.
     aliases: [ cert_name ]
     default: C(private_key) basename
+  output_level:
+    description:
+    - Influence the output of this ACI module.
+    - C(normal) means the standard output, incl. C(current) dict
+    - C(info) means informational output, incl. C(previous), C(proposed) and C(sent) dicts
+    - C(debug) means debugging output, incl. C(filter_string), C(method), C(response), C(status) and C(url) information
+    choices: [ debug, info, normal ]
+    default: normal
   timeout:
     description:
     - The socket level timeout in seconds.
@@ -61,8 +69,8 @@ options:
   use_proxy:
     description:
       - If C(no), it will not use a proxy, even if one is defined in an environment variable on the target hosts.
-    default: 'yes'
     type: bool
+    default: 'yes'
   use_ssl:
     description:
     - If C(no), an HTTP connection will be used instead of the default HTTPS connection.
