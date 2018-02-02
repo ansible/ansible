2169bdbacf9d7246f118d57887fb6c11ebb5a0ad
Dag Wieers <dag@wieers.com>
2018-02-03 00:41:56 +0100

    ACI: Change RETURN output as discussed (#35617)
    * ACI: Change result output as discussed
    
    * Update all modules to use new aci.exit_json()
    
    * Update output_level spec and docs
    
    * Fix integration tests
    
    * Small PEP8 fix
    
    * Asorted fixes to tests and aci_rest
    
    * More test fixes and support for ANSIBLE_DEBUG
    
    * Fix another PEP8 issues
    
    * Move response handling inside ACI module
    
    * Reform of ACI error handling and error output
    
    * Diff multiline json output
    
    * Fix a few more tests
    
    * Revert aci_bd tests
    
    * Small correction
    
    * UI change: existing->current, original->previous
    
    * UI change: config->sent
    
    * Update all modules with RETURN values
    
    * Fix a few more tests
    
    * Improve docstring and add 'raw' return value
    
    * Fix thinko
    
    * Fix sanity/pep8 issues
    
    * Rewrite unit tests to comply with new design

--- lib/ansible/modules/network/aci/aci_epg_monitoring_policy.py
+++ lib/ansible/modules/network/aci/aci_epg_monitoring_policy.py
@@ -62,7 +62,108 @@ EXAMPLES = r'''
 '''
 
 RETURN = r'''
-#
+current:
+  description: The existing configuration from the APIC after the module has finished
+  returned: success
+  type: list
+  sample:
+    [
+        {
+            "fvTenant": {
+                "attributes": {
+                    "descr": "Production environment",
+                    "dn": "uni/tn-production",
+                    "name": "production",
+                    "nameAlias": "",
+                    "ownerKey": "",
+                    "ownerTag": ""
+                }
+            }
+        }
+    ]
+error:
+  description: The error information as returned from the APIC
+  returned: failure
+  type: dict
+  sample:
+    {
+        "code": "122",
+        "text": "unknown managed object class foo"
+    }
+raw:
+  description: The raw output returned by the APIC REST API (xml or json)
+  returned: parse error
+  type: string
+  sample: '<?xml version="1.0" encoding="UTF-8"?><imdata totalCount="1"><error code="122" text="unknown managed object class foo"/></imdata>'
+sent:
+  description: The actual/minimal configuration pushed to the APIC
+  returned: info
+  type: list
+  sample:
+    {
+        "fvTenant": {
+            "attributes": {
+                "descr": "Production environment"
+            }
+        }
+    }
+previous:
+  description: The original configuration from the APIC before the module has started
+  returned: info
+  type: list
+  sample:
+    [
+        {
+            "fvTenant": {
+                "attributes": {
+                    "descr": "Production",
+                    "dn": "uni/tn-production",
+                    "name": "production",
+                    "nameAlias": "",
+                    "ownerKey": "",
+                    "ownerTag": ""
+                }
+            }
+        }
+    ]
+proposed:
+  description: The assembled configuration from the user-provided parameters
+  returned: info
+  type: dict
+  sample:
+    {
+        "fvTenant": {
+            "attributes": {
+                "descr": "Production environment",
+                "name": "production"
+            }
+        }
+    }
+filter_string:
+  description: The filter string used for the request
+  returned: failure or debug
+  type: string
+  sample: ?rsp-prop-include=config-only
+method:
+  description: The HTTP method used for the request to the APIC
+  returned: failure or debug
+  type: string
+  sample: POST
+response:
+  description: The HTTP response from the APIC
+  returned: failure or debug
+  type: string
+  sample: OK (30 bytes)
+status:
+  description: The HTTP status from the APIC
+  returned: failure or debug
+  type: int
+  sample: 200
+url:
+  description: The HTTP url used for the request to the APIC
+  returned: failure or debug
+  type: string
+  sample: https://10.11.12.13/api/mo/uni/tn-production.json
 '''
 
 from ansible.module_utils.network.aci.aci import ACIModule, aci_argument_spec
@@ -131,7 +232,7 @@ def main():
     elif state == 'absent':
         aci.delete_config()
 
-    module.exit_json(**aci.result)
+    aci.exit_json()
 
 
 if __name__ == "__main__":
