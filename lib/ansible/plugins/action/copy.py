dbf951b4e150ba81db9790649ea8c1d050adc5ec
Toshio Kuratomi <a.badger@gmail.com>
2018-02-05 13:20:33 -0800

    Remove explicit make_tmp_path() in copy
    The copy action plugin sets TRANSFER_FILES=True so it does not need to
    set the temporary directory explicitly; the base class's run() method
    will do that for us.

--- lib/ansible/plugins/action/copy.py
+++ lib/ansible/plugins/action/copy.py
@@ -393,9 +393,6 @@ class ActionModule(ActionBase):
         result = super(ActionModule, self).run(tmp, task_vars)
         del tmp  # tmp no longer has any effect
 
-        # Could be skipped by superclass __init__ if we don't need a tmp path early
-        self._make_tmp_path()
-
         source = self._task.args.get('src', None)
         content = self._task.args.get('content', None)
         dest = self._task.args.get('dest', None)
