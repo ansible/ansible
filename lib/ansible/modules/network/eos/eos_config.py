f1970260e251baf4a56de81672bd2458bea38e48
Trishna Guha <trishnaguha17@gmail.com>
2018-02-05 17:39:47 +0530

    address ganeshrn's comment - update doc
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/modules/network/eos/eos_config.py
+++ lib/ansible/modules/network/eos/eos_config.py
@@ -63,6 +63,8 @@ options:
         or relative to the root of the implemented role or playbook.
         This argument is mutually exclusive with the I(lines) and
         I(parents) arguments. It can be a Jinja2 template as well.
+        src file must have same indentation as a live switch config.
+        Arista EOS device config has 3 spaces indentation.
     required: false
     default: null
     version_added: "2.2"
