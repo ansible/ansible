ca09bcc87b3c0c164ceb32dba0e14cc86241e007
Sylvain Rabot <sylvain@abstraction.fr>
2018-02-06 05:10:05 +0100

    Fix typo in azure_rm_subnet (#35731)
    Signed-off-by: Sylvain Rabot <s.rabot@lectra.com>

--- lib/ansible/modules/cloud/azure/azure_rm_subnet.py
+++ lib/ansible/modules/cloud/azure/azure_rm_subnet.py
@@ -171,7 +171,7 @@ class AzureRMSubnet(AzureRMModuleBase):
         self.resource_group = None
         self.name = None
         self.state = None
-        self.virtual_etwork_name = None
+        self.virtual_network_name = None
         self.address_prefix_cidr = None
         self.security_group_name = None
 
