fab6498465e3eb8f5df51ddce38f47ebcbb1360e
Ansible Test Runner <noreply@example.com>
2018-02-06 11:42:24 -0500

    test_compat_platform.py cleanup

--- test/units/module_utils/test_compat_platform.py
+++ test/units/module_utils/test_compat_platform.py
@@ -11,8 +11,6 @@ import pytest
 
 from ansible.module_utils import compat_platform
 
-import pprint
-
 
 # to test fully need to mock:
 # open('/etc/lsb-release')
@@ -22,39 +20,37 @@ import pprint
 class TestDist:
     def test_dist(self):
         dist = compat_platform.dist()
-        pprint.pprint(dist)
         assert isinstance(dist, tuple), \
             "return of dist() is expected to be a tuple but was a %s" % type(dist)
 
     def test_dist_all_none(self):
         with pytest.raises(TypeError):
-            dist = compat_platform.dist(distname=None, version=None, id=None, supported_dists=None)
-            pprint.pprint(dist)
+            compat_platform.dist(distname=None, version=None, id=None, supported_dists=None)
 
     def test_dist_empty_supported_dists(self):
         dist = compat_platform.dist(supported_dists=tuple())
-        pprint.pprint(dist)
+        assert dist == ('', '', ''), \
+            "no supported dists were provided so dist() should have returned ('', '', '')"
 
 
 # TODO: test that we dont show deprecation warnings
 class TestLinuxDistribution:
     def test_linux_distribution(self):
         linux_dist = compat_platform.linux_distribution()
-        pprint.pprint(linux_dist)
+        # This will be empty unknown for non linux
         assert isinstance(linux_dist, tuple), \
             "return of linux_distribution() is expected to be a tuple but was a %s" % type(linux_dist)
 
     def test_linux_distribution_all_none(self):
         with pytest.raises(TypeError):
-            linux_dist = compat_platform.linux_distribution(distname=None, version=None,
-                                                            id=None, supported_dists=None,
-                                                            full_distribution_name=None)
-            pprint.pprint(linux_dist)
+            compat_platform.linux_distribution(distname=None, version=None,
+                                               id=None, supported_dists=None,
+                                               full_distribution_name=None)
 
     def test_linux_distribution_empty_supported_dists(self):
         linux_dist = compat_platform.linux_distribution(supported_dists=tuple())
-        pprint.pprint(linux_dist)
         assert linux_dist[0] == ''
         assert linux_dist[1] == ''
         assert linux_dist[2] == ''
-
+        assert linux_dist[0] == linux_dist[1] == linux_dist[2] == '', \
+            "linux_distribtion was expected to return ('', '', '') with no supported_dists"
