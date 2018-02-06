db96fc89fc0ebc521046ca6c44dade2f493f9b85
zikalino <zikalino@microsoft.com>
2018-02-06 15:16:33 +0800

    added things that went missing during rebase

--- lib/ansible/modules/cloud/azure/azure_rm_keyvaultkey.py
+++ lib/ansible/modules/cloud/azure/azure_rm_keyvaultkey.py
@@ -90,9 +90,13 @@ state:
 from ansible.module_utils.azure_rm_common import AzureRMModuleBase
 
 try:
-    from azure.keyvault import KeyVaultClient, KeyVaultAuthentication, KeyVaultId
+    import re
+    import codecs
+    from azure.keyvault import KeyVaultClient, KeyVaultId
+    from azure.keyvault.models import KeyAttributes, JsonWebKey
     from azure.common.credentials import ServicePrincipalCredentials
     from azure.keyvault.models.key_vault_error import KeyVaultErrorException
+    from OpenSSL import crypto
 except ImportError:
     # This is handled in azure_rm_common
     pass
@@ -106,6 +110,9 @@ class AzureRMKeyVaultKey(AzureRMModuleBase):
         self.module_arg_spec = dict(
             key_name=dict(type='str', required=True),
             keyvault_uri=dict(type='str', required=True),
+            pem_file=dict(type='str'),
+            pem_password=dict(type='str'),
+            byok_file=dict(type='str'),
             state=dict(type='str', default='present', choices=['present', 'absent'])
         )
 
@@ -116,12 +123,19 @@ class AzureRMKeyVaultKey(AzureRMModuleBase):
 
         self.key_name = None
         self.keyvault_uri = None
+        self.pem_file = None
+        self.pem_password = None
         self.state = None
         self.client = None
         self.tags = None
 
+        required_if = [
+            ('pem_password', 'present', ['pem_file'])
+        ]
+
         super(AzureRMKeyVaultKey, self).__init__(self.module_arg_spec,
                                                  supports_check_mode=True,
+                                                 required_if=required_if,
                                                  supports_tags=True)
 
     def exec_module(self, **kwargs):
@@ -162,6 +176,11 @@ class AzureRMKeyVaultKey(AzureRMModuleBase):
                 results['key_id'] = self.delete_key(self.key_name)
                 self.results['state'] = results
                 self.results['state']['status'] = 'Deleted'
+        else:
+            if self.state == 'present' and changed:
+                self.results['state']['status'] = 'Created'
+            elif self.state == 'absent' and changed:
+                self.results['state']['status'] = 'Deleted'
 
         return self.results
 
@@ -183,6 +202,79 @@ class AzureRMKeyVaultKey(AzureRMModuleBase):
         deleted_key = self.client.delete_key(self.keyvault_uri, name)
         key_id = KeyVaultId.parse_key_id(deleted_key.key.kid)
         return key_id.id
+
+    def import_key(self, key_name, destination=None, key_ops=None, disabled=False, expires=None,
+                   not_before=None, tags=None, pem_file=None, pem_password=None, byok_file=None):
+        """ Import a private key. Supports importing base64 encoded private keys from PEM files.
+            Supports importing BYOK keys into HSM for premium KeyVaults. """
+
+        def _to_bytes(hex_string):
+            # zero pads and decodes a hex string
+            if len(hex_string) % 2:
+                hex_string = '0{}'.format(hex_string)
+            return codecs.decode(hex_string, 'hex_codec')
+
+        def _set_rsa_parameters(dest, src):
+            # map OpenSSL parameter names to JsonWebKey property names
+            conversion_dict = {
+                'modulus': 'n',
+                'publicExponent': 'e',
+                'privateExponent': 'd',
+                'prime1': 'p',
+                'prime2': 'q',
+                'exponent1': 'dp',
+                'exponent2': 'dq',
+                'coefficient': 'qi'
+            }
+            # regex: looks for matches that fit the following patterns:
+            #   integerPattern: 65537 (0x10001)
+            #   hexPattern:
+            #      00:a0:91:4d:00:23:4a:c6:83:b2:1b:4c:15:d5:be:
+            #      d8:87:bd:c9:59:c2:e5:7a:f5:4a:e7:34:e8:f0:07:
+            # The desired match should always be the first component of the match
+            regex = re.compile(r'([^:\s]*(:[^\:)]+\))|([^:\s]*(:\s*[0-9A-Fa-f]{2})+))')
+            # regex2: extracts the hex string from a format like: 65537 (0x10001)
+            regex2 = re.compile(r'(?<=\(0x{1})([0-9A-Fa-f]*)(?=\))')
+
+            key_params = crypto.dump_privatekey(crypto.FILETYPE_TEXT, src).decode('utf-8')
+            for match in regex.findall(key_params):
+                comps = match[0].split(':', 1)
+                name = conversion_dict.get(comps[0], None)
+                if name:
+                    value = comps[1].replace(' ', '').replace('\n', '').replace(':', '')
+                    try:
+                        value = _to_bytes(value)
+                    except Exception:  # pylint:disable=broad-except
+                        # if decoding fails it is because of an integer pattern. Extract the hex
+                        # string and retry
+                        value = _to_bytes(regex2.findall(value)[0])
+                    setattr(dest, name, value)
+
+        key_attrs = KeyAttributes(not disabled, not_before, expires)
+        key_obj = JsonWebKey(key_ops=key_ops)
+        if pem_file:
+            key_obj.kty = 'RSA'
+            with open(pem_file, 'r') as f:
+                pem_data = f.read()
+            # load private key and prompt for password if encrypted
+            try:
+                pem_password = str(pem_password).encode() if pem_password else None
+                # despite documentation saying password should be a string, it needs to actually
+                # be UTF-8 encoded bytes
+                pkey = crypto.load_privatekey(crypto.FILETYPE_PEM, pem_data, pem_password)
+            except crypto.Error:
+                pass  # wrong password
+            except TypeError:
+                pass  # no pass provided
+            _set_rsa_parameters(key_obj, pkey)
+        elif byok_file:
+            with open(byok_file, 'rb') as f:
+                byok_data = f.read()
+            key_obj.kty = 'RSA-HSM'
+            key_obj.t = byok_data
+
+        return self.client.import_key(
+            self.keyvault_uri, key_name, key_obj, destination == 'hsm', key_attrs, tags)
 
 
 def main():
