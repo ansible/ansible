33be0176fea4abb5e40496e9fb3b75d8bbfaaf38
Chad Norgan <chad@norgan.email>
2018-02-07 09:36:26 -0600

    [WIP] Add new network module for EdgeOS (#33109)
    * Add edgeos network module using network_cli
    
    * Add documentation and some sanity test fixes
    
    * Update copyright and documentation

---
+++ test/units/modules/network/edgeos/test_edgeos_command.py
@@ -0,0 +1,36 @@
+# (c) 2017 Red Hat Inc.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+import json
+
+from ansible.compat.tests.mock import patch
+from ansible.modules.network.edgeos import edgeos_command
+from units.modules.utils import set_module_args
+from .edgeos_module import TestEdgeosModule, load_fixture
+
+
+class TestEdgeosCommandModule(TestEdgeosModule):
+
+    module = edgeos_command
+
+    def setUp(self):
+        super(TestEdgeosCommandModule, self).setUp()
+        self.mock_run_commands = patch()
