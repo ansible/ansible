ca0af4ba215c1194b9466437bf9cb6aeb30fd6b6
Sloane Hertel <shertel@redhat.com>
2018-02-07 21:05:51 -0500

    [ecs_service_facts] replace map with dict for Python3 compatibility (#35355)

--- lib/ansible/modules/cloud/amazon/ecs_service_facts.py
+++ lib/ansible/modules/cloud/amazon/ecs_service_facts.py
@@ -167,7 +167,9 @@ class EcsServiceManager:
             fn_args['cluster'] = cluster
         fn_args['services'] = services.split(",")
         response = self.ecs.describe_services(**fn_args)
-        relevant_response = dict(services=map(self.extract_service_from, response['services']))
+        relevant_response = {'services': []}
+        for service in response.get('services', []):
+            relevant_response['services'].append(self.extract_service_from(service))
         if 'failures' in response and len(response['failures']) > 0:
             relevant_response['services_not_running'] = response['failures']
         return relevant_response
