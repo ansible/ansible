c8e693e08362aa5286ba425cb0e5f1cb8af36f75
rush <github@avaika.me>
2018-02-08 18:45:16 +0300

    remove leading dot in efs_facts mountpoint

--- lib/ansible/modules/cloud/amazon/efs_facts.py
+++ lib/ansible/modules/cloud/amazon/efs_facts.py
@@ -235,7 +235,7 @@ class EFSConnection(object):
             Suffix of network path to be used as NFS device for mount. More detail here:
             http://docs.aws.amazon.com/efs/latest/ug/gs-step-three-connect-to-ec2-instance.html
             """
-            item['MountPoint'] = '.%s.efs.%s.amazonaws.com:/' % (item['FileSystemId'], self.region)
+            item['MountPoint'] = '%s.efs.%s.amazonaws.com:/' % (item['FileSystemId'], self.region)
             if 'Timestamp' in item['SizeInBytes']:
                 item['SizeInBytes']['Timestamp'] = str(item['SizeInBytes']['Timestamp'])
             if item['LifeCycleState'] == self.STATE_AVAILABLE:
