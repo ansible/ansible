27e4507955fccbb7c7f8d6756a37371585dbdf4f
David Newswanger <dnewswan@redhat.com>
2018-02-09 12:23:39 -0500

    added tests for network.common.utils

--- test/integration/targets/ios_smoke/tests/cli/common_utils.py
+++
@@ -1,4 +0,0 @@
-# ios_command -> ComplexList
-# ios_interface -> conditional, remove_default_spec
-# ios_logging -> validate_ip_address
-# ios_l3_interface -> is_netmask, is_masklen, to_subnet, to_netmask, to_masklen
