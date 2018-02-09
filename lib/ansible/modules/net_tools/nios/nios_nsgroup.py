be57584bfb035b0333f04a5b3ec5b3a7fd506ed3
Erich Birngruber <erich.birngruber@gmi.oeaw.ac.at>
2018-02-09 10:56:53 +0100

    fix version strings

--- lib/ansible/modules/net_tools/nios/nios_nsgroup.py
+++ lib/ansible/modules/net_tools/nios/nios_nsgroup.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-ANSIBLE_METADATA = {'metadata_version': '1.0',
+ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
@@ -18,7 +18,7 @@ short_description: manage InfoBlox DNS nameserver groups
 extends_documentation_fragment: nios
 author:
     - Erich Birngruber (@ebirn)
-version_added: "2.5"
+version_added: "2.6"
 description:
     - add or remove nameserver groups form Infoblox NIOS servers.
       This module manages NIOS C(nsgroup) objects using the Infoblox
