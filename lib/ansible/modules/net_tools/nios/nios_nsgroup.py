ecae14816b91f533d6cb7e38c8cd899ea1ba0508
Erich Birngruber <erich.birngruber@gmi.oeaw.ac.at>
2018-02-09 10:22:50 +0100

    fix doc test

--- lib/ansible/modules/net_tools/nios/nios_nsgroup.py
+++ lib/ansible/modules/net_tools/nios/nios_nsgroup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 
@@ -129,6 +131,7 @@ from ansible.module_utils.net_tools.nios.api import WapiModule
 
 
 def main():
+    ''' entrypoint for module execution'''
     argument_spec = dict(
         provider=dict(required=True),
         state=dict(default='present', choices=['present', 'absent']),
