bb0a676adff4b4339cf1ea2b332595ad441d67c9
amuraleedhar <amuraleedhar@lenovo.com>
2018-02-14 13:11:07 +0530

    Changing Lenovo Inc to Lenovo and update License file to be consistent.

--- lib/ansible/modules/network/enos/enos_facts.py
+++ lib/ansible/modules/network/enos/enos_facts.py
@@ -12,6 +12,9 @@
 # GNU General Public License for more details.
 #
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#
+#
 # Module to Collect facts from Lenovo Switches running Lenovo ENOS commands
 # Lenovo Networking
 #
