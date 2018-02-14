67a65ebb2a7ecf5b7c20e1ab90efc57ea917c032
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-02-14 16:14:05 +0530

    Changing Lenovo Inc to Lenovo and update License file to be consistent. (#36155)

--- lib/ansible/modules/network/enos/enos_config.py
+++ lib/ansible/modules/network/enos/enos_config.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
-# Copyright (C) 2017 Lenovo, Inc.
+# Copyright (C) 2017 Lenovo.
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#
+# Module to configure Lenovo Switches.
+# Lenovo Networking
+#
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
