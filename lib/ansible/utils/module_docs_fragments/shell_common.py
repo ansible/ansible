a612aee8b9138f201f378fa4e2830698c539201f
Toshio Kuratomi <a.badger@gmail.com>
2018-02-14 19:02:56 -0800

    Fix line too long

--- lib/ansible/utils/module_docs_fragments/shell_common.py
+++ lib/ansible/utils/module_docs_fragments/shell_common.py
@@ -19,7 +19,9 @@ options:
       - name: ansible_remote_tmp
   system_temps:
     description:
-       - List of valid system temporary directories for Ansible to choose when it cannot use ``remote_temp``, normally due to permission issues.  These must be world readable, writable, and executable.
+       - "List of valid system temporary directories for Ansible to choose when it cannot use
+         ``remote_temp``, normally due to permission issues.  These must be world readable, writable,
+         and executable."
     default: [ /var/tmp, /tmp ]
     type: list
     env: [{name: ANSIBLE_SYSTEM_TMPS}]
