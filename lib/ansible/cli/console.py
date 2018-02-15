7e32010bdb57fbe38ee0da5430c4fff9cb4d0d68
Adrian Likins <alikins@redhat.com>
2018-02-15 10:52:04 -0500

    Fix ansible-console always asking for vault passwords
    ansible-console was calling setup_vault_secrets
    twice. Once directly and once via _play_prereqs()
    
    The direct invocation was not setting auto_prompt=False.
    
    However, the direct invocation isn't need at all so
    this removes it so only _play_reqs() is used.
    That fixes the unrequested vault password
    prompting.
    
    Fixes #33027
    
    (cherry picked from commit 810fa7046bb886c37e0ba1135e1e36d2120f9e69)

--- lib/ansible/cli/console.py
+++ lib/ansible/cli/console.py
@@ -416,15 +416,6 @@ class ConsoleCLI(CLI, cmd.Cmd):
 
         self.loader, self.inventory, self.variable_manager = self._play_prereqs(self.options)
 
-        default_vault_ids = C.DEFAULT_VAULT_IDENTITY_LIST
-        vault_ids = self.options.vault_ids
-        vault_ids = default_vault_ids + vault_ids
-        vault_secrets = self.setup_vault_secrets(self.loader,
-                                                 vault_ids=vault_ids,
-                                                 vault_password_files=self.options.vault_password_files,
-                                                 ask_vault_pass=self.options.ask_vault_pass)
-        self.loader.set_vault_secrets(vault_secrets)
-
         hosts = CLI.get_host_list(self.inventory, self.options.subset, self.pattern)
 
         self.groups = self.inventory.list_groups()
