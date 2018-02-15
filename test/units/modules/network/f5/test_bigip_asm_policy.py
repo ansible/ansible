46ce57ee0389a0ae0596cc46bce0e75045a5d0a9
Tim Rupp <caphrim007@gmail.com>
2018-02-15 12:14:01 -0800

    Reduces the unit test time of select bigip modules (#36256)
    The modules in this patch include waits that need to happen to ensure
    something is correctly configured on a BIG-IP. These waits were
    raised as an issue in a recent ansible-testing meeting.
    
    This patch eliminates the waits by mocking time.sleep

--- test/units/modules/network/f5/test_bigip_asm_policy.py
+++ test/units/modules/network/f5/test_bigip_asm_policy.py
@@ -89,6 +89,11 @@ class TestManager(unittest.TestCase):
     def setUp(self):
         self.spec = ArgumentSpec()
         self.policy = os.path.join(fixture_path, 'fake_policy.xml')
+        self.patcher1 = patch('time.sleep')
+        self.patcher1.start()
+
+    def tearDown(self):
+        self.patcher1.stop()
 
     def test_activate_import_from_file(self, *args):
         set_module_args(dict(
