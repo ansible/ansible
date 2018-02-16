58cee97775089b9e4b0fb5662387d7cfd79bad9f
Mike Wiebe <mwiebe@cisco.com>
2018-02-16 11:04:26 -0500

    Fix structured output bug (#36193)
    (cherry picked from commit ac169f5ffd43f83b292bfe13e1a5815b1112dc5c)

--- lib/ansible/modules/network/nxos/nxos_user.py
+++ lib/ansible/modules/network/nxos/nxos_user.py
@@ -221,7 +221,10 @@ def parse_password(data):
 
 
 def parse_roles(data):
-    configured_roles = data.get('TABLE_role')['ROW_role']
+    configured_roles = None
+    if 'TABLE_role' in data:
+        configured_roles = data.get('TABLE_role')['ROW_role']
+
     roles = list()
     if configured_roles:
         for item in to_list(configured_roles):
