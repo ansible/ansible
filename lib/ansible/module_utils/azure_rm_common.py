949db85881d7bb9046b547993919414e1436c2b3
George Wilson <george.wilson@iproov.com>
2018-02-16 19:02:08 +0000

    Backport part of #33221 (commit: 8fa1373a67da1801bd1df121851d6a619d002499) to fix azure functionapp ansible modules fataling due to a bad URL

--- lib/ansible/module_utils/azure_rm_common.py
+++ lib/ansible/module_utils/azure_rm_common.py
@@ -758,7 +758,8 @@ class AzureRMModuleBase(object):
     def web_client(self):
         self.log('Getting web client')
         if not self._web_client:
-            self._web_client = self.get_mgmt_svc_client(WebSiteManagementClient, base_url=self.base_url)
+            self._web_client = self.get_mgmt_svc_client(WebSiteManagementClient,
+                                                        base_url=self._cloud_environment.endpoints.resource_manager)
         return self._web_client
 
     @property
