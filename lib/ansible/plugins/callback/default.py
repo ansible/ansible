c837783c18374d6d7cfc5c641f3bc2bd2a6e78f5
Brian Coca <bcoca@ansible.com>
2018-02-16 09:59:54 -0500

    remove extra fields from debug output
    fixes #35493
    
    updated tests

--- lib/ansible/plugins/callback/default.py
+++ lib/ansible/plugins/callback/default.py
@@ -68,7 +68,6 @@ class CallbackModule(CallbackBase):
     def v2_runner_on_ok(self, result):
 
         delegated_vars = result._result.get('_ansible_delegated_vars', None)
-        self._clean_results(result._result, result._task.action)
 
         if self._play.strategy == 'free' and self._last_task_banner != result._task._uuid:
             self._print_task_banner(result._task)
@@ -93,10 +92,9 @@ class CallbackModule(CallbackBase):
         if result._task.loop and 'results' in result._result:
             self._process_items(result)
         else:
+            self._clean_results(result._result, result._task.action)
 
             if (self._display.verbosity > 0 or '_ansible_verbose_always' in result._result) and '_ansible_verbose_override' not in result._result:
-                if result._task.action == 'debug' and 'changed' in result._result:
-                    del result._result['changed']
                 msg += " => %s" % (self._dump_results(result._result),)
             self._display.display(msg, color=color)
 
