96df310e68fe7c6ffff0b6782a58b3a32de203b0
René Moser <mail@renemoser.net>
2018-02-20 23:08:19 +0100

    cs_user: fix return user_api_secret for ACS v4.10 and later (#36447)

--- lib/ansible/modules/cloud/cloudstack/cs_user.py
+++ lib/ansible/modules/cloud/cloudstack/cs_user.py
@@ -400,6 +400,13 @@ class AnsibleCloudStackUser(AnsibleCloudStack):
                     if value == user['accounttype']:
                         self.result['account_type'] = key
                         break
+
+            # secretkey has been removed since CloudStack 4.10 from listUsers API
+            if self.module.params.get('keys_registered') and 'apikey' in user and 'secretkey' not in user:
+                user_keys = self.query_api('getUserKeys', id=user['id'])
+                if user_keys:
+                    self.result['user_api_secret'] = user_keys['userkeys'].get('secretkey')
+
         return self.result
 
 
