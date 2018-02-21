31bcf2efae1caacac691227de827415eb9f02998
Erich Birngruber <erich.birngruber@gmi.oeaw.ac.at>
2018-02-21 16:41:23 +0100

    extend documentation with suboptions for primary and secondary nameservers

--- lib/ansible/modules/net_tools/nios/nios_nsgroup.py
+++ lib/ansible/modules/net_tools/nios/nios_nsgroup.py
@@ -32,31 +32,136 @@ options:
     required: true
   grid_primary:
     description:
-      - This list of hosts is to be used as primary servers in this nameserver group. They must be grid members.
+      - This host is to be used as primary server in this nameserver group. It must be a grid member.
         This option is required when setting I(use_external_primaries) to C(false).
+    suboptions:
+      name:
+        description:
+          - Provide the name of the grid member to identify the host.
+        required: true
+      enable_preferred_primaries:
+        description:
+          - This flag represents whether the preferred_primaries field values of this member are used (see Infoblox WAPI docs).
+        default: false
+        type: bool
+      grid_replicate:
+        description:
+          - Use DNS zone transfers if set to C(True) or ID Grid Replication if set to C(False)
+        type: bool
+        default: false
+      lead:
+        description:
+          - This flag controls if the grid lead secondary nameserver performs zone transfers to non lead secondaries.
+        type: bool
+        default: false
+      stealth:
+        description:
+          - Configure the external nameserver as stealth server (without NS record) in the zones.
+        type: bool
+        default: false
   grid_secondaries:
     description:
      - Configures the list of grid member hosts that act as secondary nameservers.
-       This option is required when setting I(use_external_primaries) to C(true). 
+       This option is required when setting I(use_external_primaries) to C(true).
+    suboptions:
+      name:
+        description:
+          - Provide the name of the grid member to identify the host.
+        required: true
+      enable_preferred_primaries:
+        description:
+          - This flag represents whether the preferred_primaries field values of this member are used (see Infoblox WAPI docs).
+        default: false
+        type: bool
+      grid_replicate:
+        description:
+          - Use DNS zone transfers if set to C(True) or ID Grid Replication if set to C(False)
+        type: bool
+        default: false
+      lead:
+        description:
+          - This flag controls if the grid lead secondary nameserver performs zone transfers to non lead secondaries.
+        type: bool
+        default: false
+      stealth:
+        description:
+          - Configure the external nameserver as stealth server (without NS record) in the zones.
+        type: bool
+        default: false
+      preferred_primaries:
+        description:
+          - Provide a list of elements like in I(external_primaries) to set the precedence of preferred primary nameservers.
   is_grid_default:
     description:
       - If set to C(True) this nsgroup will become the default nameserver group for new zones.
+    type: bool
     required: false
     default: false
   use_external_primary:
     description:
       - This flag controls whether the group is using an external primary nameserver.
         Note that modification of this field requires passing values for I(grid_secondaries) and I(external_primaries).
+    type: bool
     required: false
     default: false
   external_primaries:
     description:
       - Configures a list of external nameservers (non-members of the grid).
         This option is required when setting I(use_external_primaries) to C(true).
+    suboptions:
+      address:
+        description:
+          - Configures the IP address of the external nameserver
+        required: true
+      name:
+        description:
+          - Set a label for the external nameserver
+        required: true
+      stealth:
+        description:
+          - Configure the external nameserver as stealth server (without NS record) in the zones.
+        type: bool
+        default: false
+      tsig_key_name:
+        description:
+          - Sets a label for the I(tsig_key) value
+      tsig_key_alg:
+        description:
+          - Provides the algorithm used for the I(tsig_key) in use.
+        choices: ['HMAC-MD5', 'HMAC-SHA256']
+        default: 'HMAC-MD5'
+      tsig_key:
+        description:
+          - Set a DNS TSIG key for the nameserver to secure zone transfers (AFXRs).
     required: false
   external_secondaries:
     description:
       - Allows to provide a list of external secondary nameservers, that are not members of the grid.
+    suboptions:
+      address:
+        description:
+          - Configures the IP address of the external nameserver
+        required: true
+      name:
+        description:
+          - Set a label for the external nameserver
+        required: true
+      stealth:
+        description:
+          - Configure the external nameserver as stealth server (without NS record) in the zones.
+        type: bool
+        default: false
+      tsig_key_name:
+        description:
+          - Sets a label for the I(tsig_key) value
+      tsig_key_alg:
+        description:
+          - Provides the algorithm used for the I(tsig_key) in use.
+        choices: ['HMAC-MD5', 'HMAC-SHA256']
+        default: 'HMAC-MD5'
+      tsig_key:
+        description:
+          - Set a DNS TSIG key for the nameserver to secure zone transfers (AFXRs).
   extattrs:
     description:
       - Allows for the configuration of Extensible Attributes on the
@@ -189,16 +294,16 @@ def main():
         address=dict(type='str', required=True, ib_req=True),
         name=dict(type='str', required=True, ib_req=True),
         stealth=dict(type='bool', default=False),
-        tsig_key=dict(type='str', default=''),
+        tsig_key=dict(type='str'),
         tsig_key_alg=dict(type='str', choices=['HMAC-MD5', 'HMAC-SHA256'], default='HMAC-MD5'),
-        tsig_key_name=dict(type='str', default='', )
+        tsig_key_name=dict(type='str', required=True)
     )
 
     memberserver_spec = dict(
+        name=dict(type='str', required=True, ib_req=True),
         enable_preferred_primaries=dict(type='bool', default=False),
         grid_replicate=dict(type='bool', default=False),
         lead=dict(type='bool', default=False),
-        name=dict(type='str', required=True, ib_req=True),
         preferred_primaries=dict(type='list', elements='dict', options=extserver_spec, default=[]),
         stealth=dict(type='bool', default=False),
     )
@@ -208,15 +313,14 @@ def main():
         grid_primary=dict(type='list', elements='dict', options=memberserver_spec,
                           transform=grid_primary_preferred_transform),
         grid_secondaries=dict(type='list', elements='dict', options=memberserver_spec,
-                              transform=grid_secondaries_preferred_primaries_transform, default=[]),
-        external_primaries=dict(type='list', elements='dict', options=extserver_spec, transform=ext_primaries_transform,
-                                default=[]),
+                              transform=grid_secondaries_preferred_primaries_transform),
+        external_primaries=dict(type='list', elements='dict', options=extserver_spec, transform=ext_primaries_transform),
         external_secondaries=dict(type='list', elements='dict', options=extserver_spec,
-                                  transform=ext_secondaries_transform, default=[]),
+                                  transform=ext_secondaries_transform),
         is_grid_default=dict(type='bool', default=False),
         use_external_primary=dict(type='bool', default=False),
         extattrs=dict(),
-        comment=dict(type='str', default=''),
+        comment=dict(type='str'),
     )
 
     argument_spec.update(ib_spec)
