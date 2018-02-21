fb2d8cf5612d71ce720ebc893d53b1e5eb36c96c
Trishna Guha <trishnaguha17@gmail.com>
2018-02-21 18:09:51 +0530

    address review

--- lib/ansible/modules/network/nxos/nxos_pim.py
+++ lib/ansible/modules/network/nxos/nxos_pim.py
@@ -34,11 +34,11 @@ options:
   ssm_range:
     description:
       - Configure group ranges for Source Specific Multicast (SSM).
-        Valid values are multicast addresses or the keyword 'none'
-        or keyword 'default'. 'none' removes all SSM group ranges.
-        'default' will set ssm_range to the default multicast address.
+        Valid values are multicast addresses or the keyword C(none)
+        or keyword C(default). C(none) removes all SSM group ranges.
+        C(default) will set ssm_range to the default multicast address.
         If you set multicast address, please ensure that it is not the
-        same as the 'default', otherwise use the 'default' option.
+        same as the C(default), otherwise use the C(default) option.
     required: true
 '''
 EXAMPLES = '''
