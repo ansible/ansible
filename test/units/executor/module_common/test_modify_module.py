df7668ae0d879f788e527e3002dadffac58ee7f5
Matt Martz <matt@sivel.net>
2018-02-22 16:32:52 -0600

    Modify test to work on 2.4 due to lacking templar kwarg

--- test/units/executor/module_common/test_modify_module.py
+++ test/units/executor/module_common/test_modify_module.py
@@ -10,8 +10,6 @@ import pytest
 from ansible.executor.module_common import modify_module
 from ansible.module_utils.six import PY2
 
-from test_module_common import templar
-
 
 FAKE_OLD_MODULE = b'''#!/usr/bin/python
 import sys
@@ -33,10 +31,10 @@ def test_shebang(fake_old_module_open):
     assert shebang == '#!/usr/bin/python'
 
 
-def test_shebang_task_vars(fake_old_module_open, templar):
+def test_shebang_task_vars(fake_old_module_open):
     task_vars = {
         'ansible_python_interpreter': '/usr/bin/python3'
     }
 
-    (data, style, shebang) = modify_module('fake_module', 'fake_path', {}, task_vars=task_vars, templar=templar)
+    (data, style, shebang) = modify_module('fake_module', 'fake_path', {}, task_vars=task_vars)
     assert shebang == '#!/usr/bin/python3'
