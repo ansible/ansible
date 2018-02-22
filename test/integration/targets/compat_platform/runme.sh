c6676e3d8606c92ab94cb048155dae04399ee929
Adrian Likins <alikins@redhat.com>
2018-02-22 10:08:01 -0500

    Better intg test for compat_platform

--- test/integration/targets/compat_platform/runme.sh
+++ test/integration/targets/compat_platform/runme.sh
@@ -5,11 +5,13 @@ set -eux
 MYTMPDIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 trap 'rm -rf "${MYTMPDIR}"' EXIT
 
-BUILTIN_OUTPUT="${MYTMPDIR}/builtin_output.txt"
-python -m platform| tee "${BUILTIN_OUTPUT}"
 
-VENDORED_OUTPUT="${MYTMPDIR}/vendored_output.txt"
-python -m ansible.module_utils.compat_platform| tee "${VENDORED_OUTPUT}"
+PLATFORM_TOOL_OUTPUT="${MYTMPDIR}/builtin_output.txt"
+./platform-tool.py | tee "${PLATFORM_TOOL_OUTPUT}"
+#python -m platform| tee "${BUILTIN_OUTPUT}"
 
-diff -u "${BUILTIN_OUTPUT}" "${VENDORED_OUTPUT}"
+#VENDORED_OUTPUT="${MYTMPDIR}/vendored_output.txt"
+#python -m ansible.module_utils.compat_platform| tee "${VENDORED_OUTPUT}"
+#
+#diff -u "${BUILTIN_OUTPUT}" "${VENDORED_OUTPUT}"
 
