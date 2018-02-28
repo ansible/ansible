75276b16fa8cb65705fd9faef545b9c6344831a2
Dag Wieers <dag@wieers.com>
2018-02-28 17:55:02 +0100

    Updated author information and copyright statement

--- lib/ansible/modules/cloud/vmware/vmware_guest_file_operation.py
+++ lib/ansible/modules/cloud/vmware/vmware_guest_file_operation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 # Copyright: (c) 2017, Stéphane Travassac <stravassac () gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
