24308e0039776ebfbab06c3d3d3ed6ac8f86beca
amuraleedhar <amuraleedhar@lenovo.com>
2018-02-28 17:42:58 +0530

    Editing the License text after Review of Lenovo Legal team.

--- lib/ansible/utils/module_docs_fragments/enos.py
+++ lib/ansible/utils/module_docs_fragments/enos.py
@@ -1,21 +1,15 @@
-# Copyright (C) 2017 Lenovo, Inc.
 #
-# This file is part of Ansible
+# Copyright © 2017 Lenovo. All rights reserved.
 #
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# GNU General Public License v3.0+
 #
-# Ansible is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
-
 
 class ModuleDocFragment(object):
 
