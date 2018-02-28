84853e40240033cfce8565a5abbd7bce8f4e4c4e
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-02-28 22:30:51 +0530

    Update enos_facts.py

--- lib/ansible/modules/network/enos/enos_facts.py
+++ lib/ansible/modules/network/enos/enos_facts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2017 Lenovo. All rights reserved.
+# Copyright (c) 2017 Lenovo. All rights reserved.
 #
 # GNU General Public License v3.0+
 #
