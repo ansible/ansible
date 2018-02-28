14022fddbf25a7507ea8e778d45eef362e6a691c
Kevin Breit <kevin.breit@kevinbreit.net>
2018-02-27 19:36:29 -0600

    Framework for creating an organization
    - Documentation example for organization creation
    - Framework exists for creating organizations, pending PR 36809
    - Created functions for HTTP calls
    - Renamed from dashboard.meraki.com to api.meraki.com
    - Added required_if for state

--- lib/ansible/modules/network/meraki/meraki_organization.py
+++ lib/ansible/modules/network/meraki/meraki_organization.py
@@ -16,34 +16,41 @@ ANSIBLE_METADATA = {
 DOCUMENTATION = '''
 ---
 module: meraki_organization
-
 short_description: Manage organizations in the Meraki cloud
-
 version_added: "2.6"
-
 description:
-    - Allows for creation, management, and visibility into organizations within Meraki
-    - More information about the Meraki API can be found at U(https://dashboard.meraki.com/api_docs).
+- Allows for creation, management, and visibility into organizations within Meraki
+
+notes:
+- More information about the Meraki API can be found at U(https://dashboard.meraki.com/api_docs).
+- Some of the options are likely only used for developers within Meraki
 
 options:
     auth_key:
         description:
-            - Authentication key provided by the dashboard. Required if environmental variable MERAKI_KEY is not set.
+        - Authentication key provided by the dashboard. Required if environmental variable MERAKI_KEY is not set.
     name:
         description:
-            - Organization ID of an organization
+        - Organization ID of an organization
+    host:
+        description:
+        - Hostname for Meraki dashboard
+        - Only useful for internal Meraki developers
+        type: string
+        default: 'api.meraki.com'
     use_proxy:
         description:
-            - If C(no), it will not use a proxy, even if one is defined in an environment variable on the target hosts.
+        - If C(no), it will not use a proxy, even if one is defined in an environment variable on the target hosts.
         type: bool
     use_ssl:
         description:
-            - If C(no), it will use HTTP. Otherwise it will use HTTPS.
+        - If C(no), it will use HTTP. Otherwise it will use HTTPS.
+        - Only useful for internal Meraki developers
         type: bool
-        default: yes
+        default: 'yes'
     output_level:
         description:
-            - Set amount of debug output during module execution
+        - Set amount of debug output during module execution
         choices: ['normal', 'debug']
 
 author:
@@ -51,16 +58,24 @@ author:
 '''
 
 EXAMPLES = '''
-- name: List organizations associated to your user
+- name: Query information about all organizations
   meraki_organization:
     auth_key: abc12345
     status: query
   delegate_to: localhost
 
-- name: Query information about an organization
+- name: Query information about a single organization named YourOrg
   meraki_organization:
     auth_key: abc12345
+    name: YourOrg
     status: query
+  delegate_to: localhost
+
+- name: Create a new organization named YourOrg
+  meraki_organization:
+    auth_key: abc12345
+    name: YourOrg
+    status: present
   delegate_to: localhost
 '''
 
@@ -73,16 +88,54 @@ response:
 '''
 
 import os
-from ansible.module_utils.basic import AnsibleModule, json, env_fallback, jsonify
+from ansible.module_utils.basic import AnsibleModule, json, env_fallback
 from ansible.module_utils.urls import fetch_url
 from ansible.module_utils._text import to_native
 
+def find_org_id(data, name):
+    ''' Find an organization's ID based on the provided name '''
+    for i in data:
+        if i['name'] == name:
+            return i['id']
+
+def meraki_request(module, result, url, method, headers, payload):
+    try:
+        resp, info = fetch_url(module, url,
+                               data=json.dumps(payload),
+                               headers=headers,
+                               method=method,
+                               use_proxy=module.params['use_proxy'],
+                               force=False,
+                               )
+    except Exception as e:
+        module.fail_json(msg=str(e), **debug_result)
+
+    # module.fail_json(msg=info['status'])
+    data = resp.read()
+    if module.params['output_level'] == 'debug':
+        debug_result['status'] = info['status']
+        debug_result['resp_headers'] = resp.headers
+        debug_result['response'] = data
+    response = json.loads(to_native(data))
+
+    if info['status'] >= 200 and info['status'] <= 299:
+        try:
+            result['message'] = response
+            if method == 'POST' or method == 'PUT':
+                if info['status'] == 201:
+                    result['changed'] = True
+        except:
+            module.fail_json(msg="Meraki dashboard didn't return JSON compatible data")
+    else:
+        module.fail_json(msg='{0}: {1} '.format(info['status'], info['body']), **result)
+    return response
 
 def main():
 
     # define the available arguments/parameters that a user can pass to
     # the module
     module_args = dict(auth_key=dict(type='str', no_log=True, fallback=(env_fallback, ['MERAKI_KEY'])),
+                       host=dict(type='str', default='api.meraki.com'),
                        name=dict(type='str'),
                        username=dict(type='str'),
                        state=dict(type='str', choices=['present', 'absent', 'query'], required=True),
@@ -110,6 +163,11 @@ def main():
                            )
     module.params['follow_redirects'] = 'all'
 
+    module.required_if=[
+                           ['state', 'present', ['name']],
+                           ['state', 'absent', ['name']],
+                       ]
+
     try:
         module.params['auth_key'] = os.environ['MERAKI_KEY']
     except KeyError:
@@ -126,21 +184,21 @@ def main():
     if module.check_mode:
         return result
 
+    # execute checks for argument completeness
+
     # manipulate or modify the state as needed (this is going to be the
     # part where your module will do what it needs to do)
     protocol = 'https'
     if module.params['use_ssl'] is not None and module.params['use_ssl'] is False:
         protocol = 'http'
-    host = 'dashboard.meraki.com'
+    host = 'api.meraki.com'
 
-    url = '{0}://dashboard.meraki.com/api/v0/organizations'.format(protocol)
+    url = '{0}://api.meraki.com/api/v0/organizations'.format(protocol)
     headers = {'Content-Type': 'application/json',
                'X-Cisco-Meraki-API-Key': module.params['auth_key'],
                }
 
     method = 'GET'
-    if module.params['state'] != 'query':
-        method = 'POST'
 
     if module.params['output_level'] == 'debug':
         debug_result=dict(url=url,
@@ -181,21 +239,29 @@ def main():
     response_items = None
     org_id = None
 
-    ''' Return all organizations'''
-    if module.params['state'] == 'query' and module.params['name'] is None:
+    if module.params['state'] == 'query' and module.params['name'] is None:  #Record all organizations, no matter what
         response_items = response
     elif module.params['state'] == 'query' and module.params['name']:
-        module.warn('If multiple organizations with the same name, only the first matching result will be returned')
+        module.warn('If there are multiple organizations with the same name, only the first matching result will be processed')
         for i in response:
             if i['name'] == module.params['name']:
                 response_items = i
                 break
 
-    ''' Return the id for further processing. Some of these checks may not be necessary
-    '''
-    if (module.params['state'] == 'present' or module.params['state'] == 'absent') and module.params['name']:
+    ''' Return the id for further processing. Some of these checks may not be necessary'''
+    if module.params['state'] == 'present' or module.params['state'] == 'absent':
         if response_items:
-            org_id = response_items['id']
+            org_id = find_org_id(response_items, module.params['name'])
+            # module.fail_json(msg=org_id)
+            if module.params['state'] == 'present':
+                module.fail_json(msg='Modifying an existing organization is not implemented')
+            elif module.params['state'] == 'absent':
+                module.fail_json(msg='Deleting an organization is not implemented')
+        else:
+            module.warn("Create module")
+            url = '{0}://{1}/api/v0/organizations'.format(protocol, module.params['host'])
+            payload = {'name': module.params['name']}
+            r = meraki_request(module, result, url, 'POST', headers, payload)
     elif module.params['state'] == 'query':
         result['message'] = response_items
 
