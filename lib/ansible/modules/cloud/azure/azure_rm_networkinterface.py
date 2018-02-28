ee86c266b2c770198d579063ebdb206118991bb5
zikalino <zikalino@microsoft.com>
2018-02-28 17:13:41 +0800

    another attempt to fix pep8 indentation

--- lib/ansible/modules/cloud/azure/azure_rm_networkinterface.py
+++ lib/ansible/modules/cloud/azure/azure_rm_networkinterface.py
@@ -503,11 +503,11 @@ class AzureRMNetworkInterface(AzureRMModuleBase):
         if changed:
             if self.state == 'present':
                 subnet = self.network_models.SubResource(
-                        '/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Network/virtualNetworks/{2}/subnets/{3}'.format(
-                                self.subscription_id,
-                                virtual_network_resource_group,
-                                virtual_network_name,
-                                self.subnet_name))
+                    '/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Network/virtualNetworks/{2}/subnets/{3}'.format(
+                        self.subscription_id,
+                        virtual_network_resource_group,
+                        virtual_network_name,
+                        self.subnet_name))
 
                 nic_ip_configurations = [
                     self.network_models.NetworkInterfaceIPConfiguration(
