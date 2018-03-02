84c4fd5c0480d92c366715b1ea97ee40f728df0e
Ryan S. Brown <sb@ryansb.com>
2018-03-02 09:31:07 -0500

    Require boto3/botocore at runtime instead of at import

--- lib/ansible/module_utils/aws/waiters/ec2.py
+++ lib/ansible/module_utils/aws/waiters/ec2.py
@@ -1,4 +1,7 @@
-import botocore.waiter as core_waiter
+try:
+    import botocore.waiter as core_waiter
+except ImportError:
+    pass # caught by HAS_BOTO3
 
 
 ec2_data = {
@@ -25,11 +28,15 @@ ec2_data = {
     }
 }
 
-ec2_models = core_waiter.WaiterModel(waiter_config=ec2_data)
+
+def model_for(name):
+    ec2_models = core_waiter.WaiterModel(waiter_config=ec2_data)
+    return ec2_models.get_waiter(name)
+
 
 waiters_by_name = {
     'route_table_exists': lambda ec2: core_waiter.Waiter(
         'route_table_exists',
-        ec2_models.get_waiter('RouteTableExists'),
+        model_for('RouteTableExists'),
         ec2.describe_route_tables)
 }
