89df157ae45dba6e114af222c764552fae727ea5
Patrick Marques <pmarques@users.noreply.github.com>
2018-03-03 21:33:07 +0000

    Fix Base64 decode TypeError (#36968)
    ```
    TypeError: expected bytes-like object, not str
    ```

--- lib/ansible/modules/cloud/digital_ocean/digital_ocean_sshkey.py
+++ lib/ansible/modules/cloud/digital_ocean/digital_ocean_sshkey.py
@@ -239,7 +239,7 @@ def core(module):
 
 def ssh_key_fingerprint(ssh_pub_key):
     key = ssh_pub_key.split(None, 2)[1]
-    fingerprint = hashlib.md5(base64.decodestring(key)).hexdigest()
+    fingerprint = hashlib.md5(base64.b64decode(key)).hexdigest()
     return ':'.join(a + b for a, b in zip(fingerprint[::2], fingerprint[1::2]))
 
 
