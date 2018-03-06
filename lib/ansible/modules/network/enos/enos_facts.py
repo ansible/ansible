23f15e6ff60809121f3daa01d54d9e4f9f71ed6d
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-03-06 23:32:46 +0530

    Lenovo license update (#36840)
    * Editing the License text after Review of Lenovo Legal team.

--- lib/ansible/modules/network/enos/enos_facts.py
+++ lib/ansible/modules/network/enos/enos_facts.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
+# (C) 2017 Red Hat Inc.
 # Copyright (C) 2017 Lenovo.
+#
 # GNU General Public License v3.0+
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-#
-#
 # Module to Collect facts from Lenovo Switches running Lenovo ENOS commands
 # Lenovo Networking
 #
