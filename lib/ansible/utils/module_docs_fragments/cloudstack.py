9e0fe8b946eb5c01dee8c88e805a77ceaaad184f
Rene Moser <mail@renemoser.net>
2018-03-07 11:09:08 +0100

    cloudstack: fix common E324 in docs (#37082)
    Notes about precedence of common args.
    Partly fixes E324.

--- lib/ansible/utils/module_docs_fragments/cloudstack.py
+++ lib/ansible/utils/module_docs_fragments/cloudstack.py
@@ -11,24 +11,35 @@ options:
   api_key:
     description:
       - API key of the CloudStack API.
+      - If not given, the C(CLOUDSTACK_KEY) env variable is considered.
+      - As the last option, the value is taken from the ini config file, also see the notes.
   api_secret:
     description:
       - Secret key of the CloudStack API.
+      - If not set, the C(CLOUDSTACK_SECRET) env variable is considered.
+      - As the last option, the value is taken from the ini config file, also see the notes.
   api_url:
     description:
       - URL of the CloudStack API e.g. https://cloud.example.com/client/api.
+      - If not given, the C(CLOUDSTACK_ENDPOINT) env variable is considered.
+      - As the last option, the value is taken from the ini config file, also see the notes.
   api_http_method:
     description:
-      - HTTP method used.
-    default: get
+      - HTTP method used to query the API endpoint.
+      - If not given, the C(CLOUDSTACK_METHOD) env variable is considered.
+      - As the last option, the value is taken from the ini config file, also see the notes.
+      - Fallback value is C(get) if not specified.
     choices: [ get, post ]
   api_timeout:
     description:
-      - HTTP timeout.
-    default: 10
+      - HTTP timeout in seconds.
+      - If not given, the C(CLOUDSTACK_TIMEOUT) env variable is considered.
+      - As the last option, the value is taken from the ini config file, also see the notes.
+      - Fallback value is 10 seconds if not specified.
   api_region:
     description:
       - Name of the ini section in the C(cloustack.ini) file.
+      - If not given, the C(CLOUDSTACK_REGION) env variable is considered.
     default: cloudstack
 requirements:
   - "python >= 2.6"
