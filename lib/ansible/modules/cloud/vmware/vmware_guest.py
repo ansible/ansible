817abdf409d0efacd175b0157a638637d0401c1d
balonik <balonik@users.noreply.github.com>
2018-03-07 17:03:51 +0100

    update as requested

--- lib/ansible/modules/cloud/vmware/vmware_guest.py
+++ lib/ansible/modules/cloud/vmware/vmware_guest.py
@@ -1482,10 +1482,8 @@ class PyVmomiHelper(PyVmomi):
         hostsystem = self.cache.get_esx_host(self.params['esxi_hostname'])
         if not hostsystem:
             self.module.fail_json(msg='Failed to find ESX host "%(esxi_hostname)s"' % self.params)
-        if hostsystem.runtime.connectionState == 'connected' and not hostsystem.runtime.inMaintenanceMode:
-            pass
-        else:
-            self.module.fail_json(msg='ESX host in invalid state.')
+        if hostsystem.runtime.connectionState != 'connected' or hostsystem.runtime.inMaintenanceMode:
+            self.module.fail_json(msg='ESXi "%(esxi_hostname)s" is in invalid state or in maintenance mode.' % self.params)
         return hostsystem
 
     def autoselect_datastore(self):
