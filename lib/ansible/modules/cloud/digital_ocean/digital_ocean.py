63d451d0740910a47a2cb9b4e8481fa939aa0755
Abhijeet Kasurde <akasurde@redhat.com>
2018-03-07 22:01:23 +0530

    DigitalOcean: typecast map function for Python3 (#37126)
    In Python2, `map` returns list whereas Python3, `map` function
     returns iterator. This fix typecast map function for Python3.
    
    Fixes: #37114
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/cloud/digital_ocean/digital_ocean.py
+++ lib/ansible/modules/cloud/digital_ocean/digital_ocean.py
@@ -307,7 +307,7 @@ class Droplet(JsonfyMixIn):
     @classmethod
     def list_all(cls):
         json = cls.manager.all_active_droplets()
-        return map(cls, json)
+        return list(map(cls, json))
 
 
 class SSH(JsonfyMixIn):
@@ -338,7 +338,7 @@ class SSH(JsonfyMixIn):
     @classmethod
     def list_all(cls):
         json = cls.manager.all_ssh_keys()
-        return map(cls, json)
+        return list(map(cls, json))
 
     @classmethod
     def add(cls, name, key_pub):
