09b9de338a9a5aa018bd094f394d6539960916e1
Matt Martz <matt@sivel.net>
2018-03-09 10:10:45 -0600

    Add test for loading a lookup plugin that shadows a keyword

--- test/integration/targets/shadowed_module/runme.sh
+++ test/integration/targets/shadowed_module/runme.sh
@@ -8,3 +8,7 @@ if [[ -z "$OUT" ]]; then
     echo "Fake tags module did not cause error"
     exit 1
 fi
+
+# This playbook calls a lookup which shadows a keyword.
+# This is an ok situation, and should not error
+ansible-playbook playbook_lookup.yml -i inventory -e @../../integration_config.yml "$@"
