29469d8bbdea2470084057e4c457b777415bd1e8
Samer Deeb <samerd@mellanox.com>
2018-03-12 16:33:54 +0000

    issue:36955 remove creating temp file for debug
    Signed-off-by: Samer Deeb <samerd@mellanox.com>

--- lib/ansible/modules/network/onyx/onyx_linkagg.py
+++ lib/ansible/modules/network/onyx/onyx_linkagg.py
@@ -246,9 +246,6 @@ class OnyxLinkAggModule(BaseOnyxModule):
             lag_summary = self._get_port_channels(if_type)
             if lag_summary:
                 self._parse_port_channels_summary(lag_type, lag_summary)
-        with open('/tmp/linagg.txt', 'w') as fp:
-            fp.write('current_config: %s\n' % self._current_config)
-            fp.write('required_config: %s\n' % self._required_config)
 
     def _get_interface_command_suffix(self, if_name):
         if if_name.startswith('Eth'):
