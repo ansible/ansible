023c04c0291a2b0d63e900a9427af93826fa65f6
amanvir@invertedi.com <amanvir@invertedi.com>
2018-03-13 13:31:20 -0400

    Fixes for failing build checks

--- lib/ansible/modules/cloud/azure/azure_rm_storageaccount_keys.py
+++ lib/ansible/modules/cloud/azure/azure_rm_storageaccount_keys.py
@@ -11,33 +11,39 @@ ANSIBLE_METADATA = {
 DOCUMENTATION = '''
 ---
 module: azure_rm_storageaccount_keys
-short_description: Retrieve Azure Storage Account Keys
+
 version_added: "2.5"
+
+short_description: Retrieve Azure Storage Account Keys.
+
 description:
-    - "Retrive keys for a Storage Account"
+    - Retrive keys for a Storage Account.
 
 options:
     resource_group:
         description:
             - This is the name of the resource group for which you want to retrieve Keys
         required: true
+        default: null
     name:
         description:
             - Name of the Storage Account instance.
         required: true
+        default: null
 
 extends_documentation_fragment:
     - azure
 
 author:
     - Amanvir Mundra (@amanvirmundra)
+
 '''
 
 EXAMPLES = '''
 - name: Get Storage account keys
-      azure_rm_storageaccount_keys:
-        resource_group: resource_group
-        name: account_name
+  azure_rm_storageaccount_keys:
+    resource_group: resource_group
+    name: name
 '''
 
 RETURN = '''
@@ -60,7 +66,7 @@ except ImportError:
 
 class AzureRMStorageAccountKeys(AzureRMModuleBase):
     def __init__(self):
-        
+
         self.module_arg_spec = dict(
             resource_group=dict(type='str', aliases=['resource_group_name'], required=True),
             name=dict(type='str', required=True)
@@ -73,12 +79,10 @@ class AzureRMStorageAccountKeys(AzureRMModuleBase):
         self.resource_group = None
         self.name = None
 
-        super(AzureRMStorageAccountKeys, self).__init__(self.module_arg_spec,
-                                                         supports_tags=False)
+        super(AzureRMStorageAccountKeys, self).__init__(self.module_arg_spec, supports_tags=False)
     
-
     def exec_module(self, **kwargs):
-        """Main module execution method"""
+
         for key in self.module_arg_spec:
             setattr(self, key, kwargs[key])
 
@@ -86,16 +90,16 @@ class AzureRMStorageAccountKeys(AzureRMModuleBase):
             account_keys = self.storage_client.storage_accounts.list_keys(self.resource_group, self.name)
         except CloudError:
             pass
-        storage_keys = {v.key_name: v.value for v in account_keys.keys}
 
-        self.results['key1'] = storage_keys['key1']
-        self.results['key2'] = storage_keys['key2']
-        
+        self.results['Key1'] = account_keys.keys[0].value
+        self.results['Key2'] = account_keys.keys[1].value
+
         return self.results
 
 
 def main():
     AzureRMStorageAccountKeys()
+
 
 if __name__ == '__main__':
     main()
