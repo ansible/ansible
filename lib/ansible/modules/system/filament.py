98b66e936fd110ed033e06bffa11cb448e60ce1d
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-03-15 19:49:57 +0100

    Fix doc errors

--- lib/ansible/modules/system/filament.py
+++ lib/ansible/modules/system/filament.py
@@ -1,1 +1,55 @@
+DOCUMENTATION = """
+---
+module: my_sample_module
+
+short_description: This is my sample module
+
+version_added: "2.4"
+
+description:
+    - "This is my longer description explaining my sample module"
+
+options:
+    name:
+        description:
+            - This is the message to send to the sample module
+        required: true
+    new:
+        description:
+            - Control to demo if the result of this module is changed or not
+        required: false
+
+extends_documentation_fragment:
+    - azure
+
+author:
+    - Your Name (@yourhandle)
+"""
+
+EXAMPLES = """
+# Pass in a message
+- name: Test with a message
+  my_new_test_module:
+    name: hello world
+
+# pass in a message and have changed true
+- name: Test with a message and changed output
+  my_new_test_module:
+    name: hello world
+    new: true
+
+# fail the module
+- name: Test failure of the module
+  my_new_test_module:
+    name: fail me
+"""
+
+RETURN = """
+original_message:
+    description: The original name param that was passed in
+    type: str
+message:
+    description: The output message that the sample module generates
+"""
+
 hello = world
