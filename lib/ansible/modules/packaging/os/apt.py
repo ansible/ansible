c834085d47e228a1e4c512e26f08d56131d8d4ea
Toshio Kuratomi <a.badger@gmail.com>
2018-03-15 01:24:32 -0700

    Clarify the apt module force documentation (#37353)
    The force parameter is inherently insecure.  Be clearer in the
    documentation that this is the case.
    
    Fixes #25242

--- lib/ansible/modules/packaging/os/apt.py
+++ lib/ansible/modules/packaging/os/apt.py
@@ -60,6 +60,8 @@ options:
   force:
     description:
       - 'Corresponds to the C(--force-yes) to I(apt-get) and implies C(allow_unauthenticated: yes)'
+      - "This option will disable checking both the packages' signatures and the certificates of the
+        web servers they are downloaded from."
       - 'This option *is not* the equivalent of passing the C(-f) flag to I(apt-get) on the command line'
       - '**This is a destructive operation with the potential to destroy your system, and it should almost never be used.**
          Please also see C(man apt-get) for more information.'
