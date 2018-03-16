64af2cfff84386eab6608b9d94f42efcc8d9a460
alu <alu@ninja.co.jp>
2018-03-16 10:39:09 +0900

    Fix crash in py3: 'TypeError: a bytes-like object is required, not 'str'
    Fixes #37387

--- lib/ansible/modules/system/interfaces_file.py
+++ lib/ansible/modules/system/interfaces_file.py
@@ -320,7 +320,7 @@ def addOptionAfterLine(option, value, iface, lines, last_line_dict, iface_option
 def write_changes(module, lines, dest):
 
     tmpfd, tmpfile = tempfile.mkstemp()
-    f = os.fdopen(tmpfd, 'wb')
+    f = os.fdopen(tmpfd, 'w')
     f.writelines(lines)
     f.close()
     module.atomic_move(tmpfile, os.path.realpath(dest))
