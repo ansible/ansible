d2c183677dfba08cb0e8634fa1a122fd5f76d914
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-03-19 13:52:48 +0100

    Add metadata

--- lib/ansible/modules/system/filament.py
+++ lib/ansible/modules/system/filament.py
@@ -7,6 +7,10 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
 DOCUMENTATION = """
 ---
 module: filament
