7f077f183f4d69ef76893d87d61f96e5e65a7e34
Matt Martz <matt@sivel.net>
2018-03-20 09:11:14 -0500

    Drop fallback, use self.get_option

--- lib/ansible/plugins/callback/json.py
+++ lib/ansible/plugins/callback/json.py
@@ -91,7 +91,7 @@ class CallbackModule(CallbackBase):
             summary[h] = s
 
         custom_stats = {}
-        if self._plugin_options.get('show_custom_stats', C.SHOW_CUSTOM_STATS) and stats.custom:  # fallback on constants for inherited plugins missing docs
+        if self.get_option('show_custom_stats') and stats.custom:
             custom_stats.update(dict((self._convert_host_to_name(k), v) for k, v in stats.custom.items()))
             custom_stats.pop('_run', None)
 
