14b83d7d45665ca044044388a0e4f3d0dfdc8c9a
Florian Paul Hoberg <florianpaulhoberg@users.noreply.github.com>
2018-03-21 12:57:49 +0100

    fixed trailing whitespace
     fixed trailing whitespace

--- lib/ansible/modules/system/java_cert.py
+++ lib/ansible/modules/system/java_cert.py
@@ -42,7 +42,7 @@ options:
   trust_cacert:
     description:
       - Trust imported cert as CAcert
-    default: False 
+    default: False
     version_added: "2.6"
   pkcs12_path:
     description:
