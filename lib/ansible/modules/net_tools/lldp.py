71736a43ab70d4c2fd16ac30f972f5ef8981f9bc
Matt Clay <matt@mystile.com>
2018-03-21 09:41:01 -0700

    Clean up non-standard module shebangs.

--- lib/ansible/modules/net_tools/lldp.py
+++ lib/ansible/modules/net_tools/lldp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -tt
+#!/usr/bin/python
 #
 # Copyright: Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
--- lib/ansible/modules/packaging/os/apt_rpm.py
+++ lib/ansible/modules/packaging/os/apt_rpm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -tt
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 # Copyright: (c) 2013, Evgenii Terechkov
