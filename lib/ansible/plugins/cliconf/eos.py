5d73f58418debd9d92a75507eeeb3e8a0c4c37c9
Nathaniel Case <ncase@redhat.com>
2018-03-21 10:01:48 -0400

    send_command casts to bytes anyway, so no need to have bytes in cliconf

--- lib/ansible/plugins/cliconf/eos.py
+++ lib/ansible/plugins/cliconf/eos.py
@@ -23,7 +23,6 @@ import json
 
 from itertools import chain
 
-from ansible.module_utils._text import to_bytes
 from ansible.module_utils.network.common.utils import to_list
 from ansible.plugins.cliconf import CliconfBase, enable_mode
 
@@ -34,13 +33,13 @@ class Cliconf(CliconfBase):
         device_info = {}
 
         device_info['network_os'] = 'eos'
-        reply = self.get(b'show version | json')
+        reply = self.get('show version | json')
         data = json.loads(reply)
 
         device_info['network_os_version'] = data['version']
         device_info['network_os_model'] = data['modelName']
 
-        reply = self.get(b'show hostname | json')
+        reply = self.get('show hostname | json')
         data = json.loads(reply)
 
         device_info['network_os_hostname'] = data['hostname']
@@ -63,7 +62,7 @@ class Cliconf(CliconfBase):
 
     @enable_mode
     def edit_config(self, command):
-        for cmd in chain([b'configure'], to_list(command), [b'end']):
+        for cmd in chain(['configure'], to_list(command), ['end']):
             self.send_command(cmd)
 
     def get(self, command, prompt=None, answer=None, sendonly=False):
