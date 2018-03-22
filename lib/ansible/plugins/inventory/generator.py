1fff0289c2fbb255df50bd52c73d0ca20003d032
Will Thames <will.thames@xvt.com.au>
2018-03-22 14:02:59 +1000

    Add generator plugin documentation

--- lib/ansible/plugins/inventory/generator.py
+++ lib/ansible/plugins/inventory/generator.py
@@ -14,6 +14,22 @@ DOCUMENTATION = '''
         - Create a template pattern that describes each host, and then use independent configuration layers
         - Every element of every layer is combined to create a host for every layer combination
         - Parent groups can be defined with reference to hosts and other groups using the same template variables
+    options:
+      hosts:
+        description:
+          - The C(name) key is a template used to generate
+            hostnames based on the C(layers) option. Each variable in the name is expanded to create a
+            cartesian product of all possible layer combinations.
+          - The C(parents) are a list of parent groups that the host belongs to. Each C(parent) item
+            contains a C(name) key, again expanded from the template, and an optional C(parents) key
+            that lists its parents.
+          - Parents can also contain C(vars), which is a dictionary of vars that
+            is then always set for that variable. This can provide easy access to the group name. E.g
+            set an C(application) variable that is set to the value of the C(application) layer name.
+      layers:
+        description:
+          - A dictionary of layers, with the key being the layer name, used as a variable name in the C(host)
+            C(name) and C(parents) keys. Each layer value is a list of possible values for that layer.
 '''
 
 EXAMPLES = '''
