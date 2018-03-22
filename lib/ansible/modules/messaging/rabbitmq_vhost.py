fd834cbbe18407c886cd9a2075075cb3cacf6610
Lachlan Cooper <lachlancooper@gmail.com>
2018-03-23 04:03:20 +1100

    Check that output from list_vhosts can be split (#37630)
    As with list_users, list_vhosts can sometimes return a value that
    doesn't contain a '\t' character. This appears to be the case if the
    server has no vhosts, for example.
    
    The same fix was applied to the rabbitmq_users module here:
    https://github.com/ansible/ansible/commit/fafb89cde58eff5994f607b2dacce880b57bd56d

--- lib/ansible/modules/messaging/rabbitmq_vhost.py
+++ lib/ansible/modules/messaging/rabbitmq_vhost.py
@@ -76,6 +76,9 @@ class RabbitMqVhost(object):
         vhosts = self._exec(['list_vhosts', 'name', 'tracing'], True)
 
         for vhost in vhosts:
+            if '\t' not in vhost:
+                continue
+
             name, tracing = vhost.split('\t')
             if name == self.name:
                 self._tracing = self.module.boolean(tracing)
