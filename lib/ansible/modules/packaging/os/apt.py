529972d568213b49ea8e6ead110de90725750cfc
Martin Krizek <martin.krizek@gmail.com>
2018-03-22 10:47:08 +0100

    Add fallback for when manual is not a thing

--- lib/ansible/modules/packaging/os/apt.py
+++ lib/ansible/modules/packaging/os/apt.py
@@ -275,6 +275,7 @@ APT_GET_ZERO = "\n0 upgraded, 0 newly installed"
 APTITUDE_ZERO = "\n0 packages upgraded, 0 newly installed"
 APT_LISTS_PATH = "/var/lib/apt/lists"
 APT_UPDATE_SUCCESS_STAMP_PATH = "/var/lib/apt/periodic/update-success-stamp"
+APT_MARK_INVALID_OP = 'Invalid operation'
 
 HAS_PYTHON_APT = True
 try:
@@ -469,6 +470,19 @@ def parse_diff(output):
     return {'prepared': '\n'.join(diff[diff_start:diff_end])}
 
 
+def mark_installed_manually(m, packages):
+    apt_mark_cmd_path = m.get_bin_path("apt-mark", required=True)
+    cmd = "%s manual %s" % (apt_mark_cmd_path, ' '.join(packages))
+    rc, out, err = m.run_command(cmd)
+
+    if APT_MARK_INVALID_OP in err:
+        cmd = "%s unmarkauto %s" % (apt_mark_cmd_path, ' '.join(packages))
+        rc, out, err = m.run_command(cmd)
+
+    if rc != 0:
+        m.fail_json(msg="'%s' failed: %s" % (cmd, err), stdout=out, stderr=err, rc=rc)
+
+
 def install(m, pkgspec, cache, upgrade=False, default_release=None,
             install_recommends=None, force=False,
             dpkg_options=expand_dpkg_options(DPKG_OPTIONS),
@@ -555,14 +569,6 @@ def install(m, pkgspec, cache, upgrade=False, default_release=None,
         mark_installed_manually(m, package_names)
 
     return (status, data)
-
-
-def mark_installed_manually(m, packages):
-    apt_mark_cmd_path = m.get_bin_path("apt-mark", required=True)
-    cmd = "%s manual %s" % (apt_mark_cmd_path, ' '.join(packages))
-    rc, out, err = m.run_command(cmd)
-    if rc != 0:
-        m.fail_json(msg="'%s' failed: %s" % (cmd, err), stdout=out, stderr=err, rc=rc)
 
 
 def get_field_of_deb(m, deb_file, field="Version"):
