38b914489df841e9a17126260604bc6e180636b3
Hiroyuki Matsuo <hiroyuki3569825@gmail.com>
2018-03-23 16:32:02 +0900

    Add author for module

--- lib/ansible/modules/messaging/rabbitmq_vhost_limits.py
+++ lib/ansible/modules/messaging/rabbitmq_vhost_limits.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# (c) 2018, First Last <email address>
+# (c) 2018, Hiroyuki Matsuo <hiroyuki3569825@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
