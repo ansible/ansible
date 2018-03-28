2f76719da9543a0a4d6738b7fa5dec701506d6d4
Martin Krizek <martin.krizek@gmail.com>
2018-03-28 21:59:30 +0200

    Remove leftover script

--- test/integration/targets/apt/files/fix-udev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat > /usr/sbin/policy-rc.d <<EOF
-#!/bin/bash
-exit 101
-EOF
-chmod +x /usr/sbin/policy-rc.d
-dpkg-divert --local --rename --add /sbin/initctl
-ln -s /bin/true /sbin/initctl
