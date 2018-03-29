a54b681de6faba8f75a948cddc6299504f726928
Yuwei Zhou <yuwzho@microsoft.com>
2018-03-29 16:27:47 +0800

    Update azure_rm_aks.py

--- lib/ansible/modules/cloud/azure/azure_rm_aks.py
+++ lib/ansible/modules/cloud/azure/azure_rm_aks.py
@@ -424,7 +424,7 @@ class AzureRMManagedCluster(AzureRMModuleBase):
                     # Cannot Update the SSH Key for now // Let service to handle it
                     if is_property_changed('linux_profile', 'ssh_key'):
                         self.log(("Linux Profile Diff SSH, Was {0} / Now {1}"
-                                  .format(response['linux_profile'].ssh.public_keys[0].key_data, self.linux_profile.get('ssh_key'))))
+                                  .format(response['linux_profile']['ssh_key'], self.linux_profile.get('ssh_key'))))
                         to_be_updated = True
                         # self.module.warn("linux_profile.ssh_key cannot be updated")
 
@@ -433,7 +433,7 @@ class AzureRMManagedCluster(AzureRMModuleBase):
                     # Cannot Update the Username for now // Let service to handle it
                     if is_property_changed('linux_profile', 'admin_username'):
                         self.log(("Linux Profile Diff User, Was {0} / Now {1}"
-                                  .format(response['linux_profile'].admin_username, self.linux_profile.get('admin_username'))))
+                                  .format(response['linux_profile']['admin_username'], self.linux_profile.get('admin_username'))))
                         to_be_updated = True
                         # self.module.warn("linux_profile.admin_username cannot be updated")
 
