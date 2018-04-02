4f318ca3fd4e340c78a82e31a8e2d6eac82defea
cahlchang <kahlua.dane@gmail.com>
2018-04-02 07:46:08 +0000

    Modify document

--- lib/ansible/modules/cloud/amazon/rds_tag.py
+++ lib/ansible/modules/cloud/amazon/rds_tag.py
@@ -15,26 +15,44 @@ DOCUMENTATION = '''
 ---
 module: rds_tag
 short_description: create and remove tag(s) to rds instance.
-description:
-    - Creates, removes and lists tags from any rds instance. The instance is referenced by its instance name.
-      It is designed to be used with complex args (tags), see the examples.  This module has a dependency on python-boto3.
+description: >
+  Creates, removes and lists tags from any rds instance.
+  The instance is referenced by its instance name.
+  It is designed to be used with complex args (tags), see the examples.
+  This module has a dependency on python-boto3.
+version_added: "2.5"
 options:
   instance_name:
     description:
       - The RDS instance name.
   state:
-    description:
-      - Whether the tags should be present or absent on the resource. Use list to interrogate the tags of an instance.
+    description: >
+      Whether the tags should be present or absent on the resource.
+      Use list to interrogate the tags of an instance.
     default: present
     choices: ['present', 'absent', 'list']
   tags:
-    description:
-      - a hash/dictionary of tags to add to the resource; '{"key":"value"}' and '{"key":"value","key":"value"}'
+    description: >-
+      a hash/dictionary of tags to add to the resource;
+      '{"key":"value"}' and '{"key":"value","key":"value"}'
     required: true
+extends_documentation_fragment:
+    - aws
+    - ec2
+
+author: "Akane Morikawa (@cahlchang)"
 '''
 
 EXAMPLES = '''
-- name: Ensure tags are present on a resource
+# Note: None of these examples set aws_access_key, aws_secret_key, or region.
+# It is assumed that their matching environment variables are set.
+- name: Ensure tags are present on a instance
+  rds_tag:
+    region: eu-west-1
+    instance_name: name-database
+    state: list
+
+- name: add tags to instance
   rds_tag:
     region: eu-west-1
     instance_name: name-database
@@ -42,6 +60,60 @@ EXAMPLES = '''
     tags:
       Name: environment
       env: sandbox
+
+- name: remove tags to instance
+  rds_tag:
+    region: eu-west-1
+    instance_name: name-database
+    state: absent
+    tags:
+      Name: environment
+      env: sandbox
+'''
+
+RETURN = '''
+---
+changed:
+  description: Whether there was any change.
+  returned: always
+  type: bool
+  sample: true
+message:
+  description: Summary of module execution results.
+  returned: always
+  type: string
+  sample: Add tag to arn:aws:rds:eu-west-1:xxxxxxxxxxxx:db:db_name instance
+invocation:
+  description: Parameters used during invocation.
+  returned: always
+  type: dict
+  sample:
+    module_args:
+      aws_access_key: null
+      aws_secret_key: null
+      ec2_url: null
+      instance_name: xxx-instance
+      profile: your profile
+      region: eu-west-1
+      security_token: null
+      state: present
+      tags:
+        your_setting_key_a: your_setting_value_a
+        your_setting_key_b: your_setting_value_b
+      validate_certs: true
+response_metadata:
+  description: Value returned by boto3.
+  returned: When not in dry run mode.
+  type: dict
+  sample:
+    http_headers:
+      content-length: xxx
+      content-type: text/xml
+      date: Mon, 02 Apr 2018 03:53:23 GMT
+      x-amzn-requestid: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+    http_status_code: 200
+    request_id: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+    retry_attempts: 0
 '''
 
 try:
