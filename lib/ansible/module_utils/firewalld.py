f8cacf1f2ba14ee1c65d57f7e1118a717843afd6
Adam Miller <admiller@redhat.com>
2018-04-02 08:58:09 -0500

    stick with GPLv3 for firewalld module_utils
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- lib/ansible/module_utils/firewalld.py
+++ lib/ansible/module_utils/firewalld.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # (c) 2018, Adam Miller (maxamillion@fedoraproject.org)
-# This file is licenced under the Simplified BSD License
-# (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 # Imports and info for sanity checking
 FW_VERSION = None
