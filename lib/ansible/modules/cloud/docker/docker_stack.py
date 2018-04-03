82834b1f4f3507de7d502c56ff84ea10668683f8
John Hu <hszqqq12@gmail.com>
2018-04-03 11:57:29 +0800

    Fix ansible boilerplate

--- lib/ansible/modules/cloud/docker/docker_stack.py
+++ lib/ansible/modules/cloud/docker/docker_stack.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 # Copyright (c) 2018 Dario Zanzico (git@dariozanzico.com)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 
 ANSIBLE_METADATA = {'status': ['preview'],
                     'supported_by': 'community',
