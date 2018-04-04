e9b944e2ad0dcf57b84c9c24ac6d26a34a24f615
Dag Wieers <dag@wieers.com>
2018-04-04 02:43:58 +0200

    Fix incorrect RN used in module (#38245)
    This requires backporting to v2.5

--- lib/ansible/modules/network/aci/aci_domain_to_vlan_pool.py
+++ lib/ansible/modules/network/aci/aci_domain_to_vlan_pool.py
@@ -302,7 +302,7 @@ def main():
     elif domain_type == 'vmm':
         domain_class = 'vmmDomP'
         domain_mo = 'uni/vmmp-{0}/dom-{1}'.format(VM_PROVIDER_MAPPING[vm_provider], domain)
-        domain_rn = 'dom-{0}'.format(domain)
+        domain_rn = 'vmmp-{0}/dom-{1}'.format(VM_PROVIDER_MAPPING[vm_provider], domain)
 
     # Ensure that querying all objects works when only domain_type is provided
     if domain is None:
