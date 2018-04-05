9a65e9dddfa83e54c49a4da753c4b0ed5c48f088
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2018-04-05 11:33:19 -0300

    New module request: azure_rm_virtualmachine_facts #38081 #38279 serialize_vm

--- lib/ansible/modules/cloud/azure/azure_rm_virtualmachine_facts.py
+++ lib/ansible/modules/cloud/azure/azure_rm_virtualmachine_facts.py
@@ -60,9 +60,10 @@ azure_virtualmachines:
     returned: always
     type: list
     example: [{
-        "id": "/subscriptions/XXXXX/resourceGroups/Testing/providers/Microsoft.Compute/virtualMachines/vm-example",
+        "id": "/subscriptions/XXXXX/resourceGroups/Testing/providers/Microsoft.Compute/virtualMachines/vm",
         "location": "brazilsouth",
-        "name": "vm-example",
+        "name": "vm",
+        "powerstate": "running",
         "properties": {
             "hardwareProfile": {
                 "vmSize": "Standard_A0"
@@ -70,7 +71,7 @@ azure_virtualmachines:
             "instanceView": {
                 "disks": [
                     {
-                        "name": "vm-example.vhd",
+                        "name": "vm.vhd",
                         "statuses": [
                             {
                                 "code": "ProvisioningState/succeeded",
@@ -135,15 +136,67 @@ azure_virtualmachines:
                 }
             },
             "networkProfile": {
-                "networkInterfaces": [
-                    {
-                        "id": "/subscriptions/XXXXX/resourceGroups/Testing/providers/Microsoft.Network/networkInterfaces/vm-example-nic"
+                "networkInterfaces": [{
+                    "id": "/subscriptions/XXXXX/resourceGroups/Testing/providers/Microsoft.Network/networkInterfaces/vm-nic",
+                    "name": "vm-nic",
+                    "properties": {
+                        "dnsSettings": {
+                            "appliedDnsServers": [],
+                            "dnsServers": [],
+                            "internalDomainNameSuffix": "yp5qqpscrrgu3kalovpyi1rqoa.nx.internal.cloudapp.net"
+                        },
+                        "enableAcceleratedNetworking": false,
+                        "enableIPForwarding": false,
+                        "ipConfigurations": [{
+                            "etag": "W/'6d0bd817-d2f8-4b21-98ff-b33abcc9b11b'",
+                            "id": "/subscriptions/XXXXX/resourceGroups/Testing/providers/Microsoft.Network/networkInterfaces/vm-nic/ipConfigurations/ipconfig1",
+                            "name": "ipconfig1",
+                            "properties": {
+                                "primary": true,
+                                "privateIPAddress": "10.0.1.4",
+                                "privateIPAddressVersion": "IPv4",
+                                "privateIPAllocationMethod": "Dynamic",
+                                "provisioningState": "Succeeded",
+                                "publicIPAddress": {
+                                    "id": "/subscriptions/XXXXX/resourceGroups/Testing/providers/Microsoft.Network/publicIPAddresses/vm-ip",
+                                    "name": "vm-ip",
+                                    "properties": {
+                                        "dnsSettings": {
+                                            "domainNameLabel": "vm-domain",
+                                            "fqdn": "vm-domain.brazilsouth.cloudapp.azure.com"
+                                        },
+                                        "idleTimeoutInMinutes": 4,
+                                        "ipAddress": "191.232.165.120",
+                                        "ipConfiguration": {
+                                            "id": "/subscriptions/XXXXX/resourceGroups/Testing/providers/Micro.../nInterfaces/vm-nic/ipConfigurations/ipconfig1"
+                                        },
+                                        "provisioningState": "Succeeded",
+                                        "publicIPAddressVersion": "IPv4",
+                                        "publicIPAllocationMethod": "Dynamic",
+                                        "resourceGuid": "4ac0bc74-3a5d-4ea1-8ceb-db5aaa1725d7"
+                                    }
+                                },
+                                "subnet": {
+                                    "id": "/subscriptions/XXXXX/resourceGroups/Testing/providers/Microsoft.Network/virtualNetworks/Testing-vnet/subnets/default"
+                                }
+                            }
+                        }],
+                        "macAddress": "00-0D-3A-C1-57-96",
+                        "networkSecurityGroup": {
+                            "id": "/subscriptions/XXXXX/resourceGroups/Testing/providers/Microsoft.Network/networkSecurityGroups/vm-nsg"
+                        },
+                        "primary": true,
+                        "provisioningState": "Succeeded",
+                        "resourceGuid": "7212cde7-8c8e-40a2-81fd-9c0fe5777300",
+                        "virtualMachine": {
+                            "id": "/subscriptions/XXXXX/resourceGroups/Testing/providers/Microsoft.Compute/virtualMachines/vm"
+                        }
                     }
-                ]
+                }]
             },
             "osProfile": {
                 "adminUsername": "ubuntu",
-                "computerName": "vm-example",
+                "computerName": "vm",
                 "linuxConfiguration": {
                     "disablePasswordAuthentication": true,
                     "ssh": {
@@ -170,10 +223,10 @@ azure_virtualmachines:
                     "caching": "ReadOnly",
                     "createOption": "FromImage",
                     "diskSizeGB": 30,
-                    "name": "vm-example.vhd",
+                    "name": "vm.vhd",
                     "osType": "Linux",
                     "vhd": {
-                        "uri": "https://ubuntu1842.blob.core.windows.net/vhds/vm-example.vhd"
+                        "uri": "https://ubuntu1842.blob.core.windows.net/vhds/vm.vhd"
                     }
                 }
             },
@@ -181,7 +234,7 @@ azure_virtualmachines:
         },
         "resources": [
             {
-                "id": "/subscriptions/XXXXX/resourceGroups/Testing/providers/Microsoft.Compute/virtualMachines/vm-example/extensions/OmsAgentForLinux",
+                "id": "/subscriptions/XXXXX/resourceGroups/Testing/providers/Microsoft.Compute/virtualMachines/vm/extensions/OmsAgentForLinux",
                 "location": "brazilsouth",
                 "name": "OmsAgentForLinux",
                 "properties": {
@@ -189,7 +242,7 @@ azure_virtualmachines:
                     "provisioningState": "Succeeded",
                     "publisher": "Microsoft.EnterpriseCloud.Monitoring",
                     "settings": {
-                        "azureResourceId": "/subscriptions/XXXXX/resourcegroups/Testing/providers/microsoft.compute/virtualmachines/vm-example",
+                        "azureResourceId": "/subscriptions/XXXXX/resourcegroups/Testing/providers/microsoft.compute/virtualmachines/vm",
                         "stopOnMultipleConnections": true,
                         "workspaceId": "43ac2095-42c7-4488-924f-bc4cbca63f8d"
                     },
@@ -212,10 +265,12 @@ except:
     # This is handled in azure_rm_common
     pass
 
-from ansible.module_utils.azure_rm_common import AzureRMModuleBase
+from ansible.module_utils.azure_rm_common import AzureRMModuleBase, azure_id_to_dict
 
 
 AZURE_OBJECT_CLASS = 'VirtualMachine'
+
+AZURE_ENUM_MODULES = ['azure.mgmt.compute.models']
 
 
 class AzureRMVirtualMachineFacts(AzureRMModuleBase):
@@ -260,9 +315,75 @@ class AzureRMVirtualMachineFacts(AzureRMModuleBase):
         results = []
         for item in items:
             if self.has_tags(item.tags, self.tags):
-                vm = self.compute_client.virtual_machines.get(self.resource_group, item.name, expand='instanceview')
-                results.append(self.serialize_obj(vm, AZURE_OBJECT_CLASS))
+                results.append(self.serialize_vm(self.get_vm(item.name)))
         return results
+
+    def get_vm(self, name):
+        '''
+        Get the VM with expanded instanceView
+
+        :return: VirtualMachine object
+        '''
+        try:
+            vm = self.compute_client.virtual_machines.get(self.resource_group, name, expand='instanceview')
+            return vm
+        except Exception as exc:
+            self.fail("Error getting virtual machine {0} - {1}".format(self.name, str(exc)))
+
+    def serialize_vm(self, vm):
+        '''
+        Convert a VirtualMachine object to dict.
+
+        :param vm: VirtualMachine object
+        :return: dict
+        '''
+
+        result = self.serialize_obj(vm, AZURE_OBJECT_CLASS, enum_modules=AZURE_ENUM_MODULES)
+        result['id'] = vm.id
+        result['name'] = vm.name
+        result['type'] = vm.type
+        result['location'] = vm.location
+        result['tags'] = vm.tags
+
+        result['powerstate'] = dict()
+        if vm.instance_view:
+            result['powerstate'] = next((s.code.replace('PowerState/', '')
+                                         for s in vm.instance_view.statuses if s.code.startswith('PowerState')), None)
+
+        # Expand network interfaces to include config properties
+        for interface in vm.network_profile.network_interfaces:
+            int_dict = azure_id_to_dict(interface.id)
+            nic = self.get_network_interface(int_dict['networkInterfaces'])
+            for interface_dict in result['properties']['networkProfile']['networkInterfaces']:
+                if interface_dict['id'] == interface.id:
+                    nic_dict = self.serialize_obj(nic, 'NetworkInterface')
+                    interface_dict['name'] = int_dict['networkInterfaces']
+                    interface_dict['properties'] = nic_dict['properties']
+
+        # Expand public IPs to include config properties
+        for interface in result['properties']['networkProfile']['networkInterfaces']:
+            for config in interface['properties']['ipConfigurations']:
+                if config['properties'].get('publicIPAddress'):
+                    pipid_dict = azure_id_to_dict(config['properties']['publicIPAddress']['id'])
+                    try:
+                        pip = self.network_client.public_ip_addresses.get(self.resource_group,
+                                                                          pipid_dict['publicIPAddresses'])
+                    except Exception as exc:
+                        self.fail("Error fetching public ip {0} - {1}".format(pipid_dict['publicIPAddresses'],
+                                                                              str(exc)))
+                    pip_dict = self.serialize_obj(pip, 'PublicIPAddress')
+                    config['properties']['publicIPAddress']['name'] = pipid_dict['publicIPAddresses']
+                    config['properties']['publicIPAddress']['properties'] = pip_dict['properties']
+
+        self.log(result, pretty_print=True)
+        return result
+
+    def get_network_interface(self, name):
+        try:
+            nic = self.network_client.network_interfaces.get(self.resource_group, name)
+            return nic
+        except Exception as exc:
+            self.fail("Error fetching network interface {0} - {1}".format(name, str(exc)))
 
 
 def main():
