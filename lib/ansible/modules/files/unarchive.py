11e260630dc0c8b8757713fa34a28062dfc67e0d
Alexander Kovtunenko <akovtunenko@slice.com>
2018-04-08 19:51:09 +0300

    remove default

--- lib/ansible/modules/files/unarchive.py
+++ lib/ansible/modules/files/unarchive.py
@@ -69,7 +69,6 @@ options:
       - Add password for unarchive password protected file.
     version_added: "2.6"
     required: false
-    default: 'no'
   extra_opts:
     description:
       - Specify additional options by passing in an array.
