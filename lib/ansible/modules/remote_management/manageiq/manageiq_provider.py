c9afb5a71087876b2ade9f94136d1a280712b8f9
Beni Cherniavsky-Paskin <cben@redhat.com>
2018-04-08 13:56:48 +0300

    manageiq_provider: refactoring step towards DRY
    `resource` is now same in `edit_provider()` and `create_provider()`,
    to be extracted to main later.

--- lib/ansible/modules/remote_management/manageiq/manageiq_provider.py
+++ lib/ansible/modules/remote_management/manageiq/manageiq_provider.py
@@ -745,24 +745,24 @@ class ManageIQProvider(object):
         # clean nulls, we do not send nulls to the api
         endpoints = delete_nulls(endpoints)
 
+        resource = dict(
+            name=name,
+            zone={'id': zone_id},
+            provider_region=provider_region,
+            host_default_vnc_port_start=host_default_vnc_port_start,
+            host_default_vnc_port_end=host_default_vnc_port_end,
+            subscription=subscription,
+            project=project,
+            uid_ems=uid_ems,
+            tenant_mapping_enabled=tenant_mapping_enabled,
+            api_version=api_version,
+            connection_configurations=endpoints,
+        )
+
         # try to create a new provider
         try:
             url = '%s/providers' % (self.api_url)
-            result = self.client.post(
-                url,
-                name=name,
-                type=supported_providers()[provider_type]['class_name'],
-                zone={'id': zone_id},
-                provider_region=provider_region,
-                host_default_vnc_port_start=host_default_vnc_port_start,
-                host_default_vnc_port_end=host_default_vnc_port_end,
-                subscription=subscription,
-                project=project,
-                uid_ems=uid_ems,
-                tenant_mapping_enabled=tenant_mapping_enabled,
-                api_version=api_version,
-                connection_configurations=endpoints,
-            )
+            result = self.client.post(url, type=supported_providers()[provider_type]['class_name'], **resource)
         except Exception as e:
             self.module.fail_json(msg="failed to create provider %s: %s" % (name, str(e)))
 
