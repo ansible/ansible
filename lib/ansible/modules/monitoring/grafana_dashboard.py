e323b0011819df5119ba83a15ca9d488e38106f9
THIERRY SALLE <thierry-externe.salle@enedis.fr>
2018-04-09 16:14:44 +0200

    Fix authot github account

--- lib/ansible/modules/monitoring/grafana_dashboard.py
+++ lib/ansible/modules/monitoring/grafana_dashboard.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2017, Thierry Sallé (@tsalle)
+# Copyright: (c) 2017, Thierry Sallé (@seuf)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
