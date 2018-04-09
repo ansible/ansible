eac102114452639e6444d7b8876791e2c9655169
Martin Krizek <martin.krizek@gmail.com>
2018-04-09 11:08:04 +0200

    Give hint when lookup fails on one element list (#38406)
    * Give hint when lookup fails on one element list
    
    * Mention wantlist first
    
    * Fix pep8...

--- lib/ansible/executor/task_executor.py
+++ lib/ansible/executor/task_executor.py
@@ -233,7 +233,11 @@ class TaskExecutor:
         elif self._task.loop:
             items = templar.template(self._task.loop)
             if not isinstance(items, list):
-                raise AnsibleError("Invalid data passed to 'loop' it requires a list, got this instead: %s" % items)
+                raise AnsibleError(
+                    "Invalid data passed to 'loop', it requires a list, got this instead: %s."
+                    " Hint: If you passed a list/dict of just one element,"
+                    " try adding wantlist=True to your lookup invocation or use q/query instead of lookup." % items
+                )
 
         # now we restore any old job variables that may have been modified,
         # and delete them if they were in the play context vars but not in
