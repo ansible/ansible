7cd84812372ee3a31ff5d9b9d2d2f62f508df6f2
Rob Nieuwenhuizen <ra.nieuwenhuizen@gmail.com>
2018-04-13 09:39:43 +0200

    Revert unintented changes

--- lib/ansible/modules/packaging/language/pip.py
+++ lib/ansible/modules/packaging/language/pip.py
@@ -246,24 +246,8 @@ def _get_packages(module, pip, chdir):
 def _is_present(name, version, installed_pkgs, pkg_command):
     '''Return whether or not package is installed.'''
     for pkg in installed_pkgs:
-<<<<<<< HEAD
-        # Package listing will be different depending on which pip
-        # command was used ('pip list' vs. 'pip freeze').
-        if 'list' in pkg_command:
-            pkg = pkg.replace('(', '').replace(')', '')
-            if ',' in pkg:
-                pkg_name, pkg_version, _ = pkg.replace(',', '').split(' ')
-            else:
-                pkg_name, pkg_version = pkg.split()
-        elif 'freeze' in pkg_command:
-            if '==' in pkg:
-                pkg_name, pkg_version = pkg.split('==')
-            else:
-                continue
-=======
         if '==' in pkg:
             pkg_name, pkg_version = pkg.split('==')
->>>>>>> 6de49f084c3a8b21d301ecd20b45d8266e51b707
         else:
             continue
 
