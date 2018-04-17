9f5b4097e9be6d763727680a8f0281cadf42f01e
Matt Clay <matt@mystile.com>
2018-04-17 11:20:40 -0700

    Update httptester container reference.

--- test/runner/test.py
+++ test/runner/test.py
@@ -627,7 +627,7 @@ def add_extra_docker_options(parser, integration=True):
 
     docker.add_argument('--docker-util',
                         metavar='IMAGE',
-                        default='ansible/ansible@sha256:fa5def8c294fc50813af131c0b5737594d852abac9cbe7ba38e17bf1c8476f3f',  # httptester
+                        default='quay.io/ansible/http-test-container:1.0.0',
                         help='docker utility image to provide test services')
 
     docker.add_argument('--docker-privileged',
