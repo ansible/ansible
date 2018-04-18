2f5bb579250d756197be57f57af11e1022af692d
Matt Clay <matt@mystile.com>
2018-04-18 09:55:24 -0700

    Categorize docs-build warnings.

--- test/sanity/code-smell/docs-build.py
+++ test/sanity/code-smell/docs-build.py
@@ -20,6 +20,22 @@ def main():
         output = warnings_fd.read().strip()
         lines = output.splitlines()
 
+    known_warnings = {
+        'block-quote-missing-blank-line': r'^Block quote ends without a blank line; unexpected unindent.$',
+        'literal-block-lex-error': r'^Could not lex literal_block as "[^"]*". Highlighting skipped.$',
+        'duplicate-label': r'^duplicate label ',
+        'undefined-label': r'undefined label: ',
+        'unknown-document': r'unknown document: ',
+        'toc-tree-missing-document': r'toctree contains reference to nonexisting document ',
+        'reference-target-not-found': r'[^ ]* reference target not found: ',
+        'not-in-toc-tree': r"document isn't included in any toctree$",
+        'unexpected-indentation': r'^Unexpected indentation.$',
+        'definition-list-missing-blank-line': r'^Definition list ends without a blank line; unexpected unindent.$',
+        'explicit-markup-missing-blank-line': r'Explicit markup ends without a blank line; unexpected unindent.$',
+        'toc-tree-glob-pattern-no-match': r"^toctree glob pattern '[^']*' didn't match any documents$",
+        'unknown-interpreted-text-role': '^Unknown interpreted text role "[^"]*".$',
+    }
+
     for line in lines:
         match = re.search('^(?P<path>[^:]+):((?P<line>[0-9]+):)?((?P<column>[0-9]+):)? (?P<level>WARNING|ERROR): (?P<message>.*)$', line)
 
@@ -27,11 +43,11 @@ def main():
             path = 'docs/docsite/rst/index.rst'
             lineno = 0
             column = 0
-            level = 'unknown'
+            code = 'unknown'
             message = line
 
             # surface unknown lines while filtering out known lines to avoid excessive output
-            print('%s:%d:%d: %s: %s' % (path, lineno, column, level, message))
+            print('%s:%d:%d: %s: %s' % (path, lineno, column, code, message))
             continue
 
         path = match.group('path')
@@ -46,9 +62,16 @@ def main():
             path = path[len(base_dir):]
 
         if level == 'warning':
-            continue
+            code = 'warning'
 
-        print('%s:%d:%d: %s: %s' % (path, lineno, column, level, message))
+            for label, pattern in known_warnings.items():
+                if re.search(pattern, message):
+                    code = label
+                    break
+        else:
+            code = 'error'
+
+        print('%s:%d:%d: %s: %s' % (path, lineno, column, code, message))
 
 
 if __name__ == '__main__':
