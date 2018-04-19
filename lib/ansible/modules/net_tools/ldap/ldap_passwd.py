ad3799a5be49ada2309c19a7ed5141e10ca15fde
The Fox in the Shell <KellerFuchs@hashbang.sh>
2018-04-19 20:21:56 +0200

    Update copyright info for ldap modules

--- lib/ansible/modules/net_tools/ldap/ldap_passwd.py
+++ lib/ansible/modules/net_tools/ldap/ldap_passwd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2017, Keller Fuchs <kellerfuchs@hashbang.sh>
+# Copyright: (c) 2017-2018, Keller Fuchs <kellerfuchs@hashbang.sh>
 #
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
