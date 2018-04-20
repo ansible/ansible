3db13ab839971f7cef504782321af60869c362d6
Benjamin Ness <shifty35@gmail.com>
2018-04-20 17:30:10 -0500

    [ec2_ami] Add missing parameters while handling botocore Exceptions.  Fixes TypeError exception (#39105)

--- lib/ansible/modules/cloud/amazon/ec2_ami.py
+++ lib/ansible/modules/cloud/amazon/ec2_ami.py
@@ -626,9 +626,9 @@ def get_image_by_id(module, connection, image_id):
                 result['ProductCodes'] = connection.describe_image_attribute(Attribute='productCodes', ImageId=image_id)['ProductCodes']
             except botocore.exceptions.ClientError as e:
                 if e.response['Error']['Code'] != 'InvalidAMIID.Unavailable':
-                    module.fail_json_aws(e, msg="Error retrieving image attributes" % image_id)
+                    module.fail_json_aws(e, msg="Error retrieving image attributes %s" % image_id)
             except botocore.exceptions.BotoCoreError as e:
-                module.fail_json_aws(e, msg="Error retrieving image attributes" % image_id)
+                module.fail_json_aws(e, msg="Error retrieving image attributes %s" % image_id)
             return result
         module.fail_json(msg="Invalid number of instances (%s) found for image_id: %s." % (str(len(images)), image_id))
     except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
