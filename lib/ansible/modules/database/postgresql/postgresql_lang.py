e962fcb3bee49397d8b98f66e688a288638544b6
John R Barker <john@johnrbarker.com>
2018-04-20 21:45:15 +0100

    Remove duplicated key

--- lib/ansible/modules/database/postgresql/postgresql_lang.py
+++ lib/ansible/modules/database/postgresql/postgresql_lang.py
@@ -41,7 +41,6 @@ options:
       - make this language trusted for the selected db
     type: bool
     default: 'no'
-    type: bool
   db:
     description:
       - name of database where the language will be added, removed or changed
