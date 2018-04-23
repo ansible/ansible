c069ae29baf042732467fd641956adfc9338d29e
Matt Clay <matt@mystile.com>
2018-04-23 12:22:36 -0700

    Convert more http links to https.

--- lib/ansible/modules/cloud/webfaction/webfaction_site.py
+++ lib/ansible/modules/cloud/webfaction/webfaction_site.py
@@ -18,7 +18,7 @@ DOCUMENTATION = '''
 module: webfaction_site
 short_description: Add or remove a website on a Webfaction host
 description:
-    - Add or remove a website on a Webfaction host.  Further documentation at http://github.com/quentinsf/ansible-webfaction.
+    - Add or remove a website on a Webfaction host.  Further documentation at https://github.com/quentinsf/ansible-webfaction.
 author: Quentin Stafford-Fraser (@quentinsf)
 version_added: "2.0"
 notes:
@@ -29,7 +29,7 @@ notes:
       You can run playbooks that use this on a local machine, or on a Webfaction host, or elsewhere, since the scripts use the remote webfaction API.
       The location is not important. However, running them on multiple hosts I(simultaneously) is best avoided. If you don't specify I(localhost) as
       your host, you may want to add C(serial: 1) to the plays.
-    - See `the webfaction API <http://docs.webfaction.com/xmlrpc-api/>`_ for more info.
+    - See `the webfaction API <https://docs.webfaction.com/xmlrpc-api/>`_ for more info.
 
 options:
 
