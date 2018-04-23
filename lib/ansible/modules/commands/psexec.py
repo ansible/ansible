fe6efe2371c880ab7ad2fab56aea89a67db10d00
Jordan Borean <jborean93@gmail.com>
2018-04-23 17:01:44 +1000

    Simplified module options around process integrity levels and the system account

--- lib/ansible/modules/commands/psexec.py
+++ lib/ansible/modules/commands/psexec.py
@@ -62,7 +62,7 @@ options:
     - This requires the SMB 3 protocol which is only supported from Windows
       Server 2012 or Windows 8, older versions like Windows 7 or Windows Server
       2008 (R2) must set this to C(no) and use no encryption.
-    - When setting to C(no), the packets are in plaintext and can be seend by
+    - When setting to C(no), the packets are in plaintext and can be seen by
       anyone sniffing the network, any process options are included in this.
     type: bool
     default: 'yes'
@@ -86,7 +86,7 @@ options:
     description:
     - Will run the command as a detached process and the module returns
       immediately after starting the processs while the process continues to
-      run the background.
+      run in the background.
     - The I(stdout) and I(stderr) return values will be null when this is set
       to C(yes).
     - The I(stdin) option does not work with this type of process.
@@ -104,43 +104,30 @@ options:
     - This can be set to run the process under an Interactive logon of the
       specified account which bypasses limitations of a Network logon used when
       this isn't specified.
-    - If ommited then the process is run under the same account as
+    - If omitted then the process is run under the same account as
       I(connection_username) with a Network logon.
+    - Set to C(System) to run as the builtin SYSTEM account, no password is
+      required with this account.
     - If I(encrypt) is C(no), the username and password are sent as a simple
       XOR scrambled byte string that is not encrypted. No special tools are
       required to get the username and password just knowledge of the protocol.
   process_password:
     description:
     - The password for I(process_username).
-    - Required if I(process_username) is defined.
-  elevated:
+    - Required if I(process_username) is defined and not C(System).
+  integrity_level:
     description:
-    - Will run the command with Administrative rights when I(process_username)
-      is defined.
-    - This only affects the process if C(process_username) is set, if it is not
-      then the process is run under the I(connection_username) account and
-      already has an elevated token to the process.
-    - Cannot be C(yes) when I(limited) is also C(yes).
-    type: bool
-    default: 'no'
-  limited:
-    description:
-    - Will run the command with limited rights similar to running a process
-      normally when UAC is turned on.
-    - This only affect the process if C(process_username) is set, if it is not
-      then the process is runder the I(connection_username) account and cannot
-      be run under a limited token.
-    - Cannot be C(yes) when I(elevated) is also C(yes).
-    type: bool
-    default: 'no'
-  use_system:
-    description:
-    - Runs the process under the local SYSTEM account instead of a normal user.
-    - This option overrides C(process_username) if set to C(yes).
-    - The process is run with the highest rights available to a process.
-    - This can be dangerous if used incorrectly.
-    type: bool
-    default: 'no'
+    - The integrity level of the process when I(process_username) is defined
+      and is not equal to C(System).
+    - When C(default), the default integrity level based on the system setup.
+    - When C(elevated), the command will be run with Administrative rights.
+    - When C(limited), the command will be forced to run with
+      non-Administrative rights.
+    choices:
+    - limited
+    - default
+    - elevated
+    default: default
   interactive:
     description:
     - Will run the process as an interactive process that shows a process
@@ -170,10 +157,10 @@ options:
     - normal
     - realtime
     default: normal
-  show_ui_on_win_logon:
+  show_ui_on_logon_screen:
     description:
-    - Shows the process UI on the Winlogon secure desktop when I(use_system) is
-      C(yes).
+    - Shows the process UI on the Winlogon secure desktop when
+      I(process_username) is C(System).
     type: bool
     default: 'no'
   process_timeout:
@@ -184,17 +171,17 @@ options:
   stdin:
     description:
     - Data to send on the stdin pipe once the process has started.
-    - This option does not do anything when I(interactive) or I(asynchronous)
-      is C(yes).
+    - This option has no effect when I(interactive) or I(asynchronous) is
+      C(yes).
 requirements:
 - pypsexec
-- smbprotocol[kerberos] for Kerberos authentication
+- smbprotocol[kerberos] for optional Kerberos authentication
 notes:
-- This module requires the Windows host to have SMB setup and the port 445
-  opened on the firewall.
-- This module will wait until the end process is finished unless
-  I(asynchronous) is C(yes), make sure the process is run as a non-interactive
-  command or send the exit command through the I(stdin) parameter.
+- This module requires the Windows host to have SMB configured and enabled,
+  and port 445 opened on the firewall.
+- This module will wait until the process is finished unless I(asynchronous)
+  is C(yes), ensure the process is run as a non-interactive command to avoid
+  infinite hangs waiting for input.
 - The I(connection_username) must be a member of the local Administrator group
   of the Windows host. For non-domain joined hosts, the
   C(LocalAccountTokenFilterPolicy) should be set to C(1) to ensure this works,
@@ -266,7 +253,7 @@ EXAMPLES = r'''
     connection_username: Administrator
     connection_password: Password01
     encrypt: no
-    elevated: yes
+    integrity_level: elevated
     process_username: Administrator
     process_password: Password01
     executable: powershell.exe
@@ -319,8 +306,9 @@ rc:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils._text import to_bytes, to_native, to_text
+from ansible.module_utils._text import to_bytes, to_text
 
+PYPSEXEC_IMP_ERR = None
 try:
     from pypsexec import client
     from pypsexec.exceptions import PypsexecException, PAExecException, \
@@ -329,30 +317,26 @@ try:
     from smbprotocol.exceptions import SMBException, SMBAuthenticationError, \
         SMBResponseException
     HAS_PYPSEXEC = True
-except ImportError:
+except ImportError as exc:
+    PYPSEXEC_IMP_ERR = exc
     HAS_PYPSEXEC = False
 
+KERBEROS_IMP_ERR = None
 try:
     import gssapi
     # GSSAPI extension required for Kerberos Auth in SMB
     from gssapi.raw import inquire_sec_context_by_oid
     HAS_KERBEROS = True
-except ImportError:
+except ImportError as exc:
+    KERBEROS_IMP_ERR = exc
     HAS_KERBEROS = False
 
 
 def remove_artifacts(module, client):
-    warning_msg = "Failed to cleanup PAExec service and executable"
     try:
         client.remove_service()
-    except PDUException as exc:
-        module.warn("%s with RPC PDU fault: %s" % (warning_msg, to_text(exc)))
-    except SCMRException as exc:
-        module.warn("%s with an SCMR fault: %s" % (warning_msg, to_text(exc)))
-    except PypsexecException as exc:
-        module.warn("%s with generic fault: %s" % (warning_msg, to_text(exc)))
-    except SMBException as exc:
-        module.warn("Failed to remove PAExec executable with SMB fault: %s"
+    except (SMBException, PypsexecException) as exc:
+        module.warn("Failed to cleanup PAExec service and executable: %s"
                     % to_text(exc))
 
 
@@ -366,35 +350,45 @@ def main():
         connection_timeout=dict(type='int', default=60),
         executable=dict(type='str', required=True),
         arguments=dict(type='str'),
-        working_directory=dict(type='str', default='C:\\Windows\\System32'),
+        working_directory=dict(type='str', default=r'C:\Windows\System32'),
         asynchronous=dict(type='bool', default=False),
         load_profile=dict(type='bool', default=True),
         process_username=dict(type='str'),
         process_password=dict(type='str', no_log=True),
-        elevated=dict(type='bool', default=False),
-        limited=dict(type='bool', default=False),
-        use_system=dict(type='bool', default=False),
+        integrity_level=dict(type='str', default='default',
+                             choices=['default', 'elevated', 'limited']),
         interactive=dict(type='bool', default=False),
         interactive_session=dict(type='int', default=0),
         priority=dict(type='str', default='normal',
                       choices=['above_normal', 'below_normal', 'high',
                                'idle', 'normal', 'realtime']),
-        show_ui_on_win_logon=dict(type='bool', default=False),
+        show_ui_on_logon_screen=dict(type='bool', default=False),
         process_timeout=dict(type='int', default=0),
         stdin=dict(type='str')
     )
     result = dict(
         changed=False,
     )
-
     module = AnsibleModule(
         argument_spec=module_args,
         supports_check_mode=False,
-        required_together=[['process_username', 'process_password']]
     )
 
+    process_username = module.params['process_username']
+    process_password = module.params['process_password']
+    use_system = False
+    if process_username is not None and process_username.lower() == "system":
+        use_system = True
+        process_username = None
+        process_password = None
+
+    if process_username is not None and process_password is None:
+        module.fail_json(msg='parameters are required together when not '
+                             'running as System: process_username, '
+                             'process_password')
     if not HAS_PYPSEXEC:
-        module.fail_json(msg='The pypsexec python module is required')
+        module.fail_json(msg='The pypsexec python module is required',
+                         exception=PYPSEXEC_IMP_ERR)
 
     hostname = module.params['hostname']
     connection_username = module.params['connection_username']
@@ -407,11 +401,8 @@ def main():
     working_directory = module.params['working_directory']
     asynchronous = module.params['asynchronous']
     load_profile = module.params['load_profile']
-    process_username = module.params['process_username']
-    process_password = module.params['process_password']
-    elevated = module.params['elevated']
-    limited = module.params['limited']
-    use_system = module.params['use_system']
+    elevated = module.params['integrity_level'] == "elevated"
+    limited = module.params['integrity_level'] == "limited"
     interactive = module.params['interactive']
     interactive_session = module.params['interactive_session']
 
@@ -423,7 +414,7 @@ def main():
         "normal": ProcessPriority.NORMAL_PRIORITY_CLASS,
         "realtime": ProcessPriority.REALTIME_PRIORITY_CLASS
     }[module.params['priority']]
-    show_ui_on_win_logon = module.params['show_ui_on_win_logon']
+    show_ui_on_logon_screen = module.params['show_ui_on_logon_screen']
 
     process_timeout = module.params['process_timeout']
     stdin = module.params['stdin']
@@ -431,7 +422,8 @@ def main():
     if connection_username is None or connection_password is None and \
             not HAS_KERBEROS:
         module.fail_json(msg='The gssapi python module with the GGF extension '
-                             'is required for Kerberos authentication')
+                             'is required for Kerberos authentication',
+                         exception=KERBEROS_IMP_ERR)
 
     win_client = client.Client(server=hostname, username=connection_username,
                                password=connection_password, port=port,
@@ -441,20 +433,18 @@ def main():
         win_client.connect(timeout=connection_timeout)
     except SMBAuthenticationError as exc:
         module.fail_json(msg='Failed to authenticate over SMB: %s'
-                             % to_native(exc))
+                             % to_text(exc))
     except SMBResponseException as exc:
         module.fail_json(msg='Received unexpected SMB response when opening '
-                             'the connection: %s' % to_native(exc))
-    except SMBException as exc:
-        module.fail_json(msg=to_native(exc), exception=exc)
+                             'the connection: %s' % to_text(exc))
     except PDUException as exc:
         module.fail_json(msg='Received an exception with RPC PDU message: %s'
-                             % to_native(exc))
+                             % to_text(exc))
     except SCMRException as exc:
         module.fail_json(msg='Received an exception when dealing with SCMR on '
-                             'the Windows host: %s' % to_native(exc))
-    except PypsexecException as exc:
-        module.fail_json(msg=to_native(exc), exception=exc)
+                             'the Windows host: %s' % to_text(exc))
+    except (SMBException, PypsexecException) as exc:
+        module.fail_json(msg=to_text(exc))
 
     # create PAExec service and run the process
     result['changed'] = True
@@ -465,60 +455,37 @@ def main():
         run_elevated=elevated, run_limited=limited,
         username=process_username, password=process_password,
         use_system_account=use_system, working_dir=working_directory,
-        priority=priority, show_ui_on_win_logon=show_ui_on_win_logon,
+        priority=priority, show_ui_on_win_logon=show_ui_on_logon_screen,
         timeout_seconds=process_timeout, stdin=b_stdin
     )
-    if not module.check_mode:
-        try:
-            win_client.create_service()
-        except PDUException as exc:
-            module.fail_json(msg='Failed to create PAExec service with PDU '
-                                 'fault: %s' % to_text(exc))
-        except SCMRException as exc:
-            module.fail_json(msg='Failed to create PAExec service with SCMR '
-                                 'Error: %s' % to_text(exc))
-        except PypsexecException as exc:
-            module.fail_json(msg=to_text(exc), exception=exc)
-        except SMBException as exc:
-            module.fail_json(msg=to_text(exc), exception=exc)
+    try:
+        win_client.create_service()
+    except (SMBException, PypsexecException) as exc:
+        module.fail_json(msg='Failed to create PAExec service: %s'
+                         % to_text(exc))
 
-        try:
-            proc_result = win_client.run_executable(**run_args)
-        except PAExecException as exc:
-            module.fail_json(msg='Received error when running remote process: '
-                                 '%s' % to_text(exc))
-        except PypsexecException as exc:
-            module.fail_json(msg=to_text(exc), exception=exc)
-        finally:
-            remove_artifacts(module, win_client)
+    try:
+        proc_result = win_client.run_executable(**run_args)
+    except (SMBException, PypsexecException) as exc:
+        module.fail_json(msg='Received error when running remote process: %s'
+                         % to_text(exc))
+    finally:
+        remove_artifacts(module, win_client)
 
-        if asynchronous:
-            result['pid'] = proc_result[2]
-        elif interactive:
-            result['rc'] = proc_result[2]
-        else:
-            result['stdout'] = proc_result[0]
-            result['stderr'] = proc_result[1]
-            result['rc'] = proc_result[2]
+    if asynchronous:
+        result['pid'] = proc_result[2]
+    elif interactive:
+        result['rc'] = proc_result[2]
+    else:
+        result['stdout'] = proc_result[0]
+        result['stderr'] = proc_result[1]
+        result['rc'] = proc_result[2]
 
     # close the SMB connection
     try:
         win_client.disconnect()
-    except SMBResponseException as exc:
-        module.warn('Received an unexpected SMB response when closing the SMB '
-                    'connection: %s' % to_native(exc))
-    except SMBException as exc:
-        module.warn('Unexpected SMB exception was raised when closing the SMB '
-                    'connection: %s' % to_native(exc))
-    except PDUException as exc:
-        module.warn('Received an unexpected PDU fault when closing SCMR: %s'
-                    % to_native(exc))
-    except SCMRException as exc:
-        module.warn('Received an unexpected SCMR fault when closing SCMR: %s'
-                    % to_native(exc))
-    except PypsexecException as exc:
-        module.warn('Unexpected pypsexec exception was raised when closing '
-                    'the SMB connection' % to_text(exc))
+    except (SMBException, PypsexecException) as exc:
+        module.warn("Failed to close the SMB connection: %s" % to_text(exc))
 
     module.exit_json(**result)
 
