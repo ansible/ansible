c344d72d464baa8ae49bc8b4633b4bbd89189f4c
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2018-04-23 19:00:52 +0300

    Modify netscaler document fragments to reflect the changes in netscaler module utils.

--- lib/ansible/utils/module_docs_fragments/netscaler.py
+++ lib/ansible/utils/module_docs_fragments/netscaler.py
@@ -6,17 +6,20 @@ options:
         description:
             - The ip address of the netscaler appliance where the nitro API calls will be made.
             - "The port can be specified with the colon (:). E.g. 192.168.1.1:555."
+        aliases: mas_ip
         required: True
 
     nitro_user:
         description:
             - The username with which to authenticate to the netscaler node.
-        required: True
+        required: False
+        aliases: mas_user
 
     nitro_pass:
         description:
             - The password with which to authenticate to the netscaler node.
-        required: True
+        required: False
+        aliases: mas_pass
 
     nitro_protocol:
         choices: [ 'http', 'https' ]
@@ -49,6 +52,26 @@ options:
             - The module will not save the configuration on the netscaler node if it made no changes.
         type: bool
         default: true
+
+    mas_proxy_call:
+        description:
+            - If true the underlying NITRO API calls made by the module will be proxied through a MAS node to the target Netscaler instance.
+            - When true you must also define the following options: I(nitro_auth_token), I(instance_ip).
+        type: bool
+        default: false
+        version_added: "2.6.0"
+
+    nitro_auth_token:
+        description:
+            - The authentication token provided by a login operation.
+        aliases: mas_auth_token
+        version_added: "2.6.0"
+
+    instance_ip:
+        description:
+            - The target Netscaler instance ip address to which all underlying NITRO API calls will be proxied to.
+            - It is meaningful only when having set C(mas_proxy_call) to C(true)
+        version_added: "2.6.0"
 notes:
   - For more information on using Ansible to manage Citrix NetScaler Network devices see U(https://www.ansible.com/ansible-netscaler).
 '''
