ee8d0af2f9bc59e5028c8849e62c6ac8a2843af9
Tim Rupp <caphrim007@gmail.com>
2018-04-25 07:16:11 -0700

    Adds various features and fixes (#39271)
    * a refactor of pool member and node modules to be inline with current f5 conventions
    * Added priority_group_activation to pools
    * various other small convention fixes and bug fixes

--- test/units/modules/network/f5/test_bigip_node.py
+++ test/units/modules/network/f5/test_bigip_node.py
@@ -20,9 +20,9 @@ from ansible.compat.tests.mock import patch
 from ansible.module_utils.basic import AnsibleModule
 
 try:
-    from library.bigip_node import Parameters
-    from library.bigip_node import ModuleManager
-    from library.bigip_node import ArgumentSpec
+    from library.modules.bigip_node import Parameters
+    from library.modules.bigip_node import ModuleManager
+    from library.modules.bigip_node import ArgumentSpec
     from library.module_utils.network.f5.common import F5ModuleError
     from library.module_utils.network.f5.common import iControlUnexpectedHTTPError
     from test.unit.modules.utils import set_module_args
