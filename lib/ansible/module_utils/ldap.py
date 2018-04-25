f4f685a031783a867c02f2246860e61d65ef6137
The Fox in the Shell <KellerFuchs@hashbang.sh>
2018-04-25 23:03:14 +0200

    fixup! Update copyright info for ldap modules

--- lib/ansible/module_utils/ldap.py
+++ lib/ansible/module_utils/ldap.py
@@ -1,21 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright 2017-2018 Keller Fuchs (@kellerfuchs) <kellerfuchs@hashbang.sh>
-
-# This file is part of Ansible by Red Hat
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# Simplified BSD License (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
 
 import traceback
 from ansible.module_utils._text import to_native
