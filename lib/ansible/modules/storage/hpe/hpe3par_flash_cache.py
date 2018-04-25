ac77a24b92aa0d3096c112d31cb915da3c9965a6
Farhan Nomani <nomani@hpe.com>
2018-04-25 14:43:16 +0530

    Fixing pep8 issues

--- lib/ansible/modules/storage/hpe/hpe3par_flash_cache.py
+++ lib/ansible/modules/storage/hpe/hpe3par_flash_cache.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 
 # (C) Copyright 2018 Hewlett Packard Enterprise Development LP
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of version 3 of the GNU General Public License as
 # published by the Free Software Foundation.  Alternatively, at your
 # choice, you may also redistribute it and/or modify it under the terms
 # of the Apache License, version 2.0, available at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 # or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 # for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <https://www.gnu.org/licenses/>
 
@@ -27,7 +27,7 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = r'''
 ---
 author: "Farhan Nomani (nomani@hpe.com)"
-description: "On HPE 3PAR - Create Flash Cache 
+description: "On HPE 3PAR - Create Flash Cache
  - Delete Flash Cache."
 module: hpe3par_flash_cache
 options:
