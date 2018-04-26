3fab448231943deba5cc39db222b65166eba6fb4
Abhijeet Kasurde <akasurde@redhat.com>
2018-04-26 11:43:25 +0530

    Include review comments from mattclay
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- test/integration/targets/vmware_inventory/runme.sh
+++ test/integration/targets/vmware_inventory/runme.sh
@@ -10,22 +10,6 @@ export vcenter_user="${vcenter_user:-user}"
 export vcenter_pass="${vcenter_pass:-pass}"
 VMWARE_CONFIG=test-config.vmware.yaml
 
-# flag for checking whether cleanup has already fired
-_is_clean=
-
-function _cleanup() {
-    [[ -n "$_is_clean" ]] && return  # don't double-clean
-    echo Cleanup: removing ${VMWARE_CONFIG}...
-    rm -vf "$VMWARE_CONFIG"
-    unset ANSIBLE_CONFIG
-    unset vcenter_host
-    unset vcenter_user
-    unset vcenter_pass
-    unset VMWARE_CONFIG
-    _is_clean=1
-}
-trap _cleanup INT TERM EXIT
-
 cat > "$VMWARE_CONFIG" <<VMWARE_YAML
 plugin: vmware_inventory
 strict: False
@@ -37,8 +21,7 @@ with_tags: False
 VMWARE_YAML
 
 echo "Installing Pyvmomi"
-pip install pyvmomi
-pip2 install pyvmomi
+${ANSIBLE_TEST_PYTHON_INTERPRETER} -m pip install pyvmomi
 
 echo "Kill all previous instances"
 curl "http://${vcenter_host}:5000/killall" > /dev/null 2>&1
