92b05838d1b8f2c2551d05ff0064602f2ac1a56f
t2d <td@tem.li>
2018-04-27 16:32:04 +0200

    Minimize output of actionable callback plugin
    * Only print playbook banner, when a task has changed/failed
    * Only print stats, when a task has changed/failed

--- lib/ansible/plugins/callback/actionable.py
+++ lib/ansible/plugins/callback/actionable.py
@@ -37,23 +37,30 @@ class CallbackModule(CallbackModule_default):
     def __init__(self):
         self.super_ref = super(CallbackModule, self)
         self.super_ref.__init__()
-        self.last_task = None
-        self.last_task_banner = None
+        self.last_play = None
+        self.shown_play = False
         self.shown_title = False
+        self.anything_shown = False
 
     def v2_playbook_on_handler_task_start(self, task):
         self.super_ref.v2_playbook_on_handler_task_start(task)
         self.shown_title = True
 
+    def v2_playbook_on_play_start(self, play):
+        self.last_play = play
+        self.shown_play = False
+
     def v2_playbook_on_task_start(self, task, is_conditional):
-        self.last_task = task
-        self.last_task_banner = self._get_task_banner(task)
         self.shown_title = False
 
-    def display_task_banner(self):
+    def display_task_banner(self, task):
+        if not self.shown_play:
+            self.super_ref.v2_playbook_on_play_start(self.last_play)
+            self.shown_play = True
         if not self.shown_title:
-            self.super_ref.v2_playbook_on_task_start(self.last_task, None)
+            self.super_ref.v2_playbook_on_task_start(task, None)
             self.shown_title = True
+        self.anything_shown = True
 
     def _print_task_banner(self, task):
         self._display.banner(self.last_task_banner)
@@ -83,32 +90,46 @@ class CallbackModule(CallbackModule_default):
         return u"TASK [%s%s]" % (task.get_name().strip(), args)
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
-        self.display_task_banner()
+        self.display_task_banner(result._task)
         self.super_ref.v2_runner_on_failed(result, ignore_errors)
 
     def v2_runner_on_ok(self, result):
         if result._result.get('changed', False):
-            self.display_task_banner()
+            self.display_task_banner(result._task)
             self.super_ref.v2_runner_on_ok(result)
 
     def v2_runner_on_unreachable(self, result):
-        self.display_task_banner()
+        self.display_task_banner(result._task)
         self.super_ref.v2_runner_on_unreachable(result)
 
     def v2_runner_on_skipped(self, result):
         pass
 
+    def v2_playbook_on_no_hosts_matched(self):
+        pass
+
     def v2_playbook_on_include(self, included_file):
         pass
 
+    def v2_playbook_on_stats(self, stats):
+        if self.anything_shown:
+            self.super_ref.v2_playbook_on_stats(stats)
+
+    def v2_on_file_diff(self, result):
+        if 'diff' in result._result and result._result['diff'] and result._result.get('changed', False):
+            diff = self._get_diff(result._result['diff'])
+            if diff:
+                self.display_task_banner(result._task)
+                self._display.display(diff)
+
     def v2_runner_item_on_ok(self, result):
         if result._result.get('changed', False):
-            self.display_task_banner()
+            self.display_task_banner(result._task)
             self.super_ref.v2_runner_item_on_ok(result)
 
     def v2_runner_item_on_skipped(self, result):
         pass
 
     def v2_runner_item_on_failed(self, result):
-        self.display_task_banner()
+        self.display_task_banner(result._task)
         self.super_ref.v2_runner_item_on_failed(result)
