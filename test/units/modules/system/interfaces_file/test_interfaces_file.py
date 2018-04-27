0d5b69bd41ceef8297c4593037fba109a63f4d58
Olivier Bourdon <olivierbourdon38@gmail.com>
2018-04-27 11:18:19 +0200

    Fix for changing method only on specified interface (#39015)
    Test added for interface method change

--- test/units/modules/system/interfaces_file/test_interfaces_file.py
+++ test/units/modules/system/interfaces_file/test_interfaces_file.py
@@ -188,3 +188,35 @@ class TestInterfacesFileModule(unittest.TestCase):
                 self.compareInterfacesLinesToFile(lines, testfile, "%s_%s" % (testfile, testname))
                 self.compareInterfacesToFile(ifaces, testfile, "%s_%s.json" % (testfile, testname))
                 self.compareFileToBackup(path, backupp)
+
+    def test_change_method(self):
+        testcases = {
+            "change_method": [
+                {
+                    'iface': 'eth0',
+                    'option': 'method',
+                    'value': 'manual',
+                    'state': 'present',
+                }
+            ],
+        }
+        for testname, options_list in testcases.items():
+            for testfile in self.getTestFiles():
+                path = os.path.join(fixture_path, testfile)
+                lines, ifaces = interfaces_file.read_interfaces_file(module, path)
+                backupp = module.backup_local(path)
+                options = options_list[0]
+                fail_json_iterations = []
+                try:
+                    _, lines = interfaces_file.setInterfaceOption(module, lines, options['iface'], options['option'], options['value'], options['state'])
+                except AnsibleFailJson as e:
+                    fail_json_iterations.append("fail_json message: %s\noptions:\n%s" %
+                                                (str(e), json.dumps(options, sort_keys=True, indent=4, separators=(',', ': '))))
+                interfaces_file.write_changes(module, [d['line'] for d in lines if 'line' in d], path)
+
+                self.compareStringWithFile("\n=====\n".join(fail_json_iterations), "%s_%s.exceptions.txt" % (testfile, testname))
+
+                self.compareInterfacesLinesToFile(lines, testfile, "%s_%s" % (testfile, testname))
+                self.compareInterfacesToFile(ifaces, testfile, "%s_%s.json" % (testfile, testname))
+                # Restore backup
+                move(backupp, path)
