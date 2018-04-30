6e7fe2b2b6da5244da6bf18ce023a55b76d30576
Tim Rupp <caphrim007@gmail.com>
2018-04-30 09:48:41 -0700

    Various f5 module fixes (#39510)
    Mainly fixing coding conventions. Also, added ipv6 suppor to pool members

--- test/units/modules/network/f5/test_bigip_monitor_snmp_dca.py
+++ test/units/modules/network/f5/test_bigip_monitor_snmp_dca.py
@@ -21,9 +21,9 @@ from ansible.compat.tests.mock import patch
 from ansible.module_utils.basic import AnsibleModule
 
 try:
-    from library.bigip_monitor_snmp_dca import Parameters
-    from library.bigip_monitor_snmp_dca import ModuleManager
-    from library.bigip_monitor_snmp_dca import ArgumentSpec
+    from library.modules.bigip_monitor_snmp_dca import Parameters
+    from library.modules.bigip_monitor_snmp_dca import ModuleManager
+    from library.modules.bigip_monitor_snmp_dca import ArgumentSpec
     from library.module_utils.network.f5.common import F5ModuleError
     from library.module_utils.network.f5.common import iControlUnexpectedHTTPError
     from test.unit.modules.utils import set_module_args
