2568de76a27beb164c04a114fc27411b39c103f2
nerzhul <loic.blot@unix-experience.fr>
2018-04-30 15:10:11 +0200

    doc fix

--- lib/ansible/modules/cloud/amazon/aws_sgw_facts.py
+++ lib/ansible/modules/cloud/amazon/aws_sgw_facts.py
@@ -20,21 +20,25 @@ options:
   gather_local_disks:
     description:
       - Gather local disks attached to the storage gateway.
+    type: bool
     required: false
     default: true
   gather_tapes:
     description:
       - Gather tape information for storage gateways in tape mode.
+    type: bool
     required: false
     default: true
   gather_file_shares:
     description:
       - Gather file share information for storage gateways in s3 mode.
+    type: bool
     required: false
     default: true
   gather_volumes:
     description:
       - Gather volume information for storage gateways in iSCSI (cached & stored) modes.
+    type: bool
     required: false
     default: true
 extends_documentation_fragment:
