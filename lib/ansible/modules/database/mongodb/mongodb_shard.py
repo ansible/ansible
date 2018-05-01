38a122db1827ad2633ecf7ab5c8fe8dd6689ab53
Rhys Campbell <rhys.james.campbell@googlemail.com>
2018-05-01 17:04:39 +0200

    Update copyright

--- lib/ansible/modules/database/mongodb/mongodb_shard.py
+++ lib/ansible/modules/database/mongodb/mongodb_shard.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 # (c) 2017, Rhys Campbell <rhys.james.campbell@googlemail.com>
-#
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
