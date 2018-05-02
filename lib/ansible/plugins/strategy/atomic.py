8b72b17f3cb807f3d54a39444de8176db23f625d
Tim Strazny <tim.strazny@energymeteo.de>
2018-05-02 16:09:09 +0200

    Fix "line too long"

--- lib/ansible/plugins/strategy/atomic.py
+++ lib/ansible/plugins/strategy/atomic.py
@@ -23,7 +23,8 @@ DOCUMENTATION = '''
     short_description: Executes tasks on each host without interruption
     description:
         - Task execution is as fast as possible per host in batch as defined by C(serial) (default all).
-          Ansible will not start a play for a host unless the play can be finished without interruption by tasks for another host, i.e. the number of hosts with an active play does not exceed the number of forks.
+          Ansible will not start a play for a host unless the play can be finished without interruption by tasks for another host,
+          i.e. the number of hosts with an active play does not exceed the number of forks.
           Ansible will not wait for other hosts to finish the current task before queuing the next task for a host that has finished.
           Once a host is done with the play, it opens it's slot to a new host that was waiting to start.
           Other than that, it behaves just like the "free" strategy.
