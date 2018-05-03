8d74438b264890d72ba24a3573eade179f610baa
Abhijeet Kasurde <akasurde@redhat.com>
2018-05-03 09:44:51 +0530

    Fixed error handling in github_issue module
    Due to recent changes in github3.py library module stopped working.
    This fix adds extra error handling for new changes in library.
    
    Fixes: #39627
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/source_control/github_issue.py
+++ lib/ansible/modules/source_control/github_issue.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python
-# (c) 2017, Abhijeet Kasurde <akasurde@redhat.com>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# Copyright: (c) 2017-18, Abhijeet Kasurde <akasurde@redhat.com>
+#
+#  GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 
-ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
 
 
 DOCUMENTATION = '''
 module: github_issue
 short_description: View GitHub issue.
 description:
-    - View GitHub issue for a given repository.
+    - View GitHub issue for a given repository and organization.
 version_added: "2.4"
 options:
   repo:
@@ -34,13 +38,12 @@ options:
     description:
         - Get various details about issue depending upon action specified.
     default: 'get_status'
-    choices:
-        - ['get_status']
+    choices: ['get_status']
 
 author:
     - Abhijeet Kasurde (@Akasurde)
 requirements:
-    - "github3.py >= 1.0.0a4"
+    - "github3.py >= 1.0.0"
 '''
 
 RETURN = '''
@@ -84,15 +87,24 @@ def main():
         argument_spec=dict(
             organization=dict(required=True),
             repo=dict(required=True),
-            issue=dict(required=True),
-            action=dict(required=False, choices=['get_status']),
+            issue=dict(type='int', required=True),
+            action=dict(choices=['get_status'], default='get_status'),
         ),
         supports_check_mode=True,
     )
 
     if not HAS_GITHUB_PACKAGE:
-        module.fail_json(msg=missing_required_lib('github3.py >= 1.0.0a4'),
+        module.fail_json(msg=missing_required_lib('github3.py >= 1.0.0'),
                          exception=GITHUB_IMP_ERR)
+
+    try:
+        current_version = tuple(map(int, github3.__version__.split(".")))
+        if current_version < (1, 0, 0):
+            raise ValueError
+    except ValueError:
+        module.fail_json(msg="Failed to parse github3.py version."
+                             " Please make sure you have correct version install."
+                             " Required is >= 1.0.0, got %s" % github3.__version__)
 
     organization = module.params['organization']
     repo = module.params['repo']
@@ -100,12 +112,18 @@ def main():
     action = module.params['action']
 
     result = dict()
+    gh_obj = None
+    not_found = False
 
-    gh_obj = github3.issue(organization, repo, issue)
-    if gh_obj is None:
+    try:
+        gh_obj = github3.issue(organization, repo, issue)
+    except (github3.exceptions.NotFoundError, github3.exceptions.GitHubError, ValueError):
+        not_found = True
+
+    if gh_obj is None or not_found:
         module.fail_json(msg="Failed to get details about issue specified. "
-                             "Please check organization, repo and issue "
-                             "details and try again.")
+                             "Please check organization [%s], repo [%s] and issue "
+                             "details [%s] and try again." % (organization, repo, issue))
 
     if action == 'get_status' or action is None:
         if module.check_mode:
