bb6d6bd6eae0391d633cc0d7cc657e274a601ff3
Brian Coca <brian.coca+git@gmail.com>
2018-05-03 15:46:54 -0400

    import

--- lib/ansible/playbook/loop_control.py
+++ lib/ansible/playbook/loop_control.py
@@ -20,7 +20,7 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 from ansible.playbook.attribute import FieldAttribute
-from ansible.playbook.base import Base
+from ansible.playbook.base import FieldAttributeBase
 
 
 class LoopControl(FieldAttributeBase):
