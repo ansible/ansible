8e55c96d88a0dac7e793da603bfaf0d0da5ef3f6
Mickaël Guérin <kael@crocobox.org>
2018-05-04 21:33:32 +0200

    Fix gathering facts in run_once play (#39453)
    * Fix gathering facts in run_once play
    
    Fixes https://github.com/ansible/ansible/issues/39312
    
    * Check that run_once doesn't prevent fact gathering

--- test/integration/targets/gathering_facts/runme.sh
+++ test/integration/targets/gathering_facts/runme.sh
@@ -5,3 +5,5 @@ set -eux
 # ANSIBLE_CACHE_PLUGINS=cache_plugins/ ANSIBLE_CACHE_PLUGIN=none ansible-playbook test_gathering_facts.yml -i ../../inventory -v "$@"
 ansible-playbook test_gathering_facts.yml -i ../../inventory -v "$@"
 #ANSIBLE_CACHE_PLUGIN=base ansible-playbook test_gathering_facts.yml -i ../../inventory -v "$@"
+
+ANSIBLE_GATHERING=smart ansible-playbook test_run_once.yml -i ../../inventory -v "$@"
