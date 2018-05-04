718bc79fcc714aa615d1f9add12d865fa1464f00
Yuwei Zhou <yuwzho@microsoft.com>
2018-05-04 13:17:05 +0800

    Fixes #37731 Create Internal Loadbalancers with azure_rm_loadbalancer, support SKU (#38077)
    * create internal loadbalancer
    
    * fix test
    
    * remove duplicate test
    
    * clean up
    
    * fix doc
    
    * lint
    
    * add sku support
    
    * update version
    
    * change to the version the same as CLI
    
    * add pip support sku
    
    * fix lint
    
    * fix test
    
    * Update main.yml
    
    * add changelog entry

--- lib/ansible/module_utils/azure_rm_common.py
+++ lib/ansible/module_utils/azure_rm_common.py
@@ -933,13 +933,13 @@ class AzureRMModuleBase(object):
         if not self._network_client:
             self._network_client = self.get_mgmt_svc_client(NetworkManagementClient,
                                                             base_url=self._cloud_environment.endpoints.resource_manager,
-                                                            api_version='2017-06-01')
+                                                            api_version='2017-11-01')
         return self._network_client
 
     @property
     def network_models(self):
         self.log("Getting network models...")
-        return NetworkManagementClient.models("2017-06-01")
+        return NetworkManagementClient.models("2017-11-01")
 
     @property
     def rm_client(self):
