792a2b97b294ff9e8396138bc34319ff2462907a
Matt Martz <matt@sivel.net>
2018-05-04 09:40:08 -0500

    Fix E126, E123 pep8 errors, and ansible-format-automatic-specification pylint error (#39740)

--- lib/ansible/modules/cloud/ovirt/ovirt_permissions_facts.py
+++ lib/ansible/modules/cloud/ovirt/ovirt_permissions_facts.py
@@ -94,10 +94,14 @@ from ansible.module_utils.ovirt import (
 def _permissions_service(connection, module):
     if module.params['user_name']:
         service = connection.system_service().users_service()
-        entity = next(iter(service.list(search='usrname={}'.format(
-                    '{}@{}'.format(module.params['user_name'], module.params['authz_name'])
+        entity = next(
+            iter(
+                service.list(
+                    search='usrname={0}'.format(
+                        '{0}@{1}'.format(module.params['user_name'], module.params['authz_name'])
+                    )
                 )
-            )),
+            ),
             None
         )
     else:
