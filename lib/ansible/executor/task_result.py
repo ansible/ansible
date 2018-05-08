7c0c4478777b376a909487f9d6f6b545fc424cef
Matt Martz <matt@sivel.net>
2018-05-08 13:16:48 -0500

    Honor ignore_errors when invoking the debugger, add config to disable this behavior. Fixes #39863

--- lib/ansible/executor/task_result.py
+++ lib/ansible/executor/task_result.py
@@ -7,6 +7,7 @@ __metaclass__ = type
 
 from copy import deepcopy
 
+from ansible import constants as C
 from ansible.parsing.dataloader import DataLoader
 from ansible.vars.clean import strip_internal_keys
 
@@ -66,18 +67,19 @@ class TaskResult:
 
     def needs_debugger(self, globally_enabled=False):
         _debugger = self._task_fields.get('debugger')
+        _ignore_errors = C.TASK_DEBUGGER_IGNORE_ERRORS and self._task_fields.get('ignore_errors')
 
         ret = False
-        if globally_enabled and (self.is_failed() or self.is_unreachable()):
+        if globally_enabled and not _ignore_errors and (self.is_failed() or self.is_unreachable()):
             ret = True
 
         if _debugger in ('always',):
             ret = True
         elif _debugger in ('never',):
             ret = False
-        elif _debugger in ('on_failed',) and self.is_failed():
+        elif _debugger in ('on_failed',) and self.is_failed() and not _ignore_errors:
             ret = True
-        elif _debugger in ('on_unreachable',) and self.is_unreachable():
+        elif _debugger in ('on_unreachable',) and self.is_unreachable() and not _ignore_errors:
             ret = True
         elif _debugger in('on_skipped',) and self.is_skipped():
             ret = True
