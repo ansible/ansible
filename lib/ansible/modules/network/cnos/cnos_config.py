3b1561a4dab64dedf61cfd2f3b1c6590e0a65b76
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-05-10 18:33:44 +0530

    Update cnos_config.py

--- lib/ansible/modules/network/cnos/cnos_config.py
+++ lib/ansible/modules/network/cnos/cnos_config.py
@@ -28,7 +28,7 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = """
 ---
 module: cnos_config
-version_added: "2.5"
+version_added: "2.6"
 author: "Anil Kumar Muraleedharan (@amuraleedhar)"
 short_description: Manage Lenovo CNOS configuration sections
 description:
@@ -36,10 +36,63 @@ description:
     for segmenting configuration into sections.  This module provides
     an implementation for working with CNOS configuration sections in
     a deterministic way.
-extends_documentation_fragment: cnos
 notes:
   - Tested against CNOS 10.8.0.42
 options:
+  authorize:
+    description:
+      - Instructs the module to enter privileged mode on the remote device
+        before sending any commands.  If not specified, the device will
+        attempt to execute all commands in non-privileged mode. If the value
+        is not specified in the task, the value of environment variable
+        C(ANSIBLE_NET_AUTHORIZE) will be used instead.
+    type: bool
+    default: 'no'
+  auth_pass:
+    description:
+      - Specifies the password to use if required to enter privileged mode
+        on the remote device.  If I(authorize) is false, then this argument
+        does nothing. If the value is not specified in the task, the value of
+        environment variable C(ANSIBLE_NET_AUTH_PASS) will be used instead.
+  provider:
+    description:
+      - A dict object containing connection details.
+    suboptions:
+      host:
+        description:
+          - Specifies the DNS host name or address for connecting to the remote
+            device over the specified transport.  The value of host is used as
+            the destination address for the transport.
+        required: true
+      port:
+        description:
+          - Specifies the port to use when building the connection to the remote device.
+        default: 22
+      username:
+        description:
+          - Configures the username to use to authenticate the connection to
+            the remote device.  This value is used to authenticate
+            the SSH session. If the value is not specified in the task, the
+            value of environment variable C(ANSIBLE_NET_USERNAME) will be used instead.
+      password:
+        description:
+          - Specifies the password to use to authenticate the connection to
+            the remote device.   This value is used to authenticate
+            the SSH session. If the value is not specified in the task, the
+            value of environment variable C(ANSIBLE_NET_PASSWORD) will be used instead.
+      timeout:
+        description:
+          - Specifies the timeout in seconds for communicating with the network device
+            for either connecting or sending commands.  If the timeout is
+            exceeded before the operation is completed, the module will error.
+        default: 10
+      ssh_keyfile:
+        description:
+          - Specifies the SSH key to use to authenticate the connection to
+            the remote device.   This value is the path to the
+            key used to authenticate the SSH session. If the value is not specified
+            in the task, the value of environment variable C(ANSIBLE_NET_SSH_KEYFILE)
+            will be used instead.
   lines:
     description:
       - The ordered set of commands that should be configured in the
