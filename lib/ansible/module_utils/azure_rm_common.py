34616b891667b529bb55261dd86ff20f8f193861
scassidy-ukcloud <37589942+scassidy-ukcloud@users.noreply.github.com>
2018-05-10 11:16:12 +0100

    Update azure_rm_common.py
    fixed syntax error (closing ")" on line 981)

--- lib/ansible/module_utils/azure_rm_common.py
+++ lib/ansible/module_utils/azure_rm_common.py
@@ -977,7 +977,7 @@ class AzureRMModuleBase(object):
         if not self._compute_client:
             self._compute_client = self.get_mgmt_svc_client(ComputeManagementClient,
                                                             base_url=self._cloud_environment.endpoints.resource_manager,
-                                                            api_version=self.get_compute_management_api(self.api_profile)
+                                                            api_version=self.get_compute_management_api(self.api_profile))
         return self._compute_client
 
     @property
