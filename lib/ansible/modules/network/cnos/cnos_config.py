c5ef54a8b547b4f4190610db85a0c319ed128d0b
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-05-11 08:53:37 +0530

    Update cnos_config.py

--- lib/ansible/modules/network/cnos/cnos_config.py
+++ lib/ansible/modules/network/cnos/cnos_config.py
@@ -39,23 +39,6 @@ description:
 notes:
   - Tested against CNOS 10.8.0.42
 options:
-  authorize:
-    version_added: "2.6"
-    description:
-      - Instructs the module to enter privileged mode on the remote device
-        before sending any commands.  If not specified, the device will
-        attempt to execute all commands in non-privileged mode. If the value
-        is not specified in the task, the value of environment variable
-        C(ANSIBLE_NET_AUTHORIZE) will be used instead.
-    type: bool
-    default: 'no'
-  auth_pass:
-    version_added: "2.6"
-    description:
-      - Specifies the password to use if required to enter privileged mode
-        on the remote device.  If I(authorize) is false, then this argument
-        does nothing. If the value is not specified in the task, the value of
-        environment variable C(ANSIBLE_NET_AUTH_PASS) will be used instead.
   provider:
     version_added: "2.6"
     description:
