48e57918609da96d5d1d3afb721d386a3f1422b1
Tim Rupp <caphrim007@gmail.com>
2018-05-11 07:55:48 -0700

    Minor fixes and additions to f5 modules (#39987)
    * Fixes to docs
    * Fix escaping quotes in bigip_command
    * F5 coding conventions cleanup
    * “warn” and “chdir” parameters added to bigip_command

--- test/units/modules/network/f5/test_bigip_device_connectivity.py
+++ test/units/modules/network/f5/test_bigip_device_connectivity.py
@@ -21,10 +21,10 @@ from ansible.compat.tests.mock import patch
 from ansible.module_utils.basic import AnsibleModule
 
 try:
-    from library.bigip_device_connectivity import ApiParameters
-    from library.bigip_device_connectivity import ModuleParameters
-    from library.bigip_device_connectivity import ModuleManager
-    from library.bigip_device_connectivity import ArgumentSpec
+    from library.modules.bigip_device_connectivity import ApiParameters
+    from library.modules.bigip_device_connectivity import ModuleParameters
+    from library.modules.bigip_device_connectivity import ModuleManager
+    from library.modules.bigip_device_connectivity import ArgumentSpec
     from library.module_utils.network.f5.common import F5ModuleError
     from library.module_utils.network.f5.common import iControlUnexpectedHTTPError
     from test.unit.modules.utils import set_module_args
