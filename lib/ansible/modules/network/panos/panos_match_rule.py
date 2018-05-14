7b14c8eb779edebbdfcdf16f1a7f0f9b54f3ba97
Jon Dufresne <jon.dufresne@gmail.com>
2018-05-14 15:41:47 -0700

    Update all pypi.python.org URLs to pypi.org (#38988)
    For details on the new PyPI, see the blog post:
    
    https://pythoninsider.blogspot.ca/2018/04/new-pypi-launched-legacy-pypi-shutting.html

--- lib/ansible/modules/network/panos/panos_match_rule.py
+++ lib/ansible/modules/network/panos/panos_match_rule.py
@@ -33,8 +33,8 @@ description:
 author: "Robert Hagen (@rnh556)"
 version_added: "2.5"
 requirements:
-    - pan-python can be obtained from PyPi U(https://pypi.python.org/pypi/pan-python)
-    - pandevice can be obtained from PyPi U(https://pypi.python.org/pypi/pandevice)
+    - pan-python can be obtained from PyPi U(https://pypi.org/project/pan-python/)
+    - pandevice can be obtained from PyPi U(https://pypi.org/project/pandevice/)
 notes:
     - Checkmode is not supported.
     - Panorama NOT is supported.
