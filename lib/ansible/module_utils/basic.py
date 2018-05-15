e31071023f0960af94fa022a382472c785fc61f4
Jordan Borean <jborean93@gmail.com>
2018-05-16 07:56:22 +1000

    Added warning to dir creation

--- lib/ansible/module_utils/basic.py
+++ lib/ansible/module_utils/basic.py
@@ -939,6 +939,11 @@ class AnsibleModule(object):
         if self._tmpdir is None:
             basedir = os.path.expanduser(os.path.expandvars(self._remote_tmp))
             if not os.path.exists(basedir):
+                self.warn("Module remote_tmp %s did not exist and was created "
+                          "with a mode of 0700, this may cause issues when "
+                          "running as another user. To avoid this, create the "
+                          "remote_tmp dir with the correct permissions "
+                          "manually" % basedir)
                 os.makedirs(basedir, mode=0o700)
 
             basefile = "ansible-moduletmp-%s-" % time.time()
