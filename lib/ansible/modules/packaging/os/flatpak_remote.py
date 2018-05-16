0163eb614c37eb6b25ea55e62cc90b40c34855a7
Alexander Bethke <oolongbrothers@gmx.net>
2018-05-16 23:06:32 +0200

    Renames remote > flatpakrepo_url and reworks documentation

--- lib/ansible/modules/packaging/os/flatpak_remote.py
+++ lib/ansible/modules/packaging/os/flatpak_remote.py
@@ -17,99 +17,116 @@ DOCUMENTATION = r'''
 ---
 module: flatpak_remote
 version_added: '2.6'
-requirements:
-- flatpak
+short_description: Manage flatpak repository remotes
+description:
+- Allows users to add or remove flatpak remotes.
+- The flatpak remotes concept is comparable to what is called repositories in other packaging
+  formats.
+- Currently, remote addition is only supported via I(flatpakrepo) file URLs.
+- Existing remotes will not be updated.
+- See the M(flatpak) module for managing flatpaks.
 author:
 - John Kwiatkoski (@jaykayy)
 - Alexander Bethke (@oolongbrothers)
-short_description: Manage flatpak repository remotes
-description:
-- The C(flatpak_remote) module adds or removes flatpak repository remotes.
-notes:
-- The C(flatpak_remote) requires the C(flatpak) binary the be installed on the managed host.
+requirements:
+- flatpak
 options:
-  name:
-    description:
-    - The name of the flatpak repository to manage
-    - When I(state) is set to C(present), I(name) is added as a remote for installing flatpaks.
-    - When used with I(state=absent) the remote with that name will be removed.
-    required: true
-  remote:
-    description:
-    - The URL of the flatpak remote to add
-    - When I(state) is set to C(present), I(remote) URL is added as a flatpak remote for the
-      specified installation C(method).
-    - When used with I(state=absent), this is not required.
-  method:
-    description:
-    - The installation method to use
-    - Defines if the I(flatpak_remote) is to installed for the globally for the whole C(system)
-      or for the current C(user) only.
-    choices: [ system, user ]
-    default: system
   executable:
     description:
-    - The path to the C(flatpak) executable to use
-    - By defaultm the C(flatpak_remote) module looks for the C(flatpak) executable on the path.
+    - The path to the C(flatpak) executable to use.
+    - By default, this module looks for the C(flatpak) executable on the path.
     default: flatpak
+  flatpakrepo_url:
+    description:
+    - The URL to the I(flatpakrepo) file representing the repository remote to add.
+    - When used with I(state=present), the flatpak remote specified under the I(flatpakrepo_url)
+      is added using the specified installation C(method).
+    - When used with I(state=absent), this is not required.
+    - Required when I(state=present).
+  method:
+    description:
+    - The installation method to use.
+    - Defines if the I(flatpak) is supposed to be installed globally for the whole C(system)
+      or only for the current C(user).
+    choices: [ system, user ]
+    default: system
+  name:
+    description:
+    - The desired name for the flatpak remote to be registered under on the managed host.
+    - When used with I(state=present), the remote will be added to the managed host under
+      the specified I(name).
+    - When used with I(state=absent) the remote with that name will be removed.
+    required: true
   state:
     description:
-      - Wether the flatpak_remote should be present on the managed host or not
-      - Setting this to C(present) will install the flatpak remote.
-      - Setting this to C(absent) will remove the flatpak remote.
+    - Indicates the desired package state.
     choices: [ absent, present ]
     default: present
 '''
 
 EXAMPLES = r'''
-- name: Add the Gnome flatpak remote to the system installation under the name 'gnome'.
+- name: Add the Gnome flatpak remote to the system installation
   flatpak_remote:
     name: gnome
     state: present
-    remote: https://sdk.gnome.org/gnome-apps.flatpakrepo
-    method: system
+    flatpakrepo_url: https://sdk.gnome.org/gnome-apps.flatpakrepo
 
-- name: Remove the Gnome flatpak remote  from the user installation.
-  flatpak_remote:
-    name: gnome
-    state: absent
-
-- name: Add the flathub flatpak repository remote to the user installation.
+- name: Add the flathub flatpak repository remote to the user installation
   flatpak_remote:
     name: flathub
     state: present
-    remote:  https://dl.flathub.org/repo/flathub.flatpakrepo
+    flatpakrepo_url: https://dl.flathub.org/repo/flathub.flatpakrepo
     method: user
 
-- name: Remove the flathub flatpak repository remote from the system installtion.
+- name: Remove the Gnome flatpak remote from the user installation
+  flatpak_remote:
+    name: gnome
+    state: absent
+    method: user
+
+- name: Remove the flathub remote from the system installation
   flatpak_remote:
     name: flathub
     state: absent
-    method: system
 '''
 
 RETURN = r'''
-reason:
-  description: On failure, the output for the failure
-  returned: failed
+command:
+  description: The exact flatpak command that was executed
+  returned: When a flatpak command has been executed
   type: string
-  sample: error while installing...
-name:
-  description: Remote of flatpak given for the operation
-  returned: always
+  sample: "/usr/bin/flatpak remote-add --system flatpak-test https://dl.flathub.org/repo/flathub.flatpakrepo"
+msg:
+  description: Module error message
+  returned: failure
   type: string
-  sample: https://sdk.gnome.org/gnome-apps.flatpakrepo
+  sample: "Executable '/usr/local/bin/flatpak' was not found on the system."
+rc:
+  description: Return code from flatpak binary
+  returned: When a flatpak command has been executed
+  type: int
+  sample: 0
+stderr:
+  description: Error output from flatpak binary
+  returned: When a flatpak command has been executed
+  type: string
+  sample: "error: GPG verification enabled, but no summary found (check that the configured URL in remote config is correct)\n"
+stdout:
+  description: Output from flatpak binary
+  returned: When a flatpak command has been executed
+  type: string
+  sample: "flathub\tFlathub\thttps://dl.flathub.org/repo/\t1\t\n"
 '''
 
 import subprocess
 from ansible.module_utils.basic import AnsibleModule
 
 
-def add_remote(module, binary, name, remote, method):
+def add_remote(module, binary, name, flatpakrepo_url, method):
     """Add a new remote."""
     global result
     command = "{0} remote-add --{1} {2} {3}".format(
-        binary, method, name, remote)
+        binary, method, name, flatpakrepo_url)
     _flatpak_command(module, module.check_mode, command)
     result['changed'] = True
 
@@ -123,7 +140,7 @@ def remove_remote(module, binary, name, method):
     result['changed'] = True
 
 
-def remote_exists(module, binary, name, remote, method):
+def remote_exists(module, binary, name, method):
     """Check if the remote exists."""
     command = "{0} remote-list -d --{1}".format(binary, method)
     # The query operation for the remote needs to be run even in check mode
@@ -158,7 +175,7 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             name=dict(type='str', required=True),
-            remote=dict(type='str'),
+            flatpakrepo_url=dict(type='str'),
             method=dict(type='str', default='system',
                         choices=['user', 'system']),
             state=dict(type='str', default="present",
@@ -170,14 +187,14 @@ def main():
     )
 
     name = module.params['name']
-    remote = module.params['remote']
+    flatpakrepo_url = module.params['flatpakrepo_url']
     method = module.params['method']
     state = module.params['state']
     executable = module.params['executable']
     binary = module.get_bin_path(executable, None)
 
-    if remote is None:
-        remote = ''
+    if flatpakrepo_url is None:
+        flatpakrepo_url = ''
 
     global result
     result = dict(
@@ -188,9 +205,9 @@ def main():
     if not binary:
         module.fail_json(msg="Executable '%s' was not found on the system." % executable, **result)
 
-    if state == 'present' and not remote_exists(module, binary, name, remote, method):
-        add_remote(module, binary, name, remote, method)
-    elif state == 'absent' and remote_exists(module, binary, name, remote, method):
+    if state == 'present' and not remote_exists(module, binary, name, method):
+        add_remote(module, binary, name, flatpakrepo_url, method)
+    elif state == 'absent' and remote_exists(module, binary, name, method):
         remove_remote(module, binary, name, method)
 
     module.exit_json(**result)
