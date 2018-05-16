4c36d384f117dbc56e4ff8b26c0b0bfe8287c8f4
Luke Weighall <luke@weighall.me>
2018-05-15 18:53:25 -0700

    fmgr_device_config pep8 fixes

--- lib/ansible/modules/network/fortimanager/fmgr_device_config.py
+++ lib/ansible/modules/network/fortimanager/fmgr_device_config.py
@@ -149,7 +149,7 @@ def update_device_interface(fmg, device_name, interface, ip, allow_access_list, 
     """
     Update a device interface IP and allow access
     """
-    # TODO - ENSURE ADOM IS BEING PROCESSED!
+
     access_list = list()
     allow_access_list = allow_access_list.replace(' ', '')
     access_list = allow_access_list.split(',')
@@ -185,7 +185,7 @@ def exec_config(fmg, device_unique_name, adom):
 def main():
     argument_spec = dict(
         host=dict(required=True, type="str"),
-        adom=dict(required=False, type="str"),
+        adom=dict(required=False, type="str", default="root"),
         password=dict(fallback=(env_fallback, ["ANSIBLE_NET_PASSWORD"]), no_log=True),
         username=dict(fallback=(env_fallback, ["ANSIBLE_NET_USERNAME"])),
 
@@ -201,10 +201,6 @@ def main():
 
     # handle params passed via provider and insure they are represented as the data type expected by fortimanager
     adom = module.params["adom"]
-    # if adom is empty, set to root
-    if adom is None:
-        adom = "root"
-
     device_unique_name = module.params["device_unique_name"]
     device_hostname = module.params["device_hostname"]
     interface = module.params["interface"]
