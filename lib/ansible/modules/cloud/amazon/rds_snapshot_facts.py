a297a799d529a827239ceaf60815643208252991
s-hertel <shertel@redhat.com>
2018-05-16 15:52:53 -0400

    Version added

--- lib/ansible/modules/cloud/amazon/rds_snapshot_facts.py
+++ lib/ansible/modules/cloud/amazon/rds_snapshot_facts.py
@@ -11,7 +11,7 @@ ANSIBLE_METADATA = {'status': ['preview'],
 DOCUMENTATION = '''
 ---
 module: rds_snapshot_facts
-version_added: "2.7"
+version_added: "2.6"
 short_description: obtain facts about one or more RDS snapshots
 description:
   - obtain facts about one or more RDS snapshots.  This does not currently include
