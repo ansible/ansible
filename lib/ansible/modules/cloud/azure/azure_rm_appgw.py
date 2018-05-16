af8ddcd8a2fce9677ced47273fae379cceede57b
zikalino <zikalino@microsoft.com>
2018-05-16 10:27:42 +0800

    just compare sku

--- lib/ansible/modules/cloud/azure/azure_rm_appgw.py
+++ lib/ansible/modules/cloud/azure/azure_rm_appgw.py
@@ -595,19 +595,19 @@ class AzureRMApplicationGateways(AzureRMModuleBase):
             if (self.parameters['location'] != old_response['location'] or
                     self.parameters['sku']['name'] != old_response['sku']['name'] or
                     self.parameters['sku']['tier'] != old_response['sku']['tier'] or
-                    self.parameters['sku']['capacity'] != old_response['sku']['capacity'] or
-                    #self.parameters['ssl_policy']['policy_type'] != old_response['ssl_policy']['policy_type'] or
-                    #self.parameters['ssl_policy']['policy_name'] != old_response['ssl_policy']['policy_name'] or
-                    #self.parameters['ssl_policy']['min_protocol_version'] != old_response['ssl_policy']['min_protocol_version'] or
-                    #not compare_arrays(self.parameters, old_response, 'authentication_certificates') or
-                    #not compare_arrays(self.parameters, old_response, 'gateway_ip_configurations') or
-                    #not compare_arrays(self.parameters, old_response, 'ssl_certificates') or
-                    #not compare_arrays(self.parameters, old_response, 'frontend_ip_configurations') or
-                    (not compare_arrays(self.parameters, old_response, 'frontend_ports')) or
-                    #not compare_arrays(self.parameters, old_response, 'backend_address_pools') or
-                    #not compare_arrays(self.parameters, old_response, 'backend_http_settings_collections') or
-                    #not compare_arrays(self.parameters, old_response, 'request_routing_rules')):
-                    (not compare_arrays(self.parameters, old_response, 'http_listeners'))):
+                    self.parameters['sku']['capacity'] != old_response['sku']['capacity']: # or
+                    # self.parameters['ssl_policy']['policy_type'] != old_response['ssl_policy']['policy_type'] or
+                    # self.parameters['ssl_policy']['policy_name'] != old_response['ssl_policy']['policy_name'] or
+                    # self.parameters['ssl_policy']['min_protocol_version'] != old_response['ssl_policy']['min_protocol_version'] or
+                    # not compare_arrays(self.parameters, old_response, 'authentication_certificates') or
+                    # not compare_arrays(self.parameters, old_response, 'gateway_ip_configurations') or
+                    # not compare_arrays(self.parameters, old_response, 'ssl_certificates') or
+                    # not compare_arrays(self.parameters, old_response, 'frontend_ip_configurations') or
+                    #(not compare_arrays(self.parameters, old_response, 'frontend_ports')) or
+                    # not compare_arrays(self.parameters, old_response, 'backend_address_pools') or
+                    # not compare_arrays(self.parameters, old_response, 'backend_http_settings_collections') or
+                    # not compare_arrays(self.parameters, old_response, 'request_routing_rules')):
+                    #(not compare_arrays(self.parameters, old_response, 'http_listeners'))):
 
                 self.to_do = Actions.Update
             else:
