9de5b94b2f2993b049eb8a2c871c7c0e072b08b1
Nathaniel Case <this.is@nathanielca.se>
2018-05-17 18:21:30 -0400

    Don't rewrite remote paths when remote is secretly local (#40259)
    * Don't rewrite remote paths when remote is secretly local
    
    * Remote overriding is configurable in connection
    
    * Use c.DEFAULT_LOCAL_TMP instead of remote_tmp
    
    * Remove remote_is_local from ConnectionBase
    
    * remote_is_local->_remote_is_local
    
    * Add warning signs to _remote_is_local and explanatory comments to its use

--- lib/ansible/plugins/connection/netconf.py
+++ lib/ansible/plugins/connection/netconf.py
@@ -169,6 +169,8 @@ class Connection(ConnectionBase):
     transport = 'netconf'
     has_pipelining = False
     force_persistence = True
+    # Do not use _remote_is_local in other connections
+    _remote_is_local = True
 
     def __init__(self, play_context, new_stdin, *args, **kwargs):
         super(Connection, self).__init__(play_context, new_stdin, *args, **kwargs)
