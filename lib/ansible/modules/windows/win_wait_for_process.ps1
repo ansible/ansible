16912b565124c3fde6ae962e01558f16c62a3983
Charles Crossan <crossan007@gmail.com>
2018-05-18 13:03:45 -0400

    try working on additional filter options and fix derps

--- lib/ansible/modules/windows/win_wait_for_process.ps1
+++ lib/ansible/modules/windows/win_wait_for_process.ps1
@@ -12,7 +12,11 @@ $ErrorActionPreference = "Stop"
 $params = Parse-Args -arguments $args -supports_check_mode $true
 
 $process_name = Get-AnsibleParam -obj $params -name "process_name" -type "str" 
+$process_id = Get-AnsibleParam -obj $params -name "process_id" -type "int" -default 0 #pid is a reserved variable in PowerShell.  use process_id instead.
+$owner = Get-AnsibleParam -obj $params -name "owner" -type "str"
 $sleep = Get-AnsibleParam -obj $params -name "sleep" -type "int" -default 1
+$pre_wait_delay = Get-AnsibleParam -obj $params -name "pre_wait_delay" -type "int" -default 0
+$post_wait_delay = Get-AnsibleParam -obj $params -name "post_wait_delay" -type "int" -default 0
 $process_min_count = Get-AnsibleParam -obj $params -name "process_min_count" -type "int" -default 1
 $state = Get-AnsibleParam -obj $params -name "state" -type "str" -default "present" -validateset "present","absent"
 $timeout = Get-AnsibleParam -obj $params -name "timeout" -type "int" -default 300
@@ -24,10 +28,65 @@ $result = @{
 # validate the input
 if ($state -eq "absent" -and $sleep -ne 1)
 {
-    Fail-json $result "sleep parameter is of no effet when waiting for a proces to stop."
+    Fail-json $result "sleep parameter is of no effect when waiting for a proces to stop."
+}
+
+if ($process_name -and $process_id)
+{
+    Fail-json $result "process_id and process_name may not be used at the same time."
+}
+if (-not ($process_name -or $process_id -or $user))
+{
+    Fail-json $result "at least one of: process_name, process_id, or user must be supplied"
 }
 
 $module_start = Get-Date
+
+#Get-Process doesn't actually return a UserName value, so get it from WMI.
+Function Test-ProcessMatchesFilter {
+    [cmdletbinding()]
+    Param(
+        [parameter(ValueFromPipeline)]
+        $WindowsProcess,
+        [String]
+        $Owner,
+        [String]
+        $ProcessName,
+        [int]
+        $ProcessId
+    )
+    Begin {
+        $owners = @{}
+        Get-WmiObject win32_process | ForEach-Object {$owners[$_.handle] = $_.getowner().user}
+    }
+    Process {
+        $include = $true
+
+        if ([String]::IsNullOrEmpty($WindowsProcess.Owner))
+        {
+            #always add the owner to proces info so that it's reported back
+            $WindowsProcess = $WindowsProcess | Select-Object -Property *,@{Name="Owner";Expression={$owners[$_.id.tostring()]}} 
+        }
+        if (-not [String]::IsNullOrEmpty($user) )
+        {
+            $include = $include -and ($WindowsProcess.Owner -eq $user)
+        }
+        if(-not [String]::IsNullOrEmpty($process_name))
+        {
+            $include = $include -and ($WindowsProcess.ProcessName -match $ProcessName)
+        }
+        if ($process_id -and $process_id -ne 0)
+        {
+            $include = $include -and ($WindowsProcess.Id -eq $ProcessId)
+        }
+       
+        if ($include)
+        {
+           $WindowsProcess
+        }
+    }
+}
+
 if ($state -eq "present" )
 {
     #wait for a process to start
@@ -36,34 +95,38 @@ if ($state -eq "present" )
     Do {
         if (((Get-Date) - $module_start).TotalSeconds -gt $timeout)
         {
-            Fail-Json $result "timeout while waiting for $process_name to start"
+            Fail-Json $result "timeout while waiting for $process_name to start.  waited $timeout seconds"
         }
-        $Processes = Get-Process | Where-Object {$_.ProcessName -match $process_name}
+       
+        $Processes = Get-Process | Test-ProcessMatchesFilter -Owner $owner -ProcessName $process_name -ProcessId $process_id | Select-Object -Property Id, ProcessName, Owner
         Start-Sleep -Seconds $sleep
         $attempts ++
-    } While ($Processes.count -lt $process_min_count)
+        $ProcessCount = $(if ($Processes -is [array]) { $Processess.count } else{ 1 })
+    } While ($ProcessCount -lt $process_min_count)
 
     if ($attempts -gt 0)
     {
         $result.changed = $true
     }
-    $result.matched_processess = ($Processes.count)
-    #$result.processess = $Processes
+    $result.matched_processess = $Processes
 }
 elseif ($state -eq "absent")
 {
     #wait for a process to stop
-    $Processes = Get-Process | Where-Object {$_.ProcessName -match $process_name} 
-    if ($Processes.count -gt 0 )
+    $Processes = Get-Process | Test-ProcessMatchesFilter -Owner $owner -ProcessName $process_name -ProcessId $process_id | Select-Object -Property ID, ProcessName, Owner
+    $result.matched_processess = $Processes
+    Fail-Json $result "faildebug"
+    $ProcessCount = $(if ($Processes -is [array]) { $Processess.count } else{ 1 })
+    if ($ProcessCount -gt 0 )
     {
         try { 
             Wait-Process -Id $($Processes | Select-Object -ExpandProperty ID) -Timeout $timeout -ErrorAction Stop
            
-            $result.matched_processess = ($Processes.count)
+            $result.matched_processess = $Processes
             $result.changed = $true
         }
         catch {
-            Fail-Json $result "timeout while waiting for $process_name to stop"
+            Fail-Json $result "$($_.Exception.Message). timeout while waiting for $process_name to stop.  waited $timeout seconds"
         }
     }
     else{
--- lib/ansible/modules/windows/win_wait_for_process.py
+++ lib/ansible/modules/windows/win_wait_for_process.py
@@ -14,40 +14,40 @@ ANSIBLE_METADATA = {'metadata_version': '1.0',
 DOCUMENTATION = r'''
 ---
 module: win_wait_for_process
-version_added: '2.4'
-short_description: Waits for a process to exsist or not exist before continuing.
+version_added: '2.6'
+short_description: Waits for a process to exist or not exist before continuing.
 description:
-- Waiting for a process to start or stop is useful when Windows services 
-  behave poorly and do not enumerate external dependencies in thier 
+- Waiting for a process to start or stop is useful when Windows services
+  behave poorly and do not enumerate external dependencies in thier
   manifset.
 options:
   process_name:
     description:
       - The name of the process for which to wait.
       - Supports RegEx input. Be sure to escape RegEx reserved
-        characters if attempting plaintext match
+        characters if attempting plaintext match.
   sleep:
     description:
     - Number of seconds to sleep between checks.
-    - Only applies when waiting for a process to start.  Waiting for a process to start 
-      does not have a native non-polling mechanism. Waiting for a stop uses native PowerShell 
+    - Only applies when waiting for a process to start.  Waiting for a process to start
+      does not have a native non-polling mechanism. Waiting for a stop uses native PowerShell
       and does not require polling.
     default: 1
   process_min_count:
     description:
-      - Minimum number of process matching the supplied pattern to satisfy C(present) condition
+      - Minimum number of process matching the supplied pattern to satisfy C(present) condition.
       - Only applies to C(present).
     default: 1
   state:
     description:
     - When checking for a running process C(present) will block execution
-      until the process exists, or until the timeout has been reached. 
-      C(absent) will block execution untile the processs no longer exists, 
+      until the process exists, or until the timeout has been reached.
+      C(absent) will block execution untile the processs no longer exists,
       or until the timeout has been reached.
     - When waiting for C(present), the module will return changed only if
-      the process was not present on the initial check but became present on 
+      the process was not present on the initial check but became present on
       subsequent checks.
-    - If, while waiting for C(absent), new processes matching the supplied 
+    - If, while waiting for C(absent), new processes matching the supplied
       pattern are started, these new processes will not be included in the
       action.
     default: present
@@ -55,23 +55,23 @@ options:
   timeout:
     description:
     - The maximum number of seconds to wait for a for a process to start or stop
-      before erroring out
+      before erroring out.
     default: 300
 author:
 - Charles Crossan (@crossan007)
 '''
 
 EXAMPLES = r'''
-- name: wait 300 seconds for all Oracle VirtualBox processes to stop. (VBoxHeadless, VirtualBox, VBoxSVC)
+- name: Wait 300 seconds for all Oracle VirtualBox processes to stop. (VBoxHeadless, VirtualBox, VBoxSVC)
   win_wait_for_process:
     process_name: "v(irtual)?box(headless|svc)?"
     state: absent
     timeout: 500
 
 
-- name: wait 300 seconds for 3 instances of cmd to start, waiting 5 seconds between each check
+- name: Wait 300 seconds for 3 instances of cmd to start, waiting 5 seconds between each check
   win_wait_for_process:
-    process_name: "cmd\.exe"
+    process_name: "cmd\\.exe"
     state: present
     timeout: 500
     sleep: 5
@@ -89,9 +89,9 @@ elapsed:
 changed:
   description: True if a process was started or stopped during the module execution.
   returned: always
-  type: boolean
-matched_processes: 
+  type: bool
+matched_processes:
   description: Count of processes stopped or started.
   returned: always
-  type: integer
+  type: int
 '''
