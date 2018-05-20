7d21fadbe78a122ed03b3a721b749449024c23d5
Kyle Snavely <kjsnavely@gmail.com>
2018-05-20 12:46:18 -0400

    Update acme_certificate.py
    Fix a small typo: s/src/csr in two spots.

--- lib/ansible/modules/web_infrastructure/acme_certificate.py
+++ lib/ansible/modules/web_infrastructure/acme_certificate.py
@@ -240,7 +240,7 @@ EXAMPLES = '''
   acme_certificate:
     account_key_src: /etc/pki/cert/private/account.key
     account_email: myself@sample.com
-    src: /etc/pki/cert/csr/sample.com.csr
+    csr: /etc/pki/cert/csr/sample.com.csr
     cert: /etc/httpd/ssl/sample.com.crt
     challenge: dns-01
     acme_directory: https://acme-v01.api.letsencrypt.org/directory
@@ -277,7 +277,7 @@ EXAMPLES = '''
   acme_certificate:
     account_key_src: /etc/pki/cert/private/account.key
     account_email: myself@sample.com
-    src: /etc/pki/cert/csr/sample.com.csr
+    csr: /etc/pki/cert/csr/sample.com.csr
     cert: /etc/httpd/ssl/sample.com.crt
     fullchain: /etc/httpd/ssl/sample.com-fullchain.crt
     chain: /etc/httpd/ssl/sample.com-intermediate.crt
