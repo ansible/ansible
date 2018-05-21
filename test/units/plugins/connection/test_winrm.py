659c42974350c199cd99b798e0bf4e92e47c70a9
Jordan Borean <jborean93@gmail.com>
2018-05-21 16:08:34 +1000

    fixed up mock for kerberos import

--- test/units/plugins/connection/test_winrm.py
+++ test/units/plugins/connection/test_winrm.py
@@ -6,9 +6,13 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import sys
+
 import pytest
 
 from io import StringIO
+
+from ansible.compat.tests.mock import patch, MagicMock
 from ansible.playbook.play_context import PlayContext
 from ansible.plugins.loader import connection_loader
 
@@ -33,7 +37,8 @@ class TestConnectionWinRM(object):
                 '_winrm_scheme': 'https',
                 '_winrm_transport': ['ssl'],
                 '_winrm_user': None
-            }
+            },
+            False
         ),
         # http through port
         (
@@ -45,9 +50,10 @@ class TestConnectionWinRM(object):
                 '_winrm_port': 5985,
                 '_winrm_scheme': 'http',
                 '_winrm_transport': ['plaintext'],
-            }
+            },
+            False
         ),
-        # kerberos user
+        # kerberos user with kerb present
         (
             {},
             {'_extras': {}, 'ansible_user': 'user@domain.com'},
@@ -60,7 +66,24 @@ class TestConnectionWinRM(object):
                 '_winrm_pass': '',
                 '_winrm_transport': ['kerberos', 'ssl'],
                 '_winrm_user': 'user@domain.com'
-            }
+            },
+            True
+        ),
+        # kerberos user without kerb present
+        (
+            {},
+            {'_extras': {}, 'ansible_user': 'user@domain.com'},
+            {},
+            {
+                '_kerb_managed': False,
+                '_kinit_cmd': 'kinit',
+                '_winrm_kwargs': {'username': 'user@domain.com',
+                                  'password': ''},
+                '_winrm_pass': '',
+                '_winrm_transport': ['ssl'],
+                '_winrm_user': 'user@domain.com'
+            },
+            False
         ),
         # kerberos user with managed ticket (implicit)
         (
@@ -75,7 +98,8 @@ class TestConnectionWinRM(object):
                 '_winrm_pass': 'pass',
                 '_winrm_transport': ['kerberos', 'ssl'],
                 '_winrm_user': 'user@domain.com'
-            }
+            },
+            True
         ),
         # kerb with managed ticket (explicit)
         (
@@ -85,7 +109,8 @@ class TestConnectionWinRM(object):
             {},
             {
                 '_kerb_managed': True,
-            }
+            },
+            True
         ),
         # kerb with unmanaged ticket (explicit))
         (
@@ -95,7 +120,8 @@ class TestConnectionWinRM(object):
             {},
             {
                 '_kerb_managed': False,
-            }
+            },
+            True
         ),
         # transport override (single)
         (
@@ -108,7 +134,8 @@ class TestConnectionWinRM(object):
                                   'password': ''},
                 '_winrm_pass': '',
                 '_winrm_transport': ['ntlm'],
-            }
+            },
+            False
         ),
         # transport override (list)
         (
@@ -121,7 +148,8 @@ class TestConnectionWinRM(object):
                                   'password': ''},
                 '_winrm_pass': '',
                 '_winrm_transport': ['ntlm', 'certificate'],
-            }
+            },
+            False
         ),
         # winrm extras
         (
@@ -133,7 +161,8 @@ class TestConnectionWinRM(object):
                 '_winrm_kwargs': {'username': None, 'password': '',
                                   'server_cert_validation': 'ignore',
                                   'service': 'WSMAN'},
-            }
+            },
+            False
         ),
         # direct override
         (
@@ -142,7 +171,8 @@ class TestConnectionWinRM(object):
             {'connection_timeout': 10},
             {
                 '_winrm_connection_timeout': 10,
-            }
+            },
+            False
         ),
         # user comes from option not play context
         (
@@ -152,20 +182,33 @@ class TestConnectionWinRM(object):
             {
                 '_winrm_user': 'user2',
                 '_winrm_kwargs': {'username': 'user2', 'password': ''}
-            }
+            },
+            False
         )
     )
 
     # pylint bug: https://github.com/PyCQA/pylint/issues/511
     # pylint: disable=undefined-variable
-    @pytest.mark.parametrize('play, options, direct, expected',
-                             ((p, o, d, e) for p, o, d, e in OPTIONS_DATA))
-    def test_set_options(self, play, options, direct, expected):
+    @pytest.mark.parametrize('play, options, direct, expected, kerb',
+                             ((p, o, d, e, k) for p, o, d, e, k in OPTIONS_DATA))
+    def test_set_options(self, play, options, direct, expected, kerb):
+        if kerb:
+            kerberos_mock = MagicMock()
+            modules = {'kerberos': kerberos_mock}
+        else:
+            modules = {'kerberos': None}
+
+        module_patcher = patch.dict(sys.modules, modules)
+        module_patcher.start()
+
         pc = PlayContext()
         for attr, value in play.items():
             setattr(pc, attr, value)
 
         new_stdin = StringIO()
+
+        # ensure we get a fresh connection plugin by clearing the cache
+        connection_loader._module_cache = {}
         conn = connection_loader.get('winrm', pc, new_stdin)
         conn.set_options(var_options=options, direct=direct)
 
@@ -174,3 +217,5 @@ class TestConnectionWinRM(object):
             assert actual == expected, \
                 "winrm attr '%s', actual '%s' != expected '%s'"\
                 % (attr, actual, expected)
+
+        module_patcher.stop()
