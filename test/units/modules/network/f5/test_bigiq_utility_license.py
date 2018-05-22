428f499fc52c64426e9c28097229abbfdbebbed0
Tim Rupp <caphrim007@gmail.com>
2018-05-21 20:18:41 -0700

    Adds bigiq utility license module (#40518)
    This module can be used to manage utility licenses on a BIG-IQ

---
+++ test/units/modules/network/f5/test_bigiq_utility_license.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright: (c) 2017, F5 Networks Inc.
+# GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import json
+import pytest
+import sys
+
+from nose.plugins.skip import SkipTest
+if sys.version_info < (2, 7):
+    raise SkipTest("F5 Ansible modules require Python >= 2.7")
+
+from ansible.compat.tests import unittest
+from ansible.compat.tests.mock import Mock
+from ansible.compat.tests.mock import patch
+from ansible.module_utils.basic import AnsibleModule
+
+try:
+    from library.modules.bigiq_utility_license import ModuleParameters
+    from library.modules.bigiq_utility_license import ModuleManager
+    from library.modules.bigiq_utility_license import ArgumentSpec
+    from library.module_utils.network.f5.common import F5ModuleError
+    from library.module_utils.network.f5.common import iControlUnexpectedHTTPError
+    from test.unit.modules.utils import set_module_args
+except ImportError:
+    try:
+        from ansible.modules.network.f5.bigiq_utility_license import ModuleParameters
+        from ansible.modules.network.f5.bigiq_utility_license import ModuleManager
+        from ansible.modules.network.f5.bigiq_utility_license import ArgumentSpec
+        from ansible.module_utils.network.f5.common import F5ModuleError
+        from ansible.module_utils.network.f5.common import iControlUnexpectedHTTPError
+        from units.modules.utils import set_module_args
+    except ImportError:
+        raise SkipTest("F5 Ansible modules require the f5-sdk Python library")
+
+fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
+fixture_data = {}
+
+
+def load_fixture(name):
+    path = os.path.join(fixture_path, name)
+
+    if path in fixture_data:
+        return fixture_data[path]
+
+    with open(path) as f:
+        data = f.read()
+
+    try:
+        data = json.loads(data)
+    except Exception:
+        pass
+
+    fixture_data[path] = data
+    return data
+
+
+class TestParameters(unittest.TestCase):
+    def test_module_parameters(self):
+        args = dict(
+            license_key='XXXX-XXXX-XXXX-XXXX-XXXX',
+            accept_eula=True,
+        )
+
+        p = ModuleParameters(params=args)
+        assert p.license_key == 'XXXX-XXXX-XXXX-XXXX-XXXX'
+        assert p.accept_eula is True
+
+
+class TestManager(unittest.TestCase):
+
+    def setUp(self):
+        self.spec = ArgumentSpec()
+        self.patcher1 = patch('time.sleep')
+        self.patcher1.start()
+
+    def tearDown(self):
+        self.patcher1.stop()
+
+    def test_create(self, *args):
+        set_module_args(dict(
+            license_key='XXXX-XXXX-XXXX-XXXX-XXXX',
+            accept_eula=True,
+            password='passsword',
+            server='localhost',
+            user='admin'
+        ))
+
+        module = AnsibleModule(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode
+        )
+        mm = ModuleManager(module=module)
+
+        # Override methods to force specific logic in the module to happen
+        mm.exists = Mock(side_effect=[False, True])
+        mm.create_on_device = Mock(return_value=True)
+        mm.wait_for_initial_license_activation = Mock(return_value=True)
+        mm.wait_for_utility_license_activation = Mock(return_value=True)
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
