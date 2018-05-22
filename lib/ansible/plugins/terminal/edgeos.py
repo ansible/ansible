e2fd9493bd621214eea73c0b361920e72aff6e8b
Sam Doran <sdoran@redhat.com>
2018-05-22 12:22:18 -0400

    EdgeOS module improvements (#39530)
    * Update docs and use to_text on strings
    
    * Add warning to use network_cli

--- lib/ansible/plugins/terminal/edgeos.py
+++ lib/ansible/plugins/terminal/edgeos.py
@@ -1,6 +1,5 @@
 # Copyright: (c) 2018, Ansible Project
-# GNU General Public License v3.0+
-# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
