a5deb86a7b36c20c64e7ae0a6ab5010f0b6bf2b4
hirsts <stuart@hirst.ws>
2018-05-23 14:59:09 +1000

    Update splunk.py

--- lib/ansible/plugins/callback/splunk.py
+++ lib/ansible/plugins/callback/splunk.py
@@ -18,34 +18,34 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 DOCUMENTATION = '''
-callback: splunk
-type: aggregate
-short_description: Sends task result events to Splunk HTTP Event Collector
-author: "Stuart Hirst <support@convergingdata.com>"
-description:
-  - This callback plugin will send task results as JSON formatted events to a Splunk HTTP collector.
-  - The companion Splunk Monitoring & Diagnostics App is available here: https://splunkbase.splunk.com/app/4023/
-  - Credit to "Ryan Currah (@ryancurrah)" for original source upon which this is based.
-version_added: "1.1"
-requirements:
-  - Whitelisting this callback plugin
-  - 'Create a HTTP Event Collector in Splunk'
-  - Define the url and token in ansible.cfg
-options:
-  url:
-    description: URL to the Splunk HTTP collector source
-    env:
-      - name: SPLUNK_URL
-    ini:
-      - section: callback_splunk
-        key: url
-  authtoken:
-    description: Token to authenticate the connection to the Splunk HTTP collector
-    env:
-      - name: SPLUNK_AUTHTOKEN
-    ini:
-      - section: callback_splunk
-        key: authtoken
+    callback: splunk
+    type: aggregate
+    short_description: Sends task result events to Splunk HTTP Event Collector
+    author: "Stuart Hirst <support@convergingdata.com>"
+    description:
+      - This callback plugin will send task results as JSON formatted events to a Splunk HTTP collector.
+      - The companion Splunk Monitoring & Diagnostics App is available here: "https://splunkbase.splunk.com/app/4023/"
+      - Credit to "Ryan Currah (@ryancurrah)" for original source upon which this is based.
+    version_added: "1.1"
+    requirements:
+      - Whitelisting this callback plugin
+      - 'Create a HTTP Event Collector in Splunk'
+      - Define the url and token in ansible.cfg
+    options:
+      url:
+        description: URL to the Splunk HTTP collector source
+        env:
+          - name: SPLUNK_URL
+        ini:
+          - section: callback_splunk
+            key: url
+      authtoken:
+        description: Token to authenticate the connection to the Splunk HTTP collector
+        env:
+          - name: SPLUNK_AUTHTOKEN
+        ini:
+          - section: callback_splunk
+            key: authtoken
 '''
 
 EXAMPLES = '''
