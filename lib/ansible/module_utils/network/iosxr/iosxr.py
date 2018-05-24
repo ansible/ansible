f12f230816bb61e5cebe036834b340b5c4240476
Deepak Agrawal <deepacks@gmail.com>
2018-05-24 06:53:35 +0000

    remove import of ConnectionError as it is not required

--- lib/ansible/module_utils/network/iosxr/iosxr.py
+++ lib/ansible/module_utils/network/iosxr/iosxr.py
@@ -34,7 +34,7 @@ from time import sleep
 from ansible.module_utils._text import to_text, to_bytes
 from ansible.module_utils.basic import env_fallback
 from ansible.module_utils.network.common.utils import to_list
-from ansible.module_utils.connection import Connection, ConnectionError
+from ansible.module_utils.connection import Connection
 from ansible.module_utils.network.common.netconf import NetconfConnection
 
 try:
