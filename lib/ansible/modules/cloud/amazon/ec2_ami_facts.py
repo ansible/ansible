6f72f1db62393d500247d93e5516ba20cb4d7b25
s-hertel <shertel@redhat.com>
2018-05-24 13:44:31 -0400

    ec2_ami_facts: return images in a consistent order

--- lib/ansible/modules/cloud/amazon/ec2_ami_facts.py
+++ lib/ansible/modules/cloud/amazon/ec2_ami_facts.py
@@ -234,6 +234,7 @@ def list_ec2_images(ec2_client, module):
             # describing launch permissions of images owned by others is not permitted, but shouldn't cause failures
             pass
 
+    images.sort(key=lambda e: e['creation_date'])
     module.exit_json(images=images)
 
 
