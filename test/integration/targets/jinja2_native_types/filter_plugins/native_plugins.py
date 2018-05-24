bd68b52e77042efb9ad531ab9d308b53e4138424
Martin Krizek <martin.krizek@gmail.com>
2018-05-24 10:56:16 +0200

    No reason for to_text to fallback to str()

--- test/integration/targets/jinja2_native_types/filter_plugins/native_plugins.py
+++ test/integration/targets/jinja2_native_types/filter_plugins/native_plugins.py
@@ -1,15 +1,8 @@
 from ansible.module_utils._text import to_text
 
 
-def make_text(data):
-    try:
-        return to_text(data)
-    except:
-        return str(data)
-
-
 class FilterModule(object):
     def filters(self):
         return {
-            'to_text': make_text
+            'to_text': to_text,
         }
