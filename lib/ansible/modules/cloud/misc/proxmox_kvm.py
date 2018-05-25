73052d5dfba6ce7b7a761d1e5ad00769fac269e9
Thijs Cramer <thijs.cramer@gmail.com>
2018-05-25 14:11:43 +0200

    PEP8 Improvements

--- lib/ansible/modules/cloud/misc/proxmox_kvm.py
+++ lib/ansible/modules/cloud/misc/proxmox_kvm.py
@@ -721,8 +721,14 @@ def create_vm(module, proxmox, vmid, newid, node, name, memory, cpu, cores, sock
 
     # Verify Cloud-Init support
     if PVE_FULL_VERSION < 5.2:
-      if 'ciuser' in kwargs or 'cipassword' in kwargs or 'citype' or 'ipconfig' in kwargs or 'nameserver' in kwargs or 'searchdomain' in kwargs or 'sshkeys' in kwargs:
-        module.fail_json(msg='Cloud-Init is not supported on Proxmox Versions older than 5.2, your version: %s' % PVE_FULL_VERSION)
+        if ('ciuser' in kwargs or
+            'cipassword' in kwargs or
+            'citype' in kwargs or
+            'ipconfig' in kwargs or
+            ' nameserver' in kwargs or
+            'searchdomain' in kwargs or
+            'sshkeys' in kwargs):
+            module.fail_json(msg='Cloud-Init is not supported on Proxmox Versions older than 5.2, your version: %s' % PVE_FULL_VERSION)
 
     # The features work only on PVE 4
     if PVE_MAJOR_VERSION < 4:
