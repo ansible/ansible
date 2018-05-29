2f86779f081c61cc6b87b84ecff7260b99b963b5
Trishna Guha <trishnaguha17@gmail.com>
2018-05-29 21:44:19 +0530

    nxos_vlan fix (#40822)
    * nxos_vlan fix
    
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>
    
    * uncomment mode test as nxapi now has get_capabilities
    
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/modules/network/nxos/nxos_vlan.py
+++ lib/ansible/modules/network/nxos/nxos_vlan.py
@@ -454,21 +454,31 @@ def parse_vni(module, vlan_id):
     return str(vni)
 
 
+def get_vlan_int(interfaces):
+    vlan_int = []
+    for i in interfaces.split(','):
+        if 'eth' in i.lower() and '-' in i:
+            int_range = i.split('-')
+            stop = int((int_range)[1])
+            start = int(int_range[0].split('/')[1])
+            eth = int_range[0].split('/')[0]
+            for r in range(start, stop + 1):
+                vlan_int.append(eth + '/' + str(r))
+        else:
+            vlan_int.append(i)
+    return vlan_int
+
+
 def parse_interfaces(module, vlan):
     vlan_int = []
     interfaces = vlan.get('vlanshowplist-ifidx')
     if interfaces:
-        for i in interfaces.split(','):
-            if 'eth' in i.lower() and '-' in i:
-                int_range = i.split('-')
-                stop = int((int_range)[1])
-                start = int(int_range[0].split('/')[1])
-                eth = int_range[0].split('/')[0]
-                for r in range(start, stop + 1):
-                    vlan_int.append(eth + '/' + str(r))
-            else:
-                vlan_int.append(i)
-
+        if isinstance(interfaces, list):
+            interfaces_list = [i.strip() for i in interfaces]
+            interfaces_str = ','.join(interfaces_list)
+            vlan_int = get_vlan_int(interfaces_str)
+        else:
+            vlan_int = get_vlan_int(interfaces)
     return vlan_int
 
 
