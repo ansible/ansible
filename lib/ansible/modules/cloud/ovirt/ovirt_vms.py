f8906367cec633fbbeaa3ab9c6d56cbea68c8114
Martin Necas <necas.marty@gmail.com>
2018-05-29 10:03:58 +0200

    corret code to pep8

--- lib/ansible/modules/cloud/ovirt/ovirt_vms.py
+++ lib/ansible/modules/cloud/ovirt/ovirt_vms.py
@@ -273,7 +273,7 @@ options:
     custom_compatibility_version:
         description:
             - "Enables a virtual machine to be customized to its own compatibility version. If
-            `C(custom_compatibility_version)` is set, it overrides the cluster's compatibility version
+            'C(custom_compatibility_version)' is set, it overrides the cluster's compatibility version
             for this particular virtual machine."
         version_added: "2.7"
     host_devices:
@@ -281,7 +281,9 @@ options:
             - Single Root I/O Virtualization - technology that allows single device to expose multiple endpoints that can be passed to VMs
             - PF - Physical Function - refers to a physical device (possibly supporting SR-IOV)
             - VF - Virtual Function - virtual function exposed by SR-IOV capable device
-            - IOMMU group - unit of isolation created by the kernel IOMMU driver. Each IOMMU group is isolated from other IOMMU groups with respect to DMA. For our purposes, IOMMU groups are a set of PCI devices which may span multiple PCI buses.
+            - IOMMU group - unit of isolation created by the kernel IOMMU driver. 
+            Each IOMMU group is isolated from other IOMMU groups with respect to DMA. For our purposes,
+            IOMMU groups are a set of PCI devices which may span multiple PCI buses.
             - VFIO[2] - Virtual Function I/O - virtualization device driver, replacement of the pci-stub driver
             - mdev - mediated devices - devices that are capable of creating and assigning device instances to a VMs (similar to SR-IOV)
         version_added: "2.7"
