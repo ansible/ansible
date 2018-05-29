6b9ba391408862d01b6a63091cb7d29bfca99844
Trishna Guha <trishnaguha17@gmail.com>
2018-05-29 21:43:34 +0530

    httpapi fix nxos (#40806)
    * httpapi fix nxos
    
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>
    
    * nxos_hsrp fix
    
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/plugins/cliconf/nxos.py
+++ lib/ansible/plugins/cliconf/nxos.py
@@ -95,8 +95,11 @@ class Cliconf(CliconfBase):
     def get_capabilities(self):
         result = {}
         result['rpc'] = self.get_base_rpc()
-        result['network_api'] = 'cliconf'
         result['device_info'] = self.get_device_info()
+        if isinstance(self._connection, NetworkCli):
+            result['network_api'] = 'cliconf'
+        else:
+            result['network_api'] = 'nxapi'
         return json.dumps(result)
 
     # Migrated from module_utils
