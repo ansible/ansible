42f3a9b1d999908ed447f9b8a11b8fb779ffe926
René Moser <mail@renemoser.net>
2018-05-30 15:34:23 +0200

    ec2_lc: fix state doc matching arg_spec (#40792)
    * ec2_lc: fix state doc matching arg_spec
    
    * Remove unused variable

--- lib/ansible/modules/cloud/amazon/ec2_lc.py
+++ lib/ansible/modules/cloud/amazon/ec2_lc.py
@@ -44,7 +44,7 @@ options:
   state:
     description:
       - Register or deregister the instance
-    required: true
+    default: present
     choices: ['present', 'absent']
   name:
     description:
@@ -389,8 +389,6 @@ except ImportError:
 
 
 def create_block_device_meta(module, volume):
-    MAX_IOPS_TO_SIZE_RATIO = 30
-
     # device_type has been used historically to represent volume_type,
     # however ec2_vol uses volume_type, as does the BlockDeviceType, so
     # we add handling for either/or but not both
