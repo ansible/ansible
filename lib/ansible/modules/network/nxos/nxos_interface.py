8e8b469416c8f0971f22c37ae79618eca2443556
Trishna Guha <trishnaguha17@gmail.com>
2018-05-30 11:21:53 +0530

    nxos bugfixes 2.5.4 cherry-pick (#40861)
    * Fix legacy Nexus 3k integration test and module issues. (#40322)
    
    * Add setup ignore_errors for nxos_config test
    
    * Fix parse_fan_info for n3k
    
    * Skip bidir tests for N3k
    
    * Omit vni config for n3k
    
    * Skip unsupported nxos_vrf_af test on N3K
    
    * Add legacy N3K platform tag
    
    (cherry picked from commit 63e16431b7e08d85bd9846a504f63e0d2442a1a8)
    
    * Skip N35 and N3L platforms for nxos_evpn_global test (#40333)
    
    (cherry picked from commit ad69ef88e7e67d54e42ce8e9ceae4ceaf8c09be5)
    
    * add normalize_interface in module_utils and fix nxos_l3_interface module (#40598)
    
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>
    (cherry picked from commit 0b7932db3000d58303462a6d1ff061adeff3c243)
    
    * nxos_interface: Disable switchport for loopback/svi (#40314)
    
    * nxos_interface: Disable switchport for loopback/svi
    
    * Replace interface_type with get_interface_type(name)
    
    (cherry picked from commit d04a989bd28b625747664064b8c8f58597b0f575)
    
    * fixes bug with matching nxos prompts (#40655)
    
    This change addresses an issue where the prompt matching regular
    expressions would not match a prompt if the hostname starts with a
    numeric value.  Before this change, the connection would fail with a
    socket.timeout() error.  This change updates the termnal_stdout_re
    values to now check for a number.
    
    See Github issue #38639
    
    (cherry picked from commit d829ff6993aa3a7e8123d2188ab04f4bfd88c1d0)
    
    * fix nxos_vrf and migrate get_interface_type to module_utils (#40825)
    
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>
    (cherry picked from commit b4baa2d48455029cfe35b14195b2ca4616e540ac)
    
    * nxos_vlan fix
    
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>
    
    * changelog nxos bugfixes 2.5.4
    
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/modules/network/nxos/nxos_interface.py
+++ lib/ansible/modules/network/nxos/nxos_interface.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python
-#
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-#
+# -*- coding: utf-8 -*-
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
@@ -229,7 +216,8 @@ import time
 from copy import deepcopy
 
 from ansible.module_utils.network.nxos.nxos import load_config, run_commands
-from ansible.module_utils.network.nxos.nxos import nxos_argument_spec
+from ansible.module_utils.network.nxos.nxos import nxos_argument_spec, normalize_interface
+from ansible.module_utils.network.nxos.nxos import get_interface_type
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.network.common.utils import conditional, remove_default_spec
 
@@ -258,27 +246,6 @@ def search_obj_in_list(name, lst):
     return None
 
 
-def get_interface_type(interface):
-    """Gets the type of interface
-    """
-    if interface.upper().startswith('ET'):
-        return 'ethernet'
-    elif interface.upper().startswith('VL'):
-        return 'svi'
-    elif interface.upper().startswith('LO'):
-        return 'loopback'
-    elif interface.upper().startswith('MG'):
-        return 'management'
-    elif interface.upper().startswith('MA'):
-        return 'management'
-    elif interface.upper().startswith('PO'):
-        return 'portchannel'
-    elif interface.upper().startswith('NV'):
-        return 'nve'
-    else:
-        return 'unknown'
-
-
 def get_interfaces_dict(module):
     """Gets all active interfaces on a given switch
     """
@@ -305,46 +272,6 @@ def get_interfaces_dict(module):
             interfaces[intf_type].append(intf)
 
     return interfaces
-
-
-def normalize_interface(name):
-    """Return the normalized interface name
-    """
-    if not name:
-        return None
-
-    def _get_number(name):
-        digits = ''
-        for char in name:
-            if char.isdigit() or char == '/':
-                digits += char
-        return digits
-
-    if name.lower().startswith('et'):
-        if_type = 'Ethernet'
-    elif name.lower().startswith('vl'):
-        if_type = 'Vlan'
-    elif name.lower().startswith('lo'):
-        if_type = 'loopback'
-    elif name.lower().startswith('po'):
-        if_type = 'port-channel'
-    elif name.lower().startswith('nv'):
-        if_type = 'nve'
-    else:
-        if_type = None
-
-    number_list = name.split(' ')
-    if len(number_list) == 2:
-        number = number_list[-1].strip()
-    else:
-        number = _get_number(name)
-
-    if if_type:
-        proper_interface = if_type + number
-    else:
-        proper_interface = name
-
-    return proper_interface
 
 
 def get_vlan_interface_attributes(name, intf_type, module):
@@ -458,10 +385,12 @@ def map_obj_to_commands(updates, module):
 
         elif state == 'present':
             if obj_in_have:
-                if mode == 'layer2' and mode != obj_in_have.get('mode'):
-                    add_command_to_interface(interface, 'switchport', commands)
-                elif mode == 'layer3' and mode != obj_in_have.get('mode'):
-                    add_command_to_interface(interface, 'no switchport', commands)
+                # Don't run switchport command for loopback and svi interfaces
+                if get_interface_type(name) in ('ethernet', 'portchannel'):
+                    if mode == 'layer2' and mode != obj_in_have.get('mode'):
+                        add_command_to_interface(interface, 'switchport', commands)
+                    elif mode == 'layer3' and mode != obj_in_have.get('mode'):
+                        add_command_to_interface(interface, 'no switchport', commands)
 
                 if admin_state == 'up' and admin_state != obj_in_have.get('admin_state'):
                     add_command_to_interface(interface, 'no shutdown', commands)
@@ -498,10 +427,12 @@ def map_obj_to_commands(updates, module):
 
             else:
                 commands.append(interface)
-                if mode == 'layer2':
-                    commands.append('switchport')
-                elif mode == 'layer3':
-                    commands.append('no switchport')
+                # Don't run switchport command for loopback and svi interfaces
+                if get_interface_type(name) in ('ethernet', 'portchannel'):
+                    if mode == 'layer2':
+                        commands.append('switchport')
+                    elif mode == 'layer3':
+                        commands.append('no switchport')
 
                 if admin_state == 'up':
                     commands.append('no shutdown')
