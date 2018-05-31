079318bf4a36f0dd151bd209f3de04b6988584d4
Matt Martz <matt@sivel.net>
2018-05-31 10:14:26 -0500

    Honor ignore_errors when invoking the debugger, add config to disable this behavior (#39868)
    * Honor ignore_errors when invoking the debugger, add config to disable this behavior. Fixes #39863
    
    * Limit ignore_errors logic to failed

--- lib/ansible/executor/task_result.py
+++ lib/ansible/executor/task_result.py
@@ -7,6 +7,7 @@ __metaclass__ = type
 
 from copy import deepcopy
 
+from ansible import constants as C
 from ansible.parsing.dataloader import DataLoader
 from ansible.vars.clean import strip_internal_keys
 
@@ -66,16 +67,17 @@ class TaskResult:
 
     def needs_debugger(self, globally_enabled=False):
         _debugger = self._task_fields.get('debugger')
+        _ignore_errors = C.TASK_DEBUGGER_IGNORE_ERRORS and self._task_fields.get('ignore_errors')
 
         ret = False
-        if globally_enabled and (self.is_failed() or self.is_unreachable()):
+        if globally_enabled and ((self.is_failed() and not _ignore_errors) or self.is_unreachable()):
             ret = True
 
         if _debugger in ('always',):
             ret = True
         elif _debugger in ('never',):
             ret = False
-        elif _debugger in ('on_failed',) and self.is_failed():
+        elif _debugger in ('on_failed',) and self.is_failed() and not _ignore_errors:
             ret = True
         elif _debugger in ('on_unreachable',) and self.is_unreachable():
             ret = True
