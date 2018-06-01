22bd86940d8004f3d720382126c071c1afb044fa
Tim Rupp <caphrim007@gmail.com>
2018-06-01 08:55:50 -0700

    Re-allow SSH due to broken delegation (#40978)
    Removes the SSH enforcement because it breaks situations where, for example,
    an admin maintains Ansible Tower and another team needs to use Ansible
    with the BIG-IP modules, but the Tower admin does not want to install
    the F5-SDK on the Ansible Tower instance.
    
    In this situation, the user needs to delegate the execution of the BIG-IP
    module to another linux host that does have the dependencies installed.
    
    Usually this is done with delegation, often over SSH. Limiting the SSH
    connection here prevents this from working.

--- lib/ansible/plugins/action/bigiq.py
+++ lib/ansible/plugins/action/bigiq.py
@@ -78,8 +78,6 @@ class ActionModule(_ActionModule):
                                    'https://docs.ansible.com/ansible/network_debug_troubleshooting.html#unable-to-open-shell'}
 
                 task_vars['ansible_socket'] = socket_path
-        else:
-            return {'failed': True, 'msg': 'Connection type %s is not valid for this module' % self._play_context.connection}
 
         if (self._play_context.connection == 'local' and transport == 'cli') or self._play_context.connection == 'network_cli':
             # make sure we are in the right cli context which should be
