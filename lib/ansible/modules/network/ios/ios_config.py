8734844b3353e2f5c62e9691e5f16f73706bbf1c
Chris Smolen <smolz@users.noreply.github.com>
2018-06-01 16:48:48 -0700

    Update ios_config.py

--- lib/ansible/modules/network/ios/ios_config.py
+++ lib/ansible/modules/network/ios/ios_config.py
@@ -272,8 +272,8 @@ EXAMPLES = """
       - shutdown
     # parents: int gig1/0/11
     parents: interface GigabitEthernet1/0/11
-    
-# Set boot image based on comparison to a group_var (version) and the version 
+
+# Set boot image based on comparison to a group_var (version) and the version
 # that is returned from the `ios_facts` module
 - name: SETTING BOOT IMAGE
     ios_config:
