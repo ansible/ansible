0ed6c64885f7ed7f5c17a1e5a86fdd45e1a92f8b
Maksim Nikiforov <maksim@redhat.com>
2018-06-03 22:51:53 -0400

    MOdified for PEP8

--- contrib/inventory/netdisco.py
+++ contrib/inventory/netdisco.py
@@ -1,45 +1,51 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # Dynamic inventory script for the web-based network management tool
 # Netdisco. Created by Maksim Nikiforov.
 # All information is extracted from Netdisco's  PostgreSQL database
 
-#!/usr/bin/python
 import json
 import psycopg2
-import sys
-import pprint
-from psycopg2.extras import RealDictCursor
 
 
 def main():
-        conn_string = "host='localhost' dbname='netdisco' user='netdisco' password='redhat'"
+    """Create JSON output for Ansible"""
+    conn_string = \
+        "host='localhost' dbname='netdisco' user='netdisco' password='redhat'"
 
-        # Make the connection
-        conn = psycopg2.connect(conn_string)
+    # Make the connection
+    conn = psycopg2.connect(conn_string)
+    # Return a cursor object to perform queries
+    cursor = conn.cursor()
 
-        # Return a cursor object to perform queries
-        cursor = conn.cursor()
+    # Execute query
+    cursor.execute('SELECT vendor,name,ip FROM device')
 
-        # execute the query
-        cursor.execute("SELECT vendor,name,ip FROM device")
-
-        inventory = {'_meta': {'hostvars': {}}}
-        for row in cursor.fetchall():
-            group = row[0]
-            host = row[1] or 'ungrouped'
-            ip = row[2]
+    inventory = {'_meta': {'hostvars': {}}}
+    for row in cursor.fetchall():
+        group = row[0]
+        host = row[1] or 'ungrouped'
+        ip = row[2]
 
         # Create the group if it doesn't exist
-            if group not in inventory:
-                inventory[group] = {'hosts': [], 'vars': {}}
+
+        if group not in inventory:
+            inventory[group] = {'hosts': [], 'vars': {}}
+
         # Add the host to the group
-            inventory[group]['hosts'].append(host)
 
-            # Add a hostvars record for the host if it doesn't exist
-            if host not in inventory['_meta']['hostvars']:
-                inventory['_meta']['hostvars'][host] = {}
+        inventory[group]['hosts'].append(host)
+
+        # Add a hostvars record for the host if it doesn't exist
+
+        if host not in inventory['_meta']['hostvars']:
+            inventory['_meta']['hostvars'][host] = {}
+
             # Add the role variable for this host to hostvars
-            inventory['_meta']['hostvars'][host]['ip'] = ip
-        print json.dumps(inventory, indent=4)
 
-if __name__ == "__main__":
-        main()
+        inventory['_meta']['hostvars'][host]['ip'] = ip
+    print json.dumps(inventory, indent=4)
+
+
+if __name__ == '__main__':
+    main()
