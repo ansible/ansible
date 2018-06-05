f7a870ed0ee81a00bb1536e21f876862860a2798
Brandon Schlueter <b@schlueter.blue>
2018-06-05 11:14:43 -0400

    Add try/catch around boto3 import in sops plugin

--- lib/ansible/plugins/vars/sops.py
+++ lib/ansible/plugins/vars/sops.py
@@ -34,7 +34,11 @@ import sys
 from base64 import b64decode
 from socket import gethostname
 
-import boto3
+try:
+    import boto3
+except ImportError:
+    raise AnsibleError("The lookup aws_account_attribute requires boto3.")
+    
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, modes, algorithms
 
