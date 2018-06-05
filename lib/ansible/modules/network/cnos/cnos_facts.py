84c34711b7e7df3a125bf017210e7952cf1594ed
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-06-05 14:31:50 +0530

    Fixing copy paste issue (#41121)

--- lib/ansible/modules/network/cnos/cnos_facts.py
+++ lib/ansible/modules/network/cnos/cnos_facts.py
@@ -277,11 +277,11 @@ class Default(FactsBase):
         return "NA"
 
     def parse_image(self, data):
-        match = re.search(r'(.*) image1(.*)', data, re.M | re.I)
+        match = re.search(r'(.*) image(.*)', data, re.M | re.I)
         if match:
             return "Image1"
         else:
-            return "Image1"
+            return "Image2"
 
     def parse_serialnum(self, data):
         for line in data.split('\n'):
