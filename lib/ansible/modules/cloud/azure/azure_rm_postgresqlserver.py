adb504b6c1295ff7f5c9deefbfa8767429976890
zikalino <zikalino@microsoft.com>
2018-06-07 14:48:52 +0800

    reappied changes in a new pr

--- lib/ansible/modules/cloud/azure/azure_rm_postgresqlserver.py
+++ lib/ansible/modules/cloud/azure/azure_rm_postgresqlserver.py
@@ -135,9 +135,9 @@ import time
 from ansible.module_utils.azure_rm_common import AzureRMModuleBase
 
 try:
-    from msrestazure.azure_exceptions import CloudError
-    from msrestazure.azure_operation import AzureOperationPoller
     from azure.mgmt.rdbms.postgresql import PostgreSQLManagementClient
+    from msrestazure.azure_exceptions import CloudError
+    from msrest.polling import LROPoller
     from msrest.serialization import Model
 except ImportError:
     # This is handled in azure_rm_common
@@ -322,7 +322,7 @@ class AzureRMServers(AzureRMModuleBase):
                 response = self.mgmt_client.servers.update(resource_group_name=self.resource_group,
                                                            server_name=self.name,
                                                            parameters=self.parameters)
-            if isinstance(response, AzureOperationPoller):
+            if isinstance(response, LROPoller):
                 response = self.get_poller_result(response)
 
         except CloudError as exc:
