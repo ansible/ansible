40d2df0ef3b33a2661c5c4abdd92215be0efd158
Sloane Hertel <shertel@redhat.com>
2018-06-12 12:15:16 -0400

    Add AWS boto3 error code exception function is_boto3_error_code (#41202)
    * Add aws/core.py function to check for specific AWS error codes
    
    * Use sys.exc_info to get exception object if it isn't passed in
    
    * Allow catching exceptions with is_boto3_error_code
    
    * Replace from_code with is_boto3_error_code
    
    * Return a type that will never be raised to support stricter type comparisons in Python 3+
    
    * Use is_boto3_error_code in aws_eks_cluster
    
    * Add duplicate-except to ignores when using is_boto3_error_code
    
    * Add is_boto3_error_code to module development guideline docs

--- lib/ansible/modules/cloud/amazon/aws_config_aggregator.py
+++ lib/ansible/modules/cloud/amazon/aws_config_aggregator.py
@@ -85,7 +85,7 @@ try:
 except ImportError:
     pass  # handled by AnsibleAWSModule
 
-from ansible.module_utils.aws.core import AnsibleAWSModule
+from ansible.module_utils.aws.core import AnsibleAWSModule, is_boto3_error_code
 from ansible.module_utils.ec2 import boto3_conn, get_aws_connection_info, AWSRetry
 from ansible.module_utils.ec2 import camel_dict_to_snake_dict, boto3_tag_list_to_ansible_dict
 
@@ -96,9 +96,9 @@ def resource_exists(client, module, resource_type, params):
             ConfigurationAggregatorNames=[params['name']]
         )
         return aggregator['ConfigurationAggregators'][0]
-    except client.exceptions.from_code('NoSuchConfigurationAggregatorException'):
+    except is_boto3_error_code('NoSuchConfigurationAggregatorException'):
         return
-    except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
+    except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:  # pylint: disable=duplicate-except
         module.fail_json_aws(e)
 
 
