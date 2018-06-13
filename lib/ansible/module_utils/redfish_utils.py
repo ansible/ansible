9953f8d41b76eb0fda92b48536b7b45dbb1b9287
Jose Delarosa <jose.delarosa@dell.com>
2018-06-13 13:42:05 -0500

    Modify exception handling to avoid AttributeError in python3

--- lib/ansible/module_utils/redfish_utils.py
+++ lib/ansible/module_utils/redfish_utils.py
@@ -42,8 +42,9 @@ class RedfishUtils(object):
             return {'ret': False, 'msg': "HTTP Error: %s" % e.code}
         except URLError as e:
             return {'ret': False, 'msg': "URL Error: %s" % e.reason}
-        except Exception as e:
-            return {'ret': False, 'msg': "Other error: %s" % e.message}
+        # Almost all errors should be caught above, but just in case
+        except:
+            return {'ret': False, 'msg': "Unknown error"}
         return {'ret': True, 'data': data}
 
     def post_request(self, uri, pyld, hdrs):
@@ -60,8 +61,9 @@ class RedfishUtils(object):
             return {'ret': False, 'msg': "HTTP Error: %s" % e.code}
         except URLError as e:
             return {'ret': False, 'msg': "URL Error: %s" % e.reason}
-        except Exception as e:
-            return {'ret': False, 'msg': "Other error: %s" % e.message}
+        # Almost all errors should be caught above, but just in case
+        except:
+            return {'ret': False, 'msg': "Unknown error"}
         return {'ret': True, 'resp': resp}
 
     def patch_request(self, uri, pyld, hdrs):
@@ -78,8 +80,9 @@ class RedfishUtils(object):
             return {'ret': False, 'msg': "HTTP Error: %s" % e.code}
         except URLError as e:
             return {'ret': False, 'msg': "URL Error: %s" % e.reason}
-        except Exception as e:
-            return {'ret': False, 'msg': "Other error: %s" % e.message}
+        # Almost all errors should be caught above, but just in case
+        except:
+            return {'ret': False, 'msg': "Unknown error"}
         return {'ret': True, 'resp': resp}
 
     def delete_request(self, uri, pyld, hdrs):
@@ -96,8 +99,9 @@ class RedfishUtils(object):
             return {'ret': False, 'msg': "HTTP Error: %s" % e.code}
         except URLError as e:
             return {'ret': False, 'msg': "URL Error: %s" % e.reason}
-        except Exception as e:
-            return {'ret': False, 'msg': "Other error: %s" % e.message}
+        # Almost all errors should be caught above, but just in case
+        except:
+            return {'ret': False, 'msg': "Unknown error"}
         return {'ret': True, 'resp': resp}
 
     def _init_session(self):
