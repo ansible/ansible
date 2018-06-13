98ef0966373d503669f8b1d69fb2e4eb43497d88
Paul <tacid@tacid.kiev.ua>
2018-06-13 17:07:33 +0300

    fix documentation parsing error
    Fixing error: missing documentation (or could not parse documentation): expected string or buffer

--- lib/ansible/modules/monitoring/zabbix/zabbix_screen.py
+++ lib/ansible/modules/monitoring/zabbix/zabbix_screen.py
@@ -34,7 +34,7 @@ options:
             - If the screen(s) already been added, the screen(s) name won't be updated.
             - When creating or updating screen(s), C(screen_name), C(host_group) are required.
             - When deleting screen(s), the C(screen_name) is required.
-            - Option C(graphs_in_row) will limit columns of screen and make multiple rows (default: 3)
+            - Option C(graphs_in_row) will limit columns of screen and make multiple rows (default 3)
             - >
               The available states are: C(present) (default) and C(absent). If the screen(s) already exists, and the state is not C(absent), the screen(s)
               will just be updated as needed.
