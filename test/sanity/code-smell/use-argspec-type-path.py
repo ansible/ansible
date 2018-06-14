5edda1ab27422743e7a00a2ffbe289fa982b9e03
Ben Berry <bengerman@gmail.com>
2018-06-13 18:18:02 -0700

    expand user before calling abspath (#40196)
    * expand user before calling abspath
    
    * omit expanduser code smell check for volumes

--- test/sanity/code-smell/use-argspec-type-path.py
+++ test/sanity/code-smell/use-argspec-type-path.py
@@ -7,6 +7,7 @@ import sys
 def main():
     skip = set([
         # add legitimate uses of expanduser to the following list
+        'lib/ansible/modules/cloud/docker/docker_container.py',  # uses colon-separated paths, can't use type=path
         'lib/ansible/modules/cloud/lxc/lxc_container.py',
         'lib/ansible/modules/cloud/rackspace/rax_files_objects.py',
         'lib/ansible/modules/database/mongodb/mongodb_parameter.py',
