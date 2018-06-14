c54990fd811ae7568f7fd6d1f737ff57f9e97694
Sylvain Rabot <sylvain@abstraction.fr>
2018-06-14 18:52:01 +0200

    Fix rabbitmq_user when using force on user with permissions (#41361)
    On rabbitmq 3.7 using `force: yes` fails because outputs of rabbitmqctl gives an empty line
    
    ```
    root@rabbitmq-vm1:~# rabbitmqctl -q list_user_permissions john
    
    root@rabbitmq-vm1:~#
    ```
    
    Provoking an error on
    
    https://github.com/ansible/ansible/blob/456af458fcc6dfe29b89b8ab5a88ad1baea2fbe9/lib/ansible/modules/messaging/rabbitmq_user.py#L185
    
    Because `perm.split('\t')` does not find any `\t`
    
    Signed-off-by: Sylvain Rabot <s.rabot@lectra.com>
    (cherry picked from commit 8ddca3e6cfd906f0ff48a2c08410a9f82d195deb)

--- lib/ansible/modules/messaging/rabbitmq_user.py
+++ lib/ansible/modules/messaging/rabbitmq_user.py
@@ -152,7 +152,7 @@ class RabbitMqUser(object):
             if self.node is not None:
                 cmd.extend(['-n', self.node])
             rc, out, err = self.module.run_command(cmd + args, check_rc=True)
-            return out.splitlines()
+            return out.splitlines() if len(out.strip()) else []
         return list()
 
     def get(self):
