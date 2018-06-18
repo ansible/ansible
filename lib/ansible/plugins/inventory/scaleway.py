7a211e5cd638841299491ab3e1f92111e2191bc9
Sloane Hertel <shertel@redhat.com>
2018-06-18 07:59:52 -0400

    read scaleway option after the options have been set (#41259)

--- lib/ansible/plugins/inventory/scaleway.py
+++ lib/ansible/plugins/inventory/scaleway.py
@@ -82,7 +82,7 @@ class InventoryModule(BaseInventoryPlugin):
     def __init__(self):
         super(InventoryModule, self).__init__()
 
-        self.token = self.get_option("oauth_token")
+        self.token = None
         self.config_data = None
 
     def verify_file(self, path):
@@ -146,6 +146,7 @@ class InventoryModule(BaseInventoryPlugin):
     def parse(self, inventory, loader, path, cache=True):
         super(InventoryModule, self).parse(inventory, loader, path)
         self.config_data = self._read_config_data(path=path)
+        self.token = self.get_option("oauth_token")
 
         for zone in self._get_zones():
             self.do_zone_inventory(zone=zone)
