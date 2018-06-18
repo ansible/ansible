5131edb1c60027bf4f1f2763f89efb090eb0d4b9
HD650 <zzhang63@ncsu.edu>
2018-06-18 14:01:08 -0400

    More explanation in docstring

--- lib/ansible/modules/utilities/logic/assert.py
+++ lib/ansible/modules/utilities/logic/assert.py
@@ -31,6 +31,7 @@ options:
     version_added: "2.7"
     description:
       - "The customized message used for a failing assertion"
+      - "This argument was called 'msg' before version 2.7, now it's renamed to 'fail_msg' with alias 'msg'"
     aliases:
       - msg
   success_msg:
@@ -52,10 +53,18 @@ EXAMPLES = '''
       - "'foo' in some_command_result.stdout"
       - "number_of_the_counting == 3"
 
+# after version 2.7 both 'msg' and 'fail_msg' can customize failing assertion message
 - assert:
     that:
       - "my_param <= 100"
       - "my_param >= 0"
     fail_msg: "'my_param' must be between 0 and 100"
     success_msg: "'my_param' is between 0 and 100"
+
+# please use 'msg' when ansible version is smaller than 2.7
+- assert:
+    that:
+      - "my_param <= 100"
+      - "my_param >= 0"
+    msg: "'my_param' must be between 0 and 100"
 '''
