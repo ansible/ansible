b03f964106345e7a8ed5631bcf538102f7747df5
Adam Miller <admiller@redhat.com>
2018-06-19 08:33:20 -0500

    add notes to service_facts about accessing fact data (#41608)
    * add notes to service_facts about accessing fact data
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * remove unquoted :
    
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- lib/ansible/modules/system/service_facts.py
+++ lib/ansible/modules/system/service_facts.py
@@ -22,6 +22,15 @@ description:
 version_added: "2.5"
 requirements: ["Any of the following supported init systems: systemd, sysv, upstart"]
 
+notes:
+  - When accessing the C(ansible_facts.services) facts collected by this module,
+    it is recommended to not use "dot notation" because services can have a C(-)
+    character in their name which would result in invalid "dot notation", such as
+    C(ansible_facts.services.zuul-gateway). It is instead recommended to
+    using the string value of the service name as the key in order to obtain
+    the fact data value like C(ansible_facts.services['zuul-gateway'])
+
+
 author:
   - Matthew Jones
   - Adam Miller (@maxamillion)
