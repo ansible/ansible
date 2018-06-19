de4688d7d835d84a1b688277c4751dc30ab224c7
Kristian Østergaard Martensen <kma@cima.dk>
2018-06-19 14:25:24 +0200

    [docs] Include "_facts" in module examples section (#41680)
    The example says module name: "cloudwatchlogs_log_group"
    where it should say: "cloudwatchlogs_log_group_facts"
    
    +label: docsite_pr

--- lib/ansible/modules/cloud/amazon/cloudwatchlogs_log_group_facts.py
+++ lib/ansible/modules/cloud/amazon/cloudwatchlogs_log_group_facts.py
@@ -30,7 +30,7 @@ extends_documentation_fragment:
 
 EXAMPLES = '''
 # Note: These examples do not set authentication details, see the AWS Guide for details.
-- cloudwatchlogs_log_group:
+- cloudwatchlogs_log_group_facts:
     log_group_name: test-log-group
 '''
 
