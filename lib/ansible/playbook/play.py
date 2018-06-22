e9a5c535123b83b29e8942ebd99afd7fbdb6fe2d
Matt Martz <matt@sivel.net>
2018-06-22 13:58:40 -0500

    Handle typing of vars_files in Play.get_vars_files. Fixes #14708 (#41801)

--- lib/ansible/playbook/play.py
+++ lib/ansible/playbook/play.py
@@ -286,6 +286,8 @@ class Play(Base, Taggable, Become):
     def get_vars_files(self):
         if self.vars_files is None:
             return []
+        elif not isinstance(self.vars_files, list):
+            return [self.vars_files]
         return self.vars_files
 
     def get_handlers(self):
