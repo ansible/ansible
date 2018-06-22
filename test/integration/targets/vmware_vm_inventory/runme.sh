90f6c9e6ae846c713598eafebc9fdce90def5c56
Abhijeet Kasurde <akasurde@redhat.com>
2018-06-22 22:54:51 +0530

    Export username and password for failing testcase
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- test/integration/targets/vmware_vm_inventory/runme.sh
+++ test/integration/targets/vmware_vm_inventory/runme.sh
@@ -6,21 +6,21 @@ set -euo pipefail
 
 export ANSIBLE_CONFIG=ansible.cfg
 export vcenter_host="${vcenter_host:-0.0.0.0}"
-export vcenter_user="${vcenter_user:-user}"
-export vcenter_pass="${vcenter_pass:-pass}"
+export VMWARE_SERVER="${vcenter_host}"
+export VMWARE_USERNAME="${VMWARE_USERNAME:-user}"
+export VMWARE_PASSWORD="${VMWARE_PASSWORD:-pass}"
 VMWARE_CONFIG=test-config.vmware.yaml
 
 cat > "$VMWARE_CONFIG" <<VMWARE_YAML
 plugin: vmware_vm_inventory
 strict: False
-hostname: ${vcenter_host}
-username: ${vcenter_user}
-password: ${vcenter_pass}
 validate_certs: False
 with_tags: False
 VMWARE_YAML
 
 trap 'rm -f "${VMWARE_CONFIG}"' INT TERM EXIT
+
+echo "DEBUG: Using ${vcenter_host} with username ${VMWARE_USERNAME} and password ${VMWARE_PASSWORD}"
 
 echo "Kill all previous instances"
 curl "http://${vcenter_host}:5000/killall" > /dev/null 2>&1
