95e92829e85ffcc02d80d1ca90074dcf4361ad42
movinalot <john@movinalot.com>
2018-06-22 18:26:03 -0400

    updates to DOCUMENTATION

--- lib/ansible/modules/remote_management/ucs/ucs_timezone.py
+++ lib/ansible/modules/remote_management/ucs/ucs_timezone.py
@@ -13,57 +13,51 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = r'''
 ---
 module: ucs_timezone
-
 short_description: Configures timezone on Cisco UCS Manager
-
-version_added: "2.7"
-
 description:
-    - Configures timezone on Cisco UCS Manager.
-    - Examples can be used with the UCS Platform Emulator U(https://communities.cisco.com/ucspe).
-
+- Configures timezone on Cisco UCS Manager.
+- Examples can be used with the L(UCS Platform Emulator,https://communities.cisco.com/ucspe).
+extends_documentation_fragment: ucs
 options:
-    state:
-        description:
-            - If C(present), will set or update timezone.
-            - If C(absent), will unset timezone.
-        choices: [present, absent]
-        default: present
-
-    admin_state:
-        description: admin state indicates if the timezone confguration is enabled and being utilized by UCS Manager
-            or disabled and being ignored by UCS Manager
-        choices: ['disabled', 'enabled']
-        default: "enabled"
-
-    timezone:
-        description:
-            - The timezone name. Time zone names are from the tz database U(https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
-            - The timezone name is case sensitive.
-            - This name can be between 0 and 510 alphanumeric characters.
-            - You cannot use spaces or any special characters other than
-            - "\"-\" (hyphen), \"_\" (underscore), \"/\" (backslash)."
-
+  state:
     description:
-        description:
-            - A user-defined description of the timezone object.
-            - Enter up to 256 characters.
-            - "You can use any characters or spaces except the following:"
-            - "` (accent mark), \ (backslash), ^ (carat), \" (double quote), = (equal sign), > (greater than), < (less than), or ' (single quote)."
-        aliases: [ descr ]
-        default: ""
+    - If C(absent), will unset timezone.
+    - If C(present), will set or update timezone.
+    choices: [absent, present]
+    default: present
 
-extends_documentation_fragment:
-    - ucs
+  admin_state:
+    description:
+    - The admin_state setting
+    - The enabled admin_state indicates the timezone confguration is utilized by UCS Manager.
+    - The disabled admin_state indicates the timezone confguration is ignored by UCS Manager.
+    choices: [disabled, enabled]
+    default: enabled
+
+  description:
+    description:
+    - A user-defined description of the timezone.
+    - Enter up to 256 characters.
+    - "You can use any characters or spaces except the following:"
+    - "` (accent mark), \ (backslash), ^ (carat), \" (double quote), = (equal sign), > (greater than), < (less than), or ' (single quote)."
+    aliases: [ descr ]
+    default: ""
+
+  timezone:
+    description:
+    - The timezone name.
+    - Time zone names are from the L(tz database,https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
+    - The timezone name is case sensitive.
+    - The timezone name can be between 0 and 510 alphanumeric characters.
+    - You cannot use spaces or any special characters other than
+    - "\"-\" (hyphen), \"_\" (underscore), \"/\" (backslash)."
 
 requirements:
-
-    - ucsmsdk
-
+- ucsmsdk
 author:
-
-    - John McDonough (@movinalot)
-    - CiscoUcs (@CiscoUcs)
+- John McDonough (@movinalot)
+- CiscoUcs (@CiscoUcs)
+version_added: '2.7'
 '''
 
 EXAMPLES = r'''
