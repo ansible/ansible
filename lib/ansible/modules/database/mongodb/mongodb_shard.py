1650697682af19843878503d55610279e72ecd57
Rhys Campbell <rhys.james.campbell@googlemail.com>
2018-06-24 18:46:43 +0200

    From future imports at top

--- lib/ansible/modules/database/mongodb/mongodb_shard.py
+++ lib/ansible/modules/database/mongodb/mongodb_shard.py
@@ -3,6 +3,9 @@
 # (c) 2018, Rhys Campbell <rhys.james.campbell@googlemail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
@@ -116,8 +119,6 @@ mongodb_shard:
     returned: success
     type: string
 '''
-from __future__ import absolute_import, division, print_function
-__metaclass__ = type
 
 import os
 import ssl as ssl_lib
