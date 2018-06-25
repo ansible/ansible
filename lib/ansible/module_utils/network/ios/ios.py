236597bd6659dca95ac9fed6b2b21f2dac885be0
Ganesh Nalawade <ganesh634@gmail.com>
2018-06-25 15:35:39 +0530

    Fix ios and vyos cliconf edit_config return type (#41896)
    * Fix ios and vyos cliconf edit_config return type
    
    Modify cliconf edit_config api to return a json string with
    diff and response received from remote host for ios and vyos.
    
    * Doc change

--- lib/ansible/module_utils/network/ios/ios.py
+++ lib/ansible/module_utils/network/ios/ios.py
@@ -166,7 +166,8 @@ def load_config(module, commands):
     connection = get_connection(module)
 
     try:
-        diff, response = connection.edit_config(commands)
-        return response
+        resp = connection.edit_config(commands)
+        resp = json.loads(resp)
+        return resp.get('response')
     except ConnectionError as exc:
         module.fail_json(msg=to_text(exc))
