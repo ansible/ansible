74f1634799722ee38f217dfa8f8c97ca720b36d3
Stoned Elipot <stoned.elipot@gmail.com>
2018-06-29 00:04:22 +0200

    dbm lookup test tweaks
    - install required dependency, python-gdbm, on openSUSE Leap
    - make the test DBM file creation Python 2/3 aware

---
+++ test/integration/targets/lookups/files/mktestdbm.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from __future__ import (absolute_import, division, print_function)                                                                      
+
+import sys
+try:
+    import dbm as db
+except ImportError:
+    import dbm.ndbm as db
+
+
+def main():
+    d = db.open(sys.argv[1], 'n')
+    d['k1'] = 'v1'
+    d['k2'] = 'v2'
+    d.close()
+
+if __name__ == "__main__":
+    main()
