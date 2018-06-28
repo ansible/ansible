5e75d51cdd72617dad82810813cb10779540b02f
Stoned Elipot <stoned.elipot@gmail.com>
2018-06-29 00:50:27 +0200

    fix whitespace !

--- test/integration/targets/lookups/files/mktestdbm.py
+++ test/integration/targets/lookups/files/mktestdbm.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from __future__ import (absolute_import, division, print_function)                                                                      
+from __future__ import (absolute_import, division, print_function)
 
 import sys
 try:
