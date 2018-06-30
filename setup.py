12c84f44a0d6b4569e3275577dc4c229137ddd16
Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>
2018-06-30 22:55:50 +0300

    Add CoC and mailing lists links to PYPI
    PR #42101

--- setup.py
+++ setup.py
@@ -232,7 +232,9 @@ static_setup_params = dict(
     project_urls={
         'Bug Tracker': 'https://github.com/ansible/ansible/issues',
         'CI: Shippable': 'https://app.shippable.com/github/ansible/ansible',
+        'Code of Conduct': 'https://docs.ansible.com/ansible/latest/community/code_of_conduct.html',
         'Documentation': 'https://docs.ansible.com/ansible/',
+        'Mailing lists': 'https://docs.ansible.com/ansible/latest/community/communication.html#mailing-list-information',
         'Source Code': 'https://github.com/ansible/ansible',
     },
     license='GPLv3+',
