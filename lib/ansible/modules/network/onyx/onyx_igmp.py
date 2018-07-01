44faa3dfb775058972933d090059a0e175a1c5d2
Samer Deeb <samerd@mellanox.com>
2018-07-01 08:42:29 +0000

    Add support for global IGMP configuration on onyx switches
    Signed-off-by: Samer Deeb <samerd@mellanox.com>

--- lib/ansible/modules/network/onyx/onyx_igmp.py
+++ lib/ansible/modules/network/onyx/onyx_igmp.py
@@ -12,15 +12,15 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 
 DOCUMENTATION = """
 ---
-module: onyx_bgp
-version_added: "2.6"
+module: onyx_igmp
+version_added: "2.7"
 author: "Samer Deeb (@samerd)"
 short_description: Configures IGMP globl parameters
 description:
   - This module provides declarative management of IGMP protocol params
     on Mellanox ONYX network devices.
 notes:
-  - Tested on ONYX 3.6.4000
+  - Tested on ONYX 3.6.6107
 options:
   state:
     description:
@@ -30,36 +30,29 @@ options:
   last_member_query_interval:
     description:
       - Configure the last member query interval, range 1-25
-    default: 1
   mrouter_timeout:
     description:
       - Configure the mrouter timeout, range 60-600
-    default: 125
   port_purge_timeout:
     description:
       - Configure the host port purge timeout, range 130-1225
-    default: 260
   proxy_reporting:
     description:
       - Configure ip igmp snooping proxy and enable reporting mode
     choices: ['enabled', 'disabled']
-    default: enabled
   report_suppression_interval:
     description:
       - Configure the report suppression interval, range 1-25
-    default: 5
   unregistered_multicast:
     description:
       - Configure the unregistered multicast mode
         Flood unregistered multicast
         Forward unregistered multicast to mrouter ports
     choices: ['flood', 'forward-to-mrouter-ports']
-    default: flood
   default_version:
     description:
       - Configure the default operating version of the IGMP snooping
     choices: ['V2','V3']
-    default: V3
 """
 
 EXAMPLES = """
