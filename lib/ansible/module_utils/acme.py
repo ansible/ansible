887e2b6da1d33f5266105dfb99ccaf52f145e45f
Felix Fontein <felix@fontein.de>
2018-07-01 14:32:01 +0200

    Move OpenSSL specific code for key parsing and request signing into global functions.

--- lib/ansible/module_utils/acme.py
+++ lib/ansible/module_utils/acme.py
@@ -176,6 +176,143 @@ def pem_to_der(pem_filename):
     return base64.b64decode(''.join(certificate_lines))
 
 
+def _parse_key_openssl(openssl_binary, module, key_file=None, key_content=None):
+    '''
+    Parses an RSA or Elliptic Curve key file in PEM format and returns a pair
+    (error, key_data).
+    '''
+    # If key_file isn't given, but key_content, write that to a temporary file
+    if key_file is None:
+        assert key_content is not None
+        fd, tmpsrc = tempfile.mkstemp()
+        module.add_cleanup_file(tmpsrc)  # Ansible will delete the file on exit
+        f = os.fdopen(fd, 'wb')
+        try:
+            f.write(key_content.encode('utf-8'))
+            key_file = tmpsrc
+        except Exception as err:
+            try:
+                f.close()
+            except Exception as e:
+                pass
+            raise ModuleFailException("failed to create temporary content file: %s" % to_native(err), exception=traceback.format_exc())
+        f.close()
+    # Parse key
+    account_key_type = None
+    with open(key_file, "rt") as f:
+        for line in f:
+            m = re.match(r"^\s*-{5,}BEGIN\s+(EC|RSA)\s+PRIVATE\s+KEY-{5,}\s*$", line)
+            if m is not None:
+                account_key_type = m.group(1).lower()
+                break
+    if account_key_type is None:
+        # This happens for example if openssl_privatekey created this key
+        # (as opposed to the OpenSSL binary). For now, we assume this is
+        # an RSA key.
+        # FIXME: add some kind of auto-detection
+        account_key_type = "rsa"
+    if account_key_type not in ("rsa", "ec"):
+        return 'unknown key type "%s"' % account_key_type, {}
+
+    openssl_keydump_cmd = [openssl_binary, account_key_type, "-in", key_file, "-noout", "-text"]
+    dummy, out, dummy = module.run_command(openssl_keydump_cmd, check_rc=True)
+
+    if account_key_type == 'rsa':
+        pub_hex, pub_exp = re.search(
+            r"modulus:\n\s+00:([a-f0-9\:\s]+?)\npublicExponent: ([0-9]+)",
+            to_text(out, errors='surrogate_or_strict'), re.MULTILINE | re.DOTALL).groups()
+        pub_exp = "{0:x}".format(int(pub_exp))
+        if len(pub_exp) % 2:
+            pub_exp = "0{0}".format(pub_exp)
+
+        return None, {
+            'key': key_file,
+            'type': 'rsa',
+            'alg': 'RS256',
+            'jwk': {
+                "kty": "RSA",
+                "e": nopad_b64(binascii.unhexlify(pub_exp.encode("utf-8"))),
+                "n": nopad_b64(binascii.unhexlify(re.sub(r"(\s|:)", "", pub_hex).encode("utf-8"))),
+            },
+            'hash': 'sha256',
+        }
+    elif account_key_type == 'ec':
+        pub_data = re.search(
+            r"pub:\s*\n\s+04:([a-f0-9\:\s]+?)\nASN1 OID: (\S+)(?:\nNIST CURVE: (\S+))?",
+            to_text(out, errors='surrogate_or_strict'), re.MULTILINE | re.DOTALL)
+        if pub_data is None:
+            return 'cannot parse elliptic curve key', {}
+        pub_hex = binascii.unhexlify(re.sub(r"(\s|:)", "", pub_data.group(1)).encode("utf-8"))
+        asn1_oid_curve = pub_data.group(2).lower()
+        nist_curve = pub_data.group(3).lower() if pub_data.group(3) else None
+        if asn1_oid_curve == 'prime256v1' or nist_curve == 'p-256':
+            bits = 256
+            alg = 'ES256'
+            hash = 'sha256'
+            point_size = 32
+            curve = 'P-256'
+        elif asn1_oid_curve == 'secp384r1' or nist_curve == 'p-384':
+            bits = 384
+            alg = 'ES384'
+            hash = 'sha384'
+            point_size = 48
+            curve = 'P-384'
+        elif asn1_oid_curve == 'secp521r1' or nist_curve == 'p-521':
+            # Not yet supported on Let's Encrypt side, see
+            # https://github.com/letsencrypt/boulder/issues/2217
+            bits = 521
+            alg = 'ES512'
+            hash = 'sha512'
+            point_size = 66
+            curve = 'P-521'
+        else:
+            return 'unknown elliptic curve: %s / %s' % (asn1_oid_curve, nist_curve), {}
+        bytes = (bits + 7) // 8
+        if len(pub_hex) != 2 * bytes:
+            return 'bad elliptic curve point (%s / %s)' % (asn1_oid_curve, nist_curve), {}
+        return None, {
+            'key': key_file,
+            'type': 'ec',
+            'alg': alg,
+            'jwk': {
+                "kty": "EC",
+                "crv": curve,
+                "x": nopad_b64(pub_hex[:bytes]),
+                "y": nopad_b64(pub_hex[bytes:]),
+            },
+            'hash': hash,
+            'point_size': point_size,
+        }
+
+
+def _sign_request_openssl(openssl_binary, module, payload64, protected64, key_data):
+    openssl_sign_cmd = [openssl_binary, "dgst", "-{0}".format(key_data['hash']), "-sign", key_data['key']]
+    sign_payload = "{0}.{1}".format(protected64, payload64).encode('utf8')
+    dummy, out, dummy = module.run_command(openssl_sign_cmd, data=sign_payload, check_rc=True, binary_data=True)
+
+    if key_data['type'] == 'ec':
+        dummy, der_out, dummy = module.run_command(
+            [openssl_binary, "asn1parse", "-inform", "DER"],
+            data=out, binary_data=True)
+        expected_len = 2 * key_data['point_size']
+        sig = re.findall(
+            r"prim:\s+INTEGER\s+:([0-9A-F]{1,%s})\n" % expected_len,
+            to_text(der_out, errors='surrogate_or_strict'))
+        if len(sig) != 2:
+            raise ModuleFailException(
+                "failed to generate Elliptic Curve signature; cannot parse DER output: {0}".format(
+                    to_text(der_out, errors='surrogate_or_strict')))
+        sig[0] = (expected_len - len(sig[0])) * '0' + sig[0]
+        sig[1] = (expected_len - len(sig[1])) * '0' + sig[1]
+        out = binascii.unhexlify(sig[0]) + binascii.unhexlify(sig[1])
+
+    return {
+        "protected": protected64,
+        "payload": payload64,
+        "signature": nopad_b64(to_bytes(out)),
+    }
+
+
 class ACMEDirectory(object):
     '''
     The ACME server directory. Gives access to the available resources,
@@ -260,108 +397,7 @@ class ACMEAccount(object):
         Parses an RSA or Elliptic Curve key file in PEM format and returns a pair
         (error, key_data).
         '''
-        # If key_file isn't given, but key_content, write that to a temporary file
-        if key_file is None:
-            assert key_content is not None
-            fd, tmpsrc = tempfile.mkstemp()
-            self.module.add_cleanup_file(tmpsrc)  # Ansible will delete the file on exit
-            f = os.fdopen(fd, 'wb')
-            try:
-                f.write(key_content.encode('utf-8'))
-                key_file = tmpsrc
-            except Exception as err:
-                try:
-                    f.close()
-                except Exception as e:
-                    pass
-                raise ModuleFailException("failed to create temporary content file: %s" % to_native(err), exception=traceback.format_exc())
-            f.close()
-        # Parse key
-        account_key_type = None
-        with open(key_file, "rt") as f:
-            for line in f:
-                m = re.match(r"^\s*-{5,}BEGIN\s+(EC|RSA)\s+PRIVATE\s+KEY-{5,}\s*$", line)
-                if m is not None:
-                    account_key_type = m.group(1).lower()
-                    break
-        if account_key_type is None:
-            # This happens for example if openssl_privatekey created this key
-            # (as opposed to the OpenSSL binary). For now, we assume this is
-            # an RSA key.
-            # FIXME: add some kind of auto-detection
-            account_key_type = "rsa"
-        if account_key_type not in ("rsa", "ec"):
-            return 'unknown key type "%s"' % account_key_type, {}
-
-        openssl_keydump_cmd = [self._openssl_bin, account_key_type, "-in", key_file, "-noout", "-text"]
-        dummy, out, dummy = self.module.run_command(openssl_keydump_cmd, check_rc=True)
-
-        if account_key_type == 'rsa':
-            pub_hex, pub_exp = re.search(
-                r"modulus:\n\s+00:([a-f0-9\:\s]+?)\npublicExponent: ([0-9]+)",
-                to_text(out, errors='surrogate_or_strict'), re.MULTILINE | re.DOTALL).groups()
-            pub_exp = "{0:x}".format(int(pub_exp))
-            if len(pub_exp) % 2:
-                pub_exp = "0{0}".format(pub_exp)
-
-            return None, {
-                'key': key_file,
-                'type': 'rsa',
-                'alg': 'RS256',
-                'jwk': {
-                    "kty": "RSA",
-                    "e": nopad_b64(binascii.unhexlify(pub_exp.encode("utf-8"))),
-                    "n": nopad_b64(binascii.unhexlify(re.sub(r"(\s|:)", "", pub_hex).encode("utf-8"))),
-                },
-                'hash': 'sha256',
-            }
-        elif account_key_type == 'ec':
-            pub_data = re.search(
-                r"pub:\s*\n\s+04:([a-f0-9\:\s]+?)\nASN1 OID: (\S+)(?:\nNIST CURVE: (\S+))?",
-                to_text(out, errors='surrogate_or_strict'), re.MULTILINE | re.DOTALL)
-            if pub_data is None:
-                return 'cannot parse elliptic curve key', {}
-            pub_hex = binascii.unhexlify(re.sub(r"(\s|:)", "", pub_data.group(1)).encode("utf-8"))
-            asn1_oid_curve = pub_data.group(2).lower()
-            nist_curve = pub_data.group(3).lower() if pub_data.group(3) else None
-            if asn1_oid_curve == 'prime256v1' or nist_curve == 'p-256':
-                bits = 256
-                alg = 'ES256'
-                hash = 'sha256'
-                point_size = 32
-                curve = 'P-256'
-            elif asn1_oid_curve == 'secp384r1' or nist_curve == 'p-384':
-                bits = 384
-                alg = 'ES384'
-                hash = 'sha384'
-                point_size = 48
-                curve = 'P-384'
-            elif asn1_oid_curve == 'secp521r1' or nist_curve == 'p-521':
-                # Not yet supported on Let's Encrypt side, see
-                # https://github.com/letsencrypt/boulder/issues/2217
-                bits = 521
-                alg = 'ES512'
-                hash = 'sha512'
-                point_size = 66
-                curve = 'P-521'
-            else:
-                return 'unknown elliptic curve: %s / %s' % (asn1_oid_curve, nist_curve), {}
-            bytes = (bits + 7) // 8
-            if len(pub_hex) != 2 * bytes:
-                return 'bad elliptic curve point (%s / %s)' % (asn1_oid_curve, nist_curve), {}
-            return None, {
-                'key': key_file,
-                'type': 'ec',
-                'alg': alg,
-                'jwk': {
-                    "kty": "EC",
-                    "crv": curve,
-                    "x": nopad_b64(pub_hex[:bytes]),
-                    "y": nopad_b64(pub_hex[bytes:]),
-                },
-                'hash': hash,
-                'point_size': point_size,
-            }
+        return _parse_key_openssl(self._openssl_bin, self.module, key_file, key_content)
 
     def sign_request(self, protected, payload, key_data):
         try:
@@ -370,31 +406,7 @@ class ACMEAccount(object):
         except Exception as e:
             raise ModuleFailException("Failed to encode payload / headers as JSON: {0}".format(e))
 
-        openssl_sign_cmd = [self._openssl_bin, "dgst", "-{0}".format(key_data['hash']), "-sign", key_data['key']]
-        sign_payload = "{0}.{1}".format(protected64, payload64).encode('utf8')
-        dummy, out, dummy = self.module.run_command(openssl_sign_cmd, data=sign_payload, check_rc=True, binary_data=True)
-
-        if key_data['type'] == 'ec':
-            dummy, der_out, dummy = self.module.run_command(
-                [self._openssl_bin, "asn1parse", "-inform", "DER"],
-                data=out, binary_data=True)
-            expected_len = 2 * key_data['point_size']
-            sig = re.findall(
-                r"prim:\s+INTEGER\s+:([0-9A-F]{1,%s})\n" % expected_len,
-                to_text(der_out, errors='surrogate_or_strict'))
-            if len(sig) != 2:
-                raise ModuleFailException(
-                    "failed to generate Elliptic Curve signature; cannot parse DER output: {0}".format(
-                        to_text(der_out, errors='surrogate_or_strict')))
-            sig[0] = (expected_len - len(sig[0])) * '0' + sig[0]
-            sig[1] = (expected_len - len(sig[1])) * '0' + sig[1]
-            out = binascii.unhexlify(sig[0]) + binascii.unhexlify(sig[1])
-
-        return {
-            "protected": protected64,
-            "payload": payload64,
-            "signature": nopad_b64(to_bytes(out)),
-        }
+        return _sign_request_openssl(self._openssl_bin, self.module, payload64, protected64, key_data)
 
     def send_signed_request(self, url, payload, key_data=None, jws_header=None):
         '''
