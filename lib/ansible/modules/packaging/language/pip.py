02239b23b16f9b057d59ba64871c68908ca9c39f
soou <soou@bbtower.co.jp>
2018-07-02 15:18:53 +0900

    fix duplicate import

--- lib/ansible/modules/packaging/language/pip.py
+++ lib/ansible/modules/packaging/language/pip.py
@@ -7,8 +7,6 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-import re
-
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'core'}
