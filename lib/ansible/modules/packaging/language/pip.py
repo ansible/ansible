4f193f988093802d062d80f5013a253118127296
soou <soou@bbtower.co.jp>
2018-07-02 13:11:41 +0900

    fix version not specified

--- lib/ansible/modules/packaging/language/pip.py
+++ lib/ansible/modules/packaging/language/pip.py
@@ -26,7 +26,7 @@ options:
       - As of 2.2 you can supply a list of names.
   version:
     description:
-      - The version number to install of the Python library specified in the I(name) parameter.
+      - The version expression to install of the Python library specified in the I(name) parameter.
   requirements:
     description:
       - The path to a pip requirements file, which should be local to the remote system.
@@ -123,7 +123,7 @@ EXAMPLES = '''
 # Install (Bottle) python package on version 0.11.
 - pip:
     name: bottle
-    version: 0.11
+    version: "==0.11"
 
 # Install (MyApp) using one of the remote protocols (bzr+,hg+,git+,svn+). You do not have to supply '-e' option in extra_args.
 - pip:
@@ -220,7 +220,7 @@ def _get_cmd_options(module, cmd):
 
 def _get_full_name(name, version=None):
     if version is None or version == "":
-        resp = name
+        resp = name + '=='
     else:
         resp = name + version
     return resp
