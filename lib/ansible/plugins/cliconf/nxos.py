94a68a6b1b6b6e764bf5c684f7ba70ba854491fe
Trishna Guha <trishnaguha17@gmail.com>
2018-07-03 10:46:02 +0530

    update additional logic
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/plugins/cliconf/nxos.py
+++ lib/ansible/plugins/cliconf/nxos.py
@@ -68,6 +68,11 @@ class Cliconf(CliconfBase):
             if match_kick_ver:
                 device_info['network_os_version'] = match_kick_ver.group(1)
 
+        if 'network_os_version' not in device_info:
+            match_sys_ver = re.search(r'\s+NXOS:\s+version (\S+)', reply, re.M)
+            if match_sys_ver:
+                device_info['network_os_version'] = match_sys_ver.group(1)
+
         match_chassis_id = re.search(r'Hardware\n\s+cisco\s+(\S+\s+\S+)', reply, re.M)
         if match_chassis_id:
             device_info['network_os_model'] = match_chassis_id.group(1)
@@ -83,6 +88,11 @@ class Cliconf(CliconfBase):
             match_kick_file_name = re.search(r'\s+kickstart image file is:\s+(\S+)', reply, re.M)
             if match_kick_file_name:
                 device_info['network_os_image'] = match_kick_file_name.group(1)
+
+        if 'network_os_image' not in device_info:
+            match_isan_file_name = re.search(r'\s+NXOS image file is:\s+(\S+)', reply, re.M)
+            if match_isan_file_name:
+                device_info['network_os_image'] = match_isan_file_name.group(1)
 
         match_os_platform = re.search(r'NAME: "Chassis",\s+DESCR: "NX-OSv Chassis\s?"\s+\n'
                                       r'PID:\s+(\S+)', platform_reply, re.M)
