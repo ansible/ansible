53cab595d0e4a0c6d8c3f7693eae55d1fbd85fdb
Scott Mishra <scottmishra@gmail.com>
2018-07-03 11:17:57 -0500

    removing 2 whitespaces one line 183 of definition.py

--- lib/ansible/playbook/role/definition.py
+++ lib/ansible/playbook/role/definition.py
@@ -180,7 +180,7 @@ class RoleDefinition(Base, Become, Conditional, Taggable):
                 path = templar.template(path)
                 role_path = unfrackpath(os.path.join(path, role_version_name))
                 if self._loader.path_exists(role_path):
-                    return (role_name, role_path)   
+                    return (role_name, role_path)
         for path in role_search_paths:
             path = templar.template(path)
             role_path = unfrackpath(os.path.join(path, role_name))
