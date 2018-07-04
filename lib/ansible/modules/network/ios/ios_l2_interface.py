3aeb1285df3aadf7b5ae31f5d589de4917b6ae40
Sumit Jaiswal <sjaiswal@ovpn-116-61.sin2.redhat.com>
2018-07-04 19:56:52 +0530

    to fix the bug41657

--- lib/ansible/modules/network/ios/ios_l2_interface.py
+++ lib/ansible/modules/network/ios/ios_l2_interface.py
@@ -155,7 +155,9 @@ def interface_is_portchannel(name, module):
 def get_switchport(name, module):
     config = run_commands(module, ['show interface {0} switchport'.format(name)])[0]
     mode = re.search(r'Administrative Mode: (?:.* )?(\w+)$', config, re.M).group(1)
-    access = re.search(r'Access Mode VLAN: (\d+)', config).group(1)
+    access = re.search(r'Access Mode VLAN: (\d+)', config)
+    if access:
+        access = access.group(1)
     native = re.search(r'Trunking Native Mode VLAN: (\d+)', config).group(1)
     trunk = re.search(r'Trunking VLANs Enabled: (.+)$', config, re.M).group(1)
     if trunk == 'ALL':
