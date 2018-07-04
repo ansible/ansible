7363e2b078c4bbf0a3067d8ca74710b86efd5c31
Hai Cao <t-haicao@microsoft.com>
2018-07-04 14:20:01 +0800

    refactor: Moved non-msi branch to a seperate fuction
    - Refactored non-msi branch, change variable of client_secret, other
    minor fix
    - Added an VM-provision example to Document

--- lib/ansible/plugins/lookup/azure_keyvault_secret.py
+++ lib/ansible/plugins/lookup/azure_keyvault_secret.py
@@ -20,23 +20,48 @@ DOCUMENTATION = """
             required: True
         vault_url:
             description: url of azure key vault to be retrieved from
-            default: 'azure-key-vault'
             required: True
         client_id:
             description: client_id of service principal that has access to the provided azure key vault
-        key:
-            description: key of service principal provided above
+        secret:
+            description: secret of service principal provided above
         tenant_id:
             description: tenant_id of service principal provided above
     notes:
-        - If this plugin is called on an azure virtual machine and the machine has access to the desired key vault via MSI, then you don't need to provide client_id, key, tenant_id.
+        - If ansible is running on Azure Virtual Machine with MSI enabled, client_id, secret and tenant isn't necessary. For how to enable MSI on Azure Virutal Machine, please refer to this doc https://docs.microsoft.com/en-us/azure/active-directory/managed-service-identity/
         - If this plugin is called on a non-azure virtual machine or it's an azure machine has no access to the desired key vault via MSI, then you have to provide a valid service principal that has access to the key vault. 
 """
 
 EXAMPLE = """
 - debug: msg="the value of this secret is {{lookup('azure_keyvault_secret','testSecret/version')}}"
 
-- debug: msg="the value of this secret is {{lookup('azure_keyvault_secret','testSecret/version',vault_url='https://myvault.vault.azure.net', cliend_id='123456789', key='abcdefg', tenant_id='uvwxyz')}}"
+- debug: msg="the value of this secret is {{lookup('azure_keyvault_secret','testSecret/version',vault_url='https://myvault.vault.azure.net', cliend_id='123456789', secret='abcdefg', tenant_id='uvwxyz')}}"
+
+# Example below creates an Azure Virtual Machine with ssh public key from key vault using 'azure_keyvault_secret' lookup plugin.
+- name: Create Azure VM
+  hosts: localhost
+  connection: local
+  vars:
+    resource_group: myResourceGroup
+    vm_name: testvm
+    location: eastus
+    ssh_key: "{{ lookup('azure_keyvault_secret','myssh_key') }}"
+  - name: Create VM
+    azure_rm_virtualmachine:
+      resource_group: "{{ resource_group }}"
+      name: "{{ vm_name }}"
+      vm_size: Standard_DS1_v2
+      admin_username: azureuser
+      ssh_password_enabled: false
+      ssh_public_keys:
+        - path: /home/azureuser/.ssh/authorized_keys
+          key_data: "{{ ssh_key }}"
+      network_interfaces: "{{ vm_name }}"
+      image:
+        offer: UbuntuServer
+        publisher: Canonical
+        sku: 16.04-LTS
+        version: latest
 """
 
 RETURN = """
@@ -62,9 +87,48 @@ try:
     token_res = requests.get('http://169.254.169.254/metadata/identity/oauth2/token', params=token_params, headers=token_headers)
     token = token_res.json()["access_token"]
     TOKEN_ACQUIRED = True
-except requests.exceptions.RequestException as e:
+except requests.exceptions.RequestException:
     print('Unable to fetch MSI token. Will use service principal if provided.')
     TOKEN_ACQUIRED = False
+
+
+def lookup_sercret_non_msi(terms, vault_url, kwargs):
+    import logging
+    logging.getLogger('msrestazure.azure_active_directory').addHandler(logging.NullHandler())
+    logging.getLogger('msrest.service_client').addHandler(logging.NullHandler())
+
+    try:
+        from azure.common.credentials import ServicePrincipalCredentials
+        from azure.keyvault import KeyVaultClient
+        from msrest.exceptions import AuthenticationError, ClientRequestError
+        from azure.keyvault.models.key_vault_error import KeyVaultErrorException
+    except ImportError:
+        raise AnsibleError('The azure_keyvault_secret lookup plugin requires azure.keyvault and azure.common.credentials to be installed.')
+
+    client_id = kwargs.pop('client_id', None)
+    secret = kwargs.pop('secret', None)
+    tenant_id = kwargs.pop('tenant_id', None)
+
+    try:
+        credentials = ServicePrincipalCredentials(
+            client_id=client_id,
+            secret=secret,
+            tenant=tenant_id
+        )
+        client = KeyVaultClient(credentials)
+    except AuthenticationError:
+        raise AnsibleError('Invalid credentials provided.')
+
+    ret = []
+    for term in terms:
+        try:
+            secret_val = client.get_secret(vault_url, term, '').value
+            ret.append(secret_val)
+        except ClientRequestError:
+            raise AnsibleError('Error occurred in request')
+        except KeyVaultErrorException:
+            raise AnsibleError('Failed to fetch secret ' + term + '.')
+    return ret
 
 
 class LookupModule(LookupBase):
@@ -80,44 +144,10 @@ class LookupModule(LookupBase):
                 try:
                     secret_res = requests.get(vault_url + 'secrets/' + term, params=secret_params, headers=secret_headers)
                     ret.append(secret_res.json()["value"])
-                except requests.exceptions.RequestException as e:
+                except requests.exceptions.RequestException:
                     raise AnsibleError('Failed to fetch secret: ' + term + ' via MSI endpoint.')
                 except KeyError:
                     raise AnsibleError('Failed to fetch secret ' + term + '.')
             return ret
         else:
-            import logging
-            logging.getLogger('msrestazure.azure_active_directory').addHandler(logging.NullHandler())
-            logging.getLogger('msrest.service_client').addHandler(logging.NullHandler())
-
-            try:
-                from azure.common.credentials import ServicePrincipalCredentials
-                from azure.keyvault import KeyVaultClient
-                from msrest.exceptions import AuthenticationError, ClientRequestError
-                from azure.keyvault.models.key_vault_error import KeyVaultErrorException
-            except ImportError:
-                raise AnsibleError('The azure_keyvault_secret lookup plugin requires azure.keyvault and azure.common.credentials to be installed.')
-
-            client_id = kwargs.pop('client_id', None)
-            key = kwargs.pop('key', None)
-            tenant_id = kwargs.pop('tenant_id', None)
-
-            try:
-                credentials = ServicePrincipalCredentials(
-                    client_id=client_id,
-                    secret=key,
-                    tenant=tenant_id
-                )
-                client = KeyVaultClient(credentials)
-            except AuthenticationError as e:
-                raise AnsibleError('Invalid credentials provided.')
-
-            for term in terms:
-                try:
-                    secret = client.get_secret(vault_url, term, '').value
-                    ret.append(secret)
-                except ClientRequestError as e:
-                    raise AnsibleError('Error occurred in request')
-                except KeyVaultErrorException as e:
-                    raise AnsibleError('Failed to fetch secret ' + term + '.')
-            return ret
+            return lookup_sercret_non_msi(terms,vault_url,kwargs)
