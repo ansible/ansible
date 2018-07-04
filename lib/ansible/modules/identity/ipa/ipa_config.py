78fdf93f4dc9f765de4adf2e53cfeab0c4cc5605
fxfitz <francis.x.fitzpatrick@gmail.com>
2018-07-04 11:58:15 -0500

    Doc: Add periods

--- lib/ansible/modules/identity/ipa/ipa_config.py
+++ lib/ansible/modules/identity/ipa/ipa_config.py
@@ -17,12 +17,12 @@ module: ipa_config
 author: Fran Fitzpatrick (@fxfitz)
 short_description: Manage Global FreeIPA Configuration Settings
 description:
-- Modify global configuration settings of a FreeIPA Server
+- Modify global configuration settings of a FreeIPA Server.
 options:
   ipadefaultloginshell:
-    description: Default shell for new users
+    description: Default shell for new users.
   ipadefaultemaildomain:
-    description: Default e-mail domain
+    description: Default e-mail domain for new users.
 extends_documentation_fragment: ipa.documentation
 version_added: "2.7"
 '''
