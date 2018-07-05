5a13237d9c4a0df39f3bb4fca3d6831d868bde11
Sumit Jaiswal <sjaiswal@redhat.com>
2018-07-05 10:58:54 +0530

    To fix the NoneType error raised in ios_l2_interface when Access Mode VLAN is unassigned (#42312)
    * to fix the bug41657
    
    * to fix review comment

--- lib/ansible/modules/network/ios/ios_l2_interface.py
+++ lib/ansible/modules/network/ios/ios_l2_interface.py
@@ -154,10 +154,18 @@ def interface_is_portchannel(name, module):
 
 def get_switchport(name, module):
     config = run_commands(module, ['show interface {0} switchport'.format(name)])[0]
-    mode = re.search(r'Administrative Mode: (?:.* )?(\w+)$', config, re.M).group(1)
-    access = re.search(r'Access Mode VLAN: (\d+)', config).group(1)
-    native = re.search(r'Trunking Native Mode VLAN: (\d+)', config).group(1)
-    trunk = re.search(r'Trunking VLANs Enabled: (.+)$', config, re.M).group(1)
+    mode = re.search(r'Administrative Mode: (?:.* )?(\w+)$', config, re.M)
+    access = re.search(r'Access Mode VLAN: (\d+)', config)
+    native = re.search(r'Trunking Native Mode VLAN: (\d+)', config)
+    trunk = re.search(r'Trunking VLANs Enabled: (.+)$', config, re.M)
+    if mode:
+        mode = mode.group(1)
+    if access:
+        access = access.group(1)
+    if native:
+        native = native.group(1)
+    if trunk:
+        trunk = trunk.group(1)
     if trunk == 'ALL':
         trunk = '1-4094'
 
