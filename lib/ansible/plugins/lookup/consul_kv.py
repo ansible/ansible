96c5c981c466beb14a8c5343187409af8a137d07
Alejandro Paredero <paredero@gmail.com>
2018-07-05 21:53:03 +0200

    Removed blank spaces on line 56
    Removed blank spaces on line 56

--- lib/ansible/plugins/lookup/consul_kv.py
+++ lib/ansible/plugins/lookup/consul_kv.py
@@ -53,7 +53,7 @@ EXAMPLES = """
   - name: retrieving a KV from a remote cluster on non default port
     debug:
       msg: "{{ lookup('consul_kv', 'my/key', host='10.10.10.10', port='2000') }}"
-      
+
   - name: retrieving a KV from a remote cluster on non default port with ACL enabled
     debug:
       msg: "{{ lookup('consul_kv', 'my/key', host='10.10.10.10 token=a20f2009-a1cf-4e19-8286-0632ea3bcad6 recurse=yes', port='2000') }}"
