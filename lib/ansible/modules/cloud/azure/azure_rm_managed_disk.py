d0e3e656257ffc8079a95b646eeec84f13cb974e
Yuwei Zhou <yuwzho@microsoft.com>
2018-07-06 16:37:00 +0800

    fix

--- lib/ansible/modules/cloud/azure/azure_rm_managed_disk.py
+++ lib/ansible/modules/cloud/azure/azure_rm_managed_disk.py
@@ -162,10 +162,8 @@ def managed_disk_to_dict(managed_disk):
 
     create_data = managed_disk.creation_data
     result['create_option'] = create_data.create_option.value.lower()
-    if create_data.source_uri:
-        result['source_uri'] = create_data.source_uri
-    if create_data.source_resource_id:
-        result['source_resource_id'] = create_data.source_resource_id
+    result['source_uri'] = create_data.source_uri
+    result['source_resource_id'] = create_data.source_resource_id
     return result
 
 
