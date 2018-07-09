f290fcbf7c82df318594dac1eb9d203b5b8ffe72
Matthew Via <matt.via@omnivore.io>
2018-07-09 10:37:43 -0400

    Support UpdateService forceNewDeployment in ecs_service module

--- lib/ansible/modules/cloud/amazon/ecs_service.py
+++ lib/ansible/modules/cloud/amazon/ecs_service.py
@@ -425,7 +425,8 @@ class EcsServiceManager:
             service=service_name,
             taskDefinition=task_definition,
             desiredCount=desired_count,
-            deploymentConfiguration=deployment_configuration)
+            deploymentConfiguration=deployment_configuration,
+            forceNewDeployment=force_new_deployment)
         if network_configuration:
             params['networkConfiguration'] = network_configuration
         if self.health_check_setable(params):
@@ -566,7 +567,10 @@ def main():
                                                           module.params['desired_count'],
                                                           deploymentConfiguration,
                                                           network_configuration,
+<<<<<<< 61b5adcf316bcab84432cc4f0f1c22c411c5a071
                                                           module.params['health_check_grace_period_seconds'],
+=======
+>>>>>>> Support UpdateService forceNewDeployment in ecs_service module
                                                           module.params['force_new_deployment'])
                 else:
                     try:
