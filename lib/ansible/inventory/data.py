38c89396144127263b1c57efbe055d915915372d
Brian Coca <brian.coca+git@gmail.com>
2018-07-10 11:11:42 -0400

    error on false group name

--- lib/ansible/inventory/data.py
+++ lib/ansible/inventory/data.py
@@ -161,13 +161,16 @@ class InventoryData(object):
     def add_group(self, group):
         ''' adds a group to inventory if not there already '''
 
-        if group not in self.groups:
-            g = Group(group)
-            self.groups[group] = g
-            self._groups_dict_cache = {}
-            display.debug("Added group %s to inventory" % group)
+        if group:
+            if group not in self.groups:
+                g = Group(group)
+                self.groups[group] = g
+                self._groups_dict_cache = {}
+                display.debug("Added group %s to inventory" % group)
+            else:
+                display.debug("group %s already in inventory" % group)
         else:
-            display.debug("group %s already in inventory" % group)
+            display.warning("Ignoring empty/false group name: %s" % group)
 
     def remove_group(self, group):
 
