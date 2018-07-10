0fd7e0da93843a2bcb4e6f9b2337b580f477244c
Marcos Alano <malano@a85.com.br>
2018-07-10 14:19:45 -0300

    Add some changes to code structure

--- lib/ansible/modules/notification/say.py
+++ lib/ansible/modules/notification/say.py
@@ -70,15 +70,17 @@ def main():
     voice = module.params['voice']
     possibles = ('say', 'espeak', 'espeak-ng')
 
+    if get_platform() != 'Darwin':
+        # 'say' binary available, it might be GNUstep tool which doesn't support 'voice' parameter
+        voice = None
+
     for possible in possibles:
         executable = module.get_bin_path(possible)
-        if executable and get_platform() != 'Darwin':
-            # 'say' binary available, it might be GNUstep tool which doesn't support 'voice' parameter
-            voice = None
-        elif executable:
+        if executable:
             break
-        else:
-            module.fail_json(msg='Unable to find either %s' % ', '.join(possibles))
+    else:
+        module.fail_json(msg='Unable to find either %s' % ', '.join(possibles))
+
     if module.check_mode:
         module.exit_json(msg=msg, changed=False)
 
