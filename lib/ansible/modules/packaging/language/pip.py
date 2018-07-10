47ada720a44d882595bda7026e4a04f32b778db5
Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>
2018-07-11 00:43:08 +0200

    Turn dict constructor into a literal

--- lib/ansible/modules/packaging/language/pip.py
+++ lib/ansible/modules/packaging/language/pip.py
@@ -364,7 +364,7 @@ def _get_packages(module, pip, chdir):
     '''Return results of pip command to get packages.'''
     # Try 'pip list' command first.
     command = '%s list --format=freeze' % pip
-    lang_env = dict(LANG='C', LC_ALL='C', LC_MESSAGES='C')
+    lang_env = {'LANG': 'C', 'LC_ALL': 'C', 'LC_MESSAGES': 'C'}
     rc, out, err = module.run_command(command, cwd=chdir, environ_update=lang_env)
 
     # If there was an error (pip version too old) then use 'pip freeze'.
