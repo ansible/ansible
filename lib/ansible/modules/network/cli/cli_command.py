524191ca1c2340863e3a4bd2d7b34e748a800a20
Nathaniel Case <ncase@redhat.com>
2018-07-17 17:28:47 -0400

    Create cli_command module to do direct cliconf calls

---
+++ lib/ansible/modules/network/cli/cli_command.py
@@ -0,0 +1,254 @@
+#!/usr/bin/python
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'network'}
+
+
+DOCUMENTATION = """
+---
+module: cli_command
+version_added: "2.7"
+author: "Nathaniel Case (@qalthos)"
+short_description: Run arbitrary commands on cli-based network devices
+description:
+  - Sends an arbitrary set of commands to a network device and returns the
+    results read from the device.  This module includes an argument that
+    will cause the module to wait for a specific condition before returning
+    or timing out if the condition is not met.
+notes:
+  - Tested against EOS 4.15
+options:
+  commands:
+    description:
+      - The commands to send to the remote EOS device over the
+        configured provider.  The resulting output from the command
+        is returned.  If the I(wait_for) argument is provided, the
+        module is not returned until the condition is satisfied or
+        the number of I(retries) has been exceeded.
+    required: true
+  wait_for:
+    description:
+      - Specifies what to evaluate from the output of the command
+        and what conditionals to apply.  This argument will cause
+        the task to wait for a particular conditional to be true
+        before moving forward.   If the conditional is not true
+        by the configured retries, the task fails.
+        Note - With I(wait_for) the value in C(result['stdout']) can be accessed
+        using C(result), that is to access C(result['stdout'][0]) use C(result[0]) See examples.
+    aliases: ['waitfor']
+    version_added: "2.2"
+  match:
+    description:
+      - The I(match) argument is used in conjunction with the
+        I(wait_for) argument to specify the match policy.  Valid
+        values are C(all) or C(any).  If the value is set to C(all)
+        then all conditionals in the I(wait_for) must be satisfied.  If
+        the value is set to C(any) then only one of the values must be
+        satisfied.
+    default: all
+    choices: ['any', 'all']
+    version_added: "2.2"
+  retries:
+    description:
+      - Specifies the number of retries a command should be tried
+        before it is considered failed.  The command is run on the
+        target device every retry and evaluated against the I(wait_for)
+        conditionals.
+    default: 10
+  interval:
+    description:
+      - Configures the interval in seconds to wait between retries
+        of the command.  If the command does not pass the specified
+        conditional, the interval indicates how to long to wait before
+        trying the command again.
+    default: 1
+"""
+
+EXAMPLES = """
+- name: run show version on remote devices
+  cli_command:
+    commands: show version
+
+- name: run show version and check to see if output contains Arista
+  cli_command:
+    commands: show version
+    wait_for: result[0] contains Arista
+
+- name: run multiple commands on remote nodes
+  cli_command:
+    commands:
+      - show version
+      - show interfaces
+
+- name: run multiple commands and evaluate the output
+  cli_command:
+    commands:
+      - show version
+      - show interfaces
+    wait_for:
+      - result[0] contains Arista
+      - result[1] contains Loopback0
+
+- name: run commands and specify the output format
+  cli_command:
+    commands:
+      - command: show version
+        output: json
+
+- name: using cli transport, check whether the switch is in maintenance mode
+  cli_command:
+    commands: show maintenance
+    wait_for: result[0] contains 'Under Maintenance'
+
+- name: using cli transport, check whether the switch is in maintenance mode using json output
+  cli_command:
+    commands: show maintenance | json
+    wait_for: result[0].units.System.state eq 'underMaintenance'
+"""
+
+RETURN = """
+stdout:
+  description: The set of responses from the commands
+  returned: always apart from low level errors (such as action plugin)
+  type: list
+  sample: ['...', '...']
+stdout_lines:
+  description: The value of stdout split into a list
+  returned: always apart from low level errors (such as action plugin)
+  type: list
+  sample: [['...', '...'], ['...'], ['...']]
+failed_conditions:
+  description: The list of conditionals that have failed
+  returned: failed
+  type: list
+  sample: ['...', '...']
+"""
+import time
+
+from ansible.module_utils._text import to_native
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.connection import Connection
+from ansible.module_utils.network.common.parsing import Conditional
+from ansible.module_utils.network.common.utils import ComplexList
+
+VALID_KEYS = ['command', 'output', 'prompt', 'response']
+
+
+def to_lines(output):
+    lines = []
+    for item in output:
+        if isinstance(item, string_types):
+            item = to_text(item).split('\n')
+        lines.append(item)
+    return lines
+
+
+def parse_commands(module, warnings):
+    spec = dict(
+        command=dict(key=True),
+        output=dict(),
+        prompt=dict(),
+        answer=dict()
+    )
+
+    transform = ComplexList(spec, module)
+    commands = transform(module.params['commands'])
+
+    if module.check_mode:
+        for item in list(commands):
+            if not item['command'].startswith('show'):
+                warnings.append(
+                    'Only show commands are supported when using check_mode, not '
+                    'executing %s' % item['command']
+                )
+                commands.remove(item)
+
+    return commands
+
+
+def main():
+    """entry point for module execution
+    """
+    argument_spec = dict(
+        commands=dict(type='list', required=True),
+
+        wait_for=dict(type='list', aliases=['waitfor']),
+        match=dict(default='all', choices=['all', 'any']),
+
+        retries=dict(default=10, type='int'),
+        interval=dict(default=1, type='int')
+    )
+    module = AnsibleModule(argument_spec=argument_spec,
+                           supports_check_mode=True)
+
+    result = {'changed': False}
+
+    warnings = list()
+    # check_args(module, warnings)
+    commands = parse_commands(module, warnings)
+    if warnings:
+        result['warnings'] = warnings
+
+    wait_for = module.params['wait_for'] or list()
+
+    try:
+        conditionals = [Conditional(c) for c in wait_for]
+    except AttributeError as exc:
+        module.fail_json(msg=to_native(exc))
+
+    retries = module.params['retries']
+    interval = module.params['interval']
+    match = module.params['match']
+
+    connection = Connection(module._socket_path)
+    while retries > 0:
+        responses = []
+        for command in commands:
+            responses.append(connection.get(**command))
+
+        for item in list(conditionals):
+            if item(responses):
+                if match == 'any':
+                    conditionals = list()
+                    break
+                conditionals.remove(item)
+
+        if not conditionals:
+            break
+
+        time.sleep(interval)
+        retries -= 1
+
+    if conditionals:
+        failed_conditions = [item.raw for item in conditionals]
+        msg = 'One or more conditional statements have not been satisfied'
+        module.fail_json(msg=msg, failed_conditions=failed_conditions)
+
+    result.update({
+        'changed': False,
+        'stdout': responses,
+        'stdout_lines': to_lines(responses)
+    })
+
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
