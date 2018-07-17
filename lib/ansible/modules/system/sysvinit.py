ca50d040b29e2caa6ae3ea2ae2f29118838b2704
uwila <uwila@runbox.com>
2018-07-17 03:33:01 +0200

    Fix 2 issues in sysvinit module (#42786)
    * Do not compare result to unset parameter in sysvinit module
    
    * Fix misformed command in sysvinit module
    
    * Small None-comparison style fix in sysvinit module
    
    (cherry picked from commit f26272a492b533ddad65f1fd4bbe8ca4c46b8bba)

--- lib/ansible/modules/system/sysvinit.py
+++ lib/ansible/modules/system/sysvinit.py
@@ -259,7 +259,7 @@ def main():
                 elif location.get('chkconfig'):
                     (rc, out, err) = module.run_command("%s --level %s %s off" % (location['chkconfig'], ''.join(runlevels), name))
     else:
-        if enabled != runlevel_status["enabled"]:
+        if enabled is not None and enabled != runlevel_status["enabled"]:
             result['changed'] = True
             result['status']['enabled']['changed'] = True
 
@@ -300,7 +300,9 @@ def main():
 
         def runme(doit):
 
-            cmd = "%s %s %s %s" % (script, doit, name, module.params['arguments'])
+            args = module.params['arguments']
+            cmd = "%s %s %s" % (script, doit, "" if args is None else args)
+
             # how to run
             if module.params['daemonize']:
                 (rc, out, err) = daemonize(cmd)
