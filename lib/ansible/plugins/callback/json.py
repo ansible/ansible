3262f0a15ebf7b0ec2a2b26c9f3dcb5a0d60cc90
David Baumann <david@tsb.at>
2018-07-18 21:10:55 +0200

    Add support to show custom_stats from non hosts and make hosts a array

--- lib/ansible/plugins/callback/json.py
+++ lib/ansible/plugins/callback/json.py
@@ -100,10 +100,20 @@ class CallbackModule(CallbackBase):
             s = stats.summarize(h)
             summary[h] = s
 
-        custom_stats = {}
+        custom_stats = {
+            'global' : {},
+            'hosts' : []
+        }
+
         if self.get_option('show_custom_stats') and stats.custom:
-            custom_stats.update(dict((self._convert_host_to_name(k), v) for k, v in stats.custom.items()))
-            custom_stats.pop('_run', None)
+
+            for k in sorted(stats.custom.keys()):
+                if k == '_run':
+                    custom_stats['global'] = stats.custom[k]
+
+                custom_stats['hosts'].append({
+                        self._convert_host_to_name(k) : stats.custom[k]
+                    })
 
         output = {
             'plays': self.results,
