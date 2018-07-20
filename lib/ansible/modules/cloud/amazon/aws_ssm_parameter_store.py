569c045361214c154c6deb8b888559d9640a78c6
Ryan Brown <sb@ryansb.com>
2018-07-20 09:32:13 -0400

    [AWS] Fix SSM Parameter Store module when description is missing (#43020)
    (cherry picked from commit 07927edfac185b50d85904cd959214082388c833)

--- lib/ansible/modules/cloud/amazon/aws_ssm_parameter_store.py
+++ lib/ansible/modules/cloud/amazon/aws_ssm_parameter_store.py
@@ -181,7 +181,7 @@ def create_update_parameter(client, module):
             if existing_parameter['Parameter']['Value'] != args['Value']:
                 (changed, response) = update_parameter(client, module, args)
 
-            if args['Description']:
+            if args.get('Description'):
                 # Description field not available from get_parameter function so get it from describe_parameters
                 describe_existing_parameter = None
                 try:
