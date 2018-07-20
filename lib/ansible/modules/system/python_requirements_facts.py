ed4c1aaee7b72ead206c2ba24af4479f98daf49b
Ryan S. Brown <sb@ryansb.com>
2018-07-20 14:35:55 -0400

    boilerplate

--- lib/ansible/modules/system/python_requirements_facts.py
+++ lib/ansible/modules/system/python_requirements_facts.py
@@ -2,6 +2,9 @@
 # Copyright (c) 2018 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+__metaclass__ = type
+from __future__ import (absolute_import, division, print_function)
+
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
     'status': ['preview'],
