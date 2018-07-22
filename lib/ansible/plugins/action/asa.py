496a60e03df923c0f171f4df62f78247fa8a73fa
Nathaniel Case <this.is@nathanielca.se>
2018-07-22 15:26:44 -0400

    Let called functions use `method` keyword (#43086)
    Hopefully we can live without `method_` instead

--- lib/ansible/plugins/action/asa.py
+++ lib/ansible/plugins/action/asa.py
@@ -27,7 +27,6 @@ from ansible import constants as C
 from ansible.plugins.action.normal import ActionModule as _ActionModule
 from ansible.module_utils.network.asa.asa import asa_provider_spec
 from ansible.module_utils.network.common.utils import load_provider
-from ansible.module_utils.connection import request_builder
 
 
 try:
