ce578aeaf0c906408a74ae277145cce71f38622c
Jakub K. Boguslaw <jboguslaw@gmail.com>
2018-07-23 23:04:51 +0200

    rebase

--- lib/ansible/modules/system/user.py
+++ lib/ansible/modules/system/user.py
@@ -1053,8 +1053,11 @@ class FreeBsdUser(User):
             cmd.append(self.comment)
 
         if self.home is not None:
-            if (info[5] != self.home and self.move_home) or (not os.path.exists(self.home) and self.createhome):
+            if (info[5] != self.home and self.move_home) or (not os.path.exists(self.home) and self.create_home):
                 cmd.append('-m')
+            if info[5] != self.home:
+                cmd.append('-d')
+                cmd.append(self.home)
             cmd.append('-d')
             cmd.append(self.home)
 
