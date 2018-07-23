8d9a98922a7fd30072504550a32b4f2ea4dd4e95
Tzur Eliyahu <tzur22@gmail.com>
2018-07-23 12:17:19 +0300

    IBM_Storage: review fixes

--- lib/ansible/modules/storage/ibm/ibm_sa_vol.py
+++ lib/ansible/modules/storage/ibm/ibm_sa_vol.py
@@ -4,18 +4,7 @@
 # Copyright (C) 2018 IBM CORPORATION
 # Author(s): Tzur Eliyahu <tzure@il.ibm.com>
 #
-# This module is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This software is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
@@ -31,8 +20,7 @@ short_description: Handle volumes on an IBM Spectrum Accelerate storage array
 version_added: "2.7"
 
 description:
-    - "This module creates or deletes volumes to be used on
-        IBM Spectrum Accelerate storage systems."
+    - "This module creates or deletes volumes to be used on IBM Spectrum Accelerate storage systems."
 
 options:
     vol:
