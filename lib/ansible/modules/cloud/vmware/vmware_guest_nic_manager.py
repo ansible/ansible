e1d8eb42f7a40d97bae488e9f82b172be2b4c889
Mohd Umar Mubeen <umar.shiats@gmail.com>
2018-07-24 15:00:25 +0530

    commit with new changes- - Module name change  -Documentation update - Boilerplate added - Example updated - Removed print statements
    Done changes 
    - Module Name Change
    - Documentation update
    - Boilerplate added
    - Example updated
    - Removed print statements

--- lib/ansible/modules/cloud/vmware/vmware_guest_nic_manager.py
+++ lib/ansible/modules/cloud/vmware/vmware_guest_nic_manager.py
@@ -1,36 +1,81 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
-                    'supported_by': ''}
+                    'supported_by': 'community'}
 
-DOCUMENTATION = '''
+DOCUMENTATION = r'''
 ---
-module: vmware_vnic
-short_description: Manage Operations on vmware vnics
+module: vmware_guest_nic_manager
+short_description: Manages virtual machines in vCenter
+description: >
+   This module can be used to configure VNICs on virtual machines,
+   manage NIC state as connect , disconnect or delete.
+version_added: '2.x'
+author:
+- Mohd Umar Mubeen (@mohdumar321) <umar.shiats@gmail.com>
+requirements:
+- python >= 2.6
+- PyVmomi
+- tools
+notes:
+    - Please make sure the user used for vmware_guest should have correct level of privileges.
+    - For example, following is the list of minimum privileges required by users to create virtual machines.
+    - "   DataStore > Allocate Space"
+    - "   Virtual Machine > Configuration > Add New Disk"
+    - "   Virtual Machine > Configuration > Add or Remove Device"
+    - "   Virtual Machine > Inventory > Create New"
+    - "   Network > Assign Network"
+    - "   Resource > Assign Virtual Machine to Resource Pool"
+    - "Module may require additional privileges as well, which may be required for gathering facts - e.g. ESXi configurations."
+    - "For additional information please visit Ansible VMware community wiki - U(https://github.com/ansible/community/wiki/VMware)."
+options:
+  nic_state:
+    description:
+    - Specify state of the virtual machine be in.
+    - 'If C(nic_state) is set to C(connect) and virtual machine exists, nic will be connected'
+    - 'If C(nic_state) is set to C(disconnect) and virtual machine exists, then the specified virtual machine nic
+      is disconnected from virtual machine.'
+    - 'If C(nic_state) is set to C(delete) and virtual machine  exists, tthen the specified virtual machine nic
+      is deleted from virtual machine.'
+    default: connect
+    choices: [ connect, disconnect , delete ]
+  name:
+    description:
+    - Name of the virtual machine to work with.
+    - This parameter is case sensitive.
+    required: yes
+  uuid:
+    description:
+    - UUID of the virtual machine to manage if known, this is VMware's unique identifier.
+    - This is required if C(name) is not supplied.
+    - If virtual machine does not exists, then this parameter is ignored.
+    - Please note that a supplied UUID will be ignored on virtual machine creation, as VMware creates the UUID internally.
+  
 '''
-
 EXAMPLES = '''
-- name: Remove Vnic
-  vmware_vnic:
-    hostname=10.x.x.x
-    username=xxxx
-    password=xxxx
-    port=443
-    name=vmname
-    uuid=vmuuid
-    nic_state=delete
-    nic_number=2
+- name: configure the NIC's
+  vmware_guest_nic_manager:
+      hostname: "{{hostname}}" 
+      username: "{{username}}"
+      validate_certs: false
+      password: "{{password}}"
+      port: "{{port}}"
+      name: "{{name}}"      
+      uuid: "{{uuid}}"
+      nic_state: "{{nic_state}}"
+      nic_number: "{{nic_number}}"
 '''
 
 import atexit
 import requests
-from tools import cli
+#from serials import tools
+#from serials.tools import cli
 from pyVmomi import vim
 from pyVim.connect import SmartConnect, Disconnect
 from tools import tasks
@@ -40,8 +85,7 @@ from ansible.module_utils.vmware import (find_obj, gather_vm_facts, get_all_objs
 
 # disable  urllib3 warnings
 if hasattr(requests.packages.urllib3, 'disable_warnings'):
-    requests.packages.urllib3.disable_warnings()
-
+    requests.packages.urllib3.disable_warnings
 
 def update_virtual_nic_state(si, vm_obj, nic_number, new_nic_state):
 
@@ -93,7 +137,8 @@ def get_args():
                               port=dict(type='int', default='443'),
                               name=dict(type='str', required=True),
                               uuid=dict(type='str', required=True),
-                              nic_state=dict(type='str'),
+                              nic_state=dict(type='str', default='connect',
+                   choices=['connect', 'disconnect' , 'delete']),
                               nic_number=dict(type='str')))
     module = AnsibleModule(argument_spec=argument_spec)
     return module
@@ -127,11 +172,14 @@ def main():
     vm_obj = get_obj(content, [vim.VirtualMachine], args.params['name'])
 
     if vm_obj:
+       
         update_virtual_nic_state(si, vm_obj, args.params['nic_number'], args.params['nic_state'])
-        print ('VM NIC {} successfully' \
+        msg = ('VM NIC {} successfully' \
               ' state changed to {}').format(args.params['nic_number'], args.params['nic_state'])
+        args.exit_json(msg=msg ,changed=True)
     else:
-        print ("VM not found")
+        msg = ("VM not found")
+        args.fail_json(msg=msg ,changed=False)
 # start
 if __name__ == "__main__":
     main()
