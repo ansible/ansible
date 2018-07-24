e84931b9c24e8a358933dd2dd51c63bf06f12cfc
Brian Coca <bcoca@users.noreply.github.com>
2018-07-24 08:54:58 -0400

    don't assume all delegation subkeys exist (#43032)
    * don't assume all delegation subkeys exist
    
    fixes #43026
    
    * add tests
    
    (cherry picked from commit cdc762dd9e09780869d26ec409061b6bd8657be9)

--- test/integration/targets/delegate_to/runme.sh
+++ test/integration/targets/delegate_to/runme.sh
@@ -6,3 +6,5 @@ ANSIBLE_SSH_ARGS='-C -o ControlMaster=auto -o ControlPersist=60s -o UserKnownHos
     ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook test_delegate_to.yml -i ../../inventory -v "$@"
 
 ansible-playbook test_loop_control.yml -v "$@"
+
+ansible-playbook delegate_and_nolog.yml -v "$@"
