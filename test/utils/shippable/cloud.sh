1bcb871cd674ce5b5ca3e763c1111e9b26deae19
Matt Clay <matt@mystile.com>
2018-07-23 20:46:22 -0700

    Update Shippable integration test groups. (#43118)
    * Update Shippable integration test groups.
    * Update integration test group aliases.
    * Rebalance AWS and Azure tests with extra group.
    * Rebalance Windows tests with another group.
    
    (cherry picked from commit 4e489d1be86a2e8eafce935d91ba4fe096c1b15e)

--- test/utils/shippable/cloud.sh
+++ test/utils/shippable/cloud.sh
@@ -5,13 +5,28 @@ set -o pipefail
 declare -a args
 IFS='/:' read -ra args <<< "$1"
 
-image="${args[1]}"
-python="${args[2]}"
-target="posix/ci/cloud/group${args[3]}/"
+cloud="${args[0]}"
+python="${args[1]}"
+group="${args[2]}"
+
+target="shippable/${cloud}/group${group}/"
 
 stage="${S:-prod}"
+
+if [ "${group}" == "1" ]; then
+    # only run smoketest tests for group1
+    changed_all_target="shippable/${cloud}/smoketest/"
+
+    if ! ansible-test integration "${changed_all_target}" --list-targets > /dev/null 2>&1; then
+        # no smoketest tests are available for this cloud
+        changed_all_target="none"
+    fi
+else
+    # smoketest tests already covered by group1
+    changed_all_target="none"
+fi
 
 # shellcheck disable=SC2086
 ansible-test integration --color -v --retry-on-error "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} ${UNSTABLE:+"$UNSTABLE"} \
     --remote-terminate always --remote-stage "${stage}" \
-    --docker "${image}" --python "${python}" --changed-all-target "${target}smoketest/"
+    --docker --python "${python}" --changed-all-target "${changed_all_target}"
