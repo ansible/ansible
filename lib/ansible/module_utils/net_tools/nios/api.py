1dc73f20a2b9277ef8ae8e52388c4ab6ba394117
Sumit Jaiswal <sjaiswal@ovpn-117-59.sin2.redhat.com>
2018-07-25 16:14:36 +0530

    resolves integration test failure

--- lib/ansible/module_utils/net_tools/nios/api.py
+++ lib/ansible/module_utils/net_tools/nios/api.py
@@ -238,7 +238,7 @@ class WapiModule(WapiBase):
                     self.create_object(ib_obj_type, proposed_object)
                 result['changed'] = True
             elif modified:
-                self.check_if_hostname_exists(obj_filter, ib_obj_ref, ib_obj_type, current_object, proposed_object)
+                self.check_if_recordname_exists(obj_filter, ib_obj_ref, ib_obj_type, current_object, proposed_object)
 
                 if (ib_obj_type in (NIOS_HOST_RECORD, NIOS_NETWORK_VIEW, NIOS_DNS_VIEW)):
                     proposed_object = self.on_update(proposed_object, ib_spec)
@@ -258,7 +258,7 @@ class WapiModule(WapiBase):
 
         return result
 
-    def check_if_hostname_exists(self, obj_filter, ib_obj_ref, ib_obj_type, current_object, proposed_object):
+    def check_if_recordname_exists(self, obj_filter, ib_obj_ref, ib_obj_type, current_object, proposed_object):
         ''' Send POST request if host record input name and retrieved ref name is same,
             but input IP and retrieved IP is different'''
 
@@ -266,13 +266,13 @@ class WapiModule(WapiBase):
             obj_host_name = obj_filter['name']
             ref_host_name = ib_obj_ref[0]['name']
             if 'ipv4addrs' in (current_object and proposed_object):
-                ref_ip_addr = current_object['ipv4addrs'][0]['ipv4addr']
-                prop_ip_addr = proposed_object['ipv4addrs'][0]['ipv4addr']
+                current_ip_addr = current_object['ipv4addrs'][0]['ipv4addr']
+                proposed_ip_addr = proposed_object['ipv4addrs'][0]['ipv4addr']
             elif 'ipv6addrs' in (current_object and proposed_object):
-                ref_ip_addr = current_object['ipv6addrs'][0]['ipv6addr']
-                prop_ip_addr = proposed_object['ipv6addrs'][0]['ipv6addr']
+                current_ip_addr = current_object['ipv6addrs'][0]['ipv6addr']
+                proposed_ip_addr = proposed_object['ipv6addrs'][0]['ipv6addr']
 
-            if obj_host_name == ref_host_name and ref_ip_addr != prop_ip_addr:
+            if obj_host_name == ref_host_name and current_ip_addr != proposed_ip_addr:
                 self.create_object(ib_obj_type, proposed_object)
 
     def issubset(self, item, objects):
