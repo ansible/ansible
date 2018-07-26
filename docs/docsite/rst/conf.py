002341d7d255ad2d8df76491bc3d8eab2801e50b
Felix Fontein <felix@fontein.de>
2018-07-26 23:11:20 +0200

    Rename extensions -> _extensions.

--- docs/docsite/rst/conf.py
+++ docs/docsite/rst/conf.py
@@ -27,7 +27,7 @@ import os
 #
 sys.path.insert(0, os.path.join('ansible', 'lib'))
 sys.path.append(os.path.abspath('_themes'))
-sys.path.append(os.path.abspath('../extensions'))
+sys.path.append(os.path.abspath(os.path.join('..', '_extensions')))
 
 VERSION = '2.6'
 AUTHOR = 'Ansible, Inc'
