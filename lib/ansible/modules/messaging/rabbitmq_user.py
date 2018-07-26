548743951a4e5790ef9cf7393fcef85b8516996e
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2018-07-25 23:29:00 -0300

    Skip empty lines when RabbitMQ permissions is empty
    The `def _exec` are returning a list of blank string but
    these lines should be ignored

--- lib/ansible/modules/messaging/rabbitmq_user.py
+++ lib/ansible/modules/messaging/rabbitmq_user.py
@@ -146,7 +146,7 @@ class RabbitMqUser(object):
             if self.node is not None:
                 cmd.extend(['-n', self.node])
             rc, out, err = self.module.run_command(cmd + args, check_rc=True)
-            return out.splitlines()
+            return out.strip().splitlines()
         return list()
 
     def get(self):
