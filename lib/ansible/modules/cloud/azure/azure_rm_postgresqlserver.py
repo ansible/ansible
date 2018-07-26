254f40f7aca51ea9b276be4096565767d72e3c28
Zim Kalinowski <zikalino@microsoft.com>
2018-07-27 04:07:07 +0800

    fixed postgresql server sample (#43299)

--- lib/ansible/modules/cloud/azure/azure_rm_postgresqlserver.py
+++ lib/ansible/modules/cloud/azure/azure_rm_postgresqlserver.py
@@ -94,9 +94,9 @@ EXAMPLES = '''
       resource_group: TestGroup
       name: testserver
       sku:
-        name: PGSQLS100
-        tier: Basic
-        capacity: 100
+        name: GP_Gen4_2
+        tier: GeneralPurpose
+        capacity: 2
       location: eastus
       storage_mb: 1024
       enforce_ssl: True
