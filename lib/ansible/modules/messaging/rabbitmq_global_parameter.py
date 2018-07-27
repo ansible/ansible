93076a946f27013a7fc0abf9d4bda8301ec1881b
jgkirschbaum <juergen.kirschbaum@gmail.com>
2018-07-27 09:11:01 +0200

    Bumped version_added

--- lib/ansible/modules/messaging/rabbitmq_global_parameter.py
+++ lib/ansible/modules/messaging/rabbitmq_global_parameter.py
@@ -18,7 +18,7 @@ DOCUMENTATION = '''
 ---
 module: rabbitmq_global_parameter
 author: "Juergen Kirschbaum (@jgkirschbaum)"
-version_added: "2.6"
+version_added: "2.7"
 
 short_description: Adds or removes global parameters to RabbitMQ
 description:
