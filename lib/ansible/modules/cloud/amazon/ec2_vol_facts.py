e0a8eea2b634324d4e81da4ebc54e191b62675e9
Roman Belyakovsky <ihryamzik@gmail.com>
2018-07-27 13:04:53 +0300

    vol_facts: formatting fixes

--- lib/ansible/modules/cloud/amazon/ec2_vol_facts.py
+++ lib/ansible/modules/cloud/amazon/ec2_vol_facts.py
@@ -66,8 +66,8 @@ except ImportError:
     pass  # caught by imported HAS_BOTO3
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.ec2 import connect_to_aws, ec2_argument_spec, get_aws_connection_info, boto3_conn, HAS_BOTO3, boto3_tag_list_to_ansible_dict,
-    ansible_dict_to_boto3_filter_list, camel_dict_to_snake_dict
+from ansible.module_utils.ec2 import connect_to_aws, ec2_argument_spec, get_aws_connection_info, boto3_conn, HAS_BOTO3, boto3_tag_list_to_ansible_dict
+from ansible.module_utils.ec2 import ansible_dict_to_boto3_filter_list, camel_dict_to_snake_dict
 from ansible.module_utils._text import to_native
 
 
