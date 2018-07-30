c4f627bc2f04de3861d3421123fe375be11749a4
Trishna Guha <trishnaguha17@gmail.com>
2018-07-30 19:58:12 +0530

    address review comment
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/modules/network/nxos/nxos_file_copy.py
+++ lib/ansible/modules/network/nxos/nxos_file_copy.py
@@ -182,8 +182,12 @@ def md5sum_check(module, dst, file_system):
     remote_filehash = to_bytes(remote_filehash, errors='surrogate_or_strict')
 
     local_file = module.params['local_file']
-    with open(local_file, 'r') as f:
-        filecontent = f.read()
+    try:
+        with open(local_file, 'r') as f:
+            filecontent = f.read()
+    except (OSError, IOError) as exc:
+        module.fail_json(msg="Error reading the file: %s" % to_text(exc))
+
     filecontent = to_bytes(filecontent, errors='surrogate_or_strict')
     local_filehash = hashlib.md5(filecontent).hexdigest()
 
