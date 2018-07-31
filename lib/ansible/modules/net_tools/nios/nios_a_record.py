c344a948461b9e3dfffe375b5e78a654db28474e
curry9999 <mi_ra_cl_e@hotmail.com>
2018-07-31 14:13:32 +0900

    version_added 2.6 -> 2.7 (#43317)

--- lib/ansible/modules/net_tools/nios/nios_a_record.py
+++ lib/ansible/modules/net_tools/nios/nios_a_record.py
@@ -13,7 +13,7 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = '''
 ---
 module: nios_a_record
-version_added: "2.6"
+version_added: "2.7"
 author: "Blair Rampling (@brampling)"
 short_description: Configure Infoblox NIOS A records
 description:
