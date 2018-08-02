e152602b7ac04a8bd42856e53dd11331b4e204b4
Pilou <pierre-louis@libregerbil.fr>
2018-08-02 17:17:39 +0200

    Tower modules: move HAS_TOWER_CLI check in module_utils and minor improvements (#39809)
    * tower_* modules: move HAS_TOWER_CLI in TowerModule
    
    Besides this change allows to define other common parameters such as
    mutually_exclusive.
    
    * tower_*: config file can not be used with auth params
    
    * tower module_utils: remove useless call to expanduser
    
    'path' type: expanduser & expandvars are automatically called

--- lib/ansible/modules/web_infrastructure/ansible_tower/tower_project.py
+++ lib/ansible/modules/web_infrastructure/ansible_tower/tower_project.py
@@ -84,7 +84,7 @@ EXAMPLES = '''
     tower_config_file: "~/tower_cli.cfg"
 '''
 
-from ansible.module_utils.ansible_tower import tower_argument_spec, tower_auth_config, tower_check_mode, HAS_TOWER_CLI
+from ansible.module_utils.ansible_tower import TowerModule, tower_auth_config, tower_check_mode
 
 try:
     import tower_cli
@@ -96,8 +96,7 @@ except ImportError:
 
 
 def main():
-    argument_spec = tower_argument_spec()
-    argument_spec.update(dict(
+    argument_spec = dict(
         name=dict(),
         description=dict(),
         organization=dict(),
@@ -111,12 +110,9 @@ def main():
         local_path=dict(),
 
         state=dict(choices=['present', 'absent'], default='present'),
-    ))
+    )
 
-    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
-
-    if not HAS_TOWER_CLI:
-        module.fail_json(msg='ansible-tower-cli required for this module')
+    module = TowerModule(argument_spec=argument_spec, supports_check_mode=True)
 
     name = module.params.get('name')
     description = module.params.get('description')
@@ -172,6 +168,5 @@ def main():
     module.exit_json(**json_output)
 
 
-from ansible.module_utils.basic import AnsibleModule
 if __name__ == '__main__':
     main()
