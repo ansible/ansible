eec605fb96ffb2dc5cb2815ed29e5b38acd119ba
Jordan Borean <jborean93@gmail.com>
2018-08-02 17:19:15 +1000

    extra text to make clearer when name is required

--- lib/ansible/modules/cloud/misc/virt.py
+++ lib/ansible/modules/cloud/misc/virt.py
@@ -25,6 +25,7 @@ options:
     description:
       - name of the guest VM being managed. Note that VM must be previously
         defined with xml.
+      - This option is required unless I(command) is C(list_vms).
   state:
     description:
       - Note that there may be some lag for state requests like C(shutdown)
