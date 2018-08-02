556aff41accb121d53f788625324b0cc66e40c8f
Petr Ruzicka <petr.ruzicka@gmail.com>
2018-08-02 09:37:14 +0200

    Removing required from documentaion of name parameter (#43530)
    * Removing required from documentaion of name parameter
    
    * extra text to make clearer when name is required

--- lib/ansible/modules/cloud/misc/virt.py
+++ lib/ansible/modules/cloud/misc/virt.py
@@ -25,7 +25,7 @@ options:
     description:
       - name of the guest VM being managed. Note that VM must be previously
         defined with xml.
-    required: true
+      - This option is required unless I(command) is C(list_vms).
   state:
     description:
       - Note that there may be some lag for state requests like C(shutdown)
