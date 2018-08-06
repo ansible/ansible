4b44bdaa76dd563451b91c7643bfeeb2d6e89e4a
Igor-Lunov <igor_lunov@epam.com>
2018-08-06 16:12:56 +0300

    Changed open socket in wait_for module with context manager

--- lib/ansible/modules/utilities/logic/wait_for.py
+++ lib/ansible/modules/utilities/logic/wait_for.py
@@ -316,28 +316,28 @@ class LinuxTCPConnectionInfo(TCPConnectionInfo):
         for family in self.source_file.keys():
             socket = self.source_file[family]
             if os.path.exists(socket):
-                f = open(socket)
-                for tcp_connection in f.readlines():
-                    tcp_connection = tcp_connection.strip().split()
-                    if tcp_connection[self.local_address_field] == 'local_address':
-                        continue
-                    if (tcp_connection[self.connection_state_field] not in
-                            [get_connection_state_id(_connection_state) for _connection_state in self.module.params['active_connection_states']]):
-                        continue
-                    (local_ip, local_port) = tcp_connection[self.local_address_field].split(':')
-                    if self.port != local_port:
-                        continue
-                    (remote_ip, remote_port) = tcp_connection[self.remote_address_field].split(':')
-                    if (family, remote_ip) in self.exclude_ips:
-                        continue
-                    if any((
-                        (family, local_ip) in self.ips,
-                        (family, self.match_all_ips[family]) in self.ips,
-                        local_ip.startswith(self.ipv4_mapped_ipv6_address['prefix']) and
-                            (family, self.ipv4_mapped_ipv6_address['match_all']) in self.ips,
-                    )):
-                        active_connections += 1
-                f.close()
+                with open(socket) as f:
+                    for tcp_connection in f.readlines():
+                        tcp_connection = tcp_connection.strip().split()
+                        if tcp_connection[self.local_address_field] == 'local_address':
+                            continue
+                        if (tcp_connection[self.connection_state_field] not in
+                                [get_connection_state_id(_connection_state) for _connection_state in self.module.params['active_connection_states']]):
+                            continue
+                        (local_ip, local_port) = tcp_connection[self.local_address_field].split(':')
+                        if self.port != local_port:
+                            continue
+                        (remote_ip, remote_port) = tcp_connection[self.remote_address_field].split(':')
+                        if (family, remote_ip) in self.exclude_ips:
+                            continue
+                        if any((
+                            (family, local_ip) in self.ips,
+                            (family, self.match_all_ips[family]) in self.ips,
+                            local_ip.startswith(self.ipv4_mapped_ipv6_address['prefix']) and
+                                (family, self.ipv4_mapped_ipv6_address['match_all']) in self.ips,
+                        )):
+                            active_connections += 1
+                    f.close()
         return active_connections
 
 
