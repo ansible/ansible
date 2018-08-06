b300df305bf98e5fbce2e5dd9453efbfd1f60205
Adam Howard <ahoward0920@gmail.com>
2018-08-06 07:40:59 -0400

    Fixing complaints from pylint

--- lib/ansible/plugins/lookup/sql.py
+++ lib/ansible/plugins/lookup/sql.py
@@ -1,6 +1,6 @@
 # (c) 2018, Adam Howard <ahoward0920@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-from __future__ import print_function
+from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 DOCUMENTATION = """
@@ -25,7 +25,7 @@ DOCUMENTATION = """
 EXAMPLES = """
     - name: Get all widgets from a local Postgres database
       debug: msg="{{ lookup('sql', 'postgres://user:pass@localhost/testdb' 'SELECT * FROM widgets') }}"
-    
+
     - name: Get all widgets from a SQLite database using a relative path
       debug: msg="{{ lookup('sql', 'sqlite:///testdb.sqlite' 'SELECT * FROM widgets') }}"
 
@@ -36,7 +36,7 @@ EXAMPLES = """
 RETURN = """
     _list:
         description:
-            - list of dictionaries, where each of which maps to a row in the 
+            - list of dictionaries, where each of which maps to a row in the
               query's results. Keys in each dictionary are the columns of the
               query.
 """
@@ -49,17 +49,18 @@ try:
 except ImportError:
     sqlalchemy = None
 
+
 class LookupModule(LookupBase):
     def run(self, terms, variables=None, **kwargs):
         if sqlalchemy is None:
             raise AnsibleError("sqlalchemy is not installed")
-        
+
         db_url, query = terms[0:2]
         try:
             engine = sqlalchemy.create_engine(db_url)
         except sqlalchemy.exc.ArgumentError as err:
             raise AnsibleError("db_url: '{0}' is not valid".format(db_url))
-        
+
         try:
             with engine.begin() as conn:
                 result = conn.execute(query)
