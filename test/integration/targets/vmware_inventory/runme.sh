e456664f3436f795a2dc4fb7f6fb853b34790def
Abhijeet Kasurde <akasurde@redhat.com>
2018-08-07 23:55:42 +0530

    Use python.py to run inventory script during test.

--- test/integration/targets/vmware_inventory/runme.sh
+++ test/integration/targets/vmware_inventory/runme.sh
@@ -8,12 +8,6 @@ set -euo pipefail
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 contrib_dir=$(pwd)/../../../../contrib/inventory
-cd "${contrib_dir}"
-
-if [ ! -f ./vmware_inventory.py ]; then
-    echo "DEBUG: Unable to find vmware_inventory.py inside ${contrib_dir}"
-    exit 1
-fi
 
 echo "DEBUG: using ${contrib_dir}"
 
@@ -24,7 +18,7 @@ export VMWARE_USERNAME="${VMWARE_USERNAME:-user}"
 export VMWARE_PASSWORD="${VMWARE_PASSWORD:-pass}"
 
 VMWARE_CONFIG=${contrib_dir}/vmware_inventory.ini
-ANSIBLE_CFG=${contrib_dir}/ansible.cfg
+
 
 trap cleanup INT TERM EXIT
 
@@ -43,19 +37,11 @@ password=${VMWARE_PASSWORD}
 validate_certs=False
 VMWARE_INI
 
-cat > "${ANSIBLE_CFG}" << ANSIBLE_CFG_INI
-[defaults]
-ANSIBLE_CFG_INI
-
 function cleanup {
     # Revert back to previous one
     if [ -f "${VMWARE_CONFIG}.bk" ]; then
         echo "DEBUG: Cleanup ${VMWARE_CONFIG}"
         mv "${VMWARE_CONFIG}.bk" "${VMWARE_CONFIG}"
-    fi
-        if [ -f "${ANSIBLE_CFG}" ]; then
-        echo "DEBUG: Cleanup ${ANSIBLE_CFG}"
-        rm "${ANSIBLE_CFG}"
     fi
 }
 
@@ -71,6 +57,6 @@ echo "Debugging new instances"
 curl "http://${vcenter_host}:5000/govc_find"
 
 # Get inventory
-ansible-playbook -i ./vmware_inventory.py "${DIR}/test_vmware_inventory.yml" --connection=local "$@"
+ansible-playbook -i ./vmware_inventory.sh "${DIR}/test_vmware_inventory.yml" --connection=local "$@"
 
 echo "DEBUG: Done"
