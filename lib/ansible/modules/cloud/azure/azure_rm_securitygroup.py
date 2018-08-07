480432e2d271012770c9dd9736bd489813176bfe
Yuwei Zhou <yuwzho@microsoft.com>
2018-08-07 16:28:01 +0800

    fix

--- lib/ansible/modules/cloud/azure/azure_rm_securitygroup.py
+++ lib/ansible/modules/cloud/azure/azure_rm_securitygroup.py
@@ -348,6 +348,7 @@ except ImportError:
 
 from ansible.module_utils.azure_rm_common import AzureRMModuleBase
 from ansible.module_utils.six import integer_types
+from ansible.module_utils._text import to_native
 
 
 def validate_rule(self, rule, rule_type=None):
@@ -388,8 +389,8 @@ def compare_rules_change(old_list, new_list, purge_list):
             changed = True
     # Compare new list and old list is the same? here only compare names
     if not changed:
-        new_names = [x['name'] for x in new_list]
-        old_names = [x['name'] for x in old_list]
+        new_names = [to_native(x['name']) for x in new_list]
+        old_names = [to_native(x['name']) for x in old_list]
         changed = (set(new_names) == set(old_names))
     return changed, new_list
 
