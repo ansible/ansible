7c926a73736851f716b20143ccedf2fdce1c92a8
Matt Martz <matt@sivel.net>
2018-08-07 16:02:13 -0500

    Deprecate with_ type loops for 2.7

--- lib/ansible/playbook/task.py
+++ lib/ansible/playbook/task.py
@@ -158,8 +158,7 @@ class Task(Base, Conditional, Taggable, Become):
             raise AnsibleError("you must specify a value when using %s" % k, obj=ds)
         new_ds['loop_with'] = loop_name
         new_ds['loop'] = v
-        # FIXME: reenable afte 2.5
-        # display.deprecated("with_ type loops are being phased out, use the 'loop' keyword instead", version="2.10")
+        display.deprecated("with_ type loops are being phased out, use the 'loop' keyword instead", version="2.11")
 
     def preprocess_data(self, ds):
         '''
