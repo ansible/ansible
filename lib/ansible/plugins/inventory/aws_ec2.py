65abf931ce888413763af021062b21d43ba3ec94
Abhijeet Kasurde <akasurde@redhat.com>
2018-08-07 23:43:51 +0530

    Misc typo fixes in function docs (#43779)
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/plugins/inventory/aws_ec2.py
+++ lib/ansible/plugins/inventory/aws_ec2.py
@@ -475,7 +475,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
             # Composed variables
             self._set_composite_vars(self.get_option('compose'), host, hostname, strict=strict)
 
-            # Complex groups based on jinaj2 conditionals, hosts that meet the conditional are added to group
+            # Complex groups based on jinja2 conditionals, hosts that meet the conditional are added to group
             self._add_host_to_composed_groups(self.get_option('groups'), host, hostname, strict=strict)
 
             # Create groups based on variable values and add the corresponding hosts to it
