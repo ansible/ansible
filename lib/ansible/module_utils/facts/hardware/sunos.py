bbc8b16be4ae00a4964d5d932644fab8275aec3b
Peter Oliver <peter.oliver@capita.co.uk>
2018-08-08 17:40:17 +0100

    Locate prtdiag even when absent from /usr/bin
    On Solaris 8 hosts, this prevents fact collection from aborting with:
    
        Argument 'args' to run_command must be list or string

--- lib/ansible/module_utils/facts/hardware/sunos.py
+++ lib/ansible/module_utils/facts/hardware/sunos.py
@@ -168,8 +168,15 @@ class SunOSHardware(Hardware):
     def get_dmi_facts(self):
         dmi_facts = {}
 
-        uname_path = self.module.get_bin_path("prtdiag")
-        rc, out, err = self.module.run_command(uname_path)
+        prtdiag_path = self.module.get_bin_path("prtdiag")
+
+        # On Solaris 8 the prtdiag wrapper is absent from /usr/sbin,
+        # but that's okay, because we know where to find the real thing:
+        if not prtdiag_path:
+             rc, platform, err = self.module.run_command('/usr/bin/uname -i')
+             prtdiag_path = '/usr/platform/' + platform.rstrip() + '/sbin/prtdiag'
+
+        rc, out, err = self.module.run_command(prtdiag_path)
         """
         rc returns 1
         """
