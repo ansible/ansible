74e4ac8d415c7712ab4a4ae01fb07860ad99bd61
akira6592 <akira6592@gmail.com>
2018-08-09 08:14:33 +0900

    Corrects module name in net_get module doc (#43816)

--- lib/ansible/modules/network/files/net_get.py
+++ lib/ansible/modules/network/files/net_get.py
@@ -61,7 +61,7 @@ EXAMPLES = """
     src: running_cfg_ios1.txt
 
 - name: copy file from ios to common location at /tmp
-  network_put:
+  net_get:
     src: running_cfg_sw1.txt
     dest : /tmp/ios1.txt
 """
