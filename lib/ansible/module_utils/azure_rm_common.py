6e15fd606d39460893b2ae9e8ce553d9a7667f62
George Wilson <georgejameswilson@googlemail.com>
2018-08-09 09:49:17 +0100

    Remove log line breaking tests

--- lib/ansible/module_utils/azure_rm_common.py
+++ lib/ansible/module_utils/azure_rm_common.py
@@ -984,7 +984,6 @@ class AzureRMModuleBase(object):
 
     @property
     def storage_models(self):
-        self.log('Getting storage models...')
         return StorageManagementClient.models("2017-10-01")
 
     @property
