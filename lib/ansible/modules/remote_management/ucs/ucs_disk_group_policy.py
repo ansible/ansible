82d647d8d450c976b0e31a383023884547b5e037
“dsoper2” <dsoper@cisco.com>
2018-08-09 12:18:55 -0500

    Fix issue with automatic config and add virtual_drive config. Integration tests added for automatic config and virtual_drive config.

--- lib/ansible/modules/remote_management/ucs/ucs_disk_group_policy.py
+++ lib/ansible/modules/remote_management/ucs/ucs_disk_group_policy.py
@@ -4,6 +4,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
@@ -87,11 +88,6 @@ options:
     - Option applies when configuration mode is automatic. Specifies whether you can use all the remaining disks in the disk group or not.
     choices: ['yes', 'no']
     default: 'no'
-  use_jbod_disks:
-    description:
-    - Option applies when configuration mode is automatic.
-    choices: ['yes', 'no']
-    default: 'no'
   manual_disks:
     description:
     - List of manually configured disks. Options are only used when you choose manual configuration_mode.
@@ -119,6 +115,34 @@ options:
           If C(absent), will verify disk slot is absent from policy.
         choices: [ present, absent ]
         default: present
+  virtual_drive:
+    description:
+    - configuraiton of virtual drive options
+    suboptions:
+      access_policy:
+        description: Configure access policy to virtual drive
+        choices: [blocked, hidden, platform-default, read-only, read-write, transport-ready]
+        default: platform-default
+      drive_cache:
+        description: Configure drive caching
+        choices: [disable, enable, no-change, platform-default]
+        default: platform-default
+      io_policy:
+        description: Direct or Cached IO path
+        choices: [cached, direct, platform-default]
+        default: platform-default
+      read_policy:
+        description: Read access policy to virtual drive
+        choices: [normal, platform-default, read-ahead]
+        default: platform-default
+      strip_size:
+        description: Virtual drive strip size
+        choices: [ present, absent ]
+        default: platform-default
+      write_cache_policy:
+        description: Write back cache policy
+        choices: [always-write-back, platform-default, write-back-good-bbu, write-through]
+        default: platform-default
   org_dn:
     description:
     - The distinguished name (dn) of the organization where the resource is assigned.
@@ -129,6 +153,7 @@ author:
 - Sindhu Sudhir (@sisudhir)
 - David Soper (@dsoper2)
 - CiscoUcs (@CiscoUcs)
+- Brett Johnson (@sdbrett)
 version_added: '2.7'
 '''
 
@@ -170,6 +195,10 @@ EXAMPLES = r'''
     - slot_num: '2'
       role: normal
       state: absent
+    virtual_drive:
+      access_policy: platform-default
+      io_policy: direct
+      strip_size: 64KB
 '''
 
 RETURN = r'''
@@ -195,33 +224,36 @@ def configure_disk_policy(ucs, module, dn):
                 raid_level=module.params['raid_level'],
             )
             if module.params['configuration_mode'] == 'automatic':
-                mo_1 = LstorageDiskGroupQualifier(
+                LstorageDiskGroupQualifier(
                     parent_mo_or_dn=mo,
                     num_drives=module.params['num_drives'],
                     drive_type=module.params['drive_type'],
-                    use_jbod_disks=module.params['use_jbod_disks'],
                     use_remaining_disks=module.params['use_remaining_disks'],
                     num_ded_hot_spares=module.params['num_ded_hot_spares'],
                     num_glob_hot_spares=module.params['num_glob_hot_spares'],
                     min_drive_size=module.params['min_drive_size'],
                 )
-            else:   # configuration_mode == 'manual'
+            else:  # configuration_mode == 'manual'
                 for disk in module.params['manual_disks']:
                     if disk['state'] == 'absent':
                         child_dn = dn + '/slot-' + disk['slot_num']
                         mo_1 = ucs.login_handle.query_dn(child_dn)
                         if mo_1:
                             ucs.login_handle.remove_mo(mo_1)
-                    else:   # state == 'present'
-                        mo_1 = LstorageLocalDiskConfigRef(
+                    else:  # state == 'present'
+                        LstorageLocalDiskConfigRef(
                             parent_mo_or_dn=mo,
                             slot_num=disk['slot_num'],
                             role=disk['role'],
                             span_id=disk['span_id'],
                         )
+
+            if module.params['virtual_drive']:
+                _configure_virtual_drive(module, mo)
+
             ucs.login_handle.add_mo(mo, True)
             ucs.login_handle.commit()
-        except Exception as e:   # generic Exception handling because SDK can throw a variety
+        except Exception as e:  # generic Exception handling because SDK can throw a variety
             ucs.result['msg'] = "setup error: %s " % str(e)
             module.fail_json(**ucs.result)
 
@@ -229,7 +261,7 @@ def configure_disk_policy(ucs, module, dn):
 
 
 def check_disk_policy_props(ucs, module, mo, dn):
-    props_match = False
+    props_match = True
 
     # check top-level mo props
     kwargs = dict(descr=module.params['description'])
@@ -242,29 +274,63 @@ def check_disk_policy_props(ucs, module, mo, dn):
             if mo_1:
                 kwargs = dict(num_drives=module.params['num_drives'])
                 kwargs['drive_type'] = module.params['drive_type']
-                kwargs['use_jbod_disks'] = module.params['use_jbod_disks']
                 kwargs['use_remaining_disks'] = module.params['use_remaining_disks']
                 kwargs['num_ded_hot_spares'] = module.params['num_ded_hot_spares']
                 kwargs['num_glob_hot_spares'] = module.params['num_glob_hot_spares']
                 kwargs['min_drive_size'] = module.params['min_drive_size']
-                if mo_1.check_prop_match(**kwargs):
-                    props_match = True
-        else:   # configuration_mode == 'manual'
+                props_match = mo_1.check_prop_match(**kwargs)
+
+        else:  # configuration_mode == 'manual'
             for disk in module.params['manual_disks']:
                 child_dn = dn + '/slot-' + disk['slot_num']
                 mo_1 = ucs.login_handle.query_dn(child_dn)
                 if mo_1:
                     if disk['state'] == 'absent':
                         props_match = False
-                    else:   # state == 'present'
+                    else:  # state == 'present'
                         kwargs = dict(slot_num=disk['slot_num'])
                         kwargs['role'] = disk['role']
                         kwargs['span_id'] = disk['span_id']
-                        if mo_1.check_prop_match(**kwargs):
-                            props_match = True
-                if not props_match:
-                    break
+                        if not mo_1.check_prop_match(**kwargs):
+                            props_match = False
+                            break
+        if props_match:
+            if module.params['virtual_drive']:
+                props_match = check_virtual_drive_props(ucs, module, dn)
+    else:
+        props_match = False
     return props_match
+
+
+def check_virtual_drive_props(ucs, module, dn):
+    child_dn = dn + '/virtual-drive-def'
+    mo_1 = ucs.login_handle.query_dn(child_dn)
+    return mo_1.check_prop_match(**module.params['virtual_drive'])
+
+
+def _configure_virtual_drive(module, mo):
+    from ucsmsdk.mometa.lstorage.LstorageVirtualDriveDef import LstorageVirtualDriveDef
+    LstorageVirtualDriveDef(parent_mo_or_dn=mo, **module.params['virtual_drive'])
+
+
+def _virtual_drive_argument_spec():
+    return dict(
+        access_policy=dict(type=str, default='platform-default',
+                           choices=["blocked", "hidden", "platform-default", "read-only", "read-write",
+                                    "transport-ready"]),
+        drive_cache=dict(type=str, default='platform-default',
+                         choices=["disable", "enable", "no-change", "platform-default"]),
+        io_policy=dict(type=str, default='platform-default',
+                       choices=["cached", "direct", "platform-default"]),
+        read_policy=dict(type=str, default='platform-default',
+                         choices=["normal", "platform-default", "read-ahead"]),
+        strip_size=dict(type=str, default='platform-default',
+                        choices=["1024KB", "128KB", "16KB", "256KB", "32KB", "512KB", "64KB", "8KB",
+                                 "platform-default"]),
+        write_cache_policy=dict(type=str, default='platform-default',
+                                choices=["always-write-back", "platform-default", "write-back-good-bbu",
+                                         "write-through"]),
+    )
 
 
 def main():
@@ -274,6 +340,7 @@ def main():
         span_id=dict(type='str', default='unspecified'),
         state=dict(type='str', default='present', choices=['present', 'absent']),
     )
+
     argument_spec = ucs_argument_spec
     argument_spec.update(
         org_dn=dict(type='str', default='org-root'),
@@ -297,19 +364,19 @@ def main():
         num_ded_hot_spares=dict(type='str', default='unspecified'),
         num_glob_hot_spares=dict(type='str', default='unspecified'),
         drive_type=dict(type='str', default='unspecified', choices=['unspecified', 'HDD', 'SSD']),
-        use_jbod_disks=dict(type='str', default='no', choices=['yes', 'no']),
         use_remaining_disks=dict(type='str', default='no', choices=['yes', 'no']),
         min_drive_size=dict(type='str', default='unspecified'),
         manual_disks=dict(type='list', elements='dict', options=manual_disk),
         state=dict(type='str', default='present', choices=['present', 'absent']),
+        virtual_drive=dict(type='dict', elements='dict', options=_virtual_drive_argument_spec()),
     )
-
     module = AnsibleModule(
         argument_spec,
         supports_check_mode=True,
     )
     ucs = UCSModule(module)
-    # UCSModule creation above verifies ucsmsdk is present and exits on failure.  Additional imports are done below or in called functions.
+    # UCSModule creation above verifies ucsmsdk is present and exits on failure.
+    # Additional imports are done below or in called functions.
 
     ucs.result['changed'] = False
     props_match = False
@@ -324,7 +391,7 @@ def main():
                 ucs.login_handle.remove_mo(mo)
                 ucs.login_handle.commit()
             ucs.result['changed'] = True
-        else:   # state == 'present'
+        else:  # state == 'present'
             props_match = check_disk_policy_props(ucs, module, mo, dn)
 
     if module.params['state'] == 'present' and not props_match:
