af3848791529eede45d3342a7f191616ac8300dc
Clement Trebuchet <clement.trebuchet@gmail.com>
2018-08-09 12:38:08 +0200

    FIX ISSUE:#43878 when the vlans string have a final comma.

--- lib/ansible/modules/network/ios/ios_l2_interface.py
+++ lib/ansible/modules/network/ios/ios_l2_interface.py
@@ -303,11 +303,12 @@ def vlan_range_to_list(vlans):
         for part in vlans.split(','):
             if part.lower() == 'none':
                 break
-            if '-' in part:
-                start, stop = (int(i) for i in part.split('-'))
-                result.extend(range(start, stop + 1))
-            else:
-                result.append(int(part))
+            if part:
+                if '-' in part:
+                    start, stop = (int(i) for i in part.split('-'))
+                    result.extend(range(start, stop + 1))
+                else:
+                    result.append(int(part))
     return sorted(result)
 
 
