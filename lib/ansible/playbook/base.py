33452aeb6664a4308872839d13bed8438c459c93
James Tanner <tanner.jc@gmail.com>
2018-08-08 22:20:12 -0400

    Initial implementation of ignore_connection_failures

--- lib/ansible/playbook/base.py
+++ lib/ansible/playbook/base.py
@@ -581,6 +581,7 @@ class Base(FieldAttributeBase):
     _no_log = FieldAttribute(isa='bool')
     _run_once = FieldAttribute(isa='bool')
     _ignore_errors = FieldAttribute(isa='bool')
+    _ignore_connection_failures = FieldAttribute(isa='bool')
     _check_mode = FieldAttribute(isa='bool')
     _diff = FieldAttribute(isa='bool')
     _any_errors_fatal = FieldAttribute(isa='bool')
--- lib/ansible/plugins/strategy/__init__.py
+++ lib/ansible/plugins/strategy/__init__.py
@@ -507,8 +507,10 @@ class StrategyBase:
                         self._tqm._stats.increment('changed', original_host.name)
                 self._tqm.send_callback('v2_runner_on_failed', task_result, ignore_errors=ignore_errors)
             elif task_result.is_unreachable():
-                self._tqm._unreachable_hosts[original_host.name] = True
-                iterator._play._removed_hosts.append(original_host.name)
+                ignore_connection_failures = original_task.ignore_connection_failures
+                if not ignore_connection_failures:
+                    self._tqm._unreachable_hosts[original_host.name] = True
+                    iterator._play._removed_hosts.append(original_host.name)
                 self._tqm._stats.increment('dark', original_host.name)
                 self._tqm.send_callback('v2_runner_on_unreachable', task_result)
             elif task_result.is_skipped():
