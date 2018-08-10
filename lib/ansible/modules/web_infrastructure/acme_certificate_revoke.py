326b9c7d59faa09802b40e78c697f5fae718c02b
Felix Fontein <felix@fontein.de>
2018-08-10 21:52:01 +0200

    Updating documentation.

--- lib/ansible/modules/web_infrastructure/acme_certificate_revoke.py
+++ lib/ansible/modules/web_infrastructure/acme_certificate_revoke.py
@@ -26,13 +26,11 @@ description:
       L(Let's Encrypt,https://letsencrypt.org/)."
    - "Note that exactly one of C(account_key_src), C(account_key_content),
       C(private_key_src) or C(private_key_content) must be specified."
-   - "Also note that in general, trying to revoke an already revoked
-      certificate will lead to an error. The module tries to detect some
-      common error messages (for example, the ones issued by
-      L(Let's Encrypt,https://letsencrypt.org/)'s
-      L(Boulder,https://github.com/letsencrypt/boulder/) software), but
-      this might stop working and probably will not work for other server
-      softwares."
+   - "Also note that trying to revoke an already revoked certificate
+      should result in an unchanged status, even if the revocation reason
+      was different than the one specified here. Also, depending on the
+      server, it can happen that some other error is returned if the
+      certificate has already been revoked."
 extends_documentation_fragment:
   - acme
 options:
