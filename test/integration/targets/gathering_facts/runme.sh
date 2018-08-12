6b997ffbf54f0df8b6f924c808fcf751cb4c56ad
Pierre-Louis Bonicoli <pierre-louis.bonicoli@libregerbil.fr>
2018-08-12 20:20:35 +0200

    check gathering if smart is on & setup was used
    When smart gathering is enabled, check that
    1. facts aren't gathered when already gathered facts are a superset of
    requested facts
    2. facts are gathered in all other cases
    
    Minimal test play:
    
        $ ANSIBLE_GATHERING=smart ansible-playbook play.yml
        $ cat play.yml
        ---
        - hosts: localhost
          gather_facts: false
          tasks:
            - setup:
                gather_subset:
                  - '!min'
        - hosts: localhost
          tasks:
            - debug:
                msg:
                  "{{ ansible_distribution_release }}"

--- test/integration/targets/gathering_facts/runme.sh
+++ test/integration/targets/gathering_facts/runme.sh
@@ -7,3 +7,12 @@ ansible-playbook test_gathering_facts.yml -i ../../inventory -v "$@"
 #ANSIBLE_CACHE_PLUGIN=base ansible-playbook test_gathering_facts.yml -i ../../inventory -v "$@"
 
 ANSIBLE_GATHERING=smart ansible-playbook test_run_once.yml -i ../../inventory -v "$@"
+
+ANSIBLE_GATHERING=smart ansible-playbook test_smart_gathering.yml -i ../../inventory -v "$@"
+
+# because facthost* are all the same host (localhost), tests are executed host by host,
+# otherwise cleaning task of a play (/etc/ansible/facts.d/uuid.fact removal) will
+# interfere with the next play
+for host in facthost1 facthost2 facthost3 facthost4 facthost5 facthost6; do
+    ANSIBLE_GATHERING=smart ansible-playbook test_smart_gathering_gather_subset.yml -i ../../inventory -l ${host} -v "$@"
+done
