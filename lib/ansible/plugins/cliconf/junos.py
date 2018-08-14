55b1d71911e24fec11da5775c7b81f5e962f5c45
Trishna Guha <trishnaguha17@gmail.com>
2018-08-14 13:01:27 +0530

    junos cliconf diff fix (#44109)
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/plugins/cliconf/junos.py
+++ lib/ansible/plugins/cliconf/junos.py
@@ -167,6 +167,11 @@ class Cliconf(CliconfBase):
         if rollback_id is not None:
             command += ' rollback %s' % int(rollback_id)
         resp = self.send_command(command)
+
+        r = resp.splitlines()
+        if len(r) == 1 and r[0] == '[edit]':
+            resp = ''
+
         return resp
 
     def get_diff(self, rollback_id=None):
