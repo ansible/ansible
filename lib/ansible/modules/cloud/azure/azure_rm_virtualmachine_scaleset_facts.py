57fcc54a601924524c79700ca57c27f6ae77d714
Zim Kalinowski <zikalino@microsoft.com>
2018-08-16 14:45:36 +0800

    adjusting curated response format, notification of raw format depreciation. (#43587)

--- lib/ansible/modules/cloud/azure/azure_rm_virtualmachine_scaleset_facts.py
+++ lib/ansible/modules/cloud/azure/azure_rm_virtualmachine_scaleset_facts.py
@@ -38,6 +38,7 @@ options:
             - If C(raw) is selected information will be returned in raw format from Azure Python SDK.
             - If C(curated) is selected the structure will be identical to input parameters of azure_rm_virtualmachine_scaleset module.
             - In Ansible 2.5 and lower facts are always returned in raw format.
+            - Please note that this option will be deprecated in 2.10 when curated format will become the only supported format.
         default: 'raw'
         choices:
             - 'curated'
@@ -70,7 +71,7 @@ EXAMPLES = '''
 '''
 
 RETURN = '''
-azure_vmss:
+vmss:
     description: List of virtual machine scale sets
     returned: always
     type: complex
@@ -344,6 +345,10 @@ class AzureRMVirtualMachineScaleSetFacts(AzureRMModuleBase):
                 }
 
                 self.results['ansible_facts']['azure_vmss'][index] = updated
+
+            # proper result format we want to support in the future
+            # dropping 'ansible_facts' and shorter name 'vmss'
+            self.results['vmss'] = self.results['ansible_facts']['azure_vmss']
 
         return self.results
 
