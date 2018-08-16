38aefef093613d9ca26ff32161ed753c00b2482a
Abhijeet Kasurde <akasurde@redhat.com>
2018-08-16 09:32:00 +0530

    ovirt: minor typo in documentation
    Fixed "attribues" to "attributes"
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/cloud/ovirt/ovirt_affinity_label_facts.py
+++ lib/ansible/modules/cloud/ovirt/ovirt_affinity_label_facts.py
@@ -83,7 +83,7 @@ EXAMPLES = '''
 
 RETURN = '''
 ovirt_affinity_labels:
-    description: "List of dictionaries describing the affinity labels. Affinity labels attribues are mapped to dictionary keys,
+    description: "List of dictionaries describing the affinity labels. Affinity labels attributes are mapped to dictionary keys,
                   all affinity labels attributes can be found at following url: http://ovirt.github.io/ovirt-engine-api-model/master/#types/affinity_label."
     returned: On success.
     type: list
