b2b78f427e6a6ac2582fbabd0ba73a43067c20d4
Trishna Guha <trishnaguha17@gmail.com>
2018-08-16 14:30:47 +0530

    check_mode in nxos_static_route module (#44252)
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>
    (cherry picked from commit 7e39c5bf079d17b74a7ebec3a0a653a75b702684)

--- lib/ansible/modules/network/nxos/nxos_static_route.py
+++ lib/ansible/modules/network/nxos/nxos_static_route.py
@@ -280,13 +280,11 @@ def main():
         candidate = CustomNetworkConfig(indent=3)
         reconcile_candidate(module, candidate, prefix, w)
 
-        if candidate:
+        if not module.check_mode and candidate:
             candidate = candidate.items_text()
             load_config(module, candidate)
             result['commands'].extend(candidate)
             result['changed'] = True
-        else:
-            result['commands'] = []
 
     module.exit_json(**result)
 
