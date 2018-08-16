94551c508065c2e306805b5917a04c19227f04ee
Court <campbco@gmail.com>
2018-08-16 07:10:56 -0500

    Added GPLv3 license header

--- lib/ansible/modules/system/vg_exim.py
+++ lib/ansible/modules/system/vg_exim.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
     'status': ['preview'],
