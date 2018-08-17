b629ccfb69318758b6bbce458b29a44aee824e62
zikalino <zikalino@microsoft.com>
2018-08-17 10:11:34 +0800

    removed ansible_facts

--- lib/ansible/modules/cloud/azure/azure_rm_mysqldatabase_facts.py
+++ lib/ansible/modules/cloud/azure/azure_rm_mysqldatabase_facts.py
@@ -130,8 +130,7 @@ class AzureRMDatabasesFacts(AzureRMModuleBase):
         )
         # store the results of the module operation
         self.results = dict(
-            changed=False,
-            ansible_facts=dict()
+            changed=False
         )
         self.resource_group = None
         self.server_name = None
