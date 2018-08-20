f2d0d790e162749f096297ca8c5e2efa9119325c
linnil1 <linnil1.886@gmail.com>
2018-08-20 17:20:55 +0800

    Fix bug of interface-mode in Junos

--- lib/ansible/modules/network/junos/junos_l2_interface.py
+++ lib/ansible/modules/network/junos/junos_l2_interface.py
@@ -48,6 +48,12 @@ options:
     description:
       - Native VLAN to be configured in trunk port. The value of C(native_vlan)
         should be vlan id.
+  enhenced_layer:
+    description:
+      - True if your device has Enhanced Layer 2 Software (ELS).
+    default: True
+    type: bool
+    version_added: "2.7"
   unit:
     description:
       - Logical interface number. Value of C(unit) should be of type
@@ -172,6 +178,7 @@ def main():
         trunk_vlans=dict(type='list'),
         unit=dict(default=0, type='int'),
         description=dict(),
+        enhenced_layer=dict(default=True, type='bool'),
         state=dict(default='present', choices=['present', 'absent']),
         active=dict(default=True, type='bool')
     )
@@ -234,6 +241,9 @@ def main():
         item = param.copy()
 
         validate_param_values(module, param_to_xpath_map, param=item)
+
+        param_to_xpath_map['mode']['xpath'] = \
+            'interface-mode' if param['enhenced_layer'] else 'port-mode'
 
         want = map_params_to_obj(module, param_to_xpath_map, param=item)
         requests.append(map_obj_to_ele(module, want, top, param=item))
