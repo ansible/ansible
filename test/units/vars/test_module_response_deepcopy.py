119d9ece9b17be4d7aac24111eb14219570a3feb
Matt Martz <matt@sivel.net>
2018-08-20 13:09:36 -0500

    Rename to module_response_deepcopy and move to vars/clean

--- test/units/vars/test_module_response_deepcopy.py
+++ test/units/vars/test_module_response_deepcopy.py
@@ -5,56 +5,56 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-from ansible.utils.naive_deepcopy import naive_deepcopy
+from ansible.vars.clean import module_response_deepcopy
 
 import pytest
 
 
-def test_naive_deepcopy_basic():
+def test_module_response_deepcopy_basic():
     x = 42
-    y = naive_deepcopy(x)
+    y = module_response_deepcopy(x)
     assert y == x
 
 
-def test_naive_deepcopy_atomic():
+def test_module_response_deepcopy_atomic():
     tests = [None, 42, 2**100, 3.14, True, False, 1j,
              "hello", "hello\u1234"]
     for x in tests:
-        assert naive_deepcopy(x) is x
+        assert module_response_deepcopy(x) is x
 
 
-def test_naive_deepcopy_list():
+def test_module_response_deepcopy_list():
     x = [[1, 2], 3]
-    y = naive_deepcopy(x)
+    y = module_response_deepcopy(x)
     assert y == x
     assert x is not y
     assert x[0] is not y[0]
 
 
-def test_naive_deepcopy_empty_tuple():
+def test_module_response_deepcopy_empty_tuple():
     x = ()
-    y = naive_deepcopy(x)
+    y = module_response_deepcopy(x)
     assert x is y
 
 
 @pytest.mark.skip(reason='No current support for this situation')
-def test_naive_deepcopy_tuple():
+def test_module_response_deepcopy_tuple():
     x = ([1, 2], 3)
-    y = naive_deepcopy(x)
+    y = module_response_deepcopy(x)
     assert y == x
     assert x is not y
     assert x[0] is not y[0]
 
 
-def test_naive_deepcopy_tuple_of_immutables():
+def test_module_response_deepcopy_tuple_of_immutables():
     x = ((1, 2), 3)
-    y = naive_deepcopy(x)
+    y = module_response_deepcopy(x)
     assert x is y
 
 
-def test_naive_deepcopy_dict():
+def test_module_response_deepcopy_dict():
     x = {"foo": [1, 2], "bar": 3}
-    y = naive_deepcopy(x)
+    y = module_response_deepcopy(x)
     assert y == x
     assert x is not y
     assert x["foo"] is not y["foo"]
