9a9c85358e27c3f120c6825b61216c5af8184556
Ken Partridge <kenp@wti.com>
2018-08-20 15:56:54 -0700

    moved redundant ansible.module_utils._text import of to_native to previous ansible.module_utils._text import line

--- lib/ansible/plugins/lookup/cpm_status.py
+++ lib/ansible/plugins/lookup/cpm_status.py
@@ -111,9 +111,8 @@ import base64
 
 from ansible.errors import AnsibleError
 from ansible.plugins.lookup import LookupBase
-from ansible.module_utils._text import to_text, to_bytes
+from ansible.module_utils._text import to_text, to_bytes, to_native
 from ansible.module_utils.six.moves.urllib.error import HTTPError, URLError
-from ansible.module_utils._text import to_text, to_native
 from ansible.module_utils.urls import open_url, ConnectionError, SSLValidationError
 
 
