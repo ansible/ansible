e8cc68a1e047dd4a56c88340bdcadc1a35f231eb
Matt Martz <matt@sivel.net>
2018-08-21 14:57:56 -0500

    Catch call to AnsibleModule.deprecate also

--- test/sanity/pylint/plugins/deprecated.py
+++ test/sanity/pylint/plugins/deprecated.py
@@ -13,7 +13,8 @@ from pylint.checkers.utils import check_messages
 from ansible.release import __version__ as ansible_version_raw
 
 MSGS = {
-    'E9501': ("Deprecated version (%r) found in call to Display.deprecated",
+    'E9501': ("Deprecated version (%r) found in call to Display.deprecated "
+              "or AnsibleModule.deprecate",
               "ansible-deprecated-version",
               "Used when a call to Display.deprecated specifies a version "
               "less than or equal to the current version of Ansible",
@@ -53,7 +54,8 @@ class AnsibleDeprecatedChecker(BaseChecker):
     def visit_call(self, node):
         version = None
         try:
-            if node.func.attrname == 'deprecated' and 'display' in _get_expr_name(node):
+            if (node.func.attrname == 'deprecated' and 'display' in _get_expr_name(node) or
+                    node.func.attrname == 'deprecate' and 'module' in _get_expr_name(node)):
                 if node.keywords:
                     for keyword in node.keywords:
                         if keyword.arg == 'version':
@@ -65,8 +67,12 @@ class AnsibleDeprecatedChecker(BaseChecker):
                         self.add_message('ansible-deprecated-no-version', node=node)
                         return
 
-                if ANSIBLE_VERSION >= StrictVersion(str(version)):
-                    self.add_message('ansible-deprecated-version', node=node, args=(version,))
+                try:
+                    if ANSIBLE_VERSION >= StrictVersion(str(version)):
+                        self.add_message('ansible-deprecated-version', node=node, args=(version,))
+                except ValueError:
+                    # TODO: Warn here?
+                    pass
         except AttributeError:
             pass
 
