9d4c9b95d7ea9ea3219a87104deae0f561d815e6
Steve Dodd <stevend@idahood.com>
2018-08-21 13:30:17 -0600

    Fix PEP8 issues

--- lib/ansible/modules/commands/shell.py
+++ lib/ansible/modules/commands/shell.py
@@ -119,10 +119,10 @@ EXAMPLES = '''
   args:
     executable: /usr/bin/expect
   delegate_to: localhost
-  
+
 # Disabling warnings
 - name: Using curl to connect to a host via SOCKS proxy (unsupported in uri). Ordinarily this would throw a warning.
-  shell: curl --socks5: localhost:9000 http://www.ansible.com
+  shell: curl --socks5 localhost:9000 http://www.ansible.com
   args:
     warn: False
 '''
