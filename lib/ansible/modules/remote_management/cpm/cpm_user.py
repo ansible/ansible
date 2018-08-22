3e5c7a96249533df8ef6e329538a2a779de94ca6
Ken Partridge <kenp@wti.com>
2018-08-22 10:37:48 -0700

    remove reference to ansible.errors

--- lib/ansible/modules/remote_management/cpm/cpm_user.py
+++ lib/ansible/modules/remote_management/cpm/cpm_user.py
@@ -208,7 +208,6 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils._text import to_text, to_bytes, to_native
 from ansible.module_utils.six.moves.urllib.error import HTTPError, URLError
 from ansible.module_utils.urls import open_url, ConnectionError, SSLValidationError
-from ansible.errors import AnsibleError
 
 
 def assemble_json(cpmmodule):
