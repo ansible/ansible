319b604336c971f50c1c6def7dbfe344776d66dc
Matt Martz <matt@sivel.net>
2018-08-23 13:22:54 -0500

    Bump sudo/su deprecation to 2.9 (#44581)

--- lib/ansible/playbook/become.py
+++ lib/ansible/playbook/become.py
@@ -78,7 +78,7 @@ class Become:
                 ds['become_user'] = ds['sudo_user']
                 del ds['sudo_user']
 
-            display.deprecated("Instead of sudo/sudo_user, use become/become_user and make sure become_method is 'sudo' (default)", '2.6')
+            display.deprecated("Instead of sudo/sudo_user, use become/become_user and make sure become_method is 'sudo' (default)", '2.9')
 
         elif 'su' in ds or 'su_user' in ds:
             ds['become_method'] = 'su'
@@ -90,6 +90,6 @@ class Become:
                 ds['become_user'] = ds['su_user']
                 del ds['su_user']
 
-            display.deprecated("Instead of su/su_user, use become/become_user and set become_method to 'su' (default is sudo)", '2.6')
+            display.deprecated("Instead of su/su_user, use become/become_user and set become_method to 'su' (default is sudo)", '2.9')
 
         return ds
