653d9c0f87f681ac386864bad4cb48f0c5e2ddfe
jctanner <tanner.jc@gmail.com>
2018-08-23 11:41:02 -0400

    New keyword: ignore_unreachable (#43857)

---
+++ test/integration/targets/ignore_unreachable/fake_connectors/bad_put_file.py
@@ -0,0 +1,11 @@
+import ansible.plugins.connection.local as ansible_local
+from ansible.errors import AnsibleConnectionFailure
+
+from ansible.utils.display import Display
+display = Display()
+
+
+class Connection(ansible_local.Connection):
+    def put_file(self, in_path, out_path):
+        display.debug('Intercepted call to send data')
+        raise AnsibleConnectionFailure('BADLOCAL Error: this is supposed to fail')
