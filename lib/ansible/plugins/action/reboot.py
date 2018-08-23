7bd12802883c8db9d7e6a478ce29a480be74bb88
Sam Doran <sdoran@redhat.com>
2018-08-23 10:49:13 -0400

    Add exponential backoff to sucess check method

--- lib/ansible/plugins/action/reboot.py
+++ lib/ansible/plugins/action/reboot.py
@@ -5,6 +5,7 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import random
 import time
 
 from datetime import datetime, timedelta
@@ -141,8 +142,11 @@ class ActionModule(ActionBase):
 
         return result
 
-    def do_until_success_or_timeout(self, action, reboot_timeout, before_uptime, action_desc, fail_sleep=1):
+    def do_until_success_or_timeout(self, action, reboot_timeout, before_uptime, action_desc):
         max_end_time = datetime.utcnow() + timedelta(seconds=reboot_timeout)
+
+        fail_count = 0
+        max_fail_sleep = 12
 
         while datetime.utcnow() < max_end_time:
             try:
@@ -151,8 +155,15 @@ class ActionModule(ActionBase):
                     display.debug('%s: %s success' % (self._task.action, action_desc))
                 return
             except Exception as e:
+                # Use exponential backoff with a max timout, plus a little bit of randomness
+                random_int = random.randint(0, 1000) / 1000
+                fail_sleep = 2 ** fail_count + random_int
+                if fail_sleep > max_fail_sleep:
+
+                    fail_sleep = max_fail_sleep + random_int
                 if action_desc:
-                    display.debug("%s: %s fail '%s', retrying in %d seconds..." % (self._task.action, action_desc, to_text(e), fail_sleep))
+                    display.debug("{0}: {1} fail '{2}', retrying in {3:.4} seconds...".format(self._task.action, action_desc, to_text(e), fail_sleep))
+                fail_count += 1
                 time.sleep(fail_sleep)
 
         raise TimedOutException('Timed out waiting for %s' % (action_desc))
