08a66bcb9f6c34325444bb06a0425bec84e395a2
Matt Martz <matt@sivel.net>
2018-08-24 13:09:23 -0500

    Correct CALLBACK_NAME

--- lib/ansible/plugins/callback/cgroup_perf_recap.py
+++ lib/ansible/plugins/callback/cgroup_perf_recap.py
@@ -146,7 +146,7 @@ class PidsProf(BaseProf):
 class CallbackModule(CallbackBase):
     CALLBACK_VERSION = 2.0
     CALLBACK_TYPE = 'aggregate'
-    CALLBACK_NAME = 'cgroup_memory_recap'
+    CALLBACK_NAME = 'cgroup_perf_recap'
     CALLBACK_NEEDS_WHITELIST = True
 
     def __init__(self, display=None):
