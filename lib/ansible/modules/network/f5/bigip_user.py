2573db4263d54057090e042cced67287087e796a
Tim Rupp <caphrim007@gmail.com>
2018-08-24 15:47:09 -0700

    Misc changes related to bigip modules
    Cleaning up a number of fixes realted to downstream f5 work that
    is related to ansible 2.7

--- lib/ansible/modules/network/f5/bigip_user.py
+++ lib/ansible/modules/network/f5/bigip_user.py
@@ -18,9 +18,8 @@ module: bigip_user
 short_description: Manage user accounts and user attributes on a BIG-IP
 description:
   - Manage user accounts and user attributes on a BIG-IP. Typically this
-    module operates only on the REST API users and not the CLI users. There
-    is one exception though and that is if you specify the C(username_credential)
-    of C(root). When specifying C(root), you may only change the password.
+    module operates only on the REST API users and not the CLI users.
+    When specifying C(root), you may only change the password.
     Your other parameters will be ignored in this case. Changing the C(root)
     password is not an idempotent operation. Therefore, it will change it
     every time this module attempts to change it.
@@ -31,8 +30,8 @@ options:
       - Full name of the user.
   username_credential:
     description:
-      - Name of the user to create, remove or modify. There is a special case
-        that exists for the user C(root).
+      - Name of the user to create, remove or modify.
+      - The C(root) user may not be removed.
     required: True
     aliases:
       - name
