762869ca2ff51546cf2dce66ad3e77b051cc4594
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-08-27 12:06:48 -0500

    oops, fix undefined variable

--- lib/ansible/galaxy/login.py
+++ lib/ansible/galaxy/login.py
@@ -42,7 +42,7 @@ except ImportError:
 
 
 def http_error_message(error):
-    flat_resp = e.read()
+    flat_resp = error.read()
     if isinstance(flat_resp, binary_type):
         flat_resp = flat_resp.decode()
     res = json.loads(flat_resp)
