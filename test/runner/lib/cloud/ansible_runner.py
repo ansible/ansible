ee622a5b7cb4c6c3e7c17227d0b1e24cb6cb472f
James Tanner <tanner.jc@gmail.com>
2018-08-27 18:36:28 -0400

    first commit

---
+++ test/runner/lib/cloud/ansible_runner.py
@@ -0,0 +1,202 @@
+"""Ansible-Runner plugin for integration tests."""
+
+from __future__ import absolute_import, print_function
+
+import os
+import tempfile
+
+from . import (
+    CloudProvider,
+    CloudEnvironment,
+)
+
+from ..util import (
+    find_executable,
+    display,
+)
+
+from ..docker_util import (
+    docker_exec,
+    docker_run,
+    docker_rm,
+    docker_inspect,
+    docker_pull,
+    docker_put,
+    get_docker_container_id,
+)
+
+from .. import pytar
+
+
+class AnsibleRunnerProvider(CloudProvider):
+    """Foreman plugin.
+
+    Sets up Foreman stub server for tests.
+    """
+
+    DOCKER_SIMULATOR_NAME = 'ansible_runner_test'
+
+    DOCKER_IMAGE = 'jctanner/ansible-runner'
+
+    def __init__(self, args):
+        """Set up container references for provider.
+
+        :type args: TestConfig
+        """
+        super(AnsibleRunnerProvider, self).__init__(args)
+
+        self.__container_from_env = os.getenv('ANSIBLE_RUNNER_CONTAINER')
+        self.image = self.__container_from_env or self.DOCKER_IMAGE
+        self.container_name = ''
+
+    def filter(self, targets, exclude):
+        """Filter out the tests with the necessary config and res unavailable.
+
+        :type targets: tuple[TestTarget]
+        :type exclude: list[str]
+        """
+        docker_cmd = 'docker'
+        docker = find_executable(docker_cmd, required=False)
+
+        if docker:
+            return
+
+        skip = 'cloud/%s/' % self.platform
+        skipped = [target.name for target in targets if skip in target.aliases]
+
+        if skipped:
+            exclude.append(skip)
+            display.warning(
+                'Excluding tests marked "%s" '
+                'which require the "%s" command: %s'
+                % (skip.rstrip('/'), docker_cmd, ', '.join(skipped))
+            )
+
+    def setup(self):
+        """Setup cloud resource before delegation and reg cleanup callback."""
+        super(AnsibleRunnerProvider, self).setup()
+
+        if self._use_static_config():
+            self._setup_static()
+        else:
+            self._setup_dynamic()
+
+    def get_docker_run_options(self):
+        """Get additional options needed when delegating tests to a container.
+
+        :rtype: list[str]
+        """
+        return ['--link', self.DOCKER_SIMULATOR_NAME] if self.managed else []
+
+    def cleanup(self):
+        """Clean up the resource and temporary configs files after tests."""
+        if self.container_name:
+            docker_rm(self.args, self.container_name)
+        super(AnsibleRunnerProvider, self).cleanup()
+        #pass
+
+    def _setup_dynamic(self):
+        """Spawn a Runner container."""
+        #runner_port = 8080
+        runner_port = 5000
+        container_id = get_docker_container_id()
+
+        if container_id:
+            display.info(
+                'Running in docker container: %s' % container_id,
+                verbosity=1,
+            )
+
+        self.container_name = self.DOCKER_SIMULATOR_NAME
+
+        results = docker_inspect(self.args, self.container_name)
+
+        if results and not results[0].get('State', {}).get('Running'):
+            docker_rm(self.args, self.container_name)
+            results = []
+
+        display.info(
+            '%s Ansible Runner docker container.'
+            % ('Using the existing' if results else 'Starting a new'),
+            verbosity=1,
+        )
+
+        if not results:
+            if self.args.docker or container_id:
+                publish_ports = []
+            else:
+                # publish the simulator ports when not running inside docker
+                publish_ports = [
+                    '-p', ':'.join((str(runner_port), ) * 2),
+                ]
+
+            #if not self.__container_from_env:
+            #    docker_pull(self.args, self.image)
+
+            docker_run(
+                self.args,
+                self.image,
+                ['-d', '--name', self.container_name] + publish_ports,
+            )
+
+        #if self.args.docker:
+        #    runner_host = self.DOCKER_SIMULATOR_NAME
+        #elif container_id:
+        if True:
+            runner_host = self._get_simulator_address()
+            display.info(
+                'Found Runner container address: %s'
+                % runner_host, verbosity=1
+            )
+        else:
+            runner_host = 'localhost'
+
+        self._set_cloud_config('ANSIBLE_RUNNER_HOST', runner_host)
+        self._set_cloud_config('ANSIBLE_RUNNER_PORT', str(runner_port))
+        import q; q(runner_host)
+        import q; q(runner_port)
+
+        self.delegate_checkout()
+
+    def _get_simulator_address(self):
+        results = docker_inspect(self.args, self.container_name)
+        ip_address = results[0]['NetworkSettings']['IPAddress']
+        #import epdb; epdb.st()
+        return ip_address
+
+    def _get_docker_container_id(self):
+        results = docker_inspect(self.args, self.container_name)
+        return results[0]['Id']
+
+    def _setup_static(self):
+        raise NotImplementedError
+
+    def delegate_checkout(self):
+        with tempfile.NamedTemporaryFile(prefix='ansible-source-', suffix='.tgz') as local_source_fd:
+            try:
+                tar_filter = pytar.DefaultTarFilter()
+                pytar.create_tarfile(local_source_fd.name, '.', tar_filter)
+            except Exception as e:
+                raise e
+            container_id = self._get_docker_container_id()
+            import q; q(container_id)
+            import q; q(local_source_fd)
+            import q; q(local_source_fd.name)
+            #import epdb; epdb.st()
+            docker_put(self.args, container_id, local_source_fd.name, '/root/ansible.tgz')
+            docker_exec(self.args, container_id, ['mkdir', '/root/ansible'])
+            docker_exec(self.args, container_id, ['tar', 'oxzf', '/root/ansible.tgz', '-C', '/root/ansible'])
+            docker_exec(self.args, container_id, ['pip', 'install', '/root/ansible'])
+
+
+class AnsibleRunnerEnvironment(CloudEnvironment):
+
+    def configure_environment(self, env, cmd):
+        """
+        :type env: dict[str, str]
+        :type cmd: list[str]
+        """
+
+        # Send the container IP down to the integration test(s)
+        env['ANSIBLE_RUNNER_HOST'] = self._get_cloud_config('ANSIBLE_RUNNER_HOST')
+        env['ANSIBLE_RUNNER_PORT'] = self._get_cloud_config('ANSIBLE_RUNNER_PORT')
