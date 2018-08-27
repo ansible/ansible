0dacc606b4e2e83ae29af32c466bf5980542c860
Tim Rupp <caphrim007@gmail.com>
2018-08-27 14:09:03 -0700

    Various fixes for f5 modules (#44734)
    A number of bugfixes for the remaining 2.7 work on the F5 modules.

--- lib/ansible/modules/network/f5/bigip_pool_member.py
+++ lib/ansible/modules/network/f5/bigip_pool_member.py
@@ -419,7 +419,7 @@ class ApiParameters(Parameters):
 
     @property
     def state(self):
-        if self._values['state'] in ['user-up', 'unchecked', 'fqdn-up-no-addr'] and self._values['session'] in ['user-enabled']:
+        if self._values['state'] in ['user-up', 'unchecked', 'fqdn-up-no-addr', 'fqdn-up'] and self._values['session'] in ['user-enabled']:
             return 'present'
         elif self._values['state'] in ['down', 'up'] and self._values['session'] == 'monitor-enabled':
             return 'present'
@@ -476,7 +476,7 @@ class ReportableChanges(Changes):
 
     @property
     def state(self):
-        if self._values['state'] in ['user-up', 'unchecked', 'fqdn-up-no-addr'] and self._values['session'] in ['user-enabled']:
+        if self._values['state'] in ['user-up', 'unchecked', 'fqdn-up-no-addr', 'fqdn-up'] and self._values['session'] in ['user-enabled']:
             return 'present'
         elif self._values['state'] in ['down', 'up'] and self._values['session'] == 'monitor-enabled':
             return 'present'
