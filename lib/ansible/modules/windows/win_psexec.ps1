15604a8de7c9c63e5b18befc8da8e66316193652
Xyon <admin@jbladen.co.uk>
2018-08-29 00:42:59 +0100

    Add new session parameter to win_psexec (#44263)
    * Add new session parameter to win_psexec
    
    * Indicate which version the session option was added in
    
    * Added changelog fragment and minor edit on doc entry

--- lib/ansible/modules/windows/win_psexec.ps1
+++ lib/ansible/modules/windows/win_psexec.ps1
@@ -24,6 +24,7 @@ $elevated = Get-AnsibleParam -obj $params -name "elevated" -type "bool" -default
 $limited = Get-AnsibleParam -obj $params -name "limited" -type "bool" -default $false
 $system = Get-AnsibleParam -obj $params -name "system" -type "bool" -default $false
 $interactive = Get-AnsibleParam -obj $params -name "interactive" -type "bool" -default $false
+$session = Get-AnsibleParam -obj $params -name "session" -type "int"
 $priority = Get-AnsibleParam -obj $params -name "priority" -type "str" -validateset "background","low","belownormal","abovenormal","high","realtime"
 $timeout = Get-AnsibleParam -obj $params -name "timeout" -type "int"
 $extra_opts = Get-AnsibleParam -obj $params -name "extra_opts" -type "list"
@@ -83,6 +84,9 @@ If ($system -eq $true) {
 
 If ($interactive -eq $true) {
     $arguments += "-i"
+    If ($session -ne $null) {
+        $arguments += $session
+    }
 }
 
 If ($limited -eq $true) {
