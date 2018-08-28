f61d2b1c0c9b74ea8fba8fb9dc798c23c4522b14
Dag Wieers <dag@wieers.com>
2018-08-29 00:08:26 +0200

    Backport of PR 43464
    Fix issue when timeout and state=present (#43464)

--- lib/ansible/modules/windows/win_wait_for.ps1
+++ lib/ansible/modules/windows/win_wait_for.ps1
@@ -37,7 +37,7 @@ if ($path -ne $null) {
     if ($state -in @("stopped","drained")) {
         Fail-Json $result "state=$state should only be used for checking a port in the win_wait_for module"
     }
-    
+
     if ($exclude_hosts -ne $null) {
         Fail-Json $result "exclude_hosts should only be used when checking a port and state=drained in the win_wait_for module"
     }
@@ -85,7 +85,7 @@ Function Get-PortConnections($hostname, $port) {
     } else {
         $active_connections = $conn_info.GetActiveTcpConnections() | Where-Object { $_.LocalEndPoint.Address -eq $hostname -and $_.LocalEndPoint.Port -eq $port }
     }
-    
+
     if ($active_connections -ne $null) {
         foreach ($active_connection in $active_connections) {
             $connections += $active_connection.RemoteEndPoint.Address
@@ -102,7 +102,7 @@ if ($delay -ne $null) {
 }
 
 $attempts = 0
-if ($path -eq $null -and $port -eq $null -and $state -eq "drained") {
+if ($path -eq $null -and $port -eq $null -and $state -ne "drained") {
     Start-Sleep -Seconds $timeout
 } elseif ($path -ne $null) {
     if ($state -in @("present", "started")) {
@@ -134,7 +134,7 @@ if ($path -eq $null -and $port -eq $null -and $state -eq "drained") {
                 Fail-Json $result "timeout while waiting for file $path to be present"
             } else {
                 Fail-Json $result "timeout while waiting for string regex $search_regex in file $path to match"
-            }  
+            }
         }
     } elseif ($state -in @("absent")) {
         # check if the file is deleted or string doesn't exist in file
@@ -166,7 +166,7 @@ if ($path -eq $null -and $port -eq $null -and $state -eq "drained") {
                 Fail-Json $result "timeout while waiting for file $path to be absent"
             } else {
                 Fail-Json $result "timeout while waiting for string regex $search_regex in file $path to not match"
-            }            
+            }
         }
     }
 } elseif ($port -ne $null) {
@@ -253,7 +253,7 @@ if ($path -eq $null -and $port -eq $null -and $state -eq "drained") {
             $result.elapsed = $elapsed_seconds
             Fail-Json $result "timeout while waiting for $($hostname):$port to drain"
         }
-    }  
+    }
 }
 
 $result.wait_attempts = $attempts
