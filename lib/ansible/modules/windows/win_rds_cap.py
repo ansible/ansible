8aa3d9facec25fdc45901980a727d1e1b3c371b7
Kévin Subileau <github@kevinsubileau.fr>
2018-08-28 17:40:28 +0200

    Fix copyright
    [skip ci]

--- lib/ansible/modules/windows/win_rds_cap.py
+++ lib/ansible/modules/windows/win_rds_cap.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2018, Ansible Project
+# Copyright: (c) 2018, Kevin Subileau (@ksubileau)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
