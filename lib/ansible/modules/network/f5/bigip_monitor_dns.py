9c11a4924e9de5084e5e5e2e3bd0883bd93fe812
Tim Rupp <caphrim007@gmail.com>
2018-08-28 11:03:28 -0700

    Remove f5-sdk usage
    This patch is part of an ongoing effort to remove the f5-sdk from
    the f5 ansible modules for dependency reasons.

--- lib/ansible/modules/network/f5/bigip_monitor_dns.py
+++ lib/ansible/modules/network/f5/bigip_monitor_dns.py
@@ -998,13 +998,15 @@ def main():
         argument_spec=spec.argument_spec,
         supports_check_mode=spec.supports_check_mode,
     )
+    client = F5RestClient(**module.params)
 
     try:
-        client = F5RestClient(**module.params)
         mm = ModuleManager(module=module, client=client)
         results = mm.exec_module()
+        cleanup_tokens(client)
         exit_json(module, results, client)
     except F5ModuleError as ex:
+        cleanup_tokens(client)
         fail_json(module, ex, client)
 
 
