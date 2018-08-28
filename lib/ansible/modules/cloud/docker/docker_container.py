f355a7c133e944d96c488a4f1e77058296c385d0
Felix Fontein <felix@fontein.de>
2018-08-28 13:03:33 +0200

    docker_container: fix check mode for container creation (#42380)
    * Create copy of HostConfig object before adding it to results.
    
    * Create general sanitize function in docker_common. Using it for all return values of docker_container.
    
    * Make Python 2.6 compatible.

--- lib/ansible/modules/cloud/docker/docker_container.py
+++ lib/ansible/modules/cloud/docker/docker_container.py
@@ -606,7 +606,7 @@ import shlex
 from distutils.version import LooseVersion
 
 from ansible.module_utils.basic import human_to_bytes
-from ansible.module_utils.docker_common import HAS_DOCKER_PY_2, HAS_DOCKER_PY_3, AnsibleDockerClient, DockerBaseClass
+from ansible.module_utils.docker_common import HAS_DOCKER_PY_2, HAS_DOCKER_PY_3, AnsibleDockerClient, DockerBaseClass, sanitize_result
 from ansible.module_utils.six import string_types
 
 try:
@@ -1774,7 +1774,7 @@ class ContainerManager(DockerBaseClass):
             self.container_remove(container.Id)
 
     def fail(self, msg, **kwargs):
-        self.client.module.fail_json(msg=msg, **kwargs)
+        self.client.module.fail_json(msg=msg, **sanitize_result(kwargs))
 
     def _get_container(self, container):
         '''
@@ -2101,7 +2101,7 @@ def main():
     )
 
     cm = ContainerManager(client)
-    client.module.exit_json(**cm.results)
+    client.module.exit_json(**sanitize_result(cm.results))
 
 
 if __name__ == '__main__':
