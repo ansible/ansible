c775ec1020303160c6c098e882a34fc2638399dc
Anton Nikulin <anton.nikulin@teamdev.com>
2018-08-29 11:50:34 +0300

    Add copyright

--- lib/ansible/module_utils/network/ftd/common.py
+++ lib/ansible/module_utils/network/ftd/common.py
@@ -1,3 +1,21 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import re
 
 INVALID_IDENTIFIER_SYMBOLS = r'[^a-zA-Z0-9_]'
--- lib/ansible/module_utils/network/ftd/fdm_swagger_client.py
+++ lib/ansible/module_utils/network/ftd/fdm_swagger_client.py
@@ -1,3 +1,21 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from ansible.module_utils.network.ftd.common import HTTPMethod
 from ansible.module_utils.six import integer_types, string_types
 
