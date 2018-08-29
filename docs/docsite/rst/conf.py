accec0428bc25fbc1e8247ba0a2bcce6ce6ae2b0
John R Barker <john@johnrbarker.com>
2018-08-29 17:51:40 +0100

    Use updated pygments (#44846)
    Before this PR we were not using _static/ now we do
    Remove all old files so we don't replace 2018 versions with 2011

--- docs/docsite/rst/conf.py
+++ docs/docsite/rst/conf.py
@@ -151,7 +151,7 @@ html_title = 'Ansible Documentation'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['.static']
+html_static_path = ['../_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
