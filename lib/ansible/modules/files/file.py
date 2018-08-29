8e025d3e40b0f6ec2438cf39caf517ac0274c4f2
Todd Walton <tdwalton@gmail.com>
2018-08-29 16:12:32 -0400

    Clarify docs re mode's octal representation (#44410)
    * Clarify docs re mode's octal representation
    
    I changed the language about how to use mode to make it more obvious
    that using "01777" is not a typo, because the leading zero is not meant
    to reflect the way that number might have been given on a command line.
    See also: issues #5409 #9196 #11385 #13115 #18952 #23491 #23521

--- lib/ansible/modules/files/file.py
+++ lib/ansible/modules/files/file.py
@@ -103,11 +103,12 @@ options:
 '''
 
 EXAMPLES = '''
-# change file ownership, group and mode. When specifying mode using octal numbers, first digit should always be 0.
+# change file ownership, group and mode
 - file:
     path: /etc/foo.conf
     owner: foo
     group: foo
+    # when specifying mode using octal numbers, add a leading 0
     mode: 0644
 - file:
     path: /work
