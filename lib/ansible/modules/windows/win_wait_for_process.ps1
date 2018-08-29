d12e6ac3d40c07f8ea6648034f9299bfca1fb751
Dag Wieers <dag@wieers.com>
2018-08-29 15:41:29 +0200

    win_wait_for_process: Limit to PowerShell 4 and higher

--- lib/ansible/modules/windows/win_wait_for_process.ps1
+++ lib/ansible/modules/windows/win_wait_for_process.ps1
@@ -1,12 +1,12 @@
 #!powershell
-# This file is part of Ansible
 
 # Copyright: (c) 2017, Ansible Project
+# Copyright: (c) 2018, Dag Wieers (@dagwieers) <dag@wieers.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 #Requires -Module Ansible.ModuleUtils.Legacy
-#Requires -Module Ansible.ModuleUtils.FileUtil
 #Requires -Module Ansible.ModuleUtils.SID
+#Requires -Version 4
 
 $ErrorActionPreference = "Stop"
 
@@ -25,32 +25,32 @@ $timeout = Get-AnsibleParam -obj $params -name "timeout" -type "int" -default 30
 
 $result = @{
     changed = $false
+    elapsed = 0
     matched_processes = @()
 }
 
 # Validate the input
-if ($state -eq "absent" -and $sleep -ne 1)
-{
+if ($state -eq "absent" -and $sleep -ne 1) {
     Add-Warning -obj $result -message "sleep parameter has no effect when waiting for a process to stop."
 }
 
-if ($state -eq "absent" -and $process_min_count -ne 1)
-{
+if ($state -eq "absent" -and $process_min_count -ne 1) {
     Add-Warning -obj $result -message "process_min_count parameter has no effect when waiting for a process to stop."
 }
 
-if (($process_name_exact -or $process_name_pattern) -and $process_id)
-{
+if (($process_name_exact -or $process_name_pattern) -and $process_id) {
     Fail-json -obj $result -message "process_id may not be used with process_name_exact or process_name_pattern."
 }
-if ($process_name_exact -and $process_name_pattern)
-{
+if ($process_name_exact -and $process_name_pattern) {
     Fail-json -obj $result -message "process_name_exact and process_name_pattern may not be used at the same time."
 }
 
-if (-not ($process_name_exact -or $process_name_pattern -or $process_id -or $owner))
-{
+if (-not ($process_name_exact -or $process_name_pattern -or $process_id -or $owner)) {
     Fail-json -obj $result -message "at least one of: process_name_exact, process_name_pattern, process_id, or owner must be supplied."
+}
+
+if ($owner -and ("IncludeUserName" -notin (Get-Command -Name Get-Process).Parameters.Keys)) {
+    Fail-json -obj $result -message "This version of Powershell does not support filtering processes by 'owner'."
 }
 
 Function Get-ProcessMatchesFilter {
@@ -64,44 +64,55 @@ Function Get-ProcessMatchesFilter {
         $ProcessId
     )
 
-    $WMIProcesses = Get-Process -IncludeUserName
-    foreach ($WMIProcess in $WMIProcesses) {
+    try {
+        $Processes = Get-Process -IncludeUserName
+        $SupportsUserNames = $true
+    } catch [System.Management.Automation.ParameterBindingException] {
+        $Processes = Get-Process
+        $SupportsUserNames = $false
+    }
+
+    foreach ($Process in $Processes) {
 
         # If a process name was specified in the filter, validate that here.
-        if (-not [String]::IsNullOrEmpty($ProcessNamePattern)) {
-            if ($WMIProcess.ProcessName -notmatch $ProcessNamePattern) {
+        if ($ProcessNamePattern) {
+            if ($Process.ProcessName -notmatch $ProcessNamePattern) {
                 continue
             }
         }
 
         # If a process name was specified in the filter, validate that here.
         if ($ProcessNameExact -is [Array]) {
-            if ($ProcessNameExact -notcontains $WMIProcess.ProcessName) {
+            if ($ProcessNameExact -notcontains $Process.ProcessName) {
                 continue
             }
-        } elseif (-not [String]::IsNullOrEmpty($ProcessNameExact)) {
-            if ($ProcessNameExact -ne $WMIProcess.ProcessName) {
+        } elseif ($ProcessNameExact) {
+            if ($ProcessNameExact -ne $Process.ProcessName) {
                 continue
             }
         }
 
         # If a PID was specified in the filter, validate that here.
         if ($ProcessId -and $ProcessId -ne 0) {
-            if ($ProcessId -ne $WMIProcess.Id) {
+            if ($ProcessId -ne $Process.Id) {
                 continue
             }
         }
 
         # If an owner was specified in the filter, validate that here.
-        if (-not [String]::IsNullOrEmpty($Owner)) {
-            if ([String]::IsNullOrEmpty($WMIProcess.UserName)) {
+        if ($Owner) {
+            if (-not $Process.UserName) {
                 continue
-            } elseif ((Convert-ToSID($Owner)) -ne (Convert-ToSID($WMIProcess.UserName))) {  # NOTE: This is rather expensive
+            } elseif ((Convert-ToSID($Owner)) -ne (Convert-ToSID($Process.UserName))) {  # NOTE: This is rather expensive
                 continue
             }
         }
 
-        $WMIProcess | Select-Object -Property Id, ProcessName, UserName
+        if ($SupportsUserNames -eq $true) {
+            $Process | Select-Object -Property Id, ProcessName, UserName
+        } else {
+            $Process | Select-Object -Property Id, ProcessName
+        }
     }
 }
 
@@ -109,6 +120,7 @@ $module_start = Get-Date
 Start-Sleep -Seconds $pre_wait_delay
 
 if ($state -eq "present" ) {
+
     # Wait for a process to start
     $Processes = @()
     $attempts = 0
@@ -119,29 +131,27 @@ if ($state -eq "present" ) {
         }
 
         $Processes = Get-ProcessMatchesFilter -Owner $owner -ProcessNameExact $process_name_exact -ProcessNamePattern $process_name_pattern -ProcessId $process_id
-        Start-Sleep -Seconds $sleep
-        $attempts ++
-        $ProcessCount = $null
         if ($Processes -is [array]) {
+            $result.matched_processes = $Processes
             $ProcessCount = $Processes.count
         } elseif ($null -ne $Processes) {
+            $result.matched_processes = ,$Processes
             $ProcessCount = 1
         } else {
+            $result.matched_processes = @()
             $ProcessCount = 0
         }
+
+        Start-Sleep -Seconds $sleep
+        $attempts ++
+
     } While ($ProcessCount -lt $process_min_count)
 
-    if ($Processes -is [array]) {
-        $result.matched_processes = $Processes
-    } elseif ($null -ne $Processes) {
-        $result.matched_processes = ,$Processes
-    } else {
-        $result.matched_processes = @()
-    }
-}
-elseif ($state -eq "absent") {
+} elseif ($state -eq "absent") {
+
     # Wait for a process to stop
     $Processes = Get-ProcessMatchesFilter -Owner $owner -ProcessNameExact $process_name_exact -ProcessNamePattern $process_name_pattern -ProcessId $process_id
+
     if ($Processes -is [array]) {
         $result.matched_processes = $Processes
         $ProcessCount = $Processes.count
@@ -161,6 +171,7 @@ elseif ($state -eq "absent") {
             Fail-Json -obj $result -message "Timeout while waiting for process(es) to stop"
         }
     }
+
 }
 
 Start-Sleep -Seconds $post_wait_delay
