90952e20a195c0786120cea6ddc83ae48ae1390c
Tim Rupp <caphrim007@gmail.com>
2018-08-29 08:45:39 -0700

    Deprecates bigip_gtm_facts (#44841)
    The code to support these facts has been added to bigip_device_facts.
    So it is no longer necessary to include this module.

--- lib/ansible/modules/network/f5/_bigip_gtm_facts.py
+++ lib/ansible/modules/network/f5/_bigip_gtm_facts.py
@@ -9,7 +9,7 @@ __metaclass__ = type
 
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
+                    'status': ['deprecated'],
                     'supported_by': 'community'}
 
 DOCUMENTATION = r'''
@@ -33,7 +33,16 @@ options:
       - Perform regex filter of response. Filtering is done on the name of
         the resource. Valid filters are anything that can be provided to
         Python's C(re) module.
+deprecated:
+  removed_in: '2.11'
+  alternative: bigip_device_facts
+  why: >
+    The bigip_gtm_facts module is an outlier as all facts are being collected
+    in the bigip_device_facts module. Additionally, the M(bigip_device_facts)
+    module is easier to maintain and use.
 extends_documentation_fragment: f5
+notes:
+  - This module is deprecated. Use the C(bigip_device_facts) module instead.
 author:
   - Tim Rupp (@caphrim007)
 '''
