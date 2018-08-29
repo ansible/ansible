e2b5bf5a7baadc858c97d73bc4511eb2c9d425a2
Todd Walton <tdwalton@gmail.com>
2018-08-29 15:47:42 -0400

    change mode param description to show quoting

--- lib/ansible/modules/files/template.py
+++ lib/ansible/modules/files/template.py
@@ -100,7 +100,7 @@ options:
       - "Mode the file or directory should be. For those used to I(/usr/bin/chmod) remember that
         modes are actually octal numbers.  You must either add a leading zero so that Ansible's
         YAML parser knows it is an octal number (like C(0644) or C(01777)) or quote it
-        (like C('644') or C('0644') so Ansible receives a string and can do its own conversion from
+        (like C('644') or C('1777')) so Ansible receives a string and can do its own conversion from
         string into number.  Giving Ansible a number without following one of these rules will end
         up with a decimal number which will have unexpected results.  As of version 1.8, the mode
         may be specified as a symbolic mode (for example, C(u+rwx) or C(u=rw,g=r,o=r)).  As of
