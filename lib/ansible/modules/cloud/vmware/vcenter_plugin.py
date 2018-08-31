feb3b7d7c7df3bb51a8e2f2c8c83be3883a833c5
Michael Tipton <mike@ibeta.org>
2018-08-31 09:08:27 -0400

    fix file header

--- lib/ansible/modules/cloud/vmware/vcenter_plugin.py
+++ lib/ansible/modules/cloud/vmware/vcenter_plugin.py
@@ -1,4 +1,7 @@
-opyright: (c) 2018, Michael Tipton <mike () ibeta.org>
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2018, Michael Tipton <mike () ibeta.org>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
