a23dfce2c90e87878897e42d6ac5bbf11ceb420e
Jonathan <jonjitsu@users.noreply.github.com>
2018-09-01 22:17:25 -0400

    Added default value for when CloudFrontOriginAccessIdentityList is (#44984)
    missing.
    
    (cherry picked from commit 93cd0502505822eda5b16c0017d8a0394e16c605)

--- lib/ansible/module_utils/aws/cloudfront_facts.py
+++ lib/ansible/module_utils/aws/cloudfront_facts.py
@@ -93,7 +93,7 @@ class CloudFrontFactsServiceManager(object):
     def list_origin_access_identities(self):
         try:
             paginator = self.client.get_paginator('list_cloud_front_origin_access_identities')
-            result = paginator.paginate().build_full_result()['CloudFrontOriginAccessIdentityList']
+            result = paginator.paginate().build_full_result().get('CloudFrontOriginAccessIdentityList', {})
             return result.get('Items', [])
         except botocore.exceptions.ClientError as e:
             self.module.fail_json_aws(e, msg="Error listing cloud front origin access identities")
