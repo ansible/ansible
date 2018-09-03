1d06a864810cc60eb0103a65edbbc3b4b97dcb06
Rémy Léone <rleone@online.net>
2018-09-03 11:21:22 +0200

    Add licence and boilerplate to Online module_utils

--- lib/ansible/module_utils/online.py
+++ lib/ansible/module_utils/online.py
@@ -1,3 +1,8 @@
+# Simplified BSD License (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 import json
 import sys
 
