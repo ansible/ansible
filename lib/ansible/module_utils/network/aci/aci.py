8da2b178e1cc12b8588c91f29d2d5c7efc6c6e8a
Dag Wieers <dag@wieers.com>
2018-09-03 19:29:32 +0200

    Backport of 45088

--- lib/ansible/module_utils/network/aci/aci.py
+++ lib/ansible/module_utils/network/aci/aci.py
@@ -461,6 +461,7 @@ class ACIModule(object):
         elif mo is None:
             # Query for all objects of the module's class (filter by properties)
             self.path = 'api/class/{0}.json'.format(obj_class)
+            self.update_qs({'query-target-filter': self.build_filter(obj_class, obj_filter)})
         else:
             # Query for a specific object in the module's class
             self.path = 'api/mo/uni/{0}.json'.format(obj_rn)
@@ -485,6 +486,7 @@ class ACIModule(object):
         elif parent_obj is None and mo is None:
             # Query for all objects of the module's class
             self.path = 'api/class/{0}.json'.format(obj_class)
+            self.update_qs({'query-target-filter': self.build_filter(obj_class, obj_filter)})
         elif parent_obj is None:  # mo is known
             # Query for all objects of the module's class that match the provided ID value
             self.path = 'api/class/{0}.json'.format(obj_class)
@@ -521,6 +523,7 @@ class ACIModule(object):
         elif root_obj is None and parent_obj is None and mo is None:
             # Query for all objects of the module's class
             self.path = 'api/class/{0}.json'.format(obj_class)
+            self.update_qs({'query-target-filter': self.build_filter(obj_class, obj_filter)})
         elif root_obj is None and parent_obj is None:  # mo is known
             # Query for all objects of the module's class matching the provided ID value of the object
             self.path = 'api/class/{0}.json'.format(obj_class)
