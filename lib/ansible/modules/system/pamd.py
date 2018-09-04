87f7e7b926424c2d5abf36f7a88e2d48627e9053
Protista <42447597+Protista@users.noreply.github.com>
2018-09-04 10:14:22 -0500

    Add definitive to valid_simple_controls in pamd module (#44601)
    * Add definitive to valid_simple_controls in pamd module
    
    * Add changelog fragment for issue 44278
    
    * Adjusted module name to not include path or extension
    
    (cherry picked from commit 80bea8adafbb1c5264d1644ecce4933d762727d4)

--- lib/ansible/modules/system/pamd.py
+++ lib/ansible/modules/system/pamd.py
@@ -210,6 +210,23 @@ class PamdRule(object):
                  rule_control, rule_module_path,
                  rule_module_args=None):
 
+
+class PamdRule(PamdLine):
+
+    valid_types = ['account', 'auth', 'password', 'session']
+    valid_simple_controls = ['required', 'requisite', 'sufficient', 'optional', 'include', 'substack', 'definitive']
+    valid_control_values = ['success', 'open_err', 'symbol_err', 'service_err', 'system_err', 'buf_err',
+                            'perm_denied', 'auth_err', 'cred_insufficient', 'authinfo_unavail', 'user_unknown',
+                            'maxtries', 'new_authtok_reqd', 'acct_expired', 'session_err', 'cred_unavail',
+                            'cred_expired', 'cred_err', 'no_module_data', 'conv_err', 'authtok_err',
+                            'authtok_recover_err', 'authtok_lock_busy', 'authtok_disable_aging', 'try_again',
+                            'ignore', 'abort', 'authtok_expired', 'module_unknown', 'bad_item', 'conv_again',
+                            'incomplete', 'default']
+    valid_control_actions = ['ignore', 'bad', 'die', 'ok', 'done', 'reset']
+
+    def __init__(self, rule_type, rule_control, rule_path, rule_args=None):
+        self._control = None
+        self._args = None
         self.rule_type = rule_type
         self.rule_control = rule_control
         self.rule_module_path = rule_module_path
