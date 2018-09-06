e48da9c018b12ffe369840334a6c59b189547909
Matt Martz <matt@sivel.net>
2018-09-06 10:28:33 -0500

    Dedupe _expand_hostpattern, making it available to all inventory plugins

--- lib/ansible/plugins/inventory/__init__.py
+++ lib/ansible/plugins/inventory/__init__.py
@@ -226,6 +226,31 @@ class BaseInventoryPlugin(AnsiblePlugin):
             if k in data:
                 self._options[k] = data.pop(k)
 
+    def _expand_hostpattern(self, hostpattern):
+        '''
+        Takes a single host pattern and returns a list of hostnames and an
+        optional port number that applies to all of them.
+        '''
+        # Can the given hostpattern be parsed as a host with an optional port
+        # specification?
+
+        try:
+            (pattern, port) = parse_address(hostpattern, allow_ranges=True)
+        except Exception:
+            # not a recognizable host pattern
+            pattern = hostpattern
+            port = None
+
+        # Once we have separated the pattern, we expand it into list of one or
+        # more hostnames, depending on whether it contains any [x:y] ranges.
+
+        if detect_range(pattern):
+            hostnames = expand_hostname_range(pattern)
+        else:
+            hostnames = [pattern]
+
+        return (hostnames, port)
+
     def clear_cache(self):
         pass
 
