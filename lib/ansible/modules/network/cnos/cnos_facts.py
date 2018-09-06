75aa1b40e10465fb3875353e7994c53caf94d0b0
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2018-09-06 15:18:15 +0530

    Update cnos_facts.py

--- lib/ansible/modules/network/cnos/cnos_facts.py
+++ lib/ansible/modules/network/cnos/cnos_facts.py
@@ -68,33 +68,37 @@ options:
         required: true
       port:
         description:
-          - Specifies the port to use when building the connection to the remote device.
+          - Specifies the port to use when building the connection to the
+            remote device.
         default: 22
       username:
         description:
           - Configures the username to use to authenticate the connection to
             the remote device.  This value is used to authenticate
             the SSH session. If the value is not specified in the task, the
-            value of environment variable C(ANSIBLE_NET_USERNAME) will be used instead.
+            value of environment variable C(ANSIBLE_NET_USERNAME) will be used
+            instead.
       password:
         description:
           - Specifies the password to use to authenticate the connection to
             the remote device.   This value is used to authenticate
             the SSH session. If the value is not specified in the task, the
-            value of environment variable C(ANSIBLE_NET_PASSWORD) will be used instead.
+            value of environment variable C(ANSIBLE_NET_PASSWORD) will be used
+            instead.
       timeout:
         description:
-          - Specifies the timeout in seconds for communicating with the network device
-            for either connecting or sending commands.  If the timeout is
-            exceeded before the operation is completed, the module will error.
+          - Specifies the timeout in seconds for communicating with the network
+            device for either connecting or sending commands.  If the timeout
+            is exceeded before the operation is completed, the module will
+            error.
         default: 10
       ssh_keyfile:
         description:
           - Specifies the SSH key to use to authenticate the connection to
             the remote device.   This value is the path to the
-            key used to authenticate the SSH session. If the value is not specified
-            in the task, the value of environment variable C(ANSIBLE_NET_SSH_KEYFILE)
-            will be used instead.
+            key used to authenticate the SSH session. If the value is not
+            specified in the task, the value of environment variable
+            C(ANSIBLE_NET_SSH_KEYFILE) will be used instead.
   gather_subset:
     version_added: "2.6"
     description:
@@ -490,7 +494,7 @@ class Interfaces(FactsBase):
             innerData = dict()
             count = len(neighborSplit)
             if count == 5:
-                device_name =  neighborSplit[0].strip()
+                device_name = neighborSplit[0].strip()
                 innerData['Local Interface'] = neighborSplit[1].strip()
                 innerData['Hold Time'] = neighborSplit[2].strip()
                 innerData['Capability'] = neighborSplit[3].strip()
