d43e89e1b0cea3303a7ddc235cb71fef3887f990
Will Thames <will@thames.id.au>
2018-09-08 10:08:09 +1000

    Use a sensible default for k8s merge_type (#45284)
    * Use a sensible default for k8s merge_type
    
    The sensible default for merge_type is `['strategic-merge', 'merge'].
    However, we can't make this the default default, as we need to support
    users who are using openshift 0.6.0, where the merge_type parameter is
    unsupported.
    
    * Refactor k8s test suite for merge_type tests
    
    Allow tests with pre-merge-type openshift and post-merge-type
    openshift.

--- lib/ansible/modules/clustering/k8s/k8s.py
+++ lib/ansible/modules/clustering/k8s/k8s.py
@@ -50,6 +50,9 @@ options:
     - See U(https://kubernetes.io/docs/tasks/run-application/update-api-object-kubectl-patch/#use-a-json-merge-patch-to-update-a-deployment)
     - Requires openshift >= 0.6.2
     - If more than one merge_type is given, the merge_types will be tried in order
+    - If openshift >= 0.6.2, this defaults to C(['strategic-merge', 'merge']), which is ideal for using the same parameters
+      on resource kinds that combine Custom Resources and built-in resources. For openshift < 0.6.2, the default
+      is simply C(strategic-merge).
     choices:
     - json
     - merge
