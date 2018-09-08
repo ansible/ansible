a42171811683dc5a5466231d8bfd0b115992cc68
angristan <angristan@pm.me>
2018-09-08 12:23:27 +0200

    Fix previous commit
    4e24e9744df8b750c9d4fe1b631240424c056977

--- lib/ansible/modules/packaging/os/snap.py
+++ lib/ansible/modules/packaging/os/snap.py
@@ -98,7 +98,7 @@ def is_snap_installed(module, snap_name):
     snap_path = module.get_bin_path("snap", True)
     cmd_parts = [snap_path, 'list', snap_name]
     cmd = ' '.join(cmd_parts)
-    rc = module.run_command(cmd, check_rc=False)
+    rc, out, err = module.run_command(cmd, check_rc=False)
 
     return rc == 0
 
