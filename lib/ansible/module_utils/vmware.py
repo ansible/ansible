a740704ff88277d416c99c1732617da083e38dea
Harald Albers <albers@users.noreply.github.com>
2018-09-10 08:57:40 +0200

    VMware: Fix timeout detection in vmware_deploy_ovf module (#45159)
    Signed-off-by: Harald Albers <github@albersweb.de>

--- lib/ansible/module_utils/vmware.py
+++ lib/ansible/module_utils/vmware.py
@@ -67,8 +67,9 @@ def wait_for_vm_ip(content, vm, timeout=300):
     facts = dict()
     interval = 15
     while timeout > 0:
-        facts = gather_vm_facts(content, vm)
-        if facts['ipv4'] or facts['ipv6']:
+        _facts = gather_vm_facts(content, vm)
+        if _facts['ipv4'] or _facts['ipv6']:
+            facts = _facts
             break
         time.sleep(interval)
         timeout -= interval
