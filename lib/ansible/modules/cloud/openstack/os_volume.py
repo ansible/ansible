252170be41ce203c7f3545b7880f97e9a979db28
Anna Nowak <anna.nowak@tomtom.com>
2018-09-10 15:03:00 +0200

    Replace non-existing OpenStackCloudTimeout exception. Fixes: #45151

--- lib/ansible/modules/cloud/openstack/os_volume.py
+++ lib/ansible/modules/cloud/openstack/os_volume.py
@@ -124,7 +124,7 @@ def _absent_volume(module, cloud, sdk):
             changed = cloud.delete_volume(name_or_id=module.params['display_name'],
                                           wait=module.params['wait'],
                                           timeout=module.params['timeout'])
-        except sdk.exceptions.OpenStackCloudTimeout:
+        except sdk.exceptions.ResourceTimeout:
             module.exit_json(changed=changed)
 
     module.exit_json(changed=changed)
