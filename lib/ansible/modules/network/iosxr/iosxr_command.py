a76c34ef94cfcb976212acc0f335c687c3bbcd7c
Nathaniel Case <ncase@redhat.com>
2018-09-12 18:01:57 -0400

    fix iosxr

--- lib/ansible/modules/network/iosxr/iosxr_command.py
+++ lib/ansible/modules/network/iosxr/iosxr_command.py
@@ -121,6 +121,7 @@ failed_conditions:
 """
 import time
 
+from ansible.module_utils._text import to_text
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.network.common.parsing import Conditional
 from ansible.module_utils.network.common.utils import to_lines
