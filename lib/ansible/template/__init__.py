347280f5cbc72efd7379c61db0eb89b1ed313a83
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2018-09-13 15:41:19 +0200

    fix swallowing line no in template syntax errors

--- lib/ansible/template/__init__.py
+++ lib/ansible/template/__init__.py
@@ -717,7 +717,8 @@ class Templar:
             try:
                 t = myenv.from_string(data)
             except TemplateSyntaxError as e:
-                raise AnsibleError("template error while templating string: %s. String: %s" % (to_native(e), to_native(data)))
+                raise AnsibleError("template error while templating string: %s. Line: %s, File: %s, String: %s" %
+                                   (to_native(e), e.lineno, e.filename, to_native(data)))
             except Exception as e:
                 if 'recursion' in to_native(e):
                     raise AnsibleError("recursive loop detected in template string: %s" % to_native(data))
