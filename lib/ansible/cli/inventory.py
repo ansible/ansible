43d21094b2803b8c81838141ffe294a75852f27d
Brian Coca <bcoca@users.noreply.github.com>
2018-09-13 15:38:52 -0400

    avoid empty groups in json output (#45621)
    they get confused as hosts by script plugin
    
    fixes #45601

--- lib/ansible/cli/inventory.py
+++ lib/ansible/cli/inventory.py
@@ -329,6 +329,8 @@ class InventoryCLI(CLI):
                 results[group.name]['vars'] = self._get_group_variables(group)
 
             self._remove_empty(results[group.name])
+            if not results[group.name]:
+                del results[group.name]
 
             return results
 
