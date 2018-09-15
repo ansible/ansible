6a42e1cd0c2298f3aa84137868d4523e878a7479
Johannes Brunswicker <johannes.brunswicker@gmail.com>
2018-09-15 13:42:10 +0200

    added some more documentation

--- lib/ansible/modules/web_infrastructure/sophos_utm/utm_proxy_frontend.py
+++ lib/ansible/modules/web_infrastructure/sophos_utm/utm_proxy_frontend.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+# Copyright: (c) 2018, Johannes Brunswicker <johannes.brunswicker@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 from __future__ import absolute_import, division, print_function
 
 from ansible.module_utils.utm_utils import UTM, UTMModule
