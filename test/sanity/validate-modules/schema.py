96a429fdc502a5e7fb532bad242c9defdf5a5f38
John Barker <john@johnrbarker.com>
2018-09-17 15:26:03 +0100

    Allow removed_in: 2.12

--- test/sanity/validate-modules/schema.py
+++ test/sanity/validate-modules/schema.py
@@ -123,7 +123,7 @@ def deprecation_schema():
         # Deprecation cycle changed at 2.4 (though not retroactively)
         # 2.3 -> removed_in: "2.5" + n for docs stub
         # 2.4 -> removed_in: "2.8" + n for docs stub
-        Required('removed_in'): Any("2.2", "2.3", "2.4", "2.5", "2.6", "2.8", "2.9", "2.10", "2.11"),
+        Required('removed_in'): Any("2.2", "2.3", "2.4", "2.5", "2.6", "2.8", "2.9", "2.10", "2.11", "2.12"),
         Required('why'): Any(*string_types),
         Required('alternative'): Any(*string_types),
         'removed': Any(True),
