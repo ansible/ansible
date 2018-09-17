c55c830be0d8f781c670513d3eaa8cf1b8c89eae
Adrian Likins <alikins@redhat.com>
2018-09-17 18:39:27 -0400

    Let the arg spec validate task 'name' be set from data
    This servces two purposes:
     1) Allow roles to add additional info about the arg spec
        that will show up in the default output.
        ie, 'Validating arg spec all_the_clouds has all the required cloud
        args'
    
     2) let test cases change the name of the injected implicit
        validate_arg_spec task so they can include 'EXPECTED FAILURE'
        in the name so ansible-test's junit_callback doesnt set the
        task as failed when it is expected to fail
    
    Default to style like:
    
        TASK [test1 : Validating arguments against arg spec 'main']
    
    If the argument_spec includes a 'description' field, then it gets
    added to the task name like:
    
        TASK [test1 : Validating arguments against arg spec \
          'main' - The argument spec for the 'test1' role]
    
    Add argument_spec_name to the validate_args_context data.

--- lib/ansible/playbook/role/__init__.py
+++ lib/ansible/playbook/role/__init__.py
@@ -323,13 +323,21 @@ class Role(Base, Become, Conditional, Taggable):
 
     def _create_arg_spec_validation_task_data(self, argument_spec, argument_spec_name,
                                               role_name, role_path, role_params):
+        # If the arg spec provides a description, use it to flesh out the validation task name
+        task_name_parts = ["Validating arguments against arg spec '%s'" % argument_spec_name]
+        if 'description' in argument_spec:
+            task_name_parts.append(argument_spec['description'])
+        task_name = ' - '.join(task_name_parts)
+
         arg_spec_task = {'action': {'module': 'validate_arg_spec',
                                     'argument_spec': argument_spec,
                                     'provided_arguments': role_params,
                                     'validate_args_context': {'type': 'role',
                                                               'name': role_name,
+                                                              'argument_spec_name': argument_spec_name,
                                                               'path': role_path},
                                     },
+                         'name': task_name,
                          # TODO: use ignore_error from include_role for the validate task?
                          # 'ignore_errors': ignore_errors,
                          # 'vars': {'argument_spec': []},
