8f841b057f1c25ae9eb9408deed518c655146e2a
ptux <ooocamel@icloud.com>
2018-09-18 15:15:59 +0900

    outline of code  --alpha

--- lib/ansible/modules/cloud/amazon/route53_record.py
+++ lib/ansible/modules/cloud/amazon/route53_record.py
@@ -13,8 +13,8 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 ANSIBLE_METADATA = {'status': ['preview'],
-                     'supported_by': 'community',
-                     'metadata_version': '1.1'}
+                    'supported_by': 'community',
+                    'metadata_version': '1.1'}
 
 DOCUMENTATION = '''
 ---
@@ -36,3 +36,76 @@ EXAMPLES = '''
 
 RETURN = '''
 '''
+
+try:
+    import botocore
+except ImportError:
+    pass  # Handled by AnsibleAWSModule
+
+from ansible.module_utils.aws.core import AnsibleAWSModule
+from ansible.module_utils._text import to_native
+from ansible.module_utils.ec2 import (
+    AWSRetry,
+    boto3_conn,
+    ec2_argument_spec,
+    get_aws_connection_info,
+    camel_dict_to_snake_dict,
+    boto3_tag_list_to_ansible_dict,
+    ansible_dict_to_boto3_filter_list,
+    ansible_dict_to_boto3_tag_list,
+    compare_aws_tags
+)
+
+
+class AWSRoute53Record(object):
+    def __init__(self, module=None, results=None):
+        self._module = module
+        self._results = results
+        self._connection = self._module.client('ec2')
+        self._check_mode = self._module.check_mode
+        self.warnings = []
+
+    def _read_zone(self):
+        pass
+
+    def _create_record(self):
+        pass
+
+    def _read_record(self):
+        pass
+
+    def _update_record(self):
+        pass
+
+    def _delete_record(self):
+        pass
+
+    def ensure_present(self):
+        pass
+
+    def ensure_absent(self):
+        pass
+
+    def process(self):
+        pass
+
+
+def main():
+    argument_spec = ec2_argument_spec()
+
+    argument_spec.update(dict(
+    ))
+
+    module = AnsibleAWSModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True
+    )
+    results = dict(changed=False)
+
+record_controller = AWSRoute53Record(module=module, results=results)
+record_controller.process()
+
+module.exit_json(**results)
+
+if __name__ == '__main__':
+    main()
