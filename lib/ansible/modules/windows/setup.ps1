46f63b20117bd40e09d5081cd6513b8b30deefa0
jdkaufman01 <jesse.kaufman@gmail.com>
2018-09-19 16:24:53 -0500

    Fix incorrect processor information
    Fixes Issue #45869 
    Windows Processor Information Incorrect from Setup

--- lib/ansible/modules/windows/setup.ps1
+++ lib/ansible/modules/windows/setup.ps1
@@ -349,7 +349,7 @@ if($gather_subset.Contains('processor')) {
     }
 
     $cpu_list = @( )
-    for ($i=1; $i -le ($win32_cpu.NumberOfLogicalProcessors / $win32_cs.NumberOfProcessors); $i++) {
+    for ($i=1; $i -le $win32_cs.NumberOfProcessors; $i++) {
         $cpu_list += $win32_cpu.Manufacturer
         $cpu_list += $win32_cpu.Name
     }
@@ -358,8 +358,8 @@ if($gather_subset.Contains('processor')) {
         ansible_processor = $cpu_list
         ansible_processor_cores = $win32_cpu.NumberOfCores
         ansible_processor_count = $win32_cs.NumberOfProcessors
-        ansible_processor_threads_per_core = ($win32_cpu.NumberOfLogicalProcessors / $win32_cs.NumberOfProcessors / $win32_cpu.NumberOfCores)
-        ansible_processor_vcpus = ($win32_cpu.NumberOfLogicalProcessors / $win32_cs.NumberOfProcessors)
+        ansible_processor_threads_per_core = ($win32_cpu.NumberOfLogicalProcessors / $win32_cpu.NumberofCores)
+        ansible_processor_vcpus = ($win32_cpu.NumberOfLogicalProcessors * $win32_cs.NumberOfProcessors)
     }
 }
 
