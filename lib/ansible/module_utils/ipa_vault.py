b7f8fbeaf7cbdf99d871d06250b656b4c4d23285
Stefan Scheglmann <scheglmann@strato.de>
2018-09-20 11:27:15 +0000

    Adds ipa_vault.py
    Includes now the changes from my PR against jparrills
    fork of ansible. https://github.com/jparrill/ansible/pull/1
    Jparrills PR is already merged and cannot be reopend,
    so this PR now include all my changes made to ipa_vault module.
    Will remove the reference to the other PR.

--- lib/ansible/module_utils/ipa_vault.py
+++ lib/ansible/module_utils/ipa_vault.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright: (c) 2018, Stefan Scheglmann <scheglmann@strato.de>
+# based on vault.py from freeipa (https://github.com/freeipa/freeipa) # noqa: E501
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) # noqa: E501
 
 from __future__ import absolute_import, division, print_function
--- lib/ansible/modules/identity/ipa/ipa_vault.py
+++ lib/ansible/modules/identity/ipa/ipa_vault.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright: (c) 2018, Juan Manuel Parrilla <jparrill@redhat.com>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# Copyright: (c) 2018, Juan Manuel Parrilla <jparrill@redhat.com> and
+# Stefan Scheglmann <scheglmann@strato.de>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) # noqa: E501
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
@@ -13,56 +14,67 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = '''
 ---
 module: ipa_vault
-author: Juan Manuel Parrilla (@jparrill)
-short_description: Manage FreeIPA vaults
+author: Juan Manuel Parrilla (@jparrill) and Stefan Scheglmann (@schegi)
+short_description: Add, delete vaults in IPA
 description:
-- Add, modify and delete vaults and secret vaults.
-- KRA service should be enabled to use this module.
+    - Add, and delete vaults in ipa
+    - KRA service should be enabled to use this module.
 options:
     cn:
         description:
-        - Vault name.
-        - Can not be changed as it is the unique identifier.
+            - Vault name.
+            - Can not be changed as it is the unique identifier.
         required: true
         aliases: ["name"]
     description:
         description:
-        - Description.
+            - Description.
     ipavaulttype:
         description:
-        - Vault types are based on security level.
-        default: "symmetric"
+            - Vault types are based on security level.
+        default: "standard"
         choices: ["standard", "symmetric", "asymmetric"]
         required: true
-        aliases: ["vault_type"]
+        aliases: ["vault_type", "type"]
     ipavaultpublickey:
         description:
-        - Public key.
-        aliases: ["vault_public_key"]
+            - Public key.
+            - Will not appear in logs.
+        aliases: ["vault_public_key", "public_key"]
+    ipavaultpublickeyfile:
+        description:
+            - File containing the vault public key
+            - Will not appear in logs.
+        aliases: ["vault_public_key_file", "public_key_file"]
+    ipavaultpassword:
+        description:
+            - The vault password.
+            - Will not appear in logs.
+        aliases: ["vault_password", "password"]
+    ipavaultpasswordfile:
+        description:
+            - File containing the vault password
+            - Will not appear in logs.
+        aliases: ["vault_password_file", "password_file"]
     ipavaultsalt:
         description:
-        - Vault Salt.
+            - Vault Salt.
         aliases: ["vault_salt"]
     username:
         description:
-        - Any user can own one or more user vaults.
-        - Mutually exclusive with service.
-        aliases: ["user"]
+            - Any user can own one or more user vaults.
+            - Mutually exclusive with service.
+        aliases: ["vaultuser", "vault_user"]
     service:
         description:
-        - Any service can own one or more service vaults.
-        - Mutually exclusive with user.
+            - Any service can own one or more service vaults.
+            - Mutually exclusive with user.
+        aliases: ["vault_service", "vaultservice"]
     state:
         description:
-        - State to ensure.
+            - State to ensure.
         default: "present"
         choices: ["present", "absent"]
-    replace:
-        description:
-        - Force replace the existant vault on IPA server.
-        type: bool
-        default: False
-        choices: ["True", "False"]
     validate_certs:
         description:
         - Validate IPA server certificates.
@@ -73,50 +85,80 @@ version_added: "2.7"
 '''
 
 EXAMPLES = '''
-# Ensure vault is present
+# Ensure standard vault is present
 - ipa_vault:
     name: vault01
-    vault_type: standard
-    user: user01
     ipa_host: ipa.example.com
     ipa_user: admin
     ipa_pass: topsecret
     validate_certs: false
 
-# Ensure vault is present for Admin user
+# Ensure standard vault is present
 - ipa_vault:
     name: vault01
-    vault_type: standard
+    username: testuser1
     ipa_host: ipa.example.com
     ipa_user: admin
     ipa_pass: topsecret
+    validate_certs: false
+
+# Ensure standard vault is present
+- ipa_vault:
+    name: vault01
+    service: test/ipa.example.com
+    ipa_host: ipa.example.com
+    ipa_user: admin
+    ipa_pass: topsecret
+    validate_certs: false
+
+# Ensure symmetric service vault is present
+- ipa_vault:
+    name: vault01
+    type: symmetric
+    password: my_secret_vault_password
+    ipa_host: ipa.example.com
+    ipa_user: admin
+    ipa_pass: topsecret
+    validate_certs: false
+
+# Ensure asymmetric user vault is present
+- ipa_vault:
+    name: vault01
+    type: asymmetric
+    public_key_file: mykey.pub
+    ipa_host: ipa.example.com
+    ipa_user: admin
+    ipa_pass: topsecret
+    validate_certs: false
 
 # Ensure vault is absent
 - ipa_vault:
     name: vault01
-    vault_type: standard
-    user: user01
+    ipa_host: ipa.example.com
+    ipa_user: admin
+    ipa_pass: topsecret
+    validate_certs: false
     state: absent
-    ipa_host: ipa.example.com
-    ipa_user: admin
-    ipa_pass: topsecret
 
-# Modify vault if already exists
+# Ensure user vault is absent
 - ipa_vault:
     name: vault01
-    vault_type: standard
-    description: "Vault for test"
+    username: testuser1
     ipa_host: ipa.example.com
     ipa_user: admin
     ipa_pass: topsecret
-    replace: True
+    validate_certs: false
+    state: absent
 
-# Get vault info if already exists
+# Ensure service vault is absent
 - ipa_vault:
     name: vault01
+    service: test/ipa.example.com
     ipa_host: ipa.example.com
     ipa_user: admin
     ipa_pass: topsecret
+    validate_certs: false
+    state: absent
 '''
 
 RETURN = '''
@@ -129,103 +171,94 @@ vault:
 import traceback
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.ipa import IPAClient, ipa_argument_spec
+from ansible.module_utils.ipa import ipa_argument_spec
+from ansible.module_utils.ipa_vault import VaultIPAClient
+
+# only for method overwrite
 from ansible.module_utils._text import to_native
 
 
-class VaultIPAClient(IPAClient):
-    def __init__(self, module, host, port, protocol):
-        super(VaultIPAClient, self).__init__(module, host, port, protocol)
-
-    def vault_find(self, name):
-        return self._post_json(method='vault_find', name=None, item={'all': True, 'cn': name})
-
-    def vault_add_internal(self, name, item):
-        return self._post_json(method='vault_add_internal', name=name, item=item)
-
-    def vault_mod_internal(self, name, item):
-        return self._post_json(method='vault_mod_internal', name=name, item=item)
-
-    def vault_del(self, name):
-        return self._post_json(method='vault_del', name=name)
-
-
-def get_vault_dict(description=None, vault_type=None, vault_salt=None, vault_public_key=None, service=None):
-    vault = {}
-
-    if description is not None:
-        vault['description'] = description
-    if vault_type is not None:
-        vault['ipavaulttype'] = vault_type
-    if vault_salt is not None:
-        vault['ipavaultsalt'] = vault_salt
-    if vault_public_key is not None:
-        vault['ipavaultpublickey'] = vault_public_key
-    if service is not None:
-        vault['service'] = service
-    return vault
-
-
-def get_vault_diff(client, ipa_vault, module_vault, module):
-    return client.get_diff(ipa_data=ipa_vault, module_data=module_vault)
-
-
 def ensure(module, client):
+    """Ensure vault state"""
     state = module.params['state']
-    name = module.params['cn']
-    user = module.params['username']
-    replace = module.params['replace']
-
-    module_vault = get_vault_dict(description=module.params['description'], vault_type=module.params['ipavaulttype'],
-                                  vault_salt=module.params['ipavaultsalt'],
-                                  vault_public_key=module.params['ipavaultpublickey'],
-                                  service=module.params['service'])
-    ipa_vault = client.vault_find(name=name)
-
     changed = False
     if state == 'present':
-        if not ipa_vault:
-            # New vault
-            changed = True
-            if not module.check_mode:
-                ipa_vault = client.vault_add_internal(name, item=module_vault)
-        else:
-            # Already exists
-            if replace:
-                diff = get_vault_diff(client, ipa_vault, module_vault, module)
-                if len(diff) > 0:
-                    changed = True
-                    if not module.check_mode:
-                        data = {}
-                        for key in diff:
-                            data[key] = module_vault.get(key)
-                        client.vault_mod_internal(name=name, item=data)
-
+        # Try to get or create vault with the name provided
+        changed, vault = client.get_or_create_vault()
+        # Initialize new vault with empty data byte string
+        if changed:
+            client.archive_data_internal(vault, b'')
     else:
-        if ipa_vault:
+        vault = client.vault_find()
+        if vault:
             changed = True
             if not module.check_mode:
-                client.vault_del(name)
-
-    return changed, client.vault_find(name=name)
+                client.vault_del(vault)
+    return changed, vault
 
 
 def main():
+    """Main module method"""
     argument_spec = ipa_argument_spec()
-    argument_spec.update(cn=dict(type='str', required=True, aliases=['name']),
-                         description=dict(type='str'),
-                         ipavaulttype=dict(type='str', default='symmetric',
-                                           choices=['standard', 'symmetric', 'asymmetric'], aliases=['vault_type']),
-                         ipavaultsalt=dict(type='str', aliases=['vault_salt']),
-                         ipavaultpublickey=dict(type='str', aliases=['vault_public_key']),
-                         service=dict(type='str'),
-                         replace=dict(type='bool', default=False, choices=[True, False]),
-                         state=dict(type='str', default='present', choices=['present', 'absent']),
-                         username=dict(type='list', aliases=['user']))
+    argument_spec.update(
+        cn=dict(
+            type='str',
+            required=True,
+            aliases=['name']
+        ),
+        description=dict(type='str'),
+        ipavaulttype=dict(
+            type='str',
+            default='standard',
+            choices=['standard', 'symmetric', 'asymmetric'],
+            aliases=['vault_type', 'type']
+        ),
+        ipavaultsalt=dict(
+            type='str',
+            aliases=['vault_salt']
+        ),
+        ipavaultpublickey=dict(
+            type='str',
+            aliases=['vault_public_key', 'public_key'],
+            no_log=True
+        ),
+        ipavaultpublickeyfile=dict(
+            type='str',
+            aliases=['vault_public_key_file', 'public_key_file'],
+            no_log=True
+        ),
+        ipavaultpassword=dict(
+            type='str',
+            aliases=['vault_password', 'password'],
+            no_log=True
+        ),
+        ipavaultpasswordfile=dict(
+            type='str',
+            aliases=['vault_password_file', 'password_file'],
+            no_log=True
+        ),
+        username=dict(
+            type='str',
+            aliases=['vaultuser', 'vault_user']
+        ),
+        service=dict(
+            type='str',
+            aliases=['vaultservice', 'vault_service']
+        ),
+        state=dict(
+            type='str',
+            default='present',
+            choices=['present', 'absent']
+        ),
+    )
 
     module = AnsibleModule(argument_spec=argument_spec,
                            supports_check_mode=True,
-                           mutually_exclusive=[['username', 'service']])
+                           mutually_exclusive=[
+                               ['username', 'service'],
+                               ['ipavaultpassword', 'ipavaultpasswordfile'],
+                               ['ipavaultpublickey', 'ipavaultpublickeyfile'],
+                           ])
 
     client = VaultIPAClient(module=module,
                             host=module.params['ipa_host'],
@@ -236,8 +269,9 @@ def main():
                      password=module.params['ipa_pass'])
         changed, vault = ensure(module, client)
         module.exit_json(changed=changed, vault=vault)
-    except Exception as e:
-        module.fail_json(msg=to_native(e), exception=traceback.format_exc())
+    except Exception as exception:
+        module.fail_json(
+            msg=to_native(exception), exception=traceback.format_exc())
 
 
 if __name__ == '__main__':
