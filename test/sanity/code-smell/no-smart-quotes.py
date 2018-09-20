d882b10212e97274b3deeb4d66048df6debfc356
Dag Wieers <dag@wieers.com>
2018-09-20 02:22:34 +0200

    no-smart-quotes: Skip files by path in code-smell
    This is to ensure the generated docs do not trigger code-smell issues on
    contributor systems.

--- test/sanity/code-smell/no-smart-quotes.py
+++ test/sanity/code-smell/no-smart-quotes.py
@@ -15,25 +15,27 @@ def main():
         'test/integration/targets/template/files/encoding_1252_utf-8.expected',
         'test/integration/targets/template/files/encoding_1252_windows-1252.expected',
         'test/integration/targets/template/templates/encoding_1252.j2',
+        'docs/docsite/_build/',
     ])
 
     for path in sys.argv[1:] or sys.stdin.read().splitlines():
-        if path in skip:
-            continue
+        for skip_path in skip:
+            if path.startswith(skip_path):
+                break
+        else:
+            with open(path, 'rb') as path_fd:
+                for line, text in enumerate(path_fd.readlines()):
+                    try:
+                        text = text.decode('utf-8')
+                    except UnicodeDecodeError as ex:
+                        print('%s:%d:%d: UnicodeDecodeError: %s' % (path, line + 1, ex.start + 1, ex))
+                        continue
 
-        with open(path, 'rb') as path_fd:
-            for line, text in enumerate(path_fd.readlines()):
-                try:
-                    text = text.decode('utf-8')
-                except UnicodeDecodeError as ex:
-                    print('%s:%d:%d: UnicodeDecodeError: %s' % (path, line + 1, ex.start + 1, ex))
-                    continue
+                    match = re.search(u'([‘’“”])', text)
 
-                match = re.search(u'([‘’“”])', text)
-
-                if match:
-                    print('%s:%d:%d: use ASCII quotes `\'` and `"` instead of Unicode quotes' % (
-                        path, line + 1, match.start(1) + 1))
+                    if match:
+                        print('%s:%d:%d: use ASCII quotes `\'` and `"` instead of Unicode quotes' % (
+                            path, line + 1, match.start(1) + 1))
 
 
 if __name__ == '__main__':
