5740fa8c78b4311ededd5c0ca2971d13ff388e2c
Andrii Bilousko <arestarh@ukr.net>
2018-09-21 21:21:40 +0300

    Added: win_iis_webhandler module to manage HTTP handlers in IIS.

---
+++ lib/ansible/modules/windows/win_iis_webhandler.ps1
@@ -0,0 +1,254 @@
+#!powershell
+
+# Copyright: (c) 2018, id27182
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#Requires -Module Ansible.ModuleUtils.Legacy
+#AnsibleRequires -OSVersion 6.2
+
+$ErrorActionPreference = "Stop"
+
+$params = Parse-Args -arguments $args -supports_check_mode $true
+$check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
+
+$allowpathinfo = Get-AnsibleParam -obj $params -name "allowpathinfo" -type "bool" -default "false"
+$modules = Get-AnsibleParam -obj $params -name "modules" -type "str" -default "ManagedPipelineHandler"
+$name = Get-AnsibleParam -obj $params -name "name" -type "str" -failifempty $true
+$path = Get-AnsibleParam -obj $params -name "path" -type "str" -failifempty $true
+$precondition = Get-AnsibleParam -obj $params -name "precondition" -type "str" -validateset 'bitness32', 'bitness64', 'integratedMode', 'ISAPIMode', 'runtimeVersionv1.1', 'runtimeVersionv2.0'
+$requireaccess = Get-AnsibleParam -obj $params -name "requireaccess" -type "str" -default "Script" -validateset 'None', 'Read', 'Write', 'Script', 'Execute'
+$resourcetype = Get-AnsibleParam -obj $params -name "resourcetype" -type "str" -default "Unspecified" -validateset 'Directory', 'Either', 'File', 'Script', 'Unspecified'
+$responsebufferlimit = Get-AnsibleParam -obj $params -name "responsebufferlimit" -type "int" -default "4194304"
+$scriptprocessor = Get-AnsibleParam -obj $params -name "scriptprocessor" -type "path"
+$type = Get-AnsibleParam -obj $params -name "type" -type "str"
+$verb = Get-AnsibleParam -obj $params -name "verb" -type "str" -failifempty $true
+$applicationname = Get-AnsibleParam -obj $params -name "applicationname" -type "str" -default ""
+$sitename = Get-AnsibleParam -obj $params -name "sitename" -type "str" -default ""
+$state = Get-AnsibleParam -obj $params -name "state" -type "str" -default "present" -validateset "absent","present"
+
+$result = @{
+    changed = $false
+}
+
+# Ensure WebAdministration PS module is installed
+if (!(Get-Module -Name WebAdministration -ListAvailable)) {
+    Fail-Json -obj $result -message "win_iis_webhandler requires WebAdministration PS module to be installed."
+}
+Import-Module -Name WebAdministration
+
+# Define IIS path and Location tag
+if (!$sitename -and !$applicationname) {
+    $iispath = "IIS:\"
+    $location = $null
+}
+elseif ($sitename -and !$applicationname) {
+    if (!(Get-Website -Name $sitename)) {
+        Fail-Json -obj $result -message "Specified site '$sitename' does not exist. Check site name and execute module again."
+    }
+
+    $iispath = "IIS:\Sites\$sitename"
+    $location = $sitename
+}
+elseif (!$sitename -and $applicationname) {
+    Fail-Json -obj $result -message "Application name '$applicationname' was specified without specifying site name. Please, specify both application name and site name and execute module again."
+}
+else {
+    if (!(Get-Website -Name $sitename)) {
+        Fail-Json -obj $result -message "Specified site '$sitename' does not exist. Check site name and execute module again."
+    }
+    else {
+        if (!(Get-WebApplication -Site $sitename -Name $applicationname)) {
+            Fail-Json -obj $result -message "Specified application '$applicationname' does not exist under site '$sitename'. Check application name and execute module again."
+        }
+    }
+
+    $iispath = "IIS:\Sites\$sitename\$applicationname"
+    $location = "$sitename\$applicationname"
+}
+
+# Ensure path to hanlder' scriptprocessor exist
+if ($scriptprocessor) {
+    if ((Test-Path -Path $scriptprocessor -PathType Leaf) -eq $false) {
+        Fail-Json -obj $result -message "Path to handler scriptprocessor '$scriptprocessor' does not exist. Check path to handler' scriptprocessor and run module again."
+    }
+}
+
+$specified_attributes = @{
+    Name                = $name
+    Path                = $path
+    Verb                = $verb
+    Type                = $type
+    Modules             = $modules
+    ScriptProcessor     = $scriptProcessor
+    ResourceType        = $resourcetype
+    RequireAccess       = $requireaccess
+    AllowPathInfo       = $allowpathinfo
+    PreCondition        = $precondition
+    ResponseBufferLimit = [uint32]$responsebufferlimit
+}
+
+# IIS path to display in messages
+if ($iispath -eq "IIS:\" -and !$location) {
+    $iispath_for_messages = "root"
+}
+else {
+    $iispath_for_messages = $iispath
+}
+
+#Get current status of web handler
+try {
+    $currentwebhandler = Get-WebHandler -Name $name -PSPath $iispath
+
+}
+catch {
+    $ErrorMessage = "Failed to get info about webhandler '$name' in IIS on '$iispath_for_messages' path: $($_.Exception.Message)"
+    Fail-Json -obj $result -message $ErrorMessage
+}
+
+#Check and set desired state
+if ($state -eq 'present') {
+    if (!$currentwebhandler) {
+        #Get rid of $null values
+        $attributes_to_set = @{}
+        $specified_attributes.GetEnumerator() | ForEach-Object -Process {
+            if ($_.Value) {
+                $attributes_to_set.add($_.Key, $_.Value)
+            }
+        }
+
+        try {
+            if ($iispath -eq "IIS:\" -and !$location) {
+                Add-WebConfigurationProperty -Filter 'system.webServer/handlers' -PSPath $iispath -Name . -Value $attributes_to_set -WhatIf:$check_mode
+            }
+            else {
+                Add-WebConfigurationProperty -Filter 'system.webServer/handlers' -PSPath "IIS:\" -Name . -Value $attributes_to_set -Location $location -WhatIf:$check_mode
+            }
+
+            Add-Warning -obj $result -message "New webhandler '$name' was added to IIS on '$iispath_for_messages' path."
+            $result.changed = $true
+        }
+        catch {
+            $ErrorMessage = "Failed to create new webhandler '$name' in IIS on '$iispath_for_messages' path: $($_.Exception.Message)"
+            Fail-Json -obj $result -message $ErrorMessage
+        }
+    }
+    else {
+        $current_webhandler_attributes = @{
+            Name                = $currentwebhandler.name
+            Path                = $currentwebhandler.path
+            Verb                = $currentwebhandler.verb
+            Type                = $currentwebhandler.type
+            Modules             = $currentwebhandler.modules
+            ScriptProcessor     = $currentwebhandler.scriptProcessor
+            ResourceType        = $currentwebhandler.resourcetype
+            RequireAccess       = $currentwebhandler.requireaccess
+            AllowPathInfo       = $currentwebhandler.allowpathinfo
+            PreCondition        = $currentwebhandler.precondition
+            ResponseBufferLimit = $currentwebhandler.responsebufferlimit
+        }
+
+        $filter = "system.webServer/handlers/Add[@Name='$name']"
+        $in_desired_state = $true
+        foreach ($key in $current_webhandler_attributes.GetEnumerator()) {
+            $keyName = $key.Name
+            if ($specified_attributes[$keyName] -ne $current_webhandler_attributes[$keyName]) {   
+                if (!$specified_attributes[$keyName] -and !$current_webhandler_attributes[$keyName]) {
+                    Continue
+                }
+                elseif (($specified_attributes[$keyName] -and $current_webhandler_attributes[$keyName]) -or ($specified_attributes[$keyName] -and !$current_webhandler_attributes[$keyName])) {
+                    $in_desired_state = $false
+                    try {
+                        if ($iispath -eq "IIS:\" -and !$location) {
+                            Set-WebConfigurationProperty -Filter $filter -PSPath $iispath -Name $keyName -Value $specified_attributes[$keyName] -WhatIf:$check_mode
+                        }
+                        else {
+                            Set-WebConfigurationProperty -Filter $filter -PSPath "IIS:\" -Name $keyName -Value $specified_attributes[$keyName] -Location $Location -WhatIf:$check_mode
+                        }
+
+                        Add-Warning -obj $result -message "Existing webhandler's '$name' property '$keyName' was edited in IIS on '$iispath_for_messages' path to be in desired state."
+                        $result.changed = $true
+                    }
+                    catch {
+                        $ErrorMessage = "Failed to set desired state for existing webhandler's '$name' for property '$keyName' in IIS on '$iispath_for_messages' path: $($_.Exception.Message)"
+                        Fail-Json -obj $result -message $ErrorMessage
+                    } 
+                }
+                else {
+                    $in_desired_state = $false
+                    try {
+                        if ($iispath -eq "IIS:\" -and !$location) {
+                            Clear-WebConfiguration -Filter "$filter/@$keyName" -PSPath $iispath -WhatIf:$check_mode
+                        }
+                        else {
+                            Clear-WebConfiguration -Filter "$filter/@$keyName" -PSPath "IIS:\" -Location $Location -WhatIf:$check_mode
+                        }
+
+                        Add-Warning -obj $result -message "Existing webhandler's '$name' property '$keyName' was cleared in IIS on '$iispath_for_messages' path to be in desired state."
+                        $result.changed = $true
+                    }
+                    catch {
+                        $ErrorMessage = "Failed to clear existing webhandler's '$name' property '$keyName' in IIS on '$iispath_for_messages' path: $($_.Exception.Message)"
+                        Fail-Json -obj $result -message $ErrorMessage
+                    }
+                }
+            }
+        }
+
+        if ($in_desired_state -eq $true) {
+            Add-Warning -obj $result -message "Existing webhandler '$name' in IIS on '$iispath_for_messages' path already in desired state."
+        }
+    }
+}
+else {
+    if ($currentwebhandler) {
+        try {
+            if ($iispath -eq "IIS:\" -and !$location) {
+                Remove-WebHandler -Name $name -PSPath $iispath -Confirm:$false -WhatIf:$check_mode
+            }
+            else {
+                Remove-WebHandler -Name $name -PSPath "IIS:\" -Location $Location -Confirm:$false -WhatIf:$check_mode
+            }
+
+            Add-Warning -obj $result -message "Webhandler '$name' was removed from IIS on '$iispath_for_messages' path."
+            $result.changed = $true
+        }
+        catch {
+            $ErrorMessage = "Failed to remove webhandler '$name' from IIS on '$iispath_for_messages' path: $($_.Exception.Message)"
+            Fail-Json -obj $result -message $ErrorMessage
+        }
+    }
+    else {
+        Add-Warning -obj $result -message "Webhandler '$name' is already removed from IIS on '$iispath_for_messages' path."
+    }
+}
+
+# Result
+try {
+    $webhandler = Get-WebHandler -Name $name -PSPath $iispath
+
+}
+catch {
+    $ErrorMessage = "Failed to get info about webhandler '$name' in IIS on '$iispath_for_messages' path (during creating the result object): $($_.Exception.Message)"
+    Fail-Json -obj $result -message $ErrorMessage
+}
+
+if ($webhandler) {
+    $result.name = $webhandler.name
+    $result.path = $webhandler.path
+    $result.verb = $webhandler.verb
+    $result.type = $webhandler.type
+    $result.modules = $webhandler.modules
+    $result.scriptprocessor = $webhandler.scriptProcessor
+    $result.resourcetype = $webhandler.resourcetype
+    $result.requireaccess = $webhandler.requireaccess
+    $result.allowpathinfo = $webhandler.allowpathinfo
+    $result.precondition = $webhandler.precondition
+    $result.responsebufferlimit = $webhandler.responsebufferlimit
+    $result.state = "present"
+}
+else {
+    $result.name = $name
+    $result.state = "absent"
+}
+
+Exit-Json -obj $result
---
+++ lib/ansible/modules/windows/win_iis_webhandler.py
@@ -0,0 +1,204 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2018, Ansible, inc
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = r'''
+---
+module: win_iis_webhandler
+short_description: Configures a IIS HTTP Handler
+description:
+- Creates, removes and configures IIS HTTP Handler.
+version_added: '2.8'
+options:
+  applicationname:
+    description:
+    - Specifies name of web application where you want to create or edit HTTP Handler.
+      This must be used with valid site name via I(sitename) parameter.
+    type: str
+  allowpathinfo:
+    description:
+    - Specifies whether the handler processes full path information in a URI,
+      such as contoso/marketing/imageGallery.aspx. If the value is true, the handler
+      processes the full path, contoso/marketing/imageGallery. If the value is false,
+      the handler processes only the last section of the path, /imageGallery.
+    type: bool
+    default: 'false'
+  modules:
+    description:
+    - Specifies the name of the module or modules to which you want to map the file name
+      or file name with extension. If you specify more than one value, separate the values with a comma ','. 
+    type: str
+    default: 'ManagedPipelineHandler'
+  name:
+    description:
+    - Specifies the unique name of the handler.
+    type: str
+    required: yes
+  path:
+    description:
+    - Specifies the file name or the file name extension for which the handler applies.
+    - For example, to process all PHP files you should use '*.php' path value.
+    - Whether to use full path or just last section of the path depends on I(allowpathinfo) parameter value.
+    type: str
+    required: yes
+  precondition:
+    description:
+    - Specifies conditions under which the handler will run.
+    - The preCondition attribute can be one or more of the following possible values.
+      If you specify more than one value, separate the values with a comma ','.
+    - If no value for I(precondition) parameter specified resulted value will be NULL.
+    type: str
+    choices: [ bitness32, bitness64, integratedMode, ISAPIMode, runtimeVersionv1.1, runtimeVersionv2.0 ]
+  requireaccess:
+    description:
+    - Specifies the type of access that a handler requires to the resource.
+    - The requireAccess attribute can be one or more of the following possible values.
+      If you specify more than one value, separate the values with a comma ','.
+    type: str
+    default: 'Script'
+    choices: [ None, Read, Write, Script, Execute ]
+  resourcetype:
+    description:
+    - Specifies the type of resource to which the handler applies.
+    type: str
+    default: 'Unspecified'
+    choices: [ Directory, Either, File, Script, Unspecified ]
+  responsebufferlimit:
+    description:
+    - Specifies the maximum size, in bytes, of the response buffer for a request handler.
+    type: int
+    default: '4194304'
+  scriptprocessor:
+    description:
+    - Specifies the physical path of the ISAPI extension .dll file or
+      Common Gateway Interface 'CGI' .exe file that processes the request.
+    - The scriptProcessor attribute is required only for script map handler mappings.
+      When you map a handler to an ISAPI extension, you must specify ISAPIModule for
+      the I(modules) attribute.
+      When you map a handler to a CGI file, you must specify CGIModule for the I(modules) attribute.
+    type: path
+  sitename:
+    description:
+    - Specifies name of web site where you want to create or edit HTTP Handler.
+      It can be used with valid web application which name specified via I(applicationname) parameter.
+    type: str
+  state:
+    description:
+    - When C(present), creates or updates HTTP Handler.
+      When C(absent), removes the HTTP Handler if it exists.
+    type: str
+    choices: [ absent, present ]
+    default: present
+  type:
+    description:
+    - Specifies the namespace path of a managed handler.
+      The type attribute is required only for managed handlers.
+      If you want to specify several values, separate the values with a comma ','.
+    - Examples of managed handler types are 'System.Web.HttpForbiddenHandler'
+      or 'System.Web.Script.Services.ScriptHandlerFactory'.
+    type: str
+  verb:
+    description:
+    - Specifies the HTTP verbs for which the handler mapping applies.
+    - If you want to specify all verbs, use '*' character.
+      If you want to specify several verbs, separate the values with a comma ',',
+      .e.g. 'GET,POST'.
+    type: str
+    required: yes
+notes:
+- This must be run on a host that has the WebAdministration powershell module installed.
+- Works with Windows Server 2012 and newer.
+- All parameter' description and other techical information was taken from U(https://docs.microsoft.com/en-us/iis/configuration/system.webserver/handlers/add) article.
+author:
+- Andrii Bilousko (@arestarh)
+'''
+
+EXAMPLES = r'''
+- name: Create http handler to process php files
+  win_iis_webhandler:
+    name: 'php-fastcgi'
+    scriptprocessor: 'C:\ProgramData\php\php-cgi.exe'
+    modules: FastCgiModule
+    path: '*.php'
+    verb: '*'
+    state: present
+
+- name: Adds a handler named "testHandler" to the Default Web Site
+  win_iis_webhandler:
+    name: testHandler
+    modules: IsapiModule
+    path: '*.test'
+    verb: 'GET,POST'
+    sitename: 'Default Web Site'
+    state: present
+'''
+
+RETURN = r'''
+allowpathinfo:
+    description: Whether the handler process full path in URI or just last section
+    returned: hanlder exists
+    type: boolean
+    sample: true
+modules:
+    description: Name of the module or modules to which the file name
+    or file name with extension is mapped.
+    returned: hanlder exists
+    type: string
+    sample: ManagedPipelineHandler
+name:
+    description: Name of the handler to create, edit or remove
+    returned: always
+    type: string
+    sample: php-handler
+path:
+    description: File name or the file name extension for which the handler applies
+    returned: hanlder exists
+    type: string
+    sample: *.py
+precondition:
+    description: Conditions under which the handler will run
+    returned: hanlder exists
+    type: string
+    sample: integratedMode
+requireaccess:
+    description: Type of access that a handler requires to the resource
+    returned: hanlder exists
+    type: string
+    sample: Write
+resourcetype:
+    description: Type of resource to which the handler applies
+    returned: hanlder exists
+    type: string
+    sample: Directory
+responsebufferlimit:
+    description: Maximum size of the response buffer for handler
+    returned: hanlder exists
+    type: int
+    sample: 4194304
+scriptprocessor:
+    description: Physical path to .dll or .exe file that processes the request
+    returned: hanlder exists
+    type: string
+    sample: C:\ProgramData\php\php-cgi.exe
+state:
+    description: The state of the handler
+    returned: always
+    type: string
+    sample: present
+type:
+    description: Namespace path of a managed handler
+    returned: hanlder exists
+    type: string
+    sample: System.Web.Script.Services.ScriptHandlerFactory
+verb:
+    description: HTTP verbs for which the handler mapping applies
+    returned: hanlder exists
+    type: string
+    sample: OPTIONS
+'''
