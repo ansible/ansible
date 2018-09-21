fb44ab2346e35c43313a5c7e8fb327745e610315
ptux <ooocamel@icloud.com>
2018-09-21 15:46:00 +0900

    create a new module, not refacting of route53

--- lib/ansible/modules/cloud/amazon/route53_record.py
+++ lib/ansible/modules/cloud/amazon/route53_record.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-# Copyright: (c) Ansible Project
 # Copyright: (c) 2018, Shuang Wang <ooocamel@icloud.com>
-
-# This code refactors the module route53.py of Ansible in order to support boto3.
-# Name this module [route53_record] for the consistency with other route53_xxx modules.
-
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
@@ -33,93 +27,33 @@ requirements:
 options:
   state:
     description:
-      - Specifies the state of the resource record. As of Ansible 2.4, the I(command) option has been changed
-        to I(state) as default and the choices 'present' and 'absent' have been added, but I(command) still works as well.
+      - Specifies the state of the resource record.
     required: true
-    aliases: [ 'command' ]
-    choices: [ 'present', 'absent', 'get', 'create', 'delete' ]
-  zone:
+    choices: [ 'present', 'absent' ]
+  hosted_zone:
     description:
       - The DNS zone to modify
     required: true
   hosted_zone_id:
     description:
       - The Hosted Zone ID of the DNS zone to modify
-  record:
+  record_set_name:
     description:
       - The full DNS record to create or delete
     required: true
-  ttl:
+  record_set_ttl:
     description:
       - The TTL(seconds) to give the new record
     type: int
     default: 3600
-  type:
+  record_set_type:
     description:
       - The type of DNS record to create
     required: true
     choices: [ 'A', 'CNAME', 'MX', 'AAAA', 'TXT', 'PTR', 'SRV', 'SPF', 'CAA', 'NS', 'SOA' ]
-  alias:
+  record_set_value:
     description:
-      - Indicates if this is an alias record.
-    type: bool
-    default: 'no'
-  alias_hosted_zone_id:
-    description:
-      - The hosted zone identifier.
-  alias_evaluate_target_health:
-    description:
-      - Whether or not to evaluate an alias target health. Useful for aliases to Elastic Load Balancers.
-    type: bool
-    default: no
-  value:
-    description:
-      - The new value when creating a DNS record.  YAML lists or multiple comma-spaced values are allowed for non-alias records.
-      - When deleting a record all values for the record must be specified or Route53 will not delete it.
-  overwrite:
-    description:
-      - Whether an existing record should be overwritten on create if values do not match
-    type: bool
-  retry_interval:
-    description:
-      - In the case that route53 is still servicing a prior request, this module will wait and try again after this many seconds. If you have many
-        domain names, the default of 500 seconds may be too long.
-    default: 500
-  private_zone:
-    description:
-      - If set to C(yes), the private zone matching the requested name within the domain will be used if there are both public and private zones.
-        The default is to use the public zone.
-    type: bool
-    default: 'no'
-  identifier:
-    description:
-      - Have to be specified for Weighted, latency-based and failover resource record sets only. An identifier
-        that differentiates among multiple resource record sets that have the
-        same combination of DNS name and type.
-  weight:
-    description:
-      - Weighted resource record sets only. Among resource record sets that
-        have the same combination of DNS name and type, a value that
-        determines what portion of traffic for the current resource record set
-        is routed to the associated location.
-  region:
-    description:
-      - Latency-based resource record sets only Among resource record sets
-        that have the same combination of DNS name and type, a value that
-        determines which region this should be associated with for the
-        latency-based routing
-  health_check:
-    description:
-      - Health check to associate with this record
-  failover:
-    description:
-      - Failover resource record sets only. Whether this is the primary or
-        secondary resource record set. Allowed values are PRIMARY and SECONDARY
-    choices: [PRIMARY, SECONDARY]
-  vpc_id:
-    description:
-      - "When used in conjunction with private_zone: true, this will only modify records in the private hosted zone attached to this VPC."
-      - This allows you to have multiple private hosted zones, all with the same name, attached to different VPCs.
+      - The new value when creating a DNS record. 
   wait:
     description:
       - Wait until the changes have been replicated to all Amazon Route 53 DNS servers.
@@ -145,219 +79,6 @@ nameservers:
   - ns-516.awsdns-00.net.
   - ns-1504.awsdns-00.co.uk.
   - ns-1.awsdns-00.com.
-set:
-  description: info specific to the resource record
-  returned: when state is 'get'
-  type: complex
-  contains:
-    alias:
-      description: whether this is an alias
-      returned: always
-      type: bool
-      sample: false
-    failover:
-      description: ""
-      returned: always
-      type: NoneType
-      sample: null
-    health_check:
-      description: health_check associated with this record
-      returned: always
-      type: NoneType
-      sample: null
-    identifier:
-      description: ""
-      returned: always
-      type: NoneType
-      sample: null
-    record:
-      description: domain name for the record set
-      returned: always
-      type: string
-      sample: new.foo.com.
-    region:
-      description: ""
-      returned: always
-      type:
-      sample:
-    ttl:
-      description: resource record cache TTL
-      returned: always
-      type: string
-      sample: '3600'
-    type:
-      description: record set type
-      returned: always
-      type: string
-      sample: A
-    value:
-      description: value
-      returned: always
-      type: string
-      sample: 52.43.18.27
-    values:
-      description: values
-      returned: always
-      type: list
-      sample:
-      - 52.43.18.27
-    weight:
-      description: weight of the record
-      returned: always
-      type: string
-      sample: '3'
-    zone:
-      description: zone this record set belongs to
-      returned: always
-      type: string
-      sample: foo.bar.com.
-'''
-
-EXAMPLES = '''
-# Add new.foo.com as an A record with 3 IPs and wait until the changes have been replicated
-- route53_record:
-      state: present
-      zone: foo.com
-      record: new.foo.com
-      type: A
-      ttl: 7200
-      value: 1.1.1.1,2.2.2.2,3.3.3.3
-      wait: yes
-
-# Update new.foo.com as an A record with a list of 3 IPs and wait until the changes have been replicated
-- route53_record:
-      state: present
-      zone: foo.com
-      record: new.foo.com
-      type: A
-      ttl: 7200
-      value:
-        - 1.1.1.1
-        - 2.2.2.2
-        - 3.3.3.3
-      wait: yes
-
-# Retrieve the details for new.foo.com
-- route53_record:
-      state: get
-      zone: foo.com
-      record: new.foo.com
-      type: A
-  register: rec
-
-# Delete new.foo.com A record using the results from the get command
-- route53_record:
-      state: absent
-      zone: foo.com
-      record: "{{ rec.set.record }}"
-      ttl: "{{ rec.set.ttl }}"
-      type: "{{ rec.set.type }}"
-      value: "{{ rec.set.value }}"
-
-# Add an AAAA record.  Note that because there are colons in the value
-# that the IPv6 address must be quoted. Also shows using the old form command=create.
-- route53_record:
-      command: create
-      zone: foo.com
-      record: localhost.foo.com
-      type: AAAA
-      ttl: 7200
-      value: "::1"
-
-# Add a SRV record with multiple fields for a service on port 22222
-# For more information on SRV records see:
-# https://en.wikipedia.org/wiki/SRV_record
-- route53_record:
-      state: present
-      zone: foo.com
-      record: "_example-service._tcp.foo.com"
-      type: SRV
-      value: "0 0 22222 host1.foo.com,0 0 22222 host2.foo.com"
-
-# Add a TXT record. Note that TXT and SPF records must be surrounded
-# by quotes when sent to Route 53:
-- route53_record:
-      state: present
-      zone: foo.com
-      record: localhost.foo.com
-      type: TXT
-      ttl: 7200
-      value: '"bar"'
-
-# Add an alias record that points to an Amazon ELB:
-- route53_record:
-      state: present
-      zone: foo.com
-      record: elb.foo.com
-      type: A
-      value: "{{ elb_dns_name }}"
-      alias: True
-      alias_hosted_zone_id: "{{ elb_zone_id }}"
-
-# Retrieve the details for elb.foo.com
-- route53_record:
-      state: get
-      zone: foo.com
-      record: elb.foo.com
-      type: A
-  register: rec
-
-# Delete an alias record using the results from the get command
-- route53_record:
-      state: absent
-      zone: foo.com
-      record: "{{ rec.set.record }}"
-      ttl: "{{ rec.set.ttl }}"
-      type: "{{ rec.set.type }}"
-      value: "{{ rec.set.value }}"
-      alias: True
-      alias_hosted_zone_id: "{{ rec.set.alias_hosted_zone_id }}"
-
-# Add an alias record that points to an Amazon ELB and evaluates it health:
-- route53_record:
-    state: present
-    zone: foo.com
-    record: elb.foo.com
-    type: A
-    value: "{{ elb_dns_name }}"
-    alias: True
-    alias_hosted_zone_id: "{{ elb_zone_id }}"
-    alias_evaluate_target_health: True
-
-# Add an AAAA record with Hosted Zone ID.
-- route53_record:
-      state: present
-      zone: foo.com
-      hosted_zone_id: Z2AABBCCDDEEFF
-      record: localhost.foo.com
-      type: AAAA
-      ttl: 7200
-      value: "::1"
-
-# Use a routing policy to distribute traffic:
-- route53_record:
-      state: present
-      zone: foo.com
-      record: www.foo.com
-      type: CNAME
-      value: host1.foo.com
-      ttl: 30
-      # Routing policy
-      identifier: "host1@www"
-      weight: 100
-      health_check: "d994b780-3150-49fd-9205-356abdd42e75"
-
-# Add a CAA record (RFC 6844):
-- route53_record:
-      state: present
-      zone: example.com
-      record: example.com
-      type: CAA
-      value:
-        - 0 issue "ca.example.net"
-        - 0 issuewild ";"
-        - 0 iodef "mailto:security@example.com"
-
 '''
 
 try:
@@ -384,11 +105,13 @@ class AWSRoute53Record(object):
         self._results = results
         self._connection = self._module.client('route53')
         self._check_mode = self._module.check_mode
-        self.warnings = []
+
+    def process(self):
+        zone = self._get_zone_by_name()
 
     def _get_zone_by_id(self, zone_id=None):
         """gets a zone by id"""
-        zone = None
+        hosted_zone = None
         if self._module.params['hosted_zone_id'] is not None:
             hosted_zone_id = self._module.params['hosted_zone_id']
         else:
@@ -396,35 +119,34 @@ class AWSRoute53Record(object):
 
         if hosted_zone_id is not None:
             try:
-                zone = self._connection.get_hosted_zone(Id=hosted_zone_id)
-                zone_name = zone['HostedZone']['Name'].rstrip('.')
-                if not self._module.params['zone'] == zone_name:
-                    self._module.fail_json(msg="hosted zone id not matches zone name")
-                if zone is None:
-                    self._module.fail_json(msg="zone id not exists: %s" % hosted_zone_id)
+                hosted_zone = self._connection.get_hosted_zone(Id=hosted_zone_id)
+                hosted_zone_name = hosted_zone['HostedZone']['Name'].rstrip('.')
+                if not self._module.params['hosted_zone_name'] == hosted_zone_name:
+                    self._module.fail_json(msg="hosted_zone_id not matches hosted_zone_name")
+                if hosted_zone is None:
+                    self._module.fail_json(msg="hosted zone id not exists: %s" % hosted_zone_id)
             except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
-                self._module.fail_json_aws(e, msg="couldn't get zone by hosted zone id")
+                self._module.fail_json_aws(e, msg="couldn't get hosted zone by hosted_zone_id")
         else:
             self._module.fail_json(msg="hosted zone id is requied.")
-        self._module.fail_json(msg=zone)
-        return zone
+        return hosted_zone
 
     def _get_zone_by_name(self):
         """gets a zone by name"""
         try:
-            zone_name = self._module.params['zone']
+            hosted_zone_name = self._module.params['hosted_zone_name']
             hosted_zones = self._connection.list_hosted_zones().get('HostedZones', [])
-            zone = None
+            hosted_zone = None
             for dic in hosted_zones:
-                if dic.get('Name').rstrip('.') == zone_name:
-                    zone_id = dic.get('Id')
-                    zone = self._get_zone_by_id(zone_id)
-            if zone is None:
-                self._module.fail_json(msg="zone name not exists: %s" % zone_name)
+                if dic.get('Name').rstrip('.') == hosted_zone_name:
+                    hosted_zone_id = dic.get('Id')
+                    hosted_zone = self._get_zone_by_id(hosted_zone_id)
+            if hosted_zone is None:
+                self._module.fail_json(msg="hosted zone name not exists: %s" % zone_name)
         except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
-            self._module.fail_json_aws(e, msg="couldn't get zone by hosted zone name")
+            self._module.fail_json_aws(e, msg="couldn't get hosted zone by hosted_zone_name")
 
-        return zone
+        return hosted_zone
 
     def _create_record(self):
         #      self._connection.change_resource_record_sets(**kwargs)
@@ -441,64 +163,42 @@ class AWSRoute53Record(object):
     def _delete_record(self):
         pass
 
-    def ensure_present(self):
+    def _ensure_present(self):
         pass
 
-    def ensure_absent(self):
+    def _ensure_absent(self):
         pass
-
-    def process(self):
-        zone = self._get_zone_by_id()
-#        zone = self._get_zone_by_name()
 
 
 def main():
     argument_spec = ec2_argument_spec()
     argument_spec.update(dict(
-        state=dict(aliases=['command'], choices=['present', 'absent', 'get', 'create', 'delete'], required=True),
-        zone=dict(required=True),
+        hosted_zone_name=dict(required=True),
         hosted_zone_id=dict(required=False, default=None),
-        record=dict(required=True),
-        ttl=dict(required=False, type='int', default=3600),
-        type=dict(choices=['A', 'CNAME', 'MX', 'AAAA', 'TXT', 'PTR', 'SRV', 'SPF', 'CAA', 'NS', 'SOA'], required=True),
-        alias=dict(required=False, type='bool'),
-        alias_hosted_zone_id=dict(required=False),
-        alias_evaluate_target_health=dict(required=False, type='bool', default=False),
-        value=dict(required=False, type='list'),
-        overwrite=dict(required=False, type='bool'),
-        retry_interval=dict(required=False, default=500),
-        private_zone=dict(required=False, type='bool', default=False),
-        identifier=dict(required=False, default=None),
-        weight=dict(required=False, type='int'),
-        health_check=dict(required=False),
-        failover=dict(required=False, choices=['PRIMARY', 'SECONDARY']),
-        vpc_id=dict(required=False),
+        record_set_name=dict(required=True),
+        record_set_type=dict(choices=['A', 'CNAME', 'MX', 'AAAA', 'TXT', 'PTR', 'SRV', 'SPF', 'CAA', 'NS', 'SOA'], required=True),
+        record_set_ttl=dict(required=False, type='int', default=3600),
+        record_set_value=dict(required=False, type='list'),
+        state=dict(choices=['present', 'absent'], required=True),
         wait=dict(required=False, type='bool', default=False),
-        wait_timeout=dict(required=False, type='int', default=300),
+        wait_timeout=dict(required=False, type='int', default=300)
     ))
 
-    required_if = [('state', 'present', ['value']),
-                   ('state', 'absent', ['value']),
-                   ('state', 'delete', ['value']),
-                   ('state', 'create', ['value'])]
-    required_together = [['alias', 'alias_hosted_zone_id']]
-    mutually_exclusive = [('failover', 'region', 'weight')]
+    required_if = [('state', 'present', ['record_set_value']),
+                   ('state', 'absent', ['record_set_value']),
+                   ('state', 'delete', ['record_set_value']),
+                   ('state', 'create', ['record_set_value'])]
 
     module = AnsibleAWSModule(
         argument_spec=argument_spec,
         required_if=required_if,
-        required_together=required_together,
-        mutually_exclusive=mutually_exclusive,
         supports_check_mode=True
     )
 
     results = dict(changed=False)
-
     record_controller = AWSRoute53Record(module=module, results=results)
     record_controller.process()
-
     module.exit_json(**results)
-
 
 if __name__ == '__main__':
     main()
