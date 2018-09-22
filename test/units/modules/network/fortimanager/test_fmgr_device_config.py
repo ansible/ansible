a2a6420f63a16bc8462c00b94dfd0190ae617d26
Luke Weighall <lweighall@gmail.com>
2018-09-21 22:52:39 -0700

    fmgr_fwobj_address PR candidate

--- test/units/modules/network/fortimanager/test_fmgr_device_config.py
+++ test/units/modules/network/fortimanager/test_fmgr_device_config.py
@@ -1,19 +1,17 @@
-# (c) 2016 Red Hat Inc.
+# Copyright 2018 Fortinet, Inc.
 #
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Ansible is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# along with Ansible.  If not, see <https://www.gnu.org/licenses/>.
 
 # Make coding more python3-ish
 from __future__ import (absolute_import, division, print_function)
@@ -27,7 +25,9 @@ import pytest
 try:
     from ansible.modules.network.fortimanager import fmgr_device_config
 except ImportError:
-    pytest.skip("Could not load required modules for testing", allow_module_level=True)
+    pytest.skip(
+        "Could not load required modules for testing",
+        allow_module_level=True)
 
 fmg_instance = FortiManager("1.1.1.1", "admin", "")
 
@@ -49,12 +49,23 @@ def fixture_data(request):
     return request.param.get(func_name, None)
 
 
-
 def test_update_device_hostname(fixture_data, mocker):
-    mocker.patch("pyFMG.fortimgr.FortiManager._post_request", side_effect=fixture_data)
-    paramgram_used = {'adom': 'ansible', 'install_config': 'disable', 'device_unique_name': 'FGT1', 'interface': None, 'device_hostname': 'ansible-fgt01', 'interface_ip': None, 'interface_allow_access': None, 'mode': 'update'}
-    output = fmgr_device_config.update_device_hostname(fmg_instance, paramgram_used)
-    ##################################################
+    mocker.patch(
+        "pyFMG.fortimgr.FortiManager._post_request",
+        side_effect=fixture_data)
+
+    paramgram_used = {
+        'adom': 'ansible',
+        'install_config': 'disable',
+        'device_unique_name': 'FGT1',
+        'interface': None,
+        'device_hostname': 'ansible-fgt01',
+        'interface_ip': None,
+        'interface_allow_access': None,
+        'mode': 'update'}
+    output = fmgr_device_config.update_device_hostname(
+        fmg_instance, paramgram_used)
+    #
     # adom: ansible
     # install_config: disable
     # device_unique_name: FGT1
@@ -63,10 +74,20 @@ def test_update_device_hostname(fixture_data, mocker):
     # interface_ip: None
     # interface_allow_access: None
     # mode: update
-    ##################################################
-    assert output['raw_response']['status']['code'] == 0    paramgram_used = {'adom': 'ansible', 'interface': None, 'device_unique_name': 'FGT1', 'install_config': 'disable', 'device_hostname': 'ansible-fgt01', 'interface_ip': None, 'interface_allow_access': None, 'mode': 'update'}
-    output = fmgr_device_config.update_device_hostname(fmg_instance, paramgram_used)
-    ##################################################
+    #
+    assert output['raw_response']['status']['code'] == 0
+    paramgram_used = {
+        'adom': 'ansible',
+        'interface': None,
+        'device_unique_name': 'FGT1',
+        'install_config': 'disable',
+        'device_hostname': 'ansible-fgt01',
+        'interface_ip': None,
+        'interface_allow_access': None,
+        'mode': 'update'}
+    output = fmgr_device_config.update_device_hostname(
+        fmg_instance, paramgram_used)
+    #
     # adom: ansible
     # interface: None
     # device_unique_name: FGT1
@@ -75,10 +96,20 @@ def test_update_device_hostname(fixture_data, mocker):
     # interface_ip: None
     # interface_allow_access: None
     # mode: update
-    ##################################################
-    assert output['raw_response']['status']['code'] == 0    paramgram_used = {'adom': 'ansible', 'interface': None, 'device_unique_name': 'FGT2', 'install_config': 'disable', 'device_hostname': 'ansible-fgt02', 'interface_ip': None, 'interface_allow_access': None, 'mode': 'update'}
-    output = fmgr_device_config.update_device_hostname(fmg_instance, paramgram_used)
-    ##################################################
+    #
+    assert output['raw_response']['status']['code'] == 0
+    paramgram_used = {
+        'adom': 'ansible',
+        'interface': None,
+        'device_unique_name': 'FGT2',
+        'install_config': 'disable',
+        'device_hostname': 'ansible-fgt02',
+        'interface_ip': None,
+        'interface_allow_access': None,
+        'mode': 'update'}
+    output = fmgr_device_config.update_device_hostname(
+        fmg_instance, paramgram_used)
+    #
     # adom: ansible
     # interface: None
     # device_unique_name: FGT2
@@ -87,10 +118,20 @@ def test_update_device_hostname(fixture_data, mocker):
     # interface_ip: None
     # interface_allow_access: None
     # mode: update
-    ##################################################
-    assert output['raw_response']['status']['code'] == 0    paramgram_used = {'adom': 'ansible', 'interface': None, 'device_unique_name': 'FGT3', 'install_config': 'disable', 'device_hostname': 'ansible-fgt03', 'interface_ip': None, 'interface_allow_access': None, 'mode': 'update'}
-    output = fmgr_device_config.update_device_hostname(fmg_instance, paramgram_used)
-    ##################################################
+    #
+    assert output['raw_response']['status']['code'] == 0
+    paramgram_used = {
+        'adom': 'ansible',
+        'interface': None,
+        'device_unique_name': 'FGT3',
+        'install_config': 'disable',
+        'device_hostname': 'ansible-fgt03',
+        'interface_ip': None,
+        'interface_allow_access': None,
+        'mode': 'update'}
+    output = fmgr_device_config.update_device_hostname(
+        fmg_instance, paramgram_used)
+    #
     # adom: ansible
     # interface: None
     # device_unique_name: FGT3
@@ -99,14 +140,27 @@ def test_update_device_hostname(fixture_data, mocker):
     # interface_ip: None
     # interface_allow_access: None
     # mode: update
-    ##################################################
+    #
     assert output['raw_response']['status']['code'] == 0
 
+
 def test_update_device_interface(fixture_data, mocker):
-    mocker.patch("pyFMG.fortimgr.FortiManager._post_request", side_effect=fixture_data)
-    paramgram_used = {'adom': 'ansible', 'install_config': 'disable', 'device_unique_name': 'FGT1', 'interface': 'port2', 'device_hostname': None, 'interface_ip': '10.1.1.1/24', 'interface_allow_access': 'ping, telnet, https, http', 'mode': 'update'}
-    output = fmgr_device_config.update_device_interface(fmg_instance, paramgram_used)
-    ##################################################
+    mocker.patch(
+        "pyFMG.fortimgr.FortiManager._post_request",
+        side_effect=fixture_data)
+
+    paramgram_used = {
+        'adom': 'ansible',
+        'install_config': 'disable',
+        'device_unique_name': 'FGT1',
+        'interface': 'port2',
+        'device_hostname': None,
+        'interface_ip': '10.1.1.1/24',
+        'interface_allow_access': 'ping, telnet, https, http',
+        'mode': 'update'}
+    output = fmgr_device_config.update_device_interface(
+        fmg_instance, paramgram_used)
+    #
     # adom: ansible
     # install_config: disable
     # device_unique_name: FGT1
@@ -115,10 +169,20 @@ def test_update_device_interface(fixture_data, mocker):
     # interface_ip: 10.1.1.1/24
     # interface_allow_access: ping, telnet, https, http
     # mode: update
-    ##################################################
-    assert output['raw_response']['status']['code'] == 0    paramgram_used = {'adom': 'ansible', 'install_config': 'disable', 'device_unique_name': 'FGT2', 'interface': 'port2', 'device_hostname': None, 'interface_ip': '10.1.2.1/24', 'interface_allow_access': 'ping, telnet, https, http', 'mode': 'update'}
-    output = fmgr_device_config.update_device_interface(fmg_instance, paramgram_used)
-    ##################################################
+    #
+    assert output['raw_response']['status']['code'] == 0
+    paramgram_used = {
+        'adom': 'ansible',
+        'install_config': 'disable',
+        'device_unique_name': 'FGT2',
+        'interface': 'port2',
+        'device_hostname': None,
+        'interface_ip': '10.1.2.1/24',
+        'interface_allow_access': 'ping, telnet, https, http',
+        'mode': 'update'}
+    output = fmgr_device_config.update_device_interface(
+        fmg_instance, paramgram_used)
+    #
     # adom: ansible
     # install_config: disable
     # device_unique_name: FGT2
@@ -127,10 +191,20 @@ def test_update_device_interface(fixture_data, mocker):
     # interface_ip: 10.1.2.1/24
     # interface_allow_access: ping, telnet, https, http
     # mode: update
-    ##################################################
-    assert output['raw_response']['status']['code'] == 0    paramgram_used = {'adom': 'ansible', 'install_config': 'disable', 'device_unique_name': 'FGT3', 'interface': 'port2', 'device_hostname': None, 'interface_ip': '10.1.3.1/24', 'interface_allow_access': 'ping, telnet, https, http', 'mode': 'update'}
-    output = fmgr_device_config.update_device_interface(fmg_instance, paramgram_used)
-    ##################################################
+    #
+    assert output['raw_response']['status']['code'] == 0
+    paramgram_used = {
+        'adom': 'ansible',
+        'install_config': 'disable',
+        'device_unique_name': 'FGT3',
+        'interface': 'port2',
+        'device_hostname': None,
+        'interface_ip': '10.1.3.1/24',
+        'interface_allow_access': 'ping, telnet, https, http',
+        'mode': 'update'}
+    output = fmgr_device_config.update_device_interface(
+        fmg_instance, paramgram_used)
+    #
     # adom: ansible
     # install_config: disable
     # device_unique_name: FGT3
@@ -139,14 +213,26 @@ def test_update_device_interface(fixture_data, mocker):
     # interface_ip: 10.1.3.1/24
     # interface_allow_access: ping, telnet, https, http
     # mode: update
-    ##################################################
+    #
     assert output['raw_response']['status']['code'] == 0
 
+
 def test_exec_config(fixture_data, mocker):
-    mocker.patch("pyFMG.fortimgr.FortiManager._post_request", side_effect=fixture_data)
-    paramgram_used = {'adom': 'ansible', 'interface': None, 'device_unique_name': 'FGT1', 'install_config': 'enable', 'device_hostname': None, 'interface_ip': None, 'interface_allow_access': None, 'mode': 'execute'}
+    mocker.patch(
+        "pyFMG.fortimgr.FortiManager._post_request",
+        side_effect=fixture_data)
+
+    paramgram_used = {
+        'adom': 'ansible',
+        'interface': None,
+        'device_unique_name': 'FGT1',
+        'install_config': 'enable',
+        'device_hostname': None,
+        'interface_ip': None,
+        'interface_allow_access': None,
+        'mode': 'execute'}
     output = fmgr_device_config.exec_config(fmg_instance, paramgram_used)
-    ##################################################
+    #
     # adom: ansible
     # interface: None
     # device_unique_name: FGT1
@@ -155,10 +241,19 @@ def test_exec_config(fixture_data, mocker):
     # interface_ip: None
     # interface_allow_access: None
     # mode: execute
-    ##################################################
-    assert isinstance(output['raw_response'], dict) is True    paramgram_used = {'adom': 'ansible', 'install_config': 'enable', 'device_unique_name': 'FGT2, FGT3', 'interface': None, 'device_hostname': None, 'interface_ip': None, 'interface_allow_access': None, 'mode': 'execute'}
+    #
+    assert isinstance(output['raw_response'], dict) is True
+    paramgram_used = {
+        'adom': 'ansible',
+        'install_config': 'enable',
+        'device_unique_name': 'FGT2, FGT3',
+        'interface': None,
+        'device_hostname': None,
+        'interface_ip': None,
+        'interface_allow_access': None,
+        'mode': 'execute'}
     output = fmgr_device_config.exec_config(fmg_instance, paramgram_used)
-    ##################################################
+    #
     # adom: ansible
     # install_config: enable
     # device_unique_name: FGT2, FGT3
@@ -167,5 +262,5 @@ def test_exec_config(fixture_data, mocker):
     # interface_ip: None
     # interface_allow_access: None
     # mode: execute
-    ##################################################
+    #
     assert isinstance(output['raw_response'], dict) is True
