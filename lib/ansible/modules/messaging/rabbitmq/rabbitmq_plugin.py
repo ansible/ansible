bf423b5f596bea7fdc698b0aacdc15e11278dbee
Dag Wieers <dag@wieers.com>
2018-09-24 13:58:47 +0200

    BOTMETA: Move rabbitmq modules to own directory
    This makes it possible to automatically include $team_rabbitmq for new modules.

--- lib/ansible/modules/messaging/rabbitmq/rabbitmq_plugin.py
+++ lib/ansible/modules/messaging/rabbitmq/rabbitmq_plugin.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 # Copyright: (c) 2013, Chatham Financial <oss@chathamfinancial.com>
-# Copyright: (c) 2018, Ansible Project
-# Copyright: (c) 2018, Abhijeet Kasurde <akasurde@redhat.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
