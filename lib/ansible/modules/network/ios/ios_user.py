1b6721ebdc91d9675ec120b9272c23bf990a0427
akira6592 <akira6592@gmail.com>
2018-09-24 23:01:50 +0900

    The sshkey option is available in version 2.7 and above

--- lib/ansible/modules/network/ios/ios_user.py
+++ lib/ansible/modules/network/ios/ios_user.py
@@ -82,7 +82,7 @@ options:
     description:
       - Specifies the SSH public key to configure
         for the given username.  This argument accepts a valid SSH key value.
-    version_added: "2.6"
+    version_added: "2.7"
   nopassword:
     description:
       - Defines the username without assigning
