b1b5f63eef38badb511e9cca4cc3052b6ac659b5
Luke Weighall <lweighall@gmail.com>
2018-09-24 11:19:43 -0700

    PR candidate

--- lib/ansible/modules/network/fortimanager/fmgr_ha.py
+++ lib/ansible/modules/network/fortimanager/fmgr_ha.py
@@ -28,7 +28,7 @@ ANSIBLE_METADATA = {
 DOCUMENTATION = '''
 ---
 module: fmgr_ha
-version_added: "2.6"
+version_added: "2.8"
 author:
     - Luke Weighall (@lweighall)
     - Andrew Welsh (@Ghilli3)
