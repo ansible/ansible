dcde085fd25bdd48e1b79f3a022d829286788227
santascsi <39088744+santascsi@users.noreply.github.com>
2018-09-24 17:06:33 +0200

    Update cron.py (#46055)
    <!--- Your description here -->
    The documentation states the use of the "value" attribute for environment variables while this should also be the "job" attribute.
    +label: docsite_pr

--- lib/ansible/modules/system/cron.py
+++ lib/ansible/modules/system/cron.py
@@ -157,13 +157,13 @@ EXAMPLES = '''
   cron:
     name: PATH
     env: yes
-    value: /opt/bin
+    job: /opt/bin
 
 - name: Creates an entry like "APP_HOME=/srv/app" and insert it after PATH declaration
   cron:
     name: APP_HOME
     env: yes
-    value: /srv/app
+    job: /srv/app
     insertafter: PATH
 
 - name: Creates a cron file under /etc/cron.d
