9a33ab4b3ad8f0394941c6c8338abb0805a4ea96
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2018-09-28 16:14:58 +1200

    modules/systemd: fix logic: allow scope to default to 'system'
    Fix logic introduced in 7ea909418e5: if 'scope' param is not specified,
    it defaults to system, but the value of module.params['scope'] is None,
    not 'system' - so allow for that.

--- lib/ansible/modules/system/systemd.py
+++ lib/ansible/modules/system/systemd.py
@@ -443,7 +443,7 @@ def main():
                 enabled = True
             elif rc == 1:
                 # if not a user or global user service and both init script and unit file exist stdout should have enabled/disabled, otherwise use rc entries
-                if module.params['scope'] == 'system' and \
+                if module.params['scope'] in (None, 'system') and \
                         not module.params['user'] and \
                         is_initd and \
                         (not out.strip().endswith('disabled') or sysv_is_enabled(unit)):
