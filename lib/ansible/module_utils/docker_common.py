0c903a106a735572461d461d0c2313e54bbc67ef
Felix Fontein <felix@fontein.de>
2018-09-30 21:44:12 +0200

    Make compatible with Python < 3.4.

--- lib/ansible/module_utils/docker_common.py
+++ lib/ansible/module_utils/docker_common.py
@@ -120,7 +120,7 @@ if not HAS_DOCKER_PY:
 
 def is_image_name_id(name):
     """Checks whether the given image name is in fact an image ID (hash)."""
-    if re.fullmatch('sha256:[0-9a-fA-F]{64}', name):
+    if re.match('^sha256:[0-9a-fA-F]{64}$', name):
         return True
     return False
 
