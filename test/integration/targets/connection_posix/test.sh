840614200ea85a734aa0b1177b912d963e784bb2
Nathan Gaberel <nathan@gnab.fr>
2018-10-05 17:28:37 +0200

    Add docker_cwd tests.

--- test/integration/targets/connection_posix/test.sh
+++ test/integration/targets/connection_posix/test.sh
@@ -16,3 +16,14 @@ INVENTORY="../connection_${group}/test_connection.inventory" ./test.sh \
     -e local_tmp=/tmp/ansible-local \
     -e remote_tmp=/tmp/ansible-remote \
     "$@"
+
+DOCKER_VERSION=$(docker version -f '{{ .Client.Version }}')
+CWD=/var/tmp
+
+if ansible all -i localhost, -m assert -a"that={{ docker_version is version('17.09', '>=') }}" -e docker_version=$DOCKER_VERSION; then
+    OLD_DOCKER=false
+else
+    OLD_DOCKER=true
+fi
+
+ansible-playbook test_workdir.yml -i test_connection.inventory -e ansible_docker_cwd=$CWD -e old_docker=$OLD_DOCKER
