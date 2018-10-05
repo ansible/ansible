ed90d922031f08c6b39915a527bbbf09a61ff334
Micah Hunsberger <mhuns14@gmail.com>
2018-10-05 13:53:33 -0400

    Add alias support to win_hosts module (#1)
    * win_hosts supports aliases
    
    added support for adding / removing aliases from a host entry, rather than adding a new entry
    
    added ability for win_hosts to detect aliases:
    `192.168.1.1 alias1 alias2 alias3`
    ```
    win_hosts:
      host_name: alias2
      ip_address: 192.168.1.1
    ```
    will result in `192.168.1.1 alias1 alias3`
    
    also includes `replace` and `add` as options for `ip_action` (`replace` is default)
    
    for example:
    ```
    192.168.1.1 my_reused_alias
    192.168.1.2 my_reused_alias
    ```
    with
    ```
    win_hosts:
      host_name: my_reused_alias
      ip_address: 192.168.1.3
      ip_action: add
    ```
    the result will be
    ```
    192.168.1.1 my_reused_alias
    192.168.1.2 my_reused_alias
    ```
    but with `ip_action=replace` the result would be
    ```
    192.168.1.3 my_reused_alias
    ```

--- lib/ansible/modules/windows/win_hosts.ps1
+++ lib/ansible/modules/windows/win_hosts.ps1
@@ -1,6 +1,10 @@
 #!powershell
 
+# Copyright: (c) 2018, Micah Hunsberger (@mhunsber)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 #Requires -Module Ansible.ModuleUtils.Legacy
+
 Set-StrictMode -Version 2
 $ErrorActionPreference = "Stop"
 
@@ -8,9 +12,10 @@ $params = Parse-Args -arguments $args -supports_check_mode $true
 $check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
 $diff_mode = Get-AnsibleParam -obj $params -name "_ansible_diff" -type "bool" -default $false
 
-$host_name = Get-AnsibleParam -obj $params -name "host_name" -type "str" -failifempty $true
 $state = Get-AnsibleParam -obj $params -name "state" -type "str" -default "present" -validateset "absent","present"
-$ip_address = Get-AnsibleParam -obj $params -name "ip_address" -type "str" -failifempty ($state -eq 'present')
+$host_name = Get-AnsibleParam -obj $params -name "host_name" -type "str" -failifempty ($state -eq 'present')
+$ip_address = Get-AnsibleParam -obj $params -name "ip_address" -type "str" -default "" -failifempty ($state -eq 'present')
+$ip_action = Get-AnsibleParam -obj $params -name "ip_action" -type "str" -default "replace" -validateset "replace","add"
 
 # TODO: validate ip address?
 
@@ -18,101 +23,184 @@ $hosts_file = Get-Item -Path "$env:SystemRoot\System32\drivers\etc\hosts"
 
 $result = @{
     changed = $false
+    diff = @{
+        prepared = ""
+    }
 }
 
-Function Get-IndexOfHostEntry($list, [string]$value){
-    $idx = 0
-    
-    # replace regex wildcard with literal
-    $value = $value.Replace('.', "\.")
+Function Get-CommentIndex($line) {
+    $c_index = $line.IndexOf('#')
+    if($c_index -lt 0) {
+        $c_index = $line.Length
+    }
+    return $c_index
+}
 
-    ForEach($el in $list) {
-        # skip comment lines
-        if(!$el.Trim().StartsWith('#')) {
-            
-            # hostname may not match ip
-            if($el -imatch "\s*(?<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(?<h>$value)"){
-                return $idx
+Function Get-IpIndex($line, $ip) {
+    $c_index = Get-CommentIndex -line $line
+    $re = New-Object regex "\s*$($ip.Replace('.',"\."))\s"
+    $match = $re.Match($line, 0, $c_index)
+    if($match.Success) {
+        return $match.Index
+    } else {
+        return -1
+    }
+}
+
+Function Find-Hostname($line, $name) {
+    $c_idx = Get-CommentIndex -line $line
+    $re = New-Object regex ("\s+$($name.Replace('.',"\."))(\s|$)", [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
+    $match = $re.Match($line, 0, $c_idx)
+    return $match
+}
+
+Function Remove-HostEntry($list, [ref]$idx) {
+    $result.changed = $true
+
+    if($diff_mode) {
+        $result.diff.prepared += "`n-[$($list[$idx.value])]`n"
+    }
+
+    if(-not $check_mode) {
+        # don't skip any lines
+        $list.RemoveAt($idx.value)
+        $idx.value--
+    }
+
+}
+
+Function Add-HostEntry($list, $name, $ip) {
+    $result.changed = $true
+
+    $line = "$ip $name"
+
+    if($diff_mode) {
+        $result.diff.prepared += "`n+[$line]`n"
+    }
+
+    if(-not $check_mode) {
+        $list.Add($line) | Out-Null
+    }
+}
+
+Function Remove-AliasFromEntry($list, [ref]$idx, $name) {
+    $line = $list[$idx.value]
+
+    $match = Find-Hostname -line $line -name $name
+
+    if($match.Success){
+        $result.changed = $true
+    
+        $line = $line.Remove($match.Index + 1, $match.Length -1)
+
+        # was this the only alias? (check for space characters after trimming)
+        if($line.Substring(0,(Get-CommentIndex -line $line)).Trim() -inotmatch "\s") {
+
+            Remove-HostEntry -list $list -idx $idx
+
+        } else {
+
+            if($diff_mode) {
+                $result.diff.prepared += "`n-[$($list[$idx.value])]`n+[$line]`n"
+            }
+    
+            if(-not $check_mode) {
+                $list[$idx.value] = $line
             }
 
         }
-
-        $idx++
     }
 
-    return -1
 }
 
-$host_name = $host_name.Trim()
-$ip_address = $ip_address.Trim()
+Function Add-AliasToEntry($list, $idx, $name) {
+    $line = $list[$idx]
+
+    $match = Find-Hostname -line $line -name $name
+
+    if(-not $match.Success) {
+        $result.changed = $true
+
+        # just add the alias before the comment
+        $line = $line.Insert((Get-CommentIndex -line $line), " $name ")
+
+        if($diff_mode) {
+            $result.diff.prepared += "`n-[$($list[$idx])]`n+[$line]`n"
+        }
+
+        if(-not $check_mode) {
+            $list[$idx] = $line
+        }
+    }
+}
 
 $hosts_lines = New-Object System.Collections.ArrayList
 
 Get-Content -Path $hosts_file.FullName | ForEach-Object { $hosts_lines.Add($_) } | Out-Null
 
-if($state -eq 'present'){
-
-    $idx = Get-IndexOfHostEntry -list $hosts_lines -value $host_name
-
-    if($idx -eq -1) {
-        $result.changed = $true
-
-        if(-not $check_mode){
-            $hosts_lines.Add("$ip_address $host_name") | Out-Null
-            Set-Content -Path $hosts_file -Value $hosts_lines
-        }
-
-        if($diff_mode) {
-            $result.diff = @{
-                prepared = "`n+[$ip_address $host_name]`n"
-            }
-        }
-
-    } else {
-        # determine if ip matches
-        if($hosts_lines[$idx] -inotmatch "\s*$($ip_address.Replace('.',"\."))\s+$($host_name.Replace('.', "\."))"){
-            $result.changed = $true
-            
-            if(-not $check_mode){
-                $removed_line = $hosts_lines[$idx]
-                $hosts_lines.RemoveAt($idx)
-                $hosts_lines.Insert($idx, "$ip_address $host_name")
-                Set-Content -Path $hosts_file -Value $hosts_lines
-            }
-
-            if($diff_mode) {
-                $result.diff = @{
-                    prepared = "`n-[$removed_line]`n+[$ip_address $host_name]`n"
+# go through and remove based on host and ips
+if ($state -eq 'absent') {
+    for($idx = 0; $idx -lt $hosts_lines.Count; $idx++) {
+    
+        $entry = $hosts_lines[$idx]
+        # skip comment lines
+        if(-not $entry.Trim().StartsWith('#')) {
+        
+            $ip_idx = Get-IpIndex -line $entry -ip $ip_address
+            if($ip_idx -ge 0) {
+                if ($host_name) {
+                    Remove-AliasFromEntry -list $hosts_lines -idx ([ref]$idx) -name $host_name
+                } else {
+                    # only ips provided
+                    Remove-HostEntry -list $hosts_lines -idx ([ref]$idx)
                 }
             }
         }
-
     }
+}
+# go through lines, determine whether to add or remove based on ip_action
+if($state -eq 'present') {
+    # for adding later
+    $entry_exists = $false
+    $found_idx = -1
 
-} elseif ($state -eq 'absent') {
+    for($idx = 0; $idx -lt $hosts_lines.Count; $idx++) {
+    
+        $entry = $hosts_lines[$idx]
+        # skip comment lines
+        if(-not $entry.Trim().StartsWith('#')) {
 
-    if ($ip_address){
-        Add-Warning -obj $result -message "When removing host entry '$host_name' it does not need an ip address '$ip_address' set"
-    }
-
-    $idx = Get-IndexOfHostEntry -list $hosts_lines -value $host_name
-
-    if($idx -ne -1){
-
-        $result.changed = $true
-        $removed_line = $hosts_lines[$idx]
-
-        if(-not $check_mode){
-            $hosts_lines.RemoveAt($idx)
-            Set-Content -Path $hosts_file -Value $hosts_lines
-        }
-
-        if($diff_mode) {
-            $result.diff = @{
-                prepared = "`n-[$removed_line]`n"
+            if((Get-IpIndex -line $entry -ip $ip_address) -ge 0) {
+                $found_idx = $idx
+                if(-not $entry_exists) {
+                    # find out if this is the one
+                    if((Find-Hostname -line $entry -name $host_name).Success) {
+                        $entry_exists = $true
+                        if($ip_action -eq 'add') {
+                            # we're done!
+                            break
+                        }
+                    }
+                }
+            } elseif($ip_action -eq 'replace') {
+                # this is not the ip we were looking for
+                # remove the alias if it exists
+                Remove-AliasFromEntry -list $hosts_lines -idx ([ref]$idx) -name $host_name
             }
         }
     }
+    
+    if(-not $entry_exists){
+        if($found_idx -ge 0) {
+            Add-AliasToEntry -list $hosts_lines -idx $found_idx -name $host_name
+        } else {
+            Add-HostEntry -list $hosts_lines -name $host_name -ip $ip_address
+        }
+    }
+}
+
+if( $result.changed ) {
+    Set-Content -Path $hosts_file -Value $hosts_lines
 }
 
 Exit-Json $result
--- lib/ansible/modules/windows/win_hosts.py
+++ lib/ansible/modules/windows/win_hosts.py
@@ -1,55 +1,87 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-ANSIBLE_METADATA = {'metadata_version': '0.1',
+# Copyright: (c) 2018, Micah Hunsberger (@mhunsber)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# this is a windows documentation stub.  actual code lives in the .ps1
+# file of the same name
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
 DOCUMENTATION = r'''
 ---
 module: win_hosts
-version_added: ''
-short_description: Modify hosts file entries on windows
+version_added: '2.5.1'
+short_description: Modify hosts file entries on windows.
 description:
-- Allows addition, replacement, and removal of hostname entries in the windows system hosts file.
+  - Allows  the addition, replacement, and removal of ip addresses and aliases from host entries in the windows system hosts file.
+  - Modifies %windir%\system32\drivers\etc\hosts.
 options:
   state:
     description:
-      - Whether the host entry should be present or absent.
+      - Whether the host should be present or absent.
+      - If only C(host_name) is provided when C(state=absent), the host_name alias will be removed from all host entries.
+      - If only C(ip_address) is provided when C(state=absent), the host entry and all aliases will be removed.
     choices:
-	  - absent
-	  - present
+      - absent
+      - present
     default: present
   host_name:
     description:
-      - The dns name of the host entry.
-    required: true
+      - An alias or dns name for the host.
+      - required for C(state=present).
   ip_address:
     description:
-      - The ip address of the host entry.
-      - Can be omitted for C(state=absent).
-	  - Required for C(state=present).
+      - The ip address of the host.
+      - Required for C(state=present).
+  ip_action:
+    choices:
+      - add
+      - replace
+    description:
+      - Only applicable with C(state=absent).
+      - If C(add), the I(host_name) will be added as an entry to I(ip_address).
+      - If C(replace), the I(host_name) will be added as an entry to I(ip_address) and removed from any other entries.
+    default: replace
 author:
-- Micah Hunsberger (@mhunsber)
+  - Micah Hunsberger (@mhunsber)
 notes:
-- See also M(win_template), M(win_file), M(win_copy)
+  - See also M(win_template), M(win_file), M(win_copy)
 '''
 
 EXAMPLES = r'''
-- name: Ensure localhost is present
+- name: Map localhost to 127.0.0.1
   win_hosts:
     state: present
     host_name: localhost
     ip_address: 127.0.0.1
 
-- name: Remove localhost from the list of hosts
+- name: Add ::1 as an ip for localhost
   win_environment:
-    state: absent
-    name: localhost
-	
-- name: Change localhost to the ipv6 loopback address
-  win_hosts:
     state: present
-	host_name: localhost
-	ip_address: '::1'
+    ip_action: add
+    host_name: localhost
+    ip_address: '::1'
+
+- name: Remove 'foo' from the list of aliases for 192.168.1.100
+  win_hosts:
+    state: absent
+    host_name: foo
+    ip_address: 192.168.1.100
+
+- name: Remove alias 'bar' from all host entries
+  win_hosts:
+    state: absent
+    host_name: bar
+
+- name: Remove 10.2.0.1 from the list of hosts
+  win_hosts:
+    state: absent
+    ip_address: 10.2.0.1
+'''
+
+RETURN = r'''
 '''
