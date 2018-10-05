5036666e9457ab374249553e09c8268026b2b061
Nikhil Singh <nikhil.singh@sap.com>
2018-10-05 15:21:01 +0200

    Fixed the ssl-certs in netbox plugin

--- lib/ansible/plugins/inventory/netbox.py
+++ lib/ansible/plugins/inventory/netbox.py
@@ -135,7 +135,7 @@ class InventoryModule(BaseInventoryPlugin):
     NAME = 'netbox'
 
     def _fetch_information(self, url):
-        response = open_url(url, headers=self.headers, timeout=self.timeout)
+        response = open_url(url, headers=self.headers, timeout=self.timeout, validate_certs=False)
 
         try:
             raw_data = to_text(response.read(), errors='surrogate_or_strict')
