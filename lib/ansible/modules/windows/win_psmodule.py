51d0a1d57b180f61aa4f492f192be88f06036d5d
Wojciech Sciesinski <wojciech@sciesinski.net>
2018-10-05 01:42:11 +0100

    Correction of documentation

--- lib/ansible/modules/windows/win_psmodule.py
+++ lib/ansible/modules/windows/win_psmodule.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright: (c) 2017, Daniele Lazzari <lazzari@mailup.com>
+# Copyright: (c) 2018, Wojciech Sciesinski <wojciech[at]sciesinski[dot]net>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 # this is a windows documentation stub.  actual code lives in the .ps1
@@ -11,56 +12,60 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
-DOCUMENTATION = r'''
+DOCUMENTATION = '''
 ---
 module: win_psmodule
 version_added: "2.4"
-short_description: Adds or removes a Powershell Module
+short_description: Adds or removes a Windows PowerShell Module
 description:
-    - This module helps to install Powershell modules and register custom modules repository on Windows Server.
+  - This module helps to install Windows PowerShell modules and register custom modules repository on Windows-based systems.
 options:
   name:
     description:
-      - Name of the PowerShell module that has to be installed.
+      - Name of the Windows PowerShell module that has to be installed.
     required: no
-state:
+  state:
     description:
       - If C(present) a new module is installed.
       - If C(absent) a module is removed.
-      - If C(latest) a module is updated.
-    choices: [ absent, present, latest ]
+      - If C(latest) a module is updated to the newest version. The option was added in version 2.8.
+    choices: [ absent, latest, present ]
     default: present
   required_version:
     description:
       - The exact version of the PowerShell module that has to be installed.
     type: str
-    required: no
+    version_added: "2.8"
   minimum_version:
     description:
       - The minimum version of the PowerShell module that has to be installed.
     type: str
-    required: no
+    version_added: "2.8"
   maximum_version:
     description:
       - The maximum version of the PowerShell module that has to be installed.
     type: str
-    required: no
+    version_added: "2.8"
   allow_clobber:
     description:
-      - If C(yes) imports all commands, even if they have the same names as commands that already exists. Available only in Powershell 5.1 or higher.
+      - If C(yes) imports all commands, even if they have the same names as commands that already exists. Available only in PowerShell 5.1 or higher.
     type: bool
     default: no
   skip_publisher_check:
     description:
-      - If C(yes) allows installs a newer version of a module that already exists on your computer in the case when a newer one is not digitally signed by a trusted publisher and the newest existing module is digitally signed by a trusted publisher.
+      - If C(yes) allows installs a newer version of a module that already exists on your computer in the case when a newer one is not
+      - digitally signed by a trusted publisher and the newest existing module is digitally signed by a trusted publisher.
     type: bool
     default: no
+    version_added: "2.8"
   allow_prerelease:
     description:
       - If C(yes) installs modules marked as prereleases.
-      - It doesn't work with the parameters minimum_version and/or maximum_version.
+      - It doesn't work with the parameters C(minimum_version) and/or C(maximum_version).
+      - It doesn't work with the C(state) set to absent.
     type: bool
     default: no
+    version_added: "2.8"
   repository:
     description:
       - Name of the custom repository to register or use.
@@ -69,9 +74,13 @@ state:
     description:
       - URL of the custom repository to register.
 notes:
+<<<<<<< HEAD
    -  Powershell 5.0 or higher is needed.
 seealso:
 - module: win_psrepository
+=======
+   -  PowerShell 5.0 or higher is needed.
+>>>>>>> Correction of documentation
 author:
 - Daniele Lazzari (@dlazz)
 - Wojciech Sciesinski (@it-praktyk)
@@ -79,30 +88,47 @@ author:
 
 EXAMPLES = r'''
 ---
-- name: Add a powershell module
+- name: Add a PowerShell module
   win_psmodule:
-    name: PowershellModule
+    name: PowerShellModule
     state: present
 
-- name: Add a powershell module and register a repository
+- name: Add an exact version of PowerShell module
+  win_psmodule:
+    name: PowerShellModule
+    required_version: "4.0.2"
+    state: present
+
+- name: Install or update an existing PowerShell module to the newest version
+  win_psmodule:
+    name: PowerShellModule
+    state: latest
+
+- name: Install newer version of built-in Windows module
+  win_psmodule:
+    name: Pester
+    skip_publisher_check: yes
+    state: present
+
+- name: Add a PowerShell module and register a repository
   win_psmodule:
     name: MyCustomModule
     repository: MyRepository
     url: https://myrepo.com
     state: present
 
-- name: Add a powershell module from a specific repository
+- name: Add a PowerShell module from a specific repository
   win_psmodule:
-    name: PowershellModule
+    name: PowerShellModule
     repository: MyRepository
     state: present
 
-- name: Remove a powershell module
+- name: Remove a PowerShell module
   win_psmodule:
-    name: PowershellModule
+    name: PowerShellModule
     state: absent
 
-- name: Remove a powershell module and a repository
+- name: Remove a PowerShell module and a repository
   win_psmodule:
     name: MyCustomModule
     repository: MyRepository
@@ -131,9 +157,11 @@ powershellget_changed:
   returned: always
   type: boolean
   sample: True
+  version_added: "2.8"
 packagemanagement_changed:
   description: true when the PackageManagement module is updated
   returned: always
   type: boolean
   sample: True
+  version_added: "2.8"
 '''
