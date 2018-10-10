a70853f728d82f82bcc0502e6cbfe423d0b8252a
Abhijeet Kasurde <akasurde@redhat.com>
2018-10-10 22:57:58 +0530

    lambda: fix unboundLocalError in lambda (#46729)
    This fix initializes current_version to None so that
    module proceeds in check mode.
    
    Fixes: #46654
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/cloud/amazon/lambda.py
+++ lib/ansible/modules/cloud/amazon/lambda.py
@@ -554,6 +554,7 @@ def main():
                                               'SecurityGroupIds': vpc_security_group_ids}})
 
         # Finally try to create function
+        current_version = None
         try:
             if not check_mode:
                 response = client.create_function(**func_kwargs)
