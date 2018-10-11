a651e200731687e56bc19a5a4b8788613ab499c1
Alicia Cozine <879121+acozine@users.noreply.github.com>
2018-10-11 14:38:54 -0500

    all amazon modules are community maintained (#46840)

--- lib/ansible/modules/cloud/amazon/efs_facts.py
+++ lib/ansible/modules/cloud/amazon/efs_facts.py
@@ -8,7 +8,7 @@ __metaclass__ = type
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
-                    'supported_by': 'certified'}
+                    'supported_by': 'community'}
 
 
 DOCUMENTATION = '''
