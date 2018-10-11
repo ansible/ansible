27145e1388acded6c1dfaea2ffa8a26fcbb3decb
Nafallo Bjälevik <nafallo.bjalevik.2@volvocars.com>
2018-10-11 19:25:11 +0200

    Check both ['metadata']['status'] and ['status']

--- lib/ansible/module_utils/lxd.py
+++ lib/ansible/module_utils/lxd.py
@@ -93,7 +93,11 @@ class LXDClient(object):
         if resp_json['type'] == 'async':
             url = '{0}/wait'.format(resp_json['operation'])
             resp_json = self._send_request('GET', url)
-            if resp_json['status'] != 'Success':
+            if 'status' in resp_json.get('metadata')
+                status = resp_json['metadata']['status']
+            else:
+                status = resp_json.get('status', 'Failure')
+            if status != 'Success':
                 self._raise_err_from_json(resp_json)
         return resp_json
 
