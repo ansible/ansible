c4212a66d44ebf046311175846b9e61884cdc0c3
Matt Clay <matt@mystile.com>
2018-10-12 12:58:23 -0700

    Fix test to support code coverage.

--- test/integration/targets/inventory_foreman_script/runme.sh
+++ test/integration/targets/inventory_foreman_script/runme.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 export FOREMAN_HOST="${FOREMAN_HOST:-localhost}"
 export FOREMAN_PORT="${FOREMAN_PORT:-8080}"
-export FOREMAN_CONFIG="${OUTPUT_DIR}/foreman.ini"
+export FOREMAN_INI_PATH="${OUTPUT_DIR}/foreman.ini"
 
-cat > "$FOREMAN_CONFIG" <<FOREMAN_INI
+cat > "$FOREMAN_INI_PATH" <<FOREMAN_INI
 [foreman]
 url = http://${FOREMAN_HOST}:${FOREMAN_PORT}
 user = ansible-tester
@@ -14,13 +14,5 @@ password = secure
 ssl_verify = False
 FOREMAN_INI
 
-cp test_foreman_inventory.yml $OUTPUT_DIR/.
-cp ../../../../contrib/inventory/foreman.py $OUTPUT_DIR/.
-cd $OUTPUT_DIR
-
-# muck the shebang to conform to test environment
-sed -i.bak "s|^#!.*|#!${ANSIBLE_TEST_PYTHON_INTERPRETER}|" foreman.py
-head -n1 foreman.py
-
 # use ansible to validate the return data
-ansible-playbook -i foreman.py test_foreman_inventory.yml --connection=local
+ansible-playbook -i foreman.sh test_foreman_inventory.yml --connection=local
