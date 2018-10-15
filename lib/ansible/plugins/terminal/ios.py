81b626feb45f9c9d7830b447e2b6a9332ec7262d
Sumit Jaiswal <sjaiswal@redhat.com>
2018-10-15 12:58:09 +0530

    revert 46677 changes

--- lib/ansible/plugins/terminal/ios.py
+++ lib/ansible/plugins/terminal/ios.py
@@ -58,8 +58,7 @@ class TerminalModule(TerminalBase):
             raise AnsibleConnectionFailure('unable to set terminal parameters')
 
     def on_become(self, passwd=None):
-        conn_prompt = self._get_prompt()
-        if not conn_prompt or conn_prompt.endswith(b'#'):
+        if self._get_prompt().endswith(b'#'):
             return
 
         cmd = {u'command': u'enable'}
