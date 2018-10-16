b70a386168cfe9141504837101521f2c95b83969
Deric Crago <deric.crago@gmail.com>
2018-10-16 10:47:00 -0400

    added Copyright & License info

--- lib/ansible/plugins/connection/win_vmware_tools.py
+++ lib/ansible/plugins/connection/win_vmware_tools.py
@@ -1,4 +1,7 @@
-"""Docstring for public module."""
+# Copyright (c) 2018 Deric Crago <deric.crago@gmail.com>
+# Copyright (c) 2018 Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 from __future__ import absolute_import, division, print_function
 
 from ansible.plugins.connection.vmware_tools import Connection as VMwareToolsConnection
