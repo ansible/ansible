94b7dd1d57ebfc9d53b4845d6a43ce3acc34050d
Baptiste Mille-Mathias <baptiste.millemathias@gmail.com>
2018-10-17 22:24:39 +0200

    Use loop instead of obsolete with_items

--- lib/ansible/modules/system/parted.py
+++ lib/ansible/modules/system/parted.py
@@ -184,9 +184,8 @@ EXAMPLES = r'''
     device: /dev/sdb
     number: '{{ item.num }}'
     state: absent
-  with_items:
-   - '{{ sdb_info.partitions }}'
-'''
+  loop: "{{ sdb_info.partitions }}"
+"""
 
 
 from ansible.module_utils.basic import AnsibleModule
