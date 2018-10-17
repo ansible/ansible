b9cf09e9d53cbaa31afa78b52d196495a8acf253
Ryan Merolle <ryanmerolle@users.noreply.github.com>
2018-10-17 10:00:56 -0400

    network.py filter_plugin typo fix (#47011)
    changed return_orginal to return_original in /lib/ansible/plugins/filter/network.py

--- lib/ansible/plugins/filter/network.py
+++ lib/ansible/plugins/filter/network.py
@@ -388,11 +388,11 @@ def hash_salt(password):
         return split_password[2]
 
 
-def comp_type5(unencrypted_password, encrypted_password, return_orginal=False):
+def comp_type5(unencrypted_password, encrypted_password, return_original=False):
 
     salt = hash_salt(encrypted_password)
     if type5_pw(unencrypted_password, salt) == encrypted_password:
-        if return_orginal is True:
+        if return_original is True:
             return encrypted_password
         else:
             return True
