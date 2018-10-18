a9128b9fb0fef93561ab8431f9015da89fed1b7e
Ondra Machacek <omachace@redhat.com>
2018-10-18 13:56:36 +0200

    ovirt_vm: Check next_run configuration update if exist
    This PR fixes the update check method so it now check also the next_run
    configuration of the virtual machine if it exists.
    
    So if previously the VM was updated with new parameters, and then reset
    back, the module didn't set the parameters to be set back in next_run.
    This PR fixes it so the next run configuration is set back with proper
    parameters.
    
    Signed-off-by: Ondra Machacek <omachace@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1639894
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- lib/ansible/modules/cloud/ovirt/ovirt_vm.py
+++ lib/ansible/modules/cloud/ovirt/ovirt_vm.py
@@ -1146,6 +1146,13 @@ class VmsModule(BaseModule):
         )
 
     def update_check(self, entity):
+        res = self._update_check(entity)
+        if entity.next_run_configuration_exists:
+            res = res and self._update_check(self._service.service(entity.id).get(next_run=True))
+
+        return res
+
+    def _update_check(self, entity):
         def check_cpu_pinning():
             if self.param('cpu_pinning'):
                 current = []
