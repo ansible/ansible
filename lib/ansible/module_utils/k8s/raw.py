ae0400a2f4d55c8810a8a49cf12bdaeefda99cee
Will Thames <will@thames.id.au>
2018-10-22 17:03:59 +1000

    params.get preserves k8s module invocation
    Using params.get rather than params.pop preserves the
    original module invocation for more useful output under
    more verbose output modes.

--- lib/ansible/module_utils/k8s/raw.py
+++ lib/ansible/module_utils/k8s/raw.py
@@ -54,11 +54,11 @@ class KubernetesRawModule(KubernetesAnsibleModule):
                                          supports_check_mode=True,
                                          **kwargs)
 
-        self.kind = self.params.pop('kind')
-        self.api_version = self.params.pop('api_version')
-        self.name = self.params.pop('name')
-        self.namespace = self.params.pop('namespace')
-        resource_definition = self.params.pop('resource_definition')
+        self.kind = self.params.get('kind')
+        self.api_version = self.params.get('api_version')
+        self.name = self.params.get('name')
+        self.namespace = self.params.get('namespace')
+        resource_definition = self.params.get('resource_definition')
         if resource_definition:
             if isinstance(resource_definition, string_types):
                 try:
@@ -69,7 +69,7 @@ class KubernetesRawModule(KubernetesAnsibleModule):
                 self.resource_definitions = resource_definition
             else:
                 self.resource_definitions = [resource_definition]
-        src = self.params.pop('src')
+        src = self.params.get('src')
         if src:
             self.resource_definitions = self.load_resource_definitions(src)
 
