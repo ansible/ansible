e0a9dd77512c8ad6f9526a9e9ff750d5e369d876
Andreas Calminder <commit@localhost>
2018-10-23 21:42:03 +0200

    really accept None as lock_timeout

--- lib/ansible/module_utils/common/file.py
+++ lib/ansible/module_utils/common/file.py
@@ -158,6 +158,11 @@ class FileLock:
 
         self.lockfd = open(lock_path, 'w')
 
+        if lock_timeout is None or lock_timeout < 0:
+            fcntl.flock(self.lockfd, fcntl.LOCK_EX)
+            os.chmod(lock_path, stat.S_IWRITE | stat.S_IREAD)
+            return True
+
         if lock_timeout == 0:
             fcntl.flock(self.lockfd, fcntl.LOCK_EX | fcntl.LOCK_NB)
             os.chmod(lock_path, stat.S_IWRITE | stat.S_IREAD)
@@ -178,10 +183,6 @@ class FileLock:
             self.lockfd.close()
             raise LockTimeout('{0} sec'.format(lock_timeout))
 
-        fcntl.flock(self.lockfd, fcntl.LOCK_EX)
-        os.chmod(lock_path, stat.S_IWRITE | stat.S_IREAD)
-
-        return True
 
     def unlock(self):
         '''
