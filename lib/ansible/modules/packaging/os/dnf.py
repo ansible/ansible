cacce8bfa3d21824d982508cac32994f0da1e67b
Adam Miller <admiller@redhat.com>
2018-10-23 11:41:27 -0500

    dnf properly gpg check local packages based on param (#47455)
    * dnf properly gpg check local packages based on param
    
    Fixes #43624
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    (cherry picked from commit 079705f8daa9fff4df76d9f5a52cb761541f72e6)

--- lib/ansible/modules/packaging/os/dnf.py
+++ lib/ansible/modules/packaging/os/dnf.py
@@ -482,6 +482,7 @@ class DnfModule(YumDnf):
 
         # Set whether to check gpg signatures
         conf.gpgcheck = not disable_gpg_check
+        conf.localpkg_gpgcheck = not disable_gpg_check
 
         # Don't prompt for user confirmations
         conf.assumeyes = True
