8ba62b24a9a9bb5f5c80a28c28046995f67a4632
FragmentedPacket <mikhail.yohman@gmail.com>
2018-10-24 09:52:20 -0600

    Netbox inventory: Fix NoneType issue if query_filters is not in netbox_inventory.yml (#46793)
    * Added if state to refresh_url to avoid failure at NoneType when guesry_filters is undefined in netbox_inventory.yml

--- lib/ansible/plugins/inventory/netbox.py
+++ lib/ansible/plugins/inventory/netbox.py
@@ -352,8 +352,9 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
 
     def refresh_url(self):
         query_parameters = [("limit", 0)]
-        query_parameters.extend(filter(lambda x: x,
-                                       map(self.validate_query_parameters, self.query_filters)))
+        if self.query_filters:
+            query_parameters.extend(filter(lambda x: x,
+                                           map(self.validate_query_parameters, self.query_filters)))
         self.device_url = self.api_endpoint + "/api/dcim/devices/" + "?" + urlencode(query_parameters)
         self.virtual_machines_url = "".join([self.api_endpoint,
                                              "/api/virtualization/virtual-machines/",
