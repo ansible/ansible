d77148b1cfe0cf44a32900e69e8c2d1aa92d0dc0
david.calvo <david.calvo@scmspain.com>
2018-10-25 16:25:28 +0200

    Merge branch 'dynamodb' into add_dynamodb

---
+++ lib/ansible/modules/cloud/amazon/dynamodb.py
@@ -0,0 +1,202 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+
+DOCUMENTATION = """
+module: dynamodb
+short_description: Reads, creates, updates or deletes items in AWS Dynamo DB tables.
+version_added: "2.7"
+author:
+  - David C. Martin @blastikman
+requirements:
+  - boto3
+options:
+  table:
+    description:
+      - Name of the DynamoDB table
+  action:
+    description:
+      - "get": Returns a set of attributes for the item with
+      the given primary key.
+      - "put": Creates a new item, or replaces an old item with a new item.
+      If an item that has the same primary key as the new item already exists
+      in the specified table, the new item completely replaces the existing item.
+      - "delete": Deletes a single item in a table by primary key. You can
+      perform a conditional delete operation that deletes the item if it exists,
+       or if it has an expected attribute value.
+  primary_key:
+    description:
+      -
+  item:
+    description:
+      -
+  update_expression:
+    description:
+      -
+  update_expression:
+    description:
+      -
+  update_expression:
+    description:
+      -
+  update_expression:
+    description:
+      -
+  update_expression:
+    description:
+      -
+  update_expression:
+    description:
+      -
+
+
+"""
+
+EXAMPLES = '''
+# Creates a single item in 'narcos' DynamoDB table where column 'bank' equals 'hsbc' string,
+# column 'quantity' equals '1000' numeric and column 'person' equals 'ochoa' string
+- name: Creates a new record
+  dynamodb:
+    table: narcos
+    action: put
+    item: {"bank": {"S": "hsbc"},"quantity": {"N": "1000"},"person": {"S": "ochoa"}}
+
+# Connects with awscli profile 'development' and
+# deletes a single item from 'releases' DynamoDB table
+# where the column 'project' equals string 'potatoes' (also primary key)
+# and column 'version' equals numeric '123456'
+- name: Delete a single item
+  dynamodb:
+    profile: development
+    table: releases
+    action: delete
+    primary_key: {"project": {"S": "potatoes"}}
+    condition_expression: version = :number
+    expression_attribute_values: {":number": {"N": "123456"}}
+'''
+
+import boto3
+
+import ansible.module_utils.ec2
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.ec2 import boto_exception
+
+
+try:
+    import boto3
+    import botocore
+    HAS_BOTO3 = True
+except ImportError:
+    HAS_BOTO3 = False
+
+
+def main():
+    argument_spec = ansible.module_utils.ec2.ec2_argument_spec()
+    argument_spec.update(dict(
+        table=dict(required=True),
+        action=dict(required=True, choices=["get", "put", "update", "delete"]),
+        primary_key=dict(required=False, type='dict'),
+        item=dict(required=False, type='dict'),
+        condition_expression=dict(required=False, type='str'),
+        expression_attribute_values=dict(required=False, type='dict'),
+        # projection_expression=dict(required=True)
+        # primary_key_type=dict(required=True),
+        # update_expression=dict(default='present', type='str', choices=[
+        #     "SET", "REMOVE", "ADD", "DELETE"]),
+        # expression_attribute_names=dict(required=False),
+    )
+    )
+
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=False,
+    )
+
+    if not HAS_BOTO3:
+        module.fail_json(msg='pip modules boto3 and botocore are required')
+
+    try:
+        region, ec2_url, aws_connect_kwargs = ansible.module_utils.ec2.get_aws_connection_info(
+            module, boto3=True)
+        connection = ansible.module_utils.ec2.boto3_conn(module, conn_type='client',
+                                                         resource='dynamodb',
+                                                         region=region, endpoint=ec2_url,
+                                                         **aws_connect_kwargs)
+    except (botocore.exceptions.NoRegionError,
+            botocore.exceptions.ProfileNotFound,
+            botocore.exceptions.NoCredentialsError) as error:
+
+        error_msg = boto_exception(error)
+        module.fail_json(msg=error_msg)
+
+    try:
+        table = module.params.get('table')
+        action = module.params.get('action')
+        primary_key = module.params.get('primary_key')
+        condition_expression = module.params.get('condition_expression')
+        item = module.params.get('item')
+        expression_attribute_values = module.params.get(
+            'expression_attribute_values')
+        # projection_expression = module.params.get('projection_expression')
+        # update_expression = module.params.get('update_expression')
+        # expression_attribute_names = module.params.get(
+        #     'expression_attribute_names')
+
+
+        if action == 'get':
+            response = connection.get_item(
+                TableName=table,
+                Key=primary_key,
+                # ProjectionExpression=projection_expression
+            )
+
+            return response
+
+        elif action == 'put':
+            result = connection.put_item(
+                TableName=table,
+                Item=item,
+            )
+
+        elif action == 'update':
+            result = connection.update_item(
+                TableName=table,
+                Key={primary_key_type: primary_key},
+                UpdateExpression=update_expression,
+                ExpressionAttributeValues=expression_attribute_values,
+            )
+
+            return result
+
+        elif action == 'delete':
+            result = connection.delete_item(
+                TableName=table,
+                Key=primary_key,
+                ConditionExpression=condition_expression,
+                ExpressionAttributeValues=expression_attribute_values,
+                ReturnValues='ALL_OLD'
+            )
+
+            return result
+
+    except connection.exceptions.ResourceNotFoundException as error:
+        error_msg = 'Table {} not found'.format(table)
+        module.fail_json(msg=error_msg)
+    except connection.exceptions.ConditionalCheckFailedException as error:
+        error_msg = 'No item matches the condition'
+        module.fail_json(msg=error_msg)
+    except Exception as error:
+        error_msg = boto_exception(error)
+        module.fail_json(msg=error_msg)
+
+    result = dict(changed=False, message=response)
+
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
