98f7b04b9112ebeb3db944d0c5ea88ebe9212b77
f-bor <frederic.bor@wanadoo.fr>
2018-10-25 11:00:57 +0200

    timeout and exception return

--- test/units/plugins/connection/test_network_cli.py
+++ test/units/plugins/connection/test_network_cli.py
@@ -127,6 +127,7 @@ class TestConnectionClass(unittest.TestCase):
         mock__shell = MagicMock()
         conn._ssh_shell = mock__shell
         conn._ssh_shell.recv_ready.return_value = True
+        conn._ssh_shell.gettimeout.return_value = 10
 
         response = b"""device#command
         command response
