5a64d9759b6b9de735916346ec1f4a25788af9e1
Brian Coca <brian.coca+git@gmail.com>
2018-10-26 13:01:42 -0400

    fix typo# with '#' will be ignored, and an empty message aborts the commit.

--- packaging/release/changelogs/changelog.py
+++ packaging/release/changelogs/changelog.py
@@ -24,7 +24,7 @@ try:
 except ImportError:
     argcomplete = None
 
-from ansilbe.constants import C
+from ansible.constants import C
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 CHANGELOG_DIR = os.path.join(BASE_DIR, 'changelogs')
