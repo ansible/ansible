10dc681807fbeffab2949c68c36cdbc80671f115
Micah Hunsberger <Micah.Hunsberger@tempustechnologies.com>
2018-10-27 14:43:21 -0400

    aliases and canonical names are separate entities. added IPv4 and IPv6 validation

--- lib/ansible/modules/windows/win_hosts.ps1
+++ lib/ansible/modules/windows/win_hosts.ps1
@@ -13,11 +13,16 @@ $check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "b
 $diff_mode = Get-AnsibleParam -obj $params -name "_ansible_diff" -type "bool" -default $false
 
 $state = Get-AnsibleParam -obj $params -name "state" -type "str" -default "present" -validateset "absent","present"
-$host_name = Get-AnsibleParam -obj $params -name "host_name" -type "str" -failifempty ($state -eq 'present')
+$aliases = Get-AnsibleParam -obj $params -name "aliases" -type "list" -failifempty $false
+$canonical_name = Get-AnsibleParam -obj $params -name "canonical_name" -type "str" -failifempty ($state -eq 'present')
 $ip_address = Get-AnsibleParam -obj $params -name "ip_address" -type "str" -default "" -failifempty ($state -eq 'present')
-$ip_action = Get-AnsibleParam -obj $params -name "ip_action" -type "str" -default "replace" -validateset "replace","add"
+$action = Get-AnsibleParam -obj $params -name "action" -type "str" -default "set" -validateset "add","remove","set"
 
-# TODO: validate ip address?
+$tmp = [ipaddress]::None
+if($ip_address -and -not [ipaddress]::TryParse($ip_address, [ref]$tmp)){
+    Fail-Json -obj @{} -message "win_hosts: Argument ip_address needs to be a valid ip address, but was $ip_address"
+}
+$ip_address_type = $tmp.AddressFamily
 
 $hosts_file = Get-Item -Path "$env:SystemRoot\System32\drivers\etc\hosts"
 
@@ -36,98 +41,122 @@ Function Get-CommentIndex($line) {
     return $c_index
 }
 
-Function Get-IpIndex($line, $ip) {
+Function Get-HostEntryParts($line) {
+    $success = $true
     $c_index = Get-CommentIndex -line $line
-    $re = New-Object regex "\s*$($ip.Replace('.',"\."))\s"
-    $match = $re.Match($line, 0, $c_index)
-    if($match.Success) {
-        return $match.Index
-    } else {
-        return -1
+    $pure_line = $line.Substring(0,$c_index).Trim()
+    $bits = $pure_line -split "\s+"
+    if($bits.Length -lt 2){
+        return @{
+            success = $false
+            ip_address = ""
+            ip_type = ""
+            canonical_name = ""
+            aliases = @()
+        }
+    }
+    $ip_obj = [ipaddress]::None
+    if(-not [ipaddress]::TryParse($bits[0], [ref]$ip_obj) ){
+        $success = $false
+    }
+    $cname = $bits[1]
+    $als = New-Object string[] ($bits.Length - 2)
+    [array]::Copy($bits, 2, $als, 0, $als.Length)
+    return @{
+        success = $success
+        ip_address = $ip_obj.IPAddressToString
+        ip_type = $ip_obj.AddressFamily
+        canonical_name = $cname
+        aliases = $als
     }
 }
 
-Function Find-Hostname($line, $name) {
+Function Find-HostName($line, $name) {
     $c_idx = Get-CommentIndex -line $line
     $re = New-Object regex ("\s+$($name.Replace('.',"\."))(\s|$)", [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
     $match = $re.Match($line, 0, $c_idx)
     return $match
 }
 
-Function Remove-HostEntry($list, [ref]$idx) {
+Function Remove-HostEntry($list, $idx) {
     $result.changed = $true
+    $removed = $false
 
     if($diff_mode) {
-        $result.diff.prepared += "`n-[$($list[$idx.value])]`n"
+        $result.diff.prepared += "`n-[$($list[$idx])]`n"
     }
 
     if(-not $check_mode) {
-        # don't skip any lines
-        $list.RemoveAt($idx.value)
-        $idx.value--
+        $list.RemoveAt($idx)
+        $removed = $true
     }
 
+    return $removed
 }
 
-Function Add-HostEntry($list, $name, $ip) {
+Function Add-HostEntry($list, $cname, $aliases, $ip) {
     $result.changed = $true
-
-    $line = "$ip $name"
-
+    $line = "$ip $cname $($aliases -join ' ')"
     if($diff_mode) {
         $result.diff.prepared += "`n+[$line]`n"
     }
-
     if(-not $check_mode) {
         $list.Add($line) | Out-Null
     }
 }
 
-Function Remove-AliasFromEntry($list, [ref]$idx, $name) {
-    $line = $list[$idx.value]
+Function Remove-HostnamesFromEntry($list, $idx, $aliases) {
+    $line = $list[$idx]
+    $line_removed = $false
 
-    $match = Find-Hostname -line $line -name $name
-
-    if($match.Success){
-        $result.changed = $true
-    
-        $line = $line.Remove($match.Index + 1, $match.Length -1)
-
-        # was this the only alias? (check for space characters after trimming)
-        if($line.Substring(0,(Get-CommentIndex -line $line)).Trim() -inotmatch "\s") {
-
-            Remove-HostEntry -list $list -idx $idx
-
-        } else {
-
-            if($diff_mode) {
-                $result.diff.prepared += "`n-[$($list[$idx.value])]`n+[$line]`n"
+    foreach($name in $aliases){
+        $match = Find-HostName -line $line -name $name
+        if($match.Success){        
+            $line = $line.Remove($match.Index + 1, $match.Length -1)
+            # was this the last alias? (check for space characters after trimming)
+            if($line.Substring(0,(Get-CommentIndex -line $line)).Trim() -inotmatch "\s") {
+                if($diff_mode){
+                    $result.diff.prepared += "`n-[$($list[$idx])]`n"
+                }
+                if(-not $check_mode) {
+                    $list.RemoveAt($idx)
+                    $line_removed = $true
+                }
+                # we're done
+                return @{
+                    line_removed = $line_removed
+                }
             }
-    
-            if(-not $check_mode) {
-                $list[$idx.value] = $line
-            }
-
         }
     }
-
-}
-
-Function Add-AliasToEntry($list, $idx, $name) {
-    $line = $list[$idx]
-
-    $match = Find-Hostname -line $line -name $name
-
-    if(-not $match.Success) {
+    if($line -ne $list[$idx]){
         $result.changed = $true
-
-        # just add the alias before the comment
-        $line = $line.Insert((Get-CommentIndex -line $line), " $name ")
-
         if($diff_mode) {
             $result.diff.prepared += "`n-[$($list[$idx])]`n+[$line]`n"
         }
+        if(-not $check_mode) {
+            $list[$idx] = $line
+        }
+    }
+    return @{
+        line_removed = $line_removed
+    }
+}
 
+Function Add-AliasesToEntry($list, $idx, $aliases) {
+    $line = $list[$idx]
+    foreach($name in $aliases){
+        $match = Find-HostName -line $line -name $name
+        if(-not $match.Success) {
+            # just add the alias before the comment
+            $line = $line.Insert((Get-CommentIndex -line $line), " $name ")
+        }
+    }
+    if($line -ne $list[$idx]){
+        $result.changed = $true
+        if($diff_mode) {
+            $result.diff.prepared += "`n-[$($list[$idx])]`n+[$line]`n"
+        }
         if(-not $check_mode) {
             $list[$idx] = $line
         }
@@ -138,63 +167,89 @@ $hosts_lines = New-Object System.Collections.ArrayList
 
 Get-Content -Path $hosts_file.FullName | ForEach-Object { $hosts_lines.Add($_) } | Out-Null
 
-# go through and remove based on host and ips
 if ($state -eq 'absent') {
-    for($idx = 0; $idx -lt $hosts_lines.Count; $idx++) {
-    
+    # go through and remove canonical_name and ip
+    for($idx = 0; $idx -lt $hosts_lines.Count; $idx++) {    
         $entry = $hosts_lines[$idx]
-        # skip comment lines
-        if(-not $entry.Trim().StartsWith('#')) {
-        
-            $ip_idx = Get-IpIndex -line $entry -ip $ip_address
-            if($ip_idx -ge 0) {
-                if ($host_name) {
-                    Remove-AliasFromEntry -list $hosts_lines -idx ([ref]$idx) -name $host_name
-                } else {
-                    # only ips provided
-                    Remove-HostEntry -list $hosts_lines -idx ([ref]$idx)
+         # skip comment lines
+         if(-not $entry.Trim().StartsWith('#')) {
+            $entry_parts = Get-HostEntryParts -line $entry
+            if($entry_parts.success) {
+                if(-not $ip_address -or $entry_parts.ip_address -eq $ip_address) {
+                    if(-not $canonical_name -or $entry_parts.canonical_name -eq $canonical_name) {
+                        if(Remove-HostEntry -list $hosts_lines -idx $idx){
+                            # keep index correct if we removed the line
+                            $idx = $idx - 1
+                        }
+                    }
                 }
             }
         }
     }
 }
-# go through lines, determine whether to add or remove based on ip_action
 if($state -eq 'present') {
-    # for adding later
-    $entry_exists = $false
-    $found_idx = -1
+    $entry_idx = -1
 
+    # go through lines, find the entry and determine what to remove based on action
     for($idx = 0; $idx -lt $hosts_lines.Count; $idx++) {
-    
         $entry = $hosts_lines[$idx]
-        # skip comment lines
-        if(-not $entry.Trim().StartsWith('#')) {
-
-            if((Get-IpIndex -line $entry -ip $ip_address) -ge 0) {
-                $found_idx = $idx
-                if(-not $entry_exists) {
-                    # find out if this is the one
-                    if((Find-Hostname -line $entry -name $host_name).Success) {
-                        $entry_exists = $true
-                        if($ip_action -eq 'add') {
-                            # we're done!
-                            break
+         # skip comment lines
+         if(-not $entry.Trim().StartsWith('#')) {
+            $entry_parts = Get-HostEntryParts -line $entry
+            if($entry_parts.success) {
+                $aliases_to_remove = @()
+                if($entry_parts.ip_address -eq $ip_address) {
+                    if($entry_parts.canonical_name -eq $canonical_name) {
+                        # don't need to worry about line being removed since canonical_name is present
+                        $entry_idx = $idx
+                        
+                        if($action -eq 'set') {
+                            # remove the entry's aliases that are not in $aliases
+                            $aliases_to_remove = $entry_parts.aliases | Where-Object { $aliases -notcontains $_ }
+                        } elseif($action -eq 'remove') {
+                            $aliases_to_remove = $aliases
                         }
+                    } else {
+                        # this is the right ip_address, but not the cname we were looking for.
+                        # we need to make sure none of aliases or canonical_name exist for this entry
+                        # since the given canonical_name should be an A record, 
+                        # and aliases should be cname records for canonical_name.
+                        $aliases_to_remove = $aliases + $canonical_name
+                    }
+                } elseif ($ip_address_type -eq $entry_parts.ip_type) {
+                    # this is not the ip_address we are looking for
+                    # ensure canonical_name and aliases removed from this entry
+                    $aliases_to_remove = $aliases + $canonical_name
+                }
+
+                if($aliases_to_remove) {
+                    if((Remove-HostnamesFromEntry -list $hosts_lines -idx $idx -aliases $aliases_to_remove).line_removed) {
+                        # keep index correct if we removed the line
+                        $idx = $idx - 1
                     }
                 }
-            } elseif($ip_action -eq 'replace') {
-                # this is not the ip we are looking for
-                # remove the alias if it exists
-                Remove-AliasFromEntry -list $hosts_lines -idx ([ref]$idx) -name $host_name
             }
         }
     }
-    
-    if(-not $entry_exists){
-        if($found_idx -ge 0) {
-            Add-AliasToEntry -list $hosts_lines -idx $found_idx -name $host_name
+
+
+    if($entry_idx -ge 0) {
+        # we found the entry
+        if($action -ne 'remove') {
+            # we can add our aliases that are not already in the list
+            $entry_parts = Get-HostEntryParts -line $hosts_lines[$entry_idx]
+            $aliases_to_add = $aliases | Where-Object { $entry_parts.aliases -notcontains $_ }
+            if($aliases_to_add) {
+                Add-AliasesToEntry -list $hosts_lines -idx $entry_idx -aliases $aliases_to_add
+            }
+        }
+        # if action -eq remove then we're already done
+    } else {
+        # add the entry at the end
+        if($action -eq 'remove') {
+            Add-HostEntry -list $hosts_lines -ip $ip_address -cname $canonical_name
         } else {
-            Add-HostEntry -list $hosts_lines -name $host_name -ip $ip_address
+            Add-HostEntry -list $hosts_lines -ip $ip_address -cname $canonical_name -aliases $aliases
         }
     }
 }
--- lib/ansible/modules/windows/win_hosts.py
+++ lib/ansible/modules/windows/win_hosts.py
@@ -15,72 +15,103 @@ DOCUMENTATION = r'''
 ---
 module: win_hosts
 version_added: '2.8'
-short_description: Modify hosts file entries on windows.
+short_description: Manages hosts file entries on Windows.
 description:
-  - Allows  the addition, replacement, and removal of ip addresses and aliases from host entries in the windows system hosts file.
+  - Manages hosts file entries on Windows.
+  - Maps IPv4 or IPv6 addresses to canonical names
+  - Adds, removes, or sets cname records for ip and hostname pairs
   - Modifies %windir%\system32\drivers\etc\hosts.
 options:
   state:
     description:
-      - Whether the host should be present or absent.
-      - If only C(host_name) is provided when C(state=absent), the host_name alias will be removed from all host entries.
-      - If only C(ip_address) is provided when C(state=absent), the host entry and all aliases will be removed.
+      - Whether the entry should be present or absent.
+      - If only C(canonical_name) is provided when C(state=absent), then
+        all hosts entries with the canonical name of I(canonical_name)
+        will be removed.
+      - If only C(ip_address) is provided when C(state=absent), then all
+        hosts entries with the ip address of I(ip_address) will be removed.
+      - If C(ip_address) and C(canonical_name) are both omitted when
+        C(state=absent), then all hosts entries will be removed.
     choices:
       - absent
       - present
     default: present
-  host_name:
+  canonical_name:
     description:
-      - An alias or dns name for the host.
+      - A canonical name for the host entry.
       - required for C(state=present).
   ip_address:
     description:
-      - The ip address of the host.
+      - The ip address for the host entry.
+      - Can be either IPv4 (A record) or IPv6 (AAAA record).
       - Required for C(state=present).
-  ip_action:
+  aliases:
+    description:
+      - A list of additional names (cname records) for the host entry.
+      - Only applicable when C(state=present).
+  action:
     choices:
       - add
-      - replace
+      - remove
+      - set
     description:
-      - Only applicable with C(state=absent).
-      - If C(add), the I(host_name) will be added as an entry to I(ip_address).
-      - If C(replace), the I(host_name) will be added as an entry to I(ip_address) and removed from any other entries.
-    default: replace
+      - Controls the behavior of C(aliases).
+      - Only applicable when C(state=present).
+      - If C(add), each alias in I(aliases) will be added to the host entry.
+      - If C(set), each alias in I(aliases) will be added to the host entry,
+        and other aliases will be removed from the entry.
+    default: set
 author:
   - Micah Hunsberger (@mhunsber)
 notes:
+  - Each canonical name can only be mapped to one IPv4 and one IPv6 address.
+    If C(canonical_name) is provided with C(state=present) and is found
+    to be mapped to another IP address that is the same type as, but unique
+    from C(ip_address), then C(canonical_name) and all C(aliases) will
+    be removed from the entry and added to an entry with the provided IP address.
+  - Each alias can only be mapped to one canonical name. If C(aliases) is provided
+    with C(state=present) and an alias is found to be mapped to another canonical
+    name, then the alias will be removed from the entry and added to or removed
+    from (based on I(action)) an entry with the provided canonical name.
   - See also M(win_template), M(win_file), M(win_copy)
 '''
 
 EXAMPLES = r'''
-- name: Map localhost to 127.0.0.1
+- name: Add 127.0.0.1 as an A record for localhost
   win_hosts:
     state: present
-    host_name: localhost
+    canonical_name: localhost
     ip_address: 127.0.0.1
 
-- name: Add ::1 as an ip for localhost
+- name: Add ::1 as an AAAA record for localhost
   win_environment:
     state: present
-    ip_action: add
-    host_name: localhost
+    canonical_name: localhost
     ip_address: '::1'
 
-- name: Remove 'foo' from the list of aliases for 192.168.1.100
+- name: Remove 'bar' and 'zed' from the list of aliases for foo (192.168.1.100)
   win_hosts:
-    state: absent
-    host_name: foo
+    state: present
+    canoncial_name: foo
     ip_address: 192.168.1.100
+    action: remove
+    aliases:
+      - bar
+      - zed
 
-- name: Remove alias 'bar' from all host entries
+- name: Remove hosts entries with canonical name 'bar'
   win_hosts:
     state: absent
-    host_name: bar
+    canonical_name: bar
 
 - name: Remove 10.2.0.1 from the list of hosts
   win_hosts:
     state: absent
     ip_address: 10.2.0.1
+
+- name: Ensure all name resolution is handled by DNS
+  win_hosts:
+    state: absent
 '''
 
 RETURN = r'''
