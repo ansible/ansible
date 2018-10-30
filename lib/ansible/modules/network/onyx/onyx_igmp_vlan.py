99c231ce753117e0ddd825478156b1eb6d69df14
Anas Badaha <anasb@mellanox.com>
2018-10-30 12:59:52 +0000

    Fix Falied Sanity Tests
    Signed-off-by: Anas Badaha <anasb@mellanox.com>

--- lib/ansible/modules/network/onyx/onyx_igmp_vlan.py
+++ lib/ansible/modules/network/onyx/onyx_igmp_vlan.py
@@ -15,80 +15,72 @@ DOCUMENTATION = """
 module: onyx_igmp_vlan
 version_added: '2.8'
 author: 'Anas Badaha(@anasb)'
-short_description: 'Configures IGMP Vlan parameters'
+short_description: Configures IGMP Vlan parameters
 description:
-    - "This module provides declarative management of IGMP vlan configuration on Mellanox ONYX network devices."
+  - This module provides declarative management of IGMP vlan configuration on Mellanox ONYX network devices.
 notes:
-    - 'Tested on ONYX 3.7.0932-01'
+  - Tested on ONYX 3.7.0932-01
 options:
-    vlan_id:
-      description:
-        - VLAN ID, vlan should exist.
-      required: true
-    state:
-      description:
-        - IGMP state.
-      choices:
-        - enabled
-        - disabled
+  vlan_id:
+    description:
+      - VLAN ID, vlan should exist.
+    required: true
+  state:
+    description:
+      - IGMP state.
+    choices: ['enabled', 'disabled']
+    default: enabled
+  mrouter:
+    description:
+      - Configure ip igmp snooping mrouter port on vlan
+    suboptions:
+      state:
+        description:
+          - Enable IGMP snooping mrouter on vlan interface.
+        choices: ['enabled', 'disabled']
         default: enabled
-    mrouter:
-      description:
-        - Configure ip igmp snooping mrouter port on vlan
-      suboptions:
-        state:
-          description:
-            Enable IGMP snooping mrouter on vlan interface.
-          choices:
-            - enabled
-            - disabled
-            default: enabled
-    name:
-      description:
-        - Configure mrouter interface
-      required: true
-    querier:
-      description:
-        - Configure the IGMP querier parameters
-      suboptions:
-        state:
-          - description:
-              - Enable IGMP snooping querier on vlan in the switch.
-          choices:
-            - enabled
-            - disabled
-            default: enabled
-        interval:
-          description:
-            - Update time interval between querier queries, range 60-600
-        address:
-          description:
-            - Update IP address for the querier
-    static_groups:
-      description:
-        - List of IGMP static groups.
-      suboptions:
-        multicast_ip_address:
-          description:
-            - Configure static IP multicast group, range 224.0.1.0-239.255.255.25.
-          required: true
-        name:
-          description:
-            - interface name to configure static groups on it.
-        sources:
-          description:
-            - List of IP sources to be configured
-    version:
-      description:
-        - IGMP snooping operation version on this vlan
-      choices:
-        - V2
-        - V3
-
+  name:
+    description:
+      - Configure mrouter interface
+    required: true
+  querier:
+    description:
+      - Configure the IGMP querier parameters
+    suboptions:
+      state:
+        description:
+          - Enable IGMP snooping querier on vlan in the switch.
+        choices: ['enabled', 'disabled']
+        default: enabled
+      interval:
+        description:
+          - Update time interval between querier queries, range 60-600
+      address:
+        description:
+          - Update IP address for the querier
+  static_groups:
+    description:
+      - List of IGMP static groups.
+    suboptions:
+      multicast_ip_address:
+        description:
+          - Configure static IP multicast group, range 224.0.1.0-239.255.255.25.
+        required: true
+      name:
+        description:
+          - interface name to configure static groups on it.
+      sources:
+        description:
+          - List of IP sources to be configured
+  version:
+    description:
+      - IGMP snooping operation version on this vlan
+    choices: ['V2','V3'] 
 """
 
 EXAMPLES = """
-- onyx_igmp_vlan:
+- name: configure igmp vlan
+  onyx_igmp_vlan:
     state: enabled
     vlan_id: 10
     version:
@@ -230,7 +222,7 @@ class OnyxIgmpVlanModule(BaseOnyxModule):
                     v3_receiver_ports = snooping_group_information[1].get('V3 Receiver Ports')
                     ports_number = v3_receiver_ports[0].get('Port Number')
                     sources = v3_receiver_ports[0].get('Include sources')
-                    if type(ports_number) is list:
+                    if isinstance(ports_number, list):
                         i = 0
                         for port_number in ports_number:
                             sources_dict[port_number] = sources[i]
