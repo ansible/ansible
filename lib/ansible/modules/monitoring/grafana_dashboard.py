3fc6716e8e24a425e43676b1353a54d8719c2f52
Igor Simonov <igor@traceair.net>
2018-11-01 08:44:09 +0300

    module grafana_dashboard fix for work python3 (#47708)
    * fix for work python3
    
    change string to str for work in python3
    
    * delete unusin import module

--- lib/ansible/modules/monitoring/grafana_dashboard.py
+++ lib/ansible/modules/monitoring/grafana_dashboard.py
@@ -136,7 +136,6 @@ uid:
 '''
 
 import json
-import string
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.urls import fetch_url, url_argument_spec
@@ -184,7 +183,7 @@ def get_grafana_version(module, grafana_url, headers):
     if info['status'] == 200:
         try:
             settings = json.loads(r.read())
-            grafana_version = string.split(settings['buildInfo']['version'], '.')[0]
+            grafana_version = str.split(settings['buildInfo']['version'], '.')[0]
         except Exception as e:
             raise GrafanaAPIException(e)
     else:
