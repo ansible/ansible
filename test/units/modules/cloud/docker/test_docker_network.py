f45d60454bf0026364319f8cda6a4ba7e56e2dcc
Stephan Müller <mail@stephanmueller.eu>
2018-11-01 10:46:36 +0100

    docker_network: migrate unittest to pytest

--- test/units/modules/cloud/docker/test_docker_network.py
+++ test/units/modules/cloud/docker/test_docker_network.py
@@ -1,38 +1,27 @@
-import sys
-
-if sys.version_info < (2, 7):
-    import unittest2 as unittest
-else:
-    import unittest
+"""Unit tests for docker_network."""
+import pytest
 
 from ansible.modules.cloud.docker.docker_network import get_ip_version
 
 
-class TestDockerNetwork(unittest.TestCase):
-    """Unit tests for docker_network."""
+@pytest.mark.parametrize("cidr,expected", [
+    ('192.168.0.1/16', 'ipv4'),
+    ('192.168.0.1/24', 'ipv4'),
+    ('192.168.0.1/32', 'ipv4'),
+    ('fdd1:ac8c:0557:7ce2::/64', 'ipv6'),
+    ('fdd1:ac8c:0557:7ce2::/128', 'ipv6'),
+])
+def test_get_ip_version_positives(cidr, expected):
+    assert get_ip_version(cidr) == expected
 
-    def test_get_ip_version(self):
-        """Ensure get_ip_version correctly identifies IP version of a CIDR string."""
 
-        self.assertEqual(get_ip_version('192.168.0.1/16'), 'ipv4')
-        self.assertEqual(get_ip_version('192.168.0.1/24'), 'ipv4')
-        self.assertEqual(get_ip_version('192.168.0.1/32'), 'ipv4')
-
-        self.assertEqual(get_ip_version('fdd1:ac8c:0557:7ce2::/64'), 'ipv6')
-        self.assertEqual(get_ip_version('fdd1:ac8c:0557:7ce2::/128'), 'ipv6')
-
-        with self.assertRaises(ValueError) as e:
-            get_ip_version('192.168.0.1')
-        self.assertEqual('"192.168.0.1" is not a valid CIDR', str(e.exception))
-
-        with self.assertRaises(ValueError) as e:
-            get_ip_version('192.168.0.1/34')
-        self.assertEqual('"192.168.0.1/34" is not a valid CIDR', str(e.exception))
-
-        with self.assertRaises(ValueError) as e:
-            get_ip_version('192.168.0.1/asd')
-        self.assertEqual('"192.168.0.1/asd" is not a valid CIDR', str(e.exception))
-
-        with self.assertRaises(ValueError) as e:
-            get_ip_version('fdd1:ac8c:0557:7ce2::')
-        self.assertEqual('"fdd1:ac8c:0557:7ce2::" is not a valid CIDR', str(e.exception))
+@pytest.mark.parametrize("cidr", [
+    '192.168.0.1',
+    '192.168.0.1/34',
+    '192.168.0.1/asd',
+    'fdd1:ac8c:0557:7ce2::',
+])
+def test_get_ip_version_negatives(cidr):
+    with pytest.raises(ValueError) as e:
+        get_ip_version(cidr)
+    assert '"{0}" is not a valid CIDR'.format(cidr) == str(e.value)
