57baddfedffe5dc669829926f772b294b7d0516a
fz <commit@localhost>
2018-11-05 13:44:49 +0100

    add common/sys_info.py to recursive finder test

--- lib/ansible/module_utils/basic.py
+++ lib/ansible/module_utils/basic.py
@@ -162,14 +162,14 @@ from ansible.module_utils.common.file import (
     _DEFAULT_PERM as DEFAULT_PERM,
     is_executable,
     format_attributes,
-    get_flags_from_attributes
+    get_flags_from_attributes,
 )
 from ansible.module_utils.common.sys_info import (
     get_platform,
     get_distribution,
     get_distribution_version,
     load_platform_subclass,
-    _get_all_subclasses as get_all_subclasses
+    _get_all_subclasses as get_all_subclasses,
 )
 from ansible.module_utils.pycompat24 import get_exception, literal_eval
 from ansible.module_utils.six import (
