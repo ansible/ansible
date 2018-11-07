f1b63552801cc941cea6e0fa941126b09ea1d663
Adam Miller <admiller@redhat.com>
2018-11-07 00:43:57 -0600

    dnf allowerasing
    Add the ability to pass allowerasing to alter the dnf transaction
    behavior.
    
    Fixes #24161
    
    This is effectively a port of the original pull request from
    poettler-ric who has since abandoned the PR
    
        https://github.com/ansible/ansible/pull/34111
    
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- lib/ansible/modules/packaging/os/dnf.py
+++ lib/ansible/modules/packaging/os/dnf.py
@@ -196,6 +196,13 @@ options:
       - Has an effect only if I(download_only) is specified.
     type: str
     version_added: "2.8"
+  allowerasing:
+    description:
+      - If C(yes) it allows  erasing  of  installed  packages to resolve dependencies.
+    required: false
+    type: bool
+    default: "no"
+    version_added: "2.9"
 notes:
   - When used with a `loop:` each package will be processed individually, it is much more efficient to pass the list directly to the `name` option.
   - Group removal doesn't work if the group was installed with Ansible because
@@ -328,6 +335,9 @@ class DnfModule(YumDnf):
             self.with_modules = dnf.base.WITH_MODULES
         except AttributeError:
             self.with_modules = False
+
+        # DNF specific args that are not part of YumDnf
+        self.allowerasing = self.module.params['allowerasing']
 
     def is_lockfile_pid_valid(self):
         # FIXME? it looks like DNF takes care of invalid lock files itself?
@@ -900,7 +910,6 @@ class DnfModule(YumDnf):
         return False  # seems like a sane default
 
     def ensure(self):
-        allow_erasing = False
 
         response = {
             'msg': "",
@@ -1149,13 +1158,13 @@ class DnfModule(YumDnf):
 
                 # Like the dnf CLI we want to allow recursive removal of dependent
                 # packages
-                allow_erasing = True
+                self.allowerasing = True
 
                 if self.autoremove:
                     self.base.autoremove()
 
         try:
-            if not self.base.resolve(allow_erasing=allow_erasing):
+            if not self.base.resolve(allow_erasing=self.allowerasing):
                 if failure_response['failures']:
                     failure_response['msg'] = 'Failed to install some of the specified packages'
                     self.module.fail_json(**failure_response)
@@ -1287,6 +1296,10 @@ def main():
     #   list=available
     #   list=repos
     #   list=pkgspec
+
+    # Extend yumdnf_argument_spec with dnf-specific features that will never be
+    # backported to yum because yum is now in "maintenance mode" upstream
+    yumdnf_argument_spec['argument_spec']['allowerasing']=dict(default=False, type='bool')
 
     module = AnsibleModule(
         **yumdnf_argument_spec
