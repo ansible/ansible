95ac5775202ae10aefaef807baa8e94b8b6eb524
Ivan Semenov <vanyas@gmail.com>
2018-11-08 22:40:23 +1000

    Fix junos_command.py network module to support spaces in rpc args

--- lib/ansible/modules/network/junos/junos_command.py
+++ lib/ansible/modules/network/junos/junos_command.py
@@ -4,7 +4,6 @@
 # (c) 2017, Ansible by Red Hat, inc
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-import shlex
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
