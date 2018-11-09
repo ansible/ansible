796583f2ae0c7b1efd3965812bf56b1cf95debce
Brian Coca <brian.coca+git@gmail.com>
2018-11-09 13:11:08 -0500

    fixed import g

--- packaging/release/changelogs/changelog.py
+++ packaging/release/changelogs/changelog.py
@@ -24,7 +24,7 @@ try:
 except ImportError:
     argcomplete = None
 
-from ansible.constants import C
+import ansible.constants as C
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 CHANGELOG_DIR = os.path.join(BASE_DIR, 'changelogs')
