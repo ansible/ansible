723d24382d62429a7d0afad423f82ed30b486e18
Steve Dodd <stevend@idahood.com>
2018-11-12 00:01:27 -0700

    Fix LLDP to use json (#48318)
    (cherry picked from commit e51964e7a6552bab3cfb7c2dfe07dd33512f477a)

--- lib/ansible/modules/network/nxos/nxos_facts.py
+++ lib/ansible/modules/network/nxos/nxos_facts.py
@@ -400,9 +400,12 @@ class Interfaces(FactsBase):
                 interfaces = self.parse_interfaces(data)
                 self.populate_ipv6_interfaces(interfaces)
 
-        data = self.run('show lldp neighbors')
+        data = self.run('show lldp neighbors', output='json')
         if data:
-            self.facts['neighbors'].update(self.populate_neighbors(data))
+            if isinstance(data, dict):
+                self.facts['neighbors'].update(self.populate_structured_neighbors_lldp(data))
+            else:
+                self.facts['neighbors'].update(self.populate_neighbors(data))
 
         data = self.run('show cdp neighbors detail', output='json')
         if data:
