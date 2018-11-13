de31004dd32bcc2cfa74106f4ffcab93d3f34054
Rhys Campbell <rhys.james.campbell@googlemail.com>
2018-11-13 22:54:10 +0100

    Correct yaml errors

--- lib/ansible/modules/database/mongodb/mongodb_shard.py
+++ lib/ansible/modules/database/mongodb/mongodb_shard.py
@@ -69,8 +69,7 @@ options:
         choices: ["present", "absent"]
 
 notes:
-    - Requires the pymongo Python package on the remote host, version 2.4.2+. This
-      can be installed using pip or the OS package manager. @see U(http://api.mongodb.org/python/current/installation.html)
+    - Requires the pymongo Python package on the remote host, version 2.4.2+. This can be installed using pip or the OS package manager. @see U(http://api.mongodb.org/python/current/installation.html).
 requirements: [ "pymongo" ]
 '''
 
