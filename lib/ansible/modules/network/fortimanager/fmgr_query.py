06d1fdc520637b529d612fb88b659588f4fd446f
Luke Weighall <lweighall@gmail.com>
2018-11-13 17:10:23 -0800

    Fixing Edits. nixing Stale CI flag.

--- lib/ansible/modules/network/fortimanager/fmgr_query.py
+++ lib/ansible/modules/network/fortimanager/fmgr_query.py
@@ -29,7 +29,7 @@ DOCUMENTATION = '''
 ---
 module: fmgr_query
 version_added: "2.8"
-author: Luke Weighall (@lweighall)
+author: Luke Weighall - lweighall
 short_description: Query FortiManager data objects for use in Ansible workflows.
 description:
   - Provides information on data objects within FortiManager so that playbooks can perform conditionals.
@@ -38,54 +38,65 @@ options:
   adom:
     description:
       - The ADOM the configuration should belong to.
-    required: true
+    required: false
     default: root
+
   host:
     description:
-      - The FortiManager's Address.
+      - The FortiManager's address.
     required: true
+
   username:
     description:
       - The username used to authenticate with the FortiManager.
     required: false
+
   password:
     description:
       - The password associated with the username account.
     required: false
+
   object:
     description:
       - The data object we wish to query (device, package, rule, etc). Will expand choices as improves.
     required: true
     choices: ['device', 'cluster_nodes', 'task', 'custom']
+
   custom_endpoint:
     description:
         - ADVANCED USERS ONLY! REQUIRES KNOWLEDGE OF FMGR JSON API!
         - The HTTP Endpoint on FortiManager you wish to GET from.
     required: false
+
   custom_dict:
     description:
         - ADVANCED USERS ONLY! REQUIRES KNOWLEDGE OF FMGR JSON API!
         - DICTIONARY JSON FORMAT ONLY -- Custom dictionary/datagram to send to the endpoint.
     required: false
+
   device_ip:
     description:
       - The IP of the device you want to query.
     required: false
+
   device_unique_name:
     description:
       - The desired "friendly" name of the device you want to query.
     required: false
+
   device_serial:
     description:
       - The serial number of the device you want to query.
     required: false
+
   task_id:
     description:
       - The ID of the task you wish to query status on. If left blank and object = 'task' a list of tasks are returned.
     required: false
+
   nodes:
     description:
-      - A LIST of firewalls in the cluster you want to verify i.e. ["firewall_A","firewall_B"]
+      - A LIST of firewalls in the cluster you want to verify i.e. ["firewall_A","firewall_B"].
     required: false
 '''
 
