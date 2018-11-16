45989023fdd33254869f928db125c1ff4247240c
Ruben Tsirunyan <rubentsirunyan@gmail.com>
2018-11-16 16:48:22 +0400

    zabbix_action: Adding error handling for getting values from server

--- lib/ansible/modules/monitoring/zabbix/zabbix_action.py
+++ lib/ansible/modules/monitoring/zabbix/zabbix_action.py
@@ -297,154 +297,193 @@ class Zapi(object):
         self._zapi = zbx
 
     def check_if_action_exists(self, name):
-        return self._zapi.action.get({
-            "selectOperations": "extend",
-            "selectRecoveryOperations": "extend",
-            "selectFilter": "extend",
-            'selectInventory': 'extend',
-            'filter': {'name': [name]}
-        })
+        try:
+            return self._zapi.action.get({
+                "selectOperations": "extend",
+                "selectRecoveryOperations": "extend",
+                "selectFilter": "extend",
+                'selectInventory': 'extend',
+                'filter': {'name': [name]}
+            })
+        except Exception as e:
+            self._module.fail_json(msg="Failed to check if action '%s' exists: %s" % (name, e))
 
     def get_action_by_name(self, name):
-        action_list = self._zapi.action.get({
-            'output': 'extend',
-            'selectInventory': 'extend',
-            'filter': {'name': [name]}
-        })
-        if len(action_list) < 1:
-            self._module.fail_json(msg="Action not found: " % name)
-        else:
-            return action_list[0]
+        try:
+            action_list = self._zapi.action.get({
+                'output': 'extend',
+                'selectInventory': 'extend',
+                'filter': {'name': [name]}
+            })
+            if len(action_list) < 1:
+                self._module.fail_json(msg="Action not found: " % name)
+            else:
+                return action_list[0]
+        except Exception as e:
+            self._module.fail_json(msg="Failed to get ID of '%s': %s" % (name, e))
 
     # get host by host name
     def get_host_by_host_name(self, host_name):
-        host_list = self._zapi.host.get({
-            'output': 'extend',
-            'selectInventory': 'extend',
-            'filter': {'host': [host_name]}
-        })
-        if len(host_list) < 1:
-            self._module.fail_json(msg="Host not found: %s" % host_name)
-        else:
-            return host_list[0]
+        try:
+            host_list = self._zapi.host.get({
+                'output': 'extend',
+                'selectInventory': 'extend',
+                'filter': {'host': [host_name]}
+            })
+            if len(host_list) < 1:
+                self._module.fail_json(msg="Host not found: %s" % host_name)
+            else:
+                return host_list[0]
+        except Exception as e:
+            self._module.fail_json(msg="Failed to get host '%s': %s" % (host_name, e))
 
     # get hostgroup by hostgroup name
     def get_hostgroup_by_hostgroup_name(self, hostgroup_name):
-        hostgroup_list = self._zapi.hostgroup.get({
-            'output': 'extend',
-            'selectInventory': 'extend',
-            'filter': {'name': [hostgroup_name]}
-        })
-        if len(hostgroup_list) < 1:
-            self._module.fail_json(msg="Host group not found: %s" % hostgroup_name)
-        else:
-            return hostgroup_list[0]
+        try:
+            hostgroup_list = self._zapi.hostgroup.get({
+                'output': 'extend',
+                'selectInventory': 'extend',
+                'filter': {'name': [hostgroup_name]}
+            })
+            if len(hostgroup_list) < 1:
+                self._module.fail_json(msg="Host group not found: %s" % hostgroup_name)
+            else:
+                return hostgroup_list[0]
+        except Exception as e:
+            self._module.fail_json(msg="Failed to get host group '%s': %s" % (host_group, e))
 
     # get template by template name
     def get_template_by_template_name(self, template_name):
-        template_list = self._zapi.template.get({
-            'output': 'extend',
-            'selectInventory': 'extend',
-            'filter': {'host': [template_name]}
-        })
-        if len(template_list) < 1:
-            self._module.fail_json(msg="Template not found: %s" % template_name)
-        else:
-            return template_list[0]
+        try:
+            template_list = self._zapi.template.get({
+                'output': 'extend',
+                'selectInventory': 'extend',
+                'filter': {'host': [template_name]}
+            })
+            if len(template_list) < 1:
+                self._module.fail_json(msg="Template not found: %s" % template_name)
+            else:
+                return template_list[0]
+        except Exception as e:
+            self._module.fail_json(msg="Failed to get template '%s': %s" % (template_name, e))
 
     def get_trigger_by_trigger_name(self, trigger_name):
-        trigger_list = self._zapi.trigger.get({
-            'output': 'extend',
-            'selectInventory': 'extend',
-            'filter': {'description': [trigger_name]}
-        })
-        if len(trigger_list) < 1:
-            self._module.fail_json(msg="Trigger not found: %s" % trigger_name)
-        else:
-            return trigger_list[0]
+        try:
+            trigger_list = self._zapi.trigger.get({
+                'output': 'extend',
+                'selectInventory': 'extend',
+                'filter': {'description': [trigger_name]}
+            })
+            if len(trigger_list) < 1:
+                self._module.fail_json(msg="Trigger not found: %s" % trigger_name)
+            else:
+                return trigger_list[0]
+        except Exception as e:
+            self._module.fail_json(msg="Failed to get trigger '%s': %s" % (trigger_name, e))
 
     def get_discovery_rule_by_discovery_rule_name(self, discovery_rule_name):
-        discovery_rule_list = self._zapi.drule.get({
-            'output': 'extend',
-            'selectInventory': 'extend',
-            'filter': {'name': [discovery_rule_name]}
-        })
-        if len(discovery_rule_list) < 1:
-            self._module.fail_json(msg="Discovery rule not found: %s" % discovery_rule_name)
-        else:
-            return discovery_rule_list[0]
+        try:
+            discovery_rule_list = self._zapi.drule.get({
+                'output': 'extend',
+                'selectInventory': 'extend',
+                'filter': {'name': [discovery_rule_name]}
+            })
+            if len(discovery_rule_list) < 1:
+                self._module.fail_json(msg="Discovery rule not found: %s" % discovery_rule_name)
+            else:
+                return discovery_rule_list[0]
+        except Exception as e:
+            self._module.fail_json(msg="Failed to get discovery rule '%s': %s" % (discovery_rule_name, e))
 
     def get_discovery_check_by_discovery_check_name(self, discovery_check_name):
-        discovery_check_list = self._zapi.dcheck.get({
-            'output': 'extend',
-            'selectInventory': 'extend',
-            'filter': {'name': [discovery_check_name]}
-        })
-        if len(discovery_check_list) < 1:
-            self._module.fail_json(msg="Discovery check not found: %s" % discovery_check_name)
-        else:
-            return discovery_check_list[0]
+        try:
+            discovery_check_list = self._zapi.dcheck.get({
+                'output': 'extend',
+                'selectInventory': 'extend',
+                'filter': {'name': [discovery_check_name]}
+            })
+            if len(discovery_check_list) < 1:
+                self._module.fail_json(msg="Discovery check not found: %s" % discovery_check_name)
+            else:
+                return discovery_check_list[0]
+        except Exception as e:
+            self._module.fail_json(msg="Failed to get discovery check '%s': %s" % (discovery_check_name, e))
 
     def get_proxy_by_proxy_name(self, proxy_name):
-        proxy_list = self._zapi.proxy.get({
-            'output': 'extend',
-            'selectInventory': 'extend',
-            'filter': {'host': [proxy_name]}
-        })
-        if len(proxy_list) < 1:
-            self._module.fail_json(msg="Proxy not found: %s" % proxy_name)
-        else:
-            return proxy_list[0]
+        try:
+            proxy_list = self._zapi.proxy.get({
+                'output': 'extend',
+                'selectInventory': 'extend',
+                'filter': {'host': [proxy_name]}
+            })
+            if len(proxy_list) < 1:
+                self._module.fail_json(msg="Proxy not found: %s" % proxy_name)
+            else:
+                return proxy_list[0]
+        except Exception as e:
+            self._module.fail_json(msg="Failed to get proxy '%s': %s" % (proxy_name, e))
 
     # get mediatype by mediatype name
     def get_mediatype_by_mediatype_name(self, mediatype_name):
-        mediatype_list = self._zapi.mediatype.get({
-            'output': 'extend',
-            'selectInventory': 'extend',
-            'filter': {'description': [mediatype_name]}
-        })
-        if len(mediatype_list) < 1:
-            self._module.fail_json(msg="Media type not found: %s" % mediatype_name)
-        else:
-            return mediatype_list[0]
+        try:
+            mediatype_list = self._zapi.mediatype.get({
+                'output': 'extend',
+                'selectInventory': 'extend',
+                'filter': {'description': [mediatype_name]}
+            })
+            if len(mediatype_list) < 1:
+                self._module.fail_json(msg="Media type not found: %s" % mediatype_name)
+            else:
+                return mediatype_list[0]
+        except Exception as e:
+            self._module.fail_json(msg="Failed to get mediatype '%s': %s" % (mediatype_name, e))
 
     # get user by user name
     def get_user_by_user_name(self, user_name):
-        user_list = self._zapi.user.get({
-            'output': 'extend',
-            'selectInventory':
-            'extend', 'filter': {'alias': [user_name]}
-        })
-        if len(user_list) < 1:
-            self._module.fail_json(msg="User not found: %s" % user_name)
-        else:
-            return user_list[0]
+        try:
+            user_list = self._zapi.user.get({
+                'output': 'extend',
+                'selectInventory':
+                'extend', 'filter': {'alias': [user_name]}
+            })
+            if len(user_list) < 1:
+                self._module.fail_json(msg="User not found: %s" % user_name)
+            else:
+                return user_list[0]
+        except Exception as e:
+            self._module.fail_json(msg="Failed to get user '%s': %s" % (user_name, e))
 
     # get usergroup by usergroup name
     def get_usergroup_by_usergroup_name(self, usergroup_name):
-        usergroup_list = self._zapi.usergroup.get({
-            'output': 'extend',
-            'selectInventory': 'extend',
-            'filter': {'name': [usergroup_name]}
-        })
-        if len(usergroup_list) < 1:
-            self._module.fail_json(msg="User group not found: %s" % usergroup_name)
-        else:
-            return usergroup_list[0]
+        try:
+            usergroup_list = self._zapi.usergroup.get({
+                'output': 'extend',
+                'selectInventory': 'extend',
+                'filter': {'name': [usergroup_name]}
+            })
+            if len(usergroup_list) < 1:
+                self._module.fail_json(msg="User group not found: %s" % usergroup_name)
+            else:
+                return usergroup_list[0]
+        except Exception as e:
+            self._module.fail_json(msg="Failed to get user group '%s': %s" % (usergroup_name, e))
 
     # get script by script name
     def get_script_by_script_name(self, script_name):
-        if script_name is None:
-            return {}
-        script_list = self._zapi.script.get({
-            'output': 'extend',
-            'selectInventory': 'extend',
-            'filter': {'name': [script_name]}
-        })
-        if len(script_list) < 1:
-            self._module.fail_json(msg="Script not found: %s" % script_name)
-        else:
-            return script_list[0]
+        try:
+            if script_name is None:
+                return {}
+            script_list = self._zapi.script.get({
+                'output': 'extend',
+                'selectInventory': 'extend',
+                'filter': {'name': [script_name]}
+            })
+            if len(script_list) < 1:
+                self._module.fail_json(msg="Script not found: %s" % script_name)
+            else:
+                return script_list[0]
+        except Exception as e:
+            self._module.fail_json(msg="Failed to get script '%s': %s" % (script_name, e))
 
 
 class Action(object):
