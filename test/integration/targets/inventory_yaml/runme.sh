f33d9de1571a63dbf2035a6ee6f9a869a7efab4b
Brian Coca <brian.coca+git@gmail.com>
2018-11-19 12:38:56 -0500

    ignore whitespace

--- test/integration/targets/inventory_yaml/runme.sh
+++ test/integration/targets/inventory_yaml/runme.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # handle empty/commented out group keys correctly https://github.com/ansible/ansible/issues/47254
-diff <(ansible-inventory -i ./test.yml --list "$@") success.json
+diff -w <(ansible-inventory -i ./test.yml --list "$@") success.json
