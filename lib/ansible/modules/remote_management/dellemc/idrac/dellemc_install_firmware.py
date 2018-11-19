3ad4d4405217272458329758c958dff42574a81a
rajeevarakkal <36444805+rajeevarakkal@users.noreply.github.com>
2018-11-19 07:36:31 -0800

    sample is added in the return section

--- lib/ansible/modules/remote_management/dellemc/idrac/dellemc_install_firmware.py
+++ lib/ansible/modules/remote_management/dellemc/idrac/dellemc_install_firmware.py
@@ -101,6 +101,26 @@ dest:
     description: Updates firmware from a repository on a network share (CIFS, NFS).
     returned: success
     type: string
+    sample: "{
+        "@odata.context": "/redfish/v1/$metadata#DellJob.DellJob",
+        "@odata.id": "/redfish/v1/Managers/iDRAC.Embedded.1/Jobs/JID_XXXXXXXXXXXXX",
+        "@odata.type": "#DellJob.v1_0_0.DellJob",
+        "CompletionTime": "2018-01-24T00:47:33",
+        "Description": "Job Instance",
+        "EndTime": null,
+        "Id": "JID_XXXXXXXXXXXX",
+        "JobState": "Completed",
+        "JobType": "ImportConfiguration",
+        "Message": "Successfully imported and applied Server Configuration Profile.",
+        "MessageArgs": [],
+        "MessageId": "SYSXXX",
+        "Name": "Import Configuration",
+        "PercentComplete": 100,
+        "StartTime": "TIME_NOW",
+        "Status": "Success",
+        "TargetSettingsURI": null,
+        "retval": true
+    }"
 """
 
 
