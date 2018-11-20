f57e921300d4af46903c8edb196d2f15f4ee27f2
Matt Martz <matt@sivel.net>
2018-11-20 12:13:37 -0600

    Implement reentrant lock for class instantiation in Singleton

--- lib/ansible/utils/singleton.py
+++ lib/ansible/utils/singleton.py
@@ -5,6 +5,8 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+from threading import RLock
+
 
 class Singleton(type):
     """Metaclass for classes that wish to implement Singleton
@@ -14,8 +16,14 @@ class Singleton(type):
     def __init__(cls, name, bases, dct):
         super(Singleton, cls).__init__(name, bases, dct)
         cls.__instance = None
+        cls.__rlock = RLock()
 
     def __call__(cls, *args, **kw):
-        if cls.__instance is None:
-            cls.__instance = super(Singleton, cls).__call__(*args, **kw)
+        if cls.__instance is not None:
+            return cls.__instance
+
+        with cls.__rlock:
+            if cls.__instance is None:
+                cls.__instance = super(Singleton, cls).__call__(*args, **kw)
+
         return cls.__instance
