09f3ab20bc443a3cce3137fd402114eae96b62b5
Matt Martz <matt@sivel.net>
2018-11-20 09:35:50 -0600

    Singleton improvements

--- lib/ansible/utils/singleton.py
+++ lib/ansible/utils/singleton.py
@@ -1,17 +1,5 @@
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (c) 2017 Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 # Make coding more python3-ish
 from __future__ import (absolute_import, division, print_function)
@@ -25,9 +13,9 @@ class Singleton(type):
     """
     def __init__(self, name, bases, dct):
         super(Singleton, self).__init__(name, bases, dct)
-        self.instance = None
+        self.__instance = None
 
     def __call__(self, *args, **kw):
-        if self.instance is None:
-            self.instance = super(Singleton, self).__call__(*args, **kw)
-        return self.instance
+        if self.__instance is None:
+            self.__instance = super(Singleton, self).__call__(*args, **kw)
+        return self.__instance
