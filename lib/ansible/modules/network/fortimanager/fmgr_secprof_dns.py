727e23f3134a60de3a5486bd11c94deb49a4a0c4
cse <awelsh@fortinet.com>
2018-11-20 14:14:19 -0700

    Adding @ in author names per @Gundalow's request per Ansible's guidelines

--- lib/ansible/modules/network/fortimanager/fmgr_secprof_dns.py
+++ lib/ansible/modules/network/fortimanager/fmgr_secprof_dns.py
@@ -29,9 +29,9 @@ DOCUMENTATION = '''
 module: fmgr_secprof_dns
 version_added: "2.8"
 author:
-    - Luke Weighall (lweighall)
-    - Andrew Welsh (Ghilli3)
-    - Jim Huber (p4r4n0y1ng)
+    - Luke Weighall (@lweighall)
+    - Andrew Welsh (@Ghilli3)
+    - Jim Huber (@p4r4n0y1ng)
 short_description: Manage DNS security profiles in FortiManager
 description:
   -  Manage DNS security profiles in FortiManager
