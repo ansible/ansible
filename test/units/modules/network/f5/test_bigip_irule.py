e7d1c0fb77363d25a30627b4626f0a51aa3cf7c0
Tim Rupp <caphrim007@gmail.com>
2018-11-19 19:02:02 -0800

    Removes f5-sdk references from bigip modules (#48911)

--- test/units/modules/network/f5/test_bigip_irule.py
+++ test/units/modules/network/f5/test_bigip_irule.py
@@ -32,22 +32,20 @@ try:
 
     from test.units.modules.utils import set_module_args
 except ImportError:
-    try:
-        from ansible.modules.network.f5.bigip_irule import Parameters
-        from ansible.modules.network.f5.bigip_irule import ModuleManager
-        from ansible.modules.network.f5.bigip_irule import ArgumentSpec
-        from ansible.modules.network.f5.bigip_irule import GtmManager
-        from ansible.modules.network.f5.bigip_irule import LtmManager
+    from ansible.modules.network.f5.bigip_irule import Parameters
+    from ansible.modules.network.f5.bigip_irule import ModuleManager
+    from ansible.modules.network.f5.bigip_irule import ArgumentSpec
+    from ansible.modules.network.f5.bigip_irule import GtmManager
+    from ansible.modules.network.f5.bigip_irule import LtmManager
 
-        # Ansible 2.8 imports
-        from units.compat import unittest
-        from units.compat.mock import Mock
-        from units.compat.mock import patch
-        from units.compat.mock import mock_open
+    # Ansible 2.8 imports
+    from units.compat import unittest
+    from units.compat.mock import Mock
+    from units.compat.mock import patch
+    from units.compat.mock import mock_open
 
-        from units.modules.utils import set_module_args
-    except ImportError:
-        raise SkipTest("F5 Ansible modules require the f5-sdk Python library")
+    from units.modules.utils import set_module_args
+
 
 fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
 fixture_data = {}
