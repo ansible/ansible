546b08482b519417818bbf615ed5b5f3425e5b8f
Jens Stark <30623972+jstark1@users.noreply.github.com>
2018-11-21 15:12:22 +0100

    added Linebreak
    added Linebreak

--- lib/ansible/modules/windows/win_feature.py
+++ lib/ansible/modules/windows/win_feature.py
@@ -24,7 +24,8 @@ description:
 options:
   name:
     description:
-      - Names of roles or features to install as a single feature or a comma-separated list of features. To list all available features use the PowerShell command Get-WindowsFeature.
+      - Names of roles or features to install as a single feature or a comma-separated list of features. 
+        To list all available features use the PowerShell command Get-WindowsFeature.
     required: yes
     type: list
   state:
