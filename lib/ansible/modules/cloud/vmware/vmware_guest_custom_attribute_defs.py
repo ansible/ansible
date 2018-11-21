8f41270a010c00d058c70bdccdc611df8b454139
John R Barker <john@johnrbarker.com>
2018-11-21 15:10:41 +0000

    Bulk fix DOCUMENTATION.author (Part 3) (#48990)

--- lib/ansible/modules/cloud/vmware/vmware_guest_custom_attribute_defs.py
+++ lib/ansible/modules/cloud/vmware/vmware_guest_custom_attribute_defs.py
@@ -23,7 +23,7 @@ description:
     - This module can be used to add and remove custom attributes definitions for the given virtual machine from VMWare.
 version_added: 2.7
 author:
-    - Jimmy Conner
+    - Jimmy Conner (@cigamit)
     - Abhijeet Kasurde (@Akasurde)
 notes:
     - Tested on vSphere 6.5
