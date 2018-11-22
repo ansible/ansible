53057c8bdb0df5ede095f9ab45a50dba9890c8cf
Tad M <wm284@cornell.edu>
2018-11-21 19:40:57 -0500

    move full test run back to the end

--- test/integration/targets/ecs_cluster/runme.sh
+++ test/integration/targets/ecs_cluster/runme.sh
@@ -12,11 +12,6 @@ trap 'rm -rf "${MYTMPDIR}"' EXIT
 # but for the python3 tests we need virtualenv to use python3
 PYTHON=${ANSIBLE_TEST_PYTHON_INTERPRETER:-python}
 
-# Run full test suite
-virtualenv --system-site-packages --python "${PYTHON}" "${MYTMPDIR}/botocore-recent"
-source "${MYTMPDIR}/botocore-recent/bin/activate"
-$PYTHON -m pip install 'botocore>=1.8.4' boto3
-ansible-playbook -i ../../inventory -e @../../integration_config.yml -e @../../cloud-config-aws.yml -v playbooks/full_test.yml "$@"
 
 
 # Test graceful failure for older versions of botocore
@@ -45,8 +40,6 @@ virtualenv --system-site-packages --python "${PYTHON}" "${MYTMPDIR}/botocore-1.8
 source "${MYTMPDIR}/botocore-1.8.5/bin/activate"
 $PYTHON -m pip install 'botocore>1.8.4' boto3
 ansible-playbook -i ../../inventory -e @../../integration_config.yml -e @../../cloud-config-aws.yml -v playbooks/network_force_new_deployment.yml "$@"
-
-
 
 # Run full test suite
 virtualenv --system-site-packages --python "${PYTHON}" "${MYTMPDIR}/botocore-recent"
