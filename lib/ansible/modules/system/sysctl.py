f4b01c73c87d502a97796c80c22204fe23bf59d3
Marcel Weinberg <marcel.weinberg@secucloud.com>
2018-11-28 19:44:50 +0100

    Rename ignoreerrors to ignore_errors
    This PR is basically to be consistent with all other modules.
    
    The sysctl module is the only one where the attribute is called ignoreerrors and not ignore_errors.
    
    The new parameter is called ignore_errors but supports ignoreerrors as an alias.
    
    The alias ensures that backward compatibility is given and this is not a breaking change.

--- lib/ansible/modules/system/sysctl.py
+++ lib/ansible/modules/system/sysctl.py
@@ -36,11 +36,12 @@ options:
             - Whether the entry should be present or absent in the sysctl file.
         choices: [ "present", "absent" ]
         default: present
-    ignoreerrors:
+    ignore_errors:
         description:
             - Use this option to ignore errors about unknown keys.
         type: bool
         default: 'no'
+        aliases: [ 'ignoreerrors' ]
     reload:
         description:
             - If C(yes), performs a I(/sbin/sysctl -p) if the C(sysctl_file) is
@@ -241,13 +242,13 @@ class SysctlModule(object):
             thiscmd = "%s %s=%s" % (self.sysctl_cmd, token, value)
         elif self.platform == 'freebsd':
             ignore_missing = ''
-            if self.args['ignoreerrors']:
+            if self.args['ignore_errors']:
                 ignore_missing = '-i'
             # freebsd doesn't accept -w, but since it's not needed, just drop it
             thiscmd = "%s %s %s=%s" % (self.sysctl_cmd, ignore_missing, token, value)
         else:
             ignore_missing = ''
-            if self.args['ignoreerrors']:
+            if self.args['ignore_errors']:
                 ignore_missing = '-e'
             thiscmd = "%s %s -w %s=%s" % (self.sysctl_cmd, ignore_missing, token, value)
         rc, out, err = self.module.run_command(thiscmd)
@@ -276,7 +277,7 @@ class SysctlModule(object):
         else:
             # system supports reloading via the -p flag to sysctl, so we'll use that
             sysctl_args = [self.sysctl_cmd, '-p', self.sysctl_file]
-            if self.args['ignoreerrors']:
+            if self.args['ignore_errors']:
                 sysctl_args.insert(1, '-e')
 
             rc, out, err = self.module.run_command(sysctl_args)
@@ -368,7 +369,7 @@ def main():
             state=dict(default='present', choices=['present', 'absent']),
             reload=dict(default=True, type='bool'),
             sysctl_set=dict(default=False, type='bool'),
-            ignoreerrors=dict(default=False, type='bool'),
+            ignore_errors=dict(aliases=['ignoreerrors'], default=False, type='bool'),
             sysctl_file=dict(default='/etc/sysctl.conf', type='path')
         ),
         supports_check_mode=True,
