34c3386a6ca9d1e75b044e8a3cd7b53399e0eaee
Sumit Jaiswal <sjaiswal@redhat.com>
2018-11-28 17:53:40 +0530

    cleanup
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>

--- lib/ansible/module_utils/web_infrastructure/ansible_tower/exceptions.py
+++ lib/ansible/module_utils/web_infrastructure/ansible_tower/exceptions.py
@@ -1,5 +1,4 @@
 # Copyright 2018, Ansible, Inc.
-# Luke Sneeringer <lsneeringer@ansible.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
