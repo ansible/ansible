071378f933be9f3667c23ccc5844407fc41ee5ec
viky 600833 <victor600833@gmail.com>
2018-11-28 21:45:21 +0100

    preventing zero-length string value for option --limit

--- lib/ansible/cli/__init__.py
+++ lib/ansible/cli/__init__.py
@@ -575,6 +575,10 @@ class CLI(with_metaclass(ABCMeta, object)):
 
         self.options, self.args = self.parser.parse_args(self.args[1:])
 
+        # --limit must receive non-zero length string
+        if hasattr(self.options, 'subset') and  self.options.subset != C.DEFAULT_SUBSET and isinstance( self.options.subset, string_types) and len(self.options.subset) == 0:
+            raise ValueError("ansible-playbook: error: --limit option requires non-zero length string")
+
         # process tags
         if hasattr(self.options, 'tags') and not self.options.tags:
             # optparse defaults does not do what's expected
