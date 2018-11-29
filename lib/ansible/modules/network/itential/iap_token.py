e3a5c6ef697dca554ca66f989d14754c6a3809a1
Ankit <>
2018-11-29 02:27:09 -0500

    updated after pep8 test

--- lib/ansible/modules/network/itential/iap_token.py
+++ lib/ansible/modules/network/itential/iap_token.py
@@ -52,10 +52,10 @@ options:
       - By default using http
     type: bool
     default: False
-    
+
   validate_certs:
     description:
-      - If C(no), SSL certificates for the target url will not be validated. This should only be used 
+      - If C(no), SSL certificates for the target url will not be validated. This should only be used
         on personally controlled sites using self-signed certificates.
     type: bool
     default: False
