9cc9cbed84bac520a7e3156d2b297ab535b330c2
Ankit <>
2018-11-29 00:19:27 -0500

    updated test import with unittest

--- test/units/modules/network/itential/test_iap_token.py
+++ test/units/modules/network/itential/test_iap_token.py
@@ -25,7 +25,7 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 
-from test.units.compat import unittest
+import unittest
 
 
 class TestClass(unittest.TestCase):
