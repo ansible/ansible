33b84d630574adc9d44663dfd37ed251f73ed6a0
rajeevarakkal <36444805+rajeevarakkal@users.noreply.github.com>
2018-11-29 04:02:34 -0800

    removed the action from module name

--- lib/ansible/modules/remote_management/dellemc/idrac/dellemc_firmware.py
+++ lib/ansible/modules/remote_management/dellemc/idrac/dellemc_firmware.py
@@ -21,7 +21,7 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 
 DOCUMENTATION = """
 ---
-module: dellemc_install_firmware
+module: dellemc_firmware
 short_description: Firmware update from a repository on a network share (CIFS, NFS).
 version_added: "2.8"
 description:
@@ -83,7 +83,7 @@ author: "Rajeev Arakkal (@rajeevarakkal)"
 EXAMPLES = """
 ---
 - name: Update firmware from repository on a Network Share
-  dellemc_install_firmware:
+  dellemc_firmware:
        idrac_ip:   "{{ idrac_ip }}"
        idrac_user: "{{ idrac_user }}"
        idrac_pwd:  "{{ idrac_pwd }}"
