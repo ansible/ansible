d980290cd9a491745a56244146d81edaade105d2
bverschueren <bverschueren@users.noreply.github.com>
2018-11-30 11:24:04 +0100

    comparing StorageConnection.password breaks idempotency (#48933)

--- lib/ansible/modules/cloud/ovirt/ovirt_storage_connection.py
+++ lib/ansible/modules/cloud/ovirt/ovirt_storage_connection.py
@@ -176,7 +176,6 @@ class StorageConnectionModule(BaseModule):
             equal(self.param('nfs_timeout'), entity.nfs_timeo) and
             equal(self.param('nfs_retrans'), entity.nfs_retrans) and
             equal(self.param('mount_options'), entity.mount_options) and
-            equal(self.param('password'), entity.password) and
             equal(self.param('username'), entity.username) and
             equal(self.param('port'), entity.port) and
             equal(self.param('target'), entity.target) and
