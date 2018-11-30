8671b82e301a67dad6f17dd7977100f63daab59d
rajeevarakkal <36444805+rajeevarakkal@users.noreply.github.com>
2018-11-30 08:54:10 -0800

    moved the validation to init block

--- lib/ansible/module_utils/remote_management/dellemc/dellemc_idrac.py
+++ lib/ansible/module_utils/remote_management/dellemc/dellemc_idrac.py
@@ -33,14 +33,16 @@ class iDRACConnection:
             results['msg'] = "Dell EMC OMSDK library is required for this module"
             module.fail_json(**results)
         self.module = module
+        self.idrac_ip = self.module.params['idrac_ip']
+        self.idrac_user = self.module.params['idrac_user']
+        self.idrac_pwd = self.module.params['idrac_pwd']
+        self.idrac_port = self.module.params['idrac_port']
+        if not all((self.idrac_ip, self.idrac_user, self.idrac_pwd)):
+            self.module.fail_json(msg="hostname, username and password required")
 
     def __enter__(self):
         self.handle = None
         results = {}
-        idrac_ip = self.module.params['idrac_ip']
-        idrac_user = self.module.params['idrac_user']
-        idrac_pwd = self.module.params['idrac_pwd']
-        idrac_port = self.module.params['idrac_port']
         try:
             sd = sdkinfra()
             sd.importPath()
@@ -48,15 +50,12 @@ class iDRACConnection:
             results['msg'] = "Could not initialize drivers"
             results['exception'] = str(e)
             self.module.fail_json(**results)
-        if not all((idrac_ip, idrac_user, idrac_pwd)):
-            self.module.fail_json(msg="hostname, username and password required")
-        else:
-            creds = UserCredentials(idrac_user, idrac_pwd)
-            pOp = WsManOptions(port=idrac_port)
-            idrac = sd.get_driver(sd.driver_enum.iDRAC, idrac_ip, creds, pOptions=pOp)
-            if idrac is None:
-                msg = "Could not find device driver for iDRAC with IP Address: {}".format(idrac_ip)
-                self.module.fail_json(msg=msg)
+        creds = UserCredentials(self.idrac_user, self.idrac_pwd)
+        pOp = WsManOptions(port=self.idrac_port)
+        idrac = sd.get_driver(sd.driver_enum.iDRAC, self.idrac_ip, creds, pOptions=pOp)
+        if idrac is None:
+            msg = "Could not find device driver for iDRAC with IP Address: {}".format(self.idrac_ip)
+            self.module.fail_json(msg=msg)
         self.handle = idrac
         return self.handle
 
