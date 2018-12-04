a8d0cb145842d61cf81315ce688e19fb9f502f08
andrey_chertolyas <andrey.chertolyas@airbnb.com>
2018-12-04 12:47:48 +0800

    Replaced str() with to_text() as more python version safe

--- lib/ansible/plugins/action/synchronize.py
+++ lib/ansible/plugins/action/synchronize.py
@@ -49,7 +49,7 @@ class ActionModule(ActionBase):
         return path
 
     def _host_is_ipv6_address(self, host):
-        return ':' in str(host)
+        return ':' in to_text(host)
 
     def _format_rsync_rsh_target(self, host, path, user):
         ''' formats rsync rsh target, escaping ipv6 addresses if needed '''
