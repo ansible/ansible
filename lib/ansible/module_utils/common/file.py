9ba36c530bda3cfcc5fb03f6afca838e4d0f1648
acalm <commit@localhost>
2018-12-05 21:20:41 +0100

    fcntl.lockf() have unusual side effects

--- lib/ansible/module_utils/common/file.py
+++ lib/ansible/module_utils/common/file.py
@@ -116,9 +116,9 @@ class LockTimeout(Exception):
 
 class FileLock:
     '''
-    Currently FileLock is implemented via fcntl.lockf on a lock file, however this
-    behaviour may change in the future. Avoid mixing lock types fcntl.lockf,
-    fcntl.lockf and module_utils.common.file.FileLock as it will certainly cause
+    Currently FileLock is implemented via fcntl.flock on a lock file, however this
+    behaviour may change in the future. Avoid mixing lock types fcntl.flock,
+    fcntl.flock and module_utils.common.file.FileLock as it will certainly cause
     unwanted and/or unexpected behaviour
     '''
     def __init__(self):
@@ -137,7 +137,7 @@ class FileLock:
 
     def set_lock(self, path, lock_timeout=None):
         '''
-        Set lock on given path via fcntl.lockf(), note that using
+        Set lock on given path via fcntl.flock(), note that using
         locks does not guarantee exclusiveness unless all accessing
         processes honor locks.
 
@@ -157,18 +157,18 @@ class FileLock:
         self.lockfd = open(lock_path_b, 'ab')
 
         if lock_timeout is None or lock_timeout < 0:
-            fcntl.lockf(self.lockfd, fcntl.LOCK_EX)
+            fcntl.flock(self.lockfd, fcntl.LOCK_EX)
             return True
 
         if lock_timeout == 0:
-            fcntl.lockf(self.lockfd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            fcntl.flock(self.lockfd, fcntl.LOCK_EX | fcntl.LOCK_NB)
             return True
 
         if lock_timeout > 0:
             e_secs = 0
             while e_secs < lock_timeout:
                 try:
-                    fcntl.lockf(self.lockfd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                    fcntl.flock(self.lockfd, fcntl.LOCK_EX | fcntl.LOCK_NB)
                     return True
                 except r_exception:
                     time.sleep(l_wait)
@@ -189,7 +189,7 @@ class FileLock:
             return True
 
         try:
-            fcntl.lockf(self.lockfd, fcntl.LOCK_UN)
+            fcntl.flock(self.lockfd, fcntl.LOCK_UN)
             self.lockfd.close()
         except ValueError:  # file wasn't opened, let context manager fail gracefully
             pass
