4305b417f027097aab23832aa0757171ba2e042b
Ripon Banik <ripon.banik@tcorp.nsw.gov.au>
2018-12-05 15:56:42 +1100

    Add the win_git module

---
+++ lib/ansible/modules/windows/win_git.ps1
@@ -0,0 +1,421 @@
+#!powershell
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# Anatoliy Ivashina <tivrobo@gmail.com>
+# Pablo Estigarribia <pablodav@gmail.com>
+# Michael Hay <project.hay@gmail.com>
+# Ripon Banik <ripon.banik@gmail.com>
+
+#Requires -Module Ansible.ModuleUtils.Legacy.psm1
+
+$params = Parse-Args -arguments $args -supports_check_mode $true
+$check_mode  = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
+
+# Module Params
+$repo = Get-AnsibleParam -obj $params -name "repo"  -type "str" -failifempty $true -aliases "name"
+$dest = Get-AnsibleParam -obj $params -name "dest"  -type "str" 
+$remote = Get-AnsibleParam -obj $params -name "remote" -type "str" -default "origin"
+$version = Get-AnsibleParam -obj $params -name "version" -type "str" -default "HEAD"
+$clone = ConvertTo-Bool (Get-AnsibleParam -obj $params -name "clone" -default $true)
+$update = ConvertTo-Bool (Get-AnsibleParam -obj $params -name "update" -default $false)
+$replace_dest = ConvertTo-Bool (Get-AnsibleParam -obj $params -name "replace_dest" -default $false)
+$accept_hostkey = ConvertTo-Bool (Get-AnsibleParam -obj $params -name "accept_hostkey" -default $true)
+$depth =  Get-AnsibleParam -obj $params -name "depth"  -type "int" -default $null
+$key_file = Get-AnsibleParam -obj $params -name "key_file" -type "str"
+
+$result = @{
+    win_git = @{
+        repo              = $null
+        dest              = $null
+        key_file          = $null       
+        replace_dest      = $false
+        accept_hostkey    = $true
+        update            = $false
+        remote            = "origin" 
+        version           = "HEAD"
+        msg               = $null 
+    }
+    changed = $false    
+}
+
+# Add Git to PATH variable
+# Test with git 2.14
+$env:Path += ";" + "C:\Program Files\Git\bin"
+$env:Path += ";" + "C:\Program Files\Git\usr\bin"
+$env:Path += ";" + "C:\Program Files (x86)\Git\bin"
+$env:Path += ";" + "C:\Program Files (x86)\Git\usr\bin"
+$env:Path += ";" + "C:\Program Files\Git\cmd"
+$env:Path += ";" + "C:\Program Files (x86)\Git\cmd"
+
+# Functions
+function Find-Command {
+    [CmdletBinding()]
+    param(
+      [Parameter(Mandatory=$true, Position=0)] [string] $command
+    )
+    $installed = get-command $command -erroraction Ignore
+    write-verbose "$installed"
+    if ($installed){
+        return $installed
+    }
+    return $null
+}
+
+function FindGit {
+    [CmdletBinding()]
+    param()
+    $p = Find-Command "git.exe"
+    if ($p -ne $null){
+        return $p
+    }
+    $a = Find-Command "C:\Program Files\Git\bin\git.exe"
+    if ($a -ne $null){
+        return $a
+    }
+    Fail-Json -obj $result -message "git.exe is not installed. It must be installed (use chocolatey)"
+}
+
+# Remove dest if it exests
+function PrepareDestination {
+    [CmdletBinding()]
+    param()
+    if ((Test-Path $dest) -And (-Not $check_mode)) {
+        try {
+            Remove-Item $dest -Force -Recurse | Out-Null            
+            if (-Not (Test-Path($dest))) {
+               New-Item -ItemType directory -Path $dest      
+            }
+            Set-Attr $result.win_git "msg" "Successfully replaced dir $dest."
+            Set-Attr $result "changed" $true            
+        } catch {
+            $ErrorMessage = $_.Exception.Message
+            Fail-Json $result "Error removing $dest! Msg: $ErrorMessage"
+        }
+    }
+}
+
+# SSH Keys
+function CheckSshKnownHosts {
+    [CmdletBinding()]
+    param()
+    # Get the Git Hostrepo
+    $gitServer = $($repo -replace "^(\w+)\@([\w-_\.]+)\:(.*)$", '$2')
+    $gitHome = ((Get-Command git.exe).Path | Split-Path | Split-Path)
+    $gitKeyGenPath = Join-Path -Path $gitHome -ChildPath /usr/bin/ssh-keygen.exe
+    $gitKeyScanPath = Join-Path -Path $gitHome -ChildPath /usr/bin/ssh-keyscan.exe
+    $gitKeyAddPath = Join-Path -Path $gitHome -ChildPath /usr/bin/ssh-add.exe
+   
+    & cmd /c $gitKeyGenPath -F $gitServer | Out-Null
+    $rc = $LASTEXITCODE
+
+    if ($rc -ne 0) {    
+        if (-Not (Test-Path -Path $env:Userprofile\.ssh\known_hosts)) {   
+           New-Item -ItemType file -Path $env:Userprofile\.ssh\known_hosts
+        }   
+    }    
+    
+    # SSH Known Host Config        
+    if ($accept_hostkey){
+        # workaround for disable BOM
+        # https://github.com/tivrobo/ansible-win_git/issues/7
+        $sshHostKey = & cmd /c $gitKeyScanPath -t ssh-rsa $gitServer
+        $sshHostKey += "`n"                
+        $sshKnownHostsPath = Join-Path -Path $env:Userprofile -ChildPath \.ssh\known_hosts
+        $key_search = Get-Content -Path $sshKnownHostsPath  | Select-String "$gitServer" 
+        if ([string]::IsNullOrEmpty($key_search)) {
+            try {  
+                [System.IO.File]::AppendAllText($sshKnownHostsPath, $sshHostKey, $(New-Object System.Text.UTF8Encoding $False))
+                Set-Attr $result.win_git "msg" "Successfully updated $sshKnownHostsPath."
+            }
+            catch {
+               $ErrorMessage = $_.Exception.Message
+               Fail-Json $result "Error updating $sshKnownHostsPath Msg: $ErrorMessage"               
+            }               
+        }  
+        else {
+            Set-Attr $result.win_git "msg" "No update to $sshKnownHostsPath. $key_search exits"
+        }    
+    }
+    else {
+        Fail-Json -obj $result -message  "Host is not known!"
+    }      
+
+    # SSH Private Key Config for Host
+    if (Test-Path -Path $key_file){
+        $sshConfigPath = Join-Path -Path ((Get-Command git.exe).Path | Split-Path | Split-Path) -ChildPath /etc/ssh/ssh_config
+        $key_search = Get-Content -Path $sshConfigPath  | Select-String "Host $gitServer" 
+        
+        if (-Not (Test-Path -Path $env:Userprofile\.ssh)) { New-Item -ItemType directory -Path $env:Userprofile\.ssh }        
+        if (-Not (Test-Path -Path $env:Userprofile\.ssh\id_rsa)) {
+          Get-Content -Path $key_file | Set-Content -Path $env:Userprofile\.ssh\id_rsa                           
+        }         
+        # Add Host Config if not exists
+        if ([string]::IsNullOrEmpty($key_search)) {               
+            try {
+              [System.IO.File]::AppendAllText($sshConfigPath, "Host $gitServer`n", $(New-Object System.Text.UTF8Encoding $False))
+              [System.IO.File]::AppendAllText($sshConfigPath, "IdentityFile $key_file", $(New-Object System.Text.UTF8Encoding $False))       
+              Set-Attr $result.win_git "msg" "Successfully updated $sshConfigPath."
+            }
+            catch {
+                $ErrorMessage = $_.Exception.Message
+                Fail-Json $result "Error updating $sshConfigPath Msg: $ErrorMessage"
+            }            
+        }   
+        else {
+            Set-Attr $result.win_git "msg" "No update to $sshConfigPath. $key_search exits"
+        }         
+        & cmd /c start-ssh-agent.cmd | Out-Null
+        & cmd /c $gitKeyAddPath $env:Userprofile\.ssh\id_rsa      
+
+    }
+    else {
+        Fail-Json -obj $result -message  "Key Path not found!"
+    }    
+    
+}
+
+
+function CheckSshIdentity {
+    [CmdletBinding()]
+    param()
+
+    & cmd /c git.exe ls-remote $repo | Out-Null
+    $rc = $LASTEXITCODE
+    if ($rc -ne 0) {
+        Fail-Json -obj $result -message  "Something wrong with connection!"
+    }
+}
+
+function CheckPath {
+    [CmdletBinding()]
+    param(
+      [Parameter(Mandatory=$true, Position=0)] [string] $path
+    )
+    if( (Test-Path -Path $path) -And ((Get-ChildItem $path | Measure-Object).Count -eq 0) ){
+        return $true
+    }    
+    return $false
+}    
+function get_version {
+    # samples the version of the git repo 
+    # example:  git rev-parse HEAD
+    #           output: 931ec5d25bff48052afae405d600964efd5fd3da
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false, Position=0)] [string] $refs = "HEAD"
+    )
+    $git_opts = @()
+    $git_opts += "--no-pager"
+    $git_opts += "rev-parse"
+    $git_opts += "$refs"
+    $git_cmd_output = ""
+    
+    [hashtable]$Return = @{} 
+    Set-Location $dest; &git $git_opts | Tee-Object -Variable git_cmd_output | Out-Null
+    $Return.rc = $LASTEXITCODE
+    $Return.git_output = $git_cmd_output
+    
+    return $Return
+}
+
+function checkout {
+    [CmdletBinding()]
+    param()
+    [hashtable]$Return = @{} 
+    $local_git_output = ""
+
+    $git_opts = @()
+    $git_opts += "--no-pager"
+    $git_opts += "checkout"
+    
+    if ($version -ne "HEAD") {
+        $git_opts += $version
+    }
+
+    Set-Location $dest; &git $git_opts  | Tee-Object -Variable local_git_output | Out-Null
+
+    $Return.git_output = $local_git_output
+    
+    if ($version -ne "HEAD") {
+      Set-Location $dest; &git status --short --branch $version | Tee-Object -Variable branch_status | Out-Null
+      $branch_status = $branch_status.split("/")[1]
+      Set-Attr $result.win_git "branch_status" "$branch_status"
+
+        if ( $branch_status -ne "$version" ) {
+            Fail-Json $result "Failed to checkout to $branch"
+        }
+    }      
+    
+    return $Return
+}
+
+function clone {
+    # git clone command
+    [CmdletBinding()]
+    param()
+
+    Set-Attr $result.win_git "method" "clone"
+    [hashtable]$Return = @{} 
+    $git_output = ""
+
+    $git_opts = @()
+    $git_opts += "--no-pager"
+    $git_opts += "clone"
+    $git_opts += $repo 
+    $git_opts += $dest
+    
+    if ($version -ne "HEAD") {
+      $git_opts += "--branch"
+      $git_opts += $version
+    } 
+
+    if ($depth){
+       $git_opts += "--depth"
+       $git_opts += $depth
+    } 
+
+    Set-Attr $result.win_git "git_opts" "$git_opts"
+
+    #Only clone if $dest does not exist and not in check mode    
+    if( -Not $check_mode) {    
+
+        if (CheckPath "$dest") {   
+            &git $git_opts | Tee-Object -Variable git_output | Out-Null
+            $Return.rc = $LASTEXITCODE
+            $Return.git_output = $git_output
+            
+            if ($LASTEXITCODE -eq 0) {
+               $result.changed = $true           
+               Set-Attr $result.win_git "msg" "Successfully cloned $repo into $dest."    
+            }                    
+     
+        }    
+        else {
+            Set-Attr $result.win_git "msg" "Failed to clone $repo into $dest. It is either not empty or does not exist"    
+        }
+
+        Set-Attr $result.win_git "return_code" $LASTEXITCODE
+        Set-Attr $result.win_git "git_output" $git_output
+    }
+    else {
+        $Return.rc = 0
+        $Return.git_output = $git_output        
+        Set-Attr $result.win_git "msg" "Check Mode - Skipping clone of $repo"
+    }
+
+    # Check if branch is the correct one
+    if ($version -ne "HEAD") {
+        Set-Location $dest; &git status --short --branch $version | Tee-Object -Variable branch_status | Out-Null
+        $branch_status = $branch_status.split("/")[1]
+        Set-Attr $result.win_git "branch_status" "$branch_status"
+
+            if ( $branch_status -ne "$version" ) {
+                Fail-Json $result "Branch $branch_status is not $branch"
+            }
+    }     
+
+    return $Return
+}
+
+function update {
+    # git clone command
+    [CmdletBinding()]
+    param()
+
+    Set-Attr $result.win_git "method" "pull"
+    [hashtable]$Return = @{}
+    $git_output = ""
+
+    # Build Arguments
+    $git_opts = @()
+    $git_opts += "--no-pager"
+    $git_opts += "pull"
+    $git_opts += $remote
+    
+    if ($version -ne "HEAD") {
+       $git_opts += $version
+    } 
+
+    Set-Attr $result.win_git "git_opts" $git_opts
+    #Only update if $dest does exist and not in check mode
+    if(-Not $check_mode) {
+        # move into correct branch before pull
+        checkout
+        # perform git pull
+        Set-Location $dest; &git $git_opts | Tee-Object -Variable git_output | Out-Null
+        $Return.rc = $LASTEXITCODE
+        $Return.git_output = $git_output
+        if ($LASTEXITCODE -eq 0) {
+            Set-Attr $result "changed" $true
+            Set-Attr $result.win_git "msg" "Successfully updated $repo to $version."
+        }  
+        #TODO: handle correct status change when using update        
+        Set-Attr $result.win_git "return_code" $LASTEXITCODE
+        Set-Attr $result.win_git "git_output" $git_output
+    } else {
+        $Return.rc = 0
+        $Return.git_output =  $git_output
+        Set-Attr $result.win_git "msg" "Check Mode - Skipping update of $repo"
+    }
+
+    return $Return
+}
+
+
+if ($repo -eq ($null -or "")) {
+    Fail-Json $result "Repository cannot be empty or `$null"
+}
+
+# set the default dest under user profile
+if ($dest -eq $null){
+    $repo_name = $repo.split("/")[-1]
+    $dest="$env:USERPROFILE\$repo_name"     
+} 
+
+if (-Not (Test-Path($dest))) {
+    New-Item -ItemType directory -Path $dest      
+ }
+
+
+Set-Attr $result.win_git "repo" $repo
+Set-Attr $result.win_git "dest" $dest
+Set-Attr $result.win_git "replace_dest" $replace_dest
+Set-Attr $result.win_git "accept_hostkey" $accept_hostkey
+Set-Attr $result.win_git "key_file" $key_file
+Set-Attr $result.win_git "update" $update
+Set-Attr $result.win_git "version" $version
+Set-Attr $result.win_git "remote" $remote
+
+
+$git_output = ""
+$rc = 0
+
+try {
+
+    FindGit
+
+    if ($replace_dest) {
+        PrepareDestination
+    }
+    if ([system.uri]::IsWellFormedUriString($repo,[System.UriKind]::Absolute)) {
+        # http/https repositories doesn't need Ssh handle
+        # fix to avoid wrong usage of CheckSshKnownHosts CheckSshIdentity for http/https
+        Set-Attr $result.win_git "valid_url" "$repo is valid url"
+    } else {
+        CheckSshKnownHosts
+        CheckSshIdentity
+    }
+    if ($clone) {
+        clone
+    }
+    if ($update) {
+        update
+    }
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    #Fail-Json $result "Error cloning $repo Msg: $ErrorMessage - $git_output"
+}
+
+Exit-Json $result
---
+++ lib/ansible/modules/windows/win_git.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# this is a windows documentation stub.  actual code lives in the .ps1
+# file of the same name
+
+# Copyright: (c) 2018, Anatoliy Ivashina <tivrobo@gmail.com>, Pablo Estigarribia <pablodav@gmail.com>, Michael Hay <project.hay@gmail.com>, Ripon Banik <ripon.banik@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+DOCUMENTATION = '''
+---
+module: win_git
+author: 
+  - Anatoliy Ivashina
+  - Pablo Estigarribia
+  - Michael Hay
+  - Ripon Banik
+version_added: "2.8"
+short_description: Deploy software (or files) from git checkouts.
+description:
+    - Deploy software (or files) from git checkouts.
+options:
+  repo:
+    description:
+        - git, SSH, or HTTP(S) protocol address of the git repository.
+    required: true
+    aliases: [ name ]
+  dest:
+    description:
+        - The path of where the repository should be checked out. If
+          not specified creates the direcctory under user profile folder.
+    required: false
+  version:
+    description:
+        - What version of the repository to check out.  This can be
+          the literal string C(HEAD), a branch name, a tag name.
+          It can also be a I(SHA-1) hash, in which case C(refspec) needs
+          to be specified if the given revision is not already available.
+    default: "HEAD"
+  remote:
+    description:
+        - Name of the remote.
+    default: "origin"  
+  replace_dest:
+    description:
+      - replace destination folder if exists (recursive!)    
+    type: bool
+    default: no
+  accept_hostkey:
+    description:
+      - add hostkey to known_hosts (before connecting to git)    
+    type: bool
+    default: yes
+  clone:
+      description:
+          - If C(no), do not clone the repository if it does not exist locally
+      type: bool
+      default: 'yes'
+  update:
+    description:
+      - do we want to update the repo (use git pull origin branch)    
+    type: bool
+    default: no 
+  depth:
+    description:
+        - Create a shallow clone with a history truncated to the specified
+          number or revisions. The minimum possible value is C(1), otherwise
+          ignored. Needs I(git>=1.9.1) to work correctly.  
+  key_file:
+        description:
+            - Specify an optional private key file path, on the target host, to use for the checkout.  
+requirements:
+    - git>=2.19.0
+notes:
+    - git for Windows need to be installed. 
+    - "If the task seems to be hanging for ssh, first verify remote host is in C(known_hosts).
+       and id_rsa is correct under user profile; Re-Run the task" 
+'''
+
+EXAMPLES = '''
+# Example git clone using https 
+win_git:
+  repo: "https://github.com/githubtraining/hellogitworld"
+  dest: "{{ ansible_env.TEMP }}\\hellogitworld"  
+
+# Example git clone using specific version
+win_git:
+  repo: 'https://foosball.example.org/path/to/repo.git'
+  dest: "{{ ansible_env.TEMP }}\\hellogitworld"    
+  version: release-0.22     
+
+# Example git clone using ssh. Also clean up existing directory
+win_git:          
+  repo: git@github.com:githubtraining/hellogitworld.git          
+  dest: c:\windows\temp\hellogitworld_ssh     
+  replace_dest: yes
+  key_file: 'c:\windows\temp\id_rsa'
+
+# Example git clone using git protocol. Update if the folder already cloned
+win_git:          
+    repo: git://github.com/githubtraining/hellogitworld.git          
+    dest: c:\windows\temp\hellogitworld_git
+    update: yes       
+  
+'''
