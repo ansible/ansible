491bf91ef18f4029ec28ddc72997e31dc53834f8
Sean Myers <sean.myers@redhat.com>
2018-12-05 20:19:59 -0500

    Fix imports in rhsm_repository_release unit test

--- test/units/modules/packaging/os/test_rhsm_repository_release.py
+++ test/units/modules/packaging/os/test_rhsm_repository_release.py
@@ -4,7 +4,7 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-from ansible.compat.tests.mock import call, patch
+from units.compat.mock import call, patch
 from ansible.modules.packaging.os import rhsm_repository_release
 from units.modules.utils import (
     AnsibleExitJson, AnsibleFailJson, ModuleTestCase, set_module_args)
