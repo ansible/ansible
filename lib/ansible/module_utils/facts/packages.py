a59c717258e6bae7ebac99808c706da7408a7ee2
Brian Coca <brian.coca+git@gmail.com>
2018-12-06 20:55:32 -0500

    missing license

--- lib/ansible/module_utils/facts/packages.py
+++ lib/ansible/module_utils/facts/packages.py
@@ -1,3 +1,5 @@
+# (c) 2018, Ansible Project
+# Simplified BSD License (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
