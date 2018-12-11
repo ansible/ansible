70cf9d192c34b9934592e78851b3ec58082edc36
rawstorage <paule.martin@dell.com>
2018-12-11 14:54:05 +0000

    Updated Documentation YML format removing linebreaks and spaces. Corrected indentation on documentation to remove duplicate key references

--- lib/ansible/modules/storage/dellemc/dellpmax_createsg.py
+++ lib/ansible/modules/storage/dellemc/dellpmax_createsg.py
@@ -1,103 +1,104 @@
 #!/usr/bin/python
+# Copyright (C) 2018 DellEMC
+# Author(s): Paul Martin <paule.martin@dell.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-ANSIBLE_METADATA = {'metadata_version': 1.1,
-                    'status': ['preview'],
-                    'supported_by': 'community'
-                    }
+ANSIBLE_METADATA = {'status': ['preview'],
+                    'supported_by': 'community',
+                    'metadata_version': '1.1'}
+
 DOCUMENTATION = r'''
 ---
 module: dellpmax_createsg
-version_added: '2.6'
+version_added: '2.7'
 short_description: Create storage group on Dell EMC PowerMax or VMAX All Flash 
 description:
-- This module has been tested against UNI 9.0. Every effort has been 
+- "This module has been tested against UNI 9.0. Every effort has been 
   made to verify the scripts run with valid input. These modules are a 
-  tech preview. Additional error handling will be added at a later date, 
-  base functionality only right now.
+  tech preview"
 author:
 - Paul Martin (@rawstorage)
 Contributors:
 - Rob Mortell (@robmortell)                
 Module_Requirements:
   description:
-    - Ansible, Python 2.7, Unisphere for PowerMax version 9.0 or higher. 
-      VMAX All Flash, VMAX3, or PowerMax storage Array. Also requires PyU4V to 
-      be installed from PyPi using PIP python -m pip install PyU4V recommend 
-      version 3.0.0.8 or higher.  
+    - Ansible, Python 2.7, Unisphere for PowerMax version 9.0 or 
+   higher. VMAX All Flash, VMAX3, or PowerMax storage Array. Also requires PyU4V 
+   to be installed from PyPi using PIP python -m pip install PyU4V recommend 
+   version 3.0.0.8 or higher.  
 options:
   unispherehost:
     description:
     - Fully Qualified Domain Name or IP address of Unisphere for PowerMax host. 
     required: True
-    universion:
+  universion:
     description:
     - Integer, version of unipshere software 
       https://{HostName|IP}:8443/univmax/restapi/{version}/{resource}
       90 is the release at time of writing module.
     required: True
-    verifycert:
+  verifycert:
     description:
     - Boolean, securitly check on ssl certificates
     required: True             
-    sgname:
+  sgname:
     description:
     - Storage Group name 32 Characters no special characters other than 
       underscore
     required: True     
-    array_id:
+  array_id:
     description:
     - Integer 12 Digit Serial Number of PowerMAX or VMAX array.
     required: True
-    srp_id:
+  srp_id:
     description:
     - Storage Resource Pool Name, Default is set to SRP_1, if your system 
       has mainframe or multiple pools you can set this to a different value to 
       match your environment
     required: Optional
-    slo:
+  slo:
     description:
     - Service Level for the storage group, Supported on VMAX3 and All Flash and
       PowerMAX NVMe Arrays running PowerMAX OS 5978 and above.  Default is 
       set to Diamond, but user can override this by setting a different value.
-     required: Optional
-    workload:
+    required: Optional
+  workload:
     description:
     - Block workload type, optional and can only be set on VMAX3 Hybrid Storage
       Arrays. Default None.
     required: Optional
-    num_vols:
+  num_vols:
     description:
     - integer value for the number of volumes. Minimum is 1, module will 
       fail if less than one volume is specified or value is 0. If volumes are 
       required of different sizes, addional tasks should be added to playbooks 
       to use dellpmax_addvolume module
     required: True
-    vol_size:
+  vol_size:
     description:
     - Integer value for the size of volumes.  All volumes will be created with 
       same size.  Use dellpmax_addvol to add additional volumes if you require 
       different sized volumes once storage group is created.
     required: True
-    cap_unit: 
+  cap_unit: 
     description:
     - String value, default is set to GB
     required: Optional 
-    choices: [ GB, TB, MB, CYL]
+    choices: [GB, TB, MB, CYL]
     async:
     description:
     - Optional Parameter to set REST call to run Asyncronously, job will be 
       submitted to job queue and executed.  Task Id will be returned in JSON 
       for lookup purposed to check job completion status. 
-    volumeIdentifier:
+  volumeIdentifier:
     description:
     - String up to 64 Characters no special character other than _ sets a 
       label to make volumes easily identified on hosts can run Dell EMC inq 
       utility command to see this label is  inq -identifier device_name
-    required: Optional 
+    required: Optional
 '''
 
 EXAMPLES = r'''
@@ -123,7 +124,7 @@ EXAMPLES = r'''
         password: '{{password}}'
         sgname: '{{sgname}}'
         array_id: '{{array_id}}'
-        srp_id:	'SRP_1'
+        srp_id: 'SRP_1'
         slo: 'Diamond'
         workload: None
         num_vols: 1
@@ -135,6 +136,7 @@ RETURN = r'''
 '''
 from ansible.module_utils.basic import AnsibleModule
 import PyU4V
+
 
 def main():
     module = AnsibleModule(
