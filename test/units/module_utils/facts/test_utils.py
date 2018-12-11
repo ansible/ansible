d4647b26a92b99f86a9eefedfc7418f49a171bd5
Abhijeet Kasurde <akasurde@redhat.com>
2018-12-11 19:29:23 +0530

    Test coverage for facts.utils
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- test/units/module_utils/facts/test_utils.py
+++ test/units/module_utils/facts/test_utils.py
@@ -1,39 +1,68 @@
-# This file is part of Ansible
+# -*- coding: utf-8 -*-
+#  Copyright: (c) 2017, Ansible Project
 #
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-# Make coding more python3-ish
 from __future__ import (absolute_import, division)
 __metaclass__ = type
 
-from units.compat import unittest
-from units.compat.mock import patch
-
+import os
+import pytest
 from ansible.module_utils.facts import utils
 
+mount_data = [
+    '/dev/null/not/a/real/mountpoint',
+    '/proc'
+]
 
-class TestGetMountSize(unittest.TestCase):
-    def test(self):
-        mount_info = utils.get_mount_size('/dev/null/not/a/real/mountpoint')
-        self.assertIsInstance(mount_info, dict)
+file_data = [
+    (
+        "/dev/null/file/hello_world",
+        ["Hello World"]
+    ),
+    (
+        "/dev/null/no/content",
+        ['']
+    ),
+]
 
-    def test_proc(self):
-        mount_info = utils.get_mount_size('/proc')
-        self.assertIsInstance(mount_info, dict)
 
-    @patch('ansible.module_utils.facts.utils.os.statvfs', side_effect=OSError('intentionally induced os error'))
-    def test_oserror_on_statvfs(self, mock_statvfs):
-        mount_info = utils.get_mount_size('/dev/null/doesnt/matter')
-        self.assertIsInstance(mount_info, dict)
-        self.assertDictEqual(mount_info, {})
+@pytest.mark.parametrize("mount_point", mount_data, ids=['non_existent_mount', 'proc'])
+def test_get_mount_size_with_different_values(mount_point):
+    mount_info = utils.get_mount_size(mount_point)
+    assert isinstance(mount_info, dict)
+
+
+def test_oserror_on_statvfs(monkeypatch):
+    def _os_statvfs(path):
+        raise OSError('%s does not exist' % path)
+
+    monkeypatch.setattr('os.statvfs', _os_statvfs)
+    mount_info = utils.get_mount_size('/dev/null/doesnt/matter')
+    assert isinstance(mount_info, dict)
+
+
+@pytest.mark.parametrize("file_name, file_content", file_data, ids=['hello_world_file', 'no_content_file'])
+def test_get_file_lines(monkeypatch, file_name, file_content):
+    def _get_file_content(path, default=None, strip=True):
+        return file_content[0]
+
+    monkeypatch.setattr('ansible.module_utils.facts.utils.get_file_content', _get_file_content)
+    file_content = utils.get_file_lines(file_name)
+    assert file_content == file_content
+
+
+@pytest.mark.parametrize("strip", [True, False], ids=['With_default_strip_true', 'With_strip_false'])
+def test_get_file_content(strip):
+    fake_file = os.path.join(os.path.dirname(__file__), './fixtures/test_file.txt')
+    content = utils.get_file_content(fake_file, strip=strip)
+    expected_content = 'Hello World\nThis file is used by test_utils'
+    if strip is False:
+        expected_content += "\n"
+    assert content == expected_content
+
+
+def test_get_file_content_without_file():
+    fake_file = os.path.join(os.path.dirname(__file__), './fixtures/non_existent_file.txt')
+    content = utils.get_file_content(fake_file)
+    assert content is None
