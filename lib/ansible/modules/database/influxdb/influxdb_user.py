f6de5cab922038e12e2095c8eaacbd1908349a9e
Michael Rose <mdr.git@gmx.com>
2018-12-12 09:43:37 -0800

    Amends #47040 - influxdb_user - Prevents potential accidental password changes to blank (#49084)
    * Amends #47040 - Prevents potential accidental password changes to blank
    
    * Added changelog fragment
    
    (cherry picked from commit b422c59ada632b5905b9fc06b8fb37ed299f3979)

--- lib/ansible/modules/database/influxdb/influxdb_user.py
+++ lib/ansible/modules/database/influxdb/influxdb_user.py
@@ -158,7 +158,7 @@ def main():
 
     state = module.params['state']
     user_name = module.params['user_name']
-    user_password = module.params['user_password'] or ''
+    user_password = module.params['user_password']
     admin = module.params['admin']
     influxdb = influx.InfluxDb(module)
     client = influxdb.connect_to_influxdb()
@@ -166,11 +166,12 @@ def main():
 
     if state == 'present':
         if user:
-            if check_user_password(module, client, user_name, user_password):
+            if user_password is None or check_user_password(module, client, user_name, user_password):
                 module.exit_json(changed=False)
             else:
                 set_user_password(module, client, user_name, user_password)
         else:
+            user_password = user_password or ''
             create_user(module, client, user_name, user_password, admin)
 
     if state == 'absent':
