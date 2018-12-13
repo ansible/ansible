2af8c50cb8e23ae7ff78406fbb0765156fa761fe
Kortum, Kevin <kortum@e-spirit.com>
2018-12-13 14:07:56 +0100

    - Fixed minor language/typing/formatting issues.

--- lib/ansible/modules/web_infrastructure/sophos_utm/utm_aaa_group.py
+++ lib/ansible/modules/web_infrastructure/sophos_utm/utm_aaa_group.py
@@ -19,10 +19,10 @@ module: utm_aaa_group
 author:
     - Johannes Brunswicker (@MatrixCrawler)
 
-short_description: create, update or destroy reverse_proxy frontend entry in Sophos UTM
+short_description: Create, update or destroy an aaa group object in Sophos UTM.
 
 description:
-    - Create, update or destroy a reverse_proxy frontend entry in SOPHOS UTM.
+    - Create, update or destroy an aaa group object in Sophos UTM.
     - This module needs to have the REST Ability of the UTM to be activated.
 
 version_added: "2.8"
@@ -30,19 +30,19 @@ version_added: "2.8"
 options:
     name:
         description:
-          - The name of the object. Will be used to identify the entry
+          - The name of the object. Will be used to identify the entry.
         required: true
-    adirectory_groups :
+    adirectory_groups:
         description:
-          - list of adirectory group strings
+          - List of adirectory group strings.
         type: list
-    adirectory_groups_sids :
+    adirectory_groups_sids:
         description:
-          - dictionary of group sids
+          - Dictionary of group sids.
         type: list
     backend_match:
         description:
-          - The backend for the group
+          - The backend for the group.
         default: none
         choices:
           - none
@@ -53,13 +53,13 @@ options:
           - ldap
     comment:
         description:
-            - Comment that describes the AAA group
+            - Comment that describes the AAA group.
         default: ''
         type: str
         required: false
     dynamic:
         description:
-          - Group type. Is static if none is selected
+          - Group type. Is static if none is selected.
         default: none
         choices:
           - none
@@ -67,35 +67,35 @@ options:
           - directory_groups
     edirectory_groups:
         description:
-          - list of edirectory group strings
+          - List of edirectory group strings.
         type: list
     ipsec_dn:
         description:
-          - The ipsec dn string
+          - The ipsec dn string.
         type: str
     ldap_attribute:
         description:
-          - The ldap attribute to check against
+          - The ldap attribute to check against.
         type: str
     ldap_attribute_value:
         description:
-          - The ldap attribute value to check against
+          - The ldap attribute value to check against.
         type: str
     members:
         description:
-          - A list of user ref names (aaa/user)
+          - A list of user ref names (aaa/user).
         default: []
     network:
         description:
-          - The network reference name. The objects contains the known ip addresses for the authentication object (network/aaa)
+          - The network reference name. The objects contains the known ip addresses for the authentication object (network/aaa).
         default: ""
     radius_groups:
         description:
-          - A list of radius group strings
+          - A list of radius group strings.
         default: []
     tacacs_groups:
         description:
-          - A list of tacacs group strings
+          - A list of tacacs group strings.
         default: []
 
 extends_documentation_fragment:
@@ -125,60 +125,60 @@ EXAMPLES = """
 
 RETURN = """
 result:
-    description: The utm object that was created
+    description: The utm object that was created.
     returned: success
     type: complex
     contains:
         _ref:
-            description: The reference name of the object
+            description: The reference name of the object.
             type: string
         _locked:
-            description: Whether or not the object is currently locked
+            description: Whether or not the object is currently locked.
             type: boolean
         _type:
-            description: The type of the object
+            description: The type of the object.
             type: string
         name:
-            description: The name of the object
+            description: The name of the object.
             type: string
         adirectory_groups:
-            description: List of Active Directory Groups
+            description: List of Active Directory Groups.
             type: string
         adirectory_groups_sids:
-            description: List of Active Directory Groups SIDS
+            description: List of Active Directory Groups SIDS.
             type: list
         backend_match:
-            description: The backend to use
+            description: The backend to use.
             type: string
         comment:
-            description: The comment string
+            description: The comment string.
             type: string
         dynamic:
-            description: Whether the group match is ipsec_dn or directory_group
+            description: Whether the group match is ipsec_dn or directory_group.
             type: string
         edirectory_groups:
-            description: List of eDirectory Groups
+            description: List of eDirectory Groups.
             type: string
         ipsec_dn:
-            description: ipsec_dn identifier to match
+            description: ipsec_dn identifier to match.
             type: string
         ldap_attribute:
-            description: The LDAP Attribute to match against
+            description: The LDAP Attribute to match against.
             type: string
         ldap_attribute_value:
-            description: The LDAP Attribute Value to match against
+            description: The LDAP Attribute Value to match against.
             type: string
         members:
-            description: List of member identifiers of the group
+            description: List of member identifiers of the group.
             type: list
         network:
-            description: The identifier of the network (network/aaa)
+            description: The identifier of the network (network/aaa).
             type: string
         radius_group:
-            description: The radius group identifier
+            description: The radius group identifier.
             type: string
         tacacs_group:
-            description: The tacacs group identifier
+            description: The tacacs group identifier.
             type: string
 """
 
