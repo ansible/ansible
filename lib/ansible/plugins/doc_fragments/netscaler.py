c73760c408a22bbba8a7ca5eed731308ff42e3dd
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2018-12-13 16:11:40 +0200

    Add Netscler documentation fragments

--- lib/ansible/plugins/doc_fragments/netscaler.py
+++ lib/ansible/plugins/doc_fragments/netscaler.py
@@ -11,20 +11,20 @@ options:
         description:
             - The ip address of the netscaler appliance where the nitro API calls will be made.
             - "The port can be specified with the colon (:). E.g. 192.168.1.1:555."
-        type: str
+        aliases: [ 'mas_ip' ]
         required: True
 
     nitro_user:
         description:
             - The username with which to authenticate to the netscaler node.
-        type: str
-        required: True
+        required: False
+        aliases: [ 'mas_user' ]
 
     nitro_pass:
         description:
             - The password with which to authenticate to the netscaler node.
-        type: str
-        required: True
+        required: False
+        aliases: [ 'mas_pass' ]
 
     nitro_protocol:
         description:
@@ -36,8 +36,8 @@ options:
     validate_certs:
         description:
             - If C(no), SSL certificates will not be validated. This should only be used on personally controlled sites using self-signed certificates.
-        type: bool
-        default: yes
+        required: false
+        default: 'yes'
 
     nitro_timeout:
         description:
@@ -59,7 +59,27 @@ options:
             - If C(yes) the module will save the configuration on the netscaler node if it makes any changes.
             - The module will not save the configuration on the netscaler node if it made no changes.
         type: bool
-        default: yes
+        default: true
+
+    mas_proxy_call:
+        description:
+            - If true the underlying NITRO API calls made by the module will be proxied through a MAS node to the target Netscaler instance.
+            - "When (mas_proxy_call=true) you must also define the following options: I(nitro_auth_token), I(instance_ip)."
+        type: bool
+        default: false
+        version_added: "2.8.0"
+
+    nitro_auth_token:
+        description:
+            - The authentication token provided by a login operation.
+        aliases: [ 'mas_auth_token' ]
+        version_added: "2.8.0"
+
+    instance_ip:
+        description:
+            - The target Netscaler instance ip address to which all underlying NITRO API calls will be proxied to.
+            - It is meaningful only when having set C(mas_proxy_call) to C(true)
+        version_added: "2.8.0"
 notes:
   - For more information on using Ansible to manage Citrix NetScaler Network devices see U(https://www.ansible.com/ansible-netscaler).
 '''
