3f3416eec6e79cb9019ab989fc5256c50534a420
zikalino <zikalino@microsoft.com>
2018-12-14 13:22:01 +0800

    add ansible_version

--- lib/ansible/module_utils/azure_rm_common_rest.py
+++ lib/ansible/module_utils/azure_rm_common_rest.py
@@ -2,6 +2,8 @@
 #
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from ansible.module_utils.ansible_release import __version__ as ANSIBLE_VERSION
+
 try:
     from msrestazure.azure_exceptions import CloudError
     from msrestazure.azure_configuration import AzureConfiguration
