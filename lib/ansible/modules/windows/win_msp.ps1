8f463cc8cfdda4439c02548cdbe8c94bddb0ea3b
Rodric GK. Vos <rodric@vosenterprises.eu>
2018-12-14 16:34:27 +0100

    Added first edition of win_msp module.

---
+++ lib/ansible/modules/windows/win_msp.ps1
@@ -0,0 +1,337 @@
+#!powershell
+
+# Copyright: (c) 2018, Rodric Vos <rodric@vosenterprises.eu>, and others
+# Copyright: (c) 2018, Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#Requires -Module Ansible.ModuleUtils.Legacy
+#Requires -Module Ansible.ModuleUtils.CommandUtil
+#Requires -Module Ansible.ModuleUtils.ArgvParser
+
+$ErrorActionPreference = 'Stop'
+
+$params = Parse-Args -arguments $args -supports_check_mode $true
+$check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
+
+$expected_return_code = Get-AnsibleParam -obj $params -name "expected_return_code" -type "list" -default @(0, 3010)
+$path = Get-AnsibleParam -obj $params -name "path" -type "str"
+$state = Get-AnsibleParam -obj $params -name "state" -type "str" -default "present" -validateset "absent","present"
+$username = Get-AnsibleParam -obj $params -name "username" -type "str" -aliases "user_name"
+$password = Get-AnsibleParam -obj $params -name "password" -type "str" -failifempty ($username -ne $null) -aliases "user_password"
+$validate_certs = Get-AnsibleParam -obj $params -name "validate_certs" -type "bool" -default $true
+$product_id = Get-AnsibleParam -obj $params -name "product_id" -type "str" -failifempty ($state -eq "absent") -aliases "productid"
+$patch_id = Get-AnsibleParam -obj $params -name "patch_id" -type "str" -failifempty ($state -eq "absent") -aliases "patchid"
+$patch_guid = Get-AnsibleParam -obj $params -name "patch_guid" -type "str" -failifempty ($state -eq "absent") -aliases "patchguid"
+
+$result = @{
+    changed = $false
+    reboot_required = $false
+}
+
+if (-not $validate_certs) {
+    [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
+}
+
+# Enable TLS1.1/TLS1.2 if they're available but disabled (eg. .NET 4.5)
+$security_protcols = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::SystemDefault
+if ([Net.SecurityProtocolType].GetMember("Tls11").Count -gt 0) {
+    $security_protcols = $security_protcols -bor [Net.SecurityProtocolType]::Tls11
+}
+if ([Net.SecurityProtocolType].GetMember("Tls12").Count -gt 0) {
+    $security_protcols = $security_protcols -bor [Net.SecurityProtocolType]::Tls12
+}
+[Net.ServicePointManager]::SecurityProtocol = $security_protcols
+
+$credential = $null
+if ($username -ne $null) {
+    $sec_user_password = ConvertTo-SecureString -String $password -AsPlainText -Force
+    $credential = New-Object -TypeName PSCredential -ArgumentList $username, $sec_user_password
+}
+
+$valid_return_codes = @()
+foreach ($rc in ($expected_return_code)) {
+    try {
+        $int_rc = [Int32]::Parse($rc)
+        $valid_return_codes += $int_rc
+    } catch {
+        Fail-Json -obj $result -message "failed to parse expected return code $rc as an integer"
+    }
+}
+
+if ($state -eq "absent") {
+    if (-not $product_id -or -not $patch_id) {
+        Fail-Json -obj $result -message "product_id and patch_id cannot be empty when state is absent"
+    }
+}
+
+Add-Type -TypeDefinition @"
+public enum LocationType {
+    Empty,
+    Local,
+    Unc,
+    Http
+}
+"@
+
+Add-Type -TypeDefinition @"
+public enum ActionState {
+    AbsentAndInstalled,
+    AbsentAndNotInstalled,
+    PresentAndInstalled,
+    PresentAndNotInstalled
+}
+"@
+
+function Download-File($url, $path) {
+    $web_client = New-Object -TypeName System.Net.WebClient
+    try {
+        $web_client.DownloadFile($url, $path)
+    } catch {
+        Fail-Json -obj $result -message "failed to download $url to $($path): $($_.Exception.Message)"
+    }
+}
+
+function Get-ProgramMetadata($state, $path, $product_id, $patch_id, $credential) {
+    # will get some metadata about the program we are trying to install or remove
+    $metadata = @{
+        installed = $false
+        location_type = $null
+        path_error = $null
+        action_state = $null
+    }
+
+    $drive = $null
+    try {
+        $drive = New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+        $patchguid_key = "HKCR:\Installer\Patches\$($metadata.patch_id)"
+        if (Test-Path -Path $patchguid_key) {
+            $metadata.installed = $true
+        }
+    }
+    catch {
+        Fail-Json -obj $result -message "failed to lookup $patchguid_key in the Windows Registry: $($_.Exception.Message)"
+    }
+    finally {
+        if ($drive) {
+            Remove-PSDrive -Name $drive.Name
+        }
+    }
+
+    switch($state)
+    {
+        "absent"
+        {
+            if ($metadata.installed) {
+                $metadata.action_state = [ActionState]::AbsentAndInstalled
+            } else {
+                $metadata.action_state = [ActionState]::AbsentAndNotInstalled
+            }
+        }
+
+        "present"
+        {
+            if ($metadata.installed) {
+                $metadata.action_state = [ActionState]::PresentAndInstalled
+            } else {
+                $metadata.action_state = [ActionState]::PresentAndNotInstalled
+                # set the location type and validate the path
+                if ($path.StartsWith("http")) {
+                    $metadata.location_type = [LocationType]::Http
+                    try {
+                        Invoke-WebRequest -Uri $path -DisableKeepAlive -UseBasicParsing -Method HEAD | Out-Null
+                    } catch {
+                        $metadata.path_error = "the file at the URL $path cannot be reached: $($_.Exception.Message)"
+                    }
+                } elseif ($path.StartsWith("/") -or $path.StartsWith("\\")) {
+                    $metadata.location_type = [LocationType]::Unc
+                    if ($credential) {
+                        # Test-Path doesn't support supplying -Credentials, need to create PSDrive before testing
+                        $file_path = Split-Path -Path $path
+                        $file_name = Split-Path -Path $path -Leaf
+                        try {
+                            New-PSDrive -Name win_package -PSProvider FileSystem -Root $file_path -Credential $credential -Scope Script
+                        } catch {
+                            Fail-Json -obj $result -message "failed to connect network drive with credentials: $($_.Exception.Message)"
+                        }
+                        $test_path = "win_package:\$file_name"
+                    } else {
+                        # Someone is using an auth that supports credential delegation, at least it will fail otherwise
+                        $test_path = $path
+                    }
+
+                    $valid_path = Test-Path -Path $test_path -PathType Leaf
+                    if ($valid_path -ne $true) {
+                        $metadata.path_error = "the file at the UNC path $path cannot be reached, ensure the user_name account has access to this path or use an auth transport with credential delegation"
+                    }
+                } else {
+                    $metadata.location_type = [LocationType]::Local
+                    $valid_path = Test-Path -Path $path -PathType Leaf
+                    if ($valid_path -ne $true) {
+                        $metadata.path_error = "the file at the local path $path cannot be reached"
+                    }
+                }
+
+                # throw error if path is not valid
+                if ($metadata.path_error) {
+                    Fail-Json -obj $result -message $metadata.path_error
+                }
+            }
+        }
+    }
+
+    return $metadata
+}
+
+function Convert-Encoding($string) {
+    # this will attempt to detect UTF-16 encoding and convert to UTF-8 for
+    # processes like msiexec
+    $bytes = ([System.Text.Encoding]::Default).GetBytes($string)
+    $is_utf16 = $true
+    for ($i = 0; $i -lt $bytes.Count; $i = $i + 2) {
+        $char = $bytes[$i + 1]
+        if ($char -ne [byte]0) {
+            $is_utf16 = $false
+            break
+        }
+    }
+
+    if ($is_utf16 -eq $true) {
+        return ([System.Text.Encoding]::Unicode).GetString($bytes)
+    } else {
+        return $string
+    }
+}
+
+$program_metadata = Get-ProgramMetadata -state $state -path $path -product_id $product_id -patch_id $patch_id -credential $credential
+
+switch($program_metadata.action_state)
+{
+    "AbsentAndInstalled"
+    {
+        $temp_path = [System.IO.Path]::GetTempPath()
+        $log_file = [System.IO.Path]::GetRandomFileName()
+        $log_path = Join-Path -Path $temp_path -ChildPath $log_file
+
+        $uninstall_arguments = @("$env:windir\system32\msiexec.exe", "/package", $product_id, "MSIPATCHREMOVE=$patch_guid", "/L*V", $log_path, "/qn", "/norestart")
+
+        if (-not $check_mode) {
+            $uninstall_command = Argv-ToString -arguments $uninstall_arguments
+
+            try {
+                $process_result = Run-Command -command $uninstall_command
+            } catch {
+                Fail-Json -obj $result -message "failed to run uninstall process ($uninstall_command): $($_.Exception.Message)"
+            }
+
+            if (($log_path -ne $null) -and (Test-Path -Path $log_path)) {
+                $log_content = Get-Content -Path $log_path | Out-String
+            } else {
+                $log_content = $null
+            }
+
+            $result.rc = $process_result.rc
+            $result.exit_code = $process_result.rc # deprecate in 2.8
+            if ($valid_return_codes -notcontains $process_result.rc) {
+                $result.stdout = Convert-Encoding -string $process_result.stdout
+                $result.stderr = Convert-Encoding -string $process_result.stderr
+                if ($log_content -ne $null) {
+                    $result.log = $log_content
+                }
+                Fail-Json -obj $result -message "unexpected rc from uninstall $uninstall_exe $($uninstall_arguments): see rc, stdout and stderr for more details"
+            } else {
+                $result.failed = $false
+            }
+
+            if ($process_result.rc -eq 3010) {
+                $result.reboot_required = $true
+            }
+        }
+
+        $result.changed = $true
+    }
+
+    "AbsentAndNotInstalled"
+    {
+        # We're all good. Nothing changed.
+    }
+
+    "PresentAndInstalled"
+    {
+        # We're all good. Nothing changed.
+    }
+
+    "PresentAndNotInstalled"
+    {
+        $cleanup_artifacts = @()
+        try {
+            # If path is on a network and we specify credentials or path is a
+            # URL and not an MSI we need to get a temp local copy
+            if ($program_metadata.location_type -eq [LocationType]::Unc -and $credential -ne $null) {
+                $file_name = Split-Path -Path $path -Leaf
+                $local_path = [System.IO.Path]::GetRandomFileName()
+                Copy-Item -Path "win_package:\$file_name" -Destination $local_path -WhatIf:$check_mode                                                
+                $cleanup_artifacts += $local_path
+            } elseif ($program_metadata.location_type -eq [LocationType]::Http -and $program_metadata.msi -ne $true) {
+                $local_path = [System.IO.Path]::GetRandomFileName()
+
+                if (-not $check_mode) {
+                    Download-File -url $path -path $local_path
+                }
+                $cleanup_artifacts += $local_path
+            } else {
+                $local_path = $path
+            }
+
+            $temp_path = [System.IO.Path]::GetTempPath()
+            $log_file = [System.IO.Path]::GetRandomFileName()
+            $log_path = Join-Path -Path $temp_path -ChildPath $log_file
+
+            $cleanup_artifacts += $log_path
+            $install_arguments = @("$env:windir\system32\msiexec.exe", "/p", $local_path, "/L*V", $log_path, "/qn", "/norestart")
+
+            if (-not $check_mode) {
+                $install_command = Argv-ToString -arguments $install_arguments
+
+                try {
+                    $process_result = Run-Command -command $install_command
+                } catch {
+                    Fail-Json -obj $result -message "failed to run install process ($install_command): $($_.Exception.Message)"
+                }
+
+                if (($log_path -ne $null) -and (Test-Path -Path $log_path)) {
+                    $log_content = Get-Content -Path $log_path | Out-String
+                } else {
+                    $log_content = $null
+                }
+
+                $result.rc = $process_result.rc
+                $result.exit_code = $process_result.rc # deprecate in 2.8
+                if ($valid_return_codes -notcontains $process_result.rc) {
+                    $result.stdout = Convert-Encoding -string $process_result.stdout
+                    $result.stderr = Convert-Encoding -string $process_result.stderr
+                    if ($log_content -ne $null) {
+                        $result.log = $log_content
+                    }
+                    Fail-Json -obj $result -message "unexpected rc from install $install_exe $($install_arguments): see rc, stdout and stderr for more details"
+                } else {
+                    $result.failed = $false
+                }
+
+                if ($process_result.rc -eq 3010) {
+                    $result.reboot_required = $true
+                }
+            }
+        } finally {
+            # make sure we cleanup any remaining artifacts
+            foreach ($cleanup_artifact in $cleanup_artifacts) {
+                if (Test-Path -Path $cleanup_artifact) {
+                    Remove-Item -Path $cleanup_artifact -Recurse -Force -WhatIf:$check_mode
+                }
+            }
+        }
+
+        $result.changed = $true
+    }
+}
+
+Exit-Json -obj $result
---
+++ lib/ansible/modules/windows/win_msp.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2018, Rodric Vos <rodric@vosenterprises.eu>, and others
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# this is a windows documentation stub. Actual code lives in the .ps1
+# file of the same name.
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = r'''
+---
+module: win_msp
+short_description: Installs/uninstalls a Microsoft Installer Patch file (*.msp).
+description:
+- Installs or uninstalls a package in MSP format.
+- These packages can be sources from the local file system, network file share
+  or a url.
+author:
+- Rodric Vos (@finzzownt)
+options:
+  expected_return_code:
+    description:
+    - One or more return codes from the package installation that indicates
+      success.
+    - A return code of C(3010) usually means that a reboot is required, the
+      I(reboot_required) return value is set if the return code is C(3010).
+    type: list
+    default: [0, 3010]
+  path:
+    description:
+    - Location of the MSP to be installed, e.g. I(state=present).
+    - This package can either be on the local file system, network share or a
+      url.
+    - If the path is on a network share and the current WinRM transport
+      doesn't support credential delegation, then I(user_name) and I(user_password)
+      must be set to access the file.
+    - There are cases where this file will be copied locally to the server so
+      it can access it, see the notes for more info.
+    - Required when I(state=present).
+  product_id:
+    description:
+    - The product id of the installed package (MSI). This parameter is
+      typically specified as a GUID including curly braces.
+    - You can find product ids for installed programs in the Windows registry
+      either at
+      C(HKLM:Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall) or for 32 bit
+      programs at
+      C(HKLM:Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall).
+    - Required when uninstalling an MSP, e.g. I(state=absent).
+    aliases: [ productid ]
+  patch_id:
+    description:
+    - The patch id of the installed MSP. This parameter is typically specified
+      as a hexadecimal string without curly braces.
+    - You can find patch id for installed patches in the Windows registry at
+      C(HKCR:Installer\\Patches\\GUID).
+    - Required when uninstalling an MSP, e.g. I(state=absent).
+    aliases: [ patchid ]
+  patch_guid:
+    description:
+    - The patch guid of the installed MSP. This parameter is typically specified
+      as a GUID including curly braces.
+    - To find the patch id for a given .MSP file, open the file properties in
+      Windows Explorer, select the Details tab and search for Revision Number.
+      Alternatively, use the psmsi package (https://github.com/heaths/psmsi) to
+      obtain information from the .MSP package.
+    - Required when uninstalling an MSP, e.g. C(state=absent).
+    aliases: [ patchguid ]
+  state:
+    description:
+    - Whether to install or uninstall the MSP.
+    default: present
+  username:
+    description:
+    - Username of an account with access to the package if it is located on a
+      file share.
+    - This is only needed if the WinRM transport is over an auth method that
+      does not support credential delegation like Basic or NTLM.
+    aliases: [ user_name ]
+  password:
+    description:
+    - The password for C(user_name), must be set when C(user_name) is.
+    aliases: [ user_password ]
+  validate_certs:
+    description:
+    - SSL certification check can be disabled, when the MSP file should be
+      obtained from a https location.
+    - If C(no), SSL certificates will not be validated. This should only be
+      used on personally controlled sites using self-signed certificates.
+    type: bool
+    default: 'yes'
+notes:
+- By default all MSP installs and uninstalls will be run with the options
+  C(/L*V /log, /qn, /norestart).
+- It is recommended you download the package first from the URL using the
+  M(win_get_url) module as it opens up more flexibility with what must be set
+  when calling C(win_package).
+- Packages will be temporarily downloaded or copied locally when path is a
+  network location and credential delegation is not set, or path is a URL
+  and the file is not an MSI.
+author:
+- Rodric Vos (@finzzownt)
+'''
+
+EXAMPLES = r'''
+- name: Install SCOM 2016 UR4 over https
+  win_msp:
+    path: http://nexusrepo.domain.local/repository/MyRepo/Microsoft/OperationsManager/KB4024941-AMD64-ENU-Console.msp
+    patch_id: 4E6A3C97C54C78E478CDDF6B3268A6FC
+    state: present
+
+- name: Install SCOM 2016 UR4 over fs
+  win_msp:
+    path: D:\\Staging\\KB4024941-AMD64-ENU-Console.msp
+    patch_id: 4E6A3C97C54C78E478CDDF6B3268A6FC
+    state: present
+
+- name: Install SCOM 2016 UR4 over unc
+  win_msp:
+    path: \\\\packagesrv\data\\KB4024941-AMD64-ENU-Console.msp
+    patch_id: 4E6A3C97C54C78E478CDDF6B3268A6FC
+    username: DOMAIN\\userID
+    password: Secret
+    state: present
+
+- name: Uninstall SCOM 2016 UR4
+  win_msp:
+    patch_id: 4E6A3C97C54C78E478CDDF6B3268A6FC
+    patch_guid: '{79C3A6E4-C45C-4E87-87DC-FDB623866ACF}'
+    product_id: '{E072D8FC-CD31-4ABE-BD65-606965965426}'
+    state: absent
+'''
+
+RETURN = r'''
+rc:
+    description: msiexec.exe return code
+    returned: changed
+    type: int
+    sample: 0
+exit_code:
+    description: destination file/path
+    returned: changed
+    type: int
+    sample: 0
+log:
+    description: msiexec.exe log
+    returned: changed
+    type: string
+reboot_required:
+    description: Indicates if a reboot is required after change
+    returned: changed
+    type: bool
+    sample: true
+'''
