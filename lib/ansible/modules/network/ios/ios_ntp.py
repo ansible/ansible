153ad3358e88c52369bc82f26793a08d91d63cca
Federico87 <lvrfrc87@gmail.com>
2018-12-14 10:38:29 +0000

    update doc

--- lib/ansible/modules/network/ios/ios_ntp.py
+++ lib/ansible/modules/network/ios/ios_ntp.py
@@ -1,7 +1,59 @@
 #!/usr/bin/python
+# Copyright: Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+DOCUMENTATION = '''
+---
+module: ios_ntp
+extends_documentation_fragment: nxos
+version_added: "2.8"
+short_description: Manages core NTP configuration.
+description:
+    - Manages core NTP configuration.
+author:
+    - Federico Olivieri (@Federico87)
+options:
+    server:
+        description:
+            - Network address of NTP server.
+    peer:
+        description:
+            - Network address of NTP peer.
+    source_int:
+        description:
+            - Source interface for NTP requests.  
+     state:
+        description:
+            - Manage the state of the resource.
+        default: present
+        choices: ['present','absent']
+'''
+
+EXAMPLES = '''
+# Set NTP Server with parameters
+- ios_ntp:
+    server: 8.8.8.8
+    source_int: Loopback0
+    acl: NTP_ACL
+    state: present
+    provider: "{{ staging }}"
+'''
+
+RETURN = '''
+commands:
+    description: command sent to the device
+    returned: always
+    type: list
+    sample: ["ntp server 192.0.2.2 prefer key 48",
+             "no ntp source-interface ethernet2/1", "ntp source 192.0.2.3"]
+changed:
+    description: check to see if a change was made on the device
+    returned: always
+    type: boolean
+    sample: true
+'''
 import re
 
-from copy import deepcopy
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.network.ios.ios import get_config, load_config
 from ansible.module_utils.network.ios.ios import ios_argument_spec, check_args
@@ -23,14 +75,6 @@ def parse_peer(line, dest):
             return peer
 
 
-def parse_prefer(line, dest):
-    if dest == 'server':
-        match = re.search(r'(ntp server )(ip )?(\d+\.\d+\.\d+\.\d+)( prefer)', line, re.M)
-        if match:
-            prefer = match.group(3)
-            return prefer
-
-
 def parse_source_int(line, dest):
     if dest == 'source':
         match = re.search(r'(ntp source )(\S+)', line, re.M)
@@ -39,35 +83,12 @@ def parse_source_int(line, dest):
             return source
 
 
-def parse_logging(line, dest):
-    if dest == 'logging':
-        logging = dest
-        return logging
-
-
 def parse_acl(line, dest):
     if dest == 'access-group':
         match = re.search(r'(ntp access-group )(peer )?(serve )?(\S+)', line, re.M)
         if match:
-            acl = match.group(3)
+            acl = match.group(4)
             return acl
-
-
-def parse_auth_key(line, dest):
-    if dest == 'authentication-key':
-        match = re.search(r'(ntp authentication-key \d+ md5 )(\w+)', line, re.M)
-        if match:
-            auth_key = match.group(2)
-            return auth_key
-
-
-def parse_key_id(line, dest):
-    if dest == 'trusted-key':
-        match = re.search(r'(ntp trusted-key )(\d+)', line, re.M)
-        if match:
-            auth_key = match.group(2)
-            return auth_key
-
 
 
 def map_config_to_obj(module):
@@ -78,32 +99,31 @@ def map_config_to_obj(module):
         if match:
             dest = match.group(1)
 
-            obj.append({
-                'server': parse_server(line, dest),
-                'peer': parse_peer(line, dest),
-                'prefer': parse_prefer(line, dest),
-                'source_int': parse_source_int(line, dest),
-                'logging': parse_logging(line, dest),
-                'acl': parse_acl(line, dest),
-                'auth_key': parse_auth_key(line, dest),
-                'key_id': parse_key_id(line, dest)
-            })
-    return obj
+            server = parse_server(line, dest)
+            peer = parse_peer(line, dest)
+            source_int = parse_source_int(line, dest)
+            acl = parse_acl(line, dest)
 
+            if server is not None:
+                obj.append({'server': server})
+            if peer is not None:
+                obj.append({'peer': peer})
+            if source_int is not None:
+                obj.append({'source_int': source_int})
+            if acl is not None:
+                obj.append({'acl': acl})
+
+    return obj
 
 def map_params_to_obj(module, required_if=None):
     obj = []
 
     obj.append({
-        'server': module.params['server'],
         'state': module.params['state'],
+        'server': module.params['server'],
         'peer': module.params['peer'],
-        'prefer': module.params['prefer'],
         'source_int': module.params['source_int'],
-        'logging': module.params['logging'],
-        'acl': module.params['acl'],
-        'auth_key': module.params['auth_key'],
-        'key_id': module.params['key_id'],
+        'acl': module.params['acl']
             })
     return obj
 
@@ -112,57 +132,32 @@ def map_obj_to_commands(updates, module):
     commands = list()
     want, have = updates
     for w in want:
-        state = w['state']
-        server = w['server']
-        peer = w['peer']
-        prefer = w['prefer']
-        source_int = w['source_int']
-        logging = w['logging']
-        acl = w['acl']
-        auth_key = w['auth_key']
-        key_id = w['key_id']
-        del w['state']
+        server = w.get('server')
+        peer = w.get('peer')
+        source_int = w.get('source_int')
+        acl = w.get('acl')
+        state = w.get('state')
 
-        if state == 'absent':
-            if server and not prefer:
+        if state == 'absent' and w in have:
+            if server:
                 commands.append('no ntp server {0}'.format(server))
-            if peer and not prefer:
+            if peer:
                 commands.append('no ntp peer {0}'.format(peer))
-            if prefer and server:
-                commands.append('no ntp server {0} prefer'.format(server))
-            if prefer and peer:
-                commands.append('no ntp peer {0} prefer'.format(peer))
             if source_int:
                 commands.append('no ntp source {0}'.format(source_int))
-            if logging:
-                commands.append('no ntp logging')
             if acl:
                 commands.append('no ntp access-group peer {0}'.format(acl))
-            if auth_key and key_id:
-                commands.append('no ntp authentication-key {0}'.format(key_id, auth_key))
-                commands.append('no ntp authenticate')
-                commands.append('no ntp trusted-key {0}'.format(key_id))
-
 
         elif state == 'present' and w not in have:
-            if server and not prefer:
+            if server:
                 commands.append('ntp server {0}'.format(server))
-            if peer and not prefer:
+            if peer:
                 commands.append('ntp peer {0}'.format(peer))
-            if prefer and server:
-                commands.append('ntp server {0} prefer'.format(server))
-            if prefer and peer:
-                commands.append('ntp peer {0} prefer'.format(peer))
             if source_int:
                 commands.append('ntp source {0}'.format(source_int))
-            if logging:
-                commands.append('ntp logging')
             if acl:
                 commands.append('ntp access-group peer {0}'.format(acl))
-            if auth_key and key_id:
-                commands.append('ntp authentication-key {0} md5 {1}'.format(key_id, auth_key))
-                commands.append('ntp authenticate')
-                commands.append('ntp trusted-key {0}'.format(key_id))
+
 
     return commands
 
@@ -172,12 +167,8 @@ def main():
     argument_spec = dict(
         server=dict(type='str'),
         peer=dict(type='str'),
-        prefer=dict(type='bool'),
         source_int=dict(type='str'),
-        logging=dict(type='str', choices=['enabled', 'disabled'], default='disabled'),
         acl=dict(type='str'),
-        auth_key=dict(type='str'),
-        key_id=dict(type='str'),
         state=dict(choices=['absent', 'present'], default='present')
         )
 
