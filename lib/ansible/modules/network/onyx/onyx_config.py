bcdfd1fda249812ecefa0e1e30c716cbd4e18f1d
Anas Badaha <anasb@mellanox.com>
2018-12-16 12:56:02 +0000

    Fix Issue #39598
    Signed-off-by: Anas Badaha <anasb@mellanox.com>

--- lib/ansible/modules/network/onyx/onyx_config.py
+++ lib/ansible/modules/network/onyx/onyx_config.py
@@ -179,6 +179,7 @@ def run(module, result):
 
     if module.params['save']:
         if not module.check_mode:
+            run_commands(module, 'configure terminal')
             run_commands(module, 'configuration write')
         result['changed'] = True
 
