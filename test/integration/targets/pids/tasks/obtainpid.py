b7739598891d51c84e4bbddd3df692d8a24deabc
saranya sridharan <saranyasridharan17@gmail.com>
2018-12-16 16:33:59 +0530

    adding script file

---
+++ test/integration/targets/pids/tasks/obtainpid.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import sys
+import psutil
+
+names = set(sys.argv[1:])
+
+print [int(p.info['pid']) for p in psutil.process_iter(attrs=['pid', 'name']) if 'python' in p.info['name']]
