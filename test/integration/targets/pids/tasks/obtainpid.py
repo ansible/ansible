9c2c526d2c6fb62bca473ac617672aec7cab3c98
saranya sridharan <saranyasridharan17@gmail.com>
2018-12-16 16:50:19 +0530

    correcting print errors

--- test/integration/targets/pids/tasks/obtainpid.py
+++ test/integration/targets/pids/tasks/obtainpid.py
@@ -2,7 +2,5 @@
 
 import sys
 import psutil
-
 names = set(sys.argv[1:])
-
-print [int(p.info['pid']) for p in psutil.process_iter(attrs=['pid', 'name']) if 'python' in p.info['name']]
+print([int(p.info['pid']) for p in psutil.process_iter(attrs=['pid', 'name']) if names in p.info['name']])
