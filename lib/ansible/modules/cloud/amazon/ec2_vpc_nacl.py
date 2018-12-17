da57c19f34b7dfe3e81becac6db7d5d198834118
Sebastien Rosset (serosset) <serosset@cisco.com>
2018-12-17 07:44:06 -0800

    Simplify logic to skip default deny-all rules

--- lib/ansible/modules/cloud/amazon/ec2_vpc_nacl.py
+++ lib/ansible/modules/cloud/amazon/ec2_vpc_nacl.py
@@ -225,7 +225,7 @@ def nacls_changed(nacl, client, module):
     nacl_id = nacl['NetworkAcls'][0]['NetworkAclId']
     nacl = describe_network_acl(client, module)
     entries = nacl['NetworkAcls'][0]['Entries']
-    tmp_egress = [entry for entry in entries if entry['Egress'] is True and rule['RuleNumber'] < 32767]
+    tmp_egress = [entry for entry in entries if entry['Egress'] is True and entry['RuleNumber'] < 32767]
     tmp_ingress = [entry for entry in entries if entry['Egress'] is False]
     egress = [rule for rule in tmp_egress if rule['RuleNumber'] < 32767]
     ingress = [rule for rule in tmp_ingress if rule['RuleNumber'] < 32767]
