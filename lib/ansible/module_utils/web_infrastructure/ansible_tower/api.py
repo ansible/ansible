25775883910c8c284eaa2ae84a36fcbdab72b729
Sumit Jaiswal <sjaiswal@redhat.com>
2018-12-17 19:55:12 +0530

    tower cli changes update
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>

--- lib/ansible/module_utils/web_infrastructure/ansible_tower/api.py
+++ lib/ansible/module_utils/web_infrastructure/ansible_tower/api.py
@@ -26,315 +26,99 @@
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+from ansible.module_utils.urls import Request
+from ansible.module_utils._text import to_native, to_text
+from ansible.module_utils.six.moves.urllib.parse import urlencode
 
-import os
-import functools
-import json
-import stat
-import urllib
-try:
-    from urlparse import urljoin, urlparse
-except Exception as e:
-    from urllib.parse import urljoin, urlparse
+class ApiEndpoint(object):
+    def __init__(self, host, api, version):
+        self.host = host
+        self.api = api
+        self.version = version
 
-from ansible.utils.display import Display
-display = Display()
+    def __str__(self):
+        url = "https://"
+        url += self.host
+        url += "/"
+        url += self.api
+        url += "/"
+        url += self.version
+        return url
 
-from datetime import datetime as dt
+class AnsibleTowerBase(object):
 
-from requests.exceptions import ConnectionError, SSLError
-from requests.sessions import Session
-from requests.models import Response
-from requests.packages import urllib3
-from requests.auth import AuthBase, HTTPBasicAuth
+    def __init__(self, provider):
+        self.api = 'api'
+        self.version = 'v2'
+        self.headers = {'Content-Type': 'application/json'}
+        self.host = provider['host']
+        self.username = provider['username']
+        self.password = provider['password']
+        self.initial_url = ApiEndpoint(self.host,
+                                       self.api,
+                                       self.version)
+        self.request = Request(headers=self.headers, url_username=self.username, url_password=self.password,
+                               validate_certs=False, force_basic_auth=True)
 
-from ansible.module_utils.web_infrastructure.ansible_tower import exceptions as exc
-from ansible.module_utils.web_infrastructure.ansible_tower.conf import settings, OrderedDict, supports_oauth
-from ansible.module_utils.web_infrastructure.ansible_tower.constants import CUR_API_VERSION
+    def construct_query(self, tower_module, filter_data):
+        # This function constructs the lookup query
+        query_url = ''
+        query_url += str(self.initial_url) + tower_module
+        query_url += "?" + urlencode(filter_data)
+        return query_url
 
-TOWER_DATETIME_FMT = r'%Y-%m-%dT%H:%M:%S.%fZ'
+
+class AnsibleTowerLookup(AnsibleTowerBase):
+
+    def __init__(self, provider, tower_module, filter_data):
+        super(AnsibleTowerLookup, self).__init__(provider)
+        self.tower_module = tower_module
+        self.filter = filter_data
+
+    def request_query(self):
+        # This function will fire get request to check for the filter data
+        query_url = self.construct_query(self.tower_module, self.filter)
+        response = self.request.get(query_url)
+        return response
 
 
-class BasicTowerAuth(AuthBase):
-
-    def __init__(self, username, password, cli_client):
-        self.username = username
-        self.password = password
-        self.cli_client = cli_client
-        self.use_legacy_token = settings.use_token
-
-    def _acquire_token(self):
-        return self.cli_client._make_request(
-            'POST', self.cli_client.get_prefix() + 'authtoken/', [],
-            {'data': json.dumps({'username': self.username, 'password': self.password}),
-             'headers': {'Content-Type': 'application/json'}}
-        ).json()
-
-    def _get_auth_token(self):
-        filename = os.path.expanduser('~/.tower_cli_token.json')
-        token_json = None
+class AnsibleTowerModule(AnsibleTowerBase):
+    def __init__(self, module):
+        self.module = module
+        provider = module.params['provider']
         try:
-            with open(filename) as f:
-                token_json = json.load(f)
-            if not isinstance(token_json, dict) or self.cli_client.get_prefix() not in token_json or \
-                    'token' not in token_json[self.cli_client.get_prefix()] or \
-                    'expires' not in token_json[self.cli_client.get_prefix()] or \
-                    dt.utcnow() > dt.strptime(token_json[self.cli_client.get_prefix()]['expires'], TOWER_DATETIME_FMT):
-                raise Exception("Current token expires.")
-            return 'Token ' + token_json[self.cli_client.get_prefix()]['token']
-        except Exception as e:
-            display.vvvv('Acquiring and caching auth token due to:\n%s' % str(e))
-            if not isinstance(token_json, dict):
-                token_json = {}
-            token_json[self.cli_client.get_prefix()] = self._acquire_token()
-            if not isinstance(token_json[self.cli_client.get_prefix()], dict) or \
-                    'token' not in token_json[self.cli_client.get_prefix()] or \
-                    'expires' not in token_json[self.cli_client.get_prefix()]:
-                raise exc.AuthError('Invalid Tower auth token format: %s' % json.dumps(
-                    token_json[self.cli_client.get_prefix()]
-                ))
-            with open(filename, 'w') as f:
-                json.dump(token_json, f)
-            try:
-                os.chmod(filename, stat.S_IRUSR | stat.S_IWUSR)
-            except Exception as e:
-                display.display("WARNING: Unable to set permissions on {0} - {1}, UserWarning".format(filename, e),
-                                color='yellow')
-            return 'Token ' + token_json[self.cli_client.get_prefix()]['token']
+            super(AnsibleTowerModule, self).__init__(provider)
+        except Exception as exc:
+            self.module.fail_json(msg=to_text(exc))
 
-    def __call__(self, request):
-        if 'Authorization' in request.headers:
-            return request
-        if settings.oauth_token:
-            if supports_oauth:
-                request.headers['Authorization'] = 'Bearer {}'.format(settings.oauth_token)
-                return request
-            else:
-                display.display("WARNING: This version of Tower does not support OAuth2.0", color='yellow')
-        if self.use_legacy_token:
-            resp = self.cli_client._make_request(
-                'OPTIONS', self.cli_client.get_prefix() + 'authtoken/', [], {}
+    def handle_exception(self, method_name, exc):
+        ''' Handles any exceptions raised
+            This method will then gracefully fail the module.
+        '''
+        if ('text' in exc.response):
+            self.module.fail_json(
+                msg=exc.response['text'],
+                type=exc.response['Error'].split(':')[0],
+                code=exc.response.get('code'),
+                operation=method_name
             )
-            if resp.ok:
-                request.headers['Authorization'] = self._get_auth_token()
-            else:
-                display.display("WARNING: use_token is not supported in this version of Tower"
-                                " (the Auth Token API has been replaced with OAuth2.0 support)", color='yellow')
-                HTTPBasicAuth(self.username, self.password)(request)
         else:
-            HTTPBasicAuth(self.username, self.password)(request)
-        return request
+            self.module.fail_json(msg=to_native(exc))
 
+    def run(self, module):
+        ''' Runs the module and performans configuration tasks
+            :args module: the tower module to operate against
+            :returns: a results dict
+        '''
+        result = {'changed': False}
+        response = self.get_object_ref(module)
+        if response.count != 0:
+            pass
+        else:
+            pass
 
-class Client(Session):
-    """A class for making HTTP requests to the Ansible Tower API and
-    returning the responses.
-
-    This functions as a wrapper around [requests][1], and returns its
-    responses; therefore, interact with response objects to this class the
-    same way you would with objects you get back from `requests.get` or
-    similar.
-
-      [1]: http://docs.python-requests.org/en/latest/
-    """
-    def __init__(self):
-        super(Client, self).__init__()
-        for adapter in self.adapters.values():
-            adapter.max_retries = 3
-        self._urlencode = urllib.urlencode
-
-    def _make_request(self, method, url, args, kwargs):
-        # Decide whether to require SSL verification
-        verify_ssl = True
-        if (settings.verify_ssl is False) or hasattr(settings, 'insecure'):
-            verify_ssl = False
-        elif settings.certificate is not None:
-            verify_ssl = settings.certificate
-
-        # Call the superclass method.
-        try:
-            # Ignore/Suppress InsecureRequestWarning
-            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-            return super(Client, self).request(
-                method, url, *args, verify=verify_ssl, **kwargs)
-
-        except SSLError:
-            # Throw error if verify_ssl not set to false and server
-            #  is not using verified certificate.
-            raise exc.ConnectionError(reason='Could not establish a secure connection. Please add the server to '
-                                             'your certificate authority.')
-        except ConnectionError:
-            # Throw error if server can not be reached.
-            raise exc.ConnectionError(reason='Failed to establish a new connection, nodename nor servname provided, or not known')
-
-    def get_prefix(self, include_version=True):
-        """Return the appropriate URL prefix to prepend to requests,
-        based on the host provided in settings.
-        """
-        host = settings.host
-        if '://' not in host:
-            host = 'https://%s' % host.strip('/')
-        elif host.startswith('http://') and settings.verify_ssl:
-            raise exc.TowerCLIError(
-                'Can not verify ssl with non-https protocol. Change the '
-                'verify_ssl configuration setting to continue.'
-            )
-        # Validate that we have either an http or https based URL
-        url_pieces = urlparse(host)
-        if url_pieces[0] not in ['http', 'https']:
-            raise exc.ConnectionError('URL must be http(s), {} is not valid'.format(url_pieces[0]))
-
-        prefix = urljoin(host, '/api/')
-        if include_version:
-            # We add the / to the end of {} so that our URL has the ending slash.
-            prefix = urljoin(prefix, "{}/".format(CUR_API_VERSION))
-
-        return prefix
-
-    def _construct_query_url(self, query_params=None, extattrs=None, force_proxy=False):
-        # to consturct query url
-        if query_params is None:
-            query_params = {}
-        if extattrs is None:
-            extattrs = {}
-        if force_proxy:
-            query_params['_proxy_search'] = 'GM'
-        query = ''
-        if query_params or extattrs:
-            query = '?'
-
-        if extattrs:
-            attrs_queries = []
-            for key, value in extattrs.items():
-                param = "*%s" % key
-                value = value['value']
-                if isinstance(value, list):
-                    for item in value:
-                        attrs_queries.append(self._urlencode({param: item}))
-                else:
-                    attrs_queries.append(self._urlencode({param: value}))
-            query += '&'.join(attrs_queries)
-        if query_params:
-            if len(query) > 1:
-                query += '&'
-            query += self._urlencode(query_params)
-        return query
-
-    @functools.wraps(Session.request)
-    def request(self, method, url, query_filter, *args, **kwargs):
-        """Make a request to the Ansible Tower API, and return the
-        response.
-        """
-        # If the URL has the api/vX at the front strip it off
-        # This is common to have if you are extracting a URL from an existing object.
-        # For example, any of the 'related' fields of an object will have this
-        import re
-        url = re.sub("^/?api/v[0-9]+/", "", url)
-        # Piece together the full URL.
-        use_version = not url.startswith('/o/')
-        url = '%s%s' % (self.get_prefix(use_version), url.lstrip('/'))
-
-        if isinstance(query_filter, dict):
-            query = self._construct_query_url(query_filter)
-            url = url + query
-
-        # Ansible Tower expects authenticated requests; add the authentication
-        # from settings if it's provided.
-        kwargs.setdefault(
-            'auth',
-            BasicTowerAuth(
-                settings.username,
-                settings.password,
-                self
-            )
-        )
-        # POST and PUT requests will send JSON by default; make this
-        # the content_type by default.  This makes it such that we don't have
-        # to constantly write that in our code, which gets repetitive.
-        headers = kwargs.get('headers', {})
-        if method.upper() in ('PATCH', 'POST', 'PUT'):
-            headers.setdefault('Content-Type', 'application/json')
-            kwargs['headers'] = headers
-
-        # If debugging is on, print the URL and data being sent.
-        display.vvvv('%s %s' % (method, url))
-        if method in ('POST', 'PUT', 'PATCH'):
-            display.vvvv('Data: %s' % kwargs.get('data', {}))
-        if method == 'GET' or kwargs.get('params', None):
-            display.vvvv('Params: %s' % kwargs.get('params', {}))
-        display.vvvv('')
-
-        # If this is a JSON request, encode the data value.
-        if headers.get('Content-Type', '') == 'application/json':
-            kwargs['data'] = json.dumps(kwargs.get('data', {}))
-
-        r = self._make_request(method, url, args, kwargs)
-        # Sanity check: Did the server send back some kind of internal error?
-        # If so, bubble this up.
-        if r.status_code >= 500:
-            raise exc.ServerError(reason='The Tower server sent back a server error. '
-                                  'Please try again later.')
-
-        # Sanity check: Did we fail to authenticate properly?
-        # If so, fail out now; this is always a failure.
-        if r.status_code == 401:
-            raise exc.AuthError(reason='Invalid Tower authentication credentials (HTTP 401).')
-
-        # Sanity check: Did we get a forbidden response, which means that
-        # the user isn't allowed to do this? Report that.
-        if r.status_code == 403:
-            raise exc.Forbidden(reason="You don't have permission to do that (HTTP 403).")
-
-        # Sanity check: Did we get a 404 response?
-        # Requests with primary keys will return a 404 if there is no response,
-        # and we want to consistently trap these.
-        if r.status_code == 404:
-            raise exc.NotFound(reason='The requested object could not be found.')
-
-        # Sanity check: Did we get a 405 response?
-        # A 405 means we used a method that isn't allowed. Usually this
-        # is a bad request, but it requires special treatment because the
-        # API sends it as a logic error in a few situations (e.g. trying to
-        # cancel a job that isn't running).
-        if r.status_code == 405:
-            raise exc.MethodNotAllowed(reason="The Tower server says you can't make a request with the "
-                                              "%s method to that URL (%s)." % (method, url))
-
-        # Sanity check: Did we get some other kind of error?
-        # If so, write an appropriate error message.
-        if r.status_code >= 400:
-            raise exc.BadRequest(reason='The Tower server claims it was sent a bad request.\n\n'
-                                        '%s %s\nParams: %s\nData: %s\n\nResponse: %s' %
-                                        (method, url, kwargs.get('params', None),
-                                         kwargs.get('data', None), r.content.decode('utf8')))
-
-        # Django REST Framework intelligently prints API keys in the
-        # order that they are defined in the models and serializer.
-        #
-        # We want to preserve this behavior when it is possible to do so
-        # with minimal effort, because while the order has no explicit meaning,
-        # we make some effort to order keys in a convenient manner.
-        #
-        # To this end, make this response into an APIResponse subclass
-        # (defined below), which has a `json` method that doesn't lose key
-        # order.
-        r.__class__ = APIResponse
-
-        # Return the response object.
-        return r.json()
-
-
-class APIResponse(Response):
-    """A Response subclass which preseves JSON key order (but makes no other
-    changes).
-    """
-    def json(self, **kwargs):
-        kwargs.setdefault('object_pairs_hook', OrderedDict)
-        try:
-            return super(APIResponse, self).json(**kwargs)
-        except Exception:
-            kwargs.pop('object_pairs_hook', None)
-            return super(APIResponse, self).json(**kwargs)
-
-
-client = Client()
+    def get_object_ref(self, tower_module):
+        tower_module = '/' + tower_module + '/'
+        query_url = self.construct_query(tower_module, self.filter)
+        response = self.request.get(query_url)
+        return response
