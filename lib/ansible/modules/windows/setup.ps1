f84ab6ba6cc29da70a83865c2d6724085a7fda16
Gregory Storme <gstorme@users.noreply.github.com>
2018-12-17 23:09:19 +0100

    fix facts memtotal_mb rounding on VMWare and swaptotal_mb conversion from KB to MB (#49940)

--- lib/ansible/modules/windows/setup.ps1
+++ lib/ansible/modules/windows/setup.ps1
@@ -298,8 +298,8 @@ if($gather_subset.Contains('memory')) {
     $win32_os = Get-LazyCimInstance Win32_OperatingSystem
     $ansible_facts += @{
         # Win32_PhysicalMemory is empty on some virtual platforms
-        ansible_memtotal_mb = ([math]::round($win32_cs.TotalPhysicalMemory / 1024 / 1024))
-        ansible_swaptotal_mb = ([math]::round($win32_os.TotalSwapSpaceSize / 1024 / 1024))
+        ansible_memtotal_mb = ([math]::ceiling($win32_cs.TotalPhysicalMemory / 1024 / 1024))
+        ansible_swaptotal_mb = ([math]::round($win32_os.TotalSwapSpaceSize / 1024))
     }
 }
 
