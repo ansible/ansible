7c12e0b0b24271e0ce73762cced2e80028e08e09
Dag Wieers <dag@wieers.com>
2018-12-18 13:07:04 +0100

    Implement feedback from review

--- test/sanity/code-smell/no-smart-quotes.py
+++ test/sanity/code-smell/no-smart-quotes.py
@@ -8,34 +8,39 @@ import sys
 
 def main():
     skip = set([
-        'test/sanity/code-smell/%s' % os.path.basename(__file__),
         'docs/docsite/rst/dev_guide/testing/sanity/no-smart-quotes.rst',
-        'test/integration/targets/unicode/unicode.yml',
         'test/integration/targets/lookup_properties/lookup-8859-15.ini',
         'test/integration/targets/template/files/encoding_1252_utf-8.expected',
         'test/integration/targets/template/files/encoding_1252_windows-1252.expected',
         'test/integration/targets/template/templates/encoding_1252.j2',
+        'test/integration/targets/unicode/unicode.yml',
+        'test/sanity/code-smell/%s' % os.path.basename(__file__),
+    ])
+
+    prune = set([
         'docs/docsite/_build/',
     ])
 
     for path in sys.argv[1:] or sys.stdin.read().splitlines():
-        for skip_path in skip:
-            if path.startswith(skip_path):
-                break
-        else:
-            with open(path, 'rb') as path_fd:
-                for line, text in enumerate(path_fd.readlines()):
-                    try:
-                        text = text.decode('utf-8')
-                    except UnicodeDecodeError as ex:
-                        print('%s:%d:%d: UnicodeDecodeError: %s' % (path, line + 1, ex.start + 1, ex))
-                        continue
+        if path in skip:
+            continue
 
-                    match = re.search(u'([‘’“”])', text)
+        if any(path.startswith(p) for p in prune):
+            continue
 
-                    if match:
-                        print('%s:%d:%d: use ASCII quotes `\'` and `"` instead of Unicode quotes' % (
-                            path, line + 1, match.start(1) + 1))
+        with open(path, 'rb') as path_fd:
+            for line, text in enumerate(path_fd.readlines()):
+                try:
+                    text = text.decode('utf-8')
+                except UnicodeDecodeError as ex:
+                    print('%s:%d:%d: UnicodeDecodeError: %s' % (path, line + 1, ex.start + 1, ex))
+                    continue
+
+                match = re.search(u'([‘’“”])', text)
+
+                if match:
+                    print('%s:%d:%d: use ASCII quotes `\'` and `"` instead of Unicode quotes' % (
+                        path, line + 1, match.start(1) + 1))
 
 
 if __name__ == '__main__':
