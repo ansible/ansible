a8cebbb7e85f4b5c229528ff4c7c05bb8520c8da
Alan Rominger <arominge@redhat.com>
2018-12-19 12:30:38 -0500

    update openshift inventory plugin docs (#49901)

--- lib/ansible/plugins/inventory/openshift.py
+++ lib/ansible/plugins/inventory/openshift.py
@@ -91,14 +91,14 @@ EXAMPLES = '''
 # Authenticate with token, and return all pods and services for all namespaces
 plugin: openshift
 connections:
-    host: https://192.168.64.4:8443
-    token: xxxxxxxxxxxxxxxx
-    ssl_verify: false
+  - host: https://192.168.64.4:8443
+    api_key: xxxxxxxxxxxxxxxx
+    verify_ssl: false
 
 # Use default config (~/.kube/config) file and active context, and return objects for a specific namespace
 plugin: openshift
 connections:
-    namespaces:
+  - namespaces:
     - testing
 
 # Use a custom config file, and a specific context.
