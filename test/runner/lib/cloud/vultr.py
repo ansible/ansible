62e13ffacd90d83029e44efc8c7098dfa9ae5140
Rene Moser <mail@renemoser.net>
2018-12-19 15:29:47 +0100

    test: vultr: convert legacy tests to targets

---
+++ test/runner/lib/cloud/vultr.py
@@ -0,0 +1,56 @@
+"""Vultr plugin for integration tests."""
+from __future__ import absolute_import, print_function
+
+import os
+
+from lib.cloud import (
+    CloudProvider,
+    CloudEnvironment
+)
+
+from lib.util import ConfigParser
+
+
+class VultrCloudProvider(CloudProvider):
+    """Checks if a configuration file has been passed or fixtures are going to be used for testing"""
+
+    def __init__(self, args):
+        super(VultrCloudProvider, self).__init__(args, config_extension='.ini')
+
+    def filter(self, targets, exclude):
+        if os.path.isfile(self.config_static_path):
+            return
+
+        super(VultrCloudProvider, self).filter(targets, exclude)
+
+    def setup(self):
+        super(VultrCloudProvider, self).setup()
+
+        if os.path.isfile(self.config_static_path):
+            self.config_path = self.config_static_path
+            self.managed = False
+            return True
+        return False
+
+
+class VultrCloudEnvironment(CloudEnvironment):
+    """
+    Updates integration test environment after delegation. Will setup the config file as parameter.
+    """
+
+    def configure_environment(self, env, cmd):
+        """
+        :type env: dict[str, str]
+        :type cmd: list[str]
+        """
+        parser = ConfigParser()
+        parser.read(self.config_path)
+
+        changes = dict(
+            VULTR_API_KEY=parser.get('default', 'key'),
+        )
+
+        env.update(changes)
+
+        cmd.append('-e')
+        cmd.append('resource_prefix=%s' % self.resource_prefix)
