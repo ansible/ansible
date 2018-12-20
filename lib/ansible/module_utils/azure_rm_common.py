80463275e90f5149d00675f321adb83d45fdcb8d
Zim Kalinowski <zikalino@microsoft.com>
2018-12-20 16:35:55 +0800

    fixing network client version (#50182)

--- lib/ansible/module_utils/azure_rm_common.py
+++ lib/ansible/module_utils/azure_rm_common.py
@@ -212,7 +212,7 @@ AZURE_PKG_VERSIONS = {
     },
     'NetworkManagementClient': {
         'package_name': 'network',
-        'expected_version': '2.2.1'
+        'expected_version': '2.3.0'
     },
     'ResourceManagementClient': {
         'package_name': 'resource',
