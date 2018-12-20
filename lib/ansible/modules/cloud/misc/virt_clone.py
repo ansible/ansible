63a9cb21578edfbb188d0ddfe452c93370760676
Abhijeet Kasurde <akasurde@redhat.com>
2018-12-20 14:52:27 -0500

    Update lib/ansible/modules/cloud/misc/virt_clone.py
    Co-Authored-By: mrlesmithjr <mrlesmithjr@gmail.com>

--- lib/ansible/modules/cloud/misc/virt_clone.py
+++ lib/ansible/modules/cloud/misc/virt_clone.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Larry Smith Jr. <mrlesmithjr@gmail.com>
+# Copyright: (c) 2017, Larry Smith Jr. <mrlesmithjr@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 # Requirements:
