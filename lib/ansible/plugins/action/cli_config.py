a60957e3532b17622a814ba8b5163b838411a69c
Nathaniel Case <this.is@nathanielca.se>
2018-12-21 09:55:14 -0500

    Add backup parameter to cli_config (#50206)
    * Add backup parameter to cli_config
    
    * Add a unit test for cli_config backup

--- lib/ansible/plugins/action/cli_config.py
+++ lib/ansible/plugins/action/cli_config.py
@@ -19,7 +19,15 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import glob
+import os
+import re
+import time
+
 from ansible.plugins.action.normal import ActionModule as _ActionModule
+
+
+PRIVATE_KEYS_RE = re.compile('__.+__')
 
 
 class ActionModule(_ActionModule):
@@ -28,4 +36,37 @@ class ActionModule(_ActionModule):
         if self._play_context.connection != 'network_cli':
             return {'failed': True, 'msg': 'Connection type %s is not valid for cli_config module' % self._play_context.connection}
 
-        return super(ActionModule, self).run(task_vars=task_vars)
+        result = super(ActionModule, self).run(task_vars=task_vars)
+
+        if self._task.args.get('backup') and result.get('__backup__'):
+            # User requested backup and no error occurred in module.
+            # NOTE: If there is a parameter error, _backup key may not be in results.
+            filepath = self._write_backup(task_vars['inventory_hostname'],
+                                          result['__backup__'])
+
+            result['backup_path'] = filepath
+
+        # strip out any keys that have two leading and two trailing
+        # underscore characters
+        for key in list(result.keys()):
+            if PRIVATE_KEYS_RE.match(key):
+                del result[key]
+
+        return result
+
+    def _get_working_path(self):
+        cwd = self._loader.get_basedir()
+        if self._task._role is not None:
+            cwd = self._task._role._role_path
+        return cwd
+
+    def _write_backup(self, host, contents):
+        backup_path = self._get_working_path() + '/backup'
+        if not os.path.exists(backup_path):
+            os.mkdir(backup_path)
+        for existing_backup in glob.glob('%s/%s_config.*' % (backup_path, host)):
+            os.remove(existing_backup)
+        tstamp = time.strftime("%Y-%m-%d@%H:%M:%S", time.localtime(time.time()))
+        filename = '%s/%s_config.%s' % (backup_path, host, tstamp)
+        open(filename, 'w').write(contents)
+        return filename
