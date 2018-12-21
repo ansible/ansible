e83bba56bf6391edcd8cfd03657a75d91e409d31
anasbadaha <43231942+anasbadaha@users.noreply.github.com>
2018-12-21 08:24:10 +0200

    Fix Issue #39598 (#49982)
    * Fix Issue #39598
    
    Signed-off-by: Anas Badaha <anasb@mellanox.com>
    
    * Fix unit test for onyx_config
    
    Signed-off-by: Anas Badaha <anasb@mellanox.com>
    
    * Add comments for save running config
    
    Signed-off-by: Anas Badaha <anasb@mellanox.com>
    
    * Enhance onyx_config bug fix
    
    Signed-off-by: Anas Badaha <anasb@mellanox.com>
    
    * Fix result['changed'] = True comment
    
    Signed-off-by: Anas Badaha <anasb@mellanox.com>

--- lib/ansible/modules/network/onyx/onyx_config.py
+++ lib/ansible/modules/network/onyx/onyx_config.py
@@ -159,6 +159,7 @@ def run(module, result):
     else:
         configobjs = candidate.items
 
+    total_commands = []
     if configobjs:
         commands = dumps(configobjs, 'commands').split('\n')
 
@@ -169,18 +170,15 @@ def run(module, result):
             if module.params['after']:
                 commands.extend(module.params['after'])
 
-        result['updates'] = commands
-
-        # send the configuration commands to the device and merge
-        # them with the current running config
-        if not module.check_mode:
-            load_config(module, commands)
-        result['changed'] = True
+        total_commands.extend(commands)
+        result['updates'] = total_commands
 
     if module.params['save']:
-        if not module.check_mode:
-            run_commands(module, 'configuration write')
+            total_commands.append('configuration write')
+    if total_commands:
         result['changed'] = True
+        if not module.check_mode:
+            load_config(module, total_commands)
 
 
 def main():
