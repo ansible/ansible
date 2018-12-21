738a30a5a0fd8a90eb9043fa9c52bd54b7fb5157
Matt Clay <matt@mystile.com>
2018-12-20 18:28:46 -0800

    Surface errors as warnings.

--- test/runner/lib/commands/env.py
+++ test/runner/lib/commands/env.py
@@ -15,6 +15,7 @@ from lib.config import (
 
 from lib.util import (
     display,
+    find_executable,
     raw_command,
     SubprocessError,
     ApplicationError,
@@ -71,6 +72,7 @@ def command_env(args):
     if args.show:
         verbose = {
             'docker': 3,
+            'docker.executable': 0,
             'environ': 2,
             'platform.uname': 1,
         }
@@ -102,7 +104,8 @@ def show_dict(data, verbose, root_verbosity=0, path=None):
             for item in value:
                 display.info(indent + '  - %s' % item, verbosity=verbosity)
         elif isinstance(value, dict):
-            display.info(indent + '%s:' % key, verbosity=verbosity)
+            min_verbosity = min([verbosity] + [v for k, v in verbose.items() if k.startswith('%s.' % key)])
+            display.info(indent + '%s:' % key, verbosity=min_verbosity)
             show_dict(value, verbose, verbosity, key_path)
         else:
             display.info(indent + '%s: %s' % (key, value), verbosity=verbosity)
@@ -120,7 +123,8 @@ def get_ansible_version(args):
     try:
         ansible_version, _dummy = raw_command(cmd, env=env, capture=True)
         ansible_version = ansible_version.strip()
-    except SubprocessError:
+    except SubprocessError as ex:
+        display.warning('Unable to get Ansible version:\n%s' % ex)
         ansible_version = None
 
     return ansible_version
@@ -131,17 +135,23 @@ def get_docker_details(args):
     :type args: CommonConfig
     :rtype: dict[str, any]
     """
-    try:
-        info = docker_info(args)
-    except ApplicationError:
-        info = None
+    docker = find_executable('docker', required=False)
+    info = None
+    version = None
 
-    try:
-        version = docker_version(args)
-    except ApplicationError:
-        version = None
+    if docker:
+        try:
+            info = docker_info(args)
+        except SubprocessError as ex:
+            display.warning('Failed to collect docker info:\n%s' % ex)
+
+        try:
+            version = docker_version(args)
+        except SubprocessError as ex:
+            display.warning('Failed to collect docker version:\n%s' % ex)
 
     docker_details = dict(
+        executable=docker,
         info=info,
         version=version,
     )
