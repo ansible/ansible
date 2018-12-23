6fc35085b4d218408487dfe9ffd47a62086274b6
John R Barker <john@johnrbarker.com>
2018-12-23 09:42:29 +0000

    Revert TEMP HACK: Upload all to codecov (#50279)
    Codecov.io have made some improvements to results processing.
    Upload all the coverage data to see is codecov can process this.
    
    Reverts https://github.com/ansible/ansible/pull/47515

--- test/utils/shippable/shippable.sh
+++ test/utils/shippable/shippable.sh
@@ -86,8 +86,7 @@ function cleanup
         cp -a test/results/reports/coverage=*.xml shippable/codecoverage/
 
         # upload coverage report to codecov.io only when using complete on-demand coverage
-        # HACK: Only upload certain results to codecov to avoid overloading it
-        if [ "${COVERAGE}" ] && [ "${CHANGED}" == "" ] && [[ "$T" =~ /1$ ]] ; then
+        if [ "${COVERAGE}" ] && [ "${CHANGED}" == "" ]; then
             for file in test/results/reports/coverage=*.xml; do
                 flags="${file##*/coverage=}"
                 flags="${flags%.xml}"
