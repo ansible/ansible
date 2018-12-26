6b641702424eed94197d87f49ac436f8406882eb
skylerbunny <skylerrabbit@yahoo.com>
2018-12-25 16:41:07 -0800

    Update opening description with valid usage locations

--- lib/ansible/modules/utilities/logic/include_role.py
+++ lib/ansible/modules/utilities/logic/include_role.py
@@ -20,7 +20,9 @@ author: Ansible Core Team (@ansible)
 module: include_role
 short_description: Load and execute a role
 description:
-  - When used underneath a `tasks:` directive, M(include_role) dynamically loads and executes a specified role as a task.
+  - M(include_role) dynamically loads and executes a specified role as a task. It may be used (and has meaning) only
+    where Ansible tasks are allowed: inside a `pre_tasks:`, `tasks:`, or `post_tasks:` playbook object, or as a
+    task inside a role.
   - Unlike M(import_role), M(Loops), and M(Conditionals) apply to this statement.
   - The do until loop is not supported on M(include_role).
   - This module is also supported for Windows targets.
