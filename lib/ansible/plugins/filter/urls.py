eed7a7a5829a4980d7ab7bcb11f607e048ba0c0f
Dag Wieers <dag@wieers.com>
2018-12-28 04:52:18 +0100

    Fixes for Python 3

--- lib/ansible/plugins/filter/urls.py
+++ lib/ansible/plugins/filter/urls.py
@@ -6,12 +6,16 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
+from ansible.module_utils.six import PY3
 from ansible.module_utils.six.moves.urllib.parse import unquote_plus
 from ansible.module_utils._text import to_bytes, to_text
 
 
 def urldecode(string):
-    return to_text(unquote_plus(to_bytes(string, errors='surrogate_then_strict')))
+    if PY3:
+        return unquote_plus(string)
+    else:
+        return to_text(unquote_plus(to_bytes(string)))
 
 
 class FilterModule(object):
