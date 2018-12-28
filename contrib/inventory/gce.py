a1510029d2f577654bf9e4f016c846e28140273f
Thiago Carvalho <thiagonbcarvalho@gmail.com>
2018-12-28 16:13:30 +0000

    Fix sanity tests issues

--- contrib/inventory/gce.py
+++ contrib/inventory/gce.py
@@ -493,7 +493,7 @@ class GceInventory(object):
 
             labels = node.extra['labels']
             if labels is not None:
-                for k,v in labels.iteritems():
+                for k, v in labels.items():
                     label = 'label_%s_value_%s' % (k,v)
                     if label in groups:
                         groups[label].append(name)
