56eeb7129bbb566f47eadf6df1e644c8b361a5b5
Tomi Raittinen <tomi.raittinen@gmail.com>
2019-01-02 15:56:35 +0200

    Added description, vsys and admin commit options.

--- lib/ansible/modules/network/panos/panos_commit.py
+++ lib/ansible/modules/network/panos/panos_commit.py
@@ -3,6 +3,7 @@
 #
 # Ansible module to manage PaloAltoNetworks Firewall
 # (c) 2016, techbizdev <techbizdev@paloaltonetworks.com>
+# (c) 2019, Tomi Raittinen <tomi.raittinen@gmail.com>
 #
 # This file is part of Ansible
 #
@@ -26,7 +27,10 @@ short_description: commit firewall's candidate configuration
 description:
     - PanOS module that will commit firewall's candidate configuration on
     - the device. The new configuration will become active immediately.
-author: "Luigi Mori (@jtschichold), Ivan Bojer (@ivanbojer)"
+author:
+    - Luigi Mori (@jtschichold)
+    - Ivan Bojer (@ivanbojer)
+    - Tomi Raittinen (@traittinen)
 version_added: "2.3"
 requirements:
     - pan-python
@@ -43,6 +47,21 @@ options:
             - if commit should be synchronous
         type: bool
         default: 'yes'
+    description:
+        description:
+            - Commit description/comment
+        type: str
+        version_added: "2.8"
+    commit_changes_by:
+        description:
+            - Commit changes made by specified admin
+        type: list
+        version_added: "2.8"
+    commit_vsys:
+        description:
+            - Commit changes for specified VSYS
+        type: list
+        version_added: "2.8"
 extends_documentation_fragment: panos
 '''
 
@@ -55,11 +74,32 @@ EXAMPLES = '''
 '''
 
 RETURN = '''
-status:
-    description: success status
-    returned: success
-    type: string
-    sample: "okey dokey"
+panos_commit:
+    description: Information about commit job.
+    returned: always
+    type: complex
+    version_added: 2.8
+    contains:
+        job_id:
+            description: Palo Alto job ID for the commit operation. Only returned if commit job is launched on device.
+            returned: always
+            type: str
+            sample: "139"
+        status_code:
+            description: Palo Alto API status code. Null if commit is successful.
+            returned: always
+            type: str
+            sample: 19
+        status_detail:
+            description: Palo Alto API detailed status message.
+            returned: always
+            type: str
+            sample: Configuration committed successfully
+        status_text:
+            description: Palo Alto API status text.
+            returned: always
+            type: str
+            sample: success
 '''
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
@@ -67,7 +107,8 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'supported_by': 'community'}
 
 
-from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.basic import AnsibleModule, env_fallback
+from lxml import etree
 
 try:
     import pan.xapi
@@ -78,12 +119,15 @@ except ImportError:
 
 def main():
     argument_spec = dict(
-        ip_address=dict(),
-        password=dict(no_log=True),
-        username=dict(default='admin'),
+        ip_address=dict(required=True, type='str'),
+        password=dict(fallback=(env_fallback, ['ANSIBLE_NET_PASSWORD']), no_log=True),
+        username=dict(fallback=(env_fallback, ['ANSIBLE_NET_USERNAME'])),
         interval=dict(default=0.5),
         timeout=dict(),
-        sync=dict(type='bool', default=True)
+        sync=dict(type='bool', default=True),
+        description=dict(type='str'),
+        commit_changes_by=dict(type='list'),
+        commit_vsys=dict(type='list')
     )
     module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=False)
 
@@ -93,10 +137,14 @@ def main():
     ip_address = module.params["ip_address"]
     if not ip_address:
         module.fail_json(msg="ip_address should be specified")
+
     password = module.params["password"]
     if not password:
         module.fail_json(msg="password is required")
+
     username = module.params['username']
+    if not username:
+        module.fail_json(msg="username is required")
 
     interval = module.params['interval']
     timeout = module.params['timeout']
@@ -108,14 +156,62 @@ def main():
         api_password=password
     )
 
+    cmd = "<commit>"
+
+    description = module.params["description"]
+    if description:
+        cmd += "<description>{}</description>".format(description)
+
+    commit_changes_by = module.params["commit_changes_by"]
+    commit_vsys = module.params["commit_vsys"]
+
+    if commit_changes_by or commit_vsys:
+
+        cmd += "<partial>"
+
+        if commit_changes_by:
+            cmd += "<admin>"
+            for admin in commit_changes_by:
+                cmd += "<member>{}</member>".format(admin)
+            cmd += "</admin>"
+
+        if commit_vsys:
+            cmd += "<vsys>"
+            for vsys in commit_vsys:
+                cmd += "<member>{}</member>".format(vsys)
+            cmd += "</vsys>"
+
+        cmd += "</partial><force></force>"
+
+    cmd += "</commit>"
+
     xapi.commit(
-        cmd="<commit></commit>",
+        cmd=cmd,
         sync=sync,
         interval=interval,
         timeout=timeout
     )
 
-    module.exit_json(changed=True, msg="okey dokey")
+    try:
+        result = xapi.xml_root().encode('utf-8')
+        root = etree.fromstring(result)
+        job_id = root.find('./result/job/id').text
+    except:
+        job_id = None
+
+    panos_commit_details = dict(
+        status_text=xapi.status,
+        status_code=xapi.status_code,
+        status_detail=xapi.status_detail,
+        job_id=job_id
+    )
+
+    if job_id:
+        changed = True
+    else:
+        changed = False
+
+    module.exit_json(changed=changed, msg=xapi.status_detail, panos_commit=panos_commit_details)
 
 
 if __name__ == '__main__':
