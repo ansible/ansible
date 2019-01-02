e9c194af1d16799924de6b38fde1f00fd820a6ef
s-hertel <shertel@redhat.com>
2019-01-02 07:54:58 -0500

    Update the inventory_cache documentation

--- lib/ansible/plugins/doc_fragments/inventory_cache.py
+++ lib/ansible/plugins/doc_fragments/inventory_cache.py
@@ -23,6 +23,7 @@ options:
     description:
       - Cache plugin to use for the inventory's source data.
     type: str
+    default: memory
     env:
       - name: ANSIBLE_INVENTORY_CACHE_PLUGIN
     ini:
@@ -35,16 +36,34 @@ options:
     type: int
     env:
       - name: ANSIBLE_INVENTORY_CACHE_TIMEOUT
+      - name: ANSIBLE_CACHE_PLUGIN_TIMEOUT
     ini:
       - section: inventory
         key: cache_timeout
+      - section: defaults
+        key: fact_caching_timeout
   cache_connection:
     description:
       - Cache connection data or path, read cache plugin documentation for specifics.
     type: str
     env:
       - name: ANSIBLE_INVENTORY_CACHE_CONNECTION
+      - name: ANSIBLE_CACHE_PLUGIN_CONNECTION
     ini:
       - section: inventory
         key: cache_connection
+      - section: defaults
+        key: fact_caching_connection
+  _prefix:
+    description:
+      - Prefix to use for cache plugin files/tables
+    default: ansible_inventory_
+    env:
+      - name: ANSIBLE_INVENTORY_CACHE_PLUGIN_PREFIX
+      - name: ANSIBLE_CACHE_PLUGIN_PREFIX
+    ini:
+      - section: inventory
+        key: cache_prefix
+      - section: default
+        key: fact_caching_prefix
 '''
