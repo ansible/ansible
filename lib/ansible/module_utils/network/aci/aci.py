f90ec1746535747c14785691ed785caa3c545947
Dag Wieers <dag@wieers.com>
2019-01-04 00:38:57 +0100

    ACI: Implement encoded query_string (#50358)
    This requires urldecode support in Ansible

--- lib/ansible/module_utils/network/aci/aci.py
+++ lib/ansible/module_utils/network/aci/aci.py
@@ -37,6 +37,7 @@ import os
 from copy import deepcopy
 
 from ansible.module_utils.parsing.convert_bool import boolean
+from ansible.module_utils.six.moves.urllib.parse import urlencode
 from ansible.module_utils.urls import fetch_url
 from ansible.module_utils._text import to_bytes
 
@@ -387,13 +388,13 @@ class ACIModule(object):
     # TODO: This could be designed to update existing keys
     def update_qs(self, params):
         ''' Append key-value pairs to self.filter_string '''
-        accepted_params = dict((k, v) for (k, v) in params.items() if v)
+        accepted_params = dict((k, v) for (k, v) in params.items() if v is not None)
         if accepted_params:
             if self.filter_string:
                 self.filter_string += '&'
             else:
                 self.filter_string = '?'
-            self.filter_string += '&'.join(['%s=%s' % (k, v) for (k, v) in accepted_params.items()])
+            self.filter_string += urlencode(accepted_params)
 
     # TODO: This could be designed to accept multiple obj_classes and keys
     def build_filter(self, obj_class, params):
