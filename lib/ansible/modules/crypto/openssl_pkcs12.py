4561a5007baa12e5333846a15047bce181805cbb
Felix Fontein <felix@fontein.de>
2019-01-03 12:34:46 +0100

    openssl_*: prevent error when path includes no path (#50322)
    * Prevent error when path includes no path.
    
    * Add changelog.
    
    (cherry picked from commit 5c5cd2dc4d0a58a12233397d9682233f7b4c5e9a)

--- lib/ansible/modules/crypto/openssl_pkcs12.py
+++ lib/ansible/modules/crypto/openssl_pkcs12.py
@@ -312,7 +312,7 @@ def main():
     if not pyopenssl_found:
         module.fail_json(msg='The python pyOpenSSL library is required')
 
-    base_dir = os.path.dirname(module.params['path'])
+    base_dir = os.path.dirname(module.params['path']) or '.'
     if not os.path.isdir(base_dir):
         module.fail_json(
             name=base_dir,
