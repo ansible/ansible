dda78779d681beb33598500af6696f6cba505f53
Matt Martz <matt@sivel.net>
2019-01-04 11:25:35 -0600

    Check for file and not just exists

--- test/integration/targets/old_style_modules_posix/library/helloworld.sh
+++ test/integration/targets/old_style_modules_posix/library/helloworld.sh
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
-if [ -e "$1" ]; then
+if [ -f "$1" ]; then
     source "$1"
 else
     echo '{"msg": "No argument file provided", "failed": True}'
