64f9820440837900224956f96453e58c27fe6ea5
Ganesh Nalawade <ganesh634@gmail.com>
2019-01-04 17:38:07 +0530

    Fix junos terminal error regex (#50538)
    *  Change junos terminal error regex to read error string
       correctly.

--- lib/ansible/plugins/terminal/junos.py
+++ lib/ansible/plugins/terminal/junos.py
@@ -38,7 +38,7 @@ class TerminalModule(TerminalBase):
     terminal_stderr_re = [
         re.compile(br"unknown command"),
         re.compile(br"syntax error"),
-        re.compile(br"error: commit failed")
+        re.compile(br"[\r\n]error:")
     ]
 
     def on_open_shell(self):
