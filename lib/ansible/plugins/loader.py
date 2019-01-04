0f6f564af412e457ce6e8a5d3dc103a792e81d7b
Toshio Kuratomi <a.badger@gmail.com>
2019-01-03 22:42:06 -0800

    Rename BECOME_PLUGIN_PATH to DEFAULT_BECOME_PLUGIN_PATH
    As alikins said, all other plugin paths are named
    DEFAULT_plugintype_PLUGIN_PATH.  If we're going to rename these, that
    should be done all at one time rather than piecemeal.

--- lib/ansible/plugins/loader.py
+++ lib/ansible/plugins/loader.py
@@ -834,6 +834,6 @@ httpapi_loader = PluginLoader(
 become_loader = PluginLoader(
     'BecomeModule',
     'ansible.plugins.become',
-    C.BECOME_PLUGIN_PATH,
+    C.DEFAULT_BECOME_PLUGIN_PATH,
     'become_plugins'
 )
