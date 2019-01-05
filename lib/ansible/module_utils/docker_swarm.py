684b327c0517c99915e4abfb28438809002e1992
WojciechowskiPiotr <devel@it-playground.pl>
2019-01-05 16:25:36 +0100

    docker_swarm: Removed the shebang

--- lib/ansible/module_utils/docker_swarm.py
+++ lib/ansible/module_utils/docker_swarm.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-#
 # (c) 2019 Piotr Wojciechowski (@wojciechowskipiotr) <piotr@it-playground.pl>
 # (c) Thierry Bouvet (@tbouvet)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
