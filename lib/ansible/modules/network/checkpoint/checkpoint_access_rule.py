e3208bdeb7af33d9b77cfe6e353372000c10a8e1
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2019-01-07 15:34:14 +0100

    Fix sanity tests issues

--- lib/ansible/modules/network/checkpoint/checkpoint_access_rule.py
+++ lib/ansible/modules/network/checkpoint/checkpoint_access_rule.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.connection import Connection
-from ansible.module_utils.network.checkpoint.checkpoint import publish, install_policy
-import json
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'network'}
+
 
 DOCUMENTATION = """
 ---
@@ -52,12 +65,18 @@ options:
   enabled:
     description:
       - Enabled or disabled flag.
-    type: boolean
+    type: bool
   state:
     description:
       - State of the access rule (present or absent). Defaults to present.
     type: str
+    default: present
 """
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.connection import Connection
+from ansible.module_utils.network.checkpoint.checkpoint import publish, install_policy
+import json
 
 
 def get_access_rule(module, connection):
