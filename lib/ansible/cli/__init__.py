1429b04d5de4875c3d65b8ee258c33945a05709b
Matt Martz <matt@sivel.net>
2019-01-08 16:31:38 -0600

    Add support for argcomplete

--- lib/ansible/cli/__init__.py
+++ lib/ansible/cli/__init__.py
@@ -30,6 +30,12 @@ from ansible.vars.manager import VariableManager
 from ansible.parsing.vault import PromptVaultSecret, get_file_vault_secret
 from ansible.plugins.loader import add_all_plugin_dirs
 
+try:
+    import argcomplete
+    HAS_ARGCOMPLETE = True
+except ImportError:
+    HAS_ARGCOMPLETE = False
+
 
 display = Display()
 
@@ -336,6 +342,10 @@ class CLI(with_metaclass(ABCMeta, object)):
         are called from this function before and after parsing the arguments.
         """
         self.init_parser()
+
+        if HAS_ARGCOMPLETE:
+            argcomplete.autocomplete(self.parser)
+
         options = self.parser.parse_args(self.args[1:])
         options = self.post_process_args(options)
         context._init_global_context(options)
