896361a6473a880930580b0426b1f19e541b3956
Alicia Cozine <879121+acozine@users.noreply.github.com>
2019-01-09 08:21:26 -0600

    fix docs for tower modules (#50710)

--- lib/ansible/modules/web_infrastructure/ansible_tower/tower_workflow_template.py
+++ lib/ansible/modules/web_infrastructure/ansible_tower/tower_workflow_template.py
@@ -68,7 +68,7 @@ EXAMPLES = '''
     name: Workflow Template
     description: My very first Worflow Template
     organization: My optional Organization
-    schema: "{{ lookup(file, my_workflow.json }}"
+    schema: "{{ lookup('file', 'my_workflow.json') }}"
 
 - tower_worflow_template:
     name: Workflow Template
