09cc61149da52efbf74ec10307a712a61e27cf16
Jonathan Hanson <triplepoint@gmail.com>
2019-01-09 01:44:15 -0800

    inventory: vagrant: rename deprectated ansible_ssh_*  (#50694)

--- contrib/inventory/vagrant.py
+++ contrib/inventory/vagrant.py
@@ -47,10 +47,10 @@ from ansible.module_utils.six.moves import StringIO
 
 
 _group = 'vagrant'  # a default group
-_ssh_to_ansible = [('user', 'ansible_ssh_user'),
-                   ('hostname', 'ansible_ssh_host'),
+_ssh_to_ansible = [('user', 'ansible_user'),
+                   ('hostname', 'ansible_host'),
                    ('identityfile', 'ansible_ssh_private_key_file'),
-                   ('port', 'ansible_ssh_port')]
+                   ('port', 'ansible_port')]
 
 # Options
 # ------------------------------
