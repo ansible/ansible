be63af31a4d92ad43cebc54f5389edc0dbfaf743
Jan-Philipp Litza <jplitza@users.noreply.github.com>
2019-01-09 16:56:30 +0100

    Fix sanitizing config lines (#50553)
    Previously, the index got out of sync with the actual config list. Invoked with lines:
      - bad
      - first
      - bad
      - second
    the sanitization would first delete index 0 and then index 2, which would result in the output
      - first
      - bad
    By reversing the list, we avoid that problem (though a filter() would be nicer)

--- lib/ansible/modules/network/edgeos/edgeos_config.py
+++ lib/ansible/modules/network/edgeos/edgeos_config.py
@@ -207,7 +207,7 @@ def diff_config(commands, config):
 def sanitize_config(config, result):
     result['filtered'] = list()
     for regex in CONFIG_FILTERS:
-        for index, line in enumerate(list(config)):
+        for index, line in reversed(list(enumerate(config))):
             if regex.search(line):
                 result['filtered'].append(line)
                 del config[index]
