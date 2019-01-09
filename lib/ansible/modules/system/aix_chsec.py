907ddc5e8e2cb749994ba97b030b92b8948479d6
Dag Wieers <dag@wieers.com>
2019-01-09 20:37:11 +0100

    Various changes to get this module past CI

--- lib/ansible/modules/system/aix_chsec.py
+++ lib/ansible/modules/system/aix_chsec.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Copyright: (c) 2018, Christian Tremel (@flynn1973)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
@@ -11,49 +13,47 @@ ANSIBLE_METADATA = {
     'supported_by': 'community'
 }
 
-DOCUMENTATION = '''
+DOCUMENTATION = r'''
 ---
 module: aix_chsec
-
 short_description: Modify AIX stanza files
-
 version_added: '2.8'
-
 description:
-    - Modify stanzas to AIX config files using the chsec command.
-notes:
-    - See `man chsec` for additional information.
-
+- Modify stanzas to AIX config files using the C(chsec) command.
 options:
-    path:
-        description:
-            - Path to the stanza file.
-        type: path
-        required: true
-    stanza:
-        description:
-            - Name of stanza.
-        required: true
-    options:
-         description:
-             - A list of key/value pairs, e.g. `key=val,key=val`
-         type: list
-    state:
-         description:
-            - If set to C(absent) the whole stanza incl. all given options will be removed.
-            - If set to C(present) stanza incl.options will be added.
-            - To remove an option from the stanza set to C(present) and set key to an empty value (key=).
-            - All rules/allowed file-stanza combos/allowed files for the chsec command also applies here, so once again, read "man chsec"!
-         type: str
-         choices: [ absent, present ]
-         default: present
-
-
+  path:
+    description:
+    - Path to the stanza file.
+    type: path
+    required: true
+    aliases: [ dest ]
+  stanza:
+    description:
+    - Name of stanza.
+    type: str
+    required: true
+  options:
+     description:
+     - A list of key/value pairs, e.g. C(key=val,key=val).
+     type: list
+  state:
+     description:
+     - If set to C(absent) the whole stanza incl. all given options will be removed.
+     - If set to C(present) stanza incl.options will be added.
+     - To remove an option from the stanza set to C(present) and set key to an empty value (key=).
+     - All rules, allowed file-stanza combos or allowed files for the C(chsec) command also applies here.
+     type: str
+     choices: [ absent, present ]
+     default: present
+seealso:
+- name: The chsec manual page from the IBM Knowledge Center
+  description: Changes the attributes in the security stanza files.
+  link: https://www.ibm.com/support/knowledgecenter/en/ssw_aix_72/com.ibm.aix.cmds1/chsec.htm
 author:
-    - Christian Tremel (@flynn1973)
+- Christian Tremel (@flynn1973)
 '''
 
-EXAMPLES = '''
+EXAMPLES = r'''
 - name: Add an LDAP user stanza
   aix_chsec:
     path: /etc/security/user
@@ -69,7 +69,7 @@ EXAMPLES = '''
     options: logintimes=:0800-1700
     state: present
 
-- name: Remove registry option from stanza 
+- name: Remove registry option from stanza
   aix_chsec:
     path: /etc/security/user
     stanza: ldapuser
@@ -79,9 +79,10 @@ EXAMPLES = '''
     state: present
 '''
 
+RETURN = r'''
+'''
 
 from ansible.module_utils.basic import AnsibleModule
-
 
 
 def do_stanza(module, filename, stanza, options, state='present'):
@@ -90,31 +91,31 @@ def do_stanza(module, filename, stanza, options, state='present'):
 
     def arguments_generator(options):
         for element in options:
-                yield '-a'
-                yield element
+            yield '-a'
+            yield element
 
     command = [chsec_command, '-f', filename, '-s', '%s' % stanza]
     options = list(arguments_generator(options))
 
     if state == 'present':
         command += options
-        rc, package_result, err = module.run_command(command)
+        rc, stdout, stderr = module.run_command(command)
         if rc != 0:
-            module.fail_json(msg='Failed to run chsec command (present).', rc=rc, err=err)
+            module.fail_json(msg='Failed to run chsec command (present).', rc=rc, stdout=stdout, stderr=stderr)
         else:
-            msg='stanza added'
-            changed=True
+            msg = 'stanza added'
+            changed = True
     elif state == 'absent':
-        # remove values from keys to enable chsec delete mode 
-        command += [s[:1+s.find('=')] or s for s in options]
-        rc, package_result, err = module.run_command(command)
+        # remove values from keys to enable chsec delete mode
+        command += [s[:1 + s.find('=')] or s for s in options]
+        rc, stdout, stderr = module.run_command(command)
         if rc != 0:
-             module.fail_json(msg='Failed to run chsec command (absent).', rc=rc, err=err)
+            module.fail_json(msg='Failed to run chsec command (absent).', rc=rc, stdout=stdout, stderr=stderr)
         else:
-             msg='stanza removed'
-             changed=True
+            msg = 'stanza removed'
+            changed = True
 
-    return (changed, msg)
+    return changed, msg
 
 
 def main():
@@ -124,7 +125,7 @@ def main():
             path=dict(type='path', required=True, aliases=['dest']),
             stanza=dict(type='str', required=True),
             options=dict(type='list', required=True),
-            state=dict(type='str', default='present', choices=['absent', 'present'])
+            state=dict(type='str', default='present', choices=['absent', 'present']),
         ),
         supports_check_mode=False,
     )
@@ -134,17 +135,15 @@ def main():
     options = module.params['options']
     state = module.params['state']
 
-    (changed, msg) = do_stanza(module, path, stanza, options, state)
-
-
     result = dict(
-        changed=changed,
-        msg=msg,
-        path=path,
+        changed=False,
     )
+
+    result['changed'], result['msg'] = do_stanza(module, path, stanza, options, state)
 
     # Mission complete
     module.exit_json(**result)
+
 
 if __name__ == '__main__':
     main()
