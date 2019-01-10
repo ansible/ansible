16433af49d2dd069b3f44a4c10a398114e1f0cf6
WojciechowskiPiotr <devel@it-playground.pl>
2019-01-10 23:19:13 +0100

    docker_node: Additional method to shared module to get formatted output list of registered nodes

--- lib/ansible/module_utils/docker_swarm.py
+++ lib/ansible/module_utils/docker_swarm.py
@@ -170,3 +170,40 @@ class AnsibleDockerSwarmClient(AnsibleDockerClient):
         json_str = json.dumps(node_info, ensure_ascii=False)
         node_info = json.loads(json_str)
         return node_info
+
+    def get_all_nodes(self, output='short'):
+        """
+        Returns list of nodes registered in Swarm
+
+        :param output: Defines format of returned data
+        :return:
+            If 'output' is 'short' then return data is list of nodes hostnames registered in Swarm,
+            if 'output' is 'long' then returns data is list of dict containing the attributes as in
+            output of command 'docker node ls'
+        """
+        nodes_list = []
+
+        nodes_inspect = self.get_all_nodes_inspect()
+        if nodes_inspect is None:
+            return None
+
+        if output == 'short':
+            for node in nodes_inspect:
+                nodes_list.append(node['Description']['Hostname'])
+        elif output == 'long':
+            for node in nodes_inspect:
+                node_property = {}
+
+                node_property.update({'ID': node['ID']})
+                node_property.update({'Hostname': node['Description']['Hostname']})
+                node_property.update({'Status': node['Status']['State']})
+                node_property.update({'Availability': node['Spec']['Availability']})
+                if 'ManagerStatus' in node:
+                    node_property.update({'ManagerStatus': node['ManagerStatus']['Reachability']})
+                node_property.update({'EngineVersion': node['Description']['Engine']['EngineVersion']})
+
+                nodes_list.append(node_property)
+        else:
+            return None
+
+        return nodes_list
