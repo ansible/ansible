9b0256229b4aabe0295d3ab818deca74a809c1c0
Matt Clay <matt@mystile.com>
2019-01-10 14:45:12 -0800

    Cleanup integration test inventory. (#50753)
    * Move var_blending test inventory into test.
    * Remove Amazon specific inventory entry for tests.
    * Remove Azure specific inventory entry for tests.
    * Move var_precedence test inventory into test.
    * Move unicode test inventory into test.
    * Remove unused inventory entry.
    * Move gathering_facts test inventory into test.
    * Move delegate_to test inventory into test.
    * Clean up inventory for binary_modules test.
    * Clean up integration test inventory.

--- test/integration/targets/var_precedence/runme.sh
+++ test/integration/targets/var_precedence/runme.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-ansible-playbook test_var_precedence.yml -i ../../inventory -v "$@" \
+ansible-playbook test_var_precedence.yml -i inventory -v "$@" \
     -e 'extra_var=extra_var' \
     -e 'extra_var_override=extra_var_override'
 
