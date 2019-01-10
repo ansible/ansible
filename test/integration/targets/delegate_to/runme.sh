26b8fbddaddb0cf1c8dfdb0326d2ae54da3a139b
Matt Clay <matt@mystile.com>
2019-01-10 14:45:12 -0800

    [stable-2.6] Cleanup integration test inventory. (#50753)
    * Move var_blending test inventory into test.
    * Remove Amazon specific inventory entry for tests.
    * Remove Azure specific inventory entry for tests.
    * Move var_precedence test inventory into test.
    * Move unicode test inventory into test.
    * Remove unused inventory entry.
    * Move gathering_facts test inventory into test.
    * Move delegate_to test inventory into test.
    * Clean up inventory for binary_modules test.
    * Clean up integration test inventory..
    (cherry picked from commit e5094e8071ad4693089f7345e7aa4715ccfaa429)
    
    Co-authored-by: Matt Clay <matt@mystile.com>

--- test/integration/targets/delegate_to/runme.sh
+++ test/integration/targets/delegate_to/runme.sh
@@ -3,7 +3,7 @@
 set -eux
 
 ANSIBLE_SSH_ARGS='-C -o ControlMaster=auto -o ControlPersist=60s -o UserKnownHostsFile=/dev/null' \
-    ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook test_delegate_to.yml -i ../../inventory -v "$@"
+    ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook test_delegate_to.yml -i inventory -v "$@"
 
 ansible-playbook test_loop_control.yml -v "$@"
 
