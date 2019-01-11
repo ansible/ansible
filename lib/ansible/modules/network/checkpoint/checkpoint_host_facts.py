cef32a96e592e17ec7cdeb4c8f9d0590a43cde5e
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2019-01-11 10:53:55 +0100

    Rebase with devel and add docstrings

--- lib/ansible/modules/network/checkpoint/checkpoint_host_facts.py
+++ lib/ansible/modules/network/checkpoint/checkpoint_host_facts.py
@@ -1,6 +1,64 @@
 #!/usr/bin/python
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'network'}
+
+
+DOCUMENTATION = """
+---
+module: checkpoint_host_facts
+short_description: Get host objects facts on Checkpoint over Web Services API
+description:
+  - Get host objects facts on Checkpoint devices.
+    All operations are performed over Web Services API.
+version_added: "2.8"
+author: "Ansible by Red Hat (@rcarrillocruz)"
+options:
+  name:
+    description:
+      - Name of the host object. If name is not provided, UID is required.
+    type: str
+  uid:
+    description:
+      - UID of the host object. If UID is not provided, name is required.
+    type: str
+"""
+
+EXAMPLES = """
+- name: Get host object facts
+  checkpoint_host_facts:
+    name: attacker
+"""
+
+RETURN = """
+ansible_hosts:
+  description: The checkpoint host object facts.
+  returned: always.
+  type: list
+"""
+
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.connection import Connection
@@ -28,9 +86,12 @@ def main():
         uid=dict(type='str'),
     )
 
-    module = AnsibleModule(argument_spec=argument_spec)
+    required_one_of = [('name', 'uid')]
+    module = AnsibleModule(argument_spec=argument_spec, required_one_of=required_one_of)
     connection = Connection(module._socket_path)
+
     code, response = get_host(module, connection)
+
     if code == 200:
         module.exit_json(ansible_facts=dict(checkpoint_hosts=response))
     else:
