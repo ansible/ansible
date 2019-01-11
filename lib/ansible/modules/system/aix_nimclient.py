d0ad749d18b5fb63499630fdbdbb88c58f0cba04
Dag Wieers <dag@wieers.com>
2019-01-11 07:09:10 +0100

    Update lib/ansible/modules/system/aix_nimclient.py
    Co-Authored-By: molekuul <joris.weijters@xs4all.nl>

--- lib/ansible/modules/system/aix_nimclient.py
+++ lib/ansible/modules/system/aix_nimclient.py
@@ -11,7 +11,7 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
-DOCUMENTATION = '''
+DOCUMENTATION = r'''
 ---
 author:
 - Joris Weijters (@molekuul)
