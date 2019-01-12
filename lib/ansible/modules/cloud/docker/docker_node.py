45b08f0d0c3c1ce695b1b78ab14bb01572443f17
WojciechowskiPiotr <devel@it-playground.pl>
2019-01-12 20:47:51 +0100

    docker_node: Updated operations on labels, tracking if change is required

--- lib/ansible/modules/cloud/docker/docker_node.py
+++ lib/ansible/modules/cloud/docker/docker_node.py
@@ -17,43 +17,42 @@ module: docker_node
 short_description: Manage Docker Swarm node
 version_added: "2.8"
 description:
-    - Manages the Docker nodes via Swarm Manager
-    - Change node role
-    - Change node availability
-    - Change or remove node labels
+    - Manages the Docker nodes via Swarm Manager.
+    - This module allows to change the node's role, its availability, and to modify, add or remove node labels.
 options:
-    state:
+    hostname:
         description:
-            - Set to C(update) to update node labels, availability and role
-        required: true
-        choices:
-          - update
-    name:
-        description:
-            - Swarm name of the node.
-            - Used with I(state=update).
+            - The hostname or ID of node as registered in Swarm.
+            - If more than one node is registered using the same hostname the ID must be used,
+              otherwise task will fail.
     labels:
         description: User-defined key/value metadata.
+        type: dict
     labels_state:
         description:
-            - Defines the operation on the lables assigned to node.
-            - Set to C(merge) to merge provided labels with already assigned ones. It will update existing keys.
-              The I(labels) must be specified.
+            - Defines the operation on the labels assigned to node.
+            - Set to C(merge) to combine labels provided in I(labels) with those already assigned to the node.
+              If no labels are assigned then it will add listed labels. The I(labels) must be specified.
+            - If set as C(merge) for labels that are assigned to node will update their values as specified in I(labels).
+            - If set as C(merge) labels assigned to node that are not specified in I(labels) will remain unchanged.
+            - If set as C(merge) will add new labels specified in I(labels) if labels are not assigned to node.
             - Set to C(replace) to replace assigned labels with provided ones. The I(labels) must be specified.
-            - Set to C(remove) to remove all labels assigned to node.
+            - Set to C(remove) to remove labels specified in I(labels). If I(labels) is not provided then will remove
+              all labels assigned to the node. If I(labels) is specified it will remove only the listed labels.
+              Unassigned labels from I(labels) are ignored.
         choices:
           - merge
           - replace
           - remove
         default: 'merge'
     availability:
-        description: Node availability status to assign
+        description: Node availability status to assign.
         choices:
           - active
           - pause
           - drain
     role:
-        description: Node role to assign
+        description: Node role to assign.
         choices:
           - manager
           - worker
@@ -78,49 +77,45 @@ author:
 '''
 
 EXAMPLES = '''
-- name: List names of registered swarm nodes
-  docker_node:
-    state: list
-
 - name: Set node role
   docker_node:
-    state: update
-    name: mynode
+    hostname: mynode
     role: manager
 
 - name: Set node availability
   docker_node:
-    state: update
-    name: mynode
+    hostname: mynode
     availability: drain
 
 - name: Replace node labels with new labels
   docker_node:
-    state: update
-    name: mynode
+    hostname: mynode
     labels:
       key: value
     labels_state: replace
 
 - name: Merge node labels and new labels
   docker_node:
-    state: update
-    name: mynode
+    hostname: mynode
     labels:
       key: value
 
 - name: Merge node labels and new labels
   docker_node:
-    state: update
-    name: mynode
+    hostname: mynode
     labels:
       key: value
     labels_state: merge
 
+- name: Remove all labels assigned to node
+  docker_node:
+    hostname: mynode
+    labels:
+    labels_state: replace
+
 - name: Remove node labels
   docker_node:
-    state: update
-    name: mynode
+    hostname: mynode
     labels_state: remove
 '''
 
@@ -156,8 +151,6 @@ class TaskParameters(DockerBaseClass):
     def __init__(self, client):
         super(TaskParameters, self).__init__()
 
-        self.state = None
-
         # Spec
         self.name = None
         self.labels = None
@@ -183,18 +176,13 @@ class SwarmNodeManager(DockerBaseClass):
 
         self.parameters = TaskParameters(client)
 
-    def __call__(self):
-        choice_map = {
-            "update": self.node_update,
-        }
-
-        choice_map.get(self.parameters.state)()
+        self.node_update()
 
     def node_update(self):
         if not (self.client.check_if_swarm_manager()):
             self.client.fail(msg="This node is not a manager.")
 
-        if not (self.client.check_if_swarm_node(node_id=self.parameters.name)):
+        if not (self.client.check_if_swarm_node(node_id=self.parameters.hostname)):
             self.results['actions'].append("This node is not part of a swarm.")
             return
 
@@ -207,48 +195,74 @@ class SwarmNodeManager(DockerBaseClass):
             self.client.fail(msg="Can not update the node. The status node is down.")
 
         try:
-            node_info = self.client.inspect_node(node_id=self.parameters.name)
+            node_info = self.client.inspect_node(node_id=self.parameters.hostname)
         except APIError as exc:
             self.client.fail(msg="Failed to get node information for %s" % to_native(exc))
 
-        if self.parameters.role is None:
-            self.parameters.role = node_info['Spec']['Role']
-
-        if self.parameters.availability is None:
-            self.parameters.availability = node_info['Spec']['Availability']
-
-        if self.parameters.labels is None:
-            self.parameters.labels = node_info['Spec']['Labels']
-        else:
-            if self.parameters.labels_state == 'remove':
-                self.parameters.labels = {}
-            if self.parameters.labels_state == 'replace':
-                pass
-            if self.parameters.labels_state == 'merge':
-                for next_key in node_info['Spec']['Labels']:
-                    if next_key not in self.parameters.labels:
-                        self.parameters.labels.update({next_key: node_info['Spec']['Labels'][next_key]})
-
+        __changed = False
         node_spec = dict(
             Availability=self.parameters.availability,
             Role=self.parameters.role,
             Labels=self.parameters.labels,
         )
 
-        try:
-            self.client.update_node(node_id=node_info['ID'], version=node_info['Version']['Index'],
-                                    node_spec=node_spec)
-        except APIError as exc:
-            self.client.fail(msg="Failed to update node : %s" % to_native(exc))
-        self.results['node_facts'] = self.client.get_node_inspect(node_id=node_info['ID'])
-        self.results['actions'].append("Node updated")
-        self.results['changed'] = True
+        if self.parameters.role is None:
+            node_spec['Role'] = node_info['Spec']['Role']
+
+        if self.parameters.availability is None:
+            node_spec['Availability'] = node_info['Spec']['Availability']
+
+        if self.parameters.labels_state == 'replace':
+            node_spec['Labels'] = self.parameters.labels
+            __changed = True
+
+        if self.parameters.labels_state == 'remove':
+            node_spec['Labels'] = node_info['Spec']['Labels']
+
+            if self.parameters.labels is None and node_info['Spec']['Labels'] is not None:
+                node_spec['Labels'] = None
+                __changed = True
+            elif self.parameters.labels is not None and node_info['Spec']['Labels'] is not None:
+                for next_key in self.parameters.labels:
+                    if next_key in node_info['Spec']['Labels']:
+                        try:
+                            node_spec['Labels'].pop(next_key)
+                            __changed = True
+                        except KeyError as exc:
+                            self.client.fail(msg="Failed to remove labels for %s" % to_native(exc))
+
+        if self.parameters.labels_state == 'merge':
+            node_spec['Labels'] = node_info['Spec']['Labels']
+
+            for next_key in self.parameters.labels:
+                if next_key in node_info['Spec']['Labels']:
+                    if self.parameters.labels.get(next_key) == node_info['Spec']['Labels'][next_key]:
+                        pass
+                    else:
+                        node_spec['Labels'].update({next_key: self.parameters.labels.get(next_key)})
+                        __changed = True
+                else:
+                    node_spec['Labels'].update({next_key: self.parameters.labels.get(next_key)})
+                    __changed = True
+
+        if __changed is True:
+            try:
+                self.client.update_node(node_id=node_info['ID'], version=node_info['Version']['Index'],
+                                        node_spec=node_spec)
+            except APIError as exc:
+                self.client.fail(msg="Failed to update node : %s" % to_native(exc))
+            self.results['node_facts'] = self.client.get_node_inspect(node_id=node_info['ID'])
+            self.results['actions'].append("Node updated")
+            self.results['changed'] = __changed
+        else:
+            self.results['node_facts'] = node_info
+            self.results['actions'].append("Nothing to update")
+            self.results['changed'] = __changed
 
 
 def main():
     argument_spec = dict(
-        state=dict(type='str', choices=['update'], required=True),
-        name=dict(type='str'),
+        hostname=dict(type='str'),
         labels=dict(type='dict'),
         labels_state=dict(type='str', choices=['merge', 'replace', 'remove'], default='merge'),
         availability=dict(type='str', choices=['active', 'pause', 'drain']),
@@ -256,9 +270,8 @@ def main():
     )
 
     required_if = [
-        ('state', 'update', ['name']),
-        ('labels_state', 'merge', ['name', 'labels']),
-        ('labels_state', 'replace', ['name', 'labels']),
+        ('labels_state', 'merge', ['hostname', 'labels']),
+        ('labels_state', 'replace', ['hostname', 'labels']),
     ]
 
     option_minimal_versions = dict(
@@ -278,11 +291,10 @@ def main():
 
     results = dict(
         changed=False,
-        result='',
         actions=[],
     )
 
-    SwarmNodeManager(client, results)()
+    SwarmNodeManager(client, results)
     client.module.exit_json(**results)
 
 
