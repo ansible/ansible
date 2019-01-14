3f3aba4d8c5c1eb21c43c3025579b63a464c49f3
Manuel Molina Cuberos <mmc@pocosmhz.org>
2019-01-14 21:38:54 +0100

    Preventing '/dev/tty not found' problems (#49950)
    * Preventing '/dev/tty not found' problems when ' apt_repository` calls GPG in order to import keys.

--- lib/ansible/modules/packaging/os/apt_repository.py
+++ lib/ansible/modules/packaging/os/apt_repository.py
@@ -430,7 +430,7 @@ class UbuntuSourcesList(SourcesList):
             if self.add_ppa_signing_keys_callback is not None:
                 info = self._get_ppa_info(ppa_owner, ppa_name)
                 if not self._key_already_exists(info['signing_key_fingerprint']):
-                    command = ['apt-key', 'adv', '--recv-keys', '--keyserver', 'hkp://keyserver.ubuntu.com:80', info['signing_key_fingerprint']]
+                    command = ['apt-key', 'adv', '--recv-keys', '--no-tty', '--keyserver', 'hkp://keyserver.ubuntu.com:80', info['signing_key_fingerprint']]
                     self.add_ppa_signing_keys_callback(command)
 
             file = file or self._suggest_filename('%s_%s' % (line, self.codename))
