4029d4f88cc9ce573849fa007fc0067b1302fd70
Sumit Jaiswal <sjaiswal@redhat.com>
2019-01-14 15:37:15 +0530

    skydive modules
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>

--- lib/ansible/modules/network/skydive/skydive_capture.py
+++ lib/ansible/modules/network/skydive/skydive_capture.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# (c) 2017, Ansible by Red Hat, inc
+# (c) 2018, Ansible by Red Hat, inc
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
