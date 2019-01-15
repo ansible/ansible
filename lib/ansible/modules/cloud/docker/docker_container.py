8cc23c080269a332d21c5d3a5428e0a4f22fbd3d
Felix Fontein <felix@fontein.de>
2019-01-15 05:02:24 +0100

    [2.7] [docker_container] Failing on non-string env values (#50899)
    * [docker_container] Failing on non-string env values (#49843)
    
    * [docker_container] Failing on non-string env values
    
    Fixes #49802
    
    * Clarify failure message
    
    Co-Authored-By: DBendit <David@ibendit.com>
    
    * Fixup from review
    
    (cherry picked from commit d62d7176b0359df0ab205b85c828b29c596a01ef)
    
    * Turn fail into warning for 2.7 backport.
    
    * Fix test for backport
    
    The behaviour in the backport is to warn rather than error

--- lib/ansible/modules/cloud/docker/docker_container.py
+++ lib/ansible/modules/cloud/docker/docker_container.py
@@ -93,6 +93,8 @@ options:
   env:
     description:
       - Dictionary of key,value pairs.
+      - Values which might be parsed as numbers, booleans or other types by the YAML parser must be quoted (e.g. C("true")) in order to avoid data loss.
+    type: dict
   env_file:
     version_added: "2.2"
     description:
@@ -489,7 +491,9 @@ EXAMPLES = '''
      - "8080:9000"
      - "127.0.0.1:8081:9001/udp"
     env:
-        SECRET_KEY: ssssh
+        SECRET_KEY: "ssssh"
+        # Values which might be parsed as numbers, booleans or other types by the YAML parser need to be quoted
+        BOOLEAN_KEY: "yes"
 
 - name: Container present
   docker_container:
@@ -1224,6 +1228,12 @@ class TaskParameters(DockerBaseClass):
                 final_env[name] = str(value)
         if self.env:
             for name, value in self.env.items():
+                if not isinstance(value, string_types):
+                    self.client.module.warn(
+                        "Non-string value found for env option. "
+                        "Ambiguous env options should be wrapped in quotes to avoid YAML parsing. "
+                        "This will become an error in Ansible 2.8. "
+                        "Key: %s; value will be treated as: %s" % (name, str(value)))
                 final_env[name] = str(value)
         return final_env
 
