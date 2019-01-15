b8bd23989995a1f79d0cfa1ab77e9dbf806a06a6
Dag Wieers <dag@wieers.com>
2019-01-15 12:24:34 +0100

    Update documentation notes, copyright and license (#50927)

--- lib/ansible/modules/network/aci/msc_label.py
+++ lib/ansible/modules/network/aci/msc_label.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Copyright: (c) 2018, Dag Wieers (@dagwieers) <dag@wieers.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
