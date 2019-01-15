e84de70f515ea71b35dda8d8f29d9587f3d7bb04
Matt Clay <matt@mystile.com>
2019-01-15 09:06:41 -0800

    Fix false positives in no-get-exception test.

--- test/sanity/code-smell/no-get-exception.py
+++ test/sanity/code-smell/no-get-exception.py
@@ -19,7 +19,7 @@ def main():
 
         with open(path, 'r') as path_fd:
             for line, text in enumerate(path_fd.readlines()):
-                match = re.search(r'(get_exception)', text)
+                match = re.search(r'([^a-zA-Z0-9_]get_exception[^a-zA-Z0-9_])', text)
 
                 if match:
                     if path == 'lib/ansible/module_utils/basic.py' and basic_allow_once:
