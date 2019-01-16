80f7d619ec1264ec56900d436d0a96530c690d42
Chris Stephens <chrisst@google.com>
2019-01-16 03:25:06 +0000

    Make Disk KMS features GA
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- lib/ansible/modules/cloud/google/gcp_compute_disk_facts.py
+++ lib/ansible/modules/cloud/google/gcp_compute_disk_facts.py
@@ -183,6 +183,11 @@ items:
             key that protects this resource.
           returned: success
           type: str
+        kmsKeyName:
+          description:
+          - The name of the encryption key that is stored in Google Cloud KMS.
+          returned: success
+          type: str
     sourceImageId:
       description:
       - The ID value of the image used to create this disk. This value identifies
@@ -218,6 +223,11 @@ items:
             key that protects this resource.
           returned: success
           type: str
+        kmsKeyName:
+          description:
+          - The name of the encryption key that is stored in Google Cloud KMS.
+          returned: success
+          type: str
     sourceSnapshot:
       description:
       - The source snapshot used to create this disk. You can provide this as a partial
@@ -235,6 +245,11 @@ items:
           description:
           - Specifies a 256-bit customer-supplied encryption key, encoded in RFC 4648
             base64 to either encrypt or decrypt this resource.
+          returned: success
+          type: str
+        kmsKeyName:
+          description:
+          - The name of the encryption key that is stored in Google Cloud KMS.
           returned: success
           type: str
         sha256:
