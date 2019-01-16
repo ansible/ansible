eca7c3c8c763db0da4bb56c17d17f0a5be0f56d8
Brian Coca <bcoca@users.noreply.github.com>
2019-01-16 13:39:03 -0500

    Prevent duplicate role insertion into roles: (#50552)
    * Corner case in which import_role would add another instance of a role with the same signature into roles: when it already existed there.
    
      roles:
    	- name: a
      tasks:
           - import_role: name=a
    
      would execute role 'a' 3 times instead of the intended 2 (x2 in roles: phase +1 in tasks:)
    
    * added tests

---
+++ test/integration/targets/roles/runme.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# test no dupes when dependencies in b and c point to a in roles:
+[ "$(ansible-playbook no_dupes.yml -i ../../inventory --tags inroles "$@" | grep -c '"msg": "A"')" = "1" ]
+[ "$(ansible-playbook no_dupes.yml -i ../../inventory --tags acrossroles "$@" | grep -c '"msg": "A"')" = "1" ]
+
+# but still dupe across plays
+[ "$(ansible-playbook no_dupes.yml -i ../../inventory "$@" | grep -c '"msg": "A"')" = "2" ]
+
+# include/import can execute another instance of role
+[ "$(ansible-playbook allowed_dupes.yml -i ../../inventory --tags importrole "$@" | grep -c '"msg": "A"')" = "2" ]
+[ "$(ansible-playbook allowed_dupes.yml -i ../../inventory --tags includerole "$@" | grep -c '"msg": "A"')" = "2" ]
