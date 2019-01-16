5e737032daab898cf0dfb6b75259cc2d0400b084
Jordan Borean <jborean93@gmail.com>
2019-01-17 06:05:27 +1000

    Do no instantiate the Sentinel object

--- lib/ansible/modules/files/file.py
+++ lib/ansible/modules/files/file.py
@@ -351,7 +351,7 @@ def get_timestamp_for_time(formatted_time, time_format):
     if formatted_time == 'preserve':
         return None
     elif formatted_time == 'now':
-        return Sentinel()
+        return Sentinel
     else:
         try:
             struct = time.strptime(formatted_time, time_format)
