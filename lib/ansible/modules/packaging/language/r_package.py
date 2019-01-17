b60ea47490da4af079ae3c1377f80b090db2957c
Evan Van Dam <evandam92@gmail.com>
2019-01-16 23:20:36 -0500

    revamp to install/update versions

--- lib/ansible/modules/packaging/language/r_package.py
+++ lib/ansible/modules/packaging/language/r_package.py
@@ -4,6 +4,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
+import re
 __metaclass__ = type
 
 ANSIBLE_METADATA = {
@@ -87,99 +88,97 @@ from numbers import Number
 from ansible.module_utils.basic import AnsibleModule
 
 
-def is_package_installed(module, exe, name, lib=None):
-    """Check if the package is installed.
-
-    :param module: The current ansible module
-    :param exe: The R executable to run
-    :param name: The R package to check
-    :param lib: The library directory to check. Optional.
-    :returns: `True` if the package was found, `False` otherwise.
-    """
-    # lib.loc isn't a valid Python variable,
-    # so pass kwargs as a dict and unpack it.
-    func = build_r_function('find.package', name, **{'lib.loc': lib})
-    retcode, out, err = run_command(module, exe, func, check_rc=False)
-    return retcode == 0
+R_OUTPUT_FORMAT = re.compile(r'\[\d+\] \'?(.*)\'?')
 
 
-def install_package(module, exe, name, src=None, pkg_type=None, lib=None, repo=None):
-    """Attempt to install the R package.
-
-    :param module: The current ansible module
-    :param exe: The R executable to run
-    :param name: The name of the R package to install
-    :param src: The source file/directory of the R package to install. Optional.
-    :param pkg_type: The package type. Ex: "source", "binary", "mac.binary". Optional.
-    :param lib: The library to use to install the package. Optional.
-    :param repo: The CRAN mirror/repo to use. Optional.
-    :return: The function string that was executed.
-    """
-    # Install the package from the repo to the lib.
-    install_name = src or name
-    func = build_r_function('install.packages', install_name, type=pkg_type, lib=lib, repos=repo)
-    run_command(module, exe, func, check_rc=True)
-    return func
+class RInstaller(object):
+    
+    def __init__(self, module, debug_info):
+        self.module = module
+        self.debug_info = debug_info
+        self.exe = self._get_R(module.params['executable'])
 
 
-def remove_package(module, exe, name, lib=None):
-    """Attempt to remove the R package.
+    def _get_R(self, executable):
+        r_cmd = None
+        if executable:
+            if os.path.isfile(executable):
+                r_cmd = executable
+            else:
+                self.module.fail_json(msg='%s is not a valid R executable' % executable)
+        else:
+            r_cmd = self.module.get_bin_path('R')
+            if not r_cmd:
+                self.module.fail_json(msg='R could not be found in the PATH and was not specified.')
+        return r_cmd
 
-    :param module: The current ansible module
-    :param exe: The R executable to run
-    :param name: The name of the R package to install
-    :param lib: The library to remove the package from. Optional.
-    :return: The output of the `remove.packages` R command.
-    """
-    # Remove the package from the specified lib or the default location.
-    func = build_r_function('remove.packages', name, lib=lib)
-    module.run_command(module, exe, func, lib=lib, check_rc=True)
-    return func
+    @staticmethod
+    def _parse_R_output(out):
+        return R_OUTPUT_FORMAT.match(out).group(1).strip()
 
+    def _run_R_function(self, funcname, *args, check_rc=False, **kwargs):
+        """Run an R function that might have positional and/or named args."""
+        # Clean up all args before passing to R
+        def _escape_arg(arg):
+            if isinstance(arg, Number):
+                return arg
+            elif arg in ('TRUE', 'FALSE'):
+                return arg
+            elif arg is None:
+                return 'NULL'
+            return '"{0}"'.format(arg)
 
-def _find_r(module):
-    """Locate R in the PATH, or verify the input exists"""
-    executable = module.params['executable']
-    if executable:
-        if os.path.isfile(executable):
-            return executable
-        module.fail_json(msg='{0} cannot be found or is not a file'.format(executable))
-    return module.get_bin_path('R', required=True)
+        r_args = [_escape_arg(a) for a in args]
+        r_args += [k + ' = ' + _escape_arg(v) for k, v in kwargs.items() if v is not None]
+        r_func = '{func}({args})'.format(func=funcname, args=', '.join(r_args))
+        return self._run_R_statement(r_func, check_rc=check_rc)
 
+    def _run_R_statement(self, statement, check_rc=False):
+        """Run a raw line of R code"""
+        cmd = [self.exe, '--slave', '--no-save', '--no-restore', '-e', statement]
+        return self.module.run_command(cmd, check_rc=check_rc) 
 
-def _escape_arg(arg):
-    """Strings must be quoted. Numbers are fine as is."""
-    if isinstance(arg, Number):
-        return arg
-    elif arg is None:
-        return 'NULL'
-    return '"{0}"'.format(arg)
+    def is_present(self, name, version=None):
+        """Return whether or not the package is installed.
+        
+        Optionally check for the correct version.
+        """
+        present = False
+        rc, out, err = self._run_R_function('packageVersion', name)
+        if rc == 0:
+            if version is not None:
+                installed_version = self._parse_R_output(out)
+                present = version == installed_version
+            present = True
+        self.debug_info['present'] = present
+        return present
 
+    def install_package(self, name, version):
+        """Install a new package."""
+        if version is not None:
+            self._run_R_function('devtools::install_version', name, version=version, check_rc=True)
+            self.debug_info['install'] = 'installed %s version %s' % (name, version)
+        else:
+            self._run_R_function('install.packages', name, check_rc=True)
+            self.debug_info['install'] = 'installed %s' % name
 
-def build_r_function(funcname, *args, **kwargs):
-    """Build an R function string to execute.
+    def is_old(self, name):
+        """Check if an update is available for the package."""
+        statement = '"{name}" %in% as.data.frame(old.packages())$Package'.format(name=name)
+        rc, out, err = self._run_R_statement(statement)
+        old = self._parse_R_output(out) == 'TRUE'
+        self.debug_info['old'] = old
+        return old
 
-    :param funcname: The name of the R function to call
-    :param args: Positional arguments to pass the R function
-    :param kwargs: Keyword arguments to pass to the R function
-    :return: A string of the R function with all parameters
-    """
-    r_args = [_escape_arg(a) for a in args]
-    r_args += [k + ' = ' + _escape_arg(v) for k, v in kwargs.items() if v is not None]
-    return funcname + '(' + ', '.join(r_args) + ')'
+    def update_package(self, name):
+        """Update to the latest version."""
+        self._run_R_function('update.packages', oldPkgs=name, ask='FALSE')
+        self.debug_info['update'] = 'Updated %s to latest version' % name
 
-
-def run_command(module, exe, func, check_rc=False):
-    """Build the R function and run the command.
-
-    :param module: The current ansible module
-    :param exe: The R executable to run
-    :param func: The full R function to run.
-    :param check_rc: module.fail_json() if the command fails.
-    :return: A tuple of the returncode, stdout, and stderr of the command.
-    """
-    cmd = [exe, '--slave', '--no-save', '--no-restore', '-e', func]
-    return module.run_command(cmd, check_rc=check_rc)
+    def remove_package(self, name):
+        """Uninstall a package."""
+        self._run_R_function('remove.packages', name)
+        self.debug_info['remove'] = 'Removed %s' % name
 
 
 def main():
@@ -187,38 +186,37 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             name=dict(required=True),
-            state=dict(default='present', choices=['present', 'absent']),
-            force=dict(default=False, type='bool'),
+            version=dict(required=False),
+            state=dict(default='present', choices=['present', 'latest', 'absent']),
             executable=dict(required=False),
-            src=dict(required=False),
-            type=dict(required=False),
             lib=dict(required=False),
-            repo=dict(required=False),
         ),
         supports_check_mode=True
     )
 
-    exe = _find_r(module)
     name = module.params['name']
-    src = module.params['src']
+    version = module.params['version']
     state = module.params['state']
-    pkg_type = module.params['type']
-    force = module.params['force']
-    lib = module.params['lib']
-    repo = module.params['repo']
 
-    installed = is_package_installed(module, exe, name, lib)
+    debug_info = {}
+    r_installer = RInstaller(module, debug_info)
+    is_present = r_installer.is_present(name, version)
+
     changed = False
-    cmd = ''
-    if state == 'absent' and installed:
-        if not module.check_mode:
-            cmd = remove_package(module, exe, name, lib=lib)
+    if state == 'present' and not is_present:
+        r_installer.install_package(name, version)
         changed = True
-    elif state == 'present' and (not installed or force):
-        if not module.check_mode:
-            cmd = install_package(module, exe, name, src, pkg_type, lib=lib, repo=repo)
+    if state == 'latest':
+        if not is_present:
+            r_installer.install_package(name, version)
+            changed = True
+        elif r_installer.is_old(name):
+            r_installer.update_package(name)
+            changed = True
+    elif state == 'absent' and is_present:
+        r_installer.remove_package(name)
         changed = True
-    module.exit_json(changed=changed, cmd=cmd)
+    module.exit_json(changed=changed, **debug_info)
 
 
 if __name__ == '__main__':
