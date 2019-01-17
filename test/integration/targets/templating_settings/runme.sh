961f0a0f0776b7bec21a8fea02759798ddb0f1fb
Brian Coca <brian.coca+git@gmail.com>
2019-01-17 13:54:57 -0500

    added test for templated register

--- test/integration/targets/templating_settings/runme.sh
+++ test/integration/targets/templating_settings/runme.sh
@@ -3,3 +3,4 @@
 set -eux
 
 ansible-playbook test_templating_settings.yml -i ../../inventory -v "$@"
+ansible-playbook warn_on_register.yml -i ../../inventory -v "$@"| grep 'is not templatable, but we found'
