dd8b30f3c037482e5d5ca9b442ddff315c4453df
Evan Van Dam <evandam92@gmail.com>
2019-01-20 18:49:55 -0500

    support list of names with version strings

--- lib/ansible/modules/packaging/language/r_package.py
+++ lib/ansible/modules/packaging/language/r_package.py
@@ -3,10 +3,6 @@
 # Copyright: (c) 2018, Evan Van Dam <evandam92@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import absolute_import, division, print_function
-import re
-__metaclass__ = type
-
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
     'status': ['preview'],
@@ -14,90 +10,112 @@ ANSIBLE_METADATA = {
 }
 
 DOCUMENTATION = '''
+---
 module: r_package
-short_description: A module to install and manage R packages
-description:
-  >
-    Install, remove, and update R packages.
-    Flexibility is provided to specify the R executable,
-    the remote repository, source to build, and library location.
+
+short_description: Manage R packages.
+
 version_added: "2.8"
-author: "Evan Van Dam (@evandam)"
+
+description:
+    - "Install, update, and remove R packages."
+
 options:
-  name:
-    description: The name of the targeted R package.
-    required: true
-  state:
-    description: The target state of the package.
-    choices: ["present", "absent"]
-    default: present
-  force:
-    description: Force the package to be installed, even if it already exists.
-    type: bool
-    default: false
-  executable:
-    description: Path to the R executable. If not specified, find it from the PATH.
-  src:
-    description: The source file or directory to build the R package.
-  type:
-    description: Type of package to install, like "source" or "binary".
-  lib:
-    description: The library location to use to install or remove the package.
-  repo:
-    description: The CRAN repo to use to install the package.
-notes:
-  - Requires R to already be installed.
-  - check mode is supported.
+    name:
+        description:
+            - The name of the R package to install.
+            - This can be a list to install multiple packages at once.
+            - Versions can be included in the name after an equal sign.
+        required: true
+    version:
+        description:
+            - The version of the R package to install.
+            - Versions specified in the name override this value.
+    state:
+        description:
+            - The state of the package
+        choices: [absent, latest, present]
+        default: present
+        required: false
+    executable:
+        description:
+            - The absoulte path to the R executable.
+            - By default, find R from PATH.
+    lib:
+        description:
+            - Library directories to install packages to,
+              as used in the install.packages function.
+    type:
+        description:
+            - The type of package to download and install,
+              as used in the install.packages function.
+    repos:
+        description:
+            - The base URL(s) of the repositories to use, such as CRAN mirrors.
+    extra_args:
+        description:
+            - Extra arguments passed to R functions:
+            - install.packages, install_version, or remove.packages
+
+requirements:
+    - R
+    - devtools (for specifying versions)
+
+author:
+    - Evan Van Dam (@evandam)
 '''
 
 EXAMPLES = '''
 - name: Install dplyr
   r_package:
     name: dplyr
-    state: present
-- name: Force dplyr to be reinstalled
+
+- name: Install dplyr 0.7.7
+  r_package:
+    name: dplyr=0.7.7
+    new: true
+
+- name: Install dplyr 0.7.7 with the version arg
   r_package:
     name: dplyr
-    force: true
-- name: Uninstall dplyr
+    version: 0.7.7
+
+- name: Install the latest version of multiple packages to a home directory
+  r_package:
+    name:
+      - dplyr
+      - stringr
+      - tidyr
+    state: latest
+    lib: ~/R-pkgs
+
+- name: Set Ncpus to install dependencies in parallel
   r_package:
     name: dplyr
-    state: absent
-- name: Install dplyr from the cloud CRAN mirror to your home lib
-  r_package:
-    name: dplyr
-    repo: https://cloud.r-project.org/
-    lib: "~/R"
-- name: Install h2o from source
-  r_package:
-    name: h2o
-    src: ~/Downloads/h2o.tar.gz
-    type: source
+    extra_args:
+        Ncpus: 4
 '''
 
 RETURN = '''
-cmd:
-  description: The R function executed by the module.
-  returned: changed
-  type: string
-  sample: install.packages("dplyr")
+packages:
+    description: List of packages that were modified.
+    returned: changed
+    type: list
+    sample: [stringr", "tidyr"]
 '''
 
 import os
+import re
 from numbers import Number
 from ansible.module_utils.basic import AnsibleModule
 
 
-R_OUTPUT_FORMAT = re.compile(r'\[\d+\] \'?(.*)\'?')
-
-
 class RInstaller(object):
-    
-    def __init__(self, module, debug_info):
-        self.module = module
-        self.debug_info = debug_info
-        self.exe = self._get_R(module.params['executable'])
+    R_OUTPUT_FORMAT = re.compile(r'\[\d+\] (\'?)(.*)\1')
 
+    def __init__(self, module):
+        self.module = module
+        self.exe = self._get_R(module.params['executable'])
 
     def _get_R(self, executable):
         r_cmd = None
@@ -114,14 +132,24 @@ class RInstaller(object):
 
     @staticmethod
     def _parse_R_output(out):
-        return R_OUTPUT_FORMAT.match(out).group(1).strip()
+        return RInstaller.R_OUTPUT_FORMAT.match(out).group(2).strip().strip('\'')
+
+    @staticmethod
+    def _parse_package_version(name, version=None):
+        if '=' in name:
+            return name.split('=')
+        else:
+            return (name, version)
 
     def _run_R_function(self, funcname, *args, check_rc=False, **kwargs):
         """Run an R function that might have positional and/or named args."""
         # Clean up all args before passing to R
         def _escape_arg(arg):
-            if isinstance(arg, Number):
-                return arg
+            if isinstance(arg, list):
+                safe_args = [_escape_arg(a) for a in arg]
+                return 'c({items})'.format(items=', '.join(safe_args))
+            elif isinstance(arg, Number):
+                return str(arg)
             elif arg in ('TRUE', 'FALSE'):
                 return arg
             elif arg is None:
@@ -131,92 +159,162 @@ class RInstaller(object):
         r_args = [_escape_arg(a) for a in args]
         r_args += [k + ' = ' + _escape_arg(v) for k, v in kwargs.items() if v is not None]
         r_func = '{func}({args})'.format(func=funcname, args=', '.join(r_args))
+
         return self._run_R_statement(r_func, check_rc=check_rc)
 
     def _run_R_statement(self, statement, check_rc=False):
         """Run a raw line of R code"""
-        cmd = [self.exe, '--slave', '--no-save', '--no-restore', '-e', statement]
-        return self.module.run_command(cmd, check_rc=check_rc) 
+        # Fancy quotes make parsing a nightmare.
+        disable_fancy_quotes = 'options(useFancyQuotes = FALSE)'
+        cmd = [self.exe, '--slave', '--no-save', '--no-restore', '-e',
+               '; '.join([disable_fancy_quotes, statement])]
+        print('Running: %s' % cmd)
+        return self.module.run_command(cmd, check_rc=check_rc)
 
-    def is_present(self, name, version=None):
+    def is_present(self, name, version=None, lib=None):
         """Return whether or not the package is installed.
         
         Optionally check for the correct version.
         """
         present = False
-        rc, out, err = self._run_R_function('packageVersion', name)
+        rc, out, err = self._run_R_function('packageVersion', name, **{'lib.loc': lib})
         if rc == 0:
             if version is not None:
                 installed_version = self._parse_R_output(out)
                 present = version == installed_version
-            present = True
-        self.debug_info['present'] = present
+            else:
+                present = True
         return present
 
-    def install_package(self, name, version):
-        """Install a new package."""
-        if version is not None:
-            self._run_R_function('devtools::install_version', name, version=version, check_rc=True)
-            self.debug_info['install'] = 'installed %s version %s' % (name, version)
-        else:
-            self._run_R_function('install.packages', name, check_rc=True)
-            self.debug_info['install'] = 'installed %s' % name
-
-    def is_old(self, name):
+    def is_old(self, name, lib=None):
         """Check if an update is available for the package."""
-        statement = '"{name}" %in% as.data.frame(old.packages())$Package'.format(name=name)
+        statement = ('"{name}" %in% as.data.frame(old.packages(lib.loc="{lib}"))$Package'
+                     .format(name=name, lib=lib))
         rc, out, err = self._run_R_statement(statement)
         old = self._parse_R_output(out) == 'TRUE'
-        self.debug_info['old'] = old
         return old
 
-    def update_package(self, name):
+    def install_packages(self, names, versions=None, lib=None,
+                         install_type=None, repos=None, **kwargs):
+        """Install a new package."""
+        # Check if we can lump all packages into one command when no versions are specified
+        if versions is None:
+           versions = [None for _ in names]
+
+        names_and_versions = list(zip(names, versions))
+        specific_versions = [(n, v) for n, v in names_and_versions if v]
+        no_versions = [n for n, v in names_and_versions if not v]
+
+        # Install all unversioned packages in batch
+        if no_versions:
+            print('Installing packages in one batch: %s' % no_versions)
+            self._run_R_function('install.packages', no_versions, check_rc=True, 
+                                 lib=lib, type=install_type, repos=repos, **kwargs)
+        
+        # Install each package one at a time, specifying versions
+        if specific_versions:
+            for name, version in specific_versions:
+                print('Installing %s=%s' % (name, version))
+                self._run_R_function('devtools::install_version', name, check_rc=True, lib=lib,
+                                     version=version, type=install_type, repos=repos, **kwargs)
+
+    def update_packages(self, name, lib=None):
         """Update to the latest version."""
-        self._run_R_function('update.packages', oldPkgs=name, ask='FALSE')
-        self.debug_info['update'] = 'Updated %s to latest version' % name
+        print('Updating packages %s' % name)
+        self._run_R_function('update.packages', oldPkgs=name, ask='FALSE', **{'lib.loc': lib})
 
-    def remove_package(self, name):
+    def remove_packages(self, name, lib=None, **kwargs):
         """Uninstall a package."""
-        self._run_R_function('remove.packages', name)
-        self.debug_info['remove'] = 'Removed %s' % name
+        print('Removing packages %s' % name)
+        self._run_R_function('remove.packages', name, lib=lib, **kwargs)
 
 
-def main():
-    """Run the Ansible module"""
-    module = AnsibleModule(
-        argument_spec=dict(
-            name=dict(required=True),
-            version=dict(required=False),
-            state=dict(default='present', choices=['present', 'latest', 'absent']),
-            executable=dict(required=False),
-            lib=dict(required=False),
-        ),
-        supports_check_mode=True
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    install_types = ['source', 'mac.binary', 'mac.binary.el-capitan',
+                     'win.binary', 'binary', 'both']
+    module_args = dict(
+        name=dict(required=True, type='list'),
+        version=dict(required=False),
+        state=dict(default='present', choices=['present', 'latest', 'absent']),
+        executable=dict(required=False),
+        lib=dict(required=False, type='list'),
+        type=dict(required=False, choices=install_types),
+        repos=dict(required=False, type='list'),
+        extra_args=dict(required=False, type='dict', default={})
     )
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # change is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )  
 
     name = module.params['name']
     version = module.params['version']
     state = module.params['state']
+    lib = module.params['lib']
+    install_type = module.params['type']
+    repos = module.params['repos']
+    extra_args = module.params['extra_args']
 
-    debug_info = {}
-    r_installer = RInstaller(module, debug_info)
-    is_present = r_installer.is_present(name, version)
+    r_installer = RInstaller(module)
+    packages = [r_installer._parse_package_version(n, version) for n in name]
 
-    changed = False
-    if state == 'present' and not is_present:
-        r_installer.install_package(name, version)
-        changed = True
-    if state == 'latest':
-        if not is_present:
-            r_installer.install_package(name, version)
-            changed = True
-        elif r_installer.is_old(name):
-            r_installer.update_package(name)
-            changed = True
-    elif state == 'absent' and is_present:
-        r_installer.remove_package(name)
-        changed = True
-    module.exit_json(changed=changed, **debug_info)
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        return result
+
+    # use whatever logic you need to determine whether or not this module
+    # made any modifications to your target
+    if state == 'present':
+        absent_packages = [x for x in packages if not r_installer.is_present(*x, lib=lib)]
+        if absent_packages:
+            if not module.check_mode:
+                packages, versions = zip(*absent_packages)
+                r_installer.install_packages(packages, versions, lib=lib, repos=repos,
+                                             install_type=install_type, **extra_args)
+            result['packages'] = absent_packages
+            result['changed'] = True
+    elif state == 'latest':
+        to_install = [n for n, v in packages
+                      if not r_installer.is_present(n, v, lib=lib)
+                      or r_installer.is_old(n, lib=lib)]
+        if to_install:
+            if not module.check_mode:
+                r_installer.install_packages(to_install, None, lib=lib, repos=repos,
+                                             install_type=install_type, **extra_args)
+            result['packages'] = to_install
+            result['changed'] = True
+    elif state == 'absent':
+        to_remove = [n for n, v in packages if r_installer.is_present(n, lib=lib)]
+        if to_remove:
+            if not module.check_mode:
+                r_installer.remove_packages(to_remove, lib=lib, **extra_args)
+            result['packages'] = to_remove
+            result['changed'] = True
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
 
 
 if __name__ == '__main__':
