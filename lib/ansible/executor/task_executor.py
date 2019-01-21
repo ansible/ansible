20b06b28b73f6ea884455fe2e1cea28ce7f28cf7
s-hertel <shertel@redhat.com>
2019-01-21 15:54:30 -0500

    Fix overwriting rather than updating module_defaults

--- lib/ansible/executor/task_executor.py
+++ lib/ansible/executor/task_executor.py
@@ -598,7 +598,11 @@ class TaskExecutor:
         # These are collected as a list of dicts, so we need to merge them
         module_defaults = {}
         for default in self._task.module_defaults:
-            module_defaults.update(default)
+            for group in default:
+                if group in module_defaults:
+                    module_defaults[group].update(default[group])
+                else:
+                    module_defaults[group] = default[group]
         if module_defaults:
             module_defaults = templar.template(module_defaults)
         if self._task.action in module_defaults:
