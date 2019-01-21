17aa3c847e152ceab4a891bea609169e643a29ac
NilashishC <nilashishchakraborty8@gmail.com>
2019-01-21 12:08:52 +0000

    Added cliconf plugin for FRR
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- lib/ansible/plugins/cliconf/frr.py
+++ lib/ansible/plugins/cliconf/frr.py
@@ -90,3 +90,18 @@ class Cliconf(CliconfBase):
             raise ValueError("'output' value %s is not supported for get" % output)
 
         return self.send_command(command=command, prompt=prompt, answer=answer, sendonly=sendonly, check_all=check_all)
+
+    def get_device_operations(self):
+        return {
+            'supports_diff_replace': True,
+            'supports_commit': False,
+            'supports_rollback': False,
+            'supports_defaults': True,
+            'supports_onbox_diff': False,
+            'supports_commit_comment': False,
+            'supports_multiline_delimiter': True,
+            'supports_diff_match': True,
+            'supports_diff_ignore_lines': True,
+            'supports_generate_diff': True,
+            'supports_replace': False
+        }
