0141dcbbcbf5229923338dbd89faf42544413b96
Andrey Tuzhilin <andrey@3adigital.ru>
2019-01-21 12:43:47 +0300

    feat: added scram stored passwords check for modification

--- lib/ansible/modules/database/postgresql/postgresql_user.py
+++ lib/ansible/modules/database/postgresql/postgresql_user.py
@@ -238,7 +238,9 @@ queries:
 import itertools
 import re
 import traceback
-from hashlib import md5
+from hashlib import md5, pbkdf2_hmac, sha256
+import hmac
+from base64 import b64decode, b64encode
 
 try:
     import psycopg2
@@ -273,6 +275,8 @@ PRIV_TO_AUTHID_COLUMN = dict(SUPERUSER='rolsuper', CREATEROLE='rolcreaterole',
                              CREATEDB='rolcreatedb', INHERIT='rolinherit', LOGIN='rolcanlogin',
                              REPLICATION='rolreplication', BYPASSRLS='rolbypassrls')
 
+SCRAM_SHA256_REGEX = r'^SCRAM-SHA-256\$(\d+):([A-Za-z0-9+\/=]+)\$([A-Za-z0-9+\/=]+):([A-Za-z0-9+\/=]+)$'
+
 executed_queries = []
 
 
@@ -282,6 +286,110 @@ class InvalidFlagsError(Exception):
 
 class InvalidPrivsError(Exception):
     pass
+
+
+class NoSaslPrepError(Exception):
+    pass
+
+# ===========================================
+# Helper utility functions
+#
+
+
+"""An implementation of RFC4013 SASLprep.
+This code is mostly (apart from utils methods) a copy of
+https://github.com/mongodb/mongo-python-driver/blob/master/pymongo/saslprep.py
+"""
+
+import sys
+
+try:
+    import stringprep
+except ImportError:
+    HAVE_STRINGPREP = False
+
+    def saslprep(data):
+        """SASLprep dummy"""
+        if isinstance(data, text_type):
+            raise NoSaslPrepError("The stringprep module is not available.")
+        return data
+else:
+    HAVE_STRINGPREP = True
+    import unicodedata
+    # RFC4013 section 2.3 prohibited output.
+    _PROHIBITED = (
+        # A strict reading of RFC 4013 requires table c12 here, but
+        # characters from it are mapped to SPACE in the Map step. Can
+        # normalization reintroduce them somehow?
+        stringprep.in_table_c12,
+        stringprep.in_table_c21_c22,
+        stringprep.in_table_c3,
+        stringprep.in_table_c4,
+        stringprep.in_table_c5,
+        stringprep.in_table_c6,
+        stringprep.in_table_c7,
+        stringprep.in_table_c8,
+        stringprep.in_table_c9)
+
+    def saslprep(data, prohibit_unassigned_code_points=True):
+        """An implementation of RFC4013 SASLprep.
+        :Parameters:
+          - `data`: The string to SASLprep. Unicode strings
+            (python 2.x unicode, 3.x str) are supported. Byte strings
+            (python 2.x str, 3.x bytes) are ignored.
+          - `prohibit_unassigned_code_points`: True / False. RFC 3454
+            and RFCs for various SASL mechanisms distinguish between
+            `queries` (unassigned code points allowed) and
+            `stored strings` (unassigned code points prohibited). Defaults
+            to ``True`` (unassigned code points are prohibited).
+        :Returns:
+        The SASLprep'ed version of `data`.
+        """
+        if not isinstance(data, text_type):
+            return data
+
+        if prohibit_unassigned_code_points:
+            prohibited = _PROHIBITED + (stringprep.in_table_a1,)
+        else:
+            prohibited = _PROHIBITED
+
+        # RFC3454 section 2, step 1 - Map
+        # RFC4013 section 2.1 mappings
+        # Map Non-ASCII space characters to SPACE (U+0020). Map
+        # commonly mapped to nothing characters to, well, nothing.
+        in_table_c12 = stringprep.in_table_c12
+        in_table_b1 = stringprep.in_table_b1
+        data = u"".join(
+            [u"\u0020" if in_table_c12(elt) else elt
+             for elt in data if not in_table_b1(elt)])
+
+        # RFC3454 section 2, step 2 - Normalize
+        # RFC4013 section 2.2 normalization
+        data = unicodedata.ucd_3_2_0.normalize('NFKC', data)
+
+        in_table_d1 = stringprep.in_table_d1
+        if in_table_d1(data[0]):
+            if not in_table_d1(data[-1]):
+                # RFC3454, Section 6, #3. If a string contains any
+                # RandALCat character, the first and last characters
+                # MUST be RandALCat characters.
+                raise ValueError("SASLprep: failed bidirectional check")
+            # RFC3454, Section 6, #2. If a string contains any RandALCat
+            # character, it MUST NOT contain any LCat character.
+            prohibited = prohibited + (stringprep.in_table_d2,)
+        else:
+            # RFC3454, Section 6, #3. Following the logic of #3, if
+            # the first character is not a RandALCat, no other character
+            # can be either.
+            prohibited = prohibited + (in_table_d1,)
+
+        # RFC3454 section 2, step 3 and 4 - Prohibit and check bidi
+        for char in data:
+            if any(in_table(char) for in_table in prohibited):
+                raise ValueError(
+                    "SASLprep: failed prohibited character check")
+
+        return data
 
 # ===========================================
 # PostgreSQL module specific support methods.
@@ -338,6 +446,36 @@ def user_should_we_change_password(current_role_attrs, user, password, encrypted
         # means we need to check if the current password is None.
         if password == '':
             if current_role_attrs['rolpassword'] is not None:
+                pwchanging = True
+        # SCRAM hashes are represented as a special object, containing hash data:
+        # `SCRAM-SHA-256$<iteration count>:<salt>$<StoredKey>:<ServerKey>`
+        # for reference, see https://www.postgresql.org/docs/10/catalog-pg-authid.html
+        elif current_role_attrs['rolpassword'] is not None \
+                and re.match(SCRAM_SHA256_REGEX, current_role_attrs['rolpassword']):
+
+            r = re.match(SCRAM_SHA256_REGEX, current_role_attrs['rolpassword'])
+            try:
+                # extract SCRAM params from rolpassword
+                it = int(r.group(1))
+                salt = b64decode(r.group(2))
+                serverKey = b64decode(r.group(4))
+                # we'll never need this, used only for server auth in SCRAM
+                # storedKey = b64decode(r.group(3))
+
+                # from RFC5802 https://tools.ietf.org/html/rfc5802#section-3
+                # SaltedPassword  := Hi(Normalize(password), salt, i)
+                # ServerKey       := HMAC(SaltedPassword, "Server Key")
+                normalizedPassword = saslprep(to_text(password))
+                saltedPassword = pbkdf2_hmac('sha256', to_bytes(normalizedPassword), salt, it)
+
+                serverKeyVerifier = hmac.new(saltedPassword, digestmod=sha256)
+                serverKeyVerifier.update(b'Server Key')
+
+                if serverKeyVerifier.digest() != serverKey:
+                    pwchanging = True
+            except:
+                # We assume the password is not scram encrypted
+                # or we cannot check it properly
                 pwchanging = True
         # 32: MD5 hashes are represented as a sequence of 32 hexadecimal digits
         #  3: The size of the 'md5' prefix
