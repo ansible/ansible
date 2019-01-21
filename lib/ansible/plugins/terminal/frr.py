8d59b77f9184569d1200c17f4918b898c903e289
NilashishC <nilashishchakraborty8@gmail.com>
2019-01-21 09:34:41 +0000

    Added terminal plugin for FRR
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

---
+++ lib/ansible/plugins/terminal/frr.py
@@ -0,0 +1,75 @@
+#
+# (c) 2016 Red Hat Inc.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import json
+import re
+
+from ansible.errors import AnsibleConnectionFailure
+from ansible.module_utils._text import to_text, to_bytes
+from ansible.plugins.terminal import TerminalBase
+from ansible.utils.display import Display
+
+display = Display()
+
+
+class TerminalModule(TerminalBase):
+
+    terminal_stdout_re = [
+        re.compile(br"[\r\n]?[\w\+\-\.:\/\[\]]+(?:\([^\)]+\)){0,3}(?:[>#]) ?$")
+    ]
+
+    terminal_stderr_re = [
+        re.compile(br"% Command incomplete", re.I),
+        re.compile(br"% Unknown command", re.I),
+        re.compile(br"% (?:\S+) instance is already running", re.I),
+        re.compile(br"% (?:Create|Specify) .* first", re.I),
+        re.compile(br"% (?:\S+) is not running", re.I),
+        re.compile(br"% Can't find .*", re.I),
+        re.compile(br"invalid input", re.I),
+        re.compile(br"connection timed out", re.I),
+        re.compile(br"[^\r\n]+ not found"),
+    ]
+
+    def on_open_shell(self):
+        try:
+            self._exec_cli_command(b'terminal length 0')
+        except AnsibleConnectionFailure:
+            raise AnsibleConnectionFailure('unable to set terminal parameters')
+
+    def on_become(self, passwd=None):
+        if self._get_prompt().endswith(b'#'):
+            return
+
+        # TO-DO: FRR 6.0 not throwing enable prompt
+
+    def on_unbecome(self):
+        # NOTE: FRR 6.0 not throwing enable prompt
+        prompt = self._get_prompt()
+        if prompt is None:
+            # if prompt is None most likely the terminal is hung up at a prompt
+            return
+
+        if b'(config' in prompt:
+            self._exec_cli_command(b'end')
+            self._exec_cli_command(b'disable')
+
+        elif prompt.endswith(b'#'):
+            self._exec_cli_command(b'disable')
