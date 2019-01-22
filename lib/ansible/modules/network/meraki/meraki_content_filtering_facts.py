99984a7fc2e0c9ee05bdfe12961a94b647caa988
Kevin Breit <kevin.breit@kevinbreit.net>
2019-01-22 14:19:24 -0600

    Refine tests and get code to work - Code now works properly - Responses are via ansible_facts data structure

--- lib/ansible/modules/network/meraki/meraki_content_filtering_facts.py
+++ lib/ansible/modules/network/meraki/meraki_content_filtering_facts.py
@@ -25,11 +25,6 @@ options:
     auth_key:
         description:
         - Authentication key provided by the dashboard. Required if environmental variable MERAKI_KEY is not set.
-    state:
-        description:
-        - Create or modify an organization.
-        choices: [absent, present, query]
-        default: present
     net_name:
         description:
         - Name of a network.
@@ -43,25 +38,10 @@ options:
     org_id:
         description:
         - ID of organization associated to a network.
-    type:
+    subset:
         description:
-        - Type of network device network manages.
-        - Required when creating a network.
-        choices: [appliance, combined, switch, wireless]
-        aliases: [net_type]
-    tags:
-        description:
-        - Comma delimited list of tags to assign to network.
-    timezone:
-        description:
-        - Timezone associated to network.
-        - See U(https://en.wikipedia.org/wiki/List_of_tz_database_time_zones) for a list of valid timezones.
-    disable_my_meraki:
-        description: >
-            - Disables the local device status pages (U[my.meraki.com](my.meraki.com), U[ap.meraki.com](ap.meraki.com), U[switch.meraki.com](switch.meraki.com),
-            U[wired.meraki.com](wired.meraki.com))
-        type: bool
-        version_added: '2.7'
+        - Display only certain facts
+        choices: [categories, policy]
 
 author:
     - Kevin Breit (@kbreit)
@@ -69,28 +49,27 @@ extends_documentation_fragment: meraki
 '''
 
 EXAMPLES = r'''
-- name: List all networks associated to the YourOrg organization
-  meraki_network:
+- name: List all content filtering information
+  meraki_content_filtering_facts:
     auth_key: abc12345
-    state: query
     org_name: YourOrg
+    net_name: YourNet
   delegate_to: localhost
-- name: Query network named MyNet in the YourOrg organization
-  meraki_network:
+
+- name: List content filtering policy information
+  meraki_content_filtering_facts:
     auth_key: abc12345
-    state: query
     org_name: YourOrg
-    net_name: MyNet
+    net_name: YourNet
+    subset: policy
   delegate_to: localhost
-- name: Create network named MyNet in the YourOrg organization
-  meraki_network:
+
+- name: List all content filtering categories
+  meraki_content_filtering_facts:
     auth_key: abc12345
-    state: present
     org_name: YourOrg
-    net_name: MyNet
-    type: switch
-    timezone: America/Chicago
-    tags: production, chicago
+    net_name: YourNet
+    subset: categories
   delegate_to: localhost
 '''
 
@@ -143,11 +122,6 @@ def main():
     meraki.url_catalog['categories'] = category_urls
     meraki.url_catalog['policy'] = policy_urls
 
-    if not meraki.params['org_name'] and not meraki.params['org_id']:
-        meraki.fail_json(msg='org_name or org_id parameters are required')
-    if meraki.params['state'] != 'query':
-        if not meraki.params['net_name'] or meraki.params['net_id']:
-            meraki.fail_json(msg='net_name or net_id is required for present or absent states')
     if meraki.params['net_name'] and meraki.params['net_id']:
         meraki.fail_json(msg='net_name and net_id are mutually exclusive')
 
@@ -167,33 +141,29 @@ def main():
 
     # check if network is created
     net_id = None
-    if meraki.params['net_name']:
-        if is_net_valid(meraki, meraki.params['net_name'], nets) is True:
-            net_id = meraki.get_net_id(net_name=meraki.params['net_name'], data=nets)
-    elif meraki.params['net_id']:
-        net_id = meraki.params['net_id'] 
+    if net_id is None:
+        nets = meraki.get_nets(org_id=org_id)
+        net_id = meraki.get_net_id(org_id, meraki.params['net_name'], data=nets)
 
-    if meraki.params['state'] == 'query':
-        if meraki.params['subset']:
-            if meraki.params['subset'] == 'categories':
-                path = meraki.construct_path('categories', net_id=net_id)
-                meraki.response['data'] = meraki.request(path, method='GET')
-            elif meraki.params['policy'] == 'policy':
-                path = meraki.construct_path('policy', net_id=net_id)
-                meraki.response['data'] = meraki.request(path, method='GET')                
-        else:
-            response_data = {'categories': None,
-                             'policy': None,
-                             }
+    if meraki.params['subset']:
+        if meraki.params['subset'] == 'categories':
             path = meraki.construct_path('categories', net_id=net_id)
-            response_data['categories'] = meraki.request(path, method='GET')
+            meraki.exit_json(ansible_facts=meraki.request(path, method='GET'))
+        elif meraki.params['subset'] == 'policy':
             path = meraki.construct_path('policy', net_id=net_id)
-            response_data['policy'] = meraki.request(path, method='GET')
-            meraki.response['data'] = response_data
+            meraki.exit_json(ansible_facts=meraki.request(path, method='GET'))
+    else:
+        response_data = {'categories': None,
+                         'policy': None,
+                         }
+        path = meraki.construct_path('categories', net_id=net_id)
+        response_data['categories'] = meraki.request(path, method='GET')
+        path = meraki.construct_path('policy', net_id=net_id)
+        response_data['policy'] = meraki.request(path, method='GET')
 
     # in the event of a successful module execution, you will want to
     # simple AnsibleModule.exit_json(), passing the key/value results
-    meraki.exit_json(**meraki.result)
+    meraki.exit_json(ansible_facts=response_data)
 
 
 if __name__ == '__main__':
