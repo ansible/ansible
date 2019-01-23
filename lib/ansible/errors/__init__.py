0088b8c908d8522a344fd9b11262c2541f5eeea0
Sam Doran <sdoran@redhat.com>
2019-01-23 11:32:25 -0500

    Catch sshpass authentication errors and don't retry multiple times to prevent account lockout (#50776)
    * Catch SSH authentication errors and don't retry multiple times to prevent account lock out
    
    Signed-off-by: Sam Doran <sdoran@redhat.com>
    
    * Subclass AnsibleAuthenticationFailure from AnsibleConnectionFailure
    
    Use comparison rather than range() because it's much more efficient.
    
    Signed-off-by: Sam Doran <sdoran@redhat.com>
    
    * Add tests
    
    Signed-off-by: Sam Doran <sdoran@redhat.com>
    
    * Make paramiko_ssh connection plugin behave the same way
    
    Signed-off-by: Sam Doran <sdoran@redhat.com>
    
    * Add changelog
    
    Signed-off-by: Sam Doran <sdoran@redhat.com>

--- lib/ansible/errors/__init__.py
+++ lib/ansible/errors/__init__.py
@@ -219,6 +219,11 @@ class AnsibleConnectionFailure(AnsibleRuntimeError):
     pass
 
 
+class AnsibleAuthenticationFailure(AnsibleConnectionFailure):
+    '''invalid username/password/key'''
+    pass
+
+
 class AnsibleCallbackError(AnsibleRuntimeError):
     ''' a callback failure '''
     pass
