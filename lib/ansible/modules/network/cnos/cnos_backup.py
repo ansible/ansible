f759b5463b5814a07235dbed3c0c760b938968a3
Matt Davis <mrd@redhat.com>
2019-01-23 17:45:00 -0800

    Backport #47219 fixes Python 3.7 bugs
    * Resolve invalid-unary-operand-type.
    
    * Resolve stop-iteration-return.
    
    (cherry-picked from c24c19594e4c4cddfda3226d49f8eeabacb18162)

--- lib/ansible/modules/network/cnos/cnos_backup.py
+++ lib/ansible/modules/network/cnos/cnos_backup.py
@@ -213,7 +213,7 @@ def doConfigBackUp(module, prompt, answer):
     elif(protocol == "tftp"):
         command = "copy " + configType + " " + protocol + " " + protocol
         command = command + "://" + server + "/" + confPath
-        command = command + + " vrf management\n"
+        command = command + " vrf management\n"
         # cnos.debugOutput(command)
         tftp_cmd = [{'command': command, 'prompt': None, 'answer': None}]
         cmd.extend(tftp_cmd)
