28b6a9c304a09b6eb8e2363d06c3aca9b62a6666
zikalino <zikalino@microsoft.com>
2019-01-24 17:21:33 +0800

    fixed line too long

--- lib/ansible/modules/cloud/azure/azure_rm_virtualmachinscaleseteextension_facts.py
+++ lib/ansible/modules/cloud/azure/azure_rm_virtualmachinscaleseteextension_facts.py
@@ -69,7 +69,8 @@ extensions:
                 - Resource Id
             returned: always
             type: str
-            sample: /subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/myrg/providers/Microsoft.Compute/virtualMachineScaleSets/myvmss/extensions/myextension
+            sample: "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/myrg/providers/Microsoft.Compute/virtualMachineScaleSets/
+                     myvmss/extensions/myextension"
         resource_group:
             description:
                 - Resource group name
