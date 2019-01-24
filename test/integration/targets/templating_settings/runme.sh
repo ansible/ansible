732a6ae25f483cb52f0485a3723b2502782d9093
Brian Coca <bcoca@users.noreply.github.com>
2019-01-24 11:51:52 -0500

    add static facility and apply to register (#49737)
    * add static facility and apply to register
    
    * added warning
    
    * added test for templated register
    
    * test register 'static' status
    
    * rely on subshell to deal with quote context
    
    * use corrects pb for test
    
    * bring constants back cause new code in devel

--- test/integration/targets/templating_settings/runme.sh
+++ test/integration/targets/templating_settings/runme.sh
@@ -3,3 +3,5 @@
 set -eux
 
 ansible-playbook test_templating_settings.yml -i ../../inventory -v "$@"
+ansible-playbook warn_on_register.yml -i ../../inventory -v "$@" 2>&1| grep 'is not templatable, but we found'
+[ "$(ansible-playbook dont_warn_register.yml -i ../../inventory -v "$@" 2>&1| grep -c 'is not templatable, but we found')" == "0" ]
