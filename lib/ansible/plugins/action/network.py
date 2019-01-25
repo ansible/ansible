ab951f4842188a7d7157f60b93b04c806839e07d
NilashishC <nilashishchakraborty8@gmail.com>
2019-01-25 11:23:09 +0530

    Add filename rv
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- lib/ansible/plugins/action/network.py
+++ lib/ansible/plugins/action/network.py
@@ -114,6 +114,7 @@ class ActionModule(_ActionModule):
             result['date'] = tstamp.split('@')[0]
             result['time'] = tstamp.split('@')[1]
             result['shortname'] = result['backup_path'][::-1].split('.', 1)[1][::-1]
+            result['filename'] = result['backup_path'].split('/')[-1]
 
         else:
             result['date'] = time.strftime('%Y-%m-%d', time.gmtime(os.stat(result['backup_path']).st_ctime))
