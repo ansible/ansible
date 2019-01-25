705156450ac769d33b6a5303ff513f198b8a7ddb
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2019-01-25 14:23:29 +0530

    Update cnos.py

--- lib/ansible/plugins/cliconf/cnos.py
+++ lib/ansible/plugins/cliconf/cnos.py
@@ -20,7 +20,7 @@ import re
 import json
 
 from itertools import chain
-
+from ansible.module_utils.common._collections_compat import Mapping
 from ansible.module_utils._text import to_bytes, to_text
 from ansible.module_utils.network.common.utils import to_list
 from ansible.plugins.cliconf import CliconfBase, enable_mode
