914151629308cf080714acc27dc06ebdcdffca15
WojciechowskiPiotr <devel@it-playground.pl>
2019-01-26 23:36:59 +0100

    * docker_host_facts: Getting lists of containers, volumes, images and networks as in respective CLI commands

--- lib/ansible/modules/cloud/docker/docker_host_facts.py
+++ lib/ansible/modules/cloud/docker/docker_host_facts.py
@@ -29,6 +29,9 @@ description:
 version_added: "2.8"
 
 options:
+    services:
+        description: List of services for which list of items should be listed in output
+
 extends_documentation_fragment:
     - docker
 
@@ -90,18 +93,80 @@ class DockerHostManager(DockerBaseClass):
         self.client = client
         self.results = results
 
+        listed_objects = ['volumes', 'networks', 'containers', 'images']
+
         self.results['docker_host_facts'] = self.get_docker_host_facts()
+
+        for docker_object in listed_objects:
+            if self.client.module.params[docker_object] is True:
+                returned_name = "docker_" + docker_object + "_list"
+                self.results[returned_name] = self.get_docker_items_list(docker_object)
 
     def get_docker_host_facts(self):
         try:
             return self.client.info()
-        except Exception as exc:
-            self.client.fail_json(msg="Error inspecting docker host: %s" % exc)
+        except APIError as exc:
+            self.client.fail_json(msg="Error inspecting docker host: %s" % to_native(exc))
+
+    def get_docker_items_list(self, object=None):
+        items = None
+        items_list = []
+
+        header_containers = ['Id', 'Image', 'Command', 'Created', 'Status', 'Ports', 'Names']
+        header_volumes = ['Driver', 'Name']
+        header_images = ['Id', 'RepoTags', 'Created', 'Size']
+        header_networks = ['Id', 'Driver', 'Name', 'Scope']
+
+        try:
+            if object == 'containers':
+                items = self.client.containers()
+            if object == 'networks':
+                items = self.client.networks()
+            if object == 'images':
+                items = self.client.images()
+            if object == 'volumes':
+                items = self.client.volumes()
+        except APIError as exc:
+            self.client.fail_json(msg="Error inspecting docker host: %s" % to_native(exc))
+
+        if object != 'volumes':
+            for item in items:
+                item_record = dict()
+
+                if object == 'containers':
+                    for key in header_containers:
+                        item_record[key] = item.get(key)
+                if object == 'networks':
+                    for key in header_networks:
+                        item_record[key] = item.get(key)
+                if object == 'images':
+                    for key in header_images:
+                        item_record[key] = item.get(key)
+
+                items_list.append(item_record)
+
+        else:
+            for item in items['Volumes']:
+                item_record = dict()
+
+                for key in header_volumes:
+                    item_record[key] = item.get(key)
+
+                items_list.append(item_record)
+
+        return items_list
 
 
 def main():
     argument_spec = dict(
-        services=dict(type='list', elements='str'),
+        containers=dict(type='bool', default=False),
+        containers_filters=dict(type='dict'),
+        images=dict(type='bool', default=False),
+        images_filters=dict(type='dict'),
+        networks=dict(type='bool', default=False),
+        networks_filters=dict(type='dict'),
+        volumes=dict(type='bool', default=False),
+        volumes_filters=dict(type='dict'),
     )
 
     client = AnsibleDockerClient(
