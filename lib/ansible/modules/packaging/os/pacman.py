ce11f796a24b6e231bc3960141abddcec86b8c8c
tchernomax <maxime.deroucy@gmail.com>
2019-01-26 18:14:39 +0000

    Update lib/ansible/modules/packaging/os/pacman.py
    Co-Authored-By: greyltc <grey@christoforo.net>

--- lib/ansible/modules/packaging/os/pacman.py
+++ lib/ansible/modules/packaging/os/pacman.py
@@ -180,7 +180,7 @@ def get_name(pacman_output):
     """Take pacman -Qi or pacman -Si output and get the package name"""
     lines = pacman_output.split('\n')
     for line in lines:
-        if 'Name' in line:
+        if line.startswith('Name '):
             return line.split(':')[1].strip()
     module.fail_json(msg="get_name: fail to retrieve package name from pacman output")
 
