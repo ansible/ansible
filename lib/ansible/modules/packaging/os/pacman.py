d51b16a781ab466d46fbd30e1e555f2c94729b3c
Grey Christoforo <grey@christoforo.net>
2019-01-26 18:18:17 +0000

    make pcman package version parsing more robust

--- lib/ansible/modules/packaging/os/pacman.py
+++ lib/ansible/modules/packaging/os/pacman.py
@@ -171,7 +171,7 @@ def get_version(pacman_output):
     """Take pacman -Qi or pacman -Si output and get the Version"""
     lines = pacman_output.split('\n')
     for line in lines:
-        if 'Version' in line:
+        if line.startswith('Version '):
             return line.split(':')[1].strip()
     return None
 
