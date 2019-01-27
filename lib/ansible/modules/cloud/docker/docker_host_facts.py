ff828226eea2d363bd31e74466fae866bf893fa5
WojciechowskiPiotr <devel@it-playground.pl>
2019-01-27 11:44:06 +0100

    * docker_host_facts: Adding filters, documentation update

--- lib/ansible/modules/cloud/docker/docker_host_facts.py
+++ lib/ansible/modules/cloud/docker/docker_host_facts.py
@@ -29,8 +29,58 @@ description:
 version_added: "2.8"
 
 options:
-    services:
-        description: List of services for which list of items should be listed in output
+  containers:
+    description:
+      - Whether to list containers.
+    type: bool
+    default: no
+  containers_filters:
+    description:
+      - A dictionary of filter values used for selecting containers to delete.
+      - "For example, C(until: 24h)."
+      - See L(the docker documentation,https://docs.docker.com/engine/reference/commandline/container_prune/#filtering)
+        for more information on possible filters.
+    type: dict
+  images:
+    description:
+      - Whether to list images.
+    type: bool
+    default: no
+  images_filters:
+    description:
+      - A dictionary of filter values used for selecting images to delete.
+      - "For example, C(dangling: true)."
+      - See L(the docker documentation,https://docs.docker.com/engine/reference/commandline/image_prune/#filtering)
+        for more information on possible filters.
+    type: dict
+  networks:
+    description:
+      - Whether to list networks.
+    type: bool
+    default: no
+  networks_filters:
+    description:
+      - A dictionary of filter values used for selecting networks to delete.
+      - See L(the docker documentation,https://docs.docker.com/engine/reference/commandline/network_prune/#filtering)
+        for more information on possible filters.
+    type: dict
+  volumes:
+    description:
+      - Whether to list volumes.
+    type: bool
+    default: no
+  volumes_filters:
+    description:
+      - A dictionary of filter values used for selecting volumes to delete.
+      - See L(the docker documentation,https://docs.docker.com/engine/reference/commandline/volume_prune/#filtering)
+        for more information on possible filters.
+    type: dict
+  disk_usage:
+    description:
+      - Summary information on used disk space by all Docker layers.
+      - The output is a sum of images, volumes, containers and build cache.
+    type: bool
+    default: no
 
 extends_documentation_fragment:
     - docker
@@ -57,20 +107,56 @@ EXAMPLES = '''
   docker_host_facts:
   register: result
 
+- name: Get info on docker host and list images
+  docker_host_facts:
+    images: true
+  register: result
+
+- name: Get info on docker host and list images matching the filter
+  docker_host_facts:
+    images: true
+    images_filters:
+      label: "mylabel"
+  register: result
+
+- name: Get info on docker host and used disk space
+  docker_host_facts:
+    disk_usage: true
+  register: result
+
 '''
 
 RETURN = '''
-exists:
-    description:
-      - Returns whether the node exists in docker swarm cluster.
-    type: bool
-    returned: always
-    sample: true
 docker_host_facts:
     description:
-      - Facts representing the current state of the docker host. Matches the C(docker system info) output.
+      - Facts representing the basic state of the docker host. Matches the C(docker system info) output.
     returned: always
     type: dict
+docker_volumes_list:
+    description:
+      - List of volumes with basic information about each. Matches the C(docker volumes ls) output.
+    returned: on success
+    type: dict
+docker_networks_list:
+    description:
+      - List of networks with basic information about each. Matches the C(docker volumes ls) output.
+    returned: on success
+    type: dict
+docker_containers_list:
+    description:
+      - List of containers with basic information about each. Matches the C(docker volumes ls) output.
+    returned: on success
+    type: dict
+docker_images_list:
+    description:
+      - List of images with basic information about each. Matches the C(docker image ls) output.
+    returned: on success
+    type: dict
+docker_disk_usage:
+    description:
+      - Facts representing the current state of the docker host. Matches the C(docker system info) output.
+    returned: on success
+    type: int
 
 '''
 
@@ -81,6 +167,12 @@ try:
     from docker.errors import APIError, NotFound
 except ImportError:
     # missing docker-py handled in ansible.module_utils.docker_common
+    pass
+
+try:
+    from ansible.module_utils.docker_common import docker_version, clean_dict_booleans_for_docker_api
+except Exception as dummy:
+    # missing docker-py handled in ansible.module_utils.docker
     pass
 
 
@@ -97,10 +189,15 @@ class DockerHostManager(DockerBaseClass):
 
         self.results['docker_host_facts'] = self.get_docker_host_facts()
 
+        if self.client.module.params['disk_usage'] is True:
+            self.results['docker_disk_usage'] = self.get_docker_disk_usage_facts()
+
         for docker_object in listed_objects:
             if self.client.module.params[docker_object] is True:
                 returned_name = "docker_" + docker_object + "_list"
-                self.results[returned_name] = self.get_docker_items_list(docker_object)
+                filter_name = docker_object + "_filters"
+                filters = clean_dict_booleans_for_docker_api(client.module.params.get(filter_name))
+                self.results[returned_name] = self.get_docker_items_list(docker_object, filters)
 
     def get_docker_host_facts(self):
         try:
@@ -108,7 +205,13 @@ class DockerHostManager(DockerBaseClass):
         except APIError as exc:
             self.client.fail_json(msg="Error inspecting docker host: %s" % to_native(exc))
 
-    def get_docker_items_list(self, object=None):
+    def get_docker_disk_usage_facts(self):
+        try:
+            return self.client.df()['LayersSize']
+        except APIError as exc:
+            self.client.fail_json(msg="Error inspecting docker host: %s" % to_native(exc))
+
+    def get_docker_items_list(self, docker_object=None, filters=None):
         items = None
         items_list = []
 
@@ -118,28 +221,28 @@ class DockerHostManager(DockerBaseClass):
         header_networks = ['Id', 'Driver', 'Name', 'Scope']
 
         try:
-            if object == 'containers':
-                items = self.client.containers()
-            if object == 'networks':
-                items = self.client.networks()
-            if object == 'images':
-                items = self.client.images()
-            if object == 'volumes':
-                items = self.client.volumes()
+            if docker_object == 'containers':
+                items = self.client.containers(filters=filters)
+            if docker_object == 'networks':
+                items = self.client.networks(filters=filters)
+            if docker_object == 'images':
+                items = self.client.images(filters=filters)
+            if docker_object == 'volumes':
+                items = self.client.volumes(filters=filters)
         except APIError as exc:
             self.client.fail_json(msg="Error inspecting docker host: %s" % to_native(exc))
 
-        if object != 'volumes':
+        if docker_object != 'volumes':
             for item in items:
                 item_record = dict()
 
-                if object == 'containers':
+                if docker_object == 'containers':
                     for key in header_containers:
                         item_record[key] = item.get(key)
-                if object == 'networks':
+                if docker_object == 'networks':
                     for key in header_networks:
                         item_record[key] = item.get(key)
-                if object == 'images':
+                if docker_object == 'images':
                     for key in header_images:
                         item_record[key] = item.get(key)
 
@@ -167,6 +270,7 @@ def main():
         networks_filters=dict(type='dict'),
         volumes=dict(type='bool', default=False),
         volumes_filters=dict(type='dict'),
+        disk_usage=dict(type='bool', default=False),
     )
 
     client = AnsibleDockerClient(
