b43d8835c51047f9bc2538d43032310e309c9826
Kevin Breit <kevin.breit@kevinbreit.net>
2019-01-28 16:53:58 -0600

    Remove blank line for PEP8

--- lib/ansible/module_utils/network/meraki/meraki.py
+++ lib/ansible/module_utils/network/meraki/meraki.py
@@ -153,7 +153,6 @@ class MerakiModule(object):
                     is_changed = True
         return is_changed
 
-
     def get_orgs(self):
         """Downloads all organizations for a user."""
         response = self.request('/organizations', method='GET')
