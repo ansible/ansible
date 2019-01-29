4cd1c7c0edf3c1d08d52c072de59b1b0a0f33da6
Adam Miller <admiller@redhat.com>
2019-01-29 16:22:23 -0600

    remove unnecessary else statement
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- lib/ansible/module_utils/facts/system/pkg_mgr.py
+++ lib/ansible/module_utils/facts/system/pkg_mgr.py
@@ -75,22 +75,21 @@ class PkgMgrFactCollector(BaseFactCollector):
         if collected_facts['ansible_distribution'] == 'Fedora':
             if os.path.exists('/run/ostree-booted'):
                 return "atomic_container"
-            else:
-                try:
-                    if int(collected_facts['ansible_distribution_major_version']) < 23:
-                        for yum in [pkg_mgr for pkg_mgr in PKG_MGRS if pkg_mgr['name'] == 'yum']:
-                            if os.path.exists(yum['path']):
-                                pkg_mgr_name = 'yum'
-                                break
-                    else:
-                        for dnf in [pkg_mgr for pkg_mgr in PKG_MGRS if pkg_mgr['name'] == 'dnf']:
-                            if os.path.exists(dnf['path']):
-                                pkg_mgr_name = 'dnf'
-                                break
-                except ValueError:
-                    # If there's some new magical Fedora version in the future,
-                    # just default to dnf
-                    pkg_mgr_name = 'dnf'
+            try:
+                if int(collected_facts['ansible_distribution_major_version']) < 23:
+                    for yum in [pkg_mgr for pkg_mgr in PKG_MGRS if pkg_mgr['name'] == 'yum']:
+                        if os.path.exists(yum['path']):
+                            pkg_mgr_name = 'yum'
+                            break
+                else:
+                    for dnf in [pkg_mgr for pkg_mgr in PKG_MGRS if pkg_mgr['name'] == 'dnf']:
+                        if os.path.exists(dnf['path']):
+                            pkg_mgr_name = 'dnf'
+                            break
+            except ValueError:
+                # If there's some new magical Fedora version in the future,
+                # just default to dnf
+                pkg_mgr_name = 'dnf'
         return pkg_mgr_name
 
     def _check_apt_flavor(self, pkg_mgr_name):
