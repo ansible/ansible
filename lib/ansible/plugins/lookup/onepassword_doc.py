2967a999d667a0c6883c37c08fdfc18d2a1bfe62
Abhijeet Kasurde <akasurde@redhat.com>
2019-01-31 08:54:17 -0500

    Update lib/ansible/plugins/lookup/onepassword_doc.py
    Remove unnecessary comma
    
    Co-Authored-By: djtaylor <djtaylor13@gmail.com>

--- lib/ansible/plugins/lookup/onepassword_doc.py
+++ lib/ansible/plugins/lookup/onepassword_doc.py
@@ -51,7 +51,7 @@ DOCUMENTATION = """
       - This lookup will use an existing 1Password session if one exists. If not, and you have already
         performed an initial sign in (meaning C(~/.op/config) exists), then only the C(master_password) is required.
         You may optionally specify C(subdomain) in this scenario, otherwise the last used subdomain will be used by C(op).
-      - This lookup can perform an initial login by providing C(subdomain), C(username), C(secret_key), and C(master_password).
+      - This lookup can perform an initial login by providing C(subdomain), C(username), C(secret_key) and C(master_password).
       - Due to the B(very) sensitive nature of these credentials, it is B(highly) recommeneded that you only pass in the minial credentials
         needed at any given time. Also, store these credentials in an Ansible Vault using a key that is equal to or greater in strength
         to the 1Password master password.
