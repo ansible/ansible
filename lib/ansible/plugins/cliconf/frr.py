4ae9cf55c2a926bc874eb055d8c6127f2248a0b2
NilashishC <nilashishchakraborty8@gmail.com>
2019-02-01 08:55:48 +0000

    Add get_diff and run_commands
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- lib/ansible/plugins/cliconf/frr.py
+++ lib/ansible/plugins/cliconf/frr.py
@@ -33,9 +33,12 @@ version_added: "2.8"
 import re
 import json
 
+from ansible.errors import AnsibleConnectionFailure
 from ansible.module_utils._text import to_text
 from ansible.module_utils.common._collections_compat import Mapping
+from ansible.module_utils.network.common.config import NetworkConfig, dumps
 from ansible.module_utils.network.common.utils import to_list
+from ansible.module_utils.basic import get_timestamp
 from ansible.plugins.cliconf import CliconfBase, enable_mode
 
 
@@ -69,6 +72,8 @@ class Cliconf(CliconfBase):
     def get_option_values(self):
         return {
             'format': ['text'],
+            'diff_match': ['line', 'strict', 'exact', 'none'],
+            'diff_replace': ['line', 'block'],
             'output': ['text']
         }
 
@@ -83,16 +88,50 @@ class Cliconf(CliconfBase):
             'supports_multiline_delimiter': False,
             'supports_diff_match': False,
             'supports_diff_ignore_lines': False,
-            'supports_generate_diff': False,
+            'supports_generate_diff': True,
             'supports_replace': False
         }
 
     def get_capabilities(self):
         result = super(Cliconf, self).get_capabilities()
+        result['rpc'] += ['get_diff', 'run_commands']
         result['device_operations'] = self.get_device_operations()
         result['supported_protocols'] = self.get_supported_protocols()
         result.update(self.get_option_values())
         return json.dumps(result)
+
+    def get_diff(self, candidate=None, running=None, diff_match='line', diff_ignore_lines=None, path=None, diff_replace='line'):
+
+        diff = {}
+        device_operations = self.get_device_operations()
+        option_values = self.get_option_values()
+
+        if candidate is None and device_operations['supports_generate_diff']:
+            raise ValueError("candidate configuration is required to generate diff")
+
+        if diff_match not in option_values['diff_match']:
+            raise ValueError("'match' value %s in invalid, valid values are %s" % (
+            diff_match, ', '.join(option_values['diff_match'])))
+
+        if diff_replace not in option_values['diff_replace']:
+            raise ValueError("'replace' value %s in invalid, valid values are %s" % (
+            diff_replace, ', '.join(option_values['diff_replace'])))
+
+        # prepare candidate configuration
+        candidate_obj = NetworkConfig(indent=1)
+        candidate_obj.load(candidate)
+
+        if running and diff_match != 'none':
+            # running configuration
+            running_obj = NetworkConfig(indent=1, contents=running, ignore_lines=diff_ignore_lines)
+            configdiffobjs = candidate_obj.difference(running_obj, path=path, match=diff_match, replace=diff_replace)
+
+        else:
+            configdiffobjs = candidate_obj.items
+
+        diff['config_diff'] = dumps(configdiffobjs, 'commands') if configdiffobjs else ''
+        return diff
+
 
     @enable_mode
     def get_config(self, source='running', flags=None, format=None):
@@ -148,3 +187,33 @@ class Cliconf(CliconfBase):
             raise ValueError("'output' value %s is not supported for get" % output)
 
         return self.send_command(command=command, prompt=prompt, answer=answer, sendonly=sendonly, check_all=check_all)
+
+    def run_commands(self, commands=None, check_rc=True, return_timestamps=False):
+        if commands is None:
+            raise ValueError("'commands' value is required")
+
+        responses = list()
+        timestamps = list()
+        for cmd in to_list(commands):
+            if not isinstance(cmd, Mapping):
+                cmd = {'command': cmd}
+
+            output = cmd.pop('output', None)
+            if output:
+                raise ValueError("'output' value %s is not supported for run_commands" % output)
+
+            try:
+                out = self.send_command(**cmd)
+                timestamp = get_timestamp()
+            except AnsibleConnectionFailure as e:
+                if check_rc:
+                    raise
+                out = getattr(e, 'err', to_text(e))
+
+            responses.append(out)
+            timestamps.append(timestamp)
+
+        if return_timestamps:
+            return responses, timestamps
+        else:
+            return responses
