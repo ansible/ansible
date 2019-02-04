7589d496fc719c13945262d3a231260fa11f0d17
Brian Coca <bcoca@users.noreply.github.com>
2019-02-04 17:16:32 -0500

    remove default from delegate_facts to inherit (#45492) (#51553)
    * remove default from delegate_facts to inherit (#45492)
    
    * remove default from delegate_facts to inherit
    
    fixes #45456
    
    * test delegate_facts
    
    * added note about inheritance and defaults
    
    * yamllint
    
    (cherry picked from commit 8743e6ae2ee7aa3ae94038554110a9255f5d8fd8)
    
    * added changelog

--- test/integration/targets/delegate_to/runme.sh
+++ test/integration/targets/delegate_to/runme.sh
@@ -12,3 +12,5 @@ ansible-playbook test_delegate_to_loop_randomness.yml -v "$@"
 ansible-playbook delegate_and_nolog.yml -i inventory -v "$@"
 
 ansible-playbook test_delegate_to_loop_caching.yml -i inventory -v "$@"
+
+ansible-playbook delegate_facts_block.yml -i inventory -v "$@"
