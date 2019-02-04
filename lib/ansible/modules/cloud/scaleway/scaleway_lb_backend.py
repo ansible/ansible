b4051249d79935f8b01f005a5753d86753ebe1dc
Rémy Léone <rleone@scaleway.com>
2019-02-04 14:16:28 +0100

    Add a Scaleway lb backend module

--- lib/ansible/modules/cloud/scaleway/scaleway_lb_backend.py
+++ lib/ansible/modules/cloud/scaleway/scaleway_lb_backend.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Scaleway Load-balancer management module
+# Scaleway Load-balancer backend management module
 #
 # Copyright (C) 2018 Online SAS.
 # https://www.scaleway.com
@@ -19,27 +19,21 @@ ANSIBLE_METADATA = {
 
 DOCUMENTATION = '''
 ---
-module: scaleway_lb
-short_description: Scaleway load-balancer management module
+module: scaleway_lb_back
+short_description: Scaleway load-balancer backend management module
 version_added: "2.8"
 author: Remy Leone (@sieben)
 description:
-    - "This module manages load-balancers on Scaleway."
+    - "This module manages load-balancers backend on Scaleway."
 extends_documentation_fragment: scaleway
 
 options:
 
   name:
     description:
-      - Name of the load-balancer
-    required: true
+      - Name of the load-balancer backend
 
-  description:
-    description:
-      - Description of the load-balancer
-    required: true
-
-  organization_id:
+  organization:
     description:
       - Organization identifier
     required: true
@@ -60,9 +54,69 @@ options:
       - nl-ams
       - fr-par
 
-  tags:
+  forward_port:
     description:
-    - List of tags to apply to the load-balancer
+      - User sessions will be forwarded to this port of backend servers
+
+  forward_port_algorithm:
+    description:
+      - Load balancing algorithm:
+      - Round-Robin: Equal share
+      - leastconn: Least amount of TCP connection
+    choices:
+      - leastconn
+      - roundrobin
+
+  forward_protocol:
+    description:
+      - Backend protocol
+    choices:
+      - tcp
+      - http
+    default:
+      - tcp
+
+  on_marked_down_action:
+    description:
+      - Modify what occurs when a backend server is marked down
+    choices:
+      - on_marked_down_action_none
+      - shutdown_sessions
+
+  send_proxy_v2:
+    description:
+    - Enables PROXY protocol version 2 (must be supported by backend servers)
+    type: bool
+    default: no
+
+  server_ip:
+    description:
+    - Backend server IP addresses list (IPv4 or IPv6)
+
+  sticky_sessions:
+    description:
+    - Enables cookie-based session persistence
+    default: none
+    choices:
+    - cookie
+    - none
+    - table (Will match on IP)
+
+  sticky_sessions_cookie_name:
+    description:
+    - Cookie name for sticky sessions
+
+  timeout_connect:
+    description:
+    - Maximum initial server connection establishment time in milliseconds
+
+  timeout_server:
+    description:
+    - Maximum server connection inactivity time in milliseconds
+
+  timeout_tunnel:
+    description:
+    - Maximum tunnel inactivity time in milliseconds
 
   wait:
     description:
@@ -84,70 +138,14 @@ options:
 '''
 
 EXAMPLES = '''
-- name: Create a load-balancer
-  scaleway_lb:
-    name: foobar
-    state: present
-    organization_id: 951df375-e094-4d26-97c1-ba548eeb9c42
-    region: fr-par
-    tags:
-      - hello
-
-- name: Delete a load-balancer
-  scaleway_lb:
-    name: foobar
-    state: absent
-    organization_id: 951df375-e094-4d26-97c1-ba548eeb9c42
-    region: fr-par
 '''
 
 RETURNS = '''
-{
-   "scaleway_lb": {
-      "backend_count": 0,
-      "frontend_count": 0,
-      "description": "Description of my load-balancer",
-      "id": "00000000-0000-0000-0000-000000000000",
-      "instances": [
-         {
-            "id": "00000000-0000-0000-0000-000000000000",
-            "ip_address": "10.0.0.1",
-            "region": "fr-par",
-            "status": "ready"
-         },
-         {
-            "id": "00000000-0000-0000-0000-000000000000",
-            "ip_address": "10.0.0.2",
-            "region": "fr-par",
-            "status": "ready"
-         }
-      ],
-      "ip": [
-         {
-            "id": "00000000-0000-0000-0000-000000000000",
-            "ip_address": "192.168.0.1",
-            "lb_id": "00000000-0000-0000-0000-000000000000",
-            "region": "fr-par",
-            "organization_id": "00000000-0000-0000-0000-000000000000",
-            "reverse": ""
-         }
-      ],
-      "name": "lb_ansible_test",
-      "organization_id": "00000000-0000-0000-0000-000000000000",
-      "region": "fr-par",
-      "status": "ready",
-      "tags": [
-        "first_tag",
-        "second_tag"
-      ]
-   }
-}
 '''
 
 import datetime
 import time
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.scaleway import SCALEWAY_REGIONS, SCALEWAY_ENDPOINT, scaleway_argument_spec, Scaleway
 from ansible.module_utils.scaleway import SCALEWAY_NEW_LOCATION, scaleway_argument_spec, Scaleway
 
 STABLE_STATES = (
@@ -163,10 +161,8 @@ MUTABLE_ATTRIBUTES = (
 
 def payload_from_wished_lb(wished_lb):
     return {
-        "organization_id": wished_lb["organization_id"],
+        # "organization_id": wished_lb["organization_id"],
         "name": wished_lb["name"],
-        "tags": wished_lb["tags"],
-        "description": wished_lb["description"]
     }
 
 
@@ -188,7 +184,7 @@ def fetch_state(api, lb):
         api.module.fail_json(msg="Could not fetch state in %s" % response.json)
 
 
-def wait_to_complete_state_transition(api, lb, force_wait=False):
+def wait_to_complete_state_transition(api, lb_backend, force_wait=False):
     wait = api.module.params["wait"]
     if not (wait or force_wait):
         return
@@ -199,26 +195,27 @@ def wait_to_complete_state_transition(api, lb, force_wait=False):
     end = start + datetime.timedelta(seconds=wait_timeout)
     while datetime.datetime.utcnow() < end:
         api.module.debug("We are going to wait for the load-balancer to finish its transition")
-        state = fetch_state(api, lb)
+        state = fetch_state(api, lb_backend)
         if state in STABLE_STATES:
             api.module.debug("It seems that the load-balancer is not in transition anymore.")
-            api.module.debug("load-balancer in state: %s" % fetch_state(api, lb))
+            api.module.debug("load-balancer in state: %s" % fetch_state(api, lb_backend))
             break
         time.sleep(wait_sleep_time)
     else:
         api.module.fail_json(msg="Server takes too long to finish its transition")
 
 
-def lb_attributes_should_be_changed(target_lb, wished_lb):
-    diff = dict((attr, wished_lb[attr]) for attr in MUTABLE_ATTRIBUTES if target_lb[attr] != wished_lb[attr])
-
+def lb_attributes_should_be_changed(target_lb_backend, wished_lb_backend):
+    diff = {
+        attr: wished_lb_backend[attr] for attr in MUTABLE_ATTRIBUTES if target_lb_backend[attr] != wished_lb_backend[attr]
+    }
     if diff:
-        return dict((attr, wished_lb[attr]) for attr in MUTABLE_ATTRIBUTES)
+        return {attr: wished_lb_backend[attr] for attr in MUTABLE_ATTRIBUTES}
     else:
         return diff
 
 
-def present_strategy(api, wished_lb):
+def present_strategy(api, wished_lb_backend):
     changed = False
 
     response = api.get(path=api.api_path)
@@ -226,19 +223,19 @@ def present_strategy(api, wished_lb):
         api.module.fail_json(msg='Error getting load-balancers [{0}: {1}]'.format(
             response.status_code, response.json['message']))
 
-    lbs_list = response.json["lbs"]
+    lbs_list = response.json["backends"]
     lb_lookup = dict((lb["name"], lb)
                      for lb in lbs_list)
 
-    if wished_lb["name"] not in lb_lookup.keys():
+    if wished_lb_backend["name"] not in lb_lookup.keys():
         changed = True
         if api.module.check_mode:
             return changed, {"status": "A load-balancer would be created."}
 
         # Create Load-balancer
-        api.warn(payload_from_wished_lb(wished_lb))
+        api.warn(payload_from_wished_lb(wished_lb_backend))
         creation_response = api.post(path=api.api_path,
-                                     data=payload_from_wished_lb(wished_lb))
+                                     data=payload_from_wished_lb(wished_lb_backend))
 
         if not creation_response.ok:
             msg = "Error during lb creation: %s: '%s' (%s)" % (creation_response.info['msg'],
@@ -246,13 +243,13 @@ def present_strategy(api, wished_lb):
                                                                creation_response.json)
             api.module.fail_json(msg=msg)
 
-        wait_to_complete_state_transition(api=api, lb=creation_response.json)
+        wait_to_complete_state_transition(api=api, lb_backend=creation_response.json)
         response = api.get(path=api.api_path + "/%s" % creation_response.json["id"])
         return changed, response.json
 
-    target_lb = lb_lookup[wished_lb["name"]]
-    patch_payload = lb_attributes_should_be_changed(target_lb=target_lb,
-                                                    wished_lb=wished_lb)
+    target_lb = lb_lookup[wished_lb_backend["name"]]
+    patch_payload = lb_attributes_should_be_changed(target_lb_backend=target_lb,
+                                                    wished_lb_backend=wished_lb_backend)
 
     if not patch_payload:
         return changed, target_lb
@@ -268,17 +265,17 @@ def present_strategy(api, wished_lb):
         api.module.fail_json(msg='Error during load-balancer attributes update: [{0}: {1}]'.format(
             lb_patch_response.status_code, lb_patch_response.json['message']))
 
-    wait_to_complete_state_transition(api=api, lb=target_lb)
+    wait_to_complete_state_transition(api=api, lb_backend=target_lb)
     return changed, lb_patch_response.json
 
 
-def absent_strategy(api, wished_lb):
+def absent_strategy(api, wished_lb_backend):
     response = api.get(path=api.api_path)
     changed = False
 
     status_code = response.status_code
     lbs_json = response.json
-    lbs_list = lbs_json["lbs"]
+    lbs_list = lbs_json["backends"]
 
     if not response.ok:
         api.module.fail_json(msg='Error getting load-balancers [{0}: {1}]'.format(
@@ -286,21 +283,21 @@ def absent_strategy(api, wished_lb):
 
     lb_lookup = dict((lb["name"], lb)
                      for lb in lbs_list)
-    if wished_lb["name"] not in lb_lookup.keys():
+    if wished_lb_backend["name"] not in lb_lookup.keys():
         return changed, {}
 
-    target_lb = lb_lookup[wished_lb["name"]]
+    target_lb = lb_lookup[wished_lb_backend["name"]]
     changed = True
     if api.module.check_mode:
         return changed, {"status": "Load-balancer would be destroyed"}
 
-    wait_to_complete_state_transition(api=api, lb=target_lb, force_wait=True)
+    wait_to_complete_state_transition(api=api, lb_backend=target_lb, force_wait=True)
     response = api.delete(path=api.api_path + "/%s" % target_lb["id"])
     if not response.ok:
         api.module.fail_json(msg='Error deleting load-balancer [{0}: {1}]'.format(
             response.status_code, response.json))
 
-    wait_to_complete_state_transition(api=api, lb=target_lb)
+    wait_to_complete_state_transition(api=api, lb_backend=target_lb)
     return changed, response.json
 
 
@@ -312,20 +309,25 @@ state_strategy = {
 
 def core(module):
     region = module.params["region"]
-    wished_load_balancer = {
-        "state": module.params["state"],
+    lb_id = module.params["lb_id"]
+    wished_load_balancer_back = {
         "name": module.params["name"],
-        "description": module.params["description"],
-        "tags": module.params["tags"],
-        "organization_id": module.params["organization_id"]
+        "state": module.params["state"],
+        "lb_id": module.params["lb_id"],
+        "forward_port": module.params["forward_port"],
+        "forward_protocol": module.params["forward_protocol"],
+        "forward_port_algorithm": module.params["forward_port_algorithm"],
+        "sticky_sessions": module.params["sticky_sessions"],
+        "timeout_connect": module.params["timeout_connect"],
+        "timeout_server": module.params["timeout_server"],
+        "timeout_tunnel": module.params["timeout_tunnel"]
     }
-    module.params['api_url'] = SCALEWAY_ENDPOINT
     module.params['api_url'] = SCALEWAY_NEW_LOCATION[region]["api_endpoint"]
     api = Scaleway(module=module)
-    api.api_path = "lbaas/v1beta1/regions/%s/lbs" % region
+    api.api_path = "lbaas/v1beta1/regions/%s/lbs/%s/backends" % (region, lb_id)
 
-    changed, summary = state_strategy[wished_load_balancer["state"]](api=api,
-                                                                     wished_lb=wished_load_balancer)
+    changed, summary = state_strategy[wished_load_balancer_back["state"]](api=api,
+                                                                          wished_lb_backend=wished_load_balancer_back)
     module.exit_json(changed=changed, scaleway_lb=summary)
 
 
@@ -333,11 +335,16 @@ def main():
     argument_spec = scaleway_argument_spec()
     argument_spec.update(dict(
         name=dict(required=True),
-        description=dict(required=True),
         state=dict(choices=state_strategy.keys(), default='present'),
-        tags=dict(type="list", default=[]),
+        lb_id=dict(required=True),
+        forward_port=dict(required=True, type='int'),
+        forward_protocol=dict(choices=["http", "tcp"], default="tcp"),
+        forward_port_algorithm=dict(choices=["roundrobin", "leastconn"], default="roundrobin"),
         region=dict(required=True, choices=SCALEWAY_NEW_LOCATION.keys()),
-        organization_id=dict(required=True),
+        sticky_sessions=dict(choices=["cookie", "none", "table"], default="none"),
+        timeout_connect=dict(type="int"),
+        timeout_server=dict(type="int"),
+        timeout_tunnel=dict(type="int"),
         wait=dict(type="bool", default=False),
         wait_timeout=dict(type="int", default=300),
         wait_sleep_time=dict(type="int", default=3),
