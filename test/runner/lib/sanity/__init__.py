83cf36cd6e6586e3632c8841fa62749edccbda6a
Matt Clay <matt@mystile.com>
2019-02-05 14:56:04 -0800

    Support `ignore_changes` in code-smell tests.

--- test/runner/lib/sanity/__init__.py
+++ test/runner/lib/sanity/__init__.py
@@ -255,6 +255,7 @@ class SanityCodeSmellTest(SanityTest):
             files = self.config.get('files')
             always = self.config.get('always')
             text = self.config.get('text')
+            ignore_changes = self.config.get('ignore_changes')
 
             if output == 'path-line-column-message':
                 pattern = '^(?P<path>[^:]*):(?P<line>[0-9]+):(?P<column>[0-9]+): (?P<message>.*)$'
@@ -263,7 +264,11 @@ class SanityCodeSmellTest(SanityTest):
             else:
                 pattern = ApplicationError('Unsupported output type: %s' % output)
 
-            paths = sorted(i.path for i in targets.include)
+            if ignore_changes:
+                paths = sorted(i.path for i in targets.targets)
+                always = False
+            else:
+                paths = sorted(i.path for i in targets.include)
 
             if always:
                 paths = []
