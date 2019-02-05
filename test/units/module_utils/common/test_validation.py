29b0a5a1764b621608a1d4b91e1aba8c24eab345
Sam Doran <sdoran@redhat.com>
2019-02-05 11:57:22 -0500

    Update tests

--- test/units/module_utils/common/test_validation.py
+++ test/units/module_utils/common/test_validation.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-# (c) 2012-2014, Michael DeHaan <michael.dehaan@gmail.com>
-# (c) 2016 Toshio Kuratomi <tkuratomi@ansible.com>
-# (c) 2017-2018 Ansible Project
+# Copyright (c) 2019 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
@@ -11,11 +9,11 @@ import pytest
 
 from ansible.module_utils.common.validation import count_terms
 
-TEST_CHECKS = [
-    (['state'], 1),
-    (['ghost'], 0),
-    (['state', 'delay', 'name', 'bob'], 3),
-]
+TEST_CHECKS = frozenset((
+    (('state',), 1),
+    (('ghost',), 0),
+    (('state', 'delay', 'name', 'bob'), 3),
+))
 
 
 @pytest.fixture
