abea8131bf9c0bf1a94f526f7e8eb4e1a66f3ec9
Dag Wieers <dag@wieers.com>
2019-02-06 20:51:27 +0100

    Fixes based on review feedback

--- lib/ansible/module_utils/powershell/Ansible.ModuleUtils.Backup.psm1
+++ lib/ansible/module_utils/powershell/Ansible.ModuleUtils.Backup.psm1
@@ -1,24 +1,23 @@
 # Copyright (c): 2018, Dag Wieers (@dagwieers) <dag@wieers.com>
 # Simplified BSD License (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
 
-Set-StrictMode -Version 2.0
-$ErrorActionPreference = "Stop"
-
-Function Backup-File($path, $obj=@{}, $check_mode=$false) {
+Function Backup-File($path, $obj=@{}) {
 <#
     .SYNOPSIS
     Helper function to make a backup of a file.
     .EXAMPLE
     Backup-File -path $path -obj $result
 #>
+    [CmdletBinding(SupportsShouldProcess=$true)]
+
     $backup_path = "$path.$pid." + [DateTime]::Now.ToString("yyyyMMdd-HHmmss") + ".bak";
     Try {
-        Copy-Item -Path $path -Destination $backup_path -WhatIf:$check_mode
+        Copy-Item -LiteralPath $path -Destination $backup_path
     } Catch {
-        Fail-Json -obj $obj -message "Failed to create backup file '$backup_path' from '$path'. ($($_.Exception.Message))"
+        throw "Failed to create backup file '$backup_path' from '$path'. ($($_.Exception.Message))"
     }
     return $backup_path
 }
 
 # This line must stay at the bottom to ensure all defined module parts are exported
-Export-ModuleMember -Alias * -Function * -Cmdlet *
+Export-ModuleMember -Function Backup-File
