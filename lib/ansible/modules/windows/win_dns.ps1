206aba20551e959ad2d0c8fae94d11590f6a7104
John Nelson <john_nelson@hitachi-id.com>
2019-02-07 21:00:59 -0700

    win_dns: Initial work

---
+++ lib/ansible/modules/windows/win_dns.ps1
@@ -0,0 +1,156 @@
+#!powershell
+
+#Requires -Module Ansible.ModuleUtils.Legacy
+
+$ErrorActionPreference = "Stop"
+
+$params = Parse-Args -arguments $args -supports_check_mode $true
+$check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
+$diff_mode = Get-AnsibleParam -obj $params -name "_ansible_diff" -type "bool" -default $false
+
+$name = Get-AnsibleParam -obj $params -name "name" -type "str" -failifempty $true
+$state = Get-AnsibleParam -obj $params -name "state" -type "str" -default "present" -validateset "present","absent"
+$ttl = Get-AnsibleParam -obj $params -name "ttl" -type "int" -default 3600
+$type = Get-AnsibleParam -obj $params -name "type" -type "str" -failifempty $true -validateset "A","AAAA","CNAME","MX","NS","TXT","PTR"
+$values = Get-AnsibleParam -obj $params -name "value" -type "list" -default @() -aliases @("values")
+$zone = Get-AnsibleParam -obj $params -name "zone" -type "str" -failifempty $true
+
+
+if ($state -eq 'present')
+{
+    if ($values.Count -eq 0)
+    {
+        Fail-Json "values must be non-empty when state='present'"
+    }
+}
+else
+{
+    if ($values.Count -ne 0)
+    {
+        Fail-Json "values must be undefined or empty when state='absent'"
+    }
+}
+
+
+$minimum_ttl = 900  # https://docs.microsoft.com/en-us/windows/desktop/ad/configuration-of-ttl-limits
+if ($ttl -lt $minimum_ttl)
+{
+    Fail-Json "ttl must be $minimum_ttl or greater"
+}
+$ttl = New-TimeSpan -Seconds $ttl
+
+
+if ($type -eq 'CNAME' -and $values -ne $null -and $values.Count -gt 0 -and $zone[-1] -ne '.')
+{
+    # CNAMES that point to zone items should be '.'-terminated, or record matching will fail
+    $values = $values | ForEach-Object {
+        if ($_ -Like "*$zone") { "$($_)." } else { $_ }
+    }
+}
+
+
+$result = @{
+    changed = $false
+}
+
+
+$record_argument_name = @{
+    A = "IPv4Address";
+    AAAA = "IPv6Address";
+    CNAME = "HostNameAlias";
+    MX = "MailExchange";
+    NS = "NameServer";
+    PTR = "PtrDomainName";
+    TXT = "DescriptiveText"
+}[$type]
+
+$record_object_types = @{
+    A = [System.Net.IPAddress];
+    AAAA = [System.Net.IPAddress];
+}
+
+
+$changes = New-Object -Typename System.Collections.ArrayList
+
+
+$records = Get-DnsServerResourceRecord -ZoneName $zone -Name $name -RRType $type -Node -ErrorAction:Ignore
+if ($records -ne $null)
+{
+    # We use [Hashtable]$required_values below as a set rather than a map.
+    # It provides quick lookup to test existing DNS record against. By removing
+    # items as each is processed, whatever remains at the end is missing
+    # content (that needs to be added).
+    $required_values = @{}
+    $values | ForEach-Object {
+        if ($record_object_types.Contains($type))
+        {
+            [System.Management.Automation.LanguagePrimitives]::ConvertTo($_, $record_object_types[$type])
+        }
+        $required_values[$_] = $null
+    }
+
+    foreach ($record in $records)
+    {
+        $record_value = $record.RecordData.$record_argument_name
+
+        if ($required_values.ContainsKey($record_value))
+        {
+            # This record matches one of the values; but does it match the TTL?
+            if ($record.TimeToLive -ne $ttl)
+            {
+                if (-not $check_mode)
+                {
+                    $new_record = $record
+                    $new_record.TimeToLive = $ttl
+                    Set-DnsServerResourceRecord -OldInputObject $record -NewInputObject $new_record
+                }
+
+                $changes += "-[$zone] $($record.HostName) $($record.TimeToLive.TotalSeconds) $type $record_value`n"
+                $changes += "+[$zone] $($record.HostName) $($ttl.TotalSeconds) $type $record_value`n"
+                $result.changed = $true
+            }
+
+            # Cross this one off the list, so we don't try adding it later
+            $required_values.Remove($record_value)
+        }
+        else
+        {
+            # This record doesn't match any of the values, and must be removed
+            if (-not $check_mode)
+            {
+                $record | Remove-DnsServerResourceRecord -ZoneName $zone -Force
+            }
+
+            $changes += "-[$zone] $($record.HostName) $($record.TimeToLive.TotalSeconds) $type $($record.RecordData.$record_argument_name)`n"
+            $result.changed = $true
+        }
+    }
+
+    # Whatever is left in $required_values needs to be added
+    $values = $required_values.Keys
+}
+
+
+if ($values -ne $null -and $values.Count -gt 0)
+{
+    $values | ForEach-Object {
+        if (-not $check_mode)
+        {
+            $splat_args = @{ $type = $true; $record_argument_name = $_ }
+            Add-DnsServerResourceRecord -ZoneName $zone -Name $name -AllowUpdateAny -TimeToLive $ttl @splat_args
+        }
+
+        $changes += "+[$zone] $( $record.HostName ) $($ttl.TotalSeconds) $type $( $record.RecordData.$record_argument_name )`n"
+    }
+
+    $result.changed = $true
+}
+
+
+if ($diff_mode)
+{
+    $result.diff = @{ prepared = ($changes -Join '') }
+}
+
+
+Exit-Json -obj $result
---
+++ lib/ansible/modules/windows/win_dns.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2019, Hitachi ID Systems, Inc.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# This is a windows documentation stub. The actual code lives in the .ps1
+# file of the same name.
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = r'''
+---
+module: win_dns
+version_added: "2.8"
+short_description: Manage Windows Server DNS records
+description:
+- Manages DNS records within an existing Windows Server DNS zone
+author: John Nelson (@johnboy2)
+options:
+  name:
+    description:
+    - The name of the record
+    required: yes
+  state:
+    description:
+    - Whether the record should exist or not
+    choices: [ absent, present ]
+    default: present
+  ttl:
+    description:
+    - The "time to live" of the record, in seconds. Must be at least 900
+      (ie 15 minutes).
+    default: 3600
+  type:
+    description:
+    - The type of DNS record to manage.
+    choices: [ A, AAAA, CNAME, MX, NS, TXT, PTR ]
+    required: yes
+  value:
+    description:
+    - The value(s) to specify. Required when `state=present`.
+    aliases: [ values ]
+  zone:
+    description:
+    - The name of the zone to manage (eg "example.com"). The zone must already
+      exist.
+    required: yes
+'''
+
+EXAMPLES = r'''
+- name: Create database server alias
+  win_dns:
+    name: db1
+    type: CNAME
+    value: cgyl1404p.amer.example.com
+    zone: amer.example.com
+
+- name: Remove static record
+  win_dns:
+    name: db1
+    type: A
+    state: absent
+    zone: amer.example.com
+'''
