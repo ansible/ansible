bbab72853d8a67b1723a1e1642e350b1394f3c44
zikalino <zikalino@microsoft.com>
2019-02-10 07:28:44 +0800

    fixed test

--- lib/ansible/modules/cloud/azure/azure_rm_devtestlab_facts.py
+++ lib/ansible/modules/cloud/azure/azure_rm_devtestlab_facts.py
@@ -16,7 +16,7 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = '''
 ---
 module: azure_rm_devtestlab_facts
-version_added: "2.8"
+version_added: "2.8" 
 short_description: Get Azure DevTest Lab facts.
 description:
     - Get facts of Azure DevTest Lab.
