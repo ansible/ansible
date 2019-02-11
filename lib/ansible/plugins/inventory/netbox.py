0d97843946a22a62530310ee77d91b48f5e4fd33
Karsten Jakobsen <karsten@karstenjakobsen.dk>
2019-02-11 06:40:24 +0100

    netbox: inventory: Fix VMs do not have device_roles, but instead use roles  (#50761)
    This makes sure that roles show up as device_role when using VMs

--- lib/ansible/plugins/inventory/netbox.py
+++ lib/ansible/plugins/inventory/netbox.py
@@ -252,7 +252,10 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
 
     def extract_device_role(self, host):
         try:
-            return [self.device_roles_lookup[host["device_role"]["id"]]]
+            if 'device_role' in host:
+                return [self.device_roles_lookup[host["device_role"]["id"]]]
+            elif 'role' in host:
+                return [self.device_roles_lookup[host["role"]["id"]]]
         except Exception:
             return
 
