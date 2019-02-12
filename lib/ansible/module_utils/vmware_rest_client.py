9df9a2c8139894070826dd90821690dc384d47b1
Abhijeet Kasurde <akasurde@redhat.com>
2019-02-12 16:53:33 +0530

    VMware: Tag management to VMware object (#46945)
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/module_utils/vmware_rest_client.py
+++ lib/ansible/module_utils/vmware_rest_client.py
@@ -185,3 +185,23 @@ class VmwareRestClient(object):
         for t in temp_tags_model:
             tags.append(t.name)
         return tags
+
+    @staticmethod
+    def search_svc_object_by_name(service, svc_obj_name=None):
+        """
+        Return service object by name
+        Args:
+            service: Service object
+            svc_obj_name: Name of service object to find
+
+        Returns: Service object if found else None
+
+        """
+        if not svc_obj_name:
+            return None
+
+        for svc_object in service.list():
+            svc_obj = service.get(svc_object)
+            if svc_obj.name == svc_obj_name:
+                return svc_obj
+        return None
