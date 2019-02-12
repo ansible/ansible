5cde95e81806b6e692746c40ba5409929de2bba8
hansmi <hansmi@users.noreply.github.com>
2019-02-12 18:47:33 +0100

    callbacks/slack: Explicitly set Content-Type header (#51824)
    There are other chat systems with hook implementations more or less
    compatible with Slack, such as Rocket.Chat. The latter requires the
    Content-Type header to be set to "application/json" (the body is JSON).
    
    Signed-off-by: Michael Hanselmann <public@hansmi.ch>

--- lib/ansible/plugins/callback/slack.py
+++ lib/ansible/plugins/callback/slack.py
@@ -114,6 +114,10 @@ class CallbackModule(CallbackBase):
                                   'variable.')
 
     def send_msg(self, attachments):
+        headers = {
+            'Content-type': 'application/json',
+        }
+
         payload = {
             'channel': self.channel,
             'username': self.username,
@@ -127,7 +131,8 @@ class CallbackModule(CallbackBase):
         self._display.debug(data)
         self._display.debug(self.webhook_url)
         try:
-            response = open_url(self.webhook_url, data=data, validate_certs=self.validate_certs)
+            response = open_url(self.webhook_url, data=data, validate_certs=self.validate_certs,
+                                headers=headers)
             return response.read()
         except Exception as e:
             self._display.warning(u'Could not submit message to Slack: %s' %
