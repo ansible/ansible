ab007c6d83aa44a5fcf66731ce8131012459c088
Jordan Borean <jborean93@gmail.com>
2019-02-12 10:05:41 +1000

    Terminate never so I can test out locally

--- test/utils/shippable/windows.sh
+++ test/utils/shippable/windows.sh
@@ -93,5 +93,5 @@ for version in "${python_versions[@]}"; do
     ansible-test windows-integration --color -v --retry-on-error win_become win_whoami ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} ${UNSTABLE:+"$UNSTABLE"} \
         "${platforms[@]}" --changed-all-target "${changed_all_target}" --changed-all-mode "${changed_all_mode}" \
         --docker default --python "2.7" \
-        --remote-terminate "${terminate}" --remote-stage "${stage}" --remote-provider "${provider}"
+        --remote-terminate "never" --remote-stage "${stage}" --remote-provider "${provider}"
 done
