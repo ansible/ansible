75d822472051c434abf1c284d5fa518d8df02ab3
Dag Wieers <dag@wieers.com>
2019-02-13 12:30:15 +0100

    Sigh

--- lib/ansible/modules/cloud/misc/qemu_img.py
+++ lib/ansible/modules/cloud/misc/qemu_img.py
@@ -5,8 +5,8 @@
 # Copyright: (c) 2013, Toshaan Bharvani <toshaan@vantosh.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-__metaclass__ = type
 from __future__ import absolute_import, division, print_function
+__metaclass__ = type
 
 ANSIBLE_METADATA = {'status': ['preview'],
                     'supported_by': 'community',
