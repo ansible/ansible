17fed9729451b85584c7386283cb839bbe60fa8a
おすし <47208255+1kaw@users.noreply.github.com>
2019-02-14 04:10:47 +0900

    Fix: Yum module does not use proxy when username is not set #51548 (#51994)
    * add test of yum with proxy
    
    * Properly handle unauthenticated yum proxy config
    
    Fixes #51548
    
    * shell executable is bash
    
    (cherry picked from commit c2a409a9e0325f9e46c6bba2294a33b392f44f98)

--- lib/ansible/modules/packaging/os/yum.py
+++ lib/ansible/modules/packaging/os/yum.py
@@ -715,7 +715,6 @@ class YumModule(YumDnf):
         # setting system proxy environment and saving old, if exists
         my = self.yum_base()
         namepass = ""
-        proxy_url = ""
         scheme = ["http", "https"]
         old_proxy_env = [os.getenv("http_proxy"), os.getenv("https_proxy")]
         try:
@@ -738,10 +737,7 @@ class YumModule(YumDnf):
                         )
                 else:
                     for item in scheme:
-                        os.environ[item + "_proxy"] = re.sub(
-                            r"(http://)",
-                            r"\g<1>", proxy_url
-                        )
+                        os.environ[item + "_proxy"] = my.conf.proxy
             yield
         except yum.Errors.YumBaseError:
             raise
