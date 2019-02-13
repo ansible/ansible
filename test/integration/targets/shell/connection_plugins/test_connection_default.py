847d089d6b903d4a3b68c5de47b0697f804e2d9b
Jordan Borean <jborean93@gmail.com>
2019-02-14 03:49:13 +1000

    Re-add support for setting shell from play context (#52139)
    * Re-add support for setting shell from play context
    
    * Add integration tests
    
    * Add more tests for shell override
    
    * fix sanity issue

---
+++ test/integration/targets/shell/connection_plugins/test_connection_default.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2019 Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = '''
+connection: test_connection_default
+short_description: test connection plugin used in tests
+description:
+- This is a test connection plugin used for shell testing
+author: ansible (@core)
+version_added: historical
+options:
+'''
+
+from ansible.plugins.connection import ConnectionBase
+
+
+class Connection(ConnectionBase):
+    ''' test connnection '''
+
+    transport = 'test_connection_default'
+
+    def __init__(self, *args, **kwargs):
+        super(Connection, self).__init__(*args, **kwargs)
+
+    def transport(self):
+        pass
+
+    def _connect(self):
+        pass
+
+    def exec_command(self, cmd, in_data=None, sudoable=True):
+        pass
+
+    def put_file(self, in_path, out_path):
+        pass
+
+    def fetch_file(self, in_path, out_path):
+        pass
+
+    def close(self):
+        pass
