150296418eecbb1c36cabb506945af05f09792a7
Chris Archibald <carchi8py@gmail.com>
2019-02-14 13:35:11 -0800

    fix author

--- lib/ansible/modules/storage/netapp/na_ontap_igroup_initiator.py
+++ lib/ansible/modules/storage/netapp/na_ontap_igroup_initiator.py
@@ -21,7 +21,7 @@ short_description: NetApp ONTAP igroup initiator configuration
 extends_documentation_fragment:
     - netapp.na_ontap
 version_added: '2.8'
-author: NetApp Ansible Team (carchi8py) <ng-ansibleteam@netapp.com>
+author: NetApp Ansible Team (@carchi8py) <ng-ansibleteam@netapp.com>
 
 description:
     - Add/Remove initiators from an igroup
