f43d05ebc271728041cec0a9cb3a2054a09009c9
Chris Archibald <carchi8py@gmail.com>
2019-02-14 13:43:38 -0800

    fix author

--- lib/ansible/modules/storage/netapp/na_ontap_interface.py
+++ lib/ansible/modules/storage/netapp/na_ontap_interface.py
@@ -23,7 +23,7 @@ short_description: NetApp ONTAP LIF configuration
 extends_documentation_fragment:
     - netapp.na_ontap
 version_added: '2.6'
-author: NetApp Ansible Team (ng-ansibleteam@netapp.com)
+author: NetApp Ansible Team (@carchi8py) <ng-ansibleteam@netapp.com>
 
 description:
     - Creating / deleting and modifying the LIF.
