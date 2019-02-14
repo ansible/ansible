6771b7fdd78c9f8f7f6fb0fda0858b888c7b753e
Chris Archibald <carchi8py@gmail.com>
2019-02-14 10:31:37 -0800

    updates

--- lib/ansible/modules/storage/netapp/na_ontap_unix_user.py
+++ lib/ansible/modules/storage/netapp/na_ontap_unix_user.py
@@ -20,7 +20,7 @@ short_description: NetApp ONTAP UNIX users
 extends_documentation_fragment:
     - netapp.na_ontap
 version_added: '2.8'
-author: NetApp Ansible Team (@carchi8py) <ng-ansibleteam@netapp.com>
+author: NetApp Ansible Team (ng-ansibleteam@netapp.com)
 
 description:
 - Create, delete or modify UNIX users local to ONTAP.
