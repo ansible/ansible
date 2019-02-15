1ef516bfe0c340b7ad9a739ebd4001838fca0729
Chris Archibald <carchi@netapp.com>
2019-02-15 14:03:09 -0800

    update copyright date

--- test/units/modules/storage/netapp/test_na_elementsw_cluster_snmp.py
+++ test/units/modules/storage/netapp/test_na_elementsw_cluster_snmp.py
@@ -1,3 +1,6 @@
+# (c) 2019, NetApp, Inc
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 ''' unit test for Ansible module: na_elementsw_cluster_snmp.py '''
 
 from __future__ import print_function
