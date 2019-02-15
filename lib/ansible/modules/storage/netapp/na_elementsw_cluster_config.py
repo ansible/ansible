19a52a8f5d70843a3bebccb1f597fcc6c2961c7f
Chris Archibald <carchi@netapp.com>
2019-02-15 12:23:14 -0800

    fix author

--- lib/ansible/modules/storage/netapp/na_elementsw_cluster_config.py
+++ lib/ansible/modules/storage/netapp/na_elementsw_cluster_config.py
@@ -23,7 +23,7 @@ short_description: Configure Element SW Cluster
 extends_documentation_fragment:
     - netapp.solidfire
 version_added: '2.8'
-author: NetApp Ansible Team (ng-ansibleteam@netapp.com)
+author: NetApp Ansible Team (@carchi8py) <ng-ansibleteam@netapp.com>
 description:
 - Configure Element Software cluster.
 
