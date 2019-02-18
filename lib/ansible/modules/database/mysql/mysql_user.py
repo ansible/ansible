8dff727dcb04a53417e710d3cf54db707cbd528f
Aliaksei <zelalexey@gmail.com>
2019-02-18 11:56:24 +0300

    init commit

--- lib/ansible/modules/database/mysql/mysql_user.py
+++ lib/ansible/modules/database/mysql/mysql_user.py
@@ -102,7 +102,6 @@ notes:
      the new root credentials. Subsequent runs of the playbook will then succeed by reading the new credentials from
      the file."
    - Currently, there is only support for the `mysql_native_password` encrypted password hash module.
-
 author:
 - Jonathan Mainguy (@Jmainguy)
 extends_documentation_fragment: mysql
@@ -114,20 +113,17 @@ EXAMPLES = r'''
     name: ''
     host: localhost
     state: absent
-
 - name: Removes all anonymous user accounts
   mysql_user:
     name: ''
     host_all: yes
     state: absent
-
 - name: Create database user with name 'bob' and password '12345' with all database privileges
   mysql_user:
     name: bob
     password: 12345
     priv: '*.*:ALL'
     state: present
-
 - name: Create database user using hashed password with all database privileges
   mysql_user:
     name: bob
@@ -135,14 +131,12 @@ EXAMPLES = r'''
     encrypted: yes
     priv: '*.*:ALL'
     state: present
-
 - name: Create database user with password and all database privileges and 'WITH GRANT OPTION'
   mysql_user:
     name: bob
     password: 12345
     priv: '*.*:ALL,GRANT'
     state: present
-
 # Note that REQUIRESSL is a special privilege that should only apply to *.* by itself.
 - name: Modify user to require SSL connections.
   mysql_user:
@@ -150,43 +144,36 @@ EXAMPLES = r'''
     append_privs: yes
     priv: '*.*:REQUIRESSL'
     state: present
-
 - name: Ensure no user named 'sally'@'localhost' exists, also passing in the auth credentials.
   mysql_user:
     login_user: root
     login_password: 123456
     name: sally
     state: absent
-
 - name: Ensure no user named 'sally' exists at all
   mysql_user:
     name: sally
     host_all: yes
     state: absent
-
 - name: Specify grants composed of more than one word
   mysql_user:
     name: replication
     password: 12345
     priv: "*.*:REPLICATION CLIENT"
     state: present
-
 - name: Revoke all privileges for user 'bob' and password '12345'
   mysql_user:
     name: bob
     password: 12345
     priv: "*.*:USAGE"
     state: present
-
 # Example privileges string format
 # mydb.*:INSERT,UPDATE/anotherdb.*:SELECT/yetanotherdb.*:ALL
-
 - name: Example using login_unix_socket to connect to server
   mysql_user:
     name: root
     password: abc123
     login_unix_socket: /var/run/mysqld/mysqld.sock
-
 - name: Example of skipping binary logging while adding user 'bob'
   mysql_user:
     name: bob
@@ -194,7 +181,6 @@ EXAMPLES = r'''
     priv: "*.*:USAGE"
     state: present
     sql_log_bin: no
-
 # Example .my.cnf file for setting the root password
 # [client]
 # user=root
@@ -385,15 +371,7 @@ def user_mod(cursor, user, host, host_all, password, encrypted, new_priv, append
             # and in the new privileges, then we need to see if there's a difference.
             db_table_intersect = set(new_priv.keys()) & set(curr_priv.keys())
             for db_table in db_table_intersect:
-                if "ALL" in new_priv[db_table] and db_table == "*.*":
-                    version = get_db_version(cursor)
-                    if version == "8":
-                        full_priv = {db_table: privileges_get_all(cursor)}
-                        priv_diff = set(full_priv[db_table]) ^ set(curr_priv[db_table])
-                    else:
-                        priv_diff = set(new_priv[db_table]) ^ set(curr_priv[db_table])
-                else:
-                    priv_diff = set(new_priv[db_table]) ^ set(curr_priv[db_table])
+                priv_diff = set(new_priv[db_table]) ^ set(curr_priv[db_table])
                 if len(priv_diff) > 0:
                     if module.check_mode:
                         return True
@@ -435,9 +413,7 @@ def privileges_get(cursor, user, host):
     """ MySQL doesn't have a better method of getting privileges aside from the
     SHOW GRANTS query syntax, which requires us to then parse the returned string.
     Here's an example of the string that is returned from MySQL:
-
      GRANT USAGE ON *.* TO 'user'@'localhost' IDENTIFIED BY 'pass';
-
     This function makes the query and returns a dictionary containing the results.
     The dictionary format is the same as that returned by privileges_unpack() below.
     """
@@ -453,7 +429,7 @@ def privileges_get(cursor, user, host):
 
     for grant in grants:
         res = re.match(
-            """GRANT (.+) ON (.+) TO (['`"]).*\\3@(['`"]).*\\4( IDENTIFIED BY PASSWORD (['`"]).+\\6)? ?(.*)""", grant[0])
+            """GRANT (.+) ON (.+) TO (['`"]).*\\3@(['`"]).*\\4( IDENTIFIED BY PASSWORD (['`"]).+\5)? ?(.*)""", grant[0])
         if res is None:
             raise InvalidPrivsError('unable to parse the MySQL grant string: %s' % grant[0])
         privileges = res.group(1).split(", ")
@@ -463,13 +439,7 @@ def privileges_get(cursor, user, host):
         if "REQUIRE SSL" in res.group(7):
             privileges.append('REQUIRESSL')
         db = res.group(2)
-        if db in output.keys() and db == '*.*':
-            privileges = res.group(1).split(",")
-            if "GRANT" in output[db]:
-                output[db].remove("GRANT")
-            output[db] += privileges
-        else:
-            output[db] = privileges
+        output[db] = privileges
     return output
 
 
@@ -494,9 +464,7 @@ def privileges_unpack(priv, mode):
     it into a dictionary, the same format as privileges_get() above. We have this
     custom format to avoid using YAML/JSON strings inside YAML playbooks. Example
     of a privileges string:
-
      mydb.*:INSERT,UPDATE/anotherdb.*:SELECT/yetanother.*:ALL
-
     The privilege USAGE stands for no privileges, so we add that in on *.* if it's
     not specified in the string, as MySQL will always provide this by default.
     """
