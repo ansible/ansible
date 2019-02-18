996a286dc9f106f0b1217803fee08f99bc968e65
Paweł Rozlach <vespian@users.noreply.github.com>
2019-02-18 18:40:51 +0100

    flatpak_remote: Handle empty output in remote_exists (#52010)
    `flatpak remote-list -d` can return an empty output on Fedora 29 (version 1.2.0).

--- lib/ansible/modules/packaging/os/flatpak_remote.py
+++ lib/ansible/modules/packaging/os/flatpak_remote.py
@@ -169,6 +169,8 @@ def remote_exists(module, binary, name, method):
     output = _flatpak_command(module, False, command)
     for line in output.splitlines():
         listed_remote = line.split()
+        if len(listed_remote) == 0:
+            continue
         if listed_remote[0] == to_native(name):
             return True
     return False
