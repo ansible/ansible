c3a1a84301f0d7f9c4cbc43b3cec95196828e20b
dpassante <david.passante@orange.com>
2019-02-18 18:46:30 +0100

    cloudstack: streamline modules doc (first 5)

--- lib/ansible/modules/cloud/cloudstack/cs_configuration.py
+++ lib/ansible/modules/cloud/cloudstack/cs_configuration.py
@@ -2,21 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # (c) 2016, René Moser <mail@renemoser.net>
-#
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['stableinterface'],
@@ -29,34 +15,41 @@ module: cs_configuration
 short_description: Manages configuration on Apache CloudStack based clouds.
 description:
     - Manages global, zone, account, storage and cluster configurations.
-version_added: "2.1"
-author: "René Moser (@resmo)"
+version_added: '2.1'
+author: René Moser (@resmo)
 options:
   name:
     description:
       - Name of the configuration.
+    type: str
     required: true
   value:
     description:
       - Value of the configuration.
+    type: str
     required: true
   account:
     description:
       - Ensure the value for corresponding account.
+    type: str
   domain:
     description:
       - Domain the account is related to.
-      - Only considered if C(account) is used.
+      - Only considered if I(account) is used.
+    type: str
     default: ROOT
   zone:
     description:
       - Ensure the value for corresponding zone.
+    type: str
   storage:
     description:
       - Ensure the value for corresponding storage pool.
+    type: str
   cluster:
     description:
       - Ensure the value for corresponding cluster.
+    type: str
 extends_documentation_fragment: cloudstack
 '''
 
