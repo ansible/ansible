f7ca741f8755a20392bfa0bdac465cbfddfff73a
Dag Wieers <dag@wieers.com>
2019-02-19 21:44:41 +0100

    keycloak: Clean up parameter types (#52542)
    This PR includes:
    - Parameter types added
    - Copyright format fixes
    - Short license statement
    
    This breaks out PR #52182

--- lib/ansible/plugins/doc_fragments/keycloak.py
+++ lib/ansible/plugins/doc_fragments/keycloak.py
@@ -1,29 +1,18 @@
-# Copyright (c) 2017 Eike Frost <ei@kefro.st>
-#
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2017, Eike Frost <ei@kefro.st>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 
 class ModuleDocFragment(object):
 
     # Standard documentation fragment
-    DOCUMENTATION = '''
+    DOCUMENTATION = r'''
 options:
     auth_keycloak_url:
         description:
             - URL to the Keycloak instance.
+        type: str
         required: true
         aliases:
           - url
@@ -31,21 +20,25 @@ options:
     auth_client_id:
         description:
             - OpenID Connect I(client_id) to authenticate to the API with.
+        type: str
         default: admin-cli
         required: true
 
     auth_realm:
         description:
             - Keycloak realm name to authenticate to for API access.
+        type: str
         required: true
 
     auth_client_secret:
         description:
             - Client Secret to use in conjunction with I(auth_client_id) (if required).
+        type: str
 
     auth_username:
         description:
             - Username to authenticate for API access with.
+        type: str
         required: true
         aliases:
           - username
@@ -53,6 +46,7 @@ options:
     auth_password:
         description:
             - Password to authenticate for API access with.
+        type: str
         required: true
         aliases:
           - password
@@ -60,6 +54,6 @@ options:
     validate_certs:
         description:
             - Verify TLS certificates (do not disable this in production).
-        default: True
         type: bool
+        default: yes
 '''
