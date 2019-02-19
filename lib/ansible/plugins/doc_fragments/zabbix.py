40eb3e625c31e2ed7204c69a1d7aa9420d8508e5
Dag Wieers <dag@wieers.com>
2019-02-19 21:56:58 +0100

    zabbix: Clean up parameter types (#52516)
    This PR includes:
    - Parameter types added
    - Copyright format fixes
    - Short license statement

--- lib/ansible/plugins/doc_fragments/zabbix.py
+++ lib/ansible/plugins/doc_fragments/zabbix.py
@@ -1,56 +1,51 @@
-# Copyright (c) 2017 Ansible, Inc
-#
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2017, Ansible, Inc
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 
 class ModuleDocFragment(object):
 
     # Standard documentation fragment
-    DOCUMENTATION = '''
+    DOCUMENTATION = r'''
 options:
     server_url:
         description:
             - URL of Zabbix server, with protocol (http or https).
               C(url) is an alias for C(server_url).
         required: true
-        aliases: [ "url" ]
+        type: str
+        aliases: [ url ]
     login_user:
         description:
             - Zabbix user name.
+        type: str
         required: true
     login_password:
         description:
             - Zabbix user password.
+        type: str
         required: true
     http_login_user:
         description:
             - Basic Auth login
+        type: str
+        required: true
         version_added: "2.1"
     http_login_password:
         description:
             - Basic Auth password
+        type: str
         version_added: "2.1"
     timeout:
         description:
             - The timeout of API request (seconds).
+        type: int
         default: 10
     validate_certs:
       description:
        - If set to False, SSL certificates will not be validated. This should only be used on personally controlled sites using self-signed certificates.
       type: bool
-      default: 'yes'
+      default: yes
       version_added: "2.5"
 '''
