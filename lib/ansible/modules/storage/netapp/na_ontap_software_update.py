14d2f05735b7aea290821fa67af27fd19021648c
Chris Archibald <carchi@netapp.com>
2019-02-19 06:33:49 -0800

    Allow to to upgrade more than 1 node at a time.  (#50780)
    * Updates to software update
    
    * remove ip address
    
    * Fix version
    
    * Update author line
    
    * Review comment

--- lib/ansible/modules/storage/netapp/na_ontap_software_update.py
+++ lib/ansible/modules/storage/netapp/na_ontap_software_update.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# (c) 2018, NetApp, Inc
+# (c) 2018-2019, NetApp, Inc
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
@@ -14,6 +14,7 @@ DOCUMENTATION = '''
 author: NetApp Ansible Team (@carchi8py) <ng-ansibleteam@netapp.com>
 description:
   - Update ONTAP software
+  - Requires an https connection and is not supported over http
 extends_documentation_fragment:
   - netapp.na_ontap
 module: na_ontap_software_update
@@ -23,17 +24,19 @@ options:
     description:
       - Whether the specified ONTAP package should update or not.
     default: present
-  node:
+  nodes:
     description:
       - List of nodes to be updated, the nodes have to be a part of a HA Pair.
+    aliases:
+      - node
   package_version:
     required: true
     description:
-      - Specifies the package version.
+      - Specifies the package version to update software.
   package_url:
     required: true
     description:
-      - Specifies the package URL.
+      - Specifies the package URL to download the package.
   ignore_validation_warning:
     description:
       - Allows the update to continue if warnings are encountered during the validation phase.
@@ -48,7 +51,7 @@ EXAMPLES = """
     - name: ONTAP software update
       na_ontap_software_update:
         state: present
-        node: laurentn-vsim1
+        nodes: vsim1
         package_url: "{{ url }}"
         package_version: "{{ version_name }}"
         ignore_validation_warning: True
@@ -76,11 +79,10 @@ class NetAppONTAPSoftwareUpdate(object):
     """
 
     def __init__(self):
-
         self.argument_spec = netapp_utils.na_ontap_host_argument_spec()
         self.argument_spec.update(dict(
             state=dict(required=False, type='str', choices=['present', 'absent'], default='present'),
-            node=dict(required=False, type='list'),
+            nodes=dict(required=False, type='list', aliases=["node"]),
             package_version=dict(required=True, type='str'),
             package_url=dict(required=True, type='str'),
             ignore_validation_warning=dict(required=False, type='bool', default=False)
@@ -120,8 +122,8 @@ class NetAppONTAPSoftwareUpdate(object):
         try:
             result = self.server.invoke_successfully(cluster_image_get_iter, enable_tunneling=True)
         except netapp_utils.zapi.NaApiError as error:
-            self.module.fail_json(msg='Error fetching cluster image details for %s: %s'
-                                      % (self.parameters['node'], to_native(error)),
+            self.module.fail_json(msg='Error fetching cluster image details: %s: %s'
+                                      % (self.parameters['package_version'], to_native(error)),
                                   exception=traceback.format_exc())
         # return cluster image details
         if result.get_child_by_name('num-records') and \
@@ -129,10 +131,47 @@ class NetAppONTAPSoftwareUpdate(object):
             return True
         return None
 
+    def cluster_image_get_for_node(self, node_name):
+        """
+        Get current cluster image info for given node
+        """
+        cluster_image_get = netapp_utils.zapi.NaElement('cluster-image-get')
+        cluster_image_get.add_new_child('node-id', node_name)
+        try:
+            self.server.invoke_successfully(cluster_image_get, enable_tunneling=True)
+        except netapp_utils.zapi.NaApiError as error:
+            self.module.fail_json(msg='Error fetching cluster image details for %s: %s'
+                                      % (node_name, to_native(error)),
+                                  exception=traceback.format_exc())
+
+    def cluster_image_update_progress_get(self):
+        """
+        Get current cluster image update progress info
+        :return: Dictionary of cluster image update progress if query successful, else return None
+        """
+        cluster_update_progress_get = netapp_utils.zapi.NaElement('cluster-image-update-progress-info')
+        cluster_update_progress_info = dict()
+        try:
+            result = self.server.invoke_successfully(cluster_update_progress_get, enable_tunneling=True)
+        except netapp_utils.zapi.NaApiError as error:
+            # return empty dict on error to satisfy package delete upon image update
+            if to_native(error.code) == 'Unexpected error' and self.parameters.get('https') is True:
+                return cluster_update_progress_info
+            else:
+                self.module.fail_json(msg='Error fetching cluster image update progress details: %s'
+                                          % (to_native(error)),
+                                      exception=traceback.format_exc())
+        # return cluster image update progress details
+        if result.get_child_by_name('attributes').get_child_by_name('ndu-progress-info'):
+            update_progress_info = result.get_child_by_name('attributes').get_child_by_name('ndu-progress-info')
+            cluster_update_progress_info['overall_status'] = update_progress_info.get_child_content('overall-status')
+            cluster_update_progress_info['completed_node_count'] = update_progress_info.\
+                get_child_content('completed-node-count')
+        return cluster_update_progress_info
+
     def cluster_image_update(self):
         """
         Update current cluster image
-        :return: None
         """
         cluster_update_info = netapp_utils.zapi.NaElement('cluster-image-update')
         cluster_update_info.add_new_child('package-version', self.parameters['package_version'])
@@ -140,7 +179,7 @@ class NetAppONTAPSoftwareUpdate(object):
                                           str(self.parameters['ignore_validation_warning']))
         if self.parameters.get('nodes'):
             cluster_nodes = netapp_utils.zapi.NaElement('nodes')
-            for node in self.parameters['node']:
+            for node in self.parameters['nodes']:
                 cluster_nodes.add_new_child('node-name', node)
             cluster_update_info.add_child_elem(cluster_nodes)
         try:
@@ -169,6 +208,19 @@ class NetAppONTAPSoftwareUpdate(object):
                                       exception=traceback.format_exc())
         return False
 
+    def cluster_image_package_delete(self):
+        """
+        Delete current cluster image package
+        """
+        cluster_image_package_delete_info = netapp_utils.zapi.NaElement('cluster-image-package-delete')
+        cluster_image_package_delete_info.add_new_child('package-version', self.parameters['package_version'])
+        try:
+            self.server.invoke_successfully(cluster_image_package_delete_info, enable_tunneling=True)
+        except netapp_utils.zapi.NaApiError as error:
+            self.module.fail_json(msg='Error deleting cluster image package for %s: %s'
+                                      % (self.parameters['package_version'], to_native(error)),
+                                  exception=traceback.format_exc())
+
     def cluster_image_package_download_progress(self):
         """
         Get current cluster image package download progress
@@ -192,15 +244,27 @@ class NetAppONTAPSoftwareUpdate(object):
             return cluster_download_progress_info
         return None
 
+    def autosupport_log(self):
+        """
+        Autosupport log for software_update
+        :return:
+        """
+        results = netapp_utils.get_cserver(self.server)
+        cserver = netapp_utils.setup_na_ontap_zapi(module=self.module, vserver=results)
+        netapp_utils.ems_log_event("na_ontap_software_update", cserver)
+
     def apply(self):
         """
         Apply action to update ONTAP software
         """
+        if self.parameters.get('https') is not True:
+            self.module.fail_json(msg='https parameter must be True')
         changed = False
+        self.autosupport_log()
         current = self.cluster_image_get()
-        results = netapp_utils.get_cserver(self.server)
-        cserver = netapp_utils.setup_ontap_zapi(module=self.module, vserver=results)
-        netapp_utils.ems_log_event("na_ontap_software_update", cserver)
+        if self.parameters.get('nodes'):
+            for node in self.parameters['nodes']:
+                self.cluster_image_get_for_node(node)
         if self.parameters.get('state') == 'present' and current:
             package_exists = self.cluster_image_package_download()
             if package_exists is False:
@@ -217,6 +281,13 @@ class NetAppONTAPSoftwareUpdate(object):
             else:
                 self.cluster_image_update()
                 changed = True
+            # delete package once update is completed
+            cluster_update_progress = self.cluster_image_update_progress_get()
+            while not cluster_update_progress or cluster_update_progress.get('overall_status') == 'in_progress':
+                time.sleep(25)
+                cluster_update_progress = self.cluster_image_update_progress_get()
+            if cluster_update_progress.get('overall_status') == 'completed':
+                self.cluster_image_package_delete()
         self.module.exit_json(changed=changed)
 
 
