25a7cc375988f7187c9130ee564b5bf629319a9c
Dag Wieers <dag@wieers.com>
2019-02-19 21:23:26 +0100

    doc_fragments: Clean up parameter types (other) (#52182)
    This PR includes:
    - Parameter types added
    - Copyright format fixes
    - Short license statement

--- lib/ansible/plugins/doc_fragments/constructed.py
+++ lib/ansible/plugins/doc_fragments/constructed.py
@@ -1,28 +1,30 @@
-# Copyright (c) 2017 Ansible Project
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2017, Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 
 class ModuleDocFragment(object):
 
-    DOCUMENTATION = """
+    DOCUMENTATION = r'''
 options:
   strict:
     description:
-        - If true make invalid entries a fatal error, otherwise skip and continue
+        - If C(yes) make invalid entries a fatal error, otherwise skip and continue.
         - Since it is possible to use facts in the expressions they might not always be available
           and we ignore those errors by default.
-    type: boolean
-    default: False
+    type: bool
+    default: no
   compose:
-    description: create vars from jinja2 expressions
-    type: dictionary
+    description: Create vars from jinja2 expressions.
+    type: dict
     default: {}
   groups:
-    description: add hosts to group based on Jinja2 conditionals
-    type: dictionary
+    description: Add hosts to group based on Jinja2 conditionals.
+    type: dict
     default: {}
   keyed_groups:
-    description: add hosts to group based on the values of a variable
+    description: Add hosts to group based on the values of a variable.
     type: list
     default: []
-"""
+'''
--- lib/ansible/plugins/doc_fragments/default_callback.py
+++ lib/ansible/plugins/doc_fragments/default_callback.py
@@ -1,51 +1,53 @@
-# (c) 2017 Ansible Project
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2017, Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 
 class ModuleDocFragment(object):
 
-    DOCUMENTATION = """
+    DOCUMENTATION = r'''
     options:
       display_skipped_hosts:
         name: Show skipped hosts
         description: "Toggle to control displaying skipped task/host results in a task"
-        default: True
+        type: bool
+        default: yes
         env:
           - name: DISPLAY_SKIPPED_HOSTS
         ini:
           - key: display_skipped_hosts
             section: defaults
-        type: boolean
       display_ok_hosts:
         name: Show 'ok' hosts
         description: "Toggle to control displaying 'ok' task/host results in a task"
-        default: True
+        type: bool
+        default: yes
         env:
           - name: ANSIBLE_DISPLAY_OK_HOSTS
         ini:
           - key: display_ok_hosts
             section: defaults
-        type: boolean
         version_added: '2.7'
       display_failed_stderr:
         name: Use STDERR for failed and unreachable tasks
         description: "Toggle to control whether failed and unreachable tasks are displayed to STDERR (vs. STDOUT)"
-        default: False
+        type: bool
+        default: no
         env:
           - name: ANSIBLE_DISPLAY_FAILED_STDERR
         ini:
           - key: display_failed_stderr
             section: defaults
-        type: boolean
         version_added: '2.7'
       show_custom_stats:
         name: Show custom stats
         description: 'This adds the custom stats set via the set_stats plugin to the play recap'
-        default: False
+        type: bool
+        default: no
         env:
           - name: ANSIBLE_SHOW_CUSTOM_STATS
         ini:
           - key: show_custom_stats
             section: defaults
-        type: bool
-"""
+'''
