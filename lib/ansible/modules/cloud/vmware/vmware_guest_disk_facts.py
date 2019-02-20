d74f5cd29d7edcea5a0d771c28f40ef3d34d5fb2
William Leemans <bushvin@users.noreply.github.com>
2019-02-20 20:46:46 +0100

    Per the request of my employer
    Per the request of my employer. No other changes required.

--- lib/ansible/modules/cloud/vmware/vmware_guest_disk_facts.py
+++ lib/ansible/modules/cloud/vmware/vmware_guest_disk_facts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2019, William Leemans (@bushvin) <willie@elaba.net>
+# Copyright: (c) 2019, NAER William Leemans (@bushvin) <willie@elaba.net>
 # Copyright: (c) 2018, Ansible Project
 # Copyright: (c) 2018, Abhijeet Kasurde <akasurde@redhat.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
