b1e6a14db2c7363a6d2fab1d7888d428011d2106
Dag Wieers <dag@wieers.com>
2019-02-21 08:37:42 +0100

    Update lib/ansible/module_utils/satellite.py
    Add coding.
    
    Co-Authored-By: stroobl <stroobl@users.noreply.github.com>

--- lib/ansible/module_utils/satellite.py
+++ lib/ansible/module_utils/satellite.py
@@ -1,4 +1,6 @@
-# (c) 2018, Luc Stroobant (luc.stroobant@wdc.com)
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2018, Luc Stroobant <luc.stroobant@wdc.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
