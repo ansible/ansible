640ac5ec47a71892414fa6eaa32866cf70ecff18
Hannes Ljungberg <hannes@5monkeys.se>
2019-02-21 12:47:06 +0100

    docker_swarm_service: Allow passing period strings on relevant options (#52530)
    * Allow duration to be passed as duration strings
    
    * Remove whitespace
    
    * Add changelog fragment
    
    * Fix broken test
    
    * Better error handling

--- test/units/modules/cloud/docker/test_docker_swarm_service.py
+++ test/units/modules/cloud/docker/test_docker_swarm_service.py
@@ -98,3 +98,17 @@ def test_get_docker_environment(mocker, docker_swarm_service):
         None, env_files=[]
     )
     assert result == []
+
+
+def test_get_nanoseconds_from_raw_option(docker_swarm_service):
+    value = docker_swarm_service.get_nanoseconds_from_raw_option('test', None)
+    assert value is None
+
+    value = docker_swarm_service.get_nanoseconds_from_raw_option('test', '1m30s535ms')
+    assert value == 90535000000
+
+    value = docker_swarm_service.get_nanoseconds_from_raw_option('test', 10000000000)
+    assert value == 10000000000
+
+    with pytest.raises(ValueError):
+        docker_swarm_service.get_nanoseconds_from_raw_option('test', [])
