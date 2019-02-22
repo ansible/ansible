f8e0d49e16542b4f827d6788fca64b77dfb8727b
Matt Martz <matt@sivel.net>
2019-02-22 10:44:38 -0600

    Don't worry with type changes, just compare strings (#52827)

--- test/sanity/validate-modules/main.py
+++ test/sanity/validate-modules/main.py
@@ -1098,7 +1098,7 @@ class ModuleValidator(Validator):
                 )
                 return
 
-        if existing_doc and version_added_raw != existing_doc.get('version_added'):
+        if existing_doc and str(version_added_raw) != str(existing_doc.get('version_added')):
             self.reporter.error(
                 path=self.object_path,
                 code=307,
