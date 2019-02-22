697b3d4fc1bc58271e59cc54a21eb4b47be58c10
Miguel Angel Muñoz González <magonzalez@fortinet.com>
2019-02-22 19:52:00 +0100

    Triggering Ansible bot due to timeout

--- lib/ansible/modules/network/fortios/fortios_web_proxy_explicit.py
+++ lib/ansible/modules/network/fortios/fortios_web_proxy_explicit.py
@@ -8,7 +8,7 @@ from __future__ import (absolute_import, division, print_function)
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# but WITHOUT ANY WARRANTY, without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
