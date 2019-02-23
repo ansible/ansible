93372d0d89ef7fb9ed35129e1d8c59d1bae2e99c
Mahesh Paolini-Subramanya <mahesh@dieswaytoofast.com>
2019-02-23 12:03:24 -0500

    Update elb_target_group.py
    `successful_response_codes` only gets set when the `health_check_protocol` parameter is set (per the code at https://github.com/ansible/ansible/blob/a54b9487307243d6716685f15794da110e6e5d96/lib/ansible/modules/cloud/amazon/elb_target_group.py#L429-L436
    
    +label: docsite_pr

--- lib/ansible/modules/cloud/amazon/elb_target_group.py
+++ lib/ansible/modules/cloud/amazon/elb_target_group.py
@@ -100,7 +100,7 @@ options:
   successful_response_codes:
     description:
       - The HTTP codes to use when checking for a successful response from a target. You can specify multiple values (for example, "200,202") or a range of
-        values (for example, "200-299").
+        values (for example, "200-299"). Requires the I(health_check_protocol) parameter to be set.
     required: false
   tags:
     description:
