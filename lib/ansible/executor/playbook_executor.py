d0ef2ceaff7e3957e21928347d6c17a2fef9f608
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-02-23 02:16:21 +0100

    Drop duplicate import from playbook executor

--- lib/ansible/executor/playbook_executor.py
+++ lib/ansible/executor/playbook_executor.py
@@ -28,7 +28,6 @@ from ansible.module_utils._text import to_native, to_text
 from ansible.plugins.loader import become_loader, connection_loader, shell_loader
 from ansible.playbook import Playbook
 from ansible.template import Templar
-from ansible.plugins.loader import connection_loader, shell_loader
 from ansible.utils.helpers import pct_to_int
 from ansible.module_utils.parsing.convert_bool import boolean
 from ansible.utils.path import makedirs_safe
