428e0bc539751bee633cc2f84e29cc63bc4962d3
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-02-26 00:20:25 +0100

    ✅ Add vault-encrypted ssh-key flow to tests

--- test/integration/targets/connection_ssh/runme.sh
+++ test/integration/targets/connection_ssh/runme.sh
@@ -2,9 +2,27 @@
 
 set -euxo pipefail
 
+ANSIBLE_VAULT_PASS_FILE=vault_pass
+SSH_PRIVATE_KEY_FILE=rsa_key
+
+VAULT_PASS_ARG=--vault-password-file=../connection_ssh/"${ANSIBLE_VAULT_PASS_FILE}"
+export ANSIBLE_PRIVATE_KEY_FILE=../connection_ssh/"${SSH_PRIVATE_KEY_FILE}"
+
+# generate a random alpha-numeric 64 characters long password
+( dd count=1 if=/dev/urandom | tr -cd "[:alpha:][:digit:]" | head -c 64; echo ) > "${ANSIBLE_VAULT_PASS_FILE}"
+
+# generate an SSH key with an empty password
+ssh-keygen -N "" -f "${SSH_PRIVATE_KEY_FILE}"
+
+# encrypt it
+ansible-vault encrypt "${VAULT_PASS_ARG}" "${SSH_PRIVATE_KEY_FILE}"
+
 # sftp
-./posix.sh "$@"
+./posix.sh "${VAULT_PASS_ARG}" "$@"
 # scp
-ANSIBLE_SCP_IF_SSH=true ./posix.sh "$@"
+ANSIBLE_SCP_IF_SSH=true ./posix.sh "${VAULT_PASS_ARG}" "$@"
 # piped
-ANSIBLE_SSH_TRANSFER_METHOD=piped ./posix.sh "$@"
+ANSIBLE_SSH_TRANSFER_METHOD=piped ./posix.sh "${VAULT_PASS_ARG}" "$@"
+
+# cleanup
+rm -fv "${SSH_PRIVATE_KEY_FILE}" "${ANSIBLE_VAULT_PASS_FILE}"
