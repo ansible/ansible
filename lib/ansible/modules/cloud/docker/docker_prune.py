bcad5c4e2e7c699d5046afbd16410cb2911a279b
Nicolas Singh Oteiza <nicolas.singh@gmail.com>
2019-02-25 05:15:15 -0300

    Add docker_prune sample to remove everything, including non-dangling images (#52744)
    * Update docker_prune.py
    
    ##### SUMMARY
    Add example to remove everything, including non-dangling images
    
    
    ##### ISSUE TYPE
    - Docs Pull Request
    
    +label: docsite_pr
    
    * fix indentation
    
    * Update lib/ansible/modules/cloud/docker/docker_prune.py
    
    Co-Authored-By: nicosingh <nicolas.singh@gmail.com>

--- lib/ansible/modules/cloud/docker/docker_prune.py
+++ lib/ansible/modules/cloud/docker/docker_prune.py
@@ -105,6 +105,16 @@ EXAMPLES = '''
     networks: yes
     volumes: yes
     builder_cache: yes
+
+- name: Prune everything (including non-dangling images)
+  docker_prune:
+    containers: yes
+    images: yes
+    images_filters:
+      dangling: false
+    networks: yes
+    volumes: yes
+    builder_cache: yes
 '''
 
 RETURN = '''
