c703ca1384fdf9c1701fa218c02e5883f2f4043a
Or Soffer <orso@checkpoint.com>
2019-02-25 18:31:25 +0200

    checkpoint module utols and service tcp module for example

--- lib/ansible/module_utils/network/checkpoint/checkpoint.py
+++ lib/ansible/module_utils/network/checkpoint/checkpoint.py
@@ -26,6 +26,10 @@
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+from __future__ import (absolute_import, division, print_function)
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.connection import Connection
+
 
 checkpoint_argument_spec = dict(auto_publish_session=dict(type='bool', default=True),
                                 policy_package=dict(type='str', default='standard'),
@@ -57,3 +61,108 @@ def install_policy(connection, policy_package, targets):
                'targets': targets}
 
     connection.send_request('/web_api/install-policy', payload)
+
+
+def get_api_call_object(connection, api_call_object, parameters):
+    code, response = connection.send_request('/web_api/show-' + api_call_object, parameters)
+
+    return code, response
+
+
+def create_api_call_object(connection, api_call_object, parameters):
+    code, response = connection.send_request('/web_api/add-' + api_call_object, parameters)
+
+    return code, response
+
+
+def update_api_call_object(connection, api_call_object, parameters):
+    code, response = connection.send_request('/web_api/set-' + api_call_object, parameters)
+
+    return code, response
+
+
+def delete_api_call_object(connection, api_call_object, parameters):
+    code, response = connection.send_request('/web_api/delete-' + api_call_object, parameters)
+
+    return code, response
+
+
+def needs_update(parameters, api_call_object):
+    for key, value in parameters.items():
+        if value != api_call_object[key]:
+            return True
+
+    # if module.params['source'] and module.params['source'] != api_call_object['source'][0]['name']:
+    #     res = True
+    # if module.params['destination'] and module.params['destination'] != api_call_object['destination'][0]['name']:
+    #     res = True
+    # if module.params['action'] != api_call_object['action']['name']:
+    #     res = True
+    # if module.params['enabled'] != api_call_object['enabled']:
+    #     res = True
+
+    return False
+
+
+def api_call_facts(argument_spec):
+    module = AnsibleModule(argument_spec=argument_spec)
+    connection = Connection(module._socket_path)
+    api_call_object = module.params.get("api_call_object")
+    parameters = module.params.get("parameters")
+    code, response = get_api_call_object(connection, api_call_object, parameters)
+    if code == 200:
+        module.exit_json(ansible_facts=dict(checkpoint_api_call_object=response))
+    else:
+        module.fail_json(msg='Checkpoint device returned error {0} with message {1}'.format(code, response))
+
+
+def api_call(argument_spec):
+    argument_spec.update(checkpoint_argument_spec)
+    module = AnsibleModule(argument_spec=argument_spec)
+    connection = Connection(module._socket_path)
+    api_call_object = module.params.get("api_call_object")
+    parameters = module.params.get("parameters")
+    code, response = get_api_call_object(connection, api_call_object, parameters)
+    result = {'changed': False}
+
+    if module.params['state'] == 'present':
+        if code == 200:
+            if needs_update(parameters, response):
+                code, response = update_api_call_object(connection, api_call_object, parameters)
+                if module.params['auto_publish_session']:
+                    publish(connection)
+
+                    if module.params['auto_install_policy']:
+                        install_policy(connection, module.params['policy_package'], module.params['targets'])
+
+                result['changed'] = True
+                result['checkpoint_access_rules'] = response
+            else:
+                pass
+        elif code == 404:
+            code, response = create_api_call_object(connection, api_call_object, parameters)
+
+            if module.params['auto_publish_session']:
+                publish(connection)
+
+                if module.params['auto_install_policy']:
+                    install_policy(connection, module.params['policy_package'], module.params['targets'])
+
+            result['changed'] = True
+            result['checkpoint_access_rules'] = response
+    else:
+        if code == 200:
+            code, response = delete_api_call_object(connection, api_call_object, parameters)
+
+            if module.params['auto_publish_session']:
+                publish(connection)
+
+                if module.params['auto_install_policy']:
+                    install_policy(connection, module.params['policy_package'], module.params['targets'])
+
+            result['changed'] = True
+        elif code == 404:
+            pass
+
+    result['checkpoint_session_uid'] = connection.get_session_uid()
+    module.exit_json(**result)
