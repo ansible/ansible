7c6f1f717c9b479843f7e39741612750d941e899
Chris Archibald <carchi@netapp.com>
2019-02-26 10:53:25 -0800

    Revert "documentation changes"
    This reverts commit 02c369d0414fdff492d90865c903bdade3174261.

--- lib/ansible/modules/storage/netapp/na_ontap_ntp.py
+++ lib/ansible/modules/storage/netapp/na_ontap_ntp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# (c) 2018-2019, NetApp, Inc
+# (c) 2018, NetApp, Inc
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
@@ -43,14 +43,12 @@ EXAMPLES = """
       na_ontap_ntp:
         state: present
         version: auto
-        server_name: "{{ server_name }}"
         hostname: "{{ netapp_hostname }}"
         username: "{{ netapp_username }}"
         password: "{{ netapp_password }}"
     - name: Delete NTP server
       na_ontap_ntp:
         state: absent
-        server_name: "{{ server_name }}"
         hostname: "{{ netapp_hostname }}"
         username: "{{ netapp_username }}"
         password: "{{ netapp_password }}"
