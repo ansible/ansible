076562e5c2c690ecd2d4ecccb0a1da370ef60420
Dag Wieers <dag@wieers.com>
2019-02-26 14:55:40 +0100

    ACI: Various changes and doc updates (#52957)
    * ACI: Various changes and doc updates
    
    * Fix typos

--- lib/ansible/modules/network/aci/aci_vrf.py
+++ lib/ansible/modules/network/aci/aci_vrf.py
@@ -17,16 +17,6 @@ short_description: Manage contexts or VRFs (fv:Ctx)
 description:
 - Manage contexts or VRFs on Cisco ACI fabrics.
 - Each context is a private network associated to a tenant, i.e. VRF.
-notes:
-- The C(tenant) used must exist before using this module in your playbook.
-  The M(aci_tenant) module can be used for this.
-seealso:
-- module: aci_tenant
-- name: APIC Management Information Model reference
-  description: More information about the internal APIC class B(fv:Ctx).
-  link: https://developer.cisco.com/docs/apic-mim-ref/
-author:
-- Jacob McGill (@jmcgill298)
 version_added: '2.4'
 options:
   tenant:
@@ -62,6 +52,16 @@ options:
     choices: [ absent, present, query ]
     default: present
 extends_documentation_fragment: aci
+notes:
+- The C(tenant) used must exist before using this module in your playbook.
+  The M(aci_tenant) module can be used for this.
+seealso:
+- module: aci_tenant
+- name: APIC Management Information Model reference
+  description: More information about the internal APIC class B(fv:Ctx).
+  link: https://developer.cisco.com/docs/apic-mim-ref/
+author:
+- Jacob McGill (@jmcgill298)
 '''
 
 EXAMPLES = r'''
@@ -214,19 +214,19 @@ url:
   sample: https://10.11.12.13/api/mo/uni/tn-production.json
 '''
 
-from ansible.module_utils.network.aci.aci import ACIModule, aci_argument_spec
 from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.network.aci.aci import ACIModule, aci_argument_spec
 
 
 def main():
     argument_spec = aci_argument_spec()
     argument_spec.update(
+        tenant=dict(type='str', aliases=['tenant_name']),  # Not required for querying all objects
+        vrf=dict(type='str', aliases=['context', 'name', 'vrf_name']),  # Not required for querying all objects
         description=dict(type='str', aliases=['descr']),
-        policy_control_direction=dict(choices=['ingress', 'egress'], type='str'),
-        policy_control_preference=dict(choices=['enforced', 'unenforced'], type='str'),
-        state=dict(choices=['absent', 'present', 'query'], type='str', default='present'),
-        tenant=dict(type='str', required=False, aliases=['tenant_name']),  # Not required for querying all objects
-        vrf=dict(type='str', required=False, aliases=['context', 'name', 'vrf_name']),  # Not required for querying all objects
+        policy_control_direction=dict(type='str', choices=['egress', 'ingress']),
+        policy_control_preference=dict(type='str', choices=['enforced', 'unenforced']),
+        state=dict(type='str', default='present', choices=['absent', 'present', 'query']),
     )
 
     module = AnsibleModule(
