d5755559a06cc7213fd8cbe7c36dd9ff66b18ff9
Felix Fontein <felix@fontein.de>
2019-02-26 08:31:05 +0100

    Improve idempotency checking: only consider parameters which are part of the generated spec.

--- lib/ansible/modules/cloud/docker/docker_swarm.py
+++ lib/ansible/modules/cloud/docker/docker_swarm.py
@@ -349,7 +349,8 @@ class TaskParameters(DockerBaseClass):
 
     def compare_to_active(self, other, differences):
         for k in self.__dict__:
-            if k in ('advertise_addr', 'listen_addr', 'rotate_worker_token', 'rotate_manager_token', 'spec'):
+            if k in ('advertise_addr', 'listen_addr', 'force_new_cluster', 'remote_addrs',
+                     'join_token', 'force', 'rotate_worker_token', 'rotate_manager_token', 'spec'):
                 continue
             if self.__dict__[k] is None:
                 continue
