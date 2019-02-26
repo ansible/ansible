bd8013b5a1a55b0a866c377d517dd68472389e8f
John R Barker <john@johnrbarker.com>
2019-02-26 11:33:40 -0600

    Update lib/ansible/modules/identity/cyberark/cyberark_credential.py
    Co-Authored-By: JimmyJamCABD <42448656+JimmyJamCABD@users.noreply.github.com>

--- lib/ansible/modules/identity/cyberark/cyberark_credential.py
+++ lib/ansible/modules/identity/cyberark/cyberark_credential.py
@@ -57,7 +57,6 @@ options:
         description:
             - A string containing the file location and name of the private key of the client certificate used for authentication.
     reason:
-        required: False
         description:
             - Reason for requesting credential if required by policy.
     state:
