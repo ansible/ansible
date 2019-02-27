291b815a4c23bffb56be4ee18631f25fd70ab26b
Dag Wieers <dag@wieers.com>
2019-02-27 03:50:43 +0100

    Fix PEP8

--- lib/ansible/modules/net_tools/basics/uri.py
+++ lib/ansible/modules/net_tools/basics/uri.py
@@ -20,7 +20,7 @@ description:
     HTTP authentication mechanisms.
   - For Windows targets, use the M(win_uri) module instead.
 version_added: "1.1"
-requires:
+requirements:
 - xmltodict (when dealing with XML content)
 options:
   url:
@@ -316,9 +316,9 @@ import tempfile
 
 try:
     import xmltodict
-    HAS_XMLTODICT=True
+    HAS_XMLTODICT = True
 except ImportError:
-    HAS_XMLTODICT=False
+    HAS_XMLTODICT = False
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.six import PY2, iteritems, string_types
@@ -329,6 +329,7 @@ from ansible.module_utils.urls import fetch_url, url_argument_spec
 
 JSON_CANDIDATES = ('javascript', 'json')
 XML_CANDIDATES = ('xhtml', 'xml')
+
 
 def format_message(err, resp):
     msg = resp.pop('msg')
