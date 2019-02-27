b532032505fac835bbde2fcb907fc979db9a0d3e
yungez <yungez@microsoft.com>
2019-02-27 15:29:52 +0800

    fix lint

--- lib/ansible/plugins/inventory/azure_rm.py
+++ lib/ansible/plugins/inventory/azure_rm.py
@@ -162,6 +162,7 @@ from itertools import chain
 from msrest import ServiceClient, Serializer, Deserializer
 from msrestazure import AzureConfiguration
 from msrestazure.polling.arm_polling import ARMPolling
+from msrestazure.tools import parse_resource_id
 
 
 class AzureRMRestConfiguration(AzureConfiguration):
@@ -446,16 +447,6 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
         return json.loads(content)
 
 
-def azure_id_to_dict(id):
-    pieces = re.sub(r'^\/', '', id).split('/')
-    result = {}
-    index = 0
-    while index < len(pieces) - 1:
-        result[pieces[index]] = pieces[index + 1]
-        index += 1
-    return result
-
-
 # VM list (all, N resource groups): VM -> InstanceView, N NICs, N PublicIPAddress)
 # VMSS VMs (all SS, N specific SS, N resource groups?): SS -> VM -> InstanceView, N NICs, N PublicIPAddress)
 
@@ -511,12 +502,9 @@ class AzureHost(object):
                 name=self._vmss['name'],
             ) if self._vmss else {},
             virtual_machine_size=self._vm_model['properties']['hardwareProfile']['vmSize'] if self._vm_model['properties'].get('hardwareProfile') else None,
-            plan=self._vm_model['properties']['plan']['name'] if self._vm_model['properties'].get('plan') else None
+            plan=self._vm_model['properties']['plan']['name'] if self._vm_model['properties'].get('plan') else None,
+            resource_group=parse_resource_id(self._vm_model['id']).get('resource_group').lower()
         )
-
-        # set resource group
-        id_dict = azure_id_to_dict(self._vm_model['id'])
-        new_hostvars['resource_group'] = id_dict['resourceGroups'].lower()
 
         # set nic-related values from the primary NIC first
         for nic in sorted(self.nics, key=lambda n: n.is_primary, reverse=True):
@@ -536,7 +524,7 @@ class AzureHost(object):
                     if pip_fqdn:
                         new_hostvars['public_dns_hostnames'].append(pip_fqdn)
 
-            new_hostvars['mac_address'] = nic._nic_model['properties'].get('macAddress') if nic._nic_model['properties'].get('macAddress') else None
+            new_hostvars['mac_address'] = nic._nic_model['properties'].get('macAddress')
             new_hostvars['network_interface'] = nic._nic_model['name']
             new_hostvars['network_interface_id'] = nic._nic_model['id']
 
@@ -546,12 +534,18 @@ class AzureHost(object):
         storageProfile = self._vm_model['properties'].get('storageProfile')
         if storageProfile:
             imageReference = storageProfile.get('imageReference')
-            new_hostvars['image'] = dict(
-                sku=imageReference.get('sku'),
-                publisher=imageReference.get('publisher'),
-                version=imageReference.get('version'),
-                offer=imageReference.get('offer')
-            ) if imageReference else {}
+            if imageReference:
+                if imageReference.get('publisher'):
+                    new_hostvars['image'] = dict(
+                        sku=imageReference.get('sku'),
+                        publisher=imageReference.get('publisher'),
+                        version=imageReference.get('version'),
+                        offer=imageReference.get('offer')
+                    )
+                elif imageReference.get('id'):
+                    new_hostvars['image'] = dict(
+                        id=imageReference.get('id')
+                    )
 
             osDisk = storageProfile.get('osDisk')
             new_hostvars['os_disk'] = dict(
