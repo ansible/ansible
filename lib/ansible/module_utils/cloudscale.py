d57c0a71a79f66b35f4514db88c4525b46f79416
Rene Moser <mail@renemoser.net>
2019-02-27 23:41:21 +0100

    check for None

--- lib/ansible/module_utils/cloudscale.py
+++ lib/ansible/module_utils/cloudscale.py
@@ -49,7 +49,7 @@ class AnsibleCloudscaleBase(object):
 
     def _post_or_patch(self, api_call, method, data):
         headers = self._auth_header.copy()
-        if data:
+        if data is not None:
             # Sanitize data dictionary
             # Deepcopy: Duplicate the data object for iteration, because
             # iterating an object and changing it at the same time is insecure
