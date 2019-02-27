f02f87c2ca82ffe1092d97b3e9d42aac5b45132c
Matt Clay <matt@mystile.com>
2019-02-26 22:10:16 -0800

    Make Azure test env vars available as vars.

--- test/runner/lib/cloud/azure.py
+++ test/runner/lib/cloud/azure.py
@@ -144,9 +144,9 @@ class AzureCloudEnvironment(CloudEnvironment):
 
         ansible_vars = dict(
             resource_prefix=self.resource_prefix,
-            resource_group=env_vars['RESOURCE_GROUP'],
-            resource_group_secondary=env_vars['RESOURCE_GROUP_SECONDARY'],
         )
+
+        ansible_vars.update(dict((key.lower(), value) for key, value in env_vars.items()))
 
         return CloudEnvironmentConfig(
             env_vars=env_vars,
