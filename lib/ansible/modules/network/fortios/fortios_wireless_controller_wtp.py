efca41dcc5666de844b7d159a5bd90761ac7f2b1
Miguel Angel Muñoz González <magonzalez@fortinet.com>
2019-02-27 10:34:06 +0100

    Avoid using global

--- lib/ansible/modules/network/fortios/fortios_wireless_controller_wtp.py
+++ lib/ansible/modules/network/fortios/fortios_wireless_controller_wtp.py
@@ -722,10 +722,8 @@ version:
 
 from ansible.module_utils.basic import AnsibleModule
 
-fos = None
 
-
-def login(data):
+def login(data, fos):
     host = data['host']
     username = data['username']
     password = data['password']
@@ -795,7 +793,7 @@ def wireless_controller_wtp(data, fos):
 
 
 def fortios_wireless_controller(data, fos):
-    login(data)
+    login(data, fos)
 
     if data['wireless_controller_wtp']:
         resp = wireless_controller_wtp(data, fos)
@@ -996,7 +994,6 @@ def main():
     except ImportError:
         module.fail_json(msg="fortiosapi module is required")
 
-    global fos
     fos = FortiOSAPI()
 
     is_error, has_changed, result = fortios_wireless_controller(module.params, fos)
