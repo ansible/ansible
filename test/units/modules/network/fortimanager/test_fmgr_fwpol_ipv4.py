7e24bcd725ed78617b2ec218b790b9f2c29da02e
ftntcorecse <43451990+ftntcorecse@users.noreply.github.com>
2019-02-27 01:45:06 -0500

    FortiManager Plugin Module Conversion: fmgr_fwpol_ipv4 (#52785)
    * Auto Commit for: fmgr_fwpol_ipv4
    
    * Auto Commit for: fmgr_fwpol_ipv4

--- test/units/modules/network/fortimanager/test_fmgr_fwpol_ipv4.py
+++ test/units/modules/network/fortimanager/test_fmgr_fwpol_ipv4.py
@@ -19,15 +19,13 @@ __metaclass__ = type
 
 import os
 import json
-from pyFMG.fortimgr import FortiManager
+from ansible.module_utils.network.fortimanager.fortimanager import FortiManagerHandler
 import pytest
 
 try:
     from ansible.modules.network.fortimanager import fmgr_fwpol_ipv4
 except ImportError:
     pytest.skip("Could not load required modules for testing", allow_module_level=True)
-
-fmg_instance = FortiManager("1.1.1.1", "admin", "")
 
 
 def load_fixtures():
@@ -41,14 +39,30 @@ def load_fixtures():
     return [fixture_data]
 
 
+@pytest.fixture(autouse=True)
+def module_mock(mocker):
+    connection_class_mock = mocker.patch('ansible.module_utils.basic.AnsibleModule')
+    return connection_class_mock
+
+
+@pytest.fixture(autouse=True)
+def connection_mock(mocker):
+    connection_class_mock = mocker.patch('ansible.modules.network.fortimanager.fmgr_fwpol_ipv4.Connection')
+    return connection_class_mock
+
+
 @pytest.fixture(scope="function", params=load_fixtures())
 def fixture_data(request):
     func_name = request.function.__name__.replace("test_", "")
     return request.param.get(func_name, None)
 
 
-def test_fmgr_firewall_policy_addsetdelete(fixture_data, mocker):
-    mocker.patch("pyFMG.fortimgr.FortiManager._post_request", side_effect=fixture_data)
+fmg_instance = FortiManagerHandler(connection_mock, module_mock)
+
+
+def test_fmgr_firewall_policy_modify(fixture_data, mocker):
+    mocker.patch("ansible.module_utils.network.fortimanager.fortimanager.FortiManagerHandler.process_request",
+                 side_effect=fixture_data)
     #  Fixture sets used:###########################
 
     ##################################################
@@ -323,160 +337,32 @@ def test_fmgr_firewall_policy_addsetdelete(fixture_data, mocker):
     # learning-mode: None
     # session-ttl: None
     # ntlm-guest: None
-    # ips-sensor: default
-    # diffservcode-rev: None
-    # match-vip: None
-    # natip: None
-    # dlp-sensor: None
-    # traffic-shaper: None
-    # groups: None
-    # schedule-timeout: None
-    # name: Basic_IPv4_Policy_3
-    # tcp-session-without-syn: None
-    # ntlm: None
-    # permit-stun-host: None
-    # diffservcode-forward: None
-    # internet-service-src-custom: None
-    # mode: set
-    # disclaimer: None
-    # rtp-nat: None
-    # auth-cert: None
-    # timeout-send-rst: None
-    # auth-redirect-addr: None
-    # ssl-mirror-intf: None
-    # identity-based-route: None
-    # natoutbound: None
-    # wanopt-profile: None
-    # per-ip-shaper: None
-    # profile-protocol-options: None
-    # diffserv-forward: None
-    # poolname: None
-    # comments: Created by Ansible
-    # label: None
-    # global-label: None
-    # firewall-session-dirty: None
-    # wanopt: None
-    # schedule: always
-    # internet-service-id: None
-    # auth-path: None
-    # vlan-cos-fwd: None
-    # custom-log-fields: None
-    # dstintf: zone_wan1, zone_wan2
-    # srcintf: zone_int1
-    # block-notification: None
-    # internet-service-src-id: None
-    # redirect-url: None
-    # waf-profile: None
-    # ntlm-enabled-browsers: None
-    # dscp-negate: None
-    # action: accept
-    # fsso-agent-for-ntlm: None
-    # logtraffic: utm
-    # vlan-filter: None
-    # policyid: None
-    # logtraffic-start: None
-    # webcache-https: None
-    # webfilter-profile: None
-    # internet-service-src: None
-    # webcache: None
-    # utm-status: None
-    # vpn_src_node: {'subnet': None, 'host': None, 'seq': None}
-    # ippool: None
-    # service: HTTP, HTTPS
-    # wccp: None
-    # auto-asic-offload: None
-    # dscp-value: None
-    # url-category: None
-    # capture-packet: None
-    # adom: ansible
-    # inbound: None
-    # internet-service: None
-    # profile-type: None
-    # ssl-mirror: None
-    # srcaddr-negate: None
-    # gtp-profile: None
-    # mms-profile: None
-    # send-deny-packet: None
-    # devices: None
-    # permit-any-host: None
-    # av-profile: sniffer-profile
-    # internet-service-src-negate: None
-    # service-negate: None
-    # rsso: None
-    # app-group: None
-    # tcp-mss-sender: None
-    # natinbound: None
-    # fixedport: None
-    # ssl-ssh-profile: None
-    # outbound: None
-    # spamfilter-profile: None
-    # application-list: None
-    # application: None
-    # dnsfilter-profile: None
-    # nat: enable
-    # fsso: None
-    # vlan-cos-rev: None
-    # status: None
-    # dsri: None
-    # users: None
-    # voip-profile: None
-    # dstaddr-negate: None
-    # traffic-shaper-reverse: None
-    # internet-service-custom: None
-    # diffserv-reverse: None
-    # srcaddr: corp_internal
-    # ssh-filter-profile: None
-    # delay-tcp-npu-session: None
-    # icap-profile: None
-    # captive-portal-exempt: None
-    # vpn_dst_node: {'subnet': None, 'host': None, 'seq': None}
-    # app-category: None
-    # rtp-addr: None
-    # wsso: None
-    # tcp-mss-receiver: None
-    # dstaddr: google-play, autoupdate.opera.com
-    # radius-mac-auth-bypass: None
-    # vpntunnel: None
-    ##################################################
-    ##################################################
-    # package_name: default
-    # wanopt-detection: None
-    # scan-botnet-connections: None
-    # profile-group: None
-    # dlp-sensor: None
-    # dscp-match: None
-    # replacemsg-override-group: None
-    # internet-service-negate: None
-    # np-acceleration: None
-    # learning-mode: None
-    # session-ttl: None
-    # ntlm-guest: None
     # ips-sensor: None
     # diffservcode-rev: None
     # match-vip: None
     # natip: None
-    # wanopt-peer: None
+    # dlp-sensor: None
     # traffic-shaper: None
     # groups: None
     # schedule-timeout: None
     # name: Basic_IPv4_Policy
     # tcp-session-without-syn: None
-    # rtp-nat: None
+    # ntlm: None
     # permit-stun-host: None
-    # natoutbound: None
+    # diffservcode-forward: None
     # internet-service-src-custom: None
     # mode: delete
-    # logtraffic: None
-    # ntlm: None
+    # disclaimer: None
+    # rtp-nat: None
     # auth-cert: None
     # timeout-send-rst: None
     # auth-redirect-addr: None
     # ssl-mirror-intf: None
     # identity-based-route: None
-    # diffservcode-forward: None
+    # natoutbound: None
     # wanopt-profile: None
     # per-ip-shaper: None
-    # users: None
+    # profile-protocol-options: None
     # diffserv-forward: None
     # poolname: None
     # comments: None
@@ -499,9 +385,9 @@ def test_fmgr_firewall_policy_addsetdelete(fixture_data, mocker):
     # dscp-negate: None
     # action: None
     # fsso-agent-for-ntlm: None
-    # disclaimer: None
+    # logtraffic: None
     # vlan-filter: None
-    # dstaddr-negate: None
+    # policyid: 36
     # logtraffic-start: None
     # webcache-https: None
     # webfilter-profile: None
@@ -531,7 +417,6 @@ def test_fmgr_firewall_policy_addsetdelete(fixture_data, mocker):
     # internet-service-src-negate: None
     # service-negate: None
     # rsso: None
-    # application-list: None
     # app-group: None
     # tcp-mss-sender: None
     # natinbound: None
@@ -539,7 +424,7 @@ def test_fmgr_firewall_policy_addsetdelete(fixture_data, mocker):
     # ssl-ssh-profile: None
     # outbound: None
     # spamfilter-profile: None
-    # wanopt-passive-opt: None
+    # application-list: None
     # application: None
     # dnsfilter-profile: None
     # nat: None
@@ -547,14 +432,14 @@ def test_fmgr_firewall_policy_addsetdelete(fixture_data, mocker):
     # vlan-cos-rev: None
     # status: None
     # dsri: None
-    # profile-protocol-options: None
+    # users: None
     # voip-profile: None
-    # policyid: 25
+    # dstaddr-negate: None
     # traffic-shaper-reverse: None
     # internet-service-custom: None
     # diffserv-reverse: None
     # srcaddr: None
-    # dstaddr: None
+    # ssh-filter-profile: None
     # delay-tcp-npu-session: None
     # icap-profile: None
     # captive-portal-exempt: None
@@ -563,17 +448,16 @@ def test_fmgr_firewall_policy_addsetdelete(fixture_data, mocker):
     # rtp-addr: None
     # wsso: None
     # tcp-mss-receiver: None
-    # ssh-filter-profile: None
+    # dstaddr: None
     # radius-mac-auth-bypass: None
     # vpntunnel: None
     ##################################################
     ##################################################
-    # wanopt-passive-opt: None
     # package_name: default
     # wanopt-detection: None
     # scan-botnet-connections: None
     # profile-group: None
-    # wanopt-peer: None
+    # dlp-sensor: None
     # dscp-match: None
     # replacemsg-override-group: None
     # internet-service-negate: None
@@ -585,140 +469,12 @@ def test_fmgr_firewall_policy_addsetdelete(fixture_data, mocker):
     # diffservcode-rev: None
     # match-vip: None
     # natip: None
-    # dlp-sensor: None
+    # wanopt-peer: None
     # traffic-shaper: None
     # groups: None
     # schedule-timeout: None
     # name: Basic_IPv4_Policy_2
     # tcp-session-without-syn: None
-    # ntlm: None
-    # permit-stun-host: None
-    # diffservcode-forward: None
-    # internet-service-src-custom: None
-    # mode: delete
-    # disclaimer: None
-    # rtp-nat: None
-    # auth-cert: None
-    # timeout-send-rst: None
-    # auth-redirect-addr: None
-    # ssl-mirror-intf: None
-    # identity-based-route: None
-    # natoutbound: None
-    # wanopt-profile: None
-    # per-ip-shaper: None
-    # profile-protocol-options: None
-    # diffserv-forward: None
-    # poolname: None
-    # comments: None
-    # label: None
-    # global-label: None
-    # firewall-session-dirty: None
-    # wanopt: None
-    # schedule: None
-    # internet-service-id: None
-    # auth-path: None
-    # vlan-cos-fwd: None
-    # custom-log-fields: None
-    # dstintf: None
-    # srcintf: None
-    # block-notification: None
-    # internet-service-src-id: None
-    # redirect-url: None
-    # waf-profile: None
-    # ntlm-enabled-browsers: None
-    # dscp-negate: None
-    # action: None
-    # fsso-agent-for-ntlm: None
-    # logtraffic: None
-    # vlan-filter: None
-    # policyid: 26
-    # logtraffic-start: None
-    # webcache-https: None
-    # webfilter-profile: None
-    # internet-service-src: None
-    # webcache: None
-    # utm-status: None
-    # vpn_src_node: {'subnet': None, 'host': None, 'seq': None}
-    # ippool: None
-    # service: None
-    # wccp: None
-    # auto-asic-offload: None
-    # dscp-value: None
-    # url-category: None
-    # capture-packet: None
-    # adom: ansible
-    # inbound: None
-    # internet-service: None
-    # profile-type: None
-    # ssl-mirror: None
-    # srcaddr-negate: None
-    # gtp-profile: None
-    # mms-profile: None
-    # send-deny-packet: None
-    # devices: None
-    # permit-any-host: None
-    # av-profile: None
-    # internet-service-src-negate: None
-    # service-negate: None
-    # rsso: None
-    # app-group: None
-    # tcp-mss-sender: None
-    # natinbound: None
-    # fixedport: None
-    # ssl-ssh-profile: None
-    # outbound: None
-    # spamfilter-profile: None
-    # application-list: None
-    # application: None
-    # dnsfilter-profile: None
-    # nat: None
-    # fsso: None
-    # vlan-cos-rev: None
-    # status: None
-    # dsri: None
-    # users: None
-    # voip-profile: None
-    # dstaddr-negate: None
-    # traffic-shaper-reverse: None
-    # internet-service-custom: None
-    # diffserv-reverse: None
-    # srcaddr: None
-    # ssh-filter-profile: None
-    # delay-tcp-npu-session: None
-    # icap-profile: None
-    # captive-portal-exempt: None
-    # vpn_dst_node: {'subnet': None, 'host': None, 'seq': None}
-    # app-category: None
-    # rtp-addr: None
-    # wsso: None
-    # tcp-mss-receiver: None
-    # dstaddr: None
-    # radius-mac-auth-bypass: None
-    # vpntunnel: None
-    ##################################################
-    ##################################################
-    # package_name: default
-    # wanopt-detection: None
-    # scan-botnet-connections: None
-    # profile-group: None
-    # dlp-sensor: None
-    # dscp-match: None
-    # replacemsg-override-group: None
-    # internet-service-negate: None
-    # np-acceleration: None
-    # learning-mode: None
-    # session-ttl: None
-    # ntlm-guest: None
-    # ips-sensor: None
-    # diffservcode-rev: None
-    # match-vip: None
-    # natip: None
-    # wanopt-peer: None
-    # traffic-shaper: None
-    # groups: None
-    # schedule-timeout: None
-    # name: Basic_IPv4_Policy_3
-    # tcp-session-without-syn: None
     # rtp-nat: None
     # permit-stun-host: None
     # natoutbound: None
@@ -807,7 +563,7 @@ def test_fmgr_firewall_policy_addsetdelete(fixture_data, mocker):
     # dsri: None
     # profile-protocol-options: None
     # voip-profile: None
-    # policyid: 27
+    # policyid: 37
     # traffic-shaper-reverse: None
     # internet-service-custom: None
     # diffserv-reverse: None
@@ -827,20 +583,14 @@ def test_fmgr_firewall_policy_addsetdelete(fixture_data, mocker):
     ##################################################
 
     # Test using fixture 1 #
-    output = fmgr_fwpol_ipv4.fmgr_firewall_policy_addsetdelete(fmg_instance, fixture_data[0]['paramgram_used'])
+    output = fmgr_fwpol_ipv4.fmgr_firewall_policy_modify(fmg_instance, fixture_data[0]['paramgram_used'])
     assert isinstance(output['raw_response'], dict) is True
     # Test using fixture 2 #
-    output = fmgr_fwpol_ipv4.fmgr_firewall_policy_addsetdelete(fmg_instance, fixture_data[1]['paramgram_used'])
+    output = fmgr_fwpol_ipv4.fmgr_firewall_policy_modify(fmg_instance, fixture_data[1]['paramgram_used'])
     assert isinstance(output['raw_response'], dict) is True
     # Test using fixture 3 #
-    output = fmgr_fwpol_ipv4.fmgr_firewall_policy_addsetdelete(fmg_instance, fixture_data[2]['paramgram_used'])
-    assert isinstance(output['raw_response'], dict) is True
+    output = fmgr_fwpol_ipv4.fmgr_firewall_policy_modify(fmg_instance, fixture_data[2]['paramgram_used'])
+    assert output['raw_response']['status']['code'] == 0
     # Test using fixture 4 #
-    output = fmgr_fwpol_ipv4.fmgr_firewall_policy_addsetdelete(fmg_instance, fixture_data[3]['paramgram_used'])
-    assert output['raw_response']['status']['code'] == 0
-    # Test using fixture 5 #
-    output = fmgr_fwpol_ipv4.fmgr_firewall_policy_addsetdelete(fmg_instance, fixture_data[4]['paramgram_used'])
-    assert output['raw_response']['status']['code'] == 0
-    # Test using fixture 6 #
-    output = fmgr_fwpol_ipv4.fmgr_firewall_policy_addsetdelete(fmg_instance, fixture_data[5]['paramgram_used'])
+    output = fmgr_fwpol_ipv4.fmgr_firewall_policy_modify(fmg_instance, fixture_data[3]['paramgram_used'])
     assert output['raw_response']['status']['code'] == 0
