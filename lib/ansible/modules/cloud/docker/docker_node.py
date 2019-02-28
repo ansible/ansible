e546280ced64d3f2eb3d408199a2594dfa38d3c3
Piotr Wojciechowski <23406016+WojciechowskiPiotr@users.noreply.github.com>
2019-02-28 20:08:35 +0100

    Documentation update for labels operations on swarm/node (#53083)
    * Update lib/ansible/modules/cloud/docker/docker_node.py
    * Update lib/ansible/modules/cloud/docker/docker_swarm.py
    
    Co-Authored-By: WojciechowskiPiotr <23406016+WojciechowskiPiotr@users.noreply.github.com>

--- lib/ansible/modules/cloud/docker/docker_node.py
+++ lib/ansible/modules/cloud/docker/docker_node.py
@@ -30,6 +30,8 @@ options:
     labels:
         description:
             - User-defined key/value metadata that will be assigned as node attribute.
+            - Label operations in this module apply to the docker swarm node specified by I(hostname).
+              Use M(docker_swarm) module to add/modify/remove swarm cluster labels.
             - The actual state of labels assigned to the node when module completes its work depends on
               I(labels_state) and I(labels_to_remove) parameters values. See description below.
         type: dict
