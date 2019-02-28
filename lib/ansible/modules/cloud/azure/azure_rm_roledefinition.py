7dcfa2bd3b74ce585e553cc3ae0ea65668859828
yungez <yungez@microsoft.com>
2019-02-28 11:37:03 +0800

    fix not_xxx_actions

--- lib/ansible/modules/cloud/azure/azure_rm_roledefinition.py
+++ lib/ansible/modules/cloud/azure/azure_rm_roledefinition.py
@@ -297,9 +297,9 @@ class AzureRMRoleDefinition(AzureRMModuleBase):
     def permissions_to_set(self, permissions):
         new_permissions = [str(dict(
             actions=(set([to_native(a) for a in item.get('actions')]) if item.get('actions') else None),
-            non_actions=(set([to_native(a) for a in item.get('non_actions')]) if item.get('non_actions') else None),
+            not_actions=(set([to_native(a) for a in item.get('not_actions')]) if item.get('not_actions') else None),
             data_actions=(set([to_native(a) for a in item.get('data_actions')]) if item.get('data_actions') else None),
-            non_data_actions=(set([to_native(a) for a in item.get('non_data_actions')]) if item.get('non_data_actions') else None),
+            not_data_actions=(set([to_native(a) for a in item.get('not_data_actions')]) if item.get('not_data_actions') else None),
         )) for item in permissions]
         return set(new_permissions)
 
