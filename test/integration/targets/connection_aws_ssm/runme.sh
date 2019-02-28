aa88418b11a24439506fa68458d94d1a5b989c2f
gau1991 <gau1991@gmail.com>
2019-02-28 09:05:57 +0000

    Added integration tests
    Added AWS SSM Executor, target and config functions
    
    Fixed more code for integration tests
    
    Improved execution
    
    Added S3 bucket name
    
    Fixed pylint
    
    Reverted lib changes
    
    Reverted few more changes
    
    Improved support for integration test execution
    
    added ansible role for aws_ssm_integration_test setup and teardown and modifiled runme.sh
    
    Reset to 17fa565 commit
    
    inventory file location changed
    
    change inventory file location
    
    deleted meta and handlers folder as it is not required
    
    deleted main.yml inside vars, removed extra space from tasks/main.yml, Added appropriate tags for ec2 and delete test folder as it is not required
    
    deleted main.yml inside vars, removed extra space from tasks/main.yml, Added appropriate tags for ec2 and delete test folder as it is not required
    
    modified task/main.yml
    
    added region variable and fixed pattern for using variable
    
    modified policy for IAM role
    
    moved to first line of scrip set -eux
    
    Updated Session Manager plugin installation
    
    edited custme policy
    
    Included tags for Session Manager plugin installation
    
    Added README.md
    
    Upddated README and added support for ssm-plugin for Amazon-Linux
    
    Added Windows Integration test support
    
    Improved user data for Linux
    
    Added random value generation for the role and policy,delete vars_to_delete.yml
    
    upadte README in vars
    
    fixed typo
    
    update policy
    
    Updated IAM policy file
    
    update playbook
    
    Updated playbook to include ssm-agent userdata
    
    modified jing2 template
    
    modified jing2 template
    
    modified jing2 template and fixed role deletion
    
    fixed role name issue while deleting and task name
    
    Updated playbook to include wait_connection for ec2
    
    Corrected Synatx changes and updated ssm-plugin debian file
    
    Changed region variable to us-east-1
    
    Removed vars file and updated  to /tmp dir
    
    fixed typo
    
    Improved setup
    
    Fixed boto3 dependency
    
    Fixed missing tag
    
    Added boto as dependency as well
    
    Improved execution workflow
    
    Trying other way of defining tags
    
    Fixed undefined var
    
    Changed AMI ID to Amazon Linux
    
    Improved Tags
    
    Ok, created different directory for WIndows test execution
    
    Fixed IAM Role Name for Windows
    
    Fixed inventory not found
    
    Improved integration test execution
    
    Fixed Windows Inventory path
    
    Fixed wrong Windows AMI ID
    
    Fixes issue for windows test execution

---
+++ test/integration/targets/connection_aws_ssm/runme.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -eux
+
+# Setup Environment
+ansible-playbook -c local aws_ssm_integration_test_setup_teardown.yml --tags setup_infra
+
+cd ../connection
+
+# Execute Integration tests for Linux
+INVENTORY=../connection_aws_ssm/inventory-linux.aws_ssm ./test.sh \
+    -e target_hosts=aws_ssm \
+    -e local_tmp=/tmp/ansible-local \
+    -e remote_tmp=/tmp/ansible-remote \
+    -e action_prefix= \
+    "$@" || true
+
+# Execute Integration tests for Windows
+INVENTORY=../connection_aws_ssm/inventory-windows.aws_ssm ./test.sh \
+    -e target_hosts=aws_ssm \
+    -e local_tmp=/tmp/ansible-local \
+    -e remote_tmp=c:/windows/temp/ansible-remote \
+    -e action_prefix=win_ \
+    "$@" || true
+
+cd ../connection_aws_ssm
+
+# Destroy Environment
+ansible-playbook -c local aws_ssm_integration_test_setup_teardown.yml --tags delete_infra
