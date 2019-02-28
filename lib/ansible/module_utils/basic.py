d44f80cb925703fa8bad915e03b5a4f59233efa7
Sam Doran <sdoran@redhat.com>
2019-02-28 07:32:00 -0500

    [stable-2.7] Modify the correct variable when setting available hashing algorithms (#52994)
    * Revert "use list instead of tuple and remove md5 on ValueError (#51357)" c459f040da88004b2fb0faa646cdd6530e99e2fe.
    * Modify the correct variable when determining available hashing algorithms.
    (cherry picked from commit 23a6b88dd28f9c25b90cbb048210d2299802022a)
    
    Co-authored-by: Sam Doran <sdoran@redhat.com>

--- lib/ansible/module_utils/basic.py
+++ lib/ansible/module_utils/basic.py
@@ -139,7 +139,7 @@ try:
     try:
         hashlib.md5()
     except ValueError:
-        algorithms.pop('md5', None)
+        AVAILABLE_HASH_ALGORITHMS.pop('md5', None)
 except Exception:
     import sha
     AVAILABLE_HASH_ALGORITHMS = {'sha1': sha.sha}
