27d37b487f4dc76efa22ac6b045334fffb827ca9
gau1991 <gaurav.ashtikar@flux7.com>
2019-03-01 11:17:52 +0530

    Reverted few more changes

--- test/runner/lib/integration/__init__.py
+++ test/runner/lib/integration/__init__.py
@@ -17,7 +17,6 @@ from lib.target import (
 from lib.config import (
     NetworkIntegrationConfig,
     PosixIntegrationConfig,
-    AwsSsmIntegrationConfig,
     WindowsIntegrationConfig,
 )
 
@@ -162,7 +161,6 @@ def integration_test_environment(args, target, inventory_path):
             PosixIntegrationConfig: 'inventory',
             WindowsIntegrationConfig: 'inventory.winrm',
             NetworkIntegrationConfig: 'inventory.networking',
-            AwsSsmIntegrationConfig: 'inventory.aws_ssm'
         }
 
         inventory_name = inventory_names[type(args)]
