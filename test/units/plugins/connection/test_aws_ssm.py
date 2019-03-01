0264cdb413e221db294099242e0dd366dab4efa3
Deepak Choudhary <deepak@flux7.com>
2019-03-01 18:43:32 +0530

    updated test file with close

--- test/units/plugins/connection/test_aws_ssm.py
+++ test/units/plugins/connection/test_aws_ssm.py
@@ -17,17 +17,6 @@ from ansible.plugins.loader import connection_loader, become_loader
 
 class TestConnectionBaseClass(unittest.TestCase):
 
-    def test_plugins_connection_aws_ssm_fetch_file(self):
-        pc = PlayContext()
-        new_stdin = StringIO()
-        conn = connection_loader.get('aws_ssm', pc, new_stdin)
-
-        conn._connect = MagicMock()
-        conn._file_transport_command = MagicMock()
-        conn._file_transport_command.return_value = (0, 'stdout', 'stderr')
-
-        res, stdout, stderr = conn.fetch_file('/in/file', '/out/file')
-
     @patch('os.path.exists')
     def test_plugins_connection_aws_ssm_put_file(self, mock_ospe):
         pc = PlayContext()
@@ -40,3 +29,33 @@ class TestConnectionBaseClass(unittest.TestCase):
 
         res, stdout, stderr = conn.put_file('/in/file', '/out/file')
 
+    def test_plugins_connection_aws_ssm_fetch_file(self):
+        pc = PlayContext()
+        new_stdin = StringIO()
+        conn = connection_loader.get('aws_ssm', pc, new_stdin)
+
+        conn._connect = MagicMock()
+        conn._file_transport_command = MagicMock()
+        conn._file_transport_command.return_value = (0, 'stdout', 'stderr')
+
+        res, stdout, stderr = conn.fetch_file('/in/file', '/out/file')
+
+    def test_plugins_connection_aws_transport_command(self):
+        pc = PlayContext()
+        new_stdin = StringIO()
+        conn = connection_loader.get('aws_ssm', pc, new_stdin)
+
+        
+
+    def test_plugins_connection_aws_ssm_close(self):
+        pc = PlayContext()
+        new_stdin = StringIO()
+        conn = connection_loader.get('aws_ssm', pc, new_stdin)
+
+        conn._session_id = MagicMock()
+        conn.get_option = MagicMock()
+        conn._connect = MagicMock()
+
+        conn.close()
+        self.assertFalse(conn._connected)
+        
