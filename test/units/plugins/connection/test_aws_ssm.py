706ceead8310b0674436679890caff9dedd60be8
gau1991 <gaurav.ashtikar@flux7.com>
2019-03-01 16:43:18 +0530

    Added Unit test cases file for AWS SSM Connection plugin

---
+++ test/units/plugins/connection/test_aws_ssm.py
@@ -0,0 +1,42 @@
+from io import StringIO
+import pytest
+
+from ansible import constants as C
+from ansible.errors import AnsibleAuthenticationFailure
+from ansible.compat.selectors import SelectorKey, EVENT_READ
+from units.compat import unittest
+from units.compat.mock import patch, MagicMock, PropertyMock
+from ansible.errors import AnsibleError, AnsibleConnectionFailure, AnsibleFileNotFound
+from ansible.module_utils.six.moves import shlex_quote
+from ansible.module_utils._text import to_bytes
+from ansible.playbook.play_context import PlayContext
+from ansible.plugins.connection import aws_ssm
+from ansible.plugins.loader import connection_loader, become_loader
+
+
+
+class TestConnectionBaseClass(unittest.TestCase):
+
+    def test_plugins_connection_aws_ssm_fetch_file(self):
+        pc = PlayContext()
+        new_stdin = StringIO()
+        conn = connection_loader.get('aws_ssm', pc, new_stdin)
+
+        conn._connect = MagicMock()
+        conn._file_transport_command = MagicMock()
+        conn._file_transport_command.return_value = (0, 'stdout', 'stderr')
+
+        res, stdout, stderr = conn.fetch_file('/in/file', '/out/file')
+
+    @patch('os.path.exists')
+    def test_plugins_connection_aws_ssm_put_file(self, mock_ospe):
+        pc = PlayContext()
+        new_stdin = StringIO()
+        conn = connection_loader.get('aws_ssm', pc, new_stdin)
+
+        conn._connect = MagicMock()
+        conn._file_transport_command = MagicMock()
+        conn._file_transport_command.return_value = (0, 'stdout', 'stderr')
+
+        res, stdout, stderr = conn.put_file('/in/file', '/out/file')
+
