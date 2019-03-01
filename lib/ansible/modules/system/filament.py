3fb6e6a028afb33ca5eb139b341623aafa64603d
Jill Rouleau <jill.rouleau@bespokess.com>
2019-03-01 16:08:48 -0700

    create filament plugin

---
+++ lib/ansible/modules/system/filament.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2019, Jill Rouleau <jillr@redhat.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+hello = world
