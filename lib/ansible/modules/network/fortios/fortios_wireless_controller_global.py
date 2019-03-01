32e3378ac92e3c1bcfb5450c1668439d769e9be3
Miguel Angel Muñoz González <magonzalez@fortinet.com>
2019-03-01 16:41:35 +0100

    Updated from comments received

--- lib/ansible/modules/network/fortios/fortios_wireless_controller_global.py
+++ lib/ansible/modules/network/fortios/fortios_wireless_controller_global.py
@@ -272,26 +272,11 @@ def filter_wireless_controller_global_data(json):
     return dictionary
 
 
-def flatten_multilists_attributes(data):
-    multilist_attrs = []
-
-    for attr in multilist_attrs:
-        try:
-            path = "data['" + "']['".join(elem for elem in attr) + "']"
-            current_val = eval(path)
-            flattened_val = ' '.join(elem for elem in current_val)
-            exec(path + '= flattened_val')
-        except BaseException:
-            pass
-
-    return data
-
-
 def wireless_controller_global(data, fos):
     vdom = data['vdom']
     wireless_controller_global_data = data['wireless_controller_global']
-    flattened_data = flatten_multilists_attributes(wireless_controller_global_data)
-    filtered_data = filter_wireless_controller_global_data(flattened_data)
+    filtered_data = filter_wireless_controller_global_data(wireless_controller_global_data)
+
     return fos.set('wireless-controller',
                    'global',
                    data=filtered_data,
