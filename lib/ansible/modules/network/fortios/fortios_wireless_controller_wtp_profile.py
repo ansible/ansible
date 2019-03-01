5676a0b44902c05610e6b918e21f44eafbdca893
Miguel Angel Muñoz González <magonzalez@fortinet.com>
2019-03-01 16:36:41 +0100

    Remove unnecessary code ('flatten' method)

--- lib/ansible/modules/network/fortios/fortios_wireless_controller_wtp_profile.py
+++ lib/ansible/modules/network/fortios/fortios_wireless_controller_wtp_profile.py
@@ -1383,26 +1383,11 @@ def filter_wireless_controller_wtp_profile_data(json):
     return dictionary
 
 
-def flatten_multilists_attributes(data):
-    multilist_attrs = []
-
-    for attr in multilist_attrs:
-        try:
-            path = "data['" + "']['".join(elem for elem in attr) + "']"
-            current_val = eval(path)
-            flattened_val = ' '.join(elem for elem in current_val)
-            exec(path + '= flattened_val')
-        except BaseException:
-            pass
-
-    return data
-
-
 def wireless_controller_wtp_profile(data, fos):
     vdom = data['vdom']
     wireless_controller_wtp_profile_data = data['wireless_controller_wtp_profile']
-    flattened_data = flatten_multilists_attributes(wireless_controller_wtp_profile_data)
-    filtered_data = filter_wireless_controller_wtp_profile_data(flattened_data)
+    filtered_data = filter_wireless_controller_wtp_profile_data(wireless_controller_wtp_profile_data)
+
     if wireless_controller_wtp_profile_data['state'] == "present":
         return fos.set('wireless-controller',
                        'wtp-profile',
