d63909e6b6f20ba12d808206ab7f9c8f7768dc5b
zikalino <zikalino@microsoft.com>
2019-03-02 21:53:47 +0800

    [WIP] adding deployment facts

---
+++ lib/ansible/modules/cloud/azure/azure_rm_deployment_facts.py
@@ -0,0 +1,180 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2018 Zim Kalinowski, <zikalino@microsoft.com>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+
+DOCUMENTATION = '''
+---
+module: azure_rm_deployment_facts
+version_added: "2.8"
+short_description: Get Azure Artifact facts.
+description:
+    - Get facts of Azure Artifact.
+
+options:
+    resource_group:
+        description:
+            - The name of the resource group.
+        required: True
+    name:
+        description:
+            - The name of the deployment.
+
+extends_documentation_fragment:
+    - azure
+
+author:
+    - "Zim Kalinowski (@zikalino)"
+
+'''
+
+EXAMPLES = '''
+  - name: Get instance of Deployment
+    azure_rm_deployment_facts:
+      resource_group: resource_group_name
+      lab_name: lab_name
+      artifact_source_name: artifact_source_name
+      name: name
+'''
+
+RETURN = '''
+artifacts:
+    description: A list of dictionaries containing facts for Artifact.
+    returned: always
+    type: complex
+    contains:
+        id:
+            description:
+                - The identifier of the resource.
+            returned: always
+            type: str
+            sample: id
+        resource_group:
+            description:
+                - Resource group name.
+            returned: always
+            sample: myResourceGroup
+        name:
+            description:
+                - Deployment name.
+            returned: always
+            sample: myDeployment
+'''
+
+from ansible.module_utils.azure_rm_common import AzureRMModuleBase
+
+try:
+    from msrestazure.azure_exceptions import CloudError
+    from azure.mgmt.devtestlabs import DevTestLabsClient
+    from msrest.serialization import Model
+except ImportError:
+    # This is handled in azure_rm_common
+    pass
+
+
+class AzureRMDeploymentFacts(AzureRMModuleBase):
+    def __init__(self):
+        self.module_arg_spec = dict(
+            resource_group=dict(
+                type='str',
+                required=True
+            ),
+            lab_name=dict(
+                type='str'
+            ),
+            artifact_source_name=dict(
+                type='str',
+                required=True
+            ),
+            name=dict(
+                type='str'
+            )
+        )
+        # store the results of the module operation
+        self.results = dict(
+            changed=False
+        )
+        self.resource_group = None
+        self.name = None
+        super(AzureRMDeploymentFacts, self).__init__(self.module_arg_spec, supports_tags=False)
+
+    def exec_module(self, **kwargs):
+        for key in self.module_arg_spec:
+            setattr(self, key, kwargs[key])
+
+        if self.name:
+            self.results['deployments'] = self.get()
+        else:
+            self.results['deployments'] = self.list()
+
+        return self.results
+
+    def get(self):
+        response = None
+        results = []
+        try:
+            response = self.rm_client.deployments.get(resource_group_name=self.resource_group,
+                                                      lab_name=self.lab_name,
+                                                      artifact_source_name=self.artifact_source_name,
+                                                      name=self.name)
+            self.log("Response : {0}".format(response))
+        except CloudError as e:
+            self.log('Could not get facts for Deployment.')
+
+        if response:
+            results.append(self.format_response(response))
+
+        return results
+
+    def list(self):
+        response = None
+        results = []
+        try:
+            response = self.rm_client.deployments.list(resource_group_name=self.resource_group,
+                                                       lab_name=self.lab_name,
+                                                       artifact_source_name=self.artifact_source_name)
+            self.log("Response : {0}".format(response))
+        except CloudError as e:
+            self.log('Could not get facts for Deployment.')
+
+        if response is not None:
+            for item in response:
+                results.append(self.format_response(item))
+
+        return results
+
+    def format_response(self, item):
+        d = item.as_dict()
+        d = {
+            #'resource_group': self.parse_resource_to_dict(d.get('id')).get('resource_group'),
+            #'lab_name': self.parse_resource_to_dict(d.get('id')).get('name'),
+            #'artifact_source_name': self.parse_resource_to_dict(d.get('id')).get('child_name_1'),
+            #'id': d.get('id'),
+            #'description': d.get('description'),
+            #'file_path': d.get('file_path'),
+            #'name': d.get('name'),
+            #'parameters': d.get('parameters'),
+            #'publisher': d.get('publisher'),
+            #'target_os_type': d.get('target_os_type'),
+            #'title': d.get('title')
+            'all': d
+        }
+        return d
+
+
+def main():
+    AzureRMDeploymentFacts()
+
+
+if __name__ == '__main__':
+    main()
