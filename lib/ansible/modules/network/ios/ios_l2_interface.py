571bdd5a7c2abb1545fb3f4998b709a27255bc2d
Chris Smolen <smolz@users.noreply.github.com>
2019-03-01 19:39:16 -0800

    Update ios_l2_interface.py
    Added version_added

--- lib/ansible/modules/network/ios/ios_l2_interface.py
+++ lib/ansible/modules/network/ios/ios_l2_interface.py
@@ -45,6 +45,7 @@ options:
     description:
       - Configure given Voice VLAN in access port.
         If C(mode=voice-access), used as the voice VLAN ID.
+    version_added: '2.8'
   trunk_vlans:
     description:
       - List of VLANs to be configured in trunk port.
