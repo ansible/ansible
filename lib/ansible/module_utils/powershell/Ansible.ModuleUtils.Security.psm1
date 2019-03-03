71c13acbe3fe3a17c25e30d9726b7a7246400d25
Christian.Rohr <Ch.Rohr@gmx.de>
2019-03-03 18:29:53 +0100

    Added module dependencies

---
+++ lib/ansible/module_utils/powershell/Ansible.ModuleUtils.Security.psm1
@@ -0,0 +1,16 @@
+<#
+    .SYNOPSIS
+    Save the username and the encrypted password as PSCredential object
+    .Parameter username
+    Username of the user which will use for the action
+    .Parameter password
+    Password of the user which is used for the action
+#>
+function ConvertTo-Credential {
+    [CmdletBinding()] param([Parameter(Mandatory = $true)][string] $username, [Parameter(Mandatory = $true)][string] $password)   
+    Write-DebugLog "Preparing credentials..."
+    $passwordEncrypted = ConvertTo-SecureString -String $password -AsPlainText -Force
+    $credentials = New-Object System.Management.Automation.PSCredential($username, $passwordEncrypted)
+    Write-DebugLog "Prepared credentials"
+    return $credentials
+}
