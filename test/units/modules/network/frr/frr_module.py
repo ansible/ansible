7b0b68670800591b2da754b3f2142060a62c9eb8
Nilashish Chakraborty <nilashishchakraborty8@gmail.com>
2019-03-04 15:02:56 +0530

    frr: New module for BGP configuration management (#51281)
    * Added frr_bgp module
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Fix pycodestyle warnings
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Fix CI docs issues
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Initial test files
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Fix dir structure to make CI happy
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Fix imports
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Fix CI
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Fix CI
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Split neighbor spec to handle general session and af session
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * New set of unittests
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Fix CI
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Fix CI - 2
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Update license
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Add port option for neighbor
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Validate network input
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Validate network input
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Fix sanity test failure
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>
    
    * Add validation for AF and more examples
    
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- test/units/modules/network/frr/frr_module.py
+++ test/units/modules/network/frr/frr_module.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-
 # (c) 2019, Ansible by Red Hat, inc
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
