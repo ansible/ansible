5b4968000a245952ba4af64c898a27006a5ff43a
Sam Doran <sdoran@redhat.com>
2019-03-04 17:02:44 -0500

    Move _handle_no_log_values() out of basic.py (#48628)
    * Rename method and make private
    * Use is_iterable, combine transformations
    * Remove unused return_values from network modules
    * Improve docstrings in new functions
    * Add new PASS_VAR
    * Add unit tests for list_no_log_values
    * Fix unit tests for Python 2.6

--- lib/ansible/module_utils/network/exos/exos.py
+++ lib/ansible/module_utils/network/exos/exos.py
@@ -27,7 +27,7 @@
 #
 import json
 from ansible.module_utils._text import to_text
-from ansible.module_utils.basic import env_fallback, return_values
+from ansible.module_utils.basic import env_fallback
 from ansible.module_utils.network.common.utils import to_list
 from ansible.module_utils.connection import Connection
 
