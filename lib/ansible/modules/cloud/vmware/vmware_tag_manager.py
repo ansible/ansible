9862776f3563e437ab74ca92013399b4c0b85931
Abhijeet Kasurde <akasurde@redhat.com>
2019-03-04 22:18:57 +0530

    VMware: Fail when unable to find managed object (#53158)
    Fixes: #52119
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/cloud/vmware/vmware_tag_manager.py
+++ lib/ansible/modules/cloud/vmware/vmware_tag_manager.py
@@ -126,13 +126,18 @@ class VmwareTagManager(VmwareRestClient):
 
         self.object_type = self.params.get('object_type')
         self.object_name = self.params.get('object_name')
+        self.managed_object = None
 
         if self.object_type == 'VirtualMachine':
             self.managed_object = self.pyv.get_vm_or_template(self.object_name)
-            self.dynamic_managed_object = DynamicID(type=self.object_type, id=self.managed_object._moId)
 
         if self.managed_object is None:
             self.module.fail_json(msg="Failed to find the managed object for %s with type %s" % (self.object_name, self.object_type))
+
+        if not hasattr(self.managed_object, '_moId'):
+            self.module.fail_json(msg="Unable to find managed object id for %s managed object" % self.object_name)
+
+        self.dynamic_managed_object = DynamicID(type=self.object_type, id=self.managed_object._moId)
 
         self.tag_service = Tag(self.connect)
         self.category_service = Category(self.connect)
