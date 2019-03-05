3e5922ad7da96beac1ccd25312975d6c5cfabab8
ftntcorecse <43451990+ftntcorecse@users.noreply.github.com>
2019-03-05 01:25:43 -0800

    FortiManager Plugin Module Conversion: fmgr_secprof_web (#52788)
    * Auto Commit for: fmgr_secprof_web
    
    * Auto Commit for: fmgr_secprof_web
    
    * Auto Commit for: fmgr_secprof_web

--- test/units/modules/network/fortimanager/test_fmgr_secprof_web.py
+++ test/units/modules/network/fortimanager/test_fmgr_secprof_web.py
@@ -19,7 +19,7 @@ __metaclass__ = type
 
 import os
 import json
-from pyFMG.fortimgr import FortiManager
+from ansible.module_utils.network.fortimanager.fortimanager import FortiManagerHandler
 import pytest
 
 try:
@@ -27,15 +27,10 @@ try:
 except ImportError:
     pytest.skip("Could not load required modules for testing", allow_module_level=True)
 
-fmg_instance = FortiManager("1.1.1.1", "admin", "")
-
 
 def load_fixtures():
-    fixture_path = os.path.join(
-        os.path.dirname(__file__),
-        'fixtures') + "/{filename}.json".format(
-        filename=os.path.splitext(
-            os.path.basename(__file__))[0])
+    fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures') + "/{filename}.json".format(
+        filename=os.path.splitext(os.path.basename(__file__))[0])
     try:
         with open(fixture_path, "r") as fixture_file:
             fixture_data = json.load(fixture_file)
@@ -44,114 +39,29 @@ def load_fixtures():
     return [fixture_data]
 
 
+@pytest.fixture(autouse=True)
+def module_mock(mocker):
+    connection_class_mock = mocker.patch('ansible.module_utils.basic.AnsibleModule')
+    return connection_class_mock
+
+
+@pytest.fixture(autouse=True)
+def connection_mock(mocker):
+    connection_class_mock = mocker.patch('ansible.modules.network.fortimanager.fmgr_secprof_web.Connection')
+    return connection_class_mock
+
+
 @pytest.fixture(scope="function", params=load_fixtures())
 def fixture_data(request):
     func_name = request.function.__name__.replace("test_", "")
     return request.param.get(func_name, None)
 
 
-def test_fmgr_webfilter_profile_addsetdelete(fixture_data, mocker):
-    mocker.patch("pyFMG.fortimgr.FortiManager._post_request", side_effect=fixture_data)
-    #  Fixture sets used:###########################
+fmg_instance = FortiManagerHandler(connection_mock, module_mock)
 
-    ##################################################
-    # comment: None
-    # web-extended-all-action-log: None
-    # url-extraction: {'status': None, 'redirect-url': None, 'server-fqdn': None, 'redirect-header': None,
-    #  'redirect-no-content': None}
-    # ftgd-wf: {'filters': {'category': None, 'log': None, 'override-replacemsg': None, 'warning-duration-type': None,
-    #  'warn-duration': None, 'auth-usr-grp': None, 'action': None, 'warning-prompt': None}, 'ovrd': None,
-    #  'rate-image-urls': None, 'quota': {'category': None, 'value': None, 'override-replacemsg': None,
-    #  'duration': None, 'type': None, 'unit': None}, 'options': None, 'rate-javascript-urls': None,
-    #  'max-quota-timeout': None, 'rate-css-urls': None, 'exempt-quota': None, 'rate-crl-urls': None}
-    # log-all-url: None
-    # extended-log: None
-    # web-filter-cookie-removal-log: None
-    # https-replacemsg: None
-    # web: {'log-search': None, 'bword-threshold': None, 'bword-table': None, 'whitelist': None,
-    #  'youtube-restrict': None, 'safe-search': None, 'blacklist': None, 'keyword-match': None,
-    #  'urlfilter-table': None, 'content-header-list': None}
-    # wisp: None
-    # web-filter-vbs-log: None
-    # youtube-channel-filter: {'comment': None, 'channel-id': None}
-    # override: {'profile': None, 'ovrd-scope': None, 'ovrd-dur-mode': None, 'profile-attribute': None,
-    #  'ovrd-dur': None, 'profile-type': None, 'ovrd-user-group': None, 'ovrd-cookie': None}
-    # ovrd-perm: None
-    # mode: delete
-    # web-content-log: None
-    # web-invalid-domain-log: None
-    # adom: root
-    # web-filter-referer-log: None
-    # inspection-mode: None
-    # post-action: None
-    # name: Ansible_Web_Filter_Profile
-    # web-filter-command-block-log: None
-    # web-filter-applet-log: None
-    # web-ftgd-err-log: None
-    # replacemsg-group: None
-    # web-filter-jscript-log: None
-    # web-filter-activex-log: None
-    # web-filter-js-log: None
-    # web-ftgd-quota-usage: None
-    # web-filter-unknown-log: None
-    # web-filter-cookie-log: None
-    # youtube-channel-status: None
-    # web-url-log: None
-    # options: None
-    # wisp-servers: None
-    # wisp-algorithm: None
-    ##################################################
-    ##################################################
-    # comment: Created by Ansible Module TEST
-    # web-filter-command-block-log: enable
-    # web-invalid-domain-log: enable
-    # web-extended-all-action-log: enable
-    # adom: root
-    # ftgd-wf: {'rate-javascript-urls': None, 'quota': {'category': None, 'value': None, 'override-replacemsg': None,
-    #  'duration': None, 'type': None, 'unit': None}, 'rate-image-urls': None, 'filters': {'category': None,
-    #  'auth-usr-grp': None, 'log': None, 'warning-prompt': None, 'override-replacemsg': None, 'action': None,
-    #  'warn-duration': None, 'warning-duration-type': None}, 'rate-css-urls': None, 'ovrd': None,
-    #  'exempt-quota': None, 'max-quota-timeout': None, 'rate-crl-urls': None, 'options': None}
-    # web-content-log: enable
-    # web-filter-referer-log: enable
-    # log-all-url: enable
-    # extended-log: enable
-    # inspection-mode: proxy
-    # web-filter-cookie-removal-log: enable
-    # post-action: block
-    # web-filter-activex-log: enable
-    # web-filter-cookie-log: enable
-    # web: {'blacklist': None, 'log-search': None, 'keyword-match': None, 'urlfilter-table': None, 'bword-table': None,
-    #  'safe-search': None, 'whitelist': None, 'content-header-list': None, 'youtube-restrict': None,
-    #  'bword-threshold': None}
-    # web-filter-applet-log: enable
-    # web-ftgd-err-log: enable
-    # replacemsg-group: None
-    # web-filter-jscript-log: enable
-    # web-ftgd-quota-usage: enable
-    # url-extraction: {'status': None, 'server-fqdn': None, 'redirect-url': None, 'redirect-header': None,
-    #  'redirect-no-content': None}
-    # web-filter-js-log: enable
-    # youtube-channel-filter: {'comment': None, 'channel-id': None}
-    # name: Ansible_Web_Filter_Profile
-    # wisp: enable
-    # web-filter-vbs-log: enable
-    # web-filter-unknown-log: enable
-    # mode: set
-    # youtube-channel-status: blacklist
-    # override: {'profile': None, 'ovrd-user-group': None, 'ovrd-scope': None, 'ovrd-cookie': None,
-    #  'ovrd-dur-mode': None, 'profile-attribute': None, 'ovrd-dur': None, 'profile-type': None}
-    # web-url-log: enable
-    # ovrd-perm: bannedword-override
-    # https-replacemsg: None
-    # options: js
-    # wisp-servers: None
-    # wisp-algorithm: auto-learning
-    ##################################################
 
-    # Test using fixture 1 #
-    output = fmgr_secprof_web.fmgr_webfilter_profile_addsetdelete(fmg_instance, fixture_data[0]['paramgram_used'])
-    assert output['raw_response']['status']['code'] == 0
-    # Test using fixture 2 #
-    output = fmgr_secprof_web.fmgr_webfilter_profile_addsetdelete(fmg_instance, fixture_data[1]['paramgram_used'])
+def test_fmgr_webfilter_profile_modify(fixture_data, mocker):
+    mocker.patch("ansible.module_utils.network.fortimanager.fortimanager.FortiManagerHandler.process_request",
+                 side_effect=fixture_data)
+    output = fmgr_secprof_web.fmgr_webfilter_profile_modify(fmg_instance, fixture_data[0]['paramgram_used'])
     assert output['raw_response']['status']['code'] == 0
