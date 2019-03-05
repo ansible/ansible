8bcff3329ee46249d55042485b72bb8abd9f876b
Felix Fontein <felix@fontein.de>
2019-03-05 15:39:03 +0100

    [stable-2.7] Python 2: accept both long and int for type=int (module options) (#53289)
    * Added unit test.
    (cherry picked from commit 07fcb60d5587a57d6b910f3e2694f23d46e500fa)
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- lib/ansible/module_utils/basic.py
+++ lib/ansible/module_utils/basic.py
@@ -1952,7 +1952,7 @@ class AnsibleModule(object):
         raise TypeError('%s cannot be converted to a bool' % type(value))
 
     def _check_type_int(self, value):
-        if isinstance(value, int):
+        if isinstance(value, integer_types):
             return value
 
         if isinstance(value, string_types):
