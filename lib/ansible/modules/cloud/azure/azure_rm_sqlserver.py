8b6ebec9f9b462c4f57efbbcaa02f33192d0eec5
Zim Kalinowski <zikalino@microsoft.com>
2019-03-05 15:44:32 +0800

    cleaning up azure_rm_sql* class names (#53261)

--- lib/ansible/modules/cloud/azure/azure_rm_sqlserver.py
+++ lib/ansible/modules/cloud/azure/azure_rm_sqlserver.py
@@ -118,7 +118,7 @@ class Actions:
     NoAction, Create, Update, Delete = range(4)
 
 
-class AzureRMServers(AzureRMModuleBase):
+class AzureRMSqlServer(AzureRMModuleBase):
     """Configuration class for an Azure RM SQL Server resource"""
 
     def __init__(self):
@@ -163,9 +163,9 @@ class AzureRMServers(AzureRMModuleBase):
         self.state = None
         self.to_do = Actions.NoAction
 
-        super(AzureRMServers, self).__init__(derived_arg_spec=self.module_arg_spec,
-                                             supports_check_mode=True,
-                                             supports_tags=True)
+        super(AzureRMSqlServer, self).__init__(derived_arg_spec=self.module_arg_spec,
+                                               supports_check_mode=True,
+                                               supports_tags=True)
 
     def exec_module(self, **kwargs):
         """Main module execution method"""
@@ -314,7 +314,7 @@ class AzureRMServers(AzureRMModuleBase):
 
 def main():
     """Main execution"""
-    AzureRMServers()
+    AzureRMSqlServer()
 
 
 if __name__ == '__main__':
