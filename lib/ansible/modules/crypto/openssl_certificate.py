b56833db59dfeba09502cebd1341770b640b8d5f
johnwesley <wesley@echoboat.org>
2019-03-05 16:17:44 -0500

    Warn user to send a password if the privatekey is password protected

--- lib/ansible/modules/crypto/openssl_certificate.py
+++ lib/ansible/modules/crypto/openssl_certificate.py
@@ -76,7 +76,7 @@ options:
 
     privatekey_passphrase:
         description:
-            - The passphrase for the I(privatekey_path).
+            - The passphrase for the I(privatekey_path). This may be required if the privatekey is password protected.
         type: str
 
     selfsigned_version:
