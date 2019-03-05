a05d3ecd698bc2dcfc9177a2ded656ab39334744
Alex Stephen <alexstephen@google.com>
2019-03-05 10:30:27 -0800

    test diff

--- test/units/module_utils/gcp/test_gcp_utils.py
+++ test/units/module_utils/gcp/test_gcp_utils.py
@@ -171,5 +171,3 @@ class GCPRequestDifferenceTestCase(unittest.TestCase):
         request2 = GcpRequest(value2)
         self.assertEquals(request1 == request2, False)
         self.assertEquals(request1.difference(request2), difference)
-
-
