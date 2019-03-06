233d5bdfe014844f730e8f23c387ac67f29e886d
James Tanner <tanner.jc@gmail.com>
2019-03-06 18:24:54 -0500

    Add the constructed config

--- test/integration/targets/inventory_aws_conformance/runme.sh
+++ test/integration/targets/inventory_aws_conformance/runme.sh
@@ -50,6 +50,83 @@ cat << EOF > $OUTPUT_DIR/test.aws_ec2.yml
 plugin: aws_ec2
 regions:
     - us-east-1
+compose:
+  # vars that don't exist
+  ec2_item: backwardscompat | default("")  # boto anomaly, always appears to be u'\n', don't see a reason to keep it around
+  ec2_monitoring: backwardscompat | default("") # boto anomaly, always appears to be u'\n', don't see a reason to keep it around
+  ec2_previous_state: backwardscompat | default("")  # This is only returned with boto3 when starting, stopping, and terminating instances, not applicable
+  ec2_previous_state_code: backwardscompat | default(0)  # This is only returned with boto3 when starting, stopping, and terminating instances
+  ec2__in_monitoring_element: backwardscompat | default(false)  # double underscore not a typo. Trying to figure out wth this was.
+  ec2_requester_id: backwardscompat | default("") # FIXME not an attribute returned anymore on instances in boto3, will need code change
+  ec2_account_id: backwardscompat | default("") # FIXME not an attribute returned anymore on instances in boto3, will need code change
+  ec2_eventsSet: backwardscompat | default("")  # FIXME would need to add a describe_instance_status API call for instances to be able to add an events hostvar
+  ec2_persistent: backwardscompat | default(false)  # FIXME would need to add a describe_spot_instance_requests API call for any instances that have the SpotInstanceRequestId variable
+
+  # vars that change
+  ec2_block_devices: dict(block_device_mappings | map(attribute='device_name') | list | zip(block_device_mappings | map(attribute='ebs.volume_id') | list))
+  ec2_dns_name: public_dns_name
+  ec2_group_name: placement.group_name
+  ec2_id: instance_id
+  ec2_instance_profile: iam_instance_profile | default("")
+  ec2_ip_address: public_ip_address
+  ec2_kernel: kernel_id | default("")
+  ec2_monitored:  monitoring.state in ['enabled', 'pending']
+  ec2_monitoring_state: monitoring.state
+  ec2_placement: placement.availability_zone
+  ec2_ramdisk: ramdisk_id | default("")
+  ec2_reason: state_transition_reason
+  ec2_security_group_ids: security_groups | map(attribute='group_id') | list |  join(',')
+  ec2_security_group_names: security_groups | map(attribute='group_name') | list |  join(',')
+  ec2_state: state.name
+  ec2_state_code: state.code
+  ec2_state_reason: state_reason.message if state_reason is defined else ""
+  ec2_sourceDestCheck: source_dest_check | lower | string  # butchered snake_case case not a typo.
+
+  # vars that just need ec2_ prefix
+  ec2_ami_launch_index: ami_launch_index | string
+  ec2_architecture: architecture
+  ec2_client_token: client_token
+  ec2_ebs_optimized: ebs_optimized
+  ec2_hypervisor: hypervisor
+  ec2_image_id: image_id
+  ec2_instance_type: instance_type
+  ec2_key_name: key_name
+  ec2_launch_time: 'launch_time | regex_replace(" ", "T") | regex_replace("(\+)(\d\d):(\d)(\d)$", ".\g<2>\g<3>Z")'
+  ec2_platform: platform | default("")
+  ec2_private_dns_name: private_dns_name
+  ec2_private_ip_address: private_ip_address
+  ec2_public_dns_name: public_dns_name
+  ec2_region: placement.region
+  ec2_root_device_name: root_device_name
+  ec2_root_device_type: root_device_type
+  ec2_spot_instance_request_id: spot_instance_request_id
+  ec2_subnet_id: subnet_id
+  ec2_virtualization_type: virtualization_type
+  ec2_vpc_id: vpc_id
+
+keyed_groups:
+  - prefix: tag
+    key: tags
+  - prefix: key
+    key: key_name
+  - prefix: ""
+    separator: ""
+    key: placement.region
+  - prefix: ""
+    separator: ""
+    key: placement.availability_zone
+  - key: security_group
+  - key: platform
+  - key: vpc_id
+  - prefix: type  # may need to replace . with _
+    key: instance_type
+  - prefix: instance_state
+    key: state.name
+  - prefix: ""
+    separator: ""
+    key: image_id
+  - key: 'security_groups | json_query("[].group_name")'
+prefix: security_group
 EOF
 
 # override boto's import path(s)
