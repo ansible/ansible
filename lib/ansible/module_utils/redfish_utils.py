cbec562d4a8416c644db2da87d66d90f89252209
dnuka <csx@tuta.io>
2019-03-07 17:46:52 +0000

    rebased

--- lib/ansible/module_utils/redfish_utils.py
+++ lib/ansible/module_utils/redfish_utils.py
@@ -951,25 +951,6 @@ class RedfishUtils(object):
         chassis_uri_list = self.chassis_uri_list
         for chassis_uri in chassis_uri_list:
             response = self.get_request(self.root_uri + chassis_uri)
-        # Get a list of all PSUs and build respective URIs
-        response = self.get_request(self.root_uri + systems_uri)
-        if response['ret'] is False:
-            return response
-        result['ret'] = True
-        data = response['data']
-
-        if 'Links' not in data:
-            return {'ret': False, 'msg': "Property not found"}
-        if key not in data[u'Links']:
-            return {'ret': False, 'msg': "Key %s not found" % key}
-
-        for psu in data[u'Links'][u'PoweredBy']:
-            psu_list.append(psu[u'@odata.id'])
-
-        for p in psu_list:
-            psu = {}
-            uri = self.root_uri + p
-            response = self.get_request(uri)
             if response['ret'] is False:
                 return response
 
