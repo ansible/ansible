64176f200e788ac9ee72e720be9e714c71dd0c32
Sam Doran <sdoran@redhat.com>
2019-03-07 10:32:42 -0500

    Change parameter variable from check to terms

--- lib/ansible/module_utils/common/validation.py
+++ lib/ansible/module_utils/common/validation.py
@@ -8,39 +8,39 @@ __metaclass__ = type
 from ansible.module_utils.common.collections import is_iterable
 
 
-def count_terms(check, params):
+def count_terms(terms, module_parameters):
     """Count the number of occurrences of a key in a given dictionary
 
-    :arg check: String or iterable of values to check
-    :arg params: Dictionary of module parameters
+    :arg terms: String or iterable of values to check
+    :arg module_parameters: Dictionary of module parameters
 
-    :returns: An integer that is the number of occurrences of the check values
+    :returns: An integer that is the number of occurrences of the terms values
         in the provided dictionary.
     """
 
-    if not is_iterable(check):
-        check = [check]
+    if not is_iterable(terms):
+        terms = [terms]
 
-    return len(set(check).intersection(params))
+    return len(set(terms).intersection(module_parameters))
 
 
 def check_mutually_exclusive(terms, module_parameters):
     """Check mutually exclusive terms against argument parameters. Accepts
     a single list or list of lists that are groups of terms that should be
-    mutually exclusive with one another.
+    mutually exclusive with one another
 
     :arg terms: List of mutually exclusive module parameters
-    :arg params: Dictionary of module parameters
+    :arg module_parameters: Dictionary of module parameters
 
     :returns: A list of terms that are mutually exclusive.
     """
 
     if terms is None:
-        return
+        return []
 
     result = []
     for check in terms:
-        count = count_terms(check, params)
+        count = count_terms(check, module_parameters)
         if count > 1:
             result.append(check)
 
