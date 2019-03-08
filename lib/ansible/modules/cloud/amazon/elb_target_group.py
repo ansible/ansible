8d1d117067e1bcf75bca1c7f97e2c05b8e6f045a
Alicia Cozine <acozine@users.noreply.github.com>
2019-03-08 17:02:03 -0600

    makes desc multi-line

--- lib/ansible/modules/cloud/amazon/elb_target_group.py
+++ lib/ansible/modules/cloud/amazon/elb_target_group.py
@@ -99,8 +99,10 @@ options:
     default: lb_cookie
   successful_response_codes:
     description:
-      - The HTTP codes to use when checking for a successful response from a target. You can specify multiple values (for example, "200,202") or a range of
-        values (for example, "200-299"). Requires the I(health_check_protocol) parameter to be set.
+      - The HTTP codes to use when checking for a successful response from a target. 
+      - Accepts multiple values (for example, "200,202") or a range of
+        values (for example, "200-299").
+      - Requires the I(health_check_protocol) parameter to be set.
     required: false
   tags:
     description:
