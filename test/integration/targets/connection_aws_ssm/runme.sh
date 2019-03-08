bacb74a3f4ec1cc6bd497ead58312992c4772edf
gau1991 <gaurav.ashtikar@flux7.com>
2019-03-08 11:20:15 +0530

    Improved support for integration test execution

--- test/integration/targets/connection_aws_ssm/runme.sh
+++ test/integration/targets/connection_aws_ssm/runme.sh
@@ -7,4 +7,6 @@ cd ../connection
 INVENTORY=../connection_aws_ssm/inventory.aws_ssm ./test.sh \
     -e target_hosts=aws_ssm \
     -e local_tmp=/tmp/ansible-local \
+    -e remote_tmp=/tmp/ansible-remote \
+    -e action_prefix= \
     "$@"
