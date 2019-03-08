1bd2b276b05952851c61d4eb2f68568369ccb55c
Rui  Moreira <ruimoreira29@gmail.com>
2019-03-08 22:11:02 +0000

    docstring implemtation and further methods initial implementation

--- lib/ansible/modules/cloud/amazon/dms_endpoint.py
+++ lib/ansible/modules/cloud/amazon/dms_endpoint.py
@@ -4,8 +4,9 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
-from ansible.module_utils.aws.core import AnsibleAWSModule, is_boto3_error_code, get_boto3_client_method_parameters
-__metaclass__ = type
+import traceback
+#from ansible.module_utils.aws.core import AnsibleAWSModule, \
+#    is_boto3_error_code, get_boto3_client_method_parameters
 
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
@@ -13,7 +14,7 @@ ANSIBLE_METADATA = {
     'supported_by': 'community'
 }
 
-DOCUMENTATION = '''
+DOCUMENTATION = """
 ---
 module: dms_endpoint
 short_description: creates or destroys a data migration services endpoint
@@ -24,7 +25,7 @@ options:
 author:
    - Rui Moreira (@ruimoreira)
 extends_documentation_fragment: aws
-'''
+"""
 
 EXAMPLES = '''
 '''
@@ -35,8 +36,7 @@ RETURN = '''
 
 from ansible.module_utils.aws.core import AnsibleAWSModule, is_boto3_error_code, get_boto3_client_method_parameters
 from ansible.module_utils.ec2 import boto3_conn, ec2_argument_spec, HAS_BOTO3, camel_dict_to_snake_dict, get_aws_connection_info, AWSRetry
-import traceback
-from time import sleep
+
 
 try:
     from botocore.exceptions import ClientError, BotoCoreError, WaiterError
@@ -45,44 +45,73 @@ except ImportError:
 
 backoff_params = dict(tries=10, delay=3, backoff=1.5)
 
+
 @AWSRetry.backoff(**backoff_params)
-def describe_endpoints():
+def describe_endpoints(connection, endpoint_identifier):
+    """ checks if the endpoint exists """
+    endpoint_filter = dict(Name='endpoint-id', Values=endpoint_identifier)
+    return connection.describe_endpoints(**endpoint_filter)
 
 
-def create_endpoint(connection):
+@AWSRetry.backoff(**backoff_params)
+def create_endpoint(connection, **params):
+    """ creates the endpoint"""
+    return connection.create_endpoint(**params)
+
+def create_dms_endpoint(connection):
+    """
+    Function to create the dms endpoint
+    :param connection: boto3 aws connection
+    :return: information about the dms endpoint object
+    """
     endpoint_identifier = module.params.get('endpointidentifier')
     endpoint_type = module.params.get('endpointtype')
+    engine_name = module.params.get('enginename')
+    username = module.params.get('username')
+    password = module.params.get('password')
+    servername = module.params.get('servername')
+    port = module.params.get('port')
+
     try:
-        endpoint = describe_endpoints()
+        endpoint_exists = describe_endpoints(connection, endpoint_identifier)
     except (ClientError, BotoCoreError) as e:
         module.fail_json(msg="Failed to describe DMS endpoint.",
                          exception=traceback.format_exc())
-
+    endpoint = dict(
+        EndpointIdentifier=endpoint_identifier,
+        EndpointType=endpoint_type,
+        EngineName=engine_name,
+        Username=username,
+        Password=password,
+        ServerName=servername,
+        Port=port,
+    )
 
 def main():
+    """ main function, instanciates the ansible module and performs the initial logic"""
     argument_spec = dict(
         state=dict(choices=['present', 'absent'], default='present'),
-        endpointidentifier = dict(required=True),
-        endpointtype = dict(choices=['source', 'target'], required=True),
-        enginename = dict(choices=['mysql', 'oracle', 'postgres', 'mariadb', 'aurora', 'redshift', 's3', 'db2', 'azuredb', 'sybase', 'dynamodb', 'mongodb', 'sqlserver'], required=True),
-        username = dict(),
-        password = dict(),
-        servername = dict(),
-        port = dict(type='int'),
-        databasename = dict(),
-        extraconnectionattributes = dict(),
-        kmskeyid = dict(),
-        tags = dict(type='dict'),
-        certificatearn = dict(),
-        sslmode = dict(choices=['none', 'require', 'verify-ca', 'verify-full'], default='none'),
-        serviceaccessrolearn = dict(),
-        externaltabledefinition = dict(),
-        dynamodbsettings = dict(type='dict'),
-        s3settings = dict(type='dict'),
-        dmstransfersettings = dict(type='dict'),
-        mongodbsettings = dict(type='dict'),
-        kinesissettings = dict(type='dict'),
-        elasticsearchsettings = dict(type='dict')
+        endpointidentifier=dict(required=True),
+        endpointtype=dict(choices=['source', 'target'], required=True),
+        enginename=dict(choices=['mysql', 'oracle', 'postgres', 'mariadb', 'aurora', 'redshift', 's3', 'db2', 'azuredb', 'sybase', 'dynamodb', 'mongodb', 'sqlserver'], required=True),
+        username=dict(),
+        password=dict(),
+        servername=dict(),
+        port=dict(type='int'),
+        databasename=dict(),
+        extraconnectionattributes=dict(),
+        kmskeyid=dict(),
+        tags=dict(type='dict'),
+        certificatearn=dict(),
+        sslmode=dict(choices=['none', 'require', 'verify-ca', 'verify-full'], default='none'),
+        serviceaccessrolearn=dict(),
+        externaltabledefinition=dict(),
+        dynamodbsettings=dict(type='dict'),
+        s3settings=dict(type='dict'),
+        dmstransfersettings=dict(type='dict'),
+        mongodbsettings=dict(type='dict'),
+        kinesissettings=dict(type='dict'),
+        elasticsearchsettings=dict(type='dict')
     )
     global module
     module = AnsibleAWSModule(
@@ -103,7 +132,7 @@ def main():
                             **aws_connect_params)
 
     if state == 'present':
-        return 0
+        create_dms_endpoint(connection)
 
     if __name__ == '__main__':
         main()
