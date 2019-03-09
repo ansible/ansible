ecd02ff782a7de1e9b28cb2888ab8f2a687a7f16
Matt Clay <matt@mystile.com>
2019-03-08 22:59:00 -0800

    Fix conditionals integration test. (#53583)
    * Fix inventory for conditionals test.
    * Remove extra groups from conditionals test.

--- test/integration/targets/conditionals/runme.sh
+++ test/integration/targets/conditionals/runme.sh
@@ -2,5 +2,5 @@
 
 set -eux
 
-ANSIBLE_CONDITIONAL_BARE_VARS=1 ansible-playbook -i inventory play.yml "$@"
-ANSIBLE_CONDITIONAL_BARE_VARS=0 ansible-playbook -i inventory play.yml "$@"
+ANSIBLE_CONDITIONAL_BARE_VARS=1 ansible-playbook -i ../../inventory play.yml "$@"
+ANSIBLE_CONDITIONAL_BARE_VARS=0 ansible-playbook -i ../../inventory play.yml "$@"
