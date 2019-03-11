119956049f0da08fd0b2e37f9420d4430223943d
John R Barker <john@johnrbarker.com>
2019-03-11 13:16:56 +0100

    Update lib/ansible/modules/cloud/hcloud/hcloud_floating_ip.py
    Co-Authored-By: LKaemmerling <4281581+LKaemmerling@users.noreply.github.com>

--- lib/ansible/modules/cloud/hcloud/hcloud_floating_ip.py
+++ lib/ansible/modules/cloud/hcloud/hcloud_floating_ip.py
@@ -25,7 +25,7 @@ description:
     - "Create, update and manage Floating IPss on the Hetzner Cloud."
 
 author:
-    - Lukas Kaemmerling (@lkaemmerling)
+    - Lukas Kaemmerling (@LKaemmerling)
 
 options:
     id:
