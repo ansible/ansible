edae5f79c8f5270f1021274b2490fad8d0fd885a
Marius Rieder <marius.rieder@scs.ch>
2019-03-11 10:21:53 +0100

    Cleanup PowerShell code

--- lib/ansible/modules/windows/win_domain_group_membership.ps1
+++ lib/ansible/modules/windows/win_domain_group_membership.ps1
@@ -9,7 +9,7 @@ try {
     Import-Module ActiveDirectory
 }
 catch {
-    Fail-Json $result "win_domain_group_membership requires the ActiveDirectory PS module to be installed"
+    Fail-Json -obj @{} -message "win_domain_group_membership requires the ActiveDirectory PS module to be installed"
 }
 
 $params = Parse-Args $args -supports_check_mode $true
@@ -19,52 +19,51 @@ $diff_mode = Get-AnsibleParam -obj $params -name "_ansible_diff" -type "bool" -d
 # Module control parameters
 $state = Get-AnsibleParam -obj $params -name "state" -type "str" -default "present" -validateset "present","absent","pure"
 $domain_username = Get-AnsibleParam -obj $params -name "domain_username" -type "str"
-$domain_password = Get-AnsibleParam -obj $params -name "domain_password" -type "str" -failifempty ($domain_username -ne $null)
+$domain_password = Get-AnsibleParam -obj $params -name "domain_password" -type "str" -failifempty ($null -ne $domain_username)
 $domain_server = Get-AnsibleParam -obj $params -name "domain_server" -type "str"
 
 # Group Membership parameters
 $name = Get-AnsibleParam -obj $params -name "name" -type "str" -failifempty $true
 $members = Get-AnsibleParam -obj $params -name "members" -type "list" -failifempty $true
 
+# Filter ADObjects by ObjectClass
+$ad_object_class_filter = "(ObjectClass -eq 'user' -or ObjectClass -eq 'group' -or ObjectClass -eq 'computer' -or ObjectClass -eq 'msDS-ManagedServiceAccount')"
+
 $extra_args = @{}
-if ($domain_username -ne $null) {
+if ($null -ne $domain_username) {
     $domain_password = ConvertTo-SecureString $domain_password -AsPlainText -Force
     $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $domain_username, $domain_password
     $extra_args.Credential = $credential
 }
-if ($domain_server -ne $null) {
+if ($nul -ne $domain_server) {
     $extra_args.Server = $domain_server
 }
 
 $result = @{
     changed = $false
-}
-if ($state -in @("present", "pure")) {
-    $result.added = @()
-}
-if ($state -in @("absent", "pure")) {
-    $result.removed = @()
+    added = [System.Collections.Generic.List`1[String]]@()
+    removed = [System.Collections.Generic.List`1[String]]@()
 }
 if ($diff_mode) {
     $result.diff = @{}
 }
 
 $members_before = Get-AdGroupMember -Identity $name @extra_args
-$pure_members = @()
+$pure_members = [System.Collections.Generic.List`1[String]]@()
 
 foreach ($member in $members) {
-    $group_member = Get-ADObject -Filter "SamAccountName -eq '$member' -and (ObjectClass -eq 'user' -or ObjectClass -eq 'group' -or ObjectClass -eq 'computer' -or ObjectClass -eq 'msDS-ManagedServiceAccount')"
+    $group_member = Get-ADObject -Filter "SamAccountName -eq '$member' -and $ad_object_class_filter" -Properties objectSid, sAMAccountName
     if (!$group_member) {
         Fail-Json -obj $result "Could not find domain user, group, service account or computer named $member"
     }
 
     if ($state -eq "pure") {
-        $pure_members += $group_member
+        $pure_members.Add($group_member.objectSid)
     }
 
     $user_in_group = $false
     foreach ($current_member in $members_before) {
-        if ($current_member.sid -eq $group_member.sid) {
+        if ($current_member.sid -eq $group_member.objectSid) {
             $user_in_group = $true
             break
         }
@@ -72,11 +71,11 @@ foreach ($member in $members) {
 
     if ($state -in @("present", "pure") -and !$user_in_group) {
         Add-ADGroupMember -Identity $name -Members $group_member -WhatIf:$check_mode @extra_args
-        $result.added += $group_member.SamAccountName
+        $result.added.Add($group_member.SamAccountName)
         $result.changed = $true
     } elseif ($state -eq "absent" -and $user_in_group) {
         Remove-ADGroupMember -Identity $name -Members $group_member -WhatIf:$check_mode @extra_args -Confirm:$False
-        $result.removed += $group_member.SamAccountName
+        $result.removed.Add($group_member.SamAccountName)
         $result.changed = $true
     }
 }
@@ -88,7 +87,7 @@ if ($state -eq "pure") {
     foreach ($current_member in $current_members) {
         $user_to_remove = $true
         foreach ($pure_member in $pure_members) {
-            if ($pure_member.sid -eq $current_member.sid) {
+            if ($pure_member -eq $current_member.sid) {
                 $user_to_remove = $false
                 break
             }
@@ -96,7 +95,7 @@ if ($state -eq "pure") {
 
         if ($user_to_remove) {
             Remove-ADGroupMember -Identity $name -Members $current_member -WhatIf:$check_mode @extra_args -Confirm:$False
-            $result.removed += $current_member.SamAccountName
+            $result.removed.Add($current_member.SamAccountName)
             $result.changed = $true
         }
     }
@@ -115,10 +114,10 @@ if ($diff_mode -and $result.changed) {
     if (!$check_mode) {
         $result.diff.after = [Array]$final_members.SamAccountName | Out-String
     } else {
-        $after = [System.Collections.ArrayList][Array]$final_members.SamAccountName
-        $result.removed | ForEach-Object { $after.Remove($_) }
-        $result.added | ForEach-Object { $after.Add($_) }
-        $result.diff.after = [Array]$after | Out-String
+        $after = [System.Collections.Generic.List`1[String]]$result.members
+        $result.removed | ForEach-Object { $after.Remove($_) > $null }
+        $after.AddRange($result.added)
+        $result.diff.after = $after | Out-String
     }
 }
 
