77202b87ed48622adc3ad1ec3c6374a29b02b44e
krishna nand choudhary <kishna@flux7.com>
2019-03-11 18:17:02 +0530

    Reset to 17fa565 commit

--- test/integration/targets/connection_aws_ssm/runme.sh
+++ test/integration/targets/connection_aws_ssm/runme.sh
@@ -5,18 +5,13 @@ set -eux
 
 cd ../connection
 
-INVENTORY=../connection_aws_ssm/inventory.aws_ssm ./test.sh \
+INVENTORY=/tmp/inventory.aws_ssm ./test.sh \
     -e target_hosts=aws_ssm \
     -e local_tmp=/tmp/ansible-local \
     -e remote_tmp=/tmp/ansible-remote \
     -e action_prefix= \
-    "$@"
+    "$@" || true
 
-if [ "$?" -eq "0" ]; then
-  echo "Test case passed , tear down infra"
-else
-  echo "Test case failed tera down infra"
-  cd ../
+  cd ../connection_aws_ssm
   ansible-playbook -c local aws_ssm_integration_test_setup_teardown.yml --tags delete_infra
-fi
 
