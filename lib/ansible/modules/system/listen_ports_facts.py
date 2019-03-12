b5e40b5f5194f18d4f373f55094f7ad0a9c160c2
Nathan Davison <ndavison85@gmail.com>
2019-03-12 21:12:05 +1100

    Fix comment

--- lib/ansible/modules/system/listen_ports_facts.py
+++ lib/ansible/modules/system/listen_ports_facts.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+# (c) 2017, Nathan Davison <ndavison85@gmail.com>
 #
+# -*- coding: utf-8 -*-
 # (c) 2017, Nathan Davison <ndavison85@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
