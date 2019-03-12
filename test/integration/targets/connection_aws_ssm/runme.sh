91381b13ed2bd53081d9867c973267839a612284
gau1991 <gaurav.ashtikar@flux7.com>
2019-03-12 21:12:04 +0530

    Added Windows Integration test support

--- test/integration/targets/connection_aws_ssm/runme.sh
+++ test/integration/targets/connection_aws_ssm/runme.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -eux
-ansible-playbook -c local aws_ssm_integration_test_setup_teardown.yml --tags setup_infra
+
+# Execute Integration for Linux
+ansible-playbook -c local aws_ssm_integration_test_setup_teardown.yml --tags setup_infra,linux
 
 cd ../connection
 
@@ -11,6 +13,22 @@ INVENTORY=../connection_aws_ssm/inventory.aws_ssm ./test.sh \
     -e action_prefix= \
     "$@" || true
 
-  cd ../connection_aws_ssm
-  ansible-playbook -c local aws_ssm_integration_test_setup_teardown.yml --tags delete_infra
+cd ../connection_aws_ssm
 
+ansible-playbook -c local aws_ssm_integration_test_setup_teardown.yml --tags delete_infra
+
+# Execute Integration for Windows
+ansible-playbook -c local aws_ssm_integration_test_setup_teardown.yml --tags setup_infra,windows
+
+cd ../connection
+
+INVENTORY=../connection_aws_ssm/inventory.aws_ssm ./test.sh \
+    -e target_hosts=aws_ssm \
+    -e local_tmp=/tmp/ansible-local \
+    -e remote_tmp=/tmp/ansible-remote \
+    -e action_prefix= \
+    "$@" || true
+
+cd ../connection_aws_ssm
+
+ansible-playbook -c local aws_ssm_integration_test_setup_teardown.yml --tags delete_infra
