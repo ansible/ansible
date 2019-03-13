4424fd469fdda6eb48a6fd476588ed1d08776bc5
Jordan Borean <jborean93@gmail.com>
2019-03-14 08:04:49 +1000

    win_get_url - refactor tests to use httptester (#53765)
    * win_get_url - refactor tests to use httptester
    
    * Use the file lookup instead of slurp

--- lib/ansible/modules/windows/win_get_url.ps1
+++ lib/ansible/modules/windows/win_get_url.ps1
@@ -381,13 +381,13 @@ $request_params = @{
 }
 
 if ($checksum) {
-    $checksum = $checksum.Trim().toLower()
+    $checksum = $checksum.Trim().ToLower()
 }
 if ($checksum_algorithm) {
-    $checksum_algorithm = $checksum_algorithm.Trim().toLower()
+    $checksum_algorithm = $checksum_algorithm.Trim().ToLower()
 }
 if ($checksum_url) {
-    $checksum_url = $checksum_url.Trim().toLower()
+    $checksum_url = $checksum_url.Trim()
 }
 
 # Check for case $checksum variable contain url. If yes, get file data from url and replace original value in $checksum
