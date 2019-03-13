0bb4cb8f988014845031103b02ae4f0e1415bb2e
Fabian von Feilitzsch <fabian@fabianism.us>
2019-03-13 17:48:01 -0400

    add support for label_selectors and automatically parsing resources that expose selectors in their spec

--- lib/ansible/modules/clustering/k8s/k8s_log.py
+++ lib/ansible/modules/clustering/k8s/k8s_log.py
@@ -58,6 +58,8 @@ options:
     - If there is only one container, this will default to that container.
     - If there is more than one container, this option is required.
     required: no
+  label_selectors:
+    description: List of label selectors to use to filter results
 
 extends_documentation_fragment:
   - k8s_auth_options
@@ -98,22 +100,6 @@ class KubernetesLogModule(KubernetesAnsibleModule):
                                          supports_check_mode=True,
                                          **kwargs)
 
-    def execute_module(self):
-        self.client = self.get_api_client()
-        resource = self.find_resource(self.params['kind'], self.params['api_version'], fail=True)
-        if 'log' not in resource.subresources:
-            self.fail(msg='{0} {1} does not support the log resource'.format(resource.group_version, resource.kind))
-
-        kwargs = {}
-        if self.params.get('container'):
-            kwargs['query_params'] = dict(containter=self.params['container'])
-
-        self.exit_json(changed=False, log=resource.log.get(
-            name=self.params['name'],
-            namespace=self.params['namespace'],
-            **kwargs
-        ))
-
     @property
     def argspec(self):
         args = copy.deepcopy(AUTH_ARG_SPEC)
@@ -124,9 +110,82 @@ class KubernetesLogModule(KubernetesAnsibleModule):
                 name=dict(),
                 namespace=dict(),
                 container=dict(),
+                label_selectors=dict(type='list', default=[]),
             )
         )
         return args
+
+    def execute_module(self):
+        name = self.params.get('name')
+        label_selector = ','.join(self.params.get('label_selectors', {}))
+        if name and label_selector:
+            self.fail(msg='Only one of name or label_selectors can be provided')
+
+        self.client = self.get_api_client()
+        resource = self.find_resource(self.params['kind'], self.params['api_version'], fail=True)
+        if 'log' not in resource.subresources:
+            if not self.params.get('name'):
+                self.fail(msg='name must be provided for resources that do not support the log subresource')
+            instance = resource.get(name=self.params['name'], namespace=self.params.get('namespace'))
+            label_selector = ','.join(self.extract_selectors(instance))
+            resource = self.find_resource('Pod', 'v1', fail=True)
+
+        if label_selector:
+            v1_pods = self.find_resource('Pod', 'v1', fail=True)
+            instances = v1_pods.get(namespace=self.params['namespace'], label_selector=label_selector)
+            if not instances.items:
+                self.fail(msg='No pods in namespace {0} matched selector {1}'.format(self.params['namespace'], label_selector))
+            # This matches the behavior of kubectl when logging pods via a selector
+            name = instances.items[0].metadata.name
+
+        kwargs = {}
+        if self.params.get('container'):
+            kwargs['query_params'] = dict(containter=self.params['container'])
+
+        self.exit_json(changed=False, log=resource.log.get(
+            name=name,
+            namespace=self.params.get('namespace'),
+            **kwargs
+        ))
+
+    def extract_selectors(self, instance):
+        # Parses selectors on an object based on the specifications documented here:
+        # https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#label-selectors
+        selectors = []
+        if not instance.spec.selector:
+            self.fail(msg='{0} {1} does not support the log subresource directly, and no Pod selector was found on the object'.format(
+                      '/'.join(instance.group, instance.apiVersion), instance.kind))
+
+        if not (instance.spec.selector.matchLabels or instance.spec.selector.matchExpressions):
+            # A few resources (like DeploymentConfigs) just use a simple key:value style instead of supporting expressions
+            for k, v in dict(instance.spec.selector).items():
+                selectors.append('{0}={1}'.format(k, v))
+            return selectors
+
+        if instance.spec.selector.matchLabels:
+            for k, v in dict(instance.spec.selector.matchLabels).items():
+                selectors.append('{0}={1}'.format(k, v))
+
+        if instance.spec.selector.matchExpressions:
+            for expression in instance.spec.selector.matchExpressions:
+                operator = expression.operator
+
+                if operator == 'Exists':
+                    selectors.append(expression.key)
+                elif operator == 'DoesNotExist':
+                    selectors.append('!{0}'.format(expression.key))
+                elif operator in ['In', 'NotIn']:
+                    selectors.append('{key} {operator} {values}'.format(
+                        key=expression.key,
+                        operator=operator.lower(),
+                        values='({0})'.format(
+                            ', '.join(expression.values)
+                        )
+                    ))
+                else:
+                    self.fail(msg='The k8s_log module does not support the {0} matchExpression operator'.format(operator.lower()))
+
+        return selectors
 
 
 def main():
