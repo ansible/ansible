4e1751013db082dc0b5aa7445d6bf7ee62fe9c7c
krisvasudevan <43845852+krisvasudevan@users.noreply.github.com>
2019-03-13 23:49:05 +0530

    Update nios_member.py

--- lib/ansible/modules/net_tools/nios/nios_member.py
+++ lib/ansible/modules/net_tools/nios/nios_member.py
@@ -287,6 +287,7 @@ options:
     description:
       - Flag for initiating a create token request for pre-provisioned members.
     type: bool
+    default: False
   state:
     description:
       - Configures the intended state of the instance of the object on
@@ -468,7 +469,7 @@ def main():
         syslog_servers=dict(type='list', elements='dict', options=syslog_spec),
         pre_provisioning=dict(type='dict', elements='dict', options=pre_prov_spec),
         extattrs=dict(type='dict'),
-        create_token=dict(type='bool'),
+        create_token=dict(type='bool',default=False),
     )
 
     argument_spec = dict(
