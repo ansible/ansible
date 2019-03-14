2f25c133fb36c18974cfded46f050137168b9b17
Sam Doran <sdoran@redhat.com>
2019-03-14 10:31:47 -0400

    Correctly count processors on ARM systems. (#52884)
    - Add unit tests for Linux CPU info
    - Add cpuinfo output from several systems for unit tests
    
    Co-authored-by: Abhijeet Kasurde <akasurde@redhat.com>

---
+++ test/units/module_utils/facts/hardware/test_linux_get_cpu_info.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+from ansible.module_utils.facts.hardware import linux
+
+from . linux_data import CPU_INFO_TEST_SCENARIOS
+
+
+def test_get_cpu_info(mocker):
+    module = mocker.Mock()
+    inst = linux.LinuxHardware(module)
+
+    mocker.patch('os.path.exists', return_value=False)
+    mocker.patch('os.access', return_value=True)
+    for test in CPU_INFO_TEST_SCENARIOS:
+        mocker.patch('ansible.module_utils.facts.hardware.linux.get_file_lines', side_effect=[[], test['cpuinfo']])
+        collected_facts = {'ansible_architecture': test['architecture']}
+        assert test['expected_result'] == inst.get_cpu_facts(collected_facts=collected_facts)
