bcd3f2dc84575b825231d66b438e40bd2a10a643
Matt Clay <matt@mystile.com>
2019-03-14 11:00:36 -0700

    Support non-ascii characters in changelogs.

--- packaging/release/changelogs/changelog.py
+++ packaging/release/changelogs/changelog.py
@@ -25,6 +25,7 @@ except ImportError:
     argcomplete = None
 
 from ansible import constants as C
+from ansible.module_utils.six import string_types
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 CHANGELOG_DIR = os.path.join(BASE_DIR, 'changelogs')
@@ -305,7 +306,7 @@ class ChangelogFragmentLinter(object):
 
         for section, lines in fragment.content.items():
             if section == self.config.prelude_name:
-                if not isinstance(lines, str):
+                if not isinstance(lines, string_types):
                     errors.append((fragment.path, 0, 0, 'section "%s" must be type str not %s' % (section, type(lines).__name__)))
             else:
                 # doesn't account for prelude but only the RM should be adding those
@@ -317,13 +318,13 @@ class ChangelogFragmentLinter(object):
 
             if isinstance(lines, list):
                 for line in lines:
-                    if not isinstance(line, str):
+                    if not isinstance(line, string_types):
                         errors.append((fragment.path, 0, 0, 'section "%s" list items must be type str not %s' % (section, type(line).__name__)))
                         continue
 
                     results = rstcheck.check(line, filename=fragment.path, report_level=docutils.utils.Reporter.WARNING_LEVEL)
                     errors += [(fragment.path, 0, 0, result[1]) for result in results]
-            elif isinstance(lines, str):
+            elif isinstance(lines, string_types):
                 results = rstcheck.check(lines, filename=fragment.path, report_level=docutils.utils.Reporter.WARNING_LEVEL)
                 errors += [(fragment.path, 0, 0, result[1]) for result in results]
 
