e3fbbd78050606b8b753b21c7c860e2ab292e7ee
s-hertel <shertel@redhat.com>
2019-03-14 13:45:03 -0500

    remove the extra imports

--- test/integration/targets/inventory_aws_conformance/lib/boto/__init__.py
+++ test/integration/targets/inventory_aws_conformance/lib/boto/__init__.py
@@ -1,5 +1,2 @@
-import boto.exception as exception  # pylint: disable=useless-import-alias
 import boto.exceptions as exceptions  # pylint: disable=useless-import-alias
 import boto.session as session  # pylint: disable=useless-import-alias
-
-from boto.session import Session
