e6470d0280668b0d77a50c9723c170d3086ff831
s-hertel <shertel@redhat.com>
2019-03-14 13:40:22 -0500

    disable pylint useless-import-alias errors

--- test/integration/targets/inventory_aws_conformance/lib/boto/__init__.py
+++ test/integration/targets/inventory_aws_conformance/lib/boto/__init__.py
@@ -1,5 +1,5 @@
-import boto.exception as exception
-import boto.exceptions as exceptions
-import boto.session as session
+import boto.exception as exception  # pylint: disable=useless-import-alias
+import boto.exceptions as exceptions  # pylint: disable=useless-import-alias
+import boto.session as session  # pylint: disable=useless-import-alias
 
 from boto.session import Session
