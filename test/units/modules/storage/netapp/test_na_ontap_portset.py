38bf1ab0d51f1e2caa7e21da1282660f4f5de8a5
Chris Archibald <carchi@netapp.com>
2019-03-14 08:39:11 -0700

    Ability to add port in portset (#52734)
    * Revert "changes to clusteR"
    
    This reverts commit 33ee1b71e4bc8435fb315762a871f8c4cb6c5f80.
    
    * Revert "changes to clusteR"
    
    This reverts commit 33ee1b71e4bc8435fb315762a871f8c4cb6c5f80.
    
    * Revert "Revert "changes to clusteR""
    
    This reverts commit f1104a37b42886aebb4d2b2ab27c91c96d97858a.
    
    * Revert "Revert "changes to clusteR""
    
    This reverts commit f1104a37b42886aebb4d2b2ab27c91c96d97858a.
    
    * add ports

--- test/units/modules/storage/netapp/test_na_ontap_portset.py
+++ test/units/modules/storage/netapp/test_na_ontap_portset.py
@@ -75,7 +75,8 @@ class MockONTAPConnection(object):
         xml = netapp_utils.zapi.NaElement('xml')
         data = {'num-records': 1,
                 'attributes-list': {'portset-info': {'portset-name': portset,
-                                                     'vserver': vserver, 'portset-type': type}}}
+                                                     'vserver': vserver, 'portset-type': type,
+                                                     'portset-port-total': '0'}}}
         xml.translate_struct(data)
         print(xml.to_string())
         return xml
@@ -134,7 +135,7 @@ class TestMyModule(unittest.TestCase):
         assert portset is None
 
     def test_ensure_portset_apply_called(self):
-        ''' a more interesting test '''
+        ''' Test successful create '''
         module_args = {'name': 'create'}
         module_args.update(self.set_default_args())
         set_module_args(module_args)
@@ -153,10 +154,33 @@ class TestMyModule(unittest.TestCase):
         portset = my_obj.portset_get()
         print('Info: test_portset_get: %s' % repr(portset))
         assert portset is not None
-        assert 'create' == portset['portset_name']
         with pytest.raises(AnsibleExitJson) as exc:
             my_obj.apply()
         print('Info: test_portset_apply: %s' % repr(exc.value))
         assert exc.value.args[0]['changed']
-        portset = my_obj.portset_get()
-        assert 'create' == portset['portset_name']
+
+    def test_modify_ports(self):
+        ''' Test modify_portset method '''
+        module_args = {'ports': ['l1', 'l2']}
+        module_args.update(self.set_default_args())
+        set_module_args(module_args)
+        my_obj = my_module()
+        if not self.use_vsim:
+            my_obj.server = MockONTAPConnection('portset')
+        with pytest.raises(AnsibleExitJson) as exc:
+            my_obj.apply()
+        print('Info: test_portset_apply: %s' % repr(exc.value))
+        assert exc.value.args[0]['changed']
+
+    def test_delete_portset(self):
+        ''' Test successful delete '''
+        module_args = {'state': 'absent'}
+        module_args.update(self.set_default_args())
+        set_module_args(module_args)
+        my_obj = my_module()
+        if not self.use_vsim:
+            my_obj.server = MockONTAPConnection('portset')
+        with pytest.raises(AnsibleExitJson) as exc:
+            my_obj.apply()
+        print('Info: test_portset_apply: %s' % repr(exc.value))
+        assert exc.value.args[0]['changed']
