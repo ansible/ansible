39f1ce5b690a48f5b9e88e6f3c01437e8f3b39a2
Matt Clay <matt@mystile.com>
2019-03-13 23:26:20 -0700

    Enforce str and list types on sections.

--- packaging/release/changelogs/changelog.py
+++ packaging/release/changelogs/changelog.py
@@ -306,12 +306,18 @@ class ChangelogFragmentLinter(object):
         for section, lines in fragment.content.items():
             if section not in self.config.sections:
                 errors.append((fragment.path, 0, 0, 'invalid section: %s' % section))
+            elif section == self.config.prelude_name:
+                if not isinstance(lines, str):
+                    errors.append((fragment.path, 0, 0, 'section "%s" must be type str not %s' % (section, type(lines).__name__)))
+            else:
+                if not isinstance(lines, list):
+                    errors.append((fragment.path, 0, 0, 'section "%s" must be type list not %s' % (section, type(lines).__name__)))
 
             if isinstance(lines, list):
                 for line in lines:
                     results = rstcheck.check(line, filename=fragment.path, report_level=docutils.utils.Reporter.WARNING_LEVEL)
                     errors += [(fragment.path, 0, 0, result[1]) for result in results]
-            else:
+            elif isinstance(lines, str):
                 results = rstcheck.check(lines, filename=fragment.path, report_level=docutils.utils.Reporter.WARNING_LEVEL)
                 errors += [(fragment.path, 0, 0, result[1]) for result in results]
 
