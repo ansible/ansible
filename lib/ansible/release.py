995a7e6453af1c6992a717c5d8e1dd7a83f987b8
Toshio Kuratomi <a.badger@gmail.com>
2019-03-14 21:39:41 -0700

    Update Ansible release version to v2.6.15.post0.

--- lib/ansible/release.py
+++ lib/ansible/release.py
@@ -19,6 +19,6 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-__version__ = '2.6.15'
+__version__ = '2.6.15.post0'
 __author__ = 'Ansible, Inc.'
 __codename__ = 'Heartbreaker'
