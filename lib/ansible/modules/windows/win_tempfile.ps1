a890d96ac6061d196d1b0181fa1d084ab90f7f4a
Jordan Borean <jborean93@gmail.com>
2019-03-15 11:58:04 +1000

    win_tempfile - return absolute path on created temp file

--- lib/ansible/modules/windows/win_tempfile.ps1
+++ lib/ansible/modules/windows/win_tempfile.ps1
@@ -16,7 +16,11 @@ Function New-TempFile {
         $randomname = [System.IO.Path]::GetRandomFileName()
         $temppath = (Join-Path -Path $path -ChildPath "$prefix$randomname$suffix")
         Try {
-            New-Item -Path $temppath -ItemType $type -WhatIf:$checkmode | Out-Null
+            $file = New-Item -Path $temppath -ItemType $type -WhatIf:$checkmode
+            # Makes sure we get the full absolute path of the created temp file and not a relative or DOS 8.3 dir
+            if (-not $checkmode) {
+                $temppath = $file.FullName
+            }
         } Catch {
             $temppath = $null
             $error = $_
