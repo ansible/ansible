484c6ea7f43c2991c96ac6553883286962178e43
Jordan Borean <jborean93@gmail.com>
2019-03-15 13:34:03 +1000

    Fix issues on older PS versions

--- lib/ansible/modules/windows/win_owner.ps1
+++ lib/ansible/modules/windows/win_owner.ps1
@@ -39,8 +39,9 @@ Try {
         $result.changed = $true
     }
 
-    If ($recurse) {
-        $files = Get-ChildItem -LiteralPath $path -Force -Recurse
+    If ($recurse -and $file -is [System.IO.DirectoryInfo]) {
+        # Get-ChildItem falls flat on pre PSv5 when dealing with complex path chars
+        $files = $file.EnumerateFileSystemInfos("*", [System.IO.SearchOption]::AllDirectories)
         ForEach($file in $files){
             $acl = Get-Acl -LiteralPath $file.FullName
 
