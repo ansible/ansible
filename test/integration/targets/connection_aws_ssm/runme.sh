e35f6e03c7cc37beb2f0d7f9d87a76c964c4ad52
gau1991 <gaurav.ashtikar@flux7.com>
2019-03-15 16:51:22 +0530

    Fixed wrong Windows AMI ID

--- test/integration/targets/connection_aws_ssm/runme.sh
+++ test/integration/targets/connection_aws_ssm/runme.sh
@@ -18,7 +18,7 @@ INVENTORY=../connection_aws_ssm/inventory-linux.aws_ssm ./test.sh \
 INVENTORY=../connection_aws_ssm/inventory-windows.aws_ssm ./test.sh \
     -e target_hosts=aws_ssm \
     -e local_tmp=/tmp/ansible-local \
-    -e remote_tmp=c:/windows/temp/ansible-remote
+    -e remote_tmp=c:/windows/temp/ansible-remote \
     -e action_prefix= \
     "$@" || true
 
