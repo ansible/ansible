288de0ebd877f026fb83ac84d43959f0529428b8
Abhijeet Kasurde <akasurde@redhat.com>
2019-03-16 11:18:01 +0530

    dataloader: check exact value of dir (#52021)
    Include path in role with directory which has 'tasks' as end.
    For example, roles/sometasks/templates is now considered while searching path.
    
    Fixes: #42585
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- test/integration/targets/template/runme.sh
+++ test/integration/targets/template/runme.sh
@@ -9,3 +9,6 @@ ansible testhost -i testhost, -m debug -a 'msg={{ hostvars["localhost"] }}' -e "
 
 # Test for https://github.com/ansible/ansible/issues/27262
 ansible-playbook ansible_managed.yml -c  ansible_managed.cfg -i ../../inventory -e @../../integration_config.yml -v "$@"
+
+# Test for #42585
+ANSIBLE_ROLES_PATH=../ ansible-playbook custom_template.yml -i ../../inventory -e @../../integration_config.yml -v "$@"
