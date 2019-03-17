ef6da8ec5c38261f39f221ab45eb6ba2af88fe17
David Passante <david.passante@orange.com>
2019-03-17 08:38:02 +0100

    cloudstack: streamline modules doc (part 4) (#53874)
    * cloudstack: streamline modules doc (part 4)
    
    * Parameter types added
    * Copyright format fixes
    * Doc style fixes
    * Examples format fixes
    * validate-module errors fixes
    
    * cs_network_offering: Rollback of the deletion of the choice list for supported_services param

--- lib/ansible/modules/cloud/cloudstack/cs_role_permission.py
+++ lib/ansible/modules/cloud/cloudstack/cs_role_permission.py
@@ -16,64 +16,70 @@ description:
     - Create, update and remove CloudStack role permissions.
     - Managing role permissions only supported in CloudStack >= 4.9.
 version_added: '2.6'
-author: "David Passante (@dpassante)"
+author: David Passante (@dpassante)
 options:
   name:
     description:
       - The API name of the permission.
+    type: str
     required: true
   role:
     description:
       - Name or ID of the role.
+    type: str
     required: true
   permission:
     description:
       - The rule permission, allow or deny. Defaulted to deny.
+    type: str
     choices: [ allow, deny ]
     default: deny
   state:
     description:
       - State of the role permission.
+    type: str
     choices: [ present, absent ]
     default: present
   description:
     description:
       - The description of the role permission.
+    type: str
   parent:
     description:
       - The parent role permission uuid. use 0 to move this rule at the top of the list.
+    type: str
 extends_documentation_fragment: cloudstack
 '''
 
 EXAMPLES = '''
-# Create a role permission
-- local_action:
-    module: cs_role_permission
-    role: "My_Custom_role"
-    name: "createVPC"
-    permission: "allow"
-    description: "My comments"
+- name: Create a role permission
+  cs_role_permission:
+    role: My_Custom_role
+    name: createVPC
+    permission: allow
+    description: My comments
+  delegate_to: localhost
 
-# Remove a role permission
-- local_action:
-    module: cs_role_permission
+- name: Remove a role permission
+  cs_role_permission:
     state: absent
-    role: "My_Custom_role"
-    name: "createVPC"
+    role: My_Custom_role
+    name: createVPC
+  delegate_to: localhost
 
-# Update a system role permission
-- local_action:
-    module: cs_role_permission
-    role: "Domain Admin"
-    name: "createVPC"
-    permission: "deny"
+- name: Update a system role permission
+  cs_role_permission:
+    role: Domain Admin
+    name: createVPC
+    permission: deny
+  delegate_to: localhost
 
-# Update rules order. Move the rule at the top of list
-- local_action:
-    module: cs_role_permission
-    role: "Domain Admin"
-    name: "createVPC"
+- name: Update rules order. Move the rule at the top of list
+  cs_role_permission:
+    role: Domain Admin
+    name: createVPC
     parent: 0
+  delegate_to: localhost
 '''
 
 RETURN = '''
