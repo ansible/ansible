2f792ea85ca0f87513840a20273824ffd6b88020
Nikhil Singh <nikhil.singh@sap.com>
2019-03-19 08:08:06 +0100

    Added virtual machines config context

--- lib/ansible/plugins/inventory/netbox.py
+++ lib/ansible/plugins/inventory/netbox.py
@@ -262,9 +262,14 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
     def extract_config_context(self, host):
         try:
             if self.config_context:
-                url = self.api_endpoint + "/api/dcim/devices/" + str(host["id"])
-                device_lookup = self._fetch_information(url)
-                return [device_lookup["config_context"]]
+                if 'device_role' in host:
+                    url = self.api_endpoint + "/api/dcim/devices/" + to_text(host["id"])
+                    device_lookup = self._fetch_information(url)
+                    return [device_lookup["config_context"]]
+                elif 'role' in host:
+                    url = self.api_endpoint + "/api/virtualization/virtual-machines/" + to_text(host["id"])
+                    device_lookup = self._fetch_information(url)
+                    return [device_lookup["config_context"]]
         except Exception:
             return
 
