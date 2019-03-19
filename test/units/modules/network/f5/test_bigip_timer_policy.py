665259d039f06c09961e64b0827b1f251313c592
Wojciech Wypior <w.wypior@f5.com>
2019-03-19 06:03:47 +0100

    BIGIP: Bugfix.multiple modules 3 (#53969)
    * Moving comparision functions to compare.py from common.py
    
    * Refactors main() function and module manager in multiple modules in line with recent changes
    Adds variable types to docs
    Refactors unit tests to remove deprecated parameters

--- test/units/modules/network/f5/test_bigip_timer_policy.py
+++ test/units/modules/network/f5/test_bigip_timer_policy.py
@@ -92,9 +92,11 @@ class TestManager(unittest.TestCase):
         set_module_args(dict(
             name='foo',
             description='my description',
-            server='localhost',
-            password='password',
-            user='admin'
+            provider=dict(
+                server='localhost',
+                password='password',
+                user='admin'
+            )
         ))
 
         module = AnsibleModule(
