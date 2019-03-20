98f0314804bd70e9156d68290b45fa679137da66
zikalino <zikalino@microsoft.com>
2019-03-20 13:42:56 +0800

    more checks

--- lib/ansible/modules/cloud/azure/azure_rm_resource.py
+++ lib/ansible/modules/cloud/azure/azure_rm_resource.py
@@ -263,14 +263,15 @@ class AzureRMResource(AzureRMModuleBase):
         if not self.api_version:
             try:
                 # extract provider and resource type
-                provider = self.url.split("/providers/")[1].split("/")[0]
-                resourceType = self.url.split(provider + "/")[1].split("/")[0]
-                url = "/subscriptions/" + self.subscription_id + "/providers/" + provider
-                api_versions = json.loads(self.mgmt_client.query(url, "GET", { 'api-version': '2015-01-01'}, None, None, [200], 0, 0).text)
-                for rt in api_versions['resourceTypes']:
-                    if rt['resourceType'].lower() == resourceType.lower():
-                        self.api_version = rt['apiVersions'][0]
-                        break
+                if "/provider/" in self.url:
+                    provider = self.url.split("/providers/")[1].split("/")[0]
+                    resourceType = self.url.split(provider + "/")[1].split("/")[0]
+                    url = "/subscriptions/" + self.subscription_id + "/providers/" + provider
+                    api_versions = json.loads(self.mgmt_client.query(url, "GET", { 'api-version': '2015-01-01'}, None, None, [200], 0, 0).text)
+                    for rt in api_versions['resourceTypes']:
+                        if rt['resourceType'].lower() == resourceType.lower():
+                            self.api_version = rt['apiVersions'][0]
+                            break
                 if not self.api_version:
                     self.fail("Couldn't find api version for {0}/{1}".format(provider, resourceType))
             except Exception as exc:
