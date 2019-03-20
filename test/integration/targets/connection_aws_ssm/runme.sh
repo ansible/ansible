0d6864efc4592de3defa97b5c10770cc207836f3
gau1991 <gaurav.ashtikar@flux7.com>
2019-03-21 00:56:41 +0530

    Fixed Integration tests

--- test/integration/targets/connection_aws_ssm/runme.sh
+++ test/integration/targets/connection_aws_ssm/runme.sh
@@ -1,28 +1,38 @@
 #!/usr/bin/env bash
-set -eux
 
 # Setup Environment
-ansible-playbook -c local aws_ssm_integration_test_setup_teardown.yml --tags setup_infra
+ansible-playbook -c local aws_ssm_integration_test_setup_teardown.yml \
+    --tags setup_infra \
+    "$@"
 
-cd ../connection
+# Execute test cases only on Python 2.7
+if [ -e aws-env-vars.sh ]; then
 
-# Execute Integration tests for Linux
-INVENTORY=../connection_aws_ssm/inventory-linux.aws_ssm ./test.sh \
-    -e target_hosts=aws_ssm \
-    -e local_tmp=/tmp/ansible-local \
-    -e remote_tmp=/tmp/ansible-remote \
-    -e action_prefix= \
-    "$@" || true
+    # Export the AWS Keys
+    . ./aws-env-vars.sh
 
-# Execute Integration tests for Windows
-INVENTORY=../connection_aws_ssm/inventory-windows.aws_ssm ./test.sh \
-    -e target_hosts=aws_ssm \
-    -e local_tmp=/tmp/ansible-local \
-    -e remote_tmp=c:/windows/temp/ansible-remote \
-    -e action_prefix=win_ \
-    "$@" || true
+    cd ../connection
 
-cd ../connection_aws_ssm
+    # Execute Integration tests for Linux
+    INVENTORY=../connection_aws_ssm/inventory-linux.aws_ssm ./test.sh \
+        -e target_hosts=aws_ssm \
+        -e local_tmp=/tmp/ansible-local \
+        -e remote_tmp=/tmp/ansible-remote \
+        -e action_prefix= \
+        "$@"
 
-# Destroy Environment
-ansible-playbook -c local aws_ssm_integration_test_setup_teardown.yml --tags delete_infra
+    # Execute Integration tests for Windows
+    INVENTORY=../connection_aws_ssm/inventory-windows.aws_ssm ./test.sh \
+        -e target_hosts=aws_ssm \
+        -e local_tmp=/tmp/ansible-local \
+        -e remote_tmp=c:/windows/temp/ansible-remote \
+        -e action_prefix=win_ \
+        "$@"
+
+    cd ../connection_aws_ssm
+
+    # Destroy Environment
+    ansible-playbook -c local aws_ssm_integration_test_setup_teardown.yml \
+        --tags delete_infra \
+        "$@"
+fi
