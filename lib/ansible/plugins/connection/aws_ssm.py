c9c1256907a326783bb774e873a9f4670b44fbba
HanumanthaRao MVL <hanumanth@flux7.com>
2019-03-20 21:37:48 +0530

    SSM Usage examples with dynamic inventory plugin

--- lib/ansible/plugins/connection/aws_ssm.py
+++ lib/ansible/plugins/connection/aws_ssm.py
@@ -92,6 +92,57 @@ EXAMPLES = r'''
       win_file:
         path: C:\Windows\temp
         state: directory
+
+# Making use of Dynamic Inventory Plugin
+# =======================================
+# aws_ec2.yml (Dynamic Inventory - Linux)
+# This will return the Instance IDs matching the filter
+plugin: aws_ec2
+regions:
+    - us-east-1
+hostnames:
+    - instance-id
+filters:
+    tag:SSMTag: ssmlinux
+# -----------------------
+- name: install aws-cli
+  hosts: all
+  gather_facts: false
+  vars:
+    ansible_connection: aws_ssm
+    ansible_aws_ssm_bucket_name: nameofthebucket
+    ansible_aws_ssm_region: us-east-1
+  tasks:
+  - name: aws-cli
+    raw: yum install -y awscli
+    tags: aws-cli
+# Execution: ansible-playbook linux.yaml -i aws_ec2.yml
+# The playbook tasks will get executed on the instance ids returned from the dynamic inventory plugin using ssm connection.
+# =====================================================
+# aws_ec2.yml (Dynamic Inventory - Windows)
+plugin: aws_ec2
+regions:
+    - us-east-1
+hostnames:
+    - instance-id
+filters:
+    tag:SSMTag: ssmwindows
+# -----------------------
+- name: Create a dir.
+  hosts: all
+  gather_facts: false
+  vars:
+    ansible_connection: aws_ssm
+    ansible_shell_type: powershell
+    ansible_aws_ssm_bucket_name: nameofthebucket
+    ansible_aws_ssm_region: us-east-1
+  tasks:
+    - name: Create the directory
+      win_file:
+        path: C:\Temp\SSM_Testing5
+        state: directory
+# Execution:  ansible-playbook win_file.yaml -i aws_ec2.yml
+# The playbook tasks will get executed on the instance ids returned from the dynamic inventory plugin using ssm connection.
 '''
 
 import os
