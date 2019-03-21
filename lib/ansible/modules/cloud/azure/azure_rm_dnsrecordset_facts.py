eb7f058a9834544d0180d4375560fdcbf8b68784
Zim Kalinowski <zikalino@microsoft.com>
2019-03-21 17:01:51 +0800

    more doc improvements (#54148)

--- lib/ansible/modules/cloud/azure/azure_rm_dnsrecordset_facts.py
+++ lib/ansible/modules/cloud/azure/azure_rm_dnsrecordset_facts.py
@@ -73,24 +73,23 @@ azure_dnsrecordset:
     returned: always
     type: list
     example: [
-            {
-                "etag": "60ac0480-44dd-4881-a2ed-680d20b3978e",
-                "id": "/subscriptions/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX/resourceGroups/testing/providers/Microsoft.Network/dnszones/newzone.com/A/servera",
-                "name": "servera",
-                "properties": {
-                    "ARecords": [
-                        {
-                            "ipv4Address": "10.4.5.7"
-                        },
-                        {
-                            "ipv4Address": "2.4.5.8"
-                        }
-                    ],
-                    "TTL": 12900
+    {
+        "etag": "60ac0480-44dd-4881-a2ed-680d20b3978e",
+        "id": "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/myResourceGroup/providers/Microsoft.Network/dnszones/newzone.com/A/servera",
+        "name": "servera",
+        "properties": {
+            "ARecords": [
+                {
+                    "ipv4Address": "10.4.5.7"
                 },
-                "type": "Microsoft.Network/dnszones/A"
-            }
-        ]
+                {
+                    "ipv4Address": "2.4.5.8"
+                }
+            ],
+            "TTL": 12900
+        },
+        "type": "Microsoft.Network/dnszones/A"
+    }]
 dnsrecordsets:
     description: List of record set dicts, which shares the same hierarchy as azure_rm_dnsrecordset module's parameter.
     returned: always
