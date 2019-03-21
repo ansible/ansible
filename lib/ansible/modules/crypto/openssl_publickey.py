5753796a6688c04d5342fcf7e2c20441c4d31ede
Felix Fontein <felix@fontein.de>
2019-03-21 20:10:39 +0100

    Cleanup.

--- lib/ansible/modules/crypto/openssl_publickey.py
+++ lib/ansible/modules/crypto/openssl_publickey.py
@@ -18,7 +18,7 @@ version_added: "2.3"
 short_description: Generate an OpenSSL public key from its private key.
 description:
     - This module allows one to (re)generate OpenSSL public keys from their private keys.
-    -  It uses the pyOpenSSL python library to interact with openssl.
+    - It uses the pyOpenSSL python library to interact with openssl.
     - Keys are generated in PEM format.
     - This module works only if the version of PyOpenSSL is recent enough (> 16.0.0).
 requirements:
@@ -131,7 +131,6 @@ fingerprint:
       sha512: "fd:ed:5e:39:48:5f:9f:fe:7f:25:06:3f:79:08:cd:ee:a5:e7:b3:3d:13:82:87:1f:84:e1:f5:c7:28:77:53:94:86:56:38:69:f0:d9:35:22:01:1e:a6:60:...:0f:9b"
 '''
 
-import hashlib
 import os
 import traceback
 
