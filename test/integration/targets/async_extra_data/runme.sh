70a7384ca5bbf1772d6ee716ad72fdc7ca68a1dc
Matt Clay <matt@mystile.com>
2019-03-21 16:09:06 -0700

    Switch async_extra_data test to inventory file.

--- test/integration/targets/async_extra_data/runme.sh
+++ test/integration/targets/async_extra_data/runme.sh
@@ -3,7 +3,7 @@
 set -eux
 
 # Verify that extra data before module JSON output during async call is ignored.
-ANSIBLE_DEBUG=0 LC_ALL=bogus ansible-playbook test_async.yml -i localhost, -e ansible_connection=ssh -v "$@"
+ANSIBLE_DEBUG=0 LC_ALL=bogus ansible-playbook test_async.yml -i inventory -v "$@"
 # Verify that the warning exists by examining debug output.
-ANSIBLE_DEBUG=1 LC_ALL=bogus ansible-playbook test_async.yml -i localhost, -e ansible_connection=ssh -v "$@" \
+ANSIBLE_DEBUG=1 LC_ALL=bogus ansible-playbook test_async.yml -i inventory -v "$@" \
     | grep 'bash: warning: setlocale: LC_ALL: cannot change locale (bogus)' > /dev/null
