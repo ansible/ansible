956b7f86173739d6df97e0ff44158feeb8f92cc5
Matt Clay <matt@mystile.com>
2019-03-21 17:31:04 -0700

    Clean up group_by integration test. (#54206)
    * Split group creation out into separate file.
    * Move skipped group into separate playbook.

--- test/integration/targets/group_by/runme.sh
+++ test/integration/targets/group_by/runme.sh
@@ -3,3 +3,4 @@
 set -eux
 
 ansible-playbook test_group_by.yml -i inventory.group_by -v "$@"
+ANSIBLE_HOST_PATTERN_MISMATCH=warning ansible-playbook test_group_by_skipped.yml -i inventory.group_by -v "$@"
