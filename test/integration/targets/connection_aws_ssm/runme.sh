15c0b8fae81e394673640be27d2cd2c51d8f399a
gau1991 <gaurav.ashtikar@flux7.com>
2019-03-23 01:47:21 +0530

    Fixed Integration tests with Python 3

--- test/integration/targets/connection_aws_ssm/runme.sh
+++ test/integration/targets/connection_aws_ssm/runme.sh
@@ -1,34 +1,42 @@
 #!/usr/bin/env bash
 
-# Setup Environment
-ansible-playbook -c local aws_ssm_integration_test_setup.yml "$@"
+set -eux
 
-# Execute test cases only on Python 2.7
-if [ -e aws-env-vars.sh ]; then
+CMD_ARGS=$@
 
-    # Export the AWS Keys
-    . ./aws-env-vars.sh
-
-    cd ../connection
-
-    # Execute Integration tests for Linux
-    INVENTORY=../connection_aws_ssm/inventory-linux.aws_ssm ./test.sh \
-        -e target_hosts=aws_ssm \
-        -e local_tmp=/tmp/ansible-local \
-        -e remote_tmp=/tmp/ansible-remote \
-        -e action_prefix= \
-        "$@" || true
-
-    # Execute Integration tests for Windows
-    INVENTORY=../connection_aws_ssm/inventory-windows.aws_ssm ./test.sh \
-        -e target_hosts=aws_ssm \
-        -e local_tmp=/tmp/ansible-local \
-        -e remote_tmp=c:/windows/temp/ansible-remote \
-        -e action_prefix=win_ \
-        "$@" || true
+# Destroy Environment
+cleanup() {
 
     cd ../connection_aws_ssm
 
-    # Destroy Environment
-    ansible-playbook -c local aws_ssm_integration_test_teardown.yml "$@"
-fi
+    ansible-playbook -c local aws_ssm_integration_test_teardown.yml $CMD_ARGS
+
+}
+
+trap "cleanup" EXIT
+
+# Setup Environment
+ansible-playbook -c local aws_ssm_integration_test_setup.yml "$@"
+
+# Export the AWS Keys
+set +x
+. ./aws-env-vars.sh
+set -x
+
+cd ../connection
+
+# Execute Integration tests for Linux
+INVENTORY=../connection_aws_ssm/inventory-linux.aws_ssm ./test.sh \
+    -e target_hosts=aws_ssm \
+    -e local_tmp=/tmp/ansible-local \
+    -e remote_tmp=/tmp/ansible-remote \
+    -e action_prefix= \
+    "$@"
+
+# Execute Integration tests for Windows
+INVENTORY=../connection_aws_ssm/inventory-windows.aws_ssm ./test.sh \
+    -e target_hosts=aws_ssm \
+    -e local_tmp=/tmp/ansible-local \
+    -e remote_tmp=c:/windows/temp/ansible-remote \
+    -e action_prefix=win_ \
+    "$@"
