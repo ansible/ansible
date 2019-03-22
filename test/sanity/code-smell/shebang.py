0bd09b274e814c5dad7bc90d6b70ef8f3d2a68fb
Toshio Kuratomi <a.badger@gmail.com>
2019-03-21 22:47:17 -0700

    Add a script to generate twitter and mailing list announcements
    Announcements taken from https://github.com/ansible/community/wiki/RelEng:-ReleaseProcess
    and then cleaned up:
    
    * Update issue reporting blurb from feedback from acozine and gundalow
    * Add a subject and to line for email output
    * Ignore long line tests on the jinja templates (as jinja doesn't give
      enough control to get rid of newlines when text wrapping)
    * Skip shebang and compile tests for older pythons since this is a
      release engineer-only script.  (ok'd by mattclay)

--- test/sanity/code-smell/shebang.py
+++ test/sanity/code-smell/shebang.py
@@ -34,6 +34,8 @@ def main():
         'test/integration/targets/win_module_utils/library/legacy_only_old_way_win_line_ending.ps1',
         'test/utils/shippable/timing.py',
         'test/integration/targets/old_style_modules_posix/library/helloworld.sh',
+        # Python 3-only.  Only run by release engineers
+        'hacking/release-announcement.py',
     ])
 
     # see https://unicode.org/faq/utf_bom.html#bom1
