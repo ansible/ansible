0f4c6793f92dd2cf9330b12841237d73a4c3846a
Kevin Breit <kevin.breit@kevinbreit.net>
2019-03-23 08:29:08 -0500

    meraki_admin - Add documentation for tags and network access (#51415)
    * Add documentation for tags and network access
    * Documentation changes from dag and sam
    * Change examples to include proper YAML and not dict
    * Update lib/ansible/modules/network/meraki/meraki_admin.py
    
    Co-Authored-By: kbreit <kevin.breit@kevinbreit.net>
    (cherry picked from commit bcbcd209f4f20e14e460a21ca3eeaec4a4dfaa37)

--- lib/ansible/modules/network/meraki/meraki_admin.py
+++ lib/ansible/modules/network/meraki/meraki_admin.py
@@ -39,10 +39,29 @@ options:
         - Tags the administrator has privileges on.
         - When creating a new administrator, C(org_name), C(network), or C(tags) must be specified.
         - If C(none) is specified, C(network) or C(tags) must be specified.
+        suboptions:
+            tag:
+                description:
+                - Object tag which privileges should be assigned.
+                type: str
+            access:
+                description:
+                - The privilege of the dashboard administrator for the tag.
+                type: str
     networks:
         description:
         - List of networks the administrator has privileges on.
         - When creating a new administrator, C(org_name), C(network), or C(tags) must be specified.
+        suboptions:
+            id:
+                description:
+                - Network ID for which administrator should have privileges assigned.
+                type: str
+            access:
+                description:
+                - The privilege of the dashboard administrator on the network.
+                - Valid options are C(full), C(read-only), or C(none).
+                type: str
     state:
         description:
         - Create or modify, or delete an organization
@@ -118,6 +137,32 @@ EXAMPLES = r'''
     org_name: YourOrg
     state: absent
     email: jane@doe.com
+
+- name: Create a new administrator with full access to two tags
+  meraki_admin:
+    auth_key: abc12345
+    org_name: YourOrg
+    state: present
+    name: Jane Doe
+    orgAccess: read-only
+    email: jane@doe.com
+    tags:
+        - tag: tenant
+          access: full
+        - tag: corporate
+          access: read-only
+
+- name: Create a new administrator with full access to a network
+  meraki_admin:
+    auth_key: abc12345
+    org_name: YourOrg
+    state: present
+    name: Jane Doe
+    orgAccess: read-only
+    email: jane@doe.com
+    networks:
+        - id: N_12345
+          access: full
 '''
 
 RETURN = r'''
