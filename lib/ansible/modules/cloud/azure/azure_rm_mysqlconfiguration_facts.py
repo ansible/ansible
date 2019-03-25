5faf5d4a104b4c85078b4c146c9b3b615afaa1f1
Zim Kalinowski <zikalino@microsoft.com>
2019-03-25 16:54:45 +0800

    remove ansible_facts added by mistake (#54320)
    * remove ansible_facts added by mistake
    
    * fixed mistake

--- lib/ansible/modules/cloud/azure/azure_rm_mysqlconfiguration_facts.py
+++ lib/ansible/modules/cloud/azure/azure_rm_mysqlconfiguration_facts.py
@@ -123,10 +123,7 @@ class AzureRMMySqlConfigurationFacts(AzureRMModuleBase):
             )
         )
         # store the results of the module operation
-        self.results = dict(
-            changed=False,
-            ansible_facts=dict()
-        )
+        self.results = dict(changed=False)
         self.mgmt_client = None
         self.resource_group = None
         self.server_name = None
