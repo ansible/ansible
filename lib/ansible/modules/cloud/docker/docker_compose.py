3937f24979da93c053f8178935e55bc290f5c6bf
Scott Luther <scott@cryptkey.io>
2019-03-24 20:54:02 -0700

    fix indentation and whitespace

--- lib/ansible/modules/cloud/docker/docker_compose.py
+++ lib/ansible/modules/cloud/docker/docker_compose.py
@@ -731,10 +731,10 @@ class ContainerManager(DockerBaseClass):
                     service_name = container.labels.get(LABEL_SERVICE)
                     if service_name not in self.project.service_names:
                         yield container
-                    
+
             orphans = list(_findOrphans())
             if orphans:
-              result['changed'] = True
+                result['changed'] = True
 
         for service in self.project.services:
             if not service_names or service.name in service_names:
