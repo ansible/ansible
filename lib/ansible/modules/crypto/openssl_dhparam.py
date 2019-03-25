3546594331b1461451ab61bd849b9cb1aa531b1b
Felix Fontein <felix@fontein.de>
2019-03-25 18:00:20 +0100

    openssl_dhparam: fix state=absent (#54296)
    * Fix remove.
    
    * Add changelog.
    
    (cherry picked from commit 121990d8c5084ac759ad8a1c1d46b3eab7a72ff7)

--- lib/ansible/modules/crypto/openssl_dhparam.py
+++ lib/ansible/modules/crypto/openssl_dhparam.py
@@ -131,6 +131,13 @@ class DHParameter(object):
 
         self.changed = changed
 
+    def remove(self, module):
+        try:
+            os.remove(self.path)
+            self.changed = True
+        except OSError as exc:
+            module.fail_json(msg=to_native(exc))
+
     def check(self, module):
         """Ensure the resource is in its desired state."""
         if self.force:
@@ -218,10 +225,11 @@ def main():
             result['changed'] = os.path.exists(module.params['path'])
             module.exit_json(**result)
 
-        try:
-            os.remove(module.params['path'])
-        except OSError as exc:
-            module.fail_json(msg=to_native(exc))
+        if os.path.exists(module.params['path']):
+            try:
+                dhparam.remove(module)
+            except Exception as exc:
+                module.fail_json(msg=to_native(exc))
 
     result = dhparam.dump()
 
