4ea716fc5830fd62541edf02428339dba76fb337
Matt Williams <matt@milliams.com>
2019-03-25 13:18:33 +0000

    Return dict directly to avoid failing key lookup
    (cherry picked from commit 545b98645d04b2d26eb95354d8edf4d8e9d4e2a2)

--- lib/ansible/plugins/inventory/foreman.py
+++ lib/ansible/plugins/inventory/foreman.py
@@ -166,7 +166,7 @@ class InventoryModule(BaseInventoryPlugin, Cacheable):
         url = "%s/api/v2/hosts/%s" % (self.foreman_url, hid)
         ret = self._get_json(url, [404])
         if not ret or not isinstance(ret, MutableMapping) or not ret.get('all_parameters', False):
-            ret = {}
+            return {}
         return ret.get('all_parameters')
 
     def _get_facts_by_id(self, hid):
