f58f7d3c8fe0c772ca2fbc64f679b0dc60d7a0bc
Abhijeet Kasurde <akasurde@redhat.com>
2019-03-25 21:50:46 +0530

    VMware: Change in update tag API (#54304)
    Changed update tag API as per new changes.
    
    Fixes: #53060
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/cloud/vmware/vmware_tag.py
+++ lib/ansible/modules/cloud/vmware/vmware_tag.py
@@ -185,13 +185,15 @@ class VmwareTag(VmwareRestClient):
 
         """
         changed = False
+        tag_id = self.global_tags[self.tag_name]['tag_id']
         results = dict(msg="Tag %s is unchanged." % self.tag_name,
-                       tag_id=self.global_tags[self.tag_name]['tag_id'])
+                       tag_id=tag_id)
         tag_update_spec = self.tag_service.UpdateSpec()
         tag_desc = self.global_tags[self.tag_name]['tag_description']
         desired_tag_desc = self.params.get('tag_description')
         if tag_desc != desired_tag_desc:
-            tag_update_spec.setDescription = desired_tag_desc
+            tag_update_spec.description = desired_tag_desc
+            self.tag_service.update(tag_id, tag_update_spec)
             results['msg'] = 'Tag %s updated.' % self.tag_name
             changed = True
 
