ansible/ansible

commit 919abe17f324bac17865391bd992da0804ca273d

Fix Foreman returning host parameters (#51034)
Foreman (1.20) returns the `all_parameters` key as a list of dicts, not a dict of key-value pairs.

lib/ansible/plugins/inventory/foreman.py
@@ -166,8 +166,8 @@ class InventoryModule(BaseInventoryPlugin, Cacheable):
         url = "%s/api/v2/hosts/%s" % (self.foreman_url, hid)
         ret = self._get_json(url, [404])
         if not ret or not isinstance(ret, MutableMapping) or not ret.get('all_parameters', False):
-            ret = {'all_parameters': [{}]}
-        return ret.get('all_parameters')[0]
+            ret = []
+        return ret.get('all_parameters')
 
     def _get_facts_by_id(self, hid):
         url = "%s/api/v2/hosts/%s/facts" % (self.foreman_url, hid)
@@ -220,11 +220,11 @@ class InventoryModule(BaseInventoryPlugin, Cacheable):
 
                 # set host vars from params
                 if self.get_option('want_params'):
-                    for k, v in self._get_all_params_by_id(host['id']).items():
+                    for p in self._get_all_params_by_id(host['id']):
                         try:
-                            self.inventory.set_variable(host['name'], k, v)
+                            self.inventory.set_variable(host['name'], p['name'], p['value'])
                         except ValueError as e:
-                            self.display.warning("Could not set parameter hostvar for %s, skipping %s: %s" % (host, k, to_native(e)))
+                            self.display.warning("Could not set parameter hostvar for %s, skipping %s: %s" % (host, p['name'], to_native(p['value'])))
 
                 # set host vars from facts
                 if self.get_option('want_facts'):

commit 4d66c48275bb791f0343b1475d09d20ef97b3af0

Fix for type error introduced by #51034 (#54333)
* Fix for type error

The empty type here should be a dict, not a list as is has a `get` done
on it next.

* Return dict directly to avoid failing key lookup

lib/ansible/plugins/inventory/foreman.py
@@ -175,7 +175,7 @@ class InventoryModule(BaseInventoryPlugin, Cacheable):
         url = "%s/api/v2/hosts/%s" % (self.foreman_url, hid)
         ret = self._get_json(url, [404])
         if not ret or not isinstance(ret, MutableMapping) or not ret.get('all_parameters', False):
-            ret = []
+            return {}
         return ret.get('all_parameters')
 
     def _get_facts_by_id(self, hid):