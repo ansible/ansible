bfc6a2a8d6342c7f36038c2216dc8f3e50a6244a
Ondra Machacek <machacek.ondra@gmail.com>
2019-03-26 15:00:27 +0100

    ovirt: Require SDK v4.3.0 (#54385)

--- lib/ansible/plugins/doc_fragments/ovirt.py
+++ lib/ansible/plugins/doc_fragments/ovirt.py
@@ -94,9 +94,9 @@ options:
         default: 3
 requirements:
   - python >= 2.7
-  - ovirt-engine-sdk-python >= 4.2.4
+  - ovirt-engine-sdk-python >= 4.3.0
 notes:
   - "In order to use this module you have to install oVirt Python SDK.
      To ensure it's installed with correct version you can create the following task:
-     I(pip: name=ovirt-engine-sdk-python version=4.2.4)"
+     I(pip: name=ovirt-engine-sdk-python version=4.3.0)"
 '''
