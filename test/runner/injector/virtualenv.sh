8612b126a84720d0e0ecbce670b843e7741e1197
Matt Clay <matt@mystile.com>
2019-03-27 02:42:11 -0700

    Fix tests which assume `virtualenv` is available.

---
+++ test/runner/injector/virtualenv.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Create and activate a fresh virtual environment with `source virtualenv.sh`.
+
+rm -rf "${OUTPUT_DIR}/venv"
+"${ANSIBLE_TEST_PYTHON_INTERPRETER}" -m virtualenv --system-site-packages --python "${ANSIBLE_TEST_PYTHON_INTERPRETER}" "${OUTPUT_DIR}/venv"
