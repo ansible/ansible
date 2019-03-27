ee859f5752b5fc619cac0eb6f79022bba610a7a3
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2019-03-27 08:55:25 +0100

    Use complex structure as documentation

--- lib/ansible/modules/cloud/hcloud/hcloud_server_facts.py
+++ lib/ansible/modules/cloud/hcloud/hcloud_server_facts.py
@@ -55,27 +55,65 @@ EXAMPLES = """
 """
 
 RETURN = """
-ansible_facts:
-    description: The server instances
-    returned: Always
+hcloud_server_facts:
+    description: The server facts as list
+    returned: always
     type: complex
     contains:
-        "hcloud_server_facts": [
-             {
-                "backup_window": null,
-                "datacenter": "nbg1-dc3",
-                "id": 1937415,
-                "image": "ubuntu-18.04",
-                "ipv4_address": "116.203.104.109",
-                "ipv6": "2a01:4f8:1c1c:c140::/64",
-                "labels": {},
-                "location": "nbg1",
-                "name": "mein-server-2",
-                "rescue_enabled": false,
-                "server_type": "cx11",
-                "status": "running"
-            }
-        ]
+        id: 
+            description: Numeric identifier of the server
+            returned: always
+            type: int
+            sample: 1937415
+        name:
+            description: Name of the server
+            returned: always
+            type: str
+            sample: my-server
+        status:
+            description: Status of the server
+            returned: always
+            type: str
+            sample: running
+        server_type:
+            description: Name of the server type of the server
+            returned: always
+            type: str
+            sample: cx11
+        ipv4_address:
+            description: Public IPv4 address of the server
+            returned: always
+            type: str
+            sample: 116.203.104.109
+        ipv6:
+            description: IPv6 network of the server
+            returned: always
+            type: str
+            sample: 2a01:4f8:1c1c:c140::/64
+        location:
+            description: Name of the location of the server
+            returned: always
+            type: str
+            sample: fsn1
+        datacenter:
+            description: Name of the datacenter of the server
+            returned: always
+            type: str
+            sample: fsn1-dc14
+        rescue_enabled:
+            description: True if rescue mode is enabled: Server will then boot into rescue system on next reboot
+            returned: always
+            type: bool
+            sample: false
+        backup_window:
+            description: Time window (UTC) in which the backup will run, or null if the backups are not enabled
+            returned: always
+            type: bool
+            sample: 22-02
+        labels:
+            description: User-defined labels (key-value pairs)
+            returned: always
+            type: dict
 """
 
 from ansible.module_utils.basic import AnsibleModule
