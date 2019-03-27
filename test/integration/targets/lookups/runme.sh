cdd412418d3c002aea87b13d16e6eadbf4a5cf2f
Matt Clay <matt@mystile.com>
2019-03-27 02:42:11 -0700

    Fix tests which assume `virtualenv` is available.

--- test/integration/targets/lookups/runme.sh
+++ test/integration/targets/lookups/runme.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-set -ex
-virtualenv --system-site-packages --python "${ANSIBLE_TEST_PYTHON_INTERPRETER}" "${OUTPUT_DIR}/venv"
-source "${OUTPUT_DIR}/venv/bin/activate"
 set -eux
+
+source virtualenv.sh
 
 # Requirements have to be installed prior to running ansible-playbook
 # because plugins and requirements are loaded before the task runs
