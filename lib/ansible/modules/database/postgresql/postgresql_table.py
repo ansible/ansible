c670c424887ebeaa9be7d87e242ab53cb92b1b71
Andrey Klychkov <aaklychkov@mail.ru>
2019-03-29 18:36:54 +0300

    New module postgresql_table - fixes

--- lib/ansible/modules/database/postgresql/postgresql_table.py
+++ lib/ansible/modules/database/postgresql/postgresql_table.py
@@ -126,12 +126,14 @@ options:
     - Default of C(prefer) matches libpq default.
     default: prefer
     choices: [ allow, disable, prefer, require, verify-ca, verify-full ]
-  ssl_rootcert:
+  ca_cert:
     description:
     - Specifies the name of a file containing SSL certificate authority (CA)
       certificate(s). If the file exists, the server's certificate will be
       verified to be signed by one of these authorities.
     type: str
+    aliases:
+    - ssl_rootcert
 notes:
 - If you do not pass db parameter, tables will be created in the database
   named postgres.
@@ -484,7 +486,7 @@ def main():
         db=dict(type='str', default='', aliases=['login_db']),
         port=dict(type='int', default=5432, aliases=['login_port']),
         ssl_mode=dict(type='str', default='prefer', choices=['allow', 'disable', 'prefer', 'require', 'verify-ca', 'verify-full']),
-        ssl_rootcert=dict(type='str'),
+        ca_cert=dict(type='str', aliases=['ssl_rootcert']),
         tablespace=dict(type='str'),
         owner=dict(type='str'),
         unlogged=dict(type='bool'),
@@ -512,7 +514,7 @@ def main():
     storage_params = module.params["storage_params"]
     truncate = module.params["truncate"]
     columns = module.params["columns"]
-    sslrootcert = module.params["ssl_rootcert"]
+    sslrootcert = module.params["ca_cert"]
     session_role = module.params["session_role"]
 
     # Check mutual exclusive parameters:
@@ -550,7 +552,7 @@ def main():
         "port": "port",
         "db": "database",
         "ssl_mode": "sslmode",
-        "ssl_rootcert": "sslrootcert"
+        "ca_cert": "sslrootcert"
     }
     kw = dict((params_map[k], v) for (k, v) in iteritems(module.params)
               if k in params_map and v != "" and v is not None)
@@ -564,7 +566,7 @@ def main():
         kw["host"] = module.params["login_unix_socket"]
 
     if psycopg2.__version__ < '2.4.3' and sslrootcert is not None:
-        module.fail_json(msg='psycopg2 must be at least 2.4.3 in order to user the ssl_rootcert parameter')
+        module.fail_json(msg='psycopg2 must be at least 2.4.3 in order to user the ca_cert parameter')
 
     try:
         db_connection = psycopg2.connect(**kw)
