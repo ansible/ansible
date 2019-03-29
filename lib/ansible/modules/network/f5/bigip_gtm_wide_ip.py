b5845fbe6e8b832a3a3378edb32fa22a771d2efd
Tim Rupp <caphrim007@gmail.com>
2019-03-29 09:56:19 -0700

    correct version

--- lib/ansible/modules/network/f5/bigip_gtm_wide_ip.py
+++ lib/ansible/modules/network/f5/bigip_gtm_wide_ip.py
@@ -667,7 +667,7 @@ class BaseManager(object):
         result.append(
             dict(
                 msg='The support for this TMOS version is deprecated.',
-                version='2.8'
+                version='2.12'
             )
         )
 
