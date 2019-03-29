a9ecf31266d5b4fe50fe2436647779ae4635dcc5
Hannes Ljungberg <hannes@5monkeys.se>
2019-03-29 22:21:43 +0100

    Add method to check if a parameter exists in diffs

--- lib/ansible/module_utils/docker/common.py
+++ lib/ansible/module_utils/docker/common.py
@@ -846,6 +846,14 @@ class DifferenceTracker(object):
             after[item['name']] = item['parameter']
         return before, after
 
+    def exists(self, name):
+        '''
+        Returns a boolean if a difference exists for name
+        '''
+        return any([
+            diff for diff in self._diff if diff['name'] == name
+        ])
+
     def get_legacy_docker_container_diffs(self):
         '''
         Return differences in the docker_container legacy format.
