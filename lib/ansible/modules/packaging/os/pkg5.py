0a783bdfc24e29171752421d6f0863d224aaa3c0
Daniel Poggenpohl <daniel.poggenpohl@fernuni-hagen.de>
2019-03-29 14:05:48 +0100

    - The filtering of FMRI patterns plus the extraction of concrete packages to install or change now happens in two functions - State "latest": all requested FMRI patterns are passed to pkg install; pkg takes care of wildcards and version requests, errors - State "absent": You have to filter the patterns of packages that don't exist or aren't installed because missing packages won't be ignored but an error will be thrown. So I filter the FMRI patterns to ones that match installed packages and then uninstall them with pkg uninstall - State "present": The hardest part... I filter the FMRI patterns so that the list of patterns to install contains all patterns with "@latest" and all patterns that do not match any possible package (to not hide any user error). Then I fetch a list of all matching package FMRIs that are or aren't installed using the remaining patterns. If no matching package is installed, install the latest one or the most specific of the requested versions If any package version is installed, - do nothing if the pattern is just a package name - if a version is requested and the installed version doesn't match, add package+version to the patterns to install Always fail if different versions are requested
    Wildcards: Previously, if a FMRI pattern contained wildcards, and there were multiple matching packages and only one of them was installed, the module didn't install the other one but filtered it away ("pkg list -- *wildcarded_package*" -> yes, there is one package that matches, don't need to do anything).

--- lib/ansible/modules/packaging/os/pkg5.py
+++ lib/ansible/modules/packaging/os/pkg5.py
@@ -101,15 +101,15 @@ def main():
             packages[-1] += ',' + fragment
         else:
             packages.append(fragment)
-    check_latest = params['state'] in ['latest']
-    install_state = get_install_state(module, packages, check_latest)
 
+    # The filtering of packages now happens in functions appropriate to the intended behaviour
+    # In the case of "latest", no filtering is necessary
     if params['state'] in ['present', 'installed']:
-        ensure(module, 'present', install_state['not_installed_packages'], params)
+        ensure(module, 'present', get_fmris_to_install(module, packages), params)
     elif params['state'] in ['latest']:
-        ensure(module, 'latest', install_state['updatable_packages'] + install_state['not_installed_packages'], params)
+        ensure(module, 'latest', packages, params)
     elif params['state'] in ['absent', 'uninstalled', 'removed']:
-        ensure(module, 'absent', install_state['installed_packages'], params)
+        ensure(module, 'absent', get_fmris_to_uninstall(module, packages), params)
 
 
 def ensure(module, state, packages, params):
@@ -156,31 +156,161 @@ def ensure(module, state, packages, params):
     module.exit_json(**response)
 
 
-def get_install_state(module, packages, check_latest):
-    rc, out, err = module.run_command(['pkg', 'list', '--'] + packages)
-    installed_packages = []
-    not_installed_packages = []
-    updatable_packages = []
+def get_fmris_to_uninstall(module, fmri_patterns):
+    rc, out, err = module.run_command(['pkg', 'list', '--'] + fmri_patterns)
+    installed_fmri_patterns = []
     if err:
-        for package in packages:
-            if (package + "\n") in err:
-                not_installed_packages += [package]
-            else:
-                installed_packages += [package]
+        for fmri_pattern in fmri_patterns:
+            if (fmri_pattern + "\n") not in err:
+                installed_fmri_patterns += [fmri_pattern]
     else:
-        installed_packages = packages
+        installed_fmri_patterns = fmri_patterns
+    return installed_fmri_patterns
 
-    if check_latest:
-        rc, out, err = module.run_command(['pkg', 'list', '-u', '--'] + installed_packages)
-        for installed_package in installed_packages:
-            if err:
-                if (installed_package + "\n") in err:
-                    updatable_packages += [installed_package]
-    return {
-        "installed_packages": installed_packages,
-        "not_installed_packages": not_installed_packages,
-        "updatable_packages": updatable_packages
+
+def get_fmris_to_install(module, all_fmri_patterns):
+    remaining_patterns = all_fmri_patterns
+    fmri_patterns_to_install = get_latest_patterns(all_fmri_patterns)
+    remaining_patterns = [fmri_pattern for fmri_pattern in remaining_patterns if fmri_pattern not in fmri_patterns_to_install]
+
+    # Now get all pattern matches that are or aren't installed (no obsolete or renamed packages) (out)
+    # and any not matched patterns (err)
+    # TODO: Unsure if using an unsafe shell would be wise if remaining_patterns contains unsanitized user input
+    # TODO: Check what happen in case of a timeout (rc code, stderr) and stop appropriately
+    rc, out, err = module.run_command(args=" ".join(['pkg', 'list', '-H', '-a', '-v', '--'] + remaining_patterns + ['|', "grep", "[-i]\-\-\$"]), use_unsafe_shell=True)
+    # Let any not found patterns always be installed to expose playbook errors
+    if err:
+        not_matched_fmri_patterns = extract_not_found_patterns(err)
+        fmri_patterns_to_install += not_matched_fmri_patterns
+        remaining_patterns = [fmri_pattern for fmri_pattern in remaining_patterns if fmri_pattern not in not_matched_fmri_patterns]
+
+    if remaining_patterns and out:
+        concrete_package_versions_to_install = []
+        package_state_lines = re.sub("\n$", "", out).split("\n")
+        current_package_name = ""
+        installed_package_version = ""
+        most_specific_requested_version = ""
+        package_count = len(package_state_lines)
+        package_parsed_count = 0
+
+        REGEX_FMRI_NAME_PART = "pkg://.+(?=@)"
+        REGEX_FMRI_VERSION_PART = "(?<=@)[^\s]*"
+        REGEX_FMRI = REGEX_FMRI_NAME_PART + "@" + REGEX_FMRI_VERSION_PART
+        REGEX_FMRI_INSTALLED = "i--"
+        REGEX_FMRI_NOT_INSTALLED = "\-\-\-"
+        REGEX_FMRI_STATE = "(" + REGEX_FMRI_INSTALLED + "|" + REGEX_FMRI_NOT_INSTALLED + ")"
+        REGEX_FMRI_STATE_LINE = "^" + REGEX_FMRI + "\s+" + REGEX_FMRI_STATE + "$"
+
+        for package_state in package_state_lines:
+            package_parsed_count += 1
+            if not package_state or not re.match(REGEX_FMRI_STATE_LINE, package_state):
+                continue
+            name_match = re.match(REGEX_FMRI_NAME_PART, package_state)
+            parsed_package_name = ""
+            if name_match:
+                parsed_package_name = name_match.group()
+            # Extract package version details
+            version_match = re.search(REGEX_FMRI_VERSION_PART, package_state)
+            parsed_package_version = ""
+            if version_match:
+                parsed_package_version = version_match.group()
+
+            if current_package_name != "" and current_package_name != parsed_package_name:
+                concrete_package_versions_to_install += get_concrete_version_to_install(current_package_name, installed_package_version, most_specific_requested_version)
+
+            # No current package yet (first package) or changing the current package
+            if current_package_name == "" or current_package_name != parsed_package_name:
+                # Create the new package context
+                installed_package_version = ""
+                current_package_name = parsed_package_name
+                most_specific_requested_version = get_most_specific_version(module, current_package_name, remaining_patterns)
+
+            if package_state.endswith(REGEX_FMRI_INSTALLED):
+                installed_package_version = parsed_package_version
+
+            # At the end of the package list add the last one
+            if package_parsed_count == package_count:
+                concrete_package_versions_to_install += get_concrete_version_to_install(current_package_name, installed_package_version, most_specific_requested_version)
+
+        fmri_patterns_to_install += concrete_package_versions_to_install
+    return fmri_patterns_to_install
+
+
+def get_concrete_version_to_install(current_package_name, installed_package_version, most_specific_requested_version):
+    package_to_install = []
+    if installed_package_version:
+        if most_specific_requested_version:
+            package_to_install = [current_package_name + "@" + most_specific_requested_version]
+    else:
+        if most_specific_requested_version:
+            package_to_install = [current_package_name + "@" + most_specific_requested_version]
+        else:
+            package_to_install = [current_package_name]
+    return package_to_install
+
+def extract_not_found_patterns(pkg_list_stderr):
+    # FIXME: Look what stderr brings when a timeout is reached (or other errors)
+    stderr_lines = pkg_list_stderr.split("\n")
+    not_found_patterns = []
+    for stderr_line in stderr_lines:
+        if stderr_line.startswith("  "):
+            not_found_patterns += [stderr_line.replace("  ", "")]
+    return not_found_patterns
+
+
+def get_most_specific_version(module, requested_package, fmri_patterns):
+    most_specific_version = ""
+    most_specific_pattern = ""
+    for fmri_pattern in fmri_patterns:
+        package_name = re.sub("\*", "", fmri_pattern)
+        if "@" in package_name:
+            package_name = package_name.split("@")[0]
+        if package_name not in requested_package:
+            continue
+        matches = re.search("(?<=@)[^\s]*", fmri_pattern)
+        requested_version = ""
+        if matches:
+            requested_version = matches.group()
+        if not requested_version:
+            continue
+        if most_specific_version == "" or requested_version == most_specific_version:
+            most_specific_version = requested_version
+            most_specific_pattern = fmri_pattern
+            continue
+        if len(requested_version) == len(most_specific_version):
+            fail_conflicting_patterns(module, [most_specific_pattern, fmri_pattern])
+        less_specific_version = min(requested_version, most_specific_version)
+        more_specific_version = max(requested_version, most_specific_version)
+        if not less_specific_version in more_specific_version:
+            fail_conflicting_patterns(module, [most_specific_pattern, fmri_pattern])
+        if more_specific_version != most_specific_version:
+            most_specific_version = more_specific_version
+            most_specific_pattern = fmri_pattern
+
+    return most_specific_version
+
+
+def get_latest_patterns(fmri_patterns):
+    latest_identifier = "@latest"
+    latest_patterns = []
+    # First select all FMRIs having @latest to always install or update, any updates are ok then
+    for fmri_pattern in fmri_patterns:
+        if latest_identifier in fmri_pattern:
+            latest_patterns += [fmri_pattern]
+            continue
+    return latest_patterns
+
+
+def fail_conflicting_patterns(module, conflicting_patterns):
+    msg = "You request two different versions of the same package: " + ",".join(conflicting_patterns)
+    response = {
+        'rc': 1,
+        'results': conflicting_patterns,
+        'msg': msg,
+        'changed': False,
+        'failed': True
     }
+    module.fail_json(**response)
 
 
 if __name__ == '__main__':
