4d5e3097d74a72dbbbb257a2c74b1ea60af37489
Hannes Ljungberg <hannes@5monkeys.se>
2019-03-30 21:40:42 +0100

    Use generator expression instead

--- lib/ansible/module_utils/docker/common.py
+++ lib/ansible/module_utils/docker/common.py
@@ -850,9 +850,7 @@ class DifferenceTracker(object):
         '''
         Returns a boolean if a difference exists for name
         '''
-        return any([
-            diff for diff in self._diff if diff['name'] == name
-        ])
+        return any(diff for diff in self._diff if diff['name'] == name)
 
     def get_legacy_docker_container_diffs(self):
         '''
