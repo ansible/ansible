f59acdab23cefb5becf96c68797acc766bf1b628
Rui  Moreira <ruimoreira29@gmail.com>
2019-03-31 21:40:24 +0100

    fixed pep8 failure

--- lib/ansible/modules/cloud/amazon/dms_endpoint.py
+++ lib/ansible/modules/cloud/amazon/dms_endpoint.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*
-# Copyright: Ansible Project
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-from __future__ import absolute_import, division, print_function
-import pdb
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
 import traceback
+from ansible.module_utils.aws.core import AnsibleAWSModule
+from ansible.module_utils.ec2 import boto3_conn, HAS_BOTO3, \
+    camel_dict_to_snake_dict, get_aws_connection_info, AWSRetry
 
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
@@ -18,48 +30,56 @@ DOCUMENTATION = """
 module: dms_endpoint
 short_description: creates or destroys a data migration services endpoint
 description:
-    - creates or destroys a data migration services endpoint, that can be used to replicate data.
+    - creates or destroys a data migration services endpoint,
+      that can be used to replicate data.
 version_added: "2.8"
 options:
-    state: 
+    state:
       description:
-        - State of the endpoint 
-      default: present  
+        - State of the endpoint
+      default: present
     endpointidentifier:
       description:
-        - An identifier name for the endpoint 
+        - An identifier name for the endpoint
     endpointtype:
       description:
         - Type of endpoint we want to manage
-       choices: ['source', 'target'] 
+      choices: ['source', 'target']
     enginetype:
       description:
-        - Ddatabase engine that we want to use, please refer to the AWS DMS for more information on the supported engines and their limitation
+        - Database engine that we want to use, please refer to
+          the AWS DMS for more information on the supported
+          engines and their limitation
       choices: ['mysql', 'oracle', 'postgres', 'mariadb', 'aurora',
                          'redshift', 's3', 'db2', 'azuredb', 'sybase',
-                         'dynamodb', 'mongodb', 'sqlserver']  
+                         'dynamodb', 'mongodb', 'sqlserver']
     username:
       description:
         - Username our endpoint will use to connect to the database
     password
       description:
-        - Password used to connect to the database. Note: this attribute can only be written, the AWS API does not return this parameter.
+        - Password used to connect to the database
+          Note: this attribute can only be written,
+          the AWS API does not return this parameter.
     servername:
       description:
         - Servername that the endpoint will connect to
-    port:                    
+    port:
       description:
         - TCP port for access to the database
     databasename:
       description:
-        - Name for the database on the origin or target side    
+        - Name for the database on the origin or target side
     extraconnectionattributes:
-      description: 
-        - Extra attributes for the database connection, the AWS documentation states " For more information about extra connection attributes, see the documentation section for your data store."
+      description:
+        - Extra attributes for the database connection, the AWS documentation
+          states " For more information about extra connection attributes,
+          see the documentation section for your data store."
     kmskeyid:
       description:
-        - Encryption key to use to encrypt replication storage and connection information
-    tags: 
+        - Encryption key to use to encrypt replication storage and
+          connection information
+    tags:
       description:
         - A list of tags to add to the endpoint
        key:
@@ -78,31 +98,36 @@ options:
        default: 'none'
      serviceaccessrolearn:
        description:
-         -  Amazon Resource Name (ARN) for the service access role that you want to use to create the endpoint.
+         -  Amazon Resource Name (ARN) for the service access role that you
+            want to use to create the endpoint.
      externaltabledefinition:
        description:
          - The external table definition
      dynamodbsettings:
        description:
-         - Settings in JSON format for the target Amazon DynamoDB endpoint if source or target is dynamodb
+         - Settings in JSON format for the target Amazon DynamoDB endpoint
+           if source or target is dynamodb
      s3settings:
        description
          - S3 buckets settings for the target Amazon S3 endpoint.
      dmstransfersettings:
        description:
-         - The settings in JSON format for the DMS transfer type of source endpoint    
+         - The settings in JSON format for the DMS transfer type of
+           source endpoint
      mongodbsettings:
        description:
          - Settings in JSON format for the source MongoDB endpoint
      kinesissettings:
        description:
-         - Settings in JSON format for the target Amazon Kinesis Data Streams endpoin
+         - Settings in JSON format for the target Amazon Kinesis
+           Data Streams endpoint
      elasticsearchsettings:
        description:
          - Settings in JSON format for the target Elasticsearch endpoint
-     wait: 
+     wait:
        description:
-         - Boolean stating if we should wait for the object to be deleted when state = absent                                                           
+         - Boolean stating if we should wait for the object to
+           be deleted when state = absent
 author:
    - Rui Moreira (@ruimoreira)
 extends_documentation_fragment: aws
@@ -110,7 +135,8 @@ extends_documentation_fragment: aws
 
 EXAMPLES = """
 
-# Note: These examples do not set authentication details, see the AWS Guide for details.
+# Note: These examples do not set authentication details,
+see the AWS Guide for details.
 
 # Endpoint Creation
 - name: create endpoints
@@ -128,14 +154,7 @@ EXAMPLES = """
     wait: false
 """
 
-
-
-from ansible.module_utils.aws.core import AnsibleAWSModule
-from ansible.module_utils.ec2 import boto3_conn, HAS_BOTO3, ansible_dict_to_boto3_filter_list, camel_dict_to_snake_dict, get_aws_connection_info, AWSRetry
-
-
 try:
-    #from botocore.exceptions import ClientError, BotoCoreError, WaiterError
     import botocore
 except ImportError:
     pass  # caught by AnsibleAWSModule
@@ -147,9 +166,10 @@ backoff_params = dict(tries=5, delay=1, backoff=1.5)
 def describe_endpoints(connection, endpoint_identifier):
     """ checks if the endpoint exists """
     try:
-        endpoint_filter = dict(Name='endpoint-id', Values=[endpoint_identifier])
+        endpoint_filter = dict(Name='endpoint-id',
+                               Values=[endpoint_identifier])
         return connection.describe_endpoints(Filters=[endpoint_filter])
-    except:
+    except botocore.exceptions.ClientError:
         return {'Endpoints': []}
 
 
@@ -187,9 +207,6 @@ def endpoint_exists(endpoint):
     return bool(len(endpoint['Endpoints']))
 
 
-#def get_dms_waiters(connection):
-#    return True
-
 def get_dms_client(aws_connect_params, region, ec2_url):
     client_params = dict(
         module=module,
@@ -201,10 +218,12 @@ def get_dms_client(aws_connect_params, region, ec2_url):
     )
     return boto3_conn(**client_params)
 
+
 def delete_dms_endpoint(connection):
     try:
-        endpoint_data = describe_endpoints(connection, module.params.get('endpointidentifier'))
-        endpoint_arn = endpoint_data['Endpoints'][0].get('EndpointArn')
+        endpoint = describe_endpoints(connection,
+                                      module.params.get('endpointidentifier'))
+        endpoint_arn = endpoint['Endpoints'][0].get('EndpointArn')
         delete_arn = dict(
             EndpointArn=endpoint_arn
         )
@@ -228,11 +247,9 @@ def delete_dms_endpoint(connection):
             return connection.delete_endpoint(**delete_arn)
     except botocore.exceptions.ClientError as e:
         module.fail_json(msg="Failed to delete the  DMS endpoint.",
-                         exception=traceback.format_exc(), **camel_dict_to_snake_dict(e.response))
+                         exception=traceback.format_exc(),
+                         **camel_dict_to_snake_dict(e.response))
     except botocore.exceptions.BotoCoreError as e:
-        module.fail_json(msg="Failed to delete the DMS endpoint.",
-                         exception=traceback.format_exc())
-    except:
         module.fail_json(msg="Failed to delete the DMS endpoint.",
                          exception=traceback.format_exc())
 
@@ -256,13 +273,16 @@ def create_module_params():
     if module.params.get('EndpointArn'):
         endpoint_parameters['EndpointArn'] = module.params.get('EndpointArn')
     if module.params.get('certificatearn'):
-        endpoint_parameters['CertificateArn'] = module.params.get('certificatearn')
+        endpoint_parameters['CertificateArn'] = \
+            module.params.get('certificatearn')
 
     if module.params.get('dmstransfersettings'):
-        endpoint_parameters['DmsTransferSettings'] = module.params.get('dmstransfersettings')
+        endpoint_parameters['DmsTransferSettings'] = \
+            module.params.get('dmstransfersettings')
 
     if module.params.get('extraconnectionattributes'):
-        endpoint_parameters['ExtraConnectionAttributes'] = module.params.get('extraconnectionattributes')
+        endpoint_parameters['ExtraConnectionAttributes'] =\
+            module.params.get('extraconnectionattributes')
 
     if module.params.get('kmskeyid'):
         endpoint_parameters['KmsKeyId'] = module.params.get('kmskeyid')
@@ -271,44 +291,55 @@ def create_module_params():
         endpoint_parameters['Tags'] = module.params.get('tags')
 
     if module.params.get('serviceaccessrolearn'):
-        endpoint_parameters['ServiceAccessRoleArn'] = module.params.get('serviceaccessrolearn')
+        endpoint_parameters['ServiceAccessRoleArn'] = \
+            module.params.get('serviceaccessrolearn')
 
     if module.params.get('externaltabledefinition'):
-        endpoint_parameters['ExternalTableDefinition'] = module.params.get('externaltabledefinition')
+        endpoint_parameters['ExternalTableDefinition'] = \
+            module.params.get('externaltabledefinition')
 
     if module.params.get('dynamodbsettings'):
-        endpoint_parameters['DynamoDbSettings'] = module.params.get('dynamodbsettings')
+        endpoint_parameters['DynamoDbSettings'] = \
+            module.params.get('dynamodbsettings')
 
     if module.params.get('s3settings'):
         endpoint_parameters['S3Settings'] = module.params.get('s3settings')
 
     if module.params.get('mongodbsettings'):
-        endpoint_parameters['MongoDbSettings'] = module.params.get('mongodbsettings')
+        endpoint_parameters['MongoDbSettings'] = \
+            module.params.get('mongodbsettings')
 
     if module.params.get('kinesissettings'):
-        endpoint_parameters['KinesisSettings'] = module.params.get('kinesissettings')
+        endpoint_parameters['KinesisSettings'] = \
+            module.params.get('kinesissettings')
 
     if module.params.get('elasticsearchsettings'):
-        endpoint_parameters['ElasticsearchSettings'] = module.params.get('elasticsearchsettings')
+        endpoint_parameters['ElasticsearchSettings'] = \
+            module.params.get('elasticsearchsettings')
 
     return endpoint_parameters
 
 
 def compare_params(param_described):
     """
-    Compares the dict obtained from the describe DMS endpoint and what we are reading from the values in the template
-    We can never compare the password as boto3's method for describing a DMS endpoint does not return the value for
+    Compares the dict obtained from the describe DMS endpoint and
+    what we are reading from the values in the template We can
+    never compare the password as boto3's method for describing
+    a DMS endpoint does not return the value for
     the password for security reasons ( I assume )
     """
     modparams = create_module_params()
     changed = False
     for paramname in modparams:
-        if paramname == 'Password' or paramname in param_described   and param_described[paramname] == modparams[paramname] or \
-                str(param_described[paramname]).lower() == modparams[paramname]:
+        if paramname == 'Password' or paramname in param_described \
+                and param_described[paramname] == modparams[paramname] or \
+                str(param_described[paramname]).lower() \
+                == modparams[paramname]:
             pass
         else:
             changed = True
     return changed
+
 
 def modify_dms_endpoint(connection):
 
@@ -317,13 +348,12 @@ def modify_dms_endpoint(connection):
         return dms_modify_endpoint(connection, **params)
     except botocore.exceptions.ClientError as e:
         module.fail_json(msg="Failed to update DMS endpoint.",
-                         exception=traceback.format_exc(), **camel_dict_to_snake_dict(e.response))
+                         exception=traceback.format_exc(),
+                         **camel_dict_to_snake_dict(e.response))
     except botocore.exceptions.BotoCoreError as e:
         module.fail_json(msg="Failed to update DMS endpoint.",
                          exception=traceback.format_exc())
-    except:
-        module.fail_json(msg="Failed to update DMS endpoint.",
-                         exception=traceback.format_exc())
+
 
 def create_dms_endpoint(connection):
     """
@@ -337,24 +367,22 @@ def create_dms_endpoint(connection):
         return dms_create_endpoint(connection, **params)
     except botocore.exceptions.ClientError as e:
         module.fail_json(msg="Failed to create DMS endpoint.",
-                         exception=traceback.format_exc(), **camel_dict_to_snake_dict(e.response))
+                         exception=traceback.format_exc(),
+                         **camel_dict_to_snake_dict(e.response))
     except botocore.exceptions.BotoCoreError as e:
-        module.fail_json(msg="Failed to create DMS endpoint.",
-                         exception=traceback.format_exc())
-    except:
         module.fail_json(msg="Failed to create DMS endpoint.",
                          exception=traceback.format_exc())
 
 
 def main():
-    """ main function, instanciates the ansible module and performs the initial logic"""
     argument_spec = dict(
         state=dict(choices=['present', 'absent'], default='present'),
         endpointidentifier=dict(required=True),
         endpointtype=dict(choices=['source', 'target'], required=True),
-        enginename=dict(choices=['mysql', 'oracle', 'postgres', 'mariadb', 'aurora',
-                                 'redshift', 's3', 'db2', 'azuredb', 'sybase',
-                                 'dynamodb', 'mongodb', 'sqlserver'], required=True),
+        enginename=dict(choices=['mysql', 'oracle', 'postgres', 'mariadb',
+                                 'aurora', 'redshift', 's3', 'db2', 'azuredb',
+                                 'sybase', 'dynamodb', 'mongodb', 'sqlserver'],
+                        required=True),
         username=dict(),
         password=dict(no_log=True),
         servername=dict(),
@@ -364,7 +392,8 @@ def main():
         kmskeyid=dict(),
         tags=dict(type='dict'),
         certificatearn=dict(),
-        sslmode=dict(choices=['none', 'require', 'verify-ca', 'verify-full'], default='none'),
+        sslmode=dict(choices=['none', 'require', 'verify-ca', 'verify-full'],
+                     default='none'),
         serviceaccessrolearn=dict(),
         externaltabledefinition=dict(),
         dynamodbsettings=dict(type='dict'),
@@ -392,12 +421,15 @@ def main():
         module.fail_json(msg='boto3 required for this module')
 
     state = module.params.get('state')
-    region, ec2_url, aws_connect_params = get_aws_connection_info(module, boto3=True)
+    region, ec2_url, aws_connect_params = get_aws_connection_info(module,
+                                                                  boto3=True)
     dmsclient = get_dms_client(aws_connect_params, region, ec2_url)
-    endpoint = describe_endpoints(dmsclient, module.params.get('endpointidentifier'))
+    endpoint = describe_endpoints(dmsclient,
+                                  module.params.get('endpointidentifier'))
     if state == 'present':
         if endpoint_exists(endpoint):
-            module.params['EndpointArn'] = endpoint['Endpoints'][0].get('EndpointArn')
+            module.params['EndpointArn'] = \
+                endpoint['Endpoints'][0].get('EndpointArn')
             changed = compare_params(endpoint["Endpoints"][0])
             if changed:
                 updated_dms = modify_dms_endpoint(dmsclient)
@@ -413,7 +445,6 @@ def main():
             module.exit_json(changed=True, **delete_results)
         else:
             module.exit_json(changed=False, msg='DMS Endpoint does not exist')
-
 
 
 if __name__ == '__main__':
