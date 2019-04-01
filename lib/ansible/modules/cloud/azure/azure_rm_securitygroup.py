a6a456e88d0dc001e31a87dfe7d5472e07d59a69
Yunge Zhu <yungez@microsoft.com>
2019-04-01 16:22:07 +0800

    fix lint

--- lib/ansible/modules/cloud/azure/azure_rm_securitygroup.py
+++ lib/ansible/modules/cloud/azure/azure_rm_securitygroup.py
@@ -474,11 +474,11 @@ def create_rule_instance(self, rule):
         source_port_ranges=rule.get('source_port_ranges', None),
         destination_port_ranges=rule.get('destination_port_ranges', None),
         source_application_security_groups=[
-            self.nsg_models.ApplicationSecurityGroup(id=p) for p in rule.get('source_application_security_groups')]
-            if rule.get('source_application_security_groups') else None,
+            self.nsg_models.ApplicationSecurityGroup(id=p) 
+            for p in rule.get('source_application_security_groups')] if rule.get('source_application_security_groups') else None,
         destination_application_security_groups=[
-            self.nsg_models.ApplicationSecurityGroup(id=p) for p in rule.get('destination_application_security_groups')]
-            if rule.get('destination_application_security_groups') else None,
+            self.nsg_models.ApplicationSecurityGroup(id=p)
+            for p in rule.get('destination_application_security_groups')] if rule.get('destination_application_security_groups') else None,
         access=rule.get('access', None),
         priority=rule.get('priority', None),
         direction=rule.get('direction', None),
