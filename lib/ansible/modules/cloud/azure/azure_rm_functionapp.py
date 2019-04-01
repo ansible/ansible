549f76066c54508ba060e7f9e7a833ad156acdff
Zim Kalinowski <zikalino@microsoft.com>
2019-04-01 16:50:08 +0800

    Fix for function app settings (#54662)

--- lib/ansible/modules/cloud/azure/azure_rm_functionapp.py
+++ lib/ansible/modules/cloud/azure/azure_rm_functionapp.py
@@ -280,10 +280,18 @@ class AzureRMFunctionApp(AzureRMModuleBase):
 
         function_app_settings = self.necessary_functionapp_settings()
         for app_setting_key in self.app_settings:
-            function_app_settings.append(NameValuePair(
-                name=app_setting_key,
-                value=self.app_settings[app_setting_key]
-            ))
+            found_setting = None
+            for s in function_app_settings:
+                if s.name == app_setting_key:
+                    found_setting = s
+                    break
+            if found_setting:
+                found_setting.value = self.app_settings[app_setting_key]
+            else:
+                function_app_settings.append(NameValuePair(
+                    name=app_setting_key,
+                    value=self.app_settings[app_setting_key]
+                ))
         return function_app_settings
 
     @property
