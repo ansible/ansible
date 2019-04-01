e317a1a7afa22182f0842e619860728517957403
zikalino <zikalino@microsoft.com>
2019-04-01 17:13:52 +0800

    and removed too much

--- lib/ansible/modules/cloud/azure/azure_rm_functionapp.py
+++ lib/ansible/modules/cloud/azure/azure_rm_functionapp.py
@@ -284,6 +284,7 @@ class AzureRMFunctionApp(AzureRMModuleBase):
             for s in function_app_settings:
                 if s.name == app_setting_key:
                     found_setting = s
+                    break
             if found_setting:
                 found_setting.value = self.app_settings[app_setting_key]
             else:
