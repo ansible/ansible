7b7f4b198edf674ef148f91e8296fe3ac0002b39
federico.olivieri <federico.olivieri@ticketmaster.co.uk>
2019-04-03 12:14:38 +0100

    change import order

--- lib/ansible/modules/network/asa/asa_og.py
+++ lib/ansible/modules/network/asa/asa_og.py
@@ -5,6 +5,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
+__metaclass__ = type
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
@@ -132,7 +133,6 @@ from ansible.module_utils.network.asa.asa import get_config, load_config, run_co
 from ansible.module_utils.network.common.config import NetworkConfig, dumps
 import re
 import sys
-__metaclass__ = type
 
 
 class Parser():
