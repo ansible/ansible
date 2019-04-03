b406058dbad824317bdf6147d282d6e2f5ad6fbf
tknipper11 <tim.knipper@gmail.com>
2019-04-03 17:01:55 -0500

    added from __future__ import absolute_import, division, print_function __metaclass__ = type

--- lib/ansible/modules/network/aci/aci_interface_policy_cdp.py
+++ lib/ansible/modules/network/aci/aci_interface_policy_cdp.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
-# Copyright: (c) 2018, Terry Jones <terry.jones@example.org>
+# Copyright: (c) 2019, Tim Knipper <tim.knipper@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
 
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
