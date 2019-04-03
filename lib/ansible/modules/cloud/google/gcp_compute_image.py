1a85b09e1f8d81901e4c5e8561f6b07ff3a173a3
The Magician <magic-modules@google.com>
2019-04-03 10:31:46 -0700

    Bug fixes for GCP modules (#54762)

--- lib/ansible/modules/cloud/google/gcp_compute_image.py
+++ lib/ansible/modules/cloud/google/gcp_compute_image.py
@@ -153,9 +153,10 @@ options:
     - You must provide either this property or the rawDisk.source property but not
       both to create an image.
     - 'This field represents a link to a Disk resource in GCP. It can be specified
-      in two ways. First, you can place in the selfLink of the resource here as a
-      string Alternatively, you can add `register: name-of-resource` to a gcp_compute_disk
-      task and then set this source_disk field to "{{ name-of-resource }}"'
+      in two ways. First, you can place a dictionary with key ''selfLink'' and value
+      of your resource''s selfLink Alternatively, you can add `register: name-of-resource`
+      to a gcp_compute_disk task and then set this source_disk field to "{{ name-of-resource
+      }}"'
     required: false
   source_disk_encryption_key:
     description:
@@ -384,7 +385,7 @@ sourceDisk:
   - You must provide either this property or the rawDisk.source property but not both
     to create an image.
   returned: success
-  type: str
+  type: dict
 sourceDiskEncryptionKey:
   description:
   - The customer-supplied encryption key of the source disk. Required if the source
@@ -451,7 +452,7 @@ def main():
                 type='dict',
                 options=dict(container_type=dict(type='str', choices=['TAR']), sha1_checksum=dict(type='str'), source=dict(required=True, type='str')),
             ),
-            source_disk=dict(),
+            source_disk=dict(type='dict'),
             source_disk_encryption_key=dict(type='dict', options=dict(raw_key=dict(type='str'))),
             source_disk_id=dict(type='str'),
             source_type=dict(type='str', choices=['RAW']),
