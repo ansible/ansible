f46f388c2ca8778d71c114e27c910d27c66fed76
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-04-03 11:54:33 +0200

    Follow variable naming convention

--- lib/ansible/plugins/callback/foreman.py
+++ lib/ansible/plugins/callback/foreman.py
@@ -139,7 +139,7 @@ class CallbackModule(CallbackBase):
 
         super(CallbackModule, self).set_options(task_keys=task_keys, var_options=var_options, direct=direct)
 
-        self.FOREMAN_URL = self.get_option('url')
+        self.foreman_url = self.get_option('url')
         ssl_cert = self.get_option('client_cert')
         ssl_key = self.get_option('client_key')
 
@@ -150,7 +150,7 @@ class CallbackModule(CallbackBase):
         else:
             self._disable_plugin(u'The `requests` python module is not installed.')
 
-        if self.FOREMAN_URL.startswith('https://'):
+        if self.foreman_url.startswith('https://'):
             if not os.path.exists(ssl_cert):
                 self._disable_plugin(u'FOREMAN_SSL_CERT %s not found.' % ssl_cert)
 
@@ -174,7 +174,7 @@ class CallbackModule(CallbackBase):
         elif option.lower() in ["0", "false", "off"]:
             requests.packages.urllib3.disable_warnings()
             self._display.warning(u"SSL verification of %s disabled" %
-                                  self.FOREMAN_URL)
+                                  self.foreman_url)
             verify = False
         else:  # Set to a CA bundle:
             verify = option
@@ -186,6 +186,8 @@ class CallbackModule(CallbackBase):
         parser.  The default fact importer should import these facts
         properly.
         """
+        url = self.foreman_url + '/api/v2/hosts/facts'
+
         for host, facts in self.facts.items():
             facts = {
                 "name": host,
@@ -197,11 +199,11 @@ class CallbackModule(CallbackBase):
             }
 
             try:
-                r = self.session.post(url=self.FOREMAN_URL + '/api/v2/hosts/facts', json=facts)
-                r.raise_for_status()
+                response = self.session.post(url=url, json=facts)
+                response.raise_for_status()
             except requests.exceptions.RequestException as err:
                 self._display.warning(u'Sending facts to Foreman at {url} failed for {host}: {err}'.format(
-                    host=host, err=to_text(err), url=self.FOREMAN_URL))
+                    host=host, err=to_text(err), url=self.foreman_url))
 
     def send_reports(self, stats):
         """
@@ -209,6 +211,8 @@ class CallbackModule(CallbackBase):
         importer. THe data is in a format that Foreman can handle
         without writing another report importer.
         """
+        url = self.foreman_url + '/api/v2/config_reports'
+
         for host in stats.processed.keys():
             total = stats.summarize(host)
             report = {
@@ -230,11 +234,11 @@ class CallbackModule(CallbackBase):
             }
 
             try:
-                r = self.session.post(url=self.FOREMAN_URL + '/api/v2/config_reports', json=report)
-                r.raise_for_status()
+                response = self.session.post(url=url, json=report)
+                response.raise_for_status()
             except requests.exceptions.RequestException as err:
                 self._display.warning(u'Sending report to Foreman at {url} failed for {host}: {err}'.format(
-                    host=host, err=to_text(err), url=self.FOREMAN_URL))
+                    host=host, err=to_text(err), url=self.foreman_url))
 
             self.items[host] = []
 
