de1c76e618d47763d19399b2fefc1f1ef3e11d2b
Ali-aqrabawi <ali_aqrabawi@yahoo.com>
2019-04-05 20:02:54 +0300

    Update django_models.py
    pep8 fixes

--- lib/ansible/plugins/inventory/django_models.py
+++ lib/ansible/plugins/inventory/django_models.py
@@ -17,8 +17,7 @@ DOCUMENTATION = r'''
         - the database should have three tables:
             - ansible_network_groups (for network devices groups)
             - ansible_aws_groups (for aws  groups)
-            - ansible_inventory_hosts (for  hosts)
-            
+            - ansible_inventory_hosts (for  hosts)            
         - table's fields like follow:        
             - ansible_network_groups:
                   - name: CharField(max_length=100)
@@ -26,18 +25,13 @@ DOCUMENTATION = r'''
                   - ansible_network_os: CharField(max_length=100)
                   - parent_group: ForeignKey('self', on_delete=models.DO_NOTHING, related_name='child_group',null=True)
                   - ansible_become: BooleanField(default=False)
-                  - arent_group: ForeignKey('self', on_delete=models.DO_NOTHING, related_name='child_group',null=True)                                                   
-    
-    
-            - ansible_aws_groups:
-                  
+                  - arent_group: ForeignKey('self', on_delete=models.DO_NOTHING, related_name='child_group',null=True)  
+            - ansible_aws_groups:                  
                   - ami: CharField(max_length=100)
                   - region: CharField(max_length=100)
                   - type: models.CharField(max_length=100)
                   - sshkey: models.CharField(max_length=100)
                   - vpcid: models.CharField(max_length=100)
-    
-
             - ansible_inventory_hosts:
                   - host: CharField(max_length=100)
                   - ansible_ssh_host: GenericIPAddressField()
@@ -45,7 +39,6 @@ DOCUMENTATION = r'''
                   - ansible_ssh_pass: models.CharField(max_length=100)
                   - ansible_become_pass: models.CharField(max_length=100)
                   - group: ForeignKey(ansible_network_groups, on_delete=models.DO_NOTHING)
-
 '''
 
 EXAMPLES = r'''
