2d6476291641f7dd80de16e54fac98c43c4ed7d9
lshort <ekultails@gmail.com>
2019-04-05 19:51:43 -0400

    flatpak: Add Luke Short (@ekultails) to the list of contributors.

--- lib/ansible/modules/packaging/os/flatpak.py
+++ lib/ansible/modules/packaging/os/flatpak.py
@@ -3,6 +3,7 @@
 
 # Copyright: (c) 2017 John Kwiatkoski (@JayKayy) <jkwiat40@gmail.com>
 # Copyright: (c) 2018 Alexander Bethke (@oolongbrothers) <oolongbrothers@gmx.net>
+# Copyright: (c) 2019 Luke Short (@ekultails) <ekultails@gmail.com>
 # Copyright: (c) 2017 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
