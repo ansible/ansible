bb3f73a85d435826a95c7faf63a49e06752e9a1d
Felix Fontein <felix@fontein.de>
2019-04-07 16:02:33 +0200

    Warn when log_options values are not strings.

--- lib/ansible/modules/cloud/docker/docker_container.py
+++ lib/ansible/modules/cloud/docker/docker_container.py
@@ -1586,7 +1586,14 @@ class TaskParameters(DockerBaseClass):
         if self.log_options is not None:
             options['Config'] = dict()
             for k, v in self.log_options.items():
-                options['Config'][k] = str(v)
+                if not isinstance(v, string_types):
+                    self.client.module.warn(
+                        "Non-string value found for log_options option. "
+                        "Ambiguous log_options values should be wrapped in quotes to avoid YAML parsing. Key: %s" % (k, )
+                    )
+                v = str(v)
+                self.log_options[k] = v
+                options['Config'][k] = v
 
         try:
             return LogConfig(**options)
