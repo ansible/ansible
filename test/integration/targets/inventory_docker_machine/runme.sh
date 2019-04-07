8a994dc1c6b8c127e5ba965f83b6c1d29c460c9a
ximon18 <3304436+ximon18@users.noreply.github.com>
2019-04-08 01:12:54 +0200

    FIX: SC2086 Double quote to prevent globbing and word splitting.

--- test/integration/targets/inventory_docker_machine/runme.sh
+++ test/integration/targets/inventory_docker_machine/runme.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 
 cleanup() {
     echo "Cleanup"
-    docker-machine rm --force ${DM_MACHINE_NAME}
+    docker-machine rm --force "${DM_MACHINE_NAME}"
     echo "Done"
     exit 0
 }
@@ -18,15 +18,15 @@ trap cleanup INT TERM EXIT
 echo "Setup"
 docker-machine create \
         --driver digitalocean \
-        --digitalocean-access-token ${DIGITALOCEAN_TOKEN} \
+        --digitalocean-access-token "${DIGITALOCEAN_TOKEN}" \
         --digitalocean-region ams3 \
         --digitalocean-image ubuntu-18-04-x64 \
         --digitalocean-size s-1vcpu-1gb \
         --digitalocean-tags sometag:somevalue,othertag:othervalue \
-        ${DM_MACHINE_NAME}
+        "${DM_MACHINE_NAME}"
 
 echo "Test docker machine inventory 0"
-ansible-inventory -i inventory_1.docker_machine.yml --list | grep -F ${DM_MACHINE_NAME}
+ansible-inventory -i inventory_1.docker_machine.yml --list | grep -F "${DM_MACHINE_NAME}"
 
 echo "Test docker_machine inventory 1"
 ansible-playbook -i inventory_1.docker_machine.yml playbooks/test_inventory_1.yml
