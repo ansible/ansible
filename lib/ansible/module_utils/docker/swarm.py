d7a52b67bed012a6f600ca065469bc4d63bdce96
Hannes Ljungberg <hannes@5monkeys.se>
2019-04-08 22:15:24 +0200

    Add docker_swarm_service_info module

--- lib/ansible/module_utils/docker/swarm.py
+++ lib/ansible/module_utils/docker/swarm.py
@@ -249,3 +249,49 @@ class AnsibleDockerSwarmClient(AnsibleDockerClient):
         if self.docker_py_version < LooseVersion('2.7.0'):
             return None
         return super(AnsibleDockerSwarmClient, self).get_unlock_key()
+
+    def get_all_services_inspect(self):
+        """
+        Returns Swarm service info as in 'docker service inspect' command about all registered services
+
+        :return:
+            Structure with information about all services
+        """
+        try:
+            service_info = self.services()
+        except APIError as exc:
+            if exc.status_code == 503:
+                self.fail("Cannot inspect service: To inspect service execute module on Swarm Manager")
+            self.fail("Error while reading from Swarm manager: %s" % to_native(exc))
+        except Exception as exc:
+            self.fail("Error inspecting swarm service: %s" % exc)
+
+        json_str = json.dumps(service_info, ensure_ascii=False)
+        service_info = json.loads(json_str)
+        return service_info
+
+    def get_service_inspect(self, service_id, skip_missing=False):
+        """
+        Returns Swarm service info as in 'docker service inspect' command about single service
+
+        :param service_id: service ID or name
+        :param skip_missing: if True then function will return None instead of failing the task
+        :return:
+            Single service information structure
+        """
+        try:
+            service_info = self.inspect_service(service=service_id)
+        except APIError as exc:
+            if exc.status_code == 503:
+                self.fail("Cannot inspect service: To inspect service execute module on Swarm Manager")
+            if exc.status_code == 404:
+                if skip_missing is False:
+                    self.fail("Error while reading from Swarm manager: %s" % to_native(exc))
+                else:
+                    return None
+        except Exception as exc:
+            self.fail("Error inspecting swarm service: %s" % exc)
+
+        json_str = json.dumps(service_info, ensure_ascii=False)
+        service_info = json.loads(json_str)
+        return service_info
