de600de48d64443925ef990ae7908e755d6c9ecb
Ondra Machacek <machacek.ondra@gmail.com>
2019-04-09 18:58:44 +0200

    kubevirt: Fix labels assignemt (#54852)

--- lib/ansible/module_utils/kubevirt.py
+++ lib/ansible/module_utils/kubevirt.py
@@ -328,7 +328,7 @@ class KubeVirtRawModule(KubernetesRawModule):
             template_spec['domain']['cpu']['model'] = cpu_model
 
         if labels:
-            self.merge_dicts(template['metadata']['labels'], labels)
+            template['metadata']['labels'] = dict(self.merge_dicts(labels, template['metadata']['labels']))
 
         if machine_type:
             template_spec['domain']['machine']['type'] = machine_type
