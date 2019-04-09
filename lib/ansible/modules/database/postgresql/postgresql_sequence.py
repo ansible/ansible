4178c2a282918e0292e7ded30b142a6c21452757
Tobias Birkefeld <tobias.birkefeld@rewe-digital.com>
2019-04-09 09:47:05 +0200

    change return variables and adjust tests

--- lib/ansible/modules/database/postgresql/postgresql_sequence.py
+++ lib/ansible/modules/database/postgresql/postgresql_sequence.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2019, Tobias Birkefeld <t@craxs.de>
+# Copyright: (c) 2019, Tobias Birkefeld (@tcraxs) <t@craxs.de>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
@@ -279,11 +279,11 @@ owner:
     samlpe: 'postgres'
 newname:
     description: Shows the new sequence name after rename.
-    returned: always
+    returned: on success
     samlpe: 'barfoo'
 newschema:
     description: Shows the new schema of the sequence after schema change.
-    returned: always
+    returned: on success
     samlpe: 'foobar'
 '''
 
