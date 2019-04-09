4ced179dd1461ed9eb54662f454de73221b20a78
ximon18 <3304436+ximon18@users.noreply.github.com>
2019-04-09 23:11:21 +0200

    Integration test improvements.

--- test/integration/targets/inventory_docker_machine/runme.sh
+++ test/integration/targets/inventory_docker_machine/runme.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # set DIGITALOCEAN_TOKEN to your Digital Ocean API token before running this test
 
+SCRIPT_DIR=$(dirname $0)
 export DM_MACHINE_NAME=dm-test-machine
 
 # restrict Ansible just to our inventory plugin, to prevent inventory data being matched by the test but being provided
@@ -13,7 +14,7 @@ set -euo pipefail
 
 cleanup() {
     echo "Cleanup"
-    ansible-playbook -i inventory_1.docker_machine.yml playbooks/teardown.yml
+    ansible-playbook -i teardown.docker_machine.yml playbooks/teardown.yml
     echo "Done"
     exit 0
 }
@@ -22,10 +23,10 @@ trap cleanup INT TERM EXIT
 
 echo "Check preconditions"
 # Host should NOT be known to Ansible before the test starts
-! ansible-inventory -i inventory_1.docker_machine.yml --list --host ${DM_MACHINE_NAME}
+ansible-inventory -i inventory_1.docker_machine.yml --host ${DM_MACHINE_NAME} >/dev/null && exit 1
 
 echo "Test that the docker_machine inventory plugin is being loaded"
-ANSIBLE_DEBUG=yes ansible-inventory -i inventory_1.docker_machine.yml --list >/dev/null | grep -Fq "Loading InventoryModule 'docker_machine'"
+ANSIBLE_DEBUG=yes ansible-inventory -i inventory_1.docker_machine.yml --list | grep -F "Loading InventoryModule 'docker_machine'"
 
 echo "Setup"
 ansible-playbook -i inventory_1.docker_machine.yml playbooks/setup.yml
@@ -33,3 +34,21 @@ ansible-playbook -i inventory_1.docker_machine.yml playbooks/setup.yml
 echo "Test docker_machine inventory 1"
 ansible-playbook -i inventory_1.docker_machine.yml playbooks/test_inventory_1.yml
 
+echo "Activate Docker Machine mock"
+SAVED_PATH=$PATH
+PATH=${SCRIPT_DIR}:$PATH
+
+echo "Test docker_machine inventory 2: daemon_required=yes daemon env success=yes"
+ansible-inventory -i inventory_2.docker_machine.yml --list
+
+echo "Test docker_machine inventory 2: daemon_required=yes daemon env success=no"
+MOCK_ERROR_IN=env ansible-inventory -i inventory_2.docker_machine.yml --list
+
+echo "Test docker_machine inventory 3: daemon_required=no daemon env success=yes"
+ansible-inventory -i inventory_3.docker_machine.yml --list
+
+echo "Test docker_machine inventory 3: daemon_required=no daemon env success=no"
+MOCK_ERROR_IN=env ansible-inventory -i inventory_2.docker_machine.yml --list
+
+echo "Deactivate Docker Machine mock"
+PATH=${SAVED_PATH}
