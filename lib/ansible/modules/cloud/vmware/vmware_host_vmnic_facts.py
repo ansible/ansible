ansible/ansible

commit f96c569f9d1568f634645e7d3d04b566812039ee

VMware: vmware_host_vmnic_facts: py3 fix (#55117)
With Python3, `filter()` returns an iterator. So we cannot do a `len()`
on the result.

lib/ansible/modules/cloud/vmware/vmware_host_vmnic_facts.py
@@ -192,11 +192,10 @@ class HostVmnicMgr(PyVmomi):
             host_vmnic_facts = dict(all=[], available=[], used=[], vswitch=dict(), dvswitch=dict())
             host_nw_system = host.configManager.networkSystem
             if host_nw_system:
+                vmnics = [pnic.device for pnic in nw_config.pnic if pnic.startswith('vmnic')]
                 nw_config = host_nw_system.networkConfig
                 host_vmnic_facts['all'] = [pnic.device for pnic in nw_config.pnic]
-                host_vmnic_facts['num_vmnics'] = (
-                    len(filter(lambda s: s.startswith('vmnic'), [pnic.device for pnic in nw_config.pnic]))
-                )
+                host_vmnic_facts['num_vmnics'] = len(vmnics)
                 host_vmnic_facts['vmnic_details'] = []
                 for pnic in host.config.network.pnic:
                     pnic_facts = dict()

commit e0e5f1d09d19d8cbed1fb7adff4da17c9d53be0c

vmware_host_vmnic_facts: ensure nm_config is define
Fix a regression introduced by f96c569f9d1568f634645e7d3d04b566812039ee.

lib/ansible/modules/cloud/vmware/vmware_host_vmnic_facts.py
@@ -192,8 +192,8 @@ class HostVmnicMgr(PyVmomi):
             host_vmnic_facts = dict(all=[], available=[], used=[], vswitch=dict(), dvswitch=dict())
             host_nw_system = host.configManager.networkSystem
             if host_nw_system:
-                vmnics = [pnic.device for pnic in nw_config.pnic if pnic.startswith('vmnic')]
                 nw_config = host_nw_system.networkConfig
+                vmnics = [pnic.device for pnic in nw_config.pnic if pnic.device.startswith('vmnic')]
                 host_vmnic_facts['all'] = [pnic.device for pnic in nw_config.pnic]
                 host_vmnic_facts['num_vmnics'] = len(vmnics)
                 host_vmnic_facts['vmnic_details'] = []