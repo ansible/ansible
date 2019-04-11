7d238df57bf182c28c07deef392c0e469d4c3ab2
Zim Kalinowski <zikalino@microsoft.com>
2019-04-11 17:28:27 +0800

    Fix in function app (#55134)

--- lib/ansible/modules/cloud/azure/azure_rm_functionapp.py
+++ lib/ansible/modules/cloud/azure/azure_rm_functionapp.py
@@ -344,11 +344,16 @@ class AzureRMFunctionApp(AzureRMModuleBase):
         """Construct the necessary app settings required for an Azure Function App"""
 
         function_app_settings = []
-        for key in ['AzureWebJobsStorage', 'WEBSITE_CONTENTAZUREFILECONNECTIONSTRING', 'AzureWebJobsDashboard']:
-            function_app_settings.append(NameValuePair(name=key, value=self.storage_connection_string))
-        function_app_settings.append(NameValuePair(name='FUNCTIONS_EXTENSION_VERSION', value='~1'))
-        function_app_settings.append(NameValuePair(name='WEBSITE_NODE_DEFAULT_VERSION', value='6.5.0'))
-        function_app_settings.append(NameValuePair(name='WEBSITE_CONTENTSHARE', value=self.name))
+
+        if self.container_settings is None:
+            for key in ['AzureWebJobsStorage', 'WEBSITE_CONTENTAZUREFILECONNECTIONSTRING', 'AzureWebJobsDashboard']:
+                function_app_settings.append(NameValuePair(name=key, value=self.storage_connection_string))
+            function_app_settings.append(NameValuePair(name='FUNCTIONS_EXTENSION_VERSION', value='~1'))
+            function_app_settings.append(NameValuePair(name='WEBSITE_NODE_DEFAULT_VERSION', value='6.5.0'))
+            function_app_settings.append(NameValuePair(name='WEBSITE_CONTENTSHARE', value=self.name))
+        else:
+            function_app_settings.append(NameValuePair(name='FUNCTIONS_EXTENSION_VERSION', value='~2'))
+
         return function_app_settings
 
     def aggregated_app_settings(self):
