e9cd2d1de6cec702cbe534f129f18634ace42435
Matt Davis <nitzmahone@users.noreply.github.com>
2019-04-10 22:59:53 -0700

    module_utils fixes in collections (#55118)
    * module_utils fixes in collections
    
    * fixed Windows module_utils in collections
    * fixed more Python module_utils cases (from X import module)
    * "medium style" Ansiballz modules now work properly with collections (ie, non-replacer but also not using basic.py)
    * added more tests
    * split Windows/POSIX exec
    
    * sanity

---
+++ test/integration/targets/collections/collection_root_user/ansible_collections/testns/testcoll/plugins/module_utils/AnotherCSMU.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AnsibleCollections.testns.testcoll.AnotherCSMU
+{
+    public class AnotherThing
+    {
+        public static string CallMe()
+        {
+            return "Hello from nested user-collection-hosted AnotherCSMU";
+        }
+    }
+}
