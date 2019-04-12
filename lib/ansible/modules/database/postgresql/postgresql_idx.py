f9c4272629f62d24e4d19bb5c5a71e2c5f77e82f
Andrey Klychkov <aaklychkov@mail.ru>
2019-04-12 15:59:32 +0300

    postgresql_idx: moved common params where they were

--- lib/ansible/modules/database/postgresql/postgresql_idx.py
+++ lib/ansible/modules/database/postgresql/postgresql_idx.py
@@ -31,10 +31,64 @@ options:
     required: true
     aliases:
     - name
+  db:
+    description:
+    - Name of database to connect to and where the index will be created/dropped.
+    type: str
+    aliases:
+    - login_db
+  port:
+    description:
+    - Database port to connect.
+    type: int
+    default: 5432
+    aliases:
+    - login_port
+  login_user:
+    description:
+    - User (role) used to authenticate with PostgreSQL.
+    type: str
+    default: postgres
+  session_role:
+    description:
+    - Switch to session_role after connecting.
+      The specified session_role must be a role that the current login_user is a member of.
+    - Permissions checking for SQL commands is carried out as though
+      the session_role were the one that had logged in originally.
+    type: str
+  login_password:
+    description:
+    - Password used to authenticate with PostgreSQL.
+    type: str
   schema:
     description:
     - Name of a database schema where the index will be created.
     type: str
+  login_host:
+    description:
+    - Host running PostgreSQL.
+    type: str
+  login_unix_socket:
+    description:
+    - Path to a Unix domain socket for local connections.
+    type: str
+  ssl_mode:
+    description:
+    - Determines whether or with what priority a secure SSL TCP/IP connection
+      will be negotiated with the server.
+    - See U(https://www.postgresql.org/docs/current/static/libpq-ssl.html) for
+      more information on the modes.
+    - Default of C(prefer) matches libpq default.
+    type: str
+    default: prefer
+    choices: [ allow, disable, prefer, require, verify-ca, verify-full ]
+  ca_cert:
+    description:
+    - Specifies the name of a file containing SSL certificate authority (CA)
+      certificate(s). If the file exists, the server's certificate will be
+      verified to be signed by one of these authorities.
+    type: str
+    aliases: [ ssl_rootcert ]
   state:
     description:
     - Index state.
@@ -97,60 +151,6 @@ options:
     - Mutually exclusive with I(concurrent=yes)
     type: bool
     default: no
-  db:
-    description:
-    - Name of database to connect to and where the index will be created/dropped.
-    type: str
-    aliases:
-    - login_db
-  port:
-    description:
-    - Database port to connect.
-    type: int
-    default: 5432
-    aliases:
-    - login_port
-  login_user:
-    description:
-    - User (role) used to authenticate with PostgreSQL.
-    type: str
-    default: postgres
-  session_role:
-    description:
-    - Switch to session_role after connecting.
-      The specified session_role must be a role that the current login_user is a member of.
-    - Permissions checking for SQL commands is carried out as though
-      the session_role were the one that had logged in originally.
-    type: str
-  login_password:
-    description:
-    - Password used to authenticate with PostgreSQL.
-    type: str
-  login_host:
-    description:
-    - Host running PostgreSQL.
-    type: str
-  login_unix_socket:
-    description:
-    - Path to a Unix domain socket for local connections.
-    type: str
-  ssl_mode:
-    description:
-    - Determines whether or with what priority a secure SSL TCP/IP connection
-      will be negotiated with the server.
-    - See U(https://www.postgresql.org/docs/current/static/libpq-ssl.html) for
-      more information on the modes.
-    - Default of C(prefer) matches libpq default.
-    type: str
-    default: prefer
-    choices: [ allow, disable, prefer, require, verify-ca, verify-full ]
-  ca_cert:
-    description:
-    - Specifies the name of a file containing SSL certificate authority (CA)
-      certificate(s). If the file exists, the server's certificate will be
-      verified to be signed by one of these authorities.
-    type: str
-    aliases: [ ssl_rootcert ]
 
 notes:
 - The index building process can affect database performance.
