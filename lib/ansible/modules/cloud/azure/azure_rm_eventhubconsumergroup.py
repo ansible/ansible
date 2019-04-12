5f4a450c75f7903d6e4c43b7bacc1a1a6eb5b8b1
FanQiu <fanqiu@microsoft.com>
2019-04-12 11:14:14 +0800

    support consumer_group creation

---
+++ lib/ansible/modules/cloud/azure/azure_rm_eventhubconsumergroup.py
@@ -0,0 +1,225 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2019 Fan Qiu, <fanqiu@microsoft.com>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+---
+module: azure_rm_eventhub_facts
+version_added: "2.9"
+short_description: Get Azure event hub facts.
+description:
+    - Get facts for an a specific event hub or all event hubs.
+
+options:
+    resource_group:
+        description:
+            - The resource group to search for the desired eventhub namespace.
+        required: true
+    namespace:
+        description:
+            - Name of the eventhub namespace
+        required: true
+    eventhub:
+        description:
+            - Name of the event hub.
+        required: true
+    consumer_group:
+        description:
+            - Name of the consumer group.
+        required: true
+    state:
+        description:
+            - Assert the state of the eventhub namespace. Use C(present) to create or update an eventhub namespace and C(absent) to delete it.
+        default: present
+        choices:
+            - absent
+            - present
+    user_metadata:
+        description:
+            - A placeholder to store user-defined string data with maximum length 1024.
+            - "It can be used to store descriptive data,
+              such as list of teams and their contact information also user-defined configuration settings can be stored."
+
+extends_documentation_fragment:
+    - azure
+
+author:
+    - "Fan Qiu (@MyronFanQiu)"
+
+'''
+
+EXAMPLES = '''
+    - name: Create a consumer group for an event hub
+      azure_rm_eventhubconsumergroup:
+        resource_group: myResourceGroup
+        namespace: testingnamespace
+        eventhub: testing
+        consumer_group: myGroup
+    - name: Update a consumer group for an event hub
+      azure_rm_eventhubconsumergroup:
+        resource_group: myResourceGroup
+        namespace: testingnamespace
+        eventhub: testing
+        consumer_group: myGroup
+        user_metadata: This_is_a_test
+    - name: Delete a consumer group for an event hub
+      azure_rm_eventhubconsumergroup:
+        resource_group: myResourceGroup
+        namespace: testingnamespace
+        eventhub: testing
+        consumer_group: myGroup
+        state: absent
+'''
+
+RETURN = '''
+id:
+    description:
+        - Resource ID of the consumer group.
+    returned: state is present
+    type: str
+name:
+    description:
+        - Name of the consumer group.
+    returned: state is present
+    type: str
+    sample: Testing
+type:
+    description:
+        - Resource type
+    returned: state is present
+    type: str
+    sample: "Microsoft.EventHub/Namespaces/EventHubs"
+user_metadata:
+    description:
+        - A placeholder to store user-defined string data with maximum length 1024.
+    returned: state is present
+    type: str
+'''
+
+from ansible.module_utils.azure_rm_common import AzureRMModuleBase
+from ansible.module_utils.common.dict_transformations import _snake_to_camel, _camel_to_snake
+try:
+    from msrestazure.tools import parse_resource_id
+    from msrestazure.azure_exceptions import CloudError
+except ImportError:
+    # This is handled in azure_rm_common
+    pass
+
+class AzureRMEventHubConsumerGroup(AzureRMModuleBase):
+
+    def __init__(self):
+
+        self.module_arg_spec = dict(
+            resource_group=dict(
+                type='str',
+                required=True
+            ),
+            namespace=dict(
+                type='str',
+                required=True
+            ),
+            eventhub=dict(
+                type='str',
+                required=True
+            ),
+            consumer_group=dict(
+                type='str',
+                required=True
+            ),
+            state=dict(
+                type='str',
+                default='present',
+                choices=['present', 'absent']
+            ),
+            user_metadata=dict(
+                tyep='str'
+            )
+        )
+
+        self.results = dict(
+            changed=False,
+            state=dict()
+        )
+
+        self.resource_group = None
+        self.namespace = None
+        self.eventhub = None
+        self.consumer_group = None
+        self.state = None
+        self.user_metadata = None
+
+        super(AzureRMEventHubConsumerGroup, self).__init__(self.module_arg_spec, supports_check_mode=True)
+
+    def exec_module(self, **kwargs):
+
+        for key in list(self.module_arg_spec.keys()):
+            setattr(self, key, kwargs[key])
+        
+        changed = False
+        result = None
+
+        result = self.get_consumer_group()
+        if self.state == 'present':
+            if not result:
+                changed = True
+                self.log('Creating a consumer group')
+                if not self.check_mode:
+                    result = self.create_or_update_consumer_group().as_dict()
+            else:
+                # Compare user_metadata
+                if self.user_metadata and self.user_metadata != result.user_metadata:
+                    changed = True
+                
+                if changed and not self.check_mode:
+                    result = self.create_or_update_consumer_group().as_dict()
+        elif result:
+            changed = True
+            if not self.check_mode:
+                self.delete_consumer_group()
+            result = True
+        self.results['changed'] = changed
+        self.results['state'] = result
+        return self.results
+
+    def get_consumer_group(self):
+        try:
+            return self.eventhub_client.consumer_groups.get(self.resource_group, self.namespace, self.eventhub, self.consumer_group)
+        except Exception as exc:
+            pass
+            return None
+
+    def create_or_update_consumer_group(self):
+        try:
+            return self.eventhub_client.consumer_groups.create_or_update(self.resource_group,
+                                                                         self.namespace,
+                                                                         self.eventhub,
+                                                                         self.consumer_group,
+                                                                         self.user_metadata)
+        except self.eventhub_models.ErrorResponseException as exc:
+            self.fail('Error creating or updating Consumer Group{0}: {1}'.format(self.consumer_group, str(exc.inner_exception) or exc.message or str(exc)))
+
+    def delete_consumer_group(self):
+        try:
+            return self.eventhub_client.consumer_groups.delete(self.resource_group,
+                                                               self.namespace,
+                                                               self.eventhub,
+                                                               self.consumer_group)
+        except self.eventhub_models.ErrorResponseException as exc:
+            self.fail('Error deleting Consumer Group{0}: {1}'.format(self.consumer_group, str(exc.inner_exception) or exc.message or str(exc)))
+
+
+def main():
+    AzureRMEventHubConsumerGroup()
+
+
+if __name__ == '__main__':
+    main()
