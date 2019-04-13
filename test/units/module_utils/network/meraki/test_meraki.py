e43d9d6c615c9511331ad34671836a23cfbd04ae
Kevin Breit <kevin.breit@kevinbreit.net>
2019-04-12 23:35:56 -0500

    Rewrote unit test to actually work - Uses pytest's fixtures structure, not classes - Added a test file for importing

--- test/units/module_utils/network/meraki/test_meraki.py
+++ test/units/module_utils/network/meraki/test_meraki.py
@@ -17,45 +17,69 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
-
-from units.compat import unittest
-from ansible.module_utils.network.meraki.meraki import MerakiModule
-from ansible.module_utils.six import PY2, PY3
-from ansible.module_utils._text import to_native, to_bytes
-
+import json
+import os
 import pytest
 
-def set_module_args(args):
-    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
-    basic._ANSIBLE_ARGS = to_bytes(args)
+from units.compat import unittest
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.network.meraki.meraki import MerakiModule, meraki_argument_spec
+from ansible.module_utils.six import PY2, PY3
+from ansible.module_utils._text import to_native, to_bytes
+from units.modules.utils import set_module_args
 
-def exit_json(*args, **kwargs):
-    if 'changed' not in kwargs:
-        kwargs['changed'] = False
-    raise AnsibleExitJson(kwargs)
+fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
+fixture_data = {}
+testcase_data = {
+    "params": {'orgs': ['orgs.json'],
+               }
+}
 
-def fail_json(*args, **kwargs):
-    if 'changed' not in kwargs:
-        kwargs['changed'] = False
-    raise AnsibleFailJson(kwargs)
+def load_fixture(name):
+    path = os.path.join(fixture_path, name)
 
-class AltModule():
-    params = dict(use_https='True',
-                  state='present',
-                  )
+    if path in fixture_data:
+        return fixture_data[path]
 
-class AltMerakiModule(MerakiModule):
-    def __init__(self):
-        self.result = dict(changed=False)
-        self.module = MerakiModule
+    with open(path) as f:
+        data = f.read()
 
-meraki = AltMerakiModule()
+    # try:
+    data = json.loads(data)
+    # except Exception:
+    #     pass
 
-class MerakiRest(unittest.TestCase):
-    def test_define_protocol(self):
-        set_module_args({'use_https': True,
-                         })
-        cdata = 'https'
-        testdata = meraki.define_protocol()
-        self.assertEqual(cdata, testdata)
+    fixture_data[path] = data
+    return data
+
+
+@pytest.fixture(scope="module")
+def module():
+    argument_spec = meraki_argument_spec()
+    set_module_args({'auth_key': 'abc123',
+                     })
+    module = AnsibleModule(argument_spec=argument_spec,
+                           supports_check_mode=False)    
+    return MerakiModule(module)
+
+def test_define_protocol_https(module):
+    module.params['use_https'] = True
+    module.define_protocol()
+    testdata = module.params['protocol']
+    assert testdata == 'https'
+
+def test_define_protocol_http(module):
+    module.params['use_https'] = False
+    module.define_protocol()
+    testdata = module.params['protocol']
+    assert testdata == 'http'
+
+def test_is_org_valid_org_name(module):
+    data = load_fixture('orgs.json')
+    org_count = module.is_org_valid(data, org_name="My organization")
+    assert org_count == 1
+
+def test_is_org_valid_org_id(module):
+    data = load_fixture('orgs.json')
+    org_count = module.is_org_valid(data, org_id=2930418)
+    assert org_count == 1
