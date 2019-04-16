a299b17f1eb1c16bcff356363da87df7a1d0fdb9
FanQiu <fanqiu@microsoft.com>
2019-04-16 10:00:13 +0800

    support eventhub creation gather facts for eventhub

---
+++ lib/ansible/modules/cloud/azure/azure_rm_eventhub.py
@@ -0,0 +1,284 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2019 Fan Qiu, <fanqiu@microsoft.com>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+---
+module: azure_rm_eventhub
+version_added: "2.9"
+short_description: Manage Azure Event hub.
+description:
+    - Create, update and delete an Azure Event hub.
+
+options:
+    resource_group:
+        description:
+            - Name of a resource group where the Event hub exists or will be created.
+        required: true
+    name:
+        description:
+            - Name of the Event hub.
+        required: true
+    state:
+        description:
+            - Assert the state of the Event hub. Use C(present) to create or update an event hub and C(absent) to delete it.
+        default: present
+        choices:
+            - absent
+            - present
+    namespace:
+        description:
+            - The Namespace name
+        required: true
+    message_retention_in_days:
+        description:
+            - Number of days to retain the events for this Event Hub, value should be 1 to 7 days
+        type: int
+    partition_count:
+        description:
+            - Number of partitions created for the Event Hub, allowed values are from 1 to 32 partitions.
+            - Is not changeable after the creation
+        type: int
+    status:
+        description:
+            - Enumerates the possible values for the status of the Event Hub.
+        choices:
+            - active
+            - disabled
+            - send_disabled
+            - receive_disabled
+
+extends_documentation_fragment:
+    - azure
+
+author:
+    - "Fan Qiu (@MyronFanQiu)"
+
+'''
+
+EXAMPLES = '''
+    - name: Create an event hub with default
+      azure_rm_eventhub:
+        name: myEventhub
+        resource_group: myResourceGroup
+        namespace: myEventhubNamespace
+
+    - name: Create an event hub with partition_count
+      azure_rm_eventhub:
+        name: myEventhub
+        resource_group: myResourceGroup
+        namespace: myEventhubNamespace
+        partition_count: 8
+
+    - name: Delete the event hub
+      azure_rm_eventhub:
+        name: myEventhub
+        resource_group: myResourceGroup
+        namespace: myEventhubNamespace
+        state: absent
+'''
+
+RETURN = '''
+id:
+    description:
+        - Resource ID of the event hub.
+    returned: state is present
+    type: str
+    sample: "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxx/resourceGroups/myResourceGroup/providers/Microsoft.EventHub/namespaces/myEventhub"
+name:
+    description:
+        - Name of the event hub.
+    returned: state is present
+    type: str
+    sample: myEventhubNamespace
+message_retention_in_days:
+    description:
+        - Number of days to retain the events for this Event Hub, value should be 1 to 7 days
+    returned: state is present
+    type: int
+    sample: 7
+status:
+    description:
+        - Enumerates the possible values for the status of the Event Hub.
+    returned: state is present
+    type: str
+    sample: Active
+partition_count:
+    description:
+        - Number of partitions created for the Event Hub, allowed values are from 1 to 32 partitions.
+    returned: state is present
+    type: str
+    sample: 4
+partition_ids:
+    description:
+        - Current number of shards on the Event Hub.
+    returned: state is present
+    type: list
+type:
+    description:
+        - Resource type
+    returned: state is present
+    type: str
+    sample: "Microsoft.EventHub/Namespaces/EventHubs"
+'''
+
+from ansible.module_utils.azure_rm_common import AzureRMModuleBase
+try:
+    from msrestazure.tools import parse_resource_id
+    from msrestazure.azure_exceptions import CloudError
+except ImportError:
+    # This is handled in azure_rm_common
+    pass
+
+
+class AzureRMEventHub(AzureRMModuleBase):
+    """Configuration class for an Azure RM Event hub resource"""
+
+    def __init__(self):
+
+        self.module_arg_spec = dict(
+            resource_group=dict(
+                type='str',
+                required=True
+            ),
+            name=dict(
+                type='str',
+                required=True
+            ),
+            state=dict(
+                type='str',
+                default='present',
+                choices=['present', 'absent']
+            ),
+            namespace=dict(
+                type='str',
+                required=True
+            ),
+            message_retention_in_days=dict(
+                type='int'
+            ),
+            partition_count=dict(
+                type='int'
+            ),
+            status=dict(
+                type='str',
+                choices=['active', 'disabled', 'send_disabled', 'receive_disabled']
+            )
+        )
+
+        self.results = dict(
+            changed=False,
+            id=None
+        )
+
+        # TODO: Add support for capture_description
+        self.resource_group = None
+        self.name = None
+        self.state = None
+        self.namespace = None
+        self.message_retention_in_days = None
+        self.partition_count = None
+        self.status = None
+
+        super(AzureRMEventHub, self).__init__(self.module_arg_spec, supports_check_mode=True, supports_tags=False)
+
+    def exec_module(self, **kwargs):
+        """Main module execution method"""
+
+        for key in self.module_arg_spec.keys():
+            setattr(self, key, kwargs[key])
+
+        changed = False
+        results = None
+
+        self.status = self.eventhub_models.EntityStatus[self.status] if self.status else None
+        event_hub = self.get_event_hub()
+
+        if self.state == 'present':
+            # TODO: Add support for capture_description
+            if not event_hub:
+                changed = True
+                event_hub_instance = self.eventhub_models.Eventhub(message_retention_in_days=self.message_retention_in_days,
+                                                                   partition_count=self.partition_count,
+                                                                   status=self.status)
+                if not self.check_mode:
+                    event_hub = self.create_or_update_event_hub(event_hub_instance)
+
+            else:
+                changed, event_hub = self.check_status(changed=changed, event_hub=event_hub)
+
+                if changed and not self.check_mode:
+                    event_hub = self.create_or_update_event_hub(event_hub)
+            results = self.to_dict(event_hub)['id'] if event_hub else None
+        elif event_hub:
+            changed = True
+            if not self.check_mode:
+                self.delete_event_hub()
+
+        self.results['changed'] = changed
+        self.results['id'] = results
+        return self.results
+
+    def get_event_hub(self):
+        try:
+            self.log('Getting an event hub')
+            return self.eventhub_client.event_hubs.get(resource_group_name=self.resource_group, namespace_name=self.namespace, event_hub_name=self.name)
+        except Exception as exc:
+            pass
+            return None
+
+    def check_status(self, changed, event_hub):
+        # Compare message_retention_in_days
+        if self.message_retention_in_days and self.message_retention_in_days != event_hub.message_retention_in_days:
+            changed = True
+            event_hub.message_retention_in_days = self.message_retention_in_days
+
+        # Compare partition_count
+        if self.partition_count and self.partition_count != event_hub.partition_count:
+            self.module.warn("partition_count is not changeable after the creation!")
+            event_hub.partition_count = None
+
+        # Compare status
+        if self.status and self.status != event_hub.status:
+            changed = True
+            event_hub.status = self.status
+        return changed, event_hub
+
+    def create_or_update_event_hub(self, event_hub):
+        try:
+            self.log('Creating or updating an event hub')
+            return self.eventhub_client.event_hubs.create_or_update(resource_group_name=self.resource_group,
+                                                                    namespace_name=self.namespace,
+                                                                    event_hub_name=self.name,
+                                                                    parameters=event_hub)
+        except self.eventhub_models.ErrorResponseException as exc:
+            self.fail('Error creating or updating Event Hub{0}: {1}'.format(self.name, str(exc.inner_exception) or exc.message or str(exc)))
+
+    def delete_event_hub(self):
+        try:
+            self.log('Deleting an event hub')
+            return self.eventhub_client.event_hubs.delete(resource_group_name=self.resource_group,
+                                                          namespace_name=self.namespace,
+                                                          event_hub_name=self.name)
+        except self.eventhub_models.ErrorResponseException as exc:
+            self.fail('Error deleting Event Hub{0}: {1}'.format(self.name, str(exc.inner_exception) or exc.message or str(exc)))
+
+    def to_dict(self, event_hub):
+        return event_hub.as_dict()
+
+
+def main():
+    AzureRMEventHub()
+
+
+if __name__ == '__main__':
+    main()
---
+++ lib/ansible/modules/cloud/azure/azure_rm_eventhub_facts.py
@@ -0,0 +1,222 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2019 Fan Qiu, <fanqiu@microsoft.com>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+---
+module: azure_rm_eventhub_facts
+version_added: "2.9"
+short_description: Get Azure event hub facts.
+description:
+    - Get facts for an a specific event hub or all event hubs.
+
+options:
+    resource_group:
+        description:
+            - The resource group to search for the desired eventhub namespace.
+        required: true
+    namespace:
+        description:
+            - Name of the eventhub namespace
+        required: true
+    name:
+        description:
+            - Name of the event hub.
+    show_sas_policies:
+        description:
+            - Whether to show the SAS policies.
+            - Note if enable this option, the facts module will raise more than two HTTP calls for each resources, need more network overhead.
+        type: bool
+    show_consumer_groups:
+        description:
+            - Whether to show the consumer groups.
+            - Note if enable this option, the facts module will raise one more HTTP call for each resources, need more network overhead.
+        type: bool
+
+extends_documentation_fragment:
+    - azure
+
+author:
+    - "Fan Qiu (@MyronFanQiu)"
+
+'''
+
+EXAMPLES = '''
+    - name: List all azure eventhubs by namespace
+      azure_rm_eventhub_facts:
+        resource_group: myResourceGroup
+        namespace: testingnamespace
+
+    - name: Get the facts for a specific eventhub
+      azure_rm_eventhub_facts:
+        resource_group: myResourceGroup
+        namespace: testingnamespace
+        name: testing
+'''
+
+RETURN = '''
+eventhubs:
+    description: List of eventhub dicts.
+    returned: always
+    type: list
+'''
+
+from ansible.module_utils.azure_rm_common import AzureRMModuleBase
+from ansible.module_utils.common.dict_transformations import _snake_to_camel, _camel_to_snake
+try:
+    from msrestazure.tools import parse_resource_id
+    from msrestazure.azure_exceptions import CloudError
+except ImportError:
+    # This is handled in azure_rm_common
+    pass
+
+
+class AzureRMEventHubFact(AzureRMModuleBase):
+
+    def __init__(self):
+
+        self.module_arg_spec = dict(
+            resource_group=dict(
+                type='str',
+                required=True
+            ),
+            namespace=dict(
+                type='str',
+                required=True
+            ),
+            name=dict(
+                type='str'
+            ),
+            show_sas_policies=dict(
+                type='bool'
+            ),
+            show_consumer_groups=dict(
+                type='bool'
+            )
+        )
+
+        self.results = dict(
+            changed=False,
+            eventhubs=[]
+        )
+
+        self.resource_group = None
+        self.namespace = None
+        self.name = None
+        self.show_sas_policies = None
+        self.show_consumer_groups = None
+
+        super(AzureRMEventHubFact, self).__init__(self.module_arg_spec, supports_tags=False, facts_module=True)
+
+    def exec_module(self, **kwargs):
+
+        for key in self.module_arg_spec:
+            setattr(self, key, kwargs[key])
+
+        response = []
+        if self.name:
+            response = self.get_item()
+        else:
+            response = self.list_by_namespace()
+        self.results['eventhubs'] = [self.to_dict(x) for x in response]
+        if self.show_sas_policies:
+            self.results['eventhubs'] = [self.get_sas_policies(x) for x in self.results['eventhubs']]
+        if self.show_consumer_groups:
+            self.results['eventhubs'] = [self.get_consumer_groups(x) for x in self.results['eventhubs']]
+        return self.results
+
+    def get_item(self):
+        '''Get a single eventhub'''
+
+        self.log('Get properies for {0}'.format(self.name))
+
+        item = None
+
+        try:
+            item = self.eventhub_client.event_hubs.get(resource_group_name=self.resource_group,
+                                                       namespace_name=self.namespace,
+                                                       event_hub_name=self.name)
+            return [item]
+        except self.eventhub_models.ErrorResponseException as exc:
+            self.fail('Error when getting eventhub {0}: {1}'.format(self.name, str(exc.inner_exception) or exc.message or str(exc)))
+
+    def list_by_namespace(self):
+        '''Get all eventhub in a resource group'''
+
+        try:
+            self.log('Getting facts for event hubs by a eventhub namespace')
+            return self.eventhub_client.event_hubs.list_by_namespace(resource_group_name=self.resource_group, namespace_name=self.namespace)
+        except self.eventhub_models.ErrorResponseException as exc:
+            self.fail('Filed to list eventhub by namespace {0}: {1}'.format(self.namespace, str(exc.inner_exception) or exc.message or str(exc)))
+
+    def get_sas_policies(self, eventhub):
+        results = eventhub
+        name = eventhub['name']
+        rules = self.list_authorization_rules(name)
+        results['sas_keys'] = [self.list_keys(eventhub=name, rule_name=x.name).as_dict() for x in rules]
+        return results
+
+    def list_authorization_rules(self, eventhub):
+        try:
+            self.log('Getting authorization rules of an event hub')
+            return self.eventhub_client.event_hubs.list_authorization_rules(resource_group_name=self.resource_group,
+                                                                            namespace_name=self.namespace,
+                                                                            event_hub_name=eventhub)
+        except self.eventhub_models.ErrorResponseException as exc:
+            self.fail('Failed to list authorization rules of namespace {0}: {1}'.format(eventhub, str(exc.inner_exception) or exc.message or str(exc)))
+
+    def list_keys(self, eventhub, rule_name):
+        try:
+            self.log('Getting sas keys of a authorization rule')
+            return self.eventhub_client.event_hubs.list_keys(resource_group_name=self.resource_group,
+                                                             namespace_name=self.namespace,
+                                                             event_hub_name=eventhub,
+                                                             authorization_rule_name=rule_name)
+        except self.eventhub_models.ErrorResponseException as exc:
+            self.fail('Failed to list keys of authorization rules {0}: {1}'.format(rule_name,
+                                                                                   str(exc.inner_exception) or exc.message or str(exc)))
+
+    def get_consumer_groups(self, eventhub):
+        results = eventhub
+        consumer_groups = self.list_consumer_groups(eventhub['name'])
+        results['consumer_groups'] = [x.as_dict() for x in consumer_groups]
+        return results
+
+    def list_consumer_groups(self, eventhub):
+        try:
+            self.log('Getting consumer groups of an event hub')
+            return self.eventhub_client.consumer_groups.list_by_event_hub(resource_group_name=self.resource_group,
+                                                                          namespace_name=self.namespace,
+                                                                          event_hub_name=eventhub)
+        except self.eventhub_models.ErrorResponseException as exc:
+            self.fail('Failed to list consumer groups of the eventhub {0}: {1}'.format(eventhub, str(exc.inner_exception) or exc.message or str(exc)))
+
+    def to_dict(self, eventhub):
+        result = dict(
+            id=eventhub.id,
+            name=eventhub.name,
+            message_retention_in_days=eventhub.message_retention_in_days,
+            partition_count=eventhub.partition_count,
+            partition_ids=eventhub.partition_ids,
+            status=eventhub.status
+        )
+        return result
+
+
+def main():
+    """Main module execution code path"""
+
+    AzureRMEventHubFact()
+
+
+if __name__ == '__main__':
+    main()
