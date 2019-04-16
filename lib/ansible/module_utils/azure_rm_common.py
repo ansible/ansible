f716bd048f7859cf134b0d5ee26197223d822d69
zikalino <zikalino@microsoft.com>
2019-04-16 20:52:29 +0800

    normalize resource id implementation

--- lib/ansible/module_utils/azure_rm_common.py
+++ lib/ansible/module_utils/azure_rm_common.py
@@ -463,14 +463,51 @@ class AzureRMModuleBase(object):
         resource_dict['subscription_id'] = resource_dict.get('subscription_id', self.subscription_id)
         return resource_dict
 
-    def normalize_resource_id(self, resource_id, pattern):
+    def normalize_resource_id(self, value, pattern):
         '''
         Return a proper resource id string..
 
         :param resource_id: It could be a resource name, resource id or dict containing parts from the pattern.
         :param pattern: pattern of resource is, just like in Azure Swagger
         '''
-        return resource_id
+        pattern_parts = pattern.split('/')
+        for i in range(len(pattern_parts)):
+            x = re.sub('[{} ]+', '', pattern_parts[i], 2)
+            if len(x) < len(pattern_parts[i]):
+            pattern_parts[i] = '{' + re.sub('([a-z0-9])([A-Z])', r'\1_\2', x).lower() + '}'
+
+        if isinstance(value, str):
+            value_parts = value.split('/')
+            if len(value_parts) == 1:
+            value_dict = {}
+            value_dict['name'] = value
+            else:  
+            if len(value_parts) != len(pattern_parts):
+                return None
+            value_dict = {}
+            for i in range(len(value_parts)):
+                if pattern_parts[i].startswith('{'):
+                value_dict[pattern_parts[i][1:-1]] = value_parts[i]
+                elif value_parts[i].lower() != pattern_parts[i].lower():
+                return None
+        elif isinstance(value, dict):
+            value_dict = value
+        else:
+            return None
+
+        if not value_dict.get('subscription_id'):
+            value_dict['subscription_id'] = self.subscription_id
+        if not value_dict.get('resource_group'):
+            value_dict['resource_group'] = self.resource_group
+
+        for i in range(len(pattern_parts)):
+            if pattern_parts[i].startswith('{'):
+            value = value_dict.get(pattern_parts[i][1:-1], None)
+            if not value:
+                return None
+            pattern_parts[i] = value
+
+        return "/".join(pattern_parts)
 
     def idempotency_check(self, old_params, new_params):
         '''
