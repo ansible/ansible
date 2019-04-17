85478accc286b7b168f06a7d6fec5bb31fb5f9b0
mnecas <necas.marty@gmail.com>
2019-04-17 14:20:40 +0200

    ovirt jobs add docs

--- lib/ansible/modules/cloud/ovirt/ovirt_job.py
+++ lib/ansible/modules/cloud/ovirt/ovirt_job.py
@@ -27,38 +27,40 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = '''
 ---
 module: ovirt_job
-short_description: Module to manage tags in oVirt/RHV
+short_description: Module to manage jobs in oVirt/RHV
 version_added: "2.9"
-author: "Ondra Machacek (@machacekondra)"
+author: "Martin Necas (@mnecas)"
 description:
-    - "This module manage tags in oVirt/RHV. It can also manage assignments
-       of those tags to entities."
+    - "This module manage jobs in oVirt/RHV. It can also manage steps of the job."
 options:
-    id:
+    description:
         description:
-            - "ID of the tag to manage."
-    name:
-        description:
-            - "Name of the tag to manage."
+            - "Description of the job."
         required: true
     state:
         description:
-            - "Should the tag be present/absent."
-            - "C(Note): I(attached) and I(detached) states are supported since version 2.4."
-        choices: ['present', 'absent']
+            - "Should the job be present/absent/failed."
+            - "Present have alias started and absent have alias finished, you can use both."
+            - "Note when finished it will finish all steps."
+        choices: ['present', 'absent', 'created', 'finished', 'failed']
         default: present
-    description:
+    steps:
         description:
-            - "Description of the tag to manage."
-    parent:
-        description:
-            - "Name of the parent tag."
-    vms:
-        description:
-            - "List of the VMs names, which should have assigned this tag."
-    hosts:
-        description:
-            - "List of the hosts names, which should have assigned this tag."
+            - "The steps of the job."
+        suboptions:
+            description:
+                description:
+                    - "Description of the step."
+                required: true
+            state:
+                description:
+                    - "Should the setp be present/absent/failed."
+                    - "C(present) have alias C(started) and C(absent) have alias C(finished), you can use both."
+                    - "Note when one step fail whole job will fail"
+                    - "Note when all steps are finished it will finish job."
+                choices: ['present', 'absent', 'created', 'finished', 'failed']
+                default: present
+        type: list
 extends_documentation_fragment: ovirt
 '''
 
@@ -66,20 +68,31 @@ EXAMPLES = '''
 # Examples don't contain auth parameter for simplicity,
 # look at ovirt_auth module to see how to reuse authentication:
 
-# Create(if not exists) and assign tag to vms vm1 and vm2:
-- ovirt_tag:
-    name: mytag
-    vms:
-      - vm1
-      - vm2
+- name: Create job with two steps
+  ovirt_job:
+    description: job_name
+    steps:
+      - description: step_name_A
+      - description: step_name_B
 
-# Attach a tag to VM 'vm3', keeping the rest already attached tags on VM:
-- ovirt_tag:
-    name: mytag
-    state: attached
-    vms:
-      - vm3
+- name: Finish one step
+  ovirt_job:
+    description: job_name
+    steps:
+      - description: step_name_A
+        state: finished
 
+- name: When you fail one step whole job will stop
+  ovirt_job:
+    description: job_name
+    steps:
+      - description: step_name_B
+        state: failed
+
+- name: Finish all steps
+  ovirt_job:
+    description: job_name
+    state: finished
 '''
 
 RETURN = '''
@@ -89,7 +102,7 @@ id:
     type: str
     sample: 7de90f31-222c-436c-a1ca-7e655bd5b60c
 job:
-    description: "Dictionary of all the tag attributes. Tag attributes can be found on your oVirt/RHV instance
+    description: "Dictionary of all the tag attributes. Job attributes can be found on your oVirt/RHV instance
                   at following url: http://ovirt.github.io/ovirt-engine-api-model/master/#types/job."
     returned: On success if tag is found.
     type: dict
@@ -104,7 +117,6 @@ except ImportError:
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.ovirt import (
-    BaseModule,
     check_sdk,
     create_connection,
     equal,
@@ -113,52 +125,55 @@ from ansible.module_utils.ovirt import (
     get_dict_of_struct,
 )
 
+
 def build_job(module):
     return otypes.Job(
-            description=module.params['description'],
-            status=otypes.JobStatus.STARTED,
-            external=True,
-            auto_cleared=True
+        description=module.params['description'],
+        status=otypes.JobStatus.STARTED,
+        external=True,
+        auto_cleared=True
     )
+
 
 def build_step(step, entity_id):
     return otypes.Step(
-            description=step.get('description'),
-            type=otypes.StepEnum.UNKNOWN,
-            job=otypes.Job(
-                id=entity_id
-            ),
-            status=otypes.StepStatus.STARTED,
-            external=True,
+        description=step.get('description'),
+        type=otypes.StepEnum.UNKNOWN,
+        job=otypes.Job(
+            id=entity_id
+        ),
+        status=otypes.StepStatus.STARTED,
+        external=True,
     )
 
+
 def attach_steps(module, entity_id, jobs_service):
-    # Attach NICs to VM, if specified:
     changed = False
     steps_service = jobs_service.job_service(entity_id).steps_service()
     if module.params.get('steps'):
         for step in module.params.get('steps'):
-            step_entity = get_entity(steps_service, step.get('description')) 
-            step_state = step.get('state','present')
-            if not module.check_mode:
-                if step_state == 'present' or step_state == 'started':
-                    if step_entity is None:
-                        steps_service.add(build_step(step, entity_id))
-                        changed = True
-                if step_entity is not None and step_entity.status != otypes.StepStatus.FINISHED:
-                    if step_state == 'absent' or step_state == 'finished':
-                        steps_service.step_service(step_entity.id).end(status=otypes.StepStatus.FINISHED, succeeded=True)
-                        changed = True
-                    elif step_state == 'failed':
-                        steps_service.step_service(step_entity.id).end(status=otypes.StepStatus.FINISHED, succeeded=False)
-                        changed = True
+            step_entity = get_entity(steps_service, step.get('description'))
+            step_state = step.get('state', 'present')
+            if step_state == 'present' or step_state == 'started':
+                if step_entity is None:
+                    steps_service.add(build_step(step, entity_id))
+                    changed = True
+            if step_entity is not None and step_entity.status not in [otypes.StepStatus.FINISHED, otypes.StepStatus.FAILED]:
+                if step_state == 'absent' or step_state == 'finished':
+                    steps_service.step_service(step_entity.id).end(status=otypes.StepStatus.FINISHED, succeeded=True)
+                    changed = True
+                elif step_state == 'failed':
+                    steps_service.step_service(step_entity.id).end(status=otypes.StepStatus.FINISHED, succeeded=False)
+                    changed = True
     return changed
+
 
 def get_entity(service, description):
     all_entities = service.list()
     for entity in all_entities:
         if entity.description == description:
             return entity
+
 
 def main():
     argument_spec = ovirt_full_argument_spec(
@@ -171,7 +186,7 @@ def main():
     )
     module = AnsibleModule(
         argument_spec=argument_spec,
-        supports_check_mode=True,
+        supports_check_mode=False,
     )
 
     check_sdk(module)
@@ -183,22 +198,23 @@ def main():
 
         state = module.params['state']
         job = get_entity(jobs_service, module.params['description'])
-        changed = False 
+        changed = False
         if state in ['present', 'started']:
             if job is None:
                 job = jobs_service.add(build_job(module))
                 changed = True
-            changed = changed or attach_steps(module, job.id, jobs_service)
+            changed = attach_steps(module, job.id, jobs_service) or changed
 
-        elif state in ['absent', 'finished']:
-            jobs_service.job_service(job.id).end(status=otypes.JobStatus.FINISHED, succeeded=True)
-            changed = True
+        if job is not None and job.status not in [otypes.JobStatus.FINISHED, otypes.JobStatus.FAILED]:
+            if state in ['absent', 'finished']:
+                jobs_service.job_service(job.id).end(status=otypes.JobStatus.FINISHED, succeeded=True)
+                changed = True
 
-        elif state == 'failed':
-            jobs_service.job_service(job.id).end(status=otypes.JobStatus.FINISHED, succeeded=False)
-            changed = True
+            elif state == 'failed':
+                jobs_service.job_service(job.id).end(status=otypes.JobStatus.FINISHED, succeeded=False)
+                changed = True
 
-        ret ={
+        ret = {
             'changed': changed,
             'id': getattr(job, 'id', None),
             type(job).__name__.lower(): get_dict_of_struct(
