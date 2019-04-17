88357b7bae223ef0af6adf42223f4c583eaa2282
orso <orso@checkpoint.com>
2019-04-17 15:23:17 +0300

    maybe this

--- lib/ansible/modules/network/checkpoint/checkpoint_host.py
+++ lib/ansible/modules/network/checkpoint/checkpoint_host.py
@@ -62,7 +62,7 @@ options:
   policy_package:
     description:
       - Package policy name to be installed.
-    type: str
+    type: bool
   targets:
     description:
       - Targets to install the package policy on.
