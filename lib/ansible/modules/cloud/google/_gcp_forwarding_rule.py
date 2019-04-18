e298de0986339090e09d229fd5f120073dc23dc1
Alex Stephen <alexstephen@google.com>
2019-04-18 07:30:27 -0700

    GCP deprecations (#53355)
    * gcdns_record deprecation
    
    * gcdns_zone deprecation
    
    * gcp_forwarding_rule deprecation
    
    * gcp_url_map deprecation
    
    * gcspanner deprecation
    
    * gcp_healthcheck deprecation
    
    * porting guide
    
    * test failures
    
    * forgot to rename
    
    * deprecating
    
    * porting guide changes
    
    * unit test fixes
    
    * changing ignores

--- lib/ansible/modules/cloud/google/_gcp_forwarding_rule.py
+++ lib/ansible/modules/cloud/google/_gcp_forwarding_rule.py
@@ -7,7 +7,7 @@ __metaclass__ = type
 
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
+                    'status': ['deprecated'],
                     'supported_by': 'community'}
 
 
@@ -28,6 +28,10 @@ requirements:
   - "google-api-python-client >= 1.6.2"
   - "google-auth >= 0.9.0"
   - "google-auth-httplib2 >= 0.0.2"
+deprecated:
+    removed_in: "2.12"
+    why: Updated modules released with increased functionality
+    alternative: Use M(gcp_compute_forwarding_rule) or M(gcp_compute_global_forwarding_rule) instead.
 notes:
   - Currently only supports global forwarding rules.
     As such, Load Balancing Scheme is always EXTERNAL.
