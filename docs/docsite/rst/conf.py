820a20cca1af3a234791edf518576a10a2fc939a
Sandra McCann <samccann@redhat.com>
2019-04-18 11:44:37 -0400

    moved theme changes to conf.py

--- docs/docsite/rst/conf.py
+++ docs/docsite/rst/conf.py
@@ -135,6 +135,8 @@ html_short_title = 'Ansible Documentation'
 
 html_theme_options = {
     'canonical_url': "https://docs.ansible.com/ansible/latest/",
+    'collapse_navigation' : "True",
+    'navigation_depth' : 2
 }
 
 # The style sheet to use for HTML and HTML Help pages. A file of that name
