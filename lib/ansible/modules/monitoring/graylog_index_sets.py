d87b5f0d5366c47149503e9bff8a38ff07dd645a
Abhijeet Kasurde <akasurde@redhat.com>
2019-04-19 16:37:19 -0400

    Update lib/ansible/modules/monitoring/graylog_index_sets.py
    Co-Authored-By: shortstack <whitney.ellis.powell@gmail.com>

--- lib/ansible/modules/monitoring/graylog_index_sets.py
+++ lib/ansible/modules/monitoring/graylog_index_sets.py
@@ -267,7 +267,7 @@ def update(module, base_url, headers, index_set_id, title, description, index_pr
         module.fail_json(msg="Fail: %s" % ("Status: " + str(info['msg']) + ", Message: " + str(info['body'])))
 
     try:
-        content = response.read()
+        content = to_text(response.read(), errors='surrogate_or_strict')
     except AttributeError:
         content = info.pop('body', '')
 
