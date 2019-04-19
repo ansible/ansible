03638f088a2ea93a2d1baf52e90f0352126d07fd
shortstack <whitney.ellis.champion@gmail.com>
2019-04-18 20:58:06 -0400

    comment fail

--- lib/ansible/modules/monitoring/graylog_pipelines.py
+++ lib/ansible/modules/monitoring/graylog_pipelines.py
@@ -587,7 +587,7 @@ def main():
     connection_url = "https://%s/api/system/pipelines/connections" % (endpoint)
 
     api_token = get_token(module, endpoint, graylog_user, graylog_password)
-    headers = '{ "Content-Type": "application/json", "X-Requested-By": "Graylog API", "Accept": "application/json",
+    headers = '{ "Content-Type": "application/json", "X-Requested-By": "Graylog API", "Accept": "application/json", \
                 "Authorization": "Basic ' + api_token.decode() + '" }'
 
     if action == "create":
