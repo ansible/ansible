a921d9f3d3c5c78a2f2c0e24d6fd64ee6a560b4b
Abhijeet Kasurde <akasurde@redhat.com>
2019-04-19 14:40:44 -0400

    Update lib/ansible/modules/monitoring/graylog_index_sets.py
    Co-Authored-By: shortstack <whitney.ellis.powell@gmail.com>

--- lib/ansible/modules/monitoring/graylog_index_sets.py
+++ lib/ansible/modules/monitoring/graylog_index_sets.py
@@ -281,7 +281,7 @@ def delete(module, base_url, headers, index_set_id):
         module.fail_json(msg="Fail: %s" % ("Status: " + str(info['msg']) + ", Message: " + str(info['body'])))
 
     try:
-        content = response.read()
+        content = to_text(response.read(), errors='surrogate_or_strict')
     except AttributeError:
         content = info.pop('body', '')
 
