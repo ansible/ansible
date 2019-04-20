08ca59f9419ab8488e6da284df6c83256b7417e0
Evgeniy Krysanov <evgeniy.krysanov@gmail.com>
2019-04-20 19:02:32 +0300

    iam_managed_policy: add diff mode

--- lib/ansible/modules/cloud/amazon/iam_managed_policy.py
+++ lib/ansible/modules/cloud/amazon/iam_managed_policy.py
@@ -278,6 +278,18 @@ def detach_all_entities(module, iam, policy, **kwargs):
         detach_all_entities(module, iam, policy, marker=entities['Marker'])
 
 
+def diff(module, policy_document_before, policy_document_after):
+    if module._diff:
+        return {
+            'diff': {
+                'before': policy_document_before,
+                'after': policy_document_after,
+            }
+        }
+    else:
+        return {}
+
+
 def main():
     argument_spec = ec2_argument_spec()
     argument_spec.update(dict(
@@ -305,10 +317,12 @@ def main():
     default = module.params.get('make_default')
     only = module.params.get('only_version')
 
-    policy = None
-
     if module.params.get('policy') is not None:
-        policy = json.dumps(json.loads(module.params.get('policy')))
+        policy_document_after = json.loads(module.params.get('policy'))
+        policy_document = json.dumps(policy_document_after)
+    else:
+        policy_document_after = None
+        policy_document = None
 
     try:
         region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
@@ -319,26 +333,37 @@ def main():
                          exceptions=traceback.format_exc(), **camel_dict_to_snake_dict(e.response))
 
     p = get_policy_by_name(module, iam, name)
+    policy_document_before = None
+
+    if p:
+        try:
+            policy_document_before = iam.get_policy_version(
+                PolicyArn=p['Arn'], VersionId=p['DefaultVersionId'])['PolicyVersion']['Document']
+        except botocore.exceptions.ClientError as e:
+            module.fail_json(msg="Couldn't get policy version %s: %s" % (p['DefaultVersionId'], str(e)),
+                             exception=traceback.format_exc(),
+                             **camel_dict_to_snake_dict(e.response))
+
     if state == 'present':
         if p is None:
             if not module.check_mode:
                 # No Policy so just create one
                 try:
-                    rvalue = iam.create_policy(PolicyName=name, Path='/',
-                                               PolicyDocument=policy, Description=description)
+                    p = iam.create_policy(PolicyName=name, Path='/',
+                                          PolicyDocument=policy_document, Description=description)['Policy']
                 except Exception as e:
                     module.fail_json(msg="Couldn't create policy %s: %s" % (name, to_native(e)),
                                      exception=traceback.format_exc(),
                                      **camel_dict_to_snake_dict(e.response))
 
-                module.exit_json(changed=True, policy=camel_dict_to_snake_dict(rvalue['Policy']))
-            else:
-                module.exit_json(changed=True, policy=None)
+            module.exit_json(changed=True, policy=p and camel_dict_to_snake_dict(p),
+                             **diff(module, policy_document_before, policy_document_after))
         else:
-            policy_version, changed = get_or_create_policy_version(module, iam, p, policy)
+            policy_version, changed = get_or_create_policy_version(module, iam, p, policy_document)
             changed = set_if_default(module, iam, p, policy_version, default) or changed
             changed = set_if_only(module, iam, p, policy_version, only) or changed
-            # If anything has changed we needto refresh the policy
+
+            # If anything has changed we need to refresh the policy
             if changed:
                 try:
                     p = iam.get_policy(PolicyArn=p['Arn'])['Policy']
@@ -347,7 +372,8 @@ def main():
                                      exception=traceback.format_exc(),
                                      **camel_dict_to_snake_dict(e.response))
 
-            module.exit_json(changed=changed, policy=camel_dict_to_snake_dict(p))
+            module.exit_json(changed=changed, policy=camel_dict_to_snake_dict(p),
+                             **diff(module, policy_document_before, policy_document_after))
     else:
         # Check for existing policy
         if p:
@@ -379,9 +405,10 @@ def main():
                                      **camel_dict_to_snake_dict(e.response))
 
             # This is the one case where we will return the old policy
-            module.exit_json(changed=True, policy=camel_dict_to_snake_dict(p))
+            module.exit_json(changed=True, policy=camel_dict_to_snake_dict(p),
+                             **diff(module, policy_document_before, None))
         else:
-            module.exit_json(changed=False, policy=None)
+            module.exit_json(changed=False, policy=None, **diff(module, None, None))
 # end main
 
 
