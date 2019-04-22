59c73d2e165688f59e2d05db784bc1f950708fb4
Jack Ivanov <e601809@gmail.com>
2019-04-22 09:42:58 +0200

    scaleway_compute: add boot_type parameter

--- lib/ansible/modules/cloud/scaleway/scaleway_compute.py
+++ lib/ansible/modules/cloud/scaleway/scaleway_compute.py
@@ -44,6 +44,15 @@ options:
     default: false
     type: bool
 
+  boot_type:
+    description:
+      - Boot method
+    default: bootscript
+    version_added: 2.9
+    choices:
+      - bootscript
+      - local
+
   image:
     description:
       - Image identifier used to start the instance with
@@ -256,6 +265,7 @@ def create_server(compute_api, server):
     compute_api.module.debug("Starting a create_server")
     target_server = None
     data = {"enable_ipv6": server["enable_ipv6"],
+            "boot_type": server["boot_type"],
             "tags": server["tags"],
             "commercial_type": server["commercial_type"],
             "image": server["image"],
@@ -617,6 +627,7 @@ def core(module):
         "name": module.params["name"],
         "commercial_type": module.params["commercial_type"],
         "enable_ipv6": module.params["enable_ipv6"],
+        "boot_type": module.params["boot_type"],
         "tags": module.params["tags"],
         "organization": module.params["organization"],
         "security_group": module.params["security_group"]
@@ -643,6 +654,7 @@ def main():
         region=dict(required=True, choices=SCALEWAY_LOCATION.keys()),
         commercial_type=dict(required=True),
         enable_ipv6=dict(default=False, type="bool"),
+        boot_type=dict(default="bootscript", choices=['bootscript', 'local']),
         public_ip=dict(default="absent"),
         state=dict(choices=state_strategy.keys(), default='present'),
         tags=dict(type="list", default=[]),
