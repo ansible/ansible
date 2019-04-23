3e0acfdf6ebe4073462772a05dc304ec7ea2e5f4
zikalino <zikalino@microsoft.com>
2019-04-23 21:07:21 +0800

    fixed client

--- lib/ansible/modules/cloud/azure/azure_rm_azurefirewall.py
+++ lib/ansible/modules/cloud/azure/azure_rm_azurefirewall.py
@@ -815,7 +815,7 @@ class AzureRMAzureFirewalls(AzureRMModuleBase):
         old_response = None
         response = None
 
-        self.mgmt_client = self.get_mgmt_svc_client(NetworkManagementClientClient,
+        self.mgmt_client = self.get_mgmt_svc_client(NetworkManagementClient,
                                                     base_url=self._cloud_environment.endpoints.resource_manager)
 
         resource_group = self.get_resource_group(self.resource_group)
