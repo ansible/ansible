12d463e87bc8ea4035cb9691de11dd0453a7cb33
xuxiaowei0512 <35824522+xuxiaowei0512@users.noreply.github.com>
2019-04-24 23:12:28 +0800

    Update ce_vxlan_gateway.py

--- lib/ansible/modules/network/cloudengine/ce_vxlan_gateway.py
+++ lib/ansible/modules/network/cloudengine/ce_vxlan_gateway.py
@@ -400,9 +400,9 @@ class VxlanGateway(object):
         flags = list()
         exp = " | ignore-case section include dfs-group"
         if self.vpn_instance:
-            exp += "|^ip vpn-instance %s$" % self.vpn_instance
+            exp += "| ip vpn-instance %s" % self.vpn_instance
         if self.vbdif_name:
-            exp += "|^interface %s$" % self.vbdif_name
+            exp += "| interface %s " % self.vbdif_name
         flags.append(exp)
         return self.get_config(flags)
 
