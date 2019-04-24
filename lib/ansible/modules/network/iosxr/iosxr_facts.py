b1d8bd7f8dd070f3734767123d1a15df1a1f166a
marouim <marouim@gmail.com>
2019-04-24 12:34:59 -0400

    Added parsing of LLDP remote port description (#54873)
    * Added parsing of LLDP remote port description
    
    * Removed whitespace on blank line.

--- lib/ansible/modules/network/iosxr/iosxr_facts.py
+++ lib/ansible/modules/network/iosxr/iosxr_facts.py
@@ -280,6 +280,7 @@ class Interfaces(FactsBase):
                 facts[intf] = list()
             fact = dict()
             fact['host'] = self.parse_lldp_host(entry)
+            fact['remote_description'] = self.parse_lldp_remote_desc(entry)
             fact['port'] = self.parse_lldp_port(entry)
             facts[intf].append(fact)
         return facts
@@ -348,6 +349,11 @@ class Interfaces(FactsBase):
 
     def parse_lldp_intf(self, data):
         match = re.search(r'^Local Interface: (.+)$', data, re.M)
+        if match:
+            return match.group(1)
+
+    def parse_lldp_remote_desc(self, data):
+        match = re.search(r'Port Description: (.+)$', data, re.M)
         if match:
             return match.group(1)
 
