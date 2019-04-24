5f02a406cf55f90d20458ac55d4024f3fa7d6d40
thefaftek <thefaftek@users.noreply.github.com>
2019-04-24 09:30:07 -0500

    Update win_domain_group_membership.py (#55594)
    * Update win_domain_group_membership.py by adding notes regarding a computer object and fixing a typo

--- lib/ansible/modules/windows/win_domain_group_membership.py
+++ lib/ansible/modules/windows/win_domain_group_membership.py
@@ -25,7 +25,8 @@ options:
   members:
     description:
       - A list of members to ensure are present/absent from the group.
-      - The given names must be a SmaAccountName of a user, group, service account, or computer
+      - The given names must be a SamAccountName of a user, group, service account, or computer.
+      - For computers, you must add "$" after the name; for example, to add "Mycomputer" to a group, use "Mycomputer$" as the member.
     type: list
     required: yes
   state:
