1b78cad34aeed4cb40ec0ae724b9b2cbd7b5234f
Kevin Breit <kevin.breit@kevinbreit.net>
2019-04-24 14:27:21 -0500

    meraki_content_filtering - Remove redundant API call with get_nets() (#55531)
    * Remove redundant API call with get_nets()
    
    * Add changelog fragment

--- lib/ansible/modules/network/meraki/meraki_content_filtering.py
+++ lib/ansible/modules/network/meraki/meraki_content_filtering.py
@@ -172,8 +172,6 @@ def main():
     org_id = meraki.params['org_id']
     if not org_id:
         org_id = meraki.get_org_id(meraki.params['org_name'])
-    nets = meraki.get_nets(org_id=org_id)
-
     net_id = None
     if net_id is None:
         nets = meraki.get_nets(org_id=org_id)
