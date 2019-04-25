9bb3b4b38611bbcc2736e487660918890304a936
zikalino <zikalino@microsoft.com>
2019-04-25 12:30:20 +0800

    added more description on comparison function

--- lib/ansible/module_utils/azure_rm_common_temp.py
+++ lib/ansible/module_utils/azure_rm_common_temp.py
@@ -535,6 +535,30 @@ class AzureRMModuleBase(object):
                 self.create_compare_modifiers(o.get('options'), p, result)
 
     def default_compare(self, modifiers, new, old, path, result):
+        '''
+            Default dictionary comparison.
+            This function will work well with most of the Azure resources.
+            It correctly handles "location" comparison.
+            
+            Value handling:
+                - if "new" value is None, it will be taken from "old" dictionary if "incremental_update"
+                  is enabled. 
+            List handling:
+                - if list contains "name" field it will be sorted by "name" before comparison is done.
+                - if module has "incremental_update" set, items missing in the new list will be copied
+                  from the old list
+
+            Warnings:
+                If field is marked as non-updatable, appropriate warning will be printed out and
+                "new" structure will be updated to old value.
+
+            :modifiers: Optional dictionary of modifiers, where key is the path and value is dict of modifiers
+            :param new: New version
+            :param old: Old version
+
+            Returns True if no difference between structures has been detected.
+            Returns False if difference was detected.
+        '''
         if new is None:
             return True
         elif isinstance(new, dict):
