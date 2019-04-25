4ccfa61fa96f43f29150ac5ad61c1537e9cd13a7
Matt Clay <matt@mystile.com>
2019-04-25 09:40:22 -0700

    Fix test_postgresql dependency analysis.

--- test/units/module_utils/test_postgresql.py
+++ test/units/module_utils/test_postgresql.py
@@ -21,6 +21,12 @@ class TestPostgres(unittest.TestCase):
             if mod in sys.modules:
                 del sys.modules[mod]
 
+    def test_import(self):
+        # this import makes dependency analysis work so the tests will run when the module_utils change
+        from ansible.module_utils.postgres import HAS_PSYCOPG2
+
+        assert HAS_PSYCOPG2 is not None
+
     @patch.object(builtins, '__import__')
     def test_postgres_pg2_missing_ensure_libs(self, mock_import):
         def _mock_import(name, *args, **kwargs):
