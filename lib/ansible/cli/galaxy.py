95792d2281e17d3a38d7bce888e3ce39498b9a94
Alicia Cozine <acozine@users.noreply.github.com>
2019-04-26 11:28:24 -0500

    removes hard-coded default, shows local value

--- lib/ansible/cli/galaxy.py
+++ lib/ansible/cli/galaxy.py
@@ -66,8 +66,7 @@ class GalaxyCLI(CLI):
         roles_path = opt_help.argparse.ArgumentParser(add_help=False)
         roles_path.add_argument('-p', '--roles-path', dest='roles_path', type=opt_help.unfrack_path(pathsep=True),
                                 default=C.DEFAULT_ROLES_PATH, action=opt_help.PrependListAction,
-                                help='The path to the directory containing your roles. The default is the roles_path '
-                                     'configured in your ansible.cfg file (~/ansible/roles if not configured)')
+                                help='The path to the directory containing your roles. By default it will use the first 'writable' directory you have configured in DEFAULT_ROLES_PATH: %s ' % ':'.join(C.DEFAULT_ROLES_PATH)
 
         force = opt_help.argparse.ArgumentParser(add_help=False)
         force.add_argument('-f', '--force', dest='force', action='store_true', default=False, help='Force overwriting an existing role')
