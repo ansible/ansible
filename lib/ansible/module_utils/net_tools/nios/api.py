71f01599cf84fb26aa63452f169b2196704b6dbf
krisvasudevan <43845852+krisvasudevan@users.noreply.github.com>
2019-04-29 01:54:54 -0700

    Update nios_member.py (#54419)
    * Update nios_member.py
    
    * Update api.py
    
    * Update nios_member.py
    
    * Update nios_member.py
    
    * Update api.py

--- lib/ansible/module_utils/net_tools/nios/api.py
+++ lib/ansible/module_utils/net_tools/nios/api.py
@@ -151,8 +151,16 @@ def member_normalize(member_spec):
     It will remove any arguments that are set to None since WAPI will error on
     that condition.
     The remainder of the value validation is performed by WAPI
+    Some parameters in ib_spec are passed as a list in order to pass the validation for elements.
+    In this function, they are converted to dictionary.
     '''
+    member_elements = ['vip_setting', 'ipv6_setting', 'lan2_port_setting', 'mgmt_port_setting',
+                       'pre_provisioning', 'network_setting', 'v6_network_setting',
+                       'ha_port_setting', 'lan_port_setting', 'lan2_physical_setting',
+                       'lan_ha_port_setting', 'mgmt_network_setting', 'v6_mgmt_network_setting']
     for key in member_spec.keys():
+        if key in member_elements:
+            member_spec[key] = member_spec[key][0]
         if isinstance(member_spec[key], dict):
             member_spec[key] = member_normalize(member_spec[key])
         elif isinstance(member_spec[key], list):
