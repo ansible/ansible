e5f7304f4405433b3c563f5d97ebd2cfbb2bcadd
Sumit Jaiswal <sjaiswal@redhat.com>
2019-04-30 21:20:28 +0530

    Commenting Cliconf support call for IOS-XR modules for Ansible 2.9 version (#55751)
    * iosxr cliconf support call
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>
    
    * iosxr cliconf support call
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>
    
    * iosxr cliconf support call
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>
    
    * iosxr cliconf support call
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>
    
    * iosxr cliconf support call
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>

--- lib/ansible/modules/network/iosxr/iosxr_interface.py
+++ lib/ansible/modules/network/iosxr/iosxr_interface.py
@@ -637,8 +637,9 @@ def main():
 
     config_object = None
     if is_cliconf(module):
-        module.deprecate("cli support for 'iosxr_interface' is deprecated. Use transport netconf instead",
-                         version='2.9')
+        # Commenting the below cliconf deprecation support call for Ansible 2.9 as it'll be continued to be supported
+        # module.deprecate("cli support for 'iosxr_interface' is deprecated. Use transport netconf instead",
+        #                  version='2.9')
         config_object = CliConfiguration(module)
     elif is_netconf(module):
         if module.params['active'] == 'preconfigure':
--- lib/ansible/modules/network/iosxr/iosxr_user.py
+++ lib/ansible/modules/network/iosxr/iosxr_user.py
@@ -700,8 +700,9 @@ def main():
 
     config_object = None
     if is_cliconf(module):
-        module.deprecate(msg="cli support for 'iosxr_user' is deprecated. Use transport netconf instead",
-                         version="2.9")
+        # Commenting the below cliconf deprecation support call for Ansible 2.9 as it'll be continued to be supported
+        # module.deprecate("cli support for 'iosxr_interface' is deprecated. Use transport netconf instead",
+        #                  version='2.9')
         config_object = CliConfiguration(module, result)
     elif is_netconf(module):
         config_object = NCConfiguration(module, result)
