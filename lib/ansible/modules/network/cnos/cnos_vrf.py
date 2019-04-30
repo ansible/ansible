1963b55d9d9094165329f17fb770b6b3c3a1953f
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2019-05-01 00:18:25 +0530

    Update cnos_vrf.py Backport coverity fix
    (cherry picked from commit ac546457c8bdda12707738dfaa843bf4d791339d)

--- lib/ansible/modules/network/cnos/cnos_vrf.py
+++ lib/ansible/modules/network/cnos/cnos_vrf.py
@@ -228,7 +228,30 @@ def map_obj_to_commands(updates, module):
 def map_config_to_obj(module):
     objs = []
     output = run_commands(module, {'command': 'show vrf'})
+<<<<<<< HEAD
     if output is None:
+=======
+    if output is not None:
+        vrfText = output[0].strip()
+        vrfList = vrfText.split('VRF')
+        for vrfItem in vrfList:
+            if 'FIB ID' in vrfItem:
+                obj = dict()
+                list_of_words = vrfItem.split()
+                vrfName = list_of_words[0]
+                obj['name'] = vrfName[:-1]
+                obj['rd'] = list_of_words[list_of_words.index('RD') + 1]
+                start = False
+                obj['interfaces'] = []
+                for intName in list_of_words:
+                    if 'Interfaces' in intName:
+                        start = True
+                    if start is True:
+                        if '!' not in intName and 'Interfaces' not in intName:
+                            obj['interfaces'].append(intName.strip().lower())
+                objs.append(obj)
+    else:
+>>>>>>> ac546457c8... Update cnos_vrf.py
         module.fail_json(msg='Could not fetch VRF details from device')
     vrfText = output[0].strip()
     vrfList = vrfText.split('VRF')
