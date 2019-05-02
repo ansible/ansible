e1914f5e184e76506d288366c1a946cc75d11a50
Xander Madsen <xander.madsen@gmail.com>
2019-05-02 08:48:57 -0400

    Add GetChassisPower command to Chassis category of redfish_facts (#54314)
    * Add GetChassisPower as viable Chassis command in redfish_facts
    
    * Add conditional to catch command == 'GetChassisPower'
    
    * Implement get_chassis_power() retrieving Watts-related and other useful power data from each Chassis
    
    * Add RelatedItem property and remove MemberId property
    
    * Check for length of data['PowerControl'] to be > 0 before checking index 0 of the list.
    
    * Fix return statement being inside loop and result key not being 'entries' as is expected
    
    * Update lib/ansible/module_utils/redfish_utils.py
    
    Co-Authored-By: xmadsen <xander.madsen@gmail.com>
    
    * Update lib/ansible/module_utils/redfish_utils.py
    
    Co-Authored-By: xmadsen <xander.madsen@gmail.com>
    
    * Update redfish_facts.py

--- lib/ansible/module_utils/redfish_utils.py
+++ lib/ansible/module_utils/redfish_utils.py
@@ -920,6 +920,44 @@ class RedfishUtils(object):
         result["entries"] = fan_results
         return result
 
+    def get_chassis_power(self):
+        result = {}
+        key = "Power"
+
+        # Get these entries, but does not fail if not found
+        properties = ['Name', 'PowerAllocatedWatts',
+                      'PowerAvailableWatts', 'PowerCapacityWatts',
+                      'PowerConsumedWatts', 'PowerMetrics',
+                      'PowerRequestedWatts', 'RelatedItem', 'Status']
+
+        chassis_power_results = []
+        # Go through list
+        for chassis_uri in self.chassis_uri_list:
+            chassis_power_result = {}
+            response = self.get_request(self.root_uri + chassis_uri)
+            if response['ret'] is False:
+                return response
+            result['ret'] = True
+            data = response['data']
+            if key in data:
+                response = self.get_request(self.root_uri + chassis_uri +
+                                            "/" + key)
+                data = response['data']
+                if 'PowerControl' in data:
+                    if len(data['PowerControl']) > 0:
+                        data = data['PowerControl'][0]
+                        for property in properties:
+                            if property in data:
+                                chassis_power_result[property] = data[property]
+                else:
+                    return {'ret': False, 'msg': 'Key PowerControl not found.'}
+                chassis_power_results.append(chassis_power_result)
+            else:
+                return {'ret': False, 'msg': 'Key Power not found.'}
+
+        result['entries'] = chassis_power_results
+        return result
+
     def get_chassis_thermals(self):
         result = {}
         sensors = []
