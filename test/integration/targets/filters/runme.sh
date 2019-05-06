5b33849b063e9cc60c6f97024b0c412e44f60bc9
droark <droark@intellinetcorp.com>
2019-05-06 16:49:37 -0400

    account for passlib <1.7 check

--- test/integration/targets/filters/runme.sh
+++ test/integration/targets/filters/runme.sh
@@ -7,6 +7,9 @@ source virtualenv.sh
 # Requirements have to be installed prior to running ansible-playbook
 # because plugins and requirements are loaded before the task runs
 
-pip install jmespath netaddr bcrypt
+pip install jmespath netaddr bcrypt passlib
 
 ANSIBLE_ROLES_PATH=../ ansible-playbook filters.yml -i ../../inventory -e @../../integration_config.yml "$@"
+
+pip install "passlib<1.7"
+ANSIBLE_ROLES_PATH=../ ansible-playbook filters.yml -t password_hash -i ../../inventory -e @../../integration_config.yml "$@"
