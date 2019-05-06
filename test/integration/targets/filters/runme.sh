08d2adf23d73f4497e5776006a745a84956a1da4
droark <droark@intellinetcorp.com>
2019-05-06 15:11:40 -0400

    move pip install to the existing line

--- test/integration/targets/filters/runme.sh
+++ test/integration/targets/filters/runme.sh
@@ -4,11 +4,9 @@ set -eux
 
 source virtualenv.sh
 
-pip install bcrypt
-
 # Requirements have to be installed prior to running ansible-playbook
 # because plugins and requirements are loaded before the task runs
 
-pip install jmespath netaddr
+pip install jmespath netaddr bcrypt
 
 ANSIBLE_ROLES_PATH=../ ansible-playbook filters.yml -i ../../inventory -e @../../integration_config.yml "$@"
