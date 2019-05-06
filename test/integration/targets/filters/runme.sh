7fbd0a21cecc889725e256d82589ea15a09b69a5
droark <droark@intellinetcorp.com>
2019-05-06 17:50:36 -0400

    support testing the CryptHash side of the password_hash filter

--- test/integration/targets/filters/runme.sh
+++ test/integration/targets/filters/runme.sh
@@ -13,3 +13,11 @@ ANSIBLE_ROLES_PATH=../ ansible-playbook filters.yml -i ../../inventory -e @../..
 
 pip install "passlib<1.7"
 ANSIBLE_ROLES_PATH=../ ansible-playbook filters.yml -t password_hash -i ../../inventory -e @../../integration_config.yml "$@"
+
+# Required to test against CryptHash class
+
+source virtualenv-isolated.sh
+
+pip install jmespath netaddr bcrypt -r ../../../../requirements.txt
+
+ANSIBLE_ROLES_PATH=../ ansible-playbook filters.yml -e crypt_skip=True -t password_hash -i ../../inventory -e @../../integration_config.yml "$@"
