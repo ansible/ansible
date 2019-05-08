21226bb13b79fdd4deae5dd47e1648f4867f32b3
Stoned Elipot <stoned.elipot@gmail.com>
2019-05-08 11:07:52 +0200

    Placate 'ansible-test sanity --test pep8'

--- lib/ansible/modules/web_infrastructure/ansible_tower/tower_role.py
+++ lib/ansible/modules/web_infrastructure/ansible_tower/tower_role.py
@@ -33,7 +33,8 @@ options:
       description:
         - The role type to grant/revoke.
       required: True
-      choices: ["admin", "read", "member", "execute", "adhoc", "update", "use", "auditor", "project_admin", "inventory_admin", "credential_admin", "workflow_admin", "notification_admin", "job_template_admin"]
+      choices: ["admin", "read", "member", "execute", "adhoc", "update", "use", "auditor", "project_admin", "inventory_admin", "credential_admin",
+                "workflow_admin", "notification_admin", "job_template_admin"]
     target_team:
       description:
         - Team that the role acts on.
@@ -114,7 +115,7 @@ def main():
         user=dict(),
         team=dict(),
         role=dict(choices=["admin", "read", "member", "execute", "adhoc", "update", "use", "auditor", "project_admin", "inventory_admin", "credential_admin",
-            "workflow_admin", "notification_admin", "job_template_admin"]),
+                           "workflow_admin", "notification_admin", "job_template_admin"]),
         target_team=dict(),
         inventory=dict(),
         job_template=dict(),
