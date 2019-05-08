2e8a3efccb8f90ae03e61e3b08d238790fb50658
Trishna Guha <trishnaguha17@gmail.com>
2019-05-08 20:49:29 +0530

    Revert nxos, ios, iosxr return_timestamps (#56206)
    * Revert "nxos_command:run_commands results failure when commands array size >1 (#52670)"
    This reverts commit 0df5b92af37d43da20125dc418f1cb92390a8a25.
    * Revert "added timestamps to nxos_command module (#50261)"
    This reverts commit e1509433144572e5010d0ced72a2129600dd8bd4.
    * Revert "added timestamps to ios_command module (#50323)"
    This reverts commit 2a432a093b392e3f8babea6b760df6a5c8716b03.
    * Revert "added response_timestamps to iosxr_command module (#50095)"
    This reverts commit 2a0c356da9d18a98399f2802ea8a52eb421f87ac.

--- lib/ansible/module_utils/network/iosxr/iosxr.py
+++ lib/ansible/module_utils/network/iosxr/iosxr.py
@@ -477,10 +477,10 @@ def load_config(module, command_filter, commit=False, replace=False,
     return diff
 
 
-def run_commands(module, commands, check_rc=True, return_timestamps=False):
+def run_commands(module, commands, check_rc=True):
     connection = get_connection(module)
     try:
-        return connection.run_commands(commands=commands, check_rc=check_rc, return_timestamps=return_timestamps)
+        return connection.run_commands(commands=commands, check_rc=check_rc)
     except ConnectionError as exc:
         module.fail_json(msg=to_text(exc))
 
