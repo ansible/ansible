8e6b84c303f17a0a1351143e2e1d9cfd50fd9efb
Nathaniel Case <ncase@redhat.com>
2019-05-09 08:32:25 -0400

    eos_config: Fix test issues (#56180)
    * Alter tests to pass
    
    * Change diff_against to make changed work again
    
    * Add another diff_against
    
    * Expose supports_sessions across all EOS connection types
    
    * Change session warning to failure
    
    * supports_sessions needs to be a method to survive the rpc boundary
    
    * Alter tests to match

--- lib/ansible/modules/network/eos/eos_config.py
+++ lib/ansible/modules/network/eos/eos_config.py
@@ -396,6 +396,10 @@ def main():
     flags = ['all'] if module.params['defaults'] else []
     connection = get_connection(module)
 
+    # Refuse to diff_against: session if essions are disabled
+    if module.params['diff_against'] == 'session' and not connection.supports_sessions:
+        module.fail_json(msg="Cannot diff against sessions when sessions are disabled. Please change diff_against to another value")
+
     if module.params['backup'] or (module._diff and module.params['diff_against'] == 'running'):
         contents = get_config(module, flags=flags)
         config = NetworkConfig(indent=1, contents=contents)
