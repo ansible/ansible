e731496c84e43f0289ee746323550dda18caef66
acalm <commitendant@localhost>
2019-05-10 09:15:48 +0200

    don't mix single and double quotes

--- lib/ansible/modules/cloud/openstack/os_server.py
+++ lib/ansible/modules/cloud/openstack/os_server.py
@@ -464,8 +464,8 @@ def _network_args(module, cloud):
 def _parse_meta(meta):
     if isinstance(meta, str):
         metas = {}
-        for kv_str in meta.split(","):
-            k, v = kv_str.split("=")
+        for kv_str in meta.split(','):
+            k, v = kv_str.split('=')
             metas[k] = v
         return metas
     if not meta:
@@ -480,7 +480,7 @@ def _delete_server(module, cloud):
             timeout=module.params['timeout'],
             delete_ips=module.params['delete_fip'])
     except Exception as e:
-        module.fail_json(msg="Error in deleting vm: %s" % e.message)
+        module.fail_json(msg='Error in deleting vm: %s' % e.message)
     module.exit_json(changed=True, result='deleted')
 
 
@@ -494,17 +494,17 @@ def _create_server(module, cloud):
         image_id = cloud.get_image_id(
             module.params['image'], module.params['image_exclude'])
         if not image_id:
-            module.fail_json(msg="Could not find image %s" %
+            module.fail_json(msg='Could not find image %s' %
                              module.params['image'])
 
     if flavor:
         flavor_dict = cloud.get_flavor(flavor)
         if not flavor_dict:
-            module.fail_json(msg="Could not find flavor %s" % flavor)
+            module.fail_json(msg='Could not find flavor %s' % flavor)
     else:
         flavor_dict = cloud.get_flavor_by_ram(flavor_ram, flavor_include)
         if not flavor_dict:
-            module.fail_json(msg="Could not find any matching flavor")
+            module.fail_json(msg='Could not find any matching flavor')
 
     nics = _network_args(module, cloud)
 
@@ -661,7 +661,7 @@ def _get_server_state(module, cloud):
     if server and state == 'present':
         if server.status not in ('ACTIVE', 'SHUTOFF', 'PAUSED', 'SUSPENDED'):
             module.fail_json(
-                msg="The instance is available but not Active state: " + server.status)
+                msg='The instance is available but not Active state: %s' % server.status)
         (ip_changed, server) = _check_ips(module, cloud, server)
         (sg_changed, server) = _check_security_groups(module, cloud, server)
         (server_changed, server) = _update_server(module, cloud, server)
@@ -670,7 +670,7 @@ def _get_server_state(module, cloud):
     if server and state == 'absent':
         return True
     if state == 'absent':
-        module.exit_json(changed=False, result="not present")
+        module.exit_json(changed=False, result='not present')
     return True
 
 
@@ -728,13 +728,11 @@ def main():
     if state == 'present':
         if not (image or boot_volume):
             module.fail_json(
-                msg="Parameter 'image' or 'boot_volume' is required "
-                    "if state == 'present'"
+                msg='Parameter image or boot_volume is required if state == present'
             )
         if not flavor and not flavor_ram:
             module.fail_json(
-                msg="Parameter 'flavor' or 'flavor_ram' is required "
-                    "if state == 'present'"
+                msg='Parameter flavor or flavor_ram is required if state == present'
             )
 
     sdk, cloud = openstack_cloud_from_module(module)
