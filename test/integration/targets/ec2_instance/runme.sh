2c2989765f85678669a1904785961700265d4f3e
Andrea Tartaglia <andrea@braingap.uk>
2019-05-10 19:43:15 +0100

    Removed nested role for ec2_instance tests
    Also moved environment setup and environment cleanup to separate task
    files
    
    Added CI group
    
    Updated c4.large instances to t3.nano, use ENA enabled AMI
    
    Updated AMIs with Amazon Linux AMI
    
    Updated us-east-1 AMI id
    
    Specify EBS size
    
    typo, volume_size
    
    Removed 'state: running', updated AMI with amz ami
    
    Also added several wait: false to speed things up
    
    Check for instance state, correctly
    
    Accept pending as valid state
    
    tags_and_vpc_settings do not wait for instance to complete
    
    wait for termination protected instance
    
    Updated IAM role name to match with policy
    
    Skip env cleanup when in CI
    
    Do not wait instance in external_resource_attach
    
    ENI remove is done in env_cleanup/CI
    
    wait some time for instance_profile instnace to be up
    
    Updated ebs_optimized_images
    
    Corrected task name
    
    Added aws_cleanup var in cloud-config-aws
    
    typo in runme
    
    default aws_cleanup to true

--- test/integration/targets/ec2_instance/runme.sh
+++ test/integration/targets/ec2_instance/runme.sh
@@ -2,14 +2,14 @@
 
 set -eux
 
-export ANSIBLE_ROLES_PATH=../
+export ANSIBLE_ROLES_PATH=../../targets/
 
 # Test graceful failure for older versions of botocore
 source virtualenv.sh
 pip install 'botocore<1.10.16' 'boto3<1.7.16'
-ansible-playbook -i ../../inventory -e @../../integration_config.yml -v playbooks/version_fail.yml "$@"
+ansible-playbook -v playbooks/version_fail.yml "$@"
 
 # Run full test suite
 source virtualenv.sh
 pip install 'botocore>=1.10.16' boto3
-ansible-playbook -i ../../inventory -e @../../integration_config.yml -v playbooks/full_test.yml "$@"
+ansible-playbook -v playbooks/full_test.yml "$@"
