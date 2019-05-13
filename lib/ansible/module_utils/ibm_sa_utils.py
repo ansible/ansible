3d699b095d5e500344ddb1ac80b9aa09ab477a2a
Tzur Eliyahu <tzur22@gmail.com>
2019-05-13 19:41:06 +0300

    IBM_Storage: fixed a bug - Unwanted arguments cause failure (#56357)
    * IBM_Storage: fixed a bug which caused several of our modules to pass unwanted arguments and fail
    
    * IBM_Storage: Added a changelog for 56357 bug

--- lib/ansible/module_utils/ibm_sa_utils.py
+++ lib/ansible/module_utils/ibm_sa_utils.py
@@ -81,6 +81,8 @@ def build_pyxcli_command(fields):
     """ Builds the args for pyxcli using the exact args from ansible"""
     pyxcli_args = {}
     for field in fields:
+        if not fields[field]:
+            continue
         if field in AVAILABLE_PYXCLI_FIELDS and fields[field] != '':
             pyxcli_args[field] = fields[field]
     return pyxcli_args
