a96bd22385d0eee26bf6d307112b88e398b48fa5
Alex IP <47459973+AlexImray@users.noreply.github.com>
2019-05-13 15:21:21 -0400

    Update copy.py
    <!--- Updated the description of the 'dest' parameter to specify that a file will be created if path is invalid if 'content' parameter is used instead of 'src' parameter -->
    
    +label: docsite_pr

--- lib/ansible/modules/files/copy.py
+++ lib/ansible/modules/files/copy.py
@@ -46,6 +46,7 @@ options:
     - If C(dest) is a non-existent path and if either C(dest) ends with "/" or C(src) is a directory, C(dest) is created.
     - If I(dest) is a relative path, the starting directory is determined by the remote host.
     - If C(src) and C(dest) are files, the parent directory of C(dest) is not created and the task fails if it does not already exist.
+    - If C(content) is used and C(dest) is a file, file is created if it does not already exist
     type: path
     required: yes
   backup:
