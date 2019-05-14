25a11a006fea37668f95deaf5ea53647b0e268e0
Jill Rouleau <jill.rouleau@bespokess.com>
2019-05-14 14:45:25 -0700

    Merge vmware.py w/ vcenter.py test provider

--- test/runner/lib/cloud/vcenter.py
+++ test/runner/lib/cloud/vcenter.py
@@ -215,7 +215,7 @@ class VcenterProvider(CloudProvider):
         self.endpoint = parser.get('DEFAULT', 'vcenter_hostname')
         self.port = parser.get('DEFAULT', 'vcenter_port')
 
-        if parser.get('DEFAULT', 'vmware_validate_certs') == 'no':
+        if parser.get('DEFAULT', 'vmware_validate_certs').lower() in ('no', 'false'):
             self.insecure = True
 
         self._wait_for_service()
