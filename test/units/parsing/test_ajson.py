1405df2569d48e32607a7859c6b94975b8c94a94
Andrey Klychkov <aaklychkov@mail.ru>
2019-05-14 14:06:14 +0300

    lib/ansible/parsing/ajson.py: added unit tests

--- test/units/parsing/test_ajson.py
+++ test/units/parsing/test_ajson.py
@@ -1,4 +1,5 @@
 # Copyright 2018, Matt Martz <matt@sivel.net>
+# Copyright 2019, Andrew Klychkov @Andersson007 <aaklychkov@mail.ru>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
@@ -9,7 +10,9 @@ import json
 
 import pytest
 
-from ansible.parsing.ajson import AnsibleJSONDecoder
+from datetime import date, datetime
+
+from ansible.parsing.ajson import AnsibleJSONEncoder, AnsibleJSONDecoder
 from ansible.parsing.yaml.objects import AnsibleVaultEncryptedUnicode
 
 
@@ -20,3 +23,22 @@ def test_AnsibleJSONDecoder_vault():
     assert isinstance(data['password'], AnsibleVaultEncryptedUnicode)
     assert isinstance(data['bar']['baz'][0]['password'], AnsibleVaultEncryptedUnicode)
     assert isinstance(data['foo']['password'], AnsibleVaultEncryptedUnicode)
+
+
+class TestAnsibleJSONEncoder():
+
+    @pytest.fixture
+    def ansible_json_encoder(self):
+        yield AnsibleJSONEncoder()
+
+    @pytest.mark.parametrize(
+        'test_input,expected',
+        [
+            (datetime(2019, 5, 14, 13, 39, 38, 569047), '2019-05-14T13:39:38.569047'),
+            (datetime(2019, 5, 14, 13, 47, 16, 923866), '2019-05-14T13:47:16.923866'),
+            (date(2019, 5, 14), '2019-05-14'),
+            (date(2020, 5, 14), '2020-05-14'),
+        ]
+    )
+    def test_date_datetime(self, ansible_json_encoder, test_input, expected):
+        assert(ansible_json_encoder.default(test_input) == expected)
