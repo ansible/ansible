986984ff84ddf49f38366c7499c34d68a77e3c59
Matt Clay <matt@mystile.com>
2019-05-14 10:39:44 -0700

    Remove old references to `cloud-config-aws.yml`.
    References to the new `cloud-config-aws.ini` file are provided by `"$@"` in tests.

--- test/integration/targets/efs/runme.sh
+++ test/integration/targets/efs/runme.sh
@@ -12,9 +12,9 @@ export ANSIBLE_ROLES_PATH=../
 virtualenv --system-site-packages --python "${PYTHON}" "${MYTMPDIR}/botocore-less-than-1.10.57"
 source "${MYTMPDIR}/botocore-less-than-1.10.57/bin/activate"
 "${PYTHON}" -m pip install 'botocore<1.10.57' boto3
-ansible-playbook -i ../../inventory -e @../../integration_config.yml -e @../../cloud-config-aws.yml -v playbooks/version_fail.yml "$@"
+ansible-playbook -i ../../inventory -e @../../integration_config.yml -v playbooks/version_fail.yml "$@"
 # Run full test suite
 virtualenv --system-site-packages --python "${PYTHON}" "${MYTMPDIR}/botocore-recent"
 source "${MYTMPDIR}/botocore-recent/bin/activate"
 $PYTHON -m pip install 'botocore>=1.10.57' boto3
-ansible-playbook -i ../../inventory -e @../../integration_config.yml -e @../../cloud-config-aws.yml -v playbooks/full_test.yml "$@"
+ansible-playbook -i ../../inventory -e @../../integration_config.yml -v playbooks/full_test.yml "$@"
