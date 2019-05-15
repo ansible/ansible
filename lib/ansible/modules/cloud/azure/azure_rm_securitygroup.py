0b43ffd9fcd971b4af93e446a9ffeaadde35feb3
James E. King III <jking@apache.org>
2019-05-14 21:30:23 -0400

    azure_rm_securitygroup - fix delete error handling (#56422)

--- lib/ansible/modules/cloud/azure/azure_rm_securitygroup.py
+++ lib/ansible/modules/cloud/azure/azure_rm_securitygroup.py
@@ -741,7 +741,8 @@ class AzureRMSecurityGroup(AzureRMModuleBase):
             poller = self.network_client.network_security_groups.delete(resource_group_name=self.resource_group, network_security_group_name=self.name)
             result = self.get_poller_result(poller)
         except CloudError as exc:
-            raise Exception("Error deleting security group {0} - {1}".format(self.name, str(exc)))
+            self.fail("Error deleting security group {0} - {1}".format(self.name, str(exc)))
+
         return result
 
     def convert_asg_to_id(self, rule):
