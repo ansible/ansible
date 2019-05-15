312fd75d77ebd77662c32cb3d5db2ed22d736311
rnsc <github@rnsc.be>
2019-05-15 12:45:57 +0200

    ansible_metadata shouldn't be changed...

--- lib/ansible/modules/windows/win_service.py
+++ lib/ansible/modules/windows/win_service.py
@@ -4,7 +4,7 @@
 # Copyright: (c) 2014, Chris Hoffman <choffman@chathamfinancial.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-ANSIBLE_METADATA = {'metadata_version': '1.2',
+ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['stableinterface'],
                     'supported_by': 'core'}
 
