462e7613cbe41722604f97d5f2a14ee8f392585c
Brian Coca <brian.coca+git@gmail.com>
2019-05-15 12:05:52 -0400

    add more tests

--- test/integration/targets/var_templating/runme.sh
+++ test/integration/targets/var_templating/runme.sh
@@ -4,3 +4,9 @@ set -eux
 
 # this should succeed since we override the undefined variable
 ansible-playbook undefined.yml -i inventory -v "$@" -e '{"mytest": False}'
+
+# this should still work, just show that var is undefined in debug
+ansible-playbook undefined.yml -i inventory -v "$@"
+
+# this should work since we dont use the variable
+ansible-playbook undall.yml -i inventory -v "$@"
