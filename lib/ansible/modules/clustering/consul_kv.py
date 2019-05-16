d58a3eb883ab89ef7d7c4061d80254b15855d4fe
Pilou <pierre-louis.bonicoli@libregerbil.fr>
2019-05-16 18:54:05 +0000

    consul modules: update documentation (#56408)
    * consul modules: Python 2.6 is always required on managed node, document all types, improve parameter descriptions, fix typos
    * consul_kv: add doc for retrieve parameter

--- lib/ansible/modules/clustering/consul_kv.py
+++ lib/ansible/modules/clustering/consul_kv.py
@@ -19,12 +19,11 @@ short_description: Manipulate entries in the key/value store of a consul cluster
 description:
   - Allows the retrieval, addition, modification and deletion of key/value entries in a
     consul cluster via the agent. The entire contents of the record, including
-    the indices, flags and session are returned as 'value'.
-  - If the key represents a prefix then Note that when a value is removed, the existing
+    the indices, flags and session are returned as C(value).
+  - If the C(key) represents a prefix then note that when a value is removed, the existing
     value if any is returned as part of the results.
   - See http://www.consul.io/docs/agent/http.html#kv for more details.
 requirements:
-  - python >= 2.6
   - python-consul
   - requests
 version_added: "2.0"
@@ -49,11 +48,13 @@ options:
     key:
         description:
           - The key at which the value should be stored.
+        type: str
         required: yes
     value:
         description:
           - The value should be associated with the given key, required if C(state)
             is C(present).
+        type: str
         required: yes
     recurse:
         description:
@@ -61,34 +62,47 @@ options:
             retrieved by setting this to C(yes).
         type: bool
         default: 'no'
+    retrieve:
+        description:
+            - If the I(state) is C(present) and I(value) is set, perform a
+              read after setting the value and return this value.
+        default: True
+        type: bool
     session:
         description:
           - The session that should be used to acquire or release a lock
             associated with a key/value pair.
+        type: str
     token:
         description:
           - The token key indentifying an ACL rule set that controls access to
             the key value pair
+        type: str
     cas:
         description:
           - Used when acquiring a lock with a session. If the C(cas) is C(0), then
             Consul will only put the key if it does not already exist. If the
             C(cas) value is non-zero, then the key is only set if the index matches
             the ModifyIndex of that key.
+        type: str
     flags:
         description:
-          - Opaque integer value that can be passed when setting a value.
+          - Opaque positive integer value that can be passed when setting a value.
+        type: str
     host:
         description:
           - Host of the consul agent.
+        type: str
         default: localhost
     port:
         description:
           - The port on which the consul agent is running.
+        type: int
         default: 8500
     scheme:
         description:
           - The protocol scheme on which the consul agent is running.
+        type: str
         default: http
         version_added: "2.1"
     validate_certs:
