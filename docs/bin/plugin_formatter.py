7de7cc138d7b117f05332341f380cb81d171da33
Sandra McCann <samccann@redhat.com>
2019-05-16 21:31:02 -0400

    move too old version to 2.3 (#56540)

--- docs/bin/plugin_formatter.py
+++ docs/bin/plugin_formatter.py
@@ -52,7 +52,7 @@ from ansible.utils._build_helpers import update_file_if_different
 
 # if a module is added in a version of Ansible older than this, don't print the version added information
 # in the module documentation because everyone is assumed to be running something newer than this already.
-TOO_OLD_TO_BE_NOTABLE = 2.0
+TOO_OLD_TO_BE_NOTABLE = 2.3
 
 # Get parent directory of the directory this script lives in
 MODULEDIR = os.path.abspath(os.path.join(
