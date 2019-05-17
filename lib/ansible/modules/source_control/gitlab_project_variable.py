33e48a13832f789f6b58f8b88c9fb240eb5dc5b9
Markus Bergholz <markus.bergholz@lekker.de>
2019-05-17 22:01:04 +0200

    adding the at style to username

--- lib/ansible/modules/source_control/gitlab_project_variable.py
+++ lib/ansible/modules/source_control/gitlab_project_variable.py
@@ -19,7 +19,7 @@ description:
     they stay untouched (purged_vars = False) or will be deleted (purged_vars = True).
 version_added: "2.9"
 author:
-    - "Markus Bergholz (markuman)"
+    - "Markus Bergholz (@markuman)"
 requirements:
   - python >= 2.7
   - python-gitlab python module
