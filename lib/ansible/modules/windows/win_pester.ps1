256bf224a2d4f99a79fede6fe869b3120e79a9bc
Coby Caldwell <cobycaldwell@gmail.com>
2019-05-20 11:54:54 -0700

    convert from legacy utils to fix mem issues on Json conversion

--- lib/ansible/modules/windows/win_pester.ps1
+++ lib/ansible/modules/windows/win_pester.ps1
@@ -3,64 +3,58 @@
 # Copyright: (c) 2017, Erwan Quelin (@equelin) <erwan.quelin@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-#Requires -Module Ansible.ModuleUtils.Legacy
+#AnsibleRequires -CSharpUtil Ansible.Basic
 
-$ErrorActionPreference = 'Stop'
-
-$params = Parse-Args -arguments $args -supports_check_mode $true
-$check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
-$diff_mode = Get-AnsibleParam -obj $params -name "_ansible_diff" -type "bool" -default $false
-
-# Modules parameters
-
-$path = Get-AnsibleParam -obj $params -name "path" -type "str" -failifempty $true
-$minimum_version = Get-AnsibleParam -obj $params -name "minimum_version" -type "str" -failifempty $false
-
-$result = @{
-    changed = $false
+$spec = @{
+    options = @{
+        path = @{ type = "str"; required = $true }
+        minimum_version = @{ type = "str" }
+    }
+    supports_check_mode = $true
 }
 
-if ($diff_mode) {
-    $result.diff = @{}
-}
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
 
-# CODE
+$path = $module.Params.path
+$minimum_version = $module.Params.minimum_version
+
 # Test if parameter $version is valid
-Try {
-    $minimum_version = [version]$minimum_version
-}
-Catch {
-    Fail-Json -obj $result -message "Value '$minimum_version' for parameter 'minimum_version' is not a valid version format"
+If ($minimum_version) {
+    Try {
+        $minimum_version = [version]$minimum_version
+    }
+    Catch {
+        $module.FailJson("Value '$minimum_version' for parameter 'minimum_version' is not a valid version format")
+    }
 }
 
 # Import Pester module if available
-$Module = 'Pester'
+$PesterModule = 'Pester'
 
-If (-not (Get-Module -Name $Module -ErrorAction SilentlyContinue)) {
-    If (Get-Module -Name $Module -ListAvailable -ErrorAction SilentlyContinue) {
-        Import-Module $Module
+If (-not (Get-Module -Name $PesterModule -ErrorAction SilentlyContinue)) {
+    If (Get-Module -Name $PesterModule -ListAvailable -ErrorAction SilentlyContinue) {
+        Import-Module $PesterModule
     } else {
-        Fail-Json -obj $result -message "Cannot find module: $Module. Check if pester is installed, and if it is not, install using win_psmodule or win_chocolatey."
+        $module.FailJson("Cannot find module: $PesterModule. Check if pester is installed, and if it is not, install using win_psmodule or win_chocolatey.")
     }
 }
 
 # Add actual pester's module version in the ansible's result variable
-$Pester_version = (Get-Module -Name $Module).Version.ToString()
-$result.pester_version = $Pester_version
+$module.result.pester_version = $( (Get-Module -Name $PesterModule).Version.ToString() )
 
 # Test if the Pester module is available with a version greater or equal than the one specified in the $version parameter
-If ((-not (Get-Module -Name $Module -ErrorAction SilentlyContinue | Where-Object {$_.Version -ge $minimum_version})) -and ($minimum_version)) {
-    Fail-Json -obj $result -message "$Module version is not greater or equal to $minimum_version"
+If (-not (Get-Module -Name $PesterModule -ErrorAction SilentlyContinue | Where-Object {$_.Version -ge $minimum_version}) ) {
+    $module.FailJson("$PesterModule version is not greater or equal to $minimum_version")
 }
 
 # Testing if test file or directory exist
 If (-not (Test-Path -LiteralPath $path)) {
-    Fail-Json -obj $result -message "Cannot find file or directory: '$path' as it does not exist"
+    $module.FailJson("Cannot find file or directory: '$path' as it does not exist")
 }
 
 #Prepare Invoke-Pester parameters depending of the Pester's version.
 #Invoke-Pester output deactivation behave differently depending on the Pester's version
-If ($result.pester_version -ge "4.0.0") {
+If ($module.result.pester_version -ge "4.0.0") {
     $Parameters = @{
         "show" = "none"
         "PassThru" = $True
@@ -73,20 +67,20 @@ If ($result.pester_version -ge "4.0.0") {
 }
 
 # Run Pester tests
-if ($check_mode) {
-    if (Test-Path -LiteralPath $path -PathType Leaf) {
-        $result.output = "Run pester test in the file: $path"
+If ($check_mode) {
+    If (Test-Path -LiteralPath $path -PathType Leaf) {
+        $module.result.output = "Run pester test in the file: $path"
     } elseif (Test-Path -LiteralPath $path -PathType Container) {
-        $result.output = "Run pester test(s) who are in the directory: $path"
+        $module.result.output = "Run pester test(s) who are in the directory: $path"
     }
 } else {
     try {
-        $result.output = Invoke-Pester $path @Parameters
+        $module.result.output = Invoke-Pester $path @Parameters
     } catch {
-        Fail-Json -obj $result -message $_.Exception
+        $module.FailJson($_.Exception)
     }
 }
 
-$result.changed = $true
+$module.result.changed = $true
 
-Exit-Json -obj $result
+$module.ExitJson()
