961040f069d5d608d98c1fa688dc29d2a939feef
Eric Anderson <eanderson@avinetworks.com>
2019-05-21 16:20:28 -0700

    update filter header

--- lib/ansible/plugins/filter/gcp_kms_filters.py
+++ lib/ansible/plugins/filter/gcp_kms_filters.py
@@ -1,5 +1,4 @@
-#!/usr/bin/pyhton
-# Copyright (c) 2019 Avi Networks
+# (c) 2019, Eric Anderson <eric.sysmin@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 # Usage:
