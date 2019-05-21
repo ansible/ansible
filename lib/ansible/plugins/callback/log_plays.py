23e5786694213d6601e8d4573c4fa34627157540
Paddy Newman <paddynewman@gmail.com>
2019-05-21 13:17:40 +0000

    Add a new "log_folder" option to the log_plays callback plugin.
    A a new option for the log_plays callback plugin which allows the user to
    control where the callback where creates log files.
    
    The option can be set via the ANSIBLE_LOG_FOLDER environment variable or in the
    Ansible configuration file, e.g.:
    
    [callback_log_plays]
    log_folder = /path/to/my/log/folder

--- lib/ansible/plugins/callback/log_plays.py
+++ lib/ansible/plugins/callback/log_plays.py
@@ -16,6 +16,15 @@ DOCUMENTATION = '''
     requirements:
      - Whitelist in configuration
      - A writeable /var/log/ansible/hosts directory by the user executing Ansible on the controller
+    options:
+      log_folder:
+        default: /var/log/ansible/hosts
+        description: The folder where log files will be created.
+        env:
+          - name: ANSIBLE_LOG_FOLDER
+        ini:
+          - section: callback_log_plays
+            key: log_folder
 '''
 
 import os
@@ -51,8 +60,13 @@ class CallbackModule(CallbackBase):
 
         super(CallbackModule, self).__init__()
 
-        if not os.path.exists("/var/log/ansible/hosts"):
-            os.makedirs("/var/log/ansible/hosts")
+    def set_options(self, task_keys=None, var_options=None, direct=None):
+        super(CallbackModule, self).set_options(task_keys=task_keys, var_options=var_options, direct=direct)
+
+        self.log_folder = self.get_option("log_folder")
+
+        if not os.path.exists(self.log_folder):
+            os.makedirs(self.log_folder)
 
     def log(self, host, category, data):
         if isinstance(data, MutableMapping):
@@ -66,7 +80,7 @@ class CallbackModule(CallbackBase):
                 if invocation is not None:
                     data = json.dumps(invocation) + " => %s " % data
 
-        path = os.path.join("/var/log/ansible/hosts", host)
+        path = os.path.join(self.log_folder, host)
         now = time.strftime(self.TIME_FORMAT, time.localtime())
 
         msg = to_bytes(self.MSG_FORMAT % dict(now=now, category=category, data=data))
