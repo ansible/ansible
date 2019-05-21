ceec8d867166816317397b460c0152f1cecb7355
Andrey Tuzhilin <andrey@3adigital.ru>
2019-05-21 17:20:55 +0300

    docs(saslprep): changed license header to GPLv3

--- lib/ansible/module_utils/saslprep.py
+++ lib/ansible/module_utils/saslprep.py
@@ -1,16 +1,5 @@
-# Copyright 2016-present MongoDB, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright (c) 2019 Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 """An implementation of RFC4013 SASLprep.
 This code is mostly (apart from utils methods) a copy of
