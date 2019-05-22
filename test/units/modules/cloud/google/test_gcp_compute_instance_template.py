4223c4167112e5e6d2da070aeb1f9fc59181239d
Zack Webb <Zachary.Webb@ca.com>
2019-05-22 09:08:47 -0600

    fix linting errors and python 2.6 unittest incompatibility

--- test/units/modules/cloud/google/test_gcp_compute_instance_template.py
+++ test/units/modules/cloud/google/test_gcp_compute_instance_template.py
@@ -3,7 +3,8 @@ import copy
 
 from ansible.modules.cloud.google.gcp_compute_instance_template import encode_request, decode_response
 
-class TestTestGCPComputeInstanceTemplate(unittest.TestCase):
+
+class TestGCPComputeInstanceTemplate(unittest.TestCase):
     """Unit tests for gcp_compute_instance_template module."""
     request_without_metadata = {
         u'kind': 'compute#instanceTemplate',
@@ -18,7 +19,7 @@ class TestTestGCPComputeInstanceTemplate(unittest.TestCase):
         u'description': 'instance template description',
         u'name': 'my-instance-template',
         u'properties': {
-            u'machineType': 'n1-standard-1', 
+            u'machineType': 'n1-standard-1',
             u'metadata': {
                 'startup-script': '#!/bin/bash\necho hello',
                 'ssh-keys': 'not-an-ssh-key'
@@ -26,39 +27,39 @@ class TestTestGCPComputeInstanceTemplate(unittest.TestCase):
         }
     }
     response_without_metadata = {
-        u'kind': u'compute#instanceTemplate', 
+        u'kind': u'compute#instanceTemplate',
         u'description': 'instance template description',
         u'properties': {
-            u'machineType': u'n1-standard-1', 
+            u'machineType': u'n1-standard-1',
             u'metadata': {
-                u'kind': u'compute#metadata', 
+                u'kind': u'compute#metadata',
                 u'fingerprint': u'ZmluZ2VycHJpbnQK'
             }
-        }, 
-        u'creationTimestamp': u'2019-05-22T06:58:43.884-07:00', 
-        u'id': u'0000000000000000001', 
-        u'selfLink': u'https://www.googleapis.com/compute/v1/projects/my-fake-gcp-project/global/instanceTemplates/my-instance-template', 
+        },
+        u'creationTimestamp': u'2019-05-22T06:58:43.884-07:00',
+        u'id': u'0000000000000000001',
+        u'selfLink': u'https://www.googleapis.com/compute/v1/projects/my-fake-gcp-project/global/instanceTemplates/my-instance-template',
         u'name': u'my-instance-template'
     }
     response_with_metadata = {
-        u'kind': u'compute#instanceTemplate', 
+        u'kind': u'compute#instanceTemplate',
         u'description': 'instance template description',
         u'properties': {
-            u'machineType': u'n1-standard-1', 
+            u'machineType': u'n1-standard-1',
             u'metadata': {
                 u'items': [
                     {
-                        u'value': u'#!/bin/bash\necho hello', 
+                        u'value': u'#!/bin/bash\necho hello',
                         u'key': u'startup-script'
                     }
-                ], 
-                u'kind': u'compute#metadata', 
+                ],
+                u'kind': u'compute#metadata',
                 u'fingerprint': u'ZmluZ2VycHJpbnQK'
             }
-        }, 
-        u'creationTimestamp': u'2019-05-22T06:58:43.884-07:00', 
-        u'id': u'0000000000000000001', 
-        u'selfLink': u'https://www.googleapis.com/compute/v1/projects/my-fake-gcp-project/global/instanceTemplates/my-instance-template', 
+        },
+        u'creationTimestamp': u'2019-05-22T06:58:43.884-07:00',
+        u'id': u'0000000000000000001',
+        u'selfLink': u'https://www.googleapis.com/compute/v1/projects/my-fake-gcp-project/global/instanceTemplates/my-instance-template',
         u'name': u'my-instance-template'
     }
 
@@ -75,14 +76,11 @@ class TestTestGCPComputeInstanceTemplate(unittest.TestCase):
             self.request_with_metadata
         )
 
-        self.assertIn('properties', encoded_request_with_metadata)
-        self.assertIn('metadata', encoded_request_with_metadata['properties'])
-        self.assertIn('items', encoded_request_with_metadata['properties']['metadata'])
+        self.assertTrue('properties' in encoded_request_with_metadata)
+        self.assertTrue('metadata' in encoded_request_with_metadata['properties'])
+        self.assertTrue('items' in encoded_request_with_metadata['properties']['metadata'])
         for metadata_key, metadata_value in self.request_with_metadata['properties']['metadata'].items():
-            self.assertIn(
-                { 'key': metadata_key, 'value': metadata_value },
-                encoded_request_with_metadata['properties']['metadata']['items']
-            )
+            self.assertTrue({'key': metadata_key, 'value': metadata_value} in encoded_request_with_metadata['properties']['metadata']['items'])
 
     def test_decode_request(self):
         self.maxDiff = None
@@ -108,15 +106,11 @@ class TestTestGCPComputeInstanceTemplate(unittest.TestCase):
             response_with_metadata_cleared
         )
 
-        self.assertIn('properties', decoded_response_with_metadata)
-        self.assertIn('metadata', decoded_response_with_metadata['properties'])
+        self.assertTrue('properties' in decoded_response_with_metadata)
+        self.assertTrue('metadata' in decoded_response_with_metadata['properties'])
         for metadata_object in self.response_with_metadata['properties']['metadata']['items']:
-            self.assertIn(
-                metadata_object['key'], 
-                decoded_response_with_metadata['properties']['metadata']
-            )
+            self.assertTrue(metadata_object['key'] in decoded_response_with_metadata['properties']['metadata'])
             self.assertEqual(
                 metadata_object['value'],
                 decoded_response_with_metadata['properties']['metadata'][metadata_object['key']]
             )
-
