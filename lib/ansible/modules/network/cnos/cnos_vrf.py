913ea616332810d73db9277915581c4fcb35a866
Anil Kumar Muraleedharan <amuraleedhar@lenovo.com>
2019-05-23 02:12:36 +0530

    Merge issue rectified

--- lib/ansible/modules/network/cnos/cnos_vrf.py
+++ lib/ansible/modules/network/cnos/cnos_vrf.py
@@ -249,27 +249,7 @@ def map_config_to_obj(module):
                 objs.append(obj)
     else:
         module.fail_json(msg='Could not fetch VRF details from device')
-    vrfText = output[0].strip()
-    vrfList = vrfText.split('VRF')
-    for vrfItem in vrfList:
-        if 'FIB ID' in vrfItem:
-            obj = dict()
-            list_of_words = vrfItem.split()
-            vrfName = list_of_words[0]
-            obj['name'] = vrfName[:-1]
-            obj['rd'] = list_of_words[list_of_words.index('RD') + 1]
-            start = False
-            obj['interfaces'] = []
-            for intName in list_of_words:
-                if 'Interfaces' in intName:
-                    start = True
-                if start is True:
-                    if '!' not in intName and 'Interfaces' not in intName:
-                        obj['interfaces'].append(intName.strip().lower())
-            objs.append(obj)
-
     return objs
-
 
 def map_params_to_obj(module):
     obj = []
