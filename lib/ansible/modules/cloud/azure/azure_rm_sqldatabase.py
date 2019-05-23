8865bbc0fb2e6918b825905b2e96fd12d8fd8cc5
FanQiu <fanqiu@microsoft.com>
2019-05-23 10:40:06 +0800

    delete pdb

--- lib/ansible/modules/cloud/azure/azure_rm_sqldatabase.py
+++ lib/ansible/modules/cloud/azure/azure_rm_sqldatabase.py
@@ -195,7 +195,6 @@ status:
 '''
 
 import time
-import pdb
 from ansible.module_utils.azure_rm_common import AzureRMModuleBase, format_resource_id
 
 try:
@@ -438,7 +437,6 @@ class AzureRMSqlDatabase(AzureRMModuleBase):
         self.log("Creating / Updating the SQL Database instance {0}".format(self.name))
 
         try:
-            pdb.set_trace()
             response = self.sql_client.databases.create_or_update(resource_group_name=self.resource_group,
                                                                   server_name=self.server_name,
                                                                   database_name=self.name,
