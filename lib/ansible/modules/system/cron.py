1c4d2dbc7cb37ec845e7515662efc41ca2181655
Julien Palard <julien@palard.fr>
2019-05-23 10:54:30 +0200

    docs: cron: fix for example raises a warning. (#56701)
    The following one:
    
        The value 2 (type int) in a string field was converted to '2'

--- lib/ansible/modules/system/cron.py
+++ lib/ansible/modules/system/cron.py
@@ -185,9 +185,9 @@ EXAMPLES = r'''
 - name: Creates a cron file under /etc/cron.d
   cron:
     name: yum autoupdate
-    weekday: 2
-    minute: 0
-    hour: 12
+    weekday: "2"
+    minute: "0"
+    hour: "12"
     user: root
     job: "YUMINTERACTIVE=0 /usr/sbin/yum-autoupdate"
     cron_file: ansible_yum-autoupdate
