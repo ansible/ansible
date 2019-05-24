692ba9224d9db1a9bcd8a975c4acfdf679720b77
Piotr Gbyliczek <piotr-gbyliczek@users.noreply.github.com>
2019-05-24 18:20:12 +0100

    Apply suggestions from code review
    Co-Authored-By: Zim Kalinowski <zikalino@microsoft.com>

--- lib/ansible/modules/cloud/azure/azure_rm_appgateway.py
+++ lib/ansible/modules/cloud/azure/azure_rm_appgateway.py
@@ -294,7 +294,7 @@ options:
             authentication_certificates:
                 description:
                     - List of references to application gateway authentication certificates.
-                    - Applicable only when cookie_based_affinity is enabled, otherwise quietly ignored.
+                    - Applicable only when C(cookie_based_affinity) is enabled, otherwise quietly ignored.
                 suboptions:
                     id:
                         description:
