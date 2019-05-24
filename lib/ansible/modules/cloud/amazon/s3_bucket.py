7dcd185aaa3bad7ad3cb8df1d9b4e4430554c077
Stefan Horning <stefan@hornings.de>
2019-05-24 17:12:07 +0200

    Bigger sleep for versioning apply, as it causes tests to fail

--- lib/ansible/modules/cloud/amazon/s3_bucket.py
+++ lib/ansible/modules/cloud/amazon/s3_bucket.py
@@ -496,7 +496,7 @@ def wait_versioning_is_applied(module, s3_client, bucket_name, required_versioni
         except (BotoCoreError, ClientError) as e:
             module.fail_json_aws(e, msg="Failed to get updated versioning for bucket")
         if versioning_status.get('Status') != required_versioning:
-            time.sleep(5)
+            time.sleep(8)
         else:
             return versioning_status
     module.fail_json(msg="Bucket versioning failed to apply in the expected time")
