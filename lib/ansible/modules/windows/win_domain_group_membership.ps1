5910f6b9f5e40d5995d36b12bb12d34d9254dc20
Aravind Balaji <40019860+AravindBalajiS@users.noreply.github.com>
2019-05-25 18:53:08 +0530

    Update win_domain_group_membership.ps1
    Bug fixes while Adding and Deleting a group from another group

--- lib/ansible/modules/windows/win_domain_group_membership.ps1
+++ lib/ansible/modules/windows/win_domain_group_membership.ps1
@@ -69,7 +69,7 @@ foreach ($member in $members) {
         }
     }
 
-    if ($state -in @("present", "pure") -and !$user_in_group) {       
+    if ($state -in @("present", "pure") -and !$user_in_group) {
         try {
             Add-ADGroupMember -Identity $name -Members $group_member -WhatIf:$check_mode @extra_args
         } catch {
