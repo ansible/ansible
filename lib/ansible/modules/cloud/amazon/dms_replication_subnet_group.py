ba4f9a4c39adb1389189c8cc83df0d6f17f037a9
Rui  Moreira <ruimoreira29@gmail.com>
2019-05-26 21:53:38 +0100

    addressing suggestions by @felixfontein

--- lib/ansible/modules/cloud/amazon/dms_replication_subnet_group.py
+++ lib/ansible/modules/cloud/amazon/dms_replication_subnet_group.py
@@ -12,7 +12,7 @@ DOCUMENTATION = '''
 module: dms_replication_subnet_group
 short_description: creates or destroys a data migration services subnet group
 description:
-    - creates or destroys a data migration services subnet group
+    - Creates or destroys a data migration services subnet group
 version_added: "2.9"
 options:
     state:
@@ -87,13 +87,13 @@ def replication_subnet_group_modify(connection, **modify_params):
 
 
 @AWSRetry.backoff(**backoff_params)
-def replication_subnet_group_delete(connection):
+def replication_subnet_group_delete(module, connection):
     subnetid = module.params.get('identifier')
     delete_parameters = dict(ReplicationSubnetGroupIdentifier=subnetid)
     return connection.delete_replication_subnet_group(**delete_parameters)
 
 
-def get_dms_client(aws_connect_params, client_region, ec2_url):
+def get_dms_client(module, aws_connect_params, client_region, ec2_url):
     client_params = dict(
         module=module,
         conn_type='client',
@@ -113,7 +113,7 @@ def replication_subnet_exists(subnet):
     return bool(len(subnet['ReplicationSubnetGroups']))
 
 
-def create_module_params():
+def create_module_params(module):
     """
     Reads the module parameters and returns a dict
     :return: dict
@@ -136,7 +136,7 @@ def compare_params(param_described):
     a DMS endpoint does not return the value for
     the password for security reasons ( I assume )
     """
-    modparams = create_module_params()
+    modparams = create_module_params(module)
     changed = False
     # need to sanitize values that get retured from the API
     if 'VpcId' in param_described.keys():
@@ -159,9 +159,9 @@ def compare_params(param_described):
     return changed
 
 
-def create_replication_subnet_group(connection):
+def create_replication_subnet_group(module, connection):
     try:
-        params = create_module_params()
+        params = create_module_params(module)
         return replication_subnet_group_create(connection, **params)
     except botocore.exceptions.ClientError as e:
         module.fail_json(msg="Failed to create DMS replication subnet group.",
@@ -172,9 +172,9 @@ def create_replication_subnet_group(connection):
                          exception=traceback.format_exc())
 
 
-def modify_replication_subnet_group(connection):
+def modify_replication_subnet_group(module, connection):
     try:
-        modify_params = create_module_params()
+        modify_params = create_module_params(module)
         return replication_subnet_group_modify(connection, **modify_params)
     except botocore.exceptions.ClientError as e:
         module.fail_json(msg="Failed to Modify the DMS replication subnet group.",
@@ -185,18 +185,14 @@ def modify_replication_subnet_group(connection):
                          exception=traceback.format_exc())
 
 
-def delete_replication_subnet_group(connection):
-    return True
-
-
 def main():
     argument_spec = dict(
-        state=dict(choices=['present', 'absent'], default='present'),
-        identifier=dict(required=True),
-        description=dict(required=True),
-        subnetids=dict(type='list', required=True),
+        state=dict(type=str, choices=['present', 'absent'], default='present'),
+        identifier=dict(type=str, required=True),
+        description=dict(type=str, required=True),
+        subnetids=dict(type='list',  elements='str', required=True),
     )
-    global module
+    #global module
     module = AnsibleAWSModule(
         argument_spec=argument_spec,
         supports_check_mode=True
@@ -204,19 +200,20 @@ def main():
     exit_message = None
     changed = False
     if not HAS_BOTO3:
+
         module.fail_json(msg='boto3 required for this module')
 
     state = module.params.get('state')
     aws_config_region, ec2_url, aws_connect_params = \
         get_aws_connection_info(module, boto3=True)
-    dmsclient = get_dms_client(aws_connect_params, aws_config_region, ec2_url)
+    dmsclient = get_dms_client(module, aws_connect_params, aws_config_region, ec2_url)
     subnet_group = describe_subnet_group(dmsclient,
                                          module.params.get('identifier'))
     if state == 'present':
         if replication_subnet_exists(subnet_group):
             if compare_params(subnet_group["ReplicationSubnetGroups"][0]):
                 if not module.check_mode:
-                    exit_message = modify_replication_subnet_group(dmsclient)
+                    exit_message = modify_replication_subnet_group(module, dmsclient)
                 else:
                     exit_message = dmsclient
                 changed = True
@@ -224,16 +221,20 @@ def main():
                 exit_message = "No changes to Subnet group"
         else:
             if not module.check_mode:
-                exit_message = create_replication_subnet_group(dmsclient)
+                exit_message = create_replication_subnet_group(module, dmsclient)
                 changed = True
             else:
                 exit_message = "Check mode enabled"
 
     elif state == 'absent':
         if replication_subnet_exists(subnet_group):
-            replication_subnet_group_delete(dmsclient)
-            changed = True
-            exit_message = "Replication subnet group Deleted"
+            if not module.check_mode:
+                replication_subnet_group_delete(module, dmsclient)
+                changed = True
+                exit_message = "Replication subnet group Deleted"
+            else:
+                exit_message = dmsclient
+                changed = True
 
         else:
             changed = False
