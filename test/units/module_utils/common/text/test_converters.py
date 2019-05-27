284f547452d286c50965688b245d9c0758d5f229
Andrey Klychkov <aaklychkov@mail.ru>
2019-05-27 08:42:27 +0300

    unit tests for lib/ansible/module_utils/common/text/converters.py: fixes

--- test/units/module_utils/common/text/test_converters.py
+++ test/units/module_utils/common/text/test_converters.py
@@ -10,7 +10,6 @@ import pytest
 from datetime import datetime
 from pytz import timezone as tz
 
-from ansible.module_utils.common._collections_compat import Set
 from ansible.module_utils.common.text.converters import (
     _json_encode_fallback,
     container_to_bytes,
@@ -25,36 +24,6 @@ DEFAULT_ENCODING = 'utf-8'
 class TestJsonEncodeFallback:
 
     """Namespace for testing _json_encode_fallback."""
-
-    @pytest.fixture(scope='class')
-    def _set(self, request):
-        """
-        Returns object of mock Set class.
-
-        The object is used for testing handling of collections.Set objects
-        in _json_encode_fallback().
-        """
-
-        class S(Set):
-
-            """Mock Set class."""
-
-            def __init__(self, iterable):
-                self.elements = lst = []
-                for value in iterable:
-                    if value not in lst:
-                        lst.append(value)
-
-            def __iter__(self):
-                return iter(self.elements)
-
-            def __contains__(self, value):
-                return value in self.elements
-
-            def __len__(self):
-                return len(self.elements)
-
-        return S(request.param)
 
     @pytest.mark.parametrize(
         'test_input,expected',
@@ -72,30 +41,15 @@ class TestJsonEncodeFallback:
         assert _json_encode_fallback(test_input) == expected
 
     @pytest.mark.parametrize(
-        '_set,expected',
-        [
-            ([1], [1]),
-            ([1, 2], [1, 2]),
-            (['one'], ['one']),
-            ([1, 'two'], [1, 'two']),
-        ], indirect=['_set'],
-    )
-    def test_collections_Set(self, _set, expected):
-        """
-        Test for passing collections.Set object to _json_encode_fallback().
-        """
-        assert _json_encode_fallback(_set) == expected
-
-    @pytest.mark.parametrize(
         'test_input,expected',
         [
             (set([1]), [1]),
             (set([2]), [2]),
         ]
     )
-    def test_common_set(self, test_input, expected):
+    def test_set(self, test_input, expected):
         """
-        Test for passing common set object to _json_encode_fallback().
+        Test for passing a set object to _json_encode_fallback().
         """
         assert _json_encode_fallback(test_input) == expected
 
@@ -122,7 +76,7 @@ class TestJsonEncodeFallback:
             _json_encode_fallback(test_input)
 
 
-class Test_container_to_bytes:
+class TestContainerToBytes:
 
     """Namespace for testing container_to_bytes()."""
 
@@ -171,7 +125,7 @@ class Test_container_to_bytes:
         assert container_to_bytes(test_input) == expected
 
 
-class Test_container_to_text:
+class TestContainerToText:
 
     """Namespace for testing container_to_text()."""
 
@@ -209,7 +163,7 @@ class Test_container_to_text:
             ('str'.encode(DEFAULT_ENCODING), 'str'),
             (['str'.encode(DEFAULT_ENCODING)], ['str']),
             (('str'.encode(DEFAULT_ENCODING)), ('str')),
-            ({b'str'.encode(DEFAULT_ENCODING): b'str'.encode(DEFAULT_ENCODING)}, {u'str': u'str'}),
+            ({b'str': b'str'}, {u'str': u'str'}),
         ]
     )
     def test_to_text_default_encoding_err(self, test_input, expected):
@@ -220,7 +174,7 @@ class Test_container_to_text:
         assert container_to_text(test_input) == expected
 
 
-class Test_jsonify:
+class TestJsonify:
 
     """Namespace for testing jsonify()."""
 
