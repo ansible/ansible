4472ab8d322cae94d1df70c0bdd95e4e38009339
Klaus Frank <agowa338@users.noreply.github.com>
2019-05-27 07:40:49 +0200

    VMware: Fix python 3 incompatibility (#54123)
    Add Python2 and Python3 compatible `string.translate` for hostname customzation.
    
    Fixes: #54118

--- lib/ansible/modules/cloud/vmware/vmware_guest.py
+++ lib/ansible/modules/cloud/vmware/vmware_guest.py
@@ -1593,7 +1593,8 @@ class PyVmomiHelper(PyVmomi):
             # Setting hostName, orgName and fullName is mandatory, so we set some default when missing
             ident.userData.computerName = vim.vm.customization.FixedName()
             # computer name will be truncated to 15 characters if using VM name
-            default_name = self.params['name'].translate(None, string.punctuation)
+            default_name = self.params['name'].replace(' ', '')
+            default_name = ''.join([c for c in default_name if c not in string.punctuation])
             ident.userData.computerName.name = str(self.params['customization'].get('hostname', default_name[0:15]))
             ident.userData.fullName = str(self.params['customization'].get('fullname', 'Administrator'))
             ident.userData.orgName = str(self.params['customization'].get('orgname', 'ACME'))
