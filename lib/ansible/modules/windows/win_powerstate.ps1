81ab3b345022d80b2f766e9901650ef911a24d30
imjoseangel <josea.munoz@gmail.com>
2019-05-27 20:00:59 +0200

    Adding New PowerState Module

---
+++ lib/ansible/modules/windows/win_powerstate.ps1
@@ -0,0 +1,107 @@
+#!powershell
+
+# Copyright: (c) 2019, Jose Angel Munoz (@imjoseangel) <josea.munoz@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#Requires -Module Ansible.ModuleUtils.Legacy
+
+$ErrorActionPreference = "Stop"
+
+$params = Parse-Args $args -supports_check_mode $true
+$check_mode = Get-AnsibleParam -obj $params -name '_ansible_check_mode' -type 'bool' -default $false
+
+$powerstate = Get-AnsibleParam -obj $params -name "powerstate" -type "str" -default "suspend" -validateset "shutdown", "suspend", "hibernate"
+$force = Get-AnsibleParam -obj $params -name "force" -type "bool" -default $false
+$disablewake = Get-AnsibleParam -obj $params -name "disablewake" -type "bool" -default $true
+
+$result = @{
+    changed     = $false
+    module_args = @{
+        powerstate  = $powerstate
+        force       = $force
+        disablewake = $disablewake
+    }
+}
+
+function Set-PowerState {
+
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param (
+
+        [parameter(Mandatory = $True)]
+        [ValidateSet(“suspend”, ”hibernate”)]$PowerState,
+
+        [parameter(Mandatory = $False)]
+        [switch]$Force,
+
+        [parameter(Mandatory = $False)]
+        [switch]$DisableWake
+    )
+
+    if ((Get-WmiObject -Class Win32_OperatingSystem).Caption -match '\sServer\s') {
+
+        Exit-Json -obj $result -message "Can not change to state $PowerState on Windows Servers"
+    }
+    else {
+
+        if ($PSCmdlet.ShouldProcess("Target", "Operation")) {
+
+            try {
+
+                Add-Type -AssemblyName System.Windows.Forms;
+                [System.Windows.Forms.Application]::SetSuspendState($PowerState, $Force, $DisableWake);
+            }
+            catch [System.InvalidOperationException], [System.Security.SecurityException] {
+
+                Fail-Json $result -message "Could not Add Windows Forms Type"
+            }
+
+            $result.changed = $true
+        }
+        else {
+            try {
+
+                Add-Type -AssemblyName System.Windows.Forms;
+            }
+            catch [System.InvalidOperationException], [System.Security.SecurityException] {
+
+                Fail-Json $result -message "Could not Add Windows Forms Type"
+            }
+
+            $result.changed = $true
+            $result.module_args.check_mode = $true
+        }
+    }
+}
+
+if (($powerstate -eq 'suspend') -or ($powerstate -eq 'hibernate')) {
+
+    try {
+
+        Set-PowerState -PowerState $powerstate -Force:$force -DisableWake:$disablewake -WhatIf:$check_mode
+    }
+    catch [System.InvalidOperationException] {
+
+        Fail-Json -obj $result -message "Could not change to state $powerstate"
+    }
+}
+
+elseif ($powerstate -eq 'shutdown') {
+    try {
+
+        Stop-Computer -Force:$force -WhatIf:$check_mode
+        $result.changed = $true
+
+        if ($check_mode) {
+
+            $result.module_args.check_mode = $true
+        }
+    }
+
+    catch [System.InvalidOperationException] {
+
+        Fail-Json -obj $result -message "Could not change to state $powerstate"
+    }
+}
+
+Exit-Json -obj $result
---
+++ lib/ansible/modules/windows/win_powerstate.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'core'
+}
+
+DOCUMENTATION = r'''
+---
+module: win_powerstate
+short_description: Changes power state of a windows machine
+description:
+- Shutdown, Suspend or Hibernate a Windows machine.
+version_added: '2.9'
+requirements:
+  - This module requires Windows Forms.
+options:
+  powerstate:
+    description:
+    - A PowerState indicating the power activity mode to which to transition.
+    type: str
+    default: suspend
+    choices: [ suspend, hibernate, shutdown ]
+  force:
+    description:
+    - If set to C(true), force the power mode immediately.
+    - If set to C(false), to cause Windows to send a power state change
+      request to every application.
+    type: bool
+    default: true
+  disablewake:
+    description:
+    - If set to C(true), disables restoring the system's power status to
+      active on a wake event.
+    - If set to C(false), enables restoring the system's power status to
+      active on a wake event.
+    type: bool
+    default: true
+notes:
+- If an application does not respond to a suspend request within 20 seconds, Windows determines
+  that it is in a non-responsive state, and that the application can either be put to sleep or
+  terminated. Once an application responds to a suspend request, however, it can take whatever
+  time it needs to clean up resources and shut down active processes.
+- The connection user must have the C(SeRemoteShutdownPrivilege) privilege enabled, see
+  U(https://docs.microsoft.com/en-us/windows/security/threat-protection/security-policy-settings/force-shutdown-from-a-remote-system)
+  for more information.
+- Suspend and hibernate states are not supported on Windows Server.
+seealso:
+- module: win_reboot
+author:
+- Jose Angel Munoz (@imjoseangel)
+'''
+
+EXAMPLES = r'''
+- name: Suspends the machine with all defaults
+  win_powerstate:
+
+- name: Forces the shutdown of a machine
+  win_powerstate:
+    powerstate: shutdown
+    force: true
+
+- name: Disables restoring from hibernated to active on a wake event
+  win_powerstate:
+    powerstate: hibernate
+    disablewake: true
+'''
+
+RETURN = r'''
+changed:
+  description: True if the machine state was changed.
+  returned: always
+  type: bool
+  sample: true
+module_args:
+  description: The arguments passed to the module.
+  returned: always
+  type: dict
+  sample: { "disablewake": true, "force": false, "powerstate": "shutdown"}
+'''
