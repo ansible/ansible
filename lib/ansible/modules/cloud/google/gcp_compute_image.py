381b9faf3d775c3acba18c3f04016e2d8be50d2b
The Magician <magic-modules@google.com>
2019-05-28 10:42:13 -0700

    Bug fixes for GCP modules (#56689)

--- lib/ansible/modules/cloud/google/gcp_compute_image.py
+++ lib/ansible/modules/cloud/google/gcp_compute_image.py
@@ -91,9 +91,8 @@ options:
           For newer Windows images, the server might also populate this property with
           the value WINDOWS to indicate that this is a Windows image. This value is
           purely informational and does not enable or disable any features.
+        - 'Some valid choices include: "VIRTIO_SCSI_MULTIQUEUE"'
         required: false
-        choices:
-        - VIRTIO_SCSI_MULTIQUEUE
   image_encryption_key:
     description:
     - Encrypts the image using a customer-supplied encryption key.
@@ -134,9 +133,8 @@ options:
         - The format used to encode and transmit the block device, which should be
           TAR. This is just a container and transmission format and not a runtime
           format. Provided by the client when the disk image is created.
+        - 'Some valid choices include: "TAR"'
         required: false
-        choices:
-        - TAR
       sha1_checksum:
         description:
         - An optional SHA1 checksum of the disk image before unpackaging.
@@ -179,9 +177,8 @@ options:
     description:
     - The type of the image used to create this disk. The default and only value is
       RAW .
+    - 'Some valid choices include: "RAW"'
     required: false
-    choices:
-    - RAW
 extends_documentation_fragment: gcp
 notes:
 - 'API Reference: U(https://cloud.google.com/compute/docs/reference/v1/images)'
@@ -443,19 +440,16 @@ def main():
             description=dict(type='str'),
             disk_size_gb=dict(type='int'),
             family=dict(type='str'),
-            guest_os_features=dict(type='list', elements='dict', options=dict(type=dict(type='str', choices=['VIRTIO_SCSI_MULTIQUEUE']))),
+            guest_os_features=dict(type='list', elements='dict', options=dict(type=dict(type='str'))),
             image_encryption_key=dict(type='dict', options=dict(raw_key=dict(type='str'))),
             labels=dict(type='dict'),
             licenses=dict(type='list', elements='str'),
             name=dict(required=True, type='str'),
-            raw_disk=dict(
-                type='dict',
-                options=dict(container_type=dict(type='str', choices=['TAR']), sha1_checksum=dict(type='str'), source=dict(required=True, type='str')),
-            ),
+            raw_disk=dict(type='dict', options=dict(container_type=dict(type='str'), sha1_checksum=dict(type='str'), source=dict(required=True, type='str'))),
             source_disk=dict(type='dict'),
             source_disk_encryption_key=dict(type='dict', options=dict(raw_key=dict(type='str'))),
             source_disk_id=dict(type='str'),
-            source_type=dict(type='str', choices=['RAW']),
+            source_type=dict(type='str'),
         )
     )
 
