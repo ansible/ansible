52adea40c12b43e47b9fcf035324a4aa50bc91e2
Sajna Shetty <Sajna_Shetty@Dell.com>
2019-05-30 12:23:14 -0400

    Changed 3-clause BSD license to 2-clause BSD

--- lib/ansible/module_utils/remote_management/dellemc/ome.py
+++ lib/ansible/module_utils/remote_management/dellemc/ome.py
@@ -14,10 +14,6 @@
 #      this list of conditions and the following disclaimer in the documentation
 #      and/or other materials provided with the distribution.
 
-#    * Neither the name of the copyright holder nor the names of its contributors
-#      may be used to endorse or promote products derived from this software
-#      without specific prior written permission.
-
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
