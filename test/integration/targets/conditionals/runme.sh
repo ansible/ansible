be7e293986f5347aedcf24e75c59679b1cd127f4
s-hertel <shertel@redhat.com>
2019-05-31 14:10:11 -0400

    typo

--- test/integration/targets/conditionals/runme.sh
+++ test/integration/targets/conditionals/runme.sh
@@ -11,5 +11,5 @@ export ANSIBLE_DEPRECATION_WARNINGS=True
 # No warnings for conditionals that are already type bool
 test "$(ansible-playbook -i ../../inventory test_no_warnings.yml "$@" 2>&1 | grep -c '\[DEPRECATION WARNING\]')" = 0
 
-# Warn for bare vars of other types since they may be interpretted differently when CONDITIONAL_BARE_VARS defaults to False
+# Warn for bare vars of other types since they may be interpreted differently when CONDITIONAL_BARE_VARS defaults to False
 test "$(ansible-playbook -i ../../inventory test_warnings.yml "$@" 2>&1 | grep -c '\[DEPRECATION WARNING\]')" = 2
