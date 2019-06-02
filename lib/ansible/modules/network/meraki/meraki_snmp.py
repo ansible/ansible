5730758f13754a0314febacec7dd0c911674da3c
Kevin Breit <kevin.breit@kevinbreit.net>
2019-06-01 19:36:03 -0500

    Add diff support

--- lib/ansible/modules/network/meraki/meraki_snmp.py
+++ lib/ansible/modules/network/meraki/meraki_snmp.py
@@ -153,6 +153,7 @@ import os
 from ansible.module_utils.basic import AnsibleModule, json, env_fallback
 from ansible.module_utils.urls import fetch_url
 from ansible.module_utils._text import to_native
+from ansible.module_utils.common.dict_transformations import recursive_diff
 from ansible.module_utils.network.meraki.meraki import MerakiModule, meraki_argument_spec
 
 
@@ -204,12 +205,19 @@ def set_snmp(meraki, org_id):
     ignored_parameters = ['v3AuthPass', 'v3PrivPass', 'hostname', 'port', 'v2CommunityString', 'v3User']
     if meraki.is_update_required(snmp, full_compare, optional_ignore=ignored_parameters):
         if meraki.module.check_mode is True:
-            meraki.result['data'] = snmp.update(payload)
+            diff = recursive_diff(snmp, full_compare)
+            snmp.update(payload)
+            meraki.result['data'] = snmp
+            meraki.result['diff'] = {'before': diff[0],
+                                     'after': diff[1]}
             meraki.exit_json(**meraki.result)
         r = meraki.request(path,
                            method='PUT',
                            payload=json.dumps(payload))
         if meraki.status == 200:
+            diff = recursive_diff(snmp, r)
+            meraki.result['diff'] = {'before': diff[0],
+                                     'after': diff[1]}
             meraki.result['changed'] = True
             return r
     else:
