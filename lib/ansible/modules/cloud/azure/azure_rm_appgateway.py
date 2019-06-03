27ffc45275899b93bbfb675b0028ebd0061c406b
Piotr Gbyliczek <piotr-gbyliczek@users.noreply.github.com>
2019-06-03 16:44:15 +0100

    Update azure_rm_appgateway.py (#56780)
    * Update azure_rm_appgateway.py documentation to explain the dependency of authentication_certificates on cookie_based_affinity being enabled

--- lib/ansible/modules/cloud/azure/azure_rm_appgateway.py
+++ lib/ansible/modules/cloud/azure/azure_rm_appgateway.py
@@ -294,6 +294,7 @@ options:
             authentication_certificates:
                 description:
                     - List of references to application gateway authentication certificates.
+                    - Applicable only when C(cookie_based_affinity) is enabled, otherwise quietly ignored.
                 suboptions:
                     id:
                         description:
