76abd89cc9e7ce37d1794051ee006aaba856383f
Andrey Klychkov <aaklychkov@mail.ru>
2019-06-03 10:59:37 +0300

    Update lib/ansible/modules/database/postgresql/postgresql_db.py
    Co-Authored-By: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/database/postgresql/postgresql_db.py
+++ lib/ansible/modules/database/postgresql/postgresql_db.py
@@ -65,7 +65,7 @@ options:
     - C(present) implies that the database should be created if necessary.
     - C(absent) implies that the database should be removed if present.
     - C(dump) requires a target definition to which the database will be backed up. (Added in Ansible 2.4)
-      Note that in some PostgreSQL versions of pg_dump, which is an embedded PostgreSQL unility and is used by the module,
+      Note that in some PostgreSQL versions of pg_dump, which is an embedded PostgreSQL utility and is used by the module,
       returns rc 0 even when errors occurred (e.g. the connection is forbidden by pg_hba.conf, etc.),
       so the module returns changed=True but the dump has not actually been done. Please, be sure that your version of
       pg_dump returns rc 1 in this case.
