becf469da4db8f4efb644adf6996bc17663f6f8f
Alicia Cozine <acozine@users.noreply.github.com>
2019-06-03 13:18:11 -0500

    clarifies language based on converstaion with bcoca

--- lib/ansible/modules/packaging/os/package.py
+++ lib/ansible/modules/packaging/os/package.py
@@ -33,7 +33,7 @@ options:
     required: true
   state:
     description:
-      - Whether to install (C(present)), or remove (C(absent)) a package. Other states depend on the underlying package module, e.g. C(latest).
+      - Whether to install (C(present)), or remove (C(absent)) a package. You can use other states like c(latest) only if they are supported by the underlying OS package manager(s) executed.
     required: true
   use:
     description:
