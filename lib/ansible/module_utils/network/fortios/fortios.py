44e7ad762f95443fb9afd4610d022ddd53712eb8
Miguel Angel Muñoz <magonzalez@fortinet.com>
2019-06-03 21:09:01 +0200

    Trigger new build due to infrastructure errors

--- lib/ansible/module_utils/network/fortios/fortios.py
+++ lib/ansible/module_utils/network/fortios/fortios.py
@@ -4,7 +4,8 @@
 # still belong to the author of the module, and may assign their own license
 # to the complete work.
 #
-# Copyright (c), Benjamin Jolivot <bjolivot@gmail.com>, 2014
+# Copyright (c), Benjamin Jolivot <bjolivot@gmail.com>, 2014,
+# Miguel Angel Muñoz <magonzalez@fortinet.com>, 2019
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification,
