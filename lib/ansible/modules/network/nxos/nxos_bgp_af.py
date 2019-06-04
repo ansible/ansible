eb7037bff9fd95d328362c7cbe62ab2554ae8a79
jacobsnapp <7643439+jacobsnapp@users.noreply.github.com>
2019-06-04 15:32:40 +0000

    fixed indentation

--- lib/ansible/modules/network/nxos/nxos_bgp_af.py
+++ lib/ansible/modules/network/nxos/nxos_bgp_af.py
@@ -764,7 +764,7 @@ def main():
     if candidate:
         candidate = candidate.items_text()
         if not module.check_mode:
-          load_config(module, candidate)
+        	load_config(module, candidate)
         result['changed'] = True
         result['commands'] = candidate
     else:
