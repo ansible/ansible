b96a315be1eecf88935941660cf08a9ed763afb5
Caio Ramos <caioramos97@gmail.com>
2019-06-05 15:17:22 -0300

    Fixing lint
    Co-Authored-By: gprangel <gabriely.pereira@usp.br>

--- lib/ansible/modules/net_tools/basics/read_mail.py
+++ lib/ansible/modules/net_tools/basics/read_mail.py
@@ -3,8 +3,8 @@
 # Copyright: (c) 2018, Caio Ramos <caioramos97@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-__metaclass__ = type
 from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
