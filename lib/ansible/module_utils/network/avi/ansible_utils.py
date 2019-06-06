2f608d950d8f0430ff498463f7f53e9670db9762
Chaitanya Deshpande <chaitanya.deshpande@avinetworks.com>
2019-06-06 13:28:00 +0530

    Added sub-options to avi credentials

--- lib/ansible/module_utils/network/avi/ansible_utils.py
+++ lib/ansible/module_utils/network/avi/ansible_utils.py
@@ -544,6 +544,19 @@ def avi_common_argument_spec():
     Returns common arguments for all Avi modules
     :return: dict
     """
+    credentials_spec = dict(
+        controller=dict(default=os.environ.get('AVI_CONTROLLER', '')),
+        username=dict(default=os.environ.get('AVI_USERNAME', '')),
+        password=dict(default=os.environ.get('AVI_PASSWORD', ''), no_log=True),
+        api_version=dict(default='16.4.4', type='str'),
+        tenant=dict(default='admin'),
+        tenant_uuid=dict(default='', type='str'),
+        port=dict(type='int'),
+        timeout=dict(default=300, type='int'),
+        session_id=dict(default='', type='str', no_log=True),
+        csrftoken=dict(default='', type='str', no_log=True)
+    )
+
     return dict(
         controller=dict(default=os.environ.get('AVI_CONTROLLER', '')),
         username=dict(default=os.environ.get('AVI_USERNAME', '')),
@@ -551,7 +564,7 @@ def avi_common_argument_spec():
         tenant=dict(default='admin'),
         tenant_uuid=dict(default=''),
         api_version=dict(default='16.4.4', type='str'),
-        avi_credentials=dict(default=None, no_log=True, type='dict'),
-        avi_login_info=dict(default=None, type='dict'),
+        avi_credentials=dict(default=None, type='dict',
+                             options=credentials_spec),
         api_context=dict(type='dict'),
         avi_disable_session_cache_as_fact=dict(default=False, type='bool'))
