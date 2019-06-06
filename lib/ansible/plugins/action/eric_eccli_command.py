631714b351fda895a3a16a2c50b955740d3c4ccd
Cheng You <cheng.you@ericsson.com>
2019-06-06 10:35:58 +0200

    modify file name(eric_eccli.py->eric_eccli_command.py)

--- lib/ansible/plugins/action/eric_eccli_command.py
+++ lib/ansible/plugins/action/eric_eccli_command.py
@@ -22,10 +22,10 @@ __metaclass__ = type
 from ansible.plugins.action.normal import ActionModule as _ActionModule
 
 
-#class ActionModule(_ActionModule):
+class ActionModule(_ActionModule):
 
-#    def run(self, tmp=None, task_vars=None):
-#        if self._play_context.connection != 'network_cli':
-#            return {'failed': True, 'msg': 'Connection type %s is not valid for cli_config module' % self._play_context.connection}
+    def run(self, tmp=None, task_vars=None):
+        if self._play_context.connection != 'network_cli':
+            return {'failed': True, 'msg': 'Connection type %s is not valid for cli_config module' % self._play_context.connection}
 
-#        return super(ActionModule, self).run(task_vars=task_vars)
+        return super(ActionModule, self).run(task_vars=task_vars)
--- lib/ansible/plugins/cliconf/eric_eccli.py
+++ lib/ansible/plugins/cliconf/eric_eccli.py
@@ -1,6 +1,5 @@
 #
 # Copyright (c) 2019 Ericsson AB.
-# This file is part of Ansible
 #
 # Ansible is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
