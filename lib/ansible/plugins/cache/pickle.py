92e80a9fdc9bc67f7568b072867418aab12c8bd7
Sander Steffann <sander@steffann.nl>
2019-06-10 19:55:16 +0200

    Don't throw error when cache entry doesn't exist

--- lib/ansible/plugins/cache/pickle.py
+++ lib/ansible/plugins/cache/pickle.py
@@ -55,12 +55,15 @@ class CacheModule(BaseFileCacheModule):
     """
 
     def _load(self, filepath):
-        # Pickle is a binary format
-        with open(filepath, 'rb') as f:
-            if PY3:
-                return pickle.load(f, encoding='bytes')
-            else:
-                return pickle.load(f)
+        try:
+            # Pickle is a binary format
+            with open(filepath, 'rb') as f:
+                if PY3:
+                    return pickle.load(f, encoding='bytes')
+                else:
+                    return pickle.load(f)
+        except IOError:
+            return None
 
     def _dump(self, value, filepath):
         with open(filepath, 'wb') as f:
