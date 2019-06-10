23a42fcc204cb5ddd01fd306f8e930e8354f7100
Felix Fontein <felix@fontein.de>
2019-06-10 17:05:20 +0200

    Two fixes.

--- lib/ansible/modules/cloud/amazon/cloudwatchlogs_log_group_info.py
+++ lib/ansible/modules/cloud/amazon/cloudwatchlogs_log_group_info.py
@@ -14,11 +14,12 @@ DOCUMENTATION = '''
 ---
 module: cloudwatchlogs_log_group_info
 short_description: get information about log_group in CloudWatchLogs
-description: Lists the specified log groups. You can list all your log groups or filter the results by prefix.
+description:
+    - Lists the specified log groups. You can list all your log groups or filter the results by prefix.
+    - This module was called C(cloudwatchlogs_log_group_facts) before Ansible 2.9. The usage did not change.
 version_added: "2.5"
 author:
     - Willian Ricardo (@willricardo) <willricardo@gmail.com>
-    - This module was called C(cloudwatchlogs_log_group_facts) before Ansible 2.9. The usage did not change.
 requirements: [ botocore, boto3 ]
 options:
     log_group_name:
