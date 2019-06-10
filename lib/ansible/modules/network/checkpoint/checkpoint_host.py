1d7d687bf2a8d971c98fabae8868a8c78b977b75
orso <orso@checkpoint.com>
2019-06-10 10:00:07 +0300

    small fixes

--- lib/ansible/modules/network/checkpoint/checkpoint_host.py
+++ lib/ansible/modules/network/checkpoint/checkpoint_host.py
@@ -49,27 +49,6 @@ options:
       - State of the access rule (present or absent). Defaults to present.
     type: str
     default: present
-  auto_publish_session:
-    description:
-      - Publish the current session if changes have been performed
-        after task completes.
-    type: bool
-    default: 'yes'
-  auto_install_policy:
-    description:
-      - Install the package policy if changes have been performed
-        after the task completes.
-    type: bool
-    default: 'yes'
-  policy_package:
-    description:
-      - Package policy name to be installed.
-    type: str
-    default: 'standard'
-  targets:
-    description:
-      - Targets to install the package policy on.
-    type: list
 """
 
 EXAMPLES = """
