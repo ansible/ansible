03a199d1a9756d9e0f871b5b3ea4a9c1e8f533f6
René Moser <mail@renemoser.net>
2019-06-11 07:51:24 +0200

    vultr: vultr_os_facts -> info (#57565)
    * vultr: vultr_os_facts: rename to info
    
    * vultr: vultr_os_facts: rename tests to info
    
    * vultr: adjust tests
    
    * vultr: new vultr_os_info module

--- lib/ansible/modules/cloud/vultr/_vultr_os_facts.py
+++ lib/ansible/modules/cloud/vultr/_vultr_os_facts.py
@@ -8,7 +8,7 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
+                    'status': ['deprecated'],
                     'supported_by': 'community'}
 
 DOCUMENTATION = r'''
@@ -19,6 +19,10 @@ description:
   - Gather facts about OSes available to boot servers.
 version_added: "2.7"
 author: "Yanis Guenane (@Spredzy)"
+deprecated:
+  removed_in: "2.12"
+  why: Transformed into an info module.
+  alternative: Use M(vultr_os_info) instead.
 extends_documentation_fragment: vultr
 '''
 
