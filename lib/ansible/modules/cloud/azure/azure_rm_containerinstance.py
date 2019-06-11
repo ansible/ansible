f64c1f68343dda635968d114478197e4fa2eff30
smile37773 <714389288@qq.com>
2019-06-11 11:31:29 +0800

    add the log and fix the keyError

--- lib/ansible/modules/cloud/azure/azure_rm_containerinstance.py
+++ lib/ansible/modules/cloud/azure/azure_rm_containerinstance.py
@@ -401,7 +401,11 @@ class AzureRMContainerInstance(AzureRMModuleBase):
 
             self.results['id'] = response['id']
             self.results['provisioning_state'] = response['provisioning_state']
-            self.results['ip_address'] = response['ip_address']['ip'] if 'ip_address' in response else ''
+            try:
+                print response['ip_address'] if 'ip_address' in response
+            except:
+                print (response['ip_address'] if 'ip_address' in response)
+            self.results['ip_address'] = response['ip_address']['ip'] if 'ip_address' in response and 'ip' in response['ip_address'] else ''
 
             self.log("Creation / Update done")
 
