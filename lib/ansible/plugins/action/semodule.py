9a31b3621bb608d429de97a8f7be61ccdaee6fff
Philip Bove <phil@bove.online>
2019-06-11 15:17:08 -0400

    addded checksumming and module utils

--- lib/ansible/plugins/action/semodule.py
+++ lib/ansible/plugins/action/semodule.py
@@ -1,6 +1,8 @@
 from ansible.plugins.action import ActionBase
 from ansible.errors import AnsibleError
 from ansible.module_utils._text import to_text
+import ansible.module_utils.semodule as semodule
+from ansible.utils.hashing import checksum
 import traceback
 
 class ActionModule(ActionBase):
@@ -15,14 +17,6 @@ class ActionModule(ActionBase):
         else:
             return False, 'same'
 
-    def _semodule_info(self, name, semodule_info):
-        cur_pol = {}
-        semodule_list = semodule_info.split('\n')
-        for line in semodule_list:
-            if name in line:
-                cur_pol['name'] ,cur_pol['version'] = line.split('\t')
-        return cur_pol
-
     def _module_exist_(self, name, semodule_info):
         if name in semodule_info:
             return True
@@ -34,30 +28,18 @@ class ActionModule(ActionBase):
             AnsibleError('semodule command failed')
         return to_text(result['stdout'])
 
-    def _module_info_(self, name, semodule_info):
-        cur_pol = {}
-        semodule_list = semodule_info.split('\n')
-        for line in semodule_list:
-            if name in line:
-                cur_pol['name'] ,cur_pol['version'] = line.strip().split('\t')
-        return cur_pol
-
-    def _read_te_file(self, file):
-        with open(file, 'r') as te_file:
-            intro_line = te_file.read().split('\n')[0].strip(';').split(' ')
-        policy_def = {}
-        policy_def['name'] = intro_line[1]
-        policy_def['version'] = intro_line[2]
-        return policy_def
-
-    def _copy_te_file(self, te_file, tmp_src):
-        remote_path = self._transfer_file(te_file, tmp_src)
-        self._fixup_perms2((self._connection._shell.tmpdir, remote_path))
-
     def _remove_module(self, name):
         result = self._low_level_execute_command('semodule -r {module}'.format(module=name))
         if result['rc'] != 0:
             AnsibleError('failed to remove policy')
+
+    def _copy_te_file(self, file, dest, task_vars):
+        local_checksum = checksum(file)
+        remote_path = self._transfer_file(file, dest)
+        self._fixup_perms2((self._connection._shell.tmpdir, remote_path))
+        remote_checksum = self._execute_remote_stat(dest, all_vars=task_vars, follow=False)
+        if local_checksum != remote_checksum['checksum']:
+            AnsibleError('.te file copy failed, checksum does not match')
 
     def run(self, tmp=None, task_vars=None):
         result = super(ActionModule, self).run(tmp, task_vars)
@@ -74,16 +56,16 @@ class ActionModule(ActionBase):
             result['msg'] = to_text(error)
             result['exception'] = traceback.format_exc()
             return result
-        policy_def = self._read_te_file(te_file)
+        policy_def = semodule.parse_module_info(semodule.read_te_file(te_file))
         semodule_info = self._get_semodule_info()
         cur_pol = None
         if self._module_exist_(policy_def['name'], semodule_info):
-            cur_pol = self._module_info_(policy_def['name'], semodule_info)
+            cur_pol = semodule.parse_pol_info(policy_def['name'], semodule_info)
         if state == 'latest':
             if cur_pol:
                 ver_check = self._check_policy_version(policy_def['version'], cur_pol['version'])
                 if ver_check[1] == 'newer':
-                    self._copy_te_file(te_file, tmp_src)
+                    self._copy_te_file(te_file, tmp_src, task_vars)
                     new_module_args.update(dict(src=tmp_src))
                     module_return = self._execute_module(module_name='semodule', module_args=new_module_args, task_vars=task_vars)
                     result.update(module_return)
@@ -92,7 +74,7 @@ class ActionModule(ActionBase):
                     result['name'] = cur_pol['name']
         elif state == 'present':
             if not cur_pol:
-                self._copy_te_file(te_file, tmp_src)
+                self._copy_te_file(te_file, tmp_src, task_vars)
                 new_module_args.update(dict(src=tmp_src))
                 module_return = self._execute_module(module_name='semodule', module_args=new_module_args, task_vars=task_vars)
                 result.update(module_return)
