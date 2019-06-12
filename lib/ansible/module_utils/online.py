aa8cff443abbd882a77c568dfb55cea589b5f1eb
Ludovic Petetin <kelkooludovicp@yahoo.fr>
2019-06-12 11:02:23 +0200

    change API default URL to avoid module argument overload

--- lib/ansible/module_utils/online.py
+++ lib/ansible/module_utils/online.py
@@ -14,7 +14,7 @@ def online_argument_spec():
     return dict(
         api_token=dict(required=True, fallback=(env_fallback, ['ONLINE_TOKEN', 'ONLINE_API_KEY', 'ONLINE_OAUTH_TOKEN', 'ONLINE_API_TOKEN']),
                        no_log=True, aliases=['oauth_token']),
-        api_url=dict(fallback=(env_fallback, ['ONLINE_API_URL']), default='https://api.online.net', aliases=['base_url']),
+        api_url=dict(fallback=(env_fallback, ['ONLINE_API_URL']), default='https://api.online.net/api/v1', aliases=['base_url']),
         api_timeout=dict(type='int', default=30, aliases=['timeout']),
         validate_certs=dict(default=True, type='bool'),
     )
