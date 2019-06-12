56f0a0b1c93f8c01fe312c9fe99e629efba95eec
Chaitanya Deshpande <chaitanya.deshpande@avinetworks.com>
2019-06-12 21:03:00 +0530

    Updated argument specs to use fallback instead of getting defaults from environment variable

--- lib/ansible/module_utils/network/avi/ansible_utils.py
+++ lib/ansible/module_utils/network/avi/ansible_utils.py
@@ -10,6 +10,7 @@ import re
 import logging
 import sys
 from copy import deepcopy
+from ansible.module_utils.basic import env_fallback
 
 try:
     from ansible.module_utils.network.avi.avi_api import (
@@ -545,9 +546,9 @@ def avi_common_argument_spec():
     :return: dict
     """
     credentials_spec = dict(
-        controller=dict(default=os.environ.get('AVI_CONTROLLER', '')),
-        username=dict(default=os.environ.get('AVI_USERNAME', '')),
-        password=dict(default=os.environ.get('AVI_PASSWORD', ''), no_log=True),
+        controller=dict(fallback=(env_fallback, ['AVI_CONTROLLER'])),
+        username=dict(fallback=(env_fallback, ['AVI_USERNAME'])),
+        password=dict(fallback=(env_fallback, ['AVI_PASSWORD']), no_log=True),
         api_version=dict(default='16.4.4', type='str'),
         tenant=dict(default='admin'),
         tenant_uuid=dict(default='', type='str'),
@@ -559,9 +560,9 @@ def avi_common_argument_spec():
     )
 
     return dict(
-        controller=dict(default=os.environ.get('AVI_CONTROLLER', '')),
-        username=dict(default=os.environ.get('AVI_USERNAME', '')),
-        password=dict(default=os.environ.get('AVI_PASSWORD', ''), no_log=True),
+        controller=dict(fallback=(env_fallback, ['AVI_CONTROLLER'])),
+        username=dict(fallback=(env_fallback, ['AVI_USERNAME'])),
+        password=dict(fallback=(env_fallback, ['AVI_PASSWORD']), no_log=True),
         tenant=dict(default='admin'),
         tenant_uuid=dict(default=''),
         api_version=dict(default='16.4.4', type='str'),
