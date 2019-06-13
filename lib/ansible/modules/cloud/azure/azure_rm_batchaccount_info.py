2ba05d506bf0efaf5f55703817c95c2228a84f50
Junyi Yi <junyi@microsoft.com>
2019-06-13 16:52:37 -0700

    Fix import issue.

--- lib/ansible/modules/cloud/azure/azure_rm_batchaccount_info.py
+++ lib/ansible/modules/cloud/azure/azure_rm_batchaccount_info.py
@@ -122,7 +122,8 @@ from ansible.module_utils.azure_rm_common import AzureRMModuleBase
 
 try:
     from msrestazure.azure_exceptions import CloudError
-    from azure.mgmt.batch import BatchManagementClient    from msrest.serialization import Model
+    from azure.mgmt.batch import BatchManagementClient
+    from msrest.serialization import Model
 except ImportError:
     # This is handled in azure_rm_common
     pass
