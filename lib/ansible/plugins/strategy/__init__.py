d53c11c80b486d4674e740621a3c2580e863e5f4
s-hertel <shertel@redhat.com>
2019-06-14 14:36:49 -0400

    move local function into a class method

--- lib/ansible/plugins/strategy/__init__.py
+++ lib/ansible/plugins/strategy/__init__.py
@@ -350,6 +350,10 @@ class StrategyBase:
 
         return [actual_host]
 
+    def get_handler_templar(self, handler_task, iterator):
+        handler_vars = self._variable_manager.get_vars(play=iterator._play, task=handler_task)
+        return Templar(loader=self._loader, variables=handler_vars)
+
     @debug_closure
     def _process_pending_results(self, iterator, one_pass=False, max_passes=None):
         '''
@@ -367,17 +371,13 @@ class StrategyBase:
             else:
                 return self._inventory.get_host(host_name)
 
-        def get_handler_templar(handler_task):
-            handler_vars = self._variable_manager.get_vars(play=iterator._play, task=handler_task)
-            return Templar(loader=self._loader, variables=handler_vars)
-
         def search_handler_blocks_by_name(handler_name, handler_blocks):
             # iterate in reversed order since last handler loaded with the same name wins
             for handler_block in reversed(handler_blocks):
                 for handler_task in handler_block.block:
                     if handler_task.name:
                         if not handler_task.cached_name:
-                            templar = get_handler_templar(handler_task)
+                            templar = self.get_handler_templar(handler_task, iterator)
                             handler_task.name = templar.template(handler_task.name)
                             handler_task.cached_name = True
 
@@ -533,7 +533,7 @@ class StrategyBase:
                                 for listening_handler_block in iterator._play.handlers:
                                     for listening_handler in listening_handler_block.block:
                                         listeners = getattr(listening_handler, 'listen', []) or []
-                                        templar = get_handler_templar(listening_handler)
+                                        templar = self.get_handler_templar(listening_handler, iterator)
                                         listeners = listening_handler.get_validated_value(
                                             'listen', listening_handler._valid_attrs['listen'], listeners, templar
                                         )
