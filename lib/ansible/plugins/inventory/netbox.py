4715860dbf8eef4bca9e38ea5ac8317ea7a6c004
Sander Steffann <sander@steffann.nl>
2019-06-14 06:29:47 +0200

    netbox: inventory: fix implementation of groups option (#57689)

--- lib/ansible/plugins/inventory/netbox.py
+++ lib/ansible/plugins/inventory/netbox.py
@@ -434,8 +434,9 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
 
             # Composed variables
             self._set_composite_vars(self.get_option('compose'), host, hostname, strict=strict)
+
             # Complex groups based on jinja2 conditionals, hosts that meet the conditional are added to group
-            self._set_composite_vars(self.get_option('compose'), host, hostname, strict=strict)
+            self._add_host_to_composed_groups(self.get_option('groups'), host, hostname, strict=strict)
 
             # Create groups based on variable values and add the corresponding hosts to it
             self._add_host_to_keyed_groups(self.get_option('keyed_groups'), host, hostname, strict=strict)
