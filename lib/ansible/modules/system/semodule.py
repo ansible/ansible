cdb1782cd7a65f8cbb41ceaad453cdd4c52dbdee
Philip Bove <phil@bove.online>
2019-06-14 17:24:04 -0400

    fixed import and formatting errors

--- lib/ansible/modules/system/semodule.py
+++ lib/ansible/modules/system/semodule.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # Copyright: (c) 2019, Philip Bove <phil@bove.online>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 
 ANSIBLE_METADATA = {
     'metdata_version': 1.1,
@@ -74,7 +76,7 @@ name:
 '''
 
 REQUIREMENTS = [
-    'semodule', 
+    'semodule',
     'checkmodule',
     'semodule_package'
 ]
@@ -90,6 +92,7 @@ def get_semodule_info(module):
         module.fail_json(msg='semodule command failed')
     return stdout
 
+
 def ensure(module, policy_def):
     changed = False
     apply_te_file(module, policy_def)
@@ -97,39 +100,47 @@ def ensure(module, policy_def):
     changed = True
     module.exit_json(changed=changed, name=end_pol['name'], version=end_pol['version'])
 
+
 def check_run_fail(run, module):
     if run[0] != 0:
         module.fail_json(msg=run[2])
 
+
 def apply_te_file(module, policy_def):
-    chk_module_out = module.run_command(['checkmodule','-M','-m', module.params['src'], '-o', policy_def['name']+'.mod'])
+    chk_module_out = module.run_command(['checkmodule', '-M', '-m', module.params['src'], '-o', policy_def['name'] + '.mod'])
     check_run_fail(chk_module_out, module)
-    semod_package_out = module.run_command(['semodule_package','-o', policy_def['name']+'.pp','-m' , policy_def['name']+'.mod'])
+    semod_package_out = module.run_command(['semodule_package', '-o', policy_def['name'] + '.pp', '-m', policy_def['name'] + '.mod'])
     check_run_fail(semod_package_out, module)
-    semodule_out = module.run_command(['semodule', '-i', policy_def['name']+'.pp'])
+    semodule_out = module.run_command(['semodule', '-i', policy_def['name'] + '.pp'])
     check_run_fail(semodule_out, module)
+
 
 def check_requirements(module):
     if os.getuid() != 0:
         module.fail_json(msg='This module can only be run elevated')
     for req in REQUIREMENTS:
         rc, stdout, stderr = module.run_command(['which', req])
-        if rc != 0 :
+        if rc != 0:
             module.fail_json(msg='missing the {name} utility which is required to run this module'.format(name=req))
+
 
 def main():
     module = AnsibleModule(
         argument_spec=dict(
             src=dict(type='path', required=True),
-            force=dict(type='bool', required=False),
+            force=dict(type='bool', required=False, choices=[True, False]),
             state=dict(type='str', default='present', choices=['present', 'latest', 'absent'])
         ),
         supports_check_mode=True
     )
     check_requirements(module)
-    te_info = semodule.read_te_file(module.params['src'])
-    policy_def = semodule.parse_module_info(te_info) 
+    try:
+        te_info = semodule.read_te_file(module.params['src'])
+    except IndexError:
+        module.fail_json(msg='Module is missing module definition line')
+    policy_def = semodule.parse_module_info(te_info)
     ensure(module, policy_def)
+
 
 if __name__ == '__main__':
     main()
