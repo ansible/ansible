dd46d4f2e5984bcdec313beee518769fe762f5f6
Cheng You <cheng.you@ericsson.com>
2019-06-16 09:44:22 +0200

    fix a bug:https://github.com/ansible/ansible/issues/30567

--- lib/ansible/modules/network/eric_eccli/eric_eccli_command.py
+++ lib/ansible/modules/network/eric_eccli/eric_eccli_command.py
@@ -26,7 +26,6 @@ description:
     before returning or timing out if the condition is not met.
   - This module also supports running commands in configuration mode
     in raw command style.
-extends_documentation_fragment: eric_eccli
 notes:
   - Tested against IPOS 19.3
 options:
