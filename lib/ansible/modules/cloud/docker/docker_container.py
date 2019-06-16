92bb3ff401f8707b200c0599b13359070fb1ef83
Felix Fontein <felix@fontein.de>
2019-06-16 13:10:12 +0200

    Don't die when network doesn't exist.

--- lib/ansible/modules/cloud/docker/docker_container.py
+++ lib/ansible/modules/cloud/docker/docker_container.py
@@ -1411,11 +1411,14 @@ class TaskParameters(DockerBaseClass):
             return ip
         for net in self.networks:
             if net.get('name'):
-                network = self.client.inspect_network(net['name'])
-                if network.get('Driver') == 'bridge' and \
-                   network.get('Options', {}).get('com.docker.network.bridge.host_binding_ipv4'):
-                    ip = network['Options']['com.docker.network.bridge.host_binding_ipv4']
-                    break
+                try:
+                    network = self.client.inspect_network(net['name'])
+                    if network.get('Driver') == 'bridge' and \
+                       network.get('Options', {}).get('com.docker.network.bridge.host_binding_ipv4'):
+                        ip = network['Options']['com.docker.network.bridge.host_binding_ipv4']
+                        break
+                except NotFound as e:
+                    self.client.fail("Cannot inspect the network '{0}' to determine the default IP.", exception=traceback.format_exc())
         return ip
 
     def _parse_publish_ports(self):
