5a755b091962a146027619c34c2a8cce639ccb03
Dusan Matejka <matejkadusan32@gmail.com>
2019-06-18 16:46:34 +0200

    updated zabbix modules to use missing_required_lib helper

--- lib/ansible/modules/monitoring/zabbix/zabbix_hostmacro.py
+++ lib/ansible/modules/monitoring/zabbix/zabbix_hostmacro.py
@@ -71,6 +71,9 @@ EXAMPLES = '''
     state: present
 '''
 
+
+import traceback
+
 try:
     from zabbix_api import ZabbixAPI, ZabbixAPISubClass
 
@@ -82,9 +85,10 @@ try:
 
     HAS_ZABBIX_API = True
 except ImportError:
+    ZBX_IMP_ERR = traceback.format_exc()
     HAS_ZABBIX_API = False
 
-from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.basic import AnsibleModule, missing_required_lib
 
 
 class HostMacro(object):
@@ -170,7 +174,7 @@ def main():
     )
 
     if not HAS_ZABBIX_API:
-        module.fail_json(msg="Missing required zabbix-api module (check docs or install with: pip install zabbix-api)")
+        module.fail_json(msg=missing_required_lib('zabbix-api', url='https://pypi.org/project/zabbix-api/'), exception=ZBX_IMP_ERR)
 
     server_url = module.params['server_url']
     login_user = module.params['login_user']
