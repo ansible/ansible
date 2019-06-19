0d868a0a7704aa2aed30127243a1a47a218bb6f3
Cheng You <cheng.you@ericsson.com>
2019-06-19 14:32:36 +0200

    modify file

--- lib/ansible/modules/network/eric_eccli/eric_eccli_command.py
+++ lib/ansible/modules/network/eric_eccli/eric_eccli_command.py
@@ -26,8 +26,54 @@ description:
     before returning or timing out if the condition is not met.
   - This module also supports running commands in configuration mode
     in raw command style.
+extends_documentation_fragment: eric_eccli
 notes:
   - Tested against IPOS 19.3
+options:
+  commands:
+    description:
+      - List of commands to send to the remote ECCLI device over the
+        configured provider. The resulting output from the command
+        is returned. If the I(wait_for) argument is provided, the
+        module is not returned until the condition is satisfied or
+        the number of retries has expired. If a command sent to the
+        device requires answering a prompt, it is possible to pass
+        a dict containing I(command), I(answer) and I(prompt).
+        Common answers are 'y' or "\\r" (carriage return, must be
+        double quotes). See examples.
+    required: true
+  wait_for:
+    description:
+      - List of conditions to evaluate against the output of the
+        command. The task will wait for each condition to be true
+        before moving forward. If the conditional is not true
+        within the configured number of retries, the task fails.
+        See examples.
+    aliases: ['waitfor']
+  match:
+    description:
+      - The I(match) argument is used in conjunction with the
+        I(wait_for) argument to specify the match policy.  Valid
+        values are C(all) or C(any).  If the value is set to C(all)
+        then all conditionals in the wait_for must be satisfied.  If
+        the value is set to C(any) then only one of the values must be
+        satisfied.
+    default: all
+    choices: ['any', 'all']
+  retries:
+    description:
+      - Specifies the number of retries a command should by tried
+        before it is considered failed. The command is run on the
+        target device every retry and evaluated against the
+        I(wait_for) conditions.
+    default: 10
+  interval:
+    description:
+      - Configures the interval in seconds to wait between retries
+        of the command. If the command does not pass the specified
+        conditions, the interval indicates how long to wait before
+        trying the command again.
+    default: 1
 """
 
 EXAMPLES = r"""
