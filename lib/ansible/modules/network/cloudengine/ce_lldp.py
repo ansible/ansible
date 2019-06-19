f8ee4519e43ace0b9468a776db0e9bcae77a0b95
xuxiaowei0512 <786018072@qq.com>
2019-06-19 22:57:05 +0800

    update for shippable

--- lib/ansible/modules/network/cloudengine/ce_lldp.py
+++ lib/ansible/modules/network/cloudengine/ce_lldp.py
@@ -36,56 +36,69 @@ options:
             - Set global LLDP enable state.
         required: false
         choices: ['enabled', 'disabled']
+        type: str
     mdnstatus:
         description:
             - Set global MDN enable state.
         required: false
         choices: ['rxOnly', 'disabled']
+        type: str
     interval:
         description:
             - Frequency at which LLDP advertisements are sent (in seconds).
         required: false
+        type: int
     hold_multiplier:
         description:
             - Time multiplier for device information in neighbor devices.
         required: false
+        type: int
     restart_delay:
         description:
             - Specifies the delay time of the interface LLDP module from disabled state to re enable.
         required: false
+        type: int
     transmit_delay:
         description:
             - Delay time for sending LLDP messages.
         required: false
+        type: int
     notification_interval:
         description:
             - Suppression time for sending LLDP alarm.
         required: false
+        type: int
     fast_count:
         description:
             - The number of LLDP messages sent to the neighbor nodes by the specified device.
         required: false
+        type: int
     mdn_notification_interval:
         description:
             - Delay time for sending MDN neighbor information change alarm.
         required: false
+        type: int
     management_address:
         description:
             - The management IP address of LLDP.
         required: false
         default: null
+        type: str
     bind_name:
         description:
             - Binding interface name.
         required: false
         default: null
+        type: str
     state:
         description:
             - Manage the state of the resource.
         required: false
         default: present
+        type: str
         choices: ['present','absent']
     provider:
+        type: dict
         description:
             - A dict parameter that is used to connect hosts and is not required and will removed.
         required: false
