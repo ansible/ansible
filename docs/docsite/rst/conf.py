9fe6a2562a3756ad684dbebcf8dec82dfc77bdc8
Sandra McCann <samccann@redhat.com>
2019-06-19 15:39:45 -0400

    Add dropdown version selection to docsite (#55655)
    * docs: Add way to select version for documentation
    
    (cherry picked from commit f3720c90e06f0a0f0abc3b5407dc944253ac2e43)

--- docs/docsite/rst/conf.py
+++ docs/docsite/rst/conf.py
@@ -244,3 +244,9 @@ autoclass_content = 'both'
 intersphinx_mapping = {'python': ('https://docs.python.org/2/', (None, '../python2-2.7.13.inv')),
                        'python3': ('https://docs.python.org/3/', (None, '../python3-3.6.2.inv')),
                        'jinja2': ('http://jinja.pocoo.org/docs/', (None, '../jinja2-2.9.7.inv'))}
+
+# list specifically out of order to make latest work
+html_context = {
+    'current_version': version,
+    'available_versions': ('latest', '2.7', '2.6', 'devel')
+}
