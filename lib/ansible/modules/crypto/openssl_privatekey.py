5f719bba18ec64edc2c9b18dba960535a2226c7b
Felix Fontein <felix@fontein.de>
2019-06-20 21:31:00 +0200

    crypto modules: Improve requirements / error messages (#57868)
    * Reformat requirements.
    
    * Include Python lib versions in lib required error messages, if available.
    
    * Update lib/ansible/modules/crypto/openssl_publickey.py
    
    Co-Authored-By: MarkusTeufelberger <mteufelberger@mgit.at>
    
    * Add changelog.
    
    (cherry picked from commit 4188cd299bd9f38025144992f47aa8974ceac479)

--- lib/ansible/modules/crypto/openssl_privatekey.py
+++ lib/ansible/modules/crypto/openssl_privatekey.py
@@ -671,11 +671,13 @@ def main():
     try:
         if backend == 'pyopenssl':
             if not PYOPENSSL_FOUND:
-                module.fail_json(msg=missing_required_lib('pyOpenSSL'), exception=PYOPENSSL_IMP_ERR)
+                module.fail_json(msg=missing_required_lib('pyOpenSSL >= {0}'.format(MINIMAL_PYOPENSSL_VERSION)),
+                                 exception=PYOPENSSL_IMP_ERR)
             private_key = PrivateKeyPyOpenSSL(module)
         elif backend == 'cryptography':
             if not CRYPTOGRAPHY_FOUND:
-                module.fail_json(msg=missing_required_lib('cryptography'), exception=CRYPTOGRAPHY_IMP_ERR)
+                module.fail_json(msg=missing_required_lib('cryptography >= {0}'.format(MINIMAL_CRYPTOGRAPHY_VERSION)),
+                                 exception=CRYPTOGRAPHY_IMP_ERR)
             private_key = PrivateKeyCryptography(module)
 
         if private_key.state == 'present':
