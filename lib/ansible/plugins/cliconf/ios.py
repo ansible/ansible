a1aaafefc5ddef49292ee5941b934e3c2c4c0ac5
Sumit Jaiswal <sjaiswal@redhat.com>
2019-06-21 14:03:16 +0530

    minor fix
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>

--- lib/ansible/plugins/cliconf/ios.py
+++ lib/ansible/plugins/cliconf/ios.py
@@ -207,13 +207,12 @@ class Cliconf(CliconfBase):
         if match:
             device_info['network_os_version'] = match.group(1).strip(',')
 
-        match = re.search(r'^Cisco (.+) \(revision', data, re.M)
-        if match:
-            device_info['network_os_model'] = match.group(1)
-        else:
-            match = re.search(r'^[Cc]isco (\S+).+bytes of .*memory', data, re.M)
+        model_search_strs = [r'^Cisco (.+) \(revision', r'^[Cc]isco (\S+).+bytes of .*memory']
+        for item in model_search_strs:
+            match = re.search(item, data, re.M)
             if match:
                 device_info['network_os_model'] = match.group(1)
+                break
 
         match = re.search(r'^(.+) uptime', data, re.M)
         if match:
