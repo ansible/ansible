1b6b7b4818de6172822be495431c184e0276c11b
Wojciech Wypior <w.wypior@f5.com>
2019-06-21 14:45:58 +0100

    Fixes issue with failure messages (#58173)

--- lib/ansible/modules/network/f5/bigip_appsvcs_extension.py
+++ lib/ansible/modules/network/f5/bigip_appsvcs_extension.py
@@ -496,9 +496,13 @@ class ModuleManager(object):
             self.want.tenants
         )
         response = self.client.api.delete(uri)
-        if response.status == 200:
-            return True
-        raise F5ModuleError(response.content)
+        if response.status != 200:
+            result = response.json()
+            errors = self._get_errors_from_response(result)
+            if errors:
+                message = "{0}".format('. '.join(errors))
+                raise F5ModuleError(message)
+            raise F5ModuleError(response.content)
 
 
 class ArgumentSpec(object):
