fd501057522c5683ec1217b5e113b20457d5c1f1
xuxiaowei0512 <786018072@qq.com>
2019-06-22 16:38:23 +0800

    Update the regular expression to match the more accurate

--- lib/ansible/modules/network/cloudengine/ce_vxlan_gateway.py
+++ lib/ansible/modules/network/cloudengine/ce_vxlan_gateway.py
@@ -400,7 +400,7 @@ class VxlanGateway(object):
         flags = list()
         exp = r" | ignore-case section include ^#\s+dfs-group"
         if self.vpn_instance:
-            exp += r"|^#\s+ip vpn-instance %s$" % self.vpn_instance
+            exp += r"|^#\s+ip vpn-instance %s" % self.vpn_instance
         if self.vbdif_name:
             exp += r"|^#\s+interface %s" % self.vbdif_name
         flags.append(exp)
