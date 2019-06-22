a694adb042c7e869787fa8fc90942eafb7e1a339
xuxiaowei0512 <786018072@qq.com>
2019-06-22 18:24:22 +0800

    update to fix a bug
    Signed-off-by: xuxiaowei0512 <786018072@qq.com>

--- lib/ansible/modules/network/cloudengine/ce_interface_ospf.py
+++ lib/ansible/modules/network/cloudengine/ce_interface_ospf.py
@@ -424,7 +424,7 @@ class InterfaceOSPF(object):
 
         # get process base info
         root = ElementTree.fromstring(xml_str)
-        ospfsite = root.find("data/ospfv2/ospfv2comm/ospfSites/ospfSite")
+        ospfsite = root.find("ospfv2/ospfv2comm/ospfSites/ospfSite")
         if not ospfsite:
             self.module.fail_json(msg="Error: ospf process does not exist.")
 
@@ -435,7 +435,7 @@ class InterfaceOSPF(object):
         # get areas info
         ospf_info["areaId"] = ""
         areas = root.find(
-            "data/ospfv2/ospfv2comm/ospfSites/ospfSite/areas/area")
+            "ospfv2/ospfv2comm/ospfSites/ospfSite/areas/area")
         if areas:
             for area in areas:
                 if area.tag == "areaId":
@@ -445,7 +445,7 @@ class InterfaceOSPF(object):
         # get interface info
         ospf_info["interface"] = dict()
         intf = root.find(
-            "data/ospfv2/ospfv2comm/ospfSites/ospfSite/areas/area/interfaces/interface")
+            "ospfv2/ospfv2comm/ospfSites/ospfSite/areas/area/interfaces/interface")
         if intf:
             for attr in intf:
                 if attr.tag in ["ifName", "networkType",
