bb2c7cf57ddc127dcfad4050027a0dfbf97fcdef
Fred-sun <37327967+Fred-sun@users.noreply.github.com>
2019-06-26 00:32:11 +0800

    Update azure_rm_sqlfirewallrule related document (#58265)
    * Update azure_rm_sqlfirewallrule related documentation

--- lib/ansible/modules/cloud/azure/azure_rm_sqlfirewallrule.py
+++ lib/ansible/modules/cloud/azure/azure_rm_sqlfirewallrule.py
@@ -17,7 +17,7 @@ DOCUMENTATION = '''
 ---
 module: azure_rm_sqlfirewallrule
 version_added: "2.7"
-short_description: Manage Firewall Rule instance.
+short_description: Manage Firewall Rule instance
 description:
     - Create, update and delete instance of Firewall Rule.
 
@@ -36,24 +36,25 @@ options:
         required: True
     start_ip_address:
         description:
-            - The start IP address of the firewall rule. Must be IPv4 format. Use value C(0.0.0.0) to represent all Azure-internal IP addresses.
+            - The start IP address of the firewall rule.
+            - Must be IPv4 format. Use value C(0.0.0.0) to represent all Azure-internal IP addresses.
     end_ip_address:
         description:
-            - "The end IP address of the firewall rule. Must be IPv4 format. Must be greater than or equal to startIpAddress. Use value C(0.0.0.0) to represe
-               nt all Azure-internal IP addresses."
+            - The end IP address of the firewall rule.
+            - Must be IPv4 format. Must be greater than or equal to I(start_ip_address). Use value C(0.0.0.0) to represent all Azure-internal IP addresses.
     state:
-      description:
-        - Assert the state of the SQL Database. Use C(present) to create or update an SQL Database and C(absent) to delete it.
-      default: present
-      choices:
-        - absent
-        - present
+        description:
+            - State of the SQL Database. Use C(present) to create or update an SQL Database and C(absent) to delete it.
+        default: present
+        choices:
+            - absent
+            - present
 
 extends_documentation_fragment:
     - azure
 
 author:
-    - "Zim Kalinowski (@zikalino)"
+    - Zim Kalinowski (@zikalino)
 
 '''
 
