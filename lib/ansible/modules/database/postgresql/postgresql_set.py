5937080977ae50823ee68ed5d87a6c4ad2433e29
Andrey Klychkov <aaklychkov@mail.ru>
2019-06-28 16:44:32 +0300

    postgresql modules: remove extra lines from doc that are presented in ext doc fragment (#58501)

--- lib/ansible/modules/database/postgresql/postgresql_set.py
+++ lib/ansible/modules/database/postgresql/postgresql_set.py
@@ -72,15 +72,6 @@ notes:
   not restarted and the value in pg_settings is not updated yet.
 - For some parameters restart of PostgreSQL server is required.
   See official documentation U(https://www.postgresql.org/docs/current/view-pg-settings.html).
-- The default authentication assumes that you are either logging in as or
-  sudo'ing to the postgres account on the host.
-- This module uses psycopg2, a Python PostgreSQL database adapter. You must
-  ensure that psycopg2 is installed on the host before using this module. If
-  the remote host is the PostgreSQL server (which is the default case), then
-  PostgreSQL must also be installed on the remote host. For Ubuntu-based
-  systems, install the postgresql, libpq-dev, and python-psycopg2 packages
-  on the remote host before using this module.
-requirements: [ psycopg2 ]
 author:
 - Andrew Klychkov (@Andersson007)
 extends_documentation_fragment: postgres
