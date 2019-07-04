a40f7c72efed63d630d64f149a2febab96336b41
Andrey Klychkov <aaklychkov@mail.ru>
2019-07-04 14:48:28 +0300

    Unit tests for bytes_to_human, remove commented function

--- test/units/module_utils/common/text/formatters/test_bytes_to_human.py
+++ test/units/module_utils/common/text/formatters/test_bytes_to_human.py
@@ -10,23 +10,6 @@ import pytest
 from ansible.module_utils.common.text.formatters import bytes_to_human
 
 
-# def bytes_to_human(size, isbits=False, unit=None):
-#    base = 'Bytes'
-#    if isbits:
-#        base = 'bits'
-#    suffix = ''
-#
-#    for suffix, limit in sorted(iteritems(SIZE_RANGES), key=lambda item: -item[1]):
-#        if (unit is None and size >= limit) or unit is not None and unit.upper() == suffix[0]:
-#            break
-#
-#    if limit != 1:
-#        suffix += base[0]
-#    else:
-#        suffix = base
-#
-#    return '%.2f %s' % (size / limit, suffix)
-
 @pytest.mark.parametrize(
     'input_data,expected',
     [
