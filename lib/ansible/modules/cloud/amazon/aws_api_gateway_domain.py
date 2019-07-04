553e33afb45e159e6cfada8d62376a9def02c961
Stefan Horning <stefan@hornings.de>
2019-07-04 13:52:24 +0200

    More work on filling the gaps

--- lib/ansible/modules/cloud/amazon/aws_api_gateway_domain.py
+++ lib/ansible/modules/cloud/amazon/aws_api_gateway_domain.py
@@ -19,6 +19,8 @@ description:
      - AWS API Gateway custom domain setups use CloudFront behind the scenenes. So you will get a CloudFront distribution as a result, configured to be aliased with your domain.
 version_added: '2.9'
 requirements: [ boto3 ]
+author:
+    - 'Stefan Horning (@stefanhorning)'
 options:
   domain_name:
     description:
@@ -37,19 +39,24 @@ options:
       - API endpoint configuration for domain. Use EDGE for edge-optimized endpoint, or use REGIONAL or PRIVATE
     default: EDGE
     choices: ['EDGE', 'REGIONAL', 'PRIVATE']
+  domain_mappings:
+    description:
+      - Map your domain base paths to your API GW REST APIs, you previously created. Use provide ID of the API setup and the release stage.
+      - domain_mappings should be a list of dictionaries containing three keys: base_path, rest_api_id and stage.
+      - Example: I([{ base_path: /, rest_api_id: abc123, stage: production }])
+    required: true
+    type: list
   state:
     description:
       - Create or delete custom domain setup
     default: present
     choices: [ 'present', 'absent' ]
-
-author:
-    - 'Stefan Horning (@stefanhorning)'
 extends_documentation_fragment:
     - aws
     - ec2
 notes:
    - Does not create a DNS entry on Route53, for that use the route53 module.
+   - Only supports TLS certificates from AWS ACM that can just be referenced by the ARN, while the AWS API still offers (deprecated) options to add own Certificates.
 '''
 
 EXAMPLES = '''
@@ -59,6 +66,8 @@ EXAMPLES = '''
     certificate_arn: 'arn:aws:acm:us-east-1:1231123123:certificate/8bd89412-abc123-xxxxx'
     security_policy: TLS_1_0
     endpoint_type: EDGE
+    domain_mappings:
+        - { base_path: '/', rest_api_id: abc123, stage: production }
     state: present
   register: api_gw_domain_result
 
@@ -97,17 +106,19 @@ except ImportError:
     # HAS_BOTOCORE taken care of in AnsibleAWSModule
     pass
 
+import copy
 import traceback
+
 from ansible.module_utils.aws.core import AnsibleAWSModule
-from ansible.module_utils.ec2 import (AWSRetry, camel_dict_to_snake_dict)
+from ansible.module_utils.ec2 import (AWSRetry, camel_dict_to_snake_dict, snake_dict_to_camel_dict)
 
 
 def get_domain(module, client):
     domain_name = module.params.get('domain_name')
     result = {}
     try:
-        result['domain_name']   = get_domain_name(client, domain_name)
-        result['path_mapping']  = get_domain_mapping(client, domain_name)
+        result['domain']        = get_domain_name(client, domain_name)
+        result['path_mappings']  = get_domain_mappings(client, domain_name)
     except (botocore.exceptions.ClientError, botocore.exceptions.EndpointConnectionError) as e:
         module.fail_json_aws(e, msg="getting API GW domain")
 
@@ -115,18 +126,39 @@ def get_domain(module, client):
 
 
 def create_domain(module, client):
-    args = module.params
+    domain_args = copy.deepcopy(module.params)
+    domain_args.pop('domain_mappings')
+    domain_args.pop('state')
+    domain_name = domain_args['domain_name']
+    path_mappings = module.params.get('domain_mappings')
+
+    result = {}
     try:
-        res = create_domain_name(client, args)
+        result['domain'] = create_domain_name(client, snake_dict_to_camel_dict(args))
+        result['path_mappings'] = {}
+        for mapping in path_mappings:
+            base_path = mapping.get('base_path', '/')
+            rest_api_id = mapping['rest_api_id']
+            stage = mapping['stage']
+            if !rest_api_id or !stage:
+                module.fail_json('Every domain mapping needs a rest_api_id and stage name')
+
+            result['path_mappings'][base_path] = add_domain_mapping(client, domain_name, base_path, rest_api_id, stage)
+
     except (botocore.exceptions.ClientError, botocore.exceptions.EndpointConnectionError) as e:
         module.fail_json_aws(e, msg="creating API GW domain")
-    return res
+
+    return result
 
 
 def update_domain(module, client):
-    args = module.params
+    domain_args = copy.deepcopy(module.params)
+    domain_args.pop('domain_mappings')
+    domain_args.pop('state')
+    domain_name = domain_args['domain_name']
+
     try:
-        res = update_domain_name(client, args)
+        res = update_domain_name(client, domain_name, snake_dict_to_camel_dict(args))
     except (botocore.exceptions.ClientError, botocore.exceptions.EndpointConnectionError) as e:
         module.fail_json_aws(e, msg="updating API GW domain")
     return res
@@ -150,7 +182,7 @@ def get_domain_name(client, domain_name):
 
 
 @AWSRetry.backoff(**retry_params)
-def get_domain_mapping(client, domain_name):
+def get_domain_mappings(client, domain_name):
     return client.get_base_path_mappings(domainName=domain_name, limit=200)
 
 
@@ -160,8 +192,18 @@ def create_domain_name(client, **args):
 
 
 @AWSRetry.backoff(**retry_params)
-def update_domain_name(client, **args):
-    return client.update_domain_name(args)
+def add_domain_mapping(client, domain_name, base_path, rest_api_id, stage):
+    return client.create_base_path_mapping(domainName=domain_name, basePath=base_path, restApiId=rest_api_id, stage=stage)
+
+
+@AWSRetry.backoff(**retry_params)
+def update_domain_name(client, domain_name, **args):
+    patch_operations = []
+
+    for key, value in args.items():
+        patch_operations = << { "op": "replace", "path": "/" + key, "value": value }
+
+    return client.update_domain_name(domainName=domain_name, patchOperations=patch_operations)
 
 
 @AWSRetry.backoff(**retry_params)
@@ -175,13 +217,13 @@ def main():
         certificate_arn=dict(type='str', required=True),
         security_policy=dict(type='str', default='TLS_1_2', choices=['TLS_1_0', 'TLS_1_2']),
         endpoint_type=dict(type='str', default='EDGE', choices=['EDGE', 'REGIONAL', 'PRIVATE']),
-        domain_mapping=dict(type='list', required=True)
+        domain_mappings=dict(type='list', required=True),
         state=dict(type='str', default='present', choices=['present', 'absent'])
     )
 
     module = AnsibleAWSModule(
         argument_spec=argument_spec,
-        supports_check_mode=False,
+        supports_check_mode=False
     )
 
     client = module.client('apigateway')
@@ -189,7 +231,6 @@ def main():
     state = module.params.get('state')
     changed = False
 
-    # TODO: add base path mapping creation/deletion
     if state == "present":
         existing_domain = get_domain(module, client)
         # if domain exists use update
