ada5336fee41219ee24195e0d2f2c96f131be23f
Alvaro A <axarriola@users.noreply.github.com>
2019-07-04 12:40:27 +0200

    Corrected typos

--- lib/ansible/modules/net_tools/netbox/netbox_vm.py
+++ lib/ansible/modules/net_tools/netbox/netbox_vm.py
@@ -91,8 +91,7 @@ options:
       - Use C(present) or C(absent) for adding or removing.
     choices: [ absent, present ]
     default: present
-    type: string
-    default: 'present'
+    type: str
   validate_certs:
     description:
       - If C(no), SSL certificates will not be validated. This should only be used on personally controlled sites using self-signed certificates.
