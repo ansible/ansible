1a36ea3d5725c89cd3c71b8b17633d8af5161c18
Andrey Klychkov <aaklychkov@mail.ru>
2019-07-04 10:48:17 +0300

    postgresql_query: add autocommit parameter, cosmetic fix

--- lib/ansible/modules/database/postgresql/postgresql_query.py
+++ lib/ansible/modules/database/postgresql/postgresql_query.py
@@ -200,10 +200,7 @@ def main():
             module.fail_json(msg="Cannot read file '%s' : %s" % (path_to_script, to_native(e)))
 
     conn_params = get_conn_params(module, module.params)
-    if autocommit:
-        db_connection = connect_to_db(module, conn_params, autocommit=True)
-    else:
-        db_connection = connect_to_db(module, conn_params, autocommit=False)
+    db_connection = connect_to_db(module, conn_params, autocommit=autocommit)
     cursor = db_connection.cursor(cursor_factory=DictCursor)
 
     # Prepare args:
