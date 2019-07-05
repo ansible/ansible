1cfb5b55ff5e689cb46611f85528580dfc1d2ec8
xuxiaowei0512 <35824522+xuxiaowei0512@users.noreply.github.com>
2019-07-06 06:37:19 +0800

    add units test to module ce_lacp.

--- test/units/modules/network/cloudengine/ce_module.py
+++ test/units/modules/network/cloudengine/ce_module.py
@@ -29,6 +29,7 @@ from units.modules.utils import set_module_args as _set_module_args
 fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
 fixture_data = {}
 
+
 def load_fixture(module_name, name, device=''):
     path = os.path.join(fixture_path, module_name, device, name)
     if not os.path.exists(path):
