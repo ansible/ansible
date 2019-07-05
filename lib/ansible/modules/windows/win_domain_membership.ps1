067f8f2304d1e61dd935b587a61dd08924fc1650
Shachaf92 <shachaf.gold@gmail.com>
2019-07-05 03:05:01 +0300

    removed return from set-workgroup

--- lib/ansible/modules/windows/win_domain_membership.ps1
+++ lib/ansible/modules/windows/win_domain_membership.ps1
@@ -154,8 +154,6 @@ Function Set-Workgroup {
     if ($swg_result.ReturnValue -ne 0) {
         Fail-Json -obj $result -message "failed to set workgroup through WMI, return value: $($swg_result.ReturnValue)"
     }
-
-    return $swg_result
 }
 
 Function Join-Workgroup {
