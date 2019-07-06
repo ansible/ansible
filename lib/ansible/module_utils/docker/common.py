103f21b9c56ee02efdad751ab240d0b963c39dd4
Felix Fontein <felix@fontein.de>
2019-07-06 13:38:25 +0200

    Add method to validate docker tags.

--- lib/ansible/module_utils/docker/common.py
+++ lib/ansible/module_utils/docker/common.py
@@ -123,10 +123,19 @@ if not HAS_DOCKER_PY:
 
 
 def is_image_name_id(name):
-    """Checks whether the given image name is in fact an image ID (hash)."""
+    """Check whether the given image name is in fact an image ID (hash)."""
     if re.match('^sha256:[0-9a-fA-F]{64}$', name):
         return True
     return False
+
+
+def is_valid_tag(tag, allow_empty=False):
+    """Check whether the given string is a valid docker tag name."""
+    if not tag:
+        return allow_empty
+    # See here ("Extended description") for a definition what tags can be:
+    # https://docs.docker.com/engine/reference/commandline/tag/
+    return re.match('^[a-zA-Z0-9_][a-zA-Z0-9_.-]{0-127}$', tag)
 
 
 def sanitize_result(data):
