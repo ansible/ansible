5842e43dbaaf44a1e72f15a3f241fe93a48113f6
kvprasoon <prasoonkarunan5@hotmail.com>
2019-07-07 00:28:36 +0530

    fix test and add doc

--- test/integration/targets/win_pester/files/test04.tests.ps1
+++ test/integration/targets/win_pester/files/test04.tests.ps1
@@ -5,12 +5,12 @@ Param(
 
 Describe "Process should exist" {
     it "Process $Process should be running" {
-        Get-Process -Name $Process -ErrorAction SilentlyContinue| Should -Not -BeNullOrEmpty
+        Get-Process -Name $Process -ErrorAction SilentlyContinue| Should Not BeNullOrEmpty
     }
 }
 
 Describe "Service should exist" -tag Service {
     it "Service $Service should exist" {
-        Get-Service -Name $Service-ErrorAction SilentlyContinue | Should -Not -BeNullOrEmpty
+        Get-Service -Name $Service-ErrorAction SilentlyContinue | Should Not BeNullOrEmpty
     }
 }
