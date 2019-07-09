4590c653e972105425d69ad05d9769b5a24af935
Daniel A. Ramaley <dramaley@lnx-consoles-prod.drake.edu>
2019-07-09 14:17:04 -0500

    Fixed version_added from 2.9 to 2.8.

--- lib/ansible/modules/monitoring/nagios.py
+++ lib/ansible/modules/monitoring/nagios.py
@@ -62,7 +62,7 @@ options:
      - Comment for C(downtime) action.
     default: Scheduling downtime
   start:
-    version_added: "2.9"
+    version_added: "2.8"
     description:
       - When downtime should start, in time_t format (epoch seconds).
   minutes:
