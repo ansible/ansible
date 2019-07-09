be96b7f5810be51b416aeffde96dbc982483b228
smile37773 <714389288@qq.com>
2019-07-09 15:08:36 +0800

    fix

--- lib/ansible/modules/cloud/azure/azure_rm_recoveryservicesvault.py
+++ lib/ansible/modules/cloud/azure/azure_rm_recoveryservicesvault.py
@@ -125,8 +125,8 @@ id:
     - Resource Id represents the complete path to the resource.
   returned: always
   type: str
-  sample: /subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/myResourceGroup
-  /providers/Microsoft.RecoveryServices/vaults/myVault
+  sample: "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/myResourceGroup
+  /providers/Microsoft.RecoveryServices/vaults/myVault"
 
 '''
 
