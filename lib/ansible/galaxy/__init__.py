1afd0bdd837a3f604b90c5935d25d4c928bb0cfa
Jordan Borean <jborean93@gmail.com>
2019-07-10 05:47:25 +1000

    ansible-galaxy: add collection sub command (#57106)
    * ansible-galaxy: add collection init sub command
    
    * Fix changelog and other sanity issues
    
    * Slim down skeleton structure, fix encoding issue on template
    
    * Fix doc generation code to include sub commands
    
    * Added build step
    
    * Tidy up the build action
    
    * Fixed up doc changes and slight testing tweaks
    
    * Re-organise tests to use pytest
    
    * Added publish step and fixed up issues after working with Galaxy
    
    * Unit test improvments
    
    * Fix unit test on 3.5
    
    * Add remaining build tests
    
    * Test fixes, make the integration tests clearer to debug on failures
    
    * Removed unicode name tests until I've got further clarification
    
    * Added publish unit tests
    
    * Change expected length value
    
    * Added collection install steps, tests forthcoming
    
    * Added unit tests for collection install entrypoint
    
    * Added some more tests for collection install
    
    * follow proper encoding rules and added more tests
    
    * Add remaining tests
    
    * tidied up tests and code based on review
    
    * exclude pre-release versions from galaxy API

--- lib/ansible/galaxy/__init__.py
+++ lib/ansible/galaxy/__init__.py
@@ -47,7 +47,10 @@ class Galaxy(object):
 
         # load data path for resource usage
         this_dir, this_filename = os.path.split(__file__)
-        type_path = context.CLIARGS.get('role_type', "default")
+        type_path = context.CLIARGS.get('role_type', 'default')
+        if type_path == 'default':
+            type_path = os.path.join(type_path, context.CLIARGS.get('type'))
+
         self.DATA_PATH = os.path.join(this_dir, 'data', type_path)
 
     @property
