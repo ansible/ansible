520af5cb4785b51950e1f0f0876e26745e0054a9
Matt Clay <matt@mystile.com>
2019-07-09 22:50:15 -0700

    Sanity test fixes for future collections support.

--- test/runner/lib/sanity/compile.py
+++ test/runner/lib/sanity/compile.py
@@ -18,6 +18,7 @@ from lib.util import (
     find_python,
     read_lines_without_comments,
     parse_to_list_of_dict,
+    INSTALL_ROOT,
 )
 
 from lib.config import (
@@ -50,7 +51,7 @@ class CompileTest(SanityMultipleVersion):
         if not paths:
             return SanitySkipped(self.name, python_version=python_version)
 
-        cmd = [find_python(python_version), 'test/sanity/compile/compile.py']
+        cmd = [find_python(python_version), os.path.join(INSTALL_ROOT, 'test/sanity/compile/compile.py')]
 
         data = '\n'.join(paths)
 
