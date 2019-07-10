0239755f1f2e305f1874906425931a197388c302
Thomas Wouters <thomaswouters@gmail.com>
2019-07-10 17:20:44 +0200

    Fix #40879 - add quotes to column grants

--- test/integration/targets/mysql_user/files/create-col-priv-db.sql
+++ test/integration/targets/mysql_user/files/create-col-priv-db.sql
@@ -2,4 +2,4 @@ DROP DATABASE IF EXISTS `colprivdb`;
 CREATE DATABASE `colprivdb`;
 USE `colprivdb`;
 DROP TABLE IF EXISTS `t1`;
-CREATE TABLE `t1` (`c1` int(1), `c2` int(1), `c3` int(1));
+CREATE TABLE `t1` (`c1` int(1), `c2` int(1), `c3` int(1), `ssl` int(1));
