2564f5c82cdc6d380ff3071753dd4454c067771f
shashik <shashi@alethea.in>
2019-07-11 19:47:32 +0530

    icx units module

--- test/units/modules/network/eric_eccli/test_eccli_command.py
+++ test/units/modules/network/eric_eccli/test_eccli_command.py
@@ -21,7 +21,7 @@ __metaclass__ = type
 
 import json
 
-from ansible.compat.tests.mock import patch
+from units.compat.mock import patch
 # from ansible.module_utils.basic import get_timestamp
 from ansible.modules.network.icx import icx_command
 from units.modules.utils import set_module_args
