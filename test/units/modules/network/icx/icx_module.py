d17eb889e663e9a2eb16368c3237ec0632eb2575
shashik <shashi@alethea.in>
2019-07-11 19:22:23 +0530

    icx test units module

--- test/units/modules/network/icx/icx_module.py
+++ test/units/modules/network/icx/icx_module.py
@@ -1,5 +1,21 @@
-# Copyright: (c) 2019, Ansible Project
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# (c) 2016 Red Hat Inc.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# Make coding more python3-ish
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
@@ -40,8 +56,8 @@ class TestICXModule(ModuleTestCase):
     def get_running_config(self, compare=None):
         if compare is not None:
             diff = compare
-        elif os.environ.get('ANSIBLE_CHECK_ICX_RUNNING_CONFIG') is not None:
-            if os.environ.get('ANSIBLE_CHECK_ICX_RUNNING_CONFIG') == 'False':
+        elif os.environ.get('CHECK_RUNNING_CONFIG') is not None:
+            if os.environ.get('CHECK_RUNNING_CONFIG') == 'False':
                 diff = False
             else:
                 diff = True
