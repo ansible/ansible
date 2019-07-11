069265ae71ce7969dc82d40fe6e2d52c7fa3ccd2
Abhijeet Kasurde <akasurde@redhat.com>
2019-07-11 10:59:24 +0530

    Refactor service now module space
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/module_utils/service_now.py
+++ lib/ansible/module_utils/service_now.py
@@ -7,7 +7,6 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 import traceback
-
 from ansible.module_utils.basic import missing_required_lib
 
 # Pull in pysnow
