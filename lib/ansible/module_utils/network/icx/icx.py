df8b3f9d69e90a6298fd3abe2513caaa9911f0b5
shashik <shashi@alethea.in>
2019-07-11 15:53:26 +0530

    module_utils

--- lib/ansible/module_utils/network/icx/icx.py
+++ lib/ansible/module_utils/network/icx/icx.py
@@ -25,6 +25,10 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
 import json
 
 from ansible.module_utils._text import to_text
