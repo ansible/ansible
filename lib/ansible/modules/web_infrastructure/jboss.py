75a6c91377c93bf4fc91767363f6771c4bdc7380
Andrey Klychkov <aaklychkov@mail.ru>
2019-07-15 19:17:08 +0300

    jboss module: add check mode support and integration tests (#58959)
    * git war file and wildfly archive from S3
    * create setup_wildfly_server role for integration tests

--- lib/ansible/modules/web_infrastructure/jboss.py
+++ lib/ansible/modules/web_infrastructure/jboss.py
@@ -28,6 +28,8 @@ options:
   src:
     description:
       - The remote path of the application ear or war to deploy.
+      - Required when I(state=present).
+      - Ignored when I(state=absent).
     type: path
   deploy_path:
     default: /var/lib/jbossas/standalone/deployments
@@ -42,25 +44,28 @@ options:
     type: str
 notes:
   - The JBoss standalone deployment-scanner has to be enabled in standalone.xml
+  - The module can wait until I(deployment) file is deployed/undeployed by deployment-scanner.
+    Duration of waiting time depends on scan-interval parameter from standalone.xml.
   - Ensure no identically named application is deployed through the JBoss CLI
 author:
   - Jeroen Hoekx (@jhoekx)
 """
 
 EXAMPLES = r"""
-- name: Deploy a hello world application
+- name: Deploy a hello world application to the default deploy_path
   jboss:
     src: /tmp/hello-1.0-SNAPSHOT.war
     deployment: hello.war
     state: present
 
-- name: Update the hello world application
+- name: Update the hello world application to the non-default deploy_path
   jboss:
     src: /tmp/hello-1.1-SNAPSHOT.war
+    deploy_path: /opt/wildfly/deployment
     deployment: hello.war
     state: present
 
-- name: Undeploy the hello world application
+- name: Undeploy the hello world application from the default deploy_path
   jboss:
     deployment: hello.war
     state: absent
@@ -72,6 +77,9 @@ import os
 import shutil
 import time
 from ansible.module_utils.basic import AnsibleModule
+
+
+DEFAULT_DEPLOY_PATH = '/var/lib/jbossas/standalone/deployments'
 
 
 def is_deployed(deploy_path, deployment):
@@ -91,10 +99,11 @@ def main():
         argument_spec=dict(
             src=dict(type='path'),
             deployment=dict(type='str', required=True),
-            deploy_path=dict(type='path', default='/var/lib/jbossas/standalone/deployments'),
+            deploy_path=dict(type='path', default=DEFAULT_DEPLOY_PATH),
             state=dict(type='str', choices=['absent', 'present'], default='present'),
         ),
-        required_if=[('state', 'present', ('src',))]
+        required_if=[('state', 'present', ('src',))],
+        supports_check_mode=True
     )
 
     result = dict(changed=False)
@@ -107,11 +116,30 @@ def main():
     if not os.path.exists(deploy_path):
         module.fail_json(msg="deploy_path does not exist.")
 
+    if state == 'absent' and src:
+        module.warn('Parameter src is ignored when state=absent')
+    elif state == 'present' and not os.path.exists(src):
+        module.fail_json(msg='Source file %s does not exist.' % src)
+
     deployed = is_deployed(deploy_path, deployment)
 
+    # === when check_mode ===
+    if module.check_mode:
+        if state == 'present':
+            if not deployed:
+                result['changed'] = True
+
+            elif deployed:
+                if module.sha1(src) != module.sha1(os.path.join(deploy_path, deployment)):
+                    result['changed'] = True
+
+        elif state == 'absent' and deployed:
+            result['changed'] = True
+
+        module.exit_json(**result)
+    # =======================
+
     if state == 'present' and not deployed:
-        if not os.path.exists(src):
-            module.fail_json(msg='Source file %s does not exist.' % src)
         if is_failed(deploy_path, deployment):
             # Clean up old failed deployment
             os.remove(os.path.join(deploy_path, "%s.failed" % deployment))
