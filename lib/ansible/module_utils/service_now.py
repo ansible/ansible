28100471e2bd4df3da97b3fba7abd4e2069adeca
Paul Knight <paul.knight@state.de.us>
2019-07-15 15:16:24 -0400

    blah

--- lib/ansible/module_utils/service_now.py
+++ lib/ansible/module_utils/service_now.py
@@ -88,8 +88,12 @@ class ServiceNowClient(object):
     def snow_argument_spec():
         return dict(
             instance=dict(type='str', required=True),
-            username=dict(type='str', required=True, no_log=True),
-            password=dict(type='str', required=True, no_log=True),
-            client_id=dict(type='str', no_log=True),
-            client_secret=dict(type='str', no_log=True),
+            username=dict(type='str', required=True),
+            password=dict(type='str', required=True),
+            client_id=dict(type='str'),
+            client_secret=dict(type='str'),
+#            username=dict(type='str', required=True, no_log=True),
+#            password=dict(type='str', required=True, no_log=True),
+#            client_id=dict(type='str', no_log=True),
+#            client_secret=dict(type='str', no_log=True),
         )
