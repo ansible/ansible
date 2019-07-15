f3a84efadcbc053afdfcba01662b3616ad978374
Paul Knight <paul.knight@state.de.us>
2019-07-15 17:25:13 -0400

    Remove NO_LOG from username

--- lib/ansible/module_utils/service_now.py
+++ lib/ansible/module_utils/service_now.py
@@ -89,7 +89,7 @@ class ServiceNowClient(object):
     def snow_argument_spec():
         return dict(
             instance=dict(type='str', required=True),
-            username=dict(type='str', required=True, no_log=True),
+            username=dict(type='str', required=True),
             password=dict(type='str', required=True, no_log=True),
             client_id=dict(type='str', no_log=True),
             client_secret=dict(type='str', no_log=True),
