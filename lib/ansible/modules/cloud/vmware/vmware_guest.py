18fc915b1560a0fac3aba7728ce7d3a1b7edb43c
Noe Gonzalez <noe.a.gonzalez@gmail.com>
2019-07-14 21:39:54 -0700

    [2.8] VMware: Fix vmware_guest cloning bug
    * fix vmware_guest cloning bug
    * added tests for resizing disks during clone
    
    (cherry picked from commit 3a5d13b0d761bb5ac5a83d14daa37848e72ec857)
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/cloud/vmware/vmware_guest.py
+++ lib/ansible/modules/cloud/vmware/vmware_guest.py
@@ -1841,7 +1841,9 @@ class PyVmomiHelper(PyVmomi):
                 continue
             elif vm_obj is None or self.params['template']:
                 # We are creating new VM or from Template
-                diskspec.fileOperation = vim.vm.device.VirtualDeviceSpec.FileOperation.create
+                # Only create virtual device if not backed by vmdk in original template
+                if diskspec.device.backing.fileName == '':
+                    diskspec.fileOperation = vim.vm.device.VirtualDeviceSpec.FileOperation.create
 
             # which datastore?
             if expected_disk_spec.get('datastore'):
