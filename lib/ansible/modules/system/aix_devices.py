5115c05b47988bb32a944f93946124928cacaf9e
David Little <david.n.little@gmail.com>
2019-07-15 14:58:04 -0500

    If device==all, `cfgmgr`, else `cfgmgr -l device`

--- lib/ansible/modules/system/aix_devices.py
+++ lib/ansible/modules/system/aix_devices.py
@@ -193,16 +193,17 @@ def discover_device(module, device):
     """ Discover AIX devices."""
     cfgmgr_cmd = module.get_bin_path('cfgmgr', True)
 
-    if device is not None:
-        device = "-l %s" % device
-
+    # device is mandatory according to doc spec,
+    #  if  device == 'all', run bare cfgmgr.  else, scan specific device
+    if device.lower() == 'all':
+        cmd = [cfgmgr_cmd]
     else:
-        device = ''
+        cmd = ["%s" % cfgmgr_cmd, "%s" % device]
 
     changed = True
     msg = ''
     if not module.check_mode:
-        rc, cfgmgr_out, err = module.run_command(["%s" % cfgmgr_cmd, "%s" % device])
+        rc, cfgmgr_out, err = module.run_command(cmd)
         changed = True
         msg = cfgmgr_out
 
