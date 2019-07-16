5a71ef14dab63928e66d8729d3573e1658d62e6a
Serge ALEXANDRE <sa@broadsoftware.com>
2019-07-16 22:27:33 +0200

    k8s: Fix a bug when manifest file ends with '---'

--- lib/ansible/module_utils/k8s/raw.py
+++ lib/ansible/module_utils/k8s/raw.py
@@ -162,13 +162,14 @@ class KubernetesRawModule(KubernetesAnsibleModule):
 
         flattened_definitions = []
         for definition in self.resource_definitions:
-            kind = definition.get('kind', self.kind)
-            api_version = definition.get('apiVersion', self.api_version)
-            if kind.endswith('List'):
-                resource = self.find_resource(kind, api_version, fail=False)
-                flattened_definitions.extend(self.flatten_list_kind(resource, definition))
-            else:
-                resource = self.find_resource(kind, api_version, fail=True)
+            if definition is not None:
+                kind = definition.get('kind', self.kind)
+                api_version = definition.get('apiVersion', self.api_version)
+                if kind.endswith('List'):
+                    resource = self.find_resource(kind, api_version, fail=False)
+                    flattened_definitions.extend(self.flatten_list_kind(resource, definition))
+                else:
+                    resource = self.find_resource(kind, api_version, fail=True)
                 flattened_definitions.append((resource, definition))
 
         for (resource, definition) in flattened_definitions:
