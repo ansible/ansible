6491c5cdd7ddf4270efb04bb27c35a483d48458c
Xu Yuandong <xuyuandong@huawei.com>
2019-07-18 03:14:48 +0800

    Backport/2.8/58226 ce_vxlan_gateway: update the regular expression to match the more acc… (#58557)
    * ce_vxlan_gateway: update the regular expression to match the more accurate. (#58226)
    
    * Update the regular expression to match the more accurate
    
    * Update the regular expression to match the more accurate
    
    * Update the regular expression to match the more accurate
    
    (cherry picked from commit 2f91266aa9b5ac682889829505f4c1d738057942)
    
    * add a changlog fragment to PR 58557.
    
    * Update 58226-update-ce_vxlan_gateway-to-fix-a-bug.yml

--- lib/ansible/modules/network/cloudengine/ce_vxlan_gateway.py
+++ lib/ansible/modules/network/cloudengine/ce_vxlan_gateway.py
@@ -398,11 +398,11 @@ class VxlanGateway(object):
         """get current configuration"""
 
         flags = list()
-        exp = " | ignore-case section include dfs-group"
+        exp = r" | ignore-case section include ^#\s+dfs-group"
         if self.vpn_instance:
-            exp += "|^ip vpn-instance %s$" % self.vpn_instance
+            exp += r"|^#\s+ip vpn-instance %s" % self.vpn_instance
         if self.vbdif_name:
-            exp += "|^interface %s$" % self.vbdif_name
+            exp += r"|^#\s+interface %s" % self.vbdif_name
         flags.append(exp)
         return self.get_config(flags)
 
