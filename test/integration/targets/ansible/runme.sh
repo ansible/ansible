a39b721db50d9e12ec04d8628a0cd593f66d2880
Brian Coca <bcoca@users.noreply.github.com>
2019-07-17 10:39:30 -0400

    fixes to config manager (#58530)
    * skip unreadable ansible.cfg
    
    * all types should check for type
    
    * patch access for tests and fix tests that relied on missing files not being checked

--- test/integration/targets/ansible/runme.sh
+++ test/integration/targets/ansible/runme.sh
@@ -14,7 +14,7 @@ ANSIBLE_REMOTE_USER=administrator ansible-config dump| grep 'DEFAULT_REMOTE_USER
 ansible-config list | grep 'DEFAULT_REMOTE_USER'
 
 # 'view' command must fail when config file is missing or has an invalid file extension
-ansible-config view -c ./ansible-non-existent.cfg 2> err1.txt || grep -Eq '(FileNotFoundError|IOError): \[Errno [0-9]+\] No such file or directory' err1.txt || (cat err*.txt; rm -f err1.txt; exit 1)
+ansible-config view -c ./ansible-non-existent.cfg 2> err1.txt || grep -Eq 'ERROR! The provided configuration file is missing or not accessible:' err1.txt || (cat err*.txt; rm -f err1.txt; exit 1)
 ansible-config view -c ./no-extension 2> err2.txt || grep -q 'Unsupported configuration file extension' err2.txt || (cat err2.txt; rm -f err*.txt; exit 1)
 rm -f err*.txt
 
