2fee251229fdfcc5473039b93d51a23beed8e0b6
Denis Afonso <denisra@users.noreply.github.com>
2019-07-19 06:21:54 +0100

    cloudformation_facts: Fixes PhysicalResourceId KeyError(#55990)
    Fixes: #38033

--- lib/ansible/modules/cloud/amazon/cloudformation_facts.py
+++ lib/ansible/modules/cloud/amazon/cloudformation_facts.py
@@ -256,7 +256,7 @@ class CloudFormationServiceManager:
 def to_dict(items, key, value):
     ''' Transforms a list of items to a Key/Value dictionary '''
     if items:
-        return dict(zip([i[key] for i in items], [i[value] for i in items]))
+        return dict(zip([i.get(key) for i in items], [i.get(value) for i in items]))
     else:
         return dict()
 
