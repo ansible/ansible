50a937aaac17e17ead88552e40acdb56c8dd9df5
Steven Barre <steven@stevenbarre.com>
2019-07-19 00:13:51 -0700

    Only convert ints to strings, leave lists alone

--- lib/ansible/modules/system/iptables.py
+++ lib/ansible/modules/system/iptables.py
@@ -483,11 +483,13 @@ def append_param(rule, param, flag, is_list):
         for item in param:
             append_param(rule, item, flag, False)
     else:
-        if isinstance(param, str):
+        if isinstance(param, int):
+            rule.extend([flag, str(param)])
+        elif param is not None:
             if param[0] == '!':
                 rule.extend(['!', flag, param[1:]])
-        elif param is not None:
-            rule.extend([flag, str(param)])
+            else
+                rule.extend([flag, param])
 
 
 def append_tcp_flags(rule, param, flag):
