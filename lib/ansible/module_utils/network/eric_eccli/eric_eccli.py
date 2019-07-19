bde07c4f5132a4afbcdad0147d636ba4b23853b1
Cheng You <cheng.you@ericsson.com>
2019-07-19 05:10:16 +0200

    modify code

--- lib/ansible/module_utils/network/eric_eccli/eric_eccli.py
+++ lib/ansible/module_utils/network/eric_eccli/eric_eccli.py
@@ -3,6 +3,9 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
 
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 import json
 
 from ansible.module_utils._text import to_text
