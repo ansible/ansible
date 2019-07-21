bf1164b4d534a2ac75e880cf89f5dfa6b7a3f2d2
Micah Hunsberger <mhuns14@gmail.com>
2019-07-21 19:08:36 -0400

    win_hosts to use Ansible.Basic CSharp Util and better diff support (#58600)
    * switch win_hosts to use csharp util
    
    * update win_hosts doc to match doc guide
    
    * changed linking format for option values

--- lib/ansible/modules/windows/win_hosts.py
+++ lib/ansible/modules/windows/win_hosts.py
@@ -18,62 +18,70 @@ version_added: '2.8'
 short_description: Manages hosts file entries on Windows.
 description:
   - Manages hosts file entries on Windows.
-  - Maps IPv4 or IPv6 addresses to canonical names
-  - Adds, removes, or sets cname records for ip and hostname pairs
-  - Modifies %windir%\system32\drivers\etc\hosts.
+  - Maps IPv4 or IPv6 addresses to canonical names.
+  - Adds, removes, or sets cname records for ip and hostname pairs.
+  - Modifies %windir%\\system32\\drivers\\etc\\hosts.
 options:
   state:
     description:
       - Whether the entry should be present or absent.
-      - If only C(canonical_name) is provided when C(state=absent), then
+      - If only I(canonical_name) is provided when C(state=absent), then
         all hosts entries with the canonical name of I(canonical_name)
         will be removed.
-      - If only C(ip_address) is provided when C(state=absent), then all
+      - If only I(ip_address) is provided when C(state=absent), then all
         hosts entries with the ip address of I(ip_address) will be removed.
-      - If C(ip_address) and C(canonical_name) are both omitted when
+      - If I(ip_address) and I(canonical_name) are both omitted when
         C(state=absent), then all hosts entries will be removed.
     choices:
       - absent
       - present
     default: present
+    type: str
   canonical_name:
     description:
       - A canonical name for the host entry.
       - required for C(state=present).
+    type: str
   ip_address:
     description:
       - The ip address for the host entry.
       - Can be either IPv4 (A record) or IPv6 (AAAA record).
       - Required for C(state=present).
+    type: str
   aliases:
     description:
       - A list of additional names (cname records) for the host entry.
       - Only applicable when C(state=present).
+    type: list
   action:
     choices:
       - add
       - remove
       - set
     description:
-      - Controls the behavior of C(aliases).
+      - Controls the behavior of I(aliases).
       - Only applicable when C(state=present).
       - If C(add), each alias in I(aliases) will be added to the host entry.
       - If C(set), each alias in I(aliases) will be added to the host entry,
         and other aliases will be removed from the entry.
     default: set
+    type: str
 author:
   - Micah Hunsberger (@mhunsber)
 notes:
   - Each canonical name can only be mapped to one IPv4 and one IPv6 address.
-    If C(canonical_name) is provided with C(state=present) and is found
+    If I(canonical_name) is provided with C(state=present) and is found
     to be mapped to another IP address that is the same type as, but unique
-    from C(ip_address), then C(canonical_name) and all C(aliases) will
+    from I(ip_address), then I(canonical_name) and all I(aliases) will
     be removed from the entry and added to an entry with the provided IP address.
-  - Each alias can only be mapped to one canonical name. If C(aliases) is provided
+  - Each alias can only be mapped to one canonical name. If I(aliases) is provided
     with C(state=present) and an alias is found to be mapped to another canonical
-    name, then the alias will be removed from the entry and added to or removed
-    from (based on I(action)) an entry with the provided canonical name.
-  - See also M(win_template), M(win_file), M(win_copy)
+    name, then the alias will be removed from the entry and either added to or removed
+    from (depending on I(action)) an entry with the provided canonical name.
+seealso:
+  - module: win_template
+  - module: win_file
+  - module: win_copy
 '''
 
 EXAMPLES = r'''
