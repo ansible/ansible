79eca9c8fb57b6e043e8ea72d6ab144ab5416607
Matt Clay <matt@mystile.com>
2019-07-22 19:24:48 -0700

    Initial ansible-test support for collections. (#59197)
    * Initial ansible-test support for collections.
    * Include cloud config in delegation payload.
    * Add missing types import and fix `t` shadowing.
    * Fix plugin traceback when config_path not set.
    * Fix encoding issues.
    * Remove unused imports.
    * More encoding fixes.
    * Handle delegation outside exception handler.
    * Inject ssh keys only if not already in place.
    * More defensive approach to getting remote pwd.
    * Add missing string format var.
    * Correct PowerShell require regex.
    * Rename `is_install` and `INSTALL_ROOT`.

--- test/runner/lib/sanity/ansible_doc.py
+++ test/runner/lib/sanity/ansible_doc.py
@@ -32,6 +32,14 @@ from lib.config import (
     SanityConfig,
 )
 
+from lib.data import (
+    data_context,
+)
+
+from lib.coverage_util import (
+    coverage_context,
+)
+
 
 class AnsibleDocTest(SanityMultipleVersion):
     """Sanity test for ansible-doc."""
@@ -69,19 +77,19 @@ class AnsibleDocTest(SanityMultipleVersion):
         target_paths = collections.defaultdict(dict)
 
         for module in modules:
-            doc_targets['module'].append(module)
+            doc_targets['module'].append(data_context().content.prefix + module)
 
         for plugin_type, plugin_name, plugin_path in plugins:
             if plugin_type in plugin_type_blacklist:
                 continue
 
-            doc_targets[plugin_type].append(plugin_name)
-            target_paths[plugin_type][plugin_name] = plugin_path
+            doc_targets[plugin_type].append(data_context().content.prefix + plugin_name)
+            target_paths[plugin_type][data_context().content.prefix + plugin_name] = plugin_path
 
         if not doc_targets:
             return SanitySkipped(self.name, python_version=python_version)
 
-        target_paths['module'] = dict((t.module, t.path) for t in targets.targets if t.module)
+        target_paths['module'] = dict((data_context().content.prefix + t.module, t.path) for t in targets.targets if t.module)
 
         env = ansible_environment(args, color=False)
         error_messages = []
@@ -90,7 +98,9 @@ class AnsibleDocTest(SanityMultipleVersion):
             cmd = ['ansible-doc', '-t', doc_type] + sorted(doc_targets[doc_type])
 
             try:
-                stdout, stderr = intercept_command(args, cmd, target_name='ansible-doc', env=env, capture=True, python_version=python_version)
+                with coverage_context(args):
+                    stdout, stderr = intercept_command(args, cmd, target_name='ansible-doc', env=env, capture=True, python_version=python_version)
+
                 status = 0
             except SubprocessError as ex:
                 stdout = ex.stdout
--- test/runner/lib/sanity/pep8.py
+++ test/runner/lib/sanity/pep8.py
@@ -17,7 +17,7 @@ from lib.util import (
     display,
     read_lines_without_comments,
     parse_to_list_of_dict,
-    INSTALL_ROOT,
+    ANSIBLE_ROOT,
 )
 
 from lib.util_common import (
@@ -47,10 +47,10 @@ class Pep8Test(SanitySingleVersion):
         skip_paths = read_lines_without_comments(PEP8_SKIP_PATH, optional=True)
         legacy_paths = read_lines_without_comments(PEP8_LEGACY_PATH, optional=True)
 
-        legacy_ignore_file = os.path.join(INSTALL_ROOT, 'test/sanity/pep8/legacy-ignore.txt')
+        legacy_ignore_file = os.path.join(ANSIBLE_ROOT, 'test/sanity/pep8/legacy-ignore.txt')
         legacy_ignore = set(read_lines_without_comments(legacy_ignore_file, remove_blank_lines=True))
 
-        current_ignore_file = os.path.join(INSTALL_ROOT, 'test/sanity/pep8/current-ignore.txt')
+        current_ignore_file = os.path.join(ANSIBLE_ROOT, 'test/sanity/pep8/current-ignore.txt')
         current_ignore = sorted(read_lines_without_comments(current_ignore_file, remove_blank_lines=True))
 
         skip_paths_set = set(skip_paths)
