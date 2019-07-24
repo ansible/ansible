1eb26f865fec896359784fc381e3c7e1226f24d3
Matt Clay <matt@mystile.com>
2019-07-23 18:10:24 -0700

    Fix import sanity test for collections. (#59484)

--- test/sanity/import/importer.py
+++ test/sanity/import/importer.py
@@ -25,6 +25,11 @@ except ImportError:
 import ansible.module_utils.basic
 import ansible.module_utils.common.removed
 
+try:
+    from ansible.utils.collection_loader import AnsibleCollectionLoader
+except ImportError:
+    AnsibleCollectionLoader = None
+
 
 class ImporterAnsibleModuleException(Exception):
     """Exception thrown during initialization of ImporterAnsibleModule."""
@@ -48,6 +53,10 @@ def main():
     """Main program function."""
     base_dir = os.getcwd()
     messages = set()
+
+    if AnsibleCollectionLoader:
+        # allow importing code from collections
+        sys.meta_path.insert(0, AnsibleCollectionLoader())
 
     for path in sys.argv[1:] or sys.stdin.read().splitlines():
         test_python_module(path, base_dir, messages, False)
