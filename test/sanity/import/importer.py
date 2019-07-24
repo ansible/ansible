63d4edfde2bb2f5f464a86929e01a4e52f6cb967
Matt Clay <matt@mystile.com>
2019-07-23 17:43:50 -0700

    Make AnsibleCollectionLoader import optional.

--- test/sanity/import/importer.py
+++ test/sanity/import/importer.py
@@ -25,6 +25,11 @@ except ImportError:
 import ansible.module_utils.basic
 import ansible.module_utils.common.removed
 
+try:
+    from ansible.utils.collection_loader import AnsibleCollectionLoader
+except ImportError:
+    AnsibleCollectionLoader = None
+
 
 class ImporterAnsibleModuleException(Exception):
     """Exception thrown during initialization of ImporterAnsibleModule."""
@@ -49,10 +54,9 @@ def main():
     base_dir = os.getcwd()
     messages = set()
 
-    from ansible.utils.collection_loader import AnsibleCollectionLoader
-
-    # allow unit tests to import code from collections
-    sys.meta_path.insert(0, AnsibleCollectionLoader())
+    if AnsibleCollectionLoader:
+        # allow unit tests to import code from collections
+        sys.meta_path.insert(0, AnsibleCollectionLoader())
 
     for path in sys.argv[1:] or sys.stdin.read().splitlines():
         test_python_module(path, base_dir, messages, False)
