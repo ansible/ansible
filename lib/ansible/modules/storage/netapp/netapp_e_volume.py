9e8a2f5654e843d03f73b66fbaed8929ef9eae6d
Nathan Swartz <swartzn@netapp.com>
2019-07-25 17:14:10 -0500

    Add controller ownership and cache without batteries option

--- lib/ansible/modules/storage/netapp/netapp_e_volume.py
+++ lib/ansible/modules/storage/netapp/netapp_e_volume.py
@@ -106,6 +106,13 @@ options:
             - Values must be between or equal to 10 and 99.
         default: 95
         version_added: 2.8
+    owning_controller:
+        description:
+            - Specifies which controller will be the primary owner of the volume
+            - Not specifying will allow the controller to choose ownership.
+        required: false
+        choices: ["A", "B"]
+        version_added: 2.9
     ssd_cache_enabled:
         description:
             - Whether an existing SSD cache should be enabled on the volume (fails if no SSD cache defined)
@@ -138,6 +145,13 @@ options:
         type: bool
         default: true
         version_added: 2.8
+    cache_without_batteries:
+        description:
+            - Indicates whether caching should be used without battery backup.
+            - Warning, M(cache_without_batteries==true) and the storage system looses power and there is no battery backup, data will be lost!
+        type: bool
+        default: false
+        version_added: 2.9
     workload_name:
         description:
             - Label for the workload defined by the metadata.
@@ -268,6 +282,7 @@ class NetAppESeriesVolume(NetAppESeriesModule):
                            type="str"),
             size=dict(type="float"),
             segment_size_kb=dict(type="int", default=128),
+            owning_controller=dict(required=False, choices=['A', 'B']),
             ssd_cache_enabled=dict(type="bool", default=False),
             data_assurance_enabled=dict(type="bool", default=False),
             thin_provision=dict(type="bool", default=False),
@@ -278,6 +293,7 @@ class NetAppESeriesVolume(NetAppESeriesModule):
             read_cache_enable=dict(type="bool", default=True),
             read_ahead_enable=dict(type="bool", default=True),
             write_cache_enable=dict(type="bool", default=True),
+            cache_without_batteries=dict(type="bool", default=False),
             workload_name=dict(type="str", required=False),
             metadata=dict(type="dict", require=False),
             wait_for_initialization=dict(type="bool", default=False),
@@ -302,10 +318,15 @@ class NetAppESeriesVolume(NetAppESeriesModule):
         if args["size"]:
             self.size_b = int(args["size"] * self.SIZE_UNIT_MAP[self.size_unit])
 
+        self.owning_controller_id = None
+        if args["owning_controller"]:
+            self.owning_controller_id = "070000000000000000000001" if args["owning_controller"] == "A" else "070000000000000000000002"
+
         self.read_cache_enable = args["read_cache_enable"]
         self.read_ahead_enable = args["read_ahead_enable"]
         self.write_cache_enable = args["write_cache_enable"]
         self.ssd_cache_enabled = args["ssd_cache_enabled"]
+        self.cache_without_batteries = args["cache_without_batteries"]
         self.data_assurance_enabled = args["data_assurance_enabled"]
 
         self.thin_provision = args["thin_provision"]
@@ -579,6 +600,11 @@ class NetAppESeriesVolume(NetAppESeriesModule):
                 self.ssd_cache_enabled != self.volume_detail["flashCached"]):
             change = True
 
+        # controller ownership
+        if self.owning_controller_id and self.owning_controller_id != self.volume_detail["preferredManager"]:
+            change = True
+            request_body.update(dict(owningControllerId=self.owning_controller_id))
+
         if self.workload_name:
             request_body.update(dict(metaTags=[dict(key="workloadId", value=self.workload_id),
                                                dict(key="volumeTypeId", value="volume")]))
@@ -595,9 +621,13 @@ class NetAppESeriesVolume(NetAppESeriesModule):
             if self.thin_volume_expansion_policy != self.volume_detail["expansionPolicy"]:
                 change = True
                 request_body.update(dict(expansionPolicy=self.thin_volume_expansion_policy))
-        elif self.read_ahead_enable != (int(self.volume_detail["cacheSettings"]["readAheadMultiplier"]) > 0):
-            change = True
-            request_body["cacheSettings"].update(dict(readAheadEnable=self.read_ahead_enable))
+        else:
+            if self.read_ahead_enable != (int(self.volume_detail["cacheSettings"]["readAheadMultiplier"]) > 0):
+                change = True
+                request_body["cacheSettings"].update(dict(readAheadEnable=self.read_ahead_enable))
+            if self.cache_without_batteries != self.volume_detail["cacheSettings"]["cwob"]:
+                change = True
+                request_body["cacheSettings"].update(dict(cacheWithoutBatteries=self.cache_without_batteries))
 
         return request_body if change else dict()
 
