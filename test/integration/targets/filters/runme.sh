1041fab366717935f1926e3f005131d7299ea98c
Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>
2019-07-26 21:07:51 +0200

    Install bcrypt w/o build isolation

--- test/integration/targets/filters/runme.sh
+++ test/integration/targets/filters/runme.sh
@@ -7,6 +7,7 @@ source virtualenv.sh
 # Requirements have to be installed prior to running ansible-playbook
 # because plugins and requirements are loaded before the task runs
 
-pip install jmespath netaddr bcrypt passlib
+pip install --no-build-isolation bcrypt
+pip install jmespath netaddr passlib
 
 ANSIBLE_ROLES_PATH=../ ansible-playbook filters.yml "$@"
