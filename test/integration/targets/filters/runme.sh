f254f47fe251cb0b8eef08d271cc10322964aea1
Sviatoslav Sydorenko <webknjaz@redhat.com>
2019-07-26 22:11:03 +0200

    Add a comment regarding Pip's build isolation bug
    Ref: https://github.com/pypa/pip/issues/6264#issuecomment-480100770

--- test/integration/targets/filters/runme.sh
+++ test/integration/targets/filters/runme.sh
@@ -7,7 +7,14 @@ source virtualenv.sh
 # Requirements have to be installed prior to running ansible-playbook
 # because plugins and requirements are loaded before the task runs
 
+# bcrypt has to be installed w/o build isolation b/c of the Pip's bug:
+# setuptools from system site-packages are leaking into an isolated
+# PEP518/517 build env.
+# Supposingly it's this one:
+# https://github.com/pypa/pip/issues/6264#issuecomment-480100770
+# Whenever that is fixed, it's okay to drop ``--no-build-isolation``
 pip install --no-build-isolation bcrypt
+
 pip install jmespath netaddr passlib
 
 ANSIBLE_ROLES_PATH=../ ansible-playbook filters.yml "$@"
