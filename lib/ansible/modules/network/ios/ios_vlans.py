1c6cf331b7369a4d9c34374278ca62395eb0834f
Sumit Jaiswal <sjaiswal@redhat.com>
2019-07-29 11:29:28 +0530

    fix shippable
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>

--- lib/ansible/modules/network/ios/ios_vlans.py
+++ lib/ansible/modules/network/ios/ios_vlans.py
@@ -42,61 +42,61 @@ COPYRIGHT = "Copyright 2019 Red Hat"
 
 DOCUMENTATION = """
 module: ios_vlans
-  version_added: 2.9
-  short_description: Manage VLANs on Cisco IOS devices.
-  description: This module provides declarative management of VLANs on Cisco IOS network devices.
-  author: Sumit Jaiswal (@justjais)
-  notes:
-  - Tested against Cisco IOSv Version 15.2 on VIRL
-  options:
-    config:
-      description: A dictionary of VLANs options
-      type: list
-      elements: dict
-      suboptions:
-        name:
-          description:
-          - Ascii name of the VLAN.
-          - NOTE, I(name) should not be named/appended with I(default) as it is reserved for device default vlans.
-          type: str
-        vlan_id:
-          description:
-          - ID of the VLAN. Range 1-4094
-          type: int
-          required: True
-        mtu:
-          description:
-          - VLAN Maximum Transmission Unit.
-          - Refer to vendor documentation for valid values.
-          type: int
-        state:
-          description:
-          - Operational state of the VLAN
-          type: str
-          choices:
-          - active
-          - suspend
-        remote_span:
-          description:
-          - Configure as Remote SPAN VLAN
-          type: bool
-        shutdown:
-          description:
-          - Shutdown VLAN switching.
-          type: str
-          choices:
-          - enabled
-          - disabled
+version_added: 2.9
+short_description: Manage VLANs on Cisco IOS devices.
+description: This module provides declarative management of VLANs on Cisco IOS network devices.
+author: Sumit Jaiswal (@justjais)
+notes:
+- Tested against Cisco IOSv Version 15.2 on VIRL
+options:
+config:
+  description: A dictionary of VLANs options
+  type: list
+  elements: dict
+  suboptions:
+    name:
+      description:
+      - Ascii name of the VLAN.
+      - NOTE, I(name) should not be named/appended with I(default) as it is reserved for device default vlans.
+      type: str
+    vlan_id:
+      description:
+      - ID of the VLAN. Range 1-4094
+      type: int
+      required: True
+    mtu:
+      description:
+      - VLAN Maximum Transmission Unit.
+      - Refer to vendor documentation for valid values.
+      type: int
     state:
       description:
-      - The state the configuration should be left in
+      - Operational state of the VLAN
       type: str
       choices:
-      - merged
-      - replaced
-      - overridden
-      - deleted
-      default: merged
+      - active
+      - suspend
+    remote_span:
+      description:
+      - Configure as Remote SPAN VLAN
+      type: bool
+    shutdown:
+      description:
+      - Shutdown VLAN switching.
+      type: str
+      choices:
+      - enabled
+      - disabled
+state:
+  description:
+  - The state the configuration should be left in
+  type: str
+  choices:
+  - merged
+  - replaced
+  - overridden
+  - deleted
+  default: merged
 """
 EXAMPLES = """
 # Using deleted
