869f0b706006e3d93b6de9a1b88c4ee7d4aa5370
rnsc <github@rnsc.be>
2019-07-29 14:52:46 +0200

    license and metadata

--- lib/ansible/modules/windows/win_data_deduplication.ps1
+++ lib/ansible/modules/windows/win_data_deduplication.ps1
@@ -1,6 +1,7 @@
 #!powershell
 
 # Copyright: (c) 2019, rnsc <github@rnsc.be>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt
 
 #Requires -Module Ansible.ModuleUtils.Legacy
 #Requires -Module Ansible.ModuleUtils.SID
--- lib/ansible/modules/windows/win_data_deduplication.py
+++ lib/ansible/modules/windows/win_data_deduplication.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 
 # (c) 2019 github@rnsc.be
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
 
 DOCUMENTATION = r'''
 ---
