2ebc4e1e7eda61f133d9425872e2a858bae92ec4
Abhijeet Kasurde <akasurde@redhat.com>
2019-07-30 22:32:17 +0530

    inventory: Handle IndexError while parsing limit file (#59776)
    Handle IndexError exception raised while parsing the limit file.
    
    Fixes: #59695
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- test/integration/targets/inventory/runme.sh
+++ test/integration/targets/inventory/runme.sh
@@ -2,6 +2,17 @@
 
 set -x
 
+empty_limit_file="/tmp/limit_file"
+touch "${empty_limit_file}"
+
+cleanup() {
+    if [[ -f "${empty_limit_file}" ]]; then
+            rm -rf "${empty_limit_file}"
+    fi
+}
+
+trap 'cleanup' EXIT
+
 # https://github.com/ansible/ansible/issues/52152
 # Ensure that non-matching limit causes failure with rc 1
 ansible-playbook -i ../../inventory --limit foo playbook.yml
@@ -9,6 +20,9 @@ if [ "$?" != "1" ]; then
     echo "Non-matching limit should cause failure"
     exit 1
 fi
+
+# Ensure that non-matching limit causes failure with rc 1
+ansible-playbook -i ../../inventory --limit @"${empty_limit_file}" playbook.yml
 
 ansible-playbook -i ../../inventory "$@" strategy.yml
 ANSIBLE_TRANSFORM_INVALID_GROUP_CHARS=always ansible-playbook -i ../../inventory "$@" strategy.yml
