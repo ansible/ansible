a8f495ebe229d805373467c699dadd35e2d318eb
rnsc <github@rnsc.be>
2019-07-31 18:54:51 +0200

    Removing unnecessary module

--- lib/ansible/modules/windows/win_data_deduplication.ps1
+++ lib/ansible/modules/windows/win_data_deduplication.ps1
@@ -4,7 +4,6 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt
 
 #Requires -Module Ansible.ModuleUtils.Legacy
-#Requires -Module Ansible.ModuleUtils.SID
 
 $ErrorActionPreference = "Stop"
 
