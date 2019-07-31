32fa4b15d1ce48e92174e5576056d55ed46b2c3b
Matt Clay <matt@mystile.com>
2019-07-30 22:36:03 -0700

    More sanity test updates for collections. (#59841)
    * Move action-plugin-docs ignores into ignore file.
    
    * Update package-data sanity test.
    
    The test now uses the file list provided by ansible-test.

--- test/sanity/code-smell/package-data.py
+++ test/sanity/code-smell/package-data.py
@@ -5,29 +5,28 @@ __metaclass__ = type
 import fnmatch
 import os
 import re
+import sys
 import tempfile
 import subprocess
 
 
 def main():
     ignore_files = frozenset((
-        '*/.git_keep',
+        '*/galaxy/data/default/*/.git_keep',
         '*/galaxy/data/default/role/*/main.yml.j2',
         '*/galaxy/data/default/role/*/test.yml.j2',
         '*/galaxy/data/default/collection/plugins/README.md.j2',
     ))
 
     non_py_files = []
-    for root, _dummy, files in os.walk('lib/ansible/'):
-        for filename in files:
-            path = os.path.join(root, filename)
-            if os.path.splitext(path)[1] not in ('.py', '.pyc', '.pyo'):
-                add = True
-                for ignore in ignore_files:
-                    if fnmatch.fnmatch(path, ignore):
-                        add = False
-                if add:
-                    non_py_files.append(os.path.relpath(path, 'lib/ansible'))
+    for path in sys.argv[1:] or sys.stdin.read().splitlines():
+        if os.path.splitext(path)[1] != '.py':
+            add = True
+            for ignore in ignore_files:
+                if fnmatch.fnmatch(path, ignore):
+                    add = False
+            if add:
+                non_py_files.append(os.path.relpath(path, 'lib/ansible'))
 
     with tempfile.TemporaryDirectory() as tmp_dir:
         stdout, _dummy = subprocess.Popen(
