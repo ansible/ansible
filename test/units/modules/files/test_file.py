0574155c4e424573bd9458ee02a53ceb4265f98c
Dmitry Nortsev <dmitry.nortsev@gmail.com>
2019-07-31 03:15:16 +0100

    Fix cosmetic issues

--- test/units/modules/files/test_file.py
+++ test/units/modules/files/test_file.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright:
-#   (c) 2018 Ansible Project
+# Copyright: 2018 Ansible Project
 # License: GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 # Make coding more python3-ish
