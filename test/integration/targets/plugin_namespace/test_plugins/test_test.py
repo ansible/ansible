01a37f215a7445221464a48b65d748dd8d674de7
Matt Clay <matt@mystile.com>
2019-08-01 13:40:51 -0700

    Make sure filter and test names are unique.

--- test/integration/targets/plugin_namespace/test_plugins/test_test.py
+++ test/integration/targets/plugin_namespace/test_plugins/test_test.py
@@ -1,13 +1,16 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import re
+
 
 def test_package_ok(value):
     return __package__ == 'ansible.plugins.test'
 
 
 def test_name_ok(value):
-    return __name__ == 'ansible.plugins.test.test_test'
+    # test names are prefixed with a unique hash value to prevent shadowing of other plugins
+    return bool(re.match(r'^ansible\.plugins\.test\.[0-9]+_test_test$', __name__))
 
 
 class TestModule:
