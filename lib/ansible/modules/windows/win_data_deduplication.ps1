41fe2411eb5579ac3cf62b8aaeda407aefde65f7
rnsc <github@rnsc.be>
2019-08-01 11:02:42 +0200

    replacing tabs with double space

--- lib/ansible/modules/windows/win_data_deduplication.ps1
+++ lib/ansible/modules/windows/win_data_deduplication.ps1
@@ -21,132 +21,132 @@ if ([System.Environment]::OSVersion.Version -lt [Version]"6.3.9600.0") {
 }
 
 $result = @{
-	changed = $false
-	reboot_required= $false
+  changed = $false
+  reboot_required= $false
 }
 
 function Set-DataDeduplication($volume, $state, $settings, $dedup_job) {
 
-	$current_state = 'disabled'
+  $current_state = 'disabled'
 
-	try {
-		$dedup_info = Get-DedupVolume -Volume "$($volume.DriveLetter):"
-	} catch {
-		$dedup_info = $null
-	}
+  try {
+    $dedup_info = Get-DedupVolume -Volume "$($volume.DriveLetter):"
+  } catch {
+    $dedup_info = $null
+  }
 
-	if($null -ne $dedup_info) {
-		if ($dedup_info.Enabled) {
-			$current_state = 'enabled'
-		}
-	} else {
-		$current_state = $null
-	}
+  if($null -ne $dedup_info) {
+    if ($dedup_info.Enabled) {
+      $current_state = 'enabled'
+    }
+  } else {
+    $current_state = $null
+  }
 
-	if ( $state -ne $current_state ) {
-		if($state -eq 'enabled') {
-			try {
-				# Enable-DedupVolume -Volume <String>
-				Enable-DedupVolume -Volume "$($volume.DriveLetter):"
-			} catch {
-				Fail-Json $result $_.Exception.Message
-			}
-		} elseif ($state -eq 'disabled') {
-			try {
-				Disable-DedupVolume -Volume "$($volume.DriveLetter):"
-			} catch {
-				Fail-Json $result $_.Exception.Message
-			}
-		} else {
-			Fail-Json $result "Unsupported state option, it can only be 'enabled' or 'disabled'."
-		}
+  if ( $state -ne $current_state ) {
+    if($state -eq 'enabled') {
+      try {
+        # Enable-DedupVolume -Volume <String>
+        Enable-DedupVolume -Volume "$($volume.DriveLetter):"
+      } catch {
+        Fail-Json $result $_.Exception.Message
+      }
+    } elseif ($state -eq 'disabled') {
+      try {
+        Disable-DedupVolume -Volume "$($volume.DriveLetter):"
+      } catch {
+        Fail-Json $result $_.Exception.Message
+      }
+    } else {
+      Fail-Json $result "Unsupported state option, it can only be 'enabled' or 'disabled'."
+    }
 
-		$result.msg += " Deduplication on volume: "+$volume.DriveLetter+" set to: $state"
-		$result.changed = $true
-	}
+    $result.msg += " Deduplication on volume: "+$volume.DriveLetter+" set to: $state"
+    $result.changed = $true
+  }
 
-	if ($state -eq 'enabled') {
+  if ($state -eq 'enabled') {
     if ($null -ne $settings) {
       Set-DataDedupJobSettings -volume $volume -settings $settings
     }
 
     if ($null -ne $dedup_job) {
-			Start-DataDedupJob -volume $volume -dedup_job $dedup_job
+      Start-DataDedupJob -volume $volume -dedup_job $dedup_job
     }
-	}
+  }
 }
 
 function Set-DataDedupJobSettings ($volume, $settings) {
 
-	try {
-		$dedup_info = Get-DedupVolume -Volume "$($volume.DriveLetter):"
-	} catch {
-		$dedup_info = $null
-	}
+  try {
+    $dedup_info = Get-DedupVolume -Volume "$($volume.DriveLetter):"
+  } catch {
+    $dedup_info = $null
+  }
 
-	ForEach ($key in $settings.keys) {
-		$update_key = $key
-		$update_value = $settings.$($key)
+  ForEach ($key in $settings.keys) {
+    $update_key = $key
+    $update_value = $settings.$($key)
 
-		if ($update_key -eq "MinimumFileSize" -and $update_value -lt 32768) {
-			$update_value = 32768
-		}
+    if ($update_key -eq "MinimumFileSize" -and $update_value -lt 32768) {
+      $update_value = 32768
+    }
 
-		try {
-			$current_value = ($dedup_info | Select-Object -ExpandProperty $update_key)
-		} catch {
-			$current_value = $null
-		}
+    try {
+      $current_value = ($dedup_info | Select-Object -ExpandProperty $update_key)
+    } catch {
+      $current_value = $null
+    }
 
-		if ($update_value -ne $current_value) {
-			$command_param = @{
-				$($update_key) = $update_value
-			}
+    if ($update_value -ne $current_value) {
+      $command_param = @{
+        $($update_key) = $update_value
+      }
 
-			# Set-DedupVolume -Volume <String>`
-			#                 -NoCompress <bool> `
-			#                 -MinimumFileAgeDays <UInt32> `
-			#                 -MinimumFileSize <UInt32> (minimum 32768)
-			try {
-				Set-DedupVolume -Volume "$($volume.DriveLetter):" @command_param
-				$result.msg += " Setting DedupVolume settings for $update_key"
+      # Set-DedupVolume -Volume <String>`
+      #                 -NoCompress <bool> `
+      #                 -MinimumFileAgeDays <UInt32> `
+      #                 -MinimumFileSize <UInt32> (minimum 32768)
+      try {
+        Set-DedupVolume -Volume "$($volume.DriveLetter):" @command_param
+        $result.msg += " Setting DedupVolume settings for $update_key"
 
-			} catch {
-				Fail-Json $result $_.Exception.Message
-			}
+      } catch {
+        Fail-Json $result $_.Exception.Message
+      }
 
-			$result.changed = $true
-		}
-	}
+      $result.changed = $true
+    }
+  }
 
 }
 
 function Start-DataDedupJob($volume, $dedup_job) {
 
-	$dedup_job_queue = Get-DedupJob -Volume "$($volume.DriveLetter):" -ErrorAction SilentlyContinue
+  $dedup_job_queue = Get-DedupJob -Volume "$($volume.DriveLetter):" -ErrorAction SilentlyContinue
 
-	if(( $dedup_job_queue | Select-Object -ExpandProperty State) -ne "Queued" ) {
+  if(( $dedup_job_queue | Select-Object -ExpandProperty State) -ne "Queued" ) {
 
-		$command_param = @{}
+    $command_param = @{}
 
-		ForEach ($key in $dedup_job.keys) {
+    ForEach ($key in $dedup_job.keys) {
 
-			$update_key = $key
-			$update_value = $dedup_job.$($key)
+      $update_key = $key
+      $update_value = $dedup_job.$($key)
 
-			$command_param.Add($($update_key), $update_value)
+      $command_param.Add($($update_key), $update_value)
 
-		}
+    }
 
-		# Start-DedupJob -Volume <String> `
-		# 	       -Type <String>
-		$dedup_job_start = Start-DedupJob -Volume "$($volume.DriveLetter):" @command_param
-		$result.msg += " DedupJob "+($dedup_job_start | Select-Object -ExpandProperty State)
+    # Start-DedupJob -Volume <String> `
+    #          -Type <String>
+    $dedup_job_start = Start-DedupJob -Volume "$($volume.DriveLetter):" @command_param
+    $result.msg += " DedupJob "+($dedup_job_start | Select-Object -ExpandProperty State)
 
-		$result.changed = $true
-	} else {
-		$result.msg += " A DedupJob is already queued for that volume."
-	}
+    $result.changed = $true
+  } else {
+    $result.msg += " A DedupJob is already queued for that volume."
+  }
 
 }
 
@@ -156,15 +156,15 @@ $feature_name = "FS-Data-Deduplication"
 $feature = Install-WindowsFeature -Name $feature_name
 
 if ($feature.RestartNeeded -eq 'Yes') {
-	$result.reboot_required = $true
+  $result.reboot_required = $true
   Fail-Json $result "$feature_name was installed but requires Windows to be rebooted to work."
   Exit-Json -obj $result
 }
 
 try {
-	$volume = Get-Volume -DriveLetter $drive_letter
+  $volume = Get-Volume -DriveLetter $drive_letter
 } catch {
-	Fail-Json $result $_.Exception.Message
+  Fail-Json $result $_.Exception.Message
 }
 if ($null -ne $volume) {
   $result.msg += "Start setting FileDeduplication"
