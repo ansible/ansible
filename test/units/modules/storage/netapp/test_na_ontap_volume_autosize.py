6de6e0987446c7193bae5dd5830e31c949573da7
Chris Archibald <carchi@netapp.com>
2019-08-01 09:47:04 -0700

    update volume

--- test/units/modules/storage/netapp/test_na_ontap_volume_autosize.py
+++ test/units/modules/storage/netapp/test_na_ontap_volume_autosize.py
@@ -3,7 +3,8 @@
 
 ''' unit tests for Ansible module: na_ontap_volume_autosize '''
 
-from __future__ import print_function
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 import json
 import pytest
 
