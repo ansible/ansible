216e59a14754d3afb39acf32e71710f330c46d36
Abhijeet Kasurde <akasurde@redhat.com>
2019-08-01 16:28:00 +0530

    vars: Fail with warning for extra_vars filename without @ sign
    Rather than silently processing extra_vars filename without @ sign,
    CLI now fails with appropriate warning about requirement.
    
    Fixes: #51857
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/utils/vars.py
+++ lib/ansible/utils/vars.py
@@ -125,10 +125,15 @@ def load_extra_vars(loader):
     for extra_vars_opt in context.CLIARGS.get('extra_vars', tuple()):
         data = None
         extra_vars_opt = to_text(extra_vars_opt, errors='surrogate_or_strict')
+        if extra_vars_opt is None or not extra_vars_opt:
+            continue
+
         if extra_vars_opt.startswith(u"@"):
             # Argument is a YAML file (JSON is a subset of YAML)
             data = loader.load_from_file(extra_vars_opt[1:])
-        elif extra_vars_opt and extra_vars_opt[0] in u'[{':
+        elif extra_vars_opt[0] in [u'/', u'.']:
+            raise AnsibleOptionsError("Please prepend extra_vars filename '%s' with '@'" % extra_vars_opt)
+        elif extra_vars_opt[0] in [u'[', u'{']:
             # Arguments as YAML
             data = loader.load(extra_vars_opt)
         else:
