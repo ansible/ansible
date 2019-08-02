ansible/ansible

commit be8048deb214909d80dd8933f6c25cb530070ddb

group: detect duplicate GIDs when local=yes (#58469)

lib/ansible/modules/system/group.py
@@ -120,9 +120,14 @@ class Group(object):
         cmd = [self.module.get_bin_path(command_name, True), self.name]
         return self.execute_command(cmd)
 
+    def _local_check_gid_exists(self):
+        if self.gid and self.gid in [gr.gr_gid for gr in grp.getgrall()]:
+            self.module.fail_json(msg="GID '{0}' already exists".format(self.gid))
+
     def group_add(self, **kwargs):
         if self.local:
             command_name = 'lgroupadd'
+            self._local_check_gid_exists()
         else:
             command_name = 'groupadd'
         cmd = [self.module.get_bin_path(command_name, True)]
@@ -140,6 +145,7 @@ class Group(object):
     def group_mod(self, **kwargs):
         if self.local:
             command_name = 'lgroupmod'
+            self._local_check_gid_exists()
         else:
             command_name = 'groupmod'
         cmd = [self.module.get_bin_path(command_name, True)]

test/integration/targets/group/tasks/tests.yml
@@ -192,3 +192,28 @@
   assert:
     that:
     - not delete_group_again is changed
+
+# https://github.com/ansible/ansible/issues/56481
+- block:
+  - name: Test duplicate GID with local=yes
+    group:
+      name: "{{ item }}"
+      gid: 1337
+      local: yes
+    loop:
+      - group1_local_test
+      - group2_local_test
+    ignore_errors: yes
+    register: local_duplicate_gid_result
+
+  - assert:
+     that:
+       - local_duplicate_gid_result['results'][0] is success
+       - local_duplicate_gid_result['results'][1]['msg'] == "GID '1337' already exists"
+  always:
+    - name: Cleanup
+      group:
+        name: group1_local_test
+        state: absent
+  # only applicable to Linux, limit further to CentOS where 'luseradd' is installed
+  when: ansible_distribution == 'CentOS'

commit 34896627c40fc1e74917b15ad0c09a3b2deb2d9e

Only error out if the gid exists with a different group name (#59769)
Only error out if the gid exists with a different group name as
otherwise it will error out if the group with this gid already
exists, like on a rerun of the playbook. This fixes a regression
introduced by be8048deb214909d80dd8933f6c25cb530070ddb.

lib/ansible/modules/system/group.py
@@ -121,8 +121,10 @@ class Group(object):
         return self.execute_command(cmd)
 
     def _local_check_gid_exists(self):
-        if self.gid and self.gid in [gr.gr_gid for gr in grp.getgrall()]:
-            self.module.fail_json(msg="GID '{0}' already exists".format(self.gid))
+        if self.gid:
+            for gr in grp.getgrall():
+                if self.gid == gr.gr_gid and self.name != gr.gr_name:
+                    self.module.fail_json(msg="GID '{0}' already exists with group '{1}'".format(self.gid, gr.gr_name))
 
     def group_add(self, **kwargs):
         if self.local:

test/integration/targets/group/tasks/tests.yml
@@ -209,9 +209,50 @@
   - assert:
      that:
        - local_duplicate_gid_result['results'][0] is success
-       - local_duplicate_gid_result['results'][1]['msg'] == "GID '1337' already exists"
+       - local_duplicate_gid_result['results'][1]['msg'] == "GID '1337' already exists with group 'group1_local_test'"
   always:
     - name: Cleanup
+      group:
+        name: group1_local_test
+        state: absent
+  # only applicable to Linux, limit further to CentOS where 'luseradd' is installed
+  when: ansible_distribution == 'CentOS'
+
+# https://github.com/ansible/ansible/pull/59769
+- block:
+  - name: create a local group with a gid
+    group:
+      name: group1_local_test
+      gid: 1337
+      local: yes
+      state: present
+    register: create_local_group_gid
+
+  - name: get gid of created local group
+    command: "{{ ansible_python_interpreter | quote }} -c \"import grp; print(grp.getgrnam('group1_local_test').gr_gid)\""
+    register: create_local_group_gid_actual
+
+  - name: assert create local group with a gid
+    assert:
+        that:
+        - create_local_group_gid is changed
+        - create_local_group_gid.gid | int == 1337 | int
+        - create_local_group_gid_actual.stdout | trim | int == 1337 | int
+
+  - name: create a local group with a gid (idempotent)
+    group:
+      name: group1_local_test
+      gid: 1337
+      state: present
+    register: create_local_group_gid_again
+
+  - name: assert create local group with a gid (idempotent)
+    assert:
+        that:
+        - not create_local_group_gid_again is changed
+        - create_local_group_gid_again.gid | int == 1337 | int
+  always:
+    - name: Cleanup create local group with a gid 
       group:
         name: group1_local_test
         state: absent