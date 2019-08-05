021b1810c941171b8b8d81d1d8a65ef9e101fc7d
Trishna Guha <trishnaguha17@gmail.com>
2019-08-05 12:50:16 +0530

    Add nxos_vlans resource module (#59294)
    * Add nxos_vlans resource module
    
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>
    
    * address review comments
    
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>
    
    * fix CI failure and udpate tests
    
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>
    
    * Add commands in module doc
    
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- lib/ansible/modules/network/nxos/_nxos_vlan.py
+++ lib/ansible/modules/network/nxos/_nxos_vlan.py
@@ -16,8 +16,11 @@
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
 ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
+                    'status': ['deprecated'],
                     'supported_by': 'network'}
 
 DOCUMENTATION = '''
@@ -28,58 +31,73 @@ version_added: "2.1"
 short_description: Manages VLAN resources and attributes.
 description:
   - Manages VLAN configurations on NX-OS switches.
+deprecated:
+  removed_in: '2.13'
+  alternative: nxos_vlans
+  why: Updated modules released with more functionality
 author: Jason Edelman (@jedelman8)
 options:
   vlan_id:
     description:
       - Single VLAN ID.
+    type: int
   vlan_range:
     description:
       - Range of VLANs such as 2-10 or 2,5,10-15, etc.
+    type: str
   name:
     description:
       - Name of VLAN or keyword 'default'.
+    type: str
   interfaces:
     description:
       - List of interfaces that should be associated to the VLAN or keyword 'default'.
     version_added: "2.5"
+    type: list
   associated_interfaces:
     description:
       - This is a intent option and checks the operational state of the for given vlan C(name)
         for associated interfaces. If the value in the C(associated_interfaces) does not match with
         the operational state of vlan interfaces on device it will result in failure.
     version_added: "2.5"
+    type: list
   vlan_state:
     description:
       - Manage the vlan operational state of the VLAN
     default: active
     choices: ['active','suspend']
+    type: str
   admin_state:
     description:
       - Manage the VLAN administrative state of the VLAN equivalent
         to shut/no shut in VLAN config mode.
     default: up
     choices: ['up','down']
+    type: str
   mapped_vni:
     description:
       - The Virtual Network Identifier (VNI) ID that is mapped to the
         VLAN. Valid values are integer and keyword 'default'. Range 4096-16773119.
     version_added: "2.2"
+    type: str
   state:
     description:
       - Manage the state of the resource.
     default: present
     choices: ['present','absent']
+    type: str
   mode:
     description:
       - Set VLAN mode to classical ethernet or fabricpath.
         This is a valid option for Nexus 5000 and 7000 series.
     choices: ['ce','fabricpath']
     default: 'ce'
+    type: str
     version_added: "2.4"
   aggregate:
     description: List of VLANs definitions.
     version_added: "2.5"
+    type: list
   purge:
     description:
       - Purge VLANs not defined in the I(aggregate) parameter.
@@ -92,6 +110,7 @@ options:
       - Time in seconds to wait before checking for the operational state on remote
         device. This wait is applicable for operational state arguments.
     default: 10
+    type: int
 '''
 
 EXAMPLES = '''
