639033fc9f00b5fe22c4a3e378b4b60127e54560
shashik <shashi@alethea.in>
2019-08-05 16:03:42 +0530

    new changes

--- lib/ansible/modules/network/icx/icx_logging.py
+++ lib/ansible/modules/network/icx/icx_logging.py
@@ -52,6 +52,42 @@ options:
     description:
       - List of logging definitions.
     type: list
+    suboptions:
+      dest:
+        description:
+          - Destination of the logs.
+        choices: ['on', 'host', 'console', 'buffered', 'persistence', 'rfc5424']
+        type: str
+      name:
+        description:
+          - ipv4 address/ipv6 address/name of  syslog server.
+        type: str
+      udp_port:
+        description:
+          - UDP port of destination host(syslog server).
+        type: str
+      facility:
+        description:
+          - Specifies log facility to log messages from the device.
+        choices: ['auth','cron','daemon','kern','local0', 'local1', 'local2', 'local3', 'local4', 'local5', 'local6', 'local7', 'user',
+                  'lpr','mail','news','syslog','sys9','sys10','sys11','sys12','sys13','sys14','user','uucp']
+        type: str
+      level:
+        description:
+          - Specifies the message level.
+        type: list
+        choices: ['alerts', 'critical', 'debugging', 'emergencies', 'errors', 'informational',
+                    'notifications', 'warnings']
+      state:
+        description:
+          - State of the logging configuration.
+        choices: ['present', 'absent']
+        type: str
+      check_running_config:
+        description:
+          - Check running configuration. This can be set as environment variable.
+           Module will use environment variable value(default:True), unless it is overriden, by specifying it as module parameter.
+        type: bool
   state:
     description:
       - State of the logging configuration.
