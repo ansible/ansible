d096f1874b411407f337a2c832bb5918ea4caac1
Matt Clay <matt@mystile.com>
2019-08-06 15:31:45 -0700

    Add missing boilerplate.

--- test/integration/targets/include_import/playbook/sub_playbook/library/helloworld.py
+++ test/integration/targets/include_import/playbook/sub_playbook/library/helloworld.py
@@ -14,6 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 from ansible.module_utils.basic import AnsibleModule
 
 
