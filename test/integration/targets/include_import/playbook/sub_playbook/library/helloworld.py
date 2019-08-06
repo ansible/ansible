dc0a5ac3536d62b8ae352b4b36def7029eac2152
Matt Martz <matt@sivel.net>
2019-08-06 15:58:15 -0500

    Move plugin loader playbook dir additions back to Playbook instead of PlaybookCLI (#59557)
    * Move plugin loader playbook dir additions back to Playbook instead of PlaybookCLI. Fixes #59548
    
    * Restore cli additions

---
+++ test/integration/targets/include_import/playbook/sub_playbook/library/helloworld.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+from ansible.module_utils.basic import AnsibleModule
+
+
+def main():
+    module = AnsibleModule(argument_spec={})
+
+    module.exit_json(msg='Hello, World!')
+
+
+if __name__ == '__main__':
+    main()
