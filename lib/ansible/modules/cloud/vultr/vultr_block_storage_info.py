22f69b78575657c7968d876cd00044e4541a79f0
Rene Moser <mail@renemoser.net>
2019-08-07 22:56:12 +0200

    fix sanity

--- lib/ansible/modules/cloud/vultr/vultr_block_storage_info.py
+++ lib/ansible/modules/cloud/vultr/vultr_block_storage_info.py
@@ -18,7 +18,8 @@ module: vultr_block_storage_info
 short_description: Get infos about the Vultr block storage volumes available.
 description:
   - Get infos about block storage volumes available in Vultr.
-version_added: "2.7"
+version_added: "2.9"
+author:
   - "Yanis Guenane (@Spredzy)"
   - "René Moser (@resmo)"
 extends_documentation_fragment: vultr
