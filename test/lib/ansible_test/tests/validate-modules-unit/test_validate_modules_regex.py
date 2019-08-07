161e4ca49360bfad1668b1c7ef16a7f8c4eb80e8
Matt Clay <matt@mystile.com>
2019-08-06 17:53:25 -0700

    Fix up unit tests.

--- test/lib/ansible_test/tests/validate-modules-unit/test_validate_modules_regex.py
+++ test/lib/ansible_test/tests/validate-modules-unit/test_validate_modules_regex.py
@@ -1,64 +1,43 @@
-# This is a standalone test for the regex inside validate-modules
-# It is not suitable to add to the make tests target because the
-# file under test is outside the test's sys.path AND has a hyphen
-# in the name making it unimportable.
-#
-# To execute this by hand:
-#   1) cd <checkoutdir>
-#   2) source hacking/env-setup
-#   3) PYTHONPATH=./lib pytest -v sanity/validate-modules
+"""Tests for validate-modules regexes."""
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import re
-import unittest
+import pytest
 
-# TYPE_REGEX = re.compile(r'.*\stype\(.*')
-# TYPE_REGEX = re.compile(r'.*(if|or)\stype\(.*')
-# TYPE_REGEX = re.compile(r'.*(if|or)(\s+.*|\s+)type\(.*')
-# TYPE_REGEX = re.compile(r'.*(if|or)(\s+.*|\s+)type\(.*')
-# TYPE_REGEX = re.compile(r'.*(if|\sor)(\s+.*|\s+)type\(.*')
-# TYPE_REGEX =  re.compile(r'.*(if|\sor)(\s+.*|\s+)(?<!_)type\(.*')
-TYPE_REGEX = re.compile(r'.*(if|or)(\s+.*|\s+)(?<!_)(?<!str\()type\(.*')
+from validate_modules.main import TYPE_REGEX
 
 
-class TestValidateModulesRegex(unittest.TestCase):
-
-    def test_type_regex(self):
-        # each of these examples needs to be matched or not matched
-        checks = [
-            ['if type(foo) is Bar', True],
-            ['if Bar is type(foo)', True],
-            ['if type(foo) is not Bar', True],
-            ['if Bar is not type(foo)', True],
-            ['if type(foo) == Bar', True],
-            ['if Bar == type(foo)', True],
-            ['if type(foo)==Bar', True],
-            ['if Bar==type(foo)', True],
-            ['if type(foo) != Bar', True],
-            ['if Bar != type(foo)', True],
-            ['if type(foo)!=Bar', True],
-            ['if Bar!=type(foo)', True],
-            ['if foo or type(bar) != Bar', True],
-            ['x = type(foo)', False],
-            ["error = err.message + ' ' + str(err) + ' - ' + str(type(err))", False],
-            # cloud/amazon/ec2_group.py
-            ["module.fail_json(msg='Invalid rule parameter type [%s].' % type(rule))", False],
-            # files/patch.py
-            ["p = type('Params', (), module.params)", False],  # files/patch.py
-            # system/osx_defaults.py
-            ["if self.current_value is not None and not isinstance(self.current_value, type(self.value)):", True],
-            # system/osx_defaults.py
-            ['raise OSXDefaultsException("Type mismatch. Type in defaults: " + type(self.current_value).__name__)', False],
-            # network/nxos/nxos_interface.py
-            ["if get_interface_type(interface) == 'svi':", False],
-        ]
-        for idc, check in enumerate(checks):
-            cstring = check[0]
-            cexpected = check[1]
-
-            match = TYPE_REGEX.match(cstring)
-            if cexpected and not match:
-                assert False, "%s should have matched" % cstring
-            elif not cexpected and match:
-                assert False, "%s should not have matched" % cstring
+@pytest.mark.parametrize('cstring,cexpected', [
+    ['if type(foo) is Bar', True],
+    ['if Bar is type(foo)', True],
+    ['if type(foo) is not Bar', True],
+    ['if Bar is not type(foo)', True],
+    ['if type(foo) == Bar', True],
+    ['if Bar == type(foo)', True],
+    ['if type(foo)==Bar', True],
+    ['if Bar==type(foo)', True],
+    ['if type(foo) != Bar', True],
+    ['if Bar != type(foo)', True],
+    ['if type(foo)!=Bar', True],
+    ['if Bar!=type(foo)', True],
+    ['if foo or type(bar) != Bar', True],
+    ['x = type(foo)', False],
+    ["error = err.message + ' ' + str(err) + ' - ' + str(type(err))", False],
+    # cloud/amazon/ec2_group.py
+    ["module.fail_json(msg='Invalid rule parameter type [%s].' % type(rule))", False],
+    # files/patch.py
+    ["p = type('Params', (), module.params)", False],  # files/patch.py
+    # system/osx_defaults.py
+    ["if self.current_value is not None and not isinstance(self.current_value, type(self.value)):", True],
+    # system/osx_defaults.py
+    ['raise OSXDefaultsException("Type mismatch. Type in defaults: " + type(self.current_value).__name__)', False],
+    # network/nxos/nxos_interface.py
+    ["if get_interface_type(interface) == 'svi':", False],
+])
+def test_type_regex(cstring, cexpected):  # type: (str, str) -> None
+    """Check TYPE_REGEX against various examples to verify it correctly matches or does not match."""
+    match = TYPE_REGEX.match(cstring)
+    if cexpected and not match:
+        assert False, "%s should have matched" % cstring
+    elif not cexpected and match:
+        assert False, "%s should not have matched" % cstring
