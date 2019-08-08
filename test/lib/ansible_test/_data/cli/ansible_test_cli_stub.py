16c048a65abbb72407a4125da208431a964ee718
Matt Clay <matt@mystile.com>
2019-08-08 09:18:31 -0700

    Move ansible-test entry point. (#60281)

---
+++ test/lib/ansible_test/_data/cli/ansible_test_cli_stub.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# PYTHON_ARGCOMPLETE_OK
+"""Command line entry point for ansible-test."""
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import sys
+
+
+def main():
+    """Main program entry point."""
+    ansible_root = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__))))
+    source_root = os.path.join(ansible_root, 'test', 'lib')
+
+    if os.path.exists(os.path.join(ansible_root, 'setup.py')) and os.path.exists(os.path.join(source_root, 'ansible_test', '_internal', 'cli.py')):
+        # running from source, use that version of ansible-test instead of any version that may already be installed
+        sys.path.insert(0, source_root)
+
+    from ansible_test._internal.cli import main as cli_main
+
+    cli_main()
+
+
+if __name__ == '__main__':
+    main()
