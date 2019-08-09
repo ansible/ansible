3290b8343cd905182a9be2f184294256c1fd1d0c
Felix Fontein <felix@fontein.de>
2019-08-09 23:50:11 +0200

    docker: fix sanity errors (#60047)
    * Remove sanity check errors.
    
    * More linting.
    
    * Forgot to update places.
    
    * Remove choices which aren't provided in argspec.

--- lib/ansible/module_utils/docker/swarm.py
+++ lib/ansible/module_utils/docker/swarm.py
@@ -2,6 +2,10 @@
 # (c) Thierry Bouvet (@tbouvet)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
 import json
 from time import sleep
 
