473bd6bb0a72ee6feeac8830c05f293c10e02bad
Matt Clay <matt@mystile.com>
2019-08-09 17:56:44 -0700

    Relax boilerplate sanity tests.
    This will avoid requiring boilerplate for documentation only python files.

--- test/lib/ansible_test/_data/sanity/code-smell/future-import-boilerplate.py
+++ test/lib/ansible_test/_data/sanity/code-smell/future-import-boilerplate.py
@@ -2,6 +2,7 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import ast
 import sys
 
 
@@ -20,6 +21,22 @@ def main():
                         b'from __future__ import absolute_import, division, print_function'):
                 missing = False
                 break
+
+        if missing:
+            with open(path) as file:
+                contents = file.read()
+
+            # noinspection PyBroadException
+            try:
+                node = ast.parse(contents)
+
+                # files consisting of only assignments have no need for future import boilerplate
+                # the only exception would be division during assignment, but we'll overlook that for simplicity
+                # the most likely case is that of a documentation only python file
+                if all(isinstance(statement, ast.Assign) for statement in node.body):
+                    missing = False
+            except Exception:  # pylint: disable=broad-except
+                pass  # the compile sanity test will report this error
 
         if missing:
             print('%s: missing: from __future__ import (absolute_import, division, print_function)' % path)
