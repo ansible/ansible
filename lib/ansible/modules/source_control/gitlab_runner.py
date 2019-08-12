0c77a5f6cd8cd229d8946e79ce3f49ad6dfe59c3
Alexander Kapshuna <kapsh@kap.sh>
2019-08-12 18:50:34 +0300

    gitlab_runner: document minimal dependency version (#60371)

--- lib/ansible/modules/source_control/gitlab_runner.py
+++ lib/ansible/modules/source_control/gitlab_runner.py
@@ -34,7 +34,7 @@ author:
   - Guillaume Martinez (@Lunik)
 requirements:
   - python >= 2.7
-  - python-gitlab python module
+  - python-gitlab >= 1.5.0
 extends_documentation_fragment:
     - auth_basic
 options:
