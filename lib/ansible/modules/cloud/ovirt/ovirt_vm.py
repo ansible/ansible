96aab4a4677bb0918a4d3f1b05ebe7b75585b2d8
mnecas <necas.marty@gmail.com>
2019-08-13 13:22:01 +0200

    ovirt_vm add kernel param check

--- lib/ansible/modules/cloud/ovirt/ovirt_vm.py
+++ lib/ansible/modules/cloud/ovirt/ovirt_vm.py
@@ -1250,6 +1250,7 @@ from ansible.module_utils.ovirt import (
     search_by_attributes,
     search_by_name,
     wait,
+    engine_supported,
 )
 
 
@@ -2255,6 +2256,15 @@ def import_vm(module, connection):
     return True
 
 
+def check_deprecated_params(module, connection):
+    if engine_supported(connection, '4.4') and \
+            (module.params.get('kernel_params_persist') or
+             module.params.get('kernel_path') or
+             module.params.get('initrd_path') or
+             module.params.get('kernel_params')):
+        module.warn('Parameters \'kernel_params_persist\', \'kernel_path\', \'initrd_path\', \'kernel_params\' are not supported with ovrit engine 4.4.')
+
+
 def control_state(vm, vms_service, module):
     if vm is None:
         return
@@ -2403,6 +2413,7 @@ def main():
         state = module.params['state']
         auth = module.params.pop('auth')
         connection = create_connection(auth)
+        check_deprecated_params(module, connection)
         vms_service = connection.system_service().vms_service()
         vms_module = VmsModule(
             connection=connection,
