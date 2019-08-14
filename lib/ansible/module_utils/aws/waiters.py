3e570052757d130b8d9c209e1beb87990f67e178
Jill Rouleau <jill.rouleau@bespokess.com>
2019-08-14 16:53:31 -0700

    Fix ec2_vpc_vgw broken tests
    Add waiter function to wait for API to report detached vgw is available.
    Also catch extra error code in attach retry as EC2 sometimes reports that
    the vgw is available several seconds before permitting the attachment.
    
    Fixes: #53185

--- lib/ansible/module_utils/aws/waiters.py
+++ lib/ansible/module_utils/aws/waiters.py
@@ -151,6 +151,24 @@ ec2_data = {
                 },
             ]
         },
+        "VpnGatewayDetached": {
+            "delay": 5,
+            "maxAttempts": 40,
+            "operation": "DescribeVpnGateways",
+            "acceptors": [
+                {
+                    "matcher": "path",
+                    "expected": True,
+                    "argument": "VpnGateways[0].State == 'available'",
+                    "state": "success"
+                },
+                {
+                    "matcher": "error",
+                    "expected": False,
+                    "state": "retry"
+                },
+            ]
+        },
     }
 }
 
@@ -314,6 +332,12 @@ waiters_by_name = {
     ('EC2', 'vpn_gateway_exists'): lambda ec2: core_waiter.Waiter(
         'vpn_gateway_exists',
         ec2_model('VpnGatewayExists'),
+        core_waiter.NormalizedOperationMethod(
+            ec2.describe_vpn_gateways
+        )),
+    ('EC2', 'vgw_detached'): lambda ec2: core_waiter.Waiter(
+        'vgw_detached',
+        ec2_model('VpnGatewayDetached'),
         core_waiter.NormalizedOperationMethod(
             ec2.describe_vpn_gateways
         )),
