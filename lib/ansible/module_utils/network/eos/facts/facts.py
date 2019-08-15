3848cc958c390e498b9b88ecaeca5fefd1919f0f
Nathaniel Case <ncase@redhat.com>
2019-08-15 12:54:47 -0400

    Initial commit of module files

--- lib/ansible/module_utils/network/eos/facts/facts.py
+++ lib/ansible/module_utils/network/eos/facts/facts.py
@@ -12,8 +12,8 @@ __metaclass__ = type
 
 from ansible.module_utils.network.common.facts.facts import FactsBase
 from ansible.module_utils.network.eos.argspec.facts.facts import FactsArgs
-from ansible.module_utils.network.eos.argspec.interfaces.interfaces import InterfacesArgs
 from ansible.module_utils.network.eos.facts.interfaces.interfaces import InterfacesFacts
+from ansible.module_utils.network.eos.facts.vlans.vlans import VlansFacts
 from ansible.module_utils.network.eos.facts.legacy.base import Default, Hardware, Config, Interfaces
 
 
@@ -25,6 +25,7 @@ FACT_LEGACY_SUBSETS = dict(
 )
 FACT_RESOURCE_SUBSETS = dict(
     interfaces=InterfacesFacts,
+    vlans=VlansFacts,
 )
 
 
