865718c4bbdf73201fcfa193c828ba292dda3b9f
Paul Belanger <pabelanger@redhat.com>
2019-08-15 14:58:57 -0400

    Remove unused imports / variables (#60654)
    This is to help with the migration to collections.
    
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- lib/ansible/modules/network/ios/ios_vlan.py
+++ lib/ansible/modules/network/ios/ios_vlan.py
@@ -118,11 +118,10 @@ from copy import deepcopy
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.network.common.utils import remove_default_spec
 from ansible.module_utils.network.ios.ios import load_config, run_commands, normalize_interface
-from ansible.module_utils.network.ios.ios import ios_argument_spec, check_args
+from ansible.module_utils.network.ios.ios import ios_argument_spec
 
 
 def search_obj_in_list(vlan_id, lst):
-    obj = list()
     for o in lst:
         if o['vlan_id'] == vlan_id:
             return o
