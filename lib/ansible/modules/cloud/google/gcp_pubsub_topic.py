18bd2b9c2a4e345312a2cdd454f06855b80437c6
The Magician <magic-modules@google.com>
2019-08-16 12:23:48 -0700

    Bug fixes for GCP modules (#60667)

--- lib/ansible/modules/cloud/google/gcp_pubsub_topic.py
+++ lib/ansible/modules/cloud/google/gcp_pubsub_topic.py
@@ -56,7 +56,7 @@ options:
   kms_key_name:
     description:
     - The resource name of the Cloud KMS CryptoKey to be used to protect access to
-      messsages published on this topic. Your project's PubSub service account (`service-{{PROJECT_NUMBER}}@gcp-sa-pubsub.iam.gserviceaccount.com`)
+      messages published on this topic. Your project's PubSub service account (`service-{{PROJECT_NUMBER}}@gcp-sa-pubsub.iam.gserviceaccount.com`)
       must have `roles/cloudkms.cryptoKeyEncrypterDecrypter` to use this feature.
     - The expected format is `projects/*/locations/*/keyRings/*/cryptoKeys/*` .
     required: false
@@ -110,7 +110,7 @@ name:
   type: str
 kmsKeyName:
   description:
-  - The resource name of the Cloud KMS CryptoKey to be used to protect access to messsages
+  - The resource name of the Cloud KMS CryptoKey to be used to protect access to messages
     published on this topic. Your project's PubSub service account (`service-{{PROJECT_NUMBER}}@gcp-sa-pubsub.iam.gserviceaccount.com`)
     must have `roles/cloudkms.cryptoKeyEncrypterDecrypter` to use this feature.
   - The expected format is `projects/*/locations/*/keyRings/*/cryptoKeys/*` .
--- lib/ansible/modules/cloud/google/gcp_sql_instance.py
+++ lib/ansible/modules/cloud/google/gcp_sql_instance.py
@@ -175,7 +175,7 @@ options:
             - Path to a SQL dump file in Google Cloud Storage from which the slave
               instance is to be created. The URI is in the form gs://bucketName/fileName.
               Compressed gzip files (.gz) are also supported. Dumps should have the
-              binlog co-ordinates from which replication should begin. This can be
+              binlog coordinates from which replication should begin. This can be
               accomplished by setting --master-data to 1 when using mysqldump.
             required: false
             type: str
@@ -495,7 +495,7 @@ replicaConfiguration:
           description:
           - Path to a SQL dump file in Google Cloud Storage from which the slave instance
             is to be created. The URI is in the form gs://bucketName/fileName. Compressed
-            gzip files (.gz) are also supported. Dumps should have the binlog co-ordinates
+            gzip files (.gz) are also supported. Dumps should have the binlog coordinates
             from which replication should begin. This can be accomplished by setting
             --master-data to 1 when using mysqldump.
           returned: success
