8da9cd16b4828f1949d12e6e88bd469c1c917d08
Sumit Jaiswal <sjaiswal@redhat.com>
2019-08-17 23:02:36 +0530

    Resource module for ios_l2_interfaces (#60344)
    * ios_l2_interface checkin
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>

--- lib/ansible/module_utils/network/common/utils.py
+++ lib/ansible/module_utils/network/common/utils.py
@@ -594,6 +594,13 @@ def validate_config(spec, data):
     return validated_data
 
 
+def search_obj_in_list(name, lst, key='name'):
+    for item in lst:
+        if item[key] == name:
+            return item
+    return None
+
+
 class Template:
 
     def __init__(self):
