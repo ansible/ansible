b0c750ccf1812ad6b927a235a0f1fb4028163e7f
s-hertel <shertel@redhat.com>
2019-08-16 20:33:43 -0400

    test extra vars, templating, and inline vaults

--- test/integration/targets/inventory_plugin_config/runme.sh
+++ test/integration/targets/inventory_plugin_config/runme.sh
@@ -14,3 +14,23 @@ ansible-inventory --list -i ./config_without_parameter.yml --export | \
 ansible-inventory --list -i ./config_with_parameter.yml --export | \
     env python -c "import json, sys; inv = json.loads(sys.stdin.read()); \
                    assert set(inv['_meta']['hostvars']['test_host']['departments']) == set(['paris'])"
+
+secret=$(ansible-vault encrypt_string 'secret' --vault-password-file ./password_file)
+
+cat << EOF > "config.yml"
+plugin: test_inventory
+vaulted_option: $secret
+templateable_option: "{{ template_me }}"
+ignore_template: "{{ do_not_template_me }}"
+EOF
+
+export ANSIBLE_INVENTORY_ANY_UNPARSED_IS_FAILED=False
+ansible-inventory -i ./config.yml --vault-password-file ./password_file --list 2>&1 | grep "'template_me' is undefined"
+
+unset ANSIBLE_INVENTORY_ANY_UNPARSED_IS_FAILED
+ansible-inventory -i ./config.yml -e template_me='templated' --vault-password-file ./password_file --list | tee out.txt
+
+grep '"ignore_template": "{{ do_not_template_me }}"' out.txt
+grep '"inline_vault": "secret"' out.txt
+grep '"template": "templated"' out.txt
+grep '"template_me": "templated"' out.txt
--- test/integration/targets/inventory_plugin_config/test_inventory.py
+++ test/integration/targets/inventory_plugin_config/test_inventory.py
@@ -18,6 +18,19 @@ DOCUMENTATION = '''
                 - seine-et-marne
                 - haute-garonne
             required: False
+        vaulted_option:
+            description: test inline vaulted options
+            type: str
+            required: False
+        templateable_option:
+            description: test templating options
+            type: str
+            required: False
+        ignore_template:
+            description: test template blacklisting
+            type: str
+            required: False
+            template_supported: False
 '''
 
 EXAMPLES = '''
@@ -33,6 +46,7 @@ from ansible.plugins.inventory import BaseInventoryPlugin
 
 class InventoryModule(BaseInventoryPlugin):
     NAME = 'test_inventory'
+    TEMPLATE_OPTIONS = True
 
     def verify_file(self, path):
         return True
@@ -42,6 +56,9 @@ class InventoryModule(BaseInventoryPlugin):
         self._read_config_data(path=path)
 
         departments = self.get_option('departments')
+        vaulted_option = self.get_option('vaulted_option')
+        templateable = self.get_option('templateable_option')
+        ignore_template = self.get_option('ignore_template')
 
         group = 'test_group'
         host = 'test_host'
@@ -49,3 +66,8 @@ class InventoryModule(BaseInventoryPlugin):
         self.inventory.add_group(group)
         self.inventory.add_host(group=group, host=host)
         self.inventory.set_variable(host, 'departments', departments)
+
+        # Set config options as host vars for easy inspection
+        self.inventory.set_variable(host, 'inline_vault', vaulted_option)
+        self.inventory.set_variable(host, 'template', templateable)
+        self.inventory.set_variable(host, 'ignore_template', ignore_template)
