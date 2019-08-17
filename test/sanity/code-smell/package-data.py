0743791298c86bfa0f97e06337c8e16d4a5eb51f
Toshio Kuratomi <a.badger@gmail.com>
2019-08-17 15:53:00 -0700

    Add ansible_test files to package-data sanity test

--- test/sanity/code-smell/package-data.py
+++ test/sanity/code-smell/package-data.py
@@ -10,23 +10,41 @@ import tempfile
 import subprocess
 
 
-def main():
+def assemble_files_to_look_for():
+    """
+    This looks for all of the files which should show up in an installation of ansible
+    """
     ignore_files = frozenset((
-        '*/galaxy/data/default/*/.git_keep',
-        '*/galaxy/data/default/role/*/main.yml.j2',
-        '*/galaxy/data/default/role/*/test.yml.j2',
-        '*/galaxy/data/default/collection/plugins/README.md.j2',
+        '*/ansible/galaxy/data/default/*/.git_keep',
+        '*/ansible/galaxy/data/default/role/*/main.yml.j2',
+        '*/ansible/galaxy/data/default/role/*/test.yml.j2',
+        '*/ansible/galaxy/data/default/collection/plugins/README.md.j2',
+        '*/ansible_test/tests/*',
+        '*/ansible_test/tests/*/*',
     ))
 
-    non_py_files = []
+    pkg_data_files = []
     for path in sys.argv[1:] or sys.stdin.read().splitlines():
-        if os.path.splitext(path)[1] != '.py':
-            add = True
+        if path.startswith("lib/ansible"):
+            if os.path.splitext(path)[1] != '.py':
+                for ignore in ignore_files:
+                    if fnmatch.fnmatch(path, ignore):
+                        break
+                else:
+                    pkg_data_files.append(os.path.relpath(path, 'lib'))
+
+        elif path.startswith("test/lib/ansible_test"):
             for ignore in ignore_files:
                 if fnmatch.fnmatch(path, ignore):
-                    add = False
-            if add:
-                non_py_files.append(os.path.relpath(path, 'lib/ansible'))
+                    break
+            else:
+                pkg_data_files.append(os.path.relpath(path, 'test/lib'))
+
+    return pkg_data_files
+
+
+def main():
+    pkg_data_files = assemble_files_to_look_for()
 
     with tempfile.TemporaryDirectory() as tmp_dir:
         stdout, _dummy = subprocess.Popen(
@@ -35,12 +53,13 @@ def main():
             stderr=subprocess.PIPE,
             universal_newlines=True,
         ).communicate()
-        match = re.search('^creating (%s/.*?/(?:site|dist)-packages/ansible)$' % tmp_dir, stdout, flags=re.M)
+        match = re.search('^creating (%s/.*?/(?:site|dist)-packages)/ansible$' %
+                          tmp_dir, stdout, flags=re.M)
 
-        for filename in non_py_files:
+        for filename in pkg_data_files:
             path = os.path.join(match.group(1), filename)
             if not os.path.exists(path):
-                print('%s: File not installed' % os.path.join('lib', 'ansible', filename))
+                print('%s: File not installed' % os.path.join('lib', filename))
 
 
 if __name__ == '__main__':
