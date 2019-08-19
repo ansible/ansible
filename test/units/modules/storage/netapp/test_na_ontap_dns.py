a8a59bba38f578c437a7c7a60f8b9172e4ff00e1
Chris Archibald <carchi8py@gmail.com>
2019-08-19 12:26:40 -0700

    fix issues with tests

--- test/units/modules/storage/netapp/test_na_ontap_dns.py
+++ test/units/modules/storage/netapp/test_na_ontap_dns.py
@@ -3,7 +3,8 @@
 
 ''' unit tests for Ansible module: na_ontap_dns'''
 
-from __future__ import print_function
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 import json
 import pytest
 
