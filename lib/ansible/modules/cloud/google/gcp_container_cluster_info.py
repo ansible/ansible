57a248764c651b94a4947d2ca355975f8641c521
The Magician <magic-modules@google.com>
2019-08-19 12:52:58 -0700

    Bug fixes for GCP modules (#60722)

--- lib/ansible/modules/cloud/google/gcp_container_cluster_info.py
+++ lib/ansible/modules/cloud/google/gcp_container_cluster_info.py
@@ -586,12 +586,7 @@ def main():
     if not module.params['scopes']:
         module.params['scopes'] = ['https://www.googleapis.com/auth/cloud-platform']
 
-    items = fetch_list(module, collection(module))
-    if items.get('clusters'):
-        items = items.get('clusters')
-    else:
-        items = []
-    return_value = {'resources': items}
+    return_value = {'resources': fetch_list(module, collection(module))}
     module.exit_json(**return_value)
 
 
@@ -601,8 +596,7 @@ def collection(module):
 
 def fetch_list(module, link):
     auth = GcpSession(module, 'container')
-    response = auth.get(link)
-    return return_if_object(module, response)
+    return auth.list(link, return_if_object, array_name='clusters')
 
 
 def return_if_object(module, response):
