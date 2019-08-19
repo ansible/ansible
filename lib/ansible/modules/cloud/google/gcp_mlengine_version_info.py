92714707816e537094db9ba9da4f5862c28a35ae
The Magician <magic-modules@google.com>
2019-08-19 12:52:47 -0700

    Bug fixes for GCP modules (#60723)

--- lib/ansible/modules/cloud/google/gcp_mlengine_version_info.py
+++ lib/ansible/modules/cloud/google/gcp_mlengine_version_info.py
@@ -211,12 +211,7 @@ def main():
     if not module.params['scopes']:
         module.params['scopes'] = ['https://www.googleapis.com/auth/cloud-platform']
 
-    items = fetch_list(module, collection(module))
-    if items.get('versions'):
-        items = items.get('versions')
-    else:
-        items = []
-    return_value = {'resources': items}
+    return_value = {'resources': fetch_list(module, collection(module))}
     module.exit_json(**return_value)
 
 
@@ -227,8 +222,7 @@ def collection(module):
 
 def fetch_list(module, link):
     auth = GcpSession(module, 'mlengine')
-    response = auth.get(link)
-    return return_if_object(module, response)
+    return auth.list(link, return_if_object, array_name='versions')
 
 
 def return_if_object(module, response):
