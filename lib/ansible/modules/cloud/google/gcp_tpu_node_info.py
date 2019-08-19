1481d44ffae25618e4dbef641aaa32be570fe22b
The Magician <magic-modules@google.com>
2019-08-19 12:52:39 -0700

    Bug fixes for GCP modules (#60724)

--- lib/ansible/modules/cloud/google/gcp_tpu_node_info.py
+++ lib/ansible/modules/cloud/google/gcp_tpu_node_info.py
@@ -167,12 +167,7 @@ def main():
     if not module.params['scopes']:
         module.params['scopes'] = ['https://www.googleapis.com/auth/cloud-platform']
 
-    items = fetch_list(module, collection(module))
-    if items.get('nodes'):
-        items = items.get('nodes')
-    else:
-        items = []
-    return_value = {'resources': items}
+    return_value = {'resources': fetch_list(module, collection(module))}
     module.exit_json(**return_value)
 
 
@@ -182,8 +177,7 @@ def collection(module):
 
 def fetch_list(module, link):
     auth = GcpSession(module, 'tpu')
-    response = auth.get(link)
-    return return_if_object(module, response)
+    return auth.list(link, return_if_object, array_name='nodes')
 
 
 def return_if_object(module, response):
