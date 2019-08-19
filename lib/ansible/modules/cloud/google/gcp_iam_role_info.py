720fbad4271fcb2006c620d7a25b5e420e833ed0
The Magician <magic-modules@google.com>
2019-08-19 12:52:58 -0700

    Bug fixes for GCP modules (#60722)

--- lib/ansible/modules/cloud/google/gcp_iam_role_info.py
+++ lib/ansible/modules/cloud/google/gcp_iam_role_info.py
@@ -112,12 +112,7 @@ def main():
     if not module.params['scopes']:
         module.params['scopes'] = ['https://www.googleapis.com/auth/iam']
 
-    items = fetch_list(module, collection(module))
-    if items.get('roles'):
-        items = items.get('roles')
-    else:
-        items = []
-    return_value = {'resources': items}
+    return_value = {'resources': fetch_list(module, collection(module))}
     module.exit_json(**return_value)
 
 
@@ -127,8 +122,7 @@ def collection(module):
 
 def fetch_list(module, link):
     auth = GcpSession(module, 'iam')
-    response = auth.get(link)
-    return return_if_object(module, response)
+    return auth.list(link, return_if_object, array_name='roles')
 
 
 def return_if_object(module, response):
