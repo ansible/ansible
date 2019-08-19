95aa86fcc3457968f647ebb74f5be49a17471b1c
The Magician <magic-modules@google.com>
2019-08-19 12:53:30 -0700

    Bug fixes for GCP modules (#60719)

--- lib/ansible/modules/cloud/google/gcp_cloudbuild_trigger_info.py
+++ lib/ansible/modules/cloud/google/gcp_cloudbuild_trigger_info.py
@@ -324,12 +324,7 @@ def main():
     if not module.params['scopes']:
         module.params['scopes'] = ['https://www.googleapis.com/auth/cloud-platform']
 
-    items = fetch_list(module, collection(module))
-    if items.get('triggers'):
-        items = items.get('triggers')
-    else:
-        items = []
-    return_value = {'resources': items}
+    return_value = {'resources': fetch_list(module, collection(module))}
     module.exit_json(**return_value)
 
 
@@ -339,8 +334,7 @@ def collection(module):
 
 def fetch_list(module, link):
     auth = GcpSession(module, 'cloudbuild')
-    response = auth.get(link)
-    return return_if_object(module, response)
+    return auth.list(link, return_if_object, array_name='triggers')
 
 
 def return_if_object(module, response):
