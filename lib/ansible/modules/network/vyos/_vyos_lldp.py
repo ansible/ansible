38cd4fd00f659fedd69fc5bc8c5fc1dca5c4e3b9
Rohit <rohitthakur2590@outlook.com>
2019-08-19 17:31:09 +0530

    vyos_lldp_global (#59627)
    * vyos_lldp_global
    
    Signed-off-by: rohitthakur2590 <rohitthakur2590@outlook.com>
    
    * sanity checks
    
    Signed-off-by: rohitthakur2590 <rohitthakur2590@outlook.com>
    
    * deprecated changes
    
    Signed-off-by: rohitthakur2590 <rohitthakur2590@outlook.com>
    
    * comments incorporated
    
    Signed-off-by: rohitthakur2590 <rohitthakur2590@outlook.com>
    
    * test cases updated
    
    Signed-off-by: rohitthakur2590 <rohitthakur2590@outlook.com>
    
    * comments incorporated
    
    Signed-off-by: rohitthakur2590 <rohitthakur2590@outlook.com>
    
    * branch conflict resolved
    
    Signed-off-by: rohitthakur2590 <rohitthakur2590@outlook.com>
    
    * sanity check passes
    
    Signed-off-by: rohitthakur2590 <rohitthakur2590@outlook.com>

--- lib/ansible/modules/network/vyos/_vyos_lldp.py
+++ lib/ansible/modules/network/vyos/_vyos_lldp.py
@@ -19,8 +19,12 @@
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
 ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
+                    'status': ['deprecated'],
                     'supported_by': 'network'}
 
 
@@ -33,14 +37,23 @@ short_description: Manage LLDP configuration on VyOS network devices
 description:
   - This module provides declarative management of LLDP service
     on VyOS network devices.
+deprecated:
+  removed_in: '2.13'
+  alternative: vyos_lldp_global
+  why: Updated modules released with more functionality.
 notes:
   - Tested against VYOS 1.1.7
 options:
+  interfaces:
+    description:
+      - Name of the interfaces.
+    type: list
   state:
     description:
-      - State of the LLDP configuration.
+      - State of the link aggregation group.
     default: present
-    choices: ['present', 'absent']
+    choices: ['present', 'absent', 'enabled', 'disabled']
+    type: str
 extends_documentation_fragment: vyos
 """
 
