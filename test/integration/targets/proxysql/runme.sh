c43156219988e0f7b2696231cd8c07c8fc7a929e
Markus Bergholz <markus.bergholz@lekker.de>
2019-08-19 13:55:34 +0200

    remove integration tests

--- test/integration/targets/proxysql/runme.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Usage: source ../setup_paramiko/setup.sh
-
-set -eux
-
-source virtualenv.sh  # for pip installs, if needed, otherwise unused
-ansible-playbook -i ../proxysql/inventory proxysql.yml "$@"
