411ea4073b3a95c44aa9b3e7da9d42b9e2fdd47f
Modular Magician <magic-modules@google.com>
2019-08-19 06:06:26 +0000

    Bug fixes for GCP modules

--- lib/ansible/modules/cloud/google/gcp_pubsub_topic_info.py
+++ lib/ansible/modules/cloud/google/gcp_pubsub_topic_info.py
@@ -117,12 +117,7 @@ def main():
     if not module.params['scopes']:
         module.params['scopes'] = ['https://www.googleapis.com/auth/pubsub']
 
-    items = fetch_list(module, collection(module))
-    if items.get('topics'):
-        items = items.get('topics')
-    else:
-        items = []
-    return_value = {'resources': items}
+    return_value = {'resources': fetch_list(module, collection(module))}
     module.exit_json(**return_value)
 
 
@@ -132,8 +127,7 @@ def collection(module):
 
 def fetch_list(module, link):
     auth = GcpSession(module, 'pubsub')
-    response = auth.get(link)
-    return return_if_object(module, response)
+    return auth.list(link, return_if_object, array_name='topics')
 
 
 def return_if_object(module, response):
