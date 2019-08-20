ac3c8263b60ffa336682a901116ec53c799114a7
Chris Archibald <carchi8py@gmail.com>
2019-08-20 10:31:21 -0700

    update copyright (#60906)

--- lib/ansible/modules/storage/netapp/na_ontap_snmp.py
+++ lib/ansible/modules/storage/netapp/na_ontap_snmp.py
@@ -3,7 +3,7 @@
 create SNMP module to add/delete/modify SNMP user
 """
 
-# (c) 2018, NetApp, Inc
+# (c) 2018-2019, NetApp, Inc
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
