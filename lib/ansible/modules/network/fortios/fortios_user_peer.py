a959ddf47dc3b07524bafc3478dabf669f2c2251
Miguel Angel Muñoz González <magonzalez@fortinet.com>
2019-08-21 21:24:15 +0200

    Update fortios_user_peer.py

--- lib/ansible/modules/network/fortios/fortios_user_peer.py
+++ lib/ansible/modules/network/fortios/fortios_user_peer.py
@@ -13,7 +13,7 @@ from __future__ import (absolute_import, division, print_function)
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>
 
 __metaclass__ = type
 
