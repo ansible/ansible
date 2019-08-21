6336e4f65ab5666cc5fc72bd9efdc6fd77911cae
Joshua Edmonds <edmonds@netapp.com>
2019-08-21 15:17:23 +1000

    fixes

--- test/units/modules/storage/netapp/test_na_ontap_qos_adaptive_policy_group.py
+++ test/units/modules/storage/netapp/test_na_ontap_qos_adaptive_policy_group.py
@@ -3,7 +3,8 @@
 
 ''' unit test template for ONTAP Ansible module '''
 
-from __future__ import print_function
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 import json
 import pytest
 
