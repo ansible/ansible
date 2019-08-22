197335b024466c13374528785bc7ffd6d9535b67
frankshen01 <49291382+frankshen01@users.noreply.github.com>
2019-08-22 18:27:49 +0800

    Update fortios_wireless_controller_hotspot20_anqp_network_auth_type.py

--- lib/ansible/modules/network/fortios/fortios_wireless_controller_hotspot20_anqp_network_auth_type.py
+++ lib/ansible/modules/network/fortios/fortios_wireless_controller_hotspot20_anqp_network_auth_type.py
@@ -9,7 +9,7 @@ from __future__ import (absolute_import, division, print_function)
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
