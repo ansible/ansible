d2d3a32f6e51db090629901e95ab33f85acd2ef9
Wojciech Wypior <w.wypior@f5.com>
2019-08-22 04:50:28 +0100

    fixes an issue where config sync with the module fails if there are sync failures on a different device group than the one being synced (#61067)

--- lib/ansible/modules/network/f5/bigip_configsync_action.py
+++ lib/ansible/modules/network/f5/bigip_configsync_action.py
@@ -352,7 +352,12 @@ class ModuleManager(object):
     def _get_details_from_resource(self):
         resource = self.read_current_from_device()
         stats = resource['entries'].copy()
-        tree = Tree(stats)
+        if HAS_OBJPATH:
+            tree = Tree(stats)
+        else:
+            raise F5ModuleError(
+                "objectpath module required, install objectpath module to continue. "
+            )
         details = list(tree.execute('$..*["details"]["description"]'))
         result = details[::-1]
         return result
