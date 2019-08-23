8b02e1633342006d98750bce28a93d629aa9cad6
Ryan Conway <ryan@rjc.cc>
2019-08-23 17:41:47 +0100

    Update module examples.

--- lib/ansible/modules/identity/onepassword_info.py
+++ lib/ansible/modules/identity/onepassword_info.py
@@ -96,6 +96,7 @@ EXAMPLES = '''
   onepassword_info:
     search_terms: My 1Password item
   delegate_to: localhost
+  register_to: my_1password_item
   no_log: true         # Don't want to log the secrets to the console!
 
 # Gather secrets from 1Password, with more advanced search terms:
@@ -107,6 +108,7 @@ EXAMPLES = '''
         section: Custom section name     # optional, defaults to 'None'
         vault:   Name of the vault       # optional, only necessary if there is more than 1 Vault available
   delegate_to: localhost
+  register_to: my_1password_item
   no_log: True                           # Don't want to log the secrets to the console!
 
 # Gather secrets combining simple and advanced search terms to retrieve two items, one of which we fetch two
@@ -123,7 +125,12 @@ EXAMPLES = '''
         vault:   Name of the vault       # optional, only necessary if there is more than 1 Vault available
       - name: A 1Password item with document attachment
   delegate_to: localhost
+  register_to: my_1password_item
   no_log: true                           # Don't want to log the secrets to the console!
+
+- name: Debug a password (for example)
+  debug:
+    msg: "{{ my_1password_item['onepassword']['My 1Password item'] }}"
 '''
 
 RETURN = '''
