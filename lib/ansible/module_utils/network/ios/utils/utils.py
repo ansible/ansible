9e75638465494ca39576bb73dff0b9e66af62b74
Sumit Jaiswal <sjaiswal@redhat.com>
2019-08-23 22:09:20 +0530

    Resource module for ios_l3_interfaces (#60405)
    * ios_l3_interfaces
    
    * fix shippable
    
    * fix tests
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>
    
    * fix ignore txt file
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>
    
    * fix doc
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>
    
    * fix shippable n tests
    
    * fix replace test
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>
    
    * fix zuul failures
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>
    
    * fix shippable n zuul
    
    * fix zuul
    
    * fix zuul
    
    * fix zuul test
    
    * fix zuul
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>
    
    * code zuul fix
    
    * fix merge
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>
    
    * fix merge
    
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>

--- lib/ansible/module_utils/network/ios/utils/utils.py
+++ lib/ansible/module_utils/network/ios/utils/utils.py
@@ -80,20 +80,22 @@ def filter_dict_having_none_value(want, have):
                 if value is None:
                     dict_val = have.get(k).get(key)
                     test_key_dict.update({key: dict_val})
-                # below conditions checks are added to check if
-                # secondary IP is configured, if yes then delete
-                # the already configured IP if want and have IP
-                # is different else if it's same no need to delete
-                if isinstance(value, str):
-                    want_ip = value.split('/')
-                have_ip = have.get('ipv4')
-                if len(want_ip) > 1 and have_ip:
-                    have_ip = have_ip[0]['address'].split(' ')[0]
-                    if have_ip != want_ip[0]:
-                        diff_ip = True
-                if key == 'secondary' and value is True and diff_ip is True:
-                    test_key_dict.update({key: value})
                 test_dict.update({k: test_key_dict})
+            # below conditions checks are added to check if
+            # secondary IP is configured, if yes then delete
+            # the already configured IP if want and have IP
+            # is different else if it's same no need to delete
+            for each in v:
+                if each.get('secondary'):
+                    want_ip = each.get('address').split('/')
+                    have_ip = have.get('ipv4')
+                    if len(want_ip) > 1 and have_ip and have_ip[0].get('secondary'):
+                        have_ip = have_ip[0]['address'].split(' ')[0]
+                        if have_ip != want_ip[0]:
+                            diff_ip = True
+                    if each.get('secondary') and diff_ip is True:
+                        test_key_dict.update({'secondary': True})
+                    test_dict.update({'ipv4': test_key_dict})
         if v is None:
             val = have.get(k)
             test_dict.update({k: val})
