c03febf9893ad72f9b10670ce8b7601dd9e33152
Strahinja Kustudic <kustodian@gmail.com>
2019-08-23 16:33:03 +0200

    Fix extended loop_control with includes

--- test/integration/targets/loop_control/runme.sh
+++ test/integration/targets/loop_control/runme.sh
@@ -9,3 +9,14 @@ MATCH='foo_label
 bar_label'
 [ "$(ansible-playbook label.yml "$@" |grep 'item='|sed -e 's/^.*(item=looped_var \(.*\)).*$/\1/')" == "${MATCH}" ]
 
+# user output has:
+#ok: [localhost] => {
+#    "msg": "1 0 3 2 True False 3 first,second,third"
+#ok: [localhost] => {
+#    "msg": "2 1 2 1 False False 3 first,second,third"
+#ok: [localhost] => {
+#    "msg": "3 2 1 0 False True 3 first,second,third"
+MATCH='"1 0 3 2 True False 3 first,second,third"
+"2 1 2 1 False False 3 first,second,third"
+"3 2 1 0 False True 3 first,second,third"'
+[ "$(ansible-playbook extended.yml "$@" |grep '"msg":'|sed -e 's/^.*"msg": \(.*\)$/\1/')" == "${MATCH}" ]
