acf04fe01f05a6c9ed54754a4490863dfa2ff1c9
shashik <shashi@alethea.in>
2019-08-23 14:51:29 +0530

    updated notes section

--- lib/ansible/modules/network/icx/icx_config.py
+++ lib/ansible/modules/network/icx/icx_config.py
@@ -21,7 +21,8 @@ description:
     an implementation for working with ICX configuration sections in
     a deterministic way.
 notes:
-  - Tested against ICX 10.1
+  - Tested against ICX 10.1.
+  - For information on using ICX platform, see L(the ICX OS Platform Options guide,../network/user_guide/platform_icx.html).
 options:
   lines:
     description:
