ff23d7c13f48dbcaf940ec51c2f5202afb505a82
Miguel Angel Muñoz González <magonzalez@fortinet.com>
2019-08-23 11:24:43 +0200

    FortiOS modules for 2.9 - 6 (#61170)
    * FortiOS modules for 2.9 - 5
    
    * Update fortios_firewall_vip.py
    
    * Update fortios_firewall_vip6.py

--- lib/ansible/modules/network/fortios/fortios_log_eventfilter.py
+++ lib/ansible/modules/network/fortios/fortios_log_eventfilter.py
@@ -14,9 +14,6 @@ from __future__ import (absolute_import, division, print_function)
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-# the lib use python logging can get it if the following is set in your
-# Ansible config.
 
 __metaclass__ = type
 
@@ -29,10 +26,10 @@ DOCUMENTATION = '''
 module: fortios_log_eventfilter
 short_description: Configure log event filters in Fortinet's FortiOS and FortiGate.
 description:
-    - This module is able to configure a FortiGate or FortiOS by allowing the
+    - This module is able to configure a FortiGate or FortiOS (FOS) device by allowing the
       user to set and modify log feature and eventfilter category.
       Examples include all parameters and values need to be adjusted to datasources before usage.
-      Tested with FOS v6.0.2
+      Tested with FOS v6.0.5
 version_added: "2.8"
 author:
     - Miguel Angel Munoz (@mamunozgonzalez)
@@ -44,97 +41,118 @@ requirements:
     - fortiosapi>=0.9.8
 options:
     host:
-       description:
-            - FortiOS or FortiGate ip address.
-       required: true
+        description:
+            - FortiOS or FortiGate IP address.
+        type: str
+        required: false
     username:
         description:
             - FortiOS or FortiGate username.
-        required: true
+        type: str
+        required: false
     password:
         description:
             - FortiOS or FortiGate password.
+        type: str
         default: ""
     vdom:
         description:
             - Virtual domain, among those defined previously. A vdom is a
               virtual instance of the FortiGate that can be configured and
               used as a different unit.
+        type: str
         default: root
     https:
         description:
-            - Indicates if the requests towards FortiGate must use HTTPS
-              protocol
+            - Indicates if the requests towards FortiGate must use HTTPS protocol.
         type: bool
         default: true
+    ssl_verify:
+        description:
+            - Ensures FortiGate certificate must be verified by a proper CA.
+        type: bool
+        default: true
+        version_added: 2.9
     log_eventfilter:
         description:
             - Configure log event filters.
         default: null
+        type: dict
         suboptions:
-            compliance-check:
+            compliance_check:
                 description:
                     - Enable/disable PCI DSS compliance check logging.
+                type: str
                 choices:
                     - enable
                     - disable
             endpoint:
                 description:
                     - Enable/disable endpoint event logging.
+                type: str
                 choices:
                     - enable
                     - disable
             event:
                 description:
                     - Enable/disable event logging.
+                type: str
                 choices:
                     - enable
                     - disable
             ha:
                 description:
                     - Enable/disable ha event logging.
+                type: str
                 choices:
                     - enable
                     - disable
             router:
                 description:
                     - Enable/disable router event logging.
+                type: str
                 choices:
                     - enable
                     - disable
-            security-rating:
+            security_rating:
                 description:
                     - Enable/disable Security Rating result logging.
+                type: str
                 choices:
                     - enable
                     - disable
             system:
                 description:
                     - Enable/disable system event logging.
+                type: str
                 choices:
                     - enable
                     - disable
             user:
                 description:
                     - Enable/disable user authentication event logging.
+                type: str
                 choices:
                     - enable
                     - disable
             vpn:
                 description:
                     - Enable/disable VPN event logging.
+                type: str
                 choices:
                     - enable
                     - disable
-            wan-opt:
+            wan_opt:
                 description:
                     - Enable/disable WAN optimization event logging.
+                type: str
                 choices:
                     - enable
                     - disable
-            wireless-activity:
+            wireless_activity:
                 description:
                     - Enable/disable wireless event logging.
+                type: str
                 choices:
                     - enable
                     - disable
@@ -147,6 +165,7 @@ EXAMPLES = '''
    username: "admin"
    password: ""
    vdom: "root"
+   ssl_verify: "False"
   tasks:
   - name: Configure log event filters.
     fortios_log_eventfilter:
@@ -156,17 +175,17 @@ EXAMPLES = '''
       vdom:  "{{ vdom }}"
       https: "False"
       log_eventfilter:
-        compliance-check: "enable"
+        compliance_check: "enable"
         endpoint: "enable"
         event: "enable"
         ha: "enable"
         router: "enable"
-        security-rating: "enable"
+        security_rating: "enable"
         system: "enable"
         user: "enable"
         vpn: "enable"
-        wan-opt: "enable"
-        wireless-activity: "enable"
+        wan_opt: "enable"
+        wireless_activity: "enable"
 '''
 
 RETURN = '''
@@ -229,14 +248,16 @@ version:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.connection import Connection
+from ansible.module_utils.network.fortios.fortios import FortiOSHandler
+from ansible.module_utils.network.fortimanager.common import FAIL_SOCKET_MSG
 
-fos = None
 
-
-def login(data):
+def login(data, fos):
     host = data['host']
     username = data['username']
     password = data['password']
+    ssl_verify = data['ssl_verify']
 
     fos.debug('on')
     if 'https' in data and not data['https']:
@@ -244,14 +265,14 @@ def login(data):
     else:
         fos.https('on')
 
-    fos.login(host, username, password)
+    fos.login(host, username, password, verify=ssl_verify)
 
 
 def filter_log_eventfilter_data(json):
-    option_list = ['compliance-check', 'endpoint', 'event',
-                   'ha', 'router', 'security-rating',
+    option_list = ['compliance_check', 'endpoint', 'event',
+                   'ha', 'router', 'security_rating',
                    'system', 'user', 'vpn',
-                   'wan-opt', 'wireless-activity']
+                   'wan_opt', 'wireless_activity']
     dictionary = {}
 
     for attribute in option_list:
@@ -261,40 +282,57 @@ def filter_log_eventfilter_data(json):
     return dictionary
 
 
+def underscore_to_hyphen(data):
+    if isinstance(data, list):
+        for elem in data:
+            elem = underscore_to_hyphen(elem)
+    elif isinstance(data, dict):
+        new_data = {}
+        for k, v in data.items():
+            new_data[k.replace('_', '-')] = underscore_to_hyphen(v)
+        data = new_data
+
+    return data
+
+
 def log_eventfilter(data, fos):
     vdom = data['vdom']
     log_eventfilter_data = data['log_eventfilter']
-    filtered_data = filter_log_eventfilter_data(log_eventfilter_data)
+    filtered_data = underscore_to_hyphen(filter_log_eventfilter_data(log_eventfilter_data))
+
     return fos.set('log',
                    'eventfilter',
                    data=filtered_data,
                    vdom=vdom)
 
 
+def is_successful_status(status):
+    return status['status'] == "success" or \
+        status['http_method'] == "DELETE" and status['http_status'] == 404
+
+
 def fortios_log(data, fos):
-    login(data)
 
-    methodlist = ['log_eventfilter']
-    for method in methodlist:
-        if data[method]:
-            resp = eval(method)(data, fos)
-            break
+    if data['log_eventfilter']:
+        resp = log_eventfilter(data, fos)
 
-    fos.logout()
-    return not resp['status'] == "success", resp['status'] == "success", resp
+    return not is_successful_status(resp), \
+        resp['status'] == "success", \
+        resp
 
 
 def main():
     fields = {
-        "host": {"required": True, "type": "str"},
-        "username": {"required": True, "type": "str"},
-        "password": {"required": False, "type": "str", "no_log": True},
+        "host": {"required": False, "type": "str"},
+        "username": {"required": False, "type": "str"},
+        "password": {"required": False, "type": "str", "default": "", "no_log": True},
         "vdom": {"required": False, "type": "str", "default": "root"},
         "https": {"required": False, "type": "bool", "default": True},
+        "ssl_verify": {"required": False, "type": "bool", "default": True},
         "log_eventfilter": {
-            "required": False, "type": "dict",
+            "required": False, "type": "dict", "default": None,
             "options": {
-                "compliance-check": {"required": False, "type": "str",
+                "compliance_check": {"required": False, "type": "str",
                                      "choices": ["enable", "disable"]},
                 "endpoint": {"required": False, "type": "str",
                              "choices": ["enable", "disable"]},
@@ -304,7 +342,7 @@ def main():
                        "choices": ["enable", "disable"]},
                 "router": {"required": False, "type": "str",
                            "choices": ["enable", "disable"]},
-                "security-rating": {"required": False, "type": "str",
+                "security_rating": {"required": False, "type": "str",
                                     "choices": ["enable", "disable"]},
                 "system": {"required": False, "type": "str",
                            "choices": ["enable", "disable"]},
@@ -312,9 +350,9 @@ def main():
                          "choices": ["enable", "disable"]},
                 "vpn": {"required": False, "type": "str",
                         "choices": ["enable", "disable"]},
-                "wan-opt": {"required": False, "type": "str",
+                "wan_opt": {"required": False, "type": "str",
                             "choices": ["enable", "disable"]},
-                "wireless-activity": {"required": False, "type": "str",
+                "wireless_activity": {"required": False, "type": "str",
                                       "choices": ["enable", "disable"]}
 
             }
@@ -323,15 +361,31 @@ def main():
 
     module = AnsibleModule(argument_spec=fields,
                            supports_check_mode=False)
-    try:
-        from fortiosapi import FortiOSAPI
-    except ImportError:
-        module.fail_json(msg="fortiosapi module is required")
 
-    global fos
-    fos = FortiOSAPI()
+    # legacy_mode refers to using fortiosapi instead of HTTPAPI
+    legacy_mode = 'host' in module.params and module.params['host'] is not None and \
+                  'username' in module.params and module.params['username'] is not None and \
+                  'password' in module.params and module.params['password'] is not None
 
-    is_error, has_changed, result = fortios_log(module.params, fos)
+    if not legacy_mode:
+        if module._socket_path:
+            connection = Connection(module._socket_path)
+            fos = FortiOSHandler(connection)
+
+            is_error, has_changed, result = fortios_log(module.params, fos)
+        else:
+            module.fail_json(**FAIL_SOCKET_MSG)
+    else:
+        try:
+            from fortiosapi import FortiOSAPI
+        except ImportError:
+            module.fail_json(msg="fortiosapi module is required")
+
+        fos = FortiOSAPI()
+
+        login(module.params, fos)
+        is_error, has_changed, result = fortios_log(module.params, fos)
+        fos.logout()
 
     if not is_error:
         module.exit_json(changed=has_changed, meta=result)
