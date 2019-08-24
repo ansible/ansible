1770de5507a96c0b084a2bbd32d23a76ea7228be
Paul Belanger <pabelanger@redhat.com>
2019-08-24 10:12:29 -0400

    Remove duplicate imports from iosxr_lacp_interfaces (#61258)
    This cleans up linting for our new iosxr collection.
    
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- lib/ansible/module_utils/network/iosxr/config/lacp_interfaces/lacp_interfaces.py
+++ lib/ansible/module_utils/network/iosxr/config/lacp_interfaces/lacp_interfaces.py
@@ -20,7 +20,7 @@ from ansible.module_utils.network.common.utils import to_list
 from ansible.module_utils.network.iosxr.facts.facts import Facts
 from ansible.module_utils.network.common.utils import dict_diff, remove_empties
 from ansible.module_utils.six import iteritems
-from ansible.module_utils.network.common.utils import to_list, search_obj_in_list, dict_diff, remove_empties
+from ansible.module_utils.network.common.utils import search_obj_in_list
 from ansible.module_utils.network.iosxr.utils.utils import dict_delete, pad_commands, flatten_dict
 
 
