4eafbd5d2699e4c7d2e9846eaad961b92f4d40da
Konstantinos Stamou <github@f3rr3t.net>
2019-08-25 21:18:48 +0300

    Returns false in case of an invalid datetime value in the datetime filter

--- lib/ansible/plugins/filter/core.py
+++ lib/ansible/plugins/filter/core.py
@@ -100,8 +100,10 @@ def to_bool(a):
 
 
 def to_datetime(string, format="%Y-%m-%d %H:%M:%S"):
-    return datetime.datetime.strptime(string, format)
-
+    try:
+      return datetime.datetime.strptime(string, format)
+    except ValueError:
+      return False
 
 def strftime(string_format, second=None):
     ''' return a date string using string. See https://docs.python.org/2/library/time.html#time.strftime for format '''
