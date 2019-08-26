11fa6c51354af8c20e51821f675e9f75afe0981c
orso <orso@checkpoint.com>
2019-08-26 14:32:15 +0300

    add '-'

--- lib/ansible/modules/network/checkpoint/cp_mgmt_group.py
+++ lib/ansible/modules/network/checkpoint/cp_mgmt_group.py
@@ -31,7 +31,7 @@ module: cp_mgmt_group
 short_description: Manages group objects on Checkpoint over Web Services API
 description:
   - Manages group objects on Checkpoint devices including creating, updating and removing objects.
-    All operations are performed over Web Services API.
+  - All operations are performed over Web Services API.
 version_added: "2.9"
 author: "Or Soffer (@chkp-orso)"
 options:
