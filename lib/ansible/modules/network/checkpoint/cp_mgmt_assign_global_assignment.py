23fbffd45ac4d1e732d7ec8e61fdcb8c50fca312
orso <orso@checkpoint.com>
2019-08-26 15:26:02 +0300

    add '-'

--- lib/ansible/modules/network/checkpoint/cp_mgmt_assign_global_assignment.py
+++ lib/ansible/modules/network/checkpoint/cp_mgmt_assign_global_assignment.py
@@ -31,7 +31,7 @@ module: cp_mgmt_assign_global_assignment
 short_description: assign global assignment on Checkpoint over Web Services API
 description:
   - assign global assignment on Checkpoint over Web Services API
-    All operations are performed over Web Services API.
+  - All operations are performed over Web Services API.
 version_added: "2.9"
 author: "Or Soffer (@chkp-orso)"
 options:
