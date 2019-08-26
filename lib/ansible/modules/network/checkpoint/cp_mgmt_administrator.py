e816ff5afbdfa5d77094c5e6a71435723cf06403
orso <orso@checkpoint.com>
2019-08-26 15:47:00 +0300

    add '-'

--- lib/ansible/modules/network/checkpoint/cp_mgmt_administrator.py
+++ lib/ansible/modules/network/checkpoint/cp_mgmt_administrator.py
@@ -31,7 +31,7 @@ module: cp_mgmt_administrator
 short_description: Manages administrator objects on Checkpoint over Web Services API
 description:
   - Manages administrator objects on Checkpoint devices including creating, updating and removing objects.
-    All operations are performed over Web Services API.
+  - All operations are performed over Web Services API.
 version_added: "2.9"
 author: "Or Soffer (@chkp-orso)"
 options:
