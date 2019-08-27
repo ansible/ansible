3ece2ca630b922fc3e8da76ddfd9295af0573d51
Matt Clay <matt@mystile.com>
2019-08-27 08:03:11 -0700

    Ignore relative import errors in test.

--- test/integration/targets/ansiballz_python/library/custom_module.py
+++ test/integration/targets/ansiballz_python/library/custom_module.py
@@ -3,8 +3,8 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-from ..module_utils.basic import AnsibleModule
-from ..module_utils.custom_util import forty_two
+from ..module_utils.basic import AnsibleModule  # pylint: disable=relative-beyond-top-level
+from ..module_utils.custom_util import forty_two  # pylint: disable=relative-beyond-top-level
 
 
 def main():
