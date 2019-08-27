3b201e99d0a0886899cee73546ab56a4a1e23da6
Chris Archibald <carchi8py@gmail.com>
2019-08-27 11:03:40 -0700

    updates

--- test/units/modules/cloud/amazon/test_aws_netapp_cvs_pool.py
+++ test/units/modules/cloud/amazon/test_aws_netapp_cvs_pool.py
@@ -3,7 +3,8 @@
 
 ''' Unit tests for AWS Cloud Volumes Services - Manage Pools '''
 
-from __future__ import print_function
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 import json
 import pytest
 
