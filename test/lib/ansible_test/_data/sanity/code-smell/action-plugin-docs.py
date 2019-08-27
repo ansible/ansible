c9b4223ebd0bf9dfb4f7862adb900039774c5dc4
Matt Clay <matt@mystile.com>
2019-08-26 20:40:53 -0700

    Update sanity tests for collections. (#61352)
    * Remove pointless sanity tests on `bin` dir.
    
    * Update action-plugin-docs test for collections.
    
    * Update no-main-display test for collections.
    
    * Update empty-init test for collections.
    
    * Update no-assert test for collections.
    
    * Move required-and-default-attributes test.
    
    This test only applies to Ansible itself.
    
    * Update use-argspec-type-path test for collections.

--- test/lib/ansible_test/_data/sanity/code-smell/action-plugin-docs.py
+++ test/lib/ansible_test/_data/sanity/code-smell/action-plugin-docs.py
@@ -13,26 +13,55 @@ def main():
 
     module_names = set()
 
+    module_prefixes = {
+        'lib/ansible/modules/': True,
+        'plugins/modules/': False,
+    }
+
+    action_prefixes = {
+        'lib/ansible/plugins/action/': True,
+        'plugins/action/': False,
+    }
+
     for path in paths:
-        if not path.startswith('lib/ansible/modules/'):
-            continue
+        full_name = get_full_name(path, module_prefixes)
 
-        name = os.path.splitext(os.path.basename(path))[0]
+        if full_name:
+            module_names.add(full_name)
 
-        if name != '__init__':
+    for path in paths:
+        full_name = get_full_name(path, action_prefixes)
+
+        if full_name and full_name not in module_names:
+            print('%s: action plugin has no matching module to provide documentation' % path)
+
+
+def get_full_name(path, prefixes):
+    """Return the full name of the plugin at the given path by matching against the given path prefixes, or None if no match is found."""
+    for prefix, flat in prefixes.items():
+        if path.startswith(prefix):
+            relative_path = os.path.relpath(path, prefix)
+
+            if flat:
+                full_name = os.path.basename(relative_path)
+            else:
+                full_name = relative_path
+
+            full_name = os.path.splitext(full_name)[0]
+
+            name = os.path.basename(full_name)
+
+            if name == '__init__':
+                return None
+
             if name.startswith('_'):
                 name = name[1:]
 
-            module_names.add(name)
+            full_name = os.path.join(os.path.dirname(full_name), name).replace(os.path.sep, '.')
 
-    for path in paths:
-        if not path.startswith('lib/ansible/plugins/action/'):
-            continue
+            return full_name
 
-        name = os.path.splitext(os.path.basename(path))[0]
-
-        if name not in module_names:
-            print('%s: action plugin has no matching module to provide documentation' % path)
+    return None
 
 
 if __name__ == '__main__':
