906f11135656cec52ce2ccf314a40b661041cfff
Miguel A. Munoz <magonzalez@fortinet.com>
2019-08-27 14:03:04 +0200

    Fortios User Device was wrongly set to 2.8 instead of 2.9

--- lib/ansible/modules/network/fortios/fortios_user_device.py
+++ lib/ansible/modules/network/fortios/fortios_user_device.py
@@ -30,7 +30,7 @@ description:
       user to set and modify user feature and device category.
       Examples include all parameters and values need to be adjusted to datasources before usage.
       Tested with FOS v6.0.5
-version_added: "2.8"
+version_added: "2.9"
 author:
     - Miguel Angel Munoz (@mamunozgonzalez)
     - Nicolas Thomas (@thomnico)
@@ -72,7 +72,6 @@ options:
             - Ensures FortiGate certificate must be verified by a proper CA.
         type: bool
         default: true
-        version_added: 2.9
     state:
         description:
             - Indicates whether to create or remove the object.
@@ -81,7 +80,6 @@ options:
         choices:
             - present
             - absent
-        version_added: 2.9
     user_device:
         description:
             - Configure devices.
