2db4e044ab0ba9961f06af1524384aa130006555
Felix Fontein <felix@fontein.de>
2019-08-27 22:43:10 +0200

    smartos_image_facts: rename to smartos_image_info (#60915)
    * Rename smartos_image_facts -> smartos_image_info
    
    * Add changelog.

--- lib/ansible/modules/cloud/smartos/smartos_image_info.py
+++ lib/ansible/modules/cloud/smartos/smartos_image_info.py
@@ -15,11 +15,12 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 
 DOCUMENTATION = '''
 ---
-module: smartos_image_facts
+module: smartos_image_info
 short_description: Get SmartOS image details.
 description:
-    - Retrieve facts about all installed images on SmartOS. Facts will be
-      inserted to the ansible_facts key.
+    - Retrieve information about all installed images on SmartOS.
+    - This module was called C(smartos_image_facts) before Ansible 2.9, returning C(ansible_facts).
+      Note that the M(smartos_image_info) module no longer returns C(ansible_facts)!
 version_added: "2.2"
 author: Adam Števko (@xen0l)
 options:
@@ -32,22 +33,31 @@ options:
 '''
 
 EXAMPLES = '''
-# Return facts about all installed images.
-- smartos_image_facts:
+# Return information about all installed images.
+- smartos_image_info:
+  register: result
 
 # Return all private active Linux images.
-- smartos_image_facts: filters="os=linux state=active public=false"
+- smartos_image_info: filters="os=linux state=active public=false"
+  register: result
 
 # Show, how many clones does every image have.
-- smartos_image_facts:
+- smartos_image_info:
+  register: result
 
-- debug: msg="{{ smartos_images[item]['name'] }}-{{smartos_images[item]['version'] }}
+- debug: msg="{{ result.smartos_images[item]['name'] }}-{{ result.smartos_images[item]['version'] }}
+            has {{ result.smartos_images[item]['clones'] }} VM(s)"
+  with_items: "{{ result.smartos_images.keys() | list }}"
+
+# When the module is called as smartos_image_facts, return values are published
+# in ansible_facts['smartos_images'] and can be used as follows.
+# Note that this is deprecated and will stop working in Ansible 2.13.
+- debug: msg="{{ smartos_images[item]['name'] }}-{{ smartos_images[item]['version'] }}
             has {{ smartos_images[item]['clones'] }} VM(s)"
-  with_items: "{{ smartos_images.keys() }}"
+  with_items: "{{ smartos_images.keys() | list }}"
 '''
 
 RETURN = '''
-# this module returns ansible_facts
 '''
 
 import json
@@ -95,12 +105,19 @@ def main():
         ),
         supports_check_mode=False,
     )
+    is_old_facts = module._name == 'smartos_image_facts'
+    if is_old_facts:
+        module.deprecate("The 'smartos_image_facts' module has been renamed to 'smartos_image_info', "
+                         "and the renamed one no longer returns ansible_facts", version='2.13')
 
     image_facts = ImageFacts(module)
 
     data = dict(smartos_images=image_facts.return_all_installed_images())
 
-    module.exit_json(ansible_facts=data)
+    if is_old_facts:
+        module.exit_json(ansible_facts=data)
+    else:
+        module.exit_json(**data)
 
 
 if __name__ == '__main__':
