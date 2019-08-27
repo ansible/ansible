bc062e8a8f607cf634016f93227a48fa11bf9268
Chris Trufan <chris.trufan@entrustdatacard.com>
2019-08-27 14:27:33 -0400

    Fixes to pep8 check to make regexp string 'raw'.

--- lib/ansible/modules/crypto/entrust/ecs_certificate.py
+++ lib/ansible/modules/crypto/entrust/ecs_certificate.py
@@ -529,9 +529,9 @@ MINIMAL_CRYPTOGRAPHY_VERSION = '1.6'
 
 
 def validate_cert_expiry(cert_expiry):
-    search_string_partial = re.compile('^([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])\Z')
-    search_string_full = re.compile('^([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):'
-                                    '([0-5][0-9]|60)(.[0-9]+)?(([Zz])|([+|-]([01][0-9]|2[0-3]):[0-5][0-9]))\Z')
+    search_string_partial = re.compile(r'^([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])\Z')
+    search_string_full = re.compile(r'^([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):'
+                                    r'([0-5][0-9]|60)(.[0-9]+)?(([Zz])|([+|-]([01][0-9]|2[0-3]):[0-5][0-9]))\Z')
     if search_string_partial.match(cert_expiry) or search_string_full.match(cert_expiry):
         return True
     return False
