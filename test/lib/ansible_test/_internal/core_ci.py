29c56563f5ea923bf57e98b8ab563ae8b86667b0
Matt Clay <matt@mystile.com>
2019-08-27 21:20:34 -0700

    Overhaul key pair management.

--- test/lib/ansible_test/_internal/core_ci.py
+++ test/lib/ansible_test/_internal/core_ci.py
@@ -558,46 +558,80 @@ class SshKey:
         """
         :type args: EnvironmentConfig
         """
-        cache_dir = ResultType.TMP.path
+        key_pair = self.get_key_pair()
 
-        self.key = os.path.join(cache_dir, self.KEY_NAME)
-        self.pub = os.path.join(cache_dir, self.PUB_NAME)
+        if not key_pair:
+            key_pair = self.generate_key_pair(args)
 
-        key_dst = os.path.relpath(self.key, data_context().content.root)
-        pub_dst = os.path.relpath(self.pub, data_context().content.root)
+        key, pub = key_pair
+        key_dst, pub_dst = self.get_in_tree_key_pair_paths()
 
-        if not os.path.isfile(self.key) or not os.path.isfile(self.pub):
-            base_dir = os.path.expanduser('~/.ansible/test/')
+        def ssh_key_callback(files):  # type: (t.List[t.Tuple[str, str]]) -> None
+            """
+            Add the SSH keys to the payload file list.
+            They are either outside the source tree or in the cache dir which is ignored by default.
+            """
+            if data_context().content.collection:
+                working_path = data_context().content.collection.directory
+            else:
+                working_path = ''
 
-            key = os.path.join(base_dir, self.KEY_NAME)
-            pub = os.path.join(base_dir, self.PUB_NAME)
+            files.append((key, os.path.join(working_path, os.path.relpath(key_dst, data_context().content.root))))
+            files.append((pub, os.path.join(working_path, os.path.relpath(pub_dst, data_context().content.root))))
 
-            if not args.explain:
-                make_dirs(base_dir)
+        data_context().register_payload_callback(ssh_key_callback)
 
-            if not os.path.isfile(key) or not os.path.isfile(pub):
-                run_command(args, ['ssh-keygen', '-m', 'PEM', '-q', '-t', 'rsa', '-N', '', '-f', key])
-
-            self.key = key
-            self.pub = pub
-
-            def ssh_key_callback(files):  # type: (t.List[t.Tuple[str, str]]) -> None
-                """Add the SSH keys to the payload file list."""
-                if data_context().content.collection:
-                    working_path = data_context().content.collection.directory
-                else:
-                    working_path = ''
-
-                files.append((key, os.path.join(working_path, key_dst)))
-                files.append((pub, os.path.join(working_path, pub_dst)))
-
-            data_context().register_payload_callback(ssh_key_callback)
+        self.key, self.pub = key, pub
 
         if args.explain:
             self.pub_contents = None
         else:
             with open(self.pub, 'r') as pub_fd:
                 self.pub_contents = pub_fd.read().strip()
+
+    def get_in_tree_key_pair_paths(self):  # type: () -> t.Optional[t.Tuple[str, str]]
+        """Return the ansible-test SSH key pair paths from the content tree."""
+        temp_dir = ResultType.TMP.path
+
+        key = os.path.join(temp_dir, self.KEY_NAME)
+        pub = os.path.join(temp_dir, self.PUB_NAME)
+
+        return key, pub
+
+    def get_source_key_pair_paths(self):  # type: () -> t.Optional[t.Tuple[str, str]]
+        """Return the ansible-test SSH key pair paths for the current user."""
+        base_dir = os.path.expanduser('~/.ansible/test/')
+
+        key = os.path.join(base_dir, self.KEY_NAME)
+        pub = os.path.join(base_dir, self.PUB_NAME)
+
+        return key, pub
+
+    def get_key_pair(self):  # type: () -> t.Optional[t.Tuple[str, str]]
+        """Return the ansible-test SSH key pair paths if present, otherwise return None."""
+        key, pub = self.get_in_tree_key_pair_paths()
+
+        if os.path.isfile(key) and os.path.isfile(pub):
+            return key, pub
+
+        key, pub = self.get_source_key_pair_paths()
+
+        if os.path.isfile(key) and os.path.isfile(pub):
+            return key, pub
+
+        return None
+
+    def generate_key_pair(self, args):  # type: (EnvironmentConfig) -> t.Tuple[str, str]
+        """Generate an SSH key pair for use by all ansible-test invocations for the current user."""
+        key, pub = self.get_source_key_pair_paths()
+
+        if not args.explain:
+            make_dirs(os.path.dirname(key))
+
+        if not os.path.isfile(key) or not os.path.isfile(pub):
+            run_command(args, ['ssh-keygen', '-m', 'PEM', '-q', '-t', 'rsa', '-N', '', '-f', key])
+
+        return key, pub
 
 
 class InstanceConnection:
