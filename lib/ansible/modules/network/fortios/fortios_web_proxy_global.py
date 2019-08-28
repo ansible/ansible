da762018e874bd4abcac1d060d08ae2be4e31126
Miguel Angel Muñoz González <magonzalez@fortinet.com>
2019-08-28 09:55:00 +0200

    FortiOS modules for 2.9 - 11 (#61387)

--- lib/ansible/modules/network/fortios/fortios_web_proxy_global.py
+++ lib/ansible/modules/network/fortios/fortios_web_proxy_global.py
@@ -26,10 +26,10 @@ DOCUMENTATION = '''
 module: fortios_web_proxy_global
 short_description: Configure Web proxy global settings in Fortinet's FortiOS and FortiGate.
 description:
-    - This module is able to configure a FortiGate or FortiOS by allowing the
+    - This module is able to configure a FortiGate or FortiOS (FOS) device by allowing the
       user to set and modify web_proxy feature and global category.
       Examples include all parameters and values need to be adjusted to datasources before usage.
-      Tested with FOS v6.0.2
+      Tested with FOS v6.0.5
 version_added: "2.8"
 author:
     - Miguel Angel Munoz (@mamunozgonzalez)
@@ -41,113 +41,140 @@ requirements:
     - fortiosapi>=0.9.8
 options:
     host:
-       description:
-            - FortiOS or FortiGate ip address.
-       required: true
+        description:
+            - FortiOS or FortiGate IP address.
+        type: str
+        required: false
     username:
         description:
             - FortiOS or FortiGate username.
-        required: true
+        type: str
+        required: false
     password:
         description:
             - FortiOS or FortiGate password.
+        type: str
         default: ""
     vdom:
         description:
             - Virtual domain, among those defined previously. A vdom is a
               virtual instance of the FortiGate that can be configured and
               used as a different unit.
+        type: str
         default: root
     https:
         description:
-            - Indicates if the requests towards FortiGate must use HTTPS
-              protocol
+            - Indicates if the requests towards FortiGate must use HTTPS protocol.
         type: bool
         default: true
+    ssl_verify:
+        description:
+            - Ensures FortiGate certificate must be verified by a proper CA.
+        type: bool
+        default: true
+        version_added: 2.9
     web_proxy_global:
         description:
             - Configure Web proxy global settings.
         default: null
+        type: dict
         suboptions:
-            fast-policy-match:
+            fast_policy_match:
                 description:
                     - Enable/disable fast matching algorithm for explicit and transparent proxy policy.
+                type: str
                 choices:
                     - enable
                     - disable
-            forward-proxy-auth:
+            forward_proxy_auth:
                 description:
                     - Enable/disable forwarding proxy authentication headers.
+                type: str
                 choices:
                     - enable
                     - disable
-            forward-server-affinity-timeout:
+            forward_server_affinity_timeout:
                 description:
-                    - Period of time before the source IP's traffic is no longer assigned to the forwarding server (6 - 60 min, default = 30).
-            learn-client-ip:
+                    - Period of time before the source IP's traffic is no longer assigned to the forwarding server (6 - 60 min).
+                type: int
+            learn_client_ip:
                 description:
                     - Enable/disable learning the client's IP address from headers.
+                type: str
                 choices:
                     - enable
                     - disable
-            learn-client-ip-from-header:
+            learn_client_ip_from_header:
                 description:
                     - Learn client IP address from the specified headers.
+                type: str
                 choices:
                     - true-client-ip
                     - x-real-ip
                     - x-forwarded-for
-            learn-client-ip-srcaddr:
+            learn_client_ip_srcaddr:
                 description:
                     - Source address name (srcaddr or srcaddr6 must be set).
+                type: list
                 suboptions:
                     name:
                         description:
                             - Address name. Source firewall.address.name firewall.addrgrp.name.
                         required: true
-            learn-client-ip-srcaddr6:
+                        type: str
+            learn_client_ip_srcaddr6:
                 description:
                     - IPv6 Source address name (srcaddr or srcaddr6 must be set).
+                type: list
                 suboptions:
                     name:
                         description:
                             - Address name. Source firewall.address6.name firewall.addrgrp6.name.
                         required: true
-            max-message-length:
+                        type: str
+            max_message_length:
                 description:
-                    - Maximum length of HTTP message, not including body (16 - 256 Kbytes, default = 32).
-            max-request-length:
+                    - Maximum length of HTTP message, not including body (16 - 256 Kbytes).
+                type: int
+            max_request_length:
                 description:
-                    - Maximum length of HTTP request line (2 - 64 Kbytes, default = 4).
-            max-waf-body-cache-length:
+                    - Maximum length of HTTP request line (2 - 64 Kbytes).
+                type: int
+            max_waf_body_cache_length:
                 description:
-                    - Maximum length of HTTP messages processed by Web Application Firewall (WAF) (10 - 1024 Kbytes, default = 32).
-            proxy-fqdn:
+                    - Maximum length of HTTP messages processed by Web Application Firewall (WAF) (10 - 1024 Kbytes).
+                type: int
+            proxy_fqdn:
                 description:
-                    - Fully Qualified Domain Name (FQDN) that clients connect to (default = default.fqdn) to connect to the explicit web proxy.
-            strict-web-check:
+                    - Fully Qualified Domain Name (FQDN) that clients connect to  to connect to the explicit web proxy.
+                type: str
+            strict_web_check:
                 description:
                     - Enable/disable strict web checking to block web sites that send incorrect headers that don't conform to HTTP 1.1.
+                type: str
                 choices:
                     - enable
                     - disable
-            tunnel-non-http:
+            tunnel_non_http:
                 description:
                     - Enable/disable allowing non-HTTP traffic. Allowed non-HTTP traffic is tunneled.
+                type: str
                 choices:
                     - enable
                     - disable
-            unknown-http-version:
+            unknown_http_version:
                 description:
                     - "Action to take when an unknown version of HTTP is encountered: reject, allow (tunnel), or proceed with best-effort."
+                type: str
                 choices:
                     - reject
                     - tunnel
                     - best-effort
-            webproxy-profile:
+            webproxy_profile:
                 description:
                     - Name of the web proxy profile to apply when explicit proxy traffic is allowed by default and traffic is accepted that does not match an
                        explicit proxy policy. Source web-proxy.profile.name.
+                type: str
 '''
 
 EXAMPLES = '''
@@ -157,6 +184,7 @@ EXAMPLES = '''
    username: "admin"
    password: ""
    vdom: "root"
+   ssl_verify: "False"
   tasks:
   - name: Configure Web proxy global settings.
     fortios_web_proxy_global:
@@ -166,25 +194,25 @@ EXAMPLES = '''
       vdom:  "{{ vdom }}"
       https: "False"
       web_proxy_global:
-        fast-policy-match: "enable"
-        forward-proxy-auth: "enable"
-        forward-server-affinity-timeout: "5"
-        learn-client-ip: "enable"
-        learn-client-ip-from-header: "true-client-ip"
-        learn-client-ip-srcaddr:
+        fast_policy_match: "enable"
+        forward_proxy_auth: "enable"
+        forward_server_affinity_timeout: "5"
+        learn_client_ip: "enable"
+        learn_client_ip_from_header: "true-client-ip"
+        learn_client_ip_srcaddr:
          -
             name: "default_name_9 (source firewall.address.name firewall.addrgrp.name)"
-        learn-client-ip-srcaddr6:
+        learn_client_ip_srcaddr6:
          -
             name: "default_name_11 (source firewall.address6.name firewall.addrgrp6.name)"
-        max-message-length: "12"
-        max-request-length: "13"
-        max-waf-body-cache-length: "14"
-        proxy-fqdn: "<your_own_value>"
-        strict-web-check: "enable"
-        tunnel-non-http: "enable"
-        unknown-http-version: "reject"
-        webproxy-profile: "<your_own_value> (source web-proxy.profile.name)"
+        max_message_length: "12"
+        max_request_length: "13"
+        max_waf_body_cache_length: "14"
+        proxy_fqdn: "<your_own_value>"
+        strict_web_check: "enable"
+        tunnel_non_http: "enable"
+        unknown_http_version: "reject"
+        webproxy_profile: "<your_own_value> (source web-proxy.profile.name)"
 '''
 
 RETURN = '''
@@ -247,12 +275,16 @@ version:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.connection import Connection
+from ansible.module_utils.network.fortios.fortios import FortiOSHandler
+from ansible.module_utils.network.fortimanager.common import FAIL_SOCKET_MSG
 
 
 def login(data, fos):
     host = data['host']
     username = data['username']
     password = data['password']
+    ssl_verify = data['ssl_verify']
 
     fos.debug('on')
     if 'https' in data and not data['https']:
@@ -260,15 +292,15 @@ def login(data, fos):
     else:
         fos.https('on')
 
-    fos.login(host, username, password)
+    fos.login(host, username, password, verify=ssl_verify)
 
 
 def filter_web_proxy_global_data(json):
-    option_list = ['fast-policy-match', 'forward-proxy-auth', 'forward-server-affinity-timeout',
-                   'learn-client-ip', 'learn-client-ip-from-header', 'learn-client-ip-srcaddr',
-                   'learn-client-ip-srcaddr6', 'max-message-length', 'max-request-length',
-                   'max-waf-body-cache-length', 'proxy-fqdn', 'strict-web-check',
-                   'tunnel-non-http', 'unknown-http-version', 'webproxy-profile']
+    option_list = ['fast_policy_match', 'forward_proxy_auth', 'forward_server_affinity_timeout',
+                   'learn_client_ip', 'learn_client_ip_from_header', 'learn_client_ip_srcaddr',
+                   'learn_client_ip_srcaddr6', 'max_message_length', 'max_request_length',
+                   'max_waf_body_cache_length', 'proxy_fqdn', 'strict_web_check',
+                   'tunnel_non_http', 'unknown_http_version', 'webproxy_profile']
     dictionary = {}
 
     for attribute in option_list:
@@ -278,10 +310,23 @@ def filter_web_proxy_global_data(json):
     return dictionary
 
 
+def underscore_to_hyphen(data):
+    if isinstance(data, list):
+        for elem in data:
+            elem = underscore_to_hyphen(elem)
+    elif isinstance(data, dict):
+        new_data = {}
+        for k, v in data.items():
+            new_data[k.replace('_', '-')] = underscore_to_hyphen(v)
+        data = new_data
+
+    return data
+
+
 def web_proxy_global(data, fos):
     vdom = data['vdom']
     web_proxy_global_data = data['web_proxy_global']
-    filtered_data = filter_web_proxy_global_data(web_proxy_global_data)
+    filtered_data = underscore_to_hyphen(filter_web_proxy_global_data(web_proxy_global_data))
 
     return fos.set('web-proxy',
                    'global',
@@ -289,54 +334,60 @@ def web_proxy_global(data, fos):
                    vdom=vdom)
 
 
+def is_successful_status(status):
+    return status['status'] == "success" or \
+        status['http_method'] == "DELETE" and status['http_status'] == 404
+
+
 def fortios_web_proxy(data, fos):
-    login(data, fos)
 
     if data['web_proxy_global']:
         resp = web_proxy_global(data, fos)
 
-    fos.logout()
-    return not resp['status'] == "success", resp['status'] == "success", resp
+    return not is_successful_status(resp), \
+        resp['status'] == "success", \
+        resp
 
 
 def main():
     fields = {
-        "host": {"required": True, "type": "str"},
-        "username": {"required": True, "type": "str"},
-        "password": {"required": False, "type": "str", "no_log": True},
+        "host": {"required": False, "type": "str"},
+        "username": {"required": False, "type": "str"},
+        "password": {"required": False, "type": "str", "default": "", "no_log": True},
         "vdom": {"required": False, "type": "str", "default": "root"},
         "https": {"required": False, "type": "bool", "default": True},
+        "ssl_verify": {"required": False, "type": "bool", "default": True},
         "web_proxy_global": {
-            "required": False, "type": "dict",
+            "required": False, "type": "dict", "default": None,
             "options": {
-                "fast-policy-match": {"required": False, "type": "str",
+                "fast_policy_match": {"required": False, "type": "str",
                                       "choices": ["enable", "disable"]},
-                "forward-proxy-auth": {"required": False, "type": "str",
+                "forward_proxy_auth": {"required": False, "type": "str",
                                        "choices": ["enable", "disable"]},
-                "forward-server-affinity-timeout": {"required": False, "type": "int"},
-                "learn-client-ip": {"required": False, "type": "str",
+                "forward_server_affinity_timeout": {"required": False, "type": "int"},
+                "learn_client_ip": {"required": False, "type": "str",
                                     "choices": ["enable", "disable"]},
-                "learn-client-ip-from-header": {"required": False, "type": "str",
+                "learn_client_ip_from_header": {"required": False, "type": "str",
                                                 "choices": ["true-client-ip", "x-real-ip", "x-forwarded-for"]},
-                "learn-client-ip-srcaddr": {"required": False, "type": "list",
+                "learn_client_ip_srcaddr": {"required": False, "type": "list",
                                             "options": {
                                                 "name": {"required": True, "type": "str"}
                                             }},
-                "learn-client-ip-srcaddr6": {"required": False, "type": "list",
+                "learn_client_ip_srcaddr6": {"required": False, "type": "list",
                                              "options": {
                                                  "name": {"required": True, "type": "str"}
                                              }},
-                "max-message-length": {"required": False, "type": "int"},
-                "max-request-length": {"required": False, "type": "int"},
-                "max-waf-body-cache-length": {"required": False, "type": "int"},
-                "proxy-fqdn": {"required": False, "type": "str"},
-                "strict-web-check": {"required": False, "type": "str",
+                "max_message_length": {"required": False, "type": "int"},
+                "max_request_length": {"required": False, "type": "int"},
+                "max_waf_body_cache_length": {"required": False, "type": "int"},
+                "proxy_fqdn": {"required": False, "type": "str"},
+                "strict_web_check": {"required": False, "type": "str",
                                      "choices": ["enable", "disable"]},
-                "tunnel-non-http": {"required": False, "type": "str",
+                "tunnel_non_http": {"required": False, "type": "str",
                                     "choices": ["enable", "disable"]},
-                "unknown-http-version": {"required": False, "type": "str",
+                "unknown_http_version": {"required": False, "type": "str",
                                          "choices": ["reject", "tunnel", "best-effort"]},
-                "webproxy-profile": {"required": False, "type": "str"}
+                "webproxy_profile": {"required": False, "type": "str"}
 
             }
         }
@@ -344,14 +395,31 @@ def main():
 
     module = AnsibleModule(argument_spec=fields,
                            supports_check_mode=False)
-    try:
-        from fortiosapi import FortiOSAPI
-    except ImportError:
-        module.fail_json(msg="fortiosapi module is required")
 
-    fos = FortiOSAPI()
+    # legacy_mode refers to using fortiosapi instead of HTTPAPI
+    legacy_mode = 'host' in module.params and module.params['host'] is not None and \
+                  'username' in module.params and module.params['username'] is not None and \
+                  'password' in module.params and module.params['password'] is not None
 
-    is_error, has_changed, result = fortios_web_proxy(module.params, fos)
+    if not legacy_mode:
+        if module._socket_path:
+            connection = Connection(module._socket_path)
+            fos = FortiOSHandler(connection)
+
+            is_error, has_changed, result = fortios_web_proxy(module.params, fos)
+        else:
+            module.fail_json(**FAIL_SOCKET_MSG)
+    else:
+        try:
+            from fortiosapi import FortiOSAPI
+        except ImportError:
+            module.fail_json(msg="fortiosapi module is required")
+
+        fos = FortiOSAPI()
+
+        login(module.params, fos)
+        is_error, has_changed, result = fortios_web_proxy(module.params, fos)
+        fos.logout()
 
     if not is_error:
         module.exit_json(changed=has_changed, meta=result)
