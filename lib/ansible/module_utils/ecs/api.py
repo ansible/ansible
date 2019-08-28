6e9882981f7336602b5ac4f2278fdd5ef4210f04
Chris Trufan <31186388+ctrufan@users.noreply.github.com>
2019-08-28 17:38:48 -0400

    Addition of ECS_Certificate Module (#60883)
    * Addition of ecs_certificate module.
    
    * Documentation and code fixes
    
    * Updates per code review
    
    * Doc fixes, rename of chain_path to full_chain_path, add regex for cert_Expiry check
    
    * Fixes to pep8 check to make regexp string 'raw'.
    
    * Mistakes with find/replace of caseing.
    
    * Added integration tests and some doc cleanup
    
    * Some additional assertions and test typo cleanup
    
    * Update lib/ansible/modules/crypto/entrust/ecs_certificate.py
    
    Co-Authored-By: Felix Fontein <felix@fontein.de>
    
    * Responses to code review comments
    
    * Remove fake passwords from aliases file.

--- lib/ansible/module_utils/ecs/api.py
+++ lib/ansible/module_utils/ecs/api.py
@@ -55,6 +55,16 @@ else:
 valid_file_format = re.compile(r".*(\.)(yml|yaml|json)$")
 
 
+def ecs_client_argument_spec():
+    return dict(
+        entrust_api_user=dict(type='str', required=True),
+        entrust_api_key=dict(type='str', required=True, no_log=True),
+        entrust_api_client_cert_path=dict(type='path', required=True),
+        entrust_api_client_cert_key_path=dict(type='path', required=True, no_log=True),
+        entrust_api_specification_path=dict(type='path', default='https://cloud.entrust.net/EntrustCloud/documentation/cms-api-2.1.0.yaml'),
+    )
+
+
 class SessionConfigurationException(Exception):
     """ Raised if we cannot configure a session with the API """
 
