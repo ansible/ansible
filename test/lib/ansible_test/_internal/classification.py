6568cb7f584a8fdb7096cf1adf3ae8fce9debb76
Matt Clay <matt@mystile.com>
2019-08-28 11:45:56 -0700

    Fix variable shadowing.

--- test/lib/ansible_test/_internal/classification.py
+++ test/lib/ansible_test/_internal/classification.py
@@ -159,7 +159,7 @@ def categorize_changes(args, paths, verbose_command=None):
     for command in commands:
         commands[command].discard('none')
 
-        if any(t == 'all' for t in commands[command]):
+        if any(target == 'all' for target in commands[command]):
             commands[command] = set(['all'])
 
     commands = dict((c, sorted(commands[c])) for c in commands if commands[c])
@@ -194,23 +194,23 @@ class PathMapper:
         self.compile_targets = list(walk_compile_targets())
         self.units_targets = list(walk_units_targets())
         self.sanity_targets = list(walk_sanity_targets())
-        self.powershell_targets = [t for t in self.sanity_targets if os.path.splitext(t.path)[1] in ('.ps1', '.psm1')]
-        self.csharp_targets = [t for t in self.sanity_targets if os.path.splitext(t.path)[1] == '.cs']
+        self.powershell_targets = [target for target in self.sanity_targets if os.path.splitext(target.path)[1] in ('.ps1', '.psm1')]
+        self.csharp_targets = [target for target in self.sanity_targets if os.path.splitext(target.path)[1] == '.cs']
 
-        self.units_modules = set(t.module for t in self.units_targets if t.module)
-        self.units_paths = set(a for t in self.units_targets for a in t.aliases)
-        self.sanity_paths = set(t.path for t in self.sanity_targets)
+        self.units_modules = set(target.module for target in self.units_targets if target.module)
+        self.units_paths = set(a for target in self.units_targets for a in target.aliases)
+        self.sanity_paths = set(target.path for target in self.sanity_targets)
 
-        self.module_names_by_path = dict((t.path, t.module) for t in self.module_targets)
-        self.integration_targets_by_name = dict((t.name, t) for t in self.integration_targets)
-        self.integration_targets_by_alias = dict((a, t) for t in self.integration_targets for a in t.aliases)
+        self.module_names_by_path = dict((target.path, target.module) for target in self.module_targets)
+        self.integration_targets_by_name = dict((target.name, target) for target in self.integration_targets)
+        self.integration_targets_by_alias = dict((a, target) for target in self.integration_targets for a in target.aliases)
 
-        self.posix_integration_by_module = dict((m, t.name) for t in self.integration_targets
-                                                if 'posix/' in t.aliases for m in t.modules)
-        self.windows_integration_by_module = dict((m, t.name) for t in self.integration_targets
-                                                  if 'windows/' in t.aliases for m in t.modules)
-        self.network_integration_by_module = dict((m, t.name) for t in self.integration_targets
-                                                  if 'network/' in t.aliases for m in t.modules)
+        self.posix_integration_by_module = dict((m, target.name) for target in self.integration_targets
+                                                if 'posix/' in target.aliases for m in target.modules)
+        self.windows_integration_by_module = dict((m, target.name) for target in self.integration_targets
+                                                  if 'windows/' in target.aliases for m in target.modules)
+        self.network_integration_by_module = dict((m, target.name) for target in self.integration_targets
+                                                  if 'network/' in target.aliases for m in target.modules)
 
         self.prefixes = load_integration_prefixes()
         self.integration_dependencies = analyze_integration_target_dependencies(self.integration_targets)
@@ -256,7 +256,7 @@ class PathMapper:
         :rtype: list[str]
         """
         paths = self.get_dependent_paths_internal(path)
-        paths += [t.path + '/' for t in self.paths_to_dependent_targets.get(path, set())]
+        paths += [target.path + '/' for target in self.paths_to_dependent_targets.get(path, set())]
         paths = sorted(set(paths))
 
         return paths
