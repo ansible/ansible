bc506f5fb466f6127ebd4e2fc1c23f7a90714363
orso <orso@checkpoint.com>
2019-08-28 20:16:36 +0300

    fix

--- lib/ansible/modules/network/checkpoint/cp_mgmt_threat_indicator.py
+++ lib/ansible/modules/network/checkpoint/cp_mgmt_threat_indicator.py
@@ -44,6 +44,84 @@ options:
     description:
       - The indicator's observables.
     type: list
+    suboptions:
+      name:
+        description:
+          - Object name. Should be unique in the domain.
+        type: str
+      md5:
+        description:
+          - A valid MD5 sequence.
+        type: str
+      url:
+        description:
+          - A valid URL.
+        type: str
+      ip_address:
+        description:
+          - A valid IP-Address.
+        type: str
+      ip_address_first:
+        description:
+          - A valid IP-Address, the beginning of the range. If you configure this parameter with a value, you must also configure the value of the
+            'ip-address-last' parameter.
+        type: str
+      ip_address_last:
+        description:
+          - A valid IP-Address, the end of the range. If you configure this parameter with a value, you must also configure the value of the
+            'ip-address-first' parameter.
+        type: str
+      domain:
+        description:
+          - The name of a domain.
+        type: str
+      mail_to:
+        description:
+          - A valid E-Mail address, recipient filed.
+        type: str
+      mail_from:
+        description:
+          - A valid E-Mail address, sender field.
+        type: str
+      mail_cc:
+        description:
+          - A valid E-Mail address, cc field.
+        type: str
+      mail_reply_to:
+        description:
+          - A valid E-Mail address, reply-to field.
+        type: str
+      mail_subject:
+        description:
+          - Subject of E-Mail.
+        type: str
+      confidence:
+        description:
+          - The confidence level the indicator has that a real threat has been uncovered.
+        type: str
+        choices: ['low', 'medium', 'high', 'critical']
+      product:
+        description:
+          - The software blade that processes the observable, AV - AntiVirus, AB - AntiBot.
+        type: str
+        choices: ['AV', 'AB']
+      severity:
+        description:
+          - The severity level of the threat.
+        type: str
+        choices: ['low', 'medium', 'high', 'critical']
+      comments:
+        description:
+          - Comments string.
+        type: str
+      ignore_warnings:
+        description:
+          - Apply changes ignoring warnings.
+        type: bool
+      ignore_errors:
+        description:
+          - Apply changes ignoring errors. You won't be able to publish such a changes. If ignore-warnings flag was omitted - warnings will also be ignored.
+        type: bool
   observables_raw_data:
     description:
       - The contents of a file containing the indicator's observables.
@@ -57,6 +135,16 @@ options:
     description:
       - Profiles in which to override the indicator's default action.
     type: list
+    suboptions:
+      action:
+        description:
+          - The indicator's action in this profile.
+        type: str
+        choices: ['Inactive', 'Ask', 'Prevent', 'Detect']
+      profile:
+        description:
+          - The profile in which to override the indicator's action.
+        type: str
   tags:
     description:
       - Collection of tag identifiers.
@@ -111,8 +199,6 @@ EXAMPLES = """
     action: prevent
     ignore_warnings: true
     name: My_Indicator
-    profile_overrides:
-      remove: My_Profile
     state: present
 
 - name: delete-threat-indicator
@@ -135,10 +221,32 @@ from ansible.module_utils.network.checkpoint.checkpoint import checkpoint_argume
 def main():
     argument_spec = dict(
         name=dict(type='str', required=True),
-        observables=dict(type='list'),
+        observables=dict(type='list', options=dict(
+            name=dict(type='str'),
+            md5=dict(type='str'),
+            url=dict(type='str'),
+            ip_address=dict(type='str'),
+            ip_address_first=dict(type='str'),
+            ip_address_last=dict(type='str'),
+            domain=dict(type='str'),
+            mail_to=dict(type='str'),
+            mail_from=dict(type='str'),
+            mail_cc=dict(type='str'),
+            mail_reply_to=dict(type='str'),
+            mail_subject=dict(type='str'),
+            confidence=dict(type='str', choices=['low', 'medium', 'high', 'critical']),
+            product=dict(type='str', choices=['AV', 'AB']),
+            severity=dict(type='str', choices=['low', 'medium', 'high', 'critical']),
+            comments=dict(type='str'),
+            ignore_warnings=dict(type='bool'),
+            ignore_errors=dict(type='bool')
+        )),
         observables_raw_data=dict(type='str'),
         action=dict(type='str', choices=['Inactive', 'Ask', 'Prevent', 'Detect']),
-        profile_overrides=dict(type='list'),
+        profile_overrides=dict(type='list', options=dict(
+            action=dict(type='str', choices=['Inactive', 'Ask', 'Prevent', 'Detect']),
+            profile=dict(type='str')
+        )),
         tags=dict(type='list'),
         color=dict(type='str', choices=['aquamarine', 'black', 'blue', 'crete blue', 'burlywood', 'cyan', 'dark green',
                                         'khaki', 'orchid', 'dark orange', 'dark sea green', 'pink', 'turquoise', 'dark blue', 'firebrick', 'brown',
