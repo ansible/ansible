e52740a1a7c15495538ab98aeeac19868f5b639d
Miguel A. Munoz <magonzalez@fortinet.com>
2019-08-28 17:37:10 +0200

    Minor bug fixes - 1

--- lib/ansible/modules/network/fortios/fortios_firewall_vipgrp46.py
+++ lib/ansible/modules/network/fortios/fortios_firewall_vipgrp46.py
@@ -76,8 +76,10 @@ options:
     state:
         description:
             - Indicates whether to create or remove the object.
+              This attribute was present already in previous version in a deeper level.
+              It has been moved out to this outer level.
         type: str
-        required: true
+        required: false
         choices:
             - present
             - absent
@@ -88,6 +90,17 @@ options:
         default: null
         type: dict
         suboptions:
+            state:
+                description:
+                    - B(Deprecated)
+                    - Starting with Ansible 2.9 we recommend using the top-level 'state' parameter.
+                    - HORIZONTALLINE
+                    - Indicates whether to create or remove the object.
+                type: str
+                required: false
+                choices:
+                    - present
+                    - absent
             color:
                 description:
                     - Integer value to determine the color of the icon in the GUI (range 1 to 32).
@@ -251,7 +264,12 @@ def underscore_to_hyphen(data):
 
 def firewall_vipgrp46(data, fos):
     vdom = data['vdom']
-    state = data['state']
+    if 'state' in data and data['state']:
+        state = data['state']
+    elif 'state' in data['firewall_vipgrp46'] and data['firewall_vipgrp46']:
+        state = data['firewall_vipgrp46']['state']
+    else:
+        state = True
     firewall_vipgrp46_data = data['firewall_vipgrp46']
     filtered_data = underscore_to_hyphen(filter_firewall_vipgrp46_data(firewall_vipgrp46_data))
 
@@ -291,11 +309,13 @@ def main():
         "vdom": {"required": False, "type": "str", "default": "root"},
         "https": {"required": False, "type": "bool", "default": True},
         "ssl_verify": {"required": False, "type": "bool", "default": True},
-        "state": {"required": True, "type": "str",
+        "state": {"required": False, "type": "str",
                   "choices": ["present", "absent"]},
         "firewall_vipgrp46": {
             "required": False, "type": "dict", "default": None,
             "options": {
+                "state": {"required": False, "type": "str",
+                          "choices": ["present", "absent"]},
                 "color": {"required": False, "type": "int"},
                 "comments": {"required": False, "type": "str"},
                 "member": {"required": False, "type": "list",
