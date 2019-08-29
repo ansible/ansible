464caef7fd91e038fe4c797bdda81a8b550eae35
Chris Van Heuveln <cvanheuv@cisco.com>
2019-08-29 11:30:34 -0400

    lint 2

--- lib/ansible/modules/network/nxos/nxos_hsrp_interfaces.py
+++ lib/ansible/modules/network/nxos/nxos_hsrp_interfaces.py
@@ -137,7 +137,7 @@ commands:
   description: The set of commands pushed to the remote device.
   returned: always
   type: list
-  sample: ['command 1', 'command 2', 'command 3']
+  sample: ['interface Ethernet1/1', 'hsrp bfd']
 """
 
 
