544ca0522cf3adc7ec58462e989ad94ce6b1293f
Felix Fontein <felix@fontein.de>
2019-08-29 12:50:31 +0200

    Make cs_zone_info return a list.

--- lib/ansible/modules/cloud/cloudstack/cs_zone_info.py
+++ lib/ansible/modules/cloud/cloudstack/cs_zone_info.py
@@ -21,8 +21,9 @@ options:
   name:
     description:
       - Name of the zone.
+      - If not specified, all zones are returned
     type: str
-    required: true
+    required: false
     aliases: [ zone ]
 extends_documentation_fragment: cloudstack
 '''
@@ -41,96 +42,100 @@ EXAMPLES = '''
 
 RETURN = '''
 ---
-id:
-  description: UUID of the zone.
-  returned: success
-  type: str
-  sample: 04589590-ac63-4ffc-93f5-b698b8ac38b6
-name:
-  description: Name of the zone.
-  returned: success
-  type: str
-  sample: zone01
-dns1:
-  description: First DNS for the zone.
-  returned: success
-  type: str
-  sample: 8.8.8.8
-dns2:
-  description: Second DNS for the zone.
-  returned: success
-  type: str
-  sample: 8.8.4.4
-internal_dns1:
-  description: First internal DNS for the zone.
-  returned: success
-  type: str
-  sample: 8.8.8.8
-internal_dns2:
-  description: Second internal DNS for the zone.
-  returned: success
-  type: str
-  sample: 8.8.4.4
-dns1_ipv6:
-  description: First IPv6 DNS for the zone.
-  returned: success
-  type: str
-  sample: "2001:4860:4860::8888"
-dns2_ipv6:
-  description: Second IPv6 DNS for the zone.
-  returned: success
-  type: str
-  sample: "2001:4860:4860::8844"
-allocation_state:
-  description: State of the zone.
-  returned: success
-  type: str
-  sample: Enabled
-domain:
-  description: Domain the zone is related to.
-  returned: success
-  type: str
-  sample: ROOT
-network_domain:
-  description: Network domain for the zone.
-  returned: success
-  type: str
-  sample: example.com
-network_type:
-  description: Network type for the zone.
-  returned: success
-  type: str
-  sample: basic
-local_storage_enabled:
-  description: Local storage offering enabled.
-  returned: success
-  type: bool
-  sample: false
-securitygroups_enabled:
-  description: Security groups support is enabled.
-  returned: success
-  type: bool
-  sample: false
-guest_cidr_address:
-  description: Guest CIDR address for the zone
-  returned: success
-  type: str
-  sample: 10.1.1.0/24
-dhcp_provider:
-  description: DHCP provider for the zone
-  returned: success
-  type: str
-  sample: VirtualRouter
-zone_token:
-  description: Zone token
-  returned: success
-  type: str
-  sample: ccb0a60c-79c8-3230-ab8b-8bdbe8c45bb7
-tags:
-  description: List of resource tags associated with the zone.
-  returned: success
-  type: dict
-  sample: [ { "key": "foo", "value": "bar" } ]
+zones:
+  description: A list of matching zones.
+  type: list
+  suboptions:
+    id:
+      description: UUID of the zone.
+      returned: success
+      type: str
+      sample: 04589590-ac63-4ffc-93f5-b698b8ac38b6
+    name:
+      description: Name of the zone.
+      returned: success
+      type: str
+      sample: zone01
+    dns1:
+      description: First DNS for the zone.
+      returned: success
+      type: str
+      sample: 8.8.8.8
+    dns2:
+      description: Second DNS for the zone.
+      returned: success
+      type: str
+      sample: 8.8.4.4
+    internal_dns1:
+      description: First internal DNS for the zone.
+      returned: success
+      type: str
+      sample: 8.8.8.8
+    internal_dns2:
+      description: Second internal DNS for the zone.
+      returned: success
+      type: str
+      sample: 8.8.4.4
+    dns1_ipv6:
+      description: First IPv6 DNS for the zone.
+      returned: success
+      type: str
+      sample: "2001:4860:4860::8888"
+    dns2_ipv6:
+      description: Second IPv6 DNS for the zone.
+      returned: success
+      type: str
+      sample: "2001:4860:4860::8844"
+    allocation_state:
+      description: State of the zone.
+      returned: success
+      type: str
+      sample: Enabled
+    domain:
+      description: Domain the zone is related to.
+      returned: success
+      type: str
+      sample: ROOT
+    network_domain:
+      description: Network domain for the zone.
+      returned: success
+      type: str
+      sample: example.com
+    network_type:
+      description: Network type for the zone.
+      returned: success
+      type: str
+      sample: basic
+    local_storage_enabled:
+      description: Local storage offering enabled.
+      returned: success
+      type: bool
+      sample: false
+    securitygroups_enabled:
+      description: Security groups support is enabled.
+      returned: success
+      type: bool
+      sample: false
+    guest_cidr_address:
+      description: Guest CIDR address for the zone
+      returned: success
+      type: str
+      sample: 10.1.1.0/24
+    dhcp_provider:
+      description: DHCP provider for the zone
+      returned: success
+      type: str
+      sample: VirtualRouter
+    zone_token:
+      description: Zone token
+      returned: success
+      type: str
+      sample: ccb0a60c-79c8-3230-ab8b-8bdbe8c45bb7
+    tags:
+      description: List of resource tags associated with the zone.
+      returned: success
+      type: dict
+      sample: [ { "key": "foo", "value": "bar" } ]
 '''
 
 from ansible.module_utils.basic import AnsibleModule
@@ -162,13 +167,19 @@ class AnsibleCloudStackZoneInfo(AnsibleCloudStack):
         }
 
     def get_zone(self):
-        return super(AnsibleCloudStackZoneInfo, self).get_zone()
+        if self.module.params['zone']:
+            zones = [super(AnsibleCloudStackZoneInfo, self).get_zone()]
+        else:
+            zones = self.query_api('listZones')
+        return {
+            'zones': [self.update_result(resource) for resource in zones]
+        }
 
 
 def main():
     argument_spec = cs_argument_spec()
     argument_spec.update(dict(
-        zone=dict(required=True, aliases=['name']),
+        zone=dict(required=False, aliases=['name']),
     ))
 
     module = AnsibleModule(
@@ -177,7 +188,7 @@ def main():
     )
 
     acs_zone_info = AnsibleCloudStackZoneInfo(module=module)
-    result = acs_zone_info.get_result(acs_zone_info.get_zone())
+    result = acs_zone_info.get_zone()
     module.exit_json(**result)
 
 
