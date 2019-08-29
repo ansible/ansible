d4a766f583bc80b66482ba2967a0fa7f38d65006
orso <orso@checkpoint.com>
2019-08-29 15:05:34 +0300

    fix suboptions of list to replacce _ with -

--- lib/ansible/module_utils/network/checkpoint/checkpoint.py
+++ lib/ansible/module_utils/network/checkpoint/checkpoint.py
@@ -74,6 +74,11 @@ def get_payload_from_parameters(params):
         if parameter_value and is_checkpoint_param(parameter):
             if isinstance(parameter_value, dict):
                 payload[parameter.replace("_", "-")] = get_payload_from_parameters(parameter_value)
+            elif isinstance(parameter_value, list) and len(parameter_value) != 0 and isinstance(parameter_value[0], dict):
+                payload_list = []
+                for element_dict in parameter_value:
+                    payload_list.append(get_payload_from_parameters(element_dict))
+                payload[parameter.replace("_", "-")] = payload_list
             else:
                 payload[parameter.replace("_", "-")] = parameter_value
     return payload
