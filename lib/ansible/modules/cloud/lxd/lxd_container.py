d6c7ca55e6054529086fd6f5c04bea6db8915629
satunnainen <satunnainen@users.noreply.github.com>
2019-08-30 18:05:26 +0300

    enables to set keys not present in existing config
    ((cherry picked from commit 95525030e2d2bcf2bb1a145700ecdf80f6a7567b)

--- lib/ansible/modules/cloud/lxd/lxd_container.py
+++ lib/ansible/modules/cloud/lxd/lxd_container.py
@@ -502,6 +502,8 @@ class LXDContainerManagement(object):
         if key == 'config':
             old_configs = dict((k, v) for k, v in self.old_container_json['metadata'][key].items() if not k.startswith('volatile.'))
             for k, v in self.config['config'].items():
+                if k not in old_configs:
+                    return True
                 if old_configs[k] != v:
                     return True
             return False
