1984d59b8042a32677c9bfe57ce1376c3382a5c8
Matt Clay <matt@mystile.com>
2019-08-29 20:56:04 -0700

    Split CI unit tests into two groups.
    ci_coverage

--- test/utils/shippable/units.sh
+++ test/utils/shippable/units.sh
@@ -6,14 +6,21 @@ declare -a args
 IFS='/:' read -ra args <<< "$1"
 
 version="${args[1]}"
+group="${args[2]}"
 
 if [[ "${COVERAGE:-}" == "--coverage" ]]; then
-    timeout=99
+    timeout=60
 else
-    timeout=11
+    timeout=6
 fi
+
+case "${group}" in
+    1) options=(--exclude test/units/modules/network/) ;;
+    2) options=(          test/units/modules/network/) ;;
+esac
 
 ansible-test env --timeout "${timeout}" --color -v
 
 # shellcheck disable=SC2086
 ansible-test units --color -v --docker default --python "${version}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
+    "${options[@]}" \
