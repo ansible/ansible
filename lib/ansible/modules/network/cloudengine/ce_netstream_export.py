60e14c59257d75c62b0ad7427a1f71b8dd2cd7c0
zhangyan <1072973303@qq.com>
2019-09-01 11:34:17 +0800

    Update ce_netstream_export to fix bugs

--- lib/ansible/modules/network/cloudengine/ce_netstream_export.py
+++ lib/ansible/modules/network/cloudengine/ce_netstream_export.py
@@ -263,7 +263,7 @@ class NetstreamExport(object):
 
         cmd = "display current-configuration | include ^netstream export"
         rc, out, err = exec_command(self.module, cmd)
-        if rc != 0：
+        if rc != 0:
             self.module.fail_json(msg=err)
         config = str(out).strip()
         return config
