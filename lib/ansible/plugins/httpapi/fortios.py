0926020e2e28838f1ede7105b13ac517aa2905ab
Miguel Angel Muñoz González <magonzalez@fortinet.com>
2019-09-03 17:36:53 +0200

    Update lib/ansible/plugins/httpapi/fortios.py
    Co-Authored-By: Nathaniel Case <this.is@nathanielca.se>
    (cherry picked from commit 7601b7fa0defeade4cad2b8e0edd73afcde8b395)

--- lib/ansible/plugins/httpapi/fortios.py
+++ lib/ansible/plugins/httpapi/fortios.py
@@ -43,7 +43,7 @@ version_added: "2.9"
 
 from ansible.plugins.httpapi import HttpApiBase
 from ansible.module_utils.basic import to_text
-import urllib
+from ansible.module_utils.six.moves import urllib
 import json
 import re
 import sys
