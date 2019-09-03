ba1c2e4ea05a1541baab0549cd8643f617eaabde
Miguel Angel Muñoz González <magonzalez@fortinet.com>
2019-09-03 17:37:32 +0200

    Update lib/ansible/module_utils/network/fortios/fortios.py
    Co-Authored-By: Nathaniel Case <this.is@nathanielca.se>

--- lib/ansible/module_utils/network/fortios/fortios.py
+++ lib/ansible/module_utils/network/fortios/fortios.py
@@ -33,7 +33,6 @@ import traceback
 
 from ansible.module_utils._text import to_text
 from ansible.module_utils.basic import env_fallback
-from ansible.module_utils.basic import to_text
 
 import json
 
