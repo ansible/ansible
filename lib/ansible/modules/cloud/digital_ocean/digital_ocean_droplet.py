01f4081b663ae260f88283aa66ae6fb03bc40ff4
Mark Chesney <mches@users.noreply.github.com>
2019-09-03 23:28:56 -0700

    Fix creation of DigitalOcean droplets using digital_ocean_droplet module (#61655)
    * Omit disallowed 'id' attribute in request body to create a droplet
    
    * Add changelog fragment for PR #61655

--- lib/ansible/modules/cloud/digital_ocean/digital_ocean_droplet.py
+++ lib/ansible/modules/cloud/digital_ocean/digital_ocean_droplet.py
@@ -252,7 +252,9 @@ class DODroplet(object):
             self.module.exit_json(changed=False, data=droplet_data)
         if self.module.check_mode:
             self.module.exit_json(changed=True)
-        response = self.rest.post('droplets', data=self.module.params)
+        request_params = dict(self.module.params)
+        del request_params['id']
+        response = self.rest.post('droplets', data=request_params)
         json_data = response.json
         if response.status_code >= 400:
             self.module.fail_json(changed=False, msg=json_data['message'])
