ac90621496439b7e9f0d2d0c223a4e95a6ee19cf
Dakota Clark <dakota.clark@pdq.com>
2019-09-04 13:39:58 -0600

    Passing sanity checks, need to test

--- lib/ansible/modules/cloud/vmware/vmware_guest_instantclone.py
+++ lib/ansible/modules/cloud/vmware/vmware_guest_instantclone.py
@@ -27,6 +27,13 @@ requirements:
     - "python >= 2.6"
     - PyVmomi
 options:
+   state:
+    description:
+    - Whether a given clone should be present or absent from the vSphere instance.
+    default: present
+    choices: [ present, absent ]
+    required: True
+    type: str
    name:
     description:
     - Name of the new instant clone VM.
@@ -64,111 +71,118 @@ from ansible.module_utils.vmware import (find_obj, gather_vm_facts, vmware_argum
                                          set_vm_power_state, PyVmomi, wait_for_task, TaskError)
 
 
-def deploy_instantclone(module):
-    # Ensure required params are given
-    if module.params['source_vm'] is None:
-        module.fail_json(msg='In order to create an instant clone the source VM name or UUID of a running VM must be specified in the source_vm parameter.')
-    if module.params['name'] is None:
-        module.fail_json(msg='A name for the new VM must be provided in the name parameter.')
-    # Gather needed facts from the base VM for the instant clone
-    vm = find_obj(module.content, [vim.VirtualMachine], module.params['source_vm'])
-    if vm is None:
-        module.fail_json(msg="Failed to find the VM: %s" % module.params['source_vm'])
-    vm_state = gather_vm_facts(module.content, vm)
-    # An instant clone requires the base VM to be running, fail if it is not
-    if vm_state['hw_power_status'] != 'poweredOn':
-        module.module.fail_json(msg='Unable to instant clone a VM in a "%s" state. It must be powered on.' % vm_state['hw_power_status'])
-    if vm_state['instant_clone_frozen'] is None:
-        module.module.fail_json(msg='Unable to determine if VM is in a frozen state. Is vSphere running 6.7 or later?')
+class PyVmomiHelper(PyVmomi):
+    def __init__(self, module):
+        super(PyVmomiHelper, self).__init__(module)
 
-    # Build VM config dict
-    config = []
-    # If customvalues is not empty, fill config
-    if len(module.params['customvalues']) != 0:
-        for kv in module.params['customvalues']:
-            if 'key' not in kv or 'value' not in kv:
-                module.module.exit_json(msg="The parameter customvalues items required both 'key' and 'value' fields.")
-            ov = vim.OptionValue()
-            ov.key = kv['key']
-            ov.value = kv['value']
-            config.append(ov)
+    def deploy_instantclone(self):
+        # Ensure required params are given
+        if self.module.params['source_vm'] is None:
+            self.module.fail_json(msg='Please specify the source VM name or UUID in the source_vm parameter. The source VM must be running.')
+        if self.module.params['name'] is None:
+            self.module.fail_json(msg='A name for the new VM must be provided in the name parameter.')
+        # Gather needed facts from the base VM for the instant clone
+        vm = find_obj(self.module.content, [vim.VirtualMachine], self.module.params['source_vm'])
+        if vm is None:
+            self.module.fail_json(msg="Failed to find the VM: %s" % self.module.params['source_vm'])
+        vm_state = gather_vm_facts(self.module.content, vm)
+        # An instant clone requires the base VM to be running, fail if it is not
+        if vm_state['hw_power_status'] != 'poweredOn':
+            self.module.module.fail_json(msg='Unable to instant clone a VM in a "%s" state. It must be powered on.' % vm_state['hw_power_status'])
+        if vm_state['instant_clone_frozen'] is None:
+            self.module.module.fail_json(msg='Unable to determine if VM is in a frozen state. Is vSphere running 6.7 or later?')
 
-    instantclone_spec = vim.VirtualMachineInstantCloneSpec()
-    location_spec = vim.VirtualMachineRelocateSpec()
-    if vm_state['instant_clone_frozen'] is False:
-        # VM is not frozen, need to do prep work for instant clone
-        vm_network_adapters = []
-        devices = vm.config.hardware.device
-        for device in devices:
-            if isinstance(device, vim.VirtualEthernetCard):
-                vm_network_adapters.append(device)
+        # Build VM config dict
+        config = []
+        # If customvalues is not empty, fill config
+        if len(self.module.params['customvalues']) != 0:
+            for kv in self.module.params['customvalues']:
+                if 'key' not in kv or 'value' not in kv:
+                    self.module.module.exit_json(msg="The parameter customvalues items required both 'key' and 'value' fields.")
+                ov = vim.OptionValue()
+                ov.key = kv['key']
+                ov.value = kv['value']
+                config.append(ov)
 
-        for vm_network_adapter in vm_network_adapters:
-            # Standard network switch
-            if isinstance(vm_network_adapter.backing, vim.VirtualEthernetCardNetworkBackingInfo):
-                network_id = vm_network_adapter.backing.network
-                device_spec = vim.VirtualDeviceConfigSpec()
-                device_spec.operation = 'edit'
-                device_spec.device = vm_network_adapter
-                device_spec.device.backing = vim.VirtualEthernetCardNetworkBackingInfo()
-                device_spec.device.backing.deviceName = network_id
-                connectable = vim.VirtualDeviceConnectInfo()
-                connectable.migrateConnect = 'disconnect'
-                device_spec.device.connectable = connectable
-                location_spec.deviceChange.append(device_spec)
-            # Distributed network switch
-            elif isinstance(vm_network_adapter.backing, vim.VirtualEthernetCardDistributedVirtualPortBackingInfo):
-                network_id = vm_network_adapter.backing.port
-                # If the port key isn't cleared the VM clone will fail as the port is in use by the running source VM.
-                network_id.portKey = None
-                device_spec = vim.VirtualDeviceConfigSpec()
-                device_spec.operation = 'edit'
-                device_spec.device = vm_network_adapter
-                device_spec.device.backing = vim.VirtualEthernetCardDistributedVirtualPortBackingInfo()
-                device_spec.device.backing.port = network_id
-                connectable = vim.VirtualDeviceConnectInfo()
-                connectable.migrateConnect = 'disconnect'
-                device_spec.device.connectable = connectable
-                location_spec.deviceChange.append(device_spec)
-            else:
-                module.module.exit_json(msg='Unknown network backing type of "%s" only Virtual Distributed switches and Standard swtiches are supported.'
-                                        % vm_network_adapter.__class__.__name__.split('.')[-1])
-
+        # Begin building the spec
+        instantclone_spec = vim.VirtualMachineInstantCloneSpec()
+        location_spec = vim.VirtualMachineRelocateSpec()
         instantclone_spec.config = config
-        instantclone_spec.location = location_spec
-        instantclone_spec.name = module.params['name']
+        instantclone_spec.name = self.module.params['name']
 
-    else:
-        # VM is frozen, can clone without prep work
-        instantclone_spec.config = config
-        instantclone_spec.location = location_spec
-        instantclone_spec.name = module.params['name']
+        if vm_state['instant_clone_frozen'] is False:
+            # VM is not frozen, need to do prep work for instant clone
+            vm_network_adapters = []
+            devices = vm.config.hardware.device
+            for device in devices:
+                if isinstance(device, vim.VirtualEthernetCard):
+                    vm_network_adapters.append(device)
 
-    task = vm.InstantClone_Task(instantclone_spec)
-    wait_for_task(task)
+            for vm_network_adapter in vm_network_adapters:
+                # Standard network switch
+                if isinstance(vm_network_adapter.backing, vim.VirtualEthernetCardNetworkBackingInfo):
+                    network_id = vm_network_adapter.backing.network
+                    device_spec = vim.VirtualDeviceConfigSpec()
+                    device_spec.operation = 'edit'
+                    device_spec.device = vm_network_adapter
+                    device_spec.device.backing = vim.VirtualEthernetCardNetworkBackingInfo()
+                    device_spec.device.backing.deviceName = network_id
+                    connectable = vim.VirtualDeviceConnectInfo()
+                    connectable.migrateConnect = 'disconnect'
+                    device_spec.device.connectable = connectable
+                    location_spec.deviceChange.append(device_spec)
+                # Distributed network switch
+                elif isinstance(vm_network_adapter.backing, vim.VirtualEthernetCardDistributedVirtualPortBackingInfo):
+                    network_id = vm_network_adapter.backing.port
+                    # If the port key isn't cleared the VM clone will fail as the port is in use by the running source VM.
+                    network_id.portKey = None
+                    device_spec = vim.VirtualDeviceConfigSpec()
+                    device_spec.operation = 'edit'
+                    device_spec.device = vm_network_adapter
+                    device_spec.device.backing = vim.VirtualEthernetCardDistributedVirtualPortBackingInfo()
+                    device_spec.device.backing.port = network_id
+                    connectable = vim.VirtualDeviceConnectInfo()
+                    connectable.migrateConnect = 'disconnect'
+                    device_spec.device.connectable = connectable
+                    location_spec.deviceChange.append(device_spec)
+                else:
+                    self.module.module.exit_json(msg='Unknown network backing type of %s only Virtual Distributed switches and Standard swtiches are supported.'
+                                                 % vm_network_adapter.__class__.__name__.split('.')[-1])
 
-    if task.info.state == 'error':
-        kwargs = {
-            'changed': module.change_applied,
-            'failed': True,
-            'msg': task.info.error.msg,
+            # Finalize the spec for a non-frozen VM
+            instantclone_spec.location = location_spec
+
+        else:
+            # VM is frozen, can clone without prep work
+            # Finalize the spec for a frozen VM
+            instantclone_spec.location = location_spec
+
+        task = vm.InstantClone_Task(instantclone_spec)
+        wait_for_task(task)
+
+        if task.info.state == 'error':
+            kwargs = {
+                'changed': self.module.change_applied,
+                'failed': True,
+                'msg': task.info.error.msg,
+                'clone_method': 'InstantClone_Task'
+            }
+            return kwargs
+
+        clone = task.info.result
+        vm_facts = gather_vm_facts(self.module.content, clone)
+        return {
+            'changed': self.module.change_applied,
+            'failed': False,
+            'instance': vm_facts,
             'clone_method': 'InstantClone_Task'
         }
-        return kwargs
-
-    clone = task.info.result
-    vm_facts = gather_vm_facts(module.content, clone)
-    return {
-        'changed': module.change_applied,
-        'failed': False,
-        'instance': vm_facts,
-        'clone_method': 'InstantClone_Task'
-    }
 
 
 def main():
     argument_spec = vmware_argument_spec()
     argument_spec.update(
+        state=dict(default='present', choices=['present', 'absent']),
         name=dict(type='str'),
         source_vm=dict(type='str'),
         customvalues=dict(type='list', default=[]),
@@ -180,14 +194,31 @@ def main():
 
     result = {'failed': False, 'changed': False}
 
-    if module.check_mode:
-        result.update(
-            changed=True,
-            desired_operation='instantclone_vm',
-        )
-        module.exit_json(**result)
+    pyv = PyVmomiHelper(module)
+    vm = pyv.get_vm()
 
-    result = deploy_instantclone(module)
+    # The clone doesn't exist
+    if not vm:
+        if module.params['state'] == 'present':
+            if module.check_mode:
+                result.update(
+                    changed=True,
+                    desired_operation='instantclone_vm',
+                )
+                module.exit_json(**result)
+            # Create and instant clone
+            result = pyv.deploy_instantclone()
+    # The clone is present
+    else:
+        if module.params['state'] == 'absent':
+            if module.check_mode:
+                result.update(
+                    changed=True,
+                    desired_operation='remove_vm',
+                )
+                module.exit_json(**result)
+            # Delete the clone
+            vm.Destory()
 
     if result['failed']:
         module.fail_json(**result)
