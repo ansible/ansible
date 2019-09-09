c21052d3a0e4e1976406f2077c918f74d58892c0
Simon Dodsley <simon@purestorage.com>
2019-09-09 03:14:46 -0400

    purefa_info: Fix failure due to API version check (#61925)

--- lib/ansible/modules/storage/purestorage/_purefa_facts.py
+++ lib/ansible/modules/storage/purestorage/_purefa_facts.py
@@ -340,6 +340,7 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.pure import get_system, purefa_argument_spec
 
 
+ADMIN_API_VERSION = '1.14'
 S3_REQUIRED_API_VERSION = '1.16'
 LATENCY_REQUIRED_API_VERSION = '1.16'
 AC_REQUIRED_API_VERSION = '1.14'
@@ -461,14 +462,16 @@ def generate_config_dict(array):
 
 
 def generate_admin_dict(array):
+    api_version = array._list_available_rest_versions()
     admin_facts = {}
-    admins = array.list_admins()
-    for admin in range(0, len(admins)):
-        admin_name = admins[admin]['name']
-        admin_facts[admin_name] = {
-            'type': admins[admin]['type'],
-            'role': admins[admin]['role'],
-        }
+    if ADMIN_API_VERSION in api_version:
+        admins = array.list_admins()
+        for admin in range(0, len(admins)):
+            admin_name = admins[admin]['name']
+            admin_facts[admin_name] = {
+                'type': admins[admin]['type'],
+                'role': admins[admin]['role'],
+            }
     return admin_facts
 
 
