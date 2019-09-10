7df6241f8c5f868f8709005b6f2822d2bea0fd59
zhongjun2 <jun.zhongjun2@gmail.com>
2019-09-10 14:22:41 +0800

    Update hwc_vpc_port.py

--- lib/ansible/modules/cloud/huawei/hwc_vpc_port.py
+++ lib/ansible/modules/cloud/huawei/hwc_vpc_port.py
@@ -22,7 +22,7 @@ module: hwc_vpc_port
 description:
     - vpc port management.
 short_description: Creates a resource of Vpc/Port in Huawei Cloud
-version_added: '2.9'
+version_added: '2.10'
 author: Huawei Inc. (@huaweicloud)
 requirements:
     - keystoneauth1 >= 3.6.0
