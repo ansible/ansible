24d212f9234726ac79e3251f39f650ac335d58a2
aizquierdo <aizquierdo@mrmilu.com>
2019-09-10 17:07:06 +0200

    control exception when import_tasks is empty

--- lib/ansible/playbook/task_include.py
+++ lib/ansible/playbook/task_include.py
@@ -72,6 +72,10 @@ class TaskInclude(Task):
             task.args['_raw_params'] = task.args.pop('file', None)
 
         apply_attrs = task.args.get('apply', {})
+
+        if task.action in ('include_tasks', 'import_tasks') and not task.args:
+            raise AnsibleParserError('Invalid arguments for TASK: %s' % task.action, obj=data)
+
         if apply_attrs and task.action != 'include_tasks':
             raise AnsibleParserError('Invalid options for %s: apply' % task.action, obj=data)
         elif not isinstance(apply_attrs, dict):
