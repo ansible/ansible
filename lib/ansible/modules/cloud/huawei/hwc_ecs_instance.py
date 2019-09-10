14b4acb19df72a359ca1295a1b3f2b9677fae8f7
zhongjun2 <jun.zhongjun2@gmail.com>
2019-09-10 11:32:16 +0800

    Update hwc_ecs_instance.py

--- lib/ansible/modules/cloud/huawei/hwc_ecs_instance.py
+++ lib/ansible/modules/cloud/huawei/hwc_ecs_instance.py
@@ -22,7 +22,7 @@ module: hwc_ecs_instance
 description:
     - instance management.
 short_description: Creates a resource of Ecs/Instance in Huawei Cloud
-version_added: '2.9'
+version_added: '2.10'
 author: Huawei Inc. (@huaweicloud)
 requirements:
     - keystoneauth1 >= 3.6.0
