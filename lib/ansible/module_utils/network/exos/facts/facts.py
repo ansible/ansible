f3192509e0e125f3acfa9fe547905198d7982408
Jayalakshmi Viswanathan <shree6vishwa@gmail.com>
2019-09-10 15:27:44 -0700

    exos_lldp_interfaces resource module

--- lib/ansible/module_utils/network/exos/facts/facts.py
+++ lib/ansible/module_utils/network/exos/facts/facts.py
@@ -16,6 +16,8 @@ from ansible.module_utils.network.common.facts.facts import FactsBase
 from ansible.module_utils.network.exos.facts.lldp_global.lldp_global import Lldp_globalFacts
 from ansible.module_utils.network.exos.facts.vlans.vlans import VlansFacts
 from ansible.module_utils.network.exos.facts.legacy.base import Default, Hardware, Interfaces, Config
+from ansible.module_utils.network.exos.facts.lldp_interfaces.lldp_interfaces import Lldp_interfacesFacts
+
 
 FACT_LEGACY_SUBSETS = dict(
     default=Default,
@@ -26,6 +28,7 @@ FACT_LEGACY_SUBSETS = dict(
 FACT_RESOURCE_SUBSETS = dict(
     lldp_global=Lldp_globalFacts,
     vlans=VlansFacts,
+    lldp_interfaces=Lldp_interfacesFacts,
 )
 
 
