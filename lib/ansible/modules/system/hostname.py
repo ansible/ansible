7ba67287dd49ae702a4ee5bacc94fd408529a697
pixiake <guofeng@yunify.com>
2019-09-10 18:14:32 +0800

    Add support for Kylin linux to hostname module
    Add support for Kylin linux to hostname module

--- lib/ansible/modules/system/hostname.py
+++ lib/ansible/modules/system/hostname.py
@@ -678,6 +678,10 @@ class DebianHostname(Hostname):
     distribution = 'Debian'
     strategy_class = DebianStrategy
 
+class DebianHostname(Hostname):
+    platform = 'Linux'
+    distribution = 'Kylin'
+    strategy_class = DebianStrategy
 
 class CumulusHostname(Hostname):
     platform = 'Linux'
