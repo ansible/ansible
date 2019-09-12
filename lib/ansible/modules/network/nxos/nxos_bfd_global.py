820d5544355f144eb4c524a6dc731eeeed90cda3
Mike Wiebe <mwiebe@cisco.com>
2019-09-12 12:15:37 -0400

    Fix bfd cmd order and test issues (#61943)

--- lib/ansible/modules/network/nxos/nxos_bfd_global.py
+++ lib/ansible/modules/network/nxos/nxos_bfd_global.py
@@ -272,6 +272,23 @@ fabricpath_vlan:
 """
 
 
+def reorder_cmds(cmds):
+    '''
+    There is a bug in some image versions where bfd echo-interface and
+    bfd echo-rx-interval need to be applied last for them to nvgen properly.
+    '''
+    regex1 = re.compile(r'^bfd echo-interface')
+    regex2 = re.compile(r'^bfd echo-rx-interval')
+    filtered_cmds = [i for i in cmds if not regex1.match(i)]
+    filtered_cmds = [i for i in filtered_cmds if not regex2.match(i)]
+    echo_int_cmd = [i for i in cmds if regex1.match(i)]
+    echo_rx_cmd = [i for i in cmds if regex2.match(i)]
+    filtered_cmds.extend(echo_int_cmd)
+    filtered_cmds.extend(echo_rx_cmd)
+
+    return filtered_cmds
+
+
 def main():
     argument_spec = dict(
         echo_interface=dict(required=False, type='str'),
@@ -297,7 +314,7 @@ def main():
     cmd_ref = NxosCmdRef(module, BFD_CMD_REF)
     cmd_ref.get_existing()
     cmd_ref.get_playvals()
-    cmds = cmd_ref.get_proposed()
+    cmds = reorder_cmds(cmd_ref.get_proposed())
 
     result = {'changed': False, 'commands': cmds, 'warnings': warnings,
               'check_mode': module.check_mode}
