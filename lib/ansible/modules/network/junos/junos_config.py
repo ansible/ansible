93ac0b0bb502c1b8250e7d829a6f75ea63f8ca9b
Ajay Chenampara <ajay.chenampara@gmail.com>
2019-09-13 01:34:36 -0400

    Update junos_config.py (#62221)
    Add the commands alias to match UX of IOS, EOS etc.

--- lib/ansible/modules/network/junos/junos_config.py
+++ lib/ansible/modules/network/junos/junos_config.py
@@ -371,7 +371,7 @@ def main():
         dir_path=dict(type='path')
     )
     argument_spec = dict(
-        lines=dict(type='list'),
+        lines=dict(aliases=['commands'], type='list'),
 
         src=dict(type='path'),
         src_format=dict(choices=['xml', 'text', 'set', 'json']),
