fd88e2e3ea4c911f2b5b6b6c1dec6389e9331392
Matt Martz <matt@sivel.net>
2019-09-13 11:02:39 -0500

    Add missing import

--- lib/ansible/utils/unsafe_proxy.py
+++ lib/ansible/utils/unsafe_proxy.py
@@ -53,7 +53,7 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-from ansible.module_utils._text import to_text
+from ansible.module_utils._text import to_bytes, to_text
 from ansible.module_utils.common._collections_compat import Mapping, MutableSequence, Set
 from ansible.module_utils.six import string_types, binary_type, text_type
 
