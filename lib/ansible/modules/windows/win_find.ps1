1f5785205ed4e57b359e12925ba8c711e5889114
joshinryz <josh.robinett@gmail.com>
2019-09-15 18:04:59 -0500

    Support large folder size comparisons for win_find (#58466)
    * Support large folder size comparisons for win_find
    
    Changed [int] to [int64] to support larger folders. Otherwise module fails as soon as a large folder is encountered.
    
    * Create 58466-FIX_win_find-Bug-Get-FileStat_fails_on_large_files.yml
    
    (cherry picked from commit 8def67939dbd5dbba84fe160f3ad187c76ebe63a)

--- lib/ansible/modules/windows/win_find.ps1
+++ lib/ansible/modules/windows/win_find.ps1
@@ -177,7 +177,7 @@ Function Assert-Size($info) {
         $size_pattern = '^(-?\d+)(b|k|m|g|t)?$'
         $match = $size -match $size_pattern
         if ($match) {
-            [int]$specified_size = $matches[1]
+            [int64]$specified_size = $matches[1]
             if ($null -eq $matches[2]) {
                 $chosen_byte = 'b'
             } else {
