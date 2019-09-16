4afef7f9cfff3b3fd3c339d73d3bc7cc6225ee0b
P Srikantha <srikantha.parampalli@avinetworks.com>
2019-09-16 12:59:37 +0530

    lint fixes

--- lib/ansible/modules/cloud/azure/azure_rm_deployment_info.py
+++ lib/ansible/modules/cloud/azure/azure_rm_deployment_info.py
@@ -4,11 +4,7 @@
 #
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import absolute_import, division, print_function
-__metaclass__ = type
 
-
-from ansible.module_utils.azure_rm_common import AzureRMModuleBase
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
@@ -124,6 +120,11 @@ deployments:
                            works/myVirtualNetwork"
 '''
 
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+from ansible.module_utils.azure_rm_common import AzureRMModuleBase
 
 try:
     from msrestazure.azure_exceptions import CloudError
