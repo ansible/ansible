e4be1c5c696265ab9b42c5dba13dee083dd5c98d
Ganesh Nalawade <ganesh634@gmail.com>
2019-09-16 12:17:27 +0530

    Change enable to enabled for junos_interfaces module (#62321)
    Fixes #62319
    
    Change `enable` option to `enabled` in junos_interfaces
    and junos_lldp_interfaces
    data model to be in sync with other network platform
    resource modules added in 2.9 version.

--- lib/ansible/modules/network/junos/junos_lldp_interfaces.py
+++ lib/ansible/modules/network/junos/junos_lldp_interfaces.py
@@ -54,7 +54,7 @@ options:
           - Name of the interface LLDP needs to be configured on.
         type: str
         required: True
-      enable:
+      enabled:
         description:
           - This is a boolean value to control disabling of LLDP on the interface C(name)
         type: bool
@@ -82,7 +82,7 @@ EXAMPLES = """
     config:
       - name: ge-0/0/1
       - name: ge-0/0/2
-        enable: False
+        enabled: False
     state: merged
 
 # After state:
@@ -112,7 +112,7 @@ EXAMPLES = """
       - name: ge-0/0/2
         disable: False
       - name: ge-0/0/3
-        enable: False
+        enabled: False
     state: replaced
 
 # After state:
@@ -141,7 +141,7 @@ EXAMPLES = """
   junos_lldp_interfaces:
     config:
       - name: ge-0/0/2
-        enable: False
+        enabled: False
     state: overridden
 
 # After state:
