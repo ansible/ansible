5c1c91c3d2ba6b24ec31b803bfa0298cb4dad8e3
Andrey Klychkov <aaklychkov@mail.ru>
2019-09-17 19:10:03 +0300

    Backport of 62333: fix typos in storage modules (#62384)

--- lib/ansible/modules/storage/netapp/na_elementsw_access_group.py
+++ lib/ansible/modules/storage/netapp/na_elementsw_access_group.py
@@ -317,7 +317,7 @@ class ElementSWAccessGroup(object):
                 self.delete_access_group()
                 changed = True
             else:
-                # If state - present, check for any parameter of exising group needs modification.
+                # If state - present, check for any parameter of existing group needs modification.
                 if self.volumes is not None and len(self.volumes) > 0:
                     # Compare the volume list
                     for volumeID in group_detail.volumes:
--- lib/ansible/modules/storage/purestorage/purefa_user.py
+++ lib/ansible/modules/storage/purestorage/purefa_user.py
@@ -17,7 +17,7 @@ module: purefa_user
 version_added: '2.8'
 short_description: Create, modify or delete FlashArray local user account
 description:
-- Create, modify or delete local users on a Pure Stoage FlashArray.
+- Create, modify or delete local users on a Pure Storage FlashArray.
 author:
 - Pure Storage Ansible Team (@sdodsley) <pure-ansible-team@purestorage.com>
 options:
