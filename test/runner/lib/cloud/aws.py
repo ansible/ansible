c01378a2998a2ef57637488fd683332d71382215
Matt Clay <matt@mystile.com>
2019-09-16 20:44:15 -0700

    [stable-2.8] Mark AWS credentials in ansible-test as sensitive.
    This avoids displaying the credentials in CI when retrying tests at maximum verbosity.
    (cherry picked from commit b73e772)
    
    Co-authored-by: Matt Clay <matt@mystile.com>

--- test/runner/lib/cloud/aws.py
+++ test/runner/lib/cloud/aws.py
@@ -73,6 +73,9 @@ class AwsCloudProvider(CloudProvider):
                 REGION='us-east-1',
             )
 
+            display.sensitive.add(values['SECRET_KEY'])
+            display.sensitive.add(values['SECURITY_TOKEN'])
+
             config = self._populate_config_template(config, values)
 
         self._write_config(config)
@@ -98,6 +101,9 @@ class AwsCloudEnvironment(CloudEnvironment):
         )
 
         ansible_vars.update(dict(parser.items('default')))
+
+        display.sensitive.add(ansible_vars.get('aws_secret_key'))
+        display.sensitive.add(ansible_vars.get('security_token'))
 
         env_vars = {'ANSIBLE_DEBUG_BOTOCORE_LOGS': 'True'}
 
