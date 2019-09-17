6c7730c33832c75cea7d1013f366b74383dcf2ae
Matt Clay <matt@mystile.com>
2019-09-17 15:45:30 -0700

    WIP - Fix ansible-doc bugs and add integration tests. (#62461)
    * Add integration tests for ansible-doc.
    
    * Enable tests that now pass
    
    * Cleanup processing of plugin docs
    
    * Mostly separate the steps of processing plugin docs
    
      1) Acquire source data
      2) Transform and calculate additonal data
      3) Format data for output
      4) Output data
    
      format_plugin_doc() is still mixing transformation and formatting but
      that should be fixed in a devel-only change
    
    * Raise exceptions in _get_plugin_doc() on errors.
    
    * Remove check to exclude on blacklisted extensions.  We already request
      only .py files
    
    * If there is no DOCUMENTATION entry in the plugin, raise an exception
      from _get_plugin_doc().  Everywhere we use _get_plugin_doc(), this is
      treated as an error
    
    * If there is no ANSIBLE_METADATA raise an exception as well as
      displaying of docs assumes that this has been set.
    
    * If there is neither DOCUMENTATION nor ANSIBLE_METADATA, warn about the
      lack of METADATA and error on the lack of DOCUMENTATION.  Lack of
      DOCUMENTATION is more important so it is what the user should see.
    
    * Add a few special cases for backwards compat.  These should probably
      be made errors in 2.10:
      * no docs but has metadata shows no documentation rather than an error
      * empty plugin file shows no doumentation rather than an error
    
    * Simplify backwards compatibility logic.
    
    (cherry picked from commit 3b86dc3e12e334707251111b4188244db95a81ea)

---
+++ test/integration/targets/ansible-doc/library/test_no_docs_removed_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['removed'],
+                    'supported_by': 'core'}
+
+
+from ansible.module_utils.common.removed import removed_module
+
+
+if __name__ == '__main__':
+    removed_module(removed_in='2.9')
