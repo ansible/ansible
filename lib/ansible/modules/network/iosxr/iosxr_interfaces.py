6d384330c6c211c0c2bbdd6a06819ed8efe5963c
Sumit Jaiswal <sjaiswal@redhat.com>
2019-09-17 15:31:14 +0530

    complete iosxr

--- lib/ansible/modules/network/iosxr/iosxr_interfaces.py
+++ lib/ansible/modules/network/iosxr/iosxr_interfaces.py
@@ -349,6 +349,10 @@ def main():
     Main entry point for module execution
     :returns: the result form module invocation
     """
+    required_if = [('state', 'merged', ('config',)),
+                   ('state', 'replaced', ('config',)),
+                   ('state', 'overridden', ('config',))]
+
     module = AnsibleModule(argument_spec=InterfacesArgs.argument_spec,
                            required_if=required_if,
                            supports_check_mode=True)
--- lib/ansible/modules/network/iosxr/iosxr_l3_interfaces.py
+++ lib/ansible/modules/network/iosxr/iosxr_l3_interfaces.py
@@ -408,6 +408,10 @@ def main():
     Main entry point for module execution
     :returns: the result form module invocation
     """
+    required_if = [('state', 'merged', ('config',)),
+                   ('state', 'replaced', ('config',)),
+                   ('state', 'overridden', ('config',))]
+
     module = AnsibleModule(argument_spec=L3_InterfacesArgs.argument_spec,
                            required_if=required_if,
                            supports_check_mode=True)
