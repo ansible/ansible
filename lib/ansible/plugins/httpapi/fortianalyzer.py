9f3f978716764b4c2fc6da97d784ee7f1910a379
Luke Weighall <lweighall@gmail.com>
2019-09-18 09:48:42 -0700

    Hotfix for connection manager persistent connection initialization.

--- lib/ansible/plugins/httpapi/fortianalyzer.py
+++ lib/ansible/plugins/httpapi/fortianalyzer.py
@@ -28,8 +28,9 @@ author:
 httpapi : fortianalyzer
 short_description: HttpApi Plugin for Fortinet FortiAnalyzer Appliance or VM
 description:
-  - This HttpApi plugin provides methods to connect to Fortinet FortiAnalyzer Appliance or VM via JSON RPC API
-version_added: "2.9"
+  - This HttpApi plugin provides methods to connect to Fortinet FortiAnalyzer Appliance or VM via JSON RPC API.
+version_added: "2.8"
+v
 
 """
 
@@ -150,7 +151,14 @@ class HttpApi(HttpApiBase):
 
         try:
             if self.sid is None and params[0]["url"] != "sys/login/user":
-                raise FAZBaseException("An attempt was made to login with the SID None and URL != login url.")
+                # If not connected, send connection request.
+                if not self.connection._connected:
+                    try:
+                        self.connection._connect()
+                    except BaseException as err:
+                        raise FAZBaseException(
+                            msg="An problem happened with the httpapi plugin self-init connection process. "
+                                "Error: " + str(err))
         except IndexError:
             raise FAZBaseException("An attempt was made at communicating with a FAZ with "
                                    "no valid session and an incorrectly formatted request.")
