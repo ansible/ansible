f0b4539517b0013298b234bdf342bfdbbfd0c846
RusoSova <vsav@caasco.ca>
2019-09-24 15:02:44 -0400

    removed some redundant checks

--- lib/ansible/modules/windows/win_description.ps1
+++ lib/ansible/modules/windows/win_description.ps1
@@ -4,9 +4,6 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 #AnsibleRequires -CSharpUtil Ansible.Basic
-#AnsibleRequires -OSVersion 6.0
-
-$ErrorActionPreference = "Stop"
 
 $spec = @{
     options = @{
