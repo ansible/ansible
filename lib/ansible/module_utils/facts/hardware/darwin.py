485d4ce153ac096f1c0694f32ccc617c1414b7ff
Sam Doran <sdoran@redhat.com>
2019-09-30 17:02:08 -0400

    Fix bug in Darwin facts for free memory
    If the vm_stat command is not found, fact gathering would fail with an unhelpful error message. Handle this gracefully and return a default value for free memory.

--- lib/ansible/module_utils/facts/hardware/darwin.py
+++ lib/ansible/module_utils/facts/hardware/darwin.py
@@ -85,7 +85,8 @@ class DarwinHardware(Hardware):
 
     def get_memory_facts(self):
         memory_facts = {
-            'memtotal_mb': int(self.sysctl['hw.memsize']) // 1024 // 1024
+            'memtotal_mb': int(self.sysctl['hw.memsize']) // 1024 // 1024,
+            'memfree_mb': 0,
         }
 
         total_used = 0
@@ -94,32 +95,33 @@ class DarwinHardware(Hardware):
             vm_stat_command = get_bin_path('vm_stat')
         except ValueError:
             vm_stat_command = None
-        rc, out, err = self.module.run_command(vm_stat_command)
-        if rc == 0:
-            # Free = Total - (Wired + active + inactive)
-            # Get a generator of tuples from the command output so we can later
-            # turn it into a dictionary
-            memory_stats = (line.rstrip('.').split(':', 1) for line in out.splitlines())
+        if vm_stat_command:
+            rc, out, err = self.module.run_command(vm_stat_command)
+            if rc == 0:
+                # Free = Total - (Wired + active + inactive)
+                # Get a generator of tuples from the command output so we can later
+                # turn it into a dictionary
+                memory_stats = (line.rstrip('.').split(':', 1) for line in out.splitlines())
 
-            # Strip extra left spaces from the value
-            memory_stats = dict((k, v.lstrip()) for k, v in memory_stats)
+                # Strip extra left spaces from the value
+                memory_stats = dict((k, v.lstrip()) for k, v in memory_stats)
 
-            for k, v in memory_stats.items():
-                try:
-                    memory_stats[k] = int(v)
-                except ValueError as ve:
-                    # Most values convert cleanly to integer values but if the field does
-                    # not convert to an integer, just leave it alone.
-                    pass
+                for k, v in memory_stats.items():
+                    try:
+                        memory_stats[k] = int(v)
+                    except ValueError as ve:
+                        # Most values convert cleanly to integer values but if the field does
+                        # not convert to an integer, just leave it alone.
+                        pass
 
-            if memory_stats.get('Pages wired down'):
-                total_used += memory_stats['Pages wired down'] * page_size
-            if memory_stats.get('Pages active'):
-                total_used += memory_stats['Pages active'] * page_size
-            if memory_stats.get('Pages inactive'):
-                total_used += memory_stats['Pages inactive'] * page_size
+                if memory_stats.get('Pages wired down'):
+                    total_used += memory_stats['Pages wired down'] * page_size
+                if memory_stats.get('Pages active'):
+                    total_used += memory_stats['Pages active'] * page_size
+                if memory_stats.get('Pages inactive'):
+                    total_used += memory_stats['Pages inactive'] * page_size
 
-        memory_facts['memfree_mb'] = memory_facts['memtotal_mb'] - (total_used // 1024 // 1024)
+                memory_facts['memfree_mb'] = memory_facts['memtotal_mb'] - (total_used // 1024 // 1024)
 
         return memory_facts
 
