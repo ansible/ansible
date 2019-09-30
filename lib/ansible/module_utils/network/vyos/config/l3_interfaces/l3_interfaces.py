162ab0a637917cda055986bb075cf8e8cc7913a4
Nilashish Chakraborty <nilashishchakraborty8@gmail.com>
2019-09-30 20:37:28 +0530

    Fix traceback for empty config (#62520)
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- lib/ansible/module_utils/network/vyos/config/l3_interfaces/l3_interfaces.py
+++ lib/ansible/module_utils/network/vyos/config/l3_interfaces/l3_interfaces.py
@@ -110,6 +110,10 @@ class L3_interfaces(ConfigBase):
         """
         commands = []
         state = self._module.params['state']
+
+        if state in ('merged', 'replaced', 'overridden') and not want:
+            self._module.fail_json(msg='value of config parameter must not be empty for state {0}'.format(state))
+
         if state == 'overridden':
             commands.extend(self._state_overridden(want=want, have=have))
 
