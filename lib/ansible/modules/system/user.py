97e605bfeb672c9b7a93726c0a5d9dcc473ca901
John Chen <johnchen456@gmail.com>
2019-09-30 12:50:16 -0400

    user: compare Mac user properties using same type
    self._get_user_property returns a string, so when doing a comparison
    using this value, cast the second variable to a string so that the
    comparison behaves correctly

--- lib/ansible/modules/system/user.py
+++ lib/ansible/modules/system/user.py
@@ -2305,7 +2305,7 @@ class DarwinUser(User):
         for field in self.fields:
             if field[0] in self.__dict__ and self.__dict__[field[0]]:
                 current = self._get_user_property(field[1])
-                if current is None or current != self.__dict__[field[0]]:
+                if current is None or current != str(self.__dict__[field[0]]):
                     cmd = self._get_dscl()
                     cmd += ['-create', '/Users/%s' % self.name, field[1], self.__dict__[field[0]]]
                     (rc, _err, _out) = self.execute_command(cmd)
