297f0276536300e22477c159e786ed7a74e1bfbf
Modular Magician <magic-modules@google.com>
2019-10-01 21:57:00 +0000

    Bug fixes for GCP modules

--- lib/ansible/modules/cloud/google/gcp_bigquery_table.py
+++ lib/ansible/modules/cloud/google/gcp_bigquery_table.py
@@ -33,7 +33,7 @@ module: gcp_bigquery_table
 description:
 - A Table that belongs to a Dataset .
 short_description: Creates a GCP Table
-version_added: 2.8
+version_added: '2.8'
 author: Google Inc. (@googlecloudplatform)
 requirements:
 - python >= 2.6
@@ -77,7 +77,7 @@ options:
       determines the sort order of the data.
     required: false
     type: list
-    version_added: 2.9
+    version_added: '2.9'
   description:
     description:
     - A user-friendly description of the dataset.
@@ -105,7 +105,7 @@ options:
       buffer.
     required: false
     type: int
-    version_added: 2.9
+    version_added: '2.9'
   view:
     description:
     - The view definition.
@@ -155,7 +155,7 @@ options:
           or REQUIRED.
         required: false
         type: str
-        version_added: 2.9
+        version_added: '2.9'
       type:
         description:
         - The only type supported is DAY, which will generate one partition per day.
@@ -460,7 +460,43 @@ options:
     - Name of the dataset.
     required: false
     type: str
-extends_documentation_fragment: gcp
+  project:
+    description:
+    - The Google Cloud Platform project to use.
+    type: str
+  auth_kind:
+    description:
+    - The type of credential used.
+    type: str
+    required: true
+    choices:
+    - application
+    - machineaccount
+    - serviceaccount
+  service_account_contents:
+    description:
+    - The contents of a Service Account JSON file, either in a dictionary or as a
+      JSON string that represents it.
+    type: jsonarg
+  service_account_file:
+    description:
+    - The path of a Service Account JSON file if serviceaccount is selected as type.
+    type: path
+  service_account_email:
+    description:
+    - An optional service account email address if machineaccount is selected and
+      the user does not wish to use the default email.
+    type: str
+  scopes:
+    description:
+    - Array of scopes to be used
+    type: list
+  env_type:
+    description:
+    - Specifies which Ansible environment you're running this module within.
+    - This should not be set unless you know what you're doing.
+    - This only alters the User Agent string for any API requests.
+    type: str
 '''
 
 EXAMPLES = '''
--- lib/ansible/modules/cloud/google/gcp_compute_address_info.py
+++ lib/ansible/modules/cloud/google/gcp_compute_address_info.py
@@ -35,7 +35,7 @@ description:
 - This module was called C(gcp_compute_address_facts) before Ansible 2.9. The usage
   has not changed.
 short_description: Gather info for GCP Address
-version_added: 2.7
+version_added: '2.7'
 author: Google Inc. (@googlecloudplatform)
 requirements:
 - python >= 2.6
@@ -54,7 +54,54 @@ options:
     - This field is not applicable to global addresses.
     required: true
     type: str
-extends_documentation_fragment: gcp
+  project:
+    description:
+    - The Google Cloud Platform project to use.
+    type: str
+  auth_kind:
+    description:
+    - The type of credential used.
+    type: str
+    required: true
+    choices:
+    - application
+    - machineaccount
+    - serviceaccount
+  service_account_contents:
+    description:
+    - The contents of a Service Account JSON file, either in a dictionary or as a
+      JSON string that represents it.
+    type: jsonarg
+  service_account_file:
+    description:
+    - The path of a Service Account JSON file if serviceaccount is selected as type.
+    type: path
+  service_account_email:
+    description:
+    - An optional service account email address if machineaccount is selected and
+      the user does not wish to use the default email.
+    type: str
+  scopes:
+    description:
+    - Array of scopes to be used
+    type: list
+  env_type:
+    description:
+    - Specifies which Ansible environment you're running this module within.
+    - This should not be set unless you know what you're doing.
+    - This only alters the User Agent string for any API requests.
+    type: str
+notes:
+- for authentication, you can set service_account_file using the c(gcp_service_account_file)
+  env variable.
+- for authentication, you can set service_account_contents using the c(GCP_SERVICE_ACCOUNT_CONTENTS)
+  env variable.
+- For authentication, you can set service_account_email using the C(GCP_SERVICE_ACCOUNT_EMAIL)
+  env variable.
+- For authentication, you can set auth_kind using the C(GCP_AUTH_KIND) env variable.
+- For authentication, you can set scopes using the C(GCP_SCOPES) env variable.
+- Environment variables values will only be used if the playbook values are not set.
+- The I(service_account_email) and I(service_account_file) options are mutually exclusive.
 '''
 
 EXAMPLES = '''
@@ -109,6 +156,14 @@ resources:
         regular expression `[a-z]([-a-z0-9]*[a-z0-9])?` which means the first character
         must be a lowercase letter, and all following characters must be a dash, lowercase
         letter, or digit, except the last character, which cannot be a dash.
+      returned: success
+      type: str
+    purpose:
+      description:
+      - 'The purpose of this resource, which can be one of the following values: -
+        GCE_ENDPOINT for addresses that are used by VM instances, alias IP ranges,
+        internal load balancers, and similar resources.'
+      - This should only be set when using an Internal address.
       returned: success
       type: str
     networkTier:
