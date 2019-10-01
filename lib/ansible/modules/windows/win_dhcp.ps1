9372af97e3a216d2ff9c74f5ac3b465fb26c7af9
Joe Zollo <jzollo@vmware.com>
2019-10-01 12:19:32 -0400

    Correct BOM/UTF8 Encoding, Add Aliases to Proper Folder, Fix Metadata String

--- lib/ansible/modules/windows/win_dhcp.ps1
+++ lib/ansible/modules/windows/win_dhcp.ps1
@@ -1,6 +1,6 @@
 ﻿#!powershell
 
-# Copyright © 2019 VMware, Inc. All Rights Reserved.
+# Copyright: (c) 2019 VMware, Inc. All Rights Reserved.
 # SPDX-License-Identifier: GPL-3.0-only
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
--- lib/ansible/modules/windows/win_dhcp.py
+++ lib/ansible/modules/windows/win_dhcp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright © 2019 VMware, Inc. All Rights Reserved.
+# Copyright: (c) 2019 VMware, Inc. All Rights Reserved.
 # SPDX-License-Identifier: GPL-3.0-only
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
