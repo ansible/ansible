d5e4d8e0099ac501c359f21298be0cd00da0ce50
Adrian Likins <alikins@redhat.com>
2019-10-07 16:41:49 -0400

    Stop appending '/api' to configured galaxy server url
    Since not all galaxy REST api server URLs live
    at '/api', stop always appending it to the
    'url' value loaded from config.

--- lib/ansible/galaxy/api.py
+++ lib/ansible/galaxy/api.py
@@ -38,7 +38,7 @@ def g_connect(versions):
 
                 # Determine the type of Galaxy server we are talking to. First try it unauthenticated then with Bearer
                 # auth for Automation Hub.
-                n_url = _urljoin(self.api_server, 'api')
+                n_url = self.api_server
                 error_context_msg = 'Error when finding available api versions from %s (%s)' % (self.name, n_url)
 
                 data = self._call_galaxy(n_url, method='GET', error_context_msg=error_context_msg)
