4f6abcf419abc45179a4cd2e6c78b061a1deacf7
kvprasoon <prasoonkarunan5@hotmail.com>
2019-10-08 23:39:59 +0530

    add new modul

---
+++ lib/ansible/modules/windows/win_auto_logon.ps1
@@ -0,0 +1,83 @@
+#!powershell
+
+# Copyright: (c) 2019, Prasoon Karunan V (@prasoonkarunan) <kvprasoon@Live.in>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
+# All helper methods are written in a binary module and has to be loaded for consuming them.
+#AnsibleRequires -CSharpUtil Ansible.Basic
+
+Set-StrictMode -Version 2.0
+
+$ErrorActionPreferrence = 'Stop'
+
+# argument spec hastable which has the input parameters and capabilities for this module
+$spec = @{
+    options = @{
+        domain = @{type = "str"; default = $env:userdomain}
+        user = @{type = "str"; required = $true}
+        password = @{type = "str"; required = $true}
+        state = @{type = "str"; choices = "absent","present"; default = "present"}
+    }
+    required_if = @(
+        @("state", "present", @("user","password")),
+        @("state","absent",@())
+    )
+}
+
+$module = [Ansible.Basic.AnsibleModule]::Create($args,$spec)
+$domain = $module.params.domain
+$user = $module.params.user
+$password = $module.params.password
+$state = $module.params.state
+
+try {
+    #Build ParamHash
+
+    $autoAdminLogon = 1
+    $stateMessage = 'added'
+    if($state -eq 'absent'){
+        $autoadminlogon = 0
+        $stateMessage = 'removed'
+    }
+    $module.Result.Msg     = "Auto logon registry keys are already $stateMessage"
+    $autoLogonKeyList   = @{
+        DefaultPassword = $password
+        DefaultUserName = $user
+        DefaultDomain   = $domain
+        AutoAdminLogon  = $autoAdminLogon
+    }
+    $actionTaken = $null
+    $autoLogonRegPath   = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\'
+    $autoLogonKeyRegList   = Get-ItemProperty -Path $autoLogonRegPath -Name $autoLogonKeyList.GetEnumerator().Name -ErrorAction SilentlyContinue
+
+    Foreach($key in $autoLogonKeyList.GetEnumerator().Name){
+        $currentKeyValue = $autoLogonKeyRegList | Select-Object -ExpandProperty $key -ErrorAction SilentlyContinue
+        if (-not [String]::IsNullOrEmpty($currentKeyValue)) {
+            $expectedValue = $autoLogonKeyList[$key]
+            if(($state -eq 'present') -and ($currentKeyValue -ne $expectedValue)) {
+                Set-ItemProperty -Path $autoLogonRegPath -Name $key -Value $autoLogonKeyList[$key] -Force
+                $actionTaken = $true
+            }
+            elseif($state -eq 'absent') {
+                $actionTaken = $true
+                Remove-ItemProperty -Path $autoLogonRegPath -Name $key -Force
+            }
+        }
+        else {
+            if ($state -eq 'present') {
+                $actionTaken = $true
+                New-ItemProperty -Path $autoLogonRegPath -Name $key -Value $autoLogonKeyList[$key] -Force | Out-Null
+            }
+        }
+    }
+    if($actionTaken){
+        $module.Result.Msg     = "Auto logon registry keys are $stateMessage"
+        $module.Result.changed = $true
+    }
+}
+catch {
+  $module.FailJson($_.Exception.Message)
+}
+
+$module.ExitJson()
---
+++ lib/ansible/modules/windows/win_auto_logon.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2019, Prasoon Karunan V (@prasoonkarunan)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = r'''
+---
+module: win_auto_logon
+short_description: Adds or Sets auto logon registry keys.
+description:
+  - Used to apply auto logon registry setting.
+version_added: "2.9"
+options:
+  user:
+    description:
+      - Username to login automatically.
+      - Value of this input will be set to DefaultUsername registry key..
+    type: str
+    required: yes
+  password:
+    description:
+      - Password to be used for automatic login.
+      - Value of this input will be used as password for Default user.
+    type: str
+    required: yes
+  domain:
+    description:
+      - Domain name to be used while automatic login.
+    type: str
+    default: default user domain.
+  state:
+    description:
+      - Whether the registry key should be C(present) or C(absent).
+    type: str
+    choices: [ absent, present ]
+    default: present
+author:
+  - Prasoon Karunan V (@prasoonkarunan)
+'''
+
+EXAMPLES = r'''
+- name: Set autologon for user1
+  win_auto_logon:
+    user: User1
+    password: str0ngp@ssword
+
+- name: Set autologon for abc.com\\user1
+  win_auto_logon:
+    user: User1
+    password: str0ngp@ssword
+    domain: abc.com
+
+- name: Remove autologon for user1
+  win_auto_logon:
+    state: absent
+'''
