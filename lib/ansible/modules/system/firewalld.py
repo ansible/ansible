652dfb33bb152552197247a8de6684bd178f2e4a
Adam Miller <admiller@redhat.com>
2019-10-10 11:13:55 -0500

    Update lib/ansible/modules/system/firewalld.py
    Co-Authored-By: Dag Wieers <dag@wieers.com>

--- lib/ansible/modules/system/firewalld.py
+++ lib/ansible/modules/system/firewalld.py
@@ -862,7 +862,7 @@ def main():
 
     if modification_count > 1:
         module.fail_json(
-            msg='can only operate on port, service, rich_rule, masquerade, icmp_block, icmp_block_inversion, interface or source at once'
+            msg='can only operate on port, service, rich_rule, masquerade, icmp_block, icmp_block_inversion, interface, source or direct_rule at once'
         )
     elif modification_count > 0 and desired_state in ['absent', 'present']:
         module.fail_json(
