db8ab4ee0f094c4dfa3c72bbb4dbba78806cce9e
gosriniv <gosriniv@localhost.localdomain>
2019-10-11 12:53:59 -0400

    Added unit testcases to eos_vlans

---
+++ test/units/modules/network/eos/test_eos_vlans.py
@@ -0,0 +1,197 @@
+#
+# (c) 2019, Ansible by Red Hat, inc
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from units.compat.mock import patch
+from ansible.modules.network.eos import eos_vlans
+from units.modules.utils import set_module_args
+from .eos_module import TestEosModule, load_fixture
+
+
+class TestEosVlansModule(TestEosModule):
+    module = eos_vlans
+
+    def setUp(self):
+        super(TestEosVlansModule, self).setUp()
+
+        self.mock_get_config = patch('ansible.module_utils.network.common.network.Config.get_config')
+        self.get_config = self.mock_get_config.start()
+
+        self.mock_load_config = patch('ansible.module_utils.network.common.network.Config.load_config')
+        self.load_config = self.mock_load_config.start()
+
+
+        self.mock_get_resource_connection_config = patch('ansible.module_utils.network.common.cfg.base.get_resource_connection')
+        self.get_resource_connection_config = self.mock_get_resource_connection_config.start()
+
+        self.mock_get_resource_connection_facts = patch('ansible.module_utils.network.common.facts.facts.get_resource_connection')
+        self.get_resource_connection_facts = self.mock_get_resource_connection_facts.start()
+
+        self.mock_edit_config = patch('ansible.module_utils.network.eos.providers.providers.CliProvider.edit_config')
+        self.edit_config = self.mock_edit_config.start()
+
+        self.mock_execute_show_command = patch('ansible.module_utils.network.eos.config.vlans.vlans.Vlans.get_vlans_facts')
+        self.execute_show_command = self.mock_execute_show_command.start()
+
+    def tearDown(self):
+        super(TestEosVlansModule, self).tearDown()
+        self.mock_get_resource_connection_config.stop()
+        self.mock_get_resource_connection_facts.stop()
+        self.mock_edit_config.stop()
+        self.mock_get_config.stop()
+        self.mock_load_config.stop()
+        self.mock_execute_show_command.stop()
+
+    def load_fixtures(self, commands=None, transport='cli'):
+        file_cmd = load_fixture('eos_vlan_config.cfg').split()
+        file_cmd_dict = {file_cmd[i]: file_cmd[i + 1] for i in range(0, len(file_cmd), 2)}
+        self.execute_show_command.return_value = [{x: int(y) if x == 'vlan_id' else y   for x,y in file_cmd_dict.items()}] 
+        
+        
+
+    def test_eos_vlan_default(self):
+        self.execute_show_command.return_value = []
+        set_module_args(dict(
+            config=[dict(
+                vlan_id = 30,
+                name = "thirty"
+            )]
+        ))
+        commands = ['vlan 30', 'name thirty']
+        self.execute_module(changed=True, commands=commands)
+
+
+    def test_eos_vlan_default_idempotent(self):
+        self.execute_show_command.return_value = load_fixture('eos_vlan_config.cfg')
+        set_module_args(dict(
+            config=[dict(
+                vlan_id = 10,
+                name = "ten"
+                )
+            ]
+        ))
+        commands = ['vlan 10', 'name ten']
+        self.execute_module(changed=False, commands=[])
+    
+    def test_eos_vlan_merged(self):
+        self.execute_show_command.return_value = []
+        set_module_args(dict(
+            config=[dict(
+                vlan_id = 30,
+                name = "thirty"
+            )], state = "merged"
+        ))
+        commands = ['vlan 30', 'name thirty']
+        self.execute_module(changed=True, commands=commands)
+
+
+    def test_eos_vlan_merged_idempotent(self):
+        self.execute_show_command.return_value = load_fixture('eos_vlan_config.cfg')
+        set_module_args(dict(
+            config=[dict(
+                vlan_id = 10,
+                name = "ten"
+                )
+            ], state = "merged"
+        ))
+        commands = ['vlan 10', 'name ten']
+        self.execute_module(changed=False, commands=[])
+
+    def test_eos_vlan_replaced(self):
+        self.execute_show_command.return_value = []
+        set_module_args(dict(
+            config=[dict(
+                vlan_id = 30,
+                name = "thirty",
+                state = "suspend" 
+            )], state = "replaced" 
+        ))
+        commands = ['vlan 30', 'name thirty', 'state suspend']
+        self.execute_module(changed=True, commands=commands)
+
+
+    def test_eos_vlan_replaced_idempotent(self):
+        self.execute_show_command.return_value = load_fixture('eos_vlan_config.cfg')
+        set_module_args(dict(
+            config=[dict(
+                vlan_id = 10,
+                name = "ten"
+                )
+            ], state = "replaced"
+        ))
+        commands = ['vlan 10', 'name ten']
+        self.execute_module(changed=False, commands=[])
+
+#Bug exists. Will be uncommented once the bug is fixed
+
+#    def test_eos_vlan_overridden(self):
+#        self.execute_show_command.return_value = []
+#        set_module_args(dict(
+#            config=[dict(
+#                vlan_id = 30,
+#                name = "thirty",
+#                state = "suspend"
+#            )], state = "overridden" 
+#        ))
+#        commands = ['vlan 30', 'name thirty', 'state suspend']
+#        self.execute_module(changed=True, commands=commands)
+
+
+    def test_eos_vlan_overridden_idempotent(self):
+        self.execute_show_command.return_value = load_fixture('eos_vlan_config.cfg')
+        set_module_args(dict(
+            config=[dict(
+                vlan_id = 10,
+                name = "ten"
+                )
+            ], state = "overridden"
+        ))
+        commands = ['vlan 10', 'name ten']
+        self.execute_module(changed=False, commands=[])
+    
+    def test_eos_vlan_deleted(self):
+        set_module_args(dict(
+            config=[dict(
+                vlan_id = 10,
+                name = "ten",
+            )], state = "deleted"
+        ))
+        commands = ['vlan 10', 'no name']
+        self.execute_module(changed=True, commands=commands)
+    
+    def test_eos_vlan_id_datatype(self):
+        set_module_args(dict(
+            config=[dict(
+                vlan_id="thirty"
+            )]
+        ))
+        commands = ['vlan thirty']
+        result = self.execute_module(failed=True)
+        self.assertIn("we were unable to convert to int", result['msg'])
+
+#Bug exists. Will be uncommented once the bug is fixed
+
+#    def test_eos_vlan_id_datarange(self):
+#        set_module_args(dict(
+#            config=[dict(
+#                vlan_id=20000
+#            )]
+#        ))
+#        commands = ['vlan outofrange']
+#        result = self.execute_module(failed=True)
+#        self.assertIn("we were unable to convert to int", result['msg'])
+
+    def test_eos_vlan_state_datatype(self):
+        set_module_args(dict(
+            config=[dict(
+                vlan_id = 30,
+                state = 10
+            )]
+        ))
+        commands = ['vlan 30', 'state 10']
+        result = self.execute_module(failed=True)
+        self.assertIn("value of state must be one of: active, suspend", result['msg'])
