ee290bf5b17e80973787bbbc0dac46e2a2311681
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-10-15 14:33:52 +0200

    Declare support for Python 3.8 @ Trove classifiers

--- setup.py
+++ setup.py
@@ -311,6 +311,7 @@ static_setup_params = dict(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: System :: Installation/Setup',
         'Topic :: System :: Systems Administration',
         'Topic :: Utilities',
