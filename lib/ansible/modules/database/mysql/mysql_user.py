d3283d65093d898098b080f88bd796a580a7954b
Andrey Klychkov <aaklychkov@mail.ru>
2019-10-15 11:48:13 +0300

    mysql_user: add seealso (#63497)

--- lib/ansible/modules/database/mysql/mysql_user.py
+++ lib/ansible/modules/database/mysql/mysql_user.py
@@ -103,6 +103,12 @@ notes:
      the file."
    - Currently, there is only support for the `mysql_native_password` encrypted password hash module.
 
+seealso:
+- module: mysql_info
+- name: MySQL access control and account management reference
+  description: Complete reference of the MySQL access control and account management documentation.
+  link: https://dev.mysql.com/doc/refman/8.0/en/access-control.html
+
 author:
 - Jonathan Mainguy (@Jmainguy)
 - Benjamin Malynovytch (@bmalynovytch)
