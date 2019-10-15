a505b2f77701c70c27f2e477a92685bba694b212
Mario Lenz <m@riolenz.de>
2019-10-15 19:58:44 +0200

    Improve create_vlan_list() helper function

--- lib/ansible/modules/cloud/vmware/vmware_dvs_portgroup.py
+++ lib/ansible/modules/cloud/vmware/vmware_dvs_portgroup.py
@@ -237,13 +237,27 @@ class VMwareDvsPortgroup(PyVmomi):
     def create_vlan_list(self):
         vlan_id_list = []
         for vlan_id_splitted in self.module.params['vlan_id'].split(','):
-            try:
-                vlan_id_start, vlan_id_end = map(int, vlan_id_splitted.split('-'))
+            vlans = vlan_id_splitted.split('-')
+            if len(vlans) > 2:
+                self.module.fail_json(msg="Invalid VLAN range %s." % vlan_id_splitted)
+            if len(vlans) == 2:
+                vlan_id_start = vlans[0].strip()
+                vlan_id_end = vlans[1].strip()
+                if not vlan_id_start.isdigit():
+                    self.module.fail_json(msg="Invalid VLAN %s." % vlan_id_start)
+                if not vlan_id_end.isdigit():
+                    self.module.fail_json(msg="Invalid VLAN %s." % vlan_id_end)
+                vlan_id_start = int(vlan_id_start)
+                vlan_id_end = int(vlan_id_end)
                 if vlan_id_start not in range(0, 4095) or vlan_id_end not in range(0, 4095):
                     self.module.fail_json(msg="vlan_id range %s specified is incorrect. The valid vlan_id range is from 0 to 4094." % vlan_id_splitted)
                 vlan_id_list.append((vlan_id_start, vlan_id_end))
-            except ValueError:
-                vlan_id_list.append((int(vlan_id_splitted.strip()), int(vlan_id_splitted.strip())))
+            else:
+                vlan_id = vlans[0].strip()
+                if not vlan_id.isdigit():
+                    self.module.fail_json(msg="Invalid VLAN %s." % vlan_id)
+                vlan_id = int(vlan_id)
+                vlan_id_list.append((vlan_id, vlan_id))
 
         vlan_id_list.sort()
 
