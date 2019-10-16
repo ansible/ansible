e2e2577cbeaafad38a7a4a20e04e294c8f438075
Andrey Klychkov <aaklychkov@mail.ru>
2019-10-16 10:05:21 +0300

    Bugfix of 23976: mysql_replication - allow to pass blank lines

--- lib/ansible/modules/database/mysql/mysql_replication.py
+++ lib/ansible/modules/database/mysql/mysql_replication.py
@@ -147,6 +147,10 @@ options:
     type: str
     version_added: '2.10'
 
+notes:
+- If an empty value for the parameter of string type is needed,
+  use string with a single space.
+
 extends_documentation_fragment:
 - mysql
 
@@ -333,6 +337,11 @@ def start_slave(cursor, connection_name='', channel=''):
 
 
 def changemaster(cursor, chm, connection_name='', channel=''):
+    # Params ransformation for issue #23976:
+    for num, param in enumerate(chm):
+        if "=' '" in param:
+            chm[num] = chm[num].replace("=' '","=''")
+
     if connection_name:
         query = "CHANGE MASTER '%s' TO %s" % (connection_name, ','.join(chm))
     else:
