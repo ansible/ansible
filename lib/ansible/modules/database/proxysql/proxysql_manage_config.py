640d05713bb43eac282f7cec4261e8154e0a6508
DOCIS-007 <56439612+DOCIS-007@users.noreply.github.com>
2019-10-16 16:49:18 +0200

    Update proxysql_manage_config.py

--- lib/ansible/modules/database/proxysql/proxysql_manage_config.py
+++ lib/ansible/modules/database/proxysql/proxysql_manage_config.py
@@ -164,7 +164,7 @@ def main():
                                                          'MYSQL SERVERS',
                                                          'MYSQL QUERY RULES',
                                                          'MYSQL VARIABLES',
-                                                         'PROXYSQL SERVERS'
+                                                         'PROXYSQL SERVERS',
                                                          'ADMIN VARIABLES',
                                                          'SCHEDULER']),
             direction=dict(required=True, choices=['FROM',
