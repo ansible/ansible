d00d0c81b379f4232421ec01a104b3ce1c6e2820
Felix Fontein <felix@fontein.de>
2019-10-17 10:40:13 +0200

    openssl_privatekey: add support for format option (#60388)
    * Add support for format option.
    
    * Improve private key format detection.
    
    * Fix raw format handling.
    
    * Improve error handling.
    
    * Improve raw key handling.
    
    * Add failed raw test.
    
    * Improve raw key loading.
    
    * Simplify tests.
    
    * Add raw format tests.
    
    * Fail if format != 'auto_ignore' is specified for pyopenssl backend.
    
    * Fix quoting.
    
    * Bump version.
    
    * Allow to convert private keys between different formats.
    
    * Improve description.

--- lib/ansible/module_utils/crypto.py
+++ lib/ansible/module_utils/crypto.py
@@ -1903,3 +1903,29 @@ else:
             no >>= 1
             count += 1
         return count
+
+
+PEM_START = '-----BEGIN '
+PEM_END = '-----'
+PKCS8_PRIVATEKEY_NAMES = ('PRIVATE KEY', 'ENCRYPTED PRIVATE KEY')
+PKCS1_PRIVATEKEY_SUFFIX = ' PRIVATE KEY'
+
+
+def identify_private_key_format(content):
+    '''Given the contents of a private key file, identifies its format.'''
+    # See https://github.com/openssl/openssl/blob/master/crypto/pem/pem_pkey.c#L40-L85
+    # (PEM_read_bio_PrivateKey)
+    # and https://github.com/openssl/openssl/blob/master/include/openssl/pem.h#L46-L47
+    # (PEM_STRING_PKCS8, PEM_STRING_PKCS8INF)
+    try:
+        lines = content.decode('utf-8').splitlines(False)
+        if lines[0].startswith(PEM_START) and lines[0].endswith(PEM_END) and len(lines[0]) > len(PEM_START) + len(PEM_END):
+            name = lines[0][len(PEM_START):-len(PEM_END)]
+            if name in PKCS8_PRIVATEKEY_NAMES:
+                return 'pkcs8'
+            if len(name) > len(PKCS1_PRIVATEKEY_SUFFIX) and name.endswith(PKCS1_PRIVATEKEY_SUFFIX):
+                return 'pkcs1'
+            return 'unknown-pem'
+    except UnicodeDecodeError:
+        pass
+    return 'raw'
