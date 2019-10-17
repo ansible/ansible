6cd2b644a2f2c37881df4388523fd433f438a707
Andrey Klychkov <aaklychkov@mail.ru>
2019-10-17 09:03:20 +0300

    postgresql_privs: add support a type parameter option for types, add schema handling

--- lib/ansible/modules/database/postgresql/postgresql_privs.py
+++ lib/ansible/modules/database/postgresql/postgresql_privs.py
@@ -70,8 +70,10 @@ options:
   schema:
     description:
     - Schema that contains the database objects specified via I(objs).
-    - May only be provided if I(type) is C(table), C(sequence), C(function)
-      or C(default_privs). Defaults to  C(public) in these cases.
+    - May only be provided if I(type) is C(table), C(sequence), C(function), C(type),
+      or C(default_privs). Defaults to C(public) in these cases.
+    - Pay attantion, for embedded types when I(type=type)
+      I(schema) can be C(pg_catalog) or C(information_schema) respectively.
     type: str
   roles:
     description:
@@ -386,6 +388,15 @@ EXAMPLES = r'''
     type: default_privs
     role: reader
     target_roles: librarian
+
+- name: Grant type privileges for pg_catalog.numeric type to alice
+  postgresql_privs:
+    type: type
+    roles: alice
+    privs: ALL
+    objs: numeric
+    schema: pg_catalog
+    db: acme
 '''
 
 RETURN = r'''
@@ -626,16 +637,17 @@ class Connection(object):
         self.cursor.execute(query, (fdws,))
         return [t[0] for t in self.cursor.fetchall()]
 
-    def get_type_acls(self, types):
-        query = """SELECT typacl FROM pg_catalog.pg_type
-                   WHERE typname = ANY (%s) ORDER BY typname"""
-        self.cursor.execute(query, (types,))
-        return [t[0] for t in self.cursor.fetchall()]
-
     def get_foreign_server_acls(self, fs):
         query = """SELECT srvacl FROM pg_catalog.pg_foreign_server
                    WHERE srvname = ANY (%s) ORDER BY srvname"""
         self.cursor.execute(query, (fs,))
+        return [t[0] for t in self.cursor.fetchall()]
+
+    def get_type_acls(self, schema, types):
+        query = """SELECT t.typacl FROM pg_catalog.pg_type t
+                   JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace
+                   WHERE n.nspname = %s AND t.typname = ANY (%s) ORDER BY typname"""
+        self.cursor.execute(query, (schema, types))
         return [t[0] for t in self.cursor.fetchall()]
 
     # Manipulating privileges
@@ -686,7 +698,7 @@ class Connection(object):
         elif obj_type == 'foreign_server':
             get_status = self.get_foreign_server_acls
         elif obj_type == 'type':
-            get_status = self.get_type_acls
+            get_status = partial(self.get_type_acls, schema_qualifier)
         else:
             raise Error('Unsupported database object type "%s".' % obj_type)
 
@@ -703,7 +715,7 @@ class Connection(object):
                 except Exception:
                     raise Error('Illegal function signature: "%s".' % obj)
                 obj_ids.append('"%s"."%s"(%s' % (schema_qualifier, f, args))
-        elif obj_type in ['table', 'sequence']:
+        elif obj_type in ['table', 'sequence', 'type']:
             obj_ids = ['"%s"."%s"' % (schema_qualifier, o) for o in objs]
         else:
             obj_ids = ['"%s"' % o for o in objs]
@@ -936,7 +948,7 @@ def main():
     # Create type object as namespace for module params
     p = type('Params', (), module.params)
     # param "schema": default, allowed depends on param "type"
-    if p.type in ['table', 'sequence', 'function', 'default_privs']:
+    if p.type in ['table', 'sequence', 'function', 'type', 'default_privs']:
         p.schema = p.schema or 'public'
     elif p.schema:
         module.fail_json(msg='Argument "schema" is not allowed '
