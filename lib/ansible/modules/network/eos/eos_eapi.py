428c1b335c278adacdb1687de40138bc5ea80bb8
gosriniv <gosriniv@localhost.localdomain>
2019-10-18 08:40:20 -0400

    Corrected lint errors

--- lib/ansible/modules/network/eos/eos_eapi.py
+++ lib/ansible/modules/network/eos/eos_eapi.py
@@ -268,7 +268,6 @@ def map_obj_to_commands(updates, module, warnings):
             add('no protocol unix-socket')
         else:
             add('protocol unix-socket')
-
     if needs_update('state'):
         if want['state'] == 'stopped':
             add('shutdown')
