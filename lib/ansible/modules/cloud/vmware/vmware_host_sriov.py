7623eaba48a0f1d2bc45e9ad9ec354c3218388d0
Viktor Tsymbalyuk (@victron) <Viktor.Tsymbalyuk@gmail.com>
2019-10-19 11:30:51 +0300

    vmware_host_sriov: working version
    - integration tests (tested on ESXi 6.0)
    - unit tests

---
+++ lib/ansible/modules/cloud/vmware/vmware_host_sriov.py
@@ -0,0 +1,329 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {
+    "metadata_version": "1.1",
+    "status": ["preview"],
+    "supported_by": "community",
+}
+
+DOCUMENTATION = r"""
+---
+module: vmware_host_sriov
+short_description: Manage SR-IOV settings on host
+description:
+- This module can be used to configure, enable/disable SR-IOV functions an ESXi host.
+- module didn't reboot host after changes, but put in output "rebootRequired" key.
+- User can specify an ESXi hostname or Cluster name. In case of cluster name, all ESXi hosts are updated.
+
+author:
+- Viktor Tsymbalyuk (@victron)
+notes:
+- Tested on vSphere 6.0
+requirements:
+- python >= 2.6
+- PyVmomi
+options:
+  esxi_hostname:
+    description:
+    - Name of the host system to work with.
+    - This parameter is required if C(cluster_name) is not specified.
+    type: str
+  cluster_name:
+    description:
+    - Name of the cluster from which all host systems will be used.
+    - This parameter is required if C(esxi_hostname) is not specified.
+    type: str
+  vmnic:
+    description:
+    - interface name, like vmnic0
+    type: str
+    required: True
+  sriovOn:
+    description:
+    - desired SR-IOV state on interface
+    type: bool
+    required: True
+  numVirtFunc:
+    description:
+    - number of functions to activate on interface
+    - if sriovOn false should be equal 0
+    - if sriovOn true should be more then 0
+    type: bool
+    required: True
+
+"""
+
+EXAMPLES = r"""
+- name: enable SR-IOV on vmnic0 with 8 functions
+  vmware_host_sriov:
+    hostname: "{{ vcenter_hostname }}"
+    username: "{{ vcenter_username }}"
+    password: "{{ vcenter_password }}"
+    esxi_hostname: "{{ esxi1 }}"
+    validate_certs: no
+    vmnic: vmnic0
+    sriovOn: true
+    numVirtFunc: 8
+
+- name: enable SR-IOV on already enabled interface vmnic0
+  vmware_host_sriov:
+    hostname: "{{ vcenter_hostname }}"
+    username: "{{ vcenter_username }}"
+    password: "{{ vcenter_password }}"
+    esxi_hostname: "{{ esxi1 }}"
+    validate_certs: no
+    vmnic: vmnic0
+    sriovOn: true
+    numVirtFunc: 8
+
+- name: enable SR-IOV on vmnic0 with big num. of functions
+  vmware_host_sriov:
+    hostname: "{{ vcenter_hostname }}"
+    username: "{{ vcenter_username }}"
+    password: "{{ vcenter_password }}"
+    esxi_hostname: "{{ esxi1 }}"
+    validate_certs: no
+    vmnic: vmnic0
+    sriovOn: true
+    numVirtFunc: 100
+  ignore_errors: yes
+
+- name: disable SR-IOV on vmnic0
+  vmware_host_sriov:
+    hostname: "{{ vcenter_hostname }}"
+    username: "{{ vcenter_username }}"
+    password: "{{ vcenter_password }}"
+    esxi_hostname: "{{ esxi1 }}"
+    validate_certs: no
+    vmnic: vmnic0
+    sriovOn: false
+    numVirtFunc: 0
+"""
+
+RETURN = r"""
+    description:
+    - contains info about SR-IOV staus on vmnic before, after and requested changes 
+    - sometimes vCenter slowly update info, as result "after" contains same info as "before"
+      need to run again in check_mode or reboot host, as ESXi requested 
+    returned: always
+    type: dict
+    "sample": {
+        "changed": true,
+        "diff": {
+            "after": {
+                "host_test": {
+                    "sriovActive": false,
+                    "sriovEnabled": true,
+                    "maxVirtualFunctionSupported": 63,
+                    "numVirtualFunction": 0,
+                    "numVirtualFunctionRequested": 8,
+                    "rebootRequired": true,
+                    "sriovCapable": true
+                }
+            },
+            "before": {
+                "host_test": {
+                    "sriovActive": false,
+                    "sriovEnabled": false,
+                    "maxVirtualFunctionSupported": 63,
+                    "numVirtualFunction": 0,
+                    "numVirtualFunctionRequested": 0,
+                    "rebootRequired": false,
+                    "sriovCapable": true
+                }
+            },
+            "changes": {
+                "host_test": {
+                    "numVirtualFunction": 8,
+                    "rebootRequired": true,
+                    "sriovEnabled": true
+                }
+            }
+        }
+"""
+
+
+try:
+    from pyVmomi import vim
+except ImportError:
+    pass
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.vmware import vmware_argument_spec, PyVmomi
+from ansible.module_utils._text import to_native
+from time import sleep
+
+
+class VmwareAdapterConfigManager(PyVmomi):
+    """Class to manage configured NTP servers"""
+
+    def __init__(self, module):
+        super(VmwareAdapterConfigManager, self).__init__(module)
+        cluster_name = self.params.get("cluster_name", None)
+        esxi_host_name = self.params.get("esxi_hostname", None)
+
+        self.vmnic = self.params.get("vmnic", None)
+        self.sriovOn = self.params.get("sriovOn", None)
+        self.numVirtFunc = self.params.get("numVirtFunc", None)
+
+        self.hosts = self.get_all_host_objs(
+            cluster_name=cluster_name, esxi_host_name=esxi_host_name
+        )
+        if not self.hosts:
+            self.module.fail_json(msg="Failed to find host system.")
+        # self.change_list = []
+        self.results = {"before": {}, "after": {}, "changes": {}}
+
+    def set_host_state(self):
+        """Check ESXi host configuration"""
+        change_list = []
+        changed = False
+        for host in self.hosts:
+            self.results["before"][host.name] = {}
+            self.results["after"][host.name] = {}
+            self.results["changes"][host.name] = {}
+            self.results["before"][host.name] = self._check_sriov(host)
+            self.results["changes"][host.name]["rebootRequired"] = self.results[
+                "before"
+            ][host.name]["rebootRequired"]
+
+            # check compatability
+            if not self.results["before"][host.name]["sriovCapable"]:
+                change_list.append(False)
+                self.results["changes"][host.name][
+                    "msg"
+                ] = "sriov not supported on host= %s, nic= %s" % (host.name, self.vmnic)
+                self.results["after"][host.name] = self._check_sriov(host)
+                continue
+
+            if (
+                self.results["before"][host.name]["maxVirtualFunctionSupported"]
+                < self.numVirtFunc
+            ):
+                change_list.append(False)
+                maxVirtualFun = self.results["before"][host.name][
+                    "maxVirtualFunctionSupported"
+                ]
+                self.results["changes"][host.name]["msg"] = (
+                    "maxVirtualFunctionSupported= %d on %s"
+                    % (maxVirtualFun, self.vmnic)
+                )
+                self.results["after"][host.name] = self._check_sriov(host)
+                continue
+
+            # check current settings
+            params_to_change = {"sriovEnabled": None, "numVirtualFunction": None}
+            if self.results["before"][host.name]["sriovEnabled"] != self.sriovOn:
+                params_to_change["sriovEnabled"] = self.sriovOn
+
+            if (
+                self.results["before"][host.name]["numVirtualFunction"]
+                != self.numVirtFunc
+            ):
+                params_to_change["numVirtualFunction"] = self.numVirtFunc
+
+            success = self._update_sriov(host, **params_to_change)
+            if success:
+                change_list.append(True)
+            else:
+                change_list.append(False)
+
+            self.results["after"][host.name] = self._check_sriov(host)
+            self.results["changes"][host.name]["rebootRequired"] = self.results[
+                "after"
+            ][host.name]["rebootRequired"]
+
+        if any(change_list):
+            changed = True
+        self.module.exit_json(changed=changed, diff=self.results)
+
+    def _check_sriov(self, host):
+        pnic_info = {}
+        pnic_info["rebootRequired"] = host.summary.rebootRequired
+        for pci_device in host.configManager.pciPassthruSystem.pciPassthruInfo:
+            if pci_device.id == self._getPciId(host):
+                try:
+                    if pci_device.sriovCapable:
+                        pnic_info["sriovCapable"] = True
+                        pnic_info["sriovEnabled"] = pci_device.sriovEnabled
+                        pnic_info["sriovActive"] = pci_device.sriovActive
+                        pnic_info["numVirtualFunction"] = pci_device.numVirtualFunction
+                        pnic_info[
+                            "numVirtualFunctionRequested"
+                        ] = pci_device.numVirtualFunctionRequested
+                        pnic_info[
+                            "maxVirtualFunctionSupported"
+                        ] = pci_device.maxVirtualFunctionSupported
+                    else:
+                        pnic_info["sriovCapable"] = False
+                except AttributeError:
+                    pnic_info["sriovCapable"] = False
+                break
+        return pnic_info
+
+    def _getPciId(self, host):
+        for pnic in host.config.network.pnic:
+            if pnic.device == self.vmnic:
+                return pnic.pci
+        self.module.fail_json(msg="No nic= %s on host= %s" % (self.vmnic, host.name))
+
+    def _update_sriov(self, host, sriovEnabled=None, numVirtualFunction=None):
+        if sriovEnabled is None and numVirtualFunction is None:
+            self.results["changes"][host.name][
+                "msg"
+            ] = "No any changes, already configured"
+            return False
+        nic_sriov = vim.host.SriovConfig()
+        nic_sriov.id = self._getPciId(host)
+        if sriovEnabled is not None:
+            nic_sriov.sriovEnabled = sriovEnabled
+            self.results["changes"][host.name]["sriovEnabled"] = sriovEnabled
+        if numVirtualFunction is not None and numVirtualFunction >= 0:
+            nic_sriov.numVirtualFunction = numVirtualFunction
+            self.results["changes"][host.name][
+                "numVirtualFunction"
+            ] = numVirtualFunction
+
+        try:
+            if not self.module.check_mode:
+                host.configManager.pciPassthruSystem.UpdatePassthruConfig([nic_sriov])
+                # looks only for refresh info
+                host.configManager.pciPassthruSystem.Refresh()
+                sleep(2)  # TODO: needed method to know that host updated info
+                return True
+            return False
+        except vim.fault.HostConfigFault as config_fault:
+            self.module.fail_json(
+                msg="Failed to configure SR-IOV for host= %s due to : %s"
+                % (host.name, to_native(config_fault.msg))
+            )
+            return False
+
+
+def main():
+    """Main"""
+    argument_spec = vmware_argument_spec()
+    argument_spec.update(
+        cluster_name=dict(type="str", required=False),
+        esxi_hostname=dict(type="str", required=False),
+        vmnic=dict(type="str", required=True),
+        sriovOn=dict(type="bool", required=True),
+        numVirtFunc=dict(type="int", required=True),
+    )
+
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        required_one_of=[["cluster_name", "esxi_hostname"], ["sriovOn", "numVirtFunc"]],
+        supports_check_mode=True,
+    )
+
+    vmware_host_adapter_config = VmwareAdapterConfigManager(module)
+    vmware_host_adapter_config.set_host_state()
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/units/modules/cloud/vmware/test_vmware_host_sriov.py
@@ -0,0 +1,194 @@
+import unittest
+from unittest import mock
+
+from ansible.modules.cloud.vmware import vmware_host_sriov
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.vmware import vmware_argument_spec, PyVmomi
+from ansible.module_utils._text import to_native
+
+
+mock_host = mock.Mock()
+mock_host.name = "test_host"
+tests = [
+    # 1. normal activate
+    {
+        "pnic_info": {
+            "sriovCapable": True,
+            "sriovEnabled": False,
+            "sriovActive": False,
+            "numVirtualFunction": 0,
+            "numVirtualFunctionRequested": 0,
+            "maxVirtualFunctionSupported": 64,
+            "rebootRequired": True,
+        },
+        "mock_attrs": {
+            "hostname": "test_vCenter",
+            "esxi_host_name": "test_host",
+            "sriovOn": True,
+            "numVirtFunc": 8,
+            "vmnic": "vmnic7",
+            "results": {"before": {}, "after": {}, "changes": {}},
+            "hosts": [mock_host],
+        },
+        "changes": {
+            "test_host": {
+                "rebootRequired": True,
+                "sriovEnabled": True,
+                "numVirtualFunction": 8,
+            }
+        },
+    },
+    # 2. already eanabled
+    {
+        "pnic_info": {
+            "sriovCapable": True,
+            "sriovEnabled": True,
+            "sriovActive": False,
+            "numVirtualFunction": 8,
+            "numVirtualFunctionRequested": 8,
+            "maxVirtualFunctionSupported": 64,
+            "rebootRequired": True,
+        },
+        "mock_attrs": {
+            "hostname": "test_vCenter",
+            "esxi_host_name": "test_host",
+            "sriovOn": True,
+            "numVirtFunc": 8,
+            "vmnic": "vmnic7",
+            "results": {"before": {}, "after": {}, "changes": {}},
+            "hosts": [mock_host],
+        },
+        "changes": {
+            "test_host": {
+                "rebootRequired": True,
+                "msg": "No any changes, already configured",
+            }
+        },
+    },
+    # 3. already active
+    {
+        "pnic_info": {
+            "sriovCapable": True,
+            "sriovEnabled": True,
+            "sriovActive": True,
+            "numVirtualFunction": 8,
+            "numVirtualFunctionRequested": 8,
+            "maxVirtualFunctionSupported": 64,
+            "rebootRequired": False,
+        },
+        "mock_attrs": {
+            "hostname": "test_vCenter",
+            "esxi_host_name": "test_host",
+            "sriovOn": True,
+            "numVirtFunc": 8,
+            "vmnic": "vmnic7",
+            "results": {"before": {}, "after": {}, "changes": {}},
+            "hosts": [mock_host],
+        },
+        "changes": {
+            "test_host": {
+                "rebootRequired": False,
+                "msg": "No any changes, already configured",
+            }
+        },
+    },
+    # 4. not suported numVirtFunc
+    {
+        "pnic_info": {
+            "sriovCapable": True,
+            "sriovEnabled": True,
+            "sriovActive": True,
+            "numVirtualFunction": 8,
+            "numVirtualFunctionRequested": 8,
+            "maxVirtualFunctionSupported": 64,
+            "rebootRequired": False,
+        },
+        "mock_attrs": {
+            "hostname": "test_vCenter",
+            "esxi_host_name": "test_host",
+            "sriovOn": True,
+            "numVirtFunc": 100,
+            "vmnic": "vmnic7",
+            "results": {"before": {}, "after": {}, "changes": {}},
+            "hosts": [mock_host],
+        },
+        "changes": {
+            "test_host": {
+                "rebootRequired": False,
+                "msg": "maxVirtualFunctionSupported= 64 on vmnic7",
+            }
+        },
+    },
+    # 5. not supported sriov
+    {
+        "pnic_info": {
+            "sriovCapable": False,
+            "sriovEnabled": False,
+            "sriovActive": False,
+            "numVirtualFunction": 0,
+            "numVirtualFunctionRequested": 0,
+            "maxVirtualFunctionSupported": 0,
+            "rebootRequired": False,
+        },
+        "mock_attrs": {
+            "hostname": "test_vCenter",
+            "esxi_host_name": "test_host",
+            "sriovOn": True,
+            "numVirtFunc": 8,
+            "vmnic": "vmnic7",
+            "results": {"before": {}, "after": {}, "changes": {}},
+            "hosts": [mock_host],
+        },
+        "changes": {
+            "test_host": {
+                "rebootRequired": False,
+                "msg": "sriov not supported on host= test_host, nic= vmnic7",
+            }
+        },
+    },
+]
+
+
+class TestAdapterMethods(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @mock.patch.object(
+        vmware_host_sriov.VmwareAdapterConfigManager,
+        "_getPciId",
+        return_value="0000:87:00.1",
+    )
+    @mock.patch.object(
+        vmware_host_sriov.VmwareAdapterConfigManager,
+        "_check_sriov",
+        # there is 2 call _check_sriov in test, need duplicate
+        side_effect=[i["pnic_info"] for i in tests for _ in range(2)],
+    )
+    @mock.patch.object(
+        vmware_host_sriov.VmwareAdapterConfigManager, "__init__", return_value=None
+    )
+    def test_check_host_state(self, mock__init__, mock__check_sriov, mock__getPciId):
+        check_sriov_count = 0
+        for case in tests:
+            config = vmware_host_sriov.VmwareAdapterConfigManager()
+            config.module = mock.Mock()
+            config.module.check_mode = False
+            config.__dict__.update(case["mock_attrs"])
+            config.set_host_state()
+            self.assertEqual(
+                config.results["changes"],
+                case["changes"],
+                'wrong "changes" in "result"',
+            )
+            check_sriov_count += 2
+            self.assertEqual(
+                mock__check_sriov.call_count,
+                check_sriov_count,
+                "mock__check_sriov called mor or less 2 times",
+            )
+
+if __name__ == "__main__":
+    unittest.main()
