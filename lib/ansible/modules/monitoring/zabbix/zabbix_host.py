30afa033b4efc4ee23add17e6901c0ef5e9de159
sky-joker <sky.jokerxx@gmail.com>
2019-10-21 19:12:10 +0900

    zabbix_host: fix https://github.com/ansible/ansible/issues/63449 (#63637)

--- lib/ansible/modules/monitoring/zabbix/zabbix_host.py
+++ lib/ansible/modules/monitoring/zabbix/zabbix_host.py
@@ -478,20 +478,20 @@ class Host(object):
         if interfaces is not None:
             if len(interfaces) >= 1:
                 for interface in interfaces:
-                    interfaces_port_list.append(int(interface['port']))
+                    interfaces_port_list.append(str(interface['port']))
 
         exist_interface_ports = []
         if len(exist_interface_list) >= 1:
             for exist_interface in exist_interface_list:
-                exist_interface_ports.append(int(exist_interface['port']))
+                exist_interface_ports.append(str(exist_interface['port']))
 
         if set(interfaces_port_list) != set(exist_interface_ports):
             return True
 
         for exist_interface in exist_interface_list:
-            exit_interface_port = int(exist_interface['port'])
+            exit_interface_port = str(exist_interface['port'])
             for interface in interfaces:
-                interface_port = int(interface['port'])
+                interface_port = str(interface['port'])
                 if interface_port == exit_interface_port:
                     for key in interface.keys():
                         if str(exist_interface[key]) != str(interface[key]):
