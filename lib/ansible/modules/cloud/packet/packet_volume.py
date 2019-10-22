a67f2468edfbe0435622d00f9cd5414e394fb2d8
Nurfet Becirevic <nurfet.becirevic@gmail.com>
2019-10-22 14:12:45 +0200

    Fix sanity tests, add check mode & few other minor changes

--- lib/ansible/modules/cloud/packet/packet_volume.py
+++ lib/ansible/modules/cloud/packet/packet_volume.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python
-# Copyright 2017 Tomas Karasek <tom.to.the.k@gmail.com>
-#
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2019, Nurfet Becirevic <nurfet.becirevic@gmail.com>
+# Copyright: (c) 2017, Tomas Karasek <tom.to.the.k@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
@@ -27,71 +16,105 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = '''
 ---
 module: packet_volume
+
 short_description: Create/delete a volume in Packet host.
+
 description:
      - Create/delete a volume in Packet host.
-     - API is documented at U(https://www.packet.net/developers/api/volumes).
-version_added: "2.4"
-author: "Tomas Karasek <tom.to.the.k@gmail.com>"
+     - API is documented at U(https://www.packet.com/developers/api/#volumes).
+
+version_added: "2.10"
+
+author:
+    - Tomas Karasek (@t0mk) <tom.to.the.k@gmail.com>
+    - Nurfet Becirevic (@nurfet-becirevic) <nurfet.becirevic@gmail.com>
+
 options:
   state:
     description:
-     - Ddesired state of the volume.
+      - Desired state of the volume.
     default: present
     choices: ['present', 'absent']
+    type: str
+
   project_id:
     description:
       - ID of project of the device.
     required: true
+    type: str
+
   auth_token:
     description:
-     - Packet api token. You can also supply it in env var C(PACKET_API_TOKEN).
+      - Packet api token. You can also supply it in env var C(PACKET_API_TOKEN).
+    type: str
+
   name:
     description:
-     - Selector for API-generated name of the volume
+      - Selector for API-generated name of the volume
+    type: str
+
   description:
     description:
-     - User-defined description attribute for Packet volume.
-     - "It is used used as idempotent identifier - if volume with given
-       description exists, new one is not created."
+      - User-defined description attribute for Packet volume.
+      - "It is used used as idempotent identifier - if volume with given
+        description exists, new one is not created."
+    type: str
+
   id:
     description:
-     - UUID of a volume.
+      - UUID of a volume.
+    type: str
+
   plan:
     description:
-     - storage_1 for standard tier, storage_2 for premium tier.
-     - Tiers are described at U(https://www.packet.net/bare-metal/services/storage/).
-    choices: [storage_1, storage_2]
-    default: storage_1
+      - storage_1 for standard tier, storage_2 for premium (performance) tier.
+      - Tiers are described at U(https://www.packet.com/cloud/storage/).
+    choices: ['storage_1', 'storage_2']
+    default: 'storage_1'
+    type: str
+
   facility:
     description:
-     - Location of the volume.
-     - Volumes can only be attached to device in the same location.
+      - Location of the volume.
+      - Volumes can only be attached to device in the same location.
+    type: str
+
   size:
     description:
-     - Size of the volume in gigabytes.
+      - Size of the volume in gigabytes.
+    type: int
+
   locked:
     description:
      - Create new volume locked.
     type: bool
     default: False
+
   billing_cycle:
     description:
-     - Billing cycle for new volume.
-    choices: [hourly, monthly]
+      - Billing cycle for new volume.
+    choices: ['hourly', 'monthly']
+    default: 'hourly'
+    type: str
+
   snapshot_policy:
     description:
       - Snapshot policy for new volume.
+    type: dict
+
     suboptions:
       snapshot_count:
         description:
-          - How many snaphots to keep, a postivie integer.
+          - How many snapshots to keep, a positive integer.
         required: True
+        type: int
+
       snapshot_frequency:
         description:
           - Frequency of snapshots.
         required: True
         choices: ["15min", "1hour", "1day", "1week", "1month", "1year"]
+        type: str
 
 requirements:
   - "python >= 2.6"
@@ -132,25 +155,25 @@ EXAMPLES = '''
 RETURN = '''
 id:
     description: UUID of specified volume
-    type: string
+    type: str
     returned: success
     sample: 53000fb2-ee46-4673-93a8-de2c2bdba33c
 name:
     description: The API-generated name of the volume resource.
-    type: string
+    type: str
     returned: if volume is attached/detached to/from some device
     sample: "volume-a91dc506"
 description:
     description: The user-defined description of the volume resource.
-    type: string
+    type: str
     returned: success
     sample: "Just another volume"
-'''  # NOQA
+'''
 
-import os
 import uuid
 
-from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.basic import AnsibleModule, env_fallback
+from ansible.module_utils._text import to_native
 
 HAS_PACKET_SDK = True
 
@@ -163,6 +186,7 @@ except ImportError:
 
 PACKET_API_TOKEN_ENV_VAR = "PACKET_API_TOKEN"
 
+VOLUME_PLANS = ["storage_1", "storage_2"]
 VOLUME_STATES = ["present", "absent"]
 BILLING = ["hourly", "monthly"]
 
@@ -179,7 +203,7 @@ def get_volume_selector(module):
     if module.params.get('id'):
         i = module.params.get('id')
         if not is_valid_uuid(i):
-            raise Exception("Volume ID '%s' is not a valid UUID" % i)
+            raise Exception("Volume ID '{0}' is not a valid UUID".format(i))
         return lambda v: v['id'] == i
     elif module.params.get('name'):
         n = module.params.get('name')
@@ -192,7 +216,7 @@ def get_volume_selector(module):
 def get_or_fail(params, key):
     item = params.get(key)
     if item is None:
-        raise Exception("%s must be specified for new volume" % key)
+        raise Exception("{0} must be specified for new volume".format(key))
     return item
 
 
@@ -200,7 +224,7 @@ def act_on_volume(target_state, module, packet_conn):
     return_dict = {'changed': False}
     s = get_volume_selector(module)
     project_id = module.params.get("project_id")
-    api_method = "projects/%s/storage" % project_id
+    api_method = "projects/{0}/storage".format(project_id)
     all_volumes = packet_conn.call_api(api_method, "GET")['volumes']
     matching_volumes = [v for v in all_volumes if s(v)]
 
@@ -227,15 +251,17 @@ def act_on_volume(target_state, module, packet_conn):
 
     else:
         if len(matching_volumes) > 1:
-            _msg = ("More than one volume matches in module call for absent state: %s"
-                    % matching_volumes)
-            raise Exception(_msg)
+            _msg = ("More than one volume matches in module call for absent state: {0}".format(
+                    to_native(matching_volumes)))
+            module.fail_json(msg=_msg)
+
         if len(matching_volumes) == 1:
             volume = matching_volumes[0]
-            packet_conn.call_api("storage/%s" % volume['id'], "DELETE")
+            packet_conn.call_api("storage/{0}".format(volume['id']), "DELETE")
             return_dict['changed'] = True
             for k in ['id', 'name', 'description']:
                 return_dict[k] = volume[k]
+
     return return_dict
 
 
@@ -246,16 +272,20 @@ def main():
             description=dict(type="str", default=None),
             name=dict(type='str', default=None),
             state=dict(choices=VOLUME_STATES, default="present"),
-            auth_token=dict(default=os.environ.get(PACKET_API_TOKEN_ENV_VAR),
-                            no_log=True),
+            auth_token=dict(
+                type='str',
+                fallback=(env_fallback, [PACKET_API_TOKEN_ENV_VAR]),
+                no_log=True
+            ),
             project_id=dict(required=True),
-            plan=dict(type="str"),
+            plan=dict(choices=VOLUME_PLANS, default="storage_1"),
             facility=dict(type="str"),
             size=dict(type="int"),
             locked=dict(type="bool", default=False),
             snapshot_policy=dict(type='dict', default=None),
             billing_cycle=dict(type='str', choices=BILLING, default="hourly"),
         ),
+        supports_check_mode=True,
         required_one_of=[("name", "id", "description")],
         mutually_exclusive=[
             ('name', 'id'),
@@ -268,9 +298,8 @@ def main():
         module.fail_json(msg='packet required for this module')
 
     if not module.params.get('auth_token'):
-        _fail_msg = ("if Packet API token is not in environment variable %s, "
-                     "the auth_token parameter is required" %
-                     PACKET_API_TOKEN_ENV_VAR)
+        _fail_msg = ("if Packet API token is not in environment variable {0}, "
+                     "the auth_token parameter is required".format(PACKET_API_TOKEN_ENV_VAR))
         module.fail_json(msg=_fail_msg)
 
     auth_token = module.params.get('auth_token')
@@ -280,14 +309,17 @@ def main():
     state = module.params.get('state')
 
     if state in VOLUME_STATES:
+        if module.check_mode:
+            module.exit_json(changed=False)
+
         try:
             module.exit_json(**act_on_volume(state, module, packet_conn))
         except Exception as e:
             module.fail_json(
-                msg='failed to set volume state %s: %s' %
-                (state, e))
+                msg="failed to set volume state {0}: {1}".format(
+                    state, to_native(e)))
     else:
-        module.fail_json(msg='%s is not a valid state for this module' % state)
+        module.fail_json(msg="{0} is not a valid state for this module".format(state))
 
 
 if __name__ == '__main__':
