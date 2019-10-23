4dd4e333b37775e9447fe2b86ead76348a7284d5
Martin Krizek <martin.krizek@gmail.com>
2019-10-23 11:24:45 +0200

    Update ansible_native_concat
    Fixes #58527
    
    Updates our copy of native_concat to include changes introduced in
    https://github.com/pallets/jinja/pull/1086.

--- lib/ansible/template/native_helpers.py
+++ lib/ansible/template/native_helpers.py
@@ -11,34 +11,39 @@ from itertools import islice, chain
 import types
 
 from jinja2._compat import text_type
-
 from jinja2.runtime import StrictUndefined
+
+from ansible.module_utils.six import string_types
 from ansible.parsing.yaml.objects import AnsibleVaultEncryptedUnicode
 
 
-def ansible_native_concat(nodes):
-    """Return a native Python type from the list of compiled nodes. If the
-    result is a single node, its value is returned. Otherwise, the nodes are
-    concatenated as strings. If the result can be parsed with
-    :func:`ast.literal_eval`, the parsed value is returned. Otherwise, the
-    string is returned.
+# NOTE: A copy of https://github.com/pallets/jinja/blob/master/jinja2/nativetypes.py
+# which handles Ansible related use cases.
+def ansible_native_concat(nodes, preserve_quotes=True):
+    """Return a native Python type from the list of compiled nodes. If
+    the result is a single node, its value is returned. Otherwise, the
+    nodes are concatenated as strings. If the result can be parsed with
+    :func:`ast.literal_eval`, the parsed value is returned. Otherwise,
+    the string is returned.
+
+    :param nodes: Iterable of nodes to concatenate.
+    :param preserve_quotes: Whether to re-wrap literal strings with
+        quotes, to preserve quotes around expressions for later parsing.
+        Should be ``False`` in :meth:`NativeEnvironment.render`.
     """
-
-    # https://github.com/pallets/jinja/blob/master/jinja2/nativetypes.py
-
     head = list(islice(nodes, 2))
 
     if not head:
         return None
 
     if len(head) == 1:
-        out = head[0]
+        raw = head[0]
 
         # TODO send unvaulted data to literal_eval?
-        if isinstance(out, AnsibleVaultEncryptedUnicode):
-            return out.data
+        if isinstance(raw, AnsibleVaultEncryptedUnicode):
+            return raw.data
 
-        if isinstance(out, StrictUndefined):
+        if isinstance(raw, StrictUndefined):
             # A hack to raise proper UndefinedError/AnsibleUndefinedVariable exception.
             # We need to access the AnsibleUndefined(StrictUndefined) object by either of the following:
             # __iter__, __str__, __len__, __nonzero__, __eq__, __ne__, __bool__, __hash__
@@ -47,19 +52,28 @@ def ansible_native_concat(nodes):
             # Otherwise the undefined error would be raised on the next access which might not be properly handled.
             # See https://github.com/ansible/ansible/issues/52158
             # We do that only here because it is taken care of by text_type() in the else block below already.
-            str(out)
+            str(raw)
 
         # short circuit literal_eval when possible
-        if not isinstance(out, list):
-            return out
+        if not isinstance(raw, list):
+            return raw
     else:
         if isinstance(nodes, types.GeneratorType):
             nodes = chain(head, nodes)
         # Stringifying the nodes is important as it takes care of
         # StrictUndefined by side-effect - by raising an exception.
-        out = u''.join([text_type(v) for v in nodes])
+        raw = u''.join([text_type(v) for v in nodes])
 
     try:
-        return literal_eval(out)
+        literal = literal_eval(raw)
     except (ValueError, SyntaxError, MemoryError):
-        return out
+        return raw
+
+    # If literal_eval returned a string, re-wrap with the original
+    # quote character to avoid dropping quotes between expression nodes.
+    # Without this, "'{{ a }}', '{{ b }}'" results in "a, b", but should
+    # be ('a', 'b').
+    if preserve_quotes and isinstance(literal, string_types):
+        return "{quote}{}{quote}".format(literal, quote=raw[0])
+
+    return literal
