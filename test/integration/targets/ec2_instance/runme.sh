11ae14be9cd7a19f3e2dd90a69dd30d5fa9a4e2b
Mark Chappell <mchappel@redhat.com>
2019-10-23 12:23:50 +0200

    Add a warning about running in parallel

--- test/integration/targets/ec2_instance/runme.sh
+++ test/integration/targets/ec2_instance/runme.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+#
+# Beware: most of our tests here are run in parallel.
+# To add new tests you'll need to add a new host to the inventory and a matching
+# '{{ inventory_hostname }}'.yml file in roles/ec2_instance/tasks/
+
 
 set -eux
 
