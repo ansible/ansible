b816e70d4a95f1c55245cf80e5863f90164f7470
The Magician <magic-modules@google.com>
2019-10-23 16:57:15 -0700

    Bug fixes for GCP modules (#63832)

--- lib/ansible/modules/cloud/google/gcp_compute_global_address.py
+++ lib/ansible/modules/cloud/google/gcp_compute_global_address.py
@@ -157,9 +157,9 @@ options:
 notes:
 - 'API Reference: U(https://cloud.google.com/compute/docs/reference/v1/globalAddresses)'
 - 'Reserving a Static External IP Address: U(https://cloud.google.com/compute/docs/ip-addresses/reserve-static-external-ip-address)'
-- for authentication, you can set service_account_file using the c(gcp_service_account_file)
+- for authentication, you can set service_account_file using the C(gcp_service_account_file)
   env variable.
-- for authentication, you can set service_account_contents using the c(GCP_SERVICE_ACCOUNT_CONTENTS)
+- for authentication, you can set service_account_contents using the C(GCP_SERVICE_ACCOUNT_CONTENTS)
   env variable.
 - For authentication, you can set service_account_email using the C(GCP_SERVICE_ACCOUNT_EMAIL)
   env variable.
