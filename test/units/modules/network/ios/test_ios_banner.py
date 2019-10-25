60672d2faa47e901dcc18ba7adf9b97af7e27d59
kalimsshar <34369784+kalimsshar@users.noreply.github.com>
2019-10-25 20:20:00 +0200

    Make banner detection non-greedy in ios_banner (#63092)
    * Make banner detection non-greedy in ios_banner
    
    * Added ios_banner unit test to detect bug #63091
    
    * Corrected PEP8 errors
    
    * Added integration test
    
    * Corrected typo in integration test
    
    (cherry picked from commit 01a92f0191de904ca8351cbfd1516e1aee874372)

--- test/units/modules/network/ios/test_ios_banner.py
+++ test/units/modules/network/ios/test_ios_banner.py
@@ -60,3 +60,18 @@ class TestIosBannerModule(TestIosModule):
         banner_text = load_fixture('ios_banner_show_banner.txt').strip()
         set_module_args(dict(banner='login', text=banner_text))
         self.execute_module()
+
+
+class TestIosBannerIos12Module(TestIosBannerModule):
+
+    def load_fixtures(self, commands):
+        show_banner_return_value = (1, '', None)
+        show_running_config_return_value = \
+            (0, load_fixture('ios_banner_show_running_config_ios12.txt').strip(), None)
+        self.exec_command.side_effect = [show_banner_return_value,
+                                         show_running_config_return_value]
+
+    def test_ios_banner_nochange(self):
+        banner_text = load_fixture('ios_banner_show_banner.txt').strip()
+        set_module_args(dict(banner='exec', text=banner_text))
+        self.execute_module()
