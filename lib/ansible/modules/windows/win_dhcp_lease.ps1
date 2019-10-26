b359c25dc7223fabe5a3056df2dc7886a0370886
Joe Zollo <jzollo@vmware.com>
2019-10-26 17:10:40 -0400

    Rename Module & Tests, Refactor Module

---
+++ lib/ansible/modules/windows/win_dhcp_lease.ps1
@@ -0,0 +1,479 @@
+#!powershell
+
+# Copyright: (c) 2019 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: GPL-3.0-only
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#AnsibleRequires -CSharpUtil Ansible.Basic
+#Requires -Module Ansible.ModuleUtils.CamelConversion
+#Requires -Module Ansible.ModuleUtils.FileUtil
+#Requires -Module Ansible.ModuleUtils.Legacy
+
+$ErrorActionPreference = "Stop"
+
+Function Convert-MacAddress {
+    Param(
+        [string]$mac
+    )
+
+    # Evaluate Length
+    if ($mac.Length -eq 12) {
+        # Insert Dashes
+        $mac = $mac.Insert(2, "-").Insert(5, "-").Insert(8, "-").Insert(11, "-").Insert(14, "-")
+        return $mac
+    }
+    elseif ($mac.Length -eq 17) {
+        # Remove Dashes
+        $mac = $mac -replace '-'
+        return $mac
+    }
+    else {
+        return $false
+    }
+}
+
+Function Compare-DhcpLease {
+    Param(
+        [PSObject]$Original,
+        [PSObject]$Updated
+    )
+
+    # Compare values that we care about
+    if (($Original.AddressState -eq $Updated.AddressState) -and ($Original.IPAddress -eq $Updated.IPAddress) -and ($Original.ScopeId -eq $Updated.ScopeId) -and ($Original.Name -eq $Updated.Name) -and ($Original.Description -eq $Updated.Description)) {
+        # changed = false
+        return $false
+    }
+    else {
+        # changed = true
+        return $true
+    }
+}
+
+Function Convert-ReturnValue {
+    Param(
+        $Object
+    )
+
+    # TODO: Use camelconversion here instead of manual
+
+    $data = @{
+        address_state = $Object.AddressState
+        client_id     = $Object.ClientId
+        ip_address    = $Object.IPAddress.IPAddressToString
+        scope_id      = $Object.ScopeId.IPAddressToString
+        name          = $Object.Name
+        description   = $Object.Description
+    }
+
+    return $data
+}
+
+$spec = @{
+    options = @{
+        type = @{ type = "str"; choices = "reservation", "lease"; }
+        ip = @{ type = "str" }
+        scope_id = @{ type = "str" }
+        mac = @{ type = "str" }
+        duration = @{ type = "int" }
+        dns_hostname = @{ type = "str"; }
+        dns_regtype = @{ type = "str"; choices = "aptr", "a", "noreg"; default = "aptr" }
+        reservation_name = @{ type = "str"; }
+        description = @{ type = "str"; }
+        state = @{ type = "str"; choices = "absent", "present"; default = "present" }
+    }
+    supports_check_mode = $true
+}
+
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
+$module.Result.changed = $false
+$check_mode = $module.CheckMode
+
+$type = $module.Params.type
+$ip = $module.Params.ip
+$scope_id = $module.Params.scope_id
+$mac = $module.Params.mac
+$duration = $module.Params.duration
+$dns_hostname = $module.Params.dns_hostname
+$dns_regtype = $module.Params.dns_regtype
+$reservation_name = $module.Params.reservation_name
+$description = $module.Params.description
+$state = $module.Params.state
+
+# Parse Regtype
+if ($dns_regtype) {
+    Switch ($dns_regtype) {
+        "aptr" { $dns_regtype = "AandPTR"; break }
+        "a" { $dns_regtype = "A"; break }
+        "noreg" { $dns_regtype = "NoRegistration"; break }
+        default { $dns_regtype = "NoRegistration"; break }
+    }
+}
+
+Try {
+    # Import DHCP Server PS Module
+    Import-Module DhcpServer
+}
+Catch {
+    # Couldn't load the DhcpServer Module
+    $module.FailJson("The DhcpServer module failed to load properly")
+}
+
+# Determine if there is an existing lease
+if ($ip) {
+    $current_lease = Get-DhcpServerv4Scope | Get-DhcpServerv4Lease | Where-Object IPAddress -eq $ip
+}
+
+# MacAddress was specified
+if ($mac) {
+    if ($mac -like "*-*") {
+        $mac_original = $mac
+        $mac = Convert-MacAddress -mac $mac
+    }
+
+    if ($mac -eq $false) {
+        $module.FailJson("The MAC Address is not properly formatted")
+    }
+    else {
+        $current_lease = Get-DhcpServerv4Scope | Get-DhcpServerv4Lease | Where-Object ClientId -eq $mac_original
+    }
+}
+
+# Did we find a lease/reservation
+if ($current_lease) {
+    $current_lease_exists = $true
+    $original_lease = $current_lease
+    $module.Diff.before = Convert-ReturnValue -Object $original_lease
+}
+else {
+    $current_lease_exists = $false
+}
+
+# If we found a lease, is it a reservation?
+if ($current_lease_exists -eq $true -and ($current_lease.AddressState -like "*Reservation*")) {
+    $current_lease_reservation = $true
+}
+else {
+    $current_lease_reservation = $false
+}
+
+# State: Absent
+# Ensure the DHCP Lease/Reservation is not present
+if ($state -eq "absent") {
+
+    # Required: MAC or IP address
+    if ((-not $mac) -and (-not $ip)) {
+        $module.Result.changed = $false
+        $module.FailJson("The ip or mac parameter is required for state=absent")
+    }
+
+    # If the lease exists, we need to destroy it
+    if ($current_lease_reservation -eq $true) {
+
+        # Try to remove reservation
+        Try {
+            $current_lease | Remove-DhcpServerv4Reservation -WhatIf:$check_mode
+            $state_absent_removed = $true
+        }
+        Catch {
+            $state_absent_removed = $false
+        }
+    }
+    else {
+
+        # Try to remove lease
+        Try {
+            $current_lease | Remove-DhcpServerv4Lease -WhatIf:$check_mode
+            $state_absent_removed = $true
+        }
+        Catch {
+            $state_absent_removed = $false
+        }
+    }
+
+    # If the lease doesn't exist, our work here is done
+    if ($current_lease_exists -eq $false) {
+        $module.Result.changed = $false
+        $module.Result.msg = "The lease or reservation doesn't exist."
+        $module.ExitJson()
+    }
+
+    # See if we removed the lease/reservation
+    if ($state_absent_removed) {
+        $module.Result.changed = $true
+        $module.ExitJson()
+    }
+    else {
+        $module.Result.lease = Convert-ReturnValue -Object $current_lease
+        $module.FailJson("Unable to remove lease/reservation")
+    }
+}
+
+# State: Present
+# Ensure the DHCP Lease/Reservation is present, and consistent
+if ($state -eq "present") {
+
+    # Current lease exists, and is not a reservation
+    if (($current_lease_reservation -eq $false) -and ($current_lease_exists -eq $true)) {
+        if ($type -eq "reservation") {
+            Try {
+                # Update parameters
+                $params = @{ }
+
+                if ($mac) {
+                    $params.ClientId = $mac
+                } else {
+                    $params.ClientId = $current_lease.ClientId
+                }
+
+                if ($description) {
+                    $params.Description = $description
+                } else {
+                    $params.Description = $current_lease.Description
+                }
+
+                if ($reservation_name) {
+                    $params.Name = $reservation_name
+                } else {
+                    $params.Name = "reservation-" + $params.ClientId
+                }
+
+                # Desired type is reservation
+                $current_lease | Add-DhcpServerv4Reservation -WhatIf:$check_mode
+
+                if(-not $check_mode) {
+                    $current_reservation = Get-DhcpServerv4Lease -ClientId $params.ClientId -ScopeId $current_lease.ScopeId
+                }
+
+                # Update the reservation with new values
+                $current_reservation | Set-DhcpServerv4Reservation @params -WhatIf:$check_mode
+
+                if(-not $check_mode) {
+                    $updated_reservation = Get-DhcpServerv4Lease -ClientId $params.ClientId -ScopeId $current_reservation.ScopeId
+                }
+
+                if(-not $check_mode) {
+                    # Compare Values
+                    $module.Result.changed = Compare-DhcpLease -Original $original_lease -Updated $updated_reservation
+                    $module.Result.lease = Convert-ReturnValue -Object $updated_reservation
+                } else {
+                    $module.Result.changed = $true
+                }
+
+                $module.ExitJson()
+            }
+            Catch {
+                $module.Result.changed = $false
+                $module.FailJson("Could not convert lease to a reservation")
+            }
+        }
+
+        # Nothing needs to be done, already in the desired state
+        if ($type -eq "lease") {
+            $module.Result.changed = $true
+            $module.msg = "The lease is already in it's desired state"
+            $module.ExitJson()
+        }
+    }
+
+    # Current lease exists, and is a reservation
+    if (($current_lease_reservation -eq $true) -and ($current_lease_exists -eq $true)) {
+        if ($type -eq "lease") {
+            Try {
+                # Desired type is a lease, remove the reservation
+                $current_lease | Remove-DhcpServerv4Reservation -WhatIf:$check_mode
+
+                # Build a new lease object with remnants of the reservation
+                $lease_params = @{
+                    ClientId = $original_lease.ClientId
+                    IPAddress = $original_lease.IPAddress.IPAddressToString
+                    ScopeId = $original_lease.ScopeId.IPAddressToString
+                    HostName = $original_lease.HostName
+                    AddressState = 'Active'
+                }
+
+                # Create new lease
+                Try {
+                    Add-DhcpServerv4Lease @lease_params -WhatIf:$check_mode
+                }
+                Catch {
+                    $module.Result.changed = $false
+                    $module.FailJson("Unable to convert the reservation to a lease")
+                }
+
+                # Get the lease we just created
+                if(-not $check_mode) {
+                    Try {
+                        $new_lease = Get-DhcpServerv4Lease -ClientId $lease_params.ClientId -ScopeId $lease_params.ScopeId
+                    }
+                    Catch {
+                        $module.Result.changed = $false
+                        $module.FailJson("Unable to retreive the newly created lease")
+                    }
+                }
+
+                if(-not $check_mode) {
+                    $module.Result.lease = Convert-ReturnValue -Object $new_lease
+                }
+
+                $module.Result.changed = $true
+                $module.ExitJson()
+            }
+            Catch {
+                $module.Result.changed = $false
+                $module.FailJson("Could not convert reservation to lease")
+            }
+        }
+
+        # Already in the desired state
+        if ($type -eq "reservation") {
+
+            # Update parameters
+            $params = @{ }
+
+            if ($mac) {
+                $params.ClientId = $mac
+            } else {
+                $params.ClientId = $current_lease.ClientId
+            }
+
+            if ($description) {
+                $params.Description = $description
+            } else {
+                $params.Description = $current_lease.Description
+            }
+
+            if ($reservation_name) {
+                $params.Name = $reservation_name
+            } else {
+                # Original lease had a null name so let's generate one
+                if ($null -eq $original_lease.Name) {
+                    $params.Name = "reservation-" + $original_lease.ClientId
+                } else {
+                    $params.Name = $original_lease.Name
+                }
+            }
+
+            # Update the reservation with new values
+            $current_lease | Set-DhcpServerv4Reservation @params -WhatIf:$check_mode
+
+            if(-not $check_mode) {
+                $reservation = Get-DhcpServerv4Lease -ClientId $current_lease.ClientId -ScopeId $current_lease.ScopeId
+                $module.Result.changed = Compare-DhcpLease -Original $original_lease -Updated $reservation
+                $module.Result.lease = Convert-ReturnValue -Object $reservation
+            } else {
+                $module.Result.changed = $true
+            }
+
+            # Return values
+            $module.ExitJson()
+        }
+    }
+
+    # Lease Doesn't Exist - Create
+    if ($current_lease_exists -eq $false) {
+
+        # Required: MAC and IP address
+        if ((-not $mac) -or (-not $ip)) {
+            $module.Result.changed = $false
+            $module.FailJson("The ip and mac parameters are required for state=present")
+        }
+
+        # Required: Scope ID
+        if (-not $scope_id) {
+            $module.Result.changed = $false
+            $module.FailJson("The scope_id parameter is required for state=present")
+        }
+
+        # Required Parameters
+        $lease_params = @{
+            ClientId = $mac
+            IPAddress = $ip
+            ScopeId = $scope_id
+            AddressState = 'Active'
+            Confirm = $false
+        }
+
+        if ($duration) {
+            $lease_params.LeaseExpiryTime = (Get-Date).AddDays($duration)
+        }
+
+        if ($dns_hostname) {
+            $lease_params.HostName = $dns_hostname
+        }
+
+        if ($dns_regtype) {
+            $lease_params.DnsRR = $dns_regtype
+        }
+
+        if ($description) {
+            $lease_params.Description = $description
+        }
+
+        # Create Lease
+        Try {
+            # Create lease based on parameters
+            Add-DhcpServerv4Lease @lease_params -WhatIf:$check_mode
+
+            # Retreive the lease
+            if(-not $check_mode) {
+                $new_lease = Get-DhcpServerv4Lease -ClientId $mac -ScopeId $scope_id
+                $module.Result.lease = Convert-ReturnValue -Object $new_lease
+            }
+
+            # If lease is the desired type
+            if ($type -eq "lease") {
+                $module.Result.changed = $true
+                $module.ExitJson()
+            }
+        }
+        Catch {
+            # Failed to create lease
+            $module.Result.changed = $false
+            $module.FailJson("Could not create DHCP lease")
+        }
+
+        # Create Reservation
+        Try {
+            # If reservation is the desired type
+            if ($type -eq "reservation") {
+                if ($reservation_name) {
+                    $lease_params.Name = $reservation_name
+                }
+                else {
+                    $lease_params.Name = "reservation-" + $mac
+                }
+
+                Try {
+                    if($check_mode) {
+                        # In check mode, a lease won't exist for conversion, make one manually
+                        Add-DhcpServerv4Reservation -ScopeId $scope_id -ClientId $mac_original -IPAddress $ip -WhatIf:$check_mode
+                    } else {
+                        # Convert to Reservation
+                        $new_lease | Add-DhcpServerv4Reservation -WhatIf:$check_mode
+                    }
+                }
+                Catch {
+                    # Failed to create reservation
+                    $module.Result.changed = $false
+                    $module.FailJson("Could not create DHCP reservation")
+                }
+
+                if(-not $check_mode) {
+                    # Get DHCP reservation object
+                    $new_lease = Get-DhcpServerv4Reservation -ClientId $mac_original -ScopeId $scope_id
+                    $module.Result.lease = Convert-ReturnValue -Object $new_lease
+                }
+
+                $module.Result.changed = $true
+                $module.ExitJson()
+            }
+        }
+        Catch {
+            # Failed to create reservation
+            $module.Result.changed = $false
+            $module.FailJson("Could not create DHCP reservation")
+        }
+    }
+}
+
+$module.ExitJson()
---
+++ lib/ansible/modules/windows/win_dhcp_lease.py
@@ -0,0 +1,143 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2019 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: GPL-3.0-only
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+ANSIBLE_METADATA = {'status': ['preview'],
+                    'supported_by': 'community',
+                    'metadata_version': '1.1'}
+
+DOCUMENTATION = r'''
+---
+module: win_dhcp_lease
+version_added: '2.10'
+short_description: Manages IP Addresses on a Windows DHCP Server
+author: Joe Zollo (@joezollo)
+requirements:
+  - This module requires Windows Server 2012 or Newer
+description:
+  - Manages IP Addresses on a Windows DHCP Server
+  - Adds or Removes DHCP Leases and Reservations
+  - Task should be delegated to a Windows DHCP Server
+options:
+  type:
+    description:
+      - The type of DHCP address
+    type: str
+    default: reservation
+    choices: [ reservation, lease ]
+  state:
+    description:
+      - Specifies the desired state of the DHCP lease or reservation
+    type: str
+    choices: [ present, absent ]
+  ip:
+    description:
+      - The IP address of the client server/computer
+    type: str
+    required: yes
+  scope_id:
+    description:
+      - Specifies the scope identifier
+      - Required when creating a new DHCP reservation/lease
+    type: str
+  mac:
+    description:
+      - Specifies the client identifier to be set on the IPv4 address
+      - Windows clients use the MAC address as the client ID
+      - Linux and other operating systems can use other types of identifiers
+    type: str
+  duration:
+    description:
+      - Specifies the duration of the DHCP lease in days
+      - The duration value only applies to l(type=lease)
+      - Defaults to the duration specified by the DHCP server
+        configuration
+    type: int
+  dns_hostname:
+    description:
+      - Specifies the DNS hostname of the client for which the IP address
+        lease is to be added
+  dns_regtype:
+    description:
+      - Indicates the type of DNS record to be registered by the DHCP
+        server service for this lease
+      - Defaults to the type specified by the DHCP server configuration
+    type: str
+    choices: [ aptr, a, noreg ]
+  reservation_name:
+    description:
+      - Specifies the name of the reservation being created
+      - Only applicable to l(type=reservation)
+    type: str
+  description:
+    description:
+      - Specifies the description for reservation being created
+      - Only applicable to l(type=reservation)
+    type: str
+'''
+
+EXAMPLES = r'''
+- name: Ensure DHCP reservation exists
+  win_dhcp_lease:
+    type: reservation
+    ip: 192.168.100.205
+    scope_id: 192.168.100.0
+    mac: 00b18ad15a1f
+    dns_hostname: "{{ ansible_inventory }}"
+    description: Testing Server
+  delegate_to: dhcp-ericb-euc.vmware.com
+
+- name: Ensure DHCP lease or reservation does not exist
+  win_dhcp_lease:
+    mac: 00b18ad15a1f
+    state: absent
+  delegate_to: dhcp-marshallb-euc.vmware.com
+
+- name: Ensure DHCP lease or reservation does not exist
+  win_dhcp_lease:
+    ip: 192.168.100.205
+    state: absent
+  delegate_to: dhcp-willp-euc.vmware.com
+
+- name: Convert DHCP lease to reservation & update description
+  win_dhcp_lease:
+    type: reservation
+    ip: 192.168.100.205
+    description: Testing Server
+  delegate_to: dhcp-danielg-euc.vmware.com
+
+- name: Convert DHCP reservation to lease
+  win_dhcp_lease:
+    type: lease
+    ip: 192.168.100.205
+  delegate_to: dhcp-devangm-euc.vmware.com
+'''
+
+RETURN = r'''
+lease:
+  description: New/Updated DHCP object parameters
+  returned: When l(state=present)
+  type: dict
+  sample:
+    address_state: InactiveReservation
+    client_id: 0a-0b-0c-04-05-aa
+    description: Really Fancy
+    ip_address: 172.16.98.230
+    name: null
+    scope_id: 172.16.98.0
+
+original:
+  description: Original DHCP object parameters
+  returned: When an existing lease is found
+  type: dict
+  sample:
+    address_state: InactiveReservation
+    client_id: 0a-0b-0c-04-05-aa
+    description: Really Fancy
+    ip_address: 172.16.98.230
+    name: null
+    scope_id: 172.16.98.0
+'''
