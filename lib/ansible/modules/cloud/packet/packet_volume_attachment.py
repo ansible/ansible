8c805530c15becc08f963b0032a6a8fbba0c1e74
Nurfet Becirevic <nurfet.becirevic@gmail.com>
2019-10-30 20:41:00 +0100

    Fix sanity tests, add check mode & few other minor changes

--- lib/ansible/modules/cloud/packet/packet_volume_attachment.py
+++ lib/ansible/modules/cloud/packet/packet_volume_attachment.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python
-# Copyright 2017 Tomas Karasek <tom.to.the.k@gmail.com>
-#
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2019, Nurfet Becirevic <nurfet.becirevic@gmail.com>
+# Copyright: (c) 2017, Tomas Karasek <tom.to.the.k@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 
-ANSIBLE_METADATA = {'metadata_version': '1.0',
+ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
@@ -28,37 +17,53 @@ ANSIBLE_METADATA = {'metadata_version': '1.0',
 DOCUMENTATION = '''
 ---
 module: packet_volume_attachment
+
 short_description: Attach/detach a volume to a device in the Packet host.
+
 description:
      - Attach/detach a volume to a device in the Packet host.
      - API is documented at U(https://www.packet.net/developers/api/volumeattachments/).
      - "This module creates the attachment route in the Packet API. In order to discover
        the block devices on the server, you have to run the Attach Scripts,
        as documented at U(https://help.packet.net/technical/storage/packet-block-storage-linux)."
-version_added: "2.4"
-author: "Tomas Karasek <tom.to.the.k@gmail.com>"
+
+version_added: "2.10"
+
+author:
+    - Tomas Karasek (@t0mk) <tom.to.the.k@gmail.com>
+    - Nurfet Becirevic (@nurfet-becirevic) <nurfet.becirevic@gmail.com>
+
 options:
   state:
     description:
-     - Indicate desired state of the attachment.
+      - Indicate desired state of the attachment.
     default: present
     choices: ['present', 'absent']
+    type: str
+
   auth_token:
     description:
-     - Packet api token. You can also supply it in env var C(PACKET_API_TOKEN).
+      - Packet api token. You can also supply it in env var C(PACKET_API_TOKEN).
+    type: str
+
   project_id:
     description:
-     - UUID of the project to which the device and volume belong.
+      - UUID of the project to which the device and volume belong.
+    type: str
+
   volume:
     description:
-     - Selector for the volume.
-     - It can be a UUID, an API-generated volume name, or user-defined description string.
-     - 'Example values: 4a347482-b546-4f67-8300-fb5018ef0c5, volume-4a347482, "my volume"'
+      - Selector for the volume.
+      - It can be a UUID, an API-generated volume name, or user-defined description string.
+      - 'Example values: 4a347482-b546-4f67-8300-fb5018ef0c5, volume-4a347482, "my volume"'
+    type: str
+
   device:
     description:
-     - Selector for the device.
-     - It can be a UUID of the device, or a hostname.
-     - 'Example values: 98a14f7a-3d27-4478-b7cf-35b5670523f3, "my device"'
+      - Selector for the device.
+      - It can be a UUID of the device, or a hostname.
+      - 'Example values: 98a14f7a-3d27-4478-b7cf-35b5670523f3, "my device"'
+    type: str
 
 requirements:
   - "python >= 2.6"
@@ -116,18 +121,19 @@ EXAMPLES = '''
 RETURN = '''
 volume_id:
     description: UUID of volume addressed by the module call.
-    type: string
+    type: str
     returned: success
-device_id:
-    description: UUID of device addressed by the module call. 
-    type: string
-    returned: success
-'''  # NOQA
 
-import os
+device_id:
+    description: UUID of device addressed by the module call.
+    type: str
+    returned: success
+'''
+
 import uuid
 
-from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.basic import AnsibleModule, env_fallback
+from ansible.module_utils._text import to_native
 
 HAS_PACKET_SDK = True
 
@@ -166,7 +172,7 @@ def get_device_selector(spec):
 
 
 def do_attach(packet_conn, vol_id, dev_id):
-    api_method = "storage/%s/attachments" % vol_id
+    api_method = "storage/{0}/attachments".format(vol_id)
     packet_conn.call_api(
         api_method,
         params={"device_id": dev_id},
@@ -184,11 +190,11 @@ def do_detach(packet_conn, vol, dev_id=None):
 
 def validate_selected(l, resource_type, spec):
     if len(l) > 1:
-        _msg = ("more than one %s matches specification %s: %s" %
-                (resource_type, spec, l))
+        _msg = ("more than one {0} matches specification {1}: {2}".format(
+                resource_type, spec, l))
         raise Exception(_msg)
     if len(l) == 0:
-        _msg = "no %s matches specification: %s" % (resource_type, spec)
+        _msg = "no {0} matches specification: {1}".format(resource_type, spec)
         raise Exception(_msg)
 
 
@@ -204,9 +210,9 @@ def act_on_volume_attachment(target_state, module, packet_conn):
     volspec = module.params.get("volume")
     devspec = module.params.get("device")
     if devspec is None and target_state == 'present':
-        raise ("If you want to attach a volume, you must specify a device.")
+        raise Exception("If you want to attach a volume, you must specify a device.")
     project_id = module.params.get("project_id")
-    volumes_api_method = "projects/%s/storage" % project_id
+    volumes_api_method = "projects/{0}/storage".format(project_id)
     volumes = packet_conn.call_api(volumes_api_method,
                                    params={'include': 'facility,attachments.device'})['volumes']
     v_match = get_volume_selector(volspec)
@@ -217,7 +223,7 @@ def act_on_volume_attachment(target_state, module, packet_conn):
 
     device = None
     if devspec is not None:
-        devices_api_method = "projects/%s/devices" % project_id
+        devices_api_method = "projects/{0}/devices".format(project_id)
         devices = packet_conn.call_api(devices_api_method)['devices']
         d_match = get_device_selector(devspec)
         matching_devices = [d for d in devices if d_match(d)]
@@ -234,8 +240,8 @@ def act_on_volume_attachment(target_state, module, packet_conn):
         elif device['id'] not in attached_device_ids:
             # Don't reattach volume which is attached to a different device.
             # Rather fail than force remove a device on state == 'present'.
-            raise Exception("volume %s is already attached to device %s" %
-                            (volume, attached_device_ids))
+            raise Exception("volume {0} is already attached to device {1}".format(
+                            volume, attached_device_ids))
     else:
         if device is None:
             if len(attached_device_ids) > 0:
@@ -252,21 +258,24 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             state=dict(choices=STATES, default="present"),
-            auth_token=dict(default=os.environ.get(PACKET_API_TOKEN_ENV_VAR),
-                            no_log=True),
+            auth_token=dict(
+                type='str',
+                fallback=(env_fallback, [PACKET_API_TOKEN_ENV_VAR]),
+                no_log=True
+            ),
             volume=dict(type="str", required=True),
             project_id=dict(type="str", required=True),
             device=dict(type="str"),
         ),
+        supports_check_mode=True,
     )
 
     if not HAS_PACKET_SDK:
         module.fail_json(msg='packet required for this module')
 
     if not module.params.get('auth_token'):
-        _fail_msg = ("if Packet API token is not in environment variable %s, "
-                     "the auth_token parameter is required" %
-                     PACKET_API_TOKEN_ENV_VAR)
+        _fail_msg = ("if Packet API token is not in environment variable {0}, "
+                     "the auth_token parameter is required".format(PACKET_API_TOKEN_ENV_VAR))
         module.fail_json(msg=_fail_msg)
 
     auth_token = module.params.get('auth_token')
@@ -276,15 +285,17 @@ def main():
     state = module.params.get('state')
 
     if state in STATES:
+        if module.check_mode:
+            module.exit_json(changed=False)
+
         try:
             module.exit_json(
                 **act_on_volume_attachment(state, module, packet_conn))
         except Exception as e:
             module.fail_json(
-                msg='failed to set volume_attachment state %s: %s' %
-                (state, e))
+                msg="failed to set volume_attachment state {0}: {1}".format(state, to_native(e)))
     else:
-        module.fail_json(msg='%s is not a valid state for this module' % state)
+        module.fail_json(msg="{0} is not a valid state for this module".format(state))
 
 
 if __name__ == '__main__':
