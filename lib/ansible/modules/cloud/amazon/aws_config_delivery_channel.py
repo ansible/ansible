0a384601764814f9f8b1731e01d1bcbdca2c9b37
Mark Chappell <mchappel@redhat.com>
2019-10-30 21:08:02 +0100

    Fixup sanity test issues related to aws_ modules (#64064)
    * Add AWS_ boilier plate
    
    * AWS_ modules: clean up missing parameter types
    
    * Remove documentation duplicated by ec2 document fragment
    
    * Add missing documentation for options and sub-options
    
    * Add missing return type documentation
    
    * Remove AWS_ sanity-check ignores
    
    * aws_waf_info: fixup conflicting 'defaults' information about the use of waf_regional
    
    * Wrap URLs in U()
    
    * Add elements entries, because felixfontein asked nicely
    
    * Apply suggestions from code review
    
    Co-Authored-By: Jill R <4121322+jillr@users.noreply.github.com>
    
    * Second round of review comments.

--- lib/ansible/modules/cloud/amazon/aws_config_delivery_channel.py
+++ lib/ansible/modules/cloud/amazon/aws_config_delivery_channel.py
@@ -27,24 +27,30 @@ options:
     description:
     - The name of the AWS Config resource.
     required: true
+    type: str
   state:
     description:
     - Whether the Config rule should be present or absent.
     default: present
     choices: ['present', 'absent']
+    type: str
   s3_bucket:
     description:
     - The name of the Amazon S3 bucket to which AWS Config delivers configuration snapshots and configuration history files.
+    type: str
   s3_prefix:
     description:
     - The prefix for the specified Amazon S3 bucket.
+    type: str
   sns_topic_arn:
     description:
     - The Amazon Resource Name (ARN) of the Amazon SNS topic to which AWS Config sends notifications about configuration changes.
+    type: str
   delivery_frequency:
     description:
     - The frequency with which AWS Config delivers configuration snapshots.
     choices: ['One_Hour', 'Three_Hours', 'Six_Hours', 'Twelve_Hours', 'TwentyFour_Hours']
+    type: str
 extends_documentation_fragment:
   - aws
   - ec2
