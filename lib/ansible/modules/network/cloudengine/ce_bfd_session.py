ea6ff48b407a2f0f87b4e8d85aaee10fdaeafa9b
xuxiaowei0512 <786018072@qq.com>
2019-11-02 19:50:14 +0800

    add notes to set modules works with C(connection)

--- lib/ansible/modules/network/cloudengine/ce_bfd_session.py
+++ lib/ansible/modules/network/cloudengine/ce_bfd_session.py
@@ -29,6 +29,10 @@ description:
     - Manages BFD session configuration, creates a BFD session or deletes a specified BFD session
       on HUAWEI CloudEngine devices.
 author: QijunPan (@QijunPan)
+notes:
+  - This module requires the netconf system service be enabled on the remote device being managed.
+  - Recommended connection is C(netconf).
+  - This module also works with C(local) connections for legacy playbooks.
 options:
     session_name:
         description:
