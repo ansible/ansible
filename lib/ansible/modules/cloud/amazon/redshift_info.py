8528fbc79091f4aedd5a831318dcc876d48862d5
Mark Chappell <mchappel@redhat.com>
2019-11-04 17:56:14 +0100

    Amazon related Sanity test fixups (remaining batch 2) (#64358)
    * Remove sanity/ignore.txt entries
    
    * replace use of "_" as a variable name
    
    * Cleanup boilerplate
    
    * Remove default values from mandatory parameters
    
    * Sanity test documentation updates
    
    * Remove unused imports from iam_role

--- lib/ansible/modules/cloud/amazon/redshift_info.py
+++ lib/ansible/modules/cloud/amazon/redshift_info.py
@@ -17,7 +17,7 @@ module: redshift_info
 author: "Jens Carl (@j-carl)"
 short_description: Gather information about Redshift cluster(s)
 description:
-  - Gather information about Redshift cluster(s)
+  - Gather information about Redshift cluster(s).
   - This module was called C(redshift_facts) before Ansible 2.9. The usage did not change.
 version_added: "2.4"
 requirements: [ boto3 ]
@@ -28,11 +28,13 @@ options:
       - "This is a regular expression match with implicit '^'. Append '$' for a complete match."
     required: false
     aliases: ['name', 'identifier']
+    type: str
   tags:
     description:
       - "A dictionary/hash of tags in the format { tag1_name: 'tag1_value', tag2_name: 'tag2_value' }
        to match against the security group(s) you are searching for."
     required: false
+    type: dict
 extends_documentation_fragment:
     - ec2
     - aws
