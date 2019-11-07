ab910e1f5a12fbf028d4d70d28dd7ec36427e602
Jordan Borean <jborean93@gmail.com>
2019-11-08 07:40:25 +1000

    win_psmodule - remove reliance on PSGallery in the tests for stable-2.7 (#64468)
    * win_psmodule - remove reliance on PSGallery in the tests for stable-2.7
    
    * Ignore non-powershell files from sanity check

---
+++ test/integration/targets/win_psmodule/files/module/template.psm1
@@ -0,0 +1,9 @@
+Function --- FUNCTION --- {
+    return [PSCustomObject]@{
+        Name = "--- NAME ---"
+        Version = "--- VERSION ---"
+        Repo = "--- REPO ---"
+    }
+}
+
+Export-ModuleMember -Function --- FUNCTION ---
