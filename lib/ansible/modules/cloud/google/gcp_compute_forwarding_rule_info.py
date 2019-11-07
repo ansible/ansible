2671d2bfd32de9ff4e3d2a65f3d8e9091085c5e9
Modular Magician <magic-modules@google.com>
2019-11-07 07:04:55 +0000

    Bug fixes for GCP modules

--- lib/ansible/modules/cloud/google/gcp_compute_forwarding_rule_info.py
+++ lib/ansible/modules/cloud/google/gcp_compute_forwarding_rule_info.py
@@ -178,11 +178,13 @@ resources:
       type: str
     loadBalancingScheme:
       description:
-      - 'This signifies what the ForwardingRule will be used for and can only take
-        the following values: INTERNAL, EXTERNAL The value of INTERNAL means that
-        this will be used for Internal Network Load Balancing (TCP, UDP). The value
-        of EXTERNAL means that this will be used for External Load Balancing (HTTP(S)
-        LB, External TCP/UDP LB, SSL Proxy) .'
+      - This signifies what the ForwardingRule will be used for and can be EXTERNAL,
+        INTERNAL, or INTERNAL_MANAGED. EXTERNAL is used for Classic Cloud VPN gateways,
+        protocol forwarding to VMs from an external IP address, and HTTP(S), SSL Proxy,
+        TCP Proxy, and Network TCP/UDP load balancers.
+      - INTERNAL is used for protocol forwarding to VMs from an internal IP address,
+        and internal TCP/UDP load balancers.
+      - INTERNAL_MANAGED is used for internal HTTP(S) load balancers.
       returned: success
       type: str
     name:
