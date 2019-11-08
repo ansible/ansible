d34435726130df8c99aa4ea6ca20435a57678763
Fred-sun <37327967+Fred-sun@users.noreply.github.com>
2019-11-09 05:09:08 +0800

    Update azure_rm_batchaccount's document (#64490)

--- lib/ansible/modules/cloud/azure/azure_rm_batchaccount.py
+++ lib/ansible/modules/cloud/azure/azure_rm_batchaccount.py
@@ -28,58 +28,63 @@ DOCUMENTATION = '''
 ---
 module: azure_rm_batchaccount
 version_added: "2.9"
-short_description: Manages a Batch Account on Azure.
+short_description: Manages a Batch Account on Azure
 description:
     - Create, update and delete instance of Azure Batch Account.
 
 options:
     resource_group:
         description:
-        - The name of the resource group in which to create the Batch Account.
+            - The name of the resource group in which to create the Batch Account.
         required: true
+        type: str
     name:
         description:
-        - The name of the Batch Account.
+            - The name of the Batch Account.
         required: true
+        type: str
     location:
         description:
-        - Specifies the supported Azure location where the resource exists.
+            - Specifies the supported Azure location where the resource exists.
+        type: str
     auto_storage_account:
         description:
-        - Existing storage account with which to associate the Batch Account.
-        - It can be the storage account name which is in the same resource group.
-        - "It can be the storage account ID. e.g.,
-          /subscriptions/{subscription_id}/resourceGroups/{resource_group}/providers/Microsoft.Storage/storageAccounts/{name}."
-        - It can be a dict which contains C(name) and C(resource_group) of the storage account.
+            - Existing storage account with which to associate the Batch Account.
+            - It can be the storage account name which is in the same resource group.
+            - It can be the storage account ID. Fox example "/subscriptions/{subscription_id}/resourceGroups/
+              {resource_group}/providers/Microsoft.Storage/storageAccounts/{name}".
+            - It can be a dict which contains I(name) and I(resource_group) of the storage account.
     key_vault:
         description:
-        - Existing key vault with which to associate the Batch Account.
-        - It can be the key vault name which is in the same resource group.
-        - "It can be the key vault ID. e.g.,
-          /subscriptions/{subscription_id}/resourceGroups/{resource_group}/providers/Microsoft.KeyVault/vaults/{name}."
-        - It can be a dict which contains C(name) and C(resource_group) of the key vault.
+            - Existing key vault with which to associate the Batch Account.
+            - It can be the key vault name which is in the same resource group.
+            - It can be the key vault ID. For example "/subscriptions/{subscription_id}/resourceGroups/
+              {resource_group}/providers/Microsoft.KeyVault/vaults/{name}".
+            - It can be a dict which contains I(name) and I(resource_group) of the key vault.
     pool_allocation_mode:
         description:
-        - The pool acclocation mode of the Batch Account.
+            - The pool acclocation mode of the Batch Account.
         default: batch_service
         choices:
-        - batch_service
-        - user_subscription
+            - batch_service
+            - user_subscription
+        type: str
     state:
         description:
-        - Assert the state of the Batch Account.
-        - Use 'present' to create or update a Batch Account and 'absent' to delete it.
+            - Assert the state of the Batch Account.
+            - Use C(present) to create or update a Batch Account and C(absent) to delete it.
         default: present
+        type: str
         choices:
-        - present
-        - absent
+            - present
+            - absent
 
 extends_documentation_fragment:
     - azure
     - azure_tags
 
 author:
-    - "Junyi Yi (@JunyiYi)"
+    - Junyi Yi (@JunyiYi)
 '''
 
 EXAMPLES = '''
@@ -96,13 +101,13 @@ EXAMPLES = '''
 RETURN = '''
 id:
     description:
-    - The ID of the Batch account.
+        - The ID of the Batch account.
     returned: always
     type: str
     sample: "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/myResourceGroup/providers/Microsoft.Batch/batchAccounts/sampleacct"
 account_endpoint:
     description:
-    - The account endpoint used to interact with the Batch service.
+        - The account endpoint used to interact with the Batch service.
     returned: always
     type: str
     sample: sampleacct.westus.batch.azure.com
