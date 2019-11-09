f6350db9dd5684546da1aef3e9cb37663382ed54
YouhuaLi <webcraft@gmail.com>
2019-11-09 00:32:36 +0000

    better excpetion handling when one_auth file is missing

--- lib/ansible/modules/cloud/opennebula/one_vm.py
+++ lib/ansible/modules/cloud/opennebula/one_vm.py
@@ -1301,13 +1301,15 @@ def get_connection_info(module):
         if not password:
             authfile = os.environ.get('ONE_AUTH')
             if authfile is None:
-                authfile = os.environ.get('HOME') + '/.one/one_auth'
+                authfile = os.path.join(os.environ.get("HOME"), ".one", "one_auth")
             try:
                 authstring = open(authfile, "r").read().rstrip()
                 username = authstring.split(":")[0]
                 password = authstring.split(":")[1]
+            except (OSError, IOError) as e:
+                module.fail_json(msg=("Could not find or read ONE_AUTH file at '%s'" % authfile))
             except BaseException:
-                module.fail_json(msg="Could not read ONE_AUTH file")
+                module.fail_json(msg=("Error occurs when read ONE_AUTH file at '%s'" % authfile))
     if not url:
         module.fail_json(msg="Opennebula API url (api_url) is not specified")
     from collections import namedtuple
