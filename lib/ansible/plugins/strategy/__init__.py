773644e35c77582c4743896cd675d47b4bffb2a4
Martin Krizek <martin.krizek@gmail.com>
2019-11-12 17:48:50 +0100

    2.8: perf: reduce getting handler variables (#59969) (#64675)
    * perf: reduce getting handler variables (#59969)
    
    ci_complete
    
    (cherry picked from commit 9ccef9e8192084e9e6b38c36a38c820fc8bd66db)
    
    * Add changelog
    
    * Fix incorrectly resolved conflict
    
    * set_available_variables

--- lib/ansible/plugins/strategy/__init__.py
+++ lib/ansible/plugins/strategy/__init__.py
@@ -350,10 +350,6 @@ class StrategyBase:
 
         return [actual_host]
 
-    def get_handler_templar(self, handler_task, iterator):
-        handler_vars = self._variable_manager.get_vars(play=iterator._play, task=handler_task)
-        return Templar(loader=self._loader, variables=handler_vars)
-
     @debug_closure
     def _process_pending_results(self, iterator, one_pass=False, max_passes=None):
         '''
@@ -362,6 +358,7 @@ class StrategyBase:
         '''
 
         ret_results = []
+        handler_templar = Templar(self._loader)
 
         def get_original_host(host_name):
             # FIXME: this should not need x2 _inventory
@@ -377,8 +374,10 @@ class StrategyBase:
                 for handler_task in handler_block.block:
                     if handler_task.name:
                         if not handler_task.cached_name:
-                            templar = self.get_handler_templar(handler_task, iterator)
-                            handler_task.name = templar.template(handler_task.name)
+                            if handler_templar.is_template(handler_task.name):
+                                handler_templar.set_available_variables(self._variable_manager.get_vars(play=iterator._play,
+                                                                                                        task=handler_task))
+                                handler_task.name = handler_templar.template(handler_task.name)
                             handler_task.cached_name = True
 
                         try:
@@ -533,9 +532,11 @@ class StrategyBase:
                                 for listening_handler_block in iterator._play.handlers:
                                     for listening_handler in listening_handler_block.block:
                                         listeners = getattr(listening_handler, 'listen', []) or []
-                                        templar = self.get_handler_templar(listening_handler, iterator)
+                                        if not listeners:
+                                            continue
+
                                         listeners = listening_handler.get_validated_value(
-                                            'listen', listening_handler._valid_attrs['listen'], listeners, templar
+                                            'listen', listening_handler._valid_attrs['listen'], listeners, handler_templar
                                         )
                                         if handler_name not in listeners:
                                             continue
