f014fe22682bee6b20711203dbc497941f23bfae
Karthikeyan Dhandapani <karthikeyan.dhandapani@hpe.com>
2019-11-13 17:39:50 -0800

    removing unnecessary Display import from accidental commit

--- lib/ansible/plugins/terminal/aruba.py
+++ lib/ansible/plugins/terminal/aruba.py
@@ -25,9 +25,7 @@ import re
 from ansible.errors import AnsibleConnectionFailure
 from ansible.module_utils._text import to_text, to_bytes
 from ansible.plugins.terminal import TerminalBase
-from ansible.utils.display import Display
 
-display = Display()
 
 class TerminalModule(TerminalBase):
 
