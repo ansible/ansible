deb4ae3b2ed4e8fe3fd2948475ea03deba37c6c7
Karthikeyan Dhandapani <karthikeyan.dhandapani@hpe.com>
2019-11-13 16:35:06 -0800

    Adding terminal prompt for Aruba Instant

--- lib/ansible/plugins/terminal/aruba.py
+++ lib/ansible/plugins/terminal/aruba.py
@@ -41,6 +41,7 @@ class TerminalModule(TerminalBase):
         re.compile(br"[pP]assword:$"),
         re.compile(br"(?<=\s)[a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])?\s*#\s*$"),
         re.compile(br"[\r\n]?[\w\+\-\.:\/\[\]]+(?:\([^\)]+\)){0,3}(?:[>#]) ?$"),
+        re.compile(br"[\r\n]?[\w]*(.+)?#(?:\s*)$")
     ]
 
     terminal_stderr_re = [
