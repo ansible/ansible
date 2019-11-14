9908ad1862cfd7bc3e72bf1f03fc25669d8144e7
Farid Da Encarnacao <farid.daencarnacao@gmail.com>
2019-11-14 15:50:16 -0500

    Add default value to 0 for disk in nova_flavor module (#64749)
    * Add default value to 0 for disk in nova_flavor module
    
    * Apply suggestions from code review
    
    Add type int for disk size
    
    Co-Authored-By: John R Barker <john@johnrbarker.com>

--- lib/ansible/modules/cloud/openstack/os_nova_flavor.py
+++ lib/ansible/modules/cloud/openstack/os_nova_flavor.py
@@ -42,6 +42,8 @@ options:
    disk:
      description:
         - Size of local disk, in GB.
+     default: 0
+     type: int
    ephemeral:
      description:
         - Ephemeral space size, in GB.
@@ -181,8 +183,8 @@ def main():
         # required when state is 'present'
         ram=dict(required=False, type='int'),
         vcpus=dict(required=False, type='int'),
-        disk=dict(required=False, type='int'),
 
+        disk=dict(required=False, default=0, type='int'),
         ephemeral=dict(required=False, default=0, type='int'),
         swap=dict(required=False, default=0, type='int'),
         rxtx_factor=dict(required=False, default=1.0, type='float'),
