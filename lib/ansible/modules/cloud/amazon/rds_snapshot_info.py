67fd17bee49c9565d6e312c61ffb13851ec06919
Sebastian Neira <sebastian.neira@smartpension.co.uk>
2019-11-14 16:02:42 +0000

    Update RDS snapshot info to use DBClusterIdentifier (#64253)

--- lib/ansible/modules/cloud/amazon/rds_snapshot_info.py
+++ lib/ansible/modules/cloud/amazon/rds_snapshot_info.py
@@ -327,13 +327,13 @@ def common_snapshot_info(module, conn, method, prefix, params):
 def cluster_snapshot_info(module, conn):
     snapshot_name = module.params.get('db_cluster_snapshot_identifier')
     snapshot_type = module.params.get('snapshot_type')
-    instance_name = module.params.get('db_cluster_instance_identifier')
+    instance_name = module.params.get('db_cluster_identifier')
 
     params = dict()
     if snapshot_name:
         params['DBClusterSnapshotIdentifier'] = snapshot_name
     if instance_name:
-        params['DBClusterInstanceIdentifier'] = instance_name
+        params['DBClusterIdentifier'] = instance_name
     if snapshot_type:
         params['SnapshotType'] = snapshot_type
         if snapshot_type == 'public':
