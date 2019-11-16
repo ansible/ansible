5b54c4bd6c6f431205ba8d272002ea89f2a640fa
Xu Yuandong <yuandongdeyouxiang@gmail.com>
2019-11-16 10:08:16 +0800

    Revert "Doc notes 4 (#64349)" (#64922)
    This reverts commit 87fc838c72fb808508cc4cb1b288ebe134486ef0.

--- lib/ansible/modules/network/cloudengine/ce_netconf.py
+++ lib/ansible/modules/network/cloudengine/ce_netconf.py
@@ -29,10 +29,6 @@ description:
     - Sends an arbitrary netconf command on HUAWEI CloudEngine switches.
 author:
     - wangdezhuang (@QijunPan)
-notes:
-    - This module requires the netconf system service be enabled on the remote device being managed.
-    - Recommended connection is C(netconf).
-    - This module also works with C(local) connections for legacy playbooks.
 options:
     rpc:
         description:
