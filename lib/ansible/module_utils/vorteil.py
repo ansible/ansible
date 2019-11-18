dda5ed0b09df3f656ff23ca6c37cf684fb4cd71f
jalfvort <jon.alfaro@vorteil.io>
2019-11-18 10:21:15 +1000

    boilerplate added to utils

--- lib/ansible/module_utils/vorteil.py
+++ lib/ansible/module_utils/vorteil.py
@@ -2,6 +2,9 @@
 # Copyright: (c) 2019 Wilhelm, Wonigkeit (wilhelm.wonigkeit@vorteil.io)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
 import uuid
 import time
