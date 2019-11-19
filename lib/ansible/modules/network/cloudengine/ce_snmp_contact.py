f3699c80bea46865d9e25f8f69b74767693a5c4a
Xu Yuandong <yuandongdeyouxiang@gmail.com>
2019-11-20 00:04:32 +0800

    Backport/2.9/64928 Doc notes 5 (#64928) (#64948)
    * Doc notes 5 cloudengine (#64928)
    * new version 2.10 can not be backported
    
    (cherry picked from commit 8ea9d0c06a6337aa9670828d13cde59c4ac71a32)
    
    * add a change log

--- lib/ansible/modules/network/cloudengine/ce_snmp_contact.py
+++ lib/ansible/modules/network/cloudengine/ce_snmp_contact.py
@@ -29,6 +29,9 @@ description:
     - Manages SNMP contact configurations on HUAWEI CloudEngine switches.
 author:
     - wangdezhuang (@QijunPan)
+notes:
+    - Recommended connection is C(network_cli).
+    - This module also works with C(local) connections for legacy playbooks.
 options:
     contact:
         description:
