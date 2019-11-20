4cd028665a456c3c2de4fa70fc4d5a1ed5765a1c
chkp-orso <47325598+chkp-orso@users.noreply.github.com>
2019-11-20 07:39:40 +0100

    fix all modules to be able to get parameter with value false (very important fix) (#65040)
    * add import of future and __metaclass__
    
    * fix all modules to be able to get parameter with value false (very importent fix)
    
    * Update ignore.txt

--- lib/ansible/module_utils/network/checkpoint/checkpoint.py
+++ lib/ansible/module_utils/network/checkpoint/checkpoint.py
@@ -73,7 +73,7 @@ def get_payload_from_parameters(params):
     payload = {}
     for parameter in params:
         parameter_value = params[parameter]
-        if parameter_value and is_checkpoint_param(parameter):
+        if parameter_value is not None and is_checkpoint_param(parameter):
             if isinstance(parameter_value, dict):
                 payload[parameter.replace("_", "-")] = get_payload_from_parameters(parameter_value)
             elif isinstance(parameter_value, list) and len(parameter_value) != 0 and isinstance(parameter_value[0], dict):
