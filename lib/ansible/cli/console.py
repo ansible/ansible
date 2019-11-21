c6301a9fc7807bca57236f19e654df85632cb3f9
Gregory Schevchenko <3405066+gvsheva@users.noreply.github.com>
2019-11-21 16:41:42 +0200

    fix utils.color.stringc: enclosure non printable sequences in SOH,STX (#64751)
    * ansible.utils.color.stringc: add wrap_nonvisible_chars flag in stringc
    * add exaplanation for `wrap_nonvisible_chars` case in utils.stringc
    * add changelog entry

--- lib/ansible/cli/console.py
+++ lib/ansible/cli/console.py
@@ -122,7 +122,7 @@ class ConsoleCLI(CLI, cmd.Cmd):
         else:
             prompt += "$ "
             color = self.NORMAL_PROMPT
-        self.prompt = stringc(prompt, color)
+        self.prompt = stringc(prompt, color, wrap_nonvisible_chars=True)
 
     def list_modules(self):
         modules = set()
