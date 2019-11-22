c9b872620f1bda8bb10411df8a5e90e617940092
Matt Davis <mrd@redhat.com>
2019-11-21 18:21:22 -0800

    test WIP pyyaml 5.2
    ci_complete

--- test/lib/ansible_test/_internal/executor.py
+++ test/lib/ansible_test/_internal/executor.py
@@ -221,6 +221,9 @@ def install_command_requirements(args, python_version=None):
         for cloud_platform in get_cloud_platforms(args):
             commands.append(generate_pip_install(pip, '%s.cloud.%s' % (args.command, cloud_platform)))
 
+    # hack in WIP pyyaml 5.2
+    commands.append(generate_pip_install(pip, 'hack', packages=['git+git://github.com/yaml/pyyaml@release/5.2']))
+
     commands = [cmd for cmd in commands if cmd]
 
     # only look for changes when more than one requirements file is needed
