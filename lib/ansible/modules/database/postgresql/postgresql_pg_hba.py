fb69cf350b84fa5c82496ba64f8d1f613ab518bc
Mads Jensen <mje@inducks.org>
2019-11-23 23:19:35 +0100

    Use context manager for mssql and postgresql modules.

--- lib/ansible/modules/database/postgresql/postgresql_pg_hba.py
+++ lib/ansible/modules/database/postgresql/postgresql_pg_hba.py
@@ -285,18 +285,17 @@ class PgHba(object):
         self.comment = []
         # read the pg_hbafile
         try:
-            file = open(self.pg_hba_file, 'r')
-            for line in file:
-                line = line.strip()
-                # uncomment
-                if '#' in line:
-                    line, comment = line.split('#', 1)
-                    self.comment.append('#' + comment)
-                try:
-                    self.add_rule(PgHbaRule(line=line))
-                except PgHbaRuleError:
-                    pass
-            file.close()
+            with open(self.pg_hba_file, 'r') as file:
+                for line in file:
+                    line = line.strip()
+                    # uncomment
+                    if '#' in line:
+                        line, comment = line.split('#', 1)
+                        self.comment.append('#' + comment)
+                    try:
+                        self.add_rule(PgHbaRule(line=line))
+                    except PgHbaRuleError:
+                        pass
             self.unchanged()
         except IOError:
             pass
