3d2cf901dcb2e812b1755898e42f740448a4ef33
Nabil BENDAFI <nbendafi@yseop.com>
2019-11-25 17:23:14 +0100

    Fix/archive empty file (#64895)
    * Remove call to filecompare to fix issue with empty file exclusion from archive
    
    * Add empty file
    
    (cherry picked from commit 277dd54d45dd32a93a3448da4a7a696f4d905479)

--- lib/ansible/modules/files/archive.py
+++ lib/ansible/modules/files/archive.py
@@ -414,16 +414,15 @@ def main():
                                     n_fullpath = to_native(b_fullpath, errors='surrogate_or_strict', encoding='ascii')
                                     n_arcname = to_native(b_match_root.sub(b'', b_fullpath), errors='surrogate_or_strict')
 
-                                    if not filecmp.cmp(b_fullpath, b_dest):
-                                        try:
-                                            if fmt == 'zip':
-                                                arcfile.write(n_fullpath, n_arcname)
-                                            else:
-                                                arcfile.add(n_fullpath, n_arcname, recursive=False)
+                                    try:
+                                        if fmt == 'zip':
+                                            arcfile.write(n_fullpath, n_arcname)
+                                        else:
+                                            arcfile.add(n_fullpath, n_arcname, recursive=False)
 
-                                            b_successes.append(b_fullpath)
-                                        except Exception as e:
-                                            errors.append('Adding %s: %s' % (to_native(b_path), to_native(e)))
+                                        b_successes.append(b_fullpath)
+                                    except Exception as e:
+                                        errors.append('Adding %s: %s' % (to_native(b_path), to_native(e)))
                         else:
                             path = to_native(b_path, errors='surrogate_or_strict', encoding='ascii')
                             arcname = to_native(b_match_root.sub(b'', b_path), errors='surrogate_or_strict')
