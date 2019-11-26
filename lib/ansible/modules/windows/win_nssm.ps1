0a9bd053757eeabb838c7b3f62c84719b2e2aac2
Shachaf92 <shachaf.gold@gmail.com>
2019-11-26 18:48:05 +0200

    Snake case change

--- lib/ansible/modules/windows/win_nssm.ps1
+++ lib/ansible/modules/windows/win_nssm.ps1
@@ -39,10 +39,10 @@ $stderrFile = Get-AnsibleParam -obj $params -name "stderr_file" -type "path"
 $executable = Get-AnsibleParam -obj $params -name "executable" -type "path" -default "nssm.exe"
 
 $executable = Get-AnsibleParam -obj $params -name "executable" -type "path" -default "nssm.exe"
-$AppRotateBytes = Get-AnsibleParam -obj $params -name "AppRotateBytes" -type "int" -default 104858
-$AppRotateOnline = Get-AnsibleParam -obj $params -name "AppRotateOnline" -type "int" -default 0 -validateset 0,1
-$AppStopMethodConsole = Get-AnsibleParam -obj $params -name "AppStopMethodConsole" -type "int"
-$AppStopMethodSkip = Get-AnsibleParam -obj $params -name "AppStopMethodSkip" -type "int" -validateset 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+$app_rotate_bytes = Get-AnsibleParam -obj $params -name "app_rotate_bytes" -type "int" -default 104858
+$app_rotate_online = Get-AnsibleParam -obj $params -name "app_rotate_online" -type "int" -default 0 -validateset 0,1
+$app_stop_method_console = Get-AnsibleParam -obj $params -name "app_stop_method_console" -type "int"
+$App_stop_method_skip = Get-AnsibleParam -obj $params -name "App_stop_method_skip" -type "int" -validateset 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
 
 # Deprecated options since 2.8. Remove in 2.12
 $startMode = Get-AnsibleParam -obj $params -name "start_mode" -type "str" -default "auto" -validateset $start_modes_map.Keys -resultobj $result
@@ -445,14 +445,14 @@ if ($state -eq 'absent') {
         Update-NssmServiceParameter -parameter "AppRotateFiles" -value 1 @common_params
 
         #don't rotate until the service restarts
-        Update-NssmServiceParameter -parameter "AppRotateOnline" -value $AppRotateOnline @common_params
+        Update-NssmServiceParameter -parameter "AppRotateOnline" -value $app_rotate_online @common_params
 
         #both of the below conditions must be met before rotation will happen
         #minimum age before rotating
         Update-NssmServiceParameter -parameter "AppRotateSeconds" -value 86400 @common_params
 
         #minimum size before rotating
-        Update-NssmServiceParameter -parameter "AppRotateBytes" -value $AppRotateBytes @common_params
+        Update-NssmServiceParameter -parameter "AppRotateBytes" -value $app_rotate_bytes @common_params
 
 
         ############## DEPRECATED block since 2.8. Remove in 2.12 ##############
@@ -478,8 +478,8 @@ if ($state -eq 'absent') {
         ########################################################################
 
         # Added per users` requests
-        Update-NssmServiceParameter -parameter "AppStopMethodConsole" -value $AppStopMethodConsole @common_params
-        Update-NssmServiceParameter -parameter "AppStopMethodSkip" -value $AppStopMethodSkip @common_params
+        Update-NssmServiceParameter -parameter "app_stop_method_console" -value $AppStopMethodConsole @common_params
+        Update-NssmServiceParameter -parameter "app_stop_method_skip" -value $AppStopMethodSkip @common_params
     }
 }
 
