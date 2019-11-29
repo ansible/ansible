bf69b4b8a4e7b9a21b7f23dff911f43f3a855146
krisvasudevan <kvasudevan@infoblox.com>
2019-11-30 00:24:37 +0530

    Update nios_fixed_address.py

--- lib/ansible/modules/net_tools/nios/nios_fixed_address.py
+++ lib/ansible/modules/net_tools/nios/nios_fixed_address.py
@@ -167,6 +167,7 @@ from ansible.module_utils.net_tools.nios.api import WapiModule
 from ansible.module_utils.network.common.utils import validate_ip_address, validate_ip_v6_address
 from ansible.module_utils.net_tools.nios.api import NIOS_IPV4_FIXED_ADDRESS, NIOS_IPV6_FIXED_ADDRESS
 
+
 def options(module):
     ''' Transforms the module argument into a valid WAPI struct
     This function will transform the options argument into a structure that
