a2de814835744c85ac32121e245c9cb463a8c140
Saori <jkmr9214@gmail.com>
2019-11-30 20:04:53 +0900

    fix_issue #65352

--- lib/ansible/modules/packaging/os/homebrew.py
+++ lib/ansible/modules/packaging/os/homebrew.py
@@ -500,6 +500,11 @@ class Homebrew(object):
 
     # updated -------------------------------- {{{
     def _update_homebrew(self):
+        if self.module.check_mode:
+            self.changed = True
+            self.message = 'Homebrew would be updated.'
+            raise HomebrewException(self.message)
+
         rc, out, err = self.module.run_command([
             self.brew_path,
             'update',
@@ -526,6 +531,11 @@ class Homebrew(object):
 
     # _upgrade_all --------------------------- {{{
     def _upgrade_all(self):
+        if self.module.check_mode:
+            self.changed = True
+            self.message = 'Homebrew packages would be upgraded.'
+            raise HomebrewException(self.message)
+
         rc, out, err = self.module.run_command([
             self.brew_path,
             'upgrade',
