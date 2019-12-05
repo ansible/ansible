e97a1a6b2ada1058b5689de35ac859ec64eee56a
GomathisevliS <gosriniv@localhost.localdomain>
2019-12-05 08:47:00 -0500

    corrected documentation for deprecation

--- lib/ansible/module_utils/network/eos/argspec/static_routes/static_routes.py
+++ lib/ansible/module_utils/network/eos/argspec/static_routes/static_routes.py
@@ -28,7 +28,8 @@ The arg spec for the eos_static_routes module
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-class Static_routesArgs(object):  # pylint: disable=R0903
+
+class Static_routesArgs(object):
     """The arg spec for the eos_static_routes module
     """
     def __init__(self, **kwargs):
