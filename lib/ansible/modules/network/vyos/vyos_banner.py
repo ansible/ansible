e970e4106538a5d7409ab3f6303a5ac835375c69
Paul Belanger <pabelanger@redhat.com>
2019-12-06 16:14:37 -0500

    WIP: testing zuul refactor
    Depends-On: https://github.com/ansible/ansible-zuul-jobs/pull/265
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- lib/ansible/modules/network/vyos/vyos_banner.py
+++ lib/ansible/modules/network/vyos/vyos_banner.py
@@ -18,7 +18,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 #
-
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'network'}
