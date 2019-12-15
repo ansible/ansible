da4f519645ba4a24dea53df6cf4df1623ee3d97c
thequest <theque5t@users.noreply.github.com>
2019-12-15 00:02:18 -0600

    Removed shebang

--- lib/ansible/plugins/callback/diy.py
+++ lib/ansible/plugins/callback/diy.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 # Copyright: (c) 2019, Trevor Highfill <trevor.highfill@outlook.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
