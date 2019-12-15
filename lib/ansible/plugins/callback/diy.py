781b71e17fc7fa5b3ae09b073c58d5142cceff23
thequest <theque5t@users.noreply.github.com>
2019-12-15 00:23:26 -0600

    Added encoding

--- lib/ansible/plugins/callback/diy.py
+++ lib/ansible/plugins/callback/diy.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Copyright: (c) 2019, Trevor Highfill <trevor.highfill@outlook.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
