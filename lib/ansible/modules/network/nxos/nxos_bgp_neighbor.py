d74264982b55cb29d505c30cb9d6a497ae6c89fc
Nilashish Chakraborty <nilashishchakraborty8@gmail.com>
2019-12-19 00:36:32 +0530

    Add debug (#65909)
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- lib/ansible/modules/network/nxos/nxos_bgp_neighbor.py
+++ lib/ansible/modules/network/nxos/nxos_bgp_neighbor.py
@@ -327,8 +327,11 @@ def state_present(module, existing, proposed, candidate):
             commands.append('no {0}'.format(key))
         elif value == 'default':
             if existing_commands.get(key):
-                existing_value = existing_commands.get(key)
-                commands.append('no {0} {1}'.format(key, existing_value))
+                if key == 'password':
+                    commands.append("no password")
+                else:
+                    existing_value = existing_commands.get(key)
+                    commands.append('no {0} {1}'.format(key, existing_value))
         else:
             if key == 'log-neighbor-changes':
                 if value == 'enable':
