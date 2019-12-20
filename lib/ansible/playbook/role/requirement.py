e254aa8809413aba55bbfd9f107b357cdf2f767a
s-hertel <shertel@redhat.com>
2019-12-20 17:05:06 -0500

    RoleRequirement - Include stderr in the error message if there's a non-0 return code

--- lib/ansible/playbook/role/requirement.py
+++ lib/ansible/playbook/role/requirement.py
@@ -149,7 +149,7 @@ class RoleRequirement(RoleDefinition):
                 display.debug("\tstderr: " + stderr)
                 raise AnsibleError("when executing %s: %s" % (ran, to_native(e)))
             if popen.returncode != 0:
-                raise AnsibleError("- command %s failed in directory %s (rc=%s)" % (' '.join(cmd), tempdir, popen.returncode))
+                raise AnsibleError("- command %s failed in directory %s (rc=%s) - %s" % (' '.join(cmd), tempdir, popen.returncode, to_text(stderr)))
 
         if scm not in ['hg', 'git']:
             raise AnsibleError("- scm %s is not currently supported" % scm)
