1422dc8239ba97bc59ba575de44cdd91f8d163ab
Abhay Kadam <abhaykadam@users.noreply.github.com>
2019-12-25 17:24:38 +0530

    Fix env lookup plugin error on utf8 values (#65541)
    This commit fixes issue #65297.
    
    The env lookup plugin used to fail when environment variable value
    contained any UTF-8 characters (e.g., δ, ζ).

--- test/integration/targets/lookups/runme.sh
+++ test/integration/targets/lookups/runme.sh
@@ -8,7 +8,8 @@ source virtualenv.sh
 # because plugins and requirements are loaded before the task runs
 pip install passlib
 
-ANSIBLE_ROLES_PATH=../ ansible-playbook lookups.yml "$@"
+# UNICODE_VAR is used in testing the env lookup plugin unicode functionality
+ANSIBLE_ROLES_PATH=../ UNICODE_VAR=café ansible-playbook lookups.yml "$@"
 
 ansible-playbook template_lookup_vaulted.yml --vault-password-file test_vault_pass "$@"
 
---
+++ test/units/plugins/lookup/test_env.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2019, Abhay Kadam <abhaykadam88@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# Make coding more python3-ish
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import pytest
+
+from ansible.plugins.loader import lookup_loader
+
+
+@pytest.mark.parametrize('env_var,exp_value', [
+    ('foo', 'bar'),
+    ('equation', 'a=b*100')
+])
+def test_env_var_value(monkeypatch, env_var, exp_value):
+    monkeypatch.setattr('ansible.utils.py3compat.environ.get', lambda x, y: exp_value)
+
+    env_lookup = lookup_loader.get('env')
+    retval = env_lookup.run([env_var], None)
+    assert retval == [exp_value]
+
+
+@pytest.mark.parametrize('env_var,exp_value', [
+    ('simple_var', 'alpha-β-gamma'),
+    ('the_var', 'ãnˈsiβle')
+])
+def test_utf8_env_var_value(monkeypatch, env_var, exp_value):
+    monkeypatch.setattr('ansible.utils.py3compat.environ.get', lambda x, y: exp_value)
+
+    env_lookup = lookup_loader.get('env')
+    retval = env_lookup.run([env_var], None)
+    assert retval == [exp_value]
