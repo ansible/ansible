1001b2ea83461722cb4866eb02f9b6ba9a3e3305
Abhijeet Kasurde <akasurde@redhat.com>
2019-12-29 21:59:37 +0530

    Fortios: Correct underscore_to_hyphen API
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/network/fortios/fortios_system_vdom_dns.py
+++ lib/ansible/modules/network/fortios/fortios_system_vdom_dns.py
@@ -226,8 +226,8 @@ def filter_system_vdom_dns_data(json):
 
 def underscore_to_hyphen(data):
     if isinstance(data, list):
-        for elem in data:
-            elem = underscore_to_hyphen(elem)
+        for i, elem in enumerate(data):
+            data[i] = underscore_to_hyphen(elem)
     elif isinstance(data, dict):
         new_data = {}
         for k, v in data.items():
