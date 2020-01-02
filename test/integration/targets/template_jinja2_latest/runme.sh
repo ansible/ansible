b219bec64ad90b00428efe406b9d4fcbd1354fc6
Matt Martz <matt@sivel.net>
2020-01-02 16:17:44 -0600

    limit py2.6 instal method. ci_complete

--- test/integration/targets/template_jinja2_latest/runme.sh
+++ test/integration/targets/template_jinja2_latest/runme.sh
@@ -4,7 +4,13 @@ set -eux
 
 source virtualenv.sh
 
-pip install --extra-index-url https://sivel.eng.ansible.com/pypi/ --no-binary jinja,jinja2 jinja2==2.11.0 jinja==2.11.0.dev0
+PY_VER=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+
+if [ "$PY_VER" == "2.6" ]; then
+    pip install -U jinja2
+else
+    pip install --extra-index-url https://sivel.eng.ansible.com/pypi/ --no-binary jinja,jinja2 jinja2==2.11.0 jinja==2.11.0.dev0
+fi
 
 ANSIBLE_ROLES_PATH=../
 export ANSIBLE_ROLES_PATH
