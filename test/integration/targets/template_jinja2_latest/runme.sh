485493d1efa6bf8fe5b3920579f9d263fe2eb095
Matt Martz <matt@sivel.net>
2020-01-02 12:26:34 -0600

    install jinja2-compat

--- test/integration/targets/template_jinja2_latest/runme.sh
+++ test/integration/targets/template_jinja2_latest/runme.sh
@@ -5,6 +5,7 @@ set -eux
 source virtualenv.sh
 
 pip install -U https://github.com/pallets/jinja/tarball/the-great-rename
+pip install -U 'https://github.com/pallets/jinja/tarball/the-great-rename#egg=jinja2&subdirectory=jinja2-compat'
 
 ANSIBLE_ROLES_PATH=../
 export ANSIBLE_ROLES_PATH
