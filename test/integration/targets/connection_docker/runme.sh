7c365d1682d5a116ce873a772314ef632740cea4
Felix Fontein <felix@fontein.de>
2020-01-03 00:54:50 +0100

    Use smaller test container.

--- test/integration/targets/connection_docker/runme.sh
+++ test/integration/targets/connection_docker/runme.sh
@@ -33,7 +33,7 @@ trap cleanup INT TERM EXIT
 
 echo "Start containers"
 for CONTAINER in ${DOCKER_CONTAINERS}; do
-    docker run --rm --name ${CONTAINER} --detach python:3 /bin/sh -c 'sleep 10m'
+    docker run --rm --name ${CONTAINER} --detach python:3-alpine /bin/sh -c 'sleep 10m'
     echo ${CONTAINER}
 done
 
