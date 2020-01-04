b48366c2fecdc5c68e13a522733df80c26f686d4
Andrew Klychkov <aaklychkov@mail.ru>
2020-01-04 13:18:45 +0300

    postgresql modules: use query parameters with cursor objects (#65862)
    * postgresql modules: use query parameters with cursor objects
    
    * add changelog fragment
    
    * fix typo in changelog fragment

--- lib/ansible/modules/database/postgresql/postgresql_table.py
+++ lib/ansible/modules/database/postgresql/postgresql_table.py
@@ -288,9 +288,10 @@ class Table(object):
                  "FROM pg_tables AS t "
                  "INNER JOIN pg_class AS c ON  c.relname = t.tablename "
                  "INNER JOIN pg_namespace AS n ON c.relnamespace = n.oid "
-                 "WHERE t.tablename = '%s' "
-                 "AND n.nspname = '%s'" % (tblname, schema))
-        res = exec_sql(self, query, add_to_executed=False)
+                 "WHERE t.tablename = %(tblname)s "
+                 "AND n.nspname = %(schema)s")
+        res = exec_sql(self, query, query_params={'tblname': tblname, 'schema': schema},
+                       add_to_executed=False)
         if res:
             self.exists = True
             self.info = dict(
