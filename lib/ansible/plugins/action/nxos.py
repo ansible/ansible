5c3e0f738b82a253b19a84d085967aa2e24dc46c
nkshrishail <42039941+nkshrishail@users.noreply.github.com>
2020-01-08 03:13:23 -0500

    nxos: Removing local-nxapi plugin and related parameters' usage from the integration tests. (#65068)
    * nxos_integration_tests: Removing obsolete provider parameter
    
    * nxos: removing support for local-nxapi plugin
    
    * nxos: removing LocalNxapi plugin implementation
    
    * nxos_nxapi: removing local plugin usage
    
    * REVERT - nxos: removing LocalNxapi plugin implementation
    
    * Addressing yamllint issues
    
    * Removing the condition on the debug messages
    
    * Restoring changes done to nxos_nxapi* tests
    
    * Retaining timeout of 500 on nxos_smoke test
    
    * Some cleanup
    
    * Addressing latest comments
    
    * Restoring the nxos_smoke and removing timeout from nxos_config and nxos_rollback

--- lib/ansible/plugins/action/nxos.py
+++ lib/ansible/plugins/action/nxos.py
@@ -41,7 +41,7 @@ class ActionModule(ActionNetworkModule):
         self._config_module = True if module_name == 'nxos_config' else False
         persistent_connection = self._play_context.connection.split('.')[-1]
 
-        if (persistent_connection == 'httpapi' or self._task.args.get('provider', {}).get('transport') == 'nxapi') \
+        if (self._play_context.connection in ('httpapi', 'local') or self._task.args.get('provider', {}).get('transport') == 'nxapi') \
                 and module_name in ('nxos_file_copy', 'nxos_nxapi'):
             return {'failed': True, 'msg': "Transport type 'nxapi' is not valid for '%s' module." % (module_name)}
 
