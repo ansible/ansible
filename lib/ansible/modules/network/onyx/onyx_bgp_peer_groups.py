0d9c14c76844fb707b0626f78b4ed54a1fd5f83c
Sara Touqan <sarato@mellanox.com>
2020-01-09 15:20:22 +0200

    Supporting bgp peer groups configuration.

---
+++ lib/ansible/modules/network/onyx/onyx_bgp_peer_groups.py
@@ -0,0 +1,316 @@
+#!/usr/bin/python
+#
+# Copyright: Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = """
+---
+module: onyx_bgp_peer_groups
+version_added: "2.10"
+author: "Sara Touqan (@sarato)"
+short_description: Configures bgp peer groups parameters
+description:
+  - This module provides declarative management of bgp peer groups protocol params
+    on Mellanox ONYX network devices.
+options:
+  peer_groups:
+    type: list
+    description:
+      - List of bgp peer groups.
+    suboptions:
+      name:
+        description:
+          - Bgp peer group name.
+        required: true
+        type: str
+      router_bgp:
+        description:
+          - configures the router-bgp number (as-number).
+        type: int
+        required: true
+      state:
+        description:
+          - Indicates if the peer group should be created or deleted.
+        choices: ['present', 'absent']
+        type: str
+      next_hop_peer_enabled:
+        description:
+          - Used to decide if we want to List the peer address as next hop in routes.
+        type: bool
+      remote_as:
+        description:
+          -  Specifys remote AS for dynamic peers in this IP range.
+        type: str
+      listen_range_ip_prefix:
+        description:
+          -  Specifys range of IP addresses to accept dynamic peering requests from.
+        type: str
+      mask_length:
+        description:
+          -  IP mask length.
+        type: str
+      listen_range_state:
+        description:
+          - Indicates if the entered range will be accepted as a dynamic peering requests or not.
+        choices: ['present', 'absent']
+        type: str
+
+  neighbors:
+    type: list
+    description:
+      - List of bgp neighbors.
+    suboptions:
+      ip_address:
+        description:
+          - Bgp neighbor ip address.
+        required: true
+        type: str
+      router_bgp:
+        description:
+          - configures the router-bgp number (as-number).
+        type: int
+        required: true
+      group_name:
+        description:
+          - Bgp peer group name.
+        type: str
+      state:
+        description:
+          - Indicates if the neighbor should be assigned or unassigned form the specified peer group.
+        choices: ['present', 'absent']
+        type: str
+      next_hop_peer_enabled:
+        description:
+          - Used to decide if we want to List the peer address as next hop in routes
+        type: bool
+"""
+
+EXAMPLES = """
+- name: Creates peer group
+  onyx_bgp_peer_groups:
+     peer_groups:
+        - name: group1
+          router_bgp: 1
+          state: present
+- name: Deletes peer group
+  onyx_bgp_peer_groups:
+     peer_groups:
+        - name: group1
+          router_bgp: 1
+          state: absent
+- name: Assign a neighbor to a peer-group
+  onyx_bgp_peer_groups:
+     neighbors:
+        - group_name: group1
+          ip_address: 1.1.1.0
+          router_bgp: 1
+          state: present
+- name: Unassign a neighbor to a peer-group
+  onyx_bgp_peer_groups:
+     neighbors:
+        - group_name: group1
+          ip_address: 1.1.1.0
+          router_bgp: 1
+          state: absent
+- name: Enables next-hop for a peer-group.
+  onyx_bgp_peer_groups:
+     peer_groups:
+        - name: group1
+          next_hop_peer_enabled: yes
+- name: configures listen range for a peer group
+  onyx_bgp_peer_groups:
+     peer_groups:
+        - name: group1
+          router_bgp: 1
+          state: present
+          mask_length: 24
+          remote_as: 3
+          listen_range_ip_prefix: 3.3.3.0
+
+"""
+
+RETURN = """
+commands:
+  description: The list of configuration mode commands to send to the device.
+  returned: always
+  type: list
+  sample:
+    - neighbor <ip-address > peer-group <peer-group-name>
+    - neighbor <peer-group-name> peer-group
+    - no neighbor <ip-address > peer-group <peer-group-name>
+    - no neighbor <peer-group-name> peer-group
+    - neighbor {<ip-address> | <peer-group-name>} next-hop-peer [disable]
+    - no neighbor {<ip-address> | <peer-group-name>} next-hop-peer
+    - bgp listen range <ip-prefix> peer-group <peer-group-name> remote-as <as-number>
+    - no bgp listen range <ip-prefix> /<mask-length> peer-group <peer-group-name>
+
+"""
+
+import re
+
+from ansible.module_utils.basic import AnsibleModule
+
+from ansible.module_utils.network.onyx.onyx import show_cmd
+from ansible.module_utils.network.onyx.onyx import BaseOnyxModule
+
+
+class OnyxBgpPeerGroupsModule(BaseOnyxModule):
+
+    def init_module(self):
+        """ initialize module
+        """
+        peer_group_spec = dict(name=dict(required=True),
+                               router_bgp=dict(type='int', required=True),
+                               state=dict(choices=['present', 'absent']),
+                               next_hop_peer_enabled=dict(type='bool'),
+                               remote_as=dict(),
+                               listen_range_ip_prefix=dict(),
+                               mask_length=dict(),
+                               listen_range_state=dict(choices=['present', 'absent']))
+        neighbor_spec = dict(ip_address=dict(required=True),
+                             router_bgp=dict(type='int', required=True),
+                             group_name=dict(),
+                             state=dict(choices=['present', 'absent']),
+                             next_hop_peer_enabled=dict(type='bool'))
+        element_spec = dict(
+            peer_groups=dict(type='list', elements='dict', options=peer_group_spec),
+            neighbors=dict(type='list', elements='dict', options=neighbor_spec),
+        )
+        argument_spec = dict()
+        argument_spec.update(element_spec)
+        self._module = AnsibleModule(
+            argument_spec=argument_spec,
+            supports_check_mode=True)
+
+    def validate_peer_values(self):
+        peer_groups = self._required_config.get("peer_groups")
+        if peer_groups is not None:
+            for group in peer_groups:
+                if group.get('listen_range_ip_prefix') is not None:
+                    if ((group.get('remote_as') is None) or (group.get('mask_length') is None)):
+                        self._module.fail_json(msg='Missing peer-group param values, remote_as and mask_length are required for the listen range.')
+
+    def get_required_config(self):
+        module_params = self._module.params
+        self._required_config = dict(module_params)
+        self.validate_param_values(self._required_config)
+        self.validate_peer_values()
+
+    def _set_bgp_config(self, bgp_config):
+        curr_config_arr = []
+        bgp_config = bgp_config.get('Lines')
+        if bgp_config is None:
+            self._current_config['curr_config_arr'] = curr_config_arr
+            return
+        for runn_config in bgp_config:
+            curr_config_arr.append(runn_config.strip())
+        self._current_config['curr_config_arr'] = curr_config_arr
+
+    def _show_bgp_config(self):
+        cmd = "show running-config | include bgp"
+        return show_cmd(self._module, cmd, json_fmt=True, fail_on_error=False)
+
+    def load_current_config(self):
+        self._current_config = dict()
+        bgp_config = self._show_bgp_config()
+        if bgp_config is not None:
+            self._set_bgp_config(bgp_config)
+
+    def generate_commands(self):
+        curr_config_arr = self._current_config.get("curr_config_arr")
+        peer_groups = self._required_config.get("peer_groups")
+        neighbors = self._required_config.get("neighbors")
+        if peer_groups is not None:
+            for group in peer_groups:
+                group_name = group.get('name')
+                state = group.get('state')
+                listen_range = group.get('listen_range_ip_prefix')
+                remote_as = group.get('remote_as')
+                listen_range_state = group.get('listen_range_state')
+                mask_length = group.get('mask_length')
+                router_bgp = group.get('router_bgp')
+                next_hop_peer_enabled = group.get('next_hop_peer_enabled')
+                if state is not None:
+                    if state == "absent":
+                        if ('router bgp {0} vrf default neighbor {1} peer-group' .format(router_bgp, group_name)) in curr_config_arr:
+                            self._commands.append('no router bgp {0} neighbor {1} peer-group' .format(router_bgp, group_name))
+                    else:
+                        if ('router bgp {0} vrf default neighbor {1} peer-group' .format(router_bgp, group_name)) not in curr_config_arr:
+                            self._commands.append('router bgp {0} neighbor {1} peer-group' .format(router_bgp, group_name))
+                else:
+                    if ('router bgp {0} vrf default neighbor {1} peer-group' .format(router_bgp, group_name)) not in curr_config_arr:
+                        self._commands.append('router bgp {0} neighbor {1} peer-group' .format(router_bgp, group_name))
+
+                if next_hop_peer_enabled is not None:
+                    if next_hop_peer_enabled is True:
+                        if ('router bgp {0} vrf default neighbor {1} next-hop-peer' .format(router_bgp, group_name)) not in curr_config_arr:
+                            self._commands.append('router bgp {0} neighbor {1} next-hop-peer' .format(router_bgp, group_name))
+                    else:
+                        if ('router bgp {0} vrf default neighbor {1} next-hop-peer disable' .format(router_bgp, group_name)) not in curr_config_arr:
+                            self._commands.append('router bgp {0} neighbor {1} next-hop-peer disable' .format(router_bgp, group_name))
+
+                if listen_range is not None:
+                    if listen_range_state is not None:
+                        if listen_range_state == "absent":
+                            if ('router bgp {0} vrf default bgp listen range {1} /{2} peer-group {3} remote-as {4}' .format(router_bgp, listen_range,
+                                                                                                                            mask_length, group_name,
+                                                                                                                            remote_as)) in curr_config_arr:
+                                self._commands.append('no router bgp {0} bgp listen range {1} /{2} peer-group {3}' .format(router_bgp, listen_range,
+                                                                                                                           mask_length, group_name))
+                        else:
+                            if ('router bgp {0} vrf default bgp listen range {1} /{2} peer-group {3} remote-as {4}' .format(router_bgp, listen_range,
+                                                                                                                            mask_length, group_name,
+                                                                                                                            remote_as)) not in curr_config_arr:
+                                self._commands.append('router bgp {0} bgp listen range {1} /{2} peer-group {3} remote-as {4}' .format(router_bgp, listen_range,
+                                                                                                                                      mask_length, group_name,
+                                                                                                                                      remote_as))
+                    else:
+                        if ('router bgp {0} vrf default bgp listen range {1} /{2} peer-group {3} remote-as {4}' .format(router_bgp, listen_range, mask_length,
+                                                                                                                        group_name,
+                                                                                                                        remote_as)) not in curr_config_arr:
+                            self._commands.append('router bgp {0} bgp listen range {1} /{2} peer-group {3} remote-as {4}' .format(router_bgp, listen_range,
+                                                                                                                                  mask_length, group_name,
+                                                                                                                                  remote_as))
+
+        if neighbors is not None:
+            for neighbor in neighbors:
+                address = neighbor.get('ip_address')
+                group_name = neighbor.get('group_name')
+                router_bgp = neighbor.get('router_bgp')
+                state = neighbor.get('state')
+                next_hop_peer_enabled = neighbor.get('next_hop_peer_enabled')
+                if group_name is not None:
+                    if state is not None:
+                        if state == "absent":
+                            if ('router bgp {0} vrf default neighbor {1} peer-group {2}' .format(router_bgp, address, group_name)) in curr_config_arr:
+                                self._commands.append('no router bgp {0} neighbor {1} peer-group {2}' .format(router_bgp, address, group_name))
+                        else:
+                            if ('router bgp {0} vrf default neighbor {1} peer-group {2}' .format(router_bgp, address, group_name)) not in curr_config_arr:
+                                self._commands.append('router bgp {0} neighbor {1} peer-group {2}' .format(router_bgp, address, group_name))
+                    else:
+                        if ('router bgp {0} vrf default neighbor {1} peer-group {2}' .format(router_bgp, address, group_name)) not in curr_config_arr:
+                            self._commands.append('router bgp {0} neighbor {1} peer-group {2}' .format(router_bgp, address, group_name))
+                if next_hop_peer_enabled is not None:
+                    if next_hop_peer_enabled is True:
+                        if ('router bgp {0} vrf default neighbor {1} next-hop-peer' .format(router_bgp, address)) not in curr_config_arr:
+                            self._commands.append('router bgp {0} neighbor {1} next-hop-peer' .format(router_bgp, address))
+                    else:
+                        if ('router bgp {0} vrf default neighbor {1} next-hop-peer disable' .format(router_bgp, address)) not in curr_config_arr:
+                            self._commands.append('router bgp {0} neighbor {1} next-hop-peer disable' .format(router_bgp, address))
+
+
+def main():
+    """ main entry point for module execution
+    """
+    OnyxBgpPeerGroupsModule.main()
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/units/modules/network/onyx/test_onyx_bgp_peer_groups.py
@@ -0,0 +1,99 @@
+#
+# Copyright: Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# Make coding more python3-ish
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from units.compat.mock import patch
+from ansible.modules.network.onyx import onyx_bgp_peer_groups
+from units.modules.utils import set_module_args
+from .onyx_module import TestOnyxModule, load_fixture
+
+
+class TestOnyxBgpPeerGroupsModule(TestOnyxModule):
+
+    module = onyx_bgp_peer_groups
+
+    def setUp(self):
+        super(TestOnyxBgpPeerGroupsModule, self).setUp()
+        self.mock_get_config = patch.object(
+            onyx_bgp_peer_groups.OnyxBgpPeerGroupsModule, "_show_bgp_config")
+        self.get_config = self.mock_get_config.start()
+
+        self.mock_load_config = patch(
+            'ansible.module_utils.network.onyx.onyx.load_config')
+        self.load_config = self.mock_load_config.start()
+
+    def tearDown(self):
+        super(TestOnyxBgpPeerGroupsModule, self).tearDown()
+        self.mock_get_config.stop()
+        self.mock_load_config.stop()
+
+    def load_fixtures(self, commands=None, transport='cli'):
+        config_file = 'onyx_show_bgp_peer_groups.cfg'
+        data = load_fixture(config_file)
+        self.get_config.return_value = data
+        self.load_config.return_value = None
+
+    def test_bgp_group_creation_no_change(self):
+        set_module_args(dict(peer_groups=[dict(name='group1',
+                                               router_bgp=1)]))
+        self.execute_module(changed=False)
+
+    def test_bgp_group_creation_with_change(self):
+        set_module_args(dict(peer_groups=[dict(name='group2',
+                                               router_bgp=1)]))
+        commands = ['router bgp 1 neighbor group2 peer-group']
+        self.execute_module(changed=True, commands=commands)
+
+    def test_bgp_neighbor_assignment_no_change(self):
+        set_module_args(dict(neighbors=[dict(group_name='group1',
+                                             router_bgp=1,
+                                             ip_address='1.1.1.1')]))
+        self.execute_module(changed=False)
+
+    def test_bgp_neighbor_assignment_with_change(self):
+        set_module_args(dict(neighbors=[dict(group_name='group2',
+                                             router_bgp=1,
+                                             ip_address='1.1.1.0')]))
+        commands = ['router bgp 1 neighbor 1.1.1.0 peer-group group2']
+        self.execute_module(changed=True, commands=commands)
+
+    def test_bgp_next_hop_peer_no_change(self):
+        set_module_args(dict(peer_groups=[dict(name='group1',
+                                               router_bgp=1,
+                                               next_hop_peer_enabled=False)]))
+        self.execute_module(changed=False)
+
+    def test_bgp_next_hop_peer_with_change(self):
+        set_module_args(dict(peer_groups=[dict(name='group1',
+                                               router_bgp=1,
+                                               next_hop_peer_enabled=True)]))
+        commands = ['router bgp 1 neighbor group1 next-hop-peer']
+        self.execute_module(changed=True, commands=commands)
+
+    def test_bgp_listen_range_peer_with_change(self):
+        set_module_args(dict(peer_groups=[dict(name='group1',
+                                               router_bgp=1,
+                                               listen_range_ip_prefix='1.1.2.3',
+                                               remote_as='3',
+                                               mask_length='24')]))
+        commands = ['router bgp 1 bgp listen range 1.1.2.3 /24 peer-group group1 remote-as 3']
+        self.execute_module(changed=True, commands=commands)
+
+    def test_bgp_delete_group_with_change(self):
+        set_module_args(dict(peer_groups=[dict(name='group1',
+                                               router_bgp=1,
+                                               state='absent')]))
+        commands = ['no router bgp 1 neighbor group1 peer-group']
+        self.execute_module(changed=True, commands=commands)
+
+    def test_bgp_delete_neighbor_with_change(self):
+        set_module_args(dict(neighbors=[dict(group_name='group1',
+                                             router_bgp=1,
+                                             ip_address='1.1.1.1',
+                                             state='absent')]))
+        commands = ['no router bgp 1 neighbor 1.1.1.1 peer-group group1']
+        self.execute_module(changed=True, commands=commands)
