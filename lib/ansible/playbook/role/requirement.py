c26291e15adecc2258a2935aecb51d43e8e9adb4
Sloane Hertel <shertel@redhat.com>
2020-01-14 09:46:23 -0500

    [stable-2.9] RoleRequirement - Include stderr in the error message (#66006)
    * RoleRequirement - Include stderr in the error message if there's a non-0 return code
    
    * Don't try to concatenate str and bytes
    
    Co-Authored-By: Sam Doran <sdoran@redhat.com>
    (cherry picked from commit 1b3ca34)
    
    Co-authored-by: Sloane Hertel <shertel@redhat.com>

--- lib/ansible/playbook/role/requirement.py
+++ lib/ansible/playbook/role/requirement.py
@@ -145,11 +145,11 @@ class RoleRequirement(RoleDefinition):
             except Exception as e:
                 ran = " ".join(cmd)
                 display.debug("ran %s:" % ran)
-                display.debug("\tstdout: " + stdout)
-                display.debug("\tstderr: " + stderr)
+                display.debug("\tstdout: " + to_text(stdout))
+                display.debug("\tstderr: " + to_text(stderr))
                 raise AnsibleError("when executing %s: %s" % (ran, to_native(e)))
             if popen.returncode != 0:
-                raise AnsibleError("- command %s failed in directory %s (rc=%s)" % (' '.join(cmd), tempdir, popen.returncode))
+                raise AnsibleError("- command %s failed in directory %s (rc=%s) - %s" % (' '.join(cmd), tempdir, popen.returncode, to_native(stderr)))
 
         if scm not in ['hg', 'git']:
             raise AnsibleError("- scm %s is not currently supported" % scm)
