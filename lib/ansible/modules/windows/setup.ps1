c390192ac59bba1f7075982dcf58983b4a707b54
Jordan Borean <jborean93@gmail.com>
2020-01-14 11:22:39 +1000

    Deprecate Windows 2008, and 2008 R2 (#66257)
    * Deprecate Windows 2008, and 2008 R2
    
    * Remove shippable nodes
    
    * Update windows_faq.rst
    
    Be less specific about 2008/R2 timeframes
    
    * Update setup.ps1
    
    tweak warning text
    
    Co-authored-by: Matt Davis <nitzmahone@users.noreply.github.com>

--- lib/ansible/modules/windows/setup.ps1
+++ lib/ansible/modules/windows/setup.ps1
@@ -140,6 +140,14 @@ $ansible_facts = @{
 
 $osversion = [Environment]::OSVersion
 
+if ($osversion.Version -lt [version]"6.2") {
+    # Server 2008, 2008 R2, and Windows 7 are not tested in CI and we want to let customers know about it before
+    # removing support altogether.
+    $version_string = "{0}.{1}" -f ($osversion.Version.Major, $osversion.Version.Minor)
+    $msg = "Windows version '$version_string' will no longer be supported or tested in the next Ansible release"
+    Add-DeprecationWarning -obj $result -message $msg -version "2.11"
+}
+
 if($gather_subset.Contains('all_ipv4_addresses') -or $gather_subset.Contains('all_ipv6_addresses')) {
     $netcfg = Get-LazyCimInstance Win32_NetworkAdapterConfiguration
 
