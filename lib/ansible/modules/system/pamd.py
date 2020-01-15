bf63c8d21fb6be3cd2bdf9ea282b3a40a0c2dd59
satken2 <g.g.satken@gmail.com>
2020-01-16 01:37:20 +0800

    pamd: Fixed AttributeError when removing first or last line(#60497)(#66504)

--- lib/ansible/modules/system/pamd.py
+++ lib/ansible/modules/system/pamd.py
@@ -480,7 +480,8 @@ class PamdService(object):
             if current_line.matches(rule_type, rule_control, rule_path):
                 if current_line.prev is not None:
                     current_line.prev.next = current_line.next
-                    current_line.next.prev = current_line.prev
+                    if current_line.next is not None:
+                        current_line.next.prev = current_line.prev
                 else:
                     self._head = current_line.next
                     current_line.next.prev = None
