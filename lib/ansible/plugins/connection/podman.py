b578282c90cd1e475f3ef2c37f7b657cd0ec6107
Adam Miller <admiller@redhat.com>
2020-01-17 15:16:26 -0600

    Update lib/ansible/plugins/connection/podman.py
    Co-Authored-By: Sam Doran <sdoran@redhat.com>

--- lib/ansible/plugins/connection/podman.py
+++ lib/ansible/plugins/connection/podman.py
@@ -166,7 +166,7 @@ class Connection(ConnectionBase):
                 "cp", [in_path, self._container_id + ":" + out_path], use_container_id=False
             )
             if rc != 0:
-                if to_native('cannot copy into running rootless container with pause set' in to_native(stderr)):
+                if 'cannot copy into running rootless container with pause set' in to_native(stderr):
                     rc, stdout, stderr = self._podman(
                         "cp", ["--pause=false", in_path, self._container_id + ":" + out_path], use_container_id=False
                     )
