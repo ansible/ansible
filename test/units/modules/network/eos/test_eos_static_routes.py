0f22c4e00076bf138dbd18ab8c0b12840e8d3118
GomathisevliS <gosriniv@localhost.localdomain>
2020-01-22 17:44:31 -0500

    lint errors

--- test/units/modules/network/eos/test_eos_static_routes.py
+++ test/units/modules/network/eos/test_eos_static_routes.py
@@ -215,26 +215,25 @@ class TestEosStaticRoutesModule(TestEosModule):
                               ])
                      ]),
                 dict(address_families=[
-                         dict(afi="ipv4",
-                              routes=[
-                                  dict(dest="10.1.1.0/24",
-                                       next_hops=[
-                                           dict(interface="Management1")
-                                       ])
-                              ])
+                     dict(afi="ipv4",
+                          routes=[
+                              dict(dest="10.1.1.0/24",
+                                   next_hops=[
+                                       dict(interface="Management1")
+                                   ])
+                          ])
                      ]),
                 dict(address_families=[
-                         dict(afi="ipv6",
-                              routes=[
-                                  dict(dest="1000:10::/64", 
-                                       next_hops=[
-                                           dict(interface="Ethernet1",
-                                                admin_distance=67,
-                                                tag=98)
-                                       ])
-                              ])
+                     dict(afi="ipv6",
+                          routes=[
+                              dict(dest="1000:10::/64",
+                                   next_hops=[
+                                       dict(interface="Ethernet1",
+                                            admin_distance=67,
+                                             tag=98)
+                                   ])
+                          ])
                      ])
-                
             ], state="overridden"))
         self.execute_module(changed=False, commands=[])
 
