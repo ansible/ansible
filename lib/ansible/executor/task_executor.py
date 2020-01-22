c512f30d12fee4af5acfeefdae41b077e050e600
Nathaniel Case <ncase@redhat.com>
2020-01-22 18:30:57 -0500

    Limit when the module collection is added to the collections list

--- lib/ansible/executor/task_executor.py
+++ lib/ansible/executor/task_executor.py
@@ -1020,13 +1020,16 @@ class TaskExecutor:
         module_collection, separator, module_name = self._task.action.rpartition(".")
         module_prefix = module_name.split('_')[0]
 
-        collections = [module_collection] + self._task.collections
+        collections = self._task.collections
+        if module_collection and module_collection not in collections:
+            network_collections = [module_collection] + collections
 
         # let action plugin override module, fallback to 'normal' action plugin otherwise
         if self._shared_loader_obj.action_loader.has_plugin(self._task.action, collection_list=collections):
             handler_name = self._task.action
-        elif all((module_prefix in C.NETWORK_GROUP_MODULES, self._shared_loader_obj.action_loader.has_plugin(module_prefix, collection_list=collections))):
+        elif all((module_prefix in C.NETWORK_GROUP_MODULES, self._shared_loader_obj.action_loader.has_plugin(module_prefix, collection_list=network_collections))):
             handler_name = module_prefix
+            collections = network_collections
         else:
             # FUTURE: once we're comfortable with collections impl, preface this action with ansible.builtin so it can't be hijacked
             handler_name = 'normal'
