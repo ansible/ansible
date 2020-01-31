422f11d1a61a669a1c7c1261b24b08524425b515
Jordan Borean <jborean93@gmail.com>
2020-01-31 11:48:04 +1000

    Fix up Python sanity issues

--- test/integration/targets/win_unzip/files/create_zip.py
+++ test/integration/targets/win_unzip/files/create_zip.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Copyright: (c) 2019, Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 import sys
 import tempfile
 import zipfile
@@ -20,4 +26,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
