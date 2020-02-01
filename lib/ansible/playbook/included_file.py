ansible/ansible

commit 50905b980d8b119d8a98e2b783c954e3c61d87be

Support item label in v2_playbook_on_include (#42478)
* Support item label in v2_playbook_on_include. Fixes #42301

* test fixes

lib/ansible/playbook/included_file.py
@@ -93,6 +93,10 @@ class IncludedFile:
                         task_vars[loop_var] = include_variables[loop_var] = include_result[loop_var]
                     if index_var and index_var in include_result:
                         task_vars[index_var] = include_variables[index_var] = include_result[index_var]
+                    if '_ansible_item_label' in include_result:
+                        task_vars['_ansible_item_label'] = include_variables['_ansible_item_label'] = include_result['_ansible_item_label']
+                    if original_task.no_log and '_ansible_no_log' not in include_variables:
+                        task_vars['_ansible_no_log'] = include_variables['_ansible_no_log'] = original_task.no_log
 
                     if original_task.action in ('include', 'include_tasks'):
                         include_file = None

lib/ansible/plugins/callback/default.py
@@ -269,6 +269,8 @@ class CallbackModule(CallbackBase):
 
     def v2_playbook_on_include(self, included_file):
         msg = 'included: %s for %s' % (included_file._filename, ", ".join([h.name for h in included_file._hosts]))
+        if 'item' in included_file._args:
+            msg += " => (item=%s)" % (self._get_item_label(included_file._args),)
         self._display.display(msg, color=C.COLOR_SKIP)
 
     def v2_playbook_on_stats(self, stats):

test/integration/targets/include_import/runme.sh
@@ -76,6 +76,6 @@ fi
 
 # Test that duplicate items in loop are not deduped
 ANSIBLE_STRATEGY='linear' ansible-playbook tasks/test_include_dupe_loop.yml -i ../../inventory "$@" | tee test_include_dupe_loop.out
-test "$(grep -c 'item=foo' test_include_dupe_loop.out)" = 3
+test "$(grep -c '"item=foo"' test_include_dupe_loop.out)" = 3
 ANSIBLE_STRATEGY='free' ansible-playbook tasks/test_include_dupe_loop.yml -i ../../inventory "$@" | tee test_include_dupe_loop.out
-test "$(grep -c 'item=foo' test_include_dupe_loop.out)" = 3
+test "$(grep -c '"item=foo"' test_include_dupe_loop.out)" = 3

commit dc9fa0708111d8631e367f3f9aeddcd95cf41b8b

included_file: Fix printing item label
I don't think this ever worked before…

With the simple playbook like:

```yaml
---
hosts: localhost
gather_facts: no
tasks:
  - include_tasks: test.yml
    loop: [1, 2]
  - include_tasks: test.yml
    loop: [1, 2]
    no_log: yes
```

Before:

```
…
included: /var/tmp/test.yml for localhost
included: /var/tmp/test.yml for localhost
…
included: /var/tmp/test.yml for localhost
included: /var/tmp/test.yml for localhost
…
```

After:

```
…
included: /var/tmp/test.yml for localhost => (item=1)
included: /var/tmp/test.yml for localhost => (item=2)
…
included: /var/tmp/test.yml for localhost => (item=(censored due to no_log))
included: /var/tmp/test.yml for localhost => (item=(censored due to no_log))
…
```

Fixes: 50905b980d8b119d8a98e2b783c954e3c61d87be
Signed-off-by: Igor Raits <i.gnatenko.brain@gmail.com>

lib/ansible/playbook/included_file.py
@@ -94,11 +94,11 @@ class IncludedFile:
                     if index_var and index_var in include_result:
                         task_vars[index_var] = special_vars[index_var] = include_result[index_var]
                     if '_ansible_item_label' in include_result:
-                        task_vars['_ansible_item_label'] = special_vars['_ansible_item_label'] = include_result['_ansible_item_label']
+                        include_args['_ansible_item_label'] = special_vars['_ansible_item_label'] = include_result['_ansible_item_label']
                     if 'ansible_loop' in include_result:
                         task_vars['ansible_loop'] = special_vars['ansible_loop'] = include_result['ansible_loop']
                     if original_task.no_log and '_ansible_no_log' not in include_args:
-                        task_vars['_ansible_no_log'] = special_vars['_ansible_no_log'] = original_task.no_log
+                        include_args['_ansible_no_log'] = special_vars['_ansible_no_log'] = original_task.no_log
 
                     # get search path for this task to pass to lookup plugins that may be used in pathing to
                     # the included file

lib/ansible/plugins/callback/default.py
@@ -350,7 +350,7 @@ class CallbackModule(CallbackBase):
 
     def v2_playbook_on_include(self, included_file):
         msg = 'included: %s for %s' % (included_file._filename, ", ".join([h.name for h in included_file._hosts]))
-        if 'item' in included_file._args:
+        if '_ansible_item_label' in included_file._args:
             msg += " => (item=%s)" % (self._get_item_label(included_file._args),)
         self._display.display(msg, color=C.COLOR_SKIP)
 