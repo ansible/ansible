87034a385b105f302cfac956a08140d4ab6afde1
ksagle77 <34070189+ksagle77@users.noreply.github.com>
2020-02-06 11:21:00 -0500

    Update lib/ansible/modules/cloud/amazon/ecs_taskdefinition.py
    Co-Authored-By: Mark Chappell <mchappel@redhat.com>

--- lib/ansible/modules/cloud/amazon/ecs_taskdefinition.py
+++ lib/ansible/modules/cloud/amazon/ecs_taskdefinition.py
@@ -62,7 +62,6 @@ options:
             - C(awsvpc) mode was added in Ansible 2.5
             - Windows containers must use I(network_mode=default), which will utilize docker NAT networking.
             - Setting I(network_mode=default) for a Linux container will use bridge mode.
-              use bridge mode.
         required: false
         default: bridge
         choices: [ 'default', 'bridge', 'host', 'none', 'awsvpc' ]
