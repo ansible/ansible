d32b5ec2f96c5b9e6819c8ef2b517e37bd6deeff
Sam Doran <sdoran@redhat.com>
2020-02-06 16:55:15 -0500

    Add unit tests for listing all collections and specific collection
    - Create fixture for creating test objects
    - Add function for controlling os.path.isdir results

--- test/units/cli/galaxy/test_execute_list.py
+++ test/units/cli/galaxy/test_execute_list.py
@@ -5,43 +5,79 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-import ansible
+import os
 import pytest
 
-from distutils.version import LooseVersion
-
 from ansible import context
-from ansible.module_utils._text import to_native
 from ansible.cli.galaxy import GalaxyCLI
+from ansible.galaxy.collection import CollectionRequirement
+from ansible.module_utils._text import to_native
 
 
-class FakeCollection:
-    def __init__(self, namespace, name, b_path, api, versions, requirement, force, parent=None, metadata=None,
-                 files=None, skip=False):
+def path_exists(path):
+    if to_native(path) == 'nope':
+        return False
+    else:
+        return os.path.isdir(path)
 
-        self.namespace = namespace
-        self.name = name
-        self.b_path = b_path
-        self.api = api
-        self.versions = set(versions)
-        self.force = force
-        self.skip = skip
-        self.required_by = []
-        self._metadata = metadata
-        self._files = files
 
-    def __str__(self):
-        return to_native("%s.%s" % (self.namespace, self.name))
+def cliargs(collections_paths=None, collection_name=None):
+    if collections_paths is None:
+        collections_paths = ['~/root/.ansible/collections', '/usr/share/ansible/collections']
 
-    def __unicode__(self):
-        return u"%s.%s" % (self.namespace, self.name)
+    context.CLIARGS._store = {
+        'collections_path': collections_paths,
+        'collection': collection_name,
+    }
 
-    @property
-    def latest_version(self):
-        try:
-            return max([v for v in self.versions if v != '*'], key=LooseVersion)
-        except ValueError:  # ValueError: max() arg is an empty sequence
-            return '*'
+
+@pytest.fixture
+def mock_collection_objects(mocker):
+    mocker.patch('ansible.cli.galaxy.GalaxyCLI._resolve_path', side_effect=['/root/.ansible/collections', '/usr/share/ansible/collections'])
+    mocker.patch('ansible.cli.galaxy.validate_collection_path',
+                 side_effect=['/root/.ansible/collections/ansible_collections', '/usr/share/ansible/collections/ansible_collections'])
+
+    collection_args = (
+        (
+            'sandwiches',
+            'ham',
+            b'/usr/share/ansible/collections/ansible_collections/sandwiches/ham',
+            mocker.Mock(),
+            ['1.0.0'],
+            '1.0.0',
+            False,
+        ),
+        (
+            'sandwiches',
+            'pbj',
+            b'/usr/share/ansible/collections/ansible_collections/sandwiches/pbj',
+            mocker.Mock(),
+            ['1.0.0', '1.5.0'],
+            '1.0.0',
+            False,
+        ),
+        (
+            'sandwiches',
+            'pbj',
+            b'/root/.ansible/collections/ansible_collections/sandwiches/pbj',
+            mocker.Mock(),
+            ['1.0.0', '1.5.0'],
+            '1.5.0',
+            False,
+        ),
+        (
+            'sandwiches',
+            'reuben',
+            b'/root/.ansible/collections/ansible_collections/sandwiches/pbj',
+            mocker.Mock(),
+            ['1.0.0', '2.5.0'],
+            '2.5.0',
+            False,
+        ),
+    )
+
+    mocker.patch('ansible.cli.galaxy.find_existing_collections', return_value=[CollectionRequirement(*cargs) for cargs in collection_args])
+    mocker.patch('ansible.galaxy.collection.CollectionRequirement.from_path', return_value=CollectionRequirement(*collection_args[0]))
 
 
 def test_execute_list_role_called(mocker):
@@ -70,65 +106,62 @@ def test_execute_list_collection_called(mocker):
     assert execute_list_collection_mock.call_count == 1
 
 
-def test_execute_list_collection_all(mocker):
-    gc = GalaxyCLI(['ansible-galaxy', 'collection', 'list'])
-    context.CLIARGS._store = {
-        'collections_path': ['~/root/.ansible/collections', '/usr/share/ansible/collections'],
-        'collection': None,
-    }
-
-    mocker.patch('ansible.cli.galaxy.GalaxyCLI._resolve_path', return_value='/usr/share/ansible/collections')
+def test_execute_list_collection_all(mocker, capsys, mock_collection_objects):
+    cliargs()
     mocker.patch('os.path.exists', return_value=True)
     mocker.patch('os.path.isdir', return_value=True)
-    mocker.patch('ansible.cli.galaxy.validate_collection_path', return_value='/usr/share/ansible/collections/ansible_collections')
-
-    collection_args = (
-        (
-            'sandwiches',
-            'ham',
-            b'/usr/share/ansible/collections/ansible_collections/sandwiches/ham',
-            mocker.Mock(),
-            ['1.0.0'],
-            {},
-            False,
-        ),
-        (
-            'sandwiches',
-            'pbj',
-            b'/usr/share/ansible/collections/ansible_collections/sandwiches/pbj',
-            mocker.Mock(),
-            ['1.5.0'],
-            {},
-            False,
-        ),
-    )
-    mocker.patch('ansible.cli.galaxy.find_existing_collections', return_value=[FakeCollection(*cargs) for cargs in collection_args])
-
-    mock_display = mocker.patch.object(ansible.utils.display.Display, 'display', return_value=None)
-    mock_display_vvv = mocker.patch.object(ansible.utils.display.Display, 'vvv', return_value=None)
+    gc = GalaxyCLI(['ansible-galaxy', 'collection', 'list'])
     gc.execute_list_collection()
 
-    header_lines = mock_display.call_args_list[0][0][0].splitlines()
+    out, err = capsys.readouterr()
+    out_lines = out.splitlines()
 
-    assert mock_display.call_count == 6
-    assert header_lines[0] == ''
-    assert header_lines[1] == '# /usr/share/ansible/collections/ansible_collections'
-    assert header_lines[2] == 'Collection     Version'
-    assert header_lines[3] == '-------------- -------'
-    assert mock_display.call_args_list[1] == (('sandwiches.ham 1.0.0',),)
-    assert mock_display.call_args_list[2] == (('sandwiches.pbj 1.5.0',),)
+    assert len(out_lines) == 16
+    assert out_lines[0] == ''
+    assert out_lines[1] == '# /root/.ansible/collections/ansible_collections'
+    assert out_lines[2] == 'Collection        Version'
+    assert out_lines[3] == '----------------- -------'
+    assert out_lines[4] == 'sandwiches.ham    1.0.0'
+    assert out_lines[5] == 'sandwiches.pbj    1.0.0'
+    assert out_lines[6] == 'sandwiches.pbj    1.5.0'
+    assert out_lines[7] == 'sandwiches.reuben 2.5.0'
+    assert out_lines[8] == ''
+    assert out_lines[9] == '# /usr/share/ansible/collections/ansible_collections'
+    assert out_lines[10] == 'Collection        Version'
+    assert out_lines[11] == '----------------- -------'
+    assert out_lines[12] == 'sandwiches.ham    1.0.0'
+    assert out_lines[13] == 'sandwiches.pbj    1.0.0'
+    assert out_lines[14] == 'sandwiches.pbj    1.5.0'
+    assert out_lines[15] == 'sandwiches.reuben 2.5.0'
 
-    assert mock_display_vvv.call_count == 2
-    assert mock_display_vvv.call_args == (('Searching /usr/share/ansible/collections/ansible_collections for collections',),)
 
+def test_execute_list_collection_specific(mocker, capsys, mock_collection_objects):
+    collection_name = 'sandwiches.ham'
 
-def test_execute_list_collection_specific():
+    cliargs(collection_name=collection_name)
+    mocker.patch('os.path.exists', side_effect=path_exists)
+    mocker.patch('os.path.isdir', return_value=True)
+    mocker.patch('ansible.galaxy.collection.validate_collection_name', collection_name)
+    mocker.patch('ansible.cli.galaxy.to_bytes', side_effect=[b'~/root/.ansible/collections/ansible_collections/sandwiches/ham', b'nope'])
+    mocker.patch('ansible.cli.galaxy._get_collection_widths', return_value=(14, 5))
+
     gc = GalaxyCLI(['ansible-galaxy', 'collection', 'list', 'sandwiches.ham'])
+    gc.execute_list_collection()
+
+    out, err = capsys.readouterr()
+    out_lines = out.splitlines()
+
+    assert len(out_lines) == 5
+    assert out_lines[0] == ''
+    assert out_lines[1] == '# /root/.ansible/collections/ansible_collections'
+    assert out_lines[2] == 'Collection     Version'
+    assert out_lines[3] == '-------------- -------'
+    assert out_lines[4] == 'sandwiches.ham 1.0.0'
 
 
-def test_execute_list_collection_specific_invalid_fqcn():
+def test_execute_list_collection_specific_invalid_fqcn(capsys, mock_collection_objects):
     gc = GalaxyCLI(['ansible-galaxy', 'collection', 'list', 'bad'])
 
 
-def test_execute_list_collection_invalid_path():
+def test_execute_list_collection_invalid_path(capsys, mock_collection_objects):
     gc = GalaxyCLI(['ansible-galaxy', 'collection', 'list', '-p', 'nope'])
