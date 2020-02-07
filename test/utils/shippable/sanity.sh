2e913539da9f4fe531505112c7c712a4ce960ff1
Matt Clay <matt@mystile.com>
2020-02-07 12:31:22 -0800

    Improve sanity test handling in CI after migration.

--- test/utils/shippable/sanity.sh
+++ test/utils/shippable/sanity.sh
@@ -16,10 +16,21 @@ fi
 case "${group}" in
     1) options=(--skip-test pylint --skip-test ansible-doc --skip-test docs-build --skip-test package-data --skip-test validate-modules) ;;
     2) options=(                   --test      ansible-doc --test      docs-build --test      package-data) ;;
-    3) options=(--test pylint --exclude test/units/ --exclude lib/ansible/module_utils/ --exclude lib/ansible/modules/network/) ;;
-    4) options=(--test pylint           test/units/           lib/ansible/module_utils/           lib/ansible/modules/network/) ;;
+    3) options=(--test pylint --exclude test/units/ --exclude lib/ansible/module_utils/) ;;
+    4) options=(--test pylint           test/units/           lib/ansible/module_utils/) ;;
     5) options=(                                                                                                --test validate-modules) ;;
 esac
+
+# allow collection migration sanity tests for groups 3 and 4 to pass without updating this script during migration
+network_path="lib/ansible/modules/network/"
+
+if [ -d "${network_path}" ]; then
+    if [ "${group}" -eq 3 ]; then
+        options+=(--exclude "${network_path}")
+    elif [ "${group}" -eq 4 ]; then
+        options+=("${network_path}")
+    fi
+fi
 
 # shellcheck disable=SC2086
 ansible-test sanity --color -v --junit ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
