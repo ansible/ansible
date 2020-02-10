7e0a40c6ab2e3246398429e37d3397933502e94b
GomathisevliS <gosriniv@localhost.localdomain>
2020-02-10 11:31:17 -0500

    addressed review comments

--- lib/ansible/module_utils/network/eos/config/acls/acls.py
+++ lib/ansible/module_utils/network/eos/config/acls/acls.py
@@ -167,7 +167,7 @@ class Acls(ConfigBase):
                   to the desired configuration
         """
         commands = []
-        if self.state in ('merged', 'replaced', 'overridden') and not want:
+        if self.state in ('merged', 'replaced', 'overridden', 'rendered') and not want:
             self._module.fail_json(msg='value of config parameter must not be empty for state {0}'.format(self.state))
         state = self._module.params['state']
         if state == 'overridden':
