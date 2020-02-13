6f2c3b5a44972f9997f15e5535c3612ea0fb2f18
Pradeep Gorthi <32436276+deepgorthi@users.noreply.github.com>
2020-02-13 14:38:27 -0500

    Updating UCS Platform Emulator Link

--- lib/ansible/modules/remote_management/ucs/ucs_uuid_pool.py
+++ lib/ansible/modules/remote_management/ucs/ucs_uuid_pool.py
@@ -16,7 +16,7 @@ module: ucs_uuid_pool
 short_description: Configures server UUID pools on Cisco UCS Manager
 description:
 - Configures server UUID pools and UUID blocks on Cisco UCS Manager.
-- Examples can be used with the L(UCS Platform Emulator,https://communities.cisco.com/ucspe).
+- Examples can be used with the L(UCS Platform Emulator,https://bit.ly/38w8JCk).
 extends_documentation_fragment: ucs
 options:
   state:
