b46747c0d5e394b39bc1fae1b88eedd254aa6ad6
Sandra McCann <samccann@redhat.com>
2020-02-14 12:20:39 -0500

    Apply suggestions from code review
    Co-Authored-By: Alicia Cozine <879121+acozine@users.noreply.github.com>

--- lib/ansible/modules/net_tools/basics/uri.py
+++ lib/ansible/modules/net_tools/basics/uri.py
@@ -315,14 +315,15 @@ RETURN = r'''
 # The return information includes all the HTTP headers in lower-case.
 content:
   description: The content of the requested resource if return_content is true
-  returned: When return_content is TRUE. 
+  returned: When return_content is TRUE.
   type: string
-  sample: <!DOCTYPE html>
-<html>
-<body>
-Hello world.
-</body>
-</html>
+  sample: 
+     <!DOCTYPE html>
+     <html>
+     <body>
+     Hello world.
+     </body>
+     </html>
 elapsed:
   description: The number of seconds that elapsed while performing the download
   returned: on success
@@ -347,7 +348,7 @@ url:
   description: The actual URL used for the request
   returned: always
   type: str
-  sample: https://www.ansible.com/ 
+  sample: https://www.ansible.com/
 '''
 
 import cgi
