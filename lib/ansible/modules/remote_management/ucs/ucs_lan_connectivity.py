7129a8a6399abdda5de33981317b691e7fccb92c
John McDonough <movinalot@gmail.com>
2020-02-14 14:11:15 -0500

    UCS Emulator link to Doc Fragment use cs.co domain

--- lib/ansible/modules/remote_management/ucs/ucs_lan_connectivity.py
+++ lib/ansible/modules/remote_management/ucs/ucs_lan_connectivity.py
@@ -16,7 +16,6 @@ module: ucs_lan_connectivity
 short_description: Configures LAN Connectivity Policies on Cisco UCS Manager
 description:
 - Configures LAN Connectivity Policies on Cisco UCS Manager.
-- Examples can be used with the L(UCS Platform Emulator,https://bit.ly/38w8JCk).
 extends_documentation_fragment: ucs
 options:
   state:
--- lib/ansible/modules/remote_management/ucs/ucs_wwn_pool.py
+++ lib/ansible/modules/remote_management/ucs/ucs_wwn_pool.py
@@ -16,7 +16,6 @@ module: ucs_wwn_pool
 short_description: Configures WWNN or WWPN pools on Cisco UCS Manager
 description:
 - Configures WWNNs or WWPN pools on Cisco UCS Manager.
-- Examples can be used with the L(UCS Platform Emulator,https://bit.ly/38w8JCk).
 extends_documentation_fragment: ucs
 options:
   state:
