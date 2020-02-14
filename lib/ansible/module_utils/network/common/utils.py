291a8ebb8dcba78e166e3c14f6fe710480f9d0be
Nilashish Chakraborty <nilashishchakraborty8@gmail.com>
2020-02-14 09:46:59 +0530

    Fix tracebacks with search_obj_in_list (#67389)
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- lib/ansible/module_utils/network/common/utils.py
+++ lib/ansible/module_utils/network/common/utils.py
@@ -596,10 +596,12 @@ def validate_config(spec, data):
 
 
 def search_obj_in_list(name, lst, key='name'):
-    for item in lst:
-        if item.get(key) == name:
-            return item
-    return None
+    if not lst:
+        return None
+    else:
+        for item in lst:
+            if item.get(key) == name:
+                return item
 
 
 class Template:
