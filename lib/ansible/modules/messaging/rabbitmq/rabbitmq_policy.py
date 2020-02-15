fc3f69ca6f5606377e7adba66e73116386da1ac0
bitchkat <kjh@flyballdogs.com>
2020-02-15 08:42:32 -0600

    support rabbitmq 3.8.x in version check (#66855)
    * support rabbitmq 3.8.x in version check
    
    * Removed extraneous white space

--- lib/ansible/modules/messaging/rabbitmq/rabbitmq_policy.py
+++ lib/ansible/modules/messaging/rabbitmq/rabbitmq_policy.py
@@ -125,7 +125,13 @@ class RabbitMqPolicy(object):
     def _rabbit_version(self):
         status = self._exec(['status'], True, False, False)
 
+        # 3.7.x erlang style output
         version_match = re.search('{rabbit,".*","(?P<version>.*)"}', status)
+        if version_match:
+            return Version(version_match.group('version'))
+
+        # 3.8.x style ouput
+        version_match = re.search('RabbitMQ version: (?P<version>.*)', status)
         if version_match:
             return Version(version_match.group('version'))
 
