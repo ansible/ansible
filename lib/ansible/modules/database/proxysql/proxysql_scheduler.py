85674d45d0284d7279c5ef56f23ba6194491f08f
Max Bubenick <maxbube@gmail.com>
2020-02-17 09:55:44 -0300

    Fix merge conflicts

--- lib/ansible/modules/database/proxysql/proxysql_scheduler.py
+++ lib/ansible/modules/database/proxysql/proxysql_scheduler.py
@@ -354,11 +354,11 @@ def main():
 
     cursor = None
     try:
-        cursor = mysql_connect(module,
-                               login_user,
-                               login_password,
-                               config_file,
-                               cursor_class=mysql_driver.cursors.DictCursor)
+        cursor, db_conn = mysql_connect(module,
+                                        login_user,
+                                        login_password,
+                                        config_file,
+                                        cursor_class='DictCursor')
     except mysql_driver.Error as e:
         module.fail_json(
             msg="unable to connect to ProxySQL Admin Module.. %s" % to_native(e)
