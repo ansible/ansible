a13b013ac8780b39b8b1223f5b8da28149ec31c0
Markus Bergholz <markuman@gmail.com>
2020-02-17 19:27:50 +0100

    Update lib/ansible/modules/source_control/gitlab/gitlab_project_variable.py
    Co-Authored-By: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/source_control/gitlab/gitlab_project_variable.py
+++ lib/ansible/modules/source_control/gitlab/gitlab_project_variable.py
@@ -53,7 +53,7 @@ options:
   vars:
     description:
       - When the list element is a simple key-value pair, masked and protected will be set to false.
-      - Wnen provided a dict with the keys "value", "masked" and "protected", you have full control about if a value should be masked, protected or both.
+      - When the list element is a dict with the keys "value", "masked" and "protected", user can have full control about if a value should be masked, protected or both.
       - Wehn you masked a value, the value must be base64 compliant and has at least a length of 8 characters.
     default: {}
     type: dict
