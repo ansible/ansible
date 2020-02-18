02ec348e9b88d6a6bdc4e3cdf869e13ff3ce9316
xuxiaowei0512 <7806018072@qq.com>
2020-02-18 12:33:31 +0800

    fix space errors

--- lib/ansible/modules/network/cloudengine/ce_acl.py
+++ lib/ansible/modules/network/cloudengine/ce_acl.py
@@ -42,7 +42,7 @@ options:
             - Specify desired state of the resource.
         default: present
         choices: ['present','absent','delete_acl']
-		type: str
+        type: str
     acl_name:
         description:
             - ACL number or name.
@@ -50,71 +50,71 @@ options:
               For a named rule group, the value is a string of 1 to 32 case-sensitive characters starting
               with a letter, spaces not supported.
         required: true
-		type: str
+        type: str
     acl_num:
         description:
             - ACL number.
               The value is an integer ranging from 2000 to 2999.
-		type: int
+        type: int
     acl_step:
         description:
             - ACL step.
               The value is an integer ranging from 1 to 20. The default value is 5.
-		type: int
+        type: int
     acl_description:
         description:
             - ACL description.
               The value is a string of 1 to 127 characters.
-		type: str
+        type: str
     rule_name:
         description:
             - Name of a basic ACL rule.
               The value is a string of 1 to 32 characters.
               The value is case-insensitive, and cannot contain spaces or begin with an underscore (_).
-		type: str
+        type: str
     rule_id:
         description:
             - ID of a basic ACL rule in configuration mode.
               The value is an integer ranging from 0 to 4294967294.
-		type: int
+        type: int
     rule_action:
         description:
             - Matching mode of basic ACL rules.
         choices: ['permit','deny']
-		type: str
+        type: str
     source_ip:
         description:
             - Source IP address.
               The value is a string of 0 to 255 characters.The default value is 0.0.0.0.
               The value is in dotted decimal notation.
-		type: str
+        type: str
     src_mask:
         description:
             - Mask of a source IP address.
               The value is an integer ranging from 1 to 32.
-		type: str
+        type: str
     frag_type:
         description:
             - Type of packet fragmentation.
         choices: ['fragment', 'clear_fragment']
-		type: str
+        type: str
     vrf_name:
         description:
             - VPN instance name.
               The value is a string of 1 to 31 characters.The default value is _public_.
-		type: str
+        type: str
     time_range:
         description:
             - Name of a time range in which an ACL rule takes effect.
               The value is a string of 1 to 32 characters.
               The value is case-insensitive, and cannot contain spaces. The name must start with an uppercase
               or lowercase letter. In addition, the word "all" cannot be specified as a time range name.
-		type: str
+        type: str
     rule_description:
         description:
             - Description about an ACL rule.
               The value is a string of 1 to 127 characters.
-		type: str
+        type: str
     log_flag:
         description:
             - Flag of logging matched data packets.
