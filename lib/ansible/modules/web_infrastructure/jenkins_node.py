eb09ac94cc75f22be50b62b57d4d45b22191f23e
nalin_garg <nalin_garg@apple.com>
2020-02-19 18:26:16 -0800

    adding boiler plate code.

--- lib/ansible/modules/web_infrastructure/jenkins_node.py
+++ lib/ansible/modules/web_infrastructure/jenkins_node.py
@@ -3,7 +3,8 @@
 # Copyright: (c) Ansible Project
 #
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
