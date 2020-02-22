7b35617abd0ec5b7a3277aaa26693f4a9ee72495
Mark Chappell <mchappel@redhat.com>
2020-02-22 07:03:05 +0100

    Make sure we don't explode if the botocore version's too old

--- lib/ansible/modules/cloud/amazon/s3_bucket.py
+++ lib/ansible/modules/cloud/amazon/s3_bucket.py
@@ -438,6 +438,9 @@ def put_bucket_versioning(s3_client, bucket_name, required_versioning):
 
 @AWSRetry.exponential_backoff(max_delay=120, catch_extra_error_codes=['NoSuchBucket'])
 def get_bucket_encryption(s3_client, bucket_name):
+    if not hasattr(s3_client, "get_bucket_encryption"):
+        return None
+
     try:
         result = s3_client.get_bucket_encryption(Bucket=bucket_name)
         return result.get('ServerSideEncryptionConfiguration', {}).get('Rules', [])[0].get('ApplyServerSideEncryptionByDefault')
