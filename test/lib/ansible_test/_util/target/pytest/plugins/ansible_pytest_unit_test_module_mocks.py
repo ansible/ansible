fd96f91ceebdaadb04a0879c3c84ce4162584978
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2020-02-24 16:42:22 +0100

    Mv units.module_utils.conftest to a pytest plugin

--- test/lib/ansible_test/_util/target/pytest/plugins/ansible_pytest_unit_test_module_mocks.py
+++ test/lib/ansible_test/_util/target/pytest/plugins/ansible_pytest_unit_test_module_mocks.py
@@ -1,17 +1,23 @@
-# Copyright (c) 2017 Ansible Project
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2017–2021 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 """Pytest fixtures for mocking Ansible modules."""
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+from io import BytesIO
 import json
+import sys
 
 import pytest
 
-from ansible.module_utils.six import string_types
-from ansible.module_utils.common.text.converters import to_bytes
+import ansible.module_utils.basic
+from ansible.module_utils.six import PY3, string_types
 from ansible.module_utils.six.moves.collections_abc import MutableMapping
+from ansible.module_utils.common.text.converters import to_bytes
 
 
 @pytest.fixture
@@ -31,3 +37,60 @@ def patch_ansible_module(request, mocker):
         raise Exception('Malformed data to the patch_ansible_module pytest fixture')
 
     mocker.patch('ansible.module_utils.basic._ANSIBLE_ARGS', to_bytes(args))
+
+
+@pytest.fixture
+def stdin(mocker, request):
+    old_args = ansible.module_utils.basic._ANSIBLE_ARGS
+    ansible.module_utils.basic._ANSIBLE_ARGS = None
+    old_argv = sys.argv
+    sys.argv = ['ansible_unittest']
+
+    if isinstance(request.param, string_types):
+        args = request.param
+    elif isinstance(request.param, MutableMapping):
+        if 'ANSIBLE_MODULE_ARGS' not in request.param:
+            request.param = {'ANSIBLE_MODULE_ARGS': request.param}
+        if '_ansible_remote_tmp' not in request.param['ANSIBLE_MODULE_ARGS']:
+            request.param['ANSIBLE_MODULE_ARGS']['_ansible_remote_tmp'] = '/tmp'
+        if '_ansible_keep_remote_files' not in request.param['ANSIBLE_MODULE_ARGS']:
+            request.param['ANSIBLE_MODULE_ARGS']['_ansible_keep_remote_files'] = False
+        args = json.dumps(request.param)
+    else:
+        raise Exception('Malformed data to the stdin pytest fixture')
+
+    fake_stdin = BytesIO(to_bytes(args, errors='surrogate_or_strict'))
+    if PY3:
+        mocker.patch('ansible.module_utils.basic.sys.stdin', mocker.MagicMock())
+        mocker.patch('ansible.module_utils.basic.sys.stdin.buffer', fake_stdin)
+    else:
+        mocker.patch('ansible.module_utils.basic.sys.stdin', fake_stdin)
+
+    yield fake_stdin
+
+    ansible.module_utils.basic._ANSIBLE_ARGS = old_args
+    sys.argv = old_argv
+
+
+# pylint: disable=invalid-name,redefined-outer-name,unused-argument
+@pytest.fixture
+def am(stdin, request):
+    old_args = ansible.module_utils.basic._ANSIBLE_ARGS
+    ansible.module_utils.basic._ANSIBLE_ARGS = None
+    old_argv = sys.argv
+    sys.argv = ['ansible_unittest']
+
+    argspec = {}
+    if hasattr(request, 'param'):
+        if isinstance(request.param, dict):
+            argspec = request.param
+
+    ans_mod = ansible.module_utils.basic.AnsibleModule(
+        argument_spec=argspec,
+    )
+    ans_mod._name = 'ansible_unittest'  # pylint: disable=protected-access
+
+    yield ans_mod
+
+    ansible.module_utils.basic._ANSIBLE_ARGS = old_args
+    sys.argv = old_argv
