0625d8dbbb8142a20065b4c488e8949444888ad6
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2020-02-24 17:52:58 +0100

    Refactor patch_ansible_module dict construction

--- test/lib/ansible_test/_util/target/pytest/plugins/ansible_pytest_unit_test_module_mocks.py
+++ test/lib/ansible_test/_util/target/pytest/plugins/ansible_pytest_unit_test_module_mocks.py
@@ -23,23 +23,28 @@ from ansible.module_utils.common.text.converters import to_bytes
 @pytest.fixture
 def patch_ansible_module(monkeypatch, request):
     """Monkey-patch given Ansible module."""
-    if isinstance(request.param, string_types):
-        args = request.param
-    elif isinstance(request.param, MutableMapping):
-        if 'ANSIBLE_MODULE_ARGS' not in request.param:
-            request.param = {'ANSIBLE_MODULE_ARGS': request.param}
-        if '_ansible_remote_tmp' not in request.param['ANSIBLE_MODULE_ARGS']:
-            request.param['ANSIBLE_MODULE_ARGS']['_ansible_remote_tmp'] = '/tmp'
-        if '_ansible_keep_remote_files' not in request.param['ANSIBLE_MODULE_ARGS']:
-            request.param['ANSIBLE_MODULE_ARGS']['_ansible_keep_remote_files'] = False
-        args = json.dumps(request.param)
-    else:
-        raise Exception('Malformed data to the patch_ansible_module pytest fixture')
+    inp_args = request.param
 
-    monkeypatch.setattr(
-        ansible.module_utils.basic, '_ANSIBLE_ARGS',
-        to_bytes(args),
-    )
+    module_args_defaults = {
+        '_ansible_keep_remote_files': False,
+        '_ansible_remote_tmp': '/tmp',
+    }
+
+    if isinstance(inp_args, string_types):
+        args = inp_args
+    elif isinstance(inp_args, MutableMapping):
+        mod_args = inp_args.get('ANSIBLE_MODULE_ARGS', inp_args)
+        mod_args = dict(module_args_defaults, **mod_args)
+        args = json.dumps({'ANSIBLE_MODULE_ARGS': mod_args})
+    else:
+        raise Exception(
+            'Malformed data to the `patch_ansible_module` '
+            'pytest fixture',
+        )
+
+    args = to_bytes(args)
+
+    monkeypatch.setattr(ansible.module_utils.basic, '_ANSIBLE_ARGS', args)
 
 
 @pytest.fixture
