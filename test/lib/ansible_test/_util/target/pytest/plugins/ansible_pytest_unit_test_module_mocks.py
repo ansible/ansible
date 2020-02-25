30e5b7e39a6fbcc499d333f36e6c4dfb9e35ef3a
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2020-02-25 16:55:50 +0100

    Use stdin fixture instead of patch_ansible_module

--- test/lib/ansible_test/_util/target/pytest/plugins/ansible_pytest_unit_test_module_mocks.py
+++ test/lib/ansible_test/_util/target/pytest/plugins/ansible_pytest_unit_test_module_mocks.py
@@ -19,33 +19,6 @@ from collections.abc import MutableMapping
 
 
 @pytest.fixture
-def patch_ansible_module(monkeypatch, request):
-    """Monkey-patch given Ansible module."""
-    inp_args = request.param
-
-    module_args_defaults = {
-        '_ansible_keep_remote_files': False,
-        '_ansible_remote_tmp': '/tmp',
-    }
-
-    if isinstance(inp_args, str):
-        args = inp_args
-    elif isinstance(inp_args, MutableMapping):
-        mod_args = inp_args.get('ANSIBLE_MODULE_ARGS', inp_args)
-        mod_args = dict(module_args_defaults, **mod_args)
-        args = json.dumps({'ANSIBLE_MODULE_ARGS': mod_args})
-    else:
-        raise Exception(
-            'Malformed data to the `patch_ansible_module` '
-            'pytest fixture',
-        )
-
-    args = to_bytes(args, errors='surrogate_or_strict')
-
-    monkeypatch.setattr(ansible.module_utils.basic, '_ANSIBLE_ARGS', args)
-
-
-@pytest.fixture
 def stdin(mocker, monkeypatch, request):
     """Patch and return stdin buffer with module args."""
     monkeypatch.setattr(ansible.module_utils.basic, '_ANSIBLE_ARGS', None)
