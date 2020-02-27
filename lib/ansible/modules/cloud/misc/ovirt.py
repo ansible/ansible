75a8f4af87cd9299b422367a653e2b817ec5a901
mnecas <necas.marty@gmail.com>
2020-02-27 21:56:07 +0100

    correct deprecate option

--- lib/ansible/modules/cloud/misc/ovirt.py
+++ lib/ansible/modules/cloud/misc/ovirt.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
+                    'status': ['deprecated'],
                     'supported_by': 'community'}
 
 DOCUMENTATION = '''
@@ -16,6 +16,10 @@ module: ovirt
 author:
 - Vincent Van der Kussen (@vincentvdk)
 short_description: oVirt/RHEV platform management
+deprecated:
+    removed_in: "2.10"
+    why: Compatible only with deprecated version of ovirt. Was not migrated to ovirt.ovirt_collection.
+    alternative: Use M(ovirt_vm) instead
 description:
     - This module only supports oVirt/RHEV version 3. A newer module M(ovirt_vm) supports oVirt/RHV version 4.
     - Allows you to create new instances, either from scratch or an image, in addition to deleting or stopping instances on the oVirt/RHEV platform.
