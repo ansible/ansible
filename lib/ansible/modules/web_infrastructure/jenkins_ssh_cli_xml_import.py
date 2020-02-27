7745f509309f6bb2cdaa07a586dcf0657e9d6bbb
Paul Wetering <paul.wetering@hcs-company.com>
2020-02-27 16:19:46 +0100

    Fixed sanity: __future__ boilerplate

--- lib/ansible/modules/web_infrastructure/jenkins_ssh_cli_xml_import.py
+++ lib/ansible/modules/web_infrastructure/jenkins_ssh_cli_xml_import.py
@@ -4,6 +4,8 @@
 # Copyright: (c) 2020, Paul Wetering <pwetering@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import (absolute_import, division, print_function)
+
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
     'status': ['preview'],
