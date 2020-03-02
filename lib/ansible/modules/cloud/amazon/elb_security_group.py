f1914ab011389b1f87f3c8e694d31d31efd12938
Michael Moyle <michaeljmoyle@gmail.com>
2020-03-02 13:09:07 +0100

    Add module with partial test

---
+++ lib/ansible/modules/cloud/amazon/elb_security_group.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+# Copyright: Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+module: elb_security_group
+short_description: Attach a security group to an elb
+description:
+  - Attach a security group to an elb
+requirements: ['boto3 >= 1.11.15']
+version_added: "2.10"
+author:
+  - "Michael Moyle (@mmoyle)"
+options:
+    elb_arn:
+        description:
+        type: string
+    security_groups:
+        description: List of security groups to attach
+        type: list
+extends_documentation_fragment:
+    - aws
+    - ec2
+'''
+
+EXAMPLES = '''
+- name: Attach SGs
+  elb_security_group:
+    profile: 'my_aws_profile'
+    region: 'my_region'
+    elb_arn: "arn:aws:elasticloadbalancing:..."
+    security_group_ids: ['sg-1aaaa', 'sg-2bbbb']
+  register: result
+- debug:
+    msg: "{{ result }}"
+'''
+
+RETURN = '''
+response:
+    description: The number or security groups attached.
+    returned: Always
+    type: int
+'''
+
+from ansible.module_utils.aws.core import AnsibleAWSModule
+from ansible.module_utils.ec2 import AWSRetry
+
+try:
+    from botocore.exceptions import ClientError
+    from botocore.exceptions import BotoCoreError
+except ImportError:
+    pass  # handled by AnsibleAWSModule
+
+
+@AWSRetry.exponential_backoff()
+def elb_sg_attach(elbv2_client, elb_arn, sg_ids):
+    '''Attach security groups to elb and return number of sgs attached '''
+
+    response = elbv2_client.set_security_groups(
+        LoadBalancerArn=elb_arn,
+        SecurityGroups=sg_ids
+    )
+
+    return len(response['SecurityGroupIds'])
+
+
+def main():
+    argument_spec = dict(
+        elb_arn=dict(required=True, type='str'),
+        security_group_ids=dict(required=True, type='list')
+    )
+    result = dict(
+        changed=False,
+        original_message=''
+    )
+
+    module = AnsibleAWSModule(argument_spec=argument_spec, supports_check_mode=False)
+    elbv2_client = module.client('elbv2')
+
+    try:
+        result['response'] = elb_sg_attach(
+            elbv2_client,
+            module.params.get('elb_arn'),
+            module.params.get('security_group_ids')
+            )
+
+        result['changed'] = True
+
+    except (ClientError, BotoCoreError) as e:
+        module.fail_json_aws(e)
+
+    result.update()
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
