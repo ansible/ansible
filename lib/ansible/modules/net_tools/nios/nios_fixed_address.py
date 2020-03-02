0b955c13cd007f9296d6ec27a45a92ee96f1c335
Vaishnavi T R <vtr@infoblox.com>
2020-03-02 16:08:24 +0530

    Fix for #67423(numerical options don't work)

--- lib/ansible/modules/net_tools/nios/nios_fixed_address.py
+++ lib/ansible/modules/net_tools/nios/nios_fixed_address.py
@@ -189,18 +189,17 @@ def options(module):
     special_options = ['routers', 'router-templates', 'domain-name-servers',
                        'domain-name', 'broadcast-address', 'broadcast-address-offset',
                        'dhcp-lease-time', 'dhcp6.name-servers']
-    special_num =  [3, 6 , 15, 28, 51]
+    #options-router-templates, broadcast-address-offset, dhcp6.name-servers don't have any associated number
+    special_num = [3, 6, 15, 28, 51] 
     options = list()
     for item in module.params['options']:
         opt = dict([(k, v) for k, v in iteritems(item) if v is not None])
         if 'name' not in opt and 'num' not in opt:
             module.fail_json(msg='one of `name` or `num` is required for option value')
-        if 'name' in opt:
-            if opt['name'] not in special_options:
-                del opt['use_option']
-        if 'num' in opt:
-            if opt['num'] not in special_num:
-                del opt['use_option']
+        if 'name' in opt and opt['name'] not in special_options:
+            del opt['use_option']
+        if 'num' in opt and opt['num'] not in special_num:
+            del opt['use_option']
         options.append(opt)
     return options
 
