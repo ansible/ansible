25ed777ff6c495446d03549e6890e368163da3ae
Ruheena Ansari <ruheena0105@tamu.edu>
2020-03-02 16:11:15 -0500

    Do not prefix remote_path IF WINDOWS
    https://github.com/ansible/ansible/pull/67832 fixes the need to run powershell modules on windows containers via docker connection. However, while running win_copy on windows containers, destination path is prefixed with / which doesn't work in the case of windows. The changes in this PR take care of bypassing that if OS is windows.

--- lib/ansible/plugins/connection/docker.py
+++ lib/ansible/plugins/connection/docker.py
@@ -280,7 +280,9 @@ class Connection(ConnectionBase):
         super(Connection, self).put_file(in_path, out_path)
         display.vvv("PUT %s TO %s" % (in_path, out_path), host=self._play_context.remote_addr)
 
-        out_path = self._prefix_login_path(out_path)
+        if not getattr(self._shell, "_IS_WINDOWS", False):
+            out_path = self._prefix_login_path(out_path)
+
         if not os.path.exists(to_bytes(in_path, errors='surrogate_or_strict')):
             raise AnsibleFileNotFound(
                 "file or module does not exist: %s" % to_native(in_path))
