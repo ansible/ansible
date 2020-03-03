f01222274a080e0195ecfe08d981d9077cd922b1
Sam Doran <sdoran@redhat.com>
2020-03-03 17:29:46 -0500

    Add a few more test cases

--- test/units/plugins/action/test_fetch.py
+++ test/units/plugins/action/test_fetch.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Copyright (c) 2020 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
@@ -20,6 +19,8 @@ from ansible.playbook.task import Task
     '../../../foo',
     '/../../../foo',
     '/../foo',
+    '//..//foo',
+    '..//..//foo',
 ]
 )
 def test_fetch_path_traversal(mocker, source):
