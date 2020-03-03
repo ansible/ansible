3d91f759c071c5fc22888c75826c8e2062a16ded
Markus Bergholz <markuman@gmail.com>
2020-03-03 02:20:46 +0100

    Backport/2.9/57737 ec2_asg unhandled exception (#67431)
    * cope with an ASG instance not having a launch config any more (#57737)
    
    * add changelog
    
    Co-authored-by: Andrew de Quincey <adq@lidskialf.net>

--- lib/ansible/modules/cloud/amazon/ec2_asg.py
+++ lib/ansible/modules/cloud/amazon/ec2_asg.py
@@ -1382,7 +1382,7 @@ def get_instances_by_launch_config(props, lc_check, initial_instances):
             # Check if migrating from launch_template to launch_config first
             if 'launch_template' in props['instance_facts'][i]:
                 old_instances.append(i)
-            elif props['instance_facts'][i]['launch_config_name'] == props['launch_config_name']:
+            elif props['instance_facts'][i].get('launch_config_name') == props['launch_config_name']:
                 new_instances.append(i)
             else:
                 old_instances.append(i)
@@ -1409,7 +1409,7 @@ def get_instances_by_launch_template(props, lt_check, initial_instances):
             # Check if migrating from launch_config_name to launch_template_name first
             if 'launch_config_name' in props['instance_facts'][i]:
                 old_instances.append(i)
-            elif props['instance_facts'][i]['launch_template'] == props['launch_template']:
+            elif props['instance_facts'][i].get('launch_template') == props['launch_template']:
                 new_instances.append(i)
             else:
                 old_instances.append(i)
