824ebca3a83257d27ad4bbfbfc9f3d0ac39feef5
Matt Martz <matt@sivel.net>
2020-03-05 14:05:50 -0600

    Re-arrange code

--- test/integration/targets/collections/collection_root_user/ansible_collections/testns/testbroken/plugins/filter/broken_filter.py
+++ test/integration/targets/collections/collection_root_user/ansible_collections/testns/testbroken/plugins/filter/broken_filter.py
@@ -2,12 +2,12 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 
-raise Exception('This is a broken filter plugin')
-
-
 class FilterModule(object):
 
     def filters(self):
         return {
             'broken': lambda x: 'broken',
         }
+
+
+raise Exception('This is a broken filter plugin')
