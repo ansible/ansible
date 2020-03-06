c6a4dc58eab5e3de694c72309a9eeac2132e8310
nalin_garg <ng1181@nyu.edu>
2020-03-06 09:24:36 -0800

    fix from license issue and spacing.

--- lib/ansible/modules/web_infrastructure/jenkins_node.py
+++ lib/ansible/modules/web_infrastructure/jenkins_node.py
@@ -2,10 +2,8 @@
 #
 # Copyright: (c) Ansible Project
 #
-# Simplified BSD License (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
-# noqa: missing-gplv3-license
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import (absolute_import, division, print_function)
-
 __metaclass__ = type
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
