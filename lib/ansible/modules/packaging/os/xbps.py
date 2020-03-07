5479aa7abf33cd325714e691551ab6830313eeea
Cameron Nemo <cnemo@tutanota.com>
2020-03-07 11:37:57 -0800

    fix(modules/xbps): do not upgrade in check mode
    Ensure that while in check mode, this module does not upgrade all
    packages on the system.

--- lib/ansible/modules/packaging/os/xbps.py
+++ lib/ansible/modules/packaging/os/xbps.py
@@ -134,6 +134,8 @@ def upgrade(module, xbps_path):
     if rc == 0:
         if(len(stdout.splitlines()) == 0):
             module.exit_json(changed=False, msg='Nothing to upgrade')
+        elif module.check_mode:
+            module.exit_json(changed=True, msg='Would have performed upgrade')
         else:
             rc, stdout, stderr = module.run_command(cmdupgrade, check_rc=False)
             if rc == 0:
