27993566b5751132987d7cce5ee6de1eb5713985
xuxiaowei0512 <786018072@qq.com>
2020-03-07 17:34:14 +0800

    fix docs in aaa_server_host

--- lib/ansible/modules/network/cloudengine/ce_aaa_server_host.py
+++ lib/ansible/modules/network/cloudengine/ce_aaa_server_host.py
@@ -42,77 +42,96 @@ options:
             - Specify desired state of the resource.
         default: present
         choices: ['present', 'absent']
+        type: str
     local_user_name:
         description:
             - Name of a local user.
               The value is a string of 1 to 253 characters.
+        type: str
     local_password:
         description:
             - Login password of a user. The password can contain letters, numbers, and special characters.
               The value is a string of 1 to 255 characters.
+        type: str
     local_service_type:
         description:
             - The type of local user login through, such as ftp ssh snmp telnet.
+        type: str
     local_ftp_dir:
         description:
             - FTP user directory.
               The value is a string of 1 to 255 characters.
+        type: str
     local_user_level:
         description:
             - Login level of a local user.
               The value is an integer ranging from 0 to 15.
+        type: str
     local_user_group:
         description:
             - Name of the user group where the user belongs. The user inherits all the rights of the user group.
               The value is a string of 1 to 32 characters.
+        type: str
     radius_group_name:
         description:
             - RADIUS server group's name.
               The value is a string of 1 to 32 case-insensitive characters.
+        type: str
     radius_server_type:
         description:
             - Type of Radius Server.
         choices: ['Authentication', 'Accounting']
+        type: str
     radius_server_ip:
         description:
             - IPv4 address of configured server.
               The value is a string of 0 to 255 characters, in dotted decimal notation.
+        type: str
     radius_server_ipv6:
         description:
             - IPv6 address of configured server.
               The total length is 128 bits.
+        type: str
     radius_server_port:
         description:
             - Configured server port for a particular server.
               The value is an integer ranging from 1 to 65535.
+        type: str
     radius_server_mode:
         description:
             - Configured primary or secondary server for a particular server.
         choices: ['Secondary-server', 'Primary-server']
+        type: str
     radius_vpn_name:
         description:
             - Set VPN instance.
               The value is a string of 1 to 31 case-sensitive characters.
+        type: str
     radius_server_name:
         description:
             - Hostname of configured server.
               The value is a string of 0 to 255 case-sensitive characters.
+        type: str
     hwtacacs_template:
         description:
             - Name of a HWTACACS template.
               The value is a string of 1 to 32 case-insensitive characters.
+        type: str
     hwtacacs_server_ip:
         description:
             - Server IPv4 address. Must be a valid unicast IP address.
               The value is a string of 0 to 255 characters, in dotted decimal notation.
+        type: str
     hwtacacs_server_ipv6:
         description:
             - Server IPv6 address. Must be a valid unicast IP address.
               The total length is 128 bits.
+        type: str
     hwtacacs_server_type:
         description:
             - Hwtacacs server type.
         choices: ['Authentication', 'Authorization', 'Accounting', 'Common']
+        type: str
     hwtacacs_is_secondary_server:
         description:
             - Whether the server is secondary.
@@ -121,6 +140,7 @@ options:
     hwtacacs_vpn_name:
         description:
             - VPN instance name.
+        type: str
     hwtacacs_is_public_net:
         description:
             - Set the public-net.
@@ -137,14 +157,6 @@ EXAMPLES = '''
   hosts: cloudengine
   connection: local
   gather_facts: no
-  vars:
-    cli:
-      host: "{{ inventory_hostname }}"
-      port: "{{ ansible_ssh_port }}"
-      username: "{{ username }}"
-      password: "{{ password }}"
-      transport: cli
-
   tasks:
 
   - name: "Config local user when use local scheme"
@@ -152,14 +164,12 @@ EXAMPLES = '''
       state: present
       local_user_name: user1
       local_password: 123456
-      provider: "{{ cli }}"
 
   - name: "Undo local user when use local scheme"
     ce_aaa_server_host:
       state: absent
       local_user_name: user1
       local_password: 123456
-      provider: "{{ cli }}"
 
   - name: "Config radius server ip"
     ce_aaa_server_host:
@@ -170,7 +180,6 @@ EXAMPLES = '''
       radius_server_port: 2000
       radius_server_mode: Primary-server
       radius_vpn_name: _public_
-      provider: "{{ cli }}"
 
   - name: "Undo radius server ip"
     ce_aaa_server_host:
@@ -181,7 +190,6 @@ EXAMPLES = '''
       radius_server_port: 2000
       radius_server_mode: Primary-server
       radius_vpn_name: _public_
-      provider: "{{ cli }}"
 
   - name: "Config hwtacacs server ip"
     ce_aaa_server_host:
@@ -190,7 +198,6 @@ EXAMPLES = '''
       hwtacacs_server_ip: 10.10.10.10
       hwtacacs_server_type: Authorization
       hwtacacs_vpn_name: _public_
-      provider: "{{ cli }}"
 
   - name: "Undo hwtacacs server ip"
     ce_aaa_server_host:
@@ -199,7 +206,6 @@ EXAMPLES = '''
       hwtacacs_server_ip: 10.10.10.10
       hwtacacs_server_type: Authorization
       hwtacacs_vpn_name: _public_
-      provider: "{{ cli }}"
 '''
 
 RETURN = '''
