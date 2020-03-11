ba437619b784642e175d7bccfe5f857cd218ed71
Gonéri Le Bouder <goneri@lebouder.net>
2020-03-11 08:26:12 -0400

    DNM iosxr_l3_interfaces: bump the copyright year
    Depends-On: https://github.com/ansible/ansible-zuul-jobs/pull/371

--- lib/ansible/modules/network/iosxr/iosxr_l3_interfaces.py
+++ lib/ansible/modules/network/iosxr/iosxr_l3_interfaces.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright 2019 Red Hat Inc.
+# Copyright 2019-2020 Red Hat Inc.
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
