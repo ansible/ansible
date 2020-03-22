3511b1128484ed1db7bbd902c5e74290232dcd73
itdependsnetworks <ken@celenza.org>
2020-03-22 01:20:30 -0400

    add GPL

--- lib/ansible/modules/cloud/digital_ocean/digital_ocean_droplet_info.py
+++ lib/ansible/modules/cloud/digital_ocean/digital_ocean_droplet_info.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
+# Copyright: Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
