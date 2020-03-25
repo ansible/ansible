f64463afa06d9a603d30ed1e5e4e65eea505e65b
Rick Elrod <rick@elrod.me>
2020-03-25 03:58:17 -0500

    Squash everything into one playbook
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- test/integration/targets/filter_urls/runme.sh
+++ test/integration/targets/filter_urls/runme.sh
@@ -4,14 +4,16 @@ set -eux
 
 export ANSIBLE_ROLES_PATH=../
 
-ansible-playbook -i ../../inventory runme.yml "$@"
+ansible-playbook runme.yml "$@"
 
 source virtualenv.sh
 
 # This has to be done outside of requirements.txt but is necessary for
-# installing Jinja 2.6
+# installing Jinja 2.6. We need this because Jinja 2.6 won't install with
+# a newer setuptools, and because setuptools 45+ won't work with Python 2.
 pip install 'setuptools<45'
 
 pip install -U -r requirements.txt
 
-ansible-playbook -i ../../inventory jinja26.yml -v "$@"
+# Run the playbook again in the venv with Jinja 2.6
+ansible-playbook runme.yml "$@"
