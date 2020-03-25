38a533e2eacfd946d80a1d989f0a82614db0d814
Jordan Borean <jborean93@gmail.com>
2020-03-25 10:57:14 +1000

    Ignore test files in new package-data sanity test (#68452)

--- test/sanity/code-smell/package-data.py
+++ test/sanity/code-smell/package-data.py
@@ -29,6 +29,7 @@ def assemble_files_to_ship(complete_file_list):
         'hacking/shippable/*',
         'hacking/tests/*',
         'hacking/ticket_stubs/*',
+        'metadata-*.json',  # ansible-test with --docker produces this tmp file.
         'test/sanity/code-smell/botmeta.*',
         'test/utils/*',
         'test/utils/*/*',
@@ -55,6 +56,7 @@ def assemble_files_to_ship(complete_file_list):
         'hacking/return_skeleton_generator.py',
         'hacking/test-module',
         'hacking/test-module.py',
+        'test/support/README.md',
         '.cherry_picker.toml',
         '.mailmap',
         # Possibly should be included
