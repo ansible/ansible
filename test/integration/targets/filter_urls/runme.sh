e221c546b52b80dbdee6be0c40d1ce4a0eb2764f
Rick Elrod <rick@elrod.me>
2020-03-25 13:07:44 -0500

    nuke requirements.txt, move the pip install to runme.sh
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- test/integration/targets/filter_urls/runme.sh
+++ test/integration/targets/filter_urls/runme.sh
@@ -8,12 +8,15 @@ ansible-playbook runme.yml "$@"
 
 source virtualenv.sh
 
-# This has to be done outside of requirements.txt but is necessary for
-# installing Jinja 2.6. We need this because Jinja 2.6 won't install with
-# a newer setuptools, and because setuptools 45+ won't work with Python 2.
+# This is necessary for installing Jinja 2.6. We need this because Jinja 2.6
+# won't install with newer setuptools, and because setuptools 45+ won't work
+# with Python 2.
 pip install 'setuptools<45'
 
-pip install -U -r requirements.txt
+# Install Jinja 2.6 since we want to test the fallback to Ansible's custom
+# urlencode functions. Jinja 2.6 does not have urlencode so we will trigger the
+# fallback.
+pip install 'jinja2 >= 2.6, < 2.7'
 
 # Run the playbook again in the venv with Jinja 2.6
 ansible-playbook runme.yml "$@"
