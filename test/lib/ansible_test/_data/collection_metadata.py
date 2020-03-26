efff7b92643f073b489e960d41c6ba2a7f742806
Felix Fontein <felix@fontein.de>
2020-03-26 09:04:30 +0100

    Linting. Adding warning if neither galaxy.yml nor MAINFEST.json is found.

--- test/lib/ansible_test/_data/collection_metadata.py
+++ test/lib/ansible_test/_data/collection_metadata.py
@@ -12,36 +12,46 @@ try:
 except ImportError:
     yaml = None
 
-result = dict(warnings=[])
+
+def load_metadata(collection_path):
+    """
+    Retrieve some collection metadata.
+    """
+    result = dict(warnings=[])
+
+    # Try reading galaxy.yml
+    galaxy_path = os.path.join(collection_path, 'galaxy.yml')
+    if os.path.exists(galaxy_path):
+        if yaml is None:
+            result['no-pyyaml'] = True
+        else:
+            try:
+                with open(galaxy_path) as galaxy_file:
+                    galaxy = yaml.safe_load(galaxy_file)
+                    result['version'] = galaxy.get('version')
+            except IOError as exc:
+                result['warnings'].append('Error while reading {0}: {1}'.format(galaxy_path, exc))
+        return result
+
+    # Try reading MANIFEST.json
+    manifest_path = os.path.join(collection_path, 'MANIFEST.json')
+    if os.path.exists(manifest_path):
+        try:
+            with open(manifest_path) as manifest_file:
+                manifest = json.load(manifest_file)
+                collection_info = manifest.get('collection_info') or dict()
+                result['version'] = collection_info.get('version')
+        except IOError as exc:
+            result['warnings'].append('Error while reading {0}: {1}'.format(manifest_path, exc))
+        return result
+
+    result['warnings'].append(
+        'Found neither galaxy.yml nor MANIFEST.json in {0}'.format(collection_path))
+    return result
+
 
 if len(sys.argv) != 2:
     print('Expect exactly one argument: path to the collection')
     sys.exit(-1)
 
-collection_path = sys.argv[1]
-
-# Try reading galaxy.yml
-fn = os.path.join(collection_path, 'galaxy.yml')
-if os.path.exists(fn):
-    if yaml is None:
-        result['no-pyyaml'] = True
-    else:
-        try:
-            with open(fn) as galaxy_file:
-                galaxy = yaml.safe_load(galaxy_file)
-                result['version'] = galaxy.get('version')
-        except IOError as exc:
-            result['warnings'].append('Error while reading {0}: {1}'.format(fn, exc))
-
-# Try reading MANIFEST.json
-fn = os.path.join(collection_path, 'MANIFEST.json')
-if os.path.exists(fn):
-    try:
-        with open(fn) as manifest_file:
-            manifest = json.load(manifest_file)
-            collection_info = manifest.get('collection_info') or dict()
-            result['version'] = collection_info.get('version')
-    except IOError as exc:
-        result['warnings'].append('Error while reading {0}: {1}'.format(fn, exc))
-
-print(json.dumps(result))
+print(json.dumps(load_metadata(sys.argv[1])))
