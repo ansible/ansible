957cc36a01d813a42a663033e37d549512f04c27
David Shrewsbury <dshrewsb@redhat.com>
2020-03-31 16:55:36 -0400

    fix boilerplates

--- lib/ansible/modules/system/filament.py
+++ lib/ansible/modules/system/filament.py
@@ -22,7 +22,10 @@ EXAMPLES = '''
 
 RETURN = ''' # '''
 
+from __future__ import (absolute_import, division, print_function)
 from ansible.module_utils.basic import AnsibleModule
+
+__metaclass__ = type
 
 
 def run_module():
