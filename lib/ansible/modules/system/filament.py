f2a028dc5682f771724374fb7b48beb25379e044
David Shrewsbury <dshrewsb@redhat.com>
2020-03-31 17:26:41 -0400

    correct future import placement

--- lib/ansible/modules/system/filament.py
+++ lib/ansible/modules/system/filament.py
@@ -4,6 +4,9 @@
 # Copyright: (c) 2020, David Shrewsbury <dshrewsb@redhat.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
@@ -22,10 +25,7 @@ EXAMPLES = '''
 
 RETURN = ''' # '''
 
-from __future__ import (absolute_import, division, print_function)
 from ansible.module_utils.basic import AnsibleModule
-
-__metaclass__ = type
 
 
 def run_module():
