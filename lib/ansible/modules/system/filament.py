f3336d859ed2661849d1646c14df2c8e39bd6a85
David Shrewsbury <dshrewsb@redhat.com>
2020-03-31 13:50:32 -0400

    Fix for missing required data

--- lib/ansible/modules/system/filament.py
+++ lib/ansible/modules/system/filament.py
@@ -1,1 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2020, David Shrewsbury <dshrewsb@redhat.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+module: filament 
+version_added: 2.10
+short_description: Test module
+description:
+    - Just a test module.
+author: "David Shrewsbury (@Shrews)"
+'''
+
+EXAMPLES = '''
+'''
+
 hello = "world"
