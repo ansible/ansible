c4a1d7fa43d202092f1d6b7d1f881ab6ce6d7d9d
David Shrewsbury <dshrewsb@redhat.com>
2020-03-31 16:40:47 -0400

    Fix integration test and module

--- lib/ansible/modules/system/filament.py
+++ lib/ansible/modules/system/filament.py
@@ -22,4 +22,19 @@ EXAMPLES = '''
 
 RETURN = ''' # '''
 
-myvar = "world"
+from ansible.module_utils.basic import AnsibleModule
+
+
+def run_module():
+    module = AnsibleModule(argument_spec={})
+    result = dict(changed=True)
+    result['myvar'] = 'world'
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
