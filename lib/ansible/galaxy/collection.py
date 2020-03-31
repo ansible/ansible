d6a82e68658d7ec59cbac49e761a51f6ebe86a62
Jordan Borean <jborean93@gmail.com>
2020-04-01 09:00:07 +1000

    galaxy - preserve mode properly on artifact (#68418) - 2.9 (#68451)
    * galaxy - preserve mode properly on artifact (#68418)
    
    * galaxy - preserve mode properly on artifact
    
    * Fix py2 encoding issue
    
    * Update lib/ansible/galaxy/collection.py
    
    Co-Authored-By: Matt Clay <matt@mystile.com>
    
    * Use sane defaults instead of sourcing from tarfile
    
    Co-authored-by: Matt Clay <matt@mystile.com>
    (cherry picked from commit 127d54b3630c65043ec12c4af2024f8ef0bc6d09)
    
    * added mode to dir creation that was missed in backport

--- lib/ansible/galaxy/collection.py
+++ lib/ansible/galaxy/collection.py
@@ -9,6 +9,8 @@ import json
 import operator
 import os
 import shutil
+import stat
+import sys
 import tarfile
 import tempfile
 import threading
@@ -744,7 +746,8 @@ def _build_collection_tar(b_collection_path, b_tar_path, collection_manifest, fi
                 b_src_path = os.path.join(b_collection_path, to_bytes(filename, errors='surrogate_or_strict'))
 
                 def reset_stat(tarinfo):
-                    tarinfo.mode = 0o0755 if tarinfo.isdir() else 0o0644
+                    existing_is_exec = tarinfo.mode & stat.S_IXUSR
+                    tarinfo.mode = 0o0755 if existing_is_exec or tarinfo.isdir() else 0o0644
                     tarinfo.uid = tarinfo.gid = 0
                     tarinfo.uname = tarinfo.gname = ''
                     return tarinfo
@@ -927,6 +930,13 @@ def _extract_tar_file(tar, filename, b_dest, b_temp_path, expected_hash=None):
         if not os.path.exists(b_parent_dir):
             # Seems like Galaxy does not validate if all file entries have a corresponding dir ftype entry. This check
             # makes sure we create the parent directory even if it wasn't set in the metadata.
-            os.makedirs(b_parent_dir)
+            os.makedirs(b_parent_dir, mode=0o0755)
 
         shutil.move(to_bytes(tmpfile_obj.name, errors='surrogate_or_strict'), b_dest_filepath)
+
+        # Default to rw-r--r-- and only add execute if the tar file has execute.
+        new_mode = 0o644
+        if stat.S_IMODE(member.mode) & stat.S_IXUSR:
+            new_mode |= 0o0111
+
+        os.chmod(b_dest_filepath, new_mode)
