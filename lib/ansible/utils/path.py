46bef3ff307da5692cd21b68fd9aea3d98b6c48d
Brian Coca <brian.coca+git@gmail.com>
2020-04-07 16:55:38 -0400

    whitey

--- lib/ansible/utils/path.py
+++ lib/ansible/utils/path.py
@@ -151,6 +151,6 @@ def is_subpath(child, parent):
     try:
         test = c[:len(p)] == p
     except IndexError:
-        pass # child is shorter than parent so cannot be subpath
+        pass  # child is shorter than parent so cannot be subpath
 
     return test
