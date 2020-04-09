6b92a69715faff784427cb61322329df919e6f97
Rafay <abdurrafay073@gmail.com>
2020-04-09 14:11:20 -0700

    refactor get_default_interfaces

--- lib/ansible/module_utils/facts/network/linux.py
+++ lib/ansible/module_utils/facts/network/linux.py
@@ -61,8 +61,7 @@ class LinuxNetwork(Network):
         network_facts['all_ipv6_addresses'] = ips['all_ipv6_addresses']
         return network_facts
 
-    def get_default_interfaces(self, ip_path, collected_facts=None):
-        collected_facts = collected_facts or {}
+    def get_default_interfaces(self, ip_path, collected_facts={}):
         # Use the commands:
         #     ip -4 route get 8.8.8.8                     -> Google public DNS
         #     ip -6 route get 2404:6800:400a:800::1012    -> ipv6.google.com
@@ -74,27 +73,32 @@ class LinuxNetwork(Network):
         interface = dict(v4={}, v6={})
 
         for v in 'v4', 'v6':
-            if (v == 'v6' and collected_facts.get('ansible_os_family') == 'RedHat' and
-                    collected_facts.get('ansible_distribution_version', '').startswith('4.')):
-                continue
-            if v == 'v6' and not socket.has_ipv6:
-                continue
+            isV6 = v == 'v6'
+            isRedHat = collected_facts.get('ansible_os_family') == 'RedHat'
+            startsWith4 = collected_facts.get('ansible_distribution_version', '').startswith('4.')
             rc, out, err = self.module.run_command(command[v], errors='surrogate_then_replace')
-            if not out:
+
+            if ((isV6 and isRedHat and startsWith4) or (isV6 and not socket.has_ipv6) or not out):
                 # v6 routing may result in
                 #   RTNETLINK answers: Invalid argument
                 continue
+
             words = out.splitlines()[0].split()
             # A valid output starts with the queried address on the first line
             if len(words) > 0 and words[0] == command[v][-1]:
                 for i in range(len(words) - 1):
-                    if words[i] == 'dev':
-                        interface[v]['interface'] = words[i + 1]
-                    elif words[i] == 'src':
-                        interface[v]['address'] = words[i + 1]
-                    elif words[i] == 'via' and words[i + 1] != command[v][-1]:
-                        interface[v]['gateway'] = words[i + 1]
+                    interface = get_default_interfaces_helper(words, interface, v, i)
+
         return interface['v4'], interface['v6']
+
+    def get_default_interfaces_helper(words, interface, v, i, command):
+        if words[i] == 'dev':
+            interface[v]['interface'] = words[i + 1]
+        elif words[i] == 'src':
+            interface[v]['address'] = words[i + 1]
+        elif words[i] == 'via' and words[i + 1] != command[v][-1]:
+            interface[v]['gateway'] = words[i + 1]
+        return interface
 
     def get_interfaces_info(self, ip_path, default_ipv4, default_ipv6):
         interfaces = {}
