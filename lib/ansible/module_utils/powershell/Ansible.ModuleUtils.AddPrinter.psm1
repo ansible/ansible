645ecbe51a43d188ab1427971616d75c00269f0e
Akchhay Alok <akchhay.alok91@gmail.com>
2020-04-09 12:07:18 +0530

    Added Powershell module for adding printer

--- lib/ansible/module_utils/powershell/Ansible.ModuleUtils.AddPrinter.psm1
+++ lib/ansible/module_utils/powershell/Ansible.ModuleUtils.AddPrinter.psm1
@@ -1,6 +1,6 @@
 
-# Module to add local printer to the current system
-Function Add-LocalPrinter
+<# Module to add local printer to the current system #>
+
 <#
     .SYNOPSIS
     Add local printer to the specified port, printer driver and printer name.
@@ -14,32 +14,33 @@ Function Add-LocalPrinter
     .PARAMTER name
     [String] The name which you want to give to printer.
     #>
-
-{
-param(
-[Parameter(Mandatory=$true)][string]$portName,
-[Parameter(Mandatory=$true)][string]$printDriverName,
-[Parameter(Mandatory=$true)][string]$name
-
-)
-
-$portExists = Get-Printerport -Name $portname -ErrorAction SilentlyContinue
-
-if (-not $portExists) 
-{
-  Add-PrinterPort -name $portName 
+Function Add-Localprinter {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)][string]$portName,
+        [Parameter(Mandatory=$true)][string]$printDriverName,
+        [Parameter(Mandatory=$true)][string]$name
+    )
+    #To Add Local Printer by specifying port name, Driver and Printer name
+    try {
+        $portExists = Get-Printerport -Name $portname -ErrorAction SilentlyContinue
+        if (-not $portExists) {
+            Add-PrinterPort -name $portName
+        }
+        $printDriverExists = Get-PrinterDriver -name $printDriverName -ErrorAction SilentlyContinue
+        if ($printDriverExists) {
+            Add-Printer -Name $name -PortName $portName -DriverName $printDriverName
+        }
+        else {
+            Write-Warning "Printer Driver not installed"
+        }
+        Restart-Service -Name Spooler -Force
 }
+    }
+    catch {
+        # It will throw an error if printer driver,
+        # Port or printer name not found
+        throw $_
+    }
 
-$printDriverExists = Get-PrinterDriver -name $printDriverName -ErrorAction SilentlyContinue
-
-if ($printDriverExists) 
-{
-    Add-Printer -Name $name -PortName $portName -DriverName $printDriverName
-}
-else
-{
-    Write-Warning "Printer Driver not installed"
-}
-Restart-Service -Name Spooler -Force
-}
-Export-ModuleMember -Function Add-LocalPrinter
+Export-ModuleMember -Function Add-Localprinter
