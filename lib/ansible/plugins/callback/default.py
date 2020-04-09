8ec05db6cd832ac4fffae2ce15b72c2588a16033
Dick Visser <dick.visser@geant.org>
2020-04-09 18:39:55 +0200

    Fix label on included tasks

--- lib/ansible/plugins/callback/default.py
+++ lib/ansible/plugins/callback/default.py
@@ -350,8 +350,8 @@ class CallbackModule(CallbackBase):
 
     def v2_playbook_on_include(self, included_file):
         msg = 'included: %s for %s' % (included_file._filename, ", ".join([h.name for h in included_file._hosts]))
-        if 'item' in included_file._args:
-            msg += " => (item=%s)" % (self._get_item_label(included_file._args),)
+        if self._get_item_label(included_file._vars):
+            msg += " => (item=%s)" % (self._get_item_label(included_file._vars),)
         self._display.display(msg, color=C.COLOR_SKIP)
 
     def v2_playbook_on_stats(self, stats):
