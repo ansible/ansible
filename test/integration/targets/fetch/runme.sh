47e107ced7dbc402b8a09cbcc60f6b4412ecdb01
Brian Coca <brian.coca+git@gmail.com>
2020-04-13 10:27:08 -0400

    handle role diffs in older versions

--- test/integration/targets/fetch/runme.sh
+++ test/integration/targets/fetch/runme.sh
@@ -3,7 +3,7 @@
 set -eux
 
 # setup required roles
-ln -s ../../setup_remote_tmp_dir roles/setup_remote_tmp_dir
+ln -s ../../prepare_tests roles/prepare_tests
 
 # run old type role tests
 ansible-playbook -i ../../inventory run_fetch_tests.yml -e "output_dir=${OUTPUT_DIR}" -v "$@"
