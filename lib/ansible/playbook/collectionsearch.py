34f2f5d3acb4b3b85656ebe61b6d80eb63626d2a
David Shrewsbury <Shrews@users.noreply.github.com>
2020-04-13 12:53:05 -0400

    Force collections to be static (#68723)
    * Force collections to be static
    
    Templating of collection names does not work at all. Force them to
    be static so that a warning is generated for the user.
    
    * Add collectionsearch unit test and fix for reviews
    
    New unit test validates the new _load_collections() code and moves
    the new check to the end of the method.
    
    * Change unit test to pytest
    
    * Adjust unit test to use capsys instead of monkeypatch
    
    * Fix pep8 error
    
    * Add changelog fragment
    
    Closes #68704

--- lib/ansible/playbook/collectionsearch.py
+++ lib/ansible/playbook/collectionsearch.py
@@ -7,6 +7,10 @@ __metaclass__ = type
 from ansible.module_utils.six import string_types
 from ansible.playbook.attribute import FieldAttribute
 from ansible.utils.collection_loader import AnsibleCollectionLoader
+from ansible.template import is_template, Environment
+from ansible.utils.display import Display
+
+display = Display()
 
 
 def _ensure_default_collection(collection_list=None):
@@ -32,7 +36,8 @@ def _ensure_default_collection(collection_list=None):
 class CollectionSearch:
 
     # this needs to be populated before we can resolve tasks/roles/etc
-    _collections = FieldAttribute(isa='list', listof=string_types, priority=100, default=_ensure_default_collection)
+    _collections = FieldAttribute(isa='list', listof=string_types, priority=100, default=_ensure_default_collection,
+                                  always_post_validate=True, static=True)
 
     def _load_collections(self, attr, ds):
         # this will only be called if someone specified a value; call the shared value
@@ -40,5 +45,14 @@ class CollectionSearch:
 
         if not ds:  # don't return an empty collection list, just return None
             return None
+
+        # This duplicates static attr checking logic from post_validate()
+        # because if the user attempts to template a collection name, it will
+        # error before it ever gets to the post_validate() warning.
+        env = Environment()
+        for collection_name in ds:
+            if is_template(collection_name, env):
+                display.warning('"collections" is not templatable, but we found: %s, '
+                                'it will not be templated and will be used "as is".' % (collection_name))
 
         return ds
