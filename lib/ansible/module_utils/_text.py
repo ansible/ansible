0323a587faadea49a08ff4e1b12c1b3183266493
Jordan Borean <jborean93@gmail.com>
2020-04-17 06:41:24 +1000

    Move docstring to start of util

--- lib/ansible/module_utils/_text.py
+++ lib/ansible/module_utils/_text.py
@@ -1,9 +1,9 @@
 # Copyright (c), Toshio Kuratomi <tkuratomi@ansible.com> 2016
 # Simplified BSD License (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
 
-# Backwards compat for people still calling it from this package
-from ansible.module_utils.common.text.converters import to_bytes, to_native, to_text
-
 """
 .. warn:: Use ansible.module_utils.common.text.converters instead.
 """
+
+# Backwards compat for people still calling it from this package
+from ansible.module_utils.common.text.converters import to_bytes, to_native, to_text
