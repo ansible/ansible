b736eb7f8a4babd70e22a3308192e3d0c87ed09c
Jordan Borean <jborean93@gmail.com>
2020-04-16 20:24:23 +1000

    windows - fix up env var quoting for unicode single quotes (#68968)
    * windows - fix up env var quoting for unicode single quotes
    
    * Add sanity ignore check for smart quotes

--- test/units/plugins/shell/test_cmd.py
+++ test/units/plugins/shell/test_cmd.py
@@ -7,7 +7,7 @@ from ansible.plugins.shell.cmd import ShellModule
     ['arg1', 'arg1'],
     [None, '""'],
     ['arg1 and 2', '^"arg1 and 2^"'],
-    ['malicious argument\\"&whoami', '^"malicious argument\\^"^&whoami^"'],
+    ['malicious argument\\"&whoami', '^"malicious argument\\\\^"^&whoami^"'],
     ['C:\\temp\\some ^%file% > nul', '^"C:\\temp\\some ^^^%file^% ^> nul^"']
 ])
 def test_quote_args(s, expected):
