acdc9eb76d23e25ed8b1c7fc4fe8e4f9fd6f49fd
Martin Krizek <martin.krizek@gmail.com>
2020-04-17 14:59:52 +0200

    native types: literal_eval all the things (#68938)
    With https://github.com/pallets/jinja/pull/1190 merged our short-circuit
    is no longer valid (has it ever been?) as now data like ' True ' may go
    through our ansible_native_concat function as opposed to going through
    intermediate call to Jinja2's native_concat before. Now we need to always
    send data through literal_eval to ensure native types are returned.

--- lib/ansible/template/native_helpers.py
+++ lib/ansible/template/native_helpers.py
@@ -13,6 +13,8 @@ import types
 from jinja2.runtime import StrictUndefined
 
 from ansible.module_utils._text import to_text
+from ansible.module_utils.common.text.converters import container_to_text
+from ansible.module_utils.six import PY2
 from ansible.parsing.yaml.objects import AnsibleVaultEncryptedUnicode
 
 
@@ -48,10 +50,6 @@ def ansible_native_concat(nodes):
             # See https://github.com/ansible/ansible/issues/52158
             # We do that only here because it is taken care of by to_text() in the else block below already.
             str(out)
-
-        # short circuit literal_eval when possible
-        if not isinstance(out, list):
-            return out
     else:
         if isinstance(nodes, types.GeneratorType):
             nodes = chain(head, nodes)
@@ -60,6 +58,10 @@ def ansible_native_concat(nodes):
         out = u''.join([to_text(v) for v in nodes])
 
     try:
-        return literal_eval(out)
+        out = literal_eval(out)
+        if PY2:
+            # ensure bytes are not returned back into Ansible from templating
+            out = container_to_text(out)
+        return out
     except (ValueError, SyntaxError, MemoryError):
         return out
