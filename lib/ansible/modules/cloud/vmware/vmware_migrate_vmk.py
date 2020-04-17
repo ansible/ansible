4fa84cc5ec53099822af1fe21a832f22c760da82
Christopher Matthews <christopher.matthews@orbit.de>
2020-04-17 16:22:09 +0200

    Add feature migrate from Distrubuted Switch to Standard Switch

--- lib/ansible/modules/cloud/vmware/vmware_migrate_vmk.py
+++ lib/ansible/modules/cloud/vmware/vmware_migrate_vmk.py
@@ -139,7 +139,7 @@ class VMwareMigrateVmk(object):
     def state_migrate_vds_vss(self):
         host_network_system = self.host_system.configManager.networkSystem
         config = vim.host.NetworkConfig()
-        config.portgroup =  [self.create_port_group_config_vds_vss()]
+        config.portgroup = [self.create_port_group_config_vds_vss()]
         host_network_system.UpdateNetworkConfig(config, "modify")
         config = vim.host.NetworkConfig()
         config.vnic = [self.create_host_vnic_config_vds_vss()]
