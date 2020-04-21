97cd1d566e6c2b13ebf918ab78f0cb1e7c1d896c
Brian Coca <brian.coca+git@gmail.com>
2020-04-21 15:54:48 -0400

    de q

--- lib/ansible/cli/doc.py
+++ lib/ansible/cli/doc.py
@@ -41,14 +41,11 @@ def jdump(text):
 
 def add_collection_plugins(plugin_list, plugin_type, coll_filter=None):
 
-    import q
     # TODO: take into account routing.yml once implemented
     colldirs = list_collection_dirs(coll_filter=coll_filter)
     for b_path in colldirs:
-        q(b_path)
         path = to_text(b_path, errors='surrogate_or_strict')
         collname = get_collection_name_from_path(b_path)
-        q(collname)
         ptype = C.COLLECTION_PTYPE_COMPAT.get(plugin_type, plugin_type)
         plugin_list.update(DocCLI.find_plugins(os.path.join(path, 'plugins', ptype), plugin_type, collection=collname))
 
