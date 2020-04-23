5aa5745541617fab22768e94fa82787f92c28142
Jordan Borean <jborean93@gmail.com>
2020-04-24 04:56:59 +1000

    Bring back more objects that were exposes in _text (#69090)

--- lib/ansible/module_utils/_text.py
+++ lib/ansible/module_utils/_text.py
@@ -6,4 +6,8 @@
 """
 
 # Backwards compat for people still calling it from this package
+import codecs
+
+from ansible.module_utils.six import PY3, text_type, binary_type
+
 from ansible.module_utils.common.text.converters import to_bytes, to_native, to_text
