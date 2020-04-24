ba88f96a76e7141fbbde0164d12c1c0c40920bf8
Brian Coca <bcoca@users.noreply.github.com>
2020-04-24 12:35:07 -0400

    Update lib/ansible/cli/doc.py
    Co-Authored-By: Matt Clay <matt@mystile.com>

--- lib/ansible/cli/doc.py
+++ lib/ansible/cli/doc.py
@@ -42,7 +42,7 @@ def jdump(text):
 def add_collection_plugins(plugin_list, plugin_type, coll_filter=None):
 
     # TODO: take into account routing.yml once implemented
-    colldirs = list_collection_dirs(coll_filter=coll_filter)
+    b_colldirs = list_collection_dirs(coll_filter=coll_filter)
     for b_path in colldirs:
         path = to_text(b_path, errors='surrogate_or_strict')
         collname = get_collection_name_from_path(b_path)
