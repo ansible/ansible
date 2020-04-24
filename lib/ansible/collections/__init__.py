f42c70583cbb08a7106dc355dca6fbc8247b65a9
Brian Coca <bcoca@users.noreply.github.com>
2020-04-24 12:35:43 -0400

    Update lib/ansible/collections/__init__.py
    Co-Authored-By: Matt Clay <matt@mystile.com>

--- lib/ansible/collections/__init__.py
+++ lib/ansible/collections/__init__.py
@@ -22,7 +22,7 @@ def is_collection_path(path):
     b_path = to_bytes(path)
     if os.path.isdir(b_path):
         for b_flag in B_FLAG_FILES:
-            if os.path.exists(os.path.join(b_path, flag)):
+            if os.path.exists(os.path.join(b_path, b_flag)):
                 is_coll = True
                 break
 
