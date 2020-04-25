1decdaaafd00c01a58bfd13ef1776b1db3084867
Chris Holland <chrisholland3553@gmail.com>
2020-04-25 12:43:04 -0700

    Address FIXME's in sysctl.py

--- lib/ansible/module_utils/facts/virtual/sysctl.py
+++ lib/ansible/module_utils/facts/virtual/sysctl.py
@@ -26,46 +26,50 @@ class VirtualSysctlDetectionMixin(object):
     def detect_virt_product(self, key):
         virtual_product_facts = {}
         self.detect_sysctl()
-        # FIXME: exit early on falsey self.sysctl_path and unindent
-        if self.sysctl_path:
-            rc, out, err = self.module.run_command("%s -n %s" % (self.sysctl_path, key))
-            if rc == 0:
-                if re.match('(KVM|kvm|Bochs|SmartDC).*', out):
-                    virtual_product_facts['virtualization_type'] = 'kvm'
-                    virtual_product_facts['virtualization_role'] = 'guest'
-                elif re.match('.*VMware.*', out):
-                    virtual_product_facts['virtualization_type'] = 'VMware'
-                    virtual_product_facts['virtualization_role'] = 'guest'
-                elif out.rstrip() == 'VirtualBox':
-                    virtual_product_facts['virtualization_type'] = 'virtualbox'
-                    virtual_product_facts['virtualization_role'] = 'guest'
-                elif out.rstrip() == 'HVM domU':
-                    virtual_product_facts['virtualization_type'] = 'xen'
-                    virtual_product_facts['virtualization_role'] = 'guest'
-                elif out.rstrip() == 'Parallels':
-                    virtual_product_facts['virtualization_type'] = 'parallels'
-                    virtual_product_facts['virtualization_role'] = 'guest'
-                elif out.rstrip() == 'RHEV Hypervisor':
-                    virtual_product_facts['virtualization_type'] = 'RHEV'
-                    virtual_product_facts['virtualization_role'] = 'guest'
-                elif (key == 'security.jail.jailed') and (out.rstrip() == '1'):
-                    virtual_product_facts['virtualization_type'] = 'jails'
-                    virtual_product_facts['virtualization_role'] = 'guest'
+
+        if self.sysctl_path is None:
+            return virtual_product_facts
+
+        rc, out, err = self.module.run_command("%s -n %s" % (self.sysctl_path, key))
+        if rc == 0:
+            if re.match('(KVM|kvm|Bochs|SmartDC).*', out):
+                virtual_product_facts['virtualization_type'] = 'kvm'
+                virtual_product_facts['virtualization_role'] = 'guest'
+            elif re.match('.*VMware.*', out):
+                virtual_product_facts['virtualization_type'] = 'VMware'
+                virtual_product_facts['virtualization_role'] = 'guest'
+            elif out.rstrip() == 'VirtualBox':
+                virtual_product_facts['virtualization_type'] = 'virtualbox'
+                virtual_product_facts['virtualization_role'] = 'guest'
+            elif out.rstrip() == 'HVM domU':
+                virtual_product_facts['virtualization_type'] = 'xen'
+                virtual_product_facts['virtualization_role'] = 'guest'
+            elif out.rstrip() == 'Parallels':
+                virtual_product_facts['virtualization_type'] = 'parallels'
+                virtual_product_facts['virtualization_role'] = 'guest'
+            elif out.rstrip() == 'RHEV Hypervisor':
+                virtual_product_facts['virtualization_type'] = 'RHEV'
+                virtual_product_facts['virtualization_role'] = 'guest'
+            elif (key == 'security.jail.jailed') and (out.rstrip() == '1'):
+                virtual_product_facts['virtualization_type'] = 'jails'
+                virtual_product_facts['virtualization_role'] = 'guest'
 
         return virtual_product_facts
 
     def detect_virt_vendor(self, key):
         virtual_vendor_facts = {}
         self.detect_sysctl()
-        # FIXME: exit early on falsey self.sysctl_path and unindent
-        if self.sysctl_path:
-            rc, out, err = self.module.run_command("%s -n %s" % (self.sysctl_path, key))
-            if rc == 0:
-                if out.rstrip() == 'QEMU':
-                    virtual_vendor_facts['virtualization_type'] = 'kvm'
-                    virtual_vendor_facts['virtualization_role'] = 'guest'
-                if out.rstrip() == 'OpenBSD':
-                    virtual_vendor_facts['virtualization_type'] = 'vmm'
-                    virtual_vendor_facts['virtualization_role'] = 'guest'
+
+        if self.sysctl_path is None:
+            return virtual_vendor_facts
+
+        rc, out, err = self.module.run_command("%s -n %s" % (self.sysctl_path, key))
+        if rc == 0:
+            if out.rstrip() == 'QEMU':
+                virtual_vendor_facts['virtualization_type'] = 'kvm'
+                virtual_vendor_facts['virtualization_role'] = 'guest'
+            if out.rstrip() == 'OpenBSD':
+                virtual_vendor_facts['virtualization_type'] = 'vmm'
+                virtual_vendor_facts['virtualization_role'] = 'guest'
 
         return virtual_vendor_facts
