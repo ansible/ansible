3b0d123a10a790c2c6c0ed0a52e49118d27d4472
Chris Holland <41524756+ChrisAHolland@users.noreply.github.com>
2020-05-05 08:25:46 -0700

    Address FIXME's in sysctl.py (#69175)
    * Address FIXME's in sysctl.py
    
    * Added changelog fragment
    
    * Updated check
    
    * Update conditions
    
    * if not instead of is None
    
    * Restore and delete FIXME comments

--- lib/ansible/module_utils/facts/virtual/sysctl.py
+++ lib/ansible/module_utils/facts/virtual/sysctl.py
@@ -26,7 +26,6 @@ class VirtualSysctlDetectionMixin(object):
     def detect_virt_product(self, key):
         virtual_product_facts = {}
         self.detect_sysctl()
-        # FIXME: exit early on falsey self.sysctl_path and unindent
         if self.sysctl_path:
             rc, out, err = self.module.run_command("%s -n %s" % (self.sysctl_path, key))
             if rc == 0:
@@ -57,7 +56,6 @@ class VirtualSysctlDetectionMixin(object):
     def detect_virt_vendor(self, key):
         virtual_vendor_facts = {}
         self.detect_sysctl()
-        # FIXME: exit early on falsey self.sysctl_path and unindent
         if self.sysctl_path:
             rc, out, err = self.module.run_command("%s -n %s" % (self.sysctl_path, key))
             if rc == 0:
