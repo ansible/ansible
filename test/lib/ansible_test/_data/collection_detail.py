71277d9c44a5f3624e6524c80a72806428c6e63a
Felix Fontein <felix@fontein.de>
2020-05-08 13:46:17 +0200

    Validating parsed versions.
    Using regular expressions instead of semantic_version.Version() since semantic_version
    is not necessarily installed.

--- test/lib/ansible_test/_data/collection_detail.py
+++ test/lib/ansible_test/_data/collection_detail.py
@@ -4,9 +4,32 @@ __metaclass__ = type
 
 import json
 import os
+import re
 import sys
 
 import yaml
+
+
+# See semantic versioning specification (https://semver.org/)
+NUMERIC_IDENTIFIER = r'(?:0|[1-9][0-9]*)'
+ALPHANUMERIC_IDENTIFIER = r'(?:[0-9]*[a-zA-Z-][a-zA-Z0-9-]*)'
+
+PRE_RELEASE_IDENTIFIER = r'(?:' + NUMERIC_IDENTIFIER + r'|' + ALPHANUMERIC_IDENTIFIER + r')'
+BUILD_IDENTIFIER = r'[a-zA-Z0-9-]+'  # equivalent to r'(?:[0-9]+|' + ALPHANUMERIC_IDENTIFIER + r')'
+
+VERSION_CORE = NUMERIC_IDENTIFIER + r'\.' + NUMERIC_IDENTIFIER + r'\.' + NUMERIC_IDENTIFIER
+PRE_RELEASE = r'(?:-' + PRE_RELEASE_IDENTIFIER + r'(?:\.' + PRE_RELEASE_IDENTIFIER + r')*)?'
+BUILD = r'(?:\+' + BUILD_IDENTIFIER + r'(?:\.' + BUILD_IDENTIFIER + r')*)?'
+
+SEMVER_REGULAR_EXPRESSION = r'^' + VERSION_CORE + PRE_RELEASE + BUILD + r'$'
+
+
+def validate_version(version):
+    if version is None:
+        return
+    if not re.match(SEMVER_REGULAR_EXPRESSION, version):
+        raise Exception('Invalid version number "{0}". Collection version numbers must '
+                        'follow semantic versioning (https://semver.org/).'.format(version))
 
 
 def read_manifest_json(collection_path):
@@ -25,6 +48,7 @@ def read_manifest_json(collection_path):
         result = dict(
             version=collection_info.get('version'),
         )
+        validate_version(result['version'])
     except Exception as ex:  # pylint: disable=broad-except
         raise Exception('{0}: {1}'.format(os.path.basename(manifest_path), ex))
 
@@ -45,6 +69,7 @@ def read_galaxy_yml(collection_path):
         result = dict(
             version=galaxy.get('version'),
         )
+        validate_version(result['version'])
     except Exception as ex:  # pylint: disable=broad-except
         raise Exception('{0}: {1}'.format(os.path.basename(galaxy_path), ex))
 
