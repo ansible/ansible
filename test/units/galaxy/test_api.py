42118cc3b68e9fd40a45a220e08428c7fa7e3995
Matt Martz <matt@sivel.net>
2020-05-08 13:57:17 -0500

    Address test assumptions

--- test/units/galaxy/test_api.py
+++ test/units/galaxy/test_api.py
@@ -290,8 +290,8 @@ def test_publish_collection(api_version, collection_url, collection_artifact, mo
     assert mock_call.mock_calls[0][1][0] == 'https://galaxy.ansible.com/api/%s/%s/' % (api_version, collection_url)
     assert mock_call.mock_calls[0][2]['headers']['Content-length'] == len(mock_call.mock_calls[0][2]['args'])
     assert mock_call.mock_calls[0][2]['headers']['Content-type'].startswith(
-        'multipart/form-data; boundary=--------------------------')
-    assert mock_call.mock_calls[0][2]['args'].startswith(b'--------------------------')
+        'multipart/form-data; boundary=')
+    assert mock_call.mock_calls[0][2]['args'].startswith(b'--')
     assert mock_call.mock_calls[0][2]['method'] == 'POST'
     assert mock_call.mock_calls[0][2]['auth_required'] is True
 
