1c6533df6a67eb792777054420e4fd3ff07b42e2
Brian Coca <brian.coca+git@gmail.com>
2020-05-13 16:33:22 -0400

    changed testing approach

--- test/integration/targets/gathering_facts/runme.sh
+++ test/integration/targets/gathering_facts/runme.sh
@@ -11,4 +11,11 @@ ANSIBLE_GATHERING=smart ansible-playbook test_run_once.yml -i inventory -v "$@"
 # ensure clean_facts is working properly
 ansible-playbook test_prevent_injection.yml -i inventory -v "$@"
 
-ANSIBLE_GATHERING=explicit ansible-playbook facts_merging.yml "$@"
+
+# ensure fact gathering merge order is expected:
+ansible -m gather_facts -e 'ansible_facts_modules=facts_one,facts_two' localhost --playbook-dir ./ > one_two.current
+diff -u one_two.output one_two.current
+
+# now reverse order
+ansible -m gather_facts -e 'ansible_facts_modules=facts_two,facts_one' localhost --playbook-dir ./ > two_one.current
+diff -u two_one.output two_one.current
