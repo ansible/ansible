f2399fe539dc1567da34200314aaac443ce011b4
Matt Davis <mrd@redhat.com>
2020-05-14 15:46:36 -0700

    transplant resource_from_fqcr

--- lib/ansible/utils/collection_loader/__init__.py
+++ lib/ansible/utils/collection_loader/__init__.py
@@ -7,3 +7,17 @@ __metaclass__ = type
 # FIXME: decide what of this we want to actually be public/toplevel, put other stuff on a utility class?
 from ._collection_config import AnsibleCollectionConfig
 from ._collection_finder import AnsibleCollectionRef
+from ansible.module_utils.common.text.converters import to_text
+
+
+def resource_from_fqcr(ref):
+    """
+    Return resource from a fully-qualified collection reference,
+    or from a simple resource name.
+    For fully-qualified collection references, this is equivalent to
+    ``AnsibleCollectionRef.from_fqcr(ref).resource``.
+    :param ref: collection reference to parse
+    :return: the resource as a unicode string
+    """
+    ref = to_text(ref, errors='strict')
+    return ref.split(u'.')[-1]
