ecb2fa10549eefdcb2ba42054660f092d5f61ec9
Bob Weinand <bobwei9@hotmail.com>
2020-05-14 17:46:34 +0200

    Fix filedescriptor out of range in select() when running commands (#65058)
    * Fix filedescriptor out of range in select() when running commands
    
    * Simplify the run_command() code
    
    Now that we're using selectors in run_command(), we can simplify some of
    the code.
    
    * Use fileobj.read() instead of os.read()
    * No longer use get_buffer_size() as we can just slurp all of the data
      instead.
    
    Also use a simpler conditional check of whether the selector map is
    empty
    
    Co-authored-by: Toshio Kuratomi <a.badger@gmail.com>

--- lib/ansible/plugins/connection/ssh.py
+++ lib/ansible/plugins/connection/ssh.py
@@ -289,7 +289,7 @@ from ansible.errors import (
     AnsibleFileNotFound,
 )
 from ansible.errors import AnsibleOptionsError
-from ansible.compat import selectors
+from ansible.module_utils.compat import selectors
 from ansible.module_utils.six import PY3, text_type, binary_type
 from ansible.module_utils.six.moves import shlex_quote
 from ansible.module_utils._text import to_bytes, to_native, to_text
