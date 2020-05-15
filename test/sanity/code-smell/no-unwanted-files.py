38cf7435f879b751dec01aa0bbd7c128cc2d4028
Matt Davis <mrd@redhat.com>
2020-05-15 14:06:10 -0700

    fix builtin routing rename for code smell

--- test/sanity/code-smell/no-unwanted-files.py
+++ test/sanity/code-smell/no-unwanted-files.py
@@ -19,7 +19,7 @@ def main():
     )
 
     skip_paths = set([
-        'lib/ansible/config/runtime.yml',  # not included in the sanity ignore file since it won't exist until after migration
+        'lib/ansible/config/ansible_builtin_runtime.yml',  # not included in the sanity ignore file since it won't exist until after migration
     ])
 
     skip_directories = (
