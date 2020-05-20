a3b5bc036c4f29a7deec251193cb9d4ce04567c9
Brian Coca <bcoca@users.noreply.github.com>
2020-05-20 18:53:37 -0400

    correctly merge multiple facts results (#68987)
    * correctly merge multiple facts results
    
      fixes #68532

---
+++ test/integration/targets/gathering_facts/library/facts_one
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo '{
+    "changed": false,
+    "ansible_facts": {
+        "factsone": "from facts_one module",
+        "common_fact": "also from facts_one module",
+		"common_dict_fact": {
+			"key_one": "from facts_one",
+			"key_two": "from facts_one"
+		},
+		"common_list_fact": [
+			"one",
+			"three",
+			"five"
+		],
+		"common_list_fact2": [
+			"one",
+			"two",
+			"three",
+			"five",
+			"five"
+		]
+    }
+}'
--- test/integration/targets/gathering_facts/runme.sh
+++ test/integration/targets/gathering_facts/runme.sh
@@ -2,11 +2,14 @@
 
 set -eux
 
-# ANSIBLE_CACHE_PLUGINS=cache_plugins/ ANSIBLE_CACHE_PLUGIN=none ansible-playbook test_gathering_facts.yml -i inventory -v "$@"
+#ANSIBLE_CACHE_PLUGINS=cache_plugins/ ANSIBLE_CACHE_PLUGIN=none ansible-playbook test_gathering_facts.yml -i inventory -v "$@"
 ansible-playbook test_gathering_facts.yml -i inventory -v "$@"
-# ANSIBLE_CACHE_PLUGIN=base ansible-playbook test_gathering_facts.yml -i inventory -v "$@"
+#ANSIBLE_CACHE_PLUGIN=base ansible-playbook test_gathering_facts.yml -i inventory -v "$@"
 
 ANSIBLE_GATHERING=smart ansible-playbook test_run_once.yml -i inventory -v "$@"
 
 # ensure clean_facts is working properly
 ansible-playbook test_prevent_injection.yml -i inventory -v "$@"
+
+# ensure fact merging is working properly
+ansible-playbook verify_merge_facts.yml -v "$@" -e 'ansible_facts_parallel: False'
