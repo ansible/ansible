85fea9cf0127341b31efc86e07a0ecf5aedd32d6
Rick Elrod <rick@elrod.me>
2020-05-21 14:10:23 -0500

    another attempt
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- test/integration/targets/cli/test_k_and_K.py
+++ test/integration/targets/cli/test_k_and_K.py
@@ -12,7 +12,7 @@ import pexpect
 os.environ['ANSIBLE_NOCOLOR'] = '1'
 
 out = pexpect.run(
-    'ansible -c ssh -i localhost, -u cliuser1 -m command -a whoami -Kkb --become-user cliuser2 localhost',
+    'ansible -c ssh -i localhost, -u cliuser1 -e ansible_python_interpreter=/usr/bin/python3 -m command -a whoami -Kkb --become-user cliuser2 localhost',
     events={
         'SSH password:': 'secretpassword\n',
         'BECOME password': 'secretpassword\n',
