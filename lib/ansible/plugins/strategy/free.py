e9814b046c9c2e938e5c7aa2e050f23c9246944f
Kevin J. Smith <kevin.j.smith2@cerner.com>
2020-05-22 11:29:28 -0500

    minor change to valide testing failure
    Signed-off-by: Kevin J. Smith <kevin.j.smith2@cerner.com>

--- lib/ansible/plugins/strategy/free.py
+++ lib/ansible/plugins/strategy/free.py
@@ -37,7 +37,7 @@ DOCUMENTATION = '''
                   Ansible will not start a play for a host unless the play can be finished without interruption
                   by tasks for another host, i.e. the number of hosts with an active play does not exceed the
                   number of forks.
-                - Ansible will not wait for other hosts to finish the current task before queuing the next task
+                  Ansible will not wait for other hosts to finish the current task before queuing the next task
                   for a host that has finished.  Once a host is done with the play, it opens it's slot to a new
                   host that was waiting to start.
             default: False
