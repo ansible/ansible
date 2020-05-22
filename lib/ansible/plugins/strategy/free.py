6d4e14492ec960d9d5a8d1eb94f55120d4e16635
Kevin J. Smith <kevin.j.smith2@cerner.com>
2020-05-22 09:49:02 -0500

    ERROR: lib/ansible/plugins/strategy/free.py:37:112: W291: trailing whitespace
    Signed-off-by: Kevin J. Smith <kevin.j.smith2@cerner.com>

--- lib/ansible/plugins/strategy/free.py
+++ lib/ansible/plugins/strategy/free.py
@@ -34,7 +34,7 @@ DOCUMENTATION = '''
             description:
                 - Executes tasks on each host without interruption.
                 - Task execution is as fast as possible per host in batch as defined by C(serial) (default all).
-                  Ansible will not start a play for a host unless the play can be finished without interruption 
+                  Ansible will not start a play for a host unless the play can be finished without interruption
                   by tasks for another host, i.e. the number of hosts with an active play does not exceed the
                   number of forks.
                 - Ansible will not wait for other hosts to finish the current task before queuing the next task
