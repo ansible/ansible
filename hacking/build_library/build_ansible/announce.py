5c2f44f221c04609685b55039aa33b20ca6181a7
Rick Elrod <rick@elrod.me>
2020-05-27 22:59:52 -0500

    unchange copyright date
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- hacking/build_library/build_ansible/announce.py
+++ hacking/build_library/build_ansible/announce.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-# Copyright: (c) 2020, Ansible Project
+# Copyright: (c) 2019, Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 # Make coding more python3-ish
