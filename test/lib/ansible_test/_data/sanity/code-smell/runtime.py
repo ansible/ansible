c75efd0d212ce00743a8117b90df099b765c8e20
John Barker <john@johnrbarker.com>
2020-05-28 13:04:00 +0100

    Correct schema for plugin_router and import_redirect

--- test/lib/ansible_test/_data/sanity/code-smell/runtime.py
+++ test/lib/ansible_test/_data/sanity/code-smell/runtime.py
@@ -1,28 +1,42 @@
 #!/usr/bin/env python
-"""Make sure the data in lib/ansible/config/ansible_builtin_runtime.yml is valid"""
+"""Schema validation of ansible-base's ansible_builtin_runtime.yml and collection's meta/runtime.yml"""
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import glob
-from pathlib import Path
 import os
 import re
 import sys
 import yaml
 
-from voluptuous import All, Any, Match, MultipleInvalid, Required, Schema, ALLOW_EXTRA, PREVENT_EXTRA, All, Any, Invalid, Length, Required, Schema
-from voluptuous import All, Self, ValueInvalid
+from voluptuous import All, Any, Match, MultipleInvalid, Required, Schema, ALLOW_EXTRA, PREVENT_EXTRA, Invalid
+from voluptuous import Length, Required, Schema, ValueInvalid
 from voluptuous.humanize import humanize_error
 
 from ansible.module_utils.six import string_types
 
 
 def main():
-    """Validate ansible-base' ansible_builtin_runtime..yml"""
-    path = 'lib/ansible/config/ansible_builtin_runtime.yml'
+    """Validate runtime"""
+    ansible_builtin_runtime = 'lib/ansible/config/ansible_builtin_runtime.yml'
+    collection_legacy_file = 'meta/routing.yml'
+    collection_runtime_file = 'meta/runtime.yml'
+    path = ''
 
-    if not os.path.isfile(path):
-        # skip if runtime file doesn't exist
+    ## Check if the correct routing files can be found
+
+    if os.path.isfile(ansible_builtin_runtime):
+        # We are in side an ansible-base checkout, so validate ansible_builtin_runtime
+        path = ansible_builtin_runtime
+    elif os.path.isfile(collection_runtime_file):
+        # We are in a collection, so validate meta/routing.yml
+        path = collection_runtime_file
+
+    if os.path.isfile(collection_legacy_file):
+        # Collection contains old name, renamed in #67684
+        print('%s:%d:%d: %s' % (collection_legacy_file, 0, 0, ("Should be called '%s'" % collection_runtime_file)))
+
+    if not path:
+        # In a collection, though no meta/routing
         sys.exit(0)
 
     try:
@@ -39,6 +53,8 @@ def main():
 
     list_string_types = list(string_types)
 
+    ## plugin_routing schema
+
     deprecation_schema = Schema(
         {
             Required('removal_date'): Any(*string_types),
@@ -48,57 +64,74 @@ def main():
         extra=PREVENT_EXTRA
     )
 
-    files_schema = Any(
+    plugin_routing_schema = Any(
         Schema(*string_types),
         Schema({
             ('deprecation'): Any(deprecation_schema),
-            Required('redirect'): Any(*string_types),
+            ('redirect'): Any(*string_types),
         },
             extra=PREVENT_EXTRA)
     )
 
-    list_dict_file_schema = [{str_type: files_schema}
+    list_dict_plugin_routing_schema = [{str_type: plugin_routing_schema}
                              for str_type in string_types]
 
     plugin_schema = Schema({
-        # FIXME: Look at how to pass an array in
-        Required('action'): Any(None, *list_dict_file_schema),
-        Required('become'): Any(None, *list_dict_file_schema),
-        Required('cache'): Any(None, *list_dict_file_schema),
-        Required('callback'): Any(None, *list_dict_file_schema),
-        Required('cliconf'): Any(None, *list_dict_file_schema),
-        Required('connection'): Any(None, *list_dict_file_schema),
-        Required('doc_fragments'): Any(None, *list_dict_file_schema),
-        Required('filter'): Any(None, *list_dict_file_schema),
-        Required('httpapi'): Any(None, *list_dict_file_schema),
-        Required('inventory'): Any(None, *list_dict_file_schema),
-        Required('lookup'): Any(None, *list_dict_file_schema),
-        Required('module_utils'): Any(None, *list_dict_file_schema),
-        Required('modules'): Any(None, *list_dict_file_schema),
-        Required('netconf'): Any(None, *list_dict_file_schema),
-        Required('shell'): Any(None, *list_dict_file_schema),
-        Required('terminal'): Any(None, *list_dict_file_schema),
-        Required('test'): Any(None, *list_dict_file_schema),
+        ('action'): Any(None, *list_dict_plugin_routing_schema),
+        ('become'): Any(None, *list_dict_plugin_routing_schema),
+        ('cache'): Any(None, *list_dict_plugin_routing_schema),
+        ('callback'): Any(None, *list_dict_plugin_routing_schema),
+        ('cliconf'): Any(None, *list_dict_plugin_routing_schema),
+        ('connection'): Any(None, *list_dict_plugin_routing_schema),
+        ('doc_fragments'): Any(None, *list_dict_plugin_routing_schema),
+        ('filter'): Any(None, *list_dict_plugin_routing_schema),
+        ('httpapi'): Any(None, *list_dict_plugin_routing_schema),
+        ('inventory'): Any(None, *list_dict_plugin_routing_schema),
+        ('lookup'): Any(None, *list_dict_plugin_routing_schema),
+        ('module_utils'): Any(None, *list_dict_plugin_routing_schema),
+        ('modules'): Any(None, *list_dict_plugin_routing_schema),
+        ('netconf'): Any(None, *list_dict_plugin_routing_schema),
+        ('shell'): Any(None, *list_dict_plugin_routing_schema),
+        ('terminal'): Any(None, *list_dict_plugin_routing_schema),
+        ('test'): Any(None, *list_dict_plugin_routing_schema),
     },
         extra=PREVENT_EXTRA)
 
-    list_dict_import_schema = [{str_type: files_schema}
+    list_dict_import_schema = [{str_type: plugin_routing_schema }
                              for str_type in string_types]
 
+    ## import_redirection schema
+
+    import_redirection_schema = Any(
+        Schema(*string_types),
+        Schema({
+            ('redirect'): Any(*string_types),
+            # import_redirect doesn't currently support deprecation
+        },
+            extra=PREVENT_EXTRA)
+    )
+
+    list_dict_import_redirection_schema = [{str_type: import_redirection_schema}
+                             for str_type in string_types]
+
+    ## top level schema
+
     schema = Schema({
+        # ansible-base and collections MUST have plugin_routing
         Required('plugin_routing'): Any(plugin_schema),
-        Required('import_redirection'): Any(None, *list_dict_file_schema),
+        # import_redirection is optional
+        ('import_redirection'): Any(None, *list_dict_import_redirection_schema),
     },
         extra=PREVENT_EXTRA
     )
 
-    # Ensure schema is valid
+    ## Ensure schema is valid
 
     try:
         schema(routing)
     except MultipleInvalid as ex:
         for error in ex.errors:
-            # No way to get line numbers
+            # No way to get line/column numbers
             print('%s:%d:%d: %s' % (path, 0, 0, humanize_error(routing, error)))
 
     # This maybe moved to https://github.com/ansible-community/ansibulled
@@ -109,7 +142,6 @@ def main():
 #            found_plugin = False
 #            if os.path.exists('%s/%s.py' % (expected_path, new_name)):
 #                found_plugin = True
-#                # FIXME: For communty.{general,network}'s modules & module_utils should we check in subdirectories?
 #            elif namespace == 'community' and collection in ['general', 'network']:
 #                if plugin_type in ['modules','module_utils']:
 #                    if glob.glob('%s/**/%s.py' % (expected_path, new_name), recursive = True):
