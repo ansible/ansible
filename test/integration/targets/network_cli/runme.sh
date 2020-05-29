0d94499f721d4ba399712a7b3127fadb8c452230
Rick Elrod <rick@elrod.me>
2020-05-29 10:14:07 -0500

    Fix network_cli test to use a trap for cleanup
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- test/integration/targets/network_cli/runme.sh
+++ test/integration/targets/network_cli/runme.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-set -ux # no -e because we want to run teardown no matter waht
+set -eux
 export ANSIBLE_ROLES_PATH=../
+
+function cleanup {
+    ansible-playbook teardown.yml -i "$INVENTORY_PATH" "$@"
+}
+
+trap cleanup EXIT
 
 ansible-playbook setup.yml -i "$INVENTORY_PATH" "$@"
 
@@ -9,11 +15,13 @@ ansible-playbook setup.yml -i "$INVENTORY_PATH" "$@"
 foo=$(mktemp)
 echo hello > "$foo"
 
+# We want to ensure that passwords make it to the network connection plugins
+# because they follow a different path than the rest of the codebase.
+# In setup.yml, we create a passworded user, and now we connect as that user
+# to make sure the password we pass here successfully makes it to the plugin.
 ansible-playbook \
     -i "$INVENTORY_PATH" \
     -e ansible_user=atester \
     -e ansible_password=testymctest \
     -e ansible_ssh_private_key_file="$foo" \
     passworded_user.yml
-
-ansible-playbook teardown.yml -i "$INVENTORY_PATH" "$@"
