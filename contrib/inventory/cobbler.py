c598a9d21878d5e59cd056dec5f7e0a303c02576
Orion Poplawski <orion@nwra.com>
2020-05-30 12:26:57 -0600

    inventory/cobbler.py - various enhancements
    - Add exclude_profiles option
    - Python 3 support
    - Better cache handling
    - --debug option
    - Remove --ignore-settings option
    - Remove environment variable handling
    - Create parent profile groups

--- contrib/inventory/cobbler.py
+++ contrib/inventory/cobbler.py
@@ -25,17 +25,7 @@ ansible talking to it twice.  The first one found will be used. If no
 not add cobbler system names because there is no requirement in cobbler
 that those correspond to addresses.
 
-Tested with Cobbler 2.0.11.
-
-Changelog:
-    - 2015-06-21 dmccue: Modified to support run-once _meta retrieval, results in
-         higher performance at ansible startup.  Groups are determined by owner rather than
-         default mgmt_classes.  DNS name determined from hostname. cobbler values are written
-         to a 'cobbler' fact namespace
-
-    - 2013-09-01 pgehres: Refactored implementation to make use of caching and to
-        limit the number of connections to external cobbler server for performance.
-        Added use of cobbler.ini file to configure settings. Tested with Cobbler 2.4.0
+Tested with Cobbler 2.8.5.
 
 """
 
@@ -59,18 +49,30 @@ Changelog:
 ######################################################################
 
 import argparse
+try:
+    import ConfigParser as configparser
+except ImportError:
+    import configparser
 import os
 import re
+import socket
+import sys
 from time import time
-import xmlrpclib
+try:
+    import xmlrpclib as xmlrpc_client
+except ImportError:
+    import xmlrpc.client as xmlrpc_client
 
-import json
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
-from ansible.module_utils.six import iteritems
-from ansible.module_utils.six.moves import configparser as ConfigParser
+from six import iteritems
 
 
 class CobblerInventory(object):
+    """ Cobbler Inventory object """
 
     def __init__(self):
 
@@ -81,19 +83,26 @@ class CobblerInventory(object):
 
         self.inventory = dict()  # A list of groups and the hosts in that group
         self.cache = dict()  # Details about hosts in the inventory
-        self.ignore_settings = False  # used to only look at env vars for settings.
 
-        # Read env vars, read settings, and parse CLI arguments
-        self.parse_env_vars()
+        # Read settings and parse CLI arguments
         self.read_settings()
         self.parse_cli_args()
 
+        # Options
+        self.exclude_profiles = [] # A list of profiles to exclude
+
         # Cache
-        if self.args.refresh_cache:
-            self.update_cache()
-        elif not self.is_cache_valid():
-            self.update_cache()
+        if self.args.refresh_cache or not self.is_cache_valid():
+            try:
+                self.update_cache()
+            except (socket.gaierror, socket.error, xmlrpc_client.ProtocolError):
+                if self.args.debug:
+                    sys.stderr.write('Cannot update cache, loading cache\n')
+                self.load_inventory_from_cache()
+                self.load_cache_from_cache()
         else:
+            if self.args.debug:
+                sys.stderr.write('Loading cache\n')
             self.load_inventory_from_cache()
             self.load_cache_from_cache()
 
@@ -112,8 +121,8 @@ class CobblerInventory(object):
 
     def _connect(self):
         if not self.conn:
-            for site in self.sites.keys():
-                self.conn[site] = xmlrpclib.Server(self.sites[site]['cobbler_host'], allow_none=True)
+            for site in list(self.sites.keys()):
+                self.conn[site] = xmlrpc_client.Server(self.sites[site]['cobbler_host'], allow_none=True)
                 self.token[site] = None
                 if self.sites[site]['cobbler_username'] is not None:
                     self.token[site] = self.conn[site].login(self.sites[site]['cobbler_username'], self.sites[site]['cobbler_password'])
@@ -133,10 +142,7 @@ class CobblerInventory(object):
     def read_settings(self):
         """ Reads the settings from the cobbler.ini file """
 
-        if(self.ignore_settings):
-            return
-
-        config = ConfigParser.SafeConfigParser()
+        config = configparser.SafeConfigParser()
         config.read(os.path.dirname(os.path.realpath(__file__)) + '/cobbler.ini')
 
         if config.has_option('cobbler', 'host'):
@@ -150,10 +156,10 @@ class CobblerInventory(object):
                 self.sites['default']['cobbler_password'] = config.get('cobbler', 'password')
         else:
             for section in config.sections():
-                m = re.search('^site_(.*)', section)
-                if m is not None:
-                    section = m.group(0)
-                    site = m.group(1)
+                match = re.search('^site_(.*)', section)
+                if match is not None:
+                    section = match.group(0)
+                    site = match.group(1)
                     self.sites[site] = dict()
                     self.sites[site]['cobbler_host'] = config.get(section, 'host')
                     self.sites[site]['cobbler_username'] = None
@@ -163,42 +169,24 @@ class CobblerInventory(object):
                     if config.has_option(section, 'password'):
                         self.sites[site]['cobbler_password'] = config.get(section, 'password')
 
-        self.orderby_keyname = config.get('cobbler', 'orderby_keyname')
+        if config.has_option('cobbler', 'exclude_profiles'):
+            self.exclude_profiles = config.get('cobbler', 'exclude_profiles').split(',')
 
         # Cache related
-        cache_path = config.get('cobbler', 'cache_path')
+        try:
+            cache_path = config.get('cobbler', 'cache_path')
+        except configparser.NoOptionError:
+            cache_path = "$XDG_CACHE_HOME"
+        if os.getenv('XDG_CACHE_HOME') is None:
+            os.environ['XDG_CACHE_HOME'] = os.path.expandvars('$HOME/.cache')
+        cache_path = os.path.expandvars(cache_path)
+        if not os.path.exists(cache_path):
+            os.makedirs(cache_path)
         self.cache_path_cache = cache_path + "/ansible-cobbler.cache"
         self.cache_path_inventory = cache_path + "/ansible-cobbler.index"
         self.cache_max_age = config.getint('cobbler', 'cache_max_age')
 
-    def parse_env_vars(self):
-        """ Reads the settings from the environment """
-
-        # Env. Vars:
-        #   COBBLER_host
-        #   COBBLER_username
-        #   COBBLER_password
-        #   COBBLER_cache_path
-        #   COBBLER_cache_max_age
-        #   COBBLER_ignore_settings
-
-        self.cobbler_host = os.getenv('COBBLER_host', None)
-        self.cobbler_username = os.getenv('COBBLER_username', None)
-        self.cobbler_password = os.getenv('COBBLER_password', None)
-        self.orderby_keyname = os.getenv('COBBLER_orderby_keyname', 'owners')
-
-        # Cache related
-        cache_path = os.getenv('COBBLER_cache_path', None)
-        if(cache_path is not None):
-            self.cache_path_cache = cache_path + "/ansible-cobbler.cache"
-            self.cache_path_inventory = cache_path + "/ansible-cobbler.index"
-
-        self.cache_max_age = int(os.getenv('COBBLER_cache_max_age', "30"))
-
-        # ignore_settings is used to ignore the settings file, for use in Ansible
-        # Tower (or AWX inventory scripts and not throw python exceptions.)
-        if(os.getenv('COBBLER_ignore_settings', False) == "True"):
-            self.ignore_settings = True
+        self.orderby_keyname = config.get('cobbler', 'orderby_keyname')
 
     def parse_cli_args(self):
         """ Command line argument processing """
@@ -206,6 +194,7 @@ class CobblerInventory(object):
         parser = argparse.ArgumentParser(description='Produce an Ansible Inventory file based on Cobbler')
         parser.add_argument('--list', action='store_true', default=True, help='List instances (default: True)')
         parser.add_argument('--host', action='store', help='Get all the variables about a specific instance')
+        parser.add_argument('--debug', action='store_true', default=False, help='Print debug info')
         parser.add_argument('--refresh-cache', action='store_true', default=False,
                             help='Force refresh of cache by making API requests to cobbler (default: False - use cache files)')
         self.args = parser.parse_args()
@@ -216,8 +205,48 @@ class CobblerInventory(object):
         self._connect()
         self.groups = dict()
         self.hosts = dict()
+        self.parent_profiles = dict()
 
-        for site in self.sites.keys():
+        for site in list(self.sites.keys()):
+            if self.args.debug:
+                sys.stderr.write('Processing site %s\n' % site)
+            if self.token[site] is not None:
+                data = self.conn[site].get_profiles(self.token[site])
+            else:
+                data = self.conn[site].get_profiles()
+
+            for profile in data:
+                if profile['parent']:
+                    if self.args.debug:
+                        sys.stderr.write('Processing profile %s with parent %s\n' % (profile['name'], profile['parent']))
+                    if profile['parent'] not in self.exclude_profiles:
+                        if profile['parent'] not in self.inventory:
+                            self.inventory[profile['parent']] = {'children': [], 'hosts': []}
+                        if profile['name'] not in self.inventory[profile['parent']]['children']:
+                            self.inventory[profile['parent']]['children'].append(profile['name'])
+                else:
+                    if self.args.debug:
+                        sys.stderr.write('Processing profile %s without parent\n' % profile['name'])
+                    # Create a heirarchy of profile names
+                    profile_elements = profile['name'].split('-')
+                    i = 0
+                    while i < len(profile_elements)-1:
+                        profile_group = '-'.join(profile_elements[0:i+1])
+                        profile_group_child = '-'.join(profile_elements[0:i+2])
+                        if profile_group in self.exclude_profiles:
+                            if self.args.debug:
+                                sys.stderr.write('Excluding profile %s\n' % profile_group)
+                            break
+                        if profile_group not in self.inventory:
+                            if self.args.debug:
+                                sys.stderr.write('Adding profile %s\n' % profile_group)
+                            self.inventory[profile_group] = {'children': [], 'hosts': []}
+                        if profile_group_child not in self.inventory[profile_group]['children']:
+                            if self.args.debug:
+                                sys.stderr.write('Adding profile child %s to %s\n' % (profile_group_child, profile_group))
+                            self.inventory[profile_group]['children'].append(profile_group_child)
+                        i = i + 1
+
             if self.token[site] is not None:
                 data = self.conn[site].get_systems(self.token[site])
             else:
@@ -226,8 +255,11 @@ class CobblerInventory(object):
             for host in data:
                 # Get the FQDN for the host and add it to the right groups
                 dns_name = host['hostname']  # None
-                ksmeta = None
                 interfaces = host['interfaces']
+                if self.args.debug:
+                    sys.stderr.write('Processing host %s dns_name %s\n' % (host['name'], dns_name))
+                if host['profile'] in self.exclude_profiles:
+                    continue
                 # hostname is often empty for non-static IP hosts
                 if dns_name == '':
                     for (iname, ivalue) in iteritems(interfaces):
@@ -235,6 +267,8 @@ class CobblerInventory(object):
                             this_dns_name = ivalue.get('dns_name', None)
                             if this_dns_name is not None and this_dns_name is not "":
                                 dns_name = this_dns_name
+                                if self.args.debug:
+                                    sys.stderr.write('Set dns_name to %s from %s\n' % (dns_name, iname))
 
                 if dns_name == '':
                     continue
@@ -247,27 +281,36 @@ class CobblerInventory(object):
                     classes = host[self.orderby_keyname]
 
                 if status not in self.inventory:
-                    self.inventory[status] = []
-                self.inventory[status].append(dns_name)
+                    self.inventory[status] = {'hosts': []}
+                self.inventory[status]['hosts'].append(dns_name)
 
+                # Add host to profile group
                 if profile not in self.inventory:
-                    self.inventory[profile] = []
-                self.inventory[profile].append(dns_name)
+                    self.inventory[profile] = {'children': [], 'hosts': []}
+                self.inventory[profile]['hosts'].append(dns_name)
+
+                if self.args.debug:
+                    sys.stderr.write('Adding host %s to profile %s\n' % (dns_name, profile))
 
                 for cls in classes:
                     if cls not in self.inventory:
-                        self.inventory[cls] = []
-                    self.inventory[cls].append(dns_name)
+                        self.inventory[cls] = {'hosts': []}
+                    if self.args.debug:
+                        sys.stderr.write('Adding host %s to class %s\n' % (dns_name, cls))
+                    self.inventory[cls]['hosts'].append(dns_name)
 
                 if site != 'default':
                     if site not in self.inventory:
-                        self.inventory[site] = []
-                    self.inventory[site].append(dns_name)
+                        if self.args.debug:
+                            sys.stderr.write('Adding site %s to inventory\n' % site)
+                        self.inventory[site] = {'hosts': []}
+                    if self.args.debug:
+                        sys.stderr.write('Adding %s to site %s\n' % (dns_name, site))
+                    self.inventory[site]['hosts'].append(dns_name)
 
                 # Since we already have all of the data for the host, update the host details as well
 
                 # The old way was ksmeta only -- provide backwards compatibility
-
                 self.cache[dns_name] = host
                 if "ks_meta" in host:
                     for key, value in iteritems(host["ks_meta"]):
@@ -325,7 +368,7 @@ class CobblerInventory(object):
     def to_safe(self, word):
         """ Converts 'bad' characters in a string to underscores so they can be used as Ansible groups """
 
-        return re.sub(r"[^A-Za-z0-9\-]", "_", word)
+        return re.sub("[^A-Za-z0-9\-]", "_", word)
 
     def json_format_dict(self, data, pretty=False):
         """ Converts a dict to a JSON object and dumps it as a formatted string """
