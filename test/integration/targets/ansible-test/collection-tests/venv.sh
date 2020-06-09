77953c4a474f6ba37bd31e7e9a38f08177dac00c
Matt Clay <matt@mystile.com>
2020-06-08 17:43:58 -0700

    Run each --venv test separately.
    This provides verification that the requirements for each test are properly specified.

--- test/integration/targets/ansible-test/collection-tests/venv.sh
+++ test/integration/targets/ansible-test/collection-tests/venv.sh
@@ -6,12 +6,34 @@ cp -a "${TEST_DIR}/ansible_collections" "${WORK_DIR}"
 cd "${WORK_DIR}/ansible_collections/ns/col"
 
 # common args for all tests
+# each test will be run in a separate venv to verify that requirements have been properly specified
 common=(--venv --python "${ANSIBLE_TEST_PYTHON_VERSION}" --color --truncate 0 "${@}")
 
-# prime the venv to work around issue with PyYAML detection in ansible-test
-ansible-test sanity "${common[@]}" --test ignores
+# sanity tests
 
-# tests
-ansible-test sanity "${common[@]}"
+tests=()
+
+set +x
+
+while IFS='' read -r line; do
+  tests+=("$line");
+done < <(
+  ansible-test sanity --list-tests
+)
+
+set -x
+
+for test in "${tests[@]}"; do
+  rm -rf "tests/output"
+  ansible-test sanity "${common[@]}" --test "${test}"
+done
+
+# unit tests
+
+rm -rf "tests/output"
 ansible-test units "${common[@]}"
+
+# integration tests
+
+rm -rf "tests/output"
 ansible-test integration "${common[@]}"
