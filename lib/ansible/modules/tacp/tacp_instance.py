1c1a4440f8d73f581e0877032e075b1a98a7abdc
Xander Madsen <xander.madsen@gmail.com>
2020-06-10 10:19:00 -0400

    Update tacp_instance.py

--- lib/ansible/modules/tacp/tacp_instance.py
+++ lib/ansible/modules/tacp/tacp_instance.py
@@ -321,7 +321,7 @@ def run_module():
 
     if instance_uuid:
         instance_properties = application_resource.get_by_uuid(
-            instance_uuid)
+            instance_uuid).to_dict()
         current_state = instance_properties['status']
     else:
         if module.params['state'] == 'absent':
