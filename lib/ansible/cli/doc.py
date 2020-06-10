957529169c1d5022705124260d8f29ad501dbcbc
Brian Coca <brian.coca+git@gmail.com>
2020-06-10 16:29:20 -0400

    unifyt

--- lib/ansible/cli/doc.py
+++ lib/ansible/cli/doc.py
@@ -451,6 +451,7 @@ class DocCLI(CLI):
         # Uses a list to get the order right
         ret = []
         for i in finder._get_paths(subdirs=False):
+            i = to_text(i, errors='surrogate_or_strict')
             if i not in ret:
                 ret.append(i)
         return os.pathsep.join(ret)
