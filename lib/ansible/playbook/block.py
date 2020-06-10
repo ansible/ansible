99e8f93ffac4a7aa16f12c94ce189e3172d2b49d
Martin Krizek <martin.krizek@gmail.com>
2020-06-10 19:12:11 +0200

    Do not keep empty blocks after tag filtering (#69987)
    This prevents PlayIterator having to go through empty blocks
    that were created in filter_tagged_tasks. This should
    be a performance improvement for playbooks that mostly skip
    tasks with tags.
    
    ci_complete

--- lib/ansible/playbook/block.py
+++ lib/ansible/playbook/block.py
@@ -371,7 +371,9 @@ class Block(Base, Conditional, CollectionSearch, Taggable):
             tmp_list = []
             for task in target:
                 if isinstance(task, Block):
-                    tmp_list.append(evaluate_block(task))
+                    filtered_block = evaluate_block(task)
+                    if filtered_block.has_tasks():
+                        tmp_list.append(filtered_block)
                 elif (task.action == 'meta' or
                         (task.action == 'include' and task.evaluate_tags([], self._play.skip_tags, all_vars=all_vars)) or
                         task.evaluate_tags(self._play.only_tags, self._play.skip_tags, all_vars=all_vars)):
