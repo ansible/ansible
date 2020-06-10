952d6a603a6208b964d8c4d29b3c92391157b275
Felix Fontein <felix@fontein.de>
2020-06-10 07:40:53 +0200

    Polishing.

--- lib/ansible/cli/doc.py
+++ lib/ansible/cli/doc.py
@@ -21,7 +21,7 @@ from ansible.cli.arguments import option_helpers as opt_help
 from ansible.collections.list import list_collection_dirs
 from ansible.errors import AnsibleError, AnsibleOptionsError
 from ansible.module_utils._text import to_native, to_text
-from ansible.module_utils.common._collections_compat import Container, MutableMapping, Sequence
+from ansible.module_utils.common._collections_compat import Container, Sequence
 from ansible.module_utils.common.json import AnsibleJSONEncoder
 from ansible.module_utils.six import string_types
 from ansible.parsing.plugin_docs import read_docstub
