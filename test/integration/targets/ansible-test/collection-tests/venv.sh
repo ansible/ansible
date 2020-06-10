b610a4cb6d6085f040205505602eb6c92f7a0707
Matt Clay <matt@mystile.com>
2020-06-09 21:53:42 -0700

    Revert "Increase verbosity to debug hang on Shippable."
    This reverts commit 0724057657b72eeb20f0bb358861f3630b0563e0.

--- test/integration/targets/ansible-test/collection-tests/venv.sh
+++ test/integration/targets/ansible-test/collection-tests/venv.sh
@@ -7,7 +7,7 @@ cd "${WORK_DIR}/ansible_collections/ns/col"
 
 # common args for all tests
 # each test will be run in a separate venv to verify that requirements have been properly specified
-common=(--venv --python "${ANSIBLE_TEST_PYTHON_VERSION}" --color --truncate 0 "${@}" -v)
+common=(--venv --python "${ANSIBLE_TEST_PYTHON_VERSION}" --color --truncate 0 "${@}")
 
 # sanity tests
 
