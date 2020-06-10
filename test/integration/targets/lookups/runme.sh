4c1777e26e1f63453de43a39ffef339609598af9
Sam Doran <sdoran@redhat.com>
2020-06-10 19:23:43 -0400

    [stable-2.9] Properly handle unicode in safe_eval (#68576) (#69626)
    * Properly handle unicode in safe_eval
    
    Fixes #66943
    
    * Update lib/ansible/template/safe_eval.py
    
    Co-authored-by: Sam Doran <sdoran@redhat.com>.
    (cherry picked from commit ecd986006ededd3ecfd4fb6704d7a68b3bfba5e1)
    
    Co-authored-by: Martin Krizek <martin.krizek@gmail.com>
    
    Co-authored-by: Martin Krizek <martin.krizek@gmail.com>

--- test/integration/targets/lookups/runme.sh
+++ test/integration/targets/lookups/runme.sh
@@ -13,3 +13,6 @@ ANSIBLE_ROLES_PATH=../ ansible-playbook lookups.yml "$@"
 ansible-playbook template_lookup_vaulted.yml --vault-password-file test_vault_pass "$@"
 
 ansible-playbook -i template_deepcopy/hosts template_deepcopy/playbook.yml "$@"
+
+# https://github.com/ansible/ansible/issues/66943
+ansible-playbook template_lookup_safe_eval_unicode/playbook.yml "$@"
