05d2421bcda6c324a66aced4b287c8b583038503
Brian Coca <bcoca@users.noreply.github.com>
2020-06-11 10:40:25 -0400

    implemented 'prefix' for file based cache (#69872)
    * implemented 'prefix' for file based cache
    
    Co-authored-by: s-hertel <shertel@redhat.com>

--- test/integration/targets/set_fact/runme.sh
+++ test/integration/targets/set_fact/runme.sh
@@ -13,9 +13,16 @@ ANSIBLE_INJECT_FACT_VARS=1 ansible-playbook -i inventory incremental.yml
 ansible-playbook -i inventory nowarn_clean_facts.yml | grep '[WARNING]: Removed restricted key from module data: ansible_ssh_common_args' && exit 1
 
 # test cached feature
-export ANSIBLE_CACHE_PLUGIN=jsonfile ANSIBLE_CACHE_PLUGIN_CONNECTION="${MYTMPDIR}"
+export ANSIBLE_CACHE_PLUGIN=jsonfile ANSIBLE_CACHE_PLUGIN_CONNECTION="${MYTMPDIR}" ANSIBLE_CACHE_PLUGIN_PREFIX=prefix_
 ansible-playbook -i inventory "$@" set_fact_cached_1.yml
 ansible-playbook -i inventory "$@" set_fact_cached_2.yml
+
+# check contents of the fact cache directory before flushing it
+if [[ "$(find "${MYTMPDIR}" -type f)" != $MYTMPDIR/prefix_* ]]; then
+    echo "Unexpected cache file"
+    exit 1
+fi
+
 ansible-playbook -i inventory --flush-cache "$@" set_fact_no_cache.yml
 
 # Test boolean conversions in set_fact
