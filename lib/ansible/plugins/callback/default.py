94df27233f707b0de69889a52c2ec05833e4d514
Brian Coca <bcoca@users.noreply.github.com>
2020-06-17 14:47:03 -0400

    fixed missing default (#69972) (#69985)
    * fixed missing default (#69972)
    
    * fixed missing default
    
     also deprecated so new options wont have to go through this
    
    (cherry picked from commit 805dff41290af657d21b00567c418c338f42e720)
    
    * remove deprecation

--- lib/ansible/plugins/callback/default.py
+++ lib/ansible/plugins/callback/default.py
@@ -43,7 +43,6 @@ from ansible.playbook.task_include import TaskInclude
 from ansible.plugins.callback import CallbackBase
 from ansible.utils.color import colorize, hostcolor
 
-
 # These values use ansible.constants for historical reasons, mostly to allow
 # unmodified derivative plugins to work. However, newer options added to the
 # plugin are not also added to ansible.constants, so authors of derivative
@@ -57,7 +56,8 @@ COMPAT_OPTIONS = (('display_skipped_hosts', C.DISPLAY_SKIPPED_HOSTS),
                   ('display_ok_hosts', True),
                   ('show_custom_stats', C.SHOW_CUSTOM_STATS),
                   ('display_failed_stderr', False),
-                  ('check_mode_markers', False),)
+                  ('check_mode_markers', False),
+                  ('show_per_host_start', False))
 
 
 class CallbackModule(CallbackBase):
