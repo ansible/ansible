cdb70fbf770901860dd5d5df1093d127ee2fc1a4
GomathiselviS <gomathiselvi@gmail.com>
2020-06-17 12:22:05 -0400

    Turn on eapi by default
    Signed-off-by: GomathiselviS <gomathiselvi@gmail.com>

--- test/units/modules/network/eos/test_eos_eapi.py
+++ test/units/modules/network/eos/test_eos_eapi.py
@@ -134,7 +134,7 @@ class TestEosEapiModule(TestEosModule):
 
     def test_eos_eapi_vrf(self):
         set_module_args(dict(vrf='test'))
-        commands = ['management api http-commands', 'vrf test', 'no shutdown']
+        commands = ['management api http-commands', 'no shutdown', 'vrf test', 'no shutdown']
         self.start_unconfigured(changed=True, commands=commands)
 
     def test_eos_eapi_change_from_default_vrf(self):
