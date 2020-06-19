cd7dfcebab5f60802e97f269f21aba30c19ab405
Sam Doran <sdoran@redhat.com>
2020-06-19 12:04:22 -0400

    [stable-2.9] Pin version of netaddr and jmespath in filters test

--- test/integration/targets/filters/runme.sh
+++ test/integration/targets/filters/runme.sh
@@ -7,6 +7,6 @@ source virtualenv.sh
 # Requirements have to be installed prior to running ansible-playbook
 # because plugins and requirements are loaded before the task runs
 
-pip install jmespath netaddr
+pip install jmespath==0.10.0 netaddr==0.7.19
 
 ANSIBLE_ROLES_PATH=../ ansible-playbook filters.yml "$@"
