804f60ce7a8c30753a4f5d022ba1ab3a2c5e2c68
Alicia Cozine <879121+acozine@users.noreply.github.com>
2020-06-22 15:25:44 -0500

    adds japanese translation as a version-switcher option (#70214)
    Co-authored-by: Alicia Cozine <acozine@users.noreply.github.com>

--- docs/docsite/rst/conf.py
+++ docs/docsite/rst/conf.py
@@ -147,7 +147,7 @@ html_context = {
     'current_version': version,
     'latest_version': '2.9',
     # list specifically out of order to make latest work
-    'available_versions': ('latest', '2.8', '2.7', 'devel'),
+    'available_versions': ('latest', '2.9_ja', '2.8', '2.7', 'devel'),
     'css_files': ('_static/ansible.css',  # overrides to the standard theme
                   ),
 }
