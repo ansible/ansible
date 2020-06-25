ba1092621d5362ee3785b5675ba53999ce95e3b0
Matt Martz <matt@sivel.net>
2020-06-25 10:49:49 -0500

    Test against galaxy_ng

--- test/lib/ansible_test/_internal/cloud/galaxy.py
+++ test/lib/ansible_test/_internal/cloud/galaxy.py
@@ -28,11 +28,19 @@ from ..docker_util import (
 )
 
 
+# We add BasicAuthentication, to make the tasks that deal with
+# direct API access easier to deal with across galaxy_ng and pulp
 SETTINGS = b'''
-CONTENT_ORIGIN='pulp:80'
-ANSIBLE_API_HOSTNAME='http://pulp:80'
-ANSIBLE_CONTENT_HOSTNAME='http://pulp:80/pulp/content'
-TOKEN_AUTH_DISABLED=True
+CONTENT_ORIGIN = 'http://pulp:80'
+ANSIBLE_API_HOSTNAME = 'http://pulp:80'
+ANSIBLE_CONTENT_HOSTNAME = 'http://pulp:80/pulp/content'
+TOKEN_AUTH_DISABLED = True
+GALAXY_REQUIRE_CONTENT_APPROVAL = False
+GALAXY_AUTHENTICATION_CLASSES = [
+    "rest_framework.authentication.SessionAuthentication",
+    "rest_framework.authentication.TokenAuthentication",
+    "rest_framework.authentication.BasicAuthentication",
+]
 '''
 
 SET_ADMIN_PASSWORD = b'''#!/usr/bin/execlineb -S0
@@ -42,7 +50,8 @@ foreground {
   export DJANGO_SETTINGS_MODULE pulpcore.app.settings
   export PULP_CONTENT_ORIGIN localhost
   s6-setuidgid postgres
-  /usr/local/bin/django-admin reset-admin-password --password password
+  if { /usr/local/bin/django-admin reset-admin-password --password password }
+  if { /usr/local/bin/pulpcore-manager create-group system:partner-engineers --users admin }
 }
 '''
 
@@ -50,9 +59,8 @@ foreground {
 class GalaxyProvider(CloudProvider):
     """Galaxy plugin.
 
-    Sets up fallaxy and pulp (ansible-galaxy) servers for tests.
+    Sets up pulp (ansible-galaxy) servers for tests.
 
-    The fallaxy source itself resides at: https://github.com/ansible/fallaxy-test-container
     The pulp source itself resides at: https://github.com/pulp/pulp-oci-images
     """
 
@@ -62,10 +70,6 @@ class GalaxyProvider(CloudProvider):
         """
         super(GalaxyProvider, self).__init__(args)
 
-        self.fallaxy = os.environ.get(
-            'ANSIBLE_FALLAXY_CONTAINER',
-            'quay.io/ansible/fallaxy-test-container:2.0.1'
-        )
         self.pulp = os.environ.get(
             'ANSIBLE_PULP_CONTAINER',
             'docker.io/pulp/pulp-fedora31@sha256:71054f92fc9c986ba823d86b68631bafc84ae61b7832ce0be1f8e74423e56f64'
@@ -102,53 +106,11 @@ class GalaxyProvider(CloudProvider):
         if container_id:
             display.info('Running in docker container: %s' % container_id, verbosity=1)
 
-        f_results = docker_inspect(self.args, 'fallaxy-stub')
         p_results = docker_inspect(self.args, 'pulp')
-
-        if f_results and not f_results[0].get('State', {}).get('Running'):
-            docker_rm(self.args, 'fallaxy-stub')
-            f_results = []
 
         if p_results and not p_results[0].get('State', {}).get('Running'):
             docker_rm(self.args, 'pulp')
             p_results = []
-
-        display.info('%s fallaxy-stub docker container.'
-                     % ('Using the existing' if f_results else 'Starting a new'),
-                     verbosity=1)
-
-        fallaxy_port = 8080
-        fallaxy_token = str(uuid.uuid4()).replace('-', '')
-
-        if not f_results:
-            if self.args.docker or container_id:
-                publish_ports = []
-            else:
-                # publish the simulator ports when not running inside docker
-                publish_ports = [
-                    '-p', ':'.join((str(fallaxy_port),) * 2),
-                ]
-
-            docker_pull(self.args, self.fallaxy)
-
-            docker_run(
-                self.args,
-                self.fallaxy,
-                ['-d', '--name', 'fallaxy-stub', '-e', 'FALLAXY_TOKEN=%s' % fallaxy_token] + publish_ports,
-            )
-            self.containers.append('fallaxy-stub')
-
-        if self.args.docker:
-            fallaxy_host = 'fallaxy-stub'
-        elif container_id:
-            fallaxy_host = self._get_simulator_address('fallaxy-stub')
-            display.info('Found Galaxy simulator container address: %s' % fallaxy_host, verbosity=1)
-        else:
-            fallaxy_host = 'localhost'
-
-        self._set_cloud_config('FALLAXY_HOST', fallaxy_host)
-        self._set_cloud_config('FALLAXY_PORT', str(fallaxy_port))
-        self._set_cloud_config('FALLAXY_TOKEN', fallaxy_token)
 
         display.info('%s pulp docker container.'
                      % ('Using the existing' if p_results else 'Starting a new'),
@@ -162,7 +124,7 @@ class GalaxyProvider(CloudProvider):
             else:
                 # publish the simulator ports when not running inside docker
                 publish_ports = [
-                    '-p', ':'.join((str(fallaxy_port),) * 2),
+                    '-p', ':'.join((str(pulp_port),) * 2),
                 ]
 
             docker_pull(self.args, self.pulp)
@@ -216,7 +178,7 @@ class GalaxyProvider(CloudProvider):
 
         :rtype: list[str]
         """
-        return ['--link', 'fallaxy-stub', '--link', 'pulp']  # if self.managed else []
+        return ['--link', 'pulp']  # if self.managed else []
 
     def cleanup(self):
         """Clean up the resource and temporary configs files after tests."""
@@ -240,9 +202,6 @@ class GalaxyEnvironment(CloudEnvironment):
         """
         :rtype: CloudEnvironmentConfig
         """
-        fallaxy_token = self._get_cloud_config('FALLAXY_TOKEN')
-        fallaxy_host = self._get_cloud_config('FALLAXY_HOST')
-        fallaxy_port = self._get_cloud_config('FALLAXY_PORT')
         pulp_user = self._get_cloud_config('PULP_USER')
         pulp_password = self._get_cloud_config('PULP_PASSWORD')
         pulp_host = self._get_cloud_config('PULP_HOST')
@@ -250,22 +209,18 @@ class GalaxyEnvironment(CloudEnvironment):
 
         return CloudEnvironmentConfig(
             ansible_vars=dict(
-                fallaxy_token=fallaxy_token,
-                fallaxy_galaxy_server='http://%s:%s/api/' % (fallaxy_host, fallaxy_port),
-                fallaxy_ah_server='http://%s:%s/api/automation-hub/' % (fallaxy_host, fallaxy_port),
                 pulp_user=pulp_user,
                 pulp_password=pulp_password,
-                pulp_v2_server='http://%s:%s/pulp_ansible/galaxy/ansible_ci/api/' % (pulp_host, pulp_port),
-                pulp_v3_server='http://%s:%s/pulp_ansible/galaxy/ansible_ci/api/' % (pulp_host, pulp_port),
+                pulp_v2_server='http://%s:%s/pulp_ansible/galaxy/automation-hub/api/' % (pulp_host, pulp_port),
+                pulp_v3_server='http://%s:%s/pulp_ansible/galaxy/automation-hub/api/' % (pulp_host, pulp_port),
                 pulp_api='http://%s:%s' % (pulp_host, pulp_port),
+                galaxy_ng_server='http://%s:%s/api/galaxy/' % (pulp_host, pulp_port),
             ),
             env_vars=dict(
-                FALLAXY_TOKEN=fallaxy_token,
-                FALLAXY_GALAXY_SERVER='http://%s:%s/api/' % (fallaxy_host, fallaxy_port),
-                FALLAXY_AH_SERVER='http://%s:%s/api/automation-hub/' % (fallaxy_host, fallaxy_port),
                 PULP_USER=pulp_user,
                 PULP_PASSWORD=pulp_password,
-                PULP_V2_SERVER='http://%s:%s/pulp_ansible/galaxy/ansible_ci/api/' % (pulp_host, pulp_port),
-                PULP_V3_SERVER='http://%s:%s/pulp_ansible/galaxy/ansible_ci/api/' % (pulp_host, pulp_port),
+                PULP_V2_SERVER='http://%s:%s/pulp_ansible/galaxy/automation-hub/api/' % (pulp_host, pulp_port),
+                PULP_V3_SERVER='http://%s:%s/pulp_ansible/galaxy/automation-hub/api/' % (pulp_host, pulp_port),
+                GALAXY_NG_SERVER='http://%s:%s/api/galaxy/' % (pulp_host, pulp_port),
             ),
         )
