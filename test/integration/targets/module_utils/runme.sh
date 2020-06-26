3204d260ddb142a75453aa1a8ea8d89cc90a87a0
Matt Clay <matt@mystile.com>
2020-06-26 16:13:28 -0700

    [stable-2.10] Add integration tests for basic.py _set_cwd.
    These tests verify that AnsibleModule can be instantiated when cwd does not exist or is unreadable.
    (cherry picked from commit d6fb42d1c5f5ffc9d16a9c227705541fab6f59c8)
    
    Co-authored-by: Matt Clay <matt@mystile.com>

--- test/integration/targets/module_utils/runme.sh
+++ test/integration/targets/module_utils/runme.sh
@@ -2,5 +2,7 @@
 
 set -eux
 
+ANSIBLE_ROLES_PATH=../ ansible-playbook module_utils_basic_setcwd.yml -i ../../inventory "$@"
+
 ansible-playbook module_utils_test.yml -i ../../inventory -v "$@"
 ANSIBLE_MODULE_UTILS=other_mu_dir ansible-playbook module_utils_envvar.yml -i ../../inventory -v "$@"
