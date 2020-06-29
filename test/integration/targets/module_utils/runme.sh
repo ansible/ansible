0423c9e95f81bba870c939d623c3792c72be912c
Rick Elrod <rick@elrod.me>
2020-06-29 14:34:53 -0500

    add -vvvvv to act as an integration test
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- test/integration/targets/module_utils/runme.sh
+++ test/integration/targets/module_utils/runme.sh
@@ -4,5 +4,8 @@ set -eux
 
 ANSIBLE_ROLES_PATH=../ ansible-playbook module_utils_basic_setcwd.yml -i ../../inventory "$@"
 
-ansible-playbook module_utils_test.yml -i ../../inventory -v "$@"
+# Keep the -vvvvv here. This acts as a test for testing that higher verbosity
+# doesn't traceback with unicode in the custom module_utils directory path.
+ansible-playbook module_utils_test.yml -i ../../inventory -vvvvv "$@"
+
 ANSIBLE_MODULE_UTILS=other_mu_dir ansible-playbook module_utils_envvar.yml -i ../../inventory -v "$@"
