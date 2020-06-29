cc2cee6980dc53378b16d42c7988524c5e1be83b
Rick Elrod <rick@elrod.me>
2020-06-29 18:02:03 -0500

    Fix -vvvvv unicode error in executor.module_common (#70368)
    Change:
    - Fix a UnicodeDecodeError in executor.module_common that could get
      triggered with -vvvvv.
    
    Test Plan:
    - `ansible-test integration --docker centos7 module_utils -vvvvv`
      This would show the error previously, and no loner does after this
      patch.
    
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- test/integration/targets/module_utils/runme.sh
+++ test/integration/targets/module_utils/runme.sh
@@ -4,5 +4,10 @@ set -eux
 
 ANSIBLE_ROLES_PATH=../ ansible-playbook module_utils_basic_setcwd.yml -i ../../inventory "$@"
 
+# Keep the -vvvvv here. This acts as a test for testing that higher verbosity
+# doesn't traceback with unicode in the custom module_utils directory path.
+ansible-playbook module_utils_vvvvv.yml -i ../../inventory -vvvvv "$@"
+
 ansible-playbook module_utils_test.yml -i ../../inventory -v "$@"
+
 ANSIBLE_MODULE_UTILS=other_mu_dir ansible-playbook module_utils_envvar.yml -i ../../inventory -v "$@"
