82287b52fe2262a39670b0caa9d3dfdfe2d42d02
Rick Elrod <rick@elrod.me>
2020-06-29 16:59:18 -0500

    v^5 not v^6
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- test/integration/targets/module_utils/runme.sh
+++ test/integration/targets/module_utils/runme.sh
@@ -6,7 +6,7 @@ ANSIBLE_ROLES_PATH=../ ansible-playbook module_utils_basic_setcwd.yml -i ../../i
 
 # Keep the -vvvvv here. This acts as a test for testing that higher verbosity
 # doesn't traceback with unicode in the custom module_utils directory path.
-ansible-playbook module_utils_vvvvvv.yml -i ../../inventory -vvvvv "$@"
+ansible-playbook module_utils_vvvvv.yml -i ../../inventory -vvvvv "$@"
 
 ansible-playbook module_utils_test.yml -i ../../inventory -v "$@"
 
