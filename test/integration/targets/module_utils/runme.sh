81655a57be64ad1a5ac5891d07799126dcd0aae8
Rick Elrod <rick@elrod.me>
2020-07-01 09:28:48 -0500

    Throw a prettier error in m_u.basic syslog (#70312)
    Change:
    - In certain situations, such as when the input string contains null
      bytes (\0), syslog.syslog will throw a TypeError. Handle that and
      fail_json instead.
    
    Test Plan:
    - New test
    - ansible-test --docker centos[68] (for py2 and py3 respectively)
    
    Tickets:
    - Refs #70269
    
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- test/integration/targets/module_utils/runme.sh
+++ test/integration/targets/module_utils/runme.sh
@@ -8,6 +8,6 @@ ANSIBLE_ROLES_PATH=../ ansible-playbook module_utils_basic_setcwd.yml -i ../../i
 # doesn't traceback with unicode in the custom module_utils directory path.
 ansible-playbook module_utils_vvvvv.yml -i ../../inventory -vvvvv "$@"
 
-ansible-playbook module_utils_test.yml -i ../../inventory -v "$@"
+ansible-playbook module_utils_test.yml -i ../../inventory -e output_dir="$OUTPUT_DIR" -v "$@"
 
 ANSIBLE_MODULE_UTILS=other_mu_dir ansible-playbook module_utils_envvar.yml -i ../../inventory -v "$@"
