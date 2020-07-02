cf55996307fa214b702fb0fcade1dca7915e8dc9
Rick Elrod <rick@elrod.me>
2020-07-02 12:37:34 -0500

    command module, nix warnings for moved modules
    Change:
    - Remove warnings from command module which point to modules that no
      longer ship with ansible-base but have moved to collections.
    
    Test Plan:
    - CI
    
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- lib/ansible/modules/command.py
+++ lib/ansible/modules/command.py
@@ -215,10 +215,10 @@ def check_command(module, commandline):
                  'rmdir': 'state=absent', 'rm': 'state=absent', 'touch': 'state=touch'}
     commands = {'curl': 'get_url or uri', 'wget': 'get_url or uri',
                 'svn': 'subversion', 'service': 'service',
-                'mount': 'mount', 'rpm': 'yum, dnf or zypper', 'yum': 'yum', 'apt-get': 'apt',
+                'yum': 'yum', 'apt-get': 'apt',
                 'tar': 'unarchive', 'unzip': 'unarchive', 'sed': 'replace, lineinfile or template',
-                'dnf': 'dnf', 'zypper': 'zypper'}
-    become = ['sudo', 'su', 'pbrun', 'pfexec', 'runas', 'pmrun', 'machinectl']
+                'dnf': 'dnf'}
+    become = ['sudo', 'su', 'runas']
     if isinstance(commandline, list):
         command = commandline[0]
     else:
