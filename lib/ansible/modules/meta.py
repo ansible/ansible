40591d5fbbe9878427fc5b1b46ec820f69feba1a
Baptiste Mille-Mathias <baptiste.millemathias@gmail.com>
2020-07-14 17:38:15 +0200

    Document tags are not supported with task meta. (#70590)
    fixes #70338

--- lib/ansible/modules/meta.py
+++ lib/ansible/modules/meta.py
@@ -40,6 +40,7 @@ notes:
     - C(clear_facts) will remove the persistent facts from M(set_fact) using C(cacheable=True),
       but not the current host variable it creates for the current run.
     - Looping on meta tasks is not supported.
+    - Skipping C(meta) tasks using tags is not supported.
     - This module is also supported for Windows targets.
 seealso:
 - module: assert
