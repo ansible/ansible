cd914a49751dc2597d75e3d31b753f673749444d
Matt Martz <matt@sivel.net>
2020-07-14 10:56:26 -0500

    [stable-2.10] Allow single vault encrypted values to be used directly as module parameters. Fixes #68275 (#70607) (cherry picked from commit a77dbf0)
    Co-authored-by: Matt Martz <matt@sivel.net>

--- lib/ansible/module_utils/common/json.py
+++ lib/ansible/module_utils/common/json.py
@@ -36,15 +36,19 @@ class AnsibleJSONEncoder(json.JSONEncoder):
     Simple encoder class to deal with JSON encoding of Ansible internal types
     '''
 
-    def __init__(self, preprocess_unsafe=False, **kwargs):
+    def __init__(self, preprocess_unsafe=False, vault_to_text=False, **kwargs):
         self._preprocess_unsafe = preprocess_unsafe
+        self._vault_to_text = vault_to_text
         super(AnsibleJSONEncoder, self).__init__(**kwargs)
 
     # NOTE: ALWAYS inform AWS/Tower when new items get added as they consume them downstream via a callback
     def default(self, o):
         if getattr(o, '__ENCRYPTED__', False):
             # vault object
-            value = {'__ansible_vault': to_text(o._ciphertext, errors='surrogate_or_strict', nonstring='strict')}
+            if self._vault_to_text:
+                value = to_text(o, errors='surrogate_or_strict')
+            else:
+                value = {'__ansible_vault': to_text(o._ciphertext, errors='surrogate_or_strict', nonstring='strict')}
         elif getattr(o, '__UNSAFE__', False):
             # unsafe object, this will never be triggered, see ``AnsibleJSONEncoder.iterencode``
             value = {'__ansible_unsafe': to_text(o, errors='surrogate_or_strict', nonstring='strict')}
