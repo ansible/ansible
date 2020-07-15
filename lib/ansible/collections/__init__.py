9cb23dbfc6be4d7d5333268e94fc2142740ffd87
Brian Coca <brian.coca+git@gmail.com>
2020-07-15 17:09:27 -0400

    no need for dir check

--- lib/ansible/collections/__init__.py
+++ lib/ansible/collections/__init__.py
@@ -20,10 +20,9 @@ def has_collection_flag(path):
 
     is_coll = False
     b_path = to_bytes(path)
-    if os.path.isdir(b_path):
-        for b_flag in B_FLAG_FILES:
-            if os.path.exists(os.path.join(b_path, b_flag)):
-                is_coll = True
-                break
+    for b_flag in B_FLAG_FILES:
+        if os.path.exists(os.path.join(b_path, b_flag)):
+            is_coll = True
+            break
 
     return is_coll
