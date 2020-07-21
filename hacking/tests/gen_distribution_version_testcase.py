edbc89eff105e2a24061bf85c7f9af81c2e57641
Abhijeet Kasurde <akasurde@redhat.com>
2020-07-21 23:17:11 +0530

    distribution: Add support for DragonFly (#70748)
    partially fixes #43739
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- hacking/tests/gen_distribution_version_testcase.py
+++ hacking/tests/gen_distribution_version_testcase.py
@@ -12,9 +12,10 @@ This assumes a working ansible version in the path.
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import os.path
-import subprocess
 import json
+import os.path
+import platform
+import subprocess
 import sys
 
 from ansible.module_utils import distro
@@ -90,5 +91,13 @@ output = {
     'platform.dist': dist,
     'result': ansible_facts,
 }
+
+system = platform.system()
+if system != 'Linux':
+    output['platform.system'] = system
+
+release = platform.release()
+if release:
+    output['platform.release'] = release
 
 print(json.dumps(output, indent=4))
