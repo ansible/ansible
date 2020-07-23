f9c16422bf7c399ff7c495cd84939b12802c9eeb
Brian Coca <brian.coca+git@gmail.com>
2020-07-23 19:59:12 -0400

    remove unused return vars

--- lib/ansible/executor/task_executor.py
+++ lib/ansible/executor/task_executor.py
@@ -36,8 +36,6 @@ from ansible.utils.vars import combine_vars, isidentifier
 display = Display()
 
 
-RETURN_VARS = [x for x in C.MAGIC_VARIABLE_MAPPING.items() if 'become' not in x and '_pass' not in x]
-
 __all__ = ['TaskExecutor']
 
 
