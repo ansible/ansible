e0aa443a882f504cd69681bbb0f007e8e8e9ef64
Mykola Grygoriev <nickgryg@gmail.com>
2020-07-24 01:35:10 -0400

    Get serial number of NVMe device without sg_inq (#70284)
    * Get serial number of NVMe device without sg_inq
    
    * Add changlelog for #70284
    
    * Use get_file_content() to get serial number from a file

--- lib/ansible/module_utils/facts/hardware/linux.py
+++ lib/ansible/module_utils/facts/hardware/linux.py
@@ -700,6 +700,9 @@ class LinuxHardware(Hardware):
 
             sg_inq = self.module.get_bin_path('sg_inq')
 
+            # we can get NVMe device's serial number from /sys/block/<name>/device/serial
+            serial_path = "/sys/block/%s/device/serial" % (block)
+
             if sg_inq:
                 device = "/dev/%s" % (block)
                 rc, drivedata, err = self.module.run_command([sg_inq, device])
@@ -707,6 +710,10 @@ class LinuxHardware(Hardware):
                     serial = re.search(r"Unit serial number:\s+(\w+)", drivedata)
                     if serial:
                         d['serial'] = serial.group(1)
+            else:
+                serial = get_file_content(serial_path)
+                if serial:
+                    d['serial'] = serial
 
             for key, test in [('removable', '/removable'),
                               ('support_discard', '/queue/discard_granularity'),
