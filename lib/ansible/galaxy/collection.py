8862a01fb29498bdd204b25978b3d22e5697c655
Jordan Borean <jborean93@gmail.com>
2020-07-30 07:28:43 +1000

    ansible-galaxy - fix fallback for AH searches (#70957)

--- lib/ansible/galaxy/collection.py
+++ lib/ansible/galaxy/collection.py
@@ -525,11 +525,17 @@ class CollectionRequirement:
                 else:
                     versions = api.get_collection_versions(namespace, name)
             except GalaxyError as err:
-                if err.http_code == 404:
-                    display.vvv("Collection '%s' is not available from server %s %s"
-                                % (collection, api.name, api.api_server))
-                    continue
-                raise
+                if err.http_code != 404:
+                    raise
+
+                versions = []
+
+            # Automation Hub doesn't return a 404 but an empty version list so we check that to align both AH and
+            # Galaxy when the collection is not available on that server.
+            if not versions:
+                display.vvv("Collection '%s' is not available from server %s %s" % (collection, api.name,
+                                                                                    api.api_server))
+                continue
 
             display.vvv("Collection '%s' obtained from server %s %s" % (collection, api.name, api.api_server))
             break
