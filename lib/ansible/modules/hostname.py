fcea87926f8311cefcf64b5d3ca95b13b44c9c67
Quentin Dawans <contact@ovv.wtf>
2020-07-30 10:22:19 +0200

    Support manjaro arm in hostname module (#69861)

--- lib/ansible/modules/hostname.py
+++ lib/ansible/modules/hostname.py
@@ -640,6 +640,12 @@ class ManjaroHostname(Hostname):
     strategy_class = SystemdStrategy
 
 
+class ManjaroARMHostname(Hostname):
+    platform = 'Linux'
+    distribution = 'Manjaro-arm'
+    strategy_class = SystemdStrategy
+
+
 class RHELHostname(Hostname):
     platform = 'Linux'
     distribution = 'Redhat'
