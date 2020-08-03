a974a8212e8a50a944d05abad812eef845ad0167
mnecas <necas.marty@gmail.com>
2020-08-03 18:13:44 +0200

    fix formats

--- lib/ansible/modules/cloud/ovirt/ovirt_vm.py
+++ lib/ansible/modules/cloud/ovirt/ovirt_vm.py
@@ -1693,10 +1693,10 @@ class VmsModule(BaseModule):
 
     def __get_cd_id(self):
         sds_service = self._connection.system_service().storage_domains_service()
-        sds = sds_service.list(search='name="{}"'.format(self.param('storage_domain') if self.param('storage_domain') else "*"))
+        sds = sds_service.list(search='name="{0}"'.format(self.param('storage_domain') if self.param('storage_domain') else "*"))
         disks = self.__get_cds_from_sds(sds)
         if not disks:
-            raise ValueError('Was not able to find disk with name or id "{}".'.format(self.param('cd_iso')))
+            raise ValueError('Was not able to find disk with name or id "{0}".'.format(self.param('cd_iso')))
         if len(disks) > 1:
             raise ValueError('Found mutiple disks with same name "{0}" please use \
                 disk ID in "cd_iso" to specify which disk should be used.'.format(self.param('cd_iso')))
