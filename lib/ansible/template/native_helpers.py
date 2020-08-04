5462ce1bd62263c1548aae3a0c59d07ba5a8e0ff
Martin Krizek <martin.krizek@gmail.com>
2020-08-04 19:06:07 +0200

    native types: properly handle Undefined in nested data (#68432)

--- lib/ansible/template/native_helpers.py
+++ lib/ansible/template/native_helpers.py
@@ -13,9 +13,33 @@ import types
 from jinja2.runtime import StrictUndefined
 
 from ansible.module_utils._text import to_text
+from ansible.module_utils.common.collections import is_sequence, Mapping
 from ansible.module_utils.common.text.converters import container_to_text
 from ansible.module_utils.six import PY2
 from ansible.parsing.yaml.objects import AnsibleVaultEncryptedUnicode
+
+
+def _fail_on_undefined(data):
+    """Recursively find an undefined value in a nested data structure
+    and properly raise the undefined exception.
+    """
+    if isinstance(data, Mapping):
+        for value in data.values():
+            _fail_on_undefined(value)
+    elif is_sequence(data):
+        for item in data:
+            _fail_on_undefined(item)
+    else:
+        if isinstance(data, StrictUndefined):
+            # To actually raise the undefined exception we need to
+            # access the undefined object otherwise the exception would
+            # be raised on the next access which might not be properly
+            # handled.
+            # See https://github.com/ansible/ansible/issues/52158
+            # and StrictUndefined implementation in upstream Jinja2.
+            str(data)
+
+    return data
 
 
 def ansible_native_concat(nodes):
@@ -24,38 +48,24 @@ def ansible_native_concat(nodes):
     concatenated as strings. If the result can be parsed with
     :func:`ast.literal_eval`, the parsed value is returned. Otherwise, the
     string is returned.
+
+    https://github.com/pallets/jinja/blob/master/src/jinja2/nativetypes.py
     """
-
-    # https://github.com/pallets/jinja/blob/master/jinja2/nativetypes.py
-
     head = list(islice(nodes, 2))
 
     if not head:
         return None
 
     if len(head) == 1:
-        out = head[0]
+        out = _fail_on_undefined(head[0])
 
         # TODO send unvaulted data to literal_eval?
         if isinstance(out, AnsibleVaultEncryptedUnicode):
             return out.data
-
-        if isinstance(out, StrictUndefined):
-            # A hack to raise proper UndefinedError/AnsibleUndefinedVariable exception.
-            # We need to access the AnsibleUndefined(StrictUndefined) object by either of the following:
-            # __iter__, __str__, __len__, __nonzero__, __eq__, __ne__, __bool__, __hash__
-            # to actually raise the exception.
-            # (see Jinja2 source of StrictUndefined to get up to date info)
-            # Otherwise the undefined error would be raised on the next access which might not be properly handled.
-            # See https://github.com/ansible/ansible/issues/52158
-            # We do that only here because it is taken care of by to_text() in the else block below already.
-            str(out)
     else:
         if isinstance(nodes, types.GeneratorType):
             nodes = chain(head, nodes)
-        # Stringifying the nodes is important as it takes care of
-        # StrictUndefined by side-effect - by raising an exception.
-        out = u''.join([to_text(v) for v in nodes])
+        out = u''.join([to_text(_fail_on_undefined(v)) for v in nodes])
 
     try:
         out = literal_eval(out)
