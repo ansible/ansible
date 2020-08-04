a09c9e07e09a5c441e07901c774505c1e5c7ae7c
Rick Elrod <rick@elrod.me>
2020-08-04 17:33:22 -0500

    comment
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- test/integration/targets/connection_ssh/runme.sh
+++ test/integration/targets/connection_ssh/runme.sh
@@ -59,12 +59,12 @@ else
     scp_args=("-e" "")
 fi
 
-# sftp
-./posix.sh "$@"
-# scp
-ANSIBLE_SCP_IF_SSH=true ./posix.sh "$@" "${scp_args[@]}"
-# piped
-ANSIBLE_SSH_TRANSFER_METHOD=piped ./posix.sh "$@"
+## sftp
+#./posix.sh "$@"
+## scp
+#ANSIBLE_SCP_IF_SSH=true ./posix.sh "$@" "${scp_args[@]}"
+## piped
+#ANSIBLE_SSH_TRANSFER_METHOD=piped ./posix.sh "$@"
 
 echo 'made it here'
 
