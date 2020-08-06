cd58db5e725636fda5b09397b9fa54720b4367da
Rick Elrod <rick@elrod.me>
2020-08-06 09:45:37 -0500

    [stable-2.10] virt facts: allow guest/host to have >1 virt tech (#70832)
    Change:
    - Allow systems to declare multiple virt techs. For example if a system
      is both a docker container, but virtualized on KVM, show both. If a
      system is set up to run virtualbox and KVM VMs, show both.
    
    - This is done by introducing new facts keys:
      - virtualization_tech_guest
      - virtualization_tech_host
    
    - Backwards compatibility is preserved by keeping track of the previous
      return-points and refusing to update those keys after we would have
      returned, but now returning them at the end, so that the new keys can
      accumulate their data.
    
    Test Plan:
    - Local
    - CI
    
    Tickets:
    - Refs #66304
    - Refs #17151
    - Refs #17058
    - Probably others
    
    Signed-off-by: Rick Elrod <rick@elrod.me>.
    (cherry picked from commit 4e55b936132c0363e23708cfbbe5e5f53e2b5cdf)
    
    Co-authored-by: Rick Elrod <rick@elrod.me>

--- lib/ansible/module_utils/facts/virtual/freebsd.py
+++ lib/ansible/module_utils/facts/virtual/freebsd.py
@@ -32,23 +32,45 @@ class FreeBSDVirtual(Virtual, VirtualSysctlDetectionMixin):
 
     def get_virtual_facts(self):
         virtual_facts = {}
+        host_tech = set()
+        guest_tech = set()
+
         # Set empty values as default
         virtual_facts['virtualization_type'] = ''
         virtual_facts['virtualization_role'] = ''
 
         if os.path.exists('/dev/xen/xenstore'):
+            guest_tech.add('xen')
             virtual_facts['virtualization_type'] = 'xen'
             virtual_facts['virtualization_role'] = 'guest'
 
-        if virtual_facts['virtualization_type'] == '':
-            virtual_product_facts = self.detect_virt_product('kern.vm_guest') or self.detect_virt_product(
-                'hw.hv_vendor') or self.detect_virt_product('security.jail.jailed')
-            virtual_facts.update(virtual_product_facts)
+        kern_vm_guest = self.detect_virt_product('kern.vm_guest')
+        guest_tech.update(kern_vm_guest['virtualization_tech_guest'])
+        host_tech.update(kern_vm_guest['virtualization_tech_host'])
+
+        hw_hv_vendor = self.detect_virt_product('hw.hv_vendor')
+        guest_tech.update(hw_hv_vendor['virtualization_tech_guest'])
+        host_tech.update(hw_hv_vendor['virtualization_tech_host'])
+
+        sec_jail_jailed = self.detect_virt_product('security.jail.jailed')
+        guest_tech.update(sec_jail_jailed['virtualization_tech_guest'])
+        host_tech.update(sec_jail_jailed['virtualization_tech_host'])
 
         if virtual_facts['virtualization_type'] == '':
-            virtual_vendor_facts = self.detect_virt_vendor('hw.model')
+            sysctl = kern_vm_guest or hw_hv_vendor or sec_jail_jailed
+            # We call update here, then re-set virtualization_tech_host/guest
+            # later.
+            virtual_facts.update(sysctl)
+
+        virtual_vendor_facts = self.detect_virt_vendor('hw.model')
+        guest_tech.update(virtual_vendor_facts['virtualization_tech_guest'])
+        host_tech.update(virtual_vendor_facts['virtualization_tech_host'])
+
+        if virtual_facts['virtualization_type'] == '':
             virtual_facts.update(virtual_vendor_facts)
 
+        virtual_facts['virtualization_tech_guest'] = guest_tech
+        virtual_facts['virtualization_tech_host'] = host_tech
         return virtual_facts
 
 
