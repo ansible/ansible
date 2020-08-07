7c60dadb9a3832dee0014113a337bc10fa1088c0
Matt Martz <matt@sivel.net>
2020-08-07 14:28:10 -0500

    Updates to Integration tests to pass against Alpine (#70946)
    * Start of alpine testing
    
    * More updates
    
    * Add forgotten file
    
    * remove debug
    
    * Add alpine3
    
    * equal
    
    * group 4
    
    * group 4
    
    * group 5
    
    * Try to decrease test length
    
    * libuser only available in testing
    
    * Remove debug
    
    * Make loops target work on hosts without gnu date
    
    * Enable alpine testing
    
    * ci_complete
    
    * Don't specify uid for creating test user
    
    * ci_complete
    
    * Re-sort docker completion
    
    * use newer container image
    
    * ci_complete
    
    * fix indentation
    
    Co-authored-by: Matt Clay <matt@mystile.com>
    
    Co-authored-by: Matt Clay <matt@mystile.com>

--- test/integration/targets/connection_ssh/runme.sh
+++ test/integration/targets/connection_ssh/runme.sh
@@ -22,12 +22,14 @@ if command -v sshpass > /dev/null; then
             -e ansible_sshpass_prompt=notThis: \
             -e ansible_password=foo \
             -e ansible_user=definitelynotroot \
-	    -i test_connection.inventory \
+            -i test_connection.inventory \
             ssh-pipelining
         ret=$?
-        if [[ $ret -ne 124 ]]; then
+        # 124 is EXIT_TIMEDOUT from gnu coreutils
+        # 143 is 128+SIGTERM(15) from BusyBox
+        if [[ $ret -ne 124 && $ret -ne 143 ]]; then
             echo "Expected to time out and we did not. Exiting with failure."
-     	exit 1
+            exit 1
         fi
     else
         ansible -m ping \
@@ -35,7 +37,7 @@ if command -v sshpass > /dev/null; then
             -e ansible_sshpass_prompt=notThis: \
             -e ansible_password=foo \
             -e ansible_user=definitelynotroot \
-	    -i test_connection.inventory \
+            -i test_connection.inventory \
             ssh-pipelining | grep 'customized password prompts'
         ret=$?
         [[ $ret -eq 0 ]] || exit $ret
