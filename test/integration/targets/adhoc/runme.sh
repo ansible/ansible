d4db163b52e90c6482489463f56900353575d091
Brian Coca <brian.coca+git@gmail.com>
2020-08-19 09:49:39 -0400

    actually use right cli switch .. DUH!

--- test/integration/targets/adhoc/runme.sh
+++ test/integration/targets/adhoc/runme.sh
@@ -3,4 +3,4 @@
 set -eux
 
 # run type tests
-ansible -a 'sleep 20' -T 5 localhost |grep 'The command action failed to execute in the expected time frame (5) and was terminated'
+ansible -a 'sleep 20' --task-timeout 5 localhost |grep 'The command action failed to execute in the expected time frame (5) and was terminated'
