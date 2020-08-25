5b4aa50f18fa4a380f48cfacddbf42f3124831bd
Jordan Borean <jborean93@gmail.com>
2020-08-26 09:45:22 +1000

    Remove test that shouldn't have been backported

--- test/units/plugins/shell/test_powershell.py
+++ test/units/plugins/shell/test_powershell.py
@@ -70,11 +70,3 @@ def test_parse_clixml_multiple_elements():
     expected = b"Error 1\r\nError 2"
     actual = _parse_clixml(multiple_elements)
     assert actual == expected
-
-
-def test_join_path_unc():
-    pwsh = ShellModule()
-    unc_path_parts = ['\\\\host\\share\\dir1\\\\dir2\\', '\\dir3/dir4', 'dir5', 'dir6\\']
-    expected = '\\\\host\\share\\dir1\\dir2\\dir3\\dir4\\dir5\\dir6'
-    actual = pwsh.join_path(*unc_path_parts)
-    assert actual == expected
