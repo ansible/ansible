6911b02f3b4b41b3cfba1eb0276d30ecf6b0f108
Matt Clay <matt@mystile.com>
2020-08-24 17:23:57 -0700

    WIP - Test new Parallels host.

--- test/utils/shippable/incidental/remote.sh
+++ test/utils/shippable/incidental/remote.sh
@@ -13,6 +13,13 @@ target="shippable/posix/incidental/"
 stage="${S:-prod}"
 provider="${P:-default}"
 
+if [ "${stage}" = "dev" ]; then
+    endpoint="https://ansible-core-ci.testing.ansible.com"
+else
+    endpoint=""
+fi
+
 # shellcheck disable=SC2086
 ansible-test integration --color -v --retry-on-error "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} ${UNSTABLE:+"$UNSTABLE"} \
+    --remote-endpoint "${endpoint}" \
     --remote "${platform}/${version}" --remote-terminate always --remote-stage "${stage}" --remote-provider "${provider}" \
