724b3648ea0de12c15c7c2f9e0d9052f83a1829f
Martin Krizek <martin.krizek@gmail.com>
2020-08-27 19:22:45 +0200

    _check_failed_state: always use the current/nested state (#71347) (#71457)
    Fixes #71306
    
    (cherry picked from commit 9792d631b1b92356d41e9a792de4b2479a1bce44)

--- test/integration/targets/blocks/runme.sh
+++ test/integration/targets/blocks/runme.sh
@@ -74,3 +74,12 @@ cat rc_test.out
 [ "$(grep -c 'Failure in always' rc_test.out )" -eq 1 ]
 [ "$(grep -c 'DID NOT RUN' rc_test.out )" -eq 0 ]
 rm -f rc_test.out
+
+# https://github.com/ansible/ansible/issues/71306
+set +e
+exit_code=0
+ansible-playbook -i host1,host2 -vv issue71306.yml > rc_test.out || exit_code=$?
+set -e
+cat rc_test.out
+[ $exit_code -eq 0 ]
+rm -f rc_test_out
