86cec8fb3d62dc764850d0aa26ce085e299e6b02
Felix Fontein <felix@fontein.de>
2020-09-12 21:59:03 +0200

    Use correct field that's expected by lib/ansible/cli/__init__.py..

--- lib/ansible/utils/plugin_docs.py
+++ lib/ansible/utils/plugin_docs.py
@@ -42,14 +42,15 @@ def merge_fragment(target, source):
 
 def _process_versions_and_dates(fragment, is_module, return_docs, callback):
     def process_deprecation(deprecation, top_level=False):
+        collection_name = 'removed_from_collection' if top_level else 'collection_name'
         if not isinstance(deprecation, MutableMapping):
             return
         if (is_module or top_level) and 'removed_in' in deprecation:  # used in module deprecations
-            callback(deprecation, 'removed_in', 'removed_from_collection')
+            callback(deprecation, 'removed_in', collection_name)
         if 'removed_at_date' in deprecation:
-            callback(deprecation, 'removed_at_date', 'removed_from_collection')
+            callback(deprecation, 'removed_at_date', collection_name)
         if not (is_module or top_level) and 'version' in deprecation:  # used in plugin option deprecations
-            callback(deprecation, 'version', 'removed_from_collection')
+            callback(deprecation, 'version', collection_name)
 
     def process_option_specifiers(specifiers):
         for specifier in specifiers:
