ece0be30ab94b3b25f70f3f71f41284a1b990e24
Sviatoslav Sydorenko <webknjaz@redhat.com>
2020-09-25 16:15:10 +0200

    Move ansible.galaxy.collection module into package
    This change preserves how all the external imports refer to this code
    while allowing us to start cutting the spaghetti into more easily
    maintainable pieces.
    
    This is a start of the upcoming refactoring effort destined to
    eliminate tight coupling, implicit data manipulation, god objects,
    abstraction leaks and other code smells.
    
    Essentially, `ansible.galaxy.collection` is going to be a package that
    holds parts of the collection management code spread across loosely
    coupled modules.
    
    PR: #71931

--- lib/ansible/galaxy/collection/__init__.py
+++ lib/ansible/galaxy/collection/__init__.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 # Copyright: (c) 2019, Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""Installed collections management package."""
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
