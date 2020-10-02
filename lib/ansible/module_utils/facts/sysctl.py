709484969c8a4ffd74b839a673431a8c5caa6457
Rick Elrod <rick@elrod.me>
2020-10-02 18:36:31 -0500

    sysctl/openbsd fact fixes (#72070)
    Change:
    - Use `sysctl -n` for openbsd uptime information
    - Allow `get_sysctl()` to account for multi-line sysctl settings
    - Add unit tests for `get_sysctl()`
    
    Test Plan:
    - New unit tests
    
    Tickets:
    - Fixes #71968
    - Refs #72025
    - Refs #72067
    
    Signed-off-by: Rick Elrod <rick@elrod.me>
    Co-authored-by: Brian Coca <brian.coca+git@gmail.com>
    Co-authored-by: Brian Coca <bcoca@ansible.com>

--- lib/ansible/module_utils/facts/sysctl.py
+++ lib/ansible/module_utils/facts/sysctl.py
@@ -18,21 +18,45 @@ __metaclass__ = type
 
 import re
 
+from ansible.module_utils._text import to_text
+
 
 def get_sysctl(module, prefixes):
     sysctl_cmd = module.get_bin_path('sysctl')
     cmd = [sysctl_cmd]
     cmd.extend(prefixes)
 
-    rc, out, err = module.run_command(cmd)
-    if rc != 0:
-        return dict()
-
     sysctl = dict()
-    for line in out.splitlines():
-        if not line:
-            continue
-        (key, value) = re.split(r'\s?=\s?|: ', line, maxsplit=1)
-        sysctl[key] = value.strip()
+
+    try:
+        rc, out, err = module.run_command(cmd)
+    except (IOError, OSError) as e:
+        module.warn('Unable to read sysctl: %s' % to_text(e))
+        rc = 1
+
+    if rc == 0:
+        key = ''
+        value = ''
+        for line in out.splitlines():
+            if not line.strip():
+                continue
+
+            if line.startswith(' '):
+                # handle multiline values, they will not have a starting key
+                # Add the newline back in so people can split on it to parse
+                # lines if they need to.
+                value += '\n' + line
+                continue
+
+            if key:
+                sysctl[key] = value.strip()
+
+            try:
+                (key, value) = re.split(r'\s?=\s?|: ', line, maxsplit=1)
+            except Exception as e:
+                module.warn('Unable to split sysctl line (%s): %s' % (to_text(line), to_text(e)))
+
+        if key:
+            sysctl[key] = value.strip()
 
     return sysctl
