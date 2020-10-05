82aa89fb78fa3094314864417ab7b66fc235f5cc
Rick Elrod <rick@elrod.me>
2020-10-05 11:44:07 -0500

    Update test/lib/ansible_test/_internal/docker_util.py
    Co-authored-by: Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>

--- test/lib/ansible_test/_internal/docker_util.py
+++ test/lib/ansible_test/_internal/docker_util.py
@@ -195,7 +195,8 @@ def docker_images(args, image):
     except SubprocessError as ex:
         if 'no such image' in ex.stderr:
             return []  # podman does not handle this gracefully, exits 125
-        elif 'function "json" not defined' in ex.stderr:
+
+        if 'function "json" not defined' in ex.stderr:
             # podman > 2 && < 2.2.0 breaks with --format {{json .}}, and requires --format json
             # So we try this as a fallback. If it fails again, we just raise the exception and bail.
             stdout, _dummy = docker_command(args, ['images', image, '--format', 'json'], capture=True, always=True)
