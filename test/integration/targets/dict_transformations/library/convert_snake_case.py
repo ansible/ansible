1f485dccaa7126a9ffe821a6ec71c99a266591e0
Sloane Hertel <shertel@redhat.com>
2020-10-06 12:10:03 -0400

    [stable-2.10] Add intentional coverage for incidental_ec2_instance (#72028)
    * Add a test suite for module_utils.common.dict_transformations
    
    * ci_complete
    
    ci_coverage
    
    Add a wait_for test using delegate_to
    
    * Remove incidental_ec2_instance
    
    * Remove unused test support modules
    
    * Requested changes
    
    ci_complete
    
    ci_coverage
    
    * Oops, put everything back to test coverage again
    
    ci_complete
    
    ci_coverage
    
    * Remove incidental_ec2_instance tests and supporting modules
    (cherry picked from commit 960e4c0809a586d1232f67153542036450b3fa71)
    
    Co-authored-by: Sloane Hertel <shertel@redhat.com>

---
+++ test/integration/targets/dict_transformations/library/convert_snake_case.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+DOCUMENTATION = '''
+---
+module: convert_snake_case
+short_description: test converting data to snake_case
+description: test converting data to snake_case
+options:
+  data:
+    description: Data to modify
+    type: dict
+    required: True
+  reversible:
+    description:
+      - Make the snake_case conversion in a way that can be converted back to the original value
+      - For example, convert IAMUser to i_a_m_user instead of iam_user
+    default: False
+  ignore_list:
+    description: list of top level keys that should not have their contents converted
+    type: list
+    default: []
+'''
+
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            data=dict(type='dict', required=True),
+            reversible=dict(type='bool', default=False),
+            ignore_list=dict(type='list', default=[]),
+        ),
+    )
+
+    result = camel_dict_to_snake_dict(
+        module.params['data'],
+        module.params['reversible'],
+        module.params['ignore_list']
+    )
+
+    module.exit_json(data=result)
+
+
+if __name__ == '__main__':
+    main()
