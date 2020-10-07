6e1b37ec895ca7bbc0e26b01b1f6cba873fb459f
Jordan Borean <jborean93@gmail.com>
2020-10-08 01:07:27 +1000

    Fix example to use correct shebang (#72129)
    * Fix example to use correct shebang
    
    * Fix other example modules as well
    
    * Ignore shebang test

--- examples/scripts/my_test_info.py
+++ examples/scripts/my_test_info.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 # Copyright: (c) 2020, Your Name <YourName@example.org>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
