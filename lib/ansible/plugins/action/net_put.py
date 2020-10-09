6610fa5cf35f8ddb20b5350effc4ac2631370860
Ganesh B Nalawade <ganesh634@gmail.com>
2020-10-09 14:18:38 +0530

    Fix net_put _handle_template error
    Fixes https://github.com/ansible/ansible/issues/66059
    
    Add the logic to handle template in `src` option within
    net_put action plugin

--- lib/ansible/plugins/action/net_put.py
+++ lib/ansible/plugins/action/net_put.py
@@ -71,7 +71,7 @@ class ActionModule(ActionBase):
 
         if mode == 'text':
             try:
-                self._handle_template(convert_data=False)
+                self._handle_src_option(convert_data=False)
             except ValueError as exc:
                 return dict(failed=True, msg=to_text(exc))
 
@@ -197,6 +197,46 @@ class ActionModule(ActionBase):
         if self._task._role is not None:
             cwd = self._task._role._role_path
         return cwd
+
+    def _handle_src_option(self, convert_data=True):
+        src = self._task.args.get("src")
+        working_path = self._get_working_path()
+
+        if os.path.isabs(src) or urlsplit("src").scheme:
+            source = src
+        else:
+            source = self._loader.path_dwim_relative(
+                working_path, "templates", src
+            )
+            if not source:
+                source = self._loader.path_dwim_relative(working_path, src)
+
+        if not os.path.exists(source):
+            raise AnsibleError("path specified in src not found")
+
+        try:
+            with open(source, "r") as f:
+                template_data = to_text(f.read())
+        except IOError as e:
+            raise AnsibleError(
+                "unable to load src file {0}, I/O error({1}): {2}".format(
+                    source, e.errno, e.strerror
+                )
+            )
+
+        # Create a template search path in the following order:
+        # [working_path, self_role_path, dependent_role_paths, dirname(source)]
+        searchpath = [working_path]
+        if self._task._role is not None:
+            searchpath.append(self._task._role._role_path)
+            if hasattr(self._task, "_block:"):
+                dep_chain = self._task._block.get_dep_chain()
+                if dep_chain is not None:
+                    for role in dep_chain:
+                        searchpath.append(role._role_path)
+        searchpath.append(os.path.dirname(source))
+        self._templar.environment.loader.searchpath = searchpath
+        self._task.args["src"] = self._templar.template(template_data)
 
     def _get_network_os(self, task_vars):
         if 'network_os' in self._task.args and self._task.args['network_os']:
