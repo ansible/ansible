38618fbcd7f8efeb7367186fadd0bfbce6f8957b
David Shrewsbury <dshrewsb@redhat.com>
2020-10-12 16:34:08 -0400

    Add some initial tests
    We need to strip the module path from the first line of the documentation output
    now before comparing expected/actual output in ansible-doc/runme.sh because we now
    unfrack the --playbook-dir option value.

--- test/integration/targets/ansible-doc/runme.sh
+++ test/integration/targets/ansible-doc/runme.sh
@@ -14,8 +14,9 @@ unset ANSIBLE_PLAYBOOK_DIR
 cd "$(dirname "$0")"
 
 # test module docs from collection
-current_out="$(ansible-doc --playbook-dir ./ testns.testcol.fakemodule)"
-expected_out="$(cat fakemodule.output)"
+# we use sed to strip the module path from the first line
+current_out="$(ansible-doc --playbook-dir ./ testns.testcol.fakemodule | sed '1 s/\(^> TESTNS\.TESTCOL\.FAKEMODULE\).*(.*)$/\1/')"
+expected_out="$(sed '1 s/\(^> TESTNS\.TESTCOL\.FAKEMODULE\).*(.*)$/\1/' fakemodule.output)"
 test "$current_out" == "$expected_out"
 
 # test listing diff plugin types from collection
@@ -38,4 +39,14 @@ do
 	justcol=$(ansible-doc -l -t ${ptype} --playbook-dir ./ testns|wc -l)
 	test "$justcol" -eq 1
 done
+
+#### test role functionality
+
+# Two collection roles are defined, but only 1 has a role arg spec with 2 entry points
+output=$(ansible-doc -t role -l --playbook-dir . testns.testcol | wc -l)
+test "$output" -eq 2
+
+# Include normal roles
+output=$(ansible-doc -t role -l --playbook-dir . --roles-path ./roles | wc -l)
+test "$output" -eq 3
 )
