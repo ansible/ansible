1b256529e6cd5082c77d67c2152d8223a2546129
Toshio Kuratomi <a.badger@gmail.com>
2020-10-19 11:58:09 -0700

    Document security concern for users setting the system_tmpdirs shell plugin config (#72213)

--- lib/ansible/plugins/doc_fragments/shell_common.py
+++ lib/ansible/plugins/doc_fragments/shell_common.py
@@ -34,9 +34,11 @@ options:
     version_added: "2.10"
   system_tmpdirs:
     description:
-       - "List of valid system temporary directories for Ansible to choose when it cannot use
-         ``remote_tmp``, normally due to permission issues.  These must be world readable, writable,
-         and executable."
+       - "List of valid system temporary directories on the managed machine for Ansible to choose
+         when it cannot use ``remote_tmp``, normally due to permission issues.  These must be world
+         readable, writable, and executable. This list should only contain directories which the
+         system administrator has pre-created with the proper ownership and permissions otherwise
+         security issues can arise."
     default: [ /var/tmp, /tmp ]
     type: list
     env: [{name: ANSIBLE_SYSTEM_TMPDIRS}]
