0755fcf48e75738c13639d6b3cbe9be8588d517a
Mark Farrell <mark.a.farrell@team.telstra.com>
2020-10-21 16:34:42 +1100

    fix runme.sh

--- test/integration/targets/callback_junit/runme.sh
+++ test/integration/targets/callback_junit/runme.sh
@@ -11,14 +11,14 @@ ansible-playbook junit-callback-test.yml -i inventory "$@"
 # Copy report to local directory
 mv "$(find "$JUNIT_OUTPUT_DIR" -name "junit-callback-test-[0-9]*" | sort -r | head -1)" reports
 # Remove classname and time
-cat reports/junit-callback-test-[0-9]* | sed -e 's/time=".*"//' | sed -e 's/classname=".*"//' > reports/junit-callback-test.xml
+cat reports/junit-callback-test-[0-9]* | sed 's/[ ]\{1,\}time="[0-9.]\{1,\}"//' | sed 's/[ ]\{1,\}classname="[^"]\{1,\}"//' > reports/junit-callback-test.xml
 
 JUNIT_TEST_CASE_PREFIX="JUNIT TASK PREFIX - " \
     ansible-playbook junit-callback-test-prefix.yml -i inventory "$@"
 # # Compare with expected result
 mv "$(find "$JUNIT_OUTPUT_DIR" -name "junit-callback-test-prefix-[0-9]*" | sort -r | head -1)" reports
 # Remove classname and time
-cat reports/junit-callback-test-prefix-[0-9]* | sed -e 's/time=".*"//' | sed -e 's/classname=".*"//' > reports/junit-callback-test-prefix.xml
+cat reports/junit-callback-test-prefix-[0-9]* | sed 's/[ ]\{1,\}time="[0-9.]\{1,\}"//' | sed 's/[ ]\{1,\}classname="[^"]\{1,\}"//' > reports/junit-callback-test-prefix.xml
 
 JUNIT_TEST_CASE_PREFIX="JUNIT TASK PREFIX - " \
 JUNIT_TEST_CASE_IGNORE_TEXT="JUNIT IGNORE THIS TASK" \
@@ -26,7 +26,7 @@ JUNIT_TEST_CASE_IGNORE_TEXT="JUNIT IGNORE THIS TASK" \
 # Compare with expected result
 mv "$(find "$JUNIT_OUTPUT_DIR" -name "junit-callback-test-ignore-[0-9]*" | sort -r | head -1)" reports
 # Remove classname and time
-cat reports/junit-callback-test-ignore-[0-9]* | sed -e 's/time=".*"//' | sed -e 's/classname=".*"//' > reports/junit-callback-test-ignore.xml
+cat reports/junit-callback-test-ignore-[0-9]* | sed 's/[ ]\{1,\}time="[0-9.]\{1,\}"//' | sed 's/[ ]\{1,\}classname="[^"]\{1,\}"//' > reports/junit-callback-test-ignore.xml
 
 diff --ignore-space-change expected_reports/junit-callback-test.xml reports/junit-callback-test.xml
 diff --ignore-space-change expected_reports/junit-callback-test-prefix.xml reports/junit-callback-test-prefix.xml
