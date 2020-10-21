96023921098c24f2d5d318277530acc2d14bd3cb
Mark Farrell <mark.a.farrell@team.telstra.com>
2020-10-21 14:29:08 +1100

    add tests

---
+++ test/integration/targets/callback_junit/runme.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# export JUNIT_OUTPUT_DIR="$PWD/reports"
+
+rm -rf reports
+mkdir reports
+
+ansible-playbook junit-callback-test.yml -i inventory "$@"
+# Copy report to local directory
+mv $(ls -t $JUNIT_OUTPUT_DIR/junit-callback-test-[0-9]* | head -1) reports
+# Remove classname and time
+cat reports/junit-callback-test-[0-9]* | sed -e 's/time=".*"//' | sed -e 's/classname=".*"//' > reports/junit-callback-test.xml
+
+JUNIT_TEST_CASE_PREFIX="JUNIT TASK PREFIX - " \
+    ansible-playbook junit-callback-test-prefix.yml -i inventory "$@"
+# # Compare with expected result
+mv $(ls -t $JUNIT_OUTPUT_DIR/junit-callback-test-prefix-[0-9]* | head -1) reports
+# Remove classname and time
+cat reports/junit-callback-test-prefix-[0-9]* | sed -e 's/time=".*"//' | sed -e 's/classname=".*"//' > reports/junit-callback-test-prefix.xml
+
+JUNIT_TEST_CASE_PREFIX="JUNIT TASK PREFIX - " \
+JUNIT_TEST_CASE_IGNORE_TEXT="JUNIT IGNORE THIS TASK" \
+    ansible-playbook junit-callback-test-ignore.yml -i inventory "$@"
+# Compare with expected result
+mv $(ls -t $JUNIT_OUTPUT_DIR/junit-callback-test-ignore-[0-9]* | head -1) reports
+# Remove classname and time
+cat reports/junit-callback-test-ignore-[0-9]* | sed -e 's/time=".*"//' | sed -e 's/classname=".*"//' > reports/junit-callback-test-ignore.xml
+
+diff --ignore-space-change expected_reports/junit-callback-test.xml reports/junit-callback-test.xml
+diff --ignore-space-change expected_reports/junit-callback-test-prefix.xml reports/junit-callback-test-prefix.xml
+diff --ignore-space-change expected_reports/junit-callback-test-ignore.xml reports/junit-callback-test-ignore.xml
