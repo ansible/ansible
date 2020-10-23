3c2e8b99be4c001c9955bcee7baa12fd46147b90
Matt Clay <mclay@redhat.com>
2020-10-22 18:45:03 -0700

    Fix ansible-test handling of user-defined docker networks. (#72256)
    * Fix ansible-test docker container detection.
    
    * Attach test containers to the correct network.
    
    * Do not assume `localhost` for accesing Docker.
    
    * Look for containers on current network.
    
    * Always map /var/run/docker.sock into containers.
    
    This fixes issues when using a remote Docker host.
    
    * Support container IP lookup from networks list.
    
    * Fix container network attachment.
    
    * Remove redundant container detection messages.
    
    * Limit DOCKER_HOST parsing to TCP.
    
    * Restore docker socket existence check.
    
    The check is skipped if the docker hostname is not localhost.
    
    * Correct changelog entry.

--- test/lib/ansible_test/_internal/delegation.py
+++ test/lib/ansible_test/_internal/delegation.py
@@ -72,6 +72,9 @@ from .docker_util import (
     docker_available,
     docker_network_disconnect,
     get_docker_networks,
+    get_docker_preferred_network_name,
+    get_docker_hostname,
+    is_docker_user_defined_network,
 )
 
 from .cloud import (
@@ -305,14 +308,18 @@ def delegate_docker(args, exclude, require, integration_targets):
             if args.docker_seccomp != 'default':
                 test_options += ['--security-opt', 'seccomp=%s' % args.docker_seccomp]
 
-            if os.path.exists(docker_socket):
+            if get_docker_hostname() != 'localhost' or os.path.exists(docker_socket):
                 test_options += ['--volume', '%s:%s' % (docker_socket, docker_socket)]
 
             if httptester_id:
                 test_options += ['--env', 'HTTPTESTER=1', '--env', 'KRB5_PASSWORD=%s' % args.httptester_krb5_password]
 
-                for host in HTTPTESTER_HOSTS:
-                    test_options += ['--link', '%s:%s' % (httptester_id, host)]
+                network = get_docker_preferred_network_name(args)
+
+                if not is_docker_user_defined_network(network):
+                    # legacy links are required when using the default bridge network instead of user-defined networks
+                    for host in HTTPTESTER_HOSTS:
+                        test_options += ['--link', '%s:%s' % (httptester_id, host)]
 
             if isinstance(args, IntegrationConfig):
                 cloud_platforms = get_cloud_providers(args)
