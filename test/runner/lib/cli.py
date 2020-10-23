73b2199fa28fa68bcf3c514f3b30da579af7c14b
Matt Clay <mclay@redhat.com>
2020-10-22 21:50:12 -0700

    [stable-2.8] [stable-2.9] [stable-2.10] Fix ansible-test handling of user-defined docker networks. (#72256) (#72310)
    * Fix ansible-test docker container detection.
    
    * Attach test containers to the correct network.
    
    * Do not assume `localhost` for accesing Docker.
    
    * Look for containers on current network.
    
    * Always map /var/run/docker.sock into containers.
    
    This fixes issues when using a remote Docker host.
    
    * Support container IP lookup from networks list.
    
    * Fix container network attachment.
    
    * Remove redundant container detection messages.
    
    * Limit DOCKER_HOST parsing to TCP.
    
    * Restore docker socket existence check.
    
    The check is skipped if the docker hostname is not localhost.
    
    * Correct changelog entry..
    (cherry picked from commit 3c2e8b99be4c001c9955bcee7baa12fd46147b90)
    
    Co-authored-by: Matt Clay <mclay@redhat.com>.
    (cherry picked from commit 6362232c304ae3e4588a3e5e14d89560c31fdb3d)
    
    Co-authored-by: Matt Clay <mclay@redhat.com>.
    (cherry picked from commit 7c83f1f045e51930eeeb988b987c44351c8cacd0)
    
    Co-authored-by: Matt Clay <mclay@redhat.com>

--- test/runner/lib/cli.py
+++ test/runner/lib/cli.py
@@ -757,6 +757,9 @@ def add_extra_docker_options(parser, integration=True):
                         action='store_true',
                         help='run docker container in privileged mode')
 
+    docker.add_argument('--docker-network',
+                        help='run using the specified docker network')
+
     docker.add_argument('--docker-memory',
                         help='memory limit for docker in bytes', type=int)
 
