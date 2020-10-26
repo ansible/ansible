f5cac3017f9a0b3e4fba5560d313fa0cd58608e4
Sam Doran <sdoran@redhat.com>
2020-10-26 17:36:02 -0400

    blockinfile - properly insert block when no trailing new line exists

--- lib/ansible/modules/blockinfile.py
+++ lib/ansible/modules/blockinfile.py
@@ -302,8 +302,12 @@ def main():
         lines[n1:n0 + 1] = []
         n0 = n1
 
-    lines[n0:n0] = blocklines
+    # No new line at the end of file, so ensure there is a new line before
+    # the block of new text to be inserted.
+    if not lines[n0 - 1].endswith(b(os.linesep)):
+        lines[n0 - 1] = lines[n0 - 1] + b(os.linesep)
 
+    lines[n0:n0] = blocklines
     if lines:
         result = b''.join(lines)
     else:
