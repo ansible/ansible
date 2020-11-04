b649b0a5ce2dbd26a792e8aa7e4829a8e18eea3f
Felix Fontein <felix@fontein.de>
2020-11-04 13:25:45 +0100

    Replace some more FQCNs.

--- lib/ansible/plugins/action/gather_facts.py
+++ lib/ansible/plugins/action/gather_facts.py
@@ -21,7 +21,7 @@ class ActionModule(ActionBase):
         mod_args = self._task.args.copy()
 
         # deal with 'setup specific arguments'
-        if fact_module not in ['ansible.legacy.setup', 'ansible.builtin.setup', 'setup']:
+        if fact_module not in C._ACTION_SETUP:
             # network facts modules must support gather_subset
             if self._connection._load_name not in ('network_cli', 'httpapi', 'netconf'):
                 subset = mod_args.pop('gather_subset', None)
