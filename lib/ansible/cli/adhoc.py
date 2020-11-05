5192cd1ef6a36b0b2f8afcef1dc88ecb03720059
Felix Fontein <felix@fontein.de>
2020-11-05 22:17:16 +0100

    Fix another case which was already fixed in stable-2.10.

--- lib/ansible/cli/adhoc.py
+++ lib/ansible/cli/adhoc.py
@@ -64,7 +64,7 @@ class AdHocCLI(CLI):
         return options
 
     def _play_ds(self, pattern, async_val, poll):
-        check_raw = context.CLIARGS['module_name'] in ('command', 'win_command', 'shell', 'win_shell', 'script', 'raw')
+        check_raw = context.CLIARGS['module_name'] in C.MODULE_REQUIRE_ARGS
 
         mytask = {'action': {'module': context.CLIARGS['module_name'], 'args': parse_kv(context.CLIARGS['module_args'], check_raw=check_raw)}}
 
