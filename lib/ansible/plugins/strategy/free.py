d8baf4d38d9d2d2116f8d7b5272ed88b2f416c8a
Brian Coca <bcoca@users.noreply.github.com>
2020-11-17 10:11:15 -0500

    Document and warn when max_fail and free are used (#72616)
    Fixes #16666

--- lib/ansible/plugins/strategy/free.py
+++ lib/ansible/plugins/strategy/free.py
@@ -92,6 +92,9 @@ class StrategyModule(StrategyBase):
 
         self._set_hosts_cache(iterator._play)
 
+        if iterator._play.max_fail_percentage is not None:
+            display.warning("Using max_fail_percentage with the free strategy is not supported, as tasks are executed independently on each host")
+
         work_to_do = True
         while work_to_do and not self._tqm._terminated:
 
