234658c8c539768f9f5b1b2f7a48b12315ffbad7
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2020-11-19 19:38:25 +0100

    Add a versioning helper module

---
+++ lib/ansible/galaxy/dependency_resolution/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2020, Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""Dependency resolution machinery."""
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
