47790aed3ef7d952f194edbb809bd34e39835b71
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2020-12-08 16:13:31 +0100

    Declare support for Python 3.9 in the metadata
    Co-authored-by: Rick Elrod <rick@elrod.me>

--- setup.py
+++ setup.py
@@ -390,6 +390,7 @@ static_setup_params = dict(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: System :: Installation/Setup',
         'Topic :: System :: Systems Administration',
         'Topic :: Utilities',
