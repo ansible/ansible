debb83d023b5073ae76b3730f544853c55f4c9b4
Tyler J Russell <22531310+Nytelife26@users.noreply.github.com>
2020-12-09 01:10:11 +0000

    fix(hostvars): change templating behaviour (#72829)
    ci_complete

--- lib/ansible/vars/hostvars.py
+++ lib/ansible/vars/hostvars.py
@@ -136,7 +136,7 @@ class HostVarsVars(Mapping):
 
     def __getitem__(self, var):
         templar = Templar(variables=self._vars, loader=self._loader)
-        foo = templar.template(self._vars[var], fail_on_undefined=False, static_vars=STATIC_VARS)
+        foo = templar.template(self._vars[var], fail_on_undefined=True, static_vars=STATIC_VARS)
         return foo
 
     def __contains__(self, var):
@@ -151,4 +151,4 @@ class HostVarsVars(Mapping):
 
     def __repr__(self):
         templar = Templar(variables=self._vars, loader=self._loader)
-        return repr(templar.template(self._vars, fail_on_undefined=False, static_vars=STATIC_VARS))
+        return repr(templar.template(self._vars, fail_on_undefined=True, static_vars=STATIC_VARS))
