6769406e12e50f750409fc8ca3a98147a14a0fa7
Matt Martz <matt@sivel.net>
2020-12-09 11:32:04 -0600

    Address shellcheck issue

--- test/integration/targets/noexec/runme.sh
+++ test/integration/targets/noexec/runme.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-trap "umount '${OUTPUT_DIR}/ramdisk'" EXIT
+trap 'umount "${OUTPUT_DIR}/ramdisk"' EXIT
 
 mkdir "${OUTPUT_DIR}/ramdisk"
 mount -t tmpfs -o size=32m,noexec,rw tmpfs "${OUTPUT_DIR}/ramdisk"
