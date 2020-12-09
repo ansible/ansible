3f59fc65f9fe3437c1d34ff39d59bdffd429d043
Sven Wegener <sven.wegener@stealer.net>
2020-12-09 09:46:32 +0100

    vault: Read stdin data as binary on python3 (#52229)
    On python3 sys.stdin is an encoded file object that does not support
    reading raw binary data. Use the supplied buffer object to do so.
    
    Signed-off-by: Sven Wegener <sven.wegener@inovex.de>
    
    Co-authored-by: Sven Wegener <sven.wegener@inovex.de>

--- lib/ansible/parsing/vault/__init__.py
+++ lib/ansible/parsing/vault/__init__.py
@@ -1038,7 +1038,10 @@ class VaultEditor:
 
         try:
             if filename == '-':
-                data = sys.stdin.read()
+                if PY3:
+                    data = sys.stdin.buffer.read()
+                else:
+                    data = sys.stdin.read()
             else:
                 with open(filename, "rb") as fh:
                     data = fh.read()
