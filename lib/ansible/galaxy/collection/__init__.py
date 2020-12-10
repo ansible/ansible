3ec89fdee015baf5eb57624b881ee788b4f9d4db
Jordan Borean <jborean93@gmail.com>
2020-12-10 14:52:37 +1000

    Galaxy - make versions list consistent across versions

--- lib/ansible/galaxy/collection/__init__.py
+++ lib/ansible/galaxy/collection/__init__.py
@@ -511,29 +511,26 @@ class CollectionRequirement:
         galaxy_meta = None
 
         for api in apis:
-            try:
-                if not (requirement == '*' or requirement.startswith('<') or requirement.startswith('>') or
-                        requirement.startswith('!=')):
-                    # Exact requirement
-                    allow_pre_release = True
+            if not (requirement == '*' or requirement.startswith('<') or requirement.startswith('>') or
+                    requirement.startswith('!=')):
+                # Exact requirement
+                allow_pre_release = True
 
-                    if requirement.startswith('='):
-                        requirement = requirement.lstrip('=')
+                if requirement.startswith('='):
+                    requirement = requirement.lstrip('=')
 
+                try:
                     resp = api.get_collection_version_metadata(namespace, name, requirement)
-
+                except GalaxyError as err:
+                    if err.http_code != 404:
+                        raise
+                    versions = []
+                else:
                     galaxy_meta = resp
                     versions = [resp.version]
-                else:
-                    versions = api.get_collection_versions(namespace, name)
-            except GalaxyError as err:
-                if err.http_code != 404:
-                    raise
+            else:
+                versions = api.get_collection_versions(namespace, name)
 
-                versions = []
-
-            # Automation Hub doesn't return a 404 but an empty version list so we check that to align both AH and
-            # Galaxy when the collection is not available on that server.
             if not versions:
                 display.vvv("Collection '%s' is not available from server %s %s" % (collection, api.name,
                                                                                     api.api_server))
