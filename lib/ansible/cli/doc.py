bd2c54cf333c25e2efa85ac82aa1debc59d83832
David Shrewsbury <Shrews@users.noreply.github.com>
2020-12-14 12:05:40 -0500

    Change role argspec file used by ansible-doc (#72927)
    * Change role argspec file used by ansible-doc
    
    This changes the file used for role argument specs from meta/argument_specs.yml
    to meta/main.yml. The argument specs are expected to be in that file under the
    top-level entry of `argument_spec`.
    
    * Switch to argument_specs

--- lib/ansible/cli/doc.py
+++ lib/ansible/cli/doc.py
@@ -78,7 +78,7 @@ class RoleMixin(object):
     Note: The methods for actual display of role data are not present here.
     """
 
-    ROLE_ARGSPEC_FILE = 'argument_specs.yml'
+    ROLE_ARGSPEC_FILE = 'main.yml'
 
     def _load_argspec(self, role_name, collection_path=None, role_path=None):
         if collection_path:
@@ -90,12 +90,15 @@ class RoleMixin(object):
 
         try:
             with open(path, 'r') as f:
-                return from_yaml(f.read(), file_name=path)
+                data = from_yaml(f.read(), file_name=path)
+                return data.get('argument_specs', {})
         except (IOError, OSError) as e:
             raise AnsibleParserError("An error occurred while trying to read the file '%s': %s" % (path, to_native(e)), orig_exc=e)
 
     def _find_all_normal_roles(self, role_paths, name_filters=None):
         """Find all non-collection roles that have an argument spec file.
+
+        Note that argument specs do not actually need to exist within the spec file.
 
         :param role_paths: A tuple of one or more role paths. When a role with the same name
             is found in multiple paths, only the first-found role is returned.
@@ -108,7 +111,7 @@ class RoleMixin(object):
         for path in role_paths:
             if not os.path.isdir(path):
                 continue
-            # Check each subdir for a meta/argument_specs.yml file
+            # Check each subdir for a meta/main.yml file
             for entry in os.listdir(path):
                 role_path = os.path.join(path, entry)
                 full_path = os.path.join(role_path, 'meta', self.ROLE_ARGSPEC_FILE)
@@ -120,7 +123,9 @@ class RoleMixin(object):
         return found
 
     def _find_all_collection_roles(self, name_filters=None, collection_filter=None):
-        """Find all collection roles with an argument spec.
+        """Find all collection roles with an argument spec file.
+
+        Note that argument specs do not actually need to exist within the spec file.
 
         :param name_filters: A tuple of one or more role names used to filter the results. These
             might be fully qualified with the collection name (e.g., community.general.roleA)
