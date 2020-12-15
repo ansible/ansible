763c39a7d9be0a8e521f4d7a94a8bc3cdd59a3a4
Matt Clay <mclay@redhat.com>
2020-12-15 15:28:44 -0800

    Update Azure Pipelines scripts and config. (#72986)
    * Increase fetch depth to reduce the frequency of full test runs for out-of-date PRs.
    * Update scripts to match those which will be used in collections.

--- .azure-pipelines/scripts/aggregate-coverage.sh
+++ .azure-pipelines/scripts/aggregate-coverage.sh
@@ -12,4 +12,9 @@ mkdir "${agent_temp_directory}/coverage/"
 options=(--venv --venv-system-site-packages --color -v)
 
 ansible-test coverage combine --export "${agent_temp_directory}/coverage/" "${options[@]}"
-ansible-test coverage analyze targets generate "${agent_temp_directory}/coverage/coverage-analyze-targets.json" "${options[@]}"
+
+if ansible-test coverage analyze targets generate --help >/dev/null 2>&1; then
+    # Only analyze coverage if the installed version of ansible-test supports it.
+    # Doing so allows this script to work unmodified for multiple Ansible versions.
+    ansible-test coverage analyze targets generate "${agent_temp_directory}/coverage/coverage-analyze-targets.json" "${options[@]}"
+fi
--- .azure-pipelines/scripts/report-coverage.sh
+++ .azure-pipelines/scripts/report-coverage.sh
@@ -5,4 +5,11 @@ set -o pipefail -eu
 
 PATH="${PWD}/bin:${PATH}"
 
+if ! ansible-test --help >/dev/null 2>&1; then
+    # Install the devel version of ansible-test for generating code coverage reports.
+    # This is only used by Ansible Collections, which are typically tested against multiple Ansible versions (in separate jobs).
+    # Since a version of ansible-test is required that can work the output from multiple older releases, the devel version is used.
+    pip install https://github.com/ansible/ansible/archive/devel.tar.gz --disable-pip-version-check
+fi
+
 ansible-test coverage xml --stub --venv --venv-system-site-packages --color -v
