e1fc3a9b35befe24de879559773d2f9ce144088d
Sam Doooran <sdoran@redhat.com>
2020-12-17 10:51:42 -0500

    ansible-test - prefer venv over virtualenv on Python 3 (#73000)
    Also pin virtualenv to 16.7.10 for older Mac OS X systems. This was the version being installed
    anway with the previous constraint (<20).
    
    On systems with Python 3, now prefer venv over virtualenv. Test to see if venv is functional since
    some systems have a non-functional venv installation (such as Debian).

--- test/lib/ansible_test/_data/injector/virtualenv-isolated.sh
+++ test/lib/ansible_test/_data/injector/virtualenv-isolated.sh
@@ -2,7 +2,13 @@
 # Create and activate a fresh virtual environment with `source virtualenv-isolated.sh`.
 
 rm -rf "${OUTPUT_DIR}/venv"
-"${ANSIBLE_TEST_PYTHON_INTERPRETER}" -m virtualenv --python "${ANSIBLE_TEST_PYTHON_INTERPRETER}" "${OUTPUT_DIR}/venv"
+
+# Try to use 'venv' if it is available, then fallback to 'virtualenv' since some systems provide 'venv' although it is non-functional.
+if [[ "${ANSIBLE_TEST_PYTHON_VERSION}" =~ ^2\. ]] || ! "${ANSIBLE_TEST_PYTHON_INTERPRETER}" -m venv "${OUTPUT_DIR}/venv" > /dev/null 2>&1; then
+    rm -rf "${OUTPUT_DIR}/venv"
+    "${ANSIBLE_TEST_PYTHON_INTERPRETER}" -m virtualenv --python "${ANSIBLE_TEST_PYTHON_INTERPRETER}" "${OUTPUT_DIR}/venv"
+fi
+
 set +ux
 source "${OUTPUT_DIR}/venv/bin/activate"
 set -ux
--- test/lib/ansible_test/_data/setup/remote.sh
+++ test/lib/ansible_test/_data/setup/remote.sh
@@ -88,18 +88,10 @@ elif [ "${platform}" = "centos" ]; then
     done
 
     install_pip
-elif [ "${platform}" = "macos" ]; then
-    while true; do
-        pip3 install --disable-pip-version-check --quiet \
-            'virtualenv<20' \
-        && break
-        echo "Failed to install packages. Sleeping before trying again..."
-        sleep 10
-    done
 elif [ "${platform}" = "osx" ]; then
     while true; do
         pip install --disable-pip-version-check --quiet \
-            'virtualenv<20' \
+            'virtualenv==16.7.10' \
         && break
         echo "Failed to install packages. Sleeping before trying again..."
         sleep 10
