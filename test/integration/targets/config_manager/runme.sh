d3172f9cb194ddc845f806bedc2cfb1d7004722a
Felix Fontein <felix@fontein.de>
2020-12-23 15:46:51 +0100

    Extend and fix tests.

--- test/integration/targets/config_manager/runme.sh
+++ test/integration/targets/config_manager/runme.sh
@@ -2,7 +2,13 @@
 
 set -eux
 
-ansible-playbook test.yml 2>&1 | tee out.txt
+# Test lookup plugin option deprecation
+ANSIBLE_DEPRECATION_WARNINGS=true ansible-playbook test.yml 2>&1 | tee out.txt
 
 grep "deprecated_option option, this option should not be" out.txt
-grep "used, use no option instead instead." out.txt
+grep "used, use no option instead instead. This feature will be removed in version" out.txt
+grep -E "(0;35m|^)10.0.0. Deprecation" out.txt
+
+grep "deprecated_option option, this option should not be" out.txt
+grep "used, use no option instead instead. This feature will be removed from" out.txt
+grep "foo_ns.bar in version 1.0.0. Deprecation" out.txt
