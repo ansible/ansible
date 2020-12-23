a802481a341cdbd93720f12b10d5ca9c46421ebe
Felix Fontein <felix@fontein.de>
2020-12-23 15:00:26 +0100

    Don't forget about later deprecations.

---
+++ test/integration/targets/config_manager/runme.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# Test lookup plugin option deprecation
+ANSIBLE_DEPRECATION_WARNINGS=true ansible-playbook test.yml 2>&1 | tee out.txt
+
+grep "deprecated_option option, this option should not be" out.txt
+grep "used, use no option instead instead. This feature will be removed in version" out.txt
+grep -E "(0;35m|^)10.0.0. Deprecation" out.txt
+
+grep "deprecated_option option, this option should not be" out.txt
+grep "used, use no option instead instead. This feature will be removed in version" out.txt
+grep -E "(0;35m|^)1.0.0. Deprecation" out.txt
+# Once https://github.com/ansible/ansible/pull/73239 is merged, replace the last two lines by:
+# grep "used, use no option instead instead. This feature will be removed from" out.txt
+# grep "foo_ns.bar in version 1.0.0. Deprecation" out.txt
