3f311d165d5c9f7ce9e8b97554f0ef9d50c0a2ed
Matt Clay <matt@mystile.com>
2021-01-08 12:51:25 -0800

    Make the new ansible-test venv behavior opt-in

--- test/lib/ansible_test/_data/injector/virtualenv.sh
+++ test/lib/ansible_test/_data/injector/virtualenv.sh
@@ -4,7 +4,7 @@
 rm -rf "${OUTPUT_DIR}/venv"
 
 # Try to use 'venv' if it is available, then fallback to 'virtualenv' since some systems provide 'venv' although it is non-functional.
-if [[ "${ANSIBLE_TEST_PYTHON_VERSION}" =~ ^2\. ]] || ! "${ANSIBLE_TEST_PYTHON_INTERPRETER}" -m venv --system-site-packages "${OUTPUT_DIR}/venv" > /dev/null 2>&1; then
+if [ -z "${ANSIBLE_TEST_PREFER_VENV:-}" ] || [[ "${ANSIBLE_TEST_PYTHON_VERSION}" =~ ^2\. ]] || ! "${ANSIBLE_TEST_PYTHON_INTERPRETER}" -m venv --system-site-packages "${OUTPUT_DIR}/venv" > /dev/null 2>&1; then
     rm -rf "${OUTPUT_DIR}/venv"
     "${ANSIBLE_TEST_PYTHON_INTERPRETER}" -m virtualenv --system-site-packages --python "${ANSIBLE_TEST_PYTHON_INTERPRETER}" "${OUTPUT_DIR}/venv"
 fi
