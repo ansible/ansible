5911dd55fe274c937ae9fd85811ff366d190cb1d
David Shrewsbury <dshrewsb@redhat.com>
2021-01-13 15:58:12 -0500

    oops, enable tests

--- test/integration/targets/roles_arg_spec/runme.sh
+++ test/integration/targets/roles_arg_spec/runme.sh
@@ -6,10 +6,10 @@ set -eux
 ansible-playbook test.yml -i ../../inventory "$@"
 
 # More complex role test
-#ansible-playbook test_complex_role_fails.yml -i ../../inventory "$@"
+ansible-playbook test_complex_role_fails.yml -i ../../inventory "$@"
 
 # Test play level role will fail
-#set +e
-#ansible-playbook test_play_level_role_fails.yml -i ../../inventory "$@"
-#test $? -ne 0
-#set -e
+set +e
+ansible-playbook test_play_level_role_fails.yml -i ../../inventory "$@"
+test $? -ne 0
+set -e
