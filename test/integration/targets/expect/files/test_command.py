17634247d09b8d00c007dfd99fabb289cc2ab5ad
Matt Martz <matt@sivel.net>
2021-01-19 15:07:39 -0600

    Update tests to ensure no pepxect encoding issues

--- test/integration/targets/expect/files/test_command.py
+++ test/integration/targets/expect/files/test_command.py
@@ -10,6 +10,16 @@ except NameError:
 
 prompts = sys.argv[1:] or ['foo']
 
+# latin1 encoded bytes
+# to ensure pexpect doesn't have any encoding errors
+data = b'premi\xe8re is first\npremie?re is slightly different\n????????? is Cyrillic\n? am Deseret\n'
+
+try:
+    sys.stdout.buffer.write(data)
+except AttributeError:
+    sys.stdout.write(data)
+print()
+
 for prompt in prompts:
     user_input = input_function(prompt)
     print(user_input)
