c2334c659d169d7b20e7ac54035a5abed41b16d1
Brian Coca <brian.coca+git@gmail.com>
2021-01-25 11:11:48 -0500

    more resilient to garbage lines

--- lib/ansible/modules/apt_key.py
+++ lib/ansible/modules/apt_key.py
@@ -180,11 +180,15 @@ def parse_output_for_keys(output, short_format=False):
     lines = to_native(output).split('\n')
     for line in lines:
         if (line.startswith("pub") or line.startswith("sub")) and "expired" not in line:
-            tokens = line.split()
-            code = tokens[1]
-            (len_type, real_code) = code.split("/")
-            if real_code:
+            try:
+                tokens = line.split()
+                code = tokens[1]
+                (len_type, real_code) = code.split("/")
                 found.append(real_code)
+            except ValueError:
+                # invalid line, skip
+                continue
+
     if found and short_format:
         found = shorten_key_ids(found)
 
