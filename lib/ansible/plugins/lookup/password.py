4ec7a0769d6afa103da1398b894f23630d673e6f
Syed Ali Haider <syeda.haider97@yahoo.com>
2021-02-03 15:48:51 -0500

    Reduce line length
    Reduce Line Length to Pass CI Tests

--- lib/ansible/plugins/lookup/password.py
+++ lib/ansible/plugins/lookup/password.py
@@ -39,7 +39,8 @@ DOCUMENTATION = """
           - Define comma separated list of names that compose a custom character set in the generated passwords.
           - 'By default generated passwords contain a random mix of upper and lowercase ASCII letters, the numbers 0-9, and punctuation (". , : - _").'
           - "They can be either parts of Python's string module attributes or represented literally ( :, -)."
-          - "Though string modules can vary by Python version, valid values for both major releases include 'ascii_lowercase', 'ascii_uppercase', 'digits', 'hexdigits', 'octdigits', 'printable', 'punctuation' and 'whitespace'."
+          - "Though string modules can vary by Python version, valid values for both major releases include: 
+            'ascii_lowercase', 'ascii_uppercase', 'digits', 'hexdigits', 'octdigits', 'printable', 'punctuation' and 'whitespace'."
           - Be aware that Python's 'hexdigits' includes lower and upper case versions of a-f, so it is not a good choice as it doubles
             the chances of those values for systems that won't distinguish case, distorting the expected entropy.
           - "To enter comma use two commas ',,' somewhere - preferably at the end. Quotes and double quotes are not supported."
