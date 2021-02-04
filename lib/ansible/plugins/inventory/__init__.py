ansible/ansible

commit b77abd049161d797013909e4263dabfa7fba612a

Warn when transforming constructed groups (#60912)
* Warn when transforming constructed groups

The `keyed_groups` field has used sanitization since 2.6, but `groups` only started doing so in 2.8.
This adds a warning for the change in behavior.

* changelog

(cherry picked from commit 3247626ac7a3e22739e4e343ebf5919b33784c8e)

lib/ansible/plugins/inventory/__init__.py
@@ -372,7 +372,7 @@ class Constructable(object):
             self.templar.set_available_variables(variables)
             for group_name in groups:
                 conditional = "{%% if %s %%} True {%% else %%} False {%% endif %%}" % groups[group_name]
-                group_name = self._sanitize_group_name(group_name)
+                group_name = original_safe(group_name, force=True)
                 try:
                     result = boolean(self.templar.template(conditional))
                 except Exception as e:

commit 4315e1880742eedc5b65fdc51fdc6a94afa61bd1

Fix regression introduced in b77abd0491 causing bug in inventory modu… (#73429)
* Fix regression introduced in b77abd0491 causing bug in inventory modules which break functionality in user setting  use_contrib_script_compatible_sanitization  parameter.

* Add changelog

Co-authored-by: s-hertel <19572925+s-hertel@users.noreply.github.com>

lib/ansible/plugins/inventory/__init__.py
@@ -389,7 +389,7 @@ class Constructable(object):
             self.templar.available_variables = variables
             for group_name in groups:
                 conditional = "{%% if %s %%} True {%% else %%} False {%% endif %%}" % groups[group_name]
-                group_name = original_safe(group_name, force=True)
+                group_name = self._sanitize_group_name(group_name)
                 try:
                     result = boolean(self.templar.template(conditional))
                 except Exception as e: