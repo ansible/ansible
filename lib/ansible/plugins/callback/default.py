64b8d61518a9035f1b6fb45b7b2093bc8ef68120
Sam Doran <sdoran@redhat.com>
2021-02-08 12:54:24 -0500

    Keep color output consistent for now
    Currently the path is display with COLOR_DEBUG formatting with verbosity >= 2.
    Instead of the color of the path changing based on verbosity level, just keep it at the
    currenty behavior of COLOR_DEBUG. Having the color of the same information change
    based on verbosity level seems incorrect and makes visual parsing of the information
    more difficult.

--- lib/ansible/plugins/callback/default.py
+++ lib/ansible/plugins/callback/default.py
@@ -92,13 +92,13 @@ class CallbackModule(CallbackBase):
         else:
             if delegated_vars:
                 if self._display.verbosity < 2 and self.get_option('show_task_path_on_failure'):
-                    self._print_task_path(result._task, color=C.COLOR_ERROR)
+                    self._print_task_path(result._task)
                 self._display.display("fatal: [%s -> %s]: FAILED! => %s" % (result._host.get_name(), delegated_vars['ansible_host'],
                                                                             self._dump_results(result._result)),
                                       color=C.COLOR_ERROR, stderr=self.display_failed_stderr)
             else:
                 if self._display.verbosity < 2 and self.get_option('show_task_path_on_failure'):
-                    self._print_task_path(result._task, color=C.COLOR_ERROR)
+                    self._print_task_path(result._task)
                 self._display.display("fatal: [%s]: FAILED! => %s" % (result._host.get_name(), self._dump_results(result._result)),
                                       color=C.COLOR_ERROR, stderr=self.display_failed_stderr)
 
