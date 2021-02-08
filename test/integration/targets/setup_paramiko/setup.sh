aedc7301f63ff469b8cf8d4b0d4668499adda087
Matt Clay <mclay@redhat.com>
2021-02-08 15:21:15 -0800

    [stable-2.10] Temporary fix for cryptography issues. (#73530). (#73533)
    (cherry picked from commit 1a2da990a403e1c836574c5883ec0c17a98d2a65)
    
    Co-authored-by: Matt Clay <mclay@redhat.com>

--- test/integration/targets/setup_paramiko/setup.sh
+++ test/integration/targets/setup_paramiko/setup.sh
@@ -5,5 +5,5 @@ set -eux
 
 export ANSIBLE_TEST_PREFER_VENV=1
 source virtualenv.sh  # for pip installs, if needed, otherwise unused
-ansible-playbook ../setup_paramiko/install.yml -i ../setup_paramiko/inventory "$@"
+ANSIBLE_ROLES_PATH=../ ansible-playbook ../setup_paramiko/install.yml -i ../setup_paramiko/inventory "$@"
 trap 'ansible-playbook ../setup_paramiko/uninstall.yml -i ../setup_paramiko/inventory "$@"' EXIT
