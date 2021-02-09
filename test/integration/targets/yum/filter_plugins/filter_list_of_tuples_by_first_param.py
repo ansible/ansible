9918a0fc5911b4d2729cf03654c1cce12b5ae966
Rick Elrod <rick@elrod.me>
2021-02-09 14:11:34 -0600

    [yum] report upgraded multiarch packages
    Change:
    - Previously when the same package name was installed twice under
      different architectures, we only reported it once in changes.updated.
    - This was the result of using a dict internally and keying on package
      name alone.
    - This change still keys on package name but turns the values into lists
      which can contain multiple packages per name.
    
    Test Plan:
    - Added a lot of tests around multi-arch support
    - Added some tests around virtual provides as well
    
    Tickets:
    - Fixes #73284
    
    Signed-off-by: Rick Elrod <rick@elrod.me>

---
+++ test/integration/targets/yum/filter_plugins/filter_list_of_tuples_by_first_param.py
@@ -0,0 +1,25 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.errors import AnsibleError, AnsibleFilterError
+
+
+def filter_list_of_tuples_by_first_param(lst, search, startswith=False):
+    out = []
+    for element in lst:
+        if startswith:
+            if element[0].startswith(search):
+                out.append(element)
+        else:
+            if search in element[0]:
+                out.append(element)
+    return out
+
+
+class FilterModule(object):
+    ''' filter '''
+
+    def filters(self):
+        return {
+            'filter_list_of_tuples_by_first_param': filter_list_of_tuples_by_first_param,
+        }
