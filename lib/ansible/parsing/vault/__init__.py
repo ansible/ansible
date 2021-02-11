6c6a7660ec7836c188fbba3c2197c36c1999987a
Lazuardi N Putra <me@laznp.id>
2021-02-12 02:14:50 +0700

    fix typo in ansible vault decrypt if vault error (#73542)

--- lib/ansible/parsing/vault/__init__.py
+++ lib/ansible/parsing/vault/__init__.py
@@ -680,7 +680,7 @@ class VaultLib:
             raise AnsibleVaultError("A vault password must be specified to decrypt data")
 
         if not is_encrypted(b_vaulttext):
-            msg = "input is not vault encrypted data"
+            msg = "input is not vault encrypted data. "
             if filename:
                 msg += "%s is not a vault encrypted file" % to_native(filename)
             raise AnsibleError(msg)
