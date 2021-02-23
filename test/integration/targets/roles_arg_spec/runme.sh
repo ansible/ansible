9228b08b50171ed6a9734cd30b6606d77501fbc1
Sam Doran <sdoran@redhat.com>
2021-02-23 10:21:28 -0500

    Disable junit callback on roles_arg_spec_tests (#73663)
    This prevents the role arg spec validation tasks from showing up as failure
    in the CI output.

--- test/integration/targets/roles_arg_spec/runme.sh
+++ test/integration/targets/roles_arg_spec/runme.sh
@@ -2,6 +2,18 @@
 
 set -eux
 
+# This effectively disables junit callback output by directing the output to
+# a directory ansible-test will not look at.
+#
+# Since the failures in these tests are on the role arg spec validation and the
+# name for those tasks is fixed (we cannot add "EXPECTED FAILURE" to the name),
+# disabling the junit callback output is the easiest way to prevent these from
+# showing up in test run output.
+#
+# Longer term, an option can be added to the junit callback allowing a custom
+# regexp to be supplied rather than the hard coded "EXPECTED FAILURE".
+export JUNIT_OUTPUT_DIR="${OUTPUT_DIR}"
+
 # Various simple role scenarios
 ansible-playbook test.yml -i ../../inventory "$@"
 
