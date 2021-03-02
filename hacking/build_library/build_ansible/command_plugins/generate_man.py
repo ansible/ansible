d013817324307b96de4c079e29c3cabf27a4c881
Matt Martz <matt@sivel.net>
2021-03-02 16:00:05 -0600

    Allow hyphens

--- hacking/build_library/build_ansible/command_plugins/generate_man.py
+++ hacking/build_library/build_ansible/command_plugins/generate_man.py
@@ -176,7 +176,7 @@ def opts_docs(cli_class_name, cli_module_name):
             # docs['actions'][action] = {}
             # docs['actions'][action]['name'] = action
             action_info['name'] = action
-            action_info['desc'] = trim_docstring(getattr(cli, 'execute_%s' % action).__doc__)
+            action_info['desc'] = trim_docstring(getattr(cli, 'execute_%s' % action.replace('-', '_')).__doc__)
 
             # docs['actions'][action]['desc'] = getattr(cli, 'execute_%s' % action).__doc__.strip()
             action_doc_list = opt_doc_list(parser)
