ce4f71b636907786be71c4051524446df02492a9
Felix Fontein <felix@fontein.de>
2021-03-02 01:10:46 +0100

    Import test: improve docs, add more tests (#73600)

--- test/integration/targets/ansible-test/collection-tests/venv.sh
+++ test/integration/targets/ansible-test/collection-tests/venv.sh
@@ -7,7 +7,13 @@ cd "${WORK_DIR}/ansible_collections/ns/col"
 
 # rename the sanity ignore file to match the current ansible version and update import ignores with the python version
 ansible_version="$(python -c 'import ansible.release; print(".".join(ansible.release.__version__.split(".")[:2]))')"
-sed "s/ import$/ import-${ANSIBLE_TEST_PYTHON_VERSION}/;" < "tests/sanity/ignore.txt" > "tests/sanity/ignore-${ansible_version}.txt"
+if [ "${ANSIBLE_TEST_PYTHON_VERSION}" == "2.6" ]; then
+    # Non-module/module_utils plugins are not checked on this remote-only Python versions
+    sed "s/ import$/ import-${ANSIBLE_TEST_PYTHON_VERSION}/;" < "tests/sanity/ignore.txt" | grep -v 'plugins/[^m].* import' > "tests/sanity/ignore-${ansible_version}.txt"
+else
+    sed "s/ import$/ import-${ANSIBLE_TEST_PYTHON_VERSION}/;" < "tests/sanity/ignore.txt" > "tests/sanity/ignore-${ansible_version}.txt"
+fi
+cat "tests/sanity/ignore-${ansible_version}.txt"
 
 # common args for all tests
 # each test will be run in a separate venv to verify that requirements have been properly specified
