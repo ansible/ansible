79e8569c748c3e0e82acced131d566a75a89743e
Sandra McCann <samccann@redhat.com>
2021-03-03 14:06:11 -0500

    add base 2.10 to docs version switcher (#73770)
    (cherry picked from commit 65140279573dd1f91b1134b7057711c46bac06ba)

--- docs/docsite/sphinx_conf/core_conf.py
+++ docs/docsite/sphinx_conf/core_conf.py
@@ -168,7 +168,7 @@ html_context = {
     'current_version': version,
     'latest_version': '2.10',
     # list specifically out of order to make latest work
-    'available_versions': ('devel'),
+    'available_versions': ('2.10', 'devel',),
     'css_files': ('_static/ansible.css',  # overrides to the standard theme
                   ),
 }
