425610786cdc69f202cba10b4b8a154633aa8381
nitzmahone <mdavis@ansible.com>
2021-03-10 11:08:13 -0800

    add optional module_utils import support (#73832)
    * add optional module_utils import support
    
    Treat core and collections module_utils imports nested within any Python block statement (eg, `try`, `if`) as optional. This allows Ansible modules to implement runtime fallback behavior for missing module_utils (eg from a newer version of ansible-core), where previously, the module payload builder would always fail when unable to locate a module_util (regardless of any runtime behavior the module may implement).
    
    * sanity test fixes
    
    ci_complete

--- test/integration/targets/module_utils/library/test_failure.py
+++ test/integration/targets/module_utils/library/test_failure.py
@@ -6,11 +6,9 @@ results = {}
 # Test that we are rooted correctly
 # Following files:
 #   module_utils/yak/zebra/foo.py
-try:
-    from ansible.module_utils.zebra import foo
-    results['zebra'] = foo.data
-except ImportError:
-    results['zebra'] = 'Failed in module as expected but incorrectly did not fail in AnsiballZ construction'
+from ansible.module_utils.zebra import foo
+
+results['zebra'] = foo.data
 
 from ansible.module_utils.basic import AnsibleModule
 AnsibleModule(argument_spec=dict()).exit_json(**results)
