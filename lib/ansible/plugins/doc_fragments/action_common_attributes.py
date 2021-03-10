f8111c87ff85000b9611e704d05d94b74c1e471d
Brian Coca <brian.coca+git@gmail.com>
2021-03-10 10:12:00 -0500

    moar attribs

--- lib/ansible/plugins/doc_fragments/action_common_attributes.py
+++ lib/ansible/plugins/doc_fragments/action_common_attributes.py
@@ -12,13 +12,13 @@ class ModuleDocFragment(object):
     DOCUMENTATION = r'''
 attributes:
     action:
-      description: Indicates this has a corresponding action plugin so some parts of the options can be executed on the controller.
+      description: Indicates this has a corresponding action plugin so some parts of the options can be executed on the controller
       support: no
     async:
       description: Supports being used with the ``async`` keyword
       support: full
     become:
-      description: Is usable alongside become keywords.
+      description: Is usable alongside become keywords
       support: full
     bypass_host_loop:
       description: Forces a 'global' task that does not execute per host, cannot be used in non lockstep strategies
@@ -29,22 +29,31 @@ attributes:
     connection:
       description: Uses the target's configured connection information to execute code on it
       support: full
+    conditional:
+      description: Will respect the `when` keyword  per item loop or task (when no loop is present)
+      support: full
     delegation:
       description: Can be used in conjunction with delegate_to and related keywords
       support: full
-    diff_mode:
-      description: Will return details on what has changed when in diff mode
+    diff:
+      description: Will return details on what has changed when in diff is enabled
       support: no
     facts:
       description: Action returns an ``ansible_facts`` dictionary that will update existing host facts
       support: no
     proprietary:
-      description: Can only be run against specific proprietary OS, normally a network appliance or similar.
+      description: Can only be run against specific proprietary OS, normally a network appliance or similar
       support: no
     posix:
-      description: Can be run against most POSIX (and GNU/Linux) OS targets.
+      description: Can be run against most POSIX (and GNU/Linux) OS targets
+      support: full
+    tags:
+      description: Tags will determine if this task considered for execution
+      support: full
+    until:
+      description: Usable inside until/retry loops
       support: full
     windows:
-      description: Can be run against Windows OS targets.
+      description: Can be run against Windows OS targets
       support: no
 '''
