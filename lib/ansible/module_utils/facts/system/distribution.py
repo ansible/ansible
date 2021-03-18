1410b8855b0e5b189fc8ee0d15f0acb8b68a4b28
Abhijeet Kasurde <akasurde@redhat.com>
2021-03-18 11:23:01 +0530

    Amazon: Fix distribution facts for older release
    Ansible can gather distribution facts for older Amazon Linux
    with /etc/os-release data.
    
    Fixes: #73946
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/module_utils/facts/system/distribution.py
+++ lib/ansible/module_utils/facts/system/distribution.py
@@ -226,9 +226,16 @@ class DistributionFiles:
         if path == '/etc/os-release':
             version = re.search(r"VERSION_ID=\"(.*)\"", data)
             if version:
-                amazon_facts['distribution_version'] = version.group(1)
-                amazon_facts['distribution_major_version'] = version.group(1).split('.')[0]
-                amazon_facts['distribution_minor_version'] = version.group(1).split('.')[1]
+                distribution_version = version.group(1)
+                amazon_facts['distribution_version'] = distribution_version
+                version_data = distribution_version.split(".")
+                if len(version_data) > 1:
+                    major, minor = version_data
+                else:
+                    major, minor = version_data[0], 'NA'
+
+                amazon_facts['distribution_major_version'] = major
+                amazon_facts['distribution_minor_version'] = minor
         else:
             version = [n for n in data.split() if n.isdigit()]
             version = version[0] if version else 'NA'
