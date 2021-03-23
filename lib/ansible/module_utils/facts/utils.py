232eeee206df090448c3f8889b3430c6f3c42632
Brian Coca <bcoca@users.noreply.github.com>
2021-03-23 10:27:53 -0400

    less blocking on fact reading (#73951)
    * less blocking on fact reading
    
    
    Co-authored-by: Martin Krizek <martin.krizek@gmail.com>

--- lib/ansible/module_utils/facts/utils.py
+++ lib/ansible/module_utils/facts/utils.py
@@ -16,26 +16,47 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import fcntl
 import os
 
 
 def get_file_content(path, default=None, strip=True):
+    '''
+        Return the contents of a given file path
+
+        :args path: path to file to return contents from
+        :args default: value to return if we could not read file
+        :args strip: controls if we strip whitespace from the result or not
+
+        :returns: String with file contents (optionally stripped) or 'default' value
+    '''
     data = default
     if os.path.exists(path) and os.access(path, os.R_OK):
         try:
+            datafile = open(path)
             try:
-                datafile = open(path)
-                data = datafile.read()
-                if strip:
-                    data = data.strip()
-                if len(data) == 0:
-                    data = default
-            finally:
-                datafile.close()
+                # try to not enter kernel 'block' mode, which prevents timeouts
+                fd = datafile.fileno()
+                flag = fcntl.fcntl(fd, fcntl.F_GETFL)
+                fcntl.fcntl(fd, fcntl.F_SETFL, flag | os.O_NONBLOCK)
+            except Exception:
+                pass  # not required to operate, but would have been nice!
+
+            # actually read the data
+            data = datafile.read()
+
+            if strip:
+                data = data.strip()
+
+            if len(data) == 0:
+                data = default
+
         except Exception:
-            # ignore errors as some jails/containers might have readable permissions but not allow reads to proc
-            # done in 2 blocks for 2.4 compat
+            # ignore errors as some jails/containers might have readable permissions but not allow reads
             pass
+        finally:
+            datafile.close()
+
     return data
 
 
