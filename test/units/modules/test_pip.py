bebc6861fe5563d26af228eb318dd33477706220
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-04-01 23:54:10 +0200

    Add example use of fixtures in test+pip

--- test/units/modules/test_pip.py
+++ test/units/modules/test_pip.py
@@ -10,10 +10,13 @@ import pytest
 from ansible.modules import pip
 
 
-pytestmark = pytest.mark.usefixtures('ansible_module_args')
+@pytest.fixture
+def ansible_global_module_name():
+    return 'ansible.modules.pip'
 
 
 @pytest.mark.parametrize('ansible_module_args', [{'name': 'six'}], indirect=['ansible_module_args'])
+@pytest.mark.usefixtures('ansible_module_args')
 def test_failure_when_pip_absent(mocker, capfd):
     mocker.patch('ansible.modules.pip._have_pip_module').return_value = False
 
@@ -36,5 +39,34 @@ def test_failure_when_pip_absent(mocker, capfd):
         ['django>1.11.1,<1.11.2', 'ipaddress', 'simpleproject<2.0.0,>1.1.0']],
     [None, ['django>1.11.1', '<1.11.2', 'ipaddress,simpleproject<2.0.0,>1.1.0'],
         ['django>1.11.1,<1.11.2', 'ipaddress', 'simpleproject<2.0.0,>1.1.0']]])
+@pytest.mark.usefixtures('ansible_module_args')
 def test_recover_package_name(test_input, expected):
     assert pip._recover_package_name(test_input) == expected
+
+
+def test_new_mod_make(make_ansible_module_caller):
+    # run_module = make_ansible_module_caller('pip')
+    # run_module = make_ansible_module_caller('ansible_collections.ansible.builtin.pip')
+    # run_module = make_ansible_module_caller(
+    #     'ansible_collections.community.general.modules.pip_package_info',
+    # )
+    # run_module = make_ansible_module_caller(
+    #     'ansible_collections.community.general.pip_package_info',
+    # )
+    run_module = make_ansible_module_caller('ansible.modules.pip')
+    assert run_module(name='bottle') == {}
+
+
+def test_new_mod_global_fail(run_ansible_module):
+    failed_response = run_module(name='bottle')
+    # assert failed_response.failed  # FIXME?
+    assert failed_response['failed']
+
+
+@pytest.mark.parametrize(
+    'ansible_module_name',
+    ('ansible.modules.pip', ),
+    indirect=('ansible_module_name', ),
+)
+def test_new_mod_param(run_ansible_module):
+    assert run_ansible_module(name='bottle', executable='pip3.9') == {}
