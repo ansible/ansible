890b6294c010454839f5ff9f2051be1e8cf947ea
LoveIsGrief <loveisgrief@tuta.io>
2021-04-05 10:40:28 +0200

    ansible.builtin.pipe: Minor beauty changes
    Editor had inserted extra newlines and imported the module at the wrong place.

--- lib/ansible/plugins/connection/ssh.py
+++ lib/ansible/plugins/connection/ssh.py
@@ -5,10 +5,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
-
 __metaclass__ = type
-
-import io
 
 DOCUMENTATION = '''
     name: ssh
@@ -330,6 +327,7 @@ DOCUMENTATION = '''
 import errno
 import fcntl
 import hashlib
+import io
 import os
 import pty
 import re
