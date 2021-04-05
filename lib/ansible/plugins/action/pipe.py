90bae5fb4558b6d3e1ee22e58117fdfb65d8d14a
LoveIsGrief <loveisgrief@tuta.io>
2021-04-05 10:35:09 +0200

    ansible.builtin.pipe: Add boilerplate header

--- lib/ansible/plugins/action/pipe.py
+++ lib/ansible/plugins/action/pipe.py
@@ -2,6 +2,10 @@
 Copyright (C) 2021 LoveIsGrief
 GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 """
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
 import subprocess
 
 from ansible.errors import AnsibleError
