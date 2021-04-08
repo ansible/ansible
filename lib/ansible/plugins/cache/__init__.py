977f2199d02e5c4a4e12537b2f6a9e9c17fde3c6
s-hertel <19572925+s-hertel@users.noreply.github.com>
2021-04-08 15:47:43 -0400

    Remove deprecated cache plugin interfaces

--- lib/ansible/plugins/cache/__init__.py
+++ lib/ansible/plugins/cache/__init__.py
@@ -33,25 +33,8 @@ from ansible.plugins import AnsiblePlugin
 from ansible.plugins.loader import cache_loader
 from ansible.utils.collection_loader import resource_from_fqcr
 from ansible.utils.display import Display
-from ansible.vars.fact_cache import FactCache as RealFactCache
 
 display = Display()
-
-
-class FactCache(RealFactCache):
-    """
-    This is for backwards compatibility.  Will be removed after deprecation.  It was removed as it
-    wasn't actually part of the cache plugin API.  It's actually the code to make use of cache
-    plugins, not the cache plugin itself.  Subclassing it wouldn't yield a usable Cache Plugin and
-    there was no facility to use it as anything else.
-    """
-    def __init__(self, *args, **kwargs):
-        display.deprecated('ansible.plugins.cache.FactCache has been moved to'
-                           ' ansible.vars.fact_cache.FactCache.  If you are looking for the class'
-                           ' to subclass for a cache plugin, you want'
-                           ' ansible.plugins.cache.BaseCacheModule or one of its subclasses.',
-                           version='2.12', collection_name='ansible.builtin')
-        super(FactCache, self).__init__(*args, **kwargs)
 
 
 class BaseCacheModule(AnsiblePlugin):
