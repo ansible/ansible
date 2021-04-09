ef554d03785dff43a03a1121e921eb55ff5c0bb4
Rick Elrod <rick@elrod.me>
2021-04-09 09:28:24 -0500

    [playbook] error on empty, error on 'include' (remove two deprecations) (#74172)
    Change:
    - Remove two deprecated features
    - We now error if a playbook is an empty list instead of just skipping
    - We now error if using 'include' instead of 'import_playbook'
    
    Test Plan:
    - Added new tests for new errors
    
    Tickets:
    - Fixes #74133
    
    Signed-off-by: Rick Elrod <rick@elrod.me>
    
    * sanity & changelog
    
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- test/integration/targets/playbook/runme.sh
+++ test/integration/targets/playbook/runme.sh
@@ -7,3 +7,9 @@ ansible-playbook -i ../../inventory types.yml -v "$@"
 
 # test timeout
 ansible-playbook -i ../../inventory timeout.yml -v "$@"
+
+echo "EXPECTED ERROR: Ensure we fail properly if a playbook is an empty list."
+set +e
+result="$(ansible-playbook -i ../../inventory empty.yml -v "$@" 2>&1)"
+set -e
+grep -q "ERROR! A playbook must contain at least one play" <<< "$result"
