de1ecfdb370e3b4b4cebb1d2b848868426e42ad8
Brian Coca <brian.coca+git@gmail.com>
2021-04-12 10:49:37 -0400

    wrap

--- lib/ansible/modules/async_wrapper.py
+++ lib/ansible/modules/async_wrapper.py
@@ -288,7 +288,7 @@ def main():
 
             notice("Return async_wrapper task started.")
             end({"started": 1, "finished": 0, "ansible_job_id": jid, "results_file": job_path,
-                              "_ansible_suppress_tmpdir_delete": not preserve_tmp})
+                              "_ansible_suppress_tmpdir_delete": (not preserve_tmp)})
         else:
             # The actual wrapper process
 
