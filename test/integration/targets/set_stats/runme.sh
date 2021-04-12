b5fd5a3e9f9301a28c594777f48467596dcd7660
David Shrewsbury <dshrewsb@redhat.com>
2021-04-12 16:25:33 -0400

    satisfy the all-knowing, ever present God of Sanity and use 'grep -c' vs. 'wc -l'

--- test/integration/targets/set_stats/runme.sh
+++ test/integration/targets/set_stats/runme.sh
@@ -9,5 +9,5 @@ ansible-playbook test_simple.yml
 
 # This playbook does two set_stats calls setting my_int to 10 and 15.
 # The aggregated output should add to 25.
-output=$(ansible-playbook test_aggregate.yml | grep '"my_int": 25' | wc -l)
+output=$(ansible-playbook test_aggregate.yml | grep -c '"my_int": 25')
 test "$output" -eq 1
