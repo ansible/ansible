2562f8b4db112886cd33970b2518378f6f196f09
Brian Coca <brian.coca+git@gmail.com>
2021-04-12 10:20:18 -0400

    really fix

--- test/units/modules/test_async_wrapper.py
+++ test/units/modules/test_async_wrapper.py
@@ -42,9 +42,10 @@ class TestAsyncWrapper:
 
         command = fn
         jobid = 0
-        jobpath = os.path.join(os.path.dirname(command), 'job')
+        job_path = os.path.join(os.path.dirname(command), 'job')
 
         monkeypatch.setattr(async_wrapper, '_get_interpreter', mock_get_interpreter)
+        monkeypatch.setattr(async_wrapper, 'job_path', job_path)
 
         res = async_wrapper._run_module(command, jobid)
 
