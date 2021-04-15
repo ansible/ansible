8f22213c4e2e8ec7f2b81175232ea5cfa43e83a0
Brian Coca <brian.coca+git@gmail.com>
2021-04-15 16:32:19 -0400

    fix subdir parsing for plays
      fixes #74283
    
      Co-authored-by: Nikolaos Kakouros <nkak@kth.se>

--- lib/ansible/utils/collection_loader/_collection_finder.py
+++ lib/ansible/utils/collection_loader/_collection_finder.py
@@ -890,13 +890,17 @@ def _get_collection_playbook_path(playbook):
             pkg = None
 
         if pkg:
+            subdirs = []
+            if acr.subdirs:
+                subdirs = acr.subdirs.split(u'.')
+
             cpath = os.path.join(sys.modules[acr.n_python_collection_package_name].__file__.replace('__synthetic__', 'playbooks'))
-            path = os.path.join(cpath, to_native(acr.resource))
+            path = os.path.join(cpath, *subdirs, to_native(acr.resource))
             if os.path.exists(to_bytes(path)):
                 return acr.resource, path, acr.collection
             elif not acr.resource.endswith(PB_EXTENSIONS):
                 for ext in PB_EXTENSIONS:
-                    path = os.path.join(cpath, to_native(acr.resource + ext))
+                    path = os.path.join(cpath, *subdirs, to_native(acr.resource + ext))
                     if os.path.exists(to_bytes(path)):
                         return acr.resource, path, acr.collection
     return None
