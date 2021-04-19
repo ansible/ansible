ef8ff1c05c8f453e4eec2ecff0200dd81b243b50
Brian Coca <brian.coca+git@gmail.com>
2021-04-19 10:49:18 -0400

    added more attributes for existing facilities

--- lib/ansible/plugins/doc_fragments/action_common_attributes.py
+++ lib/ansible/plugins/doc_fragments/action_common_attributes.py
@@ -14,6 +14,10 @@ attributes:
     action:
       description: Indicates this has a corresponding action plugin so some parts of the options can be executed on the controller
       support: none
+    action_group:
+      description: If module_defaults are settable for this action, as it is part of an action_group
+      support: none
+      membership: []
     async:
       description: Supports being used with the ``async`` keyword
       support: full
@@ -41,6 +45,16 @@ attributes:
     facts:
       description: Action returns an ``ansible_facts`` dictionary that will update existing host facts
       support: none
+    forced_local:
+      description: The connection itself is passed to the action while the code is still executed on the controller
+      support: none
+    forced_action_plugin:
+      description: This action uses a specific action plugin instead of 'normal' or matching by name
+      support: none
+      action_plugin: none
+    info:
+      description: This returns general info (not facts) that you might want to register into a variable for later use
+      support: none
     loops:
       description: both ``loop`` and ``with_`` looping keywords will be honored.
       support: full
@@ -53,8 +67,15 @@ attributes:
     tags:
       description: Tags will determine if this task considered for execution
       support: full
+    tty:
+      description: requires direct access to a TTY
+      support: none
+    turbo:
+      description: Uses an Ansible supplied caching mechanism (Turbo!) on the remote for authentication and
+                   3rd party libraries to speed up recurrent execution.
+      support: none
     until:
-      description: Usable inside until/retry loops
+      description: Usable with until/retry loops
       support: full
     windows:
       description: Can be run against Windows OS targets
