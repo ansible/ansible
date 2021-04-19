ansible/ansible

commit 0d7c144ce44cd40ffa7c109a027d0927961d6a63

Allow macOS ACLs to work for unpriv -> unpriv (#70785)
Change:
- Use `chmod +a` in the fallback chain to allow MacOS to use ACLs to
  allow an unprivileged user to become an unprivileged user.

Test Plan:
- CI, new tests

Tickets:
- Fixes #70648

Signed-off-by: Rick Elrod <rick@elrod.me>

lib/ansible/plugins/action/__init__.py
@@ -518,7 +518,9 @@ class ActionBase(with_metaclass(ABCMeta, object)):
           file with chown which only works in case the remote_user is
           privileged or the remote systems allows chown calls by unprivileged
           users (e.g. HP-UX)
-        * If the chown fails, we check if ansible_common_remote_group is set.
+        * If the above fails, we next try 'chmod +a' which is a macOS way of
+          setting ACLs on files.
+        * If the above fails, we check if ansible_common_remote_group is set.
           If it is, we attempt to chgrp the file to its value. This is useful
           if the remote_user has a group in common with the become_user. As the
           remote_user, we can chgrp the file to that group and allow the
@@ -571,12 +573,16 @@ class ActionBase(with_metaclass(ABCMeta, object)):
         if execute:
             chmod_mode = 'rx'
             setfacl_mode = 'r-x'
+            # Apple patches their "file_cmds" chmod with ACL support
+            chmod_acl_mode = '{0} allow read,execute'.format(become_user)
         else:
             chmod_mode = 'rX'
             # TODO: this form fails silently on freebsd.  We currently
             # never call _fixup_perms2() with execute=False but if we
             # start to we'll have to fix this.
             setfacl_mode = 'r-X'
+            # Apple
+            chmod_acl_mode = '{0} allow read'.format(become_user)
 
         # Step 3a: Are we able to use setfacl to add user ACLs to the file?
         res = self._remote_set_user_facl(
@@ -613,7 +619,15 @@ class ActionBase(with_metaclass(ABCMeta, object)):
                 'Unprivileged become user would be unable to read the '
                 'file.')
 
-        # Step 3d: Common group
+        # Step 3d: Try macOS's special chmod + ACL
+        # macOS chmod's +a flag takes its own argument. As a slight hack, we
+        # pass that argument as the first element of remote_paths. So we end
+        # up running `chmod +a [that argument] [file 1] [file 2] ...`
+        res = self._remote_chmod([chmod_acl_mode] + remote_paths, '+a')
+        if res['rc'] == 0:
+            return remote_paths
+
+        # Step 3e: Common group
         # Otherwise, we're a normal user. We failed to chown the paths to the
         # unprivileged user, but if we have a common group with them, we should
         # be able to chown it to that.

test/units/plugins/action/test_action.py
@@ -358,6 +358,9 @@ class TestActionBase(unittest.TestCase):
                 return args_kv.get(option, default)
             return _helper
 
+        action_base.get_become_option = MagicMock()
+        action_base.get_become_option.return_value = 'remoteuser2'
+
         # Step 1: On windows, we just return remote_paths
         action_base._connection._shell._IS_WINDOWS = True
         assertSuccess(execute=False)
@@ -413,12 +416,6 @@ class TestActionBase(unittest.TestCase):
             'stderr': '',
         }
         action_base._remote_chmod.return_value = {
-            'rc': 0,
-            'stdout': 'some stuff here',
-            'stderr': '',
-        }
-        assertSuccess(execute=True)
-        action_base._remote_chmod.return_value = {
             'rc': 1,
             'stdout': 'some stuff here',
             'stderr': '',
@@ -426,6 +423,12 @@ class TestActionBase(unittest.TestCase):
         assertThrowRegex(
             'Failed to set file mode on remote temporary file',
             execute=True)
+        action_base._remote_chmod.return_value = {
+            'rc': 0,
+            'stdout': 'some stuff here',
+            'stderr': '',
+        }
+        assertSuccess(execute=True)
 
         # Step 3c: chown
         action_base._remote_chown = MagicMock()
@@ -446,7 +449,18 @@ class TestActionBase(unittest.TestCase):
         assertThrowRegex('user would be unable to read the file.')
         remote_user = 'remoteuser1'
 
-        # Step 3d: Common group
+        # Step 3d: chmod +a on osx
+        assertSuccess()
+        action_base._remote_chmod.assert_called_with(
+            ['remoteuser2 allow read'] + remote_paths,
+            '+a')
+
+        # Step 3e: Common group
+        action_base._remote_chmod = MagicMock()
+        action_base._remote_chmod.side_effect = \
+            lambda x, y: \
+            dict(rc=1, stdout='', stderr='') if y == '+a' \
+            else dict(rc=0, stdout='', stderr='')
 
         get_shell_option = action_base.get_shell_option
         action_base.get_shell_option = MagicMock()
@@ -463,11 +477,6 @@ class TestActionBase(unittest.TestCase):
         }
         # TODO: Add test to assert warning is shown if
         # ALLOW_WORLD_READABLE_TMPFILES is set in this case.
-        action_base._remote_chmod.return_value = {
-            'rc': 0,
-            'stdout': '',
-            'stderr': '',
-        }
         assertSuccess()
         action_base._remote_chgrp.assert_called_once_with(
             remote_paths,
@@ -486,6 +495,7 @@ class TestActionBase(unittest.TestCase):
             'stderr': '',
         }
         assertSuccess()
+        action_base._remote_chmod = MagicMock()
         action_base._remote_chmod.return_value = {
             'rc': 1,
             'stdout': 'some stuff here',

test/integration/targets/become_unprivileged/aliases
@@ -2,3 +2,4 @@ destructive
 shippable/posix/group1
 skip/aix
 needs/ssh
+needs/root

test/integration/targets/become_unprivileged/chmod_acl_macos/test.yml
@@ -1,0 +1,26 @@
+- name: Tests for chmod +a ACL functionality on macOS
+  hosts: ssh
+  gather_facts: yes
+  remote_user: unpriv1
+  become: yes
+  become_user: unpriv2
+
+  tasks:
+    - name: Get AnsiballZ temp directory
+      action: tmpdir
+      register: tmpdir
+      become_user: unpriv2
+      become: yes
+
+    - name: run whoami
+      command: whoami
+      register: whoami
+
+    - name: Ensure we used the right fallback
+      shell: ls -le /var/tmp/ansible*/*_command.py
+      register: ls
+
+    - assert:
+        that:
+          - whoami.stdout == "unpriv2"
+          - "'user:unpriv2 allow read' in ls.stdout"

test/integration/targets/become_unprivileged/runme.sh
@@ -2,6 +2,11 @@
 
 set -eux
 
+export ANSIBLE_KEEP_REMOTE_FILES=True
+ANSIBLE_ACTION_PLUGINS="$(pwd)/action_plugins"
+export ANSIBLE_ACTION_PLUGINS
+export ANSIBLE_BECOME_PASS='iWishIWereCoolEnoughForRoot!'
+
 begin_sandwich() {
     ansible-playbook setup_unpriv_users.yml -i inventory -v "$@"
 }
@@ -24,12 +29,24 @@ end_sandwich() {
 trap "end_sandwich \"\$@\"" EXIT
 
 # Common group tests
-begin_sandwich "$@"
-  ansible-playbook common_remote_group/setup.yml -i inventory -v "$@"
-  export ANSIBLE_KEEP_REMOTE_FILES=True
-  export ANSIBLE_COMMON_REMOTE_GROUP=commongroup
-  export ANSIBLE_BECOME_PASS='iWishIWereCoolEnoughForRoot!'
-  ANSIBLE_ACTION_PLUGINS="$(pwd)/action_plugins"
-  export ANSIBLE_ACTION_PLUGINS
-  ansible-playbook common_remote_group/test.yml -i inventory -v "$@"
-end_sandwich "$@"
+# Skip on macOS, chmod fallback will take over.
+# 1) chmod is stupidly hard to disable, so hitting this test case on macOS would
+#    be a suuuuuuper edge case scenario
+# 2) even if we can trick it so chmod doesn't exist, then other things break.
+#    Ansible wants a `chmod` around, even if it's not the final thing that gets
+#    us enough permission to run the task.
+if [[ "$OSTYPE" != darwin* ]]; then
+  begin_sandwich "$@"
+    ansible-playbook common_remote_group/setup.yml -i inventory -v "$@"
+    export ANSIBLE_COMMON_REMOTE_GROUP=commongroup
+    ansible-playbook common_remote_group/test.yml -i inventory -v "$@"
+  end_sandwich "$@"
+fi
+
+if [[ "$OSTYPE" == darwin* ]]; then
+  begin_sandwich "$@"
+    # In the default case this should happen on macOS, so no need for a setup
+    # It should just work.
+    ansible-playbook chmod_acl_macos/test.yml -i inventory -v "$@"
+  end_sandwich "$@"
+fi

commit 6eafa4028093003abdd1d4388d29fd13edbfc2ef

[become] Fix solaris permissions regression
Change:
- Regression introduced in #70785
- When macOS chmod ACL syntax is used, Solaris-derived chmods return
  with a status of 5. This is also used for our sshpass handling,
  because sshpass will return 5 on auth failure. This means on Solaris,
  we incorrectly assume auth failure when we reach this branch of logic
  and try to run chmod with macOS syntax.
- We now wrap this specific use of chmod in an exception handler that
  looks for AnsibleAuthenticationFailure and skips over it. This adds
  another authentication attempt (something we normally avoid to prevent
  account lockout), but seems better than the regression of not allowing
  other fallbacks to be used.
- Without this patch, if setfacl fails on Solaris (and sshpass is used),
  we do not try common_remote_group or world-readable tmpdir fallbacks.

Test Plan:
- New unit

Signed-off-by: Rick Elrod <rick@elrod.me>

lib/ansible/plugins/action/__init__.py
@@ -18,7 +18,7 @@ import time
 from abc import ABCMeta, abstractmethod
 
 from ansible import constants as C
-from ansible.errors import AnsibleError, AnsibleConnectionFailure, AnsibleActionSkip, AnsibleActionFail, AnsiblePluginRemovedError
+from ansible.errors import AnsibleError, AnsibleConnectionFailure, AnsibleActionSkip, AnsibleActionFail, AnsiblePluginRemovedError, AnsibleAuthenticationFailure
 from ansible.executor.module_common import modify_module
 from ansible.executor.interpreter_discovery import discover_interpreter, InterpreterDiscoveryRequiredError
 from ansible.module_utils.common._collections_compat import Sequence
@@ -624,9 +624,20 @@ class ActionBase(with_metaclass(ABCMeta, object)):
         # macOS chmod's +a flag takes its own argument. As a slight hack, we
         # pass that argument as the first element of remote_paths. So we end
         # up running `chmod +a [that argument] [file 1] [file 2] ...`
-        res = self._remote_chmod([chmod_acl_mode] + remote_paths, '+a')
-        if res['rc'] == 0:
-            return remote_paths
+        try:
+            res = self._remote_chmod([chmod_acl_mode] + remote_paths, '+a')
+        except AnsibleAuthenticationFailure as e:
+            # Solaris-based chmod will return 5 when it sees an invalid mode,
+            # and +a is invalid there. Because it returns 5, which is the same
+            # thing sshpass returns on auth failure, our sshpass code will
+            # assume that auth failed. If we don't handle that case here, none
+            # of the other logic below will get run. This is fairly hacky and a
+            # corner case, but probably one that shows up pretty often in
+            # Solaris-based environments (and possibly others).
+            pass
+        else:
+            if res['rc'] == 0:
+                return remote_paths
 
         # Step 3e: Common group
         # Otherwise, we're a normal user. We failed to chown the paths to the

test/units/plugins/action/test_action.py
@@ -27,7 +27,7 @@ from ansible import constants as C
 from units.compat import unittest
 from units.compat.mock import patch, MagicMock, mock_open
 
-from ansible.errors import AnsibleError
+from ansible.errors import AnsibleError, AnsibleAuthenticationFailure
 from ansible.module_utils.six import text_type
 from ansible.module_utils.six.moves import shlex_quote, builtins
 from ansible.module_utils._text import to_bytes
@@ -332,6 +332,9 @@ class TestActionBase(unittest.TestCase):
         remote_paths = ['/tmp/foo/bar.txt', '/tmp/baz.txt']
         remote_user = 'remoteuser1'
 
+        # Used for skipping down to common group dir.
+        CHMOD_ACL_FLAGS = ('+a', 'A+user:remoteuser2:r:allow')
+
         def runWithNoExpectation(execute=False):
             return action_base._fixup_perms2(
                 remote_paths,
@@ -455,12 +458,27 @@ class TestActionBase(unittest.TestCase):
             ['remoteuser2 allow read'] + remote_paths,
             '+a')
 
+        # This case can cause Solaris chmod to return 5 which the ssh plugin
+        # treats as failure. To prevent a regression and ensure we still try the
+        # rest of the cases below, we mock the thrown exception here.
+        # This function ensures that only the macOS case (+a) throws this.
+        def raise_if_plus_a(definitely_not_underscore, mode):
+            if mode == '+a':
+                raise AnsibleAuthenticationFailure()
+            return {'rc': 0, 'stdout': '', 'stderr': ''}
+
+        action_base._remote_chmod.side_effect = raise_if_plus_a
+        assertSuccess()
+
         # Step 3e: Common group
+        def rc_1_if_chmod_acl(definitely_not_underscore, mode):
+            rc = 0
+            if mode in CHMOD_ACL_FLAGS:
+                rc = 1
+            return {'rc': rc, 'stdout': '', 'stderr': ''}
+
         action_base._remote_chmod = MagicMock()
-        action_base._remote_chmod.side_effect = \
-            lambda x, y: \
-            dict(rc=1, stdout='', stderr='') if y == '+a' \
-            else dict(rc=0, stdout='', stderr='')
+        action_base._remote_chmod.side_effect = rc_1_if_chmod_acl
 
         get_shell_option = action_base.get_shell_option
         action_base.get_shell_option = MagicMock()