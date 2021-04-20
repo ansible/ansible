199ec95ded32bd407d6ad4041757a40be1a48d9f
Florian Manschwetus <manschwetus@cs-software-gmbh.de>
2021-04-20 14:46:18 +0200

    fixup! updated version added

--- lib/ansible/modules/copy.py
+++ lib/ansible/modules/copy.py
@@ -124,7 +124,7 @@ options:
     - If this is not provided, or no, ansible will preserve historical behaviour of copying stats from src to dest.
     default: no
     type: bool
-    version_added: '2.11'
+    version_added: '2.12'
 extends_documentation_fragment:
 - decrypt
 - files
