2a5a5e0dd8743c35d42c5d10dedf951cad4ac5e0
Brian Coca <bcoca@users.noreply.github.com>
2021-04-26 12:45:03 -0400

    Update lib/ansible/executor/task_queue_manager.py
    Co-authored-by: David Shrewsbury <Shrews@users.noreply.github.com>

--- lib/ansible/executor/task_queue_manager.py
+++ lib/ansible/executor/task_queue_manager.py
@@ -225,7 +225,7 @@ class TaskQueueManager:
             host = self._inventory.get_host(host_name)
             if host is None:
                 display.warning("Lost host '%s' from inventory, removing from rest of play execution." % host_name)
-                iterator.host_not_in_invenotry(host_name)
+                iterator.host_not_in_inventory(host_name)
             else:
                 iterator.mark_host_failed(host)
 
