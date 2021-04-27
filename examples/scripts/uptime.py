9f7767e1f9d4381a351a1895ac5f19704775d120
Abhijeet Kasurde <akasurde@redhat.com>
2021-04-27 20:57:55 +0530

    Update examples/scripts/uptime.py (#74438)
    Fixes missing parameter 'verbosity'
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- examples/scripts/uptime.py
+++ examples/scripts/uptime.py
@@ -54,7 +54,7 @@ def main():
     host_list = ['localhost', 'www.example.com', 'www.google.com']
     # since the API is constructed for CLI it expects certain options to always be set in the context object
     context.CLIARGS = ImmutableDict(connection='smart', module_path=['/to/mymodules', '/usr/share/ansible'], forks=10, become=None,
-                                    become_method=None, become_user=None, check=False, diff=False)
+                                    become_method=None, become_user=None, check=False, diff=False, verbosity=0)
     # required for
     # https://github.com/ansible/ansible/blob/devel/lib/ansible/inventory/manager.py#L204
     sources = ','.join(host_list)
