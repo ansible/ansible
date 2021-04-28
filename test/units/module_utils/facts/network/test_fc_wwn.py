456ea20150b1e664a56003da8206e2493a1c23e0
Anatoly Pugachev <matorola@gmail.com>
2021-04-28 17:17:19 +0300

    [facts] add hpux fc info, refactor aix and solaris code (#57406)
    * correctly parse device from string
    * check for command presence before running them
    * check for command presence and return code for solaris and aix as well
    * add changelog

--- test/units/module_utils/facts/network/test_fc_wwn.py
+++ test/units/module_utils/facts/network/test_fc_wwn.py
@@ -54,8 +54,43 @@ HBA Port WWN: 10000090fa1658de
         NPIV Not Supported
 """
 
+IOSCAN_OUT = """
+Class     I  H/W Path    Driver S/W State   H/W Type     Description
+==================================================================
+fc        0  2/0/10/1/0  fcd   CLAIMED     INTERFACE    HP AB379-60101 4Gb Dual Port PCI/PCI-X Fibre Channel Adapter (FC Port 1)
+                        /dev/fcd0
+"""
 
-def mock_get_bin_path(cmd, required=False):
+FCMSUTIL_OUT = """
+                           Vendor ID is = 0x1077
+                           Device ID is = 0x2422
+            PCI Sub-system Vendor ID is = 0x103C
+                   PCI Sub-system ID is = 0x12D7
+                               PCI Mode = PCI-X 133 MHz
+                       ISP Code version = 5.4.0
+                       ISP Chip version = 3
+                               Topology = PTTOPT_FABRIC
+                             Link Speed = 4Gb
+                     Local N_Port_id is = 0x010300
+                  Previous N_Port_id is = None
+            N_Port Node World Wide Name = 0x50060b00006975ed
+            N_Port Port World Wide Name = 0x50060b00006975ec
+            Switch Port World Wide Name = 0x200300051e046c0f
+            Switch Node World Wide Name = 0x100000051e046c0f
+              N_Port Symbolic Port Name = server1_fcd0
+              N_Port Symbolic Node Name = server1_HP-UX_B.11.31
+                           Driver state = ONLINE
+                       Hardware Path is = 2/0/10/1/0
+                     Maximum Frame Size = 2048
+         Driver-Firmware Dump Available = NO
+         Driver-Firmware Dump Timestamp = N/A
+                                   TYPE = PFC
+                         NPIV Supported = YES
+                         Driver Version = @(#) fcd B.11.31.1103 Dec  6 2010
+"""
+
+
+def mock_get_bin_path(cmd, required=False, opt_dirs=None):
     result = None
     if cmd == 'lsdev':
         result = '/usr/sbin/lsdev'
@@ -63,6 +98,10 @@ def mock_get_bin_path(cmd, required=False):
         result = '/usr/sbin/lscfg'
     elif cmd == 'fcinfo':
         result = '/usr/sbin/fcinfo'
+    elif cmd == 'ioscan':
+        result = '/usr/bin/ioscan'
+    elif cmd == 'fcmsutil':
+        result = '/opt/fcms/bin/fcmsutil'
     return result
 
 
@@ -74,6 +113,10 @@ def mock_run_command(cmd):
         result = LSCFG_OUTPUT
     elif 'fcinfo' in cmd:
         result = FCINFO_OUTPUT
+    elif 'ioscan' in cmd:
+        result = IOSCAN_OUT
+    elif 'fcmsutil' in cmd:
+        result = FCMSUTIL_OUT
     else:
         rc = 1
         result = 'Error'
@@ -87,7 +130,7 @@ def test_get_fc_wwn_info(mocker):
     mocker.patch.object(module, 'get_bin_path', side_effect=mock_get_bin_path)
     mocker.patch.object(module, 'run_command', side_effect=mock_run_command)
 
-    d = {'aix6': ['10000090FA551508'], 'sunos5': ['10000090fa1658de']}
+    d = {'aix6': ['10000090FA551508'], 'sunos5': ['10000090fa1658de'], 'hp-ux11': ['0x50060b00006975ec']}
     for key, value in d.items():
         mocker.patch('sys.platform', key)
         wwn_expected = {"fibre_channel_wwn": value}
