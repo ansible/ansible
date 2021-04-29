3aa14e92647e3d30ca0c1e3cd04c8a7b1cb9d0bc
Rene Moser <mail@renemoser.net>
2021-04-29 17:29:07 +0200

    modules: get_url: Fix checksum binary validation
    From the sha512sum man page:
    
    ... The default mode is to print a line with checksum, a character indicating type ('*' for binary, ' ' for text), and name for each FILE.

--- lib/ansible/modules/get_url.py
+++ lib/ansible/modules/get_url.py
@@ -511,14 +511,17 @@ def main():
             os.remove(checksum_tmpsrc)
             checksum_map = []
             for line in lines:
-                parts = line.split(None, 1)
+                # Split by one whitespace to keep the leading type char ' ' (whitespace) for text and '*' for binary
+                parts = line.split(" ", 1)
                 if len(parts) == 2:
-                    checksum_map.append((parts[0], parts[1]))
+                    # Remove the leading type char and use basename
+                    checksum_map.append((parts[0], os.path.basename(parts[1][1:])))
+
             filename = url_filename(url)
 
             # Look through each line in the checksum file for a hash corresponding to
             # the filename in the url, returning the first hash that is found.
-            for cksum in (s for (s, f) in checksum_map if f.strip('./') == filename):
+            for cksum in (s for (s, f) in checksum_map if f == filename):
                 checksum = cksum
                 break
             else:
