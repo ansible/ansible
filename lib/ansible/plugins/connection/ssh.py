9b1ef05aa4f20ddfbea41bfd673dbdefc3ba3e60
Brian Coca <bcoca@users.noreply.github.com>
2021-05-03 17:19:52 -0400

    Update lib/ansible/plugins/connection/ssh.py
    Co-authored-by: Sam Doran <sdoran@redhat.com>

--- lib/ansible/plugins/connection/ssh.py
+++ lib/ansible/plugins/connection/ssh.py
@@ -21,7 +21,7 @@ DOCUMENTATION = '''
     version_added: historical
     notes:
         - Many options default to 'None' here but that only means we don't override the ssh tool's defaults and/or configuration.
-          For example, if you specify the port in this plugin it will override any `Port` entry in your `.ssh/config`.
+          For example, if you specify the port in this plugin it will override any C(Port) entry in your C(.ssh/config).
     options:
       host:
           description: Hostname/ip to connect to.
