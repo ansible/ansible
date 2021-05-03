3542464728ad853ae417ebadfe7753ceaf42301d
Brian Coca <brian.coca+git@gmail.com>
2021-05-03 17:30:48 -0400

    actually test it

--- test/integration/targets/connection_ssh/runme.sh
+++ test/integration/targets/connection_ssh/runme.sh
@@ -65,3 +65,7 @@ fi
 ANSIBLE_SCP_IF_SSH=true ./posix.sh "$@" "${scp_args[@]}"
 # piped
 ANSIBLE_SSH_TRANSFER_METHOD=piped ./posix.sh "$@"
+
+
+# test config defaults override
+ansible-playbook check_ssh_defaults.yml "$@"
