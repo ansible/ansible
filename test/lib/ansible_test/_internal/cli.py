2713ce7fc69b6af6dd5a56df74ed795819ceb589
Matt Clay <matt@mystile.com>
2021-05-05 15:30:48 -0700

    Relocate ansible-test integration code.

--- test/lib/ansible_test/_internal/cli.py
+++ test/lib/ansible_test/_internal/cli.py
@@ -22,6 +22,7 @@ from .util import (
     read_lines_without_comments,
     MAXFD,
     ANSIBLE_TEST_DATA_ROOT,
+    SUPPORTED_PYTHON_VERSIONS,
 )
 
 from .delegation import (
@@ -30,15 +31,23 @@ from .delegation import (
 )
 
 from .executor import (
-    command_posix_integration,
-    command_network_integration,
-    command_windows_integration,
-    SUPPORTED_PYTHON_VERSIONS,
     ApplicationWarning,
     Delegate,
     generate_pip_install,
     check_startup,
     configure_pypi_proxy,
+)
+
+from .commands.integration.posix import (
+    command_posix_integration,
+)
+
+from .commands.integration.network import (
+    command_network_integration,
+)
+
+from .commands.integration.windows import (
+    command_windows_integration,
 )
 
 from .commands.shell import (
