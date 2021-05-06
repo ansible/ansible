779e659dd83fbdd1bf9f5b2fc22157cdc8543989
Brian Coca <brian.coca+git@gmail.com>
2021-05-06 18:03:26 -0400

    overboard

--- lib/ansible/plugins/action/async_status.py
+++ lib/ansible/plugins/action/async_status.py
@@ -4,18 +4,42 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import json
+import os
+import tempfile
+
+from ansible import constants as C
 from ansible.errors import AnsibleActionFail
+from ansible.module_utils._text import to_native
+from ansible.module_utils.six import iteritems
 from ansible.plugins.action import ActionBase
-from ansible.utils.vars import merge_hash
 
 
 class ActionModule(ActionBase):
 
     _VALID_ARGS = frozenset(('jid', 'mode'))
 
+    def _get_async_dir(self):
+
+        # async directory based on the shell option
+        async_dir = self.get_shell_option('async_dir', default="~/.ansible_async")
+
+        # for backwards compatibility we need to get the dir from
+        # ANSIBLE_ASYNC_DIR that is defined in the environment. This is
+        # deprecated and will be removed in favour of shell options
+        env_async_dir = [e for e in self._task.environment if "ANSIBLE_ASYNC_DIR" in e]
+        if len(env_async_dir) > 0:
+            async_dir = env_async_dir[0]['ANSIBLE_ASYNC_DIR']
+            msg = "Setting the async dir from the environment keyword " \
+                  "ANSIBLE_ASYNC_DIR is deprecated. Set the async_dir " \
+                  "shell option instead"
+            self._display.deprecated(msg, "2.12", collection_name='ansible.builtin')
+
+        return self._remote_expand_user(async_dir)
+
     def run(self, tmp=None, task_vars=None):
+
         results = super(ActionModule, self).run(tmp, task_vars)
-        del tmp  # tmp no longer has any effect
 
         if "jid" not in self._task.args:
             raise AnsibleActionFail("jid is required")
@@ -23,23 +47,40 @@ class ActionModule(ActionBase):
         jid = self._task.args["jid"]
         mode = self._task.args.get("mode", "status")
 
-        env_async_dir = [e for e in self._task.environment if "ANSIBLE_ASYNC_DIR" in e]
-        if len(env_async_dir) > 0:
-            # for backwards compatibility we need to get the dir from
-            # ANSIBLE_ASYNC_DIR that is defined in the environment. This is
-            # deprecated and will be removed in favour of shell options
-            async_dir = env_async_dir[0]['ANSIBLE_ASYNC_DIR']
+        async_dir = self._get_async_dir()
+        log_path = os.path.join(async_dir, jid)
 
-            msg = "Setting the async dir from the environment keyword " \
-                  "ANSIBLE_ASYNC_DIR is deprecated. Set the async_dir " \
-                  "shell option instead"
-            self._display.deprecated(msg, "2.12", collection_name='ansible.builtin')
+        results['ansible_job_id'] = jid
+
+        if mode == 'cleanup':
+            self._remove_tmp_path(log_path)
+            results['erased'] = log_path
         else:
-            # inject the async directory based on the shell option into the
-            # module args
-            async_dir = self.get_shell_option('async_dir', default="~/.ansible_async")
+            results['results_file'] = log_path
+            results['started'] = 1
+            results['finished'] = 0
 
-        module_args = dict(jid=jid, mode=mode, _async_dir=async_dir)
-        # TODO: could just use slurp or connection transfer and have all logic here, making async_status a 'doc only' module.
-        results = merge_hash(results, self._execute_module(module_name='ansible.legacy.async_status', task_vars=task_vars, module_args=module_args))
+            fd, tmpfile = tempfile.mkstemp(prefix='_async_%s' % jid, dir=C.DEFAULT_LOCAL_TMP)
+            self._connection.fetch_file(log_path, tmpfile)
+            try:
+                with open(tmpfile) as f:
+                    file_data = f.read()
+                    data = json.loads(file_data)
+
+                if 'started' not in data:
+                    data['finished'] = 1
+                    data['ansible_job_id'] = jid
+                elif 'finished' not in data:
+                    data['finished'] = 0
+
+                results.update(dict([(to_native(k), v) for k, v in iteritems(data)]))
+
+            except Exception:
+                if file_data:
+                    results['finished'] = 1
+                    results['failed'] = True
+                    results['msg'] = "Could not parse job output: %s" % to_native(file_data, errors='surrogate_or_strict')
+            finally:
+                self._remove_tmp_path(tmpfile)
+
         return results
