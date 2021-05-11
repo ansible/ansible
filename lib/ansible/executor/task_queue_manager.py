00094d3392fb7ca6242b25f2a64f786d5dad0d35
Sloane Hertel <19572925+s-hertel@users.noreply.github.com>
2021-05-11 11:12:34 -0400

    Exclude unreachable hosts in ansible_play_batch between plays (#74625)
    ci_complete

--- lib/ansible/executor/task_queue_manager.py
+++ lib/ansible/executor/task_queue_manager.py
@@ -301,6 +301,8 @@ class TaskQueueManager:
         for host_name in self._failed_hosts.keys():
             host = self._inventory.get_host(host_name)
             iterator.mark_host_failed(host)
+        for host_name in self._unreachable_hosts.keys():
+            iterator._play._removed_hosts.append(host_name)
 
         self.clear_failed_hosts()
 
