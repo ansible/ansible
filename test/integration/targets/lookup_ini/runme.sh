787d6503baa2875243d9d3989156c9f8b893766c
Sam Doran <sdoran@redhat.com>
2021-05-12 16:57:02 -0400

    [stable-2.11] ini lookup - catch and handle duplicate key and missing section errors (#74629)
    * Remove unused import
    * Add integration tests for errors
    * Cleanup formatting on existing tests
    * Add changelog.
    (cherry picked from commit 0affe4d027ef4ca5517c06da44dcd1b5b8e2544c)

--- test/integration/targets/lookup_ini/runme.sh
+++ test/integration/targets/lookup_ini/runme.sh
@@ -2,4 +2,5 @@
 
 set -eux
 
-ansible-playbook test_lookup_properties.yml -i ../../inventory -v "$@"
+ansible-playbook test_lookup_properties.yml -i inventory -v "$@"
+ansible-playbook test_errors.yml -i inventory -v "$@"
