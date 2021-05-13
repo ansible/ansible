72866762e9cad9994dcde2ffe26cc0d3a19d6178
David Shrewsbury <dshrewsb@redhat.com>
2021-05-13 10:22:44 -0400

    no e

--- lib/ansible/modules/replace.py
+++ lib/ansible/modules/replace.py
@@ -246,7 +246,7 @@ def main():
         try:
             with open(path, 'rb') as f:
                 contents = to_text(f.read(), errors='surrogate_or_strict', encoding=encoding)
-        except Exception as e:
+        except Exception:
             module.fail_json(msg='Unable to read the contents of %s' % path, exception=format_exc())
 
     pattern = u''
