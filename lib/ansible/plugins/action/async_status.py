7dc750dc18e19b54b304c07799e92fd5b0c490a3
Brian Coca <brian.coca+git@gmail.com>
2021-05-14 10:17:26 -0400

    fix windows path, restored to 'old way'

--- lib/ansible/plugins/action/async_status.py
+++ lib/ansible/plugins/action/async_status.py
@@ -5,7 +5,6 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 import json
-import os
 import tempfile
 
 from ansible.constants import config
@@ -13,6 +12,7 @@ from ansible.errors import AnsibleError, AnsibleActionFail, AnsibleConnectionFai
 from ansible.module_utils._text import to_native
 from ansible.module_utils.six import iteritems
 from ansible.plugins.action import ActionBase
+from ansible.utils.vars import merge_hash
 
 
 class ActionModule(ActionBase):
@@ -37,12 +37,48 @@ class ActionModule(ActionBase):
 
         return self._remote_expand_user(async_dir)
 
+    def _read_remote_file(self, jid, log_path, results):
+
+        # local tempfile to copy job file to, using local tmp which is auto cleaned on exit
+        fd, tmpfile = tempfile.mkstemp(prefix='_async_%s' % jid, dir=config.get_config_value('DEFAULT_LOCAL_TMP'))
+
+        try:
+            self._connection.fetch_file(log_path, tmpfile)
+        except AnsibleConnectionFailure:
+            raise
+        except AnsibleFileNotFound as e:
+            raise AnsibleActionFail("could not find job", orig_exc=e)
+        except AnsibleError as e:
+            raise AnsibleActionFail("failed to fetch the job file: %s" % to_native(e), orig_exc=e)
+
+        try:
+            with open(tmpfile) as f:
+                file_data = f.read()
+                data = json.loads(file_data)
+
+            if 'started' not in data:
+                data['finished'] = 1
+                data['ansible_job_id'] = jid
+            elif 'finished' not in data:
+                data['finished'] = 0
+
+            results.update(dict([(to_native(k), v) for k, v in iteritems(data)]))
+
+        except Exception:
+            if file_data:
+                results['finished'] = 1
+                results['failed'] = True
+                results['msg'] = "Could not parse job output: %s" % to_native(file_data, errors='surrogate_or_strict')
+
     def run(self, tmp=None, task_vars=None):
 
         results = super(ActionModule, self).run(tmp, task_vars)
+
+        # always needed
         results['stdout'] = results['stderr'] = ''
         results['stdout_lines'] = results['stderr_lines'] = []
 
+        # read params
         try:
             jid = self._task.args["jid"]
         except KeyError:
@@ -50,56 +86,24 @@ class ActionModule(ActionBase):
 
         mode = self._task.args.get("mode", "status")
 
+        results['ansible_job_id'] = jid
         async_dir = self._get_async_dir()
-
         log_path = self._connection._shell.join_path(async_dir, jid)
 
-        cleanup = False
-
-        results['ansible_job_id'] = jid
-
-        if mode == 'cleanup':
-            cleanup = True
-        else:
+        if mode != 'cleanup':
             results['results_file'] = log_path
             results['started'] = 1
             results['finished'] = 0
 
+            if getattr(self._connection._shell, '_IS_WINDOWS', False):
+                # TODO: eventually fix so we can get remote user (%USERPROFILE%) like we get ~/ for posix
+                module_args = dict(jid=jid, mode=mode, _async_dir=async_dir)
+                results = merge_hash(results, self._execute_module(module_name='ansible.legacy.async_status', task_vars=task_vars, module_args=module_args))
+            else:
+                # fetch remote file and read locally
+                self._read_remote_file(jid, log_path, results)
 
-            # local tempfile to copy job file to
-            fd, tmpfile = tempfile.mkstemp(prefix='_async_%s' % jid, dir=config.get_config_value('DEFAULT_LOCAL_TMP'))
-
-            try:
-                self._connection.fetch_file(log_path, tmpfile)
-            except AnsibleConnectionFailure:
-                raise
-            except AnsibleFileNotFound as e:
-                raise AnsibleActionFail("could not find job", orig_exc=e)
-            except AnsibleError as e:
-                raise AnsibleActionFail("failed to fetch the job file: %s" % to_native(e), orig_exc=e)
-
-            try:
-                with open(tmpfile) as f:
-                    file_data = f.read()
-                    data = json.loads(file_data)
-
-                if 'started' not in data:
-                    data['finished'] = 1
-                    data['ansible_job_id'] = jid
-                    cleanup = True
-                elif 'finished' not in data:
-                    data['finished'] = 0
-
-                results.update(dict([(to_native(k), v) for k, v in iteritems(data)]))
-
-            except Exception:
-                if file_data:
-                    cleanup = True
-                    results['finished'] = 1
-                    results['failed'] = True
-                    results['msg'] = "Could not parse job output: %s" % to_native(file_data, errors='surrogate_or_strict')
-
-        if cleanup:
+        if mode == 'cleanup' or results['finished'] == 1:
             self._remove_tmp_path(log_path, force=True)
             results['erased'] = log_path
 
