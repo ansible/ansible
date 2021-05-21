d19d8ec28664be6998c11c322ab686846b5b58fc
s-hertel <19572925+s-hertel@users.noreply.github.com>
2021-05-21 18:15:17 -0400

    Add test coverage to make sure the correct module is found when gather_facts is smart

--- test/units/plugins/action/test_gather_facts.py
+++ test/units/plugins/action/test_gather_facts.py
@@ -46,6 +46,58 @@ class TestNetworkFacts(unittest.TestCase):
     def tearDown(self):
         pass
 
+    @patch.object(module_common, '_get_collection_metadata', return_value={})
+    def test_network_gather_facts_smart_facts_module(self, mock_collection_metadata):
+        self.fqcn_task_vars = {'ansible_network_os': 'ios'}
+        self.task.action = 'gather_facts'
+        self.task.async_val = False
+        self.task.args = {}
+
+        plugin = ActionModule(self.task, self.connection, self.play_context, loader=None, templar=self.templar, shared_loader_obj=plugin_loader)
+        get_module_args = MagicMock()
+        plugin._get_module_args = get_module_args
+        plugin._execute_module = MagicMock()
+
+        res = plugin.run(task_vars=self.fqcn_task_vars)
+
+        # assert the gather_facts config is 'smart'
+        facts_modules = C.config.get_config_value('FACTS_MODULES', variables=self.fqcn_task_vars)
+        self.assertEqual(facts_modules, ['smart'])
+
+        # assert the correct module was found
+        self.assertEqual(get_module_args.call_count, 1)
+
+        self.assertEqual(
+            get_module_args.call_args.args,
+            ('ansible.legacy.ios_facts', {'ansible_network_os': 'ios'},)
+        )
+
+    @patch.object(module_common, '_get_collection_metadata', return_value={})
+    def test_network_gather_facts_smart_facts_module_fqcn(self, mock_collection_metadata):
+        self.fqcn_task_vars = {'ansible_network_os': 'cisco.ios.ios'}
+        self.task.action = 'gather_facts'
+        self.task.async_val = False
+        self.task.args = {}
+
+        plugin = ActionModule(self.task, self.connection, self.play_context, loader=None, templar=self.templar, shared_loader_obj=plugin_loader)
+        get_module_args = MagicMock()
+        plugin._get_module_args = get_module_args
+        plugin._execute_module = MagicMock()
+
+        res = plugin.run(task_vars=self.fqcn_task_vars)
+
+        # assert the gather_facts config is 'smart'
+        facts_modules = C.config.get_config_value('FACTS_MODULES', variables=self.fqcn_task_vars)
+        self.assertEqual(facts_modules, ['smart'])
+
+        # assert the correct module was found
+        self.assertEqual(get_module_args.call_count, 1)
+
+        self.assertEqual(
+            get_module_args.call_args.args,
+            ('cisco.ios.ios_facts', {'ansible_network_os': 'cisco.ios.ios'},)
+        )
+
     def test_network_gather_facts(self):
         self.task_vars = {'ansible_network_os': 'ios'}
         self.task.action = 'gather_facts'
@@ -61,10 +113,6 @@ class TestNetworkFacts(unittest.TestCase):
 
         mod_args = plugin._get_module_args('ios_facts', task_vars=self.task_vars)
         self.assertEqual(mod_args['gather_subset'], 'min')
-
-        # gather_facts should not mutate the config list so the default ['smart'] is still the value
-        facts_modules = C.config.get_config_value('FACTS_MODULES', variables=self.task_vars)
-        self.assertEqual(facts_modules, ['smart'])
 
     @patch.object(module_common, '_get_collection_metadata', return_value={})
     def test_network_gather_facts_fqcn(self, mock_collection_metadata):
@@ -82,7 +130,3 @@ class TestNetworkFacts(unittest.TestCase):
 
         mod_args = plugin._get_module_args('cisco.ios.ios_facts', task_vars=self.fqcn_task_vars)
         self.assertEqual(mod_args['gather_subset'], 'min')
-
-        # gather_facts should not mutate the config list so the default ['smart'] is still the value
-        facts_modules = C.config.get_config_value('FACTS_MODULES', variables=self.fqcn_task_vars)
-        self.assertEqual(facts_modules, ['smart'])
