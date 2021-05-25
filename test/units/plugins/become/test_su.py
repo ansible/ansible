49a212c35ab4b3a8db6d173826190fc5a1d9c388
Matt Martz <matt@sivel.net>
2021-05-25 10:03:55 -0500

    Remove deprecated PlayContext.make_become_cmd (#74790)

--- test/units/plugins/become/test_su.py
+++ test/units/plugins/become/test_su.py
@@ -10,31 +10,21 @@ __metaclass__ = type
 import re
 
 from ansible import context
-from ansible.playbook.play_context import PlayContext
-from ansible.plugins.loader import become_loader
+from ansible.plugins.loader import become_loader, shell_loader
 
 
 def test_su(mocker, parser, reset_cli_args):
     options = parser.parse_args([])
     context._init_global_context(options)
-    play_context = PlayContext()
 
-    default_cmd = "/bin/foo"
-    default_exe = "/bin/bash"
-    su_exe = 'su'
-    su_flags = ''
+    su = become_loader.get('su')
+    sh = shell_loader.get('sh')
+    sh.executable = "/bin/bash"
 
-    cmd = play_context.make_become_cmd(cmd=default_cmd, executable=default_exe)
-    assert cmd == default_cmd
+    su.set_options(direct={
+        'become_user': 'foo',
+        'become_flags': '',
+    })
 
-    success = 'BECOME-SUCCESS-.+?'
-
-    play_context.become = True
-    play_context.become_user = 'foo'
-    play_context.become_pass = None
-    play_context.become_method = 'su'
-    play_context.set_become_plugin(become_loader.get('su'))
-    play_context.become_flags = su_flags
-    cmd = play_context.make_become_cmd(cmd=default_cmd, executable=default_exe)
-    assert (re.match("""%s  %s -c '%s -c '"'"'echo %s; %s'"'"''""" % (su_exe, play_context.become_user, default_exe,
-                                                                      success, default_cmd), cmd) is not None)
+    cmd = su.build_become_command('/bin/foo', sh)
+    assert re.match(r"""su\s+foo -c '/bin/bash -c '"'"'echo BECOME-SUCCESS-.+?; /bin/foo'"'"''""", cmd)
--- test/units/plugins/become/test_sudo.py
+++ test/units/plugins/become/test_sudo.py
@@ -10,36 +10,31 @@ __metaclass__ = type
 import re
 
 from ansible import context
-from ansible.playbook.play_context import PlayContext
-from ansible.plugins.loader import become_loader
+from ansible.plugins.loader import become_loader, shell_loader
 
 
 def test_sudo(mocker, parser, reset_cli_args):
     options = parser.parse_args([])
     context._init_global_context(options)
-    play_context = PlayContext()
 
-    default_cmd = "/bin/foo"
-    default_exe = "/bin/bash"
-    sudo_exe = 'sudo'
-    sudo_flags = '-H -s -n'
+    sudo = become_loader.get('sudo')
+    sh = shell_loader.get('sh')
+    sh.executable = "/bin/bash"
 
-    cmd = play_context.make_become_cmd(cmd=default_cmd, executable=default_exe)
-    assert cmd == default_cmd
+    sudo.set_options(direct={
+        'become_user': 'foo',
+        'become_flags': '-n -s -H',
+    })
 
-    success = 'BECOME-SUCCESS-.+?'
+    cmd = sudo.build_become_command('/bin/foo', sh)
 
-    play_context.become = True
-    play_context.become_user = 'foo'
-    play_context.set_become_plugin(become_loader.get('sudo'))
-    play_context.become_flags = sudo_flags
-    cmd = play_context.make_become_cmd(cmd=default_cmd, executable=default_exe)
+    assert re.match(r"""sudo\s+-n -s -H\s+-u foo /bin/bash -c 'echo BECOME-SUCCESS-.+? ; /bin/foo'""", cmd), cmd
 
-    assert (re.match("""%s %s  -u %s %s -c 'echo %s; %s'""" % (sudo_exe, sudo_flags, play_context.become_user,
-                                                               default_exe, success, default_cmd), cmd) is not None)
+    sudo.set_options(direct={
+        'become_user': 'foo',
+        'become_flags': '-n -s -H',
+        'become_pass': 'testpass',
+    })
 
-    play_context.become_pass = 'testpass'
-    cmd = play_context.make_become_cmd(cmd=default_cmd, executable=default_exe)
-    assert (re.match("""%s %s -p "%s" -u %s %s -c 'echo %s; %s'""" % (sudo_exe, sudo_flags.replace('-n', ''),
-                                                                      r"\[sudo via ansible, key=.+?\] password:", play_context.become_user,
-                                                                      default_exe, success, default_cmd), cmd) is not None)
+    cmd = sudo.build_become_command('/bin/foo', sh)
+    assert re.match(r"""sudo\s+-s\s-H\s+-p "\[sudo via ansible, key=.+?\] password:" -u foo /bin/bash -c 'echo BECOME-SUCCESS-.+? ; /bin/foo'""", cmd), cmd
