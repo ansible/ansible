d068396c96e76471747d0299d560dfca6d45a8cf
Ruediger Pluem <ruediger.pluem@vodafone.com>
2021-05-26 15:23:33 +0200

    Fix indent next round

--- lib/ansible/modules/user.py
+++ lib/ansible/modules/user.py
@@ -1334,15 +1334,15 @@ class User(object):
 
     @property
     def default_useradd(self):
-         if self._default_useradd:
-             return self._default_useradd
-         if os.path.exists(self.DEFAULT_USERADD_FILE):
-             with open(self.DEFAULT_USERADD_FILE, 'r') as f:
-                 for line in f:
-                     m = re.match(r'^([A-Z_]+)\s*=\s*(\w+)$', line)
-                     if m:
-                         self._default_useradd[m.group(1)] = m.group(2)
-         return self._default_useradd
+        if self._default_useradd:
+            return self._default_useradd
+        if os.path.exists(self.DEFAULT_USERADD_FILE):
+            with open(self.DEFAULT_USERADD_FILE, 'r') as f:
+                for line in f:
+                    m = re.match(r'^([A-Z_]+)\s*=\s*(\w+)$', line)
+                    if m:
+                        self._default_useradd[m.group(1)] = m.group(2)
+        return self._default_useradd
 
 
 # ===========================================
