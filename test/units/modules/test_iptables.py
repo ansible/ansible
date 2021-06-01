007f1925423d228138fc3ade4cd69c0a3d2ea319
Abhijeet Kasurde <akasurde@redhat.com>
2021-06-01 20:56:38 +0530

    Remove commented code (#74860)
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- test/units/modules/test_iptables.py
+++ test/units/modules/test_iptables.py
@@ -87,8 +87,6 @@ class TestIptables(ModuleTestCase):
                 self.assertTrue(result.exception.args[0]['changed'])
 
         self.assertEqual(run_command.call_count, 2)
-        # import pdb
-        # pdb.set_trace()
         self.assertEqual(run_command.call_args_list[0][0][0], [
             '/sbin/iptables',
             '-t',
@@ -123,8 +121,6 @@ class TestIptables(ModuleTestCase):
                 self.assertFalse(result.exception.args[0]['changed'])
 
         self.assertEqual(run_command.call_count, 1)
-        # import pdb
-        # pdb.set_trace()
         self.assertEqual(run_command.call_args_list[0][0][0], [
             '/sbin/iptables',
             '-t',
@@ -151,8 +147,6 @@ class TestIptables(ModuleTestCase):
                 self.assertTrue(result.exception.args[0]['changed'])
 
         self.assertEqual(run_command.call_count, 1)
-        # import pdb
-        # pdb.set_trace()
         self.assertEqual(run_command.call_args_list[0][0][0], [
             '/sbin/iptables',
             '-t',
@@ -188,8 +182,6 @@ class TestIptables(ModuleTestCase):
                 self.assertTrue(result.exception.args[0]['changed'])
 
         self.assertEqual(run_command.call_count, 1)
-        # import pdb
-        # pdb.set_trace()
         self.assertEqual(run_command.call_args_list[0][0][0], [
             '/sbin/iptables',
             '-t',
@@ -226,8 +218,6 @@ class TestIptables(ModuleTestCase):
                 self.assertTrue(result.exception.args[0]['changed'])
 
         self.assertEqual(run_command.call_count, 2)
-        # import pdb
-        # pdb.set_trace()
         self.assertEqual(run_command.call_args_list[0][0][0], [
             '/sbin/iptables',
             '-t',
