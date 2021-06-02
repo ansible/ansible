ansible/ansible

commit be9f8c69d1e968c8342449d7e0ab5504a3f8a5c2

unarchive: Fail when zipinfo is not available (#74632)
Fixes: #39029

Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

lib/ansible/modules/unarchive.py
@@ -238,6 +238,7 @@ from zipfile import ZipFile, BadZipfile
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.urls import fetch_file
 from ansible.module_utils._text import to_bytes, to_native, to_text
+from ansible.module_utils.common.process import get_bin_path
 
 try:  # python 3.3+
     from shlex import quote
@@ -289,8 +290,14 @@ class ZipArchive(object):
         self.excludes = module.params['exclude']
         self.includes = []
         self.include_files = self.module.params['include']
-        self.cmd_path = self.module.get_bin_path('unzip')
-        self.zipinfocmd_path = self.module.get_bin_path('zipinfo')
+        try:
+            self.cmd_path = get_bin_path('unzip')
+        except ValueError:
+            self.module.fail_json(msg="Unable to find required 'unzip' binary in the path")
+        try:
+            self.zipinfocmd_path = get_bin_path('zipinfo')
+        except ValueError:
+            self.module.fail_json(msg="Unable to find required 'zipinfo' binary in the path")
         self._files_in_archive = []
         self._infodict = dict()
 
@@ -308,11 +315,7 @@ class ZipArchive(object):
         return (mode & ~umask)
 
     def _legacy_file_list(self):
-        unzip_bin = self.module.get_bin_path('unzip')
-        if not unzip_bin:
-            raise UnarchiveError('Python Zipfile cannot read %s and unzip not found' % self.src)
-
-        rc, out, err = self.module.run_command([unzip_bin, '-v', self.src])
+        rc, out, err = self.module.run_command([self.cmd_path, '-v', self.src])
         if rc:
             raise UnarchiveError('Neither python zipfile nor unzip can read %s' % self.src)
 
@@ -385,6 +388,7 @@ class ZipArchive(object):
     def is_unarchived(self):
         # BSD unzip doesn't support zipinfo listings with timestamp.
         cmd = [self.zipinfocmd_path, '-T', '-s', self.src]
+
         if self.excludes:
             cmd.extend(['-x', ] + self.excludes)
         if self.include_files:
@@ -726,10 +730,14 @@ class TgzArchive(object):
         self.excludes = [path.rstrip('/') for path in self.module.params['exclude']]
         self.include_files = self.module.params['include']
         # Prefer gtar (GNU tar) as it supports the compression options -z, -j and -J
-        self.cmd_path = self.module.get_bin_path('gtar', None)
-        if not self.cmd_path:
+        try:
+            self.cmd_path = get_bin_path('gtar')
+        except ValueError:
             # Fallback to tar
-            self.cmd_path = self.module.get_bin_path('tar')
+            try:
+                self.cmd_path = get_bin_path('tar')
+            except ValueError:
+                self.module.fail_json(msg="Unable to find required 'gtar' or 'tar' binary in the path")
         self.zipflag = '-z'
         self._files_in_archive = []
 
@@ -865,9 +873,6 @@ class TgzArchive(object):
         return dict(cmd=cmd, rc=rc, out=out, err=err)
 
     def can_handle_archive(self):
-        if not self.cmd_path:
-            return False, 'Commands "gtar" and "tar" not found.'
-
         if self.tar_type != 'gnu':
             return False, 'Command "%s" detected as tar type %s. GNU tar required.' % (self.cmd_path, self.tar_type)
 

test/units/modules/test_unarchive.py
@@ -1,0 +1,76 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
+import pytest
+
+from ansible.modules.unarchive import ZipArchive, TgzArchive
+
+
+class AnsibleModuleExit(Exception):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
+class ExitJson(AnsibleModuleExit):
+    pass
+
+
+class FailJson(AnsibleModuleExit):
+    pass
+
+
+@pytest.fixture
+def fake_ansible_module():
+    return FakeAnsibleModule()
+
+
+class FakeAnsibleModule:
+    def __init__(self):
+        self.params = {}
+        self.tmpdir = None
+
+    def exit_json(self, *args, **kwargs):
+        raise ExitJson(*args, **kwargs)
+
+    def fail_json(self, *args, **kwargs):
+        raise FailJson(*args, **kwargs)
+
+
+class TestCaseZipArchive:
+    def test_no_zip_binary(self, mocker, fake_ansible_module):
+        mocker.patch("ansible.modules.unarchive.get_bin_path", side_effect=ValueError)
+        fake_ansible_module.params = {
+            "extra_opts": "",
+            "exclude": "",
+            "include": "",
+        }
+        with pytest.raises(FailJson) as exec_info:
+            z = ZipArchive(
+                src="",
+                b_dest="",
+                file_args="",
+                module=fake_ansible_module,
+            )
+        assert "Unable to find required" in exec_info.value.kwargs["msg"]
+
+
+class TestCaseTgzArchive:
+    def test_no_tar_binary(self, mocker, fake_ansible_module):
+        mocker.patch("ansible.modules.unarchive.get_bin_path", side_effect=ValueError)
+        fake_ansible_module.params = {
+            "extra_opts": "",
+            "exclude": "",
+            "include": "",
+        }
+        fake_ansible_module.check_mode = False
+        with pytest.raises(FailJson) as exec_info:
+            z = TgzArchive(
+                src="",
+                b_dest="",
+                file_args="",
+                module=fake_ansible_module,
+            )
+        assert "Unable to find required" in exec_info.value.kwargs["msg"]

commit 9ada0261d03ac2398ab1d3547e78a6ffbd3bdd0d

unarchive: handle exception
Do not use ``fail_json`` API if ``get_bin_path`` does not find
required binaries.

Resolves regression introduced by #74632

Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

lib/ansible/modules/unarchive.py
@@ -238,7 +238,6 @@ from zipfile import ZipFile, BadZipfile
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.urls import fetch_file
 from ansible.module_utils._text import to_bytes, to_native, to_text
-from ansible.module_utils.common.process import get_bin_path
 
 try:  # python 3.3+
     from shlex import quote
@@ -290,14 +289,8 @@ class ZipArchive(object):
         self.excludes = module.params['exclude']
         self.includes = []
         self.include_files = self.module.params['include']
-        try:
-            self.cmd_path = get_bin_path('unzip')
-        except ValueError:
-            self.module.fail_json(msg="Unable to find required 'unzip' binary in the path")
-        try:
-            self.zipinfocmd_path = get_bin_path('zipinfo')
-        except ValueError:
-            self.module.fail_json(msg="Unable to find required 'zipinfo' binary in the path")
+        self.cmd_path = self.module.get_bin_path('unzip')
+        self.zipinfocmd_path = self.module.get_bin_path('zipinfo')
         self._files_in_archive = []
         self._infodict = dict()
 
@@ -709,7 +702,9 @@ class ZipArchive(object):
 
     def can_handle_archive(self):
         if not self.cmd_path:
-            return False, 'Command "unzip" not found.'
+            return False, 'Command "unzip" not found in the path.'
+        if not self.zipinfocmd_path:
+            return False, 'Command "zipinfo" not found in the path.'
         cmd = [self.cmd_path, '-l', self.src]
         rc, out, err = self.module.run_command(cmd)
         if rc == 0:
@@ -730,14 +725,9 @@ class TgzArchive(object):
         self.excludes = [path.rstrip('/') for path in self.module.params['exclude']]
         self.include_files = self.module.params['include']
         # Prefer gtar (GNU tar) as it supports the compression options -z, -j and -J
-        try:
-            self.cmd_path = get_bin_path('gtar')
-        except ValueError:
-            # Fallback to tar
-            try:
-                self.cmd_path = get_bin_path('tar')
-            except ValueError:
-                self.module.fail_json(msg="Unable to find required 'gtar' or 'tar' binary in the path")
+        self.cmd_path = self.module.get_bin_path('gtar')
+        if not self.cmd_path:
+            self.cmd_path = self.module.get_bin_path('tar')
         self.zipflag = '-z'
         self._files_in_archive = []
 
@@ -873,6 +863,9 @@ class TgzArchive(object):
         return dict(cmd=cmd, rc=rc, out=out, err=err)
 
     def can_handle_archive(self):
+        if not self.cmd_path:
+            return False, 'Command "gtar" or "tar" not found in the path.'
+
         if self.tar_type != 'gnu':
             return False, 'Command "%s" detected as tar type %s. GNU tar required.' % (self.cmd_path, self.tar_type)
 

test/units/modules/test_unarchive.py
@@ -3,74 +3,47 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 
-import pytest
-
 from ansible.modules.unarchive import ZipArchive, TgzArchive
 
 
-class AnsibleModuleExit(Exception):
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-
-
-class ExitJson(AnsibleModuleExit):
-    pass
-
-
-class FailJson(AnsibleModuleExit):
-    pass
-
-
-@pytest.fixture
-def fake_ansible_module():
-    return FakeAnsibleModule()
-
-
-class FakeAnsibleModule:
-    def __init__(self):
-        self.params = {}
-        self.tmpdir = None
-
-    def exit_json(self, *args, **kwargs):
-        raise ExitJson(*args, **kwargs)
-
-    def fail_json(self, *args, **kwargs):
-        raise FailJson(*args, **kwargs)
-
-
 class TestCaseZipArchive:
-    def test_no_zip_binary(self, mocker, fake_ansible_module):
-        mocker.patch("ansible.modules.unarchive.get_bin_path", side_effect=ValueError)
+    def test_no_zip_binary(self, mocker):
+        fake_ansible_module = mocker.Mock()
+        fake_ansible_module.get_bin_path = mocker.Mock(return_value=None)
         fake_ansible_module.params = {
             "extra_opts": "",
             "exclude": "",
             "include": "",
         }
-        with pytest.raises(FailJson) as exec_info:
-            z = ZipArchive(
-                src="",
-                b_dest="",
-                file_args="",
-                module=fake_ansible_module,
-            )
-        assert "Unable to find required" in exec_info.value.kwargs["msg"]
+
+        zip_obj = ZipArchive(
+            src="",
+            b_dest="",
+            file_args="",
+            module=fake_ansible_module,
+        )
+        status, msg = zip_obj.can_handle_archive()
+        assert not status
+        assert 'Command "unzip"' in msg
 
 
 class TestCaseTgzArchive:
-    def test_no_tar_binary(self, mocker, fake_ansible_module):
-        mocker.patch("ansible.modules.unarchive.get_bin_path", side_effect=ValueError)
+    def test_no_tar_binary(self, mocker):
+        fake_ansible_module = mocker.Mock()
+        fake_ansible_module.get_bin_path = mocker.Mock(return_value=None)
+        fake_ansible_module.check_mode = False
         fake_ansible_module.params = {
             "extra_opts": "",
             "exclude": "",
             "include": "",
         }
-        fake_ansible_module.check_mode = False
-        with pytest.raises(FailJson) as exec_info:
-            z = TgzArchive(
-                src="",
-                b_dest="",
-                file_args="",
-                module=fake_ansible_module,
-            )
-        assert "Unable to find required" in exec_info.value.kwargs["msg"]
+        tgz_obj = TgzArchive(
+            src="",
+            b_dest="",
+            file_args="",
+            module=fake_ansible_module,
+        )
+
+        status, msg = tgz_obj.can_handle_archive()
+        assert not status
+        assert 'Command "gtar"' in msg