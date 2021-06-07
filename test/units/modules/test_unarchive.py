18068e064d11061c2377888da38ab9186f75c2a2
Sam Doran <sdoran@redhat.com>
2021-06-07 12:59:06 -0400

    unarchive - do not fail in init when trying to find required binary (#74892)
    Test for the required binaries in the can_handle_archive() method and fail there. This
    prevents failures for missing binaries unrelated to the archive type.
    
    * Update missing zip binary message to match tar message
    * Update unit tests
    * Add integration tests
    * Define packages based on the system rather than ignoring failures

--- test/units/modules/test_unarchive.py
+++ test/units/modules/test_unarchive.py
@@ -40,21 +40,31 @@ class FakeAnsibleModule:
 
 
 class TestCaseZipArchive:
-    def test_no_zip_binary(self, mocker, fake_ansible_module):
-        mocker.patch("ansible.modules.unarchive.get_bin_path", side_effect=ValueError)
+    @pytest.mark.parametrize(
+        'side_effect, expected_reason', (
+            ([ValueError, '/bin/zipinfo'], "Unable to find required 'unzip'"),
+            (ValueError, "Unable to find required 'unzip' or 'zipinfo'"),
+        )
+    )
+    def test_no_zip_zipinfo_binary(self, mocker, fake_ansible_module, side_effect, expected_reason):
+        mocker.patch("ansible.modules.unarchive.get_bin_path", side_effect=side_effect)
         fake_ansible_module.params = {
             "extra_opts": "",
             "exclude": "",
             "include": "",
         }
-        with pytest.raises(FailJson) as exec_info:
-            z = ZipArchive(
-                src="",
-                b_dest="",
-                file_args="",
-                module=fake_ansible_module,
-            )
-        assert "Unable to find required" in exec_info.value.kwargs["msg"]
+
+        z = ZipArchive(
+            src="",
+            b_dest="",
+            file_args="",
+            module=fake_ansible_module,
+        )
+        can_handle, reason = z.can_handle_archive()
+
+        assert can_handle is False
+        assert expected_reason in reason
+        assert z.cmd_path is None
 
 
 class TestCaseTgzArchive:
@@ -66,11 +76,16 @@ class TestCaseTgzArchive:
             "include": "",
         }
         fake_ansible_module.check_mode = False
-        with pytest.raises(FailJson) as exec_info:
-            z = TgzArchive(
-                src="",
-                b_dest="",
-                file_args="",
-                module=fake_ansible_module,
-            )
-        assert "Unable to find required" in exec_info.value.kwargs["msg"]
+
+        t = TgzArchive(
+            src="",
+            b_dest="",
+            file_args="",
+            module=fake_ansible_module,
+        )
+        can_handle, reason = t.can_handle_archive()
+
+        assert can_handle is False
+        assert 'Unable to find required' in reason
+        assert t.cmd_path is None
+        assert t.tar_type is None
