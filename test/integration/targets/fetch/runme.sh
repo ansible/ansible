ab1add129255e012d4c5b9c0f15462ae6f69b530
Sam Doran <sdoran@redhat.com>
2021-06-09 17:10:23 -0400

    Increase test coverage more and add another test run with became=True

--- test/integration/targets/fetch/runme.sh
+++ test/integration/targets/fetch/runme.sh
@@ -8,5 +8,8 @@ ln -s ../../setup_remote_tmp_dir roles/setup_remote_tmp_dir
 # run old type role tests
 ansible-playbook -i ../../inventory run_fetch_tests.yml -e "output_dir=${OUTPUT_DIR}" -v "$@"
 
+# run same test with become
+ansible-playbook -i ../../inventory run_fetch_tests.yml -e "output_dir=${OUTPUT_DIR}" -b -v "$@"
+
 # run tests to avoid path injection from slurp when fetch uses become
 ansible-playbook -i ../../inventory injection/avoid_slurp_return.yml -e "output_dir=${OUTPUT_DIR}" -v "$@"
