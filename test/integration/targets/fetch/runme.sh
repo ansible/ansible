6e67983fa4f8346e7baf9e5dc349a7e2ef43bd92
Sam Doran <sdoran@redhat.com>
2021-06-14 18:08:31 -0400

    Use fact caching to persist remote tmp dir across playbook runs

--- test/integration/targets/fetch/runme.sh
+++ test/integration/targets/fetch/runme.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 set -eux
-trap 'ansible-playbook -i ../../inventory cleanup.yml -b "$@"' EXIT
+
+function cleanup {
+    ansible-playbook -i hosts.yml cleanup.yml -b "$@"
+    unset ANSIBLE_SSH_EXTRA_ARGS
+    unset ANSIBLE_CACHE_PLUGIN
+    unset ANSIBLE_CACHE_PLUGIN_CONNECTION
+}
+
+trap 'cleanup "$@"' EXIT
 
 # setup required roles
 ln -s ../../setup_remote_tmp_dir roles/setup_remote_tmp_dir
@@ -15,8 +23,14 @@ ansible-playbook -i ../../inventory run_fetch_tests.yml -e "output_dir=${OUTPUT_
 # run tests to avoid path injection from slurp when fetch uses become
 ansible-playbook -i ../../inventory injection/avoid_slurp_return.yml -e "output_dir=${OUTPUT_DIR}" "$@"
 
-# Test unreadable file with stat. Requires running without become and as a user other than root.
-export ANSIBLE_SSH_EXTRA_ARGS="-o UserKnownHostsFile=/dev/null"
+## Test unreadable file with stat. Requires running without become and as a user other than root.
+#
+# Change the known_hosts file to avoid changing the test environment
+export ANSIBLE_SSH_COMMON_ARGS="-o UserKnownHostsFile=/dev/null"
+export ANSIBLE_CACHE_PLUGIN=jsonfile
+export ANSIBLE_CACHE_PLUGIN_CONNECTION="${OUTPUT_DIR}/cache"
+# Create a non-root user account and configure SSH acccess for that account
 ansible-playbook -i hosts.yml setup_unreadable_test.yml -e "output_dir=${OUTPUT_DIR}" "$@"
+
+# Run the tests as the unprivileged user without become to test the use of the stat module from the fetch module
 ansible-playbook --user fetcher -i hosts.yml test_unreadable_with_stat.yml -e "output_dir=${OUTPUT_DIR}" "$@"
-unset ANSIBLE_SSH_EXTRA_ARGS
