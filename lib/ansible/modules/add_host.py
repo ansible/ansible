e6fcfe4b6be66ef3b693620650b443512c331b9d
Brian Coca <bcoca@users.noreply.github.com>
2021-06-14 09:28:55 -0400

    Update lib/ansible/modules/add_host.py
    Co-authored-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/add_host.py
+++ lib/ansible/modules/add_host.py
@@ -39,7 +39,7 @@ notes:
 - Windows targets are supported by this module.
 - Though this module does not change the remote host, we do provide 'changed' status as it can be useful for those trying to track inventory changes.
 - The hosts added will not bypass the ``--limit`` from the command line, so both of those need to be in agreement to make them available as play targets.
-  They are still available via hostvars and for delegation as normal part of the inventory.
+  They are still available via hostvars and for delegation as a normal part of the inventory.
 seealso:
 - module: ansible.builtin.group_by
 author:
