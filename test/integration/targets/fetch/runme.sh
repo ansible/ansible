ea14983b22c0d39f33d4a1e7ed6b18e8dc741dff
Sam Doran <sdoran@redhat.com>
2021-06-15 16:54:39 -0400

    Set known_hosts file earlier with the hope of avoiding changes to the test environment

--- test/integration/targets/fetch/runme.sh
+++ test/integration/targets/fetch/runme.sh
@@ -11,6 +11,8 @@ function cleanup {
 
 trap 'cleanup "$@"' EXIT
 
+export ANSIBLE_SSH_COMMON_ARGS="-o UserKnownHostsFile=/dev/null"
+
 # setup required roles
 ln -s ../../setup_remote_tmp_dir roles/setup_remote_tmp_dir
 
@@ -26,7 +28,6 @@ ansible-playbook -i ../../inventory injection/avoid_slurp_return.yml -e "output_
 ## Test unreadable file with stat. Requires running without become and as a user other than root.
 #
 # Change the known_hosts file to avoid changing the test environment
-export ANSIBLE_SSH_COMMON_ARGS="-o UserKnownHostsFile=/dev/null"
 export ANSIBLE_CACHE_PLUGIN=jsonfile
 export ANSIBLE_CACHE_PLUGIN_CONNECTION="${OUTPUT_DIR}/cache"
 # Create a non-root user account and configure SSH acccess for that account
