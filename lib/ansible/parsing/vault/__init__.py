e417868054e011a51a79205efa0a459c072a7a5b
bartscherer <thomas@bartscherer.net>
2021-06-26 16:20:14 +0200

    None type check with 'is' instead of '=='

--- lib/ansible/parsing/vault/__init__.py
+++ lib/ansible/parsing/vault/__init__.py
@@ -766,7 +766,7 @@ class VaultLib:
             which were read from the vault map file
         '''
 
-        if vault_map_file_path == None:
+        if vault_map_file_path is None:
             return []
 
         vault_map_file_path = os.path.realpath(os.path.expanduser(vault_map_file_path))
