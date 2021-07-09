57507c35cb4857b0a6eec2676f57c89b04df08d3
Martin Krizek <martin.krizek@gmail.com>
2021-07-09 10:34:52 +0200

    Enable jinja2_native before tests
    Co-Authored-By: Matt Martz <matt@sivel.net>

--- test/units/template/test_native_concat.py
+++ test/units/template/test_native_concat.py
@@ -5,42 +5,48 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import importlib
+import sys
+
 import pytest
 
 from ansible import constants as C
-# NOTE DEFAULT_JINJA2_NATIVE must be mocked here first so native
-# modules are imported correctly within the template module.
-# Any imports that would import ansible.template at any level
-# must not be imported before setting DEFAULT_JINJA2_NATIVE.
-C.DEFAULT_JINJA2_NATIVE = True
-from ansible.template import Templar, AnsibleNativeEnvironment
-
 from ansible.errors import AnsibleUndefinedVariable
 from ansible.playbook.conditional import Conditional
 
 from units.mock.loader import DictDataLoader
 
 
-# https://github.com/ansible/ansible/issues/52158
-def test_undefined_variable():
-    fake_loader = DictDataLoader({})
-    variables = {}
-    templar = Templar(loader=fake_loader, variables=variables)
-    assert isinstance(templar.environment, AnsibleNativeEnvironment)
-
-    with pytest.raises(AnsibleUndefinedVariable):
-        templar.template("{{ missing }}")
+@pytest.fixture
+def enable_jinja2_native(monkeypatch):
+    monkeypatch.delitem(sys.modules, 'ansible.template')
+    monkeypatch.setattr(C, 'DEFAULT_JINJA2_NATIVE', True)
+    mod = importlib.import_module('ansible.template')
+    globals()['Templar'] = mod.Templar
+    globals()['AnsibleNativeEnvironment'] = mod.AnsibleNativeEnvironment
 
 
-def test_cond_eval():
-    fake_loader = DictDataLoader({})
-    # True must be stored in a variable to trigger templating. Using True
-    # directly would be caught by optimization for bools to short-circuit
-    # templating.
-    variables = {"foo": True}
-    templar = Templar(loader=fake_loader, variables=variables)
-    assert isinstance(templar.environment, AnsibleNativeEnvironment)
+@pytest.mark.usefixtures("enable_jinja2_native")
+class TestNativeConcat:
+    # https://github.com/ansible/ansible/issues/52158
+    def test_undefined_variable(self):
+        fake_loader = DictDataLoader({})
+        variables = {}
+        templar = Templar(loader=fake_loader, variables=variables)
+        assert isinstance(templar.environment, AnsibleNativeEnvironment)
 
-    cond = Conditional(loader=fake_loader)
-    cond.when = ["foo"]
-    assert cond.evaluate_conditional(templar, variables)
+        with pytest.raises(AnsibleUndefinedVariable):
+            templar.template("{{ missing }}")
+
+    def test_cond_eval(self):
+        fake_loader = DictDataLoader({})
+        # True must be stored in a variable to trigger templating. Using True
+        # directly would be caught by optimization for bools to short-circuit
+        # templating.
+        variables = {"foo": True}
+        templar = Templar(loader=fake_loader, variables=variables)
+        assert isinstance(templar.environment, AnsibleNativeEnvironment)
+
+        cond = Conditional(loader=fake_loader)
+        cond.when = ["foo"]
+        assert cond.evaluate_conditional(templar, variables)
