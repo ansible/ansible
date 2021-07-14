9c0832c56d412cb508d1daeea47d9619d014f931
Sam Doran <sdoran@redhat.com>
2021-07-14 18:01:10 -0400

    Update docstring

--- lib/ansible/module_utils/splitter.py
+++ lib/ansible/module_utils/splitter.py
@@ -251,7 +251,8 @@ def split_multiext(name, min=3, max=4, count=2):
     Returns '(root, extension)'.
 
     Define the valid extension length (including the '.') with 'min' and 'max',
-    The number of extensions, counting from the end, to evaluate.
+    'count' sets the number of extensions, counting from the end, to evaluate.
+    Evaluation stops on the first file extenios that is outside the min and max range.
 
     :arg name: File name or path
     :kwarg min: Minimum length of a valid file extension.
