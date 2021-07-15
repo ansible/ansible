7e18dfda44dbeb9614cf46234226edc8bfa773c9
Sloane Hertel <19572925+s-hertel@users.noreply.github.com>
2021-07-15 15:32:00 -0400

    Add a test for deduplicating dependencies with include_role (#75259)

--- test/integration/targets/roles/runme.sh
+++ test/integration/targets/roles/runme.sh
@@ -5,9 +5,10 @@ set -eux
 # test no dupes when dependencies in b and c point to a in roles:
 [ "$(ansible-playbook no_dupes.yml -i ../../inventory --tags inroles "$@" | grep -c '"msg": "A"')" = "1" ]
 [ "$(ansible-playbook no_dupes.yml -i ../../inventory --tags acrossroles "$@" | grep -c '"msg": "A"')" = "1" ]
+[ "$(ansible-playbook no_dupes.yml -i ../../inventory --tags intasks "$@" | grep -c '"msg": "A"')" = "1" ]
 
 # but still dupe across plays
-[ "$(ansible-playbook no_dupes.yml -i ../../inventory "$@" | grep -c '"msg": "A"')" = "2" ]
+[ "$(ansible-playbook no_dupes.yml -i ../../inventory "$@" | grep -c '"msg": "A"')" = "3" ]
 
 # include/import can execute another instance of role
 [ "$(ansible-playbook allowed_dupes.yml -i ../../inventory --tags importrole "$@" | grep -c '"msg": "A"')" = "2" ]
