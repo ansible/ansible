332348a7e18837eac0c4a121b06b1688ee724461
Felix Fontein <felix@fontein.de>
2021-07-26 15:15:30 +0200

    Make sure AnsibleModule positional arguments are validated.

--- test/lib/ansible_test/_data/sanity/validate-modules/validate_modules/module_args.py
+++ test/lib/ansible_test/_data/sanity/validate-modules/validate_modules/module_args.py
@@ -34,6 +34,12 @@ from ansible.module_utils._text import to_bytes, to_text
 from .utils import CaptureStd, find_executable, get_module_name_from_filename
 
 
+ANSIBLE_MODULE_CONSTURCTOR_ARGS = (
+    'argument_spec', 'bypass_checks', 'no_log', 'mutually_exclusive', 'required_together',
+    'required_one_of', 'add_file_common_args', 'supports_check_mode', 'required_if', 'required_by',
+)
+
+
 class AnsibleModuleCallError(RuntimeError):
     pass
 
@@ -146,11 +152,11 @@ def get_py_argument_spec(filename, collection):
             raise AnsibleModuleNotInitialized()
 
     try:
+        # Convert positional arguments to kwargs to make sure that all parameters are actually checked
+        for arg, arg_name in zip(fake.args, ANSIBLE_MODULE_CONSTURCTOR_ARGS):
+            fake.kwargs[arg_name] = arg
         # for ping kwargs == {'argument_spec':{'data':{'type':'str','default':'pong'}}, 'supports_check_mode':True}
-        if 'argument_spec' in fake.kwargs:
-            argument_spec = fake.kwargs['argument_spec']
-        else:
-            argument_spec = fake.args[0]
+        argument_spec = fake.kwargs['argument_spec']
         # If add_file_common_args is truish, add options from FILE_COMMON_ARGUMENTS when not present.
         # This is the only modification to argument_spec done by AnsibleModule itself, and which is
         # not caught by setup_env's AnsibleModule replacement
@@ -158,7 +164,7 @@ def get_py_argument_spec(filename, collection):
             for k, v in FILE_COMMON_ARGUMENTS.items():
                 if k not in argument_spec:
                     argument_spec[k] = v
-        return argument_spec, fake.args, fake.kwargs
+        return argument_spec, [], fake.kwargs
     except (TypeError, IndexError):
         return {}, (), {}
 
