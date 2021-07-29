0f0ef4a352cf493bc7d24a95f95fe965b696e6d0
Brian Coca <brian.coca+git@gmail.com>
2021-07-29 12:08:25 -0400

    ginore eet

--- test/sanity/code-smell/package-data.py
+++ test/sanity/code-smell/package-data.py
@@ -48,6 +48,7 @@ def assemble_files_to_ship(complete_file_list):
         'hacking/metadata-tool.py',
         'hacking/report.py',
         'hacking/return_skeleton_generator.py',
+        'hacking/task_templating_stages.txt',
         'hacking/test-module',
         'hacking/test-module.py',
         'test/support/README.md',
