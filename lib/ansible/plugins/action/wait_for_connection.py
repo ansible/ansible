136b91446215ce97b9a06b4f747f729c2e8cfa02
Daniel Goldman <daniel.goldman2@mail.mcgill.ca>
2021-08-02 15:49:15 -0400

    fix uses total_seconds method

--- lib/ansible/plugins/action/wait_for_connection.py
+++ lib/ansible/plugins/action/wait_for_connection.py
@@ -49,12 +49,12 @@ class ActionModule(ActionBase):
         # We check that there is at least 1 second remaining,
         # since that's the minimum we can specify to a timeout to ping.
         # If we pass 0 seconds as the timeout to ping, it never times out.
-        while (max_end_time - datetime.utcnow()) > timedelta(seconds=0):
+        while int((max_end_time - datetime.utcnow()).total_seconds()) > 0:
             # The effective timeout ensures that we don't exceed the timeout value
             # as an example without it, if timeout is 10 and connect_timeout is 8,
             # 2 attempts will be made with a timeout of 8s for a total of 16s, > timeout
             # so we use effective timeout to have attempts of 8s and 2s < timeout
-            seconds_remaining = (max_end_time - datetime.now()).seconds
+            seconds_remaining = int((max_end_time - datetime.utcnow()).total_seconds())
             if ping_connect_timeout == 0:
                 # this prevents the edge case of ping never timing out
                 effective_timeout = seconds_remaining
