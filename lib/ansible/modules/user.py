a6dbb50e4020bd326207d2d02170f6ef3c71b05c
Daniel Goldman <daniel.goldman2@mail.mcgill.ca>
2021-08-02 21:19:47 -0400

    [0] return result of execute_command from set_password_expire*

--- lib/ansible/modules/user.py
+++ lib/ansible/modules/user.py
@@ -1031,10 +1031,11 @@ class User(object):
         cmd.append(self.password_expire_max)
         cmd.append(self.name)
         if self.password_expire_max == spwd.getspnam(self.name).sp_max:
-            self.module.exit_json(changed=False)
+            return (0, '', '')
+            # self.module.exit_json(changed=False)
         else:
-            self.execute_command(cmd)
-            self.module.exit_json(changed=True)
+            return self.execute_command(cmd)
+            # self.module.exit_json(changed=True)
 
     def set_password_expire_min(self):
         command_name = 'chage'
@@ -1043,10 +1044,11 @@ class User(object):
         cmd.append(self.password_expire_min)
         cmd.append(self.name)
         if self.password_expire_min == spwd.getspnam(self.name).sp_min:
-            self.module.exit_json(changed=False)
+            return (0, '', '')
+            # self.module.exit_json(changed=False)
         else:
-            self.execute_command(cmd)
-            self.module.exit_json(changed=True)
+            return self.execute_command(cmd)
+            # self.module.exit_json(changed=True)
 
     def user_password(self):
         passwd = ''
