3f51e5fa7ebcccf0821d7a71ad955a06e34cbaa0
Daniel Goldman <daniel.goldman2@mail.mcgill.ca>
2021-08-03 22:49:12 -0400

    remove now redundant condition
    already asserted above

--- lib/ansible/modules/user.py
+++ lib/ansible/modules/user.py
@@ -3207,27 +3207,25 @@ def main():
 
         # deal with password expire max
         if user.password_expire_max is not None:
-            if user.user_exists():
-                (rc, out, err) = user.set_password_expire_max()
-                if rc is None:
-                    pass  # target state reached, nothing to do
+            (rc, out, err) = user.set_password_expire_max()
+            if rc is None:
+                pass  # target state reached, nothing to do
+            else:
+                if rc != 0:
+                    module.fail_json(name=user.name, msg=err, rc=rc)
                 else:
-                    if rc != 0:
-                        module.fail_json(name=user.name, msg=err, rc=rc)
-                    else:
-                        result['changed'] = True
+                    result['changed'] = True
 
         # deal with password expire min
         if user.password_expire_min is not None:
-            if user.user_exists():
-                (rc, out, err) = user.set_password_expire_min()
-                if rc is None:
-                    pass  # target state reached, nothing to do
+            (rc, out, err) = user.set_password_expire_min()
+            if rc is None:
+                pass  # target state reached, nothing to do
+            else:
+                if rc != 0:
+                    module.fail_json(name=user.name, msg=err, rc=rc)
                 else:
-                    if rc != 0:
-                        module.fail_json(name=user.name, msg=err, rc=rc)
-                    else:
-                        result['changed'] = True
+                    result['changed'] = True
 
     module.exit_json(**result)
 
