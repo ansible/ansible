85814f4a58e1c85e6907d298a48174e2ba63d8bf
icamys <prisacari.dmitrii@gmail.com>
2021-08-05 22:53:13 +0300

    added more tests for NFLOG iptables extension

--- test/units/modules/test_iptables.py
+++ test/units/modules/test_iptables.py
@@ -764,6 +764,29 @@ class TestIptables(ModuleTestCase):
                 '--nflog-threshold', '2',
             ])
 
+        set_module_args({
+            'chain': 'INPUT',
+            'nflog_group': '1',
+        })
+        commands_results = [
+            (0, '', ''),
+        ]
+
+        with patch.object(basic.AnsibleModule, 'run_command') as run_command:
+            run_command.side_effect = commands_results
+            with self.assertRaises(AnsibleExitJson) as result:
+                iptables.main()
+                self.assertTrue(result.exception.args[0]['changed'])
+
+            self.assertEqual(run_command.call_count, 1)
+            self.assertEqual(run_command.call_args_list[0][0][0], [
+                '/sbin/iptables',
+                '-t', 'filter',
+                '-C', 'INPUT',
+                '-j', 'NFLOG',
+                '--nflog-group', '1',
+            ])
+
     def test_iprange(self):
         """ Test iprange module with its flags src_range and dst_range """
         set_module_args({
