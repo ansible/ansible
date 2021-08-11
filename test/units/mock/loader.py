3897a67ae726962510481cf26b474d09358f4f6c
Matt Clay <mclay@redhat.com>
2021-08-11 13:08:31 -0700

    Clean up unit tests to prepare for pylint update. (#75473)
    * Fix argument name in mocked function.
    * Use from import in unit tests.
    * Remove unused imports.

--- test/units/mock/loader.py
+++ test/units/mock/loader.py
@@ -47,12 +47,12 @@ class DictDataLoader(DataLoader):
 
     # TODO: the real _get_file_contents returns a bytestring, so we actually convert the
     #       unicode/text it's created with to utf-8
-    def _get_file_contents(self, path):
-        path = to_text(path)
+    def _get_file_contents(self, file_name):
+        path = to_text(file_name)
         if path in self._file_mapping:
-            return (to_bytes(self._file_mapping[path]), False)
+            return to_bytes(self._file_mapping[file_name]), False
         else:
-            raise AnsibleParserError("file not found: %s" % path)
+            raise AnsibleParserError("file not found: %s" % file_name)
 
     def path_exists(self, path):
         path = to_text(path)
