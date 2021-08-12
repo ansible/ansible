0b735f9c462562c8139e5aaa5dbc79213bc8a9ea
Brian Coca <brian.coca+git@gmail.com>
2021-08-12 16:22:00 -0400

    threadit

--- lib/ansible/module_utils/facts/system/platform.py
+++ lib/ansible/module_utils/facts/system/platform.py
@@ -16,13 +16,18 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import platform
 import re
 import socket
-import platform
+import time
 
-from ansible.module_utils.facts.utils import get_file_content
+from multiprocessing import cpu_count
+from multiprocessing.pool import ThreadPool
 
+from ansible.module_utils._text import to_text
 from ansible.module_utils.facts.collector import BaseFactCollector
+from ansible.module_utils.facts.utils import get_file_content
+from ansible.module_utils.facts import timeout
 
 # i86pc is a Solaris and derivatives-ism
 SOLARIS_I86_RE_PATTERN = r'i([3456]86|86pc)'
@@ -41,17 +46,59 @@ class PlatformFactCollector(BaseFactCollector):
 
     def _get_hostname_cli_info(self, module):
         r = {}
-        HOSTNAMES = {'hostname': '', 'short': '-s', 'dnsdomainname': '-f', 'domainname': '-d', 'nisdomainname': '--nis', 'nodename': '-n', 'ypdomainname': '-y', 'ip': '-i'}
+
+        HOSTNAMES = {'hostname': None, 'alias': '-a', 'short': '-s', 'dnsdomainname': '-f', 'domainname': '-d', 'nisdomainname': '--nis', 'nodename': '-n', 'ypdomainname': '-y', 'ip': '-i'}
         hostname_bin = module.get_bin_path('hostname')
         if hostname_bin:
             r['info'] = hostname_bin
-            for name in HOSTNAMES.keys():
+
+            def _get_hostname(module, name):
                 rc, out, err = module.run_command([hostname_bin, HOSTNAMES[name]])
-                if rc !=0 or not out:
-                    r[name] = ''
+                if rc != 0 or not out:
+                    result = ''
                 else:
                     data = out.splitlines()
-                    r[name]  = data[0]
+                    result = data[0]
+                return result
+
+            results = {}
+            pool = ThreadPool(processes=min(len(HOSTNAMES), cpu_count()))
+            maxtime = globals().get('GATHER_TIMEOUT') or timeout.DEFAULT_GATHER_TIMEOUT
+            for name in HOSTNAMES.keys():
+                results[name] = {'r': pool.apply_async(_get_hostname(module, name)), 'limit': maxtime}
+
+            pool.close()  # done with new workers, start gc
+
+            while results:  # now wait for results or timeout
+                for name in list(results):
+                    done = False
+                    res = results[name]
+                    try:
+                        if res['r'].ready():
+                            done = True
+                            if res['r'].successful():
+                                r[name] = res['r'].get()
+                            else:
+                                # failed, try to find out why, if 'res.successful' we know there are no exceptions
+                                r[name] = 'fail, no reason given'
+
+                        elif time.time() > res['limit']:
+                            r[name] = 'Timeout exceeded %d' %  res['limit']
+                            done = True
+                            module.warn("Timeout exceeded when getting info for %s" % name)
+                    except Exception as e:
+                        import traceback
+                        done = True
+                        r[name] = 'it broke: %s' % to_text(e)
+                        module.warn("Error prevented getting extra info for 'hostname_cli[%s]': [%s] %s." % (name, type(e), to_text(e)))
+                        module.debug(traceback.format_exc())
+
+                    if done:
+                        # move results outside and make loop only handle pending
+                        del results[name]
+
+                # avoid cpu churn, sleep between retrying for loop with remaining mounts
+                time.sleep(0.1)
         else:
             r['info'] = 'No hostname CLI found'
             for name in HOSTNAMES.keys():
@@ -78,7 +125,9 @@ class PlatformFactCollector(BaseFactCollector):
         try:
             platform_facts['hostname_cli'] = self._get_hostname_cli_info(module)
         except (OSError, IOError) as e:
-            platform_facts['hostname_cli'] = {'info': str(e)}
+            platform_facts['hostname_cli'] = {'info': to_text(e)}
+        except Exception as e:
+            platform_facts['hostname_cli'] = {'info': to_text(e)}
 
         arch_bits = platform.architecture()[0]
 
@@ -111,6 +160,7 @@ class PlatformFactCollector(BaseFactCollector):
                 rc, out, err = module.run_command([bootinfo_bin, '-p'])
                 data = out.splitlines()
                 platform_facts['architecture'] = data[0]
+
         elif platform_facts['system'] == 'OpenBSD':
             platform_facts['architecture'] = platform.uname()[5]
 
