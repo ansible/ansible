3f69f75d89150493982f3ae1e35bd9d5a76b367b
Brian Coca <brian.coca+git@gmail.com>
2021-08-19 11:50:40 -0400

    add play path

--- lib/ansible/vars/manager.py
+++ lib/ansible/vars/manager.py
@@ -477,6 +477,8 @@ class VariableManager:
             variables['role_names'] = variables['ansible_play_role_names']
 
             variables['ansible_play_name'] = play.get_name()
+            # get path returns path:line
+            variables['ansible_play_path'] = play.get_path.split(':')[0]
 
         if task:
             if task._role:
