463cf9fe244430249701509fc3ee7636a18afc44
Brian Coca <bcoca@users.noreply.github.com>
2021-09-01 12:38:19 -0400

    Attributes compat (#75563)
    * ignore attributes in output
    
    * ignore 'attributes' for json dump
    
    * actually add file
    
    * clog
    
    * sometimtes its not a dict
    
    * always ignore
    
    * also allow validation to work on newer modules
    
    * remove attributes doc
    
      let existing overrides display, wont be full info but still pertinent info
      though user will have to check newer versions

--- lib/ansible/cli/doc.py
+++ lib/ansible/cli/doc.py
@@ -334,6 +334,7 @@ class DocCLI(CLI, RoleMixin):
 
     # default ignore list for detailed views
     IGNORE = ('module', 'docuri', 'version_added', 'short_description', 'now_date', 'plainexamples', 'returndocs', 'collection')
+    JSON_IGNORE = ('attributes',)
 
     # Warning: If you add more elements here, you also need to add it to the docsite build (in the
     # ansible-community/antsibull repo)
@@ -686,6 +687,12 @@ class DocCLI(CLI, RoleMixin):
                 docs = self._get_plugins_docs(plugin_type, loader)
 
         if do_json:
+            for entry in docs.keys():
+                for forbid in DocCLI.JSON_IGNORE:
+                    try:
+                        del docs[entry]['doc'][forbid]
+                    except (KeyError, TypeError):
+                        pass
             jdump(docs)
         else:
             text = []
