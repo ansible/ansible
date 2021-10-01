b114426435792594a529d72a81fac9cb65f75822
Matt Clay <matt@mystile.com>
2021-09-30 18:08:00 -0700

    [stable-2.12] ansible-test - Fix inventory generation. (cherry picked from commit 099d80829f2293976be5866c07c880c02f1b0865)
    Co-authored-by: Matt Clay <matt@mystile.com>

--- test/lib/ansible_test/_internal/host_profiles.py
+++ test/lib/ansible_test/_internal/host_profiles.py
@@ -463,7 +463,7 @@ class NetworkRemoteProfile(RemoteProfile[NetworkRemoteConfig]):
             ansible_host=connection.hostname,
             ansible_port=connection.port,
             ansible_user=connection.username,
-            ansible_ssh_private_key=core_ci.ssh_key.key,
+            ansible_ssh_private_key_file=core_ci.ssh_key.key,
             ansible_network_os=f'{self.config.collection}.{self.config.platform}' if self.config.collection else self.config.platform,
         )
 
@@ -689,7 +689,7 @@ class WindowsRemoteProfile(RemoteProfile[WindowsRemoteConfig]):
             ansible_port=connection.port,
             ansible_user=connection.username,
             ansible_password=connection.password,
-            ansible_ssh_private_key=core_ci.ssh_key.key,
+            ansible_ssh_private_key_file=core_ci.ssh_key.key,
         )
 
         # HACK: force 2016 to use NTLM + HTTP message encryption
