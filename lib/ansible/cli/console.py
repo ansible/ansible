ae94f78aec441069c4089ede2ff8d3c3a85d294d
Matt Martz <matt@sivel.net>
2021-10-08 11:36:53 -0500

    PYTHON_ARGCOMPLETE_OK

--- lib/ansible/cli/console.py
+++ lib/ansible/cli/console.py
@@ -2,6 +2,7 @@
 # Copyright: (c) 2016, Redhat Inc
 # Copyright: (c) 2018, Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# PYTHON_ARGCOMPLETE_OK
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
--- lib/ansible/cli/playbook.py
+++ lib/ansible/cli/playbook.py
@@ -1,6 +1,7 @@
 # (c) 2012, Michael DeHaan <michael.dehaan@gmail.com>
 # Copyright: (c) 2018, Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# PYTHON_ARGCOMPLETE_OK
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
