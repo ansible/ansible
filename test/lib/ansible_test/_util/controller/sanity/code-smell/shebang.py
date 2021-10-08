7115a4f26d4a4a0f16b98c8872e38333a6331e94
Matt Martz <matt@sivel.net>
2021-10-08 16:03:06 -0500

    Update shebang test to ignore cli entry points

--- test/lib/ansible_test/_util/controller/sanity/code-smell/shebang.py
+++ test/lib/ansible_test/_util/controller/sanity/code-smell/shebang.py
@@ -70,6 +70,8 @@ def main():
                 is_module = True
             elif path == 'test/lib/ansible_test/_util/target/cli/ansible_test_cli_stub.py':
                 pass  # ansible-test entry point must be executable and have a shebang
+            elif re.search('^lib/ansible/cli/[^/]+\.py', path):
+                pass  # cli entry points must be executable and have a shebang
             elif path.startswith('lib/') or path.startswith('test/lib/'):
                 if executable:
                     print('%s:%d:%d: should not be executable' % (path, 0, 0))
