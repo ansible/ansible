3be2ab4e0faf5400db417d1a2a2f4ce63063dbaf
Matt Martz <matt@sivel.net>
2021-10-11 12:36:08 -0500

    fix some test issues

--- test/integration/targets/entry_points/runme.sh
+++ test/integration/targets/entry_points/runme.sh
@@ -8,7 +8,8 @@ unset PYTHONPATH
 base_dir="$(dirname "$(dirname "$(dirname "$(dirname "${OUTPUT_DIR}")")")")"
 bin_dir="$(dirname "$(command -v pip)")"
 
-pip install "${base_dir}" --disable-pip-version-check --use-feature=in-tree-build
+pip install "${base_dir}" --disable-pip-version-check
+# --use-feature=in-tree-build not available on all platforms
 
 for bin in "${bin_dir}/ansible"*; do
     name="$(basename "${bin}")"
@@ -16,8 +17,8 @@ for bin in "${bin_dir}/ansible"*; do
     echo "=== ${name}=${bin} ==="
 
     if [ "${name}" == "ansible-test" ]; then
-        "${bin}" --help | tee /dev/stderr | egrep -o "^usage:\ ansible-test\ .*"
+        "${bin}" --help | tee /dev/stderr | grep -Eo "^usage:\ ansible-test\ .*"
     else
-        "${bin}" --version | tee /dev/stderr | egrep -o "(^${name}\ \[core\ .*|executable location = ${bin}$)"
+        "${bin}" --version | tee /dev/stderr | grep -Eo "(^${name}\ \[core\ .*|executable location = ${bin}$)"
     fi
 done
