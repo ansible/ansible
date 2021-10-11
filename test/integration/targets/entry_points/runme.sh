82430c7fbdb40e46023d8a0b8317eeb1ffffba7a
Matt Martz <matt@sivel.net>
2021-10-11 13:08:47 -0500

    Don't install deps in entry_points test

--- test/integration/targets/entry_points/runme.sh
+++ test/integration/targets/entry_points/runme.sh
@@ -8,7 +8,8 @@ unset PYTHONPATH
 base_dir="$(dirname "$(dirname "$(dirname "$(dirname "${OUTPUT_DIR}")")")")"
 bin_dir="$(dirname "$(command -v pip)")"
 
-pip install "${base_dir}" --disable-pip-version-check
+# --version doesn't require deps
+pip install "${base_dir}" --disable-pip-version-check --no-deps
 # --use-feature=in-tree-build not available on all platforms
 
 for bin in "${bin_dir}/ansible"*; do
