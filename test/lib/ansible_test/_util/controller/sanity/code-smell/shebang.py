a413503bbf3c8468bd3b0d2a9d8fcab0f7dda6a7
Matt Martz <matt@sivel.net>
2021-10-15 11:38:53 -0500

    Address shebang skips for examples/ in code

--- test/lib/ansible_test/_util/controller/sanity/code-smell/shebang.py
+++ test/lib/ansible_test/_util/controller/sanity/code-smell/shebang.py
@@ -72,6 +72,8 @@ def main():
                 pass  # ansible-test entry point must be executable and have a shebang
             elif re.search(r'^lib/ansible/cli/[^/]+\.py', path):
                 pass  # cli entry points must be executable and have a shebang
+            elif path.startswith('examples/'):
+                pass  # examples trigger some false positives due to location
             elif path.startswith('lib/') or path.startswith('test/lib/'):
                 if executable:
                     print('%s:%d:%d: should not be executable' % (path, 0, 0))
