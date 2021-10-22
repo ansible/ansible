6f983c64d74813df7e2574fd15cba96659d56b7a
Matt Clay <matt@mystile.com>
2021-10-21 20:05:12 -0700

    Fix unit tests in ansible-test integration test.
    Unit tests are now run for available Python versions which
    provide `virtualenv` (Python 2.x) or `venv` (Python 3.x).

--- test/integration/targets/ansible-test/collection-tests/units.sh
+++ test/integration/targets/ansible-test/collection-tests/units.sh
@@ -5,4 +5,7 @@ set -eux -o pipefail
 cp -a "${TEST_DIR}/ansible_collections" "${WORK_DIR}"
 cd "${WORK_DIR}/ansible_collections/ns/col"
 
-ansible-test units --venv --color --truncate 0 "${@}"
+options=$("${TEST_DIR}"/collection-tests/venv-pythons.py)
+IFS=', ' read -r -a pythons <<< "${options}"
+
+ansible-test units --color --truncate 0 "${pythons[@]}" "${@}"
