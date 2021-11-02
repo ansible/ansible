c089b8ff77b9eb7e905972725150998c425d4dfb
Sam Doran <sdoran@redhat.com>
2021-11-02 17:47:32 -0400

    Pass TemporaryFile object into function rather than Path object
    Pass TemporaryFile object into function rather than Path object

--- .azure-pipelines/scripts/publish-codecov.py
+++ .azure-pipelines/scripts/publish-codecov.py
@@ -5,6 +5,7 @@
 
 import argparse
 import dataclasses
+import os
 import shutil
 import subprocess
 import urllib.request
@@ -15,7 +16,7 @@ from pathlib import Path
 from tempfile import NamedTemporaryFile
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class CoverageFile:
     name: str
     path: Path
@@ -56,10 +57,10 @@ def process_files(directory: Path) -> t.Tuple[CoverageFile, ...]:
     return tuple(processed)
 
 
-def upload_files(codecov_bin: Path, files: t.Tuple[CoverageFile], dry_run: t.Optional[bool] = False) -> None:
+def upload_files(codecov_bin: str, files: t.Tuple[CoverageFile], dry_run: bool = False) -> None:
     for file in files:
         cmd = [
-            str(codecov_bin),
+            codecov_bin,
             '--name', file.name,
             '--file', str(file.path),
         ]
@@ -67,35 +68,33 @@ def upload_files(codecov_bin: Path, files: t.Tuple[CoverageFile], dry_run: t.Opt
             cmd.extend(['--flags', flag])
 
         if dry_run:
-            print(f'DRY-RUN: Would run command: {" ".join(cmd)}')
+            print(f'DRY-RUN: Would run command: {cmd}')
             continue
 
         subprocess.run(cmd, check=True)
 
 
-def download_file(url: str, dest: Path, flags: t.Optional[int] = None, dry_run: t.Optional[bool] = False) -> None:
+def download_file(url: str, dest: t.IO[bytes], flags: t.Optional[int] = None, dry_run: bool = False) -> None:
     if dry_run:
         print(f'DRY-RUN: Would download {url} to {dest} and set mode to {flags:o}')
         return
 
     with urllib.request.urlopen(url) as resp:
-        with dest.open("wb") as bf:
-            # Read data in chunks rather than all at once
-            shutil.copyfileobj(resp, bf, 64 * 1024)
+        # Read data in chunks rather than all at once
+        shutil.copyfileobj(resp, dest, 64 * 1024)
 
     if flags is not None:
-        dest.chmod(flags)
+        os.chmod(dest.name, flags)
 
 
 def main():
     args = parse_args()
     url = 'https://ansible-ci-files.s3.amazonaws.com/codecov/linux/codecov'
-    with NamedTemporaryFile(prefix='codecov-') as tmpf:
-        codecov_bin = Path(tmpf.name)
+    with NamedTemporaryFile(prefix='codecov-') as codecov_bin:
         download_file(url, codecov_bin, 0o755, args.dry_run)
 
         files = process_files(args.path)
-        upload_files(codecov_bin, files, args.dry_run)
+        upload_files(codecov_bin.name, files, args.dry_run)
 
 
 if __name__ == '__main__':
