7c58c95c0e27e7525a8d51d9224e22da4fa2b1a1
Sam Doran <sdoran@redhat.com>
2021-11-02 16:00:04 -0400

    Fix sanity test failures
    ci_complete
    ci_coverage

--- .azure-pipelines/scripts/publish-codecov.py
+++ .azure-pipelines/scripts/publish-codecov.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # Upload code coverage reports to codecov.io.
 # Multiple coverage files from multiple languages are accepted and aggregated after upload.
 # Python coverage, as well as PowerShell and Python stubs can all be uploaded.
