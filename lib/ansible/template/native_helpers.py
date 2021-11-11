b1d1e8b95855bdf1cb6755a4fd22f0a0874d09d9
Martin Krizek <martin.krizek@gmail.com>
2021-11-11 20:12:46 +0100

    Keep pre Python 3.10 literal_eval behavior (#76261)

--- lib/ansible/template/native_helpers.py
+++ lib/ansible/template/native_helpers.py
@@ -140,6 +140,11 @@ def ansible_native_concat(nodes):
         out = ''.join([to_text(_fail_on_undefined(v)) for v in chain(head, nodes)])
 
     try:
-        return ast.literal_eval(out)
+        return ast.literal_eval(
+            # In Python 3.10+ ast.literal_eval removes leading spaces/tabs
+            # from the given string. For backwards compatibility we need to
+            # parse the string ourselves without removing leading spaces/tabs.
+            ast.parse(out, mode='eval')
+        )
     except (ValueError, SyntaxError, MemoryError):
         return out
