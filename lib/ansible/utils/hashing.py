dab8767335d5daa281b1c60b5861c2cca1d0e132
Martin Krizek <martin.krizek@gmail.com>
2021-11-12 07:54:41 +0100

    Remove comment as per review

--- lib/ansible/utils/hashing.py
+++ lib/ansible/utils/hashing.py
@@ -21,8 +21,6 @@ __metaclass__ = type
 
 import os
 
-# Note, sha1 is the only hash algorithm compatible with python2.4 and with
-# FIPS-140 mode (as of 11-2014)
 from hashlib import sha1
 
 # Backwards compat only
