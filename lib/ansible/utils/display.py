dad53204b490e6e0b656a610614da4fbebace320
Brian Coca <brian.coca+git@gmail.com>
2021-11-19 11:16:06 -0500

    removed dupe line

--- lib/ansible/utils/display.py
+++ lib/ansible/utils/display.py
@@ -219,7 +219,6 @@ class Display(metaclass=Singleton):
             try:
                 cmd = subprocess.Popen([self.b_cowsay, "-l"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 (out, err) = cmd.communicate()
-                self.cows_available = {to_text(c) for c in out.split()}
                 if cmd.returncode:
                     raise Exception
                 self.cows_available = set([to_text(c) for c in out.split()])
