f88f79442fbd4b38da7459feb3c7bdab383b24d2
Brian Coca <brian.coca+git@gmail.com>
2021-12-02 09:26:55 -0500

    fix test

--- test/units/plugins/become/test_sudo.py
+++ test/units/plugins/become/test_sudo.py
@@ -59,9 +59,9 @@ def test_sudo(mocker, parser, reset_cli_args):
 
     sudo.set_options(direct={
         'become_user': 'foo',
-        'become_flags': '-non-interactive -nC=5 -s -H',
+        'become_flags': '--non-interactive -nC5 -s -H',
         'become_pass': 'testpass',
     })
 
     cmd = sudo.build_become_command('/bin/foo', sh)
-    assert re.match(r"""sudo\s+-C=5\s-s\s-H\s+-p "\[sudo via ansible, key=.+?\] password:" -u foo /bin/bash -c 'echo BECOME-SUCCESS-.+? ; /bin/foo'""", cmd), cmd
+    assert re.match(r"""sudo\s+-C5\s-s\s-H\s+-p "\[sudo via ansible, key=.+?\] password:" -u foo /bin/bash -c 'echo BECOME-SUCCESS-.+? ; /bin/foo'""", cmd), cmd
