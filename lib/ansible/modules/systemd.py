413f3fb9dd89e131e2b7f2fcf13c968f2323ed0b
James Livulpi <james.livulpi@me.com>
2021-12-04 12:39:54 -0500

    complete fix for handling indirect services/ along with integration tests

--- lib/ansible/modules/systemd.py
+++ lib/ansible/modules/systemd.py
@@ -497,11 +497,21 @@ def main():
 
             # do we need to enable the service?
             enabled = False
-            (rc, out, err) = module.run_command("%s is-enabled '%s'" % (systemctl, unit))
+            (rc, out, err) = module.run_command("%s is-enabled '%s' -l" % (systemctl, unit))
 
             # check systemctl result or if it is a init script
-            if rc == 0 and not out.strip().endswith('indirect'):
-                enabled = True
+            if rc == 0:
+                # check if service is indirect
+                if out.splitlines()[0] == 'indirect':
+                    # get the service that should be targeted for enable
+                    indirect_unit = out.split()[-1].split('/')[-1]
+                    # check on service thats targeted if already enabled
+                    (rc, out, err) = module.run_command("%s is-enabled '%s' -l" % (systemctl, indirect_unit))
+                    if rc == 0:
+                        enabled = True
+                else:
+                    enabled = True
+
             elif rc == 1:
                 # if not a user or global user service and both init script and unit file exist stdout should have enabled/disabled, otherwise use rc entries
                 if module.params['scope'] == 'system' and \
