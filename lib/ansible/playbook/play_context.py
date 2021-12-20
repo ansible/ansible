39a58c209bd681895e357a2a7241fb20168e00f4
Brian Coca <brian.coca+git@gmail.com>
2021-12-20 14:58:09 -0500

    avoid poluting vars with incorrect settings
    ci_complete

--- lib/ansible/playbook/play_context.py
+++ lib/ansible/playbook/play_context.py
@@ -317,18 +317,7 @@ class PlayContext(Base):
         Adds 'magic' variables relating to connections to the variable dictionary provided.
         In case users need to access from the play, this is a legacy from runner.
         '''
-
-        for prop, var_list in C.MAGIC_VARIABLE_MAPPING.items():
-            try:
-                if 'become' in prop:
-                    continue
-
-                var_val = getattr(self, prop)
-                for var_opt in var_list:
-                    if var_opt not in variables and var_val is not None:
-                        variables[var_opt] = var_val
-            except AttributeError:
-                continue
+        return
 
     def _get_attr_connection(self):
         ''' connections are special, this takes care of responding correctly '''
