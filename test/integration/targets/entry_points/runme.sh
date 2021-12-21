ea5c978059a7c3a762b348f7bec8a979062d0ddb
s-hertel <19572925+s-hertel@users.noreply.github.com>
2021-12-21 10:22:44 -0500

    ci_complete

--- test/integration/targets/entry_points/runme.sh
+++ test/integration/targets/entry_points/runme.sh
@@ -5,6 +5,8 @@ source virtualenv.sh
 set +x
 unset PYTHONPATH
 
+export SETUPTOOLS_USE_DISTUTILS=stdlib
+
 base_dir="$(dirname "$(dirname "$(dirname "$(dirname "${OUTPUT_DIR}")")")")"
 bin_dir="$(dirname "$(command -v pip)")"
 
