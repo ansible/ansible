30d6525680abbdca02e344f28ad6d24c86c9e082
Matt Clay <matt@mystile.com>
2022-01-05 19:19:55 -0800

    Remove module shebangs.

--- lib/ansible/modules/blockinfile.py
+++ lib/ansible/modules/blockinfile.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 # Copyright: (c) 2014, 2015 YAEGASHI Takeshi <yaegashi@debian.org>
--- lib/ansible/modules/known_hosts.py
+++ lib/ansible/modules/known_hosts.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 
 # Copyright: (c) 2014, Matthew Vernon <mcv21@cam.ac.uk>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
