d31696e3841e460f1e5ad4e7b96f96015b2f4277
Brian Coca <bcoca@users.noreply.github.com>
2022-01-10 17:25:00 -0500

    enable module defaults for implicit gather_facts (#76053)
    * deprecated default fact path setting
    * subset and timeout on same boat
    * avoid pushing module args when none
    
    Co-authored-by: Sloane Hertel <19572925+s-hertel@users.noreply.github.com>

--- lib/ansible/playbook/play.py
+++ lib/ansible/playbook/play.py
@@ -58,6 +58,8 @@ class Play(Base, Taggable, CollectionSearch):
 
     # Facts
     _gather_facts = FieldAttribute(isa='bool', default=None, always_post_validate=True)
+
+    # defaults to be deprecated, should be 'None' in future
     _gather_subset = FieldAttribute(isa='list', default=(lambda: C.DEFAULT_GATHER_SUBSET), listof=string_types, always_post_validate=True)
     _gather_timeout = FieldAttribute(isa='int', default=C.DEFAULT_GATHER_TIMEOUT, always_post_validate=True)
     _fact_path = FieldAttribute(isa='string', default=C.DEFAULT_FACT_PATH)
