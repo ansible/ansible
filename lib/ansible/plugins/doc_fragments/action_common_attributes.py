48e57efb480bae2b7a416e0f9ebd7a3d31b522d2
Brian Coca <brian.coca+git@gmail.com>
2022-01-17 14:03:21 -0500

    fix, thanks felix

--- lib/ansible/plugins/doc_fragments/action_common_attributes.py
+++ lib/ansible/plugins/doc_fragments/action_common_attributes.py
@@ -25,6 +25,7 @@ attributes:
 '''
 
     ACTIONGROUPS = r'''
+options: {}
 attributes:
     action_group:
       description: Action is part of action_group(s), for convenient setting of module_defaults.
@@ -33,6 +34,7 @@ attributes:
 '''
 
     CONN = r'''
+options: {}
 attributes:
     become:
       description: Is usable alongside become keywords
@@ -43,12 +45,14 @@ attributes:
 '''
 
     FACTS = r'''
+options: {}
 attributes:
     facts:
       description: Action returns an C(ansible_facts) dictionary that will update existing host facts
 '''
 
     FILES = r'''
+options: {}
 attributes:
     safe_file_operations:
       description: Uses Ansbile's strict file operation functions to ensure proper permissions and avoid data corruption
@@ -57,6 +61,7 @@ attributes:
 '''
 
     FLOW = r'''
+options: {}
 attributes:
     action:
       description: Indicates this has a corresponding action plugin so some parts of the options can be executed on the controller
@@ -70,6 +75,7 @@ attributes:
             - This action will not work normally outside of lockstep strategies
 '''
     RAW = r'''
+options: {}
 attributes:
     raw:
       description: Indicates if an action takes a 'raw' or 'free form' string as an option and has it's own special parsing of it
