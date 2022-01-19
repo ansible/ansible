9bf50514823f08138e12469241d04bd84daa8565
Brian Coca <brian.coca+git@gmail.com>
2022-01-19 13:35:37 -0500

    restore note

--- lib/ansible/plugins/doc_fragments/action_common_attributes.py
+++ lib/ansible/plugins/doc_fragments/action_common_attributes.py
@@ -5,6 +5,10 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 
+# NOTE: this file is here to allow modules using the new attributes feature to
+#       work w/o errors in this version of ansible, it does NOT provide the full
+#       attributes feature, just a shim to avoid the fragment not being found.
+
 class ModuleDocFragment(object):
 
     # Standard documentation fragment
