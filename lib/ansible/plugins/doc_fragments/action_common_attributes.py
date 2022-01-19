2f7b84f6c3cb6962d6c2d66966bae2a3fa113864
Brian Coca <brian.coca+git@gmail.com>
2022-01-19 12:49:52 -0500

    fix bad automerge

--- lib/ansible/plugins/doc_fragments/action_common_attributes.py
+++ lib/ansible/plugins/doc_fragments/action_common_attributes.py
@@ -1,21 +1,14 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
-
-=======
->>>>>>> f2218c57b2 (bacwards compat doc frag)
 # Copyright: Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 
-<<<<<<< HEAD
 # NOTE: this file is here to allow modules using the new attributes feature to
 #       work w/o errors in this version of ansible, it does NOT provide the full
 #       attributes feature, just a shim to avoid the fragment not being found.
 
-=======
->>>>>>> f2218c57b2 (bacwards compat doc frag)
 class ModuleDocFragment(object):
 
     # Standard documentation fragment
