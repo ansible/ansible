92fb7dbec1f3ba03a4a55d25ea4ca4ce4179516e
Brian Coca <bcoca@users.noreply.github.com>
2022-01-19 15:12:34 -0500

    bacwards compat doc frag (#75820)

--- lib/ansible/plugins/doc_fragments/action_common_attributes.py
+++ lib/ansible/plugins/doc_fragments/action_common_attributes.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 # Copyright: Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import (absolute_import, division, print_function)
@@ -14,5 +13,28 @@ class ModuleDocFragment(object):
 
     # Standard documentation fragment
     DOCUMENTATION = r'''
+options: {}
+'''
+
+    ACTIONGROUPS = r'''
+options: {}
+'''
+
+    CONN = r'''
+options: {}
+'''
+
+    FACTS = r'''
+options: {}
+'''
+
+    FILES = r'''
+options: {}
+'''
+
+    FLOW = r'''
+options: {}
+'''
+    RAW = r'''
 options: {}
 '''
