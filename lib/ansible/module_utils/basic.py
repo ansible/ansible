aec7cbeab94fa0b994564830a924e10e7c0a6b48
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2022-01-21 11:27:36 -0500

    Module Logging with WSL2 (#76375)
    * adding systemd daemon import
    
    * adding coments and AttributeError exception
    
    * adding minor fix to repeated syntax
    
    * adding changelog

--- lib/ansible/module_utils/basic.py
+++ lib/ansible/module_utils/basic.py
@@ -64,11 +64,13 @@ except ImportError:
     HAS_SYSLOG = False
 
 try:
-    from systemd import journal
+    from systemd import journal, daemon as systemd_daemon
     # Makes sure that systemd.journal has method sendv()
     # Double check that journal has method sendv (some packages don't)
-    has_journal = hasattr(journal, 'sendv')
-except ImportError:
+    # check if the system is running under systemd
+    has_journal = hasattr(journal, 'sendv') and systemd_daemon.booted()
+except (ImportError, AttributeError):
+    # AttributeError would be caused from use of .booted() if wrong systemd
     has_journal = False
 
 HAVE_SELINUX = False
