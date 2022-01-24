2269348580809504eb5aa616f280186b5398ee0c
Sandra McCann <samccann@redhat.com>
2022-01-24 15:21:04 -0500

    make older roadmaps also show up

--- docs/docsite/sphinx_conf/ansible_conf.py
+++ docs/docsite/sphinx_conf/ansible_conf.py
@@ -98,11 +98,6 @@ exclude_patterns = [
     'porting_guides/porting_guide_base_2.10.rst',
     'porting_guides/porting_guide_core_*',
     'roadmap/index.rst',
-    'roadmap/ROADMAP_2_5.rst',
-    'roadmap/ROADMAP_2_6.rst',
-    'roadmap/ROADMAP_2_7.rst',
-    'roadmap/ROADMAP_2_8.rst',
-    'roadmap/ROADMAP_2_9.rst',
 ]
 
 # The reST default role (used for this markup: `text`) to use for all
