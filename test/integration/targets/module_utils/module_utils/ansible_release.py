8be0b7aaadb892f8009ed6d413b87fd223487720
Matt Martz <matt@sivel.net>
2022-01-26 15:49:12 -0600

    Add some notes, and update tests, to reflect overridden ansible_release.py

--- test/integration/targets/module_utils/module_utils/ansible_release.py
+++ test/integration/targets/module_utils/module_utils/ansible_release.py
@@ -1,1 +1,4 @@
-data = 'overridden facts.py'
+# This file overrides the builtin ansible.module_utils.ansible_release file
+# to test that it can be overridden. Previously this was facts.py but caused issues
+# with dependencies that may need to execute a module that makes use of facts
+data = 'overridden ansible_release.py'
