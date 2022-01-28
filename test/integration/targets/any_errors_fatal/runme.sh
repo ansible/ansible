59553fee69aaa2e519d92095d0ee6f689711761c
Martin Krizek <martin.krizek@gmail.com>
2022-01-28 20:35:19 +0100

    Track failures in the includes results to avoid special handling of include errors (#53688)
    * Make include_role/include_tasks work with any_errors_fatal v2
    
    Fixes #50897
    
    Co-authored-by: Matt Martz <matt@sivel.net>
    
    * Add failed to results in free strategy too
    
    * Fix
    
    * Avoid duplicating results
    
    * ci_complete
    
    Co-authored-by: Matt Martz <matt@sivel.net>

--- test/integration/targets/any_errors_fatal/runme.sh
+++ test/integration/targets/any_errors_fatal/runme.sh
@@ -5,14 +5,14 @@ ansible-playbook -i inventory "$@" play_level.yml| tee out.txt | grep 'any_error
 res=$?
 cat out.txt
 if [ "${res}" -eq 0 ] ; then
-	exit 1
+    exit 1
 fi
 
 ansible-playbook -i inventory "$@" on_includes.yml | tee out.txt | grep 'any_errors_fatal_this_should_never_be_reached'
 res=$?
 cat out.txt
 if [ "${res}" -eq 0 ] ; then
-	exit 1
+    exit 1
 fi
 
 set -ux
@@ -20,4 +20,18 @@ set -ux
 ansible-playbook -i inventory "$@" always_block.yml | tee out.txt | grep 'any_errors_fatal_always_block_start'
 res=$?
 cat out.txt
-exit $res
+
+if [ "${res}" -ne 0 ] ; then
+    exit 1
+fi
+
+set -ux
+
+for test_name in test_include_role test_include_tasks; do
+  ansible-playbook -i inventory "$@" -e test_name=$test_name 50897.yml | tee out.txt | grep 'any_errors_fatal_this_should_never_be_reached'
+  res=$?
+  cat out.txt
+  if [ "${res}" -eq 0 ] ; then
+      exit 1
+  fi
+done
