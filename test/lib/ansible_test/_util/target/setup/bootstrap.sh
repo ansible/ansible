0d5401d950346d4398b1f8e1ab80e63e33a8266b
Matt Clay <matt@mystile.com>
2022-02-07 13:59:30 -0800

    ansible-test - Remove unused code.
    This was overlooked in a previous commit that removed FreeBSD 12.2 support.

--- test/lib/ansible_test/_util/target/setup/bootstrap.sh
+++ test/lib/ansible_test/_util/target/setup/bootstrap.sh
@@ -104,11 +104,6 @@ bootstrap_remote_freebsd()
         # Declare platform/python version combinations which do not have supporting OS packages available.
         # For these combinations ansible-test will use pip to install the requirements instead.
         case "${platform_version}/${python_version}" in
-            "12.2/3.8")
-                jinja2_pkg=""  # not available
-                cryptography_pkg=""  # not available
-                pyyaml_pkg=""  # not available
-                ;;
             "13.0/3.8")
                 jinja2_pkg=""  # not available
                 cryptography_pkg=""  # not available
