4cc1e7dc4c891136c299e311f9b9a69bb9c11d15
Jordan Borean <jborean93@gmail.com>
2022-02-10 05:41:04 +1000

    Use single definition and added changelog

--- lib/ansible/modules/replace.py
+++ lib/ansible/modules/replace.py
@@ -241,7 +241,7 @@ def main():
     params = module.params
     path = params['path']
     encoding = params['encoding']
-    res_args = dict()
+    res_args = dict(rc=0)
 
     params['after'] = to_text(params['after'], errors='surrogate_or_strict', nonstring='passthru')
     params['before'] = to_text(params['before'], errors='surrogate_or_strict', nonstring='passthru')
@@ -278,7 +278,6 @@ def main():
         else:
             res_args['msg'] = 'Pattern for before/after params did not match the given file: %s' % pattern
             res_args['changed'] = False
-            res_args['rc'] = 0
             module.exit_json(**res_args)
     else:
         section = contents
@@ -309,7 +308,6 @@ def main():
         path = os.path.realpath(path)
         write_changes(module, to_bytes(result[0], encoding=encoding), path)
 
-    res_args['rc'] = 0
     res_args['msg'], res_args['changed'] = check_file_attrs(module, changed, msg)
     module.exit_json(**res_args)
 
