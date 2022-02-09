d35bef68f5d5502d3718d7b9a260308686c1c981
Jack Scheible <jscheibl@redhat.com>
2022-02-09 15:23:04 -0500

    replace - always return rc (#71963)
    * Return rc=0 on success.
    
    Error handling in playbooks generally expects `rc` to be set to 0 when a module has not failed.  Playbook authors should not have to check for the existence of `rc` first.
    
    * Use single definition and added changelog
    
    * Fix up tests with new return value
    
    Co-authored-by: Jordan Borean <jborean93@gmail.com>

--- lib/ansible/modules/replace.py
+++ lib/ansible/modules/replace.py
@@ -241,7 +241,7 @@ def main():
     params = module.params
     path = params['path']
     encoding = params['encoding']
-    res_args = dict()
+    res_args = dict(rc=0)
 
     params['after'] = to_text(params['after'], errors='surrogate_or_strict', nonstring='passthru')
     params['before'] = to_text(params['before'], errors='surrogate_or_strict', nonstring='passthru')
