c9d3518d2f3812787e1627806b5fa93f8fae48a6
Brian Coca <bcoca@users.noreply.github.com>
2022-02-11 18:19:38 -0500

    Fix final fact delegation (#77008)
    * fix facts delegation loop overwrite
    
     partial revert of change to allow facts to be present in each loop iteration
     was not needed in final results as result processing alreayd had the disctiontion
     and ended up breaking the assumptions in the calling code.
    
     fixes #76676

--- lib/ansible/executor/task_executor.py
+++ lib/ansible/executor/task_executor.py
@@ -786,13 +786,7 @@ class TaskExecutor:
 
         if 'ansible_facts' in result and self._task.action not in C._ACTION_DEBUG:
             if self._task.action in C._ACTION_WITH_CLEAN_FACTS:
-                if self._task.delegate_to and self._task.delegate_facts:
-                    if '_ansible_delegated_vars' in variables:
-                        variables['_ansible_delegated_vars'].update(result['ansible_facts'])
-                    else:
-                        variables['_ansible_delegated_vars'] = result['ansible_facts']
-                else:
-                    variables.update(result['ansible_facts'])
+                variables.update(result['ansible_facts'])
             else:
                 # TODO: cleaning of facts should eventually become part of taskresults instead of vars
                 af = wrap_var(result['ansible_facts'])
