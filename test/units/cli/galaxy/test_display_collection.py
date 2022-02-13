654fb581a90a97c67c612d2ad0037e2bb8801f98
Daniel Goldman <danielgoldman4@gmail.com>
2022-02-13 17:04:56 -0500

    clean tests

--- test/units/cli/galaxy/test_display_collection.py
+++ test/units/cli/galaxy/test_display_collection.py
@@ -11,32 +11,21 @@ from ansible.cli.galaxy import _display_collection
 from ansible.galaxy.dependency_resolution.dataclasses import Requirement
 
 
-@pytest.fixture
-def collection_object():
-    def _cobj(fqcn='sandwiches.ham'):
-        return Requirement(fqcn, '1.5.0', None, 'galaxy', None)
-    return _cobj
+def collection_object(fqcn='sandwiches.ham'):
+    return Requirement(fqcn, '1.5.0', None, 'galaxy', None)
 
 
-def test_display_collection(collection_object):
-    out = _display_collection(collection_object())
-
-    assert out == 'sandwiches.ham 1.5.0  '
+def test_display_collection():
+    assert _display_collection(collection_object()) == 'sandwiches.ham 1.5.0  '
 
 
-def test_display_collections_small_max_widths(collection_object):
-    out = _display_collection(collection_object(), 1, 1)
-
-    assert out == 'sandwiches.ham 1.5.0  '
+def test_display_collections_small_max_widths():
+    assert _display_collection(collection_object(), 1, 1) == 'sandwiches.ham 1.5.0  '
 
 
-def test_display_collections_large_max_widths(collection_object):
-    out = _display_collection(collection_object(), 20, 20)
-
-    assert out == 'sandwiches.ham       1.5.0               '
+def test_display_collections_large_max_widths():
+    assert _display_collection(collection_object(), 20, 20) == 'sandwiches.ham       1.5.0               '
 
 
-def test_display_collection_small_minimum_widths(collection_object):
-    out = _display_collection(collection_object('a.b'), min_cwidth=0, min_vwidth=0)
-
-    assert out == 'a.b        1.5.0  '
+def test_display_collection_small_minimum_widths():
+    assert _display_collection(collection_object('a.b'), min_cwidth=0, min_vwidth=0) == 'a.b        1.5.0  '
--- test/units/cli/galaxy/test_display_header.py
+++ test/units/cli/galaxy/test_display_header.py
@@ -5,34 +5,36 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
+import textwrap
+
 from ansible.cli.galaxy import _display_header
 
 
 def test_display_header_default():
     out = _display_header('/collections/path', 'h1', 'h2')
-    out_lines = out.splitlines()
-
-    assert out_lines[0] == ''
-    assert out_lines[1] == '# /collections/path'
-    assert out_lines[2] == 'h1         h2     '
-    assert out_lines[3] == '---------- -------'
+    assert out == textwrap.dedent('''\
+        
+        # /collections/path
+        h1         h2     
+        ---------- -------'''
+    )
 
 
 def test_display_header_widths():
     out = _display_header('/collections/path', 'Collection', 'Version', 18, 18)
-    out_lines = out.splitlines()
-
-    assert out_lines[0] == ''
-    assert out_lines[1] == '# /collections/path'
-    assert out_lines[2] == 'Collection         Version           '
-    assert out_lines[3] == '------------------ ------------------'
+    assert out == textwrap.dedent('''\
+        
+        # /collections/path
+        Collection         Version           
+        ------------------ ------------------'''
+    )
 
 
 def test_display_header_small_widths():
     out = _display_header('/collections/path', 'Col', 'Ver', 1, 1)
-    out_lines = out.splitlines()
-
-    assert out_lines[0] == ''
-    assert out_lines[1] == '# /collections/path'
-    assert out_lines[2] == 'Col Ver'
-    assert out_lines[3] == '--- ---'
+    assert out == textwrap.dedent('''\
+        
+        # /collections/path
+        Col Ver
+        --- ---'''
+    )
