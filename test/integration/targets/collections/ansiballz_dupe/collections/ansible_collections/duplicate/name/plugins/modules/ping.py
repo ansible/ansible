74e41073c87717e03411531df2bbf77eeb59c07b
Matt Martz <matt@sivel.net>
2022-02-21 15:26:16 -0600

    Be a little more explicit about test goals

--- test/integration/targets/collections/ansiballz_dupe/collections/ansible_collections/duplicate/name/plugins/modules/ping.py
+++ test/integration/targets/collections/ansiballz_dupe/collections/ansible_collections/duplicate/name/plugins/modules/ping.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 from ansible.module_utils.basic import AnsibleModule
-AnsibleModule({}).exit_json(ping='not ansible.builtin.ping')
+AnsibleModule({}).exit_json(ping='duplicate.name.pong')
