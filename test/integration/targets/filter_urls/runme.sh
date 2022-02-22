124c843b21ffb9ff923d57604aa6160871cdaaf7
Matt Clay <matt@mystile.com>
2022-02-21 23:39:41 -0800

    Fix integration tests that install MarkupSafe.

--- test/integration/targets/filter_urls/runme.sh
+++ test/integration/targets/filter_urls/runme.sh
@@ -16,7 +16,7 @@ pip install 'setuptools<45'
 # Install Jinja 2.6 since we want to test the fallback to Ansible's custom
 # urlencode functions. Jinja 2.6 does not have urlencode so we will trigger the
 # fallback.
-pip install 'jinja2 >= 2.6, < 2.7'
+pip install 'jinja2 >= 2.6, < 2.7' 'markupsafe < 2.1'
 
 # Run the playbook again in the venv with Jinja 2.6
 ansible-playbook runme.yml "$@"
--- test/integration/targets/groupby_filter/runme.sh
+++ test/integration/targets/groupby_filter/runme.sh
@@ -7,10 +7,10 @@ source virtualenv.sh
 # Update pip in the venv to a version that supports constraints
 pip install --requirement requirements.txt
 
-pip install -U jinja2==2.9.4 --constraint "../../../lib/ansible_test/_data/requirements/constraints.txt"
+pip install -U jinja2==2.9.4 "markupsafe<2.1.0" --constraint "../../../lib/ansible_test/_data/requirements/constraints.txt"
 
 ansible-playbook -i ../../inventory test_jinja2_groupby.yml -v "$@"
 
-pip install -U "jinja2<2.9.0" --constraint "../../../lib/ansible_test/_data/requirements/constraints.txt"
+pip install -U "jinja2<2.9.0" "markupsafe<2.1.0" --constraint "../../../lib/ansible_test/_data/requirements/constraints.txt"
 
 ansible-playbook -i ../../inventory test_jinja2_groupby.yml -v "$@"
