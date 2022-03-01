50a511f4de6d07107c23ac134516a33fb8424032
Matt Clay <matt@mystile.com>
2022-03-01 10:18:03 -0800

    Fix ansible-runner integration test on RHEL 7.9.

--- test/integration/targets/ansible-runner/runme.sh
+++ test/integration/targets/ansible-runner/runme.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+source virtualenv.sh
+
 set -eux
+
+# remove `*.py` scripts from the venv to avoid attempts to load them as plugins
+rm -f "${OUTPUT_DIR}"/venv/bin/*.py
 
 ANSIBLE_ROLES_PATH=../ ansible-playbook test.yml -i inventory "$@"
