24331de9da2a2ce2d890f815fecc9f6f77aacd48
Sandra McCann <samccann@redhat.com>
2022-03-02 15:04:24 -0500

    try on core

--- docs/docsite/sphinx_conf/core_conf.py
+++ docs/docsite/sphinx_conf/core_conf.py
@@ -258,14 +258,14 @@ htmlhelp_basename = 'Poseidodoc'
 # the extension builds 404.rst into a location-agnostic 404 page
 #
 # default is `en` - using this for the sub-site:
-notfound_default_language = "ansible"
+# notfound_default_language = "ansible"
 # default is `latest`:
 # setting explicitly - docsite serves up /ansible/latest/404.html
 # so keep this set to `latest` even on the `devel` branch
 # then no maintenance is needed when we branch a new stable_x.x
-notfound_default_version = "latest"
+#notfound_default_version = "latest"
 # makes default setting explicit:
-notfound_no_urls_prefix = False
+notfound__urls_prefix = "/ansible-core/devel/"
 
 # Options for LaTeX output
 # ------------------------
