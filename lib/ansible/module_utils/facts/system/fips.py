fab23dffa5590f901a8a5ff7bda51e7da7fb8857
Matt Clay <matt@mystile.com>
2022-03-03 09:08:12 -0800

    Fix type hints in module_utils.facts.

--- lib/ansible/module_utils/facts/system/fips.py
+++ lib/ansible/module_utils/facts/system/fips.py
@@ -18,6 +18,8 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import ansible.module_utils.compat.typing as t
+
 from ansible.module_utils.facts.utils import get_file_content
 
 from ansible.module_utils.facts.collector import BaseFactCollector
@@ -25,7 +27,7 @@ from ansible.module_utils.facts.collector import BaseFactCollector
 
 class FipsFactCollector(BaseFactCollector):
     name = 'fips'
-    _fact_ids = set()
+    _fact_ids = set()  # type: t.Set[str]
 
     def collect(self, module=None, collected_facts=None):
         # NOTE: this is populated even if it is not set
