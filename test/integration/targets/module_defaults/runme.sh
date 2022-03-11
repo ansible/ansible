67cf3ffc66378ee376119f296acf57ad90542508
s-hertel <19572925+s-hertel@users.noreply.github.com>
2022-03-11 11:20:08 -0500

    add integration tests

--- test/integration/targets/module_defaults/runme.sh
+++ test/integration/targets/module_defaults/runme.sh
@@ -2,7 +2,11 @@
 
 set -eux
 
+sudo ln -s "${PWD}/collections/ansible_collections/testns/testcoll/plugins/action/vyos.py" ./collections/ansible_collections/testns/testcoll/plugins/action/vyos_facts.py
+
 ansible-playbook test_defaults.yml "$@"
+
+sudo rm ./collections/ansible_collections/testns/testcoll/plugins/action/vyos_facts.py
 
 ansible-playbook test_action_groups.yml "$@"
 
