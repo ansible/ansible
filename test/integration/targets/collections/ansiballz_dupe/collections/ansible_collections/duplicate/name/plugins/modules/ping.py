ef46bc46f459cf702f098d64b37c74cf0557ebd7
Matt Martz <matt@sivel.net>
2022-03-15 13:09:40 -0500

    [stable-2.11] Use full python package for ansiballz cache filenames (#77090) (#77126)
    * [stable-2.11] Use full python package for ansiballz cache filenames (#77090)
    
    * Use full python package for ansiballz cache filenames
    
    * Be a little more explicit about test goals
    (cherry picked from commit 8cbe143)
    
    Co-authored-by: Matt Martz <matt@sivel.net>
    
    * linting

---
+++ test/integration/targets/collections/ansiballz_dupe/collections/ansible_collections/duplicate/name/plugins/modules/ping.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+from ansible.module_utils.basic import AnsibleModule
+AnsibleModule({}).exit_json(ping='duplicate.name.pong')
