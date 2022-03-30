a294b885ac72ce98508a5688eb23f97573dbc6a0
Brian Coca <brian.coca+git@gmail.com>
2022-03-30 14:44:18 -0400

    moar cases

--- lib/ansible/plugins/test/uri.py
+++ lib/ansible/plugins/test/uri.py
@@ -11,6 +11,7 @@ def is_uri(value, schemas=None):
     ''' Will verify that the string passed is a valid 'uri' according to spec, if passed a list of valid schemas it will even match those '''
     try:
         x = urlparse(value)
+        print(x)
         isit = all([x.scheme, x.netloc, not schemas or x.scheme in schemas])
     except Exception as e:
         isit = False
