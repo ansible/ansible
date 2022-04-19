5d8624e47bc25071e37a77dac86796fd04b73e04
Sloane Hertel <19572925+s-hertel@users.noreply.github.com>
2022-04-19 15:17:36 -0400

    Add a test and changelog for non-string host names in yaml inventory files (#77569)

--- test/integration/targets/inventory_yaml/runme.sh
+++ test/integration/targets/inventory_yaml/runme.sh
@@ -2,3 +2,5 @@
 
 # handle empty/commented out group keys correctly https://github.com/ansible/ansible/issues/47254
 ANSIBLE_VERBOSITY=0 diff -w <(ansible-inventory -i ./test.yml --list) success.json
+
+ansible-inventory -i ./test_int_hostname.yml --list 2>&1 | grep 'Host pattern 1234 must be a string'
