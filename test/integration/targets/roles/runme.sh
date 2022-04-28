4f3c43b2f89df338f07bac352a3a40e33122719b
Brian Coca <brian.coca+git@gmail.com>
2022-04-28 15:33:19 -0400

    gimme space

--- test/integration/targets/roles/runme.sh
+++ test/integration/targets/roles/runme.sh
@@ -18,4 +18,4 @@ set -eux
 # ensure role data is merged correctly
 ansible-playbook data_integrity.yml -i ../../inventory "$@"
 
-[ "$(ansible-playbook no_outside.yml -i ../../inventory "$@" 2>&1 |grep -c 'Role skipped loading')" = "1"]
+[ "$(ansible-playbook no_outside.yml -i ../../inventory "$@" 2>&1 |grep -c 'Role skipped loading')" = "1" ]
