b2b64de5293bb652d76df670f21c4cc52a6a5913
Martin Krizek <martin.krizek@gmail.com>
2022-05-03 13:53:05 +0200

    Simplify tests

--- test/integration/targets/invalid_var_names/runme.sh
+++ test/integration/targets/invalid_var_names/runme.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -eux
+
 export ANSIBLE_FORCE_COLOR=0
 export ANSIBLE_DEPRECATION_WARNINGS=1
 
@@ -9,10 +10,10 @@ ansible-playbook import_tasks.yml "$@" 2>&1 | grep "Invalid variable name"
 ansible-playbook include_role.yml "$@" 2>&1 | grep "Invalid variable name"
 ansible-playbook include_tasks.yml "$@" 2>&1 | grep "Invalid variable name"
 ansible-playbook include_vars.yml "$@" 2>&1 | grep "Invalid variable name"
-ansible-playbook playbook_with_debug_task.yml -i inventory_invalid_host_vars/inventory "$@" 2>&1 | grep "Invalid variable name"
-ansible-playbook playbook_with_debug_task.yml -i inventory_invalid_host_vars_file/inventory "$@" 2>&1 | grep "Invalid variable name"
-ansible-playbook playbook_with_debug_task.yml -i inventory_invalid_group_vars/inventory "$@" 2>&1 | grep "Invalid variable name"
-ansible-playbook playbook_with_debug_task.yml -i inventory_invalid_group_vars_file/inventory "$@" 2>&1 | grep "Invalid variable name"
+ansible -m debug -i inventory_invalid_host_vars/inventory host1 "$@" 2>&1 | grep "Invalid variable name"
+ansible -m debug -i inventory_invalid_host_vars_file/inventory host1 "$@" 2>&1 | grep "Invalid variable name"
+ansible -m debug -i inventory_invalid_group_vars/inventory host1 "$@" 2>&1 | grep "Invalid variable name"
+ansible -m debug -i inventory_invalid_group_vars_file/inventory host1 "$@" 2>&1 | grep "Invalid variable name"
 ansible-playbook play_vars.yml "$@" 2>&1 | grep "Invalid variable name"
 ansible-playbook register.yml "$@" 2>&1 | grep "Invalid variable name"
 ansible-playbook roles-defaults.yml "$@" 2>&1 | grep "Invalid variable name"
@@ -22,3 +23,9 @@ ansible-playbook set_fact.yml "$@" 2>&1 | grep "Invalid variable name"
 ansible-playbook set_stats.yml "$@" 2>&1 | grep "Invalid variable name"
 ansible-playbook task_vars.yml "$@" 2>&1 | grep "Invalid variable name"
 ansible-playbook vars_files.yml "$@" 2>&1 | grep "Invalid variable name"
+
+
+
+ansible -m debug -e 'lookup=function' localhost "$@" 2>&1 | grep "Invalid variable name"
+ansible -m debug -e 'name=attribute' localhost "$@" 2>&1 | grep "Invalid variable name"
+ansible -m debug -e 'loop_with=private_attribute' localhost 2>&1 | grep "Invalid variable name"
