5e50284693cb5531eb4265a0ab94b35be89457f6
Brian Coca <bcoca@users.noreply.github.com>
2022-05-17 11:41:47 -0400

    Restrict role loading .. to role ... (#77683)
    * Ansible will now error out if you try to use the `*_from` to load files from outside the role being loaded by `{import,include}_role`

--- test/integration/targets/roles/runme.sh
+++ test/integration/targets/roles/runme.sh
@@ -17,3 +17,12 @@ set -eux
 
 # ensure role data is merged correctly
 ansible-playbook data_integrity.yml -i ../../inventory "$@"
+
+# ensure role fails when trying to load 'non role' in  _from
+ansible-playbook no_outside.yml -i ../../inventory "$@" > role_outside_output.log 2>&1 || true
+if grep "as it is not inside the expected role path" role_outside_output.log >/dev/null; then
+  echo "Test passed (playbook failed with expected output, output not shown)."
+else
+  echo "Test failed, expected output from playbook failure is missing, output not shown)."
+  exit 1
+fi
