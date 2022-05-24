669088017f6c743b8bfdc83920b26c6fd58c9558
s-hertel <19572925+s-hertel@users.noreply.github.com>
2022-05-24 17:06:13 -0400

    Update temporary hardcoded fallback for ansible-test

--- lib/ansible/galaxy/dependency_resolution/providers.py
+++ lib/ansible/galaxy/dependency_resolution/providers.py
@@ -41,7 +41,7 @@ except ImportError:
 
 # TODO: add python requirements to ansible-test's ansible-core distribution info and remove the hardcoded lowerbound/upperbound fallback
 RESOLVELIB_LOWERBOUND = SemanticVersion("0.5.3")
-RESOLVELIB_UPPERBOUND = SemanticVersion("0.6.0")
+RESOLVELIB_UPPERBOUND = SemanticVersion("0.9.0")
 RESOLVELIB_VERSION = SemanticVersion.from_loose_version(LooseVersion(resolvelib_version))
 
 
