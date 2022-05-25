e7e1d592a699f02e591965e252751e3b24c7220d
Matt Martz <matt@sivel.net>
2022-05-25 13:57:31 -0500

    Incidental coverage 1 (#77846)
    * Add find test to assert patterns with regex
    
    * Add test to assert that nested includes with relative path work
    
    * Add test to assert symmetric_difference supports unhashable types
    
    * ci_complete ci_coverage
    
    * Add test to assert JSON values are converted to Python
    
    * Add test to assert how unexpected module failures behave
    
    * Add test to verify remote_is_local
    
    * ci_complete ci_coverage
    
    * Get real path of local tmp
    
    * ci_complete ci_coverage
    
    * Expanded symmetric_difference tests
    
    * ci_complete ci_coverage
    
    * I don't even know
    
    * ci_complete ci_coverage
    
    * Revert mathstuff changes
    
    * ci_complete ci_coverage

---
+++ test/integration/targets/unexpected_executor_exception/action_plugins/unexpected.py
@@ -0,0 +1,8 @@
+from ansible.plugins.action import ActionBase
+
+
+class ActionModule(ActionBase):
+    TRANSFERS_FILES = False
+
+    def run(self, tmp=None, task_vars=None):
+        raise Exception('boom')
