814fad4117330c99a748305c7e17a6d751804d4f
Sandra McCann <samccann@redhat.com>
2022-06-23 14:25:23 -0400

    Update list for gather_subset (#78133)

--- lib/ansible/modules/setup.py
+++ lib/ansible/modules/setup.py
@@ -17,9 +17,19 @@ options:
         version_added: "2.1"
         description:
             - "If supplied, restrict the additional facts collected to the given subset.
-              Possible values: C(all), C(min), C(hardware), C(network), C(virtual), C(ohai), and
-              C(facter). Can specify a list of values to specify a larger subset.
-              Values can also be used with an initial C(!) to specify that
+              Possible values: C(all), C(all_ipv4_addresses), C(all_ipv6_addresses), C(apparmor), C(architecture),
+              C(caps), C(chroot),C(cmdline), C(date_time), C(default_ipv4), C(default_ipv6), C(devices),
+              C(distribution), C(distribution_major_version), C(distribution_release), C(distribution_version),
+              C(dns), C(effective_group_ids), C(effective_user_id), C(env), C(facter), C(fips), C(hardware),
+              C(interfaces), C(is_chroot), C(iscsi), C(kernel), C(local), C(lsb), C(machine), C(machine_id),
+              C(mounts), C(network), C(ohai), C(os_family), C(pkg_mgr), C(platform), C(processor), C(processor_cores),
+              C(processor_count), C(python), C(python_version), C(real_user_id), C(selinux), C(service_mgr),
+              C(ssh_host_key_dsa_public), C(ssh_host_key_ecdsa_public), C(ssh_host_key_ed25519_public),
+              C(ssh_host_key_rsa_public), C(ssh_host_pub_keys), C(ssh_pub_keys), C(system), C(system_capabilities),
+              C(system_capabilities_enforced), C(user), C(user_dir), C(user_gecos), C(user_gid), C(user_id),
+              C(user_shell), C(user_uid), C(virtual), C(virtualization_role), C(virtualization_type).
+             Can specify a list of values to specify a larger subset.
+             Values can also be used with an initial C(!) to specify that
               that specific subset should not be collected.  For instance:
               C(!hardware,!network,!virtual,!ohai,!facter). If C(!all) is specified
               then only the min subset is collected. To avoid collecting even the
