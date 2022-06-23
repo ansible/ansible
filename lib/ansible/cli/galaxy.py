51674b9a885f932fe504599785968a47e81249f9
Brian Coca <brian.coca+git@gmail.com>
2022-06-23 13:19:39 -0400

    2lines

--- lib/ansible/cli/galaxy.py
+++ lib/ansible/cli/galaxy.py
@@ -642,7 +642,8 @@ class GalaxyCLI(CLI):
         cmd_token = GalaxyToken(token=context.CLIARGS['api_key'])
 
         # resolve validate_certs
-        validate_certs = (True if C.GALAXY_IGNORE_CERTS is None else not C.GALAXY_IGNORE_CERTS) if context.CLIARGS['validate_certs'] is None else context.CLIARGS['validate_certs']
+        v_config_default = True if C.GALAXY_IGNORE_CERTS is None else not C.GALAXY_IGNORE_CERTS
+        validate_certs = v_config_default if context.CLIARGS['validate_certs'] is None else context.CLIARGS['validate_certs']
         if cmd_server:
             # Cmd args take precedence over the config entry but fist check if the arg was a name and use that config
             # entry, otherwise create a new API entry for the server specified.
