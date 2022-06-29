f9aa78f467c41869c02e300362df449fd195bb2f
Matt Clay <matt@mystile.com>
2022-06-29 15:30:44 -0700

    Omit waitstatus_to_exitcode.
    The waitstatus_to_exitcode function was only added in Python 3.9.
    Fortunately we don't need to know the actual code, just if the command failed or not.

--- test/integration/targets/ansible-test-no-tty/ansible_collections/ns/col/run-with-pty.py
+++ test/integration/targets/ansible-test-no-tty/ansible_collections/ns/col/run-with-pty.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 """Run a command using a PTY."""
 
-import os
 import pty
 import sys
 
-sys.exit(os.waitstatus_to_exitcode(pty.spawn(sys.argv[1:])))
+sys.exit(pty.spawn(sys.argv[1:]))
