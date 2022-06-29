0612b4118809e706e4153e5a36497f71eb0ccbe5
Brian Coca <brian.coca+git@gmail.com>
2022-06-29 16:35:41 -0400

    shell plugin now uses config system fully
      removed 'action plugin hack' that did it's own
      inheritance/traversal of environment keywords
    
      this also allows for plugins to expand/define more sources of config

--- lib/ansible/plugins/action/__init__.py
+++ lib/ansible/plugins/action/__init__.py
@@ -356,38 +356,11 @@ class ActionBase(ABC, _AnsiblePluginInfoMixin):
 
         return module_bits, module_path
 
-    def _compute_environment_string(self, raw_environment_out=None):
-        """
+    def _compute_environment_string(self):
+        '''
         Builds the environment string to be used when executing the remote task.
-        """
-
-        final_environment = dict()
-        if self._task.environment is not None:
-            environments = self._task.environment
-            if not isinstance(environments, list):
-                environments = [environments]
-
-            # The order of environments matters to make sure we merge
-            # in the parent's values first so those in the block then
-            # task 'win' in precedence
-            for environment in environments:
-                if environment is None or len(environment) == 0:
-                    continue
-                temp_environment = self._templar.template(environment)
-                if not isinstance(temp_environment, dict):
-                    raise AnsibleError("environment must be a dictionary, received %s (%s)" % (temp_environment, type(temp_environment)))
-                # very deliberately using update here instead of combine_vars, as
-                # these environment settings should not need to merge sub-dicts
-                final_environment.update(temp_environment)
-
-        if len(final_environment) > 0:
-            final_environment = self._templar.template(final_environment)
-
-        if isinstance(raw_environment_out, dict):
-            raw_environment_out.clear()
-            raw_environment_out.update(final_environment)
-
-        return self._connection._shell.env_prefix(**final_environment)
+        '''
+        return self._connection._shell.env_prefix()
 
     def _early_needs_tmp_path(self):
         """
