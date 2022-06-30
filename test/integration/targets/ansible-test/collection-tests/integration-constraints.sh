892d1f76f18593d2bed0f50e56c7a31c967bb473
Matt Clay <matt@mystile.com>
2022-06-29 19:47:05 -0700

    Split out integration-constraints test.

--- test/integration/targets/ansible-test/collection-tests/integration-constraints.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux -o pipefail
-
-cp -a "${TEST_DIR}/ansible_collections" "${WORK_DIR}"
-cd "${WORK_DIR}/ansible_collections/ns/col_constraints"
-
-ansible-test integration --venv --color --truncate 0 "${@}"
