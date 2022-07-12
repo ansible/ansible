acb5d8f5883f0c733968f43caf7a2fd19bd95503
Kay Yan <yankay@users.noreply.github.com>
2022-07-12 23:24:51 +0800

    Added Kylin Linux Advanced Server Support (#78232)

--- lib/ansible/module_utils/facts/system/distribution.py
+++ lib/ansible/module_utils/facts/system/distribution.py
@@ -511,7 +511,7 @@ class Distribution(object):
                                 'Ascendos', 'CloudLinux', 'PSBM', 'OracleLinux', 'OVS',
                                 'OEL', 'Amazon', 'Virtuozzo', 'XenServer', 'Alibaba',
                                 'EulerOS', 'openEuler', 'AlmaLinux', 'Rocky', 'TencentOS',
-                                'EuroLinux'],
+                                'EuroLinux', 'Kylin Linux Advanced Server'],
                      'Debian': ['Debian', 'Ubuntu', 'Raspbian', 'Neon', 'KDE neon',
                                 'Linux Mint', 'SteamOS', 'Devuan', 'Kali', 'Cumulus Linux',
                                 'Pop!_OS', 'Parrot', 'Pardus GNU/Linux', 'Uos', 'Deepin'],
