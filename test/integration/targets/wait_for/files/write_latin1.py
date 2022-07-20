02a5ff05371ea5c835a26d657622229098e8b7b2
Matt Martz <matt@sivel.net>
2022-07-20 10:48:58 -0500

    Write non-ascii via script instead of static file

---
+++ test/integration/targets/wait_for/files/write_latin1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import sys
+
+# latin1 encoded bytes
+# to ensure wait_for doesn't have any encoding errors
+data = b'premi\xe8re is first\npremie?re is slightly different\n????????? is Cyrillic\n? am Deseret\n'
+
+with open(sys.argv[1], 'wb') as f:
+    f.write(data)
