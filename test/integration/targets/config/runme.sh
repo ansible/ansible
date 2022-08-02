58dc050bac8afd9cedc2f5587e5234113d066674
Brian Coca <bcoca@users.noreply.github.com>
2022-08-02 11:26:54 -0400

    config error origin (#78407)
    (cherry picked from commit 9b79d6ba3582bb0ef339738b7e1ade879d00dfe3)

--- test/integration/targets/config/runme.sh
+++ test/integration/targets/config/runme.sh
@@ -7,7 +7,7 @@ set -eux
 ANSIBLE_TIMEOUT= ansible -m ping testhost -i ../../inventory "$@"
 
 # env var is wrong type, this should be a fatal error pointing at the setting
-ANSIBLE_TIMEOUT='lola' ansible -m ping testhost -i ../../inventory "$@" 2>&1|grep 'Invalid type for configuration option setting: DEFAULT_TIMEOUT'
+ANSIBLE_TIMEOUT='lola' ansible -m ping testhost -i ../../inventory "$@" 2>&1|grep 'Invalid type for configuration option setting: DEFAULT_TIMEOUT (from env: ANSIBLE_TIMEOUT)'
 
 # https://github.com/ansible/ansible/issues/69577
 ANSIBLE_REMOTE_TMP="$HOME/.ansible/directory_with_no_space"  ansible -m ping testhost -i ../../inventory "$@"
