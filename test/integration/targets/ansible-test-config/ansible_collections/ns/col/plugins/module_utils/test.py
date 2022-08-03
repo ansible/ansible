6034b91640a3e2e395d6bfbc801539b9dbd75a5c
Matt Clay <matt@mystile.com>
2022-08-02 22:20:21 -0700

    ansible-test - Parse content config only once. (#78418)

---
+++ test/integration/targets/ansible-test-config/ansible_collections/ns/col/plugins/module_utils/test.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+
+def version_to_str(value):
+    return '.'.join(str(v) for v in value)
+
+
+controller_min_python_version = tuple(int(v) for v in os.environ['ANSIBLE_CONTROLLER_MIN_PYTHON_VERSION'].split('.'))
+current_python_version = sys.version_info[:2]
+
+if current_python_version < controller_min_python_version:
+    raise Exception('Current Python version %s is lower than the minimum controller Python version of %s. '
+                    'Did the collection config get ignored?' % (version_to_str(current_python_version), version_to_str(controller_min_python_version)))
