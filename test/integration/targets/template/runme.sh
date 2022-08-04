6c5e577ee30154325dfde5d47a82afe7ab3e8e8f
Brian Coca <brian.coca+git@gmail.com>
2022-08-04 15:36:05 -0400

    ensure they exist

--- test/integration/targets/template/runme.sh
+++ test/integration/targets/template/runme.sh
@@ -48,6 +48,7 @@ ansible-playbook undefined_in_import.yml -i ../../inventory -v "$@"
 # ensure diff null configs work #76493
 for badcfg in "badnull1" "badnull2"
 do
+	[ -f "./${badcfg}.cfg" ]
 	ANSIBLE_CONFIG="./${badcfg}.cfg" ansible-config dump --only-changed
 done
 
