3add5e932178799f8409f3255f148260efa7b4ac
Sloane Hertel <19572925+s-hertel@users.noreply.github.com>
2022-08-11 15:08:50 -0400

    Fix apt making changes in check mode (#78496)
    * Don't actually update the cache in check mode
    
    Add tests for updating the cache in check mode
    
    * Don't mark packages as manually installed in check mode and add a test for it
    
    * changelog
    
    * Update test/integration/targets/apt/tasks/apt.yml
    
    * fix test

--- lib/ansible/modules/apt.py
+++ lib/ansible/modules/apt.py
@@ -809,7 +809,7 @@ def install(m, pkgspec, cache, upgrade=False, default_release=None,
         status = True
         data = dict(changed=False)
 
-    if not build_dep:
+    if not build_dep and not m.check_mode:
         mark_installed_manually(m, package_names)
 
     return (status, data)
@@ -1332,7 +1332,8 @@ def main():
 
                     for retry in range(update_cache_retries):
                         try:
-                            cache.update()
+                            if not module.check_mode:
+                                cache.update()
                             break
                         except apt.cache.FetchFailedException as e:
                             err = to_native(e)
@@ -1347,7 +1348,7 @@ def main():
 
                     cache.open(progress=None)
                     mtimestamp, post_cache_update_time = get_updated_cache_time()
-                    if updated_cache_time != post_cache_update_time:
+                    if module.check_mode or updated_cache_time != post_cache_update_time:
                         updated_cache = True
                     updated_cache_time = post_cache_update_time
 
