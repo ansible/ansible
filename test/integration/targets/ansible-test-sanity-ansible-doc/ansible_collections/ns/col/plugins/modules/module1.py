d680fe39682866c500c2bef4faaa885db18ccb14
Matt Clay <matt@mystile.com>
2022-08-11 10:53:23 -0700

    ansible-test - Fix ansible-doc sanity test FQCN. (#78518)

---
+++ test/integration/targets/ansible-test-sanity-ansible-doc/ansible_collections/ns/col/plugins/modules/module1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+DOCUMENTATION = '''
+module: module1
+short_description: Hello test module
+description: Hello test module.
+options: {}
+author:
+  - Ansible Core Team
+'''
+
+EXAMPLES = '''
+- minimal:
+'''
+
+RETURN = ''''''
+
+from ansible.module_utils.basic import AnsibleModule
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec={},
+    )
+
+    module.exit_json()
+
+
+if __name__ == '__main__':
+    main()
