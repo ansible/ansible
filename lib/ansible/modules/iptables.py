b2c06a8cd279bc81211f773b923f58d51a5c02e1
nothing <foo@bar.baz>
2022-08-15 09:36:31 -0400

    Try to fix output parameter order.

--- lib/ansible/modules/iptables.py
+++ lib/ansible/modules/iptables.py
@@ -756,9 +756,6 @@ def construct_rule(params):
     append_param(rule, params['out_interface'], '-o', False)
     append_param(rule, params['fragment'], '-f', False)
     append_param(rule, params['set_counters'], '-c', False)
-    append_param(rule, params['source_port'], '--source-port', False)
-    append_param(rule, params['destination_port'], '--destination-port', False)
-    append_param(rule, params['to_ports'], '--to-ports', False)
     append_param(rule, params['set_dscp_mark'], '--set-dscp', False)
     append_param(
         rule,
@@ -773,13 +770,6 @@ def construct_rule(params):
     elif params['ctstate']:
         append_match(rule, params['ctstate'], 'conntrack')
         append_csv(rule, params['ctstate'], '--ctstate')
-    if 'iprange' in params['match']:
-        append_param(rule, params['src_range'], '--src-range', False)
-        append_param(rule, params['dst_range'], '--dst-range', False)
-    elif params['src_range'] or params['dst_range']:
-        append_match(rule, params['src_range'] or params['dst_range'], 'iprange')
-        append_param(rule, params['src_range'], '--src-range', False)
-        append_param(rule, params['dst_range'], '--dst-range', False)
     if 'set' in params['match']:
         append_param(rule, params['match_set'], '--match-set', False)
         append_match_flag(rule, 'match', params['match_set_flags'], False)
@@ -796,6 +786,13 @@ def construct_rule(params):
     append_match(rule, params['gid_owner'], 'owner')
     append_match_flag(rule, params['gid_owner'], '--gid-owner', True)
     append_param(rule, params['gid_owner'], '--gid-owner', False)
+    if 'iprange' in params['match']:
+        append_param(rule, params['src_range'], '--src-range', False)
+        append_param(rule, params['dst_range'], '--dst-range', False)
+    elif params['src_range'] or params['dst_range']:
+        append_match(rule, params['src_range'] or params['dst_range'], 'iprange')
+        append_param(rule, params['src_range'], '--src-range', False)
+        append_param(rule, params['dst_range'], '--dst-range', False)
     append_match(rule, params['comment'], 'comment')
     append_param(rule, params['comment'], '--comment', False)
     append_param(rule, params['jump'], '-j', False)
@@ -804,6 +801,9 @@ def construct_rule(params):
     if params['jump'] is None:
         append_jump(rule, params['reject_with'], 'REJECT')
     append_param(rule, params['reject_with'], '--reject-with', False)
+    append_param(rule, params['source_port'], '--source-port', False)
+    append_param(rule, params['destination_port'], '--destination-port', False)
+    append_param(rule, params['to_ports'], '--to-ports', False)
     append_param(
         rule,
         params['icmp_type'],
