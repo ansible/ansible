8acd498a57df733526977052c0a10b5f2ea052b9
Matt Clay <matt@mystile.com>
2022-08-25 11:51:59 -0700

    Skip paramiko test on failing platforms. (#78642)
    The paramiko install fails due to the new bcrypt 4.0.0 release.
    
    The test is being skipped on the affected platforms since ansible-core 2.11 will reach end-of-life in November.

--- test/integration/targets/connection_paramiko_ssh/runme.sh
+++ test/integration/targets/connection_paramiko_ssh/runme.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+source /etc/os-release
+
+if [ "${ID}" == "alpine" ]; then
+  echo "skipping due to issues installing paramiko"
+  exit 0
+fi
+
 set -eux
 
 source ../setup_paramiko/setup.sh
