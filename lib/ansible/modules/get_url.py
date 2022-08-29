5afc5b5d799230b34c3590545f3043e2952aafaf
Matt Martz <matt@sivel.net>
2022-08-29 09:32:21 -0500

    Expand documentation for ciphers

--- lib/ansible/modules/get_url.py
+++ lib/ansible/modules/get_url.py
@@ -29,6 +29,11 @@ options:
   ciphers:
     description:
       - SSL/TLS Ciphers to use for the request
+      ->
+        When a list is provided, all ciphers are joined in order with C(:)
+      - See the L(OpenSSL Cipher List Format,https://www.openssl.org/docs/manmaster/man1/openssl-ciphers.html#CIPHER-LIST-FORMAT)
+        for more details.
+      - The available ciphers is dependent on the Python and OpenSSL/LibreSSL versions
     type: list
     elements: str
     version_added: '2.14'
--- lib/ansible/modules/uri.py
+++ lib/ansible/modules/uri.py
@@ -19,7 +19,12 @@ version_added: "1.1"
 options:
   ciphers:
     description:
-      - SSL/TLS Ciphers to use for the request
+      - SSL/TLS Ciphers to use for the request.
+      ->
+        When a list is provided, all ciphers are joined in order with C(:)
+      - See the L(OpenSSL Cipher List Format,https://www.openssl.org/docs/manmaster/man1/openssl-ciphers.html#CIPHER-LIST-FORMAT)
+        for more details.
+      - The available ciphers is dependent on the Python and OpenSSL/LibreSSL versions
     type: list
     elements: str
     version_added: '2.14'
