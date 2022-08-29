5b3c1ddd6ef59b6960e530762a4c9112faa1d1ba
Matt Martz <matt@sivel.net>
2022-08-29 11:10:33 -0500

    Adjust YAML formatting

--- lib/ansible/modules/uri.py
+++ lib/ansible/modules/uri.py
@@ -20,8 +20,7 @@ options:
   ciphers:
     description:
       - SSL/TLS Ciphers to use for the request.
-      ->
-        When a list is provided, all ciphers are joined in order with C(:)
+      - 'When a list is provided, all ciphers are joined in order with C(:)'
       - See the L(OpenSSL Cipher List Format,https://www.openssl.org/docs/manmaster/man1/openssl-ciphers.html#CIPHER-LIST-FORMAT)
         for more details.
       - The available ciphers is dependent on the Python and OpenSSL/LibreSSL versions
