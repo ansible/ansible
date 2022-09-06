f20b5a9312694a7635b865240accea392e59f4b8
Matt Martz <matt@sivel.net>
2022-09-06 12:35:22 -0500

    Consolidate old and new tests into one

--- test/units/module_utils/urls/test_fetch_file.py
+++ test/units/module_utils/urls/test_fetch_file.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# (c) 2018 Matt Martz <matt@sivel.net>
+# Copyright: Contributors to the Ansible project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
@@ -18,21 +18,9 @@ class FakeTemporaryFile:
         self.name = name
 
 
-def test_fetch_file(mocker):
-    tempfile = mocker.patch('ansible.module_utils.urls.tempfile')
-    tempfile.NamedTemporaryFile.side_effect = RuntimeError('boom')
-
-    module = MagicMock()
-    module.tmpdir = '/tmp'
-
-    with pytest.raises(RuntimeError):
-        fetch_file(module, 'http://ansible.com/foo.tar.gz?foo=%s' % ('bar' * 100))
-
-    tempfile.NamedTemporaryFile.assert_called_once_with(dir='/tmp', prefix='foo.tar', suffix='.gz', delete=False)
-
-
 @pytest.mark.parametrize(
     'url, prefix, suffix, expected', (
+        ('http://ansible.com/foo.tar.gz?foo=%s' % ('bar' * 100), 'foo', '.tar.gz', 'foo.tar.gz'),
         ('https://www.gnu.org/licenses/gpl-3.0.txt', 'gpl-3.0', '.txt', 'gpl-3.0.txt'),
         ('http://pyyaml.org/download/libyaml/yaml-0.2.5.tar.gz', 'yaml-0.2.5', '.tar.gz', 'yaml-0.2.5.tar.gz'),
         (
@@ -47,11 +35,11 @@ def test_file_multiple_extensions(mocker, url, prefix, suffix, expected):
     module = mocker.Mock()
     module.tmpdir = '/tmp'
     module.add_cleanup_file = mocker.Mock(side_effect=AttributeError('raised intentionally'))
-    
+
     mock_NamedTemporaryFile = mocker.patch('ansible.module_utils.urls.tempfile.NamedTemporaryFile',
                                            return_value=FakeTemporaryFile(os.path.join(module.tmpdir, expected)))
-                                           
+
     with pytest.raises(AttributeError, match='raised intentionally'):
         fetch_file(module, url)
-                                           
+
     mock_NamedTemporaryFile.assert_called_with(dir=module.tmpdir, prefix=prefix, suffix=suffix, delete=False)
