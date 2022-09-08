7dac2d9c2e30035f83dc6606cb5d96671de091ad
Brian Coca <brian.coca+git@gmail.com>
2022-09-08 15:53:06 -0400

    betah testing

--- test/integration/targets/vault/runme.sh
+++ test/integration/targets/vault/runme.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eux
+set -euxv
 
 # 'real script'
 ansible-playbook realpath.yml "$@" --vault-password-file script/vault-secret.sh
@@ -10,16 +10,18 @@ ansible-playbook symlink.yml "$@" --vault-password-file symlink/get-password-sym
 
 ### NEGATIVE TESTS
 
+ER='Attempting to decrypt'
 #### no secrets
 # 'real script'
-ansible-playbook realpath.yml "$@" || /bin/true
+ansible-playbook realpath.yml "$@" 2>&1 |grep "${ER}"
 
 # using symlink
-ansible-playbook symlink.yml "$@" || /bin/true
+ansible-playbook symlink.yml "$@" 2>&1 |grep "${ER}"
 
+ER='Decryption failed'
 ### wrong secrets
 # 'real script'
-ansible-playbook realpath.yml "$@" --vault-password-file symlink/get-password-symlink || /bin/true
+ansible-playbook realpath.yml "$@" --vault-password-file symlink/get-password-symlink 2>&1 |grep "${ER}"
 
 # using symlink
-ansible-playbook symlink.yml "$@" --vault-password-file script/vault-secret.sh || /bin/true
+ansible-playbook symlink.yml "$@" --vault-password-file script/vault-secret.sh 2>&1 |grep "${ER}"
