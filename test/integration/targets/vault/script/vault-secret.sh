f348ce5ff1f82188b4106276534db9576253298d
Brian Coca <brian.coca+git@gmail.com>
2022-09-08 10:37:12 -0400

    ignore shellcheck

--- test/integration/targets/vault/script/vault-secret.sh
+++ test/integration/targets/vault/script/vault-secret.sh
@@ -2,7 +2,10 @@
 
 set -eu
 
+# shellcheck disable=SC2086
 basename="$(basename $0)"
+# shellcheck disable=SC2046
+# shellcheck disable=SC2086
 dirname="$(basename $(dirname $0))"
 basename_prefix="get-password"
 default_password="foo-bar"
@@ -17,3 +20,7 @@ case "${basename}" in
 esac
 
 echo "${password}_${dirname}"
+
+06:24 ERROR: test/integration/targets/vault/script/vault-secret.sh:5:22: SC2086: Double quote to prevent globbing and word splitting. (100%)
+06:24 ERROR: test/integration/targets/vault/script/vault-secret.sh:6:21: SC2046: Quote this to prevent word splitting. (100%)
+06:24 ERROR: test/integration/targets/vault/script/vault-secret.sh:6:31: SC2086
