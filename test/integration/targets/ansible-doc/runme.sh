e7e4ffffbd38b24250a7b17492d9acbe3df076f3
s-hertel <19572925+s-hertel@users.noreply.github.com>
2022-09-12 10:09:59 -0400

    fix quotes

--- test/integration/targets/ansible-doc/runme.sh
+++ test/integration/targets/ansible-doc/runme.sh
@@ -200,11 +200,11 @@ pyc_2="$(pwd)/library/notaplugin.pyc"
 trap 'rm -rf "$pyc_1" "$pyc_2"' EXIT
 
 # test pyc files are not used as adjacent documentation
-python -c "import py_compile; py_compile.compile('"$pyc_src"', cfile='"$pyc_1"')"
+python -c "import py_compile; py_compile.compile('$pyc_src', cfile='$pyc_1')"
 ansible-doc -t filter split --playbook-dir ./ |grep histerical
 
 # test pyc files are not listed as plugins
-python -c "import py_compile; py_compile.compile('"$pyc_src"', cfile='"$pyc_2"')"
+python -c "import py_compile; py_compile.compile('$pyc_src', cfile='$pyc_2')"
 test "$(ansible-doc -l -t module --playbook-dir ./ 2>&1 1>/dev/null |grep -c "notaplugin")" == 0
 
 # without playbook dir, builtin should return
