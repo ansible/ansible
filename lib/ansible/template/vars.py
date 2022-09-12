db3699d094905f01dc00b7ba5b1596ef7026ea35
Martin Krizek <martin.krizek@gmail.com>
2022-09-12 12:54:24 +0200

    Unused import

--- lib/ansible/template/vars.py
+++ lib/ansible/template/vars.py
@@ -2,7 +2,6 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from collections import ChainMap
-from collections.abc import Mapping
 
 from jinja2.utils import missing
 
