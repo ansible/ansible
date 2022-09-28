8877691ca0955c6cf64f5af4d3461591aa2ea8d5
Zhanibek Adilbekov <zhanibek.adilbekov@proton.me>
2022-09-28 06:44:38 +0600

    feat(vault): use config system to get ansible vault editor

--- lib/ansible/parsing/vault/__init__.py
+++ lib/ansible/parsing/vault/__init__.py
@@ -1123,7 +1123,7 @@ class VaultEditor:
             os.chown(dest, prev.st_uid, prev.st_gid)
 
     def _editor_shell_command(self, filename):
-        env_editor = os.environ.get('ANSIBLE_EDITOR', os.environ.get('EDITOR', 'vi'))
+        env_editor = getattr(C, 'ANSIBLE_VAULT_EDITOR') or os.environ.get('EDITOR', 'vi')
         editor = shlex.split(env_editor)
         editor.append(filename)
 
