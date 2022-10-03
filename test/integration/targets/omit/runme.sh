2959f2b6f210aba6a45f3353e8ae53e7951cfee2
Brian Coca <brian.coca+git@gmail.com>
2022-10-03 12:55:23 -0400

    fixed functions and now better tests

--- test/integration/targets/omit/runme.sh
+++ test/integration/targets/omit/runme.sh
@@ -2,4 +2,10 @@
 
 set -eux
 
+# positive inheritance works
 ANSIBLE_ROLES_PATH=../ ansible-playbook 48673.yml 75692.yml  -i ../../inventory -v "$@"
+
+# ensure negative also works
+ansible-playbook -C C75692.yml -i ../../inventory -v "$@"
+ansible-playbook C75692.yml -i ../../inventory -v "$@"  # this actuall creates 'foo'
+ansible-playbook -C C75692.yml -i ../../inventory -v "$@"
