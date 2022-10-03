3775cd12fa7fc4ab91eedd9c8653c797e42dcfc6
Brian Coca <brian.coca+git@gmail.com>
2022-10-03 13:06:27 -0400

    commentary

--- test/integration/targets/omit/runme.sh
+++ test/integration/targets/omit/runme.sh
@@ -6,6 +6,6 @@ set -eux
 ANSIBLE_ROLES_PATH=../ ansible-playbook 48673.yml 75692.yml  -i ../../inventory -v "$@"
 
 # ensure negative also works
-ansible-playbook -C C75692.yml -i ../../inventory -v "$@"
-ansible-playbook C75692.yml -i ../../inventory -v "$@"  # this actuall creates 'foo'
-ansible-playbook -C C75692.yml -i ../../inventory -v "$@"
+ansible-playbook -C C75692.yml -i ../../inventory -v "$@"	# expects 'foo' not to exist
+ansible-playbook C75692.yml -i ../../inventory -v "$@"		# creates 'foo'
+ansible-playbook -C C75692.yml -i ../../inventory -v "$@"	# expects 'foo' does exist
