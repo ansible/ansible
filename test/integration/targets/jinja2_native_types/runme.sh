86f394845ecef8d3b4322315b85b4bed71426858
Martin Krizek <martin.krizek@gmail.com>
2022-10-12 19:16:06 +0200

    jinja2_native: preserve quotes in strings (#79119)
    Fixes #79083

--- test/integration/targets/jinja2_native_types/runme.sh
+++ test/integration/targets/jinja2_native_types/runme.sh
@@ -7,4 +7,5 @@ ansible-playbook runtests.yml -v "$@"
 ansible-playbook --vault-password-file test_vault_pass test_vault.yml -v "$@"
 ansible-playbook test_hostvars.yml -v "$@"
 ansible-playbook nested_undefined.yml -v "$@"
+ansible-playbook test_preserving_quotes.yml -v "$@"
 unset ANSIBLE_JINJA2_NATIVE
