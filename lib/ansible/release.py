f62824e425d25a7d4db4d927e308f32048b91753
Ricardo Carrillo Cruz <ricarril@redhat.com>
2022-10-17 17:59:28 +0200

    Update Ansible release version to v2.14.0rc1.post0.

--- lib/ansible/release.py
+++ lib/ansible/release.py
@@ -19,6 +19,6 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-__version__ = '2.14.0rc1'
+__version__ = '2.14.0rc1.post0'
 __author__ = 'Ansible, Inc.'
 __codename__ = "C'mon Everybody"
