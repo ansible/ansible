227d6c7c29206b44f4d5f7bc2519583763f4d79f
seachanged <dll@sonic.net>
2022-11-17 12:48:50 -0800

    Update fileglob.py (#78887)
    Improve documentation for relative file paths

--- lib/ansible/plugins/lookup/fileglob.py
+++ lib/ansible/plugins/lookup/fileglob.py
@@ -18,6 +18,7 @@ DOCUMENTATION = """
         required: True
     notes:
       - Patterns are only supported on files, not directory/paths.
+      - See R(Ansible task paths,playbook_task_paths) to understand how file lookup occurs with paths.
       - Matching is against local system files on the Ansible controller.
         To iterate a list of files on a remote node, use the M(ansible.builtin.find) module.
       - Returns a string list of paths joined by commas, or an empty list if no files match. For a 'true list' pass C(wantlist=True) to the lookup.
