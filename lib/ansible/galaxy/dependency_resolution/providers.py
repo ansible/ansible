9548a9fd2b4596fdf4b83f9a7ad80a58e3f1f787
Wong Hoi Sing Edison <hswong3i@gmail.com>
2022-11-23 21:57:24 +0800

    ansible-galaxy - support ``resolvelib >= 0.5.3, < 0.10.0`` (#79399)
    * Upgrade `resolvelib >= 0.5.3, < 0.10.0`
    
    https://pypi.org/project/resolvelib/0.9.0/ released on 2022-11-17:
    
      * https://github.com/sarugaku/resolvelib/blob/master/CHANGELOG.rst#090-2022-11-17
      * https://github.com/sarugaku/resolvelib/releases/tag/0.9.0
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- lib/ansible/galaxy/dependency_resolution/providers.py
+++ lib/ansible/galaxy/dependency_resolution/providers.py
@@ -42,7 +42,7 @@ except ImportError:
 
 # TODO: add python requirements to ansible-test's ansible-core distribution info and remove the hardcoded lowerbound/upperbound fallback
 RESOLVELIB_LOWERBOUND = SemanticVersion("0.5.3")
-RESOLVELIB_UPPERBOUND = SemanticVersion("0.9.0")
+RESOLVELIB_UPPERBOUND = SemanticVersion("0.10.0")
 RESOLVELIB_VERSION = SemanticVersion.from_loose_version(LooseVersion(resolvelib_version))
 
 
@@ -220,7 +220,7 @@ class CollectionDependencyProviderBase(AbstractProvider):
             Mapping of identifier, list of named tuple pairs.
             The named tuples have the entries ``requirement`` and ``parent``.
 
-        resolvelib >=0.8.0, <= 0.8.1
+        resolvelib >=0.8.0, <= 0.9.0
 
         :param identifier: The value returned by ``identify()``.
 
