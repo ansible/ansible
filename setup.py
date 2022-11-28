b7c3b6092be407335bca726366bb28520c93a0f5
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2022-11-28 16:16:46 +0100

    Set explicit meta `long_description_content_type` (#79468)
    This is necessary for `twine check --strict` to pass.
    
    (cherry picked from commit 80551633c7b4ed782c293b09b04d77d8385614c4)

--- setup.py
+++ setup.py
@@ -319,6 +319,7 @@ static_setup_params = dict(
     name='ansible-core',
     version=__version__,
     description='Radically simple IT automation',
+    long_description_content_type='text/x-rst',
     author=__author__,
     author_email='info@ansible.com',
     url='https://ansible.com/',
