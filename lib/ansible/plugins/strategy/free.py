70066c090f3cd089e9ca2141ea7ae59a9f56c998
Matt Martz <matt@sivel.net>
2022-11-29 15:56:41 -0600

    Always create new role (#78661)
    Don't use role cache for determining whether to create a new instance of role

--- lib/ansible/plugins/strategy/free.py
+++ lib/ansible/plugins/strategy/free.py
@@ -173,10 +173,9 @@ class StrategyModule(StrategyBase):
 
                         # check to see if this task should be skipped, due to it being a member of a
                         # role which has already run (and whether that role allows duplicate execution)
-                        if not isinstance(task, Handler) and task._role and task._role.has_run(host):
-                            # If there is no metadata, the default behavior is to not allow duplicates,
-                            # if there is metadata, check to see if the allow_duplicates flag was set to true
-                            if task._role._metadata is None or task._role._metadata and not task._role._metadata.allow_duplicates:
+                        if not isinstance(task, Handler) and task._role:
+                            role_obj = self._get_cached_role(task, iterator._play)
+                            if role_obj.has_run(host) and role_obj._metadata.allow_duplicates is False:
                                 display.debug("'%s' skipped because role has already run" % task, host=host_name)
                                 del self._blocked_hosts[host_name]
                                 continue
