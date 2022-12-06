3ae796bdcfc48f2f96aef0e784d38d39741d2f01
Matt Clay <matt@mystile.com>
2022-12-06 14:03:34 -0800

    [stable-2.14] ansible-test - Improve container management. (#78550) (#79507)
    * [stable-2.14] ansible-test - Improve container management. (#78550)
    
    See changelogs/fragments/ansible-test-container-management.yml for details..
    (cherry picked from commit cda16cc5e9aa8703fb4e1ac0a0be6b631d9076cc)
    
    Co-authored-by: Matt Clay <matt@mystile.com>
    
    * ansible-test - Fix container detection. (#79530)
    
    (cherry picked from commit 80d2f8da02052f64396da6b8caaf820eedbf18e2)

--- test/lib/ansible_test/_internal/cli/argparsing/parsers.py
+++ test/lib/ansible_test/_internal/cli/argparsing/parsers.py
@@ -287,6 +287,19 @@ class ChoicesParser(DynamicChoicesParser):
         return '|'.join(self.choices)
 
 
+class EnumValueChoicesParser(ChoicesParser):
+    """Composite argument parser which relies on a static list of choices derived from the values of an enum."""
+    def __init__(self, enum_type: t.Type[enum.Enum], conditions: MatchConditions = MatchConditions.CHOICE) -> None:
+        self.enum_type = enum_type
+
+        super().__init__(choices=[str(item.value) for item in enum_type], conditions=conditions)
+
+    def parse(self, state: ParserState) -> t.Any:
+        """Parse the input from the given state and return the result."""
+        value = super().parse(state)
+        return self.enum_type(value)
+
+
 class IntegerParser(DynamicChoicesParser):
     """Composite argument parser for integers."""
     PATTERN = re.compile('^[1-9][0-9]*$')
