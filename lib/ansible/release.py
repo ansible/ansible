ed924d64935b15341731544af6a03b55e9739741
jamesmarshall24 <james.marshall@redhat.com>
2022-12-06 15:24:59 -0500

    New release v2.14.1 (#79550)

--- lib/ansible/release.py
+++ lib/ansible/release.py
@@ -19,6 +19,6 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-__version__ = '2.14.1rc1.post0'
+__version__ = '2.14.1'
 __author__ = 'Ansible, Inc.'
 __codename__ = "C'mon Everybody"
