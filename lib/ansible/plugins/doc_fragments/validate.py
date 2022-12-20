c31d248835fc8fc88aa8ec1d122b65d0762fc28f
Rune Juhl Jacobsen <rune@juhljacobsen.dk>
2022-12-20 20:23:11 +0100

    Fix broken reference to FAQ

--- lib/ansible/plugins/doc_fragments/validate.py
+++ lib/ansible/plugins/doc_fragments/validate.py
@@ -15,7 +15,8 @@ options:
     - The validation command to run before copying the updated file into the final destination.
     - A temporary file path is used to validate, passed in through '%s' which must be present as in the examples below.
     - Also, the command is passed securely so shell features such as expansion and pipes will not work.
-    - For an example on how to handle more complex validation than what this option provides,
-      see L(Complex configuration validation,https://docs.ansible.com/ansible/devel/reference_appendices/faq.html).
+    - For an example on how to handle more complex validation than what this
+      option provides, see L("The 'validate' option is not enough for my needs, what do I
+      do?",https://docs.ansible.com/ansible/devel/reference_appendices/faq.html#the-validate-option-is-not-enough-for-my-needs-what-do-i-do).
     type: str
 '''
