5d47c40cdff45d75105f478a5ab53233773067b9
Felix Fontein <felix@fontein.de>
2023-01-09 18:25:56 +0100

    Remove notes.

--- lib/ansible/plugins/action/normal.py
+++ lib/ansible/plugins/action/normal.py
@@ -33,7 +33,6 @@ class ActionModule(ActionBase):
         result = super(ActionModule, self).run(tmp, task_vars)
         del tmp  # tmp no longer has any effect
 
-        # FUTURE: better to let _execute_module calculate this internally?
         wrap_async = self._task.async_val and not self._connection.has_native_async
 
         # do work!
@@ -41,7 +40,6 @@ class ActionModule(ActionBase):
 
         # hack to keep --verbose from showing all the setup module result
         # moved from setup module as now we filter out all _ansible_ from result
-        # FIXME: is this still accurate with gather_facts etc, or does it need support for FQ and other names?
         if self._task.action in C._ACTION_SETUP:
             result['_ansible_verbose_override'] = True
 
