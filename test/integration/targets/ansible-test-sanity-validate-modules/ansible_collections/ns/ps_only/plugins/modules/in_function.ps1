8e9834fde5e51becdf5a9a8b76671cf3f1ce392c
Jordan Borean <jborean93@gmail.com>
2023-01-11 08:05:50 +1000

    ansible-test - fix ps argspec check inside cmdlet (#79699)
    * ansible-test - fix ps argspec check inside cmdlet
    
    * Added error condition test
    
    * Fix sanity problem
    
    (cherry picked from commit ee33be9484b2240aa7b6b3bfdb30b452b6048e85)

---
+++ test/integration/targets/ansible-test-sanity-validate-modules/ansible_collections/ns/ps_only/plugins/modules/in_function.ps1
@@ -0,0 +1,7 @@
+#!powershell
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#AnsibleRequires -CSharpUtil Ansible.Basic
+#AnsibleRequires -PowerShell ..module_utils.share_module
+
+Invoke-AnsibleModule
