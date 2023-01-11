3d3a70274d760b0043a512cf213b364a40df8a16
Felix Fontein <felix@fontein.de>
2023-01-11 15:07:00 +0100

    normal action plugin: remove superfluous code (#79690)
    * Remove obsolete 'if'
    
    The result dict returned by super.run() will never have a 'skipped' entry.
    Without the if, it is more clear that wrap_async is actually defined at the end of the function.
    
    * Remove more dead code.
    
    The result dict will also not contain invocation.
    
    * Remove notes.

--- lib/ansible/plugins/action/normal.py
+++ lib/ansible/plugins/action/normal.py
@@ -33,24 +33,15 @@ class ActionModule(ActionBase):
         result = super(ActionModule, self).run(tmp, task_vars)
         del tmp  # tmp no longer has any effect
 
-        if not result.get('skipped'):
+        wrap_async = self._task.async_val and not self._connection.has_native_async
 
-            if result.get('invocation', {}).get('module_args'):
-                # avoid passing to modules in case of no_log
-                # should not be set anymore but here for backwards compatibility
-                del result['invocation']['module_args']
+        # do work!
+        result = merge_hash(result, self._execute_module(task_vars=task_vars, wrap_async=wrap_async))
 
-            # FUTURE: better to let _execute_module calculate this internally?
-            wrap_async = self._task.async_val and not self._connection.has_native_async
-
-            # do work!
-            result = merge_hash(result, self._execute_module(task_vars=task_vars, wrap_async=wrap_async))
-
-            # hack to keep --verbose from showing all the setup module result
-            # moved from setup module as now we filter out all _ansible_ from result
-            # FIXME: is this still accurate with gather_facts etc, or does it need support for FQ and other names?
-            if self._task.action in C._ACTION_SETUP:
-                result['_ansible_verbose_override'] = True
+        # hack to keep --verbose from showing all the setup module result
+        # moved from setup module as now we filter out all _ansible_ from result
+        if self._task.action in C._ACTION_SETUP:
+            result['_ansible_verbose_override'] = True
 
         if not wrap_async:
             # remove a temporary path we created
