9ac9de4770b9f2c3ac3aa550636cac8cc7b6f82f
Felix Fontein <felix@fontein.de>
2023-01-14 09:24:47 +0100

    Also test deprecation in list of dicts.

--- test/integration/targets/argspec/library/argspec.py
+++ test/integration/targets/argspec/library/argspec.py
@@ -204,6 +204,41 @@ def main():
                         'removed_from_collection': 'foo.bar',
                     },
                 },
+            },
+            'subdeprecation_list': {
+                'type': 'list',
+                'elements': 'dict',
+                'options': {
+                    'deprecation_aliases': {
+                        'type': 'str',
+                        'aliases': [
+                            'deprecation_aliases_version',
+                            'deprecation_aliases_date',
+                        ],
+                        'deprecated_aliases': [
+                            {
+                                'name': 'deprecation_aliases_version',
+                                'version': '2.0.0',
+                                'collection_name': 'foo.bar',
+                            },
+                            {
+                                'name': 'deprecation_aliases_date',
+                                'date': '2023-01-01',
+                                'collection_name': 'foo.bar',
+                            },
+                        ],
+                    },
+                    'deprecation_param_version': {
+                        'type': 'str',
+                        'removed_in_version': '2.0.0',
+                        'removed_from_collection': 'foo.bar',
+                    },
+                    'deprecation_param_date': {
+                        'type': 'str',
+                        'removed_at_date': '2023-01-01',
+                        'removed_from_collection': 'foo.bar',
+                    },
+                },
             }
         },
         required_if=(
