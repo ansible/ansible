bc6649571d29cff3bef8b4ff0cb37b22c3f5338b
Matt Clay <matt@mystile.com>
2023-01-23 14:55:04 -0800

    ansible-test - Add assert, casts to assist mypy.

--- test/lib/ansible_test/_internal/completion.py
+++ test/lib/ansible_test/_internal/completion.py
@@ -276,7 +276,9 @@ def filter_completion(
 ) -> dict[str, TCompletionConfig]:
     """Return the given completion dictionary, filtering out configs which do not support the controller if controller_only is specified."""
     if controller_only:
-        completion = {name: config for name, config in completion.items() if isinstance(config, PosixCompletionConfig) and config.controller_supported}
+        # The cast is needed because mypy gets confused here and forgets that completion values are TCompletionConfig.
+        completion = {name: t.cast(TCompletionConfig, config) for name, config in completion.items() if
+                      isinstance(config, PosixCompletionConfig) and config.controller_supported}
 
     if not include_defaults:
         completion = {name: config for name, config in completion.items() if not config.is_default}
