ba0461222b3988b56656a2913aacfea8ab84c32a
Matt Clay <matt@mystile.com>
2023-01-30 16:19:06 -0800

    Test Windows Azure instances. Do not merge.
    ci_complete

--- test/utils/shippable/shippable.sh
+++ test/utils/shippable/shippable.sh
@@ -20,6 +20,7 @@ docker ps
 
 export PATH="${PWD}/bin:${PATH}"
 export PYTHONIOENCODING='utf-8'
+export P=azure
 
 if [ -n "${COVERAGE:-}" ]; then
     # on-demand coverage reporting triggered by setting the COVERAGE environment variable to a non-empty value
