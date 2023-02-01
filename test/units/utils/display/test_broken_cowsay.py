75b64c76f5745cd42e495799a821e881594ed69e
Matt Clay <matt@mystile.com>
2023-02-01 13:35:05 -0800

    Fix cowsay test
    - The test no longer depends on another test to initialize config.
    - Also remove unreachable code.

--- test/units/utils/display/test_broken_cowsay.py
+++ test/units/utils/display/test_broken_cowsay.py
@@ -12,16 +12,13 @@ from unittest.mock import MagicMock
 
 
 def test_display_with_fake_cowsay_binary(capsys, mocker):
+    display = Display()
+
     mocker.patch("ansible.constants.ANSIBLE_COW_PATH", "./cowsay.sh")
 
-    def mock_communicate(input=None, timeout=None):
-        return b"", b""
-
     mock_popen = MagicMock()
-    mock_popen.return_value.communicate = mock_communicate
     mock_popen.return_value.returncode = 1
     mocker.patch("subprocess.Popen", mock_popen)
 
-    display = Display()
     assert not hasattr(display, "cows_available")
     assert display.b_cowsay is None
