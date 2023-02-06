ad198fad72ac2da92804226794978089b9ca901b
Tomas Kopal <Tomas.Kopal@eccam.com>
2023-02-06 16:13:09 +0100

    Add integration tests for encrypted fetch

--- test/integration/targets/fetch/runme.sh
+++ test/integration/targets/fetch/runme.sh
@@ -13,11 +13,15 @@ trap 'cleanup "$@"' EXIT
 # setup required roles
 ln -s ../../setup_remote_tmp_dir roles/setup_remote_tmp_dir
 
+# create vault password files
+echo "secret" > vault_pass
+echo "secret1" > vault_pass1
+
 # run old type role tests
-ansible-playbook -i ../../inventory run_fetch_tests.yml -e "output_dir=${OUTPUT_DIR}" "$@"
+ansible-playbook -i ../../inventory --vault-id default@vault_pass --vault-id vault1@vault_pass1 run_fetch_tests.yml -e "output_dir=${OUTPUT_DIR}" "$@"
 
 # run same test with become
-ansible-playbook -i ../../inventory run_fetch_tests.yml -e "output_dir=${OUTPUT_DIR}" -b "$@"
+ansible-playbook -i ../../inventory --vault-id default@vault_pass --vault-id vault1@vault_pass1 run_fetch_tests.yml -e "output_dir=${OUTPUT_DIR}" -b "$@"
 
 # run tests to avoid path injection from slurp when fetch uses become
 ansible-playbook -i ../../inventory injection/avoid_slurp_return.yml -e "output_dir=${OUTPUT_DIR}" "$@"
