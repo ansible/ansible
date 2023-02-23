d746984d968bd7283c4c860c560e87dc79a736c6
Martin Krizek <martin.krizek@gmail.com>
2023-02-23 10:04:27 +0100

    fix sanity

--- lib/ansible/playbook/notifiable.py
+++ lib/ansible/playbook/notifiable.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
- # Copyright The Ansible project
- # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# Copyright The Ansible project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from ansible.playbook.attribute import FieldAttribute
 
