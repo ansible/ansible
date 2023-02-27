2993bbf455e0011ad8c0812f07a4e48f56cd1a2a
Mark Chappell <mchappel@redhat.com>
2023-02-27 22:23:40 +0100

    sanity tests - ignore pre-release versions for deprecation comparisons (#79185)

---
+++ test/integration/targets/ansible-test-sanity-pylint/ansible_collections/ns/col/plugins/lookup/deprecated.py
@@ -0,0 +1,22 @@
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+DOCUMENTATION = '''
+name: deprecated
+short_description: lookup
+description: Lookup.
+author:
+  - Ansible Core Team
+'''
+
+EXAMPLES = '''#'''
+RETURN = '''#'''
+
+from ansible.plugins.lookup import LookupBase
+
+
+class LookupModule(LookupBase):
+    def run(self, **kwargs):
+        return []
