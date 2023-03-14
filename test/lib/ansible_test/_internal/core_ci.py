8e2148943b84d9cf9314c35bcd0035b48a04070f
Matt Clay <matt@mystile.com>
2023-03-14 15:56:21 -0700

    [stable-2.13] ansible-test - Fix file permissions for delegation (#80204)
    * [stable-2.13] ansible-test - Fix file permissions for delegation (#79932)
    
    * ansible-test - Fix file permissions for delegation
    
    * Set more restrictive permissions for SSH key
    
    * Check all execute bits, not just owner
    
    * Add a breaking_changes changelog entry.
    (cherry picked from commit c8c1402ff66cf971469b7d49ada9fde894dabe0d)
    
    Co-authored-by: Matt Clay <matt@mystile.com>
    
    * ansible-test - Fix collection delegation (#79947)
    
    (cherry picked from commit 079383384790310dd6722b08ac18990e2a4d0ed9)
    
    ---------
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- test/lib/ansible_test/_internal/core_ci.py
+++ test/lib/ansible_test/_internal/core_ci.py
@@ -6,6 +6,7 @@ import dataclasses
 import json
 import os
 import re
+import stat
 import traceback
 import uuid
 import errno
@@ -47,6 +48,7 @@ from .ci import (
 
 from .data import (
     data_context,
+    PayloadConfig,
 )
 
 
@@ -447,13 +449,18 @@ class SshKey:
         key, pub = key_pair
         key_dst, pub_dst = self.get_in_tree_key_pair_paths()
 
-        def ssh_key_callback(files):  # type: (t.List[t.Tuple[str, str]]) -> None
+        def ssh_key_callback(payload_config: PayloadConfig) -> None:
             """
             Add the SSH keys to the payload file list.
             They are either outside the source tree or in the cache dir which is ignored by default.
             """
+            files = payload_config.files
+            permissions = payload_config.permissions
+
             files.append((key, os.path.relpath(key_dst, data_context().content.root)))
             files.append((pub, os.path.relpath(pub_dst, data_context().content.root)))
+
+            permissions[os.path.relpath(key_dst, data_context().content.root)] = stat.S_IRUSR | stat.S_IWUSR
 
         data_context().register_payload_callback(ssh_key_callback)
 
