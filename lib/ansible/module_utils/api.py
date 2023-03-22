1a9ef232ee7a31295836dcb20939a549501e0903
Matt Martz <matt@sivel.net>
2023-03-22 11:04:56 -0500

    ansible-galaxy collection install retry improvements (#80180)
    * clog frag
    
    * Fix retries so that each explicit call to _call_galaxy is retried for the correct number of attempts. Fixes #80174
    
    * Extend retry logic to common URL related connection errors. Fixes #80170
    
    * Extend retries to downloading artifacts
    
    * Extend param docs for change
    
    * Rework the exception handling
    
    * Don't be overly broad, reduce to TimeoutError, and BadStatusLine for now
    
    * _download_file needs to raise AnsibleError.orig_exc
    
    * Remove unused import
    
    * Add IncompleteRead
    
    * Add socket.timeout for py39
    
    * Add 502 to retry codes
    
    * Move http error code checking first
    
    * Use itertools.tee to replay the backoff_iterator instead of using a callable
    
    * Actually set a CLI default of 60s for timeout, to prevent implicit galaxy from using 10s as default from Request.open
    
    * Import typing
    
    * fix type hints
    
    * Use http.HTTPStatus instead of int HTTP error codes where feasible
    
    * Split exception handling
    
    Co-authored-by: Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>
    
    * Add missing import
    
    ---------
    
    Co-authored-by: Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>

--- lib/ansible/module_utils/api.py
+++ lib/ansible/module_utils/api.py
@@ -26,10 +26,14 @@ The 'api' module provides the following common argument specs:
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import copy
 import functools
+import itertools
 import random
 import sys
 import time
+
+import ansible.module_utils.compat.typing as t
 
 
 def rate_limit_argument_spec(spec=None):
@@ -141,6 +145,15 @@ def retry_with_delays_and_condition(backoff_iterator, should_retry_error=None):
     :param backoff_iterator: An iterable of delays in seconds.
     :param should_retry_error: A callable that takes an exception of the decorated function and decides whether to retry or not (returns a bool).
     """
+    def _emit_isolated_iterator_copies(original_iterator):  # type: (t.Iterable[t.Any]) -> t.Generator
+        # Ref: https://stackoverflow.com/a/30232619/595220
+        _copiable_iterator, _first_iterator_copy = itertools.tee(original_iterator)
+        yield _first_iterator_copy
+        while True:
+            yield copy.copy(_copiable_iterator)
+    backoff_iterator_generator = _emit_isolated_iterator_copies(backoff_iterator)
+    del backoff_iterator  # prevent accidental use elsewhere
+
     if should_retry_error is None:
         should_retry_error = retry_never
 
@@ -152,7 +165,7 @@ def retry_with_delays_and_condition(backoff_iterator, should_retry_error=None):
             """
             call_retryable_function = functools.partial(function, *args, **kwargs)
 
-            for delay in backoff_iterator:
+            for delay in next(backoff_iterator_generator):
                 try:
                     return call_retryable_function()
                 except Exception as e:
