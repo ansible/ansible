125e167c8711ec1b43f1c62eb6be48270749db15
Jonathan Kamens <jik@kamens.us>
2023-03-28 21:52:22 -0400

    Add SPDX-License-Identifier to sample module

--- examples/scripts/my_test.py
+++ examples/scripts/my_test.py
@@ -2,6 +2,7 @@
 
 # Copyright (c) 2018, Terry Jones <terry.jones@example.org>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
