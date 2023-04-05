96f7fd5539f6124004fecef8f7110d4965686872
Felix Fontein <felix@fontein.de>
2023-04-06 01:14:35 +0200

    Extend ansible-doc test with more classic markup. (#80407)

--- test/integration/targets/ansible-doc/collections/ansible_collections/testns/testcol/plugins/modules/randommodule.py
+++ test/integration/targets/ansible-doc/collections/ansible_collections/testns/testcol/plugins/modules/randommodule.py
@@ -10,6 +10,10 @@ short_description: A random module
 description:
     - A random module.
     - See O(foo.bar.baz#role:main:foo=bar) for how this is used in the P(foo.bar.baz#role)'s C(main) entrypoint.
+    - See L(the docsite,https://docs.ansible.com/ansible-core/devel/) for more information on ansible-core.
+    - This module is not related to the M(ansible.builtin.copy) module. HORIZONTALLINE You might also be interested
+      in R(ansible_python_interpreter, ansible_python_interpreter).
+    - Sometimes you have M(broken markup) that will result in error messages.
 author:
     - Ansible Core Team
 version_added: 1.0.0
