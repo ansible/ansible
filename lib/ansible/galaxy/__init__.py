8891028078508c1fbcb9f191f79eedaa94ff3c4e
OP (ompragash) <21008429+Ompragash@users.noreply.github.com>
2023-04-06 22:25:41 +0530

    Update lib/ansible/galaxy/__init__.py
    Co-authored-by: Sloane Hertel <19572925+s-hertel@users.noreply.github.com>

--- lib/ansible/galaxy/__init__.py
+++ lib/ansible/galaxy/__init__.py
@@ -55,7 +55,7 @@ class Galaxy(object):
 
         # load data path for resource usage
         this_dir, this_filename = os.path.split(__file__)
-        type_path = context.CLIARGS.get('{content_type}_type'.format(content_type=context.CLIARGS.get('type')), 'default')
+        type_path = context.CLIARGS.get(f'{context.CLIARGS.get("type")}_type', 'default')
         if type_path == 'default':
             type_path = os.path.join(type_path, context.CLIARGS.get('type'))
 
