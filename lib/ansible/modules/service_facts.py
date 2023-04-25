7615723f2902978d3c5aaae0657d838cc9ddeae9
Brian Coca <brian.coca+git@gmail.com>
2023-04-25 12:20:35 -0400

    also catch rogues! one less loop

--- lib/ansible/modules/service_facts.py
+++ lib/ansible/modules/service_facts.py
@@ -382,8 +382,9 @@ class OpenBSDScanService(BaseService):
         self.rcctl_path = self.module.get_bin_path("rcctl")
         if self.rcctl_path:
 
+            # populate services will all possible
             for svc in self.query_rcctl('all'):
-                services[svc] = {'name': svc, 'source': 'rcctl'}
+                services[svc] = {'name': svc, 'source': 'rcctl', 'rogue': False}
                 services[svc].update(self.get_info(svc))
 
             for svc in self.query_rcctl('on'):
@@ -392,15 +393,21 @@ class OpenBSDScanService(BaseService):
             for svc in self.query_rcctl('started'):
                 services[svc].update({'state': 'running'})
 
-            # Based on the list of services that are enabled, determine which are disabled
-            [services[svc].update({'status': 'disabled'}) for svc in services if services[svc].get('status') is None]
-
-            # and do the same for those are aren't running
-            [services[svc].update({'state': 'stopped'}) for svc in services if services[svc].get('state') is None]
-
             # Override the state for services which are marked as 'failed'
             for svc in self.query_rcctl('failed'):
                 services[svc].update({'state': 'failed'})
+
+            for svc in services.keys():
+                # Based on the list of services that are enabled/failed, determine which are disabled
+                if services[svc].get('status') is None:
+                    services[svc].update({'status': 'disabled'})
+
+                # and do the same for those are aren't running
+                if services[svc].get('state') is None:
+                    services[svc].update({'state': 'stopped'})
+
+            for svc in self.query_rcctl('rogue'):
+                services[svc]['rogue'] = True
 
         return services
 
