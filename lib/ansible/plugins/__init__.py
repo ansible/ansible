9ddbd507f7a7597ded6331e3fc8336756efbb76d
Brian Coca <brian.coca+git@gmail.com>
2023-04-26 17:18:09 -0400

    config lookup, new option to show origin of value

--- lib/ansible/plugins/__init__.py
+++ lib/ansible/plugins/__init__.py
@@ -23,7 +23,6 @@ __metaclass__ = type
 
 from abc import ABC
 
-import types
 import typing as t
 
 from ansible import constants as C
@@ -69,12 +68,17 @@ class AnsiblePlugin(ABC):
             possible_fqcns.add(name)
         return bool(possible_fqcns.intersection(set(self.ansible_aliases)))
 
+    def get_option_and_origin(self, option, hostvars=None):
+        try:
+            option_value, origin = C.config.get_config_value_and_origin(option, plugin_type=self.plugin_type, plugin_name=self._load_name, variables=hostvars)
+        except AnsibleError as e:
+            raise KeyError(to_native(e))
+        return option_value, origin
+
     def get_option(self, option, hostvars=None):
+
         if option not in self._options:
-            try:
-                option_value = C.config.get_config_value(option, plugin_type=self.plugin_type, plugin_name=self._load_name, variables=hostvars)
-            except AnsibleError as e:
-                raise KeyError(to_native(e))
+            option_value, dummy = self.get_option_and_origin(option, hostvars=hostvars)
             self.set_option(option, option_value)
         return self._options.get(option)
 
