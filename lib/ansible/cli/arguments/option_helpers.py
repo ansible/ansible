e622145da36087685530b0b4c907dd7edd8ed518
Brian Coca <brian.coca+git@gmail.com>
2023-04-26 16:57:15 -0400

    Don't include a default for cli as it overrides config with lesser precedence
      connection plugins should already use the same config entries (and more)
      than DEFAULT_TIMEOUT.

--- lib/ansible/cli/arguments/option_helpers.py
+++ lib/ansible/cli/arguments/option_helpers.py
@@ -250,8 +250,8 @@ def add_connect_options(parser):
                                help='connect as this user (default=%s)' % C.DEFAULT_REMOTE_USER)
     connect_group.add_argument('-c', '--connection', dest='connection', default=C.DEFAULT_TRANSPORT,
                                help="connection type to use (default=%s)" % C.DEFAULT_TRANSPORT)
-    connect_group.add_argument('-T', '--timeout', default=C.DEFAULT_TIMEOUT, type=int, dest='timeout',
-                               help="override the connection timeout in seconds (default=%s)" % C.DEFAULT_TIMEOUT)
+    connect_group.add_argument('-T', '--timeout', default=None, type=int, dest='timeout',
+                               help="override the connection timeout in seconds (default depends on connection)")
 
     # ssh only
     connect_group.add_argument('--ssh-common-args', default=None, dest='ssh_common_args',
