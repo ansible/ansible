d9a566f9e1ce56e90c86dfca00a7bc9b23403986
Brian Coca <brian.coca+git@gmail.com>
2023-05-01 17:25:53 -0400

    trecharous whitespace eliminated

--- lib/ansible/plugins/action/add_host.py
+++ lib/ansible/plugins/action/add_host.py
@@ -42,7 +42,6 @@ class ActionModule(ActionBase):
 
     def run(self, tmp=None, task_vars=None):
 
-
         result = super(ActionModule, self).run(tmp, task_vars)
         del tmp  # tmp no longer has any effect
 
