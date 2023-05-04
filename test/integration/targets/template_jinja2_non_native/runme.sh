97d3ec98a60bf43fdaeae13a34eeeaa79517f540
Martin Krizek <martin.krizek@gmail.com>
2023-05-04 14:55:27 +0200

    Account for overlays when interacting with Jinja envs (#80705)
    Instead of using Templar.environment in Templar.do_template for
    accessing/mutating the environment, myenv local variable should be used
    because it is the environment used for actual templating. It can either
    point to Templar.environment or newly created environment overlay.
    
    Fixes #80605
    
    (cherry picked from commit 8cd95a8e664ccd634dc3a95642ef7ad41f007169)

--- test/integration/targets/template_jinja2_non_native/runme.sh
+++ test/integration/targets/template_jinja2_non_native/runme.sh
@@ -4,4 +4,6 @@ set -eux
 
 export ANSIBLE_JINJA2_NATIVE=1
 ansible-playbook 46169.yml -v "$@"
+python -m pip install "Jinja2>=3.1.0"
+ansible-playbook macro_override.yml -v "$@"
 unset ANSIBLE_JINJA2_NATIVE
