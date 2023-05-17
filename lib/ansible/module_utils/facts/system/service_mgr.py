bbf9f8f40ed93dac8d2b184d95473612cecb196e
Ikko Eltociear Ashimine <eltociear@gmail.com>
2023-05-17 21:55:41 +0900

    Fix typo in service_mgr.py
    faild -> failed

--- lib/ansible/module_utils/facts/system/service_mgr.py
+++ lib/ansible/module_utils/facts/system/service_mgr.py
@@ -47,7 +47,7 @@ class ServiceMgrFactCollector(BaseFactCollector):
         # tools must be installed
         if module.get_bin_path('systemctl'):
 
-            # this should show if systemd is the boot init system, if checking init faild to mark as systemd
+            # this should show if systemd is the boot init system, if checking init failed to mark as systemd
             # these mirror systemd's own sd_boot test http://www.freedesktop.org/software/systemd/man/sd_booted.html
             for canary in ["/run/systemd/system/", "/dev/.run/systemd/", "/dev/.systemd/"]:
                 if os.path.exists(canary):
