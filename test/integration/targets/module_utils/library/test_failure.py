7c256dc9ba46b5a950f6d0c8cbe01b644cbd9cbe
Abhijeet Kasurde <akasurde@redhat.com>
2023-05-19 15:23:01 -0700

    Pylint fixes
    * disallowed-names
    * used-before-assignment
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- test/integration/targets/module_utils/library/test_failure.py
+++ test/integration/targets/module_utils/library/test_failure.py
@@ -6,9 +6,9 @@ results = {}
 # Test that we are rooted correctly
 # Following files:
 #   module_utils/yak/zebra/foo.py
-from ansible.module_utils.zebra import foo
+from ansible.module_utils.zebra import foo4
 
-results['zebra'] = foo.data
+results['zebra'] = foo4.data
 
 from ansible.module_utils.basic import AnsibleModule
 AnsibleModule(argument_spec=dict()).exit_json(**results)
--- test/support/integration/plugins/modules/timezone.py
+++ test/support/integration/plugins/modules/timezone.py
@@ -121,7 +121,7 @@ class Timezone(object):
             # running in the global zone where changing the timezone has no effect.
             zonename_cmd = module.get_bin_path('zonename')
             if zonename_cmd is not None:
-                (rc, stdout, _) = module.run_command(zonename_cmd)
+                (rc, stdout, stderr) = module.run_command(zonename_cmd)
                 if rc == 0 and stdout.strip() == 'global':
                     module.fail_json(msg='Adjusting timezone is not supported in Global Zone')
 
@@ -731,7 +731,7 @@ class BSDTimezone(Timezone):
         # Strategy 3:
         #   (If /etc/localtime is not symlinked)
         #   Check all files in /usr/share/zoneinfo and return first non-link match.
-        for dname, _, fnames in sorted(os.walk(zoneinfo_dir)):
+        for dname, dirs, fnames in sorted(os.walk(zoneinfo_dir)):
             for fname in sorted(fnames):
                 zoneinfo_file = os.path.join(dname, fname)
                 if not os.path.islink(zoneinfo_file) and filecmp.cmp(zoneinfo_file, localtime_file):
