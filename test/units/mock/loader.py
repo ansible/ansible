43c31c5dc2bf596176762e4e7c01df35d80459fe
Matt Clay <matt@mystile.com>
2023-05-26 19:25:26 -0700

    Use isinstance in unit tests for type checks (#80906)

--- test/units/mock/loader.py
+++ test/units/mock/loader.py
@@ -30,7 +30,7 @@ class DictDataLoader(DataLoader):
 
     def __init__(self, file_mapping=None):
         file_mapping = {} if file_mapping is None else file_mapping
-        assert type(file_mapping) == dict
+        assert isinstance(file_mapping, dict)
 
         super(DictDataLoader, self).__init__()
 
