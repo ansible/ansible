719bd1d59512e9f390fd819ed0e8fb457846ab1f
s-hertel <19572925+s-hertel@users.noreply.github.com>
2023-06-02 13:34:22 -0400

    update ansible-galaxy testserver to use SSLContext.load_cert_chain for Python 3.12 compatibility

--- test/integration/targets/ansible-galaxy/files/testserver.py
+++ test/integration/targets/ansible-galaxy/files/testserver.py
@@ -9,12 +9,15 @@ if __name__ == '__main__':
         import http.server
         import socketserver
         Handler = http.server.SimpleHTTPRequestHandler
+        context = ssl.SSLContext()
+        context.load_cert_chain(certfile='./cert.pem', keyfile='./key.pem')
         httpd = socketserver.TCPServer(("", 4443), Handler)
+        httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
     else:
         import BaseHTTPServer
         import SimpleHTTPServer
         Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
         httpd = BaseHTTPServer.HTTPServer(("", 4443), Handler)
+        httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./cert.pem', keyfile='./key.pem', server_side=True)
 
-    httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./cert.pem', keyfile='./key.pem', server_side=True)
     httpd.serve_forever()
