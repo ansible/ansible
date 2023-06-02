b1478b66f3ce7dce9f738eac5802988bc41108d8
s-hertel <19572925+s-hertel@users.noreply.github.com>
2023-06-02 13:54:15 -0400

    remove unused Python 2 code

--- test/integration/targets/ansible-galaxy/files/testserver.py
+++ test/integration/targets/ansible-galaxy/files/testserver.py
@@ -3,21 +3,14 @@ __metaclass__ = type
 
 import sys
 import ssl
+import http.server
+import socketserver
 
 if __name__ == '__main__':
-    if sys.version_info[0] >= 3:
-        import http.server
-        import socketserver
-        Handler = http.server.SimpleHTTPRequestHandler
-        context = ssl.SSLContext()
-        context.load_cert_chain(certfile='./cert.pem', keyfile='./key.pem')
-        httpd = socketserver.TCPServer(("", 4443), Handler)
-        httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
-    else:
-        import BaseHTTPServer
-        import SimpleHTTPServer
-        Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-        httpd = BaseHTTPServer.HTTPServer(("", 4443), Handler)
-        httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./cert.pem', keyfile='./key.pem', server_side=True)
+    Handler = http.server.SimpleHTTPRequestHandler
+    context = ssl.SSLContext()
+    context.load_cert_chain(certfile='./cert.pem', keyfile='./key.pem')
+    httpd = socketserver.TCPServer(("", 4443), Handler)
+    httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
     httpd.serve_forever()
