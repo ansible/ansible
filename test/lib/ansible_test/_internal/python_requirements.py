ecbbd7e5d155dffa8bc8d17611e62525b23b3ae0
Sloane Hertel <19572925+s-hertel@users.noreply.github.com>
2023-06-14 18:05:50 -0400

    Remove Python 3.5 support (#80972)
    * Remove Python 3.5 support
    
    * changelog
    
    * add requested changes
    
    * Update changelogs/fragments/remove-python3.5.yml
    
    Co-authored-by: Matt Clay <matt@mystile.com>
    
    * remove Python3 < 3.5 error handling
    
    * remove Python3 < 3.5 error handling in TE too
    
    ---------
    
    Co-authored-by: Matt Clay <matt@mystile.com>

--- test/lib/ansible_test/_internal/python_requirements.py
+++ test/lib/ansible_test/_internal/python_requirements.py
@@ -445,11 +445,6 @@ def get_venv_packages(python: PythonConfig) -> dict[str, str]:
             setuptools='44.1.1',  # 45.0.0 requires Python 3.5+
             wheel=None,
         ),
-        '3.5': dict(
-            pip='20.3.4',  # 21.0 requires Python 3.6+
-            setuptools='50.3.2',  # 51.0.0 requires Python 3.6+
-            wheel=None,
-        ),
         '3.6': dict(
             pip='21.3.1',  # 22.0 requires Python 3.7+
             setuptools='59.6.0',  # 59.7.0 requires Python 3.7+
