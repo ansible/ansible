724ca4c1f31468de6f7da3e68f6b412aaf986086
Everson Leal <everson.leal@sonda.com>
2023-06-24 13:57:31 -0300

    Fix assert message

--- test/units/plugins/inventory/test_script.py
+++ test/units/plugins/inventory/test_script.py
@@ -110,4 +110,4 @@ class TestInventoryModule(unittest.TestCase):
 
         with pytest.raises(AnsibleError) as e:
             self.inventory_module.get_host_variables('/foo/bar/foobar.py', 'dummy host')
-        assert e.value.message == "Failed to parse (/foo/bar/foobar.py) script plugin: dummy error"
+        assert e.value.message == "Inventory script (/foo/bar/foobar.py) had an execution error: dummy error"
