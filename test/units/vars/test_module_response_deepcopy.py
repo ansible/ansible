6088fd66217d2702519b00289d3f622729854514
Matt Clay <matt@mystile.com>
2023-06-27 16:54:39 -0700

    Remove unused unit test

--- test/units/vars/test_module_response_deepcopy.py
+++ test/units/vars/test_module_response_deepcopy.py
@@ -37,15 +37,6 @@ def test_module_response_deepcopy_empty_tuple():
     assert x is y
 
 
-@pytest.mark.skip(reason='No current support for this situation')
-def test_module_response_deepcopy_tuple():
-    x = ([1, 2], 3)
-    y = module_response_deepcopy(x)
-    assert y == x
-    assert x is not y
-    assert x[0] is not y[0]
-
-
 def test_module_response_deepcopy_tuple_of_immutables():
     x = ((1, 2), 3)
     y = module_response_deepcopy(x)
