867ddc7cf32fd737544d7b3057af16ee9c1ecdd7
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2023-06-30 03:59:59 +0200

    Test connection drop post reboot
    This covers an except block code path that happens when a reboot
    command caused a connection reset before said connection had a
    chance to shut down gracefully.
    
    ci_complete
    ci_coverage

--- test/units/plugins/action/test_reboot.py
+++ test/units/plugins/action/test_reboot.py
@@ -5,6 +5,7 @@ import os
 
 import pytest
 
+from ansible.errors import AnsibleConnectionFailure
 from ansible.playbook.task import Task
 from ansible.plugins.action.reboot import ActionModule as RebootAction
 from ansible.plugins.loader import connection_loader
@@ -128,6 +129,65 @@ def test_reboot_command(action_plugin, mocker, monkeypatch, task_args):
         'stdout': '<test command stub-stdout>',
     }
     assert low_level_cmd_spy.spy_exception is None
+
+    assert 'failed' not in action_result
+    assert action_result == {'rebooted': True, 'changed': True, 'elapsed': 0}
+
+
+@pytest.mark.parametrize(
+    'task_args',
+    (
+        {
+            'reboot_timeout': 5,
+            'reboot_command': _SENTINEL_REBOOT_COMMAND,
+            'test_command': _SENTINEL_TEST_COMMAND,
+        },
+    ),
+    ids=('reboot command with spaces', ),
+    indirect=('task_args', ),
+)
+def test_reboot_command_connection_fail(action_plugin, mocker, monkeypatch, task_args):
+    """Check that the reboot command gets called and reboot verified."""
+    def _patched_low_level_execute_command(cmd, *args, **kwargs):
+        if cmd == _SENTINEL_REBOOT_COMMAND:
+            raise AnsibleConnectionFailure('Fake connection drop')
+        return {
+            _SENTINEL_TEST_COMMAND: {
+                'rc': 0,
+                'stderr': '<test command stub-stderr>',
+                'stdout': '<test command stub-stdout>',
+            },
+        }[cmd]
+
+    monkeypatch.setattr(
+        action_plugin,
+        '_low_level_execute_command',
+        _patched_low_level_execute_command,
+    )
+
+    action_plugin._connection = mocker.Mock()
+
+    monkeypatch.setattr(action_plugin, 'check_boot_time', lambda *_a, **_kw: 5)
+    monkeypatch.setattr(action_plugin, 'get_distribution', mocker.MagicMock())
+    monkeypatch.setattr(action_plugin, 'get_system_boot_time', lambda d: 0)
+
+    low_level_cmd_spy = mocker.spy(action_plugin, '_low_level_execute_command')
+
+    action_result = action_plugin.run()
+
+    assert low_level_cmd_spy.called
+
+    low_level_cmd_spy.assert_any_call(
+        task_args['reboot_command'], sudoable=True,
+    )
+    low_level_cmd_spy.assert_any_call(task_args['test_command'], sudoable=True)
+
+    assert low_level_cmd_spy.call_count == 2
+    assert low_level_cmd_spy.spy_return == {
+        'rc': 0,
+        'stderr': '<test command stub-stderr>',
+        'stdout': '<test command stub-stdout>',
+    }
 
     assert 'failed' not in action_result
     assert action_result == {'rebooted': True, 'changed': True, 'elapsed': 0}
