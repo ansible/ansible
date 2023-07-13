3858e2a59009fae7c49c167efc474073299f54d0
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2023-07-13 16:21:12 +0530

    Update setup.py
    Maintained the original structure, package directories, and entry points.
    Simplified reading the requirements.txt file by using open().read().splitlines().

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
-
-import pathlib
+from __future__ import absolute_import, division, print_function
 
 from setuptools import find_packages, setup
 
-here = pathlib.Path(__file__).parent.resolve()
-
-install_requires = (here / 'requirements.txt').read_text(encoding='utf-8').splitlines()
-
 setup(
-    install_requires=install_requires,
-    package_dir={'': 'lib',
-                 'ansible_test': 'test/lib/ansible_test'},
+    install_requires=open('requirements.txt', encoding='utf-8').read().splitlines(),
+    package_dir={'': 'lib', 'ansible_test': 'test/lib/ansible_test'},
     packages=find_packages('lib') + find_packages('test/lib'),
     entry_points={
         'console_scripts': [
