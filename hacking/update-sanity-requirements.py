6656ec1aa33fdace2eb8ca4f3f43be0383bf52fa
Matt Clay <matt@mystile.com>
2023-07-18 18:17:48 -0700

    [stable-2.13] ansible-test - Pre-build PyYAML wheels (#81300)
    This works around Cython failures when attempting to install PyYAML >= 5.4 <= 6.0..
    (cherry picked from commit e964078a83af240b103e34eafca9162deff0e16f)
    
    Co-authored-by: Matt Clay <matt@mystile.com>

--- hacking/update-sanity-requirements.py
+++ hacking/update-sanity-requirements.py
@@ -7,10 +7,14 @@ from __future__ import annotations
 import argparse
 import dataclasses
 import pathlib
+import re
 import subprocess
 import tempfile
 import typing as t
 import venv
+
+import packaging.version
+import packaging.specifiers
 
 try:
     import argcomplete
@@ -47,7 +51,22 @@ class SanityTest:
 
             pip_freeze = subprocess.run(pip + ['freeze'], env=env, check=True, capture_output=True, text=True)
 
-        requirements = f'# edit "{self.source_path.name}" and generate with: {SELF} --test {self.name}\n{pip_freeze.stdout}'
+        self.write_requirements(pip_freeze.stdout)
+
+    def update_pre_build(self) -> None:
+        """Update requirements in place with current pre-build instructions."""
+        requirements = pathlib.Path(self.requirements_path).read_text()
+        lines = requirements.splitlines(keepends=True)
+        lines = [line for line in lines if not line.startswith('#')]
+        requirements = ''.join(lines)
+
+        self.write_requirements(requirements)
+
+    def write_requirements(self, requirements: str) -> None:
+        """Write the given test requirements to the requirements file for this test."""
+        pre_build = pre_build_instructions(requirements)
+
+        requirements = f'# edit "{self.source_path.name}" and generate with: {SELF} --test {self.name}\n{pre_build}{requirements}'
 
         with open(self.requirements_path, 'w') as requirement_file:
             requirement_file.write(requirements)
@@ -59,6 +78,38 @@ class SanityTest:
             requirements_path=path,
             source_path=path.with_suffix('.in'),
         )
+
+
+def pre_build_instructions(requirements: str) -> str:
+    """Parse the given requirements and return any applicable pre-build instructions."""
+    parsed_requirements = requirements.splitlines()
+
+    package_versions = {
+        match.group('package').lower(): match.group('version') for match
+        in (re.search('^(?P<package>.*)==(?P<version>.*)$', requirement) for requirement in parsed_requirements)
+        if match
+    }
+
+    instructions: list[str] = []
+
+    build_constraints = (
+        ('pyyaml', '>= 5.4, <= 6.0', ('Cython < 3.0',)),
+    )
+
+    for package, specifier, constraints in build_constraints:
+        version_string = package_versions.get(package)
+
+        if version_string:
+            version = packaging.version.Version(version_string)
+            specifier_set = packaging.specifiers.SpecifierSet(specifier)
+
+            if specifier_set.contains(version):
+                instructions.append(f'# pre-build requirement: {package} == {version}\n')
+
+                for constraint in constraints:
+                    instructions.append(f'# pre-build constraint: {constraint}\n')
+
+    return ''.join(instructions)
 
 
 def main() -> None:
@@ -74,6 +125,12 @@ def main() -> None:
         help='test requirements to update'
     )
 
+    parser.add_argument(
+        '--pre-build-only',
+        action='store_true',
+        help='apply pre-build instructions to existing requirements',
+    )
+
     if argcomplete:
         argcomplete.autocomplete(parser)
 
@@ -83,8 +140,12 @@ def main() -> None:
     tests = [test for test in tests if test.name in test_names] if test_names else tests
 
     for test in tests:
-        print(f'===[ {test.name} ]===')
-        test.freeze_requirements()
+        print(f'===[ {test.name} ]===', flush=True)
+
+        if args.pre_build_only:
+            test.update_pre_build()
+        else:
+            test.freeze_requirements()
 
 
 def find_tests() -> t.List[SanityTest]:
