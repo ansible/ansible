aec64249e9689718d4435c2b977bde322d204402
Phil <pbagli57@gmail.com>
2023-08-09 21:07:30 +0200

    Correction for reloaded state

--- lib/ansible/modules/systemd_service.py
+++ lib/ansible/modules/systemd_service.py
@@ -27,7 +27,7 @@ options:
         description:
             - V(started)/V(stopped) are idempotent actions that will not run commands unless necessary.
               V(restarted) will always bounce the unit.
-              V(reloaded) will always reload and if the service is not running at the moment of the reload, it is restarted.
+              V(reloaded) will always reload and if the service is not running at the moment of the reload, it is started.
         type: str
         choices: [ reloaded, restarted, started, stopped ]
     enabled:
