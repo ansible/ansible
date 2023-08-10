809c9b646da918334862c4dd1e143d6eb9479ae7
s-hertel <19572925+s-hertel@users.noreply.github.com>
2023-08-10 11:20:31 -0400

    macosx/13.2 seems to need a longer start up time for the first task
    since the 2nd task can also be completed within the new timeframe, extend the pause of the 2nd task loop to try to try to avoid new race condition

--- test/integration/targets/loops/runme.sh
+++ test/integration/targets/loops/runme.sh
@@ -14,14 +14,14 @@ sleep 1
 
 echo "waiting for first loop result..."
 # wait up to 3s for first loop result to appear
-python ../test_utils/scripts/timeout.py -- 1 tail -f out.txt | grep item=0 -m 1 || (echo "failed to get first loop result in time" && false)
+python ../test_utils/scripts/timeout.py -- 2 tail -f out.txt | grep item=0 -m 1 || (echo "failed to get first loop result in time" && false)
 
 echo "checking for absence of second loop result..."
 # fail if the second loop result appeared too early
-grep item=2 out.txt && (echo "found the second loop result early, failing" && false)
+grep item=3 out.txt && (echo "found the second loop result early, failing" && false)
 
 echo "waiting for second loop result..."
 # wait up to 3s for second loop result to appear
-python ../test_utils/scripts/timeout.py -- 3 tail -f out.txt | grep item=2 -m 1 || (echo "failed to get second loop result in time" && false)
+python ../test_utils/scripts/timeout.py -- 3 tail -f out.txt | grep item=3 -m 1 || (echo "failed to get second loop result in time" && false)
 
 echo "success"
