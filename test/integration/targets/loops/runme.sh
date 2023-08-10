4463a691b386d253b670c26d6e7f428a4bc2eea9
s-hertel <19572925+s-hertel@users.noreply.github.com>
2023-08-10 13:59:38 -0400

    consolidate file cleanup/creation

--- test/integration/targets/loops/runme.sh
+++ test/integration/targets/loops/runme.sh
@@ -4,17 +4,13 @@ set -eu
 
 ansible-playbook playbook.yml -i ../../inventory "$@"
 
-rm -f out.txt
+rm -f out.txt && touch out.txt
 
-python ../test_utils/scripts/timeout.py -- 10 'ansible-playbook test_loop_item_display.yml "$@" > out.txt' &
-# pause to try to avoid race condition:
-# tail: cannot open 'out.txt' for reading: No such file or directory
-# tail: no files remaining
-sleep 1
+python ../test_utils/scripts/timeout.py -- 10 'ansible-playbook test_loop_item_display.yml "$@" >> out.txt' &
 
 echo "waiting for first loop result..."
 # wait up to 3s for first loop result to appear
-python ../test_utils/scripts/timeout.py -- 2 tail -f out.txt | grep item=0 -m 1 || (echo "failed to get first loop result in time" && false)
+python ../test_utils/scripts/timeout.py -- 3 tail -f out.txt | grep item=0 -m 1 || (echo "failed to get first loop result in time" && false)
 
 echo "checking for absence of second loop result..."
 # fail if the second loop result appeared too early
