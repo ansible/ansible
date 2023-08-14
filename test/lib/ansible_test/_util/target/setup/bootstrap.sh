5c7d361b6ec01f3dc82f5dbb53fa95e1eb378ef4
Sviatoslav Sydorenko <webknjaz@redhat.com>
2023-08-14 22:34:33 +0200

    [stable-2.13] Replace FreeBSD 12.{3->4} & 13.{0->1} @ testing (#81498)
    * Replace FreeBSD 12.3 w/ 12.4 in CI & ansible-test
    
    FreeBSD 12.3 bootstrap packages stopped being available.
    
    * Replace FreeBSD 13.0 w/ 13.1 in CI & ansible-test

--- test/lib/ansible_test/_util/target/setup/bootstrap.sh
+++ test/lib/ansible_test/_util/target/setup/bootstrap.sh
@@ -171,6 +171,8 @@ bootstrap_remote_freebsd()
         # Declare platform/python version combinations which do not have supporting OS packages available.
         # For these combinations ansible-test will use pip to install the requirements instead.
         case "${platform_version}/${python_version}" in
+            "12.4/3.9")
+                ;;
             "13.0/3.8")
                 jinja2_pkg=""  # not available
                 cryptography_pkg=""  # not available
