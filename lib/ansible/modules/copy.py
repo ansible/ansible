80e622374123ac1e7c25cb92b8e141074768dbea
Karl O. Pinc <kop@karlpinc.com>
2023-08-28 15:17:43 -0500

    Respond to Don Naro's second review

--- lib/ansible/modules/copy.py
+++ lib/ansible/modules/copy.py
@@ -82,7 +82,7 @@ options:
     - Set the access permissions of newly created directories to the given mode.
       Permissions on existing directories do not change.
     - See C(mode) for the syntax of accepted values.
-    - The system defaults are used when this parameter is not set.
+    - The target system's defaults determine permissions when this parameter is not set.
     type: raw
     version_added: '1.5'
   remote_src:
