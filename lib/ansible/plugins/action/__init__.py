fa2507904b7f07db9cac2e47c4579c33f3e4466c
lav <lav3831@gmail.com>
2023-09-04 16:53:18 +0530

    change become link

--- lib/ansible/plugins/action/__init__.py
+++ lib/ansible/plugins/action/__init__.py
@@ -739,7 +739,7 @@ class ActionBase(ABC):
             return remote_paths
 
         # we'll need this down here
-         https://docs.ansible.com/ansible-core/2.15/playbook_guide/playbooks_privilege_escalation.html#risks-of-becoming-an-unprivileged-user
+       become_link = get_versioned_doclink('playbook_guide/playbooks_privilege_escalation.html')
         # Step 3f: Common group
         # Otherwise, we're a normal user. We failed to chown the paths to the
         # unprivileged user, but if we have a common group with them, we should
