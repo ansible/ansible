b27b9d858ef6f39cb2a2dcbccd5ac9b964b565e3
1LAV1 <143288298+1LAV1@users.noreply.github.com>
2023-09-05 21:15:57 +0530

    changed link  ‘Risks of becoming an unprivileged user’  (#81623)
    * changes made in become link
    
    * change become link
    
    * Update lib/ansible/plugins/action/__init__.py
    
    ---------
    
    Co-authored-by: Martin Krizek <martin.krizek@gmail.com>

--- lib/ansible/plugins/action/__init__.py
+++ lib/ansible/plugins/action/__init__.py
@@ -739,8 +739,7 @@ class ActionBase(ABC):
             return remote_paths
 
         # we'll need this down here
-        become_link = get_versioned_doclink('playbook_guide/playbooks_privilege_escalation.html#risks-of-becoming-an-unprivileged-user')
-
+        become_link = get_versioned_doclink('playbook_guide/playbooks_privilege_escalation.html')
         # Step 3f: Common group
         # Otherwise, we're a normal user. We failed to chown the paths to the
         # unprivileged user, but if we have a common group with them, we should
