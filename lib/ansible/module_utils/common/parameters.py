6836a869edb9e65726aa19a60339392cbe6ac8dc
Felix Fontein <felix@fontein.de>
2023-09-05 13:00:24 +0200

    Avoid internal error when validating argspec.

--- lib/ansible/module_utils/common/parameters.py
+++ lib/ansible/module_utils/common/parameters.py
@@ -838,6 +838,9 @@ def env_fallback(*args, **kwargs):
 
 def set_fallbacks(argument_spec, parameters):
     no_log_values = set()
+    if not isinstance(parameters, Mapping):
+        # The wrong type is handled somewhere else
+        return no_log_values
     for param, value in argument_spec.items():
         fallback = value.get('fallback', (None,))
         fallback_strategy = fallback[0]
