424ef58c6b7b1831f95f31a08572e788958242d0
Sviatoslav Sydorenko <webknjaz@redhat.com>
2023-09-16 04:33:49 +0200

    Implement an informative reporter for resolvelib

--- lib/ansible/galaxy/dependency_resolution/reporters.py
+++ lib/ansible/galaxy/dependency_resolution/reporters.py
@@ -5,11 +5,49 @@
 
 from __future__ import annotations
 
+import typing as t
+from collections import defaultdict
+from contextlib import suppress as suppress_exceptions
+from logging import getLogger
+
 try:
     from resolvelib import BaseReporter
 except ImportError:
     class BaseReporter:  # type: ignore[no-redef]
         pass
+
+try:
+    from resolvelib.resolvers import Criterion
+except ImportError:
+    try:
+        from resolvelib.resolvers.criterion import Criterion
+    except ImportError:
+        class Criterion:  # type: ignore[no-redef]
+            pass
+
+from .dataclasses import Candidate, Requirement
+
+
+_CLI_APP_NAME = 'ansible-galaxy'
+_MESSAGES_AT_REJECT_COUNT = {
+    1: (
+        f'{_CLI_APP_NAME} is looking at multiple versions of {{fqcn}} to '
+        'determine which version is compatible with other '
+        'requirements. This could take a while.'
+    ),
+    8: (
+        f'{_CLI_APP_NAME} is looking at multiple versions of {{fqcn}} to '
+        'determine which version is compatible with other '
+        'requirements. This could take a while.'
+    ),
+    13: (
+        'This is taking longer than usual. You might need to provide '
+        'the dependency resolver with stricter constraints to reduce '
+        'runtime. If you want to abort this run, press Ctrl + C.'
+    ),
+}
+
+_logger = getLogger(__name__)
 
 
 class CollectionDependencyReporter(BaseReporter):
@@ -18,3 +56,51 @@ class CollectionDependencyReporter(BaseReporter):
     This is a proxy class allowing us to abstract away importing resolvelib
     outside of the `ansible.galaxy.dependency_resolution` Python package.
     """
+
+    def __init__(self) -> None:
+        """Initialize the collection rejection counter."""
+        super().__init__()
+
+        self.reject_count_by_fqcn: t.DefaultDict[str, int] = defaultdict(int)
+
+    def _log_mapped_rejection_message(self, candidate: Candidate) -> None:
+        """Print out rejection messages on pre-defined limit hits."""
+        # Inspired by https://github.com/pypa/pip/commit/9731131
+        self.reject_count_by_fqcn[candidate.fqcn] += 1
+
+        collection_rejections_count = self.reject_count_by_fqcn[candidate.fqcn]
+        try:
+            collection_rejection_message = _MESSAGES_AT_REJECT_COUNT[
+                collection_rejections_count
+            ]
+        except KeyError as key_err:
+            raise LookupError from key_err
+
+        _logger.info(collection_rejection_message.format(fqcn=candidate.fqcn))
+
+    def rejecting_candidate(  # resolvelib >= 0.9.0
+            self,
+            criterion: Criterion[Candidate, Requirement],
+            candidate: Candidate,
+    ) -> None:
+        """Augment rejection messages with conflict details."""
+        try:
+            self._log_mapped_rejection_message(candidate)
+        except LookupError:
+            return
+
+        msg = 'Will try a different candidate, due to conflict:'
+        for req_info in criterion.information:
+            req, parent = req_info.requirement, req_info.parent
+            msg += '\n    '
+            if parent:
+                msg += f'{parent !r} depends on '
+            else:
+                msg += 'The user requested '
+            msg += repr(req)
+        _logger.debug(msg)
+
+    def backtracking(self, candidate: Candidate) -> None:  # resolvelib < 0.9.0
+        """Print out rejection messages on pre-defined limit hits."""
+        with suppress_exceptions(LookupError):
+            return self._log_mapped_rejection_message(candidate)
