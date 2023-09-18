f7234968d241d7171aadb1e873a67510753f3163
Matt Martz <matt@sivel.net>
2023-09-18 16:24:44 -0500

    bump devel to 2.17.0.dev0 (#81712)

--- lib/ansible/release.py
+++ lib/ansible/release.py
@@ -19,6 +19,6 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-__version__ = '2.16.0.dev0'
+__version__ = '2.17.0.dev0'
 __author__ = 'Ansible, Inc.'
-__codename__ = "All My Love"
+__codename__ = "Gallows Pole"
