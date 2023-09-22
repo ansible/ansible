095ef3fe2960645144fbd4204e4519c0550fae7c
Sander Ruitenbeek <sander@getgoing.nl>
2023-09-22 12:25:11 +0200

    Fix for 'apt_repository modify comment line /etc/apt/sources.list' (#54403)
    Rebuild the SourceList from its parsed chunks. Use re.split() instead of split(), so it will not make additional spaces disappear. Furthermore, join the comment and source lines without additional spaces before and after the hash.
    
    Fixes #54403
    
    Signed-off-by: Sander Ruitenbeek <sander@getgoing.nl>

--- lib/ansible/modules/apt_repository.py
+++ lib/ansible/modules/apt_repository.py
@@ -295,14 +295,12 @@ class SourcesList(object):
         # Check for another "#" in the line and treat a part after it as a comment.
         i = line.find('#')
         if i > 0:
-            comment = line[i + 1:].strip()
-            line = line[:i]
+            line, comment = re.split('#', line, maxsplit=1)
 
-        # Split a source into substring to make sure that it is source spec.
-        # Duplicated whitespaces in a valid source spec will be removed.
-        source = line.strip()
+        # Check if a line is a source spec and if it is, mark it as a valid source line.
+        source = line
         if source:
-            chunks = source.split()
+            chunks = re.split(' ', source, maxsplit=1)
             if chunks[0] in VALID_SOURCE_TYPES:
                 valid = True
                 source = ' '.join(chunks)
@@ -361,10 +359,10 @@ class SourcesList(object):
                 for n, valid, enabled, source, comment in sources:
                     chunks = []
                     if not enabled:
-                        chunks.append('# ')
+                        chunks.append('#')
                     chunks.append(source)
                     if comment:
-                        chunks.append(' # ')
+                        chunks.append('#')
                         chunks.append(comment)
                     chunks.append('\n')
                     line = ''.join(chunks)
@@ -392,10 +390,10 @@ class SourcesList(object):
                 for n, valid, enabled, source, comment in sources:
                     chunks = []
                     if not enabled:
-                        chunks.append('# ')
+                        chunks.append('#')
                     chunks.append(source)
                     if comment:
-                        chunks.append(' # ')
+                        chunks.append('#')
                         chunks.append(comment)
                     chunks.append('\n')
                     lines.append(''.join(chunks))
