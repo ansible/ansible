bf0a222cde866c8ed971ba4f03c6f340075f5852
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2023-09-27 17:03:19 +0200

    Emit a warning if depresolver selects pre-releases
    This warning is only shown when the end-user doesn't explicitly
    request pre-release versions to be considered globally but the
    dependency resolver returns some despite that.

--- lib/ansible/galaxy/dependency_resolution/dataclasses.py
+++ lib/ansible/galaxy/dependency_resolution/dataclasses.py
@@ -29,6 +29,7 @@ if t.TYPE_CHECKING:
 from ansible.errors import AnsibleError, AnsibleAssertionError
 from ansible.galaxy.api import GalaxyAPI
 from ansible.galaxy.collection import HAS_PACKAGING, PkgReq
+from ansible.galaxy.dependency_resolution.versioning import is_pre_release
 from ansible.module_utils.common.text.converters import to_bytes, to_native, to_text
 from ansible.module_utils.common.arg_spec import ArgumentSpecValidator
 from ansible.utils.collection_loader import AnsibleCollectionRef
@@ -582,6 +583,11 @@ class _ComputedReqKindsMixin:
         return version_spec_start_char.isdigit() or not (
             version_spec_start_char.startswith(('<', '>', '!', '*'))
         )
+
+    @property
+    def is_pre_release(self) -> bool:
+        """Return whether this candidate has a pre-release version."""
+        return is_pre_release(self.ver)
 
     @property
     def source_info(self):
