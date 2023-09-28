15c029d5cc8cc3867ab9eae58718338d11b92e14
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2023-09-29 00:40:17 +0200

    Fix `is_pinned` property of `Requirement`
    Previously, requirement version specs starting with `!=` were
    incorrectly considered as pinned release requests because the
    comparison was being made against a one-char string while the
    operator is two-char. This patch changes the check to test against `!`
    which is enough to detect this case.

--- lib/ansible/galaxy/dependency_resolution/dataclasses.py
+++ lib/ansible/galaxy/dependency_resolution/dataclasses.py
@@ -580,16 +580,15 @@ class _ComputedReqKindsMixin:
 
         See https://github.com/ansible/ansible/pull/81606 for extra context.
         """
-        version_string = self.ver[0]
-        return version_string.isdigit() or not (
-            version_string == '*' or
-            version_string.startswith(('<', '>', '!='))
+        version_spec_start_char = self.ver.strip()[0]
+        return version_spec_start_char.isdigit() or not (
+            version_spec_start_char.startswith(('<', '>', '!', '*'))
         )
 
     @property
     def is_pre_release(self) -> bool:
         """Return whether this candidate has a pre-release version."""
-        return is_pre_release(self.ver)
+        return self.is_pinned and is_pre_release(self.ver)
 
     @property
     def source_info(self):
