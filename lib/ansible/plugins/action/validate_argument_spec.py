9c9725dd8ddec6a3aebb45d437968e5d26f11aa6
s-hertel <19572925+s-hertel@users.noreply.github.com>
2023-10-03 15:12:01 -0400

    Fix validate_argument_spec so the argument spec is validated before passing it to the ArgumentSpecValidator to prevent unhandled errors during variable validation.
    Make role argument spec errors non-fatal at runtime. Display a warning
    and include details with -vvv. Pass ArgumentSpecValidator a pruned
    argument spec containing the valid portion.
    
    Give an error and return argument_spec_errors in the task result for
    non-implicit validate_argument_spec tasks.
    
    Add tests for role argument spec errors and validate_argument_spec
    argument_spec errors.

--- lib/ansible/plugins/action/validate_argument_spec.py
+++ lib/ansible/plugins/action/validate_argument_spec.py
@@ -6,7 +6,110 @@ from __future__ import annotations
 from ansible.errors import AnsibleError
 from ansible.plugins.action import ActionBase
 from ansible.module_utils.common.arg_spec import ArgumentSpecValidator
+from ansible.utils.display import Display
 from ansible.utils.vars import combine_vars
+
+from contextlib import suppress
+from copy import deepcopy
+from dataclasses import asdict, field, dataclass, make_dataclass
+
+import typing as t
+
+OPTION_TYPES = [None, 'bits', 'bool', 'bytes', 'dict', 'float', 'int', 'json', 'jsonarg', 'list', 'path', 'raw', 'sid', 'str']
+
+ROLE_OPTION_SPEC = dict(
+    description=dict(type='list', elements='str'),
+    type=dict(choices=OPTION_TYPES),
+    default=dict(type='raw'),
+    choices=dict(type='list'),
+    elements=dict(choices=OPTION_TYPES),
+    options=dict(type='dict'),
+    required=dict(type='bool'),
+    version_added=dict(type='raw'),
+    # some module argument spec features are intentionally omitted since role argument specs are read-only
+    # no_log, aliases, apply_defaults, and fallback are not supported
+)
+
+OPTION_SPEC = dict(ROLE_OPTION_SPEC)
+OPTION_SPEC.update(dict(
+    no_log=dict(type='bool'),
+    aliases=dict(type='list', elements='str'),
+    apply_defaults=dict(type='bool'),
+    # fallback is excluded because tuple[callable, list[str]] can't be set via a task
+))
+
+display = Display()
+
+
+@dataclass
+class RoleSpec:
+    """Argument spec supported by the ArgumentSpecValidator and roles."""
+
+    @classmethod
+    def from_spec(cls: t.Type[t.Self], fields: dict, role_name: str, spec_name: str, role_path: str) -> t.Self:
+        """Load a valid role argument spec from a foreign argument spec."""
+        fields = deepcopy(fields)
+
+        errors: list[str] = []
+        validator = ArgumentSpecValidator(ROLE_OPTION_SPEC, required_if=[('type', 'list', ('elements',))])
+        validate_argspec(validator, [], fields, errors, prune=True)
+
+        if errors:
+            display.warning(f"Role '{role_name}' entrypoint '{spec_name}' contains errors in the argument spec. Use -vvv to see details.")
+            display.vvv(f"Role '{role_name}' ({role_path}) argument spec '{spec_name}' contains errors:")
+        for error in errors:
+            display.vvv(f"- {error}")
+
+        class_fields = []
+        for key, value in fields.items():
+            default = field(default_factory=lambda: value) if is_mutable(value) else field(default=value)  # pylint: disable=invalid-field-call
+            class_fields.append((key, type(value), default))
+
+        return make_dataclass(cls.__name__, class_fields)(**fields)
+
+
+def validate_argspec(validator: ArgumentSpecValidator, context: list[str], argument_spec: dict, errors: list[str], prune: bool = False) -> None:
+    """Validate the options in an argument spec and optionally prune those with errors."""
+    invalid_options = []
+    for option, fields in argument_spec.items():
+        if not isinstance(option, str):
+            invalid_options.append(option)
+            continue
+        validate_option_spec(validator, context + [option], fields, errors, prune)
+
+    for option in invalid_options:
+        str_option = f"{option}"
+        errors.append(f"{'.'.join(context + [str_option])}: type {type(option)}. Option names must be strings.")
+        if prune:
+            argument_spec.pop(option)
+
+
+def validate_option_spec(validator: ArgumentSpecValidator, context: list[str], fields: dict, errors: list[str], prune: bool = False) -> None:
+    """Validate the fields for an option and optionally prune those with errors."""
+    result = validator.validate(fields)
+
+    if result.error_messages:
+        errors.append(f"{'.'.join(context)}: {', '.join(result.error_messages)}")
+    if prune and result.error_messages:
+        for error in result.error_messages:
+            field = error.split('.', 1)[0]
+            fields.pop(field, None)
+
+    if fields.get('type') in ('list', 'dict') and fields.get('options'):
+        validate_argspec(validator, context, fields['options'], errors, prune)
+
+    elif fields.get('options'):
+        errors.append(f"{'.'.join(context)}: options. Suboptions are supported for types dict and list.")
+        if prune:
+            fields.pop('options')
+
+
+def is_mutable(value: t.Any) -> bool:
+    """Return true is a value is not hashable."""
+    with suppress(TypeError):
+        hash(value)
+        return False
+    return True
 
 
 class ActionModule(ActionBase):
@@ -75,8 +178,30 @@ class ActionModule(ActionBase):
         if not isinstance(provided_arguments, dict):
             raise AnsibleError('Incorrect type for provided_arguments, expected dict and got %s' % type(provided_arguments))
 
+        if self._task.implicit:
+            argument_spec = RoleSpec.from_spec(
+                argument_spec_data,
+                result['validate_args_context']['name'],
+                result['validate_args_context']['argument_spec_name'],
+                result['validate_args_context']['path']
+            )
+            valid_argument_spec = asdict(argument_spec)
+        else:
+            errors = []
+            argspec_option_validator = ArgumentSpecValidator(OPTION_SPEC, required_if=[('type', 'list', ('elements',))])
+            validate_argspec(argspec_option_validator, [], argument_spec_data, errors)
+
+            if errors:
+                result['failed'] = True
+                result['msg'] = "Invalid argument_spec cannot be used to validate variables."
+                result['argument_spec_errors'] = errors
+                result['argument_spec_data'] = argument_spec_data
+                return result
+
+            valid_argument_spec = argument_spec_data
+
         args_from_vars = self.get_args_from_task_vars(argument_spec_data, task_vars)
-        validator = ArgumentSpecValidator(argument_spec_data)
+        validator = ArgumentSpecValidator(valid_argument_spec)
         validation_result = validator.validate(combine_vars(args_from_vars, provided_arguments), validate_role_argument_spec=True)
 
         if validation_result.error_messages:
--- lib/ansible/plugins/callback/junit.py
+++ lib/ansible/plugins/callback/junit.py
@@ -168,6 +168,8 @@ class CallbackModule(CallbackBase):
         if not os.path.exists(self._output_dir):
             os.makedirs(self._output_dir)
 
+        self.wants_implicit_tasks = True
+
     def _start_task(self, task: Task) -> None:
         """ record the start of a task for one or more hosts """
 
