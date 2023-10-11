0ab648da917da718f53ac6be392a6ff335dcd133
Sviatoslav Sydorenko <webknjaz@redhat.com>
2023-10-11 16:36:20 +0100

    Remove Python `< 3.5` `selectors` fallbacks (#81872)

--- lib/ansible/module_utils/compat/selectors.py
+++ lib/ansible/module_utils/compat/selectors.py
@@ -15,41 +15,18 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
-
-'''
-Compat selectors library.  Python-3.5 has this builtin.  The selectors2
-package exists on pypi to backport the functionality as far as python-2.6.
-'''
 from __future__ import annotations
 
-# The following makes it easier for us to script updates of the bundled code
-_BUNDLED_METADATA = {"pypi_name": "selectors2", "version": "1.1.1", "version_constraints": ">1.0,<2.0"}
-
-# Added these bugfix commits from 2.1.0:
-# * https://github.com/SethMichaelLarson/selectors2/commit/3bd74f2033363b606e1e849528ccaa76f5067590
-#   Wrap kqueue.control so that timeout is a keyword arg
-# * https://github.com/SethMichaelLarson/selectors2/commit/6f6a26f42086d8aab273b30be492beecb373646b
-#   Fix formatting of the kqueue.control patch for pylint
-# * https://github.com/SethMichaelLarson/selectors2/commit/f0c2c6c66cfa7662bc52beaf4e2d65adfa25e189
-#   Fix use of OSError exception for py3 and use the wrapper of kqueue.control so retries of
-#   interrupted syscalls work with kqueue
-
+import selectors
 import sys
-import types  # pylint: disable=unused-import
 
-try:
-    # Python 3.4+
-    import selectors as _system_selectors
-except ImportError:
-    try:
-        # backport package installed in the system
-        import selectors2 as _system_selectors  # type: ignore[no-redef]
-    except ImportError:
-        _system_selectors = None  # type: types.ModuleType | None  # type: ignore[no-redef]
+from ansible.module_utils.common.warnings import deprecate
 
-if _system_selectors:
-    selectors = _system_selectors
-else:
-    # Our bundled copy
-    from ansible.module_utils.compat import _selectors2 as selectors  # type: ignore[no-redef]
+
 sys.modules['ansible.module_utils.compat.selectors'] = selectors
+
+
+deprecate(
+    msg='The `ansible.module_utils.compat.selectors` module is deprecated.',
+    version='2.19',
+)
