08eb33861303da92ee19d68c74192717d3dc623b
Matt Clay <matt@mystile.com>
2023-10-11 17:31:04 -0700

    Unit test cleanup (#81958)
    Remove obsolete Python 2.7 and 3.6 support code and fix up existing tests.

--- test/units/utils/collection_loader/fixtures/collections/ansible_collections/testns/testcoll/plugins/module_utils/my_util.py
+++ test/units/utils/collection_loader/fixtures/collections/ansible_collections/testns/testcoll/plugins/module_utils/my_util.py
@@ -1,6 +1,6 @@
 # WARNING: Changing line numbers of code in this file will break collection tests that use tracing to check paths and line numbers.
-from __future__ import annotations
+#          Also, do not import annotations from __future__ as this will break detection of __future__ inheritance.
 
 
-def question():
+def question() -> float:
     return 3 / 2
