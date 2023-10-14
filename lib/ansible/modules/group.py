0aac02e8672c3a960f0a9915951006b630e0b4fc
Krishna <113199449+Krishna01work@users.noreply.github.com>
2023-10-14 07:21:42 +0530

    Update group.py

--- lib/ansible/modules/group.py
+++ lib/ansible/modules/group.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2012, Stephen From <sfromm@gmail.com>
+# Copyright: (c) 2012, Stephen Fromm <sfromm@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
