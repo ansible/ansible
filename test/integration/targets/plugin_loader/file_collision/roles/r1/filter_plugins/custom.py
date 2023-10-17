7741f3b07cc713178bae6015629c14a8133f46fe
Brian Coca <brian.coca+git@gmail.com>
2023-10-17 12:41:57 -0400

    added clog and tests

---
+++ test/integration/targets/plugin_loader/file_collision/roles/r1/filter_plugins/custom.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+
+def do_nothing(myval):
+    return myval
+
+
+class FilterModule(object):
+    ''' Ansible core jinja2 filters '''
+
+    def filters(self):
+        return {
+            'filter1': do_nothing,
+            'filter3': do_nothing,
+        }
+
