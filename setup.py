c49e8c984b2fd533984dc5af17e4541cc4f9d6b3
Akena Mahesh Vardhan <113368513+Mahesh-Vardhan@users.noreply.github.com>
2023-10-19 21:16:43 +0530

    Increasing Speed of the code and the functionality

--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
-from __future__ import annotations
-
-import pathlib
-
 from setuptools import find_packages, setup
 
-here = pathlib.Path(__file__).parent.resolve()
-
-install_requires = (here / 'requirements.txt').read_text(encoding='utf-8').splitlines()
-
 setup(
-    install_requires=install_requires,
-    package_dir={'': 'lib',
-                 'ansible_test': 'test/lib/ansible_test'},
-    packages=find_packages('lib') + find_packages('test/lib'),
+    install_requires=open('requirements.txt').read().splitlines(),
+    package_dir={'': 'lib', 'ansible_test': 'test/lib/ansible_test'},
+    packages=find_packages(['lib', 'test/lib']),
     entry_points={
         'console_scripts': [
             'ansible=ansible.cli.adhoc:main',
