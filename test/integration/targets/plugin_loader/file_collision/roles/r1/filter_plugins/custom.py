bcccba0f49004cd7b62995b4aa3e77e516b73aaa
Brian Coca <bcoca@users.noreply.github.com>
2023-10-20 19:00:41 -0400

    Fix Jinja plugin deduplication (#82002)
    for j2 plugins dedupe on path and  not basename
    for j2 this is a container file , for other plugins file name == plugin name

---
+++ test/integration/targets/plugin_loader/file_collision/roles/r1/filter_plugins/custom.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+
+def do_nothing(myval):
+    return myval
+
+
+class FilterModule(object):
+    ''' Ansible core jinja2 filters '''
+
+    def filters(self):
+        return {
+            'filter1': do_nothing,
+            'filter3': do_nothing,
+        }
---
+++ test/integration/targets/plugin_loader/file_collision/roles/r2/filter_plugins/custom.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+
+def do_nothing(myval):
+    return myval
+
+
+class FilterModule(object):
+    ''' Ansible core jinja2 filters '''
+
+    def filters(self):
+        return {
+            'filter2': do_nothing,
+        }
