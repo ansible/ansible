c9677cbcce20c51f0e52ffeebb41652ac4957105
Aditya Putta <puttaa@skiff.com>
2023-10-20 14:19:41 -0500

    Use a Specific Package Manager

--- lib/ansible/modules/package.py
+++ lib/ansible/modules/package.py
@@ -83,4 +83,10 @@ EXAMPLES = '''
       - httpd
       - mariadb-server
     state: latest
+
+- name: Use YUM to Install a Package
+  ansible.builtin.package:
+    name: httpd
+    state: present
+    use: yum    
 '''
