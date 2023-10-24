9aa1c35cf3dbd475a74571708a8c38bdd15adca0
Brian Coca <bcoca@users.noreply.github.com>
2023-10-24 09:08:56 -0400

    Update lib/ansible/plugins/connection/ssh.py
    Co-authored-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/plugins/connection/ssh.py
+++ lib/ansible/plugins/connection/ssh.py
@@ -20,7 +20,7 @@ DOCUMENTATION = '''
     version_added: historical
     notes:
         - This plugin is mostly a wrapper to the ``ssh`` CLI utility and the exact behavior of the options depends on this tool.
-          This means that the documentation provided here is subject to be overriden by the CLI tool itself.
+          This means that the documentation provided here is subject to be overridden by the CLI tool itself.
         - Many options default to V(None) here but that only means we do not override the SSH tool's defaults and/or configuration.
           For example, if you specify the port in this plugin it will override any C(Port) entry in your C(.ssh/config).
         - The ssh CLI tool uses return code 255 as a 'connection error', this can conflict with commands/tools that
