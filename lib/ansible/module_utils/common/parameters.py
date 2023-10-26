47736a3d3525277d4ed8c2a0663701bfd6628361
Brian Coca <brian.coca+git@gmail.com>
2023-10-26 10:33:18 -0400

    fixed params

--- lib/ansible/module_utils/common/parameters.py
+++ lib/ansible/module_utils/common/parameters.py
@@ -89,6 +89,7 @@ PASS_VARS = {
     'diff': ('_diff', False),
     'keep_remote_files': ('_keep_remote_files', False),
     'ignore_unknown_opts': ('_ignore_unknown_opts', False),
+    'live_updates': ('live', None),
     'module_name': ('_name', None),
     'no_log': ('no_log', False),
     'remote_tmp': ('_remote_tmp', None),
@@ -101,10 +102,9 @@ PASS_VARS = {
     'tmpdir': ('_tmpdir', None),
     'verbosity': ('_verbosity', 0),
     'version': ('ansible_version', '0.0'),
-    'live': ('live_updates', None),
 }
 
-PASS_BOOLS = ('check_mode', 'debug', 'diff', 'keep_remote_files', 'ignore_unknown_opts', 'no_log')
+PASS_BOOLS = ('check_mode', 'debug', 'diff', 'keep_remote_files', 'ignore_unknown_opts', 'live', 'no_log')
 
 DEFAULT_TYPE_VALIDATORS = {
     'str': check_type_str,
