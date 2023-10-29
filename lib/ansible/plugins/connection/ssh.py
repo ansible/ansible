6439c16575de2620c9501e74369a2035fbf24f20
Deeptanshu Das <97671843+DeeptanshuDas@users.noreply.github.com>
2023-10-29 21:11:30 +0530

    Update ssh.py

--- lib/ansible/plugins/connection/ssh.py
+++ lib/ansible/plugins/connection/ssh.py
@@ -469,6 +469,12 @@ def _handle_error(
                               'Upgrade sshpass to use sshpass_prompt, or otherwise switch to ssh keys.'
                     raise AnsibleError('{0} {1}'.format(msg, details))
                 msg = '{0} {1}'.format(msg, details)
+                else:
+                  if "authentication failed" in to_native(return_tuple[2]):
+                      raise AnsibleAuthenticationFailure(msg)
+                  else:
+                      raise AnsibleConnectionFailure(msg)
+
 
     if return_tuple[0] == 255:
         SSH_ERROR = True
