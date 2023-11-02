61a427428c5f4bf99b69ad921182701410729f94
Matt Clay <matt@mystile.com>
2023-11-02 14:23:03 -0700

    Fail collection loader test if wrong code runs

--- test/units/utils/collection_loader/fixtures/collections/ansible_collections/testns/testcoll/plugins/action/my_action.py
+++ test/units/utils/collection_loader/fixtures/collections/ansible_collections/testns/testcoll/plugins/action/my_action.py
@@ -4,4 +4,4 @@ from ..module_utils.my_util import question  # pylint: disable=unused-import
 
 
 def action_code():
-    return "hello from my_action.py"
+    raise Exception('hello from my_action.py, this code should never execute')  # pragma: nocover
