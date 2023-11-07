f86bba06332544d6dcee96ab617ea811dea9f9b2
Matt Clay <matt@mystile.com>
2023-11-07 12:13:48 -0800

    Remove unused imports

--- test/units/module_utils/basic/test_run_command.py
+++ test/units/module_utils/basic/test_run_command.py
@@ -4,7 +4,6 @@
 
 from __future__ import annotations
 
-import errno
 import selectors
 from itertools import product
 from io import BytesIO
--- test/units/module_utils/basic/test_tmpdir.py
+++ test/units/module_utils/basic/test_tmpdir.py
@@ -6,7 +6,6 @@ from __future__ import annotations
 
 import json
 import os
-import shutil
 import tempfile
 
 import pytest
