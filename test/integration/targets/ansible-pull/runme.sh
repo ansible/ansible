8825e60add98f3400497928105a0747cbfa2346f
Brian Coca <bcoca@users.noreply.github.com>
2023-11-15 10:09:04 -0500

    ansible-pull: expand destinantion directoy to avoid purgin in / (#82030)
    * expand destinantion directoy to avoid purgin in /
    
      bad things could happen and help alone is not enough

--- test/integration/targets/ansible-pull/runme.sh
+++ test/integration/targets/ansible-pull/runme.sh
@@ -88,3 +88,6 @@ ANSIBLE_CONFIG='' ansible-pull -d "${pull_dir}" -U "${repo_dir}" "$@" multi_play
 pass_tests_multi
 
 ANSIBLE_CONFIG='' ansible-pull -d "${pull_dir}" -U "${repo_dir}" conn_secret.yml --connection-password-file "${repo_dir}/secret_connection_password" "$@"
+
+# fail if we try do delete /var/tmp
+ANSIBLE_CONFIG='' ansible-pull -d var/tmp -U "${repo_dir}" --purge "$@"
