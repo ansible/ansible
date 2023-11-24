77c874e3595855daea5e076fa57123b6f03408fc
Abhijeet Kasurde <akasurde@redhat.com>
2023-11-24 07:26:58 -0800

    find: do not fail on PermissionError
    * Log and skip permission errors on files and directories
    
    Fixes: #82027
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/find.py
+++ lib/ansible/modules/find.py
@@ -262,6 +262,7 @@ skipped_paths:
     version_added: '2.12'
 '''
 
+import errno
 import fnmatch
 import grp
 import os
@@ -447,6 +448,8 @@ def statinfo(st):
 
 
 def handle_walk_errors(e):
+    if e.errno in (errno.EPERM, errno.EACCES):
+        return
     raise e
 
 
