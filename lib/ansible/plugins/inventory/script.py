250adc1e7a14b32232df0b3287fbb9b6499434bf
Brian Coca <brian.coca+git@gmail.com>
2023-12-05 10:06:33 -0500

    Added example to scripts inventory plugin

--- lib/ansible/plugins/inventory/script.py
+++ lib/ansible/plugins/inventory/script.py
@@ -23,13 +23,53 @@ DOCUMENTATION = '''
         - The source provided must be an executable that returns Ansible inventory JSON
         - The source must accept C(--list) and C(--host <hostname>) as arguments.
           C(--host) will only be used if no C(_meta) key is present.
-          This is a performance optimization as the script would be called per host otherwise.
+          This is a performance optimization as the script would be called one additional time per host otherwise.
     notes:
         - Enabled in configuration by default.
         - The plugin does not cache results because external inventory scripts are responsible for their own caching.
         - To write your own inventory script see (R(Developing dynamic inventory,developing_inventory) from the documentation site.
         - To find the scripts that used to be part of the code release, go to U(https://github.com/ansible-community/contrib-scripts/).
 '''
+
+EXAMPLES = '''
+
+   #! /bin/bash
+
+   if [ "$1" == "--list" ]; then
+   cat<<EOF
+   {
+     "bash_hosts": {
+       "hosts": [
+         "myhost.domain.com",
+         "myhost2,domain.com"
+       ],
+       "vars": {
+         "host_test": "test-value"
+       }
+     },
+     "_meta": {
+       "hostvars": {
+         "myhost.domain.com": {
+           "host_specific_test_var": "test-value"
+         }
+       }
+     }
+   }
+   EOF
+   elif [ "$1" == "--host" ]; then
+     # this should not normally be called by Ansible as we return _meta above
+     if [ "$2" == "myhost.domain.com" ]; then
+        echo '{"_meta": {hostvars": {"myhost.domain.com": {"host_specific-test_var": "test-value"}}}}'
+     else
+        echo '{"_meta": {hostvars": {}}}'
+     fi
+   else
+     echo "Invalid option: use --list or --host <hostname>"
+     exit 1
+   fi
+
+'''
+
 
 import os
 import subprocess
