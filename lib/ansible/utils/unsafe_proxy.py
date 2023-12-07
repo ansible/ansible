afe3fc184f135ec707327d3349ce2ad6a368961d
Matt Martz <matt@sivel.net>
2023-12-07 17:17:14 -0600

    Additional Unsafe fixes (#82376)
    * Allow older pickle protocols to pickle unsafe classes. Fixes #82356
    
    * Address issues when iterating or getting single index from AnsibleUnsafeBytes. Fixes #82375
    
    * clog frag

--- lib/ansible/utils/unsafe_proxy.py
+++ lib/ansible/utils/unsafe_proxy.py
@@ -71,6 +71,9 @@ class AnsibleUnsafeBytes(bytes, AnsibleUnsafe):
     def _strip_unsafe(self):
         return super().__bytes__()
 
+    def __reduce__(self, /):
+        return (self.__class__, (self._strip_unsafe(),))
+
     def __str__(self, /):  # pylint: disable=invalid-str-returned
         return self.decode()
 
@@ -84,11 +87,9 @@ class AnsibleUnsafeBytes(bytes, AnsibleUnsafe):
         return AnsibleUnsafeText(super().__format__(format_spec))
 
     def __getitem__(self, key, /):
+        if isinstance(key, int):
+            return super().__getitem__(key)
         return self.__class__(super().__getitem__(key))
-
-    def __iter__(self, /):
-        cls = self.__class__
-        return (cls(c) for c in super().__iter__())
 
     def __reversed__(self, /):
         return self[::-1]
@@ -191,6 +192,9 @@ class AnsibleUnsafeBytes(bytes, AnsibleUnsafe):
 class AnsibleUnsafeText(str, AnsibleUnsafe):
     def _strip_unsafe(self, /):
         return super().__str__()
+
+    def __reduce__(self, /):
+        return (self.__class__, (self._strip_unsafe(),))
 
     def __str__(self, /):  # pylint: disable=invalid-str-returned
         return self
