3813c59f431588501b1f90aa99dcb8b86333f97c
Mark Chappell <mchappel@redhat.com>
2023-12-08 13:58:28 +0100

    linting

--- test/integration/targets/ansible-test-sanity-yamllint/ansible_collections/ns/col/plugins/modules/module1.py
+++ test/integration/targets/ansible-test-sanity-yamllint/ansible_collections/ns/col/plugins/modules/module1.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import annotations
 
 DOCUMENTATION = r"""
 module: module1
