df7324597d26866f354ca8dc5a2e62eab60d092c
Sloane Hertel <19572925+s-hertel@users.noreply.github.com>
2024-01-11 14:11:56 -0500

    [ansible-galaxy] Remove role name conversion when the repo startswith 'ansible-role' (#82508)
    * Remove role name conversion based on whether the repo name starts with
    ansible-role
    
    This was added in 2.3 to match the Galaxy ui behavior of truncating the
    'ansible-role-' prefix automatically, but the new backend requires an
    alternate name to be provided or defined in the ``galaxy_info``
    metadata.
    
    Roles that were imported using the ansible-role-$name convention will
    need to use ``--role-name`` or add ``role_name`` to the ``galaxy_info``
    dictionary in ``meta/main.yml``.
    
    changelog
    
    * Update changelogs/fragments/fix-default-ansible-galaxy-role-import-name.yml
    
    * Update changelogs/fragments/fix-default-ansible-galaxy-role-import-name.yml
    
    Co-authored-by: flowerysong <junk+github@flowerysong.com>
    
    ---------
    
    Co-authored-by: flowerysong <junk+github@flowerysong.com>

--- lib/ansible/galaxy/api.py
+++ lib/ansible/galaxy/api.py
@@ -482,8 +482,6 @@ class GalaxyAPI:
         }
         if role_name:
             args['alternate_role_name'] = role_name
-        elif github_repo.startswith('ansible-role'):
-            args['alternate_role_name'] = github_repo[len('ansible-role') + 1:]
         data = self._call_galaxy(url, args=urlencode(args), method="POST")
         if data.get('results', None):
             return data['results']
