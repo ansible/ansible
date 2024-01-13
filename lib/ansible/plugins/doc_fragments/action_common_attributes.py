dae275145fbe5f9c7f2f09e7c73fdf75fb891f33
unknown <souroagorouko@gmail.com>
2024-01-13 21:58:43 +0000

    Update attribute description for check_mode

--- lib/ansible/plugins/doc_fragments/action_common_attributes.py
+++ lib/ansible/plugins/doc_fragments/action_common_attributes.py
@@ -10,7 +10,7 @@ class ModuleDocFragment(object):
     DOCUMENTATION = r'''
 attributes:
     check_mode:
-      description: Can run in check_mode and return changed status prediction without modifying target
+      description: Full support means that it can be run in check_mode and return changed status prediction without modifying target
     diff_mode:
       description: Will return details on what has changed (or possibly needs changing in check_mode), when in diff mode
     platform:
