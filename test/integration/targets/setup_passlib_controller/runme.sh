f3dc15275fd4aa6d2b97a30cef9888bad1cdab47
Martin Krizek <martin.krizek@gmail.com>
2024-01-17 20:29:53 +0100

    Update test/integration/targets/setup_passlib_controller/runme.sh
    Co-authored-by: Matt Clay <matt@mystile.com>

--- test/integration/targets/setup_passlib_controller/runme.sh
+++ test/integration/targets/setup_passlib_controller/runme.sh
@@ -2,6 +2,11 @@
 
 set -eux
 
+# Temporary hack for PEP 668 on newer systems.
+# Remove once ansible-test can provide targets their own virtual environment.
+# Tests which can manage their own virtual environment should not use this approach.
+export PIP_BREAK_SYSTEM_PACKAGES=1
+
 # Requirements have to be installed prior to running ansible-playbook
 # because plugins and requirements are loaded before the task runs
 python -m pip install passlib
