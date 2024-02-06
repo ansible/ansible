a452c53375e0a05900103df25363fc055fb51543
Sloane Hertel <19572925+s-hertel@users.noreply.github.com>
2024-02-06 10:13:47 -0500

    import_role - allow subdirectories with _from options (#82642)
    * Allow subdirectories with import_role _from options
    
    Add tests that tasks_from is restricted to the role
    
    Note that a task like:
    
    - import_role:
        name: role
        tasks_from: tasks/entrypoint.yml
    
    will now load tasks from "{{ role_path }}/tasks/tasks/entrypoint.yml"
    instead of "{{ role_path }}/tasks/entrypoint.yml". This change in
    behavior matches include_role.
    
    * better test case (filename doesn't match one in tasks/)
    
    Fixes #82584

--- test/integration/targets/roles/runme.sh
+++ test/integration/targets/roles/runme.sh
@@ -26,13 +26,19 @@ ansible-playbook role_complete.yml -i ../../inventory -i fake, --tags unreachabl
 ansible-playbook data_integrity.yml -i ../../inventory "$@"
 
 # ensure role fails when trying to load 'non role' in  _from
-ansible-playbook no_outside.yml -i ../../inventory > role_outside_output.log 2>&1 || true
-if grep "as it is not inside the expected role path" role_outside_output.log >/dev/null; then
-  echo "Test passed (playbook failed with expected output, output not shown)."
-else
-  echo "Test failed, expected output from playbook failure is missing, output not shown)."
-  exit 1
-fi
+test_no_outside=("no_outside.yml" "no_outside_import.yml")
+for file in "${test_no_outside[@]}"; do
+  ansible-playbook "$file" -i ../../inventory > "${file}_output.log" 2>&1 || true
+  if grep "as it is not inside the expected role path" "${file}_output.log" >/dev/null; then
+    echo "Test passed for $file (playbook failed with expected output, output not shown)."
+  else
+    echo "Test failed for $file, expected output from playbook failure is missing, output not shown)."
+    exit 1
+  fi
+done
+
+# ensure subdir contained to role in tasks_from is valid
+ansible-playbook test_subdirs.yml -i ../../inventory "$@"
 
 # ensure vars scope is correct
 ansible-playbook vars_scope.yml -i ../../inventory "$@"
