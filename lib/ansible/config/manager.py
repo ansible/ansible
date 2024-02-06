622a8a24b13ea9a9d4910b0378a835dcf8bcbbe8
Brian Coca <brian.coca+git@gmail.com>
2024-02-06 12:08:13 -0500

    f string it

--- lib/ansible/config/manager.py
+++ lib/ansible/config/manager.py
@@ -613,10 +613,8 @@ class ConfigManager(object):
         removal = ''
         if include_removal:
             if 'removed_at_date' in dep_docs:
-                removal = "Will be removed in a release after %(removed_at_date)s\n\t"
+                removal = "Will be removed in a release after {dep_docs['removed_at_date']}\n\t"
             else:
-                removal = "Will be removed in: Ansible %(removed_in)s\n\t"
+                removal = "Will be removed in: Ansible {dep_docs['removed_in']}\n\t"
 
-        msg = "Reason: %(why)s\n\t" + removal + "Alternatives: %(alternatives)s"
-
-        return msg % dep_docs
+        return "Reason: {dep_docs['why']}\n\t{removal}Alternatives: {dep_docs['alternatives']}"
