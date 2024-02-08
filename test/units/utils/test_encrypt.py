0cf5546d924754d7f65098da3c4c5d110ddf4d67
Matt Martz <matt@sivel.net>
2024-02-08 14:08:44 -0600

    Add support for crypt/libxcrypt via ctypes, as an alternative to passlib

--- test/units/utils/test_encrypt.py
+++ test/units/utils/test_encrypt.py
@@ -14,8 +14,16 @@ from ansible.utils import encrypt
 
 
 def assert_hash(expected, secret, algorithm, **settings):
-    assert encrypt.do_encrypt(secret, algorithm, **settings) == expected
-    assert encrypt.PasslibHash(algorithm).hash(secret, **settings) == expected
+    if isinstance(expected, tuple):
+        expected_crypt, expected_passlib = expected
+    else:
+        expected_crypt = expected_passlib = expected
+
+    if encrypt.HAS_CRYPT and algorithm in encrypt.CryptHash.algorithms:
+        assert encrypt.CryptHash(algorithm).hash(secret, **settings) == expected_crypt
+
+    if encrypt.PASSLIB_AVAILABLE:
+        assert encrypt.PasslibHash(algorithm).hash(secret, **settings) == expected_passlib
 
 
 @pytest.mark.parametrize(
@@ -26,7 +34,10 @@ def assert_hash(expected, secret, algorithm, **settings):
             None,
             "1234567890123456789012",
             None,
-            "$2b$12$123456789012345678901ugbM1PeTfRQ0t6dCJu5lQA8hwrZOYgDu",
+            (
+                "$2b$12$KRGxLBS0Lxe3KBCwKxOzLe6odk8yM9lJBgNtLuDQxUkLDkpGI6twK",
+                "$2b$12$123456789012345678901ugbM1PeTfRQ0t6dCJu5lQA8hwrZOYgDu",
+            ),
             id="bcrypt_default",
         ),
         pytest.param(
@@ -36,13 +47,17 @@ def assert_hash(expected, secret, algorithm, **settings):
             None,
             "$2$12$123456789012345678901ufd3hZRrev.WXCbemqGIV/gmWaTGLImm",
             id="bcrypt_ident_2",
+            marks=pytest.mark.xfail(reason="crypt_gensalt rejects old bcrypt ident '2', unlike passlib"),
         ),
         pytest.param(
             "bcrypt",
             "2y",
             "1234567890123456789012",
             None,
-            "$2y$12$123456789012345678901ugbM1PeTfRQ0t6dCJu5lQA8hwrZOYgDu",
+            (
+                "$2y$12$KRGxLBS0Lxe3KBCwKxOzLe6odk8yM9lJBgNtLuDQxUkLDkpGI6twK",
+                "$2y$12$123456789012345678901ugbM1PeTfRQ0t6dCJu5lQA8hwrZOYgDu",
+            ),
             id="bcrypt_ident_2y",
         ),
         pytest.param(
@@ -50,7 +65,10 @@ def assert_hash(expected, secret, algorithm, **settings):
             "2a",
             "1234567890123456789012",
             None,
-            "$2a$12$123456789012345678901ugbM1PeTfRQ0t6dCJu5lQA8hwrZOYgDu",
+            (
+                "$2a$12$KRGxLBS0Lxe3KBCwKxOzLe6odk8yM9lJBgNtLuDQxUkLDkpGI6twK",
+                "$2a$12$123456789012345678901ugbM1PeTfRQ0t6dCJu5lQA8hwrZOYgDu",
+            ),
             id="bcrypt_ident_2a",
         ),
         pytest.param(
@@ -58,7 +76,10 @@ def assert_hash(expected, secret, algorithm, **settings):
             "2b",
             "1234567890123456789012",
             None,
-            "$2b$12$123456789012345678901ugbM1PeTfRQ0t6dCJu5lQA8hwrZOYgDu",
+            (
+                "$2b$12$KRGxLBS0Lxe3KBCwKxOzLe6odk8yM9lJBgNtLuDQxUkLDkpGI6twK",
+                "$2b$12$123456789012345678901ugbM1PeTfRQ0t6dCJu5lQA8hwrZOYgDu",
+            ),
             id="bcrypt_ident_2b",
         ),
         pytest.param(
@@ -66,8 +87,9 @@ def assert_hash(expected, secret, algorithm, **settings):
             "invalid_ident",
             "12345678",
             5000,
-            "$5$12345678$uAZsE3BenI2G.nA8DpTl.9Dc8JiqacI53pEqRr5ppT7",
+            "$5$l6nAoIXB$HtZrcvuIcvGwySXwnxGHuwyIha2FvzAt4QebHp43Wq4",
             id="sha256_crypt_invalid_ident",
+            marks=pytest.mark.xfail(reason="crypt_gensalt rejects invalid idents, unlike passlib"),
         ),
         pytest.param(
             "crypt16",
@@ -84,44 +106,55 @@ def test_encrypt_with_ident(algorithm, ident, salt, rounds, expected):
     assert_hash(expected, secret="123", algorithm=algorithm, salt=salt, rounds=rounds, ident=ident)
 
 
-# If passlib is not installed. this is identical to the test_encrypt_with_rounds_no_passlib() test
 @pytest.mark.parametrize(
     ("algorithm", "rounds", "expected"),
     [
         pytest.param(
             "sha256_crypt",
             None,
-            "$5$rounds=535000$12345678$uy3TurUPaY71aioJi58HvUY8jkbhSQU8HepbyaNngv.",
+            (
+                "$5$rounds=535000$l6nAoIXB$A2HBLoxGx60mfezwjZ6VFd9vI1.V4oKpd.iwYU4n776",
+                "$5$rounds=535000$12345678$uy3TurUPaY71aioJi58HvUY8jkbhSQU8HepbyaNngv.",
+            ),
             id="sha256_crypt_default_rounds",
         ),
         pytest.param(
             "sha256_crypt",
             5000,
-            "$5$12345678$uAZsE3BenI2G.nA8DpTl.9Dc8JiqacI53pEqRr5ppT7",
+            ("$5$l6nAoIXB$HtZrcvuIcvGwySXwnxGHuwyIha2FvzAt4QebHp43Wq4", "$5$12345678$uAZsE3BenI2G.nA8DpTl.9Dc8JiqacI53pEqRr5ppT7"),
             id="sha256_crypt_rounds_5000",
         ),
         pytest.param(
             "sha256_crypt",
             10000,
-            "$5$rounds=10000$12345678$JBinliYMFEcBeAXKZnLjenhgEhTmJBvZn3aR8l70Oy/",
+            (
+                "$5$rounds=10000$l6nAoIXB$//KYvMXLmzwUUYyWJmAJAeZuP7rsroMayX9hUhpwRC.",
+                "$5$rounds=10000$12345678$JBinliYMFEcBeAXKZnLjenhgEhTmJBvZn3aR8l70Oy/",
+            ),
             id="sha256_crypt_rounds_10000",
         ),
         pytest.param(
             "sha512_crypt",
             None,
-            "$6$rounds=656000$12345678$InMy49UwxyCh2pGJU1NpOhVSElDDzKeyuC6n6E9O34BCUGVNYADnI.rcA3m.Vro9BiZpYmjEoNhpREqQcbvQ80",
+            (
+                "$6$rounds=656000$l6nAoIXB$wDP2gGfore3TlBdPvi0wqot7zj8oodeHnEPerC1blJBRGEodsNewNfzxM5nYdfPMkvCh5Af/w82wvG2U3PpCT0",
+                "$6$rounds=656000$12345678$InMy49UwxyCh2pGJU1NpOhVSElDDzKeyuC6n6E9O34BCUGVNYADnI.rcA3m.Vro9BiZpYmjEoNhpREqQcbvQ80",
+            ),
             id="sha512_crypt_default_rounds",
         ),
         pytest.param(
             "sha512_crypt",
             5000,
-            "$6$12345678$LcV9LQiaPekQxZ.OfkMADjFdSO2k9zfbDQrHPVcYjSLqSdjLYpsgqviYvTEP/R41yPmhH3CCeEDqVhW1VHr3L.",
+            (
+                "$6$l6nAoIXB$Zva3RkTY95C0FM0fV9WhLyrQO7/jMt1sICo8bQZvpbRIhDYcgiNdL7IzdlGxG6j6CdkWdqeAk4/W49qkAuv/h/",
+                "$6$12345678$LcV9LQiaPekQxZ.OfkMADjFdSO2k9zfbDQrHPVcYjSLqSdjLYpsgqviYvTEP/R41yPmhH3CCeEDqVhW1VHr3L.",
+            ),
             id="sha512_crypt_rounds_5000",
         ),
         pytest.param(
             "md5_crypt",
             None,
-            "$1$12345678$tRy4cXc3kmcfRZVj4iFXr/",
+            ("$1$l6nAoIXB$FCE6sRsviKJtqJWkAK6ff0", "$1$12345678$tRy4cXc3kmcfRZVj4iFXr/"),
             id="md5_crypt_default_rounds",
         ),
     ],
@@ -144,31 +177,31 @@ def test_password_hash_filter_passlib_with_exception():
         pytest.param(
             "sha256",
             None,
-            "$5$rounds=535000$12345678$uy3TurUPaY71aioJi58HvUY8jkbhSQU8HepbyaNngv.",
+            "$5$rounds=535000$l6nAoIXB$A2HBLoxGx60mfezwjZ6VFd9vI1.V4oKpd.iwYU4n776",
             id="sha256_default_rounds",
         ),
         pytest.param(
             "sha256",
             5000,
-            "$5$12345678$uAZsE3BenI2G.nA8DpTl.9Dc8JiqacI53pEqRr5ppT7",
+            "$5$l6nAoIXB$HtZrcvuIcvGwySXwnxGHuwyIha2FvzAt4QebHp43Wq4",
             id="sha256_rounds_5000",
         ),
         pytest.param(
             "sha256",
             10000,
-            "$5$rounds=10000$12345678$JBinliYMFEcBeAXKZnLjenhgEhTmJBvZn3aR8l70Oy/",
+            "$5$rounds=10000$l6nAoIXB$//KYvMXLmzwUUYyWJmAJAeZuP7rsroMayX9hUhpwRC.",
             id="sha256_rounds_10000",
         ),
         pytest.param(
             "sha512",
             5000,
-            "$6$12345678$LcV9LQiaPekQxZ.OfkMADjFdSO2k9zfbDQrHPVcYjSLqSdjLYpsgqviYvTEP/R41yPmhH3CCeEDqVhW1VHr3L.",
+            "$6$l6nAoIXB$Zva3RkTY95C0FM0fV9WhLyrQO7/jMt1sICo8bQZvpbRIhDYcgiNdL7IzdlGxG6j6CdkWdqeAk4/W49qkAuv/h/",
             id="sha512_rounds_5000",
         ),
         pytest.param(
             "sha512",
             6000,
-            "$6$rounds=6000$12345678$l/fC67BdJwZrJ7qneKGP1b6PcatfBr0dI7W6JLBrsv8P1wnv/0pu4WJsWq5p6WiXgZ2gt9Aoir3MeORJxg4.Z/",
+            "$6$rounds=6000$l6nAoIXB$DMD7Me00a9FHa5hF22mKek6.Hf7dD6UvJBuRLKus7K//G2kRwcKx5pkp5vGDk5E/MEQgR0yaEsv6ooq3iGRqp/",
             id="sha512_rounds_6000",
         ),
     ],
