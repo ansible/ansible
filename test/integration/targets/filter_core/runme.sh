f81ccbf5f183f57054840421f724d0beeadef23a
Matt Martz <matt@sivel.net>
2024-02-12 12:24:39 -0600

    site-packages fixes

--- test/integration/targets/filter_core/runme.sh
+++ test/integration/targets/filter_core/runme.sh
@@ -7,6 +7,6 @@ ANSIBLE_ROLES_PATH=../ ansible-playbook handle_undefined_type_errors.yml "$@"
 
 # Remove passlib installed by setup_passlib_controller
 source virtualenv.sh
-SITE_PACKAGES=$(python -c "import sysconfig; print(sysconfig.get_path('purelib', scheme='venv'))")
-echo "raise ImportError('passlib')" > ${SITE_PACKAGES}/passlib.py
+SITE_PACKAGES=$(python -c "import sysconfig; print(sysconfig.get_path('purelib'))")
+echo "raise ImportError('passlib')" > "${SITE_PACKAGES}/passlib.py"
 ANSIBLE_ROLES_PATH=../ ansible-playbook password_hash.yml "$@"
