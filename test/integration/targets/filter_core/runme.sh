0eb5bc8f20fbfd8b6838c7dce32b3f3cc85161cf
Matt Martz <matt@sivel.net>
2024-02-12 12:11:45 -0600

    bypass OS installed passlib

--- test/integration/targets/filter_core/runme.sh
+++ test/integration/targets/filter_core/runme.sh
@@ -6,5 +6,7 @@ ANSIBLE_ROLES_PATH=../ ansible-playbook runme.yml "$@"
 ANSIBLE_ROLES_PATH=../ ansible-playbook handle_undefined_type_errors.yml "$@"
 
 # Remove passlib installed by setup_passlib_controller
-python -m pip uninstall -y passlib
+source virtualenv.sh
+SITE_PACKAGES=$(python -c "import sysconfig; print(sysconfig.get_path('purelib', scheme='venv'))")
+echo "raise ImportError('passlib')" > ${SITE_PACKAGES}/passlib.py
 ANSIBLE_ROLES_PATH=../ ansible-playbook password_hash.yml "$@"
