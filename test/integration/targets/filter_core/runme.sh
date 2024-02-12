cfed6c60b0593c358ef7a96ef28c13ff26b4d949
Matt Martz <matt@sivel.net>
2024-02-12 10:37:07 -0600

    Add tests for crypt

--- test/integration/targets/filter_core/runme.sh
+++ test/integration/targets/filter_core/runme.sh
@@ -4,3 +4,7 @@ set -eux
 
 ANSIBLE_ROLES_PATH=../ ansible-playbook runme.yml "$@"
 ANSIBLE_ROLES_PATH=../ ansible-playbook handle_undefined_type_errors.yml "$@"
+
+# Remove passlib installed by setup_passlib_controller
+python -m pip uninstall -y passlib
+ANSIBLE_ROLES_PATH=../ ansible-playbook password_hash.yml "$@"
