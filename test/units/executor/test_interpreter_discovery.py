ac232f9b9635148ce9ac4af7079a63d8af2187f3
Matt Martz <matt@sivel.net>
2024-02-14 13:16:54 -0600

    Updates to interpreter discovery for 2.17 (#82420)

--- test/units/executor/test_interpreter_discovery.py
+++ test/units/executor/test_interpreter_discovery.py
@@ -12,63 +12,63 @@ from ansible.module_utils.common.text.converters import to_text
 from ansible.errors import AnsibleConnectionFailure
 
 mock_ubuntu_platform_res = to_text(
-    r'{"osrelease_content": "NAME=\"Ubuntu\"\nVERSION=\"16.04.5 LTS (Xenial Xerus)\"\nID=ubuntu\nID_LIKE=debian\n'
-    r'PRETTY_NAME=\"Ubuntu 16.04.5 LTS\"\nVERSION_ID=\"16.04\"\nHOME_URL=\"http://www.ubuntu.com/\"\n'
-    r'SUPPORT_URL=\"http://help.ubuntu.com/\"\nBUG_REPORT_URL=\"http://bugs.launchpad.net/ubuntu/\"\n'
-    r'VERSION_CODENAME=xenial\nUBUNTU_CODENAME=xenial\n", "platform_dist_result": ["Ubuntu", "16.04", "xenial"]}'
+    r'{"osrelease_content": "NAME=\"Ansible Test\"\nVERSION=\"100\"\nID=ansible-test\nID_LIKE=debian\n'
+    r'PRETTY_NAME=\"Ansible Test 100\"\nVERSION_ID=\"100\"\nHOME_URL=\"http://ansible.com/\"\n'
+    r'SUPPORT_URL=\"http://github.com/ansible/ansible\"\nBUG_REPORT_URL=\"http://github.com/ansible/ansible/\"\n'
+    r'VERSION_CODENAME=beans\nUBUNTU_CODENAME=beans\n", "platform_dist_result": ["Ansible Test", "100", "beans"]}'
 )
 
 
 def test_discovery_interpreter_linux_auto_legacy():
-    res1 = u'PLATFORM\nLinux\nFOUND\n/usr/bin/python\n/usr/bin/python3\nENDFOUND'
+    res1 = u'PLATFORM\nLinux\nFOUND\n/usr/bin/python99\n/usr/bin/python3\nENDFOUND'
 
     mock_action = MagicMock()
     mock_action._low_level_execute_command.side_effect = [{'stdout': res1}, {'stdout': mock_ubuntu_platform_res}]
 
     actual = discover_interpreter(mock_action, 'python', 'auto_legacy', {'inventory_hostname': u'host-fóöbär'})
 
-    assert actual == u'/usr/bin/python'
+    assert actual == u'/usr/bin/python3'
     assert len(mock_action.method_calls) == 3
     assert mock_action.method_calls[2][0] == '_discovery_warnings.append'
-    assert u'Distribution Ubuntu 16.04 on host host-fóöbär should use /usr/bin/python3, but is using /usr/bin/python' \
+    assert u'Distribution Ansible Test 100 on host host-fóöbär should use /usr/bin/python99, but is using /usr/bin/python3' \
            u' for backward compatibility' in mock_action.method_calls[2][1][0]
 
 
 def test_discovery_interpreter_linux_auto_legacy_silent():
-    res1 = u'PLATFORM\nLinux\nFOUND\n/usr/bin/python\n/usr/bin/python3\nENDFOUND'
+    res1 = u'PLATFORM\nLinux\nFOUND\n/usr/bin/python3.9\n/usr/bin/python3\nENDFOUND'
 
     mock_action = MagicMock()
     mock_action._low_level_execute_command.side_effect = [{'stdout': res1}, {'stdout': mock_ubuntu_platform_res}]
 
     actual = discover_interpreter(mock_action, 'python', 'auto_legacy_silent', {'inventory_hostname': u'host-fóöbär'})
 
-    assert actual == u'/usr/bin/python'
+    assert actual == u'/usr/bin/python3'
     assert len(mock_action.method_calls) == 2
 
 
 def test_discovery_interpreter_linux_auto():
-    res1 = u'PLATFORM\nLinux\nFOUND\n/usr/bin/python\n/usr/bin/python3\nENDFOUND'
+    res1 = u'PLATFORM\nLinux\nFOUND\n/usr/bin/python99\n/usr/bin/python3\nENDFOUND'
 
     mock_action = MagicMock()
     mock_action._low_level_execute_command.side_effect = [{'stdout': res1}, {'stdout': mock_ubuntu_platform_res}]
 
     actual = discover_interpreter(mock_action, 'python', 'auto', {'inventory_hostname': u'host-fóöbär'})
 
-    assert actual == u'/usr/bin/python3'
+    assert actual == u'/usr/bin/python99'
     assert len(mock_action.method_calls) == 2
 
 
 def test_discovery_interpreter_non_linux():
     mock_action = MagicMock()
     mock_action._low_level_execute_command.return_value = \
-        {'stdout': u'PLATFORM\nDarwin\nFOUND\n/usr/bin/python\nENDFOUND'}
+        {'stdout': u'PLATFORM\nDarwin\nFOUND\n/usr/bin/python3\nENDFOUND'}
 
     actual = discover_interpreter(mock_action, 'python', 'auto_legacy', {'inventory_hostname': u'host-fóöbär'})
 
-    assert actual == u'/usr/bin/python'
+    assert actual == u'/usr/bin/python3'
     assert len(mock_action.method_calls) == 2
     assert mock_action.method_calls[1][0] == '_discovery_warnings.append'
-    assert u'Platform darwin on host host-fóöbär is using the discovered Python interpreter at /usr/bin/python, ' \
+    assert u'Platform darwin on host host-fóöbär is using the discovered Python interpreter at /usr/bin/python3, ' \
            u'but future installation of another Python interpreter could change the meaning of that path' \
            in mock_action.method_calls[1][1][0]
 
@@ -79,7 +79,7 @@ def test_no_interpreters_found():
 
     actual = discover_interpreter(mock_action, 'python', 'auto_legacy', {'inventory_hostname': u'host-fóöbär'})
 
-    assert actual == u'/usr/bin/python'
+    assert actual == u'/usr/bin/python3'
     assert len(mock_action.method_calls) == 2
     assert mock_action.method_calls[1][0] == '_discovery_warnings.append'
     assert u'No python interpreters found for host host-fóöbär (tried' \
