00f40c92dba047a1e88f8215aac6ac89ad6cd9ce
s-hertel <19572925+s-hertel@users.noreply.github.com>
2024-02-21 19:51:08 -0500

    deprecate both and fix up the changelog

--- lib/ansible/module_utils/common/validation.py
+++ lib/ansible/module_utils/common/validation.py
@@ -13,7 +13,7 @@ from ansible.module_utils.common.text.converters import to_native
 from ansible.module_utils.common.collections import is_iterable
 from ansible.module_utils.common.text.converters import jsonify
 from ansible.module_utils.common.text.formatters import human_to_bytes
-from ansible.module_utils.common.warnings import warn, deprecate
+from ansible.module_utils.common.warnings import deprecate
 from ansible.module_utils.parsing.convert_bool import boolean
 from ansible.module_utils.six import (
     binary_type,
@@ -387,7 +387,7 @@ def check_required_none(required_options, argument_spec, parameters, options_con
         if allow_none_value is False:
             raise TypeError(to_native(msg))
         else:
-            warn(msg)
+            deprecate(msg, version='2.20')
 
 
 def check_missing_parameters(parameters, required_parameters=None):
