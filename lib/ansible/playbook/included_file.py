4633d8313c6295231c3b276b2fff8fd649b1e648
Thomas W <69774548+yctomwang@users.noreply.github.com>
2024-03-08 06:08:29 +1100

    Fix  handling of ansible_loop_var and ansible_index_var inside ansible.builtin.include_tasks (#82789)
    * added integration test for issue 82655 regarding ansible_loop_var and ansible_index_var inside included yml
    
    * ensure correct handling of ansible_loop_var and ansible_index_var inside ansible builtin include_tasks Co-authored-by: Matt <matt@sivel.net>
    
    * added changelog
    
    * fixed new line issue

--- lib/ansible/playbook/included_file.py
+++ lib/ansible/playbook/included_file.py
@@ -95,8 +95,10 @@ class IncludedFile:
                     index_var = include_result.get('ansible_index_var')
                     if loop_var in include_result:
                         task_vars[loop_var] = special_vars[loop_var] = include_result[loop_var]
+                        task_vars['ansible_loop_var'] = special_vars['ansible_loop_var'] = loop_var
                     if index_var and index_var in include_result:
                         task_vars[index_var] = special_vars[index_var] = include_result[index_var]
+                        task_vars['ansible_index_var'] = special_vars['ansible_index_var'] = index_var
                     if '_ansible_item_label' in include_result:
                         task_vars['_ansible_item_label'] = special_vars['_ansible_item_label'] = include_result['_ansible_item_label']
                     if 'ansible_loop' in include_result:
