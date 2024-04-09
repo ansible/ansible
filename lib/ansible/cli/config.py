b83bffdf6f3f36fb913cb8148b1d8d611d318fdd
Brian Coca <bcoca@users.noreply.github.com>
2024-04-09 14:57:26 -0400

    ansible-config: ensure we get templated default (#82974)
    AKA all defaults rendered
    
    (cherry picked from commit 46137127a238d8e4b86a648cfda972a90f22b729)

--- lib/ansible/cli/config.py
+++ lib/ansible/cli/config.py
@@ -270,7 +270,7 @@ class ConfigCLI(CLI):
             if not settings[setting].get('description'):
                 continue
 
-            default = settings[setting].get('default', '')
+            default = self.config.template_default(settings[setting].get('default', ''), get_constants())
             if subkey == 'env':
                 stype = settings[setting].get('type', '')
                 if stype == 'boolean':
@@ -352,7 +352,7 @@ class ConfigCLI(CLI):
                 if entry['key'] not in seen[entry['section']]:
                     seen[entry['section']].append(entry['key'])
 
-                    default = opt.get('default', '')
+                    default = self.config.template_default(opt.get('default', ''), get_constants())
                     if opt.get('type', '') == 'list' and not isinstance(default, string_types):
                         # python lists are not valid ini ones
                         default = ', '.join(default)
@@ -414,14 +414,16 @@ class ConfigCLI(CLI):
             if context.CLIARGS['format'] == 'display':
                 if isinstance(config[setting], Setting):
                     # proceed normally
+                    value = config[setting].value
                     if config[setting].origin == 'default':
                         color = 'green'
+                        value = self.config.template_default(value, get_constants())
                     elif config[setting].origin == 'REQUIRED':
                         # should include '_terms', '_input', etc
                         color = 'red'
                     else:
                         color = 'yellow'
-                    msg = "%s(%s) = %s" % (setting, config[setting].origin, config[setting].value)
+                    msg = "%s(%s) = %s" % (setting, config[setting].origin, value)
                 else:
                     color = 'green'
                     msg = "%s(%s) = %s" % (setting, 'default', config[setting].get('default'))
