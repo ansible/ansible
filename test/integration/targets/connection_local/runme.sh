6ac75a20d09b27b77482cfc4d1219b62317cd7af
Sloane Hertel <19572925+s-hertel@users.noreply.github.com>
2024-04-10 15:07:56 -0400

    Fix traceback inheriting from NetworkConnectionBase and add integration tests (#82954) (#82956)
    (cherry picked from commit 4bddbe69d55589e8ec4935333e5a4d2616d8869f)
    
    Co-authored-by: Jeroen van Bemmel <jvb127@gmail.com>

--- test/integration/targets/connection_local/runme.sh
+++ test/integration/targets/connection_local/runme.sh
@@ -12,3 +12,10 @@ INVENTORY="../connection_${group}/test_connection.inventory" ./test.sh \
     -e local_tmp=/tmp/ansible-local \
     -e remote_tmp=/tmp/ansible-remote \
     "$@"
+
+ANSIBLE_CONNECTION_PLUGINS="../connection_${group}/connection_plugins" INVENTORY="../connection_${group}/test_network_connection.inventory" ./test.sh \
+    -e target_hosts="${group}" \
+    -e action_prefix= \
+    -e local_tmp=/tmp/ansible-local \
+    -e remote_tmp=/tmp/ansible-remote \
+    "$@"
