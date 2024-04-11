8bb639b568da69fc25422ff52196454a8654cad3
Martin Krizek <martin.krizek@gmail.com>
2024-04-11 18:43:58 +0200

    dnf5: replace removed API calls
    https://github.com/rpm-software-management/dnf5/commit/bfb6f32e15c359011e078c81b78d160ef10708dd
    https://github.com/rpm-software-management/dnf5/commit/96c9188f9c5b5a78aa550b9a15ed748e6f306e8d

--- lib/ansible/modules/dnf5.py
+++ lib/ansible/modules/dnf5.py
@@ -496,7 +496,7 @@ class Dnf5Module(YumDnf):
             conf.config_file_path = self.conf_file
 
         try:
-            base.load_config_from_file()
+            base.load_config()
         except RuntimeError as e:
             self.module.fail_json(
                 msg=str(e),
@@ -536,7 +536,8 @@ class Dnf5Module(YumDnf):
         log_router = base.get_logger()
         global_logger = libdnf5.logger.GlobalLogger()
         global_logger.set(log_router.get(), libdnf5.logger.Logger.Level_DEBUG)
-        logger = libdnf5.logger.create_file_logger(base)
+        # FIXME hardcoding the filename does not seem right, should libdnf5 expose the default file name?
+        logger = libdnf5.logger.create_file_logger(base, "dnf5.log")
         log_router.add_logger(logger)
 
         if self.update_cache:
