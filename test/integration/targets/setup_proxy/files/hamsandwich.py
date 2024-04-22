2447f66eda99ee4567bff425d7f2fafeb8e9529b
Matt Martz <matt@sivel.net>
2024-04-22 15:11:27 -0500

    improve plugin

--- test/integration/targets/setup_proxy/files/hamsandwich.py
+++ test/integration/targets/setup_proxy/files/hamsandwich.py
@@ -1,12 +1,32 @@
 from __future__ import annotations
 
 from proxy.http.proxy import HttpProxyBasePlugin
+from proxy.http.parser import HttpParser, httpParserStates
 
 
 class HamSandwichPlugin(HttpProxyBasePlugin):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.parser = None
+        self.parsable = True
+
     def handle_upstream_chunk(self, chunk):
-        headers, sep, body = chunk.tobytes().partition(b'\r\n\r\n')
-        if not sep:
+        if not self.parsable:
             return chunk
-        return memoryview(bytearray(headers + b'\r\nX-Sandwich: ham' + sep + body))
+
+        if not self.parser:
+            self.parser = HttpParser.response(chunk)
+        else:
+            self.parser.parse(chunk)
+
+        if self.parser.state == httpParserStates.INITIALIZED:
+            # This is likely TLS without interception
+            self.parsable = False
+            return chunk
+
+        if not self.parser.is_complete:
+            return None
+
+        self.parser.add_header(b'X-Sandwich', b'ham')
+        return memoryview(bytearray(self.parser.build_response()))
