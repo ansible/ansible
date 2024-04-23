8f8589bd49cdf8e0e70baa90efffb3f941eb2d4b
Martin Krizek <martin.krizek@gmail.com>
2024-04-23 11:10:26 +0200

    insert tasks in place

--- lib/ansible/executor/play_iterator.py
+++ lib/ansible/executor/play_iterator.py
@@ -533,27 +533,21 @@ class PlayIterator:
                 state.child_state = self._insert_tasks_into_state(state.child_state, task_list)
             else:
                 target_block = state._blocks[state.cur_block].copy()
-                before = target_block.block[:state.cur_regular_task]
-                after = target_block.block[state.cur_regular_task:]
-                target_block.block = before + task_list + after
+                target_block.block[state.cur_regular_task:state.cur_regular_task] = task_list
                 state._blocks[state.cur_block] = target_block
         elif state.run_state == IteratingStates.RESCUE:
             if state.child_state:
                 state.child_state = self._insert_tasks_into_state(state.child_state, task_list)
             else:
                 target_block = state._blocks[state.cur_block].copy()
-                before = target_block.rescue[:state.cur_rescue_task]
-                after = target_block.rescue[state.cur_rescue_task:]
-                target_block.rescue = before + task_list + after
+                target_block.rescue[state.cur_rescue_task:state.cur_rescue_task] = task_list
                 state._blocks[state.cur_block] = target_block
         elif state.run_state == IteratingStates.ALWAYS:
             if state.child_state:
                 state.child_state = self._insert_tasks_into_state(state.child_state, task_list)
             else:
                 target_block = state._blocks[state.cur_block].copy()
-                before = target_block.always[:state.cur_always_task]
-                after = target_block.always[state.cur_always_task:]
-                target_block.always = before + task_list + after
+                target_block.always[state.cur_always_task:state.cur_always_task] = task_list
                 state._blocks[state.cur_block] = target_block
         elif state.run_state == IteratingStates.HANDLERS:
             state.handlers[state.cur_handlers_task:state.cur_handlers_task] = [h for b in task_list for h in b.block]
