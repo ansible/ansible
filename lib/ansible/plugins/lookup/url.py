fcc6d0bd0c10421a803bddd66529e1c724bf8b04
Abhijeet Kasurde <akasurde@redhat.com>
2024-05-01 12:03:48 -0700

    [stable-2.17] uri: update docs for follow_redirects
    * Updated the docs for the parameter follow_redirects
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/plugins/lookup/url.py
+++ lib/ansible/plugins/lookup/url.py
@@ -99,12 +99,12 @@ options:
         - section: url_lookup
           key: follow_redirects
     choices:
-        - urllib2
-        - all
-        - 'yes'
-        - safe
-        - none
-        - 'no'
+      all: Will follow all redirects.
+      none: Will not follow any redirects.
+      safe: Only redirects doing GET or HEAD requests will be followed.
+      urllib2: Defer to urllib2 behavior (As of writing this follows HTTP redirects).
+      'no': (DEPRECATED, will be removed in the future version) alias of V(none).
+      'yes': (DEPRECATED, will be removed in the future version) alias of V(all).
   use_gssapi:
     description:
     - Use GSSAPI handler of requests
