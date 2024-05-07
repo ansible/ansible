17a43fcbd03184dc79f6049778b8fbccf414aebc
Martin Krizek <martin.krizek@gmail.com>
2024-05-07 17:49:53 +0200

    remove unused import

--- lib/ansible/plugins/strategy/linear.py
+++ lib/ansible/plugins/strategy/linear.py
@@ -31,7 +31,6 @@ DOCUMENTATION = '''
 
 from ansible import constants as C
 from ansible.errors import AnsibleError, AnsibleAssertionError, AnsibleParserError
-from ansible.executor.play_iterator import IteratingStates
 from ansible.module_utils.common.text.converters import to_text
 from ansible.playbook.handler import Handler
 from ansible.playbook.included_file import IncludedFile
