318b53525343111633a0f0c0a505d17b966dac71
Abhijeet Kasurde <akasurde@redhat.com>
2024-05-15 18:00:05 -0700

    CI fixes
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/module_utils/facts/hardware/linux.py
+++ lib/ansible/module_utils/facts/hardware/linux.py
@@ -29,7 +29,6 @@ from multiprocessing.pool import ThreadPool
 
 from ansible.module_utils.common.text.converters import to_text
 from ansible.module_utils.common.locale import get_best_parsable_locale
-from ansible.module_utils.common.process import get_bin_path
 from ansible.module_utils.common.text.formatters import bytes_to_human
 from ansible.module_utils.facts.hardware.base import Hardware, HardwareCollector
 from ansible.module_utils.facts.utils import get_file_content, get_file_lines, get_mount_size
