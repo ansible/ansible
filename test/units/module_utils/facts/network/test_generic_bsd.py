9c69a6e851183d24e05fc8ca5d4468a76c2cd208
Abhijeet Kasurde <akasurde@redhat.com>
2024-05-17 12:38:51 -0700

    Review requests
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- test/units/module_utils/facts/network/test_generic_bsd.py
+++ test/units/module_utils/facts/network/test_generic_bsd.py
@@ -53,55 +53,39 @@ re0: flags=0x8843<UP,BROADCAST,RUNNING,SIMPLEX,MULTICAST> mtu 1500
         inet6 fe80::5054:ff:fe63:55af%re0/64 flags 0x0 scopeid 0x2
 '''
 
-NETBSD_EXPECTED = {
-    "all_ipv4_addresses": ["192.168.122.205"],
-    "all_ipv6_addresses": ["fe80::5054:ff:fe63:55af%re0"],
-    "default_ipv4": {},
-    "default_ipv6": {},
-    "interfaces": ["lo0", "re0"],
-    "lo0": {
-        "device": "lo0",
-        "flags": ["UP", "LOOPBACK", "RUNNING", "MULTICAST"],
-        "ipv4": [
-            {
-                "address": "127.0.0.1",
-                "broadcast": "127.255.255.255",
-                "netmask": "255.0.0.0",
-                "network": "127.0.0.0",
-            }
-        ],
-        "ipv6": [
-            {"address": "::1", "prefix": "128"},
-            {"address": "fe80::1%lo0", "prefix": "64", "scope": "0x1"},
-        ],
-        "macaddress": "unknown",
-        "mtu": "33624",
-        "type": "loopback",
-    },
-    "re0": {
-        "device": "re0",
-        "flags": ["UP", "BROADCAST", "RUNNING", "SIMPLEX", "MULTICAST"],
-        "ipv4": [
-            {
-                "address": "192.168.122.205",
-                "broadcast": "192.168.122.255",
-                "netmask": "255.255.255.0",
-                "network": "192.168.122.0",
-            }
-        ],
-        "ipv6": [
-            {"address": "fe80::5054:ff:fe63:55af%re0", "prefix": "64", "scope": "0x2"}
-        ],
-        "macaddress": "unknown",
-        "media": "Ethernet",
-        "media_options": [],
-        "media_select": "autoselect",
-        "media_type": "100baseTX",
-        "mtu": "1500",
-        "status": "active",
-        "type": "ether",
-    },
-}
+NETBSD_EXPECTED = {'all_ipv4_addresses': ['192.168.122.205'],
+                   'all_ipv6_addresses': ['fe80::5054:ff:fe63:55af%re0'],
+                   'default_ipv4': {},
+                   'default_ipv6': {},
+                   'interfaces': ['lo0', 're0'],
+                   'lo0': {'device': 'lo0',
+                           'flags': ['UP', 'LOOPBACK', 'RUNNING', 'MULTICAST'],
+                           'ipv4': [{'address': '127.0.0.1',
+                                     'broadcast': '127.255.255.255',
+                                     'netmask': '255.0.0.0',
+                                     'network': '127.0.0.0'}],
+                           'ipv6': [{'address': '::1', 'prefix': '128'},
+                                    {'address': 'fe80::1%lo0', 'prefix': '64', 'scope': '0x1'}],
+                           'macaddress': 'unknown',
+                           'mtu': '33624',
+                           'type': 'loopback'},
+                   're0': {'device': 're0',
+                           'flags': ['UP', 'BROADCAST', 'RUNNING', 'SIMPLEX', 'MULTICAST'],
+                           'ipv4': [{'address': '192.168.122.205',
+                                     'broadcast': '192.168.122.255',
+                                     'netmask': '255.255.255.0',
+                                     'network': '192.168.122.0'}],
+                           'ipv6': [{'address': 'fe80::5054:ff:fe63:55af%re0',
+                                     'prefix': '64',
+                                     'scope': '0x2'}],
+                           'macaddress': 'unknown',
+                           'media': 'Ethernet',
+                           'media_options': [],
+                           'media_select': 'autoselect',
+                           'media_type': '100baseTX',
+                           'mtu': '1500',
+                           'status': 'active',
+                           'type': 'ether'}}
 
 
 @pytest.mark.parametrize(
