6ab26707de651b4787c4fb6bfddd5f1e7ee2cd39
Matt Clay <matt@mystile.com>
2024-06-10 17:13:15 -0700

    Fix quoting in fork_safe_stdio test (#83412)

--- test/integration/targets/fork_safe_stdio/runme.sh
+++ test/integration/targets/fork_safe_stdio/runme.sh
@@ -9,7 +9,7 @@ echo "testing for stdio deadlock on forked workers (10s timeout)..."
 # Redirect stdout to /dev/null since it's full of non-printable garbage we don't want to display unless it failed
 ANSIBLE_CALLBACKS_ENABLED=spewstdio SPEWSTDIO_ENABLED=1 python run-with-pty.py ../test_utils/scripts/timeout.py -- 10 ansible-playbook -i hosts -f 5 test.yml > stdout.txt && RC=$? || RC=$?
 
-if [ $RC != 0 ]; then
+if [ "${RC}" != 0 ]; then
   echo "failed; likely stdout deadlock. dumping raw output (may be very large)"
   cat stdout.txt
   exit 1
