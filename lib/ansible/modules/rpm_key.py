856b5b959a5609804cf1f2fa844ffbcc743ba48b
Pablo Casillas <84393537+pvcasillasg@users.noreply.github.com>
2024-06-11 14:41:31 +0200

    Update rpm_key.py

--- lib/ansible/modules/rpm_key.py
+++ lib/ansible/modules/rpm_key.py
@@ -5,7 +5,7 @@
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import absolute_import, division, print_function, annotations
+from __future__ import annotations
 
 
 
