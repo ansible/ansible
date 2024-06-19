f2d1e7523e6be4df28942690bf9108ff70a4119f
Martin Krizek <martin.krizek@gmail.com>
2024-06-19 18:08:04 +0200

    fix sanity

--- test/integration/targets/strategy_host_pinned/callback_plugins/callback_host_count.py
+++ test/integration/targets/strategy_host_pinned/callback_plugins/callback_host_count.py
@@ -1,6 +1,8 @@
 # (c) 2024 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import annotations
+
 from ansible.plugins.callback import CallbackBase
 
 
