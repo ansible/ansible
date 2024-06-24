339452c1050b5b8fa6fd916e00519dff90443ceb
Brian Coca <bcoca@users.noreply.github.com>
2024-06-24 09:52:06 -0400

    Ensure the correct connection name is shown in results (#83354)

--- test/integration/targets/callback_results/runme.sh
+++ test/integration/targets/callback_results/runme.sh
@@ -4,10 +4,17 @@ set -eux
 
 # we are looking to verify the callback for v2_retry_runner gets a correct task name, include
 # if the value needs templating based on results of previous tasks
-OUTFILE="callback_retry_task_name.out"
+OUTFILE="callback_output_copy.out"
 trap 'rm -rf "${OUTFILE}"' EXIT
 
+# test task retry name
 EXPECTED_REGEX="^.*TASK.*18236 callback task template fix OUTPUT 2"
-ansible-playbook "$@" -i ../../inventory test.yml | tee "${OUTFILE}"
+ansible-playbook "$@" -i ../../inventory task_name.yml | tee "${OUTFILE}"
 echo "Grepping for ${EXPECTED_REGEX} in stdout."
 grep -e "${EXPECTED_REGEX}" "${OUTFILE}"
+
+# test connection tracking
+EXPECTED_CONNECTION='{"testhost":{"ssh":4}}'
+OUTPUT_TAIL=$(tail -n5 ${OUTFILE} | tr -d '[:space:]')
+[ "${EXPECTED_CONNECTION}" == "${OUTPUT_TAIL}" ]
+echo $?
