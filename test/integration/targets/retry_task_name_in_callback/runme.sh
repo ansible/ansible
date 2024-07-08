850da817c5d40e839a8762a6c35e79c772182d66
Brian Coca <bcoca@users.noreply.github.com>
2024-07-08 17:17:58 -0400

    linear strategy, show templated task name on start (#83473) (#83486)
    we only templated in some cases but when queueing we could get an untemplated name for the 'on start' event.
    
    (cherry picked from commit 0d28705ce5c6a4048645e919efd218a4ba8e5da1)

--- test/integration/targets/retry_task_name_in_callback/runme.sh
+++ test/integration/targets/retry_task_name_in_callback/runme.sh
@@ -11,3 +11,8 @@ EXPECTED_REGEX="^.*TASK.*18236 callback task template fix OUTPUT 2"
 ansible-playbook "$@" -i ../../inventory test.yml | tee "${OUTFILE}"
 echo "Grepping for ${EXPECTED_REGEX} in stdout."
 grep -e "${EXPECTED_REGEX}" "${OUTFILE}"
+
+# check variables are interpolated in 'started'
+UNTEMPLATED_STARTED="^.*\[started .*{{.*}}.*$"
+echo "Checking we dont have untemplated started in stdout."
+grep -e "${UNTEMPLATED_STARTED}" "${OUTFILE}" || exit 0
