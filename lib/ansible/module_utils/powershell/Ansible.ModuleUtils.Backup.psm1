22ed94ca8741f24e5fb05b355907f4376a4986e0
Brian Coca <brian.coca+git@gmail.com>
2024-07-10 15:14:11 -0400

    wstrans

--- lib/ansible/module_utils/powershell/Ansible.ModuleUtils.Backup.psm1
+++ lib/ansible/module_utils/powershell/Ansible.ModuleUtils.Backup.psm1
@@ -19,15 +19,15 @@ Function Backup-File {
         $backup_path = $null
         if (Test-Path -LiteralPath $path -PathType Leaf) {
 
-			$dirname = Split-Path -Parent $path -Resolve
+            $dirname = Split-Path -Parent $path -Resolve
 
-			# set variables for templating purposes
-			$basename = Split-Path -Path $path -Leaf -Resolve
-			$stripname = Split-Path -Path $path -LeafBase -Resolve
-			$extension = Split-Path -Path $path -Extension -Resolve
-			$timestamp = [DateTime]::Now.ToString("yyyyMMdd-HHmmss")
+            # set variables for templating purposes
+            $basename = Split-Path -Path $path -Leaf -Resolve
+            $stripname = Split-Path -Path $path -LeafBase -Resolve
+            $extension = Split-Path -Path $path -Extension -Resolve
+            $timestamp = [DateTime]::Now.ToString("yyyyMMdd-HHmmss")
 
-			# TODO: make childpath configurable via template stirng from config
+            # TODO: make childpath configurable via template stirng from config
             $backup_path = Join-Path -Path $dirname -ChildPath "$basename.$pid.$timestamp.bak"
             Try {
                 Copy-Item -LiteralPath $path -Destination $backup_path
