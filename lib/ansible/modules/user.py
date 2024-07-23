c036807a0bae84a5e2c2232c53764289ef5b8d3c
Brandon Bennett <brandon@brbe.me>
2024-07-23 14:20:50 -0600

    user: create Buildroot subclass as alias to Busybox
    Fixes: #83665

--- lib/ansible/modules/user.py
+++ lib/ansible/modules/user.py
@@ -3228,6 +3228,11 @@ class Alpine(BusyBox):
     distribution = 'Alpine'
 
 
+class Buildroot(BusyBox):
+    platform = 'Linux'
+    distribution = 'Buildroot'
+
+
 def main():
     ssh_defaults = dict(
         bits=0,
