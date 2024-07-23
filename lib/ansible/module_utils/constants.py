a3850ae2e87d5c380aab14990709dabce8498d70
Matt Martz <matt@sivel.net>
2024-07-23 09:27:49 -0500

    Limit respawn to supported python versions

---
+++ lib/ansible/module_utils/constants.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright: Contributors to the Ansible project
+# Simplified BSD License (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
+
+# Used for determining if the system is running a new enough python version
+# and should only restrict on our documented minimum versions
+_PY_MIN = (3, 8)
