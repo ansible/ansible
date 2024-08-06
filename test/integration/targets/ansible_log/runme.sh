a97a1b14d650c918038f6adbcec0328606a86539
Brian Coca <bcoca@users.noreply.github.com>
2024-08-06 16:18:03 -0400

    Fix display to log severity mapping (#83712)
    add caplevel to display to pass through
    also reverse dict order as 'last update wins'
    added tests ... and also log severity to log
    
    Co-authored-by: Sloane Hertel <19572925+s-hertel@users.noreply.github.com>

--- test/integration/targets/ansible_log/runme.sh
+++ test/integration/targets/ansible_log/runme.sh
@@ -4,14 +4,21 @@ set -eux
 
 ALOG=${OUTPUT_DIR}/ansible_log_test.log
 
+# no log enabled
 ansible-playbook logit.yml
+# ensure file is not created
 [ ! -f "${ALOG}" ]
 
+# log enabled
 ANSIBLE_LOG_PATH=${ALOG} ansible-playbook logit.yml
+# ensure log file is created
 [ -f "${ALOG}" ]
-grep -q 'ping' "${ALOG}"
-
+# Ensure tasks and log levels appear
+grep -q '\[normal task\]' "${ALOG}"
+grep -q 'INFO| TASK \[force warning\]' "${ALOG}"
+grep -q 'WARNING| \[WARNING\]: conditional statements' "${ALOG}"
 rm "${ALOG}"
+
 # inline grep should fail if EXEC was present
 set +e
 ANSIBLE_LOG_PATH=${ALOG} ANSIBLE_LOG_VERBOSITY=3 ansible-playbook -v logit.yml | tee /dev/stderr | grep -q EXEC
