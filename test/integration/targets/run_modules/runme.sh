d77f02df4b3e5aeaa919f3c05924d2121302336a
Brian Coca <brian.coca+git@gmail.com>
2024-08-07 09:33:03 -0400

    re captured escaped =

--- test/integration/targets/run_modules/runme.sh
+++ test/integration/targets/run_modules/runme.sh
@@ -5,7 +5,7 @@ set -eux
 # test running module directly
 python.py library/test.py args.json
 
-TMPFILE==$(shell mktemp -d -p "${OUTPUT_DIR}" 2>/dev/null || mktemp -d -t 'ansible-testing-XXXXXXXXXX' -p "${OUTPUT_DIR}")
+TMPFILE=$(shell mktemp -d -p "${OUTPUT_DIR}" 2>/dev/null || mktemp -d -t 'ansible-testing-XXXXXXXXXX' -p "${OUTPUT_DIR}")
 
 # ensure 'command' can use 'raw args'
 ansible -m command -a "dd if=/dev/zero of=\"${TMPFILE}\" bs=1K count=1"
