a20c77dbfe1bf197cf282b16d8481225424449e0
Brian Coca <brian.coca+git@gmail.com>
2024-08-07 09:22:22 -0400

    use diff intermediate var to preserve function
    add tests

--- test/integration/targets/run_modules/runme.sh
+++ test/integration/targets/run_modules/runme.sh
@@ -4,3 +4,11 @@ set -eux
 
 # test running module directly
 python.py library/test.py args.json
+
+TMPFILE==$(shell mktemp -d -p "${OUTPUT_DIR}" 2>/dev/null || mktemp -d -t 'ansible-testing-XXXXXXXXXX' -p "${OUTPUT_DIR}")
+
+# ensure 'command' can use 'raw args'
+ansible -m command -a "dd if=/dev/zero of=\"${TMPFILE}\" bs=1K count=1"
+
+# ensure fqcn 'command' can use 'raw args'
+ansible -m ansible.legacy.command -a "dd if=/dev/zero of=\"${TMPFILE}\" bs=1K count=1"
