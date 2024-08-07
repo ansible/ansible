08f13f0c8151f94134da748901bee9b697758915
Brian Coca <brian.coca+git@gmail.com>
2024-08-07 10:33:25 -0400

    target localhost!

--- test/integration/targets/run_modules/runme.sh
+++ test/integration/targets/run_modules/runme.sh
@@ -8,7 +8,7 @@ python.py library/test.py args.json
 TMPFILE=$(shell mktemp -d -p "${OUTPUT_DIR}" 2>/dev/null || mktemp -d -t 'ansible-testing-XXXXXXXXXX' -p "${OUTPUT_DIR}")
 
 # ensure 'command' can use 'raw args'
-ansible -m command -a "dd if=/dev/zero of=\"${TMPFILE}\" bs=1K count=1"
+ansible -m command -a "dd if=/dev/zero of=\"${TMPFILE}\" bs=1K count=1" localhost
 
 # ensure fqcn 'command' can use 'raw args'
-ansible -m ansible.legacy.command -a "dd if=/dev/zero of=\"${TMPFILE}\" bs=1K count=1"
+ansible -m ansible.legacy.command -a "dd if=/dev/zero of=\"${TMPFILE}\" bs=1K count=1" localhost
