0f2dfd55ae8af3119226df002498f64aa83efb3a
Brian Coca <bcoca@users.noreply.github.com>
2024-08-08 15:14:05 -0400

    Add vaulted_file test (#83717)
    * Add vaulted_file test
    * fix is_encrypted_file while we are here
    Co-authored-by: Martin Krizek <martin.krizek@gmail.com>

--- lib/ansible/parsing/dataloader.py
+++ lib/ansible/parsing/dataloader.py
@@ -18,7 +18,7 @@ from ansible.module_utils.six import binary_type, text_type
 from ansible.module_utils.common.text.converters import to_bytes, to_native, to_text
 from ansible.parsing.quoting import unquote
 from ansible.parsing.utils.yaml import from_yaml
-from ansible.parsing.vault import VaultLib, b_HEADER, is_encrypted, is_encrypted_file, parse_vaulttext_envelope, PromptVaultSecret
+from ansible.parsing.vault import VaultLib, is_encrypted, is_encrypted_file, parse_vaulttext_envelope, PromptVaultSecret
 from ansible.utils.path import unfrackpath
 from ansible.utils.display import Display
 
@@ -388,7 +388,7 @@ class DataLoader:
                     # Limit how much of the file is read since we do not know
                     # whether this is a vault file and therefore it could be very
                     # large.
-                    if is_encrypted_file(f, count=len(b_HEADER)):
+                    if is_encrypted_file(f):
                         # if the file is encrypted and no password was specified,
                         # the decrypt call would throw an error, but we check first
                         # since the decrypt function doesn't know the file name
