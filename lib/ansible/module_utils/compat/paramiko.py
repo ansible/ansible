3a97ec8db2e90c9c9c5532cfdc154d2cfd876fde
Matt Clay <matt@mystile.com>
2024-08-12 16:04:57 -0700

    Update comment to reflect paramiko changes

--- lib/ansible/module_utils/compat/paramiko.py
+++ lib/ansible/module_utils/compat/paramiko.py
@@ -14,9 +14,8 @@ try:
         # Blowfish is deprecated in cryptography, but still used by paramiko versions older than 2.9.5.
         # See: https://github.com/paramiko/paramiko/pull/2039
         warnings.filterwarnings('ignore', message='Blowfish has been ', category=UserWarning)
-        # TripleDES is deprecated in cryptography, but still used by current versions of paramiko.
-        # See: https://github.com/pyca/cryptography/pull/10286
-        # See: https://github.com/ansible/ansible/issues/83757
+        # TripleDES is deprecated in cryptography, but still used by paramiko versions older than 3.3.2 and 3.4.1.
+        # See: https://github.com/paramiko/paramiko/pull/2421
         warnings.filterwarnings('ignore', message='TripleDES has been ', category=UserWarning)
         import paramiko  # pylint: disable=unused-import
 # paramiko and gssapi are incompatible and raise AttributeError not ImportError
