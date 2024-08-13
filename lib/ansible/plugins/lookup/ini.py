50c666e7d840609e1216f6b439e65d528c27c697
Martin Krizek <martin.krizek@gmail.com>
2024-08-13 17:04:42 +0200

    ini lookup: add new interpolation option (#83773)
    Fixes #83755

--- lib/ansible/plugins/lookup/ini.py
+++ lib/ansible/plugins/lookup/ini.py
@@ -49,6 +49,12 @@ DOCUMENTATION = """
         default: False
         aliases: ['allow_none']
         version_added: '2.12'
+      interpolation:
+        description:
+          Allows for interpolation of values, see https://docs.python.org/3/library/configparser.html#configparser.BasicInterpolation
+        type: bool
+        default: True
+        version_added: '2.18'
     seealso:
       - ref: playbook_task_paths
         description: Search paths used for relative files.
@@ -140,7 +146,10 @@ class LookupModule(LookupBase):
         self.set_options(var_options=variables, direct=kwargs)
         paramvals = self.get_options()
 
-        self.cp = configparser.ConfigParser(allow_no_value=paramvals.get('allow_no_value', paramvals.get('allow_none')))
+        self.cp = configparser.ConfigParser(
+            allow_no_value=paramvals.get('allow_no_value', paramvals.get('allow_none')),
+            interpolation=configparser.BasicInterpolation() if paramvals.get('interpolation') else None,
+        )
         if paramvals['case_sensitive']:
             self.cp.optionxform = to_native
 
