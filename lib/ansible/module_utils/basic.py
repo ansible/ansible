0b86fccf13732a2f280da944d4a7825bb9935669
Brian Coca <bcoca@users.noreply.github.com>
2024-08-26 14:22:15 -0400

    copy, fix permissions and atime on diff partitions (#83824)
    we just set time also, when on diff partitions
    
    (cherry picked from commit 2a676ff897cd32d6cc34c7bc00b71ae5d54d32a4)

--- lib/ansible/module_utils/basic.py
+++ lib/ansible/module_utils/basic.py
@@ -1620,7 +1620,7 @@ class AnsibleModule(object):
         #   Similar to shutil.copy(), but metadata is copied as well - in fact,
         #   this is just shutil.copy() followed by copystat(). This is similar
         #   to the Unix command cp -p.
-        #
+
         # shutil.copystat(src, dst)
         #   Copy the permission bits, last access time, last modification time,
         #   and flags from src to dst. The file contents, owner, and group are
@@ -1734,6 +1734,8 @@ class AnsibleModule(object):
                                 tmp_stat = os.stat(b_tmp_dest_name)
                                 if dest_stat and (tmp_stat.st_uid != dest_stat.st_uid or tmp_stat.st_gid != dest_stat.st_gid):
                                     os.chown(b_tmp_dest_name, dest_stat.st_uid, dest_stat.st_gid)
+                                os.utime(b_tmp_dest_name, times=(time.time(), time.time()))
+>>>>>>> 2a676ff897 (copy, fix permissions and atime on diff partitions (#83824))
                             except OSError as e:
                                 if e.errno != errno.EPERM:
                                     raise
