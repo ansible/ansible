1f987423fd4887228736f27a525a98aca36b881a
Martin Krizek <martin.krizek@gmail.com>
2024-08-27 11:59:10 +0200

    Print the name of the option being deprecated (#83761)
    Fixes #83759

--- test/integration/targets/deprecations/runme.sh
+++ test/integration/targets/deprecations/runme.sh
@@ -9,8 +9,8 @@ export ANSIBLE_DEPRECATION_WARNINGS=True
 # check for entry key valid, no deprecation
 [ "$(ANSIBLE_CONFIG='entry_key_not_deprecated.cfg' ansible -m meta -a 'noop'  localhost 2>&1 | grep -c 'DEPRECATION')" -eq "0" ]
 
-# check for entry key deprecation, must be defined to trigger
-[ "$(ANSIBLE_CONFIG='entry_key_deprecated.cfg' ansible -m meta -a 'noop'  localhost 2>&1 | grep -c 'DEPRECATION')" -eq "1" ]
+# check for entry key deprecation including the name of the option, must be defined to trigger
+[ "$(ANSIBLE_CONFIG='entry_key_deprecated.cfg' ansible -m meta -a 'noop' localhost 2>&1 | grep -c "\[DEPRECATION WARNING\]: \[testing\]deprecated option.")" -eq "1" ]
 
 # check for deprecation of entry itself, must be consumed to trigger
 [ "$(ANSIBLE_TEST_ENTRY2=1 ansible -m debug -a 'msg={{q("config", "_Z_TEST_ENTRY_2")}}' localhost  2>&1 | grep -c 'DEPRECATION')" -eq "1" ]
