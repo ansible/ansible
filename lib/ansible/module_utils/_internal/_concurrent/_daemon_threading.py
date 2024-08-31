f1e2eb185ce23cd14186c5c6beaf17c8e132e025
Matt Davis <nitzmahone@redhat.com>
2024-08-30 17:45:03 -0700

    Add DaemonThreadPoolExecutor impl * Provide a simple parallel execution method with the ability to abandon timed-out operations that won't block threadpool/process shutdown, and without a dependency on /dev/shm (as multiprocessing Thread/Process pools have). * Create module_utils/_internal to ensure that this is clearly not supported for public consumption.

---
+++ lib/ansible/module_utils/_internal/_concurrent/_daemon_threading.py
@@ -0,0 +1,27 @@
+"""Proxy stdlib threading module that only supports non-joinable daemon threads."""
+
+from __future__ import annotations as _annotations
+
+import threading as _threading
+import typing as _t
+
+
+class _DaemonThread(_threading.Thread):
+    """
+    Daemon-only Thread subclass; prevents running threads of this type from blocking interpreter shutdown and process exit.
+    The join() method is a no-op.
+    """
+
+    def __init__(self, *args, daemon: bool | None = None, **kwargs) -> None:
+        super().__init__(*args, daemon=daemon or True, **kwargs)
+
+    def join(self, timeout=None) -> None:
+        """ThreadPoolExecutor's atexit handler joins all queue threads before allowing shutdown; prevent them from blocking."""
+
+
+Thread = _DaemonThread  # shadow the real Thread attr with our _DaemonThread
+
+
+def __getattr__(name: str) -> _t.Any:
+    """Delegate anything not defined locally to the real `threading` module."""
+    return getattr(_threading, name)
