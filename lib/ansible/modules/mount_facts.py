7c96c2555cab82dbe0bcc4050f172c62cc8e75b1
s-hertel <19572925+s-hertel@users.noreply.github.com>
2024-09-12 22:57:26 -0400

    Make mount output matching more strict

--- lib/ansible/modules/mount_facts.py
+++ lib/ansible/modules/mount_facts.py
@@ -275,11 +275,11 @@ def run_mount_bin(module: AnsibleModule, mount_bin: str) -> str:  # type: ignore
 def get_mount_pattern(stdout: str):
     lines = stdout.splitlines()
     pattern = None
-    if any(LINUX_MOUNT_RE.match(line) for line in lines):
+    if all(LINUX_MOUNT_RE.match(line) for line in lines):
         pattern = LINUX_MOUNT_RE
-    elif any(BSD_MOUNT_RE.match(line) for line in lines):
+    elif all(BSD_MOUNT_RE.match(line) for line in lines if not line.startswith("map ")):
         pattern = BSD_MOUNT_RE
-    elif any(AIX_MOUNT_RE.match(line) for line in lines):
+    elif len(lines) > 2 and all(AIX_MOUNT_RE.match(line) for line in lines[2:]):
         pattern = AIX_MOUNT_RE
     return pattern
 
