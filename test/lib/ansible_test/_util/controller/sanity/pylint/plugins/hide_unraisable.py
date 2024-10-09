c7218d8b159b499b4a88032e9af43a3d89a94039
Matt Clay <matt@mystile.com>
2024-10-09 10:19:55 -0700

    ansible-test - Work around pylint issue on 3.11 (#84094)

--- test/lib/ansible_test/_util/controller/sanity/pylint/plugins/hide_unraisable.py
+++ test/lib/ansible_test/_util/controller/sanity/pylint/plugins/hide_unraisable.py
@@ -1,4 +1,4 @@
-"""Temporary plugin to prevent stdout noise pollution from finalization of abandoned generators under Python 3.12"""
+"""Temporary plugin to prevent stdout noise pollution from finalization of abandoned generators."""
 from __future__ import annotations
 
 import sys
@@ -10,7 +10,7 @@ if t.TYPE_CHECKING:
 
 def _mask_finalizer_valueerror(ur: t.Any) -> None:
     """Mask only ValueErrors from finalizing abandoned generators; delegate everything else"""
-    # work around Py3.12 finalizer changes that sometimes spews this error message to stdout
+    # work around Python finalizer issue that sometimes spews this error message to stdout
     # see https://github.com/pylint-dev/pylint/issues/9138
     if ur.exc_type is ValueError and 'generator already executing' in str(ur.exc_value):
         return
@@ -20,5 +20,4 @@ def _mask_finalizer_valueerror(ur: t.Any) -> None:
 
 def register(linter: PyLinter) -> None:  # pylint: disable=unused-argument
     """PyLint plugin registration entrypoint"""
-    if sys.version_info >= (3, 12):
-        sys.unraisablehook = _mask_finalizer_valueerror
+    sys.unraisablehook = _mask_finalizer_valueerror
