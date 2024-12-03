ee44ae402da5d421be2dee201405997b44681549
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-12-03 16:14:37 +0100

    fix documentation about avoiding apt_key (#84326)

--- lib/ansible/modules/apt_repository.py
+++ lib/ansible/modules/apt_repository.py
@@ -143,7 +143,7 @@ EXAMPLES = """
 
     - name: somerepo | apt source
       ansible.builtin.apt_repository:
-        repo: "deb [arch=amd64 signed-by=/etc/apt/keyrings/myrepo.asc] https://download.example.com/linux/ubuntu {{ ansible_distribution_release }} stable"
+        repo: "deb [arch=amd64 signed-by=/etc/apt/keyrings/somerepo.asc] https://download.example.com/linux/ubuntu {{ ansible_distribution_release }} stable"
         state: present
 """
 
