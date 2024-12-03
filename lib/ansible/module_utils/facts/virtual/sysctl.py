51b5773c68a7bf3218c1372eb8a4199103646d80
Abhijeet Kasurde <akasurde@redhat.com>
2024-12-03 07:33:30 -0800

    test coverage for virtual/sysctl.py (#84356)
    * test coverage for virtual/sysctl.py
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/module_utils/facts/virtual/sysctl.py
+++ lib/ansible/module_utils/facts/virtual/sysctl.py
@@ -1,24 +1,11 @@
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-
+# Copyright: Contributors to the Ansible project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import annotations
 
 import re
 
 
-class VirtualSysctlDetectionMixin(object):
+class VirtualSysctlDetectionMixin:
     def detect_sysctl(self):
         self.sysctl_path = self.module.get_bin_path('sysctl')
 
