57e0ffbe80b81ca352872767390832c832f55038
Brian Coca <brian.coca+git@gmail.com>
2025-01-08 13:38:41 -0500

    move vars_prompt into an play obj
    normalize with loop_control

---
+++ lib/ansible/playbook/vars_prompt.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from ansible.playbook.attribute import NonInheritableFieldAttribute
+
+
+class VarsPrompt(FieldAttributeBase):
+
+    name = NonInheritableFieldAttribute(isa='string', always_post_validate=True)
+    prompt = NonInheritableFieldAttribute(isa='string')
+    default = NonInheritableFieldAttribute(isa='raw')
+    private = NonInheritableFieldAttribute(isa='bool')
+    confirm = NonInheritableFieldAttribute(isa='bool')
+    encrypt = NonInheritableFieldAttribute(isa='bool')
+    salt_size = NonInheritableFieldAttribute(isa='int')
+    salt = NonInheritableFieldAttribute(isa='string')
+    unsafe = NonInheritableFieldAttribute(isa='bool')
+
+    def __init__(self):
+        super(VarsPrompt, self).__init__()
+
+    @staticmethod
+    def load(data, variable_manager=None, loader=None):
+        t = LoopControl()
+        return t.load_data(data, variable_manager=variable_manager, loader=loader)
