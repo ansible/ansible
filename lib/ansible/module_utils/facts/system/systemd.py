d426ee4aaf9d66d0c152c3a4028cb9198381512e
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2025-01-21 16:46:02 +0000

    import contextlib
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- lib/ansible/module_utils/facts/system/systemd.py
+++ lib/ansible/module_utils/facts/system/systemd.py
@@ -18,6 +18,7 @@
 from __future__ import annotations
 
 import ansible.module_utils.compat.typing as t
+import contextlib
 
 from ansible.module_utils.facts.collector import BaseFactCollector
 from ansible.module_utils.facts.system.service_mgr import ServiceMgrFactCollector
