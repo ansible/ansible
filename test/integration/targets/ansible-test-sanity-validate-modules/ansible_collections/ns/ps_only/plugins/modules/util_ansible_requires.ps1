39cdf3e8f776615529d9cf278538940d5e11f0c8
Jordan Borean <jborean93@gmail.com>
2025-01-27 06:15:21 +1000

    ansible-test validate-modules - fix ps util checks
    Fix the module util import checks done by `ansible-test sanity --test
    validate-modules` to support the newer `#AnsibleRequires` import
    statement and `-Optional` flag.

---
+++ test/integration/targets/ansible-test-sanity-validate-modules/ansible_collections/ns/ps_only/plugins/modules/util_ansible_requires.ps1
@@ -0,0 +1,6 @@
+#!powershell
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#AnsibleRequires -PowerShell Ansible.ModuleUtils.Legacy
+
+'{"changed": false, "msg": "Hello, World!"}'
