93f0be2a7b6c509a574975016334b7ae5dda2899
Matt Davis <nitzmahone@redhat.com>
2025-01-28 11:01:00 -0800

    stub dump_object filter to protomatter

---
+++ lib/ansible/_internal/ansible_collections/ansible/_protomatter/plugins/filter/dump_object.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+import dataclasses
+import typing as t
+
+
+def dump_object(value: object) -> object:
+    """Internal filter to convert objects not supported by JSON to types which are."""
+    if dataclasses.is_dataclass(value):
+        return dataclasses.asdict(value)
+
+    return value
+
+
+class FilterModule(object):
+    def filters(self) -> dict[str, t.Callable]:
+        return dict(dump_object=dump_object)
