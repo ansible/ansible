5186642a5eeb9dad903cd95f001e932d1e63db4d
Abhijeet Kasurde <akasurde@redhat.com>
2025-01-30 08:43:38 -0800

    Make CI green I
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/modules/user.py
+++ lib/ansible/modules/user.py
@@ -521,6 +521,7 @@ from ansible.module_utils import distro
 from ansible.module_utils.common.text.converters import to_bytes, to_native, to_text
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.common.locale import get_best_parsable_locale
+from ansible.module_utils.common.parameters import env_fallback
 from ansible.module_utils.common.sys_info import get_platform_subclass
 
 
