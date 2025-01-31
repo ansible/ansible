b4fe9e2567a3413b43816db8115a303553f35c58
anvitpusalkar <anvit.pusalkar@gmail.com>
2025-02-01 00:14:43 +0530

    Dynamically configure callback plugins using extra_vars

--- lib/ansible/plugins/callback/default.py
+++ lib/ansible/plugins/callback/default.py
@@ -55,7 +55,7 @@ class CallbackModule(CallbackBase):
         if option_name in self._extra_vars:
             return self._extra_vars.get(option_name).lower() == 'true'
 
-        # Fallback to default functionality 
+        # Fallback to default functionality
         return super(CallbackModule, self).get_option(option_name)
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
