f7a51c0d8fce4a2342942dba397d483c4c4ec227
Brian Coca <bcoca@users.noreply.github.com>
2025-02-04 11:18:23 -0500

    Update lib/ansible/plugins/__init__.py
    Co-authored-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/plugins/__init__.py
+++ lib/ansible/plugins/__init__.py
@@ -98,8 +98,8 @@ class AnsiblePlugin(ABC):
         return options
 
     def get_ff_option(self, *options, hostvars=None, pc=None):
-        ''' convinience function for when plugins have not followed strict conventions (user/remote_user)
-            and/or relied on playcontext, which is not strictly up2date, specially in loops. '''
+        ''' convenience function for when plugins have not followed strict conventions (user/remote_user)
+            and/or relied on playcontext, which is not strictly up-to-date, especially in loops. '''
         for option in options:
             if self.has_option(option):
                 return self.get_option(option, hostvars)
