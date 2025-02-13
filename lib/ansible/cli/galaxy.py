c69ba66cd37afff6192c6922b937c2414d4b7e62
Brian Coca <brian.coca+git@gmail.com>
2025-02-13 13:22:30 -0500

    update consumers to use simpler function

--- lib/ansible/cli/galaxy.py
+++ lib/ansible/cli/galaxy.py
@@ -247,12 +247,11 @@ class GalaxyCLI(CLI):
         offline.add_argument('--offline', dest='offline', default=False, action='store_true',
                              help="Don't query the galaxy API when creating roles")
 
-        default_roles_path = C.config.get_configuration_definition('DEFAULT_ROLES_PATH').get('default', '')
         roles_path = opt_help.ArgumentParser(add_help=False)
         roles_path.add_argument('-p', '--roles-path', dest='roles_path', type=opt_help.unfrack_path(pathsep=True),
                                 default=C.DEFAULT_ROLES_PATH, action=opt_help.PrependListAction,
                                 help='The path to the directory containing your roles. The default is the first '
-                                     'writable one configured via DEFAULT_ROLES_PATH: %s ' % default_roles_path)
+                                     'writable one configured via DEFAULT_ROLES_PATH: %s ' % C.config.get_config_default('DEFAULT_ROLES_PATH')
 
         collections_path = opt_help.ArgumentParser(add_help=False)
         collections_path.add_argument('-p', '--collections-path', dest='collections_path', type=opt_help.unfrack_path(pathsep=True),
--- lib/ansible/plugins/connection/local.py
+++ lib/ansible/plugins/connection/local.py
@@ -252,8 +252,7 @@ class Connection(ConnectionBase):
     def _become_success_timeout(self) -> int:
         """Timeout value for become success in seconds."""
         if (timeout := self.get_option('become_success_timeout')) < 1:
-            timeout = C.config.get_configuration_definitions('connection', 'local')['become_success_timeout']['default']
-
+            timeout = self.get_option_default('become_success_timeout')
         return timeout
 
     def put_file(self, in_path: str, out_path: str) -> None:
