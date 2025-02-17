b4a40c4fd23ca0f4963d67420a50f73ae7890d8b
simonLeary42 <71396965+simonLeary42@users.noreply.github.com>
2025-02-17 16:12:14 -0500

    better error message for malformed documentation (#84705)
    No the file name that caused the error will be apparent
    ---------
    
    Signed-off-by: Abhijeet Kasurde <Akasurde@redhat.com>
    Co-authored-by: Abhijeet Kasurde <Akasurde@redhat.com>

---
+++ test/integration/targets/ansible-doc/lookup_plugins/broken_docs.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2022 Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import annotations
+
+
+DOCUMENTATION = """
+    name: broken_docs
+    -
+"""
+
+EXAMPLE = """
+"""
+
+RETURN = """
+"""
