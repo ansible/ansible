bc7399ea4cae3eeeacdd22b9182b13656b3d70b1
Matt Clay <matt@mystile.com>
2025-02-19 18:33:23 -0800

    Fix script inventory type handling and tests

--- lib/ansible/plugins/inventory/script.py
+++ lib/ansible/plugins/inventory/script.py
@@ -328,19 +328,19 @@ def detect_profile_name(value: str) -> str:
         raise ValueError('Value could not be parsed as JSON.') from ex
 
     if not isinstance(data, dict):
-        raise TypeError(f'Value is {type(data)} instead of {dict}.')
+        raise TypeError(f'Value is {native_type_name(data)!r} instead of {native_type_name(dict)!r}.')
 
     if (meta := data.get('_meta', ...)) is ...:
         return inventory_legacy.Decoder.profile_name
 
     if not isinstance(meta, dict):
-        raise TypeError(f"Value contains '_meta' which is {type(meta)} instead of {dict}.")
+        raise TypeError(f"Value contains '_meta' which is {native_type_name(meta)!r} instead of {native_type_name(dict)!r}.")
 
     if (profile := meta.get('profile', ...)) is ...:
         return inventory_legacy.Decoder.profile_name
 
     if not isinstance(profile, str):
-        raise TypeError(f"Value contains '_meta.profile' which is {type(profile)} instead of {str}.")
+        raise TypeError(f"Value contains '_meta.profile' which is {native_type_name(profile)!r} instead of {native_type_name(str)!r}.")
 
     if not profile.startswith('inventory_'):
         raise ValueError(f"Non-inventory profile {profile!r} is not allowed.")
