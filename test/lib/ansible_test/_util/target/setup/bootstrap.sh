544c066164358ddf5db44aa81010f585850557e6
Matt Clay <matt@mystile.com>
2025-03-11 15:21:38 -0700

    ansible-test - Replace FreeBSD 13.4 with 13.5 (#84812)
    (cherry picked from commit 4efb01c2f3dd17a18cd79a04d584633751b8a537)

--- test/lib/ansible_test/_util/target/setup/bootstrap.sh
+++ test/lib/ansible_test/_util/target/setup/bootstrap.sh
@@ -168,8 +168,8 @@ bootstrap_remote_freebsd()
         # Declare platform/python version combinations which do not have supporting OS packages available.
         # For these combinations ansible-test will use pip to install the requirements instead.
         case "${platform_version}/${python_version}" in
-            13.4/3.11)
-                # defaults available
+            14.1/3.9)
+                # defaults above 'just work'TM
                 ;;
             14.1/3.11)
                 cryptography_pkg=""  # not available
