970e7c90f9136e73d3a8a82bfe4af574f64c1f1c
Abhijeet Kasurde <akasurde@redhat.com>
2025-03-11 08:40:46 -0700

    facts: use pagesize for darwin (#84779)
    Fixes: #84773
    
    Signed-off-by: Abhijeet Kasurde <Akasurde@redhat.com>

--- test/units/module_utils/facts/hardware/test_darwin_facts.py
+++ test/units/module_utils/facts/hardware/test_darwin_facts.py
@@ -5,15 +5,16 @@ from __future__ import annotations
 
 import pathlib
 
+import pytest
+
 from ansible.module_utils.facts.hardware import darwin
 from ansible.module_utils.facts.sysctl import get_sysctl
-import pytest
 
 
 class TestDarwinHardwareFacts:
-    def _get_mock_sysctl_data(self):
-        fixtures = pathlib.Path(__file__).parent / "fixtures"
-        return (fixtures / "sysctl_darwin.txt").read_text()
+    def _get_mock_sysctl_data(self, filename="sysctl_darwin_silicon.txt"):
+        fixture_file = pathlib.Path(__file__).parent / "fixtures" / filename
+        return fixture_file.read_text()
 
     @pytest.fixture()
     def mocked_module(self, mocker, request):
@@ -52,15 +53,32 @@ class TestDarwinHardwareFacts:
         }
         assert cpu_facts == expected_cpu_facts
 
-    def test_get_memory_facts(self, mocked_module):
+    @pytest.mark.parametrize(
+        ("vm_stat_file", "sysctl_file", "expected_memory_facts"),
+        [
+            pytest.param(
+                "vm_stat_darwin_intel.txt",
+                "sysctl_darwin_intel.txt",
+                {'memtotal_mb': 2048, 'memfree_mb': 178},
+                id="intel",
+            ),
+            pytest.param(
+                "vm_stat_darwin_silicon.txt",
+                "sysctl_darwin_silicon.txt",
+                {"memtotal_mb": 32768, "memfree_mb": 7660},
+                id="silicon",
+            ),
+        ],
+    )
+    def test_get_memory_facts(self, mocked_module, vm_stat_file, sysctl_file, expected_memory_facts):
         fixtures = pathlib.Path(__file__).parent / "fixtures"
         mocked_module.get_bin_path.side_effect = [
             "/usr/sbin/sysctl",
             "/usr/bin/vm_stat",
         ]
-        mocked_vm_stat = (fixtures / "vm_stat_darwin.txt").read_text()
+        mocked_vm_stat = (fixtures / vm_stat_file).read_text()
         mocked_module.run_command.side_effect = [
-            (0, self._get_mock_sysctl_data(), ""),
+            (0, self._get_mock_sysctl_data(filename=sysctl_file), ""),
             (0, mocked_vm_stat, ""),
         ]
         darwin_hardware = darwin.DarwinHardware(mocked_module)
@@ -69,7 +87,6 @@ class TestDarwinHardwareFacts:
         )
 
         memory_facts = darwin_hardware.get_memory_facts()
-        expected_memory_facts = {"memtotal_mb": 32768, "memfree_mb": 26491}
         assert memory_facts == expected_memory_facts
 
     def test_get_uptime_facts(self, mocked_module):
