24f0dea9db908770d3de114b0d85a59cac893ff0
Matt Clay <matt@mystile.com>
2025-03-14 11:03:23 -0700

    Fix issues reported by sanity tests

--- test/integration/targets/deprecations/action_plugins/action_with_dep.py
+++ test/integration/targets/deprecations/action_plugins/action_with_dep.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from ansible.module_utils.datatag import deprecate_value
 from ansible.plugins.action import ActionBase
 
@@ -5,8 +7,8 @@ from ansible.plugins.action import ActionBase
 class ActionModule(ActionBase):
     def run(self, tmp=None, task_vars=None):
         result = super().run(tmp, task_vars)
-        result.update(deprecated_thing=deprecate_value("deprecated thing", msg="Deprecated thing is deprecated.", removal_version='1.2.3'))
+        result.update(deprecated_thing=deprecate_value("deprecated thing", msg="Deprecated thing is deprecated.", removal_version='999.999'))
 
-        self._display.deprecated("did a deprecated thing", version="1.2.3")
+        self._display.deprecated("did a deprecated thing", version="999.999")
 
         return result
--- test/units/mock/messages.py
+++ test/units/mock/messages.py
@@ -4,9 +4,9 @@ import typing as t
 
 from ansible.module_utils.common.messages import SummaryBase, Detail
 
-_TSummaryBase = t.TypeVar('_TSummaryBase', bound=t.Type[SummaryBase])
+_TSummaryBase = t.TypeVar('_TSummaryBase', bound=SummaryBase)
 
 
-def make_summary(summary_type: _TSummaryBase, *details: Detail, formatted_traceback: t.Optional[str] = None, **kwargs) -> _TSummaryBase:
+def make_summary(summary_type: type[_TSummaryBase], *details: Detail, formatted_traceback: t.Optional[str] = None, **kwargs) -> _TSummaryBase:
     """Utility factory method to avoid inline tuples."""
     return summary_type(details=details, formatted_traceback=formatted_traceback, **kwargs)
