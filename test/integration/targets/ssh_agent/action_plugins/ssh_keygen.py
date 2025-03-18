6173a0bde955c302839fc4c92bb3ca11d1a681fb
Martin Krizek <martin.krizek@gmail.com>
2025-03-18 14:25:58 +0100

    test all key types

--- test/integration/targets/ssh_agent/action_plugins/ssh_keygen.py
+++ test/integration/targets/ssh_agent/action_plugins/ssh_keygen.py
@@ -6,8 +6,10 @@ from ansible.module_utils.common.text.converters import to_bytes, to_text
 
 
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric.rsa import generate_private_key
+from cryptography.hazmat.primitives.asymmetric.rsa import generate_private_key as rsa_generate_private_key
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
+from cryptography.hazmat.primitives.asymmetric.dsa import generate_private_key as dsa_generate_private_key
+from cryptography.hazmat.primitives.asymmetric.ec import SECP384R1, generate_private_key as ecdsa_generate_private_key
 
 
 class ActionModule(ActionBase):
@@ -19,7 +21,11 @@ class ActionModule(ActionBase):
             case 'ed25519':
                 private_key = Ed25519PrivateKey.generate()
             case 'rsa':
-                private_key = generate_private_key(65537, 4096)
+                private_key = rsa_generate_private_key(65537, 4096)
+            case 'dsa':
+                private_key = dsa_generate_private_key(1024)
+            case 'ecdsa':
+                private_key = ecdsa_generate_private_key(SECP384R1())
             case _:
                 return {'failed': True, 'msg': 'not implemented'}
 
