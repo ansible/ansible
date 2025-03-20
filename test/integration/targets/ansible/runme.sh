83589faf2d60150e5fee0651d40bdd1e4cf01e1a
Matt Clay <matt@mystile.com>
2025-03-20 09:48:02 -0700

    Miscellaneous integration test fixes (#84871)

--- test/integration/targets/ansible/runme.sh
+++ test/integration/targets/ansible/runme.sh
@@ -20,7 +20,7 @@ ANSIBLE_COLLECTIONS_PATH=/tmp/collections ansible-config dump| grep 'COLLECTIONS
 ansible-config list | grep 'COLLECTIONS_PATHS'
 
 # 'view' command must fail when config file is missing or has an invalid file extension
-ansible-config view -c ./ansible-non-existent.cfg 2> err1.txt || grep -Eq 'ERROR! The provided configuration file is missing or not accessible:' err1.txt || (cat err*.txt; rm -f err1.txt; exit 1)
+ansible-config view -c ./ansible-non-existent.cfg 2> err1.txt || grep -Eq 'The provided configuration file is missing or not accessible:' err1.txt || (cat err*.txt; rm -f err1.txt; exit 1)
 ansible-config view -c ./no-extension 2> err2.txt || grep -q 'Unsupported configuration file extension' err2.txt || (cat err2.txt; rm -f err*.txt; exit 1)
 rm -f err*.txt
 
