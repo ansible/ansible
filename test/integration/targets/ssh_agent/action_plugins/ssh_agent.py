2f5f1626c6bace0216acf6bb391e5331df464168
Martin Krizek <martin.krizek@gmail.com>
2025-03-31 15:48:14 +0200

    Add tests for remove and remove_all

--- test/integration/targets/ssh_agent/action_plugins/ssh_agent.py
+++ test/integration/targets/ssh_agent/action_plugins/ssh_agent.py
@@ -5,6 +5,8 @@ import os
 from ansible.plugins.action import ActionBase
 from ansible.utils._ssh_agent import SshAgentClient
 
+from cryptography.hazmat.primitives.serialization import ssh
+
 
 class ActionModule(ActionBase):
 
@@ -14,8 +16,28 @@ class ActionModule(ActionBase):
         match self._task.args['action']:
             case 'list':
                 return self.list()
+            case 'remove':
+                return self.remove(self._task.args['pubkey'])
+            case 'remove_all':
+                return self.remove_all()
             case _:
                 return {'failed': True, 'msg': 'not implemented'}
+
+    def remove(self, pubkey_data):
+        with SshAgentClient(os.environ['SSH_AUTH_SOCK']) as client:
+            public_key = ssh.load_ssh_public_key(pubkey_data.encode())
+            client.remove(public_key)
+            return {'failed': public_key in client}
+
+    def remove_all(self):
+        with SshAgentClient(os.environ['SSH_AUTH_SOCK']) as client:
+            nkeys_before = client.list().nkeys
+            client.remove_all()
+            nkeys_after = client.list().nkeys
+            return {
+                'failed': nkeys_after != 0,
+                'nkeys_removed': nkeys_before,
+            }
 
     def list(self):
         result = {'keys': [], 'nkeys': 0}
