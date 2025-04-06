82c42d30092c33c25429cd1a35f72fa86b59088b
aniketpuro <aniketpurohit851@gmail.com>
2025-04-06 19:08:58 +0530

    Revert "Revert "Fix issue #84909: Proper handling for None in collection loader""
    This reverts commit decabca56d3445589c8c050cbe0ce17b58ebb893.

--- lib/ansible/playbook/playbook_include.py
+++ lib/ansible/playbook/playbook_include.py
@@ -84,7 +84,7 @@ class PlaybookInclude(Base, Conditional, Taggable):
                 playbook = os.path.join(basedir, playbook)
 
             # might still be collection playbook
-            playbook_collection = _get_collection_name_from_path(playbook)
+            playbook_collection = _get_collection_name_from_path(playbook) or "default_collection"
 
         if playbook_collection:
             # it is a collection playbook, setup default collections
