dec14eba920ae9f9e7a7a17b6439fb9019e345c4
Brian Coca <bcoca@users.noreply.github.com>
2025-04-24 09:50:13 -0400

    These actions do not support until (#84847)

--- lib/ansible/plugins/doc_fragments/action_core.py
+++ lib/ansible/plugins/doc_fragments/action_core.py
@@ -29,7 +29,7 @@ attributes:
       platforms: all
     until:
       description: Denotes if this action obeys until/retry/poll keywords
-      support: full
+      support: none
     tags:
       description: Allows for the 'tags' keyword to control the selection of this action for execution
       support: full
