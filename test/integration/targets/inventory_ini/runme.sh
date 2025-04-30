2e3089f0033c2563f5a62b05ee137a711d7563cb
Brian Coca <brian.coca+git@gmail.com>
2025-04-30 11:14:35 -0400

    correct redir order

--- test/integration/targets/inventory_ini/runme.sh
+++ test/integration/targets/inventory_ini/runme.sh
@@ -12,21 +12,21 @@ trap 'rm -r inventory.toml' EXIT
 cp inventory.ini inventory.toml
 
 # check allowed_extensions, deprecation no setting
-ansible-inventory -v -i inventory.toml --list 2>&1 >out
+ansible-inventory -v -i inventory.toml --list >out 2>&1
 test "$(grep -c 'Parsed inventory source with invalid extension' out)" -eq 0
 test "$(grep -c 'testhost' out)" -ne 0
 
 # check allowed_extensions, no deprecation when set
-ANSIBLE_INVENTORY_PLUGIN_INI_EXT='toml' ansible-inventory -v -i inventory.toml --list 2>&1 >out
+ANSIBLE_INVENTORY_PLUGIN_INI_EXT='toml' ansible-inventory -v -i inventory.toml --list >out 2>&1
 test "$(grep -c 'Parsed inventory source with invalid extension' out)" -eq 0
 test "$(grep -c 'testhost' out)" -ne 0
 
 # check allowed_extensions, set to ini
-ANSIBLE_INVENTORY_PLUGIN_INI_EXT='ini' ansible-inventory -v -i inventory.ini --list 2>&1 >out
+ANSIBLE_INVENTORY_PLUGIN_INI_EXT='ini' ansible-inventory -v -i inventory.ini --list >out 2>&1
 test "$(grep -c 'Parsed inventory source with invalid extension' out)" -eq 0
 test "$(grep -c 'testhost' out)" -ne 0
 
 # should not parse file at all and no msg
-ANSIBLE_INVENTORY_PLUGIN_INI_EXT='ini' ansible-inventory -v -i inventory.toml --list 2>&1 >out
+ANSIBLE_INVENTORY_PLUGIN_INI_EXT='ini' ansible-inventory -v -i inventory.toml --list >out 2>&1
 test "$(grep -c 'Parsed inventory source with invalid extension' out)" -eq 0
 test "$(grep -c 'testhost' out)" -eq 0
