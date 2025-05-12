516be30abbf3cb4edc6442219a09971ddcbaaadf
Yogesh Rana <Yogesh.Rana@ibm.com>
2025-05-12 12:29:31 +0530

    changed blockinfile processing from byte to text mode

--- lib/ansible/modules/blockinfile.py
+++ lib/ansible/modules/blockinfile.py
@@ -102,6 +102,12 @@ options:
     type: bool
     default: no
     version_added: '2.16'
+  encoding:
+    description:
+      - The character set in which the target file is encoded.
+    type: str
+    default: utf-8
+    version_added: "2.19"  
 notes:
   - When using C(with_*) loops be aware that if you do not set a unique mark the block will be overwritten on each iteration.
   - As of Ansible 2.3, the O(dest) option has been changed to O(path) as default, but O(dest) still works as well.
@@ -194,14 +200,17 @@ import os
 import tempfile
 from ansible.module_utils.six import b
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.common.text.converters import to_bytes, to_native
+from ansible.module_utils.common.text.converters import to_bytes, to_native, to_text
 
 
-def write_changes(module, contents, path):
+def write_changes(module, contents, path,encoding=None):
 
     tmpfd, tmpfile = tempfile.mkstemp(dir=module.tmpdir)
-    with os.fdopen(tmpfd, 'wb') as tf:
-        tf.write(contents)
+    # with os.fdopen(tmpfd, 'wb') as tf:
+    #     tf.write(contents)
+    with os.fdopen(tmpfd,'w',encoding=encoding) as tf:
+        tf.writelines(contents)
+
 
     validate = module.params.get('validate', None)
     valid = not validate
@@ -246,6 +255,7 @@ def main():
             marker_end=dict(type='str', default='END'),
             append_newline=dict(type='bool', default=False),
             prepend_newline=dict(type='bool', default=False),
+            encoding=dict(type='str'),
         ),
         mutually_exclusive=[['insertbefore', 'insertafter']],
         add_file_common_args=True,
@@ -253,6 +263,8 @@ def main():
     )
     params = module.params
     path = params['path']
+
+    encoding=module.params.get('encoding',None)
 
     if os.path.isdir(path):
         module.fail_json(rc=256,
@@ -274,9 +286,13 @@ def main():
         original = None
         lines = []
     else:
-        with open(path, 'rb') as f:
-            original = f.read()
-        lines = original.splitlines(True)
+        # with open(path, 'rb') as f:
+        #     original = f.read()
+        with open(path,'r',encoding=encoding) as f:
+            original=f.read()
+            lines = original.splitlines(True)
+            # lines = [s for s in original]
+        # lines = original.splitlines(True)
 
     diff = {'before': '',
             'after': '',
@@ -288,10 +304,13 @@ def main():
 
     insertbefore = params['insertbefore']
     insertafter = params['insertafter']
-    block = to_bytes(params['block'])
-    marker = to_bytes(params['marker'])
+    # block = to_bytes(params['block'])
+    # marker = to_bytes(params['marker'])
+    block = to_text(params['block'])
+    marker = to_text(params['marker'])
     present = params['state'] == 'present'
-    blank_line = [b(os.linesep)]
+    # blank_line = [b(os.linesep)]
+    blank_line = [(os.linesep)]
 
     if not present and not path_exists:
         module.exit_json(changed=False, msg="File %s not present" % path)
@@ -300,17 +319,27 @@ def main():
         insertafter = 'EOF'
 
     if insertafter not in (None, 'EOF'):
-        insertre = re.compile(to_bytes(insertafter, errors='surrogate_or_strict'))
+        insertre = re.compile(to_text(insertafter, errors='surrogate_or_strict'))
+        # insertre = re.compile(to_bytes(insertafter, errors='surrogate_or_strict'))
     elif insertbefore not in (None, 'BOF'):
-        insertre = re.compile(to_bytes(insertbefore, errors='surrogate_or_strict'))
+        insertre = re.compile(to_text(insertbefore, errors='surrogate_or_strict'))
+        # insertre = re.compile(to_bytes(insertbefore, errors='surrogate_or_strict'))
     else:
         insertre = None
 
-    marker0 = re.sub(b(r'{mark}'), b(params['marker_begin']), marker) + b(os.linesep)
-    marker1 = re.sub(b(r'{mark}'), b(params['marker_end']), marker) + b(os.linesep)
+    # marker0 = re.sub(b(r'{mark}'), b(params['marker_begin']), marker) + b(os.linesep)
+    # marker1 = re.sub(b(r'{mark}'), b(params['marker_end']), marker) + b(os.linesep)
+    marker0 = re.sub((r'{mark}'), (params['marker_begin']), marker) + (os.linesep)
+    marker1 = re.sub((r'{mark}'), (params['marker_end']), marker) + (os.linesep)
+
+    # diff['marker0'] = marker0
+    # diff['marker1'] = marker1
+
     if present and block:
-        if not block.endswith(b(os.linesep)):
-            block += b(os.linesep)
+        if not block.endswith((os.linesep)):
+            block += (os.linesep)
+        # if not block.endswith(b(os.linesep)):
+        #     block += b(os.linesep)
         blocklines = [marker0] + block.splitlines(True) + [marker1]
     else:
         blocklines = []
@@ -352,15 +381,18 @@ def main():
 
     # Ensure there is a line separator before the block of lines to be inserted
     if n0 > 0:
-        if not lines[n0 - 1].endswith(b(os.linesep)):
-            lines[n0 - 1] += b(os.linesep)
+        if not lines[n0 - 1].endswith((os.linesep)):
+            lines[n0 - 1] += (os.linesep)
+        # if not lines[n0 - 1].endswith(b(os.linesep)):
+        #     lines[n0 - 1] += b(os.linesep)
 
     # Before the block: check if we need to prepend a blank line
     # If yes, we need to add the blank line if we are not at the beginning of the file
     # and the previous line is not a blank line
     # In both cases, we need to shift by one on the right the inserting position of the block
     if params['prepend_newline'] and present:
-        if n0 != 0 and lines[n0 - 1] != b(os.linesep):
+          if n0 != 0 and lines[n0 - 1] != (os.linesep):
+        # if n0 != 0 and lines[n0 - 1] != b(os.linesep):
             lines[n0:n0] = blank_line
             n0 += 1
 
@@ -372,13 +404,18 @@ def main():
     # and the line right after is not a blank line
     if params['append_newline'] and present:
         line_after_block = n0 + len(blocklines)
-        if line_after_block < len(lines) and lines[line_after_block] != b(os.linesep):
+        if line_after_block < len(lines) and lines[line_after_block] != (os.linesep):
+        # if line_after_block < len(lines) and lines[line_after_block] != b(os.linesep):
             lines[line_after_block:line_after_block] = blank_line
 
+    # if lines:
+    #     result = b''.join(lines)
+    # else:
+    #     result = b''
     if lines:
-        result = b''.join(lines)
+        result = ''.join(lines)
     else:
-        result = b''
+        result = ''
 
     if module._diff:
         diff['after'] = result
@@ -402,7 +439,8 @@ def main():
             backup_file = module.backup_local(path)
         # We should always follow symlinks so that we change the real file
         real_path = os.path.realpath(params['path'])
-        write_changes(module, result, real_path)
+        # write_changes(module, result, real_path)
+        write_changes(module, result, real_path,encoding)
 
     if module.check_mode and not path_exists:
         module.exit_json(changed=changed, msg=msg, diff=diff)
