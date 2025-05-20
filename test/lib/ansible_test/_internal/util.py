332377d5b9cef16d274df0d17b4335e019bdda17
Matt Clay <matt@mystile.com>
2025-05-20 11:11:45 -0700

    ansible-test - Fix option filtering (#85182)

--- test/lib/ansible_test/_internal/util.py
+++ test/lib/ansible_test/_internal/util.py
@@ -254,12 +254,29 @@ def filter_args(args: list[str], filters: dict[str, int]) -> list[str]:
     """Return a filtered version of the given command line arguments."""
     remaining = 0
     result = []
+    pass_through_args: list[str] = []
+    pass_through_explicit = False
+    pass_through_implicit = False
 
     for arg in args:
-        if not arg.startswith('-') and remaining:
-            remaining -= 1
+        if pass_through_explicit:
+            pass_through_args.append(arg)
             continue
 
+        if arg == '--':
+            pass_through_explicit = True
+            continue
+
+        if not arg.startswith('-') and remaining:
+            remaining -= 1
+            pass_through_implicit = not remaining
+            continue
+
+        if not arg.startswith('-') and pass_through_implicit:
+            pass_through_args.append(arg)
+            continue
+
+        pass_through_implicit = False
         remaining = 0
 
         parts = arg.split('=', 1)
@@ -270,6 +287,9 @@ def filter_args(args: list[str], filters: dict[str, int]) -> list[str]:
             continue
 
         result.append(arg)
+
+    if pass_through_args:
+        result += ['--'] + pass_through_args
 
     return result
 
--- test/units/ansible_test/_internal/test_util.py
+++ test/units/ansible_test/_internal/test_util.py
@@ -34,3 +34,48 @@ def test_failed_interactive_command() -> None:
 
     assert error.value.stdout is None
     assert error.value.stderr is None
+
+
+@pytest.mark.parametrize("args,filters,expected", (
+    (
+        # args after a known option must be separated from existing args
+        ['--color', '--docker', 'default', 'ping', 'split'],
+        {'--docker': 1},
+        ['--color', '--', 'ping', 'split'],
+    ),
+    (
+        # args after '--' are not options
+        ['--', '--color'],
+        {'--color': 1},
+        ['--', '--color'],
+    ),
+    (
+        # args after a known option must be separated from existing args without conflicting with an explicit '--'
+        ['--color', '--docker', 'default', 'ping', '--', 'split'],
+        {'--docker': 1},
+        ['--color', '--', 'ping', 'split'],
+    ),
+    (
+        # args before options are properly handled
+        ['ping', '--docker', 'default', '-v'],
+        {'--docker': 1},
+        ['ping', '-v'],
+    ),
+    (
+        # no options
+        ['ping', 'split'],
+        {'--docker': 1},
+        ['ping', 'split'],
+    ),
+    (
+        # only options
+        ['--color', '--docker', 'default', '-v'],
+        {'--docker': 1},
+        ['--color', '-v'],
+    )
+))
+def test_filter_args(args: list[str], filters: dict[str, int], expected: list[str]) -> None:
+    """Verify arg filtering properly handles various scenarios."""
+    from ansible_test._internal.util import filter_args
+
+    assert filter_args(args, filters) == expected
