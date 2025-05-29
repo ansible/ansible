8a9ba90187ae581b35dc15b30ccf9e90fd65a85d
Martin Krizek <martin.krizek@gmail.com>
2025-05-29 10:54:53 +0200

    Deprecate ansible.compat.importlib_resources

--- lib/ansible/compat/importlib_resources.py
+++ lib/ansible/compat/importlib_resources.py
@@ -3,17 +3,13 @@
 
 from __future__ import annotations
 
-import sys
+from ansible.utils.display import Display as _Display
 
-HAS_IMPORTLIB_RESOURCES = False
+from importlib.resources import files  # pylint: disable=unused-import
 
-if sys.version_info < (3, 10):
-    try:
-        from importlib_resources import files  # type: ignore[import]  # pylint: disable=unused-import
-    except ImportError:
-        files = None  # type: ignore[assignment]
-    else:
-        HAS_IMPORTLIB_RESOURCES = True
-else:
-    from importlib.resources import files
-    HAS_IMPORTLIB_RESOURCES = True
+HAS_IMPORTLIB_RESOURCES = True
+
+_Display().deprecated(
+    msg="The `ansible.compat.importlib_resources` module is deprecated. Use `importlib.resources` from Python standard library instead.",
+    version="2.23",
+)
