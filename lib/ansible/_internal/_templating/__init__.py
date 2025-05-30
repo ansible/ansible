d7ee3afd55009d5d66ad3895403fafdd2516ac54
Martin Krizek <martin.krizek@gmail.com>
2025-05-30 11:40:10 +0200

    jinja2.__version__ is deprecated (#85221)
    Prepare for its removal in Jinja 3.3.
    
    See https://github.com/pallets/jinja/pull/2098

--- lib/ansible/_internal/_templating/__init__.py
+++ lib/ansible/_internal/_templating/__init__.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
-from jinja2 import __version__ as _jinja2_version
+import importlib.metadata
+
+jinja2_version = importlib.metadata.version('jinja2')
 
 # DTFIX-FUTURE: sanity test to ensure this doesn't drift from requirements
 _MINIMUM_JINJA_VERSION = (3, 1)
-_CURRENT_JINJA_VERSION = tuple(map(int, _jinja2_version.split('.', maxsplit=2)[:2]))
+_CURRENT_JINJA_VERSION = tuple(map(int, jinja2_version.split('.', maxsplit=2)[:2]))
 
 if _CURRENT_JINJA_VERSION < _MINIMUM_JINJA_VERSION:
-    raise RuntimeError(f'Jinja version {".".join(map(str, _MINIMUM_JINJA_VERSION))} or higher is required (current version {_jinja2_version}).')
+    raise RuntimeError(f'Jinja version {".".join(map(str, _MINIMUM_JINJA_VERSION))} or higher is required (current version {jinja2_version}).')
