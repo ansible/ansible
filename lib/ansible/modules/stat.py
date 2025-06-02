0b774170a178e4cf67398f5dc56ed26b20dde77e
Martin Krizek <martin.krizek@gmail.com>
2025-06-02 10:24:37 +0200

    revert stat

--- lib/ansible/modules/stat.py
+++ lib/ansible/modules/stat.py
@@ -458,6 +458,8 @@ def main():
     except FileNotFoundError:
         output = {'exists': False}
         module.exit_json(changed=False, stat=output)
+    except OSError as ex:
+        module.fail_json(msg=ex.strerror, exception=ex)
 
     # process base results
     output = format_output(module, path, st)
