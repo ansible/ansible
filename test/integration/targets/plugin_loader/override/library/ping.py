11fcb37dfb396cc441f950c8c667fd97800732e6
s-hertel <19572925+s-hertel@users.noreply.github.com>
2025-06-04 10:47:21 -0400

    Fix boilerplate

--- test/integration/targets/plugin_loader/override/library/ping.py
+++ test/integration/targets/plugin_loader/override/library/ping.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
-
 # (c) 2012, Michael DeHaan <michael.dehaan@gmail.com>
 # (c) 2016, Toshio Kuratomi <tkuratomi@ansible.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-
+from __future__ import annotations
 
 DOCUMENTATION = '''
 ---
