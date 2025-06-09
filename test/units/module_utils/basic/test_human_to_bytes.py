41d3d0d2d1af99d3717c34b4df7f13f51ade6b74
Felix Fontein <felix@fontein.de>
2025-06-09 16:52:39 +0200

    Forgot tests without keyword.
    Signed-off-by: Abhijeet Kasurde <Akasurde@redhat.com>

--- test/units/module_utils/basic/test_human_to_bytes.py
+++ test/units/module_utils/basic/test_human_to_bytes.py
@@ -1,29 +1,47 @@
 # -*- coding: utf-8 -*-
-# (c) 2025 Ansible Project
+# Copyright: (c) 2025 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import annotations
 
-import unittest
-
 import pytest
+
+from ansible.module_utils.basic import AnsibleModule
+
+
+DATA = [
+    ("4KB", False, 4096),
+    ("4KB", None, 4096),
+    ("4Kb", True, 4096),
+]
+
+WO_ISBITS_DATA = [
+    ("4KB", 4096),
+]
+
+EXCEPTION_DATA = [
+    ("4Kb", False),
+    ("4KB", True),
+]
 
 
 @pytest.mark.usefixtures("stdin")
-class TestOtherFilesystem(unittest.TestCase):
-    def test_human_to_bytes(self):
-        from ansible.module_utils import basic
+@pytest.mark.parametrize('input, isbits, expected', DATA)
+def test_validator_function(input, isbits, expected):
+    am = AnsibleModule(argument_spec=dict())
+    assert am.human_to_bytes(input, isbits=isbits) == expected
 
-        am = basic.AnsibleModule(
-            argument_spec=dict(),
-        )
 
-        self.assertEqual(am.human_to_bytes("4KB"), 4096)
-        self.assertEqual(am.human_to_bytes("4KB", False), 4096)
-        self.assertEqual(am.human_to_bytes("4KB", isbits=False), 4096)
-        with pytest.raises(ValueError):
-            am.human_to_bytes("4Kb", isbits=False)
-        self.assertEqual(am.human_to_bytes("4Kb", True), 4096)
-        self.assertEqual(am.human_to_bytes("4Kb", isbits=True), 4096)
-        with pytest.raises(ValueError):
-            am.human_to_bytes("4KB", isbits=True)
+@pytest.mark.usefixtures("stdin")
+@pytest.mark.parametrize('input, expected', WO_ISBITS_DATA)
+def test_validator_functio(input, expected):
+    am = AnsibleModule(argument_spec=dict())
+    assert am.human_to_bytes(input) == expected
+
+
+@pytest.mark.usefixtures("stdin")
+@pytest.mark.parametrize('input, isbits', EXCEPTION_DATA)
+def test_validator_functions(input, isbits):
+    am = AnsibleModule(argument_spec=dict())
+    with pytest.raises(ValueError):
+        am.human_to_bytes(input, isbits=isbits)
