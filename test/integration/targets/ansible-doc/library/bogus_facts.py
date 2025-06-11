59b55e70514b681a46f0d464dd678ba42598e68f
Brian Coca <brian.coca+git@gmail.com>
2025-06-11 15:01:18 -0400

    fix for pytest

--- test/integration/targets/ansible-doc/library/bogus_facts.py
+++ test/integration/targets/ansible-doc/library/bogus_facts.py
@@ -1,5 +1,5 @@
-DOCUMENTATION="""
-    module : bogus_facts
+DOCUMENTATION = """
+    module: bogus_facts
     short_description: bad facts returned
     version_added: historical
     description: bogus facts for testing
