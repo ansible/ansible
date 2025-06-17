5d3777dff6071b6e1a7881d0098dd47f2ace3a88
Patrick Kingston <pkingsto@redhat.com>
2025-06-17 14:16:08 -0400

    Signed the wall.

--- lib/ansible/cli/adhoc.py
+++ lib/ansible/cli/adhoc.py
@@ -4,6 +4,8 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 # PYTHON_ARGCOMPLETE_OK
 
+# Patrick was here
+
 from __future__ import annotations
 
 import json
