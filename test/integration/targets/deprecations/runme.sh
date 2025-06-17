9416af70cf9ae570812482c6ac24fdbaa9cb89f9
Brian Coca <brian.coca+git@gmail.com>
2025-06-17 08:55:22 -0400

    just copy

--- test/integration/targets/deprecations/runme.sh
+++ test/integration/targets/deprecations/runme.sh
@@ -56,7 +56,6 @@ export ANSIBLE_CACHE_PLUGIN=notjsonfile
 # check for plugin deprecation
 [ "$(ansible-doc -t cache notjsonfile --playbook-dir ./ | grep -c 'DEPRECATED:')" -eq "1" ]
 
-DEP_MSG='INJECT_FACTS_AS_VARS default to '
 # Injection default is deprecated
-[ "$(ansible-playbook injectfacts.yml 2>&1 | grep -c """${DEP_MSG}")""" -eq "1" ]
-[ "$(ANSIBLE_INJECT_FACT_VARS=1 ansible-playbook injectfacts.yml 2>&1 | grep -c """${DEP_MSG}""")" -eq "0" ]
+[ "$(ansible-playbook injectfacts.yml 2>&1 | grep -c 'INJECT_FACTS_AS_VARS default to ' -eq "1" ]
+[ "$(ANSIBLE_INJECT_FACT_VARS=1 ansible-playbook injectfacts.yml 2>&1 | grep -c 'INJECT_FACTS_AS_VARS default to ')" -eq "0" ]
