ebae950db2a148705f2a133e5a1a1b7fec37ead8
Matt Davis <6775756+nitzmahone@users.noreply.github.com>
2025-06-25 17:42:23 -0700

    Encryptedstring redact fixes (#85390)
    * misc DTFIX/docstring cleanup
    
    * fix EncryptedString redaction, add tests
    
    Co-authored-by: Matt Clay <matt@mystile.com>
    
    * Fix test failures
    
    ---------
    
    Co-authored-by: Matt Clay <matt@mystile.com>
    (cherry picked from commit 649c9ec4431feca274e4d14d7a1bc835d70c10b9)

--- lib/ansible/_internal/_json/__init__.py
+++ lib/ansible/_internal/_json/__init__.py
@@ -147,9 +147,20 @@ class AnsibleVariableVisitor:
         """Internal implementation to recursively visit a data structure's contents."""
         self._current = key  # supports StateTrackingMixIn
 
-        value_type = type(value)
+        value_type: type = type(value)
 
-        if self.apply_transforms and value_type in _transform._type_transform_mapping:
+        # handle EncryptedString conversion before more generic transformation and native conversions
+        if value_type is EncryptedString:  # pylint: disable=unidiomatic-typecheck
+            match self.encrypted_string_behavior:
+                case EncryptedStringBehavior.DECRYPT:
+                    value = str(value)  # type: ignore[assignment]
+                    value_type = str
+                case EncryptedStringBehavior.REDACT:
+                    value = "<redacted>"  # type: ignore[assignment]
+                    value_type = str
+                case EncryptedStringBehavior.FAIL:
+                    raise AnsibleVariableTypeError.from_value(obj=value)
+        elif self.apply_transforms and value_type in _transform._type_transform_mapping:
             value = self._template_engine.transform(value)
             value_type = type(value)
 
@@ -162,8 +173,6 @@ class AnsibleVariableVisitor:
         # DTFIX-FUTURE: Visitor generally ignores dict/mapping keys by default except for debugging and schema-aware checking.
         #               It could be checking keys destined for variable storage to apply more strict rules about key shape and type.
 
-        # DTFIX0: some type lists being consulted (the ones from datatag) are probably too permissive, and perhaps should not be dynamic
-
         if (result := self._early_visit(value, value_type)) is not _sentinel:
             pass
         # DTFIX7: de-duplicate and optimize; extract inline generator expressions and fallback function or mapping for native type calculation?
@@ -173,14 +182,6 @@ class AnsibleVariableVisitor:
         elif value_type in _ANSIBLE_ALLOWED_NON_SCALAR_COLLECTION_VAR_TYPES:
             with self:  # supports StateTrackingMixIn
                 result = AnsibleTagHelper.tag_copy(value, (self._visit(k, v) for k, v in enumerate(t.cast(t.Iterable, value))), value_type=value_type)
-        elif self.encrypted_string_behavior != EncryptedStringBehavior.FAIL and isinstance(value, EncryptedString):
-            match self.encrypted_string_behavior:
-                case EncryptedStringBehavior.REDACT:
-                    result = "<redacted>"  # type: ignore[assignment]
-                case EncryptedStringBehavior.PRESERVE:
-                    result = value  # type: ignore[assignment]
-                case EncryptedStringBehavior.DECRYPT:
-                    result = str(value)  # type: ignore[assignment]
         elif self.convert_mapping_to_dict and _internal.is_intermediate_mapping(value):
             with self:  # supports StateTrackingMixIn
                 result = {self._visit_key(k): self._visit(k, v) for k, v in value.items()}  # type: ignore[assignment]
@@ -193,12 +194,13 @@ class AnsibleVariableVisitor:
             result = float(value)  # type: ignore[assignment]
         elif self.convert_custom_scalars and isinstance(value, int) and not isinstance(value, bool):
             result = int(value)  # type: ignore[assignment]
-        else:
-            if value_type not in _ANSIBLE_ALLOWED_VAR_TYPES:
-                raise AnsibleVariableTypeError.from_value(obj=value)
-
+        elif value_type in _ANSIBLE_ALLOWED_VAR_TYPES:
             # supported scalar type that requires no special handling, just return as-is
             result = value
+        elif self.encrypted_string_behavior is EncryptedStringBehavior.PRESERVE and isinstance(value, EncryptedString):
+            result = value  # type: ignore[assignment]
+        else:
+            raise AnsibleVariableTypeError.from_value(obj=value)
 
         if self.origin and not Origin.is_tagged_on(result):
             # apply shared instance default origin tag
