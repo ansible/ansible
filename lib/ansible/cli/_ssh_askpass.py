16999ea4d95d9e84220f3bff1b03841042d30e34
Martin Krizek <martin.krizek@gmail.com>
2025-06-28 04:01:33 +0200

    Limit askpass prompts to single attempt (#85364)
    * Limit askpass prompts to single attempt
    
    OpenSSH client option NumberOfPasswordPrompts defaults to 3 so in case
    an incorrect connection password is provided it is excessively tried 3
    times. Not only that but running the `_ssh_askpass` entry point multiple
    times (via ssh) results in `json.decoder.JSONDecodeError` as after the
    first run the shared memory is zero'd and the subsequent runs end up
    calling `json.loads` on empty data.
    
    `json.decoder.JSONDecodeError` does not happen prior to Python 3.13 as
    the share memory is unlinked automatically on `.close()` and the
    `_ssh_askpass` entry point exits with return code 1 before attempting to
    load zero'd memory.
    
    Fixes #85359
    
    * changelog and tests
    
    * Update changelogs/fragments/85359-askpass-incorrect-password-retries.yml
    
    Co-authored-by: Matt Davis <6775756+nitzmahone@users.noreply.github.com>
    
    * Update lib/ansible/cli/_ssh_askpass.py
    
    Co-authored-by: Martin Krizek <martin.krizek@gmail.com>
    
    * Avoid race condition in second unlink
    
    ---------
    
    Co-authored-by: Matt Davis <6775756+nitzmahone@users.noreply.github.com>
    Co-authored-by: Matt Clay <matt@mystile.com>
    (cherry picked from commit 54ccad9e460cb6bfa0ff585a46289239966b56cc)

--- lib/ansible/cli/_ssh_askpass.py
+++ lib/ansible/cli/_ssh_askpass.py
@@ -3,45 +3,52 @@
 from __future__ import annotations
 
 import json
+import multiprocessing.resource_tracker
 import os
 import re
 import sys
 import typing as t
-from multiprocessing.shared_memory import SharedMemory
 
-HOST_KEY_RE = re.compile(
-    r'(The authenticity of host |differs from the key for the IP address)',
-)
+from multiprocessing.shared_memory import SharedMemory
 
 
 def main() -> t.Never:
-    try:
-        if HOST_KEY_RE.search(sys.argv[1]):
-            sys.stdout.buffer.write(b'no')
-            sys.stdout.flush()
-            sys.exit(0)
-    except IndexError:
-        pass
+    if len(sys.argv) > 1:
+        exit_code = 0 if handle_prompt(sys.argv[1]) else 1
+    else:
+        exit_code = 1
 
-    kwargs: dict[str, bool] = {}
-    if sys.version_info[:2] >= (3, 13):
-        # deprecated: description='unneeded due to track argument for SharedMemory' python_version='3.12'
-        kwargs['track'] = False
-    try:
-        shm = SharedMemory(name=os.environ['_ANSIBLE_SSH_ASKPASS_SHM'], **kwargs)
-    except FileNotFoundError:
-        # We must be running after the ansible fork is shutting down
-        sys.exit(1)
+    sys.exit(exit_code)
+
+
+def handle_prompt(prompt: str) -> bool:
+    if re.search(r'(The authenticity of host |differs from the key for the IP address)', prompt):
+        sys.stdout.write('no')
+        sys.stdout.flush()
+        return True
+
+    # deprecated: description='Python 3.13 and later support track' python_version='3.12'
+    can_track = sys.version_info[:2] >= (3, 13)
+    kwargs = dict(track=False) if can_track else {}
+
+    # This SharedMemory instance is intentionally not closed or unlinked.
+    # Closing will occur naturally in the SharedMemory finalizer.
+    # Unlinking is the responsibility of the process which created it.
+    shm = SharedMemory(name=os.environ['_ANSIBLE_SSH_ASKPASS_SHM'], **kwargs)
+
+    if not can_track:
+        # When track=False is not available, we must unregister explicitly, since it otherwise only occurs during unlink.
+        # This avoids resource tracker noise on stderr during process exit.
+        multiprocessing.resource_tracker.unregister(shm._name, 'shared_memory')
+
     cfg = json.loads(shm.buf.tobytes().rstrip(b'\x00'))
 
-    try:
-        if cfg['prompt'] not in sys.argv[1]:
-            sys.exit(1)
-    except IndexError:
-        sys.exit(1)
+    if cfg['prompt'] not in prompt:
+        return False
 
-    sys.stdout.buffer.write(cfg['password'].encode('utf-8'))
+    # Report the password provided by the SharedMemory instance.
+    # The contents are left untouched after consumption to allow subsequent attempts to succeed.
+    # This can occur when multiple password prompting methods are enabled, such as password and keyboard-interactive, which is the default on macOS.
+    sys.stdout.write(cfg['password'])
     sys.stdout.flush()
-    shm.buf[:] = b'\x00' * shm.size
-    shm.close()
-    sys.exit(0)
+    return True
