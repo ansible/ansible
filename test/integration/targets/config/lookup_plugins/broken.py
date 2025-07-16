a403e038e65ebc35390bd2b07acbec88a6f60670
Brian Coca <briancoca+git@gmail.com>
2025-07-16 10:04:34 -0400

    broken test lookup now compliant

--- test/integration/targets/config/lookup_plugins/broken.py
+++ test/integration/targets/config/lookup_plugins/broken.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2025, Felix Fontein <felix@fontein.de>
+# Copyright (c) 2025, Felix Fontein <felix@fontein.de>, The Ansible Project
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
+from __future__ import annotations
+
 
 DOCUMENTATION = r"""
 name: broken
