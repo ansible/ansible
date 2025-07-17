c6a168d2df32830772e52c48f33e5775870144a3
Abhijeet Kasurde <Akasurde@redhat.com>
2025-07-17 12:14:52 -0700

    encryption: Raise an error when vaultid is used
    Raise an exception when using deprecated vaultid option
    
    Signed-off-by: Abhijeet Kasurde <Akasurde@redhat.com>

--- lib/ansible/plugins/filter/encryption.py
+++ lib/ansible/plugins/filter/encryption.py
@@ -21,16 +21,7 @@ def do_vault(data, secret, salt=None, vault_id='filter_default', wrap_object=Fal
         raise TypeError(f"Can only vault strings, instead we got {type(data)}.")
 
     if vaultid is not None:
-        display.deprecated(
-            msg="Use of undocumented `vaultid`.",
-            version="2.20",
-            help_text="Use `vault_id` instead.",
-        )
-
-        if vault_id == 'filter_default':
-            vault_id = vaultid
-        else:
-            display.warning("Ignoring vaultid as vault_id is already set.")
+        raise AnsibleError("Using option 'vaultid' is deprecated. Use 'vault_id' instead.")
 
     vs = VaultSecret(to_bytes(secret))
     vl = VaultLib()
@@ -62,16 +53,7 @@ def do_unvault(vault, secret, vault_id='filter_default', vaultid=None):
         raise TypeError(f"Vault should be in the form of a string, instead we got {type(vault)}.")
 
     if vaultid is not None:
-        display.deprecated(
-            msg="Use of undocumented `vaultid`.",
-            version="2.20",
-            help_text="Use `vault_id` instead.",
-        )
-
-        if vault_id == 'filter_default':
-            vault_id = vaultid
-        else:
-            display.warning("Ignoring vaultid as vault_id is already set.")
+        raise AnsibleError("Using option 'vaultid' is deprecated. Use 'vault_id' instead.")
 
     vs = VaultSecret(to_bytes(secret))
     vl = VaultLib([(vault_id, vs)])
