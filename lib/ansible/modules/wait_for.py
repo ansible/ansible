06a610cd300f4029619f8bcc3471def03193a954
Abhijeet Kasurde <akasurde@redhat.com>
2025-07-22 08:18:38 -0700

    docs: use correct URL in docs (#85491)
    * wait_for: use correct URL in docs
    
    Signed-off-by: Abhijeet Kasurde <Akasurde@redhat.com>

--- lib/ansible/modules/wait_for.py
+++ lib/ansible/modules/wait_for.py
@@ -216,13 +216,13 @@ elapsed:
   type: int
   sample: 23
 match_groups:
-  description: Tuple containing all the subgroups of the match as returned by U(https://docs.python.org/3/library/re.html#re.MatchObject.groups)
+  description: Tuple containing all the subgroups of the match as returned by U(https://docs.python.org/3/library/re.html#re.Match.groups)
   returned: always
   type: list
   sample: ['match 1', 'match 2']
 match_groupdict:
   description: Dictionary containing all the named subgroups of the match, keyed by the subgroup name,
-    as returned by U(https://docs.python.org/3/library/re.html#re.MatchObject.groupdict)
+    as returned by U(https://docs.python.org/3/library/re.html#re.Match.groupdict)
   returned: always
   type: dict
   sample:
