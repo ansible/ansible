77ab4ff8a7ad79db678b746439677c08a0185eba
Abhijeet Kasurde <Akasurde@redhat.com>
2025-07-22 12:46:29 -0700

    Review requests
    Signed-off-by: Abhijeet Kasurde <Akasurde@redhat.com>

--- lib/ansible/modules/wait_for_connection.py
+++ lib/ansible/modules/wait_for_connection.py
@@ -104,13 +104,9 @@ EXAMPLES = r"""
         - cmd.exe /c winrm.cmd quickconfig -quiet -force
     delegate_to: localhost
 
-  - name: Wait for system to become reachable over WinRM
+  - name: Wait for system to become reachable over WinRM, polling every 10 seconds
     ansible.builtin.wait_for_connection:
       timeout: 900
-
-  - name: Wait for server to become reachable, polling every 10 seconds
-    ansible.builtin.wait_for_connection:
-      timeout: 300
       sleep: 10
 
   - name: Gather facts for first time
