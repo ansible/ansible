5eace8cbf931827b13cd53868d63fa9f425a9a95
Matt Clay <matt@mystile.com>
2025-07-23 14:42:45 -0700

    ansible-test - Use OS packages on FreeBSD 13.5 (#85530)

--- test/lib/ansible_test/_util/target/setup/bootstrap.sh
+++ test/lib/ansible_test/_util/target/setup/bootstrap.sh
@@ -169,6 +169,9 @@ bootstrap_remote_freebsd()
         # Declare platform/python version combinations which do not have supporting OS packages available.
         # For these combinations ansible-test will use pip to install the requirements instead.
         case "${platform_version}/${python_version}" in
+            13.5/3.11)
+                # defaults available
+                ;;
             14.2/3.11)
                 # defaults available
                 ;;
