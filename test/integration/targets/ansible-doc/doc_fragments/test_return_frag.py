dae6d3dc09bdd423b26760907e78b18ad903a1fe
Brian Coca <briancoca+git@gmail.com>
2025-07-28 18:55:50 -0400

    ansible-doc add ability to use doc_fragments for RETURN

---
+++ test/integration/targets/ansible-doc/doc_fragments/test_return_frag.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+
+class ModuleDocFragment(object):
+
+    # Standard documentation fragment
+    RETURN = r'''
+z_last:
+    description: A last result.
+    type: str
+    returned: success
+'''
