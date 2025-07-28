1dec4e6d82e1317db4387e3819b1529d981205ee
Matt Clay <matt@mystile.com>
2025-07-28 11:56:40 -0700

    Add support for Python 3.14 and drop Python 3.8

--- lib/ansible/utils/collection_loader/_collection_finder.py
+++ lib/ansible/utils/collection_loader/_collection_finder.py
@@ -26,20 +26,15 @@ from . import _to_bytes, _to_text
 from ._collection_config import AnsibleCollectionConfig
 
 try:
-    try:
-        # Available on Python >= 3.11
-        # We ignore the import error that will trigger when running mypy with
-        # older Python versions.
-        from importlib.resources.abc import TraversableResources  # type: ignore[import]
-    except ImportError:
-        # Used with Python 3.9 and 3.10 only
-        # This member is still available as an alias up until Python 3.14 but
-        # is deprecated as of Python 3.12.
-        from importlib.abc import TraversableResources  # deprecated: description='TraversableResources move' python_version='3.10'
+    # Available on Python >= 3.11
+    # We ignore the import error that will trigger when running mypy with
+    # older Python versions.
+    from importlib.resources.abc import TraversableResources  # type: ignore[import]
 except ImportError:
-    # Python < 3.9
-    # deprecated: description='TraversableResources fallback' python_version='3.8'
-    TraversableResources = object  # type: ignore[assignment,misc]
+    # Used with Python 3.9 and 3.10 only
+    # This member is still available as an alias up until Python 3.14 but
+    # is deprecated as of Python 3.12.
+    from importlib.abc import TraversableResources  # deprecated: description='TraversableResources move' python_version='3.10'
 
 # NB: this supports import sanity test providing a different impl
 try:
