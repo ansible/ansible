38ee0d4c964e805c39f73e119c7568ec83ac019a
Patrick Kingston <pkingsto@redhat.com>
2025-08-18 16:21:28 -0400

    Catch error in dnf.py and dnf5.py

--- lib/ansible/modules/dnf5.py
+++ lib/ansible/modules/dnf5.py
@@ -566,7 +566,10 @@ class Dnf5Module(YumDnf):
         if self.conf_file:
             conf.config_file_path = self.conf_file
 
-        base.load_config()
+        try:
+            base.load_config()
+        except libdnf5.exception.InvalidConfigError as e:
+            self.module.fail_json(msg="Failed to load configuration file", exception=e, conf_file=conf.config_file_path)
 
         if self.releasever is not None:
             variables = base.get_vars()
