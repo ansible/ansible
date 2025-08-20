dfbff0a782f95ac8aee69a875591ebc7897495a8
Patrick Kingston <pkingsto@redhat.com>
2025-08-20 15:14:29 -0400

    Remove error catching in dnf5

--- lib/ansible/modules/dnf5.py
+++ lib/ansible/modules/dnf5.py
@@ -562,10 +562,7 @@ class Dnf5Module(YumDnf):
         if self.conf_file:
             conf.config_file_path = self.conf_file
 
-        try:
-            base.load_config()
-        except LIBDNF5_ERRORS as e:
-            self.module.fail_json(msg="Failed to load configuration file", exception=e, conf_file=conf.config_file_path)
+        base.load_config()
 
         if self.releasever is not None:
             variables = base.get_vars()
