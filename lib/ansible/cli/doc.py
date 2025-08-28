5194fa0de7dd24f8d6fc2f6d74a0ac15ebe50f86
Brian Coca <briancoca+git@gmail.com>
2025-08-28 12:02:43 -0400

    ignore doc fragments in return
    backwards compatibility shim for collections

--- lib/ansible/cli/doc.py
+++ lib/ansible/cli/doc.py
@@ -1244,6 +1244,11 @@ class DocCLI(CLI, RoleMixin):
     def add_fields(text, fields, limit, opt_indent, return_values=False, base_indent='', man=False):
 
         for o in sorted(fields):
+
+            if o == 'extends_documentation_fragment':
+                display.warning('Skipping unsupported documentation fragment in return')
+                continue
+
             # Create a copy so we don't modify the original (in case YAML anchors have been used)
             opt = dict(fields[o])
 
