127e76054770599dd2e85fac2196f1640e6e636c
Martin Krizek <martin.krizek@gmail.com>
2025-09-04 11:09:40 +0200

    Don't special case implicit meta tasks when filtering on tags
    Fixes #85475

--- lib/ansible/playbook/block.py
+++ lib/ansible/playbook/block.py
@@ -376,8 +376,7 @@ class Block(Base, Conditional, CollectionSearch, Taggable, Notifiable, Delegatab
                     filtered_block = evaluate_block(task)
                     if filtered_block.has_tasks():
                         tmp_list.append(filtered_block)
-                elif ((task.action in C._ACTION_META and task.implicit) or
-                        task.evaluate_tags(self._play.only_tags, self._play.skip_tags, all_vars=all_vars)):
+                elif task.evaluate_tags(self._play.only_tags, self._play.skip_tags, all_vars=all_vars):
                     tmp_list.append(task)
             return tmp_list
 
