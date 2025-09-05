b7a06e4f2f2b80ad83c3916f5a9963a8b818de4e
Jordan Borean <jborean93@gmail.com>
2025-09-05 10:24:01 +1000

    Remove unused import

--- lib/ansible/utils/ssh_functions.py
+++ lib/ansible/utils/ssh_functions.py
@@ -20,9 +20,7 @@ from __future__ import annotations
 
 import subprocess
 
-from ansible import constants as C
 from ansible.module_utils.common.text.converters import to_bytes
-from ansible.module_utils.compat.paramiko import _paramiko as paramiko
 from ansible.utils.display import Display
 
 display = Display()
