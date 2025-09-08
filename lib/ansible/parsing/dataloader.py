8d4e2d92a4bfcd01c580a0b35493919e0ed9a397
Matt Martz <matt@sivel.net>
2025-09-08 11:32:36 -0500

    Remove duplicate future import

--- lib/ansible/parsing/dataloader.py
+++ lib/ansible/parsing/dataloader.py
@@ -3,7 +3,6 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import annotations
-from __future__ import annotations
 
 import copy
 import os
