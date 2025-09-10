69027d5cc3e740ce3c916907ba6401f10e8cadd6
Abhijeet Kasurde <Akasurde@redhat.com>
2025-09-10 13:41:12 -0700

    Review requests
    Signed-off-by: Abhijeet Kasurde <Akasurde@redhat.com>

--- lib/ansible/plugins/action/__init__.py
+++ lib/ansible/plugins/action/__init__.py
@@ -673,12 +673,14 @@ class ActionBase(ABC, _AnsiblePluginInfoMixin):
 
         if res['rc'] == 0:
             return remote_paths
-        if res['rc'] == 127:
+        elif res['rc'] == 127:
             # setfacl binary does not exists or we don't have permission to use it.
             self._display.debug(f"setfacl binary does not exist or does not have permission to use it. Trying chmod instead. Err: {res!r}")
-        if res['rc'] == 2:
+        elif res['rc'] == 2:
             # invalid syntax (for example, missing user, missing colon)
             self._display.debug(f"setfacl command failed with an invalid syntax. Trying chmod instead. Err: {res!r}")
+        else:
+            self._display.debug(f'Failed to set facl {setfacl_mode}, got:{res!r}')
 
         # Step 3b: Set execute if we need to. We do this before anything else
         # because some of the methods below might work but not let us set
