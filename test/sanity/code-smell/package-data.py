c06c6b1bfa780d50fb172b7a9bf10d8dd17c8c55
Matt Martz <matt@sivel.net>
2025-09-11 15:15:38 -0500

    Add an override of the /review slash command in claude code

--- test/sanity/code-smell/package-data.py
+++ test/sanity/code-smell/package-data.py
@@ -26,6 +26,7 @@ def collect_sdist_files(complete_file_list: list[str]) -> list[str]:
     ignore_patterns = (
         '.azure-pipelines/*',
         '.cherry_picker.toml',
+        '.claude/*',
         '.git*',
         '.mailmap',
         'bin/*',
