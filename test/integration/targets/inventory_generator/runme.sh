ba0a676a0a787645c902035936552bec208dd75f
Brian Coca <brian.coca+git@gmail.com>
2025-10-18 18:40:01 -0400

    fix test

--- test/integration/targets/inventory_generator/runme.sh
+++ test/integration/targets/inventory_generator/runme.sh
@@ -4,7 +4,7 @@ set -eux
 
 ansible-playbook verify.yml -i generator.yml "${@}"
 
-ANSIBLE_INVENTORY_USE_EXTRA_VARS=True ansible-inventory -i extra_vars_generator.yml --graph -e "region=pune"
+ANSIBLE_INVENTORY_USE_EXTRA_VARS=True ansible-inventory -i extra_vars_generator.yml --graph -e "region=pune" > out.txt
 
 grep 'pune_web' out.txt
 grep 'pune_db' out.txt
