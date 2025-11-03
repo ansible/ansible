ab604d6a13d4fdf2b5f9f91021ed7e0ac90cda34
Matt Davis <6775756+nitzmahone@users.noreply.github.com>
2025-11-03 13:17:43 -0800

    Fix incorrect propagation of task.connection (#86121)
    Co-authored-by: Matt Clay <matt@mystile.com>

--- test/integration/targets/callback_results/callback_plugins/track_connections.py
+++ test/integration/targets/callback_results/callback_plugins/track_connections.py
@@ -11,7 +11,10 @@ DOCUMENTATION = """
     type: aggregate
 """
 
+import functools
+import inspect
 import json
+
 from collections import defaultdict
 
 from ansible.plugins.callback import CallbackBase
@@ -22,20 +25,46 @@ class CallbackModule(CallbackBase):
     CALLBACK_VERSION = 2.0
     CALLBACK_TYPE = 'aggregate'
     CALLBACK_NAME = 'track_connections'
+    CALLBACK_NEEDS_ENABLED = True
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._conntrack = defaultdict(lambda : defaultdict(int))
 
-    def _track(self, result: CallbackTaskResult, *args, **kwargs):
+        self._conntrack = defaultdict(lambda : defaultdict(list))
+
+        # dynamically implement all v2 callback methods that accept `result`
+        for name, sig in ((cb, inspect.signature(getattr(self, cb))) for cb in dir(self) if cb.startswith('v2_')):
+            if 'result' in sig.parameters:
+                setattr(self, name, functools.partial(self._track, event_name=name))
+
+    def _track(self, result: CallbackTaskResult, *_args, event_name: str, **_kwargs):
         host = result.host.get_name()
         task = result.task
 
-        self._conntrack[host][task.connection] += 1
-
-    v2_runner_on_ok = v2_runner_on_failed = _track
-    v2_runner_on_async_poll = v2_runner_on_async_ok = v2_runner_on_async_failed = _track
-    v2_runner_item_on_ok = v2_runner_item_on_failed = _track
+        self._conntrack[host][task.connection].append(f'{event_name}: {task.name}')
 
     def v2_playbook_on_stats(self, stats):
-        self._display.display(json.dumps(self._conntrack, indent=4))
+        expected = {
+            "testhost": {
+                "ansible.builtin.local": [
+                    "v2_runner_on_ok: execute a successful non-loop task with the local connection",
+                    "v2_runner_on_failed: execute a failing non-loop task with the local connection",
+                    "v2_runner_item_on_ok: execute a successful looped task with the local connection",
+                    "v2_runner_on_ok: execute a successful looped task with the local connection",
+                    "v2_runner_item_on_failed: execute a failing looped task with the local connection",
+                    "v2_runner_on_failed: execute a failing looped task with the local connection",
+                    "v2_runner_on_async_ok: execute a successful async task with the local connection",
+                    "v2_runner_on_ok: execute a successful async task with the local connection",
+                    "v2_runner_on_async_failed: execute a failing async task with the local connection",
+                    "v2_runner_on_failed: execute a failing async task with the local connection"
+                ],
+            }
+        }
+
+        if self._conntrack == expected:
+            self._display.display('FOUND EXPECTED EVENTS')
+            return
+
+        # pragma: nocover
+        self._display.display(f'ACTUAL\n{json.dumps(self._conntrack, indent=4)}')
+        self._display.display(f'EXPECTED\n{json.dumps(expected, indent=4)}')
