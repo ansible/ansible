ef94e17649a4e189ef4a8ae52ca51d80e691d287
Brian Coca <brian.coca+git@gmail.com>
2025-11-05 15:04:02 -0500

    fix tests

--- test/integration/targets/inventory_ini/runme.sh
+++ test/integration/targets/inventory_ini/runme.sh
@@ -10,22 +10,14 @@ test "$(grep -c 'SyntaxWarning' out)" -eq 0
 
 cp inventory.ini inventory.toml
 
-# check allowed_extensions, deprecation no setting
-ansible-inventory -v -i inventory.toml --list >out 2>&1
-test "$(grep -c 'Parsed inventory source with invalid extension' out)" -eq 0
-test "$(grep -c 'testhost' out)" -ne 0
-
-# check allowed_extensions, no deprecation when set
+# check allowed_extensions
 ANSIBLE_INVENTORY_PLUGIN_INI_EXT='toml' ansible-inventory -v -i inventory.toml --list >out 2>&1
-test "$(grep -c 'Parsed inventory source with invalid extension' out)" -eq 0
 test "$(grep -c 'testhost' out)" -ne 0
 
 # check allowed_extensions, set to ini
 ANSIBLE_INVENTORY_PLUGIN_INI_EXT='ini' ansible-inventory -v -i inventory.ini --list >out 2>&1
-test "$(grep -c 'Parsed inventory source with invalid extension' out)" -eq 0
 test "$(grep -c 'testhost' out)" -ne 0
 
-# should not parse file at all and no msg
+# should not parse file at all
 ANSIBLE_INVENTORY_PLUGIN_INI_EXT='ini' ansible-inventory -v -i inventory.toml --list >out 2>&1
-test "$(grep -c 'Parsed inventory source with invalid extension' out)" -eq 0
 test "$(grep -c 'testhost' out)" -eq 0
