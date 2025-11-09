42be03b59e8c58d3d344a203f2807d8c199a3563
SoulSniper1212 <warush23@gmail.com>
2025-11-08 22:30:07 -0500

    fix: resolve issue #86150
    Signed-off-by: SoulSniper1212 <warush23@gmail.com>

--- test/integration/targets/ignore_errors/runme.sh
+++ test/integration/targets/ignore_errors/runme.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eux
 
+# Run the original tests
 ansible-playbook -i ../../inventory test_ignore_errors.yml "$@"
 
 if ansible-playbook -i ../../inventory test_ignore_errors_false.yml "$@" > out.txt; then
@@ -9,3 +10,10 @@ if ansible-playbook -i ../../inventory test_ignore_errors_false.yml "$@" > out.t
 fi
 # The first task should fail and not be ignored
 grep out.txt -e 'ok=0' | grep 'ignored=0' | grep 'failed=1'
+
+# Test the new check_mode templating functionality to ensure the fix works
+# Test in normal mode (ansible_check_mode should be false, so errors should NOT be ignored when ignore_errors="{{ ansible_check_mode }}")
+ansible-playbook -i ../../inventory test_comprehensive_templating.yml "$@"
+
+# Test in check mode to verify behavior there as well
+ansible-playbook -i ../../inventory --check test_comprehensive_templating.yml "$@"
