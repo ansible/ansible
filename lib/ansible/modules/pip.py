f58f93f3d43b751d238ce52081f847daca3fbe40
Patrick Kingston <66141901+pkingstonxyz@users.noreply.github.com>
2025-11-14 13:20:50 -0500

    Fix pip package name resolution in check mode (#85623)
    Co-authored-by: Abhijeet Kasurde <akasurde@redhat.com>
    Co-authored-by: Sviatoslav Sydorenko (Святослав Сидоренко) <webknjaz@redhat.com>

--- lib/ansible/modules/pip.py
+++ lib/ansible/modules/pip.py
@@ -293,6 +293,7 @@ virtualenv:
 """
 
 import argparse
+import json
 import os
 import re
 import sys
@@ -611,6 +612,57 @@ def setup_virtualenv(module, env, chdir, out, err):
     return out, err, cmd
 
 
+def _resolve_package_names(
+        module: AnsibleModule,
+        package_list: list[Package],
+        pip: list[str],
+        python_bin: str,
+) -> list[Package]:
+    """Resolve package references in the list.
+
+    This helper function downloads metadata from PyPI
+    using ``pip install``'s ability to return JSON.
+    """
+    pkgs_to_resolve = [pkg for pkg in package_list if not pkg.has_requirement]
+
+    if not pkgs_to_resolve:
+        return package_list
+
+    # pip install --dry-run is not available in pip versions older than 22.2 and it doesn't
+    # work correctly on all cases until 24.1, so check for this and use the non-resolved
+    # package names if pip is outdated.
+    pip_dep = _get_package_info(module, "pip", python_bin)
+
+    installed_pip = LooseVersion(pip_dep.split('==')[1])
+    minimum_pip = LooseVersion("24.1")
+
+    if installed_pip < minimum_pip:
+        module.warn("Using check mode with packages from vcs urls, file paths, or archives will not behave as expected when using pip versions <24.1.")
+        return package_list  # Just use the default behavior
+
+    with tempfile.NamedTemporaryFile() as tmpfile:
+        # Uses a tmpfile instead of capturing and parsing stdout because it circumvents the need to fuss with ANSI color output
+        module.run_command(
+            [
+                *pip, 'install',
+                '--dry-run',
+                '--ignore-installed',
+                f'--report={tmpfile.name}',
+                *map(str, pkgs_to_resolve),
+            ],
+            check_rc=True,
+        )
+        report = json.load(tmpfile)
+
+    package_objects = (
+        Package(install_report['metadata']['name'], version_string=install_report['metadata']['version'])
+        for install_report in report['install']
+    )
+
+    other_packages = (pkg for pkg in package_list if pkg.has_requirement)
+    return [*other_packages, *package_objects]
+
+
 class Package:
     """Python distribution package metadata wrapper.
 
@@ -662,6 +714,11 @@ class Package:
                 op_dict[op](version_to_test, LooseVersion(ver))
                 for op, ver in self._requirement.specs
             )
+
+    @property
+    def has_requirement(self) -> bool:
+        """Compute whether the object represents complex requirement."""
+        return self._requirement is not None
 
     @staticmethod
     def canonicalize_name(name):
@@ -845,7 +902,9 @@ def main():
                                 pkg_list.append(formatted_dep)
                                 out += '%s\n' % formatted_dep
 
-                for package in packages:
+                normalized_package_list = _resolve_package_names(module, packages, pip, py_bin)
+
+                for package in normalized_package_list:
                     is_present = _is_present(module, package, pkg_list, pkg_cmd)
                     if (state == 'present' and not is_present) or (state == 'absent' and is_present):
                         changed = True
