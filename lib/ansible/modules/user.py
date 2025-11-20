285f06b350eb43b635f58470b31e71e4bd710165
Melek KNANI <melek.knani@etu.ec-lyon.fr>
2025-11-20 14:28:51 +0100

    fix ansible-test sanity check

--- lib/ansible/modules/user.py
+++ lib/ansible/modules/user.py
@@ -153,6 +153,7 @@ options:
             - The behavior is the same as C(userdel --selinux-user), check the man page for details and support.
         type: bool
         default: no
+        version_added: "2.21"
     login_class:
         description:
             - Optionally sets the user's login class, a feature of most BSD OSs.
