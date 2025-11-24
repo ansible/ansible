55c6f879f7db0a24a3c1813d55af410c6d7d02b2
Felix Fontein <felix@fontein.de>
2025-11-24 16:19:07 +0100

    runtime-metadata sanity test: ignore pre-release and build identifier (#85198) (#86160)
    * Ignore pre-release and build identifier.
    
    * Add tests.
    
    * Improve changelog fragment.
    
    
    
    * Extend tests.
    
    ---------
    
    
    (cherry picked from commit 89ba882b085a47a8cd064972dfe3348249652686)
    
    Co-authored-by: Matt Clay <matt@mystile.com>

--- test/integration/targets/ansible-test-sanity-runtime-metadata/runme.sh
+++ test/integration/targets/ansible-test-sanity-runtime-metadata/runme.sh
@@ -13,3 +13,8 @@ cd ../no_version
 ansible-test sanity --test runtime-metadata --color --truncate 0 --failure-ok --lint "${@}" 1> actual-stdout.txt 2> actual-stderr.txt
 diff -u "${TEST_DIR}/expected-no_version.txt" actual-stdout.txt
 grep -F -f "${TEST_DIR}/expected-no_version.txt" actual-stderr.txt
+
+cd ../prerelease
+ansible-test sanity --test runtime-metadata --color --truncate 0 --failure-ok --lint "${@}" 1> actual-stdout.txt 2> actual-stderr.txt
+diff -u "${TEST_DIR}/expected-prerelease.txt" actual-stdout.txt
+grep -F -f "${TEST_DIR}/expected-prerelease.txt" actual-stderr.txt
