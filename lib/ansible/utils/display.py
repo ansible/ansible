4e2136cdbd37e209f1773735af2a53307d4404cc
Abhijeet Kasurde <akasurde@redhat.com>
2025-11-24 07:17:12 -0800

    [stable-2.20] Fix getuser fallback error handling (#86144) (#86167)
    (cherry picked from commit 4184d96)
    
    Co-authored-by: Matt Clay <matt@mystile.com>

--- lib/ansible/utils/display.py
+++ lib/ansible/utils/display.py
@@ -174,7 +174,8 @@ class FilterUserInjector(logging.Filter):
 
     try:
         username = getpass.getuser()
-    except KeyError:
+    except (ImportError, KeyError, OSError):
+        # deprecated: description='only OSError is required for Python 3.13+' python_version='3.12'
         # people like to make containers w/o actual valid passwd/shadow and use host uids
         username = 'uid=%s' % os.getuid()
 
