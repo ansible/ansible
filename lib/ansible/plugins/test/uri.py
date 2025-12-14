47bc6638403d096afbf52f3d1b366d184f313d71
Akshat Sinha <akshatsinhasramhardy@gmail.com>
2025-12-14 22:35:02 +0530

    Bugfix(uri test): Fixed is_uri to correctly reject strings without a URI scheme.

--- lib/ansible/plugins/test/uri.py
+++ lib/ansible/plugins/test/uri.py
@@ -9,7 +9,8 @@ def is_uri(value, schemes=None):
     """ Will verify that the string passed is a valid 'URI', if given a list of valid schemes it will match those """
     try:
         x = urlparse(value)
-        isit = all([x.scheme is not None, x.path is not None, not schemes or x.scheme in schemes])
+        # urlparse returns empty string for scheme when not present, not None
+        isit = all([x.scheme, x.path is not None, not schemes or x.scheme in schemes])
     except Exception as e:
         isit = False
     return isit
