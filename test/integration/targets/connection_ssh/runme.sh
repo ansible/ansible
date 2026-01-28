81b7ad34ad7bcb55b156da0329b9d7e50d5b3bc2
s-hertel <19572925+s-hertel@users.noreply.github.com>
2026-01-28 08:31:34 -0500

    Add initial tests for pipelining in conjunction with other factors

--- test/integration/targets/connection_ssh/runme.sh
+++ test/integration/targets/connection_ssh/runme.sh
@@ -96,4 +96,7 @@ ANSIBLE_SSH_VERBOSITY=1 ansible ssh -m raw -a whoami -i test_connection.inventor
 # enable SSH client verbosity level 3 via var; ensure debug3 lines
 ansible ssh -m raw -a whoami -i test_connection.inventory -vvvvv -e ansible_ssh_verbosity=3 2>&1 | grep 'debug3:'
 
+# test factors that impact pipelining
+ansible-playbook test_pipelining.yml "$@"
+
 echo PASS
