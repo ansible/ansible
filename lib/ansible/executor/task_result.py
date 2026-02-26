51e3f7aa2eeae36abf2f559b93db32e96023b145
Brian Coca <brian.coca+git@gmail.com>
2026-02-26 14:09:40 -0500

    drop module_defaults from task_fields
    it can reflect the play version that might be unrelated to the current
    task and lead to confustion, if the callback author needs the actual parameters
    they can consult the task's module's options.

--- lib/ansible/executor/task_result.py
+++ lib/ansible/executor/task_result.py
@@ -56,6 +56,11 @@ class _BaseTaskResult:
         self._return_data = return_data  # FIXME: this should be immutable, but strategy result processing mutates it in some corner cases
         self.__task_fields = task_fields
 
+        # removing these as they can be inhertied and probably unrelated to the current task
+        # and could be pointing at information from other tasks
+        if self.__task_fields.get('ansible_task', {}).get('module_defaults'):
+            del self.__task_fields['ansible_task']['module_defaults']
+
     @property
     def host(self) -> Host:
         """The host associated with this result."""
