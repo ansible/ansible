766260909a77dc640a1de96d5d029178795bb489
Matt Clay <matt@mystile.com>
2026-03-02 15:40:46 -0800

    Remove long-dead test code

--- test/integration/targets/delegate_to/runme.sh
+++ test/integration/targets/delegate_to/runme.sh
@@ -45,9 +45,6 @@ trap teardown EXIT
 ANSIBLE_SSH_ARGS='-C -o ControlMaster=auto -o ControlPersist=60s -o UserKnownHostsFile=/dev/null' \
     ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook test_delegate_to.yml -i inventory -v "$@"
 
-# this test is not doing what it says it does, also relies on var that should not be available
-#ansible-playbook test_loop_control.yml -v "$@"
-
 ansible-playbook test_delegate_to_loop_randomness.yml -i inventory -v "$@"
 
 ansible-playbook delegate_and_nolog.yml -i inventory -v "$@"
