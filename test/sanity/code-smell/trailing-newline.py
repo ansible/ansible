8e20e1afee9c5a3e9c824ed81ea58731a3c1d52c
Patrick Kingston <pkingsto@redhat.com>
2026-03-05 18:13:40 -0500

    Change error, restructure try-except

--- test/sanity/code-smell/trailing-newline.py
+++ test/sanity/code-smell/trailing-newline.py
@@ -11,11 +11,11 @@ def main():
         with open(path, 'rb') as path_fd:
             try:
                 path_fd.seek(-1, 2)  # End of the file minus one byte
-                last_char = path_fd.read(1)
-                if last_char != b'\n':
-                    print(f'{path}: file should have a trailing "\\n" character instead of EOF')
-            except OSError:
-                pass
+            except OSError as e:  # catch empty files
+                continue
+            last_char = path_fd.read(1)
+            if last_char != b'\n':
+                print(f'{path}: text files should end with a newline character "\\n"')
 
 
 if __name__ == '__main__':
