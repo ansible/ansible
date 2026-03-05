ad76e96c37238876ca6003f584b64560d2e38fa6
Patrick Kingston <pkingsto@redhat.com>
2026-03-05 13:40:23 -0500

    Add file trailing \n ending sanity test

---
+++ test/sanity/code-smell/trailing-newline.py
@@ -0,0 +1,22 @@
+"""Require Unix line endings."""
+
+from __future__ import annotations
+
+import sys
+
+
+def main():
+    """Main entry point."""
+    for path in sys.argv[1:] or sys.stdin.read().splitlines():
+        with open(path, 'rb') as path_fd:
+            try:
+                path_fd.seek(-1, 2)  # End of the file minus one byte
+                last_char = path_fd.read(1)
+                if last_char != b'\n':
+                    print(f'{path}: file should have a trailing "\\n" character instead of EOF')
+            except OSError:
+                pass
+
+
+if __name__ == '__main__':
+    main()
