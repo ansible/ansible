87d6a6b2e046a51ba0c941ef1eb22a9399f8c240
Jordan Borean <jborean93@gmail.com>
2026-03-10 17:31:02 +1000

    More sanity fixes

--- .azure-pipelines/commands/powershell.sh
+++ .azure-pipelines/commands/powershell.sh
@@ -5,18 +5,17 @@ set -o pipefail -eux
 declare -a args
 IFS='/:' read -ra args <<< "$1"
 
-powershell="${args[0]}"
-pyver=default
+powershell="${args[1]}"
 
-if [ "${#args[@]}" -gt 1 ]; then
-    target="shippable/powershell/group${args[1]}/"
+if [ "${#args[@]}" -gt 2 ]; then
+    target="shippable/powershell/group${args[2]}/"
 else
     target="shippable/powershell/"
 fi
 
 # Add this once I've figured out how to get the support/integration/collections
 # into the controller
-# --controller "docker:default,python=${pyver}"
+# --controller "docker:default,python=default"
 
 # shellcheck disable=SC2086
 ansible-test integration --color -v --retry-on-error "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} ${UNSTABLE:+"$UNSTABLE"} \
