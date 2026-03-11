cd8d040ff14096df8e52f45e29922ab094c8157f
Jordan Borean <jborean93@gmail.com>
2026-03-11 20:47:56 +1000

    Fix up test integration aliases for powershell

--- .azure-pipelines/commands/powershell.sh
+++ .azure-pipelines/commands/powershell.sh
@@ -5,19 +5,17 @@ set -o pipefail -eux
 declare -a args
 IFS='/:' read -ra args <<< "$1"
 
-powershell="${args[1]}"
+target_type="${args[1]}"
+platform="${args[2]}"
+powershell="${args[3]}"
 
-if [ "${#args[@]}" -gt 2 ]; then
-    target="shippable/powershell/group${args[2]}/"
+if [ "${#args[@]}" -gt 4 ]; then
+    target="shippable/powershell/group${args[4]}/"
 else
     target="shippable/powershell/"
 fi
 
-# Add this once I've figured out how to get the support/integration/collections
-# into the controller
-
-
 # shellcheck disable=SC2086
 ansible-test integration --color -v --retry-on-error "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} ${UNSTABLE:+"$UNSTABLE"} \
     --controller "docker:default,python=default" \
-    --target "docker:ubuntu2404,powershell=${powershell}"
+    --target "${target_type}:${platform},powershell=${powershell}"
