6a6771895d8364f395e09b598e9aba5ee87e2af2
Martin Krizek <martin.krizek@gmail.com>
2026-03-12 15:13:25 +0100

    Fix max_fail_percentage with always
    Fixes #86574
    
    ci_complete

--- test/integration/targets/strategy_linear/runme.sh
+++ test/integration/targets/strategy_linear/runme.sh
@@ -7,3 +7,7 @@ ansible-playbook test_include_file_noop.yml -i inventory "$@"
 ansible-playbook task_action_templating.yml -i inventory "$@"
 
 ansible-playbook task_templated_run_once.yml -i inventory "$@"
+
+if ansible-playbook test_max_fail_percentage.yml -i inventory "$@" | tee /dev/stderr | grep -q "SHOULD_NOT_HAPPEN"; then
+  exit 1
+fi
