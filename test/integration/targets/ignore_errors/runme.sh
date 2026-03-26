729735d4d722d02c04b4e38c48ad80e7756df5e2
Matt Clay <matt@mystile.com>
2026-03-26 12:34:55 -0700

    Add disabled test for RP-induced ignore_errors+loop bug

--- test/integration/targets/ignore_errors/runme.sh
+++ test/integration/targets/ignore_errors/runme.sh
@@ -3,6 +3,10 @@ set -eux
 
 ansible-playbook -i ../../inventory test_ignore_errors.yml "$@"
 
+# RPFIX-5: BUG: ignore_errors is incorrectly handled on loop items
+# ansible-playbook -i ../../inventory test_ignore_errors_loop.yml "$@" | tee out.txt || true
+# grep 'CHECKS PASSED' out.txt
+
 ansible-playbook test_looped_errors.yml "$@" | tee out.txt
 grep "dynamichost.*ok=3.*failed=1.*ignored=2" out.txt
 
