dcbb373c01e505da5c1ab656d1332699d728118e
Jordan Borean <jborean93@gmail.com>
2026-03-26 14:56:38 +1000

    Simplify test matrix and use default container

--- .azure-pipelines/commands/powershell.sh
+++ .azure-pipelines/commands/powershell.sh
@@ -5,12 +5,10 @@ set -o pipefail -eux
 declare -a args
 IFS='/:' read -ra args <<< "$1"
 
-target_type="${args[1]}"
-platform="${args[2]}"
-powershell="${args[3]}"
+powershell="${args[1]}"
 
-if [ "${#args[@]}" -gt 4 ]; then
-    target="shippable/powershell/group${args[4]}/"
+if [ "${#args[@]}" -gt 2 ]; then
+    target="shippable/powershell/group${args[2]}/"
 else
     target="shippable/powershell/"
 fi
@@ -18,4 +16,4 @@ fi
 # shellcheck disable=SC2086
 ansible-test integration --color -v --retry-on-error "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} ${UNSTABLE:+"$UNSTABLE"} \
     --controller "docker:default,python=default" \
-    --target "${target_type}:${platform},powershell=${powershell}"
+    --target "docker:default,powershell=${powershell}"
