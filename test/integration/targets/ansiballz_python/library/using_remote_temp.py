e2ae54514e7698704cc55c0cc8762ab128e12634
Brian Coca <brian.coca+git@gmail.com>
2026-03-31 11:27:20 -0400

    remove module docs

--- test/integration/targets/ansiballz_python/library/using_remote_temp.py
+++ test/integration/targets/ansiballz_python/library/using_remote_temp.py
@@ -5,39 +5,12 @@ from __future__ import annotations
 
 
 DOCUMENTATION = """
----
-module: using_remote_temp
-version_added: historical
-short_description: see name
-description: see name
-options:
-extends_documentation_fragment:
-    - action_common_attributes
-attributes:
-    check_mode:
-        support: full
-    diff_mode:
-        support: none
-    platform:
-        platforms: posix
-author:
-  - Ansible Core Team
 """
 
 EXAMPLES = """
-# Test we where module is unzipped to by ansiballz
-
-- name: Example from an Ansible Playbook
-  using_remote_temp:
-
 """
 
 RETURN = """
-me:
-    description: output of __file__
-    returned: success
-    type: str
-    sample: /var/tmp/me.py
 """
 import pathlib
 
@@ -47,7 +20,6 @@ from ansible.module_utils.basic import AnsibleModule
 def main():
     module = AnsibleModule(
         argument_spec=dict(),
-        supports_check_mode=True
     )
 
     result = dict(
