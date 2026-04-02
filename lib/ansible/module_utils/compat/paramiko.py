2d12edf83f0b4b60b5a924165de2dd9ac3393710
s-hertel <19572925+s-hertel@users.noreply.github.com>
2026-04-02 13:38:03 -0400

    Fully remove ansible.module_utils.compat.paramiko

--- lib/ansible/module_utils/compat/paramiko.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2019 Ansible Project
-# Simplified BSD License (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
-
-from __future__ import annotations as _annotations
-
-from ansible.module_utils.common.warnings import deprecate as _deprecate
-_deprecate('The paramiko compat import is deprecated', version='2.25')
