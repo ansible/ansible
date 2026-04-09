1350996a374237df0fdbda8e181102a680f33b15
sivel / Matt Martz <matt@sivel.net>
2026-04-09 11:45:43 -0500

    Add a skill for LLMs on how to get the azp logs for analysis (#86765)

--- hacking/azp/download.py
+++ hacking/azp/download.py
@@ -27,20 +27,15 @@ import json
 import os
 import re
 import io
+import shutil
 import zipfile
-
-import requests
+import urllib.request
+import urllib.error
 
 try:
     import argcomplete
 except ImportError:
     argcomplete = None
-
-# Following changes should be made to improve the overall style:
-# TODO use new style formatting method.
-# TODO use requests session.
-# TODO type hints.
-# TODO pathlib.
 
 
 def main():
@@ -134,9 +129,8 @@ def download_run(args):
 
     if args.run_metadata:
         run_url = 'https://dev.azure.com/ansible/ansible/_apis/pipelines/%s/runs/%s?api-version=6.0-preview.1' % (args.pipeline_id, args.run)
-        run_info_response = requests.get(run_url)
-        run_info_response.raise_for_status()
-        run = run_info_response.json()
+        with urllib.request.urlopen(run_url) as run_info_response:
+            run = json.load(run_info_response)
 
         path = os.path.join(output_dir, 'run.json')
         contents = json.dumps(run, sort_keys=True, indent=4)
@@ -148,9 +142,8 @@ def download_run(args):
             with open(path, 'w') as metadata_fd:
                 metadata_fd.write(contents)
 
-    timeline_response = requests.get('https://dev.azure.com/ansible/ansible/_apis/build/builds/%s/timeline?api-version=6.0' % args.run)
-    timeline_response.raise_for_status()
-    timeline = timeline_response.json()
+    with urllib.request.urlopen('https://dev.azure.com/ansible/ansible/_apis/build/builds/%s/timeline?api-version=6.0' % args.run) as timeline_response:
+        timeline = json.load(timeline_response)
     roots = set()
     by_id = {}
     children_of = {}
@@ -185,18 +178,21 @@ def download_run(args):
 
     if args.artifacts:
         artifact_list_url = 'https://dev.azure.com/ansible/ansible/_apis/build/builds/%s/artifacts?api-version=6.0' % args.run
-        artifact_list_response = requests.get(artifact_list_url)
-        artifact_list_response.raise_for_status()
-        for artifact in artifact_list_response.json()['value']:
+        with urllib.request.urlopen(artifact_list_url) as artifact_list_response:
+            artifact_list = json.load(artifact_list_response)
+
+        for artifact in artifact_list['value']:
             if artifact['source'] not in allowed or not args.match_artifact_name.match(artifact['name']):
                 continue
             if args.verbose:
                 print('%s/%s' % (output_dir, artifact['name']))
             if not args.test:
-                response = requests.get(artifact['resource']['downloadUrl'])
-                response.raise_for_status()
-                archive = zipfile.ZipFile(io.BytesIO(response.content))
-                archive.extractall(path=output_dir)
+                with urllib.request.urlopen(artifact['resource']['downloadUrl']) as response:
+                    with io.BytesIO() as buffer:
+                        shutil.copyfileobj(response, buffer)
+                        buffer.seek(0)
+                        with zipfile.ZipFile(buffer) as archive:
+                            archive.extractall(path=output_dir)
 
     if args.console_logs:
         for r in timeline['records']:
@@ -220,9 +216,9 @@ def download_run(args):
             if args.verbose:
                 print(log_path)
             if not args.test:
-                log = requests.get(r['log']['url'])
-                log.raise_for_status()
-                open(log_path, 'wb').write(log.content)
+                with urllib.request.urlopen(r['log']['url']) as log:
+                    with open(log_path, 'wb') as log_file:
+                        shutil.copyfileobj(log, log_file)
 
 
 if __name__ == '__main__':
