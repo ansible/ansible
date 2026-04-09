fe3a45f5e36cb1dc68af936e3fe6826391e8ab6d
Sloane Hertel <19572925+s-hertel@users.noreply.github.com>
2026-04-09 18:15:54 -0400

    Fix --start-at-task (#86345) (#86816)
    * Add VALIDATE state to IteratingStates and FailedStates instead of overloading cur_regular_task, which marks the --start-at-task location.
    
    fixes #86268
    
    * Fix skipping validate_argspec by not tagging with 'always' when inheriting play-level tags. Now it aligns with play-level gather_facts tag usage.
    
    (cherry picked from commit efb9446769fef1439b7f4630a961bc435ecb7355)

--- test/integration/targets/play_iterator/runme.sh
+++ test/integration/targets/play_iterator/runme.sh
@@ -3,3 +3,10 @@
 set -eux
 
 ansible-playbook playbook.yml --start-at-task 'task 2' "$@"
+
+ansible-playbook playbook.yml --start-at-task 'task 2' \
+    --extra-vars 'setup="{{ True }}" spec="test"' "$@" | tee out.txt
+
+grep "TASK \[Gathering Facts\]" out.txt
+grep "TASK \[Validating arguments against arg spec test\]" out.txt
+grep "TASK \[task 2\]" out.txt
