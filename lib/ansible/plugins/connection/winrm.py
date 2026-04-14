cf94604b8157725933b2dd750113352bc7f9ed98
RoomWithOutRoof <166608075+Jah-yee@users.noreply.github.com>
2026-04-15 01:26:36 +0800

    Update lib/ansible/plugins/connection/winrm.py
    Co-authored-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/plugins/connection/winrm.py
+++ lib/ansible/plugins/connection/winrm.py
@@ -620,7 +620,7 @@ class Connection(ConnectionBase):
                     self._winrm_write_stdin(command_id, stdin_iterator)
 
             except Exception as ex:
-                display.error_as_warning("ERROR DURING WINRM SEND INPUT to %s. Attempting to recover." % self._winrm_host, ex)
+                display.error_as_warning(f"ERROR DURING WINRM SEND INPUT to {self._winrm_host}. Attempting to recover.", ex)
                 stdin_push_failed = True
 
             # Even on a failure above we try at least once to get the output
