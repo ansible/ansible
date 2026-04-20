10a5bad7fc885542bacb4e7a82efec5ce8a058b8
Patrick Kingston <pkingsto@redhat.com>
2026-04-20 16:03:05 -0400

    Refactor switch_version
    - duplicate code paths when handling 'HEAD'
    - Fixed a FIXME

--- lib/ansible/modules/git.py
+++ lib/ansible/modules/git.py
@@ -1032,49 +1032,34 @@ def set_remote_branch(git_path, module, dest, remote, version, depth):
 
 
 def switch_version(git_path, module, dest, remote, version, verify_commit, depth, gpg_allowlist, force=False):
-    cmd = ''
-    if version == 'HEAD':
-        branch = get_head_branch(git_path, module, dest, remote)
-        (rc, out, err) = module.run_command("%s checkout --force %s" % (git_path, branch), cwd=dest)
-        if rc != 0:
-            module.fail_json(msg="Failed to checkout branch %s" % branch,
-                             stdout=out, stderr=err, rc=rc)
-        cmd = "%s reset --hard %s/%s --" % (git_path, remote, branch)
-    else:
-        # FIXME check for local_branch first, should have been fetched already
-        if is_remote_branch(git_path, module, dest, remote, version):
-            if depth and not is_local_branch(git_path, module, dest, version):
-                # git clone --depth implies --single-branch, which makes
-                # the checkout fail if the version changes
-                # fetch the remote branch, to be able to check it out next
-                set_remote_branch(git_path, module, dest, remote, version, depth)
-            if not is_local_branch(git_path, module, dest, version):
-                cmd = "%s checkout --track -b %s %s/%s" % (git_path, version, remote, version)
+    target_version = get_head_branch(git_path, module, dest, remote) if version == 'HEAD' else version
+
+    if is_local_branch(git_path, module, dest, target_version):
+        (rc, out, err) = module.run_command("%s checkout --force %s" % (git_path, target_version), check_rc=True, cwd=dest)
+
+        if is_remote_branch(git_path, module, dest, remote, target_version):
+            if version == 'HEAD' or force:
+                cmd = "%s reset --hard %s/%s" % (git_path, remote, target_version)
             else:
-                (rc, out, err) = module.run_command("%s checkout --force %s" % (git_path, version), cwd=dest)
-                if rc != 0:
-                    module.fail_json(msg="Failed to checkout branch %s" % version, stdout=out, stderr=err, rc=rc)
-                if force:
-                    cmd = "%s reset --hard %s/%s" % (git_path, remote, version)
-                else:
-                    cmd = f"{git_path} merge --ff-only {remote}/{version}"
                 if 'ahead' in out:
-                    module.fail_json(msg=f"Unable to advance to {remote}/{version} because local commits will be lost. Use `force: yes` to overwrite local commits")
-        else:
-            cmd = "%s checkout --force %s" % (git_path, version)
-    (rc, out1, err1) = module.run_command(cmd, cwd=dest)
-    if rc != 0:
-        if version != 'HEAD':
-            module.fail_json(msg="Failed to checkout %s" % (version),
-                             stdout=out1, stderr=err1, rc=rc, cmd=cmd)
-        else:
-            module.fail_json(msg="Failed to checkout branch %s" % (branch),
-                             stdout=out1, stderr=err1, rc=rc, cmd=cmd)
+                    module.fail_json(msg=f"Unable to advance to {remote}/{target_version} because local commits will be lost. Use `force: yes` to overwrite local commits")
+                cmd = f"{git_path} merge --ff-only {remote}/{target_version}"
+            (rc, out, err) = module.run_command(cmd, check_rc=True, cwd=dest)
+
+    elif is_remote_branch(git_path, module, dest, remote, target_version):
+        if depth:
+            set_remote_branch(git_path, module, dest, remote, target_version, depth)
+        cmd = "%s checkout --track -b %s %s/%s" % (git_path, target_version, remote, target_version)
+        (rc, out, err) = module.run_command(cmd, check_rc=True, cwd=dest)
+
+    else:
+        cmd = "%s checkout --force %s" % (git_path, target_version)
+        (rc, out, err) = module.run_command(cmd, check_rc=True, cwd=dest)
 
     if verify_commit:
         verify_commit_sign(git_path, module, dest, version, gpg_allowlist)
 
-    return (rc, out1, err1)
+    return (rc, out, err)
 
 
 def verify_commit_sign(git_path, module, dest, version, gpg_allowlist):
