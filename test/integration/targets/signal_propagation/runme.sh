632d2fe479110000e8a34754f7fbbd452b261933
Matt Davis <6775756+nitzmahone@users.noreply.github.com>
2026-04-20 14:50:06 -0700

    Add WORKER_SESSION_ISOLATION config for TTY sharing (#86762)
    * Add WORKER_SESSION_ISOLATION config for TTY sharing
    
    * Worker should always be new PG leader
    
    * Use non-deprecated os.setpgid for non-session-isolates case
    * Validate session-isolated and not in signal_propagation integration test

--- test/integration/targets/signal_propagation/runme.sh
+++ test/integration/targets/signal_propagation/runme.sh
@@ -2,20 +2,24 @@
 
 set -x
 
-../test_utils/scripts/timeout.py -s SIGINT 3 -- \
-    ansible all -i inventory -m debug -a 'msg={{lookup("pipe", "sleep 33")}}' -f 10
-if [[ "$?" != "124" ]]; then
-    echo "Process was not terminated due to timeout"
-    exit 1
-fi
+echo "Test signal propagation with isolated and non-isolated workers"
+for isolation in 0 1
+do
+  ANSIBLE_WORKER_SESSION_ISOLATION=$isolation ../test_utils/scripts/timeout.py -s SIGINT 3 -- \
+      ansible all -i inventory -m debug -a 'msg={{lookup("pipe", "sleep 33")}}' -f 10
+  if [[ "$?" != "124" ]]; then
+      echo "Process was not terminated due to timeout"
+      exit 1
+  fi
 
-# a short sleep to let processes die
-sleep 2
+  # a short sleep to let processes die
+  sleep 2
 
-sleeps="$(pgrep -alf 'sleep\ 33')"
-rc="$?"
-if [[ "$rc" == "0" ]]; then
-    echo "Found lingering processes:"
-    echo "$sleeps"
-    exit 1
-fi
+  sleeps="$(pgrep -alf 'sleep\ 33')"
+  rc="$?"
+  if [[ "$rc" == "0" ]]; then
+      echo "Found lingering processes:"
+      echo "$sleeps"
+      exit 1
+  fi
+done
