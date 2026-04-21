9bb02cec4dae7aacd82054aab4a65bd018367142
Matt Davis <nitzmahone@redhat.com>
2026-04-21 15:01:19 -0700

    Ensure ansible-galaxy test server shutdown
    * Accidental reuse on test retry usually timed out prematurely

--- test/integration/targets/ansible-galaxy/files/testserver.py
+++ test/integration/targets/ansible-galaxy/files/testserver.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
 import http.server
+import os
+import pathlib
 import socketserver
 import ssl
+
 
 if __name__ == '__main__':
     Handler = http.server.SimpleHTTPRequestHandler
@@ -11,4 +14,11 @@ if __name__ == '__main__':
     httpd = socketserver.TCPServer(("", 4443), Handler)
     httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
-    httpd.serve_forever()
+    pf = pathlib.Path('./testserver.json')
+
+    try:
+        pf.write_text(str(os.getpid()))
+        httpd.serve_forever()
+    except BaseException:
+        pf.unlink(missing_ok=True)
+        raise
