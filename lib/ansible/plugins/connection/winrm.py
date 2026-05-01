69a1eb5dec29baba389733d7b65fb499312129bf
Jordan Borean <jborean93@gmail.com>
2026-05-01 15:13:31 +1000

    winrm/psrp - apply no_log to stdout/stderr logs (#86919)
    * winrm/psrp - apply no_log to stdout/stderr logs
    
    This change censors the raw stdout/stderr logging used on the `winrm`
    and `psrp` connection plugins with a verbosity level of 5 being set.
    While by default the raw output isn't sensitive if a user has set a task
    with `no_log: true` we shouldn't be displaying the raw results of that
    task.
    
    * Apply suggestions from code review
    
    Co-authored-by: Abhijeet Kasurde <akasurde@redhat.com>
    Co-authored-by: Jordan Borean <jborean93@gmail.com>
    
    ---------
    
    Co-authored-by: Abhijeet Kasurde <akasurde@redhat.com>

--- lib/ansible/plugins/connection/winrm.py
+++ lib/ansible/plugins/connection/winrm.py
@@ -634,11 +634,16 @@ class Connection(ConnectionBase):
             stdout = to_text(b_stdout)
             stderr = to_text(b_stderr)
 
+            log_stdout = stdout
+            log_stderr = stderr
+            if self._play_context.no_log:
+                log_stdout = log_stderr = '<censored due to no log>'
+
             if from_exec:
-                display.vvvvv('WINRM RESULT <Response code %d, out %r, err %r>' % (rc, stdout, stderr), host=self._winrm_host)
+                display.vvvvv(f'WINRM RESULT <Response code {rc}, out {log_stdout!r}, err {log_stderr!r}>', host=self._winrm_host)
             display.vvvvvv('WINRM RC %d' % rc, host=self._winrm_host)
-            display.vvvvvv('WINRM STDOUT %s' % stdout, host=self._winrm_host)
-            display.vvvvvv('WINRM STDERR %s' % stderr, host=self._winrm_host)
+            display.vvvvvv(f'WINRM STDOUT {log_stdout}', host=self._winrm_host)
+            display.vvvvvv(f'WINRM STDERR {log_stderr}', host=self._winrm_host)
 
             # This is done after logging so we can still see the raw stderr for
             # debugging purposes.
