0a7d9f6f4742d9a45df1b3f18a3531baa839a356
Matt Clay <matt@mystile.com>
2026-05-15 18:17:43 -0700

    ansible-test - Update base/default containers (#86958)

--- test/lib/ansible_test/_internal/commands/sanity/pylint.py
+++ test/lib/ansible_test/_internal/commands/sanity/pylint.py
@@ -13,6 +13,7 @@ from . import (
     SanitySingleVersion,
     SanityMessage,
     SanityFailure,
+    SanitySkipped,
     SanitySuccess,
     SanityTargets,
     SANITY_ROOT,
@@ -39,6 +40,7 @@ from ...util import (
     SubprocessError,
     display,
     is_subdir,
+    str_to_version,
 )
 
 from ...util_common import (
@@ -86,6 +88,12 @@ class PylintTest(SanitySingleVersion):
         return [target for target in targets if os.path.splitext(target.path)[1] == '.py' or is_subdir(target.path, 'bin')]
 
     def test(self, args: SanityConfig, targets: SanityTargets, python: PythonConfig) -> TestResult:
+        if str_to_version(python.version) >= (3, 15):
+            # see: https://github.com/uqfoundation/dill/issues/753
+            result = SanitySkipped(self.name, python.version)
+            result.reason = f'Skipping sanity test "{self.name}" due to lack of support for Python version {python.version} in the "dill" package.'
+            return result
+
         target_paths = set(target.path for target in self.filter_remote_targets(list(targets.targets)))
 
         plugin_dir = os.path.join(SANITY_ROOT, 'pylint', 'plugins')
