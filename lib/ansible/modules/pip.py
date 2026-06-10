1e7f144f997cf36589a43f7ba08525db87d29589
Ekaanksh-dev <pekanksh@gmail.com>
2026-06-10 16:31:23 +0530

    fix:relative virtualenv path when chdir is not specified

--- lib/ansible/modules/pip.py
+++ lib/ansible/modules/pip.py
@@ -782,6 +782,8 @@ def main():
     venv_created = False
     if env and chdir:
         env = os.path.join(chdir, env)
+    elif env and not os.path.isabs(env):
+        env = os.path.join(os.getcwd(),env)
 
     if umask and not isinstance(umask, int):
         try:
