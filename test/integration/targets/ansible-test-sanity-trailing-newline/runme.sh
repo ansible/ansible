0a2908a686d7d7b8bebeb2f6e50d442148a80385
Patrick Kingston <pkingsto@redhat.com>
2026-07-02 13:20:46 -0400

    Switch to if-then based approach instead of && exit 1

--- test/integration/targets/ansible-test-sanity-trailing-newline/runme.sh
+++ test/integration/targets/ansible-test-sanity-trailing-newline/runme.sh
@@ -18,22 +18,32 @@ echo -n "# test" > "$NO_NEWLINE"
 export ANSIBLE_TEST_FIX_MODE=0
 
 # Check that sanity fails NO_NEWLINE, and succeeds on HAS_NEWLINE
-python "$SCRIPT_PATH" "$HAS_NEWLINE" "$NO_NEWLINE" | tee "$RESULTS"  
+python "$SCRIPT_PATH" "$HAS_NEWLINE" "$NO_NEWLINE" | tee "$RESULTS"
 cat "$RESULTS"
-! grep -q "$HAS_NEWLINE: text files should end with a newline character" "$RESULTS" && exit 1
+if grep -q "$HAS_NEWLINE: text files should end with a newline character" "$RESULTS"; then
+    exit 1
+fi
 grep -q "$NO_NEWLINE: text files should end with a newline character" "$RESULTS"
 
 # Fix file, check no errors
 ANSIBLE_TEST_FIX_MODE=1 python "$SCRIPT_PATH" "$NO_NEWLINE" "$HAS_NEWLINE" | tee "$RESULTS"
 cat "$RESULTS"
-! grep -q "$NO_NEWLINE: text files should end with a newline character" "$RESULTS" && exit 1
-! grep -q "$HAS_NEWLINE: text files should end with a newline character" "$RESULTS" && exit 1
+if grep -q "$NO_NEWLINE: text files should end with a newline character" "$RESULTS"; then
+    exit 1
+fi
+if grep -q "$HAS_NEWLINE: text files should end with a newline character" "$RESULTS"; then
+    exit 1
+fi
 
 # Check again for no errors after fix
-python "$SCRIPT_PATH" "$HAS_NEWLINE" "$NO_NEWLINE" | tee "$RESULTS"  
+python "$SCRIPT_PATH" "$HAS_NEWLINE" "$NO_NEWLINE" | tee "$RESULTS"
 cat "$RESULTS"
-! grep -q "$NO_NEWLINE: text files should end with a newline character" "$RESULTS" && exit 1
-! grep -q "$HAS_NEWLINE: text files should end with a newline character" "$RESULTS" && exit 1
+if grep -q "$NO_NEWLINE: text files should end with a newline character" "$RESULTS"; then
+    exit 1
+fi
+if grep -q "$HAS_NEWLINE: text files should end with a newline character" "$RESULTS"; then
+    exit 1
+fi
 
 # Manual check for sanity (count newlines in last byte of file)
 [[ $(tail -c1 "$NO_NEWLINE" | wc -l) -gt 0 ]]
