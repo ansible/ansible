903c52eeb5262e0afba6baed3da22d5c95cd0781
Apoorv Darshan <ad13dtu@gmail.com>
2026-07-03 10:44:09 +0530

    Move -t to per-default shared parsers built by a factory
    Per review: instead of a None sentinel resolved in post_process_args,
    provide two small shared parent parsers for -t (one with default
    'base', one with default 'all', built by a factory) so each
    subcommand's --help states its actual default.
    
    Signed-off-by: Apoorv Darshan <ad13dtu@gmail.com>

--- lib/ansible/cli/config.py
+++ lib/ansible/cli/config.py
@@ -103,29 +103,39 @@ class ConfigCLI(CLI):
         opt_help.add_verbosity_options(common)
         common.add_argument('-c', '--config', dest='config_file',
                             help="path to configuration file, defaults to first file found in precedence.")
-        common.add_argument("-t", "--type", action="store", default=None, dest='type', choices=['all', 'base'] + list(C.CONFIGURABLE_PLUGINS),
-                            help="Filter down to a specific plugin type. Defaults to 'all' for the validate action and to 'base' otherwise.")
         common.add_argument('args', help='Specific plugin to target, requires type of plugin to be set', nargs='*')
+
+        def type_parser(default: str) -> opt_help.ArgumentParser:
+            parser = opt_help.ArgumentParser(add_help=False)
+            parser.add_argument("-t", "--type", action="store", default=default, dest='type',
+                                choices=['all', 'base'] + list(C.CONFIGURABLE_PLUGINS),
+                                help=f"Filter down to a specific plugin type. Defaults to '{default}'.")
+            return parser
+
+        # validate defaults to considering all plugin configuration (#86398),
+        # the other actions default to the base settings only
+        base_type = type_parser('base')
+        all_type = type_parser('all')
 
         subparsers = self.parser.add_subparsers(dest='action')
         subparsers.required = True
 
-        list_parser = subparsers.add_parser('list', help='Print all config options', parents=[common])
+        list_parser = subparsers.add_parser('list', help='Print all config options', parents=[common, base_type])
         list_parser.set_defaults(func=self.execute_list)
         list_parser.add_argument('--format', '-f', dest='format', action='store', choices=['json', 'yaml'], default='yaml',
                                  help='Output format for list')
 
-        dump_parser = subparsers.add_parser('dump', help='Dump configuration', parents=[common])
+        dump_parser = subparsers.add_parser('dump', help='Dump configuration', parents=[common, base_type])
         dump_parser.set_defaults(func=self.execute_dump)
         dump_parser.add_argument('--only-changed', '--changed-only', dest='only_changed', action='store_true',
                                  help="Only show configurations that have changed from the default")
         dump_parser.add_argument('--format', '-f', dest='format', action='store', choices=['json', 'yaml', 'display'], default='display',
                                  help='Output format for dump')
 
-        view_parser = subparsers.add_parser('view', help='View configuration file', parents=[common])
+        view_parser = subparsers.add_parser('view', help='View configuration file', parents=[common, base_type])
         view_parser.set_defaults(func=self.execute_view)
 
-        init_parser = subparsers.add_parser('init', help='Create initial configuration', parents=[common])
+        init_parser = subparsers.add_parser('init', help='Create initial configuration', parents=[common, base_type])
         init_parser.set_defaults(func=self.execute_init)
         init_parser.add_argument('--format', '-f', dest='format', action='store', choices=['ini', 'env', 'vars'], default='ini',
                                  help='Output format for init')
@@ -135,7 +145,7 @@ class ConfigCLI(CLI):
         validate_parser = subparsers.add_parser('validate',
                                                 help='Validate the configuration file and environment variables. '
                                                      'By default it checks the base settings and all installed plugins (use -t to narrow down).',
-                                                parents=[common])
+                                                parents=[common, all_type])
         validate_parser.set_defaults(func=self.execute_validate)
         validate_parser.add_argument('--format', '-f', dest='format', action='store', choices=['ini', 'env'] , default='ini',
                                      help='Output format for init')
@@ -143,12 +153,6 @@ class ConfigCLI(CLI):
     def post_process_args(self, options):
         options = super(ConfigCLI, self).post_process_args(options)
         display.verbosity = options.verbosity
-
-        if options.type is None:
-            # validate is most useful when it takes the configuration of
-            # installed plugins into account, the other actions default to
-            # the base settings only (#86398)
-            options.type = 'all' if options.action == 'validate' else 'base'
 
         return options
 
