35ed7e746647ce6355e7895f3de0c9ec005c774d
Abhijeet Kasurde <Akasurde@redhat.com>
2026-07-14 12:49:01 -0400

    ci_complete

--- test/integration/targets/inventory_cache/runme.sh
+++ test/integration/targets/inventory_cache/runme.sh
@@ -22,5 +22,4 @@ readhost="$(grep 'testhost[0-9]\{1,2\}' out.txt)"
 
 test "$readhost" = "$writehost"
 
-ansible-inventory -i exercise_cache.yml --graph
 test "$(ansible-inventory -i exercise_cache.yml --graph 2>&1 | grep -c '\[WARNING\]')" = 0
