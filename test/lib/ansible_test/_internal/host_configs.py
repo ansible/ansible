e91f73f28eebc09c2f15e771c0724ed72584e797
Patrick Kingston <pkingsto@redhat.com>
2026-07-28 14:19:16 -0400

    Fix application of defaults in ansible-test --target resolution

--- test/lib/ansible_test/_internal/host_configs.py
+++ test/lib/ansible_test/_internal/host_configs.py
@@ -420,7 +420,9 @@ class PosixRemoteConfig(RemoteConfig, ControllerHostConfig, PosixConfig):
         super().apply_defaults(context, defaults)
 
         self.become = self.become or defaults.become
-        self.name = defaults.name
+
+        if self.version in ('latest', ''):
+            self.name = defaults.name
 
     @property
     def have_root(self) -> bool:
@@ -451,7 +453,9 @@ class WindowsRemoteConfig(RemoteConfig, WindowsConfig):
         super().apply_defaults(context, defaults)
 
         self.connection = self.connection or defaults.connection
-        self.name = defaults.name
+
+        if self.version in ('latest', ''):
+            self.name = defaults.name
 
 
 @dataclasses.dataclass
