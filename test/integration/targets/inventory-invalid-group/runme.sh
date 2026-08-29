f899c047b4921d5a48f1796bf584ad462347b7fe
Max Mitschke <mmitschk@redhat.com>
2026-08-29 13:33:12 -0400

    Revert "Fixing tests"
    This reverts commit 76202922a86fdd0982a555b88d426a919ec5f0d7.
    
    Signed-off-by: Max Mitschke <mmitschk@redhat.com>

--- test/integration/targets/inventory-invalid-group/runme.sh
+++ test/integration/targets/inventory-invalid-group/runme.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 command=(ansible-playbook -v -i inventory.ini test.yml)
  never='Invalid characters were found in group names but not replaced'
-always='was normalized to'
+always='Invalid characters were found in group names and automatically'
 
 ANSIBLE_TRANSFORM_INVALID_GROUP_CHARS=never    "${command[@]}" -l "local-" 2>&1 | grep -c  -e "${never}"
 ANSIBLE_TRANSFORM_INVALID_GROUP_CHARS=always   "${command[@]}" -l "local_" 2>&1 | grep -c  -e "${always}"
