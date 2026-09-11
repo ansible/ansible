906bd0b83fe7a0ad3a5936345046980944da8c6a
Sloane Hertel <19572925+s-hertel@users.noreply.github.com>
2026-09-11 17:21:14 -0400

    Add no_log tests for play/role argument specs

--- test/integration/targets/play_arg_spec/library/custom_facts.py
+++ test/integration/targets/play_arg_spec/library/custom_facts.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 from __future__ import annotations
 import json
-print(json.dumps({"ansible_facts": {"custom_fact": "required"}}))
+print(json.dumps({"ansible_facts": {"custom_fact": "required", "secret_fact": "SECRET"}}))
