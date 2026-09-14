4d972f07635fdfa5cd68588412acf312bcb05127
Sloane Hertel <19572925+s-hertel@users.noreply.github.com>
2026-09-14 15:18:56 -0400

    Add no_log tests for play/role argument specs (#87522)
    Co-authored-by: David Shrewsbury <Shrews@users.noreply.github.com>

--- test/integration/targets/roles_arg_spec/runme.sh
+++ test/integration/targets/roles_arg_spec/runme.sh
@@ -15,7 +15,11 @@ set -eux
 export JUNIT_OUTPUT_DIR="${OUTPUT_DIR}"
 
 # Various simple role scenarios
-ansible-playbook test.yml -i ../../inventory "$@"
+set -o pipefail
+ansible-playbook test.yml -i ../../inventory "$@" | tee "${OUTPUT_DIR}/out.txt"
+set +o pipefail
+[ "$(grep -c "SECRET" "${OUTPUT_DIR}/out.txt")" = "0" ]
+[ "$(grep -cF "\$REDACTED\$ \$REDACTED\$" "${OUTPUT_DIR}/out.txt")" = 1 ]
 
 # More complex role test
 ansible-playbook test_complex_role_fails.yml -i ../../inventory "$@"
